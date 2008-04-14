c951eac0271032cc1f793f6a61fcf8f285ffe075
San Mehat <san@android.com>
2008-04-14 15:22:49 -0700

    ANDROID: mmc: sdio: Add concept of an 'embedded' SDIO device.
    This is required to support chips which use SDIO for signaling/
    communication but do not implement the various card enumeration registers
    as required for full SD / SDIO cards.
    
    mmc: sdio: Fix bug where we're freeing the CIS tables we never allocated when using EMBEDDED_SDIO
    mmc: Add max_blksize to embedded SDIO data
    
    Change-Id: Ibff2e3e991e5522f55ec8c6edc25ed09f2553736
    Signed-off-by: San Mehat <san@google.com>
    
    [AmitP: Rebased the original commit to align with the upstream commit
            5fc3d80ef496 ("mmc: sdio: don't use rocr to check if the card could support UHS mode")
    
            Also folded following android-4.9 commit changes into this patch
            d775b65ecaf6 ("ANDROID: mmc: core: Remove stray CONFIG_EXPERIMENTAL dependencies")]
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -439,6 +439,15 @@ struct mmc_host {
 	bool			cqe_enabled;
 	bool			cqe_on;
 
+#ifdef CONFIG_MMC_EMBEDDED_SDIO
+	struct {
+		struct sdio_cis			*cis;
+		struct sdio_cccr		*cccr;
+		struct sdio_embedded_func	*funcs;
+		int				num_funcs;
+	} embedded_sdio_data;
+#endif
+
 	unsigned long		private[0] ____cacheline_aligned;
 };
 
@@ -450,6 +459,14 @@ void mmc_remove_host(struct mmc_host *);
 void mmc_free_host(struct mmc_host *);
 int mmc_of_parse(struct mmc_host *host);
 int mmc_of_parse_voltage(struct device_node *np, u32 *mask);
+
+#ifdef CONFIG_MMC_EMBEDDED_SDIO
+extern void mmc_set_embedded_sdio_data(struct mmc_host *host,
+				       struct sdio_cis *cis,
+				       struct sdio_cccr *cccr,
+				       struct sdio_embedded_func *funcs,
+				       int num_funcs);
+#endif
 
 static inline void *mmc_priv(struct mmc_host *host)
 {
