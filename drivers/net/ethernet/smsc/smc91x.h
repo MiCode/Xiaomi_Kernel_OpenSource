78b6421ac6688f69cde7e465d9848fb83ca2551b
Brian Swetland <swetland@google.com>
2009-03-18 12:41:05 -0700

    smc91x: ARCH_MSM configuration

--- drivers/net/ethernet/smsc/smc91x.h
+++ drivers/net/ethernet/smsc/smc91x.h
@@ -223,6 +223,20 @@ SMC_outw(u16 val, void __iomem *ioaddr, int reg)
 #define SMC_outsl(a, r, p, l)	writesl((a) + (r), p, l)
 #define SMC_IRQ_FLAGS		(-1)	/* from resource */
 
+#elif defined(CONFIG_ARCH_MSM)
+
+#define SMC_CAN_USE_8BIT	0
+#define SMC_CAN_USE_16BIT	1
+#define SMC_CAN_USE_32BIT	0
+#define SMC_NOWAIT		1
+
+#define SMC_inw(a, r)		readw((a) + (r))
+#define SMC_outw(v, a, r)	writew(v, (a) + (r))
+#define SMC_insw(a, r, p, l)	readsw((a) + (r), p, l)
+#define SMC_outsw(a, r, p, l)	writesw((a) + (r), p, l)
+
+#define SMC_IRQ_FLAGS		IRQF_TRIGGER_HIGH
+
 #elif defined(CONFIG_MN10300)
 
 /*
