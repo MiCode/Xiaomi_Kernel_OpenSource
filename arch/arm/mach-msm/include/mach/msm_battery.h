489b39e1faf424c762c11f6a4e3eebbb3dbae6b2
Kiran Kandi <kkandi@quicinc.com>
2009-04-02 23:45:17 -0700

    msm_battery: battery driver for Qualcomm MSM chipsets.
    Battery driver for reporting status of battery and charger.
    
    Signed-off-by: Kiran Kandi <kkandi@quicinc.com>

---
+++ arch/arm/mach-msm/include/mach/msm_battery.h
@@ -0,0 +1,30 @@
+/* Copyright (c) 2009, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+
+#ifndef __MSM_BATTERY_H__
+#define __MSM_BATTERY_H__
+
+#define AC_CHG     0x00000001
+#define USB_CHG    0x00000002
+
+struct msm_psy_batt_pdata {
+	u32 voltage_max_design;
+	u32 voltage_min_design;
+	u32 voltage_fail_safe;
+	u32 avail_chg_sources;
+	u32 batt_technology;
+	u32 (*calculate_capacity)(u32 voltage);
+};
+
+#endif
