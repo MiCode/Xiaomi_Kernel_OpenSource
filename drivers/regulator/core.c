cb0d17a6aae0a3ed5765bc5e912de9e78a1f33a1
Bobby Crabtree <bobbyc@codeaurora.org>
2010-08-10 16:55:47 -0700

    regulator: Add support for voltage aggregation
    With this patch the regulator core aggregates voltage requests and
    applies the minimum voltage range that satisifies all consumers of
    a shared regulator supply. Only the voltages of consumers that have
    enabled the supply are aggregated.
    
    Updated voltage ranges are applied to the corresponding driver upon
    the following events:
    
    1. A consumer that has already enabled the regulator sets a voltage
    2. A consumer enables the regulator
    3. A consumer disables the regulator
    
    Change-Id: Ib5012886cb77185a2038d45f08f3758faff68f60
    Signed-off-by: David Collins <collinsd@codeaurora.org>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -178,6 +178,15 @@ static int regulator_check_voltage(struct regulator_dev *rdev,
 		return -EPERM;
 	}
 
+	/* check if requested voltage range actually overlaps the constraints */
+	if (*max_uV < rdev->constraints->min_uV ||
+	    *min_uV > rdev->constraints->max_uV) {
+		rdev_err(rdev, "requested voltage range [%d, %d] does not fit "
+			"within constraints: [%d, %d]\n", *min_uV, *max_uV,
+			rdev->constraints->min_uV, rdev->constraints->max_uV);
+		return -EINVAL;
+	}
+
 	if (*max_uV > rdev->constraints->max_uV)
 		*max_uV = rdev->constraints->max_uV;
 	if (*min_uV < rdev->constraints->min_uV)
@@ -199,6 +208,8 @@ static int regulator_check_consumers(struct regulator_dev *rdev,
 				     int *min_uV, int *max_uV)
 {
 	struct regulator *regulator;
+	int init_min_uV = *min_uV;
+	int init_max_uV = *max_uV;
 
 	list_for_each_entry(regulator, &rdev->consumer_list, list) {
 		/*
@@ -207,6 +218,13 @@ static int regulator_check_consumers(struct regulator_dev *rdev,
 		 */
 		if (!regulator->min_uV && !regulator->max_uV)
 			continue;
+
+		if (init_max_uV < regulator->min_uV
+		    || init_min_uV > regulator->max_uV)
+			rdev_err(rdev, "requested voltage range [%d, %d] does "
+				"not fit within previously voted range: "
+				"[%d, %d]\n", init_min_uV, init_max_uV,
+				regulator->min_uV, regulator->max_uV);
 
 		if (*max_uV > regulator->max_uV)
 			*max_uV = regulator->max_uV;
