54948008c293fdf48552a5c39c91c09c3eb76ed2
Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
2010-09-15 13:08:00 -0600

    msm_rmnet: merge support for RAWIP msm_rmnet device
    Add to support for msm_rmnet device using ARPHRD_RAWIP.
    
    CRs-Fixed: 1078373
    Change-Id: Ie1e5433f440b26b644cccb18083ef325129f7942
    Acked-by: Andrew Richardson <randrew@qualcomm.com>
    Signed-off-by: Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -2128,6 +2128,9 @@ static int ipv6_generate_eui64(u8 *eui, struct net_device *dev)
 		return addrconf_ifid_ieee1394(eui, dev);
 	case ARPHRD_TUNNEL6:
 		return addrconf_ifid_ip6tnl(eui, dev);
+	case ARPHRD_RAWIP:
+		get_random_bytes(eui, 8);
+		return 0;
 	}
 	return -1;
 }
@@ -3227,7 +3230,8 @@ static void addrconf_dev_config(struct net_device *dev)
 	    (dev->type != ARPHRD_IEEE1394) &&
 	    (dev->type != ARPHRD_TUNNEL6) &&
 	    (dev->type != ARPHRD_6LOWPAN) &&
-	    (dev->type != ARPHRD_NONE)) {
+	    (dev->type != ARPHRD_NONE) &&
+	    (dev->type != ARPHRD_RAWIP)) {
 		/* Alas, we support only Ethernet autoconfiguration. */
 		return;
 	}
