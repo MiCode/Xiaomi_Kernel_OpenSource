887eb0e9e98388fb35ff9be72a7137df1fdf6f82
Jeegar Lakhani <jlakhani@codeaurora.org>
2010-09-21 17:58:29 +0530

    mtd: set read ahead value of read queue to 4KB
    The read ahead value was set to 4 KB in the .29/.32 kernel since it was
    found to be the optimal value for boot time. This setting isn't present
    in the .35 kernel hence setting the read ahead value to 4 KB in the .35
    kernel
    
    Change-Id: I1cc2fb5495c321869c10c9d4c030507b67711a3e
    CRs-fixed: 256706
    Signed-off-by: Jeegar Lakhani <jlakhani@codeaurora.org>

--- drivers/mtd/mtd_blkdevs.c
+++ drivers/mtd/mtd_blkdevs.c
@@ -410,6 +410,13 @@ int add_mtd_blktrans_dev(struct mtd_blktrans_dev *new)
 		goto error3;
 
 	new->rq->queuedata = new;
+
+	/*
+	 * Empirical measurements revealed that read ahead values larger than
+	 * 4 slowed down boot time, so start out with this small value.
+	 */
+	new->rq->backing_dev_info.ra_pages = (4 * 1024) / PAGE_CACHE_SIZE;
+
 	blk_queue_logical_block_size(new->rq, tr->blksize);
 
 	queue_flag_set_unlocked(QUEUE_FLAG_NONROT, new->rq);
