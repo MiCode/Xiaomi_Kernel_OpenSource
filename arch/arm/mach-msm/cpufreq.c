c79a2e06447a6253f24142b0143ab9b1b2a1e488
David Ng <dave@codeaurora.org>
2011-03-26 06:13:32 -0700

    msm: Add sysfs interface for mfreq
    Add mfreq interface in sysfs.  This forces the system to use the
    maximum CPU clock on all CPU cores.
    (mfreq=1 enables, mfreq=0 disables)
    
    Signed-off-by: David Ng <dave@codeaurora.org>
    
    Conflicts:
    
    	arch/arm/mach-msm/cpufreq.c

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -49,13 +49,21 @@ struct cpufreq_suspend_t {
 
 static DEFINE_PER_CPU(struct cpufreq_suspend_t, cpufreq_suspend);
 
+static int override_cpu;
+
 static int set_cpu_freq(struct cpufreq_policy *policy, unsigned int new_freq)
 {
 	int ret = 0;
 	struct cpufreq_freqs freqs;
 
 	freqs.old = policy->cur;
-	freqs.new = new_freq;
+	if (override_cpu) {
+		if (policy->cur == policy->max)
+			return 0;
+		else
+			freqs.new = policy->max;
+	} else
+		freqs.new = new_freq;
 	freqs.cpu = policy->cpu;
 	cpufreq_notify_transition(&freqs, CPUFREQ_PRECHANGE);
 	ret = acpuclk_set_rate(policy->cpu, new_freq, SETRATE_CPUFREQ);
@@ -247,6 +255,25 @@ static int msm_cpufreq_pm_event(struct notifier_block *this,
 	}
 }
 
+static ssize_t store_mfreq(struct sysdev_class *class,
+			struct sysdev_class_attribute *attr,
+			const char *buf, size_t count)
+{
+	u64 val;
+
+	if (strict_strtoull(buf, 0, &val) < 0) {
+		pr_err("Invalid parameter to mfreq\n");
+		return 0;
+	}
+	if (val)
+		override_cpu = 1;
+	else
+		override_cpu = 0;
+	return count;
+}
+
+static SYSDEV_CLASS_ATTR(mfreq, 0200, NULL, store_mfreq);
+
 static struct cpufreq_driver msm_cpufreq_driver = {
 	/* lps calculations are handled here. */
 	.flags		= CPUFREQ_STICKY | CPUFREQ_CONST_LOOPS,
@@ -263,6 +290,11 @@ static struct notifier_block msm_cpufreq_pm_notifier = {
 static int __init msm_cpufreq_register(void)
 {
 	int cpu;
+
+	int err = sysfs_create_file(&cpu_sysdev_class.kset.kobj,
+			&attr_mfreq.attr);
+	if (err)
+		pr_err("Failed to create sysfs mfreq\n");
 
 	for_each_possible_cpu(cpu) {
 		mutex_init(&(per_cpu(cpufreq_suspend, cpu).suspend_mutex));
