5217ee0f955603361d6d45dcf00aa1928f053849
Shawn Guo <shawn.guo@linaro.org>
2011-04-17 00:48:36 +0800

    mmc: sdhci: add support for pre_req and post_req
    pre_req() runs dma_map_sg() post_req() runs dma_unmap_sg.
    If not calling pre_req() before sdhci_request(), request()
    will prepare the cache just like it did it before.
    It is optional to use pre_req() and post_req().
    
    Change-Id: I32cabddc4ec824515fa9ba4ff0ad7bce3679a3ef
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>
    Patch-mainline: linux-mmc @ 16/04/11,  09:43
    Signed-off-by: Asutosh Das <asutoshd@codeaurora.org>

--- include/linux/mmc/sdhci.h
+++ include/linux/mmc/sdhci.h
@@ -18,6 +18,11 @@
 #include <linux/pm_qos.h>
 #include <linux/mmc/host.h>
 
+struct sdhci_next {
+	unsigned int sg_count;
+	s32 cookie;
+};
+
 struct sdhci_host {
 	/* Data set by hardware interface driver */
 	const char *hw_name;	/* Hardware bus name */
@@ -204,6 +209,7 @@ struct sdhci_host {
 	unsigned int		cpu_dma_latency_us;
 	struct pm_qos_request	pm_qos_req_dma;
 	struct timer_list	tuning_timer;	/* Timer for tuning */
+	struct sdhci_next next_data;
 
 	unsigned long private[0] ____cacheline_aligned;
 };
