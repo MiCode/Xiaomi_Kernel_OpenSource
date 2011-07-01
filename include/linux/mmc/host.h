1cb54eaaca201a71ae239b586ffd02acc436d51c
Per Forlin <per.forlin@linaro.org>
2011-07-01 18:55:22 +0200

    mmc: core: add non-blocking mmc request function
    Previously there has only been one function mmc_wait_for_req()
    to start and wait for a request. This patch adds:
    
     * mmc_start_req() - starts a request wihtout waiting
       If there is on ongoing request wait for completion
       of that request and start the new one and return.
       Does not wait for the new command to complete.
    
    This patch also adds new function members in struct mmc_host_ops
    only called from core.c:
    
     * pre_req - asks the host driver to prepare for the next job
     * post_req - asks the host driver to clean up after a completed job
    
    The intention is to use pre_req() and post_req() to do cache maintenance
    while a request is active. pre_req() can be called while a request is
    active to minimize latency to start next job. post_req() can be used after
    the next job is started to clean up the request. This will minimize the
    host driver request end latency. post_req() is typically used before
    ending the block request and handing over the buffer to the block layer.
    
    Add a host-private member in mmc_data to be used by pre_req to mark the
    data. The host driver will then check this mark to see if the data is
    prepared or not.
    
    Signed-off-by: Per Forlin <per.forlin@linaro.org>
    Acked-by: Kyungmin Park <kyungmin.park@samsung.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Reviewed-by: Venkatraman S <svenkatr@ti.com>
    Tested-by: Sourav Poddar <sourav.poddar@ti.com>
    Tested-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>
    [kdorfman@codeaurora.org: mmc_card_removed call removed. Instead
    wait_for_completion_io used wait_for_completion]
    Signed-off-by: Konstantin Dorfman <kdorfman@codeaurora.org>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -107,6 +107,15 @@ struct mmc_host_ops {
 	 */
 	int (*enable)(struct mmc_host *host);
 	int (*disable)(struct mmc_host *host, int lazy);
+	/*
+	 * It is optional for the host to implement pre_req and post_req in
+	 * order to support double buffering of requests (prepare one
+	 * request while another request is active).
+	 */
+	void	(*post_req)(struct mmc_host *host, struct mmc_request *req,
+			    int err);
+	void	(*pre_req)(struct mmc_host *host, struct mmc_request *req,
+			   bool is_first_req);
 	void	(*request)(struct mmc_host *host, struct mmc_request *req);
 	/*
 	 * Avoid calling these three functions too often or in a "fast path",
@@ -144,6 +153,16 @@ struct mmc_host_ops {
 
 struct mmc_card;
 struct device;
+
+struct mmc_async_req {
+	/* active mmc request */
+	struct mmc_request	*mrq;
+	/*
+	 * Check error status of completed mmc request.
+	 * Returns 0 if success otherwise non zero.
+	 */
+	int (*err_check) (struct mmc_card *, struct mmc_async_req *);
+};
 
 struct mmc_host {
 	struct device		*parent;
@@ -315,6 +334,9 @@ struct mmc_host {
 		ktime_t start;
 	} perf;
 #endif
+
+	struct mmc_async_req	*areq;		/* active async req */
+
 	unsigned long		private[0] ____cacheline_aligned;
 };
 
