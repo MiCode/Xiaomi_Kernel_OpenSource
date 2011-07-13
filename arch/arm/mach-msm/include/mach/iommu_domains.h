7b79aaed3687d5446f27ef128c8387cdbc56e5d7
Laura Abbott <lauraa@codeaurora.org>
2011-07-13 14:04:58 -0700

    msm: Simplify subsystem ids
    Most contexts related to a subsystem (e.g. camera, display)
    will all share the same domain. Get rid of all the extra subsystem
    ids which just create confusion.
    
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- arch/arm/mach-msm/include/mach/iommu_domains.h
+++ arch/arm/mach-msm/include/mach/iommu_domains.h
@@ -22,28 +22,6 @@
  * These subsytem ids are NOT for public use. Please check the iommu
  * wrapper header for the properly abstracted id to pass in.
  */
-enum msm_subsystem_id {
-	INVALID_SUBSYS_ID = -1,
-	JPEGD_SUBSYS_ID,
-	VPE_SUBSYS_ID,
-	MDP0_SUBSYS_ID,
-	MDP1_SUBSYS_ID,
-	ROT_SUBSYS_ID,
-	IJPEG_SUBSYS_ID,
-	VFE_SUBSYS_ID,
-	VCODEC_A_SUBSYS_ID,
-	VCODEC_B_SUBSYS_ID,
-	GFX3D_SUBSYS_ID,
-	GFX2D0_SUBSYS_ID,
-	GFX2D1_SUBSYS_ID,
-	VIDEO_FWARE_ID,
-	MAX_SUBSYSTEM_ID
-};
-
-static inline int msm_subsystem_check_id(int subsys_id)
-{
-	return subsys_id > INVALID_SUBSYS_ID && subsys_id < MAX_SUBSYSTEM_ID;
-}
 
 #if defined(CONFIG_MSM_IOMMU)
 extern struct iommu_domain *msm_subsystem_get_domain(int subsys_id);
