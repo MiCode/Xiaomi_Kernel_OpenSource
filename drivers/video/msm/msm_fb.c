7b57bb815b5287b0c8ef618459d989475aa2f071
Ravishangar Kalyanam <rkalya@codeaurora.org>
2011-07-19 19:14:23 -0700

    msm_fb: display: Use __va instead of ioremap for framebuffer memory map
    Signed-off-by: Ravishangar Kalyanam <rkalya@codeaurora.org>

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -303,7 +303,7 @@ static int msm_fb_probe(struct platform_device *pdev)
 		fbram_size =
 			pdev->resource[0].end - pdev->resource[0].start + 1;
 		fbram_phys = (char *)pdev->resource[0].start;
-		fbram = ioremap((unsigned long)fbram_phys, fbram_size);
+		fbram = __va(fbram_phys);
 
 		if (!fbram) {
 			printk(KERN_ERR "fbram ioremap failed!\n");
