4317676a54fd89d971e3c1b16fc310fde3021812
David Collins <collinsd@codeaurora.org>
2011-07-26 15:33:38 -0700

    msm: board-msm8960: Add enable times for PM8921 regulators
    Specify board specific enable times for each of the PMIC 8921
    regulators.  Assume worst case values for each.
    
    CRs-Fixed: 296183
    Signed-off-by: David Collins <collinsd@codeaurora.org>

--- arch/arm/mach-msm/board-msm8960-regulator.c
+++ arch/arm/mach-msm/board-msm8960-regulator.c
@@ -219,7 +219,7 @@ VREG_CONSUMERS(EXT_L2) = {
 
 #define PM8921_VREG_INIT(_id, _min_uV, _max_uV, _modes, _ops, _apply_uV, \
 			 _pull_down, _always_on, _supply_regulator, \
-			 _system_uA) \
+			 _system_uA, _enable_time) \
 	{ \
 		.init_data = { \
 			.constraints = { \
@@ -239,51 +239,55 @@ VREG_CONSUMERS(EXT_L2) = {
 		.id			= PM8921_VREG_ID_##_id, \
 		.pull_down_enable	= _pull_down, \
 		.system_uA		= _system_uA, \
+		.enable_time		= _enable_time, \
 	}
 
 #define PM8921_VREG_INIT_LDO(_id, _always_on, _pull_down, _min_uV, _max_uV, \
-		_supply_regulator, _system_uA) \
+		_enable_time, _supply_regulator, _system_uA) \
 	PM8921_VREG_INIT(_id, _min_uV, _max_uV, REGULATOR_MODE_NORMAL \
 		| REGULATOR_MODE_IDLE, REGULATOR_CHANGE_VOLTAGE | \
 		REGULATOR_CHANGE_STATUS | REGULATOR_CHANGE_MODE | \
 		REGULATOR_CHANGE_DRMS, 0, _pull_down, _always_on, \
-		_supply_regulator, _system_uA)
+		_supply_regulator, _system_uA, _enable_time)
 
 #define PM8921_VREG_INIT_NLDO1200(_id, _always_on, _pull_down, _min_uV, \
-		_max_uV, _supply_regulator, _system_uA) \
+		_max_uV, _enable_time, _supply_regulator, _system_uA) \
 	PM8921_VREG_INIT(_id, _min_uV, _max_uV, REGULATOR_MODE_NORMAL \
 		| REGULATOR_MODE_IDLE, REGULATOR_CHANGE_VOLTAGE | \
 		REGULATOR_CHANGE_STATUS | REGULATOR_CHANGE_MODE | \
 		REGULATOR_CHANGE_DRMS, 0, _pull_down, _always_on, \
-		_supply_regulator, _system_uA)
+		_supply_regulator, _system_uA, _enable_time)
 
 #define PM8921_VREG_INIT_SMPS(_id, _always_on, _pull_down, _min_uV, _max_uV, \
-		_supply_regulator, _system_uA) \
+		_enable_time, _supply_regulator, _system_uA) \
 	PM8921_VREG_INIT(_id, _min_uV, _max_uV, REGULATOR_MODE_NORMAL \
 		| REGULATOR_MODE_IDLE, REGULATOR_CHANGE_VOLTAGE | \
 		REGULATOR_CHANGE_STATUS | REGULATOR_CHANGE_MODE | \
 		REGULATOR_CHANGE_DRMS, 0, _pull_down, _always_on, \
-		_supply_regulator, _system_uA)
+		_supply_regulator, _system_uA, _enable_time)
 
 #define PM8921_VREG_INIT_FTSMPS(_id, _always_on, _pull_down, _min_uV, _max_uV, \
-		_supply_regulator, _system_uA) \
+		_enable_time, _supply_regulator, _system_uA) \
 	PM8921_VREG_INIT(_id, _min_uV, _max_uV, REGULATOR_MODE_NORMAL, \
 		REGULATOR_CHANGE_VOLTAGE | REGULATOR_CHANGE_STATUS \
 		| REGULATOR_CHANGE_MODE, 0, _pull_down, _always_on, \
-		_supply_regulator, _system_uA)
+		_supply_regulator, _system_uA, _enable_time)
 
-#define PM8921_VREG_INIT_VS(_id, _always_on, _pull_down, _supply_regulator) \
+#define PM8921_VREG_INIT_VS(_id, _always_on, _pull_down, _enable_time, \
+		_supply_regulator) \
 	PM8921_VREG_INIT(_id, 0, 0, 0, REGULATOR_CHANGE_STATUS, 0, _pull_down, \
-		_always_on, _supply_regulator, 0)
+		_always_on, _supply_regulator, 0, _enable_time)
 
-#define PM8921_VREG_INIT_VS300(_id, _always_on, _pull_down, _supply_regulator) \
+#define PM8921_VREG_INIT_VS300(_id, _always_on, _pull_down, _enable_time, \
+		_supply_regulator) \
 	PM8921_VREG_INIT(_id, 0, 0, 0, REGULATOR_CHANGE_STATUS, 0, _pull_down, \
-		_always_on, _supply_regulator, 0)
+		_always_on, _supply_regulator, 0, _enable_time)
 
-#define PM8921_VREG_INIT_NCP(_id, _always_on, _min_uV, _max_uV, \
+#define PM8921_VREG_INIT_NCP(_id, _always_on, _min_uV, _max_uV, _enable_time, \
 		_supply_regulator) \
 	PM8921_VREG_INIT(_id, _min_uV, _max_uV, 0, REGULATOR_CHANGE_VOLTAGE | \
-		REGULATOR_CHANGE_STATUS, 0, 0, _always_on, _supply_regulator, 0)
+		REGULATOR_CHANGE_STATUS, 0, 0, _always_on, _supply_regulator, \
+		0, _enable_time)
 
 /* Pin control initialization */
 #define PM8921_PC_INIT(_id, _always_on, _pin_fn, _pin_ctrl, _supply_regulator) \
@@ -346,54 +350,60 @@ struct regulator_init_data msm_saw_regulator_pdata_s6 =
 /* PM8921 regulator constraints */
 struct pm8921_regulator_platform_data
 msm_pm8921_regulator_pdata[] __devinitdata = {
-	/*		      ID  always_on pd min_uV   max_uV  supply sys_uA */
-	PM8921_VREG_INIT_SMPS(S1,	 1, 1, 1225000, 1225000, NULL, 100000),
-	PM8921_VREG_INIT_SMPS(S2,	 0, 1, 1300000, 1300000, NULL, 0),
-	PM8921_VREG_INIT_SMPS(S3,	 1, 1, 1150000, 1150000, NULL, 100000),
-	PM8921_VREG_INIT_SMPS(S4,	 1, 1, 1800000, 1800000, NULL, 100000),
-	PM8921_VREG_INIT_SMPS(S7,	 0, 1, 1150000, 1150000, NULL, 100000),
-	PM8921_VREG_INIT_SMPS(S8,	 0, 1, 2200000, 2200000, NULL, 100000),
+	/*		      ID  a_on  pd min_uV   max_uV en_t supply sys_uA */
+	PM8921_VREG_INIT_SMPS(S1,    1, 1, 1225000, 1225000, 500, NULL, 100000),
+	PM8921_VREG_INIT_SMPS(S2,    0, 1, 1300000, 1300000, 500, NULL, 0),
+	PM8921_VREG_INIT_SMPS(S3,    1, 1, 1150000, 1150000, 500, NULL, 100000),
+	PM8921_VREG_INIT_SMPS(S4,    1, 1, 1800000, 1800000, 500, NULL, 100000),
+	PM8921_VREG_INIT_SMPS(S7,    0, 1, 1150000, 1150000, 500, NULL, 100000),
+	PM8921_VREG_INIT_SMPS(S8,    0, 1, 2200000, 2200000, 500, NULL, 100000),
 
-	PM8921_VREG_INIT_LDO(L1,	 1, 1, 1050000, 1050000, "8921_s4", 0),
-	PM8921_VREG_INIT_LDO(L2,	 0, 1, 1200000, 1200000, "8921_s4", 0),
-	PM8921_VREG_INIT_LDO(L3,	 0, 1, 3075000, 3075000, NULL, 0),
-	PM8921_VREG_INIT_LDO(L4,	 1, 1, 1800000, 1800000, NULL, 0),
-	PM8921_VREG_INIT_LDO(L5,	 0, 1, 2950000, 2950000, NULL, 0),
-	PM8921_VREG_INIT_LDO(L6,	 0, 1, 2950000, 2950000, NULL, 0),
-	PM8921_VREG_INIT_LDO(L7,	 1, 1, 1850000, 2950000, NULL, 0),
-	PM8921_VREG_INIT_LDO(L8,	 0, 1, 2800000, 3000000, NULL, 0),
-	PM8921_VREG_INIT_LDO(L9,	 0, 1, 2850000, 2850000, NULL, 0),
-	PM8921_VREG_INIT_LDO(L10,	 0, 1, 2900000, 2900000, NULL, 0),
-	PM8921_VREG_INIT_LDO(L11,	 0, 1, 2850000, 2850000, NULL, 0),
-	PM8921_VREG_INIT_LDO(L12,	 0, 1, 1200000, 1200000, "8921_s4", 0),
-	PM8921_VREG_INIT_LDO(L14,	 0, 1, 1800000, 1800000, NULL, 0),
-	PM8921_VREG_INIT_LDO(L15,	 0, 1, 1800000, 2950000, NULL, 0),
-	PM8921_VREG_INIT_LDO(L16,	 0, 1, 2800000, 2800000, NULL, 0),
-	PM8921_VREG_INIT_LDO(L17,	 0, 1, 1800000, 2950000, NULL, 0),
-	PM8921_VREG_INIT_LDO(L18,	 0, 1, 1300000, 1300000, "8921_s4", 0),
-	PM8921_VREG_INIT_LDO(L21,	 0, 1, 1900000, 1900000, "8921_s8", 0),
-	PM8921_VREG_INIT_LDO(L22,	 0, 1, 2750000, 2750000, NULL, 0),
-	PM8921_VREG_INIT_LDO(L23,	 1, 1, 1800000, 1800000, "8921_s8", 0),
-	PM8921_VREG_INIT_NLDO1200(L24,	 1, 1, 1150000, 1150000, "8921_s1",
+	PM8921_VREG_INIT_LDO(L1,     1, 1, 1050000, 1050000, 200, "8921_s4", 0),
+	PM8921_VREG_INIT_LDO(L2,     0, 1, 1200000, 1200000, 200, "8921_s4", 0),
+	PM8921_VREG_INIT_LDO(L3,     0, 1, 3075000, 3075000, 200, NULL, 0),
+	PM8921_VREG_INIT_LDO(L4,     1, 1, 1800000, 1800000, 200, NULL, 0),
+	PM8921_VREG_INIT_LDO(L5,     0, 1, 2950000, 2950000, 200, NULL, 0),
+	PM8921_VREG_INIT_LDO(L6,     0, 1, 2950000, 2950000, 200, NULL, 0),
+	PM8921_VREG_INIT_LDO(L7,     1, 1, 1850000, 2950000, 200, NULL, 0),
+	PM8921_VREG_INIT_LDO(L8,     0, 1, 2800000, 3000000, 200, NULL, 0),
+	PM8921_VREG_INIT_LDO(L9,     0, 1, 2850000, 2850000, 200, NULL, 0),
+	PM8921_VREG_INIT_LDO(L10,    0, 1, 2900000, 2900000, 200, NULL, 0),
+	PM8921_VREG_INIT_LDO(L11,    0, 1, 2850000, 2850000, 200, NULL, 0),
+	PM8921_VREG_INIT_LDO(L12,    0, 1, 1200000, 1200000, 200, "8921_s4", 0),
+	PM8921_VREG_INIT_LDO(L14,    0, 1, 1800000, 1800000, 200, NULL, 0),
+	PM8921_VREG_INIT_LDO(L15,    0, 1, 1800000, 2950000, 200, NULL, 0),
+	PM8921_VREG_INIT_LDO(L16,    0, 1, 2800000, 2800000, 200, NULL, 0),
+	PM8921_VREG_INIT_LDO(L17,    0, 1, 1800000, 2950000, 200, NULL, 0),
+	PM8921_VREG_INIT_LDO(L18,    0, 1, 1300000, 1300000, 200, "8921_s4", 0),
+	PM8921_VREG_INIT_LDO(L21,    0, 1, 1900000, 1900000, 200, "8921_s8", 0),
+	PM8921_VREG_INIT_LDO(L22,    0, 1, 2750000, 2750000, 200, NULL, 0),
+	PM8921_VREG_INIT_LDO(L23,    1, 1, 1800000, 1800000, 200, "8921_s8", 0),
+	PM8921_VREG_INIT_NLDO1200(L24, 1, 1, 1150000, 1150000, 200, "8921_s1",
 		10000),
-	PM8921_VREG_INIT_NLDO1200(L25,	 1, 1, 1225000, 1225000, "8921_s1", 0),
-	PM8921_VREG_INIT_NLDO1200(L26,	 0, 1, 1050000, 1050000, "8921_s7", 0),
-	PM8921_VREG_INIT_NLDO1200(L27,	 0, 1, 1050000, 1050000, "8921_s7", 0),
-	PM8921_VREG_INIT_NLDO1200(L28,	 0, 1, 1050000, 1050000, "8921_s7", 0),
-	PM8921_VREG_INIT_LDO(L29,	 0, 1, 2050000, 2100000, "8921_s8", 0),
+	PM8921_VREG_INIT_NLDO1200(L25, 1, 1, 1225000, 1225000, 200, "8921_s1",
+		0),
+	PM8921_VREG_INIT_NLDO1200(L26, 0, 1, 1050000, 1050000, 200, "8921_s7",
+		0),
+	PM8921_VREG_INIT_NLDO1200(L27, 0, 1, 1050000, 1050000, 200, "8921_s7",
+		0),
+	PM8921_VREG_INIT_NLDO1200(L28, 0, 1, 1050000, 1050000, 200, "8921_s7",
+		0),
+	PM8921_VREG_INIT_LDO(L29,    0, 1, 2050000, 2100000, 200, "8921_s8", 0),
 
-	PM8921_VREG_INIT_VS(LVS1,	 0, 1,			 "8921_s4"),
-	PM8921_VREG_INIT_VS300(LVS2,	 0, 1,			 "8921_s1"),
-	PM8921_VREG_INIT_VS(LVS3,	 0, 1,			 "8921_s4"),
-	PM8921_VREG_INIT_VS(LVS4,	 0, 1,			 "8921_s4"),
-	PM8921_VREG_INIT_VS(LVS5,	 0, 1,			 "8921_s4"),
-	PM8921_VREG_INIT_VS(LVS6,	 0, 1,			 "8921_s4"),
-	PM8921_VREG_INIT_VS(LVS7,	 0, 1,			 "8921_s4"),
+	/*		    ID   always_on  pd			en_t supply */
+	PM8921_VREG_INIT_VS(LVS1,	 0, 1,			 0, "8921_s4"),
+	PM8921_VREG_INIT_VS300(LVS2,	 0, 1,			 0, "8921_s1"),
+	PM8921_VREG_INIT_VS(LVS3,	 0, 1,			 0, "8921_s4"),
+	PM8921_VREG_INIT_VS(LVS4,	 0, 1,			 0, "8921_s4"),
+	PM8921_VREG_INIT_VS(LVS5,	 0, 1,			 0, "8921_s4"),
+	PM8921_VREG_INIT_VS(LVS6,	 0, 1,			 0, "8921_s4"),
+	PM8921_VREG_INIT_VS(LVS7,	 0, 1,			 0, "8921_s4"),
 
-	PM8921_VREG_INIT_VS300(USB_OTG,  0, 1,			 "ext_5v"),
-	PM8921_VREG_INIT_VS300(HDMI_MVS, 0, 1,			 "ext_5v"),
+	PM8921_VREG_INIT_VS300(USB_OTG,  0, 1,			 0, "ext_5v"),
+	PM8921_VREG_INIT_VS300(HDMI_MVS, 0, 1,			 0, "ext_5v"),
 
-	PM8921_VREG_INIT_NCP(NCP,	 0,    1800000, 1800000, "8921_l6"),
+	/*		     ID  always_on    min_uV   max_uV  en_t  supply */
+	PM8921_VREG_INIT_NCP(NCP,	 0,   1800000, 1800000, 200, "8921_l6"),
 };
 
 int msm_pm8921_regulator_pdata_len __devinitdata =
