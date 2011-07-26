16f61afb1c63c592ff0399451b29bc8e9d9962a4
Trilok Soni <tsoni@codeaurora.org>
2011-07-26 16:06:58 +0530

    board: msm7x27a: Add separate init machine hook for rumi3
    Adding separate init machine hook for rumi3 will avoid
    un-necessary machine macro checks.
    
    Signed-off-by: Trilok Soni <tsoni@codeaurora.org>

--- arch/arm/mach-msm/board-msm7x27a.c
+++ arch/arm/mach-msm/board-msm7x27a.c
@@ -3100,6 +3100,13 @@ static struct platform_device msm_proccomm_regulator_dev = {
 	}
 };
 
+static void __init msm7627a_rumi3_init(void)
+{
+	msm7x27a_init_ebi2();
+	platform_add_devices(rumi_sim_devices,
+			ARRAY_SIZE(rumi_sim_devices));
+}
+
 #define LED_GPIO_PDM		96
 #define UART1DM_RX_GPIO		45
 
@@ -3139,29 +3146,23 @@ static void __init msm7x2x_init(void)
 	msm_device_uart_dm1.dev.platform_data = &msm_uart_dm1_pdata;
 #endif
 
-	if (machine_is_msm7x27a_rumi3()) {
-		platform_add_devices(rumi_sim_devices,
-				ARRAY_SIZE(rumi_sim_devices));
-	}
-	if (machine_is_msm7x27a_surf() || machine_is_msm7x27a_ffa()) {
 #ifdef CONFIG_USB_MSM_OTG_72K
-		msm_otg_pdata.swfi_latency =
-			msm7x27a_pm_data
+	msm_otg_pdata.swfi_latency =
+		msm7x27a_pm_data
 		[MSM_PM_SLEEP_MODE_RAMP_DOWN_AND_WAIT_FOR_INTERRUPT].latency;
-		msm_device_otg.dev.platform_data = &msm_otg_pdata;
+	msm_device_otg.dev.platform_data = &msm_otg_pdata;
 #endif
-		msm_device_gadget_peripheral.dev.platform_data =
-							&msm_gadget_pdata;
-		msm7x27a_cfg_smsc911x();
-		platform_add_devices(msm_footswitch_devices,
-			     msm_num_footswitch_devices);
-		platform_add_devices(surf_ffa_devices,
-				ARRAY_SIZE(surf_ffa_devices));
-		msm_fb_add_devices();
+	msm_device_gadget_peripheral.dev.platform_data =
+		&msm_gadget_pdata;
+	msm7x27a_cfg_smsc911x();
+	platform_add_devices(msm_footswitch_devices,
+			msm_num_footswitch_devices);
+	platform_add_devices(surf_ffa_devices,
+			ARRAY_SIZE(surf_ffa_devices));
+	msm_fb_add_devices();
 #ifdef CONFIG_USB_EHCI_MSM_72K
-		msm7x2x_init_host();
+	msm7x2x_init_host();
 #endif
-	}
 
 	msm_pm_set_platform_data(msm7x27a_pm_data,
 				ARRAY_SIZE(msm7x27a_pm_data));
@@ -3219,7 +3220,7 @@ MACHINE_START(MSM7X27A_RUMI3, "QCT MSM7x27a RUMI3")
 	.map_io		= msm_common_io_init,
 	.reserve	= msm7x27a_reserve,
 	.init_irq	= msm_init_irq,
-	.init_machine	= msm7x2x_init,
+	.init_machine	= msm7627a_rumi3_init,
 	.timer		= &msm_timer,
 	.init_early     = msm7x2x_init_early,
 MACHINE_END
