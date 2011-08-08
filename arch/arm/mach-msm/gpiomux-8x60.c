0c62bf6056a7b681ec6a8a2fbb1238fa35061860
Venkat Gopalakrishnan <venkatg@codeaurora.org>
2011-08-08 12:17:11 -0700

    mmc: msm_sdcc: Configure sdcc2 and sdcc5 GPIO's to always PULL_UP.
    As per the SD spec the CMD and DAT lines need to be set
    to PULL_UP, but we set it to PULL_NONE during sleep. This was
    causing the client to get into an error state during sleep/wakeup
    sequence on rare scenarios. Now setting them to PULL_UP on
    both active and sleep state.
    
    Signed-off-by: Venkat Gopalakrishnan <venkatg@codeaurora.org>

--- arch/arm/mach-msm/gpiomux-8x60.c
+++ arch/arm/mach-msm/gpiomux-8x60.c
@@ -187,7 +187,7 @@ static struct gpiomux_setting sdcc2_clk_actv_cfg = {
 static struct gpiomux_setting sdcc2_suspend_config = {
 	.func = GPIOMUX_FUNC_GPIO,
 	.drv = GPIOMUX_DRV_2MA,
-	.pull = GPIOMUX_PULL_NONE,
+	.pull = GPIOMUX_PULL_UP,
 };
 
 static struct gpiomux_setting sdcc5_dat_0_3_cmd_actv_cfg = {
@@ -205,7 +205,7 @@ static struct gpiomux_setting sdcc5_clk_actv_cfg = {
 static struct gpiomux_setting sdcc5_suspend_config = {
 	.func = GPIOMUX_FUNC_GPIO,
 	.drv = GPIOMUX_DRV_2MA,
-	.pull = GPIOMUX_PULL_NONE,
+	.pull = GPIOMUX_PULL_UP,
 };
 
 static struct gpiomux_setting aux_pcm_active_config = {
