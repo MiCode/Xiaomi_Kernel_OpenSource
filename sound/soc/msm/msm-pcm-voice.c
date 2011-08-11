74a13cbe503db1864058962a9d4099568a1f5701
Helen Zeng <xiaoyunz@codeaurora.org>
2011-08-10 18:20:10 -0700

    ASoC: msm: Add mixer command to enable/disable widevoice
    Signed-off-by: Helen Zeng <xiaoyunz@codeaurora.org>

--- sound/soc/msm/msm-pcm-voice.c
+++ sound/soc/msm/msm-pcm-voice.c
@@ -238,6 +238,25 @@ static int msm_voice_tty_mode_put(struct snd_kcontrol *kcontrol,
 
 	return 0;
 }
+static int msm_voice_widevoice_put(struct snd_kcontrol *kcontrol,
+				struct snd_ctl_elem_value *ucontrol)
+{
+	int wv_enable = ucontrol->value.integer.value[0];
+
+	pr_debug("%s: wv enable=%d\n", __func__, wv_enable);
+
+	voc_set_widevoice_enable(wv_enable);
+
+	return 0;
+}
+
+static int msm_voice_widevoice_get(struct snd_kcontrol *kcontrol,
+				struct snd_ctl_elem_value *ucontrol)
+{
+	ucontrol->value.integer.value[0] = voc_get_widevoice_enable();
+	return 0;
+}
+
 static struct snd_kcontrol_new msm_voice_controls[] = {
 	SOC_SINGLE_EXT("Voice Tx Mute", SND_SOC_NOPM, 0, 1, 0,
 				msm_voice_mute_get, msm_voice_mute_put),
@@ -245,6 +264,8 @@ static struct snd_kcontrol_new msm_voice_controls[] = {
 				msm_voice_volume_get, msm_voice_volume_put),
 	SOC_ENUM_EXT("TTY Mode", msm_tty_mode_enum[0], msm_voice_tty_mode_get,
 				msm_voice_tty_mode_put),
+	SOC_SINGLE_EXT("Widevoice Enable", SND_SOC_NOPM, 0, 1, 0,
+			msm_voice_widevoice_get, msm_voice_widevoice_put),
 };
 
 static struct snd_pcm_ops msm_pcm_ops = {
