5a4f1ba19d0172fe3566f3df5bc78844d84c485b
Matt Wagantall <mattw@codeaurora.org>
2011-08-18 18:13:03 -0700

    msm: hdmi: Register device with clock driver and rename clocks
    Rename the clocks per the new naming convention under which
    similarly named clocks are distinguished between using their
    associated device's name and ID.
    
    Change-Id: I35f164e902003b1c3cd7243bc08d00b4c5d37b0c
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

--- drivers/video/msm/hdmi_msm.c
+++ drivers/video/msm/hdmi_msm.c
@@ -3416,23 +3416,23 @@ static int __devinit hdmi_msm_probe(struct platform_device *pdev)
 		return 0;
 	}
 
-	hdmi_msm_state->hdmi_app_clk = clk_get(NULL, "hdmi_app_clk");
+	hdmi_msm_state->hdmi_app_clk = clk_get(&pdev->dev, "core_clk");
 	if (IS_ERR(hdmi_msm_state->hdmi_app_clk)) {
-		DEV_ERR("'hdmi_app_clk' clk not found\n");
+		DEV_ERR("'core_clk' clk not found\n");
 		rc = IS_ERR(hdmi_msm_state->hdmi_app_clk);
 		goto error;
 	}
 
-	hdmi_msm_state->hdmi_m_pclk = clk_get(NULL, "hdmi_m_pclk");
+	hdmi_msm_state->hdmi_m_pclk = clk_get(&pdev->dev, "master_iface_clk");
 	if (IS_ERR(hdmi_msm_state->hdmi_m_pclk)) {
-		DEV_ERR("'hdmi_m_pclk' clk not found\n");
+		DEV_ERR("'master_iface_clk' clk not found\n");
 		rc = IS_ERR(hdmi_msm_state->hdmi_m_pclk);
 		goto error;
 	}
 
-	hdmi_msm_state->hdmi_s_pclk = clk_get(NULL, "hdmi_s_pclk");
+	hdmi_msm_state->hdmi_s_pclk = clk_get(&pdev->dev, "slave_iface_clk");
 	if (IS_ERR(hdmi_msm_state->hdmi_s_pclk)) {
-		DEV_ERR("'hdmi_s_pclk' clk not found\n");
+		DEV_ERR("'slave_iface_clk' clk not found\n");
 		rc = IS_ERR(hdmi_msm_state->hdmi_s_pclk);
 		goto error;
 	}
