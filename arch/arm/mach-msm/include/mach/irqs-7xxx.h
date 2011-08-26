caf114bc5d104f7291eb4ab850f7daff490cf678
Neil Leeder <nleeder@codeaurora.org>
2011-08-26 10:19:37 -0400

    msm: rename PMU irq for 7xxx
    Make the name the same as for other platforms so it can be
    used by the common code in pmu.c
    
    Signed-off-by: Neil Leeder <nleeder@codeaurora.org>

--- arch/arm/mach-msm/include/mach/irqs-7xxx.h
+++ arch/arm/mach-msm/include/mach/irqs-7xxx.h
@@ -59,7 +59,7 @@
 #define INT_AXI_MPU_SMI      (32 + 7)
 #define INT_AXI_MPU_EBI1     (32 + 8)
 #define INT_AD_HSSD          (32 + 9)
-#define INT_ARM11_PMU        (32 + 10)
+#define INT_ARMQC_PERFMON    (32 + 10)
 #define INT_ARM11_DMA        (32 + 11)
 #define INT_TSIF_IRQ         (32 + 12)
 #define INT_UART1DM_IRQ      (32 + 13)
