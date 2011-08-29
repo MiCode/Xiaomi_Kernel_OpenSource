7f80896c2144ad9f4b42326f65d61e09b2ed62d3
Manu Gautam <mgautam@codeaurora.org>
2011-08-29 16:38:07 +0530

    msm: bam_dmux: Return -ENODEV if BAM_DMUX is not compiled-in
    Define BAM_DMUX APIs, returning -ENODEV, if kernel is built
    without BAM_DMUX support. This allows client drivers to
    call BAM_DMUX APIs without checking for its support.
    
    Signed-off-by: Manu Gautam <mgautam@codeaurora.org>
    
    Conflicts:
    
    	arch/arm/mach-msm/include/mach/bam_dmux.h

--- arch/arm/mach-msm/include/mach/bam_dmux.h
+++ arch/arm/mach-msm/include/mach/bam_dmux.h
@@ -29,6 +29,23 @@ enum {
 	BAM_DMUX_NUM_CHANNELS
 };
 
+/* event type enum */
+enum {
+	BAM_DMUX_RECEIVE, /* data is struct sk_buff */
+	BAM_DMUX_WRITE_DONE, /* data is struct sk_buff */
+};
+
+/*
+ * Open a bam_dmux logical channel
+ *     id - the logical channel to open
+ *     priv - private data pointer to be passed to the notify callback
+ *     notify - event callback function
+ *          priv - private data pointer passed to msm_bam_dmux_open()
+ *          event_type - type of event
+ *          data - data relevant to event.  May not be valid. See event_type
+ *                    enum for valid cases.
+ */
+#ifdef CONFIG_MSM_BAM_DMUX
 int msm_bam_dmux_open(uint32_t id, void *priv,
 		       void (*receive_cb)(void *, struct sk_buff *),
 		       void (*write_done)(void *, struct sk_buff *));
@@ -36,5 +53,22 @@ int msm_bam_dmux_open(uint32_t id, void *priv,
 int msm_bam_dmux_close(uint32_t id);
 
 int msm_bam_dmux_write(uint32_t id, struct sk_buff *skb);
+#else
+int msm_bam_dmux_open(uint32_t id, void *priv,
+		       void (*receive_cb)(void *, struct sk_buff *),
+		       void (*write_done)(void *, struct sk_buff *))
+{
+	return -ENODEV;
+}
 
+int msm_bam_dmux_close(uint32_t id)
+{
+	return -ENODEV;
+}
+
+int msm_bam_dmux_write(uint32_t id, struct sk_buff *skb)
+{
+	return -ENODEV;
+}
+#endif
 #endif /* _BAM_DMUX_H */
