cc075d4644f9727c0ea04a95a902f142692d51e5
Neil Leeder <nleeder@codeaurora.org>
2011-08-31 10:04:23 -0400

    arm: oprofile: Add OProfile support for 7x27a or Cortex A5 cpu type
    This patch adds support to recognise cortex A5 cputype for oprofile,
    which in turn enables the armv7 generic events to be picked.
    
    Change-Id: I025f4b361b4e85bbeea4eefc71572f4ae5b602e4
    Signed-off-by: Neil Leeder <nleeder@codeaurora.org>
    (cherry picked from commit 1540f31eb76f2e775d1a04edb6d9f8397d4f6637)

--- arch/arm/oprofile/common.c
+++ arch/arm/oprofile/common.c
@@ -37,6 +37,7 @@ static struct op_perf_name {
 	{ "xscale1",		"arm/xscale2"	},
 	{ "v6",			"arm/armv6"	},
 	{ "v6mpcore",		"arm/mpcore"	},
+	{ "ARMv7 Cortex-A5",	"arm/armv7"	},
 	{ "ARMv7 Cortex-A8",	"arm/armv7"	},
 	{ "ARMv7 Cortex-A9",	"arm/armv7-ca9"	},
 };
