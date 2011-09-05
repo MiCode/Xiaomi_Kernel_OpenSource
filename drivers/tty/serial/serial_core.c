d64dacaac94e76c89d9e68923a893f97c9628255
Mayank Rana <mrana@codeaurora.org>
2011-09-05 12:47:47 +0530

    serial_core: Add uart_change_pm in resume path
    UART clock rate is set to zero while disabling the uart clock for
    subsystem restart feature. It is required to set the uart clock rate
    to 7372800 Hz while enabling UART clock. Hence use uart_change_pm in
    serial core to call msm_hsl_power api to achieve the same.
    
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>

--- drivers/tty/serial/serial_core.c
+++ drivers/tty/serial/serial_core.c
@@ -2005,7 +2005,13 @@ int uart_resume_port(struct uart_driver *drv, struct uart_port *uport)
 		 */
 		if (port->tty && port->tty->termios && termios.c_cflag == 0)
 			termios = *(port->tty->termios);
-
+		/*
+		 * As we need to set the uart clock rate back to 7.3 MHz.
+		 * We need this change.
+		 *
+		 */
+		if (console_suspend_enabled)
+			uart_change_pm(state, 0);
 		uport->ops->set_termios(uport, &termios, NULL);
 		if (console_suspend_enabled)
 			console_start(uport->cons);
