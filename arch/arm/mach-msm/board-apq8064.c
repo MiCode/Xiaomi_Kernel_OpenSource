7d4ce03222c052e655aca52e2344cc18d75429d6
Amol Jadi <amoljadi@codeaurora.org>
2011-09-09 17:07:18 -0700

    apq8064: sdcc: disable bam for RUMI3
    Signed-off-by: Amol Jadi <amoljadi@codeaurora.org>

--- arch/arm/mach-msm/board-apq8064.c
+++ arch/arm/mach-msm/board-apq8064.c
@@ -80,7 +80,7 @@ static struct mmc_platform_data *apq8064_sdc3_pdata;
 
 static void __init apq8064_init_mmc(void)
 {
-	if (machine_is_apq8064_sim()) {
+	if ((machine_is_apq8064_rumi3()) || machine_is_apq8064_sim()) {
 		if (apq8064_sdc1_pdata) {
 			apq8064_sdc1_pdata->disable_bam = true;
 			apq8064_sdc1_pdata->disable_runtime_pm = true;
