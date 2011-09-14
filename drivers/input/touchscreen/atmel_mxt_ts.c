7e447434cc372fe10f509c38401d63fd2621b0ab
Amy Maloche <amaloche@codeaurora.org>
2011-09-14 11:36:30 -0700

    input: atmel_mxt_ts: Update proper reset time for mxt1386
    Signed-off-by: Amy Maloche <amaloche@codeaurora.org>

--- drivers/input/touchscreen/atmel_mxt_ts.c
+++ drivers/input/touchscreen/atmel_mxt_ts.c
@@ -190,8 +190,8 @@
 #define MXT_BACKUP_VALUE	0x55
 #define MXT_BACKUP_TIME		25	/* msec */
 #define MXT224_RESET_TIME		65	/* msec */
-#define MXT1386_RESET_TIME		200	/* msec */
-#define MXT_RESET_TIME		200	/* msec */
+#define MXT1386_RESET_TIME		250	/* msec */
+#define MXT_RESET_TIME		250	/* msec */
 #define MXT_RESET_NOCHGREAD		400	/* msec */
 
 #define MXT_FWRESET_TIME	175	/* msec */
@@ -817,6 +817,22 @@ static int mxt_get_object_table(struct mxt_data *data)
 	return 0;
 }
 
+static void mxt_reset_delay(struct mxt_data *data)
+{
+	struct mxt_info *info = &data->info;
+
+	switch (info->family_id) {
+	case MXT224_ID:
+		msleep(MXT224_RESET_TIME);
+		break;
+	case MXT1386_ID:
+		msleep(MXT1386_RESET_TIME);
+		break;
+	default:
+		msleep(MXT_RESET_TIME);
+	}
+}
+
 static int mxt_initialize(struct mxt_data *data)
 {
 	struct i2c_client *client = data->client;
@@ -861,9 +877,9 @@ static int mxt_initialize(struct mxt_data *data)
 					&command_register);
 		if (error)
 			return error;
-		msleep(2);
-	} while ((command_register != 0) && (timeout_counter++ <= 100));
-	if (timeout_counter >= 100) {
+		usleep_range(1000, 2000);
+	} while ((command_register != 0) && (++timeout_counter <= 100));
+	if (timeout_counter > 100) {
 		dev_err(&client->dev, "No response after backup!\n");
 		return -EIO;
 	}
@@ -873,27 +889,7 @@ static int mxt_initialize(struct mxt_data *data)
 	mxt_write_object(data, MXT_GEN_COMMAND,
 			MXT_COMMAND_RESET, 1);
 
-	if (data->pdata->read_chg == NULL) {
-		msleep(MXT_RESET_NOCHGREAD);
-	} else {
-		switch (info->family_id) {
-		case MXT224_ID:
-			msleep(MXT224_RESET_TIME);
-			break;
-		case MXT1386_ID:
-			msleep(MXT1386_RESET_TIME);
-			break;
-		default:
-			msleep(MXT_RESET_TIME);
-		}
-		timeout_counter = 0;
-		while ((timeout_counter++ <= 100) && data->pdata->read_chg())
-			msleep(2);
-		if (timeout_counter >= 100) {
-			dev_err(&client->dev, "No response after reset!\n");
-			return -EIO;
-		}
-	}
+	mxt_reset_delay(data);
 
 	/* Update matrix size at info struct */
 	error = mxt_read_reg(client, MXT_MATRIX_X_SIZE, &val);
@@ -989,7 +985,8 @@ static int mxt_load_fw(struct device *dev, const char *fn)
 	/* Change to the bootloader mode */
 	mxt_write_object(data, MXT_GEN_COMMAND,
 			MXT_COMMAND_RESET, MXT_BOOT_VALUE);
-	msleep(MXT_RESET_TIME);
+
+	mxt_reset_delay(data);
 
 	/* Change to slave address of bootloader */
 	if (client->addr == MXT_APP_LOW)
@@ -1156,7 +1153,7 @@ static int mxt_power_on(struct mxt_data *data, bool on)
 		}
 	}
 
-	msleep(50);
+	msleep(100);
 
 	return 0;
 
@@ -1275,7 +1272,7 @@ static int mxt_resume(struct device *dev)
 	mxt_write_object(data, MXT_GEN_COMMAND,
 			MXT_COMMAND_RESET, 1);
 
-	msleep(MXT_RESET_TIME);
+	mxt_reset_delay(data);
 
 	mutex_lock(&input_dev->mutex);
 
