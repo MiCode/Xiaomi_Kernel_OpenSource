28ef14eef19bde7df5c7ca1ecaacdd6ed8ba0c45
Kiran Kandi <kkandi@codeaurora.org>
2011-09-16 15:45:47 -0700

    ASoC: Do not schedule playback DAPM turn off if pmdown_time is zero
    CRs-fixed: 307501
    Change-Id: I369178bd9d142f094ca7b7184b6ff1e0d06bed53
    Signed-off-by: Kiran Kandi <kkandi@codeaurora.org>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -794,9 +794,15 @@ int soc_pcm_close(struct snd_pcm_substream *substream)
 
 	if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK) {
 		/* start delayed pop wq here for playback streams */
-		codec_dai->pop_wait = 1;
-		schedule_delayed_work(&rtd->delayed_work,
+		if (rtd->pmdown_time) {
+			codec_dai->pop_wait = 1;
+			schedule_delayed_work(&rtd->delayed_work,
 			msecs_to_jiffies(rtd->pmdown_time));
+		} else {
+			snd_soc_dapm_stream_event(rtd,
+			codec_dai->driver->playback.stream_name,
+			SND_SOC_DAPM_STREAM_STOP);
+		}
 	} else {
 		/* capture streams can be powered down now */
 		snd_soc_dapm_stream_event(rtd,
