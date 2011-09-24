35d12f4f3ac9e36153659ade86b855a4ef6f9634
Jay Chokshi <jchokshi@codeaurora.org>
2011-09-23 17:46:00 -0700

    mfd: pm8xxx-irq: Don't request for IRQ if devirq is 0
    Add check if PMIC devirq is set to valid value or not.
    
    Signed-off-by: Jay Chokshi <jchokshi@codeaurora.org>
    
    Conflicts:
    
    	drivers/mfd/pm8xxx-irq.c

--- drivers/mfd/pm8xxx-irq.c
+++ drivers/mfd/pm8xxx-irq.c
@@ -404,13 +404,17 @@ struct pm_irq_chip *  __devinit pm8xxx_irq_init(struct device *dev,
 #endif
 	}
 
-	rc = request_irq(devirq, pm8xxx_irq_handler, pdata->irq_trigger_flag,
+	if (devirq != 0) {
+		rc = request_irq(devirq, pm8xxx_irq_handler,
+				pdata->irq_trigger_flag,
 				"pm8xxx_usr_irq", chip);
-	if (rc) {
-		pr_err("failed to request_irq for %d rc=%d\n", devirq, rc);
-		return ERR_PTR(rc);
+		if (rc) {
+			pr_err("failed to request_irq for %d rc=%d\n",
+								devirq, rc);
+		} else {
+			irq_set_irq_wake(devirq, 1);
+		}
 	}
-	irq_set_irq_wake(devirq, 1);
 
 	return chip;
 }
