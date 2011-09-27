3bc87ac1f8c759e297e74039f7f29f0c3c863122
Swaminathan Sathappan <Swami@codeaurora.org>
2011-09-27 15:36:36 -0700

    msm: audio: qdsp6v2: Disable idle wakelocks for pcm driver
    Change-Id: I8705869cfeb4549a54c3fe866c382c4dadbe009a
    CRs-fixed: 307647
    Signed-off-by: Swaminathan Sathappan <Swami@codeaurora.org>

--- arch/arm/mach-msm/qdsp6v2/pcm_out.c
+++ arch/arm/mach-msm/qdsp6v2/pcm_out.c
@@ -52,7 +52,6 @@ struct pcm {
 	atomic_t out_stopped;
 	atomic_t out_prefill;
 	struct wake_lock wakelock;
-	struct wake_lock idlelock;
 };
 
 void pcm_out_cb(uint32_t opcode, uint32_t token,
@@ -77,14 +76,12 @@ static void audio_prevent_sleep(struct pcm *audio)
 {
 	pr_debug("%s:\n", __func__);
 	wake_lock(&audio->wakelock);
-	wake_lock(&audio->idlelock);
 }
 
 static void audio_allow_sleep(struct pcm *audio)
 {
 	pr_debug("%s:\n", __func__);
 	wake_unlock(&audio->wakelock);
-	wake_unlock(&audio->idlelock);
 }
 
 static int pcm_out_enable(struct pcm *pcm)
@@ -336,8 +333,6 @@ static int pcm_out_open(struct inode *inode, struct file *file)
 	atomic_set(&pcm->out_opened, 1);
 	snprintf(name, sizeof name, "audio_pcm_%x", pcm->ac->session);
 	wake_lock_init(&pcm->wakelock, WAKE_LOCK_SUSPEND, name);
-	snprintf(name, sizeof name, "audio_pcm_idle_%x", pcm->ac->session);
-	wake_lock_init(&pcm->idlelock, WAKE_LOCK_IDLE, name);
 
 	rc = auddev_register_evt_listner(pcm->stream_event,
 					AUDDEV_CLNT_DEC,
@@ -441,7 +436,6 @@ static int pcm_out_release(struct inode *inode, struct file *file)
 	q6asm_audio_client_free(pcm->ac);
 	audio_allow_sleep(pcm);
 	wake_lock_destroy(&pcm->wakelock);
-	wake_lock_destroy(&pcm->idlelock);
 	mutex_destroy(&pcm->lock);
 	mutex_destroy(&pcm->write_lock);
 	kfree(pcm);
