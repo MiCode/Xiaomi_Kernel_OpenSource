2c704c926f9416376c4ce283f3f442a9727dba0b
Lucille Sylvester <lsylvest@codeaurora.org>
2011-09-27 13:30:39 -0600

    msm: kgsl: Add a periodic check to turn the TZ algorithm on
    In use cases with very high graphics for some time followed by
    low constant usage, the power level is never dropped.  Periodically
    allow the TZ algorithm to run to see if any change in the external
    conditions has occured.
    
    CRs-fixed: 309131
    Signed-off-by: Lucille Sylvester <lsylvest@codeaurora.org>

--- drivers/gpu/msm/kgsl_pwrscale_trustzone.c
+++ drivers/gpu/msm/kgsl_pwrscale_trustzone.c
@@ -27,9 +27,12 @@
 struct tz_priv {
 	int governor;
 	unsigned int no_switch_cnt;
+	unsigned int skip_cnt;
 };
 
 #define SWITCH_OFF		200
+#define SWITCH_OFF_RESET_TH	40
+#define SKIP_COUNTER		500
 #define TZ_RESET_ID		0x3
 #define TZ_UPDATE_ID		0x4
 
@@ -128,11 +131,20 @@ static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 
 	/* If the GPU has stayed in turbo mode for a while, *
 	 * stop writing out values. */
-	if (pwr->active_pwrlevel)
+	if (pwr->active_pwrlevel == 0) {
+		if (priv->no_switch_cnt > SWITCH_OFF) {
+			priv->skip_cnt++;
+			if (priv->skip_cnt > SKIP_COUNTER) {
+				priv->no_switch_cnt -= SWITCH_OFF_RESET_TH;
+				priv->skip_cnt = 0;
+			}
+			return;
+		}
+		priv->no_switch_cnt++;
+	} else {
 		priv->no_switch_cnt = 0;
-	else if (priv->no_switch_cnt > SWITCH_OFF)
-		return;
-	priv->no_switch_cnt++;
+	}
+
 	val = __secure_tz_entry(TZ_UPDATE_ID,
 				stats.total_time - stats.busy_time);
 	if (val)
