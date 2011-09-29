c383e04b54e6787bdd2603d36fc078506ad066ce
Rob Herring <rob.herring@calxeda.com>
2011-09-28 21:25:31 -0500

    ARM: gic: add irq_domain support
    Convert the gic interrupt controller to use irq domains in preparation
    for device-tree binding and MULTI_IRQ. This allows for translation between
    GIC interrupt IDs and Linux irq numbers.
    
    The meaning of irq_offset has changed. It now is just the number of skipped
    GIC interrupt IDs for the controller. It will be 16 for primary GIC and 32
    for secondary GICs.
    
    Change-Id: Ie94f443aedea16036a6d2c30ff5ea4b3e4cfe9e1
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>
    Cc: Marc Zyngier <marc.zyngier@arm.com>
    Reviewed-by: Jamie Iles <jamie@jamieiles.com>
    Tested-by: Thomas Abraham <thomas.abraham@linaro.org>
    Acked-by: Grant Likely <grant.likely@secretlab.ca>
    [tsoni@codeaurora.org: Fix merge conflicts]
    Signed-off-by: Trilok Soni <tsoni@codeaurora.org>

--- arch/arm/include/asm/hardware/gic.h
+++ arch/arm/include/asm/hardware/gic.h
@@ -33,6 +33,9 @@
 #define GIC_DIST_SOFTINT		0xf00
 
 #ifndef __ASSEMBLY__
+#include <linux/irqdomain.h>
+struct device_node;
+
 extern void __iomem *gic_cpu_base_addr;
 extern struct irq_chip gic_arch_extn;
 
@@ -59,6 +62,9 @@ struct gic_chip_data {
 	u32 saved_spi_target[DIV_ROUND_UP(1020, 4)];
 	u32 __percpu *saved_ppi_enable;
 	u32 __percpu *saved_ppi_conf;
+#endif
+#ifdef CONFIG_IRQ_DOMAIN
+	struct irq_domain domain;
 #endif
 	unsigned int gic_irqs;
 };
