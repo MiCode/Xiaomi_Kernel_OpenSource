c7e4ebda4ab85605b3b2aba66411989d3dafdb7d
Gustavo F. Padovan <padovan@profusion.mobi>
2011-10-07 01:29:51 -0300

    Bluetooth: Rename hidp_find_connection()
    hidp_get_connection() makes more sense because we hold a reference to the
    connection inside this function.
    
    Change-Id: Id8e8b2fda98a86f45728e1aeace0c35fb47f4d82
    Signed-off-by: Gustavo F. Padovan <padovan@profusion.mobi>
    Signed-off-by: Sumit Bajpai <sbajpai@codeaurora.org>

--- net/bluetooth/hidp/core.c
+++ net/bluetooth/hidp/core.c
@@ -635,7 +635,7 @@ static int hidp_session(void *arg)
 	return 0;
 }
 
-static struct hci_conn *hidp_find_connection(struct hidp_session *session)
+static struct hci_conn *hidp_get_connection(struct hidp_session *session)
 {
 	bdaddr_t *src = &bt_sk(session->ctrl_sock->sk)->src;
 	bdaddr_t *dst = &bt_sk(session->ctrl_sock->sk)->dst;
@@ -852,7 +852,7 @@ int hidp_add_connection(struct hidp_connadd_req *req, struct socket *ctrl_sock, 
 		goto failed;
 	}
 
-	session->conn = hidp_find_connection(session);
+	session->conn = hidp_get_connection(session);
 	if (!session->conn) {
 		err = -ENOTCONN;
 		goto failed;
