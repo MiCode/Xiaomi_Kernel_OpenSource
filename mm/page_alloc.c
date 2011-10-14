6e206b79df384f33b9b24f5a2bfd7a9b51e272c2
Larry Bassel <lbassel@codeaurora.org>
2011-10-14 10:59:07 -0700

    mm: use required fixed size of movable zone if FIX_MOVABLE_ZONE
    If FIX_MOVABLE_ZONE is enabled, we want a specific size and
    location of ZONE_MOVABLE.
    
    Change-Id: I0b858c7310cd328e1118abc9d5fe6f364bb4ffad
    Signed-off-by: Larry Bassel <lbassel@codeaurora.org>
    (cherry picked from commit e6436864f939e77226c8e971610539649ed5f869)

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4885,6 +4885,9 @@ static void __init find_zone_movable_pfns_for_nodes(void)
 	unsigned long totalpages = early_calculate_totalpages();
 	int usable_nodes = nodes_weight(node_states[N_MEMORY]);
 
+#ifdef CONFIG_FIX_MOVABLE_ZONE
+	required_movablecore = movable_reserved_size >> PAGE_SHIFT;
+#endif
 	/*
 	 * If movablecore was specified, calculate what size of
 	 * kernelcore that corresponds so that memory usable for
