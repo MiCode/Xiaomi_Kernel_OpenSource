2b992ad2e54d1e372540d6a793a5cc7223f1a6a8
Stepan Moskovchenko <stepanm@codeaurora.org>
2011-10-18 17:24:18 -0700

    msm: iomap: Add I/O register definitions for MSM8930
    Add the I/O register definitions and macros to map the
    devices on the MSM8930 platform.
    
    Change-Id: I74ae53d2a5725a1c8085b9e97f10348ed7f5a352
    Signed-off-by: Stepan Moskovchenko <stepanm@codeaurora.org>

--- arch/arm/mach-msm/include/mach/msm_iomap.h
+++ arch/arm/mach-msm/include/mach/msm_iomap.h
@@ -43,8 +43,8 @@
 #define IOMEM(x)	((void __force __iomem *)(x))
 #endif
 
-#if defined(CONFIG_ARCH_MSM8960) || defined(CONFIG_ARCH_APQ8064) \
-	|| defined(CONFIG_ARCH_MSM9615)
+#if defined(CONFIG_ARCH_MSM8960) || defined(CONFIG_ARCH_APQ8064) || \
+	defined(CONFIG_ARCH_MSM8930) || defined(CONFIG_ARCH_MSM9615)
 /* Unified iomap */
 
 #define MSM_TMR_BASE		IOMEM(0xFA000000)	/*  4K	*/
@@ -82,6 +82,7 @@
 #endif
 
 #include "msm_iomap-8960.h"
+#include "msm_iomap-8930.h"
 #include "msm_iomap-8064.h"
 #include "msm_iomap-9615.h"
 
