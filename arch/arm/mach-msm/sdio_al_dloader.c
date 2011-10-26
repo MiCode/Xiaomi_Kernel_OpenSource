d0d12e78f960d444c88e17620152e2cdbb979951
Alhad Purnapatre <alhadp@codeaurora.org>
2011-10-26 14:04:42 -0700

    msm: sdio: Fix wrong quoting in #include of local files
    Includes of local files should not use < and >, rather use double quotes.
    This causes errors in building kernel on some toolchains.
    
    Change-Id: I0f09e00ffbc3de95d33e9c743ba0e7461ed41ab2
    Signed-off-by: Alhad Purnapatre <alhadp@codeaurora.org>

--- arch/arm/mach-msm/sdio_al_dloader.c
+++ arch/arm/mach-msm/sdio_al_dloader.c
@@ -25,7 +25,7 @@
 #include <linux/dma-mapping.h>
 #include <mach/dma.h>
 #include <linux/mmc/sdio_func.h>
-#include <sdio_al_private.h>
+#include "sdio_al_private.h"
 #include <linux/tty.h>
 #include <linux/tty_flip.h>
 #include <linux/kthread.h>
--- arch/arm/mach-msm/sdio_al_test.c
+++ arch/arm/mach-msm/sdio_al_test.c
@@ -30,7 +30,7 @@
 #include <linux/wakelock.h>
 #include <linux/uaccess.h>
 
-#include <sdio_al_private.h>
+#include "sdio_al_private.h"
 #include <linux/debugfs.h>
 
 #include <linux/kthread.h>
