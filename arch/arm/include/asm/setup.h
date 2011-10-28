43c1fa63205420eaf30d41e28fe77049acac9d0e
Olav Haugan <ohaugan@codeaurora.org>
2011-10-28 14:34:48 -0700

    ARM: Fix bank_pfn_end macro overflow
    bank_pfn_end macro overflows when physical memory space
    configuration ends at 0xFFFFFFFF.
    
    The macro adds start and size together before converting
    to a page frame number. Change the macro to convert start
    and size first and then add them together.
    
    Change-Id: If091fd860e6cc94f2221164bd79bf34415819e66
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>
    (cherry picked from commit b40b91eb8101b996f6dca5ccfee91b1949aa27e6)

--- arch/arm/include/asm/setup.h
+++ arch/arm/include/asm/setup.h
@@ -43,7 +43,8 @@ extern struct meminfo meminfo;
 	for (iter = 0; iter < (mi)->nr_banks; iter++)
 
 #define bank_pfn_start(bank)	__phys_to_pfn((bank)->start)
-#define bank_pfn_end(bank)	__phys_to_pfn((bank)->start + (bank)->size)
+#define bank_pfn_end(bank)	(__phys_to_pfn((bank)->start) + \
+						__phys_to_pfn((bank)->size))
 #define bank_pfn_size(bank)	((bank)->size >> PAGE_SHIFT)
 #define bank_phys_start(bank)	(bank)->start
 #define bank_phys_end(bank)	((bank)->start + (bank)->size)
