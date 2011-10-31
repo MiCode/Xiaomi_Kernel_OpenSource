3647ac3c7e87fbde76aa51a51eeebd252b1879f3
Laura Abbott <lauraa@codeaurora.org>
2011-10-31 14:09:53 -0700

    gpu: ion: Add debug information for individual heaps
    Currently, the debugfs information provides aggregated data
    for all carveout heaps. This isn't particularly helpful for
    determining allocation problems with individual carveout heaps.
    Fix this to give data for individual carveout heaps.
    
    Change-Id: I60a10851052412e20c40f1862d6dcc22093fd982
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- drivers/gpu/ion/ion.c
+++ drivers/gpu/ion/ion.c
@@ -1237,7 +1237,7 @@ static const struct file_operations ion_fops = {
 };
 
 static size_t ion_debug_heap_total(struct ion_client *client,
-				   enum ion_heap_type type)
+				   enum ion_heap_ids id)
 {
 	size_t size = 0;
 	struct rb_node *n;
@@ -1247,7 +1247,7 @@ static size_t ion_debug_heap_total(struct ion_client *client,
 		struct ion_handle *handle = rb_entry(n,
 						     struct ion_handle,
 						     node);
-		if (handle->buffer->heap->type == type)
+		if (handle->buffer->heap->id == id)
 			size += handle->buffer->size;
 	}
 	mutex_unlock(&client->lock);
@@ -1265,7 +1265,7 @@ static int ion_debug_heap_show(struct seq_file *s, void *unused)
 		struct ion_client *client = rb_entry(n, struct ion_client,
 						     node);
 		char task_comm[TASK_COMM_LEN];
-		size_t size = ion_debug_heap_total(client, heap->type);
+		size_t size = ion_debug_heap_total(client, heap->id);
 		if (!size)
 			continue;
 
@@ -1277,7 +1277,7 @@ static int ion_debug_heap_show(struct seq_file *s, void *unused)
 	for (n = rb_first(&dev->kernel_clients); n; n = rb_next(n)) {
 		struct ion_client *client = rb_entry(n, struct ion_client,
 						     node);
-		size_t size = ion_debug_heap_total(client, heap->type);
+		size_t size = ion_debug_heap_total(client, heap->id);
 		if (!size)
 			continue;
 		seq_printf(s, "%16.s %16u %16u\n", client->name, client->pid,
