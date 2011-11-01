7e31792023479cd5cd6f0c6d1a24edb4e7ac1a92
Kevin Chan <ktchan@codeaurora.org>
2011-11-01 15:54:10 -0700

    msm: camera: Add i2c util functions for camera sensors
    Add support for bitwise register set/unset
    Add register polling function
    Add register compare function
    
    Change-Id: I10d2e3af02685dc4496723319f96c42d2ce3db70
    Signed-off-by: Kevin Chan <ktchan@codeaurora.org>

--- drivers/media/video/msm/io/msm_camera_i2c.c
+++ drivers/media/video/msm/io/msm_camera_i2c.c
@@ -138,34 +138,155 @@ int32_t msm_camera_i2c_write_seq(struct msm_camera_i2c_client *client,
 	return rc;
 }
 
+int32_t msm_camera_i2c_set_mask(struct msm_camera_i2c_client *client,
+	uint16_t addr, uint16_t mask,
+	enum msm_camera_i2c_data_type data_type, uint16_t set_mask)
+{
+	int32_t rc;
+	uint16_t reg_data;
+
+	rc = msm_camera_i2c_read(client, addr, &reg_data, data_type);
+	if (rc < 0) {
+		S_I2C_DBG("%s read fail\n", __func__);
+		return rc;
+	}
+	S_I2C_DBG("%s addr: 0x%x data: 0x%x setmask: 0x%x\n",
+			__func__, addr, reg_data, mask);
+
+	if (set_mask)
+		reg_data |= mask;
+	else
+		reg_data &= ~mask;
+	S_I2C_DBG("%s write: 0x%x\n", __func__, reg_data);
+
+	rc = msm_camera_i2c_write(client, addr, reg_data, data_type);
+	if (rc < 0)
+		S_I2C_DBG("%s write fail\n", __func__);
+
+	return rc;
+}
+
+int32_t msm_camera_i2c_compare(struct msm_camera_i2c_client *client,
+	uint16_t addr, uint16_t data,
+	enum msm_camera_i2c_data_type data_type)
+{
+	int32_t rc = -EIO;
+	uint16_t reg_data = 0;
+	int data_len = 0;
+	switch (data_type) {
+	case MSM_CAMERA_I2C_BYTE_DATA:
+	case MSM_CAMERA_I2C_WORD_DATA:
+		data_len = data_type;
+		break;
+	case MSM_CAMERA_I2C_SET_BYTE_MASK:
+	case MSM_CAMERA_I2C_UNSET_BYTE_MASK:
+		data_len = MSM_CAMERA_I2C_BYTE_DATA;
+		break;
+	case MSM_CAMERA_I2C_SET_WORD_MASK:
+	case MSM_CAMERA_I2C_UNSET_WORD_MASK:
+		data_len = MSM_CAMERA_I2C_WORD_DATA;
+		break;
+	default:
+		pr_err("%s: Unsupport data type: %d\n", __func__, data_type);
+		break;
+	}
+
+	rc = msm_camera_i2c_read(client,
+		addr, &reg_data, data_len);
+	if (rc < 0)
+		return rc;
+
+	rc = 0;
+	switch (data_type) {
+	case MSM_CAMERA_I2C_BYTE_DATA:
+	case MSM_CAMERA_I2C_WORD_DATA:
+		if (data == reg_data)
+			return rc;
+		break;
+	case MSM_CAMERA_I2C_SET_BYTE_MASK:
+	case MSM_CAMERA_I2C_SET_WORD_MASK:
+		if ((reg_data & data) == data)
+			return rc;
+		break;
+	case MSM_CAMERA_I2C_UNSET_BYTE_MASK:
+	case MSM_CAMERA_I2C_UNSET_WORD_MASK:
+		if (!(reg_data & data))
+			return rc;
+		break;
+	default:
+		pr_err("%s: Unsupport data type: %d\n", __func__, data_type);
+		break;
+	}
+
+	S_I2C_DBG("%s: Register and data does not match\n", __func__);
+	rc = 1;
+	return rc;
+}
+
+int32_t msm_camera_i2c_poll(struct msm_camera_i2c_client *client,
+	uint16_t addr, uint16_t data,
+	enum msm_camera_i2c_data_type data_type)
+{
+	int32_t rc = -EIO;
+	int i;
+	S_I2C_DBG("%s: addr: 0x%x data: 0x%x dt: %d\n",
+		__func__, addr, data, data_type);
+
+	for (i = 0; i < 20; i++) {
+		rc = msm_camera_i2c_compare(client,
+			addr, data, data_type);
+		if (rc == 0 || rc < 0)
+			break;
+		usleep_range(10000, 11000);
+	}
+	return rc;
+}
+
 int32_t msm_camera_i2c_write_tbl(struct msm_camera_i2c_client *client,
-	struct msm_camera_i2c_reg_conf *reg_conf_tbl, uint8_t size,
+	struct msm_camera_i2c_reg_conf *reg_conf_tbl, uint16_t size,
 	enum msm_camera_i2c_data_type data_type)
 {
 	int i;
 	int32_t rc = -EFAULT;
 	for (i = 0; i < size; i++) {
-		rc = msm_camera_i2c_write(
-			client,
-			reg_conf_tbl->reg_addr,
-			reg_conf_tbl->reg_data, data_type);
-		if (rc < 0)
-			break;
-		reg_conf_tbl++;
-	}
-	return rc;
-}
+		enum msm_camera_i2c_data_type dt;
+		if (reg_conf_tbl->dt == 0)
+			dt = data_type;
+		else
+			dt = reg_conf_tbl->dt;
 
-int32_t msm_camera_i2c_write_dt_tbl(struct msm_camera_i2c_client *client,
-	struct msm_camera_i2c_reg_dt_conf *reg_conf_tbl, uint16_t size)
-{
-	int i;
-	int32_t rc = -EFAULT;
-	for (i = 0; i < size; i++) {
-		rc = msm_camera_i2c_write(
-			client,
-			reg_conf_tbl->reg_addr,
-			reg_conf_tbl->reg_data, reg_conf_tbl->dt);
+		switch (dt) {
+		case MSM_CAMERA_I2C_BYTE_DATA:
+		case MSM_CAMERA_I2C_WORD_DATA:
+			rc = msm_camera_i2c_write(
+				client,
+				reg_conf_tbl->reg_addr,
+				reg_conf_tbl->reg_data, dt);
+			break;
+		case MSM_CAMERA_I2C_SET_BYTE_MASK:
+			rc = msm_camera_i2c_set_mask(client,
+				reg_conf_tbl->reg_addr, reg_conf_tbl->reg_data,
+				MSM_CAMERA_I2C_BYTE_DATA, 1);
+			break;
+		case MSM_CAMERA_I2C_UNSET_BYTE_MASK:
+			rc = msm_camera_i2c_set_mask(client,
+				reg_conf_tbl->reg_addr, reg_conf_tbl->reg_data,
+				MSM_CAMERA_I2C_BYTE_DATA, 0);
+			break;
+		case MSM_CAMERA_I2C_SET_WORD_MASK:
+			rc = msm_camera_i2c_set_mask(client,
+				reg_conf_tbl->reg_addr, reg_conf_tbl->reg_data,
+				MSM_CAMERA_I2C_WORD_DATA, 1);
+			break;
+		case MSM_CAMERA_I2C_UNSET_WORD_MASK:
+			rc = msm_camera_i2c_set_mask(client,
+				reg_conf_tbl->reg_addr, reg_conf_tbl->reg_data,
+				MSM_CAMERA_I2C_WORD_DATA, 0);
+			break;
+		default:
+			pr_err("%s: Unsupport data type: %d\n", __func__, dt);
+			break;
+		}
 		if (rc < 0)
 			break;
 		reg_conf_tbl++;
@@ -202,7 +323,7 @@ int32_t msm_camera_i2c_read(struct msm_camera_i2c_client *client,
 	else
 		*data = buf[0] << 8 | buf[1];
 
-	S_I2C_DBG("%s addr = 0x%x data: 0x%x", __func__, addr, *data);
+	S_I2C_DBG("%s addr = 0x%x data: 0x%x\n", __func__, addr, *data);
 	return rc;
 }
 
@@ -243,15 +364,16 @@ int32_t msm_sensor_write_conf_array(struct msm_camera_i2c_client *client,
 			struct msm_camera_i2c_conf_array *array, uint16_t index)
 {
 	int32_t rc;
-	if (array[index].data_type != MSM_CAMERA_I2C_HYBRID_DATA) {
-		rc = msm_camera_i2c_write_tbl(client,
-			(struct msm_camera_i2c_reg_conf *) array[index].conf,
-			array[index].size, array[index].data_type);
-	} else {
-		rc = msm_camera_i2c_write_dt_tbl(client,
-			(struct msm_camera_i2c_reg_dt_conf *) array[index].conf,
-			array[index].size);
-	}
+	if (array[index].pre_process != NULL)
+		array[index].pre_process();
+
+	rc = msm_camera_i2c_write_tbl(client,
+		(struct msm_camera_i2c_reg_conf *) array[index].conf,
+		array[index].size, array[index].data_type);
+
+	if (array[index].post_process != NULL)
+		array[index].post_process();
+
 	if (array[index].delay > 20)
 		msleep(array[index].delay);
 	else
