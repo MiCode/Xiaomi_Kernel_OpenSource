93650bb78fb95107bd405654a5a26a348eb1f5fd
Lucille Sylvester <lsylvest@codeaurora.org>
2011-11-02 14:37:10 -0700

    msm: kgsl: Decrease the idle_timeout to 50ms
    Originally higher due to high latencies.  Now that they've decreased
    we can go to SLEEP earlier.
    
    CRs-fixed: 312297
    Change-Id: I2a3813b12c0a72f3b75699fcd377508e7eb1044e
    Signed-off-by: Lucille Sylvester <lsylvest@codeaurora.org>

--- arch/arm/mach-msm/devices-8960.c
+++ arch/arm/mach-msm/devices-8960.c
@@ -2266,7 +2266,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.init_level = 0,
 	.num_levels = 5,
 	.set_grp_async = NULL,
-	.idle_timeout = HZ/5,
+	.idle_timeout = HZ/20,
 	.nap_allowed = true,
 	.clk_map = KGSL_CLK_CORE | KGSL_CLK_IFACE | KGSL_CLK_MEM_IFACE,
 #ifdef CONFIG_MSM_BUS_SCALING
