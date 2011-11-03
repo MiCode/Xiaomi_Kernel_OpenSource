380cc0b6840ff8eabf621553adde4d31ad5451bf
Amy Maloche <amaloche@codeaurora.org>
2011-11-03 12:55:04 -0700

    input: atmel_mxt_ts: add mxt224E family reset specifics
    Change-Id: Ia07f558a52e1f821da031da7b8f7e6b9afb6bfe9
    Signed-off-by: Amy Maloche <amaloche@codeaurora.org>

--- drivers/input/touchscreen/atmel_mxt_ts.c
+++ drivers/input/touchscreen/atmel_mxt_ts.c
@@ -32,6 +32,7 @@
 
 /* Family ID */
 #define MXT224_ID	0x80
+#define MXT224E_ID	0x81
 #define MXT1386_ID	0xA0
 
 /* Version */
@@ -206,6 +207,7 @@
 #define MXT_BACKUP_VALUE	0x55
 #define MXT_BACKUP_TIME		25	/* msec */
 #define MXT224_RESET_TIME		65	/* msec */
+#define MXT224E_RESET_TIME		22	/* msec */
 #define MXT1386_RESET_TIME		250	/* msec */
 #define MXT_RESET_TIME		250	/* msec */
 #define MXT_RESET_NOCHGREAD		400	/* msec */
@@ -833,6 +835,9 @@ static void mxt_reset_delay(struct mxt_data *data)
 	switch (info->family_id) {
 	case MXT224_ID:
 		msleep(MXT224_RESET_TIME);
+		break;
+	case MXT224E_ID:
+		msleep(MXT224E_RESET_TIME);
 		break;
 	case MXT1386_ID:
 		msleep(MXT1386_RESET_TIME);
