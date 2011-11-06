e6bee116fbcc0721e35658beb3c65fdab4dde057
Willie Ruan <wruan@codeaurora.org>
2011-11-06 15:52:52 -0800

    power: core: add power supply APIs
    Add two APIs, 1) set_current_limit API and 2) set_charging_by.
    set_current_limit api is used for external control, such as for USB
    driver to tell a charger driver the maximum current to draw.
    set_charging_by is used to change the state of the charger.
    
    Change-Id: I147eab37836b54627ca1458167eb117ba3eacd7a
    Signed-off-by: Willie Ruan <wruan@codeaurora.org>

--- drivers/power/power_supply_core.c
+++ drivers/power/power_supply_core.c
@@ -25,6 +25,44 @@ EXPORT_SYMBOL_GPL(power_supply_class);
 
 static struct device_type power_supply_dev_type;
 
+/**
+ * power_supply_set_current_limit - set current limit
+ * @psy:	the power supply to control
+ * @limit:	current limit in uA from the power supply.
+ *		0 will disable the power supply.
+ *
+ * This function will set a maximum supply current from a source
+ * and it will disable the charger when limit is 0.
+ */
+int power_supply_set_current_limit(struct power_supply *psy, int limit)
+{
+	const union power_supply_propval ret = {limit,};
+
+	if (psy->set_property)
+		return psy->set_property(psy, POWER_SUPPLY_PROP_CURRENT_MAX,
+								&ret);
+
+	return -ENXIO;
+}
+EXPORT_SYMBOL_GPL(power_supply_set_current_limit);
+
+/**
+ * power_supply_set_charging_by - set charging state of the charger
+ * @psy:	the power supply to control
+ * @enable:	enables or disables the charger
+ */
+int power_supply_set_charging_by(struct power_supply *psy, bool enable)
+{
+	const union power_supply_propval ret = {enable,};
+
+	if (psy->set_property)
+		return psy->set_property(psy, POWER_SUPPLY_PROP_ONLINE,
+								&ret);
+
+	return -ENXIO;
+}
+EXPORT_SYMBOL_GPL(power_supply_set_charging_by);
+
 static int __power_supply_changed_work(struct device *dev, void *data)
 {
 	struct power_supply *psy = (struct power_supply *)data;
