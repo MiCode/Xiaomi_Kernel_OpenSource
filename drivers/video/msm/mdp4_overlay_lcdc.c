81be67988ef149f601064a9e8e5d91705e636ff5
kuogee hsieh <khsieh@codeaurora.org>
2011-11-07 14:18:51 -0800

    msm_fb: display: update busy flag before check blt_addr
    dma_busy flag is set to TRUE on every vsycn push and set it
    to FALSE at overlay0_done ISR. When blt disable, dma_busy need
    to be set to FALSE before return due to blt_addr == NULL at
    overlay0_done ISR.
    
    Change-Id: If2622631a7cd03276ae7fbd8326155d0e9c7b66b
    Signed-off-by: kuogee hsieh <khsieh@codeaurora.org>

--- drivers/video/msm/mdp4_overlay_lcdc.c
+++ drivers/video/msm/mdp4_overlay_lcdc.c
@@ -451,11 +451,11 @@ void mdp4_dma_p_done_lcdc(void)
 void mdp4_overlay0_done_lcdc(struct mdp_dma_data *dma)
 {
 	spin_lock(&mdp_spin_lock);
+	dma->busy = FALSE;
 	if (lcdc_pipe->blt_addr == 0) {
 		spin_unlock(&mdp_spin_lock);
 		return;
 	}
-	dma->busy = FALSE;
 	mdp4_lcdc_blt_dmap_update(lcdc_pipe);
 	lcdc_pipe->dmap_cnt++;
 	mdp_disable_irq_nosync(MDP_OVERLAY0_TERM);
