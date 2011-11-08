0f3c9d7ae4b29e98c30e7ace3c32391e159c3e60
Matt Wagantall <mattw@codeaurora.org>
2011-11-08 14:31:27 -0800

    msm: iommu: Discontinue use of clk_set_min_rate()
    Support for the proprietary clk_set_min_rate() API is being removed from
    the clock driver. Replace it with a clk_round_rate() and clk_set_rate().
    
    Change-Id: Icd62a8768d99cf007cf899c705626f8eacbaa7c8
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

--- drivers/iommu/msm_iommu_dev.c
+++ drivers/iommu/msm_iommu_dev.c
@@ -29,7 +29,6 @@
 
 #include <mach/iommu_hw-8xxx.h>
 #include <mach/iommu.h>
-#include <mach/clk.h>
 
 struct iommu_ctx_iter_data {
 	/* input */
@@ -168,8 +167,10 @@ static int msm_iommu_probe(struct platform_device *pdev)
 	iommu_clk = clk_get(&pdev->dev, "iommu_clk");
 
 	if (!IS_ERR(iommu_clk))	{
-		if (clk_get_rate(iommu_clk) == 0)
-			clk_set_min_rate(iommu_clk, 1);
+		if (clk_get_rate(iommu_clk) == 0) {
+			ret = clk_round_rate(iommu_clk, 1);
+			clk_set_rate(iommu_clk, ret);
+		}
 
 		ret = clk_prepare_enable(iommu_clk);
 		if (ret) {
