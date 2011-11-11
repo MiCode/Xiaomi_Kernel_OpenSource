263e6c8ccebdab6eac529b0ac378ce8bda525f07
Chiranjeevi Velempati <cvelempa@codeaurora.org>
2011-11-11 23:07:36 +0530

    USB: frmnet: Fix control response re-ordering issue
    QMI control responses are being stacked rather than queued. Fix
    this issue by queueing the control packets.
    
    CRs-fixed: 314727
    Change-Id: I5f93194d10e446d2d0e24504376e96c786d17946
    Signed-off-by: Chiranjeevi Velempati <cvelempa@codeaurora.org>

--- drivers/usb/gadget/f_rmnet.c
+++ drivers/usb/gadget/f_rmnet.c
@@ -487,7 +487,7 @@ frmnet_send_cpkt_response(struct grmnet *gr, struct rmnet_ctrl_pkt *cpkt)
 	}
 
 	spin_lock_irqsave(&dev->lock, flags);
-	list_add(&cpkt->list, &dev->cpkt_resp_q);
+	list_add_tail(&cpkt->list, &dev->cpkt_resp_q);
 	spin_unlock_irqrestore(&dev->lock, flags);
 
 	frmnet_ctrl_response_available(dev);
