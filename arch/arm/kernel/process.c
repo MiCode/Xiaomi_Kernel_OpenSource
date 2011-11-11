eee780eafdf990702fdd8178b865af322b2a7621
Rohit Vaswani <rvaswani@codeaurora.org>
2011-11-10 21:06:31 -0800

    ARM: Disable the external caches during restart
    ARM restart code flushes the cpu caches, but leaves
    the external caches unflushed. Disable the external cache
    to get all the desired data and prevent loss of further
    information.
    
    Change-Id: Ie6a6765cdc1dfc8ee7d4c4ed473fdbf4d5ef9b88
    Signed-off-by: Rohit Vaswani <rvaswani@codeaurora.org>

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -142,6 +142,9 @@ void arm_machine_restart(char mode, const char *cmd)
 	/* Push out any further dirty data, and ensure cache is empty */
 	flush_cache_all();
 
+	/*Push out the dirty data from external caches */
+	outer_disable();
+
 	/*
 	 * Now call the architecture specific reboot code.
 	 */
