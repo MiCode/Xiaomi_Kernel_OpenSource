5a57201a8874b58c686865a377097a09051ee76d
Anji jonnala <anjir@codeaurora.org>
2011-12-06 10:03:24 +0530

    msm: board-8960: modify phy analog registers to pass signal quality
    Signal quality tests like eye diagram depend on PHY
    analog settings. These settings vary across different
    revisions of boards that use synopsis 28nm PHY.
    Use PHY initialization sequence platform data for this.
    
    Change-Id: I010cdc8b2f89e5dcaa9840f3b4d8f1dcdcded07d
    Signed-off-by: Anji jonnala <anjir@codeaurora.org>

--- drivers/usb/phy/phy-msm-usb.c
+++ drivers/usb/phy/phy-msm-usb.c
@@ -516,14 +516,15 @@ static int msm_otg_reset(struct usb_phy *phy)
 		return ret;
 	}
 
-	ulpi_init(motg);
-
 	ret = msm_otg_link_reset(motg);
 	if (ret) {
 		dev_err(phy->dev, "link reset failed\n");
 		return ret;
 	}
 	msleep(100);
+
+	ulpi_init(motg);
+
 	/* Ensure that RESET operation is completed before turning off clock */
 	mb();
 
