13490c0187d06e32acb4103fbc16d5df7c55232b
Chintan Pandya <cpandya@codeaurora.org>
2011-12-20 13:03:36 +0530

    msm: board-msm7627a-bt: Create separate board file for BT-FM
    Change-Id: I1e858a8074692ddc97a413595602a7d1d202e472
    Signed-off-by: Chintan Pandya <cpandya@codeaurora.org>

--- arch/arm/mach-msm/board-msm7627a.h
+++ arch/arm/mach-msm/board-msm7627a.h
@@ -15,4 +15,41 @@
 
 void __init msm7627a_init_mmc(void);
 
+#if defined(CONFIG_BT) && defined(CONFIG_MARIMBA_CORE)
+
+#define FPGA_MSM_CNTRL_REG2 0x90008010
+#define BAHAMA_SLAVE_ID_FM_REG 0x02
+#define BAHAMA_SLAVE_ID_FM_ADDR  0x2A
+#define BAHAMA_SLAVE_ID_QMEMBIST_ADDR   0x7B
+#define FM_GPIO 83
+#define BT_PCM_BCLK_MODE  0x88
+#define BT_PCM_DIN_MODE   0x89
+#define BT_PCM_DOUT_MODE  0x8A
+#define BT_PCM_SYNC_MODE  0x8B
+#define FM_I2S_SD_MODE    0x8E
+#define FM_I2S_WS_MODE    0x8F
+#define FM_I2S_SCK_MODE   0x90
+#define I2C_PIN_CTL       0x15
+#define I2C_NORMAL        0x40
+
+struct bahama_config_register {
+	u8 reg;
+	u8 value;
+	u8 mask;
+};
+
+struct bt_vreg_info {
+	const char *name;
+	unsigned int pmapp_id;
+	unsigned int min_level;
+	unsigned int max_level;
+	unsigned int is_pin_controlled;
+	struct regulator *reg;
+};
+
+extern struct platform_device msm_bt_power_device;
+
+void __init msm7627a_bt_power_init(void);
+#endif
+
 #endif
