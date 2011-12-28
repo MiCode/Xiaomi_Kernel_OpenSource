79e268c593f7f1c3647070e53b5bb5a16828027d
Ajay Dudani <adudani@codeaurora.org>
2011-12-28 13:14:44 -0800

    msm: audio: Bring msm8930 on par with msm8960 configuration
    * Update msm8930 board file to include additional audio devices.
    * Update msm8960 audio platform driver check to work on 8930.
    
    Change-Id: Ia6ab94457fd7e465c99a85692686d00a8ba08ffe
    Signed-off-by: Ajay Dudani <adudani@codeaurora.org>

--- sound/soc/msm/msm8960.c
+++ sound/soc/msm/msm8960.c
@@ -1165,7 +1165,7 @@ static int __init msm8960_audio_init(void)
 {
 	int ret;
 
-	if (!cpu_is_msm8960()) {
+	if (!cpu_is_msm8960() && !cpu_is_msm8930()) {
 		pr_err("%s: Not the right machine type\n", __func__);
 		return -ENODEV ;
 	}
@@ -1195,7 +1195,7 @@ module_init(msm8960_audio_init);
 
 static void __exit msm8960_audio_exit(void)
 {
-	if (!cpu_is_msm8960()) {
+	if (!cpu_is_msm8960() && !cpu_is_msm8930()) {
 		pr_err("%s: Not the right machine type\n", __func__);
 		return ;
 	}
