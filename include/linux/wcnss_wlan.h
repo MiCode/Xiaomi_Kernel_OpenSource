aebc0d8c2474d3e46e4d484c3d7cec92ecde3e85
Sameer Thalappil <sameert@codeaurora.org>
2012-01-01 11:45:38 -0800

    wcnss: Add Riva 'power on lock' APIs
    During SSR Riva should not be 'powered on' until all the host
    drivers finish their shutdown routines. Add APIs to support
    this synchronization mechanism.
    
    Change-Id: Id211cf2deb7accd0e4bdfe8cd328e6807eac875e
    Signed-off-by: Sameer Thalappil <sameert@codeaurora.org>

--- include/linux/wcnss_wlan.h
+++ include/linux/wcnss_wlan.h
@@ -40,7 +40,8 @@ struct wcnss_wlan_config *wcnss_get_wlan_config(void);
 int wcnss_wlan_power(struct device *dev,
 				struct wcnss_wlan_config *cfg,
 				enum wcnss_opcode opcode);
-
+int req_riva_power_on_lock(char *driver_name);
+int free_riva_power_on_lock(char *driver_name);
 #define wcnss_wlan_get_drvdata(dev) dev_get_drvdata(dev)
 #define wcnss_wlan_set_drvdata(dev, data) dev_set_drvdata((dev), (data))
 
