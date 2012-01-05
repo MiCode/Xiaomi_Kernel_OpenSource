4b931ed127737e1cd97c29f576d0606d7f0fb644
Angshuman Sarkar <angshuman@codeaurora.org>
2012-01-05 13:38:36 +0530

    msm: smd: fix legacy DS port registration
    Correctly register the DS port for legacy platforms.
    
    For 8660 targets, only the standalone device (platform
    subtype 0) uses the legacy DS port.
    
    Change-Id: Idb0b67dc8b737831ea3f51716350804b6b58ada5
    CRs-Fixed: 327119
    Signed-off-by: Angshuman Sarkar <angshuman@codeaurora.org>
    Signed-off-by: Eric Holmberg <eholmber@codeaurora.org>

--- arch/arm/mach-msm/smd_tty.c
+++ arch/arm/mach-msm/smd_tty.c
@@ -1,7 +1,7 @@
 /* arch/arm/mach-msm/smd_tty.c
  *
  * Copyright (C) 2007 Google, Inc.
- * Copyright (c) 2009-2011, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2009-2012, Code Aurora Forum. All rights reserved.
  * Author: Brian Swetland <swetland@google.com>
  *
  * This software is licensed under the terms of the GNU General Public
@@ -552,8 +552,11 @@ static int __init smd_tty_init(void)
 			legacy_ds |= cpu_is_msm7x01() || cpu_is_msm7x25();
 			legacy_ds |= cpu_is_msm7x27() || cpu_is_msm7x30();
 			legacy_ds |= cpu_is_qsd8x50() || cpu_is_msm8x55();
+			/*
+			 * use legacy mode for 8660 Standalone (subtype 0)
+			 */
 			legacy_ds |= cpu_is_msm8x60() &&
-					(socinfo_get_platform_subtype() == 0x1);
+					(socinfo_get_platform_subtype() == 0x0);
 
 			if (!legacy_ds)
 				continue;
