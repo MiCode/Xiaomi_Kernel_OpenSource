d8881e83e22be12dba63536a368552b7b53a5394
Ankush Khandelwal <akhand@codeaurora.org>
2012-01-11 18:53:54 +0530

    msm: rc: Add support for MSM GPIO TSOP IR Reciever driver.
    Add GPIO based TSOP IR Reciever driver. It decodes signals based on
    NEC protocol.
    
    Change-Id: Ic19fab61152a99828806cf451ae797797602ec70
    Signed-off-by: Ankush Khandelwal <akhand@codeaurora.org>

---
+++ include/media/tsop-ir.h
@@ -0,0 +1,23 @@
+/* Copyright (c) 2012, Code Aurora Forum. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __TSOP_IR_H__
+#define __TSOP_IR_H__
+
+struct tsop_platform_data {
+	unsigned int gpio_nr;
+	bool active_low;
+	bool can_wakeup;
+};
+
+#endif /* __TSOP_IR_H__ */
+
