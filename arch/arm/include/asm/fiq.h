eb81fb3bdea411fed5c26400f9769bad7fb0743f
Rohit Vaswani <rvaswani@codeaurora.org>
2012-01-13 15:53:12 -0800

    arm: fiq: Provide empty stubs for fiq functions
    A driver using fiq functions should be usable on targets
    with or without CONFIG_FIQ enabled. Instead of making code
    dependent on #ifdef CONFIG_FIQ, add empty stubs for the fiq
    functions.
    
    Change-Id: Ie8b1905ba53664d99bd707be3c83291c97eb1066
    Signed-off-by: Rohit Vaswani <rvaswani@codeaurora.org>

--- arch/arm/include/asm/fiq.h
+++ arch/arm/include/asm/fiq.h
@@ -33,11 +33,22 @@ struct fiq_handler {
 	void *dev_id;
 };
 
+#ifdef CONFIG_FIQ
 extern int claim_fiq(struct fiq_handler *f);
 extern void release_fiq(struct fiq_handler *f);
 extern void set_fiq_handler(void *start, unsigned int length);
 extern void enable_fiq(int fiq);
 extern void disable_fiq(int fiq);
+#else
+static inline int claim_fiq(struct fiq_handler *f)
+{
+	return 0;
+}
+static inline void release_fiq(struct fiq_handler *f) { }
+static inline void set_fiq_handler(void *start, unsigned int length) { }
+static inline void enable_fiq(int fiq) { }
+static inline void disable_fiq(int fiq) { }
+#endif
 
 /* helpers defined in fiqasm.S: */
 extern void __set_fiq_regs(unsigned long const *regs);
