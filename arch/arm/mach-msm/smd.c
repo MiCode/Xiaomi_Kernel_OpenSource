d464fcf5fdf9b8bcba4bd6992cebddcef113bbda
Eric Holmberg <eholmber@codeaurora.org>
2012-01-26 12:27:27 -0700

    msm: smd: Add support for GNSS Subsystem Restart
    Register for GNSS subsystem restart handler.
    
    Change-Id: I997e6c81aa5ea46061ecc05a9980aee2935ba032
    Signed-off-by: Eric Holmberg <eholmber@codeaurora.org>

--- arch/arm/mach-msm/smd.c
+++ arch/arm/mach-msm/smd.c
@@ -2622,6 +2622,7 @@ static struct restart_notifier_block restart_notifiers[] = {
 	{SMD_Q6, "lpass", .nb.notifier_call = restart_notifier_cb},
 	{SMD_WCNSS, "riva", .nb.notifier_call = restart_notifier_cb},
 	{SMD_DSPS, "dsps", .nb.notifier_call = restart_notifier_cb},
+	{SMD_MODEM, "gnss", .nb.notifier_call = restart_notifier_cb},
 };
 
 static int restart_notifier_cb(struct notifier_block *this,
