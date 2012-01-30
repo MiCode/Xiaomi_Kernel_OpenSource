f3e34e5d2b85478346bf3a826d2e0a74e67b97aa
Manu Gautam <mgautam@codeaurora.org>
2012-01-30 12:10:15 +0530

    USB: gadget: Enable streaming mode support in hardware
    Without streaming mode enabled, USB hardware fills Tx/Rx
    buffers completely before starting a new transfer on USB
    BUS. This helps to avoid underruns/overruns in bandwidth
    limited systems. But, downside of using stream disable is
    that it degrades USB throughput due to NYET handshake.
    
    Since, MSM chips support high bandwidth over fabric, hence,
    enable streaming mode to achieve better USB throughput.
    
    Change-Id: I9a9e80c2c3061906ef24e60d6fb6626669ea19ce
    Signed-off-by: Manu Gautam <mgautam@codeaurora.org>

--- drivers/usb/gadget/ci13xxx_msm.c
+++ drivers/usb/gadget/ci13xxx_msm.c
@@ -47,7 +47,6 @@ static struct ci13xxx_udc_driver ci13xxx_msm_udc_driver = {
 	.flags			= CI13XXX_REGS_SHARED |
 				  CI13XXX_REQUIRE_TRANSCEIVER |
 				  CI13XXX_PULLUP_ON_VBUS |
-				  CI13XXX_DISABLE_STREAMING |
 				  CI13XXX_ZERO_ITC,
 
 	.notify_event		= ci13xxx_msm_notify_event,
