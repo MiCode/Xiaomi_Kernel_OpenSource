89c734be1bf76c75bf74fcef58f0261a909ea4b6
Eric Holmberg <eholmber@codeaurora.org>
2012-01-30 12:45:54 -0700

    msm: bam_dmux: Make stubs static inline
    Make stub function implementations static inline to avoid
    compilation and linker failures when BAM DMUX is not
    enabled.
    
    Change-Id: I08aaf880863ffebee8bb05155c1d88575dd06b2c
    Signed-off-by: Eric Holmberg <eholmber@codeaurora.org>

--- arch/arm/mach-msm/include/mach/bam_dmux.h
+++ arch/arm/mach-msm/include/mach/bam_dmux.h
@@ -68,19 +68,19 @@ int msm_bam_dmux_is_ch_full(uint32_t id);
 
 int msm_bam_dmux_is_ch_low(uint32_t id);
 #else
-int msm_bam_dmux_open(uint32_t id, void *priv,
+static inline int msm_bam_dmux_open(uint32_t id, void *priv,
 		       void (*notify)(void *priv, int event_type,
 						unsigned long data))
 {
 	return -ENODEV;
 }
 
-int msm_bam_dmux_close(uint32_t id)
+static inline int msm_bam_dmux_close(uint32_t id)
 {
 	return -ENODEV;
 }
 
-int msm_bam_dmux_write(uint32_t id, struct sk_buff *skb)
+static inline int msm_bam_dmux_write(uint32_t id, struct sk_buff *skb)
 {
 	return -ENODEV;
 }
@@ -100,12 +100,12 @@ static inline int msm_bam_dmux_ul_power_unvote(void)
 	return -ENODEV;
 }
 
-int msm_bam_dmux_is_ch_full(uint32_t id)
+static inline int msm_bam_dmux_is_ch_full(uint32_t id)
 {
 	return -ENODEV;
 }
 
-int msm_bam_dmux_is_ch_low(uint32_t id)
+static inline int msm_bam_dmux_is_ch_low(uint32_t id)
 {
 	return -ENODEV;
 }
