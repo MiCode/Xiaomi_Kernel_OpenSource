3fb6bc2ae5081e864566ee0335281d07248f572d
Ankush Khandelwal <akhand@codeaurora.org>
2012-01-31 19:09:58 +0530

    msm: rc: Rename TSOP IR Receiver Driver
    Since the driver is a generic gpio based IR Receiver driver,
    name of the driver has been changed from tsop-ir to gpio-ir-recv.
    Variables of the driver change accordingly.
    
    Change-Id: I2e41c320b3f2f1a9796fc404791024c112a498c1
    Signed-off-by: Ankush Khandelwal <akhand@codeaurora.org>

--- drivers/media/rc/gpio-ir-recv.c
+++ drivers/media/rc/gpio-ir-recv.c
@@ -19,12 +19,12 @@
 #include <linux/platform_device.h>
 #include <linux/irq.h>
 #include <media/rc-core.h>
-#include <media/tsop-ir.h>
+#include <media/gpio-ir-recv.h>
 
-#define TSOP_DRIVER_NAME	"tsop-rc"
-#define TSOP_DEVICE_NAME	"tsop_ir"
+#define TSOP_DRIVER_NAME	"gpio-rc-recv"
+#define TSOP_DEVICE_NAME	"gpio_ir_recv"
 
-struct tsop_remote {
+struct gpio_rc_dev {
 	struct rc_dev *rcdev;
 	struct mutex lock;
 	unsigned int gpio_nr;
@@ -35,48 +35,49 @@ struct tsop_remote {
 
 static void ir_decoder_work(struct work_struct *work)
 {
-	struct tsop_remote *tsop_dev = container_of(work,
-			struct tsop_remote, work);
+	struct gpio_rc_dev *gpio_dev = container_of(work,
+			struct gpio_rc_dev, work);
 	unsigned int gval;
 	int rc = 0;
 	enum raw_event_type type = IR_SPACE;
 
-	mutex_lock(&tsop_dev->lock);
-	gval = gpio_get_value_cansleep(tsop_dev->gpio_nr);
+	mutex_lock(&gpio_dev->lock);
+	gval = gpio_get_value_cansleep(gpio_dev->gpio_nr);
 
 	if (gval < 0)
 		goto err_get_value;
 
-	if (tsop_dev->active_low)
+	if (gpio_dev->active_low)
 		gval = !gval;
 
 	if (gval == 1)
 		type = IR_PULSE;
 
-	rc = ir_raw_event_store_edge(tsop_dev->rcdev, type);
+	rc = ir_raw_event_store_edge(gpio_dev->rcdev, type);
 	if (rc < 0)
 		goto err_get_value;
 
-	ir_raw_event_handle(tsop_dev->rcdev);
+	ir_raw_event_handle(gpio_dev->rcdev);
 
 err_get_value:
-	mutex_unlock(&tsop_dev->lock);
+	mutex_unlock(&gpio_dev->lock);
 }
 
-static irqreturn_t tsop_irq_handler(int irq, void *data)
+static irqreturn_t gpio_ir_recv_irq_handler(int irq, void *data)
 {
-	struct tsop_remote *tsop_dev = data;
+	struct gpio_rc_dev *gpio_dev = data;
 
-	schedule_work(&tsop_dev->work);
+	schedule_work(&gpio_dev->work);
 
 	return IRQ_HANDLED;
 }
 
-static int __devinit tsop_driver_probe(struct platform_device *pdev)
+static int __devinit gpio_ir_recv_probe(struct platform_device *pdev)
 {
-	struct tsop_remote *tsop_dev;
+	struct gpio_rc_dev *gpio_dev;
 	struct rc_dev *rcdev;
-	const struct tsop_platform_data *pdata = pdev->dev.platform_data;
+	const struct gpio_ir_recv_platform_data *pdata =
+					pdev->dev.platform_data;
 	int rc = 0;
 
 	if (!pdata)
@@ -85,11 +86,11 @@ static int __devinit tsop_driver_probe(struct platform_device *pdev)
 	if (pdata->gpio_nr < 0)
 		return -EINVAL;
 
-	tsop_dev = kzalloc(sizeof(struct tsop_remote), GFP_KERNEL);
-	if (!tsop_dev)
+	gpio_dev = kzalloc(sizeof(struct gpio_rc_dev), GFP_KERNEL);
+	if (!gpio_dev)
 		return -ENOMEM;
 
-	mutex_init(&tsop_dev->lock);
+	mutex_init(&gpio_dev->lock);
 
 	rcdev = rc_allocate_device();
 	if (!rcdev) {
@@ -104,14 +105,14 @@ static int __devinit tsop_driver_probe(struct platform_device *pdev)
 	rcdev->driver_name = TSOP_DRIVER_NAME;
 	rcdev->map_name = RC_MAP_SAMSUNG_NECX;
 
-	tsop_dev->rcdev = rcdev;
-	tsop_dev->gpio_nr = pdata->gpio_nr;
-	tsop_dev->active_low = pdata->active_low;
-	tsop_dev->can_wakeup = pdata->can_wakeup;
+	gpio_dev->rcdev = rcdev;
+	gpio_dev->gpio_nr = pdata->gpio_nr;
+	gpio_dev->active_low = pdata->active_low;
+	gpio_dev->can_wakeup = pdata->can_wakeup;
 
-	INIT_WORK(&tsop_dev->work, ir_decoder_work);
+	INIT_WORK(&gpio_dev->work, ir_decoder_work);
 
-	rc = gpio_request(pdata->gpio_nr, "tsop-ir");
+	rc = gpio_request(pdata->gpio_nr, "gpio-ir-recv");
 	if (rc < 0)
 		goto err_gpio_request;
 	rc  = gpio_direction_input(pdata->gpio_nr);
@@ -124,11 +125,11 @@ static int __devinit tsop_driver_probe(struct platform_device *pdev)
 		goto err_register_rc_device;
 	}
 
-	platform_set_drvdata(pdev, tsop_dev);
+	platform_set_drvdata(pdev, gpio_dev);
 
-	rc = request_irq(gpio_to_irq(pdata->gpio_nr), tsop_irq_handler,
+	rc = request_irq(gpio_to_irq(pdata->gpio_nr), gpio_ir_recv_irq_handler,
 			IRQF_TRIGGER_FALLING | IRQF_TRIGGER_RISING,
-			"tsop-irq", tsop_dev);
+			"gpio-ir-recv-irq", gpio_dev);
 	if (rc < 0)
 		goto err_request_irq;
 
@@ -141,7 +142,7 @@ static int __devinit tsop_driver_probe(struct platform_device *pdev)
 	return 0;
 
 err_enable_irq_wake:
-	free_irq(gpio_to_irq(tsop_dev->gpio_nr), tsop_dev);
+	free_irq(gpio_to_irq(gpio_dev->gpio_nr), gpio_dev);
 err_request_irq:
 	platform_set_drvdata(pdev, NULL);
 	rc_unregister_device(rcdev);
@@ -152,47 +153,47 @@ err_gpio_request:
 	rc_free_device(rcdev);
 	rcdev = NULL;
 err_allocate_device:
-	mutex_destroy(&tsop_dev->lock);
-	kfree(tsop_dev);
+	mutex_destroy(&gpio_dev->lock);
+	kfree(gpio_dev);
 	return rc;
 }
 
-static int __devexit tsop_driver_remove(struct platform_device *pdev)
+static int __devexit gpio_ir_recv_remove(struct platform_device *pdev)
 {
-	struct tsop_remote *tsop_dev = platform_get_drvdata(pdev);
+	struct gpio_rc_dev *gpio_dev = platform_get_drvdata(pdev);
 
-	flush_work_sync(&tsop_dev->work);
-	disable_irq_wake(gpio_to_irq(tsop_dev->gpio_nr));
-	free_irq(gpio_to_irq(tsop_dev->gpio_nr), tsop_dev);
+	flush_work_sync(&gpio_dev->work);
+	disable_irq_wake(gpio_to_irq(gpio_dev->gpio_nr));
+	free_irq(gpio_to_irq(gpio_dev->gpio_nr), gpio_dev);
 	platform_set_drvdata(pdev, NULL);
-	rc_unregister_device(tsop_dev->rcdev);
-	gpio_free(tsop_dev->gpio_nr);
-	rc_free_device(tsop_dev->rcdev);
-	mutex_destroy(&tsop_dev->lock);
-	kfree(tsop_dev);
+	rc_unregister_device(gpio_dev->rcdev);
+	gpio_free(gpio_dev->gpio_nr);
+	rc_free_device(gpio_dev->rcdev);
+	mutex_destroy(&gpio_dev->lock);
+	kfree(gpio_dev);
 	return 0;
 }
 
-static struct platform_driver tsop_driver = {
-	.probe  = tsop_driver_probe,
-	.remove = __devexit_p(tsop_driver_remove),
+static struct platform_driver gpio_ir_recv_driver = {
+	.probe  = gpio_ir_recv_probe,
+	.remove = __devexit_p(gpio_ir_recv_remove),
 	.driver = {
 		.name   = TSOP_DRIVER_NAME,
 		.owner  = THIS_MODULE,
 	},
 };
 
-static int __init tsop_init(void)
+static int __init gpio_ir_recv_init(void)
 {
-	return platform_driver_register(&tsop_driver);
+	return platform_driver_register(&gpio_ir_recv_driver);
 }
-module_init(tsop_init);
+module_init(gpio_ir_recv_init);
 
-static void __exit tsop_exit(void)
+static void __exit gpio_ir_recv_exit(void)
 {
-	platform_driver_unregister(&tsop_driver);
+	platform_driver_unregister(&gpio_ir_recv_driver);
 }
-module_exit(tsop_exit);
+module_exit(gpio_ir_recv_exit);
 
-MODULE_DESCRIPTION("TSOP IR driver");
+MODULE_DESCRIPTION("GPIO IR Receiver driver");
 MODULE_LICENSE("GPL v2");
