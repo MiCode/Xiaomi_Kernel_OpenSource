5fbf4763fbfcf81cb00150d9eb8d21c17154552c
Manu Gautam <mgautam@codeaurora.org>
2012-02-01 12:06:09 +0530

    USB: OTG: Do not start charging before device enumerates
    USB battery charging specification defines that device should
    not start charging from SDP (or host port) before connecting
    i.e. pulling high data line.
    Hence, instead of charging immediately after performing detection,
    wait till device configuration is selected by host.
    
    Change-Id: I77dd29d1b4fbb57bc2f4362ef77d6a9c73ab6653
    CRs-Fixed: 333556
    Signed-off-by: Manu Gautam <mgautam@codeaurora.org>

--- drivers/usb/phy/phy-msm-usb.c
+++ drivers/usb/phy/phy-msm-usb.c
@@ -1878,7 +1878,6 @@ static void msm_otg_sm_work(struct work_struct *w)
 					otg->state = OTG_STATE_B_PERIPHERAL;
 					break;
 				case USB_SDP_CHARGER:
-					msm_otg_notify_charger(motg, IUNIT);
 					msm_otg_start_peripheral(otg->phy, 1);
 					otg->phy->state
 						= OTG_STATE_B_PERIPHERAL;
