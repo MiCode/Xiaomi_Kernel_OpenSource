46af59fb8f4bce302ad4d787b63b25e46548df6a
Stepan Moskovchenko <stepanm@codeaurora.org>
2012-02-07 14:38:59 -0800

    msm: rng: Disable RNG init on APQ8064
    Change-Id: Ic42a85c51faea8a17b02eb4987d0f5db732716c2
    Signed-off-by: Stepan Moskovchenko <stepanm@codeaurora.org>

--- drivers/char/hw_random/msm_rng.c
+++ drivers/char/hw_random/msm_rng.c
@@ -242,6 +242,9 @@ static struct platform_driver rng_driver = {
 
 static int __init msm_rng_init(void)
 {
+	if (cpu_is_apq8064())
+		return -ENODEV;
+
 	return platform_driver_register(&rng_driver);
 }
 
