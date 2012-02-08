fddd00f89db45d612c1cca7b0c4ae948480041b2
Kenneth Heitke <kheitke@codeaurora.org>
2012-02-08 13:45:33 -0700

    spmi: Linux driver framework for SPMI
    SPMI (System Power Management Interface) is a specification
    developed by the MIPI (Mobile Industry Process Interface) Alliance
    optimized for the real time control of Power Management ICs (PMIC).
    
    SPMI is a two-wire serial interface that supports up to 4 master
    devices and up to 16 logical slaves.  A physical PMIC device may
    consist of multiple logical slave interfaces.
    
    The framework supports message APIs, multiple busses (1 controller
    per bus) and multiple clients/slave devices per controller.
    
    Change-Id: Ib6319047b4ab9cea6dfb0879312c4c1f63462439
    Signed-off-by: Kenneth Heitke <kheitke@codeaurora.org>

--- include/linux/mod_devicetable.h
+++ include/linux/mod_devicetable.h
@@ -431,6 +431,24 @@ struct spi_device_id {
 	kernel_ulong_t driver_data;	/* Data private to the driver */
 };
 
+#define SLIMBUS_NAME_SIZE	32
+#define SLIMBUS_MODULE_PREFIX "slim:"
+
+struct slim_device_id {
+	char name[SLIMBUS_NAME_SIZE];
+	kernel_ulong_t driver_data	/* Data private to the driver */
+			__attribute__((aligned(sizeof(kernel_ulong_t))));
+};
+
+#define SPMI_NAME_SIZE	32
+#define SPMI_MODULE_PREFIX "spmi:"
+
+struct spmi_device_id {
+	char name[SPMI_NAME_SIZE];
+	kernel_ulong_t driver_data	/* Data private to the driver */
+			__attribute__((aligned(sizeof(kernel_ulong_t))));
+};
+
 /* dmi */
 enum dmi_field {
 	DMI_NONE,
