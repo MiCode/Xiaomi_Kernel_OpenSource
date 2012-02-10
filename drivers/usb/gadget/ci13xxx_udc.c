8d1bbd58c8b74a022f69016a7d09a5d3e6065f9b
Vamsi Krishna <vskrishn@codeaurora.org>
2012-02-09 21:13:24 -0800

    usb: ci13xxx_udc: Add debug mask to control debug events
    udc driver generates lot of debug events which quickly fills up
    its circular event queue and override important events which are
    required for debugging. Add debug mask support so that only the
    events that match mask are captured discarding the rest. Debug
    mask can be changed by module parameter and bit position
    represents endpoint number.
    
    Example: To capture 2 in endpoint events
    echo -n 4 > /sys/module/ci13xxx_msm/parameters/ep_addr_txdbg_mask
    To capture 3 out endpoint events
    echo -n 8 > /sys/module/ci13xxx_msm/parameters/ep_addr_rxdbg_mask
    
    Change-Id: Ia0383df67c85245a8f8415eee3d9b67f38ac2d1e
    Signed-off-by: Vamsi Krishna <vskrishn@codeaurora.org>

--- drivers/usb/gadget/ci13xxx_udc.c
+++ drivers/usb/gadget/ci13xxx_udc.c
@@ -858,6 +858,32 @@ static void dbg_inc(unsigned *idx)
 	*idx = (*idx + 1) & (DBG_DATA_MAX-1);
 }
 
+
+static unsigned int ep_addr_txdbg_mask;
+module_param(ep_addr_txdbg_mask, uint, S_IRUGO | S_IWUSR);
+static unsigned int ep_addr_rxdbg_mask;
+module_param(ep_addr_rxdbg_mask, uint, S_IRUGO | S_IWUSR);
+
+static int allow_dbg_print(u8 addr)
+{
+	int dir, num;
+
+	/* allow bus wide events */
+	if (addr == 0xff)
+		return 1;
+
+	dir = addr & USB_ENDPOINT_DIR_MASK ? TX : RX;
+	num = addr & ~USB_ENDPOINT_DIR_MASK;
+	num = 1 << num;
+
+	if ((dir == TX) && (num & ep_addr_txdbg_mask))
+		return 1;
+	if ((dir == RX) && (num & ep_addr_rxdbg_mask))
+		return 1;
+
+	return 0;
+}
+
 /**
  * dbg_print:  prints the common part of the event
  * @addr:   endpoint address
@@ -870,6 +896,9 @@ static void dbg_print(u8 addr, const char *name, int status, const char *extra)
 	struct timeval tval;
 	unsigned int stamp;
 	unsigned long flags;
+
+	if (!allow_dbg_print(addr))
+		return;
 
 	write_lock_irqsave(&dbg_data.lck, flags);
 
@@ -1407,7 +1436,7 @@ static ssize_t print_dtds(struct device *dev,
 
 	n = hw_ep_bit(mEp->num, mEp->dir);
 	pr_info("%s: prime:%08x stat:%08x ep#%d dir:%s"
-			"dTD_update_fail_count: %lu"
+			"dTD_update_fail_count: %lu "
 			"mEp->dTD_update_fail_count: %lu\n", __func__,
 			hw_cread(CAP_ENDPTPRIME, ~0),
 			hw_cread(CAP_ENDPTSTAT, ~0),
