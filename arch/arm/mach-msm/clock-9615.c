69d35e3b70f98a77fe7837bd5aaa5aaf2d98362c
Stephen Boyd <sboyd@codeaurora.org>
2012-02-14 15:33:30 -0800

    msm: bam_dmux: Vote for CXO when bam is in use
    CXO is not always guaranteed to be on when the system is running.
    For example, when usb is unplugged and the modem is power
    collapsed there are no votes for CXO in the system and thus CXO
    can be shut off.
    
    Unfortunately, bam_dmux is relying on a clock that is sourced
    from CXO to turn on automatically when the a2bam is accessed.
    Since the clock is hardware controlled CXO must be on or the
    access to the a2bam will stall waiting for the clock to turn on.
    
    Therefore, whenever the a2 bam is accessed turn on the CXO clock
    as well so that the hardware clock can turn on and the access can
    work.
    
    Change-Id: I9be04a8091f071092cb921e89e3fc8d83aa33005
    CRs-Fixed: 336329
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- arch/arm/mach-msm/clock-9615.c
+++ arch/arm/mach-msm/clock-9615.c
@@ -1622,6 +1622,7 @@ static struct measure_clk measure_clk = {
 
 static struct clk_lookup msm_clocks_9615[] = {
 	CLK_LOOKUP("xo",	cxo_clk.c,	"msm_otg"),
+	CLK_LOOKUP("xo",	cxo_clk.c,	"BAM_RMNT"),
 	CLK_LOOKUP("pll0",	pll0_clk.c,	NULL),
 	CLK_LOOKUP("pll8",	pll8_clk.c,	NULL),
 	CLK_LOOKUP("pll14",	pll14_clk.c,	NULL),
