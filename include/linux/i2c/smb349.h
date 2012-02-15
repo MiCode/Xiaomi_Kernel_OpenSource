dd77ff377dba5aa5f6032b58c9feb048718c6fda
David Keitel <dkeitel@codeaurora.org>
2012-02-15 11:32:58 -0800

    power: smb349: add support for smb349 charger chip
    The SMB349 charger chip allows charge batteries
    with a higher current. Add this driver to be able to
    control charging from kernel space and deliver charging
    information to userspace.
    
    Also add debugfs entries to show current register
    settings of SMB349 charger.
    
    Change-Id: Ic88b539304539a49ebe69517d13045cbb18091bf
    Signed-off-by: David Keitel <dkeitel@codeaurora.org>

---
+++ include/linux/i2c/smb349.h
@@ -0,0 +1,31 @@
+/* Copyright (c) 2012 The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful;
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+#ifndef __SMB349_H__
+#define __SMB349_H__
+
+#define SMB349_NAME		"smb349"
+
+/**
+ * struct smb349_platform_data
+ * structure to pass board specific information to the smb137b charger driver
+ * @chg_current_ma:		maximum fast charge current in mA
+ * @en_n_gpio:		gpio to enable or disable charging
+ * @chg_susp_gpio:	put active low to allow chip to suspend and disable I2C
+ */
+struct smb349_platform_data {
+	int en_n_gpio;
+	int chg_susp_gpio;
+	int chg_current_ma;
+};
+
+#endif
