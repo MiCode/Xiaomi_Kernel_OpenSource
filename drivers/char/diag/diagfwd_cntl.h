f50076b757c85b2249aa57ba5c09d44be70da6a9
Shalabh Jain <shalabhj@codeaurora.org>
2012-02-15 19:27:01 -0800

    diag: Add sanity check for data
    DIAG driver communicates to peripherals over SMD channels. It parses
    data read and then takes appropriate actions. Sometimes, SMD can pass
    corrupted data and cause DIAG driver to crash. Adding checks around
    data will prevent such crashes.
    
    Change-Id: Ic82b1e31557eb432e57f3f6b152b22c1d3f5cd37
    Signed-off-by: Shalabh Jain <shalabhj@codeaurora.org>

--- drivers/char/diag/diagfwd_cntl.h
+++ drivers/char/diag/diagfwd_cntl.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2011-2012, Code Aurora Forum. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -13,7 +13,26 @@
 #ifndef DIAGFWD_CNTL_H
 #define DIAGFWD_CNTL_H
 
-#define DIAG_CTRL_MSG_REG 1	/* Message registration commands */
+/* Message registration commands */
+#define DIAG_CTRL_MSG_REG		1
+/* Message passing for DTR events */
+#define DIAG_CTRL_MSG_DTR		2
+/* Control Diag sleep vote, buffering etc */
+#define DIAG_CTRL_MSG_DIAGMODE		3
+/* Diag data based on "light" diag mask */
+#define DIAG_CTRL_MSG_DIAGDATA		4
+/* Deprecated */
+#define DIAG_CTRL_MSG_LOG_MASK		5
+#define DIAG_CTRL_MSG_EVENT_MASK	6
+#define DIAG_CTRL_MSG_F3_MASK		7
+/* Send diag internal feature mask 'diag_int_feature_mask' */
+#define DIAG_CTRL_MSG_FEATURE		8
+/* Send Diag log mask for a particular equip id */
+#define DIAG_CTRL_MSG_EQUIP_LOG_MASK	9
+/* Send Diag event mask */
+#define DIAG_CTRL_MSG_EVENT_MASK_V2	10
+/* Send Diag F3 mask */
+#define DIAG_CTRL_MSG_F3_MASK_V2	11
 
 struct cmd_code_range {
 	uint16_t cmd_code_lo;
