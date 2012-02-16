4ca275378e40c0ea857259d1b28a63056e019029
Aravind Venkateswaran <aravindh@codeaurora.org>
2012-02-16 14:27:05 -0800

    board: 8064: Add Rotator Device Driver
    Add the rotator device driver to the 8064 board file.
    
    Signed-off-by: Aravind Venkateswaran <aravindh@codeaurora.org>
    Change-Id: I60bcb1776cc7d582b25be586e1a8504bd57001de

--- arch/arm/mach-msm/board-8064.c
+++ arch/arm/mach-msm/board-8064.c
@@ -1709,6 +1709,9 @@ static struct platform_device *cdp_devices[] __initdata = {
 	&apq8064_device_uart_gsbi1,
 	&apq8064_device_uart_gsbi7,
 	&msm_device_sps_apq8064,
+#ifdef CONFIG_MSM_ROTATOR
+	&msm_rotator_device,
+#endif
 };
 
 static struct msm_spi_platform_data apq8064_qup_spi_gsbi5_pdata = {
