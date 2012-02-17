3c2cc9e9787d2cf7349dba0a247132eb564cba8f
Tarun Karra <tkarra@codeaurora.org>
2012-02-17 14:58:07 -0800

    Revert "msm: board-8064: Fix 8064 GPU clk at 192 Mhz"
    This reverts commit 0e43cd39ecc42c4a956229682be128ad3a1b89b5.
    Verified GPU clk scaling works on 8064. Enable GPU clk scaling
    with this revert.
    
    Change-Id: Ib07918195c2c3fd4a19c00ff0db62ace55150a5c
    Signed-off-by: Tarun Karra <tkarra@codeaurora.org>

--- arch/arm/mach-msm/board-8064-gpu.c
+++ arch/arm/mach-msm/board-8064-gpu.c
@@ -150,14 +150,14 @@ static struct kgsl_device_iommu_data kgsl_3d0_iommu_data[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 192000000,
-			.bus_freq = 2,
-			.io_fraction = 100,
+			.gpu_freq = 400000000,
+			.bus_freq = 4,
+			.io_fraction = 0,
 		},
 		{
-			.gpu_freq = 192000000,
-			.bus_freq = 2,
-			.io_fraction = 100,
+			.gpu_freq = 320000000,
+			.bus_freq = 3,
+			.io_fraction = 33,
 		},
 		{
 			.gpu_freq = 1920000000,
@@ -165,9 +165,8 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.io_fraction = 100,
 		},
 		{
-			.gpu_freq = 192000000,
-			.bus_freq = 2,
-			.io_fraction = 100,
+			.gpu_freq = 27000000,
+			.bus_freq = 0,
 		},
 	},
 	.init_level = 0,
