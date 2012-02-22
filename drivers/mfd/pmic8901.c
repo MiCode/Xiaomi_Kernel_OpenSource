9a8f4eb2803805f9ce4eab4f90c74453f1d13b0c
Stephen Boyd <sboyd@codeaurora.org>
2012-02-21 23:51:00 -0800

    mfd: pmic8901: Fix section mismatches
    WARNING: drivers/mfd/built-in.o(.text+0x3750): Section mismatch
    in reference from the function pm8901_probe() to the function
    .devinit.text:pm8xxx_irq_init()
    The function pm8901_probe() references
    the function __devinit pm8xxx_irq_init().
    This is often because pm8901_probe lacks a __devinit
    annotation or the annotation of pm8xxx_irq_init is wrong.
    
    Change-Id: I4d6ea52fdacad73bed80e3f5be3e9e08cc49be65
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/mfd/pmic8901.c
+++ drivers/mfd/pmic8901.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2011, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2010-2012, Code Aurora Forum. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -270,7 +270,7 @@ bail:
 	return rc;
 }
 
-static int pm8901_probe(struct platform_device *pdev)
+static int __devinit pm8901_probe(struct platform_device *pdev)
 {
 	int rc;
 	struct pm8901_platform_data *pdata = pdev->dev.platform_data;
