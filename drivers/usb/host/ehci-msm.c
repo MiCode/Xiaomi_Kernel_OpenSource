a3152031da5d2dbce83bd0ab8b56b31108ad6255
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2012-03-05 16:29:30 +0530

    USB: OTG: msm: Add support for SRP and HNP
    This patch changes OTG driver to support different
    OTG statemachine states for OTG2.0 complaince. It will
    also enable to swap Host role between two OTG devices
    via HNP and enable B device to request A device to start
    VBUS session via SRP.
    
    Change-Id: Ibe2519ff1570768f51e879af483a6f5c23b38d8f
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- drivers/usb/host/ehci-msm.c
+++ drivers/usb/host/ehci-msm.c
@@ -176,6 +176,7 @@ static int ehci_msm_probe(struct platform_device *pdev)
 		goto put_transceiver;
 	}
 
+	hcd_to_ehci(hcd)->transceiver = otg;
 	device_init_wakeup(&pdev->dev, 1);
 	pm_runtime_enable(&pdev->dev);
 
@@ -199,6 +200,7 @@ static int __devexit ehci_msm_remove(struct platform_device *pdev)
 	pm_runtime_disable(&pdev->dev);
 	pm_runtime_set_suspended(&pdev->dev);
 
+	hcd_to_ehci(hcd)->transceiver = NULL;
 	otg_set_host(otg, NULL);
 	otg_put_transceiver(otg);
 
