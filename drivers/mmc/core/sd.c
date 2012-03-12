1a5c0d16016e88d5b12852a616cfe9b53b3cf1b3
Subhash Jadavani <subhashj@codeaurora.org>
2012-03-13 00:33:00 +0530

    mmc: sd: fix the issue with paranoid SD card init retry
    mmc_resume_host() is called during resume (runtime or system) which calls
    	1. mmc_power_up
    	2. mmc_select_voltage
    	3. host->bus_ops->resume() (mmc_sd_resume())
    
    mmc_sd_resume() ultimately calls mmc_sd_init_card() to initialize the
    SD card. But let's say if mmc_sd_init_card() fails during the
    execute_tuning(). At this point, host controller timing is changed
    to SDR104/SDR50 mode and host clock will also be > 100 MHz.
    
    If there is an error returned by mmc_sd_init_card() and
    if CONFIG_MMC_PARANOID_SD_INIT defined, it retries by calling
    mmc_sd_init_card() without making sure that host controller timing
    and clock be initialized again. This may cause the further
    mmc_sd_init_card() also to fail.
    
    To fix this, mmc_sd_resume() should call mmc_power_up()
    and mmc_select_voltage() before retrying.
    
    Change-Id: I8de39ea547fa0d5eca478719a4cf9255b6652503
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- drivers/mmc/core/sd.c
+++ drivers/mmc/core/sd.c
@@ -1214,8 +1214,11 @@ static int mmc_sd_resume(struct mmc_host *host)
 		if (err) {
 			printk(KERN_ERR "%s: Re-init card rc = %d (retries = %d)\n",
 			       mmc_hostname(host), err, retries);
-			mdelay(5);
 			retries--;
+			mmc_power_off(host);
+			usleep_range(5000, 5500);
+			mmc_power_up(host);
+			mmc_select_voltage(host, host->ocr);
 			continue;
 		}
 		break;
@@ -1342,6 +1345,10 @@ int mmc_attach_sd(struct mmc_host *host)
 		err = mmc_sd_init_card(host, host->ocr, NULL);
 		if (err) {
 			retries--;
+			mmc_power_off(host);
+			usleep_range(5000, 5500);
+			mmc_power_up(host);
+			mmc_select_voltage(host, host->ocr);
 			continue;
 		}
 		break;
