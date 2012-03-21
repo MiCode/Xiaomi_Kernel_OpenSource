d56429b1a1623418f6659b1e05c323d175437af8
Raju P.L.S.S.S.N <rplsssn@codeaurora.org>
2012-03-21 20:22:39 +0530

    msm: camera: Fix for enabling vfe_clk
    call prepare before enabling vfe_clk.
    
    Change-Id: I1ea305ed87cc01fcb0c0d96f526793042a453020
    Signed-off-by: Raju P.L.S.S.S.N <rplsssn@codeaurora.org>

--- drivers/media/video/msm/msm_axi_qos.c
+++ drivers/media/video/msm/msm_axi_qos.c
@@ -22,8 +22,10 @@ int add_axi_qos(void)
 	ebi1_clk = clk_get(NULL, "ebi1_vfe_clk");
 	if (IS_ERR(ebi1_clk))
 		ebi1_clk = NULL;
-	else
+	else {
+		clk_prepare(ebi1_clk);
 		clk_enable(ebi1_clk);
+	}
 
 	return 0;
 }
@@ -42,6 +44,7 @@ void release_axi_qos(void)
 		return;
 
 	clk_disable(ebi1_clk);
+	clk_unprepare(ebi1_clk);
 	clk_put(ebi1_clk);
 	ebi1_clk = NULL;
 }
