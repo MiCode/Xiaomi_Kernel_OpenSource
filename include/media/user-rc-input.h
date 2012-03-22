3984d57b2520e75826e5fc7c162cd6a5c0d05389
Ravi Kumar V <kumarrav@codeaurora.org>
2012-03-22 19:04:13 +0530

    media: rc: Add remote control event report from user space input
    Some of user space applications need to send the remote key events
    to kernel input subsystem, so for these type of applications this driver
    enables them to send key events to kernel.
    User has to send the key event type (pressed,repeated,released) and scan
    code value.
    
    Change-Id: I31a03044702020bd7bfd6cfdc310e65f85d282ef
    Signed-off-by: Ravi Kumar V <kumarrav@codeaurora.org>

---
+++ include/media/user-rc-input.h
@@ -0,0 +1,21 @@
+/* Copyright (c) 2012, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __USER_RC_INPUT_H__
+#define __USER_RC_INPUT_H__
+
+#define USER_CONTROL_PRESSED    0x01
+#define USER_CONTROL_REPEATED   0x02
+#define USER_CONTROL_RELEASED   0x03
+
+#endif /* __USER_RC_INPUT_H__ */
+
