a91fab565dad89d6c2f825624d283ae525bd1067
Ofir Cohen <ofirc@codeaurora.org>
2012-03-25 14:57:28 +0200

    usb: gadget: change HSIC voting to xo.
    Changed HSIC peripheral for xo voting as HSIC uses the D0 input.
    
    
    Change-Id: If21da9d6b28b5216e0d47f2c3ccfe1910650ee68
    Signed-off-by: Ofir Cohen <ofirc@codeaurora.org>

--- drivers/usb/gadget/ci13xxx_msm_hsic.c
+++ drivers/usb/gadget/ci13xxx_msm_hsic.c
@@ -643,7 +643,7 @@ static int __devinit msm_hsic_probe(struct platform_device *pdev)
 	}
 	dev_info(&pdev->dev, "HSIC Peripheral regs = %p\n", mhsic->regs);
 
-	mhsic->xo_handle = msm_xo_get(MSM_XO_CXO, "hsic_peripheral");
+	mhsic->xo_handle = msm_xo_get(MSM_XO_TCXO_D0, "hsic_peripheral");
 	if (IS_ERR(mhsic->xo_handle)) {
 		dev_err(&pdev->dev, "%s not able to get the handle "
 			"to vote for TCXO\n", __func__);
