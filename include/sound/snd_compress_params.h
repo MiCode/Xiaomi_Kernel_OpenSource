e27ed78b176812062febc2f7457e5f30ba71c166
Santosh Mardi <gsantosh@codeaurora.org>
2012-03-29 04:20:38 +0530

    ASoC: msm: Add support for AC3 pass through playback
    Add support for AC3 tunnel mode playback where dsp acting
    As pass through.
    
    Change-Id: Ibc6536a439f7cae8cbbadcaddd4ad27e4ee84543
    Signed-off-by: Santosh Mardi <gsantosh@codeaurora.org>

--- include/sound/snd_compress_params.h
+++ include/sound/snd_compress_params.h
@@ -71,6 +71,7 @@
 #define SND_AUDIOCODEC_G729                  ((__u32) 0x0000000D)
 #define SND_AUDIOCODEC_AC3		     ((__u32) 0x0000000E)
 #define SND_AUDIOCODEC_DTS		     ((__u32) 0x0000000F)
+#define SND_AUDIOCODEC_AC3_PASS_THROUGH		((__u32) 0x00000010)
 /*
  * Profile and modes are listed with bit masks. This allows for a
  * more compact representation of fields that will not evolve
