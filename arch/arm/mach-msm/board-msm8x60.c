aee8f560a13ae22ccf0c0b26cee0046713daafef
Ankit Premrajka <ankitp@codeaurora.org>
2012-04-09 03:57:53 -0700

    mach: increase camera heap size to 66 MB.
    In order to support full size ZSL feature on 8960, we need a
    total of 120 MB. This change increases the camera EBI heap size
    to 66 MB, which combined with 54MB from SMI gives us
    120MB of carved out memory.
    
    Change-Id: Ifba317d454a8d9149662b07a45995ab90a0c9a78
    Signed-off-by: Ankit Premrajka <ankitp@codeaurora.org>

--- arch/arm/mach-msm/board-msm8x60.c
+++ arch/arm/mach-msm/board-msm8x60.c
@@ -2630,7 +2630,7 @@ unsigned char hdmi_is_primary;
 #endif  /* CONFIG_FB_MSM_OVERLAY1_WRITEBACK */
 
 #define MSM_PMEM_KERNEL_EBI1_SIZE  0x3BC000
-#define MSM_PMEM_ADSP_SIZE         0x2000000
+#define MSM_PMEM_ADSP_SIZE         0x4200000
 #define MSM_PMEM_AUDIO_SIZE        0x4CF000
 
 #define MSM_SMI_BASE          0x38000000
