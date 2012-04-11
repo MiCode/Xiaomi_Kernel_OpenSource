24696325583d5a6f5a2d8f87178df38f1330feba
Ranjhith Kalisamy <ranjhith@codeaurora.org>
2012-04-11 22:09:17 +0530

    msm: board-msm7x27a: Increase GPU frequency in 8x25
    8x25 supports a higher GPU frequency than it is configured now.
    
    Change-Id: If1c0992f6ac497edc2139f482ef3da9f47902cde
    Signed-off-by: Ranjhith Kalisamy <ranjhith@codeaurora.org>

--- arch/arm/mach-msm/devices-msm7x27a.c
+++ arch/arm/mach-msm/devices-msm7x27a.c
@@ -840,6 +840,9 @@ void __init msm8x25_kgsl_3d0_init(void)
 	if (cpu_is_msm8625()) {
 		kgsl_3d0_pdata.idle_timeout = HZ/5;
 		kgsl_3d0_pdata.strtstp_sleepwake = false;
+		/* 8x25 supports a higher GPU frequency */
+		kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 300000000;
+		kgsl_3d0_pdata.pwrlevel[0].bus_freq = 200000000;
 	}
 }
 
