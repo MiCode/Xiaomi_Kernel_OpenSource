3bd641ee4fc68c7f7742bcd51fcf845914f2c790
Suman Tatiraju <sumant@codeaurora.org>
2012-04-11 10:46:11 -0700

    msm: kgsl: enable DCVS on 8064
    Change the idle timeout to 100 msec so that we dont
    sleep as often.This is to avoid performance hit with
    50msec
    
    Change-Id: I3fb1b2508ad15a030480e9af40ebca4dbaf4bff0
    Signed-off-by: Suman Tatiraju <sumant@codeaurora.org>

--- drivers/gpu/msm/kgsl_pwrscale_trustzone.c
+++ drivers/gpu/msm/kgsl_pwrscale_trustzone.c
@@ -180,7 +180,7 @@ static int tz_init(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 	struct tz_priv *priv;
 
 	/* Trustzone is only valid for some SOCs */
-	if (!(cpu_is_msm8x60() || cpu_is_msm8960()))
+	if (!(cpu_is_msm8x60() || cpu_is_msm8960() || cpu_is_apq8064()))
 		return -EINVAL;
 
 	priv = pwrscale->priv = kzalloc(sizeof(struct tz_priv), GFP_KERNEL);
