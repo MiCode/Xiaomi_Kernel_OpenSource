4098529688bfd441a5047a4017c222944a83b645
Rajkumar Raghupathy <raghup@codeaurora.org>
2012-04-12 15:19:53 +0530

    USB: u_serial: Don't allow UDC to append and send a zero length packet
    Two zero length packets are sent by the target in the current scenario:
    1. From the device controller driver (as the req->zero flag is set if
       it is the last packet).
    2. From the u_serial driver when the length of the packet is zero while
       checking for the next packet in the list.
    
    Hence removing the code for the case1, as the zero length packet is
    sent via u_serial function driver.
    
    Change-Id: Ifc1bc9cf8435149534c271343ea7ef64b9ed4d4a
    CRs-Fixed: 350179
    Signed-off-by: Rajkumar Raghupathy <raghup@codeaurora.org>

--- drivers/usb/gadget/u_serial.c
+++ drivers/usb/gadget/u_serial.c
@@ -386,7 +386,9 @@ __acquires(&port->port_lock)
 		req = list_entry(pool->next, struct usb_request, list);
 		len = gs_send_packet(port, req->buf, TX_BUF_SIZE);
 		if (len == 0) {
-			/* Queue zero length packet */
+			/* Queue zero length packet explicitly to make it
+			 * work with UDCs which don't support req->zero flag
+			 */
 			if (prev_len && (prev_len % in->maxpacket == 0)) {
 				req->length = 0;
 				list_del(&req->list);
@@ -411,7 +413,6 @@ __acquires(&port->port_lock)
 
 		req->length = len;
 		list_del(&req->list);
-		req->zero = (gs_buf_data_avail(&port->port_write_buf) == 0);
 
 		pr_vdebug(PREFIX "%d: tx len=%d, 0x%02x 0x%02x 0x%02x ...\n",
 				port->port_num, len, *((u8 *)req->buf),
