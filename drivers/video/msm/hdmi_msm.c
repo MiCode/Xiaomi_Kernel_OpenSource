514b1c651a33b7b73fb6f654d04355eac5ecb146
Ajay Singh Parmar <aparmar@codeaurora.org>
2012-04-19 11:17:26 +0530

    msm_fb: hdmi: 3D configurations in HDMI
    3D configurations using Generic0 packets need to be done
    after enabling HDMI Control to take these configurations
    into effect.
    
    CRs-fixed: 349578
    Change-Id: I8d658ddf4f2ccb2ae47015fe411e8c1a5a55e4df
    Signed-off-by: Ajay Singh Parmar <aparmar@codeaurora.org>

--- drivers/video/msm/hdmi_msm.c
+++ drivers/video/msm/hdmi_msm.c
@@ -4136,6 +4136,8 @@ static void hdmi_msm_turn_on(void)
 	/* HDMI_USEC_REFTIMER[0x0208] */
 	HDMI_OUTP(0x0208, 0x0001001B);
 
+	hdmi_msm_set_mode(TRUE);
+
 	hdmi_msm_video_setup(external_common_state->video_resolution);
 	if (!hdmi_msm_is_dvi_mode())
 		hdmi_msm_audio_setup();
@@ -4151,8 +4153,6 @@ static void hdmi_msm_turn_on(void)
 	/* Toggle HPD circuit to trigger HPD sense */
 	HDMI_OUTP(0x0258, ~(1 << 28) & hpd_ctrl);
 	HDMI_OUTP(0x0258, (1 << 28) | hpd_ctrl);
-
-	hdmi_msm_set_mode(TRUE);
 
 	/* Setup HPD IRQ */
 	HDMI_OUTP(0x0254, 4 | (external_common_state->hpd_state ? 0 : 2));
