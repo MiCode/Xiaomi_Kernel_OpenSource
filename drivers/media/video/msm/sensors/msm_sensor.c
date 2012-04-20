ca00c341b7657888f6a2b3d06c9a9a3d5aa66209
Deepti Mudadla <dmudadla@codeaurora.org>
2012-04-19 17:20:14 -0700

    msm:camera: Fix for msm_sensor_set_fps function
    The msm_sensor_set_fps function should update only fps_div and the
    fll update should be done in the msm_sensor_write_exp_gain function.
    
    Change-Id: I5d1320b7af826e113cb798fc77ac61155204cf13
    Signed-off-by: Deepti Mudadla <dmudadla@codeaurora.org>

--- drivers/media/video/msm/sensors/msm_sensor.c
+++ drivers/media/video/msm/sensors/msm_sensor.c
@@ -143,20 +143,9 @@ void msm_sensor_group_hold_off(struct msm_sensor_ctrl_t *s_ctrl)
 int32_t msm_sensor_set_fps(struct msm_sensor_ctrl_t *s_ctrl,
 						struct fps_cfg *fps)
 {
-	uint16_t total_lines_per_frame;
-	int32_t rc = 0;
 	s_ctrl->fps_divider = fps->fps_div;
 
-	if (s_ctrl->curr_res != MSM_SENSOR_INVALID_RES) {
-		total_lines_per_frame = (uint16_t)
-			((s_ctrl->curr_frame_length_lines) *
-			s_ctrl->fps_divider/Q10);
-
-		rc = msm_camera_i2c_write(s_ctrl->sensor_i2c_client,
-			s_ctrl->sensor_output_reg_addr->frame_length_lines,
-			total_lines_per_frame, MSM_CAMERA_I2C_WORD_DATA);
-	}
-	return rc;
+	return 0;
 }
 
 int32_t msm_sensor_write_exp_gain1(struct msm_sensor_ctrl_t *s_ctrl,
