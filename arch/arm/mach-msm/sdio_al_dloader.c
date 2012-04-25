b503871f69f70eca6614e93bc8304cde55adc3f5
Stephen Boyd <sboyd@codeaurora.org>
2012-04-25 11:40:56 -0700

    msm: sdio: Fix section mismatch
    WARNING: vmlinux.o(.text+0x29918): Section mismatch in reference
    from the function sdio_downloader_setup() to the function
    .init.text:bootloader_debugfs_init()
    The function sdio_downloader_setup() references
    the function __init bootloader_debugfs_init().
    This is often because sdio_downloader_setup lacks a __init
    annotation or the annotation of bootloader_debugfs_init is wrong.
    
    Just remove the __init marking from bootloader_debugfs_init()
    since the call chain is long and nothing is marked __init in the
    chain.
    
    Change-Id: I4b29b45b3b60d9565fd70af27cf83814f3056a0c
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- arch/arm/mach-msm/sdio_al_dloader.c
+++ arch/arm/mach-msm/sdio_al_dloader.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2011-2012, Code Aurora Forum. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -530,7 +530,7 @@ static void update_gd(int code)
 	curr_index++;
 }
 
-static int __init bootloader_debugfs_init(void)
+static int bootloader_debugfs_init(void)
 {
 	/* /sys/kernel/debug/bootloader there will be dld_arr file */
 	root = debugfs_create_dir("bootloader", NULL);
