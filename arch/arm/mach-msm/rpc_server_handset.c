24db643f1905fae634c381dd42ac5aa4fa978a18
Stephen Boyd <sboyd@codeaurora.org>
2012-04-25 11:40:08 -0700

    msm: rpc_server_handset: Fix section mismatch
    WARNING: vmlinux.o(.devinit.text+0x228): Section mismatch in
    reference from the function hs_rpc_init() to the function
    .init.text:hs_rpc_cb_init()
    The function __devinit hs_rpc_init() references
    a function __init hs_rpc_cb_init().
    If hs_rpc_cb_init is only used by hs_rpc_init then
    annotate hs_rpc_cb_init with a matching annotation.
    
    Change-Id: I40b596f1b851801dc9b2a19c39429faf6435be39
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- arch/arm/mach-msm/rpc_server_handset.c
+++ arch/arm/mach-msm/rpc_server_handset.c
@@ -1,6 +1,6 @@
 /* arch/arm/mach-msm/rpc_server_handset.c
  *
- * Copyright (c) 2008-2010, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2008-2010,2012 Code Aurora Forum. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -517,7 +517,7 @@ static int hs_cb_func(struct msm_rpc_client *client, void *buffer, int in_size)
 	return 0;
 }
 
-static int __init hs_rpc_cb_init(void)
+static int __devinit hs_rpc_cb_init(void)
 {
 	int rc = 0, i, num_vers;
 
