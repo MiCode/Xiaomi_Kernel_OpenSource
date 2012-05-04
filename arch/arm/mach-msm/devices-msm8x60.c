e4454b833d34b348495e80488948b60589adbdc8
Matt Wagantall <mattw@codeaurora.org>
2012-05-03 20:48:01 -0700

    msm: gemini: Pass device pointers to regulator_get()
    Passing a device pointer instead of NULL as the first argument
    to regulator_get() allows the device to be taken into account
    when finding a matching regulator.
    
    Change-Id: Ib5cd5858cd0f61ea1b699385763adec9ca9ae72e
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -2656,7 +2656,7 @@ static struct fs_driver_data vpe_fs_data = {
 };
 
 struct platform_device *msm8660_footswitch[] __initdata = {
-	FS_8X60(FS_IJPEG,  "fs_ijpeg",	NULL,	&ijpeg_fs_data),
+	FS_8X60(FS_IJPEG,  "vdd",	"msm_gemini.0",	&ijpeg_fs_data),
 	FS_8X60(FS_MDP,    "vdd",	"mdp.0",	&mdp_fs_data),
 	FS_8X60(FS_ROT,    "vdd",	"msm_rotator.0", &rot_fs_data),
 	FS_8X60(FS_VED,    "vdd",	"msm_vidc.0",	&ved_fs_data),
