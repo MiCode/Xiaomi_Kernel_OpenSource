5c92211b8726c69388ca9a28e3cd67000518c538
Matt Wagantall <mattw@codeaurora.org>
2012-05-03 19:25:28 -0700

    msm: footswitch: Allow assigning of consumer names to footswitches
    regulator_get() expects two arguments: a consumer device pointer and
    a regulator name.  For footswitch regulators, users of this API have
    usually passed NULL in place of the device pointer, which was treated
    like a wildcard for the purposes of matching. Those that passed
    pointers received no advantage from it anyway, since it was not
    possible to specify consumer device names in the devices-* files using
    the macros in footswitch.h.
    
    Improve on this by expanding the footswitch macros to take a consumer
    name strings. This allows regulator_get() to find matches using a
    stricter criteria that considers the consumer device name and ID,
    as well as the regulator name.
    
    Change-Id: I77618007b674480aff031329757b41cc00014478
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

--- arch/arm/mach-msm/devices-msm7x27.c
+++ arch/arm/mach-msm/devices-msm7x27.c
@@ -870,7 +870,7 @@ struct platform_device msm_kgsl_3d0 = {
 };
 
 struct platform_device *msm_footswitch_devices[] = {
-	FS_PCOM(FS_GFX3D,  "fs_gfx3d"),
+	FS_PCOM(FS_GFX3D,  "fs_gfx3d", NULL),
 };
 unsigned msm_num_footswitch_devices = ARRAY_SIZE(msm_footswitch_devices);
 
