ff806df47392ba1110f417bc75047241e0d3b4d8
Kevin Matlage <kmatlage@codeaurora.org>
2012-05-07 18:13:21 -0600

    msm: kgsl: Fix "Resize CP ROQ for A320/A305"
    Change e8d3586fe8fc18d5c58c83781bc36122370455db
    reduced the size of the ST queue in the CP ROQ to
    8 bytes. Although that queue is almost never used,
    it appears that setting it this low can sometimes
    cause an unexpected hang when it is used. This fix
    increases the size of the ST queue to 16 bytes,
    which gets rid of the hang.
    
    We should still be able to get the majority of
    the performance benefits with this new size.
    
    Change-Id: Id3dc79d7cd62f6f1669e62825e49aa0502b90bca
    Signed-off-by: Kevin Matlage <kmatlage@codeaurora.org>

--- drivers/gpu/msm/adreno_ringbuffer.c
+++ drivers/gpu/msm/adreno_ringbuffer.c
@@ -337,8 +337,9 @@ int adreno_ringbuffer_start(struct adreno_ringbuffer *rb, unsigned int init_ram)
 	if (status != 0)
 		return status;
 
+	/* CP ROQ queue sizes (bytes) - RB:16, ST:16, IB1:32, IB2:64 */
 	if (adreno_is_a305(adreno_dev) || adreno_is_a320(adreno_dev))
-		adreno_regwrite(device, REG_CP_QUEUE_THRESHOLDS, 0x000F0602);
+		adreno_regwrite(device, REG_CP_QUEUE_THRESHOLDS, 0x000E0602);
 
 	rb->rptr = 0;
 	rb->wptr = 0;
