edf938dde9457d3c45a70ed3228c4c4ff61f1e11
Colin Cross <ccross@android.com>
2012-05-09 16:09:50 -0700

    trace: power: Add clock_set_rate_complete and clock_state trace events
    The clock_set_rate_complete even is used to monitor the latency
    of the clk_set_rate API, and the clock_state event is used to
    add support in debugfs to take a snapshot of all the clocks
    present and their current state to ftrace.
    
    This is a squash of the following commits:
    	1. clk: qcom: Add support to record clock state using ftrace
    	2. tracing: power: Define clock_set_rate_complete trace event
    
    Signed-off-by: Taniya Das <tdas@codeaurora.org>

--- include/trace/events/power.h
+++ include/trace/events/power.h
@@ -298,6 +298,13 @@ DEFINE_EVENT(clock, clock_set_rate,
 	TP_ARGS(name, state, cpu_id)
 );
 
+DEFINE_EVENT(clock, clock_set_rate_complete,
+
+	TP_PROTO(const char *name, unsigned int state, unsigned int cpu_id),
+
+	TP_ARGS(name, state, cpu_id)
+);
+
 TRACE_EVENT(clock_set_parent,
 
 	TP_PROTO(const char *name, const char *parent_name),
@@ -315,6 +322,31 @@ TRACE_EVENT(clock_set_parent,
 	),
 
 	TP_printk("%s parent=%s", __get_str(name), __get_str(parent_name))
+);
+
+TRACE_EVENT(clock_state,
+
+	TP_PROTO(const char *name, unsigned long prepare_count,
+		unsigned long count, unsigned long rate),
+
+	TP_ARGS(name, prepare_count, count, rate),
+
+	TP_STRUCT__entry(
+		__string(name,			name)
+		__field(unsigned long,		prepare_count)
+		__field(unsigned long,		count)
+		__field(unsigned long,		rate)
+	),
+
+	TP_fast_assign(
+		__assign_str(name, name);
+		__entry->prepare_count = prepare_count;
+		__entry->count = count;
+		__entry->rate = rate;
+	),
+
+	TP_printk("%s\t[%lu:%lu]\t%lu", __get_str(name), __entry->prepare_count,
+					__entry->count, __entry->rate)
 );
 
 /*
