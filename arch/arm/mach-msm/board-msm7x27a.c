72ab89b5237d2af871c5ee73ab0fb909bca29f13
Chaithanya Krishna Bacharaju <chaithan@codeaurora.org>
2012-05-11 14:24:04 +0530

    msm: board-7x27a: Use AUDPLAY0 task for tunnel and nontunnel mode
    Move non-tunnel mode to AUDPLAY0 task from AUDPLAY1 task to
    acheive low power numbers.
    For both tunnel and non-tunnel mode use only AUDPLAY0.
    
    CRs-Fixed: 359944
    Change-Id: I855ebf26cc0e74d71722fd7e27d715a295d1baed
    Signed-off-by: Chaithanya Krishna Bacharaju <chaithan@codeaurora.org>

--- arch/arm/mach-msm/board-msm7x27a.c
+++ arch/arm/mach-msm/board-msm7x27a.c
@@ -563,9 +563,9 @@ static unsigned int dec_concurrency_table[] = {
 	(DEC4_FORMAT),
 
 	/* Concurrency 6 */
-	(DEC0_FORMAT|(1<<MSM_ADSP_MODE_TUNNEL)|(1<<MSM_ADSP_OP_DM)),
-	(DEC1_FORMAT|(1<<MSM_ADSP_MODE_NONTUNNEL)|(1<<MSM_ADSP_OP_DM)),
-	0, 0, 0,
+	(DEC0_FORMAT|(1<<MSM_ADSP_MODE_TUNNEL)|
+			(1<<MSM_ADSP_MODE_NONTUNNEL)|(1<<MSM_ADSP_OP_DM)),
+	0, 0, 0, 0,
 
 	/* Concurrency 7 */
 	(DEC0_FORMAT|(1<<MSM_ADSP_MODE_NONTUNNEL)|(1<<MSM_ADSP_OP_DM)),
