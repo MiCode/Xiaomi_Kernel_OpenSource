ae43043b4bbb2424e535ec4b27d2a51414064d7c
Olav Haugan <ohaugan@codeaurora.org>
2012-05-14 09:47:11 -0700

    gpu: ion: Add check for overlapping heaps
    During bootup add check to ensure heap addresses are not overlapping
    with each other. If a heap is found to be overlapping with another
    heap boot will be stopped with a call to panic.
    
    Change-Id: Id37aefa2e09aa165ec376e241e282faf5f3c5737
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>

--- drivers/gpu/ion/msm/msm_ion.c
+++ drivers/gpu/ion/msm/msm_ion.c
@@ -215,6 +215,45 @@ static void msm_ion_allocate(struct ion_platform_heap *heap)
 	}
 }
 
+static int is_heap_overlapping(const struct ion_platform_heap *heap1,
+				const struct ion_platform_heap *heap2)
+{
+	unsigned long heap1_base = heap1->base;
+	unsigned long heap2_base = heap2->base;
+	unsigned long heap1_end = heap1->base + heap1->size - 1;
+	unsigned long heap2_end = heap2->base + heap2->size - 1;
+
+	if (heap1_base == heap2_base)
+		return 1;
+	if (heap1_base < heap2_base && heap1_end >= heap2_base)
+		return 1;
+	if (heap2_base < heap1_base && heap2_end >= heap1_base)
+		return 1;
+	return 0;
+}
+
+static void check_for_heap_overlap(const struct ion_platform_heap heap_list[],
+				   unsigned long nheaps)
+{
+	unsigned long i;
+	unsigned long j;
+
+	for (i = 0; i < nheaps; ++i) {
+		const struct ion_platform_heap *heap1 = &heap_list[i];
+		if (!heap1->base)
+			continue;
+		for (j = i + 1; j < nheaps; ++j) {
+			const struct ion_platform_heap *heap2 = &heap_list[j];
+			if (!heap2->base)
+				continue;
+			if (is_heap_overlapping(heap1, heap2)) {
+				panic("Memory in heap %s overlaps with heap %s\n",
+					heap1->name, heap2->name);
+			}
+		}
+	}
+}
+
 static int msm_ion_probe(struct platform_device *pdev)
 {
 	struct ion_platform_data *pdata = pdev->dev.platform_data;
@@ -260,6 +299,8 @@ static int msm_ion_probe(struct platform_device *pdev)
 
 		ion_device_add_heap(idev, heaps[i]);
 	}
+
+	check_for_heap_overlap(pdata->heaps, num_heaps);
 	platform_set_drvdata(pdev, idev);
 	return 0;
 
