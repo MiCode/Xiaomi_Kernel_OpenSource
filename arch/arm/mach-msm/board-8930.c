494e4dc5e0a966a6fb27e675b5b47056ebca3331
Olav Haugan <ohaugan@codeaurora.org>
2012-05-15 15:14:31 -0700

    board: 8030: Increase QSECOM ION heap to 7.5MB.
    When IOMMU is enabled the SF heap is not needed anymore. QSECOM was
    using this heap in addition to the QSECOM heap and QSECOM
    cannot use the IOMMU heap and thus we need to increase the
    QSECOM heap size.
    
    Increase QSECOM ION heap to 7.5MB from 6MB only when IOMMU is
    enabled.
    
    Change-Id: If1487de5ba0cf5717c8e28a06e94e69b377cd7d3
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>

--- arch/arm/mach-msm/board-8930.c
+++ arch/arm/mach-msm/board-8930.c
@@ -131,14 +131,15 @@ struct sx150x_platform_data msm8930_sx150x_data[] = {
 #ifdef CONFIG_MSM_IOMMU
 #define MSM_ION_MM_SIZE            0x3800000 /* Need to be multiple of 64K */
 #define MSM_ION_SF_SIZE            0x0
+#define MSM_ION_QSECOM_SIZE	0x780000 /* (7.5MB) */
 #define MSM_ION_HEAP_NUM	7
 #else
 #define MSM_ION_SF_SIZE		MSM_PMEM_SIZE
 #define MSM_ION_MM_SIZE		MSM_PMEM_ADSP_SIZE
+#define MSM_ION_QSECOM_SIZE	0x600000 /* (6MB) */
 #define MSM_ION_HEAP_NUM	8
 #endif
 #define MSM_ION_MM_FW_SIZE	0x200000 /* (2MB) */
-#define MSM_ION_QSECOM_SIZE	0x600000 /* (6MB) */
 #define MSM_ION_MFC_SIZE	SZ_8K
 #define MSM_ION_AUDIO_SIZE	MSM_PMEM_AUDIO_SIZE
 
