095128a9fd51cc7f2373a303507dd45a735bb7fe
Hemant Kumar <hemantk@codeaurora.org>
2012-05-15 12:18:12 -0700

    usb: serial: Fix race between write after suspend and resume
    wwan serial driver sets suspend flag when it goes to suspend.
    Driver puts any write request to delayed queue during suspend
    and submits the delayed write requests while resuming and clears
    the suspend flag after that. This creates a race condition where
    write request comes between queuing the delayed write request and
    clearing the suspend flag.
    
    CRs-Fixed: 360555
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>
    Change-Id: Ib15b80bb7e289db66a6306b6147f058b5e78f3e5

--- drivers/usb/serial/usb_wwan.c
+++ drivers/usb/serial/usb_wwan.c
@@ -666,6 +666,10 @@ int usb_wwan_resume(struct usb_serial *serial)
 		}
 	}
 
+	spin_lock_irq(&intfdata->susp_lock);
+	intfdata->suspended = 0;
+	spin_unlock_irq(&intfdata->susp_lock);
+
 	for (i = 0; i < serial->num_ports; i++) {
 		/* walk all ports */
 		port = serial->port[i];
@@ -691,9 +695,6 @@ int usb_wwan_resume(struct usb_serial *serial)
 		play_delayed(port);
 		spin_unlock_irq(&intfdata->susp_lock);
 	}
-	spin_lock_irq(&intfdata->susp_lock);
-	intfdata->suspended = 0;
-	spin_unlock_irq(&intfdata->susp_lock);
 err_out:
 	return err;
 }
