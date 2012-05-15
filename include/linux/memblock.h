bf7400e7166de695eedcffef0b20882508c71a03
Stephen Boyd <sboyd@codeaurora.org>
2012-05-14 18:55:50 -0700

    memblock: Add memblock_overlaps_memory()
    Add a new function, memblock_overlaps_memory(), to check if a
    region overlaps with a memory bank. This will be used by
    peripheral loader code to detect when kernel memory would be
    overwritten.
    
    Change-Id: I851f8f416a0f36e85c0e19536b5209f7d4bd431c
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    (cherry picked from commit cc2753448d9f2adf48295f935a7eee36023ba8d3)
    Signed-off-by: Josh Cartwright <joshc@codeaurora.org>

--- include/linux/memblock.h
+++ include/linux/memblock.h
@@ -285,6 +285,7 @@ phys_addr_t memblock_end_of_DRAM(void);
 void memblock_enforce_memory_limit(phys_addr_t memory_limit);
 int memblock_is_memory(phys_addr_t addr);
 int memblock_is_region_memory(phys_addr_t base, phys_addr_t size);
+int memblock_overlaps_memory(phys_addr_t base, phys_addr_t size);
 int memblock_is_reserved(phys_addr_t addr);
 int memblock_is_region_reserved(phys_addr_t base, phys_addr_t size);
 
