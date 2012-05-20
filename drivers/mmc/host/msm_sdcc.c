8afe8d2a98a1bbf3804162ff5c95a56226935f5a
Maya Erez <merez@codeaurora.org>
2012-05-20 15:11:52 +0300

    mmc: msm_sdcc: enable the write packing control
    Write packing control prevents degradation of read throughput
    in parallel read and write scenarios when the write packing
    feature is enabled
    
    Change-Id: I844430941c98248a8c0324788de8704143f3019a
    Signed-off-by: Maya Erez <merez@codeaurora.org>

--- drivers/mmc/host/msm_sdcc.c
+++ drivers/mmc/host/msm_sdcc.c
@@ -5020,6 +5020,7 @@ msmsdcc_probe(struct platform_device *pdev)
 				MMC_CAP_SET_XPC_180);
 
 	mmc->caps2 |= MMC_CAP2_PACKED_WR;
+	mmc->caps2 |= MMC_CAP2_PACKED_WR_CONTROL;
 	mmc->caps2 |= (MMC_CAP2_BOOTPART_NOACC | MMC_CAP2_DETECT_ON_ERR);
 	if (pdev->dev.of_node) {
 		if (of_get_property((&pdev->dev)->of_node,
