389d9d07728c9b56242f361bb3a4accdadb69ec4
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2012-05-25 16:20:46 +0530

    USB: msm_otg: Don't call power_supply_get_by_name in IRQ context
    OTG driver should not be using power_supply_get_by_name in IRQ context
    and calling this api for every USB cable insertion/removal is very
    inefficient.
    
    CRs-Fixed: 363098
    Change-Id: Iae7f63c755fbb177abd23cbff0b81d3cd76f95b8
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>

--- drivers/usb/phy/phy-msm-usb.c
+++ drivers/usb/phy/phy-msm-usb.c
@@ -80,6 +80,7 @@ static struct regulator *hsusb_1p8;
 static struct regulator *hsusb_vddcx;
 static struct regulator *vbus_otg;
 static struct regulator *mhl_analog_switch;
+static struct power_supply *psy;
 
 static bool aca_id_turned_on;
 static inline bool aca_enabled(void)
@@ -1006,9 +1007,7 @@ static int msm_otg_notify_chg_type(struct msm_otg *motg)
 
 static int msm_otg_notify_power_supply(struct msm_otg *motg, unsigned mA)
 {
-	struct power_supply *psy;
 
-	psy = power_supply_get_by_name("usb");
 	if (!psy)
 		goto psy_not_supported;
 
@@ -2014,6 +2013,9 @@ static void msm_otg_sm_work(struct work_struct *w)
 	case OTG_STATE_UNDEFINED:
 		msm_otg_reset(otg->phy);
 		msm_otg_init_sm(motg);
+		psy = power_supply_get_by_name("usb");
+		if (!psy)
+			pr_err("couldn't get usb power supply\n");
 		otg->phy->state = OTG_STATE_B_IDLE;
 		if (!test_bit(B_SESS_VLD, &motg->inputs) &&
 				test_bit(ID, &motg->inputs)) {
