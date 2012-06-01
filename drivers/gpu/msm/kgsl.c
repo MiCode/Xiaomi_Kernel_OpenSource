dad9df5f82856197bfc921eb71e4515f57441145
Ranjhith Kalisamy <ranjhith@codeaurora.org>
2012-06-01 17:05:13 +0530

    msm: kgsl: Follow reverse sequence of init procedure during exit
    Fixes kernel warnings about sysfs entries being unavailable when they
    are removed during exit.
    
    Change-Id: I6caaaff935b31d3f39a3610cdd2b048baf2f1a97
    Signed-off-by: Ranjhith Kalisamy <ranjhith@codeaurora.org>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -2595,10 +2595,13 @@ kgsl_ptdata_init(void)
 
 static void kgsl_core_exit(void)
 {
-	unregister_chrdev_region(kgsl_driver.major, KGSL_DEVICE_MAX);
-
 	kgsl_mmu_ptpool_destroy(kgsl_driver.ptpool);
 	kgsl_driver.ptpool = NULL;
+
+	kgsl_drm_exit();
+	kgsl_cffdump_destroy();
+	kgsl_core_debugfs_close();
+	kgsl_sharedmem_uninit_sysfs();
 
 	device_unregister(&kgsl_driver.virtdev);
 
@@ -2607,10 +2610,7 @@ static void kgsl_core_exit(void)
 		kgsl_driver.class = NULL;
 	}
 
-	kgsl_drm_exit();
-	kgsl_cffdump_destroy();
-	kgsl_core_debugfs_close();
-	kgsl_sharedmem_uninit_sysfs();
+	unregister_chrdev_region(kgsl_driver.major, KGSL_DEVICE_MAX);
 }
 
 static int __init kgsl_core_init(void)
