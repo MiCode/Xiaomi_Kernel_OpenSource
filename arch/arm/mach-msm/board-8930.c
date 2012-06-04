19e7881725a500fd8fdb02abf4d8c61c44d10198
David Collins <collinsd@codeaurora.org>
2012-06-04 15:50:09 -0700

    msm: board-8930: Increase LDO 20 voltage request from 1.2 V to 1.25 V
    Hardware characterization has shown that memory stability
    improves when the DDR supply voltage (provided by LDO 20) is
    increased to 1.25 V. Therefore, increase the voltage constraints
    for LDO 20 so that 1.25 V is requested. This also requires
    modifying the voltages requested by the consumers of LDO 20.
    LDO 20 is used by the Sitar audio CODEC.
    
    Change-Id: Ia2fb75b509ea96d25bfacbcf9a14561b1b439f9e
    Signed-off-by: David Collins <collinsd@codeaurora.org>

--- arch/arm/mach-msm/board-8930.c
+++ arch/arm/mach-msm/board-8930.c
@@ -786,13 +786,13 @@ static struct wcd9xxx_pdata sitar_platform_data = {
 	{
 		.name = "VDDD_CDC_D",
 		.min_uV = 1200000,
-		.max_uV = 1200000,
+		.max_uV = 1250000,
 		.optimum_uA = WCD9XXX_VDDD_CDC_D_CUR_MAX,
 	},
 	{
 		.name = "CDC_VDDA_A_1P2V",
 		.min_uV = 1200000,
-		.max_uV = 1200000,
+		.max_uV = 1250000,
 		.optimum_uA = WCD9XXX_VDDD_CDC_A_CUR_MAX,
 	},
 	},
@@ -850,13 +850,13 @@ static struct wcd9xxx_pdata sitar1p1_platform_data = {
 	{
 		.name = "VDDD_CDC_D",
 		.min_uV = 1200000,
-		.max_uV = 1200000,
+		.max_uV = 1250000,
 		.optimum_uA = WCD9XXX_VDDD_CDC_D_CUR_MAX,
 	},
 	{
 		.name = "CDC_VDDA_A_1P2V",
 		.min_uV = 1200000,
-		.max_uV = 1200000,
+		.max_uV = 1250000,
 		.optimum_uA = WCD9XXX_VDDD_CDC_A_CUR_MAX,
 	},
 	},
