bccd68098d1b8e25d97202aeece398317cacf80b
Felipe Balbi <balbi@ti.com>
2012-06-06 10:20:23 +0300

    usb: gadget: composite: fix ep->maxburst initialization
    bMaxBurst field on endpoint companion descriptor
    is supposed to contain the number of burst minus
    1. When passing that to controller drivers, we
    should be passing the real number instead (by
    incrementing 1).
    
    While doing that, also fix the assumption on
    dwc3 that value comes decremented by one.
    
    Change-Id: I1b6331662af2d6dffa971c2c560dcf2ace768d1f
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    [pkondeti@codeaurora.org: resolved merge conflicts]
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -117,6 +117,7 @@ int config_ep_by_speed(struct usb_gadget *g,
 			struct usb_function *f,
 			struct usb_ep *_ep)
 {
+	struct usb_composite_dev	*cdev = get_gadget_data(g);
 	struct usb_endpoint_descriptor *chosen_desc = NULL;
 	struct usb_descriptor_header **speed_desc = NULL;
 
@@ -177,14 +178,16 @@ ep_found:
 		switch (usb_endpoint_type(_ep->desc)) {
 		case USB_ENDPOINT_XFER_BULK:
 		case USB_ENDPOINT_XFER_INT:
-			_ep->maxburst = comp_desc->bMaxBurst;
+			_ep->maxburst = comp_desc->bMaxBurst + 1;
 			break;
 		case USB_ENDPOINT_XFER_ISOC:
 			/* mult: bits 1:0 of bmAttributes */
 			_ep->mult = comp_desc->bmAttributes & 0x3;
 			break;
 		default:
-			/* Do nothing for control endpoints */
+			if (comp_desc->bMaxBurst != 0)
+				ERROR(cdev, "ep0 bMaxBurst must be 0\n");
+			_ep->maxburst = 1;
 			break;
 		}
 	}
