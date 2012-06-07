991d7099f512653500ea000b0116b5b3307a6d33
Trilok Soni <tsoni@codeaurora.org>
2012-06-07 18:55:37 +0530

    ARM: gic: Remove ARCH_MSM8625 around the gic_save APIs
    msm_gic_save/restore APIs were added to save the context of the
    GIC across the apps. power collapse since 8625 PM framework
    doesn't use the CPUIdle framework of the kernel. If the CPUIdle
    framework was in use then we could have used the GIC driver provided
    notification mechanism which takes of calling appropriate functions.
    There is no need to protect these APIs using this #ifdef since there
    is nothing specific to 8625 inside, also add empty functions for save
    and restore since not all targets have CPU_PM defined.
    
    Change-Id: I02bb4e4021c31caf7ab1282fb675d45ffba42a66
    Signed-off-by: Trilok Soni <tsoni@codeaurora.org>

--- drivers/irqchip/irq-gic.c
+++ drivers/irqchip/irq-gic.c
@@ -851,6 +851,22 @@ static void __init gic_pm_init(struct gic_chip_data *gic)
 static void __init gic_pm_init(struct gic_chip_data *gic)
 {
 }
+
+static void gic_cpu_restore(unsigned int gic_nr)
+{
+}
+
+static void gic_cpu_save(unsigned int gic_nr)
+{
+}
+
+static void gic_dist_restore(unsigned int gic_nr)
+{
+}
+
+static void gic_dist_save(unsigned int gic_nr)
+{
+}
 #endif
 
 #ifdef CONFIG_SMP
@@ -1202,6 +1218,7 @@ unsigned int msm_gic_spi_ppi_pending(void)
 
 	return 0;
 }
+#endif
 
 void msm_gic_save(bool modem_wake, int from_idle)
 {
@@ -1261,4 +1278,3 @@ void gic_configure_and_raise(unsigned int irq, unsigned int cpu)
 
 	raw_spin_unlock_irqrestore(&irq_controller_lock, flags);
 }
-#endif
