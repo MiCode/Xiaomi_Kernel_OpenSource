cc87ab232563d656df440d313bfd69359936097d
Wentao Xu <wentaox@codeaurora.org>
2012-06-12 11:52:34 -0400

    input: change mpu3050 driver to request regulator, add poll support
    Needed on 8x30 MTP/MDP platforms, where interrupt line polled up and
    does not work with driver's default configuration
    
    Change-Id: I4827c7a1f7df147245336d18523088c013381f1e
    Signed-off-by: Wentao Xu <wentaox@codeaurora.org>

---
+++ include/linux/input/mpu3050.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2012, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+
+#ifndef __MPU3050_H__
+#define __MPU3050_H__
+
+struct mpu3050_gyro_platform_data {
+	int poll_interval;
+	int min_interval;
+
+	int (*init)(void);
+	void (*exit)(void);
+	int (*power_on)(void);
+	int (*power_off)(void);
+
+	int gpio_int;
+	int gpio_fsync;
+};
+
+#endif /* __MPU3050_H__ */
