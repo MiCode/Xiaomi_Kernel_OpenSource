e7c576103b8af32bec9fbb1f3359e78f8c1887b5
Raju P.L.S.S.S.N <rplsssn@codeaurora.org>
2012-06-13 20:13:27 +0530

    msm: board-qrd7627a: Add gpio regulator for ov8825 sensor
    Use gpio regulator driver for ov8825 (8MP) sensor as the
    power related gpios are shared between the sensor and
    lcd display.
    
    Change-Id: Id6a4cc88dadd28c57135052c1dad43065d336f9b
    Signed-off-by: Raju P.L.S.S.S.N <rplsssn@codeaurora.org>

--- arch/arm/mach-msm/board-qrd7627a.c
+++ arch/arm/mach-msm/board-qrd7627a.c
@@ -651,14 +651,16 @@ static struct platform_device fmem_device = {
 #define GPIO_VREG_ID_EXT_1P8V	1
 
 static struct regulator_consumer_supply vreg_consumers_EXT_2P85V[] = {
-	REGULATOR_SUPPLY("cam0_avdd", "0-006c"),
-	REGULATOR_SUPPLY("cam1_avdd", "0-0078"),
+	REGULATOR_SUPPLY("cam_ov5647_avdd", "0-006c"),
+	REGULATOR_SUPPLY("cam_ov7692_avdd", "0-0078"),
+	REGULATOR_SUPPLY("cam_ov8825_avdd", "0-000d"),
 	REGULATOR_SUPPLY("lcd_vdd", "mipi_dsi.1"),
 };
 
 static struct regulator_consumer_supply vreg_consumers_EXT_1P8V[] = {
-	REGULATOR_SUPPLY("cam0_vdd", "0-006c"),
-	REGULATOR_SUPPLY("cam1_vdd", "0-0078"),
+	REGULATOR_SUPPLY("cam_ov5647_vdd", "0-006c"),
+	REGULATOR_SUPPLY("cam_ov7692_vdd", "0-0078"),
+	REGULATOR_SUPPLY("cam_ov8825_vdd", "0-000d"),
 	REGULATOR_SUPPLY("lcd_vddi", "mipi_dsi.1"),
 };
 
