0aea8fa33d562e6bb6481873057972c0fadc05ed
Ravi Kumar V <kumarrav@codeaurora.org>
2012-06-15 17:25:44 +0530

    media: rc: Fix Kernel bug message from gpio_ir_recv driver
    Getting invalid context warning message from gpio_ir_recv driver when
    remote is operated, gpio_get_value_cansleep function is called from
    interrupt context, check gpio_cansleep and use appropriate context API's
    
    Change-Id: I1bcfbbce5e9737eecb9ebc46553f9121b4ee779a
    Signed-off-by: Ravi Kumar V <kumarrav@codeaurora.org>

--- drivers/media/rc/gpio-ir-recv.c
+++ drivers/media/rc/gpio-ir-recv.c
@@ -29,6 +29,7 @@ struct gpio_rc_dev {
 	struct rc_dev *rcdev;
 	int gpio_nr;
 	bool active_low;
+	int can_sleep;
 };
 
 #ifdef CONFIG_OF
@@ -77,7 +78,10 @@ static irqreturn_t gpio_ir_recv_irq(int irq, void *dev_id)
 	int rc = 0;
 	enum raw_event_type type = IR_SPACE;
 
-	gval = gpio_get_value_cansleep(gpio_dev->gpio_nr);
+	if (gpio_dev->can_sleep)
+		gval = gpio_get_value_cansleep(gpio_dev->gpio_nr);
+	else
+		gval = gpio_get_value(gpio_dev->gpio_nr);
 
 	if (gval < 0)
 		goto err_get_value;
@@ -156,6 +160,9 @@ static int gpio_ir_recv_probe(struct platform_device *pdev)
 	rc = gpio_request(pdata->gpio_nr, "gpio-ir-recv");
 	if (rc < 0)
 		goto err_gpio_request;
+
+	gpio_dev->can_sleep = gpio_cansleep(pdata->gpio_nr);
+
 	rc  = gpio_direction_input(pdata->gpio_nr);
 	if (rc < 0)
 		goto err_gpio_direction_input;
