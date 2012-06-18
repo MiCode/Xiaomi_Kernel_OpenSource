4bca39eb114ce1fe424317d8df834b7a4479935c
Amit Blay <ablay@codeaurora.org>
2012-06-18 20:56:38 +0300

    usb: gadget: Fix HW enqueue of SPS transfer
    As part of kernel 3.4 merge, this driver was modified such
    that TRB of size 0 points to DMA_ADDR_INVALID. This causes
    infinite SPS transfer to fail after a single transfer.
    The fix changes the TRB to point to 0.
    
    Change-Id: I772631f79582db69dccc7aa1801e69695505b958
    Signed-off-by: Amit Blay <ablay@codeaurora.org>

--- drivers/usb/chipidea/udc.c
+++ drivers/usb/chipidea/udc.c
@@ -466,6 +466,7 @@ static int _hardware_enqueue(struct ci13xxx_ep *mEp, struct ci13xxx_req *mReq)
 			if (!mReq->req.no_interrupt)
 				mReq->ptr->token |= MSM_ETD_IOC;
 		}
+		mReq->req.dma = 0;
 	}
 	mReq->ptr->page[0]  = cpu_to_le32(mReq->req.dma);
 	for (i = 1; i < TD_PAGE_COUNT; i++) {
