1467ac69e55c5938706ef4a09c4a0d72e6f19172
Pavankumar Kondeti <pkondeti@codeaurora.org>
2012-06-19 18:06:13 +0530

    USB: EHCI: Fix bug in updating root hub state during suspend
    commit e879990 (USB: EHCI: remove usages of hcd->state) removed the
    hcd->state usage in EHCI driver.  Instead rh_state field of ehci_hcd
    struct should be used. But the current code is still using
    hcd->state. This bug is introduced during 3.4 kernel porting.
    
    If rh_state field is not updated to SUSPEND during root hub suspend,
    the root hub will not be resumed upon device connection or remote wakeup.
    
    Change-Id: Id47e7c650c03cda2e7be00be4e8b92a47257fc6a
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- drivers/usb/host/ehci-hub.c
+++ drivers/usb/host/ehci-hub.c
@@ -326,7 +326,7 @@ static int ehci_bus_suspend (struct usb_hcd *hcd)
 	if (!ehci->susp_sof_bug)
 		ehci_halt(ehci); /* turn off now-idle HC */
 
-	hcd->state = HC_STATE_SUSPENDED;
+	ehci->rh_state = EHCI_RH_SUSPENDED;
 
 	spin_lock_irq(&ehci->lock);
 	if (ehci->enabled_hrtimer_events & BIT(EHCI_HRTIMER_POLL_DEAD))
@@ -599,6 +599,10 @@ ehci_hub_status_data (struct usb_hcd *hcd, char *buf)
 	int		ports, i, retval = 1;
 	unsigned long	flags;
 	u32		ppcd = ~0;
+
+	/* if !USB_SUSPEND, root hub timers won't get shut down ... */
+	if (ehci->rh_state != EHCI_RH_RUNNING)
+		return 0;
 
 	/* init status to no-changes */
 	buf [0] = 0;
