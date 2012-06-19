e14d767ab760f89ee5cfb2d0a2ca85cbfb191f92
Jeff Ohlstein <johlstei@codeaurora.org>
2012-06-19 15:59:46 -0700

    sched_avg: add run queue averaging
    Add code to calculate the run queue depth of a cpu and iowait
    depth of the cpu.
    
    The scheduler calls in to sched_update_nr_prod whenever there
    is a runqueue change. This function maintains the runqueue average
    and the iowait of that cpu in that time interval.
    
    Whoever wants to know the runqueue average is expected to call
    sched_get_nr_running_avg periodically to get the accumulated
    runqueue and iowait averages for all the cpus.
    
    Change-Id: Id8cb2ecf0ed479f090a83ccb72dd59c53fa73e0c
    Signed-off-by: Jeff Ohlstein <johlstei@codeaurora.org>
    (cherry picked from commit 0299fcaaad80e2c0ac9aa583c95107f6edc27750)

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1075,6 +1075,7 @@ static inline u64 steal_ticks(u64 steal)
 
 static inline void inc_nr_running(struct rq *rq)
 {
+	sched_update_nr_prod(cpu_of(rq), rq->nr_running, true);
 	rq->nr_running++;
 
 #ifdef CONFIG_NO_HZ_FULL
@@ -1090,6 +1091,7 @@ static inline void inc_nr_running(struct rq *rq)
 
 static inline void dec_nr_running(struct rq *rq)
 {
+	sched_update_nr_prod(cpu_of(rq), rq->nr_running, false);
 	rq->nr_running--;
 }
 
