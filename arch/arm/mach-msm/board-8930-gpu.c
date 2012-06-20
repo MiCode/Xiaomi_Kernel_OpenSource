d8f323385ad37faf969b39148ff931dec37a5fd9
Sudhakara Rao Tentu <srtentu@codeaurora.org>
2012-06-20 14:42:58 +0530

    msm: board-8930: Configure GPU turbo clock to 400MHz for msm8627
    As per msm8627 HW, GPU turbo clock is 400MHz
    
    CRs-Fixed: 374874
    Change-Id: I37e16ddc33dd2971562511e6c2c7df56f0f27e1e
    Signed-off-by: Sudhakara Rao Tentu <srtentu@codeaurora.org>

--- arch/arm/mach-msm/board-8930-gpu.c
+++ arch/arm/mach-msm/board-8930-gpu.c
@@ -16,6 +16,7 @@
 #include <linux/msm_kgsl.h>
 #include <mach/msm_bus_board.h>
 #include <mach/board.h>
+#include <mach/socinfo.h>
 
 #include "devices.h"
 #include "board-8930.h"
@@ -159,5 +160,8 @@ static struct platform_device device_kgsl_3d0 = {
 
 void __init msm8930_init_gpu(void)
 {
+	if (cpu_is_msm8627())
+		kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 400000000;
+
 	platform_device_register(&device_kgsl_3d0);
 }
