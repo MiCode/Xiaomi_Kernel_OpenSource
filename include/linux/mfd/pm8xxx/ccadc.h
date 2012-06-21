ab43ad14e02db7c4c8af59847e97f95d5544e4b7
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2012-06-21 16:15:43 -0700

    power: pm8xxx-ccadc: remove support for pm8921 v1.0 and v1.1
    This ccadc module is reused in multiple pmics. It is now unnecessary
    to check for 8921 versions in the driver. As a result of this change
    older 8921 versions are not supported.
    
    CRs-Fixed: 372679
    Change-Id: I2114f9428e2ba1a29c2bc9caaaa75106caf1dfc3
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- include/linux/mfd/pm8xxx/ccadc.h
+++ include/linux/mfd/pm8xxx/ccadc.h
@@ -27,32 +27,13 @@ struct pm8xxx_ccadc_platform_data {
 	unsigned int	calib_delay_ms;
 };
 
-#define CCADC_READING_RESOLUTION_N_V1	1085069
-#define CCADC_READING_RESOLUTION_D_V1	100000
-#define CCADC_READING_RESOLUTION_N_V2	542535
-#define CCADC_READING_RESOLUTION_D_V2	100000
-
-static s64 pm8xxx_ccadc_reading_to_microvolt_v1(s64 cc)
-{
-	return div_s64(cc * CCADC_READING_RESOLUTION_N_V1,
-					CCADC_READING_RESOLUTION_D_V1);
-}
-
-static s64 pm8xxx_ccadc_reading_to_microvolt_v2(s64 cc)
-{
-	return div_s64(cc * CCADC_READING_RESOLUTION_N_V2,
-					CCADC_READING_RESOLUTION_D_V2);
-}
+#define CCADC_READING_RESOLUTION_N	542535
+#define CCADC_READING_RESOLUTION_D	100000
 
 static inline s64 pm8xxx_ccadc_reading_to_microvolt(int revision, s64 cc)
 {
-	/*
-	 * resolution (the value of a single bit) was changed after revision 2.0
-	 * for more accurate readings
-	 */
-	return (revision < PM8XXX_REVISION_8921_2p0) ?
-				pm8xxx_ccadc_reading_to_microvolt_v1((s64)cc) :
-				pm8xxx_ccadc_reading_to_microvolt_v2((s64)cc);
+	return div_s64(cc * CCADC_READING_RESOLUTION_N,
+					CCADC_READING_RESOLUTION_D);
 }
 
 #if defined(CONFIG_PM8XXX_CCADC) || defined(CONFIG_PM8XXX_CCADC_MODULE)
