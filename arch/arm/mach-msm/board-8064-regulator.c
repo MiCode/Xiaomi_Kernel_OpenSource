a041748abd20736d20a55e7fe4ffc6f3330b6977
Ravishangar Kalyanam <rkalya@codeaurora.org>
2012-06-20 18:30:05 -0700

    msm: display: Add MDP footswitch regulator control for HDMI PLL
    Add support for enabling HDMI PLL regulator along with footswitch
    enable/disable to avoid PLL lock detection failures during
    suspend/resume
    
    Change-Id: I9b161cab50d2b6c39de39b588e326f5ff97cbc65
    Signed-off-by: Ravishangar Kalyanam <rkalya@codeaurora.org>

--- arch/arm/mach-msm/board-8064-regulator.c
+++ arch/arm/mach-msm/board-8064-regulator.c
@@ -221,6 +221,7 @@ VREG_CONSUMERS(LVS7) = {
 	REGULATOR_SUPPLY("8921_lvs7",		NULL),
 	REGULATOR_SUPPLY("pll_vdd",		"pil_riva"),
 	REGULATOR_SUPPLY("lvds_vdda",		"lvds.0"),
+	REGULATOR_SUPPLY("hdmi_pll_fs",		"mdp.0"),
 	REGULATOR_SUPPLY("dsi1_vddio",		"mipi_dsi.1"),
 	REGULATOR_SUPPLY("hdmi_vdda",		"hdmi_msm.0"),
 };
