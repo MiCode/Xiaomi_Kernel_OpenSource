f61d2e7d4fdcd2c2c2d62e2e4825bc9a06bb26c0
Sujit Reddy Thumma <sthumma@codeaurora.org>
2012-06-22 15:56:39 +0530

    mmc: msm_sdcc: Add support for card detect, write protect gpios
    Add support for SD card insertion/removal detection and
    write protect switch detection using gpio numbers passed
    through device tree.
    
    Change-Id: I63aed72f38f912b33de4b8eaedf72e52bbaeb4a0
    Signed-off-by: Sujit Reddy Thumma <sthumma@codeaurora.org>

--- arch/arm/include/asm/mach/mmc.h
+++ arch/arm/include/asm/mach/mmc.h
@@ -146,7 +146,7 @@ struct mmc_platform_data {
 	unsigned int uhs_caps2;
 	void (*sdio_lpm_gpio_setup)(struct device *, unsigned int);
         unsigned int status_irq;
-	unsigned int status_gpio;
+	int status_gpio;
 	/* Indicates the polarity of the GPIO line when card is inserted */
 	bool is_status_gpio_active_low;
         unsigned int sdiowakeup_irq;
@@ -158,7 +158,7 @@ struct mmc_platform_data {
 	unsigned int msmsdcc_fmax;
 	bool nonremovable;
 	unsigned int mpm_sdiowakeup_int;
-	unsigned int wpswitch_gpio;
+	int wpswitch_gpio;
 	bool is_wpswitch_active_low;
 	struct msm_mmc_slot_reg_data *vreg_data;
 	int is_sdio_al_client;
