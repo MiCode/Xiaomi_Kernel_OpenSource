27362bd4211a695ccc6d335ce8b454dac39a60be
Pavankumar Kondeti <pkondeti@codeaurora.org>
2012-06-26 09:44:36 +0530

    USB: android: Add support for USB Target Function (UASP)
    Add UASP function to Android supported functions array.  UASP
    function depend on Target core.  Provide empty functions when
    Target core is not enabled.
    
    Change-Id: I959f26ebd63e036dae041708d0ad6169213a3cf0
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- include/linux/usb/android.h
+++ include/linux/usb/android.h
@@ -17,11 +17,31 @@
 #ifndef	__LINUX_USB_ANDROID_H
 #define	__LINUX_USB_ANDROID_H
 
+#include <linux/usb/composite.h>
+
 struct android_usb_platform_data {
 	int (*update_pid_and_serial_num)(uint32_t, const char *);
 	u32 swfi_latency;
 	u8 usb_core_id;
 	bool cdrom;
 };
+
+#ifndef CONFIG_TARGET_CORE
+static inline int f_tcm_init(int (*connect_cb)(bool connect))
+{
+	/*
+	 * Fail bind() not init(). If a function init() returns error
+	 * android composite registration would fail.
+	 */
+	return 0;
+}
+static inline void f_tcm_exit(void)
+{
+}
+static inline int tcm_bind_config(struct usb_configuration *c)
+{
+	return -ENODEV;
+}
+#endif
 
 #endif	/* __LINUX_USB_ANDROID_H */
