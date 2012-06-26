61587baea5375cb97f220a2036fb95c5b4e2392f
Lucille Sylvester <lsylvest@codeaurora.org>
2012-06-26 17:55:10 -0600

    msm: kgsl: Fix the graphics bandwidth requests on 8064/8930
    Bandwidth requests were created to hit Apps Fabrics nominal and
    SVS voltages.  They need to be updated to also hit the right
    values for the MM Fabric.  This will allow the default power
    level to be at nominal voltage rather than turbo voltage.
    
    CRs-Fixed: 374734
    Change-Id: I80cebd64bf62d8a40d95242f5c187dd902bb0f6a
    Signed-off-by: Lucille Sylvester <lsylvest@codeaurora.org>

--- arch/arm/mach-msm/board-8064-gpu.c
+++ arch/arm/mach-msm/board-8064-gpu.c
@@ -97,13 +97,13 @@ static struct msm_bus_vectors grp3d_nominal_high_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(3200),
+		.ib = KGSL_CONVERT_TO_MBPS(2656),
 	},
 	{
 		.src = MSM_BUS_MASTER_GRAPHICS_3D_PORT1,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(3200),
+		.ib = KGSL_CONVERT_TO_MBPS(2656),
 	},
 };
 
