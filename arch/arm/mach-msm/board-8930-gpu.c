fa976e15125a2acaca42edac277c53cb6c3cec31
Lucille Sylvester <lsylvest@codeaurora.org>
2012-06-29 11:57:43 -0600

    msm: kgsl: Set the default 8930 graphics power level to nominal
    Save power by waking at nominal voltage, rather than turbo.
    
    Change-Id: Iea16f952809d39120463a9bc2791851c5b37d076
    Signed-off-by: Lucille Sylvester <lsylvest@codeaurora.org>

--- arch/arm/mach-msm/board-8930-gpu.c
+++ arch/arm/mach-msm/board-8930-gpu.c
@@ -135,7 +135,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.bus_freq = 0,
 		},
 	},
-	.init_level = 0,
+	.init_level = 1,
 	.num_levels = 4,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
