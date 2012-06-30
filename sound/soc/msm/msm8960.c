c7c9f10bbcea3fc40c60b13146f0d170d5c227b9
Jayasena Sangaraboina <jsanga@codeaurora.org>
2012-06-29 18:10:23 -0700

    ASoc: msm: Add fix for Unsupported Proxy backend sample rates.
    - AFE does not support sampling rate 44.1k
    - This fix addresses the issue by setting backend proxy device
      sampling rate to 48k.
    
    Change-Id: I4cd1ac6566d3230fa16fd70d99b8e758d8c606ad
    CRs-fixed: 374556
    Signed-off-by: Jayasena Sangaraboina <jsanga@codeaurora.org>

--- sound/soc/msm/msm8960.c
+++ sound/soc/msm/msm8960.c
@@ -1042,6 +1042,17 @@ static int msm8960_auxpcm_be_params_fixup(struct snd_soc_pcm_runtime *rtd,
 
 	return 0;
 }
+static int msm8960_proxy_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
+			struct snd_pcm_hw_params *params)
+{
+	struct snd_interval *rate = hw_param_interval(params,
+	SNDRV_PCM_HW_PARAM_RATE);
+
+	pr_debug("%s()\n", __func__);
+	rate->min = rate->max = 48000;
+
+	return 0;
+}
 static int msm8960_aux_pcm_get_gpios(void)
 {
 	int ret = 0;
@@ -1407,6 +1418,7 @@ static struct snd_soc_dai_link msm8960_dai_common[] = {
 		.codec_dai_name = "msm-stub-rx",
 		.no_pcm = 1,
 		.be_id = MSM_BACKEND_DAI_AFE_PCM_RX,
+		.be_hw_params_fixup = msm8960_proxy_be_hw_params_fixup,
 		.ignore_pmdown_time = 1, /* this dainlink has playback support */
 	},
 	{
@@ -1418,6 +1430,7 @@ static struct snd_soc_dai_link msm8960_dai_common[] = {
 		.codec_dai_name = "msm-stub-tx",
 		.no_pcm = 1,
 		.be_id = MSM_BACKEND_DAI_AFE_PCM_TX,
+		.be_hw_params_fixup = msm8960_proxy_be_hw_params_fixup,
 	},
 	/* AUX PCM Backend DAI Links */
 	{
