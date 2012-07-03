11052c7f3aeb1bd00b0cc5338142d1e6db633191
Aaron Lu <aaron.lu@amd.com>
2012-07-03 17:27:49 +0800

    mmc: sdhci: fix incorrect command used in tuning
    For SD hosts using retuning mode 1, when retuning timer expired, it will
    need to do retuning in sdhci_request before processing the actual
    request. But the retuning command is fixed: cmd19 for SD card and cmd21
    for eMMC card, so we can't use the original request's command to do the
    tuning.
    
    And since the tuning command depends on the card type attached to the
    host, we will need to know the card type to use the correct tuning
    command.
    
    Change-Id: Ibde71d8a8b4e0ff75456923c7c3d8579c04109b4
    Signed-off-by: Aaron Lu <aaron.lu@amd.com>
    Reviewed-by: Philip Rakity <prakity@marvell.com>
    Cc: stable <stable@vger.kernel.org> [3.3+]
    Signed-off-by: Chris Ball <cjb@laptop.org>
    Signed-off-by: Asutosh Das <asutoshd@codeaurora.org>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1498,6 +1498,7 @@ static void sdhci_request(struct mmc_host *mmc, struct mmc_request *mrq)
 	struct sdhci_host *host;
 	bool present;
 	unsigned long flags;
+	u32 tuning_opcode;
 
 	host = mmc_priv(mmc);
 
@@ -1554,8 +1555,12 @@ static void sdhci_request(struct mmc_host *mmc, struct mmc_request *mrq)
 		 */
 		if ((host->flags & SDHCI_NEEDS_RETUNING) &&
 		    !(present_state & (SDHCI_DOING_WRITE | SDHCI_DOING_READ))) {
+			/* eMMC uses cmd21 while sd and sdio use cmd19 */
+			tuning_opcode = mmc->card->type == MMC_TYPE_MMC ?
+				MMC_SEND_TUNING_BLOCK_HS200 :
+				MMC_SEND_TUNING_BLOCK;
 			spin_unlock_irqrestore(&host->lock, flags);
-			sdhci_execute_tuning(mmc, mrq->cmd->opcode);
+			sdhci_execute_tuning(mmc, tuning_opcode);
 			spin_lock_irqsave(&host->lock, flags);
 
 			/* Restore original mmc_request structure */
