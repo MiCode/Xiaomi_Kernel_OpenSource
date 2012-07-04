0c1b8b2174f85c4437a197c2e58350bd6f6c3d02
Mayank Rana <mrana@codeaurora.org>
2012-07-04 16:17:38 +0530

    USB: ci13xxx_udc: Flush ep0IN and ep0OUT endpoint from ep_dequeue
    Flush control endpoints from ep_dequeue and set request completion
    to NULL once calling completion handler of function driver. This will
    make sure that all control endpoint requests are flushed in disconnect.
    
    CRs-Fixed: 373672
    Change-Id: I7506c2e53d13f137ae6f222a06fbcc0dee77f68e
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>

--- drivers/usb/gadget/ci13xxx_udc.c
+++ drivers/usb/gadget/ci13xxx_udc.c
@@ -1957,6 +1957,8 @@ __acquires(mEp->lock)
 				mReq->req.length)
 				mEpTemp = &_udc->ep0in;
 			mReq->req.complete(&mEpTemp->ep, &mReq->req);
+			if (mEp->type == USB_ENDPOINT_XFER_CONTROL)
+				mReq->req.complete = NULL;
 			spin_lock(mEp->lock);
 		}
 	}
@@ -2803,7 +2805,12 @@ static int ep_dequeue(struct usb_ep *ep, struct usb_request *req)
 
 	dbg_event(_usb_addr(mEp), "DEQUEUE", 0);
 
-	hw_ep_flush(mEp->num, mEp->dir);
+	if ((mEp->type == USB_ENDPOINT_XFER_CONTROL)) {
+		hw_ep_flush(_udc->ep0out.num, RX);
+		hw_ep_flush(_udc->ep0in.num, TX);
+	} else {
+		hw_ep_flush(mEp->num, mEp->dir);
+	}
 
 	/* pop request */
 	list_del_init(&mReq->queue);
@@ -2821,6 +2828,8 @@ static int ep_dequeue(struct usb_ep *ep, struct usb_request *req)
 				mReq->req.length)
 			mEpTemp = &_udc->ep0in;
 		mReq->req.complete(&mEpTemp->ep, &mReq->req);
+		if (mEp->type == USB_ENDPOINT_XFER_CONTROL)
+			mReq->req.complete = NULL;
 		spin_lock(mEp->lock);
 	}
 
