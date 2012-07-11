531ac3d0852335d2d9d00e3073a9d14c32bd2053
Joonwoo Park <joonwoop@codeaurora.org>
2012-07-11 13:31:55 -0700

    ASoC: apq8064: Add unsupported headset detection support
    The wcd9310 codec driver already can detect and report unsupported headset
    plugging.  Create headset jack with unsupported headset mask to be able to
    report via sound core.
    
    Change-Id: I0119d01c039362cc7b185f9f3407d78c958bc49a
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- sound/soc/msm/apq8064.c
+++ sound/soc/msm/apq8064.c
@@ -1152,8 +1152,9 @@ static int msm_audrx_init(struct snd_soc_pcm_runtime *rtd)
 	snd_soc_dapm_sync(dapm);
 
 	err = snd_soc_jack_new(codec, "Headset Jack",
-		(SND_JACK_HEADSET | SND_JACK_OC_HPHL | SND_JACK_OC_HPHR),
-		&hs_jack);
+			       (SND_JACK_HEADSET | SND_JACK_OC_HPHL |
+				SND_JACK_OC_HPHR | SND_JACK_UNSUPPORTED),
+			       &hs_jack);
 	if (err) {
 		pr_err("failed to create new jack\n");
 		return err;
