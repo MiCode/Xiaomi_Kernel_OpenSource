e14d99147b7126d80258a5b05119d50ef277f761
Adrian Salido-Moreno <adrianm@codeaurora.org>
2012-07-12 20:16:14 -0700

    msm: mdss: refactor mdss color formats
    Add missing color formats and reorganize color formats to have better
    code reuse and make it easy to add new formats if required.
    
    kernel 3.14 upgrade conflicts:
    	include/uapi/linux/msm_mdp.h
    
    Change-Id: Ic7c6bb6f35a9c282d9ef99bd5e1d18b0b8033971
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_formats.h
+++ drivers/video/msm/mdss/mdss_mdp_formats.h
@@ -18,311 +18,147 @@
 
 #include "mdss_mdp.h"
 
-#define C3_ALPHA	3	/* alpha */
-#define C2_R_Cr		2	/* R/Cr */
-#define C1_B_Cb		1	/* B/Cb */
-#define C0_G_Y		0	/* G/luma */
+	/*
+	 * number of bits for source component,
+	 * 0 = 1 bit, 1 = 2 bits, 2 = 6 bits, 3 = 8 bits
+	 */
+enum {
+	COLOR_4BIT,
+	COLOR_5BIT,
+	COLOR_6BIT,
+	COLOR_8BIT,
+};
 
-static struct mdss_mdp_format_params mdss_mdp_format_map[MDP_IMGTYPE_LIMIT] = {
-	[MDP_RGB_565] = {
-		.format = MDP_RGB_565,
+#define FMT_RGB_565(fmt, e0, e1, e2)				\
+	{							\
+		.format = (fmt),				\
+		.fetch_planes = MDSS_MDP_PLANE_INTERLEAVED,	\
+		.unpack_tight = 1,				\
+		.unpack_align_msb = 0,				\
+		.alpha_enable = 0,				\
+		.unpack_count = 3,				\
+		.bpp = 2,					\
+		.element = { (e0), (e1), (e2) },		\
+		.bits = {					\
+			[C2_R_Cr] = COLOR_5BIT,			\
+			[C0_G_Y] = COLOR_6BIT,			\
+			[C1_B_Cb] = COLOR_5BIT,			\
+		},						\
+	}
+
+#define FMT_RGB_888(fmt, e0, e1, e2)				\
+	{							\
+		.format = (fmt),				\
+		.fetch_planes = MDSS_MDP_PLANE_INTERLEAVED,	\
+		.unpack_tight = 1,				\
+		.unpack_align_msb = 0,				\
+		.alpha_enable = 0,				\
+		.unpack_count = 3,				\
+		.bpp = 3,					\
+		.element = { (e0), (e1), (e2) },		\
+		.bits = {					\
+			[C2_R_Cr] = COLOR_8BIT,			\
+			[C0_G_Y] = COLOR_8BIT,			\
+			[C1_B_Cb] = COLOR_8BIT,			\
+		},						\
+	}
+
+#define FMT_RGB_8888(fmt, alpha_en, e0, e1, e2, e3)		\
+	{							\
+		.format = (fmt),				\
+		.fetch_planes = MDSS_MDP_PLANE_INTERLEAVED,	\
+		.unpack_tight = 1,				\
+		.unpack_align_msb = 0,				\
+		.alpha_enable = (alpha_en),			\
+		.unpack_count = 4,				\
+		.bpp = 4,					\
+		.element = { (e0), (e1), (e2), (e3) },		\
+		.bits = {					\
+			[C3_ALPHA] = COLOR_8BIT,		\
+			[C2_R_Cr] = COLOR_8BIT,			\
+			[C0_G_Y] = COLOR_8BIT,			\
+			[C1_B_Cb] = COLOR_8BIT,			\
+		},						\
+	}
+
+#define FMT_YUV_COMMON(fmt)					\
+		.format = (fmt),				\
+		.is_yuv = 1,					\
+		.bits = {					\
+			[C2_R_Cr] = COLOR_8BIT,			\
+			[C0_G_Y] = COLOR_8BIT,			\
+			[C1_B_Cb] = COLOR_8BIT,			\
+		},						\
+		.alpha_enable = 0,				\
+		.unpack_tight = 1,				\
+		.unpack_align_msb = 0
+
+#define FMT_YUV_PSEUDO(fmt, samp, e0, e1)			\
+	{							\
+		FMT_YUV_COMMON(fmt),				\
+		.fetch_planes = MDSS_MDP_PLANE_PSEUDO_PLANAR,	\
+		.chroma_sample = samp,				\
+		.unpack_count = 2,				\
+		.bpp = 2,					\
+		.element = { (e0), (e1) },			\
+	}
+
+#define FMT_YUV_PLANR(fmt, samp, e0, e1)			\
+	{							\
+		FMT_YUV_COMMON(fmt),				\
+		.fetch_planes = MDSS_MDP_PLANE_PLANAR,		\
+		.chroma_sample = samp,				\
+		.element = { (e0), (e1) }			\
+	}
+
+static struct mdss_mdp_format_params mdss_mdp_format_map[] = {
+	FMT_RGB_565(MDP_RGB_565, C1_B_Cb, C0_G_Y, C2_R_Cr),
+	FMT_RGB_565(MDP_BGR_565, C2_R_Cr, C0_G_Y, C1_B_Cb),
+	FMT_RGB_888(MDP_RGB_888, C2_R_Cr, C0_G_Y, C1_B_Cb),
+	FMT_RGB_888(MDP_BGR_888, C1_B_Cb, C0_G_Y, C2_R_Cr),
+
+	FMT_RGB_8888(MDP_XRGB_8888, 0, C3_ALPHA, C2_R_Cr, C0_G_Y, C1_B_Cb),
+	FMT_RGB_8888(MDP_ARGB_8888, 1, C3_ALPHA, C2_R_Cr, C0_G_Y, C1_B_Cb),
+	FMT_RGB_8888(MDP_RGBA_8888, 1, C2_R_Cr, C0_G_Y, C1_B_Cb, C3_ALPHA),
+	FMT_RGB_8888(MDP_RGBX_8888, 0, C2_R_Cr, C0_G_Y, C1_B_Cb, C3_ALPHA),
+	FMT_RGB_8888(MDP_BGRA_8888, 1, C1_B_Cb, C0_G_Y, C2_R_Cr, C3_ALPHA),
+
+	FMT_YUV_PSEUDO(MDP_Y_CRCB_H1V1, MDSS_MDP_CHROMA_RGB, C2_R_Cr, C1_B_Cb),
+	FMT_YUV_PSEUDO(MDP_Y_CBCR_H1V1, MDSS_MDP_CHROMA_RGB, C1_B_Cb, C2_R_Cr),
+	FMT_YUV_PSEUDO(MDP_Y_CRCB_H2V1, MDSS_MDP_CHROMA_H2V1, C2_R_Cr, C1_B_Cb),
+	FMT_YUV_PSEUDO(MDP_Y_CBCR_H2V1, MDSS_MDP_CHROMA_H2V1, C1_B_Cb, C2_R_Cr),
+	FMT_YUV_PSEUDO(MDP_Y_CRCB_H1V2, MDSS_MDP_CHROMA_H1V2, C2_R_Cr, C1_B_Cb),
+	FMT_YUV_PSEUDO(MDP_Y_CBCR_H1V2, MDSS_MDP_CHROMA_H1V2, C1_B_Cb, C2_R_Cr),
+	FMT_YUV_PSEUDO(MDP_Y_CRCB_H2V2, MDSS_MDP_CHROMA_420, C2_R_Cr, C1_B_Cb),
+	FMT_YUV_PSEUDO(MDP_Y_CBCR_H2V2, MDSS_MDP_CHROMA_420, C1_B_Cb, C2_R_Cr),
+
+	FMT_YUV_PLANR(MDP_Y_CR_CB_H2V2, MDSS_MDP_CHROMA_420, C2_R_Cr, C1_B_Cb),
+	FMT_YUV_PLANR(MDP_Y_CB_CR_H2V2, MDSS_MDP_CHROMA_420, C1_B_Cb, C2_R_Cr),
+	FMT_YUV_PLANR(MDP_Y_CR_CB_GH2V2, MDSS_MDP_CHROMA_420, C2_R_Cr, C1_B_Cb),
+
+	{
+		FMT_YUV_COMMON(MDP_YCBCR_H1V1),
 		.fetch_planes = MDSS_MDP_PLANE_INTERLEAVED,
-		.a_bit = 0,
-		.r_bit = 1,	/* R, 5 bits */
-		.b_bit = 1,	/* B, 5 bits */
-		.g_bit = 2,	/* G, 6 bits */
-		.alpha_enable = 0,
-		.unpack_tight = 1,
-		.unpack_align_msb = 0,
-		.unpack_count = 2,
-		.element2 = C2_R_Cr,
-		.element1 = C0_G_Y,
-		.element0 = C1_B_Cb,
-		.bpp = 1,	/* 2 bpp */
-	},
-	[MDP_BGR_565] = {
-		.format = MDP_BGR_565,
-		.fetch_planes = MDSS_MDP_PLANE_INTERLEAVED,
-		.a_bit = 0,
-		.r_bit = 1,	/* R, 5 bits */
-		.b_bit = 1,	/* B, 5 bits */
-		.g_bit = 2,	/* G, 6 bits */
-		.alpha_enable = 0,
-		.unpack_tight = 1,
-		.unpack_align_msb = 0,
-		.unpack_count = 2,
-		.element2 = C1_B_Cb,
-		.element1 = C0_G_Y,
-		.element0 = C2_R_Cr,
-		.bpp = 1,	/* 2 bpp */
-	},
-	[MDP_RGB_888] = {
-		.format = MDP_RGB_888,
-		.fetch_planes = MDSS_MDP_PLANE_INTERLEAVED,
-		.a_bit = 0,
-		.r_bit = 3,	/* R, 8 bits */
-		.b_bit = 3,	/* B, 8 bits */
-		.g_bit = 3,	/* G, 8 bits */
-		.alpha_enable = 0,
-		.unpack_tight = 1,
-		.unpack_align_msb = 0,
-		.unpack_count = 2,
-		.element2 = C1_B_Cb,
-		.element1 = C0_G_Y,
-		.element0 = C2_R_Cr,
-		.bpp = 2,	/* 3 bpp */
-	},
-	[MDP_XRGB_8888] = {
-		.format = MDP_XRGB_8888,
-		.fetch_planes = MDSS_MDP_PLANE_INTERLEAVED,
-		.a_bit = 3,	/* alpha, 4 bits */
-		.r_bit = 3,	/* R, 8 bits */
-		.b_bit = 3,	/* B, 8 bits */
-		.g_bit = 3,	/* G, 8 bits */
-		.alpha_enable = 0,
-		.unpack_tight = 1,
-		.unpack_align_msb = 0,
+		.chroma_sample = MDSS_MDP_CHROMA_RGB,
 		.unpack_count = 3,
-		.element3 = C1_B_Cb,
-		.element2 = C0_G_Y,
-		.element1 = C2_R_Cr,
-		.element0 = C3_ALPHA,
-		.bpp = 3,	/* 4 bpp */
+		.bpp = 3,
+		.element = { C2_R_Cr, C1_B_Cb, C0_G_Y },
 	},
-	[MDP_ARGB_8888] = {
-		.format = MDP_ARGB_8888,
+	{
+		FMT_YUV_COMMON(MDP_YCRCB_H1V1),
 		.fetch_planes = MDSS_MDP_PLANE_INTERLEAVED,
-		.a_bit = 3,	/* alpha, 4 bits */
-		.r_bit = 3,	/* R, 8 bits */
-		.b_bit = 3,	/* B, 8 bits */
-		.g_bit = 3,	/* G, 8 bits */
-		.alpha_enable = 1,
-		.unpack_tight = 1,
-		.unpack_align_msb = 0,
+		.chroma_sample = MDSS_MDP_CHROMA_RGB,
 		.unpack_count = 3,
-		.element3 = C1_B_Cb,
-		.element2 = C0_G_Y,
-		.element1 = C2_R_Cr,
-		.element0 = C3_ALPHA,
-		.bpp = 3,	/* 4 bpp */
+		.bpp = 3,
+		.element = { C1_B_Cb, C2_R_Cr, C0_G_Y },
 	},
-	[MDP_RGBA_8888] = {
-		.format = MDP_RGBA_8888,
-		.fetch_planes = MDSS_MDP_PLANE_INTERLEAVED,
-		.a_bit = 3,	/* alpha, 4 bits */
-		.r_bit = 3,	/* R, 8 bits */
-		.b_bit = 3,	/* B, 8 bits */
-		.g_bit = 3,	/* G, 8 bits */
-		.alpha_enable = 1,
-		.unpack_tight = 1,
-		.unpack_align_msb = 0,
-		.unpack_count = 3,
-		.element3 = C3_ALPHA,
-		.element2 = C1_B_Cb,
-		.element1 = C0_G_Y,
-		.element0 = C2_R_Cr,
-		.bpp = 3,	/* 4 bpp */
-	},
-	[MDP_RGBX_8888] = {
-		.format = MDP_RGBX_8888,
-		.fetch_planes = MDSS_MDP_PLANE_INTERLEAVED,
-		.a_bit = 3,
-		.r_bit = 3,	/* R, 8 bits */
-		.b_bit = 3,	/* B, 8 bits */
-		.g_bit = 3,	/* G, 8 bits */
-		.alpha_enable = 0,
-		.unpack_tight = 1,
-		.unpack_align_msb = 0,
-		.unpack_count = 3,
-		.element3 = C3_ALPHA,
-		.element2 = C1_B_Cb,
-		.element1 = C0_G_Y,
-		.element0 = C2_R_Cr,
-		.bpp = 3,	/* 4 bpp */
-	},
-	[MDP_BGRA_8888] = {
-		.format = MDP_BGRA_8888,
-		.fetch_planes = MDSS_MDP_PLANE_INTERLEAVED,
-		.a_bit = 3,	/* alpha, 4 bits */
-		.r_bit = 3,	/* R, 8 bits */
-		.b_bit = 3,	/* B, 8 bits */
-		.g_bit = 3,	/* G, 8 bits */
-		.alpha_enable = 1,
-		.unpack_tight = 1,
-		.unpack_align_msb = 0,
-		.unpack_count = 3,
-		.element3 = C3_ALPHA,
-		.element2 = C2_R_Cr,
-		.element1 = C0_G_Y,
-		.element0 = C1_B_Cb,
-		.bpp = 3,	/* 4 bpp */
-	},
-	[MDP_YCRYCB_H2V1] = {
-		.format = MDP_YCRYCB_H2V1,
-		.is_yuv = 1,
-		.a_bit = 0,
-		.r_bit = 3,	/* R, 8 bits */
-		.b_bit = 3,	/* B, 8 bits */
-		.g_bit = 3,	/* G, 8 bits */
-		.alpha_enable = 0,
-		.unpack_tight = 1,
-		.unpack_align_msb = 0,
-		.unpack_count = 1,	/* 2 */
-		.bpp = 1,	/* 2 bpp */
+	{
+		FMT_YUV_COMMON(MDP_YCRYCB_H2V1),
 		.fetch_planes = MDSS_MDP_PLANE_INTERLEAVED,
 		.chroma_sample = MDSS_MDP_CHROMA_H2V1,
-		.unpack_count = 3,
-		.element3 = C0_G_Y,
-		.element2 = C2_R_Cr,
-		.element1 = C0_G_Y,
-		.element0 = C1_B_Cb,
-	},
-	[MDP_Y_CRCB_H2V1] = {
-		.format = MDP_Y_CRCB_H2V1,
-		.is_yuv = 1,
-		.a_bit = 0,
-		.r_bit = 3,	/* R, 8 bits */
-		.b_bit = 3,	/* B, 8 bits */
-		.g_bit = 3,	/* G, 8 bits */
-		.alpha_enable = 0,
-		.unpack_tight = 1,
-		.unpack_align_msb = 0,
-		.unpack_count = 1,	/* 2 */
-		.bpp = 1,	/* 2 bpp */
-		.fetch_planes = MDSS_MDP_PLANE_PSEUDO_PLANAR,
-		.chroma_sample = MDSS_MDP_CHROMA_H2V1,
-		.element1 = C1_B_Cb,
-		.element0 = C2_R_Cr,
-	},
-	[MDP_Y_CBCR_H2V1] = {
-		.format = MDP_Y_CBCR_H2V1,
-		.is_yuv = 1,
-		.a_bit = 0,
-		.r_bit = 3,	/* R, 8 bits */
-		.b_bit = 3,	/* B, 8 bits */
-		.g_bit = 3,	/* G, 8 bits */
-		.alpha_enable = 0,
-		.unpack_tight = 1,
-		.unpack_align_msb = 0,
-		.unpack_count = 1,	/* 2 */
-		.bpp = 1,	/* 2 bpp */
-		.fetch_planes = MDSS_MDP_PLANE_PSEUDO_PLANAR,
-		.chroma_sample = MDSS_MDP_CHROMA_H2V1,
-		.element1 = C2_R_Cr,
-		.element0 = C1_B_Cb,
-	},
-	[MDP_Y_CRCB_H1V2] = {
-		.format = MDP_Y_CRCB_H1V2,
-		.is_yuv = 1,
-		.a_bit = 0,
-		.r_bit = 3,	/* R, 8 bits */
-		.b_bit = 3,	/* B, 8 bits */
-		.g_bit = 3,	/* G, 8 bits */
-		.alpha_enable = 0,
-		.unpack_tight = 1,
-		.unpack_align_msb = 0,
-		.unpack_count = 1,	/* 2 */
-		.bpp = 1,	/* 2 bpp */
-		.fetch_planes = MDSS_MDP_PLANE_PSEUDO_PLANAR,
-		.chroma_sample = MDSS_MDP_CHROMA_H1V2,
-		.element1 = C1_B_Cb,
-		.element0 = C2_R_Cr,
-	},
-	[MDP_Y_CBCR_H1V2] = {
-		.format = MDP_Y_CBCR_H1V2,
-		.is_yuv = 1,
-		.a_bit = 0,
-		.r_bit = 3,	/* R, 8 bits */
-		.b_bit = 3,	/* B, 8 bits */
-		.g_bit = 3,	/* G, 8 bits */
-		.alpha_enable = 0,
-		.unpack_tight = 1,
-		.unpack_align_msb = 0,
-		.unpack_count = 1,	/* 2 */
-		.bpp = 1,	/* 2 bpp */
-		.fetch_planes = MDSS_MDP_PLANE_PSEUDO_PLANAR,
-		.chroma_sample = MDSS_MDP_CHROMA_H1V2,
-		.element1 = C2_R_Cr,
-		.element0 = C1_B_Cb,
-	},
-	[MDP_Y_CRCB_H2V2] = {
-		.format = MDP_Y_CRCB_H2V2,
-		.is_yuv = 1,
-		.a_bit = 0,
-		.r_bit = 3,	/* R, 8 bits */
-		.b_bit = 3,	/* B, 8 bits */
-		.g_bit = 3,	/* G, 8 bits */
-		.alpha_enable = 0,
-		.unpack_tight = 1,
-		.unpack_align_msb = 0,
-		.unpack_count = 1,	/* 2 */
-		.bpp = 1,	/* 2 bpp */
-		.fetch_planes = MDSS_MDP_PLANE_PSEUDO_PLANAR,
-		.chroma_sample = MDSS_MDP_CHROMA_420,
-		.element1 = C1_B_Cb,
-		.element0 = C2_R_Cr,
-	},
-	[MDP_Y_CBCR_H2V2] = {
-		.format = MDP_Y_CBCR_H2V2,
-		.is_yuv = 1,
-		.a_bit = 0,
-		.r_bit = 3,	/* R, 8 bits */
-		.b_bit = 3,	/* B, 8 bits */
-		.g_bit = 3,	/* G, 8 bits */
-		.alpha_enable = 0,
-		.unpack_tight = 1,
-		.unpack_align_msb = 0,
-		.unpack_count = 1,	/* 2 */
-		.bpp = 1,	/* 2 bpp */
-		.fetch_planes = MDSS_MDP_PLANE_PSEUDO_PLANAR,
-		.chroma_sample = MDSS_MDP_CHROMA_420,
-		.element1 = C2_R_Cr,
-		.element0 = C1_B_Cb,
-	},
-	[MDP_Y_CR_CB_H2V2] = {
-		.format = MDP_Y_CR_CB_H2V2,
-		.is_yuv = 1,
-		.a_bit = 0,
-		.r_bit = 3,	/* R, 8 bits */
-		.b_bit = 3,	/* B, 8 bits */
-		.g_bit = 3,	/* G, 8 bits */
-		.alpha_enable = 0,
-		.unpack_tight = 1,
-		.unpack_align_msb = 0,
-		.unpack_count = 1,	/* 2 */
-		.bpp = 1,	/* 2 bpp */
-		.fetch_planes = MDSS_MDP_PLANE_PLANAR,
-		.chroma_sample = MDSS_MDP_CHROMA_420,
-	},
-	[MDP_Y_CB_CR_H2V2] = {
-		.format = MDP_Y_CB_CR_H2V2,
-		.is_yuv = 1,
-		.a_bit = 0,
-		.r_bit = 3,	/* R, 8 bits */
-		.b_bit = 3,	/* B, 8 bits */
-		.g_bit = 3,	/* G, 8 bits */
-		.alpha_enable = 0,
-		.unpack_tight = 1,
-		.unpack_align_msb = 0,
-		.unpack_count = 1,	/* 2 */
-		.bpp = 1,	/* 2 bpp */
-		.fetch_planes = MDSS_MDP_PLANE_PLANAR,
-		.chroma_sample = MDSS_MDP_CHROMA_420,
-	},
-	[MDP_Y_CR_CB_GH2V2] = {
-		.format = MDP_Y_CR_CB_GH2V2,
-		.is_yuv = 1,
-		.a_bit = 0,
-		.r_bit = 3,	/* R, 8 bits */
-		.b_bit = 3,	/* B, 8 bits */
-		.g_bit = 3,	/* G, 8 bits */
-		.alpha_enable = 0,
-		.unpack_tight = 1,
-		.unpack_align_msb = 0,
-		.unpack_count = 1,	/* 2 */
-		.bpp = 1,	/* 2 bpp */
-		.fetch_planes = MDSS_MDP_PLANE_PLANAR,
-		.chroma_sample = MDSS_MDP_CHROMA_420,
+		.unpack_count = 4,
+		.bpp = 2,
+		.element = { C1_B_Cb, C0_G_Y, C2_R_Cr, C0_G_Y },
 	},
 };
 #endif
