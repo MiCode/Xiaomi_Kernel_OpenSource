f81247d20b522dbdb12322f1d66881aac4f1d18c
Pravin Tamkhane <pravint@codeaurora.org>
2012-07-24 13:34:23 -0700

    video: msm_fb: Increase display timeout to 2 seconds
    One second time out for no frame update, as in current implementation,
    does not allow CABL to reach minimum backlight level possible because
    histograms dont get generated after one second. So increase time out
    period to two seconds which seem to be sufficient for complete black to
    complete white and vice versa transitions.
    
    CRs-Fixed: 370804
    Change-Id: I0712924d669abcb820654cd6ca5f7da37bed57e6
    Signed-off-by: Pravin Tamkhane <pravint@codeaurora.org>
    Signed-off-by: Carl Vanderlip <carlv@codeaurora.org>

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -1729,8 +1729,7 @@ static int msm_fb_pan_display(struct fb_var_screeninfo *var,
 	if (mfd->msmfb_no_update_notify_timer.function)
 		del_timer(&mfd->msmfb_no_update_notify_timer);
 
-	mfd->msmfb_no_update_notify_timer.expires =
-				jiffies + ((1000 * HZ) / 1000);
+	mfd->msmfb_no_update_notify_timer.expires = jiffies + (2 * HZ);
 	add_timer(&mfd->msmfb_no_update_notify_timer);
 	mutex_unlock(&msm_fb_notify_update_sem);
 
@@ -2918,8 +2917,7 @@ static int msmfb_overlay_play(struct fb_info *info, unsigned long *argp)
 	if (mfd->msmfb_no_update_notify_timer.function)
 		del_timer(&mfd->msmfb_no_update_notify_timer);
 
-	mfd->msmfb_no_update_notify_timer.expires =
-				jiffies + ((1000 * HZ) / 1000);
+	mfd->msmfb_no_update_notify_timer.expires = jiffies + (2 * HZ);
 	add_timer(&mfd->msmfb_no_update_notify_timer);
 	mutex_unlock(&msm_fb_notify_update_sem);
 
