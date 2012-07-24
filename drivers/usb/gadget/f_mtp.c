7c3c45b9a4b51f422db4434c87e7951102ec2841
Rajkumar Raghupathy <raghup@codeaurora.org>
2012-07-24 16:13:36 +0530

    USB: msm72k_udc: Check if the ep is not disabled before queuing it
    Queuing a request on a disabled endpoint during composition switch leads
    to prime failure. Hence return -EINVAL if a request is queued on a disabled
    endpoint.
    
    Also, in f_mtp, block queuing a request in OUT ep in receive_file_work, if
    the device state is STATE_OFFLINE.
    
    Change-Id: I0e706d5280a2460baf6ab05dbf97a09c59b642fb
    CRs-Fixed: 378207
    Signed-off-by: Rajkumar Raghupathy <raghup@codeaurora.org>

--- drivers/usb/gadget/f_mtp.c
+++ drivers/usb/gadget/f_mtp.c
@@ -788,7 +788,8 @@ static void receive_file_work(struct work_struct *data)
 			/* wait for our last read to complete */
 			ret = wait_event_interruptible(dev->read_wq,
 				dev->rx_done || dev->state != STATE_BUSY);
-			if (dev->state == STATE_CANCELED) {
+			if (dev->state == STATE_CANCELED
+					|| dev->state == STATE_OFFLINE) {
 				r = -ECANCELED;
 				if (!dev->rx_done)
 					usb_ep_dequeue(dev->ep_out, read_req);
