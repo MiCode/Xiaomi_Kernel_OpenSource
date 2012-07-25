f00c59aabbe712113431cd3afaf116066b1524c7
Laxminath Kasam <lkasam@codeaurora.org>
2012-07-25 16:48:15 +0530

    msm: adsp: qdsp5: Restrict jpeg and video concurrency
    - DSP crashes if JPEG and VIDEO modules are enabled
    and used simultaneously.
    - DSP has limitation in supporting both VIDEOTASK and
    JPEGTASK since both use same shared memory.
    - Restrict enabling the VIDEOTASK module if JPEGTASK
    module is already enabled in adsp driver. Similarly
    restrict enabling the JPEGTASK module if VIDEOTASK
    is already enabled.
    
    CRs-Fixed: 382295
    Change-Id: I609f66880e097b8b71862c79ff5f23e2696b750b
    Signed-off-by: Laxminath Kasam <lkasam@codeaurora.org>

--- arch/arm/mach-msm/qdsp5/adsp.c
+++ arch/arm/mach-msm/qdsp5/adsp.c
@@ -1072,12 +1072,28 @@ int msm_adsp_generate_event(void *data,
 int msm_adsp_enable(struct msm_adsp_module *module)
 {
 	int rc = 0;
+	struct msm_adsp_module *module_en = NULL;
 
 	if (!module)
 		return -EINVAL;
 
 	MM_INFO("enable '%s'state[%d] id[%d]\n",
 				module->name, module->state, module->id);
+	if (!strncmp(module->name, "JPEGTASK", sizeof(module->name)))
+		module_en = find_adsp_module_by_name(&adsp_info, "VIDEOTASK");
+	else if (!strncmp(module->name, "VIDEOTASK", sizeof(module->name)))
+		module_en = find_adsp_module_by_name(&adsp_info, "JPEGTASK");
+	if (module_en) {
+		mutex_lock(&module_en->lock);
+		if (module_en->state == ADSP_STATE_ENABLED ||
+			module_en->state == ADSP_STATE_ENABLING) {
+			MM_ERR("both jpeg and video module can't"\
+				" exist at a time\n");
+			mutex_unlock(&module_en->lock);
+			return -EINVAL;
+		}
+		mutex_unlock(&module_en->lock);
+	}
 
 	mutex_lock(&module->lock);
 	switch (module->state) {
