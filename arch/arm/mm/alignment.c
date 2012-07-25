7d1716597cc9049da73ece31c67f2c1e6879ee6d
Rohit Vaswani <rvaswani@codeaurora.org>
2012-07-25 11:32:37 -0700

    arm: Fix compilation error with gcc 4.5.2
    Fix warning with the LDM/STM instructions. The instructions dont
    have a register offset so set the offset to 0.
    
    Change-Id: I2c988373c88e280015faa43076139650747d7ff3
    Acked-by: Kaushik Sikdar <ksikdar@qualcomm.com>
    Signed-off-by: Rohit Vaswani <rvaswani@codeaurora.org>

--- arch/arm/mm/alignment.c
+++ arch/arm/mm/alignment.c
@@ -856,8 +856,10 @@ do_alignment(unsigned long addr, unsigned int fsr, struct pt_regs *regs)
 	case 0x08000000:	/* ldm or stm, or thumb-2 32bit instruction */
 		if (thumb2_32b)
 			handler = do_alignment_t32_to_handler(&instr, regs, &offset);
-		else
+		else {
 			handler = do_alignment_ldmstm;
+			offset.un = 0;
+		}
 		break;
 
 	default:
