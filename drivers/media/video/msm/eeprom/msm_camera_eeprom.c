ae5f5e800c0db491f7e96ba1f8de42a206c4dc29
Jeyaprakash Soundrapandian <jsoundra@codeaurora.org>
2012-07-25 20:52:49 -0700

    msm: camera: eeprom support flag moved to right place
    The eeprom support is set to true and should be populated
    and passed in the eprom getinfo
    
    CRs-Fixed: 383179
    Change-Id: I2b67247b6175978eef6effec42b6995e1a2458d6
    Signed-off-by: Jeyaprakash Soundrapandian <jsoundra@codeaurora.org>

--- drivers/media/video/msm/eeprom/msm_camera_eeprom.c
+++ drivers/media/video/msm/eeprom/msm_camera_eeprom.c
@@ -100,7 +100,7 @@ int32_t msm_eeprom_config(struct msm_eeprom_ctrl_t *e_ctrl,
 		}
 		rc = e_ctrl->func_tbl.eeprom_get_info(e_ctrl,
 			&cdata.cfg.get_info);
-
+		cdata.is_eeprom_supported = 1;
 		if (copy_to_user((void *)argp,
 			&cdata,
 			sizeof(struct msm_eeprom_cfg_data)))
