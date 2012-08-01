e541526f3b35b02b25831c14cbc4f43baf6310df
Praneeth Paladugu <ppaladug@codeaurora.org>
2012-08-01 16:21:08 -0700

    msm: vidc: Adds support to send new command
    - Adds supports to send DECODER command to
    decoder which inturn used to CLOSE the instance.
    - Adds supports to send ENCODER command to
    encoder which inturn used to CLOSE the instance.
    
    Change-Id: Ia6037fdf66824538124f775442150b7a4ed201b8
    Signed-off-by: Praneeth Paladugu <ppaladug@codeaurora.org>

--- drivers/media/video/msm_vidc/msm_vidc.c
+++ drivers/media/video/msm_vidc/msm_vidc.c
@@ -29,11 +29,6 @@ int msm_vidc_poll(void *instance, struct file *filp,
 	struct vb2_buffer *out_vb = NULL;
 	struct vb2_buffer *cap_vb = NULL;
 	unsigned long flags;
-	if (!outq->streaming && !capq->streaming) {
-		pr_err("Returning POLLERR from here: %d, %d\n",
-			outq->streaming, capq->streaming);
-		return POLLERR;
-	}
 	poll_wait(filp, &inst->event_handler.wait, wait);
 	poll_wait(filp, &capq->done_wq, wait);
 	poll_wait(filp, &outq->done_wq, wait);
@@ -137,6 +132,22 @@ int msm_vidc_release_buf(void *instance, struct v4l2_buffer *b)
 	struct msm_vidc_inst *inst = instance;
 	if (inst->session_type == MSM_VIDC_DECODER)
 		return msm_vdec_release_buf(instance, b);
+	return -EINVAL;
+}
+
+int msm_vidc_encoder_cmd(void *instance, struct v4l2_encoder_cmd *enc)
+{
+	struct msm_vidc_inst *inst = instance;
+	if (inst->session_type == MSM_VIDC_ENCODER)
+		return msm_venc_cmd(instance, enc);
+	return -EINVAL;
+}
+
+int msm_vidc_decoder_cmd(void *instance, struct v4l2_decoder_cmd *dec)
+{
+	struct msm_vidc_inst *inst = instance;
+	if (inst->session_type == MSM_VIDC_DECODER)
+		return msm_vdec_cmd(instance, dec);
 	return -EINVAL;
 }
 
