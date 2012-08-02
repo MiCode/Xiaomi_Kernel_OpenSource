506a5ad882538071148bc2259bdb1d07964dffdc
Sravan Kumar D.V.N <sravank1@codeaurora.org>
2012-08-02 19:08:30 +0530

    msm: board-7627a: Reduce mdp memory size
    Reduce pmem mdp size to 27MB for 7x27a/8x25 targets.
    
    Change-Id: Idefb7ac7c0f777f08c621d42ca2f52e1b7ce3264
    Signed-off-by: Sravan Kumar D.V.N <sravank1@codeaurora.org>

--- arch/arm/mach-msm/board-qrd7627a.c
+++ arch/arm/mach-msm/board-qrd7627a.c
@@ -128,7 +128,7 @@ static struct msm_i2c_platform_data msm_gsbi1_qup_i2c_pdata = {
 };
 
 #ifdef CONFIG_ARCH_MSM7X27A
-#define MSM_PMEM_MDP_SIZE       0x2300000
+#define MSM_PMEM_MDP_SIZE       0x1B00000
 #define MSM_PMEM_ADSP_SIZE      0x1200000
 
 #define MSM_ION_AUDIO_SIZE	(MSM_PMEM_AUDIO_SIZE + PMEM_KERNEL_EBI1_SIZE)
