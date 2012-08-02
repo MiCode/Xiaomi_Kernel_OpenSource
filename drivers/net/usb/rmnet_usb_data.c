9d36704dd89f78bf822e975fb1817608c1e3b42b
Jack Pham <jackp@codeaurora.org>
2012-08-02 02:33:47 -0700

    net: rmnet_usb: Enable remote wakeup for root hub
    A recent change in the kernel disabled USB root hubs' wakeup
    capability by default. This forces the setting for this particular
    device/hub combination to always enable remote wakeup.
    
    Change-Id: If7ef1e731e71feb9730a67b2d912f03890e7210d
    Signed-off-by: Jack Pham <jackp@codeaurora.org>

--- drivers/net/usb/rmnet_usb_data.c
+++ drivers/net/usb/rmnet_usb_data.c
@@ -560,11 +560,12 @@ static int rmnet_usb_probe(struct usb_interface *iface,
 
 	usb_enable_autosuspend(udev);
 
-	/* allow modem to wake up suspended system */
-	device_set_wakeup_enable(&udev->dev, 1);
-
-	/* set default autosuspend timeout for modem and roothub */
 	if (udev->parent && !udev->parent->parent) {
+		/* allow modem and roothub to wake up suspended system */
+		device_set_wakeup_enable(&udev->dev, 1);
+		device_set_wakeup_enable(&udev->parent->dev, 1);
+
+		/* set default autosuspend timeout for modem and roothub */
 		pm_runtime_set_autosuspend_delay(&udev->dev, 1000);
 		pm_runtime_set_autosuspend_delay(&udev->parent->dev, 200);
 	}
