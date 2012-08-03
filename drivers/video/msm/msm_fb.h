d635cad054f3860f8ca13a8cf79f3e46f8892ce6
Pradeep Jilagam <pjilagam@codeaurora.org>
2012-08-03 15:52:06 +0530

    msm_fb: display: Copy splash image to kernel memory
    The bootloader memory from which the splashscreen is shown
    gets reclaimed before frame update happens from framework.
    Hence, copy the splash image to a local buffer as soon as
    display is initialized. This will ensure proper continuous
    splashscreen functionality.
    
    CRs-Fixed: 378721
    Change-Id: I55a7b97eeced56d2f108d34998d58966610c4305
    Signed-off-by: Pradeep Jilagam <pjilagam@codeaurora.org>

--- drivers/video/msm/msm_fb.h
+++ drivers/video/msm/msm_fb.h
@@ -187,6 +187,8 @@ struct msm_fb_data_type {
 	u32 writeback_state;
 	bool writeback_active_cnt;
 	int cont_splash_done;
+	void *copy_splash_buf;
+	unsigned char *copy_splash_phys;
 };
 
 struct dentry *msm_fb_get_debugfs_root(void);
