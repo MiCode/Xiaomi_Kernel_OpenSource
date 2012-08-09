5b2d8e8a7735c1008bfafd181878d9a86cd18c26
Aviral Gupta <aviralg@codeaurora.org>
2012-08-09 17:00:30 +0530

    ASoC : msm: HDMI 5.1 LPCM playback support
    Support 6 channels configuration in HDMI driver
    and the ALSA framework to play 5.1 LPCM format
    
    CRs-Fixed: 370096
    Change-Id: I9f132b7c3028a58baaeac832fcbd5dfb14e274e9
    Signed-off-by: Aviral Gupta <aviralg@codeaurora.org>

--- sound/soc/msm/mpq8064.c
+++ sound/soc/msm/mpq8064.c
@@ -133,7 +133,7 @@ static int msm_ext_bottom_spk_pamp;
 static int msm_ext_top_spk_pamp;
 static int msm_slim_0_rx_ch = 1;
 static int msm_slim_0_tx_ch = 1;
-static int msm_hdmi_rx_ch = 2;
+static int msm_hdmi_rx_ch = 8;
 
 static struct clk *codec_clk;
 static int clk_users;
@@ -515,14 +515,15 @@ static const struct snd_soc_dapm_route common_audio_map[] = {
 static const char *spk_function[] = {"Off", "On"};
 static const char *slim0_rx_ch_text[] = {"One", "Two"};
 static const char *slim0_tx_ch_text[] = {"One", "Two", "Three", "Four"};
-static const char *hdmi_rx_ch_text[] = {"Two", "Three", "Four", "Five", "Six"};
+static const char * const hdmi_rx_ch_text[] = {"Two", "Three", "Four",
+					"Five", "Six", "Seven", "Eight"};
 
 
 static const struct soc_enum msm_enum[] = {
 	SOC_ENUM_SINGLE_EXT(2, spk_function),
 	SOC_ENUM_SINGLE_EXT(2, slim0_rx_ch_text),
 	SOC_ENUM_SINGLE_EXT(4, slim0_tx_ch_text),
-	SOC_ENUM_SINGLE_EXT(5, hdmi_rx_ch_text),
+	SOC_ENUM_SINGLE_EXT(7, hdmi_rx_ch_text),
 
 };
 
