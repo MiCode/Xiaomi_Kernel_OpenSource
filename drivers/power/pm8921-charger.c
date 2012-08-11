0a6ff48f233de8cb00000879ed05985cf887470c
David Keitel <dkeitel@codeaurora.org>
2012-08-10 17:14:50 -0700

    power: pm8921-charger: PMIC version check for workarounds
    Since the revision registers across different PMIC chips
    may match up some workarounds may be applied to chips
    that do not require them and cause issues.
    
    Fix this by adding version checks to these workarounds
    to make sure that the workarounds are applied to the correct
    PMIC version.
    
    Change-Id: I8af031570466a88656c89ee7764f78c5ec14d247
    Signed-off-by: David Keitel <dkeitel@codeaurora.org>

--- drivers/power/pm8921-charger.c
+++ drivers/power/pm8921-charger.c
@@ -3698,7 +3698,8 @@ static int __devinit pm8921_chg_hw_init(struct pm8921_chg_chip *chip)
 	}
 
 	/* Workarounds for die 3.0 */
-	if (pm8xxx_get_revision(chip->dev->parent) == PM8XXX_REVISION_8921_3p0)
+	if (pm8xxx_get_revision(chip->dev->parent) == PM8XXX_REVISION_8921_3p0
+	&& pm8xxx_get_version(chip->dev->parent) == PM8XXX_VERSION_8921)
 		pm8xxx_writeb(chip->dev->parent, CHG_BUCK_CTRL_TEST3, 0xAC);
 
 	/* Enable isub_fine resolution AICL for PM8917 */
