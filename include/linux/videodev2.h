f4bd6a3fbdb5d70b7000cdceb811557876bb7953
Praneeth Paladugu <ppaladug@codeaurora.org>
2012-08-12 01:37:41 -0700

    v4l2: Adds support for codec config
    Adds support for codec config flag to be passed
    to driver. This flag indicates that the buffer
    associated contains codec specific data.
    
    Change-Id: I80cfedc02dfc1efc8727f97e90ac2100e47d2f2a
    Signed-off-by: Praneeth Paladugu <ppaladug@codeaurora.org>

--- include/linux/videodev2.h
+++ include/linux/videodev2.h
@@ -2088,6 +2088,8 @@ struct v4l2_encoder_cmd {
 	};
 };
 
+#define V4L2_QCOM_BUF_FLAG_CODECCONFIG	0x4000
+
 /* Decoder commands */
 #define V4L2_DEC_CMD_START       (0)
 #define V4L2_DEC_CMD_STOP        (1)
