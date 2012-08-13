211f6a059df0e7c2ace678352f3abff7de0a4f37
Carter Cooper <ccooper@codeaurora.org>
2012-08-13 14:44:47 -0600

    msm: kgsl: Update log messages for usability
    Many of the log messages were outdated and did not tell enough
    information about what they were logging.  Update the content
    and/or remove log messages.
    
    Change-Id: Ica21510150be034bb3551ef0279d3afa91e559ea
    Signed-off-by: Carter Cooper <ccooper@codeaurora.org>

--- drivers/gpu/msm/adreno_drawctxt.c
+++ drivers/gpu/msm/adreno_drawctxt.c
@@ -288,8 +288,10 @@ void adreno_drawctxt_switch(struct adreno_device *adreno_dev,
 		return;
 	}
 
-	KGSL_CTXT_INFO(device, "from %p to %p flags %d\n",
-			adreno_dev->drawctxt_active, drawctxt, flags);
+	KGSL_CTXT_INFO(device, "from %d to %d flags %d\n",
+		adreno_dev->drawctxt_active ?
+		adreno_dev->drawctxt_active->id : 0,
+		drawctxt ? drawctxt->id : 0, flags);
 
 	/* Save the old context */
 	adreno_dev->gpudev->ctxt_save(adreno_dev, adreno_dev->drawctxt_active);
