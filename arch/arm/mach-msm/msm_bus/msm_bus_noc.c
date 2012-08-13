f1ed82a82bddaf729f88f8ba98e515edd03e74c7
Gagan Mac <gmac@codeaurora.org>
2012-08-13 11:23:02 -0600

    msm: msm_bus: Avoid initializing NoCs not owned by Apps
    The NoCs not owned by apps are initialized by bootloaders.
    Do not touch such remote NoC registers to avoid unclocked
    register accesses.
    
    Change-Id: I8e5d36c4967daf606fdad36e2705d4daec25f060
    Signed-off-by: Gagan Mac <gmac@codeaurora.org>

--- arch/arm/mach-msm/msm_bus/msm_bus_noc.c
+++ arch/arm/mach-msm/msm_bus/msm_bus_noc.c
@@ -388,7 +388,8 @@ static void msm_bus_noc_node_init(void *hw_data,
 		(struct msm_bus_noc_info *)hw_data;
 
 	if (!IS_SLAVE(info->node_info->priv_id))
-		msm_bus_noc_mas_init(ninfo, info);
+		if (info->node_info->hw_sel != MSM_BUS_RPM)
+			msm_bus_noc_mas_init(ninfo, info);
 }
 
 static int msm_bus_noc_allocate_commit_data(struct msm_bus_fabric_registration
