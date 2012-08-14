22fdb9cbe76caf72c39cc79c69b916ba3b1937fb
Abhishek Kharbanda <akharban@codeaurora.org>
2012-08-13 18:45:02 -0700

    msm: clock-8974: Add support for HDMI PHY PLL control through clock driver.
    The hdmi phy pll drives the HDMI PHY as well as extpclk required by
    hdmi tx controller. Add support to control this pll through clock
    driver apis.
    
    Change-Id: I851c586a14afda5b4079f850a3161518bee5af4a
    Signed-off-by: Ujwal Patel <ujwalp@codeaurora.org>

--- arch/arm/mach-msm/clock-mdss-8974.h
+++ arch/arm/mach-msm/clock-mdss-8974.h
@@ -13,8 +13,12 @@
 #ifndef __ARCH_ARM_MACH_MSM_CLOCK_MDSS_8974
 #define __ARCH_ARM_MACH_MSM_CLOCK_MDSS_8974
 
-void mdss_clk_ctrl_init(void);
 extern struct clk_ops clk_ops_dsi_byte_pll;
 extern struct clk_ops clk_ops_dsi_pixel_pll;
+
+void mdss_clk_ctrl_init(void);
+int hdmi_pll_enable(void);
+void hdmi_pll_disable(void);
+int hdmi_pll_set_rate(unsigned long rate);
 
 #endif
