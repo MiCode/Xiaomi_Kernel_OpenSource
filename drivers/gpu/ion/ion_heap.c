39f0589088f721fd016e335fdca50a6e3f688091
Benjamin Gaignard <benjamin.gaignard@linaro.org>
2012-08-15 10:55:10 -0700

    add CMA heap
    New heap type ION_HEAP_TYPE_DMA where allocation is done with dma_alloc_coherent API.
    device coherent_dma_mask must be set to DMA_BIT_MASK(32).
    ion_platform_heap private field is used to retrieve the device linked to CMA,
    if NULL the default CMA area is used.
    ion_cma_get_sgtable is a copy of dma_common_get_sgtable function which should
    be in kernel 3.5
    
    Change-Id: I9ae54a3a021cb3513c2b0e8c58b69f3ae118561b
    Signed-off-by: Benjamin Gaignard <benjamin.gaignard@linaro.org>
    [lauraa: Fix context in ion_priv.h/ion.h and omit Makefile change for now]
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- drivers/gpu/ion/ion_heap.c
+++ drivers/gpu/ion/ion_heap.c
@@ -40,6 +40,11 @@ struct ion_heap *ion_heap_create(struct ion_platform_heap *heap_data)
 	case ION_HEAP_TYPE_CP:
 		heap = ion_cp_heap_create(heap_data);
 		break;
+#ifdef CONFIG_CMA
+	case ION_HEAP_TYPE_DMA:
+		heap = ion_cma_heap_create(heap_data);
+		break;
+#endif
 	default:
 		pr_err("%s: Invalid heap type %d\n", __func__,
 		       heap_data->type);
@@ -80,6 +85,11 @@ void ion_heap_destroy(struct ion_heap *heap)
 	case ION_HEAP_TYPE_CP:
 		ion_cp_heap_destroy(heap);
 		break;
+#ifdef CONFIG_CMA
+	case ION_HEAP_TYPE_DMA:
+		ion_cma_heap_destroy(heap);
+		break;
+#endif
 	default:
 		pr_err("%s: Invalid heap type %d\n", __func__,
 		       heap->type);
