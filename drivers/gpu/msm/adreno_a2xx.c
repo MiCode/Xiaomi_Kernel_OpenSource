29112fd7566baa4f5de7a591dd6bf80eba9dfddc
Shubhraprakash Das <sadas@codeaurora.org>
2012-08-15 04:11:55 -0700

    msm: kgsl: Do not save context if it is being destroyed
    No need to save a context if it is being destroyed since
    it will no longer be used at all. This is better for
    performance and also avoids the use of legacy kgsl
    context save code for contexts that use preambles
    
    Change-Id: I19a64e82188b4132f353bb61c21e4ed2281092fc
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>

--- drivers/gpu/msm/adreno_a2xx.c
+++ drivers/gpu/msm/adreno_a2xx.c
@@ -1507,7 +1507,7 @@ static void a2xx_drawctxt_save(struct adreno_device *adreno_dev,
 {
 	struct kgsl_device *device = &adreno_dev->dev;
 
-	if (context == NULL)
+	if (context == NULL || (context->flags & CTXT_FLAGS_BEING_DESTOYED))
 		return;
 
 	if (context->flags & CTXT_FLAGS_GPU_HANG)
