d8413769c6ecac074c10d505cb2f1552c4f15c26
Chintan Pandya <cpandya@codeaurora.org>
2012-08-17 13:30:45 +0530

    msm: 7627a: Allocate bigger heap size for ZSL feature
    ZSL feature in camera requires more heap size (60 MB)
    than usual requirement (18 MB). So, check that if we
    have sufficient RAM available and allocate more memory
    accordingly.
    
    Change-Id: I8c1358e09a8a9f442fd6da395dd0e866f718deac
    Signed-off-by: Chintan Pandya <cpandya@codeaurora.org>

--- arch/arm/mach-msm/board-msm7x27a.c
+++ arch/arm/mach-msm/board-msm7x27a.c
@@ -164,7 +164,7 @@ static struct msm_i2c_platform_data msm_gsbi1_qup_i2c_pdata = {
 
 #define MSM_PMEM_ADSP_SIZE      0x1200000
 #define MSM7x25A_MSM_PMEM_ADSP_SIZE      0xB91000
-
+#define CAMERA_ZSL_SIZE		(SZ_1M * 60)
 #endif
 
 #ifdef CONFIG_ION_MSM
@@ -748,6 +748,9 @@ static void fix_sizes(void)
 		pmem_mdp_size = MSM_PMEM_MDP_SIZE;
 		pmem_adsp_size = MSM_PMEM_ADSP_SIZE;
 	}
+
+	if (get_ddr_size() > SZ_512M)
+		pmem_adsp_size = CAMERA_ZSL_SIZE;
 #ifdef CONFIG_ION_MSM
 	msm_ion_camera_size = pmem_adsp_size;
 	msm_ion_audio_size = (MSM_PMEM_AUDIO_SIZE + PMEM_KERNEL_EBI1_SIZE);
