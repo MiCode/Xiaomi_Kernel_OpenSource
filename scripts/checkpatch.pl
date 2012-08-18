f469a9f12bf8947553e51347e2843c979887ac0b
Matt Wagantall <mattw@codeaurora.org>
2012-08-18 11:21:00 -0700

    checkpatch: Excuse reverts from "summary line over 75 characters" check
    Rather than forcing authors of reverts to truncate the summary line
    because the "Revert" prefix added pushes it over the character limit,
    excuse reverts from this rule.
    
    Change-Id: I395dfff3327e360ef935d4a685c38df6577e3867
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>
    Signed-off-by: Stepan Moskovchenko <stepanm@codeaurora.org>
    Signed-off-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -2648,7 +2648,7 @@ sub process {
 				$subjectline = $line;
 				$sublinenr = "#$linenr & ";
 # Check for Subject line less than line limit
-				if (length($1) > SHORTTEXT_LIMIT) {
+				if (length($1) > SHORTTEXT_LIMIT && !($1 =~ m/Revert\ \"/)) {
 					WARN("LONG_SUMMARY_LINE",
 					     "summary line over " .
 					     SHORTTEXT_LIMIT .
@@ -2660,7 +2660,7 @@ sub process {
 				# headers so we are now in the shorttext.
 				$shorttext = IN_SHORTTEXT_BLANKLINE;
 				$shorttext_exspc = 4;
-				if (length($1) > SHORTTEXT_LIMIT) {
+				if (length($1) > SHORTTEXT_LIMIT && !($1 =~ m/Revert\ \"/)) {
 					WARN("LONG_SUMMARY_LINE",
 					     "summary line over " .
 					     SHORTTEXT_LIMIT .
