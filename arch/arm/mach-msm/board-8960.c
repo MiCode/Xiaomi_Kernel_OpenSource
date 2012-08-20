697e91ba3acd35d0b91503aec9811a3a52f7742d
Arun Menon <menon@codeaurora.org>
2012-08-20 15:25:50 -0700

    msm: vidc: Bus clk changes for 8960 PRO.
    Assign the correct master clk ids to vidc
    bus client. Without this change video init
    fails.
    
    Change-Id: I88b0c6798f4c44c78d1c7428e5774f54bcb5de1a
    Signed-off-by: Arun Menon <menon@codeaurora.org>

--- arch/arm/mach-msm/board-8960.c
+++ arch/arm/mach-msm/board-8960.c
@@ -2642,7 +2642,6 @@ static struct platform_device *common_devices[] __initdata = {
 	&msm8960_android_pmem_audio_device,
 #endif
 #endif
-	&msm_device_vidc,
 	&msm_device_bam_dmux,
 	&msm_fm_platform_init,
 #if defined(CONFIG_TSIF) || defined(CONFIG_TSIF_MODULE)
@@ -3153,6 +3152,7 @@ static void __init msm8960_cdp_init(void)
 	else
 		platform_device_register(&msm8960_device_acpuclk);
 	platform_add_devices(common_devices, ARRAY_SIZE(common_devices));
+	msm8960_add_vidc_device();
 
 	msm8960_pm8921_gpio_mpp_init();
 	platform_add_devices(cdp_devices, ARRAY_SIZE(cdp_devices));
--- arch/arm/mach-msm/board-8960.h
+++ arch/arm/mach-msm/board-8960.h
@@ -92,3 +92,4 @@ void msm8960_mdp_writeback(struct memtype_reserve *reserve_table);
 
 extern struct msm_rtb_platform_data msm8960_rtb_pdata;
 extern struct msm_cache_dump_platform_data msm8960_cache_dump_pdata;
+extern void msm8960_add_vidc_device(void);
