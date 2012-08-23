fb4a2f8e62af9cfa0852b16b2370f0d3df324e59
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2012-08-23 14:36:59 -0700

    msm: 8064: add board file structures for mpdecision and msm governor
    Add device structures in the board file for mpdecision and the msm
    governor. This essentially probes mpdecision driver and the msm
    governor driver. When enabled mpdecision communicates the run queue
    average with TZ and obtains back a mask of which cpus should be
    online. The msm governor communicates the frequencies of the cpus
    with TZ and obtains newer frequency for that cpu.
    
    Both combined they govern which cpus stay online and how fast they
    run.
    
    Change-Id: I0cb71b200450290dea17f9fd9197a2749bb9a752
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- arch/arm/mach-msm/board-8064.c
+++ arch/arm/mach-msm/board-8064.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2012, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2011-2012, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -2542,6 +2542,7 @@ static struct platform_device *common_devices[] __initdata = {
 	&msm_gss,
 	&apq8064_rtb_device,
 	&apq8064_cpu_idle_device,
+	&apq8064_msm_gov_device,
 	&apq8064_device_cache_erp,
 	&msm8960_device_ebi1_ch0_erp,
 	&msm8960_device_ebi1_ch1_erp,
@@ -2565,6 +2566,7 @@ static struct platform_device *common_devices[] __initdata = {
 #ifdef CONFIG_BATTERY_BCL
 	&battery_bcl_device,
 #endif
+	&apq8064_msm_mpd_device,
 };
 
 static struct platform_device *cdp_devices[] __initdata = {
