cd5b51af0490d41ca241fcbbc41f6d56892bed64
Tianyi Gou <tgou@codeaurora.org>
2012-08-23 11:00:47 -0700

    msm: clock-8960: Disable hardware clock gating for vcap_ahb_clk
    Hardware doesn't have the support on the auto gating of vcap_ahb_clk.
    Therefore, disable this feature in this patch.
    
    Change-Id: Id64f074f29593bfe0ea1fb1d2828b1f0699b5c47
    Signed-off-by: Tianyi Gou <tgou@codeaurora.org>

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -1208,8 +1208,6 @@ static struct branch_clk vcap_p_clk = {
 	.b = {
 		.ctl_reg = AHB_EN3_REG,
 		.en_mask = BIT(1),
-		.hwcg_reg = AHB_EN3_REG,
-		.hwcg_mask = BIT(0),
 		.reset_reg = SW_RESET_AHB2_REG,
 		.reset_mask = BIT(2),
 		.halt_reg = DBG_BUS_VEC_J_REG,
@@ -6332,7 +6330,7 @@ static void __init reg_init(void)
 	}
 
 	if (cpu_is_apq8064() || cpu_is_apq8064ab())
-		rmwreg(0x00000001, AHB_EN3_REG, 0x00000001);
+		rmwreg(0x00000000, AHB_EN3_REG, 0x00000001);
 
 	/* Deassert all locally-owned MM AHB resets. */
 	rmwreg(0, SW_RESET_AHB_REG, 0xFFF7DFFF);
