be4dcf4da57b9397a2fa81a517dddaa59f442f20
Siddartha Mohanadoss <smohanad@codeaurora.org>
2012-08-27 09:10:13 -0700

    thermal: tsens8974: Fix delay in the TSENS handler
    Add a workqueue to process the status register reads for
    the sensors to verify if sensors thresholds are crossed
    and send uevent to clients. This change is to avoid any delay
    within the TSENS interrupt handler.
    
    Change-Id: I3f3b713a84a6e74fb94e27b8a69be3b1dd0a9dac
    Signed-off-by: Siddartha Mohanadoss <smohanad@codeaurora.org>

--- drivers/thermal/msm8974-tsens.c
+++ drivers/thermal/msm8974-tsens.c
@@ -157,6 +157,7 @@ struct tsens_tm_device {
 	int				calib_len;
 	struct resource			*res_tsens_mem;
 	struct resource			*res_calib_mem;
+	struct work_struct		tsens_work;
 	struct tsens_tm_device_sensor	sensor[0];
 };
 
@@ -430,9 +431,10 @@ static void notify_uspace_tsens_fn(struct work_struct *work)
 					NULL, "type");
 }
 
-static irqreturn_t tsens_isr(int irq, void *data)
+static void tsens_scheduler_fn(struct work_struct *work)
 {
-	struct tsens_tm_device *tm = data;
+	struct tsens_tm_device *tm = container_of(work, struct tsens_tm_device,
+						tsens_work);
 	unsigned int i, status, threshold;
 	unsigned int sensor_status_addr, sensor_status_ctrl_addr;
 
@@ -466,6 +468,12 @@ static irqreturn_t tsens_isr(int irq, void *data)
 		sensor_status_ctrl_addr += TSENS_SN_ADDR_OFFSET;
 	}
 	mb();
+}
+
+static irqreturn_t tsens_isr(int irq, void *data)
+{
+	schedule_work(&tmdev->tsens_work);
+
 	return IRQ_HANDLED;
 }
 
@@ -888,6 +896,8 @@ static int _tsens_register_thermal(void)
 		goto fail;
 	}
 	platform_set_drvdata(pdev, tmdev);
+
+	INIT_WORK(&tmdev->tsens_work, tsens_scheduler_fn);
 
 	return 0;
 fail:
