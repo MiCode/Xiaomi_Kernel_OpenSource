3fdea485d42c77560b0e14d33ef4b3c29f140c79
Tarun Karra <tkarra@codeaurora.org>
2012-08-28 16:30:16 -0700

    msm: kgsl: Align ION buffer mappings from userspace to 4k
    When userspace allocates an ION buffer and maps it to GPU virtual space,
    adjust the buffer size to be 4k aligned before mapping it to GPU virtual
    space. GPU virtual space map routine expects the buffer to be 4k aligned.
    
    Change-Id: Ibdb95597c24e2b5d0c3390d2c8550abb6b4b730b
    Acked-by: Rama Vaddula <rvaddula@qualcomm.com>
    Signed-off-by: Tarun Karra <tkarra@codeaurora.org>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -1747,6 +1747,8 @@ static int kgsl_setup_ion(struct kgsl_mem_entry *entry,
 		entry->memdesc.sglen++;
 	}
 
+	entry->memdesc.size = PAGE_ALIGN(entry->memdesc.size);
+
 	return 0;
 err:
 	ion_free(kgsl_ion_client, handle);
