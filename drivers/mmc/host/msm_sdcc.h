14ad9679ce0ace729eff6f4d233bb64ca9886901
Subhash Jadavani <subhashj@codeaurora.org>
2012-08-28 18:33:15 +0530

    mmc: msm_sdcc: add support for AUTO_CMD21
    Using HS200 (supported by eMMC 4.5 spec compliant cards) bus speed
    mode requires DLL (Delay Locked Loop for sampling clock generation) HW
    block to be tuned if clock rate is >100MHz so that both host and card
    are in sync. eMMC 4.5 spec has added CMD21 for this tuning mechanism.
    
    During card initialization, driver uses the tuning command to search
    for an optimal sampling point and then programs the DLL HW with this
    optimal sampling point.
    
    In addition, msm SDCC controller's DLL HW has a mechansim for data
    tracking (CDR) which can be enabled during read transactions so the host
    will not lose the sampling point as a result of voltage and, especially,
    temperature variations. Tuning pattern command (CMD21) can be sent
    automatically by HW (by using AUTO_CMD21 bit) before any RX transaction
    to help CDR keep track of the correct sampling point.
    
    Since sending CMD21 automatically before every read commands is an
    overhead, reducing read performance by ~1 MB/s and eMMC spec does not
    mandate sending the tuning command before every read operation, this
    change adds support for AUTO_CMD21 (and keeps it disabled by default)
    while providing a sysfs attribute to enable or disable AUTO_CMD21 as
    needed.
    
    Change-Id: I6e053db8878b273a407fa7caf8332eb064f351ce
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>
    Signed-off-by: Krishna Konda <kkonda@codeaurora.org>

--- drivers/mmc/host/msm_sdcc.h
+++ drivers/mmc/host/msm_sdcc.h
@@ -65,6 +65,7 @@
 #define MCI_CSPM_CCSENABLE	(1 << 14)
 #define MCI_CSPM_CCSDISABLE	(1 << 15)
 #define MCI_CSPM_AUTO_CMD19	(1 << 16)
+#define MCI_CSPM_AUTO_CMD21	(1 << 21)
 
 
 #define MMCIRESPCMD		0x010
@@ -402,6 +403,7 @@ struct msmsdcc_host {
 	bool tuning_in_progress;
 	bool tuning_needed;
 	bool en_auto_cmd19;
+	bool en_auto_cmd21;
 	bool sdio_gpio_lpm;
 	bool irq_wake_enabled;
 	struct pm_qos_request pm_qos_req_dma;
@@ -419,9 +421,14 @@ struct msmsdcc_host {
 	struct device_attribute	polling;
 	struct device_attribute idle_timeout;
 	struct device_attribute auto_cmd19_attr;
+	struct device_attribute auto_cmd21_attr;
 };
 
-#define MSMSDCC_VERSION_MASK	0xFFFF
+#define MSMSDCC_VERSION_STEP_MASK	0x0000FFFF
+#define MSMSDCC_VERSION_MINOR_MASK	0x0FFF0000
+#define MSMSDCC_VERSION_MINOR_SHIFT	16
+#define MSMSDCC_VERSION_MAJOR_MASK	0xF0000000
+#define MSMSDCC_VERSION_MAJOR_SHIFT	28
 #define MSMSDCC_DMA_SUP	(1 << 0)
 #define MSMSDCC_SPS_BAM_SUP	(1 << 1)
 #define MSMSDCC_SOFT_RESET	(1 << 2)
@@ -432,6 +439,7 @@ struct msmsdcc_host {
 #define MSMSDCC_WAIT_FOR_TX_RX	(1 << 7)
 #define MSMSDCC_IO_PAD_PWR_SWITCH	(1 << 8)
 #define MSMSDCC_AUTO_CMD19	(1 << 9)
+#define MSMSDCC_AUTO_CMD21	(1 << 10)
 
 #define set_hw_caps(h, val)		((h)->hw_caps |= val)
 #define is_sps_mode(h)			((h)->hw_caps & MSMSDCC_SPS_BAM_SUP)
@@ -444,11 +452,14 @@ struct msmsdcc_host {
 #define is_wait_for_tx_rx_active(h)	((h)->hw_caps & MSMSDCC_WAIT_FOR_TX_RX)
 #define is_io_pad_pwr_switch(h)	((h)->hw_caps & MSMSDCC_IO_PAD_PWR_SWITCH)
 #define is_auto_cmd19(h)		((h)->hw_caps & MSMSDCC_AUTO_CMD19)
+#define is_auto_cmd21(h)		((h)->hw_caps & MSMSDCC_AUTO_CMD21)
 
 /* Set controller capabilities based on version */
 static inline void set_default_hw_caps(struct msmsdcc_host *host)
 {
 	u32 version;
+	u16 step, minor;
+
 	/*
 	 * Lookup the Controller Version, to identify the supported features
 	 * Version number read as 0 would indicate SDCC3 or earlier versions.
@@ -459,15 +470,21 @@ static inline void set_default_hw_caps(struct msmsdcc_host *host)
 	if (!version)
 		return;
 
-	version &= MSMSDCC_VERSION_MASK;
+	step = version & MSMSDCC_VERSION_STEP_MASK;
+	minor = (version & MSMSDCC_VERSION_MINOR_MASK) >>
+			MSMSDCC_VERSION_MINOR_SHIFT;
+
 	if (version) /* SDCC v4 and greater */
 		host->hw_caps |= MSMSDCC_AUTO_PROG_DONE |
 			MSMSDCC_SOFT_RESET | MSMSDCC_REG_WR_ACTIVE
 			| MSMSDCC_WAIT_FOR_TX_RX | MSMSDCC_IO_PAD_PWR_SWITCH
 			| MSMSDCC_AUTO_CMD19;
 
+	if ((step == 0x18) && (minor >= 3))
+		host->hw_caps |= MSMSDCC_AUTO_CMD21;
+
 	if (version >= 0x2b) /* SDCC v4 2.1.0 and greater */
-		host->hw_caps |= MSMSDCC_SW_RST;
+		host->hw_caps |= MSMSDCC_SW_RST | MSMSDCC_AUTO_CMD21;
 }
 
 int msmsdcc_set_pwrsave(struct mmc_host *mmc, int pwrsave);
