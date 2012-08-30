e5e307658a5e4d494194980571f083bbf1dbe6bd
Vikram Mulukutla <markivx@codeaurora.org>
2012-08-30 10:59:45 -0700

    msm: modem-ssr-8974: Add support for phase 3 modem restart
    Add support to restart just the modem when it encounters
    a fatal error. This includes calling PIL to shutdown and
    boot the modem up, as well as adding support to allow
    userspace to collect the modem ramdump.
    
    Change-Id: I0cd3a662e555a7a1c6104f714ae133b382414500
    Signed-off-by: Vikram Mulukutla <markivx@codeaurora.org>

--- arch/arm/mach-msm/modem-ssr-8974.c
+++ arch/arm/mach-msm/modem-ssr-8974.c
@@ -15,10 +15,14 @@
 #include <linux/module.h>
 #include <linux/err.h>
 
+#include <mach/peripheral-loader.h>
 #include <mach/subsystem_restart.h>
 #include <mach/msm_smsm.h>
 
+#include "ramdump.h"
+
 static int crash_shutdown;
+static int modem_ssr_ignore_errors;
 static struct subsys_device *modem_ssr_dev;
 
 #define MAX_SSR_REASON_LEN 81U
@@ -50,6 +54,7 @@ static void log_modem_sfr(void)
 static void restart_modem(void)
 {
 	log_modem_sfr();
+	modem_ssr_ignore_errors = 1;
 	subsystem_restart("modem");
 }
 
@@ -67,11 +72,21 @@ static void smsm_state_cb(void *data, uint32_t old_state, uint32_t new_state)
 
 static int modem_shutdown(const struct subsys_desc *subsys)
 {
+	pil_force_shutdown("modem");
+	pil_force_shutdown("mba");
 	return 0;
 }
 
 static int modem_powerup(const struct subsys_desc *subsys)
 {
+	/*
+	 * At this time, the modem is shutdown. Therefore this function cannot
+	 * run concurrently with either the watchdog bite error handler or the
+	 * SMSM callback, making it safe to unset the flag below.
+	 */
+	modem_ssr_ignore_errors = 0;
+	pil_force_boot("mba");
+	pil_force_boot("modem");
 	return 0;
 }
 
@@ -81,15 +96,53 @@ void modem_crash_shutdown(const struct subsys_desc *subsys)
 	smsm_reset_modem(SMSM_RESET);
 }
 
-static int modem_ramdump(int enable,
-				const struct subsys_desc *crashed_subsys)
+static struct ramdump_segment modem_segments[] = {
+	{0x08400000, 0x0D100000 - 0x08400000},
+};
+
+static struct ramdump_segment smem_segments[] = {
+	{0x0FA00000, 0x0FC00000 - 0x0FA00000},
+};
+
+static void *modem_ramdump_dev;
+static void *smem_ramdump_dev;
+
+static int modem_ramdump(int enable, const struct subsys_desc *crashed_subsys)
 {
-	return 0;
+	int ret = 0;
+
+	if (!enable)
+		return ret;
+
+	pil_force_boot("mba");
+
+	ret = do_ramdump(modem_ramdump_dev, modem_segments,
+				ARRAY_SIZE(modem_segments));
+
+	if (ret < 0) {
+		pr_err("Unable to dump modem fw memory (rc = %d).\n",
+			ret);
+		goto out;
+	}
+
+	ret = do_ramdump(smem_ramdump_dev, smem_segments,
+		ARRAY_SIZE(smem_segments));
+
+	if (ret < 0) {
+		pr_err("Unable to dump smem memory (rc = %d).\n", ret);
+		goto out;
+	}
+
+out:
+	pil_force_shutdown("mba");
+	return ret;
 }
 
 static irqreturn_t modem_wdog_bite_irq(int irq, void *dev_id)
 {
-	pr_err("Watchdog bite received from modem software!\n");
+	if (modem_ssr_ignore_errors)
+		return IRQ_HANDLED;
+	pr_err("Watchdog bite received from the modem!\n");
 	restart_modem();
 	return IRQ_HANDLED;
 }
@@ -133,9 +186,27 @@ static int __init modem_8974_init(void)
 		goto out;
 	}
 
+	modem_ramdump_dev = create_ramdump_device("modem");
+
+	if (!modem_ramdump_dev) {
+		pr_err("%s: Unable to create a modem ramdump device.\n",
+			__func__);
+		ret = -ENOMEM;
+		goto out;
+	}
+
+	smem_ramdump_dev = create_ramdump_device("smem-modem");
+
+	if (!smem_ramdump_dev) {
+		pr_err("%s: Unable to create an smem ramdump device.\n",
+			__func__);
+		ret = -ENOMEM;
+		goto out;
+	}
+
 	pr_info("%s: modem subsystem restart driver init'ed.\n", __func__);
 out:
 	return ret;
 }
 
-arch_initcall(modem_8974_init);
+module_init(modem_8974_init);
