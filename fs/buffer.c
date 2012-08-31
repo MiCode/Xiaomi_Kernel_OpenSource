1c67df6a97a4819508572a4da00ccd2a440db64e
Laura Abbott <lauraa@codeaurora.org>
2012-08-30 18:01:17 -0700

    fs/buffer.c: Revoke LRU when trying to drop buffers
    When a buffer is added to the LRU list, a reference is taken which is
    not dropped until the buffer is evicted from the LRU list. This is the
    correct behavior, however this LRU reference will prevent the buffer
    from being dropped. This means that the buffer can't actually be dropped
    until it is selected for eviction. There's no bound on the time spent
    on the LRU list, which means that the buffer may be undroppable for
    very long periods of time. Given that migration involves dropping
    buffers, the associated page is now unmigratible for long periods of
    time as well. CMA relies on being able to migrate a specific range
    of pages, so these these types of failures make CMA significantly
    less reliable, especially under high filesystem usage.
    
    Rather than waiting for the LRU algorithm to eventually kick out
    the buffer, explicitly remove the buffer from the LRU list when trying
    to drop it. There is still the possibility that the buffer
    could be added back on the list, but that indicates the buffer is
    still in use and would probably have other 'in use' indicates to
    prevent dropping.
    
    Change-Id: I253f4ee2069e190c1115afc421dadd27a7fa87dc
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- fs/buffer.c
+++ fs/buffer.c
@@ -1455,11 +1455,47 @@ static bool has_bh_in_lru(int cpu, void *dummy)
 	return 0;
 }
 
+static void __evict_bh_lru(void *arg)
+{
+	struct bh_lru *b = &get_cpu_var(bh_lrus);
+	struct buffer_head *bh = arg;
+	int i;
+
+	for (i = 0; i < BH_LRU_SIZE; i++) {
+		if (b->bhs[i] == bh) {
+			brelse(b->bhs[i]);
+			b->bhs[i] = NULL;
+			goto out;
+		}
+	}
+out:
+	put_cpu_var(bh_lrus);
+}
+
+static bool bh_exists_in_lru(int cpu, void *arg)
+{
+	struct bh_lru *b = per_cpu_ptr(&bh_lrus, cpu);
+	struct buffer_head *bh = arg;
+	int i;
+
+	for (i = 0; i < BH_LRU_SIZE; i++) {
+		if (b->bhs[i] == bh)
+			return 1;
+	}
+
+	return 0;
+
+}
 void invalidate_bh_lrus(void)
 {
 	on_each_cpu_cond(has_bh_in_lru, invalidate_bh_lru, NULL, 1, GFP_KERNEL);
 }
 EXPORT_SYMBOL_GPL(invalidate_bh_lrus);
+
+static void evict_bh_lrus(struct buffer_head *bh)
+{
+	on_each_cpu_cond(bh_exists_in_lru, __evict_bh_lru, bh, 1, GFP_ATOMIC);
+}
 
 void set_bh_page(struct buffer_head *bh,
 		struct page *page, unsigned long offset)
@@ -3250,8 +3286,15 @@ drop_buffers(struct page *page, struct buffer_head **buffers_to_free)
 	do {
 		if (buffer_write_io_error(bh) && page->mapping)
 			mapping_set_error(page->mapping, -EIO);
-		if (buffer_busy(bh))
-			goto failed;
+		if (buffer_busy(bh)) {
+			/*
+			 * Check if the busy failure was due to an
+			 * outstanding LRU reference
+			 */
+			evict_bh_lrus(bh);
+			if (buffer_busy(bh))
+				goto failed;
+		}
 		bh = bh->b_this_page;
 	} while (bh != head);
 
