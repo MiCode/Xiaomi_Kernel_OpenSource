beb49a9823b1da2145dd963ca7f3509addbba442
Hemant Kumar <hemantk@codeaurora.org>
2012-09-02 11:10:35 -0700

    usb: mdm_bridge: Free driver data structure memory in correct order
    mdm bridge driver allocates driver data structure for dial up network
    and RMNET interface in the order interface probe is called. Driver frees
    the data structure memory for control channel in reverse order in
    interface disconnect. This causes the usb core driver to access freed
    memory(freed during dial up network interface unbind) for control
    channel when it unbinds RMNET interface. Hence allocate and free data
    structure memory for control channel in the same order in which it was
    allocated.
    
    CRs-Fixed: 394951
    Change-Id: Ic399c123253fcee46895e3b0b749dcd0fe43d99d
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- drivers/usb/misc/mdm_data_bridge.c
+++ drivers/usb/misc/mdm_data_bridge.c
@@ -990,10 +990,10 @@ static void bridge_disconnect(struct usb_interface *intf)
 	}
 
 	ch_id--;
-	ctrl_bridge_disconnect(ch_id);
+	ctrl_bridge_disconnect(dev->id);
 	platform_device_unregister(dev->pdev);
 	usb_set_intfdata(intf, NULL);
-	__dev[ch_id] = NULL;
+	__dev[dev->id] = NULL;
 
 	cancel_work_sync(&dev->process_rx_w);
 	cancel_work_sync(&dev->kevent);
