1356b5826e2f9c75ac3feef61ff31fa272a97232
Srinu Gorle <sgorle@codeaurora.org>
2012-09-05 18:14:24 +0530

    msm: vidc: Enable turbo mode based on client request.
    - video core is bumped up to turbo mode, if more performance
      level required from multiple clients.
    
    - Enable turbo mode atleast one of client set turbo mode.
    
    Change-Id: Ied777463fdfe54ea6ff3b2a29cbaf0d27a9586cb
    CRs-Fixed: 385454
    Signed-off-by: Srinu Gorle <sgorle@codeaurora.org>

--- drivers/video/msm/vidc/common/vcd/vcd_core.h
+++ drivers/video/msm/vidc/common/vcd/vcd_core.h
@@ -147,7 +147,7 @@ struct vcd_dev_ctxt {
 	u32 reqd_perf_lvl;
 	u32 curr_perf_lvl;
 	u32 set_perf_lvl_pending;
-	u32 turbo_mode_set;
+	bool turbo_mode_set;
 };
 
 struct vcd_clnt_status {
@@ -214,6 +214,7 @@ struct vcd_clnt_ctxt {
 	u32 meta_mode;
 	int perf_set_by_client;
 	int secure;
+	bool is_turbo_enabled;
 };
 
 #define VCD_BUFFERPOOL_INUSE_DECREMENT(val) \
