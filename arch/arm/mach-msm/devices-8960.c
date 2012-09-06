e011d225a6474faeb1d45571e80aba0277ab0c0b
Mitchel Humpherys <mitchelh@codeaurora.org>
2012-09-06 11:36:08 -0700

    msm: 8960: include msm-specific ion header
    All msm_ion clients need to use <linux/msm_ion.h> instead of
    <linux/ion.h>
    
    Change-Id: Ia8cf2d01d72e30a9fa68a1608ecefb07867e6504
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- arch/arm/mach-msm/devices-8960.c
+++ arch/arm/mach-msm/devices-8960.c
@@ -15,7 +15,7 @@
 #include <linux/list.h>
 #include <linux/platform_device.h>
 #include <linux/msm_rotator.h>
-#include <linux/ion.h>
+#include <linux/msm_ion.h>
 #include <linux/gpio.h>
 #include <linux/coresight.h>
 #include <asm/clkdev.h>
