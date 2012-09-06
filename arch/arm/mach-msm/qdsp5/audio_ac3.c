1da6ebee4e683ff702663af817b07d631dbc38a1
Mitchel Humpherys <mitchelh@codeaurora.org>
2012-09-06 10:15:56 -0700

    msm: audio: include msm-specific ion header
    All msm_ion clients need to use <linux/msm_ion.h> instead of
    <linux/ion.h>
    
    Change-Id: Ida4e46d0f257e8c27467a8c13f7a0eb710dd3f5c
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- arch/arm/mach-msm/qdsp5/audio_ac3.c
+++ arch/arm/mach-msm/qdsp5/audio_ac3.c
@@ -37,7 +37,7 @@
 #include <linux/msm_audio.h>
 #include <linux/memory_alloc.h>
 #include <linux/msm_audio_ac3.h>
-#include <linux/ion.h>
+#include <linux/msm_ion.h>
 
 #include <mach/msm_adsp.h>
 #include <mach/iommu.h>
