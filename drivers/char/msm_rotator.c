6c7b2d322826aaf12b7682079cadb8ac9179fbce
Mitchel Humpherys <mitchelh@codeaurora.org>
2012-09-06 10:33:12 -0700

    msm: display: include msm-specific ion header
    All msm_ion clients need to use <linux/msm_ion.h> instead of
    <linux/ion.h>
    
    Change-Id: Id079277f1f5688f08c8dec7f20308315ab46a081
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- drivers/char/msm_rotator.c
+++ drivers/char/msm_rotator.c
@@ -28,7 +28,7 @@
 #include <linux/file.h>
 #include <linux/major.h>
 #include <linux/regulator/consumer.h>
-#include <linux/ion.h>
+#include <linux/msm_ion.h>
 #ifdef CONFIG_MSM_BUS_SCALING
 #include <mach/msm_bus.h>
 #include <mach/msm_bus_board.h>
