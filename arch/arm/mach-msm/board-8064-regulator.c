344455a6d4ea9e0ebfa24d08c71a61e1d2268734
Santosh Mardi <gsantosh@codeaurora.org>
2012-09-07 13:22:16 +0530

    msm: board-8064: Add support for ashiko codec for mpq HRD board
    Hybrid HRD board ashiko codec's regulator sit on L2 rail.
    Update the board file to enable the regulator from L2.
    
    Change-Id: I20127c5c4c5938e468a774fe87859b1ce996fb53
    Signed-off-by: Santosh Mardi <gsantosh@codeaurora.org>

--- arch/arm/mach-msm/board-8064-regulator.c
+++ arch/arm/mach-msm/board-8064-regulator.c
@@ -33,6 +33,8 @@ VREG_CONSUMERS(L2) = {
 	REGULATOR_SUPPLY("mipi_csi_vdd",	"msm_csid.2"),
 	REGULATOR_SUPPLY("lvds_pll_vdda",	"lvds.0"),
 	REGULATOR_SUPPLY("dsi1_pll_vdda",	"mipi_dsi.1"),
+	REGULATOR_SUPPLY("HRD_VDDD_CDC_D",		"tabla2x-slim"),
+	REGULATOR_SUPPLY("HRD_CDC_VDDA_A_1P2V",	"tabla2x-slim"),
 };
 VREG_CONSUMERS(L3) = {
 	REGULATOR_SUPPLY("8921_l3",		NULL),
