c7f2cffe29201fe20fe570ee205d96dea547c38a
Lucille Sylvester <lsylvest@codeaurora.org>
2012-09-10 16:21:00 -0600

    msm: kgsl: Hold the pm_qos vote at all times when ACTIVE
    The GPU needs access to the L2 cache while running.  If the
    CPU is not also active using the cache can lead to stability
    issues.  Keep the CPU out of idle power collapse at all times
    while the GPU is running.
    
    CRs-Fixed: 397489
    Change-Id: Iab2d8c601e903aadc4060f7c6d67bc128840edbf
    Signed-off-by: Lucille Sylvester <lsylvest@codeaurora.org>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -970,9 +970,8 @@ void kgsl_pwrctrl_wake(struct kgsl_device *device)
 		/* Re-enable HW access */
 		mod_timer(&device->idle_timer,
 				jiffies + device->pwrctrl.interval_timeout);
-		if (device->pwrctrl.restore_slumber == false)
-			pm_qos_update_request(&device->pm_qos_req_dma,
-						GPU_SWFI_LATENCY);
+		pm_qos_update_request(&device->pm_qos_req_dma,
+					GPU_SWFI_LATENCY);
 	case KGSL_STATE_ACTIVE:
 		break;
 	default:
