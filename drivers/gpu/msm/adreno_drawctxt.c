3ea912bdb071f43c7bd97dcaf951e3c83d1fa115
Shubhraprakash Das <sadas@codeaurora.org>
2012-09-11 18:58:34 -0700

    msm: kgsl: Define unique values for context flags
    Use BIT to define all context related flags. This ensures
    that these flags are unique and is easier to maintain. Also,
    fix spelling of CTXT_FLAGS_BEING_DESTOYED to
    CTXT_FLAGS_BEING_DESTROYED
    
    Change-Id: I866c67b9c5d59d6117e31714756af3106018f9cb
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>

--- drivers/gpu/msm/adreno_drawctxt.c
+++ drivers/gpu/msm/adreno_drawctxt.c
@@ -221,7 +221,7 @@ void adreno_drawctxt_destroy(struct kgsl_device *device,
 				     CTXT_FLAGS_GMEM_SHADOW |
 				     CTXT_FLAGS_STATE_SHADOW);
 
-		drawctxt->flags |= CTXT_FLAGS_BEING_DESTOYED;
+		drawctxt->flags |= CTXT_FLAGS_BEING_DESTROYED;
 
 		adreno_drawctxt_switch(adreno_dev, NULL, 0);
 	}
