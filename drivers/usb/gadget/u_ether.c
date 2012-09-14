9468710b050a05088e179d9710f8e1dc27558af2
taeju.park <taeju.park@lge.com>
2012-09-14 14:09:03 +0900

    usb: gadget: prevent change of Host MAC address of 'usb0' interface
    On windows 7 platform, previously allocated ip address is maintained.
    However, Host MAC address of 'usb0' interface is changed when the
    tethering driver re-enumerated. Thus, the tethering network driver
    can't be allocated ip address from dhcp. It causes connection delay
    between host and phone for usb tethering.
    
    This patch prevents from changing Host MAC address of 'usb0' interface.
    In other words, this patch maintains the Host MAC address allocated when
    first tethering driver although the driver is re-enumerated. However,
    after reboot, the Host MAC address can be changed.
    
    Change-Id: I43add9925e9d6d90c56cffbd3ed999104448f818
    Signed-off-by: Badhri Jagan Sridharan <Badhri@google.com>

--- drivers/usb/gadget/u_ether.c
+++ drivers/usb/gadget/u_ether.c
@@ -717,6 +717,8 @@ static int eth_stop(struct net_device *net)
 
 /*-------------------------------------------------------------------------*/
 
+static u8 host_ethaddr[ETH_ALEN];
+
 static int get_ether_addr(const char *str, u8 *dev_addr)
 {
 	if (str) {
@@ -747,6 +749,17 @@ static int get_ether_addr_str(u8 dev_addr[ETH_ALEN], char *str, int len)
 		 dev_addr[0], dev_addr[1], dev_addr[2],
 		 dev_addr[3], dev_addr[4], dev_addr[5]);
 	return 18;
+}
+
+static int get_host_ether_addr(u8 *str, u8 *dev_addr)
+{
+	memcpy(dev_addr, str, ETH_ALEN);
+	if (is_valid_ether_addr(dev_addr))
+		return 0;
+
+	random_ether_addr(dev_addr);
+	memcpy(str, dev_addr, ETH_ALEN);
+	return 1;
 }
 
 static const struct net_device_ops eth_netdev_ops = {
@@ -806,9 +819,11 @@ struct eth_dev *gether_setup_name(struct usb_gadget *g,
 	if (get_ether_addr(dev_addr, net->dev_addr))
 		dev_warn(&g->dev,
 			"using random %s ethernet address\n", "self");
-	if (get_ether_addr(host_addr, dev->host_mac))
-		dev_warn(&g->dev,
-			"using random %s ethernet address\n", "host");
+
+	if (get_host_ether_addr(host_ethaddr, dev->host_mac))
+		dev_warn(&g->dev, "using random %s ethernet address\n", "host");
+	else
+		dev_warn(&g->dev, "using previous %s ethernet address\n", "host");
 
 	if (ethaddr)
 		memcpy(ethaddr, dev->host_mac, ETH_ALEN);
