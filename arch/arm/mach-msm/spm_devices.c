73a449d80bb185ee0afe45a11dc974fa494d8912
Praveen Chidambaram <pchidamb@codeaurora.org>
2012-09-16 14:54:35 -0600

    msm: spm: Add cross-calling support to change voltages
    SPM block is responsible for power collapsing the core, but also
    interacts with the PMIC through hardware to change core voltage. It
    would be beneficial to frequency algorithms to change frequencies and
    thereby voltages of all cores running from a single core. In some
    targets, the SPM is configured to set voltage only for that core. Ensure
    that the core voltage is set only from that core.
    
    Change-Id: I656e20a49d9784f08e9ce4d4e8f6c15760ba015b
    Signed-off-by: Praveen Chidambaram <pchidamb@codeaurora.org>

--- arch/arm/mach-msm/spm_devices.c
+++ arch/arm/mach-msm/spm_devices.c
@@ -38,16 +38,38 @@ struct msm_spm_device {
 	uint32_t num_modes;
 };
 
+struct msm_spm_vdd_info {
+	uint32_t cpu;
+	uint32_t vlevel;
+	int err;
+};
+
 static struct msm_spm_device msm_spm_l2_device;
 static DEFINE_PER_CPU_SHARED_ALIGNED(struct msm_spm_device, msm_cpu_spm_device);
 
-int msm_spm_set_vdd(unsigned int cpu, unsigned int vlevel)
+
+/* Must be called on the same cpu as the one being set to */
+static void msm_spm_smp_set_vdd(void *data)
 {
 	struct msm_spm_device *dev;
-	int ret = -EIO;
+	struct msm_spm_vdd_info *info = (struct msm_spm_vdd_info *)data;
 
-	dev = &per_cpu(msm_cpu_spm_device, cpu);
-	ret = msm_spm_drv_set_vdd(&dev->reg_data, vlevel);
+	dev = &per_cpu(msm_cpu_spm_device, info->cpu);
+	info->err = msm_spm_drv_set_vdd(&dev->reg_data, info->vlevel);
+}
+
+int msm_spm_set_vdd(unsigned int cpu, unsigned int vlevel)
+{
+	struct msm_spm_vdd_info info;
+	int ret;
+
+	info.cpu = cpu;
+	info.vlevel = vlevel;
+
+	/* Set to true to block on vdd change */
+	ret = smp_call_function_single(cpu, msm_spm_smp_set_vdd, &info, true);
+	if (!ret)
+		ret = info.err;
 
 	return ret;
 }
