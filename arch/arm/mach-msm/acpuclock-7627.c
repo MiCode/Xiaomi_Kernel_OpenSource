b6b3998a08d8f6a1f54a612a1f6e6fca9b0f27f5
Trilok Soni <tsoni@codeaurora.org>
2012-09-17 14:21:48 +0530

    msm: acpuclock-7627: Avoid AXI freq. change while going into power collapse
    There is no reason to change the AXI freq. while going into the PC
    since the same AXI frequency is maintained by vote while coming
    out of the PC. If the suspend power collapse happens from the
    arch_idle path then we would see the sleeping calls while atomic
    warning due to mutex being held in the clk_set_rate calls.
    
    Change-Id: I5343c7ea6927c236046d62e94791462cdd8eccf6
    Signed-off-by: Trilok Soni <tsoni@codeaurora.org>

--- arch/arm/mach-msm/acpuclock-7627.c
+++ arch/arm/mach-msm/acpuclock-7627.c
@@ -827,7 +827,8 @@ done:
 		goto out;
 
 	/* Change the AXI bus frequency if we can. */
-	if (strt_s->axiclk_khz != tgt_s->axiclk_khz) {
+	if (reason != SETRATE_PC &&
+		strt_s->axiclk_khz != tgt_s->axiclk_khz) {
 		res = clk_set_rate(drv_state.ebi1_clk,
 				tgt_s->axiclk_khz * 1000);
 		if (res < 0)
