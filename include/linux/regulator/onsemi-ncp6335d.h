990e862f18c12daaa18765db2838379eeb54a2db
Anirudh Ghayal <aghayal@codeaurora.org>
2012-09-18 16:19:10 +0530

    drivers: regulator: Add support for OnSemi NCP6335D regulator
    This driver supports OnSemi NCP6335D synchronous buck converter.
    The NCP6335D is a step-down switching voltage regulator which
    provides a voltage range from 0.6 to 1.4V. The regulator is
    controlled via the I2C interface.
    
    Change-Id: I237fe781d5c54d2b3ffc65096b9e83c387d3b6f5
    Signed-off-by: Anirudh Ghayal <aghayal@codeaurora.org>

---
+++ include/linux/regulator/onsemi-ncp6335d.h
@@ -0,0 +1,28 @@
+/* Copyright (c) 2012, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __NCP6335D_H__
+#define __NCP6335D_H__
+
+enum {
+	NCP6335D_VSEL0,
+	NCP6335D_VSEL1,
+};
+
+struct ncp6335d_platform_data {
+	struct regulator_init_data *init_data;
+	int default_vsel;
+	int slew_rate_ns;
+	int discharge_enable;
+};
+
+#endif
