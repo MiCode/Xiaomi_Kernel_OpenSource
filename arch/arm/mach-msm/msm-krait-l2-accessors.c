0e43fdcfc9992e16534a7bc7c1aeacb9bf45f52a
Matt Wagantall <mattw@codeaurora.org>
2012-09-18 19:11:59 -0700

    msm: krait-l2-accessors: Remove workaround for 8064 Krait errata
    This workaround can be removed in favor of an alternate solution
    implemented in the power-collapse path which backs up the
    corruptable L2CPUCPMR and L2CPMR registers before L2 power-collase
    and restores them afterwards.
    
    CRs-Fixed: 400700
    Change-Id: I8bfa87fa9c163cecb712e18ed64151e9bbde4cc3
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

--- arch/arm/mach-msm/msm-krait-l2-accessors.c
+++ arch/arm/mach-msm/msm-krait-l2-accessors.c
@@ -18,75 +18,12 @@
 
 DEFINE_RAW_SPINLOCK(l2_access_lock);
 
-#define L2CPMR		0x500
-#define L2CPUCPMR	0x501
-#define L2CPUVRF8	0x708
-#define CPUNDX_MASK	(0x7 << 12)
-
-/*
- * For Krait versions found in APQ8064v1.x, save L2CPUVRF8 before
- * L2CPMR or L2CPUCPMR writes and restore it after to work around an
- * issue where L2CPUVRF8 becomes corrupt.
- */
-static bool l2cpuvrf8_needs_fix(u32 reg_addr)
-{
-	switch (read_cpuid_id()) {
-	case 0x510F06F0: /* KR28M4A10  */
-	case 0x510F06F1: /* KR28M4A10B */
-	case 0x510F06F2: /* KR28M4A11  */
-		break;
-	default:
-		return false;
-	};
-
-	switch (reg_addr & ~CPUNDX_MASK) {
-	case L2CPMR:
-	case L2CPUCPMR:
-		return true;
-	default:
-		return false;
-	}
-}
-
-static u32 l2cpuvrf8_fix_save(u32 reg_addr, u32 *l2cpuvrf8_val)
-{
-	u32 l2cpuvrf8_addr = L2CPUVRF8 | (reg_addr & CPUNDX_MASK);
-
-	mb();
-	asm volatile ("mcr     p15, 3, %[l2cpselr], c15, c0, 6\n\t"
-		      "isb\n\t"
-		      "mrc     p15, 3, %[l2cpdr],   c15, c0, 7\n\t"
-			: [l2cpdr]"=r" (*l2cpuvrf8_val)
-			: [l2cpselr]"r" (l2cpuvrf8_addr)
-	);
-
-	return l2cpuvrf8_addr;
-}
-
-static void l2cpuvrf8_fix_restore(u32 l2cpuvrf8_addr, u32 l2cpuvrf8_val)
-{
-	mb();
-	asm volatile ("mcr     p15, 3, %[l2cpselr], c15, c0, 6\n\t"
-		      "isb\n\t"
-		      "mcr     p15, 3, %[l2cpdr],   c15, c0, 7\n\t"
-		      "isb\n\t"
-			:
-			: [l2cpselr]"r" (l2cpuvrf8_addr),
-			  [l2cpdr]"r" (l2cpuvrf8_val)
-	);
-}
-
 u32 set_get_l2_indirect_reg(u32 reg_addr, u32 val)
 {
 	unsigned long flags;
-	u32 uninitialized_var(l2cpuvrf8_val), l2cpuvrf8_addr = 0;
 	u32 ret_val;
 
 	raw_spin_lock_irqsave(&l2_access_lock, flags);
-
-	if (l2cpuvrf8_needs_fix(reg_addr))
-		l2cpuvrf8_addr = l2cpuvrf8_fix_save(reg_addr, &l2cpuvrf8_val);
-
 	mb();
 	asm volatile ("mcr     p15, 3, %[l2cpselr], c15, c0, 6\n\t"
 		      "isb\n\t"
@@ -96,10 +33,6 @@ u32 set_get_l2_indirect_reg(u32 reg_addr, u32 val)
 			: [l2cpdr_read]"=r" (ret_val)
 			: [l2cpselr]"r" (reg_addr), [l2cpdr]"r" (val)
 	);
-
-	if (l2cpuvrf8_addr)
-		l2cpuvrf8_fix_restore(l2cpuvrf8_addr, l2cpuvrf8_val);
-
 	raw_spin_unlock_irqrestore(&l2_access_lock, flags);
 
 	return ret_val;
@@ -109,13 +42,8 @@ EXPORT_SYMBOL(set_get_l2_indirect_reg);
 void set_l2_indirect_reg(u32 reg_addr, u32 val)
 {
 	unsigned long flags;
-	u32 uninitialized_var(l2cpuvrf8_val), l2cpuvrf8_addr = 0;
 
 	raw_spin_lock_irqsave(&l2_access_lock, flags);
-
-	if (l2cpuvrf8_needs_fix(reg_addr))
-		l2cpuvrf8_addr = l2cpuvrf8_fix_save(reg_addr, &l2cpuvrf8_val);
-
 	mb();
 	asm volatile ("mcr     p15, 3, %[l2cpselr], c15, c0, 6\n\t"
 		      "isb\n\t"
@@ -124,10 +52,6 @@ void set_l2_indirect_reg(u32 reg_addr, u32 val)
 			:
 			: [l2cpselr]"r" (reg_addr), [l2cpdr]"r" (val)
 	);
-
-	if (l2cpuvrf8_addr)
-		l2cpuvrf8_fix_restore(l2cpuvrf8_addr, l2cpuvrf8_val);
-
 	raw_spin_unlock_irqrestore(&l2_access_lock, flags);
 }
 EXPORT_SYMBOL(set_l2_indirect_reg);
