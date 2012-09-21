bd556dc51ccf32a7ddc8d435d31f39e981815f8f
Kuogee Hsieh <khsieh@codeaurora.org>
2012-09-21 14:11:30 -0700

    msm_fb: display: check base layer pipe at irq handler
    There is a corner case when overlay and dmap interrupts
    are fired before base layer pipe is allocated. Therefore
    Check for base layer pipe before performing any operations.
    
    Change-Id: Ib188d575044f5479672503508042c8c1e9c759c4
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>

--- drivers/video/msm/mdp4_overlay_dsi_video.c
+++ drivers/video/msm/mdp4_overlay_dsi_video.c
@@ -881,6 +881,8 @@ void mdp4_dmap_done_dsi_video(int cndx)
 	}
 	vctrl = &vsync_ctrl_db[cndx];
 	pipe = vctrl->base_pipe;
+	if (pipe == NULL)
+		return;
 
 	spin_lock(&vctrl->spin_lock);
 	vsync_irq_disable(INTR_DMA_P_DONE, MDP_DMAP_TERM);
@@ -916,6 +918,8 @@ void mdp4_overlay0_done_dsi_video(int cndx)
 
 	vctrl = &vsync_ctrl_db[cndx];
 	pipe = vctrl->base_pipe;
+	if (pipe == NULL)
+		return;
 
 	spin_lock(&vctrl->spin_lock);
 	vsync_irq_disable(INTR_OVERLAY0_DONE, MDP_OVERLAY0_TERM);
