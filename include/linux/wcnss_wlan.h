f2e71af489490ba3a74308b01c349a0b3c273960
Sameer Thalappil <sameert@codeaurora.org>
2012-09-24 15:26:12 -0700

    wcnss: Add interface to Reset wcnss
    Add wcnss reset interface, this interface generates an FIQ to Riva.
    Upon receiving this interrupt, Riva will execute the ERR_FATAL
    routine and invoke SSR.
    
    Change-Id: I74c293121cb69b312ed980902a12019adb700792
    Signed-off-by: Sameer Thalappil <sameert@codeaurora.org>

--- include/linux/wcnss_wlan.h
+++ include/linux/wcnss_wlan.h
@@ -58,6 +58,7 @@ void wcnss_prevent_suspend(void);
 int wcnss_hardware_type(void);
 void *wcnss_prealloc_get(unsigned int size);
 int wcnss_prealloc_put(void *ptr);
+void wcnss_reset_intr(void);
 
 #define wcnss_wlan_get_drvdata(dev) dev_get_drvdata(dev)
 #define wcnss_wlan_set_drvdata(dev, data) dev_set_drvdata((dev), (data))
