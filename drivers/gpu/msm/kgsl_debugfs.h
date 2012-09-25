269b60c3630a2814a42418bdfddfdd33cef87c88
Jeremy Gebben <jgebben@codeaurora.org>
2012-09-25 10:57:38 -0600

    msm: kgsl: add per-process memory info to debugfs
    Add a file for each process in debugfs named, kgsl/proc/<pid>/mem
    which contains information about all memory allocations the process
    has made.
    
    Change-Id: Ice3f039d92cc1b1cdb5a6192808441ddfdf8abfb
    Signed-off-by: Jeremy Gebben <jgebben@codeaurora.org>

--- drivers/gpu/msm/kgsl_debugfs.h
+++ drivers/gpu/msm/kgsl_debugfs.h
@@ -15,6 +15,7 @@
 #define _KGSL_DEBUGFS_H
 
 struct kgsl_device;
+struct kgsl_process_private;
 
 #ifdef CONFIG_DEBUG_FS
 void kgsl_core_debugfs_init(void);
@@ -28,11 +29,16 @@ static inline struct dentry *kgsl_get_debugfs_dir(void)
 	return kgsl_debugfs_dir;
 }
 
+int kgsl_process_init_debugfs(struct kgsl_process_private *);
 #else
 static inline void kgsl_core_debugfs_init(void) { }
 static inline void kgsl_device_debugfs_init(struct kgsl_device *device) { }
 static inline void kgsl_core_debugfs_close(void) { }
 static inline struct dentry *kgsl_get_debugfs_dir(void) { return NULL; }
+static inline int kgsl_process_init_debugfs(struct kgsl_process_private *)
+{
+	return 0;
+}
 
 #endif
 
