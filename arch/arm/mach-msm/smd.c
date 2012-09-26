c3e580d3c6f8ed0d8a5c8df6ec2e3a279cee8627
Jeff Hugo <jhugo@codeaurora.org>
2012-09-26 15:30:10 -0600

    msm: smd: add adsp restart notifier callback
    Starting with 8974, the proper name to register the SMD_Q6 subsystem
    restart notifier callback changed from "lpass" to "adsp".  Register an
    additional callback notifier under the "adsp" name to support both
    legacy targets and 8974.  Only one SMD_q6 callback will ever be called on
    a target.
    
    CRs-Fixed: 404860
    Change-Id: I57928b6e9f24e356db8971100be06ca3158b1f14
    Signed-off-by: Jeffrey Hugo <jhugo@codeaurora.org>

--- arch/arm/mach-msm/smd.c
+++ arch/arm/mach-msm/smd.c
@@ -3515,6 +3515,7 @@ static struct restart_notifier_block restart_notifiers[] = {
 	{SMD_WCNSS, "riva", .nb.notifier_call = restart_notifier_cb},
 	{SMD_DSPS, "dsps", .nb.notifier_call = restart_notifier_cb},
 	{SMD_MODEM, "gss", .nb.notifier_call = restart_notifier_cb},
+	{SMD_Q6, "adsp", .nb.notifier_call = restart_notifier_cb},
 };
 
 static int restart_notifier_cb(struct notifier_block *this,
