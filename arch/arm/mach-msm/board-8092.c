cb4dd00d88cb3677d7a1a9cd177b9bd9958c4315
Ravi Kumar V <kumarrav@codeaurora.org>
2012-09-28 15:08:41 +0530

    msm: 8092: Add QPNP interrupt support for 8092
    QPNP is a Qualcomm SPMI based implementation that specifies 256
    devices per SPMI slave ID, with up to 8 interrupts per device.
    This brings the theoretical max interrupt support to 32768 per
    bus for this architecture.
    
    Change-Id: Ie6f8719e313e5f4e04d1bf87c5d4b238c6f34396
    Signed-off-by: Ravi Kumar V <kumarrav@codeaurora.org>

--- arch/arm/mach-msm/board-8092.c
+++ arch/arm/mach-msm/board-8092.c
@@ -24,6 +24,7 @@
 #include <mach/socinfo.h>
 #include <mach/board.h>
 #include <mach/msm_memtypes.h>
+#include <mach/qpnp-int.h>
 #include <linux/io.h>
 #include <linux/gpio.h>
 #include <linux/irq.h>
@@ -34,6 +35,7 @@
 static struct of_device_id irq_match[] __initdata  = {
 	{ .compatible = "qcom,msm-qgic2", .data = gic_of_init, },
 	{ .compatible = "qcom,msm-gpio", .data = msm_gpio_of_init, },
+	{ .compatible = "qcom,spmi-pmic-arb", .data = qpnpint_of_init, },
 	{}
 };
 
