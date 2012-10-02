523a928c3ea5a308de1f15f89c4e9c97972365bf
Huaibin Yang <huaibiny@codeaurora.org>
2012-10-02 15:08:56 -0700

    msm_fb: display: wake up system before vsync for cmd panel
    Add this feature for cmd mode panel. The feature is to avoid frame
    drops due to power collapse and it is already present for other
    display interfaces.
    
    Change-Id: Ic68f41cd59931294b592577475ed7480459f2cf8
    Signed-off-by: Huaibin Yang <huaibiny@codeaurora.org>

--- drivers/video/msm/mdp4_overlay_dsi_cmd.c
+++ drivers/video/msm/mdp4_overlay_dsi_cmd.c
@@ -273,6 +273,8 @@ int mdp4_dsi_cmd_pipe_commit(int cndx, int wait)
 	pipe = vctrl->base_pipe;
 	mixer = pipe->mixer_num;
 
+	mdp_update_pm(vctrl->mfd, vctrl->vsync_time);
+
 	if (vp->update_cnt == 0) {
 		mutex_unlock(&vctrl->update_lock);
 		return cnt;
