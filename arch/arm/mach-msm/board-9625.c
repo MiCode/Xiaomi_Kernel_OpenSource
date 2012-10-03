46e5e2b77809b97b86a24299dae338fabbf715dc
Abhimanyu Kapur <abhimany@codeaurora.org>
2012-10-02 20:45:37 -0700

    msm: 9625: Enable the MSM restart driver
    The MSM restart driver allows MSM chipsets to reboot.
    Enable it for MSM 9625.
    
    Change-Id: Ie20a8f963b7018df08b489b599ed19cc12738eda
    Signed-off-by: Abhimanyu Kapur <abhimany@codeaurora.org>

--- arch/arm/mach-msm/board-9625.c
+++ arch/arm/mach-msm/board-9625.c
@@ -28,6 +28,7 @@
 #include <asm/mach/time.h>
 #include <mach/socinfo.h>
 #include <mach/board.h>
+#include <mach/restart.h>
 #include <mach/gpio.h>
 #include <mach/clk-provider.h>
 #include <mach/qpnp-int.h>
@@ -308,4 +309,5 @@ DT_MACHINE_START(MSM_DT, "Qualcomm MSM (Flattened Device Tree)")
 	.timer = &msm_dt_timer,
 	.dt_compat = msm9625_dt_match,
 	.reserve = msm9625_reserve,
+	.restart = msm_restart,
 MACHINE_END
