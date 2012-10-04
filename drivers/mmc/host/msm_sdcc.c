429b4a64716c6b878e7da0728029b62d7e45dbe9
Visweswara Kumar <visweswa@codeaurora.org>
2012-10-04 10:08:25 +0530

    Revert "mmc: msm_sdcc: REmove unused flag variables"
    This reverts commit 84d7de3a1d97a901467554d344c603a135402f54.

--- drivers/mmc/host/msm_sdcc.c
+++ drivers/mmc/host/msm_sdcc.c
@@ -3466,6 +3466,7 @@ static int msmsdcc_enable(struct mmc_host *mmc)
 {
 	struct device *dev = mmc->parent;
 	struct msmsdcc_host *host = mmc_priv(mmc);
+	unsigned long flags;
 	int rc = 0;
 
 	msmsdcc_pm_qos_update_latency(host, 1);
@@ -3499,6 +3500,7 @@ out:
 static int msmsdcc_disable(struct mmc_host *mmc)
 {
 	struct msmsdcc_host *host = mmc_priv(mmc);
+	unsigned long flags;
 	int rc = 0;
 
 	msmsdcc_pm_qos_update_latency(host, 0);
