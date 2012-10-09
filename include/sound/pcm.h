ddbe60b879175f7260a910e77fc8e9e9369de84f
Santosh Mardi <gsantosh@codeaurora.org>
2012-10-09 10:32:42 +0530

    ASoC: core: Update ALSA core to issue restart in underrun.
    When compressed and pcm session is started the buffer pull will
    Be from the DSP, in mmap mode for the scenario Hardware pointer
    Is same as application pointer implies application has not filled the
    Buffer to be send to DSP, and platform driver has to break and restart
    The de queue process when the application has sufficient buffers.
    
    When hardware pointer is same as application pointer it is an under run
    Scenario where the ALSA framework has to trigger the under run and in HAL
    The session has to re-prepare and re-triggered. based on the stop
    Threshold, but in our system the stop threshold is INT_MAX which
    Indicates that under run is not triggered in practical cases.
    
    We have a brodcast usecase in which the mentioned under run case
    Is hit mulitple times, and every time re-prepare and re-trigger will be
    Expensive
    
    Update the ASoC core framework to restart de queue process stopped
    By platform Driver when the under run is hit, based on the available
    Buffer and render flag, so the System will recover from the hang state.
    
    To restart de queue process the ASoC core will be calling the restart
    Callback registered by platform driver and this callback will be running in
    Atomic context.
    
    Change-Id: Ic9ea05a0dc6246346e9913493232882e2f5447d1
    Signed-off-by: Santosh Mardi <gsantosh@codeaurora.org>
    Signed-off-by: Meng Wang <mwang@codeaurora.org>

--- include/sound/pcm.h
+++ include/sound/pcm.h
@@ -91,6 +91,7 @@ struct snd_pcm_ops {
 			     unsigned long offset);
 	int (*mmap)(struct snd_pcm_substream *substream, struct vm_area_struct *vma);
 	int (*ack)(struct snd_pcm_substream *substream);
+	int (*restart)(struct snd_pcm_substream *substream);
 };
 
 /*
@@ -118,6 +119,12 @@ struct snd_pcm_ops {
 #define SNDRV_PCM_TRIGGER_DRAIN		7
 
 #define SNDRV_PCM_POS_XRUN		((snd_pcm_uframes_t)-1)
+
+#define SNDRV_DMA_MODE          (0)
+#define SNDRV_NON_DMA_MODE      (1 << 0)
+#define SNDRV_RENDER_STOPPED    (1 << 1)
+#define SNDRV_RENDER_RUNNING    (1 << 2)
+
 
 /* If you change this don't forget to change rates[] table in pcm_native.c */
 #define SNDRV_PCM_RATE_5512		(1<<0)		/* 5512Hz */
@@ -381,6 +388,7 @@ struct snd_pcm_runtime {
 	unsigned int rate_num;
 	unsigned int rate_den;
 	unsigned int no_period_wakeup: 1;
+	unsigned int render_flag;
 
 	/* -- SW params -- */
 	int tstamp_mode;		/* mmap timestamp is updated */
