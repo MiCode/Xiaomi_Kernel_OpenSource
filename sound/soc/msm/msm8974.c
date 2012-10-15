f0d9df478c0f6cc0d5aad0869a941993a5bf60d8
Joonwoo Park <joonwoop@codeaurora.org>
2012-10-15 16:05:23 -0700

    ASoC: wcd9xxx: integrate 4-pole extension cable support
    Commit 07b6bd6a9a1e431a86efa76b14030d882ee7771b is checked into wcd9310
    driver to support 4-pole extension cable detection.
    Integrate the commit to wcd9xxx-mbhc driver.
    
    Change-Id: I44973b5d9562b71b041498e3c9ad9709f4266181
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- sound/soc/msm/msm8974.c
+++ sound/soc/msm/msm8974.c
@@ -69,6 +69,7 @@ static struct wcd9xxx_mbhc_config mbhc_cfg = {
 	.gpio = 0,
 	.gpio_irq = 0,
 	.gpio_level_insert = 1,
+	.detect_extn_cable = true,
 	.insert_detect = true,
 	.swap_gnd_mic = NULL,
 };
