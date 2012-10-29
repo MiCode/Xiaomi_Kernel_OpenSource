ef99fc1f6136424306d6e19f2c1ed3bfb7b91d7a
Saket Saurabh <ssaurabh@codeaurora.org>
2012-10-29 19:51:28 +0530

    msm_serial_hs : handle uart_flush_buffer
    Serial core allocates circular buffer for uart tx transfer.
    Circular buffer head and tail is updated in serial core and
    circular buffer tail is updated in uart driver.While uart
    transfer is happening, uart client bluetooth hci_ldisc,calls
    uart_flush_buffer in serial core and sets circular buffer
    head and tail to zero.As uart driver does not know when
    uart_flush_buffer is called in serial core by uart client,it
    updates circular buffer tail for the previous ADM Tx completion.
    This leads to queueing Tx command to ADM although no data is
    there in circular buffer.Because of this,uart client is not
    functional.
    
    Hence adding msm_hs_flush_buffer api which notifies to uart driver
    that uart_flush_buffer is called in serial core by uart client and
    set tty_flush_receive flag to true.In uart interrupt handler ,do
    not update circular buffer tail if uart_flush_buffer is already
    called in serial core by uart client else update circular buffer
    tail on ADM Tx completion.
    
    CRs-Fixed: 419054
    Change-Id: Ide13960bd5f9c4c374cadfab2a9860e928ef2e1c
    Signed-off-by: Saket Saurabh <ssaurabh@codeaurora.org>

--- drivers/tty/serial/msm_serial_hs.c
+++ drivers/tty/serial/msm_serial_hs.c
@@ -168,6 +168,7 @@ struct msm_hs_port {
 	struct work_struct clock_off_w; /* work for actual clock off */
 	struct workqueue_struct *hsuart_wq; /* hsuart workqueue */
 	struct mutex clk_mutex; /* mutex to guard against clock off/clock on */
+	bool tty_flush_receive;
 };
 
 #define MSM_UARTDM_BURST_SIZE 16   /* DM burst size (in bytes) */
@@ -1251,6 +1252,13 @@ static void msm_hs_enable_ms_locked(struct uart_port *uport)
 
 }
 
+static void msm_hs_flush_buffer(struct uart_port *uport)
+{
+	struct msm_hs_port *msm_uport = UARTDM_TO_MSM(uport);
+
+	msm_uport->tty_flush_receive = true;
+}
+
 /*
  *  Standard API, Break Signal
  *
@@ -1466,7 +1474,14 @@ static irqreturn_t msm_hs_isr(int irq, void *dev)
 		 */
 		mb();
 		/* Complete DMA TX transactions and submit new transactions */
-		tx_buf->tail = (tx_buf->tail + tx->tx_count) & ~UART_XMIT_SIZE;
+
+		/* Do not update tx_buf.tail if uart_flush_buffer already
+						called in serial core */
+		if (!msm_uport->tty_flush_receive)
+			tx_buf->tail = (tx_buf->tail +
+					tx->tx_count) & ~UART_XMIT_SIZE;
+		else
+			msm_uport->tty_flush_receive = false;
 
 		tx->dma_in_flight = 0;
 
@@ -2210,6 +2225,7 @@ static struct uart_ops msm_hs_ops = {
 	.config_port = msm_hs_config_port,
 	.release_port = msm_hs_release_port,
 	.request_port = msm_hs_request_port,
+	.flush_buffer = msm_hs_flush_buffer,
 };
 
 module_init(msm_serial_hs_init);
