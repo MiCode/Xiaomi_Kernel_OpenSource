1073b45c0b120f2ebbe1b5034d4088f9294b54af
Kuogee Hsieh <khsieh@codeaurora.org>
2012-10-30 18:26:00 -0700

    msm_fb: display: fix frame buffer's ref count leak
    This patch clear MDP_MEMORY_ID_TYPE_FB from pipe's
    flag so that fput_light() and fget_light() can be applied
    to frame buffer evenly at end of pipe operation. Otherwise
    there has kernel unable to handle NULL pointer panic caused
    by frame buffer's ref count leak.
    
    CRs-fixed: 411394
    Change-Id: Ib2b72e6277480b5ace23cda554be485d9d477b3d
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -192,12 +192,19 @@ void mdp4_overlay_iommu_pipe_free(int ndx, int all)
 		return;
 
 	if (pipe->flags & MDP_MEMORY_ID_TYPE_FB) {
-		if (pipe->put0_need)
+		pipe->flags &= ~MDP_MEMORY_ID_TYPE_FB;
+		if (pipe->put0_need) {
 			fput_light(pipe->srcp0_file, pipe->put0_need);
-		if (pipe->put1_need)
+			pipe->put0_need = 0;
+		}
+		if (pipe->put1_need) {
 			fput_light(pipe->srcp1_file, pipe->put1_need);
-		if (pipe->put2_need)
+			pipe->put1_need = 0;
+		}
+		if (pipe->put2_need) {
 			fput_light(pipe->srcp2_file, pipe->put2_need);
+			pipe->put2_need = 0;
+		}
 
 		pr_debug("%s: ndx=%d flags=%x put=%d\n", __func__,
 			pipe->pipe_ndx, pipe->flags, pipe->put0_need);
