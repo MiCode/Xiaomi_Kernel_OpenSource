9a2f0eb2a3d66d77ad98765bb448c43015031ad8
Maheshwar Ajja <majja@codeaurora.org>
2012-10-31 10:04:28 +0530

    msm:vidc: Amend error checks on ION API failures
    This change will validate the ION API failures
    to fix target freeze issues.
    
    Change-Id: I00fee94955ba9f0e72734daccd22aa7d0bf86dda
    CRs-fixed: 412958
    Signed-off-by: Maheshwar Ajja <majja@codeaurora.org>

--- drivers/video/msm/vidc/720p/resource_tracker/vcd_res_tracker.c
+++ drivers/video/msm/vidc/720p/resource_tracker/vcd_res_tracker.c
@@ -681,6 +681,10 @@ static struct ion_client *res_trk_create_ion_client(void){
 	struct ion_client *video_client;
 	VCDRES_MSG_LOW("%s", __func__);
 	video_client = msm_ion_client_create(-1, "video_client");
+	if (IS_ERR_OR_NULL(video_client)) {
+		VCDRES_MSG_ERROR("%s: Unable to create ION client\n", __func__);
+		video_client = NULL;
+	}
 	return video_client;
 }
 
