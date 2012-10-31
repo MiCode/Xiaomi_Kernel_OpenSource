c86e25f0f775d7effaf192ded6fb7a6885694d7b
Pushkar Joshi <pushkarj@codeaurora.org>
2012-10-31 13:39:26 -0700

    tracing: have trace_printk process arguments before storing
    trace_printk records its arguments in the internal ring buffer for
    them to be processed later. The mapping between the format string
    for the particular instance of a trace_printk call and the
    arguments is maintained using the instruction pointer internally
    in the kernel. However, this makes it harder for an external tool
    to process this data, which is necessary when the same data
    is logged to the STM and needs to be processed externally.
    
    Change-Id: Ifa2ae5fe1f1fd62c85fac432a581737450781f88
    Signed-off-by: Pushkar Joshi <pushkarj@codeaurora.org>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -486,7 +486,7 @@ do {									\
 		  __attribute__((section("__trace_printk_fmt"))) =	\
 			__builtin_constant_p(fmt) ? fmt : NULL;		\
 									\
-		__trace_bprintk(_THIS_IP_, trace_printk_fmt, ##args);	\
+		__trace_printk(_THIS_IP_, trace_printk_fmt, ##args);	\
 	} else								\
 		__trace_printk(_THIS_IP_, fmt, ##args);		\
 } while (0)
