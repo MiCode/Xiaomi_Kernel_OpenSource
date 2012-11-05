5b5c38c304aaa99c81dd085bb74ab590c7fce93c
Vladimir Razgulin <vrazguli@codeaurora.org>
2012-11-05 11:39:55 -0700

    msm: kgsl: Add memfree_history debug feature
    A kgsl user can free up an allocation of GPU memory with
    kgsl_ioctl_sharedmem_free() function, but the GPU address
    might still be in use resulting in a GPU page fault.
    A bug of this kind is hard to find because the driver keeps
    no trace of kgsl_ioctl_sharedmem_free calls.
    This change implements a "memfree history" debug feture that
    allows to see an information about a last couple of hundred
    memory free calls in kernel/debug/kgsl/kgsl-3d0/memfree_history
    file.
    
    Change-Id: I12e0e3702db56d99d5de644739b364dea4cc37b1
    Signed-off-by: Vladimir Razgulin <vrazguli@codeaurora.org>

--- drivers/gpu/msm/kgsl.h
+++ drivers/gpu/msm/kgsl.h
@@ -71,6 +71,23 @@
 #define KGSL_STATS_ADD(_size, _stat, _max) \
 	do { _stat += (_size); if (_stat > _max) _max = _stat; } while (0)
 
+
+#define KGSL_MEMFREE_HIST_SIZE	((int)(PAGE_SIZE * 2))
+
+struct kgsl_memfree_hist_elem {
+	unsigned int pid;
+	unsigned int gpuaddr;
+	unsigned int size;
+	unsigned int flags;
+};
+
+struct kgsl_memfree_hist {
+	void *base_hist_rb;
+	unsigned int size;
+	struct kgsl_memfree_hist_elem *wptr;
+};
+
+
 struct kgsl_device;
 
 struct kgsl_driver {
@@ -97,6 +114,9 @@ struct kgsl_driver {
 	struct mutex devlock;
 
 	void *ptpool;
+
+	struct mutex memfree_hist_mutex;
+	struct kgsl_memfree_hist memfree_hist;
 
 	struct {
 		unsigned int vmalloc;
