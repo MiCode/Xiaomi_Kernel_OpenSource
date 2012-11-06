3e598a936aaf5b141d183df547a1f77f40f364a5
Jeff Hugo <jhugo@codeaurora.org>
2012-11-06 13:18:25 -0700

    msm: smd: notify RPM processor of SMD SSR events
    When SSR occurs, SMD may update channels interacting with the RPM
    processor, which requires Apps to send interrupt notifications to RPM
    in order for RPM to process any such updates.
    
    Change-Id: Iaaa3411553c38c14414684c563532ea14329aa2b
    Signed-off-by: Jeffrey Hugo <jhugo@codeaurora.org>

--- arch/arm/mach-msm/smd.c
+++ arch/arm/mach-msm/smd.c
@@ -1015,6 +1015,7 @@ void smd_channel_reset(uint32_t restart_pid)
 	notify_dsp_smd();
 	notify_dsps_smd();
 	notify_wcnss_smd();
+	notify_rpm_smd();
 
 	/* change all remote states to CLOSED */
 	mutex_lock(&smd_probe_lock);
@@ -1030,6 +1031,7 @@ void smd_channel_reset(uint32_t restart_pid)
 	notify_dsp_smd();
 	notify_dsps_smd();
 	notify_wcnss_smd();
+	notify_rpm_smd();
 
 	SMD_DBG("%s: finished reset\n", __func__);
 }
