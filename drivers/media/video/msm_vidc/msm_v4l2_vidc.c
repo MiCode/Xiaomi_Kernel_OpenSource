c4e0e5de4869c0bc3c91fddb0657c93bc997b324
Vinay Kalia <vkalia@codeaurora.org>
2012-11-06 15:50:10 -0800

    msm: vidc: Remove SSR init/uninit calls
    SSR architecture has changed so driver does
    not need to call ssr init/uninit anymore.
    Changes need to be made according to new
    architecture. Removing the old code.
    
    Change-Id: I5a4334a4da604a78775864a310f2a12378a8de83
    Signed-off-by: Vinay Kalia <vkalia@codeaurora.org>

--- drivers/media/video/msm_vidc/msm_v4l2_vidc.c
+++ drivers/media/video/msm_vidc/msm_v4l2_vidc.c
@@ -29,7 +29,6 @@
 #include "msm_vidc_debug.h"
 #include "vidc_hal_api.h"
 #include "msm_smem.h"
-#include "msm_vidc_ssr.h"
 
 #define BASE_DEVICE_NUMBER 32
 #define SHARED_QSIZE 0x1000000
@@ -1383,9 +1382,6 @@ static int __devinit msm_vidc_probe(struct platform_device *pdev)
 	core->debugfs_root = msm_vidc_debugfs_init_core(
 		core, vidc_driver->debugfs_root);
 	pdev->dev.platform_data = core;
-	rc = msm_vidc_ssr_init(core);
-	if (rc < 0)
-		dprintk(VIDC_ERR, "msm_vidc : Sub Systrem Restart failed\n");
 	return rc;
 
 err_cores_exceeded:
@@ -1414,7 +1410,6 @@ static int __devexit msm_vidc_remove(struct platform_device *pdev)
 	vidc_hal_delete_device(core->device);
 	video_unregister_device(&core->vdev[MSM_VIDC_ENCODER].vdev);
 	video_unregister_device(&core->vdev[MSM_VIDC_DECODER].vdev);
-	rc = msm_vidc_ssr_uninit(core);
 	v4l2_device_unregister(&core->v4l2_dev);
 	if (core->resources.ocmem.handle)
 		ocmem_notifier_unregister(core->resources.ocmem.handle,
