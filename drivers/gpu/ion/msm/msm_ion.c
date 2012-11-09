51b29448739b549eb691f9650fc20a280dbccd10
Mitchel Humpherys <mitchelh@codeaurora.org>
2012-11-09 13:03:26 -0800

    gpu: ion: fix race condition in checking virtual address
    Currently, on an Ion cache operation, there is a short period of time
    between checking the validity of the virtual address range of the
    operation and the actual cache operation. During this time another
    thread could twiddle the vm areas, resulting in an invalid cache
    operation. Fix this by holding mmap_sem of the active_mm of the
    current thread across the address range check and the cache operation.
    
    CRs-fixed: 418933
    Change-Id: I859e2e3d888dea38a26d04958c1577522471a62c
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- drivers/gpu/ion/msm/msm_ion.c
+++ drivers/gpu/ion/msm/msm_ion.c
@@ -616,18 +616,15 @@ static int check_vaddr_bounds(unsigned long start, unsigned long end)
 	if (end < start)
 		goto out;
 
-	down_read(&mm->mmap_sem);
 	vma = find_vma(mm, start);
 	if (vma && vma->vm_start < end) {
 		if (start < vma->vm_start)
-			goto out_up;
+			goto out;
 		if (end > vma->vm_end)
-			goto out_up;
+			goto out;
 		ret = 0;
 	}
 
-out_up:
-	up_read(&mm->mmap_sem);
 out:
 	return ret;
 }
@@ -645,19 +642,11 @@ static long msm_ion_custom_ioctl(struct ion_client *client,
 		unsigned long start, end;
 		struct ion_handle *handle = NULL;
 		int ret;
+		struct mm_struct *mm = current->active_mm;
 
 		if (copy_from_user(&data, (void __user *)arg,
 					sizeof(struct ion_flush_data)))
 			return -EFAULT;
-
-		start = (unsigned long) data.vaddr;
-		end = (unsigned long) data.vaddr + data.length;
-
-		if (check_vaddr_bounds(start, end)) {
-			pr_err("%s: virtual address %p is out of bounds\n",
-				__func__, data.vaddr);
-			return -EINVAL;
-		}
 
 		if (!data.handle) {
 			handle = ion_import_dma_buf(client, data.fd);
@@ -668,10 +657,26 @@ static long msm_ion_custom_ioctl(struct ion_client *client,
 			}
 		}
 
+		down_read(&mm->mmap_sem);
+
+		start = (unsigned long) data.vaddr;
+		end = (unsigned long) data.vaddr + data.length;
+
+		if (check_vaddr_bounds(start, end)) {
+			up_read(&mm->mmap_sem);
+			pr_err("%s: virtual address %p is out of bounds\n",
+				__func__, data.vaddr);
+			if (!data.handle)
+				ion_free(client, handle);
+			return -EINVAL;
+		}
+
 		ret = ion_do_cache_op(client,
 				data.handle ? data.handle : handle,
 				data.vaddr, data.offset, data.length,
 				cmd);
+
+		up_read(&mm->mmap_sem);
 
 		if (!data.handle)
 			ion_free(client, handle);
