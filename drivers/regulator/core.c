f6acc6cffbc18831831e7769920ce6b31caf7a2a
David Collins <collinsd@codeaurora.org>
2012-11-13 16:14:53 -0800

    regulator: Correct voltage value used in regulator_set_voltage_complete
    The voltage value specified in the tracepoint function
    trace_regulator_set_voltage_complete() inside of the function
    _regulator_do_set_voltage() is erroneously set to -1 if a
    list_voltage() callback is not specified by a regulator device
    even if a get_voltage() callback is present.
    
    Add a check for the get_voltage() callback function and call it
    in order to determine the voltage that was chosen from the min_uV
    to max_uV range.
    
    Change-Id: Idea8de9b921494c22029496e3b20e6535d0b9f27
    Signed-off-by: David Collins <collinsd@codeaurora.org>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -1897,6 +1897,8 @@ static int _regulator_do_set_voltage(struct regulator_dev *rdev,
 		if (rdev->desc->ops->list_voltage)
 			selector = rdev->desc->ops->list_voltage(rdev,
 								 selector);
+		else if (rdev->desc->ops->get_voltage)
+			selector = rdev->desc->ops->get_voltage(rdev);
 		else
 			selector = -1;
 	} else if (rdev->desc->ops->set_voltage_sel) {
