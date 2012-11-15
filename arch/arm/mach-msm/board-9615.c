a5955ecbecd5417fc5f7a4e53f6ed1a04df169d4
Prashanth Reddy <pgreddy@codeaurora.org>
2012-11-14 20:03:25 -0800

    msm: 9615: Add platform data for in-call playback and recording BE DAIs
    Add platform data for in-call playback and recording BE DAIs which are
    needed to support in-call playback and in-call recording in a voice call.
    
    CRs-Fixed: 411475, 411476
    Change-Id: I0ef45b2a1c3780c85cad2dcbbf03c6bfb1b2d25a
    Signed-off-by: Prashanth Reddy <pgreddy@codeaurora.org>

--- arch/arm/mach-msm/board-9615.c
+++ arch/arm/mach-msm/board-9615.c
@@ -925,6 +925,9 @@ static struct platform_device *common_devices[] = {
 	&msm_cpudai_sec_auxpcm_rx,
 	&msm_cpudai_sec_auxpcm_tx,
 	&msm_cpudai_stub,
+	&msm_cpudai_incall_music_rx,
+	&msm_cpudai_incall_record_rx,
+	&msm_cpudai_incall_record_tx,
 
 #if defined(CONFIG_CRYPTO_DEV_QCRYPTO) || \
 		defined(CONFIG_CRYPTO_DEV_QCRYPTO_MODULE)
