78917dfc07a4f13d32b61269ae04e27f5b2eb1e5
Willie Ruan <wruan@codeaurora.org>
2012-11-15 15:55:15 -0800

    mfd: pm8821-core: fix a potential NULL dereference
    Null pointer of pmic may be dereferenced for pmic->irq_chip.
    
    Change-Id: I2751038a7e8a54726078f2f09e4e91d162b74386
    Signed-off-by: Willie Ruan <wruan@codeaurora.org>

--- drivers/mfd/pm8821-core.c
+++ drivers/mfd/pm8821-core.c
@@ -312,11 +312,11 @@ static int pm8821_remove(struct platform_device *pdev)
 	drvdata = platform_get_drvdata(pdev);
 	if (drvdata)
 		pmic = drvdata->pm_chip_data;
-	if (pmic)
-		mfd_remove_devices(pmic->dev);
-	if (pmic->irq_chip) {
-		pm8821_irq_exit(pmic->irq_chip);
-		pmic->irq_chip = NULL;
+	if (pmic) {
+		if (pmic->dev)
+			mfd_remove_devices(pmic->dev);
+		if (pmic->irq_chip)
+			pm8821_irq_exit(pmic->irq_chip);
 	}
 	platform_set_drvdata(pdev, NULL);
 	kfree(pmic);
