96b57c676c121a361490a438785d81c4504869da
Mohan Kumar Gubbihalli Lachma Naik <mgubbi@codeaurora.org>
2012-11-15 18:51:51 -0800

    ASOC: msm: Handle Modem SSR
    clean up mvm, cvp and cvs handles during ssr while
    in voice call or after the voice call. Without doing
    this, after modem boots back on first voice call, it
    will create another MVM session and the session which was
    owned by Linux Audio will be stale.
    
    CRs-fixed: 411497
    Change-Id: I288359740eb936c3fab0a4c43f73578c085229db
    Signed-off-by: Mohan Kumar Gubbihalli Lachma Naik <mgubbi@codeaurora.org>

--- sound/soc/msm/qdsp6v2/q6voice.h
+++ sound/soc/msm/qdsp6v2/q6voice.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2012, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -67,6 +67,7 @@ enum {
 	VOC_RUN,
 	VOC_CHANGE,
 	VOC_RELEASE,
+	VOC_ERROR,
 };
 
 struct mem_buffer {
