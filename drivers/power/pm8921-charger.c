9be59cd2dbb6b3ed7568f71bfae4318ab2fc1bcb
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2012-11-19 13:03:00 -0800

    power: pm8921-charger: disable battery temperature wakeup
    The battery temperature interrupts are functional only when there is a
    valid charger source present. There is no point making it a wakeup
    interrupt, since if the charger is absent the interrupt wont be
    triggered (if it is triggered it should be ignored).
    
    Note that for 8921 charging, the software does not take any action if
    the battery gets hot/cold. The 8921 hardware state machine stops
    charging if the battery temperature goes out of the limits.
    
    For external chargers which depend on the temperature monitoring of
    8921, we rely on the fact that the external charger will hold a wakelock
    and avoid sleep as long as charging is in progress.
    
    In cases when external charging is finished and the system did go into
    suspend, we don't want to wakeup the device only to realize that external
    charging was not in progress.
    
    Basically no case requires that we wakeup from battery temperature
    interrupts.
    
    Change-Id: Idf2cf7f40780cf46ba95228764790b818bc72db5
    CRs-Fixed: 395312
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- drivers/power/pm8921-charger.c
+++ drivers/power/pm8921-charger.c
@@ -4579,7 +4579,6 @@ static int pm8921_charger_probe(struct platform_device *pdev)
 
 	enable_irq_wake(chip->pmic_chg_irq[USBIN_VALID_IRQ]);
 	enable_irq_wake(chip->pmic_chg_irq[DCIN_VALID_IRQ]);
-	enable_irq_wake(chip->pmic_chg_irq[BAT_TEMP_OK_IRQ]);
 	enable_irq_wake(chip->pmic_chg_irq[VBATDET_LOW_IRQ]);
 	enable_irq_wake(chip->pmic_chg_irq[FASTCHG_IRQ]);
 
