553621771edd30ffbd0bd0926cb546fbe59a4287
Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
2012-11-20 14:56:44 -0800

    msm: 8226: add support for machine restart
    Add support for SOC reset in case of a kernel panic or a reboot
    command.
    
    Change-Id: I386dbf99cf110c4fa1ce38562f0de51b79077e27
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>

--- arch/arm/mach-msm/board-8226.c
+++ arch/arm/mach-msm/board-8226.c
@@ -32,6 +32,7 @@
 #include <mach/board.h>
 #include <mach/gpiomux.h>
 #include <mach/msm_iomap.h>
+#include <mach/restart.h>
 #ifdef CONFIG_ION_MSM
 #include <mach/ion.h>
 #endif
@@ -125,5 +126,6 @@ DT_MACHINE_START(MSM8226_DT, "Qualcomm MSM 8226 (Flattened Device Tree)")
 	.init_machine = msm8226_init,
 	.dt_compat = msm8226_dt_match,
 	.reserve = msm8226_reserve,
-	.init_very_early = msm8226_early_memory
+	.init_very_early = msm8226_early_memory,
+	.restart = msm_restart,
 MACHINE_END
