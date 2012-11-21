20d985ff33593565d309131b38739f14312d6bef
Pavankumar Kondeti <pkondeti@codeaurora.org>
2012-11-21 14:52:15 +0530

    USB: diag: Add stubs for USB-DIAG API
    Diag char driver can support SD card, UART and socket transports
    apart from USB.  Add stubs for USB-DIAG API to make diag char
    independent of USB. Remove unused diag_read_from_cb().
    
    CRs-Fixed: 423153
    Change-Id: Id1562f9738b6f0cf5434e4dd54ce943373d1322f
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- arch/arm/mach-msm/include/mach/usbdiag.h
+++ arch/arm/mach-msm/include/mach/usbdiag.h
@@ -1,6 +1,6 @@
 /* include/asm-arm/arch-msm/usbdiag.h
  *
- * Copyright (c) 2008-2010, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2008-2010, 2012, The Linux Foundation. All rights reserved.
  *
  * All source code in this file is licensed under the following license except
  * where indicated.
@@ -20,6 +20,8 @@
 
 #ifndef _DRIVERS_USB_DIAG_H_
 #define _DRIVERS_USB_DIAG_H_
+
+#include <linux/err.h>
 
 #define DIAG_LEGACY		"diag"
 #define DIAG_MDM		"diag_mdm"
@@ -45,6 +47,7 @@ struct usb_diag_ch {
 	void *priv_usb;
 };
 
+#ifdef CONFIG_USB_G_ANDROID
 struct usb_diag_ch *usb_diag_open(const char *name, void *priv,
 		void (*notify)(void *, unsigned, struct diag_request *));
 void usb_diag_close(struct usb_diag_ch *ch);
@@ -52,7 +55,32 @@ int usb_diag_alloc_req(struct usb_diag_ch *ch, int n_write, int n_read);
 void usb_diag_free_req(struct usb_diag_ch *ch);
 int usb_diag_read(struct usb_diag_ch *ch, struct diag_request *d_req);
 int usb_diag_write(struct usb_diag_ch *ch, struct diag_request *d_req);
-
-int diag_read_from_cb(unsigned char * , int);
-
+#else
+static inline struct usb_diag_ch *usb_diag_open(const char *name, void *priv,
+		void (*notify)(void *, unsigned, struct diag_request *))
+{
+	return ERR_PTR(-ENODEV);
+}
+static inline void usb_diag_close(struct usb_diag_ch *ch)
+{
+}
+static inline
+int usb_diag_alloc_req(struct usb_diag_ch *ch, int n_write, int n_read)
+{
+	return -ENODEV;
+}
+static inline void usb_diag_free_req(struct usb_diag_ch *ch)
+{
+}
+static inline
+int usb_diag_read(struct usb_diag_ch *ch, struct diag_request *d_req)
+{
+	return -ENODEV;
+}
+static inline
+int usb_diag_write(struct usb_diag_ch *ch, struct diag_request *d_req)
+{
+	return -ENODEV;
+}
+#endif /* CONFIG_USB_G_ANDROID */
 #endif /* _DRIVERS_USB_DIAG_H_ */
