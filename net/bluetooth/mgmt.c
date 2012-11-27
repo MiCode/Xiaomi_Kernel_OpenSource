1272761d3a7e6262cad5b05a05b98b6b24bf21d6
Ram Mohan Korukonda <rkorukon@codeaurora.org>
2012-11-27 16:13:54 +0530

    Bluetooth: Support for Remote name request cancel command
    Add support for remote name request cancel command in mgmtops
    plugin.
    
    Change-Id: If8bec8b457461f7bca90af940d2473b810677322
    CRs-fixed: 414884
    Signed-off-by: Ram Mohan Korukonda <rkorukon@codeaurora.org>

--- net/bluetooth/mgmt.c
+++ net/bluetooth/mgmt.c
@@ -1956,6 +1956,38 @@ failed:
 	return err;
 }
 
+static int cancel_resolve_name(struct sock *sk, u16 index, unsigned char *data,
+								u16 len)
+{
+	struct mgmt_cp_cancel_resolve_name *mgmt_cp = (void *) data;
+	struct hci_cp_remote_name_req_cancel hci_cp;
+	struct hci_dev *hdev;
+	int err;
+
+	BT_DBG("");
+
+	if (len != sizeof(*mgmt_cp))
+		return cmd_status(sk, index, MGMT_OP_CANCEL_RESOLVE_NAME,
+								EINVAL);
+
+	hdev = hci_dev_get(index);
+	if (!hdev)
+		return cmd_status(sk, index, MGMT_OP_CANCEL_RESOLVE_NAME,
+								ENODEV);
+
+	hci_dev_lock_bh(hdev);
+
+	memset(&hci_cp, 0, sizeof(hci_cp));
+	bacpy(&hci_cp.bdaddr, &mgmt_cp->bdaddr);
+	err = hci_send_cmd(hdev, HCI_OP_REMOTE_NAME_REQ_CANCEL, sizeof(hci_cp),
+								&hci_cp);
+
+	hci_dev_unlock_bh(hdev);
+	hci_dev_put(hdev);
+
+	return err;
+}
+
 static int set_connection_params(struct sock *sk, u16 index,
 				unsigned char *data, u16 len)
 {
@@ -2189,9 +2221,10 @@ void mgmt_inquiry_complete_evt(u16 index, u8 status)
 	struct mgmt_mode cp = {0};
 	int err = -1;
 
-	BT_DBG("");
-
 	hdev = hci_dev_get(index);
+
+	if (hdev)
+		BT_DBG("disco_state: %d", hdev->disco_state);
 
 	if (!hdev || !lmp_le_capable(hdev)) {
 
@@ -2199,6 +2232,8 @@ void mgmt_inquiry_complete_evt(u16 index, u8 status)
 						discovery_terminated, NULL);
 
 		mgmt_event(MGMT_EV_DISCOVERING, index, &cp, sizeof(cp), NULL);
+
+		hdev->disco_state = SCAN_IDLE;
 
 		if (hdev)
 			goto done;
@@ -2315,6 +2350,7 @@ static int start_discovery(struct sock *sk, u16 index)
 	if (!hdev)
 		return cmd_status(sk, index, MGMT_OP_START_DISCOVERY, ENODEV);
 
+	BT_DBG("disco_state: %d", hdev->disco_state);
 	hci_dev_lock_bh(hdev);
 
 	if (hdev->disco_state && timer_pending(&hdev->disco_timer)) {
@@ -2394,6 +2430,8 @@ static int stop_discovery(struct sock *sk, u16 index)
 	hdev = hci_dev_get(index);
 	if (!hdev)
 		return cmd_status(sk, index, MGMT_OP_STOP_DISCOVERY, ENODEV);
+
+	BT_DBG("disco_state: %d", hdev->disco_state);
 
 	hci_dev_lock_bh(hdev);
 
@@ -2658,6 +2696,9 @@ int mgmt_control(struct sock *sk, struct msghdr *msg, size_t msglen)
 		break;
 	case MGMT_OP_RESOLVE_NAME:
 		err = resolve_name(sk, index, buf + sizeof(*hdr), len);
+		break;
+	case MGMT_OP_CANCEL_RESOLVE_NAME:
+		err = cancel_resolve_name(sk, index, buf + sizeof(*hdr), len);
 		break;
 	case MGMT_OP_SET_CONNECTION_PARAMS:
 		err = set_connection_params(sk, index, buf + sizeof(*hdr), len);
