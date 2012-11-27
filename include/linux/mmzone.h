1cf9ec2094c9d1074b07b6d2f251ed9ea0b7723e
Laura Abbott <lauraa@codeaurora.org>
2012-11-27 10:17:24 -0800

    mm: Add is_cma_pageblock definition
    Bring back the is_cma_pageblock definition for determining if a
    page is CMA or not.
    
    Change-Id: I39fd546e22e240b752244832c79514f109c8e84b
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -72,8 +72,10 @@ enum {
 extern int *get_migratetype_fallbacks(int mtype);
 
 #ifdef CONFIG_CMA
+bool is_cma_pageblock(struct page *page);
 #  define is_migrate_cma(migratetype) unlikely((migratetype) == MIGRATE_CMA)
 #else
+#  define is_cma_pageblock(page) false
 #  define is_migrate_cma(migratetype) false
 #endif
 
