75d0b9354ff48f23533a0818328446f1af98f6dc
Deepa Madiregama <dmadireg@codeaurora.org>
2012-11-28 14:57:43 +0530

    msm: audio: qdsp6v2: Fix seek issue in tunnel player
    - Seeking to 0 position during tunnel playback, music stream
      jumps to incorrect postion.
    - Initial buffer after seek to new position is corrupted as
      flush ioctl is not synchronous. Ioctl FLUSH is posting
      CMD_FLUSH to DSP, but WRITE_DONE to user space are posted
      even before it receives FLUSH ack from DSP.
    - Fix is to wait for FLUSH ack from DSP, then post the
      WRITE_DONE events to user space, which will ensure FLUSH
      is completely handled.
    
    CRs-Fixed: 425420
    Change-Id: Ieca3353dba3a67af39a2967659034f073acf5277
    Signed-off-by: Deepa Madiregama <dmadireg@codeaurora.org>

--- arch/arm/mach-msm/qdsp6v2/audio_utils_aio.c
+++ arch/arm/mach-msm/qdsp6v2/audio_utils_aio.c
@@ -1,6 +1,6 @@
 /* Copyright (C) 2008 Google, Inc.
  * Copyright (C) 2008 HTC Corporation
- * Copyright (c) 2009-2012, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2009-2013, Code Aurora Forum. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -1031,7 +1031,7 @@ static int audio_aio_buf_add(struct q6audio_aio *audio, unsigned dir,
 	return 0;
 }
 
-static void audio_aio_ioport_reset(struct q6audio_aio *audio)
+void audio_aio_ioport_reset(struct q6audio_aio *audio)
 {
 	if (audio->drv_status & ADRV_STATUS_AIO_INTF) {
 		/* If fsync is in progress, make sure
@@ -1205,15 +1205,15 @@ long audio_aio_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 				audio, audio->ac->session);
 		mutex_lock(&audio->lock);
 		audio->stopped = 1;
-		audio_aio_flush(audio);
-		audio->enabled = 0;
-		audio->drv_status &= ~ADRV_STATUS_PAUSE;
+		rc = audio_aio_flush(audio);
 		if (rc < 0) {
 			pr_err("%s[%p]:Audio Stop procedure failed rc=%d\n",
 				__func__, audio, rc);
 			mutex_unlock(&audio->lock);
 			break;
 		}
+		audio->enabled = 0;
+		audio->drv_status &= ~ADRV_STATUS_PAUSE;
 		if (audio->drv_status & ADRV_STATUS_FSYNC) {
 			pr_debug("%s[%p] Waking up the audio_aio_fsync\n",
 					__func__, audio);
@@ -1262,8 +1262,6 @@ long audio_aio_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 		}
 		/* Flush DSP */
 		rc = audio_aio_flush(audio);
-		/* Flush input / Output buffer in software*/
-		audio_aio_ioport_reset(audio);
 		if (rc < 0) {
 			pr_err("%s[%p]:AUDIO_FLUSH interrupted\n",
 				__func__, audio);
