cd90bbce0fc065ade44155a5d60c2c9fc013127b
Adrian Salido-Moreno <adrianm@codeaurora.org>
2012-11-28 18:41:33 -0800

    msm: mdss: support panel resolution changes
    Allow framebuffer resolution changes to be propagated to panel driver to
    support different display resolutions. Use a new panel event so that
    panel driver can take the decision of accepting new params.
    
    Change-Id: I04b26cbb4d06a3cbc9c4416fbb7202bb185eee9b
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>
    Signed-off-by: Ujwal Patel <ujwalp@codeaurora.org>

--- drivers/video/msm/mdss/mdss_wb.c
+++ drivers/video/msm/mdss/mdss_wb.c
@@ -25,11 +25,49 @@
 
 #include "mdss_panel.h"
 
+/**
+ * mdss_wb_check_params - check new panel info params
+ * @pdata: current panel information
+ * @new: updates to panel info
+ *
+ * Checks if there are any changes that require panel reconfiguration
+ * in order to be reflected on writeback buffer.
+ *
+ * Return negative errno if invalid input, zero if there is no panel reconfig
+ * needed and non-zero if reconfiguration is needed.
+ */
+static int mdss_wb_check_params(struct mdss_panel_data *pdata,
+	struct mdss_panel_info *new)
+{
+	struct mdss_panel_info *old;
+
+	if (!pdata || !new) {
+		pr_err("%s: Invalid input\n", __func__);
+		return -EINVAL;
+	}
+
+	old = &pdata->panel_info;
+
+	if ((old->xres != new->xres) || (old->yres != new->yres))
+		return 1;
+
+	return 0;
+}
+
 static int mdss_wb_event_handler(struct mdss_panel_data *pdata,
 				 int event, void *arg)
 {
-	pr_debug("%s: event=%d\n", __func__, event);
-	return 0;
+	int rc = 0;
+
+	switch (event) {
+	case MDSS_EVENT_CHECK_PARAMS:
+		rc = mdss_wb_check_params(pdata, (struct mdss_panel_info *)arg);
+		break;
+	default:
+		pr_debug("%s: panel event (%d) not handled\n", __func__, event);
+		break;
+	}
+	return rc;
 }
 
 static int mdss_wb_parse_dt(struct platform_device *pdev,
