99ea6cf93f21fc50b602bc6d0b764019e641ce88
Kiran Kumar H N <hurlisal@codeaurora.org>
2012-12-03 14:32:02 -0800

    msm: camera: Update the bus vector entry for video usecase.
    The current bandwidth requested in video recording usecase is not
    sufficient for all resolutions. Update it as per the system team's
    recommendation to avoid bus overflow issues.
    
    Change-Id: I7b26d39bc5c794cbe10d0d9e809c9e17d80bf2c2
    Signed-off-by: Kiran Kumar H N <hurlisal@codeaurora.org>

--- arch/arm/mach-msm/board-8930-camera.c
+++ arch/arm/mach-msm/board-8930-camera.c
@@ -264,7 +264,7 @@ static struct msm_bus_vectors cam_video_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_VFE,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
-		.ab  = 274406400,
+		.ab  = 600000000,
 		.ib  = 2656000000UL,
 	},
 	{
