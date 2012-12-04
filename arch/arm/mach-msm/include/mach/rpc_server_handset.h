61ba5afcfc0801a75f3d7719827bd823c7c2f87f
Mohan Pallaka <mpallaka@codeaurora.org>
2012-12-04 22:35:43 +0530

    msm: rpc_server_handset: Add support to ignore END key
    Legacy targets (7201, 7627, 7630..) using this driver use
    a key to report both END(short press) and POWER(long press).
    Recent targets that use this driver (QRD7x27a platform)
    needs the same key only for power. END functionality is to be
    supported from UI interface. Sending both END and POWER cause
    problems in these targets. So add support to ignore end key for
    such platforms.
    
    Change-Id: I636ac3eae5cd60041121e1d0ab0b1f169743c76b
    Signed-off-by: Mohan Pallaka <mpallaka@codeaurora.org>

--- arch/arm/mach-msm/include/mach/rpc_server_handset.h
+++ arch/arm/mach-msm/include/mach/rpc_server_handset.h
@@ -17,6 +17,7 @@
 struct msm_handset_platform_data {
 	const char *hs_name;
 	uint32_t pwr_key_delay_ms; /* default 500ms */
+	bool ignore_end_key;
 };
 
 void report_headset_status(bool connected);
