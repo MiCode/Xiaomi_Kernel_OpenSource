37a305c39dfb7e6f8f482293a9a6096fb681e5a6
Gopikrishnaiah Anandan <agopik@codeaurora.org>
2012-12-05 17:53:12 -0500

    vidc: Disable error logs in video driver.
    Change disables error logs in the video driver
    which was enabled by default.
    
    CRs-fixed: 429938
    Change-Id: I9288b4e49f87c01fd4e8e9aef74471dac696adce
    Signed-off-by: Gopikrishnaiah Anandan <agopik@codeaurora.org>

--- drivers/video/msm/vidc/1080p/ddl/vcd_ddl_utils.h
+++ drivers/video/msm/vidc/1080p/ddl/vcd_ddl_utils.h
@@ -38,13 +38,15 @@ do { \
 #define DDL_MSG_LOW(x...)    printk(KERN_INFO x)
 #define DDL_MSG_MED(x...)    printk(KERN_INFO x)
 #define DDL_MSG_HIGH(x...)   printk(KERN_INFO x)
+#define DDL_MSG_ERROR(x...)  printk(KERN_INFO x)
 #else
 #define DDL_MSG_LOW(x...)
 #define DDL_MSG_MED(x...)
 #define DDL_MSG_HIGH(x...)
+#define DDL_MSG_ERROR(x...)
 #endif
 
-#define DDL_MSG_ERROR(x...)  printk(KERN_INFO x)
+
 #define DDL_MSG_FATAL(x...)  printk(KERN_INFO x)
 
 #define DDL_ALIGN_SIZE(sz, guard_bytes, align_mask) \
