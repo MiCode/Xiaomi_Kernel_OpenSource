ce3c5bf354557fa77288ba4704782aaa67f6587c
Hemant Kumar <hemantk@codeaurora.org>
2012-12-06 15:52:02 -0800

    HSIC: Add support for product id 9075
    Product Id 0x9075 includes DIAG, SER3(System monitor), SER4(EFS sync),
    DUN, CSVT, NET1(Tethered RmNet) + NET2(Muxed Embedded RmNet) device
    interfaces.
    
    Change-Id: I75f61445af0ff9eab3d8f22bc2239011a9495e0e
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- drivers/usb/misc/mdm_data_bridge.c
+++ drivers/usb/misc/mdm_data_bridge.c
@@ -1017,6 +1017,7 @@ static void bridge_disconnect(struct usb_interface *intf)
 #define PID9034_IFACE_MASK	0xC
 #define PID9048_IFACE_MASK	0x18
 #define PID904C_IFACE_MASK	0x28
+#define PID9075_IFACE_MASK	0x28
 
 static const struct usb_device_id bridge_ids[] = {
 	{ USB_DEVICE(0x5c6, 0x9001),
@@ -1030,6 +1031,9 @@ static const struct usb_device_id bridge_ids[] = {
 	},
 	{ USB_DEVICE(0x5c6, 0x904c),
 	.driver_info = PID904C_IFACE_MASK,
+	},
+	{ USB_DEVICE(0x5c6, 0x9075),
+	.driver_info = PID9075_IFACE_MASK,
 	},
 
 	{ } /* Terminating entry */
