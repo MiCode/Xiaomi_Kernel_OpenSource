7459bfb82f3cd1df225ad00d9d9149734e81433f
Tarun Karra <tkarra@codeaurora.org>
2012-12-10 15:30:55 -0800

    msm: kgsl: Reduce cmd buffer size for adreno_iommu_setstate
    Command buffer of size 250 is not required in adreno_iommu_setstate.
    Reduce the size to 230 to prevent a big stack frame.
    
    Change-Id: I92bbbce67014255c9e22875491fb4bab64d83766
    Signed-off-by: Tarun Karra <tkarra@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -298,7 +298,7 @@ static void adreno_iommu_setstate(struct kgsl_device *device,
 					uint32_t flags)
 {
 	unsigned int pt_val, reg_pt_val;
-	unsigned int link[250];
+	unsigned int link[230];
 	unsigned int *cmds = &link[0];
 	int sizedwords = 0;
 	struct adreno_device *adreno_dev = ADRENO_DEVICE(device);
