8bffda9645e16467522f7d668a2d407d8b82f64d
Phani Kumar Uppalapati <phanik@codeaurora.org>
2012-12-14 14:31:09 -0800

    ASoC: msm: Modify min and max period bytes and num periods
    Modify min and max period bytes and number of periods in
    compressed driver so as to be consistent with lpa and to
    meet ION memory requirements.
    
    Change-Id: I216b7a7d3f38867725782b450dd62237faa856ee
    Signed-off-by: Phani Kumar Uppalapati <phanik@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-compr-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-compr-q6-v2.c
@@ -87,11 +87,11 @@ static struct snd_pcm_hardware msm_compr_hardware_playback = {
 	.rate_max =	     48000,
 	.channels_min =	 1,
 	.channels_max =	 8,
-	.buffer_bytes_max =     1200 * 1024 * 2,
-	.period_bytes_min =	2400,
-	.period_bytes_max =     1200 * 1024,
-	.periods_min =	  2,
-	.periods_max =	  1024,
+	.buffer_bytes_max =     1024 * 1024,
+	.period_bytes_min =	128 * 1024,
+	.period_bytes_max =     256 * 1024,
+	.periods_min =	  4,
+	.periods_max =	  8,
 	.fifo_size =	    0,
 };
 
