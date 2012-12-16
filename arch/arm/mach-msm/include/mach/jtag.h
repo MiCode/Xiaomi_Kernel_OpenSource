1983eaaa9f28b0d78adbaf0ff7a5598c1e2b3c51
Pushkar Joshi <pushkarj@codeaurora.org>
2012-12-15 18:59:03 -0800

    jtagmm: Add save-restore support for Debug and ETM registers
    Debug and ETM registers do not retain their state on a power collapse.
    This change provides save-restore support for these registers using
    memory mapped access in case cp14 access to these registers is not
    permitted.
    
    Change-Id: I8e3ed63102f64677e691dd4575e711f4a28af601
    Signed-off-by: Pushkar Joshi <pushkarj@codeaurora.org>

--- arch/arm/mach-msm/include/mach/jtag.h
+++ arch/arm/mach-msm/include/mach/jtag.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -13,7 +13,7 @@
 #ifndef __MACH_JTAG_H
 #define __MACH_JTAG_H
 
-#ifdef CONFIG_MSM_JTAG
+#if defined(CONFIG_MSM_JTAG) || defined(CONFIG_MSM_JTAG_MM)
 extern void msm_jtag_save_state(void);
 extern void msm_jtag_restore_state(void);
 #else
