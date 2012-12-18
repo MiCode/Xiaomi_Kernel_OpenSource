3b358f5954a337aef4d6ff34a5dda4109b253143
Bhalchandra Gajare <gajare@codeaurora.org>
2012-12-18 11:54:35 -0800

    msm: 8226: Add Audio DSP support for MSM8226/8910
    Add support for Audio (QDSP) devices for MSM8226 and MSM8910
    MSM8226/MSM8910 uses QDSP v2 for Audio
    
    Change-Id: I59adfbc50dc5f86e87af881b086f5ce5809da222
    Signed-off-by: Bhalchandra Gajare <gajare@codeaurora.org>

--- arch/arm/mach-msm/qdsp6v2/q6audio_common.h
+++ arch/arm/mach-msm/qdsp6v2/q6audio_common.h
@@ -15,7 +15,9 @@
 #ifndef __Q6_AUDIO_COMMON_H__
 #define __Q6_AUDIO_COMMON_H__
 
-#if defined(CONFIG_ARCH_MSM8974) || defined(CONFIG_ARCH_MSM9625)
+#if defined(CONFIG_ARCH_MSM8974) || defined(CONFIG_ARCH_MSM9625) \
+	|| defined(CONFIG_ARCH_MSM8226)
+
 #include <sound/apr_audio-v2.h>
 #include <sound/q6asm-v2.h>
 #else
