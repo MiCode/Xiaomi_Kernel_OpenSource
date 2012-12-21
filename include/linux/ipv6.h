bc4eca1540920b350c9722b6b63808c2b0233460
Tianyi Gou <tgou@codeaurora.org>
2012-12-21 12:38:38 -0800

    net/ipv6/addrconf: IPv6 tethering enhancement
    Added new procfs flag to toggle the automatic addition of prefix
    routes on a per device basis. The new flag is accept_ra_prefix_route.
    Defaults to 1 as to not break existing behavior.
    
    Change-Id: If25493890c7531c27f5b2c4855afebbbbf5d072a
    CRs-Fixed: 435320
    Acked-by: Harout S. Hedeshian <harouth@qti.qualcomm.com>
    Signed-off-by: Tianyi Gou <tgou@codeaurora.org>

--- include/linux/ipv6.h
+++ include/linux/ipv6.h
@@ -172,6 +172,7 @@ struct ipv6_devconf {
 	__s32		disable_ipv6;
 	__s32		accept_dad;
 	__s32		force_tllao;
+	__s32		accept_ra_prefix_route;
 	void		*sysctl;
 };
 
@@ -213,6 +214,7 @@ enum {
 	DEVCONF_DISABLE_IPV6,
 	DEVCONF_ACCEPT_DAD,
 	DEVCONF_FORCE_TLLAO,
+	DEVCONF_ACCEPT_RA_PREFIX_ROUTE,
 	DEVCONF_MAX
 };
 
