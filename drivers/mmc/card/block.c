338ed94db45ab6d4b5ff610add7781896ae4d066
Oluwafemi Adeyemi <aadeyemi@codeaurora.org>
2013-01-03 11:32:53 -0800

    mmc: Keep track of current hard partition
    eMMC may have several hard partitions such as BOOT and RPMB in addition
    to the main USER partition. On resume(if card is reinitialized), the
    current hard partition is always switched to the default USER partition.
    But this switch to default partition is not propagated to the MMC block
    driver, which may have set the hard partition to any partition(BOOT/RPMB)
    other than USER before suspend. After resume, the MMC block driver still
    assumes it is accessing the previously set partition(BOOT/RPMB), and
    instead overwrites the USER partition(the current selected partition on
    the eMMC device). To prevent this, make MMC block driver aware of the
    partition switches done as part of card reinitialization.
    
    CRs-Fixed: 439313
    Change-Id: I3e959101a1c56c1e6631da3d660f4b914e100503
    Signed-off-by: Oluwafemi Adeyemi <aadeyemi@codeaurora.org>
    Signed-off-by: Venkat Gopalakrishnan <venkatg@codeaurora.org>

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -914,7 +914,8 @@ static inline int mmc_blk_part_switch(struct mmc_card *card,
 	int ret;
 	struct mmc_blk_data *main_md = mmc_get_drvdata(card);
 
-	if (main_md->part_curr == md->part_type)
+	if ((main_md->part_curr == md->part_type) &&
+	    (card->part_curr == md->part_type))
 		return 0;
 
 	if (mmc_card_mmc(card)) {
@@ -930,6 +931,7 @@ static inline int mmc_blk_part_switch(struct mmc_card *card,
 			return ret;
 
 		card->ext_csd.part_config = part_config;
+		card->part_curr = md->part_type;
 	}
 
 	main_md->part_curr = md->part_type;
