f19cefe545c88a61ba6eac613fdf645bfe9e29c1
Patrick Lai <plai@codeaurora.org>
2013-01-05 21:46:38 -0800

    arm: mach-msm: qdsp6v2: update dual mono setting check
    AUDIO_AAC_DUAL_MONO_PL_PR is 0 anyway and dual mono
    setting is defined as unsigned value. Hence,
    there is no need if new setting is less than
    AUDIO_AAC_DUAL_MONO_PL_PR.
    
    Change-Id: I2e18a875e211d05d100d827fcd5b67545de168ac
    Signed-off-by: Patrick Lai <plai@codeaurora.org>

--- arch/arm/mach-msm/qdsp6v2/audio_multi_aac.c
+++ arch/arm/mach-msm/qdsp6v2/audio_multi_aac.c
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 2008 Google, Inc.
  * Copyright (C) 2008 HTC Corporation
- * Copyright (c) 2011-2012, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -147,10 +147,8 @@ static long audio_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 		} else {
 			uint16_t sce_left = 1, sce_right = 2;
 			aac_config = audio->codec_cfg;
-			if ((aac_config->dual_mono_mode <
-				AUDIO_AAC_DUAL_MONO_PL_PR) ||
-				(aac_config->dual_mono_mode >
-				AUDIO_AAC_DUAL_MONO_PL_SR)) {
+			if (aac_config->dual_mono_mode >
+			    AUDIO_AAC_DUAL_MONO_PL_SR) {
 				pr_err("%s:AUDIO_SET_AAC_CONFIG: Invalid dual_mono mode =%d\n",
 					 __func__, aac_config->dual_mono_mode);
 			} else {
