2178cf9262de42c676f1ef18eb97f9db1268a24d
Patrick Lai <plai@codeaurora.org>
2013-01-06 00:27:22 -0800

    arm: mach-msm: qdsp6v2: remove unnecessary write count check
    write count is never going to be negative value
    
    Change-Id: Ie9468d4fbcccb2b7df87cbb67d0a911e584c8e62
    Signed-off-by: Patrick Lai <plai@codeaurora.org>

--- arch/arm/mach-msm/qdsp6v2/q6core.c
+++ arch/arm/mach-msm/qdsp6v2/q6core.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2011, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2010-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -253,8 +253,6 @@ static ssize_t apr_debug_write(struct file *file, const char __user *buf,
 	int len;
 	static int t_len;
 
-	if (count < 0)
-		return 0;
 	len = count > 63 ? 63 : count;
 	if (copy_from_user(l_buf + 20 , buf, len)) {
 		pr_info("Unable to copy data from user space\n");
