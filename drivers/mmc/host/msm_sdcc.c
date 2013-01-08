444f1f0dc586a901e4e3a6d05c24763573b6d40e
Subhash Jadavani <subhashj@codeaurora.org>
2013-01-08 17:54:12 +0530

    mmc: msm_sdcc: check for pending_resume in SDCC system suspend
    If system comes out of suspend, msmsdcc_pm_resume() sets the
    host->pending_resume flag if the SDCC wasn't runtime suspended.
    Now if the system again goes to suspend without any SDCC activity
    then host->pending_resume flag will remain set which may cause the SDCC
    resume to happen first and then suspend.
    
    To avoid this unnecessary resume/suspend, make sure that pending_resume
    flag is cleared before calling the msmsdcc_runtime_suspend().
    
    CRs-Fixed: 438081
    Change-Id: I2b667bec704f938a532d4780dd9b7937e2eb12c7
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- drivers/mmc/host/msm_sdcc.c
+++ drivers/mmc/host/msm_sdcc.c
@@ -6764,9 +6764,21 @@ static int msmsdcc_pm_suspend(struct device *dev)
 		msmsdcc_disable_status_gpio(host);
 	}
 
-	if (!pm_runtime_suspended(dev))
+	/*
+	 * If system comes out of suspend, msmsdcc_pm_resume() sets the
+	 * host->pending_resume flag if the SDCC wasn't runtime suspended.
+	 * Now if the system again goes to suspend without any SDCC activity
+	 * then host->pending_resume flag will remain set which may cause
+	 * the SDCC resume to happen first and then suspend.
+	 * To avoid this unnecessary resume/suspend, make sure that
+	 * pending_resume flag is cleared before calling the
+	 * msmsdcc_runtime_suspend().
+	 */
+	if (!pm_runtime_suspended(dev) && !host->pending_resume)
 		rc = msmsdcc_runtime_suspend(dev);
  out:
+	/* This flag must not be set if system is entering into suspend */
+	host->pending_resume = false;
 	msmsdcc_print_pm_stats(host, start, __func__, rc);
 	return rc;
 }
