dd391c5ca7e2e45d249e904355ed254f3ca52324
Anji Jonnala <anjir@codeaurora.org>
2013-01-10 16:19:56 +0530

    msm: spm-v2: NULL check for dev pointer
    Make sure dev pointer is not NULL before accessing it.
    
    Change-Id: I1808d166c1eefd4f17be13d5252c0fcd0ec86c80
    Signed-off-by: Anji Jonnala <anjir@codeaurora.org>

--- arch/arm/mach-msm/spm-v2.c
+++ arch/arm/mach-msm/spm-v2.c
@@ -358,10 +358,12 @@ static void msm_spm_drv_set_avs_vlevel(struct msm_spm_driver_data *dev,
 int msm_spm_drv_set_vdd(struct msm_spm_driver_data *dev, unsigned int vlevel)
 {
 	uint32_t timeout_us, new_level;
-	bool avs_enabled = msm_spm_drv_is_avs_enabled(dev);
+	bool avs_enabled;
 
 	if (!dev)
 		return -EINVAL;
+
+	avs_enabled  = msm_spm_drv_is_avs_enabled(dev);
 
 	if (!msm_spm_pmic_arb_present(dev))
 		return -ENOSYS;
