702d81bcf8190775d7fa155085645a4d8492742a
Priyanka Mathur <pmathur@codeaurora.org>
2013-01-10 17:03:04 -0800

    msm: lpm-levels: Do RPM/MPM communication only during PC.
    When the system isn't entering RPM assisted power collapse, do not
    perform RPM/MPM communication. Otherwise, this would result in increased
    boot delays as the latency associated with entering shallow low power
    modes is increased.
    
    Change-Id: I7882b3488fa392077e700229fd10b03fbfab71f1
    Signed-off-by: Priyanka Mathur <pmathur@codeaurora.org>
    Signed-off-by: Mahesh Sivasubramanian <msivasub@codeaurora.org>

--- arch/arm/mach-msm/lpm_resources.c
+++ arch/arm/mach-msm/lpm_resources.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -668,7 +668,8 @@ int msm_lpmrs_enter_sleep(uint32_t sclk_count, struct msm_rpmrs_limits *limits,
 	}
 	msm_lpm_get_rpm_notif = true;
 
-	msm_mpm_enter_sleep(sclk_count, from_idle);
+	if (notify_rpm)
+		msm_mpm_enter_sleep(sclk_count, from_idle);
 
 	return ret;
 }
