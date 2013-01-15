a276c90446fa861519b95e3f1ca43b342eb3272a
Jay Chokshi <jchokshi@codeaurora.org>
2013-01-15 13:06:27 -0800

    msm: socinfo: Add support for APQ8064AA
    Add the SoC ID and cpu_is wrapper for identifying the
    APQ8064AA SoC.
    
    Change-Id: Ia038088045e1170fe897f8ba0a9ab725e5acab20
    Signed-off-by: Jay Chokshi <jchokshi@codeaurora.org>

--- arch/arm/mach-msm/include/mach/socinfo.h
+++ arch/arm/mach-msm/include/mach/socinfo.h
@@ -105,6 +105,7 @@ enum msm_cpu {
 	MSM_CPU_7X25AB,
 	MSM_CPU_8064,
 	MSM_CPU_8064AB,
+	MSM_CPU_8064AA,
 	MSM_CPU_8930,
 	MSM_CPU_8930AA,
 	MSM_CPU_8930AB,
@@ -332,6 +333,15 @@ static inline int cpu_is_apq8064ab(void)
 #endif
 }
 
+static inline int cpu_is_apq8064aa(void)
+{
+#ifdef CONFIG_ARCH_APQ8064
+	return read_msm_cpu_type() == MSM_CPU_8064AA;
+#else
+	return 0;
+#endif
+}
+
 static inline int cpu_is_msm8930(void)
 {
 #ifdef CONFIG_ARCH_MSM8930
@@ -473,7 +483,7 @@ static inline int soc_class_is_msm8960(void)
 
 static inline int soc_class_is_apq8064(void)
 {
-	return cpu_is_apq8064() || cpu_is_apq8064ab();
+	return cpu_is_apq8064() || cpu_is_apq8064ab() || cpu_is_apq8064aa();
 }
 
 static inline int soc_class_is_msm8930(void)
