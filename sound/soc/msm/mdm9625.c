3d75ce4e4b96a6177a2112f501ff9f288ced3e9b
Venkat Sudhir <vsudhir@codeaurora.org>
2013-01-14 17:35:39 -0800

    ASoC: msm: Add VoLTE support for MDM9625
    Add support for VoLTE DAI in MDM9625 machine
    driver. VoLTE is used for making Voice calls
    over LTE.
    
    Change-Id: I1f4b84f7033b5659d10b82fce2f33e2ffb3ec445
    Signed-off-by: Venkat Sudhir <vsudhir@codeaurora.org>

--- sound/soc/msm/mdm9625.c
+++ sound/soc/msm/mdm9625.c
@@ -695,6 +695,22 @@ static struct snd_soc_dai_link mdm9625_dai[] = {
 		.codec_dai_name = "snd-soc-dummy-dai",
 		.codec_name = "snd-soc-dummy",
 	},
+	{
+		.name = "VoLTE",
+		.stream_name = "VoLTE",
+		.cpu_dai_name   = "VoLTE",
+		.platform_name  = "msm-pcm-voice",
+		.dynamic = 1,
+		.trigger = {SND_SOC_DPCM_TRIGGER_POST,
+			    SND_SOC_DPCM_TRIGGER_POST},
+		.no_host_mode = SND_SOC_DAI_LINK_NO_HOST,
+		.ignore_suspend = 1,
+		/* this dainlink has playback support */
+		.ignore_pmdown_time = 1,
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.codec_name = "snd-soc-dummy",
+		.be_id = MSM_FRONTEND_DAI_VOLTE,
+	},
 	/* Backend DAI Links */
 	{
 		.name = LPASS_BE_MI2S_RX,
