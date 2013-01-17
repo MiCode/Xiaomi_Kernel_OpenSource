a0f253e9aff4deac4cc77f9e5aed32d8c63f1618
Seemanta Dutta <seemanta@codeaurora.org>
2013-01-16 18:54:40 -0800

    msm: pil-q6v5-mss: Proxy unvote after clock ready interrupt from modem
    Use the SMP2P framework to intercept the modem clock ready interrupt
    to proxy unvote after modem has booted up. Do this by getting the SMP2P
    IRQ for modem clock ready and then include the IRQ in the pil descriptor
    during registration with the pil framework.
    
    Change-Id: Idb7c2b3dd2951174b715786d09de5b9a81b433f7
    Signed-off-by: Seemanta Dutta <seemanta@codeaurora.org>

--- arch/arm/mach-msm/pil-q6v5-mss.c
+++ arch/arm/mach-msm/pil-q6v5-mss.c
@@ -709,6 +709,15 @@ static int __devinit pil_mss_loadable_init(struct mba_data *drv,
 	struct resource *res;
 	int ret;
 
+	int clk_ready = of_get_named_gpio(pdev->dev.of_node,
+			"qcom,gpio-proxy-unvote", 0);
+	if (clk_ready < 0)
+		return clk_ready;
+
+	clk_ready = gpio_to_irq(clk_ready);
+	if (clk_ready < 0)
+		return clk_ready;
+
 	q6 = pil_q6v5_init(pdev);
 	if (IS_ERR(q6))
 		return PTR_ERR(q6);
@@ -718,6 +727,7 @@ static int __devinit pil_mss_loadable_init(struct mba_data *drv,
 	q6_desc->ops = &pil_mss_ops;
 	q6_desc->owner = THIS_MODULE;
 	q6_desc->proxy_timeout = PROXY_TIMEOUT_MS;
+	q6_desc->proxy_unvote_irq = clk_ready;
 
 	drv->self_auth = of_property_read_bool(pdev->dev.of_node,
 							"qcom,pil-self-auth");
@@ -781,6 +791,7 @@ static int __devinit pil_mss_loadable_init(struct mba_data *drv,
 	mba_desc->ops = &pil_mba_ops;
 	mba_desc->owner = THIS_MODULE;
 	mba_desc->proxy_timeout = PROXY_TIMEOUT_MS;
+	mba_desc->proxy_unvote_irq = clk_ready;
 
 	ret = pil_desc_init(mba_desc);
 	if (ret)
