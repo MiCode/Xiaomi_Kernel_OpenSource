d749879e43bed5f24e58993cfeb9f6fad3cd4093
Stephen Boyd <sboyd@codeaurora.org>
2013-01-18 18:57:50 -0800

    ashmem: Move ashmem.h from staging to include/linux and export it
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    [abhimany: resolve trivial merge conflicts]
    Signed-off-by: Abhimanyu Kapur <abhimany@codeaurora.org>

--- include/linux/ashmem.h
+++ include/linux/ashmem.h
@@ -12,16 +12,6 @@
 #ifndef _LINUX_ASHMEM_H
 #define _LINUX_ASHMEM_H
 
-#include <linux/limits.h>
-#include <linux/ioctl.h>
-#include <linux/compat.h>
-
-#include "uapi/ashmem.h"
-
-/* support of 32bit userspace on 64bit platforms */
-#ifdef CONFIG_COMPAT
-#define COMPAT_ASHMEM_SET_SIZE		_IOW(__ASHMEMIOC, 3, compat_size_t)
-#define COMPAT_ASHMEM_SET_PROT_MASK	_IOW(__ASHMEMIOC, 5, unsigned int)
-#endif
+#include <uapi/linux/ashmem.h>
 
 #endif	/* _LINUX_ASHMEM_H */
--- include/uapi/linux/ashmem.h
+++ include/uapi/linux/ashmem.h
@@ -1,19 +1,10 @@
-/*
- * drivers/staging/android/uapi/ashmem.h
- *
- * Copyright 2008 Google Inc.
- * Author: Robert Love
- *
- * This file is dual licensed.  It may be redistributed and/or modified
- * under the terms of the Apache 2.0 License OR version 2 of the GNU
- * General Public License.
- */
-
 #ifndef _UAPI_LINUX_ASHMEM_H
 #define _UAPI_LINUX_ASHMEM_H
 
+#include <linux/limits.h>
 #include <linux/ioctl.h>
 #include <linux/types.h>
+#include <linux/compat.h>
 
 #define ASHMEM_NAME_LEN		256
 
@@ -45,4 +36,11 @@ struct ashmem_pin {
 #define ASHMEM_GET_PIN_STATUS	_IO(__ASHMEMIOC, 9)
 #define ASHMEM_PURGE_ALL_CACHES	_IO(__ASHMEMIOC, 10)
 
-#endif	/* _UAPI_LINUX_ASHMEM_H */
+
+/* support of 32bit userspace on 64bit platforms */
+#ifdef CONFIG_COMPAT
+#define COMPAT_ASHMEM_SET_SIZE		_IOW(__ASHMEMIOC, 3, compat_size_t)
+#define COMPAT_ASHMEM_SET_PROT_MASK	_IOW(__ASHMEMIOC, 5, unsigned int)
+#endif
+
+#endif /* _UAPI_LINUX_ASHMEM_H */
