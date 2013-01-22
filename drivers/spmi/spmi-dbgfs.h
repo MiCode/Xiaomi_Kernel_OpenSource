5998fab41b4f890e69dc626794d57d2d30855167
Kenneth Heitke <kheitke@codeaurora.org>
2013-01-22 16:09:19 -0700

    spmi: Add API to create controller specific debugfs files
    Add API function 'spmi_dfs_create_file' for SPMI controller drivers to
    create their own implementation specific files under the SPMI debugfs
    directory.
    
    Change-Id: Ib4d0557333371a2a29b6dc6ae6ca6a41ebf13c34
    Signed-off-by: Kenneth Heitke <kheitke@codeaurora.org>

--- drivers/spmi/spmi-dbgfs.h
+++ drivers/spmi/spmi-dbgfs.h
@@ -12,10 +12,16 @@
 #ifndef _SPMI_DBGFS_H
 #define _SPMI_DBGFS_H
 
+#include <linux/debugfs.h>
+
 #ifdef CONFIG_DEBUG_FS
 int spmi_dfs_add_controller(struct spmi_controller *ctrl);
 #else
-int spmi_dfs_add_controller(struct spmi_controller *ctrl) { return 0; }
+static int spmi_dfs_add_controller(struct spmi_controller *ctrl) { return 0; }
 #endif
+
+struct dentry *spmi_dfs_create_file(struct spmi_controller *ctrl,
+					const char *name, void *data,
+					const struct file_operations *fops);
 
 #endif /* _SPMI_DBGFS_H */
