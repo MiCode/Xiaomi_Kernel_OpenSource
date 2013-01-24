0a547f9587bf86c4018ad4055592f0d9d77e56bf
Chandan Uddaraju <chandanu@codeaurora.org>
2013-01-24 14:33:19 -0800

    msm: mdss: enable split display in mdp
    Allow a second panel to be registered to a single frame buffer, the
    image from frame buffer would then be displayed across the two panels.
    
    Change-Id: I2bc78741ed8408bedfa563cd312c4e628045bfae
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>
    Signed-off-by: Chandan Uddaraju <chandanu@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_fb.h
+++ drivers/video/fbdev/msm/mdss_fb.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008-2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2008-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -60,6 +60,7 @@ struct msm_fb_data_type {
 
 	struct panel_id panel;
 	struct mdss_panel_info *panel_info;
+	int split_display;
 
 	u32 dest;
 	struct fb_info *fbi;
