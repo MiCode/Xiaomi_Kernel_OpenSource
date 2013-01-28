2d18c47a6c6a69739b89ecdc2b8125c6a2320a20
Vikram Mulukutla <markivx@codeaurora.org>
2013-01-28 11:39:51 -0800

    msm: clock-8610: Add lookup entry for the Pronto PIL driver
    The Pronto PIL driver needs the XO clock for its operation.
    Add the necessary lookup table entry.
    
    Change-Id: Ib480c273dad0d082497be25e8a4fbebf8cbdb188
    Signed-off-by: Vikram Mulukutla <markivx@codeaurora.org>

--- arch/arm/mach-msm/clock-8610.c
+++ arch/arm/mach-msm/clock-8610.c
@@ -2935,7 +2935,7 @@ static struct clk_lookup msm_clocks_8610[] = {
 	CLK_LOOKUP("xo",	gcc_xo_clk_src.c, "pil-q6v5-mss"),
 	CLK_LOOKUP("xo",	gcc_xo_clk_src.c, "pil-mba"),
 	CLK_LOOKUP("xo",	gcc_xo_clk_src.c, "fb000000.qcom,wcnss-wlan"),
-	CLK_LOOKUP("xo",	gcc_xo_clk_src.c, "pil_pronto"),
+	CLK_LOOKUP("xo",	gcc_xo_clk_src.c, "fb21b000.qcom,pronto"),
 	CLK_LOOKUP("measure",	measure_clk.c,	"debug"),
 
 	CLK_LOOKUP("iface_clk",  gcc_blsp1_ahb_clk.c, "f991f000.serial"),
