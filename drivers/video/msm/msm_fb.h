32a7b92d33ead9ee237550ad48f35b204bef15cb
Ajay Singh Parmar <aparmar@codeaurora.org>
2013-02-01 07:51:35 +0530

    msm_fb: hdmi: Do not turn on HDMI if not ready
    At bootup, if HPD is not configured, HDMI is not in
    the state of getting turned on. This change makes sure
    HDMI is not turned on/off unnecessarily which turns on
    clocks as well.
    
    CRs-Fixed: 447511
    Change-Id: I074d9c463943770775b060c4a93b9120dfe9bcf0
    Signed-off-by: Ajay Singh Parmar <aparmar@codeaurora.org>

--- drivers/video/msm/msm_fb.h
+++ drivers/video/msm/msm_fb.h
@@ -139,6 +139,7 @@ struct msm_fb_data_type {
 	void (*vsync_ctrl) (int enable);
 	void (*vsync_init) (int cndx);
 	void (*update_panel_info)(struct msm_fb_data_type *mfd);
+	bool (*is_panel_ready)(void);
 	void *vsync_show;
 	void *cursor_buf;
 	void *cursor_buf_phys;
