f335f26c136aa02ebfd5493e33573db19522235b
Ajay Singh Parmar <aparmar@codeaurora.org>
2013-02-02 22:47:38 +0530

    msm_fb: hdmi: hdmi 3d length check
    This change fixes a case where the length of 3d data is not
    properly used which could have resulted in wrong 3d data parsing.
    
    CRs-Fixed: 445505
    Change-Id: Id285204bdb8c2d881becaa5b07b973eba4240d93
    Signed-off-by: Ajay Singh Parmar <aparmar@codeaurora.org>

--- drivers/video/msm/external_common.c
+++ drivers/video/msm/external_common.c
@@ -1573,7 +1573,8 @@ static void hdmi_edid_get_display_vsd_3d_mode(const uint8 *data_buf,
 	struct hdmi_disp_mode_list_type *disp_mode_list,
 	uint32 num_og_cea_blocks)
 {
-	uint8 len, offset, present_multi_3d, hdmi_vic_len, hdmi_3d_len;
+	uint8 len, offset, present_multi_3d, hdmi_vic_len;
+	int hdmi_3d_len;
 	uint16 structure_all, structure_mask;
 	const uint8 *vsd = num_og_cea_blocks ?
 		hdmi_edid_find_block(data_buf+0x80, DBC_START_OFFSET,
