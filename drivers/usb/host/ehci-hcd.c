7d18afc61385359a84419ae1cf7c91ea2be673f4
Chiranjeevi Velempati <cvelempa@codeaurora.org>
2013-02-03 18:06:05 +0530

    USB: ehci_hcd: Don't panic on receiving FATAL ERROR
    "commit 105d07f EHCI: HSIC: Add debug support for HSIC system error"
     has introduced panic() on receiving FATAL STATUS interrupt. For OTG
    and 2nd/3rd HSUSB instance this panic() is not required as the system
    can be functional with USB system error, hence avoiding this panic()
    for OTG and 2nd/3rd HSUSB instances.
    
    CRs-Fixed: 447040
    Change-Id: I94dc63c5e8ac612ce9bbe42d2ef8266816be2693
    Signed-off-by: Chiranjeevi Velempati <cvelempa@codeaurora.org>

--- drivers/usb/host/ehci-hcd.c
+++ drivers/usb/host/ehci-hcd.c
@@ -976,9 +976,10 @@ static irqreturn_t ehci_irq (struct usb_hcd *hcd)
 	/* PCI errors [4.15.2.4] */
 	if (unlikely ((status & STS_FATAL) != 0)) {
 		ehci_err(ehci, "fatal error\n");
-		if (hcd->driver->dump_regs)
+		if (hcd->driver->dump_regs) {
 			hcd->driver->dump_regs(hcd);
-		panic("System error\n");
+			panic("System error\n");
+		}
 		dbg_cmd(ehci, "fatal", cmd);
 		dbg_status(ehci, "fatal", status);
 		ehci_halt(ehci);
