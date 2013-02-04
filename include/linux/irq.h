7c22eb3057e5a6e986d68e5ae5584e18c76281d6
Rohit Vaswani <rvaswani@codeaurora.org>
2013-02-04 15:28:05 -0800

    include: irq.h: Undo deleting check_irq_resend from upstream
    Change-Id: I16bdb468fd4983aedf0ace58d6ca4c58c59d8e13
    Signed-off-by: Rohit Vaswani <rvaswani@codeaurora.org>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -444,6 +444,8 @@ extern void handle_nested_irq(unsigned int irq);
 extern void note_interrupt(unsigned int irq, struct irq_desc *desc,
 			   irqreturn_t action_ret);
 
+/* Resending of interrupts :*/
+void check_irq_resend(struct irq_desc *desc, unsigned int irq);
 
 /* Enable/disable irq debugging output: */
 extern int noirqdebug_setup(char *str);
