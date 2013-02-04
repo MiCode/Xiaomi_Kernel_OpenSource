b6ade10a6f2cf48021af028942eb7ae1fca4642e
Ming-yi Lin <mylin@codeaurora.org>
2013-02-04 14:05:08 +0800

    msm: board-8974: add request_firmware support for hsic
    Firmware download was failing because HSIC driver registers itself
    as msm_hsic_host. But in /sys/devices HSIC host is created as f9a0
    0000.hsic. This leads to the problem that calling request_firmware
    will always timeout if the device driver is on the HSIC bus.
    
    Change-Id: I2a7d2ffa2882e2d30c0b554c0beb2ab47c440703
    Signed-off-by: Ming-yi Lin <mylin@codeaurora.org>

--- arch/arm/mach-msm/board-8974.c
+++ arch/arm/mach-msm/board-8974.c
@@ -181,6 +181,8 @@ static struct of_dev_auxdata msm8974_auxdata_lookup[] __initdata = {
 			"qcedev.0", NULL),
 	OF_DEV_AUXDATA("qcom,qcrypto", 0xFD440000, \
 			"qcrypto.0", NULL),
+	OF_DEV_AUXDATA("qcom,hsic-host", 0xF9A00000, \
+			"msm_hsic_host", NULL),
 	{}
 };
 
