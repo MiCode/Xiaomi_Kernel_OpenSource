d7c3cbe042b87782afeebf0d4333e76eac8b3988
Dolev Raviv <draviv@codeaurora.org>
2013-02-11 11:58:14 +0200

    scsi: ufs: created UFS unit-tests framework
    Created a new UFS block device test framework.
    This test includes:
    - Basic read after write command
    - Random buffer length
    - Ability to run test multiple times
    
    Change-Id: I06fe7bf7d325037d6039a4acfdfb9c2c8960a435
    Signed-off-by: Dolev Raviv <draviv@codeaurora.org>
    Signed-off-by: Maya Erez <merez@codeaurora.org>

--- include/scsi/scsi.h
+++ include/scsi/scsi.h
@@ -562,4 +562,6 @@ static inline __u32 scsi_to_u32(__u8 *ptr)
 	return (ptr[0]<<24) + (ptr[1]<<16) + (ptr[2]<<8) + ptr[3];
 }
 
+struct scsi_disk *scsi_disk_get_from_dev(struct device *dev);
+
 #endif /* _SCSI_SCSI_H */
