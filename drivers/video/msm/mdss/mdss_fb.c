08bbc104dc7c8d53a3dd5cd07c0dde281603394c
Adrian Salido-Moreno <adrianm@codeaurora.org>
2013-02-12 19:33:02 -0800

    msm: mdss: release sync fences when timeout happens
    When sync fence times out it means buffer should be dropped, at this
    point need to release reference on it so it can be reused.
    
    CRs-Fixed: 449267
    Change-Id: I1a03f809b06e234a094d598b26e715049ac4c3b6
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1043,15 +1043,22 @@ static void mdss_fb_power_setting_idle(struct msm_fb_data_type *mfd)
 
 void mdss_fb_wait_for_fence(struct msm_fb_data_type *mfd)
 {
-	int i, ret;
+	int i, ret = 0;
 	/* buf sync */
 	for (i = 0; i < mfd->acq_fen_cnt; i++) {
 		ret = sync_fence_wait(mfd->acq_fen[i], WAIT_FENCE_TIMEOUT);
-		sync_fence_put(mfd->acq_fen[i]);
 		if (ret < 0) {
 			pr_err("%s: sync_fence_wait failed! ret = %x\n",
 				__func__, ret);
 			break;
+		}
+		sync_fence_put(mfd->acq_fen[i]);
+	}
+
+	if (ret < 0) {
+		while (i < mfd->acq_fen_cnt) {
+			sync_fence_put(mfd->acq_fen[i]);
+			i++;
 		}
 	}
 	mfd->acq_fen_cnt = 0;
