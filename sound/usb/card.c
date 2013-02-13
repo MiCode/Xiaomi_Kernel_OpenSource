639292f24a8e0f97978c248606298a403e41093e
Asish Bhattacharya <asishb@codeaurora.org>
2013-02-13 22:06:41 +0530

    ASoC: usb: move the switch setting to more appropriate location
    There is a delay between creation of sound card and creating of
    corresponding steam. Currently we set the switch at the time of
    creation of card. And later streams get created, so its more
    optimal if we set the switch only after creating the stream.
    
    Change-Id: Ic9f29d7a8dbcd32cde0097289ce6e1404a837372
    Signed-off-by: Asish Bhattacharya <asishb@codeaurora.org>

--- sound/usb/card.c
+++ sound/usb/card.c
@@ -296,7 +296,7 @@ static int snd_usb_create_streams(struct snd_usb_audio *chip, int ctrlif)
 		break;
 	}
 	}
-
+	switch_set_state(usbaudiosdev, 1);
 	return 0;
 }
 
@@ -455,7 +455,6 @@ static int snd_usb_audio_create(struct usb_device *dev, int idx,
 	}
 
 	snd_usb_audio_create_proc(chip);
-	switch_set_state(usbaudiosdev, 1);
 
 	*rchip = chip;
 	return 0;
