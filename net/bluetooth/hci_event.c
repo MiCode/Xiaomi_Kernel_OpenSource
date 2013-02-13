f1fb43ca5a1f96ef0d4958c6b0d518ac0ff44b39
Bhakthavatsala Raghavendra <braghave@codeaurora.org>
2013-02-13 13:27:45 +0530

    Revert "Bluetooth: Avoid re-authentication for secure high connections"
    This reverts commit 45c9e8141e54a691180c55ed9e242302a74224a8 to
    add the high security feature for Bluetooth SAP connections and
    modify it accordingly based on newly introduced security level
    BT_SECURITY_VERY_HIGH
    
    Change-Id: Iee644f37c2108013654e99d82f63101db3da009e
    Signed-off-by: Bhakthavatsala Raghavendra <braghave@codeaurora.org>

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -2669,6 +2669,12 @@ static inline void hci_link_key_request_evt(struct hci_dev *hdev, struct sk_buff
 		BT_DBG("Conn pending sec level is %d, ssp is %d, key len is %d",
 			conn->pending_sec_level, conn->ssp_mode, key->pin_len);
 	}
+	if (conn && (conn->ssp_mode == 0) &&
+		(conn->pending_sec_level == BT_SECURITY_VERY_HIGH) &&
+		(key->pin_len != 16)) {
+		BT_DBG("Security is high ignoring this key");
+		goto not_found;
+	}
 
 	if (key->key_type == 0x04 && conn && conn->auth_type != 0xff &&
 						(conn->auth_type & 0x01)) {
