e50d914a959414d7174f9f57208b224e13e5a829
Ido Shayevitz <idos@codeaurora.org>
2013-02-17 20:43:26 +0200

    EHCI: HSIC: fix driver removal function for msm_hsic_host
    Upon driver removal function we need to disable the device pm runtime
    in order to, first, undo all the logic done in probe and second, to keep
    balanced disable depth counter in case of platform driver unbind.
    
    However the removal function can be called also in consequences of
    platform_device_del(), in this case the platform device was already
    disabled and removed so if we call pm_runtime_disable() again we
    decrease the disable_depth for the second time causing for unbalanced
    counter.
    
    Therefore we use an if statement to check if the device was already
    removed or not.
    
    CRs-Fixed: 445674
    Change-Id: I5ff6b20d0f8e811a5c673903ca340f7bcc2e2d78
    Signed-off-by: Ido Shayevitz <idos@codeaurora.org>

--- drivers/usb/host/ehci-msm-hsic.c
+++ drivers/usb/host/ehci-msm-hsic.c
@@ -1929,6 +1929,11 @@ static int __devexit ehci_hsic_msm_remove(struct platform_device *pdev)
 
 	ehci_hsic_msm_debugfs_cleanup();
 	device_init_wakeup(&pdev->dev, 0);
+
+	/* If the device was removed no need to call pm_runtime_disable */
+	if (pdev->dev.power.power_state.event != PM_EVENT_INVALID)
+		pm_runtime_disable(&pdev->dev);
+
 	pm_runtime_set_suspended(&pdev->dev);
 
 	destroy_workqueue(ehci_wq);
