f7804d4700707c8c27254ed48d401409e7346aed
Neil Leeder <nleeder@codeaurora.org>
2013-02-19 16:10:10 -0500

    Perf: Correct irq for CPU hotplug detection
    The platform data was requesting the wrong number,
    it should request the first perf irq item.
    
    Change-Id: I4a25b4704ed9e76172c6b0d4ca4b28a3286ab2ad
    Signed-off-by: Neil Leeder <nleeder@codeaurora.org>

--- arch/arm/mach-msm/perf_debug.c
+++ arch/arm/mach-msm/perf_debug.c
@@ -25,6 +25,7 @@ static char *descriptions =
 	"0  msm: perf: add debug patch logging framework\n"
 	"1  Perf: Restore counter after powercollapse for generic ARM PMU's\n"
 	"2  Perf: Toggle PMU IRQ when CPU's are hotplugged\n"
+	"3  Perf: Correct irq for CPU hotplug detection\n"
 ;
 
 static ssize_t desc_read(struct file *fp, char __user *buf,
