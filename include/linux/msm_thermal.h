3014d2c6135c02f3902a3ceee032ff7a10775a06
Praveen Chidambaram <pchidamb@codeaurora.org>
2013-02-20 17:42:17 -0700

    thermal: msm: Add core control support to thermal driver
    Detect and limit the numbers used during boot up if the core
    temperatures are already higher than the control threshold.
    
    CRs-Fixed: 460083
    Change-Id: I2ad4b6d1b6834fa610a95d53b420e914054aa66a
    Signed-off-by: Praveen Chidambaram <pchidamb@codeaurora.org>

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -17,9 +17,12 @@
 struct msm_thermal_data {
 	uint32_t sensor_id;
 	uint32_t poll_ms;
-	uint32_t limit_temp_degC;
-	uint32_t temp_hysteresis_degC;
+	int32_t limit_temp_degC;
+	int32_t temp_hysteresis_degC;
 	uint32_t freq_step;
+	int32_t core_limit_temp_degC;
+	int32_t core_temp_hysteresis_degC;
+	uint32_t core_control_mask;
 };
 
 #ifdef CONFIG_THERMAL_MONITOR
