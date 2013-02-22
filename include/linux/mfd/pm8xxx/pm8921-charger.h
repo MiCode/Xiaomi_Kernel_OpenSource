2e9dbea2b1465144f7a9b7d0e2e852bcf36454b2
Anirudh Ghayal <aghayal@codeaurora.org>
2013-02-22 11:17:19 +0530

    power: pm8921-charger: Make charger removal detection workaround optional
    On some platforms such as MPQ8064 the charger removal detection
    workarounds - reverse boost fix and OVP FET stuck do not work
    as expected as the SMBC module is disconnected from USBout. On
    these targets the USB interface is not used for charging but merely
    to indicate the USB driver of the insertion/removal.
    
    Add a platform data variable to make this optional and enable
    the same for MPQ8064
    
    Change-Id: I3fca1b357f8fc280483302f72ffbc61fb50109a7
    Signed-off-by: Anirudh Ghayal <aghayal@codeaurora.org>

--- include/linux/mfd/pm8xxx/pm8921-charger.h
+++ include/linux/mfd/pm8xxx/pm8921-charger.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -188,6 +188,7 @@ struct pm8921_charger_platform_data {
 	int				btc_delay_ms;
 	int				btc_panic_if_cant_stop_chg;
 	int				stop_chg_upon_expiry;
+	bool				disable_chg_rmvl_wrkarnd;
 };
 
 enum pm8921_charger_source {
