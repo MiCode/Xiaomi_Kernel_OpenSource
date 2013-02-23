65d97852e9629e983775f09b7b0ef27a7b7dc9d8
Mitchel Humpherys <mitchelh@codeaurora.org>
2013-02-22 17:36:16 -0800

    gpu: ion: make cached Iommu allocations multiples of PAGE_SIZE
    In future changes to Ion, we will fault in the pages one at a time
    using vm_insert_page for cached allocations. There appears to be a
    limitation on vm_insert_page that the pages must be of size
    PAGE_SIZE. Make sure that we only allocate pages of size PAGE_SIZE for
    cached allocations.
    
    Change-Id: I2243e73c4f50b90c9345c25b9e4bc30ccfa7e198
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- drivers/gpu/ion/ion_iommu_heap.c
+++ drivers/gpu/ion/ion_iommu_heap.c
@@ -109,7 +109,7 @@ static int ion_iommu_heap_allocate(struct ion_heap *heap,
 		void *ptr = NULL;
 		unsigned int npages_to_vmap, total_pages, num_large_pages = 0;
 		long size_remaining = PAGE_ALIGN(size);
-		unsigned int max_order = orders[0];
+		unsigned int max_order = ION_IS_CACHED(flags) ? 0 : orders[0];
 
 		data = kmalloc(sizeof(*data), GFP_KERNEL);
 		if (!data)
