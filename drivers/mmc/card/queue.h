1de2ffdbffc79d08b88d1edf66490fab42272551
Subhash Jadavani <subhashj@codeaurora.org>
2013-02-26 17:32:58 +0530

    mmc: card: abort the suspend if the card is busy
    mmc_blk_suspend() is triggered when system is going into suspend which
    then waits for the mmcqd thread (which processes active MMC transfer
    requests) to complete all queued transfer requests but if mmcqd has
    multiple requests to be processed at this point, mmc_blk_suspend() may
    very well sleep for duration longer than DPM (Device Power Management)
    timeout (currently 12 sec) causing the DPM timeout bug.
    
    To fix this issue, mmc_blk_suspend() checks if there are any more pending
    MMC requests in progress or not and if there are any, it returns error
    which should abort the ongoing system suspend.
    
    CRs-Fixed: 439192
    Change-Id: Ic7ba2f013d10a1952c30ea2ed93f74b831fcc27d
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- drivers/mmc/card/queue.h
+++ drivers/mmc/card/queue.h
@@ -62,7 +62,7 @@ struct mmc_queue {
 extern int mmc_init_queue(struct mmc_queue *, struct mmc_card *, spinlock_t *,
 			  const char *);
 extern void mmc_cleanup_queue(struct mmc_queue *);
-extern void mmc_queue_suspend(struct mmc_queue *);
+extern int mmc_queue_suspend(struct mmc_queue *);
 extern void mmc_queue_resume(struct mmc_queue *);
 
 extern unsigned int mmc_queue_map_sg(struct mmc_queue *,
