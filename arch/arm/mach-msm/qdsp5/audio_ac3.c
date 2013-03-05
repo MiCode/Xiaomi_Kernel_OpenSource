4c406883e6015b42e5e6363c438726c3382d8208
Anurag Singh <anursing@codeaurora.org>
2013-03-05 15:35:28 -0800

    qdsp5: audio: Check for earlysuspend support.
    Declaration of audac3_post_event is not currently
    guarded by an ifdef check for CONFIG_HAS_EARLYSUSPEND
    (the function's definition is), causing builds without
    early suspend to fail. Use the ifdef check for the
    declaration to avoid this.
    
    Change-Id: I5a5758df6b543d100351644b20795b30f74e556b
    Signed-off-by: Anurag Singh <anursing@codeaurora.org>

--- arch/arm/mach-msm/qdsp5/audio_ac3.c
+++ arch/arm/mach-msm/qdsp5/audio_ac3.c
@@ -1,6 +1,6 @@
 /* arch/arm/mach-msm/audio_ac3.c
  *
- * Copyright (c) 2008-2009, 2011-2012 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2008-2009, 2011-2013 The Linux Foundation. All rights reserved.
  *
  * This code also borrows from audio_aac.c, which is
  * Copyright (C) 2008 Google, Inc.
@@ -186,8 +186,10 @@ static void audac3_send_data(struct audio *audio, unsigned needed);
 static void audac3_dsp_event(void *private, unsigned id, uint16_t *msg);
 static void audac3_config_hostpcm(struct audio *audio);
 static void audac3_buffer_refresh(struct audio *audio);
+#ifdef CONFIG_HAS_EARLYSUSPEND
 static void audac3_post_event(struct audio *audio, int type,
 		union msm_audio_event_payload payload);
+#endif
 
 static int rmt_put_resource(struct audio *audio)
 {
