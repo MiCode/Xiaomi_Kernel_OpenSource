76568639811744e1be36169ba7276961c7ebf306
Joonwoo Park <joonwoop@codeaurora.org>
2013-03-05 19:07:07 -0800

    msm: ion: Cleanup userspace exported header's marking
    Remove marking from userspace exported header.
    
    Change-Id: I98961d9628054330c822f9596f72a27d8b884d1f
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- include/linux/msm_ion.h
+++ include/linux/msm_ion.h
@@ -1,18 +1,3 @@
-/*
- *
- * Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
- *
- * This software is licensed under the terms of the GNU General Public
- * License version 2, as published by the Free Software Foundation, and
- * may be copied, distributed, and modified under those terms.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- */
-
 #ifndef _LINUX_MSM_ION_H
 #define _LINUX_MSM_ION_H
 
