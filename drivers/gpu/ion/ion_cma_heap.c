4505341657ea4a1270fbfbf181fca00e7678b07e
Neeti Desai <neetid@codeaurora.org>
2013-03-08 17:29:53 -0800

    msm: Add support for ION Flushing without virtual address
    Video has very large buffers to be flushed. Due to lack of
    vmalloc space, ion_map_kernel cannot be called on each of
    the video buffers. With this change the ion handle can be
    flushed without the need of the kernel mapping.
    
    Change-Id: If026f21e44a2cce6c2b8c232fc80a69d0dabcd14
    Signed-off-by: Neeti Desai <neetid@codeaurora.org>

--- drivers/gpu/ion/ion_cma_heap.c
+++ drivers/gpu/ion/ion_cma_heap.c
@@ -281,15 +281,30 @@ int ion_cma_cache_ops(struct ion_heap *heap,
 
 	switch (cmd) {
 	case ION_IOC_CLEAN_CACHES:
-		dmac_clean_range(vaddr, vaddr + length);
+		if (!vaddr)
+			dma_sync_sg_for_device(NULL, buffer->sg_table->sgl,
+				buffer->sg_table->nents, DMA_TO_DEVICE);
+		else
+			dmac_clean_range(vaddr, vaddr + length);
 		outer_cache_op = outer_clean_range;
 		break;
 	case ION_IOC_INV_CACHES:
-		dmac_inv_range(vaddr, vaddr + length);
+		if (!vaddr)
+			dma_sync_sg_for_cpu(NULL, buffer->sg_table->sgl,
+				buffer->sg_table->nents, DMA_FROM_DEVICE);
+		else
+			dmac_inv_range(vaddr, vaddr + length);
 		outer_cache_op = outer_inv_range;
 		break;
 	case ION_IOC_CLEAN_INV_CACHES:
-		dmac_flush_range(vaddr, vaddr + length);
+		if (!vaddr) {
+			dma_sync_sg_for_device(NULL, buffer->sg_table->sgl,
+				buffer->sg_table->nents, DMA_TO_DEVICE);
+			dma_sync_sg_for_cpu(NULL, buffer->sg_table->sgl,
+				buffer->sg_table->nents, DMA_FROM_DEVICE);
+		} else {
+			dmac_flush_range(vaddr, vaddr + length);
+		}
 		outer_cache_op = outer_flush_range;
 		break;
 	default:
