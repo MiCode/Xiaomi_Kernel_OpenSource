87aa06b5d909ccf29c5e14db78f6db05db134555
Sahitya Tummala <stummala@codeaurora.org>
2013-03-10 14:12:52 +0530

    mmc: sdhci: Vote for the required MSM bus bandwidth
    Vote for the MSM bus bandwidth required by SDHC driver
    based on the speed and bus width of the card. Otherwise,
    the system clocks may run at minimum clock speed and
    thus affecting the performance.
    
    Change-Id: Icf0c8710adbe2770f4eae283a50f4a13671f703f
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -296,6 +296,7 @@ struct sdhci_ops {
 	void    (*adma_workaround)(struct sdhci_host *host, u32 intmask);
 	void	(*platform_init)(struct sdhci_host *host);
 	void    (*card_event)(struct sdhci_host *host);
+	void	(*platform_bus_voting)(struct sdhci_host *host, u32 enable);
 };
 
 #ifdef CONFIG_MMC_SDHCI_IO_ACCESSORS
