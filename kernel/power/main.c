9b785e31469ad62db38b7459a281716263b543fe
Anurag Singh <anursing@codeaurora.org>
2013-03-11 11:45:47 -0700

    Revert "power: main: Add conditional compilation for touch nodes"
    This reverts commit 8c880ff58fd257d3e9f20e04704cd9d1f3371fc7.
    Reverting the change is required to allow for cleaner conflict
    resolution as we move to the upstream kernel's wakeup sources
    based wakelock implementation.
    
    Change-Id: Ie14fe26a0fa244815dde7845330351beeef66f99
    Signed-off-by: Anurag Singh <anursing@codeaurora.org>

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -502,6 +502,8 @@ power_attr(wake_unlock);
 #endif
 
 static struct attribute *g[] = {
+	&touch_event_attr.attr,
+	&touch_event_timer_attr.attr,
 	&state_attr.attr,
 #ifdef CONFIG_PM_TRACE
 	&pm_trace_attr.attr,
@@ -510,8 +512,6 @@ static struct attribute *g[] = {
 #ifdef CONFIG_PM_SLEEP
 	&pm_async_attr.attr,
 	&wakeup_count_attr.attr,
-	&touch_event_attr.attr,
-	&touch_event_timer_attr.attr,
 #ifdef CONFIG_PM_DEBUG
 	&pm_test_attr.attr,
 #endif
