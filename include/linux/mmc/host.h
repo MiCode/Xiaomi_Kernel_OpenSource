24d46be8225be98a91394870a9605dded812aa79
Venkat Gopalakrishnan <venkatg@codeaurora.org>
2013-03-14 11:50:25 -0700

    mmc: core: Add support for HS400
    HS400 is a new bus speed mode introduced in eMMC5.0 specification. This
    patch adds the capability in mmc core to switch to HS400 mode when both
    the host and the card supports it.
    
    Change-Id: I53ee4982eae9e5cfccafe065b532d4a2f7225d17
    Signed-off-by: Venkat Gopalakrishnan <venkatg@codeaurora.org>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -62,6 +62,7 @@ struct mmc_ios {
 #define MMC_TIMING_UHS_SDR104	6
 #define MMC_TIMING_UHS_DDR50	7
 #define MMC_TIMING_MMC_HS200	8
+#define MMC_TIMING_MMC_HS400	9
 
 #define MMC_SDR_MODE		0
 #define MMC_1_2V_DDR_MODE	1
@@ -322,6 +323,11 @@ struct mmc_host {
 #define MMC_CAP2_SANITIZE	(1 << 20)		/* Support Sanitize */
 /* Allows Asynchronous SDIO irq while card is in 4-bit mode */
 #define MMC_CAP2_ASYNC_SDIO_IRQ_4BIT_MODE (1 << 21)
+
+#define MMC_CAP2_HS400_1_8V	(1 << 22)        /* can support */
+#define MMC_CAP2_HS400_1_2V	(1 << 23)        /* can support */
+#define MMC_CAP2_HS400		(MMC_CAP2_HS400_1_8V | \
+				 MMC_CAP2_HS400_1_2V)
 	mmc_pm_flag_t		pm_caps;	/* supported pm features */
 
 	int			clk_requests;	/* internal reference counter */
--- include/uapi/linux/mmc/mmc.h
+++ include/uapi/linux/mmc/mmc.h
@@ -29,6 +29,7 @@
 #define MMC_READ_MULTIPLE_BLOCK  18   /* adtc [31:0] data addr   R1  */
 #define MMC_SEND_TUNING_BLOCK    19   /* adtc                    R1  */
 #define MMC_SEND_TUNING_BLOCK_HS200	21	/* adtc R1  */
+#define MMC_SEND_TUNING_BLOCK_HS400	MMC_SEND_TUNING_BLOCK_HS200
 
   /* class 3 */
 #define MMC_WRITE_DAT_UNTIL_STOP 20   /* adtc [31:0] data addr   R1  */
