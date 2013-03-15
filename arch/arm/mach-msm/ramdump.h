da2a088545d58522b112e1cb0f89455b9d4932ae
Eric Holmberg <eholmber@codeaurora.org>
2013-03-15 15:22:01 -0600

    msm: ramdump: Add stubs if not enabled
    For targets that do not support subsytem restart, calling any of the
    ramdump APIs will result in a compilation failure.
    
    Add stubs for ramdump API.
    
    Change-Id: I8ba956a758d05d983c55ee12c33592671d1e8b7c
    Signed-off-by: Eric Holmberg <eholmber@codeaurora.org>

--- arch/arm/mach-msm/ramdump.h
+++ arch/arm/mach-msm/ramdump.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -20,11 +20,36 @@ struct ramdump_segment {
 	unsigned long size;
 };
 
+#ifdef CONFIG_MSM_SUBSYSTEM_RESTART
 void *create_ramdump_device(const char *dev_name, struct device *parent);
 void destroy_ramdump_device(void *dev);
 int do_ramdump(void *handle, struct ramdump_segment *segments,
 		int nsegments);
 int do_elf_ramdump(void *handle, struct ramdump_segment *segments,
 		int nsegments);
+
+#else
+static inline void *create_ramdump_device(const char *dev_name,
+		struct device *parent)
+{
+	return NULL;
+}
+
+static inline void destroy_ramdump_device(void *dev)
+{
+}
+
+static inline int do_ramdump(void *handle, struct ramdump_segment *segments,
+		int nsegments)
+{
+	return -ENODEV;
+}
+
+static inline int do_elf_ramdump(void *handle, struct ramdump_segment *segments,
+		int nsegments)
+{
+	return -ENODEV;
+}
+#endif /* CONFIG_MSM_SUBSYSTEM_RESTART */
 
 #endif
