5fd2d1402ac9a40ee077e7063d0303a47f5f3700
Laura Abbott <lauraa@codeaurora.org>
2013-03-18 11:14:47 -0700

    gpu: ion: Correctly print out physical addresses
    Physical addresses may not always be able to be printed
    with %lx. Use %pa to print out physical addresses.
    
    Change-Id: If7e91277b04495bbb13112259428f449043fc63f
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- drivers/gpu/ion/ion_cma_secure_heap.c
+++ drivers/gpu/ion/ion_cma_secure_heap.c
@@ -164,8 +164,8 @@ static int ion_secure_cma_phys(struct ion_heap *heap, struct ion_buffer *buffer,
 	struct device *dev = heap->priv;
 	struct ion_secure_cma_buffer_info *info = buffer->priv_virt;
 
-	dev_dbg(dev, "Return buffer %p physical address 0x%x\n", buffer,
-		info->handle);
+	dev_dbg(dev, "Return buffer %p physical address 0x%pa\n", buffer,
+		&info->handle);
 
 	*addr = info->handle;
 	*len = buffer->size;
