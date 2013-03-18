42c43835439d672629a5dc8fa3b9e105f0e6eece
Vladislav Hristov <vhrist@codeaurora.org>
2013-03-18 10:08:34 -0700

    msm: camera: separate clk enable in ispif
    ISPIF driver should request VFE clocks on a need bases. It is generally
    not a good idea to request all the possible clocks at open time, and
    free them at close. For this reason the ISPIF driver is modified to
    request only the clock which are really needed at some moment for its
    operation. This includes clock requests for VFE0 and VFE1 at init time
    when the reset should happen, and release them immediately after that.
    
    Also to ensure the normal working when there is a request for operation
    with the ISPIF, the clocks are enabled and disabled immediately. The
    reason for this is that VFE clocks are enabled by the VFE when there is
    a need of VFE, and the ISPIF only requires the clocks when it is being
    configured. In all other cases the VFE will take care of enabling them.
    
    Change-Id: Ie72afa9448436c14447b2212ae54c6111038d516
    Signed-off-by: Vladislav Hristov <vhrist@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/ispif/msm_ispif.h
+++ drivers/media/platform/msm/camera_v2/ispif/msm_ispif.h
@@ -56,6 +56,6 @@ struct ispif_device {
 	struct ispif_sof_count sof_count[VFE_MAX];
 	struct ispif_intf_cmd applied_intf_cmd[VFE_MAX];
 	enum msm_ispif_state_t ispif_state;
-	struct clk *ispif_clk[INTF_MAX];
+	struct clk *ispif_clk[VFE_MAX][INTF_MAX];
 };
 #endif
