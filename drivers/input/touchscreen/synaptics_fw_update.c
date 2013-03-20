0982da96f9284f7df8efdc7fcdc020452acab6ea
Alexandra Chin <alexandra.chin@tw.synaptics.com>
2013-03-19 17:46:05 -0700

    input: synaptics: Correct bugs in fw update
    - Fixed reset command could fail after firmware update
    - Config reset pin if reset gpio is defined in platform data
    - Force do a reset in function probe
    - Skip fw/config update process if image firmware id is smaller
      than device firmware id
    
    Change-Id: I3525410c791454f64c680506fe4c77e9da650a46
    Git-commit: db1a35c603df138842bdee3b0d9e7f9e24012e4e
    Git-repo: git://github.com/synaptics-touch/synaptics-dsx-i2c.git
    [amaloche@codeaurora.org: modified change w.r.t current driver]
    Signed-off-by: Amy Maloche <amaloche@codeaurora.org>

--- drivers/input/touchscreen/synaptics_fw_update.c
+++ drivers/input/touchscreen/synaptics_fw_update.c
@@ -561,8 +561,6 @@ static enum flash_area fwu_go_nogo(void)
 		goto exit;
 	}
 
-	imagePR = kzalloc(sizeof(MAX_FIRMWARE_ID_LEN), GFP_KERNEL);
-
 	/* Force update firmware when device is in bootloader mode */
 	if (f01_device_status.flash_prog) {
 		dev_info(&i2c_client->dev,
@@ -615,6 +613,11 @@ static enum flash_area fwu_go_nogo(void)
 			(unsigned int)imageFirmwareID);
 	if (imageFirmwareID > deviceFirmwareID) {
 		flash_area = UI_FIRMWARE;
+		goto exit;
+	} else if (imageFirmwareID < deviceFirmwareID) {
+		flash_area = NONE;
+		dev_info(&i2c_client->dev,
+			"Img fw is older than device fw.  Skip fw update.\n");
 		goto exit;
 	}
 
@@ -1650,6 +1653,9 @@ static int synaptics_rmi4_fwu_init(struct synaptics_rmi4_data *rmi4_data)
 			&fwu->fwu_work,
 			msecs_to_jiffies(1000));
 #endif
+
+	init_completion(&remove_complete);
+
 	return 0;
 
 exit_remove_attrs:
@@ -1700,7 +1706,6 @@ static int __init rmi4_fw_update_module_init(void)
 
 static void __exit rmi4_fw_update_module_exit(void)
 {
-	init_completion(&remove_complete);
 	synaptics_rmi4_new_function(RMI_FW_UPDATER, false,
 			synaptics_rmi4_fwu_init,
 			synaptics_rmi4_fwu_remove,
