cad846b546906112f124e987ec9c239baae5b527
Hamad Kadmany <hkadmany@codeaurora.org>
2013-03-21 09:43:01 +0200

    media: dvb: mpq: Add proper handling of stream-buffer termination
    When demux video filter is stopped, payload and meta-data buffers
    of the stream buffer are released by demux in case the buffers
    are not set externally by user-space. Calls to stream-buffer API
    should take into consideration whether buffers were freed to avoid
    accessing invalid addresses.
    
    Change-Id: I62db29c40133b3448dd487e30b0f8d70ee4c9b5f
    Signed-off-by: Hamad Kadmany <hkadmany@codeaurora.org>

--- drivers/media/platform/msm/dvb/demux/mpq_dmx_plugin_common.c
+++ drivers/media/platform/msm/dvb/demux/mpq_dmx_plugin_common.c
@@ -1480,7 +1480,6 @@ static int mpq_dmx_init_streambuffer(
 			dec_buffs->buffers_size,
 			dec_buffs->is_linear);
 
-	feed_data->buffer_desc.decoder_buffers_num = dec_buffs->buffers_num;
 	if (0 == dec_buffs->buffers_num)
 		ret = mpq_dmx_init_internal_buffers(
 			feed_data, dec_buffs, client);
@@ -1525,6 +1524,8 @@ static void mpq_dmx_release_streambuffer(
 		feed->dvb_demux_feed->feed.ts.decoder_buffers;
 
 	mpq_adapter_unregister_stream_if(feed_data->stream_interface);
+
+	mpq_streambuffer_terminate(video_buffer);
 
 	vfree(video_buffer->packet_data.data);
 
