42acdeff75430b12cb0e69824a1637538bab9396
Pawan Kumar <pavaku@codeaurora.org>
2013-03-21 19:55:49 +0530

    msm_fb: Fix color format mapping between V4L2 and MDP
    Add V4L2_PIX_FMT_UYVY, V4L2_PIX_FMT_YUYV, V4L2_PIX_FMT_YVU420
    format mapping in video driver. Add support  for
    MDP_CBYCRY_H2V1 format for mdp4.
    
    Change-Id: I4d5f1ae4ea71aa57c168f7143ff15f79e27cc1d0
    Signed-off-by: Pawan Kumar <pavaku@codeaurora.org>

--- include/linux/msm_mdp.h
+++ include/linux/msm_mdp.h
@@ -95,6 +95,7 @@ enum {
 	MDP_RGB_888,      /* RGB 888 planer */
 	MDP_Y_CRCB_H2V2,  /* Y and CrCb, pseudo planer w/ Cr is in MSB */
 	MDP_YCRYCB_H2V1,  /* YCrYCb interleave */
+	MDP_CBYCRY_H2V1,  /* CbYCrY interleave */
 	MDP_Y_CRCB_H2V1,  /* Y and CrCb, pseduo planer w/ Cr is in MSB */
 	MDP_Y_CBCR_H2V1,   /* Y and CrCb, pseduo planer w/ Cr is in MSB */
 	MDP_Y_CRCB_H1V2,
