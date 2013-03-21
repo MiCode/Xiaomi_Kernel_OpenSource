877fc301bdccc10cd4d4e925ba89b7b0a311ff98
Ravi Aravamudhan <aravamud@codeaurora.org>
2013-03-20 19:29:01 -0700

    diag: Add Support for Callback mode to communicate with remote processors
    Diag Callback client can send and receive data from the local
    processor. This change adds support for the callback clients to
    send and receive diag traffic to and from the remote processors.
    
    Change-Id: Id9a3a6ff6e9a86daa3194d2b0775879ea83f4d25
    Signed-off-by: Ravi Aravamudhan <aravamud@codeaurora.org>

--- include/linux/diagchar.h
+++ include/linux/diagchar.h
@@ -20,6 +20,7 @@
 #define DEINIT_TYPE			16
 #define USER_SPACE_DATA_TYPE		32
 #define DCI_DATA_TYPE			64
+#define CALLBACK_DATA_TYPE		128
 #define USB_MODE			1
 #define MEMORY_DEVICE_MODE		2
 #define NO_LOGGING_MODE			3
