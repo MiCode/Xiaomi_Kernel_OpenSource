e0380ec5dfb496365e8daa490390650818193f77
Ujwal Patel <ujwalp@codeaurora.org>
2013-03-21 19:37:27 -0700

    msm: mdss: remove early overlay kickoff for HDMI panel
    MDP driver sends an event to power on panel as soon as framebuffer is
    opened without waiting for resolution settings from user-space. This
    assumption works for panels where resolution is static. HDMI is a panel
    where resolution is dynamic and based on connected sink. So early power
    on for HDMI panel results in incorrect sequence of events. Correct this
    by eliminating early overlay kickoff for HDMI panel.
    
    Change-Id: I57c15123da873fed43a416d302298a0408147a98
    Signed-off-by: Ujwal Patel <ujwalp@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -1467,7 +1467,7 @@ static int mdss_mdp_overlay_on(struct msm_fb_data_type *mfd)
 
 	if (!mfd->panel_info->cont_splash_enabled) {
 		rc = mdss_mdp_overlay_start(mfd);
-		if (!IS_ERR_VALUE(rc))
+		if (!IS_ERR_VALUE(rc) && (mfd->panel_info->type != DTV_PANEL))
 			rc = mdss_mdp_overlay_kickoff(mfd->ctl);
 	} else {
 		rc = mdss_mdp_ctl_setup(mfd->ctl);
