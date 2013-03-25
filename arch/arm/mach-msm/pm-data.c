9a069584f26c47e716ba468f918c2c16a543df51
Venkat Devarasetty <vdevaras@codeaurora.org>
2013-03-25 23:37:45 +0530

    msm: pm: avoid array overrun for msm_pm_sleep_modes
    The array is not defined for the maximum possible size.
    So it is possible that the array may overrun when it is
    dereferenced using MSM_PM_MODE macro.
    
    Add a dummy entry to make sure array is of max size.
    
    Change-Id: I5783b0aa6c8295c5c5aabcb498700c6af1a3eba5
    Signed-off-by: Venkat Devarasetty <vdevaras@codeaurora.org>

--- arch/arm/mach-msm/pm-data.c
+++ arch/arm/mach-msm/pm-data.c
@@ -125,4 +125,11 @@ struct msm_pm_platform_data msm_pm_sleep_modes[] = {
 		.idle_enabled = 1,
 		.suspend_enabled = 0,
 	},
+
+	[MSM_PM_MODE(3, MSM_PM_SLEEP_MODE_NR)] = {
+		.idle_supported = 0,
+		.suspend_supported = 0,
+		.idle_enabled = 0,
+		.suspend_enabled = 0,
+	},
 };
