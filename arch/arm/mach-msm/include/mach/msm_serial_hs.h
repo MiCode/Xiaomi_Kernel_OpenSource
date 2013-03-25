3715fe6c342d2facb30c75594e4824c16e2ef828
Mayank Rana <mrana@codeaurora.org>
2013-03-25 18:33:58 +0530

    msm_serial_hs: Add support to get UART port reference using port index
    Add support to get reference of UART port using its port index.
    i.e. if you want reference of UART port for enumerated device as
    /dev/ttyHS10, then use the exported API with argument as 10.
    
    Change-Id: I45fc0f2e0d44b21653fd4c875fa3b2f5735fdf84
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>

--- arch/arm/mach-msm/include/mach/msm_serial_hs.h
+++ arch/arm/mach-msm/include/mach/msm_serial_hs.h
@@ -51,6 +51,7 @@ struct msm_serial_hs_platform_data {
 unsigned int msm_hs_tx_empty(struct uart_port *uport);
 void msm_hs_request_clock_off(struct uart_port *uport);
 void msm_hs_request_clock_on(struct uart_port *uport);
+struct uart_port *msm_hs_get_uart_port(int port_index);
 void msm_hs_set_mctrl(struct uart_port *uport,
 				    unsigned int mctrl);
 #endif
