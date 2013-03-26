c4b665b6b837f3d573c3a76d16749b06404a4b2f
Mona Hossain <mhossain@codeaurora.org>
2013-03-26 14:05:49 -0700

    crypto: msm: Fix XO shutdown issues
    ce-core_src clk is being left on preventing device from going
    into low power mode. Furthere more, clks are enabled without
    checking if the handle is valid.
    This fix removes enabling of ce_crore_src_clk. Also checks if
    clk handle is valid before enabling them.
    
    Change-Id: Ieb64b4b11385838ba18dd05d47ccc978e5b1bdf1
    Signed-off-by: Mona Hossain <mhossain@codeaurora.org>

--- drivers/crypto/msm/qce50.c
+++ drivers/crypto/msm/qce50.c
@@ -2728,6 +2728,7 @@ static int __qce_init_clk(struct qce_device *pce_dev)
 		rc = clk_set_rate(pce_dev->ce_core_src_clk, 100000000);
 		if (rc) {
 			clk_put(pce_dev->ce_core_src_clk);
+			pce_dev->ce_core_src_clk = NULL;
 			pr_err("Unable to set the core src clk @100Mhz.\n");
 			goto err_clk;
 		}
@@ -2735,7 +2736,6 @@ static int __qce_init_clk(struct qce_device *pce_dev)
 		pr_warn("Unable to get CE core src clk, set to NULL\n");
 		pce_dev->ce_core_src_clk = NULL;
 	}
-	pce_dev->ce_core_src_clk = ce_core_src_clk;
 
 	/* Get CE core clk */
 	ce_core_clk = clk_get(pce_dev->pdev, "core_clk");
@@ -2805,34 +2805,29 @@ static int __qce_enable_clk(void *handle)
 	int rc = 0;
 
 	/* Enable CE core clk */
-	rc = clk_prepare_enable(pce_dev->ce_core_clk);
-	if (rc) {
-		pr_err("Unable to enable/prepare CE core clk\n");
-		return rc;
+	if (pce_dev->ce_core_clk != NULL) {
+		rc = clk_prepare_enable(pce_dev->ce_core_clk);
+		if (rc) {
+			pr_err("Unable to enable/prepare CE core clk\n");
+			return rc;
+		}
 	}
 	/* Enable CE clk */
-	rc = clk_prepare_enable(pce_dev->ce_clk);
-	if (rc) {
-		pr_err("Unable to enable/prepare CE iface clk\n");
-		clk_disable_unprepare(pce_dev->ce_core_clk);
-	return rc;
+	if (pce_dev->ce_clk != NULL) {
+		rc = clk_prepare_enable(pce_dev->ce_clk);
+		if (rc) {
+			pr_err("Unable to enable/prepare CE iface clk\n");
+			clk_disable_unprepare(pce_dev->ce_core_clk);
+			return rc;
+		}
 	}
 	/* Enable AXI clk */
-	rc = clk_prepare_enable(pce_dev->ce_bus_clk);
-	if (rc) {
-		pr_err("Unable to enable/prepare CE BUS clk\n");
-		clk_disable_unprepare(pce_dev->ce_clk);
-		clk_disable_unprepare(pce_dev->ce_core_clk);
-		return rc;
-	}
-	/* Enable CORE SRC (INTERFACE)  clk */
-	if (pce_dev->ce_core_src_clk != NULL) {
-		rc = clk_prepare_enable(pce_dev->ce_core_src_clk);
+	if (pce_dev->ce_bus_clk != NULL) {
+		rc = clk_prepare_enable(pce_dev->ce_bus_clk);
 		if (rc) {
 			pr_err("Unable to enable/prepare CE BUS clk\n");
 			clk_disable_unprepare(pce_dev->ce_clk);
 			clk_disable_unprepare(pce_dev->ce_core_clk);
-			clk_disable_unprepare(pce_dev->ce_bus_clk);
 			return rc;
 		}
 	}
@@ -2846,8 +2841,6 @@ static int __qce_disable_clk(void *handle)
 
 	if (pce_dev->ce_clk != NULL)
 		clk_disable_unprepare(pce_dev->ce_clk);
-	if (pce_dev->ce_core_src_clk != NULL)
-		clk_disable_unprepare(pce_dev->ce_core_src_clk);
 	if (pce_dev->ce_core_clk != NULL)
 		clk_disable_unprepare(pce_dev->ce_core_clk);
 	if (pce_dev->ce_bus_clk != NULL)
