3d07bce49164722b2bf7f27c2220841c854374a6
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2013-03-27 15:16:27 +0530

    USB: dwc3: Add support for ZLP handling for control endpoint
    If the control data length is multiple of endpoint zero maximum
    packet size, DWC3 gadger driver is not sending zero length packet
    to inform Host of end of data transfer. Due to this, USB enumeration
    will fail. Hence add support for handling ZLP in case of control
    data length that is mulitple endpoint maximum packet size.
    
    CRs-Fixed: 465540
    Change-Id: I139e4798be46c5a3e1e6a6d10fa3cd425301a1eb
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>

--- drivers/usb/dwc3/ep0.c
+++ drivers/usb/dwc3/ep0.c
@@ -756,6 +756,7 @@ out:
 		dwc3_ep0_stall_and_restart(dwc);
 }
 
+bool zlp_required;
 static void dwc3_ep0_complete_data(struct dwc3 *dwc,
 		const struct dwc3_event_depevt *event)
 {
@@ -775,18 +776,27 @@ static void dwc3_ep0_complete_data(struct dwc3 *dwc,
 
 	r = next_request(&ep0->request_list);
 	ur = &r->request;
+	if ((epnum & 1) && ur->zero &&
+		(ur->length % ep0->endpoint.maxpacket == 0)) {
+		zlp_required = true;
+		ur->zero = false;
+	}
 
 	trb = dwc->ep0_trb;
 
 	status = DWC3_TRB_SIZE_TRBSTS(trb->size);
 	if (status == DWC3_TRBSTS_SETUP_PENDING) {
 		dev_dbg(dwc->dev, "Setup Pending received\n");
+		zlp_required = false;
 
 		if (r)
 			dwc3_gadget_giveback(ep0, r, -ECONNRESET);
 
 		return;
 	}
+
+	if (zlp_required)
+		return;
 
 	length = trb->size & DWC3_TRB_SIZE_MASK;
 
@@ -930,6 +940,10 @@ static void __dwc3_ep0_do_control_data(struct dwc3 *dwc,
 			return;
 		}
 
+		if (dep->number &&
+			!(req->request.length % dwc->gadget.ep0->maxpacket))
+			req->request.zero = true;
+
 		ret = dwc3_ep0_start_trans(dwc, dep->number, req->request.dma,
 				req->request.length, DWC3_TRBCTL_CONTROL_DATA);
 	}
@@ -993,7 +1007,11 @@ static void dwc3_ep0_end_control_data(struct dwc3 *dwc, struct dwc3_ep *dep)
 static void dwc3_ep0_xfernotready(struct dwc3 *dwc,
 		const struct dwc3_event_depevt *event)
 {
+	u8			epnum;
+	int			ret;
+
 	dwc->setup_packet_pending = true;
+	epnum = event->endpoint_number;
 
 	switch (event->status) {
 	case DEPEVT_STATUS_CONTROL_DATA:
@@ -1017,6 +1035,15 @@ static void dwc3_ep0_xfernotready(struct dwc3 *dwc,
 			return;
 		}
 
+		if (zlp_required) {
+			zlp_required = false;
+			ret = dwc3_ep0_start_trans(dwc, epnum,
+					dwc->ctrl_req_addr, 0,
+					DWC3_TRBCTL_CONTROL_DATA);
+			dbg_event(epnum, "ZLP", ret);
+			WARN_ON(ret < 0);
+		}
+
 		break;
 
 	case DEPEVT_STATUS_CONTROL_STATUS:
@@ -1025,6 +1052,7 @@ static void dwc3_ep0_xfernotready(struct dwc3 *dwc,
 
 		dev_vdbg(dwc->dev, "Control Status\n");
 
+		zlp_required = false;
 		dwc->ep0state = EP0_STATUS_PHASE;
 
 		if (dwc->delayed_status &&
