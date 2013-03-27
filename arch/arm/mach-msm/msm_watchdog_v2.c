2369ab7ddaecf9f94c2a76da743b58ad494e7ea1
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2013-03-26 22:15:50 -0700

    msm: msm_watchdog_v2: Initialize watchdog early to handle early failures
    Currently watchdog is initialized as late_initcall which is not
    helping in handling early failures. Hence initialize watchdog as
    pure_initcall to handle the aforementioned cases.
    
    Change-Id: Ia4ecee31b3557457905495a4870f527fb7e975aa
    Signed-off-by: Subbaraman Narayanamurthy <subbaram@codeaurora.org>

--- arch/arm/mach-msm/msm_watchdog_v2.c
+++ arch/arm/mach-msm/msm_watchdog_v2.c
@@ -568,6 +568,6 @@ static int init_watchdog(void)
 	return platform_driver_register(&msm_watchdog_driver);
 }
 
-late_initcall(init_watchdog);
+pure_initcall(init_watchdog);
 MODULE_DESCRIPTION("MSM Watchdog Driver");
 MODULE_LICENSE("GPL v2");
