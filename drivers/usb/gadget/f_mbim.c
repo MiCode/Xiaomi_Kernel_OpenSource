fa10543b718227f1d0abd2a6307afc92c45f8966
Amit Blay <ablay@codeaurora.org>
2013-03-27 15:46:08 +0200

    usb: gadget: mbim: Fix union descriptor for high-speed
    Fix MBIM to support a "Union Descriptor" for High-Speed connection.
    Without this fix, the union descriptor is only supported for
    Full-Speed connection.
    
    CRs-Fixed: 441117
    Change-Id: I6b03b675b48cfb9c69e1d1b20798a5a698b84d41
    Signed-off-by: Amit Blay <ablay@codeaurora.org>

--- drivers/usb/gadget/f_mbim.c
+++ drivers/usb/gadget/f_mbim.c
@@ -345,6 +345,7 @@ static struct usb_descriptor_header *mbim_hs_function[] = {
 	/* MBIM control descriptors */
 	(struct usb_descriptor_header *) &mbim_control_intf,
 	(struct usb_descriptor_header *) &mbim_header_desc,
+	(struct usb_descriptor_header *) &mbim_union_desc,
 	(struct usb_descriptor_header *) &mbb_desc,
 	(struct usb_descriptor_header *) &ext_mbb_desc,
 	(struct usb_descriptor_header *) &hs_mbim_notify_desc,
