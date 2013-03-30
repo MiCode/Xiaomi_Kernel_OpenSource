2314e15b6110d34f84fc518217e6bc5660bdd05b
Viresh Kumar <viresh.kumar@linaro.org>
2013-03-30 16:25:15 +0530

    cpufreq: rename index as driver_data in cpufreq_frequency_table
    The "index" field of struct cpufreq_frequency_table was never an
    index and isn't used at all by the cpufreq core.  It only is useful
    for cpufreq drivers for their internal purposes.
    
    Many people nowadays blindly set it in ascending order with the
    assumption that the core will use it, which is a mistake.
    
    Rename it to "driver_data" as that's what its purpose is. All of its
    users are updated accordingly.
    
    [rjw: Changelog]
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Acked-by: Simon Horman <horms+renesas@verge.net.au>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Git-commit: 5070158804b5339c71809f5e673cea1cfacd804d
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    [junjiew@codeaurora.org: update non-upstream files]
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -199,7 +199,7 @@ static int msm_cpufreq_target(struct cpufreq_policy *policy,
 	cpu_work = &per_cpu(cpufreq_work, policy->cpu);
 	cpu_work->policy = policy;
 	cpu_work->frequency = table[index].frequency;
-	cpu_work->index = table[index].index;
+	cpu_work->index = table[index].driver_data;
 	cpu_work->status = -ENODEV;
 
 	cancel_work_sync(&cpu_work->work);
@@ -271,7 +271,8 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 	 * Call set_cpu_freq unconditionally so that when cpu is set to
 	 * online, frequency limit will always be updated.
 	 */
-	ret = set_cpu_freq(policy, table[index].frequency, table[index].index);
+	ret = set_cpu_freq(policy, table[index].frequency,
+			   table[index].driver_data);
 	if (ret)
 		return ret;
 	pr_debug("cpufreq: cpu%d init at %d switching to %d\n",
@@ -455,7 +456,7 @@ static int cpufreq_parse_dt(struct device *dev)
 		if (i > 0 && f <= freq_table[i-1].frequency)
 			break;
 
-		freq_table[i].index = i;
+		freq_table[i].driver_data = i;
 		freq_table[i].frequency = f;
 
 		if (l2_clk) {
@@ -473,7 +474,7 @@ static int cpufreq_parse_dt(struct device *dev)
 		mem_bw[i] = data[j++];
 	}
 
-	freq_table[i].index = i;
+	freq_table[i].driver_data = i;
 	freq_table[i].frequency = CPUFREQ_TABLE_END;
 
 	devm_kfree(dev, data);
--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -414,7 +414,7 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_TABLE_END     ~1
 
 struct cpufreq_frequency_table {
-	unsigned int	index;     /* any */
+	unsigned int	driver_data; /* driver specific data, not used by core */
 	unsigned int	frequency; /* kHz - doesn't need to be in ascending
 				    * order */
 };
