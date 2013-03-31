91e5253e5382ee5b5f8b1dfa83965e6bff166ffc
Kuirong Wang <kuirongw@codeaurora.org>
2013-03-31 14:24:22 -0700

    ASoC: msm8x10-wcd: Update codec driver
    Update codec driver to remove virtual port widgets, add
    device tree parsing functions and route table, and fix playback
    and recording issue.
    
    Change-Id: Ie7ffac6f7a0d28fdbd3135d74f40ce5a340cf450
    Signed-off-by: Kuirong Wang <kuirongw@codeaurora.org>

--- sound/soc/codecs/msm8x10-wcd.h
+++ sound/soc/codecs/msm8x10-wcd.h
@@ -199,7 +199,7 @@ struct msm8x10_wcd {
 	int (*read_dev)(struct msm8x10_wcd *msm8x10,
 			unsigned short reg, unsigned int *val);
 	int (*write_dev)(struct msm8x10_wcd *msm8x10,
-			 unsigned short reg, unsigned int val);
+			 unsigned short reg, u8 val);
 
 	u32 num_of_supplies;
 	struct regulator_bulk_data *supplies;
