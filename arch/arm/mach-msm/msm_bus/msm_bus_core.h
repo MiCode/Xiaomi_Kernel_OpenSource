a38d2923d0894613ef0990b0225a919bc07e9a75
Gagan Mac <gmac@codeaurora.org>
2013-04-01 21:10:51 -0600

    msm: msm_bus: Fix the 64-bit division during interleaving
    When interleaving flag is enabled, the bandwidth is divided
    between ports. There was an error in the division as the
    macro was not correctly updated with the right order of using
    bandwidth and port number when 64-bit division function
    was used by the bus driver.
    Correcting this for fixing the token allocation issues
    observed.
    
    CRs-Fixed: 472791
    Change-Id: Ie7c7f9557006cbb946eaa9eff4b50771cc60525e
    Signed-off-by: Gagan Mac <gmac@codeaurora.org>

--- arch/arm/mach-msm/msm_bus/msm_bus_core.h
+++ arch/arm/mach-msm/msm_bus/msm_bus_core.h
@@ -36,7 +36,7 @@
 	(((slv >= MSM_BUS_SLAVE_FIRST) && (slv <= MSM_BUS_SLAVE_LAST)) ? 1 : 0)
 
 #define INTERLEAVED_BW(fab_pdata, bw, ports) \
-	((fab_pdata->il_flag) ? msm_bus_div64((bw), (ports)) : (bw))
+	((fab_pdata->il_flag) ? msm_bus_div64((ports), (bw)) : (bw))
 #define INTERLEAVED_VAL(fab_pdata, n) \
 	((fab_pdata->il_flag) ? (n) : 1)
 
