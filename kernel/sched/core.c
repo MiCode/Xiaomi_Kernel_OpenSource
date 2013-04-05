a32dfbad17fedc6a2e67f4c0eca9455d74f06816
Jordan Crouse <jcrouse@codeaurora.org>
2013-04-05 16:22:01 -0600

    sched: Mark schedule_io_timeout() with EXPORT_SYMBOL
    Make schedule_io_timeout() visible to modules.
    
    Change-Id: Ic0dedbad8a591a9a721f0d2e8f6c372ec75bc4b2
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4757,6 +4757,7 @@ long __sched io_schedule_timeout(long timeout)
 	delayacct_blkio_end();
 	return ret;
 }
+EXPORT_SYMBOL(io_schedule_timeout);
 
 /**
  * sys_sched_get_priority_max - return maximum RT priority.
