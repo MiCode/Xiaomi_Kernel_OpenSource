1361935a2e1bb902916205a83fa0a1f16268a8f3
Laura Abbott <lauraa@codeaurora.org>
2013-04-09 10:44:16 -0700

    msm: memory: Use phys_addr_t for memory reservation code
    When reserving memory, the reserved regions may be placed above
    a 32-bit address on LPAE based systems. Adjust the reservation code to
    account for this.
    
    Change-Id: If7e1775352d2557106cca0b50caec3d5134e0174
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- arch/arm/mach-msm/board-8960.c
+++ arch/arm/mach-msm/board-8960.c
@@ -236,7 +236,7 @@ static void __init reserve_rtb_memory(void)
 #endif
 }
 
-static int msm8960_paddr_to_memtype(unsigned int paddr)
+static int msm8960_paddr_to_memtype(phys_addr_t paddr)
 {
 	return MEMTYPE_EBI1;
 }
--- arch/arm/mach-msm/board-8974.c
+++ arch/arm/mach-msm/board-8974.c
@@ -59,7 +59,7 @@ static struct memtype_reserve msm8974_reserve_table[] __initdata = {
 	},
 };
 
-static int msm8974_paddr_to_memtype(unsigned int paddr)
+static int msm8974_paddr_to_memtype(phys_addr_t paddr)
 {
 	return MEMTYPE_EBI1;
 }
