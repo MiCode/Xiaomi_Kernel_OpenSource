676c8d03787f83e3e63e9f24f7c6c7d56b97bc85
Laura Abbott <lauraa@codeaurora.org>
2013-04-09 17:07:02 -0700

    mm: highmem: Add definitions for kmap_atomic_flush_unused
    kmap_flush_unused does not flush kmap_atomic mappings which
    are handled separately. Architectures may have use cases to require
    these to be flushed. Add an option to let architectures define
    kmap_atomic flushing to get rid of extra mappings.
    
    Change-Id: I5a338ed9e215f0f0ad3ab58a3066d2f4c8ce3ba7
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- include/linux/highmem.h
+++ include/linux/highmem.h
@@ -39,6 +39,12 @@ extern unsigned long totalhigh_pages;
 
 void kmap_flush_unused(void);
 
+#ifdef CONFIG_ARCH_WANT_KMAP_ATOMIC_FLUSH
+void kmap_atomic_flush_unused(void);
+#else
+static inline void kmap_atomic_flush_unused(void) { }
+#endif
+
 struct page *kmap_to_page(void *addr);
 
 #else /* CONFIG_HIGHMEM */
@@ -79,6 +85,7 @@ static inline void __kunmap_atomic(void *addr)
 #define kmap_atomic_to_page(ptr)	virt_to_page(ptr)
 
 #define kmap_flush_unused()	do {} while(0)
+#define kmap_atomic_flush_unused()	do {} while (0)
 #endif
 
 #endif /* CONFIG_HIGHMEM */
