bc174b81b0f497c27efa2437d68592b9faf28ca9
Mukesh Jha <cmjha@codeaurora.org>
2013-04-10 12:11:43 -0700

    msm: mdss: Support updates from both the mixers on splitdisplay
    Support updates in both mixers when running ARGC,PCC test
    (destination side) on split display.Allow the dualpipe case
    to update simultaneously on both right and left mixers by using
    "ctl" structures's pointers to left and right mixers instead of
    "split ctl" structures.
    
    Change-Id: Ic5fb291802af35f08870f2f121c5aff93d0b993e
    Signed-off-by: Mukesh Jha <cmjha@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_mdp_ctl.c
+++ drivers/video/fbdev/msm/mdss_mdp_ctl.c
@@ -1505,7 +1505,6 @@ int mdss_mdp_display_commit(struct mdss_mdp_ctl *ctl, void *arg)
 	mdss_mdp_pp_setup_locked(ctl);
 	mdss_mdp_ctl_write(ctl, MDSS_MDP_REG_CTL_FLUSH, ctl->flush_bits);
 	if (sctl) {
-		mdss_mdp_pp_setup_locked(sctl);
 		mdss_mdp_ctl_write(sctl, MDSS_MDP_REG_CTL_FLUSH,
 			sctl->flush_bits);
 	}
