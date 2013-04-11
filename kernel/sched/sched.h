702603d5e842c9937327dc2f7077c2f709b020a9
Neil Zhang <zhangwm@marvell.com>
2013-04-11 21:04:59 +0800

    sched: Remove redundant update_runtime notifier
    migration_call() will do all the things that update_runtime() does.
    So let's remove it.
    
    Furthermore, there is potential risk that the current code will catch
    BUG_ON at line 689 of rt.c when do cpu hotplug while there are realtime
    threads running because of enabling runtime twice while the rt_runtime
    may already changed.
    
    Change-Id: If2d953316d93c6b7e32f94bd49f2c10e64de6ed8
    Signed-off-by: Neil Zhang <zhangwm@marvell.com>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/1365685499-26515-1-git-send-email-zhangwm@marvell.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Git-commit: c5405a495e88d93cf9b4f4cc91507c7f4afcb901
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    [mattw@codeaurora.org: resolved trivial header file context conflict]
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -878,7 +878,6 @@ extern void sysrq_sched_debug_show(void);
 extern void sched_init_granularity(void);
 extern void update_max_interval(void);
 extern void update_group_power(struct sched_domain *sd, int cpu);
-extern int update_runtime(struct notifier_block *nfb, unsigned long action, void *hcpu);
 extern void init_sched_rt_class(void);
 extern void init_sched_fair_class(void);
 
