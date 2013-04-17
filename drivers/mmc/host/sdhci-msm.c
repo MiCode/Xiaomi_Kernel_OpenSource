23fa6c577bc06ca83ea46f47e3e08f8ffa8fe83c
Konstantin Dorfman <kdorfman@codeaurora.org>
2013-04-17 16:26:11 +0300

    mmc: sdhci-msm: enable stop transmission support
    This change enables handling of urgent data transfer request by setting
    MMC_CAP2_STOP_REQUEST bit of caps2 field.
    
    In order to decrease a latency of a prioritized requests (aka Urgent
    request), we might want to stop the transmission of a running "low
    priority" request in order to handle the Urgent request. The urgency of
    the request is decided by the block layer I/O scheduler. When the block
    layer notifies the MMC layer of an urgent request and if the MMC layer is
    blocked on current request to complete, it will be woken up, current
    request will be stopped, any outstanding requests are reinserted back to
    the block layer and urgent request execution started.
    
    Change-Id: I542da0b2230dd8c884902f3b14c13b0744aedaa1
    Signed-off-by: Konstantin Dorfman <kdorfman@codeaurora.org>

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -2232,6 +2232,7 @@ static int sdhci_msm_probe(struct platform_device *pdev)
 	msm_host->mmc->caps2 |= MMC_CAP2_CACHE_CTRL;
 	msm_host->mmc->caps2 |= MMC_CAP2_POWEROFF_NOTIFY;
 	msm_host->mmc->caps2 |= MMC_CAP2_CLK_SCALE;
+	msm_host->mmc->caps2 |= MMC_CAP2_STOP_REQUEST;
 
 	if (msm_host->pdata->nonremovable)
 		msm_host->mmc->caps |= MMC_CAP_NONREMOVABLE;
