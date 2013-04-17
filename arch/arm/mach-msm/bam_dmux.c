d23530550dfc83337ae2c5b204e7954652257f95
Brent Hronik <bhronik@codeaurora.org>
2013-04-17 15:07:19 -0600

    msm: bam_dmux: Fix potential memory leaks
    Added code to fix potential memory leaks.
    
    Change-Id: I76f92fc95539feee67600a6ab4a730bbb50643a1
    Signed-off-by: Brent Hronik <bhronik@codeaurora.org>

--- arch/arm/mach-msm/bam_dmux.c
+++ arch/arm/mach-msm/bam_dmux.c
@@ -913,8 +913,10 @@ int msm_bam_dmux_open(uint32_t id, void *priv,
 	if (!bam_is_connected) {
 		read_unlock(&ul_wakeup_lock);
 		ul_wakeup();
-		if (unlikely(in_global_reset == 1))
+		if (unlikely(in_global_reset == 1)) {
+			kfree(hdr);
 			return -EFAULT;
+		}
 		read_lock(&ul_wakeup_lock);
 		notify_all(BAM_DMUX_UL_CONNECTED, (unsigned long)(NULL));
 	}
