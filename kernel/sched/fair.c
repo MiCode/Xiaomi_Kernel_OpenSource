0134603b8bba7ce7c8ddd28628a2903af42c3f18
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2013-04-18 11:42:22 -0700

    sched: Fix SCHED_HRTICK bug leading to late preemption of tasks
    SCHED_HRTICK feature is useful to preempt SCHED_FAIR tasks on-the-dot
    (just when they would have exceeded their ideal_runtime). It makes use
    of a a per-cpu hrtimer resource and hence alarming that hrtimer should
    be based on total SCHED_FAIR tasks a cpu has across its various cfs_rqs,
    rather than being based on number of tasks in a particular cfs_rq (as
    implemented currently). As a result, with current code, its possible for
    a running task (which is the sole task in its cfs_rq) to be preempted
    much after its ideal_runtime has elapsed, resulting in increased latency
    for tasks in other cfs_rq on same cpu.
    
    Fix this by alarming sched hrtimer based on total number of SCHED_FAIR
    tasks a CPU has across its various cfs_rqs.
    
    Change-Id: I1f23680a64872f8ce0f451ac4bcae28e8967918f
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    [rameezmustafa@codeaurora.org]: Squash "c24fb502 sched: fix reference to
    				wrong cfs_rq" into this patch]
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3926,7 +3926,7 @@ static void hrtick_start_fair(struct rq *rq, struct task_struct *p)
 
 	WARN_ON(task_rq(p) != rq);
 
-	if (cfs_rq->nr_running > 1) {
+	if (rq->cfs.h_nr_running > 1) {
 		u64 slice = sched_slice(cfs_rq, se);
 		u64 ran = se->sum_exec_runtime - se->prev_sum_exec_runtime;
 		s64 delta = slice - ran;
@@ -3942,8 +3942,7 @@ static void hrtick_start_fair(struct rq *rq, struct task_struct *p)
 
 /*
  * called from enqueue/dequeue and updates the hrtick when the
- * current task is from our class and nr_running is low enough
- * to matter.
+ * current task is from our class.
  */
 static void hrtick_update(struct rq *rq)
 {
@@ -3952,8 +3951,7 @@ static void hrtick_update(struct rq *rq)
 	if (!hrtick_enabled(rq) || curr->sched_class != &fair_sched_class)
 		return;
 
-	if (cfs_rq_of(&curr->se)->nr_running < sched_nr_latency)
-		hrtick_start_fair(rq, curr);
+	hrtick_start_fair(rq, curr);
 }
 #else /* !CONFIG_SCHED_HRTICK */
 static inline void
