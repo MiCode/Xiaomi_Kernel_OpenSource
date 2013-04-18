7fbbddc1a5246059d14504b9be598ae5c5e3fe11
Jordan Crouse <jcrouse@codeaurora.org>
2013-04-18 14:19:51 -0600

    msm: kgsl: Only allow two counters for VBIF performance counters
    There are only two VBIF counter groups so validate that the user
    doesn't pass in > 1 and clean up the if/else clause.
    
    Change-Id: Ic0dedbad3d5a54e4ceb1a7302762d6bf13b25da1
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/adreno_a3xx.c
+++ drivers/gpu/msm/adreno_a3xx.c
@@ -2758,7 +2758,7 @@ static void a3xx_perfcounter_enable_vbif(struct kgsl_device *device,
 {
 	unsigned int in, out, bit, sel;
 
-	if (countable > 0x7f)
+	if (counter > 1 || countable > 0x7f)
 		return;
 
 	adreno_regread(device, A3XX_VBIF_PERF_CNT_EN, &in);
@@ -2767,7 +2767,7 @@ static void a3xx_perfcounter_enable_vbif(struct kgsl_device *device,
 	if (counter == 0) {
 		bit = VBIF_PERF_CNT_0;
 		sel = (sel & ~VBIF_PERF_CNT_0_SEL_MASK) | countable;
-	} else if (counter == 1) {
+	} else {
 		bit = VBIF_PERF_CNT_1;
 		sel = (sel & ~VBIF_PERF_CNT_1_SEL_MASK)
 			| (countable << VBIF_PERF_CNT_1_SEL);
