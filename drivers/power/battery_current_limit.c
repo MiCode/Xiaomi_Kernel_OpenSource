e2903290942791354b9c1d0714b50b03e5832b5d
Jennifer Liu <chihning@codeaurora.org>
2013-04-19 13:39:56 -0700

    power: battery_current_limit: Fix issue with subsecond polling
    Remove unecessary call to round_jiffies_relative() when scheduling
    delayed work. round_jiffies_relative() rounds scheduled work intervals
    to full second intervals. This driver needs to support polling intervals
    down to 10ms.
    
    Add debug logs to give indication of when polling attempts are made.
    These logs includes adding an error log if call to
    power_supply_get_by_name("battery"); fails, and debug logs for normal
    operation.
    
    CRs-Fixed: 477933
    Change-Id: Idf7dd88e3560ab097880096c33c07efe6bc1762e
    Signed-off-by: Jennifer Liu <chihning@codeaurora.org>

--- drivers/power/battery_current_limit.c
+++ drivers/power/battery_current_limit.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -120,8 +120,10 @@ static void bcl_calculate_imax_trigger(void)
 
 	if (psy == NULL) {
 		psy = power_supply_get_by_name("battery");
-		if (psy == NULL)
+		if (psy == NULL) {
+			pr_err("failed to get ps battery\n");
 			return;
+		}
 	}
 
 	if (psy->get_property(psy, POWER_SUPPLY_PROP_CURRENT_NOW, &ret))
@@ -143,6 +145,7 @@ static void bcl_calculate_imax_trigger(void)
 	gbcl->bcl_imax_ma = imax_ma;
 	gbcl->bcl_vbat_mv = vbatt_mv;
 
+	pr_debug("ibatt %d, imax %d, vbatt %d\n", ibatt_ma, imax_ma, vbatt_mv);
 	if (gbcl->bcl_threshold_mode[BCL_IBAT_IMAX_THRESHOLD_TYPE_HIGH]
 		== BCL_IBAT_IMAX_THRESHOLD_ENABLED) {
 		imax_high_threshold =
@@ -179,8 +182,7 @@ static void bcl_imax_work(struct work_struct *work)
 		bcl_calculate_imax_trigger();
 		/* restart the delay work for caculating imax */
 		schedule_delayed_work(&bcl->bcl_imax_work,
-			round_jiffies_relative(msecs_to_jiffies
-				(bcl->bcl_poll_interval_msec)));
+			msecs_to_jiffies(bcl->bcl_poll_interval_msec));
 	}
 }
 
