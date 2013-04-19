d8bf54767c75beebb838231cc77c7a6f83d9bff4
Manu Gautam <mgautam@codeaurora.org>
2013-04-19 14:45:37 +0530

    USB: gadget: Define gadget_is_dwc3
    Some function drivers e.g. QDSS require to know if gadget is DWC3 or
    chipidea in order to set correct flags while queueing a request.
    
    Signed-off-by: Manu Gautam <mgautam@codeaurora.org>

--- drivers/usb/gadget/gadget_chips.h
+++ drivers/usb/gadget/gadget_chips.h
@@ -28,6 +28,7 @@
  * do that for you.
  */
 #define gadget_is_at91(g)		(!strcmp("at91_udc", (g)->name))
+#define gadget_is_dwc3(g)		(!strcmp("dwc3-gadget", (g)->name))
 #define gadget_is_goku(g)		(!strcmp("goku_udc", (g)->name))
 #define gadget_is_musbhdrc(g)		(!strcmp("musb-hdrc", (g)->name))
 #define gadget_is_net2280(g)		(!strcmp("net2280", (g)->name))
