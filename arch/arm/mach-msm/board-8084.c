78723c6233ffad10ace436e8a0ba1ed47c2e41fe
Hemant Kumar <hemantk@codeaurora.org>
2013-04-19 18:35:26 -0700

    msm: board-8084: Add dummy clocks for primary SSUSB core
    Dummy clocks are added for dwc3 msm SSUSB driver to enable
    usb device enumeration on simulator target.
    
    Change-Id: I0947318ee149e19090dd9585cd26738920826fe8
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- arch/arm/mach-msm/board-8084.c
+++ arch/arm/mach-msm/board-8084.c
@@ -81,6 +81,13 @@ static struct clk_lookup msm_clocks_dummy[] = {
 	CLK_DUMMY("iface_clk",	SDC1_P_CLK,	"msm_sdcc.1", OFF),
 	CLK_DUMMY("core_clk",	SDC2_CLK,	"msm_sdcc.2", OFF),
 	CLK_DUMMY("iface_clk",	SDC2_P_CLK,	"msm_sdcc.2", OFF),
+	CLK_DUMMY("xo",   NULL, "f9200000.qcom,ssusb", OFF),
+	CLK_DUMMY("core_clk",   NULL, "f9200000.qcom,ssusb", OFF),
+	CLK_DUMMY("iface_clk",  NULL, "f9200000.qcom,ssusb", OFF),
+	CLK_DUMMY("sleep_clk",  NULL, "f9200000.qcom,ssusb", OFF),
+	CLK_DUMMY("sleep_a_clk",   NULL, "f9200000.qcom,ssusb", OFF),
+	CLK_DUMMY("utmi_clk",   NULL, "f9200000.qcom,ssusb", OFF),
+	CLK_DUMMY("ref_clk",    NULL, "f9200000.qcom,ssusb", OFF),
 };
 
 static struct clock_init_data msm_dummy_clock_init_data __initdata = {
