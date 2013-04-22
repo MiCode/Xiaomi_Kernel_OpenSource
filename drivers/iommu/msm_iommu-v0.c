894caf2108b45057494b23758724d9a99cbf3a94
Laura Abbott <lauraa@codeaurora.org>
2013-04-22 10:01:55 -0700

    iommu: msm: Don't treat address 0 as an error case
    Currently, the iommu page table code treats a scattergather
    list with physical address 0 as an error. This may not be
    correct in all cases. Physical address 0 is a valid part
    of the system and may be used for valid page allocations.
    Nothing else in the system checks for physical address 0
    for error so don't treat it as an error.
    
    Change-Id: Ie9f0dae9dace4fff3b1c3449bc89c3afdd2e63a0
    CRs-Fixed: 478304
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- drivers/iommu/msm_iommu-v0.c
+++ drivers/iommu/msm_iommu-v0.c
@@ -1026,12 +1026,6 @@ static int msm_iommu_map_range(struct iommu_domain *domain, unsigned int va,
 				chunk_offset = 0;
 				sg = sg_next(sg);
 				pa = get_phys_addr(sg);
-				if (pa == 0) {
-					pr_debug("No dma address for sg %p\n",
-							sg);
-					ret = -EINVAL;
-					goto fail;
-				}
 			}
 			continue;
 		}
@@ -1085,12 +1079,6 @@ static int msm_iommu_map_range(struct iommu_domain *domain, unsigned int va,
 				chunk_offset = 0;
 				sg = sg_next(sg);
 				pa = get_phys_addr(sg);
-				if (pa == 0) {
-					pr_debug("No dma address for sg %p\n",
-							sg);
-					ret = -EINVAL;
-					goto fail;
-				}
 			}
 		}
 
