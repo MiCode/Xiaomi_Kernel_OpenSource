0fcbdd6cff0b818e306c6eb337e799701a434ca1
Huaibin Yang <huaibiny@codeaurora.org>
2013-04-22 17:08:50 -0700

    msm: mdss: allow not having frame buffer memory
    If pan_display is not used, the frame buffer memory is not reserved
    and allocated. Adding checks to make sure no memory operations on
    framebuffer.
    
    Change-Id: Ia837d4ea0cad4e40e52ee5817c8d58e7db852a97
    Signed-off-by: Huaibin Yang <huaibiny@codeaurora.org>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -656,6 +656,11 @@ static int mdss_fb_mmap(struct fb_info *info, struct vm_area_struct *vma)
 	unsigned long off = vma->vm_pgoff << PAGE_SHIFT;
 	struct msm_fb_data_type *mfd = (struct msm_fb_data_type *)info->par;
 
+	if (!start) {
+		pr_warn("No framebuffer memory is allocated.\n");
+		return -ENOMEM;
+	}
+
 	mdss_fb_pan_idle(mfd);
 	if (off >= len) {
 		/* memory mapped io */
