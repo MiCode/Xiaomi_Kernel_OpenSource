4364eca9c7d371a26fbe8477afa418120ecc64c7
Laura Abbott <lauraa@codeaurora.org>
2013-04-24 16:13:03 -0700

    msm: pm-8x60: Use dma_alloc_coherent for the saved region
    dma_alloc_coherent is the linux standard way of allocating coherent
    (i.e. uncached) memory. This API provides more guarantess than the
    custom allocate_contiguous_ebi function, including the ability to
    specify allocations from the lower 4G of memory. Switch to this API
    for allocation of uncached memory. Because using dma_alloc_coherent
    may have other setup involved, switch the setup function to be a
    later initcall as well (from core_initcall to arch_initcall).
    
    Change-Id: I96ea4575e5ee82ccbf7e691ba75ea1414dc6d81a
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- arch/arm/mach-msm/pm-8x60.c
+++ arch/arm/mach-msm/pm-8x60.c
@@ -11,6 +11,7 @@
  *
  */
 
+#include <linux/dma-mapping.h>
 #include <linux/debugfs.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -1291,6 +1292,7 @@ static int __init msm_pm_setup_saved_state(void)
 	pmd_t *pmd;
 	unsigned long pmdval;
 	unsigned long exit_phys;
+	dma_addr_t temp_phys;
 
 	/* Page table for cores to come back up safely. */
 	pc_pgd = pgd_alloc(&init_mm);
@@ -1306,16 +1308,20 @@ static int __init msm_pm_setup_saved_state(void)
 	pmd[0] = __pmd(pmdval);
 	pmd[1] = __pmd(pmdval + (1 << (PGDIR_SHIFT - 1)));
 
-	msm_saved_state_phys =
-		allocate_contiguous_ebi_nomap(CPU_SAVED_STATE_SIZE *
-					      num_possible_cpus(), 4);
-	if (!msm_saved_state_phys)
-		return -ENOMEM;
-	msm_saved_state = ioremap_nocache(msm_saved_state_phys,
-					  CPU_SAVED_STATE_SIZE *
-					  num_possible_cpus());
+	msm_saved_state = dma_zalloc_coherent(NULL, CPU_SAVED_STATE_SIZE *
+						num_possible_cpus(),
+						&temp_phys, 0);
+
 	if (!msm_saved_state)
 		return -ENOMEM;
+
+	/*
+	 * Explicitly cast here since msm_saved_state_phys is defined
+	 * in assembly and we want to avoid any kind of truncation
+	 * or endian problems.
+	 */
+	msm_saved_state_phys = (unsigned long)temp_phys;
+
 
 	/* It is remotely possible that the code in msm_pm_collapse_exit()
 	 * which turns on the MMU with this mapping is in the
@@ -1331,7 +1337,7 @@ static int __init msm_pm_setup_saved_state(void)
 
 	return 0;
 }
-core_initcall(msm_pm_setup_saved_state);
+arch_initcall(msm_pm_setup_saved_state);
 
 static void setup_broadcast_timer(void *arg)
 {
