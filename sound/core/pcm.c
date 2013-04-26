58f7b95476cd3803d44dd4b20612729acfd5519f
Subhash Chandra Bose Naripeddy <snariped@codeaurora.org>
2013-04-25 19:32:52 -0700

    ALSA: PCM: volume API implementation
    Introduced a new helper function snd_pcm_add_volume_ctls() to
    create control elements representing the volume for each PCM
    (sub)stream.
    
    Change-Id: Id483257c4fe84a909e7a4af3a5aa3e4f0d5d9ea5
    Signed-off-by: Damir Didjusto <damird@codeaurora.org>

--- sound/core/pcm.c
+++ sound/core/pcm.c
@@ -1199,6 +1199,10 @@ static int snd_pcm_dev_disconnect(struct snd_device *device)
 			snd_ctl_remove(pcm->card, pcm->streams[cidx].chmap_kctl);
 			pcm->streams[cidx].chmap_kctl = NULL;
 		}
+		if (pcm->streams[cidx].vol_kctl) {
+			snd_ctl_remove(pcm->card, pcm->streams[cidx].vol_kctl);
+			pcm->streams[cidx].vol_kctl = NULL;
+		}
 	}
  unlock:
 	mutex_unlock(&register_mutex);
