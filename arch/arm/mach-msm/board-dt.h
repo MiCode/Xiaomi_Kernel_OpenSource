616700b91dd2e4718d1d93a501173e5e6ef44082
Stepan Moskovchenko <stepanm@codeaurora.org>
2013-04-25 17:10:55 -0700

    arm/dt: msm: Move I/O devices to a dedicated SoC node
    The Device Tree specification suggests that memory-mapped
    I/O devices should be declared within the 'soc' node rather
    than being declared at the root level. Refactor the MSM
    device tree files to follow to this design pattern.
    
    Furthermore, declaring I/O devices with the 'soc' node
    facilitates reuse of common DTSI files across device trees
    whose root-level #address-cells and #size-cells properties
    might be different.
    
    Explicitly call of_platform_populate on the /soc node to
    parent its devices to the root node, to preserve the kernel
    ABI until userspace is updated.
    
    Change-Id: Ib6a4a42c3a3d419b5f8d8c49706ed5d78c1b6adc
    Signed-off-by: Stepan Moskovchenko <stepanm@codeaurora.org>

--- arch/arm/mach-msm/board-dt.h
+++ arch/arm/mach-msm/board-dt.h
@@ -10,8 +10,11 @@
  * GNU General Public License for more details.
  */
 
+#include <linux/of_platform.h>
+
 void __init msm_dt_init_irq(void);
 void __init msm_dt_init_irq_nompm(void);
 void __init msm_dt_init_irq_l2x0(void);
 int __init msm_scan_dt_map_imem(unsigned long node, const char *uname,
 				int depth, void *data);
+void __init board_dt_populate(struct of_dev_auxdata *adata);
