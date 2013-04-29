e2aa60c9b67b1cde82d461fe15bb3aa34d9c5389
Xiaozhe Shi <xiaozhes@codeaurora.org>
2013-04-29 16:41:58 -0700

    power: qpnp-bms: detect warm resets
    During warm PMIC resets, the BMS will not take a new OCV. This may
    cause the SOC upon reboot to be completely wrong if no recent OCVs
    have been taken.
    
    Fix this by checking for invalid OCVs and warm resets. If either
    occur, estimate a new OCV based on vbat and use that instead.
    
    Change-Id: I2e72cfc3dfc99bac7d440da403cf64847795992e
    Signed-off-by: Xiaozhe Shi <xiaozhes@codeaurora.org>

--- drivers/power/qpnp-bms.c
+++ drivers/power/qpnp-bms.c
@@ -24,6 +24,7 @@
 #include <linux/rtc.h>
 #include <linux/delay.h>
 #include <linux/qpnp/qpnp-adc.h>
+#include <linux/qpnp/power-on.h>
 #include <linux/mfd/pm8xxx/batterydata-lib.h>
 #include <linux/wakelock.h>
 
@@ -681,10 +682,12 @@ static void reset_for_new_battery(struct qpnp_bms_chip *chip, int batt_temp)
 }
 
 #define OCV_RAW_UNINITIALIZED	0xFFFF
+#define MIN_OCV_UV		2000000
 static int read_soc_params_raw(struct qpnp_bms_chip *chip,
 				struct raw_soc_params *raw,
 				int batt_temp)
 {
+	bool warm_reset = false;
 	int rc;
 
 	mutex_lock(&chip->bms_output_lock);
@@ -717,7 +720,18 @@ static int read_soc_params_raw(struct qpnp_bms_chip *chip,
 
 	if (chip->prev_last_good_ocv_raw == OCV_RAW_UNINITIALIZED) {
 		convert_and_store_ocv(chip, raw, batt_temp);
-		pr_debug("PON_OCV_UV = %d\n", chip->last_ocv_uv);
+		pr_debug("PON_OCV_UV = %d, cc = %llx\n",
+				chip->last_ocv_uv, raw->cc);
+		warm_reset = qpnp_pon_is_warm_reset();
+		if (raw->last_good_ocv_uv < MIN_OCV_UV
+				|| warm_reset > 0) {
+			pr_debug("OCV is stale or bad, estimating new OCV.\n");
+			chip->last_ocv_uv = estimate_ocv(chip);
+			raw->last_good_ocv_uv = chip->last_ocv_uv;
+			reset_cc(chip);
+			pr_debug("New PON_OCV_UV = %d, cc = %llx\n",
+					chip->last_ocv_uv, raw->cc);
+		}
 	} else if (chip->new_battery) {
 		/* if a new battery was inserted, estimate the ocv */
 		reset_for_new_battery(chip, batt_temp);
@@ -2441,6 +2455,7 @@ static int qpnp_bms_probe(struct spmi_device *spmi)
 {
 	struct qpnp_bms_chip *chip;
 	union power_supply_propval retval = {0,};
+	bool warm_reset;
 	int rc, vbatt;
 
 	chip = kzalloc(sizeof *chip, GFP_KERNEL);
@@ -2461,6 +2476,11 @@ static int qpnp_bms_probe(struct spmi_device *spmi)
 		pr_info("iadc not ready: %d, deferring probe\n", rc);
 		goto error_read;
 	}
+
+	warm_reset = qpnp_pon_is_warm_reset();
+	rc = warm_reset;
+	if (rc < 0)
+		goto error_read;
 
 	rc = register_spmi(chip, spmi);
 	if (rc) {
@@ -2572,9 +2592,9 @@ static int qpnp_bms_probe(struct spmi_device *spmi)
 		goto unregister_dc;
 	}
 
-	pr_info("probe success: soc =%d vbatt = %d ocv = %d r_sense_uohm = %u\n",
-				get_prop_bms_capacity(chip),
-				vbatt, chip->last_ocv_uv, chip->r_sense_uohm);
+	pr_info("probe success: soc =%d vbatt = %d ocv = %d r_sense_uohm = %u warm_reset = %d\n",
+			get_prop_bms_capacity(chip), vbatt, chip->last_ocv_uv,
+			chip->r_sense_uohm, warm_reset);
 	return 0;
 
 unregister_dc:
