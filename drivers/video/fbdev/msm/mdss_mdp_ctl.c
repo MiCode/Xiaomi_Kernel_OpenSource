ce073779445b7efd903aa089528ecc5c21028a79
Carl Vanderlip <carlv@codeaurora.org>
2013-04-30 13:28:12 -0700

    msm: mdss: Configure IGC for limited range values
    Support limited range DTV output by configuring IGC values to limit output
    range to 16-235.
    
    Change-Id: I6ce59b5aa6cd859f6c3a526549fc5b05021ec8be
    Signed-off-by: Mukesh Jha <cmjha@codeaurora.org>
    Signed-off-by: Carl Vanderlip <carlv@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_mdp_ctl.c
+++ drivers/video/fbdev/msm/mdss_mdp_ctl.c
@@ -711,6 +711,9 @@ struct mdss_mdp_ctl *mdss_mdp_ctl_init(struct mdss_panel_data *pdata,
 		ctl->intf_type = MDSS_INTF_HDMI;
 		ctl->opmode = MDSS_MDP_CTL_OP_VIDEO_MODE;
 		ctl->start_fnc = mdss_mdp_video_start;
+		ret = mdss_mdp_limited_lut_igc_config(ctl);
+		if (ret)
+			pr_err("Unable to config IGC LUT data");
 		break;
 	case WRITEBACK_PANEL:
 		ctl->intf_num = MDSS_MDP_NO_INTF;
