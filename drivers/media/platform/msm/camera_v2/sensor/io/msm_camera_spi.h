a907f0a587b712ad34972401b46f7f107c3297e6
Junjie Wu <junjiew@codeaurora.org>
2013-05-01 16:50:42 -0700

    msm: camera: Improve interface for large SPI reads
    For each SPI read, two buffers of read size + header need to be
    allocated.  These allocation might fail if read size is large.  It's also
    expensive to allocate memory each time for repeated transfers.
    Therefore, msm_camera_spi_read_seq_l() is provided to use pre-allocated
    buffers instead of allocating them each time.
    
    Change-Id: Ibd52fd26bb0a4fdd913bef9c2bfc410152245c3b
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_spi.h
+++ drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_spi.h
@@ -17,6 +17,12 @@
 #include <media/msm_cam_sensor.h>
 #include "msm_camera_i2c.h"
 
+/**
+  * Common SPI communication scheme
+  * tx: <opcode>[addr][wait][write buffer]
+  * rx: [read buffer]
+  * Some inst require polling busy reg until it's done
+  */
 struct msm_camera_spi_inst {
 	uint8_t opcode;		/* one-byte opcode */
 	uint8_t addr_len;	/* addr len in bytes */
@@ -38,12 +44,21 @@ struct msm_camera_spi_client {
 	uint8_t retries;	/* retry times upon failure */
 };
 
+static __always_inline
+uint16_t msm_camera_spi_get_hlen(struct msm_camera_spi_inst *inst)
+{
+	return sizeof(inst->opcode) + inst->addr_len + inst->dummy_len;
+}
+
 int32_t msm_camera_spi_read(struct msm_camera_i2c_client *client,
 	uint32_t addr, uint16_t *data,
 	enum msm_camera_i2c_data_type data_type);
 
 int32_t msm_camera_spi_read_seq(struct msm_camera_i2c_client *client,
 	uint32_t addr, uint8_t *data, uint32_t num_byte);
+
+int32_t msm_camera_spi_read_seq_l(struct msm_camera_i2c_client *client,
+	uint32_t addr, uint32_t num_byte, char *tx, char *rx);
 
 int32_t msm_camera_spi_query_id(struct msm_camera_i2c_client *client,
 	uint32_t addr, uint8_t *data, uint32_t num_byte);
