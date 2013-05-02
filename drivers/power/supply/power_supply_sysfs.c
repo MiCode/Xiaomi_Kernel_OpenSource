540f4ebc7aec5f3eca71bde7f28ccd738b889bd8
Xiaozhe Shi <xiaozhes@codeaurora.org>
2013-05-02 15:54:27 -0700

    power_supply: Add RESISTANCE property to power supply
    Add a "RESISTANCE" property to the power supply frame work that
    signifies the internal resistance of the power supply. The units
    for this property should be in micro-ohms.
    
    Change-Id: I2fcd3e7e314e61812d364dc0bd85ef1c8eec1622
    Signed-off-by: Xiaozhe Shi <xiaozhes@codeaurora.org>
    Signed-off-by: David Collins <collinsd@codeaurora.org>

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -216,6 +216,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(scope),
 	POWER_SUPPLY_ATTR(charge_term_current),
 	POWER_SUPPLY_ATTR(calibrate),
+	POWER_SUPPLY_ATTR(resistance),
 	/* Local extensions */
 	POWER_SUPPLY_ATTR(usb_hc),
 	POWER_SUPPLY_ATTR(usb_otg),
