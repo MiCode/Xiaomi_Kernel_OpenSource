4c61feae426f7453a11e0965b8a39bf47708de4b
Steve Muckle <smuckle@codeaurora.org>
2013-05-08 10:30:54 -0700

    msm: dcvs: fail gracefully if TZ does not support dcvs
    If dcvs attempts to initialize on a system where TZ does not support
    dcvs, it will eventually cause a crash. Prevent this by disabling
    dcvs if TZ does not successfully initialize its part of dcvs.
    
    Change-Id: Ic9cd361243c03c3832133614c4847a2815b0bbb4
    Signed-off-by: Steve Muckle <smuckle@codeaurora.org>

--- arch/arm/mach-msm/msm_dcvs.c
+++ arch/arm/mach-msm/msm_dcvs.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1026,6 +1026,9 @@ int msm_dcvs_register_core(
 	uint32_t ret1;
 	uint32_t ret2;
 
+	if (!msm_dcvs_enabled)
+		return ret;
+
 	offset = get_core_offset(type, type_core_num);
 	if (offset < 0)
 		return ret;
@@ -1277,6 +1280,9 @@ static int __init msm_dcvs_late_init(void)
 	struct kobject *module_kobj = NULL;
 	int ret = 0;
 
+	if (!msm_dcvs_enabled)
+		return ret;
+
 	module_kobj = kset_find_obj(module_kset, KBUILD_MODNAME);
 	if (!module_kobj) {
 		pr_err("%s: cannot find kobject for module %s\n",
@@ -1343,6 +1349,7 @@ static int __init msm_dcvs_early_init(void)
 	ret = msm_dcvs_scm_init(SZ_32K);
 	if (ret) {
 		__err("Unable to initialize DCVS err=%d\n", ret);
+		msm_dcvs_enabled = 0;
 		goto done;
 	}
 
