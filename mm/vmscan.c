434fe4e62b1761cc5e80563a0676f4fb69ef678d
Minchan Kim <minchan@kernel.org>
2013-05-09 16:21:27 +0900

    mm: Enhance per process reclaim to consider shared pages
    Some pages could be shared by several processes. (ex, libc)
    In case of that, it's too bad to reclaim them from the beginnig.
    
    This patch causes VM to keep them on memory until last task
    try to reclaim them so shared pages will be reclaimed only if
    all of task has gone swapping out.
    
    This feature doesn't handle non-linear mapping on ramfs because
    it's very time-consuming and doesn't make sure of reclaiming and
    not common.
    
    Change-Id: I7e5f34f2e947f5db6d405867fe2ad34863ca40f7
    Signed-off-by: Sangseok Lee <sangseok.lee@lge.com>
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Patch-mainline: linux-mm @ 9 May 2013 16:21:27
    [vinmenon@codeaurora.org: trivial merge conflict fixes + changes
    to make the patch work with 4.14 kernel + Avoid targetted reclaim
    from ksm since vma_address(page) is not valid for a ksm page]
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -116,6 +116,13 @@ struct scan_control {
 
 	/* Number of pages freed so far during a call to shrink_zones() */
 	unsigned long nr_reclaimed;
+
+	/*
+	 * Reclaim pages from a vma. If the page is shared by other tasks
+	 * it is zapped from a vma without reclaim so it ends up remaining
+	 * on memory until last task zap it.
+	 */
+	struct vm_area_struct *target_vma;
 };
 
 #ifdef ARCH_HAS_PREFETCH
@@ -1189,7 +1196,7 @@ static unsigned long shrink_page_list(struct list_head *page_list,
 
 			if (unlikely(PageTransHuge(page)))
 				flags |= TTU_SPLIT_HUGE_PMD;
-			if (!try_to_unmap(page, flags)) {
+			if (!try_to_unmap(page, flags, sc->target_vma)) {
 				nr_unmap_fail++;
 				goto activate_locked;
 			}
@@ -1412,7 +1419,8 @@ unsigned long reclaim_clean_pages_from_list(struct zone *zone,
 }
 
 #ifdef CONFIG_PROCESS_RECLAIM
-unsigned long reclaim_pages_from_list(struct list_head *page_list)
+unsigned long reclaim_pages_from_list(struct list_head *page_list,
+					struct vm_area_struct *vma)
 {
 	struct scan_control sc = {
 		.gfp_mask = GFP_KERNEL,
@@ -1420,6 +1428,7 @@ unsigned long reclaim_pages_from_list(struct list_head *page_list)
 		.may_writepage = 1,
 		.may_unmap = 1,
 		.may_swap = 1,
+		.target_vma = vma,
 	};
 
 	unsigned long nr_reclaimed;
