86596799c57423d60fa5b98e4fff2cf87f4ffbe4
Minchan Kim <minchan@kernel.org>
2013-05-09 16:21:24 +0900

    mm: Per process reclaim
    These day, there are many platforms avaiable in the embedded market
    and they are smarter than kernel which has very limited information
    about working set so they want to involve memory management more heavily
    like android's lowmemory killer and ashmem or recent many lowmemory
    notifier(there was several trial for various company NOKIA, SAMSUNG,
    Linaro, Google ChromeOS, Redhat).
    
    One of the simple imagine scenario about userspace's intelligence is that
    platform can manage tasks as forground and backgroud so it would be
    better to reclaim background's task pages for end-user's *responsibility*
    although it has frequent referenced pages.
    
    This patch adds new knob "reclaim under proc/<pid>/" so task manager
    can reclaim any target process anytime, anywhere. It could give another
    method to platform for using memory efficiently.
    
    It can avoid process killing for getting free memory, which was really
    terrible experience because I lost my best score of game I had ever
    after I switch the phone call while I enjoyed the game.
    
    Reclaim file-backed pages only.
    	echo file > /proc/PID/reclaim
    Reclaim anonymous pages only.
    	echo anon > /proc/PID/reclaim
    Reclaim all pages
    	echo all > /proc/PID/reclaim
    
    Change-Id: Iabdb7bc2ef3dc4d94e3ea005fbe18f4cd06739ab
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Patch-mainline: linux-mm @ 9 May 2013 16:21:24
    [vinmenon@codeaurora.org: trivial merge conflict fixes]
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- fs/proc/internal.h
+++ fs/proc/internal.h
@@ -204,6 +204,7 @@ struct pde_opener {
 };
 
 extern const struct inode_operations proc_pid_link_inode_operations;
+extern const struct file_operations proc_reclaim_operations;
 
 extern void proc_init_inodecache(void);
 extern struct inode *proc_get_inode(struct super_block *, struct proc_dir_entry *);
