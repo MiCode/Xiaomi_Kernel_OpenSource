fd3a0c0194efe57afe82c039a70743319ecd7e4d
Minchan Kim <minchan@kernel.org>
2013-05-09 16:21:24 +0900

    mm: Per process reclaim
    These day, there are many platforms available in the embedded market
    and they are smarter than kernel which has very limited information
    about working set so they want to involve memory management more heavily
    like android's lowmemory killer and ashmem or recent many lowmemory
    notifier.
    
    One of the simple imagine scenario about userspace's intelligence is that
    platform can manage tasks as forground and background so it would be
    better to reclaim background's task pages for end-user's *responsibility*
    although it has frequent referenced pages.
    
    This patch adds new knob "reclaim under proc/<pid>/" so task manager
    can reclaim any target process anytime, anywhere. It could give another
    method to platform for using memory efficiently.
    
    It can avoid process killing for getting free memory, which was really
    terrible experience because I lost my best score of game I had ever
    after I switch the phone call while I enjoyed the game.
    
    Reclaim file-backed pages only.
    	echo file > /proc/PID/reclaim
    Reclaim anonymous pages only.
    	echo anon > /proc/PID/reclaim
    Reclaim all pages
    	echo all > /proc/PID/reclaim
    
    Change-Id: Iabdb7bc2ef3dc4d94e3ea005fbe18f4cd06739ab
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Patch-mainline: linux-mm @ 9 May 2013 16:21:24
    [vinmenon@codeaurora.org: merge conflict fixes, minor tweak of the commit msg]
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- fs/proc/internal.h
+++ fs/proc/internal.h
@@ -208,6 +208,7 @@ struct pde_opener {
 extern const struct inode_operations proc_link_inode_operations;
 
 extern const struct inode_operations proc_pid_link_inode_operations;
+extern const struct file_operations proc_reclaim_operations;
 
 void proc_init_kmemcache(void);
 void set_proc_pid_nlink(void);
