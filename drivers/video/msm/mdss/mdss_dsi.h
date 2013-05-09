ae10ec26454d313af36ac3c4c9cbc01585c8d871
Adrian Salido-Moreno <adrianm@codeaurora.org>
2013-05-08 20:10:40 -0700

    msm: mdss: add DSI state machine
    Maintain state of DSI panel, and avoid performing some operations
    depending on the current state.
    
    Change-Id: Id8c2cc30e5058dea7585cc9d06b2ab11368bc337
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi.h
+++ drivers/video/msm/mdss/mdss_dsi.h
@@ -89,6 +89,12 @@ enum dsi_ctrl_state {
 	DSI_HS_MODE,
 };
 
+enum dsi_panel_state {
+	UNKNOWN_STATE,
+	PANEL_ON,
+	PANEL_OFF,
+};
+
 #define DSI_NON_BURST_SYNCH_PULSE	0
 #define DSI_NON_BURST_SYNCH_EVENT	1
 #define DSI_BURST_MODE			2
@@ -283,6 +289,7 @@ struct mdss_dsi_ctrl_pdata {
 	struct clk *byte_clk;
 	struct clk *esc_clk;
 	struct clk *pixel_clk;
+	u8 panel_state;
 	int irq_cnt;
 	int mdss_dsi_clk_on;
 	int rst_gpio;
