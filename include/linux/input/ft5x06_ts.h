ed4262249e33ddb3cf60b7b92f7ed5f8afdffb8a
Mohan Pallaka <mpallaka@codeaurora.org>
2013-05-09 16:30:00 +0530

    input: ft5x06_ts: Add DT support
    Add DeviceTree support for focaltech controller driver.
    The data is represented in device tree format and parsed
    by the driver using of framework apis.
    
    Change-Id: Ibcdd8c3579f401ac0632d670e1b7c674aa67ba8d
    Signed-off-by: Mohan Pallaka <mpallaka@codeaurora.org>

--- include/linux/input/ft5x06_ts.h
+++ include/linux/input/ft5x06_ts.h
@@ -3,7 +3,7 @@
  * FocalTech ft5x06 TouchScreen driver header file.
  *
  * Copyright (c) 2010  Focal tech Ltd.
- * Copyright (c) 2012, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -18,12 +18,28 @@
 #ifndef __LINUX_FT5X06_TS_H__
 #define __LINUX_FT5X06_TS_H__
 
+#define FT5X06_ID		0x55
+#define FT5X16_ID		0x0A
+#define FT5X36_ID		0x14
+#define FT6X06_ID		0x06
+
 struct ft5x06_ts_platform_data {
-	unsigned long irqflags;
+	u32 irqflags;
+	u32 irq_gpio;
+	u32 irq_gpio_flags;
+	u32 reset_gpio;
+	u32 reset_gpio_flags;
+	u32 family_id;
 	u32 x_max;
 	u32 y_max;
-	u32 irq_gpio;
-	u32 reset_gpio;
+	u32 x_min;
+	u32 y_min;
+	u32 panel_minx;
+	u32 panel_miny;
+	u32 panel_maxx;
+	u32 panel_maxy;
+	bool no_force_update;
+	bool i2c_pull_up;
 	int (*power_init) (bool);
 	int (*power_on) (bool);
 };
