e0eccf8f6dece61cdb639c4b43374c34c3acf3ea
Mahesh Sivasubramanian <msivasub@codeaurora.org>
2013-05-10 09:38:51 -0600

    msm: rpm-smd: Configure WQ for higer priority
    When the workqueue runs at a lower priority, it gets starved by higher
    priority threads. Bump the WQ priority to high to ensure rpm smd work
    queue gets a fair chance
    
    Change-Id: I06b864611cf45afe6931d6030327806032894663
    Signed-off-by: Mahesh Sivasubramanian <msivasub@codeaurora.org>

--- arch/arm/mach-msm/rpm-smd.c
+++ arch/arm/mach-msm/rpm-smd.c
@@ -1349,7 +1349,8 @@ static int __devinit msm_rpm_dev_probe(struct platform_device *pdev)
 	smd_disable_read_intr(msm_rpm_data.ch_info);
 
 	if (!standalone) {
-		msm_rpm_smd_wq = create_singlethread_workqueue("rpm-smd");
+		msm_rpm_smd_wq = alloc_workqueue("rpm-smd",
+				WQ_UNBOUND | WQ_MEM_RECLAIM | WQ_HIGHPRI, 1);
 		if (!msm_rpm_smd_wq)
 			return -EINVAL;
 		queue_work(msm_rpm_smd_wq, &msm_rpm_data.work);
