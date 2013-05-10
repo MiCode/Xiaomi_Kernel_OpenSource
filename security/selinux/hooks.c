baaf9721b1ea3e27f8620329e339af76c4ca78d2
Stephen Smalley <sds@tycho.nsa.gov>
2013-05-10 10:16:19 -0400

    Enable setting security contexts on rootfs inodes.
    rootfs (ramfs) can support setting of security contexts
    by userspace due to the vfs fallback behavior of calling
    the security module to set the in-core inode state
    for security.* attributes when the filesystem does not
    provide an xattr handler.  No xattr handler required
    as the inodes are pinned in memory and have no backing
    store.
    
    This is useful in allowing early userspace to label individual
    files within a rootfs while still providing a policy-defined
    default via genfs.
    
    Change-Id: Ic499167ba9d596a1954c2fe55b1613ab3ec07181
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>
    Signed-off-by: Satish Kamuju <skamuj@codeaurora.org>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -439,6 +439,13 @@ static int sb_finish_set_opts(struct super_block *sb)
 	    !strcmp(sb->s_type->name, "rootfs"))
 		sbsec->flags |= SE_SBLABELSUPP;
 
+	/*
+	 * Special handling for rootfs. Is genfs but supports
+	 * setting SELinux context on in-core inodes.
+	 */
+	if (strncmp(sb->s_type->name, "rootfs", sizeof("rootfs")) == 0)
+		sbsec->flags |= SE_SBLABELSUPP;
+
 	/* Initialize the root inode. */
 	rc = inode_doinit_with_dentry(root_inode, root);
 
