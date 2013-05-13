63a5fe4edc7cbcb0f8379dd635241c070023f2df
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2013-05-13 15:11:55 -0700

    sched: fix reference to wrong cfs_rq
    Commit 7db16c8c (sched: Fix SCHED_HRTICK bug leading to late preemption
    of tasks) introduced a bug in sched_slice() calculation by using wrong
    cfs_rq for tasks. rq->cfs was incorrectly used as task's cfs_rq, rather
    than the correct one to which they belonged.
    
    Fix the bug by using correct cfs_rq for tasks.
    
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2126,11 +2126,11 @@ void unthrottle_offline_cfs_rqs(struct rq *rq) {}
 static void hrtick_start_fair(struct rq *rq, struct task_struct *p)
 {
 	struct sched_entity *se = &p->se;
-	struct cfs_rq *cfs_rq = &rq->cfs;
+	struct cfs_rq *cfs_rq = cfs_rq_of(se);
 
 	WARN_ON(task_rq(p) != rq);
 
-	if (cfs_rq->h_nr_running > 1) {
+	if (rq->cfs.h_nr_running > 1) {
 		u64 slice = sched_slice(cfs_rq, se);
 		u64 ran = se->sum_exec_runtime - se->prev_sum_exec_runtime;
 		s64 delta = slice - ran;
