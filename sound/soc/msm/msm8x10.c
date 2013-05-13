05890444c95d2c516357fdc017763f0b67f3c934
Helen Zeng <xiaoyunz@codeaurora.org>
2013-05-13 12:08:17 -0700

    ASoC: msm8x10: Add support for Voice2
    Add front-end DAI Voice2 to enable a second voice path in the DSP
    for calls from the second modem.
    
    Change-Id: I6223a5319ebd42450395e7f047a9c82a78be5b9d
    Signed-off-by: Helen Zeng <xiaoyunz@codeaurora.org>

--- sound/soc/msm/msm8x10.c
+++ sound/soc/msm/msm8x10.c
@@ -613,6 +613,21 @@ static struct snd_soc_dai_link msm8x10_dai[] = {
 		.ignore_pmdown_time = 1,
 		.be_id = MSM_FRONTEND_DAI_MULTIMEDIA5,
 	},
+	{/* hw:x,13 */
+		.name = "Voice2",
+		.stream_name = "Voice2",
+		.cpu_dai_name   = "Voice2",
+		.platform_name  = "msm-pcm-voice",
+		.dynamic = 1,
+		.trigger = {SND_SOC_DPCM_TRIGGER_POST,
+			    SND_SOC_DPCM_TRIGGER_POST},
+		.no_host_mode = SND_SOC_DAI_LINK_NO_HOST,
+		.ignore_suspend = 1,
+		/* this dainlink has playback support */
+		.ignore_pmdown_time = 1,
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.codec_name = "snd-soc-dummy",
+	},
 	/* Backend I2S DAI Links */
 	{
 		.name = LPASS_BE_SEC_MI2S_RX,
