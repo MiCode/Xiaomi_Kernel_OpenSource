0fbc63fa2f09e04c51c8fa98f91b99f221e9d044
Gilad Avidov <gavidov@codeaurora.org>
2013-05-16 17:17:13 -0600

    arm/dt: apq8084: qup_i2c: enable BLSP1 QUP3 for EEPROM test
    Configure I2C device on BLSP1 QUP3 and sets its clocks and
    GPIOs such that it may access the EEPROM device.
    The EEPROM device is commonly used to verify I2C.
    
    Change-Id: Ibb0036d6bbcb37394dd177adfed423306ff0477b
    Signed-off-by: Gilad Avidov <gavidov@codeaurora.org>

--- arch/arm/mach-msm/board-8084-gpiomux.c
+++ arch/arm/mach-msm/board-8084-gpiomux.c
@@ -17,6 +17,27 @@
 #include <mach/board.h>
 #include <mach/gpiomux.h>
 
+static struct gpiomux_setting gpio_i2c_config = {
+	.func = GPIOMUX_FUNC_3,
+	.drv  = GPIOMUX_DRV_2MA,
+	.pull = GPIOMUX_PULL_NONE,
+};
+
+static struct msm_gpiomux_config msm_blsp_configs[] __initdata = {
+	{
+		.gpio      = 10,		/* BLSP1 QUP3 I2C_SDA */
+		.settings = {
+			[GPIOMUX_SUSPENDED] = &gpio_i2c_config,
+		},
+	},
+	{
+		.gpio      = 11,		/* BLSP1 QUP3 I2C_SCL */
+		.settings = {
+			[GPIOMUX_SUSPENDED] = &gpio_i2c_config,
+		},
+	},
+};
+
 void __init apq8084_init_gpiomux(void)
 {
 	int rc;
@@ -26,4 +47,6 @@ void __init apq8084_init_gpiomux(void)
 		pr_err("%s failed %d\n", __func__, rc);
 		return;
 	}
+
+	msm_gpiomux_install(msm_blsp_configs, ARRAY_SIZE(msm_blsp_configs));
 }
