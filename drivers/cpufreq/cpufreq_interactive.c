6c59f48ad4606cdb5e978321e47c91d341dec01e
Viresh Kumar <viresh.kumar@linaro.org>
2013-05-16 14:58:53 +0530

    cpufreq: interactive: Move definition of cpufreq_gov_interactive downwards
    This moves definition of cpufreq_gov_interactive towards the bottom of file, so
    that we don't have to add prototype of cpufreq_governor_interactive() in the
    beginning of file.
    
    Change-Id: I04bd1004954eb36502c5cd7e35d3d7274cddaf95
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Git-commit: e50d640cacfe61e69f1b4fa92fa0174c688e919c
    Git-Repo: https://android.googlesource.com/kernel/common.git
    [junjiew@codeaurora.org: Resolve merge conflicts]
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -137,19 +137,6 @@ static unsigned int up_threshold_any_cpu_load;
 static unsigned int sync_freq;
 static unsigned int up_threshold_any_cpu_freq;
 
-static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
-		unsigned int event);
-
-#ifndef CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE
-static
-#endif
-struct cpufreq_governor cpufreq_gov_interactive = {
-	.name = "interactive",
-	.governor = cpufreq_governor_interactive,
-	.max_transition_latency = 10000000,
-	.owner = THIS_MODULE,
-};
-
 static void cpufreq_interactive_timer_resched(
 	struct cpufreq_interactive_cpuinfo *pcpu)
 {
@@ -1349,6 +1336,16 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 	}
 	return 0;
 }
+
+#ifndef CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE
+static
+#endif
+struct cpufreq_governor cpufreq_gov_interactive = {
+	.name = "interactive",
+	.governor = cpufreq_governor_interactive,
+	.max_transition_latency = 10000000,
+	.owner = THIS_MODULE,
+};
 
 static void cpufreq_interactive_nop_timer(unsigned long data)
 {
