e17bc230bede6861c3162ce9b41257e2a48138e0
Catalin Marinas <catalin.marinas@arm.com>
2013-05-21 17:35:19 +0100

    arm64: Implement coherent DMA API based on swiotlb
    This patch adds support for DMA API cache maintenance on SoCs without
    hardware device cache coherency.
    
    Change-Id: I8e242d1324b017d2c080175d5f4686221c9368fc
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Git-commit: b377cd6e41b8af449c10d7c2ac85f58facb42f04
    Git-repo: https://git.kernel.org/cgit/linux/kernel/git/cmarinas/linux-aarch64.git
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- arch/arm64/include/asm/dma-mapping.h
+++ arch/arm64/include/asm/dma-mapping.h
@@ -26,6 +26,8 @@
 #define ARCH_HAS_DMA_GET_REQUIRED_MASK
 
 extern struct dma_map_ops *dma_ops;
+extern struct dma_map_ops coherent_swiotlb_dma_ops;
+extern struct dma_map_ops noncoherent_swiotlb_dma_ops;
 
 static inline struct dma_map_ops *get_dma_ops(struct device *dev)
 {
@@ -33,6 +35,11 @@ static inline struct dma_map_ops *get_dma_ops(struct device *dev)
 		return dma_ops;
 	else
 		return dev->archdata.dma_ops;
+}
+
+static inline void set_dma_ops(struct device *dev, struct dma_map_ops *ops)
+{
+	dev->archdata.dma_ops = ops;
 }
 
 #include <asm-generic/dma-mapping-common.h>
