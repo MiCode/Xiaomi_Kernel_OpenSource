dac02719fbc6a0361ddcd56c639a921830d42692
Bryan Huntsman <bryanh@codeaurora.org>
2013-05-22 14:17:47 -0700

    gcc-wrapper: trim whitelist for gcc 4.7
    With recent code and defconfig changes, the number warnings needing to
    be ignored has gone done.  Update the wrapper accordingly.
    
    Signed-off-by: Bryan Huntsman <bryanh@codeaurora.org>

--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -40,10 +40,7 @@ import subprocess
 # force LANG to be set to en_US.UTF-8 to get consistent warnings.
 
 allowed_warnings = set([
-    "alignment.c:327",
-    "mmu.c:602",
     "return_address.c:62",
-    "extents.c:2091",
  ])
 
 # Capture the name of the object file, can find it.
