19f34b3df527ac19cc8ebc429e12197773e1fdda
Sahitya Tummala <stummala@codeaurora.org>
2013-05-24 08:47:26 +0530

    mmc: sdhci: initialize sdhci_host lock in sdhci_alloc_host()
    Currently, the sdhci host lock is initialized in sdhci_add_host()
    but there can be a case where it is required even before that.
    Hence, initialize it in sdhci_alloc_host() where sdhci_host
    structure is allocated.
    
    Change-Id: If99d82679c07bc2d36e0aad9354757288aa400b8
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -2740,6 +2740,8 @@ struct sdhci_host *sdhci_alloc_host(struct device *dev,
 	host = mmc_priv(mmc);
 	host->mmc = mmc;
 
+	spin_lock_init(&host->lock);
+
 	return host;
 }
 
@@ -3131,8 +3133,6 @@ int sdhci_add_host(struct sdhci_host *host)
 			"support voltages.\n", mmc_hostname(mmc));
 		return -ENODEV;
 	}
-
-	spin_lock_init(&host->lock);
 
 	/*
 	 * Maximum number of segments. Depends on if the hardware
