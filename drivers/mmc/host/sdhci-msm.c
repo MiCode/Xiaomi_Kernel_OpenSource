61a52c9f0263b72169cca1646712d3555c2013a8
Subhash Jadavani <subhashj@codeaurora.org>
2013-05-29 15:52:10 +0530

    mmc: sdhci-msm: enable asynchronous interrupt support in 4-bit mode
    SDIO 3.0 specification has added the support for asynchronous interrupt
    period during which card allows the clock to be gated off.
    
    As SDCC driver is capable of handling the asynchronous interrupt,
    advertise MMC_CAP2_ASYNC_SDIO_IRQ_4BIT_MODE capability.
    
    Change-Id: Id5a86bc70b7b798b23be3a0fc0d59b2db05e0409
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -2416,6 +2416,7 @@ static int __devinit sdhci_msm_probe(struct platform_device *pdev)
 	msm_host->mmc->caps2 |= MMC_CAP2_POWEROFF_NOTIFY;
 	msm_host->mmc->caps2 |= MMC_CAP2_CLK_SCALE;
 	msm_host->mmc->caps2 |= MMC_CAP2_STOP_REQUEST;
+	msm_host->mmc->caps2 |= MMC_CAP2_ASYNC_SDIO_IRQ_4BIT_MODE;
 
 	if (msm_host->pdata->nonremovable)
 		msm_host->mmc->caps |= MMC_CAP_NONREMOVABLE;
