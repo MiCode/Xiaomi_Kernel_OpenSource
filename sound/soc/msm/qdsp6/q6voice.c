e87330eb2d68a6b4d8686f2cc2395edd34114335
Venkat Sudhir <vsudhir@codeaurora.org>
2013-05-30 10:06:35 -0700

    ASoC: msm: qdsp6v2: Fix bug in device V2 command
    This change is to fix missing braces in Device V2
    command. Without the braces Device V2 command will
    return error code even though the command is
    successful. Device V2 command is used to send device
    configuration to CVD during voice call. This command
    is used to inform ADSP about internal or external EC mode.
    
    CRs-Fixed: 492994
    Change-Id: I2bf12b8cdbc6765bffb2ed9e6460160cf8afa81a
    Signed-off-by: Venkat Sudhir <vsudhir@codeaurora.org>

--- sound/soc/msm/qdsp6/q6voice.c
+++ sound/soc/msm/qdsp6/q6voice.c
@@ -2356,10 +2356,11 @@ static int voice_setup_vocproc(struct voice_data *v)
 	}
 	if (common.ec_ref_ext == true) {
 		ret = voice_send_set_device_cmd_v2(v);
-		if (ret < 0)
+		if (ret < 0) {
 			pr_err("%s:  set device V2 failed rc =%x\n",
 			       __func__, ret);
 			goto fail;
+		}
 	}
 	/* send cvs cal */
 	ret = voice_send_cvs_map_memory_cmd(v);
@@ -3354,10 +3355,11 @@ int voc_enable_cvp(uint16_t session_id)
 	if (v->voc_state == VOC_CHANGE) {
 		if (common.ec_ref_ext == true) {
 			ret = voice_send_set_device_cmd_v2(v);
-			if (ret < 0)
+			if (ret < 0) {
 				pr_err("%s: set device V2 failed\n"
 				       "rc =%x\n", __func__, ret);
 				goto fail;
+			}
 		} else {
 			ret = voice_send_set_device_cmd(v);
 			if (ret < 0) {
