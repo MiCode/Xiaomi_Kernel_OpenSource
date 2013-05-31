b50af9af9c9087a2732ec2ca50710484cf678ebc
Sameer Thalappil <sameert@codeaurora.org>
2013-05-31 14:01:25 -0700

    wcnss: Prevent access to WCNSS driver after a failure
    Once the WCNSS driver trigger fails, the WCNSS platform driver main
    environment is set to NULL. Any further access to WCNSS platform driver
    should be prevented after this.
    
    Change-Id: I6d391a347a144c31b70db0fd74904e5d1e020692
    Signed-off-by: Sameer Thalappil <sameert@codeaurora.org>

--- drivers/net/wireless/wcnss/wcnss_wlan.c
+++ drivers/net/wireless/wcnss/wcnss_wlan.c
@@ -1683,6 +1683,9 @@ static int wcnss_node_open(struct inode *inode, struct file *file)
 {
 	struct platform_device *pdev;
 
+	if (!penv)
+		return -EFAULT;
+
 	/* first open is only to trigger WCNSS platform driver */
 	if (!penv->triggered) {
 		pr_info(DEVICE " triggered by userspace\n");
@@ -1710,7 +1713,7 @@ static ssize_t wcnss_wlan_read(struct file *fp, char __user
 {
 	int rc = 0;
 
-	if (!penv->device_opened)
+	if (!penv || !penv->device_opened)
 		return -EFAULT;
 
 	rc = wait_event_interruptible(penv->read_wait, penv->fw_cal_rcvd
@@ -1749,7 +1752,7 @@ static ssize_t wcnss_wlan_write(struct file *fp, const char __user
 	int rc = 0;
 	int size = 0;
 
-	if (!penv->device_opened || penv->user_cal_available)
+	if (!penv || !penv->device_opened || penv->user_cal_available)
 		return -EFAULT;
 
 	if (penv->user_cal_rcvd == 0 && count >= 4
