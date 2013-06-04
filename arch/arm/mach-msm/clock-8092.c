b4a2c515b8df5b1ebd37817522787aa4943315fc
Srinivas Ramana <sramana@codeaurora.org>
2013-06-04 19:53:20 +0530

    mpq8092: add support to boot linux on RUMI
    add dt compatible strings to mpq8092 to support booting
    kernel on RUMI.
    
    Change-Id: Ibc569eb51f4c05ed4694891b4963b2dfd5080d34
    Signed-off-by: Srinivas Ramana <sramana@codeaurora.org>

--- arch/arm/mach-msm/clock-8092.c
+++ arch/arm/mach-msm/clock-8092.c
@@ -38,6 +38,8 @@
 static struct clk_lookup msm_clocks_8092[] = {
 	CLK_DUMMY("core_clk",   BLSP1_UART_CLK, "msm_serial_hsl.0", OFF),
 	CLK_DUMMY("iface_clk",  BLSP1_UART_CLK, "msm_serial_hsl.0", OFF),
+	CLK_DUMMY("core_clk",   BLSP1_UART_CLK, "msm_serial_hsl.1", OFF),
+	CLK_DUMMY("iface_clk",  BLSP1_UART_CLK, "msm_serial_hsl.1", OFF),
 	CLK_DUMMY("core_clk",	SDC1_CLK,	"msm_sdcc.1", OFF),
 	CLK_DUMMY("iface_clk",	SDC1_P_CLK,	"msm_sdcc.1", OFF),
 	CLK_DUMMY("core_clk",	SDC2_CLK,	"msm_sdcc.2", OFF),
