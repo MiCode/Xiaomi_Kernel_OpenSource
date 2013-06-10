aac982b31ac8b4c3168337fa0f38f143d7111eb0
Ulf Hansson <ulf.hansson@linaro.org>
2013-06-10 17:03:46 +0200

    mmc: core: Invent MMC_CAP2_FULL_PWR_CYCLE
    MMC_CAP2_FULL_PWR_CYCLE shall be set by host drivers which are able to
    do a complete power cycle of the card. In the eMMC case that includes
    both vcc and vccq.
    
    This CAP is providing the protocol layer with important information,
    needed to take optimized decisions during card initialization and in
    the suspend/resume sequence.
    
    MMC_CAP2_POWEROFF_NOTIFY is replaced by MMC_CAP2_FULL_PWR_CYCLE, since
    it makes sense to use a wider scope for it.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>
    Git-commit: 211f8b40b27a83c8513bcade87188ab168aece90
    Git-repo: https://android.googlesource.com/kernel/common.git
    [imaund@codeaurora.org: MMC_CAP2_POWEROFF_NOTIFY has been replaced by
      MMC_CAP2_FULL_PWR_CYCLE]
    Signed-off-by: Ian Maund <imaund@codeaurora.org>

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -3466,7 +3466,7 @@ static int sdhci_msm_probe(struct platform_device *pdev)
 	msm_host->mmc->caps2 |= (MMC_CAP2_BOOTPART_NOACC |
 				MMC_CAP2_DETECT_ON_ERR);
 	msm_host->mmc->caps2 |= MMC_CAP2_CACHE_CTRL;
-	msm_host->mmc->caps2 |= MMC_CAP2_POWEROFF_NOTIFY;
+	msm_host->mmc->caps2 |= MMC_CAP2_FULL_PWR_CYCLE;
 	msm_host->mmc->caps2 |= MMC_CAP2_CLK_SCALE;
 	msm_host->mmc->caps2 |= MMC_CAP2_STOP_REQUEST;
 	msm_host->mmc->caps2 |= MMC_CAP2_ASYNC_SDIO_IRQ_4BIT_MODE;
