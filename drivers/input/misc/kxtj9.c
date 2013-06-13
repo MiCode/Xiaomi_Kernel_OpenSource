bc5568db4b6b4a3e34d5b28515b960b938d40826
Figo Wang <figow@codeaurora.org>
2013-06-13 16:31:06 +0800

    sensor: Remove device open/close callback for G-sensor driver kxtj9
    Remove these two callback that device will not powerup when device is
    opened, it power on/off state will be controled though device enable node.
    
    Signed-off-by: Figo Wang <figow@codeaurora.org>
    Change-Id: I7200c862dec950cb28969f4e646ed6c70a7d16c4

--- drivers/input/misc/kxtj9.c
+++ drivers/input/misc/kxtj9.c
@@ -421,19 +421,6 @@ static void kxtj9_disable(struct kxtj9_data *tj9)
 	tj9->enable = false;
 }
 
-static int kxtj9_input_open(struct input_dev *input)
-{
-	struct kxtj9_data *tj9 = input_get_drvdata(input);
-
-	return kxtj9_enable(tj9);
-}
-
-static void kxtj9_input_close(struct input_dev *dev)
-{
-	struct kxtj9_data *tj9 = input_get_drvdata(dev);
-
-	kxtj9_disable(tj9);
-}
 
 static void __devinit kxtj9_init_input_device(struct kxtj9_data *tj9,
 					      struct input_dev *input_dev)
@@ -461,8 +448,6 @@ static int __devinit kxtj9_setup_input_device(struct kxtj9_data *tj9)
 
 	tj9->input_dev = input_dev;
 
-	input_dev->open = kxtj9_input_open;
-	input_dev->close = kxtj9_input_close;
 	input_set_drvdata(input_dev, tj9);
 
 	kxtj9_init_input_device(tj9, input_dev);
