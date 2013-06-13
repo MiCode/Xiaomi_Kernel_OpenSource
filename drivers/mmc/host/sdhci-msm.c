214b966474658ec6cc5fb8fd79ea87a5674870ee
Asutosh Das <asutoshd@codeaurora.org>
2013-06-13 14:27:42 +0530

    mmc: sdhci-msm: ignore data-end-bit error in 1 bit mode
    Some SDHC controllers are unable to handle data end-bit
    errors in one bit mode.
    This patch adds a quirk to ignore data-end-bit error in
    1-bit mode in Qualcomm SDHC controllers.
    
    Change-Id: Ica0f10573d654021449c32197b126e12bb1a3c10
    Signed-off-by: Asutosh Das <asutoshd@codeaurora.org>
    [venkatg@codeaurora.org: sdhci_clear_set_irqs was removed
    from 3.14 kernel, write the registers directly]
    Signed-off-by: Venkat Gopalakrishnan <venkatg@codeaurora.org>
    [subhashj@codeaurora.org: fixed minor merge conflict and fixed
    the bitmap for the quirk macro]
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>
    [xiaonian@codeaurora.org: fix trivial merge conflict]
    Signed-off-by: Xiaonian Wang <xiaonian@codeaurora.org>

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -2161,6 +2161,8 @@ static int sdhci_msm_probe(struct platform_device *pdev)
 		host->quirks2 |= SDHCI_QUIRK2_RDWR_TX_ACTIVE_EOT;
 	}
 
+	host->quirks2 |= SDHCI_QUIRK2_IGN_DATA_END_BIT_ERROR;
+
 	/* Setup PWRCTL irq */
 	pwr_irq = platform_get_irq_byname(pdev, "pwr_irq");
 	if (pwr_irq < 0) {
