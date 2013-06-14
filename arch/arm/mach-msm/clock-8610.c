30c84355bb73f14d65264ccaf6d306ec9fd0c042
Li Sun <sunli@codeaurora.org>
2013-06-14 15:14:22 +0800

    msm: camera: add Hi256 camera driver for msm8x10 qrd board
    Add driver from Hi256 camera sensor, also enable it's clock.
    It's a 2 MP YUV Sensor, with 1 mipi lane.
    
    Change-Id: Iafaf439572a14e02bcd255badd5f6fed3ad13ad4
    Signed-off-by: Li Sun <sunli@codeaurora.org>

--- arch/arm/mach-msm/clock-8610.c
+++ arch/arm/mach-msm/clock-8610.c
@@ -3000,9 +3000,11 @@ static struct clk_lookup msm_clocks_8610[] = {
 
 	/* MM sensor clocks */
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "6-006f"),
+	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "6-007d"),
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "6-006d"),
 	CLK_LOOKUP("cam_src_clk", mclk1_clk_src.c, "6-0078"),
 	CLK_LOOKUP("cam_clk", mclk0_clk.c, "6-006f"),
+	CLK_LOOKUP("cam_clk", mclk0_clk.c, "6-007d"),
 	CLK_LOOKUP("cam_clk", mclk0_clk.c, "6-006d"),
 	CLK_LOOKUP("cam_clk", mclk1_clk.c, "6-0078"),
 
