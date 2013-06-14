13d2856a4a813c5fc7f286938731a488f331745c
Pratik Patel <pratikp@codeaurora.org>
2013-06-13 23:21:40 -0700

    coresight: add jtag fuse driver
    Add support for JTag Fuse driver which can be used by other
    JTag save-restore driver(s) to query the state of the jtag fuses to
    determine if the Hardware they manage is functionally disabled or
    not.
    
    Drivers can then take necessary actions like failing the probe if
    the Hardware they manage is functionally disabled.
    
    Change-Id: Ie8c0dc159e52cf869d3ed63b45e5332d3e380e6d
    Signed-off-by: Pratik Patel <pratikp@codeaurora.org>

--- arch/arm/mach-msm/include/mach/jtag.h
+++ arch/arm/mach-msm/include/mach/jtag.h
@@ -16,9 +16,11 @@
 #if defined(CONFIG_MSM_JTAG) || defined(CONFIG_MSM_JTAG_MM)
 extern void msm_jtag_save_state(void);
 extern void msm_jtag_restore_state(void);
+extern bool msm_jtag_fuse_apps_access_disabled(void);
 #else
 static inline void msm_jtag_save_state(void) {}
 static inline void msm_jtag_restore_state(void) {}
+static inline bool msm_jtag_fuse_apps_access_disabled(void) { return false; }
 #endif
 
 #endif
