e6ccd030774b0b83ce709b05d23ff7e84d9afc68
Neeti Desai <neetid@codeaurora.org>
2013-06-14 17:39:33 -0700

    msm: Increase the kernel virtual area to include lowmem
    Even though lowmem is accounted for in vmalloc space, allocation
    comes only from the region bounded by VMALLOC_START and VMALLOC_END.
    The kernel virtual area can now allocate from any unmapped region
    starting from PAGE_OFFSET.
    
    Change-Id: I291b9eb443d3f7445fd979bd7b09e9241ff22ba3
    Signed-off-by: Neeti Desai <neetid@codeaurora.org>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -1416,15 +1416,26 @@ struct vm_struct *__get_vm_area_caller(unsigned long size, unsigned long flags,
  */
 struct vm_struct *get_vm_area(unsigned long size, unsigned long flags)
 {
+#ifdef CONFIG_ENABLE_VMALLOC_SAVING
+	return __get_vm_area_node(size, 1, flags, PAGE_OFFSET, VMALLOC_END,
+				-1, GFP_KERNEL, __builtin_return_address(0));
+#else
 	return __get_vm_area_node(size, 1, flags, VMALLOC_START, VMALLOC_END,
 				-1, GFP_KERNEL, __builtin_return_address(0));
+#endif
+
 }
 
 struct vm_struct *get_vm_area_caller(unsigned long size, unsigned long flags,
 				const void *caller)
 {
-	return __get_vm_area_node(size, 1, flags, VMALLOC_START, VMALLOC_END,
+#ifdef CONFIG_ENABLE_VMALLOC_SAVING
+	return __get_vm_area_node(size, 1, flags, PAGE_OFFSET, VMALLOC_END,
 						-1, GFP_KERNEL, caller);
+#else
+	return __get_vm_area_node(size, 1, flags, VMALLOC_START, VMALLOC_END,
+				-1, GFP_KERNEL, __builtin_return_address(0));
+#endif
 }
 
 /**
