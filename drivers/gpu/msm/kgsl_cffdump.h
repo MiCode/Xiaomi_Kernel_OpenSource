36a80268fd5d2baf9bf9abd3cf7f033c8ee73efc
Jordan Crouse <jcrouse@codeaurora.org>
2013-06-18 09:32:51 -0600

    msm: kgsl: convert cffdump stub functions to inlines
    Since cffdump is not often enabled in Kconfig, it is helpful
    to at least catch compiler errors and warnings when it is off.
    
    Change-Id: Ic0dedbad41b0faac6e0a2514e694f8e369c61f7f
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/kgsl_cffdump.h
+++ drivers/gpu/msm/kgsl_cffdump.h
@@ -14,13 +14,15 @@
 #ifndef __KGSL_CFFDUMP_H
 #define __KGSL_CFFDUMP_H
 
-extern unsigned int kgsl_cff_dump_enable;
-
-#ifdef CONFIG_MSM_KGSL_CFF_DUMP
-
 #include <linux/types.h>
 
-#include "kgsl_device.h"
+extern unsigned int kgsl_cff_dump_enable;
+
+static inline bool kgsl_cffdump_flags_no_memzero(void) { return true; }
+
+struct kgsl_device_private;
+
+#ifdef CONFIG_MSM_KGSL_CFF_DUMP
 
 void kgsl_cffdump_init(void);
 void kgsl_cffdump_destroy(void);
@@ -42,7 +44,6 @@ void kgsl_cffdump_user_event(struct kgsl_device *device,
 		unsigned int cff_opcode, unsigned int op1,
 		unsigned int op2, unsigned int op3,
 		unsigned int op4, unsigned int op5);
-static inline bool kgsl_cffdump_flags_no_memzero(void) { return true; }
 
 void kgsl_cffdump_memory_base(struct kgsl_device *device, unsigned int base,
 			      unsigned int range, unsigned int gmemsize);
@@ -53,20 +54,69 @@ int kgsl_cff_dump_enable_get(void *data, u64 *val);
 
 #else
 
-#define kgsl_cffdump_init()					(void)0
-#define kgsl_cffdump_destroy()					(void)0
-#define kgsl_cffdump_open(device)				(void)0
-#define kgsl_cffdump_close(device)				(void)0
-#define kgsl_cffdump_syncmem(dev_priv, memdesc, addr, sizebytes, clean_cache) \
-	(void) 0
-#define kgsl_cffdump_setmem(device, addr, value, sizebytes)	(void)0
-#define kgsl_cffdump_regwrite(device, addr, value)		(void)0
-#define kgsl_cffdump_regpoll(device, addr, value, mask)	(void)0
-#define kgsl_cffdump_parse_ibs(dev_priv, memdesc, gpuaddr, \
-	sizedwords, check_only)					true
-#define kgsl_cffdump_flags_no_memzero()				true
-#define kgsl_cffdump_memory_base(davice, base, range, gmemsize)	(void)0
-#define kgsl_cffdump_hang(device)				(void)0
+static inline void kgsl_cffdump_init(void)
+{
+	return;
+}
+
+static inline void kgsl_cffdump_destroy(void)
+{
+	return;
+}
+
+static inline void kgsl_cffdump_open(struct kgsl_device *device)
+{
+	return;
+}
+
+static inline void kgsl_cffdump_close(struct kgsl_device *device)
+{
+	return;
+}
+
+static inline void kgsl_cffdump_syncmem(struct kgsl_device_private *dev_priv,
+		struct kgsl_memdesc *memdesc, uint physaddr, uint sizebytes,
+		bool clean_cache)
+{
+	return;
+}
+
+static inline void kgsl_cffdump_setmem(struct kgsl_device *device, uint addr,
+		uint value, uint sizebytes)
+{
+	return;
+}
+
+static inline void kgsl_cffdump_regwrite(struct kgsl_device *device, uint addr,
+					 uint value)
+{
+	return;
+}
+
+static inline void kgsl_cffdump_regpoll(struct kgsl_device *device, uint addr,
+		uint value, uint mask)
+{
+	return;
+}
+
+static inline bool kgsl_cffdump_parse_ibs(struct kgsl_device_private *dev_priv,
+	const struct kgsl_memdesc *memdesc, uint gpuaddr, int sizedwords,
+	bool check_only)
+{
+	return false;
+}
+
+static inline void kgsl_cffdump_memory_base(struct kgsl_device *device,
+		unsigned int base, unsigned int range, unsigned int gmemsize)
+{
+	return;
+}
+
+static inline void kgsl_cffdump_hang(struct kgsl_device *device)
+{
+	return;
+}
+
 static inline void kgsl_cffdump_user_event(struct kgsl_device *device,
 		unsigned int cff_opcode, unsigned int op1,
 		unsigned int op2, unsigned int op3,
