5f9c85c77502bb099ea3fc21027f3210d380d90f
Stephen Boyd <sboyd@codeaurora.org>
2013-06-21 16:33:44 -0700

    Revert "gpu: ion: __dma_page_cpu_to_dev -> arm_dma_ops.sync_single_for_device hack"
    This reverts commit 7ad626a3d9a1d44ad9e3219f8b189e2af2f7df33.

--- drivers/gpu/ion/ion_page_pool.c
+++ drivers/gpu/ion/ion_page_pool.c
@@ -37,10 +37,8 @@ static void *ion_page_pool_alloc_pages(struct ion_page_pool *pool)
 	/* this is only being used to flush the page for dma,
 	   this api is not really suitable for calling from a driver
 	   but no better way to flush a page for dma exist at this time */
-	arm_dma_ops.sync_single_for_device(NULL,
-					   pfn_to_dma(NULL, page_to_pfn(page)),
-					   PAGE_SIZE << pool->order,
-					   DMA_BIDIRECTIONAL);
+	__dma_page_cpu_to_dev(page, 0, PAGE_SIZE << pool->order,
+			      DMA_BIDIRECTIONAL);
 	return page;
 }
 
