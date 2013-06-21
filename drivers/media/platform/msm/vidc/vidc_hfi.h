95529de643d1e3c4460de52c9ac4c3e907f9048c
Rajeshwar Kurapaty <rkurapat@codeaurora.org>
2013-06-22 00:30:23 +0530

    msm: vidc: Fix for fuzz issues in firmware
    Firmware is reporting session error for etb/ftb
    due to incorrect packet sizes being sent. Fix this
    by modifying the size of zero lenth array elements
    in etb/ftb packets to one.
    
    Change-Id: I2127290b9bd7f62e524d19f2fee42691d26e6d0a
    Signed-off-by: Rajeshwar Kurapaty <rkurapat@codeaurora.org>

--- drivers/media/platform/msm/vidc/vidc_hfi.h
+++ drivers/media/platform/msm/vidc/vidc_hfi.h
@@ -408,7 +408,7 @@ struct hfi_cmd_session_empty_buffer_compressed_packet {
 	u32 input_tag;
 	u8 *packet_buffer;
 	u8 *extra_data_buffer;
-	u32 rgData[0];
+	u32 rgData[1];
 };
 
 struct hfi_cmd_session_empty_buffer_uncompressed_plane0_packet {
@@ -427,7 +427,7 @@ struct hfi_cmd_session_empty_buffer_uncompressed_plane0_packet {
 	u32 input_tag;
 	u8 *packet_buffer;
 	u8 *extra_data_buffer;
-	u32 rgData[0];
+	u32 rgData[1];
 };
 
 struct hfi_cmd_session_empty_buffer_uncompressed_plane1_packet {
@@ -436,7 +436,7 @@ struct hfi_cmd_session_empty_buffer_uncompressed_plane1_packet {
 	u32 filled_len;
 	u32 offset;
 	u8 *packet_buffer2;
-	u32 rgData[0];
+	u32 rgData[1];
 };
 
 struct hfi_cmd_session_empty_buffer_uncompressed_plane2_packet {
@@ -445,7 +445,7 @@ struct hfi_cmd_session_empty_buffer_uncompressed_plane2_packet {
 	u32 filled_len;
 	u32 offset;
 	u8 *packet_buffer3;
-	u32 rgData[0];
+	u32 rgData[1];
 };
 
 struct hfi_cmd_session_fill_buffer_packet {
@@ -459,7 +459,7 @@ struct hfi_cmd_session_fill_buffer_packet {
 	u32 output_tag;
 	u8 *packet_buffer;
 	u8 *extra_data_buffer;
-	u32 rgData[0];
+	u32 rgData[1];
 };
 
 struct hfi_cmd_session_flush_packet {
