cac0580eeb7fae6a4dd65fce249cb6200f54d30a
Subhash Jadavani <subhashj@codeaurora.org>
2013-06-25 13:13:24 +0530

    mmc: core: expose HPI capability to SWITCH commands
    Some of the time consuming operations such as BKOPS, SANITIZE, CACHE
    flush/off use the SWITCH command (CMD6) but as these operations don't
    have card specification defined timeout for completion, we may see
    timeout errors if card doesn't complete the operation within the SW
    defined timeout. If SW defined timeout is hit, above operations are
    considered to be failed and no real recovery mechanism is implemented
    after timeout.
    
    Most of the above operations (BKOPS/SANITIZE/CACHE flush/off) can be
    interrupted by sending the HPI (High Priority Interrupt) command to card
    if they taking longer than expected. This change adds the base support
    which will these operations to be HPIed after timeout.
    
    Change-Id: Ibd9061525756aaae656b1ceeeaed62e04fb80cce
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -785,25 +785,13 @@ static void mmc_wait_for_req_done(struct mmc_host *host,
 		cmd = mrq->cmd;
 
 		/*
-		 * If host has timed out waiting for the blocking BKOPs
-		 * or sanitize to complete, card might be still in programming
-		 * state so let's try to bring the card out of programming
-		 * state.
+		 * If host has timed out waiting for the commands which can be
+		 * HPIed then let the caller handle the timeout error as it may
+		 * want to send the HPI command to bring the card out of
+		 * programming state.
 		 */
-		if ((cmd->bkops_busy || cmd->sanitize_busy) &&
-		     cmd->error == -ETIMEDOUT) {
-			if (!mmc_interrupt_hpi(host->card)) {
-				pr_warning("%s: %s: Interrupted blocking bkops"
-					   "or sanitize\n",
-					   mmc_hostname(host), __func__);
-				cmd->error = 0;
-				break;
-			} else {
-				pr_err("%s: %s: Failed to interrupt blocking "
-				       "bkops or sanitize\n",
-				       mmc_hostname(host), __func__);
-			}
-		}
+		if (cmd->ignore_timeout && cmd->error == -ETIMEDOUT)
+			break;
 
 		if (!cmd->error || !cmd->retries ||
 		    mmc_card_removed(host->card))
@@ -3486,7 +3474,7 @@ int mmc_flush_cache(struct mmc_card *card)
 	if (mmc_card_mmc(card) &&
 			(card->ext_csd.cache_size > 0) &&
 			(card->ext_csd.cache_ctrl & 1)) {
-		err = mmc_switch(card, EXT_CSD_CMD_SET_NORMAL,
+		err = mmc_switch_ignore_timeout(card, EXT_CSD_CMD_SET_NORMAL,
 				EXT_CSD_FLUSH_CACHE, 1, 0);
 		if (err)
 			pr_err("%s: cache flush error %d\n",
@@ -3519,7 +3507,8 @@ int mmc_cache_ctrl(struct mmc_host *host, u8 enable)
 
 		if (card->ext_csd.cache_ctrl ^ enable) {
 			timeout = enable ? card->ext_csd.generic_cmd6_time : 0;
-			err = mmc_switch(card, EXT_CSD_CMD_SET_NORMAL,
+			err = mmc_switch_ignore_timeout(card,
+					EXT_CSD_CMD_SET_NORMAL,
 					EXT_CSD_CACHE_CTRL, enable, timeout);
 			if (err)
 				pr_err("%s: cache %s error %d\n",
