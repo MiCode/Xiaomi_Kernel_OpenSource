bba9f3a122639db1e8f0c48925b8dbaa9856d83d
David Collins <collinsd@codeaurora.org>
2013-06-25 11:02:22 -0700

    regulator: qpnp-regulator: Don't print errors during probe deferral
    Make the error printing statements after the qpnp-regulator
    drivers calls regulator_register() conditional upon the error
    code not being -EPROBE_DEFER.  This removes some unnecessary
    kernel log messages that take place when a qpnp-regulator device
    attempts to probe before its parent regulator.
    
    Change-Id: I92d4ed61a238d384bae43458e3ca86c0079380c0
    Signed-off-by: David Collins <collinsd@codeaurora.org>

--- drivers/regulator/qpnp-regulator.c
+++ drivers/regulator/qpnp-regulator.c
@@ -1539,7 +1539,9 @@ static int qpnp_regulator_probe(struct spmi_device *spmi)
 	vreg->rdev = regulator_register(rdesc, &reg_config);
 	if (IS_ERR(vreg->rdev)) {
 		rc = PTR_ERR(vreg->rdev);
-		vreg_err(vreg, "regulator_register failed, rc=%d\n", rc);
+		if (rc != -EPROBE_DEFER)
+			vreg_err(vreg, "regulator_register failed, rc=%d\n",
+				rc);
 		goto cancel_ocp_work;
 	}
 
@@ -1551,7 +1553,7 @@ cancel_ocp_work:
 	if (vreg->ocp_irq)
 		cancel_delayed_work_sync(&vreg->ocp_work);
 bail:
-	if (rc)
+	if (rc && rc != -EPROBE_DEFER)
 		vreg_err(vreg, "probe failed, rc=%d\n", rc);
 
 	kfree(vreg->rdesc.name);
