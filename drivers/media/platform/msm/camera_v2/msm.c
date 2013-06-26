31aa49f1d2316c6cbdaf206df78c52f5f2e0ff48
Sunid Wilson <sunidw@codeaurora.org>
2013-06-25 19:46:55 -0700

    msm: camera: Fix improper subdev find functionality
    There is a bug in msm_sd_find function in which if the
    subdev is not present, the last entry will be sent to
    the caller. Added the code to fix this.
    
    Change-Id: I250d0383f5b6c954d301ce0caa6a1f7a9adbf267
    Signed-off-by: Sunid Wilson <sunidw@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/msm.c
+++ drivers/media/platform/msm/camera_v2/msm.c
@@ -852,16 +852,19 @@ static struct v4l2_subdev *msm_sd_find(const char *name)
 {
 	unsigned long flags;
 	struct v4l2_subdev *subdev = NULL;
+	struct v4l2_subdev *subdev_out = NULL;
 
 	spin_lock_irqsave(&msm_v4l2_dev->lock, flags);
 	if (!list_empty(&msm_v4l2_dev->subdevs)) {
 		list_for_each_entry(subdev, &msm_v4l2_dev->subdevs, list)
-			if (!strcmp(name, subdev->name))
+			if (!strcmp(name, subdev->name)) {
+				subdev_out = subdev;
 				break;
+			}
 	}
 	spin_unlock_irqrestore(&msm_v4l2_dev->lock, flags);
 
-	return subdev;
+	return subdev_out;
 }
 
 static void msm_sd_notify(struct v4l2_subdev *sd,
