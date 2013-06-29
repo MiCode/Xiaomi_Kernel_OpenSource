dcd464d7524aed0a476c29911569cb4bfaa13c76
Neeti Desai <neetid@codeaurora.org>
2013-06-29 09:50:58 -0700

    msm: ocmem: Handle shrinking of memory after passive evictions
    During a passive eviction, the information of the client
    which triggered the eviction is not stored in the request
    structure of the evicted client. This is needed during the
    shrink operation of the evicted client.
    
    Store the eviction data of the request being evicted in the
    request structure to handle the shrink operation.
    
    Change-Id: Ie03573d0c488801a6e436babccf6b84bd777fb3e
    Signed-off-by: Neeti Desai <neetid@codeaurora.org>

--- arch/arm/mach-msm/include/mach/ocmem_priv.h
+++ arch/arm/mach-msm/include/mach/ocmem_priv.h
@@ -174,8 +174,8 @@ struct ocmem_req {
 	/* Request Power State */
 	unsigned power_state;
 	struct ocmem_eviction_data *edata;
-	/* Request that triggered eviction */
-	struct ocmem_req *e_handle;
+	/* Eviction data of the request being evicted */
+	struct ocmem_eviction_data *eviction_info;
 };
 
 struct ocmem_handle {
