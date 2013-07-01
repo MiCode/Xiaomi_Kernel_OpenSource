98eae4b3762b78c833255801183c1ad14d201e7d
Tanya Finkel <tfinkel@codeaurora.org>
2013-07-01 15:35:56 +0300

    arm/dt:apq8074-Dragon board: Add PCM GPIO lines
    The audio connection between apq8074 and BT on Dragon Board is through
    AUX PCM lines. Add support for MSM GPIOs used for PCM ports on
    Dragon Board.
    
    Change-Id: If1a9737f6b25af5bc27542b9b512064b2e17f16b
    Signed-off-by: Tanya Finkel <tfinkel@codeaurora.org>

--- arch/arm/mach-msm/board-8974-gpiomux.c
+++ arch/arm/mach-msm/board-8974-gpiomux.c
@@ -1236,7 +1236,8 @@ void __init msm_8974_init_gpiomux(void)
 		msm_gpiomux_install(msm_mhl_configs,
 				    ARRAY_SIZE(msm_mhl_configs));
 
-	if (of_board_is_liquid())
+	if (of_board_is_liquid() ||
+	    (of_board_is_dragonboard() && machine_is_apq8074()))
 		msm_gpiomux_install(msm8974_pri_ter_auxpcm_configs,
 				 ARRAY_SIZE(msm8974_pri_ter_auxpcm_configs));
 	else
