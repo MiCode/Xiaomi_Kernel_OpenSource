1e86e8bf7cbf7b8bac420de02291a76140af0131
Praveen Chidambaram <pchidamb@codeaurora.org>
2013-07-02 13:04:58 -0600

    thermal: Add sensor API to allow any driver to set thresholds
    Sensor API allow drivers to set min or max thresholds and get
    notified when the corresponding sensor crosses these thresholds.
    The sensor API, uses the THERMAL_TRIP_CONFIGURABLE_HI and
    THERMAL_TRIP_CONFIGURABLE_LOW, to set the threshold. The existing
    sysfs interfaces will not be affected by the newly added API.
    
    Change-Id: I85d2ae132fc3b7b6d157faf0a7390e31fdc7e6da
    Signed-off-by: Praveen Chidambaram <pchidamb@codeaurora.org>

--- include/linux/thermal.h
+++ include/linux/thermal.h
@@ -166,6 +166,27 @@ struct thermal_attr {
 	char name[THERMAL_NAME_LENGTH];
 };
 
+struct sensor_threshold {
+	int temp;
+	enum thermal_trip_type trip;
+	int (*notify)(enum thermal_trip_type type, int temp, void *data);
+	void *data;
+	struct list_head list;
+};
+
+struct sensor_info {
+	uint32_t sensor_id;
+	struct thermal_zone_device *tz;
+	int threshold_min;
+	int threshold_max;
+	int max_idx;
+	int min_idx;
+	struct list_head sensor_list;
+	struct list_head threshold_list;
+	struct mutex lock;
+	struct work_struct work;
+};
+
 struct thermal_zone_device {
 	int id;
 	char type[THERMAL_NAME_LENGTH];
@@ -190,6 +211,8 @@ struct thermal_zone_device {
 	struct mutex lock; /* protect thermal_instances list */
 	struct list_head node;
 	struct delayed_work poll_queue;
+	struct sensor_threshold tz_threshold[2];
+	struct sensor_info sensor;
 };
 
 /* Structure that holds thermal governor information */
@@ -257,6 +280,12 @@ struct thermal_instance *get_thermal_instance(struct thermal_zone_device *,
 		struct thermal_cooling_device *, int);
 void thermal_cdev_update(struct thermal_cooling_device *);
 void thermal_notify_framework(struct thermal_zone_device *, int);
+
+int sensor_get_id(char *name);
+int sensor_set_trip(uint32_t sensor_id, struct sensor_threshold *threshold);
+int sensor_cancel_trip(uint32_t sensor_id, struct sensor_threshold *threshold);
+int thermal_sensor_trip(struct thermal_zone_device *tz,
+		enum thermal_trip_type trip, unsigned long temp);
 
 #ifdef CONFIG_NET
 extern int thermal_generate_netlink_event(struct thermal_zone_device *tz,
