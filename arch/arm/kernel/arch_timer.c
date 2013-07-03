5cc2743fcb0ea7999b063a311daeb2c74d011c1a
Stephen Boyd <sboyd@codeaurora.org>
2013-07-03 11:05:39 -0700

    Revert "ARM: arch_timer: use full 64-bit counter for sched_clock"
    This reverts commit 023796b9be3a77481cd5ee0b64a13a55bb0d5df4.
    Leaving the timer running in the bootloader and booting the
    kernel later causes the sched_clock values to return very large
    values. This also fails to stop sched_clock during suspend
    leading to problems with RT threads.
    
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- arch/arm/kernel/arch_timer.c
+++ arch/arm/kernel/arch_timer.c
@@ -22,11 +22,9 @@ static unsigned long arch_timer_read_counter_long(void)
 	return arch_timer_read_counter();
 }
 
-static u32 sched_clock_mult __read_mostly;
-
-static unsigned long long notrace arch_timer_sched_clock(void)
+static u32 arch_timer_read_counter_u32(void)
 {
-	return arch_timer_read_counter() * sched_clock_mult;
+	return arch_timer_read_counter();
 }
 
 static struct delay_timer arch_delay_timer;
@@ -41,18 +39,12 @@ static void __init arch_timer_delay_timer_register(void)
 
 int __init arch_timer_arch_init(void)
 {
-        u32 arch_timer_rate = arch_timer_get_rate();
-
-	if (arch_timer_rate == 0)
+	if (arch_timer_get_rate() == 0)
 		return -ENXIO;
 
 	arch_timer_delay_timer_register();
 
-	/* Cache the sched_clock multiplier to save a divide in the hot path. */
-	sched_clock_mult = NSEC_PER_SEC / arch_timer_rate;
-	sched_clock_func = arch_timer_sched_clock;
-	pr_info("sched_clock: ARM arch timer >56 bits at %ukHz, resolution %uns\n",
-		arch_timer_rate / 1000, sched_clock_mult);
-
+	setup_sched_clock(arch_timer_read_counter_u32,
+			  32, arch_timer_get_rate());
 	return 0;
 }
