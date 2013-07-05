1bbf97af3a3923a9b74521fdcc96986875a03a83
Sameer Thalappil <sameert@codeaurora.org>
2013-07-05 14:58:32 -0700

    cfg80211: Add AP stopped interface
    AP stopped interface can be used to indicate that the AP mode has
    stopped functioning, WLAN driver may have encountered errors that has
    forced the driver to stop the AP mode.
    
    When the driver is in P2P-Go mode, and when it goes thru automatic
    recovery from firmware crashes, it uses this interface to notify the
    userspace that the group has been deleted.
    
    CRs-Fixed: 453060
    Change-Id: Ifcd8d4f0c0b26f56a56fb8560aa474297b7521d4
    Signed-off-by: Sameer Thalappil <sameert@codeaurora.org>

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -5367,6 +5367,13 @@ wiphy_ext_feature_isset(struct wiphy *wiphy,
 /* ethtool helper */
 void cfg80211_get_drvinfo(struct net_device *dev, struct ethtool_drvinfo *info);
 
+/**
+ * cfg80211_ap_stopped - notify userspace that AP mode stopped
+ * @netdev: network device
+ * @gfp: context flags
+ */
+void cfg80211_ap_stopped(struct net_device *netdev, gfp_t gfp);
+
 /* Logging, debugging and troubleshooting/diagnostic helpers. */
 
 /* wiphy_printk helpers, similar to dev_printk */
