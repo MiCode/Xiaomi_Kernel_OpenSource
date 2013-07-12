18ffde2b6cc09a760bef13fa4d5362998ea87e50
Adrian Salido-Moreno <adrianm@codeaurora.org>
2013-07-11 21:11:02 -0700

    msm: mdss: expose MDP capabilities through sysfs
    User space applications currently rely on special private ioctl to query
    capabilities of MDP driver. As more capabilities are added it becomes
    more difficult to have to extend and maintain header file compatibility.
    Provide a sysfs node which can be queried for MDP capabilities.
    
    Change-Id: I59efb29bf465083a1ef65c0419226788b44085b1
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>
    Signed-off-by: Shalabh Jain <shalabhj@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_mdp_overlay.c
+++ drivers/video/fbdev/msm/mdss_mdp_overlay.c
@@ -2158,6 +2158,11 @@ int mdss_mdp_overlay_init(struct msm_fb_data_type *mfd)
 		goto init_fail;
 	}
 
+	rc = sysfs_create_link_nowarn(&dev->kobj,
+			&mdp5_data->mdata->pdev->dev.kobj, "mdp");
+	if (rc)
+		pr_warn("problem creating link to mdp sysfs\n");
+
 	pm_runtime_set_suspended(&mfd->pdev->dev);
 	pm_runtime_enable(&mfd->pdev->dev);
 
