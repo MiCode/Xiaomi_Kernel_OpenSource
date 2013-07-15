ab1680997cb5aa52084a80ff2f818fef871fcd7a
Vinay Kalia <vkalia@codeaurora.org>
2013-07-15 11:32:17 -0700

    msm: vidc: Increase maximum buffer count limit
    Some clients want to allocate more buffers on decoder
    input port than required (mainly for performance reasons).
    Video decoder should honor the client request as long as
    V4L2 framework permits.
    
    CRs-fixed: 509088
    Change-Id: Ic0da1b11ca49ebcd4498f8c8d93ba5b0e2dae8ba
    Signed-off-by: Vinay Kalia <vkalia@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vdec.c
+++ drivers/media/platform/msm/vidc/msm_vdec.c
@@ -21,7 +21,7 @@
 
 #define MSM_VDEC_DVC_NAME "msm_vdec_8974"
 #define MIN_NUM_OUTPUT_BUFFERS 4
-#define MAX_NUM_OUTPUT_BUFFERS 6
+#define MAX_NUM_OUTPUT_BUFFERS VIDEO_MAX_FRAME
 
 enum msm_vdec_ctrl_cluster {
 	MSM_VDEC_CTRL_CLUSTER_MAX = 1 << 0,
