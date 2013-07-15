1c014f321e6d67f47504284701013f4c1eeba0b5
Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
2013-07-15 11:52:09 -0700

    kernel/lib: add additional debug capabilites for data corruption
    Data corruptions in the kernel often end up in system crashes that
    are easier to debug closer to the time of detection. Specifically,
    if we do not panic immediately after lock or list corruptions have been
    detected, the problem context is lost in the ensuing system mayhem.
    Add support for allowing system crash immediately after such corruptions
    are detected. The CONFIG option controls the enabling/disabling of the
    feature.
    
    Change-Id: I9b2eb62da506a13007acff63e85e9515145909ff
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>

--- lib/spinlock_debug.c
+++ lib/spinlock_debug.c
@@ -12,6 +12,7 @@
 #include <linux/debug_locks.h>
 #include <linux/delay.h>
 #include <linux/export.h>
+#include <linux/bug.h>
 
 void __raw_spin_lock_init(raw_spinlock_t *lock, const char *name,
 			  struct lock_class_key *key)
@@ -64,6 +65,7 @@ static void spin_dump(raw_spinlock_t *lock, const char *msg)
 		owner ? owner->comm : "<none>",
 		owner ? task_pid_nr(owner) : -1,
 		lock->owner_cpu);
+	BUG_ON(PANIC_CORRUPTION);
 	dump_stack();
 }
 
