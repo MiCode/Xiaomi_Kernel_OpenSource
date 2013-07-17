838b77548405d8b0fd7ef12f6d99ce23e5971fcf
Matt Wagantall <mattw@codeaurora.org>
2013-07-16 23:53:08 -0700

    net: flow: Prevent bringing up new CPUs during per-CPU initialization
    In a rare race-condition, it's possible that a CPU will be brought
    online between when the for_each_online_cpu() loop executes to
    call flow_cache_cpu_prepare(), and when the hotplug notifier for
    calling this same function is registered. If this happens,
    flow_cache_cpu_prepare() will never be called for the new CPU,
    resulting in crashes due to uninitialized per-cpu data.
    
    Fix this by preventing CPUs from being added or removed during
    this small but sensitive window.
    
    Change-Id: Iafbbaa8a50e5c527392d130561874313720849d0
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

--- net/core/flow.c
+++ net/core/flow.c
@@ -456,6 +456,7 @@ static int __init flow_cache_init(struct flow_cache *fc)
 	if (!fc->percpu)
 		return -ENOMEM;
 
+	get_online_cpus();
 	for_each_online_cpu(i) {
 		if (flow_cache_cpu_prepare(fc, i))
 			goto err;
@@ -464,6 +465,7 @@ static int __init flow_cache_init(struct flow_cache *fc)
 		.notifier_call = flow_cache_cpu,
 	};
 	register_hotcpu_notifier(&fc->hotcpu_notifier);
+	put_online_cpus();
 
 	setup_timer(&fc->rnd_timer, flow_cache_new_hashrnd,
 		    (unsigned long) fc);
@@ -473,6 +475,7 @@ static int __init flow_cache_init(struct flow_cache *fc)
 	return 0;
 
 err:
+	put_online_cpus();
 	for_each_possible_cpu(i) {
 		struct flow_cache_percpu *fcp = per_cpu_ptr(fc->percpu, i);
 		kfree(fcp->hash_table);
