a8bebb0800181b3dac08e48578c100e36f605901
dgaribal <dgaribal@codeaurora.org>
2013-07-17 15:31:52 -0700

    tty: serial: Use "no_log" readl/writel variants in hsl_read/write
    hsl_read and hsl_write both directly call ioread32/iowrite32, which
    leads to either a __raw_readl or __raw_writel call. These
    readl/writel call are logged to the register trace buffer (RTB) by
    default and generate a very large amount of output. This patch
    directly calls __raw_readl_no_log and __raw_writel_no_log in
    hsl_read and hsl_write to avoid logging these events to the RTB.
    
    Change-Id: I9b3253996188f0642a4f6dedf449ec23fa1de05a
    Signed-off-by: David Garibaldi <dgaribal@codeaurora.org>

--- drivers/tty/serial/msm_serial_hs_lite.c
+++ drivers/tty/serial/msm_serial_hs_lite.c
@@ -47,6 +47,8 @@
 #include <linux/of_device.h>
 #include <linux/of_gpio.h>
 #include <linux/wakelock.h>
+#include <linux/types.h>
+#include <asm/byteorder.h>
 #include <mach/board.h>
 #include <mach/msm_serial_hs_lite.h>
 #include <mach/msm_bus.h>
@@ -166,12 +168,17 @@ static inline void wait_for_xmitr(struct uart_port *port);
 static inline void msm_hsl_write(struct uart_port *port,
 				 unsigned int val, unsigned int off)
 {
-	iowrite32(val, port->membase + off);
+	__iowmb();
+	__raw_writel_no_log((__force __u32)cpu_to_le32(val),
+		port->membase + off);
 }
 static inline unsigned int msm_hsl_read(struct uart_port *port,
 		     unsigned int off)
 {
-	return ioread32(port->membase + off);
+	unsigned int v = le32_to_cpu((__force __le32)__raw_readl_no_log(
+		port->membase + off));
+	__iormb();
+	return v;
 }
 
 static unsigned int msm_serial_hsl_has_gsbi(struct uart_port *port)
