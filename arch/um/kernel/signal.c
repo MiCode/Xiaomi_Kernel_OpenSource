c177c662df8565396ebf655d72d2b9b2d3e39c60
Richard Weinberger <richard@nod.at>
2013-07-19 11:31:36 +0200

    um: siginfo cleanup
    Currently we use both struct siginfo and siginfo_t.
    Let's use struct siginfo internally to avoid ongoing
    compiler warning. We are allowed to do so because
    struct siginfo and siginfo_t are equivalent
    
    [cherry-pick of upstream 9a8c1359571c5d5e2fbc43cf457a6486b70a70cb]
    
    Change-Id: I564775c9ed515c39ffff7d7d600a85d50291f31d
    Signed-off-by: Richard Weinberger <richard@nod.at>
    Signed-off-by: Lorenzo Colitti <lorenzo@google.com>
    Git-commit: dfca41467fa4570189f76e0fb9e0f911bc88ceb8
    Git-repo: https://android.googlesource.com/kernel/common.git
    Signed-off-by: Ian Maund <imaund@codeaurora.org>
    Signed-off-by: Srinivasarao P <spathi@codeaurora.org>

--- arch/um/kernel/signal.c
+++ arch/um/kernel/signal.c
@@ -23,7 +23,7 @@ EXPORT_SYMBOL(unblock_signals);
  * OK, we're invoking a handler
  */
 static int handle_signal(struct pt_regs *regs, unsigned long signr,
-			 struct k_sigaction *ka, siginfo_t *info,
+			 struct k_sigaction *ka, struct siginfo *info,
 			 sigset_t *oldset)
 {
 	unsigned long sp;
@@ -76,7 +76,7 @@ static int handle_signal(struct pt_regs *regs, unsigned long signr,
 static int kern_do_signal(struct pt_regs *regs)
 {
 	struct k_sigaction ka_copy;
-	siginfo_t info;
+	struct siginfo info;
 	sigset_t *oldset;
 	int sig, handled_sig = 0;
 
