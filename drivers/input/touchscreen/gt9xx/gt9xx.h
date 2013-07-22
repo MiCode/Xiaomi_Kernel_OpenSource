ed6a734b04351ee2b1305f14d3edfa354f1e707e
Bingzhe Cai <bingzhec@codeaurora.org>
2013-07-22 18:23:20 +0800

    input: touchpanel: Add FB notifications support to GT9xx
    Add support for FB notifications to trigger suspend/resume
    based on FB blank/unblank events.
    
    Change-Id: I61803e3a731e047d0641e3b1ccbae58b9bd1bd89
    Signed-off-by: Bingzhe Cai <bingzhec@codeaurora.org>
    Signed-off-by: Sudhakar Manapati <smanap@codeaurora.org>

--- drivers/input/touchscreen/gt9xx/gt9xx.h
+++ drivers/input/touchscreen/gt9xx/gt9xx.h
@@ -33,7 +33,11 @@
 #include <linux/regulator/consumer.h>
 #include <linux/firmware.h>
 #include <linux/debugfs.h>
-#if defined(CONFIG_HAS_EARLYSUSPEND)
+
+#if defined(CONFIG_FB)
+#include <linux/notifier.h>
+#include <linux/fb.h>
+#elif defined(CONFIG_HAS_EARLYSUSPEND)
 #include <linux/earlysuspend.h>
 #define GOODIX_SUSPEND_LEVEL 1
 #endif
@@ -65,9 +69,6 @@ struct goodix_ts_data {
 	struct hrtimer timer;
 	struct workqueue_struct *goodix_wq;
 	struct work_struct	work;
-#if defined(CONFIG_HAS_EARLYSUSPEND)
-	struct early_suspend early_suspend;
-#endif
 	s32 irq_is_disabled;
 	s32 use_irq;
 	u16 abs_x_max;
@@ -87,6 +88,11 @@ struct goodix_ts_data {
 	struct regulator *avdd;
 	struct regulator *vdd;
 	struct regulator *vcc_i2c;
+#if defined(CONFIG_FB)
+	struct notifier_block fb_notif;
+#elif defined(CONFIG_HAS_EARLYSUSPEND)
+	struct early_suspend early_suspend;
+#endif
 };
 
 extern u16 show_len;
@@ -97,7 +103,7 @@ extern u16 total_len;
 #define GTP_CHANGE_X2Y			0
 #define GTP_DRIVER_SEND_CFG		1
 #define GTP_HAVE_TOUCH_KEY		1
-#define GTP_POWER_CTRL_SLEEP	1
+#define GTP_POWER_CTRL_SLEEP	0
 #define GTP_ICS_SLOT_REPORT	0
 
 /* auto updated by .bin file as default */
