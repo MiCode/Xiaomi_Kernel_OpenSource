0858fc7e446736501ca8203c94c0977b8e73aa86
Alexandra Chin <alexandra.chin@tw.synaptics.com>
2013-07-25 11:58:17 -0700

    input: synaptics: Add support for new firmware versions
    - Add DS5 firmware update support
    - Support polling mode in firmware update
    
    Signed-off-by: Alexandra Chin <alexandra.chin@tw.synaptics.com>
    Change-Id: Ibf9a6ffcd843866683b3b138af56f09545f3742c
    Git-commit: e01cec21f08bc35ba3e97981b967bb4e2c52267a
    Git-repo: git://github.com/synaptics-touch/msm-3.4.git
    [amaloche@codeaurora.org: Changes due to base driver
    differences]
    Signed-off-by: Amy Maloche <amaloche@codeaurora.org>

--- drivers/input/touchscreen/synaptics_i2c_rmi4.c
+++ drivers/input/touchscreen/synaptics_i2c_rmi4.c
@@ -3096,6 +3096,11 @@ static void synaptics_rmi4_early_suspend(struct early_suspend *h)
 			container_of(h, struct synaptics_rmi4_data,
 			early_suspend);
 
+	if (rmi4_data->stay_awake)
+		rmi4_data->staying_awake = true;
+	else
+		rmi4_data->staying_awake = false;
+
 	rmi4_data->touch_stopped = true;
 	wake_up(&rmi4_data->wait);
 	synaptics_rmi4_irq_enable(rmi4_data, false);
@@ -3121,6 +3126,9 @@ static void synaptics_rmi4_late_resume(struct early_suspend *h)
 	struct synaptics_rmi4_data *rmi4_data =
 			container_of(h, struct synaptics_rmi4_data,
 			early_suspend);
+
+	if (rmi4_data->staying_awake)
+		return;
 
 	if (rmi4_data->full_pm_cycle)
 		synaptics_rmi4_resume(&(rmi4_data->input_dev->dev));
@@ -3266,6 +3274,12 @@ static int synaptics_rmi4_suspend(struct device *dev)
 	struct synaptics_rmi4_data *rmi4_data = dev_get_drvdata(dev);
 	int retval;
 
+	if (rmi4_data->stay_awake) {
+		rmi4_data->staying_awake = true;
+		return 0;
+	} else
+		rmi4_data->staying_awake = false;
+
 	if (rmi4_data->suspended) {
 		dev_info(dev, "Already in suspend state\n");
 		return 0;
@@ -3316,6 +3330,9 @@ static int synaptics_rmi4_resume(struct device *dev)
 {
 	struct synaptics_rmi4_data *rmi4_data = dev_get_drvdata(dev);
 	int retval;
+
+	if (rmi4_data->staying_awake)
+		return 0;
 
 	if (!rmi4_data->suspended) {
 		dev_info(dev, "Already in awake state\n");
