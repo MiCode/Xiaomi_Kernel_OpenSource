03c3dba6e4b3592773ef2261dc3b6fe158a87a22
Jordan Crouse <jcrouse@codeaurora.org>
2013-07-29 16:02:18 -0600

    msm: kgsl: Try to run soft reset on all targets that support it
    Run soft reset on all targets that define a soft_reset function hook.
    If the target defines jump table offsets then we can run through a
    faster reset sequence, otherwise default to the slower full reset
    path.  In any event, both these options are much faster than the
    hard reset path that toggles the regulators.
    
    Change-Id: Ic0dedbad4cbfdc083f16b026e133159814481886
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -2293,8 +2293,7 @@ int adreno_soft_reset(struct kgsl_device *device)
 	struct adreno_device *adreno_dev = ADRENO_DEVICE(device);
 	int ret;
 
-	/* If the jump table index is 0 soft reset is not supported */
-	if ((!adreno_dev->pm4_jt_idx) || (!adreno_dev->gpudev->soft_reset)) {
+	if (!adreno_dev->gpudev->soft_reset) {
 		dev_WARN_ONCE(device->dev, 1, "Soft reset not supported");
 		return -EINVAL;
 	}
@@ -2331,10 +2330,15 @@ int adreno_soft_reset(struct kgsl_device *device)
 	device->ftbl->irqctrl(device, 1);
 
 	/*
-	 * Restart the ringbuffer - we can go down the warm start path because
-	 * power was never yanked
+	 * If we have offsets for the jump tables we can try to do a warm start,
+	 * otherwise do a full ringbuffer restart
 	 */
-	ret = adreno_ringbuffer_warm_start(&adreno_dev->ringbuffer);
+
+	if (adreno_dev->pm4_jt_idx)
+		ret = adreno_ringbuffer_warm_start(&adreno_dev->ringbuffer);
+	else
+		ret = adreno_ringbuffer_start(&adreno_dev->ringbuffer);
+
 	if (ret)
 		return ret;
 
