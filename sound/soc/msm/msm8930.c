138af1f018392d440da42dce807cdded9715272f
Simmi Pateriya <simmip@codeaurora.org>
2013-07-29 17:29:46 +0530

    ASoC: wcd9304: Fix incorrect detection of headphone
    On slow insertion of headphone, if the v_hs_max value
    is too high, it can be detected as headset instead of
    lineout when the mic is floating. Adjust the value of
    v_hs_max to correct plug type detection.
    
    CRs-Fixed: 507511
    Change-Id: I80a54aac1555d1e81bf75ace6a7e3ca5f6114706
    Signed-off-by: Simmi Pateriya <simmip@codeaurora.org>

--- sound/soc/msm/msm8930.c
+++ sound/soc/msm/msm8930.c
@@ -581,7 +581,7 @@ static void *def_sitar_mbhc_cal(void)
 #undef S
 #define S(X, Y) ((SITAR_MBHC_CAL_PLUG_TYPE_PTR(sitar_cal)->X) = (Y))
 	S(v_no_mic, 30);
-	S(v_hs_max, 1650);
+	S(v_hs_max, 1500);
 #undef S
 #define S(X, Y) ((SITAR_MBHC_CAL_BTN_DET_PTR(sitar_cal)->X) = (Y))
 	S(c[0], 62);
