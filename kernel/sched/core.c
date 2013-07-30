315381ec5f5db27932a11f749a8e8f4414fb526c
Steve Muckle <smuckle@codeaurora.org>
2013-07-30 14:22:33 -0700

    sched: change WARN_ON_ONCE to printk_deferred() in try_to_wake_up_local()
    try_to_wake_up_local() is called with the rq lock held. Printing to
    console in this context can result in a deadlock if klogd needs to
    be woken up. Print to the kernel log buffer via printk_sched()
    instead which avoids the wakeup.
    
    Change-Id: Ia07baea3cb7e0b158545207fdbbb866203256d3c
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1770,9 +1770,13 @@ static void try_to_wake_up_local(struct task_struct *p)
 {
 	struct rq *rq = task_rq(p);
 
-	if (WARN_ON_ONCE(rq != this_rq()) ||
-	    WARN_ON_ONCE(p == current))
+	if (rq != this_rq() || p == current) {
+		printk_deferred("%s: Failed to wakeup task %d (%s), rq = %p,"
+				" this_rq = %p, p = %p, current = %p\n",
+			__func__, task_pid_nr(p), p->comm, rq,
+			this_rq(), p, current);
 		return;
+	}
 
 	lockdep_assert_held(&rq->lock);
 
