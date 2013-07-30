9e6997507a0c9652c7deab889fcd2bede21b1771
Vimal Puthanveed <vimalp@codeaurora.org>
2013-07-30 14:40:34 -0700

    ASoC: msm: qdsp6: Add support to configure ISO and ARIB coefficients
    Add support for configuring ISO or ARIB stereo mixing
    coefficients. This change introduces a function with which,
    AAC multi-channel driver can configure ISO or ARIB coefficients
    according to configuration done by the client.
    
    Change-Id: I1ca3b0a74a7258f9adb6818595f070e249abca4d
    Signed-off-by: Vimal Puthanveed <vimalp@codeaurora.org>

--- include/sound/apr_audio-v2.h
+++ include/sound/apr_audio-v2.h
@@ -600,6 +600,15 @@ struct adm_cmd_matrix_mute_v5 {
 	/* Clients must set this field to zero.*/
 } __packed;
 
+#define ASM_PARAM_ID_AAC_STEREO_MIX_COEFF_SELECTION_FLAG_V2 (0x00010DD8)
+
+struct asm_aac_stereo_mix_coeff_selection_param_v2 {
+	struct apr_hdr          hdr;
+	u32                     param_id;
+	u32                     param_size;
+	u32                     aac_stereo_mix_coeff_flag;
+} __packed;
+
 /* Allows a client to connect the desired stream to
  * the desired AFE port through the stream router
  *
