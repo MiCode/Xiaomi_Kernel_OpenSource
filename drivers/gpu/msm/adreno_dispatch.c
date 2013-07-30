6c468fc6b47bfbf5de9a7defb05c71451b4b5cbc
Jordan Crouse <jcrouse@codeaurora.org>
2013-07-30 16:43:02 -0600

    msm: kgsl: Improve the dispatcher issuecmds loop
    Instead of playing silly tricks to try to avoid going into an
    infinite loop while processing pending contexts do the smart
    thing and copy off the entire pending queue into a temporary
    list.  That leaves the master list free to accept new and
    requeued contexts and we can do evil things to our temporary
    list.
    
    Change-Id: Ic0dedbad365206031854fc95b5353184cabd40a1
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -393,6 +393,13 @@ static int dispatcher_context_sendcmds(struct adreno_device *adreno_dev,
 	return requeued ? -1 : 0;
 }
 
+static void plist_move(struct plist_head *old, struct plist_head *new)
+{
+	plist_head_init(new);
+	list_splice_tail(&old->node_list, &new->node_list);
+	plist_head_init(old);
+}
+
 /**
  * _adreno_dispatcher_issuecmds() - Issue commmands from pending contexts
  * @adreno_dev: Pointer to the adreno device struct
@@ -403,31 +410,36 @@ static int dispatcher_context_sendcmds(struct adreno_device *adreno_dev,
 static int _adreno_dispatcher_issuecmds(struct adreno_device *adreno_dev)
 {
 	struct adreno_dispatcher *dispatcher = &adreno_dev->dispatcher;
-	int last_id = 0;
+	struct plist_head tmp;
+	struct adreno_context *drawctxt, *next;
 
+	/* Leave early if the dispatcher isn't in a happy state */
+	if ((dispatcher->state != ADRENO_DISPATCHER_ACTIVE) ||
+		adreno_gpu_fault(adreno_dev) != 0)
+			return 0;
+
+	/* Copy the current context list to a temporary list */
+	spin_lock(&dispatcher->plist_lock);
+	plist_move(&dispatcher->pending, &tmp);
+	spin_unlock(&dispatcher->plist_lock);
+
+	/* Try to fill the ringbuffer as much as possible */
 	while (dispatcher->inflight < _dispatcher_inflight) {
-		struct adreno_context *drawctxt = NULL;
 
-		/* Don't do anything if the dispatcher is paused */
-		if (dispatcher->state != ADRENO_DISPATCHER_ACTIVE)
+		/* Stop doing things if the dispatcher is paused or faulted */
+		if ((dispatcher->state != ADRENO_DISPATCHER_ACTIVE) ||
+			adreno_gpu_fault(adreno_dev) != 0)
 			break;
 
-		if (adreno_gpu_fault(adreno_dev) != 0)
+		if (plist_head_empty(&tmp))
 			break;
 
-		spin_lock(&dispatcher->plist_lock);
+		/* Get the next entry on the list */
+		drawctxt = plist_first_entry(&tmp, struct adreno_context,
+			pending);
 
-		if (!plist_head_empty(&dispatcher->pending)) {
-			drawctxt = plist_first_entry(&dispatcher->pending,
-				struct adreno_context, pending);
-
-			plist_del(&drawctxt->pending, &dispatcher->pending);
-		}
-
-		spin_unlock(&dispatcher->plist_lock);
-
-		if (drawctxt == NULL)
-			break;
+		/* Remove it from the list */
+		plist_del(&drawctxt->pending, &tmp);
 
 		if (kgsl_context_detached(&drawctxt->base) ||
 			drawctxt->state == ADRENO_CONTEXT_STATE_INVALID) {
@@ -435,24 +447,25 @@ static int _adreno_dispatcher_issuecmds(struct adreno_device *adreno_dev)
 			continue;
 		}
 
-		/*
-		 * Don't loop endlessly on the same stalled context - if we see
-		 * the same context twice in a row then assume that there is
-		 * nothing else to do and bail.  But don't forget to put the
-		 * stalled context back on the queue otherwise it will get
-		 * forgotten
-		 */
-
-		if (last_id == drawctxt->base.id) {
-			dispatcher_queue_context(adreno_dev, drawctxt);
-			kgsl_context_put(&drawctxt->base);
-			break;
-		}
-
-		last_id = drawctxt->base.id;
 		dispatcher_context_sendcmds(adreno_dev, drawctxt);
 		kgsl_context_put(&drawctxt->base);
 	}
+
+	/* Requeue any remaining contexts for the next go around */
+
+	spin_lock(&dispatcher->plist_lock);
+
+	plist_for_each_entry_safe(drawctxt, next, &tmp, pending) {
+		int prio = drawctxt->pending.prio;
+
+		/* Reset the context node */
+		plist_node_init(&drawctxt->pending, prio);
+
+		/* And put it back in the master list */
+		plist_add(&drawctxt->pending, &dispatcher->pending);
+	}
+
+	spin_unlock(&dispatcher->plist_lock);
 
 	return 0;
 }
