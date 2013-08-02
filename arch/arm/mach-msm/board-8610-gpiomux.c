3ef74f04f46bcfe339aa92ec1def9c13ad6cdc98
Kuirong Wang <kuirongw@codeaurora.org>
2013-08-02 14:46:45 -0700

    msm: board-8610-gpio-mux: Add DMIC gpio configuration
    GPIO_100 and GPIO_101 can be configured as different functions
    depending on the HW usage. On MSM8610 CDP, GPIO_100 and GPIO_101
    are used as digital MIC signals for audio recording. Configure
    these GPIOs for the digital MIC usage.
    
    CRs-fixed: 585675
    Change-Id: Ibeafbbc7246c328922d294cc8b6dd4862bf5a1d6
    Signed-off-by: Bhalchandra Gajare <gajare@codeaurora.org>

--- arch/arm/mach-msm/board-8610-gpiomux.c
+++ arch/arm/mach-msm/board-8610-gpiomux.c
@@ -631,6 +631,28 @@ static struct msm_gpiomux_config msm_interrupt_configs[] __initdata = {
 	},
 };
 
+static struct gpiomux_setting gpio_cdc_dmic_cfg = {
+	.func = GPIOMUX_FUNC_1,
+	.drv = GPIOMUX_DRV_4MA,
+	.pull = GPIOMUX_PULL_NONE,
+};
+
+
+static struct msm_gpiomux_config msm_cdc_dmic_configs[] __initdata = {
+	{
+		.gpio = 100,	/* DMIC CLK */
+		.settings = {
+			[GPIOMUX_SUSPENDED] = &gpio_cdc_dmic_cfg,
+		},
+	},
+	{
+		.gpio = 101,	/* DMIC DATA */
+		.settings = {
+			[GPIOMUX_SUSPENDED] = &gpio_cdc_dmic_cfg,
+		},
+	},
+};
+
 void __init msm8610_init_gpiomux(void)
 {
 	int rc;
@@ -668,4 +690,7 @@ void __init msm8610_init_gpiomux(void)
 		msm_gpiomux_install(msm_non_qrd_configs,
 			ARRAY_SIZE(msm_non_qrd_configs));
 	}
+	if (of_board_is_cdp())
+		msm_gpiomux_install(msm_cdc_dmic_configs,
+			ARRAY_SIZE(msm_cdc_dmic_configs));
 }
