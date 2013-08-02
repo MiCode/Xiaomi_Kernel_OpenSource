ca4e012922e2363169568f46a5d5b11a315b5827
Siddartha Mohanadoss <smohanad@codeaurora.org>
2013-08-02 13:37:12 -0700

    hwmon: qpnp-adc-current: Fix error path
    Invalid jump to cleanup before the hwmon device
    is registered. Cleanup the error path to avoid
    panic if a failure occurs before hwmon device
    register occurs.
    
    Change-Id: I3477d207dc6402bed33d15723f2763ee85a1ca46
    Signed-off-by: Siddartha Mohanadoss <smohanad@codeaurora.org>

--- drivers/hwmon/qpnp-adc-current.c
+++ drivers/hwmon/qpnp-adc-current.c
@@ -1067,8 +1067,7 @@ static int qpnp_iadc_probe(struct spmi_device *spmi)
 			GFP_KERNEL);
 	if (!adc_qpnp) {
 		dev_err(&spmi->dev, "Unable to allocate memory\n");
-		rc = -ENOMEM;
-		goto fail;
+		return -ENOMEM;
 	}
 
 	iadc->dev = &(spmi->dev);
@@ -1077,7 +1076,7 @@ static int qpnp_iadc_probe(struct spmi_device *spmi)
 	rc = qpnp_adc_get_devicetree_data(spmi, iadc->adc);
 	if (rc) {
 		dev_err(&spmi->dev, "failed to read device tree\n");
-		goto fail;
+		return rc;
 	}
 
 	iadc->vadc_dev = qpnp_get_vadc(&spmi->dev, "iadc");
@@ -1085,7 +1084,7 @@ static int qpnp_iadc_probe(struct spmi_device *spmi)
 		rc = PTR_ERR(iadc->vadc_dev);
 		if (rc != -EPROBE_DEFER)
 			pr_err("vadc property missing, rc=%d\n", rc);
-		goto fail;
+		return rc;
 	}
 
 	mutex_init(&iadc->adc->adc_lock);
