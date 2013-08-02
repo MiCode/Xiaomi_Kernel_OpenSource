120b7dce7520cfccd37a9e958bede672e5424aff
Sahitya Tummala <stummala@codeaurora.org>
2013-08-02 09:17:54 +0530

    mmc: sdhci: Enhance debug register dump
    Add new host operation dump_vendor_regs to provide a
    mechanism through which host drivers can dump vendor
    specific registers in addition to SDHC registers
    during error conditions.
    
    Change-Id: Ifba3ddcb18c3c0917343d99fe58d5ed04b2da871
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -312,6 +312,7 @@ struct sdhci_ops {
 	unsigned int	(*get_max_segments)(void);
 	void	(*platform_bus_voting)(struct sdhci_host *host, u32 enable);
 	void    (*disable_data_xfer)(struct sdhci_host *host);
+	void	(*dump_vendor_regs)(struct sdhci_host *host);
 };
 
 #ifdef CONFIG_MMC_SDHCI_IO_ACCESSORS
