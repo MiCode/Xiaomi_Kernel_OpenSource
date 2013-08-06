1a538b3997d39f27947117e7d14c993691e8a2ed
Tingting Yang <tingting@codeaurora.org>
2013-08-06 11:12:52 +0800

    msm: move printk out of spin lock low_water_lock
    cpu3 stuck in printk more time in spin lock low_water_lock cause cpu0
    get spin lock fail and system crashed.
    
    CRs-Fixed: 969097
    Change-Id: I75356a4b4171ae2888ce6cce792f569b5ca8cdcf
    Signed-off-by: Tingting Yang <tingting@codeaurora.org>
    Signed-off-by: Prasad Sodagudi <psodagud@codeaurora.org>
    Signed-off-by: Minming Qi <mqi@codeaurora.org>

--- kernel/exit.c
+++ kernel/exit.c
@@ -717,6 +717,7 @@ static void check_stack_usage(void)
 	static DEFINE_SPINLOCK(low_water_lock);
 	static int lowest_to_date = THREAD_SIZE;
 	unsigned long free;
+	int islower = false;
 
 	free = stack_not_used(current);
 
@@ -725,11 +726,15 @@ static void check_stack_usage(void)
 
 	spin_lock(&low_water_lock);
 	if (free < lowest_to_date) {
-		pr_info("%s (%d) used greatest stack depth: %lu bytes left\n",
-			current->comm, task_pid_nr(current), free);
 		lowest_to_date = free;
+		islower = true;
 	}
 	spin_unlock(&low_water_lock);
+
+	if (islower) {
+		pr_info("%s (%d) used greatest stack depth: %lu bytes left\n",
+				current->comm, task_pid_nr(current), free);
+	}
 }
 #else
 static inline void check_stack_usage(void) {}
