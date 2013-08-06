c289d0488fdc06cc3fb2676195886f84a75715f4
Rohit Vaswani <rvaswani@codeaurora.org>
2013-08-05 19:49:57 -0700

    msm: fsm9900: Add dummy clocks for crypto
    These dummy clocks will be used by the crypto driver in the
    fsm9900 sim target.
    
    Change-Id: I18e906934506e2abccc493927bbd2e2f9d64e59e
    Acked-by: Kaushik Sikdar <ksikdar@qti.qualcomm.com>
    Signed-off-by: Rohit Vaswani <rvaswani@codeaurora.org>

--- arch/arm/mach-msm/board-fsm9900.c
+++ arch/arm/mach-msm/board-fsm9900.c
@@ -71,6 +71,31 @@ static struct clk_lookup msm_clocks_dummy[] = {
 	CLK_DUMMY("iface_clk",	SDC1_P_CLK,	"msm_sdcc.1", OFF),
 	CLK_DUMMY("core_clk",	SDC2_CLK,	"msm_sdcc.2", OFF),
 	CLK_DUMMY("iface_clk",	SDC2_P_CLK,	"msm_sdcc.2", OFF),
+	CLK_DUMMY("core_clk",   BLSP1_UART_CLK, "f991f000.serial", OFF),
+	CLK_DUMMY("iface_clk",  BLSP1_UART_CLK, "f991f000.serial", OFF),
+
+	CLK_DUMMY("core_clk",     NULL,	        "fd400000.qcom,qcrypto", OFF),
+	CLK_DUMMY("iface_clk",    NULL,	        "fd400000.qcom,qcrypto", OFF),
+	CLK_DUMMY("bus_clk",      NULL,	        "fd400000.qcom,qcrypto", OFF),
+	CLK_DUMMY("core_clk_src", NULL,	        "fd400000.qcom,qcrypto", OFF),
+
+	CLK_DUMMY("core_clk",     NULL,         "fd440000.qcom,qcrypto", OFF),
+	CLK_DUMMY("iface_clk",    NULL,         "fd440000.qcom,qcrypto", OFF),
+	CLK_DUMMY("bus_clk",      NULL,         "fd440000.qcom,qcrypto", OFF),
+	CLK_DUMMY("core_clk_src", NULL,         "fd440000.qcom,qcrypto", OFF),
+
+	CLK_DUMMY("core_clk",     NULL,         "fe140000.qcom,qcota", OFF),
+	CLK_DUMMY("iface_clk",    NULL,         "fe140000.qcom,qctoa", OFF),
+	CLK_DUMMY("bus_clk",      NULL,         "fe140000.qcom,qcota", OFF),
+	CLK_DUMMY("core_clk_src", NULL,         "fe140000.qcom,qcota", OFF),
+
+	CLK_DUMMY("core_clk",     NULL,         "fe100000.qcom,qcota", OFF),
+	CLK_DUMMY("iface_clk",    NULL,         "fe100000.qcom,qcota", OFF),
+	CLK_DUMMY("bus_clk",      NULL,         "fe100000.qcom,qcota", OFF),
+	CLK_DUMMY("core_clk_src", NULL,         "fe100000.qcom,qcota", OFF),
+
+	CLK_DUMMY("dma_bam_pclk", NULL,         "msm_sps",             OFF),
+	CLK_DUMMY("dfab_clk",     NULL,         "msm_sps",             OFF),
 };
 
 static struct clock_init_data msm_dummy_clock_init_data __initdata = {
