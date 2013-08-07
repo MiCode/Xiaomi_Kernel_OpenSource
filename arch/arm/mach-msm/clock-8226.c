adb944895bd19adf8d9b611e6569df637de59eca
Wang Wenbin <wenbinw@codeaurora.org>
2013-08-07 14:26:09 +0800

    msm: camera: Add clock setting for OV12830 eeprom
    Add clock config for OV12830 OTP on SKUF.
    
    Change-Id: I61ac41bafebdee051ab317dce52d4d9a5a8ad97e
    Signed-off-by: Wang Wenbin <wenbinw@codeaurora.org>

--- arch/arm/mach-msm/clock-8226.c
+++ arch/arm/mach-msm/clock-8226.c
@@ -3400,6 +3400,8 @@ static struct clk_lookup msm_clocks_8226[] = {
 	/* eeprom clocks */
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "6c.qcom,eeprom"),
 	CLK_LOOKUP("cam_clk", camss_mclk0_clk.c, "6c.qcom,eeprom"),
+	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "18.qcom,eeprom"),
+	CLK_LOOKUP("cam_clk", camss_mclk0_clk.c, "18.qcom,eeprom"),
 
 	/* CCI clocks */
 	CLK_LOOKUP("camss_top_ahb_clk", camss_top_ahb_clk.c,
