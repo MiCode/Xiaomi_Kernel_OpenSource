c692168628d0d26c1d6a1e008ada9f2cd6c6fc46
Siddartha Mohanadoss <smohanad@codeaurora.org>
2013-08-07 08:45:41 -0700

    hwmon: qpnp-adc-current: Add temperature compensation
    The IADC does not meet accuracy spec with just gain and
    offset calibration. Additional compensation is
    required on top of gain/offset calibration to compensate
    for variations due to chip revision, manufacturer and
    temperature.
    
    Change-Id: If69ec412b05746e49e98fff761a70cd987bd6f7e
    Signed-off-by: Siddartha Mohanadoss <smohanad@codeaurora.org>

--- include/linux/qpnp/qpnp-adc.h
+++ include/linux/qpnp/qpnp-adc.h
@@ -622,26 +622,11 @@ enum qpnp_comp_scheme_type {
 	COMP_ID_NUM,
 };
 
-enum qpnp_iadc_rev {
-	QPNP_IADC_VER_3_0 = 0x1,
-	QPNP_IADC_VER_3_1 = 0x3,
-};
-
 #define QPNP_VBAT_SNS_COEFF_1_TYPEA				3000
 #define QPNP_VBAT_SNS_COEFF_2_TYPEA				45810000
 #define QPNP_VBAT_SNS_COEFF_3					100000
 #define QPNP_VBAT_SNS_COEFF_1_TYPEB				3500
 #define QPNP_VBAT_SNS_COEFF_2_TYPEB				80000000
-
-#define QPNP_COEFF_1					969000
-#define QPNP_COEFF_2					34
-#define QPNP_COEFF_3_TYPEA				1700000
-#define QPNP_COEFF_3_TYPEB				1000000
-#define QPNP_COEFF_4					100
-#define QPNP_COEFF_5					15000
-#define QPNP_COEFF_6					100000
-#define QPNP_COEFF_7					21700
-#define QPNP_COEFF_8					100000000
 
 /**
  * struct qpnp_adc_tm_config - Represent ADC Thermal Monitor configuration.
