d1ceca795c9541b6535b3f06e9c2da277162cdf0
Sree Sesha Aravind Vadrevu <svadrevu@codeaurora.org>
2013-08-07 18:20:47 -0700

    msm: mdss: Support planar output format for rotator
    For high end targets planar format as output from the rotator
    can be supported as there are more SMP blocks available relatively.
    Also, this gives HAL flexibility with the output buffer as currently
    its always defaulted to Psuedo Planar.
    
    Change-Id: I0428ea2a227bf73bc4eada0d2533520799321195
    Signed-off-by: Sree Sesha Aravind Vadrevu <svadrevu@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_intf_writeback.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_writeback.c
@@ -248,6 +248,7 @@ static int mdss_mdp_writeback_prepare_rot(struct mdss_mdp_ctl *ctl, void *arg)
 	struct mdss_mdp_writeback_ctx *ctx;
 	struct mdss_mdp_writeback_arg *wb_args;
 	struct mdss_mdp_rotator_session *rot;
+	struct mdss_data_type *mdata;
 	u32 format;
 
 	ctx = (struct mdss_mdp_writeback_ctx *) ctl->priv_data;
@@ -260,6 +261,11 @@ static int mdss_mdp_writeback_prepare_rot(struct mdss_mdp_ctl *ctl, void *arg)
 	rot = (struct mdss_mdp_rotator_session *) wb_args->priv_data;
 	if (!rot) {
 		pr_err("unable to retrieve rot session ctl=%d\n", ctl->num);
+		return -ENODEV;
+	}
+	mdata = ctl->mdata;
+	if (!mdata) {
+		pr_err("no mdata attached to ctl=%d", ctl->num);
 		return -ENODEV;
 	}
 	pr_debug("rot setup wb_num=%d\n", ctx->wb_num);
@@ -280,7 +286,8 @@ static int mdss_mdp_writeback_prepare_rot(struct mdss_mdp_ctl *ctl, void *arg)
 
 	ctx->rot90 = !!(rot->flags & MDP_ROT_90);
 
-	if (ctx->bwc_mode || ctx->rot90)
+	if (ctx->bwc_mode || (ctx->rot90 &&
+			     (mdata->mdp_rev < MDSS_MDP_HW_REV_102)))
 		format = mdss_mdp_get_rotator_dst_format(rot->format);
 	else
 		format = rot->format;
