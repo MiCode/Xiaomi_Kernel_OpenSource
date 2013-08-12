67b74894fadd467c7ad7b24b2779752e5248c793
Junjie Wu <junjiew@codeaurora.org>
2013-08-12 16:37:25 -0700

    msm: clock: Call regulator_enable/disable when updating vdd
    When all clocks on a regulator are disabled, regulator_disable should
    be called.  Likewise, when first clock is prepared, regulator_enable
    should be called.
    
    Add regulator_enable/disable function during voltage voting.
    
    Conflicts:
    
            arch/arm/mach-msm/clock.c
    
    Change-Id: Id036e95a4807574ae143a83cf0280fd61635257f
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>
    Signed-off-by: Vikram Mulukutla <markivx@codeaurora.org>

--- arch/arm/mach-msm/clock.c
+++ arch/arm/mach-msm/clock.c
@@ -63,36 +63,44 @@ int find_vdd_level(struct clk *clk, unsigned long rate)
 /* Update voltage level given the current votes. */
 static int update_vdd(struct clk_vdd_class *vdd_class)
 {
-	int level, rc = 0, i;
+	int level, rc = 0, i, ignore;
 	struct regulator **r = vdd_class->regulator;
 	int *uv = vdd_class->vdd_uv;
 	int *ua = vdd_class->vdd_ua;
 	int n_reg = vdd_class->num_regulators;
+	int cur_lvl = vdd_class->cur_level;
 	int max_lvl = vdd_class->num_levels - 1;
-	int lvl_base;
+	int cur_base = cur_lvl * n_reg;
+	int new_base;
 
+	/* aggregate votes */
 	for (level = max_lvl; level > 0; level--)
 		if (vdd_class->level_votes[level])
 			break;
 
-	if (level == vdd_class->cur_level)
+	if (level == cur_lvl)
 		return 0;
 
 	max_lvl = max_lvl * n_reg;
-	lvl_base = level * n_reg;
+	new_base = level * n_reg;
 	for (i = 0; i < vdd_class->num_regulators; i++) {
-		rc = regulator_set_voltage(r[i], uv[lvl_base + i],
+		rc = regulator_set_voltage(r[i], uv[new_base + i],
 					   uv[max_lvl + i]);
 		if (rc)
 			goto set_voltage_fail;
 
-		if (!ua)
-			continue;
-
-		rc = regulator_set_optimum_mode(r[i], ua[lvl_base + i]);
-		rc = rc > 0 ? 0 : rc;
+		if (ua) {
+			rc = regulator_set_optimum_mode(r[i], ua[new_base + i]);
+			rc = rc > 0 ? 0 : rc;
+			if (rc)
+				goto set_mode_fail;
+		}
+		if (cur_lvl == 0 || cur_lvl == vdd_class->num_levels)
+			rc = regulator_enable(r[i]);
+		else if (level == 0)
+			rc = regulator_disable(r[i]);
 		if (rc)
-			goto set_mode_fail;
+			goto enable_disable_fail;
 	}
 	if (vdd_class->set_vdd && !vdd_class->num_regulators)
 		rc = vdd_class->set_vdd(vdd_class, level);
@@ -102,20 +110,29 @@ static int update_vdd(struct clk_vdd_class *vdd_class)
 
 	return rc;
 
-set_mode_fail:
-	regulator_set_voltage(r[i], uv[vdd_class->cur_level * n_reg + i],
-			      uv[max_lvl + i]);
-
-set_voltage_fail:
-	lvl_base = vdd_class->cur_level * n_reg;
-	for (i--; i >= 0; i--) {
-		regulator_set_voltage(r[i], uv[lvl_base + i], uv[max_lvl + i]);
-
-		if (!ua)
-			continue;
-		regulator_set_optimum_mode(r[i], ua[lvl_base + i]);
+enable_disable_fail:
+	/*
+	 * set_optimum_mode could use voltage to derive mode.  Restore
+	 * previous voltage setting for r[i] first.
+	 */
+	if (ua) {
+		regulator_set_voltage(r[i], uv[cur_base + i], uv[max_lvl + i]);
+		regulator_set_optimum_mode(r[i], ua[cur_base + i]);
 	}
 
+set_mode_fail:
+	regulator_set_voltage(r[i], uv[cur_base + i], uv[max_lvl + i]);
+
+set_voltage_fail:
+	for (i--; i >= 0; i--) {
+		regulator_set_voltage(r[i], uv[cur_base + i], uv[max_lvl + i]);
+		if (ua)
+			regulator_set_optimum_mode(r[i], ua[cur_base + i]);
+		if (cur_lvl == 0 || cur_lvl == vdd_class->num_levels)
+			regulator_disable(r[i]);
+		else if (level == 0)
+			ignore = regulator_enable(r[i]);
+	}
 	return rc;
 }
 
@@ -607,7 +624,6 @@ static void init_sibling_lists(struct clk_lookup *clock_tbl, size_t num_clocks)
 static void vdd_class_init(struct clk_vdd_class *vdd)
 {
 	struct handoff_vdd *v;
-	int i;
 
 	if (!vdd)
 		return;
@@ -620,9 +636,6 @@ static void vdd_class_init(struct clk_vdd_class *vdd)
 	pr_debug("voting for vdd_class %s\n", vdd->class_name);
 	if (vote_vdd_level(vdd, vdd->num_levels - 1))
 		pr_err("failed to vote for %s\n", vdd->class_name);
-
-	for (i = 0; i < vdd->num_regulators; i++)
-		regulator_enable(vdd->regulator[i]);
 
 	v = kmalloc(sizeof(*v), GFP_KERNEL);
 	if (!v) {
