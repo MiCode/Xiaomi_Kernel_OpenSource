d4a32a7a132dc0a5ef2d3ee1839a2c71a248cfc3
Laura Abbott <lauraa@codeaurora.org>
2013-08-13 11:37:53 -0700

    init: Move stack canary initialization after setup_arch
    Stack canary initialization involves getting a random number.
    Getting this random number may involve accessing caches or other
    architectural specific features which are not available until
    after the architecture is setup. Move the stack canary initialization
    later to accommodate this.
    
    Change-Id: I00b564a2c3172229a44339c061fa380c17fe7d8e
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    [ohaugan@codeaurora.org: Fix trivial merge conflict]
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>

--- init/main.c
+++ init/main.c
@@ -485,11 +485,6 @@ asmlinkage __visible void __init start_kernel(void)
 	smp_setup_processor_id();
 	debug_objects_early_init();
 
-	/*
-	 * Set up the the initial canary ASAP:
-	 */
-	boot_init_stack_canary();
-
 	cgroup_init_early();
 
 	local_irq_disable();
@@ -503,6 +498,10 @@ asmlinkage __visible void __init start_kernel(void)
 	page_address_init();
 	pr_notice("%s", linux_banner);
 	setup_arch(&command_line);
+	/*
+	 * Set up the the initial canary ASAP:
+	 */
+	boot_init_stack_canary();
 	mm_init_cpumask(&init_mm);
 	setup_command_line(command_line);
 	setup_nr_cpu_ids();
