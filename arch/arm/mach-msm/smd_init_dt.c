966a81985f893a0ee73bc0998503297b19f8de85
Kumar Gala <galak@codeaurora.org>
2013-08-16 07:20:34 -0500

    msm: smd: fix in-tree compilation
    In-tree compilation for arch/arm/mach-msm/smd_init_dt.c was hitting this
    error:
    
    arch/arm/mach-msm/smd_init_dt.c:24:25: fatal error: smd_private.h: No such
    file or directory
     #include <smd_private.h>
    
    This was due to using <> instead of "" style include.
    
    Change-Id: I462d259410377244cd551db910df9e15521544ed
    Signed-off-by: Kumar Gala <galak@codeaurora.org>

--- arch/arm/mach-msm/smd_init_dt.c
+++ arch/arm/mach-msm/smd_init_dt.c
@@ -21,7 +21,7 @@
 #include <linux/io.h>
 
 #include <mach/msm_ipc_logging.h>
-#include <smd_private.h>
+#include "smd_private.h"
 
 #define MODULE_NAME "msm_smd"
 #define IPC_LOG(level, x...) do { \
