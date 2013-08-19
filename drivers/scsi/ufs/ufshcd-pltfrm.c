2ee0f9d95809901339a7045d16b40fe98376cfa1
Akinobu Mita <mita@fixstars.com>
2013-08-19 22:56:05 +0900

    ufs-pltfrm: initialize DMA mask for device-tree probed device
    The device-tree probed device for ARM doesn't have dev->dma_mask.
    So dma_set_mask() for the device doesn't succeed.  The popular trick
    for this is - dev->dma_mask = &dev->coherent_dma_mask;
    
    Currently there is no dma_set_mask() call in ufs-pltfrm, but the
    forthcoming fix needs proper DMA mask setting in ufs core driver.  So
    initializing dev->dma_mask as described above is required.
    
    Change-Id: I8428f106ca2d97badb7bf57cbb1d3ee14dddaf7f
    Signed-off-by: Akinobu Mita <mita@fixstars.com>
    Cc: Sujit Reddy Thumma <sthumma@codeaurora.org>
    Cc: Vinayak Holikatti <vinholikatti@gmail.com>
    Cc: Santosh Y <santoshsy@gmail.com>
    Cc: "James E.J. Bottomley" <JBottomley@parallels.com>
    Cc: linux-scsi@vger.kernel.org
    Patch-mainline: linux-scsi@vger.kernel.org @ 2013/08/19 19:26
    Signed-off-by: Sujit Reddy Thumma <sthumma@codeaurora.org>
    [cang@codeaurora.org: resolved trivial merge conflicts]
    Signed-off-by: Can Guo <cang@codeaurora.org>

--- drivers/scsi/ufs/ufshcd-pltfrm.c
+++ drivers/scsi/ufs/ufshcd-pltfrm.c
@@ -343,6 +343,9 @@ int ufshcd_pltfrm_init(struct platform_device *pdev,
 	pm_runtime_set_active(&pdev->dev);
 	pm_runtime_enable(&pdev->dev);
 
+	if (!dev->dma_mask)
+		dev->dma_mask = &dev->coherent_dma_mask;
+
 	ufshcd_init_lanes_per_dir(hba);
 
 	err = ufshcd_init(hba, mmio_base, irq);
