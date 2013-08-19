800bae7244e9f5c7e84a7c669d5d18cb8c6eb1fe
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2013-08-19 15:03:14 -0700

    msm: show_resume_irq: print the irq name
    Just printing the irq number is not informative. Update the code
    to show the name of the interrupt's irq action.
    
    Change-Id: Ia3e7d37948860b23d4390808769cd198656eba48
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- drivers/gpio/gpio-msm-common.c
+++ drivers/gpio/gpio-msm-common.c
@@ -373,9 +373,18 @@ void msm_gpio_show_resume_irq(void)
 	for_each_set_bit(i, msm_gpio.wake_irqs, ngpio) {
 		intstat = __msm_gpio_get_intr_status(i);
 		if (intstat) {
+			struct irq_desc *desc;
+			const char *name = "null";
+
 			irq = msm_gpio_to_irq(&msm_gpio.gpio_chip, i);
-			pr_warning("%s: %d triggered\n",
-				__func__, irq);
+			desc = irq_to_desc(irq);
+			if (desc == NULL)
+				name = "stray irq";
+			else if (desc->action && desc->action->name)
+				name = desc->action->name;
+
+			pr_warning("%s: %d triggered %s\n",
+					__func__, irq, name);
 		}
 	}
 	spin_unlock_irqrestore(&tlmm_lock, irq_flags);
