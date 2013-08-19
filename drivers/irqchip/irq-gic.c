6ec799f69656ee624b6be61205531cf0557f5b03
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2013-08-19 15:03:14 -0700

    msm: show_resume_irq: print the irq name
    Just printing the irq number is not informative. Update the code
    to show the name of the interrupt's irq action.
    
    Change-Id: Ia3e7d37948860b23d4390808769cd198656eba48
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- drivers/irqchip/irq-gic.c
+++ drivers/irqchip/irq-gic.c
@@ -238,8 +238,16 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 	for (i = find_first_bit(pending, gic->gic_irqs);
 	     i < gic->gic_irqs;
 	     i = find_next_bit(pending, gic->gic_irqs, i+1)) {
-		pr_warning("%s: %d triggered", __func__,
-					i + gic->irq_offset);
+		struct irq_desc *desc = irq_to_desc(i + gic->irq_offset);
+		const char *name = "null";
+
+		if (desc == NULL)
+			name = "stray irq";
+		else if (desc->action && desc->action->name)
+			name = desc->action->name;
+
+		pr_warning("%s: %d triggered %s\n", __func__,
+					i + gic->irq_offset, name);
 	}
 }
 
