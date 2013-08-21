abe2638945e4968ccd59cc1d6f340465c53e6f52
Prasad Sodagudi <psodagud@codeaurora.org>
2013-08-21 18:15:08 +0530

    platform/msm: qpnp-power-on: Implement cold_boot sysctl parameter
    Update the cold_boot sysctl parameter by using
    qpnp_pon_is_warm_reset API. cold_boot sysctrl
    is updated with 1 or 0 when system was booted from
    cold or warm boot states respectively.
    
    CRs-Fixed: 461256
    Change-Id: I4bb87602f76526b3e67a6466044e77fb1d823b29
    Signed-off-by: Prasad Sodagudi <psodagud@codeaurora.org>

--- drivers/platform/msm/qpnp-power-on.c
+++ drivers/platform/msm/qpnp-power-on.c
@@ -1111,8 +1111,11 @@ static int qpnp_pon_probe(struct spmi_device *spmi)
 	index = ffs(pon_sts);
 	if ((index > PON_REASON_MAX) || (index < 0))
 		index = 0;
-	pr_info("PMIC@SID%d Power-on reason: %s\n", pon->spmi->sid,
-			index ? qpnp_pon_reason[index - 1] : "Unknown");
+
+	cold_boot = !qpnp_pon_is_warm_reset();
+	pr_info("PMIC@SID%d Power-on reason: %s and '%s' boot\n",
+		pon->spmi->sid, index ? qpnp_pon_reason[index - 1] :
+		"Unknown", cold_boot ? "cold" : "warm");
 
 	/* program s3 debounce */
 	rc = of_property_read_u32(pon->spmi->dev.of_node,
