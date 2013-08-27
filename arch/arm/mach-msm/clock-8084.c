8eca4acb3794c04d0b8ddcfc6cb08ce04c7b97f2
Tianyi Gou <tgou@codeaurora.org>
2013-08-26 21:34:45 -0700

    msm: clock-8084: Add xo rate support for usb mock clock
    There is a use case of gcc_usb_hsic_mock_utmi_clk running at
    xo rate. Therefore, add this support.
    
    Change-Id: Icdb9394ce0df1b881cf6d7ea6af74b5e31faa06e
    Signed-off-by: Tianyi Gou <tgou@codeaurora.org>

--- arch/arm/mach-msm/clock-8084.c
+++ arch/arm/mach-msm/clock-8084.c
@@ -1901,6 +1901,7 @@ static struct rcg_clk usb_hsic_io_cal_clk_src = {
 };
 
 static struct clk_freq_tbl ftbl_gcc_usb_hsic_mock_utmi_clk[] = {
+	F( 19200000,      xo,      1,  0, 0),
 	F( 60000000,      gpll0,   10, 0, 0),
 	F_END
 };
