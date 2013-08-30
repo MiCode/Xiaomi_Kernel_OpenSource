fd9bf69cdeaa76fa950f7c1a6ed6fbf2f2283077
Xu Han <hanxu@codeaurora.org>
2013-08-30 14:29:01 -0700

    msm: camera: use 24MHz for camera sensor ext clk
    8974 V1 hardware was not supporting 24MHz mclk.
    adding support to use 24 Mhz clk from V2.
    
    Change-Id: I3b75fe81d127b755ac9a93e35fedd9fc670566d5
    Signed-off-by: Xu Han <hanxu@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
@@ -957,7 +957,7 @@ static struct msm_cam_clk_info cam_8610_clk_info[] = {
 };
 
 static struct msm_cam_clk_info cam_8974_clk_info[] = {
-	[SENSOR_CAM_MCLK] = {"cam_src_clk", 19200000},
+	[SENSOR_CAM_MCLK] = {"cam_src_clk", 24000000},
 	[SENSOR_CAM_CLK] = {"cam_clk", 0},
 };
 
