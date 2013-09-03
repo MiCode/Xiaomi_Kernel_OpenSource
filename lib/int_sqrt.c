44960f079d68741741f9e6c3e17c4b7e68ed6068
ramgear <ramgear@gmail.com>
2013-09-03 14:25:04 +0700

    int_sqrt: correction square root algo with naming
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- lib/int_sqrt.c
+++ lib/int_sqrt.c
@@ -16,23 +16,33 @@
  */
 unsigned long int_sqrt(unsigned long x)
 {
-	unsigned long b, m, y = 0;
+	unsigned long tmp;
+	unsigned long place;
+	unsigned long root;
+	unsigned long remainder;
 
 	if (x <= 1)
 		return x;
 
-	m = 1UL << (BITS_PER_LONG - 2);
-	while (m != 0) {
-		b = y + m;
-		y >>= 1;
+	root = 0;
+	remainder = x;
+	place = 1UL << (BITS_PER_LONG - 2);
+	
+	while (place > remainder)  
+		place >>= 2;
 
-		if (x >= b) {
-			x -= b;
-			y += m;
+	while (place != 0) {
+		tmp = root + place;
+
+		if (remainder >= tmp) 
+		{
+			remainder -= tmp;
+			root += (place << 1);
 		}
-		m >>= 2;
+		root >>= 1;
+		place >>= 2;
 	}
 
-	return y;
+	return root;
 }
 EXPORT_SYMBOL(int_sqrt);
