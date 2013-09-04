MotorolaMobilityLLC/kernel-msm

msm: video: vcap: Add video capture driver
commit 6a744e339817d68e2b371fa35b0040382b38d044
Author: Terence Hampson <thampson@codeaurora.org>
Date:   Fri Dec 7 15:50:32 2012 -0500

    media: video: Allow NR threshold to be updated by motion est

    VCAP did not allow for client to update threshold values based
    on the motion estimation. By enabling an option for this threshold
    to be updated dynamically by hardware, this allows for a more
    aggressive noise reduction to be applied at the request of the
    client.

    Change-Id: I0ac590ef826ce4567c048745efafbeb05011f31e
    Signed-off-by: Terence Hampson <thampson@codeaurora.org>

commit 06550627e1f08b3956e9bf190999cd14123acf9d
Author: Terence Hampson <thampson@codeaurora.org>
Date:   Fri Nov 30 14:35:16 2012 -0500

    media: video: Prevent memory leak in VCAP

    If a client were to do the following, VCAP would not free the ion
    handle:
     -Opened VCAP
     -Start initial configuration
     -Queues buffers
     -Close VCAP without ever calling stream on

    Since VCAP was not freeing ion this would cause a memory leak

    CRs-Fixed: 423881
    Change-Id: I594924d85884aa5e0a647bc52fa9e89e720f4ff6
    Signed-off-by: Terence Hampson <thampson@codeaurora.org>

commit 14600e5b94992cebb0cdc1b541293c8f6c952715
Author: Terence Hampson <thampson@codeaurora.org>
Date:   Wed Nov 7 17:16:27 2012 -0500

    media: video: vp_enabled state incorrect after a forced reset

    After a forced reset occurred on VP, during a stream_off call,
    vp_enabled state was incorrect causing one extra call to
    disable_irq. This would result in VP isr to be called whenever
    interrupt was triggered.

    CRs-Fixed: 418792
    Change-Id: Ia66b2d70506d4b06782bc5c25c83587c78f8e9c8
    Signed-off-by: Terence Hampson <thampson@codeaurora.org>

commit df1d4deafb7931eed7ae102440aea76647aab234
Author: Terence Hampson <thampson@codeaurora.org>
Date:   Mon Nov 5 17:06:02 2012 -0500

    media: video: Stride alignment configurable

include/media/vcap_fmt.h
@@ -1,0 +1,77 @@
+/* Copyright (c) 2012, Code Aurora Forum. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef VCAP_FMT_H
+#define VCAP_FMT_H
+
+#define V4L2_BUF_TYPE_INTERLACED_IN_DECODER (V4L2_BUF_TYPE_PRIVATE)
+#define V4L2_BUF_TYPE_VP_OUT (V4L2_BUF_TYPE_PRIVATE + 1)
+
+enum hal_vcap_mode {
+	HAL_VCAP_MODE_PRO = 0,
+	HAL_VCAP_MODE_INT,
+};
+
+enum hal_vcap_polar {
+	HAL_VCAP_POLAR_NEG = 0,
+	HAL_VCAP_POLAR_POS,
+};
+
+enum hal_vcap_color {
+	HAL_VCAP_YUV = 0,
+	HAL_VCAP_RGB,
+};
+
+struct v4l2_format_vc_ext {
+	enum hal_vcap_mode     mode;
+	enum hal_vcap_polar    h_polar;
+	enum hal_vcap_polar    v_polar;
+	enum hal_vcap_polar    d_polar;
+	enum hal_vcap_color    color_space;
+
+	float  clk_freq;
+	uint32_t vtotal;
+	uint32_t htotal;
+	uint32_t hactive_start;
+	uint32_t hactive_end;
+	uint32_t vactive_start;
+	uint32_t vactive_end;
+	uint32_t vsync_start;
+	uint32_t vsync_end;
+	uint32_t hsync_start;
+	uint32_t hsync_end;
+	uint32_t f2_vactive_start;
+	uint32_t f2_vactive_end;
+	uint32_t f2_vsync_h_start;
+	uint32_t f2_vsync_h_end;
+	uint32_t f2_vsync_v_start;
+	uint32_t f2_vsync_v_end;
+	uint32_t sizeimage;
+	uint32_t bytesperline;
+};
+
+enum vcap_type {
+	VC_TYPE,
+	VP_IN_TYPE,
+	VP_OUT_TYPE,
+};
+
+struct vcap_priv_fmt {
+	enum vcap_type type;
+	union {
+		struct v4l2_format_vc_ext timing;
+		struct v4l2_pix_format pix;
+		/* Once VP is created there will be another type in here */
+	} u;
+};
+#endif

MiCode/Xiaomi_Kernel_OpenSource

include/uapi/media/vcap_fmt.h
@@ -1,0 +1,77 @@
+/* Copyright (c) 2012, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __UAPI_VCAP_FMT_H
+#define __UAPI_VCAP_FMT_H
+
+#define V4L2_BUF_TYPE_INTERLACED_IN_DECODER (V4L2_BUF_TYPE_PRIVATE)
+#define V4L2_BUF_TYPE_VP_OUT (V4L2_BUF_TYPE_PRIVATE + 1)
+
+enum hal_vcap_mode {
+	HAL_VCAP_MODE_PRO = 0,
+	HAL_VCAP_MODE_INT,
+};
+
+enum hal_vcap_polar {
+	HAL_VCAP_POLAR_NEG = 0,
+	HAL_VCAP_POLAR_POS,
+};
+
+enum hal_vcap_color {
+	HAL_VCAP_YUV = 0,
+	HAL_VCAP_RGB,
+};
+
+struct v4l2_format_vc_ext {
+	enum hal_vcap_mode     mode;
+	enum hal_vcap_polar    h_polar;
+	enum hal_vcap_polar    v_polar;
+	enum hal_vcap_polar    d_polar;
+	enum hal_vcap_color    color_space;
+
+	float  clk_freq;
+	uint32_t vtotal;
+	uint32_t htotal;
+	uint32_t hactive_start;
+	uint32_t hactive_end;
+	uint32_t vactive_start;
+	uint32_t vactive_end;
+	uint32_t vsync_start;
+	uint32_t vsync_end;
+	uint32_t hsync_start;
+	uint32_t hsync_end;
+	uint32_t f2_vactive_start;
+	uint32_t f2_vactive_end;
+	uint32_t f2_vsync_h_start;
+	uint32_t f2_vsync_h_end;
+	uint32_t f2_vsync_v_start;
+	uint32_t f2_vsync_v_end;
+	uint32_t sizeimage;
+	uint32_t bytesperline;
+};
+
+enum vcap_type {
+	VC_TYPE,
+	VP_IN_TYPE,
+	VP_OUT_TYPE,
+};
+
+struct vcap_priv_fmt {
+	enum vcap_type type;
+	union {
+		struct v4l2_format_vc_ext timing;
+		struct v4l2_pix_format pix;
+		/* Once VP is created there will be another type in here */
+	} u;
+};
+#endif