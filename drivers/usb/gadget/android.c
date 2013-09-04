f4899eaeb0e0e2020171cc7db079f6b29dadafc6
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2013-09-04 17:30:43 +0530

    USB: android: Prevent change of host mac address of 'rndis0' interface
    On windows 7 platform, previously allocated ip address is maintained.
    However, Host MAC address of 'rndis0' interface is changed when the
    tethering driver re-enumerated. Thus, the tethering network driver
    can't be allocated ip address from dhcp. It causes connection delay
    between host and phone for usb tethering.
    
    This patch prevents from changing Host MAC address of 'rndis0' interface.
    In other words, this patch maintains the Host MAC address allocated by
    userspace although the driver is re-enumerated.
    
    CRs-Fixed: 534083
    Change-Id: I94176ec010fc2cf5c57c35ed31a311dd747200bd
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -1684,7 +1684,11 @@ rndis_function_bind_config(struct android_usb_function *f,
 		rndis->ethaddr[0], rndis->ethaddr[1], rndis->ethaddr[2],
 		rndis->ethaddr[3], rndis->ethaddr[4], rndis->ethaddr[5]);
 
-	dev = gether_setup_name(c->cdev->gadget, rndis->ethaddr, "rndis");
+	if (rndis->ethaddr[0])
+		dev = gether_setup_name(c->cdev->gadget, NULL, "rndis");
+	else
+		dev = gether_setup_name(c->cdev->gadget, rndis->ethaddr,
+								"rndis");
 	if (IS_ERR(dev)) {
 		ret = PTR_ERR(dev);
 		pr_err("%s: gether_setup failed\n", __func__);
