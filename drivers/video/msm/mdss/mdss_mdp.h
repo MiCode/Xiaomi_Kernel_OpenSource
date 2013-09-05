88e1b3a691d441067111fc02ae854daf076405a7
Sree Sesha Aravind Vadrevu <svadrevu@codeaurora.org>
2013-09-04 19:32:17 -0700

    msm: mdss: Program phase factors for QSEED in Dual display mode
    When scaling is involved during video playback for dual display
    high resolution panels, the scaling factors such as phase step and
    initial phase need to be programmed to avoid mismatched textures
    and hardware hangs. Also, software pixel extension needs to be
    enabled. The data is received from user module through OVERLAY_SET.
    This feature is only enabled for YUV formats in VIG pipes.
    
    Change-Id: Id6ed3fd3ee6af7383ef152400789decef7302db2
    Signed-off-by: Sree Sesha Aravind Vadrevu <svadrevu@codeaurora.org>
    
    Conflicts:
    
    	drivers/video/msm/mdss/mdss_mdp_pipe.c
    
    Signed-off-by: Pawan Kumar <pavaku@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp.h
+++ drivers/video/msm/mdss/mdss_mdp.h
@@ -345,9 +345,6 @@ struct mdss_mdp_pipe {
 	u8 vert_deci;
 	struct mdss_mdp_img_rect src;
 	struct mdss_mdp_img_rect dst;
-	u32 phase_step_x;
-	u32 phase_step_y;
-
 	struct mdss_mdp_format_params *src_fmt;
 	struct mdss_mdp_plane_sizes src_planes;
 
@@ -374,6 +371,9 @@ struct mdss_mdp_pipe {
 
 	struct mdp_overlay_pp_params pp_cfg;
 	struct mdss_pipe_pp_res pp_res;
+	struct mdp_scale_data scale;
+	u8 chroma_sample_h;
+	u8 chroma_sample_v;
 };
 
 struct mdss_mdp_writeback_arg {
@@ -633,6 +633,7 @@ int mdss_mdp_wb_set_format(struct msm_fb_data_type *mfd, int dst_format);
 int mdss_mdp_wb_get_format(struct msm_fb_data_type *mfd,
 					struct mdp_mixer_cfg *mixer_cfg);
 
+int mdss_mdp_pipe_program_pixel_extn(struct mdss_mdp_pipe *pipe);
 #define mfd_to_mdp5_data(mfd) (mfd->mdp.private1)
 #define mfd_to_mdata(mfd) (((struct mdss_overlay_private *)\
 				(mfd->mdp.private1))->mdata)
