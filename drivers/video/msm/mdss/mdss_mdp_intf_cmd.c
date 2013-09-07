ce685953575b5a79f68ec89bd08f1d03a7406003
Jeykumar Sankaran <jsanka@codeaurora.org>
2013-09-07 00:58:43 -0700

    mdss: mdp: Add support for partial panel updates
    This change programs MDP only for the output resolution (ROI)
    set by the client. All staged pipes are cropped to fetch data
    only for the resolution programmed.
    
    Change-Id: I1e113fd051a01d8fc1247a74d363a26cd9a0cd61
    Signed-off-by: Jeykumar Sankaran <jsanka@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
@@ -431,16 +431,15 @@ static int mdss_mdp_cmd_wait4pingpong(struct mdss_mdp_ctl *ctl, void *arg)
 	return rc;
 }
 
-static int mdss_mdp_cmd_set_partial_roi(struct mdss_mdp_ctl *ctl,
-		struct mdss_mdp_img_rect *roi)
+static int mdss_mdp_cmd_set_partial_roi(struct mdss_mdp_ctl *ctl)
 {
 	int rc = 0;
-	if (roi->w && roi->h &&
+	if (ctl->roi.w && ctl->roi.h && ctl->roi_changed &&
 			ctl->panel_data->panel_info.partial_update_enabled) {
-		ctl->panel_data->panel_info.roi_x = roi->x;
-		ctl->panel_data->panel_info.roi_y = roi->y;
-		ctl->panel_data->panel_info.roi_w = roi->w;
-		ctl->panel_data->panel_info.roi_h = roi->h;
+		ctl->panel_data->panel_info.roi_x = ctl->roi.x;
+		ctl->panel_data->panel_info.roi_y = ctl->roi.y;
+		ctl->panel_data->panel_info.roi_w = ctl->roi.w;
+		ctl->panel_data->panel_info.roi_h = ctl->roi.h;
 
 		rc = mdss_mdp_ctl_intf_event(ctl,
 				MDSS_EVENT_ENABLE_PARTIAL_UPDATE, NULL);
@@ -452,7 +451,6 @@ int mdss_mdp_cmd_kickoff(struct mdss_mdp_ctl *ctl, void *arg)
 {
 	struct mdss_mdp_cmd_ctx *ctx;
 	unsigned long flags;
-	struct mdss_mdp_img_rect roi;
 	int rc;
 
 	ctx = (struct mdss_mdp_cmd_ctx *) ctl->priv_data;
@@ -471,8 +469,7 @@ int mdss_mdp_cmd_kickoff(struct mdss_mdp_ctl *ctl, void *arg)
 		WARN(rc, "intf %d panel on error (%d)\n", ctl->intf_num, rc);
 	}
 
-	roi = (struct mdss_mdp_img_rect){0, 0, ctl->width, ctl->height};
-	mdss_mdp_cmd_set_partial_roi(ctl, &roi);
+	mdss_mdp_cmd_set_partial_roi(ctl);
 
 	/*
 	 * tx dcs command if had any
