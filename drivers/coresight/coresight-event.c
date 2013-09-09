2019121d55e0b23f2862148638d845f01dc1d631
Sarang Joshi <spjoshi@codeaurora.org>
2013-09-09 10:37:21 -0700

    coresight: make coresight event module configurable
    The coresight event module helps abort tracing on user fault
    and undefined instruction exceptions. Add module parameter to
    control it dynamically.
    
    Change-Id: Iecaa144b99e3786c8c8f570f989f747be12c4fdc
    Signed-off-by: Sarang Joshi <spjoshi@codeaurora.org>

--- drivers/coresight/coresight-event.c
+++ drivers/coresight/coresight-event.c
@@ -20,7 +20,12 @@
 
 #include <trace/events/exception.h>
 
-static void event_trace_user_fault(void *ignore,
+static int event_abort_enable;
+static int event_abort_set(const char *val, struct kernel_param *kp);
+module_param_call(event_abort_enable, event_abort_set, param_get_int,
+		  &event_abort_enable, 0644);
+
+static void event_abort_user_fault(void *ignore,
 				   struct task_struct *task,
 				   unsigned long addr,
 				   unsigned int fsr)
@@ -30,7 +35,7 @@ static void event_trace_user_fault(void *ignore,
 		(char *)task->comm, addr, fsr);
 }
 
-static void event_trace_undef_instr(void *ignore,
+static void event_abort_undef_instr(void *ignore,
 				    struct pt_regs *regs,
 				    void *pc)
 {
@@ -40,28 +45,57 @@ static void event_trace_undef_instr(void *ignore,
 	}
 }
 
+static int event_abort_register(void)
+{
+	int ret;
+
+	ret = register_trace_user_fault(event_abort_user_fault, NULL);
+	if (ret)
+		goto err_usr_fault;
+	ret = register_trace_undef_instr(event_abort_undef_instr, NULL);
+	if (ret)
+		goto err_undef_instr;
+
+	return 0;
+
+err_undef_instr:
+	unregister_trace_user_fault(event_abort_user_fault, NULL);
+err_usr_fault:
+	return ret;
+}
+
+static void event_abort_unregister(void)
+{
+	unregister_trace_user_fault(event_abort_user_fault, NULL);
+	unregister_trace_undef_instr(event_abort_undef_instr, NULL);
+}
+
+static int event_abort_set(const char *val, struct kernel_param *kp)
+{
+	int ret;
+
+	ret = param_set_int(val, kp);
+	if (ret) {
+		pr_err("coresight_event: error setting value %d\n", ret);
+		return ret;
+	}
+
+	if (event_abort_enable)
+		ret = event_abort_register();
+	else
+		event_abort_unregister();
+
+	return ret;
+}
+
 static int __init event_init(void)
 {
-	int ret_user_fault, ret_undef_instr;
-	ret_user_fault = register_trace_user_fault(
-				event_trace_user_fault, NULL);
-	ret_undef_instr = register_trace_undef_instr(
-				event_trace_undef_instr, NULL);
-	if (ret_user_fault != 0 || ret_undef_instr != 0) {
-		pr_info("coresight_event: Module Not Registered\n");
-		return (ret_user_fault < 0 ?\
-			ret_user_fault : ret_undef_instr);
-	}
-	pr_info("coresight_event: Module Initialized\n");
 	return 0;
 }
 module_init(event_init);
 
 static void __exit event_exit(void)
 {
-	unregister_trace_user_fault(event_trace_user_fault, NULL);
-	unregister_trace_undef_instr(event_trace_undef_instr, NULL);
-	pr_info("coresight_event: Module Removed\n");
 }
 module_exit(event_exit);
 
