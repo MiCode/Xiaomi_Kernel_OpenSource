e17597ed0d1139d9b06c8b8ca738dbf88aa03dce
Siddartha Mohanadoss <smohanad@codeaurora.org>
2013-09-11 17:46:54 -0700

    hwmon: qpnp-adc-current: Add temperature compensation
    Add temperature compensation parameters for PM8026 2.1.
    Compensate the vbatt voltage for temperature variance above
    25degC. Add similar compensation for current compensation.
    
    Also update using the revid api's to check for PMIC revision
    instead of using peripheral revision registers. Using
    revid PMIC api's makes it easy to follow on what PMIC versions
    are the temperature compensation being applied to. Depending
    on VADC/IADC revision registers alone may not suffice since
    there is no correlation between the individual peripheral
    revision and the PMIC number. You could have the same
    VADC revision on different PMIC's and to avoid such a scenario
    use the PMIC revid.
    
    Change-Id: I90adb1130b2e2461f762df774ddcd018e806cf78
    Signed-off-by: Siddartha Mohanadoss <smohanad@codeaurora.org>

--- drivers/hwmon/qpnp-adc-voltage.c
+++ drivers/hwmon/qpnp-adc-voltage.c
@@ -512,36 +512,14 @@ static int32_t qpnp_vadc_version_check(struct qpnp_vadc_chip *dev)
 #define QPNP_VBAT_COEFF_14	22220000
 #define QPNP_VBAT_COEFF_15	83060000
 
-#define QPNP_VADC_REV_ID_8941_3_1	1
-#define QPNP_VADC_REV_ID_8026_1_0	2
-#define QPNP_VADC_REV_ID_8026_2_0	3
-
-static void qpnp_temp_comp_version_check(struct qpnp_vadc_chip *vadc,
-							int32_t *version)
-{
-	if (vadc->revision_dig_major == 3 &&
-			vadc->revision_ana_minor == 2)
-		*version = QPNP_VADC_REV_ID_8941_3_1;
-	else if (vadc->revision_dig_major == 1 &&
-			vadc->revision_ana_minor == 2)
-		*version = QPNP_VADC_REV_ID_8026_1_0;
-	else if (vadc->revision_dig_major == 2 &&
-			vadc->revision_ana_minor == 2)
-		*version = QPNP_VADC_REV_ID_8026_2_0;
-	else
-		*version = -EINVAL;
-
-	return;
-}
-
 static int32_t qpnp_ocv_comp(int64_t *result,
 			struct qpnp_vadc_chip *vadc, int64_t die_temp)
 {
 	int64_t temp_var = 0;
 	int64_t old = *result;
-	int32_t version;
+	int version;
 
-	qpnp_temp_comp_version_check(vadc, &version);
+	version = qpnp_adc_get_revid_version(vadc->dev);
 	if (version == -EINVAL)
 		return 0;
 
@@ -552,7 +530,7 @@ static int32_t qpnp_ocv_comp(int64_t *result,
 		die_temp = 60000;
 
 	switch (version) {
-	case QPNP_VADC_REV_ID_8941_3_1:
+	case QPNP_REV_ID_8941_3_1:
 		switch (vadc->id) {
 		case COMP_ID_TSMC:
 			temp_var = (((die_temp *
@@ -567,7 +545,7 @@ static int32_t qpnp_ocv_comp(int64_t *result,
 			break;
 		}
 		break;
-	case QPNP_VADC_REV_ID_8026_1_0:
+	case QPNP_REV_ID_8026_1_0:
 		switch (vadc->id) {
 		case COMP_ID_TSMC:
 			temp_var = (((die_temp *
@@ -582,7 +560,8 @@ static int32_t qpnp_ocv_comp(int64_t *result,
 			break;
 		}
 		break;
-	case QPNP_VADC_REV_ID_8026_2_0:
+	case QPNP_REV_ID_8026_2_0:
+	case QPNP_REV_ID_8026_2_1:
 		switch (vadc->id) {
 		case COMP_ID_TSMC:
 			temp_var = ((die_temp - 2500) *
@@ -617,9 +596,9 @@ static int32_t qpnp_vbat_sns_comp(int64_t *result,
 {
 	int64_t temp_var = 0;
 	int64_t old = *result;
-	int32_t version;
+	int version;
 
-	qpnp_temp_comp_version_check(vadc, &version);
+	version = qpnp_adc_get_revid_version(vadc->dev);
 	if (version == -EINVAL)
 		return 0;
 
@@ -631,7 +610,7 @@ static int32_t qpnp_vbat_sns_comp(int64_t *result,
 		die_temp = 60000;
 
 	switch (version) {
-	case QPNP_VADC_REV_ID_8941_3_1:
+	case QPNP_REV_ID_8941_3_1:
 		switch (vadc->id) {
 		case COMP_ID_TSMC:
 			temp_var = (die_temp *
@@ -645,7 +624,7 @@ static int32_t qpnp_vbat_sns_comp(int64_t *result,
 			break;
 		}
 		break;
-	case QPNP_VADC_REV_ID_8026_1_0:
+	case QPNP_REV_ID_8026_1_0:
 		switch (vadc->id) {
 		case COMP_ID_TSMC:
 			temp_var = (((die_temp *
@@ -660,7 +639,8 @@ static int32_t qpnp_vbat_sns_comp(int64_t *result,
 			break;
 		}
 		break;
-	case QPNP_VADC_REV_ID_8026_2_0:
+	case QPNP_REV_ID_8026_2_0:
+	case QPNP_REV_ID_8026_2_1:
 		switch (vadc->id) {
 		case COMP_ID_TSMC:
 			temp_var = ((die_temp - 2500) *
