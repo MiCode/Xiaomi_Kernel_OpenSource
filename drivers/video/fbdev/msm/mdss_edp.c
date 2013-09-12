8f1201e713bcf38f7db2d46d62b0594d7dfccdfa
Kuogee Hsieh <khsieh@codeaurora.org>
2013-09-11 18:03:32 -0700

    mdss: display: add edp frame rate
    Add edp frame rate so that run time bus bandwidth requirement
    can be calculated correctly.
    
    Change-Id: I6ac147f35dd6d35aec9799d8f9e5c96e149d8161
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_edp.c
+++ drivers/video/fbdev/msm/mdss_edp.c
@@ -722,6 +722,8 @@ static int mdss_edp_device_register(struct mdss_edp_drv_pdata *edp_drv)
 	mdss_edp_edid2pinfo(edp_drv);
 	edp_drv->panel_data.panel_info.bl_min = 1;
 	edp_drv->panel_data.panel_info.bl_max = 255;
+	edp_drv->panel_data.panel_info.edp.frame_rate =
+				DEFAULT_FRAME_RATE;/* 60 fps */
 
 	edp_drv->panel_data.event_handler = mdss_edp_event_handler;
 	edp_drv->panel_data.set_backlight = mdss_edp_set_backlight;
--- drivers/video/fbdev/msm/mdss_panel.h
+++ drivers/video/fbdev/msm/mdss_panel.h
@@ -216,6 +216,10 @@ struct mipi_panel_info {
 	char hw_vsync_mode;
 };
 
+struct edp_panel_info {
+	char frame_rate;	/* fps */
+};
+
 enum dynamic_fps_update {
 	DFPS_SUSPEND_RESUME_MODE,
 	DFPS_IMMEDIATE_CLK_UPDATE_MODE,
@@ -301,6 +305,7 @@ struct mdss_panel_info {
 	struct fbc_panel_info fbc;
 	struct mipi_panel_info mipi;
 	struct lvds_panel_info lvds;
+	struct edp_panel_info edp;
 };
 
 struct mdss_panel_data {
@@ -340,6 +345,9 @@ static inline u32 mdss_panel_get_framerate(struct mdss_panel_info *panel_info)
 	case MIPI_VIDEO_PANEL:
 	case MIPI_CMD_PANEL:
 		frame_rate = panel_info->mipi.frame_rate;
+		break;
+	case EDP_PANEL:
+		frame_rate = panel_info->edp.frame_rate;
 		break;
 	case WRITEBACK_PANEL:
 		frame_rate = DEFAULT_FRAME_RATE;
