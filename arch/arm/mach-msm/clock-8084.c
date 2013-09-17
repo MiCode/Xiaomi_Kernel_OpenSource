82c4b6f726f5a8f22ab62090941b80a0c5122c34
Tianyi Gou <tgou@codeaurora.org>
2013-09-16 17:05:13 -0700

    msm: clock-8084: Add parent fields for CE clocks
    The sources for CE clocks need to be captured in the CE clock
    structs to keep the RCG state always correct.
    
    Change-Id: I81a1c35b547950da984b60bb6fae3afc2e4d16df
    Signed-off-by: Tianyi Gou <tgou@codeaurora.org>

--- arch/arm/mach-msm/clock-8084.c
+++ arch/arm/mach-msm/clock-8084.c
@@ -2464,6 +2464,7 @@ static struct local_vote_clk gcc_ce1_clk = {
 	.en_mask = BIT(5),
 	.base = &virt_bases[GCC_BASE],
 	.c = {
+		.parent = &ce1_clk_src.c,
 		.dbg_name = "gcc_ce1_clk",
 		.ops = &clk_ops_vote,
 		CLK_INIT(gcc_ce1_clk.c),
@@ -2500,6 +2501,7 @@ static struct local_vote_clk gcc_ce2_clk = {
 	.en_mask = BIT(2),
 	.base = &virt_bases[GCC_BASE],
 	.c = {
+		.parent = &ce2_clk_src.c,
 		.dbg_name = "gcc_ce2_clk",
 		.ops = &clk_ops_vote,
 		CLK_INIT(gcc_ce2_clk.c),
@@ -2536,6 +2538,7 @@ static struct local_vote_clk gcc_ce3_clk = {
 	.en_mask = BIT(30),
 	.base = &virt_bases[GCC_BASE],
 	.c = {
+		.parent = &ce3_clk_src.c,
 		.dbg_name = "gcc_ce3_clk",
 		.ops = &clk_ops_vote,
 		CLK_INIT(gcc_ce3_clk.c),
@@ -5776,14 +5779,17 @@ static struct clk_lookup apq_clocks_8084[] = {
 	/* CE clocks */
 	CLK_LOOKUP("",	gcc_ce1_ahb_clk.c,	""),
 	CLK_LOOKUP("",	gcc_ce1_axi_clk.c,	""),
+	CLK_LOOKUP("",	ce1_clk_src.c,	""),
 	CLK_LOOKUP("",	gcc_ce1_clk.c,	""),
 
 	CLK_LOOKUP("",	gcc_ce2_ahb_clk.c,	""),
 	CLK_LOOKUP("",	gcc_ce2_axi_clk.c,	""),
+	CLK_LOOKUP("",	ce2_clk_src.c,	""),
 	CLK_LOOKUP("",	gcc_ce2_clk.c,	""),
 
 	CLK_LOOKUP("",	gcc_ce3_ahb_clk.c,	""),
 	CLK_LOOKUP("",	gcc_ce3_axi_clk.c,	""),
+	CLK_LOOKUP("",	ce3_clk_src.c,	""),
 	CLK_LOOKUP("",	gcc_ce3_clk.c,	""),
 
 	CLK_LOOKUP("",	gcc_copss_smmu_ahb_clk.c,	""),
