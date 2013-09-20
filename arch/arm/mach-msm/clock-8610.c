594b011c3e8d7f0bf42deb35e96dd79c8c5c86ff
Aditya Jonnalagadda <ajonnala@codeaurora.org>
2013-09-20 19:59:41 +0530

    msm: camera: Add clock setting for imx134
    Add clock config for imx134. It is 8MP rear sensor
    with 4 lane MIPI.
    
    Change-Id: Ia7e0432deafd353e715c3f13d8a0732bcd2323a5
    Signed-off-by: Aditya Jonnalagadda <ajonnala@codeaurora.org>

--- arch/arm/mach-msm/clock-8610.c
+++ arch/arm/mach-msm/clock-8610.c
@@ -3024,12 +3024,14 @@ static struct clk_lookup msm_clocks_8610[] = {
 
 	/* MM sensor clocks */
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "6-006f"),
+	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "6-0034"),
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "6-007d"),
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "6-006d"),
 	CLK_LOOKUP("cam_src_clk", mclk1_clk_src.c, "6-0078"),
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "6-0020"),
 	CLK_LOOKUP("cam_src_clk", mclk0_clk_src.c, "6-006a"),
 	CLK_LOOKUP("cam_clk", mclk0_clk.c, "6-006f"),
+	CLK_LOOKUP("cam_clk", mclk0_clk.c, "6-0034"),
 	CLK_LOOKUP("cam_clk", mclk0_clk.c, "6-007d"),
 	CLK_LOOKUP("cam_clk", mclk0_clk.c, "6-006d"),
 	CLK_LOOKUP("cam_clk", mclk1_clk.c, "6-0078"),
