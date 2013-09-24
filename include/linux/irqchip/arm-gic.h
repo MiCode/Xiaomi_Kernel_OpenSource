1f4bb668c080f9d99c83f53e7b8f1c134c12d4d7
Abhimanyu Kapur <abhimany@codeaurora.org>
2013-09-23 17:23:09 -0700

    Revert "ARM: gic: Remove parameters from msm_gic_save"
    This reverts commit 7c2c583f9356cfd1563ed6b907f479483bb9a643.
    Dropping support for MSM8625.
    
    Change-Id: I814f5ded6e8f59e87ddb9f716e821e99fa5d4bed
    Signed-off-by: Abhimanyu Kapur <abhimany@codeaurora.org>

--- include/linux/irqchip/arm-gic.h
+++ include/linux/irqchip/arm-gic.h
@@ -79,7 +79,7 @@ static inline void gic_init(unsigned int nr, int start,
 	gic_init_bases(nr, start, dist, cpu, 0, NULL);
 }
 
-void msm_gic_save(void);
+void msm_gic_save(bool modem_wake, int from_idle);
 void msm_gic_restore(void);
 void gic_configure_and_raise(unsigned int irq, unsigned int cpu);
 #endif
