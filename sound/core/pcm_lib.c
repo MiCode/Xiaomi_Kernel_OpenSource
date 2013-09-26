154f0730b34199d428d14ee700a6a5b4633cb83c
Phani Kumar Uppalapati <phaniu@codeaurora.org>
2013-09-26 12:27:56 -0700

    ALSA: pcm: check for integer overflow during multiplication
    Channel info data structure is parsed from userspace and if
    user does not set the no. of channels correctly, it could lead
    to security vulnerability if we don't check for overflow when the
    no. of channels is multiplied with pcm bit width. Add a condition
    to check for overflow during multiplication.
    
    CRs-fixed: 537818
    Change-Id: Ib9631b6e45d77b39be2c27343d4aee3e9244d8cc
    Signed-off-by: Phani Kumar Uppalapati <phaniu@codeaurora.org>

--- sound/core/pcm_lib.c
+++ sound/core/pcm_lib.c
@@ -1761,6 +1761,11 @@ static int snd_pcm_lib_ioctl_channel_info(struct snd_pcm_substream *substream,
 	switch (runtime->access) {
 	case SNDRV_PCM_ACCESS_MMAP_INTERLEAVED:
 	case SNDRV_PCM_ACCESS_RW_INTERLEAVED:
+		if ((UINT_MAX/width) < info->channel) {
+			snd_printd("%s: integer overflow while multiply\n",
+				   __func__);
+			return -EINVAL;
+		}
 		info->first = info->channel * width;
 		info->step = runtime->channels * width;
 		break;
@@ -1768,6 +1773,12 @@ static int snd_pcm_lib_ioctl_channel_info(struct snd_pcm_substream *substream,
 	case SNDRV_PCM_ACCESS_RW_NONINTERLEAVED:
 	{
 		size_t size = runtime->dma_bytes / runtime->channels;
+
+		if ((size > 0) && ((UINT_MAX/(size * 8)) < info->channel)) {
+			snd_printd("%s: integer overflow while multiply\n",
+				   __func__);
+			return -EINVAL;
+		}
 		info->first = info->channel * size * 8;
 		info->step = width;
 		break;
