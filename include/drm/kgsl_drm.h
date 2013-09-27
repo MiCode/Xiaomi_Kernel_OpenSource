00d104aa21c1056c4b55021efc52a05605b6e270
Deepak Kumar <dkumar@codeaurora.org>
2013-09-27 14:42:28 +0530

    msm: kgsl: Clean-up KGSL DRM ioctls
    Remove DRM_IOCTL_KGSL_GEM_LOCK_HANDLE,
    DRM_IOCTL_KGSL_GEM_UNLOCK_HANDLE and
    DRM_IOCTL_KGSL_GEM_UNLOCK_ON_TS ioctls from
    KGSL DRM interface as these ioctls are unused.
    
    CRs-fixed: 546856
    Change-Id: I91711c3079e964993bb5f47ab62fe5133630ab88
    Signed-off-by: Deepak Kumar <dkumar@codeaurora.org>

--- include/drm/kgsl_drm.h
+++ include/drm/kgsl_drm.h
@@ -15,9 +15,10 @@
 #define DRM_KGSL_GEM_GET_BUFINFO 0x08
 #define DRM_KGSL_GEM_SET_BUFCOUNT 0x09
 #define DRM_KGSL_GEM_SET_ACTIVE 0x0A
-#define DRM_KGSL_GEM_LOCK_HANDLE 0x0B
-#define DRM_KGSL_GEM_UNLOCK_HANDLE 0x0C
-#define DRM_KGSL_GEM_UNLOCK_ON_TS 0x0D
+/*
+ * Do not use ioctl code 0x0B, 0x0C and 0x0D
+ * to maintain backward compatibility
+ */
 #define DRM_KGSL_GEM_CREATE_FD 0x0E
 #define DRM_KGSL_GEM_GET_ION_FD 0x0F
 #define DRM_KGSL_GEM_CREATE_FROM_ION 0x10
@@ -27,7 +28,6 @@
 #define DRM_KGSL_GEM_SET_USERDATA 0x14
 #define DRM_KGSL_GEM_GET_USERDATA 0x15
 #define DRM_KGSL_GEM_CACHE_OPS 0x16
-
 
 
 #define DRM_IOCTL_KGSL_GEM_CREATE \
@@ -72,18 +72,6 @@ DRM_IOWR(DRM_COMMAND_BASE + DRM_KGSL_GEM_GET_BUFCOUNT, \
 #define DRM_IOCTL_KGSL_GEM_SET_ACTIVE \
 DRM_IOWR(DRM_COMMAND_BASE + DRM_KGSL_GEM_SET_ACTIVE, \
 	 struct drm_kgsl_gem_active)
-
-#define DRM_IOCTL_KGSL_GEM_LOCK_HANDLE \
-DRM_IOWR(DRM_COMMAND_BASE + DRM_KGSL_GEM_LOCK_HANDLE, \
-struct drm_kgsl_gem_lock_handles)
-
-#define DRM_IOCTL_KGSL_GEM_UNLOCK_HANDLE \
-DRM_IOWR(DRM_COMMAND_BASE + DRM_KGSL_GEM_UNLOCK_HANDLE, \
-struct drm_kgsl_gem_unlock_handles)
-
-#define DRM_IOCTL_KGSL_GEM_UNLOCK_ON_TS \
-DRM_IOWR(DRM_COMMAND_BASE + DRM_KGSL_GEM_UNLOCK_ON_TS, \
-struct drm_kgsl_gem_unlock_on_ts)
 
 #define DRM_IOCTL_KGSL_GEM_CREATE_FD \
 DRM_IOWR(DRM_COMMAND_BASE + DRM_KGSL_GEM_CREATE_FD, \
@@ -159,11 +147,6 @@ struct drm_kgsl_gem_cache_ops)
 #define DRM_KGSL_GEM_TYPE_FD_FBMEM   0x1000000
 #define DRM_KGSL_GEM_TYPE_FD_MASK    0xF000000
 
-/* Timestamp types */
-#define DRM_KGSL_GEM_TS_3D         0x00000430
-#define DRM_KGSL_GEM_TS_2D         0x00000180
-
-
 struct drm_kgsl_gem_create {
 	uint32_t size;
 	uint32_t handle;
@@ -218,23 +201,6 @@ struct drm_kgsl_gem_bufcount {
 struct drm_kgsl_gem_active {
 	uint32_t handle;
 	uint32_t active;
-};
-
-struct drm_kgsl_gem_lock_handles {
-	uint32_t num_handles;
-	uint32_t *handle_list;
-	uint32_t pid;
-	uint32_t lock_id;	  /* Returned lock id used for unlocking */
-};
-
-struct drm_kgsl_gem_unlock_handles {
-	uint32_t lock_id;
-};
-
-struct drm_kgsl_gem_unlock_on_ts {
-	uint32_t lock_id;
-	uint32_t timestamp;	 /* This field is a hw generated ts */
-	uint32_t type;		 /* Which pipe to check for ts generation */
 };
 
 struct drm_kgsl_gem_create_fd {
