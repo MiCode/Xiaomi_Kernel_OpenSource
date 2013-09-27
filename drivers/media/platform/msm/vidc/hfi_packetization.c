f8330101e030c0c0370c1220ed93c9d29e0b117c
Jayasena Sangaraboina <jsanga@codeaurora.org>
2013-09-26 17:58:03 -0700

    msm: vidc: Add AFD and CC metadata support to extradata
    Add support and control for setting Active format
    description and closed caption meta data in the
    extradata. FW parses metadata and adds it to the
    extradata. Client can use control to parse extradata
    for the metadata information.
    
    Change-Id: I79fb71e635927c95e0792862c9dea7d96f58e895
    Signed-off-by: Jayasena Sangaraboina <jsanga@codeaurora.org>

--- drivers/media/platform/msm/vidc/hfi_packetization.c
+++ drivers/media/platform/msm/vidc/hfi_packetization.c
@@ -361,12 +361,6 @@ static int get_hfi_extradata_index(enum hal_extradata_id index)
 	case HAL_EXTRADATA_RECOVERY_POINT_SEI:
 		ret = HFI_PROPERTY_PARAM_VDEC_RECOVERY_POINT_SEI_EXTRADATA;
 		break;
-	case HAL_EXTRADATA_CLOSED_CAPTION_UD:
-		ret = HFI_PROPERTY_PARAM_VDEC_CLOSED_CAPTION_EXTRADATA;
-		break;
-	case HAL_EXTRADATA_AFD_UD:
-		ret = HFI_PROPERTY_PARAM_VDEC_AFD_EXTRADATA;
-		break;
 	case HAL_EXTRADATA_MULTISLICE_INFO:
 		ret = HFI_PROPERTY_PARAM_VENC_MULTI_SLICE_INFO;
 		break;
@@ -378,6 +372,9 @@ static int get_hfi_extradata_index(enum hal_extradata_id index)
 		break;
 	case HAL_EXTRADATA_MPEG2_SEQDISP:
 		ret = HFI_PROPERTY_PARAM_VDEC_MPEG2_SEQDISP_EXTRADATA;
+		break;
+	case HAL_EXTRADATA_STREAM_USERDATA:
+		ret = HFI_PROPERTY_PARAM_VDEC_STREAM_USERDATA_EXTRADATA;
 		break;
 	default:
 		dprintk(VIDC_WARN, "Extradata index not found: %d\n", index);
