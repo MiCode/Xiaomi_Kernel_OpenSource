9a36225c973455731cd70043b9ef00bc45d2680f
Hemant Kumar <hemantk@codeaurora.org>
2013-09-29 15:31:06 -0700

    net: usb: Add support for ETH_P_MAP as skb protocol type
    This allows upper layer to handle muxing and de-muxing of skb
    supporting MAP protocol.
    
    Change-Id: If1a64613917a42f303c71ce8ae64f6cdb77dfd35
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- drivers/net/usb/rmnet_usb_data.c
+++ drivers/net/usb/rmnet_usb_data.c
@@ -396,8 +396,11 @@ static __be16 rmnet_ip_type_trans(struct sk_buff *skb,
 		protocol = htons(ETH_P_IPV6);
 		break;
 	default:
-		pr_err("[%s] rmnet_recv() L3 protocol decode error: 0x%02x",
-		       dev->name, skb->data[0] & 0xf0);
+		/*
+		 * There is no good way to determine if a packet has
+		 * a MAP header. For now default to MAP protocol
+		 */
+		protocol = htons(ETH_P_MAP);
 	}
 
 	return protocol;
