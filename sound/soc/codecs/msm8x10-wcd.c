faf042967058d22a88e40fd536d66c6633afc28e
Simmi Pateriya <simmip@codeaurora.org>
2013-10-01 17:05:44 +0530

    ASoC: wcd9xxx: Register callback for external micbias
    Even if external micbias is used for recording, callback
    needs to be registered to setup for recording. Register
    same callback as used with internal micbias.
    
    CRs-Fixed: 550513
    Change-Id: If7f606f265d46aa20a29fc97afd2468e7fe97379
    Signed-off-by: Simmi Pateriya <simmip@codeaurora.org>

--- sound/soc/codecs/msm8x10-wcd.c
+++ sound/soc/codecs/msm8x10-wcd.c
@@ -2424,6 +2424,11 @@ static const struct snd_soc_dapm_widget msm8x10_wcd_dapm_widgets[] = {
 		MSM8X10_WCD_A_MICB_1_CTL, 7, 0,
 		msm8x10_wcd_codec_enable_micbias, SND_SOC_DAPM_PRE_PMU |
 		SND_SOC_DAPM_POST_PMU | SND_SOC_DAPM_POST_PMD),
+	SND_SOC_DAPM_MICBIAS_E("MIC BIAS External",
+		MSM8X10_WCD_A_MICB_1_CTL, 7, 0,
+		msm8x10_wcd_codec_enable_micbias, SND_SOC_DAPM_PRE_PMU |
+		SND_SOC_DAPM_POST_PMU | SND_SOC_DAPM_POST_PMD),
+
 	SND_SOC_DAPM_ADC_E("ADC1", NULL, MSM8X10_WCD_A_TX_1_EN, 7, 0,
 		msm8x10_wcd_codec_enable_adc, SND_SOC_DAPM_PRE_PMU |
 		SND_SOC_DAPM_POST_PMU | SND_SOC_DAPM_POST_PMD),
@@ -2438,9 +2443,6 @@ static const struct snd_soc_dapm_widget msm8x10_wcd_dapm_widgets[] = {
 
 	SND_SOC_DAPM_MUX("ADC2 MUX", SND_SOC_NOPM, 0, 0,
 		&tx_adc2_mux),
-
-	SND_SOC_DAPM_MICBIAS("MIC BIAS External", MSM8X10_WCD_A_MICB_1_CTL,
-			     7, 0),
 
 	SND_SOC_DAPM_INPUT("AMIC3"),
 
