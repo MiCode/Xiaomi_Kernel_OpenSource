1dba2a2cd10499495cdb08609c676863218479c7
Siddartha Mohanadoss <smohanad@codeaurora.org>
2013-10-03 22:45:31 -0700

    msm: board-krypton: Add TSENS init
    TSENS provides an interface to read the temperature.
    Add early initialization to allow kernel thermal
    driver to read the temperature at initialization.
    
    Change-Id: Ifb2bb5fbd170c2fb1a10ff5209552093a231e930
    Signed-off-by: Siddartha Mohanadoss <smohanad@codeaurora.org>

--- arch/arm/mach-msm/board-krypton.c
+++ arch/arm/mach-msm/board-krypton.c
@@ -16,6 +16,7 @@
 #include <linux/of_address.h>
 #include <linux/of_platform.h>
 #include <linux/memory.h>
+#include <linux/msm_tsens.h>
 #include <asm/mach/map.h>
 #include <asm/mach/arch.h>
 #include <mach/board.h>
@@ -66,6 +67,7 @@ void __init msmkrypton_add_drivers(void)
 	msm_smd_init();
 	msm_rpm_driver_init();
 	msm_clock_init(&msmkrypton_clock_init_data);
+	tsens_tm_init_driver();
 }
 
 void __init msmkrypton_reserve(void)
