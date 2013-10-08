87e7e1b2ecf895494ad3aa95a62e6a50ecd11dd0
Lee Susman <lsusman@codeaurora.org>
2013-10-08 09:47:59 +0300

    mm: change max readahead size to 512KB
    Change the VM_MAX_READAHEAD value from the default 128KB
    to 512KB. This will allow the readahead window to grow to a maximum size
    of 512KB, which greatly benefits to sequential read throughput.
    
    Change-Id: Ia0780ea4e2a4ae0b6111485b72fb25376dcb1f96
    Signed-off-by: Lee Susman <lsusman@codeaurora.org>
    [vinmenon@codeaurora.org: switch to VM_READAHEAD_PAGES on 5.4]
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2542,7 +2542,7 @@ int __must_check write_one_page(struct page *page);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-#define VM_READAHEAD_PAGES	(SZ_128K / PAGE_SIZE)
+#define VM_READAHEAD_PAGES	(SZ_512K / PAGE_SIZE)
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 			pgoff_t offset, unsigned long nr_to_read);
