94b8281c15a4e094513e7fe4da2b5cb418e5f402
Asish Bhattacharya <asishb@codeaurora.org>
2013-10-10 18:01:08 +0530

    msm: qdsp: memset stack buffer allocation.
    This is needed to avoid garbage data in uninitialized variables.
    
    Change-Id: I456be431ee2e22fa5cd5fd2951d7079e177e3646
    CRs-Fixed: 553262
    Signed-off-by: Asish Bhattacharya <asishb@codeaurora.org>

--- arch/arm/mach-msm/qdsp5/audio_aac_in.c
+++ arch/arm/mach-msm/qdsp5/audio_aac_in.c
@@ -2,7 +2,7 @@
  *
  * aac audio input device
  *
- * Copyright (c) 2011-2012, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
  *
  * This code is based in part on arch/arm/mach-msm/qdsp5v2/audio_aac_in.c,
  * Copyright (C) 2008 Google, Inc.
@@ -770,6 +770,7 @@ static long audaac_in_ioctl(struct file *file,
 	MM_DBG("\n");
 	if (cmd == AUDIO_GET_STATS) {
 		struct msm_audio_stats stats;
+		memset(&stats, 0, sizeof(stats));
 		stats.byte_count = atomic_read(&audio->in_bytes);
 		stats.sample_count = atomic_read(&audio->in_samples);
 		if (copy_to_user((void *) arg, &stats, sizeof(stats)))
