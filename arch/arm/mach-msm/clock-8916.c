fea80a64eab16d5a8e82a55430b097a1f58e572a
Pratibhasagar V <pratibha@codeaurora.org>
2013-10-11 17:45:13 +0530

    msm: clock-8916: Add dummy clock support for SDHC
    For eMMC and SD card dummy clock support needs to be added.
    
    Change-Id: Ic18bb8cbc75d6826c40d39c1807d1fa8f03c1e10
    Signed-off-by: Pratibhasagar V <pratibha@codeaurora.org>

--- arch/arm/mach-msm/clock-8916.c
+++ arch/arm/mach-msm/clock-8916.c
@@ -30,6 +30,10 @@
  * they need to control.
  */
 static struct clk_lookup msm_clocks_8916[] = {
+	CLK_DUMMY("core_clk",	SDC1_CLK,	"msm_sdcc.1", OFF),
+	CLK_DUMMY("iface_clk",	SDC1_P_CLK,	"msm_sdcc.1", OFF),
+	CLK_DUMMY("core_clk",	SDC2_CLK,	"msm_sdcc.2", OFF),
+	CLK_DUMMY("iface_clk",	SDC2_P_CLK,	"msm_sdcc.2", OFF),
 	CLK_DUMMY("core_clk",   BLSP1_UART_CLK, "78af000.serial", OFF),
 	CLK_DUMMY("iface_clk",  BLSP1_UART_CLK, "78af000.serial", OFF),
 	CLK_DUMMY("",	camss_ahb_clk_src.c,	"", OFF),
