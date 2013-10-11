ed47654c4146b2c0aaef557c13d37c7e10ecc450
Banajit Goswami <bgoswami@codeaurora.org>
2013-10-10 18:17:43 -0700

    ASoC: Add debug message to track widget power up/down
    Add a debug message which can be used for tracking widgets being
    power-up and powered-down by DAPM, mainly used for debugging
    purpose.
    
    Change-Id: I291a381da44edca3bc2bb07be53b1969c03a48f0
    Signed-off-by: Banajit Goswami <bgoswami@codeaurora.org>

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -1470,6 +1470,11 @@ static void dapm_seq_run(struct snd_soc_card *card,
 			break;
 		}
 
+		/* Add this debug log to keep track of widgets being
+		 * powered-up and powered-down */
+		dev_dbg(w->dapm->dev, "dapm: powering %s widget %s\n",
+			power_up ? "up" : "down", w->name);
+
 		if (ret < 0)
 			dev_err(w->dapm->dev,
 				"ASoC: Failed to apply widget power: %d\n", ret);
