4167fa6147989fe5bd54c002f819476428c0ab50
Abhimanyu Kapur <abhimany@codeaurora.org>
2013-10-15 19:20:40 -0700

    arm64: process management: Add TIF_MM_RELEASED
    Add a new task info flag to represent a task
    whose mm struct has been freed. This is used by the
    android low memory killer to track tasks whose
    mm struct has been freed.
    
    Change-Id: Id72e67e31fb52e07f01fb6e234b102f63b400aa5
    Signed-off-by: Abhimanyu Kapur <abhimany@codeaurora.org>

--- arch/arm64/include/asm/thread_info.h
+++ arch/arm64/include/asm/thread_info.h
@@ -114,6 +114,7 @@ static inline struct thread_info *current_thread_info(void)
 #define TIF_SINGLESTEP		21
 #define TIF_32BIT		22	/* 32bit process */
 #define TIF_SWITCH_MM		23	/* deferred switch_mm */
+#define TIF_MM_RELEASED		24
 
 #define _TIF_SIGPENDING		(1 << TIF_SIGPENDING)
 #define _TIF_NEED_RESCHED	(1 << TIF_NEED_RESCHED)
