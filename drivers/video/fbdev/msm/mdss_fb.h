0df863c8161ca2e7d96211bbe7fb8e2aa18c890a
Adrian Salido-Moreno <adrianm@codeaurora.org>
2013-10-15 20:31:50 -0700

    msm: mdss: implement display thread to handle commits
    Refactor sync point implementation by using a commit thread to handle
    all commits for better performance. Make use of commit count to
    synchronize the release fences with the right point in the timeline.
    Keep a separate commits pending variable to be able to queue multiple
    commits as well.
    
    Change-Id: I1303c49f2c56eb5b9a34c162a1896670d307a674
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_fb.h
+++ drivers/video/fbdev/msm/mdss_fb.h
@@ -60,12 +60,12 @@ struct msm_sync_pt_data {
 	char *fence_name;
 	u32 acq_fen_cnt;
 	struct sync_fence *acq_fen[MDP_MAX_FENCE_FD];
-	int cur_rel_fen_fd;
-	struct sync_pt *cur_rel_sync_pt;
-	struct sync_fence *cur_rel_fence;
+
 	struct sw_sync_timeline *timeline;
 	int timeline_value;
 	u32 threshold;
+
+	atomic_t commit_cnt;
 	struct mutex sync_mutex;
 };
 
@@ -106,6 +106,11 @@ struct mdss_fb_proc_info {
 	int pid;
 	u32 ref_cnt;
 	struct list_head list;
+};
+
+struct msm_fb_backup_type {
+	struct fb_info info;
+	struct mdp_display_commit disp_commit;
 };
 
 struct msm_fb_data_type {
@@ -162,20 +167,17 @@ struct msm_fb_data_type {
 	struct msm_sync_pt_data mdp_sync_pt_data;
 
 	/* for non-blocking */
-	struct completion commit_comp;
-	u32 is_committing;
-	struct work_struct commit_work;
-	void *msm_fb_backup;
+	struct task_struct *disp_thread;
+	atomic_t commits_pending;
+	wait_queue_head_t commit_wait_q;
+	wait_queue_head_t idle_wait_q;
+
+	struct msm_fb_backup_type msm_fb_backup;
 	struct completion power_set_comp;
 	u32 is_power_setting;
 
 	u32 dcm_state;
 	struct list_head proc_list;
-};
-
-struct msm_fb_backup_type {
-	struct fb_info info;
-	struct mdp_display_commit disp_commit;
 };
 
 static inline void mdss_fb_update_notify_update(struct msm_fb_data_type *mfd)
@@ -200,7 +202,7 @@ static inline void mdss_fb_update_notify_update(struct msm_fb_data_type *mfd)
 int mdss_fb_get_phys_info(dma_addr_t *start, unsigned long *len, int fb_num);
 void mdss_fb_set_backlight(struct msm_fb_data_type *mfd, u32 bkl_lvl);
 void mdss_fb_update_backlight(struct msm_fb_data_type *mfd);
-void mdss_fb_wait_for_fence(struct msm_sync_pt_data *sync_pt_data);
+int mdss_fb_wait_for_fence(struct msm_sync_pt_data *sync_pt_data);
 void mdss_fb_signal_timeline(struct msm_sync_pt_data *sync_pt_data);
 int mdss_fb_register_mdp_instance(struct msm_mdp_interface *mdp);
 int mdss_fb_dcm(struct msm_fb_data_type *mfd, int req_state);
