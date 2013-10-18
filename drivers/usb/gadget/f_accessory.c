17799062e1335656a1d7cec895c08ca4c13f0463
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2013-10-18 15:43:29 +0530

    usb: gadget: f_accessory: Make RX buffer size aligned to EP's MTU
    Synopsys USB3 Controller (DWC3) has a restriction where size
    of OUT requests (TRB) queued to the controller must be aligned
    with the endpoint's max packet size. Generally, accessory userspace
    module submits RX requests not aligned to endpoint's max packet
    size. Hence to overcome this, align the size of RX request buffer to
    endpoint's max packet while submitting to DCD as the buffers are
    already allocated with the size of 16KB.
    
    CRs-Fixed: 563000
    Change-Id: I3acdfc055d0c6a79a0aa65a715bae06dc475d078
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>

--- drivers/usb/gadget/f_accessory.c
+++ drivers/usb/gadget/f_accessory.c
@@ -596,7 +596,7 @@ static ssize_t acc_read(struct file *fp, char __user *buf,
 {
 	struct acc_dev *dev = fp->private_data;
 	struct usb_request *req;
-	int r = count, xfer;
+	int r = count, xfer, len;
 	int ret = 0;
 
 	pr_debug("acc_read(%d)\n", count);
@@ -606,6 +606,8 @@ static ssize_t acc_read(struct file *fp, char __user *buf,
 
 	if (count > BULK_BUFFER_SIZE)
 		count = BULK_BUFFER_SIZE;
+
+	len = ALIGN(count, dev->ep_out->maxpacket);
 
 	/* we will block until we're online */
 	pr_debug("acc_read: waiting for online\n");
@@ -618,7 +620,7 @@ static ssize_t acc_read(struct file *fp, char __user *buf,
 requeue_req:
 	/* queue a request */
 	req = dev->rx_req[0];
-	req->length = count;
+	req->length = len;
 	dev->rx_done = 0;
 	ret = usb_ep_queue(dev->ep_out, req, GFP_KERNEL);
 	if (ret < 0) {
