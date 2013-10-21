6599f780c1a5301abdf3d2456f48f6274264dd9e
Kiran Gunda <kgunda@codeaurora.org>
2013-10-21 17:00:15 +0530

    slim_ngd: Add NULL pointer check apart from error check
    Check against NULL pointer after allocating
    the memory for msm_slim_ctrl structure pointer
    dynamically to avoid unexpected crashes.
    
    Earlier this pointer is checked only for error
    codes. In this patch NULL pointer check is also
    added.
    
    CRs-Fixed: 562417
    Change-Id: I5cd731144f88b89ee412d19f110b3df6426186f7
    Signed-off-by: Kiran Gunda <kgunda@codeaurora.org>

--- drivers/slimbus/slim-msm-ngd.c
+++ drivers/slimbus/slim-msm-ngd.c
@@ -1076,7 +1076,7 @@ static int ngd_slim_probe(struct platform_device *pdev)
 	}
 
 	dev = kzalloc(sizeof(struct msm_slim_ctrl), GFP_KERNEL);
-	if (IS_ERR(dev)) {
+	if (IS_ERR_OR_NULL(dev)) {
 		dev_err(&pdev->dev, "no memory for MSM slimbus controller\n");
 		return PTR_ERR(dev);
 	}
