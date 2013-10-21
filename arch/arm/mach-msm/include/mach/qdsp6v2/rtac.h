d9f1b17e54b831959946991acfed4bf59b2a93f9
Ravi Kumar Alamanda <ralama@codeaurora.org>
2013-10-21 13:57:12 -0700

    ASoC: qdsp6: Add support for post processing on low latency path
    Current implementation does not send audio calibration data to
    DSP if the playback goes through low latency path. This results
    poor quality audio on Speaker if it needs tuning.
    Update ADM and RTAC drivers to send audio calibration data
    to DSP and update properly in the QACT tool.
    
    Change-Id: Ib4bf612ffd81b63a5e083cae7d7ded5b8239c582
    Signed-off-by: Ravi Kumar Alamanda <ralama@codeaurora.org>
    Signed-off-by: Krishnankutty Kolathappilly <kkolat@codeaurora.org>

--- arch/arm/mach-msm/include/mach/qdsp6v2/rtac.h
+++ arch/arm/mach-msm/include/mach/qdsp6v2/rtac.h
@@ -45,7 +45,7 @@ struct rtac_cal_block_data {
 };
 
 void rtac_add_adm_device(u32 port_id, u32 copp_id, u32 path_id, u32 popp_id);
-void rtac_remove_adm_device(u32 port_id);
+void rtac_remove_adm_device(u32 port_id, u32 copp_id);
 void rtac_remove_popp_from_adm_devices(u32 popp_id);
 void rtac_add_voice(u32 cvs_handle, u32 cvp_handle, u32 rx_afe_port,
 	u32 tx_afe_port, u32 session_id);
