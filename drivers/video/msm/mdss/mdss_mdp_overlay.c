333cad8f3611c8fc0acfffd955db1c9d24eacf10
Pawan Kumar <pavaku@codeaurora.org>
2013-10-23 12:10:34 +0530

    msm: mdss: Fix rgb_565 format for 90 degree rotation
    Make rotator output format for rgb_565 and bgr_565
    as rgb_888 for 90 degree rotation as expected by
    MDSS.
    
    Change-Id: I0865b5688fa662d1e88d1efebef26a38d4abfd61
    Signed-off-by: Pawan Kumar <pavaku@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -414,15 +414,21 @@ static int mdss_mdp_overlay_pipe_setup(struct msm_fb_data_type *mfd,
 	pr_debug("pipe ctl=%u req id=%x mux=%d\n", mdp5_data->ctl->num, req->id,
 			mixer_mux);
 
-	if ((req->flags & MDP_BWC_EN) || ((req->flags & MDP_SOURCE_ROTATED_90)
-		&& (mdata->mdp_rev < MDSS_MDP_HW_REV_102)))
-		req->src.format =
-			mdss_mdp_get_rotator_dst_format(req->src.format, 1);
-
 	fmt = mdss_mdp_get_format_params(req->src.format);
 	if (!fmt) {
 		pr_err("invalid pipe format %d\n", req->src.format);
 		return -EINVAL;
+	}
+
+	if ((req->flags & MDP_BWC_EN) || ((req->flags & MDP_SOURCE_ROTATED_90)
+		&& ((mdata->mdp_rev < MDSS_MDP_HW_REV_102) || !fmt->is_yuv))) {
+		req->src.format =
+			mdss_mdp_get_rotator_dst_format(req->src.format, 1);
+		fmt = mdss_mdp_get_format_params(req->src.format);
+		if (!fmt) {
+			pr_err("invalid pipe format %d\n", req->src.format);
+			return -EINVAL;
+		}
 	}
 
 	ret = mdss_mdp_overlay_req_check(mfd, req, fmt);
