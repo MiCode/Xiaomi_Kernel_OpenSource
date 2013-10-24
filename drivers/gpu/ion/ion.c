61194233e45e3d5caac4591deffa8765ed97eabb
Rom Lemarchand <romlem@google.com>
2013-10-24 05:39:34 -0700

    ion: add compat_ioctl
    Add a compat_ioctl to the ion driver
    
    Change-Id: I43da387e22ff9c4a29f0159dfe9e898efc500553
    Signed-off-by: Rom Lemarchand <romlem@google.com>
    Git-commit: a402f70ff3c94edb73b9f5670b5a55a27b1f1f00
    Git-repo: http://android.googlesource.com/kernel/common/
    [mitchelh@codeaurora.org: minor conflicts due to msm Makefile changes]
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- drivers/gpu/ion/ion.c
+++ drivers/gpu/ion/ion.c
@@ -42,6 +42,7 @@
 
 
 #include "ion_priv.h"
+#include "compat_ion.h"
 
 /**
  * struct ion_device - the metadata of the ion device node
@@ -1471,6 +1472,7 @@ static const struct file_operations ion_fops = {
 	.open           = ion_open,
 	.release        = ion_release,
 	.unlocked_ioctl = ion_ioctl,
+	.compat_ioctl   = compat_ion_ioctl,
 };
 
 static size_t ion_debug_heap_total(struct ion_client *client,
