fa39a284d13d6657d2eea1a5566a79146e902384
Satish Kodishala <skodisha@codeaurora.org>
2013-10-24 12:14:45 +0530

    v4l2-dev: Set VIDIOC_DQBUF as valid ioctl for radio.
    Set VIDIOC_DQBUF as valid ioctl for FM TUNER interface of
    V4L2 so that DQBUF calls from v4l2 tuner would pass without
    any issue
    
    Change-Id: Ie65b4783e095867fcfb1cbb9ded0913c21259a96
    CRs-fixed: 565235
    Signed-off-by: Satish Kodishala <skodisha@codeaurora.org>

--- drivers/media/v4l2-core/v4l2-dev.c
+++ drivers/media/v4l2-core/v4l2-dev.c
@@ -712,6 +712,9 @@ static void determine_valid_ioctls(struct video_device *vdev)
 		SET_VALID_IOCTL(ops, VIDIOC_G_DV_TIMINGS, vidioc_g_dv_timings);
 		SET_VALID_IOCTL(ops, VIDIOC_ENUM_DV_TIMINGS, vidioc_enum_dv_timings);
 		SET_VALID_IOCTL(ops, VIDIOC_DV_TIMINGS_CAP, vidioc_dv_timings_cap);
+	} else {
+		/* ioctls valid for radio */
+		SET_VALID_IOCTL(ops, VIDIOC_DQBUF, vidioc_dqbuf);
 	}
 	if (is_tx) {
 		/* transmitter only ioctls */
