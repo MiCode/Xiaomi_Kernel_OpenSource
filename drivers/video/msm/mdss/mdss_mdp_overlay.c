7cdd3a434654502ed0324996f01b337a14f922da
Adrian Salido-Moreno <adrianm@codeaurora.org>
2013-10-23 18:23:06 -0700

    msm: mdss: keep mdp clk on during overlay kick off
    This will reduce the delay caused by turning on/off mdp clk multiple
    times during kickoff. Currently during kickoff, mdp pipe registers and
    flush registers are programmed each with mdp clk on/off. We only need
    turn mdp clk on/off once to program those registers. Increase scope of
    mdp clock vote to keep them on during the entire programming sequence.
    
    Change-Id: I5270c5e7aeef2f16c6a64d56827a73a8cb380fc5
    Signed-off-by: Huaibin Yang <huaibiny@codeaurora.org>
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -905,6 +905,7 @@ int mdss_mdp_overlay_kickoff(struct msm_fb_data_type *mfd,
 	}
 
 	mdss_mdp_ctl_notify(ctl, MDP_NOTIFY_FRAME_BEGIN);
+	mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_ON, false);
 
 	if (data)
 		mdss_mdp_set_roi(ctl, data);
@@ -1002,6 +1003,7 @@ int mdss_mdp_overlay_kickoff(struct msm_fb_data_type *mfd,
 	mdss_fb_update_notify_update(mfd);
 commit_fail:
 	mdss_mdp_overlay_cleanup(mfd);
+	mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_OFF, false);
 	mdss_mdp_ctl_notify(ctl, MDP_NOTIFY_FRAME_FLUSHED);
 
 	mutex_unlock(&mdp5_data->ov_lock);
