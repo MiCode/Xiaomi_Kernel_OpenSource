96e6b110bfc431e292353917045f6be421d1260e
Krishna Konda <kkonda@codeaurora.org>
2013-10-28 15:25:03 -0700

    mmc: sdhci-msm: improve tuning process
    In newer hardware, the tuning process is not able to always
    find a reliable phase to use for sampling data. This is mostly
    due to hardware. This problem manifests itself as all successful
    tuning phases, which means that the phase choosen could be a bad
    one but is not identified as such at the time of tuning. So in
    order to work around this, rely on optional drive types implemented
    by the eMMC card, in addition to the mandatory drive type (50 ohm).
    
    By using drive types supported by the card, when all phases are
    sucessful in tuning, change drive type to a different value in the
    list of supported drive types and retune. This will continue for all
    tuning phases until a valid one is found. After that the drive type
    is reset to the default one, if changed.
    
    Change-Id: I348fb30daa43d97c58f83f7e4a22019f94ef4954
    Signed-off-by: Krishna Konda <kkonda@codeaurora.org>
    Signed-off-by: Asutosh Das <asutoshd@codeaurora.org>
    [venkatg@codeaurora.org: rename cmd_timeout_ms to busy_timeout as
    part of 3.14 kernel upgrade]
    Signed-off-by: Venkat Gopalakrishnan <venkatg@codeaurora.org>
    [subhashj@codeaurora.org: fixed minor merge conflict & compilation
    error]
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1555,6 +1555,7 @@ static int mmc_init_card(struct mmc_host *host, u32 ocr,
 		card->type = MMC_TYPE_MMC;
 		card->rca = 1;
 		memcpy(card->raw_cid, cid, sizeof(card->raw_cid));
+		host->card = card;
 	}
 
 	/*
@@ -1784,12 +1785,10 @@ static int mmc_init_card(struct mmc_host *host, u32 ocr,
 		}
 	}
 
-	if (!oldcard)
-		host->card = card;
-
 	return 0;
 
 free_card:
+	host->card = NULL;
 	if (!oldcard)
 		mmc_remove_card(card);
 err:
