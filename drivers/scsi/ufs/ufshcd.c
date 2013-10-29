30f1734c7b0817e55ebc05a54c68e21b282ea424
Yaniv Gardi <ygardi@codeaurora.org>
2013-10-29 12:32:16 +0200

    scsi: ufs: undo disabling LCC configuration in UFS device
    So far we have disabled the LCC configuration in device and also in the
    controller.
    However, in some UFS devices, disabling the LCC configuration might fail
    so we can remove this command. It was presented at first, just that the
    controller and device will be configured identically (both with
    TX_LCC_ENABLE disabled), but it shouldn't have any impact on the UFS
    device behavior when removing this configuration command from the device.
    
    Change-Id: I87342a63c75d28a91f0d1ab8edd819d13b3a21a1
    Signed-off-by: Yaniv Gardi <ygardi@codeaurora.org>
    [subhashj@codeaurora.org: resolved trivial merge conflicts]
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>
    [cang@codeaurora.org: resolved trivial merge conflicts]
    Signed-off-by: Can Guo <cang@codeaurora.org>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -4176,7 +4176,6 @@ static int ufshcd_link_startup(struct ufs_hba *hba)
 	int ret;
 	int retries = DME_LINKSTARTUP_RETRIES;
 	bool link_startup_again = false;
-	int device_tx_lanes;
 	int hc_tx_lanes;
 	int i;
 
@@ -4218,23 +4217,9 @@ link_startup:
 		ufshcd_dme_get(hba,
 			UIC_ARG_MIB(PA_CONNECTEDTXDATALANES), &hc_tx_lanes);
 
-		ufshcd_dme_peer_get(hba,
-			UIC_ARG_MIB(PA_CONNECTEDTXDATALANES),
-						&device_tx_lanes);
-
 		for (i = 0; i < hc_tx_lanes; ++i)
 			ufshcd_dme_set(hba,
 				UIC_ARG_MIB_SEL(TX_LCC_ENABLE, i), 0);
-
-		for (i = 0; i < device_tx_lanes; ++i)
-			ufshcd_dme_peer_st_set(hba,
-				UIC_ARG_MIB_SEL(TX_LCC_ENABLE, i), 0);
-
-		/*
-		 * some devices might need a power mode change to apply
-		 * the above values
-		 */
-		ufshcd_uic_change_pwr_mode(hba, UNCHANGED << 4 | UNCHANGED);
 	}
 
 	if (link_startup_again) {
