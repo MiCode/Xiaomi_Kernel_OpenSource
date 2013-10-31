0c68486d5d3a2b1d354e8eeedc5de78bd9691247
Stepan Moskovchenko <stepanm@codeaurora.org>
2013-10-31 12:19:20 -0700

    msm: msm_watchdog_v2: Update module parameter comments
    Update the comments describing the module parameters for
    the msm_watchdog_v2 driver to reflect the correct name of
    the driver / module.
    
    Change-Id: I7916b8b96c0b33940f9182007b7e1c2fa3c3e3b4
    Signed-off-by: Stepan Moskovchenko <stepanm@codeaurora.org>

--- arch/arm/mach-msm/msm_watchdog_v2.c
+++ arch/arm/mach-msm/msm_watchdog_v2.c
@@ -68,7 +68,7 @@ struct msm_watchdog_data {
 
 /*
  * On the kernel command line specify
- * msm_watchdog.enable=1 to enable the watchdog
+ * msm_watchdog_v2.enable=1 to enable the watchdog
  * By default watchdog is turned on
  */
 static int enable = 1;
@@ -76,7 +76,7 @@ module_param(enable, int, 0);
 
 /*
  * On the kernel command line specify
- * msm_watchdog.WDT_HZ=<clock val in HZ> to set Watchdog
+ * msm_watchdog_v2.WDT_HZ=<clock val in HZ> to set Watchdog
  * ticks. By default it is set to 32765.
  */
 static long WDT_HZ = 32765;
