43b74e7febd6f8d8f0382780469496fe57663b84
Vimal Puthanveed <vimalp@codeaurora.org>
2013-10-31 15:12:34 -0700

    ASoC: msm: Add routing for HFP using AFE and ASM Loopback
    HFP hostless driver is used to loopback BT data into
    SLIM_0_RX. And asm loopback MM6 is used for looping
    data from mic to BT so that pre_proc effects can be
    added.
    
    Change-Id: Ic818b91c545a4d17043ecdf933d6d3398ad591d3
    Signed-off-by: Vimal Puthanveed <vimalp@codeaurora.org>

--- sound/soc/msm/msm-dai-fe.c
+++ sound/soc/msm/msm-dai-fe.c
@@ -422,6 +422,30 @@ static struct snd_soc_dai_driver msm_fe_dais[] = {
 	},
 	{
 		.playback = {
+			.stream_name = "INT_HFP_BT Hostless Playback",
+			.aif_name = "INTHFP_DL_HL",
+			.rates = SNDRV_PCM_RATE_8000 | SNDRV_PCM_RATE_16000,
+			.formats = SNDRV_PCM_FMTBIT_S16_LE,
+			.channels_min = 1,
+			.channels_max = 2,
+			.rate_min =     8000,
+			.rate_max =     16000,
+		},
+		.capture = {
+			.stream_name = "INT_HFP_BT Hostless Capture",
+			.aif_name = "INTHFP_UL_HL",
+			.rates = SNDRV_PCM_RATE_8000 | SNDRV_PCM_RATE_16000,
+			.formats = SNDRV_PCM_FMTBIT_S16_LE,
+			.channels_min = 1,
+			.channels_max = 2,
+			.rate_min =     8000,
+			.rate_max =     16000,
+		},
+		.ops = &msm_fe_dai_ops,
+		.name = "INT_HFP_BT_HOSTLESS",
+	},
+	{
+		.playback = {
 			.stream_name = "AFE-PROXY Playback",
 			.aif_name = "PCM_RX",
 			.rates = (SNDRV_PCM_RATE_8000 |
