a02bd1cad515b8710e2ad93c8877cc5ace2012c6
Alexy Joseph <alexyj@codeaurora.org>
2013-10-31 17:11:31 -0700

    qdsp6v2: Add support for suspend command for WMA decoder
    WMA DSP decoder consumes more power in pause state. This is because
    WMA decoder expects suspend command after it is paused. Add support
    for suspend command.
    
    Change-Id: Ifdfccb6cf6270db5b131a65f17edac40ef1a85db
    CRs-Fixed: 553828
    Signed-off-by: Alexy Joseph <alexyj@codeaurora.org>

--- include/sound/q6asm-v2.h
+++ include/sound/q6asm-v2.h
@@ -54,6 +54,7 @@
 #define CMD_EOS            0x0003
 #define CMD_CLOSE          0x0004
 #define CMD_OUT_FLUSH      0x0005
+#define CMD_SUSPEND        0x0006
 
 /* bit 0:1 represents priority of stream */
 #define STREAM_PRIORITY_NORMAL	0x0000
--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -1225,6 +1225,7 @@ static int32_t q6asm_callback(struct apr_client_data *data, void *priv)
 					data->payload_size))
 				break;
 		case ASM_SESSION_CMD_PAUSE:
+		case ASM_SESSION_CMD_SUSPEND:
 		case ASM_DATA_CMD_EOS:
 		case ASM_STREAM_CMD_CLOSE:
 		case ASM_STREAM_CMD_FLUSH:
@@ -3868,6 +3869,11 @@ int q6asm_cmd(struct audio_client *ac, int cmd)
 	case CMD_PAUSE:
 		pr_debug("%s:CMD_PAUSE\n", __func__);
 		hdr.opcode = ASM_SESSION_CMD_PAUSE;
+		state = &ac->cmd_state;
+		break;
+	case CMD_SUSPEND:
+		pr_debug("%s:CMD_SUSPEND\n", __func__);
+		hdr.opcode = ASM_SESSION_CMD_SUSPEND;
 		state = &ac->cmd_state;
 		break;
 	case CMD_FLUSH:
