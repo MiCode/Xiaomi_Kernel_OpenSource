5be83267015500c62711d2617fc25e1479fe1ad8
Patrick Daly <pdaly@codeaurora.org>
2013-11-05 14:15:10 -0800

    msm: clock-8610: Add clock lookup entries for clock-a7 driver
    Add the clock entries used to control the cpu.
    
    Change-Id: Id0e98ac05203a932f4f92a67cc3afbcb4b95dce9
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

--- arch/arm/mach-msm/clock-8610.c
+++ arch/arm/mach-msm/clock-8610.c
@@ -3113,6 +3113,8 @@ static struct clk_lookup msm_clocks_8610[] = {
 	CLK_LOOKUP("xo",        cxo_acpu_clk.c, "f9011050.qcom,acpuclk"),
 	CLK_LOOKUP("gpll0", gpll0_ao_clk_src.c, "f9011050.qcom,acpuclk"),
 	CLK_LOOKUP("a7sspll",        a7sspll.c, "f9011050.qcom,acpuclk"),
+	CLK_LOOKUP("clk-4",  gpll0_ao_clk_src.c, "f9011050.qcom,clock-a7"),
+	CLK_LOOKUP("clk-5", a7sspll.c, "f9011050.qcom,clock-a7"),
 
 	CLK_LOOKUP("measure_clk", apc0_m_clk, ""),
 	CLK_LOOKUP("measure_clk", apc1_m_clk, ""),
