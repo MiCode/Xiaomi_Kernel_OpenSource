db3947a1a6599beaaf0ddc8b45479b878d89003c
Arun Menon <avmenon@codeaurora.org>
2013-11-05 16:58:58 -0800

    msm: vidc: clean up msm_vidc.h
    Move definitions needed by user space to uapi/media/msm_vidc.h
    
    Change-Id: If13f845be8a072ec645b136373abb24da4462ebe
    Signed-off-by: Arun Menon <avmenon@codeaurora.org>

--- include/media/msm_vidc.h
+++ include/media/msm_vidc.h
@@ -1,12 +1,24 @@
+/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
 #ifndef _MSM_VIDC_H_
 #define _MSM_VIDC_H_
-
-#ifdef __KERNEL__
 
 #include <linux/poll.h>
 #include <linux/videodev2.h>
 #include <linux/types.h>
 #include <linux/msm_ion.h>
+#include <uapi/media/msm_vidc.h>
 
 #define HAL_BUFFER_MAX 0xb
 
@@ -105,95 +117,4 @@ int msm_vidc_smem_get_domain_partition(void *instance,
 		u32 flags, enum hal_buffer buffer_type,
 		int *domain_num, int *partition_num);
 void *msm_vidc_smem_get_client(void *instance);
-#endif
-struct msm_vidc_extradata_header {
-	unsigned int size;
-	unsigned int:32; /** Keeping binary compatibility */
-	unsigned int:32; /* with firmware and OpenMAX IL **/
-	unsigned int type; /* msm_vidc_extradata_type */
-	unsigned int data_size;
-	unsigned char data[1];
-};
-struct msm_vidc_interlace_payload {
-	unsigned int format;
-};
-struct msm_vidc_framerate_payload {
-	unsigned int frame_rate;
-};
-struct msm_vidc_ts_payload {
-	unsigned int timestamp_lo;
-	unsigned int timestamp_hi;
-};
-struct msm_vidc_concealmb_payload {
-	unsigned int num_mbs;
-};
-struct msm_vidc_recoverysei_payload {
-	unsigned int flags;
-};
-struct msm_vidc_aspect_ratio_payload {
-	unsigned int size;
-	unsigned int version;
-	unsigned int port_index;
-	unsigned int aspect_width;
-	unsigned int aspect_height;
-};
-struct msm_vidc_mpeg2_seqdisp_payload {
-	unsigned int video_format;
-	unsigned int color_descp;
-	unsigned int color_primaries;
-	unsigned int transfer_char;
-	unsigned int matrix_coeffs;
-	unsigned int disp_width;
-	unsigned int disp_height;
-};
-struct msm_vidc_panscan_window {
-	unsigned int panscan_height_offset;
-	unsigned int panscan_width_offset;
-	unsigned int panscan_window_width;
-	unsigned int panscan_window_height;
-};
-struct msm_vidc_panscan_window_payload {
-	unsigned int num_panscan_windows;
-	struct msm_vidc_panscan_window wnd[1];
-};
-struct msm_vidc_stream_userdata_payload {
-	unsigned int type;
-	unsigned int data[1];
-};
-enum msm_vidc_extradata_type {
-	MSM_VIDC_EXTRADATA_NONE = 0x00000000,
-	MSM_VIDC_EXTRADATA_MB_QUANTIZATION = 0x00000001,
-	MSM_VIDC_EXTRADATA_INTERLACE_VIDEO = 0x00000002,
-	MSM_VIDC_EXTRADATA_VC1_FRAMEDISP = 0x00000003,
-	MSM_VIDC_EXTRADATA_VC1_SEQDISP = 0x00000004,
-	MSM_VIDC_EXTRADATA_TIMESTAMP = 0x00000005,
-	MSM_VIDC_EXTRADATA_S3D_FRAME_PACKING = 0x00000006,
-	MSM_VIDC_EXTRADATA_FRAME_RATE = 0x00000007,
-	MSM_VIDC_EXTRADATA_PANSCAN_WINDOW = 0x00000008,
-	MSM_VIDC_EXTRADATA_RECOVERY_POINT_SEI = 0x00000009,
-	MSM_VIDC_EXTRADATA_MPEG2_SEQDISP = 0x0000000D,
-	MSM_VIDC_EXTRADATA_STREAM_USERDATA = 0x0000000E,
-	MSM_VIDC_EXTRADATA_MULTISLICE_INFO = 0x7F100000,
-	MSM_VIDC_EXTRADATA_NUM_CONCEALED_MB = 0x7F100001,
-	MSM_VIDC_EXTRADATA_INDEX = 0x7F100002,
-	MSM_VIDC_EXTRADATA_ASPECT_RATIO = 0x7F100003,
-	MSM_VIDC_EXTRADATA_METADATA_FILLER = 0x7FE00002,
-};
-enum msm_vidc_interlace_type {
-	MSM_VIDC_INTERLACE_FRAME_PROGRESSIVE = 0x01,
-	MSM_VIDC_INTERLACE_INTERLEAVE_FRAME_TOPFIELDFIRST = 0x02,
-	MSM_VIDC_INTERLACE_INTERLEAVE_FRAME_BOTTOMFIELDFIRST = 0x04,
-	MSM_VIDC_INTERLACE_FRAME_TOPFIELDFIRST = 0x08,
-	MSM_VIDC_INTERLACE_FRAME_BOTTOMFIELDFIRST = 0x10,
-};
-enum msm_vidc_recovery_sei {
-	MSM_VIDC_FRAME_RECONSTRUCTION_INCORRECT = 0x0,
-	MSM_VIDC_FRAME_RECONSTRUCTION_CORRECT = 0x01,
-	MSM_VIDC_FRAME_RECONSTRUCTION_APPROXIMATELY_CORRECT = 0x02,
-};
-enum msm_vidc_userdata_type {
-	MSM_VIDC_USERDATA_TYPE_FRAME = 0x1,
-	MSM_VIDC_USERDATA_TYPE_TOP_FIELD = 0x2,
-	MSM_VIDC_USERDATA_TYPE_BOTTOM_FIELD = 0x3,
-};
 #endif
