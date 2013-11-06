80a53b1f98c0281aba4bf39457c83f5526ce2c31
Krishnankutty Kolathappilly <kkolat@codeaurora.org>
2013-11-06 10:08:39 -0800

    ALSA: compress: Memset timestamp structure to zero.
    snd_compr_tstamp is initialized using aggregate initialization
    that does not zero out the padded bytes.
    Initialize timestamp structure to zero using memset to avoid this.
    
    CRs-Fixed: 568717
    Change-Id: I7a7d188705161f06201f1a1f2945bb6acd633d5d
    Signed-off-by: Krishnankutty Kolathappilly <kkolat@codeaurora.org>
    Signed-off-by: Banajit Goswami <bgoswami@codeaurora.org>
    Signed-off-by: Meng Wang <mwang@codeaurora.org>

--- sound/core/compress_offload.c
+++ sound/core/compress_offload.c
@@ -655,9 +655,10 @@ snd_compr_set_metadata(struct snd_compr_stream *stream, unsigned long arg)
 static inline int
 snd_compr_tstamp(struct snd_compr_stream *stream, unsigned long arg)
 {
-	struct snd_compr_tstamp tstamp = {0};
+	struct snd_compr_tstamp tstamp;
 	int ret;
 
+	memset(&tstamp, 0, sizeof(tstamp));
 	ret = snd_compr_update_tstamp(stream, &tstamp);
 	if (ret == 0)
 		ret = copy_to_user((struct snd_compr_tstamp __user *)arg,
