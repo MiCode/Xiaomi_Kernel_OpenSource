9741065bddf246bdb7418b3bcba23c8f8775eb45
Colin Cross <ccross@android.com>
2013-11-07 12:25:56 -0800

    ion: move into staging
    Move ion from drivers/gpu/ion to drivers/android/staging/ion.
    
    Change-Id: Id6e996aa3954cbb8e1a8abc9578a56204f5eb211
    Signed-off-by: Colin Cross <ccross@android.com>
    Git-commit: 8cb378f23cb15b6d7e5d57bde7bac8f157c9d301
    Git-repo: http://android.googlesource.com/kernel/common/
    [mitchelh@codeaurora.org: Some conflicts since we already did the UAPI split.]
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- drivers/staging/android/ion/ion_system_heap.c
+++ drivers/staging/android/ion/ion_system_heap.c
@@ -19,12 +19,12 @@
 #include <linux/dma-mapping.h>
 #include <linux/err.h>
 #include <linux/highmem.h>
-#include <linux/ion.h>
 #include <linux/mm.h>
 #include <linux/scatterlist.h>
 #include <linux/seq_file.h>
 #include <linux/slab.h>
 #include <linux/vmalloc.h>
+#include "ion.h"
 #include "ion_priv.h"
 #include <linux/dma-mapping.h>
 #include <trace/events/kmem.h>
