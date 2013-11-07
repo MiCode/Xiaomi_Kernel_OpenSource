38a40145db5efb14deddf8c4990bdc267966fa0a
Pratik Patel <pratikp@codeaurora.org>
2013-11-07 13:11:55 -0800

    coresight: add cti pulse trigger function
    Add support for pulse triggering to allow uses cases that require
    software based pulse trigger generation.
    
    Change-Id: I1ce8fd40cfd0622364c101f9d9bc8aa4540b0344
    Signed-off-by: Pratik Patel <pratikp@codeaurora.org>

--- include/linux/coresight-cti.h
+++ include/linux/coresight-cti.h
@@ -35,6 +35,7 @@ extern void coresight_cti_unmap_trigin(
 extern void coresight_cti_unmap_trigout(
 			struct coresight_cti *cti, int trig, int ch);
 extern void coresight_cti_reset(struct coresight_cti *cti);
+extern int coresight_cti_pulse_trig(struct coresight_cti *cti, int ch);
 #else
 static inline struct coresight_cti *coresight_cti_get(const char *name)
 {
@@ -56,6 +57,10 @@ static inline void coresight_cti_unmap_trigin(
 static inline void coresight_cti_unmap_trigout(
 			struct coresight_cti *cti, int trig, int ch) {}
 static inline void coresight_cti_reset(struct coresight_cti *cti) {}
+static inline int coresight_cti_pulse_trig(struct coresight_cti *cti, int ch)
+{
+	return -ENOSYS;
+}
 #endif
 
 #endif
