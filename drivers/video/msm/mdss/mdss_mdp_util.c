7e1ae220efcee5f213ce27d9ec40353d104a532a
Jeykumar Sankaran <jsanka@codeaurora.org>
2013-11-07 17:15:41 -0800

    mdp: mdss: Tune bandwidth and clock based on ROI
    Calculate bandwidth and MDP clock requirement considering
    the ROI submitted for a frame. When doing partial frame update
    for smart panels, the data actually fetched for a pipe is not
    always the full resolution of the buffer programmed. Use the
    ROI passed to find out the region of fetching and calculate the
    bandwidth and clock values accordingly.
    
    Change-Id: I01f24f8fd9a42a2c4d705b8eb0a7658e79884175
    Signed-off-by: Jeykumar Sankaran <jsanka@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_util.c
+++ drivers/video/msm/mdss/mdss_mdp_util.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -257,6 +257,27 @@ void mdss_mdp_intersect_rect(struct mdss_mdp_img_rect *res_rect,
 	else
 		*res_rect = (struct mdss_mdp_img_rect){l, t, (r-l), (b-t)};
 }
+
+void mdss_mdp_crop_rect(struct mdss_mdp_img_rect *src_rect,
+	struct mdss_mdp_img_rect *dst_rect,
+	const struct mdss_mdp_img_rect *sci_rect)
+{
+	struct mdss_mdp_img_rect res;
+	mdss_mdp_intersect_rect(&res, dst_rect, sci_rect);
+
+	if (res.w && res.h) {
+		if ((res.w != dst_rect->w) || (res.h != dst_rect->h)) {
+			src_rect->x = src_rect->x + (res.x - dst_rect->x);
+			src_rect->y = src_rect->y + (res.y - dst_rect->y);
+			src_rect->w = res.w;
+			src_rect->h = res.h;
+		}
+		*dst_rect = (struct mdss_mdp_img_rect)
+			{(res.x - sci_rect->x), (res.y - sci_rect->y),
+			res.w, res.h};
+	}
+}
+
 int mdss_mdp_get_rau_strides(u32 w, u32 h,
 			       struct mdss_mdp_format_params *fmt,
 			       struct mdss_mdp_plane_sizes *ps)
