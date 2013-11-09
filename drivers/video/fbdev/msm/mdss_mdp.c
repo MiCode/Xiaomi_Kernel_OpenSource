565860a4aefeb9e5dfd77f06612a2e079000f54c
Laura Abbott <lauraa@codeaurora.org>
2013-11-08 17:19:33 -0800

    msm: Remove lib/memory_alloc.c and related files
    The memory pools are gone for good. Remove memory_alloc.c and the
    associated header file from clients.
    
    Change-Id: I8d303c72fa03cdca7eee34b39d57d5cbb5df920d
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_mdp.c
+++ drivers/video/fbdev/msm/mdss_mdp.c
@@ -30,7 +30,6 @@
 #include <linux/pm.h>
 #include <linux/pm_runtime.h>
 #include <linux/regulator/consumer.h>
-#include <linux/memory_alloc.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
 #include <linux/sched.h>
