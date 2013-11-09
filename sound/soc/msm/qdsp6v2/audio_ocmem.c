6b7b94c3ade6dfdc2db8bc0797f1ba19d40f275c
Laura Abbott <lauraa@codeaurora.org>
2013-11-08 17:04:45 -0800

    ASoC: msm: qdsp6v2: Migrate away from allocate_contiguous_ebi APIs
    The allocate_contiguous_ebi APIs are being deprecated in favor of
    the standard dma_alloc_coherent APIs. Migrate accordingly.
    
    Change-Id: I01dece36f7aaae192afa689daf0b51de4c1ec592
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- sound/soc/msm/qdsp6v2/audio_ocmem.c
+++ sound/soc/msm/qdsp6v2/audio_ocmem.c
@@ -22,7 +22,7 @@
 #include <linux/slab.h>
 #include <linux/io.h>
 #include <linux/of_device.h>
-#include <linux/memory_alloc.h>
+#include <linux/dma-mapping.h>
 #include <mach/msm_bus.h>
 #include <mach/msm_bus_board.h>
 #include <mach/ocmem.h>
@@ -131,7 +131,8 @@ struct audio_ocmem_prv {
 	bool audio_ocmem_running;
 	void *ocmem_ramdump_dev;
 	struct ramdump_segment ocmem_ramdump_segment;
-	unsigned long ocmem_dump_addr;
+	dma_addr_t ocmem_dump_addr;
+	void *ocmem_dump_virt;
 };
 
 static struct audio_ocmem_prv audio_ocmem_lcl;
@@ -776,13 +777,10 @@ static int audio_ocmem_platform_data_populate(struct platform_device *pdev)
 static void do_ocmem_ramdump(void)
 {
 	int ret = 0;
-	void *virt = NULL;
 
-	virt = ioremap(audio_ocmem_lcl.ocmem_dump_addr, AUDIO_OCMEM_BUF_SIZE);
 	ret = ocmem_dump(OCMEM_LP_AUDIO,
 			 audio_ocmem_lcl.buf,
-			 (unsigned long)virt);
-	iounmap(virt);
+			 (unsigned long)audio_ocmem_lcl.ocmem_dump_virt);
 
 	if (ret)
 		pr_err("%s: ocmem_dump failed\n", __func__);
@@ -889,12 +887,12 @@ static int ocmem_audio_client_probe(struct platform_device *pdev)
 	}
 	subsys_notif_register_notifier("adsp", &anb);
 
-	audio_ocmem_lcl.ocmem_dump_addr =
-		allocate_contiguous_memory_nomap(AUDIO_OCMEM_BUF_SIZE,
-						 MEMTYPE_EBI1,
-						 AUDIO_OCMEM_BUF_SIZE);
+	audio_ocmem_lcl.ocmem_dump_virt = dma_alloc_coherent(NULL,
+					AUDIO_OCMEM_BUF_SIZE,
+					&audio_ocmem_lcl.ocmem_dump_addr,
+					GFP_KERNEL);
 
-	if (audio_ocmem_lcl.ocmem_dump_addr) {
+	if (audio_ocmem_lcl.ocmem_dump_virt) {
 		audio_ocmem_lcl.ocmem_ramdump_dev =
 			create_ramdump_device("audio-ocmem", &pdev->dev);
 
@@ -967,9 +965,10 @@ destroy_audio_wq:
 destroy_ramdump:
 	if (audio_ocmem_lcl.ocmem_ramdump_dev)
 		destroy_ramdump_device(audio_ocmem_lcl.ocmem_ramdump_dev);
-	if (audio_ocmem_lcl.ocmem_dump_addr)
-		free_contiguous_memory_by_paddr(
-		    audio_ocmem_lcl.ocmem_dump_addr);
+	if (audio_ocmem_lcl.ocmem_dump_virt)
+		dma_free_coherent(NULL, AUDIO_OCMEM_BUF_SIZE,
+			audio_ocmem_lcl.ocmem_dump_virt,
+			audio_ocmem_lcl.ocmem_dump_addr);
 	return ret;
 }
 
@@ -985,9 +984,11 @@ static int ocmem_audio_client_remove(struct platform_device *pdev)
 					&audio_ocmem_client_nb);
 	if (audio_ocmem_lcl.ocmem_ramdump_dev)
 		destroy_ramdump_device(audio_ocmem_lcl.ocmem_ramdump_dev);
-	if (audio_ocmem_lcl.ocmem_dump_addr)
-		free_contiguous_memory_by_paddr(
-		    audio_ocmem_lcl.ocmem_dump_addr);
+
+	if (audio_ocmem_lcl.ocmem_dump_virt)
+		dma_free_coherent(NULL, AUDIO_OCMEM_BUF_SIZE,
+			audio_ocmem_lcl.ocmem_dump_virt,
+			audio_ocmem_lcl.ocmem_dump_addr);
 
 	return 0;
 }
