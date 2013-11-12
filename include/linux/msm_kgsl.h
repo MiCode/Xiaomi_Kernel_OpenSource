0022c0faba623c8d9171e1c004c2960e6bee865a
Lucille Sylvester <lsylvest@codeaurora.org>
2013-11-12 14:46:19 -0700

    msm: kgsl: Clean-up unneeded power code
    No platforms with direct ebi1 connections exist, nor
    is idling before changing the frequency needed.
    
    Change-Id: I80911c81ef7303e229494ce6ba3df57ee25b24a5
    Signed-off-by: Lucille Sylvester <lsylvest@codeaurora.org>

--- include/linux/msm_kgsl.h
+++ include/linux/msm_kgsl.h
@@ -73,7 +73,6 @@ struct kgsl_pwrlevel {
  * @strtstp_sleepwake:  Flag to decide b/w SLEEP and SLUMBER
  * @bus_control:	Flag if independent bus voting is supported
  * @clk_map:		Clocks map per platform
- * @idle_needed:        Flag to specify if idle needed before clock change
  * @bus_scale_table:	Bus table with different b/w votes
  * @iommu_data:		Struct holding iommu context data
  * @iommu_count:	Number of IOMMU units for the GPU
@@ -89,7 +88,6 @@ struct kgsl_device_platform_data {
 	bool strtstp_sleepwake;
 	bool bus_control;
 	unsigned int clk_map;
-	unsigned int idle_needed;
 	unsigned int step_mul;
 	struct msm_bus_scale_pdata *bus_scale_table;
 	struct kgsl_device_iommu_data *iommu_data;
