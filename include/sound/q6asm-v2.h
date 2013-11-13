6f952d5924a20d7b2fd11c502f7fa63b33cea785
Damir Didjusto <damird@codeaurora.org>
2013-11-13 14:28:12 -0800

    ASoC: msm: Add support for ASM loopback
    Add ASM loopback driver. When audio path wants to use filters in DSP,
    it will do a loopback in ASM.  The new ASM loopback driver enables
    the loopback.
    
    Change-Id: I84b81cab6c3b2e57855dd4b3bb5e7ed01618e4ec
    Signed-off-by: Damir Didjusto <damird@codeaurora.org>
    Signed-off-by: Vimal Puthanveed <vimalp@codeaurora.org>

--- include/sound/q6asm-v2.h
+++ include/sound/q6asm-v2.h
@@ -218,6 +218,9 @@ int q6asm_open_read_write(struct audio_client *ac,
 			uint32_t rd_format,
 			uint32_t wr_format);
 
+int q6asm_open_loopback_v2(struct audio_client *ac,
+			   uint16_t bits_per_sample);
+
 int q6asm_write(struct audio_client *ac, uint32_t len, uint32_t msw_ts,
 				uint32_t lsw_ts, uint32_t flags);
 int q6asm_write_nolock(struct audio_client *ac, uint32_t len, uint32_t msw_ts,
