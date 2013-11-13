dbcc41cac28aa977b357551d74264c026a614ae3
Terence Hampson <thampson@codeaurora.org>
2013-11-13 11:08:33 -0500

    msm: mdss: turn off the DSI clocks on command mode panel
    On command mode panel, panel is refreshing itself using
    its own internal memory. Shutting down the dsi clocks
    when there is no display update will save power.
    
    Change-Id: Ibce33bed8c5ed65581557089b6537b1a8d9428b4
    Signed-off-by: Xiaoming Zhou <zhoux@codeaurora.org>
    Signed-off-by: Terence Hampson <thampson@codeaurora.org>

--- drivers/video/msm/mdss/mdp3.c
+++ drivers/video/msm/mdss/mdp3.c
@@ -1735,6 +1735,11 @@ static int mdp3_fb_mem_get_iommu_domain(void)
 	return mdp3_res->domains[MDP3_DMA_IOMMU_DOMAIN].domain_idx;
 }
 
+int mdp3_get_cont_spash_en(void)
+{
+	return mdp3_res->cont_splash_en;
+}
+
 int mdp3_continuous_splash_copy(struct mdss_panel_data *pdata)
 {
 	unsigned long splash_phys;
@@ -1805,6 +1810,9 @@ static int mdp3_continuous_splash_on(struct mdss_panel_data *pdata)
 
 	pr_debug("mdp3__continuous_splash_on\n");
 
+	mdp3_clk_set_rate(MDP3_CLK_VSYNC, MDP_VSYNC_CLK_RATE,
+			MDP3_CLIENT_DMA_P);
+
 	rc = mdp3_clk_prepare();
 	if (rc) {
 		pr_err("fail to prepare clk\n");
@@ -1848,6 +1856,8 @@ static int mdp3_continuous_splash_on(struct mdss_panel_data *pdata)
 		mdp3_res->intf[MDP3_DMA_OUTPUT_SEL_DSI_VIDEO].active = 1;
 	else
 		mdp3_res->intf[MDP3_DMA_OUTPUT_SEL_DSI_CMD].active = 1;
+
+	mdp3_res->cont_splash_en = 1;
 	return 0;
 
 splash_on_err:
