c8fe13d6c18c421ebed5a228414f44eadada1a8c
Jeff Johnson <jjohnson@codeaurora.org>
2013-11-14 14:20:09 -0800

    wcnss: Fix mismatch between format and argument in pr_err()
    Static source analysis identified an instance where an incorrect
    format string was used to output a size_t value, so fix the format.
    
    Change-Id: Ibe0f2553d9b10d16b07854d7ddee229ea29ed4c9
    CRs-fixed: 575729
    Signed-off-by: Jeff Johnson <jjohnson@codeaurora.org>

--- drivers/net/wireless/wcnss/wcnss_wlan.c
+++ drivers/net/wireless/wcnss/wcnss_wlan.c
@@ -2110,7 +2110,7 @@ static ssize_t wcnss_wlan_write(struct file *fp, const char __user
 
 	if ((UINT32_MAX - count < penv->user_cal_rcvd) ||
 	     MAX_CALIBRATED_DATA_SIZE < count + penv->user_cal_rcvd) {
-		pr_err(DEVICE " invalid size to write %d\n", count +
+		pr_err(DEVICE " invalid size to write %zu\n", count +
 				penv->user_cal_rcvd);
 		rc = -ENOMEM;
 		goto exit;
