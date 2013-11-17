77d00b423c44a0be8094b3139a3bc238330e1248
Eugene Yasman <eyasman@codeaurora.org>
2013-11-17 20:37:49 +0200

    msm: socinfo: Add board identification function for SBC
    Add support to recognize the SBC board based on the device
    tree compatible string.
    
    Change-Id: I6a68858ecebc34858e7365855d45d3f573830b88
    Signed-off-by: Eugene Yasman <eyasman@codeaurora.org>

--- arch/arm/mach-msm/include/mach/socinfo.h
+++ arch/arm/mach-msm/include/mach/socinfo.h
@@ -44,6 +44,7 @@
 #define of_board_is_qrd()	of_machine_is_compatible("qcom,qrd")
 #define of_board_is_xpm()	of_machine_is_compatible("qcom,xpm")
 #define of_board_is_skuf()	of_machine_is_compatible("qcom,skuf")
+#define of_board_is_sbc()	of_machine_is_compatible("qcom,sbc")
 
 #define machine_is_msm8974()	of_machine_is_compatible("qcom,msm8974")
 #define machine_is_msm9625()	of_machine_is_compatible("qcom,msm9625")
@@ -75,6 +76,7 @@
 #define of_board_is_qrd()		0
 #define of_board_is_xpm()		0
 #define of_board_is_skuf()		0
+#define of_board_is_sbc()		0
 
 #define machine_is_msm8974()		0
 #define machine_is_msm9625()		0
