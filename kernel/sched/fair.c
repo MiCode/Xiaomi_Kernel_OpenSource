04ca28640c9b19c2b5d7ac9e845d633e5ed86898
Steve Muckle <smuckle@codeaurora.org>
2013-11-19 14:16:53 -0800

    tracing/sched: add load balancer tracepoint
    When doing performance analysis it can be useful to see exactly
    what is going on with the load balancer - when it runs and why
    exactly it may not be redistributing load.
    
    This additional tracepoint will show the idle context of the
    load balance operation (idle, not idle, newly idle), various
    values from the load balancing operation, the final result,
    and the new balance interval.
    
    Change-Id: I9e5c97ae3878bea44e60d189ff3cec2275f2c75e
    Signed-off-by: Steve Muckle <smuckle@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4422,7 +4422,7 @@ static int load_balance(int this_cpu, struct rq *this_rq,
 	int ld_moved, active_balance = 0;
 	struct sched_group *group;
 	unsigned long imbalance;
-	struct rq *busiest;
+	struct rq *busiest = NULL;
 	unsigned long flags;
 	struct cpumask *cpus = __get_cpu_var(load_balance_tmpmask);
 
@@ -4591,6 +4591,10 @@ out_one_pinned:
 
 	ld_moved = 0;
 out:
+	trace_sched_load_balance(this_cpu, idle, *balance,
+				 group ? group->cpumask[0] : 0,
+				 busiest ? busiest->nr_running : 0, imbalance,
+				 env.flags, ld_moved, sd->balance_interval);
 	return ld_moved;
 }
 
