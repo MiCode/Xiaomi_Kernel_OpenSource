fb0a61fd78bfc8e5abd2333fcff167b78f7302f4
Jeff Hugo <jhugo@codeaurora.org>
2013-11-20 14:15:31 -0700

    msm: timer: Use smem_find_to_proc()
    smem_find() is deprecated.  Use the replacement function,
    smem_find_to_proc(), instead.
    
    Change-Id: Ib656a6b4ee218c0cde86108dd5c779d0c90d3d65
    Signed-off-by: Jeffrey Hugo <jhugo@codeaurora.org>

--- arch/arm/mach-msm/timer.c
+++ arch/arm/mach-msm/timer.c
@@ -481,7 +481,8 @@ static uint32_t msm_timer_do_sync_to_sclk(
 	uint32_t smem_clock_val;
 	uint32_t state;
 
-	smem_clock = smem_find(SMEM_SMEM_SLOW_CLOCK_VALUE, sizeof(uint32_t));
+	smem_clock = smem_find_to_proc(SMEM_SMEM_SLOW_CLOCK_VALUE,
+				sizeof(uint32_t), 0, SMEM_ANY_HOST_FLAG);
 	if (smem_clock == NULL) {
 		printk(KERN_ERR "no smem clock\n");
 		return 0;
@@ -556,8 +557,8 @@ static uint32_t msm_timer_do_sync_to_sclk(
 	uint32_t last_state;
 	uint32_t state;
 
-	smem_clock = smem_find(SMEM_SMEM_SLOW_CLOCK_VALUE,
-				sizeof(uint32_t));
+	smem_clock = smem_find_to_proc(SMEM_SMEM_SLOW_CLOCK_VALUE,
+				sizeof(uint32_t), 0, SMEM_ANY_HOST_FLAG);
 
 	if (smem_clock == NULL) {
 		printk(KERN_ERR "no smem clock\n");
