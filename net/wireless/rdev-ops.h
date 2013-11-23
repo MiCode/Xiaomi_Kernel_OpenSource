dc5f42725d2f4d480eaf1703031596fa12c105d3
Ganesh Kondabattini <ganeshk@codeaurora.org>
2013-11-23 11:05:56 +0530

    Revert "cfg80211/mac80211: disconnect on suspend"
    This reverts commit 8125696991194aacb1173b6e8196d19098b44e17.
    
    The commit"cfg80211/mac80211: disconnect on suspend" forces disconnection
    on wlan interfaces if wake on wireless is not enabled from the user space.
    Wake on Wireless is enabled by default in wlan driver (CLD) and cfg80211
    layer never aware of wake on wirless enabled status done in wlan driver.
    
    To avoid the disconnection while going to suspend and keep wlan driver
    in WOW mode by default, the commit "cfg80211/mac80211: disconnect on
    suspend" should be reverted.
    
    CRs-Fixed: 540571
    Change-Id: I483fe0530f9f00c338680416449215af326e3df1
    Signed-off-by: Ganesh Kondabattini <ganeshk@codeaurora.org>

--- net/wireless/rdev-ops.h
+++ net/wireless/rdev-ops.h
@@ -6,12 +6,11 @@
 #include "core.h"
 #include "trace.h"
 
-static inline int rdev_suspend(struct cfg80211_registered_device *rdev,
-			       struct cfg80211_wowlan *wowlan)
+static inline int rdev_suspend(struct cfg80211_registered_device *rdev)
 {
 	int ret;
-	trace_rdev_suspend(&rdev->wiphy, wowlan);
-	ret = rdev->ops->suspend(&rdev->wiphy, wowlan);
+	trace_rdev_suspend(&rdev->wiphy, rdev->wowlan);
+	ret = rdev->ops->suspend(&rdev->wiphy, rdev->wowlan);
 	trace_rdev_return_int(&rdev->wiphy, ret);
 	return ret;
 }
