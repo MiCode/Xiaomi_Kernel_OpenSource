96eff2f73f3773a285faaf131f29243402c01e24
Mayank Chopra <makchopra@codeaurora.org>
2013-11-26 20:42:11 +0530

    msm: mdss: Check extra mmb allocated to a pipe
    If set call to a pipe changes smp configuration such that
    calculated no. of  mmb blocks are less than allocated mmb
    blocks, fail this set call. This ensures pipe disconnection
    and smp configuration is possible in next commit cycle.
    Also, check that no extra mmb is left allocated to any plane
    when pipe's format changes. If so, fail the set call to let
    correct smp configuration happen in next commit cycle.
    
    CRs-Fixed: 592505
    Change-Id: Iec2b8cb18088587655cf0c68dc81e3ac7b819cbc
    Signed-off-by: Mayank Chopra <makchopra@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -589,12 +589,6 @@ static int mdss_mdp_overlay_pipe_setup(struct msm_fb_data_type *mfd,
 		!mdp5_data->mdata->has_wfd_blk)
 		mdss_mdp_smp_release(pipe);
 
-	/*
-	 * Clear previous SMP reservations and reserve according to the
-	 * latest configuration
-	 */
-	mdss_mdp_smp_unreserve(pipe);
-
 	ret = mdss_mdp_smp_reserve(pipe);
 	if (ret) {
 		pr_debug("mdss_mdp_smp_reserve failed. ret=%d\n", ret);
