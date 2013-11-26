c8e3bd5a3d9d475fa699a38037fa363260475c3a
Yue Ma <yuem@codeaurora.org>
2013-11-26 15:32:34 -0800

    ARM: dts: msm: Rename CNSS platform driver device tree name
    Rename the device tree name of CNSS platform driver from qcom.cnss
    to qti.cnss. Also update the corresponding device tree documentation.
    
    Change-Id: I03c5256f9153ce47565dc146fb2e56fa99345f6f
    Signed-off-by: Yue Ma <yuem@codeaurora.org>

--- drivers/net/wireless/cnss/cnss.c
+++ drivers/net/wireless/cnss/cnss.c
@@ -616,7 +616,7 @@ static int cnss_remove(struct platform_device *pdev)
 }
 
 static const struct of_device_id cnss_dt_match[] = {
-	{.compatible = "qcom,cnss"},
+	{.compatible = "qti,cnss"},
 	{}
 };
 
