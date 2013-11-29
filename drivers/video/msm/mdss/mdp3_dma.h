1a1e097f006b248b580ae462b54f136ba21a6491
Terence Hampson <thampson@codeaurora.org>
2013-11-29 13:30:29 -0500

    msm: mdp3: implement asynchronous frame events
    Asynchronous frame events from MDP allows commit thread to be unblocked
    once the frame contents have been flushed to MDP HW and new frame can be
    programmed while waiting for DMA transfer to complete.
    
    Change-Id: If2ecc3cedfd92e20b2e98b0ac4f6f3608ce78579
    Signed-off-by: Terence Hampson <thampson@codeaurora.org>

--- drivers/video/msm/mdss/mdp3_dma.h
+++ drivers/video/msm/mdss/mdp3_dma.h
@@ -14,6 +14,7 @@
 #ifndef MDP3_DMA_H
 #define MDP3_DMA_H
 
+#include <linux/notifier.h>
 #include <linux/sched.h>
 
 #define MDP_HISTOGRAM_BL_SCALE_MAX 1024
@@ -227,7 +228,7 @@ struct mdp3_dma_histogram_data {
 	u32 extra[2];
 };
 
-struct mdp3_vsync_notification {
+struct mdp3_notification {
 	void (*handler)(void *arg);
 	void *arg;
 };
@@ -245,7 +246,8 @@ struct mdp3_dma {
 	struct completion vsync_comp;
 	struct completion dma_comp;
 	struct completion histo_comp;
-	struct mdp3_vsync_notification vsync_client;
+	struct mdp3_notification vsync_client;
+	struct mdp3_notification dma_notifier_client;
 
 	struct mdp3_dma_output_config output_config;
 	struct mdp3_dma_source source_config;
@@ -291,7 +293,10 @@ struct mdp3_dma {
 	void (*config_stride)(struct mdp3_dma *dma, int stride);
 
 	void (*vsync_enable)(struct mdp3_dma *dma,
-			struct mdp3_vsync_notification *vsync_client);
+			struct mdp3_notification *vsync_client);
+
+	void (*dma_done_notifier)(struct mdp3_dma *dma,
+			struct mdp3_notification *dma_client);
 };
 
 struct mdp3_video_intf_cfg {
