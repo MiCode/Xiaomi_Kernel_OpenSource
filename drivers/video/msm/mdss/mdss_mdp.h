e8f9507cd07c2bf1cb121dacab65800f631988d2
Pawan Kumar <pavaku@codeaurora.org>
2013-12-02 12:19:20 +0530

    msm: mdss: Detach mdss iommu on static screen
    Detach mdss iommu on static screen and re-attach
    on next commit to allow full power collapse of MDSS.
    IOMMU driver keeps its vote for bus bandwidth for as
    long as the MDP driver is attached to the IOMMU.
    When MDP detaches from the IOMMU, IOMMU driver will
    remove its vote for bus bandwidth.
    
    Change-Id: I1e5c5c50bc157c3ebfc63e198e4608dad820a288
    Signed-off-by: Pawan Kumar <pavaku@codeaurora.org>
    Signed-off-by: Shruthi Krishna <skrish@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp.h
+++ drivers/video/msm/mdss/mdss_mdp.h
@@ -460,6 +460,7 @@ static inline u32 mdss_mdp_pingpong_read(struct mdss_mdp_mixer *mixer, u32 reg)
 
 irqreturn_t mdss_mdp_isr(int irq, void *ptr);
 int mdss_iommu_attach(struct mdss_data_type *mdata);
+int mdss_iommu_dettach(struct mdss_data_type *mdata);
 void mdss_mdp_irq_clear(struct mdss_data_type *mdata,
 		u32 intr_type, u32 intf_num);
 int mdss_mdp_irq_enable(u32 intr_type, u32 intf_num);
