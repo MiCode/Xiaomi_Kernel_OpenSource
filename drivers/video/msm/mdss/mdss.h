42cae43124e9bee1009cb8430b37b7d139beb438
Huaibin Yang <huaibiny@codeaurora.org>
2013-12-06 18:27:47 -0800

    msm: mdss: add mdp overlap and prefill bandwidth calculations
    This patch implements a set of new equations proposed by h/w team to
    calculate mdp bandwidth. MDP has bandwidth requests during both
    blanking and active periods. During blanking area, mdp needs to fill
    its internal buffers, such as latency buffer, outstanding buffer,
    pingpong buffer, etc. This bandwidth is called prefill bandwidth and
    it is determined by the total amount of bytes of buffers to be filled
    and the total amount of blanking time available. The bandwidth for the
    active peroid is called overlap bandwidth which is the max bandwidth
    request of all overlapped regions. The final vote from mdp would be
    the worst case of prefill bandwidth and overlap bandwidth.
    
    Change-Id: Ib83caf6a3e15c33a2cd71d14e8413b1d9049de29
    Signed-off-by: Huaibin Yang <huaibiny@codeaurora.org>

--- drivers/video/msm/mdss/mdss.h
+++ drivers/video/msm/mdss/mdss.h
@@ -136,7 +136,6 @@ struct mdss_data_type {
 
 	struct mdss_fudge_factor ab_factor;
 	struct mdss_fudge_factor ib_factor;
-	struct mdss_fudge_factor high_ib_factor;
 	struct mdss_fudge_factor clk_factor;
 
 	struct mdss_hw_settings *hw_settings;
