0d9a3e213f1263a958575e037422db6a9729c8d8
Xu Kai <kaixu@codeaurora.org>
2013-12-17 13:53:56 +0800

    pwm: qpnp: modify to use pwm_config_us() for some qcom drivers
    Kernel 3.10 defines pwm_config() to use nanosecond parameters, but
    now some qcom drivers still use pwm_config() to pass microsecond
    parameters. pwm-qpnp driver has already provided a new API called
    pwm_config_us() to support microsecond parameters, so we should
    modify to use pwm_config_us() instead of pwm_config() for these
    drivers.
    
    CRs-Fixed: 545809
    Change-Id: I222440bb7636972b9ae35eba175ddffc58095ce8
    Signed-off-by: Xu Kai <kaixu@codeaurora.org>

--- drivers/video/msm/mdss/mdss_edp.c
+++ drivers/video/msm/mdss/mdss_edp.c
@@ -23,7 +23,7 @@
 #include <linux/gpio.h>
 #include <linux/err.h>
 #include <linux/regulator/consumer.h>
-#include <linux/pwm.h>
+#include <linux/qpnp/pwm.h>
 #include <linux/clk.h>
 #include <linux/spinlock_types.h>
 #include <linux/kthread.h>
@@ -210,11 +210,11 @@ void mdss_edp_set_backlight(struct mdss_panel_data *pdata, u32 bl_level)
 		if (bl_level > bl_max)
 			bl_level = bl_max;
 
-		ret = pwm_config(edp_drv->bl_pwm,
+		ret = pwm_config_us(edp_drv->bl_pwm,
 				bl_level * edp_drv->pwm_period / bl_max,
 				edp_drv->pwm_period);
 		if (ret) {
-			pr_err("%s: pwm_config() failed err=%d.\n", __func__,
+			pr_err("%s: pwm_config_us() failed err=%d.\n", __func__,
 					ret);
 			return;
 		}
