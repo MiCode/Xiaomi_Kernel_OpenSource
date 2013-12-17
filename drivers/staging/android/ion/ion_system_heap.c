14d80bb850d3677f5cd56ff146b6430f3039f07c
John Stultz <john.stultz@linaro.org>
2013-12-16 21:07:52 -0800

    staging: ion: Fix possible null pointer dereference
    The kbuild test robot reported:
    
    drivers/staging/android/ion/ion_system_heap.c:122 alloc_largest_available() error: potential null dereference 'info'.  (kmalloc returns null)
    
    Where the pointer returned from kmalloc goes unchecked for failure.
    
    This patch checks the return for NULL, and reworks the logic, as
    suggested by Colin, so we allocate the page_info structure first.
    
    Change-Id: I3d2894d4f68c9a8e802304d3211f9670294d00c0
    Acked-by: Colin Cross <ccross@android.com>
    Cc: Android Kernel Team <kernel-team@android.com>
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Git-commit: 60e11dfc002962181fe4f0db54b33c65fee02c52
    Git-repo: http://android.googlesource.com/kernel/common/
    [mitchelh@codeaurora.org: some context differences since we were already
     checking for a NULL return value]
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- drivers/staging/android/ion/ion_system_heap.c
+++ drivers/staging/android/ion/ion_system_heap.c
@@ -118,6 +118,10 @@ static struct page_info *alloc_largest_available(struct ion_system_heap *heap,
 	int i;
 	bool from_pool;
 
+	info = kmalloc(sizeof(struct page_info), GFP_KERNEL);
+	if (!info)
+		return NULL;
+
 	for (i = 0; i < num_orders; i++) {
 		if (size < order_to_size(orders[i]))
 			continue;
@@ -128,14 +132,13 @@ static struct page_info *alloc_largest_available(struct ion_system_heap *heap,
 		if (!page)
 			continue;
 
-		info = kmalloc(sizeof(struct page_info), GFP_KERNEL);
-		if (info) {
-			info->page = page;
-			info->order = orders[i];
-			info->from_pool = from_pool;
-		}
+		info->page = page;
+		info->order = orders[i];
+		info->from_pool = from_pool;
 		return info;
 	}
+	kfree(info);
+
 	return NULL;
 }
 static unsigned int process_info(struct page_info *info,
