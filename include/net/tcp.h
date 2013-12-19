73cd2de5349064c2f0529ed59d80887b221cff6f
Ravi Joshi <ravij@codeaurora.org>
2013-12-18 16:14:15 -0800

    WLAN subsystem: Sysctl support for key TCP/IP parameters
    It has been observed that default values for some of key tcp/ip
    parameters are affecting the tput/performance of the system. Hence
    extending configuration capabilities to TCP/Ip stack through
    sysctl interface
    
    Change-Id: I4287e9103769535f43e0934bac08435a524ee6a4
    CRs-Fixed: 507581
    Signed-off-by: Ravi Joshi <ravij@codeaurora.org>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -141,6 +141,9 @@ extern void tcp_time_wait(struct sock *sk, int state, int timeo);
 						 * most likely due to retrans in 3WHS.
 						 */
 
+/* Number of full MSS to receive before Acking RFC2581 */
+#define TCP_DELACK_SEG          1
+
 #define TCP_RESOURCE_PROBE_INTERVAL ((unsigned)(HZ/2U)) /* Maximal interval between probes
 					                 * for local resources.
 					                 */
@@ -289,6 +292,11 @@ extern int sysctl_tcp_limit_output_bytes;
 extern int sysctl_tcp_challenge_ack_limit;
 
 extern atomic_long_t tcp_memory_allocated;
+
+/* sysctl variables for controlling various tcp parameters */
+extern int sysctl_tcp_delack_seg;
+extern int sysctl_tcp_use_userconfig;
+
 extern struct percpu_counter tcp_sockets_allocated;
 extern int tcp_memory_pressure;
 
@@ -385,6 +393,12 @@ extern void tcp_twsk_destructor(struct sock *sk);
 extern ssize_t tcp_splice_read(struct socket *sk, loff_t *ppos,
 			       struct pipe_inode_info *pipe, size_t len,
 			       unsigned int flags);
+
+/* sysctl master controller */
+extern int tcp_use_userconfig_sysctl_handler(struct ctl_table *, int,
+				void __user *, size_t *, loff_t *);
+extern int tcp_proc_delayed_ack_control(struct ctl_table *, int,
+				void __user *, size_t *, loff_t *);
 
 static inline void tcp_dec_quickack_mode(struct sock *sk,
 					 const unsigned int pkts)
