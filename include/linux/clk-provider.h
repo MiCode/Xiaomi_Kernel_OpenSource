68ac3411bc4beed89533ee50137e9a4c7cb0be97
Junjie Wu <junjiew@codeaurora.org>
2013-12-19 18:13:56 -0800

    clk: Remove dependency on CONFIG_COMMON_CLK
    Our clock framework is not compatible with COMMON_CLK upstream.
    Remove the COMMON_CLK selection in arm64 Kconfig and stub out
    of_clk_init.
    
    Change-Id: I633156063a532daaa840a62bcef7cb7c833f090c
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>
    [abhimany: resolved minor merge conflict]
    Signed-off-by: Abhimanyu Kapur <abhimany@codeaurora.org>

--- include/linux/clk-provider.h
+++ include/linux/clk-provider.h
@@ -655,6 +655,13 @@ static inline void clk_writel(u32 val, u32 __iomem *reg)
 struct dentry *clk_debugfs_add_file(struct clk *clk, char *name, umode_t mode,
 				void *data, const struct file_operations *fops);
 #endif
+#else
+struct of_device_id;
+
+static inline void __init of_clk_init(const struct of_device_id *matches)
+{
+	return;
+}
 
 #endif /* CONFIG_COMMON_CLK */
 #endif /* CLK_PROVIDER_H */
