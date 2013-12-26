ca39e441af3521e2fc046d04cefc6873988ef020
Zhen Kong <zkong@codeaurora.org>
2013-12-25 22:57:08 -0800

    qseecom: change key management apis
    Remove 2 seconds sleep if create_key or update_key fails, and
    this delay is now handled by TZ. change errno when maximum
    attempt to input password is reached.
    
    Change-Id: I14350219f43094d280ebc1e3717418285063448d
    Signed-off-by: Zhen Kong <zkong@codeaurora.org>

--- arch/arm/mach-msm/include/mach/qseecomi.h
+++ arch/arm/mach-msm/include/mach/qseecomi.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -27,6 +27,7 @@
 #define QSEOS_RESULT_FAIL_KS_ALREADY_DONE     -69
 #define QSEOS_RESULT_FAIL_KEY_ID_DNE          -70
 #define QSEOS_RESULT_FAIL_INCORRECT_PSWD      -71
+#define QSEOS_RESULT_FAIL_MAX_ATTEMPT         -72
 
 enum qseecom_command_scm_resp_type {
 	QSEOS_APP_ID = 0xEE01,
