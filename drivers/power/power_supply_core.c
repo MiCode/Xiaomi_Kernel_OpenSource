2a24bcf37152f0a58cc5ecc3d141486fdaf8ce64
Liu, Chuansheng <chuansheng.liu@intel.com>
2013-12-26 11:21:55 +0800

    power_supply: Removing the redundant calling power_supply_remove_triggers()
    BZ: 161223
    
    Removing the redundant calling power_supply_remove_triggers(), which may cause
    memory corruption due to freeing freed memory.
    
    Change-Id: I60f72e293ca9cc32235a360fb82d7e8b6599edad
    Signed-off-by: Liu, Chuansheng <chuansheng.liu@intel.com>

--- drivers/power/power_supply_core.c
+++ drivers/power/power_supply_core.c
@@ -696,7 +696,6 @@ void power_supply_unregister(struct power_supply *psy)
 {
 	cancel_work_sync(&psy->changed_work);
 	sysfs_remove_link(&psy->dev->kobj, "powers");
-	power_supply_remove_triggers(psy);
 	if (IS_CHARGER(psy))
 		power_supply_unregister_charger(psy);
 	power_supply_remove_triggers(psy);
