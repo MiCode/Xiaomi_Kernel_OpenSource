61c641f92254bbd0804b22b8d23daebad480efcc
Shivaramraje Nimbalkar Joshi <nimbalkars@codeaurora.org>
2013-12-27 15:09:43 -0800

    msm-camera: Update sensor mount angle and position
    Update sensor driver to add sensor mount angle and
    position as part of media entity flag for Camera
    MultiModule sensor sub-devices. This info is used
    by upper layers to get the info from media device
    entity insted of opening camera to reduce the launch
    latency.
    
    Change-Id: Ieba103d5ab9e74a3210261b5660f41867ef5c773
    Signed-off-by: Evgeniy Borisov <eborisov@codeaurora.org>

--- include/media/msm_cam_sensor.h
+++ include/media/msm_cam_sensor.h
@@ -246,16 +246,6 @@ enum cci_i2c_master_t {
 	MASTER_MAX,
 };
 
-struct msm_camera_sensor_slave_info {
-	char sensor_name[32];
-	char eeprom_name[32];
-	char actuator_name[32];
-	enum msm_sensor_camera_id_t camera_id;
-	uint16_t slave_addr;
-	enum msm_camera_i2c_reg_addr_type addr_type;
-	struct msm_sensor_id_info_t sensor_id_info;
-	struct msm_sensor_power_setting_array power_setting_array;
-};
 
 struct msm_camera_i2c_reg_array {
 	uint16_t reg_addr;
@@ -343,6 +333,7 @@ struct csi_lane_params_t {
 enum camb_position_t {
 	BACK_CAMERA_B,
 	FRONT_CAMERA_B,
+	INVALID_CAMERA_B,
 };
 
 struct msm_sensor_info_t {
@@ -366,7 +357,8 @@ struct camera_vreg_t {
 
 enum camerab_mode_t {
 	CAMERA_MODE_2D_B = (1<<0),
-	CAMERA_MODE_3D_B = (1<<1)
+	CAMERA_MODE_3D_B = (1<<1),
+	CAMERA_MODE_INVALID = (1<<2),
 };
 
 struct msm_sensor_init_params {
@@ -376,6 +368,19 @@ struct msm_sensor_init_params {
 	enum camb_position_t position;
 	/* sensor mount angle */
 	uint32_t            sensor_mount_angle;
+};
+
+struct msm_camera_sensor_slave_info {
+	char sensor_name[32];
+	char eeprom_name[32];
+	char actuator_name[32];
+	enum msm_sensor_camera_id_t camera_id;
+	uint16_t slave_addr;
+	enum msm_camera_i2c_reg_addr_type addr_type;
+	struct msm_sensor_id_info_t sensor_id_info;
+	struct msm_sensor_power_setting_array power_setting_array;
+	uint8_t  is_init_params_valid;
+	struct msm_sensor_init_params sensor_init_params;
 };
 
 struct sensorb_cfg_data {
