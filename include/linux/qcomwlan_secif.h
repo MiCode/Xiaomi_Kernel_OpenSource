3269589b0031de2ed9b14e80c8421c419c315e3a
Sameer Thalappil <sameert@codeaurora.org>
2013-12-28 19:18:59 -0800

    cnss: Implementation of 802.11w (BIP) algorithm
    Implementation of 802.11w (BIP) algorithm as defined in RFC 4493.
    This is an AES-CMAC 128 bit algorithm to validate the integrity
    of the protected broadcast/multicast frames.
    
    CRs-Fixed: 594426
    Change-Id: Ic7939945671b6a6a0a5077b0f90ad6f89e58b152
    Acked-by: Srinivas Girigowda <sgirigow@codeaurora.org>
    Signed-off-by: Sameer Thalappil <sameert@codeaurora.org>

--- include/linux/qcomwlan_secif.h
+++ include/linux/qcomwlan_secif.h
@@ -15,6 +15,8 @@
 
 #include <crypto/hash.h>
 
+#define CMAC_TLEN 8 /* CMAC TLen = 64 bits (8 octets) */
+
 /*
  * Prototypes for WLAN Security Interface Functions
  */
@@ -30,7 +32,10 @@ extern struct crypto_ablkcipher *
 wcnss_wlan_crypto_alloc_ablkcipher(const char *alg_name, u32 type, u32 mask);
 extern void wcnss_wlan_ablkcipher_request_free(struct ablkcipher_request *req);
 extern void wcnss_wlan_crypto_free_cipher(struct crypto_cipher *tfm);
+extern void wcnss_wlan_crypto_free_ablkcipher(struct crypto_ablkcipher *tfm);
 extern struct crypto_cipher *
 wcnss_wlan_crypto_alloc_cipher(const char *alg_name, u32 type, u32 mask);
+extern void wcnss_wlan_cmac_calc_mic(struct crypto_cipher *tfm, u8 *m,
+				u16 length, u8 *mac);
 
 #endif /* __QCOM_WLAN_SECIF_H__ */
