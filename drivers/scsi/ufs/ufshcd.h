042d7595048ec1ffed3b075df2c5b388f813bdad
Sahitya Tummala <stummala@codeaurora.org>
2014-01-02 09:23:11 +0530

    ARM: dts: Add freq-table-hz property for apq8084 UFS device
    Add freq-table-hz propery for UFS device to keep track of
    <min max> frequencies supported by UFS clocks.
    
    Change-Id: Ie4add67cae21ef956520e9a411e5f4dad7790961
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -228,6 +228,7 @@ struct debugfs_files {
  * @clk: clock node
  * @name: clock name
  * @max_freq: maximum frequency supported by the clock
+ * @min_freq: min frequency that can be used for clock scaling
  * @enabled: variable to check against multiple enable/disable
  */
 struct ufs_clk_info {
@@ -235,6 +236,7 @@ struct ufs_clk_info {
 	struct clk *clk;
 	const char *name;
 	u32 max_freq;
+	u32 min_freq;
 	bool enabled;
 };
 
