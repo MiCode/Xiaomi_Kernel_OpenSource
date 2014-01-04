4cc293a9b0da22461fe39fb8f643fad7e1a63825
Vinod Koul <vinod.koul@intel.com>
2014-01-04 16:59:13 +0530

    ALSA: compress: update struct snd_codec_desc for sample rate
    Now that we don't use SNDRV_PCM_RATE_xxx bit fields for sample rate, we need to
    change the description to an array for describing the sample rates supported by
    the sink/source
    
    Change-Id: I2dc6b4e48cccbc7a3da7207be42cf11502373572
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Git-commit: b8bab04829ab190f71921d4180bda438ba6124ae
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git
    Signed-off-by: Banajit Goswami <bgoswami@codeaurora.org>
    Signed-off-by: vivek mehta <mvivek@codeaurora.org>
    [fred@codeaurora.org: resolved context conflict in struct snd_codec_desc]
    [fred@codeaurora.org: added msm-compress-q6-v2.c to resolve compilation error]
    Signed-off-by: Fred Oh <fred@codeaurora.org>

--- include/uapi/sound/compress_params.h
+++ include/uapi/sound/compress_params.h
@@ -57,6 +57,7 @@
 #define MAX_NUM_CODECS 32
 #define MAX_NUM_CODEC_DESCRIPTORS 32
 #define MAX_NUM_BITRATES 32
+#define MAX_NUM_SAMPLE_RATES 32
 
 /* compressed TX */
 #define MAX_NUM_FRAMES_PER_BUFFER 1
@@ -383,7 +384,7 @@ union snd_codec_options {
 
 struct snd_codec_desc {
 	__u32 max_ch;
-	__u32 sample_rates;
+	__u32 sample_rates[MAX_NUM_SAMPLE_RATES];
 	__u32 bit_rate[MAX_NUM_BITRATES];
 	__u32 num_bitrates;
 	__u32 rate_control;
