2feed59c378606d2d1c013d0355be685f1d61a89
Vladimir Razgulin <vrazguli@codeaurora.org>
2014-01-10 15:29:11 -0700

    ARM: dt: apq8084: Add new bus bandwidth votes and turn BMC on
    Update the bus bandwidth votes to save power and turn BMC on for
    apq8084 target.
    
    Change-Id: Ie43acf6333764066e33faf887e27c68c989b0f3d
    Signed-off-by: Vladimir Razgulin <vrazguli@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -61,7 +61,7 @@ static struct devfreq_simple_ondemand_data adreno_ondemand_data = {
 
 static struct devfreq_msm_adreno_tz_data adreno_tz_data = {
 	.bus = {
-		.max = 450,
+		.max = 350,
 	},
 	.device_id = KGSL_DEVICE_3D0,
 };
