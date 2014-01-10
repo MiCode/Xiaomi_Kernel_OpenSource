5b831a192158ac71c5c05a6c3666ea5f0ef9dc07
Kamen Kirov <kkirov@codeaurora.org>
2014-01-10 12:17:56 +0200

    camera_v2: jpeg_10: Fix return value of MSM_JPEG_IOCTL_HW_CMD
    The ioctl MSM_JPEG_IOCTL_HW_CMD was returning -1,
    when using MSM_JPEG_HW_CMD_TYPE_WRITE due to incorrect input
    validation logic.
    
    Signed-off-by: Kamen Kirov <kkirov@codeaurora.org>
    Change-Id: Ib47973fe6e2c398ef11e6ec3179388a7823e0b3f

--- drivers/media/platform/msm/camera_v2/jpeg_10/msm_jpeg_sync.c
+++ drivers/media/platform/msm/camera_v2/jpeg_10/msm_jpeg_sync.c
@@ -722,6 +722,9 @@ int msm_jpeg_ioctl_hw_cmds(struct msm_jpeg_device *pgmn_dev,
 			kfree(hw_cmds_p);
 			return -EFAULT;
 		}
+	} else {
+		kfree(hw_cmds_p);
+		return is_copy_to_user;
 	}
 	kfree(hw_cmds_p);
 	return 0;
