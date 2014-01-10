fe8ac00db1bd84580043565a185f6f3d55bf6565
Shubhraprakash Das <sadas@codeaurora.org>
2014-01-09 22:30:56 -0800

    msm: kgsl: Increment the array size of IB to dump
    The array holding list of IB's to dump in snapshot was
    not incremented as a result of which IB's were missing in
    snapshot.
    
    Change-Id: Idbca6eebf817c9456f6d52aa3c2ea509e732e119
    CRs-Fixed: 596932
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>

--- drivers/gpu/msm/adreno_snapshot.c
+++ drivers/gpu/msm/adreno_snapshot.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -82,7 +82,7 @@ static void push_object(struct kgsl_device *device, int type,
 	objbuf[objbufptr].gpuaddr = gpuaddr;
 	objbuf[objbufptr].ptbase = ptbase;
 	objbuf[objbufptr].dwords = dwords;
-	objbuf[objbufptr].entry = entry;
+	objbuf[objbufptr++].entry = entry;
 }
 
 /*
