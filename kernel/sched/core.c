ca454d54c437dc6cb4ec1754ab1a46524b640194
Peter Zijlstra <peterz@infradead.org>
2014-01-15 16:33:20 +0100

    sched: Fix up scheduler syscall LTP fails
    Wu reported LTP failures:
    
      > ltp.sched_setparam02.1.TFAIL
      > ltp.sched_setparam02.2.TFAIL
      > ltp.sched_setparam02.3.TFAIL
      > ltp.sched_setparam03.1.TFAIL
    
    There were 2 things wrong; firstly __setscheduler() failed on
    sched_setparam()'s policy = -1, fix that by reading from p->policy in
    that case.
    
    Secondly, getparam() (and getattr()) would still report !0
    sched_priority for !FIFO/RR tasks after having been such. So
    unconditionally set p->rt_priority.
    
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@gmail.com>
    Cc: Dario Faggioli <raistlin@linux.it>
    Fixes: d50dde5a10f3 ("sched: Add new scheduler syscalls to support an extended scheduling parameters ABI")
    Link: http://lkml.kernel.org/r/20140115153320.GH31570@twins.programming.kicks-ass.net
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Git-commit: 39fd8fd22b3224ec6819d33b3e34ae4da6a35f05
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    [imaund@codeaurora.org: Resolve merge conflicts. We have not yet pulled
      DEADLINE support.]
    Signed-off-by: Ian Maund <imaund@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4010,12 +4010,20 @@ static void __setscheduler(struct rq *rq, struct task_struct *p,
 {
 	int policy = attr->sched_policy;
 
+	if (policy == -1) /* setparam */
+		policy = p->policy;
+
 	p->policy = policy;
 
-	if (rt_policy(policy))
-		p->rt_priority = attr->sched_priority;
-	else
+	if (fair_policy(policy))
 		p->static_prio = NICE_TO_PRIO(attr->sched_nice);
+
+	/*
+	 * __sched_setscheduler() ensures attr->sched_priority == 0 when
+	 * !rt_policy. Always setting this ensures that things like
+	 * getparam()/getattr() don't report silly values for !rt tasks.
+	 */
+	p->rt_priority = attr->sched_priority;
 
 	p->normal_prio = normal_prio(p);
 	p->prio = rt_mutex_getprio(p);
