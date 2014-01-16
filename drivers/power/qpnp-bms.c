d681c8e79c597a5e1ae79803dc0215d02df8fe34
Xu Kai <kaixu@codeaurora.org>
2014-01-16 19:21:01 +0800

    power: qpnp-bms: fix unbalanced IRQs in qpnp-bms.c
    In qpnp-bms.c there is a bug where IRQs are getting used
    before being registered. Hence, bms_request_irqs() needs
    to be moved before battery_insertion_check().
    
    CRs-fixed: 596174
    Change-Id: I74c2aa9e14495c22b3cdaa1a3d1f712f9b3cabf6
    Signed-off-by: Xu Kai <kaixu@codeaurora.org>

--- drivers/power/qpnp-bms.c
+++ drivers/power/qpnp-bms.c
@@ -4308,6 +4308,12 @@ static int qpnp_bms_probe(struct spmi_device *spmi)
 		goto error_setup;
 	}
 
+	rc = bms_request_irqs(chip);
+	if (rc) {
+		pr_err("error requesting bms irqs, rc = %d\n", rc);
+		goto error_setup;
+	}
+
 	battery_insertion_check(chip);
 	batfet_status_check(chip);
 	battery_status_check(chip);
@@ -4338,12 +4344,6 @@ static int qpnp_bms_probe(struct spmi_device *spmi)
 	if (rc) {
 		pr_err("error reading vbat_sns adc channel = %d, rc = %d\n",
 						VBAT_SNS, rc);
-		goto unregister_dc;
-	}
-
-	rc = bms_request_irqs(chip);
-	if (rc) {
-		pr_err("error requesting bms irqs, rc = %d\n", rc);
 		goto unregister_dc;
 	}
 
