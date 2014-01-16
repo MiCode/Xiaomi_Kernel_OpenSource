364c2967105881dc29f6572b9342c92c9684d50a
Himanshu Aggarwal <haggarwa@codeaurora.org>
2014-01-16 19:30:41 +0530

    input: synaptics_fw_update:  fix print statements
    %zu is the correct printk format specifier when printing size_t types.
    
    Update printks to use %zu for size_t type variables.
    
    This patch is propagated from the msm-3.10 kernel
    (commit: 1fd3845651a5d9451ac7568cb8bdba98bb8554ab
    input: synaptics_fw_update:  fix print statements)
    
    Change-Id: Ie556fd14293ecd36143353e575fd5ef22fab20f7
    Signed-off-by: Himanshu Aggarwal <haggarwa@codeaurora.org>

--- drivers/input/touchscreen/synaptics_fw_update.c
+++ drivers/input/touchscreen/synaptics_fw_update.c
@@ -1563,7 +1563,7 @@ static int fwu_start_reflash(void)
 		}
 
 		dev_dbg(&fwu->rmi4_data->i2c_client->dev,
-				"%s: Firmware image size = %zd\n",
+				"%s: Firmware image size = %zu\n",
 				__func__, fw_entry->size);
 
 		fwu->data_buffer = fw_entry->data;
@@ -1674,7 +1674,7 @@ static ssize_t fwu_sysfs_show_image(struct file *data_file,
 
 	if (count < fwu->config_size) {
 		dev_err(&rmi4_data->i2c_client->dev,
-				"%s: Not enough space (%zd bytes) in buffer\n",
+				"%s: Not enough space (%zu bytes) in buffer\n",
 				__func__, count);
 		return -EINVAL;
 	}
