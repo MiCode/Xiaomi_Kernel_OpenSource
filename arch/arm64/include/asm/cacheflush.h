0930bab0db67cc0d91e52e385e3e061871c6be05
Larry Bassel <lbassel@codeaurora.org>
2014-01-17 10:33:08 -0800

    arm64: provide dma cache routines with same API as 32 bit
    The APIs __dma_inv_range() and __dma_clean_range() were
    not exported by the third party patch. Since the functions
    starting with underscores are not to be directly used by drivers,
    related functions without the underscores are provided
    which have the same name and functionality as the 32 bit APIs.
    
    Change-Id: Ie0e681614307d9d9a19e58cacfb9b5dff4528977
    Signed-off-by: Larry Bassel <lbassel@codeaurora.org>
    
    arm64: add defines for dmac_*_range for compatibility with arm32
    
    An earlier patch created defines for dma_*_range APIs to be
    compatible with arm 32 bit, however it appears
    these API names have not (at least yet) appeared there, so
    revise the names to dmac_*_range, which is defined
    for arm 32 bit so that there is one name defined
    for both architectures.
    
    Change-Id: I6456c02bad73fb54a874dc9925d3d43d9b8be2f2
    Signed-off-by: Larry Bassel <lbassel@codeaurora.org>

--- arch/arm64/include/asm/cacheflush.h
+++ arch/arm64/include/asm/cacheflush.h
@@ -90,6 +90,12 @@ static inline void flush_cache_page(struct vm_area_struct *vma,
 extern void __dma_map_area(const void *, size_t, int);
 extern void __dma_unmap_area(const void *, size_t, int);
 extern void __dma_flush_range(const void *, const void *);
+extern void __dma_inv_range(const void *, const void *);
+extern void __dma_clean_range(const void *, const void *);
+
+#define dmac_flush_range __dma_flush_range
+#define dmac_inv_range __dma_inv_range
+#define dmac_clean_range __dma_clean_range
 
 /*
  * Copy user data from/to a page which is mapped into a different
