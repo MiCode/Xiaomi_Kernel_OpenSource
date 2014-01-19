d77ca58b85fc6f4247fd837fae2278a8539f0a21
SpaceKiller <alexisalbo.1@gmail.com>
2014-01-19 18:42:10 -0300

    drivers: platform: msm: qpnp-vibrator: Add sysfs interface for intensity
    To control the Haptic intensity using sysfs interface.
    
    Signed-off-by: SpaceKiller <alexisalbo.1@gmail.com>
    
    Fix vibrator sysfs
    
    Change-Id: Ic33ab91b35039bb3ca9f42949dfffdb34b6276b1
    
    Conflicts:
    	drivers/platform/msm/qpnp-vibrator.c

--- drivers/platform/msm/qpnp-vibrator.c
+++ drivers/platform/msm/qpnp-vibrator.c
@@ -10,6 +10,7 @@
  * GNU General Public License for more details.
  */
 
+#include <linux/device.h>
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
@@ -140,6 +141,56 @@ static int qpnp_vibrator_config(struct qpnp_vib *vib)
 
 	return rc;
 }
+
+static ssize_t qpnp_vib_level_show(struct device *dev,
+                                        struct device_attribute *attr,
+                                        char *buf)
+{
+        struct timed_output_dev *tdev = dev_get_drvdata(dev);
+        struct qpnp_vib *vib = container_of(tdev, struct qpnp_vib,
+                                         timed_dev);
+
+        return scnprintf(buf, PAGE_SIZE, "%d\n", vib->vtg_level);
+}
+
+static ssize_t qpnp_vib_level_store(struct device *dev,
+                                        struct device_attribute *attr,
+                                        const char *buf, size_t count)
+{
+        struct timed_output_dev *tdev = dev_get_drvdata(dev);
+        struct qpnp_vib *vib = container_of(tdev, struct qpnp_vib,
+                                         timed_dev);
+        int val;
+        int rc;
+
+        rc = kstrtoint(buf, 10, &val);
+        if (rc) {
+                pr_err("%s: error getting level\n", __func__);
+                return -EINVAL;
+        }
+
+        if (val < QPNP_VIB_MIN_LEVEL) {
+                pr_err("%s: level %d not in range (%d - %d), using min.", __func__, val, QPNP_VIB_MIN_LEVEL, QPNP_VIB_MAX_LEVEL);
+                val = QPNP_VIB_MIN_LEVEL;
+        } else if (val > QPNP_VIB_MAX_LEVEL) {
+                pr_err("%s: level %d not in range (%d - %d), using max.", __func__, val, QPNP_VIB_MIN_LEVEL, QPNP_VIB_MAX_LEVEL);
+                val = QPNP_VIB_MAX_LEVEL;
+        }
+
+        vib->vtg_level = val;
+
+	rc = qpnp_vibrator_config(vib);
+	if (rc) {
+		dev_err(&vib->spmi->dev, "vib config failed\n");
+		return rc;
+	}
+
+        return strnlen(buf, count);
+}
+
+static DEVICE_ATTR(vtg_level, S_IRUGO | S_IWUSR, qpnp_vib_level_show, qpnp_vib_level_store);
+
+
 
 static int qpnp_vib_set(struct qpnp_vib *vib, int on)
 {
@@ -371,6 +422,8 @@ static int qpnp_vibrator_probe(struct spmi_device *spmi)
 	rc = timed_output_dev_register(&vib->timed_dev);
 	if (rc < 0)
 		return rc;
+
+        device_create_file(vib->timed_dev.dev, &dev_attr_vtg_level);
 
 	return rc;
 }
