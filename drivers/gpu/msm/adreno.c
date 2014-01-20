fac5c94c573534b4220a0d8f618b44512dec6e68
Prakash Kamliya <pkamliya@codeaurora.org>
2014-01-21 00:29:34 +0530

    msm: kgsl: get pm_qos_latency from device tree
    pm_qos_latency parameter can be configured from
    device tree as well. Will set to default value if
    entry not found in target device tree.
    
    Conflicts:
    	include/linux/msm_kgsl.h
    
    Change-Id: I76271f4fdc26e191431e5dfe2a6de9e2b3ab426f
    Signed-off-by: Prakash Kamliya <pkamliya@codeaurora.org>
    Signed-off-by: Sunil Khatri <sunilkh@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1690,6 +1690,12 @@ static int adreno_of_get_pdata(struct platform_device *pdev)
 	if (ret)
 		goto err;
 
+	/* get pm_qos from target, set it to default if not found */
+	if (adreno_of_read_property(pdev->dev.of_node, "qcom,pm_qos_latency",
+		&pdata->pm_qos_latency))
+		pdata->pm_qos_latency = 501;
+
+
 	if (adreno_of_read_property(pdev->dev.of_node, "qcom,idle-timeout",
 		&pdata->idle_timeout))
 		pdata->idle_timeout = HZ/12;
