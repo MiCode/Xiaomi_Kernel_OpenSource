b98be7eb714c7ccd37f122c37a42ac1b7c6604b2
Saravana Kannan <skannan@codeaurora.org>
2014-01-20 12:16:29 -0800

    devfreq: governor_cpubw_hwmon: Fix use of decay_rate
    The decay rate tunable is used to compute the current averaged bandwidth
    use, but the calculated value is never used. Fix this by using the
    calculated averaged bandwidth instead of the instantaneous measured
    bandwidth when computing the AB and IB votes.
    
    Change-Id: Ic0ba3ce0d98f0ec185349fe6d86c4af26f222ff2
    Signed-off-by: Saravana Kannan <skannan@codeaurora.org>

--- drivers/devfreq/governor_cpubw_hwmon.c
+++ drivers/devfreq/governor_cpubw_hwmon.c
@@ -246,8 +246,9 @@ static void compute_bw(int mbps, unsigned long *freq, unsigned long *ab)
 		new_bw /= 100;
 	}
 
-	*ab = roundup(mbps, bw_step);
-	*freq = (mbps * 100) / io_percent;
+	prev_ab = new_bw;
+	*ab = roundup(new_bw, bw_step);
+	*freq = (new_bw * 100) / io_percent;
 }
 
 #define TOO_SOON_US	(1 * USEC_PER_MSEC)
@@ -340,7 +341,6 @@ static int devfreq_cpubw_hwmon_get_freq(struct devfreq *df,
 
 	mbps = measure_bw_and_set_irq();
 	compute_bw(mbps, freq, df->data);
-	prev_ab = *(unsigned long *) df->data;
 
 	return 0;
 }
