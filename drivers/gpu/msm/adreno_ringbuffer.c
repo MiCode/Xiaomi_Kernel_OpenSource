73cefe2eb7a36ba27131ca4e0ccaa54e4da32b71
Lynus Vaz <lvaz@codeaurora.org>
2014-01-20 18:17:39 +0530

    msm: kgsl: Fix the NULL context submission size
    When submitting commands with a NULL context the size may not be
    calculated properly. Fix this calculation.
    
    Change-Id: Idcc37772349673da8a9b9d5127f88946ced8fff6
    Signed-off-by: Lynus Vaz <lvaz@codeaurora.org>

--- drivers/gpu/msm/adreno_ringbuffer.c
+++ drivers/gpu/msm/adreno_ringbuffer.c
@@ -665,7 +665,8 @@ adreno_ringbuffer_addcmds(struct adreno_ringbuffer *rb,
 	total_sizedwords += (flags & KGSL_CMD_FLAGS_INTERNAL_ISSUE) ? 2 : 0;
 
 	/* Add two dwords for the CP_INTERRUPT */
-	total_sizedwords += drawctxt ? 2 : 0;
+	total_sizedwords +=
+		(drawctxt || (flags & KGSL_CMD_FLAGS_INTERNAL_ISSUE)) ?  2 : 0;
 
 	if (adreno_is_a3xx(adreno_dev))
 		total_sizedwords += 7;
