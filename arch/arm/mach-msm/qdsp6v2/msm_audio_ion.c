11e633bdd39b9c0fcd89f74872ab202e08c6d816
Gopikrishnaiah Anandan <agopik@codeaurora.org>
2014-01-20 16:01:24 -0800

    ASoC: msm: qdsp6v2: Release IPA mapping
    When DSP has a SMMU associated with it, audio driver will allocate
    (KVA)kernel virtual address and (IPA)intermediate physical
    address.When freeing memory audio driver should release IPA and KVA
    mappings. Change fixes the issue where IPA mapping was not released
    
    Change-Id: Id81ef9f2af29f2b6793853d8d0a1ef960c5d6310
    CRs-fixed: 600938
    Signed-off-by: Gopikrishnaiah Anandan <agopik@codeaurora.org>

--- arch/arm/mach-msm/qdsp6v2/msm_audio_ion.c
+++ arch/arm/mach-msm/qdsp6v2/msm_audio_ion.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -391,7 +391,9 @@ err:
 int msm_audio_ion_free_legacy(struct ion_client *client,
 			      struct ion_handle *handle)
 {
-	/* To add condition for SMMU enabled */
+	if (msm_audio_ion_data.smmu_enabled)
+		ion_unmap_iommu(client, handle,
+		msm_audio_ion_data.domain_id, 0);
 	ion_unmap_kernel(client, handle);
 
 	ion_free(client, handle);
