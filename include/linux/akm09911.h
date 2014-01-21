88ffa250cdd598e4fa9b5dc50b355ca7365e58b4
xingxiao <xwings.nexus@gmail.com>
2014-01-22 00:59:01 +0800

    input:misc:add AKM ecompass linux driver
    The change to add linux drivers support for akm0991,akm8963 and akm8975.
    
    Change-Id: I0ecfac506f2c0c4724af1f0514f1a9cb47334004
    Signed-off-by: xingxiao <xwings.nexus@gmail.com>
    Git-commit: 694fa07b8274c8ab8d6cb4b8935567f4964c0089
    Git-repo: https://github.com/x-xwings/AKM_Kernel.git
    [mengmeng@codeaurora.org: cherry-picked from mirrored branch]
    Signed-off-by: Oliver Wang <mengmeng@codeaurora.org>

---
+++ include/linux/akm09911.h
@@ -0,0 +1,100 @@
+/*
+ * Definitions for akm09911 compass chip.
+ */
+#ifndef AKM09911_H
+#define AKM09911_H
+
+#include <linux/ioctl.h>
+
+/* Device specific constant values */
+#define AK09911_REG_WIA1			0x00
+#define AK09911_REG_WIA2			0x01
+#define AK09911_REG_INFO1			0x02
+#define AK09911_REG_INFO2			0x03
+#define AK09911_REG_ST1				0x10
+#define AK09911_REG_HXL				0x11
+#define AK09911_REG_HXH				0x12
+#define AK09911_REG_HYL				0x13
+#define AK09911_REG_HYH				0x14
+#define AK09911_REG_HZL				0x15
+#define AK09911_REG_HZH				0x16
+#define AK09911_REG_TMPS			0x17
+#define AK09911_REG_ST2				0x18
+#define AK09911_REG_CNTL1			0x30
+#define AK09911_REG_CNTL2			0x31
+#define AK09911_REG_CNTL3			0x32
+
+#define AK09911_FUSE_ASAX			0x60
+#define AK09911_FUSE_ASAY			0x61
+#define AK09911_FUSE_ASAZ			0x62
+
+#define AK09911_MODE_SNG_MEASURE	0x01
+#define AK09911_MODE_SELF_TEST		0x10
+#define AK09911_MODE_FUSE_ACCESS	0x1F
+#define AK09911_MODE_POWERDOWN		0x00
+#define AK09911_RESET_DATA			0x01
+
+#define AK09911_REGS_SIZE		13
+#define AK09911_WIA1_VALUE		0x48
+#define AK09911_WIA2_VALUE		0x05
+
+/* To avoid device dependency, convert to general name */
+#define AKM_I2C_NAME			"akm09911"
+#define AKM_MISCDEV_NAME		"akm09911_dev"
+#define AKM_SYSCLS_NAME			"compass"
+#define AKM_SYSDEV_NAME			"akm09911"
+#define AKM_REG_MODE			AK09911_REG_CNTL2
+#define AKM_REG_RESET			AK09911_REG_CNTL3
+#define AKM_REG_STATUS			AK09911_REG_ST1
+#define AKM_MEASURE_TIME_US		10000
+#define AKM_DRDY_IS_HIGH(x)		((x) & 0x01)
+#define AKM_SENSOR_INFO_SIZE	2
+#define AKM_SENSOR_CONF_SIZE	3
+#define AKM_SENSOR_DATA_SIZE	9
+
+#define AKM_YPR_DATA_SIZE		16
+#define AKM_RWBUF_SIZE			16
+#define AKM_REGS_SIZE			AK09911_REGS_SIZE
+#define AKM_REGS_1ST_ADDR		AK09911_REG_WIA1
+#define AKM_FUSE_1ST_ADDR		AK09911_FUSE_ASAX
+
+#define AKM_MODE_SNG_MEASURE	AK09911_MODE_SNG_MEASURE
+#define AKM_MODE_SELF_TEST		AK09911_MODE_SELF_TEST
+#define AKM_MODE_FUSE_ACCESS	AK09911_MODE_FUSE_ACCESS
+#define AKM_MODE_POWERDOWN		AK09911_MODE_POWERDOWN
+#define AKM_RESET_DATA			AK09911_RESET_DATA
+
+#define ACC_DATA_FLAG		0
+#define MAG_DATA_FLAG		1
+#define FUSION_DATA_FLAG	2
+#define AKM_NUM_SENSORS		3
+
+#define ACC_DATA_READY		(1<<(ACC_DATA_FLAG))
+#define MAG_DATA_READY		(1<<(MAG_DATA_FLAG))
+#define FUSION_DATA_READY	(1<<(FUSION_DATA_FLAG))
+
+#define AKMIO				0xA1
+
+/* IOCTLs for AKM library */
+#define ECS_IOCTL_READ				_IOWR(AKMIO, 0x01, char)
+#define ECS_IOCTL_WRITE				_IOW(AKMIO, 0x02, char)
+#define ECS_IOCTL_RESET				_IO(AKMIO, 0x03)
+#define ECS_IOCTL_SET_MODE			_IOW(AKMIO, 0x10, char)
+#define ECS_IOCTL_SET_YPR			_IOW(AKMIO, 0x11, int[AKM_YPR_DATA_SIZE])
+#define ECS_IOCTL_GET_INFO			_IOR(AKMIO, 0x20, unsigned char[AKM_SENSOR_INFO_SIZE])
+#define ECS_IOCTL_GET_CONF			_IOR(AKMIO, 0x21, unsigned char[AKM_SENSOR_CONF_SIZE])
+#define ECS_IOCTL_GET_DATA			_IOR(AKMIO, 0x22, unsigned char[AKM_SENSOR_DATA_SIZE])
+#define ECS_IOCTL_GET_OPEN_STATUS	_IOR(AKMIO, 0x23, int)
+#define ECS_IOCTL_GET_CLOSE_STATUS	_IOR(AKMIO, 0x24, int)
+#define ECS_IOCTL_GET_DELAY			_IOR(AKMIO, 0x25, long long int)
+#define ECS_IOCTL_GET_LAYOUT		_IOR(AKMIO, 0x26, char)
+#define ECS_IOCTL_GET_ACCEL			_IOR(AKMIO, 0x30, short[3])
+
+struct akm09911_platform_data {
+	char layout;
+	int gpio_DRDY;
+	int gpio_RSTN;
+};
+
+#endif
+
---
+++ include/linux/akm8975.h
@@ -0,0 +1,99 @@
+/*
+ * Definitions for akm8975 compass chip.
+ */
+#ifndef AKM8975_H
+#define AKM8975_H
+
+#include <linux/ioctl.h>
+
+/* Device specific constant values */
+#define AK8975_REG_WIA		0x00
+#define AK8975_REG_INFO		0x01
+#define AK8975_REG_ST1		0x02
+#define AK8975_REG_HXL		0x03
+#define AK8975_REG_HXH		0x04
+#define AK8975_REG_HYL		0x05
+#define AK8975_REG_HYH		0x06
+#define AK8975_REG_HZL		0x07
+#define AK8975_REG_HZH		0x08
+#define AK8975_REG_ST2		0x09
+#define AK8975_REG_CNTL		0x0A
+#define AK8975_REG_RSV		0x0B
+#define AK8975_REG_ASTC		0x0C
+#define AK8975_REG_TS1		0x0D
+#define AK8975_REG_TS2		0x0E
+#define AK8975_REG_I2CDIS	0x0F
+
+#define AK8975_FUSE_ASAX	0x10
+#define AK8975_FUSE_ASAY	0x11
+#define AK8975_FUSE_ASAZ	0x12
+
+#define AK8975_MODE_SNG_MEASURE		0x01
+#define AK8975_MODE_SELF_TEST		0x08
+#define AK8975_MODE_FUSE_ACCESS		0x0F
+#define AK8975_MODE_POWERDOWN		0x00
+#define AK8975_RESET_DATA			0x00
+
+#define AK8975_REGS_SIZE		13
+#define AK8975_WIA_VALUE		0x48
+
+/* To avoid device dependency, convert to general name */
+#define AKM_I2C_NAME			"akm8975"
+#define AKM_MISCDEV_NAME		"akm8975_dev"
+#define AKM_SYSCLS_NAME			"compass"
+#define AKM_SYSDEV_NAME			"akm8975"
+#define AKM_REG_MODE			AK8975_REG_CNTL
+#define AKM_REG_RESET			0
+#define AKM_REG_STATUS			AK8975_REG_ST1
+#define AKM_MEASURE_TIME_US		10000
+#define AKM_DRDY_IS_HIGH(x)		((x) & 0x01)
+#define AKM_SENSOR_INFO_SIZE	2
+#define AKM_SENSOR_CONF_SIZE	3
+#define AKM_SENSOR_DATA_SIZE	8
+
+#define AKM_YPR_DATA_SIZE		16
+#define AKM_RWBUF_SIZE			16
+#define AKM_REGS_SIZE			AK8975_REGS_SIZE
+#define AKM_REGS_1ST_ADDR		AK8975_REG_WIA
+#define AKM_FUSE_1ST_ADDR		AK8975_FUSE_ASAX
+
+#define AKM_MODE_SNG_MEASURE	AK8975_MODE_SNG_MEASURE
+#define AKM_MODE_SELF_TEST		AK8975_MODE_SELF_TEST
+#define AKM_MODE_FUSE_ACCESS	AK8975_MODE_FUSE_ACCESS
+#define AKM_MODE_POWERDOWN		AK8975_MODE_POWERDOWN
+#define AKM_RESET_DATA			AK8975_RESET_DATA
+
+#define ACC_DATA_FLAG		0
+#define MAG_DATA_FLAG		1
+#define FUSION_DATA_FLAG	2
+#define AKM_NUM_SENSORS		3
+
+#define ACC_DATA_READY		(1<<(ACC_DATA_FLAG))
+#define MAG_DATA_READY		(1<<(MAG_DATA_FLAG))
+#define FUSION_DATA_READY	(1<<(FUSION_DATA_FLAG))
+
+#define AKMIO				0xA1
+
+/* IOCTLs for AKM library */
+#define ECS_IOCTL_READ				_IOWR(AKMIO, 0x01, char)
+#define ECS_IOCTL_WRITE				_IOW(AKMIO, 0x02, char)
+#define ECS_IOCTL_RESET				_IO(AKMIO, 0x03)
+#define ECS_IOCTL_SET_MODE			_IOW(AKMIO, 0x10, char)
+#define ECS_IOCTL_SET_YPR			_IOW(AKMIO, 0x11, int[AKM_YPR_DATA_SIZE])
+#define ECS_IOCTL_GET_INFO			_IOR(AKMIO, 0x20, unsigned char[AKM_SENSOR_INFO_SIZE])
+#define ECS_IOCTL_GET_CONF			_IOR(AKMIO, 0x21, unsigned char[AKM_SENSOR_CONF_SIZE])
+#define ECS_IOCTL_GET_DATA			_IOR(AKMIO, 0x22, unsigned char[AKM_SENSOR_DATA_SIZE])
+#define ECS_IOCTL_GET_OPEN_STATUS	_IOR(AKMIO, 0x23, int)
+#define ECS_IOCTL_GET_CLOSE_STATUS	_IOR(AKMIO, 0x24, int)
+#define ECS_IOCTL_GET_DELAY			_IOR(AKMIO, 0x25, long long int)
+#define ECS_IOCTL_GET_LAYOUT		_IOR(AKMIO, 0x26, char)
+#define ECS_IOCTL_GET_ACCEL			_IOR(AKMIO, 0x30, short[3])
+
+struct akm8975_platform_data {
+	char layout;
+	int gpio_DRDY;
+	int gpio_RSTN;
+};
+
+#endif
+
