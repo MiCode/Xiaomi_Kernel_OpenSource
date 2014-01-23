5ee897c267e58b304afb32e7d04c8e3a8ac1063c
Raviv Shvili <rshvili@codeaurora.org>
2014-01-23 16:00:01 +0200

    linux: irqchip: fix compilation issue
    Compilation error due to missing comma at function definition.
    
    Change-Id: If842fab71b811088a8ab961bb8cc3b57d830c20a
    Signed-off-by: Raviv Shvili <rshvili@codeaurora.org>

--- include/linux/irqchip/msm-mpm-irq.h
+++ include/linux/irqchip/msm-mpm-irq.h
@@ -140,7 +140,7 @@ static inline bool msm_mpm_irqs_detectable(bool from_idle)
 { return false; }
 static inline bool msm_mpm_gpio_irqs_detectable(bool from_idle)
 { return false; }
-static inline void msm_mpm_enter_sleep(uint32_t sclk_count, bool from_idle
+static inline void msm_mpm_enter_sleep(uint32_t sclk_count, bool from_idle,
 		const struct cpumask *cpumask) {}
 static inline void msm_mpm_exit_sleep(bool from_idle) {}
 static inline void __init of_mpm_init(void) {}
