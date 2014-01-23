9fdc6d08d75336780f9bb52d13556e9e23d02139
Anirudh Ghayal <aghayal@codeaurora.org>
2014-01-24 00:45:58 +0530

    power: qpnp-vm-bms: Add QPNP voltage mode(VM) BMS driver
    The voltage-mode BMS driver uses periodic VBATT readings
    to calculate the state of charge(SOC) of the battery.
    The final SOC is calculated by an userspace application
    after post-processing the VBATT readings.
    
    Change-Id: I3c19e8dc7f1952c4e8274209d55aad176d90a180
    Signed-off-by: Anirudh Ghayal <aghayal@codeaurora.org>

--- include/linux/batterydata-lib.h
+++ include/linux/batterydata-lib.h
@@ -130,7 +130,8 @@ struct bms_battery_data {
 
 #if defined(CONFIG_PM8921_BMS) || \
 	defined(CONFIG_PM8921_BMS_MODULE) || \
-	defined(CONFIG_QPNP_BMS)
+	defined(CONFIG_QPNP_BMS) || \
+	defined(CONFIG_QPNP_VM_BMS)
 extern struct bms_battery_data  palladium_1500_data;
 extern struct bms_battery_data  desay_5200_data;
 extern struct bms_battery_data  oem_batt_data;
