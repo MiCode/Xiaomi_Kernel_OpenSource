7b3724ff29b6062fd8ed054ebefb9bde1b5bdf55
Ujwal Patel <ujwalp@codeaurora.org>
2014-01-24 16:34:53 -0800

    msm: mdss: enable under-run interrupt for secondary ctl
    In split-display use-cases two ctl paths are active but one acts as
    master and other as secondary. Currently under-run interrupt is only
    enabled for master ctl. Enable under-run interrupt for secondary ctl
    for seamless tracking.
    
    Change-Id: I849419c4077fed384431079952027149c31b33fc
    Signed-off-by: Ujwal Patel <ujwalp@codeaurora.org>
    Signed-off-by: Jayant Shekhar <jshekhar@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp.h
+++ drivers/video/msm/mdss/mdss_mdp.h
@@ -452,6 +452,16 @@ enum mdss_screen_state {
 };
 
 #define is_vig_pipe(_pipe_id_) ((_pipe_id_) <= MDSS_MDP_SSPP_VIG2)
+
+static inline struct mdss_mdp_ctl *mdss_mdp_get_split_ctl(
+	struct mdss_mdp_ctl *ctl)
+{
+	if (ctl && ctl->mixer_right && (ctl->mixer_right->ctl != ctl))
+		return ctl->mixer_right->ctl;
+
+	return NULL;
+}
+
 static inline void mdss_mdp_ctl_write(struct mdss_mdp_ctl *ctl,
 				      u32 reg, u32 val)
 {
