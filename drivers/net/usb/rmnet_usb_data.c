789410e61caae2f3b7e17e7ee13a9be7e02dd0ed
Hemant Kumar <hemantk@codeaurora.org>
2014-01-24 17:38:52 -0800

    net_usb: rmnet_usb_data: Add support for PID 0x90A0
    PID 0x90A0 includes DIAG, System monitor, EFS sync
    via int ep, DUN, CSVT, tethered rmnet, embedded rmnet
    ipc router.
    
    Change-Id: Ic5cf39e062e0ea9ecd4661aa4ed2aad099970067
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- drivers/net/usb/rmnet_usb_data.c
+++ drivers/net/usb/rmnet_usb_data.c
@@ -957,6 +957,9 @@ static const struct usb_device_id vidpids[] = {
 	{ USB_DEVICE_INTERFACE_NUMBER(0x05c6, 0x908A, 6), /*mux over hsic mdm*/
 	.driver_info = (unsigned long)&rmnet_info,
 	},
+	{ USB_DEVICE_INTERFACE_NUMBER(0x05c6, 0x90A0, 6), /*mux over hsic mdm*/
+	.driver_info = (unsigned long)&rmnet_info,
+	},
 
 	{ }, /* Terminating entry */
 };
