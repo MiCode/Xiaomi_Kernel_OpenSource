5958175f1edbc6ebedf63f5ebb2cd920a3228058
Asutosh Das <asutoshd@codeaurora.org>
2014-01-29 15:27:07 +0530

    fat: limit the number of error prints
    Use fat_fs_error_ratelimit instead of fat_fs_error so as to
    avoid large number of prints.
    
    Change-Id: I2e21e6bd0c8e7e79d3ced9c213668c405fd6260d
    Signed-off-by: Asutosh Das <asutoshd@codeaurora.org>

--- fs/fat/cache.c
+++ fs/fat/cache.c
@@ -294,8 +294,9 @@ static int fat_bmap_cluster(struct inode *inode, int cluster)
 	if (ret < 0)
 		return ret;
 	else if (ret == FAT_ENT_EOF) {
-		fat_fs_error(sb, "%s: request beyond EOF (i_pos %lld)",
-			     __func__, MSDOS_I(inode)->i_pos);
+		fat_fs_error_ratelimit(sb,
+				       "%s: request beyond EOF (i_pos %lld)",
+				       __func__, MSDOS_I(inode)->i_pos);
 		return -EIO;
 	}
 	return dclus;
