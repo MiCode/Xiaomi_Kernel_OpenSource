12914a73fff53463f938cb553da6d6a63120d6a0
Kenneth Westfield <kwestfie@codeaurora.org>
2014-01-30 14:44:28 -0800

    ASoC: msm: Add sample rates to PCM front-end
    Add two additional sample rates to the PCM front-
    end: 88200 KHz and 176400 KHz.
    
    CRs-Fixed: 601715
    Change-Id: If61fb034a60f41b5c63a00fd764da50db7333cb5
    Signed-off-by: Kenneth Westfield <kwestfie@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-pcm-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-q6-v2.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -99,7 +99,7 @@ static struct snd_pcm_hardware msm_pcm_hardware_playback = {
 /* Conventional and unconventional sample rate supported */
 static unsigned int supported_sample_rates[] = {
 	8000, 11025, 12000, 16000, 22050, 24000, 32000, 44100, 48000,
-	96000, 192000
+	88200, 96000, 176400, 192000
 };
 
 static uint32_t in_frame_info[CAPTURE_MAX_NUM_PERIODS][2];
