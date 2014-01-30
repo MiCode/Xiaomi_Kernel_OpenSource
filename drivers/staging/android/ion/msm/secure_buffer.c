a0bc2544f4ef3f106efb58c9dec1852c3493c244
Laura Abbott <lauraa@codeaurora.org>
2014-01-29 20:50:54 -0800

    ion: Add EXPORT_SYMBOL for buffer securing APIs
    Test modules may need to use the secure/unsecure buffer APIs.
    Export them.
    
    Change-Id: I1c322c9923cc856d7baf23aec809f3fb1cfde227
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- drivers/staging/android/ion/msm/secure_buffer.c
+++ drivers/staging/android/ion/msm/secure_buffer.c
@@ -229,7 +229,7 @@ int msm_ion_secure_buffer(struct ion_client *client, struct ion_handle *handle,
 out:
 	return ret;
 }
-
+EXPORT_SYMBOL(msm_ion_secure_buffer);
 
 static void msm_secure_buffer_release(struct kref *kref)
 {
@@ -282,6 +282,7 @@ int msm_ion_unsecure_buffer(struct ion_client *client,
 out:
 	return ret;
 }
+EXPORT_SYMBOL(msm_ion_unsecure_buffer);
 
 #define MAKE_CP_VERSION(major, minor, patch) \
 	(((major & 0x3FF) << 22) | ((minor & 0x3FF) << 12) | (patch & 0xFFF))
