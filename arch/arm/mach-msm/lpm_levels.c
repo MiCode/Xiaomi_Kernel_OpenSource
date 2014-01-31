b5c68f7ecee4fe65783bc9d8b983959b6fb887a3
Mahesh Sivasubramanian <msivasub@codeaurora.org>
2014-01-31 13:57:00 -0700

    msm: lpm_levels: Fix incorrect computation of next wakeup time
    The next event timer wakeup was incorrectly computed when the hard timer
    event doesn't exist. Fix by ensuring the next wakeup is computed only
    when a timer event is valid
    
    Change-Id: I40c1f372f84d8f43b2101e37dcfb592193d4b2e0
    Signed-off-by: Mahesh Sivasubramanian <msivasub@codeaurora.org>

--- arch/arm/mach-msm/lpm_levels.c
+++ arch/arm/mach-msm/lpm_levels.c
@@ -526,7 +526,7 @@ static noinline int lpm_cpu_power_select(struct cpuidle_device *dev, int *index)
 		if (latency_us < pwr->latency_us)
 			continue;
 
-		if (next_event_us)
+		if (next_event_us) {
 			if (next_event_us < pwr->latency_us)
 				continue;
 
@@ -535,6 +535,7 @@ static noinline int lpm_cpu_power_select(struct cpuidle_device *dev, int *index)
 				next_wakeup_us = next_event_us
 					- pwr->latency_us;
 			}
+		}
 
 		if (next_wakeup_us <= pwr->time_overhead_us)
 			continue;
