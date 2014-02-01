7d3ca8d5ac29a086dbcdd188494408fee7d72f37
Shrenuj Bansal <shrenujb@codeaurora.org>
2014-01-31 16:13:26 -0800

    msm: kgsl: Remove kzalloc failure message
    Remove kzalloc failure message from kgsl_iommu_clk_disable_on_ts.
    A lot of other stuff would go bad if kzalloc fails so we don't
    really need this message.
    
    Change-Id: Ifcd8b93b23b4669423e276dda765703d5c06248d
    Signed-off-by: Shrenuj Bansal <shrenujb@codeaurora.org>

--- drivers/gpu/msm/kgsl_iommu.c
+++ drivers/gpu/msm/kgsl_iommu.c
@@ -535,10 +535,9 @@ kgsl_iommu_disable_clk_on_ts(struct kgsl_mmu *mmu,
 	struct kgsl_iommu_disable_clk_param *param;
 
 	param = kzalloc(sizeof(*param), GFP_KERNEL);
-	if (!param) {
-		KGSL_CORE_ERR("kzalloc(%d) failed\n", sizeof(*param));
+	if (!param)
 		return;
-	}
+
 	param->mmu = mmu;
 	param->ctx_id = ctx_id;
 	param->ts = ts;
