e01fd0739d63341ef04e727a2c0df1420e12c458
Kenneth Westfield <kwestfie@codeaurora.org>
2014-01-31 16:16:33 -0800

    ASoC: Compress: Remove unused hw_params pointer
    Remove the hw_params variable in
    soc_compr_set_params_fe as the memory allocated
    for it was not freed and the pointer itself was
    not used.
    
    CRs-Fixed: 601556
    Change-Id: If2abfe58ad6dedab9b7ba21ae6f3b4bb5335cf42
    Signed-off-by: Kenneth Westfield <kwestfie@codeaurora.org>

--- sound/soc/soc-compress.c
+++ sound/soc/soc-compress.c
@@ -475,17 +475,12 @@ static int soc_compr_set_params_fe(struct snd_compr_stream *cstream,
 	struct snd_soc_pcm_runtime *fe = cstream->private_data;
 	struct snd_pcm_substream *fe_substream = fe->pcm->streams[0].substream;
 	struct snd_soc_platform *platform = fe->platform;
-	struct snd_pcm_hw_params *hw_params;
 	int ret = 0, stream;
 
 	if (cstream->direction == SND_COMPRESS_PLAYBACK)
 		stream = SNDRV_PCM_STREAM_PLAYBACK;
 	else
 		stream = SNDRV_PCM_STREAM_CAPTURE;
-
-	hw_params = kzalloc(sizeof(*hw_params), GFP_KERNEL);
-	if (hw_params == NULL)
-		return -ENOMEM;
 
 	mutex_lock(&fe->card->dpcm_mutex);
 	/* first we call set_params for the platform driver
