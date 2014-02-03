a79bc4180c90fe0f68db13405c92fb37da62980a
Ivan Evlogiev <ivanevlogiev@codeaurora.org>
2014-02-03 12:16:56 +0200

    msm: camera: fix update frame drop pattern
    Update frame drop pattern needs to support SKIP_ALL.
    Frame drop pattern needs to be updated for burst streams.
    
    Change-Id: Id85f7d284011610afc2dcec84994b0b5112b4e50
    CRs-Fixed: 621941
    Signed-off-by: Ivan Evlogiev <ivanevlogiev@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -1389,7 +1389,10 @@ int msm_isp_update_axi_stream(struct vfe_device *vfe_dev, void *arg)
 			return -EINVAL;
 		}
 		if (stream_info->state == ACTIVE &&
-			stream_info->stream_type == BURST_STREAM) {
+			stream_info->stream_type == BURST_STREAM &&
+			(1 != update_cmd->num_streams ||
+				UPDATE_STREAM_FRAMEDROP_PATTERN !=
+					update_cmd->update_type)) {
 			pr_err("%s: Cannot update active burst stream\n",
 				__func__);
 			return -EINVAL;
@@ -1416,7 +1419,10 @@ int msm_isp_update_axi_stream(struct vfe_device *vfe_dev, void *arg)
 				msm_isp_get_framedrop_period(
 				   update_info->skip_pattern);
 			stream_info->runtime_init_frame_drop = 0;
-			stream_info->framedrop_pattern = 0x1;
+			if (update_info->skip_pattern == SKIP_ALL)
+				stream_info->framedrop_pattern = 0x0;
+			else
+				stream_info->framedrop_pattern = 0x1;
 			stream_info->framedrop_period = framedrop_period - 1;
 			vfe_dev->hw_info->vfe_ops.axi_ops.
 				cfg_framedrop(vfe_dev, stream_info);
