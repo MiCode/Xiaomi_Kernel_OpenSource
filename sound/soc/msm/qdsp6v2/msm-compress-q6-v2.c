3cd8e6654649f0f8ee3fe836577c1c8bdd1bb650
Krishnankutty Kolathappilly <kkolat@codeaurora.org>
2014-02-03 13:06:13 -0800

    ASoC: msm: Reset the stream open flag after CMD_CLOSE is sent to DSP
    In case of gapless playback, driver does not wait for EOS. CMD_CLOSE
    for stream one or two can be called from compress driver free before
    EOS ACK for stream one is received. This can result in CMD_CLOSE to
    be sent for already closed stream. Reset the stream open flag after
    CMD_CLOSE is sent to DSP to prevent multiple CMD_CLOSE on same
    stream.
    
    Change-Id: Ic186ae37a760a2996ebd7b9b5dc5d5846621ab66
    Signed-off-by: Krishnankutty Kolathappilly <kkolat@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
@@ -819,6 +819,7 @@ static int msm_compr_free(struct snd_compr_stream *cstream)
 
 	if ((stream_index < MAX_NUMBER_OF_STREAMS && stream_index >= 0) &&
 	    (prtd->gapless_state.stream_opened[stream_index])) {
+		prtd->gapless_state.stream_opened[stream_index] = 0;
 		spin_unlock_irqrestore(&prtd->lock, flags);
 		pr_debug(" close stream %d", NEXT_STREAM_ID(stream_id));
 		q6asm_stream_cmd(ac, CMD_CLOSE, NEXT_STREAM_ID(stream_id));
@@ -828,6 +829,7 @@ static int msm_compr_free(struct snd_compr_stream *cstream)
 	stream_index = STREAM_ARRAY_INDEX(stream_id);
 	if ((stream_index < MAX_NUMBER_OF_STREAMS && stream_index >= 0) &&
 	    (prtd->gapless_state.stream_opened[stream_index])) {
+		prtd->gapless_state.stream_opened[stream_index] = 0;
 		spin_unlock_irqrestore(&prtd->lock, flags);
 		pr_debug("close stream %d", stream_id);
 		q6asm_stream_cmd(ac, CMD_CLOSE, stream_id);
