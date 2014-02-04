a3ee1370ff905c5cc45d063581962e5968736833
Deepa Madiregama <dmadireg@codeaurora.org>
2014-02-05 00:15:19 +0530

    ASoC: msm: qdsp6v2: Fix voice mute issue when EC ref is set
    Function voc_set_ext_ec_ref returns 0 on success, handle the
    return value of this function appropriately to avoid mute issue.
    
    CRs-Fixed: 601078
    Change-Id: I7226c088410bf4a2effe0d060f839dc7928359bf
    Signed-off-by: Deepa Madiregama <dmadireg@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1475,7 +1475,7 @@ static int msm_routing_ext_ec_put(struct snd_kcontrol *kcontrol,
 		msm_route_ext_ec_ref = AFE_PORT_INVALID;
 		break;
 	}
-	if (voc_set_ext_ec_ref(msm_route_ext_ec_ref, state)) {
+	if (!voc_set_ext_ec_ref(msm_route_ext_ec_ref, state)) {
 		mutex_unlock(&routing_lock);
 		snd_soc_dapm_mux_update_power(widget, kcontrol, 1, mux, e);
 	} else {
