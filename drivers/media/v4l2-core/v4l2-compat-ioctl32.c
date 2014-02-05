5a17adb1064d2319b2d5f97a1e68c59330e290fa
Praneeth Paladugu <ppaladug@codeaurora.org>
2014-02-05 13:44:21 -0800

    [media] v4l: Copy Reserved and bytesused in multi-planar API
    Reserved and bytesused fields copied in multi-planar ioctl
    handling to the 32bit compatibility layer.
    
    Change-Id: I25711ba92aa6236f28c74067515a594103e8d345
    Signed-off-by: Praneeth Paladugu <ppaladug@codeaurora.org>

--- drivers/media/v4l2-core/v4l2-compat-ioctl32.c
+++ drivers/media/v4l2-core/v4l2-compat-ioctl32.c
@@ -336,6 +336,10 @@ static int get_v4l2_plane32(struct v4l2_plane __user *up, struct v4l2_plane32 __
 
 	if (copy_in_user(up, up32, 2 * sizeof(__u32)) ||
 		copy_in_user(&up->data_offset, &up32->data_offset,
+				sizeof(__u32)) ||
+		copy_in_user(up->reserved, up32->reserved,
+				sizeof(up->reserved)) ||
+		copy_in_user(&up->length, &up32->length,
 				sizeof(__u32)))
 		return -EFAULT;
 
