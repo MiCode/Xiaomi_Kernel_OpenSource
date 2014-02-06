379c2e85564ae85f7133175cfd14adad40099dba
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2014-02-06 04:41:46 +0000

    extcon: arizona: Declare 3-pole jack if we detect open circuit on mic
    Change-Id: I7bfec30eddde46617af1f5298d06d47282d0feac
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    Signed-off-by: Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>

--- drivers/extcon/extcon-arizona.c
+++ drivers/extcon/extcon-arizona.c
@@ -878,6 +878,9 @@ static void arizona_micd_detect(struct work_struct *work)
 	/* Due to jack detect this should never happen */
 	if (!(val & ARIZONA_MICD_STS)) {
 		dev_warn(arizona->dev, "Detected open circuit\n");
+		info->mic = false;
+		arizona_stop_mic(info);
+		arizona_identify_headphone(info);
 		info->detecting = false;
 		goto handled;
 	}
