540932f694e402b747e0107f311e4498964f269d
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2014-02-07 16:25:02 +0530

    mdss: dsi: Handle gpio configuration properly
    Dsi related GPIOs are freed even when gpio request fails.
    Fix this by removing the gpio_free() calls when the gpio request
    fails. Add the gpio_request() and gpio_free()
    calls to panel reset sequence so that gpio pins would
    go to suspend state when not in use.
    
    Change-Id: Id5ef45c5cc106d2a3ced3359739037f488e1919f
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi.h
+++ drivers/video/msm/mdss/mdss_dsi.h
@@ -337,7 +337,7 @@ int mdss_dsi_clk_init(struct platform_device *pdev,
 void mdss_dsi_clk_deinit(struct mdss_dsi_ctrl_pdata *ctrl_pdata);
 int mdss_dsi_enable_bus_clocks(struct mdss_dsi_ctrl_pdata *ctrl_pdata);
 void mdss_dsi_disable_bus_clocks(struct mdss_dsi_ctrl_pdata *ctrl_pdata);
-void mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable);
+int mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable);
 void mdss_dsi_phy_disable(struct mdss_dsi_ctrl_pdata *ctrl);
 void mdss_dsi_phy_init(struct mdss_panel_data *pdata);
 void mdss_dsi_phy_sw_reset(unsigned char *ctrl_base);
