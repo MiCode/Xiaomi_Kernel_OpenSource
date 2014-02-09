db16b27ca47d38e5885b795d57e808fe23b8e03c
Alexandre Courbot <acourbot@nvidia.com>
2014-02-09 17:43:54 +0900

    gpiolib: add gpiochip_get_desc() driver function
    Some drivers dealing with a gpio_chip might need to act on its
    descriptors directly; one example is pinctrl drivers that need to lock a
    GPIO for being used as IRQ using gpiod_lock_as_irq().
    
    This patch exports a gpiochip_get_desc() function that returns the
    GPIO descriptor at the requested index. It also sweeps the
    gpio_to_chip() function out of the consumer interface since any holder
    of a gpio_chip reference can manipulate its GPIOs way beyond what a
    consumer should be allowed to do.
    
    As a result, gpio_chip is not visible anymore to simple GPIO consumers.
    
    (cherry picked from upstream
     commit bb1e88ccb771492ac908ac295ec135efa1d53093)
    
    Change-Id: Icd2c2411a9b343e70f3bb92a152e74c2c1344865
    Signed-off-by: Alexandre Courbot <acourbot@nvidia.com>
    Reviewed-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Reviewed-by: Jean-Jacques Hiblot <jjhiblot@traphandler.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- include/linux/gpio/consumer.h
+++ include/linux/gpio/consumer.h
@@ -5,7 +5,6 @@
 #include <linux/kernel.h>
 
 struct device;
-struct gpio_chip;
 
 /**
  * Opaque descriptor for a GPIO. These are obtained using gpiod_get() and are
@@ -59,7 +58,6 @@ int gpiod_to_irq(const struct gpio_desc *desc);
 /* Convert between the old gpio_ and new gpiod_ interfaces */
 struct gpio_desc *gpio_to_desc(unsigned gpio);
 int desc_to_gpio(const struct gpio_desc *desc);
-struct gpio_chip *gpiod_to_chip(const struct gpio_desc *desc);
 
 #else /* CONFIG_GPIOLIB */
 
@@ -206,12 +204,6 @@ static inline int desc_to_gpio(const struct gpio_desc *desc)
 	/* GPIO can never have been requested */
 	WARN_ON(1);
 	return -EINVAL;
-}
-static inline struct gpio_chip *gpiod_to_chip(const struct gpio_desc *desc)
-{
-	/* GPIO can never have been requested */
-	WARN_ON(1);
-	return ERR_PTR(-ENODEV);
 }
 
 
