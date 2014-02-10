87d0319ad025d06efd999f5d72ed0a5e9afee4c1
Talel Shenhar <tatias@codeaurora.org>
2014-02-10 12:04:57 +0200

    msm: ipa: adds USB pipe to sps dump
    This change adds the new USB pipe to the
    pipes being dumped (this is pipe number 15).
    
    Change-Id: I29ae170e8fb843170ec6af5fdc193484d9967d2b
    Signed-off-by: Talel Shenhar <tatias@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_utils.c
+++ drivers/platform/msm/ipa/ipa_utils.c
@@ -2923,7 +2923,7 @@ void ipa_bam_reg_dump(void)
 	if (__ratelimit(&_rs)) {
 		ipa_inc_client_enable_clks();
 		pr_err("IPA BAM START\n");
-		sps_get_bam_debug_info(ipa_ctx->bam_handle, 5, 479182, 0, 0);
+		sps_get_bam_debug_info(ipa_ctx->bam_handle, 5, 511950, 0, 0);
 		sps_get_bam_debug_info(ipa_ctx->bam_handle, 93, 0, 0, 0);
 		ipa_dec_client_disable_clks();
 	}
