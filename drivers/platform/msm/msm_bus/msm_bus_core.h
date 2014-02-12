0952b331adb34cfd51bc5a3787e25fdb599483ce
Girish Mahadevan <girishm@codeaurora.org>
2014-02-12 09:19:18 -0700

    msm: msm_bus: Introduce multi level BKE limiting
    Introduce multi level BKE throttling for BIMC masters. By specifying an
    array of thresholds and limiting bandwidth values, multiple levels of
    master throttling can be achieved.
    
    Change-Id: Ic3b483d3e987266e8f37e66dff8bedd49ddd1d4d
    Signed-off-by: Girish Mahadevan <girishm@codeaurora.org>

--- drivers/platform/msm/msm_bus/msm_bus_core.h
+++ drivers/platform/msm/msm_bus/msm_bus_core.h
@@ -90,10 +90,12 @@ struct msm_bus_node_info {
 	unsigned int prio_wr;
 	unsigned int prio1;
 	unsigned int prio0;
-	u64 th;
+	unsigned int num_thresh;
+	u64 *th;
+	u64 cur_lim_bw;
 	unsigned int mode_thresh;
 	bool dual_conf;
-	u64 bimc_bw;
+	u64 *bimc_bw;
 	u32 bimc_gp;
 	u32 bimc_thmp;
 	const char *name;
