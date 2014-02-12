5ae82c90ab5d3e591d789c19b39b79404c514a84
Jordan Crouse <jcrouse@codeaurora.org>
2014-02-12 10:48:59 -0700

    msm: kgsl: don't use sscanf()
    sscanf() is really hard to get right with different variable types
    and it shouldn't be used when kstrtoul works just as well (and is
    way safer).
    
    Change-Id: Ic0dedbad56af5bbee4fc833701d8bc93b118efa9
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/kgsl_sharedmem.c
+++ drivers/gpu/msm/kgsl_sharedmem.c
@@ -74,12 +74,12 @@ static struct kgsl_process_private *
 _get_priv_from_kobj(struct kobject *kobj)
 {
 	struct kgsl_process_private *private;
-	unsigned long name;
+	unsigned int name;
 
 	if (!kobj)
 		return NULL;
 
-	if (sscanf(kobj->name, "%ld", &name) != 1)
+	if (kstrtou32(kobj->name, 0, &name))
 		return NULL;
 
 	list_for_each_entry(private, &kgsl_driver.process_list, list) {
