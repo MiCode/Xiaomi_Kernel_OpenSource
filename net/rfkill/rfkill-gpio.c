cfba7d5f983c5e717e1b946aaac5dd9dfc178788
Heikki Krogerus <heikki.krogerus@linux.intel.com>
2014-02-17 15:54:51 +0200

    net: rfkill: gpio: add ACPI ID for GPS module on Lenovo Miix2
    On Lenovo Miix 2 8", BCM4752 is renamed LNV4752.
    
    Signed-off-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>

--- net/rfkill/rfkill-gpio.c
+++ net/rfkill/rfkill-gpio.c
@@ -158,6 +158,7 @@ static int rfkill_gpio_remove(struct platform_device *pdev)
 
 static const struct acpi_device_id rfkill_acpi_match[] = {
 	{ "BCM4752", RFKILL_TYPE_GPS },
+	{ "LNV4752", RFKILL_TYPE_GPS },
 	{ },
 };
 
