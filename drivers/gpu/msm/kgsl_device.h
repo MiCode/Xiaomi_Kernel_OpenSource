5a3c9f9a36e02ddc17a100832c240b721ca72361
Jordan Crouse <jcrouse@codeaurora.org>
2014-02-18 11:38:05 -0700

    msm: kgsl: Remove sscanf() from kgsl_pwrctrl.c
    Replace sscanf() in kgsl_pwrctrl.c with a generic function to safely
    convert 32 bit unsigned ints.
    
    CRs-fixed: 563585
    Change-Id: Ic0dedbad10e2264ec8fccab622d094d123b49b77
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/kgsl_device.h
+++ drivers/gpu/msm/kgsl_device.h
@@ -723,4 +723,23 @@ static inline int kgsl_cmdbatch_sync_pending(struct kgsl_cmdbatch *cmdbatch)
 	return ret;
 }
 
+/**
+ * kgsl_sysfs_store() - parse a string from a sysfs store function
+ * @buf: Incoming string to parse
+ * @ptr: Pointer to an unsigned int to store the value
+ */
+static inline int kgsl_sysfs_store(const char *buf, unsigned int *ptr)
+{
+	unsigned int val;
+	int rc;
+
+	rc = kstrtou32(buf, 0, &val);
+	if (rc)
+		return rc;
+
+	if (ptr)
+		*ptr = val;
+
+	return 0;
+}
 #endif  /* __KGSL_DEVICE_H */
