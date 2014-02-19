a092e9e67631ccf88ea49e4eab3a4b8179da9f8d
Jeremy Gebben <jgebben@codeaurora.org>
2014-02-19 13:41:53 -0700

    msm: kgsl: allow creation of events from the future
    For contexts created with the KGSL_CONTEXT_USER_GENERATED_TS,
    allow events to be created for timestamps that have not
    been issued yet. Presumably these contexts know what they
    are doing.
    
    Change-Id: Iccf2e549b38e1a11850d26ee64b7279ab0b8a1ae
    Signed-off-by: Jeremy Gebben <jgebben@codeaurora.org>

--- drivers/gpu/msm/kgsl_events.c
+++ drivers/gpu/msm/kgsl_events.c
@@ -210,7 +210,7 @@ int kgsl_add_event(struct kgsl_device *device, u32 id, u32 ts,
 	kgsl_event_func func, void *priv, void *owner)
 {
 	struct kgsl_event *event;
-	unsigned int queued, cur_ts;
+	unsigned int queued = 0, cur_ts;
 	struct kgsl_context *context = NULL;
 
 	BUG_ON(!mutex_is_locked(&device->mutex));
@@ -224,11 +224,21 @@ int kgsl_add_event(struct kgsl_device *device, u32 id, u32 ts,
 			return -EINVAL;
 	}
 
-	queued = kgsl_readtimestamp(device, context, KGSL_TIMESTAMP_QUEUED);
+	/*
+	 * If the caller is creating their own timestamps, let them schedule
+	 * events in the future. Otherwise only allow timestamps that have been
+	 * queued.
+	 */
+	if (context == NULL ||
+		((context->flags & KGSL_CONTEXT_USER_GENERATED_TS) == 0)) {
 
-	if (timestamp_cmp(ts, queued) > 0) {
-		kgsl_context_put(context);
-		return -EINVAL;
+		queued = kgsl_readtimestamp(device, context,
+						KGSL_TIMESTAMP_QUEUED);
+
+		if (timestamp_cmp(ts, queued) > 0) {
+			kgsl_context_put(context);
+			return -EINVAL;
+		}
 	}
 
 	cur_ts = kgsl_readtimestamp(device, context, KGSL_TIMESTAMP_RETIRED);
