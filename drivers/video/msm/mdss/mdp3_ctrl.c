3213c61a6f90b0e3942c30ac4263988c34842001
Xiaoming Zhou <zhoux@codeaurora.org>
2014-02-20 15:55:38 -0500

    msm: mdss: fix the display reset error
    Increase the dma stop time out value as stopping operation
    may take longer than the previous defined value. Gracefully
    handle the time out condition to allow it to retry when
    the next frame comes in.
    
    Change-Id: I5120f309db2dbaeebef3b2878caa13cec17c9e1f
    Signed-off-by: Xiaoming Zhou <zhoux@codeaurora.org>

--- drivers/video/msm/mdss/mdp3_ctrl.c
+++ drivers/video/msm/mdss/mdp3_ctrl.c
@@ -799,7 +799,7 @@ static int mdp3_ctrl_reset(struct msm_fb_data_type *mfd)
 
 	rc = mdp3_dma->stop(mdp3_dma, mdp3_session->intf);
 	if (rc) {
-		pr_err("fail to stop the MDP3 dma\n");
+		pr_err("fail to stop the MDP3 dma %d\n", rc);
 		goto reset_error;
 	}
 
@@ -1013,7 +1013,11 @@ static int mdp3_ctrl_display_commit_kickoff(struct msm_fb_data_type *mfd,
 	panel = mdp3_session->panel;
 	if (!mdp3_iommu_is_attached(MDP3_CLIENT_DMA_P)) {
 		pr_debug("continuous splash screen, IOMMU not attached\n");
-		mdp3_ctrl_reset(mfd);
+		rc = mdp3_ctrl_reset(mfd);
+		if (rc) {
+			pr_err("fail to reset display\n");
+			return -EINVAL;
+		}
 		reset_done = true;
 	}
 
@@ -1096,7 +1100,11 @@ static void mdp3_ctrl_pan_display(struct msm_fb_data_type *mfd,
 
 	if (!mdp3_iommu_is_attached(MDP3_CLIENT_DMA_P)) {
 		pr_debug("continuous splash screen, IOMMU not attached\n");
-		mdp3_ctrl_reset(mfd);
+		rc = mdp3_ctrl_reset(mfd);
+		if (rc) {
+			pr_err("fail to reset display\n");
+			return;
+		}
 	}
 
 	mutex_lock(&mdp3_session->lock);
