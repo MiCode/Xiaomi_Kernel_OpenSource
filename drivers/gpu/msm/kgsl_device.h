8c7828257e2933f092eb322bb24961f6688ec87b
Hareesh Gundu <hareeshg@codeaurora.org>
2014-02-20 22:56:36 +0530

    msm: kgsl: Remove unused legacy IB timeout variables
    Remove unused variables from legacy IB timeout detection.
    
    Change-Id: I4df94ee43072ccebff4045c5950edbc406de38a8
    Signed-off-by: Hareesh Gundu <hareeshg@codeaurora.org>

--- drivers/gpu/msm/kgsl_device.h
+++ drivers/gpu/msm/kgsl_device.h
@@ -28,7 +28,6 @@
 #define KGSL_TIMEOUT_NONE           0
 #define KGSL_TIMEOUT_DEFAULT        0xFFFFFFFF
 #define KGSL_TIMEOUT_PART           50 /* 50 msec */
-#define KGSL_TIMEOUT_LONG_IB_DETECTION  2000 /* 2 sec*/
 
 #define FIRST_TIMEOUT (HZ / 2)
 
