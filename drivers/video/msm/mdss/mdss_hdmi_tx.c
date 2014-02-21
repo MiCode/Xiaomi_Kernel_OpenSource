a59569ca1211b0bb047f6b1ec443455c57236776
Mitchel Humpherys <mitchelh@codeaurora.org>
2014-02-21 00:20:23 -0800

    msm: mdss: hdmi: add missing sentinel to of_match_table
    The DT of_match_table is missing the NULL entry that is needed to
    terminate the table. Without this there is a risk of corrupting memory.
    
    Change-Id: Ida223b91b00c72a6a2990653e3cb5ee693bd7e05
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- drivers/video/msm/mdss/mdss_hdmi_tx.c
+++ drivers/video/msm/mdss/mdss_hdmi_tx.c
@@ -3976,6 +3976,7 @@ static int hdmi_tx_remove(struct platform_device *pdev)
 
 static const struct of_device_id hdmi_tx_dt_match[] = {
 	{.compatible = COMPATIBLE_NAME,},
+	{ /* Sentinel */ },
 };
 MODULE_DEVICE_TABLE(of, hdmi_tx_dt_match);
 
