638cce8890c35e2045d29d5ceefa59d3bf34976f
Yue Ma <yuem@codeaurora.org>
2014-02-20 17:12:05 -0800

    msm: wcnss: Add the support to access IRIS registers using bit-bang
    During cold boot, if firmware watchdog bite happens, WCNSS platform driver
    will try to log Pronto registers, but in some cases it cannot access them
    since there is no clock on Pronto. Add the support to log IRIS registers
    using bit-bang in order to debug this issue.
    
    CRs-fixed: 616588
    Change-Id: I9315c295d33d131224605adddca26796de25e1fb
    Signed-off-by: Yue Ma <yuem@codeaurora.org>

--- arch/arm/mach-msm/include/mach/board.h
+++ arch/arm/mach-msm/include/mach/board.h
@@ -35,6 +35,12 @@
 #define RF_TYPE_48 0x30
 #define RF_TYPE_49 0x31
 
+#define WLAN_RF_REG_ADDR_START_OFFSET   0x3
+#define WLAN_RF_REG_DATA_START_OFFSET   0xf
+#define WLAN_RF_READ_REG_CMD            0x3
+#define WLAN_RF_WRITE_REG_CMD           0x2
+#define WLAN_RF_READ_CMD_MASK           0x3fff
+
 struct msm_camera_io_ext {
 	uint32_t mdcphy;
 	uint32_t mdcsz;
@@ -690,4 +696,5 @@ int smd_debugfs_init(void);
 static inline int smd_debugfs_init(void) { return 0; }
 #endif
 
+u32 wcnss_rf_read_reg(u32 rf_reg_addr);
 #endif
