254a78f283a4928decd8d5433b4611242a1f84ec
Kaushal Kumar <kaushalk@codeaurora.org>
2014-02-22 15:52:07 -0800

    pinctrl: msm: Fix gpio direction configuration in tlmm-v4
    GPIO direction data derived from device tree should not be
    manipulated prior to write operation to the register.
    
    Change-Id: I01349c35af907237a532c42cdf302c282960bf5a
    Signed-off-by: Kaushal Kumar <kaushalk@codeaurora.org>

--- drivers/pinctrl/pinctrl-msm-tlmm-v4.c
+++ drivers/pinctrl/pinctrl-msm-tlmm-v4.c
@@ -392,7 +392,6 @@ static int msm_tlmm_v4_gp_cfg(uint pin_no, unsigned long *config,
 			data = pinconf_to_config_argument(*config);
 			inout_val = dir_to_inout_val(data);
 			writel_relaxed(inout_val, inout_reg);
-			data = (mask << shft);
 		} else {
 			inout_val = readl_relaxed(inout_reg);
 			data = inout_val_to_dir(inout_val);
