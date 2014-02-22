f431412d10e5c3a3575414d13619dc4aad117b68
Jeevan Shriram <jshriram@codeaurora.org>
2014-02-22 15:02:43 -0800

    clk: qcom: mdss: add DSI PLL clock driver support for 8916
    This change adds the DSI PLL clock driver support for
    8916. In addition, it adds support of DSI PLL programming
    of different MDSS revisions using the same DSI PLL driver.
    Also rename the compatibility string of the DSI
    PLL handle so that the detection and support of DSI
    PLL driver for 8974 and 8916 happens dynamically.
    
    Change-Id: I169ebeaf23e4be8ff4b533fce1057144edd8b692
    Signed-off-by: Jeevan Shriram <jshriram@codeaurora.org>
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>
    (cherry picked from commit b4f79b7bc368b52f056b721a0be1336f9023d693)

--- include/dt-bindings/clock/msm-clocks-8916.h
+++ include/dt-bindings/clock/msm-clocks-8916.h
@@ -159,6 +159,9 @@
 #define clk_gcc_bimc_gpu_clk			0x19922503
 #define clk_gcc_bimc_gfx_clk			0x3edd69ad
 
+#define clk_pixel_clk_src			0x8b6f83d8
+#define clk_byte_clk_src			0x3a911c53
+
 /* RPM */
 #define clk_pcnoc_clk				0xc1296d0f
 #define clk_pcnoc_a_clk				0x9bcffee4
