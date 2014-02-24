e112b1c7a02577f26854d1d8e4e5fe295cefc84d
Pu Chen <puchen@codeaurora.org>
2014-02-23 20:12:44 -0800

    msm: kgsl: Fix miscellaneous compilation errors
    Do not include ocmem header file for non-ocmem targets.
    Use correct print type for size_t.
    
    Change-Id: Ica17bdf908b8e9b2849f95f45746d5c877ed4cfc
    Signed-off-by: Pu Chen <puchen@codeaurora.org>

--- drivers/gpu/msm/adreno.h
+++ drivers/gpu/msm/adreno.h
@@ -18,7 +18,11 @@
 #include "adreno_ringbuffer.h"
 #include "adreno_profile.h"
 #include "kgsl_iommu.h"
+#include <linux/stat.h>
+
+#ifdef CONFIG_MSM_OCMEM
 #include <mach/ocmem.h>
+#endif
 
 #include "a3xx_reg.h"
 
