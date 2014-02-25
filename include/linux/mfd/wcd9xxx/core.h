ca2824f878e4bbbe889347a44d75c596881baba3
Bhalchandra Gajare <gajare@codeaurora.org>
2014-02-24 21:30:16 -0800

    mfd: wcd9xxx: Add support for REPEAT_CHANGE_VALUE
    Slimbus supports ability to write upto 16 bytes to the same register as
    one transaction on the bus through REPEAT_CHANGE_VALUE slimbus message.
    Add support to write codec register with multiple values using
    REPEAT_CHANGE_VALUE. Not all sizes from 1 to 16 are supported by such a
    message, add support in driver to be able to write all sizes from 1 to
    16 by splitting the register writes of unsupported sizes into multiple
    writes of supported sizes.
    
    Change-Id: I33fdd983068bc10f65523b2a95f1d7741fa6c486
    Signed-off-by: Bhalchandra Gajare <gajare@codeaurora.org>

--- include/linux/mfd/wcd9xxx/core.h
+++ include/linux/mfd/wcd9xxx/core.h
@@ -212,6 +212,8 @@ int wcd9xxx_interface_reg_read(struct wcd9xxx *wcd9xxx, unsigned short reg);
 int wcd9xxx_interface_reg_write(struct wcd9xxx *wcd9xxx, unsigned short reg,
 		u8 val);
 int wcd9xxx_get_logical_addresses(u8 *pgd_la, u8 *inf_la);
+int wcd9xxx_slim_write_repeat(struct wcd9xxx *wcd9xxx, unsigned short reg,
+			     int bytes, void *src);
 
 #if defined(CONFIG_WCD9310_CODEC) || \
 	defined(CONFIG_WCD9304_CODEC) || \
