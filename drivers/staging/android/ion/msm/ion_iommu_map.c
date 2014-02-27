c2a39c5b53c74673939cb3505db06dc2bed001c0
Larry Bassel <lbassel@codeaurora.org>
2014-02-26 16:07:43 -0800

    ion: do not use sg_dma_len() macro
    Use of sg_dma_len() is not portable to 64 bit and is not used any more
    by ion or iommu. Replace the usages by direct access of the length
    structure member.
    
    Change-Id: I6c18b2a77e288be312448c36f8ed3ad6db90ac0f
    Signed-off-by: Larry Bassel <lbassel@codeaurora.org>

--- drivers/staging/android/ion/msm/ion_iommu_map.c
+++ drivers/staging/android/ion/msm/ion_iommu_map.c
@@ -208,8 +208,8 @@ static int ion_iommu_map_iommu(struct ion_iommu_meta *meta,
 	 * biggest entry. To take advantage of bigger mapping sizes both the
 	 * VA and PA addresses have to be aligned to the biggest size.
 	 */
-	if (sg_dma_len(table->sgl) > align)
-		align = sg_dma_len(table->sgl);
+	if (table->sgl->length > align)
+		align = table->sgl->length;
 
 	ret = msm_allocate_iova_address(domain_num, partition_num,
 						data->mapped_size, align,
@@ -394,7 +394,7 @@ int ion_map_iommu(struct ion_client *client, struct ion_handle *handle,
 		return PTR_ERR(table);
 
 	for_each_sg(table->sgl, sg, table->nents, i)
-		size += sg_dma_len(sg);
+		size += sg->length;
 
 	if (!msm_use_iommu()) {
 		unsigned long pa = sg_dma_address(table->sgl);
