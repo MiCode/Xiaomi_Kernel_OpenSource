dff12b81d767f5877068083b33a899e862c891cb
Junjie Wu <junjiew@codeaurora.org>
2014-02-27 12:24:48 -0800

    clock-a7: Add lookup entries for PM driver
    Add lookup entries of CPU1/2/3 for PM driver.
    
    Change-Id: Ie7b0f66cdf74bf60d11a8416bed201366512d6e6
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- drivers/clk/qcom/clock-a7.c
+++ drivers/clk/qcom/clock-a7.c
@@ -55,7 +55,13 @@ static struct clk_lookup clock_tbl_a7[] = {
 	CLK_LOOKUP("cpu2_clk",	a7ssmux.c, "0.qcom,msm-cpufreq"),
 	CLK_LOOKUP("cpu3_clk",	a7ssmux.c, "0.qcom,msm-cpufreq"),
 	CLK_LOOKUP("cpu0_clk",	a7ssmux.c, "fe805664.qcom,pm"),
+	CLK_LOOKUP("cpu1_clk",	a7ssmux.c, "fe805664.qcom,pm"),
+	CLK_LOOKUP("cpu2_clk",	a7ssmux.c, "fe805664.qcom,pm"),
+	CLK_LOOKUP("cpu3_clk",	a7ssmux.c, "fe805664.qcom,pm"),
 	CLK_LOOKUP("cpu0_clk",   a7ssmux.c, "8600664.qcom,pm"),
+	CLK_LOOKUP("cpu1_clk",   a7ssmux.c, "8600664.qcom,pm"),
+	CLK_LOOKUP("cpu2_clk",   a7ssmux.c, "8600664.qcom,pm"),
+	CLK_LOOKUP("cpu3_clk",   a7ssmux.c, "8600664.qcom,pm"),
 };
 
 static int of_get_fmax_vdd_class(struct platform_device *pdev, struct clk *c,
