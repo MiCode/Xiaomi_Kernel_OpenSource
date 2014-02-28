fed1f3437df673098e5c018ba655e53d2bd0fc8f
Ujwal Patel <ujwalp@codeaurora.org>
2014-02-28 13:49:23 -0800

    msm: mdss: propagate pipe priority to user-space
    Priority is returned by the driver when overlay is set for the first
    time. It indicates the priority of the underlying pipe serving the
    overlay. This priority can be used by user-space in source split when
    pipes are re-used and shuffled around to reduce fall-backs related
    priority restrictions.
    
    Change-Id: Ibfda0280279e7057a1d3494f4e5f68962b26ef4a
    Signed-off-by: Ujwal Patel <ujwalp@codeaurora.org>

--- include/uapi/linux/msm_mdp.h
+++ include/uapi/linux/msm_mdp.h
@@ -586,6 +586,11 @@ struct mdp_scale_data {
  *		4: decimation by 16 (drop 15 lines for each line fetched)
  * @overlay_pp_cfg: Overlay post processing configuration, for more information
  *		see struct mdp_overlay_pp_params.
+ * @priority:	Priority is returned by the driver when overlay is set for the
+ *		first time. It indicates the priority of the underlying pipe
+ *		serving the overlay. This priority can be used by user-space
+ *		in source split when pipes are re-used and shuffled around to
+ *		reduce fallbacks.
  */
 struct mdp_overlay {
 	struct msmfb_img src;
@@ -598,6 +603,7 @@ struct mdp_overlay {
 	uint32_t transp_mask;
 	uint32_t flags;
 	uint32_t id;
+	uint8_t priority;
 	uint32_t user_data[6];
 	uint32_t bg_color;
 	uint8_t horz_deci;
