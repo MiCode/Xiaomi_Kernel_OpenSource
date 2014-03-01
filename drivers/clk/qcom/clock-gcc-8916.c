2b5c0ef694c1ffc98b6e3a27fb06f991262bee46
Aditya Jonnalagadda <ajonnala@codeaurora.org>
2014-03-01 18:02:34 +0530

    msm: clock-8916: camera clocks for 8916
    enabling camera clocks for 8916
    
    Change-Id: Ic578d4458f5506a845eaeb2f107c26dba8caeb2f
    Signed-off-by: Aditya Jonnalagadda <ajonnala@codeaurora.org>
    Signed-off-by: Krupal Divvela <kdivvela@codeaurora.org>
    Signed-off-by: Abhinay Reddy Vanipally <avanip@codeaurora.org>

--- drivers/clk/qcom/clock-gcc-8916.c
+++ drivers/clk/qcom/clock-gcc-8916.c
@@ -906,6 +906,7 @@ static struct rcg_clk jpeg0_clk_src = {
 static struct clk_freq_tbl ftbl_gcc_camss_mclk0_1_clk[] = {
 	F(  66670000,	   gpll0,  12,	  0,	0),
 	F(  23880000,      gpll0,   1,    2,   67),
+        F( 24000000, gpll0, 1, 2, 67),
 	F_END
 };
 
@@ -1435,7 +1436,7 @@ static struct local_vote_clk gcc_boot_rom_ahb_clk = {
 
 static struct branch_clk gcc_camss_cci_ahb_clk = {
 	.cbcr_reg = CAMSS_CCI_AHB_CBCR,
-	.has_sibling = 1,
+	.has_sibling = 0,
 	.base = &virt_bases[GCC_BASE],
 	.c = {
 		.dbg_name = "gcc_camss_cci_ahb_clk",
@@ -1616,7 +1617,7 @@ static struct branch_clk gcc_camss_gp1_clk = {
 
 static struct branch_clk gcc_camss_ispif_ahb_clk = {
 	.cbcr_reg = CAMSS_ISPIF_AHB_CBCR,
-	.has_sibling = 1,
+	.has_sibling = 0,
 	.base = &virt_bases[GCC_BASE],
 	.c = {
 		.dbg_name = "gcc_camss_ispif_ahb_clk",
@@ -1641,7 +1642,7 @@ static struct branch_clk gcc_camss_jpeg0_clk = {
 
 static struct branch_clk gcc_camss_jpeg_ahb_clk = {
 	.cbcr_reg = CAMSS_JPEG_AHB_CBCR,
-	.has_sibling = 1,
+	.has_sibling = 0,
 	.base = &virt_bases[GCC_BASE],
 	.c = {
 		.dbg_name = "gcc_camss_jpeg_ahb_clk",
