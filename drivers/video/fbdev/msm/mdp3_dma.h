a56e626d9a933fa0de3d2e5551f5ebc2386215c5
Terence Hampson <thampson@codeaurora.org>
2014-03-06 15:07:24 -0500

    mdp3: configure dma source when displaying new frame
    During Linux Bootloader to kernel transition, kernel's first call
    to overlay_set would reconfigure dma source configurations. This
    reconfiguration could happen before vsync but the corresponding
    call to overlay_commit would occur after the vsync. This race
    condition could cause artifacts to appear on screen. This issue
    only occurs on video mode panels.
    
    Change-Id: I1c1274a9c32cb74276162e74c22ad35afef0eb7c
    Signed-off-by: Terence Hampson <thampson@codeaurora.org>

--- drivers/video/fbdev/msm/mdp3_dma.h
+++ drivers/video/fbdev/msm/mdp3_dma.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -259,6 +259,7 @@ struct mdp3_dma {
 	int histo_state;
 	struct mdp3_dma_histogram_data histo_data;
 	unsigned int vsync_status;
+	bool update_src_cfg;
 
 	int (*dma_config)(struct mdp3_dma *dma,
 			struct mdp3_dma_source *source_config,
