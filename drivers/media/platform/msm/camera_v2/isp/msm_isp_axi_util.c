f59aec376d09cdd416ada12d361510db8adcd0cb
Mohit Setia <msetia@codeaurora.org>
2014-03-07 01:14:30 +0530

    msm: camera: exit camera incase axi gets timeout
    AXI hits timeout while stopping due to sensor not streaming.
    Simply returning error from here puts AXI and camif in still
    running state whereas userspace proceeds with releasing the
    buffers. This fix ensures we halt AXi and stop camif in sensor
    not streaming case. Thus avoid page faults and a delay in
    stopping camera.
    
    Change-Id: I4159ac7b21d2a22a5684d7b4a614b0dfd8f06e46
    Signed-off-by: Mohit Setia <msetia@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -1331,7 +1331,15 @@ static int msm_isp_stop_axi_stream(struct vfe_device *vfe_dev,
 		rc = msm_isp_axi_wait_for_cfg_done(vfe_dev, camif_update);
 		if (rc < 0) {
 			pr_err("%s: wait for config done failed\n", __func__);
-			return rc;
+			for (i = 0; i < stream_cfg_cmd->num_streams; i++) {
+				stream_info = &axi_data->stream_info[
+				HANDLE_TO_IDX(
+					stream_cfg_cmd->stream_handle[i])];
+				stream_info->state = STOP_PENDING;
+				msm_isp_axi_stream_enable_cfg(
+					vfe_dev, stream_info);
+				stream_info->state = INACTIVE;
+			}
 		}
 	}
 	msm_isp_update_stream_bandwidth(vfe_dev);
