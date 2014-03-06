5a50efa95eb4fad2cfc58a41ceabf43e50e428dd
Abhimanyu Kapur <abhimany@codeaurora.org>
2014-03-05 20:21:45 -0800

    ARM/ARM64: Introduce arch_read_hardware_id
    Moving towards device tree and arm single binary referring to
    machine descriptor name for hardware id information under
    /proc/cpuinfo is not suitable for certain soc vendors. Add a
    hook for soc vendors to supply a per-soc hardware read method.
    
    Change-Id: Ic8ba82a18f156b3f47030dba0c932170442dbc61
    Signed-off-by: Abhimanyu Kapur <abhimany@codeaurora.org>
    Signed-off-by: Kaushal Kumar <kaushalk@codeaurora.org>
    Signed-off-by: Neeraj Upadhyay <neeraju@codeaurora.org>

--- arch/arm64/kernel/setup.c
+++ arch/arm64/kernel/setup.c
@@ -554,6 +554,11 @@ static int c_show(struct seq_file *m, void *v)
 	}
 #endif
 
+	if (!arch_read_hardware_id)
+		seq_printf(m, "Hardware\t: %s\n", machine_name);
+	else
+		seq_printf(m, "Hardware\t: %s\n", arch_read_hardware_id());
+
 	return 0;
 }
 
