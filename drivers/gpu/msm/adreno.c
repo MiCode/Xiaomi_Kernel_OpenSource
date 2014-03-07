2f42ba8be3fa5d84ce74d984d47832dbcb7d0719
Jordan Crouse <jcrouse@codeaurora.org>
2014-03-07 10:11:08 -0700

    msm: kgsl: Protect fault detect start with active count
    Get the active count when enabling GFT from sysfs so the performance
    counters can be properly programmed.
    
    CRs-Fixed: 622252
    Change-Id: Ic0dedbadfe56f8c66aca15fb521494a2fc00332b
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -2415,9 +2415,12 @@ static int _ft_fast_hang_detect_store(struct device *dev,
 
 	if (tmp != adreno_dev->fast_hang_detect) {
 		if (adreno_dev->fast_hang_detect) {
-			if (adreno_dev->gpudev->fault_detect_start)
+			if (adreno_dev->gpudev->fault_detect_start &&
+				!kgsl_active_count_get(&adreno_dev->dev)) {
 				adreno_dev->gpudev->fault_detect_start(
 					adreno_dev);
+				kgsl_active_count_put(&adreno_dev->dev);
+			}
 		} else {
 			if (adreno_dev->gpudev->fault_detect_stop)
 				adreno_dev->gpudev->fault_detect_stop(
