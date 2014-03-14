d3f95262fcd19a44eb58819be269d40df15fa500
Rohit Gupta <rohgup@codeaurora.org>
2014-03-13 20:57:32 -0700

    cpufreq: cpu-boost: Handle wakeup hints received for foreground tasks
    A previous change modifies the notification conditions in the scheduler
    to call the notifier chain even on foreground_thread wakeups for tasks
    having load more than a threshold value.
    
    If load_based_syncs is turned OFF then we do not need to perform
    cpu boost for wakeup hints for foreground tasks from scheduler
    
    Change-Id: Ifbdd3eccac5c9892dfc3a3c3edbfc0df766478ed
    Signed-off-by: Rohit Gupta <rohgup@codeaurora.org>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -212,6 +212,9 @@ static int boost_migration_notify(struct notifier_block *nb,
 		return NOTIFY_OK;
 	}
 
+	if (!load_based_syncs && (mnd->src_cpu == mnd->dest_cpu))
+		return NOTIFY_OK;
+
 	if (!boost_ms)
 		return NOTIFY_OK;
 
