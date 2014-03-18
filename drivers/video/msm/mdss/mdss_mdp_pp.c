fd0f1cc2c84c8ac5564e2aff53910445523308ad
Benet Clark <benetc@codeaurora.org>
2014-03-17 17:53:35 -0700

    msm: mdss: Move histogram completion initialization to pp_init
    The histogram completion structure only needs to be initialized fully
    once, and this can be done when the pp resource structure is created.
    Otherwise, when enabling the histograms, it is sufficient to use the
    reusable INIT_COMPLETION, in order to avoid any wait_queue errors.
    
    Change-Id: Id4bfb19c91bd5ab07c01a5eb347ba50621fe8a97
    Signed-off-by: Benet Clark <benetc@codeaurora.org>
    Signed-off-by: Krishna Chaitanya Parimi <cparimi@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_pp.c
+++ drivers/video/msm/mdss/mdss_mdp_pp.c
@@ -1748,6 +1748,8 @@ int mdss_mdp_pp_init(struct device *dev)
 					&mdss_pp_res->dspp_hist[i].hist_mutex);
 				spin_lock_init(
 					&mdss_pp_res->dspp_hist[i].hist_lock);
+				init_completion(
+					&mdss_pp_res->dspp_hist[i].comp);
 			}
 		}
 	}
@@ -1756,6 +1758,7 @@ int mdss_mdp_pp_init(struct device *dev)
 		for (i = 0; i < mdata->nvig_pipes; i++) {
 			mutex_init(&vig[i].pp_res.hist.hist_mutex);
 			spin_lock_init(&vig[i].pp_res.hist.hist_lock);
+			init_completion(&vig[i].pp_res.hist.comp);
 		}
 		if (!mdata->pp_bus_hdl) {
 			pp_bus_pdata = &mdp_pp_bus_scale_table;
@@ -2958,7 +2961,7 @@ static int pp_histogram_enable(struct pp_hist_col_info *hist_info,
 		goto exit;
 	}
 	hist_info->frame_cnt = req->frame_cnt;
-	init_completion(&hist_info->comp);
+	INIT_COMPLETION(hist_info->comp);
 	hist_info->hist_cnt_read = 0;
 	hist_info->hist_cnt_sent = 0;
 	hist_info->hist_cnt_time = 0;
