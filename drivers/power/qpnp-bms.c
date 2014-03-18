faf29f3a3fbbf537169ba2bd52238bd1686b8384
Xiaozhe Shi <xiaozhes@codeaurora.org>
2014-03-18 14:27:43 -0700

    power: qpnp-bms: do not clamp SOC based on voltage when charging.
    The BMS driver currently reports SOC to be at least 1% whenever the
    battery voltage is above cutoff voltage. However, this can cause an
    issue when charging, where the IR drop across the battery causes the
    battery terminal voltages to be higher than the cutoff even when the
    battery is effectively dead. This makes the BMS driver always report 1%
    when charging, even when it should be reporting 0%.
    
    Fix this by not clamping the SOC when the battery is charging.
    
    CRs-Fixed: 634856
    Signed-off-by: Natiq Ahmed <mnatiq@codeaurora.org>
    
    Conflicts:
    	drivers/power/qpnp-bms.c
    
    Change-Id: I3d85b5b35973123471dc21eb9651c958219795f9
    Signed-off-by: Xiaozhe Shi <xiaozhes@codeaurora.org>
    Signed-off-by: Vignesh Veeramani <quicvignes@codeaurora.org>

--- drivers/power/qpnp-bms.c
+++ drivers/power/qpnp-bms.c
@@ -2207,6 +2207,11 @@ static int clamp_soc_based_on_voltage(struct qpnp_bms_chip *chip, int soc)
 		pr_err("adc vbat failed err = %d\n", rc);
 		return soc;
 	}
+
+	/* only clamp when discharging */
+	if (is_battery_charging(chip))
+		return soc;
+
 	if (soc <= 0 && vbat_uv > chip->v_cutoff_uv) {
 		pr_debug("clamping soc to 1, vbat (%d) > cutoff (%d)\n",
 						vbat_uv, chip->v_cutoff_uv);
