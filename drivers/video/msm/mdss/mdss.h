b53a4ccff5dc19fa462811e3d36df363a1cab532
Ingrid Gallardo <ingridg@codeaurora.org>
2014-03-19 13:26:34 -0700

    msm: mdss: add property to set from device tree the overlap BW fudge factor
    Current calculations apply one fudge factor to the final bandwidth
    regardless if this is a result from the overlap or prefill calculation.
    With this change the final bandwidth can be tuned up to apply a different
    fudge factor when the bandwidth depends on the overlap calculation.
    
    CRs-Fixed: 635005
    Change-Id: Iaa9718bda3264a47bf70a2facba0d23e4b2414f3
    Signed-off-by: Ingrid Gallardo <ingridg@codeaurora.org>

--- drivers/video/msm/mdss/mdss.h
+++ drivers/video/msm/mdss/mdss.h
@@ -148,6 +148,7 @@ struct mdss_data_type {
 
 	struct mdss_fudge_factor ab_factor;
 	struct mdss_fudge_factor ib_factor;
+	struct mdss_fudge_factor ib_factor_overlap;
 	struct mdss_fudge_factor clk_factor;
 
 	struct mdss_hw_settings *hw_settings;
