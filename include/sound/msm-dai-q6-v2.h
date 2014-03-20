e3727e9808a2c144ad5324987cdd76b3f4e6f30d
Damir Didjusto <damird@codeaurora.org>
2014-03-19 23:29:25 -0700

    ASoC: msm: qdsp6v2: Add support for multiple slots in auxpcm data
    Auxpcm data can be placed in multiple slots in one frame.
    The number of slots that can be in one frame is determined by
    diving the number of bits per frame with bits per sample. For example
    in 256 bit frame size configuration with 16 bits per sample
    there will be 16 slots.  ADSP currently supports receiving
    information on four slots, however the driver only supports
    one slot. This change adds support for multiple auxpcm slots.
    
    Change-Id: Icfa3fd0cba5d2009be89d9230494d2a33a7b930c
    Signed-off-by: Damir Didjusto <damird@codeaurora.org>

--- include/sound/msm-dai-q6-v2.h
+++ include/sound/msm-dai-q6-v2.h
@@ -32,10 +32,8 @@ struct msm_dai_auxpcm_config {
 	u16 sync;
 	u16 frame;
 	u16 quant;
-	/* modify slot to arr[4] to specify
-	* the slot number for each channel
-	* in multichannel scenario */
-	u16 slot;
+	u16 num_slots;
+	u16 *slot_mapping;
 	u16 data;
 	u32 pcm_clk_rate;
 };
