2aa1a4241ce2a81f1095cf49d1876387ee382325
Deva Ramasubramanian <dramasub@codeaurora.org>
2014-03-19 17:04:54 -0700

    msm: wfd: Lock the state in vsg_start
    Protect the state in vsg_start with a mutex as it can be accessed from
    other threads.
    
    CRs-Fixed: 634425
    Change-Id: I823b34f1b310092ea100d366d92a3293a0c74e6d
    Signed-off-by: Deva Ramasubramanian <dramasub@codeaurora.org>

--- drivers/media/platform/msm/wfd/vsg-subdev.c
+++ drivers/media/platform/msm/wfd/vsg-subdev.c
@@ -337,6 +337,7 @@ static int vsg_close(struct v4l2_subdev *sd)
 static int vsg_start(struct v4l2_subdev *sd)
 {
 	struct vsg_context *context = NULL;
+	int rc = 0;
 
 	if (!sd) {
 		WFD_MSG_ERR("ERROR, invalid arguments into %s\n", __func__);
@@ -345,18 +346,24 @@ static int vsg_start(struct v4l2_subdev *sd)
 
 	context = (struct vsg_context *)sd->dev_priv;
 
+	mutex_lock(&context->mutex);
 	if (context->state == VSG_STATE_STARTED) {
 		WFD_MSG_ERR("VSG not stopped, start not allowed\n");
-		return -EINPROGRESS;
+		rc = -EINPROGRESS;
+		goto err_bad_state;
 	} else if (context->state == VSG_STATE_ERROR) {
 		WFD_MSG_ERR("VSG in error state, not allowed to restart\n");
-		return -ENOTRECOVERABLE;
+		rc = -ENOTRECOVERABLE;
+		goto err_bad_state;
 	}
 
 	context->state = VSG_STATE_STARTED;
 	hrtimer_start(&context->threshold_timer, ns_to_ktime(context->
 			max_frame_interval), HRTIMER_MODE_REL);
-	return 0;
+
+err_bad_state:
+	mutex_unlock(&context->mutex);
+	return rc;
 }
 
 static int vsg_stop(struct v4l2_subdev *sd)
