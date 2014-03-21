ded6954becb87f9a5f504757ca1a86d795f240b8
Matt Wagantall <mattw@codeaurora.org>
2014-03-21 00:13:12 -0700

    msm: krait-l2-accessors: Remove set_get_l2_indirect_reg() API
    There are no remaining users of this API, which writes to an indirect
    register and reads back the new value. To simplify the future addition
    of RTB logging for L2 indirect CP15 accesses, remove the unused API.
    
    set_l2_indirect_reg() and get_l2_indirect_reg() can be individually
    called to accomplish the same purpose that set_get_l2_indirect_reg()
    served, with a negligible performance difference for common usecases.
    
    Change-Id: If372370eabda7cf5d9eee488222a17e139609a36
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

--- arch/arm/mach-msm/msm-krait-l2-accessors.c
+++ arch/arm/mach-msm/msm-krait-l2-accessors.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2012, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2011-2012, 2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -17,27 +17,6 @@
 #include <asm/cputype.h>
 
 DEFINE_RAW_SPINLOCK(l2_access_lock);
-
-u32 set_get_l2_indirect_reg(u32 reg_addr, u32 val)
-{
-	unsigned long flags;
-	u32 ret_val;
-
-	raw_spin_lock_irqsave(&l2_access_lock, flags);
-	mb();
-	asm volatile ("mcr     p15, 3, %[l2cpselr], c15, c0, 6\n\t"
-		      "isb\n\t"
-		      "mcr     p15, 3, %[l2cpdr],   c15, c0, 7\n\t"
-		      "isb\n\t"
-		      "mrc p15, 3, %[l2cpdr_read], c15, c0, 7\n\t"
-			: [l2cpdr_read]"=r" (ret_val)
-			: [l2cpselr]"r" (reg_addr), [l2cpdr]"r" (val)
-	);
-	raw_spin_unlock_irqrestore(&l2_access_lock, flags);
-
-	return ret_val;
-}
-EXPORT_SYMBOL(set_get_l2_indirect_reg);
 
 void set_l2_indirect_reg(u32 reg_addr, u32 val)
 {
