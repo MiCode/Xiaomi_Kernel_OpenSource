56431e732646122a8332706b7c1e0d7ee331459a
Tanya Finkel <tfinkel@codeaurora.org>
2014-03-24 16:00:31 +0200

    msm: Fix camera framework NULL pointer
    Add validation of the function parameters that avoid using of the NULL
    pointer further in the code.
    
    Change-Id: I2b36a9e907cb4c384fea1fa7cb32811f03a22a70
    Signed-off-by: Tanya Finkel <tfinkel@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_io_util.c
+++ drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_io_util.c
@@ -476,6 +476,13 @@ int msm_camera_config_single_vreg(struct device *dev,
 	int rc = 0;
 	if (config) {
 		CDBG("%s enable %s\n", __func__, cam_vreg->reg_name);
+
+		if (dev == NULL || cam_vreg == NULL ||
+			reg_ptr == NULL || *reg_ptr == NULL) {
+			pr_err("%s: get failed NULL parameter\n", __func__);
+			goto vreg_get_fail;
+		}
+
 		*reg_ptr = regulator_get(dev, cam_vreg->reg_name);
 		if (IS_ERR(*reg_ptr)) {
 			pr_err("%s: %s get failed\n", __func__,
