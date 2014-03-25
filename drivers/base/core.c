cb53cd1f39285360ba4ebf57f5924ee93550dd92
Sravan Kumar Ambapuram <asravan@codeaurora.org>
2014-03-25 15:00:54 +0530

    drivercore: Avoid adding devices without pm_ops to dpm list
    Devices without pm_ops are not required to be added in dpm list.
    It also helps in improving suspend/resume latencies.
    
    Change-Id: I19a85742fdb6f4d01363b69662ea6d7721a08b3a
    Signed-off-by: Sravan Kumar Ambapuram <asravan@codeaurora.org>

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -1084,7 +1084,13 @@ int device_add(struct device *dev)
 	error = dpm_sysfs_add(dev);
 	if (error)
 		goto DPMError;
-	device_pm_add(dev);
+	if ((dev->pm_domain) || (dev->type && dev->type->pm)
+		|| (dev->class && (dev->class->pm || dev->class->resume))
+		|| (dev->bus && (dev->bus->pm || dev->bus->resume)) ||
+		(dev->driver && dev->driver->pm)) {
+		device_pm_add(dev);
+	}
+
 
 	/* Notify clients of device addition.  This call must come
 	 * after dpm_sysfs_add() and before kobject_uevent().
