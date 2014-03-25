ac1defc4d318f8782a8fc5ecdef819f78e11497d
Steve Muckle <smuckle@codeaurora.org>
2014-03-25 14:27:16 -0700

    sched: run idle_balance() on most power-efficient CPU
    When a CPU goes idle, it checks to see whether it can pull any load
    from other busy CPUs. The CPU going idle may not be the most
    power-efficient idle CPU in the system however.
    
    This patch causes the CPU going idle to check to see whether
    there is a more power-efficient idle CPU within the same
    lowest sched domain. If there is, then it runs the load balancer
    on behalf of that CPU instead of itself.
    
    Since it is unknown at this point what task load would be pulled,
    a frequency must be assumed for this in order to do a comparison
    of CPU power consumption. The maximum freqeuncy supported by all
    CPUs is used for this.
    
    Change-Id: I5eedddc1f7d10df58ecd358f37dba563eeecf4fc
    Signed-off-by: Steve Muckle <smuckle@codeaurora.org>
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1739,6 +1739,11 @@ static inline int power_cost(struct task_struct *p, int cpu)
 	return SCHED_POWER_SCALE;
 }
 
+static unsigned int power_cost_at_freq(int cpu, unsigned int freq)
+{
+	return 1;
+}
+
 static inline int mostly_idle_cpu(int cpu)
 {
 	return 0;
@@ -6075,20 +6080,48 @@ void idle_balance(int this_cpu, struct rq *this_rq)
 	struct sched_domain *sd;
 	int pulled_task = 0;
 	unsigned long next_balance = jiffies + HZ;
+	int i, cost;
+	int min_power = INT_MAX;
+	int balance_cpu = -1;
+	struct rq *balance_rq = NULL;
 
 	this_rq->idle_stamp = this_rq->clock;
 
 	if (this_rq->avg_idle < sysctl_sched_migration_cost)
 		return;
 
+	/* If this CPU is not the most power-efficient idle CPU in the
+	 * lowest level domain, run load balance on behalf of that
+	 * most power-efficient idle CPU. */
+	rcu_read_lock();
+	sd = rcu_dereference_check_sched_domain(this_rq->sd);
+	if (sd && sysctl_sched_enable_power_aware) {
+		for_each_cpu(i, sched_domain_span(sd)) {
+			if (i == this_cpu || idle_cpu(i)) {
+				cost = power_cost_at_freq(i, 0);
+				if (cost < min_power) {
+					min_power = cost;
+					balance_cpu = i;
+				}
+			}
+		}
+		BUG_ON(balance_cpu == -1);
+
+	} else {
+		balance_cpu = this_cpu;
+	}
+	rcu_read_unlock();
+	balance_rq = cpu_rq(balance_cpu);
+
+
 	/*
 	 * Drop the rq->lock, but keep IRQ/preempt disabled.
 	 */
 	raw_spin_unlock(&this_rq->lock);
 
-	update_blocked_averages(this_cpu);
+	update_blocked_averages(balance_cpu);
 	rcu_read_lock();
-	for_each_domain(this_cpu, sd) {
+	for_each_domain(balance_cpu, sd) {
 		unsigned long interval;
 		int balance = 1;
 
@@ -6097,15 +6130,18 @@ void idle_balance(int this_cpu, struct rq *this_rq)
 
 		if (sd->flags & SD_BALANCE_NEWIDLE) {
 			/* If we've pulled tasks over stop searching: */
-			pulled_task = load_balance(this_cpu, this_rq,
-						   sd, CPU_NEWLY_IDLE, &balance);
+			pulled_task = load_balance(balance_cpu, balance_rq,
+						   sd,
+						   (this_cpu == balance_cpu ?
+						    CPU_NEWLY_IDLE :
+						    CPU_IDLE), &balance);
 		}
 
 		interval = msecs_to_jiffies(sd->balance_interval);
 		if (time_after(next_balance, sd->last_balance + interval))
 			next_balance = sd->last_balance + interval;
 		if (pulled_task) {
-			this_rq->idle_stamp = 0;
+			balance_rq->idle_stamp = 0;
 			break;
 		}
 	}
@@ -6113,7 +6149,8 @@ void idle_balance(int this_cpu, struct rq *this_rq)
 
 	raw_spin_lock(&this_rq->lock);
 
-	if (!pulled_task || time_after(jiffies, this_rq->next_balance)) {
+	if (balance_cpu == this_cpu &&
+	    (!pulled_task || time_after(jiffies, this_rq->next_balance))) {
 		/*
 		 * We are going idle. next_balance may be set based on
 		 * a busy processor. So reset next_balance.
