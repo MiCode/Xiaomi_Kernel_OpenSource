a16261c8314a3122edf2a6ba526ce4c378a6688e
Shubhraprakash Das <sadas@codeaurora.org>
2014-03-26 16:06:29 -0700

    msm: kgsl: Increase the timeout value for fault detection
    Increment the timeout value for detecting faults from 50ms to
    200ms. The fault detection mechanism is a backup to hardware
    fault detection. 50ms is small since it has been observed that
    this mechanism triggers false positives with 50ms.
    
    CRs-Fixed: 630776
    Change-Id: I8f382051e06bdc47f152309a2868092e1980604e
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -50,7 +50,7 @@ static unsigned int _dispatcher_inflight = 15;
 static unsigned int _cmdbatch_timeout = 2000;
 
 /* Interval for reading and comparing fault detection registers */
-static unsigned int _fault_timer_interval = 50;
+static unsigned int _fault_timer_interval = 200;
 
 /* Local array for the current set of fault detect registers */
 static unsigned int fault_detect_regs[FT_DETECT_REGS_COUNT];
@@ -1608,13 +1608,7 @@ void adreno_dispatcher_irq_fault(struct kgsl_device *device)
  */
 void adreno_dispatcher_start(struct kgsl_device *device)
 {
-	struct adreno_device *adreno_dev = ADRENO_DEVICE(device);
-
 	complete_all(&device->cmdbatch_gate);
-
-	/* a305c GPU is slower than a330 and needs a larger timer */
-	if (adreno_is_a305c(adreno_dev))
-		_fault_timer_interval = 200;
 
 	/* Schedule the work loop to get things going */
 	adreno_dispatcher_schedule(device);
