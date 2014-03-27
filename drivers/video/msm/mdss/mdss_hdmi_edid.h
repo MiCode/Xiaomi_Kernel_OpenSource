638affbac5919244bd450f2bf9b0c6270e6dac92
Casey Piper <cpiper@codeaurora.org>
2014-03-26 18:22:37 -0700

    linux: Move hdmi audio codec header to common location
    Move contents of msm_hdmi_audio_codec.h
    directory to msm_hdmi.h in common linux
    directory to remove platform dependence.
    
    Change-Id: I6331073ba1e5e119770c5e8cb50f6ff677807292
    Signed-off-by: Casey Piper <cpiper@codeaurora.org>

--- drivers/video/msm/mdss/mdss_hdmi_edid.h
+++ drivers/video/msm/mdss/mdss_hdmi_edid.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2010-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -13,7 +13,7 @@
 #ifndef __HDMI_EDID_H__
 #define __HDMI_EDID_H__
 
-#include <mach/msm_hdmi_audio_codec.h>
+#include <linux/msm_hdmi.h>
 #include "mdss_hdmi_util.h"
 
 struct hdmi_edid_init_data {
