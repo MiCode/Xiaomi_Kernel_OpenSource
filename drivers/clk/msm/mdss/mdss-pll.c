007623f80021f4f77acca3578a83796680258ae6
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2014-03-27 20:03:11 +0530

    clk: qcom: mdss: split the DSI PLL driver based on PLL mode
    Re-organize the DSI PLL driver code and split it based on
    the DSI PLL HPM/LPM mode. Add a common PLL util file to use
    the APIs which are common for both PLLs. Update the DSI PLL
    enable sequence with the recommended settings for LPM mode.
    
    Change-Id: I3f86554522e16579d5c2eccab976136c7afb0dd2
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- drivers/clk/msm/mdss/mdss-pll.c
+++ drivers/clk/msm/mdss/mdss-pll.c
@@ -110,15 +110,19 @@ static int mdss_pll_resource_parse(struct platform_device *pdev,
 		goto err;
 	}
 
-	if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_8916") ||
-		!strcmp(compatible_stream, "qcom,mdss_dsi_pll_8974"))
-		pll_res->pll_interface_type = MDSS_DSI_PLL;
-	else if (!strcmp(compatible_stream, "qcom,mdss_edp_pll"))
+	if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_8916")) {
+		pll_res->pll_interface_type = MDSS_DSI_PLL_LPM;
+		pll_res->target_id = MDSS_PLL_TARGET_8916;
+	} else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_8974")) {
+		pll_res->pll_interface_type = MDSS_DSI_PLL_HPM;
+		pll_res->target_id = MDSS_PLL_TARGET_8974;
+	} else if (!strcmp(compatible_stream, "qcom,mdss_edp_pll")) {
 		pll_res->pll_interface_type = MDSS_EDP_PLL;
-	else if (!strcmp(compatible_stream, "qcom,mdss_hdmi_pll"))
+	} else if (!strcmp(compatible_stream, "qcom,mdss_hdmi_pll")) {
 		pll_res->pll_interface_type = MDSS_HDMI_PLL;
-	else
+	} else {
 		goto err;
+	}
 
 	return rc;
 
@@ -139,8 +143,11 @@ static int mdss_pll_clock_register(struct platform_device *pdev,
 	}
 
 	switch (pll_res->pll_interface_type) {
-	case MDSS_DSI_PLL:
-		rc = dsi_pll_clock_register(pdev, pll_res);
+	case MDSS_DSI_PLL_LPM:
+		rc = dsi_pll_clock_register_lpm(pdev, pll_res);
+		break;
+	case MDSS_DSI_PLL_HPM:
+		rc = dsi_pll_clock_register_hpm(pdev, pll_res);
 		break;
 	case MDSS_EDP_PLL:
 		rc = edp_pll_clock_register(pdev, pll_res);
