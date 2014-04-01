067e0796cfae296797e67ab0808d155b6308962e
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-03-31 17:32:30 -0700

    sched: Track number of big and small tasks on a cpu
    This patch adds 'nr_big_tasks' and 'nr_small_tasks' per-cpu counters
    that tracks number of big and small tasks on a cpu respectively. This
    will be used in load balance decisions introduced in a subsequent
    patch.
    
    Change-Id: Ia174904140f81dd6d1946286889a50be3f16ea83
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    [rameezmustafa@codeaurora.org]: Port to msm-3.18]
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1567,6 +1567,8 @@ update_history(struct rq *rq, struct task_struct *p, u32 runtime, int samples)
 	if (p->on_rq) {
 		rq->cumulative_runnable_avg -= p->ravg.demand;
 		BUG_ON((s64)rq->cumulative_runnable_avg < 0);
+		if (p->sched_class == &fair_sched_class)
+			dec_nr_big_small_task(rq, p);
 	}
 
 	avg = div64_u64(sum, RAVG_HIST_SIZE);
@@ -1580,8 +1582,11 @@ update_history(struct rq *rq, struct task_struct *p, u32 runtime, int samples)
 
 	p->ravg.demand = demand;
 
-	if (p->on_rq)
+	if (p->on_rq) {
 		rq->cumulative_runnable_avg += p->ravg.demand;
+		if (p->sched_class == &fair_sched_class)
+			inc_nr_big_small_task(rq, p);
+	}
 }
 
 static int __init set_sched_ravg_window(char *str)
@@ -7358,6 +7363,12 @@ static int cpufreq_notifier_policy(struct notifier_block *nb,
 	load_scale *= load_scale_cpu_freq(cpu);
 	load_scale >>= 10;
 
+	/*
+	 * Changed load_scale_factor can trigger reclassification of tasks as
+	 * big or small. Make this change "atomic" so that tasks are accounted
+	 * properly due to changed load_scale_factor
+	 */
+	pre_big_small_task_count_change();
 	for_each_cpu(i, policy->related_cpus) {
 		struct rq *rq = cpu_rq(i);
 
@@ -7366,6 +7377,7 @@ static int cpufreq_notifier_policy(struct notifier_block *nb,
 	}
 
 	update_min_max_capacity();
+	post_big_small_task_count_change();
 
 	return 0;
 }
@@ -7540,6 +7552,9 @@ void __init sched_init(void)
 		rq->rt.rt_runtime = def_rt_bandwidth.rt_runtime;
 #ifdef CONFIG_RT_GROUP_SCHED
 		init_tg_rt_entry(&root_task_group, &rq->rt, NULL, i, NULL);
+#endif
+#ifdef CONFIG_SCHED_HMP
+		rq->nr_small_tasks = rq->nr_big_tasks = 0;
 #endif
 
 		for (j = 0; j < CPU_LOAD_IDX_MAX; j++)
