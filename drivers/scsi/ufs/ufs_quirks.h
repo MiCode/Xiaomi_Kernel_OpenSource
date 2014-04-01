61df89e001cbb30e16b63e2d260cfd5126ae7cc9
Raviv Shvili <rshvili@codeaurora.org>
2014-04-01 22:20:15 +0300

    scsi: ufs: seperate device and host quirks
    Currently we use the host quirks mechanism in order to
    handle both device and host controller quirks.
    In order to support many UFS devices we should separate
    handling the device quirks from the host controller's.
    
    Change-Id: I6c23be42ff78689f9aad4d5c0c9f0a678bfb5c14
    Signed-off-by: Raviv Shvili <rshvili@codeaurora.org>
    [venkatg@codeaurora.org: resolved trivial merge conflicts]
    Signed-off-by: Venkat Gopalakrishnan <venkatg@codeaurora.org>

---
+++ drivers/scsi/ufs/ufs_quirks.h
@@ -0,0 +1,62 @@
+/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _UFS_QUIRKS_H_
+#define _UFS_QUIRKS_H_
+
+/* return true if s1 is a prefix of s2 */
+#define STR_PRFX_EQUAL(s1, s2) !strncmp(s1, s2, strlen(s1))
+
+#define UFS_ANY_VENDOR -1
+#define UFS_ANY_MODEL  "ANY_MODEL"
+
+#define MAX_MODEL_LEN 16
+
+#define UFS_VENDOR_TOSHIBA 0x98
+/* UFS TOSHIBA MODELS */
+#define UFS_MODEL_TOSHIBA_32GB "THGLF2G8D4KBADR"
+#define UFS_MODEL_TOSHIBA_64GB "THGLF2G9D8KBADG"
+
+/**
+ * ufs_card_info - ufs device details
+ * @vendor: card details
+ * @model: card model
+ */
+struct ufs_card_info {
+	unsigned int vendor;
+	char *model;
+};
+
+/**
+ * ufs_card_fix - ufs device quirk info
+ * @card: ufs card details
+ * @quirk: device quirk
+ */
+struct ufs_card_fix {
+	struct ufs_card_info card;
+	unsigned int quirk;
+};
+
+#define END_FIX { { 0 } , 0 }
+
+/* add specific device quirk */
+#define UFS_FIX(_vendor, _model, _quirk) \
+		{					 \
+				.card.vendor = (_vendor),\
+				.card.model = (_model),	 \
+				.quirk = (_quirk),	 \
+		}
+
+struct ufs_hba;
+void ufs_advertise_fixup_device(struct ufs_hba *hba);
+#endif /* UFS_QUIRKS_H_ */
