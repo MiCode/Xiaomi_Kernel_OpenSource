b3d0c5b7a46353f9d709b63febff51462efc0ab6
Ravi Aravamudhan <aravamud@codeaurora.org>
2014-04-01 11:34:39 -0700

    diag: Separate SMUX structures from diag
    Diag driver is deeply coupled with SMUX structures. Make changes
    to remove references of SMUX from diag core files.
    
    Change-Id: I2abc6b98659fea5b5b429467af9ccf5ed3d37eaf
    Signed-off-by: Ravi Aravamudhan <aravamud@codeaurora.org>

--- drivers/char/diag/diagfwd_smux.h
+++ drivers/char/diag/diagfwd_smux.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012,2014 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -16,6 +16,17 @@
 #include <linux/smux.h>
 #define LCID_VALID	SMUX_USB_DIAG_0
 #define LCID_INVALID	0
+
+struct diag_smux_info {
+	int lcid;
+	unsigned char *read_buf;
+	int read_len;
+	int in_busy;
+	int enabled;
+	int connected;
+};
+
+extern struct diag_smux_info *diag_smux;
 
 int diagfwd_read_complete_smux(void);
 int diagfwd_write_complete_smux(void);
