703fa498e3b8050a9d9c772b324ebe8f90988b83
Ravit Katzav <rkatzav@codeaurora.org>
2014-04-07 12:33:31 -0700

    msm: ipa: enable clock scaling by default
    Enable IPA clock scaling feature by default.
    IPA clock frequency is set according to RM performance setting.
    
    Change-Id: Icf226ab9a58b34876469e0208eeb6859f1b21a0e
    Acked-by: Ady Abraham <adya@qti.qualcomm.com>
    Signed-off-by: Ravit Katzav <rkatzav@codeaurora.org>

--- drivers/platform/msm/ipa/ipa.c
+++ drivers/platform/msm/ipa/ipa.c
@@ -2009,7 +2009,7 @@ static int ipa_init(const struct ipa_plat_drv_res *resource_p,
 		goto fail_bind;
 	}
 
-	ipa_ctx->enable_clock_scaling = 0;
+	ipa_ctx->enable_clock_scaling = 1;
 	ipa_ctx->curr_ipa_clk_rate = ipa_ctx->ctrl->ipa_clk_rate_hi;
 
 	/* enable IPA clocks explicitly to allow the initialization */
