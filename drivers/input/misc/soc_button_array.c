4a654fa9b456082d008d946281a657af0c96f981
Faouaz TENOUTIT <faouaz.tenoutit@intel.com>
2014-04-07 12:25:53 +0200

    input: soc_button: Fix HOME button id
    Change-Id: I9e39ae5318afcb039694832e62f0e4237f248d34
    Signed-off-by: Faouaz TENOUTIT <faouaz.tenoutit@intel.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/input/misc/soc_button_array.c
+++ drivers/input/misc/soc_button_array.c
@@ -39,7 +39,7 @@ struct soc_button_info {
 
 static struct soc_button_info soc_button_tbl[] = {
 	{"power", 0, KEY_POWER, 0, 1, -1},
-	{"home", 1, KEY_HOME, 0, 1, -1},
+	{"home", 1, KEY_HOMEPAGE, 0, 1, -1},
 	{"volume_up", 2, KEY_VOLUMEUP, 1, 0, -1},
 	{"volume_down", 3, KEY_VOLUMEDOWN, 1, 0, -1},
 	{"rotation_lock", 4, KEY_RO, 0, 0, -1},
