e177077c33f1ec5418df6ae829facec7d3538423
Ujwal Patel <ujwalp@codeaurora.org>
2014-04-07 18:50:31 -0700

    msm: mdss: fix multiple overlay release sequence
    Overlay release allows multiple releases to happen by having
    multiple bits set in overlay unset call. Fix logic to find
    all pipes which match the bitmap.
    
    Change-Id: I6a25a81fe4c437b5546537d48e250055357682f0
    Signed-off-by: Ujwal Patel <ujwalp@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_mdp_overlay.c
+++ drivers/video/fbdev/msm/mdss_mdp_overlay.c
@@ -1371,7 +1371,7 @@ static int mdss_mdp_overlay_release(struct msm_fb_data_type *mfd, int ndx)
 				continue;
 			}
 
-			unset_ndx |= ndx;
+			unset_ndx |= pipe->ndx;
 
 			pipe->pid = 0;
 			destroy_pipe = pipe->play_cnt == 0;
