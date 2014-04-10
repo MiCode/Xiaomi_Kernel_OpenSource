8e05b3e8365e3ea6909ea6037998a82652657e93
Ian Maund <imaund@codeaurora.org>
2014-04-09 18:21:12 -0700

    Revert "genirq: Provide an accessor for IRQ_PER_CPU flag"
    This reverts commit 2868c0348b3158bb3993fdb0eef4ad27aec5626a. An upstream
    patch, providing the same functionality, has been added, allowing this
    commit to be removed.
    
    Change-Id: I1ae50c0efb1ef560c8a3ff257fada3edfaf328ea
    Signed-off-by: Ian Maund <imaund@codeaurora.org>

--- include/linux/irqdesc.h
+++ include/linux/irqdesc.h
@@ -152,14 +152,6 @@ static inline int irq_balancing_disabled(unsigned int irq)
 	return desc->status_use_accessors & IRQ_NO_BALANCING_MASK;
 }
 
-static inline int irq_is_per_cpu(unsigned int irq)
-{
-        struct irq_desc *desc;
-
-        desc = irq_to_desc(irq);
-        return desc->status_use_accessors & IRQ_PER_CPU;
-}
-
 static inline int irq_is_percpu(unsigned int irq)
 {
 	struct irq_desc *desc;
