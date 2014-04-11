3ad2306110e9e8ff796ca5324f37b74622175a34
Saket Saurabh <ssaurabh@codeaurora.org>
2014-04-11 16:59:57 +0530

    phy-msm-usb: Set pm_done to true only when usb is in lpm
    As part of USB disconnect event, msm_otg_suspend() executes and USB
    enters into low power mode and pm_done flag is set to true. In the
    scenario in which before the msm_otg_suspend() finished executing,
    then USB connect event came in, in this case msm_otg_suspend() returns
    -EBUSY. And later USB connect event is processed. As device is already
    in resumed state, hence pm_done flag is never set to false. So on
    next USB connect pm usage counter gets incremented in msm_otg_sm_work()
    and device cannot enter into sleep state.
    
    Fix the issue by setting pm_done flag to true as part of USB disconnect
    event only when USB finished executing msm_otg_suspend() and is able to
    enter low power mode.
    
    CRs-fixed: 649434
    Change-Id: Icd8d612ce6280300d0d97656f3dcf1921b45126f
    Signed-off-by: Saket Saurabh <ssaurabh@codeaurora.org>

--- include/linux/usb/msm_hsusb.h
+++ include/linux/usb/msm_hsusb.h
@@ -370,7 +370,10 @@ struct msm_otg_platform_data {
  * @chg_check_timer: The timer used to implement the workaround to detect
  *               very slow plug in of wall charger.
  * @ui_enabled: USB Intterupt is enabled or disabled.
- * @pm_done: Indicates whether USB is PM resumed
+ * @pm_done: It is used to increment the pm counter using pm_runtime_get_sync.
+	     This handles the race case when PM resume thread returns before
+	     the charger detection starts. When USB is disconnected and in lpm
+	     pm_done is set to true.
  * @ext_id_irq: IRQ for ID interrupt.
  */
 struct msm_otg {
