57f75333b002d3236cc3fe81ae590d592ad27ed4
Sree Sesha Aravind Vadrevu <svadrevu@codeaurora.org>
2014-04-11 19:39:00 -0700

    msm: mdss: synchronize formats across rotator output and pipe setup
    The rules applied to switch formats must be the same across
    rotator output and the following pipe setup. Also, yuv rotator
    output format should always be set to pseudo planar for MDP
    hardware optimality.
    
    Change-Id: I3f5f09bcc599627e47495fab253ec0d9d8d5fa1c
    Signed-off-by: Sree Sesha Aravind Vadrevu <svadrevu@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_intf_writeback.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_writeback.c
@@ -313,14 +313,11 @@ static int mdss_mdp_writeback_prepare_rot(struct mdss_mdp_ctl *ctl, void *arg)
 		return -EINVAL;
 	}
 
-	if (ctx->bwc_mode || (ctx->rot90 &&
-			((mdata->mdp_rev < MDSS_MDP_HW_REV_102)
-			|| !fmt->is_yuv)))
-		format = mdss_mdp_get_rotator_dst_format(rot->format, 1,
-			ctx->bwc_mode);
+	if (ctx->bwc_mode || ctx->rot90)
+		format = mdss_mdp_get_rotator_dst_format(rot->format,
+				ctx->rot90, ctx->bwc_mode);
 	else
-		format = mdss_mdp_get_rotator_dst_format(rot->format, 0,
-			ctx->bwc_mode);
+		format = rot->format;
 
 	if (ctx->rot90) {
 		ctx->opmode |= BIT(5); /* ROT 90 */
