1d7e842fa8ca10e1b98bbbf4c866b0979ea71549
Laura Abbott <lauraa@codeaurora.org>
2014-04-14 19:42:04 -0700

    arm: Allow remapping lowmem as 4K pages
    Lowmem is currently mapped with sections (1MB/2MB) whenever
    possible due to TLB performance boosts. Sections cannot easily
    be changed at runtime however which makes implementing certain
    features annoying. Add an option to map lowmem with 4K patches.
    This is intended to be used as a debugging feature and should
    NOT be used for performance testing.
    
    Change-Id: I9612a99b8e05a022f5ba7e568f21307cf66b5667
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    Signed-off-by: Susheel Khiani <skhiani@codeaurora.org>
    Signed-off-by: Charan Teja Reddy <charante@codeaurora.org>
    Signed-off-by: Vijayanand Jitta <vjitta@codeaurora.org>
    Signed-off-by: Qingqing Zhou <qqzhou@codeaurora.org>

--- arch/arm/mm/pageattr.c
+++ arch/arm/mm/pageattr.c
@@ -56,7 +56,8 @@ static int change_memory_common(unsigned long addr, int numpages,
 	if (!size)
 		return 0;
 
-	if (!in_range(start, size, MODULES_VADDR, MODULES_END) &&
+	if (!IS_ENABLED(CONFIG_FORCE_PAGES) &&
+	    !in_range(start, size, MODULES_VADDR, MODULES_END) &&
 	    !in_range(start, size, VMALLOC_START, VMALLOC_END))
 		return -EINVAL;
 
