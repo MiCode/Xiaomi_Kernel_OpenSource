8fb9711a1d1acf1133a8bf6c4ac647944413e67e
Mitchel Humpherys <mitchelh@codeaurora.org>
2014-04-15 14:49:03 -0700

    ion: move some UAPI header files
    Move some UAPI header files to align with what upstream is doing.
    
    Change-Id: Ie7679f7fde12c3c912662df7c1ff9eb4bf4a864a
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- include/uapi/linux/ion.h
+++ include/uapi/linux/ion.h
@@ -1,6 +1,6 @@
 #ifndef __UAPI_LINUX_ION_H__
 #define __UAPI_LINUX_ION_H__
 
-#include "../../drivers/staging/android/linux/ion.h"
+#include <linux/staging/android/uapi/ion.h>
 
 #endif /* __UAPI_LINUX_ION_H__ */
