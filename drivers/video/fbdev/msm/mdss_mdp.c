58e6d3f6c9e0f33f06c48d798535605aa96b89d7
Aravind Venkateswaran <aravindh@codeaurora.org>
2014-04-17 16:18:02 -0700

    mdss: mdp: do not send panel events for idle power collapse
    When mdss idle power collapse is enabled, panel suspend and resume
    events should not be sent to ensure that panel drivers remain active.
    
    Change-Id: Ifbcdee69432cbd0d30f16c86af9c126d9443e269
    Signed-off-by: Aravind Venkateswaran <aravindh@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_mdp.c
+++ drivers/video/fbdev/msm/mdss_mdp.c
@@ -2914,7 +2914,10 @@ static int mdss_mdp_runtime_resume(struct device *dev)
 		return -ENODEV;
 
 	dev_dbg(dev, "pm_runtime: resuming...\n");
-	device_for_each_child(dev, &device_on, mdss_fb_suspres_panel);
+
+	/* do not resume panels when coming out of idle power collapse */
+	if (!mdata->idle_pc)
+		device_for_each_child(dev, &device_on, mdss_fb_suspres_panel);
 	mdss_mdp_footswitch_ctrl(mdata, true);
 
 	return 0;
@@ -2943,7 +2946,10 @@ static int mdss_mdp_runtime_suspend(struct device *dev)
 		pr_err("MDP suspend failed\n");
 		return -EBUSY;
 	}
-	device_for_each_child(dev, &device_on, mdss_fb_suspres_panel);
+
+	/* do not suspend panels when going in to idle power collapse */
+	if (!mdata->idle_pc)
+		device_for_each_child(dev, &device_on, mdss_fb_suspres_panel);
 	mdss_mdp_footswitch_ctrl(mdata, false);
 
 	return 0;
