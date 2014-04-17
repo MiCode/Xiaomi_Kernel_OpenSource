75002595ef3327cf0d2335df34c82c9efcf587b7
Manoj Rao <manojraj@codeaurora.org>
2014-04-16 18:27:23 -0700

    msm: mdss: fb: remove unused page protection flags
    In mmap implementation for mdss-fb, the argument for page protection
    flags were used only in the legacy MDP drivers. Remove these
    these unused flags.
    
    Change-Id: I5d67008b5b61165d2cc87ac15e5ef544dcdf819f
    Signed-off-by: Manoj Rao <manojraj@codeaurora.org>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -983,27 +983,6 @@ static int mdss_fb_blank(int blank_mode, struct fb_info *info)
 	return mdss_fb_blank_sub(blank_mode, info, mfd->op_enable);
 }
 
-/* Set VM page protection */
-static inline void __mdss_fb_set_page_protection(struct vm_area_struct *vma,
-		struct msm_fb_data_type *mfd)
-{
-	if (mfd->mdp_fb_page_protection == MDP_FB_PAGE_PROTECTION_WRITECOMBINE)
-		vma->vm_page_prot = pgprot_writecombine(vma->vm_page_prot);
-	else if (mfd->mdp_fb_page_protection ==
-			MDP_FB_PAGE_PROTECTION_WRITETHROUGHCACHE)
-		vma->vm_page_prot = pgprot_writethroughcache(vma->vm_page_prot);
-	else if (mfd->mdp_fb_page_protection ==
-			MDP_FB_PAGE_PROTECTION_WRITEBACKCACHE)
-		vma->vm_page_prot = pgprot_writebackcache(vma->vm_page_prot);
-	else if (mfd->mdp_fb_page_protection ==
-			MDP_FB_PAGE_PROTECTION_WRITEBACKWACACHE)
-		vma->vm_page_prot = pgprot_writebackwacache(vma->vm_page_prot);
-	else
-		vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
-
-
-}
-
 static inline int mdss_fb_create_ion_client(struct msm_fb_data_type *mfd)
 {
 	mfd->fb_ion_client  = msm_ion_client_create(-1 , "mdss_fb_iclient");
@@ -1188,8 +1167,6 @@ static int mdss_fb_fbmem_ion_mmap(struct fb_info *info,
 				offset = 0;
 			}
 			len = min(len, remainder);
-
-			__mdss_fb_set_page_protection(vma, mfd);
 
 			pr_debug("vma=%p, addr=%x len=%ld",
 					vma, (unsigned int)addr, len);
