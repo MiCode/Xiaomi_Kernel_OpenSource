a8c645cb00980e807d90c3be2cc0fb8d23a59811
Chintan Pandya <cpandya@codeaurora.org>
2014-04-18 12:45:36 +0530

    dma-contiguous: Retrun 'zero' pfn in case of error
    When CMA allocator gets error return from the page
    allocator framework, except for the -EBUSY case, it
    will bail out. Caller depends on the 'pfn' for
    confirming allocation successful or not. Return 0
    for those error case.
    
    Change-Id: Ica4e04a9f9f18b1a29035ba2bae9deecfd68a5e8
    Signed-off-by: Chintan Pandya <cpandya@codeaurora.org>

--- drivers/base/dma-contiguous.c
+++ drivers/base/dma-contiguous.c
@@ -627,6 +627,7 @@ unsigned long dma_alloc_from_contiguous(struct device *dev, int count,
 		if (ret == 0) {
 			break;
 		} else if (ret != -EBUSY) {
+			pfn = 0;
 			clear_cma_bitmap(cma, pfn, count);
 			break;
 		}
