e4fc5882272c268f57166ee475a7df34fd0658d0
Aravind Venkateswaran <aravindh@codeaurora.org>
2014-04-22 12:25:58 -0700

    ARM: dts: msm: add mdss gdsc supply to dsi controller device
    It is possible for the DSI controller to be active when MDP is
    power collapsed. DSI controller needs to have it's own vote for
    mdss gdsc to ensure that gdsc remains on in such cases.
    
    Change-Id: I37f98c6e4f6d30908373b812fc50e29ba001b752
    Signed-off-by: Aravind Venkateswaran <aravindh@codeaurora.org>
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi.h
+++ drivers/video/msm/mdss/mdss_dsi.h
@@ -105,6 +105,7 @@ enum dsi_lane_map_type {
 };
 
 enum dsi_pm_type {
+	DSI_CORE_PM,
 	DSI_CTRL_PM,
 	DSI_PANEL_PM,
 	DSI_MAX_PM
@@ -394,6 +395,7 @@ int mdss_panel_get_dst_fmt(u32 bpp, char mipi_mode, u32 pixel_packing,
 static inline const char *__mdss_dsi_pm_name(enum dsi_pm_type module)
 {
 	switch (module) {
+	case DSI_CORE_PM:	return "DSI_CORE_PM";
 	case DSI_CTRL_PM:	return "DSI_CTRL_PM";
 	case DSI_PANEL_PM:	return "PANEL_PM";
 	default:		return "???";
@@ -404,6 +406,7 @@ static inline const char *__mdss_dsi_pm_supply_node_name(
 	enum dsi_pm_type module)
 {
 	switch (module) {
+	case DSI_CORE_PM:	return "qcom,core-supply-entries";
 	case DSI_CTRL_PM:	return "qcom,ctrl-supply-entries";
 	case DSI_PANEL_PM:	return "qcom,panel-supply-entries";
 	default:		return "???";
