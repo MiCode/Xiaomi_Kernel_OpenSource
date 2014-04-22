40bd1e7d6c996b2289f93aad6f123d9740eced17
Prasad Sodagudi <psodagud@codeaurora.org>
2014-04-22 12:00:48 +0530

    soc: qcom: watchdog-v2: Update last_pet during the suspend and resume
    Currently last_pet time variable is not updated properly during
    the device suspend and resume. When watchdog bark is reported,
    last_pet does not have proper value and leading to
    misinterpretation on watchdog's last pet. Hence update
    the last_pet during the suspend and resume.
    
    Change-Id: I92b8b15e0830985415b3a3910c23c5aa3bebd6b3
    Signed-off-by: Prasad Sodagudi <psodagud@codeaurora.org>

--- drivers/soc/qcom/watchdog_v2.c
+++ drivers/soc/qcom/watchdog_v2.c
@@ -106,6 +106,7 @@ static int msm_watchdog_suspend(struct device *dev)
 	__raw_writel(0, wdog_dd->base + WDT0_EN);
 	mb();
 	wdog_dd->enabled = false;
+	wdog_dd->last_pet = sched_clock();
 	return 0;
 }
 
@@ -119,6 +120,7 @@ static int msm_watchdog_resume(struct device *dev)
 	__raw_writel(1, wdog_dd->base + WDT0_RST);
 	mb();
 	wdog_dd->enabled = true;
+	wdog_dd->last_pet = sched_clock();
 	return 0;
 }
 
