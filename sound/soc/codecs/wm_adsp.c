508620f05525605e14b265a227e93e11fcebd908
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2014-04-24 15:58:58 +0100

    ASoC: wm_adsp: Add firmware option for Ez2Record
    Change-Id: Ieff59d7665273bc2b6d33bcf3c1a08d0dfb2f947
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
    Signed-off-by: Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>

--- sound/soc/codecs/wm_adsp.c
+++ sound/soc/codecs/wm_adsp.c
@@ -197,7 +197,7 @@ static void wm_adsp_buf_free(struct list_head *list)
 /* Must remain a power of two */
 #define WM_ADSP_CAPTURE_BUFFER_SIZE      1048576
 
-#define WM_ADSP_NUM_FW 16
+#define WM_ADSP_NUM_FW 17
 
 #define WM_ADSP_FW_MBC_VSS        0
 #define WM_ADSP_FW_TX             1
@@ -215,6 +215,7 @@ static void wm_adsp_buf_free(struct list_head *list)
 #define WM_ADSP_FW_EZ2FACETALK_RX 13
 #define WM_ADSP_FW_EZ2GROUPTALK_TX 14
 #define WM_ADSP_FW_EZ2GROUPTALK_RX 15
+#define WM_ADSP_FW_EZ2RECORD       16
 
 static const char *wm_adsp_fw_text[WM_ADSP_NUM_FW] = {
 	[WM_ADSP_FW_MBC_VSS] =    "MBC/VSS",
@@ -233,6 +234,7 @@ static const char *wm_adsp_fw_text[WM_ADSP_NUM_FW] = {
 	[WM_ADSP_FW_EZ2FACETALK_RX] = "Ez2FaceTalk Rx",
 	[WM_ADSP_FW_EZ2GROUPTALK_TX] = "Ez2GroupTalk Tx",
 	[WM_ADSP_FW_EZ2GROUPTALK_RX] = "Ez2GroupTalk Rx",
+	[WM_ADSP_FW_EZ2RECORD] = "Ez2Record",
 };
 
 struct wm_adsp_system_config_xm_hdr {
@@ -391,6 +393,7 @@ static struct wm_adsp_fw_defs wm_adsp_fw[WM_ADSP_NUM_FW] = {
 	[WM_ADSP_FW_EZ2FACETALK_RX] = { .file = "ez2facetalk-rx" },
 	[WM_ADSP_FW_EZ2GROUPTALK_TX] = { .file = "ez2grouptalk-tx" },
 	[WM_ADSP_FW_EZ2GROUPTALK_RX] = { .file = "ez2grouptalk-rx" },
+	[WM_ADSP_FW_EZ2RECORD] = { .file = "ez2record" },
 };
 
 struct wm_coeff_ctl_ops {
