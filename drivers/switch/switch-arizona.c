68fea90899032157f25bee7754e9eb9d2347c020
Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>
2014-04-26 11:17:03 +0100

    switch: arizona: Remove compilation errors when no CONFIG_OF
    Change-Id: I3f938ebfa16ec0408de9fa69284dfe2d7d485a2a
    Signed-off-by: Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    Signed-off-by: Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>

--- drivers/switch/switch-arizona.c
+++ drivers/switch/switch-arizona.c
@@ -1340,6 +1340,7 @@ static void arizona_micd_set_level(struct arizona *arizona, int index,
 	regmap_update_bits(arizona->regmap, reg, mask, level);
 }
 
+#ifdef CONFIG_OF
 static int arizona_extcon_get_pdata(struct arizona *arizona)
 {
 	struct arizona_pdata *pdata = &arizona->pdata;
@@ -1393,6 +1394,12 @@ static int arizona_extcon_get_pdata(struct arizona *arizona)
 
 	return 0;
 }
+#else
+static inline int arizona_extcon_get_pdata(struct arizona *arizona)
+{
+	return 0;
+}
+#endif
 
 static ssize_t arizona_extcon_show(struct device *dev,
 				   struct device_attribute *attr,
