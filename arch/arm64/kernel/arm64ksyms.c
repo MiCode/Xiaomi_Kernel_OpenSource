586fac4aed9bd1daad7f4fd0b225a7971293e7f1
zhichang.yuan <zhichang.yuan@linaro.org>
2014-04-28 13:11:34 +0800

    arm64: lib: Implement optimized string length routines
    This patch, based on Linaro's Cortex Strings library, adds
    an assembly optimized strlen() and strnlen() functions.
    
    Signed-off-by: Zhichang Yuan <zhichang.yuan@linaro.org>
    Signed-off-by: Deepak Saxena <dsaxena@linaro.org>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Git-commit: 0a42cb0a6fa64cb17db11164a1ad3511b43acefe
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- arch/arm64/kernel/arm64ksyms.c
+++ arch/arm64/kernel/arm64ksyms.c
@@ -47,6 +47,8 @@ EXPORT_SYMBOL(strchr);
 EXPORT_SYMBOL(strrchr);
 EXPORT_SYMBOL(strcmp);
 EXPORT_SYMBOL(strncmp);
+EXPORT_SYMBOL(strlen);
+EXPORT_SYMBOL(strnlen);
 EXPORT_SYMBOL(memset);
 EXPORT_SYMBOL(memcpy);
 EXPORT_SYMBOL(memmove);
