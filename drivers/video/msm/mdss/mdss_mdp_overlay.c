c5baa369b318dda90355df09668db4a4aff301be
Ujwal Patel <ujwalp@codeaurora.org>
2014-04-30 14:58:06 -0700

    msm: mdss: fix sync fence deadlock when rotator is used
    In the current implementation, whenever rotator play is called, it
    forces DMA pipes to be released by calling commit on video mode ctl
    paths if they are using DMA pipes. Here assumption was that DMA pipes
    will either be used in block mode or line mode, all or none. But this
    assumption is not valid any more where we can have a use-case where
    DMA0 is used for rotator and DMA1 for video mode ctl path. In this
    use-case, current implementation can lead to sync fence deadlock. Fix
    this by removing obsolete assumption.
    
    Change-Id: I2d1da9dca5d958257796d617db11d93631c98f27
    CRs-Fixed: 656463
    Signed-off-by: Ujwal Patel <ujwalp@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -1599,44 +1599,6 @@ end:
 	return ret;
 }
 
-static void mdss_mdp_overlay_force_cleanup(struct msm_fb_data_type *mfd)
-{
-	struct mdss_overlay_private *mdp5_data = mfd_to_mdp5_data(mfd);
-	struct mdss_mdp_ctl *ctl = mdp5_data->ctl;
-	int ret;
-
-	pr_debug("forcing cleanup to unset dma pipes on fb%d\n", mfd->index);
-
-	/*
-	 * video mode panels require the layer to be unstaged and wait for
-	 * vsync to be able to release buffer.
-	 */
-	if (ctl && ctl->is_video_mode) {
-		ret = mdss_mdp_display_commit(ctl, NULL);
-		if (!IS_ERR_VALUE(ret))
-			mdss_mdp_display_wait4comp(ctl);
-	}
-
-	mdss_mdp_overlay_cleanup(mfd);
-}
-
-static void mdss_mdp_overlay_force_dma_cleanup(struct mdss_data_type *mdata)
-{
-	struct mdss_mdp_pipe *pipe;
-	int i;
-
-	for (i = 0; i < mdata->ndma_pipes; i++) {
-		pipe = mdata->dma_pipes + i;
-
-		if (!mdss_mdp_pipe_map(pipe)) {
-			struct msm_fb_data_type *mfd = pipe->mfd;
-			mdss_mdp_pipe_unmap(pipe);
-			if (mfd)
-				mdss_mdp_overlay_force_cleanup(mfd);
-		}
-	}
-}
-
 static int mdss_mdp_overlay_play(struct msm_fb_data_type *mfd,
 				 struct msmfb_overlay_data *req)
 {
@@ -1661,8 +1623,6 @@ static int mdss_mdp_overlay_play(struct msm_fb_data_type *mfd,
 	}
 
 	if (req->id & MDSS_MDP_ROT_SESSION_MASK) {
-		mdss_mdp_overlay_force_dma_cleanup(mfd_to_mdata(mfd));
-
 		ret = mdss_mdp_rotator_play(mfd, req);
 	} else if (req->id == BORDERFILL_NDX) {
 		pr_debug("borderfill enable\n");
