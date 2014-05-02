04a545814357e206f9e840e643e4637881fe4ada
Shivaraj Shetty <shivaraj@codeaurora.org>
2014-05-02 18:05:44 +0530

    msm: mdss: Protect iommu attach/detach with mutex for mdp3
    In case of command mode panels we get iommu attach/detach call
    from different threads. Hence protect the iommu attach/detach
    operations with a mutex.
    
    Change-Id: If18ecc18ffea3f8a88f48b91251523e8df29eda7
    Signed-off-by: Shivaraj Shetty <shivaraj@codeaurora.org>

--- drivers/video/fbdev/msm/mdp3.c
+++ drivers/video/fbdev/msm/mdp3.c
@@ -739,9 +739,11 @@ int mdp3_iommu_attach(int context)
 	if (context >= MDP3_IOMMU_CTX_MAX)
 		return -EINVAL;
 
+	mutex_lock(&mdp3_res->iommu_lock);
 	context_map = mdp3_res->iommu_contexts + context;
 	if (context_map->attached) {
 		pr_warn("mdp iommu already attached\n");
+		mutex_unlock(&mdp3_res->iommu_lock);
 		return 0;
 	}
 
@@ -750,6 +752,7 @@ int mdp3_iommu_attach(int context)
 	iommu_attach_device(domain_map->domain, context_map->ctx);
 
 	context_map->attached = true;
+	mutex_unlock(&mdp3_res->iommu_lock);
 	return 0;
 }
 
@@ -762,9 +765,11 @@ int mdp3_iommu_dettach(int context)
 		context >= MDP3_IOMMU_CTX_MAX)
 		return -EINVAL;
 
+	mutex_lock(&mdp3_res->iommu_lock);
 	context_map = mdp3_res->iommu_contexts + context;
 	if (!context_map->attached) {
 		pr_warn("mdp iommu not attached\n");
+		mutex_unlock(&mdp3_res->iommu_lock);
 		return 0;
 	}
 
@@ -772,6 +777,7 @@ int mdp3_iommu_dettach(int context)
 	iommu_detach_device(domain_map->domain, context_map->ctx);
 	context_map->attached = false;
 
+	mutex_unlock(&mdp3_res->iommu_lock);
 	return 0;
 }
 
