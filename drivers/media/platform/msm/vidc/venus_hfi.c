013dbac9c9227bab66e217c19eccb7a0b1328105
Ravi Kiran Vonteddu <rvontedd@codeaurora.org>
2014-05-02 13:01:20 +0530

    msm: vidc: Check maximum width capability of Q6
    Maximum width capability check is required to take
    care of indefinite behavior when a clip having width
    more than Q6 capability is played. Also, make the
    capability check generic for Q6 and Venus.
    
    CRs-fixed: 626642
    Change-Id: Ic10be0ad4434019fea45e7a090b21ba5cf54d9a6
    Signed-off-by: Ravi Kiran Vonteddu <rvontedd@codeaurora.org>

--- drivers/media/platform/msm/vidc/venus_hfi.c
+++ drivers/media/platform/msm/vidc/venus_hfi.c
@@ -3744,28 +3744,6 @@ int venus_hfi_get_core_capabilities(void)
 	return rc;
 }
 
-int venus_hfi_capability_check(u32 fourcc, u32 width,
-		u32 *max_width, u32 *max_height)
-{
-	int rc = 0;
-	if (!max_width || !max_height) {
-		dprintk(VIDC_ERR, "%s - invalid parameter\n", __func__);
-		return -EINVAL;
-	}
-
-	if (msm_vp8_low_tier && fourcc == V4L2_PIX_FMT_VP8) {
-		*max_width = DEFAULT_WIDTH;
-		*max_height = DEFAULT_HEIGHT;
-	}
-	if (width > *max_width) {
-		dprintk(VIDC_ERR,
-		"Unsupported width = %u supported max width = %u",
-		width, *max_width);
-		rc = -ENOTSUPP;
-	}
-	return rc;
-}
-
 static void *venus_hfi_add_device(u32 device_id,
 			struct msm_vidc_platform_resources *res,
 			hfi_cmd_response_callback callback)
@@ -3927,7 +3905,6 @@ static void venus_init_hfi_callbacks(struct hfi_device *hdev)
 	hdev->get_fw_info = venus_hfi_get_fw_info;
 	hdev->get_info = venus_hfi_get_info;
 	hdev->get_stride_scanline = venus_hfi_get_stride_scanline;
-	hdev->capability_check = venus_hfi_capability_check;
 	hdev->get_core_capabilities = venus_hfi_get_core_capabilities;
 	hdev->power_enable = venus_hfi_power_enable;
 }
