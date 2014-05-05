e336a5422bdb36badf2c6aae7cc8b77827266ab6
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2014-05-05 15:49:47 -0700

    power: power_supply: Add USB_PARALLEL charger type
    Add definitions for usb parallel charger type. This charger assists
    the main charging path by splitting the current drawn from the usb
    charger. This reduces the current flowing through the main charging
    path thereby reducing thermal impact and size of the inductor.
    
    Change-Id: I13ffa386c234387ec057d27f35818527c2c4fcdd
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -45,7 +45,8 @@ static ssize_t power_supply_show_property(struct device *dev,
 					  char *buf) {
 	static char *type_text[] = {
 		"Unknown", "Battery", "UPS", "Mains", "USB",
-		"USB_DCP", "USB_CDP", "USB_ACA", "Wireless", "BMS"
+		"USB_DCP", "USB_CDP", "USB_ACA", "Wireless", "BMS",
+		"USB_Parallel"
 	};
 	static char *status_text[] = {
 		"Unknown", "Charging", "Discharging", "Not charging", "Full"
