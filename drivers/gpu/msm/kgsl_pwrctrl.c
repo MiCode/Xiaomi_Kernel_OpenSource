6c70cd86a6c9ae58b5b0ceadef699c12903ebc61
Jordan Crouse <jcrouse@codeaurora.org>
2014-05-06 09:37:45 -0600

    msm: kgsl: Reset the bus_mod value when power is re-enabled
    kgsl_pwrctrl_pwrlevel_change() resets the bus_mod level when the
    power level is changed but the power level doesn't get changed
    on re-init if constraints are enabled with the net effect of
    turning on the bus at a lower level than we would expect.  Reset
    bus_mod in  kgsl_pwrctrl_enable() to make sure it always comes
    up at the expected level.
    
    Change-Id: Ic0dedbad303cc938d91583b3e3309d232590b975
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -1492,6 +1492,8 @@ int kgsl_pwrctrl_enable(struct kgsl_device *device)
 	} else
 		level = pwr->default_pwrlevel;
 
+	pwr->bus_mod = 0;
+
 	if (pwr->constraint.type == KGSL_CONSTRAINT_NONE)
 		kgsl_pwrctrl_pwrlevel_change(device, level);
 
