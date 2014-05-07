85038cfd2e3345a39e76c874356e9ff4b542d864
Olav Haugan <ohaugan@codeaurora.org>
2014-05-07 10:25:34 -0700

    staging: ion: Optimize zeroing when allocating from system heap
    Currently when allocating from system heap each page is individually
    mapped and zero'ed in the page pool code. Under high memory pressure when
    we allocate a lot of small pages this becomes a bottleneck. Instead,
    do the zeroing in one fell swoop at the end of allocating all the pages
    but only for the pages that are not already zero'ed and sync'ed (pages
    coming from the page pool).
    
    Test results
    ------------
    Time recorded for allocating 20 x 19MB ION buffers from System heap with
    forced 4K page allocations (all pages allocated was order-0) right after
    boot to Android homescreen using performance kernel configuration.
    
    Before change (non-cached allocations):
    Run 1: 768ms
    Run 2: 752ms
    Run 3: 759ms
    
    Before change (cached allocations):
    Run 1: 726ms
    Run 2: 769ms
    Run 3: 645ms
    
    After change (cached allocations):
    Run 1: 262ms
    Run 2: 249ms
    Run 3: 310ms
    
    After change (non-cached allocations):
    Run 1: 275ms
    Run 2: 234ms
    Run 3: 259ms
    
    Time recorded for allocating 20 x 19MB ION buffers from System heap (with no
    forced page size) right after boot to Android homescreen using
    performance kernel configuration. Run 1 is taken right after boot while
    run 2 and 3 are subsequent runs without reboot between.
    
    Before change (non-cached allocations):
    Run 1: 181ms
    Run 2: 1ms
    Run 3: 1ms
    
    Before change (cached allocations):
    Run 1: 220ms
    Run 2: 1ms
    Run 3: 2ms
    
    After change (non-cached allocations):
    Run 1: 166ms
    Run 2: 1ms
    Run 3: 1ms
    
    After change (cached allocations):
    Run 1: 170ms
    Run 2: 1ms
    Run 3: 3ms
    
    CRs-fixed: 687475
    Change-Id: I8dcfdb4adb8d5f4480768f951523fa5c64db87b1
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>
    [resolved merge conflicts]
    Signed-off-by: Chintan Pandya <cpandya@codeaurora.org>

--- drivers/staging/android/ion/ion_priv.h
+++ drivers/staging/android/ion/ion_priv.h
@@ -223,6 +223,7 @@ void ion_device_add_heap(struct ion_device *dev, struct ion_heap *heap);
 
 struct pages_mem {
 	struct page **pages;
+	u32 size;
 	void (*free_fn) (const void *);
 };
 
@@ -237,6 +238,8 @@ int ion_heap_map_user(struct ion_heap *, struct ion_buffer *,
 int ion_heap_pages_zero(struct page **pages, int num_pages);
 int ion_heap_buffer_zero(struct ion_buffer *buffer);
 int ion_heap_high_order_page_zero(struct page *page, int order);
+int ion_heap_alloc_pages_mem(struct pages_mem *pages_mem);
+void ion_heap_free_pages_mem(struct pages_mem *pages_mem);
 
 /**
  * ion_heap_init_deferred_free -- initialize deferred free functionality
