45cb0cc131bd6217504ca98948c144045b20e03b
Jayant Shekhar <jshekhar@codeaurora.org>
2014-05-07 20:09:28 +0530

    msm: mdss: Acquire shared lock after getting frame on wfd
    Currently there is a deadlock when wfd and rotation is done
    simultaneously and trying to acquire the shared lock. To
    prevent this, acquire shared lock after rotated frame is
    ready for wfd.
    
    Change-Id: I3865f02c345ddadd985ee35134454a789936a79a
    Signed-off-by: Jayant Shekhar <jshekhar@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_ctl.c
+++ drivers/video/msm/mdss/mdss_mdp_ctl.c
@@ -2575,7 +2575,8 @@ int mdss_mdp_display_commit(struct mdss_mdp_ctl *ctl, void *arg)
 		}
 	}
 
-	mdss_mdp_ctl_notify(ctl, MDP_NOTIFY_FRAME_READY);
+	if (!ctl->shared_lock)
+		mdss_mdp_ctl_notify(ctl, MDP_NOTIFY_FRAME_READY);
 
 	if (ctl->wait_pingpong)
 		ctl->wait_pingpong(ctl, NULL);
