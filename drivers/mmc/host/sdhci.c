869a749332e21095d6dde39cf2c4ac2d0c7282af
Eric Ernst <eric.ernst@intel.com>
2014-05-08 10:14:03 -0700

    mmc: check cmd registers' DAT[0] before tuning
    Add extra check to resolve tuning failure observed
    on T100 (BYT platform) when using R1B/poll for cmd erase.
    
    Without checking present state register's DAT[0],
    retune execution could take place while the card is
    indeed busy with an erase operation, causing BLKDISCARD failure.
    
    This patch needs to be reviewed for upstream applicability
    and possible rework to quirk.  This should only be necessary
    when polling for erase completion rather than IRQ based, but a
    harmless check otherwise.
    
    Based on fixes and enhancements from MCG's MMC driver.
    
    Signed-off-by: Eric Ernst <eric.ernst@intel.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1399,7 +1399,8 @@ static void sdhci_request(struct mmc_host *mmc, struct mmc_request *mrq)
 		 * tuning procedure before sending command.
 		 */
 		if ((host->flags & SDHCI_NEEDS_RETUNING) &&
-		    !(present_state & (SDHCI_DOING_WRITE | SDHCI_DOING_READ))) {
+		    !(present_state & (SDHCI_DOING_WRITE | SDHCI_DOING_READ)) &&
+		    (present_state & SDHCI_DATA_0_LVL_MASK)) {
 			if (mmc->card) {
 				/* eMMC uses cmd21 but sd and sdio use cmd19 */
 				tuning_opcode =
