cbb7015db4b3c9c5b80c3ec540f8dc05609314ff
Yue Ma <yuem@codeaurora.org>
2014-05-07 17:21:26 -0700

    cnss: Add API to get monotonic boot time
    Add CNSS platform driver API to expose monotonic boot time so that
    WLAN host driver can get the timestamp.
    
    CRs-Fixed: 658966
    Change-Id: Ic12da4cc6bf36b343ab0858165ae8bf4a2ab95e8
    Signed-off-by: Yue Ma <yuem@codeaurora.org>

--- include/net/cnss.h
+++ include/net/cnss.h
@@ -69,6 +69,7 @@ extern void cnss_wlan_unregister_driver(struct cnss_wlan_driver *driver);
 extern int cnss_get_fw_files(struct cnss_fw_files *pfw_files);
 extern void cnss_flush_work(void *work);
 extern void cnss_flush_delayed_work(void *dwork);
+extern void cnss_get_monotonic_boottime(struct timespec *ts);
 extern int cnss_request_bus_bandwidth(int bandwidth);
 
 extern void cnss_pm_wake_lock_init(struct wakeup_source *ws, const char *name);
