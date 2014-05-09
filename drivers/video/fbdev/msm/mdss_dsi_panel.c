a3b8b7d84b62c80a5f035f4d04afeab489332f6a
Shivaraj Shetty <shivaraj@codeaurora.org>
2014-05-09 20:27:31 +0530

    msm: mdss: Add support for checking panel status through register read
    ESD feature may need to check the status of the panel by reading specific
    panel register depending on the panel. Add support for ESD status check
    through status register read and to configure the method used for checking
    panel status through panel dtsi entry.
    
    CRs-Fixed: 660805
    Change-Id: I13b11362722eb2ad379162944eafff04ec01d1cb
    Signed-off-by: Shivaraj Shetty <shivaraj@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -1097,6 +1097,23 @@ static int mdss_panel_parse_dt(struct device_node *np,
 	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->off_cmds,
 		"qcom,mdss-dsi-off-command", "qcom,mdss-dsi-off-command-state");
 
+	mdss_dsi_parse_dcs_cmds(np, &ctrl_pdata->status_cmds,
+			"qcom,mdss-dsi-panel-status-command",
+				"qcom,mdss-dsi-panel-status-command-state");
+	rc = of_property_read_u32(np, "qcom,mdss-dsi-panel-status-value", &tmp);
+	ctrl_pdata->status_value = (!rc ? tmp : 0);
+
+
+	ctrl_pdata->status_mode = ESD_MAX;
+	rc = of_property_read_string(np,
+				"qcom,mdss-dsi-panel-status-check-mode", &data);
+	if (!rc) {
+		if (!strcmp(data, "bta_check"))
+			ctrl_pdata->status_mode = ESD_BTA;
+		else if (!strcmp(data, "reg_read"))
+			ctrl_pdata->status_mode = ESD_REG;
+	}
+
 	rc = mdss_dsi_parse_panel_features(np, ctrl_pdata);
 	if (rc) {
 		pr_err("%s: failed to parse panel features\n", __func__);
