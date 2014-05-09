70eca89f19e7ec7d4f50edda55a44d4f5207157c
Vicky Sehrawat <vickys@codeaurora.org>
2014-05-09 16:21:16 -0700

    ASoC: msm8974: Add VoWLAN dia link
    Add VoWLAN dai link to the machine driver
    to enable Voice over WLAN.
    
    CRs-fixed: 662362
    Change-Id: I3ada1f10436e5724670052901b648f46885d3bf6
    Signed-off-by: Vicky Sehrawat <vickys@codeaurora.org>

--- sound/soc/msm/msm8974.c
+++ sound/soc/msm/msm8974.c
@@ -2413,6 +2413,21 @@ static struct snd_soc_dai_link msm8974_common_dai_links[] = {
 		.ignore_pmdown_time = 1,
 		.be_id = MSM_FRONTEND_DAI_MULTIMEDIA9,
 	},
+	{
+		.name = "VoWLAN",
+		.stream_name = "VoWLAN",
+		.cpu_dai_name   = "VoWLAN",
+		.platform_name  = "msm-pcm-voice",
+		.dynamic = 1,
+		.trigger = {SND_SOC_DPCM_TRIGGER_POST,
+			    SND_SOC_DPCM_TRIGGER_POST},
+		.no_host_mode = SND_SOC_DAI_LINK_NO_HOST,
+		.ignore_suspend = 1,
+		.ignore_pmdown_time = 1,
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.codec_name = "snd-soc-dummy",
+		.be_id = MSM_FRONTEND_DAI_VOWLAN,
+	},
 	/* Backend BT/FM DAI Links */
 	{
 		.name = LPASS_BE_INT_BT_SCO_RX,
