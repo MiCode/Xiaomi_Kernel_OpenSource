089a3a18994a5b2c54bf857d0001afd35a000fe3
Vineet Bajaj <vbajaj@codeaurora.org>
2014-05-12 17:52:53 +0530

    msm: mdss: Avoid unclocked register access with dsi_event_thread
    In the case when ESD status check fails with DSI ACK
    errors, there is a chance of unclocked register access when
    DSI_INTR_CTRL register is being set.
    
    Change-Id: I629b4ec642a86369b6ed693bdc6f8b4260ec1f5d
    Signed-off-by: Vineet Bajaj <vbajaj@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi_host.c
+++ drivers/video/msm/mdss/mdss_dsi_host.c
@@ -1350,8 +1350,10 @@ static int dsi_event_thread(void *data)
 
 		if (todo & DSI_EV_MDP_FIFO_UNDERFLOW) {
 			if (ctrl->recovery) {
+				mdss_dsi_clk_ctrl(ctrl, DSI_ALL_CLKS, 1);
 				mdss_dsi_sw_reset_restore(ctrl);
 				ctrl->recovery->fxn(ctrl->recovery->data);
+				mdss_dsi_clk_ctrl(ctrl, DSI_ALL_CLKS, 0);
 			}
 		}
 
@@ -1363,7 +1365,9 @@ static int dsi_event_thread(void *data)
 			spin_unlock_irqrestore(&ctrl->mdp_lock, flag);
 
 			/* enable dsi error interrupt */
+			mdss_dsi_clk_ctrl(ctrl, DSI_ALL_CLKS, 1);
 			mdss_dsi_err_intr_ctrl(ctrl, DSI_INTR_ERROR_MASK, 1);
+			mdss_dsi_clk_ctrl(ctrl, DSI_ALL_CLKS, 0);
 		}
 
 	}
