5b518f7144de53a3ff5058b36c006eacf3eaccdc
Bin Han <bin.b.han@intel.com>
2014-05-13 18:26:49 +0800

    v4l2: copy reserved and reserved2 of v4l2_buffer in compat_ioctl32
    In the per-frame parameter setting feature, camera HAL needs to use the
    reserved2 in v4l2_buffer to specify which parameter it intends to apply and
    also use reserved2 to indicate if this frame buffer do not need a per-frame
    parameter. So v4l2 needs to copy the reserved2 and reserved in compat_ioctl32
    for this purpose.
    
    (Note that this is a workaround due to the atomisp driver/userspace
    storing data in those reserved fields which are supposed to be
    ignored.  Needs a long term fix.)
    
    Change-Id: I0f6d26f4ccf05931324f9ba73be6e2c12f48a2f9
    Signed-off-by: Bin Han <bin.b.han@intel.com>

--- drivers/media/v4l2-core/v4l2-compat-ioctl32.c
+++ drivers/media/v4l2-core/v4l2-compat-ioctl32.c
@@ -392,7 +392,9 @@ static int get_v4l2_buffer32(struct v4l2_buffer *kp, struct v4l2_buffer32 __user
 		get_user(kp->index, &up->index) ||
 		get_user(kp->type, &up->type) ||
 		get_user(kp->flags, &up->flags) ||
-		get_user(kp->memory, &up->memory))
+		get_user(kp->memory, &up->memory) ||
+		get_user(kp->reserved2, &up->reserved2) ||
+		get_user(kp->reserved, &up->reserved))
 			return -EFAULT;
 
 	if (V4L2_TYPE_IS_OUTPUT(kp->type))
