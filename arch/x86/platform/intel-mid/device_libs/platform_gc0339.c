ac8b5c40a0349b4e375cf50d0e2317681ae13e65
Ilkka Koskinen <ilkka.koskinen@intel.com>
2014-05-14 17:53:55 +0300

    gc0339: Get csi configuration from ACPI
    Signed-off-by: Ilkka Koskinen <ilkka.koskinen@intel.com>

--- arch/x86/platform/intel-mid/device_libs/platform_gc0339.c
+++ arch/x86/platform/intel-mid/device_libs/platform_gc0339.c
@@ -209,8 +209,22 @@ static int gc0339_power_ctrl(struct v4l2_subdev *sd, int flag)
 
 static int gc0339_csi_configure(struct v4l2_subdev *sd, int flag)
 {
-	return camera_sensor_csi(sd, ATOMISP_CAMERA_PORT_PRIMARY, 1,
-		ATOMISP_INPUT_FORMAT_RAW_10, atomisp_bayer_order_grbg, flag);
+	/* Default from legacy platform w/o firmware config */
+	int port = ATOMISP_CAMERA_PORT_PRIMARY;
+	int lanes = 1;
+	int format = ATOMISP_INPUT_FORMAT_RAW_10;
+	int bayer = atomisp_bayer_order_grbg;
+
+	struct i2c_client *client = v4l2_get_subdevdata(sd);
+	if (client && ACPI_COMPANION(&client->dev)) {
+		struct device *dev = &client->dev;
+		port = getvar_int(dev, "CsiPort", port);
+		lanes = getvar_int(dev, "CsiLanes", lanes);
+		format = getvar_int(dev, "CsiFmt", format);
+		bayer = getvar_int(dev, "CsiBayer", bayer);
+	}
+
+	return camera_sensor_csi(sd, port, lanes, format, bayer, flag);
 }
 
 static struct camera_sensor_platform_data gc0339_sensor_platform_data = {
