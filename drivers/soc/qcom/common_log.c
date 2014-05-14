08709d78283b2a3bf71e83af72537f9e535adf19
jin wu <jinw@codeaurora.org>
2014-05-13 22:51:50 -0700

    Revert "soc: qcom: common_log: Support for memory dump v2"
    This reverts commit 37486875ea7120d560034cb3fc38b8b60cb0b0dd
    
    Change-Id: I19e9fe4ead67303190cdbebc7c910f51c032c789
    Signed-off-by: Jin Wu <jinw@codeaurora.org>

--- drivers/soc/qcom/common_log.c
+++ drivers/soc/qcom/common_log.c
@@ -10,74 +10,35 @@
  * GNU General Public License for more details.
  *
  */
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/kallsyms.h>
-#include <linux/slab.h>
 #include <soc/qcom/memory_dump.h>
 
-static void __init common_log_register(char *addr, uint32_t length,
-						unsigned long *reserved)
+static void __init common_log_register(void)
 {
 	struct msm_client_dump dump;
-	struct msm_dump_entry dump_entry;
-	struct msm_dump_data *dump_data;
-	int ret;
-	static int count;
+	char **log_bufp;
+	uint32_t *log_buf_lenp;
 
-	if (MSM_DUMP_MAJOR(msm_dump_table_version()) == 1) {
-		dump.id = MSM_COMMON_LOG + count;
-		dump.start_addr = virt_to_phys(addr);
-		dump.end_addr = virt_to_phys(addr + length);
-		if (msm_dump_tbl_register(&dump))
-			pr_err("Unable to register %d.\n", dump.id);
-	} else {
-		dump_data = kzalloc(sizeof(struct msm_dump_data), GFP_KERNEL);
-		if (!dump_data) {
-			pr_err("Unable to alloc data space.\n");
-			return;
-		}
-		if (reserved != 0)
-			dump_data->len = virt_to_phys(reserved);
-		else
-			dump_data->len = 0;
-		dump_data->reserved = length;
-		dump_data->addr = virt_to_phys(addr);
-		dump_entry.id = MSM_DUMP_DATA_COMMON_LOG + count;
-		dump_entry.addr = virt_to_phys(dump_data);
-		ret = msm_dump_data_register(MSM_DUMP_TABLE_APPS, &dump_entry);
-		if (ret) {
-			kfree(dump_data);
-			pr_err("Unable to register %d.\n", dump_entry.id);
-		}
-	}
-	count++;
-}
-
-static void __init register_symbols(char *name, char *len)
-{
-	char **addr;
-	uint32_t *length;
-	unsigned long *reserved = 0;
-
-	addr = (char **)kallsyms_lookup_name(name);
-	length = (uint32_t *)kallsyms_lookup_name(len);
-	if (!addr || !length) {
-		pr_err("Unable to find %s by kallsyms!\n", name);
+	log_bufp = (char **)kallsyms_lookup_name("log_buf");
+	log_buf_lenp = (uint32_t *)kallsyms_lookup_name("log_buf_len");
+	if (!log_bufp || !log_buf_lenp) {
+		pr_err("%s: Symbol log_buf not found!\n", __func__);
 		return;
 	}
-	/* If item is log_buf, register first index to locate
-	* the start address of kmsg in ring buffer. */
-	if (strcmp(name, "log_buf") == 0)
-		reserved = (unsigned long *)kallsyms_lookup_name(
-							"log_first_idx");
-	common_log_register(*addr, *length, reserved);
+	dump.id = MSM_LOG_BUF;
+	dump.start_addr = virt_to_phys(*log_bufp);
+	dump.end_addr = virt_to_phys(*log_bufp + *log_buf_lenp);
+	if (msm_dump_tbl_register(&dump))
+		pr_err("%s: Could not register log_buf.\n", __func__);
 }
 
 static int __init msm_common_log_init(void)
 {
-	register_symbols("log_buf", "log_buf_len");
+	common_log_register();
 	return 0;
 }
 late_initcall(msm_common_log_init);
