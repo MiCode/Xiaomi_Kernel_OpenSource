5dbfa5de20b3018f3ef3d7860cdacfa8cc59211b
Wei Ding <weiding@codeaurora.org>
2014-05-14 13:09:54 +0800

    msm: msm8916: camera: Change torch op current.
    Currently, torch mode current and flash mode
    current are set to the same value which turns
    off after short time.
    
    Maintaining same current for torch mode will
    cause the LED to turn off which is not desired.
    So fix this by setting the current
    for torch mode to a lower value so that it
    does not turn off after short time.
    
    Change-Id: Icc22d7f05063b44fd173dd58865389efbaa77351
    Signed-off-by: Wei Ding <weiding@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/sensor/flash/msm_led_trigger.c
+++ drivers/media/platform/msm/camera_v2/sensor/flash/msm_led_trigger.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -242,7 +242,7 @@ static int32_t msm_led_trigger_probe(struct platform_device *pdev)
 
 			if (flashtype == GPIO_FLASH) {
 				/* use fake current */
-				fctrl.torch_op_current = LED_FULL;
+				fctrl.torch_op_current = LED_HALF;
 				if (temp)
 					fctrl.torch_trigger = temp;
 				else
