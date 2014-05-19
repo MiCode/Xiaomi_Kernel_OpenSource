cc08b2b222a278e030b9064611eb4a8cc8b86d39
Chaithanya Krishna Bacharaju <chaithan@codeaurora.org>
2014-05-19 16:27:06 +0530

    ASoC: msm: Add MultiKeyword support for Listen
    Listen is a Voice activation and recognition feature.
    Currently Listen feature supports recognition of single
    keyword for a given session. This change adds support
    for MultiKeyword in Listen interface header. Userspace
    hal will use this interface header to determine Sound
    Model version based on App ID.
    
    Change-Id: I35e5205d78c169370e06548835700f51fec3489d
    Signed-off-by: Chaithanya Krishna Bacharaju <chaithan@codeaurora.org>

--- include/sound/lsm_params.h
+++ include/sound/lsm_params.h
@@ -6,6 +6,11 @@
 
 #define SNDRV_LSM_VERSION SNDRV_PROTOCOL_VERSION(0, 1, 0)
 
+enum lsm_app_id {
+	LSM_VOICE_WAKEUP_APP_ID = 1,
+	LSM_VOICE_WAKEUP_APP_ID_V2 = 2,
+};
+
 enum lsm_detection_mode {
 	LSM_MODE_KEYWORD_ONLY_DETECTION = 1,
 	LSM_MODE_USER_KEYWORD_DETECTION
@@ -20,6 +25,19 @@ struct snd_lsm_sound_model {
 	bool detect_failure;
 };
 
+struct snd_lsm_sound_model_v2 {
+	__u8 __user *data;
+	__u8 *confidence_level;
+	__u32 data_size;
+	enum lsm_detection_mode detection_mode;
+	__u8 num_confidence_levels;
+	bool detect_failure;
+};
+
+struct snd_lsm_session_data {
+	enum lsm_app_id app_id;
+};
+
 struct snd_lsm_event_status {
 	__u16 status;
 	__u16 payload_size;
@@ -32,5 +50,8 @@ struct snd_lsm_event_status {
 #define SNDRV_LSM_ABORT_EVENT	_IOW('U', 0x03, int)
 #define SNDRV_LSM_START		_IOW('U', 0x04, int)
 #define SNDRV_LSM_STOP		_IOW('U', 0x05, int)
+#define SNDRV_LSM_SET_SESSION_DATA _IOW('U', 0x06, struct snd_lsm_session_data)
+#define SNDRV_LSM_REG_SND_MODEL_V2 _IOW('U', 0x07,\
+					struct snd_lsm_sound_model_v2)
 
 #endif
