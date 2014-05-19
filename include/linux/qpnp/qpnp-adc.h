dc04bdbe7b837a718f225c59d7afccf0583c7fd5
Zhenhua Huang <zhenhuah@codeaurora.org>
2014-05-19 16:35:21 +0800

    hwmon: qpnp-adc: add battery thermistor mapping table for skuh
    Tcold = 0 DegC and Thot = 58 DegC are used for 8916 SKUH device.
    Update battery thermistor mapping table for it.
    
    Change-Id: I37a76022d7488734924d5fe213bc86dbf0fcea07
    Signed-off-by: Zhenhua Huang <zhenhuah@codeaurora.org>

--- include/linux/qpnp/qpnp-adc.h
+++ include/linux/qpnp/qpnp-adc.h
@@ -230,7 +230,9 @@ enum qpnp_adc_channel_scaling_param {
  * %SCALE_SMB_BATT_THERM: Conversion to temperature(decidegC) based on
  *          btm parameters for SMB.
  * %SCALE_QRD_SKUG_BATT_THERM: Conversion to temperature(decidegC) based on
- * btm parametersi for SKUG.
+ *          btm parameters for SKUG.
+ * %SCALE_QRD_SKUH_BATT_THERM: Conversion to temperature(decidegC) based on
+ *          btm parameters for SKUH
  * %SCALE_NONE: Do not use this scaling type.
  */
 enum qpnp_adc_scale_fn_type {
@@ -244,6 +246,7 @@ enum qpnp_adc_scale_fn_type {
 	SCALE_QRD_SKUAA_BATT_THERM,
 	SCALE_SMB_BATT_THERM,
 	SCALE_QRD_SKUG_BATT_THERM,
+	SCALE_QRD_SKUH_BATT_THERM,
 	SCALE_NONE,
 };
 
@@ -1186,6 +1189,23 @@ int32_t qpnp_adc_scale_qrd_skug_batt_therm(struct qpnp_vadc_chip *dev,
 			const struct qpnp_vadc_chan_properties *chan_prop,
 			struct qpnp_vadc_result *chan_rslt);
 /**
+ * qpnp_adc_scale_qrd_skuh_batt_therm() - Scales the pre-calibrated digital output
+ *		of an ADC to the ADC reference and compensates for the
+ *		gain and offset. Returns the temperature in decidegC.
+ * @dev:	Structure device for qpnp vadc
+ * @adc_code:	pre-calibrated digital ouput of the ADC.
+ * @adc_prop:	adc properties of the pm8xxx adc such as bit resolution,
+ *		reference voltage.
+ * @chan_prop:	individual channel properties to compensate the i/p scaling,
+ *		slope and offset.
+ * @chan_rslt:	physical result to be stored.
+ */
+int32_t qpnp_adc_scale_qrd_skuh_batt_therm(struct qpnp_vadc_chip *dev,
+			int32_t adc_code,
+			const struct qpnp_adc_properties *adc_prop,
+			const struct qpnp_vadc_chan_properties *chan_prop,
+			struct qpnp_vadc_result *chan_rslt);
+/**
  * qpnp_adc_scale_smb_batt_therm() - Scales the pre-calibrated digital output
  *		of an ADC to the ADC reference and compensates for the
  *		gain and offset. Returns the temperature in decidegC.
@@ -1500,6 +1520,12 @@ static inline int32_t qpnp_adc_scale_qrd_skug_batt_therm(
 			const struct qpnp_adc_properties *adc_prop,
 			const struct qpnp_vadc_chan_properties *chan_prop,
 			struct qpnp_vadc_result *chan_rslt)
+{ return -ENXIO; }
+static inline int32_t qpnp_adc_scale_qrd_skuh_batt_therm(
+			struct qpnp_vadc_chip *vdev, int32_t adc_code,
+			const struct qpnp_adc_properties *adc_prop,
+			const struct qpnp_vadc_chan_properties *chan_prop,
+			struct qpnp_vadc_result *chan_rslt);
 { return -ENXIO; }
 static inline int32_t qpnp_adc_scale_smb_batt_therm(struct qpnp_vadc_chip *vadc,
 			int32_t adc_code,
