653ca422e20aeed0b1c78bd809fbd6e6bfef1dcb
Anji Jonnala <anjir@codeaurora.org>
2014-05-20 20:02:34 +0530

    soc: qcom: rq_stats: remove the redundant iowait check
    Iowait_time check is redundant and it would lead to heavy load
    due to miscaluculation of idle time. Hence remove this check
    as iowait will considered based on io_busy flag.
    
    Change-Id: I4dd9f9c79205eca588daea8cc0fe892394a83391
    CRs-fixed: 662052
    Signed-off-by: Anji Jonnala <anjir@codeaurora.org>

--- drivers/soc/qcom/msm_rq_stats.c
+++ drivers/soc/qcom/msm_rq_stats.c
@@ -42,7 +42,6 @@ struct notifier_block cpu_hotplug;
 struct cpu_load_data {
 	cputime64_t prev_cpu_idle;
 	cputime64_t prev_cpu_wall;
-	cputime64_t prev_cpu_iowait;
 	unsigned int avg_load_maxfreq;
 	unsigned int samples;
 	unsigned int window_size;
@@ -54,27 +53,16 @@ struct cpu_load_data {
 
 static DEFINE_PER_CPU(struct cpu_load_data, cpuload);
 
-static inline cputime64_t get_cpu_iowait_time(unsigned int cpu,
-							cputime64_t *wall)
-{
-	u64 iowait_time = get_cpu_iowait_time_us(cpu, wall);
-
-	if (iowait_time == -1ULL)
-		return 0;
-
-	return iowait_time;
-}
 
 static int update_average_load(unsigned int freq, unsigned int cpu)
 {
 
 	struct cpu_load_data *pcpu = &per_cpu(cpuload, cpu);
-	cputime64_t cur_wall_time, cur_idle_time, cur_iowait_time;
-	unsigned int idle_time, wall_time, iowait_time;
+	cputime64_t cur_wall_time, cur_idle_time;
+	unsigned int idle_time, wall_time;
 	unsigned int cur_load, load_at_max_freq;
 
 	cur_idle_time = get_cpu_idle_time(cpu, &cur_wall_time, 0);
-	cur_iowait_time = get_cpu_iowait_time(cpu, &cur_wall_time);
 
 	wall_time = (unsigned int) (cur_wall_time - pcpu->prev_cpu_wall);
 	pcpu->prev_cpu_wall = cur_wall_time;
@@ -82,11 +70,6 @@ static int update_average_load(unsigned int freq, unsigned int cpu)
 	idle_time = (unsigned int) (cur_idle_time - pcpu->prev_cpu_idle);
 	pcpu->prev_cpu_idle = cur_idle_time;
 
-	iowait_time = (unsigned int) (cur_iowait_time - pcpu->prev_cpu_iowait);
-	pcpu->prev_cpu_iowait = cur_iowait_time;
-
-	if (idle_time >= iowait_time)
-		idle_time -= iowait_time;
 
 	if (unlikely(!wall_time || wall_time < idle_time))
 		return 0;
