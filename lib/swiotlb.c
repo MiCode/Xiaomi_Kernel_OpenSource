ad1d7a397df1e44eaea2348076093f37d414e5e1
Abhimanyu Garg <agarg@codeaurora.org>
2014-05-21 16:13:50 -0700

    swiotlb: Setting default IO TBL value to 1MB
    swiotlb was allocting the 64MB of memory at boot up time, because we
    used CMA so we do not use it. We can reduce it to 1MB.
    
    Change-Id: I137f6e9069f86dcc9dcd883868b3e8a08c29d710
    Signed-off-by: Abhimanyu Garg <agarg@codeaurora.org>

--- lib/swiotlb.c
+++ lib/swiotlb.c
@@ -116,8 +116,8 @@ unsigned long swiotlb_nr_tbl(void)
 }
 EXPORT_SYMBOL_GPL(swiotlb_nr_tbl);
 
-/* default to 64MB */
-#define IO_TLB_DEFAULT_SIZE (64UL<<20)
+/* default to 1MB */
+#define IO_TLB_DEFAULT_SIZE SZ_1M
 unsigned long swiotlb_size_or_default(void)
 {
 	unsigned long size;
