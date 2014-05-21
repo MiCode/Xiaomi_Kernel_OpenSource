30d64e4f7df8288ad3ccb6b11151f8c404b31d75
Brad Volkin <bradley.d.volkin@intel.com>
2014-05-21 12:48:30 -0700

    MUST_REBASE [VPG]: drm/i915: Change cmd parser version number
    Since MUST_REBASE Android patches modify the behavior of upstream
    command parsing, we should also modify the version number as the
    intention is for userspace to rely on the version number to know
    what rules the kernel enforces.
    
    For: APDEV-870
    Change-Id: Ia481fdbe21a4bf49abd4a95255b249b3c5034261
    Signed-off-by: Brad Volkin <bradley.d.volkin@intel.com>

--- drivers/gpu/drm/i915/i915_dma.c
+++ drivers/gpu/drm/i915/i915_dma.c
@@ -1023,6 +1023,8 @@ static int i915_getparam(struct drm_device *dev, void *data,
 		break;
 	case I915_PARAM_CMD_PARSER_VERSION:
 		value = i915_cmd_parser_get_version();
+		/* Adjust the parser version for Android */
+		value |= (value << 16);
 		break;
 	case I915_PARAM_EU_COUNT:
 		value = INTEL_INFO(dev)->eu_total;
