ae69e15ebd7f375fbad888ae03c156a7aae3ea6a
Yan He <yanhe@codeaurora.org>
2014-05-23 15:02:19 -0700

    msm: pcie: cancel suspending state when link is up
    PCIe link can be up during recovery as well as resume. Thus,
    cancel suspending state in SW when the link is up.
    
    Change-Id: Ib5ee2c88cd3d617221836ce68363f8492c783344
    Signed-off-by: Yan He <yanhe@codeaurora.org>

--- arch/arm/mach-msm/pcie.c
+++ arch/arm/mach-msm/pcie.c
@@ -1231,6 +1231,7 @@ int msm_pcie_enable(struct msm_pcie_dev_t *dev, u32 options)
 
 	dev->link_status = MSM_PCIE_LINK_ENABLED;
 	dev->power_on = true;
+	dev->suspending = false;
 	goto out;
 
 link_fail:
