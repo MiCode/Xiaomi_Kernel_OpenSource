fdf8e42079cf6e01176537642a5a15d48cae01cf
Rohit Vaswani <rvaswani@codeaurora.org>
2014-05-22 20:57:35 -0700

    arm64: perf_events: Allow edac to use the pmu interrupt
    In the event that edac driver needs to use the percpu
    pmu interrupt, the PMU driver should relinquish
    control over the interrupt. The edac driver takes the
    responsibilty of invoking the pmu interrupt handler
    if a non-edac pmu event triggers the pmu interrupt.
    This is synchronized using a global variable.
    
    Change-Id: I21274377cb51b890e0bfb2f05d601c49dd0b9f73
    Signed-off-by: Rohit Vaswani <rvaswani@codeaurora.org>

--- arch/arm64/include/asm/perf_event.h
+++ arch/arm64/include/asm/perf_event.h
@@ -17,11 +17,21 @@
 #ifndef __ASM_PERF_EVENT_H
 #define __ASM_PERF_EVENT_H
 
+#include <linux/irqreturn.h>
+
 #ifdef CONFIG_HW_PERF_EVENTS
 struct pt_regs;
 extern unsigned long perf_instruction_pointer(struct pt_regs *regs);
 extern unsigned long perf_misc_flags(struct pt_regs *regs);
+irqreturn_t armv8pmu_handle_irq(int irq_num, void *dev);
+void arm64_pmu_irq_handled_externally(void);
 #define perf_misc_flags(regs)	perf_misc_flags(regs)
+#else
+static inline irqreturn_t armv8pmu_handle_irq(int irq_num, void *dev)
+{
+	return IRQ_HANDLED;
+}
+void arm64_pmu_irq_handled_externally(void) { }
 #endif
 
 #endif
