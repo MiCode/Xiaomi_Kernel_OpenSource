60031b879efaeb80c9be40abb5b69f11cd5ad884
Laura Abbott <lauraa@codeaurora.org>
2014-05-28 10:56:41 -0700

    staging: android: ashmem: Factor out compat code from uapi
    Compat related code should not be exported to userspace directly.
    Factor out the compat related code back into an ashmem.h file.
    
    Change-Id: I90ae26a0045a5ff7d7af7e9caadf5d807270eaef
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

---
+++ drivers/staging/android/ashmem.h
@@ -0,0 +1,25 @@
+/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _ANDROID_ASHMEM_H
+#define _ANDROID_ASHMEM_H
+
+#include <linux/compat.h>
+
+/* support of 32bit userspace on 64bit platforms */
+#ifdef CONFIG_COMPAT
+#define COMPAT_ASHMEM_SET_SIZE		_IOW(__ASHMEMIOC, 3, compat_size_t)
+#define COMPAT_ASHMEM_SET_PROT_MASK	_IOW(__ASHMEMIOC, 5, unsigned int)
+#endif
+
+#endif
