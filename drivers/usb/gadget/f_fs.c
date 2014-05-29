6ed8bf3c91192ad8b86d405cfe2203d311d3cd0d
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2014-05-29 17:15:01 +0530

    USB: f_fs: Check error status before doing epfile I/O
    Set error status before disabling endpoint during function
    disable and also check error status before handling I/O. If error
    status is set, return error status to read/write calls made by
    userspace. Also set file's private data to NULL during epfile
    release.
    
    CRs-Fixed: 671880
    Change-Id: I14b5ee541dfc18a7802ef4a8033878a7729d9adb
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>

--- drivers/usb/gadget/f_fs.c
+++ drivers/usb/gadget/f_fs.c
@@ -769,6 +769,9 @@ static ssize_t ffs_epfile_io(struct file *file,
 
 	pr_debug("%s: len %zu, read %d\n", __func__, len, read);
 
+	if (atomic_read(&epfile->error))
+		return -ENODEV;
+
 	goto first_try;
 	do {
 		spin_unlock_irq(&epfile->ffs->eps_lock);
@@ -945,6 +948,7 @@ ffs_epfile_release(struct inode *inode, struct file *file)
 
 	atomic_set(&epfile->error, 1);
 	ffs_data_closed(epfile->ffs);
+	file->private_data = NULL;
 
 	return 0;
 }
@@ -1586,12 +1590,12 @@ static void ffs_func_eps_disable(struct ffs_function *func)
 
 	spin_lock_irqsave(&func->ffs->eps_lock, flags);
 	do {
+		atomic_set(&epfile->error, 1);
 		/* pending requests get nuked */
 		if (likely(ep->ep)) {
 			usb_ep_disable(ep->ep);
 			ep->ep->driver_data = NULL;
 		}
-		atomic_set(&epfile->error, 1);
 		epfile->ep = NULL;
 
 		++ep;
