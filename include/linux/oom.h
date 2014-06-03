c6ec8d930aa6087f7c4d1fc2d6d49e7c35faa0ae
Liam Mark <lmark@codeaurora.org>
2014-06-03 13:33:18 -0700

    mm, oom: make dump_tasks public
    Allow other functions to dump the list of tasks.
    Useful for when debugging memory leaks.
    
    Change-Id: I76c33a118a9765b4c2276e8c76de36399c78dbf6
    Signed-off-by: Liam Mark <lmark@codeaurora.org>

--- include/linux/oom.h
+++ include/linux/oom.h
@@ -84,6 +84,9 @@ static inline void oom_killer_enable(void)
 
 extern struct task_struct *find_lock_task_mm(struct task_struct *p);
 
+extern void dump_tasks(const struct mem_cgroup *memcg,
+		const nodemask_t *nodemask);
+
 /* sysctls */
 extern int sysctl_oom_dump_tasks;
 extern int sysctl_oom_kill_allocating_task;
--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -343,7 +343,7 @@ static struct task_struct *select_bad_process(unsigned int *ppoints,
  * State information includes task's pid, uid, tgid, vm size, rss, nr_ptes,
  * swapents, oom_score_adj value, and name.
  */
-static void dump_tasks(const struct mem_cgroup *memcg, const nodemask_t *nodemask)
+void dump_tasks(const struct mem_cgroup *memcg, const nodemask_t *nodemask)
 {
 	struct task_struct *p;
 	struct task_struct *task;
