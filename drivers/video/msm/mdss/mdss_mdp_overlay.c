f78bef0cca75518b898a5d0670b96dd352273c88
Zohaib Alam <zalam@codeaurora.org>
2014-06-10 01:09:52 -0400

    msm: mdss: Allow display commit in DCM state for CMD panels
    Allow display commit in DCM state for command mode panels. QDCM
    tool requires explicit display kickoff during the calibration
    stage in which the panel power is turned off.
    
    Change-Id: Id6c7a0becf488ad2bfe9652b05d781384109f5c4
    Signed-off-by: Zohaib Alam <zalam@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -1533,7 +1533,8 @@ static void mdss_mdp_overlay_pan_display(struct msm_fb_data_type *mfd)
 	if (mutex_lock_interruptible(&mdp5_data->ov_lock))
 		return;
 
-	if (!mfd->panel_power_on) {
+	if ((!mfd->panel_power_on) && !((mfd->dcm_state == DCM_ENTER) &&
+				(mfd->panel.type == MIPI_CMD_PANEL))) {
 		mutex_unlock(&mdp5_data->ov_lock);
 		return;
 	}
