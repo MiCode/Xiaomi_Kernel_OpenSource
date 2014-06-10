0a226f3a30fea6bbf527800f3a96f16dfac4ca93
Manikanta Sivapala <msivap@codeaurora.org>
2014-06-10 17:46:13 +0530

    msm-vidc: Modify HFI not to alter VP8 max supported width
    Modify HFI not to alter firmware returned max supported width
    and height and make generic both for Venus and Q6. Remove
    creation of debugfs file for VP8 mode.
    
    Change-Id: I77d9077a79214d90f59eb7314bb66e310b5a418f
    Signed-off-by: Manikanta Sivapala <msivap@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc_debug.c
+++ drivers/media/platform/msm/vidc/msm_vidc_debug.c
@@ -20,7 +20,6 @@ int msm_vidc_debug_out = VIDC_OUT_PRINTK;
 int msm_fw_debug = 0x18;
 int msm_fw_debug_mode = 0x1;
 int msm_fw_low_power_mode = 0x1;
-int msm_vp8_low_tier = 0x1;
 int msm_vidc_hw_rsp_timeout = 1000;
 
 struct debug_buffer {
@@ -181,11 +180,6 @@ struct dentry *msm_vidc_debugfs_init_core(struct msm_vidc_core *core,
 	}
 	if (!debugfs_create_u32("fw_low_power_mode", S_IRUGO | S_IWUSR,
 			parent, &msm_fw_low_power_mode)) {
-		dprintk(VIDC_ERR, "debugfs_create_file: fail\n");
-		goto failed_create_dir;
-	}
-	if (!debugfs_create_u32("vp8_low_tier", S_IRUGO | S_IWUSR,
-			parent, &msm_vp8_low_tier)) {
 		dprintk(VIDC_ERR, "debugfs_create_file: fail\n");
 		goto failed_create_dir;
 	}
