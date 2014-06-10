06848c5f1a0309e80ad90c5f435561d2a4fa79c6
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-06-10 16:58:07 -0700

    sched: make sched_set_window() return failure when PELT is in use
    Window-based load tracking is a pre-requisite for the scheduler to
    feed cpu load information to the governor. When PELT is in use, return
    failure when governor attempts to set window-size. This will let
    governor fall back to other APIs for retrieving cpu load statistics.
    
    Change-Id: I0e11188594c1a54b3b7ff55447d30bfed1a01115
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1466,7 +1466,7 @@ unsigned long sched_get_busy(int cpu)
 			  NSEC_PER_USEC);
 }
 
-void sched_set_window(u64 window_start, unsigned int window_size)
+int sched_set_window(u64 window_start, unsigned int window_size)
 {
 	int cpu;
 	u64 ws;
@@ -1475,6 +1475,9 @@ void sched_set_window(u64 window_start, unsigned int window_size)
 	unsigned long flags;
 	u64 wallclock;
 	struct task_struct *g, *p;
+
+	if (sched_use_pelt)
+		return -EINVAL;
 
 	delta = window_start - now; /* how many jiffies ahead */
 
@@ -1526,6 +1529,8 @@ void sched_set_window(u64 window_start, unsigned int window_size)
 	}
 
 	local_irq_restore(flags);
+
+	return 0;
 }
 
 /* Keep track of max/min capacity possible across CPUs "currently" */
