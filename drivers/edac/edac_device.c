a8349c14d32363782780a46a38fe70a02a5fbb06
Stepan Moskovchenko <stepanm@codeaurora.org>
2014-06-10 19:24:50 -0700

    edac: Allow panic on correctable errors (CE).
    Add an EDAC device flag and associated sysfs entries to
    allow an EDAC driver to be configured to panic the kernel
    if a correctable error is detected. Though correctable
    errors (by definition) have no adverse system effects,
    a panic may still be useful, since a correctable error may
    be indicative of a marginal system state.
    
    Change-Id: I98921469254aa7b999979c1c7d9186286f982a0c
    Signed-off-by: Stepan Moskovchenko <stepanm@codeaurora.org>

--- drivers/edac/edac_device.c
+++ drivers/edac/edac_device.c
@@ -602,6 +602,12 @@ static inline int edac_device_get_log_ue(struct edac_device_ctl_info *edac_dev)
 	return edac_dev->log_ue;
 }
 
+static inline int edac_device_get_panic_on_ce(struct edac_device_ctl_info
+					*edac_dev)
+{
+	return edac_dev->panic_on_ce;
+}
+
 static inline int edac_device_get_panic_on_ue(struct edac_device_ctl_info
 					*edac_dev)
 {
@@ -651,6 +657,11 @@ void edac_device_handle_ce(struct edac_device_ctl_info *edac_dev,
 				"CE: %s instance: %s block: %s '%s'\n",
 				edac_dev->ctl_name, instance->name,
 				block ? block->name : "N/A", msg);
+
+	if (edac_device_get_panic_on_ce(edac_dev))
+		panic("EDAC %s: CE instance: %s block %s '%s'\n",
+			edac_dev->ctl_name, instance->name,
+			block ? block->name : "N/A", msg);
 }
 EXPORT_SYMBOL_GPL(edac_device_handle_ce);
 
