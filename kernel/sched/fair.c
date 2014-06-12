2a17b805457510ac4c70d34f65816300633602f3
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-06-12 10:42:56 -0700

    sched: Send NOHZ kick to idle cpu in same cluster
    A busy cpu will kick (via IPI) one of the idle cpus in tickless state
    to run load balance and help move tasks off itself. The cpu chosen to
    receive kick is simply the "first" idle cpu found in
    nohz.idle_cpus_mask. This could cause unnecessary wakeups of a
    cluster. A better choice would be to look for an idle cpu that is in
    the same cluster as busy cpu, which should minimize cluster wakeups.
    
    Change-Id: Ia63038d7c34b416b53c8feef3c3b31dab5200e42
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    [rameezmustafa@codeaurora.org]: Port to msm-3.18]
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
    [joonwoop@codeaurora.org: fixed minor conflict about return value.]
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2793,9 +2793,56 @@ int sched_hmp_proc_update_handler(struct ctl_table *table, int write,
 
 }
 
+static inline int find_new_hmp_ilb(void)
+{
+	int i;
+	int call_cpu = raw_smp_processor_id();
+	int best_cpu = nr_cpu_ids;
+	struct sched_domain *sd;
+	int min_cost = INT_MAX, cost;
+
+	rcu_read_lock();
+
+	/* Pick an idle cpu "closest" to call_cpu */
+	for_each_domain(call_cpu, sd) {
+		for_each_cpu(i, sched_domain_span(sd)) {
+			if (!idle_cpu(i))
+				continue;
+
+			cost = power_cost(NULL, i);
+			if (cost < min_cost) {
+				best_cpu = i;
+				min_cost = cost;
+			}
+		}
+
+		if (best_cpu < nr_cpu_ids)
+			break;
+	}
+
+	rcu_read_unlock();
+
+	return best_cpu;
+}
+
 #else	/* CONFIG_SCHED_HMP */
 
 static inline int select_best_cpu(struct task_struct *p, int target)
+{
+	return 0;
+}
+
+static inline int find_new_hmp_ilb(void)
+{
+	return 0;
+}
+
+static inline int power_cost(struct task_struct *p, int cpu)
+{
+	return SCHED_CAPACITY_SCALE;
+}
+
+static inline int mostly_idle_cpu(int cpu)
 {
 	return 0;
 }
@@ -7859,7 +7906,12 @@ static struct {
 
 static inline int find_new_ilb(void)
 {
-	int ilb = cpumask_first(nohz.idle_cpus_mask);
+	int ilb;
+
+	if (sysctl_sched_enable_hmp_task_placement)
+		return find_new_hmp_ilb();
+
+	ilb = cpumask_first(nohz.idle_cpus_mask);
 
 	if (ilb < nr_cpu_ids && idle_cpu(ilb))
 		return ilb;
@@ -8202,7 +8254,7 @@ static inline bool nohz_kick_needed(struct rq *rq)
 	if (time_before(now, nohz.next_balance))
 		return false;
 
-	if (rq->nr_running >= 2)
+	if (rq->nr_running >= 2 && !mostly_idle_cpu(cpu))
 		return true;
 
 	rcu_read_lock();
