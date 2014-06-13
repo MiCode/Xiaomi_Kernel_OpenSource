ab5f376fb612bc73b1c1512890c178f4f739491f
Stepan Moskovchenko <stepanm@codeaurora.org>
2014-06-12 18:55:06 -0700

    arm64: Log the machine name string during boot up
    The change to refactor kernel/setup.c to use the common
    of_flat_dt_get_machine_name() API has apparently removed
    the line which prints the device tree model string during
    boot. Having the model string in the kernel log is helpful,
    so add it back in. This change was already merged in past
    but possibly got overridden during upmerge. Add it back.
    While at it, add back print for the processor name and
    its rev id as well.
    
    Change-Id: I661684f7e64bfd820c30512aa96b9ac58ae40b7c
    Signed-off-by: Stepan Moskovchenko <stepanm@codeaurora.org>
    Signed-off-by: Kaushal Kumar <kaushalk@codeaurora.org>
    Signed-off-by: Neeraj Upadhyay <neeraju@codeaurora.org>

--- arch/arm64/kernel/setup.c
+++ arch/arm64/kernel/setup.c
@@ -336,8 +336,10 @@ static void __init setup_machine_fdt(phys_addr_t dt_phys)
 	}
 
 	machine_name = of_flat_dt_get_machine_name();
-	if (machine_name)
+	if (machine_name) {
+		dump_stack_set_arch_desc("%s (DT)", machine_name);
 		pr_info("Machine: %s\n", machine_name);
+	}
 }
 
 /*
@@ -507,6 +509,8 @@ static int c_show(struct seq_file *m, void *v)
 {
 	int i, j;
 
+	seq_printf(m, "Processor\t: %s rev %d (%s)\n",
+		cpu_name, read_cpuid_id() & 15, ELF_PLATFORM);
 	for_each_present_cpu(i) {
 		struct cpuinfo_arm64 *cpuinfo = &per_cpu(cpu_data, i);
 		u32 midr = cpuinfo->reg_midr;
