d01bb071c912cdead9a898cb60a5b3df1f9cec8c
Casey Piper <cpiper@codeaurora.org>
2014-06-12 21:30:41 -0700

    clk: qcom: mdss: add PLL clock support for 20nm HDMI PHY
    Add support for HDMI PLL on msm8994.
    Support is added to register this new clock
    driver. Also modify makefile to compile new
    20nm PLL source and add support for registering
    PLL clock driver for 20nm HDMI PHY.
    
    Change-Id: I57421ac638075358c46ddd938e441a8e525f3a5a
    Signed-off-by: Casey Piper <cpiper@codeaurora.org>

--- drivers/clk/qcom/mdss/mdss-pll.c
+++ drivers/clk/qcom/mdss/mdss-pll.c
@@ -143,6 +143,8 @@ static int mdss_pll_resource_parse(struct platform_device *pdev,
 		pll_res->pll_interface_type = MDSS_EDP_PLL;
 	} else if (!strcmp(compatible_stream, "qcom,mdss_hdmi_pll")) {
 		pll_res->pll_interface_type = MDSS_HDMI_PLL;
+	} else if (!strcmp(compatible_stream, "qcom,mdss_hdmi_pll_8994")) {
+		pll_res->pll_interface_type = MDSS_HDMI_PLL_20NM;
 	} else {
 		goto err;
 	}
@@ -180,6 +182,8 @@ static int mdss_pll_clock_register(struct platform_device *pdev,
 		break;
 	case MDSS_HDMI_PLL:
 		rc = hdmi_pll_clock_register(pdev, pll_res);
+	case MDSS_HDMI_PLL_20NM:
+		rc = hdmi_20nm_pll_clock_register(pdev, pll_res);
 		break;
 	case MDSS_UNKNOWN_PLL:
 	default:
@@ -314,6 +318,7 @@ static int mdss_pll_remove(struct platform_device *pdev)
 static const struct of_device_id mdss_pll_dt_match[] = {
 	{.compatible = "qcom,mdss_dsi_pll_8974"},
 	{.compatible = "qcom,mdss_dsi_pll_8994"},
+	{.compatible = "qcom,mdss_hdmi_pll_8994"},
 	{.compatible = "qcom,mdss_dsi_pll_8916"},
 	{.compatible = "qcom,mdss_dsi_pll_8939"},
 	{.compatible = "qcom,mdss_edp_pll"},
