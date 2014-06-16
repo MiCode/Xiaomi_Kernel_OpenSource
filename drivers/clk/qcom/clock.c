1f63f054eea2059aaff7455b79d6c0e4dd48e0b1
Patrick Daly <pdaly@codeaurora.org>
2014-06-16 13:24:22 -0700

    clk: qcom: Change default behaviour for clk_round_rate
    If a clock supports multiple rates, the round_rate op should be
    defined. For clocks which only support one rate, this op is no
    longer required.
    
    Change-Id: I68617537a0c119885941a5c6f478d31f530a3c0b
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

--- drivers/clk/qcom/clock.c
+++ drivers/clk/qcom/clock.c
@@ -558,17 +558,19 @@ long clk_round_rate(struct clk *clk, unsigned long rate)
 	if (IS_ERR_OR_NULL(clk))
 		return -EINVAL;
 
-	if (!clk->ops->round_rate)
-		return -ENOSYS;
-
 	for (i = 0; i < clk->num_fmax; i++)
 		fmax = max(fmax, clk->fmax[i]);
-
 	if (!fmax)
 		fmax = ULONG_MAX;
-
 	rate = min(rate, fmax);
-	rrate = clk->ops->round_rate(clk, rate);
+
+	if (clk->ops->round_rate)
+		rrate = clk->ops->round_rate(clk, rate);
+	else if (clk->rate)
+		rrate = clk->rate;
+	else
+		return -ENOSYS;
+
 	if (rrate > fmax)
 		return -EINVAL;
 	return rrate;
