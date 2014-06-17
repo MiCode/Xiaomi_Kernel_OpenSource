bd0c9726ecf4937c956f7cc62a0287f8af0d5305
Ahmed Abdul-Salam <aabdulsa@codeaurora.org>
2014-06-17 19:24:35 -0400

    msm: VPU: Clock frequency and bus bandwidth power optimizations
    - Increase clock frequency load threshold values
    - Correct bus bandwidth voting to be in bytes instead of bits
    - Assume a framerate of 30fps if none is set by client
    - Inform RPM of VPU power up only if it exceeds SVS frequencies
    
    Change-Id: Ie9df827999d3fcd47d7cb2c8fc1fa4c160453ee2
    Signed-off-by: Ahmed Abdul-Salam <aabdulsa@codeaurora.org>

--- drivers/media/platform/msm/vpu/vpu_resources.c
+++ drivers/media/platform/msm/vpu/vpu_resources.c
@@ -253,10 +253,11 @@ static int __vpu_load_bus_vector_data(struct vpu_platform_resources *res,
 		res->bus_table.loads[i] = vectors[i].load;
 
 		for (j = 0; j < num_ports; j++) {
+			/* change ab and ib values from kilobits to bytes */
 			bus_pdata->usecase[i].vectors[j].ab =
-					(u64)vectors[i].ab * 1000;
+					(u64)vectors[i].ab * 1000 / 8;
 			bus_pdata->usecase[i].vectors[j].ib =
-					(u64)vectors[i].ib * 1000;
+					(u64)vectors[i].ib * 1000 / 8;
 			bus_pdata->usecase[i].vectors[j].src =
 					bus_pdata_config.masters[j];
 			bus_pdata->usecase[i].vectors[j].dst =
