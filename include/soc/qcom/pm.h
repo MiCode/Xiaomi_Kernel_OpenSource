29f3c58b30a4c4b503744b5b18ab34b31f69be04
Venkat Devarasetty <vdevaras@codeaurora.org>
2014-06-20 16:54:26 +0530

    msm: pm: debug counters to DDR
    Today the PC debug counters are placed in IMEM. There is
    a need for more memory to save these counters for extra
    CPUs in newer platforms. As IMEM is scarce resource, move
    these per CPU counters to non cacheable area in DDR.
    Also save the physical start address in IMEM for easy
    access to location of counters while analyzing dumps.
    
    Change-Id: I049a67594d6a7b292c7fe617396639efa46c8d96
    Signed-off-by: Venkat Devarasetty <vdevaras@codeaurora.org>

--- include/soc/qcom/pm.h
+++ include/soc/qcom/pm.h
@@ -157,5 +157,5 @@ static inline void msm_pm_l2_add_stat(uint32_t id, int64_t t) {}
 #endif
 
 void msm_pm_set_cpr_ops(struct msm_pm_cpr_ops *ops);
-extern unsigned long msm_pc_debug_counters_phys;
+extern dma_addr_t msm_pc_debug_counters_phys;
 #endif  /* __ARCH_ARM_MACH_MSM_PM_H */
