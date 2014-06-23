be9938de2240e7c94dd44653c6e7af800a4bf7ad
Aviral Gupta <aviralg@codeaurora.org>
2014-06-23 16:23:20 +0530

    ASoC: q6asm: fix the error during playback in ffbm
    If the calibration data is not present the open of the audio
    client was failing.
    Even if the calibration data is not present then also the playback
    should continue.
    Fix by returning success even if the calibration data is not
    present.
    
    Change-Id: I768b6541686c71b03d5aa140cc62a59dce291f89
    CRs-Fixed: 683320
    Signed-off-by: Aviral Gupta <aviralg@codeaurora.org>

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -399,14 +399,12 @@ static int q6asm_map_cal_memory(struct cal_block_data *cal_block)
 	if (cal_block == NULL) {
 		pr_err("%s: cal_block is NULL!\n",
 			__func__);
-		result = -EINVAL;
 		goto done;
 	}
 
 	if (cal_block->cal_data.paddr == 0) {
 		pr_debug("%s: No address to map!\n",
 			__func__);
-		result = -EINVAL;
 		goto done;
 	}
 
@@ -421,7 +419,6 @@ static int q6asm_map_cal_memory(struct cal_block_data *cal_block)
 	if (cal_block->map_data.map_size == 0) {
 		pr_debug("%s: map size is 0!\n",
 			__func__);
-		result = -EINVAL;
 		goto done;
 	}
 
