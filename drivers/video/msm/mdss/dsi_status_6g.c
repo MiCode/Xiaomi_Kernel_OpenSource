336f79f7ef8f8967a95e9df52f26f4665ed3518f
Jayant Shekhar <jshekhar@codeaurora.org>
2014-06-24 19:27:09 +0530

    msm: mdss: Check ctl power on status before BTA status check
    Control path power on status should be checked before BTA status
    check else command sending failure and IOMMU page faults during
    bootup can be observed if ESD is enabled for device. Currently
    IOMMU is not powered on when commands are being sent.
    
    Change-Id: Ice66f38e79755eb14843fc63afe058e4bab2f0e2
    Signed-off-by: Jayant Shekhar <jshekhar@codeaurora.org>

--- drivers/video/msm/mdss/dsi_status_6g.c
+++ drivers/video/msm/mdss/dsi_status_6g.c
@@ -65,6 +65,13 @@ void mdss_check_dsi_ctrl_status(struct work_struct *work, uint32_t interval)
 		return;
 	}
 
+	if (!ctl->power_on) {
+		schedule_delayed_work(&pstatus_data->check_status,
+			msecs_to_jiffies(interval));
+		pr_err("%s: ctl not powered on\n", __func__);
+		return;
+	}
+
 	if (ctl->shared_lock)
 		mutex_lock(ctl->shared_lock);
 	mutex_lock(&mdp5_data->ov_lock);
