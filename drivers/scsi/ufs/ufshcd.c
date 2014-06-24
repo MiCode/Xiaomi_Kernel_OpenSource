6e65bf2dcb58cef1b169e573473f4a557a2852ef
Lee Susman <lsusman@codeaurora.org>
2014-06-24 17:28:50 +0300

    scsi: ufs: add trace event for ufs commands
    Use the ftrace infrastructure to conditionally trace ufs command events.
    New trace event is created, which samples the following ufs command data:
    - device name
    - optional identification string
    - task tag
    - doorbell register
    - number of transfer bytes
    - interrupt status register
    - request start LBA
    - command opcode
    
    Currently we only fully trace read(10) and write(10) commands.
    All other commands which pass through ufshcd_send_command() will be
    printed with "-1" in the lba and transfer_len fields.
    
    Usage:
    	echo 1 > /sys/kernel/debug/tracing/events/ufs/enable
    	cat /sys/kernel/debug/tracing/trace_pipe
    
    Change-Id: Id6e16a8c0b2a833c5453ed9923384f641e374a71
    Signed-off-by: Lee Susman <lsusman@codeaurora.org>
    [gbroner@codeaurora.org: fix merge conflict]
    Signed-off-by: Gilad Broner <gbroner@codeaurora.org>
    [subhashj@codeaurora.org: resolved merge conflicts and compilation error]
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -337,6 +337,51 @@ static inline void ufshcd_remove_non_printable(char *val)
 		*val = ' ';
 }
 
+#ifdef CONFIG_TRACEPOINTS
+static void ufshcd_add_command_trace(struct ufs_hba *hba,
+		unsigned int tag, const char *str)
+{
+	sector_t lba = -1;
+	u8 opcode = 0;
+	u32 intr, doorbell;
+	struct ufshcd_lrb *lrbp;
+	int transfer_len = -1;
+
+	lrbp = &hba->lrb[tag];
+
+	if (lrbp->cmd) { /* data phase exists */
+		opcode = (u8)(*lrbp->cmd->cmnd);
+		if ((opcode == READ_10) || (opcode == WRITE_10)) {
+			/*
+			 * Currently we only fully trace read(10) and write(10)
+			 * commands
+			 */
+			if (lrbp->cmd->request && lrbp->cmd->request->bio)
+				lba =
+				  lrbp->cmd->request->bio->bi_iter.bi_sector;
+			transfer_len = be32_to_cpu(
+				lrbp->ucd_req_ptr->sc.exp_data_transfer_len);
+		}
+	}
+
+	intr = ufshcd_readl(hba, REG_INTERRUPT_STATUS);
+	doorbell = ufshcd_readl(hba, REG_UTP_TRANSFER_REQ_DOOR_BELL);
+	trace_ufshcd_command(dev_name(hba->dev), str, tag,
+				doorbell, transfer_len, intr, lba, opcode);
+}
+
+static inline void ufshcd_cond_add_cmd_trace(struct ufs_hba *hba,
+					unsigned int tag, const char *str)
+{
+	ufshcd_add_command_trace(hba, tag, str);
+}
+#else
+static inline void ufshcd_cond_add_cmd_trace(struct ufs_hba *hba,
+					unsigned int tag, const char *str)
+{
+}
+#endif
+
 static void ufshcd_print_host_regs(struct ufs_hba *hba)
 {
 	/*
@@ -1660,6 +1705,7 @@ int ufshcd_send_command(struct ufs_hba *hba, unsigned int task_tag)
 	ufshcd_writel(hba, 1 << task_tag, REG_UTP_TRANSFER_REQ_DOOR_BELL);
 	/* Make sure that doorbell is commited immediately */
 	wmb();
+	ufshcd_cond_add_cmd_trace(hba, task_tag, "send");
 	UFSHCD_UPDATE_TAG_STATS(hba, task_tag);
 	return ret;
 }
@@ -4497,6 +4543,7 @@ static void ufshcd_transfer_req_compl(struct ufs_hba *hba)
 		lrbp = &hba->lrb[index];
 		cmd = lrbp->cmd;
 		if (cmd) {
+			ufshcd_cond_add_cmd_trace(hba, index, "complete");
 			UFSHCD_UPDATE_TAG_STATS_COMPLETION(hba, cmd);
 			result = ufshcd_transfer_rsp_status(hba, lrbp);
 			scsi_dma_unmap(cmd);
@@ -4510,8 +4557,11 @@ static void ufshcd_transfer_req_compl(struct ufs_hba *hba)
 			__ufshcd_pm_qos_release(hba);
 			__ufshcd_hibern8_release(hba);
 		} else if (lrbp->command_type == UTP_CMD_TYPE_DEV_MANAGE) {
-			if (hba->dev_cmd.complete)
+			if (hba->dev_cmd.complete) {
+				ufshcd_cond_add_cmd_trace(hba, index,
+						"dev_complete");
 				complete(hba->dev_cmd.complete);
+			}
 		}
 	}
 
