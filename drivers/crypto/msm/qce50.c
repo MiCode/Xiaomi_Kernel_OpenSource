8c65c916b6cb161dd762e5b88d87ecc981dacea8
AnilKumar Chimata <anilc@codeaurora.org>
2014-06-24 19:31:10 +0530

    crypto: msm: Fix SHA-1/256 zero length input data issue
    Crypto module generating wrong hash value for zero length input data
    using SHA1 and SHA256 functions. This patch fixes the issue to generate
    proper hash value.
    
    Change-Id: I795969cf2d3e3887064d56e35cbb488937051ac8
    Signed-off-by: AnilKumar Chimata <anilc@codeaurora.org>

--- drivers/crypto/msm/qce50.c
+++ drivers/crypto/msm/qce50.c
@@ -4837,9 +4837,9 @@ int qce_process_sha_req(void *handle, struct qce_sha_req *sreq)
 	if (_qce_sps_add_sg_data(pce_dev, areq->src, areq->nbytes,
 						 &pce_dev->ce_sps.in_transfer))
 		goto bad;
-	_qce_set_flag(&pce_dev->ce_sps.in_transfer,
-				SPS_IOVEC_FLAG_EOT|SPS_IOVEC_FLAG_NWD);
-
+	if (areq->nbytes)
+		_qce_set_flag(&pce_dev->ce_sps.in_transfer,
+					SPS_IOVEC_FLAG_EOT|SPS_IOVEC_FLAG_NWD);
 	if (_qce_sps_add_data(GET_PHYS_ADDR(pce_dev->ce_sps.result_dump),
 					CRYPTO_RESULT_DUMP_SIZE,
 					  &pce_dev->ce_sps.out_transfer))
