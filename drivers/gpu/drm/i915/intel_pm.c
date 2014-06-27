8c5de92d9a467c4ecbe48fb2dcd35036b30efc2d
Ville Syrjälä <ville.syrjala@linux.intel.com>
2014-06-28 02:04:07 +0300

    FROM_UPSTREAM [VPG]: drm/i915: Add chv_power_wells[]
    Add chv_power_wells[] so we can start to build up the power well support
    for chv. Just the "always on" well there initialy.
    
    Change-Id: I710ce6012e611d6bb020dd19d8a8be752138280d
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Upstream-repo: git://anongit.freedesktop.org/drm-intel
    Upstream-commit: a1e633f780e86e7071d7cb547ed82666145fd4a4
    Upstream-as-of-tag: <not available>

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -7144,6 +7144,16 @@ static struct i915_power_well vlv_power_wells[] = {
 	},
 };
 
+static struct i915_power_well chv_power_wells[] = {
+	{
+		.name = "always-on",
+		.always_on = 1,
+		.domains = VLV_ALWAYS_ON_POWER_DOMAINS,
+		.ops = &i9xx_always_on_power_well_ops,
+	},
+};
+
+
 #define set_power_wells(power_domains, __power_wells) ({		\
 	(power_domains)->power_wells = (__power_wells);			\
 	(power_domains)->power_well_count = ARRAY_SIZE(__power_wells);	\
@@ -7165,6 +7175,8 @@ int intel_power_domains_init(struct drm_i915_private *dev_priv)
 	} else if (IS_BROADWELL(dev_priv->dev)) {
 		set_power_wells(power_domains, bdw_power_wells);
 		hsw_pwr = power_domains;
+	} else if (IS_CHERRYVIEW(dev_priv->dev)) {
+		set_power_wells(power_domains, chv_power_wells);
 	} else if (IS_VALLEYVIEW(dev_priv->dev)) {
 		set_power_wells(power_domains, vlv_power_wells);
 	} else {
