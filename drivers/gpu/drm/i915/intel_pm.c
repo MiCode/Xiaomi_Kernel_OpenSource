2f00df95e0cd55d2cbe828e2db260e0cafc54bfd
Deepak S <deepak.s@linux.intel.com>
2014-06-28 11:26:26 +0530

    FROM_UPSTREAM [VPG]: drm/i915/chv: Drop WaGsvBringDownFreqInRc6
    Drop WaGsvBringDownFreq on CHV.
    When in RC6 requesting the min freq should be fine to bring the
    voltage down.
    
    Change-Id: Ie81f426ef0929f485a54b3f25b09a4f174bdbb21
    Signed-off-by: Deepak S <deepak.s@linux.intel.com>
    Reviewed-by: Jesse Barnes <jbarnes@Virtuousgeek.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Upstream-repo: git://anongit.freedesktop.org/drm-intel
    Upstream-commit: 34638118f987c3f4136e442b65de22d73a0458cb
    Upstream-as-of-tag: <not available>

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -3746,7 +3746,9 @@ void gen6_rps_idle(struct drm_i915_private *dev_priv)
 	mutex_lock(&dev_priv->rps.hw_lock);
 	if (dev_priv->rps.enabled && !dev_priv->rps.manual_mode
 			&& !dev_priv->rps.debugfs_disable_boost) {
-		if (IS_VALLEYVIEW(dev))
+		if (IS_CHERRYVIEW(dev))
+			valleyview_set_rps(dev_priv->dev, dev_priv->rps.min_freq_softlimit);
+		else if (IS_VALLEYVIEW(dev))
 			vlv_set_rps_idle(dev_priv);
 		else
 			gen6_set_rps(dev_priv->dev, dev_priv->rps.min_freq_softlimit);
