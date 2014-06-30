f6afdaae42156c3cbce6a404f74ac3d47de5c244
Himanshu Aggarwal <haggarwa@codeaurora.org>
2014-06-30 14:46:29 +0530

    input: synaptics_dsx: fix up world writable sysfs file
    Allowing the data sysfs file is not a good idea. We don't need this
    sysfs file to be world writable or group writable. By default only
    root should be able to write to this file. Change the write permissions
    of this file to S_IWUSR (00200).
    
    Signed-off-by: Himanshu Aggarwal <haggarwa@codeaurora.org>
    Change-Id: I1d92d0e3eb2cfdc52a3873babe94c9c562a9df85

--- drivers/input/touchscreen/synaptics_dsx/synaptics_dsx_rmi_dev.c
+++ drivers/input/touchscreen/synaptics_dsx/synaptics_dsx_rmi_dev.c
@@ -72,7 +72,7 @@ struct rmidev_data {
 static struct bin_attribute attr_data = {
 	.attr = {
 		.name = "data",
-		.mode = (S_IRUGO | S_IWUGO),
+		.mode = (S_IRUGO | S_IWUSR),
 	},
 	.size = 0,
 	.read = rmidev_sysfs_data_show,
