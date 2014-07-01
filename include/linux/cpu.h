9a43ce1192377cc360ca072dca2771154a901911
Osvaldo Banuelos <osvaldob@codeaurora.org>
2014-07-01 12:21:00 -0700

    cpu: fix section mismatch warnings with __hotcpu_register
    Upstream v.3.14.10+ drops the use of __cpuinit infrastructure.
    "CPU hotplug: Provide lockless versions of callback registration
     functions", which fixes a legitimate ABBA deadlock is brought
    in from v3.15.3-stable and introduces __hotcpu_register. Since
    functions used as argument to this API are all annotated with
    __cpuinit in msm-3.10, annotate __hotcpu_register with
    __cpuinitdata to match this definition.
    
    Change-Id: I69588fc780974a2a9b7c6d63b247aca2608d1c00
    Signed-off-by: Osvaldo Banuelos <osvaldob@codeaurora.org>

--- include/linux/cpu.h
+++ include/linux/cpu.h
@@ -122,7 +122,7 @@ enum {
 }
 
 #define __cpu_notifier(fn, pri) {				\
-	static struct notifier_block fn##_nb =			\
+	static struct notifier_block fn##_nb __cpuinitdata =	\
 		{ .notifier_call = fn, .priority = pri };	\
 	__register_cpu_notifier(&fn##_nb);			\
 }
