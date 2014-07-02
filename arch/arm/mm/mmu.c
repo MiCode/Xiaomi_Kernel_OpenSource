870a4ac613521304de562fe4dd98c079936938d2
Rob Herring <robh@kernel.org>
2014-07-02 02:01:15 -0500

    ARM: expand fixmap region to 3MB
    With commit a05e54c103b0b8 "ARM: 8031/2: change fixmap mapping region to
    support 32 CPUs", the fixmap region was expanded to 2MB, but it
    precluded any other uses of the fixmap region in order to support up to
    32 CPUs. In order to support other uses the fixmap region needs to be
    expanded beyond 2MB. Fortunately, the adjacent 1MB range
    0xffe00000-0xfff00000 is availabe to use.
    
    Remove fixmap_page_table ptr and lookup the page table via the virtual
    address so that the fixmap region can span more that one pmd. The 2nd
    pmd is already created since it is shared with the vector page.
    
    Change-Id: I3312ab8d4641ab522c501a0144e2b08aba7364b2
    Cc: Leif Lindholm <leif.lindholm@linaro.org>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Russell King <linux@arm.linux.org.uk>
    Signed-off-by: Rob Herring <robh@kernel.org>
    Git-commit: 6bca312b359bdad80324cc750fe5d6a075eb66ad
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/robh/linux.git
    Signed-off-by: Venkatesh Yadav Abbarapu <vabbar@codeaurora.org>

--- arch/arm/mm/mmu.c
+++ arch/arm/mm/mmu.c
@@ -1327,7 +1327,7 @@ static void __init kmap_init(void)
 	pkmap_page_table = early_pte_alloc(pmd_off_k(PKMAP_BASE),
 		PKMAP_BASE, _PAGE_KERNEL_TABLE);
 
-	fixmap_page_table = early_pte_alloc(pmd_off_k(FIXADDR_START),
+	early_pte_alloc(pmd_off_k(FIXADDR_START),
 		FIXADDR_START, _PAGE_KERNEL_TABLE);
 #endif
 }
