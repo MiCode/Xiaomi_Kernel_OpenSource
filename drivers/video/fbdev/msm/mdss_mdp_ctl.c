332378092471e5446ecc4051ea0cb42118d008ed
Ujwal Patel <ujwalp@codeaurora.org>
2014-07-02 17:43:48 -0700

    msm: mdss: reset ping-pong registers when display is turned off
    Ping-pong registers are used for tear check when command mode panel
    is used. They are used for frame buffer compression configuration if
    panel supports it. But if video mode panel is used, like HDMI or eDP,
    those registers should be at their reset value otherwise it can cause
    unpredictable hw state and results in under-runs. To avoid this, reset
    ping-pong registers when panel is turned off.
    
    Change-Id: If792c71ac584550a24fd3424481b34179fbf1d77
    Signed-off-by: Ujwal Patel <ujwalp@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_mdp_ctl.c
+++ drivers/video/fbdev/msm/mdss_mdp_ctl.c
@@ -2097,14 +2097,20 @@ int mdss_mdp_ctl_stop(struct mdss_mdp_ctl *ctl)
 
 	mdss_mdp_hist_intr_setup(&mdata->hist_intr, MDSS_IRQ_SUSPEND);
 
-	if (ctl->stop_fnc)
+	if (ctl->stop_fnc) {
 		ret = ctl->stop_fnc(ctl);
-	else
+		if (ctl->panel_data->panel_info.fbc.enabled)
+			mdss_mdp_ctl_fbc_enable(0, ctl->mixer_left,
+				&ctl->panel_data->panel_info);
+	} else {
 		pr_warn("no stop func for ctl=%d\n", ctl->num);
+	}
 
 	if (sctl && sctl->stop_fnc) {
 		ret = sctl->stop_fnc(sctl);
-
+		if (ctl->panel_data->panel_info.fbc.enabled)
+			mdss_mdp_ctl_fbc_enable(0, sctl->mixer_left,
+				&sctl->panel_data->panel_info);
 		mdss_mdp_ctl_split_display_enable(0, ctl, sctl);
 	}
 
