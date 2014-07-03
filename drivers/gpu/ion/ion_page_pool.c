f713c8cf8817ee04706459eb8b36398c256d6a5e
Olav Haugan <ohaugan@codeaurora.org>
2014-07-03 12:11:21 +0530

    gpu: ion: Optimize zeroing when allocating from system heap
    Currently when allocating from system heap each page is individually
    mapped and zero'ed in the page pool code. Under high memory pressure when
    we allocate a lot of small pages this becomes a bottleneck. Instead,
    do the zeroing in one fell swoop at the end of allocating all the pages
    but only for the pages that are not already zero'ed and sync'ed (pages
    coming from the page pool).
    
    Test results
    ------------
    Time recorded for allocating 20 x 19MB ION buffers from System heap with
    forced 4K page allocations (all pages allocated was order-0) right after
    boot to Android homescreen using performance kernel configuration.
    
    Before change (non-cached allocations):
    Run 1: 768ms
    Run 2: 752ms
    Run 3: 759ms
    
    Before change (cached allocations):
    Run 1: 726ms
    Run 2: 769ms
    Run 3: 645ms
    
    After change (cached allocations):
    Run 1: 262ms
    Run 2: 249ms
    Run 3: 310ms
    
    After change (non-cached allocations):
    Run 1: 275ms
    Run 2: 234ms
    Run 3: 259ms
    
    Time recorded for allocating 20 x 19MB ION buffers from System heap (with no
    forced page size) right after boot to Android homescreen using
    performance kernel configuration. Run 1 is taken right after boot while
    run 2 and 3 are subsequent runs without reboot between.
    
    Before change (non-cached allocations):
    Run 1: 181ms
    Run 2: 1ms
    Run 3: 1ms
    
    Before change (cached allocations):
    Run 1: 220ms
    Run 2: 1ms
    Run 3: 2ms
    
    After change (non-cached allocations):
    Run 1: 166ms
    Run 2: 1ms
    Run 3: 1ms
    
    After change (cached allocations):
    Run 1: 170ms
    Run 2: 1ms
    Run 3: 3ms
    
    CRs-fixed: 660049
    Change-Id: I8dcfdb4adb8d5f4480768f951523fa5c64db87b1
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>
    [nabrah@codeaurora.org: changes manually merged from
     drivers/staging/android/ion. In ion_system_heap.c, in function
     ion_system_heap_allocate, the call to 'list_first_entry_or_null'
     is replaced with  'list_first_entry' and 'list_empty' as
     'list_first_entry_or_null is not supported in 3.4 kernel branch]
    Signed-off-by: Nirmal Abraham <nabrah@codeaurora.org>

--- drivers/gpu/ion/ion_page_pool.c
+++ drivers/gpu/ion/ion_page_pool.c
@@ -32,7 +32,6 @@ struct ion_page_pool_item {
 static void *ion_page_pool_alloc_pages(struct ion_page_pool *pool)
 {
 	struct page *page;
-	struct scatterlist sg;
 
 	page = alloc_pages(pool->gfp_mask & ~__GFP_ZERO, pool->order);
 
@@ -42,11 +41,6 @@ static void *ion_page_pool_alloc_pages(struct ion_page_pool *pool)
 	if (pool->gfp_mask & __GFP_ZERO)
 		if (ion_heap_high_order_page_zero(page, pool->order))
 			goto error_free_pages;
-
-	sg_init_table(&sg, 1);
-	sg_set_page(&sg, page, PAGE_SIZE << pool->order, 0);
-	sg_dma_address(&sg) = sg_phys(&sg);
-	dma_sync_sg_for_device(NULL, &sg, 1, DMA_BIDIRECTIONAL);
 
 	return page;
 error_free_pages:
