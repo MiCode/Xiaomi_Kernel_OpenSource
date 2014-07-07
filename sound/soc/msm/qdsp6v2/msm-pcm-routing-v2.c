2c743eb75f349c99a73e0462742347c74787c9b6
Asish Bhattacharya <asishb@codeaurora.org>
2014-07-07 10:52:27 +0530

    ASoC: msm: reorg q6core header file
    This is needed to allow include from non soc folder.
    
    CRs-Fixed: 689849
    Change-Id: I753012d5ce9c5a788e75b4edf9b0099b58b30ec0
    Signed-off-by: Asish Bhattacharya <asishb@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
@@ -30,6 +30,7 @@
 #include <sound/tlv.h>
 #include <sound/asound.h>
 #include <sound/pcm_params.h>
+#include <sound/q6core.h>
 #include <linux/slab.h>
 
 #include "msm-pcm-routing-v2.h"
@@ -37,7 +38,6 @@
 #include "msm-dts-srs-tm-config.h"
 #include "msm-dolby-dap-config.h"
 #include "q6voice.h"
-#include "q6core.h"
 #include "sound/q6lsm.h"
 
 #define EC_PORT_ID_PRIMARY_MI2S_TX    1
