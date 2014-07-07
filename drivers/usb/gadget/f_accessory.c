afba599e102f8bf90a6b7396a97b1db8e6820188
Mian Yousaf Kaukab <yousaf.kaukab@intel.com>
2014-07-07 15:21:44 +0200

    usb: gadget: accessory: align read length to maxp
    Change-Id: I7cd33ca3ea7efd52bc72cce2c56a6622cd5f87fb
    Signed-off-by: Mian Yousaf Kaukab <yousaf.kaukab@intel.com>

--- drivers/usb/gadget/f_accessory.c
+++ drivers/usb/gadget/f_accessory.c
@@ -550,6 +550,7 @@ static ssize_t acc_read(struct file *fp, char __user *buf,
 	ssize_t r = count;
 	unsigned xfer;
 	int ret = 0;
+	int aligned_count;
 
 	pr_debug("acc_read(%zu)\n", count);
 
@@ -558,8 +559,9 @@ static ssize_t acc_read(struct file *fp, char __user *buf,
 		return -ENODEV;
 	}
 
-	if (count > BULK_BUFFER_SIZE)
-		count = BULK_BUFFER_SIZE;
+	aligned_count = ALIGN(count, dev->ep_out->maxpacket);
+	if (aligned_count > BULK_BUFFER_SIZE)
+		aligned_count = BULK_BUFFER_SIZE;
 
 	/* we will block until we're online */
 	pr_debug("acc_read: waiting for online\n");
@@ -578,7 +580,7 @@ static ssize_t acc_read(struct file *fp, char __user *buf,
 requeue_req:
 	/* queue a request */
 	req = dev->rx_req[0];
-	req->length = count;
+	req->length = aligned_count;
 	dev->rx_done = 0;
 	ret = usb_ep_queue(dev->ep_out, req, GFP_KERNEL);
 	if (ret < 0) {
