0db8fc47f09e5dc0e8cd4eecefb175c987109024
Sivan Reinstein <sivanr@codeaurora.org>
2014-07-10 16:22:41 +0300

    net: rmnet_data: add support for DL MAP based checksum offload
    Add DL checksum offload routines for MAPv3. Can bypass checksum software
    for IPv4/IPv6 TCP/UDP protocols.
    
    CRs-fixed: 692334
    Change-Id: Ic13a5d9a1ebfdc57b6eb53ee93da92c3aee547b1
    Signed-off-by: Sivan Reinstein <sivanr@codeaurora.org>

--- net/rmnet_data/rmnet_map.h
+++ net/rmnet_data/rmnet_map.h
@@ -48,6 +48,22 @@ struct rmnet_map_control_command_s {
 	};
 }  __aligned(1);
 
+struct rmnet_map_dl_checksum_trailer_s {
+	unsigned char  reserved_h;
+#if defined(__LITTLE_ENDIAN_BITFIELD)
+	unsigned char  valid:1;
+	unsigned char  reserved_l:7;
+#elif defined(__BIG_ENDIAN_BITFIELD)
+	unsigned char  reserved_l:7;
+	unsigned char  valid:1;
+#else
+#error "Please fix <asm/byteorder.h>"
+#endif
+	unsigned short checksum_start_offset;
+	unsigned short checksum_length;
+	unsigned short checksum_value;
+} __aligned(1);
+
 enum rmnet_map_results_e {
 	RMNET_MAP_SUCCESS,
 	RMNET_MAP_CONSUMED,
@@ -70,11 +86,12 @@ enum rmnet_map_checksum_errors_e {
 	RMNET_MAP_CHECKSUM_OK,
 	RMNET_MAP_CHECKSUM_VALID_FLAG_NOT_SET,
 	RMNET_MAP_CHECKSUM_VALIDATION_FAILED,
-	RMNET_MAP_CHECKSUM_ERROR_UNKOWN,
-	RMNET_MAP_CHECKSUM_ERROR_NOT_DATA_PACKET,
-	RMNET_MAP_CHECKSUM_ERROR_BAD_BUFFER,
-	RMNET_MAP_CHECKSUM_ERROR_UNKNOWN_IP_VERSION,
-	RMNET_MAP_CHECKSUM_ERROR_UNKNOWN_TRANSPORT,
+	RMNET_MAP_CHECKSUM_ERR_UNKOWN,
+	RMNET_MAP_CHECKSUM_ERR_NOT_DATA_PACKET,
+	RMNET_MAP_CHECKSUM_ERR_BAD_BUFFER,
+	RMNET_MAP_CHECKSUM_ERR_UNKNOWN_IP_VERSION,
+	RMNET_MAP_CHECKSUM_ERR_UNKNOWN_TRANSPORT,
+	RMNET_MAP_CHECKSUM_FRAGMENTED_PACKET,
 	/* This should always be the last element */
 	RMNET_MAP_CHECKSUM_ENUM_LENGTH
 };
@@ -93,11 +110,6 @@ enum rmnet_map_agg_state_e {
 	RMNET_MAP_TXFER_SCHEDULED
 };
 
-#define RMNET_MAP_P_ICMP4  0x01
-#define RMNET_MAP_P_TCP    0x06
-#define RMNET_MAP_P_UDP    0x11
-#define RMNET_MAP_P_ICMP6  0x3a
-
 #define RMNET_MAP_COMMAND_REQUEST     0
 #define RMNET_MAP_COMMAND_ACK         1
 #define RMNET_MAP_COMMAND_UNSUPPORTED 2
@@ -113,5 +125,8 @@ rx_handler_result_t rmnet_map_command(struct sk_buff *skb,
 				      struct rmnet_phys_ep_conf_s *config);
 void rmnet_map_aggregate(struct sk_buff *skb,
 			 struct rmnet_phys_ep_conf_s *config);
+
+int rmnet_map_checksum_downlink_packet(struct sk_buff *skb);
+
 
 #endif /* _RMNET_MAP_H_ */
