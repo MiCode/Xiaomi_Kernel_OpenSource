16ccbc425d19a0345e7ce881f3091f5de1262f81
Tarun Gupta <tarung@codeaurora.org>
2014-07-10 14:36:05 +0530

    Revert "USB: storage: Allow UICC cards to perform house keeping operations"
    This reverts commit 953f1de83aa27c293ad51e232efc0cdf7de4becc.
    
    The house keeping operations introduced in above mentioned change are not
    performed by linux host, UICC card works reliably with linux host. With
    this operations the mass storage errors are reported more frequently, so
    revert the mentioned commit.
    
    CRs-Fixed: 691655
    Change-Id: I872cfcb4d046c4d658b1f0edecf279418d3e5ee1
    Signed-off-by: Tarun Gupta <tarung@codeaurora.org>

--- drivers/usb/storage/scsiglue.c
+++ drivers/usb/storage/scsiglue.c
@@ -261,14 +261,6 @@ static int slave_configure(struct scsi_device *sdev)
 			sdev->autosuspend_delay = us->sdev_autosuspend_delay;
 		}
 
-		/*
-		 * This quirk enables sending consecutive TEST_UNIT_READY
-		 * commands in WRITE(10) command processing context. Increase
-		 * the timeout to 60 seconds.
-		 */
-		if (us->fflags & US_FL_TUR_AFTER_WRITE)
-			blk_queue_rq_timeout(sdev->request_queue, (60 * HZ));
-
 	} else {
 
 		/* Non-disk-type devices don't need to blacklist any pages
