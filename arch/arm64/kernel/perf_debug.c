561bac78b6c4fff2450ea500eb26404c576991dc
Neil Leeder <nleeder@codeaurora.org>
2014-07-11 14:06:02 -0400

    Perf: arm64: add perf user-mode permissions
    Allow user-mode applications to use the performance
    counters for debugging and performance analysis.
    
    Change-Id: I3985c0fccfe925c50ace5d33435f6f69b31e07a5
    Signed-off-by: Neil Leeder <nleeder@codeaurora.org>

--- arch/arm64/kernel/perf_debug.c
+++ arch/arm64/kernel/perf_debug.c
@@ -24,6 +24,7 @@
 static char *descriptions =
 	" 0 arm64: perf: add debug patch logging framework\n"
 	" 1 Perf: arm64: Add L1 counters to tracepoints\n"
+	" 5 Perf: arm64: add perf user-mode permissions\n"
 ;
 
 static ssize_t desc_read(struct file *fp, char __user *buf,
