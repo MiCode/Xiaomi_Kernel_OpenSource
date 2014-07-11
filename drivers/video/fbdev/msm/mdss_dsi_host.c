db066eaba487b51abaa6965d9599f9127a9ca780
Kuogee Hsieh <khsieh@codeaurora.org>
2014-07-11 13:41:37 -0700

    msm: mdss: call MDSS_XLOG_TOUT_HANDLER at thread context
    MDSS_XLOG_TOUT_HANDLER() is for debug purpose. It acquires
    a mutex and thus should not be called from an interrupt
    context. Move this call to a thread context instead.
    
    CRs-Fixed: 692386
    Change-Id: I631ffa4c972b66991eb65ad0de294378e0d3f266
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_dsi_host.c
+++ drivers/video/fbdev/msm/mdss_dsi_host.c
@@ -1445,6 +1445,9 @@ static int dsi_event_thread(void *data)
 				mdss_dsi_clk_ctrl(ctrl, DSI_ALL_CLKS, 0);
 			}
 			mutex_unlock(&ctrl->mutex);
+
+			MDSS_XLOG_TOUT_HANDLER("mdp", "dsi0", "dsi1",
+						"edp", "hdmi", "panic");
 		}
 
 		if (todo & DSI_EV_DSI_FIFO_EMPTY)
@@ -1530,8 +1533,6 @@ void mdss_dsi_fifo_status(struct mdss_dsi_ctrl_pdata *ctrl)
 		pr_err("%s: status=%x\n", __func__, status);
 		if (status & 0x0080)  /* CMD_DMA_FIFO_UNDERFLOW */
 			dsi_send_events(ctrl, DSI_EV_MDP_FIFO_UNDERFLOW);
-			MDSS_XLOG_TOUT_HANDLER("mdp", "dsi0", "dsi1",
-						"edp", "hdmi", "panic");
 		if (status & 0x11110000) /* DLN_FIFO_EMPTY */
 			dsi_send_events(ctrl, DSI_EV_DSI_FIFO_EMPTY);
 	}
