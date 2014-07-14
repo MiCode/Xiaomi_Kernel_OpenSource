26fcbad8697e1b630577611d8b406844138b05bc
Vikram Mulukutla <markivx@codeaurora.org>
2014-07-14 12:52:56 -0700

    qcom: smem: Rework SMEM ramdump logic
    SMEM ramdumps should only be transferred if the subsystem
    restart framework indicates that ramdumps are enabled.
    Furthermore, use the SUBSYS_RAMDUMP_NOTIFICATION notifier
    type instead of AFTER_SHUTDOWN.
    
    Change-Id: I8d48add907e57687fa320b4ed80de61a46e27b91
    Signed-off-by: Vikram Mulukutla <markivx@codeaurora.org>

--- drivers/soc/qcom/smem.c
+++ drivers/soc/qcom/smem.c
@@ -24,6 +24,7 @@
 #include <linux/slab.h>
 #include <linux/stat.h>
 #include <soc/qcom/subsystem_notif.h>
+#include <soc/qcom/subsystem_restart.h>
 #include <soc/qcom/ramdump.h>
 
 #include <soc/qcom/smem.h>
@@ -987,34 +988,38 @@ static int restart_notifier_cb(struct notifier_block *this,
 				unsigned long code,
 				void *data)
 {
-	if (code == SUBSYS_AFTER_SHUTDOWN) {
-		struct restart_notifier_block *notifier;
+	struct restart_notifier_block *notifier;
+	struct notif_data *notifdata = data;
+	int ret;
 
+	switch (code) {
+
+	case SUBSYS_AFTER_SHUTDOWN:
 		notifier = container_of(this,
 					struct restart_notifier_block, nb);
 		SMEM_INFO("%s: ssrestart for processor %d ('%s')\n",
 				__func__, notifier->processor,
 				notifier->name);
-
 		remote_spin_release(&remote_spinlock, notifier->processor);
 		remote_spin_release_all(notifier->processor);
-
-		if (smem_ramdump_dev) {
-			int ret;
-
-			SMEM_DBG("%s: saving ramdump\n", __func__);
-			/*
-			 * XPU protection does not currently allow the
-			 * auxiliary memory regions to be dumped.  If this
-			 * changes, then num_smem_areas + 1 should be passed
-			 * into do_elf_ramdump() to dump all regions.
-			 */
-			ret = do_elf_ramdump(smem_ramdump_dev,
-					smem_ramdump_segments, 1);
-			if (ret < 0)
-				LOG_ERR("%s: unable to dump smem %d\n",
-								__func__, ret);
-		}
+		break;
+	case SUBSYS_RAMDUMP_NOTIFICATION:
+		if (!(smem_ramdump_dev && notifdata->enable_ramdump))
+			break;
+		SMEM_DBG("%s: saving ramdump\n", __func__);
+		/*
+		 * XPU protection does not currently allow the
+		 * auxiliary memory regions to be dumped.  If this
+		 * changes, then num_smem_areas + 1 should be passed
+		 * into do_elf_ramdump() to dump all regions.
+		 */
+		ret = do_elf_ramdump(smem_ramdump_dev,
+				smem_ramdump_segments, 1);
+		if (ret < 0)
+			LOG_ERR("%s: unable to dump smem %d\n", __func__, ret);
+		break;
+	default:
+		break;
 	}
 
 	return NOTIFY_DONE;
