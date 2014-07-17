b2ede7f1877d7b2ff4174622ddc5ca6b117ce74c
Ankit Gupta <ankgupta@codeaurora.org>
2014-07-17 12:07:16 -0600

    i2c-msm-v2: improve the bus recovery procedure in i2c driver
    Bus recovery procedure may help when bus goes in a bad state.
    Recovery procedure may not get success in single iteration. This
    patch allows the bus recover to retry until success.
    
    Change-Id: I035843784cc82a09aed686d8457ec1eaef7e6a53
    Signed-off-by: Ankit Gupta <ankgupta@codeaurora.org>

--- drivers/i2c/busses/i2c-msm-v2.h
+++ drivers/i2c/busses/i2c-msm-v2.h
@@ -232,6 +232,7 @@ enum msm_i2c_power_state {
 #define I2C_MSM_TIMEOUT_SAFTY_COEF      (10)
 #define I2C_MSM_TIMEOUT_MIN_USEC        (500000)
 #define I2C_MSM_HS_ADDR                 (0x0f)
+#define I2C_QUP_MAX_BUS_RECOVERY_RETRY  (10)
 
 /* QUP v2 tags */
 #define QUP_TAG2_DATA_WRITE        (0x82ULL)
