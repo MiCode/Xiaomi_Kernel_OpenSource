b7d786e456a6dad09680457f7a615b3a60dc0830
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-07-21 08:24:04 -0700

    sched: remove sysctl control for HMP and power-aware task placement
    There is no real need to control HMP and power-aware task placement at
    runtime after kernel has booted. Boot-time control should be
    sufficient. Not allowing for runtime (sysctl) support simplifies the
    code quite a bit.
    
    Also rename sysctl_sched_enable_hmp_task_placement to be shorter.
    
    Change-Id: I60cae51a173c6f73b79cbf90c50ddd41a27604aa
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1073,6 +1073,36 @@ void check_preempt_curr(struct rq *rq, struct task_struct *p, int flags)
 		rq->skip_clock_update = 1;
 }
 
+#ifdef CONFIG_SCHED_HMP
+
+static int __init set_sched_enable_hmp(char *str)
+{
+	int enable_hmp = 0;
+
+	get_option(&str, &enable_hmp);
+
+	sched_enable_hmp = !!enable_hmp;
+
+	return 0;
+}
+
+early_param("sched_enable_hmp", set_sched_enable_hmp);
+
+static int __init set_sched_enable_power_aware(char *str)
+{
+	int enable_power_aware = 0;
+
+	get_option(&str, &enable_power_aware);
+
+	sched_enable_power_aware = !!enable_power_aware;
+
+	return 0;
+}
+
+early_param("sched_enable_power_aware", set_sched_enable_power_aware);
+
+#endif	/* CONFIG_SCHED_HMP */
+
 #if defined(CONFIG_SCHED_FREQ_INPUT) || defined(CONFIG_SCHED_HMP)
 
 /* Window size (in ns) */
@@ -1133,7 +1163,7 @@ int rq_freq_margin(struct rq *rq)
 	int margin;
 	u64 demand;
 
-	if (!sysctl_sched_enable_hmp_task_placement)
+	if (!sched_enable_hmp)
 		return INT_MAX;
 
 	demand = scale_load_to_cpu(rq->prev_runnable_sum, rq->cpu);
@@ -1391,7 +1421,7 @@ static void init_cpu_efficiency(void)
 	int i, efficiency;
 	unsigned int max = 0, min = UINT_MAX;
 
-	if (!sysctl_sched_enable_hmp_task_placement)
+	if (!sched_enable_hmp)
 		return;
 
 	for_each_possible_cpu(i) {
@@ -1434,7 +1464,7 @@ static inline void set_window_start(struct rq *rq)
 	int cpu = cpu_of(rq);
 	struct rq *sync_rq = cpu_rq(sync_cpu);
 
-	if (rq->window_start || !sysctl_sched_enable_hmp_task_placement)
+	if (rq->window_start || !sched_enable_hmp)
 		return;
 
 	if (cpu == sync_cpu) {
@@ -1718,7 +1748,7 @@ static int register_sched_callback(void)
 {
 	int ret;
 
-	if (!sysctl_sched_enable_hmp_task_placement)
+	if (!sched_enable_hmp)
 		return 0;
 
 	ret = cpufreq_register_notifier(&notifier_policy_block,
@@ -1882,8 +1912,7 @@ void set_task_cpu(struct task_struct *p, unsigned int new_cpu)
 		p->se.nr_migrations++;
 		perf_sw_event(PERF_COUNT_SW_CPU_MIGRATIONS, 1, NULL, 0);
 
-		if (sysctl_sched_enable_hmp_task_placement &&
-		    (p->on_rq || p->state == TASK_WAKING))
+		if (sched_enable_hmp && (p->on_rq || p->state == TASK_WAKING))
 			fixup_busy_time(p, new_cpu);
 	}
 
@@ -3322,7 +3351,7 @@ void sched_exec(void)
 	unsigned long flags;
 	int dest_cpu;
 
-	if (sysctl_sched_enable_hmp_task_placement)
+	if (sched_enable_hmp)
 		return;
 
 	raw_spin_lock_irqsave(&p->pi_lock, flags);
