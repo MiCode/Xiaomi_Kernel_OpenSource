84a7a5722bfb038b94f3ed37ff182269f42fa84c
Shiju Mathew <shijum@codeaurora.org>
2014-07-23 19:36:14 -0400

    msm: thermal: Add ftrace logs in KTM
    Add ftrace logs in KTM. This would log core offline
    and online as well as core frequency updates in
    ftrace.
    
    Change-Id: I02b6c047b2cbb9c042ee6a64d9fc849087dcb635
    Signed-off-by: Shiju Mathew <shijum@codeaurora.org>

--- include/trace/trace_thermal.h
+++ include/trace/trace_thermal.h
@@ -18,7 +18,111 @@
 #define _TRACE_THERMAL_H
 
 #include <linux/tracepoint.h>
+#ifdef TRACE_MSM_THERMAL
+DECLARE_EVENT_CLASS(msm_thermal_post_core_ctl,
 
+	TP_PROTO(unsigned int cpu, unsigned int online),
+
+	TP_ARGS(cpu, online),
+
+	TP_STRUCT__entry(
+		__field(unsigned int, cpu)
+		__field(unsigned int, online)
+	),
+
+	TP_fast_assign(
+		__entry->cpu = cpu;
+		__entry->online = online;
+	),
+
+	TP_printk("device=cpu%u online=%u",
+		 __entry->cpu,  __entry->online)
+);
+DECLARE_EVENT_CLASS(msm_thermal_pre_core_ctl,
+
+	TP_PROTO(unsigned int cpu),
+
+	TP_ARGS(cpu),
+
+	TP_STRUCT__entry(
+		__field(unsigned int, cpu)
+	),
+
+	TP_fast_assign(
+		__entry->cpu = cpu;
+	),
+
+	TP_printk("device=cpu%u", __entry->cpu)
+);
+
+DEFINE_EVENT(msm_thermal_pre_core_ctl, thermal_pre_core_offline,
+
+	TP_PROTO(unsigned int cpu),
+
+	TP_ARGS(cpu)
+);
+
+DEFINE_EVENT(msm_thermal_post_core_ctl, thermal_post_core_offline,
+
+	TP_PROTO(unsigned int cpu, unsigned int online),
+
+	TP_ARGS(cpu, online)
+);
+
+DEFINE_EVENT(msm_thermal_pre_core_ctl, thermal_pre_core_online,
+
+	TP_PROTO(unsigned int cpu),
+
+	TP_ARGS(cpu)
+);
+
+DEFINE_EVENT(msm_thermal_post_core_ctl, thermal_post_core_online,
+
+	TP_PROTO(unsigned int cpu, unsigned int online),
+
+	TP_ARGS(cpu, online)
+);
+
+DECLARE_EVENT_CLASS(msm_thermal_freq_mit,
+
+	TP_PROTO(unsigned int cpu, unsigned int max_freq,
+		unsigned int min_freq),
+
+	TP_ARGS(cpu, max_freq, min_freq),
+
+	TP_STRUCT__entry(
+		__field(unsigned int, cpu)
+		__field(unsigned int, max_freq)
+		__field(unsigned int, min_freq)
+	),
+
+	TP_fast_assign(
+		__entry->cpu = cpu;
+		__entry->max_freq = max_freq;
+		__entry->min_freq = min_freq;
+	),
+
+	TP_printk("device=cpu%u max_frequency=%u min_frequency=%u",
+			 __entry->cpu, __entry->max_freq,
+			 __entry->min_freq)
+);
+
+DEFINE_EVENT(msm_thermal_freq_mit, thermal_pre_frequency_mit,
+
+	TP_PROTO(unsigned int cpu, unsigned int max_freq,
+		unsigned int min_freq),
+
+	TP_ARGS(cpu, max_freq, min_freq)
+);
+
+DEFINE_EVENT(msm_thermal_freq_mit, thermal_post_frequency_mit,
+
+	TP_PROTO(unsigned int cpu, unsigned int max_freq,
+		unsigned int min_freq),
+
+	TP_ARGS(cpu, max_freq, min_freq)
+);
+#else
 DECLARE_EVENT_CLASS(tsens,
 
 	TP_PROTO(unsigned long temp, unsigned int sensor),
@@ -60,7 +164,7 @@ DEFINE_EVENT(tsens, tsens_threshold_clear,
 	TP_ARGS(temp, sensor)
 );
 #endif
-
+#endif
 #undef TRACE_INCLUDE_PATH
 #undef TRACE_INCLUDE_FILE
 #define TRACE_INCLUDE_PATH .
