0750837c901126dba63faf867c2915ac8f0cf909
Aravind Venkateswaran <aravindh@codeaurora.org>
2014-07-23 15:54:53 -0700

    msm: mdss: reset cont splash screen flag after notifying panel intf
    Starting with commit a702d63198e3524740be22b66ce1933064174019, the
    continuous splash screen feature flag for the secondary panel was also
    disabled along with that of the primary panel. However, this should only
    be done after notifying the interface drivers about the transition from
    the splash screen to the boot animation. This is needed as the DSI panel
    driver relies on this flag to avoid unnecessarily configuring the DSI
    lanes in to the ultra low power state during this transition.
    
    Change-Id: I59d312248c579d27913f23dc23e9c91a99549725
    Signed-off-by: Aravind Venkateswaran <aravindh@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_ctl.c
+++ drivers/video/msm/mdss/mdss_mdp_ctl.c
@@ -1478,10 +1478,6 @@ int mdss_mdp_wb_mixer_destroy(struct mdss_mdp_mixer *mixer)
 
 int mdss_mdp_ctl_splash_finish(struct mdss_mdp_ctl *ctl, bool handoff)
 {
-	struct mdss_mdp_ctl *sctl = mdss_mdp_get_split_ctl(ctl);
-	if (sctl)
-		sctl->panel_data->panel_info.cont_splash_enabled = 0;
-
 	switch (ctl->panel_data->panel_info.type) {
 	case MIPI_VIDEO_PANEL:
 	case EDP_PANEL:
