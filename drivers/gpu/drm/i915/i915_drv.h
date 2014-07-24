76d6a145f6f5d92481388f04d83b316dd83638c2
Oscar Mateo <oscar.mateo@intel.com>
2014-07-24 17:04:11 +0100

    FROM_UPSTREAM [VPG]: drm/i915/bdw: Macro for LRCs and module option for Execlists
    GEN8 brings an expansion of the HW contexts: "Logical Ring Contexts".
    These expanded contexts enable a number of new abilities, especially
    "Execlists".
    
    The macro is defined to off until we have things in place to hope to
    work.
    
    v2: Rename "advanced contexts" to the more correct "logical ring
    contexts".
    
    v3: Add a module parameter to enable execlists. Execlist are relatively
    new, and so it'd be wise to be able to switch back to ring submission
    to debug subtle problems that will inevitably arise.
    
    v4: Add an intel_enable_execlists function.
    
    v5: Sanitize early, as suggested by Daniel. Remove lrc_enabled.
    
    Signed-off-by: Ben Widawsky <ben@bwidawsk.net> (v1)
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com> (v3)
    Signed-off-by: Oscar Mateo <oscar.mateo@intel.com> (v2, v4 & v5)
    Reviewed-by: Damien Lespiau <damien.lespiau@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    (cherry picked from commit 127f100369a1f302904335950387d566680eb275)
    
    Change-Id: Ice2d36ce2022c2aed6a65ae3aad23ba38339e56e
    Upstream-repo: git://anongit.freedesktop.org/drm-intel
    Upstream-commit: 127f100369a1f302904335950387d566680eb275
    Upstream-as-of-tag: drm-intel-next-2014-08-22
    For: GMIN-2332
    For: VIZ-2020
    Signed-off-by: Dave Gordon <david.s.gordon@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -2180,6 +2180,7 @@ struct drm_i915_cmd_table {
 #define I915_NEED_GFX_HWS(dev)	(INTEL_INFO(dev)->need_gfx_hws)
 
 #define HAS_HW_CONTEXTS(dev)	(INTEL_INFO(dev)->gen >= 6)
+#define HAS_LOGICAL_RING_CONTEXTS(dev)	0
 #define HAS_ALIASING_PPGTT(dev)	(INTEL_INFO(dev)->gen >= 6 && \
 				 (!IS_VALLEYVIEW(dev) || IS_CHERRYVIEW(dev)))
 #define HAS_PPGTT(dev)		(INTEL_INFO(dev)->gen >= 7 \
@@ -2270,6 +2271,7 @@ struct i915_params {
 	int enable_rc6;
 	int enable_fbc;
 	int enable_ppgtt;
+	int enable_execlists;
 	int enable_psr;
 	unsigned int preliminary_hw_support;
 	int disable_power_well;
