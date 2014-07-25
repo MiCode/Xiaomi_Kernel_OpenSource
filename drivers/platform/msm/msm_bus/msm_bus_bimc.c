b36672a073f411fd5539639a5747acc1bf7af82f
Girish Mahadevan <girishm@codeaurora.org>
2014-07-25 15:32:40 -0600

    msm: msm_bus: Fix inadvertent stomp on BKE_EN register
    While setting the QoS mode of fixed mode masters bad masking
    operations is causing accidental stomp on the upper nibble of the
    BKE_EN register.
    
    Change-Id: I5675e1dbc6fd5a01faac49c903e12a9c95e32254
    Signed-off-by: Girish Mahadevan <girishm@codeaurora.org>

--- drivers/platform/msm/msm_bus/msm_bus_bimc.c
+++ drivers/platform/msm/msm_bus/msm_bus_bimc.c
@@ -1122,7 +1122,7 @@ static void msm_bus_bimc_set_qos_mode(void __iomem *base,
 	switch (qmode_sel) {
 	case BIMC_QOS_MODE_FIXED:
 		reg_val = readl_relaxed(M_BKE_EN_ADDR(base,
-			mas_index)) & M_BKE_EN_RMSK;
+			mas_index));
 		writel_relaxed((reg_val & (~M_BKE_EN_EN_BMSK)),
 			M_BKE_EN_ADDR(base, mas_index));
 		/* Ensure that the book-keeping register writes
@@ -1136,7 +1136,7 @@ static void msm_bus_bimc_set_qos_mode(void __iomem *base,
 
 	case BIMC_QOS_MODE_BYPASS:
 		reg_val = readl_relaxed(M_BKE_EN_ADDR(base,
-			mas_index)) & M_BKE_EN_RMSK;
+			mas_index));
 		writel_relaxed((reg_val & (~M_BKE_EN_EN_BMSK)),
 			M_BKE_EN_ADDR(base, mas_index));
 		/* Ensure that the book-keeping register writes
@@ -1152,7 +1152,7 @@ static void msm_bus_bimc_set_qos_mode(void __iomem *base,
 	case BIMC_QOS_MODE_LIMITER:
 		set_qos_mode(base, mas_index, 0, 0, 0);
 		reg_val = readl_relaxed(M_BKE_EN_ADDR(base,
-			mas_index)) & M_BKE_EN_RMSK;
+			mas_index));
 		val = 1 << M_BKE_EN_EN_SHFT;
 		/* Ensure that the book-keeping register writes
 		 * go through before setting QoS mode.
