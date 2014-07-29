3ac395c5ec7109e598337dd67b34b5f35c6f1570
Aravind Venkateswaran <aravindh@codeaurora.org>
2014-07-28 23:24:46 -0700

    msm: mdss: add support for different panel power states
    Currently a panel either be in a powered on state or a powered off
    state. However, it is possible for panels to be configured in an
    intermediate low-power state where the panel is always kept on. In
    order to support such use-cases, it is necessary to add the concept
    of panel power states in MDSS so that each software layer can
    independently maintain it's state and manage transitions between them.
    This change just refactors the driver with only two existing power
    states - panel on and panel off.
    
    Change-Id: I49d1fda506761546c5e22d9106c5177e75053a81
    Signed-off-by: Aravind Venkateswaran <aravindh@codeaurora.org>

--- drivers/video/msm/mdss/dsi_status_6g.c
+++ drivers/video/msm/mdss/dsi_status_6g.c
@@ -65,7 +65,7 @@ void mdss_check_dsi_ctrl_status(struct work_struct *work, uint32_t interval)
 		return;
 	}
 
-	if (!ctl->power_on) {
+	if (ctl->power_state == MDSS_PANEL_POWER_OFF) {
 		schedule_delayed_work(&pstatus_data->check_status,
 			msecs_to_jiffies(interval));
 		pr_err("%s: ctl not powered on\n", __func__);
@@ -111,7 +111,7 @@ void mdss_check_dsi_ctrl_status(struct work_struct *work, uint32_t interval)
 		mutex_unlock(ctl->shared_lock);
 	mutex_unlock(&ctrl_pdata->mutex);
 
-	if ((pstatus_data->mfd->panel_power_on)) {
+	if ((pstatus_data->mfd->panel_power_state == MDSS_PANEL_POWER_ON)) {
 		if (ret > 0) {
 			schedule_delayed_work(&pstatus_data->check_status,
 				msecs_to_jiffies(interval));
