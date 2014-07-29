251ac4b81c72c0a10507388b0870eef08f40a977
Jussi Kivilinna <jussi.kivilinna@iki.fi>
2014-07-29 17:14:14 +0100

    ARM: 8119/1: crypto: sha1: add ARM NEON implementation
    This patch adds ARM NEON assembly implementation of SHA-1 algorithm.
    
    tcrypt benchmark results on Cortex-A8, sha1-arm-asm vs sha1-neon-asm:
    
    block-size      bytes/update    old-vs-new
    16              16              1.04x
    64              16              1.02x
    64              64              1.05x
    256             16              1.03x
    256             64              1.04x
    256             256             1.30x
    1024            16              1.03x
    1024            256             1.36x
    1024            1024            1.52x
    2048            16              1.03x
    2048            256             1.39x
    2048            1024            1.55x
    2048            2048            1.59x
    4096            16              1.03x
    4096            256             1.40x
    4096            1024            1.57x
    4096            4096            1.62x
    8192            16              1.03x
    8192            256             1.40x
    8192            1024            1.58x
    8192            4096            1.63x
    8192            8192            1.63x
    
    Acked-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Tested-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Jussi Kivilinna <jussi.kivilinna@iki.fi>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Change-Id: I351ceac3c3c6d08caa6bb0fd1431211a07404198
    Reviewed-on: http://gerrit.mot.com/787013
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>

--- arch/arm/crypto/sha1_glue.c
+++ arch/arm/crypto/sha1_glue.c
@@ -23,6 +23,7 @@
 #include <linux/types.h>
 #include <crypto/sha.h>
 #include <asm/byteorder.h>
+#include <asm/crypto/sha1.h>
 
 struct SHA1_CTX {
 	uint32_t h0,h1,h2,h3,h4;
@@ -71,8 +72,8 @@ static int __sha1_update(struct SHA1_CTX *sctx, const u8 *data,
 }
 
 
-static int sha1_update(struct shash_desc *desc, const u8 *data,
-			     unsigned int len)
+int sha1_update_arm(struct shash_desc *desc, const u8 *data,
+		    unsigned int len)
 {
 	struct SHA1_CTX *sctx = shash_desc_ctx(desc);
 	unsigned int partial = sctx->count % SHA1_BLOCK_SIZE;
@@ -87,6 +88,7 @@ static int sha1_update(struct shash_desc *desc, const u8 *data,
 	res = __sha1_update(sctx, data, len, partial);
 	return res;
 }
+EXPORT_SYMBOL_GPL(sha1_update_arm);
 
 
 /* Add padding and return the message digest. */
@@ -141,7 +143,7 @@ static int sha1_import(struct shash_desc *desc, const void *in)
 static struct shash_alg alg = {
 	.digestsize	=	SHA1_DIGEST_SIZE,
 	.init		=	sha1_init,
-	.update		=	sha1_update,
+	.update		=	sha1_update_arm,
 	.final		=	sha1_final,
 	.export		=	sha1_export,
 	.import		=	sha1_import,
