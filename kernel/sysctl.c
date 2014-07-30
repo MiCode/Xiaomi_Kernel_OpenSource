5e8f14fbbc2c792330ad6cc544260c7c6ee7691c
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-07-30 03:27:13 -0700

    sched: window-stats: Allow acct_wait_time to be tuned
    Add sysctl interface to tune sched_acct_wait_time variable at runtime
    
    Change-Id: I38339cdb388a507019e429709a7c28e80b5b3585
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -312,6 +312,13 @@ static struct ctl_table kern_table[] = {
 #endif
 #if defined(CONFIG_SCHED_FREQ_INPUT) || defined(CONFIG_SCHED_HMP)
 	{
+		.procname       = "sched_account_wait_time",
+		.data           = &sysctl_sched_account_wait_time,
+		.maxlen         = sizeof(unsigned int),
+		.mode           = 0644,
+		.proc_handler   = sched_acct_wait_time_update_handler,
+	},
+	{
 		.procname       = "sched_window_stats_policy",
 		.data           = &sysctl_sched_window_stats_policy,
 		.maxlen         = sizeof(unsigned int),
