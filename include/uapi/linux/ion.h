c75946685baebd0b9c6b2e546e143c16589effd3
Laura Abbott <lauraa@codeaurora.org>
2014-08-04 14:20:26 -0700

    ion: Export ion headers to userspace
    Ion headers need to go to userspace. Export them via the staging tree
    
    Change-Id: I9b622efaae3d0ed764ce064da228112c363c066d
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- include/uapi/linux/ion.h
+++ include/uapi/linux/ion.h
@@ -1,1 +1,6 @@
-../../../drivers/staging/android/uapi/ion.h
+#ifndef __UAPI_LINUX_ION_H__
+#define __UAPI_LINUX_ION_H__
+
+#include <linux/staging/android/uapi/ion.h>
+
+#endif /* __UAPI_LINUX_ION_H__ */
---
+++ include/uapi/linux/msm_ion.h
@@ -0,0 +1,6 @@
+#ifndef __UAPI_LINUX_MSM_ION_H__
+#define __UAPI_LINUX_MSM_ION_H__
+
+#include <linux/staging/android/uapi/msm_ion.h>
+
+#endif /* __UAPI_LINUX_MSM_ION_H__ */
