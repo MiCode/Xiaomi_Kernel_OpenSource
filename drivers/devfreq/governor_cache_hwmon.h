413a95377802dcea598bbcdb22f06ed4f5c06996
Kumar Gala <galak@codeaurora.org>
2014-08-04 13:41:54 -0500

    PM / devfreq: Add cache HW monitor governor
    This is a snapshot of the HW monitor governor driver as of msm-3.10
    commit:
    
    acdce027751d5a7488b283f0ce3111f873a5816d (Merge "defconfig: arm64:
    Enable ONESHOT_SYNC for msm8994")
    
    Signed-off-by: Kumar Gala <galak@codeaurora.org>

---
+++ drivers/devfreq/governor_cache_hwmon.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2014, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _GOVERNOR_CACHE_HWMON_H
+#define _GOVERNOR_CACHE_HWMON_H
+
+#include <linux/kernel.h>
+#include <linux/devfreq.h>
+
+struct mrps_stats {
+	unsigned long high;
+	unsigned long med;
+	unsigned long low;
+	unsigned int busy_percent;
+};
+
+struct cache_hwmon {
+	int (*start_hwmon)(struct devfreq *df, struct mrps_stats *mrps);
+	void (*stop_hwmon)(struct devfreq *df);
+	bool (*is_valid_irq)(struct devfreq *df);
+	unsigned long (*meas_mrps_and_set_irq)(struct devfreq *df,
+					unsigned int tol, unsigned int us,
+					struct mrps_stats *mrps);
+	int irq;
+};
+
+#ifdef CONFIG_DEVFREQ_GOV_MSM_CACHE_HWMON
+int register_cache_hwmon(struct cache_hwmon *hwmon);
+#else
+static inline int register_cache_hwmon(struct cache_hwmon *hwmon)
+{
+	return 0;
+}
+#endif
+
+#endif /* _GOVERNOR_CACHE_HWMON_H */
