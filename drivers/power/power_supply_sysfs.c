77a90a4c5b36a2589e742f761953680718263e87
Ashay Jaiswal <ashayj@codeaurora.org>
2014-08-07 14:54:44 +0530

    power: power_supply: Add TAPER charge type
    Add definitions for TAPER charger type.
    This is the charging type for constant voltage(CV)
    charging mode.
    
    Change-Id: Icc7a56f149053ef1edfc7afda037658160eeff22
    Signed-off-by: Ashay Jaiswal <ashayj@codeaurora.org>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -52,7 +52,7 @@ static ssize_t power_supply_show_property(struct device *dev,
 		"Unknown", "Charging", "Discharging", "Not charging", "Full"
 	};
 	static char *charge_type[] = {
-		"Unknown", "N/A", "Trickle", "Fast"
+		"Unknown", "N/A", "Trickle", "Fast", "Taper"
 	};
 	static char *health_text[] = {
 		"Unknown", "Good", "Overheat", "Warm", "Dead", "Over voltage",
