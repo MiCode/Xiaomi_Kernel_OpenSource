bb5095e41e5bdceaae68eb9c8181d64baeb5142a
Terence Hampson <thampson@codeaurora.org>
2014-08-08 14:53:34 -0400

    mdss: Display related changes commit below
    While merging to 3.14 kernel this is only display
    related changes for commit below
    Change ID: Ie0d5b104882d1534fae262af85e99cc09a56ab04
    
    	msm: msm_bus: Move bus scaling to platform drivers
    
    Change-Id: Id726a65b5641b57c0edd3ba33f2e5b3f68aff643
    Signed-off-by: Terence Hampson <thampson@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_mdp.c
+++ drivers/video/fbdev/msm/mdss_mdp.c
@@ -43,8 +43,8 @@
 
 #include <mach/board.h>
 #include <mach/hardware.h>
-#include <mach/msm_bus.h>
-#include <mach/msm_bus_board.h>
+#include <linux/msm_bus.h>
+#include <linux/msm_bus_board.h>
 #include <linux/qcom_iommu.h>
 #include <linux/msm_iommu_domains.h>
 #include <mach/memory.h>
