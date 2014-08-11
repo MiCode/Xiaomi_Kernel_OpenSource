0a9953650a29082921aeffd342fc0145579d23fa
Ingrid Gallardo <ingridg@codeaurora.org>
2014-08-11 13:41:35 -0700

    msm: mdss: fix rotator failures when pixel format changes
    Current code fails the SMP allocation in the rotator session
    when the pixel format during the session changes from YUV to
    RGB. Fix these failures by releasing the SMP allocation
    if pixel format changes.
    
    Change-Id: Ic051ad4c48be6d4998d392fa74f7b3fb2c131980
    Signed-off-by: Ingrid Gallardo <ingridg@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_rotator.c
+++ drivers/video/msm/mdss/mdss_mdp_rotator.c
@@ -179,6 +179,7 @@ static int mdss_mdp_rotator_pipe_dequeue(struct mdss_mdp_rotator_session *rot)
 					       head);
 
 			rc = mdss_mdp_rotator_busy_wait(tmp);
+			mdss_mdp_smp_release(tmp->pipe);
 			list_del(&tmp->head);
 			if (rc) {
 				pr_err("no pipe attached to session=%d\n",
@@ -440,6 +441,7 @@ int mdss_mdp_rotator_setup(struct msm_fb_data_type *mfd,
 	struct mdss_mdp_rotator_session *rot = NULL;
 	struct mdss_mdp_format_params *fmt;
 	u32 bwc_enabled;
+	bool format_changed = false;
 	int ret = 0;
 
 	mutex_lock(&rotator_lock);
@@ -479,6 +481,9 @@ int mdss_mdp_rotator_setup(struct msm_fb_data_type *mfd,
 			flush_work(&rot->commit_work);
 			mutex_lock(&rotator_lock);
 		}
+
+		if (rot->format != fmt->format)
+			format_changed = true;
 
 	} else {
 		pr_err("invalid rotator session id=%x\n", req->id);
@@ -598,6 +603,12 @@ int mdss_mdp_rotator_setup(struct msm_fb_data_type *mfd,
 	}
 
 	rot->params_changed++;
+
+	/* If the format changed, release the smp alloc */
+	if (format_changed && rot->pipe) {
+		mdss_mdp_rotator_busy_wait(rot);
+		mdss_mdp_smp_release(rot->pipe);
+	}
 
 	ret = __mdss_mdp_rotator_pipe_reserve(rot);
 	if (!ret && rot->next)
