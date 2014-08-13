cf0685cff52a6caf85bfe5aeb57c7a6378c843a0
Laura Abbott <lauraa@codeaurora.org>
2014-08-13 14:52:53 -0700

    arm: Add weak function definition for random pool intialization
    The random pool relies on devices and other items in the system
    to add entropy to the pool. Most of these devices may not be
    added until later in the bootup process. This leaves a large
    period of time where the random pool may not actually give
    random numbers. Add a weak function for devices to override
    with their own function to setup the random pool.
    
    Change-Id: I0de63420b11f1dd363ccd0ef6ac0fa4a617a1152
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- arch/arm/kernel/setup.c
+++ arch/arm/kernel/setup.c
@@ -893,6 +893,8 @@ void __init hyp_mode_check(void)
 #endif
 }
 
+void __init __weak init_random_pool(void) { }
+
 void __init setup_arch(char **cmdline_p)
 {
 	const struct machine_desc *mdesc;
@@ -965,6 +967,8 @@ void __init setup_arch(char **cmdline_p)
 
 	if (mdesc->init_early)
 		mdesc->init_early();
+
+	init_random_pool();
 }
 
 
