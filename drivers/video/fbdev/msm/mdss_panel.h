f9c4def53dc9af1be77a0122f88f3a29afba0d96
Kuogee Hsieh <khsieh@codeaurora.org>
2014-08-13 10:03:15 -0700

    msm: mdss: move dcs-cmd-by-left out of partial update
    dcs-cmd-by-left flag can be set without partial update being
    enabled. Therefore remove partial update checking during
    parsing dcs-cmd-by-left.
    
    CRs-Fixed: 698733
    Change-Id: I3e81ecc95e80a2d2bc970d8d9350cfc7736d1a29
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_panel.h
+++ drivers/video/fbdev/msm/mdss_panel.h
@@ -378,7 +378,7 @@ struct mdss_panel_info {
 
 	u32 cont_splash_enabled;
 	u32 partial_update_enabled;
-	u32 partial_update_dcs_cmd_by_left;
+	u32 dcs_cmd_by_left;
 	u32 partial_update_roi_merge;
 	struct ion_handle *splash_ihdl;
 	int panel_power_state;
