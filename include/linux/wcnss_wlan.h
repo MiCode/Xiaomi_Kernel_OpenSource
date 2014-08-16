89beb6154f8a31cba1b13738b1b29e9952a07890
Hardik Kantilal Patel <hkpatel@codeaurora.org>
2014-08-16 11:28:44 +0530

    wcnss: Add API to get monotonic boot time
    Add CNSS platform driver API to expose monotonic boot time so that
    WLAN host driver can get the timestamp.
    
    Change-Id: I722fa42600bf910254ac01a7305bbbaa35dfa83e
    CRs-Fixed: 706813
    Signed-off-by: Hardik Kantilal Patel <hkpatel@codeaurora.org>

--- include/linux/wcnss_wlan.h
+++ include/linux/wcnss_wlan.h
@@ -43,6 +43,7 @@ enum {
 #define HAVE_WCNSS_RX_BUFF_COUNT 1
 #define WLAN_MAC_ADDR_SIZE (6)
 
+void wcnss_get_monotonic_boottime(struct timespec *ts);
 struct device *wcnss_wlan_get_device(void);
 struct resource *wcnss_wlan_get_memory_map(struct device *dev);
 int wcnss_wlan_get_dxe_tx_irq(struct device *dev);
