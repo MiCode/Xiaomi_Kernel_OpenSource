18514c39554c80633d1212b541e6987afac641ee
Gaurav K Singh <gaurav.k.singh@intel.com>
2014-08-19 00:30:34 +0530

    FOR_UPSTREAM [VPG]: drm/i915: Fix the issue for Long packet send to DSI Controller
    Fix the issue for Long packet send to DSI Controller. Make sure
    that the generic data fifo is empty before sending long packet.
    currently we are checking for fifo full. When the fifo full bit
    is cleared by hardware there will be only space for 4 more bytes
    to be pushed and hence it may not be enough to push long packet.
    
    Issue: GMINL-1532
    Change-Id: I37f4355b52167e38c6362afc84137ca8d1ddd539
    Signed-off-by: Gaurav K Singh <gaurav.k.singh@intel.com>
    Signed-off-by: Yogesh Mohan Marimuthu <yogesh.mohan.marimuthu@intel.com>

--- drivers/gpu/drm/i915/intel_dsi_cmd.c
+++ drivers/gpu/drm/i915/intel_dsi_cmd.c
@@ -172,7 +172,7 @@ static int dsi_vc_send_long(struct intel_dsi *intel_dsi, int channel,
 	struct drm_device *dev = encoder->dev;
 	struct drm_i915_private *dev_priv = dev->dev_private;
 	enum pipe pipe = intel_dsi->port;
-	u32 data_reg;
+	u32 data_reg, ctrl_reg, ctrl;
 	int i, j, n;
 	u32 mask;
 
@@ -181,14 +181,16 @@ static int dsi_vc_send_long(struct intel_dsi *intel_dsi, int channel,
 
 	if (intel_dsi->hs) {
 		data_reg = MIPI_HS_GEN_DATA(pipe);
-		mask = HS_DATA_FIFO_FULL;
+		ctrl_reg = MIPI_HS_GEN_CTRL(pipe);
+		mask = HS_CTRL_FIFO_EMPTY | HS_DATA_FIFO_EMPTY;
 	} else {
 		data_reg = MIPI_LP_GEN_DATA(pipe);
-		mask = LP_DATA_FIFO_FULL;
+		ctrl_reg = MIPI_LP_GEN_CTRL(pipe);
+		mask = LP_CTRL_FIFO_EMPTY | LP_DATA_FIFO_EMPTY;
 	}
 
-	if (wait_for((I915_READ(MIPI_GEN_FIFO_STAT(pipe)) & mask) == 0, 50))
-		DRM_ERROR("Timeout waiting for HS/LP DATA FIFO !full\n");
+	if (wait_for((I915_READ(MIPI_GEN_FIFO_STAT(pipe)) & mask) == mask, 50))
+		DRM_ERROR("Timeout waiting for HS/LP DATA FIFO to be empty\n");
 
 	for (i = 0; i < len; i += n) {
 		u32 val = 0;
@@ -201,8 +203,13 @@ static int dsi_vc_send_long(struct intel_dsi *intel_dsi, int channel,
 		/* XXX: check for data fifo full, once that is set, write 4
 		 * dwords, then wait for not set, then continue. */
 	}
+	ctrl = len << LONG_PACKET_WORD_COUNT_SHIFT;
+	ctrl |= channel << VIRTUAL_CHANNEL_SHIFT;
+	ctrl |= data_type << DATA_TYPE_SHIFT;
 
-	return dsi_vc_send_short(intel_dsi, channel, data_type, len);
+	I915_WRITE(ctrl_reg, ctrl);
+	return 0;
+
 }
 
 static int dsi_vc_write_common(struct intel_dsi *intel_dsi,
