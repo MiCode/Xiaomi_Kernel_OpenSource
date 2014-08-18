6f57b178d89bce673de9b545599878702adfbe4c
Krishnankutty Kolathappilly <kkolat@codeaurora.org>
2014-08-17 18:13:51 -0700

    ASoC: msm: qdsp6v2: Add support for DS2 Dolby Audio Processing
    Add support for Dolby DS2 driver for Dolby audio processing.Dolby
    ds2 driver uses ioctl interface to transfer parameters to and from
    userspace. The parameters are send on active DAP instance on DSP.
    The driver supports set parameter, get parameter and get visualizer
    interface.
    
    Change-Id: Ica88bde6c5484eb147a48c561eea821e026c03bc
    Signed-off-by: Krishnankutty Kolathappilly <kkolat@codeaurora.org>

---
+++ include/uapi/sound/devdep_params.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _DEV_DEP_H
+#define _DEV_DEP_H
+
+
+struct dolby_param_data {
+	int32_t version;
+	int32_t device_id;
+	int32_t be_id;
+	int32_t param_id;
+	int32_t length;
+	int32_t __user *data;
+};
+
+#define SNDRV_DEVDEP_DAP_IOCTL_SET_PARAM\
+		_IOWR('U', 0x10, struct dolby_param_data)
+#define SNDRV_DEVDEP_DAP_IOCTL_GET_PARAM\
+		_IOR('U', 0x11, struct dolby_param_data)
+#define SNDRV_DEVDEP_DAP_IOCTL_DAP_COMMAND\
+		_IOWR('U', 0x13, struct dolby_param_data)
+#define SNDRV_DEVDEP_DAP_IOCTL_DAP_LICENSE\
+		_IOWR('U', 0x14, void *)
+#define SNDRV_DEVDEP_DAP_IOCTL_GET_VISUALIZER\
+		_IOR('U', 0x15, struct dolby_param_data)
+
+#endif
