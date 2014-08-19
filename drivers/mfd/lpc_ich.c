7819e1fd8bc957498d7d68229d8fb2c89bd23493
Francois-Nicolas Muller <francois-nicolas.muller@intel.com>
2014-08-19 11:49:38 +0200

    Add PCI id for Cherry Trail SoC in lpc_ich driver
    Change-Id: If4d2aa914c1753ec905074e376737da4b9407b49
    Signed-off-by: Francois-Nicolas Muller <francois-nicolas.muller@intel.com>

--- drivers/mfd/lpc_ich.c
+++ drivers/mfd/lpc_ich.c
@@ -217,6 +217,7 @@ enum lpc_chipsets {
 	LPC_BAYTRAIL,   /* Bay Trail SoC */
 	LPC_COLETO,	/* Coleto Creek */
 	LPC_WPT_LP,	/* Wildcat Point-LP */
+	LPC_CHERRYTRAIL,/* Cherry Trail SoC */
 };
 
 static struct lpc_ich_info lpc_chipset_info[] = {
@@ -517,6 +518,10 @@ static struct lpc_ich_info lpc_chipset_info[] = {
 		.name = "Wildcat Point_LP",
 		.iTCO_version = 2,
 	},
+	[LPC_CHERRYTRAIL] = {
+		.name = "Cherry Trail SoC",
+		.iTCO_version = 3,
+	},
 };
 
 /*
@@ -743,6 +748,7 @@ static const struct pci_device_id lpc_ich_ids[] = {
 	{ PCI_VDEVICE(INTEL, 0x9cc6), LPC_WPT_LP},
 	{ PCI_VDEVICE(INTEL, 0x9cc7), LPC_WPT_LP},
 	{ PCI_VDEVICE(INTEL, 0x9cc9), LPC_WPT_LP},
+	{ PCI_VDEVICE(INTEL, 0x229c), LPC_CHERRYTRAIL},
 	{ 0, },			/* End of list */
 };
 MODULE_DEVICE_TABLE(pci, lpc_ich_ids);
