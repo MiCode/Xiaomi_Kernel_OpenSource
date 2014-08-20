861d67e19dbbe66a22f5fa97e5276b26ce21fe2e
Guoping Yu <guopingy@codeaurora.org>
2014-08-20 16:56:18 +0800

    mmc: sdhci-msm: add nonhotplug support
    With some devices like QRD SKUK, SD card could not support
    hotplug as there is no cd-gpio, and also could not use polling
    way due to power comsumption. So add nonhotplug to meet such
    requirement and when SD card lost or removed manually, device
    will not crash until next reboot process to detect SD card.
    
    Change-Id: I318d3db72fc09248c5dada2fb6d69ade1bbf85cb
    Signed-off-by: Guoping Yu <guopingy@codeaurora.org>

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -300,6 +300,7 @@ struct sdhci_msm_pltfm_data {
 	unsigned long mmc_bus_width;
 	struct sdhci_msm_slot_reg_data *vreg_data;
 	bool nonremovable;
+	bool nonhotplug;
 	bool pin_cfg_sts;
 	struct sdhci_msm_pin_data *pin_data;
 	struct sdhci_pinctrl_data *pctrl_data;
@@ -1551,6 +1552,9 @@ static struct sdhci_msm_pltfm_data *sdhci_msm_populate_pdata(struct device *dev)
 
 	if (of_get_property(np, "qcom,nonremovable", NULL))
 		pdata->nonremovable = true;
+
+	if (of_get_property(np, "qcom,nonhotplug", NULL))
+		pdata->nonhotplug = true;
 
 	if (!of_property_read_u32(np, "qcom,dat1-mpm-int",
 				  &mpm_int))
@@ -3212,6 +3216,9 @@ static int sdhci_msm_probe(struct platform_device *pdev)
 
 	if (msm_host->pdata->nonremovable)
 		msm_host->mmc->caps |= MMC_CAP_NONREMOVABLE;
+
+	if (msm_host->pdata->nonhotplug)
+		msm_host->mmc->caps2 |= MMC_CAP2_NONHOTPLUG;
 
 	host->cpu_dma_latency_us = msm_host->pdata->cpu_dma_latency_us;
 
