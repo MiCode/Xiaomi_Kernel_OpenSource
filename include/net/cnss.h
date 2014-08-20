42d5c6b42ace0341db4cab3871caf3f21822f79d
Yue Ma <yuem@codeaurora.org>
2014-08-20 16:55:07 -0700

    cnss: Add API to schedule recovery work
    Expose API to schedule recovery work which WLAN host driver can use to
    serialize multiple recoveries.
    
    Change-Id: I2ba66d2a96adce851e678f47ee0479543a9d6cb0
    CRs-fixed: 712603
    Signed-off-by: Yue Ma <yuem@codeaurora.org>

--- include/net/cnss.h
+++ include/net/cnss.h
@@ -79,6 +79,7 @@ extern int cnss_get_wlan_unsafe_channel(u16 *unsafe_ch_list,
 		u16 *ch_count, u16 buf_len);
 extern int cnss_wlan_set_dfs_nol(void *info, u16 info_len);
 extern int cnss_wlan_get_dfs_nol(void *info, u16 info_len);
+extern void cnss_schedule_recovery_work(void);
 extern void cnss_wlan_pci_link_down(void);
 extern int cnss_pcie_shadow_control(struct pci_dev *dev, bool enable);
 extern int cnss_wlan_register_driver(struct cnss_wlan_driver *driver);
