fa44f8cece91ee94d90d75141246eb3da5583851
Banajit Goswami <bgoswami@codeaurora.org>
2014-08-20 17:40:38 -0700

    ALSA: jack: Update supported jack switch types
    Change adds support for jack switch types supported
    by platform
    
    Signed-off-by: Gopikrishnaiah Anandan <agopik@codeaurora.org>
    Signed-off-by: Banajit Goswami <bgoswami@codeaurora.org>

--- include/uapi/linux/input.h
+++ include/uapi/linux/input.h
@@ -889,8 +889,11 @@ struct input_keymap_entry {
 #define SW_FRONT_PROXIMITY	0x0b  /* set = front proximity sensor active */
 #define SW_ROTATE_LOCK		0x0c  /* set = rotate locked/disabled */
 #define SW_LINEIN_INSERT	0x0d  /* set = inserted */
-#define SW_MUTE_DEVICE		0x0e  /* set = device disabled */
-#define SW_MAX			0x0f
+#define SW_HPHL_OVERCURRENT	0x0e  /* set = over current on left hph */
+#define SW_HPHR_OVERCURRENT	0x0f  /* set = over current on right hph */
+#define SW_UNSUPPORT_INSERT	0x10  /* set = unsupported device inserted */
+#define SW_MUTE_DEVICE		0x12  /* set = device disabled */
+#define SW_MAX			0x20
 #define SW_CNT			(SW_MAX+1)
 
 /*
