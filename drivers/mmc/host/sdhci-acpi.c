73672b1d912fd351abee7b301bbf84c7e89b15f7
Chuanxiao Dong <chuanxiao.dong@intel.com>
2014-08-27 10:19:45 +0800

    mmc: sdhci-acpi: Add CHT SD card host support
    Change-Id: Id63cb82ccc77533ba81e2b7822232336dff0f540
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- drivers/mmc/host/sdhci-acpi.c
+++ drivers/mmc/host/sdhci-acpi.c
@@ -176,6 +176,7 @@ static const struct sdhci_acpi_uid_slot sdhci_acpi_uids[] = {
 	{ "80860F14" , "1" , &sdhci_acpi_slot_int_emmc },
 	{ "80860F14" , "3" , &sdhci_acpi_slot_int_sd   },
 	{ "INT33BB"  , "2" , &sdhci_acpi_slot_int_sdio },
+	{ "INT33BB"  , "3" , &sdhci_acpi_slot_int_sd   },
 	{ "INT33C6"  , NULL, &sdhci_acpi_slot_int_sdio },
 	{ "INT3436"  , NULL, &sdhci_acpi_slot_int_sdio },
 	{ "PNP0D40"  },
