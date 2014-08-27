66be5bea2deefc10115c99b49c5986c82bd1e648
Mahesh Sivasubramanian <msivasub@codeaurora.org>
2014-08-27 11:42:48 -0600

    qos: Pass the list of cpus with affected qos to notifier
    Send the list of cpus whose qos has been affected along with the changed
    value. Driver listening in for notifier can use this to apply the qos value
    for the respective cpus.
    
    Change-Id: I8f3c2ea624784c806c55de41cc7c7fcf8ebf02da
    Signed-off-by: Mahesh Sivasubramanian <msivasub@codeaurora.org>
    [mattw@codeaurora.org: resolve trivial context conflicts]
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -189,7 +189,8 @@ static inline void pm_qos_set_value(struct pm_qos_constraints *c, s32 value)
 	c->target_value = value;
 }
 
-static inline void pm_qos_set_value_for_cpus(struct pm_qos_constraints *c)
+static inline void pm_qos_set_value_for_cpus(struct pm_qos_constraints *c,
+		struct cpumask *cpus)
 {
 	struct pm_qos_request *req = NULL;
 	int cpu;
@@ -213,8 +214,11 @@ static inline void pm_qos_set_value_for_cpus(struct pm_qos_constraints *c)
 		}
 	}
 
-	for_each_possible_cpu(cpu)
+	for_each_possible_cpu(cpu) {
+		if (c->target_per_cpu[cpu] != qos_val[cpu])
+			cpumask_set_cpu(cpu, cpus);
 		c->target_per_cpu[cpu] = qos_val[cpu];
+	}
 }
 
 /**
@@ -235,6 +239,7 @@ int pm_qos_update_target(struct pm_qos_constraints *c,
 	unsigned long flags;
 	int prev_value, curr_value, new_value;
 	struct plist_node *node = &req->node;
+	struct cpumask cpus;
 	int ret;
 
 	spin_lock_irqsave(&pm_qos_lock, flags);
@@ -265,8 +270,9 @@ int pm_qos_update_target(struct pm_qos_constraints *c,
 	}
 
 	curr_value = pm_qos_get_value(c);
+	cpumask_clear(&cpus);
 	pm_qos_set_value(c, curr_value);
-	pm_qos_set_value_for_cpus(c);
+	pm_qos_set_value_for_cpus(c, &cpus);
 
 	spin_unlock_irqrestore(&pm_qos_lock, flags);
 
@@ -276,7 +282,7 @@ int pm_qos_update_target(struct pm_qos_constraints *c,
 		if (c->notifiers)
 			blocking_notifier_call_chain(c->notifiers,
 						     (unsigned long)curr_value,
-						     NULL);
+						     &cpus);
 	} else {
 		ret = 0;
 	}
