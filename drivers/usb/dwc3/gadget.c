e7cd838a28a8034ddbb9ae178472965a1b314be4
Mayank Rana <mrana@codeaurora.org>
2014-08-28 16:47:51 -0700

    dwc3: gadget: Ignore suspend event if USB speed is unknown
    In some of host machine reboot usecases, it has been found that
    multiple suspend events are being received before usb device is
    being enumerated with host including receiving suspend before
    BUS RESET. This suspend event is being seen due to not host
    initiated suspend but due to LINE state at that point and may
    put USB Host and Device out of sync where host may not send any
    more BUS RESET which would cause USB device enumeration as unknown
    device with host machine. Fix this issue by honouring suspend
    event only when USB enumeration speed is known.
    
    CRs-Fixed: 717151
    Change-Id: I201f106d1be71716eaa7a6246d2187889ea53a9c
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>
    Signed-off-by: Azhar Shaikh <azhars@codeaurora.org>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -2887,6 +2887,7 @@ static void dwc3_gadget_reset_interrupt(struct dwc3 *dwc)
 	reg = dwc3_readl(dwc->regs, DWC3_DCFG);
 	reg &= ~(DWC3_DCFG_DEVADDR_MASK);
 	dwc3_writel(dwc->regs, DWC3_DCFG, reg);
+	dwc->gadget.speed = USB_SPEED_UNKNOWN;
 	dwc->link_state = DWC3_LINK_STATE_U0;
 	wake_up_interruptible(&dwc->wait_linkstate);
 }
@@ -3276,9 +3277,13 @@ static void dwc3_gadget_interrupt(struct dwc3 *dwc,
 			dbg_event(0xFF, "GAD SUS", 0);
 			dwc->dbg_gadget_events.suspend++;
 
-			/* ignore if usb cable is not connected */
-			if (dwc->vbus_active)
-				dwc3_gadget_suspend_interrupt(dwc,
+			/*
+			 * Ignore suspend event if usb cable is not connected
+			 * and speed is not being detected.
+			 */
+			if (dwc->gadget.speed != USB_SPEED_UNKNOWN &&
+				dwc->vbus_active)
+					dwc3_gadget_suspend_interrupt(dwc,
 							event->event_info);
 		}
 		break;
