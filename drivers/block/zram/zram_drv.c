d9e2e2744ae160a8a116449b8160b2dcf2998bbb
Chao Yu <chao2.yu@samsung.com>
2014-08-29 15:18:37 -0700

    UPSTREAM: zram: fix incorrect stat with failed_reads
    (cherry-pick from commit 0cf1e9d6c34d4c82ac3af8015594849814843d36)
    
    Since we allocate a temporary buffer in zram_bvec_read to handle partial
    page operations in commit 924bd88d703e ("Staging: zram: allow partial
    page operations"), our ->failed_reads value may be incorrect as we do
    not increase its value when failing to allocate the temporary buffer.
    
    Let's fix this issue and correct the annotation of failed_reads.
    
    Bug: 25951511
    
    Change-Id: Id3e857b5cda53187c264ce3e5779c38f7b4aa610
    Signed-off-by: Chao Yu <chao2.yu@samsung.com>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Cc: Nitin Gupta <ngupta@vflare.org>
    Acked-by: Jerome Marchand <jmarchan@redhat.com>
    Acked-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -425,7 +425,6 @@ static int zram_decompress_page(struct zram *zram, char *mem, u32 index)
 	/* Should NEVER happen. Return bio error if it does. */
 	if (unlikely(ret)) {
 		pr_err("Decompression failed! err=%d, page=%u\n", ret, index);
-		atomic64_inc(&zram->stats.failed_reads);
 		return ret;
 	}
 
@@ -606,8 +605,6 @@ out:
 		zcomp_strm_release(zram->comp, zstrm);
 	if (is_partial_io(bvec))
 		kfree(uncmem);
-	if (ret)
-		atomic64_inc(&zram->stats.failed_writes);
 	return ret;
 }
 
@@ -639,6 +636,13 @@ static int zram_bvec_rw(struct zram *zram, struct bio_vec *bvec, u32 index,
 		handle_pending_slot_free(zram);
 		atomic64_inc(&zram->stats.num_writes);
 		ret = zram_bvec_write(zram, bvec, index, offset);
+	}
+
+	if (unlikely(ret)) {
+		if (rw == READ)
+			atomic64_inc(&zram->stats.failed_reads);
+		else
+			atomic64_inc(&zram->stats.failed_writes);
 	}
 
 	return ret;
