f7525bcb9b9e4adea27fe0b512f430ba3f9adb8c
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2014-09-08 09:53:50 +0100

    ASoC: wm_adsp: Add support for MasterHiFi firmware
    Change-Id: Idcf25be405a91b68b73cfe4a5edfa447eada7296
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm_adsp.c
+++ sound/soc/codecs/wm_adsp.c
@@ -198,7 +198,7 @@ static void wm_adsp_buf_free(struct list_head *list)
 /* Must remain a power of two */
 #define WM_ADSP_CAPTURE_BUFFER_SIZE      1048576
 
-#define WM_ADSP_NUM_FW 18
+#define WM_ADSP_NUM_FW 19
 
 #define WM_ADSP_FW_MBC_VSS        0
 #define WM_ADSP_FW_TX             1
@@ -218,6 +218,7 @@ static void wm_adsp_buf_free(struct list_head *list)
 #define WM_ADSP_FW_EZ2GROUPTALK_RX 15
 #define WM_ADSP_FW_EZ2RECORD       16
 #define WM_ADSP_FW_ASR_ASSIST     17
+#define WM_ADSP_FW_MASTERHIFI     18
 
 static const char *wm_adsp_fw_text[WM_ADSP_NUM_FW] = {
 	[WM_ADSP_FW_MBC_VSS] =    "MBC/VSS",
@@ -238,6 +239,7 @@ static const char *wm_adsp_fw_text[WM_ADSP_NUM_FW] = {
 	[WM_ADSP_FW_EZ2GROUPTALK_RX] = "Ez2GroupTalk Rx",
 	[WM_ADSP_FW_EZ2RECORD] = "Ez2Record",
 	[WM_ADSP_FW_ASR_ASSIST] = "ASR Assist",
+	[WM_ADSP_FW_MASTERHIFI] = "MasterHiFi",
 };
 
 struct wm_adsp_system_config_xm_hdr {
@@ -400,6 +402,7 @@ static struct wm_adsp_fw_defs wm_adsp_fw[WM_ADSP_NUM_FW] = {
 	[WM_ADSP_FW_EZ2GROUPTALK_RX] = { .file = "ez2grouptalk-rx" },
 	[WM_ADSP_FW_EZ2RECORD] = { .file = "ez2record" },
 	[WM_ADSP_FW_ASR_ASSIST] =     { .file = "asr-assist" },
+	[WM_ADSP_FW_MASTERHIFI] =     { .file = "masterhifi" },
 };
 
 struct wm_coeff_ctl_ops {
@@ -2164,7 +2167,8 @@ int wm_adsp2_event(struct snd_soc_dapm_widget *w,
 		break;
 
 	case SND_SOC_DAPM_PRE_PMD:
-		if (dsp->fw_id == 0x40019) {
+		if (dsp->fw_id == 0x40019 || dsp->fw_id == 0x5001f ||
+		    dsp->fw_id == 0x4001f) {
 			wm_adsp_edac_shutdown(dsp);
 		}
 
