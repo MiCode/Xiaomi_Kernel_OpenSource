f0e5372a273f14f79067cf02de86fbcdf022673e
Mitchel Humpherys <mitchelh@codeaurora.org>
2014-09-09 16:02:15 -0700

    arm64: dma-mapping: make dma_ops const
    dma_ops should be const since they shouldn't be changing after being
    defined (there's even a checkpatch warning for this). Convert existing
    dma_ops to const.
    
    Change-Id: Ic27ba9a0832b12cae43cd45c72f053335845fd8f
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- arch/arm64/mm/dma-mapping.c
+++ arch/arm64/mm/dma-mapping.c
@@ -33,7 +33,7 @@
 
 #include "mm.h"
 
-struct dma_map_ops *dma_ops;
+const struct dma_map_ops *dma_ops;
 EXPORT_SYMBOL(dma_ops);
 
 static pgprot_t __get_dma_pgprot(struct dma_attrs *attrs, pgprot_t prot,
@@ -331,7 +331,7 @@ static int __swiotlb_mmap_coherent(struct device *dev,
 	return __dma_common_mmap(dev, vma, cpu_addr, dma_addr, size);
 }
 
-struct dma_map_ops noncoherent_swiotlb_dma_ops = {
+const struct dma_map_ops noncoherent_swiotlb_dma_ops = {
 	.alloc = __dma_alloc_noncoherent,
 	.free = __dma_free_noncoherent,
 	.mmap = __swiotlb_mmap_noncoherent,
@@ -348,7 +348,7 @@ struct dma_map_ops noncoherent_swiotlb_dma_ops = {
 };
 EXPORT_SYMBOL(noncoherent_swiotlb_dma_ops);
 
-struct dma_map_ops coherent_swiotlb_dma_ops = {
+const struct dma_map_ops coherent_swiotlb_dma_ops = {
 	.alloc = __dma_alloc_coherent,
 	.free = __dma_free_coherent,
 	.mmap = __swiotlb_mmap_coherent,
