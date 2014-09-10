f00232d3bbdccd40e0fcff8add24a36344dbae51
Aravind Venkateswaran <aravindh@codeaurora.org>
2014-09-09 17:10:35 -0700

    msm: mdss: fix issues related to transitions in and out of doze mode
    When entering doze mode, ensure that the tear-check setup is not
    torn down. In addition, do not disable the split-display configuration.
    This is to ensure that mdss driver can continue to handle any display
    updates in doze mode. Similarly, when transitioning from doze power
    state to blank power state, ensure that we do not send panel unblank and
    panel on events. These events should only be sent when transitioning to
    unblank power state.
    
    Change-Id: If5ca1cdd20bb442e773912575c0a6e204c0ca1a2
    Signed-off-by: Aravind Venkateswaran <aravindh@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_ctl.c
+++ drivers/video/msm/mdss/mdss_mdp_ctl.c
@@ -2353,7 +2353,6 @@ int mdss_mdp_ctl_stop(struct mdss_mdp_ctl *ctl, int power_state)
 		if (ctl->panel_data->panel_info.fbc.enabled)
 			mdss_mdp_ctl_fbc_enable(0, sctl->mixer_left,
 				&sctl->panel_data->panel_info);
-		mdss_mdp_ctl_split_display_enable(0, ctl, sctl);
 	}
 	if (ret) {
 		pr_warn("error powering off intf ctl=%d\n", ctl->num);
@@ -2364,6 +2363,9 @@ int mdss_mdp_ctl_stop(struct mdss_mdp_ctl *ctl, int power_state)
 		pr_debug("panel is not off, leaving ctl power on\n");
 		goto end;
 	}
+
+	if (sctl)
+		mdss_mdp_ctl_split_display_enable(0, ctl, sctl);
 
 	mdss_mdp_ctl_write(ctl, MDSS_MDP_REG_CTL_TOP, 0);
 	if (sctl)
