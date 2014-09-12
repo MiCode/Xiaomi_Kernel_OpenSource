05546ecb11307a50409f6fa10c4dbb9a30247511
Sudheer Papothi <spapothi@codeaurora.org>
2014-09-12 21:40:09 +0530

    ASoC: wcd9xxx: Set chopper settings for UHQA mode
    UHQA (Ultra High Quality Audio) requires chopper to set the
    frequency to the actual frequency. Change enables chopper
    to set the frequency to half for all audio streams
    with different sampling rates and 24-bits per sample
    other than UHQA mode.
    
    CRs-Fixed: 722714
    Change-Id: I7dbf2629c37f0139af67c8491a49197bb85cd315
    Signed-off-by: Sudheer Papothi <spapothi@codeaurora.org>

--- sound/soc/codecs/wcd9xxx-common.h
+++ sound/soc/codecs/wcd9xxx-common.h
@@ -44,6 +44,8 @@
 #define WCD9XXX_CLSAB_REQ_ENABLE  true
 #define WCD9XXX_CLSAB_REQ_DISABLE false
 
+#define WCD9XXX_NON_UHQA_MODE	0
+
 #define WCD9XXX_DMIC_SAMPLE_RATE_DIV_2    0x0
 #define WCD9XXX_DMIC_SAMPLE_RATE_DIV_3    0x1
 #define WCD9XXX_DMIC_SAMPLE_RATE_DIV_4    0x2
@@ -183,7 +185,7 @@ extern void wcd9xxx_clsh_fsm(struct snd_soc_codec *codec,
 
 extern void wcd9xxx_enable_high_perf_mode(struct snd_soc_codec *codec,
 				struct wcd9xxx_clsh_cdc_data *clsh_d,
-				u8 req_state, bool req_type);
+				u8 uhqa_mode, u8 req_state, bool req_type);
 
 extern void wcd9xxx_clsh_init(struct wcd9xxx_clsh_cdc_data *clsh,
 			      struct wcd9xxx_resmgr *resmgr);
