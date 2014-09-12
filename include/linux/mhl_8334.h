ff7c49624aa062c5b870734fc5bc2b888670385b
raghavendra ambadas <rambad@codeaurora.org>
2014-09-12 17:42:42 +0530

    msm: mhl: sii8334: Add mutex lock to avoid race condition
    Add mutex lock betweeen mhl_sii_config and mhl_sii_device_discovery
    functions to avoid accesing the variable flag before it is updated.
    This will avoid accessing the mhl init settings i2c register
    before turning on the mhl regulator's and gpio's.
    
    Change-Id: If3f7fbe3d6ab84f101c407c43ac8b410e6df38fe
    Signed-off-by: Raghavendra Ambadas <rambad@codeaurora.org>

--- include/linux/mhl_8334.h
+++ include/linux/mhl_8334.h
@@ -164,6 +164,11 @@ struct mhl_tx_ctrl {
 	int wr_burst_pending;
 	struct completion req_write_done;
 	spinlock_t lock;
+	/*
+	* Lock between mhl_sii_config and
+	* mhl_sii_device_discovery functions
+	*/
+	struct mutex sii_config_lock;
 	bool tx_powered_off;
 	uint8_t dwnstream_hpd;
 	bool mhl_det_discon;
