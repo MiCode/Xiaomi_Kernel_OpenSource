6c235ec6203fa77f7c66713db3e3fc299821d5da
Laura Abbott <lauraa@codeaurora.org>
2014-09-22 13:26:28 -0700

    mm: Add notifier framework for showing memory
    There are many drivers in the kernel which can hold on
    to lots of memory. It can be useful to dump out all those
    drivers at key points in the kernel. Introduct a notifier
    framework for dumping this information. When the notifiers
    are called, drivers can dump out the state of any memory
    they may be using.
    
    Change-Id: Ifb2946964bf5d072552dd56d8d6dfdd794af6d84
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

---
+++ include/linux/show_mem_notifier.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2014, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/notifier.h>
+
+int show_mem_notifier_register(struct notifier_block *nb);
+
+int show_mem_notifier_unregister(struct notifier_block *nb);
+
+void show_mem_call_notifiers(void);
