f49905e2becf55b3ba1218171bd521cf849640eb
Laura Abbott <lauraa@codeaurora.org>
2014-09-23 19:31:19 -0700

    lowmemorykiller: Run the lowmemory notifier when killing
    When the lowmemory killer starts killing processes, it's useful to
    be able to see the state of other memory in the system. Call the
    lowmemory notifier to get other clients to dump out their state.
    
    Change-Id: Ie3b1d47149f0108b42b7961fd47a8cc5efe2c590
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -44,6 +44,7 @@
 #include <linux/swap.h>
 #include <linux/fs.h>
 #include <linux/cpuset.h>
+#include <linux/show_mem_notifier.h>
 
 #ifdef CONFIG_HIGHMEM
 #define _ZONE ZONE_HIGHMEM
@@ -412,6 +413,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		if (lowmem_debug_level >= 2 && selected_oom_score_adj == 0) {
 			show_mem(SHOW_MEM_FILTER_NODES);
 			dump_tasks(NULL, NULL);
+			show_mem_call_notifiers();
 		}
 
 		lowmem_deathpending_timeout = jiffies + HZ;
