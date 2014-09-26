b274003e1306fcaf3594fbe517150c97d1be37c6
Sudheer Papothi <spapothi@codeaurora.org>
2014-09-26 08:28:01 +0530

    ASoC: wcd9330: Update device path sequence for EAR
    Update device path sequence for EAR. This change will
    not enable EAR device path when device switch happens
    from EAR to headphone path.
    
    Change-Id: Id8606036203309afd8a104093dda0f1bb716043c
    Signed-off-by: Sudheer Papothi <spapothi@codeaurora.org>

--- sound/soc/codecs/wcd9330.c
+++ sound/soc/codecs/wcd9330.c
@@ -5937,10 +5937,8 @@ static int tomtom_codec_ear_dac_event(struct snd_soc_dapm_widget *w,
 						 WCD9XXX_CLSH_STATE_EAR,
 						 WCD9XXX_CLSH_REQ_ENABLE,
 						 WCD9XXX_CLSH_EVENT_PRE_DAC);
-		snd_soc_update_bits(codec, w->reg, 0x20, 0x20);
 		break;
 	case SND_SOC_DAPM_POST_PMD:
-		snd_soc_update_bits(codec, w->reg, 0x20, 0x00);
 		wcd9xxx_clsh_fsm(codec, &tomtom_p->clsh_d,
 						 WCD9XXX_CLSH_STATE_EAR,
 						 WCD9XXX_CLSH_REQ_DISABLE,
