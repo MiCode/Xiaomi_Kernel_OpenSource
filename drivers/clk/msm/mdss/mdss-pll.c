6e9b4cd96b31b3b63596350e3d13b74d93bcc161
Shivaraj Shetty <shivaraj@codeaurora.org>
2014-09-26 12:08:01 +0530

    clk: qcom: mdss: add DSI PLL clock driver support for msm8909
    Add changes for DSI PLL clock driver support for msm8909. Add
    the compatibility string of the DSI PLL handle so that the
    detection and support of DSI PLL driver for 8909 happens
    dynamically.
    
    Change-Id: If1fb96982433f90c5b82dda8686b7284825bcd09
    Signed-off-by: Shivaraj Shetty <shivaraj@codeaurora.org>

--- drivers/clk/msm/mdss/mdss-pll.c
+++ drivers/clk/msm/mdss/mdss-pll.c
@@ -133,6 +133,9 @@ static int mdss_pll_resource_parse(struct platform_device *pdev,
 	} else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_8939")) {
 		pll_res->pll_interface_type = MDSS_DSI_PLL_LPM;
 		pll_res->target_id = MDSS_PLL_TARGET_8939;
+	} else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_8909")) {
+		pll_res->pll_interface_type = MDSS_DSI_PLL_LPM;
+		pll_res->target_id = MDSS_PLL_TARGET_8909;
 	} else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_8974")) {
 		pll_res->pll_interface_type = MDSS_DSI_PLL_HPM;
 		pll_res->target_id = MDSS_PLL_TARGET_8974;
@@ -365,6 +368,7 @@ static const struct of_device_id mdss_pll_dt_match[] = {
 	{.compatible = "qcom,mdss_hdmi_pll_8994"},
 	{.compatible = "qcom,mdss_dsi_pll_8916"},
 	{.compatible = "qcom,mdss_dsi_pll_8939"},
+	{.compatible = "qcom,mdss_dsi_pll_8909"},
 	{.compatible = "qcom,mdss_edp_pll"},
 	{.compatible = "qcom,mdss_hdmi_pll"},
 	{}
