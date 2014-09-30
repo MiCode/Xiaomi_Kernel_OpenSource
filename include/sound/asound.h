c829aa0b0f1a7f314b6ddbea8826ebc6b9a0cd05
Anish Kumar <kanish@codeaurora.org>
2014-09-29 21:18:35 -0700

    ASoC: wcd9xxx: use hwdep node for codec calibration
    Currently codec driver uses request_firmware to get
    calibration data but as this firmware file is also written by
    userspace process after bootup, it is not recommended to
    use request_firmware. ALSA core provides mechanism to get
    hardware dependant data using hwdep nodes. Tomtom codec will
    use aforementioned nodes to get calibration data from userspace.
    
    Changed ANC calibration data size
    
    Change-Id: Iace6e239029c7b0d93a5d61a0025ee551acf952f
    Signed-off-by: Anish Kumar <kanish@codeaurora.org>
    Signed-off-by: Pavan Chikkala <pavanc@codeaurora.org>

--- include/sound/asound.h
+++ include/sound/asound.h
@@ -95,9 +95,10 @@ enum {
 	SNDRV_HWDEP_IFACE_SB_RC,	/* SB Extigy/Audigy2NX remote control */
 	SNDRV_HWDEP_IFACE_HDA,		/* HD-audio */
 	SNDRV_HWDEP_IFACE_USB_STREAM,	/* direct access to usb stream */
+        SNDRV_HWDEP_IFACE_AUDIO_CODEC,  /* codec Audio Control */
 
 	/* Don't forget to change the following: */
-	SNDRV_HWDEP_IFACE_LAST = SNDRV_HWDEP_IFACE_USB_STREAM
+	SNDRV_HWDEP_IFACE_LAST = SNDRV_HWDEP_IFACE_AUDIO_CODEC
 };
 
 struct snd_hwdep_info {
