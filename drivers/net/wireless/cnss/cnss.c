f17212d440686cbc7cc8e6f14805e104ed4558e7
Sameer Thalappil <sameert@codeaurora.org>
2014-09-30 14:43:29 -0700

    cnss: Fix spelling and downgrade error level
    Fix spelling in error message; also downgrade the error level.
    Swappable partition is an optional feature, not finding the image
    is not an error.
    
    CRs-Fixed: 732663
    Change-Id: If3c15d6ae1984d75db6ca7e64766eff3198e8bbe
    Signed-off-by: Sameer Thalappil <sameert@codeaurora.org>

--- drivers/net/wireless/cnss/cnss.c
+++ drivers/net/wireless/cnss/cnss.c
@@ -900,7 +900,7 @@ int cnss_get_codeswap_struct(struct codeswap_codeseg_info *swap_seg)
 		return -ENOENT;
 	}
 	if (!penv->fw_available) {
-		pr_err("%s: fw is not availabe\n", __func__);
+		pr_debug("%s: fw is not available\n", __func__);
 		return -ENOENT;
 	}
 
@@ -928,7 +928,7 @@ static void cnss_wlan_memory_expansion(void)
 	cnss_seg_info = penv->cnss_seg_info;
 
 	if (!cnss_seg_info) {
-		pr_err("cnss: cnss_seg_info is NULL\n");
+		pr_debug("cnss: cnss_seg_info is NULL\n");
 		goto end;
 	}
 
