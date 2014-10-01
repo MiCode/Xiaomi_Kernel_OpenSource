94d0fa9c7cea0e4b2af18745823347d596a8603d
Neil Leeder <nleeder@codeaurora.org>
2014-10-01 13:29:57 -0400

    Perf: arm64: Refine disable/enable in tracecounters
    Tracecounter code was not disabling counters as early as possible
    and enabling them as late as possible. Refine the logic so cycles
    spent inside the handler are lower. This results in more accurate
    per-process counter values.
    
    Change-Id: I5a83028da3c747c30a9e5a0ea3003638beadffec
    Signed-off-by: Neil Leeder <nleeder@codeaurora.org>

--- arch/arm64/kernel/perf_debug.c
+++ arch/arm64/kernel/perf_debug.c
@@ -28,6 +28,7 @@ static char *descriptions =
 	" 6 Perf: arm64: Add debugfs node to clear PMU\n"
 	" 7 Perf: arm64: Update PMU force reset\n"
 	"10 Perf: arm64: tracectr: initialize counts after hotplug\n"
+	"11 Perf: arm64: Refine disable/enable in tracecounters\n"
 ;
 
 static ssize_t desc_read(struct file *fp, char __user *buf,
