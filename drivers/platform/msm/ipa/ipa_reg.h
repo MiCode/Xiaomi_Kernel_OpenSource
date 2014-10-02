1543477db2087f3bb35f9fa88cda5de832643245
Ravi Gummadidala <rgummadi@codeaurora.org>
2014-10-02 10:27:45 -0700

    msm ipa: update uC mailbox offset
    In IPA v2.5, the uC mailbox address offset different from
    V2.0. Add new macro to use for 2.5
    
    Change-Id: I937dba7499ab3f0b4dcf194d100f723358cc578c
    Acked-by: Sunil Kumar Paidimarri <hisunil@qti.qualcomm.com>
    Signed-off-by: Ravi Gummadidala <rgummadi@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_reg.h
+++ drivers/platform/msm/ipa/ipa_reg.h
@@ -328,6 +328,7 @@ Common Registers
 #define IPA_IRQ_EE_UC_n_INT_SHFT 0x0
 
 #define IPA_UC_MAILBOX_m_n_OFFS(m, n) (0x0003a000 + 0x80 * (m) + 0x4 * (n))
+#define IPA_UC_MAILBOX_m_n_OFFS_v2_5(m, n) (0x00062000 + 0x80 * (m) + 0x4 * (n))
 
 #define IPA_SYS_PKT_PROC_CNTXT_BASE_OFST (0x000005d8)
 #define IPA_LOCAL_PKT_PROC_CNTXT_BASE_OFST (0x000005e0)
