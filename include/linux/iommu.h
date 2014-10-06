317608ea8973fc43457ea902fb1c6c666422e085
Varun Sethi <Varun.Sethi@freescale.com>
2014-10-06 11:28:00 +0100

    Introduce the IOMMU_DEVICE flag.
    This is used for indicating device memory type for a DMA transaction. IOMMU
    driver would set up attributes indicationg access to device memory.
    
    CRs-Fixed: 792402
    Change-Id: Ifd6f2288353ef737ff4c15d9bd1514c66af8fdd2
    Signed-off-by: Varun Sethi <Varun.Sethi@freescale.com>
    [neetid: changed IOMMU_DEVICE to be on bit 5 instead of bit 4]
    Patch-mainline: iommu @ 10/06/14, 10:28
    Signed-off-by: Neeti Desai <neetid@codeaurora.org>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -29,6 +29,7 @@
 #define IOMMU_CACHE	(1 << 2) /* DMA cache coherency */
 #define IOMMU_EXEC	(1 << 3)
 #define IOMMU_PRIV	(1 << 4)
+#define IOMMU_DEVICE	(1 << 5) /* Indicates access to device memory */
 
 struct iommu_ops;
 struct iommu_group;
