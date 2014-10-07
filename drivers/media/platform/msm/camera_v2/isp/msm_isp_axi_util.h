76add8fdf6edc4618ccb770f57980d5cc09b4016
Abhishek Kondaveeti <akondave@codeaurora.org>
2014-10-07 16:26:03 +0530

    msm: isp: Fix burst count update
    Reduce burst count for  a stream based
    on the reg update of vfe input source
    associated with that particular stream.
    
    Change-Id: I7355a78334cf6d266101ffa6c066f06c3b05c16a
    Signed-off-by: Abhishek Kondaveeti <akondave@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.h
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.h
@@ -55,9 +55,11 @@ int msm_isp_axi_reset(struct vfe_device *vfe_dev,
 int msm_isp_axi_restart(struct vfe_device *vfe_dev,
 	struct msm_vfe_axi_restart_cmd *restart_cmd);
 
-void msm_isp_axi_stream_update(struct vfe_device *vfe_dev);
+void msm_isp_axi_stream_update(struct vfe_device *vfe_dev,
+	uint8_t input_src);
 
-void msm_isp_update_framedrop_reg(struct vfe_device *vfe_dev);
+void msm_isp_update_framedrop_reg(struct vfe_device *vfe_dev,
+	uint8_t input_src);
 void msm_isp_sof_notify(struct vfe_device *vfe_dev,
 	enum msm_vfe_input_src frame_src, struct msm_isp_timestamp *ts);
 void msm_isp_process_axi_irq(struct vfe_device *vfe_dev,
