fb44464063ceab439260a2386293c9d1f9ff1176
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2014-10-09 18:13:06 +0530

    msm: mdss: cleanup ESD related code for NT35596 panel
    Currently the NT35596 ESD related DSI read values are defined
    in mdss_dsi_panel.c file. Add changes to pass these values from
    panel DTSI and parsing them in DSI drivers accordingly. This
    helps making the code generic and can be easily extended to
    other panels also.
    
    Change-Id: If62fa45fdf41429d2efe14a78857433883a329a9
    (cherry picked from commit 51684bee60e503e9b06174a294ed2eb7b939d5b2)
    [veeras@codeaurora.org: As part of 3.14 upgrade reomve
    arch/arm/boot/dts/qcom/dsi-panel-nt35596-1080p-skuk-video.dtsi]
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>
    Signed-off-by: Veera Sundaram Sankaran <veeras@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_dsi.h
+++ drivers/video/fbdev/msm/mdss_dsi.h
@@ -351,8 +351,9 @@ struct mdss_dsi_ctrl_pdata {
 	struct dsi_panel_cmds off_cmds;
 	struct dsi_panel_cmds status_cmds;
 	u32 status_cmds_rlen;
-	u32 status_value;
+	u32 *status_value;
 	u32 status_error_count;
+	u32 max_status_error_count;
 
 	struct dsi_panel_cmds video2cmd;
 	struct dsi_panel_cmds cmd2video;
@@ -588,6 +589,12 @@ static inline bool mdss_dsi_ulps_feature_enabled(
 	struct mdss_panel_data *pdata)
 {
 	return pdata->panel_info.ulps_feature_enabled;
+}
+
+static inline bool mdss_dsi_cmp_panel_reg(struct dsi_buf status_buf,
+	u32 *status_val, int i)
+{
+	return status_buf.data[i] == status_val[i];
 }
 
 #endif /* MDSS_DSI_H */
