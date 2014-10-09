5fe47126eed83cf1ea139383d3c66a05b3b016aa
Subhash Jadavani <subhashj@codeaurora.org>
2014-10-09 02:54:41 -0700

    scsi: ufs: Add sysfs node to dynamically control hibern8 on idle
    Provide an option to enable/disable hibern8 on idle functionality during
    runtime. Write 1 or 0 to "hibern8_on_idle_enable" sysfs node to
    enable/disable hibern8 on idle functionality.
    
    Change-Id: Id4b6253c3c53ed71575c05596abbd4dd99821eff
    [subhashj@codeaurora.org: resolved trivial merge conflicts]
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>
    [venkatg@codeaurora.org: resolved trivial merge conflicts]
    Signed-off-by: Venkat Gopalakrishnan <venkatg@codeaurora.org>
    [cang@codeaurora.org: Resolved trivial merge conflicts]
    Signed-off-by: Can Guo <cang@codeaurora.org>

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -408,6 +408,9 @@ enum ufshcd_hibern8_on_idle_state {
  * during suspend/resume
  * @active_reqs: number of requests that are pending and should be waited for
  * completion before scheduling delayed "enter_work".
+ * @delay_attr: sysfs attribute to control delay_attr
+ * @enable_attr: sysfs attribute to enable/disable hibern8 on idle
+ * @is_enabled: Indicates the current status of hibern8
  */
 struct ufs_hibern8_on_idle {
 	struct delayed_work enter_work;
@@ -416,6 +419,9 @@ struct ufs_hibern8_on_idle {
 	unsigned long delay_ms;
 	bool is_suspended;
 	int active_reqs;
+	struct device_attribute delay_attr;
+	struct device_attribute enable_attr;
+	bool is_enabled;
 };
 
 /**
