4b682648c78e88a9b91517ad2f59b7172dc82656
Gilad Broner <gbroner@codeaurora.org>
2014-10-12 19:13:50 +0300

    scsi: ufs: commit descriptors before setting the doorbell
    Add a write memory barrier to make sure descriptors prepared are actually
    written to memory before ringing the doorbell. We have also added the
    write memory barrier after ringing the doorbell register so that
    controller sees the new request immediately.
    
    Change-Id: Ifb3fb294eda81a5bccddb05227eade1b7453921a
    Signed-off-by: Gilad Broner <gbroner@codeaurora.org>
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>
    [gbroner@codeaurora.org: fix merge conflict]
    Signed-off-by: Gilad Broner <gbroner@codeaurora.org>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -1457,6 +1457,8 @@ void ufshcd_send_command(struct ufs_hba *hba, unsigned int task_tag)
 	ufshcd_clk_scaling_start_busy(hba);
 	__set_bit(task_tag, &hba->outstanding_reqs);
 	ufshcd_writel(hba, 1 << task_tag, REG_UTP_TRANSFER_REQ_DOOR_BELL);
+	/* Make sure that doorbell is commited immediately */
+	wmb();
 	UFSHCD_UPDATE_TAG_STATS(hba, task_tag);
 }
 
@@ -2029,6 +2031,8 @@ static int ufshcd_queuecommand(struct Scsi_Host *host, struct scsi_cmnd *cmd)
 		goto out;
 	}
 
+	/* Make sure descriptors are ready before ringing the doorbell */
+	wmb();
 	/* issue command to the controller */
 	spin_lock_irqsave(hba->host->host_lock, flags);
 	ufshcd_send_command(hba, tag);
@@ -2230,6 +2234,8 @@ static int ufshcd_exec_dev_cmd(struct ufs_hba *hba,
 
 	hba->dev_cmd.complete = &wait;
 
+	/* Make sure descriptors are ready before ringing the doorbell */
+	wmb();
 	spin_lock_irqsave(hba->host->host_lock, flags);
 	ufshcd_send_command(hba, tag);
 	spin_unlock_irqrestore(hba->host->host_lock, flags);
