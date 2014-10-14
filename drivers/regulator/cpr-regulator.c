31a41c69fdd7ed98011460a31704b0364c771079
David Collins <collinsd@codeaurora.org>
2014-10-13 17:53:33 -0700

    regulator: cpr-regulator: add support for CPR ceiling voltage reduction
    Add support for a pair of device tree properties which specify
    if the CPR ceiling voltage may be reduced to either the per-fuse-
    corner initial voltage fuse value or the per-virtual-corner
    interpolated initial voltage value (i.e. the open-loop voltage).
    
    These properties can be used to save power on targets where the
    closed-loop settled CPR voltage never needs to exceed the
    open-loop initial voltage.
    
    Change-Id: I359b6d8e0c98f4b7f00f7bd90efc72c2aa3865a7
    Signed-off-by: David Collins <collinsd@codeaurora.org>

--- drivers/regulator/cpr-regulator.c
+++ drivers/regulator/cpr-regulator.c
@@ -1785,6 +1785,43 @@ static int cpr_populate_opp_table(struct cpr_regulator *cpr_vreg,
 }
 
 /*
+ * Conditionally reduce the per-virtual-corner ceiling voltages if certain
+ * device tree flags are present.  This must be called only after the ceiling
+ * array has been initialized and the open_loop_volt array values have been
+ * initialized and limited to the existing floor to ceiling voltage range.
+ */
+static int cpr_reduce_ceiling_voltage(struct cpr_regulator *cpr_vreg,
+				struct device *dev)
+{
+	bool reduce_to_fuse_open_loop, reduce_to_interpolated_open_loop;
+	int i;
+
+	reduce_to_fuse_open_loop = of_property_read_bool(dev->of_node,
+				"qcom,cpr-init-voltage-as-ceiling");
+	reduce_to_interpolated_open_loop = of_property_read_bool(dev->of_node,
+				"qcom,cpr-scaled-init-voltage-as-ceiling");
+
+	if (!reduce_to_fuse_open_loop && !reduce_to_interpolated_open_loop)
+		return 0;
+
+	for (i = CPR_CORNER_MIN; i <= cpr_vreg->num_corners; i++) {
+		if (reduce_to_interpolated_open_loop &&
+		    cpr_vreg->open_loop_volt[i] < cpr_vreg->ceiling_volt[i])
+			cpr_vreg->ceiling_volt[i] = cpr_vreg->open_loop_volt[i];
+		else if (reduce_to_fuse_open_loop &&
+				cpr_vreg->pvs_corner_v[cpr_vreg->corner_map[i]]
+				< cpr_vreg->ceiling_volt[i])
+			cpr_vreg->ceiling_volt[i]
+				= max((u32)cpr_vreg->floor_volt[i],
+			       cpr_vreg->pvs_corner_v[cpr_vreg->corner_map[i]]);
+		cpr_debug(cpr_vreg, "lowered ceiling[%d] = %d uV\n",
+			i, cpr_vreg->ceiling_volt[i]);
+	}
+
+	return 0;
+}
+
+/*
  * cpr_get_corner_quot_adjustment() -- get the quot_adjust for each corner.
  *
  * Get the virtual corner to fuse corner mapping and virtual corner to APC clock
@@ -2699,6 +2736,11 @@ static int cpr_init_cpr(struct platform_device *pdev,
 	 * open-loop voltage pairs.
 	 */
 	rc = cpr_populate_opp_table(cpr_vreg, &pdev->dev);
+	if (rc)
+		return rc;
+
+	/* Reduce the ceiling voltage if allowed. */
+	rc = cpr_reduce_ceiling_voltage(cpr_vreg, &pdev->dev);
 	if (rc)
 		return rc;
 
