117192b5050f61e21621f95e0d7f80ef54691eed
Jack Pham <jackp@codeaurora.org>
2014-10-15 18:23:37 -0700

    usb: dwc3: gadget: Keep track of IRQ timing statistics
    It may be useful to debug how much time is spent in the
    DWC3 hard IRQ context. Add low-overhead statistics counters
    which keep a histogram of the last 10 interrupts' time of
    occurence, time spent, and number of events read (which will
    get processed in the bottom half handler).
    
    Change-Id: I466144d8141f4236b9fb834dded93225b56d0422
    Signed-off-by: Jack Pham <jackp@codeaurora.org>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -3476,7 +3476,10 @@ static irqreturn_t dwc3_interrupt(int irq, void *_dwc)
 	struct dwc3			*dwc = _dwc;
 	int				i;
 	irqreturn_t			ret = IRQ_NONE;
+	unsigned			temp_cnt = 0;
+	ktime_t				start_time;
 
+	start_time = ktime_get();
 	spin_lock(&dwc->lock);
 
 	if (dwc->err_evt_seen) {
@@ -3491,9 +3494,17 @@ static irqreturn_t dwc3_interrupt(int irq, void *_dwc)
 		status = dwc3_process_event_buf(dwc, i);
 		if (status == IRQ_WAKE_THREAD)
 			ret = status;
+
+		temp_cnt += dwc->ev_buffs[i]->count;
 	}
 
 	spin_unlock(&dwc->lock);
+
+	dwc->irq_start_time[dwc->irq_dbg_index] = start_time;
+	dwc->irq_completion_time[dwc->irq_dbg_index] =
+		ktime_us_delta(ktime_get(), start_time);
+	dwc->irq_event_count[dwc->irq_dbg_index] = temp_cnt / 4;
+	dwc->irq_dbg_index = (dwc->irq_dbg_index + 1) % MAX_INTR_STATS;
 
 	if (ret == IRQ_WAKE_THREAD) {
 		disable_irq_nosync(irq);
