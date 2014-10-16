52b472e6dfe0768fb396e929f6c2c3d3c05a18ae
Jordan Crouse <jcrouse@codeaurora.org>
2014-10-16 16:42:23 -0600

    msm: kgsl: Do not allow a fence to be created on an invalid context
    Don't allow a new fence to be created on an invalidated context thus
    preventing fence timeouts for timestamps that are never going to
    happen.
    
    Change-Id: Ic0dedbad3b9e47f14f0bdce748159c1ddd8c1a26
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/kgsl_sync.c
+++ drivers/gpu/msm/kgsl_sync.c
@@ -170,6 +170,13 @@ int kgsl_add_fence_event(struct kgsl_device *device,
 	if (context == NULL)
 		goto unlock;
 
+	if (test_bit(KGSL_CONTEXT_PRIV_INVALID, &context->priv)) {
+		KGSL_DRV_ERR(device,
+			"Cannot create a fence: context %d is invalid\n",
+			context->id);
+		goto unlock;
+	}
+
 	pt = kgsl_sync_pt_create(context->timeline, context, timestamp);
 	if (pt == NULL) {
 		KGSL_DRV_ERR(device, "kgsl_sync_pt_create failed\n");
