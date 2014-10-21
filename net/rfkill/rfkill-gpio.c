9b716de6408f32054874d0fa9cb058e810134ca8
Loic Poulain <loic.poulain@intel.com>
2014-10-21 02:54:22 +0200

    net: rfkill: gpio: Add BCM2E3A ACPI ID
    Add BCM2E3A ID to support BCM43340 Bluetooth chip.
    This chip supports wake & host wkae.
    
    Signed-off-by: Loic Poulain <loic.poulain@intel.com>

--- net/rfkill/rfkill-gpio.c
+++ net/rfkill/rfkill-gpio.c
@@ -333,6 +333,7 @@ static const struct acpi_device_id rfkill_acpi_match[] = {
 	{ "BCM2E1A", (kernel_ulong_t)&acpi_default_bluetooth },
 	{ "BCM2E39", (kernel_ulong_t)&acpi_default_bluetooth },
 	{ "BCM2E3D", (kernel_ulong_t)&acpi_default_bluetooth },
+	{ "BCM2E3A", (kernel_ulong_t)&acpi_bluetooth_wake },
 	{ "OBDA8723", (kernel_ulong_t)&acpi_bluetooth_wake },
 	{ "BCM4752", (kernel_ulong_t)&acpi_default_gps },
 	{ "LNV4752", (kernel_ulong_t)&acpi_default_gps },
