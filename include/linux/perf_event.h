676f8c4b5a71b93c2e1ae34456d965d7d6ced606
Neil Leeder <nleeder@codeaurora.org>
2014-10-21 16:40:12 -0400

    perf: export perf_create_debug_dir func prototype
    Add the exported function into the header so it can
    be used by other drivers.
    
    Change-Id: Ic1c2fba3efa1d091033d66e25ec59627134b065f
    Signed-off-by: Neil Leeder <nleeder@codeaurora.org>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -552,6 +552,7 @@ extern void perf_pmu_migrate_context(struct pmu *pmu,
 extern u64 perf_event_read_value(struct perf_event *event,
 				 u64 *enabled, u64 *running);
 
+extern struct dentry *perf_create_debug_dir(void);
 
 struct perf_sample_data {
 	u64				type;
