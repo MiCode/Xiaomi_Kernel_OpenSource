81aa63f72705950253fbd15428fe9e7a4ad5b5ef
Neil Leeder <nleeder@codeaurora.org>
2014-10-21 16:35:11 -0400

    Perf: arm64: make debug dir handle exportable
    The perf debug directory is exported so that other
    drivers can place nodes there.
    
    Change-Id: I570f4b61267ce4ef20275852ec769464b7f68827
    Signed-off-by: Neil Leeder <nleeder@codeaurora.org>

--- arch/arm64/kernel/perf_event.c
+++ arch/arm64/kernel/perf_event.c
@@ -1899,6 +1899,15 @@ unsigned long perf_misc_flags(struct pt_regs *regs)
 	return misc;
 }
 
+static struct dentry *perf_debug_dir;
+
+struct dentry *perf_create_debug_dir(void)
+{
+	if (!perf_debug_dir)
+		perf_debug_dir = debugfs_create_dir("msm_perf", NULL);
+	return perf_debug_dir;
+}
+
 #ifdef CONFIG_PERF_EVENTS_RESET_PMU_DEBUGFS
 static __ref void reset_pmu_force(void)
 {
@@ -1944,15 +1953,13 @@ int __init init_pmu_actions(void)
 	struct dentry *file;
 	unsigned int value = 1;
 
-	dir = debugfs_create_dir("msm_perf", NULL);
+	dir = perf_create_debug_dir();
 	if (!dir)
 		return -ENOMEM;
 	file = debugfs_create_file("resetpmu", 0220, dir,
 		&value, &fops_pmuaction);
-	if (!file) {
-		debugfs_remove(dir);
+	if (!file)
 		return -ENOMEM;
-	}
 	return 0;
 }
 #else
