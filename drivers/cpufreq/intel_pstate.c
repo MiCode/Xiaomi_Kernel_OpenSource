bc3af81fc35d3dbf6fa91555f008ca0dbf49f7d7
Zhongmin Wu <zhongmin.wu@intel.com>
2014-10-23 15:24:12 +0800

    Revert "intel_pstate: Correct rounding in busy calculation"
    This reverts commit f4e368a53c65f8acd462b2b038c18a21d30eaeb8.

--- drivers/cpufreq/intel_pstate.c
+++ drivers/cpufreq/intel_pstate.c
@@ -214,7 +214,10 @@ static signed int pid_calc(struct _pid *pid, int32_t busy)
 	pid->last_err = fp_error;
 
 	result = pterm + mul_fp(pid->integral, pid->i_gain) + dterm;
-	result = result + (1 << (FRAC_BITS-1));
+	if (result >= 0)
+		result = result + (1 << (FRAC_BITS-1));
+	else
+		result = result - (1 << (FRAC_BITS-1));
 	return (signed int)fp_toint(result);
 }
 
