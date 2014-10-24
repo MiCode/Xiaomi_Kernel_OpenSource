1e095a5bdb641ff8126f1380e3f0b9db9ab64bd1
Sagar Dharia <sdharia@codeaurora.org>
2014-10-24 17:35:49 -0600

    slim_ngd: Use DSP before-shutdown notification for DSP SSR
    QMI_SERVER_EXIT notification comes too late (after the DSP has
    shutdown). This can result in unclocked access by BAM if there was
    in-flight transaction. Use BEFORE_SHUTDOWN notification of SSR
    framework instead to make sure in-flight transaction is finished
    before DSP turns off its resources.
    
    CRs-Fixed: 730552
    Change-Id: If3c2787c74ffa7743772e5e37eac2f768e239050
    Signed-off-by: Sagar Dharia <sdharia@codeaurora.org>

--- drivers/slimbus/slim-msm.h
+++ drivers/slimbus/slim-msm.h
@@ -214,7 +214,7 @@ struct msm_slim_qmi {
 	struct work_struct		ssr_up;
 };
 
-struct msm_slim_mdm {
+struct msm_slim_ss {
 	struct notifier_block nb;
 	void *ssr;
 	enum msm_ctrl_state state;
@@ -269,7 +269,8 @@ struct msm_slim_ctrl {
 	u32			ver;
 	struct msm_slim_qmi	qmi;
 	struct msm_slim_pdata	pdata;
-	struct msm_slim_mdm	mdm;
+	struct msm_slim_ss	ext_mdm;
+	struct msm_slim_ss	dsp;
 	int			default_ipc_log_mask;
 	int			ipc_log_mask;
 	bool			sysfs_created;
