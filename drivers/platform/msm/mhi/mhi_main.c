a71b10255dd6311dd54ab10d3d21783968a12ea9
Andrei Danaila <adanaila@codeaurora.org>
2014-10-29 11:44:36 -0700

    msm: mhi: Reorder operations on MHI TRE
    Reorder operations on MHI TRE to prevent a race condition
    of publishing the TRE to the device before the TRE is fully
    populated.
    
    CRs-Fixed: 726655
    Change-Id: Ide208c77c93f0e68eee0c6c304eaebc451028341
    Signed-off-by: Andrei Danaila <adanaila@codeaurora.org>

--- drivers/platform/msm/mhi/mhi_main.c
+++ drivers/platform/msm/mhi/mhi_main.c
@@ -562,7 +562,6 @@ enum MHI_STATUS mhi_queue_xfer(struct mhi_client_handle *client_handle,
 	enum MHI_CLIENT_CHANNEL chan;
 	struct mhi_device_ctxt *mhi_dev_ctxt;
 	unsigned long flags;
-	uintptr_t trb_index;
 
 	if (NULL == client_handle || !VALID_CHAN_NR(client_handle->chan) ||
 		0 == buf || 0 == buf_len) {
@@ -583,21 +582,10 @@ enum MHI_STATUS mhi_queue_xfer(struct mhi_client_handle *client_handle,
 		mhi_assert_device_wake(mhi_dev_ctxt);
 	read_unlock_irqrestore(&mhi_dev_ctxt->xfer_lock, flags);
 
-	/* Add the TRB to the correct transfer ring */
-	ret_val = ctxt_add_element(&mhi_dev_ctxt->mhi_local_chan_ctxt[chan],
-				(void *)&pkt_loc);
-	if (unlikely(MHI_STATUS_SUCCESS != ret_val)) {
-		mhi_log(MHI_MSG_CRITICAL,
-				"Failed to insert trb in xfer ring\n");
-		goto error;
-	}
-
+	pkt_loc = mhi_dev_ctxt->mhi_local_chan_ctxt[chan].wp;
 	pkt_loc->data_tx_pkt.buffer_ptr = buf;
-
-	get_element_index(&mhi_dev_ctxt->mhi_local_chan_ctxt[chan],
-				pkt_loc, &trb_index);
-
 	pkt_loc->type.info = mhi_flags;
+
 	if (likely(0 != client_handle->intmod_t))
 		MHI_TRB_SET_INFO(TX_TRB_BEI, pkt_loc, 1);
 	else
@@ -609,12 +597,22 @@ enum MHI_STATUS mhi_queue_xfer(struct mhi_client_handle *client_handle,
 		"Channel %d Has buf size of %d and buf addr %lx, flags 0x%x\n",
 				chan, buf_len, (uintptr_t)buf, mhi_flags);
 
+	/* Add the TRB to the correct transfer ring */
+	ret_val = ctxt_add_element(&mhi_dev_ctxt->mhi_local_chan_ctxt[chan],
+				(void *)&pkt_loc);
+	if (unlikely(MHI_STATUS_SUCCESS != ret_val)) {
+		mhi_log(MHI_MSG_CRITICAL,
+				"Failed to insert trb in xfer ring\n");
+		goto error;
+	}
+
 	if (chan % 2 == 0) {
 		atomic_inc(&mhi_dev_ctxt->counters.outbound_acks);
 		mhi_log(MHI_MSG_VERBOSE,
 			"Queued outbound pkt. Pending Acks %d\n",
 		atomic_read(&mhi_dev_ctxt->counters.outbound_acks));
 	}
+
 	mhi_notify_device(mhi_dev_ctxt, chan);
 	atomic_dec(&mhi_dev_ctxt->flags.data_pending);
 	return MHI_STATUS_SUCCESS;
