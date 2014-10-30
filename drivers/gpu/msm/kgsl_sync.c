e392445c310e22467cbf2f7ea34509de96dc0bf8
Jordan Crouse <jcrouse@codeaurora.org>
2014-10-30 10:08:09 -0600

    msm: kgsl: Add a file count to struct kgsl_process_private
    Some process specific resources need to be released when all of the
    open files for a process are closed including memory objects,
    syncsources and the sysfs/debugfs entries. Currently these
    resources are released when the first file is closed which causes
    obvious problems for processes with multiple open file descriptors.
    Use a file descriptor count to keep track of how many files are
    open for the process and release the resources when the last one
    goes away.
    
    Note that struct kgsl_process_private does double duty - it also
    contains the memory space information for the process (including
    the pagetable) and that might have a longer lifespan beyond
    the life of the file descriptors. In an ideal world we would seperate
    out all of the other stuff into a different container but that
    would involve a LOT of struct renaming and other sundry code
    editing. For now this should give us the stability we crave.
    
    CRs-Fixed: 740433
    Change-Id: Ic0dedbadb09ecb4ff70777377544ddef256576dd
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/kgsl_sync.c
+++ drivers/gpu/msm/kgsl_sync.c
@@ -476,8 +476,12 @@ long kgsl_ioctl_syncsource_create(struct kgsl_device_private *dev_priv,
 		goto out;
 	}
 
-	mutex_lock(&private->process_private_mutex);
-	id = idr_alloc(&private->syncsource_idr, syncsource, 1, 0, GFP_KERNEL);
+	idr_preload(GFP_KERNEL);
+	spin_lock(&private->syncsource_lock);
+	id = idr_alloc(&private->syncsource_idr, syncsource, 1, 0, GFP_NOWAIT);
+	spin_unlock(&private->syncsource_lock);
+	idr_preload_end();
+
 	if (id > 0) {
 		kref_init(&syncsource->refcount);
 		syncsource->id = id;
@@ -488,7 +492,7 @@ long kgsl_ioctl_syncsource_create(struct kgsl_device_private *dev_priv,
 	} else {
 		ret = id;
 	}
-	mutex_unlock(&private->process_private_mutex);
+
 out:
 	if (ret) {
 		if (syncsource && syncsource->oneshot)
@@ -505,13 +509,13 @@ kgsl_syncsource_get(struct kgsl_process_private *private, int id)
 	int result = 0;
 	struct kgsl_syncsource *syncsource = NULL;
 
-	mutex_lock(&private->process_private_mutex);
+	spin_lock(&private->syncsource_lock);
 
 	syncsource = idr_find(&private->syncsource_idr, id);
 	if (syncsource)
 		result = kref_get_unless_zero(&syncsource->refcount);
 
-	mutex_unlock(&private->process_private_mutex);
+	spin_unlock(&private->syncsource_lock);
 
 	return result ? syncsource : NULL;
 }
@@ -524,13 +528,13 @@ static void kgsl_syncsource_destroy(struct kref *kref)
 
 	struct kgsl_process_private *private = syncsource->private;
 
-	mutex_lock(&private->process_private_mutex);
+	spin_lock(&private->syncsource_lock);
 	if (syncsource->id != 0) {
 		idr_remove(&private->syncsource_idr, syncsource->id);
 		syncsource->id = 0;
 	}
 	oneshot_timeline_destroy(syncsource->oneshot);
-	mutex_unlock(&private->process_private_mutex);
+	spin_unlock(&private->syncsource_lock);
 
 	kfree(syncsource);
 }
@@ -556,10 +560,10 @@ long kgsl_ioctl_syncsource_destroy(struct kgsl_device_private *dev_priv,
 
 	private = syncsource->private;
 
-	mutex_lock(&private->process_private_mutex);
+	spin_lock(&private->syncsource_lock);
 	idr_remove(&private->syncsource_idr, param->id);
 	syncsource->id = 0;
-	mutex_unlock(&private->process_private_mutex);
+	spin_unlock(&private->syncsource_lock);
 
 	/* put reference from syncsource creation */
 	kgsl_syncsource_put(syncsource);
