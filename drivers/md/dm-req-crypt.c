8be3bfa1fa9fc8310e5672ce0a1248d390cdfe34
Dinesh K Garg <dineshg@codeaurora.org>
2014-10-30 10:38:14 -0700

    md: dm-req-crypt Recalculate nr_phys_segments
    Recalculate nr_phys_segments after pages are allocated
    for write requests. Move _req_crypt_io_pool allocation
    and de-allocation to ctr and dtr instead of driver init
    and exit.
    
    Change-Id: I8576dce1f7c9bc39dcc975762562fb84a349bba7
    Acked-by: Baranidharan Muthukumaran <bmuthuku@qti.qualcomm.com>
    Signed-off-by: Dinesh K Garg <dineshg@codeaurora.org>

--- drivers/md/dm-req-crypt.c
+++ drivers/md/dm-req-crypt.c
@@ -605,6 +605,11 @@ static void req_cryptd_crypt_write_convert(struct req_dm_crypt_io *io)
 		blk_queue_bounce(clone->q, &bio_src);
 	}
 
+	/*
+	 * Recalculate the phy_segments as we allocate new pages
+	 * This is used by storage driver to fill the sg list.
+	 */
+	blk_recalc_rq_segments(clone);
 
 ablkcipher_req_alloc_failure:
 	if (req)
@@ -867,6 +872,8 @@ static void req_crypt_dtr(struct dm_target *ti)
 		destroy_workqueue(req_crypt_queue);
 		req_crypt_queue = NULL;
 	}
+
+	kmem_cache_destroy(_req_crypt_io_pool);
 	if (dev) {
 		dm_put_device(ti, dev);
 		dev = NULL;
@@ -934,6 +941,12 @@ static int req_crypt_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 	} else {
 		DMERR(" %s Arg[5] missing, set FDE enabled.\n", __func__);
 		is_fde_enabled = true; /* backward compatible */
+	}
+
+	_req_crypt_io_pool = KMEM_CACHE(req_dm_crypt_io, 0);
+	if (!_req_crypt_io_pool) {
+		err =  DM_REQ_CRYPT_ERROR;
+		goto ctr_exit;
 	}
 
 	req_crypt_queue = alloc_workqueue("req_cryptd",
@@ -1060,14 +1073,11 @@ static int __init req_dm_crypt_init(void)
 {
 	int r;
 
-	_req_crypt_io_pool = KMEM_CACHE(req_dm_crypt_io, 0);
-	if (!_req_crypt_io_pool)
-		return -ENOMEM;
 
 	r = dm_register_target(&req_crypt_target);
 	if (r < 0) {
 		DMERR("register failed %d", r);
-		kmem_cache_destroy(_req_crypt_io_pool);
+		return r;
 	}
 
 	DMINFO("dm-req-crypt successfully initalized.\n");
@@ -1077,7 +1087,6 @@ static int __init req_dm_crypt_init(void)
 
 static void __exit req_dm_crypt_exit(void)
 {
-	kmem_cache_destroy(_req_crypt_io_pool);
 	dm_unregister_target(&req_crypt_target);
 }
 
