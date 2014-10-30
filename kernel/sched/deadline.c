45477675483bf3c2c56118e0d6edb70848677c44
Wanpeng Li <wanpeng.li@linux.intel.com>
2014-10-31 06:39:36 +0800

    sched/deadline: Don't check CONFIG_SMP in switched_from_dl()
    There are both UP and SMP version of pull_dl_task(), so don't need
    to check CONFIG_SMP in switched_from_dl();
    
    Signed-off-by: Wanpeng Li <wanpeng.li@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@arm.com>
    Cc: Kirill Tkhai <ktkhai@parallels.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Link: http://lkml.kernel.org/r/1414708776-124078-6-git-send-email-wanpeng.li@linux.intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit cad3bb32e181c286c46ec12b2deb1f26a6f9835d)
    Signed-off-by: Punit Agrawal <punit.agrawal@arm.com>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -1596,7 +1596,6 @@ static void switched_from_dl(struct rq *rq, struct task_struct *p)
 
 	__dl_clear_params(p);
 
-#ifdef CONFIG_SMP
 	/*
 	 * Since this might be the only -deadline task on the rq,
 	 * this is the right place to try to pull some other one
@@ -1607,7 +1606,6 @@ static void switched_from_dl(struct rq *rq, struct task_struct *p)
 
 	if (pull_dl_task(rq))
 		resched_curr(rq);
-#endif
 }
 
 /*
