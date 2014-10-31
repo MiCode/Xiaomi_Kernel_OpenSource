4fbfb76a3526f3bcff63384fd870a492b03f40e0
Dmitry Shmidt <dimitrysh@google.com>
2014-10-31 16:05:46 -0700

    ANDROID: power: wakeup_reason: Add check_wakeup_reason() to verify wakeup source irq
    Wakeup reason is set before driver resume handlers are called.
    It is cleared before driver suspend handlers are called, on
    PM_SUSPEND_PREPARE.
    
    Change-Id: I04218c9b0c115a7877e8029c73e6679ff82e0aa4
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- include/linux/wakeup_reason.h
+++ include/linux/wakeup_reason.h
@@ -21,6 +21,8 @@
 #define MAX_SUSPEND_ABORT_LEN 256
 
 void log_wakeup_reason(int irq);
+int check_wakeup_reason(int irq);
+
 #ifdef CONFIG_SUSPEND
 void log_suspend_abort_reason(const char *fmt, ...);
 #else
