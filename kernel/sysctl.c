6e778f0cdcceb40cf0d924cb2e645dfed1e0e851
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-10-31 16:04:00 -0700

    sched: Provide knob to prefer mostly_idle over idle cpus
    sysctl_sched_prefer_idle lets the scheduler bias selection of
    idle cpus over mostly idle cpus for tasks. This knob could be
    useful to control balance between power and performance.
    
    Change-Id: Ide6eef684ef94ac8b9927f53c220ccf94976fe67
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -410,6 +410,13 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= proc_dointvec,
 	},
 	{
+		.procname	= "sched_prefer_idle",
+		.data		= &sysctl_sched_prefer_idle,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
+	},
+	{
 		.procname	= "sched_init_task_load",
 		.data		= &sysctl_sched_init_task_load_pct,
 		.maxlen		= sizeof(unsigned int),
