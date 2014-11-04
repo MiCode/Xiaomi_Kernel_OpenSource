0fcaa0215fd41d800c948e125f5d70e383843ac5
Deepak Katragadda <dkatraga@codeaurora.org>
2014-11-04 15:39:02 -0800

    soc: qcom: pil-msa: Shift storing firmware size
    Move storing the firmware size till after the check for
    storing the firmware data. This change is to avoid any
    kind of NULL pointer dereference from happening.
    
    Change-Id: I876b7cdae99cddd9c2479ab8ac78649c7f8b3b91
    Signed-off-by: Deepak Katragadda <dkatraga@codeaurora.org>

--- drivers/soc/qcom/pil-msa.c
+++ drivers/soc/qcom/pil-msa.c
@@ -422,13 +422,13 @@ int pil_mss_reset_load_mba(struct pil_desc *pil)
 	dev_info(pil->dev, "MBA: loading from %pa to %pa\n", &mba_phys,
 								&mba_phys_end);
 	/* Load the MBA image into memory */
-	count = fw->size;
 	data = fw ? fw->data : NULL;
 	if (!data) {
 		dev_err(pil->dev, "MBA data is NULL\n");
 		ret = -ENOMEM;
 		goto err_mss_reset;
 	}
+	count = fw->size;
 	memcpy(mba_virt, data, count);
 	wmb();
 
