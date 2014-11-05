51c81d3a999b0a9e35d347e878a34dc572cd5771
Skylar Chang <chiaweic@codeaurora.org>
2014-11-05 12:43:43 -0800

    msm: ipa: Move IPv4 filtering table to DDR
    Move IPv4 filtering table to DDR for IPA v2.5
    for Ethernet Bridging.
    
    Change-Id: I71e8308d5b4f0c82d4dc391532d51c84a003febb
    Acked-by: Shihuan Liu <shihuanl@qti.qualcomm.com>
    Signed-off-by: Skylar Chang <chiaweic@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_utils.c
+++ drivers/platform/msm/ipa/ipa_utils.c
@@ -627,7 +627,7 @@ void _ipa_sram_settings_read_v2_5(void)
 	}
 	ipa_ctx->ip4_rt_tbl_lcl = 0;
 	ipa_ctx->ip6_rt_tbl_lcl = 0;
-	ipa_ctx->ip4_flt_tbl_lcl = 1;
+	ipa_ctx->ip4_flt_tbl_lcl = 0;
 	ipa_ctx->ip6_flt_tbl_lcl = 0;
 }
 
