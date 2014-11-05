1a99a30535b2357eb061f25f5a1d0056534e5891
Vikram Mulukutla <markivx@codeaurora.org>
2014-11-04 22:43:40 -0800

    soc: qcom: peripheral-loader: Don't use virt addresses in do_elf_ramdump
    Relocatable PIL ensures that there is no kernel mapping
    present for PIL images, in order to prevent accesses to
    those regions whilst they are owned by the subsystem.
    Therefore don't attempt to pass in a non-existent mapping
    to the ramdump API.
    
    Also ensure that the segment structures are allocated
    with kcalloc instead of kmalloc_array in order to ensure
    zeroing out of other fields in the segment structures.
    
    A future improvement would be to pass in the device pointer
    used to do the dma_remap by peripheral-loader (while actually
    loading the image) to the ramdump API to actually allow the
    latter to do the remap on its own.
    
    Change-Id: I182ed375b8588f775df7135e1d6699bc53ffccb7
    Signed-off-by: Vikram Mulukutla <markivx@codeaurora.org>

--- drivers/soc/qcom/peripheral-loader.c
+++ drivers/soc/qcom/peripheral-loader.c
@@ -79,7 +79,6 @@ struct pil_mdt {
  * struct pil_seg - memory map representing one segment
  * @next: points to next seg mentor NULL if last segment
  * @paddr: physical start address of segment
- * @vaddr: virtual start address of segment
  * @sz: size of segment
  * @filesz: size of segment on disk
  * @num: segment number
@@ -90,7 +89,6 @@ struct pil_mdt {
  */
 struct pil_seg {
 	phys_addr_t paddr;
-	void *vaddr;
 	unsigned long sz;
 	unsigned long filesz;
 	int num;
@@ -154,14 +152,13 @@ int pil_do_ramdump(struct pil_desc *desc, void *ramdump_dev)
 	list_for_each_entry(seg, &priv->segs, list)
 		count++;
 
-	ramdump_segs = kmalloc_array(count, sizeof(*ramdump_segs), GFP_KERNEL);
+	ramdump_segs = kcalloc(count, sizeof(*ramdump_segs), GFP_KERNEL);
 	if (!ramdump_segs)
 		return -ENOMEM;
 
 	s = ramdump_segs;
 	list_for_each_entry(seg, &priv->segs, list) {
 		s->address = seg->paddr;
-		s->v_address = seg->vaddr;
 		s->size = seg->sz;
 		s++;
 	}
@@ -296,8 +293,6 @@ static struct pil_seg *pil_init_seg(const struct pil_desc *desc,
 		return ERR_PTR(-ENOMEM);
 	seg->num = num;
 	seg->paddr = reloc ? pil_reloc(priv, phdr->p_paddr) : phdr->p_paddr;
-	seg->vaddr = reloc ? priv->region +
-			(seg->paddr - priv->region_start) : 0;
 	seg->filesz = phdr->p_filesz;
 	seg->sz = phdr->p_memsz;
 	seg->relocated = reloc;
