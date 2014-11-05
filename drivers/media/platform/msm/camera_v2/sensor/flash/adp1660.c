daad59f37ead54d531385de80d45e4f4bda7aae2
Mayank Parashar <mayankp@codeaurora.org>
2014-11-05 16:12:02 +0530

    msm: camera: Enable LED Flash support on 8909
    Add support for LED flash as I2C device on 8909.
    
    Change-Id: I671aedb28c925ce9633ef5246d54bd1d259d07b8
    Signed-off-by: Mayank Parashar <mayankp@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/sensor/flash/adp1660.c
+++ drivers/media/platform/msm/camera_v2/sensor/flash/adp1660.c
@@ -118,11 +118,20 @@ static struct platform_driver adp1660_platform_driver = {
 static int __init msm_flash_adp1660_init_module(void)
 {
 	int32_t rc = 0;
+
 	rc = platform_driver_register(&adp1660_platform_driver);
-	if (!rc)
+	if (fctrl.pdev != NULL && rc == 0) {
+		pr_err("adp1660 platform_driver_register success");
 		return rc;
-	pr_debug("%s:%d rc %d\n", __func__, __LINE__, rc);
-	return i2c_add_driver(&adp1660_i2c_driver);
+	} else if (rc != 0) {
+		pr_err("adp1660 platform_driver_register failed");
+		return rc;
+	} else {
+		rc = i2c_add_driver(&adp1660_i2c_driver);
+		if (!rc)
+			pr_err("adp1660 i2c_add_driver success");
+	}
+	return rc;
 }
 
 static void __exit msm_flash_adp1660_exit_module(void)
