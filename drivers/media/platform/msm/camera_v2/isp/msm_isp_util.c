5facb5c3b08a5176b1b356c39db800ea2284ad0f
Sreesudhan Ramakrish Ramkumar <srramku@codeaurora.org>
2014-11-06 10:47:04 -0800

    msm: camera: isp: Process stats buffer followed by AXI buffer
    If stats irq and axi irq comes together, currently axi buffer
    is processed first followed by stats buffer. This causes stats
    processing geting delayed which affects auto focus performance.
    Process stats buffer first to fix the issue.
    
    Change-Id: I497cbd42c270a1c76328f9e50e34a94e484b4894
    Signed-off-by: Sreesudhan Ramakrish Ramkumar <srramku@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
@@ -1645,9 +1645,9 @@ void msm_isp_do_tasklet(unsigned long data)
 		msm_isp_process_error_info(vfe_dev);
 		irq_ops->process_camif_irq(vfe_dev,
 			irq_status0, irq_status1, &ts);
-		irq_ops->process_axi_irq(vfe_dev,
-			irq_status0, irq_status1, &ts);
 		irq_ops->process_stats_irq(vfe_dev,
+			irq_status0, irq_status1, &ts);
+		irq_ops->process_axi_irq(vfe_dev,
 			irq_status0, irq_status1, &ts);
 		irq_ops->process_reg_update(vfe_dev,
 			irq_status0, irq_status1, &ts);
