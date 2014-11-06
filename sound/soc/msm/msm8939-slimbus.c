435aa7a7a0be7107dbbcd944eaca8fe7867a9b3c
Meng Wang <mwang@codeaurora.org>
2014-11-06 17:08:56 +0800

    ASoC: msm: add Quaternary MI2S RX Hostless FE support
    Add a Hostless Front-end DAI for Quaternary MI2S port.
    When external PA is connected to Quaternary MI2S, the
    Hostless Front-end DAI is needed for FM playback.
    
    CRs-Fixed: 753254
    Change-Id: I0d6e467ffb90031f4558a5ba10bcdb8e4a3d1461
    Signed-off-by: Meng Wang <mwang@codeaurora.org>

--- sound/soc/msm/msm8939-slimbus.c
+++ sound/soc/msm/msm8939-slimbus.c
@@ -2177,6 +2177,21 @@ static struct snd_soc_dai_link msm8x16_dai[] = {
 		.codec_dai_name = "snd-soc-dummy-dai",
 		.codec_name = "snd-soc-dummy",
 	},
+	{ /* hw:x,27 */
+		.name = "QUAT_MI2S Hostless",
+		.stream_name = "QUAT_MI2S Hostless",
+		.cpu_dai_name = "QUAT_MI2S_RX_HOSTLESS",
+		.platform_name = "msm-pcm-hostless",
+		.dynamic = 1,
+		.trigger = {SND_SOC_DPCM_TRIGGER_POST,
+			SND_SOC_DPCM_TRIGGER_POST},
+		.no_host_mode = SND_SOC_DAI_LINK_NO_HOST,
+		.ignore_suspend = 1,
+		/* this dainlink has playback support */
+		.ignore_pmdown_time = 1,
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.codec_name = "snd-soc-dummy",
+	},
 	{
 		.name = LPASS_BE_QUAT_MI2S_RX,
 		.stream_name = "Quaternary MI2S Playback",
