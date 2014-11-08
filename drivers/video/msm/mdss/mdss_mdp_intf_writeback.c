e2b78299dd02bb591d434d3a7c53b2c3e8fc1d8f
Veera Sundaram Sankaran <veeras@codeaurora.org>
2014-11-07 16:30:39 -0800

    msm: mdss: fix possible null dereferencing in interface writeback
    Added null check to avoid possible null pointer dereferencing.
    
    Change-Id: Iedafe6e308b7b4a1d43efa7c37a26afd16fa1989
    Signed-off-by: Veera Sundaram Sankaran <veeras@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_intf_writeback.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_writeback.c
@@ -232,7 +232,7 @@ static int mdss_mdp_writeback_format_setup(struct mdss_mdp_writeback_ctx *ctx,
 	outsize = (ctx->dst_rect.h << 16) | ctx->dst_rect.w;
 
 	if (ctx->type == MDSS_MDP_WRITEBACK_TYPE_ROTATOR &&
-			mdata->has_rot_dwnscale) {
+			mdata && mdata->has_rot_dwnscale) {
 		dnsc_factor = (ctx->dnsc_factor_h) | (ctx->dnsc_factor_w << 16);
 		mdp_wb_write(ctx, MDSS_MDP_REG_WB_ROTATOR_PIPE_DOWNSCALER,
 								dnsc_factor);
