f5613774dc0cbc106da24fd29d76f1b2fd1b36ae
Hemant Kumar <hemantk@codeaurora.org>
2014-11-08 00:44:27 -0800

    usb: dwc3: Perform core soft reset when erratic event occurs
    Sometimes after receiving erratic event controller is generating
    interrupt storm even if driver acknowledges events generated
    after erratic events. This causes watchdog bark. Hence assert
    core soft reset and wait until erratic event recovery is
    performed which brings the core out of reset for normal
    operation.
    
    CRs-Fixed: 752945
    Change-Id: Ia05bd69404a316ddba5ef205e7495ac50e1815a6
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -786,6 +786,7 @@ struct dwc3_scratchpad_array {
  * @hird_thresh: value to configure in DCTL[HIRD_Thresh]
  * @in_lpm: if 1, indicates that the controller is in low power mode (no clocks)
  * @irq: irq number
+ * @irq_cnt: total irq count
  * @bh: tasklet which handles the interrupt
  * @bh_completion_time: time taken for taklet completion
  * @bh_handled_evt_cnt: no. of events handled by tasklet per interrupt
@@ -907,6 +908,7 @@ struct dwc3 {
 
 	/* offload IRQ handling to tasklet */
 	int			irq;
+	unsigned long		irq_cnt;
 	struct tasklet_struct	bh;
 	unsigned                bh_completion_time[MAX_INTR_STATS];
 	unsigned                bh_handled_evt_cnt[MAX_INTR_STATS];
