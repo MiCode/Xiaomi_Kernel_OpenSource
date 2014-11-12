e52e7cbcf9c9b678bf68ff42705c5f1c607de54f
Girish Mahadevan <girishm@codeaurora.org>
2014-11-12 11:54:08 -0700

    msm: msm_bus: Modify state machine handling for rules engine
    Evaluate every rule for a given node when a bus transaction happens and
    apply the first matched rule allowing for multiple rules to be applicable
    but apply the most restrictive.
    
    Change-Id: I25018ac4260916fd5c42d8a73b886b13a0d2b3a0
    Signed-off-by: Girish Mahadevan <girishm@codeaurora.org>

--- drivers/platform/msm/msm_bus/msm_bus_rules.c
+++ drivers/platform/msm/msm_bus/msm_bus_rules.c
@@ -15,6 +15,7 @@
 #include <linux/msm_bus_rules.h>
 #include <linux/slab.h>
 #include <linux/types.h>
+#include <trace/events/trace_msm_bus.h>
 
 struct node_vote_info {
 	int id;
@@ -207,41 +208,24 @@ static void match_rule(struct rule_update_path_info *inp_node,
 {
 	struct rules_def *rule;
 	int i;
-	bool match_found = false;
-	bool relevant_trans = false;
 
 	list_for_each_entry(rule, &node->node_rules, link) {
 		for (i = 0; i < rule->num_src; i++) {
 			if (rule->src_info[i].id == inp_node->id) {
-				relevant_trans = true;
 				if (check_rule(rule, inp_node)) {
-					node->cur_rule = rule->rule_id;
+					trace_bus_rules_matches(node->cur_rule,
+						inp_node->id, inp_node->ab,
+						inp_node->ib, inp_node->clk);
 					if (rule->state ==
-						RULE_STATE_NOT_APPLIED) {
-						rule->state =
-							RULE_STATE_APPLIED;
+						RULE_STATE_NOT_APPLIED)
 						rule->state_change = true;
-						match_found = true;
-					}
-					break;
+					rule->state = RULE_STATE_APPLIED;
+				} else {
+					if (rule->state ==
+						RULE_STATE_APPLIED)
+						rule->state_change = true;
+					rule->state = RULE_STATE_NOT_APPLIED;
 				}
-			}
-		}
-		if (match_found)
-			break;
-	}
-
-	if (!relevant_trans)
-		return;
-
-	if (!match_found)
-		node->cur_rule = -1;
-
-	list_for_each_entry(rule, &node->node_rules, link) {
-		if (rule->rule_id != node->cur_rule) {
-			if (rule->state == RULE_STATE_APPLIED) {
-				rule->state = RULE_STATE_NOT_APPLIED;
-				rule->state_change = true;
 			}
 		}
 	}
@@ -252,7 +236,12 @@ static void apply_rule(struct rule_node_info *node,
 {
 	struct rules_def *rule;
 
+	node->cur_rule = -1;
 	list_for_each_entry(rule, &node->node_rules, link) {
+		if ((rule->state == RULE_STATE_APPLIED) &&
+						(node->cur_rule == -1))
+			node->cur_rule = rule->rule_id;
+
 		if (node->id == NB_ID) {
 			if (rule->state_change) {
 				rule->state_change = false;
@@ -260,13 +249,14 @@ static void apply_rule(struct rule_node_info *node,
 					rule->state, (void *)&rule->rule_ops);
 			}
 		} else {
-			rule->state_change = false;
-			if ((rule->state == RULE_STATE_APPLIED)) {
+			if ((rule->state == RULE_STATE_APPLIED) &&
+				(node->cur_rule == rule->rule_id)) {
 				node->apply.id = rule->rule_ops.dst_node[0];
 				node->apply.throttle = rule->rule_ops.mode;
 				node->apply.lim_bw = rule->rule_ops.dst_bw;
 				list_add_tail(&node->apply.link, output_list);
 			}
+			rule->state_change = false;
 		}
 	}
 
