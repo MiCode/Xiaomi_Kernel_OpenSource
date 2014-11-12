d01107520eb572cdbfbe71baf641b4a43a5a1a36
Fengguang Wu <fengguang.wu@intel.com>
2014-11-12 21:28:24 +0800

    usb: gadget: midi: f_midi_alloc() can be static
    drivers/usb/gadget/function/f_midi.c:1072:21: sparse: symbol 'f_midi_alloc' was not declared. Should it be static?
    drivers/usb/gadget/legacy/gmidi.c:118:30: sparse: symbol 'fi_midi' was not declared. Should it be static?
    drivers/usb/gadget/legacy/gmidi.c:119:21: sparse: symbol 'f_midi' was not declared. Should it be static?
    
    BACKPORT FROM MAINLINE KERNEL
    
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    Change-Id: I7794cd62da8deb408848e86163747d5ce655f33b

--- drivers/usb/gadget/legacy/gmidi.c
+++ drivers/usb/gadget/legacy/gmidi.c
@@ -116,6 +116,9 @@ static struct usb_gadget_strings *dev_strings[] = {
 	NULL,
 };
 
+static struct usb_function_instance *fi_midi;
+static struct usb_function *f_midi;
+
 static int __exit midi_unbind(struct usb_composite_dev *dev)
 {
 	return 0;
