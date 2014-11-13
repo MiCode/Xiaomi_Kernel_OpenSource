fa22c3d7caa2d465bf5db64779812bac8677d7cb
Junjie Wu <junjiew@codeaurora.org>
2014-11-12 16:20:43 -0800

    cpufreq: interactive: Do not align sample windows by default
    Disable sample window alignment by default to match default behavior
    of upstream interactive governor.
    
    Change-Id: Ibbf4bdd4dd423f97d3a9dd5442eba78b378e66e2
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -1474,7 +1474,6 @@ static struct cpufreq_interactive_tunables *alloc_tunable(
 	tunables->timer_rate = DEFAULT_TIMER_RATE;
 	tunables->boostpulse_duration_val = DEFAULT_MIN_SAMPLE_TIME;
 	tunables->timer_slack_val = DEFAULT_TIMER_SLACK;
-	tunables->align_windows = true;
 
 	spin_lock_init(&tunables->target_loads_lock);
 	spin_lock_init(&tunables->above_hispeed_delay_lock);
