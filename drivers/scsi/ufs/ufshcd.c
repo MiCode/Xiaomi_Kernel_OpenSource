b76dcb847c5adb25954dd2eda45fb19a25e048ab
Maya Erez <merez@codeaurora.org>
2014-11-13 13:31:29 +0200

    scsi: ufs: Set DBD setting in mode sense for caching mode page
    Host sends MODE_SENSE_10 with caching mode page, to check if the device
    supports the cache feature.
    UFS standards requires DBD field to be set to 1.
    
    Some card vendors are more strict and check the DBD field, hence
    respond with CHECK_CONDITION (Sense key set to ILLEGAL_REQUEST and
    ASC set to INVALID FIELD IN CDB).
    As a result of the CHECK_CONDITION response, host assumes that the
    device doesn't support the cache feature and doesn't send
    SYNCHORONIZE_CACHE commands to flush the device cache.
    This can result in data corruption in case of sudden power down, when
    there is data stored in the device cache.
    
    This patch fixes the DBD field setting in case of caching mode page.
    
    Change-Id: I4d63cba26331f04c34e87b99dbdf17c05ab96839
    Signed-off-by: Maya Erez <merez@codeaurora.org>
    [subhashj@codeaurora.org: resolved trivial merge conflicts]
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -8223,6 +8223,7 @@ int ufshcd_init(struct ufs_hba *hba, void __iomem *mmio_base, unsigned int irq)
 	host->max_channel = UFSHCD_MAX_CHANNEL;
 	host->unique_id = host->host_no;
 	host->max_cmd_len = MAX_CDB_SIZE;
+	host->set_dbd_for_caching = 1;
 
 	hba->max_pwr_info.is_valid = false;
 
