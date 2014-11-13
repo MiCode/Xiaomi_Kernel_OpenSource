4787531d28b69be8de9c96bf57dc5246228d9b71
Yingjoe Chen <yingjoe.chen@mediatek.com>
2014-11-13 23:37:05 +0800

    genirq: Add more helper functions to support stacked irq_chip
    Add more helper function for stacked irq_chip to just call parent's
    function.
    
    Signed-off-by: Yingjoe Chen <yingjoe.chen@mediatek.com>
    Cc: Rob Herring <robh+dt@kernel.org>
    Cc: Pawel Moll <pawel.moll@arm.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Matthias Brugger <matthias.bgg@gmail.com>
    Cc: Russell King <linux@arm.linux.org.uk>
    Cc: Jason Cooper <jason@lakedaemon.net>
    Cc: Gran Likely <grant.likely@linaro.org>
    Cc: Boris BREZILLON <boris.brezillon@free-electrons.com>
    Cc: <linux-arm-kernel@lists.infradead.org>
    Cc: Bjorn Helgaas <bhelgaas@google.com>
    Cc: Yijing Wang <wangyijing@huawei.com>
    Cc: <srv_heupstream@mediatek.com>
    Cc: <yingjoe.chen@gmail.com>
    Cc: <hc.yen@mediatek.com>
    Cc: <eddie.huang@mediatek.com>
    Cc: <nathan.chung@mediatek.com>
    Cc: <yh.chen@mediatek.com>
    Cc: Sascha Hauer <kernel@pengutronix.de>
    Cc: Jiang Liu <jiang.liu@linux.intel.com>
    Cc: Marc Zyngier <marc.zyngier@arm.com>
    Link: http://lkml.kernel.org/r/1415893029-2971-3-git-send-email-yingjoe.chen@mediatek.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Git-commit: 56e8abab615e0c5858cfb9fa0015a44641762b9d
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Signed-off-by: Puja Gupta <pujag@codeaurora.org>

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -455,6 +455,12 @@ extern int irq_chip_compose_msi_msg(struct irq_data *data, struct msi_msg *msg);
 #ifdef	CONFIG_IRQ_DOMAIN_HIERARCHY
 extern void irq_chip_ack_parent(struct irq_data *data);
 extern int irq_chip_retrigger_hierarchy(struct irq_data *data);
+extern void irq_chip_mask_parent(struct irq_data *data);
+extern void irq_chip_unmask_parent(struct irq_data *data);
+extern void irq_chip_eoi_parent(struct irq_data *data);
+extern int irq_chip_set_affinity_parent(struct irq_data *data,
+					const struct cpumask *dest,
+					bool force);
 #endif
 
 /* Handling of unhandled and spurious interrupts: */
