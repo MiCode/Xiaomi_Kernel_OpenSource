d7453c4fc3c72e592da77f1c037d7147faebb6c5
Sandeep Panda <spanda@codeaurora.org>
2014-11-14 16:47:47 +0530

    msm: mdss:  enable ulps during suspend for command mode
    Enter into ULPS only during suspend for command mode
    panel if ulps_suspend flag is enabled in DT.
    
    Change-Id: Ie03fd4c4c59baf7390d7c50e1a9d7727a5e7b06e
    Signed-off-by: Sandeep Panda <spanda@codeaurora.org>

--- drivers/video/msm/mdss/msm_mdss_io_8974.c
+++ drivers/video/msm/mdss/msm_mdss_io_8974.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1395,7 +1395,9 @@ static int mdss_dsi_clk_ctrl_sub(struct mdss_dsi_ctrl_pdata *ctrl,
 			if (((mdss_dsi_ulps_feature_enabled(pdata)) &&
 				(pdata->panel_info.blank_state !=
 				 MDSS_PANEL_BLANK_BLANK)) ||
-				(pdata->panel_info.ulps_suspend_enabled))
+				(pdata->panel_info.ulps_suspend_enabled &&
+				(pdata->panel_info.blank_state ==
+				 MDSS_PANEL_BLANK_BLANK)))
 				mdss_dsi_ulps_config(ctrl, 1);
 
 			mdss_dsi_link_clk_stop(ctrl);
