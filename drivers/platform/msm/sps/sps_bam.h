184cd0349f989e554986090e5c248bdc605cb18b
Yan He <yanhe@codeaurora.org>
2014-11-16 23:55:06 -0800

    msm: sps: check the pending descriptor
    Check if a pipe of a BAM has any pending descriptor.
    
    Change-Id: I7313de53dc49cdc5ae14a7e10740f0ffb35fb26b
    Signed-off-by: Yan He <yanhe@codeaurora.org>

--- drivers/platform/msm/sps/sps_bam.h
+++ drivers/platform/msm/sps/sps_bam.h
@@ -578,4 +578,15 @@ int sps_bam_pipe_get_unused_desc_num(struct sps_bam *dev, u32 pipe_index,
  * Return: 0 on success, negative value on error
  */
 int sps_bam_check_irq(struct sps_bam *dev);
+
+/*
+ * sps_bam_pipe_pending_desc - checking pending descriptor.
+ * @dev:	BAM device handle
+ * @pipe_index:	pipe index
+ *
+ * This function checks if a pipe of a BAM has any pending descriptor.
+ *
+ * @return true if there is any desc pending
+ */
+bool sps_bam_pipe_pending_desc(struct sps_bam *dev, u32 pipe_index);
 #endif	/* _SPSBAM_H_ */
