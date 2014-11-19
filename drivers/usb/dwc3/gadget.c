d9b8dd5deaccba7fd63bb81dfce449d69e1cc484
Mayank Rana <mrana@codeaurora.org>
2014-11-19 15:56:40 -0800

    usb: dwc3: gadget: Update link_state based on receiving interrupt
    Currently dwc->link_state is being updated by reading DWC3_DSTS
    register. DWC3_DSTS register is having delay in updation of LINK
    state. Hence instead of relying on this register for U0 and U3
    state, update dwc->link_state directly on receiving reset/wakeup
    and suspend/disconnect interrupts respectively.
    
    Change-Id: I8640f9566ab21c60fab24a2e5d25ba24545183ff
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>
    [jackp@codeaurora.org: resolved a trivial context conflict]
    Signed-off-by: Jack Pham <jackp@codeaurora.org>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -2721,7 +2721,7 @@ static void dwc3_gadget_disconnect_interrupt(struct dwc3 *dwc)
 
 	dwc->gadget.speed = USB_SPEED_UNKNOWN;
 	dwc->setup_packet_pending = false;
-	dwc->link_state = dwc3_get_link_state(dwc);
+	dwc->link_state = DWC3_LINK_STATE_SS_DIS;
 }
 
 static void dwc3_gadget_reset_interrupt(struct dwc3 *dwc)
@@ -2790,7 +2790,7 @@ static void dwc3_gadget_reset_interrupt(struct dwc3 *dwc)
 	reg = dwc3_readl(dwc->regs, DWC3_DCFG);
 	reg &= ~(DWC3_DCFG_DEVADDR_MASK);
 	dwc3_writel(dwc->regs, DWC3_DCFG, reg);
-	dwc->link_state = dwc3_get_link_state(dwc);
+	dwc->link_state = DWC3_LINK_STATE_U0;
 }
 
 static void dwc3_update_ram_clk_sel(struct dwc3 *dwc, u32 speed)
@@ -2830,8 +2830,6 @@ static void dwc3_gadget_conndone_interrupt(struct dwc3 *dwc)
 	dwc->speed = speed;
 
 	dwc3_update_ram_clk_sel(dwc, speed);
-	dwc->link_state = dwc3_get_link_state(dwc);
-	pr_debug("%s(): LINK_STATE:%d\n", __func__, dwc->link_state);
 
 	switch (speed) {
 	case DWC3_DCFG_SUPERSPEED:
@@ -2930,11 +2928,10 @@ static void dwc3_gadget_wakeup_interrupt(struct dwc3 *dwc)
 		if (dwc->enable_bus_suspend)
 			usb_phy_set_suspend(dwc->dotg->otg.phy, 0);
 
-		dwc->link_state = dwc3_get_link_state(dwc);
 		dwc3_resume_gadget(dwc);
-	} else {
-		dwc->link_state = dwc3_get_link_state(dwc);
 	}
+
+	dwc->link_state = DWC3_LINK_STATE_U0;
 }
 
 static void dwc3_gadget_linksts_change_interrupt(struct dwc3 *dwc,
