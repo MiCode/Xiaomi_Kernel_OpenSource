01486504b100805b670a3096338d85c58d261dc7
Dipen Parmar <dipenp@codeaurora.org>
2014-11-21 14:56:23 +0530

    hwmon: qpnp-adc-voltage: Add temperature compensation for PM8909
    Add VBAT and OCV temperature compensation for PM8909
    v1.0 SMIC parts.
    
    The temperature compensation is required on top of
    gain/offset calibration to compensate for variations
    due to chip revision, manufacturer and temperature.
    
    Change-Id: Ic0f110ed53dba314196af31c16c1fd58d20f7f1d
    Signed-off-by: Dipen Parmar <dipenp@codeaurora.org>

--- drivers/hwmon/qpnp-adc-common.c
+++ drivers/hwmon/qpnp-adc-common.c
@@ -1718,6 +1718,20 @@ int qpnp_adc_get_revid_version(struct device *dev)
 		(revid_data->pmic_type == PM8916_V2P0_TYPE) &&
 		(revid_data->pmic_subtype == PM8916_V2P0_SUBTYPE))
 			return QPNP_REV_ID_8916_2_0;
+	else if ((revid_data->rev1 == PM8909_V1P0_REV1) &&
+		(revid_data->rev2 == PM8909_V1P0_REV2) &&
+		(revid_data->rev3 == PM8909_V1P0_REV3) &&
+		(revid_data->rev4 == PM8909_V1P0_REV4) &&
+		(revid_data->pmic_type == PM8909_V1P0_TYPE) &&
+		(revid_data->pmic_subtype == PM8909_V1P0_SUBTYPE))
+			return QPNP_REV_ID_8909_1_0;
+	else if ((revid_data->rev1 == PM8909_V1P1_REV1) &&
+		(revid_data->rev2 == PM8909_V1P1_REV2) &&
+		(revid_data->rev3 == PM8909_V1P1_REV3) &&
+		(revid_data->rev4 == PM8909_V1P1_REV4) &&
+		(revid_data->pmic_type == PM8909_V1P1_TYPE) &&
+		(revid_data->pmic_subtype == PM8909_V1P1_SUBTYPE))
+			return QPNP_REV_ID_8909_1_1;
 	else
 		return -EINVAL;
 }
