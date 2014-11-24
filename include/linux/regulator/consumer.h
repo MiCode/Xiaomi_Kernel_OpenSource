4105dcfedd75eb1e74a889e12b681eb711908c3b
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2014-11-24 14:10:52 +0000

    regulator: core: Add PRE_DISABLE notification
    Add a PRE_DISABLE notification so that consumers can use a
    notifier to run any steps required to prepare for the
    regulator being switched off. Since the regulator disable
    can fail an abort notification is also added.
    
    Change-Id: Ib7f77d27b3ba3ef2a7704247398db8ab6b625c42
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Git-commit: a1c8a5512b7cddc81767172f0de37b155cea039f
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
    [osvaldob@codeaurora.org: resolve trivial merge conflicts]
    Signed-off-by: Osvaldo Banuelos <osvaldob@codeaurora.org>

--- include/linux/regulator/consumer.h
+++ include/linux/regulator/consumer.h
@@ -96,12 +96,14 @@ struct regmap;
  * FORCE_DISABLE  Regulator forcibly shut down by software.
  * VOLTAGE_CHANGE Regulator voltage changed.
  *                Data passed is old voltage cast to (void *).
+ * ENABLE         Regulator was enabled.
  * DISABLE        Regulator was disabled.
  * PRE_VOLTAGE_CHANGE   Regulator is about to have voltage changed.
  *                      Data passed is "struct pre_voltage_change_data"
  * ABORT_VOLTAGE_CHANGE Regulator voltage change failed for some reason.
  *                      Data passed is old voltage cast to (void *).
- * ENABLE         Regulator was enabled.
+ * PRE_DISABLE    Regulator is about to be disabled
+ * ABORT_DISABLE  Regulator disable failed for some reason
  *
  * NOTE: These events can be OR'ed together when passed into handler.
  */
@@ -116,6 +118,8 @@ struct regmap;
 #define REGULATOR_EVENT_DISABLE 		0x80
 #define REGULATOR_EVENT_PRE_VOLTAGE_CHANGE	0x100
 #define REGULATOR_EVENT_ABORT_VOLTAGE_CHANGE	0x200
+#define REGULATOR_EVENT_PRE_DISABLE		0x400
+#define REGULATOR_EVENT_ABORT_DISABLE		0x800
 
 /**
  * struct pre_voltage_change_data - Data sent with PRE_VOLTAGE_CHANGE event
