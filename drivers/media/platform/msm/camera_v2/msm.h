4d10dc4de263374c8fd5d81db6c3b9fd5524ab37
Aditya Jonnalagadda <ajonnala@codeaurora.org>
2014-11-26 15:25:26 +0530

    Power: Enable/Disable power collapse after camera open
    Disabling power collapse during camera open. We send
    disable request before opening camera and enable it
    back after opening camera
    
    Change-Id: Idd55021063eace18fa20f00183a41a502f5c88a1
    Signed-off-by: Aditya Jonnalagadda <ajonnala@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/msm.h
+++ drivers/media/platform/msm/camera_v2/msm.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -33,6 +33,9 @@
 #define MSM_POST_EVT_TIMEOUT 5000
 #define MSM_POST_EVT_NOTIMEOUT 0xFFFFFFFF
 #define MSM_CAMERA_STREAM_CNT_BITS  32
+
+#define CAMERA_DISABLE_PC_LATENCY 100
+#define CAMERA_ENABLE_PC_LATENCY PM_QOS_DEFAULT_VALUE
 
 struct msm_video_device {
 	struct video_device *vdev;
@@ -101,6 +104,8 @@ struct msm_session {
 	struct msm_queue_head stream_q;
 	struct mutex lock;
 };
+
+void msm_pm_qos_update_request(int val);
 
 int msm_post_event(struct v4l2_event *event, int timeout);
 int  msm_create_session(unsigned int session, struct video_device *vdev);
