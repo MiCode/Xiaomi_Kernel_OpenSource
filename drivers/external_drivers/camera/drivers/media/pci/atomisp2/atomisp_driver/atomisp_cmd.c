8ab9df36a67e7a5f1dfc9f8995001c79cc1cf5a6
Tianshu Qiu <tian.shu.qiu@intel.com>
2014-11-28 13:52:46 +0800

    atomisp2: Check pipe id to identify post view when re-triggering capture
    In SDV mode, the preview buffers are also from the VF output of video pipe.
    Need to chek pipe id when identify the post view buffer.
    
    Change-Id: Iac7aabff678cca9bc56c9b99c1719f04a716c80f
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-4029
    Signed-off-by: Tianshu Qiu <tian.shu.qiu@intel.com>
    Reviewed-on: https://icggerrit.ir.intel.com/30524
    Approver: Wei Tao <wei.tao@intel.com>
    Build: Wei Tao <wei.tao@intel.com>
    Reviewed-by: Wei Tao <wei.tao@intel.com>
    Maintainer: Wei Tao <wei.tao@intel.com>

--- drivers/external_drivers/camera/drivers/media/pci/atomisp2/atomisp_driver/atomisp_cmd.c
+++ drivers/external_drivers/camera/drivers/media/pci/atomisp2/atomisp_driver/atomisp_cmd.c
@@ -1049,7 +1049,8 @@ void atomisp_buf_done(struct atomisp_sub_device *asd, int error,
 			WARN_ON(!vb);
 			if (vb)
 				pipe->frame_config_id[vb->i] = frame->isp_config_id;
-			if (asd->pending_capture_request > 0) {
+			if (css_pipe_id == IA_CSS_PIPE_ID_CAPTURE &&
+			    asd->pending_capture_request > 0) {
 				err = atomisp_css_offline_capture_configure(
 					asd,
 					asd->params.offline_parm.num_captures,
