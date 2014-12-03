fce95c9a127810b80999a090baf064f8b18e9424
Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
2014-12-03 10:18:12 -0800

    sched: Make RT tasks eligible for boost
    During sched boost RT tasks currently end up going to the lowest
    power cluster. This can be a performance bottleneck especially if
    the frequency and IPC differences between clusters are high.
    Furthermore, when RT tasks go over to the little cluster during
    boost, the load balancer keeps attempting to pull work over to the
    big cluster. This results in pre-emption of the executing RT task
    causing more delays. Finally, containing more work on a single
    cluster during boost might help save some power if the little
    cluster can then enter deeper low power modes.
    
    Change-Id: I177b2e81be5657c23e7ac43889472561ce9993a9
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1498,7 +1498,7 @@ static void boost_kick_cpus(void)
 	}
 }
 
-static inline int sched_boost(void)
+int sched_boost(void)
 {
 	return boost_refcount > 0;
 }
@@ -2249,11 +2249,6 @@ void check_for_migration(struct rq *rq, struct task_struct *p)
 					&rq->active_balance_work);
 }
 
-static inline int capacity(struct rq *rq)
-{
-	return rq->capacity;
-}
-
 static inline int nr_big_tasks(struct rq *rq)
 {
 	return rq->nr_big_tasks;
@@ -2312,11 +2307,6 @@ static inline int is_big_task(struct task_struct *p)
 static inline int nr_big_tasks(struct rq *rq)
 {
 	return 0;
-}
-
-static inline int capacity(struct rq *rq)
-{
-	return SCHED_LOAD_SCALE;
 }
 
 #endif	/* CONFIG_SCHED_HMP */
