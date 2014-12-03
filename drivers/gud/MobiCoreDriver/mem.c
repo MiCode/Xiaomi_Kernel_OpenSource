b876d15c99c83a78d8f91949a9c90ae1c3f11bf7
Sylvain Trosset <trosset.sylvain@gmail.com>
2014-12-03 15:45:37 +0100

    fix memory corruption in map_buffer TBUG-411 TQC-24
    Change-Id: I509320ca4c31d6f5e3a30b3050a6055c83d3b7c7
    Signed-off-by: Sylvain Trosset <trosset.sylvain@gmail.com>
    Git-commit: 6814f81977d4f5b99ef8b55c8c1958af46f3ea01
    Git-repo: https://code.google.com/p/tee-mobicore-driver.kernel/
    Signed-off-by: Keith Fallows <keithf@codeaurora.org>
    Acked-by: Tony Hamilton <tonyh@qti.qualcomm.com>

--- drivers/gud/MobiCoreDriver/mem.c
+++ drivers/gud/MobiCoreDriver/mem.c
@@ -347,7 +347,7 @@ static int map_buffer(struct task_struct *task, void *wsm_buffer,
 	void		*virt_addr_page;
 	struct page	*page;
 	struct mmutable	*mmutable;
-	struct page	**mmutable_as_array_of_pointers_to_page;
+	struct page	**mmutable_as_array_of_pointers_to_page = NULL;
 	/* page offset in wsm buffer */
 	unsigned int offset;
 
@@ -384,12 +384,21 @@ static int map_buffer(struct task_struct *task, void *wsm_buffer,
 	}
 
 	mmutable = table->virt;
+#if (defined LPAE_SUPPORT) || !(defined CONFIG_ARM64)
 	/*
 	 * We use the memory for the MMU table to hold the pointer
 	 * and convert them later. This works, as everything comes
 	 * down to a 32 bit value.
 	 */
 	mmutable_as_array_of_pointers_to_page = (struct page **)mmutable;
+#else
+	mmutable_as_array_of_pointers_to_page = kmalloc(
+		sizeof(struct page *)*nr_of_pages, GFP_KERNEL | __GFP_ZERO);
+	if (mmutable_as_array_of_pointers_to_page == NULL) {
+		ret = -ENOMEM;
+		goto map_buffer_end;
+	}
+#endif
 
 	/* Request comes from user space */
 	if (task != NULL && !is_vmalloc_addr(wsm_buffer)) {
@@ -407,7 +416,7 @@ static int map_buffer(struct task_struct *task, void *wsm_buffer,
 				 mmutable_as_array_of_pointers_to_page);
 		if (ret != 0) {
 			MCDRV_DBG_ERROR(mcd, "lock_user_pages() failed");
-			return ret;
+			goto map_buffer_end;
 		}
 	}
 	/* Request comes from kernel space(cont buffer) */
@@ -449,20 +458,10 @@ static int map_buffer(struct task_struct *task, void *wsm_buffer,
 	 * - or fails and used_mmutable->table contains the list of page
 	 * pointers.
 	 * Any mixed contents will make cleanup difficult.
-	 */
-#if defined(CONFIG_ARM64) && !defined(LPAE_SUPPORT)
-	/*
-	 * When NWd pointers are 64bits and SWd pte 32bits we need to fill the
-	 * table from 0.
-	 */
-	i = 0;
-#else
-	/*
 	 * Fill the table in reverse order as the table is used as input and
 	 * output.
 	 */
 	i = MC_ARM_MMU_TABLE_ENTRIES-1;
-#endif
 	do {
 		if (i < nr_of_pages) {
 #ifdef LPAE_SUPPORT
@@ -521,12 +520,12 @@ static int map_buffer(struct task_struct *task, void *wsm_buffer,
 			/* ensure rest of table is empty */
 			mmutable->table_entries[i] = 0;
 		}
-#if defined(CONFIG_ARM64) && !defined(LPAE_SUPPORT)
-	} while (++i < MC_ARM_MMU_TABLE_ENTRIES);
-#else
 	} while (i-- != 0);
-#endif
 
+map_buffer_end:
+#if !(defined LPAE_SUPPORT) && (defined CONFIG_ARM64)
+	kfree(mmutable_as_array_of_pointers_to_page);
+#endif
 	return ret;
 }
 
