83d95c50fb7c138177290641436a926ed4700e9b
Subhash Jadavani <subhashj@codeaurora.org>
2014-12-04 21:42:45 +0200

    mmc: sdio: enable asynchronous interrupt support in 4-bit mode
    SDIO 3.0 specification has added the support for asynchronous interrupt
    period during which card allows the clock to be gated off. Host needs
    to first read the "Support Asynchronous Interrupt" bit in CCCR register
    space to check if the card supports the feature or not. If yes and if
    the host wants to enable the feature, host needs to write '1' to
    "Enable Asynchronous Interrupt" bit in CCCR register space.
    
    This change allows the host controller driver to control whether to enable
    the asynchronous interrupt in card or not and if the asynchronous interrupt
    is enabled then clock gating feature would be enabled for such cards.
    
    Change-Id: I678cffb63af6a2013640a5eafa6ce9bfad8a51d6
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>
    [merez@codeaurora.org: fix CAPS2 index]
    Signed-off-by: Maya Erez <merez@codeaurora.org>
    [subhashj@codeaurora.org: fixed trivial merge conflicts]
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -310,6 +310,8 @@ struct mmc_host {
 #define MMC_CAP2_NO_WRITE_PROTECT (1 << 18)	/* No physical write protect pin, assume that card is always read-write */
 #define MMC_CAP2_PACKED_WR_CONTROL (1 << 19)	/* Allow write packing control */
 #define MMC_CAP2_CLK_SCALE	(1 << 20)	/* Allow dynamic clk scaling */
+/* Allows Asynchronous SDIO irq while card is in 4-bit mode */
+#define MMC_CAP2_ASYNC_SDIO_IRQ_4BIT_MODE (1 << 21)
 #define MMC_CAP2_NONHOTPLUG	(1 << 25)	/*Don't support hotplug*/
 
 	mmc_pm_flag_t		pm_caps;	/* supported pm features */
