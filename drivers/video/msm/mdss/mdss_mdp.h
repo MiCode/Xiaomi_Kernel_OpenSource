7627377cca357758ab3589116f587af6188b6dc1
Gopikrishnaiah Anandan <agopik@codeaurora.org>
2014-12-03 19:58:50 -0800

    msm: mdss: move caching of pp params
    For source pipes post processing feature params are cached in the pipe
    structure. Post processing(pp) feature versions can change across
    different versions of mdp and might require changes to caching code.
    Post processing driver can handle caching the params based on version.
    This change moves the caching into post processing driver
    
    Change-Id: Ic02fec43dbbff5d4404b618d6d82b2c8b8eef07a
    Signed-off-by: Gopikrishnaiah Anandan <agopik@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp.h
+++ drivers/video/msm/mdss/mdss_mdp.h
@@ -919,6 +919,8 @@ int mdss_mdp_pp_setup_locked(struct mdss_mdp_ctl *ctl);
 int mdss_mdp_pipe_pp_setup(struct mdss_mdp_pipe *pipe, u32 *op);
 int mdss_mdp_pipe_sspp_setup(struct mdss_mdp_pipe *pipe, u32 *op);
 void mdss_mdp_pipe_sspp_term(struct mdss_mdp_pipe *pipe);
+int mdss_mdp_pp_sspp_config(struct mdss_mdp_pipe *pipe);
+
 int mdss_mdp_smp_setup(struct mdss_data_type *mdata, u32 cnt, u32 size);
 
 void mdss_hw_init(struct mdss_data_type *mdata);
