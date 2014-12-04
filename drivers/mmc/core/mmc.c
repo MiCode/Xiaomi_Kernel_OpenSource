bd66cc3b99ca9178af5c9a0642bcb0c7747469be
Tatyana Brokhman <tlinder@codeaurora.org>
2014-12-04 22:03:42 +0200

    mmc: Add long power off notification support
    At the moment only POWER_OFF_SHORT is sent to the device in case the host
    is suspended. This patch adds the support of sending POWER_OFF_LONG
    notification in case the device is powered off.
    
    According to device vendors the POWER_OFF_LONG notification will shorten
    the initialization time of the eMMC card during next boot up.
    
    Change-Id: I3c6f224398450cf10463cbb316613fd430d1e8d2
    Signed-off-by: Tatyana Brokhman <tlinder@codeaurora.org>
    Signed-off-by: Sujit Reddy Thumma <sthumma@codeaurora.org>
    [merez@codeaurora.org: fix conflicts as some of the code was already included]
    Signed-off-by: Maya Erez <merez@codeaurora.org>
    [venkatg@codeaurora.org: Remove PM related code]
    Signed-off-by: Venkat Gopalakrishnan <venkatg@codeaurora.org>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -18,6 +18,7 @@
 #include <linux/mmc/host.h>
 #include <linux/mmc/card.h>
 #include <linux/mmc/mmc.h>
+#include <linux/reboot.h>
 
 #include "core.h"
 #include "bus.h"
@@ -1205,6 +1206,20 @@ out:
 	return err;
 }
 
+static int mmc_reboot_notify(struct notifier_block *notify_block,
+		unsigned long event, void *unused)
+{
+	struct mmc_card *card = container_of(
+			notify_block, struct mmc_card, reboot_notify);
+
+	if (event != SYS_RESTART)
+		card->issue_long_pon = true;
+	else
+		card->issue_long_pon = false;
+
+	return NOTIFY_OK;
+}
+
 /*
  * Activate High Speed or HS200 mode if supported.
  */
@@ -1382,6 +1397,7 @@ static int mmc_init_card(struct mmc_host *host, u32 ocr,
 		card->rca = 1;
 		memcpy(card->raw_cid, cid, sizeof(card->raw_cid));
 		host->card = card;
+		card->reboot_notify.notifier_call = mmc_reboot_notify;
 	}
 
 	/*
@@ -1724,6 +1740,22 @@ static int mmc_poweroff_notify(struct mmc_card *card, unsigned int notify_type)
 	return err;
 }
 
+int mmc_send_long_pon(struct mmc_card *card)
+{
+	int err = 0;
+	struct mmc_host *host = card->host;
+
+	mmc_claim_host(host);
+	if (card->issue_long_pon && mmc_can_poweroff_notify(card)) {
+		err = mmc_poweroff_notify(host->card, EXT_CSD_POWER_OFF_LONG);
+		if (err)
+			pr_warning("%s: error %d sending Long PON",
+					mmc_hostname(host), err);
+	}
+	mmc_release_host(host);
+	return err;
+}
+
 /*
  * Host is being removed. Free up the current card.
  */
@@ -1732,6 +1764,7 @@ static void mmc_remove(struct mmc_host *host)
 	BUG_ON(!host);
 	BUG_ON(!host->card);
 
+	unregister_reboot_notifier(&host->card->reboot_notify);
 	mmc_remove_card(host->card);
 
 	mmc_claim_host(host);
@@ -2035,6 +2068,8 @@ int mmc_attach_mmc(struct mmc_host *host)
 		goto remove_card;
 
 	mmc_init_clk_scaling(host);
+
+	register_reboot_notifier(&host->card->reboot_notify);
 
 	return 0;
 
