49cbe96252918783fd8a09edbe78175bd7b00648
Jordan Crouse <jcrouse@codeaurora.org>
2014-12-04 14:36:03 -0700

    msm: kgsl: Replace CP_INTERRUPT with EVENT_WRITE interrupt
    Ever since the beginning of time we have used CP_INTERRUPT to trigger
    an interrupt when a command batch is completed.  Back in the dark
    legacy days we used it because it was the only thing that would work
    with the conditional interrupt. We kept it after the great dispatcher
    enlightenment of 2013 because if it ain't broke, don't fix it.
    
    And then it broke. Actually it didn't really break, but we've always
    been lucky when using it because CP_INTERRUPT is triggered when the CP
    reads the opcode so there isn't really any assurance that the
    command was completed (to be technical - there isn't any assurance
    that the retired timestamp was written but that is the only way
    the CPU knows the command was completed).  We manage to get by
    99.99% of the time because usually there is another command coming
    right behind us and the dispatcher will always have reason to be
    run. But in that .01% window there is a chance that we the timestamp
    will not yet be written in the short time between processing an
    interrupt and reading the memstore - the dispatcher thinks nothing
    has retired and it leaves.
    
    The good news is that we have a better way - since we don't need
    conditional interrupts (yay!) we can use the interrupt selector
    from the EVENT_WRITE command that writes the timestamp. This is
    good news because that guy *does* wait for the timestamp before
    firing the interrupt so no more chance of missing things.
    
    Change-Id: Ic0dedbadf612763e3e9af32419badf5034f25277
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Harshdeep Dhatt <hdhatt@codeaurora.org>

--- drivers/gpu/msm/adreno_a3xx.c
+++ drivers/gpu/msm/adreno_a3xx.c
@@ -751,6 +751,7 @@ static void a3xx_err_callback(struct adreno_device *adreno_dev, int bit)
 	 (1 << A3XX_INT_CP_IB1_INT) |            \
 	 (1 << A3XX_INT_CP_IB2_INT) |            \
 	 (1 << A3XX_INT_CP_RB_INT) |             \
+	 (1 << A3XX_INT_CACHE_FLUSH_TS) |	 \
 	 (1 << A3XX_INT_CP_REG_PROTECT_FAULT) |  \
 	 (1 << A3XX_INT_CP_AHB_ERROR_HALT) |     \
 	 (1 << A3XX_INT_UCHE_OOB_ACCESS))
@@ -778,7 +779,7 @@ static struct adreno_irq_funcs a3xx_irq_funcs[] = {
 	ADRENO_IRQ_CALLBACK(NULL),	       /* 17 - CP_RB_DONE_TS */
 	ADRENO_IRQ_CALLBACK(NULL),	       /* 18 - CP_VS_DONE_TS */
 	ADRENO_IRQ_CALLBACK(NULL),	       /* 19 - CP_PS_DONE_TS */
-	ADRENO_IRQ_CALLBACK(NULL),	       /* 20 - CP_CACHE_FLUSH_TS */
+	ADRENO_IRQ_CALLBACK(adreno_cp_callback), /* 20 - CP_CACHE_FLUSH_TS */
 	/* 21 - CP_AHB_ERROR_FAULT */
 	ADRENO_IRQ_CALLBACK(a3xx_err_callback),
 	ADRENO_IRQ_CALLBACK(NULL),	       /* 22 - Unused */
--- drivers/gpu/msm/adreno_ringbuffer.c
+++ drivers/gpu/msm/adreno_ringbuffer.c
@@ -565,10 +565,6 @@ adreno_ringbuffer_addcmds(struct adreno_ringbuffer *rb,
 
 	total_sizedwords += (secured_ctxt) ? 26 : 0;
 
-	/* Add two dwords for the CP_INTERRUPT */
-	total_sizedwords +=
-		(drawctxt || (flags & KGSL_CMD_FLAGS_INTERNAL_ISSUE)) ?  2 : 0;
-
 	/* context rollover */
 	if (adreno_is_a3xx(adreno_dev))
 		total_sizedwords += 3;
@@ -707,7 +703,10 @@ adreno_ringbuffer_addcmds(struct adreno_ringbuffer *rb,
 	 * set and hence the rb timestamp will be used in else statement below.
 	 */
 	*ringcmds++ = cp_mem_packet(adreno_dev, CP_EVENT_WRITE, 3, 1);
-	*ringcmds++ = CACHE_FLUSH_TS;
+	if (drawctxt || (flags & KGSL_CMD_FLAGS_INTERNAL_ISSUE))
+		*ringcmds++ = CACHE_FLUSH_TS | (1 << 31);
+	else
+		*ringcmds++ = CACHE_FLUSH_TS;
 
 	if (drawctxt && !(flags & KGSL_CMD_FLAGS_INTERNAL_ISSUE)) {
 		ringcmds += cp_gpuaddr(adreno_dev, ringcmds, gpuaddr +
@@ -721,11 +720,6 @@ adreno_ringbuffer_addcmds(struct adreno_ringbuffer *rb,
 		ringcmds += cp_gpuaddr(adreno_dev, ringcmds, gpuaddr +
 				KGSL_MEMSTORE_RB_OFFSET(rb, eoptimestamp));
 		*ringcmds++ = timestamp;
-	}
-
-	if (drawctxt || (flags & KGSL_CMD_FLAGS_INTERNAL_ISSUE)) {
-		*ringcmds++ = cp_packet(adreno_dev, CP_INTERRUPT, 1);
-		*ringcmds++ = CP_INTERRUPT_RB;
 	}
 
 	if (adreno_is_a3xx(adreno_dev)) {
