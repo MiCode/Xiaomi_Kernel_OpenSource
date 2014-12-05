ec7cc5f1e427ea9a3e7c7e5e01fc363b39181632
Sridhar Gujje <sgujje@codeaurora.org>
2014-12-05 18:26:56 +0530

    ASoC: msm: qdsp6v2: Unblock EOS on reset event callback
    Issue: When SSR command is issued in the end of stream of
    compress offload playback, ANR is seen in music app. This
    is because EOS is waiting indefinitely and will not return
    since ADSP is dead in this case.
    
    Fix is to unblock EOS on reset event callback from DSP.
    
    Change-Id: Ifc379360b0ba7c589c3e2dddb7b2a3c712af7fb0
    CRs-Fixed: 731606
    Signed-off-by: Divya Narayanan Poojary <dnaray@codeaurora.org>
    Signed-off-by: Sridhar Gujje <sgujje@codeaurora.org>
    Signed-off-by: Banajit Goswami <bgoswami@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
@@ -532,6 +532,10 @@ static void compr_event_handler(uint32_t opcode,
 		prtd->copied_total = prtd->bytes_received;
 		atomic_set(&prtd->error, 1);
 		wake_up(&prtd->drain_wait);
+		if (atomic_cmpxchg(&prtd->eos, 1, 0)) {
+			pr_debug("%s:unblock eos wait queues", __func__);
+			wake_up(&prtd->eos_wait);
+		}
 		spin_unlock_irqrestore(&prtd->lock, flags);
 		break;
 	default:
@@ -1259,7 +1263,8 @@ static int msm_compr_drain_buffer(struct msm_compr_audio *prtd,
 		prtd->cmd_interrupt = 0;
 	}
 	if (atomic_read(&prtd->error)) {
-		pr_err("%s: Got RESET EVENTS notification, return\n", __func__);
+		pr_err("%s: Got RESET EVENTS notification, return\n",
+			__func__);
 		rc = -ENETRESET;
 	}
 	return rc;
@@ -1566,7 +1571,9 @@ static int msm_compr_trigger(struct snd_compr_stream *cstream, int cmd)
 
 		/* Wait indefinitely for  DRAIN. Flush can also signal this*/
 		rc = wait_event_interruptible(prtd->eos_wait,
-				      (prtd->eos_ack || prtd->cmd_interrupt));
+						(prtd->cmd_ack ||
+						prtd->cmd_interrupt ||
+						atomic_read(&prtd->error)));
 
 		if (rc < 0)
 			pr_err("%s: EOS wait failed\n", __func__);
@@ -1576,6 +1583,12 @@ static int msm_compr_trigger(struct snd_compr_stream *cstream, int cmd)
 
 		if (prtd->cmd_interrupt)
 			rc = -EINTR;
+
+		if (atomic_read(&prtd->error)) {
+			pr_err("%s: Got RESET EVENTS notification, return\n",
+				__func__);
+			rc = -ENETRESET;
+		}
 
 		/*FIXME : what if a flush comes while PC is here */
 		if (rc == 0) {
