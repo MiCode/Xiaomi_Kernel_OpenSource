59c3a63c616575f1ffe7c5482b38b8b4bf345eb2
Aditya Jonnalagadda <ajonnala@codeaurora.org>
2014-12-05 11:24:30 +0530

    msm: camera: isp: Fix Liveshot for YUV sensor
    Reset frame id of streamon source only.
    
    Change-Id: I3c65fd4b6d00e5288b15c55e5c09af2d95070e76
    Signed-off-by: Aditya Jonnalagadda <ajonnala@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -1550,7 +1550,7 @@ static int msm_isp_start_axi_stream(struct vfe_device *vfe_dev,
 		if (SRC_TO_INTF(stream_info->stream_src) != VFE_PIX_0 &&
 			stream_info->stream_src < VFE_AXI_SRC_MAX) {
 			vfe_dev->axi_data.src_info[SRC_TO_INTF(
-				stream_info->stream_src)].frame_id = 0;
+				stream_info->stream_src)].frame_id = init_frm_drop;
 		}
 	}
 	msm_isp_update_stream_bandwidth(vfe_dev);
@@ -1565,12 +1565,6 @@ static int msm_isp_start_axi_stream(struct vfe_device *vfe_dev,
 		vfe_dev->hw_info->vfe_ops.core_ops.
 			update_camif_state(vfe_dev, camif_update);
 	}
-	if (vfe_dev->axi_data.src_info[VFE_RAW_0].raw_stream_count > 0)
-		vfe_dev->axi_data.src_info[VFE_RAW_0].frame_id = 0;
-	else if (vfe_dev->axi_data.src_info[VFE_RAW_1].raw_stream_count > 0)
-		vfe_dev->axi_data.src_info[VFE_RAW_1].frame_id = 0;
-	else if (vfe_dev->axi_data.src_info[VFE_RAW_2].raw_stream_count > 0)
-		vfe_dev->axi_data.src_info[VFE_RAW_2].frame_id = 0;
 
 	if (wait_for_complete) {
 		vfe_dev->axi_data.stream_update = stream_cfg_cmd->num_streams;
