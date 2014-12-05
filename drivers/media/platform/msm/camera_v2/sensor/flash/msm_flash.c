7761892108981b2f450583ce1e04475fe791f3ae
Deepak Kaushal <dkaushal@codeaurora.org>
2014-12-04 18:56:13 -0800

    msm: camera: Fix led release on Sensor Shutdown
    On sensor shutdown, flash was calling flash_high.
    When camera daemon is killed, this flash_high was getting
    invoked which resulted the flash to fire.
    This is now fixed by calling flash_release.
    
    Change-Id: Iace1fee56253dc43c1525a89a2b6e369065ca7aa
    Signed-off-by: Deepak Kaushal <dkaushal@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.c
+++ drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.c
@@ -580,7 +580,7 @@ static long msm_flash_subdev_ioctl(struct v4l2_subdev *sd,
 	case VIDIOC_MSM_FLASH_CFG:
 		return msm_flash_config(fctrl, argp);
 	case MSM_SD_SHUTDOWN:
-		*(int *)argp = MSM_CAMERA_LED_RELEASE;
+		*(int *)argp = CFG_FLASH_RELEASE;
 		return msm_flash_config(fctrl, argp);
 	default:
 		pr_err_ratelimited("invalid cmd %d\n", cmd);
