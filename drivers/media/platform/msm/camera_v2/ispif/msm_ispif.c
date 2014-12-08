0484211778ec28f371b3e8604a565aa685b4f87d
Sreesudhan Ramakrish Ramkumar <srramku@codeaurora.org>
2014-12-08 11:10:55 -0800

    msm: camera: ispif: Validate vfe_intf parameter
    Validate vfe_intf parameter to avoid invalid register access.
    
    Change-Id: Ie0b57071cc5fca1c48d3a5e2e7819f9af9ff544c
    Signed-off-by: Sreesudhan Ramakrish Ramkumar <srramku@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/ispif/msm_ispif.c
+++ drivers/media/platform/msm/camera_v2/ispif/msm_ispif.c
@@ -593,6 +593,11 @@ static int msm_ispif_config(struct ispif_device *ispif,
 
 	for (i = 0; i < params->num; i++) {
 		vfe_intf = params->entries[i].vfe_intf;
+		if (vfe_intf >= VFE_MAX) {
+			pr_err("%s: %d invalid i %d vfe_intf %d\n", __func__,
+				__LINE__, i, vfe_intf);
+			return -EINVAL;
+		}
 		if (!msm_ispif_is_intf_valid(ispif->csid_version,
 				vfe_intf)) {
 			pr_err("%s: invalid interface type\n", __func__);
