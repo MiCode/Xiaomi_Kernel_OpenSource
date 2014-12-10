f6a0a17732e30f76fc678180de3934d032ee8d09
Neha Atri <natri@codeaurora.org>
2014-12-10 11:53:39 -0800

    iommu: Fix bug in __msm_map_iommu_common()
    In error situations, iommu_meta remains NULL and msm_iommu_meta_put()
    gets called with this NULL pointer. Fix this.
    
    CRs-Fixed: 769233
    Change-Id: I91c1e234c16d40af249a2d54faf6eae087281f5b
    Signed-off-by: Neha Atri <natri@codeaurora.org>

--- drivers/iommu/msm_iommu_mapping.c
+++ drivers/iommu/msm_iommu_mapping.c
@@ -428,6 +428,11 @@ static int __msm_map_iommu_common(
 
 	if (!iommu_meta) {
 		iommu_meta = msm_iommu_meta_create(dma_buf, table, size);
+
+		if (IS_ERR(iommu_meta)) {
+			ret = PTR_ERR(iommu_meta);
+			goto out;
+		}
 	} else {
 		/*
 		 * Drop the dma_buf reference here. We took the reference
@@ -479,8 +484,8 @@ static int __msm_map_iommu_common(
 out_unlock:
 	mutex_unlock(&iommu_meta->lock);
 out:
-
-	msm_iommu_meta_put(iommu_meta);
+	if (iommu_meta)
+		msm_iommu_meta_put(iommu_meta);
 	return ret;
 
 }
