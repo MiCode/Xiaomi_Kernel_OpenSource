4538cfcaee573ea3ece90e989400b5020ca40696
Ingrid Gallardo <ingridg@codeaurora.org>
2014-12-09 19:01:33 -0800

    msm: mdss: always get the rotator destination format
    Rotator destination format can be different depending
    in the source format. Previously we were only checking
    the destination format for bwc or rot90, but this is
    wrong since there are other scenarios like rotator
    downscaling, where we need to get the destination
    pixel format, otherwise we get some misconfiguration
    and corruption is observed. This change makes sure
    that always we get the expected destination format
    for the rotator.
    
    Change-Id: I9e3e331e011fcf8301183560ac41fd94cae833c5
    Signed-off-by: Ingrid Gallardo <ingridg@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_mdp_intf_writeback.c
+++ drivers/video/fbdev/msm/mdss_mdp_intf_writeback.c
@@ -407,11 +407,8 @@ static int mdss_mdp_writeback_prepare_rot(struct mdss_mdp_ctl *ctl, void *arg)
 		return -EINVAL;
 	}
 
-	if (ctx->bwc_mode || ctx->rot90)
-		format = mdss_mdp_get_rotator_dst_format(rot->format,
-				ctx->rot90, ctx->bwc_mode);
-	else
-		format = rot->format;
+	format = mdss_mdp_get_rotator_dst_format(rot->format,
+			ctx->rot90, ctx->bwc_mode);
 
 	if (ctx->rot90)
 		ctx->opmode |= BIT(5); /* ROT 90 */
