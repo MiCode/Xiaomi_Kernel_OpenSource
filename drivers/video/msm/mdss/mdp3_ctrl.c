44997dae26ad2a8d072554a9ad17384d61bc5eaa
Krishna Chaitanya Parimi <cparimi@codeaurora.org>
2014-12-15 10:05:54 +0530

    msm: mdss: Do not stop histogram from control off in mdp3
    Histogram status is maintained in userspace code which is
    changed with every histogram start and stop ioctl call
    appropriately. Calling histogram stop from control off
    results in userspace not knowing the changed histogram
    collection status. Hence userspace may keep sending read
    requests for histogram not knowing if collection stopped.
    Removing histogram stop call from control off so that
    userspace alone controls the histogram block. This is
    how it was intended in MDP5 as well.
    
    Change-Id: I3e1c26c6794512bb39b4d1ade923c6940101460c
    Signed-off-by: Krishna Chaitanya Parimi <cparimi@codeaurora.org>

--- drivers/video/msm/mdss/mdp3_ctrl.c
+++ drivers/video/msm/mdss/mdp3_ctrl.c
@@ -752,8 +752,6 @@ static int mdp3_ctrl_off(struct msm_fb_data_type *mfd)
 
 	mdp3_ctrl_clk_enable(mfd, 1);
 
-	mdp3_histogram_stop(mdp3_session, MDP_BLOCK_DMA_P);
-
 	if (mdp3_session->in_splash_screen) {
 		rc = mdp3_session->dma->stop(mdp3_session->dma,
 						mdp3_session->intf);
