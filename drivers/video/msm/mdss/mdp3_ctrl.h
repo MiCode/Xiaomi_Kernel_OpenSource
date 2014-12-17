a3fd2da90421166110f4d0a1a2470ba0c3cb3c65
Nitesh Gupta <niteshg@codeaurora.org>
2014-12-17 22:20:46 +0530

    msm: mdss: Create sysfs entry for PU state in MDP3
    Creating a sysfs entry for PU state which will be used
    for dynamically switching between display PP features
    and PU.
    
    state of sysfs entry:
    0: PU disabled and PP enabled
    1: PU enabled and PP disabled
    
    Change-Id: If800db588339a3f2f346279c86edd841897a4481
    Signed-off-by: Nitesh Gupta <niteshg@codeaurora.org>

--- drivers/video/msm/mdss/mdp3_ctrl.h
+++ drivers/video/msm/mdss/mdp3_ctrl.h
@@ -66,6 +66,7 @@ struct mdp3_session_data {
 	int vsync_enabled;
 	atomic_t vsync_countdown; /* Used to count down  */
 	bool in_splash_screen;
+	int dyn_pu_state; /* dynamic partial update status */
 
 	bool dma_active;
 	struct completion dma_completion;
