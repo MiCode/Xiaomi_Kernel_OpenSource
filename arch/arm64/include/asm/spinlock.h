88a066074bb489ecae9f4959fc6bbf1d5145acc1
Rohit Vaswani <rvaswani@codeaurora.org>
2014-12-17 00:26:36 -0800

    ARM64: spinlock: Add SEV and dsb in unlock code
    In certain unexplained cases, the stlr alone might not wakeup
    the processor waiting in WFE on a spinlock.
    Add an explicity dsb and SEV in write_unlock, read_unlock
    and spin_unlock to ensure that the core waiting on the lock
    wakes up from WFE.
    Selectable by config option SEV_IN_LOCK_UNLOCK
    
    Change-Id: I691ff5713d4d564623b75b053b40d1f46d74868a
    Signed-off-by: Rohit Vaswani <rvaswani@codeaurora.org>

--- arch/arm64/include/asm/spinlock.h
+++ arch/arm64/include/asm/spinlock.h
@@ -86,6 +86,10 @@ static inline void arch_spin_unlock(arch_spinlock_t *lock)
 {
 	asm volatile(
 "	stlrh	%w1, %0\n"
+#ifdef CONFIG_ARM64_SEV_IN_LOCK_UNLOCK
+"	dsb sy\n"
+"	sev\n"
+#endif
 	: "=Q" (lock->owner)
 	: "r" (lock->owner + 1)
 	: "memory");
@@ -155,6 +159,10 @@ static inline void arch_write_unlock(arch_rwlock_t *rw)
 {
 	asm volatile(
 	"	stlr	%w1, %0\n"
+#ifdef CONFIG_ARM64_SEV_IN_LOCK_UNLOCK
+	"	dsb sy\n"
+	"	sev\n"
+#endif
 	: "=Q" (rw->lock) : "r" (0) : "memory");
 }
 
@@ -198,6 +206,10 @@ static inline void arch_read_unlock(arch_rwlock_t *rw)
 	"1:	ldxr	%w0, %2\n"
 	"	sub	%w0, %w0, #1\n"
 	"	stlxr	%w1, %w0, %2\n"
+#ifdef CONFIG_ARM64_SEV_IN_LOCK_UNLOCK
+	"	dsb sy\n"
+	"	sev\n"
+#endif
 	"	cbnz	%w1, 1b\n"
 	: "=&r" (tmp), "=&r" (tmp2), "+Q" (rw->lock)
 	:
