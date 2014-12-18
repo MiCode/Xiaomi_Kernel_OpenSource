da40a5a21b30aec3f3135c8bead6186a6855d4e0
Andrei Danaila <adanaila@codeaurora.org>
2014-12-18 12:45:02 -0800

    mhi: core: Enable additional MHI logging
    Enable additional MHI logs for monitoring SSR flow.
    
    CRs-Fixed: 773805
    Change-Id: I579c083ad9ec0238ab3a6d51fec25b4ce5ccf62a
    Signed-off-by: Andrei Danaila <adanaila@codeaurora.org>

--- drivers/platform/msm/mhi/mhi_macros.h
+++ drivers/platform/msm/mhi/mhi_macros.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -12,7 +12,7 @@
 #ifndef _H_MHI_MACROS
 #define _H_MHI_MACROS
 
-#define MHI_IPC_LOG_PAGES (50)
+#define MHI_IPC_LOG_PAGES (100)
 #define MHI_LOG_SIZE 0x1000
 #define MHI_LINK_STABILITY_WAIT_MS 100
 #define MHI_MAX_LINK_RETRIES 9
