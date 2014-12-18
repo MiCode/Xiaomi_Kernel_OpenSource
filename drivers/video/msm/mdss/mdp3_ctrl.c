ea95152dfc731f860cf0d77d8d22c4c60a4c4620
Shivaraj Shetty <shivaraj@codeaurora.org>
2014-12-18 15:13:21 +0530

    msm: mdss: fix backlight issue on ESD recovery
    On ESD recovery the backlight update call doesn't come in
    after resume. Due to this though the panel is recovered
    from ESD attack display doesn't show up as backlight is
    zero. Update the backlight on first commit after ESD
    recovery to avoid this.
    
    Change-Id: I3b6140c401096a2f60b004ba473e0dfcc1eb017e
    Signed-off-by: Shivaraj Shetty <shivaraj@codeaurora.org>

--- drivers/video/msm/mdss/mdp3_ctrl.c
+++ drivers/video/msm/mdss/mdp3_ctrl.c
@@ -765,6 +765,8 @@ static int mdp3_ctrl_on(struct msm_fb_data_type *mfd)
 	mdp3_session->clk_on = 1;
 
 	mdp3_session->first_commit = true;
+	if (mfd->panel_info->panel_dead)
+		mdp3_session->esd_recovery = true;
 
 	mdp3_session->status = 1;
 
@@ -1177,9 +1179,11 @@ static int mdp3_ctrl_display_commit_kickoff(struct msm_fb_data_type *mfd,
 	}
 
 	mdp3_session->vsync_before_commit = 0;
-	if (!splash_done && (panel && panel->set_backlight)) {
+	if ((panel && panel->set_backlight) && (!splash_done ||
+					(mdp3_session->esd_recovery == true))) {
 		panel->set_backlight(panel, panel->panel_info.bl_max);
 		splash_done = true;
+		mdp3_session->esd_recovery = false;
 	}
 
 	mutex_unlock(&mdp3_session->lock);
