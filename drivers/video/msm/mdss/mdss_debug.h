5573c7607611d5d467d37873644e406e24ef8f9d
Ingrid Gallardo <ingridg@codeaurora.org>
2014-12-19 06:08:54 -0800

    msm: mdss: debugfs: xlog: add support to dump from a work item
    Add support to initiate the dump from a work item
    so the driver can dump from an interrupt context.
    
    Change-Id: I51a9719884c826713cc4c9abe926205eaac4d721
    Signed-off-by: Ingrid Gallardo <ingridg@codeaurora.org>

--- drivers/video/msm/mdss/mdss_debug.h
+++ drivers/video/msm/mdss/mdss_debug.h
@@ -47,7 +47,11 @@ enum mdss_dbg_xlog_flag {
 		##__VA_ARGS__, DATA_LIMITER)
 
 #define MDSS_XLOG_TOUT_HANDLER(...)	\
-	mdss_xlog_tout_handler_default(__func__, ##__VA_ARGS__, \
+	mdss_xlog_tout_handler_default(false, __func__, ##__VA_ARGS__, \
+		XLOG_TOUT_DATA_LIMITER)
+
+#define MDSS_XLOG_TOUT_HANDLER_WQ(...)	\
+	mdss_xlog_tout_handler_default(true, __func__, ##__VA_ARGS__, \
 		XLOG_TOUT_DATA_LIMITER)
 
 #define MDSS_XLOG_DBG(...) mdss_xlog(__func__, __LINE__, MDSS_XLOG_DBG, \
@@ -130,7 +134,7 @@ void mdss_misr_crc_collect(struct mdss_data_type *mdata, int block_id);
 
 int mdss_create_xlog_debug(struct mdss_debug_data *mdd);
 void mdss_xlog(const char *name, int line, int flag, ...);
-void mdss_xlog_tout_handler_default(const char *name, ...);
+void mdss_xlog_tout_handler_default(bool queue, const char *name, ...);
 int mdss_xlog_tout_handler_iommu(struct iommu_domain *domain,
 	struct device *dev, unsigned long iova, int flags, void *token);
 #else
@@ -159,7 +163,8 @@ static inline int create_xlog_debug(struct mdss_data_type *mdata) { return 0; }
 static inline void mdss_xlog_dump(void) { }
 static inline void mdss_xlog(const char *name, int line, int flag...) { }
 static inline void mdss_dsi_debug_check_te(struct mdss_panel_data *pdata) { }
-static inline void mdss_xlog_tout_handler_default(const char *name, ...) { }
+static inline void mdss_xlog_tout_handler_default(bool queue,
+	const char *name, ...) { }
 static inline int  mdss_xlog_tout_handler_iommu(struct iommu_domain *domain,
 	struct device *dev, unsigned long iova, int flags, void *token) { }
 #endif
