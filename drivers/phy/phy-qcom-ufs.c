5cdbfe11ca75af09b97123498075b56a7fa02b8d
Gilad Broner <gbroner@codeaurora.org>
2014-12-21 10:48:42 +0200

    scsi: ufs-qcom: set device ref. clk bit
    As of HW major version 2, a new bit 'UFS_DEV_REF_CLK_EN' was added
    to UFS_CFG1 register which needs to be set as part of hibernate
    enter/exit sequences during suspend/resume.
    
    Change-Id: I66a6a75dc5a1cf130b1cee90ae20f9f950edfb3a
    Signed-off-by: Gilad Broner <gbroner@codeaurora.org>
    [imaund@codeaurora.org: Resolved context conflicts and updated a
      conditional in ufs_qcom_advertise_quirks to use the ufs_qcom_host
      struct when querying major hw versions]
    Signed-off-by: Ian Maund <imaund@codeaurora.org>
    [subhashj@codeaurora.org: resolved trivial merge conflicts]
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>
    [venkatg@codeaurora.org: resolved trivial merge conflicts,
    drop changes to include/linux/phy/phy-qcom-ufs.h]
    Signed-off-by: Venkat Gopalakrishnan <venkatg@codeaurora.org>

--- drivers/phy/phy-qcom-ufs.c
+++ drivers/phy/phy-qcom-ufs.c
@@ -144,7 +144,6 @@ int ufs_qcom_phy_base_init(struct platform_device *pdev,
 		phy_common->mmio = NULL;
 		dev_err(dev, "%s: ioremap for phy_mem resource failed %d\n",
 			__func__, err);
-		goto out;
 	}
 
 	/* "dev_ref_clk_ctrl_mem" is optional resource */
@@ -443,38 +442,6 @@ void ufs_qcom_phy_disable_ref_clk(struct phy *generic_phy)
 		clk_disable_unprepare(phy->ref_clk_src);
 		phy->is_ref_clk_enabled = false;
 	}
-}
-
-#define UFS_REF_CLK_EN	(1 << 5)
-
-static void ufs_qcom_phy_dev_ref_clk_ctrl(struct phy *generic_phy, bool enable)
-{
-	struct ufs_qcom_phy *phy = get_ufs_qcom_phy(generic_phy);
-
-	if (phy->dev_ref_clk_ctrl_mmio &&
-	    (enable ^ phy->is_dev_ref_clk_enabled)) {
-		u32 temp = readl_relaxed(phy->dev_ref_clk_ctrl_mmio);
-
-		if (enable)
-			temp |= UFS_REF_CLK_EN;
-		else
-			temp &= ~UFS_REF_CLK_EN;
-
-		writel_relaxed(temp, phy->dev_ref_clk_ctrl_mmio);
-		/* ensure that ref_clk is enabled/disabled before we return */
-		wmb();
-		phy->is_dev_ref_clk_enabled = enable;
-	}
-}
-
-void ufs_qcom_phy_enable_dev_ref_clk(struct phy *generic_phy)
-{
-	ufs_qcom_phy_dev_ref_clk_ctrl(generic_phy, true);
-}
-
-void ufs_qcom_phy_disable_dev_ref_clk(struct phy *generic_phy)
-{
-	ufs_qcom_phy_dev_ref_clk_ctrl(generic_phy, false);
 }
 
 /* Turn ON M-PHY RMMI interface clocks */
