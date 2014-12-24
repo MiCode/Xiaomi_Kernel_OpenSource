7269ac10017b52fb4dcb5cf5cbb4d5c18092fe40
Jack Pham <jackp@codeaurora.org>
2014-12-23 20:01:40 -0800

    usb: dwc3-msm: Ignore multiple VBUS notifications
    VBUS notifications arrive externally. There is a possibility that
    they can arrive spuriously with the same value, which can cause
    the resume_work function to get queued multiple times back-to-back.
    This can result in the runtime PM counters getting out of sync,
    For instance in the case of cable plug-in, if resume_work is called
    multiple times, pm_runtime_get() is called multiply resulting in
    runaway usage_counter increase. Upon cable disconnect, there would
    be a mismatch in the number of times pm_runtime_put() is called
    which leaves the device in a non-suspended state.
    
    Prevent this by queuing the work function only when VBUS state
    changes. Also add additional checking to the PHY_RESUME handling
    such that pm_runtime_get() is not called if the usage counter is
    already non-zero.
    
    Change-Id: Ie7ac27c32e81f3c149ad7f2a64d8c8ebd0073617
    Signed-off-by: Jack Pham <jackp@codeaurora.org>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -2301,6 +2301,9 @@ static int dwc3_msm_power_set_property_usb(struct power_supply *psy,
 		dev_dbg(mdwc->dev, "%s: notify xceiv event\n", __func__);
 		if (mdwc->otg_xceiv && !mdwc->ext_inuse &&
 		    (mdwc->ext_xceiv.otg_capability || !init)) {
+			if (mdwc->ext_xceiv.bsv == val->intval)
+				break;
+
 			mdwc->ext_xceiv.bsv = val->intval;
 			/*
 			 * Set debouncing delay to 120ms. Otherwise battery
