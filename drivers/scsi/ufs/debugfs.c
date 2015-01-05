6021822ecaf116058f1099992ba8d4fce9e08d6b
Dov Levenglick <dovl@codeaurora.org>
2015-01-05 17:48:45 +0200

    scsi: ufs-qcom: add print suppressing debugfs mechanism
    Provide a mechanism for the userspace to suppress specific
    debug prints via debugfs. This is useful in order to avoid
    cases where too much printing would cause watchdog timers
    to expire.
    
    Change-Id: I2500b7621b631e260d98595ed8cfe7d5a496dc10
    Signed-off-by: Dov Levenglick <dovl@codeaurora.org>

--- drivers/scsi/ufs/debugfs.c
+++ drivers/scsi/ufs/debugfs.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1065,6 +1065,9 @@ void ufsdbg_add_debugfs(struct ufs_hba *hba)
 
 	ufsdbg_setup_fault_injection(hba);
 
+	if (hba->vops && hba->vops->add_debugfs)
+		hba->vops->add_debugfs(hba, hba->debugfs_files.debugfs_root);
+
 	return;
 
 err:
@@ -1076,6 +1079,8 @@ err_no_root:
 
 void ufsdbg_remove_debugfs(struct ufs_hba *hba)
 {
+	if (hba->vops && hba->vops->remove_debugfs)
+		hba->vops->remove_debugfs(hba);
 	debugfs_remove_recursive(hba->debugfs_files.debugfs_root);
 	kfree(hba->ufs_stats.tag_stats);
 
