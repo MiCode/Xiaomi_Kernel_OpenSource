4a998284dd04ab7c589705e64e701b299cf976a5
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2015-01-07 10:48:36 -0800

    power: power_supply: add HVDCP power supply type
    Add power supply type for High voltage dedicated charging port
    (HVDCP) chargers.
    
    CRs-Fixed: 775241
    Change-Id: I9148bb547640d2d721cedac8f652ec234f9fa4be
    Signed-off-by: Subbaraman Narayanamurthy <subbaram@codeaurora.org>

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -185,6 +185,7 @@ enum power_supply_type {
 	POWER_SUPPLY_TYPE_USB_DCP,	/* Dedicated Charging Port */
 	POWER_SUPPLY_TYPE_USB_CDP,	/* Charging Downstream Port */
 	POWER_SUPPLY_TYPE_USB_ACA,	/* Accessory Charger Adapters */
+	POWER_SUPPLY_TYPE_USB_HVDCP,	/* High Voltage DCP */
 	POWER_SUPPLY_TYPE_WIRELESS,	/* Accessory Charger Adapters */
 	POWER_SUPPLY_TYPE_BMS,		/* Battery Monitor System */
 	POWER_SUPPLY_TYPE_USB_PARALLEL,		/* USB Parallel Path */
