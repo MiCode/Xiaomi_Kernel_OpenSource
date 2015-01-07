3776ca43a9bc5937312d14ed7002c6a409d9cbb5
Mitchel Humpherys <mitchelh@codeaurora.org>
2015-01-06 17:56:50 -0800

    ion: system_secure_heap: add DEFER_FREE heap flag
    Ion's deferred freeing feature has been shown to greatly improve user
    experience for some use cases.  Enable the feature in the system secure
    heap by adding the appropriate heap flag.
    
    Change-Id: I9be7f2b3365b2d1a086acc3d182d46bfee2e6cfe
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- drivers/staging/android/ion/ion_system_secure_heap.c
+++ drivers/staging/android/ion/ion_system_secure_heap.c
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (c) 2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -200,6 +200,7 @@ struct ion_heap *ion_system_secure_heap_create(struct ion_platform_heap *unused)
 		return ERR_PTR(-ENOMEM);
 	heap->heap.ops = &system_secure_heap_ops;
 	heap->heap.type = ION_HEAP_TYPE_SYSTEM_SECURE;
+	heap->heap.flags = ION_HEAP_FLAG_DEFER_FREE;
 	heap->sys_heap = get_ion_heap(ION_SYSTEM_HEAP_ID);
 	return &heap->heap;
 }
