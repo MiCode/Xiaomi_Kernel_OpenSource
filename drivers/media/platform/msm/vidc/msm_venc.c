f54cf6ffeeef10c7aa4d672797c652693a7ed23c
Ashray Kulkarni <ashrayk@codeaurora.org>
2015-01-07 13:05:14 -0800

    msm: vidc: change timestamp variable from 32 bit to 64 bit
    change timestamp variable from 32 bit to 64 bit thereby
    increasing timestamp precision.
    
    Change-Id: I454f907500d2aa8eeeabb1c385f2b3dd19f45206
    Signed-off-by: Ashray Kulkarni <ashrayk@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_venc.c
+++ drivers/media/platform/msm/vidc/msm_venc.c
@@ -3026,7 +3026,8 @@ int msm_venc_enum_fmt(struct msm_vidc_inst *inst, struct v4l2_fmtdesc *f)
 
 int msm_venc_s_parm(struct msm_vidc_inst *inst, struct v4l2_streamparm *a)
 {
-	u32 property_id = 0, us_per_frame = 0;
+	u32 property_id = 0;
+	u64 us_per_frame = 0;
 	void *pdata;
 	int rc = 0, fps = 0;
 	struct hal_frame_rate frame_rate;
