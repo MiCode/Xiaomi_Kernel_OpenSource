464fda2a84eb968e553ec6e01ffbae3d84fca017
Sreesudhan Ramakrish Ramkumar <srramku@codeaurora.org>
2015-01-07 17:58:16 -0800

    msm: camera: ispif: Validate vfe_intf parameter
    Validate vfe_intf parameter to avoid invalid register access.
    
    Change-Id: I6081ff7c43dea34215a3c1d9b6bb948f6e4c435a
    Signed-off-by: Sreesudhan Ramakrish Ramkumar <srramku@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/ispif/msm_ispif.c
+++ drivers/media/platform/msm/camera_v2/ispif/msm_ispif.c
@@ -63,8 +63,8 @@ static void msm_ispif_io_dump_reg(struct ispif_device *ispif)
 static inline int msm_ispif_is_intf_valid(uint32_t csid_version,
 	uint8_t intf_type)
 {
-	return (csid_version <= CSID_VERSION_V22 && intf_type != VFE0) ?
-		false : true;
+	return ((csid_version <= CSID_VERSION_V22 && intf_type != VFE0) ||
+		(intf_type >= VFE_MAX)) ? false : true;
 }
 
 static struct msm_cam_clk_info ispif_8626_reset_clk_info[] = {
@@ -587,11 +587,6 @@ static int msm_ispif_config(struct ispif_device *ispif,
 
 	for (i = 0; i < params->num; i++) {
 		vfe_intf = params->entries[i].vfe_intf;
-		if (vfe_intf >= VFE_MAX) {
-			pr_err("%s: %d invalid i %d vfe_intf %d\n", __func__,
-				__LINE__, i, vfe_intf);
-			return -EINVAL;
-		}
 		if (!msm_ispif_is_intf_valid(ispif->csid_version,
 				vfe_intf)) {
 			pr_err("%s: invalid interface type\n", __func__);
