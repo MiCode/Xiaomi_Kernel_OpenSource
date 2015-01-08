63d2c6c06e14d42dac143d0c5e6d13e80701c735
Jeykumar Sankaran <jsanka@codeaurora.org>
2015-01-08 15:58:25 -0800

    msm: mdss: Add panic signal support for msm8992
    Add panic signal support for msm8992.
    
    Change-Id: I5fe9d370bebf3ccd82f6930da0d4e833fffb67e4
    Signed-off-by: Jeykumar Sankaran <jsanka@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp.h
+++ drivers/video/msm/mdss/mdss_mdp.h
@@ -710,7 +710,9 @@ static inline int mdss_mdp_panic_signal_support_mode(
 		    IS_MDSS_MAJOR_MINOR_SAME(mdata->mdp_rev,
 					MDSS_MDP_HW_REV_108) ||
 		    IS_MDSS_MAJOR_MINOR_SAME(mdata->mdp_rev,
-					MDSS_MDP_HW_REV_109))
+					MDSS_MDP_HW_REV_109) ||
+		    IS_MDSS_MAJOR_MINOR_SAME(mdata->mdp_rev,
+					MDSS_MDP_HW_REV_110))
 			signal_mode = MDSS_MDP_PANIC_COMMON_REG_CFG;
 		else if (IS_MDSS_MAJOR_MINOR_SAME(mdata->mdp_rev,
 					MDSS_MDP_HW_REV_107))
