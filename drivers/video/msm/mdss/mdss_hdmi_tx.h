3b8f860d0a64a35a772e09f7daee6ffc6119870b
Ajay Singh Parmar <aparmar@codeaurora.org>
2015-01-08 17:53:02 -0800

    msm: mdss: hdmi: proper synchronization between hpd and tx power
    Currently, HDMI driver has HPD (Hot Plug Detect) work to handle HPD
    interrupts and power off work to handle the Tx power off. Power off
    work waits for HPD work to finish if in progress. In case of suspend,
    this can lead to a dead lock or power off work might starve for long
    time HPD work may take some time to finish resulting in suspend or next
    resume to timeout.
    
    Power off work was added to unblock the user thread as it used to take
    a long time waiting for audio engine to finish. That part of the code
    has been moved to HPD work. Now power off can be synchronous with the
    call as now it doesn't account for the delay. This removes the dependency
    on other work.
    
    Also, use a separate mutex for power on/off. The existing mutex is being
    used in large number of places which are not related. During suspend/resume
    use of mutex in places like sysfs read/write might result in longer hold of
    the mutex resulting in power off delay which eventually may timeout the
    suspend or resume.
    
    Change-Id: Ie1e461f6cb7f90d440bfd203b935cd7dea069b5e
    Signed-off-by: Ajay Singh Parmar <aparmar@codeaurora.org>
    Signed-off-by: Raghavendra Ambadas <rambad@codeaurora.org>

--- drivers/video/msm/mdss/mdss_hdmi_tx.h
+++ drivers/video/msm/mdss/mdss_hdmi_tx.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2010-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -56,6 +56,7 @@ struct hdmi_tx_ctrl {
 
 	struct mutex mutex;
 	struct mutex lut_lock;
+	struct mutex power_mutex;
 	struct mutex cable_notify_mutex;
 	struct list_head cable_notify_handlers;
 	struct kobject *kobj;
@@ -77,10 +78,11 @@ struct hdmi_tx_ctrl {
 	u8  timing_gen_on;
 	u32 mhl_max_pclk;
 	u8  mhl_hpd_on;
-	struct completion hpd_done;
+
+	struct completion hpd_int_done;
+	struct completion hpd_off_done;
 	struct work_struct hpd_int_work;
 
-	struct work_struct power_off_work;
 	struct work_struct cable_notify_work;
 
 	bool hdcp_feature_on;
