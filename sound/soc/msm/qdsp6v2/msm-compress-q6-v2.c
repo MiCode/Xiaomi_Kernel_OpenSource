657ac3c64d54fbc74e15c527c8489e388500cd60
Dhananjay Kumar <dhakumar@codeaurora.org>
2015-01-16 00:34:46 +0800

    ASoC: msm: Add support to enable PBE in DSP
    Add interface to support configuring pbe effects
    in DSP for offload playback.
    PBE provides BassBoost support on small devices
    like handset speaker.
    Handset speakers are too small to produce proper
    Bass, hence PBE is introduced, which adds effect
    similar to BassBoost but suitable for small devices.
    
    Change-Id: Ic66f3073cb259c8ef593a1ad6aa06d14efbfb058
    Signed-off-by: wjiang <wjiang@codeaurora.org>
    Signed-off-by: Dhananjay Kumar <dhakumar@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
@@ -189,6 +189,7 @@ struct query_audio_effect {
 
 struct msm_compr_audio_effects {
 	struct bass_boost_params bass_boost;
+	struct pbe_params pbe;
 	struct virtualizer_params virtualizer;
 	struct reverb_params reverb;
 	struct eq_params equalizer;
@@ -2211,6 +2212,14 @@ static int msm_compr_audio_effects_config_put(struct snd_kcontrol *kcontrol,
 						prtd->audio_client->topology))
 			msm_audio_effects_bass_boost_handler(prtd->audio_client,
 						   &(audio_effects->bass_boost),
+						     values);
+		break;
+	case PBE_MODULE:
+		pr_debug("%s: PBE_MODULE\n", __func__);
+		if (msm_audio_effects_is_effmodule_supp_in_top(effects_module,
+						prtd->audio_client->topology))
+			msm_audio_effects_pbe_handler(prtd->audio_client,
+						   &(audio_effects->pbe),
 						     values);
 		break;
 	case EQ_MODULE:
