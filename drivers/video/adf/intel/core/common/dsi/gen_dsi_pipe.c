601636b6df3871e2ed125632c4c7a3c3abfccd82
Deepak M <m.deepak@intel.com>
2015-01-17 13:08:14 +0530

    MUST_REBASE [VPG}: intel/core/dsi: Adding missing license
    The missing licence is added to the file gen_dsi_pipe.c
    
    For: GMINL-5213
    Change-Id: I22483b078da6704627c5c86053a70a30d6063e43
    Signed-off-by: Deepak M <m.deepak@intel.com>

--- drivers/video/adf/intel/core/common/dsi/gen_dsi_pipe.c
+++ drivers/video/adf/intel/core/common/dsi/gen_dsi_pipe.c
@@ -1,8 +1,17 @@
 /*
- * gen_dsi_pipe.c
+ * Copyright (C) 2014, Intel Corporation.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  *  Created on: September 1, 2014
- *      Author: Shobhit Kumar <shobhit.kumar@intel.com>
+ *  Author: Shobhit Kumar <shobhit.kumar@intel.com>
  */
 
 #include <drm/drm_mode.h>
