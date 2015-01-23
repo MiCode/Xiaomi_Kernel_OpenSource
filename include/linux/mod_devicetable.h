2571ab189b425a304f2f98a37eff9ed604db93d6
Sudheer Papothi <spapothi@codeaurora.org>
2015-01-24 01:50:48 +0530

    soundwire: add support for device table match
    With this patch soundwire drivers can use id_table and
    MODULE_DEVICE_TABLE() method to bind against the devices just
    like I2C or SPI drivers.
    
    Change-Id: I4e8eee3cb9626a5dc4fbfa238b5d2a578355f2a3
    Signed-off-by: Sudheer Papothi <spapothi@codeaurora.org>
    Signed-off-by: Meng Wang <mwang@codeaurora.org>

--- include/linux/mod_devicetable.h
+++ include/linux/mod_devicetable.h
@@ -490,6 +490,16 @@ struct spmi_device_id {
 	kernel_ulong_t driver_data;	/* Data private to the driver */
 };
 
+/* soundwire */
+
+#define SOUNDWIRE_NAME_SIZE	32
+#define SOUNDWIRE_MODULE_PREFIX "swr:"
+
+struct swr_device_id {
+	char name[SOUNDWIRE_NAME_SIZE];
+	kernel_ulong_t driver_data;	/* Data private to the driver */
+};
+
 /* dmi */
 enum dmi_field {
 	DMI_NONE,
