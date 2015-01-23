1649952d2491ae9c9acb4a593d23938a22089694
Ankit Gupta <ankgupta@codeaurora.org>
2015-01-22 20:20:14 -0700

    i2c-msm-v2: update adapter device node in i2c driver
    I2C framework in 3.14 kernel registers child nodes in the
    i2c core instead of doing this manually in each driver.
    This patch ensures adapter device node is same as i2c
    bus platform device node before registration of the
    nodes to i2c framework.
    
    Change-Id: I9e5d4f7e4d924b0856af5afcb1c2e61ffd9d826f
    Signed-off-by: Ankit Gupta <ankgupta@codeaurora.org>

--- drivers/i2c/busses/i2c-msm-v2.c
+++ drivers/i2c/busses/i2c-msm-v2.c
@@ -3306,6 +3306,7 @@ static int i2c_msm_frmwrk_reg(struct platform_device *pdev,
 
 	ctrl->adapter.nr = pdev->id;
 	ctrl->adapter.dev.parent = &pdev->dev;
+	ctrl->adapter.dev.of_node = pdev->dev.of_node;
 	ret = i2c_add_numbered_adapter(&ctrl->adapter);
 	if (ret) {
 		dev_err(ctrl->dev, "error i2c_add_adapter failed\n");
