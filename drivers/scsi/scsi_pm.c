d5b76d4f559111e7d979027e477463913df8060e
Subhash Jadavani <subhashj@codeaurora.org>
2015-01-25 15:06:37 -0800

    scsi: pm: fix null pointer access during sg_open
    When user space application opens any scsi generic device node (/dev/sg*),
    we will have following call stack:
    -> sg_open()
     -> scsi_autopm_get_device()
      -> pm_runtime_get_sync()
       -> scsi_runtime_resume()
        -> sdev_runtime_resume()
         -> blk_post_runtime_resume()
    
    The request queue device pointer (q->dev) is deferenced but it is NULL.
    q->dev is assigned when blk_pm_runtime_init() is called which only takes
    place for sd/sr type scsi devices.
    
    SCSI generic driver handles runtime PM get/put on its instead of block
    layer handling it. We can move the runtime PM get/put responsibility to
    block layer only if all the requests handled by sg driver are getting
    inserted to block layer request queue but this is not always true.
    For example, if LLD ioctl is issued via scsi generic ioctl then it may
    not get converted to block layer request (and hence doesn't get inserted
    into block layer request queue) and this may put us in situation where
    LLD driver handler the ioctl when it's runtime suspended.
    
    Better solution would be to let the sg driver manage its own runtime PM
    get/sync operations and fix the broken scsi_runtime_suspend and
    scsi_runtime_resume callbacks to handle SCSI devices which do not have
    upper layer driver (like SCSI disk).
    
    Change-Id: I1cae7f992f21a1c420d38c275a817d06baa8923d
    Signed-off-by: Gilad Broner <gbroner@codeaurora.org>
    [subhashj@codeaurora.org: fixed the compilation failure]
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- drivers/scsi/scsi_pm.c
+++ drivers/scsi/scsi_pm.c
@@ -215,11 +215,31 @@ static int scsi_bus_restore(struct device *dev)
 
 #ifdef CONFIG_PM_RUNTIME
 
+static int do_scsi_runtime_suspend(struct device *dev,
+				   const struct dev_pm_ops *pm)
+{
+	return pm && pm->runtime_suspend ? pm->runtime_suspend(dev) : 0;
+}
+
+static int do_scsi_runtime_resume(struct device *dev,
+				   const struct dev_pm_ops *pm)
+{
+	return pm && pm->runtime_resume ? pm->runtime_resume(dev) : 0;
+}
+
 static int sdev_runtime_suspend(struct device *dev)
 {
 	const struct dev_pm_ops *pm = dev->driver ? dev->driver->pm : NULL;
 	struct scsi_device *sdev = to_scsi_device(dev);
 	int err;
+
+	if (!sdev->request_queue->dev) {
+		err = scsi_dev_type_suspend(dev, do_scsi_runtime_suspend);
+		if (err == -EAGAIN)
+			pm_schedule_suspend(dev, jiffies_to_msecs(
+					round_jiffies_up_relative(HZ/10)));
+		return err;
+	}
 
 	err = blk_pre_runtime_suspend(sdev->request_queue);
 	if (err)
@@ -249,6 +269,9 @@ static int sdev_runtime_resume(struct device *dev)
 	struct scsi_device *sdev = to_scsi_device(dev);
 	const struct dev_pm_ops *pm = dev->driver ? dev->driver->pm : NULL;
 	int err = 0;
+
+	if (!sdev->request_queue->dev)
+		return scsi_dev_type_resume(dev, do_scsi_runtime_resume);
 
 	blk_pre_runtime_resume(sdev->request_queue);
 	if (pm && pm->runtime_resume)
