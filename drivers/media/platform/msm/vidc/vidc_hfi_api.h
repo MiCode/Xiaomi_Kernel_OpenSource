e48812db6eeac2ae05dbc5dcb60b194f1c15e3ea
Deva Ramasubramanian <dramasub@codeaurora.org>
2015-01-28 15:45:55 -0800

    msm: vidc: Don't hide event messages inside command messages
    The event struct is being passed as a cookie inside the command message
    struct.  There's no reason to do this, especially when we can just
    return the event struct directly.
    
    Also save a level of indentation in handle_event() by performing a
    negative NULL check rather than a positive NULL check.
    
    Change-Id: I12a392a9f9bbf245cbba57a5c36f579f0b6b0418
    Signed-off-by: Deva Ramasubramanian <dramasub@codeaurora.org>

--- drivers/media/platform/msm/vidc/vidc_hfi_api.h
+++ drivers/media/platform/msm/vidc/vidc_hfi_api.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1154,7 +1154,7 @@ struct msm_vidc_cb_cmd_done {
 
 struct msm_vidc_cb_event {
 	u32 device_id;
-	u32 session_id;
+	void *session_id;
 	enum vidc_status status;
 	u32 height;
 	u32 width;
