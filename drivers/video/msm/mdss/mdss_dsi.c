f104acdcf1e339b522d23c87381e013a2aee01ca
Aravind Venkateswaran <aravindh@codeaurora.org>
2015-01-29 14:39:27 -0800

    msm: mdss: dsi: fix dsi init sequence with ulps during suspend feature
    If continuous splash screen feature is disabled and ULPS during suspend
    feature is enabled, DSI phy initialization does when device is booting
    up, due to incorrect checks in the current implementation. Fix this issue
    by ensuring that DSI phy initialization is always done when DSI interface
    is turned on.
    
    Signed-off-by: Aravind Venkateswaran <aravindh@codeaurora.org>
    Change-Id: Icdc90eef8d5f3380b47f67268249eb670f8ca824

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -740,7 +740,15 @@ int mdss_dsi_on(struct mdss_panel_data *pdata)
 	 * clocks.
 	 */
 	mdss_dsi_clk_ctrl(ctrl_pdata, DSI_BUS_CLKS, 1);
-	if (!pdata->panel_info.ulps_suspend_enabled) {
+
+	/*
+	 * If ULPS during suspend feature is enabled, then DSI PHY was
+	 * left on during suspend. In this case, we do not need to reset/init
+	 * PHY. This would have already been done when the BUS clocks are
+	 * turned on. However, if cont splash is disabled, the first time DSI
+	 * is powered on, phy init needs to be done unconditionally.
+	 */
+	if (!pdata->panel_info.ulps_suspend_enabled || !ctrl_pdata->ulps) {
 		mdss_dsi_phy_sw_reset(ctrl_pdata);
 		mdss_dsi_phy_init(ctrl_pdata);
 		mdss_dsi_ctrl_setup(ctrl_pdata);
