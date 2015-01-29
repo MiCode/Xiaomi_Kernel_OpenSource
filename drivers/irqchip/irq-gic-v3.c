4507cc735dcca6198490928a43075837d996862a
Stepan Moskovchenko <stepanm@codeaurora.org>
2015-01-29 12:31:12 -0800

    arm64: gicv3: Insert barriers after SGI operations
    Some early samples of the MSMTHULIUM SoC require that two
    successive writes to the ICC_SGI1R_EL1 register be
    separated by a barrier instruction. Implement a Kconfig
    option to enable this behavior if necessary.
    
    Change-Id: I830f5a6db3f21c8e0a9cf2815b602dc56eac7598
    Signed-off-by: Stepan Moskovchenko <stepanm@codeaurora.org>

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -138,7 +138,17 @@ static void __maybe_unused gic_write_grpen1(u64 val)
 
 static void __maybe_unused gic_write_sgi1r(u64 val)
 {
+#ifdef CONFIG_MSM_GIC_SGI_NEEDS_BARRIER
+	static DEFINE_RAW_SPINLOCK(sgi_lock);
+	unsigned long flags;
+	raw_spin_lock_irqsave(&sgi_lock, flags);
+#endif
+
 	asm volatile("msr_s " __stringify(ICC_SGI1R_EL1) ", %0" : : "r" (val));
+#ifdef CONFIG_MSM_GIC_SGI_NEEDS_BARRIER
+	dsb(nsh);
+	raw_spin_unlock_irqrestore(&sgi_lock, flags);
+#endif
 }
 
 static void gic_enable_sre(void)
