881bc12668d55d310f99d15044a253fa700696a5
William Clark <wclark@codeaurora.org>
2015-01-29 18:03:55 -0800

    msm: crypto: Modify crypto libraries for FIPS139-2 Certificate
    Modify header files by adding data strcture and MACROs to support
    certification related self-tests and integrity tests.
    Add data structure qcrypto_func_set in qcrpto.h, and use it in
    dm-req-crypt.c, to support qualcomm crypto library as a dynamic
    loadable kernel module (for the FIPS certificate requirement).
    
    Change-Id: I9e60374b93014b5783559820de2056168fb73bbd
    Acked-by: Jing Deng <jingd@qti.qualcomm.com>
    Signed-off-by: Zhen Kong <zkong@codeaurora.org>
    Signed-off-by: William Clark <wclark@codeaurora.org>

--- include/linux/qcrypto.h
+++ include/linux/qcrypto.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -51,5 +51,14 @@ int qcrypto_cipher_set_device_hw(struct ablkcipher_request *req,
 				unsigned int fde_pfe,
 				unsigned int hw_inst);
 
+struct qcrypto_func_set {
+	int (*cipher_set)(struct ablkcipher_request *req,
+			unsigned int fde_pfe,
+			unsigned hw_inst);
+	int (*cipher_flag)(struct ablkcipher_request *req, unsigned int flags);
+	int (*get_num_engines)(void);
+	void (*get_engine_list)(size_t num_engines,
+				struct crypto_engine_entry *arr);
+};
 
 #endif /* _DRIVERS_CRYPTO_MSM_QCRYPTO_H */
