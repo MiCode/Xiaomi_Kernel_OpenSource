096209caa43c9e39d54144d0597502ec9b08cacc
Subhash Jadavani <subhashj@codeaurora.org>
2015-01-29 18:52:18 -0800

    scsi: ufs-qcom: disable low power modes and run slow on newer controller
    Newer UFS controllers (revision 2) still need to be validated properly
    hence disable the low power modes for it until they are validated
    thoroughly.
    Also run the UFS interface in PWM gear until High Speed Gear operation
    is stabililized.
    
    Change-Id: I310b49030d084557f487f3095fc7e1cfa68335e6
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- drivers/scsi/ufs/ufs-qcom.c
+++ drivers/scsi/ufs/ufs-qcom.c
@@ -900,6 +900,13 @@ static int ufs_qcom_pwr_change_notify(struct ufs_hba *hba,
 				ufs_qcom_cap.hs_rx_gear = UFS_HS_G2;
 		}
 
+		/*
+		 * Until High Speed gear mode is stabilized, operate in PWM gear
+		 * mode for newer controller versions.
+		 */
+		if (host->hw_ver.major >= 0x2)
+			ufs_qcom_cap.desired_working_mode = SLOW;
+
 		ret = ufs_qcom_get_pwr_dev_param(&ufs_qcom_cap,
 						 dev_max_params,
 						 dev_req_params);
@@ -1006,9 +1013,11 @@ static void ufs_qcom_set_caps(struct ufs_hba *hba)
 
 	ufs_qcom_get_controller_revision(hba, &major, &minor, &step);
 
-	hba->caps |= UFSHCD_CAP_CLK_GATING | UFSHCD_CAP_CLK_SCALING;
-	hba->caps |= UFSHCD_CAP_AUTO_BKOPS_SUSPEND;
-	hba->caps |= UFSHCD_CAP_HIBERN8_ENTER_ON_IDLE;
+	if (host->hw_ver.major < 0x2) {
+		hba->caps |= UFSHCD_CAP_CLK_GATING | UFSHCD_CAP_CLK_SCALING;
+		hba->caps |= UFSHCD_CAP_AUTO_BKOPS_SUSPEND;
+		hba->caps |= UFSHCD_CAP_HIBERN8_ENTER_ON_IDLE;
+	}
 
 	if (host->hw_ver.major >= 0x2) {
 		hba->caps |= UFSHCD_CAP_POWER_COLLAPSE_DURING_HIBERN8;
