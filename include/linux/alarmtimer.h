b93528d4d4e00c220b8adde3a67000b63897043b
Mao Jinlong <c_jmao@codeaurora.org>
2015-01-30 19:44:48 +0800

    alarm: init power_on_alarm in alarm_dev_init
    The power_on alarm might be missed during suspend/resume if
    power_on_alarm was not set in some cases like power-off charging
    using wall charger. So init power_on_alarm according to RTC alarm
    reg during booting to guarantee power_on alarm will not be missed
    by accident.
    
    Change-Id: If4e84b5d34fd199897b178081e764f96d2b64194
    Signed-off-by: Mao Jinlong <c_jmao@codeaurora.org>

--- include/linux/alarmtimer.h
+++ include/linux/alarmtimer.h
@@ -49,6 +49,7 @@ void alarm_restart(struct alarm *alarm);
 int alarm_try_to_cancel(struct alarm *alarm);
 int alarm_cancel(struct alarm *alarm);
 void set_power_on_alarm(long secs, bool enable);
+void power_on_alarm_init(void);
 
 u64 alarm_forward(struct alarm *alarm, ktime_t now, ktime_t interval);
 u64 alarm_forward_now(struct alarm *alarm, ktime_t interval);
