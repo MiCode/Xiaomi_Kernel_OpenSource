9f4354d2eabe988253e15a60e02b664be0887ee3
Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
2015-02-02 18:08:07 +0530

    msm: thermal: Add support for sensor manager in KTM
    Add and export sensor manager framework APIs for kernel clients
    who are interested in thermal sensors. Sensor manager allows
    multiple clients to set and get notification for a threshold.
    Clients can also use multiple sensors to trigger a single
    mitigation action.
    
    Below are main sensor manager APIs to monitor sensor thresholds.
    
    1.int sensor_mgr_init_threshold(struct threshold_info *thresh_inp,
    			int sensor_id, int32_t high_temp,
    			int32_t low_temp,
    			void (*callback)(struct therm_threshold *))
    
    API to initialize thresholds data structure for sensor(s) with high
    and low thresholds and threshold callback.
    @thresh_inp: Client threshold data structure.
    @sensor_id: Sensor h/w ID to be monitored. Use MONITOR_ALL_TSENS to
                monitor all temperature sensor.
    @high_temp: Trigger threshold value for sensor_id or all sensors.
    @low_temp: Clear threshold value for sensor_id or all sensors.
    @callback: Callback pointer for threshold notification.
    
    Returns zero on success, negative error number on failure.
    
    2.int sensor_mgr_set_threshold(uint32_t zone_id,
    			struct sensor_threshold *threshold)
    
    It sets thermal threshold trips for a sensor.
    @zone_id: Thermal zone id for the sensor.
    @threshold: Threshold info for the sensor.
    
    Returns which threshold is set on success, negative error number
    on failure. MACRO IS_HI_THRESHOLD_SET/IS_LOW_THRESHOLD_SET can be
    used to decipher which threshold being set.
    
    3.int sensor_mgr_convert_id_and_set_threshold(
    			struct threshold_info *thresh_inp)
    
    It accepts sensor HW id, converts it to sensor zone id and sets
    thermal threshold for those sensors listed in threshold info.
    @thresh_inp: Client threshold data structure.
    
    Returns zero on success, negative error number on failure.
    
    4.void sensor_mgr_remove_threshold(struct threshold_info *thresh_inp)
    
    It cancels threshold notification and removes threshold from sensor
    manager threshold list.
    @thresh_inp: Threshold info which needs to be removed.
    
    Change-Id: I916104bde472bd9b45bdb66d81b09cd850e11fba
    Signed-off-by: Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -13,6 +13,12 @@
 
 #ifndef __MSM_THERMAL_H
 #define __MSM_THERMAL_H
+
+#include <linux/thermal.h>
+
+#define MAX_THRESHOLD  2
+#define TSENS_NAME_MAX 20
+#define MONITOR_ALL_TSENS -1
 
 struct msm_thermal_data {
 	struct platform_device *pdev;
@@ -55,6 +61,29 @@ struct msm_thermal_data {
 	int32_t therm_reset_temp_degC;
 };
 
+enum sensor_id_type {
+	THERM_ZONE_ID,
+	THERM_TSENS_ID,
+	THERM_ID_MAX_NR,
+};
+
+struct threshold_info;
+struct therm_threshold {
+	int32_t                     sensor_id;
+	enum sensor_id_type         id_type;
+	struct sensor_threshold     threshold[MAX_THRESHOLD];
+	int32_t                     trip_triggered;
+	void (*notify)(struct therm_threshold *);
+	struct threshold_info       *parent;
+};
+
+struct threshold_info {
+	uint32_t                     thresh_ct;
+	bool                         thresh_triggered;
+	struct list_head             list_ptr;
+	struct therm_threshold       *thresh_list;
+};
+
 #ifdef CONFIG_THERMAL_MONITOR
 extern int msm_thermal_init(struct msm_thermal_data *pdata);
 extern int msm_thermal_device_init(void);
@@ -66,6 +95,57 @@ extern int msm_thermal_get_freq_plan_size(uint32_t cluster,
 	unsigned int *table_len);
 extern int msm_thermal_get_cluster_freq_plan(uint32_t cluster,
 	unsigned int *table_ptr);
+/**
+ * sensor_mgr_init_threshold - Initialize thresholds data structure for
+ *                             sensor(s) with high and low thresholds and
+ *                             threshold callback.
+ *
+ * @thresh_inp: Client threshold data structure.
+ * @sensor_id: Sensor h/w ID to be monitored. Use MONITOR_ALL_TSENS
+ *             to monitor all temperature sensors.
+ *
+ * @high_temp: Trigger threshold value for sensor_id or all sensors.
+ * @low_temp: Clear threshold value for sensor_id or all sensors.
+ * @callback: Callback pointer for threshold notification.
+ *
+ * Returns which threshold is set on success, negative error number
+ * on failure. MACRO IS_HI_THRESHOLD_SET/IS_LOW_THRESHOLD_SET can be used
+ * to decipher which threshold being set.
+ */
+extern int sensor_mgr_init_threshold(struct threshold_info *thresh_inp,
+				int sensor_id, int32_t high_temp,
+				int32_t low_temp,
+				void (*callback)(struct therm_threshold *));
+/**
+ * sensor_mgr_convert_id_and_set_threshold - It accepts sensor h/w ID, converts
+ *                                           it to sensor zone id and sets
+ *                                           thermal threshold for those
+ *                                           sensors listed in threshold info.
+ *
+ * @thresh_inp: Client threshold data structure.
+ *
+ * Returns zero on success, negative error number on failure.
+ */
+extern int sensor_mgr_convert_id_and_set_threshold(
+				struct threshold_info *thresh_inp);
+/**
+ * sensor_mgr_set_threshold- It sets thermal threshold trips for a sensor.
+ *
+ * @zone_id: Thermal zone ID for the sensor.
+ * @threshold: threshold info for the sensor.
+ *
+ * Returns zero on success, negative error number on failure.
+ */
+extern int sensor_mgr_set_threshold(uint32_t zone_id,
+				struct sensor_threshold *threshold);
+/**
+ * sensor_mgr_remove_threshold- It cancels threshold notification and
+ *                              removes threshold from sensor manager
+ *                              threshold list.
+ *
+ * @thresh_inp: The threshold info which needs to be removed.
+ */
+extern void sensor_mgr_remove_threshold(struct threshold_info *thresh_inp);
 #else
 static inline int msm_thermal_init(struct msm_thermal_data *pdata)
 {
@@ -94,6 +174,27 @@ static inline int msm_thermal_get_cluster_freq_plan(uint32_t cluster,
 	unsigned int *table_ptr);
 {
 	return -ENOSYS;
+}
+static inline int sensor_mgr_init_threshold(struct threshold_info *thresh_inp,
+				int sensor_id, int32_t high_temp,
+				int32_t low_temp,
+				void (*callback)(struct therm_threshold *))
+{
+	return -ENOSYS;
+}
+static inline int sensor_mgr_convert_id_and_set_threshold(
+			struct threshold_info *thresh_inp)
+{
+	return -ENOSYS;
+}
+static inline int sensor_mgr_set_threshold(uint32_t zone_id,
+			struct sensor_threshold *threshold)
+{
+	return -ENOSYS;
+}
+static inline void sensor_mgr_remove_threshold(
+				struct threshold_info *thresh_inp)
+{
 }
 #endif
 
