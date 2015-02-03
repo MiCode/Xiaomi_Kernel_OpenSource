3a0a476b425a8ba40e3799fe2b0cfbefbd94bd2a
Mayank Rana <mrana@codeaurora.org>
2015-02-02 20:14:43 -0800

    usb: qmp: phy: Fix misc_config related handling
    After programming PHY with required misc config related
    configuration, current code is returning error and failed
    PHY initialization. Fix this issue by checking return value
    and fail if there is any error.
    
    Also fix usage of configure_phy_regs() API by checking return
    value and in case returning same received error.
    
    Change-Id: Id0e0a9002d8be5a4be18a8c234447893e6d8cadf
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>

--- drivers/usb/phy/phy-msm-ssusb-qmp.c
+++ drivers/usb/phy/phy-msm-ssusb-qmp.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -466,7 +466,8 @@ static int msm_ssphy_qmp_init(struct usb_phy *uphy)
 	writel_relaxed(0x01, phy->base + PCIE_USB3_PHY_POWER_DOWN_CONTROL);
 
 	/* Main configuration */
-	if (configure_phy_regs(uphy, reg)) {
+	ret = configure_phy_regs(uphy, reg);
+	if (ret) {
 		dev_err(uphy->dev, "Failed the main PHY configuration\n");
 		return ret;
 	}
@@ -474,16 +475,19 @@ static int msm_ssphy_qmp_init(struct usb_phy *uphy)
 	/* Feature specific configurations */
 	if (phy->override_pll_cal) {
 		reg = qmp_override_pll;
-		if (configure_phy_regs(uphy, reg)) {
+		ret = configure_phy_regs(uphy, reg);
+		if (ret) {
 			dev_err(uphy->dev,
 				"Failed the PHY PLL override configuration\n");
 			return ret;
 		}
 	}
 	if (phy->misc_config) {
-		configure_phy_regs(uphy, misc);
-		dev_err(uphy->dev, "Failed the misc PHY configuration\n");
-		return ret;
+		ret = configure_phy_regs(uphy, misc);
+		if (ret) {
+			dev_err(uphy->dev, "Failed the misc PHY configuration\n");
+			return ret;
+		}
 	}
 
 	writel_relaxed(0x00, phy->base + PCIE_USB3_PHY_SW_RESET);
