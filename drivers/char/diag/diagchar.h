078f2aa2223d9ac1efc9e84b1d18d6171a4324e3
Ravi Aravamudhan <aravamud@codeaurora.org>
2015-02-03 19:52:15 -0800

    diag: Add support for Extended Mobile ID
    Diag driver has to support a new command for retrieving the
    mobile id of the target. The response contains the chip family.
    
    Change-Id: I2e12dd6834f8be95688678286e360c4e75af9524
    Signed-off-by: Sreelakshmi Gownipalli <sgownipa@codeaurora.org>

--- drivers/char/diag/diagchar.h
+++ drivers/char/diag/diagchar.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2008-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -90,6 +90,7 @@
 #define DIAG_DIAG_POLL		0x03
 #define DIAG_DEL_RSP_WRAP	0x04
 #define DIAG_DEL_RSP_WRAP_CNT	0x05
+#define DIAG_EXT_MOBILE_ID	0x06
 
 #define DIAG_CMD_OP_LOG_DISABLE		0
 #define DIAG_CMD_OP_GET_LOG_RANGE	1
@@ -209,6 +210,13 @@ struct diag_pkt_header_t {
 	uint8_t subsys_id;
 	uint16_t subsys_cmd_code;
 } __packed;
+
+struct diag_cmd_ext_mobile_rsp_t {
+	struct diag_pkt_header_t header;
+	uint8_t version;
+	uint8_t padding[3];
+	uint32_t family;
+};
 
 struct diag_master_table {
 	uint16_t cmd_code;
