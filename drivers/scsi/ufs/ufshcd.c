24a03a4cc93f7f7f928fa289abb71e1274fb823f
Gilad Broner <gbroner@codeaurora.org>
2015-02-04 13:38:32 +0200

    scsi: ufs: don't use FTRACE_EVENT_ENABLED macro
    Trace events have their own trace_<name>_enabled() function
    that can be called in order to check if its enabled or not.
    FTRACE_EVENT_ENABLED is redundant and will be dropped so
    use the trace event function instead.
    
    Change-Id: I4d2cc031ebbd5250bcff9526eaa916d6d691dbfc
    Signed-off-by: Gilad Broner <gbroner@codeaurora.org>
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>
    [subhashj@codeaurora.org: resolved merge conflicts]

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -383,7 +383,8 @@ static void ufshcd_add_command_trace(struct ufs_hba *hba,
 static inline void ufshcd_cond_add_cmd_trace(struct ufs_hba *hba,
 					unsigned int tag, const char *str)
 {
-	ufshcd_add_command_trace(hba, tag, str);
+	if (trace_ufshcd_command_enabled())
+		ufshcd_add_command_trace(hba, tag, str);
 }
 #else
 static inline void ufshcd_cond_add_cmd_trace(struct ufs_hba *hba,
