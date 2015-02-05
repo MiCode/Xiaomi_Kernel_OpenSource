bbbac5a99bc52633740ab88aeda11c015abffee5
Katish Paran <kparan@codeaurora.org>
2015-02-05 14:51:52 +0530

    diag: dci: Send mode reset command to the apps processor
    Diag driver was sending all mode commands to the apps processor while
    only mode reset command was handled on the apps processor. This patch
    makes an exception for mode reset command.
    
    Change-Id: I51e4971cbffcc8fedd68090cb5a15c4c67363c57
    Signed-off-by: Katish Paran <kparan@codeaurora.org>

--- drivers/char/diag/diag_dci.c
+++ drivers/char/diag/diag_dci.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1289,9 +1289,11 @@ static int diag_process_dci_pkt_rsp(unsigned char *buf, int len)
 				 * registered on the Apps Processor
 				 */
 				if (entry.cmd_code_lo == MODE_CMD &&
-				    entry.cmd_code_hi == MODE_CMD)
+				    entry.cmd_code_hi == MODE_CMD &&
+					header->subsys_id == RESET_ID) {
 					if (entry.client_id != APPS_DATA)
 						continue;
+				}
 					ret = diag_send_dci_pkt(entry, buf, len,
 								req_entry->tag);
 					found = 1;
