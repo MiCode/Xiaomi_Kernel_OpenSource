d0bcf46645a2bc7a49dcbf39bbe9fb4d6d42af45
Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
2015-02-11 14:47:39 -0800

    sched: Update cur_freq in the cpufreq policy notifier callback
    At boot, the cpufreq framework sends transition notifiers before
    sending out the policy notifier. Since the scheduler relies on the
    policy notifier to build up the frequency domain masks, when the
    initial set of transition notifiers are sent, the scheduler has no
    frequency domains. As a result the scheduler fails to update the
    cur_freq information. Update cur_freq as part of the policy notifier
    so that the scheduler always has the current frequency information.
    
    Change-Id: I7bd2958dfeb064dd20b9ccebafd372436484e5d6
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2400,6 +2400,7 @@ static int cpufreq_notifier_policy(struct notifier_block *nb,
 		orig_max_freq = cpu_rq(i)->max_freq;
 		cpu_rq(i)->min_freq = policy->min;
 		cpu_rq(i)->max_freq = policy->max;
+		cpu_rq(i)->cur_freq = policy->cur;
 		cpu_rq(i)->max_possible_freq = policy->cpuinfo.max_freq;
 	}
 
