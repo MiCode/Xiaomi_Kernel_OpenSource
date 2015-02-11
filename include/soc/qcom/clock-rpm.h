1ae0e8cb8c347abb9f5a7344f161310191d648d5
Pushkar Joshi <pushkarj@codeaurora.org>
2015-02-11 15:14:31 -0800

    clk: qcom: clock-rpm: Add interface to vote for BIMC clock
    On some targets, Linux needs to make an explicit vote for the BIMC clock
    before registering all the rpm controlled clocks. Add an interface to do
    the same.
    
    Change-Id: Ic207575eea09da1e2236182f81999adcff5fa424
    Signed-off-by: Pushkar Joshi <pushkarj@codeaurora.org>

--- include/soc/qcom/clock-rpm.h
+++ include/soc/qcom/clock-rpm.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2010-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -53,6 +53,8 @@ static inline struct rpm_clk *to_rpm_clk(struct clk *clk)
  * rpm clock scaling enable.
  */
 int enable_rpm_scaling(void);
+
+int vote_bimc(struct rpm_clk *r, uint32_t value);
 
 extern struct clk_rpmrs_data clk_rpmrs_data_smd;
 
