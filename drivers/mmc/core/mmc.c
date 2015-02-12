892f84e08777fa06ef94225e051188ed98c5fed6
Konstantin Dorfman <kdorfman@codeaurora.org>
2015-02-12 13:37:56 +0200

    mmc: core: Add tracepoints to enhance pm debugging
    Instrument the mmc core layer with tracepoints to aid in
    debugging issues and identifying latencies in the following
    paths:
    * System suspend/resume
    * Runtime suspend/resume
    
    Change-Id: I1e0fa7d3f8b54c102b4055f910b58a42412748da
    Signed-off-by: Konstantin Dorfman <kdorfman@codeaurora.org>
    [subhashj@codeaurora.org: fixed trivial merge conflicts]
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>
    [xiaonian@codeaurora.org: fixedtrivial merge conflicts,
    removed trace_mmc_resume() in mmc_resume() function]
    Signed-off-by: Xiaonian Wang <xiaonian@codeaurora.org>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -20,6 +20,7 @@
 #include <linux/mmc/card.h>
 #include <linux/mmc/mmc.h>
 #include <linux/reboot.h>
+#include <trace/events/mmc.h>
 
 #include "core.h"
 #include "host.h"
@@ -2251,6 +2252,7 @@ out:
 static int mmc_suspend(struct mmc_host *host)
 {
 	int err;
+	ktime_t start = ktime_get();
 
 	err = _mmc_suspend(host, true);
 	if (!err) {
@@ -2258,6 +2260,8 @@ static int mmc_suspend(struct mmc_host *host)
 		pm_runtime_set_suspended(&host->card->dev);
 	}
 
+	trace_mmc_suspend(mmc_hostname(host), err,
+			ktime_to_us(ktime_sub(ktime_get(), start)));
 	return err;
 }
 
@@ -2338,6 +2342,7 @@ static int mmc_shutdown(struct mmc_host *host)
 static int mmc_resume(struct mmc_host *host)
 {
 	pm_runtime_enable(&host->card->dev);
+
 	return 0;
 }
 
@@ -2347,6 +2352,7 @@ static int mmc_resume(struct mmc_host *host)
 static int mmc_runtime_suspend(struct mmc_host *host)
 {
 	int err;
+	ktime_t start = ktime_get();
 
 	if (!(host->caps & MMC_CAP_AGGRESSIVE_PM))
 		return 0;
@@ -2356,6 +2362,8 @@ static int mmc_runtime_suspend(struct mmc_host *host)
 		pr_err("%s: error %d doing aggressive suspend\n",
 			mmc_hostname(host), err);
 
+	trace_mmc_runtime_suspend(mmc_hostname(host), err,
+			ktime_to_us(ktime_sub(ktime_get(), start)));
 	return err;
 }
 
@@ -2365,6 +2373,7 @@ static int mmc_runtime_suspend(struct mmc_host *host)
 static int mmc_runtime_resume(struct mmc_host *host)
 {
 	int err;
+	ktime_t start = ktime_get();
 
 	err = _mmc_resume(host);
 	if (err && err != -ENOMEDIUM)
@@ -2372,6 +2381,9 @@ static int mmc_runtime_resume(struct mmc_host *host)
 			mmc_hostname(host), err);
 
 	mmc_init_clk_scaling(host);
+
+	trace_mmc_runtime_resume(mmc_hostname(host), err,
+			ktime_to_us(ktime_sub(ktime_get(), start)));
 
 	return 0;
 }
