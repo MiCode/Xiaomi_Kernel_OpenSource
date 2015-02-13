c49a69791422c2f3560c906f2c3663b8aff9baae
Adriana Reus <adriana.reus@intel.com>
2015-02-13 18:41:13 +0200

    iio: accel: kxcjk-1013: use available_scan_masks
    Use available_scan_masks to allow the iio core to select
    the data to send to userspace depending on which axes are
    enabled, instead of doing this in the driver's interrupt
    handler.
    
    This also fixes the issue of accessing the buffer scan_mask
    instead of active_scan_mask, since these might not be the
    same due to client devices.
    
    Change-Id: I563cab83ecc855b511eb0d33af9a72e121dfbaac
    Tracked-On: https://jira01.devtools.intel.com/browse/IRDA-3480
    Signed-off-by: Adriana Reus <adriana.reus@intel.com>
    Signed-off-by: Irina Tirdea <irina.tirdea@intel.com>

--- drivers/iio/accel/kxcjk-1013.c
+++ drivers/iio/accel/kxcjk-1013.c
@@ -114,6 +114,7 @@ enum kxcjk1013_axis {
 	AXIS_X,
 	AXIS_Y,
 	AXIS_Z,
+	AXIS_MAX,
 };
 
 enum kxcjk1013_mode {
@@ -940,6 +941,8 @@ static const struct iio_info kxcjk1013_info = {
 	.driver_module		= THIS_MODULE,
 };
 
+static const unsigned long kxcjk1013_scan_masks[] = {0x7, 0};
+
 static irqreturn_t kxcjk1013_trigger_handler(int irq, void *p)
 {
 	struct iio_poll_func *pf = p;
@@ -948,9 +951,7 @@ static irqreturn_t kxcjk1013_trigger_handler(int irq, void *p)
 	int bit, ret, i = 0;
 
 	mutex_lock(&data->mutex);
-
-	for_each_set_bit(bit, indio_dev->buffer->scan_mask,
-			 indio_dev->masklength) {
+	for (bit = 0; bit < AXIS_MAX; bit++) {
 		ret = kxcjk1013_get_acc_reg(data, bit);
 		if (ret < 0) {
 			mutex_unlock(&data->mutex);
@@ -1213,6 +1214,7 @@ static int kxcjk1013_probe(struct i2c_client *client,
 	indio_dev->dev.parent = &client->dev;
 	indio_dev->channels = kxcjk1013_channels;
 	indio_dev->num_channels = ARRAY_SIZE(kxcjk1013_channels);
+	indio_dev->available_scan_masks = kxcjk1013_scan_masks;
 	indio_dev->name = name;
 	indio_dev->modes = INDIO_DIRECT_MODE;
 	indio_dev->info = &kxcjk1013_info;
