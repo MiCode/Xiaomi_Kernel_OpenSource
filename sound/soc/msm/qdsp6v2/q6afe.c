58bfba635d00026d0f12630246a20ebc4b1def41
Chaithanya Krishna Bacharaju <chaithan@codeaurora.org>
2015-02-17 14:32:45 +0530

    ASoC: msm: qdsp6v2: Avoid populating memory handle in error case
    Memory map handle is populated even in error case of afe memory
    map and when this handle is sent to adsp, querying corresponding
    virtual address results in failure as the region was never created.
    Memory map command is retried in remap_cal_data only if handle is
    zero.
    Due to this, in SVA case out of band parameters related to SWMAD
    and duty cycle are not sent to adsp leading to detection issues.
    
    Change-Id: I3f0f0fac86d8a21fb29e7d82079fd43a6695f208
    Signed-off-by: Chaithanya Krishna Bacharaju <chaithan@codeaurora.org>

--- sound/soc/msm/qdsp6v2/q6afe.c
+++ sound/soc/msm/qdsp6v2/q6afe.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -4288,6 +4288,7 @@ static int afe_map_cal_data(int32_t cal_type,
 			__func__,
 			&cal_block->cal_data.paddr,
 			cal_block->map_data.map_size);
+		goto done;
 	}
 	cal_block->map_data.q6map_handle = atomic_read(&this_afe.
 		mem_map_cal_handles[cal_index]);
