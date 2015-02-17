3f92fa6e510fd356c9b503adfafd0ab6f22b844f
ChandanaKishori Chiluveru <cchilu@codeaurora.org>
2015-02-17 16:29:41 +0530

    usb: ci13xxx_udc: Set USB system clock at 100 MHz in streaming mode
    Internal USB DMA engine and some of the USB HW state machine runs with
    USB system clock frequency. Increasing USB system clock directly helps
    to transfer data from/to USB HW FIFO to external memory and vice versa
    and helps performance improvement. In non-USB streaming mode, there are
    some HW limitations, which can cause EP prime failures, if we run USB
    system clock at 100MHz and beyond frequencies.
    
    In USB streaming mode, allowed to increase USB system clock upto 133MHz.
    But, running at 133MHz require system to run in turbo mode and not
    efficient from power perspective. Hence, going with optimum USB
    system clock frequency 100MHz, which operates in nominal mode.
    
    Increasing USB system clock from 80MHz to 100MHz in streaming mode enable.
    
    Change-Id: If0622fe2a18d0810ec8eb0db9740cbb3a68257d7
    Signed-off-by: ChandanaKishori Chiluveru <cchilu@codeaurora.org>

--- include/linux/usb/msm_hsusb.h
+++ include/linux/usb/msm_hsusb.h
@@ -299,6 +299,7 @@ struct msm_otg_platform_data {
 	bool disable_retention_with_vdd_min;
 	int usb_id_gpio;
 	bool phy_dvdd_always_on;
+	struct clk *system_clk;
 };
 
 /* phy related flags */
@@ -562,6 +563,7 @@ struct ci13xxx_platform_data {
 	void *prv_data;
 	bool l1_supported;
 	bool enable_ahb2ahb_bypass;
+	struct clk *system_clk;
 };
 
 /**
