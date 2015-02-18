cc2abb9e997e8526d1be0312b50126ff7bee4814
Ravinder Konka <rkonka@codeaurora.org>
2015-02-18 14:18:57 +0530

    net: core: Send ARP probe when neighbor entry is created in NUD_STALE state
    Send ARP probe to the connected client when the neighbor entry created
    due to a solicit ARP request from the client moves directly to
    NUD_STALE state.
    
    Change-Id: I726c3523d32c996f8fbb44f1f3eba88cf8823983
    Signed-off-by: Ravinder Konka <rkonka@codeaurora.org>

--- net/core/neighbour.c
+++ net/core/neighbour.c
@@ -1236,9 +1236,12 @@ struct neighbour *neigh_event_ns(struct neigh_table *tbl,
 {
 	struct neighbour *neigh = __neigh_lookup(tbl, saddr, dev,
 						 lladdr || !dev->addr_len);
-	if (neigh)
+	if (neigh) {
 		neigh_update(neigh, lladdr, NUD_STALE,
 			     NEIGH_UPDATE_F_OVERRIDE);
+		write_lock(&neigh->lock);
+		neigh_probe(neigh);
+	}
 	return neigh;
 }
 EXPORT_SYMBOL(neigh_event_ns);
