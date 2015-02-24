d2cefed4c4853d75ca2c631e6d6675b5bc9e86df
Jeff Hugo <jhugo@codeaurora.org>
2015-02-24 14:51:30 -0700

    soc: qcom: glink: Ensure received version commands are valid
    Receiving version commands from the remote side for a transport after that
    transport has already sucessfully negotiated a version is invalid according
    to the negotiation protocol.  If the core acts upon these invalid version
    commands, it could confuse the remote side or underlying transport plugin
    and enter an undefined state.
    
    Check that the transport version hasn't completely negotiated before
    accepting version commands as valid.
    
    Change-Id: I6f5192345ae2c6a972c2f00b7df460eb2b952c80
    Signed-off-by: Jeffrey Hugo <jhugo@codeaurora.org>

--- drivers/soc/qcom/glink.c
+++ drivers/soc/qcom/glink.c
@@ -2671,6 +2671,12 @@ static void glink_core_rx_cmd_version(struct glink_transport_if *if_ptr,
 	bool neg_complete = false;
 	uint32_t l_version, l_features;
 
+	if (xprt_ptr->local_state == GLINK_XPRT_OPENED) {
+		GLINK_ERR_XPRT(xprt_ptr,
+			"%s: Negotiation already complete\n", __func__);
+		return;
+	}
+
 	l_version = versions[xprt_ptr->remote_version_idx].version;
 	l_features = versions[xprt_ptr->remote_version_idx].features;
 
@@ -2757,6 +2763,12 @@ static void glink_core_rx_cmd_version_ack(struct glink_transport_if *if_ptr,
 	const struct glink_core_version *versions = xprt_ptr->versions;
 	uint32_t l_version, l_features;
 	bool neg_complete = false;
+
+	if (xprt_ptr->local_state == GLINK_XPRT_OPENED) {
+		GLINK_ERR_XPRT(xprt_ptr,
+			"%s: Negotiation already complete\n", __func__);
+		return;
+	}
 
 	l_version = versions[xprt_ptr->local_version_idx].version;
 	l_features = versions[xprt_ptr->local_version_idx].features;
