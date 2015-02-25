0a972d5ed4480386f6ec1a18f126417c29248d78
Vinayak Menon <vinmenon@codeaurora.org>
2015-02-25 19:43:59 +0530

    mm: swap: don't delay swap free for fast swap devices
    There are couple of issues with swapcache usage when ZRAM is used
    as swap device.
    1) Kernel does a swap readahead which can be around 6 to 8 pages
    depending on total ram, which is not required for zram since
    accesses are fast.
    2) Kernel delays the freeing up of swapcache expecting a later hit,
    which again is useless in the case of zram.
    3) This is not related to swapcache, but zram usage itself.
    As mentioned in (2) kernel delays freeing of swapcache, but along with
    that it delays zram compressed page free also. i.e. there can be 2 copies,
    though one is compressed.
    
    This patch addresses these issues using two new flags
    QUEUE_FLAG_FAST and SWP_FAST, to indicate that accesses to the device
    will be fast and cheap, and instructs the swap layer to free up
    swap space agressively, and not to do read ahead.
    
    Change-Id: I5d2d5176a5f9420300bb2f843f6ecbdb25ea80e4
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -73,6 +73,26 @@ static inline unsigned char swap_count(unsigned char ent)
 	return ent & ~SWAP_HAS_CACHE;	/* may include SWAP_HAS_CONT flag */
 }
 
+bool is_swap_fast(swp_entry_t entry)
+{
+	struct swap_info_struct *p;
+	unsigned long type;
+
+	if (non_swap_entry(entry))
+		return false;
+
+	type = swp_type(entry);
+	if (type >= nr_swapfiles)
+		return false;
+
+	p = swap_info[type];
+
+	if (p->flags & SWP_FAST)
+		return true;
+
+	return false;
+}
+
 /* returns 1 if swap entry is freed */
 static int
 __try_to_reclaim_swap(struct swap_info_struct *si, unsigned long offset)
@@ -293,7 +313,7 @@ checks:
 		scan_base = offset = si->lowest_bit;
 
 	/* reuse swap entry of cache-only swap if not busy. */
-	if (vm_swap_full() && si->swap_map[offset] == SWAP_HAS_CACHE) {
+	if (vm_swap_full(si) && si->swap_map[offset] == SWAP_HAS_CACHE) {
 		int swap_was_freed;
 		spin_unlock(&si->lock);
 		swap_was_freed = __try_to_reclaim_swap(si, offset);
@@ -382,7 +402,8 @@ scan:
 			spin_lock(&si->lock);
 			goto checks;
 		}
-		if (vm_swap_full() && si->swap_map[offset] == SWAP_HAS_CACHE) {
+		if (vm_swap_full(si) &&
+			si->swap_map[offset] == SWAP_HAS_CACHE) {
 			spin_lock(&si->lock);
 			goto checks;
 		}
@@ -397,7 +418,8 @@ scan:
 			spin_lock(&si->lock);
 			goto checks;
 		}
-		if (vm_swap_full() && si->swap_map[offset] == SWAP_HAS_CACHE) {
+		if (vm_swap_full(si) &&
+			si->swap_map[offset] == SWAP_HAS_CACHE) {
 			spin_lock(&si->lock);
 			goto checks;
 		}
@@ -763,7 +785,8 @@ int free_swap_and_cache(swp_entry_t entry)
 		 * Also recheck PageSwapCache now page is locked (above).
 		 */
 		if (PageSwapCache(page) && !PageWriteback(page) &&
-				(!page_mapped(page) || vm_swap_full())) {
+				(!page_mapped(page) ||
+				vm_swap_full(page_swap_info(page)))) {
 			delete_from_swap_cache(page);
 			SetPageDirty(page);
 		}
@@ -2125,6 +2148,8 @@ SYSCALL_DEFINE2(swapon, const char __user *, specialfile, int, swap_flags)
 		}
 		if ((swap_flags & SWAP_FLAG_DISCARD) && discard_swap(p) == 0)
 			p->flags |= SWP_DISCARDABLE;
+		if (blk_queue_fast(bdev_get_queue(p->bdev)))
+			p->flags |= SWP_FAST;
 	}
 
 	mutex_lock(&swapon_mutex);
