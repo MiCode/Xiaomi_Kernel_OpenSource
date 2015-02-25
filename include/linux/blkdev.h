9cd7b88565911e57211d32256f9f6e9456982c6a
Vinayak Menon <vinmenon@codeaurora.org>
2015-02-25 19:43:59 +0530

    mm: swap: don't delay swap free for fast swap devices
    There are couple of issues with swapcache usage when ZRAM is used
    as swap device.
    1) Kernel does a swap readahead which can be around 6 to 8 pages
    depending on total ram, which is not required for zram since
    accesses are fast.
    2) Kernel delays the freeing up of swapcache expecting a later hit,
    which again is useless in the case of zram.
    3) This is not related to swapcache, but zram usage itself.
    As mentioned in (2) kernel delays freeing of swapcache, but along with
    that it delays zram compressed page free also. i.e. there can be 2 copies,
    though one is compressed.
    
    This patch addresses these issues using two new flags
    QUEUE_FLAG_FAST and SWP_FAST, to indicate that accesses to the device
    will be fast and cheap, and instructs the swap layer to free up
    swap space agressively, and not to do read ahead.
    
    Change-Id: I5d2d5176a5f9420300bb2f843f6ecbdb25ea80e4
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -470,6 +470,7 @@ struct request_queue {
 #define QUEUE_FLAG_SECDISCARD  17	/* supports SECDISCARD */
 #define QUEUE_FLAG_SAME_FORCE  18	/* force complete on same CPU */
 #define QUEUE_FLAG_DEAD        19	/* queue tear-down finished */
+#define QUEUE_FLAG_FAST        20	/* fast block device (e.g. ram based) */
 
 #define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_IO_STAT) |		\
 				 (1 << QUEUE_FLAG_STACKABLE)	|	\
@@ -553,6 +554,7 @@ static inline void queue_flag_clear(unsigned int flag, struct request_queue *q)
 #define blk_queue_discard(q)	test_bit(QUEUE_FLAG_DISCARD, &(q)->queue_flags)
 #define blk_queue_secdiscard(q)	(blk_queue_discard(q) && \
 	test_bit(QUEUE_FLAG_SECDISCARD, &(q)->queue_flags))
+#define blk_queue_fast(q)	test_bit(QUEUE_FLAG_FAST, &(q)->queue_flags)
 
 #define blk_noretry_request(rq) \
 	((rq)->cmd_flags & (REQ_FAILFAST_DEV|REQ_FAILFAST_TRANSPORT| \
--- mm/memory.c
+++ mm/memory.c
@@ -3166,7 +3166,8 @@ static int do_swap_page(struct mm_struct *mm, struct vm_area_struct *vma,
 	mem_cgroup_commit_charge_swapin(page, ptr);
 
 	swap_free(entry);
-	if (vm_swap_full() || (vma->vm_flags & VM_LOCKED) || PageMlocked(page))
+	if ((PageSwapCache(page) && vm_swap_full(page_swap_info(page))) ||
+		(vma->vm_flags & VM_LOCKED) || PageMlocked(page))
 		try_to_free_swap(page);
 	unlock_page(page);
 	if (page != swapcache) {
