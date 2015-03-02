9c3d0cc9df64c603fb1dbe39678286f72a09d65b
Alok Chauhan <alokc@codeaurora.org>
2015-03-02 17:11:14 +0530

    Revert "msm: msm_bus: Add trace events to ad-hoc bus driver"
    There are too many bus events that stomp on other events
    and make it difficult to live debug of use cases.
    With this revert those additional bus events are removed.
    
    This reverts commit 757a1fd684e2b88660b951a272bf607fb8cee46d.
    
    Change-Id: I835fe2e9d3d4a704630c54180334c239cded1804
    Signed-off-by: Alok Chauhan <alokc@codeaurora.org>

--- drivers/platform/msm/msm_bus/msm_bus_dbg.c
+++ drivers/platform/msm/msm_bus/msm_bus_dbg.c
@@ -1,4 +1,5 @@
-/* Copyright (c) 2010-2012, 2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2010-2012, 2014-2015, The Linux Foundation. All rights
+ * reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -395,8 +396,7 @@ int msm_bus_dbg_rec_transaction(const struct msm_bus_client_handle *pdata,
 	cldata->size = i;
 
 	trace_bus_update_request((int)ts.tv_sec, (int)ts.tv_nsec,
-		pdata->name, pdata->mas, pdata->slv, ab, ib,
-		pdata->active_only);
+		pdata->name, pdata->mas, pdata->slv, ab, ib);
 
 	return i;
 }
@@ -512,8 +512,7 @@ static int msm_bus_dbg_fill_cl_buffer(const struct msm_bus_scale_pdata *pdata,
 		pdata->usecase[index].vectors[j].src,
 		pdata->usecase[index].vectors[j].dst,
 		pdata->usecase[index].vectors[j].ab,
-		pdata->usecase[index].vectors[j].ib,
-		pdata->active_only);
+		pdata->usecase[index].vectors[j].ib);
 
 	cldata->index = index;
 	cldata->size = i;
--- include/trace/events/trace_msm_bus.h
+++ include/trace/events/trace_msm_bus.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -21,9 +21,9 @@
 TRACE_EVENT(bus_update_request,
 
 	TP_PROTO(int sec, int nsec, const char *name, int src, int dest,
-		unsigned long long ab, unsigned long long ib, int active_only),
+		unsigned long long ab, unsigned long long ib),
 
-	TP_ARGS(sec, nsec, name, src, dest, ab, ib, active_only),
+	TP_ARGS(sec, nsec, name, src, dest, ab, ib),
 
 	TP_STRUCT__entry(
 		__field(int, sec)
@@ -33,7 +33,6 @@ TRACE_EVENT(bus_update_request,
 		__field(int, dest)
 		__field(u64, ab)
 		__field(u64, ib)
-		__field(int, active_only)
 	),
 
 	TP_fast_assign(
@@ -44,18 +43,16 @@ TRACE_EVENT(bus_update_request,
 		__entry->dest = dest;
 		__entry->ab = ab;
 		__entry->ib = ib;
-		__entry->active_only = active_only;
 	),
 
-	TP_printk("time:%d.%d name:%s src:%d dest:%d ab:%llu ib:%llu active:%d",
+	TP_printk("time= %d.%d name=%s src=%d dest=%d ab=%llu ib=%llu",
 		__entry->sec,
 		__entry->nsec,
 		__get_str(name),
 		__entry->src,
 		__entry->dest,
 		(unsigned long long)__entry->ab,
-		(unsigned long long)__entry->ib,
-		__entry->active_only)
+		(unsigned long long)__entry->ib)
 );
 
 TRACE_EVENT(bus_update_request_end,
@@ -72,7 +69,7 @@ TRACE_EVENT(bus_update_request_end,
 		__assign_str(name, name);
 	),
 
-	TP_printk("client-name:%s", __get_str(name))
+	TP_printk("client-name=%s", __get_str(name))
 );
 
 TRACE_EVENT(bus_bimc_config_limiter,
@@ -91,7 +88,7 @@ TRACE_EVENT(bus_bimc_config_limiter,
 		__entry->cur_lim_bw = cur_lim_bw;
 	),
 
-	TP_printk("Master:%d cur_lim_bw:%llu",
+	TP_printk("Master=%d cur_lim_bw=%llu",
 		__entry->mas_id,
 		(unsigned long long)__entry->cur_lim_bw)
 );
@@ -112,7 +109,7 @@ TRACE_EVENT(bus_avail_bw,
 		__entry->cur_mdp_bw = cur_mdp_bw;
 	),
 
-	TP_printk("cur_bimc_bw:%llu cur_mdp_bw:%llu",
+	TP_printk("cur_bimc_bw = %llu cur_mdp_bw = %llu",
 		(unsigned long long)__entry->cur_bimc_bw,
 		(unsigned long long)__entry->cur_mdp_bw)
 );
@@ -140,7 +137,7 @@ TRACE_EVENT(bus_rules_matches,
 		__entry->node_clk = node_clk;
 	),
 
-	TP_printk("node:%d rule:%d node-ab:%llu ib:%llu clk:%llu",
+	TP_printk("Rule match node%d rule%d node-ab%llu:ib%llu:clk%llu",
 		__entry->node_id, __entry->rule_id,
 		(unsigned long long)__entry->node_ab,
 		(unsigned long long)__entry->node_ib,
@@ -169,120 +166,9 @@ TRACE_EVENT(bus_bke_params,
 		__entry->thh = thh;
 	),
 
-	TP_printk("GC:0x%x GP:0x%x THL:0x%x THM:0x%x THH:0x%x",
+	TP_printk("BKE Params GC=0x%x GP=0x%x THL=0x%x THM=0x%x THH=0x%x",
 		__entry->gc, __entry->gp, __entry->thl, __entry->thm,
 			__entry->thh)
-);
-
-TRACE_EVENT(bus_noc_set_qos_mode,
-	TP_PROTO(long base, uint32_t qos_off,
-		uint32_t mport, uint32_t qos_delta, uint8_t mode,
-		uint8_t perm_mode),
-
-	TP_ARGS(base, qos_off, mport, qos_delta, mode, perm_mode),
-
-	TP_STRUCT__entry(
-		__field(long, base)
-		__field(uint32_t, qos_off)
-		__field(uint32_t, mport)
-		__field(uint32_t, qos_delta)
-		__field(uint8_t, mode)
-		__field(uint8_t, perm_mode)
-	),
-
-	TP_fast_assign(
-		__entry->base = base;
-		__entry->qos_off = qos_off;
-		__entry->mport = mport;
-		__entry->qos_delta = qos_delta;
-		__entry->mode = mode;
-		__entry->perm_mode = perm_mode;
-	),
-
-	TP_printk("base:%ld q_off:%d mport:%d q_delta:%d mode:%d perm_mode:%d",
-		__entry->base,
-		__entry->qos_off,
-		__entry->mport,
-		__entry->qos_delta,
-		(uint32_t)__entry->mode,
-		(uint32_t)__entry->perm_mode)
-);
-
-TRACE_EVENT(bus_agg_bw,
-
-	TP_PROTO(unsigned int node_id, int rpm_id, int ctx_set,
-		unsigned long long agg_ab),
-
-	TP_ARGS(node_id, rpm_id, ctx_set, agg_ab),
-
-	TP_STRUCT__entry(
-		__field(unsigned int, node_id)
-		__field(int, rpm_id)
-		__field(int, ctx_set)
-		__field(u64, agg_ab)
-	),
-
-	TP_fast_assign(
-		__entry->node_id = node_id;
-		__entry->rpm_id = rpm_id;
-		__entry->ctx_set = ctx_set;
-		__entry->agg_ab = agg_ab;
-	),
-
-	TP_printk("node_id:%u rpm_id:%d ctx:%d agg_ab:%llu",
-		__entry->node_id,
-		__entry->rpm_id,
-		__entry->ctx_set,
-		(unsigned long long)__entry->agg_ab)
-);
-
-TRACE_EVENT(bus_agg_clk,
-
-	TP_PROTO(unsigned int node_id, int ctx_set,
-		unsigned long long agg_clk),
-
-	TP_ARGS(node_id, ctx_set, agg_clk),
-
-	TP_STRUCT__entry(
-		__field(unsigned int, node_id)
-		__field(int, ctx_set)
-		__field(u64, agg_clk)
-	),
-
-	TP_fast_assign(
-		__entry->node_id = node_id;
-		__entry->ctx_set = ctx_set;
-		__entry->agg_clk = agg_clk;
-	),
-
-	TP_printk("node_id:%u ctx:%d agg_clk:%llu",
-		__entry->node_id,
-		__entry->ctx_set,
-		(unsigned long long)__entry->agg_clk)
-);
-
-TRACE_EVENT(bus_rules_apply,
-
-	TP_PROTO(int rule_id, unsigned long long limit_bw, int throttle_en),
-
-	TP_ARGS(rule_id, limit_bw, throttle_en),
-
-	TP_STRUCT__entry(
-		__field(int, rule_id)
-		__field(u64, limit_bw)
-		__field(int, throttle_en)
-	),
-
-	TP_fast_assign(
-		__entry->rule_id = rule_id;
-		__entry->limit_bw = limit_bw;
-		__entry->throttle_en = throttle_en;
-	),
-
-	TP_printk("rule:%d limit_bw:%llu throttle_enable:%d",
-		__entry->rule_id,
-		(unsigned long long)__entry->limit_bw,
-		__entry->throttle_en)
 );
 #endif
 #define TRACE_INCLUDE_FILE trace_msm_bus
