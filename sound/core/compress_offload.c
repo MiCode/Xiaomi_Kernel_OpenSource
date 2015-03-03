3545941a09a1c29a142cbc4a75ae9c611eedc278
Samreen Nilofer <samreen.nilofer@intel.com>
2015-03-03 15:05:51 +0530

    ALSA: compress: Allow pause during draining
    Issue: GMINL-6632
    
    The stream can be paused during the draining state.
    
    Change-Id: I5a5b39ba0ff545a0063b1ceba7611af8ad1c781a
    Signed-off-by: Samreen Nilofer <samreen.nilofer@intel.com>

--- sound/core/compress_offload.c
+++ sound/core/compress_offload.c
@@ -636,7 +636,8 @@ static int snd_compr_pause(struct snd_compr_stream *stream)
 {
 	int retval;
 
-	if (stream->runtime->state != SNDRV_PCM_STATE_RUNNING)
+	if ((stream->runtime->state != SNDRV_PCM_STATE_RUNNING) &&
+	    (stream->runtime->state != SNDRV_PCM_STATE_DRAINING))
 		return -EPERM;
 	retval = stream->ops->trigger(stream, SNDRV_PCM_TRIGGER_PAUSE_PUSH);
 	if (!retval)
