981462ff2fe036ed9c6423af3fbd005c91e0158f
Katish Paran <kparan@codeaurora.org>
2015-03-03 23:58:26 +0530

    diag: Flush the data only in case for Memory Device Mode
    Once the logging is switched, from USB_MODE to MEMORY_DEVICE_MODE
    diag is not expecting write done notification from USB. In case
    for asynchronous writes notification for USB, the data can be
    dropped. This ensures that the flush_workqueue is called only
    in case where diag is logging in Memory device mode and write
    done notification is being called from memory device context.
    
    Change-Id: Ideed995f96797e1950647c3657e4ab013dda74aa
    Signed-off-by: Katish Paran <kparan@codeaurora.org>

--- drivers/char/diag/diag_memorydevice.c
+++ drivers/char/diag/diag_memorydevice.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -223,7 +223,8 @@ int diag_md_copy_to_user(char __user *buf, int *pret)
 			num_data++;
 drop_data:
 			ch->ops->write_done(entry->buf, entry->len,
-					    entry->ctx, ch->ctx);
+					    entry->ctx,
+					    DIAG_MEMORY_DEVICE_MODE);
 			diag_ws_on_copy(DIAG_WS_MD);
 			spin_lock_irqsave(&entry->lock, flags);
 			entry->buf = NULL;
