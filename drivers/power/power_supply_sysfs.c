a98229aa3c0291b5d4b9f47128de9f194c2b7339
Xiaozhe Shi <xiaozhes@codeaurora.org>
2015-03-05 15:40:56 -0800

    power_supply: add local extension POWER_SUPPLY_PROP_CHARGE_DONE
    Add the charge done property in order to allow notifications when
    charging completes.
    
    CRs-Fixed: 799673
    Change-Id: If207bd8188e75b840231ec274b24dbb7ad67e145
    Signed-off-by: Xiaozhe Shi <xiaozhes@codeaurora.org>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -218,6 +218,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(update_now),
 	POWER_SUPPLY_ATTR(esr_count),
 	POWER_SUPPLY_ATTR(safety_timer_enabled),
+	POWER_SUPPLY_ATTR(charge_done),
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_ATTR(charge_counter_ext),
 	/* Properties of type `const char *' */
--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -170,6 +170,7 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_UPDATE_NOW,
 	POWER_SUPPLY_PROP_ESR_COUNT,
 	POWER_SUPPLY_PROP_SAFETY_TIMER_ENABLE,
+	POWER_SUPPLY_PROP_CHARGE_DONE,
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_PROP_CHARGE_COUNTER_EXT,
 	/* Properties of type `const char *' */
