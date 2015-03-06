854902887c9a56e36a0b8611b95a770d9be762ab
franciscofranco <franciscofranco.1990@gmail.com>
2015-03-06 00:02:59 +0000

    mako_hotplug: don't offline cores if the load at that sample is above the threshold, even if we met all the other conditions
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/kernel/mako_hotplug.c
+++ arch/arm/kernel/mako_hotplug.c
@@ -160,7 +160,7 @@ online_all:
 	stats.timestamp = ktime_to_us(ktime_get());
 }
 
-static void cpu_smash(void)
+static void cpu_smash(unsigned int load)
 {
 	struct hotplug_tunables *t = &tunables;
 	u64 extra_time = MIN_CPU_UP_US;
@@ -185,6 +185,13 @@ static void cpu_smash(void)
 		extra_time = t->min_time_cpu_online * MIN_CPU_UP_US;
 
 	if (ktime_to_us(ktime_get()) < stats.timestamp + extra_time)
+		return;
+
+	/*
+	 * If current load is higher than our threshold we can skip offlining
+	 * on the next sample
+	 */
+	if (load >= t->load_threshold)
 		return;
 
 	cpus_offline_work();
@@ -231,7 +238,7 @@ static void __ref decide_hotplug_func(struct work_struct *work)
 			--stats.counter;
 
 		if (online_cpus > 2)
-			cpu_smash();
+			cpu_smash(cur_load);
 	}
 
 reschedule:
