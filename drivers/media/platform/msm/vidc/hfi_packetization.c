e55799e587551cadb59e4e58c58c3eea5ff2c8ea
Manikanta Sivapala <msivap@codeaurora.org>
2015-03-10 20:35:49 -0700

    msm: vidc: Remove setting of display hold count on firmware
    The hfi property HFI_PROPERTY_PARAM_BUFFER_DISPLAY_HOLD_COUNT_ACTUAL
    was not being used by firmware and has been deprecated. Remove setting
    of this property from the driver as well.
    
    Change-Id: I7c0d5a47803ce6105de644d77e5e08edd563ae07
    Signed-off-by: Manikanta Sivapala <msivap@codeaurora.org>

--- drivers/media/platform/msm/vidc/hfi_packetization.c
+++ drivers/media/platform/msm/vidc/hfi_packetization.c
@@ -1000,30 +1000,6 @@ int create_pkt_cmd_session_set_property(
 				hfi_buffer_count_actual);
 		break;
 	}
-	case HAL_PARAM_BUFFER_DISPLAY_HOLD_COUNT_ACTUAL:
-	{
-		struct hfi_buffer_display_hold_count_actual *hfi;
-		struct hal_buffer_display_hold_count_actual *prop =
-			(struct hal_buffer_display_hold_count_actual *) pdata;
-		u32 buffer_type;
-
-		pkt->rg_property_data[0] =
-			HFI_PROPERTY_PARAM_BUFFER_DISPLAY_HOLD_COUNT_ACTUAL;
-
-		hfi = (struct hfi_buffer_display_hold_count_actual *)
-			&pkt->rg_property_data[1];
-		hfi->hold_count = prop->hold_count;
-
-		buffer_type = get_hfi_buffer(prop->buffer_type);
-		if (buffer_type)
-			hfi->buffer_type = buffer_type;
-		else
-			return -EINVAL;
-
-		pkt->size += sizeof(u32) + sizeof(struct
-				hfi_buffer_count_actual);
-		break;
-	}
 	case HAL_PARAM_NAL_STREAM_FORMAT_SELECT:
 	{
 		struct hfi_nal_stream_format_select *hfi;
