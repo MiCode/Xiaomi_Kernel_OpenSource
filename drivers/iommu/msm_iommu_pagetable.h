6243093b11ed1307014c1201834d653e316fa486
Laura Abbott <lauraa@codeaurora.org>
2015-03-12 16:58:33 -0700

    iommu: msm: Correct map_range va argument type
    va is an unsigned long not an int. Correct it.
    
    Change-Id: Ie08675134b5115e016ae8dc165b821e5823e242f
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- drivers/iommu/msm_iommu_pagetable.h
+++ drivers/iommu/msm_iommu_pagetable.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -22,7 +22,7 @@ int msm_iommu_pagetable_map(struct msm_iommu_pt *pt, unsigned long va,
 			phys_addr_t pa, size_t len, int prot);
 size_t msm_iommu_pagetable_unmap(struct msm_iommu_pt *pt, unsigned long va,
 				size_t len);
-int msm_iommu_pagetable_map_range(struct msm_iommu_pt *pt, unsigned int va,
+int msm_iommu_pagetable_map_range(struct msm_iommu_pt *pt, unsigned long va,
 			struct scatterlist *sg, unsigned int len, int prot);
 void msm_iommu_pagetable_unmap_range(struct msm_iommu_pt *pt, unsigned int va,
 				unsigned int len);
