775600102e748a6491f702c600b41c926709c579
Vikram Mulukutla <markivx@codeaurora.org>
2015-03-12 12:40:40 -0700

    clk: qcom: clock-pll: Remove no_prepared_reconfig optimization check
    Previously, we were allowing a prepared PLL to support
    a requested rate if it was already at that rate, even
    with the no_prepared_reconfig flag set. This does not
    account for downstream clock elements that may still
    want to be able to select a different source, and does
    not provide much benefit in terms of optimization anyway.
    It is easier to design clock drivers without this
    optimization in mind.
    
    Change-Id: I74121535f798a28a6479bd5cd3161062b1cb17c0
    Signed-off-by: Vikram Mulukutla <markivx@codeaurora.org>

--- drivers/clk/qcom/clock-pll.c
+++ drivers/clk/qcom/clock-pll.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -509,7 +509,7 @@ static long variable_rate_pll_round_rate(struct clk *c, unsigned long rate)
 	if (!pll->src_rate)
 		return 0;
 
-	if (pll->no_prepared_reconfig && c->prepare_count && c->rate != rate)
+	if (pll->no_prepared_reconfig && c->prepare_count)
 		return -EINVAL;
 
 	if (rate < pll->min_rate)
