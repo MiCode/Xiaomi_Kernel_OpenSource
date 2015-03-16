bea19db82340c97543b9824ca7643fba1c9a0c56
Eric Dumazet <edumazet@google.com>
2015-03-15 21:12:12 -0700

    net: add sk_fullsock() helper
    We have many places where we want to check if a socket is
    not a timewait or request socket. Use a helper to avoid
    hard coding this.
    
    [Backport of net-next 1d0ab253872cdd3d8e7913f59c266c7fd01771d0]
    [lorenzo@google.com: removed TCPF_NEW_SYN_RECV, and added a comment to add it back.]
    
    Bug: 24163529
    Change-Id: Ibf09017e1ab00af5e6925273117c335d7f515d73
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Lorenzo Colitti <lorenzo@google.com>

--- include/net/sock.h
+++ include/net/sock.h
@@ -68,6 +68,7 @@
 #include <net/dst.h>
 #include <net/checksum.h>
 #include <linux/net_tstamp.h>
+#include <net/tcp_states.h>
 
 struct cgroup;
 struct cgroup_subsys;
@@ -2263,6 +2264,15 @@ static inline struct sock *skb_steal_sock(struct sk_buff *skb)
 		return sk;
 	}
 	return NULL;
+}
+
+/* This helper checks if a socket is a full socket,
+ * ie _not_ a timewait or request socket.
+ * TODO: Check for TCPF_NEW_SYN_RECV when that starts to exist.
+ */
+static inline bool sk_fullsock(const struct sock *sk)
+{
+	return (1 << sk->sk_state) & ~(TCPF_TIME_WAIT);
 }
 
 void sock_enable_timestamp(struct sock *sk, int flag);
