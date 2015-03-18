84a2daddb182c307fe20948df9764d0ffb5e42b2
Suman Tatiraju <sumant@codeaurora.org>
2015-03-18 10:38:23 -0700

    msm: kgsl: Fix INIT to ACTIVE state transition
    The state machine should transition from INIT to AWARE always and
    not to ACTIVE under any condition. Also if the device fails to wake up,
    put the device back to SLUMBER and retry.
    
    Change-Id: I2f5feb45395d02ae8e94e2ead9939e4316324bca
    Signed-off-by: Suman Tatiraju <sumant@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1102,6 +1102,7 @@ static int _adreno_start(struct adreno_device *adreno_dev)
 	struct kgsl_device *device = &adreno_dev->dev;
 	struct adreno_gpudev *gpudev = ADRENO_GPU_DEVICE(adreno_dev);
 	int status = -EINVAL;
+	unsigned int state = device->state;
 	unsigned int regulator_left_on = 0;
 	unsigned int pmqos_wakeup_vote = device->pwrctrl.pm_qos_wakeup_latency;
 	unsigned int pmqos_active_vote = device->pwrctrl.pm_qos_active_latency;
@@ -1213,7 +1214,8 @@ error_mmu_off:
 	kgsl_mmu_stop(&device->mmu);
 
 error_pwr_off:
-	kgsl_pwrctrl_change_state(device, KGSL_STATE_INIT);
+	/* set the state back to original state */
+	kgsl_pwrctrl_change_state(device, state);
 
 	if (pmqos_active_vote != pmqos_wakeup_vote)
 		pm_qos_update_request(&device->pwrctrl.pm_qos_req_dma,
