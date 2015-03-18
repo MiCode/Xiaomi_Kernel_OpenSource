43621f9f60c1410801cb15a213deec21a8fdd1a5
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2015-03-18 15:51:38 -0700

    power: power_supply: add POWER_SUPPLY_PROP_FLASH_ACTIVE property
    Add a local extension property POWER_SUPPLY_PROP_FLASH_ACTIVE
    to the list of power supply properties. This will be used to
    notify the power supply if high current loads like flash
    is active or not.
    
    CRs-Fixed: 793295
    Change-Id: I02f27da81f13a258b8c7cd72dae515698a38a3e5
    Signed-off-by: Subbaraman Narayanamurthy <subbaram@codeaurora.org>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -230,6 +230,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(safety_timer_enabled),
 	POWER_SUPPLY_ATTR(charge_done),
 	POWER_SUPPLY_ATTR(allow_detection),
+	POWER_SUPPLY_ATTR(flash_active),
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_ATTR(charge_counter_ext),
 	/* Properties of type `const char *' */
