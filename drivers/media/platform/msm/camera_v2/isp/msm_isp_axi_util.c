8eb418d12a5ed2218b68483c0360b7824da0b94d
Sreesudhan Ramakrish Ramkumar <srramku@codeaurora.org>
2015-03-20 15:46:07 -0700

    msm: camera: isp: Handle axi stream off failure
    In case axi stream off fails, we do a disable write master and
    a reg update. The error needs to be propagated to user space
    after we ensure reg update succeeds. Hence, when stream off fails,
    we wait for config done and then proagate the error to user space.
    
    Change-Id: I7ad01ae77a8ec2e8eae529fec64935b6de5c31c0
    Signed-off-by: Sreesudhan Ramakrish Ramkumar <srramku@codeaurora.org>
    Signed-off-by: Harsh Shah <harshs@codeaurora.org>
    Signed-off-by: Nagesh Subba Reddy <nageshsreddy@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -378,7 +378,8 @@ int msm_isp_axi_check_stream_state(
 			if ((stream_info->state == PAUSING ||
 				stream_info->state == PAUSED ||
 				stream_info->state == RESUME_PENDING ||
-				stream_info->state == RESUMING) &&
+				stream_info->state == RESUMING ||
+				stream_info->state == UPDATING) &&
 				(stream_cfg_cmd->cmd == STOP_STREAM ||
 				stream_cfg_cmd->cmd == STOP_IMMEDIATELY)) {
 				stream_info->state = ACTIVE;
@@ -1318,7 +1319,8 @@ static int msm_isp_update_stream_bandwidth(struct vfe_device *vfe_dev)
 }
 
 static int msm_isp_axi_wait_for_cfg_done(struct vfe_device *vfe_dev,
-	enum msm_isp_camif_update_state camif_update, uint32_t src_mask)
+	enum msm_isp_camif_update_state camif_update,
+	uint32_t src_mask, int regUpdateCnt)
 {
 	int rc;
 	unsigned long flags;
@@ -1326,8 +1328,14 @@ static int msm_isp_axi_wait_for_cfg_done(struct vfe_device *vfe_dev,
 	spin_lock_irqsave(&vfe_dev->shared_data_lock, flags);
 
 	for (i = 0; i < VFE_SRC_MAX; i++) {
-		if (src_mask & (1 << i))
-			vfe_dev->axi_data.stream_update[i] = 2;
+		if (src_mask & (1 << i)) {
+			if (vfe_dev->axi_data.stream_update[i] > 0) {
+				pr_err("%s: Stream Update already in progress. Cannot satisfy request\n",
+					__func__);
+				return -EINVAL;
+			}
+			vfe_dev->axi_data.stream_update[i] = regUpdateCnt;
+		}
 	}
 	if (src_mask) {
 		init_completion(&vfe_dev->stream_config_complete);
@@ -1635,7 +1643,7 @@ static int msm_isp_start_axi_stream(struct vfe_device *vfe_dev,
 
 	if (wait_for_complete) {
 		rc = msm_isp_axi_wait_for_cfg_done(vfe_dev, camif_update,
-			src_mask);
+			src_mask, 2);
 		if (rc < 0)
 			pr_err("%s: wait for config done failed\n", __func__);
 	}
@@ -1706,20 +1714,24 @@ static int msm_isp_stop_axi_stream(struct vfe_device *vfe_dev,
 
 	if (src_mask) {
 		rc = msm_isp_axi_wait_for_cfg_done(vfe_dev, camif_update,
-			src_mask);
+			src_mask, 2);
 		if (rc < 0) {
 			pr_err("%s: wait for config done failed\n", __func__);
 			for (i = 0; i < stream_cfg_cmd->num_streams; i++) {
 				stream_info = &axi_data->stream_info[
 				HANDLE_TO_IDX(
 					stream_cfg_cmd->stream_handle[i])];
-				stream_info->state = STOP_PENDING;
+				stream_info->state = STOPPING;
 				msm_isp_axi_stream_enable_cfg(
 					vfe_dev, stream_info);
 				vfe_dev->hw_info->vfe_ops.core_ops.reg_update(
 					vfe_dev,
 					SRC_TO_INTF(stream_info->stream_src));
-				stream_info->state = INACTIVE;
+				rc = msm_isp_axi_wait_for_cfg_done(vfe_dev,
+					camif_update, src_mask, 1);
+				if (rc < 0)
+					pr_err("cfg done failed\n");
+				rc = -EBUSY;
 			}
 		}
 	}
@@ -2048,7 +2060,7 @@ int msm_isp_update_axi_stream(struct vfe_device *vfe_dev, void *arg)
 				stream_info->state = UPDATING;
 				rc = msm_isp_axi_wait_for_cfg_done(vfe_dev,
 					NO_UPDATE, (1 << SRC_TO_INTF(
-					stream_info->stream_src)));
+					stream_info->stream_src)), 2);
 				if (rc < 0)
 					pr_err("%s: wait for update failed\n",
 						__func__);
@@ -2167,3 +2179,4 @@ void msm_isp_process_axi_irq(struct vfe_device *vfe_dev,
 	}
 	return;
 }
+
