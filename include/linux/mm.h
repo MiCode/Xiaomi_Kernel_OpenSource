7964b3ce47f0d87fbbb1cfdd1fb4aadb620133dd
Vinayak Menon <vinmenon@codeaurora.org>
2015-03-20 14:15:39 +0530

    mm: process reclaim: vmpressure based process reclaim
    With this patch, anon pages of inactive tasks can be reclaimed,
    depending on memory pressure. Memory pressure is detected
    using vmpressure events. 'N' best tasks in terms of anon
    size is selected and pages proportional to their tasksize
    is reclaimed. The total number of pages reclaimed at each
    run of the swap work, can be tuned from userspace, the
    default being SWAP_CLUSTER_MAX * 32.
    
    The patch also adds tracepoints to debug and tune the
    feature.
    
    echo 1 > /sys/module/process_reclaim/parameters/enable_process_reclaim
    to enable the feature.
    
    echo <pages> > /sys/module/process_reclaim/parameters/per_swap_size,
    to set the number of pages reclaimed in each scan.
    
    /sys/module/process_reclaim/parameters/reclaim_avg_efficiency, provides
    the average efficiency (scan to reclaim ratio) of the algorithm.
    
    /sys/module/process_reclaim/parameters/swap_eff_win, to set the window
    period (in unit of number of times reclaim is triggered) to detect
    low efficiency runs.
    
    /sys/module/process_reclaim/parameters/swap_opt_eff, to set the optimal
    efficiency threshold for low efficiency detection.
    
    Change-Id: I895986f10c997d1715761eaaadc4bbbee60db9d2
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2749,5 +2749,19 @@ static inline void setup_nr_node_ids(void) {}
 
 extern int want_old_faultaround_pte;
 
+#ifdef CONFIG_PROCESS_RECLAIM
+struct reclaim_param {
+	struct vm_area_struct *vma;
+	/* Number of pages scanned */
+	int nr_scanned;
+	/* max pages to reclaim */
+	int nr_to_reclaim;
+	/* pages reclaimed */
+	int nr_reclaimed;
+};
+extern struct reclaim_param reclaim_task_anon(struct task_struct *task,
+		int nr_to_reclaim);
+#endif
+
 #endif /* __KERNEL__ */
 #endif /* _LINUX_MM_H */
