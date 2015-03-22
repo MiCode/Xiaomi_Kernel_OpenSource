e146c8b817d9a963d11b9d7116d7e40266cf485c
flar2 <asegaert@gmail.com>
2015-03-22 14:48:06 -0400

    deadline: decrease fifo_batch
    Signed-off-by: flar2 <asegaert@gmail.com>

--- block/deadline-iosched.c
+++ block/deadline-iosched.c
@@ -20,7 +20,7 @@
 static const int read_expire = HZ / 2;  /* max time before a read is submitted. */
 static const int write_expire = 5 * HZ; /* ditto for writes, these limits are SOFT! */
 static const int writes_starved = 2;    /* max times reads can starve a write */
-static const int fifo_batch = 16;       /* # of sequential requests treated as one
+static const int fifo_batch = 8;       /* # of sequential requests treated as one
 				     by the above parameters. For throughput. */
 
 struct deadline_data {
