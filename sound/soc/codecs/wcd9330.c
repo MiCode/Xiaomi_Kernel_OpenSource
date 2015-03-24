c07f00d989f23b1e552ed1cd795dafc1e796fc55
Phani Kumar Uppalapati <phaniu@codeaurora.org>
2015-03-23 20:29:12 -0700

    ASoC: wcd9330: Remove unnecessary check for BG_CLK lock
    Having a check whether BG_CLK acquired or not during
    RCO enable will prevent callers requesting for RCO
    if the lock is acquired in a different context. Thus,
    RCO clock will not get turned ON leading to the use-case
    failure. Remove the redundant check for the BG_CLK lock.
    
    Change-Id: I1d422dcb4e4627dc86c4f63da79f91cd216ad8ce
    Signed-off-by: Phani Kumar Uppalapati <phaniu@codeaurora.org>

--- sound/soc/codecs/wcd9330.c
+++ sound/soc/codecs/wcd9330.c
@@ -2965,13 +2965,6 @@ static int tomtom_codec_internal_rco_ctrl(struct snd_soc_codec *codec,
 	struct tomtom_priv *tomtom = snd_soc_codec_get_drvdata(codec);
 	int ret = 0;
 
-	if (mutex_is_locked(&tomtom->resmgr.codec_bg_clk_lock)) {
-		dev_err(codec->dev, "%s: BG_CLK already acquired\n",
-			__func__);
-		ret = -EINVAL;
-		goto done;
-	}
-
 	if (enable) {
 		if (wcd9xxx_resmgr_get_clk_type(&tomtom->resmgr) ==
 		    WCD9XXX_CLK_RCO) {
@@ -2997,7 +2990,6 @@ static int tomtom_codec_internal_rco_ctrl(struct snd_soc_codec *codec,
 		WCD9XXX_BG_CLK_UNLOCK(&tomtom->resmgr);
 	}
 
-done:
 	return ret;
 }
 
