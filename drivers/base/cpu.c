669e200daec020f5a2ada8bd1b32cbc79e9255a0
Abhimanyu Kapur <abhimany@codeaurora.org>
2015-03-23 17:53:06 -0700

    drivers: base: cpu: fix compiler warning
    Linux Kernel 3.18 introduced few compile-time warnings, fix
    them.
    
    1. /kernel/drivers/base/cpu.c:317:12: warning: 'cpu_uevent' defined but not used [-Wunused-function]
     static int cpu_uevent(struct device *dev, struct kobj_uevent_env *evt)
    
    Signed-off-by: Abhimanyu Kapur <abhimany@codeaurora.org>

--- drivers/base/cpu.c
+++ drivers/base/cpu.c
@@ -314,6 +314,7 @@ static ssize_t print_cpu_modalias(struct device *dev,
 #define print_cpu_modalias	arch_print_cpu_modalias
 #endif
 
+#ifdef CONFIG_ARCH_HAS_CPU_AUTOPROBE
 static int cpu_uevent(struct device *dev, struct kobj_uevent_env *env)
 {
 	char *buf = kzalloc(PAGE_SIZE, GFP_KERNEL);
@@ -324,6 +325,7 @@ static int cpu_uevent(struct device *dev, struct kobj_uevent_env *env)
 	}
 	return 0;
 }
+#endif /*CONFIG_ARCH_HAS_CPU_AUTOPROBE*/
 #endif
 
 /*
