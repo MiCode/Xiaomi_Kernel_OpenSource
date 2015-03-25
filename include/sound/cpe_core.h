7d1ff9a36bb6cf65250334124bbde409be5d6043
Bhalchandra Gajare <gajare@codeaurora.org>
2015-03-24 19:20:09 -0700

    ASoC: msm-cpe-lsm: Buffer allocation cleanup
    The buffer used for look ahead buffering is allocated by cpe core
    driver. Fix to perform cleanup of data structures shared across platform
    driver and cpe core to allow buffer allocation to be done by platform
    driver.
    
    Change-Id: Ifaff24025292f2e5be6572ca50dcc281ed8d4f84
    Signed-off-by: Bhalchandra Gajare <gajare@codeaurora.org>

--- include/sound/cpe_core.h
+++ include/sound/cpe_core.h
@@ -18,7 +18,6 @@
 #include <linux/wait.h>
 #include <linux/dma-mapping.h>
 #include <sound/lsm_params.h>
-#include <linux/mfd/wcd9xxx/wcd9xxx-slimslave.h>
 
 enum {
 	CMD_INIT_STATE = 0,
@@ -40,47 +39,13 @@ struct wcd_cpe_afe_port_cfg {
 	u32 sample_rate;
 };
 
-enum wcd_cpe_lab_thread {
-	MSM_LSM_LAB_THREAD_STOP,
-	MSM_LSM_LAB_THREAD_RUNNING,
-	MSM_LSM_LAB_THREAD_ERROR,
-};
-
-struct wcd_cpe_data_pcm_buf {
-	u8 *mem;
-	phys_addr_t phys;
-};
-
-struct wcd_cpe_lab_hw_params {
-	u16 sample_rate;
-	u16 sample_size;
-	u32 buf_sz;
-	u32 period_count;
-};
-
-struct wcd_cpe_lsm_lab {
-	u32 lab_enable;
-	void *core_handle;
-	atomic_t in_count;
-	atomic_t abort_read;
-	u32 dma_write;
-	u32 buf_idx;
-	u32 pcm_size;
-	enum wcd_cpe_lab_thread thread_status;
-	struct cpe_lsm_session *lsm_s;
-	struct snd_pcm_substream *substream;
-	struct wcd_cpe_lab_hw_params hw_params;
-	struct wcd_cpe_data_pcm_buf *pcm_buf;
-	wait_queue_head_t period_wait;
-	struct completion thread_complete;
-	struct completion comp;
-};
-
 struct cpe_lsm_session {
 	/* sound model related */
 	void *snd_model_data;
 	u8 *conf_levels;
 	void *cmi_reg_handle;
+
+	/* Clients private data */
 	void *priv_d;
 
 	void (*event_cb) (void *priv_data,
@@ -97,8 +62,9 @@ struct cpe_lsm_session {
 	u8 id;
 	u8 num_confidence_levels;
 	struct task_struct *lsm_lab_thread;
-	struct wcd_cpe_lsm_lab lab;
 	bool started;
+
+	u32 lab_enable;
 };
 
 struct wcd_cpe_afe_ops {
@@ -156,7 +122,6 @@ struct wcd_cpe_lsm_ops {
 
 	int (*lsm_lab_control)(void *core_handle,
 			       struct cpe_lsm_session *session,
-			       u32 bufsz, u32 bufcnt,
 			       bool enable);
 
 	int (*lab_ch_setup)(void *core_handle,
