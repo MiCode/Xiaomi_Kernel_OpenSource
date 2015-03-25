51527079bd503f199e56f54b263f1c03ddfe7822
Ken Zhang <kenz@codeaurora.org>
2015-03-25 12:32:34 -0700

    Revert "msm: mdss: Override partial update PP block using ioctl"
    This reverts commit 679dc362a9effee922ef2ce1447424b318f667bc.
    The way overriding partial update via PP ioctl is obsolete,
    partial update control is going through sysfs. The left code
    is blocking pp ioctl even partial update is disabled dynamically.
    
    Change-Id: Icb455a0101b53c58ae6c1b2686bd2297b4832396
    Signed-off-by: Ken Zhang <kenz@codeaurora.org>
    Signed-off-by: Ping Li <pingli@codeaurora.org>

--- drivers/video/msm/mdss/mdss_compat_utils.c
+++ drivers/video/msm/mdss/mdss_compat_utils.c
@@ -2648,17 +2648,6 @@ static int __from_user_calib_dcm_state(
 	return 0;
 }
 
-static int __from_user_pp_init_data(
-			struct mdp_pp_init_data32 __user *init_data32,
-			struct mdp_pp_init_data __user *init_data)
-{
-	if (copy_in_user(&init_data->init_request, &init_data32->init_request,
-			sizeof(uint32_t)))
-		return -EFAULT;
-
-	return 0;
-}
-
 static u32 __pp_compat_size_igc(void)
 {
 	u32 alloc_size = 0;
@@ -3022,14 +3011,6 @@ static int mdss_compat_pp_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = __from_user_calib_dcm_state(
 			compat_ptr((uintptr_t)&pp32->data.calib_dcm),
 			&pp->data.calib_dcm);
-		if (ret)
-			goto pp_compat_exit;
-		ret = mdss_fb_do_ioctl(info, cmd, (unsigned long) pp);
-		break;
-	case mdp_op_pp_init_cfg:
-		ret = __from_user_pp_init_data(
-			compat_ptr((uintptr_t)&pp32->data.init_data),
-			&pp->data.init_data);
 		if (ret)
 			goto pp_compat_exit;
 		ret = mdss_fb_do_ioctl(info, cmd, (unsigned long) pp);
