255285a03b7cf2ca40fc74b35922e5c7c1be3fb6
Joonwoo Park <joonwoop@codeaurora.org>
2015-03-25 11:54:55 -0700

    timer: reduce cache bouncing of deferral timer wheel
    Commit a40f752 (timer: make deferrable cpu unbound timers really not
    bound to a cpu) made any non-idle CPUs in the system can service expired
    deferral timer entries.  This means deferral timer entries can be
    serviced as early as possible after it expires but in many cases it's
    suboptimal since switching the CPU of timer wheel incurs a cache
    bouncing/synchronization cost.
    
    Reduce cache bouncing by servicing the deferrable timer wheel with timer
    tick CPU as it doesn't bounce as often but still guarantees to run timer
    softirq when there is any CPU that isn't idle.
    
    CRs-fixed: 815184
    Change-Id: I6d51390832538ad90eccf459e90bff0b25aad09f
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- kernel/timer.c
+++ kernel/timer.c
@@ -49,6 +49,8 @@
 #include <asm/timex.h>
 #include <asm/io.h>
 
+#include "time/tick-internal.h"
+
 #define CREATE_TRACE_POINTS
 #include <trace/events/timer.h>
 
@@ -1154,20 +1156,15 @@ static void call_timer_fn(struct timer_list *timer, void (*fn)(unsigned long),
 /**
  * __run_timers - run all expired timers (if any) on this CPU.
  * @base: the timer vector to be processed.
- * @try: try and just return if base's lock already acquired.
  *
  * This function cascades all vectors and executes all expired timer
  * vectors.
  */
-static inline void __run_timers(struct tvec_base *base, bool try)
+static inline void __run_timers(struct tvec_base *base)
 {
 	struct timer_list *timer;
 
-	if (!try)
-		spin_lock_irq(&base->lock);
-	else if (!spin_trylock_irq(&base->lock))
-		return;
-
+	spin_lock_irq(&base->lock);
 	while (time_after_eq(jiffies, base->timer_jiffies)) {
 		struct list_head work_list;
 		struct list_head *head = &work_list;
@@ -1395,16 +1392,13 @@ static void run_timer_softirq(struct softirq_action *h)
 	hrtimer_run_pending();
 
 #ifdef CONFIG_SMP
-	if (time_after_eq(jiffies, tvec_base_deferral->timer_jiffies))
-		/*
-		 * if other cpu is handling cpu unbound deferrable timer base,
-		 * current cpu doesn't need to handle it so pass try=true.
-		 */
-		__run_timers(tvec_base_deferral, true);
+	if (smp_processor_id() == tick_do_timer_cpu &&
+	    time_after_eq(jiffies, tvec_base_deferral->timer_jiffies))
+		__run_timers(tvec_base_deferral);
 #endif
 
 	if (time_after_eq(jiffies, base->timer_jiffies))
-		__run_timers(base, false);
+		__run_timers(base);
 }
 
 /*
