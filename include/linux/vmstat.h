7da4414ce51d198099cf0a099e6a887500e99930
Vinayak Menon <vinmenon@codeaurora.org>
2015-03-26 11:58:45 +0530

    mm: page-writeback: fix page state calculation in throttle_vm_writeout
    It was found that a number of tasks were blocked in the reclaim path
    (throttle_vm_writeout) for seconds, because of vmstat_diff not being
    synced in time. Fix that by adding a new function
    global_page_state_snapshot.
    
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    Change-Id: Iec167635ad724a55c27bdbd49eb8686e7857216c

--- include/linux/vmstat.h
+++ include/linux/vmstat.h
@@ -142,6 +142,25 @@ static inline unsigned long zone_page_state_snapshot(struct zone *zone,
 	return x;
 }
 
+static inline unsigned long global_page_state_snapshot(enum zone_stat_item item)
+{
+	long x = atomic_long_read(&vm_stat[item]);
+
+#ifdef CONFIG_SMP
+	struct zone *zone;
+	int cpu;
+	for_each_online_cpu(cpu) {
+		for_each_populated_zone(zone)
+			x += per_cpu_ptr(zone->pageset,
+				cpu)->vm_stat_diff[item];
+	}
+
+	if (x < 0)
+		x = 0;
+#endif
+	return x;
+}
+
 #ifdef CONFIG_NUMA
 /*
  * Determine the per node value of a stat item. This function
