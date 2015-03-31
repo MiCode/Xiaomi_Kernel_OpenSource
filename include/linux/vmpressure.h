764ae0076ae8ba7ea0e19e14c70f7312b4586120
Vinayak Menon <vinmenon@codeaurora.org>
2015-03-31 11:06:29 +0530

    mm: vmpressure: scale pressure based on reclaim context
    The existing calculation of vmpressure takes into account only
    the ratio of reclaimed to scanned pages, but not the time spent
    or the difficulty in reclaiming those pages. For e.g. when there
    are quite a number of file pages in the system, an allocation
    request can be satisfied by reclaiming the file pages alone. If
    such a reclaim is successful, the vmpressure value will remain low
    irrespective of the time spent by the reclaim code to free up the
    file pages. With a feature like lowmemorykiller, killing a task
    can be faster than reclaiming the file pages alone. So if the
    vmpressure values reflect the reclaim difficulty level, clients
    can make a decision based on that, for e.g. to kill a task early.
    
    This patch monitors the number of pages scanned in the direct
    reclaim path and scales the vmpressure level according to that.
    
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    Change-Id: I6e643d29a9a1aa0814309253a8b690ad86ec0b13

--- include/linux/vmpressure.h
+++ include/linux/vmpressure.h
@@ -12,6 +12,7 @@
 struct vmpressure {
 	unsigned long scanned;
 	unsigned long reclaimed;
+	unsigned long stall;
 	/* The lock is used to keep the scanned/reclaimed above in sync. */
 	struct spinlock sr_lock;
 
