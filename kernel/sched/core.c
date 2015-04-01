3846054d399ffd91300434730766c144d0df94d1
Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
2015-04-01 00:29:14 -0700

    Revert "sched: Fix up scheduler syscall LTP fails"
    This reverts commit ca454d54c437dc6cb4ec1754ab1a46524b640194.
    The above commit was an upstream cherry-pick ported to msm-3.10
    in the absence of the deadline scheduler. Revert this in anticipation
    of introducing the deadline scheduler. The original patch will be
    brought in as part of the deadline scheduler port.
    
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5635,20 +5635,12 @@ static void __setscheduler(struct rq *rq, struct task_struct *p,
 {
 	int policy = attr->sched_policy;
 
-	if (policy == -1) /* setparam */
-		policy = p->policy;
-
 	p->policy = policy;
 
-	if (fair_policy(policy))
+	if (rt_policy(policy))
+		p->rt_priority = attr->sched_priority;
+	else
 		p->static_prio = NICE_TO_PRIO(attr->sched_nice);
-
-	/*
-	 * __sched_setscheduler() ensures attr->sched_priority == 0 when
-	 * !rt_policy. Always setting this ensures that things like
-	 * getparam()/getattr() don't report silly values for !rt tasks.
-	 */
-	p->rt_priority = attr->sched_priority;
 
 	p->normal_prio = normal_prio(p);
 	p->prio = rt_mutex_getprio(p);
