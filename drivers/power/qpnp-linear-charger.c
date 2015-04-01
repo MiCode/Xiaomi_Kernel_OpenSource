91a9759f5f9c5336ada699d73ee9111117801ab6
Venkatesh Yadav Abbarapu <quicvenkat@codeaurora.org>
2015-04-01 13:55:34 +0530

    power: qpnp-linear-charger: Fix array index issue
    The array index can go out of bound, so fixing it.
    
    Change-Id: I058060b33221654f797284d38e88061058b5e0f1
    Signed-off-by: Venkatesh Yadav Abbarapu <quicvenkat@codeaurora.org>

--- drivers/power/qpnp-linear-charger.c
+++ drivers/power/qpnp-linear-charger.c
@@ -600,7 +600,7 @@ static u8 qpnp_lbc_get_trim_val(struct qpnp_lbc_chip *chip)
 		i = 0;
 		break;
 	case 1:
-		for (i = TRIM_CENTER; i <= ARRAY_SIZE(vddtrim_map); i++) {
+		for (i = TRIM_CENTER; i < ARRAY_SIZE(vddtrim_map); i++) {
 			if (vddtrim_map[i].trim_uv < chip->delta_vddmax_uv) {
 				delta_uv = AVG(vddtrim_map[i].trim_uv,
 						vddtrim_map[i - 1].trim_uv);
@@ -610,7 +610,7 @@ static u8 qpnp_lbc_get_trim_val(struct qpnp_lbc_chip *chip)
 					return vddtrim_map[i].trim_val;
 			}
 		}
-		i = ARRAY_SIZE(vddtrim_map);
+		i = ARRAY_SIZE(vddtrim_map) - 1;
 		break;
 	}
 
