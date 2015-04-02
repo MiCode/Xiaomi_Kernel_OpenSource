919bea9fd5ff29ee12f2d009b7533f7d2f0d1926
Ram Chandrasekar <rkumbako@codeaurora.org>
2015-04-02 10:44:57 -0600

    leds: led-class: Retain the latest user brightness request
    Retain the latest user brightness request. This ensures
    that when a max brightness limit is altered, the last
    brightness request is taken into account, when restoring
    the current brightness.
    
    Change-Id: I6461e06f64abe336cabc27a3992d734f1fb745c6
    Signed-off-by: Ram Chandrasekar <rkumbako@codeaurora.org>

--- drivers/leds/led-class.c
+++ drivers/leds/led-class.c
@@ -53,6 +53,7 @@ static ssize_t led_brightness_store(struct device *dev,
 	if (ret)
 		return ret;
 
+	led_cdev->usr_brightness_req = state;
 	__led_set_brightness(led_cdev, state);
 
 	return size;
@@ -71,7 +72,7 @@ static ssize_t led_max_brightness_store(struct device *dev,
 		if (state > LED_FULL)
 			state = LED_FULL;
 		led_cdev->max_brightness = state;
-		led_set_brightness(led_cdev, led_cdev->brightness);
+		led_set_brightness(led_cdev, led_cdev->usr_brightness_req);
 	}
 
 	return ret;
