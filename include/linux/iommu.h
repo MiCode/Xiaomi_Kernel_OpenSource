044e57d726fbcc5f8091665f071144dca6d144c0
Mitchel Humpherys <mitchelh@codeaurora.org>
2015-04-01 19:03:31 -0700

    iommu: add IOMMU_PRIV attribute
    Add the IOMMU_PRIV attribute, which is used to indicate privileged
    mappings.
    
    Change-Id: If8576c09898bd7953475ab72e178d3288a763b0c
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -28,6 +28,7 @@
 #define IOMMU_WRITE	(1 << 1)
 #define IOMMU_CACHE	(1 << 2) /* DMA cache coherency */
 #define IOMMU_EXEC	(1 << 3)
+#define IOMMU_PRIV	(1 << 4)
 
 struct iommu_ops;
 struct iommu_group;
