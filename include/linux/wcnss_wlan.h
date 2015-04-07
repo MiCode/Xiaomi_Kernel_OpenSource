0b4f84b1dfe7fa2a4461c4374f761b105ec5b9f1
Siddharth Bhal <sbhal@codeaurora.org>
2015-04-07 16:31:04 +0530

    wcnss: Ensure wcnss clocks are present before register dump
    Adding wcnss 'measure' and 'debug' clock checks before dumping
    wcnss pronto registers. This is required to ensure that registers
    are dumped only when required clocks are present, otherwise it
    could lead to undefined behavior.
    
    CRs-Fixed: 815208
    Change-Id: Iaa920b7f9ad02ef0a4e9e1537f2cd5de8be60b63
    Signed-off-by: Siddharth Bhal <sbhal@codeaurora.org>

--- include/linux/wcnss_wlan.h
+++ include/linux/wcnss_wlan.h
@@ -78,6 +78,7 @@ int wcnss_hardware_type(void);
 void *wcnss_prealloc_get(unsigned int size);
 int wcnss_prealloc_put(void *ptr);
 void wcnss_reset_intr(void);
+void wcnss_reset_fiq(bool clk_chk_en);
 void wcnss_suspend_notify(void);
 void wcnss_resume_notify(void);
 void wcnss_riva_log_debug_regs(void);
