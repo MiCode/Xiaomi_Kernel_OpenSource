6b318172a3511645f6a41a42768d6aee95002580
Chunmei Cai <ccai@codeaurora.org>
2015-04-08 15:15:46 +0800

    thermal: qpnp-adc-tm: add new scale function for skue battery thermistor
    The skue is using the different battery thermistor and mapping table
    from the default. A new scale function is needed to use this mapping
    table in the thermal adc driver.
    
    Change-Id: Ib154bafe821ceae33803efced3c15321a39f1dad
    Signed-off-by: Chunmei Cai <ccai@codeaurora.org>

--- drivers/thermal/qpnp-adc-tm.c
+++ drivers/thermal/qpnp-adc-tm.c
@@ -292,6 +292,7 @@ static struct qpnp_adc_tm_reverse_scale_fn adc_tm_rscale_fn[] = {
 	[SCALE_R_SMB_BATT_THERM] = {qpnp_adc_smb_btm_rscaler},
 	[SCALE_R_ABSOLUTE] = {qpnp_adc_absolute_rthr},
 	[SCALE_QRD_SKUH_RBATT_THERM] = {qpnp_adc_qrd_skuh_btm_scaler},
+	[SCALE_QRD_SKUE_RBATT_THERM] = {qpnp_adc_qrd_skue_btm_scaler},
 };
 
 static int32_t qpnp_adc_tm_read_reg(struct qpnp_adc_tm_chip *chip,
