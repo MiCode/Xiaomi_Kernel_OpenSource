d12c004a3121ab62b1d816b519455672e7d017c3
Casey Piper <cpiper@codeaurora.org>
2015-04-13 11:58:50 -0700

    msm: mdss: update polarity based on HDMI resolution
    Set horizontal and vertical polarities based on the
    HDMI resolution and use these polarities when setting up
    the MDP timing generator.
    
    CRs-Fixed: 2016373
    Change-Id: Ifaf4d72a25e512ecccf13f479a77c8e860978082
    Signed-off-by: Casey Piper <cpiper@codeaurora.org>
    Signed-off-by: Krishna Srinivas <krisrini@codeaurora.org>
    Signed-off-by: Yuchao Ma <yuchaom@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_hdmi_edid.c
+++ drivers/video/fbdev/msm/mdss_hdmi_edid.c
@@ -62,11 +62,6 @@
 #define EDID_VENDOR_ID_SIZE     4
 #define EDID_IEEE_REG_ID        0x0c03
 
-enum edid_sink_mode {
-	SINK_MODE_DVI,
-	SINK_MODE_HDMI
-};
-
 enum luminance_value {
 	NO_LUMINANCE_DATA = 3,
 	MAXIMUM_LUMINANCE = 4,
@@ -2406,7 +2401,7 @@ end:
 	return scaninfo;
 } /* hdmi_edid_get_sink_scaninfo */
 
-static u32 hdmi_edid_get_sink_mode(void *input)
+u32 hdmi_edid_get_sink_mode(void *input, u32 mode)
 {
 	struct hdmi_edid_ctrl *edid_ctrl = (struct hdmi_edid_ctrl *)input;
 	bool sink_mode;
@@ -2419,8 +2414,13 @@ static u32 hdmi_edid_get_sink_mode(void *input)
 	if (edid_ctrl->edid_override &&
 		(edid_ctrl->override_data.sink_mode != -1))
 		sink_mode = edid_ctrl->override_data.sink_mode;
-	else
-		sink_mode = edid_ctrl->sink_mode;
+	else {
+		if (edid_ctrl->sink_mode &&
+			(mode > 0 && mode <= HDMI_EVFRMT_END))
+			sink_mode = SINK_MODE_HDMI;
+		else
+			sink_mode = SINK_MODE_DVI;
+	}
 
 	return sink_mode;
 } /* hdmi_edid_get_sink_mode */
@@ -2435,10 +2435,21 @@ static u32 hdmi_edid_get_sink_mode(void *input)
  */
 bool hdmi_edid_is_dvi_mode(void *input)
 {
-	if (hdmi_edid_get_sink_mode(input))
-		return false;
-	else
+	struct hdmi_edid_ctrl *edid_ctrl = (struct hdmi_edid_ctrl *)input;
+	int sink_mode;
+
+	if (!edid_ctrl) {
+		DEV_ERR("%s: invalid input\n", __func__);
 		return true;
+	}
+
+	if (edid_ctrl->edid_override &&
+		(edid_ctrl->override_data.sink_mode != -1))
+		sink_mode = edid_ctrl->override_data.sink_mode;
+	else
+		sink_mode = edid_ctrl->sink_mode;
+
+	return (sink_mode == SINK_MODE_DVI);
 }
 
 /**
