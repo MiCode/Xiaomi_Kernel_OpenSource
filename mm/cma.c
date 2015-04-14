f26b730b95ed62e0c8762865605d32764aef5c33
Sasha Levin <sasha.levin@oracle.com>
2015-04-14 15:44:57 -0700

    mm: cma: debugfs interface
    I've noticed that there is no interfaces exposed by CMA which would let me
    fuzz what's going on in there.
    
    This small patchset exposes some information out to userspace, plus adds
    the ability to trigger allocation and freeing from userspace.
    
    This patch (of 3):
    
    Implement a simple debugfs interface to expose information about CMA areas
    in the system.
    
    Useful for testing/sanity checks for CMA since it was impossible to
    previously retrieve this information in userspace.
    
    Change-Id: Idf4fa2c58503a6aa46f70caa01048e24ec9447f3
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>
    Acked-by: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Marek Szyprowski <m.szyprowski@samsung.com>
    Cc: Laura Abbott <lauraa@codeaurora.org>
    Cc: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Git-commit: 28b24c1fc8c22cabe5b8a16ffe6a61dfce51a1f2
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    [lmark@codeaurora.org: resolve trivial merge conflicts]
    Signed-off-by: Liam Mark <lmark@codeaurora.org>

--- mm/cma.c
+++ mm/cma.c
@@ -35,16 +35,10 @@
 #include <linux/highmem.h>
 #include <linux/delay.h>
 
-struct cma {
-	unsigned long	base_pfn;
-	unsigned long	count;
-	unsigned long	*bitmap;
-	unsigned int order_per_bit; /* Order of pages represented by one bit */
-	struct mutex	lock;
-};
+#include "cma.h"
 
-static struct cma cma_areas[MAX_CMA_AREAS];
-static unsigned cma_area_count;
+struct cma cma_areas[MAX_CMA_AREAS];
+unsigned cma_area_count;
 static DEFINE_MUTEX(cma_mutex);
 
 phys_addr_t cma_get_base(struct cma *cma)
@@ -62,11 +56,6 @@ static unsigned long cma_bitmap_aligned_mask(struct cma *cma, int align_order)
 	if (align_order <= cma->order_per_bit)
 		return 0;
 	return (1UL << (align_order - cma->order_per_bit)) - 1;
-}
-
-static unsigned long cma_bitmap_maxno(struct cma *cma)
-{
-	return cma->count >> cma->order_per_bit;
 }
 
 static unsigned long cma_bitmap_pages_to_bits(struct cma *cma,
