7b3d59e23e302cc6ead46d1ffbe47ca9ead0f65c
Arun KS <arunks@codeaurora.org>
2015-04-16 02:03:34 +0530

    clk: qcom: 8952: Fix parent for apss_ahb_clk_src
    apss_ahb_clk_src need to vote only for active set of xo clk.
    So that xo vote will not be applied to sleep set and hence
    vote will be ignored during a power collapse.
    Correct the parent to xo active clk.
    
    Change-Id: Id3afc6ce8f9a15bd239d37394c7615a45d72affe
    Signed-off-by: Arun KS <arunks@codeaurora.org>

--- drivers/clk/qcom/clock-gcc-8952.c
+++ drivers/clk/qcom/clock-gcc-8952.c
@@ -500,7 +500,7 @@ static struct rcg_clk camss_top_ahb_clk_src = {
 };
 
 static struct clk_freq_tbl ftbl_gcc_apss_ahb_clk[] = {
-	F( 19200000,	xo,	1,	0,	0),
+	F( 19200000,	xo_a,	1,	0,	0),
 	F( 50000000,	gpll0,	16,	0,	0),
 	F( 100000000,	gpll0,	8,	0,	0),
 	F( 133330000,	gpll0,	6,	0,	0),
