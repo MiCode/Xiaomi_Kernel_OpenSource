5d37480381c0ff065a3a96a5f2165014584acd9b
Subhash Jadavani <subhashj@codeaurora.org>
2015-04-15 12:32:08 -0700

    scsi: ufs: don't print gear switch message during clock scaling
    We print the ERROR level log message for every gear switch which would
    generally end up in serial console but clock scaling also does gear
    switch and these gear switch log messages might clutter the kernel logs
    unnecessarily hence this change avoids printing gear switch log messages
    during clock scaling.
    
    Change-Id: If1079bbff633c3270ddae8579801d1f265707254
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -4105,8 +4105,6 @@ int ufshcd_change_power_mode(struct ufs_hba *hba,
 			sizeof(struct ufs_pa_layer_attr));
 	}
 
-	ufshcd_print_pwr_info(hba);
-
 	return ret;
 }
 
@@ -4128,6 +4126,8 @@ static int ufshcd_config_pwr_mode(struct ufs_hba *hba,
 		memcpy(&final_params, desired_pwr_mode, sizeof(final_params));
 
 	ret = ufshcd_change_power_mode(hba, &final_params);
+	if (!ret)
+		ufshcd_print_pwr_info(hba);
 
 	return ret;
 }
