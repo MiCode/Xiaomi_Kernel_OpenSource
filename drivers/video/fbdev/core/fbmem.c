615ee3afbef30d695c40cf5413e1126c0f901168
Veera Sundaram Sankaran <veeras@codeaurora.org>
2015-04-16 18:53:46 -0700

    fbmem: Remove mutex use from do_fb_ioctl()
    Remove use of this mutex to allow multiple msm_fb ioctl calls.
    Mutexes within the individual fb_ioctl calls already provide
    the necessary locking.
    
    This is based on android-msm-2.6.35 commit:
    a809819d (msm_fb: display: Add delay kickoff to MDDI)
    which in includes this change among others.
    
    Change-Id: Iee68bf3556ab913f916868988f4adbad1ed6b2a0
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>
    Signed-off-by: Veera Sundaram Sankaran <veeras@codeaurora.org>

--- drivers/video/fbdev/core/fbmem.c
+++ drivers/video/fbdev/core/fbmem.c
@@ -1212,14 +1212,11 @@ static long do_fb_ioctl(struct fb_info *info, unsigned int cmd,
 		console_unlock();
 		break;
 	default:
-		if (!lock_fb_info(info))
-			return -ENODEV;
 		fb = info->fbops;
 		if (fb->fb_ioctl)
 			ret = fb->fb_ioctl(info, cmd, arg);
 		else
 			ret = -ENOTTY;
-		unlock_fb_info(info);
 	}
 	return ret;
 }
