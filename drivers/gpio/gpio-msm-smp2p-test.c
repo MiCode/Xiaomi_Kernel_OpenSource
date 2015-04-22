74b382f8ddae742d2d559061d570d5c4fcfe740d
Steven Cahail <scahail@codeaurora.org>
2015-04-22 11:45:01 -0600

    soc: qcom: smp2p: Add support for TZ subsystem
    The TZ subsystem now supports SMP2P communication, so add the necessary
    support to allow the apps subsystem to talk to the TZ subsystem.
    
    Change-Id: I09512a9af6f5f8da486156c7e063fd1155ece402
    Signed-off-by: Steven Cahail <scahail@codeaurora.org>

--- drivers/gpio/gpio-msm-smp2p-test.c
+++ drivers/gpio/gpio-msm-smp2p-test.c
@@ -95,6 +95,10 @@ static int smp2p_gpio_test_probe(struct platform_device *pdev)
 		gpio_info_ptr = &gpio_info[SMP2P_WIRELESS_PROC].in;
 	} else if (strcmp("qcom,smp2pgpio_test_smp2p_4_out", node->name) == 0) {
 		gpio_info_ptr = &gpio_info[SMP2P_WIRELESS_PROC].out;
+	} else if (strcmp("qcom,smp2pgpio_test_smp2p_7_in", node->name) == 0) {
+		gpio_info_ptr = &gpio_info[SMP2P_TZ_PROC].in;
+	} else if (strcmp("qcom,smp2pgpio_test_smp2p_7_out", node->name) == 0) {
+		gpio_info_ptr = &gpio_info[SMP2P_TZ_PROC].out;
 	} else if (strcmp("qcom,smp2pgpio_test_smp2p_15_in", node->name) == 0) {
 		gpio_info_ptr = &gpio_info[SMP2P_REMOTE_MOCK_PROC].in;
 	} else if (
@@ -143,6 +147,10 @@ static struct of_device_id msm_smp2p_match_table[] = {
 	/* wcnss */
 	{.compatible = "qcom,smp2pgpio_test_smp2p_4_out", },
 	{.compatible = "qcom,smp2pgpio_test_smp2p_4_in", },
+
+	/* TZ */
+	{.compatible = "qcom,smp2pgpio_test_smp2p_7_out", },
+	{.compatible = "qcom,smp2pgpio_test_smp2p_7_in", },
 
 	/* mock loopback */
 	{.compatible = "qcom,smp2pgpio_test_smp2p_15_out", },
