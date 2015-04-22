20daa23365444da21ee14700d25fd7fe44dd3388
Junjie Wu <junjiew@codeaurora.org>
2015-04-22 09:52:00 -0700

    cpufreq: interactive: Add cpuload trace events
    With per-cluster timer implementation, only max load across CPUs in
    cluster is traced in timer function. Add cpufreq_interactive_cpuload
    trace to provide per-cpu load information.
    
    Change-Id: Icea9f2574332a4bc472b14193e77d76100a896ed
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -451,6 +451,8 @@ static void __cpufreq_interactive_timer(unsigned long data, bool is_notif)
 		}
 		tmploadadjfreq = (unsigned int)cputime_speedadj * 100;
 		pcpu->loadadjfreq = tmploadadjfreq;
+		trace_cpufreq_interactive_cpuload(i, tmploadadjfreq /
+						  ppol->policy->cur);
 
 		if (tmploadadjfreq > loadadjfreq) {
 			loadadjfreq = tmploadadjfreq;
