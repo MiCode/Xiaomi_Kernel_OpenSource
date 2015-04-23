d11c6a5da7081a22fb1d5594e08a0046cfc2f87c
Laxminath Kasam <lkasam@codeaurora.org>
2015-04-23 19:21:07 +0530

    ASoC: change audio drivers to accomodate sid 33bit in 32-mode
    In kernel 32-mode, SID address being 33bit is truncated.
    To address this issue, update audio drivers to fetch
    SMMU SID info from audio ion driver and fill the upper 32 bits
    accordingly for 32-bit mode.
    
    Change-Id: Ie2fa573def36d7b346d6a96249239ef1ecc7f8e0
    Signed-off-by: Laxminath Kasam <lkasam@codeaurora.org>

--- drivers/soc/qcom/qdsp6v2/msm_audio_ion.c
+++ drivers/soc/qcom/qdsp6v2/msm_audio_ion.c
@@ -477,6 +477,23 @@ static int msm_audio_ion_get_phys(struct ion_client *client,
 	return rc;
 }
 
+
+u32 msm_audio_ion_get_smmu_sid_mode32(void)
+{
+	if (msm_audio_ion_data.smmu_enabled)
+		return upper_32_bits(msm_audio_ion_data.smmu_sid_bits);
+	else
+		return 0;
+}
+
+u32 populate_upper_32_bits(ion_phys_addr_t pa)
+{
+	if (sizeof(ion_phys_addr_t) == sizeof(u32))
+		return msm_audio_ion_get_smmu_sid_mode32();
+	else
+		return upper_32_bits(pa);
+}
+
 static int msm_audio_ion_probe(struct platform_device *pdev)
 {
 	int rc = 0;
--- include/linux/msm_audio_ion.h
+++ include/linux/msm_audio_ion.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -41,5 +41,6 @@ int msm_audio_ion_import_legacy(const char *name, struct ion_client *client,
 			ion_phys_addr_t *paddr, size_t *pa_len, void **vaddr);
 int msm_audio_ion_free_legacy(struct ion_client *client,
 			struct ion_handle *handle);
+u32 populate_upper_32_bits(ion_phys_addr_t pa);
 #endif /* _LINUX_MSM_AUDIO_ION_H */
 
