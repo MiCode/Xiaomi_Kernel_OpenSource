0bba5777adc04175f19ba9fedb40c02c157b5e6e
David Brown <davidb@codeaurora.org>
2015-04-23 14:29:13 -0700

    scripts: Call out python2 explicitly
    Many systems these days have both python2 and python3 installed.  The
    unversioned executable `python` may point to either version.  Per
    https://www.python.org/dev/peps/pep-0394/ , scripts that need Python 2
    should explicitly call this out in the shebang line.
    
    These build scripts are both specific to python 2, so call this out in
    the shebang line.
    
    Change-Id: I11f70c341e484254dc1679db65c2ed5ac61e510f
    Signed-off-by: David Brown <davidb@codeaurora.org>

--- scripts/build-all.py
+++ scripts/build-all.py
@@ -1,6 +1,6 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2
 
-# Copyright (c) 2009-2014, The Linux Foundation. All rights reserved.
+# Copyright (c) 2009-2015, The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -1,7 +1,7 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2011-2014, The Linux Foundation. All rights reserved.
+# Copyright (c) 2011-2015, The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
