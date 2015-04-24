798d4edd436dd9cc4ca618e3f3a31e5a9651ac1e
Vignesh Radhakrishnan <vigneshr@codeaurora.org>
2015-04-24 17:40:05 +0530

    mm: vmstat: make vmstat_work cpu bound during requeue
    vmstat_work is currently a per cpu worker thread that requeues
    itself using schedule_delayed_work().
    
    schedule_delayed_work() makes the worker thread unbound. Since
    its unbound, when the timer for the delayed workqueue is migrated,
    the current code  path  can cause the per cpu worker to get
    executed on the CPU other than what it is intended for causing
    undesired effects. This overrides the choice of making the worker
    per cpu in the first place.
    
    Fix this by using schedule_delayed_work_on() and make it CPU
    bound.
    
    Change-Id: Ib7952c544bda7d8ec0a79c52de8f2d80b11637e8
    Signed-off-by: Vignesh Radhakrishnan <vigneshr@codeaurora.org>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1277,7 +1277,8 @@ int sysctl_stat_interval __read_mostly = HZ;
 static void vmstat_update(struct work_struct *w)
 {
 	refresh_cpu_vm_stats(smp_processor_id());
-	schedule_delayed_work(&__get_cpu_var(vmstat_work),
+	schedule_delayed_work_on(smp_processor_id(),
+		&__get_cpu_var(vmstat_work),
 		round_jiffies_relative(sysctl_stat_interval));
 }
 
