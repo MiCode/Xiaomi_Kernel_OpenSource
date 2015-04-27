d1733c95305b350a9d9015e32907651ed8325dca
Dhaval Patel <pdhaval@codeaurora.org>
2015-04-27 10:37:21 -0700

    clk: msm: mdss: update pll ldo configuration for 8996 v2
    msm8996 v2 pll needs different ldo configuration in DSI
    pll compared to v1 target. This changes updates the DSI
    pll driver to support this new configuration.
    
    Change-Id: Idccfad2e388273a15b45a0e8bb822513fcbbe70e
    Signed-off-by: Dhaval Patel <pdhaval@codeaurora.org>

--- drivers/clk/msm/mdss/mdss-pll.c
+++ drivers/clk/msm/mdss/mdss-pll.c
@@ -129,6 +129,11 @@ static int mdss_pll_resource_parse(struct platform_device *pdev,
 	if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_8996")) {
 		pll_res->pll_interface_type = MDSS_DSI_PLL_8996;
 		pll_res->target_id = MDSS_PLL_TARGET_8996;
+		pll_res->revision = 1;
+	} else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_8996_v2")) {
+		pll_res->pll_interface_type = MDSS_DSI_PLL_8996;
+		pll_res->target_id = MDSS_PLL_TARGET_8996;
+		pll_res->revision = 2;
 	} else if (!strcmp(compatible_stream, "qcom,mdss_hdmi_pll_8996")) {
 		pll_res->pll_interface_type = MDSS_HDMI_PLL_8996;
 	} else {
@@ -334,6 +339,7 @@ static int mdss_pll_remove(struct platform_device *pdev)
 
 static const struct of_device_id mdss_pll_dt_match[] = {
 	{.compatible = "qcom,mdss_dsi_pll_8996"},
+	{.compatible = "qcom,mdss_dsi_pll_8996_v2"},
 	{.compatible = "qcom,mdss_hdmi_pll_8996"},
 	{}
 };
