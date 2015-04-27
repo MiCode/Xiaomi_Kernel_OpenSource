14be4a9d3d8aac07df64f85be20183cab72bfa37
Azhar Shaikh <azhars@codeaurora.org>
2015-04-27 15:09:23 -0700

    usb: dwc3: gadget: Fix PM runtime get/put in case of remote wakeup
    In case of remote wakeup interrupt, dwc3_gadget_wakeup_work()
    does a pm_runtime_get_sync() and dwc3_gadget_wakeup_interrupt()
    does not notify OTG state machine about this state change.
    Due to which OTG will be still in B_SUSPEND state.
    Now when bus suspend happens, since OTG state machine is already in
    B_SUSPEND state, it does not do a pm_runtime_put_autosuspend() and the
    count is not decremented.
    To fix, this do an unconditional pm_runtime_get_sync() and
    pm_runtime_put_noidle() in dwc3_gadget_wakeup_work() to avoid any
    unclocked register access. In dwc3_gadget_wakeup_interrupt() kick in
    SM machine in all cases and handle the remote wakeup same as host
    initiated resume, by clearing the B_SUSPEND bit so that OTG state machine
    moves to B_PERIPHERAL state.
    
    CRs-fixed: 829205
    Change-Id: I270df517d5dd58fe28bf0087e0ba951c4e9e15ba
    Signed-off-by: Azhar Shaikh <azhars@codeaurora.org>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1637,11 +1637,9 @@ static void dwc3_gadget_wakeup_work(struct work_struct *w)
 
 	dwc = container_of(w, struct dwc3, wakeup_work);
 
-	if (atomic_read(&dwc->in_lpm)) {
-		pm_runtime_get_sync(dwc->dev);
-		dbg_event(0xFF, "Gdgwake gsyn",
-			atomic_read(&dwc->dev->power.usage_count));
-	}
+	pm_runtime_get_sync(dwc->dev);
+	dbg_event(0xFF, "Gdgwake gsyn",
+		atomic_read(&dwc->dev->power.usage_count));
 
 	ret = dwc3_gadget_wakeup_int(dwc);
 
@@ -1649,6 +1647,10 @@ static void dwc3_gadget_wakeup_work(struct work_struct *w)
 		pr_err("Remote wakeup failed. ret = %d.\n", ret);
 	else
 		pr_debug("Remote wakeup succeeded.\n");
+
+	pm_runtime_put_noidle(dwc->dev);
+	dbg_event(0xFF, "Gdgwake put",
+		atomic_read(&dwc->dev->power.usage_count));
 }
 
 static int dwc3_gadget_wakeup_int(struct dwc3 *dwc)
@@ -3031,12 +3033,10 @@ static void dwc3_gadget_wakeup_interrupt(struct dwc3 *dwc, bool remote_wakeup)
 		 * In case of remote wake up dwc3_gadget_wakeup_work()
 		 * is doing pm_runtime_get_sync().
 		 */
-		if (!remote_wakeup) {
-			dev_dbg(dwc->dev, "Notify OTG from %s\n", __func__);
-			dwc->b_suspend = false;
-			dwc3_notify_event(dwc,
-					DWC3_CONTROLLER_NOTIFY_OTG_EVENT);
-		}
+		dev_dbg(dwc->dev, "Notify OTG from %s\n", __func__);
+		dwc->b_suspend = false;
+		dwc3_notify_event(dwc,
+				DWC3_CONTROLLER_NOTIFY_OTG_EVENT);
 
 		dwc3_resume_gadget(dwc);
 	}
