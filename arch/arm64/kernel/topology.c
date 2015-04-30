6498ce766cb6ca8f4643a4ab6dfbbb2f1fafca93
Juri Lelli <juri.lelli@arm.com>
2015-04-30 11:53:48 +0100

    arm64: Cpu invariant scheduler load-tracking and capacity support
    Provides the scheduler with a cpu scaling correction factor for more
    accurate load-tracking and cpu capacity handling.
    
    The Energy Model (EM) (in fact the capacity value of the last element
    of the capacity states vector of the core (MC) level sched_group_energy
    structure) is used as the source for this cpu scaling factor.
    
    The cpu capacity value depends on the micro-architecture and the
    maximum frequency of the cpu.
    
    The maximum frequency part should not be confused with the frequency
    invariant scheduler load-tracking support which deals with frequency
    related scaling due to DFVS functionality.
    
    Change-Id: Iddc0f792184ad9d3ad6cb1cfc405ff6e3778d194
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>

--- arch/arm64/kernel/topology.c
+++ arch/arm64/kernel/topology.c
@@ -23,6 +23,18 @@
 #include <asm/cputype.h>
 #include <asm/topology.h>
 
+static DEFINE_PER_CPU(unsigned long, cpu_scale) = SCHED_CAPACITY_SCALE;
+
+unsigned long scale_cpu_capacity(struct sched_domain *sd, int cpu)
+{
+	return per_cpu(cpu_scale, cpu);
+}
+
+static void set_capacity_scale(unsigned int cpu, unsigned long capacity)
+{
+	per_cpu(cpu_scale, cpu) = capacity;
+}
+
 static int __init get_cpu_for_node(struct device_node *node)
 {
 	struct device_node *cpu_node;
@@ -211,6 +223,21 @@ const struct cpumask *cpu_coregroup_mask(int cpu)
 	return &cpu_topology[cpu].core_sibling;
 }
 
+static void update_cpu_capacity(unsigned int cpu)
+{
+	unsigned long capacity = SCHED_CAPACITY_SCALE;
+
+	if (cpu_core_energy(cpu)) {
+		int max_cap_idx = cpu_core_energy(cpu)->nr_cap_states - 1;
+		capacity = cpu_core_energy(cpu)->cap_states[max_cap_idx].cap;
+	}
+
+	set_capacity_scale(cpu, capacity);
+
+	pr_info("CPU%d: update cpu_capacity %lu\n",
+		cpu, arch_scale_cpu_capacity(NULL, cpu));
+}
+
 static void update_siblings_masks(unsigned int cpuid)
 {
 	struct cpu_topology *cpu_topo, *cpuid_topo = &cpu_topology[cpuid];
@@ -269,6 +296,7 @@ void store_cpu_topology(unsigned int cpuid)
 
 topology_populated:
 	update_siblings_masks(cpuid);
+	update_cpu_capacity(cpuid);
 }
 
 static void __init reset_cpu_topology(void)
