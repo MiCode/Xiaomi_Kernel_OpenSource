b82e09ff72fae9390c9fef92ce13a14f628df592
Harsh Shah <harshs@codeaurora.org>
2015-04-29 17:29:52 -0700

    msm: camera: isp: Make AB and IB equal for 8992
    As per request from dcvs and ddr team, make AB=IB for 8992.
    
    Change-Id: I7ed8cda461bd295602acab20e8a69798da68aaeb
    Signed-off-by: Harsh Shah <harshs@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -1379,8 +1379,9 @@ static int msm_isp_update_stream_bandwidth(struct vfe_device *vfe_dev)
 	}
 	total_bandwidth = total_pix_bandwidth + total_rdi_bandwidth;
 	rc = msm_isp_update_bandwidth(ISP_VFE0 + vfe_dev->pdev->id,
-		total_bandwidth,  total_bandwidth *
-		ISP_BUS_UTILIZATION_FACTOR / ISP_Q2);
+		(total_bandwidth + MSM_ISP_MIN_AB),
+		(total_bandwidth + MSM_ISP_MIN_IB));
+
 	if (rc < 0)
 		pr_err("%s: update failed\n", __func__);
 
