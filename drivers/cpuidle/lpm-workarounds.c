918cc81461f17f69cbcdcaf04a4b563b5ffaf9aa
Anji Jonnala <anjir@codeaurora.org>
2015-04-30 17:09:28 +0530

    drivers: lpm-workarounds: Enable L1/L2 clock gating WA
    L1/L2 dynamic clock gating feature gates the L1/L2 clock
    automatically which saves dynamic power. Core and L2 should be
    quiescent state while enabling L1/L2 clock gating, hence early boot
    code can't enable this for perf core0 and L2.
    Implement a WA which will get called when perf cluster is in
    low power mode to enable L1/L2 clock gating for perf core0 and L2.
    
    Change-Id: Ia268d90a306770a4089370f83e3c9101c2661f1c
    Signed-off-by: Anji Jonnala <anjir@codeaurora.org>
    Signed-off-by: Sridhar Gujje <sgujje@codeaurora.org>

--- drivers/cpuidle/lpm-workarounds.c
+++ drivers/cpuidle/lpm-workarounds.c
@@ -13,45 +13,124 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
 #include <linux/of.h>
 #include <linux/err.h>
+#include <linux/io.h>
 #include <linux/device.h>
 #include <linux/platform_device.h>
 #include <linux/regulator/consumer.h>
 #include <linux/regulator/rpm-smd-regulator.h>
+#include <linux/msm_thermal.h>
+#include <linux/delay.h>
+#include <linux/cpu.h>
+#include <soc/qcom/scm.h>
 
-static struct regulator *lpm_cx_reg;
-static struct work_struct dummy_vote_work;
+#define L2_HS_STS_SET	0x200
+
+static struct work_struct lpm_wa_work;
 static struct workqueue_struct *lpm_wa_wq;
-static bool lpm_wa_cx_turbo_unvote;
 static bool skip_l2_spm;
+static bool enable_dynamic_clock_gating;
+static bool is_l1_l2_gcc_secure;
+static bool store_clock_gating;
+int non_boot_cpu_index;
+void __iomem *l2_pwr_sts;
+void __iomem *l1_l2_gcc;
+struct device_clnt_data      *hotplug_handle;
+union device_request curr_req;
+cpumask_t l1_l2_offline_mask;
+cpumask_t offline_mask;
+struct resource *l1_l2_gcc_res;
+uint32_t l2_status = -1;
 
-/* While exiting from RPM assisted power collapse on some targets like MSM8939
- * the CX is bumped to turbo mode by RPM. To reduce the power impact, APSS
- * low power driver need to remove the CX turbo vote.
- */
-static void send_dummy_cx_vote(struct work_struct *w)
+static int lpm_wa_callback(struct notifier_block *cpu_nb,
+	unsigned long action, void *hcpu)
 {
-	if (lpm_cx_reg) {
-		regulator_set_voltage(lpm_cx_reg,
-			RPM_REGULATOR_CORNER_SUPER_TURBO,
-			RPM_REGULATOR_CORNER_SUPER_TURBO);
+	unsigned long cpu = (unsigned long) hcpu;
 
-		regulator_set_voltage(lpm_cx_reg,
-			RPM_REGULATOR_CORNER_NONE,
-			RPM_REGULATOR_CORNER_SUPER_TURBO);
+	if ((action != CPU_POST_DEAD) && (action != CPU_ONLINE))
+		return NOTIFY_OK;
+
+	if (cpu >= non_boot_cpu_index)
+		return NOTIFY_OK;
+
+
+	switch (action & ~CPU_TASKS_FROZEN) {
+	case CPU_POST_DEAD:
+		cpumask_set_cpu(cpu, &offline_mask);
+		break;
+	case CPU_ONLINE:
+		cpumask_clear_cpu(cpu, &offline_mask);
+		break;
+	default:
+		break;
+	}
+
+	if (cpumask_equal(&offline_mask, &l1_l2_offline_mask)
+					&& store_clock_gating)
+		queue_work(lpm_wa_wq, &lpm_wa_work);
+
+	return NOTIFY_OK;
+}
+
+static struct notifier_block __refdata lpm_wa_nblk = {
+	.notifier_call = lpm_wa_callback,
+};
+
+static void process_lpm_workarounds(struct work_struct *w)
+{
+	int ret = 0, status = 0;
+
+
+	/* MSM8952 have L1/L2 dynamic clock gating disabled in HW for
+	 * performance cluster cores. Enable it via SW to reduce power
+	 * impact.
+	 */
+	if (enable_dynamic_clock_gating) {
+
+		/* Skip enabling L1/L2 clock gating if perf l2 is not in low
+		 * power mode.
+		 */
+
+		status = (__raw_readl(l2_pwr_sts) & L2_HS_STS_SET)
+							== L2_HS_STS_SET;
+		if (status) {
+			pr_err("%s: perf L2 is not in low power mode\n",
+								__func__);
+			queue_work(lpm_wa_wq, &lpm_wa_work);
+			return;
+		}
+
+		l2_status = __raw_readl(l2_pwr_sts);
+
+		pr_err("Set L1_L2_GCC from cpu%d when perf L2 status=0x%x\n",
+			smp_processor_id(), l2_status);
+
+		if (is_l1_l2_gcc_secure) {
+			scm_io_write((u32)(l1_l2_gcc_res->start), 0x0);
+			if (scm_io_read((u32)(l1_l2_gcc_res->start)) !=0)
+				pr_err("Failed to set L1_L2_GCC\n");
+		}
+		else {
+			__raw_writel(0x0, l1_l2_gcc);
+			if (__raw_readl(l1_l2_gcc) != 0x0)
+				pr_err("Failed to set L1_L2_GCC\n");
+		}
+
+		HOTPLUG_NO_MITIGATION(&curr_req.offline_mask);
+		ret = devmgr_client_request_mitigation(
+				hotplug_handle,
+				HOTPLUG_MITIGATION_REQ,
+				&curr_req);
+		if (ret) {
+			pr_err("hotplug request failed. err:%d\n", ret);
+			return;
+		}
+		enable_dynamic_clock_gating = false;
+		unregister_hotcpu_notifier(&lpm_wa_nblk);
 	}
 }
-
-/*
- * lpm_wa_cx_unvote_send(): Unvote for CX turbo mode
- */
-void lpm_wa_cx_unvote_send(void)
-{
-	if (lpm_wa_cx_turbo_unvote)
-		queue_work(lpm_wa_wq, &dummy_vote_work);
-}
-EXPORT_SYMBOL(lpm_wa_cx_unvote_send);
 
 /*
  * lpm_wa_skip_l2_spm: Dont program the l2 SPM as TZ is programming the
@@ -63,60 +142,160 @@ bool lpm_wa_get_skip_l2_spm(void)
 }
 EXPORT_SYMBOL(lpm_wa_get_skip_l2_spm);
 
-
-static int lpm_wa_cx_unvote_init(struct platform_device *pdev)
+static ssize_t store_clock_gating_enabled(struct kobject *kobj,
+		struct kobj_attribute *attr, const char *buf, size_t count)
 {
-	int ret = 0;
+	int ret = 0, val = 0;
 
-	lpm_cx_reg = devm_regulator_get(&pdev->dev, "lpm-cx");
-	if (IS_ERR(lpm_cx_reg)) {
-		ret = PTR_ERR(lpm_cx_reg);
-		if (ret != -EPROBE_DEFER)
-			pr_err("Unable to get the CX regulator\n");
-		return ret;
+	ret = kstrtoint(buf, 10, &val);
+	if (ret) {
+		pr_err("Invalid input%s %s. err:%d\n", __func__, buf, ret);
+		return count;
+	}
+	cpumask_copy(&curr_req.offline_mask, &l1_l2_offline_mask);
+	ret = devmgr_client_request_mitigation(
+			hotplug_handle,
+			HOTPLUG_MITIGATION_REQ,
+			&curr_req);
+	if (ret) {
+		pr_err("hotplug request failed. err:%d\n", ret);
+		return count;
 	}
 
-	INIT_WORK(&dummy_vote_work, send_dummy_cx_vote);
-
-	lpm_wa_wq = alloc_workqueue("lpm-wa",
-				WQ_UNBOUND | WQ_MEM_RECLAIM | WQ_HIGHPRI, 1);
-
-	return ret;
+	store_clock_gating = true;
+	return count;
 }
 
-static int lpm_wa_cx_unvote_exit(void)
-{
-	if (lpm_wa_wq)
-		destroy_workqueue(lpm_wa_wq);
-
-	return 0;
-}
+static struct kobj_attribute clock_gating_enabled_attr =
+__ATTR(dynamic_clock_gating, 0644, NULL, store_clock_gating_enabled);
 
 static int lpm_wa_probe(struct platform_device *pdev)
 {
-	int ret = 0;
-
-	lpm_wa_cx_turbo_unvote = of_property_read_bool(pdev->dev.of_node,
-					"qcom,lpm-wa-cx-turbo-unvote");
-	if (lpm_wa_cx_turbo_unvote) {
-		ret = lpm_wa_cx_unvote_init(pdev);
-		if (ret) {
-			pr_err("%s: Failed to initialize lpm_wa_cx_unvote (%d)\n",
-				__func__, ret);
-			return ret;
-		}
-	}
+	int ret = 0, c = 0, cpu = 0;
+	struct kobject *module_kobj = NULL;
+	struct kobject *module_lpm_wa = NULL;
+	struct resource *res = NULL;
+	unsigned long cpu_mask = 0;
+	char *key;
 
 	skip_l2_spm = of_property_read_bool(pdev->dev.of_node,
 					"qcom,lpm-wa-skip-l2-spm");
+
+	/*
+	 * Enabling L1/L2 tag ram clock gating requires core and L2 to be
+	 * in quiescent state. lpm-wa-dynamic-clock-gating flag specifies
+	 * WA implementation in SW for perf core0 and L2.
+	 */
+	enable_dynamic_clock_gating = of_property_read_bool(pdev->dev.of_node,
+					"qcom,lpm-wa-dynamic-clock-gating");
+	if (!enable_dynamic_clock_gating)
+		return ret;
+	is_l1_l2_gcc_secure = of_property_read_bool(pdev->dev.of_node,
+					"qcom,l1_l2_gcc_secure");
+
+	res = platform_get_resource_byname(pdev, IORESOURCE_MEM,
+							"l2-pwr-sts");
+	if (!res) {
+		pr_err("%s(): Missing perf l2-pwr-sts resource\n",
+							 __func__);
+		return -EINVAL;
+	}
+
+	l2_pwr_sts = devm_ioremap(&pdev->dev, res->start,
+						resource_size(res));
+	if (!l2_pwr_sts) {
+		pr_err("%s: cannot ioremap l2_pwr_status %s\n",	__func__,
+							KBUILD_MODNAME);
+		return -ENOMEM;
+	}
+
+	res = platform_get_resource_byname(pdev, IORESOURCE_MEM,
+							"l1-l2-gcc");
+	if (!res) {
+		pr_err("%s(): Missing L1/L2 GCC override resource\n",
+								__func__);
+		return -EINVAL;
+	}
+	l1_l2_gcc_res = res;
+
+	l1_l2_gcc = devm_ioremap(&pdev->dev, res->start,
+						resource_size(res));
+	if (!l1_l2_gcc) {
+		pr_err("%s: cannot ioremap L1/L2 GCC override %s\n",
+						__func__, KBUILD_MODNAME);
+		return -ENOMEM;
+	}
+
+	key = "qcom,non-boot-cpu-index";
+	ret = of_property_read_u32(pdev->dev.of_node, key,
+						&non_boot_cpu_index);
+	if (!ret) {
+		pr_err("%s: Missing qcom,non_boot_cpu_index property\n"
+							, __func__);
+	}
+
+	key = "qcom,cpu-offline-mask";
+	ret = of_property_read_u32(pdev->dev.of_node, key,
+						(u32 *) &cpu_mask);
+	if (!ret) {
+		for_each_set_bit(c, &cpu_mask, num_possible_cpus()) {
+			cpumask_set_cpu(c, &l1_l2_offline_mask);
+		}
+	}
+
+	module_kobj = kset_find_obj(module_kset, "lpm_levels");
+	if (!module_kobj) {
+		pr_err("%s: cannot find kobject for module lpm_levels\n",
+							__func__);
+		ret = -ENOENT;
+	}
+
+	module_lpm_wa = kobject_create_and_add("lpm_workarounds",
+							module_kobj);
+	if (!module_lpm_wa) {
+		pr_err("%s: cannot create kobject for module %s\n",
+			__func__, KBUILD_MODNAME);
+		ret = -ENOENT;
+	}
+
+	ret = sysfs_create_file(module_lpm_wa,
+					&clock_gating_enabled_attr.attr);
+	if (ret) {
+		pr_err("cannot create attr group. err:%d\n", ret);
+		return ret;
+	}
+
+	hotplug_handle = devmgr_register_mitigation_client(&pdev->dev,
+						HOTPLUG_DEVICE,	NULL);
+	if (IS_ERR_OR_NULL(hotplug_handle)) {
+		ret = PTR_ERR(hotplug_handle);
+		pr_err("Error registering for hotplug. ret:%d\n", ret);
+		return ret;
+	}
+
+	INIT_WORK(&lpm_wa_work, process_lpm_workarounds);
+
+	lpm_wa_wq = alloc_workqueue("lpm-wa",
+			WQ_UNBOUND | WQ_MEM_RECLAIM | WQ_HIGHPRI, 1);
+	if (IS_ERR_OR_NULL(lpm_wa_wq)) {
+		ret = PTR_ERR(lpm_wa_wq);
+		pr_err("%s: Failed to allocate workqueue\n", __func__);
+		return ret;
+	}
+
+	for_each_present_cpu(cpu) {
+		if (cpu < non_boot_cpu_index) {
+			if (!cpumask_test_cpu(cpu, cpu_online_mask))
+				cpumask_set_cpu(cpu, &offline_mask);
+		}
+	}
+	register_hotcpu_notifier(&lpm_wa_nblk);
 	return ret;
 }
 
 static int lpm_wa_remove(struct platform_device *pdev)
 {
 	int ret = 0;
-	if (lpm_wa_cx_turbo_unvote)
-		ret = lpm_wa_cx_unvote_exit();
 
 	return ret;
 }
