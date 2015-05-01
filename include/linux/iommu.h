86d54e985a1a495377aab070438aa33bc435b1c9
Neeti Desai <neetid@codeaurora.org>
2015-04-30 19:11:44 -0700

    iommu/arm-smmu: change IOMMU_EXEC to IOMMU_NOEXEC
    Exposing the XN flag of the SMMU driver as IOMMU_NOEXEC instead of
    IOMMU_EXEC makes it enforceable, since for IOMMUs that don't support
    the XN flag pages will always be executable.
    
    Change-Id: Ib785acd8a188fa95aea9991116139a392862764e
    Signed-off-by: Antonios Motakis <a.motakis@virtualopensystems.com>
    Acked-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Git-commit: a720b41c41f5a7e4c51558cf087882c57331581f
    [rvaswani@codeaurora.org: resolve trivial merge conflicts]
    Signed-off-by: Rohit Vaswani <rvaswani@codeaurora.org>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -27,7 +27,7 @@
 #define IOMMU_READ	(1 << 0)
 #define IOMMU_WRITE	(1 << 1)
 #define IOMMU_CACHE	(1 << 2) /* DMA cache coherency */
-#define IOMMU_EXEC	(1 << 3)
+#define IOMMU_NOEXEC	(1 << 3)
 #define IOMMU_PRIV	(1 << 4)
 #define IOMMU_DEVICE	(1 << 5) /* Indicates access to device memory */
 
