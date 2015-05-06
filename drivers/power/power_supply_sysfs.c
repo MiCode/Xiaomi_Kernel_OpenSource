e5ac1f46aa31566f37e64b3a4e270a6dbbda4122
Xiaozhe Shi <xiaozhes@codeaurora.org>
2015-05-06 16:06:04 -0700

    power_supply: add FORCE_TLIM property
    Add POWER_SUPPLY_PROP_FORCE_TLIM as a power supply property in order to
    force on the charger therm circuitry.
    
    CRs-Fixed: 793985
    Change-Id: If62f2eadf25c1c6a96314e05ab9c03bb0d700c14
    Signed-off-by: Xiaozhe Shi <xiaozhes@codeaurora.org>
    Signed-off-by: Subbaraman Narayanamurthy <subbaram@codeaurora.org>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -222,8 +222,9 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(esr_count),
 	POWER_SUPPLY_ATTR(safety_timer_enabled),
 	POWER_SUPPLY_ATTR(charge_done),
-	POWER_SUPPLY_ATTR(flash_active),
 	POWER_SUPPLY_ATTR(allow_detection),
+	POWER_SUPPLY_ATTR(flash_active),
+	POWER_SUPPLY_ATTR(force_tlim),
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_ATTR(charge_counter_ext),
 	/* Properties of type `const char *' */
