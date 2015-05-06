b6eea41d493c5f6c106532d912d53b280421ad0a
Rama Krishna Phani A <rphani@codeaurora.org>
2015-05-06 15:32:58 +0530

    Revert "msm: sps: Support low memory condition in SPS driver"
    In low memory conditions, BAM DMUX is getting time out while
    allocating memory through kzalloc. Atomic flag is then used with
    kzalloc to avoid delay during memory allocation. However, this
    method results in memory allocation failure instead of timeout in
    BAM DMUX. Thus, the change here reverts commit
    1ac9b940a1e33a8cba64d4d5932944c752af154c.
    
    Change-Id: I64257d30f5f694a212a43ff2ef02d4529b894942
    Signed-off-by: Rama Krishna Phani A <rphani@codeaurora.org>

--- include/linux/msm-sps.h
+++ include/linux/msm-sps.h
@@ -113,8 +113,6 @@
 #define SPS_BAM_SMMU_EN             (1UL << 9)
 /* Confirm resource status before access BAM*/
 #define SPS_BAM_RES_CONFIRM         (1UL << 7)
-/* Verify Low Memory status */
-#define SPS_BAM_ATMC_MEM            (1UL << 8)
 
 /* BAM device management flags */
 
