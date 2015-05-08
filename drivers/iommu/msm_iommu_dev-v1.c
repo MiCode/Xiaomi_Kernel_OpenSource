8b1a0f7c3e4ca5e322df905a587bb5a7b155c5d0
Chintan Pandya <cpandya@codeaurora.org>
2015-05-08 19:21:23 +0530

    iommu: msm: allow CBs to report an error when faults
    Some clients of IOMMU treats the CB fault as non-recoverable
    and they may want to trap the fault for debug purpose. Provide
    that provision via context bank DT property.
    
    Change-Id: Icb9cb67ed3dac44e144fcd7bc85deca833bf941c
    Signed-off-by: Chintan Pandya <cpandya@codeaurora.org>

--- drivers/iommu/msm_iommu_dev-v1.c
+++ drivers/iommu/msm_iommu_dev-v1.c
@@ -591,6 +591,10 @@ static int msm_iommu_ctx_parse_dt(struct platform_device *pdev,
 					&ctx_drvdata->name))
 		ctx_drvdata->name = dev_name(&pdev->dev);
 
+	ctx_drvdata->report_error_on_fault =
+		of_property_read_bool(pdev->dev.of_node,
+				"qcom,report-error-on-fault");
+
 	if (!of_get_property(pdev->dev.of_node, "qcom,iommu-ctx-sids", &nsid)) {
 		ret = -EINVAL;
 		goto out;
