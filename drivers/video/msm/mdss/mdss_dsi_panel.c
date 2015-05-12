c69971928d01c6a92f52c549444d86f6fe15fbe2
Prashant Nukala <pnukala@codeaurora.org>
2015-05-12 13:06:33 +0530

    msm: mdss: change default value of minimum refresh rate to 48
    Most of the DSI panels do not support refresh rate lesser than 48.
    So change the default value to 48.
    
    Change-Id: Id47fe3488d7cd85b1eefaea18f3a7b9e6e8b3e31
    Signed-off-by: Prashant Nukala <pnukala@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -25,7 +25,7 @@
 #include "mdss_dsi.h"
 
 #define DT_CMD_HDR 6
-#define MIN_REFRESH_RATE 30
+#define MIN_REFRESH_RATE 48
 #define DEFAULT_MDP_TRANSFER_TIME 14000
 
 DEFINE_LED_TRIGGER(bl_led_trigger);
