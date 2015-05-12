a4f3d9480c659793fc79fdbc03bbd8e3c3ec2d99
Rajesh Bondugula <rajeshb@codeaurora.org>
2015-05-11 17:45:18 -0700

    msm: camera: sensor: Remove is_flash_supported flag
    Remove is_flash_supported flag. This flag is redundant
    existence of flash is cheked with flash_name variable.
    
    Change-Id: I91c34cd3b78be225f751a47356ad7f0dc1db9bdf
    Signed-off-by: Rajesh Bondugula <rajeshb@codeaurora.org>
    Signed-off-by: Xu Han <hanxu@codeaurora.org>

--- include/media/msm_camsensor_sdk.h
+++ include/media/msm_camsensor_sdk.h
@@ -241,7 +241,6 @@ struct msm_camera_sensor_slave_info {
 	struct msm_sensor_power_setting_array power_setting_array;
 	unsigned char  is_init_params_valid;
 	struct msm_sensor_init_params sensor_init_params;
-	unsigned char is_flash_supported;
 	enum msm_sensor_output_format_t output_format;
 };
 
