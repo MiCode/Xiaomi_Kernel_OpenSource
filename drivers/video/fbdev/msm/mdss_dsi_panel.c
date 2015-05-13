8303b9eb194d7fc7bda6dee67444fe9a9e88a9a3
Ajay Singh Parmar <aparmar@codeaurora.org>
2015-05-12 22:19:25 -0700

    msm: adv7533: register with dba (Display Bridge Abstract)
    Register ADV7533 driver with DBA (Display Bridge-chip Abstraction)
    module so that interface drivers can utilize functionality exposed
    by this driver.
    
    Change-Id: Ife0b94acfc5d1c8108ffff8a320228224ab29eaf
    Signed-off-by: Ajay Singh Parmar <aparmar@codeaurora.org>
    Signed-off-by: Siddharth Zaveri <szaveri@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -678,6 +678,7 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 	struct mdss_dsi_ctrl_pdata *ctrl = NULL;
 	struct mdss_panel_info *pinfo;
 	struct dsi_panel_cmds *on_cmds;
+	int ret = 0;
 
 	if (pdata == NULL) {
 		pr_err("%s: Invalid input data\n", __func__);
@@ -710,10 +711,17 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 	if (pinfo->compression_mode == COMPRESSION_DSC)
 		mdss_dsi_panel_dsc_pps_send(ctrl);
 
+	if (ctrl->ds_registered) {
+		if (ctrl->dba_ops.video_on)
+			ret = ctrl->dba_ops.video_on(
+				ctrl->dba_data, true,
+				&ctrl->dba_video_cfg, 0);
+	}
+
 end:
 	pinfo->blank_state = MDSS_PANEL_BLANK_UNBLANK;
 	pr_debug("%s:-\n", __func__);
-	return 0;
+	return ret;
 }
 
 static int mdss_dsi_post_panel_on(struct mdss_panel_data *pdata)
