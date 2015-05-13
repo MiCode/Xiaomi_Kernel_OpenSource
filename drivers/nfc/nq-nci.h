7620346454865b81d7086167d531aea7bb716926
Puneet Mishra <puneetm@codeaurora.org>
2015-05-13 18:32:49 +0100

    NFC: change reset and read flow
    Change reset flow using the power ioctl function. Rename the
    dis-gpio pin to en-gpio in order to describe the funtionality.
    Also change the read flow. No need for multiple i2c reads.
    
    Change-Id: Ide2b5dafe8cad643c018aef3200590cb0d5ba97b
    Acked-by: Afroditi Ilioudi <ailioudi@qti.qualcomm.com>
    Signed-off-by: Puneet Mishra <puneetm@codeaurora.org>

--- drivers/nfc/nq-nci.h
+++ drivers/nfc/nq-nci.h
@@ -25,6 +25,7 @@
 
 #define NFC_SET_PWR	_IOW(0xE9, 0x01, unsigned int)
 #define SET_RX_BLOCK	_IOW(0xE9, 0x04, unsigned int)
+#define NFC_CLK_REQ	_IOW(0xE9, 0x02, unsigned int)
 #define SET_EMULATOR_TEST_POINT		_IOW(0xE9, 0x05, unsigned int)
 #define NFCC_INITIAL_CORE_RESET_NTF	_IOW(0xE9, 0x10, unsigned int)
 
