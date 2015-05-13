d1d2e494043c8394d09c987bf9dd6441e160292a
Faouaz TENOUTIT <faouaz.tenoutit@intel.com>
2015-05-13 02:33:35 -0700

    acpi_platform: Remove PNPC040
    This is a PnP device managed by soc_button driver, so remove it from
    the plafom ACPI devices list.
    
    Signed-off-by: Faouaz TENOUTIT <faouaz.tenoutit@intel.com>

--- drivers/acpi/acpi_platform.c
+++ drivers/acpi/acpi_platform.c
@@ -35,7 +35,6 @@ static const struct acpi_device_id acpi_platform_device_ids[] = {
 	{ "BCM2E1A" },
 	{ "BCM2E39" },
 	{ "BCM2E3D" },
-	{ "PNP0C40" }, /* GPIO buttons device */
 
 	/* Intel Smart Sound Technology */
 	{ "INT33C8" },
