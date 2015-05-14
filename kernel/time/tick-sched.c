e90c6c690585b5dcbc97a9bd9b23b11737e71e6a
Sudarshan N Ramachandra <sudarshan.n.ramachandra@intel.com>
2015-05-14 11:58:15 +0530

    Revert Revert "cpuidle: Quickly notice prediction failure for repeat mode"
    This patch reintroduces the commit 69a37beabf1f0a6705c08e879bdd5d82ff6486c4
    Whose author is Youquan Song <youquan.song@intel.com>
    
    Without the logic to reevaluate C state prediction when
    a shallow C state is picked, The cores end up being in a
    shallow state for long time periods which lasts ~4 seconds.
    This in turn prevents the platform from entering S0ix during that
    time.
    
    Change-Id: I062609a1dea65c6fa5df8f4675d2a069dafdf863
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-36340
    Signed-off-by: Sudarshan N Ramachandra <sudarshan.n.ramachandra@intel.com>
    Reviewed-on: https://android.intel.com:443/366067

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -820,8 +820,14 @@ void tick_nohz_irq_exit(void)
 {
 	struct tick_sched *ts = &__get_cpu_var(tick_cpu_sched);
 
-	if (ts->inidle)
+	if (ts->inidle) {
+		/*
+		 * Cancel the timer because CPU already waken up from
+		 * the C-states.
+		 */
+		menu_hrtimer_cancel();
 		__tick_nohz_idle_enter(ts);
+	}
 	else
 		tick_nohz_full_stop_tick(ts);
 }
@@ -920,6 +926,8 @@ void tick_nohz_idle_exit(void)
 
 	ts->inidle = 0;
 
+	/* Cancel the timer because CPU already waken up from the C-states*/
+	menu_hrtimer_cancel();
 	if (ts->idle_active || ts->tick_stopped)
 		now = ktime_get();
 
