1a46de947d556b74d4596d990370fa9321cb309e
Jeff Bernard <jbernard@codeaurora.org>
2015-05-15 14:37:21 -0400

    msm: thermal: Pass correct size of voltage table to IOCTL
    Fix issue in msm_thermal_process_voltage_table_request.
    For voltage tables bigger than 16 it is not correclty
    passing the partial table size to user space.
    The full size is passed instead of the partial size.
    
    For example if reading a table of 18 values the
    first read returns 16, and the partial read returns 18
    but it should return 2.
    
    Signed-off-by: Jeff Bernard <jbernard@codeaurora.org>
    Change-Id: I75943e94341388cca772ee45bc1275fb5d2091d2

--- drivers/thermal/msm_thermal-dev.c
+++ drivers/thermal/msm_thermal-dev.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -261,6 +261,7 @@ static long msm_thermal_process_voltage_table_req(
 		voltage->voltage_table[idx] =
 			voltage_table_ptr[cluster_id][table_idx];
 	}
+	voltage->voltage_table_len = idx;
 
 copy_and_return:
 	ret = copy_to_user((void __user *)(*arg), query,
