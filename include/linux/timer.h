cda7669adec3229679020ea283224afbaabc4014
Prasad Sodagudi <psodagud@codeaurora.org>
2015-05-16 10:01:38 +0530

    sched: Add a check for cpu unbound deferrable timers
    Add a check for cpu unbound deferrable timer expiry and raise
    softirq for handling the expired timers so that the CPU can
    process the cpu unbound deferrable times as early as possible
    when a cpu tries to enter/exit idle loop.
    
    Change-Id: Ieffa74fa22a4d25493f5590b5ac1e0d784fcbbad
    Signed-off-by: Prasad Sodagudi <psodagud@codeaurora.org>

--- include/linux/timer.h
+++ include/linux/timer.h
@@ -176,6 +176,9 @@ extern int del_timer(struct timer_list * timer);
 extern int mod_timer(struct timer_list *timer, unsigned long expires);
 extern int mod_timer_pending(struct timer_list *timer, unsigned long expires);
 extern int mod_timer_pinned(struct timer_list *timer, unsigned long expires);
+#ifdef CONFIG_SMP
+extern bool check_pending_deferrable_timers(int cpu);
+#endif
 
 extern void set_timer_slack(struct timer_list *time, int slack_hz);
 
