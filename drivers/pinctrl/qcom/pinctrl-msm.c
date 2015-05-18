22b8b6608174c1308208d5bc6c143f4998744547
Patrick Daly <pdaly@codeaurora.org>
2015-05-18 14:52:47 -0700

    pinctrl: qcom: Fix bug in iteration through functions
    Fix iteration beyond array bounds when looping through
    the pin functions
    
    Change-Id: I7e88eed814364062fd93daf03f24ccd4baabf125
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>
    Signed-off-by: Hanumant Singh <hanumant@codeaurora.org>

--- drivers/pinctrl/qcom/pinctrl-msm.c
+++ drivers/pinctrl/qcom/pinctrl-msm.c
@@ -895,7 +895,7 @@ static void msm_pinctrl_setup_pm_reset(struct msm_pinctrl *pctrl)
 	int i = 0;
 	const struct msm_function *func = pctrl->soc->functions;
 
-	for (; i <= pctrl->soc->nfunctions; i++)
+	for (; i < pctrl->soc->nfunctions; i++)
 		if (!strcmp(func[i].name, "ps_hold")) {
 			pctrl->restart_nb.notifier_call = msm_ps_hold_restart;
 			pctrl->restart_nb.priority = 128;
