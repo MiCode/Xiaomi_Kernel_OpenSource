7a769239c7f5dc88b295504faea975014c02be1e
Kishan Kumar <kishank@codeaurora.org>
2015-05-18 19:41:49 +0530

    soc: qcom: watchdog_v2: use kmemleak_not_leak for cpu context dump data
    cpu_data & cpu_buf are virtual addresses,which are
    converted to their physical addresses by using
    virt_to_phys() API. Then this is added to the mem dump
    apps data table by calling msm_dump_table_register().
    This memory is not intended to be freed. When kmemleak
    scans, it can't deduce that the reference we are
    holding is via physical address and not virtual address.
    Hence, kmemleak will treat this as a leak as it can't
    find a reference to this variable. Supress this by
    using kmemleak_not_leak.
    
    Change-Id: I4ee30c87a29d66f34a5c8a6c18f2ebc59760e981
    Signed-off-by: Kishan Kumar <kishank@codeaurora.org>

--- drivers/soc/qcom/watchdog_v2.c
+++ drivers/soc/qcom/watchdog_v2.c
@@ -29,6 +29,7 @@
 #include <soc/qcom/scm.h>
 #include <soc/qcom/memory_dump.h>
 #include <soc/qcom/watchdog.h>
+#include <linux/kmemleak.h>
 
 #define MODULE_NAME "msm_watchdog"
 #define WDT0_ACCSCSSNBARK_INT 0
@@ -482,13 +483,14 @@ static void configure_bark_dump(struct msm_watchdog_data *wdog_dd)
 			pr_err("cpu dump data structure allocation failed\n");
 			goto out0;
 		}
+		kmemleak_not_leak(cpu_data);
 		cpu_buf = kzalloc(MAX_CPU_CTX_SIZE * num_present_cpus(),
 				  GFP_KERNEL);
 		if (!cpu_buf) {
 			pr_err("cpu reg context space allocation failed\n");
 			goto out1;
 		}
-
+		kmemleak_not_leak(cpu_buf);
 		for_each_cpu(cpu, cpu_present_mask) {
 			cpu_data[cpu].addr = virt_to_phys(cpu_buf +
 							cpu * MAX_CPU_CTX_SIZE);
