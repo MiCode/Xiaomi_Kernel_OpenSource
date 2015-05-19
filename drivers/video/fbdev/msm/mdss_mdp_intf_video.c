5760ffaa492d5110e03612346a04a9b5bfc029aa
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2015-05-19 12:27:03 +0530

    msm: mdss: avoid check for MDP line count during fps update
    On latest MDP revisions where there is support for double
    buffering of timing generator registers, it is not needed to
    check for MDP line count while updating the fps. Add check for
    this.
    
    Change-Id: I4b722fb53f856f289e145d0369f1823ff215ab06
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_mdp_intf_video.c
+++ drivers/video/fbdev/msm/mdss_mdp_intf_video.c
@@ -967,9 +967,11 @@ static int mdss_mdp_video_config_fps(struct mdss_mdp_ctl *ctl,
 			mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_ON);
 			spin_lock_irqsave(&ctx->dfps_lock, flags);
 
-			rc = mdss_mdp_video_dfps_check_line_cnt(ctl);
-			if (rc < 0)
-				goto exit_dfps;
+			if (mdata->mdp_rev < MDSS_MDP_HW_REV_105) {
+				rc = mdss_mdp_video_dfps_check_line_cnt(ctl);
+				if (rc < 0)
+					goto exit_dfps;
+			}
 
 			rc = mdss_mdp_video_fps_update(ctx, pdata, new_fps);
 			if (rc < 0) {
