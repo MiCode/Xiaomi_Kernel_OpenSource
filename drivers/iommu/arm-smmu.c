a6d990e2fd7286ae8473306790a75d1503309bdf
Mitchel Humpherys <mitchelh@codeaurora.org>
2015-05-18 21:16:10 -0700

    Revert "iommu/arm-smmu: change IOMMU_EXEC to IOMMU_NOEXEC"
    This reverts commit 433a4b6a37c8f96295a7e80cb603c2318872c1f2.  An
    equivalent patch is being brought in with the upcoming page table
    refactoring, so rip this guy out in preparation.
    
    This also reverts commit af3e3c36d08077856640b28d68c9f99d2188a000, which
    introduced users of the IOMMU_NOEXEC flag.
    
    Change-Id: Ibb36f60f2148170d8a01632043bf36b5f8c60777
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>
    [pdaly@codeaurora.org Resolve minor conflicts]
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -1648,7 +1648,7 @@ static int arm_smmu_alloc_init_pte(struct arm_smmu_domain *smmu_domain,
 				   unsigned long pfn, int prot, int stage)
 {
 	pte_t *pte, *start;
-	pteval_t pteval = ARM_SMMU_PTE_PAGE | ARM_SMMU_PTE_AF;
+	pteval_t pteval = ARM_SMMU_PTE_PAGE | ARM_SMMU_PTE_AF | ARM_SMMU_PTE_XN;
 	int ret;
 
 	if (pmd_none(*pmd)) {
@@ -1698,11 +1698,10 @@ static int arm_smmu_alloc_init_pte(struct arm_smmu_domain *smmu_domain,
 			pteval |= ARM_SMMU_PTE_MEMATTR_DEV;
 	}
 
-	if (prot & IOMMU_NOEXEC)
-		pteval |= ARM_SMMU_PTE_XN;
-
 	/* If no access, create a faulting entry to avoid TLB fills */
-	if (!(prot & (IOMMU_READ | IOMMU_WRITE)))
+	if (prot & IOMMU_EXEC)
+		pteval &= ~ARM_SMMU_PTE_XN;
+	else if (!(prot & (IOMMU_READ | IOMMU_WRITE)))
 		pteval &= ~ARM_SMMU_PTE_PAGE;
 
 	pteval |= ARM_SMMU_PTE_SH_IS;
