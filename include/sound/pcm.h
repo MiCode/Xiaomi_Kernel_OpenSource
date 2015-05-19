32c2ea1ffe2dd3a187d545e63c8d18fd99c9d593
Kenneth Westfield <kwestfie@codeaurora.org>
2015-05-19 12:03:55 -0700

    ASoC: pcm: Add delay_blk feature
    Add delay_blk() to pcm, platform and DAI ops.
    The pcm delay_blk() op collects the audio DSP path
    delays from the low-level drivers and sets the
    runtime->delay field to their aggregate.
    
    Change-Id: Ib7e10f44ab8ccb46dc2f5825081d0afef662d827
    Signed-off-by: Kenneth Westfield <kwestfie@codeaurora.org>

--- include/sound/pcm.h
+++ include/sound/pcm.h
@@ -73,12 +73,13 @@ struct snd_pcm_ops {
 	int (*prepare)(struct snd_pcm_substream *substream);
 	int (*trigger)(struct snd_pcm_substream *substream, int cmd);
 	snd_pcm_uframes_t (*pointer)(struct snd_pcm_substream *substream);
+	int (*delay_blk)(struct snd_pcm_substream *substream);
 	int (*wall_clock)(struct snd_pcm_substream *substream,
 			  struct timespec *audio_ts);
 	int (*copy)(struct snd_pcm_substream *substream, int channel,
 		    snd_pcm_uframes_t pos,
 		    void __user *buf, snd_pcm_uframes_t count);
-	int (*silence)(struct snd_pcm_substream *substream, int channel, 
+	int (*silence)(struct snd_pcm_substream *substream, int channel,
 		       snd_pcm_uframes_t pos, snd_pcm_uframes_t count);
 	struct page *(*page)(struct snd_pcm_substream *substream,
 			     unsigned long offset);
