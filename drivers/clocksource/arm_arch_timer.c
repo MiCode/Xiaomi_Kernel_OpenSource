f1a1ad6dbd946b7bca13f584ffedb38d0501d6a5
Se Wang (Patrick) Oh <sewango@codeaurora.org>
2015-05-19 20:14:44 -0700

    clocksource: arch_timer: Enable user access to the physical counter
    A user space application is planned to support feature for
    synchronized timestamp among debug packets across peripherals.
    As part of the feature, it is responsible for providing physical
    timer count value to user space. So Enable user access to the
    physical counter in cp15 register.
    
    Change-Id: Idf7f6375713d842925e6f72a4b1fb98a7168726d
    Signed-off-by: Se Wang (Patrick) Oh <sewango@codeaurora.org>
    [psodagud: fixed up trivial merge conflict]
    Signed-off-by: Prasad Sodagudi <psodagud@codeaurora.org>

--- drivers/clocksource/arm_arch_timer.c
+++ drivers/clocksource/arm_arch_timer.c
@@ -773,6 +773,9 @@ static void arch_counter_set_user_access(void)
 	else
 		cntkctl |= ARCH_TIMER_USR_VCT_ACCESS_EN;
 
+	/* Enable user access to the physical counters */
+	cntkctl |= ARCH_TIMER_USR_PCT_ACCESS_EN;
+
 	arch_timer_set_cntkctl(cntkctl);
 }
 
