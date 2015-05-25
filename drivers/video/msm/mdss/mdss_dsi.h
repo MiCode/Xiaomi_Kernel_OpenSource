c4cb9fa2257a77dfd785cfb8146972a375048eeb
Sandeep Panda <spanda@codeaurora.org>
2015-05-25 14:42:07 +0530

    msm: mdss: enable burst mode for DSI command mode panels
    Enable burst mode for DSI command mode panels, so that
    dsi pixel data and dcs commands can be sent independent
    of each other. In case of burst mode we don't need to
    wait for DSI command transfer to complete before command
    kickoff.
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_dsi.c
    	drivers/video/msm/mdss/mdss_dsi_host.c
    
    Change-Id: I5777a72b8ad84a7929aa6e06849060efdebb0d7b
    Signed-off-by: Sandeep Panda <spanda@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi.h
+++ drivers/video/msm/mdss/mdss_dsi.h
@@ -455,6 +455,7 @@ struct mdss_dsi_ctrl_pdata {
 	int mdp_busy;
 	struct mutex mutex;
 	struct mutex cmd_mutex;
+	struct mutex cmdlist_mutex;
 	struct regulator *lab; /* vreg handle */
 	struct regulator *ibb; /* vreg handle */
 	struct mutex clk_lane_mutex;
@@ -465,7 +466,7 @@ struct mdss_dsi_ctrl_pdata {
 	bool mmss_clamp;
 	char dlane_swap;	/* data lane swap */
 	bool is_phyreg_enabled;
-
+	bool burst_mode_enabled;
 
 	struct dsi_buf tx_buf;
 	struct dsi_buf rx_buf;
