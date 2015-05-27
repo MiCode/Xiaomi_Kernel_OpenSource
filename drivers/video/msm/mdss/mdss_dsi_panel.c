8badfac731c0c9c06b8cf34c2ca699c17cd42a5c
Vineet Bajaj <vbajaj@codeaurora.org>
2015-05-27 19:26:53 +0530

    msm: mdss: unregister wled in case of probe failure
    If the DSI probe fails and wled has been registered, then
    when the next time probe comes there are failures seen.
    Fix this by unregistering the wled in case of DSI probe
    failures.
    
    Change-Id: Ia3fd2fa2b5f5bd446c15bc4c28602846f8540a68
    Signed-off-by: Vineet Bajaj <vbajaj@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -1499,6 +1499,12 @@ int mdss_panel_parse_bl_settings(struct device_node *np,
 	return 0;
 }
 
+void mdss_dsi_unregister_bl_settings(struct mdss_dsi_ctrl_pdata *ctrl_pdata)
+{
+	if (ctrl_pdata->bklt_ctrl == BL_WLED)
+		led_trigger_unregister_simple(bl_led_trigger);
+}
+
 static int mdss_panel_parse_dt(struct device_node *np,
 			struct mdss_dsi_ctrl_pdata *ctrl_pdata)
 {
