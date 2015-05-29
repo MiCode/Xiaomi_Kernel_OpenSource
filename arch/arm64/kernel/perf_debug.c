2db0bae1e73b551c8660444de67e85cadc2449df
Neil Leeder <nleeder@codeaurora.org>
2015-05-29 14:08:33 -0400

    perf: arm64: add support for kryo pmu
    Initialise the kryo PMU driver if it is detected.
    
    Change-Id: Idc8c1d515c2d913e8d79af5287aaa8154d7be7f4
    Signed-off-by: Neil Leeder <nleeder@codeaurora.org>
    [satyap: trivial merge conflict resolution]
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>

--- arch/arm64/kernel/perf_debug.c
+++ arch/arm64/kernel/perf_debug.c
@@ -31,6 +31,7 @@ static char *descriptions =
 	"11 Perf: arm64: Refine disable/enable in tracecounters\n"
 	"15 Perf: arm64: make debug dir handle exportable\n"
 	"16 Perf: arm64: add perf trace user\n"
+	"17 Perf: arm64: add support for kryo pmu\n"
 ;
 
 static ssize_t desc_read(struct file *fp, char __user *buf,
--- arch/arm64/kernel/perf_event.c
+++ arch/arm64/kernel/perf_event.c
@@ -674,6 +674,7 @@ static const struct of_device_id armv8_pmu_of_device_ids[] = {
 	{.compatible = "arm,armv8-pmuv3",	.data = armv8_pmuv3_init},
 	{.compatible = "arm,cortex-a53-pmu",	.data = armv8_a53_pmu_init},
 	{.compatible = "arm,cortex-a57-pmu",	.data = armv8_a57_pmu_init},
+	{.compatible = "qcom,kryo-pmuv3", .data = kryo_pmu_init},
 	{},
 };
 
