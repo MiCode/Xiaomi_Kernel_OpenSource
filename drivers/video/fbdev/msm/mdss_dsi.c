0b9e9534fb6b871a1339ff08f6dcaa4f8eadbaf2
Anusha Koduru <kanusha@codeaurora.org>
2015-06-01 19:00:51 +0530

    mdss: dsi: fix DSI clk rate update during dynamic resolution switch
    Current implementation updates the DSI link clock rate during UNBLANK.
    By this time link rate is already set with a random value, which
    might lead to unexpected behaviour. Update the clock during LINK READY
    so that the new  values take effect.
    
    Change-Id: Ib6d3f27b5607adb1d47ec5b8094af4f280424c2b
    Signed-off-by: Anusha Koduru <kanusha@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_dsi.c
+++ drivers/video/fbdev/msm/mdss_dsi.c
@@ -1937,15 +1937,15 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		ctrl_pdata->refresh_clk_rate = true;
 		break;
 	case MDSS_EVENT_LINK_READY:
+		if (ctrl_pdata->refresh_clk_rate)
+			rc = mdss_dsi_clk_refresh(pdata);
+
 		mdss_dsi_get_hw_revision(ctrl_pdata);
 		rc = mdss_dsi_on(pdata);
 		mdss_dsi_op_mode_config(pdata->panel_info.mipi.mode,
 							pdata);
 		break;
 	case MDSS_EVENT_UNBLANK:
-		if (ctrl_pdata->refresh_clk_rate)
-			rc = mdss_dsi_clk_refresh(pdata);
-
 		if (ctrl_pdata->on_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_unblank(pdata);
 		break;
