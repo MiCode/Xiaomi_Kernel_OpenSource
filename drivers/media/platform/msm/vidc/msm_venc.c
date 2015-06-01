625ce54ae545a320f9128ff0af326f3fc355e7a5
Praneeth Paladugu <ppaladug@codeaurora.org>
2015-06-01 15:55:47 -0700

    msm: vidc: Handle max clients error properly
    When FW returned max clients error, driver is changing inst->state
    to INVALID. With this driver doesn't know if FW had valid session or
    not to send ABORT command. If driver sends ABORT command, FW times out
    because there is no session in FW. Hence clean the session in HFI when
    FW returns max clients error so that ABORT doesn't reach FW.
    
    Change-Id: I4da5b457b30415458ce79cf7b0de0bf22f82f8e3
    Signed-off-by: Praneeth Paladugu <ppaladug@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_venc.c
+++ drivers/media/platform/msm/vidc/msm_venc.c
@@ -1175,6 +1175,7 @@ static int msm_venc_queue_setup(struct vb2_queue *q,
 	rc = msm_comm_try_state(inst, MSM_VIDC_OPEN_DONE);
 	if (rc) {
 		dprintk(VIDC_ERR, "Failed to open instance\n");
+		msm_comm_session_clean(inst);
 		return rc;
 	}
 
@@ -3243,6 +3244,7 @@ int msm_venc_s_fmt(struct msm_vidc_inst *inst, struct v4l2_format *f)
 		rc = msm_comm_try_state(inst, MSM_VIDC_OPEN_DONE);
 		if (rc) {
 			dprintk(VIDC_ERR, "Failed to open instance\n");
+			msm_comm_session_clean(inst);
 			goto exit;
 		}
 
