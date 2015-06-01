bc69c707244b0476a584e3a05354a44f80f774e6
Nikhilesh Reddy <reddyn@codeaurora.org>
2015-06-01 16:08:32 -0700

    uio: Add snapshot of MSM sharedmem driver
    This is a snapshot of the MSM sharedmem driver as of msm-3.14
    commit:
    
    commit 149717c082aab81 ("uio: msm_sharedmem: Add custom mmap")
    
    The following changes are included:
    02d55287 uio: msm_sharedmem: Restrict debugfs write to root.
    de961fc7 uio: msm_sharedmem: Return ENOMEM if the shared mem addr
    	 is zero.
    b974ce64 uio: msm_sharedmem: Add addtional information to debugfs
    c46af547 uio: msm_sharedmem: Add support for dynamic shared memory
    	 allocation
    
    Change-Id: I49902f018bde1d59d41027b7e46268cc17231a3e
    Signed-off-by: Nikhilesh Reddy <reddyn@codeaurora.org>
    [riteshh@codeaurora.org: fixed trivial merge conflicts]
    Signed-off-by: Ritesh Harjani <riteshh@codeaurora.org>

---
+++ drivers/uio/msm_sharedmem/sharedmem_qmi.h
@@ -0,0 +1,33 @@
+/* Copyright (c) 2014-2015, 2017, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __SHAREDMEM_QMI_H__
+#define __SHAREDMEM_QMI_H__
+
+#include <linux/module.h>
+
+struct sharemem_qmi_entry {
+	const char *client_name;
+	u32 client_id;
+	u64 address;
+	u32 size;
+	bool is_addr_dynamic;
+};
+
+int sharedmem_qmi_init(void);
+
+void sharedmem_qmi_exit(void);
+
+void sharedmem_qmi_add_entry(struct sharemem_qmi_entry *qmi_entry);
+
+#endif /* __SHAREDMEM_QMI_H__ */
