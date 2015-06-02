3904b3ed823dd921fe20e4a7eed6ed345b860420
Yan He <yanhe@codeaurora.org>
2015-06-01 19:46:46 -0700

    msm: pcie: correct the array sizes
    Correct the sizes of PCIe clock array and pipe clock array during
    initialization.
    
    Change-Id: Ic90850c3e5366a3c18ec80ee1c189d016ce5bb26
    Signed-off-by: Yan He <yanhe@codeaurora.org>

--- arch/arm/mach-msm/pcie.c
+++ arch/arm/mach-msm/pcie.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1871,9 +1871,9 @@ static int msm_pcie_probe(struct platform_device *pdev)
 	memcpy(msm_pcie_dev[rc_idx].gpio, msm_pcie_gpio_info,
 				sizeof(msm_pcie_gpio_info));
 	memcpy(msm_pcie_dev[rc_idx].clk, msm_pcie_clk_info[rc_idx],
-				sizeof(msm_pcie_clk_info));
+				sizeof(msm_pcie_clk_info[rc_idx]));
 	memcpy(msm_pcie_dev[rc_idx].pipeclk, msm_pcie_pipe_clk_info[rc_idx],
-				sizeof(msm_pcie_pipe_clk_info));
+				sizeof(msm_pcie_pipe_clk_info[rc_idx]));
 	memcpy(msm_pcie_dev[rc_idx].res, msm_pcie_res_info,
 				sizeof(msm_pcie_res_info));
 	memcpy(msm_pcie_dev[rc_idx].irq, msm_pcie_irq_info,
