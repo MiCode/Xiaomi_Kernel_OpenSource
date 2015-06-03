eefb8d69cda9e74a0057dec6ad2cb4547657b4e9
Jeevan Shriram <jshriram@codeaurora.org>
2015-06-03 10:23:48 -0700

    msm: mdss: enable clamp control configuration during clock on/off
    Enable MMSS clamp control configuration during clock on and off cases
    on DSI v1.04. This clamp configuration helps in retaining PHY register
    configuration when the MDSS gdsc is toggled.
    
    Change-Id: I403417442aedd394d4e8d7c4d20c361c2130dbb1
    Signed-off-by: Jeevan Shriram <jshriram@codeaurora.org>

--- drivers/video/fbdev/msm/msm_mdss_io_8974.c
+++ drivers/video/fbdev/msm/msm_mdss_io_8974.c
@@ -1411,12 +1411,6 @@ static int mdss_dsi_clamp_ctrl(struct mdss_dsi_ctrl_pdata *ctrl, int enable)
 		return -EINVAL;
 	}
 
-	if (IS_MDSS_MAJOR_MINOR_SAME(ctrl->shared_data->hw_rev,
-		MDSS_DSI_HW_REV_104)) {
-		pr_debug("%s: clamp ctrl configuration is skipped\n", __func__);
-		return 0;
-	}
-
 	clamp_reg_off = ctrl->shared_data->ulps_clamp_ctrl_off;
 	phyrst_reg_off = ctrl->shared_data->ulps_phyrst_ctrl_off;
 	mipi = &ctrl->panel_data.panel_info.mipi;
@@ -1640,9 +1634,6 @@ static int mdss_dsi_core_power_ctrl(struct mdss_dsi_ctrl_pdata *ctrl,
 			goto error_ulps;
 		}
 	} else {
-		if (pdata->panel_info.cont_splash_enabled)
-			mdss_dsi_read_hw_revision(ctrl);
-
 		/*
 		 * Enable DSI clamps only if entering idle power collapse or
 		 * when ULPS during suspend is enabled.
