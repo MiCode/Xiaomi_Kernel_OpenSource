1b458c04b48a4975a47c0f0246d7afa26c5d930f
Neeti Desai <neetid@codeaurora.org>
2015-06-03 11:07:48 -0700

    msm: Return -ENOSYS when secure buffer apis are not implemented
    The return value -EINVAL is returned in the case of invalid
    arguments, and is not the correct value when the function is not
    implemented.
    
    Return -ENOSYS instead.
    
    Change-Id: I196537f121d5a290fec74e2b7bcb1cfd490468c7
    Signed-off-by: Neeti Desai <neetid@codeaurora.org>
    [pdaly@codeaurora.org Resolve minor conflicts]
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

--- include/soc/qcom/secure_buffer.h
+++ include/soc/qcom/secure_buffer.h
@@ -52,25 +52,24 @@ const char *msm_secure_vmid_to_string(int secure_vmid);
 #else
 static inline int msm_secure_table(struct sg_table *table)
 {
-	return -EINVAL;
+	return -ENOSYS;
 }
 static inline int msm_unsecure_table(struct sg_table *table)
 {
-	return -EINVAL;
+	return -ENOSYS;
 }
 static inline int hyp_assign_table(struct sg_table *table,
 			u32 *source_vm_list, int source_nelems,
 			int *dest_vmids, int *dest_perms,
 			int dest_nelems)
 {
-	return -EINVAL;
+	return -ENOSYS;
 }
-
 static inline int hyp_assign_phys(phys_addr_t addr, u64 size,
-			u32 *source_vmlist, int source_nelems,
-			int *dest_vmids, int *dest_perms, int dest_nelems)
+			int *dest_vmids, int *dest_perms,
+			int dest_nelems)
 {
-	return -EINVAL;
+	return -ENOSYS;
 }
 
 static inline bool msm_secure_v2_is_supported(void)
