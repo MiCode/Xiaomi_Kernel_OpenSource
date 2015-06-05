f5c5247f16bfcb68694552413b07bbb291a74908
Ravi Aravamudhan <aravamud@codeaurora.org>
2015-06-04 17:43:43 -0700

    diag: Ratelimit error message on failure to write to remote processor
    Diag driver is printing an error dmesg on failure to write to diagfwd
    bridge. Change pr_err to pr_err_ratelimited to avoid flooding kernel
    dmesg.
    
    Change-Id: Iafe6e8270367ac27011b4e3935f67c2f97556e3f
    Signed-off-by: Ravi Aravamudhan <aravamud@codeaurora.org>

--- drivers/char/diag/diagchar_core.c
+++ drivers/char/diag/diagchar_core.c
@@ -938,8 +938,8 @@ send_data:
 	err = diagfwd_bridge_write(proc, driver->hdlc_encode_buf,
 				   driver->hdlc_encode_buf_len);
 	if (err) {
-		pr_err("diag: Error writing Callback packet to proc: %d, err: %d\n",
-			proc, err);
+		pr_err_ratelimited("diag: Error writing Callback packet to proc: %d, err: %d\n",
+				   proc, err);
 		driver->hdlc_encode_buf_len = 0;
 	}
 
