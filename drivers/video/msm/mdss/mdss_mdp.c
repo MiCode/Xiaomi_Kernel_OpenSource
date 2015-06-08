c942c3895c419feb6ee5766269eb4cfc3c3e596d
Krishna Chaitanya Parimi <cparimi@codeaurora.org>
2015-06-08 14:15:09 +0530

    msm: mdss: unlock intr lock before returning early in hist calls
    In hist mask and unmask functions, there is an early return that
    can cause function to exit without unlocking the spin_lock. This
    would cause further calls in commit to be called in atomic context
    causing kernel panics. Handling the early returns properly.
    
    Change-Id: Iaeb1f8a1f9951c7a9a0da7b3fb6c7a994f99c8ea
    Signed-off-by: Krishna Chaitanya Parimi <cparimi@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp.c
+++ drivers/video/msm/mdss/mdss_mdp.c
@@ -3705,8 +3705,10 @@ void mdss_mdp_hist_irq_set_mask(u32 irq)
 	enabled_irq = mdata->hist_intr.curr;
 
 	/* Do not enable irq for which hist_enable not called */
-	if (!(enabled_irq & irq))
+	if (!(enabled_irq & irq)) {
+		spin_unlock_irqrestore(&mdata->hist_intr.lock, flag);
 		return;
+	}
 
 	mask = readl_relaxed(mdata->mdp_base + MDSS_MDP_REG_HIST_INTR_EN);
 	mask |= irq;
@@ -3725,8 +3727,10 @@ void mdss_mdp_hist_irq_unset_mask(u32 irq)
 	enabled_irq = mdata->hist_intr.curr;
 
 	/* Do not disable irq for which hist_enable not called */
-	if (!(enabled_irq & irq))
+	if (!(enabled_irq & irq)) {
+		spin_unlock_irqrestore(&mdata->hist_intr.lock, flag);
 		return;
+	}
 
 	mask = readl_relaxed(mdata->mdp_base + MDSS_MDP_REG_HIST_INTR_EN);
 	mask &= ~irq;
