2f348ca95ca41315843fbc662aff4d6f3e36cca6
Carter Cooper <ccooper@codeaurora.org>
2015-06-10 12:08:23 -0600

    msm: kgsl: Directly clear structure over using memset
    Using memset for 12 bytes is overkill. Clear the values directly
    to save precious nanoseconds.
    
    Change-Id: Ic489489befacddbdc7c965f8ff3a4acb4f2d10a7
    Signed-off-by: Carter Cooper <ccooper@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1450,7 +1450,9 @@ static int _adreno_start(struct adreno_device *adreno_dev)
 	}
 
 	/* Clear the busy_data stats - we're starting over from scratch */
-	memset(&adreno_dev->busy_data, 0, sizeof(adreno_dev->busy_data));
+	adreno_dev->busy_data.gpu_busy = 0;
+	adreno_dev->busy_data.vbif_ram_cycles = 0;
+	adreno_dev->busy_data.vbif_starved_ram = 0;
 
 	/* Restore performance counter registers with saved values */
 	adreno_perfcounter_restore(adreno_dev);
