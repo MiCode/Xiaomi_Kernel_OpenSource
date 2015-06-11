129d5ad7724026149f185bb6f96186694246d6e5
Ujwal Patel <ujwalp@codeaurora.org>
2015-06-11 09:57:05 -0700

    msm: mdss: fix secondary ctl flush issue on command mode panels
    Starting with patch 2551ce16a5d44a3b03d0885b499924f6d218c78e, msm: mdss:
    add async support for hw cursor, flush logic was modifed. However it
    introduced an issue where if mixer configuration is skipped then
    secondary flush is skipped. This can cause issues. Fix the flush logic
    by ensuring that secondary flush is not skipped.
    
    Change-Id: Iee8b9ca80353b2fdb096e64b12c865cd10f0dd08
    Signed-off-by: Ujwal Patel <ujwalp@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_mdp_ctl.c
+++ drivers/video/fbdev/msm/mdss_mdp_ctl.c
@@ -4256,10 +4256,14 @@ int mdss_mdp_display_commit(struct mdss_mdp_ctl *ctl, void *arg,
 		/* postprocessing setup, including dspp */
 		mdss_mdp_pp_setup_locked(ctl);
 
-	if (sctl && ctl->split_flush_en) {
-		ctl->flush_bits |= sctl->flush_bits;
-		sctl->flush_bits = 0;
-		sctl_flush_bits = sctl->flush_bits;
+	if (sctl) {
+		if (ctl->split_flush_en) {
+			ctl->flush_bits |= sctl->flush_bits;
+			sctl->flush_bits = 0;
+			sctl_flush_bits = 0;
+		} else {
+			sctl_flush_bits = sctl->flush_bits;
+		}
 	}
 	ctl_flush_bits = ctl->flush_bits;
 
