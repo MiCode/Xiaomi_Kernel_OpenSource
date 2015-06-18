486155f53e2180d5fac41e85bb12cf8755e12241
Matt Wagantall <mattw@codeaurora.org>
2015-06-18 14:02:53 -0700

    arm: psci: drop info prints seen during cpu hotplug to pr_debug
    These message are printed every time a CPU in hotplugged out,
    resulting in log noise and hurting hotplug latency. Silence them
    by default, by dropping them to pr_debug.
    
    Change-Id: I21632c1dbf54342b5980a82ab7389713e943bf6c
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

--- arch/arm/kernel/psci_smp.c
+++ arch/arm/kernel/psci_smp.c
@@ -98,12 +98,12 @@ int psci_cpu_kill(unsigned int cpu)
 	for (i = 0; i < 10; i++) {
 		err = psci_ops.affinity_info(cpu_logical_map(cpu), 0);
 		if (err == PSCI_0_2_AFFINITY_LEVEL_OFF) {
-			pr_info("CPU%d killed.\n", cpu);
+			pr_debug("CPU%d killed.\n", cpu);
 			return 1;
 		}
 
 		msleep(10);
-		pr_info("Retrying again to check for CPU kill\n");
+		pr_debug("Retrying again to check for CPU kill\n");
 	}
 
 	pr_warn("CPU%d may not have shut down cleanly (AFFINITY_INFO reports %d)\n",
