1213f42de72b270b2420ce439a83bb1c9c72ae9b
Sunil Khatri <sunilkh@codeaurora.org>
2015-06-20 00:19:43 +0530

    msm: kgsl: Cast perfcounter query count to an unsigned int
    In adreno_perfcounter_query_group() make sure to cast the user passed
    count to an unsigned int before comparing it to the group count.
    Otherwise the user count could be interpeted as a signed int and
    hilarity ensues.
    
    Change-Id: Ic0dedbad825f5b3fd4434f9b9f6d4d308206c0d9
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Sunil Khatri <sunilkh@codeaurora.org>

--- drivers/gpu/msm/adreno_perfcounter.c
+++ drivers/gpu/msm/adreno_perfcounter.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2002,2007-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2002,2007-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -451,7 +451,7 @@ int adreno_perfcounter_query_group(struct adreno_device *adreno_dev,
 		return 0;
 	}
 
-	t = min_t(int, group->reg_count, count);
+	t = min_t(unsigned int, group->reg_count, count);
 
 	buf = kmalloc(t * sizeof(unsigned int), GFP_KERNEL);
 	if (buf == NULL) {
