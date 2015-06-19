ac2b4345ef8ed7fa18c47d707ffdcff881596f5b
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2015-06-19 13:07:08 +0530

    nohz_full: Force RCU's grace-period kthreads onto timekeeping CPU
    Because RCU's quiescent-state-forcing mechanism is used to drive the
    full-system-idle state machine, and because this mechanism is executed
    by RCU's grace-period kthreads, this commit forces these kthreads to
    run on the timekeeping CPU (tick_do_timer_cpu).  To do otherwise would
    mean that the RCU grace-period kthreads would force the system into
    non-idle state every time they drove the state machine, which would
    be just a bit on the futile side.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Lai Jiangshan <laijs@cn.fujitsu.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Git-commit: eb75767be0e514f97bf1b5cec763696cfc7f7e2a
    Signed-off-by: Kishan Kumar <kishank@codeaurora.org>

--- kernel/rcu/tree.h
+++ kernel/rcu/tree.h
@@ -546,7 +546,7 @@ static void rcu_boot_init_nocb_percpu_data(struct rcu_data *rdp);
 static void rcu_spawn_nocb_kthreads(struct rcu_state *rsp);
 static void rcu_kick_nohz_cpu(int cpu);
 static bool init_nocb_callback_list(struct rcu_data *rdp);
-
+static void rcu_bind_gp_kthread(void);
 #endif /* #ifndef RCU_TREE_NONCORE */
 
 #ifdef CONFIG_RCU_TRACE
