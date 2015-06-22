5129f649348db303db6ed0ea890cf397f2f271e7
Hanumath Prasad <hpprasad@codeaurora.org>
2015-06-22 16:03:57 +0530

    cpufreq: interactive:call __cpufreq_driver_target() for cur frequency
    __cpufreq_driver_target() is not getting called by the governor
    when the current frequency is within the new policy limits when some
    external client (say thermal mitigation) change the policy limits.
    However, there is a race here that current frequency might be
    getting set in parallel by speedchange task which can be higher than
    the new policy->max which was set by external client. This results the
    current frequency being higher than the policy->max and voilating the
    policy limits. This causes thermal impact as well and in turn high power
    consumption. So Fix this by calling __cpufreq_driver_target() even for
    the current frequency and update the cpu frequency inside the cpufreq
    driver based on the policy check.
    
    Change-Id: I9136e9245677e8fc90a628d3099aca8d63d3677c
    Signed-off-by: Hanumath Prasad <hpprasad@codeaurora.org>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -1686,12 +1686,9 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 		break;
 
 	case CPUFREQ_GOV_LIMITS:
-		if (policy->max < policy->cur)
-			__cpufreq_driver_target(policy,
-					policy->max, CPUFREQ_RELATION_H);
-		else if (policy->min > policy->cur)
-			__cpufreq_driver_target(policy,
-					policy->min, CPUFREQ_RELATION_L);
+		__cpufreq_driver_target(policy,
+				policy->cur, CPUFREQ_RELATION_L);
+
 		for_each_cpu(j, policy->cpus) {
 			pcpu = &per_cpu(cpuinfo, j);
 
