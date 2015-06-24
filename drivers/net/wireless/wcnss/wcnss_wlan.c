d0e2624e63d200bc711161508d515ee48f87edbe
Anand N Sunkad <asunka@codeaurora.org>
2015-06-24 13:34:17 +0530

    wcnss: Enable Qos Ref clock for 8916/8939
    Add support to enable Qos Ref Clock for Qos Box support.
    
    CRs-Fixed: 860487
    Change-Id: I46cd3fc809807050efb4f38372dce3d854141bcf
    Signed-off-by: Anand N Sunkad <asunka@codeaurora.org>

--- drivers/net/wireless/wcnss/wcnss_wlan.c
+++ drivers/net/wireless/wcnss/wcnss_wlan.c
@@ -2703,6 +2703,7 @@ wcnss_trigger_config(struct platform_device *pdev)
 {
 	int ret;
 	int rc, index = 0;
+	struct clk *snoc_qosgen;
 	struct qcom_wcnss_opts *pdata;
 	struct resource *res;
 	int is_pronto_vadc;
@@ -3111,6 +3112,18 @@ wcnss_trigger_config(struct platform_device *pdev)
 	} else {
 		INIT_DELAYED_WORK(&penv->vbatt_work, wcnss_update_vbatt);
 		penv->fw_vbatt_state = WCNSS_CONFIG_UNSPECIFIED;
+	}
+
+	snoc_qosgen = clk_get(&pdev->dev, "snoc_qosgen");
+
+	if (IS_ERR(snoc_qosgen)) {
+		pr_err("Couldn't get snoc_qosgen\n");
+	} else {
+		if (clk_prepare_enable(snoc_qosgen)) {
+			pr_err("snoc_qosgen enable failed\n");
+		} else {
+			pr_info("snoc_qosgen configured successfully\n");
+		}
 	}
 
 	if (penv->wlan_config.is_pronto_vadc) {
