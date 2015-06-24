3eac23bf1a79166be1959d17f4cbb8762210ff34
Neeti Desai <neetid@codeaurora.org>
2015-06-24 15:41:54 -0700

    msm: ion: Add secure cma heap
    Add the infrastructure to support a secure cma heap
    needed for secure usecases. The new heap uses
    the existing cma heap infrastructure, along with adding
    the assign call to assign the correct VM to the buffers
    during allocation and free.
    
    Change-Id: I0c959f46ffa6eb67128b6794e9e60c470a3d292e
    Signed-off-by: Neeti Desai <neetid@codeaurora.org>

--- drivers/staging/android/ion/msm_ion_priv.h
+++ drivers/staging/android/ion/msm_ion_priv.h
@@ -2,7 +2,7 @@
  * drivers/staging/android/ion/msm_ion_priv.h
  *
  * Copyright (C) 2011 Google, Inc.
- * Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -51,6 +51,9 @@ void ion_cp_heap_destroy(struct ion_heap *);
 
 struct ion_heap *ion_system_secure_heap_create(struct ion_platform_heap *);
 void ion_system_secure_heap_destroy(struct ion_heap *);
+
+struct ion_heap *ion_cma_secure_heap_create(struct ion_platform_heap *);
+void ion_cma_secure_heap_destroy(struct ion_heap *);
 
 long msm_ion_custom_ioctl(struct ion_client *client,
 				unsigned int cmd,
@@ -116,6 +119,8 @@ int ion_heap_allow_secure_allocation(enum ion_heap_type type);
 int ion_heap_allow_heap_secure(enum ion_heap_type type);
 
 int ion_heap_allow_handle_secure(enum ion_heap_type type);
+
+int get_secure_vmid(unsigned long);
 
 /**
  * ion_create_chunked_sg_table - helper function to create sg table
