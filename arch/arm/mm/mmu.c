a4bba1150fc4d276996b1982e3ef29ea73973610
Shiraz Hashim <shashim@codeaurora.org>
2015-06-30 09:14:46 +0530

    arm: mm: consider only lowmem regions while remap
    Consider only those memory blocks with lowmem while
    remapping into 4KB regions using CONFIG_FORCE_PAGES
    feature.
    
    CRs-fixed: 855005
    Change-Id: Ie209fce6c310f911d8cf02d977e226660684a6ab
    Signed-off-by: Shiraz Hashim <shashim@codeaurora.org>
    Signed-off-by: Swetha Chikkaboraiah <schikk@codeaurora.org>

--- arch/arm/mm/mmu.c
+++ arch/arm/mm/mmu.c
@@ -1571,6 +1571,8 @@ static void __init remap_pages(void)
 		bool fixup = false, end_fixup = false;
 		unsigned long saved_start = addr;
 
+		if (phys_start > arm_lowmem_limit)
+			break;
 		if (phys_end > arm_lowmem_limit)
 			end = (unsigned long)__va(arm_lowmem_limit);
 		if (phys_start >= phys_end)
