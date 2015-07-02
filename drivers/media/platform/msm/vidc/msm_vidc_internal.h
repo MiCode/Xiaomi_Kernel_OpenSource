0e054883e84d0a7e1f27d6e0f14fe08379f7028a
Maheshwar Ajja <majja@codeaurora.org>
2015-07-02 20:18:04 +0530

    msm: vidc: read capabilities as part of sys_init_done
    Venus firmware now giving codec capabilities as part
    of sys_init_done instead of session_init_done message
    for all codecs. Parse capabilities in sys_init_done
    and assign them to codecs indicated by venus firmware.
    This change also maintains the backward compatibility
    where firmware will send the capability as part of
    session_init_done.
    
    Change-Id: I020f614538f26b2d2e1c544373f2af1f665e2449
    Signed-off-by: Maheshwar Ajja <majja@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc_internal.h
+++ drivers/media/platform/msm/vidc/msm_vidc_internal.h
@@ -227,22 +227,6 @@ enum msm_vidc_modes {
 	VIDC_POWER_SAVE = 1 << 4,
 };
 
-struct msm_vidc_core_capability {
-	struct hal_capability_supported width;
-	struct hal_capability_supported height;
-	struct hal_capability_supported frame_rate;
-	u32 pixelprocess_capabilities;
-	struct hal_capability_supported scale_x;
-	struct hal_capability_supported scale_y;
-	struct hal_capability_supported hier_p;
-	struct hal_capability_supported ltr_count;
-	struct hal_capability_supported mbs_per_frame;
-	struct hal_capability_supported secure_output2_threshold;
-	u32 capability_set;
-	enum buffer_mode_type buffer_mode[MAX_PORT_NUM];
-	u32 buffer_size_limit;
-};
-
 struct msm_vidc_idle_stats {
 	bool idle;
 	u32 fb_err_level;
@@ -269,6 +253,8 @@ struct msm_vidc_core {
 	struct msm_vidc_platform_resources resources;
 	u32 enc_codec_supported;
 	u32 dec_codec_supported;
+	u32 codec_count;
+	struct msm_vidc_capability *capabilities;
 	struct msm_vidc_idle_stats idle_stats;
 	struct delayed_work fw_unload_work;
 };
@@ -307,7 +293,8 @@ struct msm_vidc_inst {
 	struct buf_count count;
 	struct dcvs_stats dcvs;
 	enum msm_vidc_modes flags;
-	struct msm_vidc_core_capability capability;
+	struct msm_vidc_capability capability;
+	u32 buffer_size_limit;
 	enum buffer_mode_type buffer_mode_set[MAX_PORT_NUM];
 	bool map_output_buffer;
 	atomic_t seq_hdr_reqs;
