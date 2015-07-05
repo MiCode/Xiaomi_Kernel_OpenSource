c3b595b7ae31d53b547dc699bb055709ccebddee
Banajit Goswami <bgoswami@codeaurora.org>
2015-07-04 22:14:27 -0700

    ASoC: compare CPU DAI stream name to find BE DAI
    While setting up route for a particular device, compare
    stream name of CPU DAI and Backend DAI to find the correct
    Backend DAI.
    
    Change-Id: Ic3f7c0e5b2a1055e7fdf52c78ded797a9a126d03
    Signed-off-by: Banajit Goswami <bgoswami@codeaurora.org>

--- sound/soc/soc-pcm.c
+++ sound/soc/soc-pcm.c
@@ -1284,7 +1284,11 @@ static struct snd_soc_pcm_runtime *dpcm_get_be(struct snd_soc_card *card,
 			if (!be->dai_link->no_pcm)
 				continue;
 
-			if (be->cpu_dai->playback_widget == widget)
+			if ((be->cpu_dai->playback_widget == widget &&
+				(be->dai_link->stream_name &&
+				!strcmp(be->dai_link->stream_name,
+				    be->cpu_dai->playback_widget->sname))) ||
+				be->codec_dai->playback_widget == widget)
 				return be;
 
 			for (j = 0; j < be->num_codecs; j++) {
@@ -1301,7 +1305,11 @@ static struct snd_soc_pcm_runtime *dpcm_get_be(struct snd_soc_card *card,
 			if (!be->dai_link->no_pcm)
 				continue;
 
-			if (be->cpu_dai->capture_widget == widget)
+			if ((be->cpu_dai->capture_widget == widget &&
+				(be->dai_link->stream_name &&
+				!strcmp(be->dai_link->stream_name,
+				    be->cpu_dai->capture_widget->sname))) ||
+				be->codec_dai->capture_widget == widget)
 				return be;
 
 			for (j = 0; j < be->num_codecs; j++) {
