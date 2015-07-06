ff89deb723da77fb1ecd7894bcb9e4676cbfd6ea
Fred Oh <fred@codeaurora.org>
2015-07-06 10:03:39 -0700

    ASoC: msm: qdsp6v2: fix incorrect packet size calculation
    The header size included twice which cause memory out of bounds.
    
    Change-Id: I3a6f429283cfa28a1f3761f655c00b154e25f8ce
    Signed-off-by: Fred Oh <fred@codeaurora.org>

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -608,9 +608,8 @@ int send_asm_custom_topology(struct audio_client *ac)
 			__func__, ASM_CUSTOM_TOP_CAL);
 		goto unlock;
 	}
-	q6asm_add_hdr_custom_topology(ac, &asm_top.hdr,
-				      APR_PKT_SIZE(APR_HDR_SIZE,
-					sizeof(asm_top)), TRUE);
+
+	q6asm_add_hdr_custom_topology(ac, &asm_top.hdr, sizeof(asm_top), TRUE);
 	atomic_set(&ac->mem_state, 1);
 	asm_top.hdr.opcode = ASM_CMD_ADD_TOPOLOGIES;
 	asm_top.payload_addr_lsw = lower_32_bits(cal_block->cal_data.paddr);
