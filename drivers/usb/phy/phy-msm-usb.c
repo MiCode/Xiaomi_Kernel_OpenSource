3c9d4f3ae0338a1301deedde99b1cce4366bb988
Sujeet Kumar <ksujeet@codeaurora.org>
2015-07-09 11:34:02 +0530

    USB: ci13xxx: Add extra allocation size to use APF feature for IN eps
    APF feature requires the software to allocate additional 256
    bytes for TX/IN buffers. This is required because optimized
    pre-fetching makes hardware to read outside the allocated
    buffer.
    
    To protect out of boundary access, SW needs to allocate and
    extra 128 bytes or more to make sure the neighbouring region
    is not protected by TZ or MSA. Introduce an extra_alloc_buf
    in usb gadget which can indicate this extra allocation. Enable
    this only if APF is enabled.
    
    Change-Id: Ifff3c0c41b5cd4a326d721e8bacc66262d1985a5
    Signed-off-by: Sujeet Kumar <ksujeet@codeaurora.org>

--- drivers/usb/phy/phy-msm-usb.c
+++ drivers/usb/phy/phy-msm-usb.c
@@ -5062,6 +5062,7 @@ static struct platform_device *msm_otg_add_pdev(
 		ci_pdata.system_clk = otg_pdata->system_clk;
 		ci_pdata.pclk = otg_pdata->pclk;
 		ci_pdata.enable_streaming = otg_pdata->enable_streaming;
+		ci_pdata.enable_axi_prefetch = otg_pdata->enable_axi_prefetch;
 		ci_pdata.max_nominal_system_clk_rate =
 					motg->max_nominal_system_clk_rate;
 		ci_pdata.default_system_clk_rate = motg->core_clk_rate;
