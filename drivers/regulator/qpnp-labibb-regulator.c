2dd890fc4855cf255b8a50318bd9ad2873a55e8c
Ravindranath Thiyagarajan <rthiyaga@codeaurora.org>
2015-07-09 14:18:02 +0530

    regulator: qpnp-labibb-regulator: Allow configuring SOFT_START_CTL
    Currently, LAB_SOFT_START_CTL and IBB_SOFT_START_CTL registers are
    configured only if the IBB module was not enabled already by the
    bootloader. Allow configuring them unconditionally if the device tree
    parameters are specified in HLOS.
    
    CRs-Fixed: 865268
    Change-Id: I8455f40be0824cb7501279fb988ab92c7beccbb9
    Signed-off-by: Ravindranath Thiyagarajan <rthiyaga@codeaurora.org>

--- drivers/regulator/qpnp-labibb-regulator.c
+++ drivers/regulator/qpnp-labibb-regulator.c
@@ -529,29 +529,6 @@ static int qpnp_lab_dt_init(struct qpnp_labibb *labibb,
 		}
 	}
 
-	rc = of_property_read_u32(of_node, "qcom,qpnp-lab-soft-start",
-					&(labibb->lab_vreg.soft_start));
-	if (rc < 0) {
-		pr_err("qcom,qpnp-lab-soft-start is missing, rc = %d\n",
-			rc);
-		return rc;
-	}
-
-	for (val = 0; val < sizeof(ARRAY_SIZE(lab_soft_start_plan)); val++)
-		if (lab_soft_start_plan[val] == labibb->lab_vreg.soft_start)
-			break;
-
-	if (val == ARRAY_SIZE(lab_soft_start_plan))
-		val = ARRAY_SIZE(lab_soft_start_plan) - 1;
-
-	rc = qpnp_labibb_write(labibb, labibb->lab_base +
-				REG_LAB_SOFT_START_CTL, &val, 1);
-	if (rc) {
-		pr_err("qpnp_lab_dt_init write register %x failed rc = %d\n",
-			REG_LAB_SOFT_START_CTL, rc);
-		return rc;
-	}
-
 	val = 0;
 
 	if (of_property_read_bool(of_node, "qcom,qpnp-lab-full-pull-down"))
@@ -1113,6 +1090,32 @@ static int register_qpnp_lab_regulator(struct qpnp_labibb *labibb,
 		return rc;
 	}
 
+	rc = of_property_read_u32(of_node, "qcom,qpnp-lab-soft-start",
+					&(labibb->lab_vreg.soft_start));
+	if (rc < 0) {
+		pr_err("qcom,qpnp-lab-soft-start is missing, rc = %d\n",
+			rc);
+		return rc;
+	}
+
+	for (val = 0; val < ARRAY_SIZE(lab_soft_start_plan); val++)
+		if (lab_soft_start_plan[val] == labibb->lab_vreg.soft_start)
+			break;
+
+	if (val == ARRAY_SIZE(lab_soft_start_plan))
+		val = ARRAY_SIZE(lab_soft_start_plan) - 1;
+
+	rc = qpnp_labibb_write(labibb, labibb->lab_base +
+				REG_LAB_SOFT_START_CTL, &val, 1);
+	if (rc) {
+		pr_err("qpnp_labibb_write register %x failed rc = %d\n",
+			REG_LAB_SOFT_START_CTL, rc);
+		return rc;
+	}
+
+	labibb->lab_vreg.soft_start = lab_soft_start_plan
+				[val & LAB_SOFT_START_CTL_MASK];
+
 	if (ibb_en_rdy_val == LAB_IBB_EN_RDY_EN) {
 		rc = qpnp_labibb_read(labibb, &val,
 			labibb->lab_base + REG_LAB_LCD_AMOLED_SEL, 1);
@@ -1161,16 +1164,6 @@ static int register_qpnp_lab_regulator(struct qpnp_labibb *labibb,
 			}
 
 		}
-
-		rc = qpnp_labibb_read(labibb, &val,
-				labibb->lab_base + REG_LAB_SOFT_START_CTL, 1);
-		if (rc) {
-			pr_err("qpnp_lab_read read register %x failed rc = %d\n",
-				REG_LAB_SOFT_START_CTL, rc);
-			return rc;
-		}
-		labibb->lab_vreg.soft_start = lab_soft_start_plan
-					[val & LAB_SOFT_START_CTL_MASK];
 
 		labibb->lab_vreg.vreg_enabled = 1;
 	} else {
@@ -1485,43 +1478,6 @@ static int qpnp_ibb_dt_init(struct qpnp_labibb *labibb,
 				REG_IBB_LCD_AMOLED_SEL, rc);
 			return rc;
 		}
-	}
-
-	rc = of_property_read_u32(of_node, "qcom,qpnp-ibb-discharge-resistor",
-			&tmp);
-
-	if (rc < 0) {
-		pr_err("qcom,qpnp-ibb-discharge-resistor is missing, rc = %d\n",
-			rc);
-		return rc;
-	}
-
-
-	if (labibb->mode == QPNP_LABIBB_AMOLED_MODE) {
-		/*
-		 * AMOLED mode needs ibb discharge resistor to be
-		 * configured for 300KOhm
-		 */
-		if (tmp < ibb_discharge_resistor_plan[0])
-			tmp = ibb_discharge_resistor_plan[0];
-	}
-
-	for (val = 0; val < sizeof(ARRAY_SIZE(ibb_discharge_resistor_plan));
-		val++)
-		if (ibb_discharge_resistor_plan[val] == tmp)
-			break;
-
-	if (val == ARRAY_SIZE(ibb_discharge_resistor_plan)) {
-		pr_err("Invalid property in qcom,qpnp-ibb-discharge-resistor\n");
-		return -EINVAL;
-	}
-
-	rc = qpnp_labibb_write(labibb, labibb->ibb_base +
-				REG_IBB_SOFT_START_CTL, &val, 1);
-	if (rc) {
-		pr_err("qpnp_ibb_dt_init write register %x failed rc = %d\n",
-			REG_IBB_SOFT_START_CTL, rc);
-		return rc;
 	}
 
 	rc = of_property_read_u32(of_node, "qcom,qpnp-ibb-lab-pwrdn-delay",
@@ -1951,7 +1907,8 @@ static int register_qpnp_ibb_regulator(struct qpnp_labibb *labibb,
 	struct regulator_init_data *init_data;
 	struct regulator_desc *rdesc;
 	struct regulator_config cfg = {};
-	u8 val;
+	u8 val, ibb_enable_ctl;
+	u32 tmp;
 
 	if (!of_node) {
 		dev_err(labibb->dev, "qpnp ibb regulator device tree node is missing\n");
@@ -1962,14 +1919,6 @@ static int register_qpnp_ibb_regulator(struct qpnp_labibb *labibb,
 	if (!init_data) {
 		pr_err("unable to get regulator init data for qpnp ibb regulator\n");
 		return -ENOMEM;
-	}
-
-	rc = qpnp_labibb_read(labibb, &val,
-				labibb->ibb_base + REG_IBB_ENABLE_CTL, 1);
-	if (rc) {
-		pr_err("qpnp_ibb_read register %x failed rc = %d\n",
-			REG_IBB_ENABLE_CTL, rc);
-		return rc;
 	}
 
 	rc = of_property_read_u32(of_node, "qcom,qpnp-ibb-min-voltage",
@@ -2003,7 +1952,50 @@ static int register_qpnp_ibb_regulator(struct qpnp_labibb *labibb,
 		return rc;
 	}
 
-	if (val != 0) {
+	rc = of_property_read_u32(of_node, "qcom,qpnp-ibb-discharge-resistor",
+			&tmp);
+
+	if (rc < 0) {
+		pr_err("qcom,qpnp-ibb-discharge-resistor is missing, rc = %d\n",
+			rc);
+		return rc;
+	}
+
+	if (labibb->mode == QPNP_LABIBB_AMOLED_MODE) {
+		/*
+		 * AMOLED mode needs ibb discharge resistor to be
+		 * configured for 300KOhm
+		 */
+		if (tmp < ibb_discharge_resistor_plan[0])
+			tmp = ibb_discharge_resistor_plan[0];
+	}
+
+	for (val = 0; val < ARRAY_SIZE(ibb_discharge_resistor_plan); val++)
+		if (ibb_discharge_resistor_plan[val] == tmp)
+			break;
+
+	if (val == ARRAY_SIZE(ibb_discharge_resistor_plan)) {
+		pr_err("Invalid property in qcom,qpnp-ibb-discharge-resistor\n");
+		return -EINVAL;
+	}
+
+	rc = qpnp_labibb_write(labibb, labibb->ibb_base +
+			REG_IBB_SOFT_START_CTL, &val, 1);
+	if (rc) {
+		pr_err("qpnp_labibb_write register %x failed rc = %d\n",
+			REG_IBB_SOFT_START_CTL, rc);
+		return rc;
+	}
+
+	rc = qpnp_labibb_read(labibb, &ibb_enable_ctl,
+				labibb->ibb_base + REG_IBB_ENABLE_CTL, 1);
+	if (rc) {
+		pr_err("qpnp_ibb_read register %x failed rc = %d\n",
+			REG_IBB_ENABLE_CTL, rc);
+		return rc;
+	}
+
+	if (ibb_enable_ctl != 0) {
 		rc = qpnp_labibb_read(labibb, &val,
 			labibb->ibb_base + REG_IBB_LCD_AMOLED_SEL, 1);
 		if (rc) {
