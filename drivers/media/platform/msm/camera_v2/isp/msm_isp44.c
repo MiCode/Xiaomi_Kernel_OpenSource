d60cc530323f1f73825cd8c5faa57126491516ee
Peter Liu <pingchie@codeaurora.org>
2015-07-08 18:37:49 -0700

    msm: camera: isp: avoid getting buffer during recovery
    Avoid getting new buffers when tasklet is waiting for recovery.
    This could avoid requesting buffers which are not needed.
    
    Change-Id: I0221846322ac6b2e1f5d82fbc25e50d01f43b3f6
    Signed-off-by: Peter Liu <pingchie@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp44.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp44.c
@@ -399,7 +399,6 @@ static void msm_vfe44_process_error_status(struct vfe_device *vfe_dev)
 	if (error_status1 & (1 << 6))
 		pr_err("%s: realign buf cr overflow\n", __func__);
 	if (error_status1 & (1 << 7)) {
-		pr_err("%s: violation\n", __func__);
 		msm_vfe44_process_violation_status(vfe_dev);
 	}
 	if (error_status1 & (1 << 9)) {
