33a67dc362bae23cf9a47ee328978c1cb32be0ba
Arun Menon <avmenon@codeaurora.org>
2015-07-12 21:13:22 -0700

    msm: vidc: Fix an issue with queueing of already mapped buffer
    Driver was holding a buffer whose ref count was 1. Since firmware had
    already released the reference of this buffer, there was no need for
    driver to hold it. By holding the buffer in driver, the buffer gets
    lost and is not returned back to client after a flush is issued.
    Fix this issue by holding the buffer in driver only if firmware is holding
    a reference of the mapped buffer, i.e, the ref count of the buffer is 2.
    
    Change-Id: I18f1de06eee72019f340f68407c07ec76f1539d1
    Signed-off-by: Arun Menon <avmenon@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc.c
+++ drivers/media/platform/msm/vidc/msm_vidc.c
@@ -456,6 +456,7 @@ int map_and_register_buf(struct msm_vidc_inst *inst, struct v4l2_buffer *b)
 			binfo, b->m.planes[0].reserved[0], b->type);
 
 	for (i = 0; i < b->length; ++i) {
+		rc = 0;
 		if (EXTRADATA_IDX(b->length) &&
 			(i == EXTRADATA_IDX(b->length)) &&
 			!b->m.planes[i].length) {
@@ -467,6 +468,8 @@ int map_and_register_buf(struct msm_vidc_inst *inst, struct v4l2_buffer *b)
 			dprintk(VIDC_DBG,
 				"This memory region has already been prepared\n");
 			rc = -EINVAL;
+			mutex_unlock(&inst->registeredbufs.lock);
+			goto exit;
 		}
 
 		if (temp && is_dynamic_output_buffer_mode(b, inst) && !i) {
@@ -484,17 +487,28 @@ int map_and_register_buf(struct msm_vidc_inst *inst, struct v4l2_buffer *b)
 				&inst->registeredbufs.list, list) {
 				if (iterator == temp) {
 					rc = buf_ref_get(inst, temp);
-					if (rc > 0) {
-						save_v4l2_buffer(b, temp);
-						rc = -EEXIST;
-					}
+					save_v4l2_buffer(b, temp);
 					break;
 				}
 			}
 		}
 		mutex_unlock(&inst->registeredbufs.lock);
-		if (rc < 0)
+		/*
+		 * rc == 1,
+		 * buffer is mapped, fw has released all reference, so skip
+		 * mapping and queue it immediately.
+		 *
+		 * rc == 2,
+		 * buffer is mapped and fw is holding a reference, hold it in
+		 * the driver and queue it later when fw has released
+		 */
+		if (rc == 1) {
+			rc = 0;
 			goto exit;
+		} else if (rc == 2) {
+			rc = -EEXIST;
+			goto exit;
+		}
 
 		same_fd_handle = get_same_fd_buffer(
 				&inst->registeredbufs,
