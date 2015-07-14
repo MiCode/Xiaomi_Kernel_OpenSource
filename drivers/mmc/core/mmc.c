730608256558f198d5b64dc7b7a2cca7fa626ebb
Dov Levenglick <dovl@codeaurora.org>
2015-07-14 14:10:57 +0300

    mmc: core: set MMC_PM_KEEP_POWER for certain Hynix mmc cards
    Certain Hynix eMMC 5.0 cards might reach a fast EOL if
    the card's power is disabled between CMD5 and CMD0
    (power off during reset).
    This patch sets the MMC_PM_KEEP_POWER flag to indicate
    that the card's power should be retained for suspend/resume
    sequences.
    
    Change-Id: I4bcc0f4bfd608745626816ca261369b432602c45
    Signed-off-by: Dov Levenglick <dovl@codeaurora.org>
    [xiaonian@codeaurora.org: fixed trivial merge conflicts]
    Signed-off-by: Xiaonian Wang <xiaonian@codeaurora.org>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -139,6 +139,12 @@ static void mmc_set_erase_size(struct mmc_card *card)
 	mmc_init_erase(card);
 }
 
+static void add_pm_flag_mmc(struct mmc_card *card, int data)
+{
+	if (mmc_card_mmc(card))
+		card->host->pm_flags |= data;
+}
+
 static const struct mmc_fixup mmc_fixups[] = {
 
 	/* avoid HPI for specific cards */
@@ -148,6 +154,9 @@ static const struct mmc_fixup mmc_fixups[] = {
 	/* Disable cache for specific cards */
 	MMC_FIXUP("MMC16G", CID_MANFID_KINGSTON, CID_OEMID_ANY,
 		add_quirk_mmc, MMC_QUIRK_CACHE_DISABLE),
+
+	MMC_FIXUP_EXT_CSD_REV(CID_NAME_ANY, CID_MANFID_HYNIX, CID_OEMID_ANY,
+		add_pm_flag_mmc, MMC_PM_KEEP_POWER, MMC_V5_0),
 
 	END_FIXUP
 };
