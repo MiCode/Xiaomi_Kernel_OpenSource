f03288ca84b9e443573f6ec3a234067d135a4268
Sujeet Kumar <ksujeet@codeaurora.org>
2015-07-14 11:41:55 +0530

    USB: composite: Increase the IN endpoint buffer allocation
    Some UDC may require allocation of some extra bytes for
    TX buffer due to hardware requirement. Add necessary
    changes for the same.
    
    Change-Id: Ia0ec21bbbe7d514f840f482d0e063559d4231338
    Signed-off-by: Sujeet Kumar <ksujeet@codeaurora.org>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -1726,7 +1726,8 @@ int composite_dev_prepare(struct usb_composite_driver *composite,
 	if (!cdev->req)
 		return -ENOMEM;
 
-	cdev->req->buf = kmalloc(USB_COMP_EP0_BUFSIZ, GFP_KERNEL);
+	cdev->req->buf = kmalloc(USB_COMP_EP0_BUFSIZ +
+				(gadget->extra_buf_alloc), GFP_KERNEL);
 	if (!cdev->req->buf)
 		goto fail;
 
