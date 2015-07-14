90305aebee4f8c88467a7c26a502852853476dd7
Liam Mark <lmark@codeaurora.org>
2015-07-14 15:42:17 -0700

    net: support __alloc_skb to always use GFP_DMA
    This makes it possible to ensure that any clients which use
    __alloc_skb always allocate memory from the DMA zone.
    
    Change-Id: I3b399b8da113e38a050b0b3e81eeaec549253bb9
    Signed-off-by: Praveen Kurapati <pkurapat@codeaurora.org>
    Signed-off-by: Liam Mark <lmark@codeaurora.org>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -183,6 +183,9 @@ struct sk_buff *__alloc_skb(unsigned int size, gfp_t gfp_mask,
 	u8 *data;
 	bool pfmemalloc;
 
+	if (IS_ENABLED(CONFIG_FORCE_ALLOC_FROM_DMA_ZONE))
+		gfp_mask |= GFP_DMA;
+
 	cache = (flags & SKB_ALLOC_FCLONE)
 		? skbuff_fclone_cache : skbuff_head_cache;
 
