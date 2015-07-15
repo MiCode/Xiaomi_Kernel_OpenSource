2be603cfa5f22f91a9ee4dcc41e47a33b9a9f26f
Ritesh Harjani <riteshh@codeaurora.org>
2015-07-15 13:31:06 +0530

    mmc: sdhci: Notify sdhci-msm for enhanced strobe
    Provide sdhci host ops of enhanced strobe to notify
    sdhci-msm on enabling/disabling cmdq. This is needed
    because of following:
    
    Before running CMDQ transfers in HS400 Enhanced Strobe mode,
    SW should write 3 to
    HC_VENDOR_SPECIFIC_FUNC3.CMDEN_HS400_INPUT_MASK_CNT register.
    Default reset value of this register is 2.
    
    Change-Id: I987605cd21f137dea49ddf3e8db3f1f41b5b501f
    Signed-off-by: Ritesh Harjani <riteshh@codeaurora.org>
    [subhashj@codeaurora.org: fixed trivial merge conflicts]
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -665,6 +665,7 @@ struct sdhci_ops {
 					  u32 type);
 	int	(*enable_controller_clock)(struct sdhci_host *host);
 	void	(*clear_set_dumpregs)(struct sdhci_host *host, bool set);
+	void	(*enhanced_strobe_mask)(struct sdhci_host *host, bool set);
 	void	(*dump_vendor_regs)(struct sdhci_host *host);
 	void	(*toggle_cdr)(struct sdhci_host *host, bool enable);
 	void	(*voltage_switch)(struct sdhci_host *host);
