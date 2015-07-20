bafe727af25bec6618c57f8b593e9577bdb6e4f5
Himanshu Aggarwal <haggarwa@codeaurora.org>
2015-07-20 19:32:49 +0530

    input: misc: hbtp_input: snapshot of driver from msm-4.4
    Driver snapshot from msm-4.4 to port to msm-4.9
    
    73b5daf input: misc: fix heap overflow issue in hbtp_input.c
    92a038f input: misc: add condition check for ddic reset
    c38f93d input: misc: hbtp_input: Add support for multiple AFE
    ed17546 input: misc: hbtp-input: fix uninitialized usage of a variable
    d7c55e4 input: misc: correct condition check for touch resolution
    ac34679 input: misc: add support for scaling X and Y touch co-ordinates
    de7fce6 input: misc: hbtp_input: Change regulator_set_optimum_mode
    d420671 input: misc: hbtp_input: Add digital regulator support
    8366ca7 input: hbtp_input: Port and add snapshot of changes from msm-3.10
    
    Signed-off-by: Alex Sarraf <asarraf@codeaurora.org>
    Change-Id: Id3e788fcbe4264c0e5df4962bf07f57084d3aa6e

---
+++ include/uapi/linux/hbtp_input.h
@@ -0,0 +1,73 @@
+#ifndef _UAPI_HBTP_INPUT_H
+#define _UAPI_HBTP_INPUT_H
+
+#include <linux/input.h>
+
+#define HBTP_MAX_FINGER		20
+#define HBTP_ABS_MT_FIRST	ABS_MT_TOUCH_MAJOR
+#define HBTP_ABS_MT_LAST	ABS_MT_TOOL_Y
+
+#define HBTP_EVENT_TYPE_DISPLAY	"EVENT_TYPE=HBTP_DISPLAY"
+
+struct hbtp_input_touch {
+	bool active;
+	__s32 tool;
+	__s32 x;
+	__s32 y;
+	__s32 pressure;
+	__s32 major;
+	__s32 minor;
+	__s32 orientation;
+};
+
+struct hbtp_input_mt {
+	__s32 num_touches;
+	struct hbtp_input_touch touches[HBTP_MAX_FINGER];
+	struct timeval time_val;
+};
+
+struct hbtp_input_absinfo {
+	bool  active;
+	__u16 code;
+	__s32 minimum;
+	__s32 maximum;
+};
+
+enum hbtp_afe_power_cmd {
+	HBTP_AFE_POWER_ON,
+	HBTP_AFE_POWER_OFF,
+};
+
+struct hbtp_input_key {
+	__u32 code;
+	__s32 value;
+};
+
+enum hbtp_afe_signal {
+	HBTP_AFE_SIGNAL_ON_RESUME,
+	HBTP_AFE_SIGNAL_ON_SUSPEND,
+};
+
+enum hbtp_afe_power_ctrl {
+	HBTP_AFE_POWER_ENABLE_SYNC,
+	HBTP_AFE_POWER_ENABLE_SYNC_SIGNAL,
+};
+
+
+/* ioctl */
+#define HBTP_INPUT_IOCTL_BASE	'T'
+#define HBTP_SET_ABSPARAM	_IOW(HBTP_INPUT_IOCTL_BASE, 201, \
+					struct hbtp_input_absinfo *)
+#define HBTP_SET_TOUCHDATA	_IOW(HBTP_INPUT_IOCTL_BASE, 202, \
+					struct hbtp_input_mt)
+#define HBTP_SET_POWERSTATE	_IOW(HBTP_INPUT_IOCTL_BASE, 203, \
+					enum hbtp_afe_power_cmd)
+#define HBTP_SET_KEYDATA	_IOW(HBTP_INPUT_IOCTL_BASE, 204, \
+					struct hbtp_input_key)
+#define HBTP_SET_SYNCSIGNAL	_IOW(HBTP_INPUT_IOCTL_BASE, 205, \
+					enum hbtp_afe_signal)
+#define HBTP_SET_POWER_CTRL	_IOW(HBTP_INPUT_IOCTL_BASE, 206, \
+					enum hbtp_afe_power_ctrl)
+
+#endif	/* _UAPI_HBTP_INPUT_H */
+
