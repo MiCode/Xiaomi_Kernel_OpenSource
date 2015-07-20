440da44a6a611bc6c6344550f4107c5cf638a92b
Daniel Campello <campello@google.com>
2015-07-20 16:23:50 -0700

    ANDROID: Included sdcardfs source code for kernel 3.0
    Only included the source code as is for kernel 3.0. Following patches
    take care of porting this file system to version 3.10.
    
    Change-Id: I09e76db77cd98a059053ba5b6fd88572a4b75b5b
    Signed-off-by: Daniel Campello <campello@google.com>

--- include/linux/namei.h
+++ include/linux/namei.h
@@ -46,6 +46,10 @@ enum {LAST_NORM, LAST_ROOT, LAST_DOT, LAST_DOTDOT, LAST_BIND};
 #define LOOKUP_EMPTY		0x4000
 #define LOOKUP_DOWN		0x8000
 
+#ifdef CONFIG_SDCARD_FS_CI_SEARCH
+#define LOOKUP_CASE_INSENSITIVE 0x8000
+#endif
+
 extern int path_pts(struct path *path);
 
 extern int user_path_at_empty(int, const char __user *, unsigned, struct path *, int *empty);
