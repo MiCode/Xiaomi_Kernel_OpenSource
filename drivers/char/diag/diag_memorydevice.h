fb87dd0fb86a2ff9266b2c5e2befa7c48a0428d0
Sreelakshmi Gownipalli <sgownipa@codeaurora.org>
2015-07-20 16:15:20 -0700

    diag: Donot drop the data in memory device mode
    Copy the data from buf table in diag_md_copy_to_user() if the
    userspace buffer has space. If the data copy fails then drain
    the data once the userpace buf is available instead of dropping
    the data.
    
    Change-Id: I6a24b13837ce7a07ea0d6bd2954b40a776c6ae0b
    Signed-off-by: Sreelakshmi Gownipalli <sgownipa@codeaurora.org>

--- drivers/char/diag/diag_memorydevice.h
+++ drivers/char/diag/diag_memorydevice.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -51,5 +51,5 @@ void diag_md_open_all(void);
 void diag_md_close_all(void);
 int diag_md_register(int id, int ctx, struct diag_mux_ops *ops);
 int diag_md_write(int id, unsigned char *buf, int len, int ctx);
-int diag_md_copy_to_user(char __user *buf, int *pret);
+int diag_md_copy_to_user(char __user *buf, int *pret, size_t buf_size);
 #endif
