94d46d9e6ba70841a930044d572bf2795b5c4a5d
Sunil Khatri <sunilkh@codeaurora.org>
2015-07-20 19:57:21 +0530

    msm: kgsl: Do not submit detached context switching commands
    Do not submit commands from a detached context to
    avoid invalid page table programming.
    
    CRs-Fixed: 847754
    Change-Id: Ibc7e9bb35b5374296c36ae542981d616c2ab92f7
    Signed-off-by: Sunil Khatri <sunilkh@codeaurora.org>
    Signed-off-by: Harshdeep Dhatt <hdhatt@codeaurora.org>

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -492,6 +492,11 @@ static int sendcmd(struct adreno_device *adreno_dev,
 		return -EBUSY;
 	}
 
+	if (kgsl_context_detached(cmdbatch->context)) {
+		mutex_unlock(&device->mutex);
+		return -ENOENT;
+	}
+
 	dispatcher->inflight++;
 	dispatch_q->inflight++;
 
