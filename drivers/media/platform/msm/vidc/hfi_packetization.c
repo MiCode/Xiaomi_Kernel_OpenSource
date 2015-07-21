6358b9e2888bbe61aa33de5fc3a323e50584f19a
Vinay Kalia <vkalia@codeaurora.org>
2015-07-21 11:32:39 -0700

    msm: vidc: Route firmware messages to both QDSS and DBGQ
    Firmware can be programmed to route messages to both QDSS
    and DBGQ. Fix this in driver. Following debugfs command can
    be used to achieve this.
    echo 0x3 > /d/msm_vidc/fw_debug_mode
    
    Change-Id: Ib303fbdf5119b2c4a5140eb37d67991c2a0af12f
    Signed-off-by: Vinay Kalia <vkalia@codeaurora.org>

--- drivers/media/platform/msm/vidc/hfi_packetization.c
+++ drivers/media/platform/msm/vidc/hfi_packetization.c
@@ -262,7 +262,8 @@ int create_pkt_cmd_sys_debug_config(
 	hfi = (struct hfi_debug_config *) &pkt->rg_property_data[1];
 	hfi->debug_config = mode;
 	hfi->debug_mode = HFI_DEBUG_MODE_QUEUE;
-	if (msm_vidc_fw_debug_mode <= HFI_DEBUG_MODE_QDSS)
+	if (msm_vidc_fw_debug_mode
+			<= (HFI_DEBUG_MODE_QUEUE | HFI_DEBUG_MODE_QDSS))
 		hfi->debug_mode = msm_vidc_fw_debug_mode;
 	return 0;
 }
