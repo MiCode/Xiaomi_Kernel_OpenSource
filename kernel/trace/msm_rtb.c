05635f8cd708c048bb1c68f425ae9c073ca50b51
Se Wang (Patrick) Oh <sewango@codeaurora.org>
2015-07-21 19:23:48 -0700

    trace: rtb: disable RTB in the first panic notifier
    As the priority of RTB panic notifier was zero, it was
    not guaranteed to disable RTB right after kernel panic.
    So RTB log buffer could be flooded with some I/O operations
    after panic. By setting the priority of RTB panic notifier
    to the highest value, make sure RTB is disabled right after
    a kernel panic.
    
    Change-Id: If9efc2ec31efa6aa17e92b2b01e81ab4df6d1730
    Signed-off-by: Se Wang (Patrick) Oh <sewango@codeaurora.org>

--- kernel/trace/msm_rtb.c
+++ kernel/trace/msm_rtb.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -88,6 +88,7 @@ static int msm_rtb_panic_notifier(struct notifier_block *this,
 
 static struct notifier_block msm_rtb_panic_blk = {
 	.notifier_call  = msm_rtb_panic_notifier,
+	.priority = INT_MAX,
 };
 
 int notrace msm_rtb_event_should_log(enum logk_event_type log_type)
