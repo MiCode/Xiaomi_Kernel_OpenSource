e9a33bcd2f02f05d55e05d5ad1e64ca9ad2c4c2a
Harout Hedeshian <harouth@codeaurora.org>
2015-07-27 10:32:01 -0600

    net: add a per-cpu counter for the number of frames coalesced in GRO
    A low cost method of determining GRO statistics is required. This
    change introduces a new counter which tracks whenever GRO coalesces
    ingress packets. The counter is per-CPU and exposed in
    /proc/net/softnet_stat as the last column of data. No user space
    impact is expected as a result of this change. However, this change
    should be reverted if legacy tools have problems with the new column
    in softnet_stat.
    
    Change-Id: I05965c0cb150947935d5977884cc4d583b37131d
    Signed-off-by: Harout Hedeshian <harouth@codeaurora.org>
    [subashab@codeaurora.org: resolve trivial merge conflicts]
    Signed-off-by: Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -2526,6 +2526,8 @@ struct softnet_data {
 	unsigned int		time_squeeze;
 	unsigned int		cpu_collision;
 	unsigned int		received_rps;
+	unsigned int		gro_coalesced;
+
 #ifdef CONFIG_RPS
 	struct softnet_data	*rps_ipi_list;
 #endif
