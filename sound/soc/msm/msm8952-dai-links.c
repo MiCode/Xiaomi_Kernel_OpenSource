05f59140ca9aaec360041195dbc4728750ac1d73
Yeleswarapu Nagaradhesh <nagaradh@codeaurora.org>
2015-07-28 15:19:02 +0530

    ASoC: msm: remove hdmi fai from common be dais
    8952 does not support HDMI audio and hence move
    hdmi be dais to tasha specific dailinks.
    
    Change-Id: I3ea95afa9b20e64f55a6f12af785c00a8726ad99
    Signed-off-by: Yeleswarapu Nagaradhesh <nagaradh@codeaurora.org>

--- sound/soc/msm/msm8952-dai-links.c
+++ sound/soc/msm/msm8952-dai-links.c
@@ -226,6 +226,20 @@ static struct snd_soc_dai_link msm8952_tasha_be_dai[] = {
 		.ops = &msm8952_slimbus_be_ops,
 		.ignore_suspend = 1,
 	},
+	{
+		.name = LPASS_BE_MI2S_HDMI_RX,
+		.stream_name = "MI2S HDMI Playback",
+		.cpu_dai_name = "msm-dai-q6-mi2s-hdmi.4118",
+		.platform_name = "msm-pcm-routing",
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.codec_name = "snd-soc-dummy",
+		.no_pcm = 1,
+		.be_id = MSM_BACKEND_DAI_MI2S_HDMI_RX,
+		.be_hw_params_fixup = msm_be_hw_params_fixup,
+		.ops = &msm8952_quin_mi2s_be_ops,
+		.ignore_pmdown_time = 1, /* dai link has playback support */
+		.ignore_suspend = 1,
+	},
 };
 
 static struct snd_soc_dai_link msm8952_tomtom_fe_dai[] = {
@@ -1129,21 +1143,6 @@ static struct snd_soc_dai_link msm8952_common_be_dai[] = {
 		.ignore_pmdown_time = 1, /* dai link has playback support */
 		.ignore_suspend = 1,
 	},
-	{
-		.name = LPASS_BE_MI2S_HDMI_RX,
-		.stream_name = "MI2S HDMI Playback",
-		.cpu_dai_name = "msm-dai-q6-mi2s-hdmi.4118",
-		.platform_name = "msm-pcm-routing",
-		.codec_dai_name = "snd-soc-dummy-dai",
-		.codec_name = "snd-soc-dummy",
-		.no_pcm = 1,
-		.be_id = MSM_BACKEND_DAI_MI2S_HDMI_RX,
-		.be_hw_params_fixup = msm_be_hw_params_fixup,
-		.ops = &msm8952_quin_mi2s_be_ops,
-		.ignore_pmdown_time = 1, /* dai link has playback support */
-		.ignore_suspend = 1,
-	},
-
 };
 
 static struct snd_soc_aux_dev msm895x_aux_dev[] = {
