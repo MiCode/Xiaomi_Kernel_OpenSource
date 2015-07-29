173e94b835e20a4f20847ff388079a3abfd50550
Ashray Kulkarni <ashrayk@codeaurora.org>
2015-07-29 16:54:02 -0700

    msm: vidc: add missing v4l2_fh_exit in driver
    During driver close v4l2 event handler is deleted but v4l2_fh_exit()
    is not called. This may result in events getting queued even after
    v4l2_fh is deleted. This change adds v4l2_fh_exit() call which
    unsubscribes all events preventing queuing further events.
    
    Change-Id: Ic5b9b12f005ae70e58afcdbe74d52bef3fceb79a
    Signed-off-by: Ashray Kulkarni <ashrayk@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc.c
+++ drivers/media/platform/msm/vidc/msm_vidc.c
@@ -1419,6 +1419,7 @@ int msm_vidc_close(void *instance)
 		return -EINVAL;
 
 	v4l2_fh_del(&inst->event_handler);
+	v4l2_fh_exit(&inst->event_handler);
 
 	mutex_lock(&inst->registeredbufs.lock);
 	list_for_each_entry_safe(bi, dummy, &inst->registeredbufs.list, list) {
