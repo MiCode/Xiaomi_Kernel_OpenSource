c5c4e693871337bf8270a2bc9f3e85d99c805212
Chunmei Cai <ccai@codeaurora.org>
2015-07-30 12:19:13 +0800

    power_supply: add SAFETY_TIMER_EXPIRED property
    Add the property SAFETY_TIMER_EXPIRED to the power supply
    framework in order to support detecting damaged battery.
    
    CRs-Fixed: 866074
    Change-Id: Icefb268a0478f7d9718e3167846fb82899fe8780
    Signed-off-by: Chunmei Cai <ccai@codeaurora.org>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -226,6 +226,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(flash_active),
 	POWER_SUPPLY_ATTR(force_tlim),
 	POWER_SUPPLY_ATTR(cycle_count_id),
+	POWER_SUPPLY_ATTR(safety_timer_expired),
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_ATTR(charge_counter_ext),
 	/* Properties of type `const char *' */
--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -178,6 +178,7 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_FLASH_ACTIVE,
 	POWER_SUPPLY_PROP_FORCE_TLIM,
 	POWER_SUPPLY_PROP_CYCLE_COUNT_ID,
+	POWER_SUPPLY_PROP_SAFETY_TIMER_EXPIRED,
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_PROP_CHARGE_COUNTER_EXT,
 	/* Properties of type `const char *' */
