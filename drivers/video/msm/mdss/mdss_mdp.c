d63f548a48aaf77a6591aa7f8d2cfdfa733c37e7
Dhaval Patel <pdhaval@codeaurora.org>
2015-08-03 15:22:52 -0700

    msm: mdss: capture vbif dump with mdss dump when panic happen
    Capture real time vbif and non-real time vbif dump with mdss
    dump when mdp related fatal error occur.
    
    Change-Id: Ia44402a0272938862569d1ed43871e6ecca17965
    Signed-off-by: Dhaval Patel <pdhaval@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp.c
+++ drivers/video/msm/mdss/mdss_mdp.c
@@ -3408,7 +3408,7 @@ int mdss_mdp_wait_for_xin_halt(u32 xin_id, bool is_vbif_nrt)
 	if (rc == -ETIMEDOUT) {
 		pr_err("VBIF client %d not halting. TIMEDOUT.\n",
 			xin_id);
-		MDSS_XLOG_TOUT_HANDLER("mdp", "vbif", "panic");
+		MDSS_XLOG_TOUT_HANDLER("mdp", "vbif", "vbif_nrt", "panic");
 	} else {
 		pr_debug("VBIF client %d is halted\n", xin_id);
 	}
