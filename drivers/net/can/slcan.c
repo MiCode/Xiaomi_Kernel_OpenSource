79e215ad098a57b3bc1e82b6891d999ddfc77f73
Sasha Levin <sasha.levin@oracle.com>
2015-08-04 13:32:39 -0400

    Revert "can: fix loss of CAN frames in raw_rcv"
    This reverts commit c215cf258214858a5a6c3e63cd7ee78b92d210b2.
    
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>

--- drivers/net/can/slcan.c
+++ drivers/net/can/slcan.c
@@ -210,7 +210,6 @@ static void slc_bump(struct slcan *sl)
 	if (!skb)
 		return;
 
-	__net_timestamp(skb);
 	skb->dev = sl->dev;
 	skb->protocol = htons(ETH_P_CAN);
 	skb->pkt_type = PACKET_BROADCAST;
