d300470c8abd0e1bb6dc50be6402a474ab8dd744
Sahitya Tummala <stummala@codeaurora.org>
2015-08-06 13:58:47 +0530

    mmc: sdhci: Add notify_load host->op
    Add notify_load host->op to enable host controllers to
    scale up/down necessary clocks based on the load.
    
    Change-Id: I39d31d5343d8aa453f29294e340e52d94bfd0ade
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>
    [subhashj@codeaurora.org: fixed trivial merge conflicts]
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1701,6 +1701,9 @@ static int sdhci_notify_load(struct mmc_host *mmc, enum mmc_load state)
 		break;
 	}
 
+	if (host->ops->notify_load)
+		err = host->ops->notify_load(host, state);
+
 	return err;
 }
 
