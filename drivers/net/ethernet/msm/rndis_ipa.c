9943521153fb74fe3507455563dbd17e558c467e
Skylar Chang <chiaweic@codeaurora.org>
2015-08-10 15:08:00 -0700

    msm: rndis_ipa: use netif_rx_ni to push packets to network stack
    rndis_ipa indicates packets to the network stack in process
    context so netif_rx_ni is the correct API to use. Using
    netif_rx in this context can cause fluctuations in latency
    of NET_RX being serviced.
    
    Change-Id: I1f8105a85b601819d4e015910520d2851a762b3b
    Signed-off-by: Skylar Chang <chiaweic@codeaurora.org>

--- drivers/net/ethernet/msm/rndis_ipa.c
+++ drivers/net/ethernet/msm/rndis_ipa.c
@@ -1120,9 +1120,9 @@ static void rndis_ipa_packet_receive_notify(void *private,
 		return;
 	}
 
-	result = netif_rx(skb);
+	result = netif_rx_ni(skb);
 	if (result)
-		RNDIS_IPA_ERROR("fail on netif_rx\n");
+		RNDIS_IPA_ERROR("fail on netif_rx_ni\n");
 	rndis_ipa_ctx->net->stats.rx_packets++;
 	rndis_ipa_ctx->net->stats.rx_bytes += packet_len;
 
