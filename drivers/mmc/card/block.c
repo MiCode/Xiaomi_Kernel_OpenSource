38c20819fce7eb22a3d0f560049fc8f5c0292bf7
Subhash Jadavani <subhashj@codeaurora.org>
2015-08-12 17:53:21 -0700

    mmc: queue: fix the cmdq thread wake up handling
    If request has to be requeued (due to any DCMD commmand pending or cmdq
    being halted) and if we change the task status to interruptible before
    going to sleep then thread may not wakeup again. Note that
    blk_requeue_request() doesn't trigger ->request_fn() again to wakeup
    the thread.
    
    Fix this issue by making cmdq thread wait for the completion of DCMD
    or until the cmdq is unhalted. This change also simplifies the
    cmdq thread function.
    
    Change-Id: Iebffc993241e5fadb2962fedc44576566dc66e9c
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>
    Signed-off-by: Venkat Gopalakrishnan <venkatg@codeaurora.org>

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -1643,9 +1643,7 @@ clear_dcmd:
 	}
 out:
 	blk_end_request(req, err, blk_rq_bytes(req));
-
-	if (test_and_clear_bit(0, &ctx_info->req_starved))
-		blk_run_queue(mq->queue);
+	wake_up(&ctx_info->wait);
 	mmc_put_card(card);
 	return err ? 1 : 0;
 }
@@ -1759,9 +1757,7 @@ clear_dcmd:
 	}
 out:
 	blk_end_request(req, err, blk_rq_bytes(req));
-
-	if (test_and_clear_bit(0, &ctx_info->req_starved))
-		blk_run_queue(mq->queue);
+	wake_up(&ctx_info->wait);
 	mmc_put_card(card);
 	return err ? 1 : 0;
 }
@@ -3141,10 +3137,9 @@ unhalt:
 out:
 	host->err_mrq = NULL;
 	pm_runtime_mark_last_busy(&card->dev);
+	clear_bit(CMDQ_STATE_ERR, &ctx_info->curr_state);
+	wake_up(&ctx_info->wait);
 	__mmc_put_card(card);
-
-	if (test_and_clear_bit(0, &ctx_info->req_starved))
-		blk_run_queue(mrq->req->q);
 }
 
 /* invoked by block layer in softirq context */
@@ -3200,9 +3195,8 @@ void mmc_blk_cmdq_complete_rq(struct request *rq)
 out:
 
 	mmc_cmdq_clk_scaling_stop_busy(host, true, is_dcmd);
-	if (!test_bit(CMDQ_STATE_ERR, &ctx_info->curr_state) &&
-			test_and_clear_bit(0, &ctx_info->req_starved))
-		blk_run_queue(mq->queue);
+	if (!test_bit(CMDQ_STATE_ERR, &ctx_info->curr_state))
+		wake_up(&ctx_info->wait);
 
 	mmc_put_card(host->card);
 	if (!ctx_info->active_reqs)
