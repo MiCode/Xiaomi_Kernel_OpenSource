36dbc03c812ebf0107739c3f272d5c84c53ddef6
Vikram Mulukutla <markivx@codeaurora.org>
2015-08-19 13:13:20 -0700

    sched: fair: Return current CPU if search_cpu(s) is empty
    If a task is affined to CPUs that are offline, best_small_task_cpu
    will attempt to select a cpu from an empty mask, resulting in
    corruption/crashes. Fix this by returning the current CPU if
    search_cpu is empty.
    
    Change-Id: Ib56a8a2513b0ae7ec8414b5ed3134e88b24dd2ac
    Signed-off-by: Vikram Mulukutla <markivx@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1814,6 +1814,8 @@ static int best_small_task_cpu(struct task_struct *p, int sync)
 	hmp_capable = !cpumask_full(&temp);
 
 	cpumask_and(&search_cpu, tsk_cpus_allowed(p), cpu_online_mask);
+	if (unlikely(cpumask_empty(&search_cpu)))
+		return task_cpu(p);
 	if (unlikely(!cpumask_test_cpu(i, &search_cpu)))
 		i = cpumask_first(&search_cpu);
 
