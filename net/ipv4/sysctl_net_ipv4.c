547ea416c36c90bc05d1006518d689af04a9fa75
Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
2015-08-19 16:33:59 -0600

    net: Fail explicit bind to local reserved ports
    Reserved ports may have some special use cases which are not suitable
    for use by general userspace applications. Currently, ports specified
    in ip_local_reserved_ports will not be returned only in case of
    automatic port assignment.
    
    Add a boolean sysctl flag 'reserved_port_bind'. Default value is 1
    which preserves the existing behavior. Setting the value to 0 will
    prevent userspace applications from binding to these ports even when
    they are explicitly requested.
    
    BUG=20663075
    Change-Id: Ib1071ca5bd437cd3c4f71b56147e4858f3b9ebec
    Signed-off-by: Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
    Signed-off-by: Mekala Natarajan <mekalan@codeaurora.org>

--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -507,6 +507,13 @@ static struct ctl_table ipv4_table[] = {
 		.proc_handler	= proc_do_large_bitmap,
 	},
 	{
+		.procname	= "reserved_port_bind",
+		.data		= &sysctl_reserved_port_bind,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec
+	},
+	{
 		.procname	= "igmp_max_memberships",
 		.data		= &sysctl_igmp_max_memberships,
 		.maxlen		= sizeof(int),
--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -106,6 +106,7 @@
 #include <net/route.h>
 #include <net/checksum.h>
 #include <net/xfrm.h>
+#include <net/ip.h>
 #include <trace/events/udp.h>
 #include <linux/static_key.h>
 #include <trace/events/skb.h>
@@ -254,6 +255,11 @@ int udp_lib_get_port(struct sock *sk, unsigned short snum,
 	} else {
 		hslot = udp_hashslot(udptable, net, snum);
 		spin_lock_bh(&hslot->lock);
+
+		if (inet_is_reserved_local_port(snum) &&
+		    !sysctl_reserved_port_bind)
+			goto fail_unlock;
+
 		if (hslot->count > 10) {
 			int exist;
 			unsigned int slot2 = udp_sk(sk)->udp_portaddr_hash ^ snum;
