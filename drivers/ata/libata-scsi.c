514f0a944c001cdad24dce56757bfc3bb991c925
Sasha Levin <sasha.levin@oracle.com>
2015-08-22 15:30:23 -0400

    Revert "libata: add ATA_HORKAGE_NOTRIM"
    This reverts commit e7a84605061b205350654641d823e1ca9589ec24.

--- drivers/ata/libata-scsi.c
+++ drivers/ata/libata-scsi.c
@@ -2514,8 +2514,7 @@ static unsigned int ata_scsiop_read_cap(struct ata_scsi_args *args, u8 *rbuf)
 		rbuf[14] = (lowest_aligned >> 8) & 0x3f;
 		rbuf[15] = lowest_aligned;
 
-		if (ata_id_has_trim(args->id) &&
-		    !(dev->horkage & ATA_HORKAGE_NOTRIM)) {
+		if (ata_id_has_trim(args->id)) {
 			rbuf[14] |= 0x80; /* TPE */
 
 			if (ata_id_has_zero_after_trim(args->id))
