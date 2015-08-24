ffec9a9df08f7798748a6828a4c803b061bbee7d
Yaniv Gardi <ygardi@codeaurora.org>
2015-08-24 17:08:51 +0300

    Revert "phy: qcom-ufs: add ufs phy type selection"
    This reverts commit 6ed4bd3af2c153ae3246d89c2d62cfe7919d0320
    ("phy: qcom-ufs: add ufs phy type selection")
    
    Since a single boot.img should support all phy-qcom-ufs configurations,
    all phy-qcom-ufs files should be compiled all together.
    
    Change-Id: I81b3891fb44d927046f39bbe72b5ed24dd9d99d2
    Signed-off-by: Yaniv Gardi <ygardi@codeaurora.org>
    [venkatg@codeaurora.org: resolved trivial merge conflicts]
    Signed-off-by: Venkat Gopalakrishnan <venkatg@codeaurora.org>
    [cang@codeaurora.org: Resolved merge conflicts and fixed
    kernel compilation errors according to upstream changes]
    Signed-off-by: Can Guo <cang@codeaurora.org>

--- drivers/phy/qualcomm/phy-qcom-ufs-qmp-v3.c
+++ drivers/phy/qualcomm/phy-qcom-ufs-qmp-v3.c
@@ -48,14 +48,14 @@ static int ufs_qcom_phy_qmp_v3_init(struct phy *generic_phy)
 	struct ufs_qcom_phy *phy_common = &phy->common_cfg;
 	int err;
 
-	err = ufs_qcom_phy_init_clks(generic_phy, phy_common);
+	err = ufs_qcom_phy_init_clks(phy_common);
 	if (err) {
 		dev_err(phy_common->dev, "%s: ufs_qcom_phy_init_clks() failed %d\n",
 			__func__, err);
 		goto out;
 	}
 
-	err = ufs_qcom_phy_init_vregulators(generic_phy, phy_common);
+	err = ufs_qcom_phy_init_vregulators(phy_common);
 	if (err) {
 		dev_err(phy_common->dev, "%s: ufs_qcom_phy_init_vregulators() failed %d\n",
 			__func__, err);
@@ -64,6 +64,11 @@ static int ufs_qcom_phy_qmp_v3_init(struct phy *generic_phy)
 
 out:
 	return err;
+}
+
+static int ufs_qcom_phy_qmp_v3_exit(struct phy *generic_phy)
+{
+	return 0;
 }
 
 static
@@ -147,7 +152,7 @@ out:
 
 struct phy_ops ufs_qcom_phy_qmp_v3_phy_ops = {
 	.init		= ufs_qcom_phy_qmp_v3_init,
-	.exit		= ufs_qcom_phy_exit,
+	.exit		= ufs_qcom_phy_qmp_v3_exit,
 	.power_on	= ufs_qcom_phy_power_on,
 	.power_off	= ufs_qcom_phy_power_off,
 	.owner		= THIS_MODULE,
@@ -194,21 +199,6 @@ out:
 	return err;
 }
 
-static int ufs_qcom_phy_qmp_v3_remove(struct platform_device *pdev)
-{
-	struct device *dev = &pdev->dev;
-	struct phy *generic_phy = to_phy(dev);
-	struct ufs_qcom_phy *ufs_qcom_phy = get_ufs_qcom_phy(generic_phy);
-	int err = 0;
-
-	err = ufs_qcom_phy_remove(generic_phy, ufs_qcom_phy);
-	if (err)
-		dev_err(dev, "%s: ufs_qcom_phy_remove failed = %d\n",
-			__func__, err);
-
-	return err;
-}
-
 static const struct of_device_id ufs_qcom_phy_qmp_v3_of_match[] = {
 	{.compatible = "qcom,ufs-phy-qmp-v3"},
 	{},
@@ -217,7 +207,6 @@ MODULE_DEVICE_TABLE(of, ufs_qcom_phy_qmp_v3_of_match);
 
 static struct platform_driver ufs_qcom_phy_qmp_v3_driver = {
 	.probe = ufs_qcom_phy_qmp_v3_probe,
-	.remove = ufs_qcom_phy_qmp_v3_remove,
 	.driver = {
 		.of_match_table = ufs_qcom_phy_qmp_v3_of_match,
 		.name = "ufs_qcom_phy_qmp_v3",
