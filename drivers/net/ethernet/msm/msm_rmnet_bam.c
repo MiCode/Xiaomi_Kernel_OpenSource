e992c3b4fa8ff7947d5617a7bf2ac6c5e6c47d13
Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
2015-08-24 21:45:15 -0600

    msm_rmnet_bam: set name_assign_type in alloc_netdev
    Commit c835a6773314 ("net: set name_assign_type in alloc_netdev()")
    extended alloc_netdev to take name_assign_type as argument. Pass this
    argument to ensure successful compilation of msm_rmnet_bam.
    
    Change-Id: If7c7cc3c6e89432910e13eff00dd8646a898a135
    Acked-by: Sharat Masetty <smasetty@qti.qualcomm.com>
    Signed-off-by: Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>

--- drivers/net/ethernet/msm/msm_rmnet_bam.c
+++ drivers/net/ethernet/msm/msm_rmnet_bam.c
@@ -811,7 +811,7 @@ static int bam_rmnet_probe(struct platform_device *pdev)
 	else
 		dev_name = "rev_rmnet%d";
 
-	dev = alloc_netdev(sizeof(*p), dev_name, rmnet_setup);
+	dev = alloc_netdev(sizeof(*p), dev_name, NET_NAME_ENUM, rmnet_setup);
 	if (!dev) {
 		pr_err("%s: no memory for netdev %d\n", __func__, i);
 		return -ENOMEM;
