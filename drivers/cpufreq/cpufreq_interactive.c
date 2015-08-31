76faa288c1c4aaf91f212247506e8e08a11da2af
Junjie Wu <junjiew@codeaurora.org>
2015-08-31 14:49:43 -0700

    cpufreq: interactive: Fix load in cpufreq_interactive_cpuload event
    CPU load is now normalized to per-policy target_load, instead of
    current frequency of CPU. Fix cpufreq_interactive_cpuload accordingly
    so that its load number matches other cpufreq interactive events like
    cpufreq_interactive_target/notyet/already.
    
    Change-Id: I0685b5930ad1bac01819e96fcdfc181167d4dae0
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -480,7 +480,7 @@ static void __cpufreq_interactive_timer(unsigned long data, bool is_notif)
 		tmploadadjfreq = (unsigned int)cputime_speedadj * 100;
 		pcpu->loadadjfreq = tmploadadjfreq;
 		trace_cpufreq_interactive_cpuload(i, tmploadadjfreq /
-						  ppol->policy->cur);
+						  ppol->target_freq);
 
 		if (tmploadadjfreq > loadadjfreq) {
 			loadadjfreq = tmploadadjfreq;
