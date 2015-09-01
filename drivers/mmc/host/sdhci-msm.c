8db42d4aac1b60e1476812c17da2f9484078b557
Sahitya Tummala <stummala@codeaurora.org>
2015-09-01 16:46:09 +0530

    mmc: sdhci-msm-ice: implement crypto_cfg_reset host operation
    When encryption/decryption is enabled in CQ mode, the
    legacy commands that are sent in HALT state will use
    different slot other than slot 0 for crypto configuration
    information. The slot that is selected depends on the last
    slot that was used when it is in CQ mode. This is causing
    the data of legacy commands to be encrypted/decrypted based
    on the wrong slot usage for crypto config details. Hence,
    clear the crypto configuration of the slot used in CQ mode
    whenever it gets completed.
    
    Change-Id: I6817de46d895b61f410dd732be57ba60efb58fb2
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -3143,6 +3143,7 @@ void sdhci_msm_reset_workaround(struct sdhci_host *host, u32 enable)
 
 static struct sdhci_ops sdhci_msm_ops = {
 	.crypto_engine_cfg = sdhci_msm_ice_cfg,
+	.crypto_cfg_reset = sdhci_msm_ice_cfg_reset,
 	.crypto_engine_reset = sdhci_msm_ice_reset,
 	.set_uhs_signaling = sdhci_msm_set_uhs_signaling,
 	.check_power_status = sdhci_msm_check_power_status,
