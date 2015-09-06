adbfdf23675f6b0213100b86df0f0576615a833f
Sultan Qasim Khan <sultanqasim@gmail.com>
2015-09-06 17:59:58 -0400

    block: zram: Backport from Linux 4.1
    Change-Id: Ieadbcae0ad55a1d9dd1ae6ea621413eee8f471f5
    
    Conflicts:
    	drivers/block/zram/Kconfig
    	drivers/staging/Kconfig
    	drivers/staging/Makefile
    	drivers/staging/zram/zram_drv.c

--- include/linux/device.h
+++ include/linux/device.h
@@ -505,6 +505,12 @@ ssize_t device_store_bool(struct device *dev, struct device_attribute *attr,
 
 #define DEVICE_ATTR(_name, _mode, _show, _store) \
 	struct device_attribute dev_attr_##_name = __ATTR(_name, _mode, _show, _store)
+#define DEVICE_ATTR_RW(_name) \
+	struct device_attribute dev_attr_##_name = __ATTR_RW(_name)
+#define DEVICE_ATTR_RO(_name) \
+	struct device_attribute dev_attr_##_name = __ATTR_RO(_name)
+#define DEVICE_ATTR_WO(_name) \
+	struct device_attribute dev_attr_##_name = __ATTR_WO(_name)
 #define DEVICE_ULONG_ATTR(_name, _mode, _var) \
 	struct dev_ext_attribute dev_attr_##_name = \
 		{ __ATTR(_name, _mode, device_show_ulong, device_store_ulong), &(_var) }
--- include/linux/sysfs.h
+++ include/linux/sysfs.h
@@ -79,6 +79,15 @@ struct attribute_group {
 	.show	= _name##_show,					\
 }
 
+
+#define __ATTR_WO(_name) {						\
+	.attr	= { .name = __stringify(_name), .mode = S_IWUSR },	\
+	.store	= _name##_store,					\
+}
+
+#define __ATTR_RW(_name) __ATTR(_name, (S_IWUSR | S_IRUGO),		\
+			 _name##_show, _name##_store)
+
 #define __ATTR_NULL { .attr = { .name = NULL } }
 
 #ifdef CONFIG_DEBUG_LOCK_ALLOC
