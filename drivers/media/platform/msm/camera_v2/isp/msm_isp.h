1769fd9372c958c26479ab3aebfb538bc40cde5b
Alok Kediya <kediya@codeaurora.org>
2015-09-07 15:04:39 +0530

    msm: isp: update stats AB/IB value.
    calculate the AB/IB values in userspace and update in kernel.
    
    Change-Id: I13c83e5e98f61a0552408e641b774a147a395baf
    Signed-off-by: Alok Kediya <kediya@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp.h
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp.h
@@ -408,6 +408,8 @@ struct msm_vfe_src_info {
 	uint32_t input_format;/*V4L2 pix format with bayer pattern*/
 	uint32_t last_updt_frm_id;
 	uint32_t sof_counter_step;
+	uint64_t stats_ab;
+	uint64_t stats_ib;
 	enum msm_vfe_dual_hw_type dual_hw_type;
 	struct msm_vfe_dual_hw_ms_info dual_hw_ms_info;
 };
