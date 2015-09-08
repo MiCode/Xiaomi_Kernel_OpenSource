03307a0339015822de9547a4b8b7d2fb22e0cf87
Susheel Khiani <skhiani@codeaurora.org>
2015-09-08 15:05:43 +0530

    msm: Allow lowmem to be non contiguous and mixed
    Currently on 32 bit systems, virtual space above
    PAGE_OFFSET is reserved for direct mapped lowmem
    and part of virtual address space is reserved for
    vmalloc. We want to optimize such as to have as
    much direct mapped memory as possible since there is
    penalty for mapping/unmapping highmem. Now, we may
    have an image that is expected to have a lifetime of
    the entire system and is reserved in physical region
    that would be part of direct mapped lowmem. The
    physical memory which is thus reserved is never used
    by Linux. This means that even though the system is
    not actually accessing the  virtual memory
    corresponding to the reserved physical memory, we
    are still losing that portion of direct mapped lowmem
    space.
    
    So by allowing lowmem to be non contiguous we can
    give this unused virtual address space of reserved
    region back for use in vmalloc.
    
    Change-Id: I980b3dfafac71884dcdcb8cd2e4a6363cde5746a
    Signed-off-by: Susheel Khiani <skhiani@codeaurora.org>

--- include/linux/vmalloc.h
+++ include/linux/vmalloc.h
@@ -20,6 +20,8 @@ struct notifier_block;		/* in notifier.h */
 #define VM_UNINITIALIZED	0x00000020	/* vm_struct is not fully initialized */
 #define VM_NO_GUARD		0x00000040      /* don't add guard page */
 #define VM_KASAN		0x00000080      /* has allocated kasan shadow memory */
+#define VM_LOWMEM		0x00000100	/* Tracking of direct mapped lowmem */
+
 /* bits [20..32] reserved for arch specific ioremap internals */
 
 /*
@@ -174,6 +176,7 @@ extern long vwrite(char *buf, char *addr, unsigned long count);
 extern struct list_head vmap_area_list;
 extern __init void vm_area_add_early(struct vm_struct *vm);
 extern __init void vm_area_register_early(struct vm_struct *vm, size_t align);
+extern __init int vm_area_check_early(struct vm_struct *vm);
 
 #ifdef CONFIG_SMP
 # ifdef CONFIG_MMU
