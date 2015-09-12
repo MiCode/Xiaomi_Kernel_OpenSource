5608ece521c77b52a5f8abe67da8415dcefa60cd
Banajit Goswami <bgoswami@codeaurora.org>
2015-09-12 11:04:47 -0700

    ASoC: wcd9335: Add support to enable vbat feature
    Add support to enable vbat monitor feature on tasha codec.
    Vbat module monitors the battery voltage and updates the
    gain on rx path if battery voltage falls below certain
    threshold value. This module can prevent brown-out
    scenarios on the mobile devices when the loads are heavy
    on the battery and it can also prolong battery life.
    
    Change-Id: I66bacf3b861618fc44f50a8e22ca3f2f0fc7763b
    Signed-off-by: Venkata Narendra Kumar Gutta <vgutta@codeaurora.org>
    Signed-off-by: Banajit Goswami <bgoswami@codeaurora.org>

--- include/uapi/sound/msmcal-hwdep.h
+++ include/uapi/sound/msmcal-hwdep.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -19,6 +19,7 @@ enum wcd_cal_type {
 	WCD9XXX_ANC_CAL = WCD9XXX_MIN_CAL,
 	WCD9XXX_MAD_CAL,
 	WCD9XXX_MBHC_CAL,
+	WCD9XXX_VBAT_CAL,
 	WCD9XXX_MAX_CAL,
 };
 
