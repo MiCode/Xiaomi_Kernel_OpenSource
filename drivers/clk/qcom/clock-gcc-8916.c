8d5f1f333551445b295e5a974ff1af56edb11b34
Pranav Vashi <neobuddy89@gmail.com>
2015-09-14 00:43:56 +0530

    msm8916: gpu: Add UC/OC frequencies
    * Maintain nominal/initial freq of 310 MHz to prevent lag.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/clk/qcom/clock-gcc-8916.c
+++ drivers/clk/qcom/clock-gcc-8916.c
@@ -598,6 +598,7 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk[] = {
 	F( 294912000,	   gpll1,   3,	  0,	0),
 	F( 310000000,	   gpll2,   3,	  0,	0),
 	F( 400000000,  gpll0_aux,   2,	  0,	0),
+	F( 465000000,      gpll2,   2,	  0,	0),
 	F_END
 };
 
@@ -610,8 +611,8 @@ static struct rcg_clk gfx3d_clk_src = {
 	.c = {
 		.dbg_name = "gfx3d_clk_src",
 		.ops = &clk_ops_rcg,
-		VDD_DIG_FMAX_MAP3(LOW, 200000000, NOMINAL, 310000000, HIGH,
-			400000000),
+		VDD_DIG_FMAX_MAP3(LOW, 100000000, NOMINAL, 310000000, HIGH,
+			465000000),
 		CLK_INIT(gfx3d_clk_src.c),
 	},
 };
