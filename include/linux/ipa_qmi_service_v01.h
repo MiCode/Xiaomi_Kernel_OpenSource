d7e4caef502a19646e59f8cc41ff8ad0b1e8a15c
Ravinder Konka <rkonka@codeaurora.org>
2015-09-15 15:29:16 +0530

    kernel: remove copyright and license info from header files
    Removing copyright and license information from
    rmnet_ipa_fd_ioctl.h and ipa_qmi_service_v01.h header files
    since these can be exported to user space.
    
    Change-Id: I3535f81c29b888e8ebcbcb32dcba9f0f08b64485
    Signed-off-by: Ravinder Konka <rkonka@codeaurora.org>

--- include/linux/ipa_qmi_service_v01.h
+++ include/linux/ipa_qmi_service_v01.h
@@ -1,15 +1,3 @@
-/* Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 and
- * only version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
-
 /*
  * This header file defines the types and structures that were defined in
  * ipa. It contains the constant values defined, enums, structures,
