c70f019a9a509e4e4a440eddaf179f5b010825fd
Theodore Ts'o <tytso@google.com>
2015-09-17 16:28:06 -0400

    ext4: disable zeroout optimization when encryption is enabled
    When we are manipulate a sparse file's extent tree, there is an
    optimization where we will explicitly zero out some data blocks instead
    of making the extent tree overly bushy.  Unfortunately, this can
    sometimes lead to the file system's extent tree getting corrupted.
    
    Disabling the optimization works around this bug, but we don't have a
    completely understood root cause yet.  So this should be understood as
    only a (hopefully!) temporary workaround.
    
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>
    Signed-off-by: "Theodore Ts'o" <tytso@google.com>
    Change-Id: Ie237434603ad1e36351b3e42954ffecbd5c0f035

--- fs/ext4/extents.c
+++ fs/ext4/extents.c
@@ -3542,7 +3542,8 @@ static int ext4_ext_convert_to_initialized(handle_t *handle,
 	 */
 	split_flag |= ee_block + ee_len <= eof_block ? EXT4_EXT_MAY_ZEROOUT : 0;
 
-	if (EXT4_EXT_MAY_ZEROOUT & split_flag)
+	if ((EXT4_EXT_MAY_ZEROOUT & split_flag) &&
+	    !ext4_encrypted_inode(inode))
 		max_zeroout = sbi->s_extent_max_zeroout_kb >>
 			(inode->i_sb->s_blocksize_bits - 10);
 
