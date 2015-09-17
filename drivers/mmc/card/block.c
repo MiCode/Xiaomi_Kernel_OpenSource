dbe2c84539f2d58dbdf94583b492fbcacc861163
Maya Erez <merez@codeaurora.org>
2015-09-17 15:10:26 +0300

    mmc: block: stop BKOPs before handling RPMB and ioctl
    IOCTL and RPMB commands can be issues while the device is busy
    with background Operations handling.
    Stop the device BKOPs before handling the RPMB / IOCTL command.
    
    Change-Id: I088f74c77026ccd901276e1214e4466ac7815bf1
    Signed-off-by: Maya Erez <merez@codeaurora.org>
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -751,6 +751,9 @@ static int mmc_blk_ioctl_cmd(struct block_device *bdev,
 		}
 	}
 
+	if (mmc_card_get_bkops_en_manual(card))
+		mmc_stop_bkops(card);
+
 	err = mmc_blk_part_switch(card, md);
 	if (err)
 		goto cmd_rel_host;
@@ -897,6 +900,9 @@ static int mmc_blk_ioctl_rpmb_cmd(struct block_device *bdev,
 
 	mmc_rpm_hold(card->host, &card->dev);
 	mmc_claim_host(card->host);
+
+	if (mmc_card_get_bkops_en_manual(card))
+		mmc_stop_bkops(card);
 
 	err = mmc_blk_part_switch(card, md);
 	if (err)
