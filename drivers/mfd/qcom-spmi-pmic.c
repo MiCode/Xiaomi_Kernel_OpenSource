828516323c7ffdcb18515c2c1f2e975d6b0c268d
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2015-09-23 11:48:07 -0700

    mfd: qcom-spmi-pmic: instantiate pmic peripherals at arch_initcall
    The spmi_devices spawn platform devices for pmic peripherals. Some of these
    devices are required early on in the boot process.
    
    Initialize the driver at arch_initcall to avoid unnecessary probe
    deferrals.
    
    Change-Id: I38474bedcb284ed5a7df1ec8d26f680787f2c074
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- drivers/mfd/qcom-spmi-pmic.c
+++ drivers/mfd/qcom-spmi-pmic.c
@@ -147,7 +147,12 @@ static struct spmi_driver pmic_spmi_driver = {
 		.of_match_table = pmic_spmi_id_table,
 	},
 };
-module_spmi_driver(pmic_spmi_driver);
+
+int __init pmic_spmi_init(void)
+{
+	return spmi_driver_register(&pmic_spmi_driver);
+}
+arch_initcall(pmic_spmi_init);
 
 MODULE_DESCRIPTION("Qualcomm SPMI PMIC driver");
 MODULE_ALIAS("spmi:spmi-pmic");
