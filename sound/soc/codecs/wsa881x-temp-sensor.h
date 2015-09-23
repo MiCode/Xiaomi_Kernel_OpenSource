82a32f5cdf2dfdcd51ee408906a716b9e376c507
Vidyakumar Athota <vathota@codeaurora.org>
2015-09-23 12:13:35 -0700

    ASoC: wsa881x: export wsa881x_get_temp API
    Export wsa881x_get_temp() API from wsa temperature sensor driver
    so that wsa881x driver can update the register values based on the
    temperature read.
    
    Change-Id: I2fc1298ce9ce896180af202e5f23e39b0dd551ca
    Signed-off-by: Vidyakumar Athota <vathota@codeaurora.org>

--- sound/soc/codecs/wsa881x-temp-sensor.h
+++ sound/soc/codecs/wsa881x-temp-sensor.h
@@ -33,6 +33,7 @@ struct wsa881x_tz_priv {
 	wsa_temp_register_read wsa_temp_reg_read;
 };
 
+int wsa881x_get_temp(struct thermal_zone_device *tz_dev, unsigned long *temp);
 int wsa881x_init_thermal(struct wsa881x_tz_priv *tz_pdata);
 void wsa881x_deinit_thermal(struct thermal_zone_device *tz_dev);
 #endif
