4ade8c364d73890539bd13a546fd93a259445e4c
Venu Yeshala <vyeshala@codeaurora.org>
2015-09-28 14:51:03 +0530

    msm: isp: Synchronize interrupt handler and close
    Avoid the possibility that interrupt handler is triggered
    while close is in progress by adding a lock mechanism.
    
    Change-Id: I6d45017b58d628e1aeb4ae1fd10a34991f5ccc22
    Signed-off-by: Venu Yeshala <vyeshala@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
@@ -2207,6 +2207,7 @@ int msm_isp_close_node(struct v4l2_subdev *sd, struct v4l2_subdev_fh *fh)
 {
 	long rc = 0;
 	struct vfe_device *vfe_dev = v4l2_get_subdevdata(sd);
+	unsigned long flags;
 	ISP_DBG("%s E open_cnt %u\n", __func__, vfe_dev->vfe_open_cnt);
 	mutex_lock(&vfe_dev->realtime_mutex);
 	mutex_lock(&vfe_dev->core_mutex);
@@ -2237,8 +2238,10 @@ int msm_isp_close_node(struct v4l2_subdev *sd, struct v4l2_subdev_fh *fh)
 	/* after regular hw stop, reduce open cnt */
 	vfe_dev->vfe_open_cnt--;
 
+	spin_lock_irqsave(&vfe_dev->tasklet_lock, flags);
 	vfe_dev->hw_info->vfe_ops.core_ops.release_hw(vfe_dev);
 	vfe_dev->buf_mgr->ops->buf_mgr_deinit(vfe_dev->buf_mgr);
+	spin_unlock_irqrestore(&vfe_dev->tasklet_lock, flags);
 	if (vfe_dev->vt_enable) {
 		msm_isp_end_avtimer();
 		vfe_dev->vt_enable = 0;
