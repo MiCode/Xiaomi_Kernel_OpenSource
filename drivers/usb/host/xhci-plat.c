a0ab9470a4ecdd674a4081b9d05529d9e762bf61
Roger Quadros <rogerq@ti.com>
2015-09-28 13:10:47 -0700

    usb: xhci: Fix suspend/resume when used with OTG core
    In the OTG case, the controller might not yet have been
    added or is removed before the system suspends.
    
    Assign xhci->main_hcd during probe to prevent NULL
    pointer de-reference in xhci_suspend/resume().
    
    Use the hcd->state flag to check if HCD is halted
    and if that is so do nothing for xhci_suspend/resume().
    
    [Only for xhci-plat devices, pci devices need it in gen_setup -Mathias]
    Signed-off-by: Roger Quadros <rogerq@ti.com>
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Git-commit: 9fa733f24bb54dd532b5f5d7abc275c4a547c69f
    Git-repo: git://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git
    [mrana@codeaurora.org: Fix minor merge conflict]
    Change-Id: If064778fa56a8e41ebfd80884802b2abced514ca
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>

--- drivers/usb/host/xhci-plat.c
+++ drivers/usb/host/xhci-plat.c
@@ -151,6 +151,7 @@ static int xhci_plat_probe(struct platform_device *pdev)
 	hcd = platform_get_drvdata(pdev);
 	xhci = hcd_to_xhci(hcd);
 	xhci->clk = clk;
+	xhci->main_hcd = hcd;
 	xhci->shared_hcd = usb_create_shared_hcd(driver, &pdev->dev,
 			dev_name(&pdev->dev), hcd);
 	if (!xhci->shared_hcd) {
--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -901,6 +901,9 @@ int xhci_suspend(struct xhci_hcd *xhci, bool do_wakeup)
 	struct usb_hcd		*hcd = xhci_to_hcd(xhci);
 	u32			command;
 
+	if (!hcd->state)
+		return 0;
+
 	if (hcd->state != HC_STATE_SUSPENDED ||
 			xhci->shared_hcd->state != HC_STATE_SUSPENDED)
 		return -EINVAL;
@@ -986,6 +989,9 @@ int xhci_resume(struct xhci_hcd *xhci, bool hibernated)
 	struct usb_hcd		*secondary_hcd;
 	int			retval = 0;
 	bool			comp_timer_running = false;
+
+	if (!hcd->state)
+		return 0;
 
 	/* Wait a bit if either of the roothubs need to settle from the
 	 * transition into bus suspend.
