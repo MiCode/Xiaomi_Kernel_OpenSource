809d8460ca183ba21cf0a342bca0ca925a59eba7
Junjie Wu <junjiew@codeaurora.org>
2015-09-30 14:47:13 -0700

    sched: Export sched_setscheduler_nocheck()
    Export sched_setscheduler_nocheck() so that external kernel modules
    can use it.
    
    Change-Id: Ib50f537f5aef50c365ba63fb8ffce05bc1c7c431
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>
    Signed-off-by: Bryan Huntsman <bryanh@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6224,6 +6224,7 @@ int sched_setscheduler_nocheck(struct task_struct *p, int policy,
 {
 	return _sched_setscheduler(p, policy, param, false);
 }
+EXPORT_SYMBOL(sched_setscheduler_nocheck);
 
 static int
 do_sched_setscheduler(pid_t pid, int policy, struct sched_param __user *param)
