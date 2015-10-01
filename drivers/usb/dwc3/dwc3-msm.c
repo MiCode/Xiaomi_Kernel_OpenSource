e60df8c05021572295387ceec6715542024b6dc8
Mayank Rana <mrana@codeaurora.org>
2015-10-01 16:26:27 -0700

    dwc3-msm: Don't fail USB LPM sequence if IN_L2 error is seen
    In some instance it is being observed that USB controller is not
    putting HSPHY into suspend state which results into IN_L2 error
    and aborts USB low power mode. Not going into IN_L2 error is not
    error fatal, hence continue with USB low power mode if this
    error is seen.
    
    Change-Id: I85f3567bdc897fbfffbbe95dec45d52d36106b06
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -1250,10 +1250,8 @@ static int dwc3_msm_prepare_suspend(struct dwc3_msm *mdwc)
 		if (reg & PWR_EVNT_LPM_IN_L2_MASK)
 			break;
 	}
-	if (!(reg & PWR_EVNT_LPM_IN_L2_MASK)) {
+	if (!(reg & PWR_EVNT_LPM_IN_L2_MASK))
 		dev_err(mdwc->dev, "could not transition HS PHY to L2\n");
-		return -EBUSY;
-	}
 
 	/* Clear L2 event bit */
 	dwc3_msm_write_reg(mdwc->base, PWR_EVNT_IRQ_STAT_REG,
