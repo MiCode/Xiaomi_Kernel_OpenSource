8e4aa84a1b23003f31561b98b0a042018e8f3ad2
Adrian Salido-Moreno <adrianm@codeaurora.org>
2015-10-02 15:54:46 -0700

    drm/msm: add smmu handler
    Add msm_smmu driver to support mapping buffers to arm smmu memory.
    MSM_SMMU adds the hooks to support drm hooks. Current change only
    supports the unsecure domain memory. MSM_GEM object is also updated
    to attach the new msm_smmu driver.
    
    Change-Id: I4899bd74d8b41b864ed5e0dec2da11e929c7fa95
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>
    Signed-off-by: Jin Li <jinl@codeaurora.org>

--- drivers/gpu/drm/msm/msm_mmu.h
+++ drivers/gpu/drm/msm/msm_mmu.h
@@ -20,6 +20,14 @@
 
 #include <linux/iommu.h>
 
+struct msm_mmu;
+struct msm_gpu;
+
+enum msm_mmu_domain_type {
+	MSM_SMMU_DOMAIN_UNSECURE,
+	MSM_SMMU_DOMAIN_MAX,
+};
+
 struct msm_mmu_funcs {
 	int (*attach)(struct msm_mmu *mmu, const char **names, int cnt);
 	void (*detach)(struct msm_mmu *mmu, const char **names, int cnt);
@@ -27,6 +35,14 @@ struct msm_mmu_funcs {
 			unsigned len, int prot);
 	int (*unmap)(struct msm_mmu *mmu, uint32_t iova, struct sg_table *sgt,
 			unsigned len);
+	int (*map_sg)(struct msm_mmu *mmu, struct sg_table *sgt,
+			enum dma_data_direction dir);
+	void (*unmap_sg)(struct msm_mmu *mmu, struct sg_table *sgt,
+		enum dma_data_direction dir);
+	int (*map_dma_buf)(struct msm_mmu *mmu, struct sg_table *sgt,
+			struct dma_buf *dma_buf, int dir);
+	void (*unmap_dma_buf)(struct msm_mmu *mmu, struct sg_table *sgt,
+			struct dma_buf *dma_buf, int dir);
 	void (*destroy)(struct msm_mmu *mmu);
 };
 
@@ -44,5 +60,7 @@ static inline void msm_mmu_init(struct msm_mmu *mmu, struct device *dev,
 
 struct msm_mmu *msm_iommu_new(struct device *dev, struct iommu_domain *domain);
 struct msm_mmu *msm_gpummu_new(struct device *dev, struct msm_gpu *gpu);
+struct msm_mmu *msm_smmu_new(struct device *dev,
+	enum msm_mmu_domain_type domain);
 
 #endif /* __MSM_MMU_H__ */
