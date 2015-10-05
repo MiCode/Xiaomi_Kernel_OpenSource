81df9dacc846e1c0b983ab64bff0b5869d1ca19a
Ravinder Konka <rkonka@codeaurora.org>
2015-10-05 15:35:36 +0530

    msm: ipa: Move holb discard for Q6 zip pipes to AFTER_SHUTDOWN
    There is a possibility that we have pending Q6 ZIP transacations
    with IPA during modem shutdown. To prevent Q6 ZIP operations to
    be blocking, move setting of holb discard and pipe delay for Q6 ZIP
    pipes to AFTER_SHUTDOWN.
    
    Change-Id: I10e2d9a9c37fc439f1e8f7ff375ff92a00e4cd74
    Acked-by: Chaitanya Pratapa <cpratapa@qti.qualcomm.com>
    Signed-off-by: Ravinder Konka <rkonka@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_i.h
+++ drivers/platform/msm/ipa/ipa_i.h
@@ -1557,8 +1557,8 @@ int ipa_write_qmapid_wdi_pipe(u32 clnt_hdl, u8 qmap_id);
 int ipa_tag_process(struct ipa_desc *desc, int num_descs,
 		    unsigned long timeout);
 
-int ipa_q6_cleanup(void);
-int ipa_q6_pipe_reset(void);
+int ipa_q6_pre_shutdown_cleanup(void);
+int ipa_q6_post_shutdown_cleanup(void);
 int ipa_init_q6_smem(void);
 
 int ipa_sps_connect_safe(struct sps_pipe *h, struct sps_connect *connect,
