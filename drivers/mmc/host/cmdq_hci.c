5b81f13c78745ff3f62732ca194e703f3cdf0d7e
Asutosh Das <asutoshd@codeaurora.org>
2015-10-06 09:53:33 +0530

    mmc: cmdq: decrease the QSR polling period
    Configure the controller to send the QSR at 1 clock period.
    This would send SQS(CMD13) when no data transfer is in
    progress. This decrease in the polling period increases the
    performance of the device.
    
    CRs-fixed: 891366
    Change-Id: Ic2807c6334a778b5f0c89fb605c6923a44f7624a
    Signed-off-by: Asutosh Das <asutoshd@codeaurora.org>
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- drivers/mmc/host/cmdq_hci.c
+++ drivers/mmc/host/cmdq_hci.c
@@ -356,8 +356,7 @@ static int cmdq_enable(struct mmc_host *mmc)
 	cmdq_writel(cq_host, mmc->card->rca, CQSSC2);
 
 	/* send QSR at lesser intervals than the default */
-	cmdq_writel(cq_host, cmdq_readl(cq_host, CQSSC1) | SEND_QSR_INTERVAL,
-				CQSSC1);
+	cmdq_writel(cq_host, SEND_QSR_INTERVAL, CQSSC1);
 
 	/* enable bkops exception indication */
 	if (mmc_card_configured_manual_bkops(mmc->card) &&
