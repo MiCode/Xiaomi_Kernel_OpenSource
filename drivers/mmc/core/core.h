2b72e9710ec4018e786a0d0c60839afda9ac64ac
Sahitya Tummala <stummala@codeaurora.org>
2015-10-09 13:30:46 +0530

    mmc: core: revert recent changes in shutdown handling code
    This change reverts the following gerrits as they break the
    existing shutdown functionality for eMMC and SD card.
    
    8635767 "mmc: core: Extend shutdown sequence to handle bus operations"
    b4f9379 "mmc: core: Add shutdown callback for (e)MMC bus_ops"
    e3126b8 "mmc: core: Add shutdown callback for SD bus_ops"
    
    Change-Id: I3d34404e653600d52015afe851ae55b7e60e07dd
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- drivers/mmc/core/core.h
+++ drivers/mmc/core/core.h
@@ -26,7 +26,6 @@ struct mmc_bus_ops {
 	int (*power_restore)(struct mmc_host *);
 	int (*alive)(struct mmc_host *);
 	int (*change_bus_speed)(struct mmc_host *, unsigned long *);
-	int (*shutdown)(struct mmc_host *);
 };
 
 void mmc_attach_bus(struct mmc_host *host, const struct mmc_bus_ops *ops);
