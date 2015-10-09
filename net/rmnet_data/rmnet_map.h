42f8729b517fb1d8649ce27b8c302fba8fc93741
Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
2015-10-08 18:59:00 -0600

    net: rmnet_data: Stop adding pad bytes for MAPv4 uplink packets
    Hardware does not handle pad bytes in egress packets when uplink
    aggregation is not enabled. This is due to the translation support
    added on hardware for MAPv4.
    
    Change-Id: Ic246a4548561450035d5252221032d72eff44518
    Signed-off-by: Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>

--- net/rmnet_data/rmnet_map.h
+++ net/rmnet_data/rmnet_map.h
@@ -128,12 +128,15 @@ enum rmnet_map_agg_state_e {
 #define RMNET_MAP_COMMAND_UNSUPPORTED 2
 #define RMNET_MAP_COMMAND_INVALID     3
 
+#define RMNET_MAP_NO_PAD_BYTES        0
+#define RMNET_MAP_ADD_PAD_BYTES       1
+
 uint8_t rmnet_map_demultiplex(struct sk_buff *skb);
 struct sk_buff *rmnet_map_deaggregate(struct sk_buff *skb,
 				      struct rmnet_phys_ep_conf_s *config);
 
 struct rmnet_map_header_s *rmnet_map_add_map_header(struct sk_buff *skb,
-						    int hdrlen);
+						    int hdrlen, int pad);
 rx_handler_result_t rmnet_map_command(struct sk_buff *skb,
 				      struct rmnet_phys_ep_conf_s *config);
 void rmnet_map_aggregate(struct sk_buff *skb,
