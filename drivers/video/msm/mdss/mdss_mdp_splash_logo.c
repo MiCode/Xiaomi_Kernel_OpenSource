c0829b3264d999c98c97fbf9dee545227d8b2b62
Dhaval Patel <pdhaval@codeaurora.org>
2015-10-21 10:55:51 -0700

    msm: mdss: fix backlight update between unblank and kickoff
    The backlight update between unblank and kickoff
    call may result in garbage/snow screen on panel
    based on panel ram initialized status.
    
    For ex:
      -> Unblank the panel - black frame transfer start,
                             panel is on but ram is still
                             uninitialized.
      -> backlight update - turn on the backlight. It will show
                            data from uninitialized ram because
                            frame transfer is not complete yet.
    
    This can be avoided by blocking the backlight update
    request between unblank and first frame kickoff. This
    gives guarantee that panel has valid data to display
    when backlight was turned on.
    
    Change-Id: I09c707cd10acd53bd6aa0935885269ffc5aec649
    Signed-off-by: Dhaval Patel <pdhaval@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_splash_logo.c
+++ drivers/video/msm/mdss/mdss_mdp_splash_logo.c
@@ -587,7 +587,7 @@ static int mdss_mdp_splash_thread(void *data)
 	unlock_fb_info(mfd->fbi);
 
 	mutex_lock(&mfd->bl_lock);
-	mfd->bl_updated = true;
+	mfd->allow_bl_update = true;
 	mdss_fb_set_backlight(mfd, mfd->panel_info->bl_max >> 1);
 	mutex_unlock(&mfd->bl_lock);
 
