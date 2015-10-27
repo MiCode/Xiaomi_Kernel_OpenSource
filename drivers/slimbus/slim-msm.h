6df82c59b96cd41bd3ad17afc7a142414e409f81
Dilip Kota <dkota@codeaurora.org>
2015-10-27 15:18:42 +0530

    slim-msm: Optimize QMI response timeout
    For a Slimbus transaction through QMI, timeout
    if there is no response within 500ms and return
    timeout to the slimbus client.
    Transactions through QMI will complete within milli
    seconds. So it is better to timeout for 500ms
    instead of 5 seconds.
    
    Change-Id: I818b7c6ff7941d183e1975b21a092f37a0d94264
    Signed-off-by: Dilip Kota <dkota@codeaurora.org>

--- drivers/slimbus/slim-msm.h
+++ drivers/slimbus/slim-msm.h
@@ -92,6 +92,9 @@
 #define SLIMBUS_QMI_SVC_V1 1
 #define SLIMBUS_QMI_INS_ID 0
 
+/* QMI response timeout of 500ms */
+#define SLIM_QMI_RESP_TOUT 500
+
 #define PGD_THIS_EE(r, v) ((v) ? PGD_THIS_EE_V2(r) : PGD_THIS_EE_V1(r))
 #define PGD_PORT(r, p, v) ((v) ? PGD_PORT_V2(r, p) : PGD_PORT_V1(r, p))
 #define CFG_PORT(r, v) ((v) ? CFG_PORT_V2(r) : CFG_PORT_V1(r))
