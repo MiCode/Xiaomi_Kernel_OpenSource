d560b4e3fa96b8abc170aa077e64f3ce3e9ed648
Balamurugan Alagarsamy <balaga@codeaurora.org>
2015-10-27 17:51:48 +0530

    msm: vidc: fix null pointer crash in sys error handler
    When the iteration pointer of list_for_each_entry is
    used to evaluate, it can have a garbage value. So,instead,
    using a inst pointer, using core to get the hdev.
    
    Also, flushing the core work queue in unload firmware
    leads to deadlock, when there is a SYS error is received
    from core, because handle_sys_error is also a part of core
    work handler, so flushing itself by its own execution,
    will lead to wait indefinitely, i.e, a deadlock. So,
    skipping it when there is a error condition.
    
    Change-Id: Ic4e2df0db2341c974cbba19ddd55ecdc5c832124
    Signed-off-by: Balamurugan Alagarsamy <balaga@codeaurora.org>

--- drivers/media/platform/msm/vidc/venus_hfi.c
+++ drivers/media/platform/msm/vidc/venus_hfi.c
@@ -3761,7 +3761,8 @@ static void venus_hfi_unload_fw(void *dev)
 		return;
 	}
 	if (device->resources.fw.cookie) {
-		flush_workqueue(device->vidc_workq);
+		if (device->state != VENUS_STATE_DEINIT)
+			flush_workqueue(device->vidc_workq);
 		flush_workqueue(device->venus_pm_workq);
 		subsystem_put(device->resources.fw.cookie);
 		venus_hfi_interface_queues_release(dev);
