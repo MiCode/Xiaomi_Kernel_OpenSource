1210e28cd5826b904713d553cf938c03da074509
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2015-10-28 15:42:28 +0530

    USB: gadget: rndis: Fix NULL pointer crash during proc file read
    Proc files are created during bootup and can be accessed before
    cable connect or before enabling rndis. This could cause NULL
    pointer access if read before cable connection or rndis enabled
    as dev pointer is being accessed, which will be set during
    set configuration request. Hence fix this by having check for
    NULL pointer access.
    
    Change-Id: I84d987e217e7493260e2dde8e3d1626249b8ac22
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>

--- drivers/usb/gadget/function/rndis.c
+++ drivers/usb/gadget/function/rndis.c
@@ -1246,9 +1246,9 @@ static int rndis_proc_show(struct seq_file *m, void *v)
 			 (param->media_state) ? 0 : param->speed*100,
 			 (param->media_state) ? "disconnected" : "connected",
 			 param->vendorID, param->vendorDescr,
-			 param->max_pkt_per_xfer *
+			 param->dev ? param->max_pkt_per_xfer *
 				 (param->dev->mtu + sizeof(struct ethhdr) +
-				  sizeof(struct rndis_packet_msg_type) + 22),
+				 sizeof(struct rndis_packet_msg_type) + 22) : 0,
 			 rndis_ul_max_xfer_size_rcvd,
 			 param->max_pkt_per_xfer,
 			 rndis_ul_max_pkt_per_xfer_rcvd,
