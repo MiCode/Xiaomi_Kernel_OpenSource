d7e8f473a1922fb5bc26977bc14dcc960d87b2d2
Yan He <yanhe@codeaurora.org>
2015-11-02 18:19:19 -0800

    msm: sps: output BAM register dump to IPC log only
    Outputting BAM register dump to kernel log could consume
    significant resource when the amount of registers is big. The
    change here is to output BAM register dump to IPC log only as long
    as IPC logging is enabled in the build.
    
    Change-Id: Ibbd31bb2e97785008117c56da250beddbd75d958
    Signed-off-by: Yan He <yanhe@codeaurora.org>

--- drivers/platform/msm/sps/spsi.h
+++ drivers/platform/msm/sps/spsi.h
@@ -51,6 +51,7 @@
 
 #define MAX_MSG_LEN 80
 #define SPS_IPC_LOGPAGES 10
+#define SPS_IPC_REG_DUMP_FACTOR 3
 
 /* Connection mapping control struct */
 struct sps_rm {
@@ -137,7 +138,10 @@ extern u8 print_limit_option;
 	} while (0)
 #define SPS_DUMP(msg, args...) do {					\
 		SPS_IPC(4, sps, msg, args); \
-		pr_info(msg, ##args);	\
+		if (sps) { \
+			if (sps->ipc_log4 == NULL) \
+				pr_info(msg, ##args);	\
+		} \
 	} while (0)
 #define SPS_DEBUGFS(msg, args...) do {					\
 		char buf[MAX_MSG_LEN];		\
