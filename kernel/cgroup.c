424309dff1904c2ad2ba3bc87f9378d9501e144d
Jeevan Shriram <jshriram@codeaurora.org>
2015-11-09 10:53:21 -0800

    cgroup: fix uninitialized usage of a variable
    It is possible that 'root' variable is used uninitialized. This
    change avoids usage of uninitialized usage of the variable.
    
    Change-Id: I9a3bd941a23736cb003f209cf6dde84fd859e9e6
    Signed-off-by: Jeevan Shriram <jshriram@codeaurora.org>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -1690,7 +1690,7 @@ static struct dentry *cgroup_mount(struct file_system_type *fs_type,
 {
 	struct super_block *pinned_sb = NULL;
 	struct cgroup_subsys *ss;
-	struct cgroup_root *root;
+	struct cgroup_root *root = NULL;
 	struct cgroup_sb_opts opts;
 	struct dentry *dentry;
 	int ret;
