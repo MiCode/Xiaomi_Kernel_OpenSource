MotorolaMobilityLLC/kernel-msm

msm: mdss: initialize shadow clocks regardless DFPS modes
Since DFPS mode can be dynamically configured through device node,
shadow clocks used by DFPS clk mode need to be initialized during boot
time regardless the default DFPS mode specified in device tree.

Change-Id: I580a05ed62a2207666ea6569abbdf588a4f8d39d
Signed-off-by: Huaibin Yang <huaibiny@codeaurora.org>

drivers/video/msm/mdss/mdss_dsi.c
@@ -2061,13 +2061,9 @@ int dsi_panel_device_register(struct device_node *pan_node,
 	if (rc)
 		pr_err("PLL 1 Clock's did not register\n");
 
-	if (pinfo->dynamic_fps &&
-			pinfo->dfps_update == DFPS_IMMEDIATE_CLK_UPDATE_MODE) {
-		if (mdss_dsi_shadow_clk_init(ctrl_pdev, ctrl_pdata)) {
+	if (pinfo->dynamic_fps)
+		if (mdss_dsi_shadow_clk_init(ctrl_pdev, ctrl_pdata))
 			pr_err("unable to initialize shadow ctrl clks\n");
-			return -EPERM;
-		}
-	}
 
 	if (mdss_dsi_retrieve_ctrl_resources(ctrl_pdev,
 					     pinfo->pdest,

MiCode/Xiaomi_Kernel_OpenSource

Signed-off-by: Vishnuvardhan Prodduturi <vproddut@codeaurora.org>
Signed-off-by: Anusha Koduru <kanusha@codeaurora.org>

drivers/video/msm/mdss/mdss_dsi.c
@@ -2635,14 +2635,10 @@ static int mdss_dsi_ctrl_probe(struct platform_device *pdev)
 	}
 
 	pinfo = &(ctrl_pdata->panel_data.panel_info);
-	if (pinfo->dynamic_fps &&
-			pinfo->dfps_update == DFPS_IMMEDIATE_CLK_UPDATE_MODE) {
-		if (mdss_dsi_shadow_clk_init(pdev, ctrl_pdata)) {
+	if (pinfo->dynamic_fps)
+		if (mdss_dsi_shadow_clk_init(pdev, ctrl_pdata))
 			pr_err("%s: unable to initialize shadow ctrl clks\n",
 					__func__);
-			return -EPERM;
-		}
-	}
 
 	rc = mdss_dsi_set_clk_rates(ctrl_pdata);
 	if (rc) {