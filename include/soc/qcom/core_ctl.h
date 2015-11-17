5214f9b7d85ba3a39d6c22dd7099d7f89370fee8
Junjie Wu <junjiew@codeaurora.org>
2015-11-17 11:36:00 -0800

    qcom: core_ctl_helper: Add wrapper for CPU hotplug
    Different kernel version needs to use different kernel APIs for
    hotplug. Add wrapper for CPU hotplug so that external module can be
    used across different kernel releases.
    
    Use device_online/offline() API for CPU hotplug.
    
    Change-Id: Id21855f4ddc62bc9e9e6b45a856c245fd18d1514
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- include/soc/qcom/core_ctl.h
+++ include/soc/qcom/core_ctl.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -21,5 +21,6 @@ extern struct cpufreq_policy *core_ctl_get_policy(int cpu);
 extern void core_ctl_put_policy(struct cpufreq_policy *policy);
 extern struct device *core_ctl_find_cpu_device(unsigned cpu);
 extern int core_ctl_online_core(unsigned int cpu);
+extern int core_ctl_offline_core(unsigned int cpu);
 
 #endif
