9042695d432edf79de257e0c2a0ddd61f6027b5b
Pavan Anamula <pavana@codeaurora.org>
2015-11-17 14:46:38 +0530

    mtd: nand_ids: Add Micron & Kingston NAND details
    Add 2K + 64 Micron and 2K + 128 Kingston NAND parts.
    
    Change-Id: I03928ea3a0a4b7bef86be087f5a315b88935b4e3
    Signed-off-by: Pavan Anamula <pavana@codeaurora.org>

--- drivers/mtd/nand/nand_ids.c
+++ drivers/mtd/nand/nand_ids.c
@@ -66,6 +66,13 @@ struct nand_flash_dev nand_flash_ids[] = {
 		{ .id = {0x98, 0xac, 0x90, 0x26, 0x76, 0x00, 0x00, 0x00} },
 		SZ_4K, SZ_512, SZ_256K, 0, 5, 128, NAND_ECC_INFO(8, SZ_512) },
 
+	{"MT29RZ2B1DZZHGSP 2G  1.8V 4-bit",
+		{ .id = {0x2c, 0xaa, 0x90, 0x15, 0x00, 0x00, 0x00, 0x00} },
+		SZ_2K, SZ_256, SZ_128K, 0, 4, 64, NAND_ECC_INFO(4, SZ_512) },
+
+	{"KSLCMAL2TA0M2A 2G 1.8V 8-bit",
+		{ .id = {0x98, 0xaa, 0x90, 0x15, 0x00, 0x00, 0x00, 0x00} },
+		SZ_2K, SZ_256, SZ_128K, 0, 4, 128, NAND_ECC_INFO(8, SZ_512)},
 
 	LEGACY_ID_NAND("NAND 4MiB 5V 8-bit",   0x6B, 4, SZ_8K, SP_OPTIONS),
 	LEGACY_ID_NAND("NAND 4MiB 3,3V 8-bit", 0xE3, 4, SZ_8K, SP_OPTIONS),
