917397c9f94f1e67e132e543400a1345fe0aadff
Shiraz Hashim <shashim@codeaurora.org>
2015-11-18 12:46:05 +0530

    dma-mapping: fix build when !CONFIG_ARM_DMA_USE_IOMMU
    Allow compilation when CONFIG_ARM_DMA_USE_IOMMU is not
    selected by providing necessary stub functions and fix few
    mismatch in function declarations.
    
    Following this, remove the changes introduced by
    f3d8d1061fb0b146b3f5 ("msm: ipa: add empty implementation
    for iommu functions") as they are no longer needed.
    
    Change-Id: I04e3aa63407064e8d9c9550a5cb0a82ede899f00
    Signed-off-by: Shiraz Hashim <shashim@codeaurora.org>

--- arch/arm/include/asm/dma-iommu.h
+++ arch/arm/include/asm/dma-iommu.h
@@ -24,6 +24,8 @@ struct dma_iommu_mapping {
 	struct kref		kref;
 };
 
+#ifdef CONFIG_ARM_DMA_USE_IOMMU
+
 struct dma_iommu_mapping *
 arm_iommu_create_mapping(struct bus_type *bus, dma_addr_t base, size_t size);
 
@@ -32,6 +34,30 @@ void arm_iommu_release_mapping(struct dma_iommu_mapping *mapping);
 int arm_iommu_attach_device(struct device *dev,
 					struct dma_iommu_mapping *mapping);
 void arm_iommu_detach_device(struct device *dev);
+
+#else  /* !CONFIG_ARM_DMA_USE_IOMMU */
+
+static inline struct dma_iommu_mapping *
+arm_iommu_create_mapping(struct bus_type *bus, dma_addr_t base, size_t size)
+{
+	return NULL;
+}
+
+static inline void arm_iommu_release_mapping(struct dma_iommu_mapping *mapping)
+{
+}
+
+static inline int arm_iommu_attach_device(struct device *dev,
+			struct dma_iommu_mapping *mapping)
+{
+	return -ENODEV;
+}
+
+static inline void arm_iommu_detach_device(struct device *dev)
+{
+}
+
+#endif	/* CONFIG_ARM_DMA_USE_IOMMU */
 
 #endif /* __KERNEL__ */
 #endif
