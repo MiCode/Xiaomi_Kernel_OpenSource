4a1d1375e8d84f58601c6d31c927d2301ccf7449
Krishna Chaitanya Devarakonda <kdevarak@codeaurora.org>
2015-11-19 17:28:34 +0530

    msm: mdss: Update MDSS caps for msm8937
    Adding initialization of appropriate flags related to QoS
    and UBWC settings in MDSS capabilities for msm8937.
    
    Change-Id: I16aaf0dd39ce6150183f9b71d4573fdbad62a1da
    Signed-off-by: Krishna Chaitanya Devarakonda <kdevarak@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp.c
+++ drivers/video/msm/mdss/mdss_mdp.c
@@ -1280,6 +1280,14 @@ static void mdss_mdp_hw_rev_caps_init(struct mdss_data_type *mdata)
 		mdata->min_prefill_lines = 12;
 		mdata->has_ubwc = true;
 		mdata->pixel_ram_size = 38 * 1024;
+		mdata->apply_post_scale_bytes = false;
+		mdata->hflip_buffer_reused = false;
+		set_bit(MDSS_QOS_OVERHEAD_FACTOR, mdata->mdss_qos_map);
+		set_bit(MDSS_QOS_CDP, mdata->mdss_qos_map);
+		set_bit(MDSS_QOS_PER_PIPE_LUT, mdata->mdss_qos_map);
+		set_bit(MDSS_QOS_SIMPLIFIED_PREFILL, mdata->mdss_qos_map);
+		set_bit(MDSS_CAPS_YUV_CONFIG, mdata->mdss_caps_map);
+		mdss_mdp_init_default_prefill_factors(mdata);
 		set_bit(MDSS_QOS_OTLIM, mdata->mdss_qos_map);
 		break;
 	default:
