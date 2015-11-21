9d4b47cc3a411f828f04ae889513d1285456222e
Ingrid Gallardo <ingridg@codeaurora.org>
2015-11-20 17:37:49 -0800

    Revert "staging: android: Clean up else statement from sync_fence_poll()"
    This reverts commit 6a44b50f9819571c2b871bffba107defec556a87
    ("staging: android: Clean up else statement from sync_fence_poll()").
    
    Change-Id: I893123f8977f0a09f97ad7ed1767c0828991fd96
    Signed-off-by: Ingrid Gallardo <ingridg@codeaurora.org>

--- drivers/staging/android/sync.c
+++ drivers/staging/android/sync.c
@@ -555,7 +555,8 @@ static unsigned int sync_fence_poll(struct file *file, poll_table *wait)
 		return POLLIN;
 	else if (status < 0)
 		return POLLERR;
-	return 0;
+	else
+		return 0;
 }
 
 static long sync_fence_ioctl_wait(struct sync_fence *fence, unsigned long arg)
