b3e68382ffcfdff0dbbdc9d3cc06f8132c014e65
Ajay Singh Parmar <aparmar@codeaurora.org>
2015-11-22 18:06:34 -0800

    msm: mdss: hdmi: add dynamic encryption enable support
    Implement qseecomm API to send command to TZ to enable/disable
    encryption based on hdcp status. Enable encryption when hdcp
    part 1 is successful and disable it when cable is disconnected
    or authentication fails.
    
    Change-Id: Ia5e1a5927dad36abc3093b16638879be823c84df
    Signed-off-by: Ajay Singh Parmar <aparmar@codeaurora.org>

--- include/linux/hdcp_qseecom.h
+++ include/linux/hdcp_qseecom.h
@@ -117,5 +117,6 @@ int hdcp_library_register(struct hdcp_register_data *data);
 void hdcp_library_deregister(void *phdcpcontext);
 bool hdcp1_check_if_supported_load_app(void);
 int hdcp1_set_keys(uint32_t *aksv_msb, uint32_t *aksv_lsb);
+int hdcp1_set_enc(bool enable);
 
 #endif /* __HDCP_QSEECOM_H */
