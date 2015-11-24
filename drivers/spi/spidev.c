9e41f712a9367c992453a0ff6cd059f6986b6183
Kuirong Wang <kuirongw@codeaurora.org>
2015-11-23 16:18:41 -0800

    spi: spidev: add Qualcomm spi codec slave driver
    Use spidev for Qualcomm spi codec driver.
    
    Change-Id: I3f06963b011d43038917a29e505536e0b38456a7
    Signed-off-by: Kuirong Wang <kuirongw@codeaurora.org>

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -695,6 +695,7 @@ static struct class *spidev_class;
 static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "rohm,dh2228fv" },
 	{ .compatible = "lineartechnology,ltc2488" },
+	{ .compatible = "qcom,spi-msm-codec-slave", },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
