51c4bdd799940373efbbb315d00dd7c88713669f
Sudheer Papothi <spapothi@codeaurora.org>
2015-11-26 06:22:24 +0530

    Revert "ASoC: wcd9335: Increase slimbus clock gear to reduce cold start latency"
    This reverts commit dac17fb61688 ("ASoC: wcd9335: Increase slimbus clock
    gear to reduce cold start latency").
    This change is not needed as pre and post dapm events are getting
    called even for front end dai links which results in latency
    during device path bringup.
    
    Change-Id: I3f35317bc11aec100fbcc1f9304bc23a97c7c39a
    Signed-off-by: Sudheer Papothi <spapothi@codeaurora.org>

--- sound/soc/codecs/wcd9335.c
+++ sound/soc/codecs/wcd9335.c
@@ -762,9 +762,6 @@ struct tasha_priv {
 	int hph_r_gain;
 };
 
-static int tasha_codec_vote_max_bw(struct snd_soc_codec *codec,
-				   bool vote);
-
 static const struct tasha_reg_mask_val tasha_spkr_default[] = {
 	{WCD9335_CDC_COMPANDER7_CTL3, 0x80, 0x80},
 	{WCD9335_CDC_COMPANDER8_CTL3, 0x80, 0x80},
@@ -8002,40 +7999,6 @@ static int tasha_codec_aif4_mixer_switch_put(struct snd_kcontrol *kcontrol,
 	return 1;
 }
 
-static int tasha_dapm_pre_powerup(struct snd_soc_dapm_widget *w,
-				  struct snd_kcontrol *kcontrol, int event)
-{
-	struct snd_soc_codec *codec = w->codec;
-
-	dev_dbg(codec->dev, "%s: w->name %s event %d\n",
-		 __func__, w->name, event);
-
-	switch (event) {
-	case SND_SOC_DAPM_PRE_PMU:
-		tasha_codec_vote_max_bw(codec, true);
-		break;
-	}
-
-	return 0;
-}
-
-static int tasha_dapm_post_powerup(struct snd_soc_dapm_widget *w,
-				   struct snd_kcontrol *kcontrol, int event)
-{
-	struct snd_soc_codec *codec = w->codec;
-
-	dev_dbg(codec->dev, "%s: w->name %s event %d\n",
-		 __func__, w->name, event);
-
-	switch (event) {
-	case SND_SOC_DAPM_POST_PMU:
-		tasha_codec_vote_max_bw(codec, false);
-		break;
-	}
-
-	return 0;
-}
-
 static const char * const tasha_ear_pa_gain_text[] = {
 	"G_6_DB", "G_4P5_DB", "G_3_DB", "G_1P5_DB",
 	"G_0_DB", "G_M2P5_DB", "UNDEFINED", "G_M12_DB"
@@ -9986,8 +9949,6 @@ static const struct snd_soc_dapm_widget tasha_dapm_widgets[] = {
 			&anc_lineout1_switch),
 	SND_SOC_DAPM_SWITCH("ANC LINEOUT2 Enable", SND_SOC_NOPM, 0, 0,
 			&anc_lineout2_switch),
-	SND_SOC_DAPM_PRE("Pre powerup", tasha_dapm_pre_powerup),
-	SND_SOC_DAPM_POST("Post powerup", tasha_dapm_post_powerup),
 };
 
 static int tasha_get_channel_map(struct snd_soc_dai *dai,
