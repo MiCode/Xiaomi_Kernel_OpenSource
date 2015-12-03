e9f98550d193b580c2bc5a14c17901becbf69d09
Vikash Garodia <vgarodia@codeaurora.org>
2015-12-03 09:21:14 +0530

    msm: vidc: Fix compilation issues for 32 bit
    Redefine 64 bit variables as size_t to support
    both 64 bit and 32 bit compilation.
    
    Change-Id: Ibcb06a170c70cafc08ddf4be416f57cf5c23e202
    Signed-off-by: Vikash Garodia <vgarodia@codeaurora.org>

--- drivers/media/platform/msm/vidc/governors/fixedpoint.h
+++ drivers/media/platform/msm/vidc/governors/fixedpoint.h
@@ -17,10 +17,16 @@
 #ifndef __FP_H__
 #define __FP_H__
 
-/* Normally would typedef'ed, but checkpatch doesn't like typedef */
-#define fp_t int64_t
+/*
+ * Normally would typedef'ed, but checkpatch doesn't like typedef.
+ * Also should be normally typedef'ed to intmax_t but that doesn't seem to be
+ * available in the kernel
+ */
+#define fp_t size_t
 
-#define FP_FRACTIONAL_BITS 16
+/* (Arbitrarily) make the first 25% of the bits to be the fractional bits */
+#define FP_FRACTIONAL_BITS ((sizeof(fp_t) * 8) / 4)
+
 #define FP(__i, __f_n, __f_d) \
 	((((fp_t)(__i)) << FP_FRACTIONAL_BITS) + \
 	(((__f_n) << FP_FRACTIONAL_BITS) / (__f_d)))
@@ -29,22 +35,22 @@
 #define FP_ONE FP_INT(1)
 #define FP_ZERO FP_INT(0)
 
-static inline int64_t fp_frac_base(void)
+static inline size_t fp_frac_base(void)
 {
 	return GENMASK(FP_FRACTIONAL_BITS - 1, 0);
 }
 
-static inline int64_t fp_frac(fp_t a)
+static inline size_t fp_frac(fp_t a)
 {
 	return a & GENMASK(FP_FRACTIONAL_BITS - 1, 0);
 }
 
-static inline int64_t fp_int(fp_t a)
+static inline size_t fp_int(fp_t a)
 {
 	return a >> FP_FRACTIONAL_BITS;
 }
 
-static inline int64_t fp_round(fp_t a)
+static inline size_t fp_round(fp_t a)
 {
 	/* is the fractional part >= frac_max / 2? */
 	bool round_up = fp_frac(a) >= fp_frac_base() / 2;
