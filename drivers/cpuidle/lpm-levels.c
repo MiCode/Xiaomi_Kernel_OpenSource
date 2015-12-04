bda3760199251da579e1af65d764b02fdc7bf240
Archana Sathyakumar <asathyak@codeaurora.org>
2015-12-04 15:52:17 -0700

    lpm: Allow cpu to enter FPC from hypervisor
    FPC using PSCI is entered from PSCI layer that is in Secure EL1.
    Switching of EL layers incur additional latency, making FPC slower.
    Issue wfi within hypervisor for cpu only sleep. This makes FPC much
    faster than entering from PSCI layer.
    
    Change-Id: Icf4c5f2484fdda79c991b842cb3a3185b638bfdb
    Signed-off-by: Archana Sathyakumar <asathyak@codeaurora.org>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -766,6 +766,7 @@ unlock_and_return:
 
 #if !defined(CONFIG_CPU_V7)
 #include <asm/cpuidle.h>
+asmlinkage int __invoke_psci_fn_smc(u64, u64, u64, u64);
 bool psci_enter_sleep(struct lpm_cluster *cluster, int idx, bool from_idle)
 {
 	/*
@@ -782,6 +783,13 @@ bool psci_enter_sleep(struct lpm_cluster *cluster, int idx, bool from_idle)
 		int power_state =
 			PSCI_POWER_STATE(cluster->cpu->levels[idx].is_reset);
 		bool success = false;
+
+		if (cluster->cpu->levels[idx].hyp_psci) {
+			stop_critical_timings();
+			__invoke_psci_fn_smc(0xC4000021, 0, 0, 0);
+			start_critical_timings();
+			return 1;
+		}
 
 		affinity_level = PSCI_AFFINITY_LEVEL(affinity_level);
 		state_id |= (power_state | affinity_level
--- drivers/cpuidle/lpm-levels.h
+++ drivers/cpuidle/lpm-levels.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, 2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -37,6 +37,7 @@ struct lpm_cpu_level {
 	unsigned int psci_id;
 	bool is_reset;
 	bool jtag_save_restore;
+	bool hyp_psci;
 };
 
 struct lpm_cpu {
