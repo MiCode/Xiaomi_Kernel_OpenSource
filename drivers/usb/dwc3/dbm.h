6a7eb1e1e978778e53fe44536587146e08b34b0b
Jack Pham <jackp@codeaurora.org>
2015-12-11 19:30:25 -0800

    usb: dwc3-msm: dbm: Change API to direct calls
    Now that there is only a single DBM driver, get rid of the
    function pointer based APIs and simply expose the functions
    to be called directly by dwc3-msm. From this we can consolidate
    the dbm and dbm_data structures and hide the definition in dbm.c.
    
    Also remove kfree() calls since devm_kzalloc() is being used. The
    memory will be automatically freed when the device is unbound.
    
    Change-Id: Ia9080ccaf36766ce58655364b247aa0025de7670
    Signed-off-by: Jack Pham <jackp@codeaurora.org>

--- drivers/usb/dwc3/dbm.h
+++ drivers/usb/dwc3/dbm.h
@@ -13,8 +13,8 @@
 #ifndef __DBM_H
 #define __DBM_H
 
+#include <linux/device.h>
 #include <linux/types.h>
-#include <linux/usb/gadget.h>
 
 /**
  *  USB DBM  Hardware registers bitmask.
@@ -52,149 +52,23 @@
 #define DBM_TRB_DMA		0x20000000
 #define DBM_TRB_EP_NUM(ep)	(ep<<24)
 
-
-struct dbm {
-	struct device		*dev;
-	struct list_head	head;
-
-	/* Reset the DBM registers upon initialization */
-	int (*soft_reset)(bool reset);
-
-	/* Configure a USB DBM ep to work in BAM mode */
-	int (*ep_config)(u8 usb_ep, u8 bam_pipe,
-			bool producer, bool disable_wb,
-			bool internal_mem, bool ioc);
-
-	/* Configure a USB DBM ep to work in normal mode */
-	int (*ep_unconfig)(u8 usb_ep);
-
-	/* Return number of configured DBM endpoints */
-	int (*get_num_of_eps_configured)(void);
-
-	/* Configure the DBM with the USB3 core event buffer */
-	int (*event_buffer_config)(u32 addr_lo, u32 addr_hi, int size);
-
-	/* Configure the DBM with the BAM's data fifo */
-	int (*data_fifo_config)(u8 dep_num, phys_addr_t addr,
-			 u32 size, u8 dst_pipe_idx);
-
-	/* Configure DBM speed : hs/ss */
-	void (*set_speed)(bool speed);
-
-	/* Enable DBM */
-	void (*enable)(void);
-
-	/* Reset a USB DBM ep */
-	int (*ep_soft_reset)(u8 dbm_ep, bool enter_reset);
-
-	/* Check whether the USB DBM requires ep reset after lpm suspend */
-	bool (*reset_ep_after_lpm)(void);
-
-	/*
-	 * Indicates whether the DBM notifies the software about the need to
-	 * come out of L1 state by interrupt
-	 */
-	bool (*l1_lpm_interrupt)(void);
-
-};
+struct dbm;
 
 struct dbm *usb_get_dbm_by_phandle(struct device *dev, const char *phandle);
 
-
-#define CHECK_DBM_PTR_INT(dbm, func) do {				   \
-	if (!(dbm) || !((dbm)->func)) {					   \
-		pr_err("Can't call %s, dbp pointer == NULL\n", __func__);  \
-		return -EPERM;						   \
-	}								   \
-} while (0)
-
-#define CHECK_DBM_PTR_VOID(dbm, func) do {				   \
-	if (!(dbm) || !((dbm)->func)) {					   \
-		pr_err("Can't call %s, dbp pointer == NULL\n", __func__);  \
-		return;							   \
-	}								   \
-} while (0)
-
-#define CHECK_DBM_PTR_BOOL(dbm, func, ret) do {				   \
-	if (!(dbm) || !((dbm)->func)) {					   \
-		pr_err("Can't call %s, dbp pointer == NULL\n", __func__);  \
-		return ret;						   \
-	}								   \
-} while (0)
-
-static inline int dbm_soft_reset(struct dbm *dbm, bool enter_reset)
-{
-	CHECK_DBM_PTR_INT(dbm, soft_reset);
-	return dbm->soft_reset(enter_reset);
-}
-
-static inline int dbm_ep_config(struct dbm  *dbm, u8 usb_ep, u8 bam_pipe,
-			bool producer, bool disable_wb, bool internal_mem,
-			bool ioc)
-{
-	CHECK_DBM_PTR_INT(dbm, ep_config);
-	return dbm->ep_config(usb_ep, bam_pipe, producer, disable_wb,
-				  internal_mem, ioc);
-
-}
-
-static inline int dbm_ep_unconfig(struct dbm *dbm, u8 usb_ep)
-{
-	CHECK_DBM_PTR_INT(dbm, ep_unconfig);
-	return dbm->ep_unconfig(usb_ep);
-}
-
-static inline int dbm_get_num_of_eps_configured(struct dbm *dbm)
-{
-	CHECK_DBM_PTR_INT(dbm, get_num_of_eps_configured);
-	return dbm->get_num_of_eps_configured();
-}
-
-static inline int dbm_event_buffer_config(struct dbm *dbm, u32 addr_lo,
-					u32 addr_hi, int size)
-{
-	CHECK_DBM_PTR_INT(dbm, event_buffer_config);
-	return dbm->event_buffer_config(addr_lo, addr_hi, size);
-}
-
-static inline int dbm_data_fifo_config(struct dbm *dbm, u8 dep_num,
-				phys_addr_t addr, u32 size, u8 dst_pipe_idx)
-{
-	CHECK_DBM_PTR_INT(dbm, data_fifo_config);
-	return dbm->data_fifo_config(dep_num, addr, size, dst_pipe_idx);
-}
-
-static inline void dbm_set_speed(struct dbm *dbm, bool speed)
-{
-	CHECK_DBM_PTR_VOID(dbm, set_speed);
-	dbm->set_speed(speed);
-}
-
-static inline void dbm_enable(struct dbm *dbm)
-{
-	CHECK_DBM_PTR_VOID(dbm, enable);
-	dbm->enable();
-}
-
-static inline int dbm_ep_soft_reset(struct dbm *dbm, u8 usb_ep,
-					bool enter_reset)
-{
-	CHECK_DBM_PTR_INT(dbm, ep_soft_reset);
-	return dbm->ep_soft_reset(usb_ep, enter_reset);
-}
-
-static inline bool dbm_reset_ep_after_lpm(struct dbm *dbm)
-{
-	/* Default (backward compatible) setting is false */
-	CHECK_DBM_PTR_BOOL(dbm, reset_ep_after_lpm, false);
-	return dbm->reset_ep_after_lpm();
-}
-
-static inline bool dbm_l1_lpm_interrupt(struct dbm *dbm)
-{
-	/* Default (backward compatible) setting is false */
-	CHECK_DBM_PTR_BOOL(dbm, l1_lpm_interrupt, false);
-	return dbm->l1_lpm_interrupt();
-}
+int dbm_soft_reset(struct dbm *dbm, bool enter_reset);
+int dbm_ep_config(struct dbm  *dbm, u8 usb_ep, u8 bam_pipe, bool producer,
+			bool disable_wb, bool internal_mem, bool ioc);
+int dbm_ep_unconfig(struct dbm *dbm, u8 usb_ep);
+int dbm_get_num_of_eps_configured(struct dbm *dbm);
+int dbm_event_buffer_config(struct dbm *dbm, u32 addr_lo, u32 addr_hi,
+				int size);
+int dbm_data_fifo_config(struct dbm *dbm, u8 dep_num, phys_addr_t addr,
+				u32 size, u8 dst_pipe_idx);
+void dbm_set_speed(struct dbm *dbm, bool speed);
+void dbm_enable(struct dbm *dbm);
+int dbm_ep_soft_reset(struct dbm *dbm, u8 usb_ep, bool enter_reset);
+bool dbm_reset_ep_after_lpm(struct dbm *dbm);
+bool dbm_l1_lpm_interrupt(struct dbm *dbm);
 
 #endif /* __DBM_H */
