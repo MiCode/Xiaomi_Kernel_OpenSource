22b03aeac6ba8d4bdf446f8935f688659d14a4c2
Sandeep Panda <spanda@codeaurora.org>
2015-12-14 20:18:40 +0530

    clk: qcom: mdss: add support for msmtitanium DSI PLL
    Add support for DSI PLL on msmtitanium platform, which is
    needed to driver DSI pixel and byte clocks.
    
    Change-Id: Ibcd5e0d01ea23f86c020119123016f011f23b6d0
    Signed-off-by: Sandeep Panda <spanda@codeaurora.org>

--- drivers/clk/msm/mdss/mdss-pll.c
+++ drivers/clk/msm/mdss/mdss-pll.c
@@ -140,6 +140,10 @@ static int mdss_pll_resource_parse(struct platform_device *pdev,
 		pll_res->pll_interface_type = MDSS_DSI_PLL_8996;
 		pll_res->target_id = MDSS_PLL_TARGET_8996;
 		pll_res->revision = 2;
+	} else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_titanium")) {
+		pll_res->pll_interface_type = MDSS_DSI_PLL_8996;
+		pll_res->target_id = MDSS_PLL_TARGET_TITANIUM;
+		pll_res->revision = 2;
 	} else if (!strcmp(compatible_stream, "qcom,mdss_hdmi_pll_8996")) {
 		pll_res->pll_interface_type = MDSS_HDMI_PLL_8996;
 	} else if (!strcmp(compatible_stream, "qcom,mdss_hdmi_pll_8996_v2")) {
@@ -383,6 +387,7 @@ static const struct of_device_id mdss_pll_dt_match[] = {
 	{.compatible = "qcom,mdss_hdmi_pll_8996_v3_1p8"},
 	{.compatible = "qcom,mdss_dsi_pll_8952"},
 	{.compatible = "qcom,mdss_dsi_pll_8937"},
+	{.compatible = "qcom,mdss_dsi_pll_titanium"},
 	{}
 };
 
