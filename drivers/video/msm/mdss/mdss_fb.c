489be1948d53ee5151f6f038c1721451df8b7e9b
Benet Clark <benetc@codeaurora.org>
2015-12-14 22:24:16 -0800

    msm: mdss: Send backlight sysfs notification in all BL update locations
    There are two places where the backlight can get updated. Currently, the
    sysfs notification only happens in mdss_fb_set_backlight. If the backlight
    is restored during the first display commit, the backlight update occurs
    in mdss_fb_update_backlight. This change adds the sysfs notification in
    update_backlight.
    
    Change-Id: Iffa1e4f0aa0cdc8babb1b6683d3175795f4b4d27
    Signed-off-by: Benet Clark <benetc@codeaurora.org>
    Signed-off-by: Krishna Chaitanya Parimi <cparimi@codeaurora.org>
    CRs-fixed: 949146

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1544,6 +1544,7 @@ void mdss_fb_update_backlight(struct msm_fb_data_type *mfd)
 			if (bl_notify)
 				mdss_fb_bl_update_notify(mfd,
 					NOTIFY_TYPE_BL_AD_ATTEN_UPDATE);
+			mdss_fb_bl_update_notify(mfd, NOTIFY_TYPE_BL_UPDATE);
 			pdata->set_backlight(pdata, temp);
 			mfd->bl_level_scaled = mfd->unset_bl_level;
 			mfd->allow_bl_update = true;
