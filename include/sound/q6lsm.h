7dc5684bc934a1f3230954d98ba004f2583e1412
Vidyakumar Athota <vathota@codeaurora.org>
2015-12-16 15:42:39 -0800

    ASoC: msm-lsm-client: free lsm client data in msm_lsm_close
    Currently lsm client data is deallocated when q6lsm_open() fails
    which can cause memory corruption if lsm client data is accessed
    after freed. Fix this issue by deallocating the client data only
    in msm_lsm_close().
    
    Change-Id: If048c26a0ffd8a346a28622183cbf2ba1e7e5ff3
    Signed-off-by: Vidyakumar Athota <vathota@codeaurora.org>

--- include/sound/q6lsm.h
+++ include/sound/q6lsm.h
@@ -78,6 +78,7 @@ struct lsm_client {
 	uint16_t	kw_sensitivity;
 	uint8_t		num_confidence_levels;
 	uint8_t		*confidence_levels;
+	bool		opened;
 	bool		started;
 	dma_addr_t	lsm_cal_phy_addr;
 	uint32_t	lsm_cal_size;
