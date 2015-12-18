467b64b1c5b6fa727a156feda7cabf60e5048c09
Devesh Jhunjhunwala <deveshj@codeaurora.org>
2015-12-17 16:57:48 -0800

    clk: msm: Add support to control MEM_PERPIH_OFF bit of branch clock
    Update cbcr_set_flags method to add support for controlling the
    MEM_PERPIH_OFF bit for branch clocks.
    
    Change-Id: I87451b02cb9000dc850fdfaa52a5a9f9fd2893a1
    Signed-off-by: Devesh Jhunjhunwala <deveshj@codeaurora.org>

--- include/linux/clk/msm-clk.h
+++ include/linux/clk/msm-clk.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009, 2012-2014 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2009, 2012-2015 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -30,6 +30,8 @@
 #define CLKFLAG_NO_RATE_CACHE		0x00004000
 #define CLKFLAG_MEASURE			0x00008000
 #define CLKFLAG_EPROBE_DEFER		0x00010000
+#define CLKFLAG_PERIPH_OFF_SET		0x00020000
+#define CLKFLAG_PERIPH_OFF_CLEAR	0x00040000
 
 struct clk_lookup;
 struct clk;
