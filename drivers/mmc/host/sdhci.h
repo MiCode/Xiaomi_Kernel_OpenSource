5b05ac87983fee071d9148ab12559f528d847fe6
Sahitya Tummala <stummala@codeaurora.org>
2015-12-21 14:43:42 +0530

    mmc: sdhci: revert MMC_PM_KEEP_POWER changes related to emmc
    This change reverts the following gerrits as they cause the
    timeout issues with Hynix cards. Also, the latest code supports
    sleep/awake through CMD5 and hence MMC_PM_KEEP_POWER need not be
    set for Hynix eMMC cards.
    
    edcf5be "mmc: host: add detect vops chain"
    09287cb "mmc: sdhci-msm: configure MMC_PM_KEEP_POWER for SDIO"
    7cf603a "mmc: schci: add support for MMC_PM_KEEP_POWER in eMMC"
    c085820 "mmc: core: set MMC_PM_KEEP_POWER for certain Hynix mmc cards"
    
    CRs-Fixed: 947299
    Change-Id: If863771191ee7c2b717d5817f4a88e4ad936653a
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -332,7 +332,6 @@ struct sdhci_ops {
 	int	(*enable_controller_clock)(struct sdhci_host *host);
 	void	(*clear_set_dumpregs)(struct sdhci_host *host, bool set);
 	void	(*enhanced_strobe_mask)(struct sdhci_host *host, bool set);
-	void	(*detect)(struct sdhci_host *host, bool detected);
 	int	(*notify_load)(struct sdhci_host *host, enum mmc_load state);
 	void	(*reset_workaround)(struct sdhci_host *host, u32 enable);
 	void	(*init)(struct sdhci_host *host);
