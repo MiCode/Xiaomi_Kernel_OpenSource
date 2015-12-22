0fd2aa9aeb08a17da3068c2605281cb432a4a790
Taniya Das <tdas@codeaurora.org>
2015-12-22 15:09:31 +0530

    ARM: dts: msm: Update frequency mapping for Vcodec on MSM8937
    The frequency precision has been updated for Vcodec, so update the same to
    reflect the code as well as device node of vcodec.
    
    Change-Id: I9ccfd01b91034cc26ac67debdc6688e5ab6a862b
    Signed-off-by: Taniya Das <tdas@codeaurora.org>

--- drivers/clk/msm/clock-gcc-8952.c
+++ drivers/clk/msm/clock-gcc-8952.c
@@ -590,7 +590,7 @@ static struct clk_freq_tbl ftbl_gcc_venus0_vcodec0_clk[] = {
 static struct clk_freq_tbl ftbl_gcc_venus0_vcodec0_clk_8937[] = {
 	F( 166150000,          gpll6,  6.5,    0,     0),
 	F( 240000000,          gpll6,  4.5,    0,     0),
-	F( 308571428,          gpll6,  3.5,    0,     0),
+	F( 308570000,          gpll6,  3.5,    0,     0),
 	F( 320000000,          gpll0,  2.5,    0,     0),
 	F( 360000000,          gpll6,    3,    0,     0),
 	F_END
