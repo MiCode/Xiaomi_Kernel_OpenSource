898728e145447bda0ad995342b72002dca70a0bf
Shashank Mittal <mittals@codeaurora.org>
2015-12-23 10:03:20 -0800

    coresight: add support to read AUTHSTATUS
    The status of the CoreSight fuses can be derived from the AUTHSTATUS
    register. This patch add support to read AUTHSTATUS, which could be used
    by Coresight device drivers to determine if that component is functional
    in hardware.
    
    Change-Id: I6b7ac2a3558c238ac5fca452ddfb6c88eef14b8b
    Signed-off-by: Shashank Mittal <mittals@codeaurora.org>

--- drivers/hwtracing/coresight/coresight-priv.h
+++ drivers/hwtracing/coresight/coresight-priv.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2012, 2016 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -50,6 +50,27 @@ static inline void CS_UNLOCK(void __iomem *addr)
 		/* Make sure everyone has seen this */
 		mb();
 	} while (0);
+}
+
+static inline bool coresight_authstatus_enabled(void __iomem *addr)
+{
+	int ret;
+	unsigned auth_val;
+
+	if (!addr)
+		return false;
+
+	auth_val = readl_relaxed(addr + CORESIGHT_AUTHSTATUS);
+
+	if ((0x2 == BMVAL(auth_val, 0, 1)) ||
+	    (0x2 == BMVAL(auth_val, 2, 3)) ||
+	    (0x2 == BMVAL(auth_val, 4, 5)) ||
+	    (0x2 == BMVAL(auth_val, 6, 7)))
+		ret = false;
+	else
+		ret = true;
+
+	return ret;
 }
 
 #ifdef CONFIG_CORESIGHT_SOURCE_ETM3X
