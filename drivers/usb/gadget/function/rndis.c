b9e9d8005632229eb826199e42070dc52c40dc76
Amit Pundir <amit.pundir@linaro.org>
2015-12-30 03:05:44 +0530

    usb: gadget: rndis: use %z format specifier for size_t
    Use '%z' format specifier for sizeof operator
    instead of '%u' to fix build warnings like:
    
        warning: format '%u' expects type 'unsigned int',
        but argument 3 has type 'long unsigned int'
    
    Change-Id: I8b3b57a1e2664367c685e9f3d6758f7ea39c4b56
    Git-Commit: 928b59a553baea2317a288dea5d957f9a9a9e8b6
    Git-repo: https://source.codeaurora.org/quic/la/kernel/msm-4.9
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    Signed-off-by: Sivasri Kumar Vanka <sivasri@codeaurora.org>

--- drivers/usb/gadget/function/rndis.c
+++ drivers/usb/gadget/function/rndis.c
@@ -1152,7 +1152,7 @@ int rndis_rm_hdr(struct gether *port,
 		}
 
 		if (skb->len < sizeof(*hdr)) {
-			pr_err("invalid rndis pkt: skblen:%u hdr_len:%lu",
+			pr_err("invalid rndis pkt: skblen:%u hdr_len:%zu",
 					skb->len, sizeof(*hdr));
 			dev_kfree_skb_any(skb);
 			return -EINVAL;
