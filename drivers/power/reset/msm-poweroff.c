a0830de1c5d56b2638ab73548726aebb811355cf
Lijuan Gao <lijuang@codeaurora.org>
2016-01-07 13:59:53 +0800

    drivers: power: reset: Do warm reset when device reboot to edl
    Device reboot to edl, it's for switching device to emergency dload mode.
    So it's need to keep device to do warm reset.
    
    Change-Id: I8f5d0845c6f2d37ea740949ce56e02c4cc7412be
    Signed-off-by: Lijuan Gao <lijuang@codeaurora.org>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -230,7 +230,9 @@ static void msm_restart_prepare(const char *cmd)
 
 	if (qpnp_pon_check_hard_reset_stored()) {
 		/* Set warm reset as true when device is in dload mode */
-		if (get_dload_mode())
+		if (get_dload_mode() ||
+			((cmd != NULL && cmd[0] != '\0') &&
+			!strcmp(cmd, "edl")))
 			need_warm_reset = true;
 	} else {
 		need_warm_reset = (get_dload_mode() ||
