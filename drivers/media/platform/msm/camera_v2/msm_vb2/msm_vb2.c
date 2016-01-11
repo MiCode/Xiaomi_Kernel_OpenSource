183516d967cd48d9549ba0973de23dcc26daa036
Trishansh Bhardwaj <tbhardwa@codeaurora.org>
2016-01-11 11:40:44 +0530

    msm: camera: Retelimit log in msm_vb2_buf_cleanup.
    Ratelimit log in msm_vb2_buf_cleanup to prevent excessive logging when
    stream is NULL.
    
    Change-Id: Ia687375c8e2a2683a4d32cd0eb984f731b2288e7
    Signed-off-by: Trishansh Bhardwaj <tbhardwa@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/msm_vb2/msm_vb2.c
+++ drivers/media/platform/msm/camera_v2/msm_vb2/msm_vb2.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -122,7 +122,7 @@ static void msm_vb2_stop_stream(struct vb2_queue *q)
 
 	stream = msm_get_stream_from_vb2q(q);
 	if (!stream) {
-		pr_err("%s:%d] NULL stream", __func__, __LINE__);
+		pr_err_ratelimited("%s:%d] NULL stream", __func__, __LINE__);
 		return;
 	}
 
