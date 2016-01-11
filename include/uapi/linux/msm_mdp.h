3182dd168b762e0f361eef86d4fe04e7d9db7a62
Jeykumar Sankaran <jsanka@codeaurora.org>
2016-01-11 11:56:13 -0800

    msm: mdss: Add mdss version for msmcobalt.
    This change adds MDSS Hardware revision for msmcobalt.
    
    Change-Id: I559be9fd98978927dff81ab19bff6b7171679428
    Signed-off-by: Jeykumar Sankaran <jsanka@codeaurora.org>
    Signed-off-by: Abhijit Kulkarni <kabhijit@codeaurora.org>
    CRs-Fixed: 979566

--- include/uapi/linux/msm_mdp.h
+++ include/uapi/linux/msm_mdp.h
@@ -110,6 +110,8 @@
 #define MDSS_MDP_HW_REV_114	MDSS_MDP_REV(1, 14, 0) /* 8937 v1.0 */
 #define MDSS_MDP_HW_REV_115	MDSS_MDP_REV(1, 15, 0) /* msm8917 */
 #define MDSS_MDP_HW_REV_116	MDSS_MDP_REV(1, 16, 0) /* msm8953 */
+#define MDSS_MDP_HW_REV_300	MDSS_MDP_REV(3, 0, 0)  /* msmcobalt */
+#define MDSS_MDP_HW_REV_301	MDSS_MDP_REV(3, 0, 1)  /* msmcobalt v1.0 */
 
 enum {
 	NOTIFY_UPDATE_INIT,
