bfd795ee3dd30109573e6252bbaf99f0d7f74f95
Patrick Bellasi <patrick.bellasi@arm.com>
2016-01-12 18:12:13 +0000

    sched/tune: add support to compute normalized energy
    The current EAS implementation considers only energy variations, while it
    disregards completely the impact on performance for the selection of
    a certain schedule candidate. Moreover, it also makes its decision based
    on the "absolute" value of expected energy variations.
    
    In order to properly define a trade-off strategy between increased energy
    consumption and performances benefits it is required to compare energy
    variations with performance variations.
    
    Thus, both performance and energy metrics must be expressed in comparable
    units. While the performance variations are expressed in terms of capacity
    deltas, which are defined in the range [0..SCHED_LOAD_SCALE], the same
    scale is not used for energy variations.
    
    This patch introduces the function:
      schedtune_normalize_energy(energy_diff)
    which returns a normalized value in the same range of capacity variations,
    i.e. [0..SCHED_LOAD_SCALE].
    
    A proper set of energy normalization constants are required to provide
    a fast division by a constant during the normalziation of the energy_diff.
    The value of these constants depends on the specific energy model and
    topology of a target device.
    Thus, this patch provides also the required support for the computation
    at boot time of this set of variables.
    
    Change-Id: I7a740ebc05383c62de6079c0cf866f05f8ac5ef4
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/tune.h
+++ kernel/sched/tune.h
@@ -9,6 +9,10 @@ int schedtune_task_boost(struct task_struct *tsk);
 void schedtune_enqueue_task(struct task_struct *p, int cpu);
 void schedtune_dequeue_task(struct task_struct *p, int cpu);
 
+int schedtune_normalize_energy(int energy);
+int schedtune_accept_deltas(int nrg_delta, int cap_delta,
+			    struct task_struct *task);
+
 #else /* CONFIG_CGROUP_SCHEDTUNE */
 
 #define schedtune_enqueue_task(task, cpu) do { } while (0)
@@ -20,5 +24,8 @@ void schedtune_dequeue_task(struct task_struct *p, int cpu);
 
 #define schedtune_enqueue_task(task, cpu) do { } while (0)
 #define schedtune_dequeue_task(task, cpu) do { } while (0)
+
+#define schedtune_normalize_energy(energy) energy
+#define schedtune_accept_deltas(nrg_delta, cap_delta, task) nrg_delta
 
 #endif /* CONFIG_SCHED_TUNE */
