db599a70cb5bcbab7db755a09c926057f0591ca8
Arun KS <arunks@codeaurora.org>
2016-01-21 13:57:23 +0530

    soc: qcom: pil-mss: Add scm call to inform TZ of modem area
    Add support to make scm_calls to TZ to inform modem start
    address and size so that TZ can unmap this range to avoid
    speculative access.
    
    Change-Id: I4640ddab56991522870e9879d17fe5732dc40223
    Signed-off-by: Avaneesh Kumar Dwivedi <akdwived@codeaurora.org>
    Signed-off-by: Arun KS <arunks@codeaurora.org>

--- drivers/soc/qcom/pil-msa.h
+++ drivers/soc/qcom/pil-msa.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -25,6 +25,7 @@ struct modem_data {
 	struct subsys_desc subsys_desc;
 	void *ramdump_dev;
 	bool crash_shutdown;
+	u32 pas_id;
 	bool ignore_errors;
 	struct completion stop_ack;
 	void __iomem *rmb_base;
--- include/soc/qcom/subsystem_restart.h
+++ include/soc/qcom/subsystem_restart.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -79,6 +79,7 @@ struct subsys_desc {
 	int shutdown_ack_gpio;
 	int ramdump_disable;
 	bool no_auth;
+	bool pil_mss_memsetup;
 	int ssctl_instance_id;
 	u32 sysmon_pid;
 	int sysmon_shutdown_ret;
