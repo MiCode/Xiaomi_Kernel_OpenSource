49b02fd9b6852f2284dc1774160d6329167a1570
Viraja Kommaraju <virajak@codeaurora.org>
2016-01-21 17:32:01 +0530

    ASoC: Add check for pointer and array index
    Add check for NULL pointer access and array index boundary.
    
    Change-Id: Ia12a28c43b99158a90d27eecd23bdfd4b42b346e
    Signed-off-by: Viraja Kommaraju <virajak@codeaurora.org>
    Signed-off-by: Meng Wang <mwang@codeaurora.org>

--- include/sound/pcm_params.h
+++ include/sound/pcm_params.h
@@ -110,10 +110,14 @@ static inline void snd_mask_reset_range(struct snd_mask *mask,
 
 static inline void snd_mask_leave(struct snd_mask *mask, unsigned int val)
 {
-	unsigned int v;
-	v = mask->bits[MASK_OFS(val)] & MASK_BIT(val);
-	snd_mask_none(mask);
-	mask->bits[MASK_OFS(val)] = v;
+	unsigned int v, bits_index;
+
+	bits_index = MASK_OFS(val);
+	if (bits_index < ((SNDRV_MASK_MAX+31)/32)) {
+		v = mask->bits[bits_index] & MASK_BIT(val);
+		snd_mask_none(mask);
+		mask->bits[bits_index] = v;
+	}
 }
 
 static inline void snd_mask_intersect(struct snd_mask *mask,
