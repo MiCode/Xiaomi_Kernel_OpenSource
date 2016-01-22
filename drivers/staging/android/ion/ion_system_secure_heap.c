7980e0c662de1fab6feb54cfd605a03b61e926d5
Patrick Daly <pdaly@codeaurora.org>
2016-01-22 15:27:46 -0800

    ion: Remove deferred free flag from system secure heap
    The system secure heap now uses page pooling. As a result, free
    operations are fast, since hyp_unassign is not necessary.
    
    Change-Id: I8da8f5adb32c089d5dcac0e560e305401d69fdc9
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

--- drivers/staging/android/ion/ion_system_secure_heap.c
+++ drivers/staging/android/ion/ion_system_secure_heap.c
@@ -320,7 +320,6 @@ struct ion_heap *ion_system_secure_heap_create(struct ion_platform_heap *unused)
 		return ERR_PTR(-ENOMEM);
 	heap->heap.ops = &system_secure_heap_ops;
 	heap->heap.type = ION_HEAP_TYPE_SYSTEM_SECURE;
-	heap->heap.flags = ION_HEAP_FLAG_DEFER_FREE;
 	heap->sys_heap = get_ion_heap(ION_SYSTEM_HEAP_ID);
 
 	heap->destroy_heap = false;
