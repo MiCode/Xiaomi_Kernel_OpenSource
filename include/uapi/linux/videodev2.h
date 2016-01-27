c136a71701e65a5ac97e45431e0390eea1af8d61
Vijay kumar Tumati <vtumati@codeaurora.org>
2016-01-27 15:10:21 +0530

    msm: camera: Add support for DPCM modes
    DPCM8 and DPCM6 are 10 bit to 8 and 10 bit
    to 6 bit compression formats respectively
    which sensor outputs and it is decompressed
    to 10 bit in CSID
    
    Change-Id: I8ec4de7d9cfa5e9c86d97bd9cb81feddc97dc7b8
    Signed-off-by: Vijay kumar Tumati <vtumati@codeaurora.org>
    Signed-off-by: Sureshnaidu Laveti <lsuresh@codeaurora.org>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -2,6 +2,7 @@
  *  Video for Linux Two header file
  *
  *  Copyright (C) 1999-2012 the contributors
+ *  Copyright (c) 2016, The Linux Foundation. All rights reserved.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -584,6 +585,11 @@ struct v4l2_pix_format {
 #define V4L2_PIX_FMT_SGRBG12 v4l2_fourcc('B', 'A', '1', '2') /* 12  GRGR.. BGBG.. */
 #define V4L2_PIX_FMT_SRGGB12 v4l2_fourcc('R', 'G', '1', '2') /* 12  RGRG.. GBGB.. */
 #define V4L2_PIX_FMT_SBGGR16 v4l2_fourcc('B', 'Y', 'R', '2') /* 16  BGBG.. GRGR.. */
+	/* 10bit raw bayer DPCM compressed to 6 bits */
+#define V4L2_PIX_FMT_SBGGR10DPCM6 v4l2_fourcc('b', 'B', 'A', '6')
+#define V4L2_PIX_FMT_SGBRG10DPCM6 v4l2_fourcc('b', 'G', 'A', '6')
+#define V4L2_PIX_FMT_SGRBG10DPCM6 v4l2_fourcc('B', 'D', '1', '6')
+#define V4L2_PIX_FMT_SRGGB10DPCM6 v4l2_fourcc('b', 'R', 'A', '6')
 
 /* compressed formats */
 #define V4L2_PIX_FMT_MJPEG    v4l2_fourcc('M', 'J', 'P', 'G') /* Motion-JPEG   */
