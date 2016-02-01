b21e23e099b166d54ff788a82e0e9addbd28cf62
Sudheer Papothi <spapothi@codeaurora.org>
2016-02-02 01:52:09 +0530

    ASoC: pcm: Check for capture path in all codec_dais
    During soc_pcm_prepare, check for capture path for all
    the codec_dais instead of the last codec_dai. This is
    will help to set pcm start event for all the codec dais
    that has capture_active set to high. Change checks for
    all the codec_dais capture_active bit to set start stream
    event.
    
    Signed-off-by: Sudheer Papothi <spapothi@codeaurora.org>

--- sound/soc/soc-pcm.c
+++ sound/soc/soc-pcm.c
@@ -817,10 +817,13 @@ static int soc_pcm_prepare(struct snd_pcm_substream *substream)
 	}
 
 	if (substream->stream == SNDRV_PCM_STREAM_CAPTURE) {
-		if (codec_dai->capture_active == 1)
-			snd_soc_dapm_stream_event(rtd,
-			SNDRV_PCM_STREAM_CAPTURE,
-			SND_SOC_DAPM_STREAM_START);
+		for (i = 0; i < rtd->num_codecs; i++) {
+			codec_dai = rtd->codec_dais[i];
+			if (codec_dai->capture_active == 1)
+				snd_soc_dapm_stream_event(rtd,
+				SNDRV_PCM_STREAM_CAPTURE,
+				SND_SOC_DAPM_STREAM_START);
+		}
 	}
 
 	for (i = 0; i < rtd->num_codecs; i++)
