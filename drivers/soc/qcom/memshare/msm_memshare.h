1c00b9fb2d0e92c634e298ed2c60033777d4dad4
Manoj Prabhu B <bmanoj@codeaurora.org>
2016-02-02 15:40:55 +0530

    memshare: Add support for hypervisor mapping
    This patch adds support for the hyp call to set up permissions
    for the memory allocated for the modem clients.
    This patch also identifies the debug messages with memshare
    driver.
    
    CRs-Fixed: 970173
    Change-Id: I862a3c21eb4f791d5c7d93d4fb523bf8714c20cb
    Signed-off-by: Katish Paran <kparan@codeaurora.org>
    Signed-off-by: Manoj Prabhu B <bmanoj@codeaurora.org>

--- drivers/soc/qcom/memshare/msm_memshare.h
+++ drivers/soc/qcom/memshare/msm_memshare.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -49,6 +49,8 @@ struct mem_blocks {
 	void *virtual_addr;
 	/* Release memory only when XPU is released*/
 	uint8_t free_memory;
+	/* Need Hypervisor mapping*/
+	uint8_t hyp_mapping;
 };
 
 int memshare_alloc(struct device *dev,
