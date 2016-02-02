83a293860e8b94dd8f58c32451714316410bf3a0
Yuanyuan Liu <yuanliu@codeaurora.org>
2016-02-01 16:47:48 -0800

    cnss: Fix compilation error of missing CONFIG_CNSS_SECURE_FW
    Add #ifdef CONFIG_CNSS_SECURE_FW for cnss_get_fw_ptr and
    cnss_get_sha_hash.
    
    Change-Id: I884b4ab3d552b12dd83f852be565a5dc4e69e21a
    CRs-Fixed: 1015558
    Signed-off-by: Yuanyuan Liu <yuanliu@codeaurora.org>

--- include/net/cnss.h
+++ include/net/cnss.h
@@ -120,9 +120,12 @@ extern int cnss_get_fw_files_for_target(struct cnss_fw_files *pfw_files,
 					u32 target_type, u32 target_version);
 
 extern int cnss_request_bus_bandwidth(int bandwidth);
+
+#ifdef CONFIG_CNSS_SECURE_FW
 extern int cnss_get_sha_hash(const u8 *data, u32 data_len,
 					u8 *hash_idx, u8 *out);
 extern void *cnss_get_fw_ptr(void);
+#endif
 
 extern int cnss_get_codeswap_struct(struct codeswap_codeseg_info *swap_seg);
 extern int cnss_get_bmi_setup(void);
