db2e1d7afd3427225a68cdb1cfb7d5dff012f0ab
Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
2016-02-03 16:23:58 +0530

    msm: thermal: Maintain state in the mitigation device monitor
    If KTM get a trip threshold trigger notification and if the
    temperature stays the same as the recent trip threshold, KTM will
    re-activate the recently triggered threshold, resulting in back to
    back interrupts. To avoid this add support in KTM to maintain the
    recently triggered threshold state and then re-active the threshold
    based on the last threshold trip.
    This state is updated for mitigation features like VDD MX retention,
    CX phase control, VDD restriction, OCR monitor and external clients
    like CPR low temperature monitor etc.
    
    CRs-Fixed: 969112 972634
    Change-Id: I44c0a93e1507a9f0b8a65e5c2ce5a98962bb335b
    Signed-off-by: Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -105,6 +105,7 @@ struct therm_threshold {
 	struct threshold_info            *parent;
 	enum msm_therm_progressive_state prog_state;
 	bool                             prog_trip_clear;
+	int32_t                          cur_state;
 };
 
 struct threshold_info {
