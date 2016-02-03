f5c8091ffedf875213f9b98115e6dd71d6ce3a7f
Sandeep Panda <spanda@codeaurora.org>
2016-02-03 11:10:35 +0530

    msm: mdss: fix NULL dereferencing issues in MDSS driver
    This change fixes multiple potential NULL pointer dereferencing
    issues in HDMI module, reported by static code analysis of MDSS
    driver.
    
    Change-Id: If2b9d8a33562ddac40172dc0665c598df7669367
    Signed-off-by: Sandeep Panda <spanda@codeaurora.org>

--- drivers/video/msm/mdss/mdss_hdmi_edid.c
+++ drivers/video/msm/mdss/mdss_hdmi_edid.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2010-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1674,8 +1674,7 @@ int hdmi_edid_parser(void *input)
 
 	if (!edid_ctrl) {
 		DEV_ERR("%s: invalid input\n", __func__);
-		status = -EINVAL;
-		goto error;
+		return -EINVAL;
 	}
 
 	/* reset edid data for new hdmi connection */
