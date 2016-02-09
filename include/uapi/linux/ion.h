be3345a0327b45f9c1334fe44f830bfe3f92a761
Jeremy Gebben <jgebben@codeaurora.org>
2016-02-09 13:35:27 -0700

    uapi: fix export of msm_ion.h
    Make this file be exported via symlink like the rest
    of the android staging uapi headers.
    
    Signed-off-by: Jeremy Gebben <jgebben@codeaurora.org>

--- include/uapi/linux/ion.h
+++ include/uapi/linux/ion.h
@@ -1,6 +1,1 @@
-#ifndef __UAPI_LINUX_ION_H__
-#define __UAPI_LINUX_ION_H__
-
-#include <linux/staging/android/uapi/ion.h>
-
-#endif /* __UAPI_LINUX_ION_H__ */
+../../../drivers/staging/android/uapi/ion.h
