bfa70fb909970a1d18f0eedd48520f81d49e91fc
Jeremy Gebben <jgebben@codeaurora.org>
2016-02-10 10:38:11 -0700

    msm: secure_buffer: fix stub functions
    All of these need to be 'static inline' to avoid multiple
    definition problems when MSM_SECURE_BUFFER=n.
    
    Signed-off-by: Jeremy Gebben <jgebben@codeaurora.org>

--- include/soc/qcom/secure_buffer.h
+++ include/soc/qcom/secure_buffer.h
@@ -58,7 +58,7 @@ static inline int msm_unsecure_table(struct sg_table *table)
 {
 	return -EINVAL;
 }
-int hyp_assign_table(struct sg_table *table,
+static inline int hyp_assign_table(struct sg_table *table,
 			u32 *source_vm_list, int source_nelems,
 			int *dest_vmids, int *dest_perms,
 			int dest_nelems)
@@ -77,7 +77,7 @@ static inline bool msm_secure_v2_is_supported(void)
 {
 	return false;
 }
-const char *msm_secure_vmid_to_string(int secure_vmid)
+static inline const char *msm_secure_vmid_to_string(int secure_vmid)
 {
 	return "N/A";
 }
