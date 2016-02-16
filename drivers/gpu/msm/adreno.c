a3019edaba0394c41dfba9fc61d7d23ecadd3195
Divya Ponnusamy <pdivya@codeaurora.org>
2016-02-16 14:43:45 +0530

    msm: kgsl: Add pm_qos_cpu_mask_latency to avoid L2PC on mask CPU
    Add a l2pc-cpu-mask-latency in device tree. This latency
    is used in kgsl_pwrctrl_update_l2pc() API to avoid L2PC
    on masked CPUs by giving reduced latency value.
    
    Change-Id: I0447977bce5ed5c09a863b03bb42b9428686a9f5
    Signed-off-by: Divya Ponnusamy <pdivya@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -832,6 +832,11 @@ static int adreno_of_get_power(struct adreno_device *adreno_dev,
 		&device->pwrctrl.pm_qos_active_latency))
 		device->pwrctrl.pm_qos_active_latency = 501;
 
+	/* get pm-qos-cpu-mask-latency, set it to default if not found */
+	if (of_property_read_u32(node, "qcom,l2pc-cpu-mask-latency",
+		&device->pwrctrl.pm_qos_cpu_mask_latency))
+		device->pwrctrl.pm_qos_cpu_mask_latency = 501;
+
 	/* get pm-qos-wakeup-latency, set it to default if not found */
 	if (of_property_read_u32(node, "qcom,pm-qos-wakeup-latency",
 		&device->pwrctrl.pm_qos_wakeup_latency))
