1f6c29a46fa0b4fededae0357f854f0c34f3a769
Girish Mahadevan <girishm@codeaurora.org>
2016-02-17 15:25:01 -0700

    slimbus: Add snapshot of slimbus driver
    Add snapshot of slimbus driver from msm-3.18 branch.
    Baseline:
    e70ad0cd5efdd9dc91a77dcdac31d6132e1315c1
    
    Change-Id: I82f8f91596d2c3e9ef111e26c80298d990f6c193
    Signed-off-by: Girish Mahadevan <girishm@codeaurora.org>

--- include/linux/mod_devicetable.h
+++ include/linux/mod_devicetable.h
@@ -481,6 +481,16 @@ struct dmi_system_id {
 	struct dmi_strmatch matches[4];
 	void *driver_data;
 };
+
+#define SLIMBUS_NAME_SIZE	32
+#define SLIMBUS_MODULE_PREFIX "slim:"
+
+struct slim_device_id {
+	char name[SLIMBUS_NAME_SIZE];
+	kernel_ulong_t driver_data	/* Data private to the driver */
+			__attribute__((aligned(sizeof(kernel_ulong_t))));
+};
+
 /*
  * struct dmi_device_id appears during expansion of
  * "MODULE_DEVICE_TABLE(dmi, x)". Compiler doesn't look inside it
