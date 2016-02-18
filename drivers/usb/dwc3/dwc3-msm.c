5ff33ae3c4007b96a9d41c261cb2676c4851e860
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2016-02-18 10:47:06 +0530

    USB: dwc3-msm: Turn on iface_clk before accessing AHB2PHY registers
    On some platforms, USB3 controller sits on PCNOC instead of SNOC.
    In this case, it is required to turn on iface_clk(pcnoc_axi_clk)
    and cfg_ahb_clk before accessing AHB2PHY register. Hence make
    required change for turning on iface_clk before AHB2PHY register
    access.
    
    Change-Id: Idf069a07edfb3e03e62cefa91fbeaa4f297af89a
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -2779,6 +2779,7 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 			 * turning on usb gdsc regulator clk is stuck off.
 			 */
 			dwc3_msm_config_gdsc(mdwc, 1);
+			clk_prepare_enable(mdwc->iface_clk);
 			clk_prepare_enable(mdwc->cfg_ahb_clk);
 			/* Configure AHB2PHY for one wait state read/write*/
 			val = readl_relaxed(mdwc->ahb2phy_base +
@@ -2791,6 +2792,7 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 				mb();
 			}
 			clk_disable_unprepare(mdwc->cfg_ahb_clk);
+			clk_disable_unprepare(mdwc->iface_clk);
 			dwc3_msm_config_gdsc(mdwc, 0);
 		}
 	}
