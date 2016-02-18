e26db65c3f084d0874cf65ce79d582ddeb4c145f
Srinu Gorle <sgorle@codeaurora.org>
2016-02-18 00:11:42 -0800

    Revert "msm: vidc: Pre-allocate biggest internal buffers for secure"
    For slave side content protection targets, memory alignment is 1MB.
    Allocating biggest internal buffers size need more CMA heap size,
    skip allocating max internal buffers size for slave side CP targets.
    
    This reverts commit 83ab881ac0b3e84e7921b12bf32f56890e2a78d3
    
    Change-Id: I9f68ea9ed0d0e4c13dd9aad0bbefe5493233fe9e
    Signed-off-by: Srinu Gorle <sgorle@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc_common.c
+++ drivers/media/platform/msm/vidc/msm_vidc_common.c
@@ -3120,8 +3120,6 @@ static bool reuse_internal_buffers(struct msm_vidc_inst *inst,
 			}
 		}
 		reused = true;
-		dprintk(VIDC_DBG,
-			"Re-using internal buffer type : %d\n", buffer_type);
 	}
 	mutex_unlock(&buf_list->lock);
 	return reused;
