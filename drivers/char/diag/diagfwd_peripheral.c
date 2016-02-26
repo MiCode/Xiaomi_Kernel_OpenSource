949dc4ed5fb52d5ab56126fb5cf6800e9c2f5f6a
Mohit Aggarwal <maggarwa@codeaurora.org>
2016-02-26 13:56:53 -0800

    diag: Add mutex protection while closing SMD
    On receiving feature mask add mutex protection while closing
    SMD and start reading from socket channel
    
    CRs-Fixed: 980999
    Change-Id: I01e5e8c9e0b03fa8b09f0ffd6857fe7e60a48091
    Signed-off-by: Sreelakshmi Gownipalli <sgownipa@codeaurora.org>
    Signed-off-by: Mohit Aggarwal <maggarwa@codeaurora.org>

--- drivers/char/diag/diagfwd_peripheral.c
+++ drivers/char/diag/diagfwd_peripheral.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2015-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -646,6 +646,7 @@ void diagfwd_close_transport(uint8_t transport, uint8_t peripheral)
 	fwd_info = &early_init_info[transport][peripheral];
 	if (fwd_info->p_ops && fwd_info->p_ops->close)
 		fwd_info->p_ops->close(fwd_info->ctxt);
+	mutex_lock(&driver->diagfwd_channel_mutex);
 	fwd_info = &early_init_info[transport_open][peripheral];
 	dest_info = &peripheral_info[TYPE_CNTL][peripheral];
 	dest_info->inited = 1;
@@ -664,6 +665,7 @@ void diagfwd_close_transport(uint8_t transport, uint8_t peripheral)
 		diagfwd_late_open(dest_info);
 	diagfwd_cntl_open(dest_info);
 	init_fn(peripheral);
+	mutex_unlock(&driver->diagfwd_channel_mutex);
 	diagfwd_queue_read(&peripheral_info[TYPE_DATA][peripheral]);
 	diagfwd_queue_read(&peripheral_info[TYPE_CMD][peripheral]);
 }
