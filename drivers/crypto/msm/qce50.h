fb490623dd6eedd0f25dc3c6ab55d47b9f9e8d80
Sivanesan Rajapupathi <srajap@codeaurora.org>
2016-02-26 09:54:44 -0500

    crypto: msm: crypto driver performance improvement
    To minimize spinlock, the qce50 client is assumed that it can only issue
    request to qce50 driver one at a time. After a request is issued to
    qce50 from qcrypto. Without waiting for completion, other requests can
    still be issued until the maximum limit of outstanding requests
    in qce50 reaches.
    
    To cut down the chance of udp socket receive buffer overflow the following
    schemes are provided -
    The number of bunched requests in qce50 is based on the data length of
    the current request to cut down delay for smaller packets. In turn,
    with smaller delay, the number of completed requests to process in
    seq_response() completion function is less.
    
    The scheduling of qcrypto requests are changed from least use to
    round robin. This way, the distribution of requests to each engine
    is more even. As the result, reordering of completed requests will
    be less. Completed requests to handle in completion callback is less
    at a time.
    
    Change-Id: I723bac2f9427cddb5248101c9ac3f2b595ad0379
    Acked-by: Che-Min Hsieh <cheminh@qti.qualcomm.com>
    Signed-off-by: Sivanesan Rajapupathi <srajap@codeaurora.org>

--- drivers/crypto/msm/qce50.h
+++ drivers/crypto/msm/qce50.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -228,6 +228,7 @@ struct ce_request_info {
 	dma_addr_t phy_ota_src;
 	dma_addr_t phy_ota_dst;
 	unsigned int ota_size;
+	unsigned int req_len;
 };
 
 struct qce_driver_stats {
