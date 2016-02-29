95bb8dae03e280f8a47108e931d3ce621dca86a8
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2016-02-29 19:03:12 +0530

    clk: msm: mdss: update the frame rate count supported for DFPS
    Update the number of frame rates supported for dynamic refresh
    feature from 10 to 20. This is needed to support all the fps
    values requested by display HAL between minimum fps and maxiumum
    fps based on the requirement.
    
    Change-Id: Ib7487ad17261ac8c4d6929787899161061e71078
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- drivers/clk/qcom/mdss/mdss-pll.h
+++ drivers/clk/qcom/mdss/mdss-pll.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -41,7 +41,7 @@ enum {
 	MDSS_PLL_TARGET_8996,
 };
 
-#define DFPS_MAX_NUM_OF_FRAME_RATES 10
+#define DFPS_MAX_NUM_OF_FRAME_RATES 20
 
 struct dfps_panel_info {
 	uint32_t enabled;
