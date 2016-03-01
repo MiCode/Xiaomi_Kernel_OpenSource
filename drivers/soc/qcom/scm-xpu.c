MotorolaMobilityLLC/kernel-msm

soc: qcom: scm-xpu: add support for XPU errors that are fatal by default
The existing scm-xpu implementation makes an unconditional scm
call at boot. The assumption was that such errors would be non-
fatal by default, and so a desired behavior could be achieved
by controlling whether the driver was compiled.

On recent targets like msm8996, XPU errors are treated as
fatal by default. To support cases where it may be desirable
to treat them as non-fatal, the driver must be changed to
make the appropriate scm call based on a new Kconfig choice.

Change-Id: Ibb55c062937f130c13538f049582575d9a6f52ad
Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

drivers/soc/qcom/scm-xpu.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -14,10 +14,12 @@
 #include <linux/kernel.h>
 #include <soc/qcom/scm.h>
 
+#if defined(CONFIG_MSM_XPU_ERR_FATAL)
+#define ERR_FATAL_VAL 0x0
+#elif defined(CONFIG_MSM_XPU_ERR_NONFATAL)
+#define ERR_FATAL_VAL 0x1
+#endif
 
-#define ERR_FATAL_ENABLE 0x0
-#define ERR_FATAL_DISABLE 0x1
-#define ERR_FATAL_READ 0x2
 #define XPU_ERR_FATAL 0xe
 
 static int __init xpu_err_fatal_init(void)
@@ -30,7 +32,7 @@ static int __init xpu_err_fatal_init(void)
 	struct scm_desc desc = {0};
 
 	desc.arginfo = SCM_ARGS(2);
-	desc.args[0] = cmd.config = ERR_FATAL_ENABLE;
+	desc.args[0] = cmd.config = ERR_FATAL_VAL;
 	desc.args[1] = cmd.spare = 0;
 
 	if (!is_scm_armv8())

MiCode/Xiaomi_Kernel_OpenSource

drivers/soc/qcom/scm-xpu.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -14,10 +14,12 @@
 #include <linux/kernel.h>
 #include <soc/qcom/scm.h>
 
+#if defined(CONFIG_QCOM_XPU_ERR_FATAL)
+#define ERR_FATAL_VAL 0x0
+#elif defined(CONFIG_QCOM_XPU_ERR_NONFATAL)
+#define ERR_FATAL_VAL 0x1
+#endif
 
-#define ERR_FATAL_ENABLE 0x0
-#define ERR_FATAL_DISABLE 0x1
-#define ERR_FATAL_READ 0x2
 #define XPU_ERR_FATAL 0xe
 
 static int __init xpu_err_fatal_init(void)
@@ -30,7 +32,7 @@ static int __init xpu_err_fatal_init(void)
 	struct scm_desc desc = {0};
 
 	desc.arginfo = SCM_ARGS(2);
-	desc.args[0] = cmd.config = ERR_FATAL_ENABLE;
+	desc.args[0] = cmd.config = ERR_FATAL_VAL;
 	desc.args[1] = cmd.spare = 0;
 
 	if (!is_scm_armv8())