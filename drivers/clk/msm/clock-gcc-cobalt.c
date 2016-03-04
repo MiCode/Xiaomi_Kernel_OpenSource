ac6a03867d0677ea41c6036d221a84c420bd8d28
Deepak Katragadda <dkatraga@codeaurora.org>
2016-03-04 10:51:38 -0800

    clk: msm: clock-gcc-cobalt: Source HMSS RCGs off cxo ao input
    The hmss_gpll0_clk_src is being sourced off the gpll0 which
    uses the cxo_clk_src RPM resource. This causes XO shutdown
    to fail. Use the gpll0_ao source instead.
    The hmss_ahb_clk_src RCG frequency table is also updated to
    use the cxo_clk_src_ao to generate XO frequency.
    
    CRs-Fixed: 1001330
    Change-Id: Ic5cba530ea22cd19a20a21f0c33433c5e023debc
    Signed-off-by: Deepak Katragadda <dkatraga@codeaurora.org>

--- drivers/clk/msm/clock-gcc-cobalt.c
+++ drivers/clk/msm/clock-gcc-cobalt.c
@@ -39,6 +39,7 @@ static void __iomem *virt_dbgbase;
 #define cxo_clk_src_source_val 0
 #define cxo_clk_src_ao_source_val 0
 #define gpll0_out_main_source_val 1
+#define gpll0_ao_source_val 1
 #define gpll4_out_main_source_val 5
 
 #define FIXDIV(div) (div ? (2 * (div) - 1) : (0))
@@ -197,7 +198,7 @@ static struct pll_vote_clk gpll4 = {
 DEFINE_EXT_CLK(gpll4_out_main, &gpll4.c);
 
 static struct clk_freq_tbl ftbl_hmss_ahb_clk_src[] = {
-	F(  19200000,    cxo_clk_src,    1,    0,     0),
+	F(  19200000, cxo_clk_src_ao,    1,    0,     0),
 	F(  37500000, gpll0_out_main,   16,    0,     0),
 	F(  75000000, gpll0_out_main,    8,    0,     0),
 	F_END
@@ -1015,7 +1016,7 @@ static struct rcg_clk usb3_phy_aux_clk_src = {
 };
 
 static struct clk_freq_tbl ftbl_hmss_gpll0_clk_src[] = {
-	F( 300000000,  gpll0_out_main,    2,    0,     0),
+	F( 300000000,  gpll0_ao,    2,    0,     0),
 	F_END
 };
 
