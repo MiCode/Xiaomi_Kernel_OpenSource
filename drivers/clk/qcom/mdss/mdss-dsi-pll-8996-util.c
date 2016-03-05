1f6c7da0f650e0aaa447e9db15e67cadb6e34cd1
Rohit Vaswani <rvaswani@codeaurora.org>
2016-03-04 18:07:09 -0800

    drivers: iommu, leds, input, clk, devfreq: fix warnings.
    Fix some variable initializations which would otherwise cause
    forbidden warnings.
    
    Change-Id: I4066ebf3cb18bf5e2749a44edb137458b7c525d8
    Signed-off-by: Rohit Vaswani <rvaswani@codeaurora.org>
    Signed-off-by: David Keitel <dkeitel@codeaurora.org>
    Signed-off-by: Narendra Muppalla <narendram@codeaurora.org>

--- drivers/clk/qcom/mdss/mdss-dsi-pll-8996-util.c
+++ drivers/clk/qcom/mdss/mdss-dsi-pll-8996-util.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2015-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -317,7 +317,7 @@ init_lock_err:
 
 static int dsi_pll_enable(struct clk *c)
 {
-	int i, rc;
+	int i, rc = 0;
 	struct dsi_pll_vco_clk *vco = to_vco_clk(c);
 	struct mdss_pll_resources *pll = vco->priv;
 
