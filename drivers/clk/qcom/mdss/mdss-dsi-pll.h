5773d73a50771131a2e36f9d1c10d293ce920507
Aravind Venkateswaran <aravindh@codeaurora.org>
2016-03-08 16:55:01 -0800

    clk: msm: mdss: add support for dsi pll on msm8998
    Add support to program the DSI PLL on msm8998 which is needed to drive
    the DSI byte and pixel clocks.
    
    CRs-Fixed: 1000576
    Change-Id: Ic11a3747a0e008e1f71df91a1a79d33242d2a2a4
    Signed-off-by: Aravind Venkateswaran <aravindh@codeaurora.org>
    Signed-off-by: Narendra Muppalla <narendram@codeaurora.org>

--- drivers/clk/qcom/mdss/mdss-dsi-pll.h
+++ drivers/clk/qcom/mdss/mdss-dsi-pll.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -65,6 +65,8 @@ int dsi_pll_clock_register_lpm(struct platform_device *pdev,
 				struct mdss_pll_resources *pll_res);
 int dsi_pll_clock_register_8996(struct platform_device *pdev,
 				struct mdss_pll_resources *pll_res);
+int dsi_pll_clock_register_8998(struct platform_device *pdev,
+				  struct mdss_pll_resources *pll_res);
 
 int set_byte_mux_sel(struct mux_clk *clk, int sel);
 int get_byte_mux_sel(struct mux_clk *clk);
