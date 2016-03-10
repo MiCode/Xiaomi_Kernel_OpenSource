271cf2ffedb2f67665cadf6b32b2fbcec0a9ca19
Yeleswarapu Nagaradhesh <nagaradh@codeaurora.org>
2016-03-10 16:33:37 +0530

    ASoC: audio-ext-clk: Change gpio default value to -1
    During stability test, gpio_direction_output is
    invoked with msm gpio 0 which is leading to PC NOC error
    as audio is not expected to access this gpio.
    Hence initialise gpio to -1 and check if gpio
    is valid before using.
    
    CRs-Fixed: 973438
    Change-Id: I32d779974f4eb497c62035f7f46c10739ebcfe5f
    Signed-off-by: Yeleswarapu Nagaradhesh <nagaradh@codeaurora.org>

--- sound/soc/codecs/audio-ext-clk.c
+++ sound/soc/codecs/audio-ext-clk.c
@@ -87,7 +87,9 @@ static int audio_ext_clk_prepare(struct clk *clk)
 	struct audio_ext_ap_clk *audio_clk = to_audio_ap_clk(clk);
 
 	pr_debug("%s: gpio: %d\n", __func__, audio_clk->gpio);
-	return gpio_direction_output(audio_clk->gpio, 1);
+	if (gpio_is_valid(audio_clk->gpio))
+		return gpio_direction_output(audio_clk->gpio, 1);
+	return 0;
 }
 
 static void audio_ext_clk_unprepare(struct clk *clk)
@@ -95,7 +97,8 @@ static void audio_ext_clk_unprepare(struct clk *clk)
 	struct audio_ext_ap_clk *audio_clk = to_audio_ap_clk(clk);
 
 	pr_debug("%s: gpio: %d\n", __func__, audio_clk->gpio);
-	gpio_direction_output(audio_clk->gpio, 0);
+	if (gpio_is_valid(audio_clk->gpio))
+		gpio_direction_output(audio_clk->gpio, 0);
 }
 
 static inline struct audio_ext_ap_clk2 *to_audio_ap_clk2(struct clk *clk)
@@ -237,6 +240,7 @@ static struct clk_ops audio_ext_lpass_mclk_ops = {
 };
 
 static struct audio_ext_pmi_clk audio_pmi_clk = {
+	.gpio = -EINVAL,
 	.c = {
 		.dbg_name = "audio_ext_pmi_clk",
 		.ops = &clk_ops_dummy,
@@ -245,6 +249,7 @@ static struct audio_ext_pmi_clk audio_pmi_clk = {
 };
 
 static struct audio_ext_ap_clk audio_ap_clk = {
+	.gpio = -EINVAL,
 	.c = {
 		.dbg_name = "audio_ext_ap_clk",
 		.ops = &audio_ext_ap_clk_ops,
