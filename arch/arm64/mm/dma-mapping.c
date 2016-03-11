ee80476115ba54018d735cfc556a2a643d197750
Vinayak Menon <vinmenon@codeaurora.org>
2016-03-11 18:24:49 +0530

    arm/arm64: dma-mapping: flush the tlb on unremap
    Make sure there are no stale tlb entries when
    dma_unremap returns, thus preventing speculative
    fetches.
    
    Change-Id: I22070de282f25fe5ea20177e67a6d629123e29a4
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    Signed-off-by: Ramesh Gupta Guntha <rgguntha@codeaurora.org>

--- arch/arm64/mm/dma-mapping.c
+++ arch/arm64/mm/dma-mapping.c
@@ -456,6 +456,8 @@ static void arm64_dma_unremap(struct device *dev, void *remapped_addr,
 		return;
 	}
 	vunmap(remapped_addr);
+	flush_tlb_kernel_range((unsigned long)remapped_addr,
+			(unsigned long)(remapped_addr + size));
 }
 
 const struct dma_map_ops noncoherent_swiotlb_dma_ops = {
