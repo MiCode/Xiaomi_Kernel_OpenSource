28e3a4040ee49cd184a9868a11305fa7adb8f1d4
Gilad Broner <gbroner@codeaurora.org>
2016-03-13 15:58:50 +0200

    selinux: removed redundant headers from objsec.h
    flask.h was included in the above file. This is autogenerated header
    which gets produced during compilation of selinux. This causes issues
    for other module (PFK) which includes objsec.h and compiles after
    selinux. Since removal of the above headers does not break compilation,
    they seem to be redundant and can be removed.
    
    Signed-off-by: Gilad Broner <gbroner@codeaurora.org>

--- security/selinux/include/objsec.h
+++ security/selinux/include/objsec.h
@@ -25,8 +25,7 @@
 #include <linux/in.h>
 #include <linux/spinlock.h>
 #include <net/net_namespace.h>
-#include "flask.h"
-#include "avc.h"
+#include "security.h"
 
 struct task_security_struct {
 	u32 osid;		/* SID prior to last execve */
