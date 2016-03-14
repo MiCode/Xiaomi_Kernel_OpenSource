cc1f245678051ff224e9aca9502ae430a8bdf5d9
Sebastian Ott <sebott@linux.vnet.ibm.com>
2016-03-14 15:47:23 +0100

    s390/pci: enforce fmb page boundary rule
    [ Upstream commit 80c544ded25ac14d7cc3e555abb8ed2c2da99b84 ]
    
    The function measurement block must not cross a page boundary. Ensure
    that by raising the alignment requirement to the smallest power of 2
    larger than the size of the fmb.
    
    Fixes: d0b088531 ("s390/pci: performance statistics and debug infrastructure")
    Cc: stable@vger.kernel.org # v3.8+
    Signed-off-by: Sebastian Ott <sebott@linux.vnet.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>

--- arch/s390/include/asm/pci.h
+++ arch/s390/include/asm/pci.h
@@ -44,7 +44,7 @@ struct zpci_fmb {
 	u64 rpcit_ops;
 	u64 dma_rbytes;
 	u64 dma_wbytes;
-} __packed __aligned(16);
+} __packed __aligned(64);
 
 #define ZPCI_MSI_VEC_BITS	11
 #define ZPCI_MSI_VEC_MAX	(1 << ZPCI_MSI_VEC_BITS)
