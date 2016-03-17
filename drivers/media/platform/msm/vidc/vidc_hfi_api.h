ce60f3d6b138e929af5a218f9416469079ea3868
Arun Menon <avmenon@codeaurora.org>
2016-03-16 18:37:22 -0700

    msm: vidc: Add support for output crop extra data
    Venus firmware reports the crop dimensions for
    each FBD in the extra data buffer. This change is
    needed to support the new extra data type -
    MSM_VIDC_EXTRADATA_OUTPUT_CROP.
    
    CRs-Fixed: 999551
    Change-Id: I3d1aa4a44701192e4bd85bdee29d6f2a49b9e5a7
    Signed-off-by: Arun Menon <avmenon@codeaurora.org>

--- drivers/media/platform/msm/vidc/vidc_hfi_api.h
+++ drivers/media/platform/msm/vidc/vidc_hfi_api.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -121,6 +121,7 @@ enum hal_extradata_id {
 	HAL_EXTRADATA_VQZIP_SEI,
 	HAL_EXTRADATA_YUV_STATS,
 	HAL_EXTRADATA_ROI_QP,
+	HAL_EXTRADATA_OUTPUT_CROP,
 };
 
 enum hal_property {
