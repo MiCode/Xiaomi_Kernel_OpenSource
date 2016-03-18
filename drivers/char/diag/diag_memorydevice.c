2e35a96d771f1243e78a100fa59c6ab84c8ba630
Sreelakshmi Gownipalli <sgownipa@codeaurora.org>
2016-03-18 16:02:44 -0700

    diag: Initialize spin lock once per memory device channel
    Initialize the spin lock only once per memory device channel
    while initializing the channel and channel buffers.
    
    Change-Id: Iabf2db03436102310ef85f04b59e6160d49ef5f5
    Signed-off-by: Sreelakshmi Gownipalli <sgownipa@codeaurora.org>

--- drivers/char/diag/diag_memorydevice.c
+++ drivers/char/diag/diag_memorydevice.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2015, 2016 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -356,8 +356,8 @@ int diag_md_init()
 			ch->tbl[j].buf = NULL;
 			ch->tbl[j].len = 0;
 			ch->tbl[j].ctx = 0;
-			spin_lock_init(&(ch->lock));
 		}
+		spin_lock_init(&(ch->lock));
 	}
 
 	return 0;
