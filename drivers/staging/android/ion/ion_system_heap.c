6cfee7505366d57d681b62e146ee1f1284a5c902
Patrick Daly <pdaly@codeaurora.org>
2016-03-18 18:01:35 -0700

    ion: Ensure pooled pages use proper free() function
    In upstream change ecd32842283c64b14243be9b20b60f351aea1b5d
    ("ion: Handle the memory mapping correctly on x86")
    
    pages allocated from the ion page pool may have special page table
    attributes. These attributes must be unset before the page is returned
    to the buddy system.
    
    This issue was introduced by an incorrect cherry-pick of the above
    change.
    
    CRs-Fixed: 997730
    Change-Id: Ia9339e78a4fa3f4734b6c75bb35d226f293dafb5
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

--- drivers/staging/android/ion/ion_system_heap.c
+++ drivers/staging/android/ion/ion_system_heap.c
@@ -128,7 +128,11 @@ static void free_buffer_page(struct ion_system_heap *heap,
 			pool = heap->cached_pools[order_to_index(order)];
 		else
 			pool = heap->uncached_pools[order_to_index(order)];
-		ion_page_pool_free(pool, page, prefetch);
+
+		if (buffer->private_flags & ION_PRIV_FLAG_SHRINKER_FREE)
+			ion_page_pool_free_immediate(pool, page);
+		else
+			ion_page_pool_free(pool, page, prefetch);
 	} else {
 		__free_pages(page, order);
 	}
