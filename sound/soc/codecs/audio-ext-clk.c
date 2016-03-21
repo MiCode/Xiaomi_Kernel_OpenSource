166dbe1734e504ff247bc55814419f1b6808eaba
Deven Patel <cdevenp@codeaurora.org>
2016-03-21 10:26:17 -0700

    ASoC: codecs: Use secondary MI2S port to start MCLK
    The primary MI2S port may already be started at the time of
    MCLK configuration, which will lead to failure to start the clock.
    Using secondary port fixes this issue.
    
    CRs-fixed: 989774
    Change-Id: I9a8c823d605547676fc4fff05ea520244f4c6d13
    Signed-off-by: Deven Patel <cdevenp@codeaurora.org>

--- sound/soc/codecs/audio-ext-clk.c
+++ sound/soc/codecs/audio-ext-clk.c
@@ -176,7 +176,7 @@ static int audio_ext_lpass_mclk_prepare(struct clk *clk)
 	memcpy(lpass_clk, &lpass_default, sizeof(struct afe_clk_cfg));
 	lpass_clk->clk_val2 = Q6AFE_LPASS_OSR_CLK_12_P288_MHZ;
 	lpass_clk->clk_set_mode = Q6AFE_LPASS_MODE_CLK2_VALID;
-	ret = afe_set_lpass_clock(MI2S_TX, lpass_clk);
+	ret = afe_set_lpass_clock(AFE_PORT_ID_SECONDARY_MI2S_RX, lpass_clk);
 	if (ret < 0) {
 		pr_err("%s afe_set_lpass_clock failed, ret = %d\n",
 			__func__, ret);
@@ -216,7 +216,7 @@ static void audio_ext_lpass_mclk_unprepare(struct clk *clk)
 	memcpy(lpass_clk, &lpass_default, sizeof(struct afe_clk_cfg));
 	lpass_clk->clk_val2 = 0;
 	lpass_clk->clk_set_mode = Q6AFE_LPASS_MODE_CLK2_VALID;
-	ret = afe_set_lpass_clock(MI2S_TX, lpass_clk);
+	ret = afe_set_lpass_clock(AFE_PORT_ID_SECONDARY_MI2S_RX, lpass_clk);
 	if (ret < 0)
 		pr_err("%s: afe_set_lpass_clock failed, ret = %d\n",
 			__func__, ret);
