217426d4c2ea6bda5205ca4b2b7f40128b12316e
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2016-03-21 15:54:35 -0700

    power_supply: add support to control hvdcp3 detection
    Currently, HVDCP3 detection is enabled by default. Add support
    to control it dynamically through a power supply property
    "allow_hvdcp3".
    
    CRs-Fixed: 991060
    Change-Id: Id283b572cdb51094ad53d590e230104dc3e1beb7
    Signed-off-by: Subbaraman Narayanamurthy <subbaram@codeaurora.org>
    Signed-off-by: Shilpa Suresh <sbsure@codeaurora.org>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -310,6 +310,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(battery_info),
 	POWER_SUPPLY_ATTR(battery_info_id),
 	POWER_SUPPLY_ATTR(enable_jeita_detection),
+	POWER_SUPPLY_ATTR(allow_hvdcp3),
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_ATTR(charge_counter_ext),
 	/* Properties of type `const char *' */
--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -265,6 +265,7 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_BATTERY_INFO,
 	POWER_SUPPLY_PROP_BATTERY_INFO_ID,
 	POWER_SUPPLY_PROP_ENABLE_JEITA_DETECTION,
+	POWER_SUPPLY_PROP_ALLOW_HVDCP3,
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_PROP_CHARGE_COUNTER_EXT,
 	/* Properties of type `const char *' */
