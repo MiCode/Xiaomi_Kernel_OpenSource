500142142449d0bbe9936df20212b9720c6e8d51
Mao Li <maol@codeaurora.org>
2016-03-22 17:48:04 +0800

    input: synaptics_dsx_2.6: remove fake touch points
    Update F12 touch report function with adding input_sync to
    correct the pointer reporting status.
    
    Change-Id: Iefe24525a9551736a6044697b447a25e2c5c65ca
    Signed-off-by: Mao Li <maol@codeaurora.org>

--- drivers/input/touchscreen/synaptics_dsx_2.6/synaptics_dsx_core.c
+++ drivers/input/touchscreen/synaptics_dsx_2.6/synaptics_dsx_core.c
@@ -1370,7 +1370,6 @@ static int synaptics_rmi4_f12_abs_report(struct synaptics_rmi4_data *rmi4_data,
 #ifndef TYPE_B_PROTOCOL
 			input_mt_sync(rmi4_data->input_dev);
 #endif
-			input_sync(rmi4_data->input_dev);
 
 			dev_dbg(rmi4_data->pdev->dev.parent,
 					"%s: Finger %d: status = 0x%02x, x = %d, y = %d, wx = %d, wy = %d\n",
@@ -1442,7 +1441,6 @@ static int synaptics_rmi4_f12_abs_report(struct synaptics_rmi4_data *rmi4_data,
 #ifndef TYPE_B_PROTOCOL
 		input_mt_sync(rmi4_data->input_dev);
 #endif
-		input_sync(rmi4_data->input_dev);
 
 		if (rmi4_data->stylus_enable) {
 			stylus_presence = 0;
@@ -1457,6 +1455,8 @@ static int synaptics_rmi4_f12_abs_report(struct synaptics_rmi4_data *rmi4_data,
 			input_sync(rmi4_data->stylus_dev);
 		}
 	}
+
+	input_sync(rmi4_data->input_dev);
 
 	mutex_unlock(&(rmi4_data->rmi4_report_mutex));
 
