acb4a107998491e4bd0444a4f0d41dadecb50652
Hemant Kumar <hemantk@codeaurora.org>
2016-03-22 19:34:20 -0700

    usb: xhci: Add helper APIs to return xhci dma addresses
    dma address of secondary event ring, transfer ring and
    device context base address are required to pass to
    remote entity. Remote entity uses these addresses to
    program xhci controller registers.
    
    Change-Id: Ie0756d646a396a11b41b93e886bca9aff636ee5d
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- include/linux/usb/hcd.h
+++ include/linux/usb/hcd.h
@@ -398,6 +398,12 @@ struct hc_driver {
 
 	int (*sec_event_ring_setup)(struct usb_hcd *hcd, unsigned intr_num);
 	int (*sec_event_ring_cleanup)(struct usb_hcd *hcd, unsigned intr_num);
+	dma_addr_t (*get_sec_event_ring_dma_addr)(struct usb_hcd *hcd,
+			unsigned intr_num);
+	dma_addr_t (*get_xfer_ring_dma_addr)(struct usb_hcd *hcd,
+			struct usb_device *udev, struct usb_host_endpoint *ep);
+	dma_addr_t (*get_dcba_dma_addr)(struct usb_hcd *hcd,
+			struct usb_device *udev);
 };
 
 static inline int hcd_giveback_urb_in_bh(struct usb_hcd *hcd)
@@ -440,6 +446,13 @@ extern int usb_hcd_sec_event_ring_setup(struct usb_device *udev,
 	unsigned intr_num);
 extern int usb_hcd_sec_event_ring_cleanup(struct usb_device *udev,
 	unsigned intr_num);
+extern dma_addr_t
+usb_hcd_get_sec_event_ring_dma_addr(struct usb_device *udev,
+		unsigned intr_num);
+extern dma_addr_t usb_hcd_get_dcba_dma_addr(struct usb_device *udev);
+extern dma_addr_t
+usb_hcd_get_xfer_ring_dma_addr(struct usb_device *udev,
+	struct usb_host_endpoint *ep);
 
 extern struct usb_hcd *usb_create_hcd(const struct hc_driver *driver,
 		struct device *dev, const char *bus_name);
