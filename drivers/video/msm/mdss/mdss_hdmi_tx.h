495c91e3e79b2c7183d1df79d403851747208e74
Ajay Singh Parmar <aparmar@codeaurora.org>
2016-03-22 19:00:52 -0700

    msm: mdss: hdmi: add dynamic fps support
    Add dynamic fps support for hdmi to update the resolution
    timings on the interface by either changing the clock rate
    or by modifying the porch values or both. Dynamic fps feature
    can by used for hdmi to support different use cases like
    matching input stream fps with hdmi out.
    
    Change-Id: Ia305e1eb5d3da1dfbf868650e5ee84018255476b
    Signed-off-by: Ajay Singh Parmar <aparmar@codeaurora.org>

--- drivers/video/msm/mdss/mdss_hdmi_tx.h
+++ drivers/video/msm/mdss/mdss_hdmi_tx.h
@@ -150,6 +150,7 @@ struct hdmi_tx_ctrl {
 	u32 hpd_feature_on;
 	u32 hpd_initialized;
 	u32 vote_hdmi_core_on;
+	u32 dynamic_fps;
 	u8  timing_gen_on;
 	u8  mhl_hpd_on;
 	u8  hdcp_status;
@@ -157,6 +158,7 @@ struct hdmi_tx_ctrl {
 	struct hdmi_util_ds_data ds_data;
 	struct completion hpd_int_done;
 	struct work_struct hpd_int_work;
+	struct work_struct fps_work;
 	struct delayed_work hdcp_cb_work;
 
 	struct work_struct cable_notify_work;
