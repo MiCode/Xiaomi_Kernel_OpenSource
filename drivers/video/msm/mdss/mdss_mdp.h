f8e87aa19937107950e9e096675867c92a0a5c73
Dhaval Patel <pdhaval@codeaurora.org>
2016-03-22 22:56:38 -0700

    msm: mdss: validate layer count before copying userdata
    Validate input layer count in rotator and async update
    ioctl call before copying the rotator request list and
    async update layer list.
    
    Change-Id: I3489e5a2d4237a47bddf56c2f44c9e3001f0b2b4
    Signed-off-by: Dhaval Patel <pdhaval@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp.h
+++ drivers/video/msm/mdss/mdss_mdp.h
@@ -84,6 +84,8 @@
 
 #define XIN_HALT_TIMEOUT_US	0x4000
 
+#define MAX_LAYER_COUNT		0xC
+
 /* hw cursor can only be setup in highest mixer stage */
 #define HW_CURSOR_STAGE(mdata) \
 	(((mdata)->max_target_zorder + MDSS_MDP_STAGE_0) - 1)
