2b55a309d2cfb32944086d1925431d75876b150c
Deven Patel <cdevenp@codeaurora.org>
2016-03-24 16:44:19 -0700

    ASoC: codecs: Add audio MCLK support for APQ8096
    For APQ8096 target, codec MCLK control needs to be sent to DSP
    when audio use cases are enabled.
    
    CRs-fixed: 992134
    Change-Id: I6c04f27119afde7203c38fc9c471a17ec2b37d0a
    Signed-off-by: Deven Patel <cdevenp@codeaurora.org>

--- include/dt-bindings/clock/msm-clocks-8996.h
+++ include/dt-bindings/clock/msm-clocks-8996.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -542,4 +542,5 @@
 #define clk_audio_ap_clk 0x312ac429
 #define clk_audio_pmi_clk 0xb7ba2274
 #define clk_audio_ap_clk2 0xf0fbaf5b
+#define clk_audio_lpass_mclk2 0x0122abee
 #endif
