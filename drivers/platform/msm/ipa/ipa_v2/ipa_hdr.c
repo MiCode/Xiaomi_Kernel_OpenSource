e9b36267d6d8e16cbe2b7ca65d4201cca34b0110
Skylar Chang <chiaweic@codeaurora.org>
2016-03-25 10:44:33 -0700

    msm: ipa: fix hdr log print
    In case IPA header has a processing context, header offset is undefined.
    This change fixes header log print in case of processing context.
    
    Change-Id: I55e895be945a690a707f6015c09578e31c1de693
    CRs-Fixed: 993907
    Acked-by: Ady Abraham <adya@qti.qualcomm.com>
    Signed-off-by: Skylar Chang <chiaweic@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v2/ipa_hdr.c
+++ drivers/platform/msm/ipa/ipa_v2/ipa_hdr.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -785,7 +785,11 @@ int __ipa_del_hdr(u32 hdr_hdl)
 		return -EINVAL;
 	}
 
-	IPADBG("del hdr of sz=%d hdr_cnt=%d ofst=%d\n", entry->hdr_len,
+	if (entry->is_hdr_proc_ctx)
+		IPADBG("del hdr of sz=%d hdr_cnt=%d phys_base=%pa\n",
+			entry->hdr_len, htbl->hdr_cnt, &entry->phys_base);
+	else
+		IPADBG("del hdr of sz=%d hdr_cnt=%d ofst=%d\n", entry->hdr_len,
 			htbl->hdr_cnt, entry->offset_entry->offset);
 
 	if (--entry->ref_cnt) {
