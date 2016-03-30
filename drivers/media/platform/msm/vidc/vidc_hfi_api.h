6c4b64fd4382cfa4a79c4377c38bd0f221b7b663
Arun Menon <avmenon@codeaurora.org>
2016-03-30 16:55:40 -0700

    msm: vidc: Set constrained intra refresh property
    If Intra Refresh mode is enabled during encode, then
    enable constrained intra refresh property on firmware.
    Without constrained intra refresh enabled, Intra Refresh
    mode will not be effective.
    
    CRs-Fixed: 1001217
    Change-Id: Id326c73f78f3fadb5193a1e840f295d764fb013b
    Signed-off-by: Arun Menon <avmenon@codeaurora.org>

--- drivers/media/platform/msm/vidc/vidc_hfi_api.h
+++ drivers/media/platform/msm/vidc/vidc_hfi_api.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -231,6 +231,7 @@ enum hal_property {
 	HAL_PARAM_VENC_BITRATE_TYPE,
 	HAL_PARAM_VENC_H264_PIC_ORDER_CNT,
 	HAL_PARAM_VENC_LOW_LATENCY,
+	HAL_PARAM_VENC_CONSTRAINED_INTRA_PRED,
 };
 
 enum hal_domain {
