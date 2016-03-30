659d13b22ff46a1159f4cf460693133a5a099ad9
Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
2016-03-30 16:03:39 +0530

    power: bcl_peripheral: Update optional devicetree property read macro
    BCL peripheral probe throws error if an optional devicetree property
    is not defined for that target. Fix optional devicetree node read
    macro so that it doesn't throw any error if property is not defined.
    
    Change-Id: I76c052ede0b396ad0ab6e8335b4ae2b09cffee6e
    Signed-off-by: Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>

--- drivers/power/bcl_peripheral.c
+++ drivers/power/bcl_peripheral.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -81,6 +81,8 @@
 			goto bcl_dev_exit; \
 		} else if (!_ret) { \
 			_dest = _val; \
+		} else { \
+			_ret = 0; \
 		} \
 	} while (0)
 
