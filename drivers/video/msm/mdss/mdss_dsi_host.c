9d835662766c1f5d4a5f33336d15caa87aaf1d1f
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2016-03-31 17:51:37 +0530

    msm: mdss: avoid FIFO overflow handling when error is masked
    In case of DSI video mode panels, interrupt generation for DSI
    FIFO overflow error is masked during DCS read commands since it
    is expected. However, when the panel is under ESD attack, it is
    possible that harmless DSI ACK/PHY lane errors are generated.
    When a DSI error interrupt is generated, all the possible DSI
    errors are checked in mdss_dsi_error API. This causes DSI FIFO
    overflow recovery even though it is not expected, causing a
    possible flicker on the panel. Avoid DSI FIFO overflow recovery
    in such cases where the mask bit is set.
    
    Change-Id: I2bd2810d5ee63ccf0242f8023ae6aee0f3957b93
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi_host.c
+++ drivers/video/msm/mdss/mdss_dsi_host.c
@@ -2657,6 +2657,13 @@ void mdss_dsi_fifo_status(struct mdss_dsi_ctrl_pdata *ctrl)
 			pr_err("%s: ctrl ndx=%d status=%x\n", __func__,
 					ctrl->ndx, status);
 
+		/*
+		 * if DSI FIFO overflow is masked,
+		 * do not report overflow error
+		 */
+		if (MIPI_INP(base + 0x10c) & 0xf0000)
+			status = status & 0xaaaaffff;
+
 		if (status & 0x44440000) {/* DLNx_HS_FIFO_OVERFLOW */
 			dsi_send_events(ctrl, DSI_EV_DLNx_FIFO_OVERFLOW, 0);
 			/* Ignore FIFO EMPTY when overflow happens */
