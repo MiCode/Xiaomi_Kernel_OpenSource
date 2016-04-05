564363f1957df30bb3d663e90e1af6fd83305f22
Sahitya Tummala <stummala@codeaurora.org>
2016-04-05 15:29:35 +0530

    mmc: sdhci-msm: use PIO for tuning commands
    There are issues with ADMA when used with ICE enabled for tuning
    commands. As a workaround, use PIO mode for these commands by
    enabling quirk SDHCI_QUIRK2_USE_PIO_FOR_EMMC_TUNING .
    
    Change-Id: I8dbec823938525af90fb990db1bb4b325ee23cba
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -3754,6 +3754,7 @@ static int sdhci_msm_probe(struct platform_device *pdev)
 	host->quirks2 |= SDHCI_QUIRK2_USE_RESERVED_MAX_TIMEOUT;
 	host->quirks2 |= SDHCI_QUIRK2_BROKEN_LED_CONTROL;
 	host->quirks2 |= SDHCI_QUIRK2_NON_STANDARD_TUNING;
+	host->quirks2 |= SDHCI_QUIRK2_USE_PIO_FOR_EMMC_TUNING;
 
 	if (host->quirks2 & SDHCI_QUIRK2_ALWAYS_USE_BASE_CLOCK)
 		host->quirks2 |= SDHCI_QUIRK2_DIVIDE_TOUT_BY_4;
