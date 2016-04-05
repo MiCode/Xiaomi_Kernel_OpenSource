43fb337625f4348c5dce61ad324848e44329d9e7
Sahitya Tummala <stummala@codeaurora.org>
2016-04-05 14:00:48 +0530

    mmc: sdhci-msm: enable quirk to define non standard tuning
    Some controllers need SW to compare the data received
    from the card for a tuning command. Enable this quirk -
    SDHCI_QUIRK2_NON_STANDARD_TUNING for sdhci msm host
    controller.
    
    Change-Id: Id6f6230520db1ad018c883cb639fe66b4b86c70c
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -4013,6 +4013,7 @@ static int sdhci_msm_probe(struct platform_device *pdev)
 	host->quirks2 |= SDHCI_QUIRK2_IGNORE_DATATOUT_FOR_R1BCMD;
 	host->quirks2 |= SDHCI_QUIRK2_BROKEN_PRESET_VALUE;
 	host->quirks2 |= SDHCI_QUIRK2_USE_RESERVED_MAX_TIMEOUT;
+	host->quirks2 |= SDHCI_QUIRK2_NON_STANDARD_TUNING;
 
 	if (host->quirks2 & SDHCI_QUIRK2_ALWAYS_USE_BASE_CLOCK)
 		host->quirks2 |= SDHCI_QUIRK2_DIVIDE_TOUT_BY_4;
