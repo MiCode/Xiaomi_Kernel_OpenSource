87231cef9ccd3ddc00ec93d6580eab4757f6fe6e
Sahitya Tummala <stummala@codeaurora.org>
2016-04-12 13:24:51 +0530

    mmc: cmdq_hci: add set_transfer_params() to CQ unhalt
    Some of the transfer parameters like DMA mode will be changed
    only when CQ is in HALT state to send some legacy commands like
    tuning etc.
    
    Also, fix a typo with set_transfer_params() host op.
    
    Change-Id: I3a9856e0d60ce6a9cc1727cd8ccd10ef33bb707c
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- drivers/mmc/host/cmdq_hci.c
+++ drivers/mmc/host/cmdq_hci.c
@@ -720,8 +720,6 @@ static int cmdq_request(struct mmc_host *mmc, struct mmc_request *mrq)
 	}
 
 	cq_host->mrq_slot[tag] = mrq;
-	if (cq_host->ops->set_tranfer_params)
-		cq_host->ops->set_tranfer_params(mmc);
 
 	/* PM QoS */
 	sdhci_msm_pm_qos_irq_vote(host);
@@ -986,6 +984,8 @@ static int cmdq_halt(struct mmc_host *mmc, bool halt)
 		}
 		ret = retries ? 0 : -ETIMEDOUT;
 	} else {
+		if (cq_host->ops->set_transfer_params)
+			cq_host->ops->set_transfer_params(mmc);
 		if (cq_host->ops->set_block_size)
 			cq_host->ops->set_block_size(mmc);
 		if (cq_host->ops->set_data_timeout)
