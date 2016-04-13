be42cb2a31aa05848f6dbb9a4b62d76182e7b9d7
Praneeth Paladugu <ppaladug@codeaurora.org>
2016-04-12 22:34:39 -0700

    msm: vidc: Clean-up duplicate code
    Similar functionality code is duplicated across decoder and
    encoder. Unifying at common place makes it easy to maintain,
    less error prone and reduce SLOC.
    
    CRs-Fixed: 2012385
    Change-Id: I3e4738c4455fd4427b81bea2ab4d2c5d4ea89f0e
    Signed-off-by: Praneeth Paladugu <ppaladug@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vdec.h
+++ drivers/media/platform/msm/vidc/msm_vdec.h
@@ -15,21 +15,17 @@
 
 #include <media/msm_vidc.h>
 #include "msm_vidc_internal.h"
+#define MSM_VDEC_DVC_NAME "msm_vidc_vdec"
 
 int msm_vdec_inst_init(struct msm_vidc_inst *inst);
-int msm_vdec_ctrl_init(struct msm_vidc_inst *inst);
-int msm_vdec_querycap(void *instance, struct v4l2_capability *cap);
+int msm_vdec_ctrl_init(struct msm_vidc_inst *inst,
+	const struct v4l2_ctrl_ops *ctrl_ops);
 int msm_vdec_enum_fmt(void *instance, struct v4l2_fmtdesc *f);
 int msm_vdec_s_fmt(void *instance, struct v4l2_format *f);
 int msm_vdec_g_fmt(void *instance, struct v4l2_format *f);
+int msm_vdec_s_ctrl(void *instance, struct v4l2_ctrl *ctrl);
+int msm_vdec_g_ctrl(void *instance, struct v4l2_ctrl *ctrl);
 int msm_vdec_s_ext_ctrl(void *instance, struct v4l2_ext_controls *a);
-int msm_vdec_reqbufs(void *instance, struct v4l2_requestbuffers *b);
-int msm_vdec_qbuf(struct msm_vidc_inst *inst, struct v4l2_buffer *b);
-int msm_vdec_dqbuf(struct msm_vidc_inst *inst, struct v4l2_buffer *b);
-int msm_vdec_streamon(struct msm_vidc_inst *inst, enum v4l2_buf_type i);
-int msm_vdec_streamoff(struct msm_vidc_inst *inst, enum v4l2_buf_type i);
-int msm_vdec_cmd(struct msm_vidc_inst *inst, struct v4l2_decoder_cmd *dec);
-int msm_vdec_s_parm(struct msm_vidc_inst *inst, struct v4l2_streamparm *a);
 struct vb2_ops *msm_vdec_get_vb2q_ops(void);
 
 #endif
