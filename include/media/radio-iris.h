184b5a872f1db0235cee8d93ef0be801ba836437
Kamal Negi <kamaln@codeaurora.org>
2016-04-14 18:36:27 +0530

    radio: iris: Check SMD channel state on init.
    Add condition to check whether channel is already opened or closed.
    This will prevent smd channel to reopen if it already opened.
    
    Change-Id: I82cbe05062d3ee58969586e70dc46dc829e529f3
    Signed-off-by: Kamal Negi <kamaln@codeaurora.org>

--- include/media/radio-iris.h
+++ include/media/radio-iris.h
@@ -77,7 +77,7 @@ struct radio_hci_dev {
 };
 
 int radio_hci_register_dev(struct radio_hci_dev *hdev);
-int radio_hci_unregister_dev(struct radio_hci_dev *hdev);
+int radio_hci_unregister_dev(void);
 int radio_hci_recv_frame(struct sk_buff *skb);
 int radio_hci_send_cmd(struct radio_hci_dev *hdev, __u16 opcode, __u32 plen,
 	void *param);
