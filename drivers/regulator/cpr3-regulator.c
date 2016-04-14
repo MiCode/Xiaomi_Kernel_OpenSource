fee72d0331cfdd352ec1f901afb367642e5ecd1d
David Collins <collinsd@codeaurora.org>
2016-04-14 12:04:31 -0700

    regulator: cpr3-regulator: unregister CPR IRQ affinity notifier correctly
    Commit b7d5b597f16a ("regulator: cpr3-regulator: add support for
    configuring CPR IRQ affinity") added a call to
    register_hotcpu_notifier() but did not add a call to
    unregister_hotcpu_notifier().  Correct this so that the IRQ
    affinity notifier is unregistered when a cpr3-regulator device
    is unregistered.
    
    Change-Id: I6379559e201f14a0fd46c1e06761fae356ec9813
    CRs-Fixed: 949650
    Signed-off-by: David Collins <collinsd@codeaurora.org>

--- drivers/regulator/cpr3-regulator.c
+++ drivers/regulator/cpr3-regulator.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2015-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -4710,6 +4710,9 @@ int cpr3_regulator_unregister(struct cpr3_controller *ctrl)
 	list_del(&ctrl->list);
 	cpr3_regulator_debugfs_ctrl_remove(ctrl);
 	mutex_unlock(&cpr3_controller_list_mutex);
+
+	if (ctrl->irq && !cpumask_empty(&ctrl->irq_affinity_mask))
+		unregister_hotcpu_notifier(&ctrl->cpu_hotplug_notifier);
 
 	cpr3_ctrl_loop_disable(ctrl);
 	cpr3_closed_loop_disable(ctrl);
