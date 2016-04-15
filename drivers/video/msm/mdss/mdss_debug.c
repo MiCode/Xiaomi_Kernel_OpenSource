e8ca852c7695d412cec650aadeefcc98eac06933
Ping Li <pingli@codeaurora.org>
2016-04-15 15:27:36 -0700

    msm: mdss: Correct block id check for mdss_mdp_misr_table
    DISPLAY_MISR_LCDC block doesn't have corresponding mdss_mdp_misr_table,
    this change corrects the block id check for mdss_mdp_misr_table.
    
    CRs-Fixed: 1001224
    Change-Id: I74b03c31542d4b239eb2ffdc4dc6345dff5eab86
    Signed-off-by: Ping Li <pingli@codeaurora.org>

--- drivers/video/msm/mdss/mdss_debug.c
+++ drivers/video/msm/mdss/mdss_debug.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2009-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1244,7 +1244,7 @@ static inline struct mdss_mdp_misr_map *mdss_misr_get_map(u32 block_id,
 	char *ctrl_reg = NULL, *value_reg = NULL;
 	char *intf_base = NULL;
 
-	if (block_id > DISPLAY_MISR_MDP) {
+	if (block_id > DISPLAY_MISR_HDMI && block_id != DISPLAY_MISR_MDP) {
 		pr_err("MISR Block id (%d) out of range\n", block_id);
 		return NULL;
 	}
@@ -1347,8 +1347,13 @@ int mdss_misr_set(struct mdss_data_type *mdata,
 	bool is_valid_wb_mixer = true;
 	bool use_mdp_up_misr = false;
 
-	map = mdss_misr_get_map(req->block_id, ctl, mdata);
+	if (!mdata || !req || !ctl) {
+		pr_err("Invalid input params: mdata = %p req = %p ctl = %p",
+			mdata, req, ctl);
+		return -EINVAL;
+	}
 
+	map = mdss_misr_get_map(req->block_id, ctl, mdata);
 	if (!map) {
 		pr_err("Invalid MISR Block=%d\n", req->block_id);
 		return -EINVAL;
