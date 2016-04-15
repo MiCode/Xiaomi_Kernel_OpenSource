af5704e16885fc5b1f589cd2d0976bb1d6f3cc44
Vinayak Menon <vinmenon@codeaurora.org>
2016-04-15 11:38:45 +0530

    arm: mmap: remove the redundant check of personality
    Fixes the merge "Merge tag 'lsk-v3.18-15.12-android' into 'msm-3.18'".
    The above mentioned merge wrongly removed part of a fix that was picked
    from upstream (arm: factor out mmap ASLR into mmap_rnd). Ressurect the
    fix.
    
    Change-Id: I9cc0057652048dafbe45f7af63a4b5117076ec76
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- arch/arm/mm/mmap.c
+++ arch/arm/mm/mmap.c
@@ -187,9 +187,8 @@ void arch_pick_mmap_layout(struct mm_struct *mm)
 {
 	unsigned long random_factor = 0UL;
 
-	if ((current->flags & PF_RANDOMIZE) &&
-	    !(current->personality & ADDR_NO_RANDOMIZE))
-		random_factor = (get_random_int() % (1 << mmap_rnd_bits)) << PAGE_SHIFT;
+	if (current->flags & PF_RANDOMIZE)
+		random_factor = arch_mmap_rnd();
 
 	if (mmap_is_legacy()) {
 		mm->mmap_base = TASK_UNMAPPED_BASE + random_factor;
