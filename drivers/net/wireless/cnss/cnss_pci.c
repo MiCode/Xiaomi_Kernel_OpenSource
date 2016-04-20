b48a17256e088271f19193a7b9f3c9534ed8671c
Sarada Prasanna Garnayak <sgarna@codeaurora.org>
2016-04-20 20:12:15 +0530

    net: cnss: remove redundant and dead code from platform driver
    The cnss subsystem restart and pm qos export API has been
    refactor in respective platform driver according to the bus
    type SDIO/PCIe.
    
    Remove redundant and dead code from platform driver to avoid
    the namespace collision compilation error. Refactor the recovery
    work handler according to bus type. This feature adds support for
    dual cnss platform driver support for dual WiFi.
    
    CRs-Fixed: 987560
    Change-Id: I0e1b5ff0e9970a40ad9d0619dcb7f8cbae241656
    Signed-off-by: Sarada Prasanna Garnayak <sgarna@codeaurora.org>

--- drivers/net/wireless/cnss/cnss_pci.c
+++ drivers/net/wireless/cnss/cnss_pci.c
@@ -1771,11 +1771,11 @@ void cnss_pci_recovery_work_handler(struct work_struct *recovery)
 	cnss_pci_device_self_recovery();
 }
 
-DECLARE_WORK(recovery_work, cnss_pci_recovery_work_handler);
+DECLARE_WORK(cnss_pci_recovery_work, cnss_pci_recovery_work_handler);
 
 void cnss_schedule_recovery_work(void)
 {
-	schedule_work(&recovery_work);
+	schedule_work(&cnss_pci_recovery_work);
 }
 EXPORT_SYMBOL(cnss_schedule_recovery_work);
 
@@ -1802,7 +1802,7 @@ void cnss_pci_events_cb(struct msm_pcie_notify *notify)
 		spin_unlock_irqrestore(&pci_link_down_lock, flags);
 
 		pr_err("PCI link down, schedule recovery\n");
-		schedule_work(&recovery_work);
+		schedule_work(&cnss_pci_recovery_work);
 		break;
 
 	case MSM_PCIE_EVENT_WAKEUP:
@@ -1836,7 +1836,7 @@ void cnss_wlan_pci_link_down(void)
 	spin_unlock_irqrestore(&pci_link_down_lock, flags);
 
 	pr_err("PCI link down detected by host driver, schedule recovery!\n");
-	schedule_work(&recovery_work);
+	schedule_work(&cnss_pci_recovery_work);
 }
 EXPORT_SYMBOL(cnss_wlan_pci_link_down);
 
@@ -2330,7 +2330,7 @@ EXPORT_SYMBOL(cnss_release_pm_sem);
 
 void cnss_pci_schedule_recovery_work(void)
 {
-	schedule_work(&recovery_work);
+	schedule_work(&cnss_pci_recovery_work);
 }
 
 void *cnss_pci_get_virt_ramdump_mem(unsigned long *size)
