1a67f3c5d57d5a64a7411dce91272e1f4ff6415b
Fenglin Wu <fenglinw@codeaurora.org>
2016-04-21 12:04:28 +0800

    power_supply: Add SOC_REPORTING_READY property
    Add SOC_REORTING_READY property to indicate if the reporting SOC is the
    actual battery capacity.
    
    CRs-Fixed: 1001496
    Change-Id: Ie0b17be6c9c4f2bf00d9902a597f4d41c7311857
    Signed-off-by: Fenglin Wu <fenglinw@codeaurora.org>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -246,6 +246,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(current_capability),
 	POWER_SUPPLY_ATTR(typec_mode),
 	POWER_SUPPLY_ATTR(allow_hvdcp3),
+	POWER_SUPPLY_ATTR(soc_reporting_ready),
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_ATTR(charge_counter_ext),
 	/* Properties of type `const char *' */
