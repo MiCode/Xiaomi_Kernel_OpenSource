32592a96602297e8fddc752f0d10190017edaa7f
Mayank Rana <mrana@codeaurora.org>
2016-04-22 09:38:43 -0700

    dwc3: Enable and disable core IRQ from dwc3 suspend/resume only
    dwc3 core IRQ's depth (disable depth) is going out of sync due to
    disabling it from dwc3's state machine when there are multiple
    time USB composition switch is being performed before USB cable
    connect. Linux IRQ framework doesn't decrement disable depth
    count with enable_irq() when IRQ is enabled previously but it does
    increment disable depth every time disable_irq() is called. This
    results into dwc3 core IRQ to remain disable causing USB enumeration
    failure as unknown device due to USB controller driver is not
    seeing any of RESET and CONNECT DONE interrupt although USB controller
    has posted those events into event buffer. Fix this issue by
    making sure that USB core IRQ is disabled by default, and
    enable/disable it from dwc3 suspend/resume only.
    
    Change-Id: I665b15f6b5aca7d63e9ced29ec94b09524ae91be
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -35,6 +35,7 @@
 #include <linux/of.h>
 #include <linux/acpi.h>
 #include <linux/pinctrl/consumer.h>
+#include <linux/irq.h>
 
 #include <linux/usb/ch9.h>
 #include <linux/usb/gadget.h>
@@ -1017,6 +1018,9 @@ static int dwc3_probe(struct platform_device *pdev)
 	dwc->xhci_resources[1].name = res->name;
 
 	irq = platform_get_irq(to_platform_device(dwc->dev), 0);
+
+	/* will be enabled in dwc3_msm_resume() */
+	irq_set_status_flags(irq, IRQ_NOAUTOEN);
 	ret = devm_request_irq(dev, irq, dwc3_interrupt, IRQF_SHARED, "dwc3",
 			dwc);
 	if (ret) {
