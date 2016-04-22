9318da04b7a51ab1f8a2c4bca96ab7c69d0fe224
Daniel Rosenberg <drosen@google.com>
2016-04-22 00:00:48 -0700

    fuse: Add support for d_canonical_path
    Allows FUSE to report to inotify that it is acting
    as a layered filesystem. The userspace component
    returns a string representing the location of the
    underlying file. If the string cannot be resolved
    into a path, the top level path is returned instead.
    
    bug: 23904372
    Change-Id: Iabdca0bbedfbff59e9c820c58636a68ef9683d9f
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -13,6 +13,7 @@
 #include <linux/poll.h>
 #include <linux/uio.h>
 #include <linux/miscdevice.h>
+#include <linux/namei.h>
 #include <linux/pagemap.h>
 #include <linux/file.h>
 #include <linux/slab.h>
@@ -1868,6 +1869,10 @@ static ssize_t fuse_dev_do_write(struct fuse_conn *fc,
 	spin_unlock(&fc->lock);
 
 	err = copy_out_args(cs, &req->out, nbytes);
+	if (req->in.h.opcode == FUSE_CANONICAL_PATH) {
+		req->out.h.error = kern_path((char *)req->out.args[0].value, 0,
+							req->canonical_path);
+	}
 	fuse_copy_finish(cs);
 
 	spin_lock(&fc->lock);
