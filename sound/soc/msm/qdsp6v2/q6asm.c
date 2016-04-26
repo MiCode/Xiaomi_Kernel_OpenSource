f84d18b60e6c71ab5a0f7f9638c5e1bc3756443b
Weiyin Jiang <wjiang@codeaurora.org>
2016-04-26 14:35:38 +0800

    ASoC: msm: audio-effects: misc fixes in h/w accelerated effect
    Adding memory copy size check and integer overflow check in h/w
    accelerated effect driver.
    
    Change-Id: I17d4cc0a38770f0c5067fa8047cd63e7bf085e48
    CRs-Fixed: 1006609
    Signed-off-by: Weiyin Jiang <wjiang@codeaurora.org>

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
  * Author: Brian Swetland <swetland@google.com>
  *
  * This software is licensed under the terms of the GNU General Public
@@ -1212,6 +1212,12 @@ int q6asm_audio_client_buf_alloc_contiguous(unsigned int dir,
 
 	ac->port[dir].buf = buf;
 
+	/* check for integer overflow */
+	if ((bufcnt > 0) && ((INT_MAX / bufcnt) < bufsz)) {
+		pr_err("%s: integer overflow\n", __func__);
+		mutex_unlock(&ac->cmd_lock);
+		goto fail;
+	}
 	bytes_to_alloc = bufsz * bufcnt;
 
 	/* The size to allocate should be multiple of 4K bytes */
