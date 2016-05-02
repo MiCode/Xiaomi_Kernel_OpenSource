318a9ea29c8be914b00d639d44c417a1737e7f23
Hemant Kumar <hemantk@codeaurora.org>
2016-05-02 11:27:21 -0700

    usb: gadget: mtp: Increase RX transfer length to 1M
    The test results indicate that larger transfers improve write speeds.
    The Rx request transfer length is now 16K bytes. Increase the Rx
    transfer length from 16K to 1MB to get good throughputs.
    
    Change-Id: I3c64e6b96d569b4dc61805a53a0835db9142d55e
    Signed-off-by: ChandanaKishori Chiluveru <cchilu@codeaurora.org>
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- drivers/usb/gadget/function/f_mtp.c
+++ drivers/usb/gadget/function/f_mtp.c
@@ -42,6 +42,7 @@
 
 #include "configfs.h"
 
+#define MTP_RX_BUFFER_INIT_SIZE    1048576
 #define MTP_BULK_BUFFER_SIZE       16384
 #define INTR_BUFFER_SIZE           28
 #define MAX_INST_NAME_LEN          40
@@ -78,7 +79,7 @@
 
 #define MAX_ITERATION		100
 
-unsigned int mtp_rx_req_len = MTP_BULK_BUFFER_SIZE;
+unsigned int mtp_rx_req_len = MTP_RX_BUFFER_INIT_SIZE;
 module_param(mtp_rx_req_len, uint, 0644);
 
 unsigned int mtp_tx_req_len = MTP_BULK_BUFFER_SIZE;
