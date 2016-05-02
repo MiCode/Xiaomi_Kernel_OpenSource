301422bde79ab8d8e509e4aabdf5474f54e0e46e
Hemant Kumar <hemantk@codeaurora.org>
2016-05-01 17:54:31 -0700

    USB: gadget: mtp: Fix OUT endpoint request length usage in read
    DWC3 UDC requires OUT endpoint request length to be multiple of
    endpoint max packet size.  Hence the OUT endpoint request is
    always set to the request buffer size in mtp read.  The buffer
    can be allocated dynamically and is always multiple of 1024.
    If the count passed to read system call is less than the buffer
    size, we end up reading more bytes and not giving it to userspace.
    
    Userspace calls read with 512 bytes for receiving request from the
    host.  If the request is for receiving a file, the read is called
    with 16K bytes for reading the header and some data.  The remaining
    data is received via ioctl.  If 1MB is used as request length for
    16K read, the received file will be corrupted.
    
    Change-Id: I2eedf829f267150ff7a303824e95963219a1f555
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- drivers/usb/gadget/function/f_mtp.c
+++ drivers/usb/gadget/function/f_mtp.c
@@ -577,18 +577,14 @@ static ssize_t mtp_read(struct file *fp, char __user *buf,
 	struct mtp_dev *dev = fp->private_data;
 	struct usb_composite_dev *cdev = dev->cdev;
 	struct usb_request *req;
-	ssize_t xfer, r = count;
+	ssize_t r = count, xfer, len;
 	int ret = 0;
-	size_t len = 0;
 
 	DBG(cdev, "mtp_read(%zu)\n", count);
 
-	if (count > mtp_rx_req_len)
+	len = ALIGN(count, dev->ep_out->maxpacket);
+	if (len > mtp_rx_req_len)
 		return -EINVAL;
-
-	if (!IS_ALIGNED(count, dev->ep_out->maxpacket))
-		DBG(cdev, "%s - count(%zu) not multiple of mtu(%d)\n", __func__,
-						count, dev->ep_out->maxpacket);
 
 	/* we will block until we're online */
 	DBG(cdev, "mtp_read: waiting for online state\n");
@@ -629,7 +625,7 @@ static ssize_t mtp_read(struct file *fp, char __user *buf,
 requeue_req:
 	/* queue a request */
 	req = dev->rx_req[0];
-	req->length = mtp_rx_req_len;
+	req->length = len;
 	dev->rx_done = 0;
 	ret = usb_ep_queue(dev->ep_out, req, GFP_KERNEL);
 	if (ret < 0) {
