4b1eefba1a038988fdafddfff5c5ecb4d7f183fb
Terence Ho <terenceh@codeaurora.org>
2016-05-05 11:48:11 -0400

    msm-camera: Set proper return code in sensor wait for probe
    Set the correct return code for sensor wait for probe function
    so caller can act accordingly.
    
    Change-Id: Ib39c34e3fde919663d49c7a0fe38c693c1ca66b8
    Signed-off-by: Terence Ho <terenceh@codeaurora.org>
    Signed-off-by: Sridhar Gujje <sgujje@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor_init.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor_init.c
@@ -49,8 +49,10 @@ static int msm_sensor_wait_for_probe_done(struct msm_sensor_init_t *s_init)
 	}
 	rc = wait_event_timeout(s_init->state_wait,
 		(s_init->module_init_status == 1), msecs_to_jiffies(tm));
-	if (rc == 0)
+	if (rc == 0) {
 		pr_err("%s:%d wait timeout\n", __func__, __LINE__);
+		rc = -1;
+	}
 
 	return rc;
 }
@@ -86,7 +88,7 @@ static int32_t msm_sensor_driver_cmd(struct msm_sensor_init_t *s_init,
 		break;
 
 	case CFG_SINIT_PROBE_WAIT_DONE:
-		msm_sensor_wait_for_probe_done(s_init);
+		rc = msm_sensor_wait_for_probe_done(s_init);
 		break;
 
 	default:
