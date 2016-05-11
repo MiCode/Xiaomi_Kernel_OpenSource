ee76770eb153fd361a459ecb0eba1a16b1604562
Jordan Crouse <jcrouse@codeaurora.org>
2016-05-11 09:37:46 -0600

    iommu/arm-smmu: Ratelimit dynamic ASID allocation error
    It is possible to aggressively push the limits of dynamic ASID
    by forking and creating lots of domains. In the most egregious
    cases enormous log storms can occur. Ratelimit the error
    message to cut down on spam.
    
    Change-Id: Ic0dedbadc8f7fd7e810505e4cf2a312cf2986047
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -2013,8 +2013,8 @@ static int arm_smmu_attach_dynamic(struct iommu_domain *domain,
 				smmu->num_context_banks + 2,
 				MAX_ASID + 1, GFP_KERNEL);
 	if (ret < 0) {
-		dev_err(smmu->dev, "dynamic ASID allocation failed: %d\n",
-			ret);
+		dev_err_ratelimited(smmu->dev,
+			"dynamic ASID allocation failed: %d\n", ret);
 		goto out;
 	}
 
