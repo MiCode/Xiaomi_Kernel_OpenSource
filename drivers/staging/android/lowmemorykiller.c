51608394a51fcf7536f1b6096808fad0d5d63b32
Srinivas Ramana <sramana@codeaurora.org>
2016-05-13 18:17:41 +0530

    lowmemorykiller:fix arguments to zone_watermark_ok_safe
    The alloc_flags argument of zone_watermark_ok_safe()
    is no more available. Fix the usage.
    
    Change-Id: I99b832418b914765a4941682929dd7183d274e1c
    Signed-off-by: Srinivas Ramana <sramana@codeaurora.org>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -323,8 +323,7 @@ static void adjust_gfp_mask(gfp_t *gfp_mask)
 		if (high_zoneidx == ZONE_NORMAL) {
 			if (zone_watermark_ok_safe(
 					preferred_zone, 0,
-					high_wmark_pages(preferred_zone), 0,
-					0))
+					high_wmark_pages(preferred_zone), 0))
 				*gfp_mask |= __GFP_HIGHMEM;
 		} else if (high_zoneidx == ZONE_HIGHMEM) {
 			*gfp_mask |= __GFP_HIGHMEM;
