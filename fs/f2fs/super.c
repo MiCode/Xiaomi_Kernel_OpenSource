621e6a83270c0cb5f3df86d94dbe1804334e494f
Jaegeuk Kim <jaegeuk@kernel.org>
2016-05-20 21:47:24 -0700

    f2fs: add lazytime mount option
    commit 6d94c74ab85fe70dc1ac29b1ffc55cf23b3cf3f9 upstream.
    
    This patch adds lazytime support.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Git-commit: deabce183a24684a5aaa8a2f4fb33409004ebb5b
    Git-repo: https://android.googlesource.com/kernel/common.git
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -109,6 +109,8 @@ enum {
 	Opt_mode,
 	Opt_io_size_bits,
 	Opt_fault_injection,
+	Opt_lazytime,
+	Opt_nolazytime,
 	Opt_quota,
 	Opt_noquota,
 	Opt_usrquota,
@@ -156,6 +158,8 @@ static match_table_t f2fs_tokens = {
 	{Opt_mode, "mode=%s"},
 	{Opt_io_size_bits, "io_bits=%u"},
 	{Opt_fault_injection, "fault_injection=%u"},
+	{Opt_lazytime, "lazytime"},
+	{Opt_nolazytime, "nolazytime"},
 	{Opt_quota, "quota"},
 	{Opt_noquota, "noquota"},
 	{Opt_usrquota, "usrquota"},
@@ -512,6 +516,12 @@ static int parse_options(struct super_block *sb, char *options)
 				"FAULT_INJECTION was not selected");
 #endif
 			break;
+		case Opt_lazytime:
+			sb->s_flags |= MS_LAZYTIME;
+			break;
+		case Opt_nolazytime:
+			sb->s_flags &= ~MS_LAZYTIME;
+			break;
 #ifdef CONFIG_QUOTA
 		case Opt_quota:
 		case Opt_usrquota:
@@ -741,6 +751,9 @@ static void f2fs_dirty_inode(struct inode *inode, int flags)
 
 	if (inode->i_ino == F2FS_NODE_INO(sbi) ||
 			inode->i_ino == F2FS_META_INO(sbi))
+		return;
+
+	if (flags == I_DIRTY_TIME)
 		return;
 
 	if (is_inode_flag_set(inode, FI_AUTO_RECOVER))
@@ -1118,6 +1131,7 @@ static void default_options(struct f2fs_sb_info *sbi)
 	set_opt(sbi, INLINE_DENTRY);
 	set_opt(sbi, EXTENT_CACHE);
 	set_opt(sbi, NOHEAP);
+	sbi->sb->s_flags |= MS_LAZYTIME;
 	set_opt(sbi, FLUSH_MERGE);
 	if (f2fs_sb_mounted_blkzoned(sbi->sb)) {
 		set_opt_mode(sbi, F2FS_MOUNT_LFS);
