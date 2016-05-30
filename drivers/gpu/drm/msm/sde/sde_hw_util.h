8b60eaf491bf726a4de4ddcb1bd0557a9c1fb03c
Lloyd Atkinson <latkinso@codeaurora.org>
2016-05-30 13:49:12 -0400

    drm/msm/sde: add register name to logging
    Use the symbol name as a best effort way to log the register
    name along with the value and offset in the register logging.
    
    Change-Id: I8befb674d9599725f66eadc4a293dcb24b96302f
    Signed-off-by: Lloyd Atkinson <latkinso@codeaurora.org>

--- drivers/gpu/drm/msm/sde/sde_hw_util.h
+++ drivers/gpu/drm/msm/sde/sde_hw_util.h
@@ -36,9 +36,14 @@ struct sde_hw_blk_reg_map {
 
 u32 *sde_hw_util_get_log_mask_ptr(void);
 
-void SDE_REG_WRITE(struct sde_hw_blk_reg_map *c, u32 reg_off, u32 val);
+void sde_reg_write(struct sde_hw_blk_reg_map *c,
+		u32 reg_off,
+		u32 val,
+		const char *name);
+int sde_reg_read(struct sde_hw_blk_reg_map *c, u32 reg_off);
 
-int SDE_REG_READ(struct sde_hw_blk_reg_map *c, u32 reg_off);
+#define SDE_REG_WRITE(c, off, val) sde_reg_write(c, off, val, #off)
+#define SDE_REG_READ(c, off) sde_reg_read(c, off)
 
 void *sde_hw_util_get_dir(void);
 
