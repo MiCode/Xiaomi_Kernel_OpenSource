078568e4259847be3cecf72052606abb93b7eed0
Pavankumar Kondeti <pkondeti@codeaurora.org>
2016-05-31 12:34:52 +0530

    sched: Introduce sched_freq_aggregate_threshold tunable
    Do the aggregation for frequency only when the total group busy time
    is above sched_freq_aggregate_threshold. This filtering is especially
    needed for the cases where groups are created by including all threads
    of an application process. This knob can be tuned to apply aggregation
    only for the heavy workload applications.
    
    When this knob is enabled and load is aggregated, the load is not
    clipped to 100% @ current frequency to ramp up the frequency faster.
    
    Change-Id: Icfd91c85938def101a989af3597d3dcaa8026d16
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -435,6 +435,13 @@ static struct ctl_table kern_table[] = {
 		.proc_handler   = sched_window_update_handler,
 	},
 	{
+		.procname	= "sched_freq_aggregate_threshold",
+		.data		= &sysctl_sched_freq_aggregate_threshold_pct,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= sched_hmp_proc_update_handler,
+	},
+	{
 		.procname	= "sched_boost",
 		.data		= &sysctl_sched_boost,
 		.maxlen		= sizeof(unsigned int),
