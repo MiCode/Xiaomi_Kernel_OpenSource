daf247770f380809fe8a39720a369a59f3bfdeeb
Srinivas Ramana <sramana@codeaurora.org>
2016-05-31 11:11:52 +0530

    mach-qcom: add support to populate dt nodes for 32-bit platforms
    Add support to populate device tree nodes for the new
    32-bit platforms under under mach-qcom.
    
    Change-Id: I5fd747de84917c16f28f3f72d7b34b5db157637c
    Signed-off-by: Srinivas Ramana <sramana@codeaurora.org>
    Signed-off-by: Jeevan Shriram <jshriram@codeaurora.org>

---
+++ arch/arm/mach-qcom/board-dt.c
@@ -0,0 +1,28 @@
+/* Copyright (c) 2016-2017, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/kernel.h>
+#include <linux/of.h>
+#include <linux/of_fdt.h>
+
+#include "board-dt.h"
+
+void __init board_dt_populate(struct of_dev_auxdata *adata)
+{
+	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
+
+	/* Explicitly parent the /soc devices to the root node to preserve
+	 * the kernel ABI (sysfs structure, etc) until userspace is updated
+	 */
+	of_platform_populate(of_find_node_by_path("/soc"),
+			     of_default_bus_match_table, adata, NULL);
+}
