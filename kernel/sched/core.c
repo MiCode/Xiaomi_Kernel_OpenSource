2ec744e1d6f68b6bf01e090e9ad395731dd9ad06
Jann Horn <jannh@google.com>
2016-06-01 11:55:07 +0200

    UPSTREAM: sched: panic on corrupted stack end
    (cherry pick from commit 29d6455178a09e1dc340380c582b13356227e8df)
    
    Until now, hitting this BUG_ON caused a recursive oops (because oops
    handling involves do_exit(), which calls into the scheduler, which in
    turn raises an oops), which caused stuff below the stack to be
    overwritten until a panic happened (e.g.  via an oops in interrupt
    context, caused by the overwritten CPU index in the thread_info).
    
    Just panic directly.
    
    Signed-off-by: Jann Horn <jannh@google.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Change-Id: Ia3acb3f747f7a58ec2d071644433b0591925969f
    Bug: 29444228

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2687,7 +2687,8 @@ static noinline void __schedule_bug(struct task_struct *prev)
 static inline void schedule_debug(struct task_struct *prev)
 {
 #ifdef CONFIG_SCHED_STACK_END_CHECK
-	BUG_ON(unlikely(task_stack_end_corrupted(prev)));
+	if (task_stack_end_corrupted(prev))
+		panic("corrupted stack end detected inside scheduler\n");
 #endif
 	/*
 	 * Test if we are atomic. Since do_exit() needs to call into
