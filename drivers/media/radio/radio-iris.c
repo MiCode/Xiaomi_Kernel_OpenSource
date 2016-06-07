cb37879a057fb2f46052287b15fde2be6ca00791
Kamal Negi <kamaln@codeaurora.org>
2016-06-07 13:01:05 +0530

    radio-iris: Serialize the call to fm_ops_release function
    fm_ops_release function can be called twice in parallel and can free
    resources before second call complete execution.
    This will lead to crashes.
    
    Change-Id: Ie19035c10931d9336599e0e3872ef46ab77e1527
    Signed-off-by: Kamal Negi <kamaln@codeaurora.org>

--- drivers/media/radio/radio-iris.c
+++ drivers/media/radio/radio-iris.c
@@ -5212,6 +5212,8 @@ static int iris_fops_release(struct file *file)
 		return -EINVAL;
 
 	FMDBG("state %d", radio->mode);
+	mutex_lock(&radio->lock);
+
 	if (radio->mode == FM_OFF)
 		goto END;
 
@@ -5231,6 +5233,7 @@ static int iris_fops_release(struct file *file)
 		radio->is_fm_closing = false;
 	} else if (radio->mode == FM_CALIB) {
 		radio->mode = FM_OFF;
+		mutex_unlock(&radio->lock);
 		return retval;
 	}
 END:
@@ -5239,6 +5242,8 @@ END:
 	if (radio->fm_hdev != NULL)
 		radio->fm_hdev->close_smd();
 	mutex_unlock(&fm_smd_enable);
+
+	mutex_unlock(&radio->lock);
 
 	if (retval < 0)
 		FMDERR("Err on disable FM %d\n", retval);
