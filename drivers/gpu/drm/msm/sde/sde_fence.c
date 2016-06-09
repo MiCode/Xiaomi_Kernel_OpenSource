6213210b1fc33aca07bccdd26ffb0f95b7ae923b
Clarence Ip <cip@codeaurora.org>
2016-06-09 13:30:07 -0400

    drm/msm/sde: add wrappers for release fence API
    Add functions for creating/incrementing timelines, and
    for creating fences at a specified sync point.
    
    Change-Id: Idfd42150f2162fd0e4c3b89916e8ed5a801c27ae
    Signed-off-by: Clarence Ip <cip@codeaurora.org>

--- drivers/gpu/drm/msm/sde/sde_fence.c
+++ drivers/gpu/drm/msm/sde/sde_fence.c
@@ -11,6 +11,7 @@
  */
 
 #include <sync.h>
+#include <sw_sync.h>
 #include "sde_kms.h"
 #include "sde_fence.h"
 
@@ -33,3 +34,68 @@ int sde_sync_wait(void *fence, long timeout_ms)
 	return sync_fence_wait(fence, timeout_ms);
 }
 
+#if IS_ENABLED(CONFIG_SW_SYNC)
+void *sde_sync_timeline_create(const char *name)
+{
+	if (!name)
+		name = "";
+	return sw_sync_timeline_create(name);
+}
+
+int sde_sync_fence_create(void *timeline, const char *name, int val)
+{
+	struct sync_pt *sync_pt;
+	struct sync_fence *fence;
+	signed int fd = -EINVAL;
+
+	if (!timeline) {
+		DRM_ERROR("Invalid timeline\n");
+		return fd;
+	}
+
+	if (val < 0) {
+		DRM_ERROR("Invalid fence value %d\n", val);
+		return fd;
+	}
+
+	if (!name)
+		name = "";
+
+	/* create sync point */
+	sync_pt = sw_sync_pt_create(timeline, val);
+	if (sync_pt == NULL) {
+		DRM_ERROR("Failed to create sync point, %s\n", name);
+		return fd;
+	}
+
+	/* create fence */
+	fence = sync_fence_create(name, sync_pt);
+	if (fence == NULL) {
+		sync_pt_free(sync_pt);
+		DRM_ERROR("Couldn't create fence, %s\n", name);
+		return fd;
+	}
+
+	/* create fd */
+	fd = get_unused_fd_flags(0);
+	if (fd < 0) {
+		DRM_ERROR("Failed to get_unused_fd_flags(), %s\n", name);
+		sync_fence_put(fence);
+		return fd;
+	}
+
+	sync_fence_install(fence, fd);
+
+	return fd;
+}
+
+void sde_sync_timeline_inc(void *timeline, int val)
+{
+	if (!timeline)
+		DRM_ERROR("Invalid timeline\n");
+	else if (val <= 0)
+		DRM_ERROR("Invalid increment, %d\n", val);
+	else
+		sw_sync_timeline_inc(timeline, val);
+}
+#endif
