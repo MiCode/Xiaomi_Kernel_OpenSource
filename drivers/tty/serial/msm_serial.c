2d7aeb93cdf2d898a1d0d34a158810d0c2c3fa63
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2016-06-16 15:13:02 -0700

    tty: serial: msm: replace iowrite32_rep with writel_relaxed_no_log
    RTB logs gets flooded during console write operation due to logged
    variant of API. This commit replaces logged variant API with no log
    variant to suppress logs.
    
    CRs-Fixed: 1030352
    Change-Id: I79f943cbc13553b3dbdce68f5c1143fa54f6eafa
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>

--- drivers/tty/serial/msm_serial.c
+++ drivers/tty/serial/msm_serial.c
@@ -1589,6 +1589,7 @@ static void __msm_console_write(struct uart_port *port, const char *s,
 		int j;
 		unsigned int num_chars;
 		char buf[4] = { 0 };
+		const u32 *buffer;
 
 		if (is_uartdm)
 			num_chars = min(count - i, (unsigned int)sizeof(buf));
@@ -1613,7 +1614,8 @@ static void __msm_console_write(struct uart_port *port, const char *s,
 		while (!(msm_read(port, UART_SR) & UART_SR_TX_READY))
 			cpu_relax();
 
-		iowrite32_rep(tf, buf, 1);
+		buffer = (const u32 *)buf;
+		writel_relaxed_no_log(*buffer, tf);
 		i += num_chars;
 	}
 	spin_unlock(&port->lock);
