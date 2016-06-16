fc946152b0b22930f819d74233b8a57b97c55be0
Shubhraprakash Das <sadas@codeaurora.org>
2016-06-15 20:19:08 -0700

    msm: camera: isp: Stop burst stream on epoch of last frame
    Program the burst stream's period and pattern to stop when the
    last frame is reached. Presently it is being stopped 1 frame
    after the last frame which causes extra power to be used. This
    change stops the stream on last frame.
    
    Change-Id: I74e9bc8317068089dd6643131e3be11ef3851909
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>
    Signed-off-by: Sridhar Gujje <sgujje@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp46.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp46.c
@@ -386,8 +386,6 @@ static void msm_vfe46_process_reg_update(struct vfe_device *vfe_dev,
 
 			switch (i) {
 			case VFE_PIX_0:
-				msm_isp_save_framedrop_values(vfe_dev,
-							VFE_PIX_0);
 				msm_isp_notify(vfe_dev, ISP_EVENT_REG_UPDATE,
 					VFE_PIX_0, ts);
 				if (atomic_read(
@@ -417,6 +415,7 @@ static void msm_vfe46_process_reg_update(struct vfe_device *vfe_dev,
 			}
 			if (vfe_dev->axi_data.stream_update[i])
 				msm_isp_axi_stream_update(vfe_dev, i);
+			msm_isp_save_framedrop_values(vfe_dev, i);
 			if (atomic_read(&vfe_dev->axi_data.axi_cfg_update[i])) {
 				msm_isp_axi_cfg_update(vfe_dev, i);
 				if (atomic_read(
