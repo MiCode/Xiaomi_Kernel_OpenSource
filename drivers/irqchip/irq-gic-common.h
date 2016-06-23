cdf2b97e783de2350b6f5c9920503e5eb2d27998
Runmin Wang <runminw@codeaurora.org>
2016-06-23 11:13:24 -0700

    drivers: GICv3: Enable logging of interrupts that triggered wakeup
    This change enables logging of irq number and name which triggered
    wake up of the system from deep sleep. This helps in debugging the
    spurious wakeups due to interrupts from various subsystems.
    
    Change-Id: I6246871e638e761031c1d2f2b4bb1964b34bd2e5
    CRs-Fixed: 1033023
    Signed-off-by: Runmin Wang <runminw@codeaurora.org>
    Signed-off-by: Maria Yu <aiquny@codeaurora.org>

--- drivers/irqchip/irq-gic-common.h
+++ drivers/irqchip/irq-gic-common.h
@@ -30,6 +30,12 @@ struct gic_quirk {
 extern bool from_suspend;
 extern struct irq_chip gic_arch_extn;
 
+#ifdef CONFIG_QCOM_SHOW_RESUME_IRQ
+extern int msm_show_resume_irq_mask;
+#else
+#define msm_show_resume_irq_mask 0
+#endif
+
 int gic_configure_irq(unsigned int irq, unsigned int type,
                        void __iomem *base, void (*sync_access)(void));
 void gic_dist_config(void __iomem *base, int gic_irqs,
