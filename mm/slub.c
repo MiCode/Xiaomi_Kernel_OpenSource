d964014ce5649c3f20def5ad5dcd87556218a805
Kees Cook <keescook@chromium.org>
2016-06-23 15:24:05 -0700

    UPSTREAM: mm: SLUB hardened usercopy support
    Under CONFIG_HARDENED_USERCOPY, this adds object size checking to the
    SLUB allocator to catch any copies that may span objects. Includes a
    redzone handling fix discovered by Michael Ellerman.
    
    Based on code from PaX and grsecurity.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Tested-by: Michael Ellerman <mpe@ellerman.id.au>
    Reviwed-by: Laura Abbott <labbott@redhat.com>
    
    Change-Id: I52dc6fb3a3492b937d52b5cf9c046bf03dc40a3a
    (cherry picked from commit ed18adc1cdd00a5c55a20fbdaed4804660772281)
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- mm/slub.c
+++ mm/slub.c
@@ -3405,6 +3405,46 @@ static size_t __ksize(const void *object)
 	return slab_ksize(page->slab_cache);
 }
 
+#ifdef CONFIG_HARDENED_USERCOPY
+/*
+ * Rejects objects that are incorrectly sized.
+ *
+ * Returns NULL if check passes, otherwise const char * to name of cache
+ * to indicate an error.
+ */
+const char *__check_heap_object(const void *ptr, unsigned long n,
+				struct page *page)
+{
+	struct kmem_cache *s;
+	unsigned long offset;
+	size_t object_size;
+
+	/* Find object and usable object size. */
+	s = page->slab_cache;
+	object_size = slab_ksize(s);
+
+	/* Reject impossible pointers. */
+	if (ptr < page_address(page))
+		return s->name;
+
+	/* Find offset within object. */
+	offset = (ptr - page_address(page)) % s->size;
+
+	/* Adjust for redzone and reject if within the redzone. */
+	if (kmem_cache_debug(s) && s->flags & SLAB_RED_ZONE) {
+		if (offset < s->red_left_pad)
+			return s->name;
+		offset -= s->red_left_pad;
+	}
+
+	/* Allow address range falling entirely within object size. */
+	if (offset <= object_size && n <= object_size - offset)
+		return NULL;
+
+	return s->name;
+}
+#endif /* CONFIG_HARDENED_USERCOPY */
+
 size_t ksize(const void *object)
 {
 	size_t size = __ksize(object);
