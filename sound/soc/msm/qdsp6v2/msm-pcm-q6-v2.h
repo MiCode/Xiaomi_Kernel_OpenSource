ae051ed76f1f945faffe79e23422689233838e1a
Chaithanya Krishna Bacharaju <chaithan@codeaurora.org>
2016-06-27 19:53:59 +0530

    ASoC: msm: qdsp6v2: Increase PCM capture period size
    Increase PCM capture period size to support recording
    instances that require bigger buffers.
    The maximum period size in bytes is calculated as 61440
    [3840(frames) * 4 (max channels) * 4 (frame size)].
    
    Change-Id: I130e1ad9aaa128c7b94ae70edc52b3f16439cb27
    Signed-off-by: Chaithanya Krishna Bacharaju <chaithan@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-pcm-q6-v2.h
+++ sound/soc/msm/qdsp6v2/msm-pcm-q6-v2.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2008 Google, Inc.
  * Copyright (C) 2008 HTC Corporation
- * Copyright (c) 2012-2015 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2016 The Linux Foundation. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -63,7 +63,7 @@ struct msm_audio_in_frame_info {
 #define PLAYBACK_MIN_PERIOD_SIZE    128
 #define CAPTURE_MIN_NUM_PERIODS     2
 #define CAPTURE_MAX_NUM_PERIODS     8
-#define CAPTURE_MAX_PERIOD_SIZE     16384
+#define CAPTURE_MAX_PERIOD_SIZE     61440
 #define CAPTURE_MIN_PERIOD_SIZE     320
 
 struct msm_audio {
