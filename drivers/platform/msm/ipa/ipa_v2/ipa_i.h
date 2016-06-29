b17ffbbf400500ad424b05394ec4459e55bad614
Utkarsh Saxena <usaxena@codeaurora.org>
2016-06-29 12:16:55 +0530

    msm: ipa: disconnect sequence change for USB 2.0
    For USB 2.0 there is a requirement to not to flush the
    USB endpoints after the pipes are disconnected. Otherwise
    this can result into NOC errors. Make a change to modify the
    disconnect sequence as below.
    
    1) USB driver first disable the pipes
    2) New API is provided from IPA to disable the endpoint.
    3) As part of disable, make sure pipes are empty and reset
    the pipes.
    4) USB resets its BAM and flushes the ep.
    5) USB then disconnects both IPA and USB pipes.
    
    Change-Id: I917f025678e6abb03058d5be4ec42d9e6d76835f
    CRs-Fixed: 1038623
    Acked-by: Chaitanya Pratapa <cpratapa@qti.qualcomm.com>
    Acked-by: Mohammed Javid <mjavid@qti.qualcomm.com>
    Signed-off-by: Utkarsh Saxena <usaxena@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v2/ipa_i.h
+++ drivers/platform/msm/ipa/ipa_v2/ipa_i.h
@@ -553,6 +553,7 @@ struct ipa_ep_context {
 	bool switch_to_intr;
 	int inactive_cycles;
 	u32 eot_in_poll_err;
+	bool ep_disabled;
 
 	/* sys MUST be the last element of this struct */
 	struct ipa_sys_context *sys;
@@ -1424,6 +1425,11 @@ int ipa2_reset_endpoint(u32 clnt_hdl);
  * Remove ep delay
  */
 int ipa2_clear_endpoint_delay(u32 clnt_hdl);
+
+/*
+ * Disable ep
+ */
+int ipa2_disable_endpoint(u32 clnt_hdl);
 
 /*
  * Configuration
