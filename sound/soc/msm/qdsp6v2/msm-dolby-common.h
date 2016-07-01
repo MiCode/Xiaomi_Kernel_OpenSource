fe12679260d2322af715be450097480a6f9a0c6f
Ashish Jain <ashishj@codeaurora.org>
2016-07-01 12:31:21 +0530

    ASoC: msm: qdsp6v2: DAP: Add check to validate param length
    To avoid buffer overflow, validate input length used to
    fetch visualizer data.
    
    CRs-Fixed: 1033540
    Change-Id: I445d1ba3bce47308bc31ae24a70d5ee358f22a2d
    Signed-off-by: Ashish Jain <ashishj@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-dolby-common.h
+++ sound/soc/msm/qdsp6v2/msm-dolby-common.h
@@ -1,5 +1,5 @@
 
-/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2014, 2016 The Linux Foundation. All rights reserved.
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
  * only version 2 as published by the Free Software Foundation.
@@ -232,6 +232,7 @@
 
 #define TOTAL_LENGTH_DOLBY_PARAM		745
 #define DOLBY_VIS_PARAM_HEADER_SIZE		25
+#define DOLBY_PARAM_VCNB_MAX_LENGTH		40
 
 #define DOLBY_INVALID_PORT_ID			-1
 
