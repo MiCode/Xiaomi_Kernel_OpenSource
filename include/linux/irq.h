1cc869442a204358ddb29d4c58cc4fd84847fe85
Thomas Gleixner <tglx@linutronix.de>
2016-07-04 17:39:23 +0900

    genirq: Introduce IRQD_AFFINITY_MANAGED flag
    Interupts marked with this flag are excluded from user space interrupt
    affinity changes. Contrary to the IRQ_NO_BALANCING flag, the kernel internal
    affinity mechanism is not blocked.
    
    This flag will be used for multi-queue device interrupts.
    
    Change-Id: I204c49bb1c8ce87fbcd163119093163b120bfe83
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: linux-block@vger.kernel.org
    Cc: linux-pci@vger.kernel.org
    Cc: linux-nvme@lists.infradead.org
    Cc: axboe@fb.com
    Cc: agordeev@redhat.com
    Link: http://lkml.kernel.org/r/1467621574-8277-3-git-send-email-hch@lst.de
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Git-commit: 9c2555835bb3d34dfac52a0be943dcc4bedd650f
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    [runminw@codeaurora.org: resolve trivial merge conflicts]
    Signed-off-by: Runmin Wang <runminw@codeaurora.org>

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -191,6 +191,7 @@ struct irq_data {
  * IRQD_IRQ_INPROGRESS		- In progress state of the interrupt
  * IRQD_WAKEUP_ARMED		- Wakeup mode armed
  * IRQD_FORWARDED_TO_VCPU	- The interrupt is forwarded to a VCPU
+ * IRQD_AFFINITY_MANAGED	- Affinity is auto-managed by the kernel
  */
 enum {
 	IRQD_TRIGGER_MASK		= 0xf,
@@ -206,6 +207,7 @@ enum {
 	IRQD_IRQ_INPROGRESS		= (1 << 18),
 	IRQD_WAKEUP_ARMED		= (1 << 19),
 	IRQD_FORWARDED_TO_VCPU		= (1 << 20),
+	IRQD_AFFINITY_MANAGED		= (1 << 21),
 };
 
 #define __irqd_to_state(d)		((d)->common->state_use_accessors)
@@ -297,6 +299,11 @@ static inline void irqd_set_forwarded_to_vcpu(struct irq_data *d)
 static inline void irqd_clr_forwarded_to_vcpu(struct irq_data *d)
 {
 	__irqd_to_state(d) &= ~IRQD_FORWARDED_TO_VCPU;
+}
+
+static inline bool irqd_affinity_is_managed(struct irq_data *d)
+{
+	return __irqd_to_state(d) & IRQD_AFFINITY_MANAGED;
 }
 
 static inline irq_hw_number_t irqd_to_hwirq(struct irq_data *d)
