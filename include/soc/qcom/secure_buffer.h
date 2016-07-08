487672ad3f621107ef027fd4bcb14a69eb621bd9
Patrick Daly <pdaly@codeaurora.org>
2016-07-08 14:26:03 -0700

    qcom: secure_buffer: Add CP VMID
    Add definitions for the CP VMIDs so they can be used by clients.
    
    Change-Id: I1ce54de87d54ce665109293422520598c5b09eed
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

--- include/soc/qcom/secure_buffer.h
+++ include/soc/qcom/secure_buffer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2015-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -24,6 +24,8 @@
 #define VMID_CP_CAMERA 0xD
 #define VMID_HLOS_FREE 0xE
 #define VMID_MSS_NONMSA 0x10
+#define VMID_CP_SEC_DISPLAY 0x11
+#define VMID_CP_APP 0x12
 #define VMID_INVAL -1
 
 #define PERM_READ                       0x4
