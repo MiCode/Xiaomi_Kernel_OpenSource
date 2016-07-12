40806589f27ef1cceca2858629d1188761fc5bf6
Ritesh Harjani <riteshh@codeaurora.org>
2016-07-12 12:11:15 +0530

    mmc: core: Remove flag MMC_PM_WAKE_SDIO_IRQ in mmc_resume_host
    MMC_PM_WAKE_SDIO_IRQ flag is used by platform driver(sdhci-msm
    here) to determine if SDIO wakeup IRQ needs to be enabled
    or not. If this flag is not removed in every mmc_resume_host,
    then while suspend, platform driver will configure the sdio
    wakeup IRQ even though wlan is disabled from UI.
    This flag can be enabled by wlan driver before entring into suspend.
    In this case since Wlan is not enable and thus not entering WoW mode,
    pull up present on this SDIO GPIO line will pull the line high (which
    otherwise is pulled low by wlan firmware in WoW mode and triggers
    interrupt by pulling it high in WoW mode) which eventually will
    trigger a false SDIO wakeup interrupt thus breaking the suspend
    everytime.
    Hence disable the flag everytime in mmc_sdio_resume and let
    wlan driver enables it (-by sdio_set_host_pm_flags)
    before entering into suspend if needed
    for sdio wakeup part.
    
    Change-Id: Iecf9eef4d91a741116bc8b8bb59c49ae7f2891fd
    Signed-off-by: Ritesh Harjani <riteshh@codeaurora.org>

--- drivers/mmc/core/sdio.c
+++ drivers/mmc/core/sdio.c
@@ -1076,6 +1076,7 @@ static int mmc_sdio_resume(struct mmc_host *host)
 	mmc_release_host(host);
 
 	host->pm_flags &= ~MMC_PM_KEEP_POWER;
+	host->pm_flags &= ~MMC_PM_WAKE_SDIO_IRQ;
 	return err;
 }
 
