7d882155032e39f3e4e48f47a9f7e0a28aa6e582
Amit Nischal <anischal@codeaurora.org>
2016-07-14 17:20:24 +0530

    clk: qcom: Add support to register rpm-smd clocks
    Update the rpm-smd communication API to send across votes for clock
    enable/disable to RPM. Use the clk_hw list for the RPM clocks and also
    update the clock ids and clock names for RPM clocks.
    
    Change-Id: I37ae97f22b1b39d040bb78c90b1ff231bc348fe6
    Signed-off-by: Amit Nischal <anischal@codeaurora.org>

--- include/dt-bindings/clock/qcom,rpmcc.h
+++ include/dt-bindings/clock/qcom,rpmcc.h
@@ -1,5 +1,6 @@
 /*
  * Copyright 2015 Linaro Limited
+ * Copyright (c) 2016, The Linux Foundation. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -38,47 +39,31 @@
 #define RPM_SFPB_CLK				20
 #define RPM_SFPB_A_CLK				21
 
-/* msm8916 */
-#define RPM_SMD_XO_CLK_SRC				0
-#define RPM_SMD_XO_A_CLK_SRC			1
-#define RPM_SMD_PCNOC_CLK				2
-#define RPM_SMD_PCNOC_A_CLK				3
-#define RPM_SMD_SNOC_CLK				4
-#define RPM_SMD_SNOC_A_CLK				5
-#define RPM_SMD_BIMC_CLK				6
-#define RPM_SMD_BIMC_A_CLK				7
-#define RPM_SMD_QDSS_CLK				8
-#define RPM_SMD_QDSS_A_CLK				9
-#define RPM_SMD_BB_CLK1				10
-#define RPM_SMD_BB_CLK1_A				11
-#define RPM_SMD_BB_CLK2				12
-#define RPM_SMD_BB_CLK2_A				13
-#define RPM_SMD_RF_CLK1				14
-#define RPM_SMD_RF_CLK1_A				15
-#define RPM_SMD_RF_CLK2				16
-#define RPM_SMD_RF_CLK2_A				17
-#define RPM_SMD_BB_CLK1_PIN				18
-#define RPM_SMD_BB_CLK1_A_PIN			19
-#define RPM_SMD_BB_CLK2_PIN				20
-#define RPM_SMD_BB_CLK2_A_PIN			21
-#define RPM_SMD_RF_CLK1_PIN				22
-#define RPM_SMD_RF_CLK1_A_PIN			23
-#define RPM_SMD_RF_CLK2_PIN				24
-#define RPM_SMD_RF_CLK2_A_PIN			25
+/* msm8916 and msm8996 */
+#define RPM_XO_CLK_SRC				0
+#define RPM_XO_A_CLK_SRC			1
+#define RPM_PCNOC_CLK				2
+#define RPM_PCNOC_A_CLK				3
+#define RPM_SNOC_CLK				4
+#define RPM_SNOC_A_CLK				5
+#define RPM_BIMC_CLK				6
+#define RPM_BIMC_A_CLK				7
+#define RPM_QDSS_CLK				8
+#define RPM_QDSS_A_CLK				9
 #define RPM_BB_CLK1				10
 #define RPM_BB_CLK1_A				11
-#define RPM_BB_CLK2				12
-#define RPM_BB_CLK2_A				13
-#define RPM_RF_CLK1				14
-#define RPM_RF_CLK1_A				15
-#define RPM_RF_CLK2				16
-#define RPM_RF_CLK2_A				17
-#define RPM_BB_CLK1_PIN				18
-#define RPM_BB_CLK1_A_PIN			19
-#define RPM_BB_CLK2_PIN				20
-#define RPM_BB_CLK2_A_PIN			21
-#define RPM_RF_CLK1_PIN				22
-#define RPM_RF_CLK1_A_PIN			23
+#define RPM_BB_CLK1_PIN				12
+#define RPM_BB_CLK1_A_PIN			13
+#define RPM_BB_CLK2				14
+#define RPM_BB_CLK2_A				15
+#define RPM_BB_CLK2_PIN				16
+#define RPM_BB_CLK2_A_PIN			17
+#define RPM_RF_CLK1				18
+#define RPM_RF_CLK1_A				19
+#define RPM_RF_CLK1_PIN				20
+#define RPM_RF_CLK1_A_PIN			21
+#define RPM_RF_CLK2				22
+#define RPM_RF_CLK2_A				23
 #define RPM_RF_CLK2_PIN				24
 #define RPM_RF_CLK2_A_PIN			25
 #define RPM_AGGR1_NOC_CLK			26
@@ -101,5 +86,47 @@
 #define RPM_DIV_CLK3_AO				43
 #define RPM_LN_BB_CLK				44
 #define RPM_LN_BB_A_CLK				45
+#define RPM_LN_BB_CLK1				46
+#define RPM_LN_BB_CLK1_AO			47
+#define RPM_LN_BB_CLK1_PIN			48
+#define RPM_LN_BB_CLK1_PIN_AO			49
+#define RPM_LN_BB_CLK2				50
+#define RPM_LN_BB_CLK2_AO			51
+#define RPM_LN_BB_CLK2_PIN			52
+#define RPM_LN_BB_CLK2_PIN_AO			53
+#define RPM_LN_BB_CLK3				54
+#define RPM_LN_BB_CLK3_AO			55
+#define RPM_LN_BB_CLK3_PIN			56
+#define RPM_LN_BB_CLK3_PIN_AO			57
+#define RPM_CNOC_PERIPH_CLK			58
+#define RPM_CNOC_PERIPH_A_CLK			59
+
+/* Voter clocks */
+#define  MMSSNOC_AXI_CLK			60
+#define  MMSSNOC_AXI_A_CLK			61
+#define  MMSSNOC_GDS_CLK			62
+#define  BIMC_MSMBUS_CLK			63
+#define  BIMC_MSMBUS_A_CLK			64
+#define  CNOC_MSMBUS_CLK			65
+#define  CNOC_MSMBUS_A_CLK			66
+#define  PNOC_KEEPALIVE_A_CLK			67
+#define  PNOC_MSMBUS_CLK			68
+#define  PNOC_MSMBUS_A_CLK			69
+#define  PNOC_PM_CLK				70
+#define  PNOC_SPS_CLK				71
+#define  MCD_CE1_CLK				72
+#define  QCEDEV_CE1_CLK				73
+#define  QCRYPTO_CE1_CLK			74
+#define  QSEECOM_CE1_CLK			75
+#define  SCM_CE1_CLK				76
+#define  SNOC_MSMBUS_CLK			77
+#define  SNOC_MSMBUS_A_CLK			78
+#define  CXO_DWC3_CLK				79
+#define  CXO_LPM_CLK				80
+#define  CXO_OTG_CLK				81
+#define  CXO_PIL_LPASS_CLK			82
+#define  CXO_PIL_SSC_CLK			83
+#define  CXO_PIL_CDSP_CLK			84
+#define  CNOC_PERIPH_KEEPALIVE_A_CLK		85
 
 #endif
