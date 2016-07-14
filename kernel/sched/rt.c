cfc771d7ada3db40b2b51ae993ffcb16424d28af
Srinath Sridharan <srinathsr@google.com>
2016-07-13 17:45:49 -0700

    sched/rt: Avoid moving rt task if destination CPU does not run low priority task
    bug: 29512132
    bug: 30115868
    Change-Id: Id18083402dfe2324f86c34a4e5d32fb7e0d11d04

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1344,7 +1344,12 @@ select_task_rq_rt(struct task_struct *p, int cpu, int sd_flag, int flags)
 	     curr->prio <= p->prio)) {
 		int target = find_lowest_rq(p);
 
-		if (target != -1)
+		/*
+		 * Possible race. Don't bother moving it if the
+		 * destination CPU is not running a lower priority task.
+		 */
+                if (target != -1 &&
+                    p->prio < cpu_rq(target)->rt.highest_prio.curr)
 			cpu = target;
 	}
 	rcu_read_unlock();
