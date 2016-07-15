8048d119f532d2c91542712201987236ce2f8eb0
Clarence Ip <cip@codeaurora.org>
2016-07-15 14:17:42 -0400

    drm/msm/sde: add utility functions for kms info
    Add utility functions to support creation of ASCII information
    text of the form,
    
    "key=value\n"
    
    One or more such information strings may be combined together
    and stored within an sde_kms_info structure to be used at a
    later time.
    
    Change-Id: I7fccc14548e66ed7c5b24dc129247572209d506b
    Signed-off-by: Clarence Ip <cip@codeaurora.org>

--- drivers/gpu/drm/msm/sde/sde_kms_utils.c
+++ drivers/gpu/drm/msm/sde/sde_kms_utils.c
@@ -171,3 +171,142 @@ const struct sde_hw_res_map *sde_rm_get_res_map(struct sde_kms *sde_kms,
 			sde_kms->hw_res.res_table[idx].ctl);
 	return &(sde_kms->hw_res.res_table[idx]);
 }
+
+void sde_kms_info_reset(struct sde_kms_info *info)
+{
+	if (info) {
+		info->len = 0;
+		info->staged_len = 0;
+	}
+}
+
+void sde_kms_info_add_keyint(struct sde_kms_info *info,
+		const char *key,
+		int32_t value)
+{
+	uint32_t len;
+
+	if (info && key) {
+		len = snprintf(info->data + info->len,
+				SDE_KMS_INFO_MAX_SIZE - info->len,
+				"%s=%d\n",
+				key,
+				value);
+
+		/* check if snprintf truncated the string */
+		if ((info->len + len) < SDE_KMS_INFO_MAX_SIZE)
+			info->len += len;
+	}
+}
+
+void sde_kms_info_add_keystr(struct sde_kms_info *info,
+		const char *key,
+		const char *value)
+{
+	uint32_t len;
+
+	if (info && key && value) {
+		len = snprintf(info->data + info->len,
+				SDE_KMS_INFO_MAX_SIZE - info->len,
+				"%s=%s\n",
+				key,
+				value);
+
+		/* check if snprintf truncated the string */
+		if ((info->len + len) < SDE_KMS_INFO_MAX_SIZE)
+			info->len += len;
+	}
+}
+
+void sde_kms_info_start(struct sde_kms_info *info,
+		const char *key)
+{
+	uint32_t len;
+
+	if (info && key) {
+		len = snprintf(info->data + info->len,
+				SDE_KMS_INFO_MAX_SIZE - info->len,
+				"%s=",
+				key);
+
+		info->start = true;
+
+		/* check if snprintf truncated the string */
+		if ((info->len + len) < SDE_KMS_INFO_MAX_SIZE)
+			info->staged_len = info->len + len;
+	}
+}
+
+void sde_kms_info_append(struct sde_kms_info *info,
+		const char *str)
+{
+	uint32_t len;
+
+	if (info) {
+		len = snprintf(info->data + info->staged_len,
+				SDE_KMS_INFO_MAX_SIZE - info->staged_len,
+				"%s",
+				str);
+
+		/* check if snprintf truncated the string */
+		if ((info->staged_len + len) < SDE_KMS_INFO_MAX_SIZE) {
+			info->staged_len += len;
+			info->start = false;
+		}
+	}
+}
+
+void sde_kms_info_append_format(struct sde_kms_info *info,
+		uint32_t pixel_format,
+		uint64_t modifier)
+{
+	uint32_t len;
+
+	if (!info)
+		return;
+
+	if (modifier) {
+		len = snprintf(info->data + info->staged_len,
+				SDE_KMS_INFO_MAX_SIZE - info->staged_len,
+				info->start ?
+				"%c%c%c%c/%llX/%llX" : " %c%c%c%c/%llX/%llX",
+				(pixel_format >> 0) & 0xFF,
+				(pixel_format >> 8) & 0xFF,
+				(pixel_format >> 16) & 0xFF,
+				(pixel_format >> 24) & 0xFF,
+				(modifier >> 56) & 0xFF,
+				modifier & ((1ULL << 56) - 1));
+	} else {
+		len = snprintf(info->data + info->staged_len,
+				SDE_KMS_INFO_MAX_SIZE - info->staged_len,
+				info->start ?
+				"%c%c%c%c" : " %c%c%c%c",
+				(pixel_format >> 0) & 0xFF,
+				(pixel_format >> 8) & 0xFF,
+				(pixel_format >> 16) & 0xFF,
+				(pixel_format >> 24) & 0xFF);
+	}
+
+	/* check if snprintf truncated the string */
+	if ((info->staged_len + len) < SDE_KMS_INFO_MAX_SIZE) {
+		info->staged_len += len;
+		info->start = false;
+	}
+}
+
+void sde_kms_info_stop(struct sde_kms_info *info)
+{
+	uint32_t len;
+
+	if (info) {
+		/* insert final delimiter */
+		len = snprintf(info->data + info->staged_len,
+				SDE_KMS_INFO_MAX_SIZE - info->staged_len,
+				"\n");
+
+		/* check if snprintf truncated the string */
+		if ((info->staged_len + len) < SDE_KMS_INFO_MAX_SIZE)
+			info->len = info->staged_len + len;
+	}
+}
+
