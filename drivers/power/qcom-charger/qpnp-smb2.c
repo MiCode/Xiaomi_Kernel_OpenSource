773ed5bacec6790dc0d6b2a1bb722215b5e30c2c
Nicholas Troast <ntroast@codeaurora.org>
2016-07-19 15:09:33 -0700

    qcom-charger: qpnp-smb2: disable vbus-error IRQ trigger
    When a Type-C legacy cable is connected to the phone but left unconnected
    on the other side an interrupt storm can be observed which is caused by
    the vbus-error interrupt trigger. Disable this interrupt trigger.
    
    Change-Id: I5342c58acc83ecff4bf5f8e425de94daa1dabb50
    Signed-off-by: Nicholas Troast <ntroast@codeaurora.org>

--- drivers/power/qcom-charger/qpnp-smb2.c
+++ drivers/power/qcom-charger/qpnp-smb2.c
@@ -528,11 +528,10 @@ static int smb2_init_hw(struct smb2 *chip)
 
 	/*
 	 * trigger the usb-typec-change interrupt only when the CC state
-	 * changes, or there was a VBUS error
+	 * changes
 	 */
 	rc = smblib_write(chg, TYPE_C_INTRPT_ENB_REG,
-			    TYPEC_CCSTATE_CHANGE_INT_EN_BIT
-			  | TYPEC_VBUS_ERROR_INT_EN_BIT);
+			  TYPEC_CCSTATE_CHANGE_INT_EN_BIT);
 	if (rc < 0) {
 		dev_err(chg->dev,
 			"Couldn't configure Type-C interrupts rc=%d\n", rc);
