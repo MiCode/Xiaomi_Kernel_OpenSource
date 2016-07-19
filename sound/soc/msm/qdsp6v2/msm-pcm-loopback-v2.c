4d808519894cf6a182c58fcfcacdbd612dc03194
Surendar karka <sukark@codeaurora.org>
2016-07-19 17:57:02 +0530

    ASoC: msm: qdsp6v2: Add support to set volume in ASM loopback
    Allow to set volume in ASM for the loopback driver. Without the
    get() function the corresponding volume mixer control is
    failing to set volume.
    
    CRs-Fixed: 1034862
    Change-Id: I621dd9de3a8d4a0f4102227989e1dd17638c20ea
    Signed-off-by: Surendar karka <sukark@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-pcm-loopback-v2.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-loopback-v2.c
@@ -51,7 +51,7 @@ struct msm_pcm_loopback {
 	int capture_start;
 	int session_id;
 	struct audio_client *audio_client;
-	int volume;
+	uint32_t volume;
 };
 
 struct fe_dai_session_map {
@@ -155,7 +155,8 @@ static int msm_pcm_loopback_probe(struct snd_soc_platform *platform)
 
 	return 0;
 }
-static int pcm_loopback_set_volume(struct msm_pcm_loopback *prtd, int volume)
+static int pcm_loopback_set_volume(struct msm_pcm_loopback *prtd,
+				   uint32_t volume)
 {
 	int rc = -EINVAL;
 
@@ -474,10 +475,49 @@ static int msm_pcm_volume_ctl_put(struct snd_kcontrol *kcontrol,
 	int rc = 0;
 	struct snd_pcm_volume *vol = kcontrol->private_data;
 	struct snd_pcm_substream *substream = vol->pcm->streams[0].substream;
-	struct msm_pcm_loopback *prtd = substream->runtime->private_data;
+	struct msm_pcm_loopback *prtd;
 	int volume = ucontrol->value.integer.value[0];
 
+	pr_debug("%s: volume : 0x%x\n", __func__, volume);
+	if ((!substream) || (!substream->runtime)) {
+		pr_err("%s substream or runtime not found\n", __func__);
+		rc = -ENODEV;
+		goto exit;
+	}
+	prtd = substream->runtime->private_data;
+	if (!prtd) {
+		rc = -ENODEV;
+		goto exit;
+	}
 	rc = pcm_loopback_set_volume(prtd, volume);
+
+exit:
+	return rc;
+}
+
+static int msm_pcm_volume_ctl_get(struct snd_kcontrol *kcontrol,
+				  struct snd_ctl_elem_value *ucontrol)
+{
+	int rc = 0;
+	struct snd_pcm_volume *vol = snd_kcontrol_chip(kcontrol);
+	struct snd_pcm_substream *substream =
+		vol->pcm->streams[SNDRV_PCM_STREAM_PLAYBACK].substream;
+	struct msm_pcm_loopback *prtd;
+
+	pr_debug("%s\n", __func__);
+	if ((!substream) || (!substream->runtime)) {
+		pr_err("%s substream or runtime not found\n", __func__);
+		rc = -ENODEV;
+		goto exit;
+	}
+	prtd = substream->runtime->private_data;
+	if (!prtd) {
+		rc = -ENODEV;
+		goto exit;
+	}
+	ucontrol->value.integer.value[0] = prtd->volume;
+
+exit:
 	return rc;
 }
 
@@ -497,6 +537,7 @@ static int msm_pcm_add_volume_controls(struct snd_soc_pcm_runtime *rtd)
 		return ret;
 	kctl = volume_info->kctl;
 	kctl->put = msm_pcm_volume_ctl_put;
+	kctl->get = msm_pcm_volume_ctl_get;
 	kctl->tlv.p = loopback_rx_vol_gain;
 	return 0;
 }
