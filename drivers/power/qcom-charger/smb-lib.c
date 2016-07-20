b4713c2ca1a04552445a8fe7602fe1669f76fce4
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2016-07-20 16:54:55 -0700

    smb-lib: account for Jeita while distributing FCC
    The master charger has built in Jeita compensation where it reduces
    FCC when the battery is in cool/warm Jeita threshold. This skews
    the distribution of FCC between master and slave in parallel
    configuration.
    
    Fix this by checking if soft Jeita is active. If so reduce the requested
    fcc so that master and slave get reduced share. However, since master
    is reducing its FCC, add the reduction again to its share so the net
    FCC on master is as per the distribution.
    
    This also calls to handle the battery temperature change interrupt and
    redistribute FCC.
    
    Change-Id: I413cc0231a9125422efc71ed67717921af939959
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- drivers/power/qcom-charger/smb-lib.c
+++ drivers/power/qcom-charger/smb-lib.c
@@ -85,10 +85,36 @@ unlock:
 static void smblib_fcc_split_ua(struct smb_charger *chg, int total_fcc,
 			int *master_ua, int *slave_ua)
 {
+	int rc, cc_reduction_ua = 0;
 	int master_percent = min(max(*chg->pl.master_percent, 0), 100);
+	union power_supply_propval pval = {0, };
 
+	/*
+	 * if master_percent is 0, s/w will configure master's fcc to zero and
+	 * slave's fcc to the max. However since master's fcc is zero it
+	 * disables its own charging and as a result the slave's charging is
+	 * disabled via the fault line.
+	 */
+
+	rc = smblib_get_prop_batt_health(chg, &pval);
+	if (rc == 0) {
+		if (pval.intval == POWER_SUPPLY_HEALTH_WARM
+			|| pval.intval == POWER_SUPPLY_HEALTH_COOL) {
+			rc = smblib_get_charge_param(chg,
+					&chg->param.jeita_cc_comp,
+					&cc_reduction_ua);
+			if (rc < 0) {
+				dev_err(chg->dev, "Could not get jeita comp, rc=%d\n",
+					rc);
+				cc_reduction_ua = 0;
+			}
+		}
+	}
+
+	total_fcc = max(0, total_fcc - cc_reduction_ua);
 	*master_ua = (total_fcc * master_percent) / 100;
 	*slave_ua = (total_fcc - *master_ua) * chg->pl.taper_percent / 100;
+	*master_ua += cc_reduction_ua;
 }
 
 /********************
@@ -1250,6 +1276,16 @@ irqreturn_t smblib_handle_chg_state_change(int irq, void *data)
 	if (pval.intval == POWER_SUPPLY_STATUS_FULL)
 		vote(chg->pl_disable_votable, TAPER_END_VOTER, false, 0);
 
+	return IRQ_HANDLED;
+}
+
+irqreturn_t smblib_handle_batt_temp_changed(int irq, void *data)
+{
+	struct smb_irq_data *irq_data = data;
+	struct smb_charger *chg = irq_data->parent_data;
+
+	rerun_election(chg->fcc_votable);
+	power_supply_changed(chg->batt_psy);
 	return IRQ_HANDLED;
 }
 
