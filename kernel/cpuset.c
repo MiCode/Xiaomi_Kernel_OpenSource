03d7df802473b5b7c783ed6ad6d0144ca94f1c42
Dmitry Shmidt <dimitrysh@google.com>
2016-07-21 11:10:39 -0700

    cpuset: Fix allow_attach hook for cpusets on android.
    Change-Id: Idc7e8c998d9b0ca76eba2aad064f69deac7aa4ee
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -2046,13 +2046,13 @@ static void cpuset_bind(struct cgroup_subsys_state *root_css)
 	mutex_unlock(&cpuset_mutex);
 }
 
-static int cpuset_allow_attach(struct cgroup_taskset *tset)
+static int cpuset_allow_attach(struct cgroup_subsys_state *css,
+			       struct cgroup_taskset *tset)
 {
 	const struct cred *cred = current_cred(), *tcred;
 	struct task_struct *task;
-	struct cgroup_subsys_state *css;
 
-	cgroup_taskset_for_each(task, css, tset) {
+	cgroup_taskset_for_each(task, tset) {
 		tcred = __task_cred(task);
 
 		if ((current != task) && !capable(CAP_SYS_ADMIN) &&
