75bbbc2dfba7f71d097d973aa2b1aebca36ddff7
Jing Zhou <jzhou70@codeaurora.org>
2016-07-22 13:09:44 -0700

    msm: camera: isp: Fix an issue in ispif
    The ispif hardware reset in the stramoff() is not correct.
    The ispif hardware reset can only be done at the open/close
    ispif node. This change is to remove the hardware reset during
    the streamoff which causes the issue in the PIP use case.
    
    Change-Id: I5a7428b7ac76c6b360d0a97c07473886170d8e65
    Signed-off-by: Jing Zhou <jzhou70@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/ispif/msm_ispif.c
+++ drivers/media/platform/msm/camera_v2/ispif/msm_ispif.c
@@ -1041,7 +1041,6 @@ static int msm_ispif_stop_immediately(struct ispif_device *ispif,
 			cid_mask, params->entries[i].vfe_intf, 0);
 	}
 
-	rc = msm_ispif_reset_hw(ispif);
 	return rc;
 }
 
@@ -1168,7 +1167,6 @@ static int msm_ispif_stop_frame_boundary(struct ispif_device *ispif,
 	}
 
 end:
-	rc = msm_ispif_reset_hw(ispif);
 	return rc;
 }
 
