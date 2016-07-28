cb9bad161a3dd993cc12294609587d989d52d335
Jack Pham <jackp@codeaurora.org>
2016-07-28 11:51:07 -0700

    usb: gadget: f_fs: Add support for ipc logging
    Log function entry and exit and dump relevant values
    into ipc log buffer. This allows to debug various
    race conditions and stability issues.
    
    Change-Id: I15011d79fc2f054e64f8bbd1f8f5db8944b46ada
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>
    Signed-off-by: Jack Pham <jackp@codeaurora.org>

--- drivers/usb/gadget/function/u_fs.h
+++ drivers/usb/gadget/function/u_fs.h
@@ -18,6 +18,7 @@
 #include <linux/mutex.h>
 #include <linux/workqueue.h>
 #include <linux/refcount.h>
+#include <linux/ipc_logging.h>
 
 #ifdef VERBOSE_DEBUG
 #ifndef pr_vdebug
@@ -285,6 +286,8 @@ struct ffs_data {
 	 * destroyed by ffs_epfiles_destroy().
 	 */
 	struct ffs_epfile		*epfiles;
+
+	void				*ipc_log;
 };
 
 
