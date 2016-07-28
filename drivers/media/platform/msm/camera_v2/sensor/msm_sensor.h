e41cdfc7457569d07c3217f7425b35b5e1e7967c
Sureshnaidu Laveti <lsuresh@codeaurora.org>
2016-07-27 23:51:04 -0700

    msm: camera: secure camera support
    Adding relay driver functionality for camera sensor
    module to pass cci commands to trustzone and operate
    in secure mode.
    
    Change-Id: I3d1345118df151f8521d5ad230311a56105b5f4a
    Signed-off-by: Alexander Kolesnikov <akolesni@codeaurora.org>
    Signed-off-by: Amit Blay <ablay@codeaurora.org>
    Signed-off-by: Nishant Pandit <npandit@codeaurora.org>
    Signed-off-by: Sureshnaidu Laveti <lsuresh@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor.h
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -88,6 +88,7 @@ struct msm_sensor_ctrl_t {
 	enum msm_camera_stream_type_t camera_stream_type;
 	uint32_t set_mclk_23880000;
 	uint8_t is_csid_tg_mode;
+	uint32_t is_secure;
 };
 
 int msm_sensor_config(struct msm_sensor_ctrl_t *s_ctrl, void __user *argp);
