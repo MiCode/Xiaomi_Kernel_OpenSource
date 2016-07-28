ac38e023504d93335c20aa22f69fdccdc1588716
Patrick Bellasi <patrick.bellasi@arm.com>
2016-07-28 17:40:49 +0100

    FIXUP: sched/tune: fix compilation error on !CONFIG_CGROUP_SCHEDTUNE
    When SchedTune is configured without CGroups support, the schedtune_accept_deltas()
    function is not declared.
    Since schedule candidate filtering is required also when global boosting
    mode is in use, this patch move the declaration of that function to make
    it dependant just on CONFIG_SCHED_TUNE.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/tune.h
+++ kernel/sched/tune.h
@@ -20,15 +20,15 @@ int schedtune_task_boost(struct task_struct *tsk);
 void schedtune_enqueue_task(struct task_struct *p, int cpu);
 void schedtune_dequeue_task(struct task_struct *p, int cpu);
 
-int schedtune_accept_deltas(int nrg_delta, int cap_delta,
-			    struct task_struct *task);
-
 #else /* CONFIG_CGROUP_SCHEDTUNE */
 
 #define schedtune_enqueue_task(task, cpu) do { } while (0)
 #define schedtune_dequeue_task(task, cpu) do { } while (0)
 
 #endif /* CONFIG_CGROUP_SCHEDTUNE */
+
+int schedtune_accept_deltas(int nrg_delta, int cap_delta,
+			    struct task_struct *task);
 
 #else /* CONFIG_SCHED_TUNE */
 
