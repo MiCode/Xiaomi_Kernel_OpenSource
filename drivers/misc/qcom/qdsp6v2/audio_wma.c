94cae8078094b8aacfbf6282bffd207b05294315
Siena Richard <sienar@codeaurora.org>
2016-08-16 13:03:56 -0700

    misc: qcom: qdsp6v2: initialize wma_config_32
    Not all memebers of wma_config_32 are set before they are used which
    might lead to invalid values being passed and used. To fix this issue
    initialize all member variables of struct wma_config_32 to 0 before
    assigning specific values individually.
    
    Change-Id: Ibb082ce691625527e9a9ffd4978dea7ba4df9e84
    CRs-Fixed: 1054352
    Signed-off-by: Siena Richard <sienar@codeaurora.org>

--- drivers/misc/qcom/qdsp6v2/audio_wma.c
+++ drivers/misc/qcom/qdsp6v2/audio_wma.c
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 2008 Google, Inc.
  * Copyright (C) 2008 HTC Corporation
- * Copyright (c) 2009-2015, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2009-2016, The Linux Foundation. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -165,6 +165,8 @@ static long audio_compat_ioctl(struct file *file, unsigned int cmd,
 	case AUDIO_GET_WMA_CONFIG_V2_32: {
 		struct msm_audio_wma_config_v2 *wma_config;
 		struct msm_audio_wma_config_v2_32 wma_config_32;
+
+		memset(&wma_config_32, 0, sizeof(wma_config_32));
 
 		wma_config = (struct msm_audio_wma_config_v2 *)audio->codec_cfg;
 		wma_config_32.format_tag = wma_config->format_tag;
