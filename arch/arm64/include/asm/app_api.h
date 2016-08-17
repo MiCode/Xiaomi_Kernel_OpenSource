9d7f377c60108b2bec037de6dc3bc3d195bb66b2
Imran Khan <kimran@codeaurora.org>
2016-08-17 21:18:14 +0530

    arm64: Add support for 32 bit app specific settings
    Add support to enable app specific settings, whenever
    a 32 bit application is running.
    
    Change-Id: I0cf40e8ea6e3f9e62258a44ccdd959e9eae09d88
    Signed-off-by: Imran Khan <kimran@codeaurora.org>

--- arch/arm64/include/asm/app_api.h
+++ arch/arm64/include/asm/app_api.h
@@ -27,16 +27,23 @@
 #ifdef CONFIG_MSM_APP_API
 extern void set_app_setting_bit(uint32_t bit);
 extern void clear_app_setting_bit(uint32_t bit);
+extern void set_app_setting_bit_for_32bit_apps(void);
+extern void clear_app_setting_bit_for_32bit_apps(void);
 #else
 static inline void set_app_setting_bit(uint32_t bit) {}
 static inline void clear_app_setting_bit(uint32_t bit) {}
+static inline void set_app_setting_bit_for_32bit_apps(void) {}
+static inline void clear_app_setting_bit_for_32bit_apps(void) {}
 #endif
 
 #ifdef CONFIG_MSM_APP_SETTINGS
 extern void switch_app_setting_bit(struct task_struct *prev,
 				   struct task_struct *next);
+extern void switch_32bit_app_setting_bit(struct task_struct *prev,
+				   struct task_struct *next);
 extern void apply_app_setting_bit(struct file *file);
 extern bool use_app_setting;
+extern bool use_32bit_app_setting;
 #endif
 
 #endif
