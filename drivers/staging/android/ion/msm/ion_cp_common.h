0b12e9cdcb9785f0c21c7881efdf32b9328628e3
Clyde Byrd III <cbyrd@codeaurora.org>
2016-08-18 19:44:00 -0700

    ion: msm: remove redundant function
    Remove a function that results in redefinition errors
    when CONFIG_ION_MSM and CONFIG_ION are disabled.
    These functions should be placed in
    /include/soc/secure_buffer.h
    
    CRs-Fixed: 1058359
    Change-Id: I380d68a80d0d3a78f1e7662f2965ed4e8c46a634
    Signed-off-by: Clyde Byrd III <cbyrd@codeaurora.org>

--- drivers/staging/android/ion/msm/ion_cp_common.h
+++ drivers/staging/android/ion/msm/ion_cp_common.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2014, 2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -74,8 +74,6 @@ int ion_cp_secure_buffer(struct ion_buffer *buffer, int version, void *data,
 
 int ion_cp_unsecure_buffer(struct ion_buffer *buffer, int force_unsecure);
 
-bool msm_secure_v2_is_supported(void);
-
 #else
 static inline int ion_cp_change_chunks_state(unsigned long chunks,
 			unsigned int nchunks, unsigned int chunk_size,
@@ -108,11 +106,6 @@ static inline int ion_cp_unsecure_buffer(struct ion_buffer *buffer,
 				int force_unsecure)
 {
 	return -ENODEV;
-}
-
-static inline bool msm_secure_v2_is_supported(void)
-{
-	return false;
 }
 
 #endif
