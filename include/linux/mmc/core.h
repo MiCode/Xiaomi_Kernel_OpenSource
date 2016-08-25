0b0a3c9ffcf2d249e2dbe5f226974de439d6f332
Mohamad Ayyash <mkayyash@google.com>
2016-08-25 01:00:06 +0000

    Revert "Android: MMC/UFS IO Latency Histograms."
    This reverts commit 5d75a20fb753fd18e410148a035dbf8b3ebfcf0b.
    
    Change-Id: I291e7bd0ddf6e45d2e3bf0d940df79e357efe421

--- include/linux/mmc/core.h
+++ include/linux/mmc/core.h
@@ -135,8 +135,6 @@ struct mmc_request {
 	struct completion	completion;
 	void			(*done)(struct mmc_request *);/* completion function */
 	struct mmc_host		*host;
-	ktime_t			io_start;
-	int			lat_hist_enabled;
 };
 
 struct mmc_card;
