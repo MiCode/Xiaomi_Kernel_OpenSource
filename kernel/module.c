1e29659b7250b6b7c4b10c7bc9f3c11b35546511
Luis R. Rodriguez <mcgrof@suse.com>
2016-08-30 21:01:24 +0530

    driver-core: add driver module asynchronous probe support
    Some init systems may wish to express the desire to have device
    drivers run their probe() code asynchronously.
    This implements support for this and allows userspace to request
    async probe as a preference through a generic shared device driver
    module parameter, async_probe.
    
    Implementation for async probe is supported through a module
    parameter given that since synchronous probe has been prevalent
    for years some userspace might exist which relies on the fact that
    the device driver will probe synchronously and the assumption that
    devices it provides will be immediately available after this.
    
    Change-Id: Ic20ee2354b2efdcf3c0d02c407863af559ff86fa
    Signed-off-by: Luis R. Rodriguez <mcgrof@suse.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Git-commit: f2411da746985e60d4d087f3a43e271c61785927
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>

--- kernel/module.c
+++ kernel/module.c
@@ -3106,7 +3106,7 @@ static int do_init_module(struct module *mod)
 	 *
 	 * http://thread.gmane.org/gmane.linux.kernel/1420814
 	 */
-	if (current->flags & PF_USED_ASYNC)
+	if (!mod->async_probe_requested && (current->flags & PF_USED_ASYNC))
 		async_synchronize_full();
 
 	mutex_lock(&module_mutex);
@@ -3236,8 +3236,15 @@ out:
 static int unknown_module_param_cb(char *param, char *val, const char *modname,
 				   void *arg)
 {
-	/* Check for magic 'dyndbg' arg */ 
-	int ret = ddebug_dyndbg_module_param_cb(param, val, modname);
+	struct module *mod = arg;
+	int ret;
+
+	if (strcmp(param, "async_probe") == 0) {
+		mod->async_probe_requested = true;
+		return 0;
+	}
+	/* Check for magic 'dyndbg' arg */
+	ret = ddebug_dyndbg_module_param_cb(param, val, modname);
 	if (ret != 0)
 		pr_warn("%s: unknown parameter '%s' ignored\n", modname, param);
 	return 0;
