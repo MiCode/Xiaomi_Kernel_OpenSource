cd70f08dcab1ff7834d0756a5db9c1aa1332beac
Walter Yang <yandongy@codeaurora.org>
2016-09-07 16:28:50 +0800

    ASoC: msm: initialize the params array before using it
    The params array is used without initialization, which may cause
    security issues. Initialize it as all zero after the definition.
    
    CRs-Fixed: 1062271
    Change-Id: If462fe3d82f139d72547f82dc7eb564f83cb35bf
    Signed-off-by: Walter Yang <yandongy@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-compr-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-compr-q6-v2.c
@@ -1036,6 +1036,7 @@ static int msm_compr_ioctl_shared(struct snd_pcm_substream *substream,
 			struct snd_dec_ddp *ddp =
 				&compr->info.codec_param.codec.options.ddp;
 			uint32_t params_length = 0;
+			memset(params_value, 0, MAX_AC3_PARAM_SIZE);
 			/* check integer overflow */
 			if (ddp->params_length > UINT_MAX/sizeof(int)) {
 				pr_err("%s: Integer overflow ddp->params_length %d\n",
@@ -1076,6 +1077,7 @@ static int msm_compr_ioctl_shared(struct snd_pcm_substream *substream,
 			struct snd_dec_ddp *ddp =
 				&compr->info.codec_param.codec.options.ddp;
 			uint32_t params_length = 0;
+			memset(params_value, 0, MAX_AC3_PARAM_SIZE);
 			/* check integer overflow */
 			if (ddp->params_length > UINT_MAX/sizeof(int)) {
 				pr_err("%s: Integer overflow ddp->params_length %d\n",
