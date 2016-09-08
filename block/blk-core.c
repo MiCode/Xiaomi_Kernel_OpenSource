8cf50759e0ff259cef5bc557cd1cdd6977273ea5
Mohan Srinivasan <srmohan@google.com>
2016-09-07 17:39:42 -0700

    Android: Fix build breakages.
    The IO latency histogram change broke allmodconfig and
    allnoconfig builds. This fixes those breakages.
    
    Change-Id: I9cdae655b40ed155468f3cef25cdb74bb56c4d3e
    Signed-off-by: Mohan Srinivasan <srmohan@google.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -3345,6 +3345,7 @@ blk_zero_latency_hist(struct io_latency_state *s)
 	s->latency_reads_elems = 0;
 	s->latency_writes_elems = 0;
 }
+EXPORT_SYMBOL(blk_zero_latency_hist);
 
 ssize_t
 blk_latency_hist_show(struct io_latency_state *s, char *buf)
@@ -3406,3 +3407,4 @@ blk_latency_hist_show(struct io_latency_state *s, char *buf)
 	}
 	return bytes_written;
 }
+EXPORT_SYMBOL(blk_latency_hist_show);
