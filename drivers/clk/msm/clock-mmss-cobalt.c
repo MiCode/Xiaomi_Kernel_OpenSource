56a1dc17462094155f6a2c105bc46d7670a3e459
Deepak Katragadda <dkatraga@codeaurora.org>
2016-09-12 11:07:46 -0700

    clk: msm: clock: Register the mnoc_maxi_clk after the vmem_maxi_clk
    Registering the vmem_maxi_clk after the mnoc_maxi_clk leads to it
    being added to the handoff list after its FSM clock. This results
    in the mnoc_maxi_clk being stuck ON when it's disabled as part of
    clock_late_init. Hence, change the order of registering these clocks.
    
    CRs-Fixed: 1065813
    Change-Id: If076545f9557f1be2633f72fca5b9e8096b6501b
    Signed-off-by: Deepak Katragadda <dkatraga@codeaurora.org>

--- drivers/clk/msm/clock-mmss-cobalt.c
+++ drivers/clk/msm/clock-mmss-cobalt.c
@@ -2626,7 +2626,6 @@ static struct clk_lookup msm_clocks_mmss_cobalt[] = {
 	CLK_LIST(mmss_misc_ahb_clk),
 	CLK_LIST(mmss_misc_cxo_clk),
 	CLK_LIST(mmss_mnoc_ahb_clk),
-	CLK_LIST(mmss_mnoc_maxi_clk),
 	CLK_LIST(mmss_video_subcore0_clk),
 	CLK_LIST(mmss_video_subcore1_clk),
 	CLK_LIST(mmss_video_ahb_clk),
@@ -2635,6 +2634,7 @@ static struct clk_lookup msm_clocks_mmss_cobalt[] = {
 	CLK_LIST(mmss_video_maxi_clk),
 	CLK_LIST(mmss_vmem_ahb_clk),
 	CLK_LIST(mmss_vmem_maxi_clk),
+	CLK_LIST(mmss_mnoc_maxi_clk),
 	CLK_LIST(mmss_debug_mux),
 };
 
