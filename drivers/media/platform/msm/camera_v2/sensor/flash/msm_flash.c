ce6efe32c8910e5bc0fe197cdf9612e1a257894f
Rajesh Bondugula <rajeshb@codeaurora.org>
2016-09-13 11:19:23 -0700

    msm: camera: sensor: Change initializer to 0 instead of -EINVAL
    When FLASH_LOW / FLASH_HIGH is called multiple times the function
    must return success without error if the flash is already in
    desired state. But due to initializer -EINVAL is returned.
    So setting initializer to 0.
    
    CRs-Fixed: 1062351
    Signed-off-by: Rajesh Bondugula <rajeshb@codeaurora.org>
    Change-Id: Ie6ff66e272005bc83b3bb6ea8c353d4eb9088048

--- drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.c
+++ drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.c
@@ -587,7 +587,7 @@ static int32_t msm_flash_release(
 static int32_t msm_flash_config(struct msm_flash_ctrl_t *flash_ctrl,
 	void __user *argp)
 {
-	int32_t rc = -EINVAL;
+	int32_t rc = 0;
 	struct msm_flash_cfg_data_t *flash_data =
 		(struct msm_flash_cfg_data_t *) argp;
 
