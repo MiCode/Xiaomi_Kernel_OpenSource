d538879e7328a6f7b48e7f7255aa629ede121a2b
Nikhilesh Reddy <reddyn@codeaurora.org>
2016-09-19 11:43:55 -0700

    UBI: Re-erase the PEB before writing the VID header
    Re-erase the PEB before writing the VID header without updating the
    EC counter of the PEB.
    
    Change-Id: I139a997460991775e467cbf57f5e7a326c54ae06
    Signed-off-by: Nikhilesh Reddy <reddyn@codeaurora.org>

--- drivers/mtd/ubi/io.c
+++ drivers/mtd/ubi/io.c
@@ -1101,6 +1101,16 @@ int ubi_io_write_vid_hdr(struct ubi_device *ubi, int pnum,
 	dbg_io("write VID header to PEB %d", pnum);
 	ubi_assert(pnum >= 0 &&  pnum < ubi->peb_count);
 
+	/*
+	 * Re-erase the PEB before using it. This should minimize any issues
+	 * from decay of charge in this block.
+	 */
+	if (ubi->wl_is_inited) {
+		err = ubi_wl_re_erase_peb(ubi, pnum);
+		if (err)
+			return err;
+	}
+
 	err = self_check_peb_ec_hdr(ubi, pnum);
 	if (err)
 		return err;
