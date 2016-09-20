20045e59c3e353b83d36766712c96476f7fb3e06
Trilok Soni <tsoni@codeaurora.org>
2016-09-20 14:55:35 -0700

    arm64: process: Reduce the no. of bytes of data around registers.
    As a part of __show_regs we print the 512 bytes of data around
    key registers like SP, PC and LR. This data is not always useful
    and can cause issue while we have more cores in the system and need
    to print them per core during the stack traces triggered through
    WARN(..) like APIs. It can also cause latency issues due to heavy
    printing on the serial console.
    
    Reduce the logging to 128 bytes (nbytes * 2) around these registers
    from 512 bytes.
    
    Change-Id: Ie19fececd9d2bc6cd3328a6c63c956bcc9eed9a6
    Signed-off-by: Trilok Soni <tsoni@codeaurora.org>

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -259,7 +259,7 @@ void __show_regs(struct pt_regs *regs)
 			printk("\n");
 	}
 	if (!user_mode(regs))
-		show_extra_register_data(regs, 256);
+		show_extra_register_data(regs, 64);
 	printk("\n");
 }
 
