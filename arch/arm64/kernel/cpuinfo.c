7a106f27164a9c20bb88f7e31cb6195de6945b09
Channagoud Kadabi <ckadabi@codeaurora.org>
2016-09-20 14:03:36 -0700

    arm: cpuinfo: drop L1 cache type print from pr_info to pr_debug
    This message is printed every time a CPU is hotplugged in,
    resulting in log noise and hurting hotplug latency. Silence
    it by default, by dropping it to a pr_debug.
    
    Change-Id: I05b643a90abc17e0134830874555e79138237261
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>
    Signed-off-by: Channagoud Kadabi <ckadabi@codeaurora.org>

--- arch/arm64/kernel/cpuinfo.c
+++ arch/arm64/kernel/cpuinfo.c
@@ -329,7 +329,8 @@ static void cpuinfo_detect_icache_policy(struct cpuinfo_arm64 *info)
 	if (l1ip == ICACHE_POLICY_AIVIVT)
 		set_bit(ICACHEF_AIVIVT, &__icache_flags);
 
-	pr_info("Detected %s I-cache on CPU%d\n", icache_policy_str[l1ip], cpu);
+	pr_debug("Detected %s I-cache on CPU%d\n", icache_policy_str[l1ip],
+			cpu);
 }
 
 static void __cpuinfo_store_cpu(struct cpuinfo_arm64 *info)
