72ce5ca478c1463ae4172c4a4aa5611affe81ad7
Vevek Venkatesan <vevekv@codeaurora.org>
2016-09-21 18:40:25 +0530

    input: misc: pat9125: code clean-up in pat9125 driver
    Remove redundant code from the Pixart PAT9125 reference driver
    - Remove unwanted struct declarations and definitions
    - Remove unwanted header files
    - Dynamically allocate platform data structure
    - Change pr_err to dev_err
    - Changes as per Linux coding guidelines
    - Clean-up probe and remove routines
    - Remove unwanted file operations
    - Remove unused functions and variables
    
    Change-Id: I71b1d2548dc07c21ab8f12327662f5bb6998cdcb
    Signed-off-by: Vevek Venkatesan <vevekv@codeaurora.org>
    Signed-off-by: Shantanu Jain <shjain@codeaurora.org>

--- drivers/input/misc/ots_pat9125/pixart_ots.c
+++ drivers/input/misc/ots_pat9125/pixart_ots.c
@@ -4,68 +4,74 @@
  *
  */
 
+#include "pixart_platform.h"
 #include "pixart_ots.h"
 
-static void OTS_WriteRead(uint8_t address, uint8_t wdata);
+static void ots_write_read(struct i2c_client *client, u8 address, u8 wdata)
+{
+	u8 read_value;
 
-bool OTS_Sensor_Init(void)
+	do {
+		write_data(client, address, wdata);
+		read_value = read_data(client, address);
+	} while (read_value != wdata);
+}
+
+bool ots_sensor_init(struct i2c_client *client)
 {
 	unsigned char sensor_pid = 0, read_id_ok = 0;
 
 	/*
 	 * Read sensor_pid in address 0x00 to check if the
-	  * serial link is valid, read value should be 0x31.
+	 * serial link is valid, read value should be 0x31.
 	 */
-	sensor_pid = ReadData(0x00);
+	sensor_pid = read_data(client, PIXART_PAT9125_PRODUCT_ID1_REG);
 
-	if (sensor_pid == 0x31) {
+	if (sensor_pid == PIXART_PAT9125_SENSOR_ID) {
 		read_id_ok = 1;
 
 		/*
-		 *PAT9125 sensor recommended settings:
-		 * switch to bank0, not allowed to perform OTS_RegWriteRead
+		 * PAT9125 sensor recommended settings:
+		 * switch to bank0, not allowed to perform ots_write_read
 		 */
-		WriteData(0x7F, 0x00);
+		write_data(client, PIXART_PAT9125_SELECT_BANK_REG,
+				PIXART_PAT9125_BANK0);
 		/*
 		 * software reset (i.e. set bit7 to 1).
 		 * It will reset to 0 automatically
 		 * so perform OTS_RegWriteRead is not allowed.
 		 */
-		WriteData(0x06, 0x97);
+		write_data(client, PIXART_PAT9125_CONFIG_REG,
+				PIXART_PAT9125_RESET);
 
 		/* delay 1ms */
-		delay_ms(1);
+		usleep_range(RESET_DELAY_US, RESET_DELAY_US + 1);
 
 		/* disable write protect */
-		OTS_WriteRead(0x09, 0x5A);
+		ots_write_read(client, PIXART_PAT9125_WRITE_PROTECT_REG,
+				PIXART_PAT9125_DISABLE_WRITE_PROTECT);
 		/* set X-axis resolution (depends on application) */
-		OTS_WriteRead(0x0D, 0x65);
+		ots_write_read(client, PIXART_PAT9125_SET_CPI_RES_X_REG,
+				PIXART_PAT9125_CPI_RESOLUTION_X);
 		/* set Y-axis resolution (depends on application) */
-		OTS_WriteRead(0x0E, 0xFF);
+		ots_write_read(client, PIXART_PAT9125_SET_CPI_RES_Y_REG,
+				PIXART_PAT9125_CPI_RESOLUTION_Y);
 		/* set 12-bit X/Y data format (depends on application) */
-		OTS_WriteRead(0x19, 0x04);
+		ots_write_read(client, PIXART_PAT9125_ORIENTATION_REG,
+				PIXART_PAT9125_MOTION_DATA_LENGTH);
 		/* ONLY for VDD=VDDA=1.7~1.9V: for power saving */
-		OTS_WriteRead(0x4B, 0x04);
+		ots_write_read(client, PIXART_PAT9125_VOLTAGE_SEGMENT_SEL_REG,
+				PIXART_PAT9125_LOW_VOLTAGE_SEGMENT);
 
-		if (ReadData(0x5E) == 0x04) {
-			OTS_WriteRead(0x5E, 0x08);
-			if (ReadData(0x5D) == 0x10)
-				OTS_WriteRead(0x5D, 0x19);
+		if (read_data(client, PIXART_PAT9125_MISC2_REG) == 0x04) {
+			ots_write_read(client, PIXART_PAT9125_MISC2_REG, 0x08);
+			if (read_data(client, PIXART_PAT9125_MISC1_REG) == 0x10)
+				ots_write_read(client, PIXART_PAT9125_MISC1_REG,
+						0x19);
 		}
-		OTS_WriteRead(0x09, 0x00);/* enable write protect */
+		/* enable write protect */
+		ots_write_read(client, PIXART_PAT9125_WRITE_PROTECT_REG,
+				PIXART_PAT9125_ENABLE_WRITE_PROTECT);
 	}
 	return read_id_ok;
-}
-
-static void OTS_WriteRead(uint8_t address, uint8_t wdata)
-{
-	uint8_t read_value;
-
-	do {
-		/* Write data to specified address */
-		WriteData(address, wdata);
-		/* Read back previous written data */
-		read_value = ReadData(address);
-		/* Check if the data is correctly written */
-	} while (read_value != wdata);
 }
