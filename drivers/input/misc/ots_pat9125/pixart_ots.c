f17d100ad47adb5b2b8d4dd3ac0a45c197bad04b
Himanshu Aggarwal <haggarwa@codeaurora.org>
2016-10-04 09:55:31 +0530

    input: misc: pat9125: fix issues found in static analysis
    Fix the following issues found in static analysis:
    - missing blank lines after declarations
    - unnecessary return in a void function
    - exporting world writable files is usually an error
    - block comments use * on subsequent and trailing lines
    - Add DT binding description and vendor prefixe for the
    driver.
    - Add Linux Foundation copyright markings.
    
    Change-Id: Id2ed9a6d4e31bd2acd3185bf7ef9c78f8a41cf36
    Signed-off-by: Himanshu Aggarwal <haggarwa@codeaurora.org>
    Signed-off-by: Vevek Venkatesan <vevekv@codeaurora.org>
    Signed-off-by: Shantanu Jain <shjain@codeaurora.org>

--- drivers/input/misc/ots_pat9125/pixart_ots.c
+++ drivers/input/misc/ots_pat9125/pixart_ots.c
@@ -1,3 +1,8 @@
+/* drivers/input/misc/ots_pat9125/pixart_ots.c
+ *
+ * Copyright (c) 2016, The Linux Foundation. All rights reserved.
+ *
+ */
 
 #include "pixart_ots.h"
 
@@ -7,22 +12,29 @@ bool OTS_Sensor_Init(void)
 {
 	unsigned char sensor_pid = 0, read_id_ok = 0;
 
-	/* Read sensor_pid in address 0x00 to check if the
-	 serial link is valid, read value should be 0x31. */
+	/*
+	 * Read sensor_pid in address 0x00 to check if the
+	  * serial link is valid, read value should be 0x31.
+	 */
 	sensor_pid = ReadData(0x00);
 
 	if (sensor_pid == 0x31) {
 		read_id_ok = 1;
 
-		/* PAT9125 sensor recommended settings: */
-		/* switch to bank0, not allowed to perform OTS_RegWriteRead */
+		/*
+		 *PAT9125 sensor recommended settings:
+		 * switch to bank0, not allowed to perform OTS_RegWriteRead
+		 */
 		WriteData(0x7F, 0x00);
-		/* software reset (i.e. set bit7 to 1).
-		It will reset to 0 automatically */
-		/* so perform OTS_RegWriteRead is not allowed. */
+		/*
+		 * software reset (i.e. set bit7 to 1).
+		 * It will reset to 0 automatically
+		 * so perform OTS_RegWriteRead is not allowed.
+		 */
 		WriteData(0x06, 0x97);
 
-		delay_ms(1);				/* delay 1ms */
+		/* delay 1ms */
+		delay_ms(1);
 
 		/* disable write protect */
 		OTS_WriteRead(0x09, 0x5A);
@@ -48,6 +60,7 @@ bool OTS_Sensor_Init(void)
 static void OTS_WriteRead(uint8_t address, uint8_t wdata)
 {
 	uint8_t read_value;
+
 	do {
 		/* Write data to specified address */
 		WriteData(address, wdata);
@@ -55,6 +68,4 @@ static void OTS_WriteRead(uint8_t address, uint8_t wdata)
 		read_value = ReadData(address);
 		/* Check if the data is correctly written */
 	} while (read_value != wdata);
-	return;
 }
-
