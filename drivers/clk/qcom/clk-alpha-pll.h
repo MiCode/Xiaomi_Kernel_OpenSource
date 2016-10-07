72609b4929859621ee4741112fe2746897444083
Taniya Das <tdas@codeaurora.org>
2016-10-07 13:00:03 +0530

    clk: qcom: Add support to be able to slew PLL
    Few alpha PLLs would require to be able to slew in the same VCO mode, which
    means the PLL would be able to update the new frequency L value without
    turning it off. But to be support this feature the PLL needs to calibrated
    at the mid of the VCO range and then enabled.
    
    Change-Id: I24820bd6254002f8a8db9604d230dcbce59b1beb
    Signed-off-by: Taniya Das <tdas@codeaurora.org>

--- drivers/clk/qcom/clk-alpha-pll.h
+++ drivers/clk/qcom/clk-alpha-pll.h
@@ -80,6 +80,7 @@ struct clk_alpha_pll_postdiv {
 extern const struct clk_ops clk_alpha_pll_ops;
 extern const struct clk_ops clk_alpha_pll_hwfsm_ops;
 extern const struct clk_ops clk_alpha_pll_postdiv_ops;
+extern const struct clk_ops clk_alpha_pll_slew_ops;
 
 void clk_alpha_pll_configure(struct clk_alpha_pll *pll, struct regmap *regmap,
 		const struct pll_config *config);
