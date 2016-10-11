7b1311c5481073846de89d84945703429f2f118b
Camus Wong <camusw@codeaurora.org>
2016-10-10 22:44:47 -0400

    msm: mdss: mdp: Fix the intf count
    In MDP_DISP_INTF_SEL register, it uses different value to represent the
    display type. It cannot assume enabled intf is always 1.
    
    CRs-Fixed: 1078202
    Change-Id: I73e36f9bb369f3200b99f66fb5231a0afe389913
    Signed-off-by: Camus Wong <camusw@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp.c
+++ drivers/video/msm/mdss/mdss_mdp.c
@@ -2727,7 +2727,8 @@ static int mdss_mdp_probe(struct platform_device *pdev)
 		MDSS_MDP_REG_SPLIT_DISPLAY_EN);
 	if (intf_sel != 0) {
 		for (i = 0; i < 4; i++)
-			num_of_display_on += ((intf_sel >> i*8) & 0x000000FF);
+			if ((intf_sel >> i*8) & 0x000000FF)
+				num_of_display_on++;
 
 		/*
 		 * For split display enabled - DSI0, DSI1 interfaces are
