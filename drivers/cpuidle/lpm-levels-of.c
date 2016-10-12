46112a9d4aea28e49b7d69626089c28f7e3aa047
Raju P.L.S.S.S.N <rplsssn@codeaurora.org>
2016-10-12 11:23:55 +0530

    lpm-levels: Change _WARN_printf to pr_err when residency < 0
    Change _WARN_printf to pr_err as former prints stack and
    register dump in logs which is not necessary.
    
    Change-Id: Ib0323014017818668a4a634e463f751fd5e9d24a
    Signed-off-by: Raju P.L.S.S.S.N <rplsssn@codeaurora.org>

--- drivers/cpuidle/lpm-levels-of.c
+++ drivers/cpuidle/lpm-levels-of.c
@@ -748,7 +748,7 @@ static int calculate_residency(struct power_params *base_pwr,
 	residency /= (int32_t)(base_pwr->ss_power  - next_pwr->ss_power);
 
 	if (residency < 0) {
-		__WARN_printf("%s: Incorrect power attributes for LPM\n",
+		pr_err("%s: residency < 0 for LPM\n",
 				__func__);
 		return next_pwr->time_overhead_us;
 	}
