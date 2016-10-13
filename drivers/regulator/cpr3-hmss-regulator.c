2e538b63eccb653dd4940119956c5d4e0e6fd423
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2016-10-13 12:49:23 -0700

    regulator: cpr3-hmss: Increase fuse combo count
    To accommodate speed bin 2 and fuse revisions associated with it
    increase the fuse combo count from 16 to 24. At present, this is
    needed for msm8996proAU SOC.
    
    Change-Id: I8555162eab2c6bfead2e0762f28525e0aa56cf10
    Signed-off-by: Subbaraman Narayanamurthy <subbaram@codeaurora.org>

--- drivers/regulator/cpr3-hmss-regulator.c
+++ drivers/regulator/cpr3-hmss-regulator.c
@@ -87,8 +87,9 @@ struct cpr3_msm8996_hmss_fuses {
 /*
  * Fuse combos 0 -  7 map to CPR fusing revision 0 - 7 with speed bin fuse = 0.
  * Fuse combos 8 - 15 map to CPR fusing revision 0 - 7 with speed bin fuse = 1.
+ * Fuse combos 16 - 23 map to CPR fusing revision 0 - 7 with speed bin fuse = 2.
  */
-#define CPR3_MSM8996_HMSS_FUSE_COMBO_COUNT	16
+#define CPR3_MSM8996_HMSS_FUSE_COMBO_COUNT	24
 
 /*
  * Constants which define the name of each fuse corner.  Note that no actual
