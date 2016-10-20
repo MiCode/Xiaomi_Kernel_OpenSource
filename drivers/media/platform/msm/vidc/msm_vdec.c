ec359c2bb375d7850fe5903e2c6cedcfc2569a91
Vikash Garodia <vgarodia@codeaurora.org>
2016-10-20 18:39:37 +0530

    msm: vdec: Avoid setting incorrect color format for DPB buffers
    When decoder output buffers get split due to color
    format, the color format for DPB buffers was wrongly
    set. Color format for OPB buffers was set for DPB as
    well. This led to color distortion while decoding
    cetain contents in split mode.
    The code change avoids overwriting the improper color
    format for DPB buffers which was earlier set correctly
    while deciding the split mode.
    
    Change-Id: I71924f370c92c7875706c350f74c7859852c4809
    Signed-off-by: Vikash Garodia <vgarodia@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vdec.c
+++ drivers/media/platform/msm/vidc/msm_vdec.c
@@ -1262,8 +1262,6 @@ int msm_vdec_s_fmt(struct msm_vidc_inst *inst, struct v4l2_format *f)
 
 		inst->prop.width[CAPTURE_PORT] = f->fmt.pix_mp.width;
 		inst->prop.height[CAPTURE_PORT] = f->fmt.pix_mp.height;
-		msm_comm_set_color_format(inst, HAL_BUFFER_OUTPUT,
-			f->fmt.pix_mp.pixelformat);
 
 		inst->fmts[fmt->type] = fmt;
 		if (msm_comm_get_stream_output_mode(inst) ==
@@ -1279,6 +1277,9 @@ int msm_vdec_s_fmt(struct msm_vidc_inst *inst, struct v4l2_format *f)
 				frame_sz.height);
 			ret = msm_comm_try_set_prop(inst,
 				HAL_PARAM_FRAME_SIZE, &frame_sz);
+		} else {
+			msm_comm_set_color_format(inst, HAL_BUFFER_OUTPUT,
+				f->fmt.pix_mp.pixelformat);
 		}
 
 		f->fmt.pix_mp.plane_fmt[0].sizeimage =
