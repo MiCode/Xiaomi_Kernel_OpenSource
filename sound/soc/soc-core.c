507e38e06a66d72caa4f0751444f0103dff159d6
Ravi Kumar Siddojigari <rsiddoji@codeaurora.org>
2016-10-20 16:26:05 +0530

    ASoC: Add args validation in  snd_soc_read/write functions
    validation of the codec variable passed to the snd_soc_read
    and snd_soc_write functions are missing and can lead to DoS
    as referred in CVE-2016-6690
    
    Bug:28838221
    
    Change-Id: I5020f77e252bade5e97efb592afb71fe1b18d952
    Signed-off-by: Ravi Kumar Siddojigari <rsiddoji@codeaurora.org>
    Signed-off-by: Srinivasa Rao Kuppala <srkupp@codeaurora.org>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -2103,10 +2103,13 @@ unsigned int snd_soc_read(struct snd_soc_codec *codec, unsigned int reg)
 {
 	unsigned int ret;
 
-	ret = codec->read(codec, reg);
-	dev_dbg(codec->dev, "read %x => %x\n", reg, ret);
-	trace_snd_soc_reg_read(codec, reg, ret);
-
+	if (codec->read) {
+		ret = codec->read(codec, reg);
+		dev_dbg(codec->dev, "read %x => %x\n", reg, ret);
+		trace_snd_soc_reg_read(codec, reg, ret);
+	} else {
+		ret = -EIO;
+	}
 	return ret;
 }
 EXPORT_SYMBOL_GPL(snd_soc_read);
@@ -2114,9 +2117,13 @@ EXPORT_SYMBOL_GPL(snd_soc_read);
 unsigned int snd_soc_write(struct snd_soc_codec *codec,
 			   unsigned int reg, unsigned int val)
 {
-	dev_dbg(codec->dev, "write %x = %x\n", reg, val);
-	trace_snd_soc_reg_write(codec, reg, val);
-	return codec->write(codec, reg, val);
+	if (codec->write) {
+		dev_dbg(codec->dev, "write %x = %x\n", reg, val);
+		trace_snd_soc_reg_write(codec, reg, val);
+		return codec->write(codec, reg, val);
+	} else {
+		return -EIO;
+	}
 }
 EXPORT_SYMBOL_GPL(snd_soc_write);
 
