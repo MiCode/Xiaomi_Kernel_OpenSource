50696377240d990e81f6400c1c9bfe538c6e2b30
Hareesh Gundu <hareeshg@codeaurora.org>
2016-10-21 16:44:05 +0530

    PM/devfreq: Fix uninitialized use of stat variable
    Initialize stat variable to zero to avoid uninitialized use of
    stat.private_data in the devfreq_vbif_ev_handler().
    
    Change-Id: Ie34fad548214f287b67a8413c8658ebac65a95d3
    Signed-off-by: Hareesh Gundu <hareeshg@codeaurora.org>

--- drivers/devfreq/governor_bw_vbif.c
+++ drivers/devfreq/governor_bw_vbif.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014, 2016 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -73,6 +73,8 @@ static int devfreq_vbif_ev_handler(struct devfreq *devfreq,
 {
 	int ret;
 	struct devfreq_dev_status stat;
+
+	memset(&stat, 0, sizeof(stat));
 
 	switch (event) {
 	case DEVFREQ_GOV_START:
