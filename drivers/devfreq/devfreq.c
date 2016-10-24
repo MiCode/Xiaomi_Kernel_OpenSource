502361d626117a13686175b3598c2125ac8188c4
Saravana Kannan <skannan@codeaurora.org>
2016-10-24 16:35:05 -0700

    PM / devfreq: Restart previous governor if new governor fails to start
    If the new governor fails to start, switch back to old governor so that the
    devfreq state is not left in some weird limbo.
    
    Change-Id: I7cf1e6ceb63d27ce08b2d17b97a9844d257464ce
    Signed-off-by: Saravana Kannan <skannan@codeaurora.org>
    [use strlcpy instead]
    Signed-off-by: Jonathan Avila <avilaj@codeaurora.org>

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -920,7 +920,7 @@ static ssize_t governor_store(struct device *dev, struct device_attribute *attr,
 	struct devfreq *df = to_devfreq(dev);
 	int ret;
 	char str_governor[DEVFREQ_NAME_LEN + 1];
-	struct devfreq_governor *governor;
+	const struct devfreq_governor *governor, *prev_gov;
 
 	ret = sscanf(buf, "%" __stringify(DEVFREQ_NAME_LEN) "s", str_governor);
 	if (ret != 1)
@@ -948,12 +948,21 @@ static ssize_t governor_store(struct device *dev, struct device_attribute *attr,
 			goto out;
 		}
 	}
+	prev_gov = df->governor;
 	df->governor = governor;
-	strncpy(df->governor_name, governor->name, DEVFREQ_NAME_LEN);
+	strlcpy(df->governor_name, governor->name, DEVFREQ_NAME_LEN);
 	ret = df->governor->event_handler(df, DEVFREQ_GOV_START, NULL);
-	if (ret)
+	if (ret) {
 		dev_warn(dev, "%s: Governor %s not started(%d)\n",
 			 __func__, df->governor->name, ret);
+		if (prev_gov) {
+			df->governor = prev_gov;
+			strlcpy(df->governor_name, prev_gov->name,
+				DEVFREQ_NAME_LEN);
+			df->governor->event_handler(df, DEVFREQ_GOV_START,
+						    NULL);
+		}
+	}
 out:
 	mutex_unlock(&devfreq_list_lock);
 
