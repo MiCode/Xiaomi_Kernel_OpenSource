a0232647df04b1265477ef1e42f2b03139f00506
Santhosh Punugu <spunug@codeaurora.org>
2016-10-27 15:19:57 +0530

    msm: kgsl: add egl_surface/egl_image usage count in debugfs
    Add more information to the debugfs kgsl/proc/<pid>/mem which
    will allow memtrack to correctly assign allocated ion buffer
    memory to a process. The additional columns show the number of
    kgsl_mem_entries which have a usage of egl_image (or) egl_surface.
    
    When attaching a dma_buf to kgsl, use the dma_buf_attachment's
    (void*)priv to point back to the kgsl_mem_entry. This makes it
    possible to iterate through all attachments on a dma_buf and
    gather statistics from each kgsl_mem_entry associated with the
    dma_buf.
    
    CRs-Fixed: 1073673
    Change-Id: I1ef3bd0da3f74fa41074021699b2226c48bde9c3
    Signed-off-by: Santhosh Punugu <spunug@codeaurora.org>

--- drivers/gpu/msm/kgsl_debugfs.c
+++ drivers/gpu/msm/kgsl_debugfs.c
@@ -132,6 +132,8 @@ static int print_mem_entry(void *data, void *ptr)
 	char flags[9];
 	char usage[16];
 	struct kgsl_memdesc *m = &entry->memdesc;
+	unsigned int usermem_type = kgsl_memdesc_usermem_type(m);
+	int egl_surface_count = 0, egl_image_count = 0;
 
 	flags[0] = kgsl_memdesc_is_global(m) ?  'g' : '-';
 	flags[1] = '-';
@@ -145,12 +147,17 @@ static int print_mem_entry(void *data, void *ptr)
 
 	kgsl_get_memory_usage(usage, sizeof(usage), m->flags);
 
-	seq_printf(s, "%pK %pK %16llu %5d %9s %10s %16s %5d %16llu",
+	if (usermem_type == KGSL_MEM_ENTRY_ION)
+		kgsl_get_egl_counts(entry, &egl_surface_count,
+						&egl_image_count);
+
+	seq_printf(s, "%pK %pK %16llu %5d %9s %10s %16s %5d %16llu %6d %6d",
 			(uint64_t *)(uintptr_t) m->gpuaddr,
 			(unsigned long *) m->useraddr,
 			m->size, entry->id, flags,
-			memtype_str(kgsl_memdesc_usermem_type(m)),
-			usage, (m->sgt ? m->sgt->nents : 0), m->mapsize);
+			memtype_str(usermem_type),
+			usage, (m->sgt ? m->sgt->nents : 0), m->mapsize,
+			egl_surface_count, egl_image_count);
 
 	if (entry->metadata[0] != 0)
 		seq_printf(s, " %s", entry->metadata);
@@ -217,9 +224,9 @@ static void *process_mem_seq_next(struct seq_file *s, void *ptr,
 static int process_mem_seq_show(struct seq_file *s, void *ptr)
 {
 	if (ptr == SEQ_START_TOKEN) {
-		seq_printf(s, "%16s %16s %16s %5s %9s %10s %16s %5s %16s\n",
-				"gpuaddr", "useraddr", "size", "id", "flags",
-				"type", "usage", "sglen", "mapsize");
+		seq_printf(s, "%16s %16s %16s %5s %9s %10s %16s %5s %16s %6s %6s\n",
+			"gpuaddr", "useraddr", "size", "id", "flags", "type",
+			"usage", "sglen", "mapsize", "eglsrf", "eglimg");
 		return 0;
 	} else
 		return print_mem_entry(s, ptr);
