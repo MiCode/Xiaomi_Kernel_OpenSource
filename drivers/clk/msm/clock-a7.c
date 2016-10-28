13827669d0cbcc7412d1375534eb5a2eb3f83f91
Vicky Wallace <vwallace@codeaurora.org>
2016-10-28 12:05:18 -0700

    clk: msm: clock: Add support for sdxhedgehog clocks
    Add CPU and peripheral clocks support on SDX HEDGEHOG.
    
    sdxhedgehog CPU and GCC Peripheral clock driver is shared
    with Californium. Update frequency table to support clock
    frequencies that are specific to sdxhedgehog. CPU clock on
    sdxhedgehog is required to run at 1.4 GHz.
    Configure CPU clock to run from Fabia PLL.
    
    Change-Id: I4efe77e3ae350aef287865a0296d13e2d46769a5
    Signed-off-by: Vicky Wallace <vwallace@codeaurora.org>

--- drivers/clk/msm/clock-a7.c
+++ drivers/clk/msm/clock-a7.c
@@ -476,6 +476,7 @@ static struct of_device_id clock_a7_match_table[] = {
 	{.compatible = "qcom,clock-a53-8916"},
 	{.compatible = "qcom,clock-a7-californium"},
 	{.compatible = "qcom,clock-a7-mdm9607"},
+	{.compatible = "qcom,clock-a7-sdxhedgehog"},
 	{}
 };
 
