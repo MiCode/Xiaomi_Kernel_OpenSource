a3b6427475da33b01556c4908b67f63b6413c35a
Amit Pundir <amit.pundir@linaro.org>
2016-10-29 15:04:05 +0800

    Revert "cgroup: refactor allow_attach function into common code"
    This reverts commit 16474253cb671233c662ad89a88fee802dc36e15.
    
    Part of Chromium commit series:
    
    CHROMIUM: remove Android's cgroup generic permissions checks
    
    The implementation is utterly broken, resulting in all processes being
    allows to move tasks between sets (as long as they have access to the
    "tasks" attribute), and upstream is heading towards checking only
    capability anyway, so let's get rid of this code.
    
    BUG=b:31790445,chromium:647994
    TEST=Boot android container, examine logcat
    
    Signed-off-by: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/394967
    Reviewed-by: Ricky Zhou <rickyz@chromium.org>
    Reviewed-by: John Stultz <john.stultz@linaro.org>
    
    Change-Id: I7d64a291ae1347281632b7cb82110f858fefeace
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2327,25 +2327,6 @@ static int cgroup_attach_task(struct cgroup *dst_cgrp,
 	return ret;
 }
 
-int subsys_cgroup_allow_attach(struct cgroup_subsys_state *css, struct cgroup_taskset *tset)
-{
-	const struct cred *cred = current_cred(), *tcred;
-	struct task_struct *task;
-
-	if (capable(CAP_SYS_NICE))
-		return 0;
-
-	cgroup_taskset_for_each(task, tset) {
-		tcred = __task_cred(task);
-
-		if (current != task && !uid_eq(cred->euid, tcred->uid) &&
-		    !uid_eq(cred->euid, tcred->suid))
-			return -EACCES;
-	}
-
-	return 0;
-}
-
 /*
  * Find the task_struct of the task to attach by vpid and pass it along to the
  * function to attach either it or all tasks in its threadgroup. Will lock
