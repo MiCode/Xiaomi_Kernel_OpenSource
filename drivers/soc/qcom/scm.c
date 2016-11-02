b2555b966b5d1fe5417e16bbfc230d2c6dec96c7
Swetha Chikkaboraiah <schikk@codeaurora.org>
2016-11-02 16:49:41 +0530

    qcom: scm: remove printing input arguments
    scm_call2 is printing the input arguments if TZ ret value is  < 0
    leading to information leak. Remove printing input arguments.
    
    Change-Id: I21dd6d83fa979aed2c79ebb2c9c8de63a247dded
    CRs-Fixed: 1076407
    Signed-off-by: Swetha Chikkaboraiah <schikk@codeaurora.org>
    Signed-off-by: Paresh Purabhiya <ppurab@codeaurora.org>

--- drivers/soc/qcom/scm.c
+++ drivers/soc/qcom/scm.c
@@ -662,10 +662,6 @@ int scm_call2(u32 fn_id, struct scm_desc *desc)
 
 		desc->ret[0] = desc->ret[1] = desc->ret[2] = 0;
 
-		pr_debug("scm_call: func id %#llx, args: %#x, %#llx, %#llx, %#llx, %#llx\n",
-			x0, desc->arginfo, desc->args[0], desc->args[1],
-			desc->args[2], desc->x5);
-
 		trace_scm_call_start(x0, desc);
 
 		if (scm_version == SCM_ARMV8_64)
@@ -695,10 +691,8 @@ int scm_call2(u32 fn_id, struct scm_desc *desc)
 	}  while (ret == SCM_V2_EBUSY && (retry_count++ < SCM_EBUSY_MAX_RETRY));
 
 	if (ret < 0)
-		pr_err("scm_call failed: func id %#llx, arginfo: %#x, args: %#llx, %#llx, %#llx, %#llx, ret: %d, syscall returns: %#llx, %#llx, %#llx\n",
-			x0, desc->arginfo, desc->args[0], desc->args[1],
-			desc->args[2], desc->x5, ret, desc->ret[0],
-			desc->ret[1], desc->ret[2]);
+		pr_err("scm_call failed: func id %#llx, ret: %d, syscall returns: %#llx, %#llx, %#llx\n",
+			x0, ret, desc->ret[0], desc->ret[1], desc->ret[2]);
 
 	if (arglen > N_REGISTER_ARGS)
 		kfree(desc->extra_arg_buf);
