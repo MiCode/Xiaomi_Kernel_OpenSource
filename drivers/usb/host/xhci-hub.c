407a2bff26cf78f328a21f77f048f60b27965735
Hemant Kumar <hemantk@codeaurora.org>
2016-11-08 15:46:44 -0800

    usb: host: xhci: Replace msleep with usleep_range
    Since usleep_range provides better accuracy in
    comparison to msleep. This helps in reducing the
    latency of host bus resume.
    
    Change-Id: Id22104b9e5b63153731df9eb55759de9a86128c6
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>

--- drivers/usb/host/xhci-hub.c
+++ drivers/usb/host/xhci-hub.c
@@ -1356,7 +1356,7 @@ int xhci_hub_control(struct usb_hcd *hcd, u16 typeReq, u16 wValue,
 				xhci_set_link_state(xhci, port_array, wIndex,
 							XDEV_RESUME);
 				spin_unlock_irqrestore(&xhci->lock, flags);
-				msleep(USB_RESUME_TIMEOUT);
+				usleep_range(21000, 21500);
 				spin_lock_irqsave(&xhci->lock, flags);
 				xhci_set_link_state(xhci, port_array, wIndex,
 							XDEV_U0);
