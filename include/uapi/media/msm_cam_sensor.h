4a95337a918678cb52bbbf3bcc38e8c6bd7be6ad
Rajesh Bondugula <rajeshb@codeaurora.org>
2016-11-15 13:52:49 -0800

    msm: camera: sensor: Validate eeprom_name string length
    Validate eeprom_name string length before copying into
    the userspace buffer.
    If more data than required is copied, userspace has the access to
    some of kernel data which is not intended.
    
    This change will fix the issue.
    
    CRs-Fixed: 1090007
    Signed-off-by: Rajesh Bondugula <rajeshb@codeaurora.org>
    Change-Id: Id40a287e0b1a93cc15d9b02c757fe9f347e285f2

--- include/uapi/media/msm_cam_sensor.h
+++ include/uapi/media/msm_cam_sensor.h
@@ -302,7 +302,7 @@ struct msm_eeprom_cfg_data {
 	enum eeprom_cfg_type_t cfgtype;
 	uint8_t is_supported;
 	union {
-		char eeprom_name[MAX_SENSOR_NAME];
+		char eeprom_name[MAX_EEPROM_NAME];
 		struct eeprom_get_t get_data;
 		struct eeprom_read_t read_data;
 		struct eeprom_write_t write_data;
