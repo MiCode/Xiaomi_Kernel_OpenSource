afacd8f46b0137357499b2a017630341c5108f86
Meng Wang <mwang@codeaurora.org>
2016-11-28 17:25:20 +0800

    include: clock: remove ifdef from header file
    As audio-ext-clk.h is finally included in device tree and
    and using ifdef results in compilation failure. Delete
    ifdef from audio-ext-clk.h.
    
    CRs-Fixed: 1090500
    Change-Id: Ib6f715c3f606770e7e0b1f0f84ab50e442398cd0
    Signed-off-by: Meng Wang <mwang@codeaurora.org>

--- include/dt-bindings/clock/qcom,audio-ext-clk.h
+++ include/dt-bindings/clock/qcom,audio-ext-clk.h
@@ -14,20 +14,18 @@
 #define __AUDIO_EXT_CLK_H
 
 /* Audio External Clocks */
-#ifdef CONFIG_COMMON_CLK_QCOM
 #define AUDIO_PMI_CLK		0
 #define AUDIO_PMIC_LNBB_CLK	1
 #define AUDIO_AP_CLK		2
 #define AUDIO_AP_CLK2		3
 #define AUDIO_LPASS_MCLK	4
 #define AUDIO_LPASS_MCLK2	5
-#else
+
 #define clk_audio_ap_clk        0x9b5727cb
 #define clk_audio_pmi_clk       0xcbfe416d
 #define clk_audio_ap_clk2       0x454d1e91
 #define clk_audio_lpass_mclk    0xf0f2a284
 #define clk_audio_pmi_lnbb_clk   0x57312343
-#endif
 
 #define clk_div_clk1            0xaa1157a6
 #define clk_div_clk1_ao         0x6b943d68
