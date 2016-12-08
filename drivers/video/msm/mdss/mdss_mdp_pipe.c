41aa72552c6c693391950df3e83c012f47536ea3
Ramakant Singh <ramaka@codeaurora.org>
2016-12-08 20:04:20 +0530

    msm: mdss: fix to handle multi-rect configuration in error cases
    In case of an error when configuring a pipe during kickoff,
    driver unstages from HW the pipe that had the error, so it
    makes sure this dirty pipe is not used by HW.
    This is a problem if the pipe is configured in multi-rect
    mode and we only fail one of the two multi-rect pipes since
    in this case HW is expecting both multi-rect pipes that
    represent the two HW rectangles and we are only configuring
    one due the error that happened.
    Fix this problem by making sure that if the configuration of
    one multi-rect fail, we unstage both multi-rect pipes.
    
    Change-Id: I78e7f89c19ee018eadba22a2ad71cf007c0874fb
    Signed-off-by: Ramakant Singh <ramaka@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_pipe.c
+++ drivers/video/msm/mdss/mdss_mdp_pipe.c
@@ -2664,20 +2664,22 @@ int mdss_mdp_pipe_queue_data(struct mdss_mdp_pipe *pipe,
 		pipe->params_changed = 0;
 		mdss_mdp_pipe_solidfill_setup(pipe);
 
-		MDSS_XLOG(pipe->num, pipe->mixer_left->num, pipe->play_cnt,
-			0x111);
+		MDSS_XLOG(pipe->num, pipe->multirect.num,
+			pipe->mixer_left->num, pipe->play_cnt, 0x111);
 
 		goto update_nobuf;
 	}
 
-	MDSS_XLOG(pipe->num, pipe->mixer_left->num, pipe->play_cnt, 0x222);
+	MDSS_XLOG(pipe->num, pipe->multirect.num, pipe->mixer_left->num,
+						pipe->play_cnt, 0x222);
 
 	if (params_changed) {
 		pipe->params_changed = 0;
 
 		ret = mdss_mdp_pipe_pp_setup(pipe, &opmode);
 		if (ret) {
-			pr_err("pipe pp setup error for pnum=%d\n", pipe->num);
+			pr_err("pipe pp setup error for pnum=%d rect=%d\n",
+					pipe->num, pipe->multirect.num);
 			goto done;
 		}
 
