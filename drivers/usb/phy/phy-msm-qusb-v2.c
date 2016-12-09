47b27b4d0f61f4ec4b3f608683cca1074eb2d211
Hemant Kumar <hemantk@codeaurora.org>
2016-12-08 20:30:52 -0800

    usb: qusb2: De-assert TCSR_QUSB2PHY_CLAMP_DIG_N_1P8 upon boot up
    This prevents leakage on 1p8 power rail upon boot up when usb cable
    is not connect.
    
    Change-Id: I28a4f495293863361843eb30b2d20f1f57889f95
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- drivers/usb/phy/phy-msm-qusb-v2.c
+++ drivers/usb/phy/phy-msm-qusb-v2.c
@@ -1003,6 +1003,9 @@ static int qusb_phy_probe(struct platform_device *pdev)
 	if (ret)
 		usb_remove_phy(&qphy->phy);
 
+	/* de-asseert clamp dig n to reduce leakage on 1p8 upon boot up */
+	writel_relaxed(0x0, qphy->tcsr_clamp_dig_n);
+
 	return ret;
 }
 
