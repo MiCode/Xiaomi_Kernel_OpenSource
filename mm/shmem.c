1dc6839336341d4091a3bc33379c32cdd4fc1139
Arnd Bergmann <arnd@arndb.de>
2016-12-12 16:42:28 -0800

    shmem: avoid maybe-uninitialized warning
    commit 23f919d4ad0eb325595f10f55be4301b2965d6d6 upstream.
    
    After enabling -Wmaybe-uninitialized warnings, we get a false-postive
    warning for shmem:
    
      mm/shmem.c: In function `shmem_getpage_gfp':
      include/linux/spinlock.h:332:21: error: `info' may be used uninitialized in this function [-Werror=maybe-uninitialized]
    
    This can be easily avoided, since the correct 'info' pointer is known at
    the time we first enter the function, so we can simply move the
    initialization up.  Moving it before the first label avoids the warning
    and lets us remove two later initializations.
    
    Note that the function is so hard to read that it not only confuses the
    compiler, but also most readers and without this patch it could\ easily
    break if one of the 'goto's changed.
    
    Link: https://www.spinics.net/lists/kernel/msg2368133.html
    Link: http://lkml.kernel.org/r/20161024205725.786455-1-arnd@arndb.de
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Acked-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Andreas Gruenbacher <agruenba@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/shmem.c
+++ mm/shmem.c
@@ -1550,7 +1550,7 @@ static int shmem_getpage_gfp(struct inode *inode, pgoff_t index,
 	struct mm_struct *fault_mm, int *fault_type)
 {
 	struct address_space *mapping = inode->i_mapping;
-	struct shmem_inode_info *info;
+	struct shmem_inode_info *info = SHMEM_I(inode);
 	struct shmem_sb_info *sbinfo;
 	struct mm_struct *charge_mm;
 	struct mem_cgroup *memcg;
@@ -1600,7 +1600,6 @@ repeat:
 	 * Fast cache lookup did not find it:
 	 * bring it back from swap or allocate.
 	 */
-	info = SHMEM_I(inode);
 	sbinfo = SHMEM_SB(inode->i_sb);
 	charge_mm = fault_mm ? : current->mm;
 
@@ -1852,7 +1851,6 @@ unlock:
 		put_page(page);
 	}
 	if (error == -ENOSPC && !once++) {
-		info = SHMEM_I(inode);
 		spin_lock_irq(&info->lock);
 		shmem_recalc_inode(inode);
 		spin_unlock_irq(&info->lock);
