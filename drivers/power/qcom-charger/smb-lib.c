fe4987d5446841de4dd9a24803e5d467248f7f74
Vic Wei <vwei@codeaurora.org>
2016-12-14 18:47:49 -0800

    smb-lib : Fix incorrect VALID_INPUT_POWER_SOURCE_BIT check
    In smblib_get_prop_usb_online() and smblib_get_prop_dc_online(),
    used incorrect VALID_INPUT_POWER_SOURCE_BIT check. Now change
    to VALID_INPUT_POWER_SOURCE_STS_BIT check.
    
    Change-Id: I70489775b2a99afff8b26366758834a98b1ffb30
    Signed-off-by: Vic Wei <vwei@codeaurora.org>
    CRs-Fixed: 1102132

--- drivers/power/qcom-charger/smb-lib.c
+++ drivers/power/qcom-charger/smb-lib.c
@@ -1498,7 +1498,7 @@ int smblib_get_prop_dc_online(struct smb_charger *chg,
 		   stat);
 
 	val->intval = (stat & USE_DCIN_BIT) &&
-		      (stat & VALID_INPUT_POWER_SOURCE_BIT);
+		      (stat & VALID_INPUT_POWER_SOURCE_STS_BIT);
 
 	return rc;
 }
@@ -1564,7 +1564,7 @@ int smblib_get_prop_usb_online(struct smb_charger *chg,
 		   stat);
 
 	val->intval = (stat & USE_USBIN_BIT) &&
-		      (stat & VALID_INPUT_POWER_SOURCE_BIT);
+		      (stat & VALID_INPUT_POWER_SOURCE_STS_BIT);
 	return rc;
 }
 
