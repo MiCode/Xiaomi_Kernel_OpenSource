613dd4f8dda86a6b73e437f9d0d791eb78fe70a8
Junzhe Zou <jnzhezou@codeaurora.org>
2016-12-15 17:20:28 -0800

    msm: isp: fix bug in cfg_camif
    Correct the bug to apply subsample_period to camif register
    
    CRs-Fixed: 1109302
    Change-Id: I7875a8de1e9dd3630c077c466181115cf51c9b83
    Signed-off-by: Junzhe Zou <jnzhezou@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp47.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp47.c
@@ -1390,7 +1390,7 @@ void msm_vfe47_cfg_camif(struct vfe_device *vfe_dev,
 	if (subsample_period && subsample_pattern) {
 		val = msm_camera_io_r(vfe_dev->vfe_base + 0x494);
 		val &= 0xFFFFE0FF;
-		val = (subsample_period - 1) << 8;
+		val |= (subsample_period - 1) << 8;
 		msm_camera_io_w(val, vfe_dev->vfe_base + 0x494);
 		ISP_DBG("%s:camif PERIOD %x PATTERN %x\n",
 			__func__,  subsample_period, subsample_pattern);
