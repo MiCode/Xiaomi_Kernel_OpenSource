dbb06ec203c36b69e903dbe3a1f3a6db23a2dbcf
Patrick Daly <pdaly@codeaurora.org>
2016-12-20 15:28:26 -0800

    soc: qcom: secure_buffer: Fix memory leak
    Free the sg_table properly.
    
    Change-Id: Idae945d17803e10756bf3a20426e57837d185067
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

--- drivers/soc/qcom/secure_buffer.c
+++ drivers/soc/qcom/secure_buffer.c
@@ -365,28 +365,19 @@ int hyp_assign_phys(phys_addr_t addr, u64 size, u32 *source_vm_list,
 			int source_nelems, int *dest_vmids,
 			int *dest_perms, int dest_nelems)
 {
-	struct sg_table *table;
+	struct sg_table table;
 	int ret;
 
-	table = kzalloc(sizeof(struct sg_table), GFP_KERNEL);
-	if (!table)
-		return -ENOMEM;
-	ret = sg_alloc_table(table, 1, GFP_KERNEL);
+	ret = sg_alloc_table(&table, 1, GFP_KERNEL);
 	if (ret)
-		goto err1;
+		return ret;
 
-	sg_set_page(table->sgl, phys_to_page(addr), size, 0);
+	sg_set_page(table.sgl, phys_to_page(addr), size, 0);
 
-	ret = hyp_assign_table(table, source_vm_list, source_nelems, dest_vmids,
-						dest_perms, dest_nelems);
-	if (ret)
-		goto err2;
+	ret = hyp_assign_table(&table, source_vm_list, source_nelems,
+			       dest_vmids, dest_perms, dest_nelems);
 
-	return ret;
-err2:
-	sg_free_table(table);
-err1:
-	kfree(table);
+	sg_free_table(&table);
 	return ret;
 }
 
