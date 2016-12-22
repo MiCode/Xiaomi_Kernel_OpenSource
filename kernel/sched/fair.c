c9507ba3b4845216b3203fef98105f596c2e540f
Leo Yan <leo.yan@linaro.org>
2016-12-22 07:58:00 -0800

    sched/fair: fix for group_smaller_cpu_capacity()
    Function group_smaller_cpu_capacity() checks if one schedule group has
    smaller capacity than another one:
    
    return sg->sgc->max_capacity + capacity_margin - SCHED_LOAD_SCALE <
    					ref->sgc->max_capacity;
    
    The value (capacity_margin - SCHED_LOAD_SCALE) is an absolute value for
    difference checking, so it's easily broken if two scheduler groups have
    no much difference (like CA53.Fast+CA53.Slow system).
    
    When this function is invalid the schedule group with misfit tasks
    will be wrongly cleared flag so misfit task has no chance to migrate to
    higher capacity CPU.
    
    This patch is to directly check schedule group maximum_capacity and has
    a minor fix for maximum_capacity assignment with original capacity.
    
    Change-Id: I8071f2ee90e0823c3232018bdbf27e759fba0ec1
    Signed-off-by: Leo Yan <leo.yan@linaro.org>
    Patch-mainline: eas-dev @ 12/22/16, 15:58
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8048,6 +8048,8 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 	raw_spin_unlock_irqrestore(&mcc->lock, flags);
 
 skip_unlock: __attribute__ ((unused));
+	sdg->sgc->max_capacity = capacity;
+
 	capacity *= scale_rt_capacity(cpu);
 	capacity >>= SCHED_CAPACITY_SHIFT;
 
@@ -8056,7 +8058,6 @@ skip_unlock: __attribute__ ((unused));
 
 	cpu_rq(cpu)->cpu_capacity = capacity;
 	sdg->sgc->capacity = capacity;
-	sdg->sgc->max_capacity = capacity;
 }
 
 void update_group_capacity(struct sched_domain *sd, int cpu)
@@ -8234,8 +8235,7 @@ group_is_overloaded(struct lb_env *env, struct sg_lb_stats *sgs)
 static inline bool
 group_smaller_cpu_capacity(struct sched_group *sg, struct sched_group *ref)
 {
-	return sg->sgc->max_capacity + capacity_margin - SCHED_CAPACITY_SHIFT <
-							ref->sgc->max_capacity;
+	return sg->sgc->max_capacity < ref->sgc->max_capacity;
 }
 
 static inline enum
