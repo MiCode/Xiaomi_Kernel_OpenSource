0394809b8d1bcbf3c4c12d1c7e844f9cbca08299
Jungseung Lee <js07.lee@samsung.com>
2016-12-22 12:37:34 +0900

    BACKPORT: mmc: core: Export device lifetime information through sysfs
    In the eMMC 5.0 version of the spec, several EXT_CSD fields about
    device lifetime are added.
    
     - Two types of estimated indications reflected by averaged wear out of memory
     - An indication reflected by average reserved blocks
    
    Export the information through sysfs.
    
    Signed-off-by: Jungseung Lee <js07.lee@samsung.com>
    Reviewed-by: Jaehoon Chung <jh80.chung@samsung.com>
    Reviewed-by: Shawn Lin <shawn.lin@rock-chips.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- include/linux/mmc/mmc.h
+++ include/linux/mmc/mmc.h
@@ -332,6 +332,9 @@ struct _mmc_csd {
 #define EXT_CSD_GENERIC_CMD6_TIME	248	/* RO */
 #define EXT_CSD_CACHE_SIZE		249	/* RO, 4 bytes */
 #define EXT_CSD_PWR_CL_DDR_200_360	253	/* RO */
+#define EXT_CSD_PRE_EOL_INFO		267	/* RO */
+#define EXT_CSD_DEVICE_LIFE_TIME_EST_TYP_A	268	/* RO */
+#define EXT_CSD_DEVICE_LIFE_TIME_EST_TYP_B	269	/* RO */
 #define EXT_CSD_TAG_UNIT_SIZE		498	/* RO */
 #define EXT_CSD_DATA_TAG_SUPPORT	499	/* RO */
 #define EXT_CSD_MAX_PACKED_WRITES	500	/* RO */
