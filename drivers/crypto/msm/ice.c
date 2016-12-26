36a2a7619f172cd7ead299ec4ca07ad18556795d
Andrey Markovytch <andreym@codeaurora.org>
2016-12-26 09:14:47 +0200

    ice: fix security issue with validating error in pointer
    get_ice_device_from_storage_type can return error pointer which is
    not NULL in case of error, this was not checked, changes fix this.
    
    Change-Id: I7dd8a068454a7bd250189ff9467c685af449f81b
    Signed-off-by: Andrey Markovytch <andreym@codeaurora.org>

--- drivers/crypto/msm/ice.c
+++ drivers/crypto/msm/ice.c
@@ -1662,6 +1662,9 @@ int qcom_ice_setup_ice_hw(const char *storage_type, int enable)
 	struct ice_device *ice_dev = NULL;
 
 	ice_dev = get_ice_device_from_storage_type(storage_type);
+	if (ice_dev == ERR_PTR(-EPROBE_DEFER))
+		return -EPROBE_DEFER;
+
 	if (!ice_dev)
 		return ret;
 
