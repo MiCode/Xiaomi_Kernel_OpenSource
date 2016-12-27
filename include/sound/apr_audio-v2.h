57d97f33333ea0c32d307e0c06c6cba6ed7d07e1
Arun Kumar Dasari <Arun@codeaurora.org>
2016-12-27 20:14:16 +0530

    ASoc: msm: qdsp6v2: add support for AAC LATM playback
    Add support for AAC LATM clips. Define macro for AAC LATM format
    and configure media format block with this value for AAC LATM clips.
    
    CRs-Fixed: 1108268
    Change-Id: I10cf2db9fc7e4d40d2bf910e162b3c2d95d8a03c
    Signed-off-by: Surendar karka <sukark@codeaurora.org>

--- include/sound/apr_audio-v2.h
+++ include/sound/apr_audio-v2.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2017, The Linux Foundation. All rights reserved.
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License version 2 and
@@ -3662,6 +3662,9 @@ struct asm_multi_channel_pcm_enc_cfg_v2 {
 
 /* Enumeration for the raw AAC format. */
 #define ASM_MEDIA_FMT_AAC_FORMAT_FLAG_RAW    3
+
+/* Enumeration for the AAC LATM format. */
+#define ASM_MEDIA_FMT_AAC_FORMAT_FLAG_LATM   4
 
 #define ASM_MEDIA_FMT_AAC_AOT_LC             2
 #define ASM_MEDIA_FMT_AAC_AOT_SBR            5
