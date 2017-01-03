aad0070487797863fb484474cbc895f9babdec91
Deepak Kumar <dkumar@codeaurora.org>
2017-01-03 21:49:03 +0530

    msm: kgsl: refcount irq to avoid racing against idle check
    Current irq handler clears the pending interrupt bits in interrupt
    status register before serving the interrupts. This leads to a race
    condition with the idle check which checks the interrupt status
    register to determine whether any interrupt is pending or not. As
    the interrupt status register is already cleared, idle check goes
    ahead and switch off the GPU clocks even when irq is yet to be served
    causing NOC errors.
    
    This change refcounts each irq handler call and uses this reference
    count to determine if any irq is still pending or not along with
    interrupt status register to avoid this race condition.
    
    Change-Id: I030d52c52055f836ea4c7519ce2d8db94a2a09a0
    Signed-off-by: Deepak Kumar <dkumar@codeaurora.org>

--- drivers/gpu/msm/adreno.h
+++ drivers/gpu/msm/adreno.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2008-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -346,6 +346,7 @@ struct adreno_gpu_core {
  * @ram_cycles_lo: Number of DDR clock cycles for the monitor session
  * @perfctr_pwr_lo: Number of cycles VBIF is stalled by DDR
  * @halt: Atomic variable to check whether the GPU is currently halted
+ * @pending_irq_refcnt: Atomic variable to keep track of running IRQ handlers
  * @ctx_d_debugfs: Context debugfs node
  * @pwrctrl_flag: Flag to hold adreno specific power attributes
  * @cmdbatch_profile_buffer: Memdesc holding the cmdbatch profiling buffer
@@ -403,6 +404,7 @@ struct adreno_device {
 	unsigned int starved_ram_lo;
 	unsigned int perfctr_pwr_lo;
 	atomic_t halt;
+	atomic_t pending_irq_refcnt;
 	struct dentry *ctx_d_debugfs;
 	unsigned long pwrctrl_flag;
 
