9b1e836e9484cccc615bca70fb5e14ae452fa545
Amir Levy <alevy@codeaurora.org>
2017-01-03 10:50:42 +0200

    msm: ipa3: separate IPAv3.5 mhi\non mhi configurations
    IPAv3.5 has different endpoints settings according to actual
    mode (MHI or non MHI). Create different configurations for
    MHI product and non MHI product. Info regarding current product
    will be acquired from IPA init script.
    
    CRs-Fixed: 1097871
    Change-Id: Ia08b113ee5e50f0e9029df48a317db9191e20ba4
    Signed-off-by: Amir Levy <alevy@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipa_i.h
+++ drivers/platform/msm/ipa/ipa_v3/ipa_i.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1230,6 +1230,7 @@ struct ipa3_context {
 	wait_queue_head_t msg_waitq;
 	enum ipa_hw_type ipa_hw_type;
 	enum ipa3_hw_mode ipa3_hw_mode;
+	bool ipa_config_is_mhi;
 	bool use_ipa_teth_bridge;
 	bool ipa_bam_remote_mode;
 	bool modem_cfg_emb_pipe_flt;
