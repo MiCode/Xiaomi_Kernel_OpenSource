c1dbed58b9621838b3442e79827a1f23ea7958c4
Amir Levy <alevy@codeaurora.org>
2017-01-05 10:19:13 +0200

    msm: ipa3: disable TX prefetch to support MBIM
    In order to enable MBIM aggregation TX prefetch needs to be disabled.
    Disable TX prefetch on boot when non MHI configuration is detected.
    
    CRs-Fixed: 1097871
    Change-Id: I4107f3e54293421b9b4fdbcb62a09528bb6b775c
    Acked-by: Ady Abraham <adya@qti.qualcomm.com>
    Signed-off-by: Skylar Chang <chiaweic@codeaurora.org>
    Signed-off-by: Amir Levy <alevy@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipa.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa.c
@@ -4010,6 +4010,13 @@ static int ipa3_post_init(const struct ipa3_plat_drv_res *resource_p,
 		IPADBG("Initialization of ipa interrupts skipped\n");
 	}
 
+	/*
+	 * IPAv3.5.x requires to disable prefetch for USB in order to allow
+	 * MBIM to work, currently MBIM is not needed in MHI mode.
+	 */
+	if (!ipa3_ctx->ipa_config_is_mhi)
+		ipa3_disable_prefetch(IPA_CLIENT_USB_CONS);
+
 	if (ipa3_ctx->transport_prototype == IPA_TRANSPORT_TYPE_GSI) {
 		memset(&gsi_props, 0, sizeof(gsi_props));
 		gsi_props.ver = ipa3_get_gsi_ver(resource_p->ipa_hw_type);
