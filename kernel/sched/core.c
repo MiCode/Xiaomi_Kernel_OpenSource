6d63f38bf2832a353cab15d9070fe8171b03bd22
Pavankumar Kondeti <pkondeti@codeaurora.org>
2017-01-07 14:59:26 +0530

    sched: kill sync_cpu maintenance
    We assume boot CPU as a sync CPU and initialize it's window_start
    to sched_ktime_clock(). As windows are synchronized across all
    CPUs, the secondary CPUs' window_start are initialized from the
    sync_cpu's window_start. A CPU's window_start is never reset, so
    this synchronization happens only once for a given CPU. Given this
    fact, there is no need to reassigning the sync_cpu role to another
    CPU when the boot CPU is going offline. Remove this unnecessary
    maintenance of sync_cpu and use any online CPU's window_start as
    reference.
    
    Change-Id: I169a8e80573c6dbcb1edeab0659c07c17102f4c9
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5905,7 +5905,6 @@ int sched_isolate_cpu(int cpu)
 	smp_call_function_any(&avail_cpus, hrtimer_quiesce_cpu, &cpu, 1);
 	smp_call_function_any(&avail_cpus, timer_quiesce_cpu, &cpu, 1);
 
-	migrate_sync_cpu(cpu, cpumask_first(&avail_cpus));
 	stop_cpus(cpumask_of(cpu), do_isolation_work_cpu_stop, 0);
 
 	calc_load_migrate(rq);
@@ -6286,7 +6285,6 @@ migration_call(struct notifier_block *nfb, unsigned long action, void *hcpu)
 		sched_ttwu_pending();
 		/* Update our root-domain */
 		raw_spin_lock_irqsave(&rq->lock, flags);
-		migrate_sync_cpu(cpu, smp_processor_id());
 
 		if (rq->rd) {
 			BUG_ON(!cpumask_test_cpu(cpu, rq->rd->span));
