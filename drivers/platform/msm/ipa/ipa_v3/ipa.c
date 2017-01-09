62f3b72a621a2aacecdc8bb159ac54214002eff4
Amir Levy <alevy@codeaurora.org>
2017-01-09 14:53:02 +0200

    msm: ipa3: disable prefetch only for IPAv3.5
    This change makes sure that IPA driver will not try to
    disable prefetch on target which don't have IPA_TX_CFG
    register.
    
    Change-Id: Ie0b1fca1dfa14f29d6773d6e2ff72f9ae5a4b1ca
    CRs-Fixed: 1097871
    Signed-off-by: Amir Levy <alevy@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipa.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa.c
@@ -4011,10 +4011,11 @@ static int ipa3_post_init(const struct ipa3_plat_drv_res *resource_p,
 	}
 
 	/*
-	 * IPAv3.5.x requires to disable prefetch for USB in order to allow
-	 * MBIM to work, currently MBIM is not needed in MHI mode.
+	 * IPAv3.5 and above requires to disable prefetch for USB in order
+	 * to allow MBIM to work, currently MBIM is not needed in MHI mode.
 	 */
-	if (!ipa3_ctx->ipa_config_is_mhi)
+	if ((ipa3_ctx->ipa_hw_type >= IPA_HW_v3_5) &&
+		(!ipa3_ctx->ipa_config_is_mhi))
 		ipa3_disable_prefetch(IPA_CLIENT_USB_CONS);
 
 	if (ipa3_ctx->transport_prototype == IPA_TRANSPORT_TYPE_GSI) {
