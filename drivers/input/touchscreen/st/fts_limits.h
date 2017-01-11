ed29ea5a01b98c10239a29004df5dd14cd636293
Yahui Wang <yahuiw@codeaurora.org>
2017-01-11 23:31:30 +0800

    input: touchpanel: Add ST Touchscreen version 4.1.0 driver
    This is the reference driver source code for ST
    Touchscreen of version 4.1.0. It is used for QVR8998
    touchscreen.
    (cherry picked from commit 2cd09314337d614e69d0ebd99afb71d99d31b69a)
    
    Signed-off-by: chenx <chenxiang0527@thundersoft.com>
    Git-commit: 2cd09314337d614e69d0ebd99afb71d99d31b69a
    Git-repo: https://source.codeaurora.org/quic/la/kernel/msm-3.10
    CRs-Fixed: 1106217
    Change-Id: I6674245402c0a5d6cca6bf82a9f8b1bf4f4f4ef3
    Signed-off-by: Jin Fu <jinf@codeaurora.org>

---
+++ drivers/input/touchscreen/st/fts_limits.h
@@ -0,0 +1,10 @@
+#ifndef FTS_LIMITS_H
+#define FTS_LIMITS_H
+/* This is an auto generated header file
+* --->Remember to change the name of the two variables!<--- */
+const uint32_t myArray2_size;
+
+const uint8_t myArray2[] = {
+};
+
+#endif
