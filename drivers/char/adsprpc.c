fcfbef4e6e5f73d4003ba4f6b3a39a43bf32716a
Sathish Ambley <sathishambley@codeaurora.org>
2017-01-10 22:29:53 -0800

    msm: ADSPRPC: Free private data on error conditions
    Ensure the buffers associated with the file private data
    is freed on error conditions.
    
    Change-Id: I21e92ec066c362e4e8897a176ec2a58b762b077a
    Acked-by: Himateja Reddy <hmreddy@qti.qualcomm.com>
    Signed-off-by: Sathish Ambley <sathishambley@codeaurora.org>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -1852,8 +1852,10 @@ static int fastrpc_device_release(struct inode *inode, struct file *file)
 		file->private_data = 0;
 	}
 bail:
-	if (err)
+	if (err) {
+		fastrpc_file_free(fl);
 		kfree(pfl);
+	}
 	return err;
 }
 
