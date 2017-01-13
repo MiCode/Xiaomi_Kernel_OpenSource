82a6fedb6ea53c9f3eb00ab1e0b839d3fcfe5fc2
Luca Stefani <luca.stefani.ge1@gmail.com>
2017-01-13 16:09:57 +0100

    arm64: kernel: Fix build warning
    * Issue: After da643dc .enable expects an int
      -> Change cpu_enable_uao protype to int
    
    Change-Id: I581a1afd13cd90637e6a7a315ede2af8950b401f
    Signed-off-by: Luca Stefani <luca.stefani.ge1@gmail.com>

--- arch/arm64/mm/fault.c
+++ arch/arm64/mm/fault.c
@@ -658,8 +658,9 @@ int cpu_enable_pan(void *__unused)
  * We need to enable the feature at runtime (instead of adding it to
  * PSR_MODE_EL1h) as the feature may not be implemented by the cpu.
  */
-void cpu_enable_uao(void *__unused)
+int cpu_enable_uao(void *__unused)
 {
 	asm(SET_PSTATE_UAO(1));
+	return 0;
 }
 #endif /* CONFIG_ARM64_UAO */
