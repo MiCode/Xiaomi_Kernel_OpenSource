a1e92a92c027d556e7a3e6b5d6272d1019ccd077
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2017-01-16 10:22:29 -0800

    power_supply: remove PARALLEL_PERCENT property
    There is no need for this property. Remove it.
    
    Change-Id: I656eb51f8b9690d0c1046d1c82d2575fae270a40
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -285,7 +285,6 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(charger_temp),
 	POWER_SUPPLY_ATTR(charger_temp_max),
 	POWER_SUPPLY_ATTR(parallel_disable),
-	POWER_SUPPLY_ATTR(parallel_percent),
 	POWER_SUPPLY_ATTR(pe_start),
 	POWER_SUPPLY_ATTR(set_ship_mode),
 	POWER_SUPPLY_ATTR(debug_battery),
