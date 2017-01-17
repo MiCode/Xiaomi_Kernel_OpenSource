7b7d68c8caf783c9eb4a28045570a5db8896c4ea
Ashwini Patil <apati@codeaurora.org>
2017-01-17 15:40:52 +0530

    cfg80211: Define macro to indicate support for Random TA for PA frames
    Define macro to indicate backport support for randomizing TA of
    Public Action frames.
    
    Change-Id: I4ce8bfe6e6612dbfc63f0a7158b29aaf5ecf3292
    CRs-Fixed: 1112321
    Signed-off-by: Ashwini Patil <apati@codeaurora.org>

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -74,6 +74,7 @@ struct wiphy;
 #define CFG80211_SCAN_RANDOM_MAC_ADDR 1
 #define CFG80211_UPDATE_CONNECT_PARAMS 1
 #define CFG80211_BEACON_TX_RATE_CUSTOM_BACKPORT 1
+#define CFG80211_RAND_TA_FOR_PUBLIC_ACTION_FRAME 1
 
 /*
  * wireless hardware capability structures
