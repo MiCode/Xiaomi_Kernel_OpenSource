a7d1c3be7f82eca0b113aab661b1b035e51105ce
Utkarsh Saxena <usaxena@codeaurora.org>
2017-01-17 12:50:37 +0530

    msm: ipa: SDM660 miscellaneous changes
    Add support to Enable/Disable uc monitor holb feature.
    
    sdm660 does not  support uc monitor holb feature.
    Make changes to get this param via device tree to
    support devices which handle holb monitoring via IPA uC.
    
    sdm660 supports WLAN offload. Make changes to configure
    wlan pipes on IPA2.6L needed to support WLAN offload.
    
    Change-Id: I07e099ffb7833e4790dda1fd4864f3f61acb2a45
    Acked-by: Mohammed Javid <mjavid@qti.qualcomm.com>
    Signed-off-by: Utkarsh Saxena <usaxena@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v2/ipa_uc.c
+++ drivers/platform/msm/ipa/ipa_v2/ipa_uc.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -768,8 +768,12 @@ int ipa_uc_monitor_holb(enum ipa_client_type ipa_client, bool enable)
 	int ep_idx;
 	int ret;
 
-	/* HOLB monitoring is applicable only to 2.6L. */
-	if (ipa_ctx->ipa_hw_type != IPA_HW_v2_6L) {
+	/*
+	 * HOLB monitoring is applicable to 2.6L.
+	 * And also could be enabled from dtsi node.
+	 */
+	if (ipa_ctx->ipa_hw_type != IPA_HW_v2_6L ||
+		!ipa_ctx->ipa_uc_monitor_holb) {
 		IPADBG("Not applicable on this target\n");
 		return 0;
 	}
