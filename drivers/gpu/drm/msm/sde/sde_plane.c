58ec3c6b0598ea71453d02c18e056bd3594df96e
Dhaval Patel <pdhaval@codeaurora.org>
2017-01-17 16:28:12 -0800

    drm/msm: fix copyright marking in "msm" drm driver
    msm driver is updated signficantly by CAF authors but
    it does not carry the CAF license. This patch update it
    accordingly. At the same time; kms, smmu, plane, crtc, encoder
    code was written by referring the mdp5 driver but it did not
    carry the license from mdp5 driver. The patch fixes the
    issue.
    
    Change-Id: I4670d2220d6df46213c7220a2b46de32e3ec15a5
    Signed-off-by: Dhaval Patel <pdhaval@codeaurora.org>

--- drivers/gpu/drm/msm/sde/sde_plane.c
+++ drivers/gpu/drm/msm/sde/sde_plane.c
@@ -1,13 +1,19 @@
-/* Copyright (c) 2015-2016, The Linux Foundation. All rights reserved.
+/*
+ * Copyright (C) 2014-2017 The Linux Foundation. All rights reserved.
+ * Copyright (C) 2013 Red Hat
+ * Author: Rob Clark <robdclark@gmail.com>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 and
- * only version 2 as published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #define pr_fmt(fmt)	"[drm:%s:%d] " fmt, __func__, __LINE__
