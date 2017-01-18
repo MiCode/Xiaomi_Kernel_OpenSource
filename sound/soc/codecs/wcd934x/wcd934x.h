037a4ab951f0b48704f09030bbeb9ac69497b7b0
Phani Kumar Uppalapati <phaniu@codeaurora.org>
2017-01-17 18:36:49 -0800

    ASoC: wcd934x: Enable slim tx port auto recovery mechanism
    Enable slimbus tx port auto recovery mechanism for wcd934x
    audio codec to recover from slimbus underflow/overflow errors
    causing audio TX mute.
    
    CRs-Fixed: 1110511
    Change-Id: Ic6f120807ec58d75b7b236e80705279a0b198d36
    Signed-off-by: Phani Kumar Uppalapati <phaniu@codeaurora.org>

--- sound/soc/codecs/wcd934x/wcd934x.h
+++ sound/soc/codecs/wcd934x/wcd934x.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2016, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2015-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -132,6 +132,13 @@ enum {
 	WCD934X_IRQ_RESERVED_4,
 	WCD934X_IRQ_SLNQ_DIGITAL,
 	WCD934X_NUM_IRQS,
+};
+
+/* WCD934X slimbus slave port error status */
+enum {
+	SB_PORT_ERR_OF, /* SB port overflow */
+	SB_PORT_ERR_UF, /* SB port underflow */
+	SB_PORT_ERR_MAX,
 };
 
 /*
