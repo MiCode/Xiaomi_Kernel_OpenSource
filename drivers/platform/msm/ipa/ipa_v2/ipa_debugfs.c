9f05b365b6e7898882abe8498b5197485621ff40
Utkarsh Saxena <usaxena@codeaurora.org>
2017-01-22 22:04:13 +0530

    msm: ipa: Fix WDI2.0 stats
    New stat param "num_qmb_int_handled" is added to rx_ch_stats
    by IPA uC in WDI2.0. This results in a size mismatch and
    WDI stats feature is broken. Make a change to update the
    new param "num_qmb_int_handled" in rx_ch_stats structure.
    
    Also make a change to read all the rx_ch_stats to avoid
    printing stale values.
    
    Change-Id: I5d141f531f1cfdab0bd0056041e22acb26657960
    Acked-by: Chaitanya Pratapa <cpratapa@qti.qualcomm.com>
    Signed-off-by: Utkarsh Saxena <usaxena@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v2/ipa_debugfs.c
+++ drivers/platform/msm/ipa/ipa_v2/ipa_debugfs.c
@@ -1273,8 +1273,9 @@ static ssize_t ipa_read_wdi(struct file *file, char __user *ubuf,
 			"RX num_db=%u\n"
 			"RX num_unexpected_db=%u\n"
 			"RX num_pkts_in_dis_uninit_state=%u\n"
-			"num_ic_inj_vdev_change=%u\n"
-			"num_ic_inj_fw_desc_change=%u\n"
+			"RX num_ic_inj_vdev_change=%u\n"
+			"RX num_ic_inj_fw_desc_change=%u\n"
+			"RX num_qmb_int_handled=%u\n"
 			"RX reserved1=%u\n"
 			"RX reserved2=%u\n",
 			stats.rx_ch_stats.max_outstanding_pkts,
@@ -1296,6 +1297,7 @@ static ssize_t ipa_read_wdi(struct file *file, char __user *ubuf,
 			stats.rx_ch_stats.num_pkts_in_dis_uninit_state,
 			stats.rx_ch_stats.num_ic_inj_vdev_change,
 			stats.rx_ch_stats.num_ic_inj_fw_desc_change,
+			stats.rx_ch_stats.num_qmb_int_handled,
 			stats.rx_ch_stats.reserved1,
 			stats.rx_ch_stats.reserved2);
 		cnt += nbytes;
