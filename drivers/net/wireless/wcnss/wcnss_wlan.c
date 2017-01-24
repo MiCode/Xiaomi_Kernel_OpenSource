4d4adddb49c1bf8b114b74af8e8b78e5afad0852
Sarada Prasanna Garnayak <sgarna@codeaurora.org>
2017-01-24 21:09:24 +0530

    wcnss: Add support to read wifi dual band capability
    The WLAN dual band support vary from target to target
    and to enable/disable dual band support for a particular
    target the wcnss platform driver read the wlan hardware
    qfuse register and export the dual band capability info
    to wlan host driver to enable/disable this dual band feature.
    
    Add export symbol for dual band capability info.
    
    CRs-Fixed: 1115909
    Change-Id: I42bee0b80b43fa638f1d418c9fada2fc285df006
    Signed-off-by: Anand Kumar <anandkumar@codeaurora.org>
    Signed-off-by: Sarada Prasanna Garnayak <sgarna@codeaurora.org>

--- drivers/net/wireless/wcnss/wcnss_wlan.c
+++ drivers/net/wireless/wcnss/wcnss_wlan.c
@@ -56,6 +56,7 @@
 #define WCNSS_PM_QOS_TIMEOUT	15000
 #define IS_CAL_DATA_PRESENT     0
 #define WAIT_FOR_CBC_IND     2
+#define WCNSS_DUAL_BAND_CAPABILITY_OFFSET	(1 << 8)
 
 /* module params */
 #define WCNSS_CONFIG_UNSPECIFIED (-1)
@@ -118,6 +119,8 @@ static DEFINE_SPINLOCK(reg_spinlock);
 #define PRONTO_PMU_CFG_OFFSET              0x1004
 #define PRONTO_PMU_COM_CSR_OFFSET          0x1040
 #define PRONTO_PMU_SOFT_RESET_OFFSET       0x104C
+
+#define PRONTO_QFUSE_DUAL_BAND_OFFSET	   0x0018
 
 #define A2XB_CFG_OFFSET				0x00
 #define A2XB_INT_SRC_OFFSET			0x0c
@@ -379,6 +382,7 @@ static struct {
 	void __iomem *pronto_saw2_base;
 	void __iomem *pronto_pll_base;
 	void __iomem *pronto_mcu_base;
+	void __iomem *pronto_qfuse;
 	void __iomem *wlan_tx_status;
 	void __iomem *wlan_tx_phy_aborts;
 	void __iomem *wlan_brdg_err_source;
@@ -424,6 +428,7 @@ static struct {
 	struct mutex pm_qos_mutex;
 	struct clk *snoc_wcnss;
 	unsigned int snoc_wcnss_clock_freq;
+	bool is_dual_band_disabled;
 } *penv = NULL;
 
 static ssize_t wcnss_wlan_macaddr_store(struct device *dev,
@@ -589,6 +594,30 @@ void wcnss_pronto_is_a2xb_bus_stall(void *tst_addr, u32 fifo_mask, char *type)
 		pr_err("%s data FIFO tstbus not stalled reg%08x\n",
 				type, reg);
 	}
+}
+
+int wcnss_get_dual_band_capability_info(struct platform_device *pdev)
+{
+	u32 reg = 0;
+	struct resource *res;
+
+	res = platform_get_resource_byname(
+			pdev, IORESOURCE_MEM, "pronto_qfuse");
+	if (!res)
+		return -EINVAL;
+
+	penv->pronto_qfuse = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(penv->pronto_qfuse))
+		return -ENOMEM;
+
+	reg = readl_relaxed(penv->pronto_qfuse +
+			PRONTO_QFUSE_DUAL_BAND_OFFSET);
+	if (reg & WCNSS_DUAL_BAND_CAPABILITY_OFFSET)
+		penv->is_dual_band_disabled = true;
+	else
+		penv->is_dual_band_disabled = false;
+
+	return 0;
 }
 
 /* Log pronto debug registers during SSR Timeout CB */
@@ -1705,6 +1734,14 @@ int wcnss_wlan_iris_xo_mode(void)
 }
 EXPORT_SYMBOL(wcnss_wlan_iris_xo_mode);
 
+int wcnss_wlan_dual_band_disabled(void)
+{
+	if (penv && penv->pdev)
+		return penv->is_dual_band_disabled;
+
+	return -EINVAL;
+}
+EXPORT_SYMBOL(wcnss_wlan_dual_band_disabled);
 
 void wcnss_suspend_notify(void)
 {
@@ -3153,6 +3190,16 @@ wcnss_trigger_config(struct platform_device *pdev)
 			pr_err("%s: ioremap pronto mcu physical failed\n",
 				__func__);
 			goto fail_ioremap2;
+		}
+
+		if (of_property_read_bool(
+			pdev->dev.of_node, "qcom,is-dual-band-disabled")) {
+			ret = wcnss_get_dual_band_capability_info(pdev);
+			if (ret) {
+				pr_err(
+				"%s: failed to get dual band info\n", __func__);
+				goto fail_ioremap2;
+			}
 		}
 	}
 
