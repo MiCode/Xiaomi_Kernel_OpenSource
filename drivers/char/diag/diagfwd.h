cfd24cdcd64f05aaf8bc133e148ce202581da862
Chris Lew <clew@codeaurora.org>
2017-01-25 11:56:30 -0800

    diag: Add mdlog session to apps pkt response path
    Apps pkt cmd response defaults to APPS_DATA peripheral.
    Error response for mdlogging does not go to the right md
    session. Pass md session along response path to ensure
    correct destination.
    
    Change-Id: Iece75fdb580a68350085bf48df374e60c2b4adb6
    Signed-off-by: Chris Lew <clew@codeaurora.org>

--- drivers/char/diag/diagfwd.h
+++ drivers/char/diag/diagfwd.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2008-2015, 2017 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -45,7 +45,8 @@ void diag_update_userspace_clients(unsigned int type);
 void diag_update_sleeping_process(int process_id, int data_type);
 int diag_process_apps_pkt(unsigned char *buf, int len,
 			  struct diag_md_session_t *info);
-void diag_send_error_rsp(unsigned char *buf, int len);
+void diag_send_error_rsp(unsigned char *buf, int len,
+			 struct diag_md_session_t *info);
 void diag_update_pkt_buffer(unsigned char *buf, uint32_t len, int type);
 int diag_process_stm_cmd(unsigned char *buf, unsigned char *dest_buf);
 void diag_md_hdlc_reset_timer_func(unsigned long pid);
