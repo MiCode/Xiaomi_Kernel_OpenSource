86aec088641bb7a82603c8aefbc3e5d6bdcce29b
Daniel Rosenberg <drosen@google.com>
2017-01-25 13:48:45 -0800

    ANDROID: sdcardfs: Add GID Derivation to sdcardfs
    This changes sdcardfs to modify the user and group in the
    underlying filesystem depending on its usage. Ownership is
    set by Android user, and package, as well as if the file is
    under obb or cache. Other files can be labeled by extension.
    Those values are set via the configfs interace.
    
    To add an entry,
    mkdir -p [configfs root]/sdcardfs/extensions/[gid]/[ext]
    
    Bug: 34262585
    Change-Id: I4e030ce84f094a678376349b1a96923e5076a0f4
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- fs/sdcardfs/lookup.c
+++ fs/sdcardfs/lookup.c
@@ -368,7 +368,7 @@ struct dentry *sdcardfs_lookup(struct inode *dir, struct dentry *dentry,
         }
 
 	/* save current_cred and override it */
-	OVERRIDE_CRED_PTR(SDCARDFS_SB(dir->i_sb), saved_cred);
+	OVERRIDE_CRED_PTR(SDCARDFS_SB(dir->i_sb), saved_cred, SDCARDFS_I(dir));
 
 	sdcardfs_get_lower_path(parent, &lower_parent_path);
 
@@ -392,6 +392,7 @@ struct dentry *sdcardfs_lookup(struct inode *dir, struct dentry *dentry,
 		/* get derived permission */
 		get_derived_permission(parent, dentry);
 		fixup_tmp_permissions(dentry->d_inode);
+		fixup_lower_ownership(dentry, dentry->d_name.name);
 	}
 	/* update parent directory's atime */
 	fsstack_copy_attr_atime(parent->d_inode,
