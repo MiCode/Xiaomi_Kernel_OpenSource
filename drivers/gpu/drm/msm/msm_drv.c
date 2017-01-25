7401b2226bc045277a170698fec77c9db9fff337
Narendra Muppalla <NarendraM@codeaurora.org>
2017-01-25 11:27:02 -0800

    Revert "drm/msm/sde: add driver for sde support"
    This reverts 'commit 54a4a3fb4194 ("drm/msm/sde: add
    driver for sde support")'
    This is partial change for display drm driver,
    that will break drm/sde merge commit.
    
    Change-Id: Iea6b08608a30979232826efba8fbb2a631d93f83
    Signed-off-by: Narendra Muppalla <NarendraM@codeaurora.org>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -235,20 +235,13 @@ static int msm_unload(struct drm_device *dev)
 	return 0;
 }
 
-#define KMS_MDP4 0
-#define KMS_MDP5 1
-#define KMS_SDE  2
-
 static int get_mdp_ver(struct platform_device *pdev)
 {
 #ifdef CONFIG_OF
 	static const struct of_device_id match_types[] = { {
 		.compatible = "qcom,mdss_mdp",
-		.data	= (void	*)KMS_MDP5,
-	},
-	{
-		.compatible = "qcom,sde-kms",
-		.data	= (void	*)KMS_SDE,
+		.data	= (void	*)5,
+	}, {
 		/* end node */
 	} };
 	struct device *dev = &pdev->dev;
@@ -257,7 +250,7 @@ static int get_mdp_ver(struct platform_device *pdev)
 	if (match)
 		return (int)(unsigned long)match->data;
 #endif
-	return KMS_MDP4;
+	return 4;
 }
 
 #include <linux/of_address.h>
@@ -376,14 +369,11 @@ static int msm_load(struct drm_device *dev, unsigned long flags)
 		goto fail;
 
 	switch (get_mdp_ver(pdev)) {
-	case KMS_MDP4:
+	case 4:
 		kms = mdp4_kms_init(dev);
 		break;
-	case KMS_MDP5:
+	case 5:
 		kms = mdp5_kms_init(dev);
-		break;
-	case KMS_SDE:
-		kms = sde_kms_init(dev);
 		break;
 	default:
 		kms = ERR_PTR(-ENODEV);
@@ -1150,7 +1140,6 @@ static const struct platform_device_id msm_id[] = {
 static const struct of_device_id dt_match[] = {
 	{ .compatible = "qcom,mdp" },      /* mdp4 */
 	{ .compatible = "qcom,mdss_mdp" }, /* mdp5 */
-	{ .compatible = "qcom,sde-kms" },  /* sde  */
 	{}
 };
 MODULE_DEVICE_TABLE(of, dt_match);
