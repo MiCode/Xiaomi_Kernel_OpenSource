4a8b50101bc60866c8f6f31c64e945bdee0f077f
Siddartha Mohanadoss <smohanad@codeaurora.org>
2017-01-26 13:25:37 -0800

    power: supply: Add property to vote for TLIM
    Enable clients to vote for TLIM. It is used by
    the VADC client to force the charger circuitry before
    reading the charger temperature channel.
    
    Change-Id: Id891b797f289d28ddb7ea2be95bd91365653cc02
    Signed-off-by: Siddartha Mohanadoss <smohanad@codeaurora.org>

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -164,6 +164,7 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_PE_START,
 	POWER_SUPPLY_PROP_SET_SHIP_MODE,
 	POWER_SUPPLY_PROP_BOOST_CURRENT,
+	POWER_SUPPLY_PROP_FORCE_TLIM,
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_PROP_CHARGE_COUNTER_EXT,
 	/* Properties of type `const char *' */
