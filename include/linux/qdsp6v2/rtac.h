6cd1cb7964eec7cb65c0acab47a6f5664cc5939e
Vidyakumar Athota <vathota@codeaurora.org>
2017-02-02 17:29:19 -0800

    ASoC: qdsp6v2: update afe topology to RTAC driver
    In device switch scenarios, AFE topology is stored in Real Time
    Audio Calibration(RTAC) driver before afe ports are open, which
    is invalid. Update AFE topology in RTAC driver after topology ID
    is obtained during AFE port open.
    
    Change-Id: I034321126fb0ffc9a2871f3c64f29f8aaedb21e7
    Signed-off-by: Vidyakumar Athota <vathota@codeaurora.org>

--- include/linux/qdsp6v2/rtac.h
+++ include/linux/qdsp6v2/rtac.h
@@ -1,4 +1,5 @@
-/* Copyright (c) 2011, 2013-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011, 2013-2015, 2017, The Linux Foundation. All rights
+ * reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -95,4 +96,5 @@ int rtac_clear_mapping(uint32_t cal_type);
 bool rtac_make_afe_callback(uint32_t *payload, u32 payload_size);
 void rtac_set_afe_handle(void *handle);
 void get_rtac_adm_data(struct rtac_adm *adm_data);
+void rtac_update_afe_topology(u32 port_id);
 #endif
