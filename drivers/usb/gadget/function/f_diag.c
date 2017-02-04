4e1b53bfc33970ffc2382da68056d1e5c2fdfbc5
Vamsi Krishna Samavedam <vskrishn@codeaurora.org>
2017-02-03 22:01:17 -0800

    usb: diag: Set interface protocol to represent diag
    PC tools can use this value to identify protocol used
    by diag interface. For example, diag interface can use
    firehouse or sahara or plain diag protocols. Update the
    protocol value to represent plain diag.
    
    Change-Id: Ifba80a21a99575d1fcdb54647319aea0822b8898
    Signed-off-by: Vamsi Krishna Samavedam <vskrishn@codeaurora.org>

--- drivers/usb/gadget/function/f_diag.c
+++ drivers/usb/gadget/function/f_diag.c
@@ -2,7 +2,7 @@
  * Diag Function Device - Route ARM9 and ARM11 DIAG messages
  * between HOST and DEVICE.
  * Copyright (C) 2007 Google, Inc.
- * Copyright (c) 2008-2016, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2008-2017, The Linux Foundation. All rights reserved.
  * Author: Brian Swetland <swetland@google.com>
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -70,7 +70,7 @@ static struct usb_interface_descriptor intf_desc = {
 	.bNumEndpoints      =	2,
 	.bInterfaceClass    =	0xFF,
 	.bInterfaceSubClass =	0xFF,
-	.bInterfaceProtocol =	0xFF,
+	.bInterfaceProtocol =	0x30,
 };
 
 static struct usb_endpoint_descriptor hs_bulk_in_desc = {
