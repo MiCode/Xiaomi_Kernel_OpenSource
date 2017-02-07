2e139766cacb8e7deb048d36798e144916e23931
Arun KS <arunks@codeaurora.org>
2017-02-07 18:41:25 +0530

    esoc: Fix user space corruption due to wrong data type
    ESOC IOCTL was designed to transfer data of type unsigned int, but
    was pushing data of unsigned long type. Hence causing data corruption
    in user space. Fix this by changing put user to use unsigned int.
    
    Change-Id: Ia233eedd76f1ca1ec3036889d6764e568ee975dd
    Signed-off-by: Arun KS <arunks@codeaurora.org>

--- include/uapi/linux/esoc_ctrl.h
+++ include/uapi/linux/esoc_ctrl.h
@@ -3,11 +3,11 @@
 
 #define ESOC_CODE		0xCC
 
-#define ESOC_CMD_EXE		_IOW(ESOC_CODE, 1, u32)
-#define ESOC_WAIT_FOR_REQ	_IOR(ESOC_CODE, 2, u32)
-#define ESOC_NOTIFY		_IOW(ESOC_CODE, 3, u32)
-#define ESOC_GET_STATUS		_IOR(ESOC_CODE, 4, u32)
-#define ESOC_WAIT_FOR_CRASH	_IOR(ESOC_CODE, 6, u32)
+#define ESOC_CMD_EXE		_IOW(ESOC_CODE, 1, unsigned int)
+#define ESOC_WAIT_FOR_REQ	_IOR(ESOC_CODE, 2, unsigned int)
+#define ESOC_NOTIFY		_IOW(ESOC_CODE, 3, unsigned int)
+#define ESOC_GET_STATUS		_IOR(ESOC_CODE, 4, unsigned int)
+#define ESOC_WAIT_FOR_CRASH	_IOR(ESOC_CODE, 6, unsigned int)
 #define ESOC_REG_REQ_ENG	_IO(ESOC_CODE, 7)
 #define ESOC_REG_CMD_ENG	_IO(ESOC_CODE, 8)
 
