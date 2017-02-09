cdc3ace6a8a1c6fc2bfc5bae57cdcb95d9e21482
Abhijit Kulkarni <kabhijit@codeaurora.org>
2017-02-08 16:41:16 -0800

    msm: mdss: idle power collapse notify
    This fix adds notification of idle power collapse to user space
    for cmd mode panels. Without this fix user space has no
    knowledge of reprogramming the QSEED3 LUTs after idle power
    collapse, this needs to be done with the user space knowledge
    to keep it's LUT bank offset in sync with the HW used LUT bank
    offset.
    
    Change-Id: Ia1fbd07da8c8c854a74cf5dad62f5ec2d1fc962e
    Signed-off-by: Abhijit Kulkarni <kabhijit@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_fb.h
+++ drivers/video/fbdev/msm/mdss_fb.h
@@ -468,4 +468,5 @@ void mdss_fb_report_panel_dead(struct msm_fb_data_type *mfd);
 void mdss_panelinfo_to_fb_var(struct mdss_panel_info *pinfo,
 						struct fb_var_screeninfo *var);
 void mdss_fb_calc_fps(struct msm_fb_data_type *mfd);
+void mdss_fb_idle_pc(struct msm_fb_data_type *mfd);
 #endif /* MDSS_FB_H */
