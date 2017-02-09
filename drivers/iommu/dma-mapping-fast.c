29f6140859f167c194ed81d6a92929d371ecd7e0
Charan Teja Reddy <charante@codeaurora.org>
2017-02-09 20:44:29 +0530

    iommu: dma-mapping: add dma mapper for io-pgtable-fast for 32 bit
    io-pgtable-fast was implemented to achieve
    better performance for IOMMU map/un-map. Add
    DMA API support that goes through io-pgtable-fast
    for 32 bit targets.
    
    Change-Id: Ib46157cabd1f9b31903837bcc0dcaad87037cdb6
    Signed-off-by: Charan Teja Reddy <charante@codeaurora.org>
    [pdaly@codeaurora.org: Included only changes to general iommu files]
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

--- drivers/iommu/dma-mapping-fast.c
+++ drivers/iommu/dma-mapping-fast.c
@@ -17,7 +17,8 @@
 #include <linux/vmalloc.h>
 #include <asm/cacheflush.h>
 #include <asm/dma-iommu.h>
-
+#include <linux/slab.h>
+#include <linux/vmalloc.h>
 
 /* some redundant definitions... :( TODO: move to io-pgtable-fast.h */
 #define FAST_PAGE_SHIFT		12
@@ -633,7 +634,7 @@ static void __fast_smmu_mapped_over_stale(struct dma_fast_smmu_mapping *fast,
 	dev_err(fast->dev, "Mapped over stale tlb at %pa\n", &iova);
 	dev_err(fast->dev, "bitmap (failure at idx %lu):\n", bitmap_idx);
 	dev_err(fast->dev, "ptep: %p pmds: %p diff: %lu\n", ptep,
-		fast->pgtbl_pmds, ptep - fast->pgtbl_pmds);
+		fast->pgtbl_pmds, bitmap_idx);
 	print_hex_dump(KERN_ERR, "bmap: ", DUMP_PREFIX_ADDRESS,
 		       32, 8, fast->bitmap, fast->bitmap_size, false);
 }
@@ -683,7 +684,7 @@ static const struct dma_map_ops fast_smmu_dma_ops = {
  * fast_smmu_attach_device function.
  */
 static struct dma_fast_smmu_mapping *__fast_smmu_create_mapping_sized(
-	dma_addr_t base, size_t size)
+	dma_addr_t base, u64 size)
 {
 	struct dma_fast_smmu_mapping *fast;
 
@@ -730,7 +731,7 @@ int fast_smmu_attach_device(struct device *dev,
 	int atomic_domain = 1;
 	struct iommu_domain *domain = mapping->domain;
 	struct iommu_pgtbl_info info;
-	size_t size = mapping->bits << PAGE_SHIFT;
+	u64 size = (u64)mapping->bits << PAGE_SHIFT;
 
 	if (mapping->base + size > (SZ_1G * 4ULL))
 		return -EINVAL;
