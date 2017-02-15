ec2302a45026d3dcc17e170751316e67393f26e3
Xu Yang <yangxu@codeaurora.org>
2017-02-15 09:29:27 +0800

    msm: mdss: Add support to update backlight when display commit
    1. Add support to update backlight when display commit. Backlight
    level is sent from userspace. And sync to set PP config, kickoff,
    and set backlight in the same vsync period.
    
    2. Add support to get current backlight level through sysfs node.
    
    Change-Id: Icf264299ecf8edd3831938745a471c2655c23542
    Signed-off-by: Xu Yang <yangxu@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_mdp_intf_video.c
+++ drivers/video/fbdev/msm/mdss_mdp_intf_video.c
@@ -2340,6 +2340,7 @@ int mdss_mdp_video_start(struct mdss_mdp_ctl *ctl)
 	ctl->ops.early_wake_up_fnc = mdss_mdp_video_early_wake_up;
 	ctl->ops.update_lineptr = mdss_mdp_video_lineptr_ctrl;
 	ctl->ops.avr_ctrl_fnc = mdss_mdp_video_avr_ctrl;
+	ctl->ops.wait_for_vsync_fnc = NULL;
 
 	return 0;
 }
