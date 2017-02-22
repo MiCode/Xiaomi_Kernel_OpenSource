ba81f70483a0928dd65823101d3a2b4ae3c25b67
Imran Khan <kimran@codeaurora.org>
2017-02-22 11:28:15 +0530

    Revert "ANDROID: fs: FS tracepoints to track IO."
    This reverts 'commit 5089c1e8dd74 ("ANDROID: fs: FS
    tracepoints to track IO" )', which was calling dput
    in atomic context, resultig in BUG reported by scheduler.
    
    Conflicts:
    	fs/mpage.c
    
    Change-Id: Iad84bf50ab29699125d04e23801cc913c13d53f3
    Signed-off-by: Imran Khan <kimran@codeaurora.org>

--- fs/ext4/inline.c
+++ fs/ext4/inline.c
@@ -18,7 +18,6 @@
 #include "ext4.h"
 #include "xattr.h"
 #include "truncate.h"
-#include <trace/events/android_fs.h>
 
 #define EXT4_XATTR_SYSTEM_DATA	"data"
 #define EXT4_MIN_INLINE_DATA_SIZE	((sizeof(__le32) * EXT4_N_BLOCKS))
@@ -502,9 +501,6 @@ int ext4_readpage_inline(struct inode *inode, struct page *page)
 		return -EAGAIN;
 	}
 
-	trace_android_fs_dataread_start(inode, page_offset(page), PAGE_SIZE,
-					current->pid, current->comm);
-
 	/*
 	 * Current inline data can only exist in the 1st page,
 	 * So for all the other pages, just set them uptodate.
@@ -515,8 +511,6 @@ int ext4_readpage_inline(struct inode *inode, struct page *page)
 		zero_user_segment(page, 0, PAGE_CACHE_SIZE);
 		SetPageUptodate(page);
 	}
-
-	trace_android_fs_dataread_end(inode, page_offset(page), PAGE_SIZE);
 
 	up_read(&EXT4_I(inode)->xattr_sem);
 
