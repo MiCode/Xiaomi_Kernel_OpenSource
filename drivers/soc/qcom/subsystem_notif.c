191015b010f38eeb835da666d235721fdd1d5272
Stephen Boyd <sboyd@codeaurora.org>
2017-03-01 17:04:04 -0800

    soc: qcom: subsystem_notif: Fix suspicious indent
    This is indented oddly, and doesn't need the local ret variable.
    Clean things up and also drop the cast from void because that
    isn't necessary and we're here.
    
    Change-Id: I02106107ea6e7500db6cc822a2d5cf88c5d0aa38
    Cc: Puja Gupta <pujag@codeaurora.org>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/soc/qcom/subsystem_notif.c
+++ drivers/soc/qcom/subsystem_notif.c
@@ -149,9 +149,7 @@ int subsys_notif_queue_notification(void *subsys_handle,
 					enum subsys_notif_type notif_type,
 					void *data)
 {
-	int ret = 0;
-	struct subsys_notif_info *subsys =
-		(struct subsys_notif_info *) subsys_handle;
+	struct subsys_notif_info *subsys = subsys_handle;
 
 	if (!subsys)
 		return -EINVAL;
@@ -159,10 +157,8 @@ int subsys_notif_queue_notification(void *subsys_handle,
 	if (notif_type < 0 || notif_type >= SUBSYS_NOTIF_TYPE_COUNT)
 		return -EINVAL;
 
-		ret = srcu_notifier_call_chain(
-			&subsys->subsys_notif_rcvr_list, notif_type,
-			data);
-	return ret;
+	return srcu_notifier_call_chain(&subsys->subsys_notif_rcvr_list,
+				       notif_type, data);
 }
 EXPORT_SYMBOL(subsys_notif_queue_notification);
 
