e704eb52754a399c4b873b7e25c7ad0af3b431b0
Arun KS <arunks@codeaurora.org>
2017-03-02 18:12:54 +0530

    esoc: Add provision to handle shutdown request in userspace
    In certain scenarios, modem shutdown requests are handled in
    userspace. Enhance request engine of esoc driver to send
    shutdown requests to userspace.
    
    Also, during a shutdown, avoid setting status to 0, if line is
    not a power source. There can be multiple mdms monitoring status
    line. This can otherwise be misinterpreted as an unexpected reset
    by other mdms.
    
    Change-Id: I9c20a86e76f892cc61dbfb814202b26e5cce3e96
    Signed-off-by: Arun KS <arunks@codeaurora.org>
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>

--- include/uapi/linux/esoc_ctrl.h
+++ include/uapi/linux/esoc_ctrl.h
@@ -16,6 +16,7 @@
 #define HSIC		"HSIC"
 #define HSICPCIe	"HSIC+PCIe"
 #define PCIe		"PCIe"
+#define ESOC_REQ_SEND_SHUTDOWN	ESOC_REQ_SEND_SHUTDOWN
 
 enum esoc_evt {
 	ESOC_RUN_STATE = 0x1,
@@ -56,6 +57,7 @@ enum esoc_req {
 	ESOC_REQ_IMG = 1,
 	ESOC_REQ_DEBUG,
 	ESOC_REQ_SHUTDOWN,
+	ESOC_REQ_SEND_SHUTDOWN,
 };
 
 #ifdef __KERNEL__
