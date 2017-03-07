1023e9bcd1698072b79f7aded3fb4c90907c489d
Jordan Crouse <jcrouse@codeaurora.org>
2017-03-07 11:14:04 -0700

    drm/msm: Remove DRM_MSM_NUM_IOCTLS
    The ioctl array is sparsely populated but the compiler will make sure
    that it is sufficiently sized for all the values that we have so we
    can safely use ARRAY_SIZE() instead of having a constantly changing
    #define in the uapi header.
    
    Change-Id: Ic0dedbad39f30abb8ce340289089d2f3a5f6fe36
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- include/uapi/drm/msm_drm.h
+++ include/uapi/drm/msm_drm.h
@@ -283,7 +283,6 @@ struct drm_panel_hdr_properties {
 #define DRM_SDE_WB_CONFIG              0x40
 #define DRM_MSM_REGISTER_EVENT         0x41
 #define DRM_MSM_DEREGISTER_EVENT       0x42
-#define DRM_MSM_NUM_IOCTLS             0x43
 
 #define DRM_IOCTL_MSM_GET_PARAM        DRM_IOWR(DRM_COMMAND_BASE + DRM_MSM_GET_PARAM, struct drm_msm_param)
 #define DRM_IOCTL_MSM_GEM_NEW          DRM_IOWR(DRM_COMMAND_BASE + DRM_MSM_GEM_NEW, struct drm_msm_gem_new)
