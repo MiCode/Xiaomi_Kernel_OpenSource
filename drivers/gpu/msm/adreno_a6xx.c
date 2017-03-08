a602c025c13a6cc23ade45ba7ec02f3e75c8fa9a
Shrenuj Bansal <shrenujb@codeaurora.org>
2017-03-08 10:40:34 -0800

    msm: kgsl: Add debug information for opcode error interrupts
    Add additional debug information (the possible opcode causing the
    error) whenever we receive a CP opcode error interrupt.
    
    Change-Id: I0759709b17f0734de0500a05de7c407a36f884f4
    Signed-off-by: Shrenuj Bansal <shrenujb@codeaurora.org>

--- drivers/gpu/msm/adreno_a6xx.c
+++ drivers/gpu/msm/adreno_a6xx.c
@@ -514,8 +514,14 @@ static void a6xx_cp_hw_err_callback(struct adreno_device *adreno_dev, int bit)
 
 	kgsl_regread(device, A6XX_CP_INTERRUPT_STATUS, &status1);
 
-	if (status1 & BIT(A6XX_CP_OPCODE_ERROR))
-		KGSL_DRV_CRIT_RATELIMIT(device, "CP opcode error interrupt\n");
+	if (status1 & BIT(A6XX_CP_OPCODE_ERROR)) {
+		unsigned int opcode;
+
+		kgsl_regwrite(device, A6XX_CP_SQE_STAT_ADDR, 1);
+		kgsl_regread(device, A6XX_CP_SQE_STAT_DATA, &opcode);
+		KGSL_DRV_CRIT_RATELIMIT(device,
+		"CP opcode error interrupt | possible opcode=0x%8.8x\n");
+	}
 	if (status1 & BIT(A6XX_CP_UCODE_ERROR))
 		KGSL_DRV_CRIT_RATELIMIT(device, "CP ucode error interrupt\n");
 	if (status1 & BIT(A6XX_CP_HW_FAULT_ERROR)) {
