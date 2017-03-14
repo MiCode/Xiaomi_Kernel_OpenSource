cf54360e5c6b8b0e0ad17b7130d49406087d6b51
Ram Chandrasekar <rkumbako@codeaurora.org>
2017-03-13 22:59:01 -0600

    drivers: of-thermal: Notify all sibling thermal zones on trip
    Sibling thermal zone trips are aggregated to set the threshold on the
    sensor. When the trip threshold is reached, sensor driver needs to
    notify all the sibling thermal zones. But the sibling relation is not
    visible to sensor driver to notify them.
    
    Provide a new API in the thermal core, which can be used by the sensor
    drivers to notify all the sibling thermal zones.
    
    Change-Id: I8e1042cbcdc6e4ac3c252042e9525ed920f55356
    Signed-off-by: Ram Chandrasekar <rkumbako@codeaurora.org>

--- drivers/thermal/thermal_core.h
+++ drivers/thermal/thermal_core.h
@@ -56,6 +56,7 @@ struct thermal_instance {
 
 int thermal_register_governor(struct thermal_governor *);
 void thermal_unregister_governor(struct thermal_governor *);
+void handle_thermal_trip(struct thermal_zone_device *tz, int trip);
 
 #ifdef CONFIG_THERMAL_GOV_STEP_WISE
 int thermal_gov_step_wise_register(void);
@@ -108,6 +109,7 @@ of_thermal_get_trip_points(struct thermal_zone_device *);
 int of_thermal_aggregate_trip(struct thermal_zone_device *tz,
 			      enum thermal_trip_type type,
 			      int *low, int *high);
+void of_thermal_handle_trip(struct thermal_zone_device *tz);
 #else
 static inline int of_parse_thermal_zones(void) { return 0; }
 static inline void of_thermal_destroy_zones(void) { }
@@ -131,6 +133,9 @@ static inline int of_thermal_aggregate_trip(struct thermal_zone_device *tz,
 {
 	return -ENODEV;
 }
+static inline
+void of_thermal_handle_trip(struct thermal_zone_device *tz)
+{ }
 #endif
 
 #endif /* __THERMAL_CORE_H__ */
