c03cbbd536407b843cc278836ea6287c31fb0c05
zhaoyuan <yzhao@codeaurora.org>
2017-03-14 14:00:06 +0800

    hbtp_input: prevent duplicated vote on TP power regulator
    Currently TP power regulator might be voted twice in some
    cases which will cause regulator not turned off in suspend
    mode. So add condition checking to prevent this.
    
    Change-Id: I88bb7ce336e0329458b78a56aef107707e7946f1
    Signed-off-by: zhaoyuan <yzhao@codeaurora.org>

--- drivers/input/misc/hbtp_input.c
+++ drivers/input/misc/hbtp_input.c
@@ -88,6 +88,7 @@ struct hbtp_data {
 	u32 power_off_delay;
 	bool manage_pin_ctrl;
 	bool afe_force_power_on;
+	bool regulator_enabled;
 };
 
 static struct hbtp_data *hbtp;
@@ -330,6 +331,11 @@ static int hbtp_pdev_power_on(struct hbtp_data *hbtp, bool on)
 	if (!on)
 		goto reg_off;
 
+	if (hbtp->regulator_enabled) {
+		pr_debug("%s: regulator already enabled\n", __func__);
+		return 0;
+	}
+
 	if (hbtp->vcc_ana) {
 		ret = reg_set_load_check(hbtp->vcc_ana,
 			hbtp->afe_load_ua);
@@ -373,9 +379,16 @@ static int hbtp_pdev_power_on(struct hbtp_data *hbtp, bool on)
 		}
 	}
 
+	hbtp->regulator_enabled = true;
+
 	return 0;
 
 reg_off:
+	if (!hbtp->regulator_enabled) {
+		pr_debug("%s: regulator not enabled\n", __func__);
+		return 0;
+	}
+
 	if (hbtp->vcc_dig) {
 		reg_set_load_check(hbtp->vcc_dig, 0);
 		regulator_disable(hbtp->vcc_dig);
@@ -392,6 +405,9 @@ reg_off:
 		reg_set_load_check(hbtp->vcc_ana, 0);
 		regulator_disable(hbtp->vcc_ana);
 	}
+
+	hbtp->regulator_enabled = false;
+
 	return 0;
 }
 
