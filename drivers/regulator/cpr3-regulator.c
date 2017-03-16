09e9be00e5bee49ca5776c48c97d830477d0e9dc
Stephen Boyd <sboyd@codeaurora.org>
2017-03-16 15:34:52 -0700

    regulator: cpr3: Add braces around suspiciously indented code
    It looks like we're missing braces here. Add them so that newer
    GCCs don't complain.
    
    Change-Id: I2aa95365dfd6ccaed082a0680ace8ac14b9083f8
    CRs-Fixed: 2014919
    Cc: David Collins <collinsd@codeaurora.org>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/regulator/cpr3-regulator.c
+++ drivers/regulator/cpr3-regulator.c
@@ -6550,11 +6550,12 @@ int cpr3_regulator_unregister(struct cpr3_controller *ctrl)
 	if (ctrl->irq && !cpumask_empty(&ctrl->irq_affinity_mask))
 		unregister_hotcpu_notifier(&ctrl->cpu_hotplug_notifier);
 
-	if (ctrl->ctrl_type == CPR_CTRL_TYPE_CPR4)
+	if (ctrl->ctrl_type == CPR_CTRL_TYPE_CPR4) {
 		rc = cpr3_ctrl_clear_cpr4_config(ctrl);
 		if (rc)
 			cpr3_err(ctrl, "failed to clear CPR4 configuration,rc=%d\n",
 				rc);
+	}
 
 	cpr3_ctrl_loop_disable(ctrl);
 
