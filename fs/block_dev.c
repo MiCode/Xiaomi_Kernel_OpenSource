d3e3998ce1324bb42af0e7864a556f6379a7c0ab
Jan Kara <jack@suse.cz>
2017-03-23 01:36:53 +0100

    block: Fix bdi assignment to bdev inode when racing with disk delete
    When disk->fops->open() in __blkdev_get() returns -ERESTARTSYS, we
    restart the process of opening the block device. However we forget to
    switch bdev->bd_bdi back to noop_backing_dev_info and as a result bdev
    inode will be pointing to a stale bdi. Fix the problem by setting
    bdev->bd_bdi later when __blkdev_get() is already guaranteed to succeed.
    
    Acked-by: Tejun Heo <tj@kernel.org>
    Reviewed-by: Hannes Reinecke <hare@suse.com>
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Jens Axboe <axboe@fb.com>
    Change-Id: I6736ed1aba8732d3146284311c718f48b9c06578
    Git-repo:
    git://git.kernel.org/pub/scm/linux/kernel/git/axboe/linux-block.git
    Git-commit: 03e262798884b0a5f948b17433afd80606cb3497
    Signed-off-by: Ritesh Harjani <riteshh@codeaurora.org>

--- fs/block_dev.c
+++ fs/block_dev.c
@@ -1223,8 +1223,6 @@ static int __blkdev_get(struct block_device *bdev, fmode_t mode, int for_part)
 		bdev->bd_disk = disk;
 		bdev->bd_queue = disk->queue;
 		bdev->bd_contains = bdev;
-		if (bdev->bd_bdi == &noop_backing_dev_info)
-			bdev->bd_bdi = bdi_get(disk->queue->backing_dev_info);
 
 		bdev->bd_inode->i_flags = disk->fops->direct_access ? S_DAX : 0;
 		if (!partno) {
@@ -1296,6 +1294,9 @@ static int __blkdev_get(struct block_device *bdev, fmode_t mode, int for_part)
 			    (bdev->bd_part->nr_sects % (PAGE_SIZE / 512)))
 				bdev->bd_inode->i_flags &= ~S_DAX;
 		}
+
+		if (bdev->bd_bdi == &noop_backing_dev_info)
+			bdev->bd_bdi = bdi_get(disk->queue->backing_dev_info);
 	} else {
 		if (bdev->bd_contains == bdev) {
 			ret = 0;
@@ -1327,8 +1328,6 @@ static int __blkdev_get(struct block_device *bdev, fmode_t mode, int for_part)
 	bdev->bd_disk = NULL;
 	bdev->bd_part = NULL;
 	bdev->bd_queue = NULL;
-	bdi_put(bdev->bd_bdi);
-	bdev->bd_bdi = &noop_backing_dev_info;
 	if (bdev != bdev->bd_contains)
 		__blkdev_put(bdev->bd_contains, mode, 1);
 	bdev->bd_contains = NULL;
