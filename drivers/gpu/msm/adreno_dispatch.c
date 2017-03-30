9361513eb0c2333d6430793c6cbe30c24766d5f7
Wenbin Wang <wwenbin@codeaurora.org>
2017-03-30 14:10:19 +0800

    msm: kgsl: Don't delete uninitialized preemption timer
    If we delete uninitialized timer on CONFIG_DEBUG_OBJECTS
    disabled build del_timer_sync() will block for ever. For
    all A3xx targets preemption timer is not initialized, but
    dispatcher fault handler is trying to delete the
    uninitialized preemption timer. Fix this issue by adding
    a preemption check before we delete it.
    
    CRs-Fixed: 2023690
    Change-Id: I2c51a0b2286b82bf2eb5ee68d923dd9585f07f00
    Signed-off-by: Wenbin Wang <wwenbin@codeaurora.org>
    Signed-off-by: Abhilash Kumar <krabhi@codeaurora.org>

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -2097,7 +2097,12 @@ static int dispatcher_do_fault(struct adreno_device *adreno_dev)
 	/* Turn off all the timers */
 	del_timer_sync(&dispatcher->timer);
 	del_timer_sync(&dispatcher->fault_timer);
-	del_timer_sync(&adreno_dev->preempt.timer);
+	/*
+	 * Deleting uninitialized timer will block for ever on kernel debug
+	 * disable build. Hence skip del timer if it is not initialized.
+	 */
+	if (adreno_is_preemption_enabled(adreno_dev))
+		del_timer_sync(&adreno_dev->preempt.timer);
 
 	mutex_lock(&device->mutex);
 
