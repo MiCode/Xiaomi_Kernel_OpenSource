68939767621fc31df1ed33725fe981062f5c134c
Yuanyuan Liu <yuanliu@codeaurora.org>
2017-04-04 16:43:03 -0700

    icnss: Update ICNSS platform driver
    Update ICNSS platform driver. This is a snapshot of the
    ICNSS driver and associated files as of msm-4.4 commit:
    commit e79dfec9b298
    ("icnss: Send FW CRASHED uevent only if driver is probed").
    
    CRs-Fixed: 2029010
    Change-Id: I4e99b6bd904ce7f4a37756baea2fcdcbb0ccf63d
    Signed-off-by: Yuanyuan Liu <yuanliu@codeaurora.org>

--- include/soc/qcom/icnss.h
+++ include/soc/qcom/icnss.h
@@ -17,6 +17,21 @@
 #define ICNSS_MAX_IRQ_REGISTRATIONS    12
 #define ICNSS_MAX_TIMESTAMP_LEN        32
 
+enum icnss_uevent {
+	ICNSS_UEVENT_FW_READY,
+	ICNSS_UEVENT_FW_CRASHED,
+	ICNSS_UEVENT_FW_DOWN,
+};
+
+struct icnss_uevent_fw_down_data {
+	bool crashed;
+};
+
+struct icnss_uevent_data {
+	enum icnss_uevent uevent;
+	void *data;
+};
+
 struct icnss_driver_ops {
 	char *name;
 	int (*probe)(struct device *dev);
@@ -28,6 +43,7 @@ struct icnss_driver_ops {
 	int (*pm_resume)(struct device *dev);
 	int (*suspend_noirq)(struct device *dev);
 	int (*resume_noirq)(struct device *dev);
+	int (*uevent)(struct device *dev, struct icnss_uevent_data *uevent);
 };
 
 
