4183c077969fc2b1e2ee32e387cba9add186e883
Honghao Liu <honghaol@codeaurora.org>
2017-04-04 17:41:22 -0400

    ASoC: msm: support up to 8 channel capture on MM1
    Update Multimedia1 FE to support up to 8 channel audio capture.
    
    CRs-fixed: 2028896
    Change-Id: I838ecd690fcdeef411d35a5facbe9c40a76b68c3
    Signed-off-by: Honghao Liu <honghaol@codeaurora.org>

--- sound/soc/msm/msm-dai-fe.c
+++ sound/soc/msm/msm-dai-fe.c
@@ -113,7 +113,7 @@ static struct snd_soc_dai_driver msm_fe_dais[] = {
 				    SNDRV_PCM_FMTBIT_S24_3LE |
 				    SNDRV_PCM_FMTBIT_S32_LE),
 			.channels_min = 1,
-			.channels_max = 4,
+			.channels_max = 8,
 			.rate_min =     8000,
 			.rate_max =	48000,
 		},
