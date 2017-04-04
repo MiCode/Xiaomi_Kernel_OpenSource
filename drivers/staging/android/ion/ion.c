dadd3624c17e5fa4bc11d3d14eaeb3cc8c5bf257
Daniel Rosenberg <drosen@google.com>
2017-04-04 14:27:16 -0700

    ANDROID: ion: Fix uninitialized variable
    Fix an uninitialized variable in user_ion_handle_put_nolock
    
    Change-Id: Ia634b790661089ad01aca8e5975984435463d148
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Bug: 34276203
    [d-cagle@codeaurora.org: Add commit message]
    Signed-off-by: Dennis Cagle <d-cagle@codeaurora.org>

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -470,7 +470,7 @@ static struct ion_handle *pass_to_user(struct ion_handle *handle)
 /* Must hold the client lock */
 static int user_ion_handle_put_nolock(struct ion_handle *handle)
 {
-	int ret;
+	int ret = 0;
 
 	if (--handle->user_ref_count == 0)
 		ret = ion_handle_put_nolock(handle);
