296bf9271ef5d8a21c7dd075521a2b894f8a2f07
Yuanyuan Liu <yuanliu@codeaurora.org>
2017-04-04 18:42:06 -0700

    cnss_prealloc: pre-alloc memory leak check on probe failure and remove
    After wlan host driver probe failure and driver unload check the
    memory leak and reset the memory pool.
    
    CRs-Fixed: 2027407
    Change-Id: Id6e413416ec7ea1d1bf0e69aab7eccb06de753d7
    Signed-off-by: Hardik Kantilal Patel <hkpatel@codeaurora.org>
    Signed-off-by: Yuanyuan Liu <yuanliu@codeaurora.org>

--- drivers/net/wireless/cnss_prealloc/cnss_prealloc.c
+++ drivers/net/wireless/cnss_prealloc/cnss_prealloc.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012,2014-2016 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012,2014-2017 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -207,6 +207,8 @@ void wcnss_prealloc_check_memory_leak(void)
 		print_stack_trace(&wcnss_allocs[i].trace, 1);
 	}
 }
+#else
+void wcnss_prealloc_check_memory_leak(void) {}
 #endif
 
 int wcnss_pre_alloc_reset(void)
