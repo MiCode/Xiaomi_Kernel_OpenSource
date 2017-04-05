e6823c3dcc143970060952fb2c486eaea702b9f7
Vinayak Menon <vinmenon@codeaurora.org>
2017-04-05 10:49:14 +0530

    mm: allow page poisoning to be enabled by default
    Add a config option to enable page poisoning by
    default. The kernel command line option "page_poison"
    can be used to change the behaviour during boot.
    
    Change-Id: Ie70763841191a722b1c6125dfad119a29ed0f605
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    Signed-off-by: Charan Teja Reddy <charante@codeaurora.org>
    [swatsrid@codeaurora.org: Fix merge errors and warnings]
    Signed-off-by: Swathi Sridhar <swatsrid@codeaurora.org>
    Signed-off-by: Isaac J. Manjarres <isaacm@codeaurora.org>

--- mm/page_poison.c
+++ mm/page_poison.c
@@ -8,7 +8,8 @@
 #include <linux/ratelimit.h>
 #include <linux/kasan.h>
 
-static bool want_page_poisoning __read_mostly;
+static bool want_page_poisoning __read_mostly
+		= IS_ENABLED(CONFIG_PAGE_POISONING_ENABLE_DEFAULT);
 
 static int __init early_page_poison_param(char *buf)
 {
