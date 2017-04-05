fbbcd07da0d2309bca7e24fbffc1f939b0cf72c6
Sunil Paidimarri <hisunil@codeaurora.org>
2017-04-04 17:43:50 -0700

    msm: ipa: Add new API to check for ipa uC readiness
    Adding new API for IPA clients to check for IPA
    uC ready before bringing up IPA uC offload data path.
    
    CRs-Fixed: 2030217
    Change-Id: I0328658cba829cacc89b7c0b8edf7e52aa16e45c
    Signed-off-by: Sunil Paidimarri <hisunil@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipa_utils.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_utils.c
@@ -4474,6 +4474,8 @@ int ipa3_bind_api_controller(enum ipa_hw_type ipa_hw_type,
 	api_ctrl->ipa_tear_down_uc_offload_pipes =
 		ipa3_tear_down_uc_offload_pipes;
 	api_ctrl->ipa_get_pdev = ipa3_get_pdev;
+	api_ctrl->ipa_ntn_uc_reg_rdyCB = ipa3_ntn_uc_reg_rdyCB;
+	api_ctrl->ipa_ntn_uc_dereg_rdyCB = ipa3_ntn_uc_dereg_rdyCB;
 
 	return 0;
 }
