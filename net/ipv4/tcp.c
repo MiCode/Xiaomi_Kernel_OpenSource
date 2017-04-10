346e2c4a613245593e63978c2d4760883f410dee
Amit Pundir <amit.pundir@linaro.org>
2017-04-10 17:41:21 +0530

    Revert "tcp: Silence warning: ‘in’ may be used uninitialized"
    This reverts commit 2729973c2c520e22c19590318cb6af6cd27d0c95.
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -3209,7 +3209,7 @@ int tcp_nuke_addr(struct net *net, struct sockaddr *addr)
 	int family = addr->sa_family;
 	unsigned int bucket;
 
-	struct in_addr *in = NULL;
+	struct in_addr *in;
 #if defined(CONFIG_IPV6) || defined(CONFIG_IPV6_MODULE)
 	struct in6_addr *in6 = NULL;
 #endif
