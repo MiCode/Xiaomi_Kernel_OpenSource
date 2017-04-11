e69e50400bab362318095d9fc5a0fbbccfa768f0
Satyajit Desai <sadesai@codeaurora.org>
2017-04-11 14:27:25 -0700

    coresight: Remove recursive mutex usage
    Call to reset source sink node leads to us trying to take
    recursive mutex which results in a deadlock. Move mutex call
    to avoid this issue.
    
    Change-Id: Ia1396b08a63fe9d9890c0ef8e6ca5ec4f553b714
    Signed-off-by: Satyajit Desai <sadesai@codeaurora.org>

--- drivers/hwtracing/coresight/coresight.c
+++ drivers/hwtracing/coresight/coresight.c
@@ -578,27 +578,29 @@ err_path:
 }
 EXPORT_SYMBOL_GPL(coresight_enable);
 
-void coresight_disable(struct coresight_device *csdev)
+static void __coresight_disable(struct coresight_device *csdev)
 {
 	int  ret;
 
-	mutex_lock(&coresight_mutex);
-
 	ret = coresight_validate_source(csdev, __func__);
 	if (ret)
-		goto out;
+		return;
 
 	if (!csdev->enable)
-		goto out;
+		return;
 
 	if (csdev->node == NULL)
-		goto out;
+		return;
 
 	coresight_disable_source(csdev);
 	coresight_disable_path(csdev->node->path);
 	coresight_release_path(csdev, csdev->node->path);
+}
 
-out:
+void coresight_disable(struct coresight_device *csdev)
+{
+	mutex_lock(&coresight_mutex);
+	__coresight_disable(csdev);
 	mutex_unlock(&coresight_mutex);
 }
 EXPORT_SYMBOL_GPL(coresight_disable);
@@ -904,7 +906,7 @@ static ssize_t reset_source_sink_store(struct bus_type *bus,
 		csdev = coresight_get_source(cspath->path);
 		if (!csdev)
 			continue;
-		coresight_disable(csdev);
+		__coresight_disable(csdev);
 	}
 
 	mutex_unlock(&coresight_mutex);
