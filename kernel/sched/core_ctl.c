53653c20ddaed61581b0f1ac382330f8cd1f0cd3
Pavankumar Kondeti <pkondeti@codeaurora.org>
2017-04-13 18:44:24 +0530

    core_ctl: Take state_lock while preparing the global_state
    global_state file output is prepared by reading the members of CPU
    and cluster data structures. These members are updated under state_lock,
    so acquire the same lock while preparing the output.
    
    Change-Id: I8cb3ca6f9100a24f0bc9b293d364ad6aa8aabefb
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- kernel/sched/core_ctl.c
+++ kernel/sched/core_ctl.c
@@ -260,6 +260,7 @@ static ssize_t show_global_state(const struct cluster_data *state, char *buf)
 	ssize_t count = 0;
 	unsigned int cpu;
 
+	spin_lock_irq(&state_lock);
 	for_each_possible_cpu(cpu) {
 		c = &per_cpu(cpu_state, cpu);
 		cluster = c->cluster;
@@ -295,6 +296,7 @@ static ssize_t show_global_state(const struct cluster_data *state, char *buf)
 		count += snprintf(buf + count, PAGE_SIZE - count,
 				"\tBoost: %u\n", (unsigned int) cluster->boost);
 	}
+	spin_unlock_irq(&state_lock);
 
 	return count;
 }
