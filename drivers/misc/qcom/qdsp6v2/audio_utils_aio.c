80383c9bcfcf967147964dfcb8925aedadd98c93
kunleiz <kunleiz@codeaurora.org>
2017-04-14 10:28:42 +0800

    ASoC: msm: qdspv2: add result check when audio process fail
    A audio_process_event_req is not always to success. Therefore,
    check the return value for audio_process_event_req, and
    initializ usr_evt before using it.
    
    CRs-Fixed: 2029798
    Change-Id: I4adf682575f5f9233a1a1a533f9c6361af8a5bcf
    Signed-off-by: kunleiz <kunleiz@codeaurora.org>

--- drivers/misc/qcom/qdsp6v2/audio_utils_aio.c
+++ drivers/misc/qcom/qdsp6v2/audio_utils_aio.c
@@ -851,6 +851,7 @@ static long audio_aio_process_event_req_compat(struct q6audio_aio *audio,
 	long rc;
 	struct msm_audio_event32 usr_evt_32;
 	struct msm_audio_event usr_evt;
+	memset(&usr_evt, 0, sizeof(struct msm_audio_event));
 
 	if (copy_from_user(&usr_evt_32, arg,
 				sizeof(struct msm_audio_event32))) {
@@ -860,6 +861,11 @@ static long audio_aio_process_event_req_compat(struct q6audio_aio *audio,
 	usr_evt.timeout_ms = usr_evt_32.timeout_ms;
 
 	rc = audio_aio_process_event_req_common(audio, &usr_evt);
+	if (rc < 0) {
+		pr_err("%s: audio process event failed, rc = %ld",
+			__func__, rc);
+		return rc;
+	}
 
 	usr_evt_32.event_type = usr_evt.event_type;
 	switch (usr_evt_32.event_type) {
