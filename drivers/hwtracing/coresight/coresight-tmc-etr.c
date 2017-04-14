0186c4d01bdf8c6549ef4e652fa594378d4b08a8
Satyajit Desai <sadesai@codeaurora.org>
2017-04-13 19:14:31 -0700

    coresight-tmc-etr: update higher order bits of trace buffer
    ETR locates the trace location in system memory by using two
    different register to obtain the full 40-bit address range.
    This patch add capability for us to program the higher
    8-bits.
    
    Change-Id: I70128a3ad59157cc7728d024c19d8ebf3791e26a
    Signed-off-by: Satyajit Desai <sadesai@codeaurora.org>
    Signed-off-by: Rama Aparna Mallavarapu <aparnam@codeaurora.org>
    Signed-off-by: Mulu He <muluhe@codeaurora.org>

--- drivers/hwtracing/coresight/coresight-tmc-etr.c
+++ drivers/hwtracing/coresight/coresight-tmc-etr.c
@@ -948,6 +948,10 @@ static void tmc_etr_enable_hw(struct tmc_drvdata *drvdata)
 		writel_relaxed(sts, drvdata->base + TMC_STS);
 	}
 
+	writel_relaxed(etr_buf->hwaddr, drvdata->base + TMC_DBALO);
+	writel_relaxed(((u64)etr_buf->hwaddr >> 32) & 0xFF,
+		       drvdata->base + TMC_DBAHI);
+
 	writel_relaxed(TMC_FFCR_EN_FMT | TMC_FFCR_EN_TI |
 		       TMC_FFCR_FON_FLIN | TMC_FFCR_FON_TRIG_EVT |
 		       TMC_FFCR_TRIGON_TRIGIN,
