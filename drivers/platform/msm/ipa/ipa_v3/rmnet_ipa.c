4d22ed9ae54d41537345b793c8ddce01dc1b2baa
Skylar Chang <chiaweic@codeaurora.org>
2017-04-14 19:23:05 -0700

    msm: rmnet_ipa: fix security issue
    Fix the security issue where mux channel name might
    not be null-terminated in ipa wan driver.
    
    Change-Id: I3ef440b62cf3861464fb60c1e7f65f2be5e39ed0
    Acked-by: Shihuan Liu <shihuanl@qti.qualcomm.com>
    Signed-off-by: Skylar Chang <chiaweic@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/rmnet_ipa.c
+++ drivers/platform/msm/ipa/ipa_v3/rmnet_ipa.c
@@ -1442,6 +1442,9 @@ static int ipa3_wwan_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
 				extend_ioctl_data.u.rmnet_mux_val.vchannel_name,
 				sizeof(mux_channel[rmnet_index]
 					.vchannel_name));
+			mux_channel[rmnet_index].vchannel_name[
+				IFNAMSIZ - 1] = '\0';
+
 			IPAWANDBG("cashe device[%s:%d] in IPA_wan[%d]\n",
 				mux_channel[rmnet_index].vchannel_name,
 				mux_channel[rmnet_index].mux_id,
