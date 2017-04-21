872b29b9ecf8e34c71fbf652a2e24707cf4ac263
Amit Pundir <amit.pundir@linaro.org>
2017-04-21 09:39:00 +0530

    ANDROID: cpufreq_interactive: Add missing includes
    Cpufreq interfaces have been moved to <linux/sched/cpufreq.h>,
    see upstream commit 7284c6d419b5 ("sched/headers: Move the
    cpufreq interfaces to <linux/sched/cpufreq.h>") for reference.
    
    Similarly upstream commit cda66725c144 ("sched/headers: Move the
    get_task_struct()/put_task_struct() and related APIs from
    <linux/sched.h> to <linux/sched/task.h>", moved [get,put]_task_struct()
    APIs as well.
    
    Also drop <linux/sched.h> which is no longer required, upstream
    commit ed53742d793b ("sched/headers: Remove <linux/sched.h>
    from <linux/sched/cpufreq.h>").
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -24,8 +24,9 @@
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/rwsem.h>
-#include <linux/sched.h>
+#include <linux/sched/cpufreq.h>
 #include <linux/sched/rt.h>
+#include <linux/sched/task.h>
 #include <linux/tick.h>
 #include <linux/time.h>
 #include <linux/timer.h>
