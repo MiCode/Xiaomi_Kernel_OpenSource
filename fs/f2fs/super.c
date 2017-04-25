3b96134d5b6df7196e4b7dba1a398e25a5c5184f
Jin Qian <jinqian@google.com>
2017-04-25 16:28:48 -0700

    f2fs: sanity check segment count
    F2FS uses 4 bytes to represent block address. As a result, supported
    size of disk is 16 TB and it equals to 16 * 1024 * 1024 / 2 segments.
    
    Signed-off-by: Jin Qian <jinqian@google.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    [dcagle@codeaurora.org: Resolve merge conflict]
    Change-Id: I64d9be6dcc4ab5d8a45359c69d6163ed0209e8fe
    Git-repo: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
    Git-commit: b9dd46188edc2f0d1f37328637860bb65a771124
    Signed-off-by: Dennis Cagle <dcagle@codeaurora.org>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -819,6 +819,13 @@ static int sanity_check_raw_super(struct super_block *sb,
 			le32_to_cpu(raw_super->log_sectorsize));
 		return 1;
 	}
+
+	if (le32_to_cpu(raw_super->segment_count) > F2FS_MAX_SEGMENT) {
+		f2fs_msg(sb, KERN_INFO,
+			"Invalid segment count (%u)",
+			le32_to_cpu(raw_super->segment_count));
+		return 1;
+	}
 	return 0;
 }
 
