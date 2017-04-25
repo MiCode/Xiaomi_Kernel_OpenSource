36b44c11d1ea924ab08965a0ce3545ad8a1e44cb
Dmitry Shmidt <dimitrysh@google.com>
2017-04-25 12:44:25 -0700

    Revert "cgroup: Change from CAP_SYS_NICE to CAP_SYS_RESOURCE for cgroup migration permissions"
    This reverts commit a274cb7af2e36349d72330220df727517926e610.
    
    Change-Id: I3e89ac4757638b30f404004e141209741289f793
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2365,7 +2365,7 @@ retry_find_task:
 		if (!uid_eq(cred->euid, GLOBAL_ROOT_UID) &&
 		    !uid_eq(cred->euid, tcred->uid) &&
 		    !uid_eq(cred->euid, tcred->suid) &&
-		    !ns_capable(tcred->user_ns, CAP_SYS_RESOURCE)) {
+		    !ns_capable(tcred->user_ns, CAP_SYS_NICE)) {
 			rcu_read_unlock();
 			ret = -EACCES;
 			goto out_unlock_cgroup;
