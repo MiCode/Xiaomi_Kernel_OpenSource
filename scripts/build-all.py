37c5e5a53fdcca22fb48e3b6edf4625dcdcac37e
Runmin Wang <runminw@codeaurora.org>
2017-04-27 11:40:04 -0700

    scripts: build_all: build sdx defconfigs
    Add support for building new sdx defconfigs.
    
    Change-Id: I81c0ee59aab46f131609dd99b7db798460a19f01
    Signed-off-by: Runmin Wang <runminw@codeaurora.org>

--- scripts/build-all.py
+++ scripts/build-all.py
@@ -307,10 +307,12 @@ def scan_configs():
         r'qsd*_defconfig',
         r'mpq*_defconfig',
         r'sdm[0-9]*_defconfig',
+        r'sdx*_defconfig',
         )
     arch64_pats = (
         r'msm*_defconfig',
         r'sdm[0-9]*_defconfig',
+        r'sdx*_defconfig',
         )
     for p in arch_pats:
         for n in glob.glob('arch/arm/configs/' + p):
