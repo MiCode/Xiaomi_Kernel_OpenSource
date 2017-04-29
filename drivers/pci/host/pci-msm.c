4e96978e66cbae001c7cc068a867d606f34e670b
Tony Truong <truong@codeaurora.org>
2017-04-28 18:17:04 -0700

    msm: pcie: switch GPIO to sleep state before asserting PERST
    Before asserting PERST, PCIe bus driver should switch GPIO to
    sleep state. This will prevent host from missing any events after
    the link is off.
    
    Change-Id: Ie57d339da02dde6cbb3c41d2be52ee0602227193
    Signed-off-by: Tony Truong <truong@codeaurora.org>

--- drivers/pci/host/pci-msm.c
+++ drivers/pci/host/pci-msm.c
@@ -6764,11 +6764,11 @@ static int msm_pcie_pm_suspend(struct pci_dev *dev,
 		PCIE_DBG(pcie_dev, "RC%d: PM_Enter_L23 is NOT received\n",
 			pcie_dev->rc_idx);
 
-		msm_pcie_disable(pcie_dev, PM_PIPE_CLK | PM_CLK | PM_VREG);
-
 	if (pcie_dev->use_pinctrl && pcie_dev->pins_sleep)
 		pinctrl_select_state(pcie_dev->pinctrl,
 					pcie_dev->pins_sleep);
+
+	msm_pcie_disable(pcie_dev, PM_PIPE_CLK | PM_CLK | PM_VREG);
 
 	PCIE_DBG(pcie_dev, "RC%d: exit\n", pcie_dev->rc_idx);
 
