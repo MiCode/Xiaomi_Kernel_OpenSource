dc6c7855de6a10d05ae156713384c4eda35104b9
Anirudh Ghayal <aghayal@codeaurora.org>
2017-05-02 16:15:27 +0530

    drivers: regulator: Add snapshot of OnSemi NCP6335D regulator
    This is snapshot of the OnSemi NCP6335D driver as of msm-3.10
    'commit 156ba1726643 ("regulator: onsemi-ncp6335d: Add i2c retry logic")'.
    
    Change-Id: I4cf0acd272fcf498462d4397385cd62f144eadf8
    Signed-off-by: Kiran Gunda <kgunda@codeaurora.org>

---
+++ include/linux/regulator/onsemi-ncp6335d.h
@@ -0,0 +1,35 @@
+/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __NCP6335D_H__
+#define __NCP6335D_H__
+
+enum {
+	NCP6335D_VSEL0,
+	NCP6335D_VSEL1,
+};
+
+struct ncp6335d_platform_data {
+	struct regulator_init_data *init_data;
+	int default_vsel;
+	int slew_rate_ns;
+	int discharge_enable;
+	bool sleep_enable;
+};
+
+#ifdef CONFIG_REGULATOR_ONSEMI_NCP6335D
+int __init ncp6335d_regulator_init(void);
+#else
+static inline int __init ncp6335d_regulator_init(void) { return 0; }
+#endif
+
+#endif
