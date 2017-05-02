92fc3b042b03fbc7dab0b74a469301f7ffe1a6f5
Praneeth Paladugu <ppaladug@codeaurora.org>
2017-05-02 15:35:17 -0700

    msm: vidc: Enhance debug message for control
    Enhance debug message for control to include control name, ID and
    HAL property to make debugging easy.
    
    CRs-Fixed: 2016190
    Change-Id: I7103c078f645a9a0abb55ebb746f48fd717eec8c
    Signed-off-by: Praneeth Paladugu <ppaladug@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_venc.c
+++ drivers/media/platform/msm/vidc/msm_venc.c
@@ -1868,9 +1868,9 @@ int msm_venc_s_ctrl(struct msm_vidc_inst *inst, struct v4l2_ctrl *ctrl)
 #undef TRY_GET_CTRL
 
 	if (!rc && property_id) {
-		dprintk(VIDC_DBG, "Control: HAL property=%x,ctrl_value=%d\n",
-				property_id,
-				ctrl->val);
+		dprintk(VIDC_DBG,
+			"Control: Name = %s, ID = 0x%x Value = %d\n",
+				ctrl->name, ctrl->id, ctrl->val);
 		rc = call_hfi_op(hdev, session_set_property,
 				(void *)inst->session, property_id, pdata);
 	}
