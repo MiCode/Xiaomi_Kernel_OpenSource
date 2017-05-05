ac8f754d2246963e20623f7ee08eb9f94df0605a
Satyajit Desai <sadesai@codeaurora.org>
2017-05-04 17:20:59 -0700

    coresight-hwevent: Change naming convention for clock
    Make use of amba framework naming convention for hw event clock.
    With this change we will be setting the clock rate for all devices
    as part of coresight register. Clock enable and disable still
    follow the existing framework.
    
    Change-Id: I54bcb31db288d3e411bfb5ea0a80fcafdb71512d
    Signed-off-by: Satyajit Desai <sadesai@codeaurora.org>

--- include/linux/coresight.h
+++ include/linux/coresight.h
@@ -41,13 +41,6 @@
 
 extern struct bus_type coresight_bustype;
 
-enum coresight_clk_rate {
-	CORESIGHT_CLK_RATE_OFF,
-	CORESIGHT_CLK_RATE_TRACE = 1000,
-	CORESIGHT_CLK_RATE_HSTRACE = 2000,
-	CORESIGHT_CLK_RATE_FIXED = 3000,
-};
-
 enum coresight_dev_type {
 	CORESIGHT_DEV_TYPE_NONE,
 	CORESIGHT_DEV_TYPE_SINK,
