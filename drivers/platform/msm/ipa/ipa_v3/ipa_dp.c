4cdd5c7260eeb278a58ae0bc0763c7bca39ac6c2
Skylar Chang <chiaweic@codeaurora.org>
2017-05-05 16:31:32 -0700

    msm: ipa: apply reset wa to GPI channels
    When resetting a GSI channel a special handing is needed
    if there is an open aggregation frame. This handling is applied
    to USB and MHI channels. This commit applies this handling to
    GPI (system) channels as well
    
    Change-Id: Iebdf3d7375a4bd584c75503f01e64d7106f25e5a
    CRs-Fixed: 2029089
    Acked-by: Ady Abraham <adya@qti.qualcomm.com>
    Signed-off-by: Skylar Chang <chiaweic@codeaurora.org>
    Signed-off-by: Sravanthi Palakonda <srapal@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
@@ -1557,7 +1557,7 @@ int ipa3_teardown_sys_pipe(u32 clnt_hdl)
 			BUG();
 			return result;
 		}
-		result = gsi_reset_channel(ep->gsi_chan_hdl);
+		result = ipa3_reset_gsi_channel(clnt_hdl);
 		if (result != GSI_STATUS_SUCCESS) {
 			IPAERR("Failed to reset chan: %d.\n", result);
 			BUG();
