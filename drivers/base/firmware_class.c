7ca3fa51b2ad4c2da41c9086c7bd5391f914d567
Kyle Yan <kyan@codeaurora.org>
2017-05-09 15:40:17 -0700

    firmware: Reword error messages when kernel is unable to find firmware
    The current error message printed when kernel fails to find the firmware
    suggests that the entire operation has failed when the user helper may
    still find the firmware. As we pass the return code back up, let the
    calling driver determine if the entire operation has failed or not.
    
    Change-Id: I5d8ce65e533e1925e98a49a0a88280549af75cb9
    Signed-off-by: Kyle Yan <kyan@codeaurora.org>
    Signed-off-by: Prasad Sodagudi <psodagud@codeaurora.org>

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -1253,11 +1253,11 @@ _request_firmware(const struct firmware **firmware_p, const char *name,
 	ret = fw_get_filesystem_firmware(device, fw->priv);
 	if (ret) {
 		if (!(opt_flags & FW_OPT_NO_WARN))
-			dev_warn(device,
-				 "Direct firmware load for %s failed with error %d\n",
+			dev_dbg(device,
+				 "Firmware %s was not found in kernel paths. rc:%d\n",
 				 name, ret);
 		if (opt_flags & FW_OPT_USERHELPER) {
-			dev_warn(device, "Falling back to user helper\n");
+			dev_dbg(device, "Falling back to user helper\n");
 			ret = fw_load_from_user_helper(fw, name, device,
 						       opt_flags);
 		}
