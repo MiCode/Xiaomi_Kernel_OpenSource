b207408120e838655f34902824b23f260a5d36de
Vikram Mulukutla <markivx@codeaurora.org>
2017-05-08 19:20:22 -0700

    sched: cpufreq: Use sched_clock instead of rq_clock when updating schedutil
    rq_clock may not be updated often enough for schedutil or other
    cpufreq governors to work correctly when it's passed as the
    timestamp for a load report. Use sched_clock instead.
    
    Change-Id: I745b727870a31da25f766c2c2f37527f568c20da
    Signed-off-by: Vikram Mulukutla <markivx@codeaurora.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2158,7 +2158,7 @@ static inline void cpufreq_update_util(struct rq *rq, unsigned int flags)
 
 	data = rcu_dereference_sched(*this_cpu_ptr(&cpufreq_update_util_data));
 	if (data)
-		data->func(data, rq_clock(rq), flags);
+		data->func(data, sched_clock(), flags);
 }
 
 static inline void cpufreq_update_this_cpu(struct rq *rq, unsigned int flags)
