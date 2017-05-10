a9a212a939617d0bcdbb2033e44c6d95215d480a
Satyajit Desai <sadesai@codeaurora.org>
2017-05-10 11:58:50 -0700

    coresight: tmc: Add usb support for coresight
    Add support to enable USB mode for coresight devices.
    Set memory as the default mode on ETR.
    
    Change-Id: Id804fb819ad19b589a1b978ea0cd65b0265a9268
    Signed-off-by: Satyajit Desai <sadesai@codeaurora.org>

--- drivers/hwtracing/coresight/coresight-tmc.c
+++ drivers/hwtracing/coresight/coresight-tmc.c
@@ -540,6 +540,7 @@ static int tmc_probe(struct amba_device *adev, const struct amba_id *id)
 			drvdata->memtype  = TMC_ETR_MEM_TYPE_CONTIG;
 		drvdata->mem_size = drvdata->size;
 		drvdata->mem_type = drvdata->memtype;
+		drvdata->out_mode = TMC_ETR_OUT_MODE_MEM;
 	} else {
 		drvdata->size = readl_relaxed(drvdata->base + TMC_RSZ) * 4;
 	}
