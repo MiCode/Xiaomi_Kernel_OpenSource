43a2e2bf047ea590cac11c65e4fa6ad5987b84bc
Prateek Sood <prsood@codeaurora.org>
2017-05-11 17:02:29 +0530

    hotplug cpu: ratelimit logs for thermal veto
    Thermal notifier callback is not allowing CPU
    to come online. Rate limit logs to avoid watchdog
    non-secure bite as it is a valid rejection due to
    high temperature of SOC.
    
    Change-Id: If3f8df7370e6ffd18b50e7451431d6a26023359d
    Signed-off-by: Prateek Sood <prsood@codeaurora.org>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -498,8 +498,8 @@ static int _cpu_up(unsigned int cpu, int tasks_frozen)
 	ret = __cpu_notify(CPU_UP_PREPARE | mod, hcpu, -1, &nr_calls);
 	if (ret) {
 		nr_calls--;
-		pr_warn("%s: attempt to bring up CPU %u failed\n",
-			__func__, cpu);
+		pr_warn_ratelimited("%s: attempt to bring up CPU %u failed\n",
+				    __func__, cpu);
 		goto out_notify;
 	}
 
