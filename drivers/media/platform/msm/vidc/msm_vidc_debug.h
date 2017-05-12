0ec1c7415f05eccdd917a7fedb5686bd84f6513d
Abdulla Anam <abdullahanam@codeaurora.org>
2017-05-12 19:47:58 +0530

    msm: vidc: Allocate memory dynamically for debugfs info_reads
    Use dynamically allocated memory for constructing strings in
    core_info_read & inst_info_read. This ensures that there is no
    contention for a shared memory & hence avoids the requirement of
    a lock. Allocate on demand, as the calls implement a debugfs
    facility and hence rarely invoked. Statically allocated memory
    otherwise remain idle.
    
    Change-Id: I3ae04e0a51801a2fc901591e41e28ff6b7d198b4
    Signed-off-by: Abdulla Anam <abdullahanam@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc_debug.h
+++ drivers/media/platform/msm/vidc/msm_vidc_debug.h
@@ -124,9 +124,9 @@ struct dentry *msm_vidc_debugfs_init_core(struct msm_vidc_core *core,
 		struct dentry *parent);
 struct dentry *msm_vidc_debugfs_init_inst(struct msm_vidc_inst *inst,
 		struct dentry *parent);
+void msm_vidc_debugfs_deinit_inst(struct msm_vidc_inst *inst);
 void msm_vidc_debugfs_update(struct msm_vidc_inst *inst,
 		enum msm_vidc_debugfs_event e);
-void msm_vidc_debugfs_deinit_drv(void);
 
 static inline void tic(struct msm_vidc_inst *i, enum profiling_points p,
 				 char *b)
