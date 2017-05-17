10fe78d7721fb050f0fb858052097595daf24c6b
Sachin Bhayare <sachin.bhayare@codeaurora.org>
2017-05-17 10:55:40 +0530

    msm: mdss: disable AVR for msm8998
    Disable AVR as dynamic fps is enabled for msm8998.
    
    Change-Id: I4d96984c45de83868f2ea4d324a598edf6d30e94
    Signed-off-by: Sachin Bhayare <sachin.bhayare@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_mdp.c
+++ drivers/video/fbdev/msm/mdss_mdp.c
@@ -2180,7 +2180,6 @@ static void mdss_mdp_hw_rev_caps_init(struct mdss_data_type *mdata)
 		mdss_set_quirk(mdata, MDSS_QUIRK_MDP_CLK_SET_RATE);
 		mdata->has_wb_ubwc = true;
 		set_bit(MDSS_CAPS_10_BIT_SUPPORTED, mdata->mdss_caps_map);
-		set_bit(MDSS_CAPS_AVR_SUPPORTED, mdata->mdss_caps_map);
 		set_bit(MDSS_CAPS_SEC_DETACH_SMMU, mdata->mdss_caps_map);
 		mdss_set_quirk(mdata, MDSS_QUIRK_HDR_SUPPORT_ENABLED);
 		break;
