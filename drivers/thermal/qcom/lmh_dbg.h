1d1adbb1eb7375227c7fa2a528dd730550b0de7a
Ram Chandrasekar <rkumbako@codeaurora.org>
2017-05-18 17:44:50 -0600

    drivers: lmh-dcvsh: Add debug support
    LMH DCVSh hardware can provide debug information. Provide a generic
    debugfs interface in LMH DCVSh driver to support querying the
    hardware for different information using a different command formats.
    
    Change-Id: I10053a224e79f549ec68f9c39d652e7668b34ffc
    Signed-off-by: Ram Chandrasekar <rkumbako@codeaurora.org>

---
+++ drivers/thermal/qcom/lmh_dbg.h
@@ -0,0 +1,20 @@
+/* Copyright (c) 2014-2017, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __QTI_LMH_H__
+#define __QTI_LMH_H__
+
+#include <linux/platform_device.h>
+
+int lmh_debug_register(struct platform_device *pdev);
+
+#endif /* __QTI_LMH_H__ */
