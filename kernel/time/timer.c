7650c440a817429453db8c2c295806790b51d5aa
Prasad Sodagudi <psodagud@codeaurora.org>
2017-05-17 23:26:09 -0700

    sched: Add a check for cpu unbound deferrable timers
    Add a check for cpu unbound deferrable timer expiry and raise
    softirq for handling the expired timers so that the CPU can
    process the cpu unbound deferrable times as early as possible
    when a cpu tries to enter/exit idle loop.
    
    Change-Id: Ieffa74fa22a4d25493f5590b5ac1e0d784fcbbad
    Signed-off-by: Prasad Sodagudi <psodagud@codeaurora.org>
    [rishabhb@codeaurora.org: Resolved conflicts due to addition of
    commit: time: tick-sched: Reorganize idle tick management code]
    Signed-off-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>
    Signed-off-by: Elliot Berman <eberman@codeaurora.org>

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -207,6 +207,7 @@ struct timer_base {
 
 static DEFINE_PER_CPU(struct timer_base, timer_bases[NR_BASES]);
 struct timer_base timer_base_deferrable;
+static atomic_t deferrable_pending;
 
 #ifdef CONFIG_NO_HZ_COMMON
 
@@ -1518,6 +1519,31 @@ static u64 cmp_next_hrtimer_event(u64 basem, u64 expires)
 	return DIV_ROUND_UP_ULL(nextevt, TICK_NSEC) * TICK_NSEC;
 }
 
+
+#ifdef CONFIG_SMP
+/*
+ * check_pending_deferrable_timers - Check for unbound deferrable timer expiry
+ * @cpu - Current CPU
+ *
+ * The function checks whether any global deferrable pending timers
+ * are exipired or not. This function does not check cpu bounded
+ * diferrable pending timers expiry.
+ *
+ * The function returns true when a cpu unbounded deferrable timer is expired.
+ */
+bool check_pending_deferrable_timers(int cpu)
+{
+	if (cpu == tick_do_timer_cpu ||
+		tick_do_timer_cpu == TICK_DO_TIMER_NONE) {
+		if (time_after_eq(jiffies, timer_base_deferrable.clk)
+			&& !atomic_cmpxchg(&deferrable_pending, 0, 1)) {
+			return true;
+		}
+	}
+	return false;
+}
+#endif
+
 /**
  * get_next_timer_interrupt - return the time (clock mono) of the next timer
  * @basej:	base time jiffies
@@ -1704,9 +1730,13 @@ static __latent_entropy void run_timer_softirq(struct softirq_action *h)
 
 	__run_timers(base);
 	if (IS_ENABLED(CONFIG_NO_HZ_COMMON)) {
-		__run_timers(&timer_base_deferrable);
 		__run_timers(this_cpu_ptr(&timer_bases[BASE_DEF]));
 	}
+
+	if ((atomic_cmpxchg(&deferrable_pending, 1, 0) &&
+		tick_do_timer_cpu == TICK_DO_TIMER_NONE) ||
+		tick_do_timer_cpu == smp_processor_id())
+		__run_timers(&timer_base_deferrable);
 }
 
 /*
