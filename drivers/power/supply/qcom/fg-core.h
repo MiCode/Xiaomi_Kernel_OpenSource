6124f7574427070e5772366e3ceb53151303104f
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2017-05-17 17:43:22 -0700

    qpnp-fg-gen3: configure ESR timers dynamically based on charge termination
    With ESR discharge timer retry, max having same value, ESR pulse
    can fire frequently when battery FET is open past charge
    termination. This frequent ESR pulses increase power consumption
    when the device is idle.
    
    Upon charge termination, change the ESR discharging timer retry
    to 0 so that ESR pulse can fire less frequently saving power.
    
    Add support for this through fg_esr_timer_config() based on end
    of charge and sleep conditions.
    
    Currently, retry/max for ESR charging and discharging timers are
    configured with the same value specified through device tree.
    Extend the device tree property to take in different retry/max
    values for those timers.
    
    Change-Id: Ib30b8ae7d893b5cab00ed83dd7318b53e3b63ac1
    Signed-off-by: Subbaraman Narayanamurthy <subbaram@codeaurora.org>

--- drivers/power/supply/qcom/fg-core.h
+++ drivers/power/supply/qcom/fg-core.h
@@ -219,6 +219,12 @@ enum slope_limit_status {
 	SLOPE_LIMIT_NUM_COEFFS,
 };
 
+enum esr_timer_config {
+	TIMER_RETRY = 0,
+	TIMER_MAX,
+	NUM_ESR_TIMERS,
+};
+
 /* DT parameters for FG device */
 struct fg_dt_props {
 	bool	force_load_profile;
@@ -234,9 +240,9 @@ struct fg_dt_props {
 	int	recharge_soc_thr;
 	int	recharge_volt_thr_mv;
 	int	rsense_sel;
-	int	esr_timer_charging;
-	int	esr_timer_awake;
-	int	esr_timer_asleep;
+	int	esr_timer_charging[NUM_ESR_TIMERS];
+	int	esr_timer_awake[NUM_ESR_TIMERS];
+	int	esr_timer_asleep[NUM_ESR_TIMERS];
 	int	rconn_mohms;
 	int	esr_clamp_mohms;
 	int	cl_start_soc;
@@ -385,6 +391,7 @@ struct fg_chip {
 	int			maint_soc;
 	int			delta_soc;
 	int			last_msoc;
+	int			esr_timer_charging_default[NUM_ESR_TIMERS];
 	enum slope_limit_status	slope_limit_sts;
 	bool			profile_available;
 	bool			profile_loaded;
