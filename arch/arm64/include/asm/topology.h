55e95f99e7e00510b09fa384c3f1b40ea2c363b4
Chris Redpath <chris.redpath@arm.com>
2017-05-25 11:17:05 +0100

    arm64/topology: link arch_scale_min_freq_capacity to cpufreq
    Cpufreq tracks minimum frequency constraints per-cpu.
    Link this to the generic arch_scale_min_freq_capacity which is
    used in the scheduler.
    
    Change-Id: I9f4f838bc7ebf929947ccc9e729e5ab24728e81c
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>

--- arch/arm64/include/asm/topology.h
+++ arch/arm64/include/asm/topology.h
@@ -37,6 +37,8 @@ struct sched_domain;
 extern unsigned long cpufreq_scale_freq_capacity(struct sched_domain *sd, int cpu);
 #define arch_scale_max_freq_capacity cpufreq_scale_max_freq_capacity
 extern unsigned long cpufreq_scale_max_freq_capacity(struct sched_domain *sd, int cpu);
+#define arch_scale_min_freq_capacity cpufreq_scale_min_freq_capacity
+extern unsigned long cpufreq_scale_min_freq_capacity(struct sched_domain *sd, int cpu);
 #endif
 #define arch_scale_cpu_capacity scale_cpu_capacity
 extern unsigned long scale_cpu_capacity(struct sched_domain *sd, int cpu);
