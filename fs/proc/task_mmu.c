e65af6b1bf857738e5f24729aecbc0423fe255ef
Vinayak Menon <vinmenon@codeaurora.org>
2017-05-26 16:29:55 +0530

    mm: process reclaim: skip locked vmas
    Skip VMA_LOCKED vmas from being reclaimed. Not doing so results
    in pages in unevictable lrus to be passed for reclaim which can
    cause issues. In one such reported problem, the trylock_page in
    shrink_page_list fails and reclaim path decides to keep the page
    and hits VM_BUG_ON_PAGE since PageUnevictable is set.
    
    Change-Id: I58947faeb5226cf8578541d7fdcd474a24f8c40f
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1617,6 +1617,9 @@ struct reclaim_param reclaim_task_anon(struct task_struct *task,
 		if (vma->vm_file)
 			continue;
 
+		if (vma->vm_flags & VM_LOCKED)
+			continue;
+
 		if (!rp.nr_to_reclaim)
 			break;
 
