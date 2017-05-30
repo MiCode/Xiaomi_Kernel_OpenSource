21315046164d5afc67454460919a18bd4bd4e4ab
Deepak Katragadda <dkatraga@codeaurora.org>
2017-05-30 15:21:44 -0700

    ARM: dts: msm: Add dummy clock support for SDM855
    Add the dummy clock nodes and the headers for clients to
    use to get their respective clock handles.
    
    Change-Id: Ia7def37be29e1a658a4b1bc03b7c971eb4925c91
    Signed-off-by: Deepak Katragadda <dkatraga@codeaurora.org>

---
+++ include/dt-bindings/clock/qcom,cpucc-sdm855.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2017, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _DT_BINDINGS_CLK_QCOM_CPU_CC_SDM855_H
+#define _DT_BINDINGS_CLK_QCOM_CPU_CC_SDM855_H
+
+#define L3_CLUSTER0_VOTE_CLK					0
+#define L3_CLUSTER1_VOTE_CLK					1
+#define L3_CLK							2
+#define CPU0_PWRCL_CLK						3
+#define CPU1_PWRCL_CLK						4
+#define CPU2_PWRCL_CLK						5
+#define CPU3_PWRCL_CLK						6
+#define PWRCL_CLK						7
+#define CPU4_PERFCL_CLK						8
+#define CPU5_PERFCL_CLK						9
+#define CPU6_PERFCL_CLK						10
+#define CPU7_PERFCL_CLK						11
+#define PERFCL_CLK						12
+
+#endif
---
+++ include/dt-bindings/clock/qcom,gpucc-sdm855.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2017, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _DT_BINDINGS_CLK_QCOM_GPU_CC_SDM855_H
+#define _DT_BINDINGS_CLK_QCOM_GPU_CC_SDM855_H
+
+#define GPU_CC_ACD_AHB_CLK					0
+#define GPU_CC_ACD_CXO_CLK					1
+#define GPU_CC_AHB_CLK						2
+#define GPU_CC_CRC_AHB_CLK					3
+#define GPU_CC_CX_APB_CLK					4
+#define GPU_CC_CX_GFX3D_CLK					5
+#define GPU_CC_CX_GFX3D_SLV_CLK					6
+#define GPU_CC_CX_GMU_CLK					7
+#define GPU_CC_CX_QDSS_AT_CLK					8
+#define GPU_CC_CX_QDSS_TRIG_CLK					9
+#define GPU_CC_CX_QDSS_TSCTR_CLK				10
+#define GPU_CC_CX_SNOC_DVM_CLK					11
+#define GPU_CC_CXO_AON_CLK					12
+#define GPU_CC_CXO_CLK						13
+#define GPU_CC_GMU_CLK_SRC					14
+#define GPU_CC_GX_CXO_CLK					15
+#define GPU_CC_GX_GFX3D_CLK					16
+#define GPU_CC_GX_GMU_CLK					17
+#define GPU_CC_GX_QDSS_TSCTR_CLK				18
+#define GPU_CC_GX_VSENSE_CLK					19
+#define GPU_CC_PLL1						20
+#define GPU_CC_PLL_TEST_CLK					21
+#define GPU_CC_RBCPR_AHB_CLK					22
+#define GPU_CC_RBCPR_CLK					23
+#define GPU_CC_RBCPR_CLK_SRC					24
+#define GPU_CC_SLEEP_CLK					25
+
+#define GPUCC_GPU_CC_ACD_BCR					0
+#define GPUCC_GPU_CC_CX_BCR					1
+#define GPUCC_GPU_CC_GFX3D_AON_BCR				2
+#define GPUCC_GPU_CC_GMU_BCR					3
+#define GPUCC_GPU_CC_GX_BCR					4
+#define GPUCC_GPU_CC_RBCPR_BCR					5
+#define GPUCC_GPU_CC_SPDM_BCR					6
+#define GPUCC_GPU_CC_XO_BCR					7
+
+#endif
