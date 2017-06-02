a8928b2c6dc64a41941a4d776d30db8f4fff7cfd
Satyajit Desai <sadesai@codeaurora.org>
2017-06-01 18:41:33 -0700

    coresight-tmc: Re-use ETR buffer across use cases
    We can run into use after free scenario for the buffer memory
    if enable and read operations happen simultaneously. Add mutex
    protection in order to prevent such a scenario.
    
    Change-Id: I3106564a46a9cffc0db8808ba03f78d76c925eca
    Signed-off-by: Satyajit Desai <sadesai@codeaurora.org>
    Signed-off-by: Rama Aparna Mallavarapu <aparnam@codeaurora.org>
    Signed-off-by: Mulu He <muluhe@codeaurora.org>
    Signed-off-by: Tingwei Zhang <tingwei@codeaurora.org>

--- drivers/hwtracing/coresight/coresight-tmc-etr.c
+++ drivers/hwtracing/coresight/coresight-tmc-etr.c
@@ -1135,6 +1135,7 @@ static int tmc_enable_etr_sink_sysfs(struct coresight_device *csdev)
 	 * buffer, provided the size matches. Any allocation has to be done
 	 * with the lock released.
 	 */
+	mutex_lock(&drvdata->mem_lock);
 	spin_lock_irqsave(&drvdata->spinlock, flags);
 	sysfs_buf = READ_ONCE(drvdata->sysfs_buf);
 	if (!sysfs_buf || (sysfs_buf->size != drvdata->size)) {
@@ -1142,9 +1143,10 @@ static int tmc_enable_etr_sink_sysfs(struct coresight_device *csdev)
 
 		/* Allocate memory with the locks released */
 		free_buf = new_buf = tmc_etr_setup_sysfs_buf(drvdata);
-		if (IS_ERR(new_buf))
+		if (IS_ERR(new_buf)) {
+			mutex_unlock(&drvdata->mem_lock);
 			return PTR_ERR(new_buf);
-
+		}
 		/* Let's try again */
 		spin_lock_irqsave(&drvdata->spinlock, flags);
 	}
@@ -1191,6 +1193,7 @@ out:
 		tmc_etr_free_sysfs_buf(free_buf);
 
 	tmc_etr_byte_cntr_start(drvdata->byte_cntr);
+	mutex_unlock(&drvdata->mem_lock);
 
 	if (!ret)
 		dev_dbg(&csdev->dev, "TMC-ETR enabled\n");
@@ -1636,10 +1639,12 @@ static int tmc_disable_etr_sink(struct coresight_device *csdev)
 	unsigned long flags;
 	struct tmc_drvdata *drvdata = dev_get_drvdata(csdev->dev.parent);
 
+	mutex_lock(&drvdata->mem_lock);
 	spin_lock_irqsave(&drvdata->spinlock, flags);
 
 	if (drvdata->reading) {
 		spin_unlock_irqrestore(&drvdata->spinlock, flags);
+		mutex_unlock(&drvdata->mem_lock);
 		return -EBUSY;
 	}
 
@@ -1667,6 +1672,7 @@ static int tmc_disable_etr_sink(struct coresight_device *csdev)
 		tmc_etr_free_sysfs_buf(drvdata->etr_buf);
 		drvdata->etr_buf = NULL;
 	}
+	mutex_unlock(&drvdata->mem_lock);
 	dev_info(&csdev->dev, "TMC-ETR disabled\n");
 	return 0;
 }
@@ -1692,6 +1698,7 @@ int tmc_read_prepare_etr(struct tmc_drvdata *drvdata)
 	if (WARN_ON_ONCE(drvdata->config_type != TMC_CONFIG_TYPE_ETR))
 		return -EINVAL;
 
+	mutex_lock(&drvdata->mem_lock);
 	spin_lock_irqsave(&drvdata->spinlock, flags);
 	if (drvdata->reading) {
 		ret = -EBUSY;
@@ -1720,6 +1727,7 @@ int tmc_read_prepare_etr(struct tmc_drvdata *drvdata)
 	drvdata->reading = true;
 out:
 	spin_unlock_irqrestore(&drvdata->spinlock, flags);
+	mutex_unlock(&drvdata->mem_lock);
 
 	return ret;
 }
@@ -1732,7 +1740,7 @@ int tmc_read_unprepare_etr(struct tmc_drvdata *drvdata)
 	/* config types are set a boot time and never change */
 	if (WARN_ON_ONCE(drvdata->config_type != TMC_CONFIG_TYPE_ETR))
 		return -EINVAL;
-
+	mutex_lock(&drvdata->mem_lock);
 	spin_lock_irqsave(&drvdata->spinlock, flags);
 
 	/* RE-enable the TMC if need be */
@@ -1759,5 +1767,7 @@ int tmc_read_unprepare_etr(struct tmc_drvdata *drvdata)
 	if (sysfs_buf)
 		tmc_etr_free_sysfs_buf(sysfs_buf);
 
+
+	mutex_unlock(&drvdata->mem_lock);
 	return 0;
 }
