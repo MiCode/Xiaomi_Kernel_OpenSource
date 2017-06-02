f447dd3d321e99b95dd83f88fb140065920d96ed
Jack Pham <jackp@codeaurora.org>
2017-06-01 19:00:45 -0700

    usb: xhci: Free leaked command in case xhci_queue_stop_endpoint fails
    commit e3020f3887db ("xhci-hub: Handle error condition with
    xhci_stop_device") added error checking to xhci_stop_device()
    if xhci_queue_stop_endpoint() returns an error. However, the
    command structure allocated was not getting freed in that case.
    Fix the leak.
    
    Change-Id: Ibc28e32c9e2d402db0c47bfe759dd442d3ab184f
    Signed-off-by: Jack Pham <jackp@codeaurora.org>

--- drivers/usb/host/xhci-hub.c
+++ drivers/usb/host/xhci-hub.c
@@ -406,6 +406,7 @@ static int xhci_stop_device(struct xhci_hcd *xhci, int slot_id, int suspend)
 					i, suspend);
 			if (ret) {
 				spin_unlock_irqrestore(&xhci->lock, flags);
+				xhci_free_command(xhci, command);
 				goto err_cmd_queue;
 			}
 		}
