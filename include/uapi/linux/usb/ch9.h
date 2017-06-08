a8f3ba4a2292eb3c42669ea332254c4bffd9e8f4
Hemant Kumar <hemantk@codeaurora.org>
2017-06-07 18:54:42 -0700

    usb: core: Add support to parse config summary capability descriptors
    This descriptor describes the list of functions present in a given
    configuration. There should be one such descriptor per configuration
    supported by the device. The order in which these descriptors appear
    in the BOS descriptor reflects the order of preference of configurations
    as desired by the device for optimal functionality. Configuration Summary
    descriptors are used by Host software to decide which Configuration to set
    to obtain the desired functionality.
    
    Change-Id: I7ec095ce19caa690c0d3dc0fdd3add5ad37a74f3
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- include/uapi/linux/usb/ch9.h
+++ include/uapi/linux/usb/ch9.h
@@ -1077,6 +1077,30 @@ struct usb_ptm_cap_descriptor {
  */
 #define USB_DT_USB_SSP_CAP_SIZE(ssac)	(16 + ssac * 4)
 
+/*
+ * Configuration Summary descriptors: Defines a list of functions in the
+ * configuration. This descriptor may be used by Host software to decide
+ * which Configuration to use to obtain the desired functionality.
+ */
+#define	USB_CAP_TYPE_CONFIG_SUMMARY	0x10
+
+struct function_class_info {
+	__u8 bClass;
+	__u8 bSubClass;
+	__u8 bProtocol;
+};
+
+struct usb_config_summary_descriptor {
+	__u8 bLength;
+	__u8 bDescriptorType;
+	__u8 bDevCapabilityType;
+	__u16 bcdVersion;
+	__u8 bConfigurationValue;
+	__u8 bMaxPower;
+	__u8 bNumFunctions;
+	struct function_class_info cs_info[];
+} __attribute__((packed));
+
 /*-------------------------------------------------------------------------*/
 
 /* USB_DT_WIRELESS_ENDPOINT_COMP:  companion descriptor associated with
