e5fa9a0fafd58d567b605914a197e4a919c7ae6f
Veerabhadrarao Badiganti <vbadigan@codeaurora.org>
2017-06-08 23:04:42 +0530

    mmc: core: Ignore CRC errors with CMD13 while executing tuning
    While executing tuning in case CMD19 fails, the driver sends CMD13
    multiple times to ensure the card is ready to receive the next tuning
    command. If this status command fails with CRC error, it will trigger
    tuning. This will result in back to back tuning multiple times.
    
    To avoid this situation, ignore CRCs error generated with CMD13 while
    executing tuning.
    
    Change-Id: Ife338768264ebebebc655fe11874e905abdf7482
    Signed-off-by: Veerabhadrarao Badiganti <vbadigan@codeaurora.org>

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -319,6 +319,7 @@ void mmc_retune_enable(struct mmc_host *host)
 		mod_timer(&host->retune_timer,
 			  jiffies + host->retune_period * HZ);
 }
+EXPORT_SYMBOL(mmc_retune_enable);
 
 void mmc_retune_disable(struct mmc_host *host)
 {
@@ -327,6 +328,7 @@ void mmc_retune_disable(struct mmc_host *host)
 	host->retune_now = 0;
 	host->need_retune = 0;
 }
+EXPORT_SYMBOL(mmc_retune_disable);
 
 void mmc_retune_timer_stop(struct mmc_host *host)
 {
