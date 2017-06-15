23f0da87e0adb8ccfa006e160e3b1bb5e48a66ea
Hardik Arya <harya@codeaurora.org>
2017-06-15 10:39:34 +0530

    diag: Add protection while processing non-hdlc packets
    Currently, there is possibility of out-of-bound accesses during
    handling of data in non-hdlc path. The patch adds proper protection
    when processing non-hdlc packet information to fix the issue.
    
    CRs-Fixed: 2029216
    Change-Id: I07c466f85bd8ac08226948fea86b1d8567e68431
    Signed-off-by: Hardik Arya <harya@codeaurora.org>

--- drivers/char/diag/diagchar.h
+++ drivers/char/diag/diagchar.h
@@ -537,6 +537,7 @@ struct diagchar_dev {
 	unsigned char *buf_feature_mask_update;
 	uint8_t hdlc_disabled;
 	struct mutex hdlc_disable_mutex;
+	struct mutex hdlc_recovery_mutex;
 	struct timer_list hdlc_reset_timer;
 	struct mutex diag_hdlc_mutex;
 	unsigned char *hdlc_buf;
