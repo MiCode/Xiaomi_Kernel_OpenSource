81851c7d3b8a086ddc89c33ef250e057d52c3bb1
Hariram Purushothaman <hariramp@codeaurora.org>
2017-06-15 18:26:32 -0700

    msm: camera: HW CDM reset failure workaround
    Sometimes HW CDM triggers irq with invalid status for
    HW reset command, so ignore reset failure and proceed further
    as a workaround.
    
    CRs-Fixed: 2062256
    Change-Id: I96ff77efb0347a0a29f782cb42474b1e26542530
    Signed-off-by: Hariram Purushothaman <hariramp@codeaurora.org>
    Signed-off-by: Harsh Shah <harshs@codeaurora.org>

--- drivers/media/platform/msm/camera/cam_cdm/cam_cdm_hw_core.c
+++ drivers/media/platform/msm/camera/cam_cdm/cam_cdm_hw_core.c
@@ -692,7 +692,19 @@ int cam_hw_cdm_init(void *hw_priv,
 
 	CDM_CDBG("Waiting for CDM HW resetdone\n");
 	time_left = wait_for_completion_timeout(&cdm_core->reset_complete,
-			msecs_to_jiffies(CAM_CDM_HW_RESET_TIMEOUT));
+		msecs_to_jiffies(CAM_CDM_HW_RESET_TIMEOUT));
+
+	/*
+	 * Check for HW error and recover as a workaround
+	 * Sometimes CDM HW triggers irq with invalid status for
+	 * HW reset command, so ignore reset failure and proceed further
+	 * as a workaround.
+	 */
+	if (time_left <= 0) {
+		pr_err("CDM HW reset Wait failed time_left=%ld\n", time_left);
+		time_left = 1;
+	}
+
 	if (time_left <= 0) {
 		pr_err("CDM HW reset Wait failed rc=%d\n", rc);
 		goto disable_return;
