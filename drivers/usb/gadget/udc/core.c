7877b279aff1598d21c0fe868b19203c4f99467b
Mayank Rana <mrana@codeaurora.org>
2017-06-19 18:03:22 -0700

    udc: core: Add handling for case where allocated buffer is not mapped
    In BAM2BAM case, USB request is allocated but it is used as dummy
    buffer. Hence it has never been map using DMA API. There is crash
    seen with USB composition switch if this allocated but not mapped
    buffer is being unmapped. Fix this issue by explicitly adding check
    DMA_ERROR_CODE.
    
    Change-Id: I0e9e9f4ea1a600ab6de1e1e7b23e797d89d32bab
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>

--- drivers/usb/gadget/udc/core.c
+++ drivers/usb/gadget/udc/core.c
@@ -880,7 +880,7 @@ void usb_gadget_unmap_request_by_dev(struct device *dev,
 				is_in ? DMA_TO_DEVICE : DMA_FROM_DEVICE);
 
 		req->num_mapped_sgs = 0;
-	} else {
+	} else if (req->dma != DMA_ERROR_CODE) {
 		dma_unmap_single(dev, req->dma, req->length,
 				is_in ? DMA_TO_DEVICE : DMA_FROM_DEVICE);
 	}
