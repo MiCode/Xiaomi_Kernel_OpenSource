28befbdf2baaa83eea9f71a32e0e9eb42ad90fa3
George Shen <sqiao@codeaurora.org>
2017-06-21 13:59:34 -0700

    Revert "msm: kgsl: Enable SPTP power collapse"
    This reverts commit c3b616abaa517a8d031d057b40178732ff1766bc.
    Enabling SPTP PC feature caused instability issue in PDT.
    Disable it until it's fixed.
    
    CRs-Fixed: 2052606
    Change-Id: I6bb25816e01dbe170197599a169b4c9e4dffb1e4
    Signed-off-by: George Shen <sqiao@codeaurora.org>

--- drivers/gpu/msm/adreno-gpulist.h
+++ drivers/gpu/msm/adreno-gpulist.h
@@ -327,8 +327,7 @@ static const struct adreno_gpu_core adreno_gpulist[] = {
 		.minor = 0,
 		.patchid = ANY_ID,
 		.features = ADRENO_64BIT | ADRENO_RPMH |
-			ADRENO_GPMU | ADRENO_CONTENT_PROTECTION |
-			ADRENO_SPTP_PC,
+			ADRENO_GPMU | ADRENO_CONTENT_PROTECTION,
 		.sqefw_name = "a630_sqe.fw",
 		.zap_name = "a630_zap",
 		.gpudev = &adreno_a6xx_gpudev,
