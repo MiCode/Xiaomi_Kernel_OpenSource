b9b2d2f87911556e6036bc1106a20f981f3a887c
Ashay Jaiswal <ashayj@codeaurora.org>
2017-06-21 12:08:38 +0530

    smb-lib: add support for float charger reporting
    Report adapter type as USB_FLOAT if charger hardware detects a
    floating charger this will allow USB driver to try/start
    communication over Tx/Rx line during compliance testing.
    
    Change-Id: I1d1e91e99806423a3e995876e9db4582ed0f1b08
    Signed-off-by: Ashay Jaiswal <ashayj@codeaurora.org>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -260,7 +260,7 @@ static const struct apsd_result const smblib_apsd_results[] = {
 	[FLOAT] = {
 		.name	= "FLOAT",
 		.bit	= FLOAT_CHARGER_BIT,
-		.pst	= POWER_SUPPLY_TYPE_USB_DCP
+		.pst	= POWER_SUPPLY_TYPE_USB_FLOAT
 	},
 	[HVDCP2] = {
 		.name	= "HVDCP2",
@@ -3446,6 +3446,7 @@ static void smblib_force_legacy_icl(struct smb_charger *chg, int pst)
 		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 1500000);
 		break;
 	case POWER_SUPPLY_TYPE_USB_DCP:
+	case POWER_SUPPLY_TYPE_USB_FLOAT:
 		vote(chg->usb_icl_votable, LEGACY_UNKNOWN_VOTER, true, 1500000);
 		break;
 	case POWER_SUPPLY_TYPE_USB_HVDCP:
