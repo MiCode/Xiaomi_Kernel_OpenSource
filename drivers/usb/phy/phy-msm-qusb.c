9262263c54f7e13e30a31fa8fb0a3f76843ffdc3
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2017-06-21 12:40:13 +0530

    USB: phy-msm-qusb: Enable autoresume only when device is connected
    Currently driver is enabling autoresume feature even if no device is
    connected which is not required. Hence enable auto resume only when
    any HS/FS/LS devices are connected during bus suspend.
    
    Change-Id: I8150a571e11ccc128298fe75acef21ffe9b08a9a
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>

--- drivers/usb/phy/phy-msm-qusb.c
+++ drivers/usb/phy/phy-msm-qusb.c
@@ -994,13 +994,15 @@ static int qusb_phy_set_suspend(struct usb_phy *phy, int suspend)
 			writel_relaxed(intr_mask,
 				qphy->base + QUSB2PHY_PORT_INTR_CTRL);
 
-			/* enable phy auto-resume */
-			writel_relaxed(0x0C,
+			if (linestate & (LINESTATE_DP | LINESTATE_DM)) {
+				/* enable phy auto-resume */
+				writel_relaxed(0x0C,
 					qphy->base + QUSB2PHY_PORT_TEST_CTRL);
-			/* flush the previous write before next write */
-			wmb();
-			writel_relaxed(0x04,
-				qphy->base + QUSB2PHY_PORT_TEST_CTRL);
+				/* flush the previous write before next write */
+				wmb();
+				writel_relaxed(0x04,
+					qphy->base + QUSB2PHY_PORT_TEST_CTRL);
+			}
 
 
 			dev_dbg(phy->dev, "%s: intr_mask = %x\n",
