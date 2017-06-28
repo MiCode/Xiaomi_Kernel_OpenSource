45272ea0e071c0800c9e9a08553e9952fbe3bb95
Patrick Bellasi <patrick.bellasi@arm.com>
2017-06-28 15:59:46 +0100

    sched/fair: remove energy_diff tracepoint in preparation to re-factoring
    The format of the energy_diff tracepoint is going to be changed by the
    following energ_diff refactoring patches. Let's remove it now to start from
    a clean slate.
    
    Change-Id: Id4f537ed60d90a7ddcca0a29a49944bfacb85c8c
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    Git-commit: 4905932b059289d22f320b92c42331752ad82845
    Git-repo: https://android.googlesource.com/kernel/common/
    [pkondeti@codeaurora.org: Resolved minor conflcits]
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5853,13 +5853,7 @@ static inline int __energy_diff(struct energy_env *eenv)
 	eenv->nrg.after = energy_after;
 	eenv->nrg.diff = eenv->nrg.after - eenv->nrg.before;
 	eenv->payoff = 0;
-#ifndef CONFIG_SCHED_TUNE
-	trace_sched_energy_diff(eenv->p,
-			eenv->src_cpu, eenv->dst_cpu, eenv->util_delta,
-			eenv->nrg.before, eenv->nrg.after, eenv->nrg.diff,
-			eenv->cap.before, eenv->cap.after, eenv->cap.delta,
-			eenv->nrg.delta, eenv->payoff);
-#endif
+
 	/*
 	 * Dead-zone margin preventing too many migrations.
 	 */
@@ -5942,12 +5936,6 @@ energy_diff(struct energy_env *eenv)
 			eenv->nrg.delta,
 			eenv->cap.delta,
 			eenv->p);
-
-	trace_sched_energy_diff(eenv->p,
-			eenv->src_cpu, eenv->dst_cpu, eenv->util_delta,
-			eenv->nrg.before, eenv->nrg.after, eenv->nrg.diff,
-			eenv->cap.before, eenv->cap.after, eenv->cap.delta,
-			eenv->nrg.delta, eenv->payoff);
 
 	/*
 	 * When SchedTune is enabled, the energy_diff() function will return
