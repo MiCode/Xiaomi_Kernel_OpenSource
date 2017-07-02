7d0f2b2c517e10e97201467acbdd1bde4ed52f5b
Sagar Dharia <sdharia@codeaurora.org>
2017-07-01 18:26:15 -0600

    slim: msm: ngd: Fix incorrect casting of slimbus context-bank devices
    Slimbus controller's context-bank(CB) devices are created to
    facilitate IOMMU mapping for respective slimbus controllers.
    Calling the suspend/resume routines on CB-devices will result in
    incorrect casting of these devices to controller devices leading to
    a kernel-panic.
    CB-devices for slimbus don't need to do anything in suspend/resume
    since power management aspects are handled by controller devices.
    
    Change-Id: Ib83ef58c2a8e212a74bb2b215345805ef3c032bd
    Signed-off-by: Sagar Dharia <sdharia@codeaurora.org>

--- drivers/slimbus/slim-msm-ngd.c
+++ drivers/slimbus/slim-msm-ngd.c
@@ -2074,8 +2074,13 @@ static int ngd_slim_suspend(struct device *dev)
 {
 	int ret = -EBUSY;
 	struct platform_device *pdev = to_platform_device(dev);
-	struct msm_slim_ctrl *cdev = platform_get_drvdata(pdev);
+	struct msm_slim_ctrl *cdev;
 
+	if (of_device_is_compatible(pdev->dev.of_node,
+				    "qcom,iommu-slim-ctrl-cb"))
+		return 0;
+
+	cdev = platform_get_drvdata(pdev);
 	if (!pm_runtime_enabled(dev) ||
 		(!pm_runtime_suspended(dev) &&
 			cdev->state == MSM_CTRL_IDLE)) {
@@ -2111,9 +2116,14 @@ static int ngd_slim_suspend(struct device *dev)
 static int ngd_slim_resume(struct device *dev)
 {
 	struct platform_device *pdev = to_platform_device(dev);
-	struct msm_slim_ctrl *cdev = platform_get_drvdata(pdev);
+	struct msm_slim_ctrl *cdev;
 	int ret = 0;
 
+	if (of_device_is_compatible(pdev->dev.of_node,
+				    "qcom,iommu-slim-ctrl-cb"))
+		return 0;
+
+	cdev = platform_get_drvdata(pdev);
 	/*
 	 * If deferred response was requested for power-off and it failed,
 	 * mark runtime-pm status as active to be consistent
