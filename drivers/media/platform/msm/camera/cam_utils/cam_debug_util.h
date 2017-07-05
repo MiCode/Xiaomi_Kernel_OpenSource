26abf7760c90e1a09790efcd5de7a037da9b28df
Junzhe Zou <jnzhezou@codeaurora.org>
2017-07-05 10:36:43 -0700

    msm: camera: debug: fix typo in copyright header
    Fix typo in copyright header of the debug header file.
    
    CRs-fixed: 2071575
    Change-Id: I2554885eabc87768663a1bd64475d8e3ae7ef3fc
    Signed-off-by: Junzhe Zou <jnzhezou@codeaurora.org>

--- drivers/media/platform/msm/camera/cam_utils/cam_debug_util.h
+++ drivers/media/platform/msm/camera/cam_utils/cam_debug_util.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017, The Linux Foundataion. All rights reserved.
+/* Copyright (c) 2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
