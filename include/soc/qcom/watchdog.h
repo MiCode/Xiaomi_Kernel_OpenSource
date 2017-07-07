55c935b077a81feb66b6ba9f4dcbe25bce3f3e0b
Rama Aparna Mallavarapu <aparnam@codeaurora.org>
2017-07-07 16:14:47 -0700

    soc: qcom: Add snapshot of watchdog_v2 driver
    This is a snapshot of the watchdog_v2 driver as of msm-4.9
    commit '1a00b60049366e3f717973040f5f97ed9b972c10'. ("Update
    last_pet variable during resume") The last_pet variable is
    set in suspend callback and during pet_watchdog() and is
    printed when watchdog bark handler gets called. This variable
    does not represent exact last pet time in cases where non secure
    bark occurs before system could pet the watchdog upon wakeup
    event from low power mode. Reset the watchdog during
    msm_watchdog_resume() and update last_pet variable so that
    watchdog bark messages will represent latest info.
    
    This commit also adds memory barrier after resetting watchdog in
    msm_watchdog_suspend(). This will make sure watchdog writes are
    complete before proceeding further.
    
    Change-Id: I50190aa37c06bac21e91450d33312139ee532109
    Signed-off-by: Rama Aparna Mallavarapu <aparnam@codeaurora.org>

---
+++ include/soc/qcom/watchdog.h
@@ -0,0 +1,29 @@
+/* Copyright (c) 2017, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _ASM_ARCH_MSM_WATCHDOG_H_
+#define _ASM_ARCH_MSM_WATCHDOG_H_
+
+#ifdef CONFIG_QCOM_FORCE_WDOG_BITE_ON_PANIC
+#define WDOG_BITE_ON_PANIC 1
+#else
+#define WDOG_BITE_ON_PANIC 0
+#endif
+
+#ifdef CONFIG_QCOM_WATCHDOG_V2
+void msm_trigger_wdog_bite(void);
+#else
+static inline void msm_trigger_wdog_bite(void) { }
+#endif
+
+#endif
