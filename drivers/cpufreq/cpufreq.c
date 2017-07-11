3c067d6da2eb83f0761c0bd3bd1848b6349c0f3f
Santosh Mardi <gsantosh@codeaurora.org>
2017-07-11 09:41:57 +0530

    cpufreq: flush queued work before freeing policy
    When system resume and suspend happening at the same time,
    the resume sequence will schedule the work (policy->update)
    before policy->update getting chance to run in the meantime
    suspend or hotplug of an cpu happens at the same time resume
    is happening, and if the cpu is the last in the cluster
    policy is freed.
    
    This triggers an issue when scheduler tries to schedule the work
    which is scheduled in resume path But the handles are freed already.
    
    Flush the Queued work if any before freeing policy.
    
    Change-Id: I41504e7d9c201916457681537ac480c92e7cbe5d
    Signed-off-by: Santosh Mardi <gsantosh@codeaurora.org>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1514,8 +1514,10 @@ static int __cpufreq_remove_dev_finish(struct device *dev,
 		list_del(&policy->policy_list);
 		write_unlock_irqrestore(&cpufreq_driver_lock, flags);
 
-		if (!cpufreq_suspended)
+		if (!cpufreq_suspended) {
+			flush_work(&policy->update);
 			cpufreq_policy_free(policy);
+		}
 	} else if (has_target()) {
 		ret = __cpufreq_governor(policy, CPUFREQ_GOV_START);
 		if (!ret)
