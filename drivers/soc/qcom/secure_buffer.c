4a1607b0074d6ed189dfc5aea4d5e8cc9d86bfd3
Sudarshan Rajagopalan <sudaraja@codeaurora.org>
2017-07-13 11:19:46 -0700

    msm: secure_buffer: Add SPSS_HLOS_SHARED vmid
    The SPSS_HLOS_SHARED vmid can be used along with SPSS VM
    if an ION buffer is to be shared between SPSS and HLOS.
    
    Change-Id: I14407343a924ab652b20ba5d93e72114473c7f8a
    Signed-off-by: Sudarshan Rajagopalan <sudaraja@codeaurora.org>
    [lmark@codeaurora.org: don't include ION changes as these
     will be applied separately]
    Signed-off-by: Liam Mark <lmark@codeaurora.org>

--- drivers/soc/qcom/secure_buffer.c
+++ drivers/soc/qcom/secure_buffer.c
@@ -416,6 +416,8 @@ const char *msm_secure_vmid_to_string(int secure_vmid)
 		return "VMID_CP_SPSS_SP";
 	case VMID_CP_SPSS_SP_SHARED:
 		return "VMID_CP_SPSS_SP_SHARED";
+	case VMID_CP_SPSS_HLOS_SHARED:
+		return "VMID_CP_SPSS_HLOS_SHARED";
 	case VMID_INVAL:
 		return "VMID_INVAL";
 	default:
