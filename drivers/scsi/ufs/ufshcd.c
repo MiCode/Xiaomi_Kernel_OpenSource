0eebfea05ed8486a719aeea03e63a2087a80ecb5
Subhash Jadavani <subhashj@codeaurora.org>
2017-07-19 16:53:47 -0700

    scsi: ufs: mark device and link state if probe fails
    It seems UFS device state is set to active during probe and it doesn't get
    set to off even after probe fails. Because of this UFS device regulators
    aren't getting voted off. So mark both device and link state to off if
    probe fails which should vote off resources.
    
    Change-Id: I85f193acbf6054223bb300098c719061759667a1
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -7833,6 +7833,11 @@ static int ufshcd_probe_hba(struct ufs_hba *hba)
 		ufshcd_set_auto_hibern8_timer(hba,
 				      hba->hibern8_on_idle.delay_ms);
 out:
+	if (ret) {
+		ufshcd_set_ufs_dev_poweroff(hba);
+		ufshcd_set_link_off(hba);
+	}
+
 	/*
 	 * If we failed to initialize the device or the device is not
 	 * present, turn off the power/clocks etc.
