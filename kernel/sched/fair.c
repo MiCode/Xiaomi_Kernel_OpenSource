4ec7d81823a2859608954c9336238ce1e838491c
Quentin Perret <quentin.perret@arm.com>
2017-07-21 15:40:36 +0100

    ANDROID: Revert "sched/fair: ensure utilization signals are synchronized before use"
    This reverts commit a4bd433852d52186f306f1621f16f90609ded67f.
    
    Change-Id: I75e9f169d12d53dec3eecc9a4196c7a900bd3e7c
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6915,13 +6915,8 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 
 	if (sd_flag & SD_BALANCE_WAKE) {
 		record_wakee(p);
-		/*
-		 * do wake_cap unconditionally as it causes task and cpu
-		 * utilization to be synced, and we need that for energy
-		 * aware wakeups
-		 */
-		want_affine = !wake_wide(p) && !wake_cap(p, cpu, prev_cpu)
-			&& cpumask_test_cpu(cpu, tsk_cpus_allowed(p));
+		want_affine = (!wake_wide(p) && !wake_cap(p, cpu, prev_cpu) &&
+			cpumask_test_cpu(cpu, tsk_cpus_allowed(p)));
 	}
 
 	if (energy_aware() && !(cpu_rq(prev_cpu)->rd->overutilized))
