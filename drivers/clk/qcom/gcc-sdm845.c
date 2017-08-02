e7bb0e632ea988d8e20bd95aed107f831d51f0c6
Deepak Katragadda <dkatraga@codeaurora.org>
2017-08-02 09:57:46 -0700

    clk: qcom: gcc-sdm845: Update the SDCC2 clock frequency on SDM845 v2
    The sdcc2_apps_clk has an updated FMAX of 201.5 MHz on SDM845 v2.
    Support this from the Linux clock driver.
    
    Change-Id: I8b56a170b708985f3d15ea9de3bfe79fbf08eb2f
    Signed-off-by: Deepak Katragadda <dkatraga@codeaurora.org>

--- drivers/clk/qcom/gcc-sdm845.c
+++ drivers/clk/qcom/gcc-sdm845.c
@@ -873,17 +873,6 @@ static const struct freq_tbl ftbl_gcc_sdcc2_apps_clk_src[] = {
 	{ }
 };
 
-static const struct freq_tbl ftbl_gcc_sdcc2_apps_clk_src_sdm845_v2[] = {
-	F(400000, P_BI_TCXO, 12, 1, 4),
-	F(9600000, P_BI_TCXO, 2, 0, 0),
-	F(19200000, P_BI_TCXO, 1, 0, 0),
-	F(25000000, P_GPLL0_OUT_EVEN, 12, 0, 0),
-	F(50000000, P_GPLL0_OUT_EVEN, 6, 0, 0),
-	F(100000000, P_GPLL0_OUT_MAIN, 6, 0, 0),
-	F(200000000, P_GPLL0_OUT_MAIN, 3, 0, 0),
-	{ }
-};
-
 static struct clk_rcg2 gcc_sdcc2_apps_clk_src = {
 	.cmd_rcgr = 0x1400c,
 	.mnd_width = 8,
@@ -4013,9 +4002,6 @@ static void gcc_sdm845_fixup_sdm845v2(void)
 		50000000;
 	gcc_qupv3_wrap1_s7_clk_src.clkr.hw.init->rate_max[VDD_CX_NOMINAL] =
 		128000000;
-	gcc_sdcc2_apps_clk_src.freq_tbl = ftbl_gcc_sdcc2_apps_clk_src_sdm845_v2;
-	gcc_sdcc2_apps_clk_src.clkr.hw.init->rate_max[VDD_CX_LOW_L1] =
-		200000000;
 	gcc_ufs_card_axi_clk_src.freq_tbl =
 		ftbl_gcc_ufs_card_axi_clk_src_sdm845_v2;
 	gcc_ufs_card_axi_clk_src.clkr.hw.init->rate_max[VDD_CX_HIGH] =
