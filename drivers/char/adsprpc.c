MotorolaMobilityLLC/kernel-msm

SDM660: ADSPRPC: Align the size of IOVA to 128MB
Align the size of the IOVA that is being passed
in the arm_iommu_create_mapping() to 128MB.

Change-Id: Ia554c2157d6c46b2f3848f993a7e61ff7f029547
Acked-by: Chenna Kesava Raju <chennak@qti.qualcomm.com>
Signed-off-by: Tharun Kumar Merugu <mtharu@codeaurora.org>

drivers/char/adsprpc.c
@@ -2730,7 +2730,7 @@ static int fastrpc_cb_probe(struct device *dev)
 		start = 0x60000000;
 	VERIFY(err, !IS_ERR_OR_NULL(sess->smmu.mapping =
 				arm_iommu_create_mapping(&platform_bus_type,
-						start, 0x7fffffff)));
+						start, 0x70000000)));
 	if (err)
 		goto bail;
 	iommu_set_fault_handler(sess->smmu.mapping->domain,

MiCode/Xiaomi_Kernel_OpenSource

drivers/char/adsprpc.c
@@ -2459,7 +2459,7 @@ static int fastrpc_cb_probe(struct device *dev)
 		start = 0x60000000;
 	VERIFY(err, !IS_ERR_OR_NULL(sess->smmu.mapping =
 				arm_iommu_create_mapping(&platform_bus_type,
-						start, 0x7fffffff)));
+						start, 0x78000000)));
 	if (err)
 		goto bail;
 