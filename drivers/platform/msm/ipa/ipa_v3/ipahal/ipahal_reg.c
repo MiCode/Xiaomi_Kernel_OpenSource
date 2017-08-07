86dd5fbdfd6ae1fc38b93586f633be1f55f233bb
Ghanim Fodi <gfodi@codeaurora.org>
2017-08-07 11:40:58 +0300

    msm: ipa3: Fix IPA aggregation force close call
    Core IPA driver passes wrong end-point index to HAL
    when generating aggregation force close parameters
    values. Fixed the call to pass the correct index.
    
    CRs-Fixed: 2082839
    Change-Id: Icc0fd2ce8a23bf0e3334f54b27b850ebf92b58a1
    Signed-off-by: Ghanim Fodi <gfodi@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipahal/ipahal_reg.c
+++ drivers/platform/msm/ipa/ipa_v3/ipahal/ipahal_reg.c
@@ -1560,6 +1560,11 @@ void ipahal_get_aggr_force_close_valmask(int ep_idx,
 		IPA_AGGR_FORCE_CLOSE_AGGR_FORCE_CLOSE_PIPE_BITMAP_BMSK_V3_5;
 	}
 
+	if (ep_idx > (sizeof(valmask->val) * 8 - 1)) {
+		IPAHAL_ERR("too big ep_idx %d\n", ep_idx);
+		ipa_assert();
+		return;
+	}
 	IPA_SETFIELD_IN_REG(valmask->val, 1 << ep_idx, shft, bmsk);
 	valmask->mask = bmsk << shft;
 }
