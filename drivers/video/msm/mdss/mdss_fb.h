5ce4d5e6e9950943cf923ab56e059849998ba6cf
Nirmal Abraham <nabrah@codeaurora.org>
2017-08-18 14:51:08 +0530

    msm: mdss: avoid kickoff wait during validation phase
    During ATOMIC_COMMIT validation phase, waiting for kickoff
    introduces delay where the commit N+1 from userspace waits
    on driver's commit N thereby causing fps drop.
    Remove this wait during validate phase.
    
    Change-Id: I20ec0f522ba2531df6abdbb04e95cdea19bc2687
    Signed-off-by: Nirmal Abraham <nabrah@codeaurora.org>

--- drivers/video/msm/mdss/mdss_fb.h
+++ drivers/video/msm/mdss/mdss_fb.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008-2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2008-2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -357,6 +357,7 @@ struct msm_fb_data_type {
 	bool mdss_fb_split_stored;
 
 	u32 wait_for_kickoff;
+	u32 skip_koff_wait;
 	u32 thermal_level;
 
 	int fb_mmap_type;
