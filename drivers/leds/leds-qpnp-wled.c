45454a41d24c72d09fd7f7be95974c4104c693e6
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2017-08-21 19:16:37 -0700

    leds: qpnp-wled: Cap the lower limit of brightness level
    As per the hardware documentation, operating limits of WLED is
    from 0.4% to 100%. Cap the lower limit to 0.4% to ensure proper
    operation.
    
    Change-Id: I7ee2684ea2186d4fdda5cea9a4d421b429a6d4fd
    Signed-off-by: Subbaraman Narayanamurthy <subbaram@codeaurora.org>

--- drivers/leds/leds-qpnp-wled.c
+++ drivers/leds/leds-qpnp-wled.c
@@ -529,6 +529,11 @@ static int qpnp_wled_set_level(struct qpnp_wled *wled, int level)
 {
 	int i, rc;
 	u8 reg;
+	u16 low_limit = WLED_MAX_LEVEL_4095 * 4 / 1000;
+
+	/* WLED's lower limit of operation is 0.4% */
+	if (level > 0 && level < low_limit)
+		level = low_limit;
 
 	/* set brightness registers */
 	for (i = 0; i < wled->num_strings; i++) {
