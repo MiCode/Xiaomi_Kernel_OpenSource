a48d831bc32822b538272b87b92bf76d1fbdb4a7
Sriharsha Allenki <sallenki@codeaurora.org>
2017-08-23 15:07:02 +0530

    usb: f_gps: Fix incorrect response available notifications
    On function resume we notify response available to the
    host even if there are no responses. Fix this by checking
    the response queue on resume before notifying the host.
    
    CRs-Fixed: 2098360
    Change-Id: If49fc6c9722b2e4be514ae128f1803f0b0d90fb6
    Signed-off-by: Sriharsha Allenki <sallenki@codeaurora.org>

--- drivers/usb/gadget/function/f_gps.c
+++ drivers/usb/gadget/function/f_gps.c
@@ -297,6 +297,12 @@ static void gps_resume(struct usb_function *f)
 		return;
 
 	dev->is_suspended = false;
+	spin_lock(&dev->lock);
+	if (list_empty(&dev->cpkt_resp_q)) {
+		spin_unlock(&dev->lock);
+		return;
+	}
+	spin_unlock(&dev->lock);
 	gps_ctrl_response_available(dev);
 }
 
