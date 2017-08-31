5d1edf77fb0f1950453318bdc70bad19bfdc8e22
Rajesh Kemisetti <rajeshk@codeaurora.org>
2017-08-31 18:24:17 +0530

    msm: kgsl: Enable Content protection and GPMU features for SDM670
    Enable the ADRENO_CONTENT_PROTECTION flag to indicate
    that the GPU is ready to handle secure allocations.
    
    Also enable ADRENO_GPMU flag to load Graphics Management Unit
    firmware to handle GPU power states.
    
    Change-Id: Ibfb13c610ab6ff91c65d505ba4d6893249da3299
    Signed-off-by: Rajesh Kemisetti <rajeshk@codeaurora.org>

--- drivers/gpu/msm/adreno-gpulist.h
+++ drivers/gpu/msm/adreno-gpulist.h
@@ -346,7 +346,8 @@ static const struct adreno_gpu_core adreno_gpulist[] = {
 		.major = 1,
 		.minor = 5,
 		.patchid = ANY_ID,
-		.features = ADRENO_64BIT | ADRENO_RPMH,
+		.features = ADRENO_64BIT | ADRENO_RPMH |
+			ADRENO_GPMU | ADRENO_CONTENT_PROTECTION,
 		.sqefw_name = "a630_sqe.fw",
 		.zap_name = "a615_zap",
 		.gpudev = &adreno_a6xx_gpudev,
