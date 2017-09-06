a1682a646a89b44009b40392d7f6e68250abd39e
Meng Wang <mwang@codeaurora.org>
2017-09-06 11:22:21 +0800

    ALSA: PCM: volume API implementation
    Introduced a new helper function snd_pcm_add_volume_ctls() to
    create control elements representing the volume for each PCM
    (sub)stream.
    
    This patch also squashes the logic implemented by below change-
    
    ASoC: Fix freed memory access of pcm stream kctl
    
    Consider sound card instantiate fails due to
    audrx init failure. In such case, all dais/ctls
    are de-registered and freed. But as part of it,
    access to unregistered ctls for pcm_chmap and similar
    controls result in crash.  Ctls are freed at disconnection
    but the disconnect is called only when it was registered.
    
    CRs-Fixed: 1038054
    Change-Id: Ief8817b4ec000c058d46aa021977b7c6003c0011
    Signed-off-by: Laxminath Kasam <lkasam@codeaurora.org>
    Signed-off-by: Damir Didjusto <damird@codeaurora.org>
    Signed-off-by: Banajit Goswami <bgoswami@codeaurora.org>
    Signed-off-by: Sudheer Papothi <spapothi@codeaurora.org>
    Signed-off-by: Meng Wang <mwang@codeaurora.org>

--- include/sound/pcm.h
+++ include/sound/pcm.h
@@ -501,6 +501,9 @@ struct snd_pcm_str {
 #endif
 #endif
 	struct snd_kcontrol *chmap_kctl; /* channel-mapping controls */
+#ifdef CONFIG_AUDIO_QGKI
+	struct snd_kcontrol *vol_kctl; /* volume controls */
+#endif
 	struct device dev;
 };
 
@@ -1416,6 +1419,32 @@ static inline u64 pcm_format_to_bits(snd_pcm_format_t pcm_format)
 {
 	return 1ULL << (__force int) pcm_format;
 }
+
+#ifdef CONFIG_AUDIO_QGKI
+/*
+ * PCM Volume control API
+ */
+/* array element of volume */
+struct snd_pcm_volume_elem {
+	int volume;
+};
+
+/* pp information; retrieved via snd_kcontrol_chip() */
+struct snd_pcm_volume {
+	struct snd_pcm *pcm;	/* assigned PCM instance */
+	int stream;		/* PLAYBACK or CAPTURE */
+	struct snd_kcontrol *kctl;
+	const struct snd_pcm_volume_elem *volume;
+	int max_length;
+	void *private_data;	/* optional: private data pointer */
+};
+
+int snd_pcm_add_volume_ctls(struct snd_pcm *pcm, int stream,
+			   const struct snd_pcm_volume_elem *volume,
+			   int max_length,
+			   unsigned long private_value,
+			   struct snd_pcm_volume **info_ret);
+#endif
 
 /* printk helpers */
 #define pcm_err(pcm, fmt, args...) \
