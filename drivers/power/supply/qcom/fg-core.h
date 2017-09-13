a87340d406f53953a15ee6fa1366d86dd44e00f6
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2017-09-13 16:38:40 -0700

    power: qpnp-fg-gen3: Improve the accuracy of charge_counter
    Currently, charge_counter is based off CC_SOC_SW which is based
    off coulomb counter. However, there could be some accumulation
    of error due to inaccuracy in ADC over time. This can potentially
    affect the accuracy of charge_counter. To overcome this, prime
    CC_SOC_SW during discharging based off battery SOC and to a full
    value during charge termination.
    
    While at it, expose the charge_counter_shadow property based off
    battery SOC for comparison.
    
    CRs-Fixed: 2109421
    Change-Id: I50de44afbdbd747db95946416a09062df205fd6c
    Signed-off-by: Subbaraman Narayanamurthy <subbaram@codeaurora.org>

--- drivers/power/supply/qcom/fg-core.h
+++ drivers/power/supply/qcom/fg-core.h
@@ -412,6 +412,7 @@ struct fg_chip {
 	int			batt_id_ohms;
 	int			ki_coeff_full_soc;
 	int			charge_status;
+	int			prev_charge_status;
 	int			charge_done;
 	int			charge_type;
 	int			online_status;
