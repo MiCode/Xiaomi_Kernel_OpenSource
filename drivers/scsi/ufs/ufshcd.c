cf2ea6ea6a74e26f2d3edad18a6e261e27978c26
Sayali Lokhande <sayalil@codeaurora.org>
2017-09-20 19:39:18 +0530

    scsi: ufs: Add counter for hibernate and gear switch
    This change adds few more debugfs entries to capture
    statistics regarding the ufs driver.
    This information include:
    1. ufs hibernate exit count
    2. ufs power mode change count
    usage : cat /sys/kernel/debug/ufshcd0/show_hba
    
    Change-Id: I75ac694c1f7e98d527392372300d16ad911ac512
    Signed-off-by: Sayali Lokhande <sayalil@codeaurora.org>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -4729,6 +4729,7 @@ int ufshcd_change_power_mode(struct ufs_hba *hba,
 
 		memcpy(&hba->pwr_info, pwr_mode,
 			sizeof(struct ufs_pa_layer_attr));
+		hba->ufs_stats.power_mode_change_cnt++;
 	}
 
 	return ret;
@@ -7449,9 +7450,6 @@ static void ufshcd_tune_unipro_params(struct ufs_hba *hba)
 static void ufshcd_clear_dbg_ufs_stats(struct ufs_hba *hba)
 {
 	int err_reg_hist_size = sizeof(struct ufs_uic_err_reg_hist);
-
-	hba->ufs_stats.hibern8_exit_cnt = 0;
-	hba->ufs_stats.last_hibern8_exit_tstamp = ktime_set(0, 0);
 
 	memset(&hba->ufs_stats.pa_err, 0, err_reg_hist_size);
 	memset(&hba->ufs_stats.dl_err, 0, err_reg_hist_size);
