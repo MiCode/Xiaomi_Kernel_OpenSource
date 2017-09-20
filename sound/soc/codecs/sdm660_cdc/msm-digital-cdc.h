bc6d56b94705aa5f2a1800b4a1c659af5e56a045
Vatsal Bucha <vbucha@codeaurora.org>
2017-09-20 15:02:22 +0530

    ASoC: sdm660_cdc: Fix pop noise issue at DMIC
    Pop noise occurs during switch from handset to speaker mode
    during voice call. This is because sufficient delay
    is not provided for tx to unmute. A delayed workqueue
    that will do unmute at the end solves the issue.
    
    CRs-Fixed: 2101423
    Change-Id: Idc1fd8e770eb0d361bd436ce35940200173106ef
    Signed-off-by: Vatsal Bucha <vbucha@codeaurora.org>

--- sound/soc/codecs/sdm660_cdc/msm-digital-cdc.h
+++ sound/soc/codecs/sdm660_cdc/msm-digital-cdc.h
@@ -32,6 +32,12 @@ enum {
 	MSM89XX_RX_MAX,
 };
 
+struct tx_mute_work {
+	struct msm_dig_priv *dig_cdc;
+	u32 decimator;
+	struct delayed_work dwork;
+};
+
 struct msm_dig_priv {
 	struct snd_soc_codec *codec;
 	u32 comp_enabled[MSM89XX_RX_MAX];
@@ -55,6 +61,7 @@ struct msm_dig_priv {
 	int (*register_notifier)(void *handle,
 				 struct notifier_block *nblock,
 				 bool enable);
+	struct tx_mute_work tx_mute_dwork[NUM_DECIMATORS];
 };
 
 struct dig_ctrl_platform_data {
