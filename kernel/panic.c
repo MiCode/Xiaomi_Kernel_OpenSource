77f7f8c31e77a352c58cd749279bf1d39f18cf96
Lingutla Chandrasekhar <clingutla@codeaurora.org>
2017-09-25 19:07:12 +0530

    soc: qcom: Register default dump entries to minidump table
    Add below common dump entries to minidump table:
    All memory dump table entries, kernel data/bss sections,
    percpu static sections, rtb, logbuf, wdogdata, lpm_debug,
    stackpointer, and current task struct.
    Also add name for memory dump table entries to accommodate
    Minidump support.
    
    Change-Id: I45d121bc36b40332cfd0a0f5142572c6ce8f8a26
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>
    Signed-off-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>

--- kernel/panic.c
+++ kernel/panic.c
@@ -29,6 +29,7 @@
 #include <linux/ratelimit.h>
 #include <linux/debugfs.h>
 #include <asm/sections.h>
+#include <soc/qcom/minidump.h>
 
 #define PANIC_TIMER_STEP 100
 #define PANIC_BLINK_SPD 18
@@ -175,6 +176,7 @@ void panic(const char *fmt, ...)
 	va_start(args, fmt);
 	vsnprintf(buf, sizeof(buf), fmt, args);
 	va_end(args);
+	dump_stack_minidump(0);
 	pr_emerg("Kernel panic - not syncing: %s\n", buf);
 #ifdef CONFIG_DEBUG_BUGVERBOSE
 	/*
