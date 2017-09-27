d56ac6f20d3b0d72569d512c5be270595d53e07b
Lingutla Chandrasekhar <clingutla@codeaurora.org>
2017-09-27 18:06:52 +0530

    elf: Add elf headers helpers support
    ELF header format is been used in multiple places,
    add helper functions to get section, program headers, and
    string table section details of the elf header.
    
    Change-Id: Ib67b6f25a3a9c32305710eae4ad66bea511d6799
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>

--- drivers/soc/qcom/msm_minidump.c
+++ drivers/soc/qcom/msm_minidump.c
@@ -87,33 +87,6 @@ bool minidump_enabled;
 static unsigned int pendings;
 static unsigned int region_idx = 1; /* First entry is ELF header*/
 
-static inline struct elf_shdr *elf_sheader(struct elfhdr *hdr)
-{
-	return (struct elf_shdr *)((size_t)hdr + (size_t)hdr->e_shoff);
-}
-
-static inline struct elf_shdr *elf_section(struct elfhdr *hdr, int idx)
-{
-	return &elf_sheader(hdr)[idx];
-}
-
-static inline struct elf_phdr *elf_pheader(struct elfhdr *hdr)
-{
-	return (struct elf_phdr *)((size_t)hdr + (size_t)hdr->e_phoff);
-}
-
-static inline struct elf_phdr *elf_program(struct elfhdr *hdr, int idx)
-{
-	return &elf_pheader(hdr)[idx];
-}
-
-static inline char *elf_str_table(struct elfhdr *hdr)
-{
-	if (hdr->e_shstrndx == SHN_UNDEF)
-		return NULL;
-	return (char *)hdr + elf_section(hdr, hdr->e_shstrndx)->sh_offset;
-}
-
 static inline char *elf_lookup_string(struct elfhdr *hdr, int offset)
 {
 	char *strtab = elf_str_table(hdr);
