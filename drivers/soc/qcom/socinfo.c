2e498ac62d308c1a9367f8e46607f47e2bbd9616
Maria Yu <aiquny@codeaurora.org>
2017-09-28 16:23:51 +0800

    socinfo: Add socinfo support for sdm450
    Add socinfo support to get soc_id and other details
    about sdm450.
    
    Change-Id: I7c82dbb69df95e2c1a06c9fe9f3f3bf5cc9b49ef
    Signed-off-by: Maria Yu <aiquny@codeaurora.org>
    Signed-off-by: Srinivasarao P <spathi@codeaurora.org>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -378,6 +378,9 @@ static struct msm_soc_info cpu_of_id[] = {
 	[293] = {MSM_CPU_8953, "MSM8953"},
 	[304] = {MSM_CPU_8953, "APQ8053"},
 
+	/* SDM450 ID */
+	[338] = {MSM_CPU_SDM450, "SDM450"},
+
 	/* Uninitialized IDs are not known to run Linux.
 	 * MSM_CPU_UNKNOWN is set to 0 to ensure these IDs are
 	 * considered as unknown CPU.
@@ -1328,6 +1331,10 @@ static void * __init setup_dummy_socinfo(void)
 	} else if (early_machine_is_msm8953()) {
 		dummy_socinfo.id = 293;
 		strlcpy(dummy_socinfo.build_id, "msm8953 - ",
+			sizeof(dummy_socinfo.build_id));
+	} else if (early_machine_is_sdm450()) {
+		dummy_socinfo.id = 338;
+		strlcpy(dummy_socinfo.build_id, "sdm450 - ",
 			sizeof(dummy_socinfo.build_id));
 	} else
 		strlcat(dummy_socinfo.build_id, "Dummy socinfo",
--- include/soc/qcom/socinfo.h
+++ include/soc/qcom/socinfo.h
@@ -98,6 +98,8 @@
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,qm215")
 #define early_machine_is_msm8953()	\
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,msm8953")
+#define early_machine_is_sdm450()	\
+	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,sdm450")
 #else
 #define of_board_is_sim()		0
 #define of_board_is_rumi()		0
@@ -141,6 +143,7 @@
 #define early_machine_is_sdm429()	0
 #define early_machine_is_qm215()	0
 #define early_machine_is_msm8953()	0
+#define early_machine_is_sdm450()	0
 #endif
 
 #define PLATFORM_SUBTYPE_MDM	1
@@ -186,6 +189,7 @@ enum msm_cpu {
 	MSM_CPU_SDM429,
 	MSM_CPU_QM215,
 	MSM_CPU_8953,
+	MSM_CPU_SDM450,
 };
 
 struct msm_soc_info {
