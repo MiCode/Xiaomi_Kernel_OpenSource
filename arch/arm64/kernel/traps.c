1ad7cba07e31f9d2d7cd65aea5bfefdc248be0f8
Greg Hackmann <ghackmann@google.com>
2017-10-04 09:31:34 -0700

    arm64: issue isb when trapping CNTVCT_EL0 access
    Change-Id: I6005a6e944494257bfc2243fde2f7a09c3fd76c6
    Signed-off-by: Neeraj Upadhyay <neeraju@codeaurora.org>

--- arch/arm64/kernel/traps.c
+++ arch/arm64/kernel/traps.c
@@ -32,6 +32,7 @@
 #include <linux/syscalls.h>
 
 #include <asm/atomic.h>
+#include <asm/barrier.h>
 #include <asm/debug-monitors.h>
 #include <asm/esr.h>
 #include <asm/traps.h>
@@ -403,6 +404,7 @@ static void cntvct_read_handler(unsigned int esr, struct pt_regs *regs)
 {
 	int rt = (esr & ESR_ELx_SYS64_ISS_RT_MASK) >> ESR_ELx_SYS64_ISS_RT_SHIFT;
 
+	isb();
 	if (rt != 31)
 		regs->regs[rt] = arch_counter_get_cntvct();
 	regs->pc += 4;
