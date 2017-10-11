e93c1c1e1f4d5db047d18745022a62397e2b001c
Michael Halcrow <mhalcrow@google.com>
2017-10-11 16:36:05 -0700

    ext4: Set the bio REQ_NOENCRYPT flag
    Sets the REQ_NOENCRYPT flag for I/O requests that ext4 has already
    encrypted.  Lower layers such as dm-crypt can observe that flag to
    avoid encrypting again if that's how the user configures the
    environment.
    
    Change-Id: I62a93e480a9f5788070446bd94694dfb9f83c277
    Signed-off-by: Michael Halcrow <mhalcrow@google.com>
    Git-repo: https://android.googlesource.com/kernel/common/
    Git-commit: 7af2a1358e1577379c04d08b5c1d2ac4565c9418
    [stummala@codeaurora.org: resolve merge conflicts]
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- fs/ext4/readpage.c
+++ fs/ext4/readpage.c
@@ -297,7 +297,8 @@ int ext4_mpage_readpages(struct address_space *mapping,
 			bio->bi_iter.bi_sector = blocks[0] << (blkbits - 9);
 			bio->bi_end_io = mpage_end_io;
 			bio->bi_private = ctx;
-			bio_set_op_attrs(bio, REQ_OP_READ, 0);
+			bio_set_op_attrs(bio, REQ_OP_READ,
+					 ctx ? REQ_NOENCRYPT : 0);
 		}
 
 		length = first_hole << blkbits;
