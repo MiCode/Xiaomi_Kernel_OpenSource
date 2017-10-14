7e0714203cdb855f3709be48152ed032447e9111
Abhinav Kumar <abhinavk@codeaurora.org>
2017-10-13 18:43:52 -0700

    drm/msm/sde: align bandwidth/clock updates with frame done
    Align clock and bandwidth updates witf frame done event for
    video mode panels.
    
    Also move perf state update to atomic check to make perf data
    persistent over state duplication.
    
    Change-Id: I5ef02dd78ce6a62cf49f1f105344764fdb3a24da
    Signed-off-by: Abhinav Kumar <abhinavk@codeaurora.org>

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -471,6 +471,7 @@ static void sde_crtc_frame_event_work(struct kthread_work *work)
 	struct sde_crtc_frame_event *fevent;
 	struct drm_crtc *crtc;
 	struct sde_crtc *sde_crtc;
+	struct sde_crtc_state *cstate;
 	struct sde_kms *sde_kms;
 	unsigned long flags;
 
@@ -480,13 +481,14 @@ static void sde_crtc_frame_event_work(struct kthread_work *work)
 	}
 
 	fevent = container_of(work, struct sde_crtc_frame_event, work);
-	if (!fevent->crtc) {
+	if (!fevent->crtc || !fevent->crtc->state) {
 		SDE_ERROR("invalid crtc\n");
 		return;
 	}
 
 	crtc = fevent->crtc;
 	sde_crtc = to_sde_crtc(crtc);
+	cstate = to_sde_crtc_state(crtc->state);
 
 	sde_kms = _sde_crtc_get_kms(crtc);
 	if (!sde_kms) {
@@ -520,6 +522,9 @@ static void sde_crtc_frame_event_work(struct kthread_work *work)
 		} else {
 			SDE_EVT32(DRMID(crtc), fevent->event, 2);
 		}
+
+		if (fevent->event == SDE_ENCODER_FRAME_EVENT_DONE)
+			sde_core_perf_crtc_update(crtc, 0, false);
 	} else {
 		SDE_ERROR("crtc%d ts:%lld unknown event %u\n", crtc->base.id,
 				ktime_to_ns(fevent->ts),
