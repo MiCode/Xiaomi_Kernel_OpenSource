54a849c6ad8655f4510c9798e03f77910c5e0301
Deepak Katragadda <dkatraga@codeaurora.org>
2017-10-14 14:52:08 -0700

    clk: qcom: CPU clock driver for SDM855
    Add the OSM clock driver to control CPU clock scaling on SDM855.
    
    Change-Id: I30265195c647a942799c9d09da7670d9d555a6c0
    Signed-off-by: Deepak Katragadda <dkatraga@codeaurora.org>

--- include/dt-bindings/clock/qcom,cpucc-sdm855.h
+++ include/dt-bindings/clock/qcom,cpucc-sdm855.h
@@ -14,18 +14,20 @@
 #ifndef _DT_BINDINGS_CLK_QCOM_CPU_CC_SDM855_H
 #define _DT_BINDINGS_CLK_QCOM_CPU_CC_SDM855_H
 
-#define L3_CLUSTER0_VOTE_CLK					0
-#define L3_CLUSTER1_VOTE_CLK					1
-#define L3_CLK							2
-#define CPU0_PWRCL_CLK						3
-#define CPU1_PWRCL_CLK						4
-#define CPU2_PWRCL_CLK						5
-#define CPU3_PWRCL_CLK						6
-#define PWRCL_CLK						7
-#define CPU4_PERFCL_CLK						8
-#define CPU5_PERFCL_CLK						9
-#define CPU6_PERFCL_CLK						10
-#define CPU7_PERFCL_CLK						11
-#define PERFCL_CLK						12
+#define L3_CLUSTER0_VOTE_CLK		0
+#define L3_CLUSTER1_VOTE_CLK		1
+#define L3_MISC_VOTE_CLK		2
+#define L3_CLK				3
+#define CPU0_PWRCL_CLK			4
+#define CPU1_PWRCL_CLK			5
+#define CPU2_PWRCL_CLK			6
+#define CPU3_PWRCL_CLK			7
+#define PWRCL_CLK			8
+#define CPU4_PERFCL_CLK			9
+#define CPU5_PERFCL_CLK			10
+#define CPU6_PERFCL_CLK			11
+#define PERFCL_CLK			12
+#define CPU7_PERFPCL_CLK		13
+#define PERFPCL_CLK			14
 
 #endif
