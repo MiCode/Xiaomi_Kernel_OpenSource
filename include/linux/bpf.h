02afae7216cbbbcdfab690e4f093463acc90eaa6
Chenbo Feng <fengc@google.com>
2017-10-18 13:00:26 -0700

    BACKPORT: selinux: bpf: Add addtional check for bpf object file receive
    Introduce a bpf object related check when sending and receiving files
    through unix domain socket as well as binder. It checks if the receiving
    process have privilege to read/write the bpf map or use the bpf program.
    This check is necessary because the bpf maps and programs are using a
    anonymous inode as their shared inode so the normal way of checking the
    files and sockets when passing between processes cannot work properly on
    eBPF object. This check only works when the BPF_SYSCALL is configured.
    
    Signed-off-by: Chenbo Feng <fengc@google.com>
    Acked-by: Stephen Smalley <sds@tycho.nsa.gov>
    Reviewed-by: James Morris <james.l.morris@oracle.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Bug: 30950746
    Change-Id: I5b2cf4ccb4eab7eda91ddd7091d6aa3e7ed9f2cd
    
    (cherry picked from commit f66e448cfda021b0bcd884f26709796fe19c7cc1)
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -246,6 +246,9 @@ int bpf_prog_test_run_skb(struct bpf_prog *prog, const union bpf_attr *kattr,
 #ifdef CONFIG_BPF_SYSCALL
 DECLARE_PER_CPU(int, bpf_prog_active);
 
+extern const struct file_operations bpf_map_fops;
+extern const struct file_operations bpf_prog_fops;
+
 #define BPF_PROG_TYPE(_id, _ops) \
 	extern const struct bpf_verifier_ops _ops;
 #define BPF_MAP_TYPE(_id, _ops) \
