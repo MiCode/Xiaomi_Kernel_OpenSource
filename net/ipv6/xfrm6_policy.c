709d3a4e3edcb0963185f9481b178c9cfb9aab55
Blagovest Kolenichev <bkolenichev@codeaurora.org>
2017-10-19 15:28:13 -0700

    net: restore upstream changes which break routing in tunnel scenarios
    This reverts commit:
    
      3d78e43 net: Revert upstream changes which break routing in
              tunnel scenarios
    
    Restored changes are:
    
      f1900fb net: Really fix vti6 with oif in dst lookups
      4148987 net: Fix vti use case with oif in dst lookups for IPv6
      42a7b32 xfrm: Add oif to dst lookups
    
    Now it is save to restore the reverted upstream changes. Also
    below upstream changes are coming with android@73a2b70 (4.4.92)
    which will hit conflict if the above changes are not restored.
    
      c6a272a BACKPORT: net: xfrm: support setting an output mark.
      511953d UPSTREAM: xfrm: Only add l3mdev oif to dst lookups
    
    Change-Id: Ia34abd5b9e2625d44922c018756d2214e2bd47bf
    Signed-off-by: Blagovest Kolenichev <bkolenichev@codeaurora.org>

--- net/ipv6/xfrm6_policy.c
+++ net/ipv6/xfrm6_policy.c
@@ -27,7 +27,7 @@
 
 static struct xfrm_policy_afinfo xfrm6_policy_afinfo;
 
-static struct dst_entry *xfrm6_dst_lookup(struct net *net, int tos,
+static struct dst_entry *xfrm6_dst_lookup(struct net *net, int tos, int oif,
 					  const xfrm_address_t *saddr,
 					  const xfrm_address_t *daddr)
 {
@@ -36,6 +36,8 @@ static struct dst_entry *xfrm6_dst_lookup(struct net *net, int tos,
 	int err;
 
 	memset(&fl6, 0, sizeof(fl6));
+	fl6.flowi6_oif = oif;
+	fl6.flowi6_flags = FLOWI_FLAG_SKIP_NH_OIF;
 	memcpy(&fl6.daddr, daddr, sizeof(fl6.daddr));
 	if (saddr)
 		memcpy(&fl6.saddr, saddr, sizeof(fl6.saddr));
@@ -51,13 +53,13 @@ static struct dst_entry *xfrm6_dst_lookup(struct net *net, int tos,
 	return dst;
 }
 
-static int xfrm6_get_saddr(struct net *net,
+static int xfrm6_get_saddr(struct net *net, int oif,
 			   xfrm_address_t *saddr, xfrm_address_t *daddr)
 {
 	struct dst_entry *dst;
 	struct net_device *dev;
 
-	dst = xfrm6_dst_lookup(net, 0, NULL, daddr);
+	dst = xfrm6_dst_lookup(net, 0, oif, NULL, daddr);
 	if (IS_ERR(dst))
 		return -EHOSTUNREACH;
 
