ac6fe6e43141707edce5619421b00b29a45a2286
David Dai <daidavid1@codeaurora.org>
2017-10-18 17:11:53 -0700

    msm: msm_bus: Add msm bus trace header snapshot
    This is a snapshot of the msm bus trace header as of msm-4.9
    commit 27c11b91dba00c9 ('spcom: ignore CONNECTED notification
    while closing the channel').
    
    Change-Id: Idb036bbb7db6a7ecece83bc1789b96953e027d0e
    Signed-off-by: David Dai <daidavid1@codeaurora.org>

---
+++ include/dt-bindings/msm/msm-bus-rule-ops.h
@@ -0,0 +1,34 @@
+/* Copyright (c) 2014-2016, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __MSM_BUS_RULE_OPS_H
+#define __MSM_BUS_RULE_OPS_H
+
+#define FLD_IB	0
+#define FLD_AB	1
+#define FLD_CLK	2
+
+#define OP_LE	0
+#define OP_LT	1
+#define OP_GE	2
+#define OP_GT	3
+#define OP_NOOP	4
+
+#define RULE_STATE_NOT_APPLIED	0
+#define RULE_STATE_APPLIED	1
+
+#define THROTTLE_ON	0
+#define THROTTLE_OFF	1
+#define THROTTLE_REG	2
+
+
+#endif
