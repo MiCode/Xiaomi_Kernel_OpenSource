20fca09e45643550652f0d3db89f8b6b381f17d1
Ajay Agarwal <ajaya@codeaurora.org>
2017-10-25 15:16:11 +0530

    USB: gadget: Replace %pM with %pm
    commit c173d5504d08ca0 ("USB: gadget: Replace %pK with
    %pM") modified the MAC address format specifier to %pM.
    So when the host PC asks for the MAC address, the
    device sends the address with colons present. Linux
    Host machines cannot handle this and ECM function fails
    to bind. Fix this by changing the format specifier to
    %pm which removes the colons and sends the MAC address.
    
    Change-Id: Id3eebfa371bd2cf976ee0f44e8a7b54690f43e77
    Signed-off-by: Ajay Agarwal <ajaya@codeaurora.org>

--- drivers/usb/gadget/function/u_ether.c
+++ drivers/usb/gadget/function/u_ether.c
@@ -1132,7 +1132,7 @@ int gether_get_host_addr_cdc(struct net_device *net, char *host_addr, int len)
 		return -EINVAL;
 
 	dev = netdev_priv(net);
-	snprintf(host_addr, len, "%pM", dev->host_mac);
+	snprintf(host_addr, len, "%pm", dev->host_mac);
 
 	return strlen(host_addr);
 }
