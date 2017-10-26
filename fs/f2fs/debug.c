eb25e774fe3114a2ba4a12c2dffcc1a565d97af8
Jaegeuk Kim <jaegeuk@kernel.org>
2017-10-26 10:31:22 +0200

    f2fs: show current cp state
    This patch shows whether checkpoint met any error case.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/debug.c
+++ fs/f2fs/debug.c
@@ -266,9 +266,10 @@ static int stat_show(struct seq_file *s, void *v)
 
 		update_general_status(si->sbi);
 
-		seq_printf(s, "\n=====[ partition info(%s). #%d, %s]=====\n",
+		seq_printf(s, "\n=====[ partition info(%s). #%d, %s, CP: %s]=====\n",
 			bdevname(si->sbi->sb->s_bdev, devname), i++,
-			f2fs_readonly(si->sbi->sb) ? "RO": "RW");
+			f2fs_readonly(si->sbi->sb) ? "RO": "RW",
+			f2fs_cp_error(si->sbi) ? "Error": "Good");
 		seq_printf(s, "[SB: 1] [CP: 2] [SIT: %d] [NAT: %d] ",
 			   si->sit_area_segs, si->nat_area_segs);
 		seq_printf(s, "[SSA: %d] [MAIN: %d",
