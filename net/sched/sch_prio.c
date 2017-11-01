a3e068e8e81c59c4060585e55acdc830bfcfb19d
Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
2017-11-01 15:33:57 -0600

    net_sched: Add flow control support to prio qdisc
    Add enable_flow flag to the prio qdisc. Packet flow is enabled by
    default, but can be disabled from userspace (e.g. IPROUTE2 tc tool).
    This allows for suspending packet dequeue on a per-qdisc basis,
    which is needed to support Quality of Service (QOS).
    
    Export a function that will look up desired qdisc and call it's
    registered change function to enable/disable flow. This API also
    returns the size of the qdisc in order to be able to collect data on
    the size of the qdisc before doing flow control operations. This is
    required to effectively diagnose the state of the queues when
    debugging flow control.
    
    The PRIO qdisc supports flow control, such that packet
    dequeue can be disabled based on boolean flag 'enable_flow'.
    When flow is re-enabled, the latency for new packets
    arriving at network driver is high.  To reduce the delay in
    scheduling packets, the qdisc will now invoke
    __netif_schedule() to expedite dequeue.  This significantly
    reduces the latency of packets arriving at network driver.
    
    CRs-Fixed: 2086060
    Change-Id: I0e9096e4241d459540028558fdec18ece460d517
    Signed-off-by: Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>

--- net/sched/sch_prio.c
+++ net/sched/sch_prio.c
@@ -18,6 +18,7 @@
 #include <linux/string.h>
 #include <linux/errno.h>
 #include <linux/skbuff.h>
+#include <linux/netdevice.h>
 #include <net/netlink.h>
 #include <net/pkt_sched.h>
 #include <net/pkt_cls.h>
@@ -28,6 +29,7 @@ struct prio_sched_data {
 	struct tcf_block *block;
 	u8  prio2band[TC_PRIO_MAX+1];
 	struct Qdisc *queues[TCQ_PRIO_BANDS];
+	u8 enable_flow;
 };
 
 
@@ -101,6 +103,9 @@ static struct sk_buff *prio_peek(struct Qdisc *sch)
 	struct prio_sched_data *q = qdisc_priv(sch);
 	int prio;
 
+	if (!q->enable_flow)
+		return NULL;
+
 	for (prio = 0; prio < q->bands; prio++) {
 		struct Qdisc *qdisc = q->queues[prio];
 		struct sk_buff *skb = qdisc->ops->peek(qdisc);
@@ -114,6 +119,9 @@ static struct sk_buff *prio_dequeue(struct Qdisc *sch)
 {
 	struct prio_sched_data *q = qdisc_priv(sch);
 	int prio;
+
+	if (!q->enable_flow)
+		return NULL;
 
 	for (prio = 0; prio < q->bands; prio++) {
 		struct Qdisc *qdisc = q->queues[prio];
@@ -139,6 +147,7 @@ prio_reset(struct Qdisc *sch)
 		qdisc_reset(q->queues[prio]);
 	sch->qstats.backlog = 0;
 	sch->q.qlen = 0;
+	q->enable_flow = 1;
 }
 
 static void
@@ -158,6 +167,7 @@ static int prio_tune(struct Qdisc *sch, struct nlattr *opt)
 	struct Qdisc *queues[TCQ_PRIO_BANDS];
 	int oldbands = q->bands, i;
 	struct tc_prio_qopt *qopt;
+	int flow_change = 0;
 
 	if (nla_len(opt) < sizeof(*qopt))
 		return -EINVAL;
@@ -183,6 +193,10 @@ static int prio_tune(struct Qdisc *sch, struct nlattr *opt)
 	}
 
 	sch_tree_lock(sch);
+	if (q->enable_flow != qopt->enable_flow) {
+		q->enable_flow = qopt->enable_flow;
+		flow_change = 1;
+	}
 	q->bands = qopt->bands;
 	memcpy(q->prio2band, qopt->priomap, TC_PRIO_MAX+1);
 
@@ -201,6 +215,12 @@ static int prio_tune(struct Qdisc *sch, struct nlattr *opt)
 	}
 
 	sch_tree_unlock(sch);
+
+	/* Schedule qdisc when flow re-enabled */
+	if (flow_change && q->enable_flow) {
+		if (!test_bit(__QDISC_STATE_DEACTIVATED, &sch->state))
+			__netif_schedule(qdisc_root(sch));
+	}
 	return 0;
 }
 
@@ -226,6 +246,7 @@ static int prio_dump(struct Qdisc *sch, struct sk_buff *skb)
 	struct tc_prio_qopt opt;
 
 	opt.bands = q->bands;
+	opt.enable_flow = q->enable_flow;
 	memcpy(&opt.priomap, q->prio2band, TC_PRIO_MAX + 1);
 
 	if (nla_put(skb, TCA_OPTIONS, sizeof(opt), &opt))
