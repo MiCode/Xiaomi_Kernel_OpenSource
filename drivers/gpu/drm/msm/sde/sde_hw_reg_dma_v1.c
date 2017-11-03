6d3f41203e922046ed91d1654450c86ce07da76f
Harsh Sahu <hsahu@codeaurora.org>
2017-11-03 14:56:20 -0700

    drm/msm/gem: add cache flush api in msm_gem
    msm_gem object allocates the memory using kzalloc or vmalloc based on
    memory size requirement. This is cacheable memory and must be flushed
    before any cpu access on it. DSI and LUTDMA path does cpu access on this
    memory while INLINE ROT and WB allows device access.
    This change adds a sync api to ensure that cache is clean before device
    does any DMA operation on the buffer.
    
    Change-Id: I9c3e0fd4a05b66a041092c09f9719dc691664770
    Signed-off-by: Harsh Sahu <hsahu@codeaurora.org>

--- drivers/gpu/drm/msm/sde/sde_hw_reg_dma_v1.c
+++ drivers/gpu/drm/msm/sde/sde_hw_reg_dma_v1.c
@@ -526,6 +526,7 @@ static int write_kick_off_v1(struct sde_reg_dma_kickoff_cfg *cfg)
 	cmd1 |= (cfg->op == REG_DMA_WRITE) ? (BIT(22)) : 0;
 	cmd1 |= (SIZE_DWORD(cfg->dma_buf->index) & MAX_DWORDS_SZ);
 
+	msm_gem_sync(cfg->dma_buf->buf);
 	SET_UP_REG_DMA_REG(hw, reg_dma);
 	SDE_REG_WRITE(&hw, reg_dma_opmode_offset, BIT(0));
 	val = SDE_REG_READ(&hw, reg_dma_intr_4_status_offset);
