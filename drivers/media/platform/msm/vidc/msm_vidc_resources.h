d23347b1844d9e99a8da46c1c1cffae6feb0b5cf
Maheshwar Ajja <majja@codeaurora.org>
2017-11-03 11:41:36 -0700

    msm: vidc: Enable caching for pagetable pages
    Enable caching for video pagetable pages by setting
    attribute DOMAIN_ATTR_USE_UPSTREAM_HINT for video
    context banks.
    
    Change-Id: I4c340db17013c2e48c7d08445fe3eb69e2ee49c7
    Signed-off-by: Maheshwar Ajja <majja@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc_resources.h
+++ drivers/media/platform/msm/vidc/msm_vidc_resources.h
@@ -185,6 +185,7 @@ struct msm_vidc_platform_resources {
 	int msm_vidc_firmware_unload_delay;
 	uint32_t msm_vidc_pwr_collapse_delay;
 	bool non_fatal_pagefaults;
+	bool cache_pagetables;
 	struct msm_vidc_codec_data *codec_data;
 	int codec_data_count;
 	struct msm_vidc_csc_coeff *csc_coeff_data;
