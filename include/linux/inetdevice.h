bb6047171d2168c83a32a476bdaf942fabeee361
Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
2017-11-05 17:36:53 -0700

    netfilter: nf_defrag_ipv4: Add sysctl to disable per interface
    Add a sysctl nf_ipv4_defrag_skip to skip defragmentation per
    interface. This is set 0 to preserve existing behavior (always
    defrag per interface).
    
    This is useful for pure ipv4 forwarding scenarios (without NAT)
    in conjunction with xfrm. It appears that network stack defrags
    the packets and then forwards them to xfrm which then encrypts
    and then later fragments them on a different boundary compared
    to the source.
    
    CRs-Fixed: 2140310
    Change-Id: I11956284a9692579274e8626f61cc6432232254c
    Signed-off-by: Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>

--- include/linux/inetdevice.h
+++ include/linux/inetdevice.h
@@ -130,6 +130,8 @@ static inline void ipv4_devconf_setall(struct in_device *in_dev)
 #define IN_DEV_ARP_ANNOUNCE(in_dev)	IN_DEV_MAXCONF((in_dev), ARP_ANNOUNCE)
 #define IN_DEV_ARP_IGNORE(in_dev)	IN_DEV_MAXCONF((in_dev), ARP_IGNORE)
 #define IN_DEV_ARP_NOTIFY(in_dev)	IN_DEV_MAXCONF((in_dev), ARP_NOTIFY)
+#define IN_DEV_NF_IPV4_DEFRAG_SKIP(in_dev) \
+	IN_DEV_ORCONF((in_dev), NF_IPV4_DEFRAG_SKIP)
 
 struct in_ifaddr {
 	struct hlist_node	hash;
