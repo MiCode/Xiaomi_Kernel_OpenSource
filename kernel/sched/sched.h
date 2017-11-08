d41481eebd9926503af255f1552ef0dbced93f3c
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2017-11-08 12:13:47 -0800

    ANDROID: sched/walt: Fix compilation issue for x86_64
    Below compilation errors observed when SCHED_WALT enabled and
    FAIR_GROUP_SCHED is disabled, fix it.
    
      CC      kernel/sched/walt.o
    kernel/sched/walt.c: In function .walt_inc_cfs_cumulative_runnable_avg.:
    kernel/sched/walt.c:157:8: error: .struct cfs_rq. has no member named .cumulative_runnable_avg.
      cfs_rq->cumulative_runnable_avg += p->ravg.demand;
            ^
    kernel/sched/walt.c: In function .walt_dec_cfs_cumulative_runnable_avg.:
    kernel/sched/walt.c:163:8: error: .struct cfs_rq. has no member named .cumulative_runnable_avg.
      cfs_rq->cumulative_runnable_avg -= p->ravg.demand;
            ^
    make[2]: *** [kernel/sched/walt.o] Error 1
    make[1]: *** [kernel/sched] Error 2
    make: *** [kernel] Error 2
    
    Change-Id: I52d506aa5494f4c351f19e6bb2f1a75c8333f321
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
    (cherry-picked for android-4.14)
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -483,10 +483,6 @@ struct cfs_rq {
 	struct list_head leaf_cfs_rq_list;
 	struct task_group *tg;	/* group that "owns" this runqueue */
 
-#ifdef CONFIG_SCHED_WALT
-	u64 cumulative_runnable_avg;
-#endif
-
 #ifdef CONFIG_CFS_BANDWIDTH
 	int runtime_enabled;
 	u64 runtime_expires;
@@ -496,6 +492,9 @@ struct cfs_rq {
 	u64 throttled_clock_task_time;
 	int throttled, throttle_count;
 	struct list_head throttled_list;
+#ifdef CONFIG_SCHED_WALT
+	u64 cumulative_runnable_avg;
+#endif /* CONFIG_SCHED_WALT */
 #endif /* CONFIG_CFS_BANDWIDTH */
 #endif /* CONFIG_FAIR_GROUP_SCHED */
 };
