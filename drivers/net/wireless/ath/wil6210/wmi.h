7d7d37295bb3a682557743b62a80e9d1be12545e
Dedy Lansky <dlansky@codeaurora.org>
2017-11-08 12:43:53 +0200

    wil6210: drop RX probe reponses with low SNR
    FW now reports SNR for RX management frames.
    Drop probe responses with SNR lower than the configured threshold.
    
    Change-Id: Ife1863b07aebe5cb6097420290c0c10b590c2da1
    Signed-off-by: Dedy Lansky <dlansky@codeaurora.org>

--- drivers/net/wireless/ath/wil6210/wmi.h
+++ drivers/net/wireless/ath/wil6210/wmi.h
@@ -1822,7 +1822,7 @@ struct wmi_rx_mgmt_info {
 	u8 range;
 	u8 sqi;
 	__le16 stype;
-	__le16 status;
+	__le16 snr;
 	__le32 len;
 	/* Not resolved when == 0xFFFFFFFF == > Broadcast to all MIDS */
 	u8 qid;
