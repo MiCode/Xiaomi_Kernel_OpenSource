1dd0857f2d3ab57fea2b5546798c1a3b2f67a1ac
Lina Iyer <ilina@codeaurora.org>
2017-11-09 00:57:38 +0000

    drivers: qcom: fix compilation errors
    Fix compilation errors when compiling with 4.9 kernel on arm
    architectures. do_div() likes its argument to be a uint64_t instead of a
    signed value.
    
    Change-Id: I81cf23fd649120a35a21f1aabc9d82192e330b67
    Signed-off-by: Lina Iyer <ilina@codeaurora.org>

--- drivers/cpuidle/lpm-levels.h
+++ drivers/cpuidle/lpm-levels.h
@@ -124,7 +124,7 @@ uint32_t *get_per_cpu_max_residency(int cpu);
 uint32_t *get_per_cpu_min_residency(int cpu);
 extern struct lpm_cluster *lpm_root_node;
 
-#if CONFIG_SMP
+#if defined(CONFIG_SMP)
 extern DEFINE_PER_CPU(bool, pending_ipi);
 static inline bool is_IPI_pending(const struct cpumask *mask)
 {
