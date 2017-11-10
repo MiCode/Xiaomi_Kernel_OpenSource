9c88935eb70fe949d075dae511b33aa0650d36a2
Harsh Shah <harshs@codeaurora.org>
2017-11-10 05:33:04 -0800

    msm: camera: core: Add missing list_del_init
    Without list_del_init, this request will not be deleted from previous
    list and also added to free list. This may result in double add.
    
    Change-Id: I5503acb13260d01d72bd07bb2a7d6afe7ffc430d
    Signed-off-by: Harsh Shah <harshs@codeaurora.org>

--- drivers/media/platform/msm/camera/cam_core/cam_context_utils.c
+++ drivers/media/platform/msm/camera/cam_core/cam_context_utils.c
@@ -178,6 +178,7 @@ static void cam_context_sync_callback(int32_t sync_obj, int status, void *data)
 			req->ctx = NULL;
 			req->flushed = 0;
 			spin_lock(&ctx->lock);
+			list_del_init(&req->list);
 			list_add_tail(&req->list, &ctx->free_req_list);
 			spin_unlock(&ctx->lock);
 		}
