0b4f73c9b47436aa8256cb6af3b131a0e4c27189
Vinayak Menon <vinmenon@codeaurora.org>
2017-11-15 18:38:55 +0530

    Revert "mm: memory: reduce fault_around_bytes"
    This reverts commit 23f8a2f5f2e0.
    
    fault_around_bytes were reduced as it was found to cause reclaim
    issues. The reclaim issues were mainly because of faultaround
    producing young ptes. The following patches will make faultaround
    produce old ptes. Thus revert the fault_around_bytes to its
    original value.
    
    Change-Id: If9a385eead5a5d7a0d40fed41ddf4753e15d2998
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- mm/memory.c
+++ mm/memory.c
@@ -3654,7 +3654,7 @@ int finish_fault(struct vm_fault *vmf)
 }
 
 static unsigned long fault_around_bytes __read_mostly =
-	rounddown_pow_of_two(4096);
+	rounddown_pow_of_two(65536);
 
 #ifdef CONFIG_DEBUG_FS
 static int fault_around_bytes_get(void *data, u64 *val)
