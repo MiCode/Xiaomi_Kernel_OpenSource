501c5bb5f78c452054875d4d7c50974e7c29ac07
Sayali Lokhande <sayalil@codeaurora.org>
2017-11-15 15:54:33 +0530

    scsi: ufs: Get TX and RX fsm state in case of abort
    If ufshcd_print_fsm_state() is called during UIC
    commands failure, mutex lockup issue is observed
    for uic_cmd_mutex. To fix this, read TX and RX
    FSM states for UFS aborts cases only, for debugging
    purpose.
    
    Change-Id: I716251ec90ef406d9cff1f8a6aa6bc2f8f46926d
    Signed-off-by: Sayali Lokhande <sayalil@codeaurora.org>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -908,7 +908,6 @@ static void ufshcd_print_host_state(struct ufs_hba *hba)
 		hba->capabilities, hba->caps);
 	dev_err(hba->dev, "quirks=0x%x, dev. quirks=0x%x\n", hba->quirks,
 		hba->dev_info.quirks);
-	ufshcd_print_fsm_state(hba);
 }
 
 /**
@@ -7033,6 +7032,7 @@ static int ufshcd_abort(struct scsi_cmnd *cmd)
 	 */
 	scsi_print_command(cmd);
 	if (!hba->req_abort_count) {
+		ufshcd_print_fsm_state(hba);
 		ufshcd_print_host_regs(hba);
 		ufshcd_print_host_state(hba);
 		ufshcd_print_pwr_info(hba);
