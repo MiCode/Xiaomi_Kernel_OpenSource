e881b353dbccd78fabd65feb1c046f01f7acd6f4
Sriharsha Allenki <sallenki@codeaurora.org>
2017-11-20 20:42:11 +0530

    USB: dwc3: Fix issues in ISOC endpoint handling
    If the interval for which the requests are
    intended is already expired, START TRANSFER fails.
    Handle this by issuing ENDXFER command and wait
    for the XFERNOTREADY event to try once again.
    
    Change-Id: I05fb3564d02528d9c952c789ec7a1b39b0bcedff
    Signed-off-by: Sriharsha Allenki <sallenki@codeaurora.org>

--- drivers/usb/dwc3/gadget.h
+++ drivers/usb/dwc3/gadget.h
@@ -68,6 +68,14 @@ static inline struct dwc3_request *next_request(struct list_head *list)
 	return list_first_entry(list, struct dwc3_request, list);
 }
 
+static inline void dwc3_gadget_move_pending_list_front(struct dwc3_request *req)
+{
+	struct dwc3_ep		*dep = req->dep;
+
+	req->started = false;
+	list_move(&req->list, &dep->pending_list);
+}
+
 static inline void dwc3_gadget_move_started_request(struct dwc3_request *req)
 {
 	struct dwc3_ep		*dep = req->dep;
