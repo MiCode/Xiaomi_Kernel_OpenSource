930b1cd5f51652f4968e41c101368e1f0d0f9731
Eric Biggers <ebiggers@google.com>
2017-11-22 11:51:36 -0800

    UPSTREAM: crypto: chacha20 - Use unaligned access macros when loading key and IV
    The generic ChaCha20 implementation has a cra_alignmask of 3, which
    ensures that the key passed into crypto_chacha20_setkey() and the IV
    passed into crypto_chacha20_init() are 4-byte aligned.  However, these
    functions are also called from the ARM and ARM64 implementations of
    ChaCha20, which intentionally do not have a cra_alignmask set.  This is
    broken because 32-bit words are being loaded from potentially-unaligned
    buffers without the unaligned access macros.
    
    Fix it by using the unaligned access macros when loading the key and IV.
    
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Acked-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    
    (cherry picked from commit dbd872a123fab81d0fa235a265c39e5ccdf735b3)
    Bug: 112008522
    Test: As series, see Ic61c13b53facfd2173065be715a7ee5f3af8760b
    Change-Id: I1809f6ffe0b679a2554a660eaca360d0fa3b0dd1
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- crypto/chacha20_generic.c
+++ crypto/chacha20_generic.c
@@ -9,16 +9,12 @@
  * (at your option) any later version.
  */
 
+#include <asm/unaligned.h>
 #include <crypto/algapi.h>
 #include <linux/crypto.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <crypto/chacha20.h>
-
-static inline u32 le32_to_cpuvp(const void *p)
-{
-	return le32_to_cpup(p);
-}
 
 static void chacha20_docrypt(u32 *state, u8 *dst, const u8 *src,
 			     unsigned int bytes)
@@ -54,10 +50,10 @@ void crypto_chacha20_init(u32 *state, struct chacha20_ctx *ctx, u8 *iv)
 	state[9]  = ctx->key[5];
 	state[10] = ctx->key[6];
 	state[11] = ctx->key[7];
-	state[12] = le32_to_cpuvp(iv +  0);
-	state[13] = le32_to_cpuvp(iv +  4);
-	state[14] = le32_to_cpuvp(iv +  8);
-	state[15] = le32_to_cpuvp(iv + 12);
+	state[12] = get_unaligned_le32(iv +  0);
+	state[13] = get_unaligned_le32(iv +  4);
+	state[14] = get_unaligned_le32(iv +  8);
+	state[15] = get_unaligned_le32(iv + 12);
 }
 EXPORT_SYMBOL_GPL(crypto_chacha20_init);
 
@@ -71,7 +67,7 @@ int crypto_chacha20_setkey(struct crypto_tfm *tfm, const u8 *key,
 		return -EINVAL;
 
 	for (i = 0; i < ARRAY_SIZE(ctx->key); i++)
-		ctx->key[i] = le32_to_cpuvp(key + i * sizeof(u32));
+		ctx->key[i] = get_unaligned_le32(key + i * sizeof(u32));
 
 	return 0;
 }
