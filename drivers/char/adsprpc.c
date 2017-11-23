33b24c0895e5a409a4343c83903300710f3259ce
c_mtharu <mtharu@codeaurora.org>
2017-11-23 19:43:23 +0530

    msm: ADSPRPC: Change SSC VMID from 5 to 38
    Change Dest VM of SSC from 5 to 38 and it can be unmapped to
    HLOS through hyp_assign.
    
    Change-Id: I8e4ace8e9722d4fa79b553ad6b8d29353954f8c5
    Signed-off-by: Tharun Kumar Merugu <mtharu@codeaurora.org>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -56,7 +56,7 @@
 #define ADSP_MMAP_HEAP_ADDR 4
 #define ADSP_MMAP_REMOTE_HEAP_ADDR 8
 #define FASTRPC_ENOSUCH 39
-#define VMID_SSC_Q6     5
+#define VMID_SSC_Q6    38
 #define VMID_ADSP_Q6    6
 #define AC_VM_ADSP_HEAP_SHARED 33
 #define DEBUGFS_SIZE 1024
