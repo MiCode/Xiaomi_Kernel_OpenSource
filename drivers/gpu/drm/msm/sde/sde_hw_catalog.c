90fb854d2e34863a26dbc469e677109c8ed873ea
Ch Ganesh Kumar <chganesh@codeaurora.org>
2017-12-05 15:29:16 +0530

    drm/msm/sde: Add HDR support capability of chipset
    This change will helps to invoke HDR library
    
    Change-Id: I31f40efdc170c6c0d3fbbfa2591b92b2ade9b8cd
    Signed-off-by: Ch Ganesh Kumar <chganesh@codeaurora.org>

--- drivers/gpu/drm/msm/sde/sde_hw_catalog.c
+++ drivers/gpu/drm/msm/sde/sde_hw_catalog.c
@@ -3173,7 +3173,8 @@ static int sde_hardware_format_caps(struct sde_mdss_cfg *sde_cfg,
 	if (IS_SDE_MAJOR_MINOR_SAME((hw_rev), SDE_HW_VER_300) ||
 	    IS_SDE_MAJOR_MINOR_SAME((hw_rev), SDE_HW_VER_301) ||
 	    IS_SDE_MAJOR_MINOR_SAME((hw_rev), SDE_HW_VER_400) ||
-	    IS_SDE_MAJOR_MINOR_SAME((hw_rev), SDE_HW_VER_401))
+	    IS_SDE_MAJOR_MINOR_SAME((hw_rev), SDE_HW_VER_401) ||
+	    IS_SDE_MAJOR_MINOR_SAME((hw_rev), SDE_HW_VER_410))
 		sde_cfg->has_hdr = true;
 
 	index = sde_copy_formats(sde_cfg->dma_formats, dma_list_size,
