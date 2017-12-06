4e619b4a7272668264f49040aa4809d34742b1ac
Harry Yang <harryy@codeaurora.org>
2017-12-06 10:13:21 -0800

    power: smb-lib: fix a mutex deadlock issue
    An early error return without releasing a mutex in functon
    smblib_vbus_regulator_enable will lead to deadlock in some
    situation.
    
    Fix it.
    
    Change-Id: I41178ee3aadfae57566e5f14f81ed82b7b1986cc
    Signed-off-by: Harry Yang <harryy@codeaurora.org>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -1462,8 +1462,10 @@ int smblib_vbus_regulator_enable(struct regulator_dev *rdev)
 	if (!chg->usb_icl_votable) {
 		chg->usb_icl_votable = find_votable("USB_ICL");
 
-		if (!chg->usb_icl_votable)
-			return -EINVAL;
+		if (!chg->usb_icl_votable) {
+			rc = -EINVAL;
+			goto unlock;
+		}
 	}
 	vote(chg->usb_icl_votable, USBIN_USBIN_BOOST_VOTER, true, 0);
 
