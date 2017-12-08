ff9900827c20ef657ecc8a959486f72bbb7863a9
Sai Krishna Juturi <jsaikrishna@codeaurora.org>
2017-12-08 15:00:43 +0530

    dwc3-msm: Notify proper current values in case of floating charger
    Currently our driver is notifying ETIMEDOUT to PMIC
    incase of floating charger. Consider if we are inserting
    an SDP charger slowly that time also we will detect
    it as floating charger then also we are notifying
    ETIMEDOUT irrespective of current that is notified from
    gadget driver. Hence notify charging current properly
    if enumeration happens even in case of
    floating charger so that PMIC can update real charger
    type to SDP
    
    Signed-off-by: Sai Krishna Juturi <jsaikrishna@codeaurora.org>
    Change-Id: Ibeeeafac0834c9df7099a3f0bad274ddb393de47

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -3720,7 +3720,10 @@ static int dwc3_msm_gadget_vbus_draw(struct dwc3_msm *mdwc, unsigned mA)
 
 	psy_type = get_psy_type(mdwc);
 	if (psy_type == POWER_SUPPLY_TYPE_USB_FLOAT) {
-		pval.intval = -ETIMEDOUT;
+		if (!mA)
+			pval.intval = -ETIMEDOUT;
+		else
+			pval.intval = 1000 * mA;
 		goto set_prop;
 	}
 
