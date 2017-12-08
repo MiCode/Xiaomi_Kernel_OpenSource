4a68d9bc2b45eab8cd969ede1e26723447f35de1
YT Chang <yt.chang@mediatek.com>
2017-12-08 09:40:37 +0800

    sched: don't use SCHED_WARN_ON to prevent deadlock
    SCHED_WARN_ON will use printk and in scheduler
    use printk will cause deadlock
    don't use SCHED_WARN_ON to prevent deadlock
    
    Change-Id: I10463af47bbe99ca94669e1c77941628375d1e9d
    Signed-off-by: YT Chang <yt.chang@mediatek.com>
    CR-Id: ALPS04222778
    Feature: Energy Aware Scheduling Plus (EAS+)

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -260,7 +260,6 @@ const struct sched_class fair_sched_class;
 #ifdef CONFIG_FAIR_GROUP_SCHED
 static inline struct task_struct *task_of(struct sched_entity *se)
 {
-	SCHED_WARN_ON(!entity_is_task(se));
 	return container_of(se, struct task_struct, se);
 }
 
