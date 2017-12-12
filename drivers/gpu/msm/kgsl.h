074a6aefe5c6f02ecaf346a088436fab90ce5275
Harshdeep Dhatt <hdhatt@codeaurora.org>
2017-12-12 11:35:51 -0700

    msm: kgsl: Increase memstore size
    Increase the memstore size to 32k. Memstore has for long
    been limited to 200 contexts and now it will be 814.
    
    Change-Id: I1287669f0be38afe13ff8e386f5296e8f4e77627
    Signed-off-by: Harshdeep Dhatt <hdhatt@codeaurora.org>

--- drivers/gpu/msm/kgsl.h
+++ drivers/gpu/msm/kgsl.h
@@ -50,7 +50,7 @@
 /* The number of memstore arrays limits the number of contexts allowed.
  * If more contexts are needed, update multiple for MEMSTORE_SIZE
  */
-#define KGSL_MEMSTORE_SIZE	((int)(PAGE_SIZE * 2))
+#define KGSL_MEMSTORE_SIZE	((int)(PAGE_SIZE * 8))
 #define KGSL_MEMSTORE_GLOBAL	(0)
 #define KGSL_PRIORITY_MAX_RB_LEVELS 4
 #define KGSL_MEMSTORE_MAX	(KGSL_MEMSTORE_SIZE / \
