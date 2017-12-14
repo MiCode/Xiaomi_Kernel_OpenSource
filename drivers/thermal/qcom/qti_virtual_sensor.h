0b90b1e71b94360beb4ccf61a3c15aa7b6a1c8e0
Ram Chandrasekar <rkumbako@codeaurora.org>
2017-12-13 17:11:54 -0700

    drivers: thermal: virtual-sensor: Add virtual sensor
    Add snapshot of virtual sensor driver from kernel 4.9
    as of 'commit <4291fee61276> ("drivers: qti-virtual-sensor: Add deca cpu
    max virtual sensor for msm8953")'.
    
    Change-Id: I4db7ded85cc4f3123b50e48690e0b6a0644cb91e
    Signed-off-by: Ram Chandrasekar <rkumbako@codeaurora.org>

---
+++ drivers/thermal/qcom/qti_virtual_sensor.h
@@ -0,0 +1,29 @@
+/* Copyright (c) 2017-2018, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __QTI_VIRT_SENS_H__
+#define __QTI_VIRT_SENS_H__
+
+#ifdef CONFIG_QTI_VIRTUAL_SENSOR
+
+int qti_virtual_sensor_register(struct device *dev);
+
+#else
+
+static inline int qti_virtual_sensor_register(struct device *dev)
+{
+	return -ENODEV;
+}
+
+#endif /* CONFIG_QTI_VIRTUAL_SENSOR */
+
+#endif /* __QTI_VIRT_SENS_H__ */
