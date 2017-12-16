080b830f55c3fb9611e460f7c3ac8fbd48f38678
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2017-12-15 17:44:50 -0800

    pinctrl: qcom: spmi-gpio: add support to enable/disable output
    Currently, if the GPIO is configured as output in the bootloader
    and user changes the mode to input in HLOS, it would end up
    getting configured as input/output. Functionally, this is fine;
    however, there may be some requirements where the output needs
    to be disabled so that it can be used only for input.
    
    Add support to enable/disable output mode through "output-enable"
    or "output-disable" pinctrl properties.
    
    Change-Id: Ie857f94d529a6209bcb82ef3775d414f19d5678a
    Signed-off-by: Subbaraman Narayanamurthy <subbaram@codeaurora.org>

--- drivers/pinctrl/qcom/pinctrl-spmi-gpio.c
+++ drivers/pinctrl/qcom/pinctrl-spmi-gpio.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2014, 2016-2017 The Linux Foundation. All rights reserved.
  */
 
 #include <linux/gpio/driver.h>
@@ -424,6 +424,9 @@ static int pmic_gpio_config_get(struct pinctrl_dev *pctldev,
 			return -EINVAL;
 		arg = 1;
 		break;
+	case PIN_CONFIG_OUTPUT_ENABLE:
+		arg = pad->output_enabled;
+		break;
 	case PIN_CONFIG_OUTPUT:
 		arg = pad->out_value;
 		break;
@@ -502,6 +505,9 @@ static int pmic_gpio_config_set(struct pinctrl_dev *pctldev, unsigned int pin,
 			break;
 		case PIN_CONFIG_INPUT_ENABLE:
 			pad->input_enabled = arg ? true : false;
+			break;
+		case PIN_CONFIG_OUTPUT_ENABLE:
+			pad->output_enabled = arg ? true : false;
 			break;
 		case PIN_CONFIG_OUTPUT:
 			pad->output_enabled = true;
