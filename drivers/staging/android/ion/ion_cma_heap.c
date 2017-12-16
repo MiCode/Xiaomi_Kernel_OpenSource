55d1200ac264db208b972c49ca3ef007d178b9a8
John Stultz <john.stultz@linaro.org>
2017-12-15 19:59:47 -0800

    staging: ion: Fix ion_cma_heap allocations
    In trying to add support for drm_hwcomposer to HiKey,
    I've needed to utilize the ION CMA heap, and I've noticed
    problems with allocations on newer kernels failing.
    
    It seems back with 204f672255c2 ("ion: Use CMA APIs directly"),
    the ion_cma_heap code was modified to use the CMA API, but
    kept the arguments as buffer lengths rather then number of pages.
    
    This results in errors as we don't have enough pages in CMA to
    satisfy the exaggerated requests.
    
    This patch converts the ion_cma_heap CMA API usage to properly
    request pages.
    
    It also fixes a minor issue in the allocation where in the error
    path, the cma_release is called with the buffer->size value which
    hasn't yet been set.
    
    Change-Id: I9a36fa71d5dc4f44b71fb3065e73900699584e00
    Cc: Laura Abbott <labbott@redhat.com>
    Cc: Sumit Semwal <sumit.semwal@linaro.org>
    Cc: Benjamin Gaignard <benjamin.gaignard@linaro.org>
    Cc: Archit Taneja <architt@codeaurora.org>
    Cc: Greg KH <gregkh@linuxfoundation.org>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: Dmitry Shmidt <dimitrysh@google.com>
    Cc: Todd Kjos <tkjos@google.com>
    Cc: Amit Pundir <amit.pundir@linaro.org>
    Fixes: 204f672255c2 ("staging: android: ion: Use CMA APIs directly")
    Acked-by: Laura Abbott <labbott@redhat.com>
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Git-commit: f292b9b28097d8fe870336108e91bd95a14294bf
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    [lmark@codeaurora.org: merging with msm-specific changes]
    Signed-off-by: Liam Mark <lmark@codeaurora.org>

--- drivers/staging/android/ion/ion_cma_heap.c
+++ drivers/staging/android/ion/ion_cma_heap.c
@@ -44,24 +44,30 @@ static int ion_cma_allocate(struct ion_heap *heap, struct ion_buffer *buffer,
 	struct ion_cma_heap *cma_heap = to_cma_heap(heap);
 	struct sg_table *table;
 	struct page *pages;
+	unsigned long size = PAGE_ALIGN(len);
+	unsigned long nr_pages = size >> PAGE_SHIFT;
+	unsigned long align = get_order(size);
 	int ret;
 	void *addr;
 	struct device *dev = heap->priv;
 
-	len = PAGE_ALIGN(len);
-	pages = cma_alloc(cma_heap->cma, len / PAGE_SIZE, 0, GFP_KERNEL);
+	if (align > CONFIG_CMA_ALIGNMENT)
+		align = CONFIG_CMA_ALIGNMENT;
+
+	pages = cma_alloc(cma_heap->cma, nr_pages, align, GFP_KERNEL);
+
 	if (!pages)
 		return -ENOMEM;
 
 	if (!(flags & ION_FLAG_SECURE)) {
 		addr = page_address(pages);
-		memset(addr, 0, len);
+		memset(addr, 0, size);
 	}
 
 	if (MAKE_ION_ALLOC_DMA_READY ||
 	    (flags & ION_FLAG_SECURE) ||
 	    !ion_buffer_cached(buffer))
-		ion_pages_sync_for_device(dev, pages, len, DMA_BIDIRECTIONAL);
+		ion_pages_sync_for_device(dev, pages, size, DMA_BIDIRECTIONAL);
 
 	table = kmalloc(sizeof(struct sg_table), GFP_KERNEL);
 	if (!table)
@@ -71,7 +77,7 @@ static int ion_cma_allocate(struct ion_heap *heap, struct ion_buffer *buffer,
 	if (ret)
 		goto free_mem;
 
-	sg_set_page(table->sgl, pages, len, 0);
+	sg_set_page(table->sgl, pages, size, 0);
 
 	buffer->priv_virt = pages;
 	buffer->sg_table = table;
@@ -80,7 +86,7 @@ static int ion_cma_allocate(struct ion_heap *heap, struct ion_buffer *buffer,
 free_mem:
 	kfree(table);
 err:
-	cma_release(cma_heap->cma, pages, buffer->size);
+	cma_release(cma_heap->cma, pages, nr_pages);
 	return -ENOMEM;
 }
 
@@ -88,10 +94,11 @@ static void ion_cma_free(struct ion_buffer *buffer)
 {
 	struct ion_cma_heap *cma_heap = to_cma_heap(buffer->heap);
 	struct page *pages = buffer->priv_virt;
-	unsigned int count = PAGE_ALIGN(buffer->size) / PAGE_SIZE;
+
+	unsigned long nr_pages = PAGE_ALIGN(buffer->size) >> PAGE_SHIFT;
 
 	/* release memory */
-	cma_release(cma_heap->cma, pages, count);
+	cma_release(cma_heap->cma, pages, nr_pages);
 	/* release sg table */
 	sg_free_table(buffer->sg_table);
 	kfree(buffer->sg_table);
