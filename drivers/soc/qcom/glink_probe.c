b2516b0bbced1cd9198fa53bc54a73297ef79bcd
Chris Lew <clew@codeaurora.org>
2017-12-17 20:02:20 -0800

    soc: qcom: glink_probe: Move node parsing logic into helper
    The complexity of adding a glink node may increase in the future for
    SSR cases and new transports. Move this logic into a helper function
    to avoid nested indentation.
    
    Change-Id: I089975994023f0a8c5774f692e5affb488efcf01
    Signed-off-by: Chris Lew <clew@codeaurora.org>

--- drivers/soc/qcom/glink_probe.c
+++ drivers/soc/qcom/glink_probe.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2017-2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -19,33 +19,40 @@
 
 static struct qcom_glink *edge_infos[NUM_SUBSYSTEMS];
 
-static int glink_probe(struct platform_device *pdev)
+static void probe_subsystem(struct device *dev, struct device_node *np)
 {
-	struct device_node *cn, *pn = pdev->dev.of_node;
 	struct qcom_glink *glink = ERR_PTR(-EINVAL);
-	const char *xprt;
+	const char *transport;
 	u32 pid;
 	int ret;
 
+	ret = of_property_read_u32(np, "qcom,remote-pid", &pid);
+	if (ret || pid >= NUM_SUBSYSTEMS) {
+		dev_err(dev, "invalid pid:%d ret:%d\n", pid, ret);
+		return;
+	}
+
+	ret = of_property_read_string(np, "transport", &transport);
+	if (ret < 0) {
+		dev_err(dev, "missing transport pid:%d\n", pid);
+		return;
+	}
+	if (!strcmp(transport, "smem"))
+		glink = qcom_glink_smem_register(dev, np);
+
+	if (IS_ERR(glink)) {
+		dev_err(dev, "%s failed %d\n", np->name, PTR_ERR(glink));
+		return;
+	}
+	edge_infos[pid] = glink;
+}
+
+static int glink_probe(struct platform_device *pdev)
+{
+	struct device_node *cn, *pn = pdev->dev.of_node;
+
 	for_each_available_child_of_node(pn, cn) {
-		ret = of_property_read_u32(cn, "qcom,remote-pid", &pid);
-		if (ret || pid >= NUM_SUBSYSTEMS) {
-			dev_err(&pdev->dev, "invalid pid:%d ret:%d\n",
-				pid, ret);
-			return -EINVAL;
-		}
-		xprt = of_get_property(cn, "transport", NULL);
-		if (!xprt) {
-			dev_err(&pdev->dev, "missing xprt pid:%d\n", pid);
-			return -EINVAL;
-		}
-		if (!strcmp(xprt, "smem"))
-			glink = qcom_glink_smem_register(&pdev->dev, cn);
-		if (IS_ERR(glink)) {
-			dev_err(&pdev->dev, "%s failed\n", cn->name);
-			return PTR_ERR(glink);
-		}
-		edge_infos[pid] = glink;
+		probe_subsystem(&pdev->dev, cn);
 	}
 	return 0;
 }
@@ -66,13 +73,13 @@ static struct platform_driver glink_probe_driver = {
 
 static int __init glink_probe_init(void)
 {
-	int rc;
+	int ret;
 
-	rc = platform_driver_register(&glink_probe_driver);
-	if (rc) {
+	ret = platform_driver_register(&glink_probe_driver);
+	if (ret) {
 		pr_err("%s: glink_probe register failed %d\n",
-			__func__, rc);
-		return rc;
+			__func__, ret);
+		return ret;
 	}
 
 	return 0;
