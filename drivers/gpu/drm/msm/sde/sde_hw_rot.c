e1c17ba518a30a0cc13f11d982195638480b91f5
Clarence Ip <cip@codeaurora.org>
2017-12-22 18:25:25 -0500

    drm/msm/sde: update inline prefill programming
    Rotator start programming for inline prefill doesn't take
    effect on the next frame, but rather all subsequent frames
    after the next one. However, prefill bandwidth calculations
    are based on the requirements for the next frame, so the
    calculated prefill bandwidth and rotator start programming
    can be mismatched whenever the rotator start is adjusted.
    
    This patch avoids this issue by fixing the rotator start
    to a constant value, and adjusting the prefill bandwidth
    for each frame instead.
    
    Change-Id: I5410f63856248a035d627126962dd407f01a8f0d
    Signed-off-by: Clarence Ip <cip@codeaurora.org>

--- drivers/gpu/drm/msm/sde/sde_hw_rot.c
+++ drivers/gpu/drm/msm/sde/sde_hw_rot.c
@@ -552,6 +552,28 @@ static void sde_hw_rot_to_v4l2_buffer(u32 drm_pixfmt, u64 drm_modifier,
 }
 
 /**
+ * sde_hw_rot_adjust_prefill_bw - update prefill bw based on pipe config
+ * @hw: Pointer to rotator hardware driver
+ * @data: Pointer to command descriptor
+ * @prefill_bw: adjusted prefill bw (output)
+ * return: 0 if success; error code otherwise
+ */
+static int sde_hw_rot_adjust_prefill_bw(struct sde_hw_rot *hw,
+		struct sde_hw_rot_cmd *data, u64 *prefill_bw)
+{
+	if (!hw || !data || !prefill_bw) {
+		SDE_ERROR("invalid parameter(s)\n");
+		return -EINVAL;
+	}
+
+	/* adjust bw for scaling */
+	if (data->dst_rect_h)
+		*prefill_bw = mult_frac(data->prefill_bw, data->crtc_h,
+				data->dst_rect_h);
+	return 0;
+}
+
+/**
  * sde_hw_rot_commit - commit/execute given rotator command
  * @hw: Pointer to rotator hardware driver
  * @data: Pointer to command descriptor
@@ -682,6 +704,8 @@ static int sde_hw_rot_commit(struct sde_hw_rot *hw, struct sde_hw_rot_cmd *data,
 				rot_cmd.dst_addr, rot_cmd.dst_len,
 				&rot_cmd.dst_planes);
 	}
+
+	sde_hw_rot_adjust_prefill_bw(hw, data, &rot_cmd.prefill_bw);
 
 	/* only process any command if client is master or for validation */
 	if (data->master || hw_cmd == SDE_HW_ROT_CMD_VALIDATE) {
@@ -918,6 +942,7 @@ struct sde_hw_rot *sde_hw_rot_init(enum sde_rot idx,
 	/* Assign ops */
 	c->idx = idx;
 	c->caps = cfg;
+	c->catalog = m;
 	_setup_rot_ops(&c->ops, c->caps->features);
 	snprintf(c->name, ARRAY_SIZE(c->name), "sde_rot_%d", idx - ROT_0);
 
