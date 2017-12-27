4bcd2df42bd4037f5b18d2c27ce087446c4bedda
Lingutla Chandrasekhar <clingutla@codeaurora.org>
2017-12-27 18:12:23 +0530

    qcom: minidump: don't register idle task's stack in minidump
    Idle task stacks are not much useful for debug in minidump.
    Remove idle task and its stack registration to optimize minidump
    size and collection time.
    
    Change-Id: Ifa9e3ba54a6a0c912aa022707496c8f2e48dee02
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>

--- drivers/soc/qcom/minidump_log.c
+++ drivers/soc/qcom/minidump_log.c
@@ -76,6 +76,9 @@ void dump_stack_minidump(u64 sp)
 	struct md_region ksp_entry, ktsk_entry;
 	u32 cpu = smp_processor_id();
 
+	if (is_idle_task(current))
+		return;
+
 	if (sp < KIMAGE_VADDR || sp > -256UL)
 		sp = current_stack_pointer;
 
