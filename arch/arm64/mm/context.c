0bfb4642f8e8a37d18595bb6067f38fe4b38b425
Marc Zyngier <marc.zyngier@arm.com>
2018-01-02 18:19:39 +0000

    FROMLIST: arm64: Move post_ttbr_update_workaround to C code
    We will soon need to invoke a CPU-specific function pointer after changing
    page tables, so move post_ttbr_update_workaround out into C code to make
    this possible.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    (cherry picked from git://git.kernel.org/pub/scm/linux/kernel/git/arm64/linux.git
     commit 400a169447ad2268b023637a118fba27246bcc19)
    
    Change-Id: Ic21e59001470a2e88db7291eb5f6393f8a64a7dd
    [ghackmann@google.com: 3.18 doesn't support CPUs that need the Cavium
     errata, so for now post_ttbr_update_workaround() is an empty stub that
     will be used in a later patch series.]
    Signed-off-by: Greg Hackmann <ghackmann@google.com>

--- arch/arm64/mm/context.c
+++ arch/arm64/mm/context.c
@@ -199,6 +199,11 @@ switch_mm_fastpath:
 		cpu_switch_mm(mm->pgd, mm);
 }
 
+/* Errata workaround post TTBRx_EL1 update. */
+asmlinkage void post_ttbr_update_workaround(void)
+{
+}
+
 static int asids_init(void)
 {
 	int fld = cpuid_feature_extract_field(read_cpuid(SYS_ID_AA64MMFR0_EL1), 4);
