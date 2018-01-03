c4f8c4b3cd7ba8a06aac70f98b5754550d0088ef
Mohammed Javid <mjavid@codeaurora.org>
2018-01-03 17:29:57 +0530

    msm: ipa: using debug logging instead of error logging
    Adding code changes to controlling logs printing
    on the console changing error logging to debug logging
    
    Change-Id: I35f4c0e3380ad700899808d9cd6c260befb8be8f
    Acked-by: Ashok Vuyyuru <avuyyuru@qti.qualcomm.com>
    Signed-off-by: Mohammed Javid <mjavid@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/rmnet_ipa.c
+++ drivers/platform/msm/ipa/ipa_v3/rmnet_ipa.c
@@ -2839,7 +2839,7 @@ int rmnet_ipa3_query_tethering_stats(struct wan_ioctl_query_tether_stats *data,
 	if (reset) {
 		req->reset_stats_valid = true;
 		req->reset_stats = true;
-		IPAWANERR("reset the pipe stats\n");
+		IPAWANDBG("reset the pipe stats\n");
 	} else {
 		/* print tethered-client enum */
 		if (data == NULL)
