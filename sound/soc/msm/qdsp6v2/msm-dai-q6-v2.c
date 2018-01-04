114d21fb1a070de9647e3295d04dae9a683fb9c3
Bala Kishore Pati <balakishorepati@codeaurora.org>
2018-01-04 15:27:47 +0530

    ASoC: msm: qdspqv2: add check for TDM clock programming
    Configure TDM clock based on AFE EBIT flag on platforms
    where it is supported.
    
    Change-Id: I900a749b8aae7a87b99b30e1edd20393eb7c4c3b
    Signed-off-by: Bala Kishore Pati <balakishorepati@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-dai-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-dai-q6-v2.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -5091,11 +5091,15 @@ static int msm_dai_q6_dai_tdm_remove(struct snd_soc_dai *dai)
 				dev_err(dai->dev, "fail to disable AFE group 0x%x\n",
 					group_id);
 			}
-			rc = msm_dai_q6_tdm_set_clk(tdm_dai_data,
-				dai->id, false);
-			if (IS_ERR_VALUE(rc)) {
-				dev_err(dai->dev, "%s: fail to disable AFE clk 0x%x\n",
+			if (!(tdm_dai_data->afe_ebit_unsupported &&
+			     !tdm_dai_data->clk_set.clk_freq_in_hz)) {
+				rc = msm_dai_q6_tdm_set_clk(tdm_dai_data,
+							    dai->id, false);
+				if (IS_ERR_VALUE(rc)) {
+					dev_err(dai->dev,
+					"%s: fail to disable AFE clk 0x%x\n",
 					__func__, dai->id);
+				}
 			}
 		}
 	}
@@ -5726,8 +5730,10 @@ static int msm_dai_q6_tdm_prepare(struct snd_pcm_substream *substream,
 				if (atomic_read(group_ref) == 0) {
 					afe_port_group_enable(group_id,
 						NULL, false);
+				if (!(dai_data->afe_ebit_unsupported &&
+				     !dai_data->clk_set.clk_freq_in_hz))
 					msm_dai_q6_tdm_set_clk(dai_data,
-					dai->id, false);
+							       dai->id, false);
 				}
 				dev_err(dai->dev, "%s: fail AFE port 0x%x\n",
 						__func__, sec_port_id);
@@ -5835,12 +5841,15 @@ static void msm_dai_q6_tdm_shutdown(struct snd_pcm_substream *substream,
 					"%s: failed to disable grp 0x%x\n",
 					__func__, group_id);
 			}
-			rc = msm_dai_q6_tdm_set_clk(dai_data,
-				dai->id, false);
-			if (IS_ERR_VALUE(rc)) {
-				dev_err(dai->dev,
+			if (!(dai_data->afe_ebit_unsupported &&
+				!dai_data->clk_set.clk_freq_in_hz)) {
+				rc = msm_dai_q6_tdm_set_clk(dai_data,
+							    dai->id, false);
+				if (IS_ERR_VALUE(rc)) {
+					dev_err(dai->dev,
 					"%s: fail to disable AFE clk 0x%x\n",
 					__func__, dai->id);
+				}
 			}
 		}
 		if (dai_data->afe_ebit_unsupported) {
