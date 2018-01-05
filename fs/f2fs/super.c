647763fa612bdf80b926517a4909d8afd5e84b3d
Wei Yongjun <weiyongjun1@huawei.com>
2018-01-05 09:41:20 +0000

    f2fs: make local functions static
    Fixes the following sparse warnings:
    
    fs/f2fs/segment.c:887:6: warning:
     symbol '__check_sit_bitmap' was not declared. Should it be static?
    fs/f2fs/segment.c:1327:6: warning:
     symbol 'f2fs_wait_discard_bio' was not declared. Should it be static?
    fs/f2fs/super.c:1661:5: warning:
     symbol 'f2fs_get_projid' was not declared. Should it be static?
    
    Signed-off-by: Wei Yongjun <weiyongjun1@huawei.com>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -1731,11 +1731,13 @@ void f2fs_quota_off_umount(struct super_block *sb)
 		f2fs_quota_off(sb, type);
 }
 
-int f2fs_get_projid(struct inode *inode, kprojid_t *projid)
+#if 0	/* not support */
+static int f2fs_get_projid(struct inode *inode, kprojid_t *projid)
 {
 	*projid = F2FS_I(inode)->i_projid;
 	return 0;
 }
+#endif
 
 static const struct dquot_operations f2fs_quota_operations = {
 	.get_reserved_space = f2fs_get_reserved_space,
