249a02309f7ccc45d6dc824c5b33c1f460206f8d
Zhiqiang Tu <ztu@codeaurora.org>
2018-01-09 15:45:52 +0800

    clk: msm: Add reset support in virtual clock frontend driver
    To support reset function in virtual clock driver, we register
    a reset controller in virtual clock front driver. It is a virtual
    reset controller which talks with host backend via virtual clock
    message.
    
    Change-Id: I13d48180534110260d66e42f3a02b63c2afcaeca
    Signed-off-by: Zhiqiang Tu <ztu@codeaurora.org>

---
+++ drivers/clk/msm/virtclk-front.h
@@ -0,0 +1,65 @@
+/* Copyright (c) 2018, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __VIRTCLK_FRONT_H
+#define __VIRTCLK_FRONT_H
+
+enum virtclk_cmd {
+	CLK_MSG_GETID = 1,
+	CLK_MSG_ENABLE,
+	CLK_MSG_DISABLE,
+	CLK_MSG_RESET,
+	CLK_MSG_SETFREQ,
+	CLK_MSG_GETFREQ,
+	CLK_MSG_MAX
+};
+
+struct clk_msg_header {
+	u32 cmd;
+	u32 len;
+	u32 clk_id;
+} __packed;
+
+struct clk_msg_rsp {
+	struct clk_msg_header header;
+	u32 rsp;
+} __packed;
+
+struct clk_msg_setfreq {
+	struct clk_msg_header header;
+	u32 freq;
+} __packed;
+
+struct clk_msg_reset {
+	struct clk_msg_header header;
+	u32 reset;
+} __packed;
+
+struct clk_msg_getid {
+	struct clk_msg_header header;
+	char name[32];
+} __packed;
+
+struct clk_msg_getfreq {
+	struct clk_msg_rsp rsp;
+	u32 freq;
+} __packed;
+
+struct virtclk_front_data {
+	int handle;
+	struct rt_mutex lock;
+};
+
+extern struct virtclk_front_data virtclk_front_ctx;
+int virtclk_front_init_iface(void);
+
+#endif
