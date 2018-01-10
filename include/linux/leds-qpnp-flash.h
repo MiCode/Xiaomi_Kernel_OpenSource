93f71a691c677855f136fb21d8a744f93156eb33
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2018-01-10 13:09:02 -0800

    drivers: add a snapshot of various QPNP PMIC peripheral drivers
    Add a snapshot of several Qualcomm Technologies, Inc. QPNP PMIC
    peripheral drivers that are used for User interface modules like
    display panels (LCD, AMOLED) and flash LED. Fixed some checkpatch
    warnings reported with error logs containing function names which
    are not needed.
    
    This snapshot is taken as of msm-4.9 commit
    1c7b30fefbb5 ("Merge "Revert "soc: qcom: pil: Log the TZ NMI
     caused subsystem err fatal""").
    
    Change-Id: Ic14005244d092cc953a1aa5dd94ca90b693b6c4c
    Signed-off-by: Subbaraman Narayanamurthy <subbaram@codeaurora.org>

---
+++ include/linux/leds-qpnp-flash.h
@@ -0,0 +1,27 @@
+/* Copyright (c) 2016-2017, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __LEDS_QPNP_FLASH_H
+#define __LEDS_QPNP_FLASH_H
+
+#include <linux/leds.h>
+
+#define ENABLE_REGULATOR	BIT(0)
+#define DISABLE_REGULATOR	BIT(1)
+#define QUERY_MAX_CURRENT	BIT(2)
+
+#define FLASH_LED_PREPARE_OPTIONS_MASK	GENMASK(3, 0)
+
+int qpnp_flash_led_prepare(struct led_trigger *trig, int options,
+					int *max_current);
+
+#endif
