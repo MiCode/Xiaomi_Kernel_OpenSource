e3768e4723e67db602fbdca718b366497ccc972c
Daniel Rosenberg <drosen@google.com>
2018-01-10 13:38:19 -0800

    Revert "ANDROID: Squashfs: replace buffer_head with BIO"
    This reverts commit afe18d1bc0cf14c6fa4d5f0791986ed47f7581bf.
    
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Change-Id: I9d5833d5c9bf1f8086ca0bc4a72a1694a2598c63

--- fs/squashfs/super.c
+++ fs/squashfs/super.c
@@ -444,15 +444,9 @@ static int __init init_squashfs_fs(void)
 	if (err)
 		return err;
 
-	if (!squashfs_init_read_wq()) {
-		destroy_inodecache();
-		return -ENOMEM;
-	}
-
 	err = register_filesystem(&squashfs_fs_type);
 	if (err) {
 		destroy_inodecache();
-		squashfs_destroy_read_wq();
 		return err;
 	}
 
@@ -466,7 +460,6 @@ static void __exit exit_squashfs_fs(void)
 {
 	unregister_filesystem(&squashfs_fs_type);
 	destroy_inodecache();
-	squashfs_destroy_read_wq();
 }
 
 
