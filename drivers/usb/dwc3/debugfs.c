ebcbb5273439872086764fb595b8f05d360c9ab7
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2018-01-16 14:51:15 +0530

    dwc3: debugfs: Add check for length before copy data from userspace
    Add boundary check before copying data from userspace buffer to dwc3
    local buffer. The third parameter passed to copy_from_user() should be
    minimum of the two values between userpsace buffer size count and
    (local_buffer size - 1). The last one byte in local_buffer should be
    reserved for null terminator.
    
    Change-Id: I9b2e3db4d5ad6b5f14515cadafa6264f9e8b786c
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>

--- drivers/usb/dwc3/debugfs.c
+++ drivers/usb/dwc3/debugfs.c
@@ -634,7 +634,7 @@ static ssize_t dwc3_store_ep_num(struct file *file, const char __user *ubuf,
 	unsigned int		num, dir, temp;
 	unsigned long		flags;
 
-	if (copy_from_user(kbuf, ubuf, count > 10 ? 10 : count))
+	if (copy_from_user(kbuf, ubuf, min_t(size_t, sizeof(kbuf) - 1, count)))
 		return -EFAULT;
 
 	if (sscanf(kbuf, "%u %u", &num, &dir) != 2)
