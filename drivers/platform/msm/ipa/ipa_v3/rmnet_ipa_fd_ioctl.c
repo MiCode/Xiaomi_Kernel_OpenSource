1b8326f1772a308d6f112a6e6a274e6e8630fcb6
Mohammed Javid <mjavid@codeaurora.org>
2018-01-17 13:19:15 +0530

    msm: ipa: Return error -ENODEV for set data quota failure
    If set data quota fails due to invalid interface
    name, return -ENODEV error.
    
    Change-Id: I45f4082cb8026d3757bd4df237e34df14750ea29
    Acked-by: Pooja Kumari <kumarip@qti.qualcomm.com>
    Signed-off-by: Mohammed Javid <mjavid@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/rmnet_ipa_fd_ioctl.c
+++ drivers/platform/msm/ipa/ipa_v3/rmnet_ipa_fd_ioctl.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -253,7 +253,7 @@ static long ipa3_wan_ioctl(struct file *filp,
 			(struct wan_ioctl_set_data_quota *)param);
 		if (rc != 0) {
 			IPAWANERR("WAN_IOC_SET_DATA_QUOTA failed\n");
-			if (retval == -ENODEV)
+			if (rc == -ENODEV)
 				retval = -ENODEV;
 			else
 				retval = -EFAULT;
