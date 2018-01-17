d37bd7b42a49fd560d5db3a9bbd1177c7dc05236
Vivek Veenam <vveenam@codeaurora.org>
2018-01-18 01:18:30 +0530

    Revert "msm: camera: Correct sequence for destroy_handle"
    This reverts commit faae73452df1cecfc163fbf705ce60fc6cac1477.
    as there was a firmware version mismatch which caused regression.
    
    Change-Id: I3959fd60a34129877b1dae00cfdcac32d1ac8e15
    Signed-off-by: Vivek Veenam <vveenam@codeaurora.org>

--- drivers/media/platform/msm/camera/cam_icp/hfi.c
+++ drivers/media/platform/msm/camera/cam_icp/hfi.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -721,7 +721,7 @@ alloc_fail:
 }
 
 
-void cam_hfi_deinit(void __iomem *icp_base)
+void cam_hfi_deinit(void)
 {
 	mutex_lock(&hfi_cmd_q_mutex);
 	mutex_lock(&hfi_msg_q_mutex);
@@ -733,9 +733,6 @@ void cam_hfi_deinit(void __iomem *icp_base)
 
 	g_hfi->cmd_q_state = false;
 	g_hfi->msg_q_state = false;
-
-	cam_io_w((uint32_t)ICP_INIT_REQUEST_RESET,
-		icp_base + HFI_REG_HOST_ICP_INIT_REQUEST);
 
 	cam_io_w((uint32_t)INTR_DISABLE,
 		g_hfi->csr_base + HFI_REG_A5_CSR_A2HOSTINTEN);
