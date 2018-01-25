2a75a4be2bad69ae609e70496565f607de4e8a12
Nirmal Abraham <nabrah@codeaurora.org>
2018-01-25 15:57:58 +0530

    msm: mdss: Increase fbmem ref count before use
    (cherry-pick from 'commit a1b61bc46be4 ("msm: mdss: Increase
     fbmem buf ref count before use")') and made similar
    changes for MDP3.
    
    The reference count for fbmem buf is not incremented
    before use, which means it can get freed unintentionally
    when the ref countis decreased to "0". In this case,
    there is a possibility of use after free. Ensure the
    fbmem buf refcount is incremented before use.
    
    Change-Id: Iab89ac332dd447b29514a6d5c3f251fcd145de4d
    Signed-off-by: Nirmal Abraham <nabrah@codeaurora.org>

--- drivers/video/msm/mdss/mdp3_ctrl.c
+++ drivers/video/msm/mdss/mdp3_ctrl.c
@@ -1724,12 +1724,15 @@ static int mdp3_get_metadata(struct msm_fb_data_type *mfd,
 		break;
 	case metadata_op_get_ion_fd:
 		if (mfd->fb_ion_handle &&  mfd->fb_ion_client) {
+			get_dma_buf(mfd->fbmem_buf);
 			metadata->data.fbmem_ionfd =
 				ion_share_dma_buf_fd(mfd->fb_ion_client,
 					mfd->fb_ion_handle);
-			if (metadata->data.fbmem_ionfd < 0)
+			if (metadata->data.fbmem_ionfd < 0) {
+				dma_buf_put(mfd->fbmem_buf);
 				pr_err("fd allocation failed. fd = %d\n",
 						metadata->data.fbmem_ionfd);
+			}
 		}
 		break;
 	default:
