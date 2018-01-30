f984f62a46ec04c44aed621d78c40c587c537f2e
Herbert Xu <herbert@gondor.apana.org.au>
2018-01-30 13:03:00 +0530

    crypto: algif_skcipher - Load TX SG list after waiting
    We need to load the TX SG list in sendmsg(2) after waiting for
    incoming data, not before.
    
    connoro@google.com: backport to 3.18,where the relevant logic is
    located in skcipher_recvmsg() rather than skcipher_recvmsg_sync()
    
    Change-Id: I4e18b385fe50c844cd62a2eb20122036ea4dfe09
    Git-commit: 36c84b22ac8aa041cbdfbe48a55ebb32e3521704
    Git-repo: https://android.googlesource.com/kernel/common.git
    Reported-by: Dmitry Vyukov <dvyukov@google.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Tested-by: Dmitry Vyukov <dvyukov@google.com>
    Signed-off-by: Connor O'Brien <connoro@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Kishor PK <kpbhat@codeaurora.org>

--- crypto/algif_skcipher.c
+++ crypto/algif_skcipher.c
@@ -446,12 +446,6 @@ static int skcipher_recvmsg(struct kiocb *unused, struct socket *sock,
 		char __user *from = iov->iov_base;
 
 		while (seglen) {
-			sgl = list_first_entry(&ctx->tsgl,
-					       struct skcipher_sg_list, list);
-			sg = sgl->sg;
-
-			while (!sg->length)
-				sg++;
 
 			used = ctx->used;
 			if (!used) {
@@ -473,6 +467,12 @@ static int skcipher_recvmsg(struct kiocb *unused, struct socket *sock,
 			err = -EINVAL;
 			if (!used)
 				goto free;
+			sgl = list_first_entry(&ctx->tsgl,
+						struct skcipher_sg_list, list);
+			sg = sgl->sg;
+
+			while (!sg->length)
+				sg++;
 
 			ablkcipher_request_set_crypt(&ctx->req, sg,
 						     ctx->rsgl.sg, used,
