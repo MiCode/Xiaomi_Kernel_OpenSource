b07d364a9fb469dfad3aea9751b0aeac93901753
Michal Hocko <mhocko@suse.com>
2018-01-31 09:16:56 +0100

    netfilter: x_tables: remove size check
    Back in 2002 vmalloc used to BUG on too large sizes.  We are much better
    behaved these days and vmalloc simply returns NULL for those.  Remove the
    check as it simply not needed and the comment is even misleading.
    
    Change-Id: Ibb85e65ee8864a8529fc51cb08092bdf3cecf7e0
    Link: http://lkml.kernel.org/r/20180131081916.GO21609@dhcp22.suse.cz
    Suggested-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Michal Hocko <mhocko@suse.com>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Florian Westphal <fw@strlen.de>
    Cc: David S. Miller <davem@davemloft.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Git-commit: fd2c19b2a28bb574b3914466a68ef830212d5cf7
    Git-repo: https://git.kernel.org/pub/scm/linux/kernel/git/davem/net.git
    Signed-off-by: Sean Tranchetti <stranche@codeaurora.org>
    Signed-off-by: Chinmay Agarwal <chinagar@codeaurora.org>

--- net/netfilter/x_tables.c
+++ net/netfilter/x_tables.c
@@ -987,10 +987,6 @@ struct xt_table_info *xt_alloc_table_info(unsigned int size)
 	if (sz < sizeof(*info))
 		return NULL;
 
-	/* Pedantry: prevent them from hitting BUG() in vmalloc.c --RR */
-	if ((size >> PAGE_SHIFT) + 2 > totalram_pages)
-		return NULL;
-
 	if (sz <= (PAGE_SIZE << PAGE_ALLOC_COSTLY_ORDER))
 		info = kmalloc(sz, GFP_KERNEL | __GFP_NOWARN | __GFP_NORETRY);
 	if (!info) {
