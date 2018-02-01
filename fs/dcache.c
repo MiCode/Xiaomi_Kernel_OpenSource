5b2e1e02f65efaf5e0f75c60b5ebb76412565076
Andrey Ryabinin <aryabinin@virtuozzo.com>
2018-02-01 21:00:52 +0300

    BACKPORT: fs: dcache: Revert "manually unpoison dname after allocation to shut up kasan's reports"
    Upstream commit babcbbc7c4e2fa7fa76417ece7c57083bee971f1.
    
    This reverts commit df4c0e36f1b1782b0611a77c52cc240e5c4752dd.
    
    It's no longer needed since dentry_string_cmp() now uses
    read_word_at_a_time() to avoid kasan's reports.
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Change-Id: I923fa1cdd8aa1910e37fe95254c9da3ff16cba15
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Bug: 128674696

--- fs/dcache.c
+++ fs/dcache.c
@@ -38,7 +38,6 @@
 #include <linux/prefetch.h>
 #include <linux/ratelimit.h>
 #include <linux/list_lru.h>
-#include <linux/kasan.h>
 
 #include "internal.h"
 #include "mount.h"
@@ -1643,9 +1642,6 @@ struct dentry *__d_alloc(struct super_block *sb, const struct qstr *name)
 		}
 		atomic_set(&ext->u.count, 1);
 		dname = ext->name;
-		if (IS_ENABLED(CONFIG_DCACHE_WORD_ACCESS))
-			kasan_unpoison_shadow(dname,
-				round_up(name->len + 1,	sizeof(unsigned long)));
 	} else  {
 		dname = dentry->d_iname;
 	}	
