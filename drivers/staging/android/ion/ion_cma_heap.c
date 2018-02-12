a568a844adc939b4b8e47ad837a8399f37b87623
Liam Mark <lmark@codeaurora.org>
2018-02-12 11:54:02 -0800

    staging: ion: Add support for ion CMA heap to allocate from carveouts
    Because of an XPU errata we need to temporarily have the ION CMA heap
    support allocating from a careveout which isn't mapped into the kernel.
    
    Once the errata is fixed this change can be reverted.
    
    Change-Id: Id2bbb81644be514bdb90e743a028e65a05ebd55e
    Signed-off-by: Liam Mark <lmark@codeaurora.org>

--- drivers/staging/android/ion/ion_cma_heap.c
+++ drivers/staging/android/ion/ion_cma_heap.c
@@ -4,7 +4,7 @@
  * Copyright (C) Linaro 2012
  * Author: <benjamin.gaignard@linaro.org> for ST-Ericsson.
  *
- * Copyright (c) 2016-2017, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2016-2018, The Linux Foundation. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -23,6 +23,7 @@
 #include <linux/err.h>
 #include <linux/cma.h>
 #include <linux/scatterlist.h>
+#include <linux/of.h>
 #include <soc/qcom/secure_buffer.h>
 
 #include "ion.h"
@@ -33,8 +34,24 @@ struct ion_cma_heap {
 	struct cma *cma;
 };
 
+struct ion_cma_buffer_info {
+	void *cpu_addr;
+	dma_addr_t handle;
+	struct page *pages;
+};
 #define to_cma_heap(x) container_of(x, struct ion_cma_heap, heap)
 
+static bool ion_cma_has_kernel_mapping(struct ion_heap *heap)
+{
+	struct device *dev = heap->priv;
+	struct device_node *mem_region;
+
+	mem_region = of_parse_phandle(dev->of_node, "memory-region", 0);
+	if (!mem_region)
+		return false;
+
+	return !of_property_read_bool(mem_region, "no-map");
+}
 
 /* ION CMA heap operations functions */
 static int ion_cma_allocate(struct ion_heap *heap, struct ion_buffer *buffer,
@@ -43,65 +60,101 @@ static int ion_cma_allocate(struct ion_heap *heap, struct ion_buffer *buffer,
 {
 	struct ion_cma_heap *cma_heap = to_cma_heap(heap);
 	struct sg_table *table;
-	struct page *pages;
+	struct page *pages = NULL;
 	unsigned long size = PAGE_ALIGN(len);
 	unsigned long nr_pages = size >> PAGE_SHIFT;
 	unsigned long align = get_order(size);
 	int ret;
 	void *addr;
 	struct device *dev = heap->priv;
+	struct ion_cma_buffer_info *info;
+
+	info = kzalloc(sizeof(*info), GFP_KERNEL);
+	if (!info)
+		return -ENOMEM;
 
 	if (align > CONFIG_CMA_ALIGNMENT)
 		align = CONFIG_CMA_ALIGNMENT;
 
-	pages = cma_alloc(cma_heap->cma, nr_pages, align, GFP_KERNEL);
+	if (!ion_cma_has_kernel_mapping(heap)) {
+		flags &= ~((unsigned long)ION_FLAG_CACHED);
+		buffer->flags = flags;
 
-	if (!pages)
-		return -ENOMEM;
+		info->cpu_addr = dma_alloc_writecombine(dev, size,
+							&info->handle,
+							GFP_KERNEL);
+		if (!info->cpu_addr) {
+			dev_err(dev, "failed to allocate buffer\n");
+			goto free_info;
+		}
+		pages = pfn_to_page(PFN_DOWN(info->handle));
+	} else {
+		pages = cma_alloc(cma_heap->cma, nr_pages, align, GFP_KERNEL);
 
-	if (!(flags & ION_FLAG_SECURE)) {
-		addr = page_address(pages);
-		memset(addr, 0, size);
+		if (!pages)
+			goto free_info;
+
+		if (!(flags & ION_FLAG_SECURE)) {
+			addr = page_address(pages);
+			memset(addr, 0, size);
+		}
+
+		if (MAKE_ION_ALLOC_DMA_READY ||
+		    (flags & ION_FLAG_SECURE) ||
+		    !ion_buffer_cached(buffer))
+			ion_pages_sync_for_device(dev, pages, size,
+						  DMA_BIDIRECTIONAL);
 	}
-
-	if (MAKE_ION_ALLOC_DMA_READY ||
-	    (flags & ION_FLAG_SECURE) ||
-	    !ion_buffer_cached(buffer))
-		ion_pages_sync_for_device(dev, pages, size, DMA_BIDIRECTIONAL);
 
 	table = kmalloc(sizeof(struct sg_table), GFP_KERNEL);
 	if (!table)
-		goto err;
+		goto err_alloc;
 
 	ret = sg_alloc_table(table, 1, GFP_KERNEL);
 	if (ret)
-		goto free_mem;
+		goto free_table;
 
 	sg_set_page(table->sgl, pages, size, 0);
 
-	buffer->priv_virt = pages;
+	info->pages = pages;
+	buffer->priv_virt = info;
 	buffer->sg_table = table;
 	return 0;
 
-free_mem:
+free_table:
 	kfree(table);
-err:
-	cma_release(cma_heap->cma, pages, nr_pages);
+err_alloc:
+	if (info->cpu_addr)
+		dma_free_attrs(dev, size, info->cpu_addr, info->handle, 0);
+	else
+		cma_release(cma_heap->cma, pages, nr_pages);
+
+free_info:
+	kfree(info);
 	return -ENOMEM;
 }
 
 static void ion_cma_free(struct ion_buffer *buffer)
 {
 	struct ion_cma_heap *cma_heap = to_cma_heap(buffer->heap);
-	struct page *pages = buffer->priv_virt;
+	struct ion_cma_buffer_info *info = buffer->priv_virt;
 
-	unsigned long nr_pages = PAGE_ALIGN(buffer->size) >> PAGE_SHIFT;
+	if (info->cpu_addr) {
+		struct device *dev = buffer->heap->priv;
 
-	/* release memory */
-	cma_release(cma_heap->cma, pages, nr_pages);
+		dma_free_attrs(dev, PAGE_ALIGN(buffer->size), info->cpu_addr,
+			       info->handle, 0);
+	} else {
+		struct page *pages = info->pages;
+		unsigned long nr_pages = PAGE_ALIGN(buffer->size) >> PAGE_SHIFT;
+
+		/* release memory */
+		cma_release(cma_heap->cma, pages, nr_pages);
+	}
 	/* release sg table */
 	sg_free_table(buffer->sg_table);
 	kfree(buffer->sg_table);
+	kfree(info);
 }
 
 static struct ion_heap_ops ion_cma_ops = {
@@ -116,9 +169,6 @@ struct ion_heap *ion_cma_heap_create(struct ion_platform_heap *data)
 {
 	struct ion_cma_heap *cma_heap;
 	struct device *dev = (struct device *)data->priv;
-
-	if (!dev->cma_area)
-		return ERR_PTR(-EINVAL);
 
 	cma_heap = kzalloc(sizeof(*cma_heap), GFP_KERNEL);
 
@@ -188,9 +238,6 @@ struct ion_heap *ion_cma_secure_heap_create(struct ion_platform_heap *data)
 {
 	struct ion_cma_heap *cma_heap;
 	struct device *dev = (struct device *)data->priv;
-
-	if (!dev->cma_area)
-		return ERR_PTR(-EINVAL);
 
 	cma_heap = kzalloc(sizeof(*cma_heap), GFP_KERNEL);
 
