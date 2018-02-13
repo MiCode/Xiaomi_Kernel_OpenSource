9b1dda1a2a0e2c82fb6c841ccd7f8e492fdb485b
Bryan Huntsman <bryanh@codeaurora.org>
2018-02-12 17:26:10 -0800

    checkpatch: allow copyright statements > 80 columns
    Exempt copyright statements from the 80 column rule as line-splitting or
    other kinds of work-arounds decrease readability in diffs.  Identify the
    copyright statement by looking for '+/* Copyright (c)', case-insensitive,
    at the beginning of the line.
    
    Change-Id: I57aafd569ff2173ca7aa3f636532060fc93b2be1
    Signed-off-by: Bryan Huntsman <bryanh@codeaurora.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -3012,6 +3012,10 @@ sub process {
 			} elsif ($line =~ /^\+.*\bEFI_GUID\s*\(/) {
 				$msg_type = "";
 
+			# Long copyright statements are another special case
+			} elsif ($rawline =~ /^\+.\*.*copyright.*\(c\).*$/i) {
+				$msg_type = "";
+
 			# Otherwise set the alternate message types
 
 			# a comment starts before $max_line_length
