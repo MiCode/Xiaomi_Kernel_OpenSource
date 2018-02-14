68ff45ac21f8f5a64e248019dbdad91745852ef3
Neeraj Soni <neersoni@codeaurora.org>
2018-02-14 14:41:51 +0530

    block: Expose IOCTL to get partition start sector number
    IOCTL added to provide start sector number of requested
    partition by client.
    
    Change-Id: I263927e88820d863a4630081cd08afb97acdde62
    Signed-off-by: Neeraj Soni <neersoni@codeaurora.org>

--- include/uapi/linux/fs.h
+++ include/uapi/linux/fs.h
@@ -226,6 +226,7 @@ struct fsxattr {
 #define BLKSECDISCARD _IO(0x12,125)
 #define BLKROTATIONAL _IO(0x12,126)
 #define BLKZEROOUT _IO(0x12,127)
+#define BLKGETSTPART _IO(0x12, 128)
 
 #define BMAP_IOCTL 1		/* obsolete - kept for compatibility */
 #define FIBMAP	   _IO(0x00,1)	/* bmap access */
