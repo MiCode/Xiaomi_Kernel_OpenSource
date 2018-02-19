55dd2f285c123473d3ab2fd7611cfb5a2e3bb324
Alexei Avshalom Lazar <ailizaro@codeaurora.org>
2018-02-19 12:15:34 +0200

    wil6210: Add support for 11ad platform driver
    Platform driver used to handle msm specific platform
    requirement for 11ad chipset connected to msm platform.
    
    Change-Id: I13c5b5a76bc17393e486c400bdfb90e4ef89e270
    CRs-Fixed: 2143032
    Signed-off-by: Alexei Avshalom Lazar <ailizaro@codeaurora.org>
    [liord@codeaurora.org: SPDX license, checkpatch errors]
    Signed-off-by: Lior David <liord@codeaurora.org>

--- drivers/net/wireless/ath/wil6210/wil_platform.c
+++ drivers/net/wireless/ath/wil6210/wil_platform.c
@@ -1,29 +1,21 @@
+// SPDX-License-Identifier: ISC
 /*
  * Copyright (c) 2014 Qualcomm Atheros, Inc.
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * Copyright (c) 2019, The Linux Foundation. All rights reserved.
  */
 
 #include <linux/device.h>
 #include "wil_platform.h"
+#include "msm_11ad.h"
 
 int __init wil_platform_modinit(void)
 {
-	return 0;
+	return msm_11ad_modinit();
 }
 
 void wil_platform_modexit(void)
 {
+	msm_11ad_modexit();
 }
 
 /**
@@ -36,7 +28,7 @@ void wil_platform_modexit(void)
 void *wil_platform_init(struct device *dev, struct wil_platform_ops *ops,
 			const struct wil_platform_rops *rops, void *wil_handle)
 {
-	void *handle = ops; /* to return some non-NULL for 'void' impl. */
+	void *handle;
 
 	if (!ops) {
 		dev_err(dev,
@@ -44,7 +36,7 @@ void *wil_platform_init(struct device *dev, struct wil_platform_ops *ops,
 		return NULL;
 	}
 
-	/* platform specific init functions should be called here */
+	handle = msm_11ad_dev_init(dev, ops, rops, wil_handle);
 
 	return handle;
 }
