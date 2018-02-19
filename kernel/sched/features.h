ec7e154d07bec20627178fee2dcbe97dde5e41ba
Pavankumar Kondeti <pkondeti@codeaurora.org>
2018-02-19 14:43:35 +0530

    sched/fair: Turn off FBT_STRICT_ORDER feature
    Turning off FBT_STRICT_ORDER feature selects whichever of target CPU
    or backup CPU that saves the most energy. The ftrace analysis
    indicate that excessive packing can be avoided by placing the
    task on backup CPU when it is idle.
    
    Change-Id: I281c599f6236aa8c9b41c56509a533048a01da91
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -97,4 +97,4 @@ SCHED_FEAT(MIN_CAPACITY_CAPPING, false)
  * ON: If the target CPU saves any energy, use that.
  * OFF: Use whichever of target or backup saves most.
  */
-SCHED_FEAT(FBT_STRICT_ORDER, true)
+SCHED_FEAT(FBT_STRICT_ORDER, false)
