34ecf31da7c52f63b287bd04deb7c675658d6441
Jitendra Sharma <shajit@codeaurora.org>
2018-02-19 11:32:46 +0530

    soc: qcom: Reset PDC along with RSC in modem shutdown.
    Since there is a PDC for each subsystem and works with
    the subsystem RSC, so, in modem shutdown path, PDC also
    needs to be reset when the subsystem RSC
    (read subsystem) is reset.
    
    Change-Id: Iea28f9c49af0eb90dfd454302972c0cbbb822c50
    Signed-off-by: Jitendra Sharma <shajit@codeaurora.org>

--- drivers/soc/qcom/pil-msa.c
+++ drivers/soc/qcom/pil-msa.c
@@ -367,10 +367,14 @@ int pil_mss_shutdown(struct pil_desc *pil)
 									ret);
 	}
 
+	pil_mss_pdc_sync(drv, 1);
+	udelay(200);
 	pil_mss_restart_reg(drv, true);
 	/* Wait 6 32kHz sleep cycles for reset */
 	udelay(200);
 	ret =  pil_mss_restart_reg(drv, false);
+	udelay(200);
+	pil_mss_pdc_sync(drv, false);
 
 	if (drv->is_booted) {
 		pil_mss_disable_clks(drv);
