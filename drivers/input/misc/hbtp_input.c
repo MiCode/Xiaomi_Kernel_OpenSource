a8215396b5484ad63491c168ea4d2fdaf24039f0
Vevek Venkatesan <vevekv@codeaurora.org>
2018-02-19 16:45:50 +0530

    input: misc: hbtp_input: fix list corruption in wait-queue.
    Fix list corruption in task_completion wait_queue, in
    hbtp_input driver.
    
    Change-Id: Ic9967a196a575ce8aaad62a85ecbf055cf66a6f0
    Signed-off-by: Vevek Venkatesan <vevekv@codeaurora.org>

--- drivers/input/misc/hbtp_input.c
+++ drivers/input/misc/hbtp_input.c
@@ -599,6 +599,7 @@ static long hbtp_input_ioctl_handler(struct file *file, unsigned int cmd,
 	enum hbtp_afe_power_cmd power_cmd;
 	enum hbtp_afe_signal afe_signal;
 	enum hbtp_afe_power_ctrl afe_power_ctrl;
+	static bool init_completion_not_done = true;
 
 	switch (cmd) {
 	case HBTP_SET_ABSPARAM:
@@ -714,6 +715,8 @@ static long hbtp_input_ioctl_handler(struct file *file, unsigned int cmd,
 			mutex_lock(&hbtp->mutex);
 			if (!hbtp->power_suspended) {
 				complete(&hbtp->power_resume_sig);
+				pr_err("%s: ***VVK*** power_resume_sig completed\n",
+					__func__);
 			} else {
 				pr_err("%s: resume signal in wrong state\n",
 					__func__);
@@ -724,6 +727,8 @@ static long hbtp_input_ioctl_handler(struct file *file, unsigned int cmd,
 			mutex_lock(&hbtp->mutex);
 			if (hbtp->power_suspended) {
 				complete(&hbtp->power_suspend_sig);
+				pr_err("%s: ***VVK*** power_suspend_sig completed\n",
+					__func__);
 			} else {
 				pr_err("%s: suspend signal in wrong state\n",
 					__func__);
@@ -783,8 +788,18 @@ static long hbtp_input_ioctl_handler(struct file *file, unsigned int cmd,
 				return -EFAULT;
 			}
 			mutex_lock(&hbtp->mutex);
-			init_completion(&hbtp->power_resume_sig);
-			init_completion(&hbtp->power_suspend_sig);
+			if (init_completion_not_done) {
+				init_completion(&hbtp->power_resume_sig);
+				init_completion(&hbtp->power_suspend_sig);
+				init_completion_not_done = false;
+				pr_err("%s: ***VVK*** init_completion\n",
+					__func__);
+			} else {
+				reinit_completion(&hbtp->power_resume_sig);
+				reinit_completion(&hbtp->power_suspend_sig);
+				pr_err("%s: ***VVK*** reinit_completion\n",
+					__func__);
+			}
 			hbtp->power_sig_enabled = true;
 			mutex_unlock(&hbtp->mutex);
 			pr_err("%s: sync_signal option is enabled\n", __func__);
@@ -1189,6 +1204,8 @@ static int hbtp_dsi_panel_suspend(struct hbtp_data *ts)
 			if (ts->power_sig_enabled) {
 				pr_debug("%s: power_sig is enabled, wait for signal\n",
 					__func__);
+				pr_err("%s: *VVK* wait_for_completion power_suspend_sig\n",
+					__func__);
 				mutex_unlock(&hbtp->mutex);
 				rc = wait_for_completion_interruptible_timeout(
 					&hbtp->power_suspend_sig,
@@ -1198,6 +1215,8 @@ static int hbtp_dsi_panel_suspend(struct hbtp_data *ts)
 						__func__);
 				}
 				mutex_lock(&hbtp->mutex);
+				pr_err("%s: *VVK* wait_for_completion power_suspend done\n",
+					__func__);
 				pr_debug("%s: Wait is done for suspend\n",
 					__func__);
 			} else {
@@ -1244,6 +1263,8 @@ static int hbtp_dsi_panel_early_resume(struct hbtp_data *ts)
 			if (ts->power_sig_enabled) {
 				pr_err("%s: power_sig is enabled, wait for signal\n",
 					__func__);
+				pr_err("%s: *VVK* wait_for_completion power_resume_sig\n",
+					__func__);
 				mutex_unlock(&hbtp->mutex);
 				rc = wait_for_completion_interruptible_timeout(
 					&hbtp->power_resume_sig,
@@ -1253,6 +1274,8 @@ static int hbtp_dsi_panel_early_resume(struct hbtp_data *ts)
 						__func__);
 				}
 				mutex_lock(&hbtp->mutex);
+				pr_err("%s: *VVK* wait_for_completion power_resume done\n",
+					__func__);
 				pr_debug("%s: wait is done\n", __func__);
 			} else {
 				pr_debug("%s: power_sig is NOT enabled\n",
