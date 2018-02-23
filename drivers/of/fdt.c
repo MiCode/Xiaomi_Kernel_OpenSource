e80f6cfecc694b56279d76418dc9544630d8dde0
Frank Rowand <frank.rowand@sony.com>
2018-02-23 14:31:39 +0530

    of: add early boot allocation of of_find_node_by_phandle() cache
    From: Frank Rowand <frank.rowand@sony.com>
    
    The initial implementation of the of_find_node_by_phandle() cache
    allocates the cache using kcalloc().  Add an early boot allocation
    of the cache so it will be usable during early boot.  Switch over
    to the kcalloc() based cache once normal memory allocation
    becomes available.
    
    Change-Id: I6acc88175ba9e4e17107d2f8a0cfd819a079f4d2
    Signed-off-by: Frank Rowand <frank.rowand@sony.com>
    Patch-mainline: linux-kernel @ 02/15/18, 06:22
    [cpandya@codeaurora.org: resolve trivial merge conflicts]
    Signed-off-by: Chintan Pandya <cpandya@codeaurora.org>

--- drivers/of/fdt.c
+++ drivers/of/fdt.c
@@ -31,6 +31,8 @@
 #include <asm/setup.h>  /* for COMMAND_LINE_SIZE */
 #include <asm/page.h>
 
+#include "of_private.h"
+
 /*
  * of_fdt_limit_memory - limit the number of regions in the /memory node
  * @limit: maximum entries
@@ -1269,6 +1271,8 @@ void __init unflatten_device_tree(void)
 
 	/* Get pointer to "/chosen" and "/aliases" nodes for use everywhere */
 	of_alias_scan(early_init_dt_alloc_memory_arch);
+
+	of_populate_phandle_cache_early();
 }
 
 /**
