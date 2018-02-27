907139485794dd7922a9632ab3507bb6a16e562e
Valentin Schneider <valentin.schneider@arm.com>
2018-02-27 16:20:21 +0000

    FROMLIST: sched: Wrap rq->rd->overload accesses with READ/WRITE_ONCE
    This variable can be read and set locklessly within update_sd_lb_stats().
    As such, READ/WRITE_ONCE are added to make sure nothing terribly wrong
    can happen because of the compiler.
    
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    [from https://lore.kernel.org/lkml/1530699470-29808-9-git-send-email-morten.rasmussen@arm.com/]
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Change-Id: I14ce007f916bffd6a7938b7d56faf2b384d27887

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9332,8 +9332,8 @@ next_group:
 
 	if (!lb_sd_parent(env->sd)) {
 		/* update overload indicator if we are at root domain */
-		if (env->dst_rq->rd->overload != overload)
-			env->dst_rq->rd->overload = overload;
+		if (READ_ONCE(env->dst_rq->rd->overload) != overload)
+			WRITE_ONCE(env->dst_rq->rd->overload, overload);
 	}
 
 	if (overutilized)
@@ -10206,7 +10206,7 @@ static int idle_balance(struct rq *this_rq, struct rq_flags *rf)
 	rq_unpin_lock(this_rq, rf);
 
 	if (this_rq->avg_idle < sysctl_sched_migration_cost ||
-	    !this_rq->rd->overload) {
+	    !READ_ONCE(this_rq->rd->overload)) {
 		rcu_read_lock();
 		sd = rcu_dereference_check_sched_domain(this_rq->sd);
 		if (sd)
