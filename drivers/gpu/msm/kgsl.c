a6e44af5614b9d619b9a07e114122f2f32e06c77
Deepak Kumar <dkumar@codeaurora.org>
2018-02-28 11:06:38 +0530

    msm: kgsl: Keep dispatcher halted during SUSPEND state
    Increment GPU halt variable after successful transition
    to SUSPEND state to halt dispatcher. This is necessary to
    make sure that any pending dispatcher threads don't go
    ahead and try to wake up GPU in SUSPEND state.
    
    Change-Id: I806884306549afbef0dfa92ea14903cc7a47d347
    Signed-off-by: Deepak Kumar <dkumar@codeaurora.org>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -754,6 +754,8 @@ static int kgsl_suspend_device(struct kgsl_device *device, pm_message_t state)
 
 	mutex_lock(&device->mutex);
 	status = kgsl_pwrctrl_change_state(device, KGSL_STATE_SUSPEND);
+	if (status == 0)
+		device->ftbl->dispatcher_halt(device);
 	mutex_unlock(&device->mutex);
 
 	KGSL_PWR_WARN(device, "suspend end\n");
@@ -768,6 +770,7 @@ static int kgsl_resume_device(struct kgsl_device *device)
 	KGSL_PWR_WARN(device, "resume start\n");
 	mutex_lock(&device->mutex);
 	if (device->state == KGSL_STATE_SUSPEND) {
+		device->ftbl->dispatcher_unhalt(device);
 		kgsl_pwrctrl_change_state(device, KGSL_STATE_SLUMBER);
 	} else if (device->state != KGSL_STATE_INIT) {
 		/*
