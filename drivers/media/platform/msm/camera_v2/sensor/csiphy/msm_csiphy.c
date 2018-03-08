169e423524f3fdcce14d614e714c5192ca6a21f2
Vijay kumar Tumati <vtumati@codeaurora.org>
2018-03-08 21:34:23 +0530

    msm: camera: sensor: Receive sensor output data rate information
    Program data rate specific camera hardware registers using this
    information.
    
    Change-Id: I0a64fd6f5b0e9ae0a5edee836998d8fea0e7478d
    Signed-off-by: Vijay kumar Tumati <vtumati@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/sensor/csiphy/msm_csiphy.c
+++ drivers/media/platform/msm/camera_v2/sensor/csiphy/msm_csiphy.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -797,10 +797,10 @@ static int msm_csiphy_lane_config(struct csiphy_device *csiphy_dev,
 		ratio = csiphy_dev->csiphy_max_clk/clk_rate;
 		csiphy_params->settle_cnt = csiphy_params->settle_cnt/ratio;
 	}
-	CDBG("%s csiphy_params, mask = 0x%x cnt = %d\n",
+	CDBG("%s csiphy_params, mask = 0x%x cnt = %d, data rate = %lu\n",
 		__func__,
 		csiphy_params->lane_mask,
-		csiphy_params->lane_cnt);
+		csiphy_params->lane_cnt, csiphy_params->data_rate);
 	CDBG("%s csiphy_params, settle cnt = 0x%x csid %d\n",
 		__func__, csiphy_params->settle_cnt,
 		csiphy_params->csid_core);
