afd3411ab2e6a36a0cf1ae49adc82e6696591bba
Prakash Kamliya <pkamliya@codeaurora.org>
2018-03-13 15:40:50 +0530

    msm: kgsl: Make GMU local functions to static
    Few functions are local to the source and
    do not need to be in the global space, so
    make them static.
    
    Change-Id: I4dd2ee39e6960c77bf1a70002fc8919591034b1a
    Signed-off-by: Prakash Kamliya <pkamliya@codeaurora.org>

--- drivers/gpu/msm/kgsl_gmu.c
+++ drivers/gpu/msm/kgsl_gmu.c
@@ -310,7 +310,7 @@ static struct {
  * @gmu: Pointer to GMU device
  * @node: Pointer to GMU device node
  */
-int gmu_iommu_init(struct gmu_device *gmu, struct device_node *node)
+static int gmu_iommu_init(struct gmu_device *gmu, struct device_node *node)
 {
 	struct device_node *child;
 	struct gmu_iommu_context *ctx = NULL;
@@ -347,7 +347,7 @@ int gmu_iommu_init(struct gmu_device *gmu, struct device_node *node)
  * from IOMMU context banks.
  * @gmu: Pointer to GMU device
  */
-void gmu_kmem_close(struct gmu_device *gmu)
+static void gmu_kmem_close(struct gmu_device *gmu)
 {
 	int i;
 	struct gmu_memdesc *md = &gmu->fw_image;
@@ -387,7 +387,7 @@ void gmu_kmem_close(struct gmu_device *gmu)
 	iommu_domain_free(ctx->domain);
 }
 
-void gmu_memory_close(struct gmu_device *gmu)
+static void gmu_memory_close(struct gmu_device *gmu)
 {
 	gmu_kmem_close(gmu);
 	/* Free user memory context */
@@ -401,7 +401,7 @@ void gmu_memory_close(struct gmu_device *gmu)
  * @gmu: Pointer to GMU device
  * @node: Pointer to GMU device node
  */
-int gmu_memory_probe(struct gmu_device *gmu, struct device_node *node)
+static int gmu_memory_probe(struct gmu_device *gmu, struct device_node *node)
 {
 	int ret;
 
@@ -754,7 +754,7 @@ static int gmu_bus_vote_init(struct gmu_device *gmu, struct kgsl_pwrctrl *pwr)
 	return 0;
 }
 
-int gmu_rpmh_init(struct gmu_device *gmu, struct kgsl_pwrctrl *pwr)
+static int gmu_rpmh_init(struct gmu_device *gmu, struct kgsl_pwrctrl *pwr)
 {
 	struct rpmh_arc_vals gfx_arc, cx_arc, mx_arc;
 	int ret;
