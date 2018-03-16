714e43c8ea89c7825e1501f3a4d0431a4f071c7d
Kyle Yan <kyan@codeaurora.org>
2018-03-15 17:36:48 -0700

    cpufreq: schedutil: Add cpu_frequency traces for each cpu in the policy
    Add cpu_frequency traces for each online CPU in the policy for more
    consistent outputs of most standard trace analysis tools.
    
    Change-Id: I01cd48909316f861cb601c2ce74330351320f1b5
    Signed-off-by: Kyle Yan <kyan@codeaurora.org>
    [avilaj@codeaurora.org: Port to 4.19, fix conflicts]
    Signed-off-by: Jonathan Avila <avilaj@codeaurora.org>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -169,6 +169,7 @@ static void sugov_fast_switch(struct sugov_policy *sg_policy, u64 time,
 			      unsigned int next_freq)
 {
 	struct cpufreq_policy *policy = sg_policy->policy;
+	unsigned int cpu;
 
 	if (!sugov_update_next_freq(sg_policy, time, next_freq))
 		return;
@@ -178,7 +179,8 @@ static void sugov_fast_switch(struct sugov_policy *sg_policy, u64 time,
 		return;
 
 	policy->cur = next_freq;
-	trace_cpu_frequency(next_freq, smp_processor_id());
+	for_each_cpu(cpu, policy->cpus)
+		trace_cpu_frequency(next_freq, smp_processor_id());
 }
 
 static void sugov_deferred_update(struct sugov_policy *sg_policy, u64 time,
