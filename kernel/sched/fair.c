0cbc2fd6426f5ca797738a4796a53f2fc34eb47d
Ionela Voinescu <ionela.voinescu@arm.com>
2018-03-19 18:03:45 +0000

    ANDROID: sched/fair: remove order from CPU selection
    find_best_target is currently split into code handling latency sensitive
    tasks and code handling non-latency sensitive tasks based on the value
    of the prefer_idle flag.
    Another differentiation is done for boosted tasks, preferring to start
    with higher-capacity CPU when boosted, and with more efficient CPUs when
    not boosted. This additional differentiation is obtained by imposing an
    order when considering CPUs for selection. This order is determined in
    typical big.LITTLE systems by the start point (the CPU with the maximum
    or minimum capacity) and by the order of big and little CPU groups
    provided in the sched domain hierarchy.
    
    However, it's not guaranteed that the sched domain hierarchy will give
    us a sorted list of CPU groups based on their maximum capacities when
    dealing with systems with more than 2 capacity groups.
    For example, if we consider a system with three groups of CPUs (LITTLEs,
    mediums, bigs), the sched domain hierarchy might provide the following
    scheduling groups ordering for a prefer_idle-boosted task:
       big CPUs -> LITTLE CPUs -> medium CPUs.
    If the big CPUs are not idle, but there are a few LITTLEs and mediums
    as idle CPUs, by returning the first idle CPU, we will be incorrectly
    prefering a lower capacity CPU over a higher capacity CPU.
    
    In order to eliminate this reliance on assuming sched groups are ordered
    by capacity, let's:
    1. Iterate though all candidate CPUs for all cases.
    2. Minimise or maximise the capacity of the considered CPU, depending
    on prefer_idle and boost information.
    
    Taking in part each of the four possible cases we analyse the
    implementation and impact of this solution:
    
    1. prefer_idle and boosted
    This type of tasks needs to favour the selection of a reserved idle CPU,
    and thus we still start from the biggest CPU in the system, but we
    iterate though all CPUs as to correctly handle the example above by
    maximising the capacity of the idle CPU we select. When all CPUs are
    active, we already iterate though all CPUs and we're able to maximise
    spare capacity or minimise utilisation for the considered target or
    backup CPU.
    
    2. prefer_idle and !boosted
    For these tasks we prefer the selection of a more energy efficient CPU
    and therefore we start from the smallest CPUs in the system, but we
    iterate through all the CPUs as to select the most energy efficient idle
    CPU, implementation which mimics existing behaviour. When all CPUs are
    active, we already iterate though all CPUs and we're able to
    maximise spare capacity or minimise utilisation for the considered
    target or backup CPU.
    
    3. !prefer_idle and boosted and
    4. !prefer_idle and !boosted
    For these tasks we already iterate though all CPUs and we're able to
    maximise the energy efficiency of the selected CPU.
    
    Change-Id: I940399e22eff29453cba0e2ec52a03b17eec12ae
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Reviewed-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Git-commit: 19c03afff2992f3553cfef445e9404262d68a67a
    Git-repo: http://android.googlesource.com/kernel/common
    [adharmap@codeaurora.org: added missing parameters in the trace]
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7114,6 +7114,18 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 
 	*backup_cpu = -1;
 
+	/*
+	 * In most cases, target_capacity tracks capacity_orig of the most
+	 * energy efficient CPU candidate, thus requiring to minimise
+	 * target_capacity. For these cases target_capacity is already
+	 * initialized to ULONG_MAX.
+	 * However, for prefer_idle and boosted tasks we look for a high
+	 * performance CPU, thus requiring to maximise target_capacity. In this
+	 * case we initialise target_capacity to 0.
+	 */
+	if (prefer_idle && boosted)
+		target_capacity = 0;
+
 	/* Find start CPU based on boost value */
 	cpu = start_cpu(p, boosted, fbt_env->rtg_target);
 	if (cpu < 0)
@@ -7242,17 +7254,26 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 
 				/*
 				 * Case A.1: IDLE CPU
-				 * Return the first IDLE CPU we find.
+				 * Return the best IDLE CPU we find:
+				 * - for boosted tasks: the CPU with the highest
+				 * performance (i.e. biggest capacity_orig)
+				 * - for !boosted tasks: the most energy
+				 * efficient CPU (i.e. smallest capacity_orig)
 				 */
 				if (idle_cpu(i)) {
-					trace_sched_find_best_target(p,
-							prefer_idle, min_util,
-							cpu, best_idle_cpu,
-							best_active_cpu,
-							-1, i, -1);
+					if (boosted &&
+					    capacity_orig <= target_capacity)
+						continue;
+					if (!boosted &&
+					    capacity_orig >= target_capacity)
+						continue;
 
-					return i;
+					target_capacity = capacity_orig;
+					best_idle_cpu = i;
+					continue;
 				}
+				if (best_idle_cpu != -1)
+					continue;
 
 				/*
 				 * Case A.2: Target ACTIVE CPU
@@ -7421,7 +7442,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 	 *
 	 * - prefer_idle tasks:
 	 *
-	 *   a) IDLE CPU available, we return immediately
+	 *   a) IDLE CPU available: best_idle_cpu
 	 *   b) ACTIVE CPU where task fits and has the bigger maximum spare
 	 *      capacity (i.e. target_cpu)
 	 *   c) ACTIVE CPU with less contention due to other tasks
@@ -7432,6 +7453,15 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 	 *   a) ACTIVE CPU: target_cpu
 	 *   b) IDLE CPU: best_idle_cpu
 	 */
+
+	if (prefer_idle && (best_idle_cpu != -1)) {
+		trace_sched_find_best_target(p, prefer_idle, min_util, cpu,
+					     best_idle_cpu, best_active_cpu,
+					     -1, best_idle_cpu, -1);
+
+		return best_idle_cpu;
+	}
+
 	if (target_cpu == -1)
 		target_cpu = prefer_idle
 			? best_active_cpu
