8fa863274cb41ca169ff14de48620c1f33599e9c
Ionela Voinescu <ionela.voinescu@arm.com>
2018-03-19 15:29:51 +0000

    ANDROID: update_group_capacity for single cpu in cluster
    If we're only left with one big CPU in a cluster (either there was only
    one to begin with or the others have been hotplugged out), the MC level
    seen from the perspective of that CPU, will only contain one group (that
    of the CPU) and the LOAD_BALANCE flag will be cleared at that level.
    The MC level is kept nonetheless as it will still contain energy
    information, as per commit 06654998ed81 ("ANDROID: sched: EAS & 'single
    cpu per cluster'/cpu hotplug interoperability").
    
    This will result in update_cpu_capacity never being called for that big
    CPU and its capacity will never be updated. Also, its capacity will
    never be considered when setting or updating the max_cpu_capacity
    structure.
    
    Therefore, call update_group_capacity for SD levels that do not have the
    SD_LOAD_BALANCE flag set in order to update the CPU capacity information
    of CPUs that are alone in a cluster and the groups that they belong to.
    The call to update_cpu_capacity will also result in appropriately
    setting the values of the max_cpu_capacity structure.
    
    Fixes: 06654998ed81 ("ANDROID: sched: EAS & 'single cpu per cluster'/cpu
    hotplug interoperability")
    Change-Id: I6074972dde1fdf586378f8a3534e3d0aa42a809a
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10319,8 +10319,12 @@ static int idle_balance(struct rq *this_rq, struct rq_flags *rf)
 		int continue_balancing = 1;
 		u64 t0, domain_cost;
 
-		if (!(sd->flags & SD_LOAD_BALANCE))
+		if (!(sd->flags & SD_LOAD_BALANCE)) {
+			if (time_after_eq(jiffies,
+					  sd->groups->sgc->next_update))
+				update_group_capacity(sd, this_cpu);
 			continue;
+		}
 
 		if (this_rq->avg_idle < curr_cost + sd->max_newidle_lb_cost) {
 			update_next_balance(sd, &next_balance);
@@ -10655,8 +10659,12 @@ static void rebalance_domains(struct rq *rq, enum cpu_idle_type idle)
 		if (energy_aware() && !sd_overutilized(sd))
 			continue;
 
-		if (!(sd->flags & SD_LOAD_BALANCE))
+		if (!(sd->flags & SD_LOAD_BALANCE)) {
+			if (time_after_eq(jiffies,
+					  sd->groups->sgc->next_update))
+				update_group_capacity(sd, cpu);
 			continue;
+		}
 
 		/*
 		 * Stop the load balance at this level. There is another
