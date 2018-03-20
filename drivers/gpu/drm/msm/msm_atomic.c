MotorolaMobilityLLC/kernel-msm

drm/msm: support shutdown sequence on msm driver
Add shutdown sequence in msm driver to turn off
all connectors/bridge gracefully. That will allow
the graceful shutdown for dsi panel.

Change-Id: I055bd57a45706478c785b7a3c41bf7623dd8d2e7
Signed-off-by: Dhaval Patel <pdhaval@codeaurora.org>
Signed-off-by: Lakshmi Narayana Kalavala <lkalaval@codeaurora.org>

drivers/gpu/drm/msm/msm_atomic.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2017, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2016-2018, The Linux Foundation. All rights reserved.
  * Copyright (C) 2014 Red Hat
  * Author: Rob Clark <robdclark@gmail.com>
  *
@@ -561,6 +561,11 @@ int msm_atomic_commit(struct drm_device *dev,
 	ktime_t timeout;
 	struct msm_commit *commit;
 	int i, ret;
+
+	if (!priv || priv->shutdown_in_progress) {
+		DRM_ERROR("priv is null or shutdwon is in-progress\n");
+		return -EINVAL;
+	}
 
 	SDE_ATRACE_BEGIN("atomic_commit");
 	ret = drm_atomic_helper_prepare_planes(dev, state);

MiCode/Xiaomi_Kernel_OpenSource

drivers/gpu/drm/msm/msm_atomic.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2017, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2016-2018, The Linux Foundation. All rights reserved.
  * Copyright (C) 2014 Red Hat
  * Author: Rob Clark <robdclark@gmail.com>
  *
@@ -561,6 +561,11 @@ int msm_atomic_commit(struct drm_device *dev,
 	ktime_t timeout;
 	struct msm_commit *commit;
 	int i, ret;
+
+	if (!priv || priv->shutdown_in_progress) {
+		DRM_ERROR("priv is null or shutdown is in-progress\n");
+		return -EINVAL;
+	}
 
 	SDE_ATRACE_BEGIN("atomic_commit");
 	ret = drm_atomic_helper_prepare_planes(dev, state);