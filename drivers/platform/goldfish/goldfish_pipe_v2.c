ed11d8f2a0d07756acace1a2a295f29e41976358
Roman Kiryanov <rkir@google.com>
2018-03-20 13:40:04 -0700

    goldfish: pipe: ANDROID: Allocate memory with GFP_KERNEL.
    We use GFP_KERNEL in the outer context.
    
    Bug: 72717639
    Bug: 66884503
    Change-Id: I5e10dba5138818351936ec0f70cd01070eaf199f
    Signed-off-by: Roman Kiryanov <rkir@google.com>

--- drivers/platform/goldfish/goldfish_pipe_v2.c
+++ drivers/platform/goldfish/goldfish_pipe_v2.c
@@ -658,7 +658,7 @@ static int get_free_pipe_id_locked(struct goldfish_pipe_dev *dev)
 		u32 new_capacity = 2 * dev->pipes_capacity;
 		struct goldfish_pipe **pipes =
 				kcalloc(new_capacity, sizeof(*pipes),
-					GFP_ATOMIC);
+					GFP_KERNEL);
 		if (!pipes)
 			return -ENOMEM;
 		memcpy(pipes, dev->pipes, sizeof(*pipes) * dev->pipes_capacity);
