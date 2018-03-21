89df99e3b8602da8a9e5b3b9100360c6d36a2406
Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
2018-03-21 12:05:27 -0600

    net: qualcomm: rmnet: Export mux_id and flags to netlink
    Define new netlink attributes for rmnet mux_id and flags. These
    flags / mux_id were earlier using vlan flags / id respectively.
    The flag bits are also moved to uapi and are renamed with
    prefix RMNET_FLAG_*.
    
    Also add the rmnet policy to handle the new netlink attributes.
    
    CRs-Fixed: 2156182
    Change-Id: I229810c1a601666e5b8b73a77647bc21f1cceb47
    Signed-off-by: Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>

--- include/uapi/linux/if_link.h
+++ include/uapi/linux/if_link.h
@@ -928,4 +928,25 @@ enum {
 	IFLA_EVENT_BONDING_OPTIONS,	/* change in bonding options */
 };
 
+/* rmnet section */
+
+#define RMNET_FLAGS_INGRESS_DEAGGREGATION         (1U << 0)
+#define RMNET_FLAGS_INGRESS_MAP_COMMANDS          (1U << 1)
+#define RMNET_FLAGS_INGRESS_MAP_CKSUMV4           (1U << 2)
+#define RMNET_FLAGS_EGRESS_MAP_CKSUMV4            (1U << 3)
+
+enum {
+	IFLA_RMNET_UNSPEC,
+	IFLA_RMNET_MUX_ID,
+	IFLA_RMNET_FLAGS,
+	__IFLA_RMNET_MAX,
+};
+
+#define IFLA_RMNET_MAX (__IFLA_RMNET_MAX - 1)
+
+struct ifla_rmnet_flags {
+	__u32   flags;
+	__u32   mask;
+};
+
 #endif /* _UAPI_LINUX_IF_LINK_H */
