c230fbe3c01b5f3f063cda174c94013ba9e3345d
Derek Chen <chenche@codeaurora.org>
2018-03-22 13:28:58 -0400

    ASoC: msm: qdsp6v2: add 24b capture support for tert mi2s
    Add support for 24 bits capture for Tertiary MI2S
    interface in CPU DAI links.
    
    Change-Id: I2453c5e7efb957d9c2a1fb832c1a456d0672af9a
    Signed-off-by: Derek Chen <chenche@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-dai-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-dai-q6-v2.c
@@ -4125,7 +4125,8 @@ static struct snd_soc_dai_driver msm_dai_q6_mi2s_dai[] = {
 				 SNDRV_PCM_RATE_32000 | SNDRV_PCM_RATE_44100 |
 				 SNDRV_PCM_RATE_48000 | SNDRV_PCM_RATE_96000 |
 				 SNDRV_PCM_RATE_192000,
-			.formats = SNDRV_PCM_FMTBIT_S16_LE,
+			.formats = SNDRV_PCM_FMTBIT_S16_LE |
+				SNDRV_PCM_FMTBIT_S24_LE,
 			.rate_min =     8000,
 			.rate_max =     192000,
 		},
