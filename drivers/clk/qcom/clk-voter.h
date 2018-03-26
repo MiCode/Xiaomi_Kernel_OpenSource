a1a230bedfd81e7ede6d0262979eb49824c81f56
Amit Nischal <anischal@codeaurora.org>
2018-03-26 09:41:53 +0530

    clk: qcom: Add handoff support for voter rpm clocks
    Some voter rpm clocks are critical for system booting and should not be
    gated until a unused clock tree late_init level.
    So add support for handoff functionality for system critical clocks by
    using CLK_ENABLE_HAND_OFF flag.
    
    Change-Id: I9f9674a25fc5f7a2bc9b5672b00716b82223b06b
    Signed-off-by: Amit Nischal <anischal@codeaurora.org>
    Signed-off-by: Taniya Das <tdas@codeaurora.org>

--- drivers/clk/qcom/clk-voter.h
+++ drivers/clk/qcom/clk-voter.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2017, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017, 2019, The Linux Foundation. All rights reserved.
  */
 
 #ifndef __QCOM_CLK_VOTER_H__
@@ -27,6 +27,7 @@ extern const struct clk_ops clk_ops_voter;
 		.hw.init = &(struct clk_init_data){			   \
 			.ops = &clk_ops_voter,				   \
 			.name = #clk_name,				   \
+			.flags = CLK_ENABLE_HAND_OFF,			   \
 			.parent_names = (const char *[]){ #_parent_name }, \
 			.num_parents = 1,				   \
 		},							   \
@@ -37,5 +38,7 @@ extern const struct clk_ops clk_ops_voter;
 
 #define DEFINE_CLK_BRANCH_VOTER(clk_name, _parent_name) \
 	 __DEFINE_CLK_VOTER(clk_name, _parent_name, 1000, 1)
+
+int voter_clk_handoff(struct clk_hw *hw);
 
 #endif
