813335480ded2450e5738a0d3e1c39396feeb3bd
Jeevan Shriram <jshriram@codeaurora.org>
2018-03-29 12:21:40 -0700

    scripts: build-all.py: update regex to compile all defconfigs
    Update regex to populate all sdm*_defconfig and compile with build-all
    script.
    
    Change-Id: Ia4aa0f6b684b647b173f0ca18e6979fb0d957149
    Signed-off-by: Jeevan Shriram <jshriram@codeaurora.org>

--- scripts/build-all.py
+++ scripts/build-all.py
@@ -308,12 +308,12 @@ def scan_configs():
         r'apq*_defconfig',
         r'qsd*_defconfig',
         r'mpq*_defconfig',
-        r'sdm[0-9]*_defconfig',
+        r'sdm*_defconfig',
         r'sdx*_defconfig',
         )
     arch64_pats = (
         r'msm*_defconfig',
-        r'sdm[0-9]*_defconfig',
+        r'sdm*_defconfig',
         r'sdx*_defconfig',
         )
     for p in arch_pats:
