1498ee332887ee3419b5220f42dacdc5166d06f7
Pratham Pratap <prathampratap@codeaurora.org>
2018-04-02 11:31:28 +0530

    usb: dwc3-msm: Set HOST MODE flag before resume
    Setting HOST MODE flag before resume ensures that
    qusb_phy_host_init is getting called when the controller
    starts in HOST MODE.
    
    Change-Id: Ie5b11df51cffcf38dfcdeefb26aea6c96fa95a6b
    Signed-off-by: Pratham Pratap <prathampratap@codeaurora.org>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -3944,10 +3944,10 @@ static int dwc3_otg_start_host(struct dwc3_msm *mdwc, int on)
 	if (on) {
 		dev_dbg(mdwc->dev, "%s: turn on host\n", __func__);
 
+		mdwc->hs_phy->flags |= PHY_HOST_MODE;
 		pm_runtime_get_sync(mdwc->dev);
 		dbg_event(0xFF, "StrtHost gync",
 			atomic_read(&mdwc->dev->power.usage_count));
-		mdwc->hs_phy->flags |= PHY_HOST_MODE;
 		if (dwc->maximum_speed >= USB_SPEED_SUPER) {
 			mdwc->ss_phy->flags |= PHY_HOST_MODE;
 			usb_phy_notify_connect(mdwc->ss_phy,
