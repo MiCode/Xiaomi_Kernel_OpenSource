d9f0a42e3130c62a56b338b7e560ffab759e90e0
Mayank Rana <mrana@codeaurora.org>
2018-04-03 10:18:46 -0700

    usb: gadget: Update functions for SSP descriptors
    This change updates USB function drivers to assign SSP descriptors
    using usb_assign_descriptors() API. USB SSP descriptors are needed
    to enumerate these functions as different composite device in
    super speed plus mode.
    
    Change-Id: Iea4d623afb2bf572358d1cc1d0673f20f2d7d5aa
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -3075,6 +3075,7 @@ static int _ffs_func_bind(struct usb_configuration *c,
 			ret = ss_len;
 			goto error;
 		}
+		func->function.ssp_descriptors = func->function.ss_descriptors;
 	} else {
 		ss_len = 0;
 	}
@@ -3476,6 +3477,7 @@ static void ffs_func_unbind(struct usb_configuration *c,
 	func->function.fs_descriptors = NULL;
 	func->function.hs_descriptors = NULL;
 	func->function.ss_descriptors = NULL;
+	func->function.ssp_descriptors = NULL;
 	func->interfaces_nums = NULL;
 
 	ffs_event_add(ffs, FUNCTIONFS_UNBIND);
