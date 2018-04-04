a2c4ce620aa8f213b5aa0c0a60db02414805f7d7
Tharun Kumar Merugu <mtharu@codeaurora.org>
2018-04-04 17:59:37 +0530

    msm: ADSPRPC: handle 32 bit support
    Handle 32 bit support without truncation.
    
    Change-Id: I158271b8fefd74f1d01fb79ddef89982078d3fee
    Acked-by: Krishnaiah Tadakamalla <ktadakam@qti.qualcomm.com>
    Signed-off-by: Tharun Kumar Merugu <mtharu@codeaurora.org>

--- drivers/char/adsprpc_shared.h
+++ drivers/char/adsprpc_shared.h
@@ -199,16 +199,16 @@ struct fastrpc_ioctl_init_attrs {
 };
 
 struct fastrpc_ioctl_munmap {
-	uintptr_t vaddrout;	/* address to unmap */
+	uint64_t vaddrout;	/* address to unmap */
 	size_t size;		/* size */
 };
 
 struct fastrpc_ioctl_mmap {
 	int fd;				/* ion fd */
 	uint32_t flags;			/* flags for dsp to map with */
-	uintptr_t vaddrin;		/* optional virtual address */
+	uint64_t vaddrin;		/* optional virtual address */
 	size_t size;			/* size */
-	uintptr_t vaddrout;		/* dsps virtual address */
+	uint64_t vaddrout;		/* dsps virtual address */
 };
 
 struct fastrpc_ioctl_munmap_fd {
