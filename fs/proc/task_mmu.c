c1d1d0d82a3a9e2f735d033341ab9cee48924bdb
Greg Kroah-Hartman <gregkh@google.com>
2018-04-08 16:32:50 +0200

    proc: task_mmu: fix up merge issue with 3.18.103
    This fixes a merge issue with 3.18.103 and the android-3.18 branch.  I
    took the merge in the incorrect order for this file.
    
    Change-Id: I7b67efd0486114214d6aed673915a58b8cc1d56e
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -373,13 +373,15 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 			goto done;
 		}
 
+		if (is_stack(priv, vma)) {
+			name = "[stack]";
+			goto done;
+		}
+
 		if (vma_get_anon_name(vma)) {
 			seq_pad(m, ' ');
 			seq_print_vma_name(m, vma);
 		}
-
-		if (is_stack(priv, vma))
-			name = "[stack]";
 	}
 
 done:
