c19bb862e1f5c095323d94c1a0fda1ab3a24d8d2
Amit Kushwaha <kushwaha@codeaurora.org>
2018-04-09 20:41:14 +0530

    msm: kgsl: create sysfs entries to expose memory usage
    Added sysfs entries to show kgsl memory usage statistics.
    gpumem_mapped: kgsl memory mapped in the process address space.
    gpumem_unmapped: kgsl allocated memory but not mapped in process.
    imported_mem: graphics memory not allocated by the kgsl.
    
    Below is the sysfs path for new entries:
    /sys/class/kgsl/kgsl/proc/<pid>/
    
    Change-Id: I08c2014d28dc0ca1e2b54ebf966d00143b303b54
    Signed-off-by: Amit Kushwaha <kushwaha@codeaurora.org>

--- drivers/gpu/msm/kgsl_device.h
+++ drivers/gpu/msm/kgsl_device.h
@@ -404,6 +404,7 @@ struct kgsl_context {
  * @kobj: Pointer to a kobj for the sysfs directory for this process
  * @debug_root: Pointer to the debugfs root for this process
  * @stats: Memory allocation statistics for this process
+ * @gpumem_mapped: KGSL memory mapped in the process address space
  * @syncsource_idr: sync sources created by this process
  * @syncsource_lock: Spinlock to protect the syncsource idr
  * @fd_count: Counter for the number of FDs for this process
@@ -424,6 +425,7 @@ struct kgsl_process_private {
 		uint64_t cur;
 		uint64_t max;
 	} stats[KGSL_MEM_ENTRY_MAX];
+	uint64_t gpumem_mapped;
 	struct idr syncsource_idr;
 	spinlock_t syncsource_lock;
 	int fd_count;
