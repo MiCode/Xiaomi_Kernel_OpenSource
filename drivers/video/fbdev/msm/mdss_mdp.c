e0f1e8e3a6e7f1dcc99fd4988a56a9609a579672
Amine Najahi <anajahi@codeaurora.org>
2018-04-10 12:17:15 -0400

    msm: mdss: Remove kfree when devm_kzalloc is used
    Remove kfree when devm_kzalloc is used. Resources
    are automatically freed when the driver is unloaded.
    
    CRs-Fixed: 2204325
    Change-Id: Ie862c52b888332a5bd7382866f88ee5d8cc294c4
    Signed-off-by: Amine Najahi <anajahi@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_mdp.c
+++ drivers/video/fbdev/msm/mdss_mdp.c
@@ -2098,10 +2098,9 @@ static u32 mdss_mdp_scaler_init(struct mdss_data_type *mdata,
 			devm_kzalloc(&mdata->pdev->dev, sizeof(u32) *
 					mdata->scaler_off->ndest_scalers,
 					GFP_KERNEL);
-		if  (!mdata->scaler_off->dest_scaler_off) {
-			kfree(mdata->scaler_off->dest_scaler_off);
+		if  (!mdata->scaler_off->dest_scaler_off)
 			return -ENOMEM;
-		}
+
 		ret = mdss_mdp_parse_dt_handler(mdata->pdev,
 				"qcom,mdss-dest-scaler-off",
 				mdata->scaler_off->dest_scaler_off,
@@ -2112,10 +2111,9 @@ static u32 mdss_mdp_scaler_init(struct mdss_data_type *mdata,
 			devm_kzalloc(&mdata->pdev->dev, sizeof(u32) *
 					mdata->scaler_off->ndest_scalers,
 					GFP_KERNEL);
-		if  (!mdata->scaler_off->dest_scaler_lut_off) {
-			kfree(mdata->scaler_off->dest_scaler_lut_off);
+		if  (!mdata->scaler_off->dest_scaler_lut_off)
 			return -ENOMEM;
-		}
+
 		ret = mdss_mdp_parse_dt_handler(mdata->pdev,
 				"qcom,mdss-dest-scalers-lut-off",
 				mdata->scaler_off->dest_scaler_lut_off,
