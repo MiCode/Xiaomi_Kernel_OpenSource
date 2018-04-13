c8b2e18c57bdb98fa2aea52f1e156dbd1a2172db
Tingwei Zhang <tingwei@codeaurora.org>
2018-04-13 11:28:05 +0800

    soc: qcom: dcc_v2: Avoid huge memory allcation
    Check size of DCC configuration. Limit the size according
    to SRAM size.
    
    Change-Id: Ib846c92458e2fef8c45128b9b83d1c549b293370
    Signed-off-by: Tingwei Zhang <tingwei@codeaurora.org>

--- drivers/soc/qcom/dcc_v2.c
+++ drivers/soc/qcom/dcc_v2.c
@@ -887,7 +887,8 @@ static int dcc_config_add(struct dcc_drvdata *drvdata, unsigned int addr,
 		goto err;
 	}
 
-	if (!len) {
+	/* Check the len to avoid allocate huge memory */
+	if (!len || len > (drvdata->ram_size / 8)) {
 		dev_err(drvdata->dev, "DCC: Invalid length\n");
 		ret = -EINVAL;
 		goto err;
