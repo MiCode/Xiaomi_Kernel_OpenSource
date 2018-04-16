fe490465787ad804c8d049c689fad033a94a3f4e
Hardik Arya <harya@codeaurora.org>
2018-04-16 16:46:58 +0530

    diag: Free usb buffer's entry after removing from list
    Currently, there is possibility of memory leak due to not
    freeing allocated memory for usb buffer's entry after
    removing it from list. The patch handle this by freeing
    the entry.
    
    Change-Id: Idb08ecad859749e6ab1b09184362de38de4a9836
    Signed-off-by: Hardik Arya <harya@codeaurora.org>

--- drivers/char/diag/diag_usb.c
+++ drivers/char/diag/diag_usb.c
@@ -139,8 +139,11 @@ static void diag_usb_buf_tbl_remove(struct diag_usb_info *usb_info,
 			 * Remove reference from the table if it is the
 			 * only instance of the buffer
 			 */
-			if (atomic_read(&entry->ref_count) == 0)
+			if (atomic_read(&entry->ref_count) == 0) {
 				list_del(&entry->track);
+				kfree(entry);
+				entry = NULL;
+			}
 			break;
 		}
 	}
@@ -330,6 +333,7 @@ static void diag_usb_write_done(struct diag_usb_info *ch,
 	buf = entry->buf;
 	len = entry->len;
 	kfree(entry);
+	entry = NULL;
 	diag_ws_on_copy_complete(DIAG_WS_MUX);
 
 	if (ch->ops && ch->ops->write_done)
