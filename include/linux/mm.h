5d1dccddd720cc20ebc8cb9c68fa4118181500f7
Laurent Dufour <ldufour@linux.vnet.ibm.com>
2018-04-17 16:33:18 +0200

    mm: cache some VMA fields in the vm_fault structure
    When handling speculative page fault, the vma->vm_flags and
    vma->vm_page_prot fields are read once the page table lock is released. So
    there is no more guarantee that these fields would not change in our back.
    They will be saved in the vm_fault structure before the VMA is checked for
    changes.
    
    This patch also set the fields in hugetlb_no_page() and
    __collapse_huge_page_swapin even if it is not need for the callee.
    
    Signed-off-by: Laurent Dufour <ldufour@linux.vnet.ibm.com>
    Change-Id: I9821f02ea32ef220b57b8bfd817992bbf71bbb1d
    Patch-mainline: linux-mm @ Tue, 17 Apr 2018 16:33:18
    [vinmenon@codeaurora.org: trivial merge conflict fixes]
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    [charante@codeaurora.org: trivial merge conflict fixes]
    Signed-off-by: Charan Teja Reddy <charante@codeaurora.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -838,9 +838,9 @@ void free_compound_page(struct page *page);
  * pte_mkwrite.  But get_user_pages can cause write faults for mappings
  * that do not have writing enabled, when used by access_process_vm.
  */
-static inline pte_t maybe_mkwrite(pte_t pte, struct vm_area_struct *vma)
+static inline pte_t maybe_mkwrite(pte_t pte, unsigned long vma_flags)
 {
-	if (likely(vma->vm_flags & VM_WRITE))
+	if (likely(vma_flags & VM_WRITE))
 		pte = pte_mkwrite(pte);
 	return pte;
 }
