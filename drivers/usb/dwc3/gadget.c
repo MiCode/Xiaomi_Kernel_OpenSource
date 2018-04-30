82e94cda6ff793cc3107793dfb52983c17067c47
Chandana Kishori Chiluveru <cchiluve@codeaurora.org>
2018-04-30 12:25:44 +0530

    usb: dwc3: restart usb upon endpoint cmd timeout
    For start new configuration command timeout restart
    is not happening. Change the logic to restart
    usb session except for end transfer command timeout.
    
    Change-Id: I03baf50456c45e5531e5138976f86cb4bd5e3b0b
    Signed-off-by: Chandana Kishori Chiluveru <cchiluve@codeaurora.org>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -470,7 +470,7 @@ int dwc3_send_gadget_ep_cmd(struct dwc3_ep *dep, unsigned cmd,
 		ret = -ETIMEDOUT;
 		dev_err(dwc->dev, "%s command timeout for %s\n",
 			dwc3_gadget_ep_cmd_string(cmd), dep->name);
-		if (!(cmd & DWC3_DEPCMD_ENDTRANSFER)) {
+		if (DWC3_DEPCMD_CMD(cmd) != DWC3_DEPCMD_ENDTRANSFER) {
 			dwc->ep_cmd_timeout_cnt++;
 			dwc3_notify_event(dwc,
 				DWC3_CONTROLLER_RESTART_USB_SESSION, 0);
