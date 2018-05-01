49d8a2adafec351dabef179c44f6f52b693f56e6
Lynus Vaz <lvaz@codeaurora.org>
2018-05-01 20:34:00 +0530

    msm: kgsl: Do not modify the userspace alignment request
    Userspace cares about the GPU VA alignment it requested, and it does
    not know about the alignment forced by the device. Do not modify the
    userspace-requested alignment value since it is shared with
    userspace.
    
    Change-Id: If5e605aeb92724785ab24af5b7bf29f8138e5f46
    Signed-off-by: Lynus Vaz <lvaz@codeaurora.org>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4349,6 +4349,8 @@ static unsigned long _get_svm_area(struct kgsl_process_private *private,
 	else
 		align = SZ_4K;
 
+	align = max_t(uint64_t, align, entry->memdesc.pad_to);
+
 	/* get the GPU pagetable's SVM range */
 	if (kgsl_mmu_svm_range(private->pagetable, &start, &end,
 				entry->memdesc.flags))
--- drivers/gpu/msm/kgsl_iommu.c
+++ drivers/gpu/msm/kgsl_iommu.c
@@ -2468,7 +2468,8 @@ static int kgsl_iommu_get_gpuaddr(struct kgsl_pagetable *pagetable,
 
 	size = kgsl_memdesc_footprint(memdesc);
 
-	align = 1 << kgsl_memdesc_get_align(memdesc);
+	align = max_t(uint64_t, 1 << kgsl_memdesc_get_align(memdesc),
+			memdesc->pad_to);
 
 	if (memdesc->flags & KGSL_MEMFLAGS_FORCE_32BIT) {
 		start = pt->compat_va_start;
