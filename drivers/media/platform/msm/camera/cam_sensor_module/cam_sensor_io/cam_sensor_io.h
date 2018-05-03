eb9686685b6f6cf515248306e1f660fb7d8bcf24
Karthik Anantha Ram <kartanan@codeaurora.org>
2018-05-03 14:36:43 -0700

    msm: camera: cci: Add support for burst read
    Configure the read threshold level and implement change
    to enable burst read. This change also bumps up the max
    bytes that can be read in a single transaction.
    
    Change-Id: I6ab4d5db302ddf49387bf47916e264714ca434e6
    Signed-off-by: Karthik Anantha Ram <kartanan@codeaurora.org>

--- drivers/media/platform/msm/camera/cam_sensor_module/cam_sensor_io/cam_sensor_io.h
+++ drivers/media/platform/msm/camera/cam_sensor_module/cam_sensor_io/cam_sensor_io.h
@@ -52,6 +52,7 @@ int32_t camera_io_dev_read(struct camera_io_master *io_master_info,
  * @io_master_info: I2C/SPI master information
  * @addr: I2C address
  * @data: I2C data
+ * @addr_type: I2C addr type
  * @data_type: I2C data type
  * @num_bytes: number of bytes
  *
@@ -60,6 +61,7 @@ int32_t camera_io_dev_read(struct camera_io_master *io_master_info,
 int32_t camera_io_dev_read_seq(struct camera_io_master *io_master_info,
 	uint32_t addr, uint8_t *data,
 	enum camera_sensor_i2c_type addr_type,
+	enum camera_sensor_i2c_type data_type,
 	int32_t num_bytes);
 
 /**
