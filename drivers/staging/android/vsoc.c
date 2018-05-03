c2f604dc5ddd30ab3d81bd66da242dd5720ecd71
Alistair Strachan <astrachan@google.com>
2018-05-03 12:41:03 -0700

    FROMLIST: staging: vsoc: Fix a i386-randconfig warning.
    Fix "warning: cast to pointer from integer of different size" when
    printing the region shm physical address. Use the %pa conversion
    specifier and pass the resource by reference.
    
    Signed-off-by: Alistair Strachan <astrachan@google.com>
    [sent upstream via staging https://patchwork.kernel.org/patch/10376967/]
    Bug: 702147120
    Change-Id: Ibb0c32b461821235155c464bfd5c5963db512734

--- drivers/staging/android/vsoc.c
+++ drivers/staging/android/vsoc.c
@@ -801,8 +801,8 @@ static int vsoc_probe_device(struct pci_dev *pdev,
 	vsoc_dev.shm_phys_start = pci_resource_start(pdev, SHARED_MEMORY_BAR);
 	vsoc_dev.shm_size = pci_resource_len(pdev, SHARED_MEMORY_BAR);
 
-	dev_info(&pdev->dev, "shared memory @ DMA %p size=0x%zx\n",
-		 (void *)vsoc_dev.shm_phys_start, vsoc_dev.shm_size);
+	dev_info(&pdev->dev, "shared memory @ DMA %pa size=0x%zx\n",
+		 &vsoc_dev.shm_phys_start, vsoc_dev.shm_size);
 	/* TODO(ghartman): ioremap_wc should work here */
 	vsoc_dev.kernel_mapped_shm = ioremap_nocache(
 			vsoc_dev.shm_phys_start, vsoc_dev.shm_size);
