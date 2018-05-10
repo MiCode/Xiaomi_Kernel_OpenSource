a7e53181170bef83b45302b6fc828f9e03f59573
Dietmar Eggemann <dietmar.eggemann@arm.com>
2018-05-10 15:48:06 +0100

    ANDROID: sched/fair: add arch scaling function for max frequency capping
    To be able to scale the cpu capacity by this factor introduce a call to
    the new arch scaling function arch_scale_max_freq_capacity() in
    update_cpu_capacity() and provide a default implementation which returns
    SCHED_CAPACITY_SCALE.
    
    Another subsystem (e.g. cpufreq) or architectural or platform specific
    code can overwrite this default implementation, exactly as for frequency
    and cpu invariance. It has to be enabled by the arch by defining
    arch_scale_max_freq_capacity to the actual implementation.
    
    Change-Id: I770a8b1f4f7340e9e314f71c64a765bf880f4b4d
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Git-commit: 44791a47f588394e3d755dc01c528bc072409eef
    Git-repo: https://android.googlesource.com/kernel/common/
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9320,6 +9320,9 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 
 	cpu_rq(cpu)->cpu_capacity_orig = capacity;
 
+	capacity *= arch_scale_max_freq_capacity(sd, cpu);
+	capacity >>= SCHED_CAPACITY_SHIFT;
+
 	capacity *= scale_rt_capacity(cpu);
 	capacity >>= SCHED_CAPACITY_SHIFT;
 
