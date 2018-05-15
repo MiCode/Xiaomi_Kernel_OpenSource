44d3dca2741b35dd3dacb4aca18bbef6fe47f7a5
Patrick Bellasi <patrick.bellasi@arm.com>
2018-05-15 12:43:31 +0100

    ANDROID: sched/fair: schedtune: update before schedutil
    When a task is enqueue its boosted value must be accounted on that CPU
    to better support the selection of the required frequency.
    However, schedutil is (implicitly) updated by update_load_avg() which
    always happens before schedtune_{en,de}queue_task(), thus potentially
    introducing a latency between boost value updates and frequency
    selections.
    
    Let's update schedtune at the beginning of enqueue_task_fair(),
    which will ensure that all schedutil updates will see the most
    updated boost value for a CPU.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Change-Id: I1038f00600dd43ca38b76b2c5681b4f438ae4036
    Git-commit: 78605d7daf0b8a9ec559fc38431dd19f015eed7a
    Git-repo: https://android.googlesource.com/kernel/common/
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5215,6 +5215,24 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	util_est_enqueue(&rq->cfs, p);
 
 	/*
+	 * The code below (indirectly) updates schedutil which looks at
+	 * the cfs_rq utilization to select a frequency.
+	 * Let's update schedtune here to ensure the boost value of the
+	 * current task is accounted for in the selection of the OPP.
+	 *
+	 * We do it also in the case where we enqueue a throttled task;
+	 * we could argue that a throttled task should not boost a CPU,
+	 * however:
+	 * a) properly implementing CPU boosting considering throttled
+	 *    tasks will increase a lot the complexity of the solution
+	 * b) it's not easy to quantify the benefits introduced by
+	 *    such a more complex solution.
+	 * Thus, for the time being we go for the simple solution and boost
+	 * also for throttled RQs.
+	 */
+	schedtune_enqueue_task(p, cpu_of(rq));
+
+	/*
 	 * If in_iowait is set, the code below may not trigger any cpufreq
 	 * utilization updates, so do it here explicitly with the IOWAIT flag
 	 * passed.
@@ -5254,25 +5272,6 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		update_cfs_shares(se);
 	}
 
-	/*
-	 * Update SchedTune accounting.
-	 *
-	 * We do it before updating the CPU capacity to ensure the
-	 * boost value of the current task is accounted for in the
-	 * selection of the OPP.
-	 *
-	 * We do it also in the case where we enqueue a throttled task;
-	 * we could argue that a throttled task should not boost a CPU,
-	 * however:
-	 * a) properly implementing CPU boosting considering throttled
-	 *    tasks will increase a lot the complexity of the solution
-	 * b) it's not easy to quantify the benefits introduced by
-	 *    such a more complex solution.
-	 * Thus, for the time being we go for the simple solution and boost
-	 * also for throttled RQs.
-	 */
-	schedtune_enqueue_task(p, cpu_of(rq));
-
 	if (!se) {
 		add_nr_running(rq, 1);
 		inc_rq_walt_stats(rq, p);
@@ -5295,6 +5294,14 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	struct cfs_rq *cfs_rq;
 	struct sched_entity *se = &p->se;
 	int task_sleep = flags & DEQUEUE_SLEEP;
+
+	/*
+	 * The code below (indirectly) updates schedutil which looks at
+	 * the cfs_rq utilization to select a frequency.
+	 * Let's update schedtune here to ensure the boost value of the
+	 * current task is not more accounted for in the selection of the OPP.
+	 */
+	schedtune_dequeue_task(p, cpu_of(rq));
 
 	for_each_sched_entity(se) {
 		cfs_rq = cfs_rq_of(se);
@@ -5337,15 +5344,6 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		update_load_avg(se, UPDATE_TG);
 		update_cfs_shares(se);
 	}
-
-	/*
-	 * Update SchedTune accounting
-	 *
-	 * We do it before updating the CPU capacity to ensure the
-	 * boost value of the current task is accounted for in the
-	 * selection of the OPP.
-	 */
-	schedtune_dequeue_task(p, cpu_of(rq));
 
 	if (!se) {
 		sub_nr_running(rq, 1);
