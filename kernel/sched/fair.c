52190baa5abd39930eb682ec1d2a8832d0c52694
Runmin Wang <runminw@codeaurora.org>
2018-05-15 18:23:56 -0700

    drivers: base: arch_topology: swap the order of updating shared_cap_level
    Update the shared_cap_level from DIE level to THREAD level. If a higher
    level has shared_cap_level set, we also consider lower level sharing
    capacity state.
    
    Change-Id: I96bd0a80d2af7937311f131e6f46cca3a82af7b0
    Signed-off-by: Runmin Wang <runminw@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5966,9 +5966,12 @@ static int compute_energy(struct energy_env *eenv)
 		 * when we took visit_cpus.
 		 */
 		sd = rcu_dereference(per_cpu(sd_scs, cpu));
-		if (sd && sd->parent)
-			sg_shared_cap = sd->parent->groups;
-
+		if (sd) {
+			if (sd->parent)
+				sg_shared_cap = sd->parent->groups;
+			else /* single cluster system */
+				sg_shared_cap = sd->groups;
+		}
 		for_each_domain(cpu, sd) {
 			sg = sd->groups;
 			/* Has this sched_domain already been visited? */
