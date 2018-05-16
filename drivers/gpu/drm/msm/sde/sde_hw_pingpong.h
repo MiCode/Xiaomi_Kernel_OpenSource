1d9dd22e1e9e34d62cfcf8f5625296630b0d7511
Narendra Muppalla <NarendraM@codeaurora.org>
2018-05-16 16:50:26 -0700

    drm/msm/sde: reset clt path after timing engine disabled
    For MDP rev 5.0, CTL path reset is required after timing engine
    is disabled for video mode interface. The reset includes 3d_merge,
    active_intf, layer mixer and ctl path after timing engine is
    disabled. This reset call does not need to wait for vsync because
    timing engine is already disabled. It may lead to continuous underrun
    if reset sequence is not followed and same ctl path is used for
    different resolution display.
    
    Change-Id: I06492a0c12e860616aa09e462e34717592f2f9e3
    Signed-off-by: Narendra Muppalla <NarendraM@codeaurora.org>

--- drivers/gpu/drm/msm/sde/sde_hw_pingpong.h
+++ drivers/gpu/drm/msm/sde/sde_hw_pingpong.h
@@ -128,6 +128,11 @@ struct sde_hw_pingpong_ops {
 	 */
 	void (*setup_3d_mode)(struct sde_hw_pingpong *pp,
 			enum sde_3d_blend_mode cfg);
+
+	/**
+	 * reset 3d blend configuration
+	 */
+	void (*reset_3d_mode)(struct sde_hw_pingpong *pp);
 };
 
 struct sde_hw_merge_3d_ops {
@@ -136,6 +141,11 @@ struct sde_hw_merge_3d_ops {
 	 */
 	void (*setup_blend_mode)(struct sde_hw_merge_3d *id,
 			enum sde_3d_blend_mode cfg);
+
+	/**
+	 * reset 3d blend mode configuration
+	 */
+	void (*reset_blend_mode)(struct sde_hw_merge_3d *id);
 };
 
 struct sde_hw_merge_3d {
