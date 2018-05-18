f5281284b8dcae9ef88bd857bc48a3580f1594e4
Karthik Anantha Ram <kartanan@codeaurora.org>
2018-05-18 14:47:00 -0700

    msm: camera: isp: Define new callback for early PCR
    Add reg_update callback to handle irq in the event that early
    PCR is enabled. This callback will be invoked only for the
    first frame after streamon.
    
    Change-Id: I007e5d6380c624910685d4f1ae5669b3458d0b1f
    Signed-off-by: Karthik Anantha Ram <kartanan@codeaurora.org>

--- drivers/media/platform/msm/camera/cam_isp/cam_isp_context.c
+++ drivers/media/platform/msm/camera/cam_isp/cam_isp_context.c
@@ -503,6 +503,18 @@ static void __cam_isp_ctx_send_sof_timestamp(
 
 }
 
+static int __cam_isp_ctx_reg_upd_in_epoch_state(
+	struct cam_isp_context *ctx_isp, void *evt_data)
+{
+	if (ctx_isp->frame_id == 1)
+		CAM_DBG(CAM_ISP, "Reg update for early PCR");
+	else
+		CAM_WARN(CAM_ISP,
+			"Unexpected reg update in activated substate:%d for frame_id:%lld",
+			ctx_isp->substate_activated, ctx_isp->frame_id);
+	return 0;
+}
+
 static int __cam_isp_ctx_reg_upd_in_activated_state(
 	struct cam_isp_context *ctx_isp, void *evt_data)
 {
@@ -1118,7 +1130,7 @@ static struct cam_isp_ctx_irq_ops
 		.irq_ops = {
 			__cam_isp_ctx_handle_error,
 			__cam_isp_ctx_sof_in_epoch,
-			NULL,
+			__cam_isp_ctx_reg_upd_in_epoch_state,
 			__cam_isp_ctx_notify_sof_in_activated_state,
 			__cam_isp_ctx_notify_eof_in_activated_state,
 			__cam_isp_ctx_buf_done_in_epoch,
