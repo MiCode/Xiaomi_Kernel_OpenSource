f2e94fd87b12cc211ef09b44b2b09bd5c1461b7b
Rishabh Bhatnagar <rishabhb@codeaurora.org>
2018-05-23 16:58:07 -0700

    esoc: Add snapshot of esoc driver
    This is a snapshot of the esoc driver as of msm-4.14 commit
    <55e5efd553c10ef>. (Merge "nl80211: Fix external_auth check
    for offloaded authentication")
    
    Change-Id: Id47bcba3a5342986c3496707eb1e7cec03f19e3b
    Signed-off-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>

---
+++ drivers/esoc/mdm-dbg.h
@@ -0,0 +1,49 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2015, 2018, The Linux Foundation. All rights reserved.
+ */
+
+static bool debug_init_done;
+
+#ifndef CONFIG_ESOC_MDM_DBG_ENG
+
+static inline bool dbg_check_cmd_mask(unsigned int cmd)
+{
+	return false;
+}
+
+static inline bool dbg_check_notify_mask(unsigned int notify)
+{
+	return false;
+}
+
+static inline int mdm_dbg_eng_init(struct esoc_drv *drv,
+						struct esoc_clink *clink)
+{
+	return 0;
+}
+
+#else
+extern bool dbg_check_cmd_mask(unsigned int cmd);
+extern bool dbg_check_notify_mask(unsigned int notify);
+extern int mdm_dbg_eng_init(struct esoc_drv *drv,
+				struct esoc_clink *clink);
+#endif
+
+static inline bool mdm_dbg_stall_cmd(unsigned int cmd)
+{
+	if (debug_init_done)
+		return dbg_check_cmd_mask(cmd);
+	else
+		return false;
+}
+
+static inline bool mdm_dbg_stall_notify(unsigned int notify)
+{
+	if (debug_init_done)
+		return dbg_check_notify_mask(notify);
+	else
+		return false;
+}
+
+
