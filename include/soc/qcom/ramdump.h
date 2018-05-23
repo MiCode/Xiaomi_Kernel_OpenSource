6ff955e258e18d22f8f6f9aab0f6dc4608dbdfe7
Rishabh Bhatnagar <rishabhb@codeaurora.org>
2018-05-22 17:57:51 -0700

    soc: qcom: add snapshot of PIL, SSR, SYSMON and SERVICE LOCATOR
    This is a snapshot of PIL, SSR ,SYSMON and SERVICE LOCATOR drivers
    ,header files and libraries as of msm-4.14 commit <55e5efd553c10ef>
    (Merge "nl80211: Fix external_auth check for offloaded authentication")
    
    Change-Id: Id973652bfa0cdcda5718a7fe3c76c50ce0d80352
    Signed-off-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>

---
+++ include/soc/qcom/ramdump.h
@@ -0,0 +1,52 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2011-2014, 2017-2018, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef _RAMDUMP_HEADER
+#define _RAMDUMP_HEADER
+
+struct device;
+
+struct ramdump_segment {
+	char *name;
+	unsigned long address;
+	void *v_address;
+	unsigned long size;
+};
+
+#ifdef CONFIG_MSM_SUBSYSTEM_RESTART
+extern void *create_ramdump_device(const char *dev_name, struct device *parent);
+extern void destroy_ramdump_device(void *dev);
+extern int do_ramdump(void *handle, struct ramdump_segment *segments,
+		int nsegments);
+extern int do_elf_ramdump(void *handle, struct ramdump_segment *segments,
+		int nsegments);
+extern int do_minidump(void *handle, struct ramdump_segment *segments,
+		       int nsegments);
+
+#else
+static inline void *create_ramdump_device(const char *dev_name,
+		struct device *parent)
+{
+	return NULL;
+}
+
+static inline void destroy_ramdump_device(void *dev)
+{
+}
+
+static inline int do_ramdump(void *handle, struct ramdump_segment *segments,
+		int nsegments)
+{
+	return -ENODEV;
+}
+
+static inline int do_elf_ramdump(void *handle, struct ramdump_segment *segments,
+		int nsegments)
+{
+	return -ENODEV;
+}
+#endif /* CONFIG_MSM_SUBSYSTEM_RESTART */
+
+#endif
