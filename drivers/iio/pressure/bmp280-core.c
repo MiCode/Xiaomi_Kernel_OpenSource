b4164f8c69cf6cd11e6859dd0065826fed4cb779
Tomasz Duszynski <tduszyns@gmail.com>
2018-05-28 17:38:59 +0200

    iio: pressure: bmp280: fix relative humidity unit
    [ Upstream commit 13399ff25f179811ce9c1df1523eb39f9e4a4772 ]
    
    According to IIO ABI relative humidity reading should be
    returned in milli percent.
    
    This patch addresses that by applying proper scaling and
    returning integer instead of fractional format type specifier.
    
    Note that the fixes tag is before the driver was heavily refactored
    to introduce spi support, so the patch won't apply that far back.
    
    Signed-off-by: Tomasz Duszynski <tduszyns@gmail.com>
    Fixes: 14beaa8f5ab1 ("iio: pressure: bmp280: add humidity support")
    Acked-by: Matt Ranostay <matt.ranostay@konsulko.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/iio/pressure/bmp280-core.c
+++ drivers/iio/pressure/bmp280-core.c
@@ -347,10 +347,9 @@ static int bmp280_read_humid(struct bmp280_data *data, int *val, int *val2)
 	adc_humidity = be16_to_cpu(tmp);
 	comp_humidity = bmp280_compensate_humidity(data, adc_humidity);
 
-	*val = comp_humidity;
-	*val2 = 1024;
+	*val = comp_humidity * 1000 / 1024;
 
-	return IIO_VAL_FRACTIONAL;
+	return IIO_VAL_INT;
 }
 
 static int bmp280_read_raw(struct iio_dev *indio_dev,
