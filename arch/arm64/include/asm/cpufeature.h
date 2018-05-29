c085b8379d21573f61a33c7e547cccc97bf8c578
Marc Zyngier <marc.zyngier@arm.com>
2018-05-29 13:11:10 +0100

    arm64: ssbd: Add global mitigation state accessor
    We're about to need the mitigation state in various parts of the
    kernel in order to do the right thing for userspace and guests.
    
    Let's expose an accessor that will let other subsystems know
    about the state.
    
    Change-Id: I8c96e8980817ea336935eda2729b023ec56a818e
    Reviewed-by: Julien Grall <julien.grall@arm.com>
    Reviewed-by: Mark Rutland <mark.rutland@arm.com>
    Acked-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Git-commit: c32e1736ca03904c03de0e4459a673be194f56fd
    Git-repo: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Signed-off-by: Isaac J. Manjarres <isaacm@codeaurora.org>

--- arch/arm64/include/asm/cpufeature.h
+++ arch/arm64/include/asm/cpufeature.h
@@ -268,6 +268,16 @@ static inline bool system_uses_ttbr0_pan(void)
 		!cpus_have_const_cap(ARM64_HAS_PAN);
 }
 
+static inline int arm64_get_ssbd_state(void)
+{
+#ifdef CONFIG_ARM64_SSBD
+	extern int ssbd_state;
+	return ssbd_state;
+#else
+	return ARM64_SSBD_UNKNOWN;
+#endif
+}
+
 #endif /* __ASSEMBLY__ */
 
 #endif
