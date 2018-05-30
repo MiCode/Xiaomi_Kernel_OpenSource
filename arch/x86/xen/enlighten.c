474928b8f0a6ba49872ef2769610b80638820aad
Juergen Gross <jgross@suse.com>
2018-05-30 13:09:56 +0200

    x86/amd: revert commit 944e0fc51a89c9827b9
    Revert commit 944e0fc51a89c9827b98813d65dc083274777c7f ("x86/amd: don't
    set X86_BUG_SYSRET_SS_ATTRS when running under Xen") as it is lacking
    a prerequisite patch and is making things worse.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/xen/enlighten.c
+++ arch/x86/xen/enlighten.c
@@ -1980,8 +1980,10 @@ EXPORT_SYMBOL_GPL(xen_hvm_need_lapic);
 
 static void xen_set_cpu_features(struct cpuinfo_x86 *c)
 {
-	if (xen_pv_domain())
+	if (xen_pv_domain()) {
+		clear_cpu_bug(c, X86_BUG_SYSRET_SS_ATTRS);
 		set_cpu_cap(c, X86_FEATURE_XENPV);
+	}
 }
 
 static void xen_pin_vcpu(int cpu)
