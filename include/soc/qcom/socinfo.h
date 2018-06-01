630e245c05afb08b7ba7b340e2eea0d4d639e8c3
Swetha Chikkaboraiah <schikk@codeaurora.org>
2018-06-01 11:15:53 +0530

    soc: qcom: socinfo: Add support for msm8917 soc-id
    Add support for getting soc-id and dummy handle on msm8917.
    
    Change-Id: Id5a5984b57d05ef7ecea9af4e6d604d0837a35bb
    Signed-off-by: Swetha Chikkaboraiah <schikk@codeaurora.org>

--- include/soc/qcom/socinfo.h
+++ include/soc/qcom/socinfo.h
@@ -114,6 +114,8 @@
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,msm8953")
 #define early_machine_is_msm8937()	\
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,msm8937")
+#define early_machine_is_msm8917()	\
+	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,msm8917")
 #define early_machine_is_mdm9607()      \
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,mdm9607")
 #define early_machine_is_sdm450()	\
@@ -177,6 +179,7 @@
 #define early_machine_is_sdm710()	0
 #define early_machine_is_msm8953()	0
 #define early_machine_is_msm8937()	0
+#define early_machine_is_msm8917()	0
 #define early_machine_is_sdm450()	0
 #define early_machine_is_sdm632()	0
 #define early_machine_is_sdm439()	0
@@ -255,6 +258,7 @@ enum msm_cpu {
 	MSM_CPU_SDM632,
 	MSM_CPU_SDA632,
 	MSM_CPU_8937,
+	MSM_CPU_8917,
 	MSM_CPU_9607,
 	MSM_CPU_SDM439,
 	MSM_CPU_SDM429,
