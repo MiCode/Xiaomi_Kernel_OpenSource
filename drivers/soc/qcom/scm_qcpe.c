7cecb53b5094f2606161bad469c63989b0394e86
Yong Ding <yongding@codeaurora.org>
2018-06-04 17:26:59 +0800

    soc: qcom: scm_qcpe: use uninterruptible habmm_socket_recv()
    This is necessary to support scm call in system shutdown
    procedure. With interruptible habmm_socket_recv(), it will
    return immediately without blocking until the response
    arrives.
    
    Change-Id: Ia7ef95e4c6878a149ec373c8d2b5ae19a36f4125
    Signed-off-by: Yong Ding <yongding@codeaurora.org>

--- drivers/soc/qcom/scm_qcpe.c
+++ drivers/soc/qcom/scm_qcpe.c
@@ -476,7 +476,8 @@ static int scm_call_qcpe(u32 fn_id, struct scm_desc *desc)
 	size_bytes = sizeof(smc_params);
 	memset(&smc_params, 0x0, sizeof(smc_params));
 
-	ret = habmm_socket_recv(handle, &smc_params, &size_bytes, 0, 0);
+	ret = habmm_socket_recv(handle, &smc_params, &size_bytes, 0,
+			HABMM_SOCKET_RECV_FLAGS_UNINTERRUPTIBLE);
 	if (ret) {
 		pr_err("habmm_socket_recv failed, ret= 0x%x\n", ret);
 		goto err_ret;
