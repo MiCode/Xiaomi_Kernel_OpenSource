ae3d0eeaf80619127cddca86bb6eb113d6ff0526
Teng Fei Fan <tengfei@codeaurora.org>
2018-06-05 10:50:32 +0800

    arm: Add BTB invalidation on switch_mm for Cortex-A73
    Invalidate the BTB when switching from one user context to
    another, to avoid aliasing attacks against the branch predictor.
    
    Change-Id: I175931afda9b81f961e6c0831da299d2f79aa175
    Signed-off-by: Teng Fei Fan <tengfei@codeaurora.org>

--- arch/arm/include/asm/mmu_context.h
+++ arch/arm/include/asm/mmu_context.h
@@ -24,6 +24,18 @@
 
 void __check_vmalloc_seq(struct mm_struct *mm);
 
+#ifdef CONFIG_HARDEN_BRANCH_PREDICTOR
+void arm_init_bp_hardening(void);
+void arm_apply_bp_hardening(void);
+#else
+static inline void arm_init_bp_hardening(void)
+{
+}
+static inline void arm_apply_bp_hardening(void)
+{
+}
+#endif
+
 #ifdef CONFIG_CPU_HAS_ASID
 
 void check_and_switch_context(struct mm_struct *mm, struct task_struct *tsk);
@@ -63,8 +75,10 @@ static inline void check_and_switch_context(struct mm_struct *mm,
 		 * finish_arch_post_lock_switch() call.
 		 */
 		mm->context.switch_pending = 1;
-	else
+	else {
+		arm_apply_bp_hardening();
 		cpu_switch_mm(mm->pgd, mm);
+	}
 }
 
 #ifndef MODULE
