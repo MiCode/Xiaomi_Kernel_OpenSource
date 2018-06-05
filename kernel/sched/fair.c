fcfb6e412d77e82816a93b8cd2776025f37f1396
Chris Redpath <chris.redpath@arm.com>
2018-06-05 12:21:33 +0100

    ANDROID: sched/fair: Also do misfit in overloaded groups
    If we can classify the group as overloaded, that overrides
    any classification as misfit but we may still have misfit
    tasks present. Check the rq we're looking at to see if
    this is the case.
    
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    [Removed stray reference to rq_has_misfit]
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Change-Id: Ida8eb66aa625e34de3fe2ee1b0dd8a78926273d8

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9854,6 +9854,10 @@ static int need_active_balance(struct lb_env *env)
 	if (env->src_grp_type == group_misfit_task)
 		return 1;
 
+	if (env->src_grp_type == group_overloaded &&
+	    env->src_rq->misfit_task_load)
+		return 1;
+
 	return unlikely(sd->nr_balance_failed > sd->cache_nice_tries+2);
 }
 
