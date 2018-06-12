78f10c5504919d77cfc79f24712a3708c390a97b
Steffen Klassert <steffen.klassert@secunet.com>
2018-06-12 12:44:26 +0200

    BACKPORT: xfrm: Extend the output_mark to support input direction and masking.
    We already support setting an output mark at the xfrm_state,
    unfortunately this does not support the input direction and
    masking the marks that will be applied to the skb. This change
    adds support applying a masked value in both directions.
    
    The existing XFRMA_OUTPUT_MARK number is reused for this purpose
    and as it is now bi-directional, it is renamed to XFRMA_SET_MARK.
    
    An additional XFRMA_SET_MARK_MASK attribute is added for setting the
    mask. If the attribute mask not provided, it is set to 0xffffffff,
    keeping the XFRMA_OUTPUT_MARK existing 'full mask' semantics.
    
    Co-developed-by: Tobias Brunner <tobias@strongswan.org>
    Co-developed-by: Eyal Birger <eyal.birger@gmail.com>
    Co-developed-by: Lorenzo Colitti <lorenzo@google.com>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: Tobias Brunner <tobias@strongswan.org>
    Signed-off-by: Eyal Birger <eyal.birger@gmail.com>
    Signed-off-by: Lorenzo Colitti <lorenzo@google.com>
    
    Conflicts: XFRM OFFLOAD not previously backported; removed incompatible
    device offload copying in xfrm_user.c, xfrm_device.c
    
    (cherry picked from commit 9b42c1f179a614e11893ae4619f0304a38f481ae)
    Signed-off-by: Benedict Wong <benedictwong@google.com>
    Bug: 115914689
    Change-Id: I9f554d1697a72ef5668e5251d560cc1ba4d0ab07

--- net/xfrm/xfrm_input.c
+++ net/xfrm/xfrm_input.c
@@ -251,6 +251,8 @@ int xfrm_input(struct sk_buff *skb, int nexthdr, __be32 spi, int encap_type)
 			goto drop;
 		}
 
+		skb->mark = xfrm_smark_get(skb->mark, x);
+
 		skb->sp->xvec[skb->sp->len++] = x;
 
 		spin_lock(&x->lock);
