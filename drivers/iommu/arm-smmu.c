c2beab3fa5240688deffeb9e87b7b7ff70ee5334
Patrick Daly <pdaly@codeaurora.org>
2018-06-13 20:44:08 -0700

    iommu: arm-smmu: Add error message for hibernation usecase
    The iommu driver may use hyp_assign to ensure its page tables are
    mapped in stage 2 page table appropriate for a secure usecase. As
    hypervisor configuration is not restored across hibernation, this
    usecase is not currently supported. Print an error instead of
    failing with an access violation.
    
    Change-Id: Iaaaae2c2199555f61cff915b994510a07984d42c
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -515,6 +515,11 @@ static void arm_smmu_secure_domain_unlock(struct arm_smmu_domain *smmu_domain)
 		mutex_unlock(&smmu_domain->assign_lock);
 }
 
+static bool arm_smmu_opt_hibernation(struct arm_smmu_device *smmu)
+{
+	return IS_ENABLED(CONFIG_HIBERNATION);
+}
+
 /*
  * init()
  * Hook for additional device tree parsing at probe time.
@@ -1685,6 +1690,14 @@ static int arm_smmu_init_domain_context(struct iommu_domain *domain,
 		dev_err(smmu->dev, "dynamic domains not supported\n");
 		ret = -EPERM;
 
+		goto out_unlock;
+	}
+
+	if (arm_smmu_has_secure_vmid(smmu_domain) &&
+	    arm_smmu_opt_hibernation(smmu)) {
+		dev_err(smmu->dev,
+			"Secure usecases not supported with hibernation\n");
+		ret = -EPERM;
 		goto out_unlock;
 	}
 
