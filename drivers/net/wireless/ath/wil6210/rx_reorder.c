89c28c31e99f4cc668ce2f20ebf3cc4706e81ed5
Dedy Lansky <dlansky@codeaurora.org>
2018-06-14 19:23:51 +0300

    wil6210: remove unused rx_reorder members
    Remove unused members from struct wil_tid_ampdu_rx
    
    Change-Id: I193d6d610ce615bd114d5a8356fa8c182c747f8a
    Signed-off-by: Dedy Lansky <dlansky@codeaurora.org>
    Signed-off-by: Maya Erez <merez@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Git-commit: 37f8d26d8347f659e4677b4e438708ce492262bf
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/kvalo/ath.git

--- drivers/net/wireless/ath/wil6210/rx_reorder.c
+++ drivers/net/wireless/ath/wil6210/rx_reorder.c
@@ -205,7 +205,6 @@ __acquires(&sta->tid_rx_lock) __releases(&sta->tid_rx_lock)
 
 	/* put the frame in the reordering buffer */
 	r->reorder_buf[index] = skb;
-	r->reorder_time[index] = jiffies;
 	r->stored_mpdu_num++;
 	wil_reorder_release(ndev, r);
 
@@ -251,11 +250,8 @@ struct wil_tid_ampdu_rx *wil_tid_ampdu_rx_alloc(struct wil6210_priv *wil,
 
 	r->reorder_buf =
 		kcalloc(size, sizeof(struct sk_buff *), GFP_KERNEL);
-	r->reorder_time =
-		kcalloc(size, sizeof(unsigned long), GFP_KERNEL);
-	if (!r->reorder_buf || !r->reorder_time) {
+	if (!r->reorder_buf) {
 		kfree(r->reorder_buf);
-		kfree(r->reorder_time);
 		kfree(r);
 		return NULL;
 	}
@@ -285,7 +281,6 @@ void wil_tid_ampdu_rx_free(struct wil6210_priv *wil,
 		kfree_skb(r->reorder_buf[i]);
 
 	kfree(r->reorder_buf);
-	kfree(r->reorder_time);
 	kfree(r);
 }
 
