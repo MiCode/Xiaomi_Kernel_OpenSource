c28e8913f5a4dc9b9fe8915eaddb61022a68b6b8
Amit Nischal <anischal@codeaurora.org>
2018-06-18 16:03:02 +0530

    clk: qcom: Update order of clocks registration
    Update the clock registration of PLLs before the branch
    and rcg clocks so as to avoid failure in getting parent
    rate during OPP table population. Thus update the clock
    references for the same.
    
    Change-Id: I4b1a92f54da8ee59a1012da0e2687355084eec7b
    Signed-off-by: Amit Nischal <anischal@codeaurora.org>

--- include/dt-bindings/clock/qcom,gpucc-sm6150.h
+++ include/dt-bindings/clock/qcom,gpucc-sm6150.h
@@ -15,17 +15,17 @@
 #define _DT_BINDINGS_CLK_QCOM_GPU_CC_SM6150_H
 
 /* GPUCC clock registers */
-#define GPU_CC_CRC_AHB_CLK			0
-#define GPU_CC_CX_APB_CLK			1
-#define GPU_CC_CX_GFX3D_CLK			2
-#define GPU_CC_CX_GFX3D_SLV_CLK			3
-#define GPU_CC_CX_GMU_CLK			4
-#define GPU_CC_CX_SNOC_DVM_CLK			5
-#define GPU_CC_CXO_AON_CLK			6
-#define GPU_CC_CXO_CLK				7
-#define GPU_CC_GMU_CLK_SRC			8
-#define GPU_CC_PLL0_OUT_AUX2			9
-#define GPU_CC_PLL1_OUT_AUX2			10
+#define GPU_CC_PLL0_OUT_AUX2			0
+#define GPU_CC_PLL1_OUT_AUX2			1
+#define GPU_CC_CRC_AHB_CLK			2
+#define GPU_CC_CX_APB_CLK			3
+#define GPU_CC_CX_GFX3D_CLK			4
+#define GPU_CC_CX_GFX3D_SLV_CLK			5
+#define GPU_CC_CX_GMU_CLK			6
+#define GPU_CC_CX_SNOC_DVM_CLK			7
+#define GPU_CC_CXO_AON_CLK			8
+#define GPU_CC_CXO_CLK				9
+#define GPU_CC_GMU_CLK_SRC			10
 #define GPU_CC_SLEEP_CLK			11
 #define GPU_CC_GX_GMU_CLK			12
 #define GPU_CC_GX_CXO_CLK			13
