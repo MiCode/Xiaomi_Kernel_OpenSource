779e18aa822348c98fca14e1ff29c0f0f3a4acd4
Josh Poimboeuf <jpoimboe@redhat.com>
2018-06-19 10:47:50 -0500

    objtool: Add machine_real_restart() to the noreturn list
    commit 684fb246578b9e81fc7b4ca5c71eae22edb650b2 upstream.
    
    machine_real_restart() is annotated as '__noreturn", so add it to the
    objtool noreturn list.  This fixes the following warning with clang and
    CONFIG_CC_OPTIMIZE_FOR_SIZE=y:
    
      arch/x86/kernel/reboot.o: warning: objtool: native_machine_emergency_restart() falls through to next function machine_power_off()
    
    Reported-by: Matthias Kaehlcke <mka@chromium.org>
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Matthias Kaehlcke <mka@chromium.org>
    Reviewed-by: Matthias Kaehlcke <mka@chromium.org>
    Link: https://lkml.kernel.org/r/791712792aa4431bdd55bf1beb33a169ddf3b4a2.1529423255.git.jpoimboe@redhat.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- tools/objtool/check.c
+++ tools/objtool/check.c
@@ -165,6 +165,7 @@ static int __dead_end_function(struct objtool_file *file, struct symbol *func,
 		"__reiserfs_panic",
 		"lbug_with_loc",
 		"fortify_panic",
+		"machine_real_restart",
 	};
 
 	if (func->bind == STB_WEAK)
