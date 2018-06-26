9b1ff4b800c1a990e5b0b4596d149559afa20010
Rishabh Bhatnagar <rishabhb@codeaurora.org>
2018-06-26 10:57:52 -0700

    drivers: soc: qcom: Add snapshot of boot stats driver
    Snapshot of boot stats driver as of msm-4.14 commmit <6a24e5b9fb27ad3>.
    (Merge "ARM: dts: msm: Disable dynamic-loading from HLOS_PHYSPOOL for qcs405").
    
    Change-Id: I72a73be6d24fed5e8aa05864b0e1b3c8896e7d58
    Signed-off-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>

---
+++ include/soc/qcom/boot_stats.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2013-2018, The Linux Foundation. All rights reserved.
+ */
+
+#ifdef CONFIG_MSM_BOOT_STATS
+int boot_stats_init(void);
+#else
+static inline int boot_stats_init(void) { return 0; }
+#endif
