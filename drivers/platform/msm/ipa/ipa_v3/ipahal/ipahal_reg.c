7ba87d508f93986f0e1fb79c471a21539a664730
Michael Adisumarta <madisuma@codeaurora.org>
2018-06-27 10:52:54 -0700

    msm: ipa4: Cleanup IPA registers dump
    Move the macros and change the return type of the functions.
    Add an if condition to include the n_registers for printing
    to dmesg. Create a separate IPC log for regdumps.
    
    Change-Id: Ic2c11eda268342277e79465a0cfb329b95bf868b
    Signed-off-by: Michael Adisumarta <madisuma@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipahal/ipahal_reg.c
+++ drivers/platform/msm/ipa/ipa_v3/ipahal/ipahal_reg.c
@@ -2280,7 +2280,7 @@ static struct ipahal_reg_obj ipahal_reg_objs[IPA_HW_MAX][IPA_REG_MAX] = {
 		0x00000830, 0x70, 8, 17, 1},
 };
 
-int ipahal_print_all_regs(bool print_to_dmesg)
+void ipahal_print_all_regs(bool print_to_dmesg)
 {
 	int i, j;
 
@@ -2290,7 +2290,7 @@ int ipahal_print_all_regs(bool print_to_dmesg)
 	if ((ipahal_ctx->hw_type < IPA_HW_v4_0) ||
 		(ipahal_ctx->hw_type >= IPA_HW_MAX)) {
 		IPAHAL_ERR("invalid IPA HW type (%d)\n", ipahal_ctx->hw_type);
-		return -EINVAL;
+		return;
 	}
 
 	for (i = 0; i < IPA_REG_MAX ; i++) {
@@ -2310,11 +2310,17 @@ int ipahal_print_all_regs(bool print_to_dmesg)
 					ipahal_read_reg_n(i, j));
 		}
 
-		for (; j < ipahal_reg_objs[ipahal_ctx->hw_type][i].n_end; j++)
-			IPAHAL_DBG_REG("%s_%u=0x%x\n", ipahal_reg_name_str(i),
-				j, ipahal_read_reg_n(i, j));
+		for (; j < ipahal_reg_objs[ipahal_ctx->hw_type][i].n_end; j++) {
+			if (print_to_dmesg)
+				IPAHAL_DBG_REG("%s_%u=0x%x\n",
+					ipahal_reg_name_str(i),
+					j, ipahal_read_reg_n(i, j));
+			else
+				IPAHAL_DBG_REG_IPC_ONLY("%s_%u=0x%x\n",
+					ipahal_reg_name_str(i),
+					j, ipahal_read_reg_n(i, j));
+		}
 	}
-	return 0;
 }
 
 /*
