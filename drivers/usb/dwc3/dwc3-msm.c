0f4b7af5dd0d95963fa6ace8dc1a4450d5bfffbd
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2018-06-28 13:11:45 +0530

    dwc3-msm: Fix panic issue during unbind of msm_dwc3
    During unbind of msm_dwc3, dwc3 core device will be removed due
    to which dwc's ipc_log_buf will be freed. Hence remove dbg_event
    after removing dwc3 core device as it access ipc_log_buf, which
    is already freed to avoid panic.
    
    Change-Id: I7e3d071f454afff4af900f07862327342842679f
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -3707,7 +3707,6 @@ static int dwc3_msm_remove(struct platform_device *pdev)
 		mdwc->hs_phy->flags &= ~PHY_HOST_MODE;
 	of_platform_depopulate(&pdev->dev);
 
-	dbg_event(0xFF, "Remov put", 0);
 	pm_runtime_disable(mdwc->dev);
 	pm_runtime_barrier(mdwc->dev);
 	pm_runtime_put_sync(mdwc->dev);
