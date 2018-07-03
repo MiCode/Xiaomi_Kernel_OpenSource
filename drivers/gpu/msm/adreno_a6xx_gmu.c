aa2bb7cf47bdd4c402a4d3155bc6fedfbe7c139d
Carter Cooper <ccooper@codeaurora.org>
2018-07-03 09:13:33 -0600

    msm: kgsl: Define all the GMU VA space
    There were holes left in the GMU VA assignment space.
    Fill the entire struct space out with known valid addresses.
    Use this master list rather than recreating it in multiple
    locations.
    
    Change-Id: I3d801457df1fe19f4dc953af913c044df79e4172
    Signed-off-by: Carter Cooper <ccooper@codeaurora.org>

--- drivers/gpu/msm/adreno_a6xx_gmu.c
+++ drivers/gpu/msm/adreno_a6xx_gmu.c
@@ -472,23 +472,14 @@ static int a6xx_rpmh_power_off_gpu(struct kgsl_device *device)
 	return 0;
 }
 
-#define GMU_ITCM_VA_START 0x0
-#define GMU_ITCM_VA_END   (GMU_ITCM_VA_START + 0x4000) /* 16 KB */
-
-#define GMU_DTCM_VA_START 0x40000
-#define GMU_DTCM_VA_END   (GMU_DTCM_VA_START + 0x4000) /* 16 KB */
-
-#define GMU_ICACHE_VA_START 0x4000
-#define GMU_ICACHE_VA_END   (GMU_ICACHE_VA_START + 0x3C000) /* 240 KB */
-
 static int load_gmu_fw(struct kgsl_device *device)
 {
 	struct gmu_device *gmu = KGSL_GMU_DEVICE(device);
 	uint8_t *fw = (uint8_t *)gmu->fw_image->data;
-	struct gmu_block_header *blk;
-	uint32_t *fwptr;
 	int j;
-	int tcm_slot;
+	int tcm_addr;
+	struct gmu_block_header *blk;
+	struct gmu_memdesc *md;
 
 	while (fw < (uint8_t *)gmu->fw_image->data + gmu->fw_image->size) {
 		blk = (struct gmu_block_header *)fw;
@@ -498,35 +489,31 @@ static int load_gmu_fw(struct kgsl_device *device)
 		if (blk->size == 0)
 			continue;
 
-		if ((blk->addr >= GMU_ITCM_VA_START) &&
-				(blk->addr < GMU_ITCM_VA_END)) {
-			fwptr = (uint32_t *)fw;
-			tcm_slot = (blk->addr - GMU_ITCM_VA_START)
-				/ sizeof(uint32_t);
+		md = gmu_get_memdesc(blk->addr, blk->size);
+		if (md == NULL) {
+			dev_err(&gmu->pdev->dev,
+					"No backing memory for 0x%8.8X\n",
+					blk->addr);
+			return -EINVAL;
+		}
+
+		if (md->mem_type == GMU_ITCM || md->mem_type == GMU_DTCM) {
+			uint32_t *fwptr = (uint32_t *)fw;
+
+			tcm_addr = (blk->addr - (uint32_t)md->gmuaddr) /
+				sizeof(uint32_t);
+
+			if (md->mem_type == GMU_ITCM)
+				tcm_addr += A6XX_GMU_CM3_ITCM_START;
+			else
+				tcm_addr += A6XX_GMU_CM3_DTCM_START;
 
 			for (j = 0; j < blk->size / sizeof(uint32_t); j++)
-				gmu_core_regwrite(device,
-					A6XX_GMU_CM3_ITCM_START + tcm_slot + j,
+				gmu_core_regwrite(device, tcm_addr + j,
 					fwptr[j]);
-		} else if ((blk->addr >= GMU_DTCM_VA_START) &&
-				(blk->addr < GMU_DTCM_VA_END)) {
-			fwptr = (uint32_t *)fw;
-			tcm_slot = (blk->addr - GMU_DTCM_VA_START)
-				/ sizeof(uint32_t);
-
-			for (j = 0; j < blk->size / sizeof(uint32_t); j++)
-				gmu_core_regwrite(device,
-					A6XX_GMU_CM3_DTCM_START + tcm_slot + j,
-					fwptr[j]);
-		} else if ((blk->addr >= GMU_ICACHE_VA_START) &&
-				(blk->addr < GMU_ICACHE_VA_END)) {
-			if (!is_cached_fw_size_valid(blk->size)) {
-				dev_err(&gmu->pdev->dev,
-						"GMU firmware size too big\n");
-				return -EINVAL;
-
-			}
-			memcpy(gmu->icache_mem->hostptr, fw, blk->size);
+		} else {
+			/* Copy the memory directly */
+			memcpy(md->hostptr, fw, blk->size);
 		}
 
 		fw += blk->size;
