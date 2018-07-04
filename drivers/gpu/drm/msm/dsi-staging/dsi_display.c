bcb373139475720beba4412299f805e916f1d5d6
Sandeep Panda <spanda@codeaurora.org>
2018-07-04 11:30:34 +0530

    drm/msm/dsi-staging: skip DCS command tx during recovery
    When ESD recovery is underway, then DSI host might be in
    a bad state and can not transmit DCS commands. So this
    change avoids sending any DCS commands when recovery is
    pending.
    
    Change-Id: I85272b6debcca8a2e40a97e1c39d9d8aeb8e31b4
    Signed-off-by: Sandeep Panda <spanda@codeaurora.org>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -747,7 +747,7 @@ int dsi_display_check_status(void *display, bool te_check_override)
 	SDE_EVT32(SDE_EVTLOG_FUNC_ENTRY);
 
 	/* Prevent another ESD check,when ESD recovery is underway */
-	if (panel->esd_recovery_pending) {
+	if (atomic_read(&panel->esd_recovery_pending)) {
 		dsi_panel_release_panel_lock(panel);
 		return rc;
 	}
@@ -783,7 +783,7 @@ int dsi_display_check_status(void *display, bool te_check_override)
 							false);
 	} else {
 		/* Handle Panel failures during display disable sequence */
-		panel->esd_recovery_pending = true;
+		atomic_set(&panel->esd_recovery_pending, 1);
 	}
 
 	dsi_display_clk_ctrl(dsi_display->dsi_clk_handle,
@@ -2775,11 +2775,19 @@ static int dsi_host_detach(struct mipi_dsi_host *host,
 static ssize_t dsi_host_transfer(struct mipi_dsi_host *host,
 				 const struct mipi_dsi_msg *msg)
 {
-	struct dsi_display *display = to_dsi_display(host);
+	struct dsi_display *display;
 	int rc = 0, ret = 0;
 
 	if (!host || !msg) {
 		pr_err("Invalid params\n");
+		return 0;
+	}
+
+	display = to_dsi_display(host);
+
+	/* Avoid sending DCS commands when ESD recovery is pending */
+	if (atomic_read(&display->panel->esd_recovery_pending)) {
+		pr_debug("ESD recovery pending\n");
 		return 0;
 	}
 
--- drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
@@ -144,7 +144,7 @@ static void dsi_bridge_pre_enable(struct drm_bridge *bridge)
 		return;
 	}
 
-	c_bridge->display->panel->esd_recovery_pending = false;
+	atomic_set(&c_bridge->display->panel->esd_recovery_pending, 0);
 
 	/* By this point mode should have been validated through mode_fixup */
 	rc = dsi_display_set_mode(c_bridge->display,
