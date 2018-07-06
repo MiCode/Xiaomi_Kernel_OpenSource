7d9fb5aea8d95789d634c860c825a2bd77e6af04
Rishabh Bhatnagar <rishabhb@codeaurora.org>
2018-07-05 23:08:02 -0700

    drivers: edac: Add Cache Error Reporting driver
    This is a snapshot of cache error reporting driver for Kryo processors
    as of msm-4.14 commit <709e9085afc60cc>. (Merge "Revert "staging,
    android: remove lowmemory killer from the tree"").
    
    Change-Id: I599ba130e6780cd4d58c00cd075fe57d1163d026
    Signed-off-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>

---
+++ arch/arm64/include/asm/kryo-arm64-edac.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2017-2018, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef ASM_KRYO_EDAC_H
+#define ASM_KRYO_EDAC_H
+
+#if defined(CONFIG_EDAC_KRYO_ARM64)
+void kryo_poll_cache_errors(void *info);
+#else
+static inline void kryo_poll_cache_errors(void *info) { }
+#endif
+
+#endif
