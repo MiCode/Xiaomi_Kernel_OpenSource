511af0973209195cc64271f6e77009741a351ae7
Sunil Khatri <sunilkh@codeaurora.org>
2018-07-19 17:10:39 +0530

    msm: kgsl: Add a property to find alignment of secure buffers
    Add a property to determine the hardware alignment
    constraint on secure buffers. XPUv2 and below
    have a minimum requirement of 1 MBytes alignment
    and hence driver should allocate memory with minimum
    alignment on size.
    
    Change-Id: Ie3ca5da489bc94ae57ddc6695e402463fd7a88c2
    Signed-off-by: Sunil Khatri <sunilkh@codeaurora.org>

--- include/uapi/linux/msm_kgsl.h
+++ include/uapi/linux/msm_kgsl.h
@@ -311,6 +311,7 @@ enum kgsl_timestamp_type {
 #define KGSL_PROP_HIGHEST_BANK_BIT	0x17
 #define KGSL_PROP_DEVICE_BITNESS	0x18
 #define KGSL_PROP_DEVICE_QDSS_STM	0x19
+#define KGSL_PROP_SECURE_BUFFER_ALIGNMENT 0x23
 
 struct kgsl_shadowprop {
 	unsigned long gpuaddr;
