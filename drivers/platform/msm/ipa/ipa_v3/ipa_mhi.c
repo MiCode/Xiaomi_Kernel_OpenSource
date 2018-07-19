d53feb8eac8eae4c88bbc12fc3960e37750a328b
Mohammed Javid <mjavid@codeaurora.org>
2018-07-19 20:16:39 +0530

    msm: ipa: Setting appropriate config on MHI ep
    Couple of configuration updates on MHI ep,
    
     - During MHI suspend sequence,
       suspend DL channels first and followed by UL channels.
       Because, if MHI suspended during high tput scenario,
       there could be a chance that Q6 is still pumping data towards it.
       Due to which UL channels disconnection
       will fail and assert the device.
    
     - Set ep_delay on MHI CLIENT PROD ep upon connect,
       to prevent some random data getting into IPA
       before even Q6 configures it.
    
     - During SSR in pre-shutdown cleanup, set ep_delay and holb on MHI ep's
       to make sure there is no ipa stall due to some packet in/out.
       Since the ep configs are done by Q6 and which is down in SSR.
    
    Change-Id: Ie315892a2d1403a7e1f2ecf40c7b6c12d4e93c91
    Signed-off-by: Mohammed Javid <mjavid@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipa_mhi.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_mhi.c
@@ -199,6 +199,7 @@ static int ipa_mhi_start_gsi_channel(enum ipa_client_type client,
 	struct ipa3_ep_context *ep;
 	const struct ipa_gsi_ep_config *ep_cfg;
 	bool burst_mode_enabled = false;
+	struct ipa_ep_cfg_ctrl ep_cfg_ctrl;
 
 	IPA_MHI_FUNC_ENTRY();
 
@@ -340,6 +341,20 @@ static int ipa_mhi_start_gsi_channel(enum ipa_client_type client,
 	}
 
 	*params->mhi = ch_scratch.mhi;
+
+	if (IPA_CLIENT_IS_PROD(ep->client) && ep->skip_ep_cfg) {
+		memset(&ep_cfg_ctrl, 0, sizeof(struct ipa_ep_cfg_ctrl));
+		ep_cfg_ctrl.ipa_ep_delay = true;
+		ep->ep_delay_set = true;
+		res = ipa3_cfg_ep_ctrl(ipa_ep_idx, &ep_cfg_ctrl);
+		if (res)
+			IPA_MHI_ERR("client (ep: %d) failed result=%d\n",
+			ipa_ep_idx, res);
+		else
+			IPA_MHI_DBG("client (ep: %d) success\n", ipa_ep_idx);
+	} else {
+		ep->ep_delay_set = false;
+	}
 
 	IPA_MHI_DBG("Starting channel\n");
 	res = gsi_start_channel(ep->gsi_chan_hdl);
@@ -524,6 +539,7 @@ int ipa3_disconnect_mhi_pipe(u32 clnt_hdl)
 {
 	struct ipa3_ep_context *ep;
 	int res;
+	struct ipa_ep_cfg_ctrl ep_cfg_ctrl;
 
 	IPA_MHI_FUNC_ENTRY();
 
@@ -538,6 +554,21 @@ int ipa3_disconnect_mhi_pipe(u32 clnt_hdl)
 	}
 
 	ep = &ipa3_ctx->ep[clnt_hdl];
+	if (ep->ep_delay_set == true) {
+		memset(&ep_cfg_ctrl, 0, sizeof(struct ipa_ep_cfg_ctrl));
+		ep_cfg_ctrl.ipa_ep_delay = false;
+		res = ipa3_cfg_ep_ctrl(clnt_hdl,
+			&ep_cfg_ctrl);
+		if (res) {
+			IPAERR
+			("client(ep:%d) failed to remove delay res=%d\n",
+				clnt_hdl, res);
+		} else {
+			IPADBG("client (ep: %d) delay removed\n",
+				clnt_hdl);
+			ep->ep_delay_set = false;
+		}
+	}
 
 	res = gsi_dealloc_channel(ep->gsi_chan_hdl);
 	if (res) {
