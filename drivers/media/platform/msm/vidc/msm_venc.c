939df9f95de91c9b9f626e362c6950fe1517a0bc
Paras Nagda <pnagda@codeaurora.org>
2018-07-19 18:51:14 +0530

    msm: vidc: Addition of enums for HLG and HDR10
    Add HLG and ST2084 Transfer characteristics enum values as
    specified in the latest HEVC spec.
    
    Change-Id: Iff19ff5c13c4861f4d97ab0433214fe95fa84459
    Signed-off-by: Paras Nagda <pnagda@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_venc.c
+++ drivers/media/platform/msm/vidc/msm_venc.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1385,7 +1385,7 @@ static struct msm_vidc_ctrl msm_venc_ctrls[] = {
 		.name = "Set Color space transfer characterstics",
 		.type = V4L2_CTRL_TYPE_INTEGER,
 		.minimum = MSM_VIDC_TRANSFER_BT709_5,
-		.maximum = MSM_VIDC_TRANSFER_BT_2020_12,
+		.maximum = MSM_VIDC_TRANSFER_HLG,
 		.default_value = MSM_VIDC_TRANSFER_601_6_625,
 		.step = 1,
 		.qmenu = NULL,
