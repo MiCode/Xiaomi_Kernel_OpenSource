07291a54b28e0ced1886c94c981cc173f4dac795
Baldev Sahu <bsahu@codeaurora.org>
2018-07-20 13:55:49 +0530

    msm: mdss: KW fixes in MDP driver
    Fixes to validate input arguments and return
    values of functions.
    
    Change-Id: I3ddef7d944bcdcfff027a57f5e0e4ec121b08104
    Signed-off-by: Nirmal Abraham <nabrah@codeaurora.org>
    Signed-off-by: Raghavendra Ambadas <rambad@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_mdp_intf_cmd.c
+++ drivers/video/fbdev/msm/mdss_mdp_intf_cmd.c
@@ -709,6 +709,7 @@ int mdss_mdp_get_split_display_ctls(struct mdss_mdp_ctl **ctl,
 			swap(*ctl, *sctl);
 		}
 	} else {
+		rc = -EINVAL;
 		pr_debug("%s no split mode:%d\n", __func__,
 			(*ctl)->mfd->split_mode);
 	}
