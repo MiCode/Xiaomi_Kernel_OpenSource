5bbc54f95157ffd1c794cad3604d845863bda2ca
Ritesh Harjani <riteshh@codeaurora.org>
2018-07-21 12:20:55 +0530

    sdcardfs: Fix the format prints in sdcardfs
    Fix the %p format prints with %pK.
    
    Change-Id: I5fbe70350d7b4d986a2b070c9a2602d9d7680dc9
    Signed-off-by: Ritesh Harjani <riteshh@codeaurora.org>

--- fs/sdcardfs/main.c
+++ fs/sdcardfs/main.c
@@ -264,7 +264,7 @@ static int sdcardfs_read_super(struct vfsmount *mnt, struct super_block *sb,
 
 	pr_info("sdcardfs: dev_name -> %s\n", dev_name);
 	pr_info("sdcardfs: options -> %s\n", (char *)raw_data);
-	pr_info("sdcardfs: mnt -> %p\n", mnt);
+	pr_info("sdcardfs: mnt -> %pK\n", mnt);
 
 	/* parse lower path */
 	err = kern_path(dev_name, LOOKUP_FOLLOW | LOOKUP_DIRECTORY,
