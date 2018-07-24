b06856655014e87103a169da6636f2607c19c655
Rahul Shahare <rshaha@codeaurora.org>
2018-07-24 13:11:40 +0530

    Revert "mmc: core: Update SD card removal logic based on cd gpio state"
    This reverts commit a4d77289532a5dd7c0d638dc20f69ea73f98f2cf.
    
    Change-Id: I0d0d9514c4080ab60071398e7f4f603a5e31bb85
    Signed-off-by: Rahul Shahare <rshaha@codeaurora.org>

--- drivers/mmc/core/slot-gpio.c
+++ drivers/mmc/core/slot-gpio.c
@@ -34,10 +34,6 @@ static irqreturn_t mmc_gpio_cd_irqt(int irq, void *dev_id)
 {
 	/* Schedule a card detection after a debounce timeout */
 	struct mmc_host *host = dev_id;
-	int present = host->ops->get_cd(host);
-
-	pr_debug("%s: cd gpio irq, gpio state %d (CARD_%s)\n",
-		mmc_hostname(host), present, present?"INSERT":"REMOVAL");
 
 	host->trigger_card_event = true;
 	mmc_detect_change(host, msecs_to_jiffies(200));
