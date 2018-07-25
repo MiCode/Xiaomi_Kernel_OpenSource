3fcb8cf104600afb3108be6cdfb7fa548eb91b41
Vevek Venkatesan <vevekv@codeaurora.org>
2018-07-25 18:23:15 +0530

    input: touchscreen: hxchipset: add display dependency
    Add display dependency for chip init in hxchipset driver,
    if UEFI is not ready.
    
    Change-Id: Ia278d971d11b432f1e387170a0ca0b3966cb84e1
    Signed-off-by: Vevek Venkatesan <vevekv@codeaurora.org>
    Signed-off-by: Satyanarayana Dash <sadash@codeaurora.org>

--- drivers/input/touchscreen/hxchipset/himax_common.h
+++ drivers/input/touchscreen/hxchipset/himax_common.h
@@ -259,6 +259,7 @@ struct himax_report_data {
 };
 
 struct himax_ts_data {
+	bool initialized;
 	bool suspended;
 	atomic_t suspend_mode;
 	uint8_t x_channel;
@@ -320,10 +321,8 @@ struct himax_ts_data {
 
 	int in_self_test;
 
-#if defined(CONFIG_FB)
+#if defined(CONFIG_FB) || defined(CONFIG_DRM)
 	struct notifier_block fb_notif;
-	struct workqueue_struct *himax_att_wq;
-	struct delayed_work work_att;
 #elif defined(CONFIG_HAS_EARLYSUSPEND)
 	struct early_suspend early_suspend;
 #endif
@@ -474,6 +473,7 @@ extern int himax_chip_common_suspend(struct himax_ts_data *ts);
 extern int himax_chip_common_resume(struct himax_ts_data *ts);
 extern int himax_chip_common_init(void);
 extern void himax_chip_common_deinit(void);
+extern int himax_fb_register(struct himax_ts_data *ts);
 extern int himax_input_register(struct himax_ts_data *ts);
 extern void himax_ts_work(struct himax_ts_data *ts);
 extern enum hrtimer_restart himax_ts_timer_func(struct hrtimer *timer);
