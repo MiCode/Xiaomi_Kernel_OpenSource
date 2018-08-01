0137ea2134c0baf935430eeee3a70a8b7a193450
Amit Pundir <amit.pundir@linaro.org>
2018-08-01 10:20:28 +0530

    ANDROID: arm64: Fix 4.9.114 merge
    4.9.114 merge commit introduced multiple definition of
    "adr_this_cpu" and "ldr_this_cpu" macros. So remove the
    duplicates added in Change-Id: I3f40a2dd358eca3bc19dde562e77a990bbfe72e9
    ("UPSTREAM: arm64: assembler: introduce ldr_this_cpu").
    
    Also android-4.9 has upstream commit c02433dd6de3
    ("BACKPORT: arm64: split thread_info from task stack")
    backported, which renamed all TI_FLAGS to TSK_TI_FLAGS,
    hence fix LTS backport of upstream commit 9dd9614f5476
    ("arm64: ssbd: Introduce thread flag to control userspace mitigation").
    
    Change-Id: I0d7b1161eb2b6691504a765d50e58d2c42936f80
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- arch/arm64/include/asm/assembler.h
+++ arch/arm64/include/asm/assembler.h
@@ -254,28 +254,6 @@ lr	.req	x30		// link register
 	 */
 	.macro adr_this_cpu, dst, sym, tmp
 	adr_l	\dst, \sym
-	mrs	\tmp, tpidr_el1
-	add	\dst, \dst, \tmp
-	.endm
-
-	/*
-	 * @dst: Result of READ_ONCE(per_cpu(sym, smp_processor_id()))
-	 * @sym: The name of the per-cpu variable
-	 * @tmp: scratch register
-	 */
-	.macro ldr_this_cpu dst, sym, tmp
-	adr_l	\dst, \sym
-	mrs	\tmp, tpidr_el1
-	ldr	\dst, [\dst, \tmp]
-	.endm
-
-	/*
-	 * @dst: Result of per_cpu(sym, smp_processor_id())
-	 * @sym: The name of the per-cpu variable
-	 * @tmp: scratch register
-	 */
-	.macro adr_this_cpu, dst, sym, tmp
-	adr_l	\dst, \sym
 alternative_if_not ARM64_HAS_VIRT_HOST_EXTN
 	mrs	\tmp, tpidr_el1
 alternative_else
