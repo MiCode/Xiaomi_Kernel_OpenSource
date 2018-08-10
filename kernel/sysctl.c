d43b69c4ad2a977406c84d47fe8a5261e0099e78
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2018-08-09 17:19:54 -0700

    sched/core: fix userspace affining threads incorrectly
    Certain userspace applications, to achieve max performance, affines its
    threads to cpus that run the fastest. This is not always the
    correct strategy. For e.g. in certain architectures all the
    cores have the same max freq but few of them have a bigger
    cache. Affining to the cpus that have bigger cache is advantageous
    but such an application would end up affining them to all the cores.
    Similarly if an architecture has just one cpu that runs at max freq,
    it ends up crowding all its thread on that single core, which is
    detrimental for performance.
    
    To address this issue, we need to detect a suspicious looking affinity
    request from userspace and check if it links in a particular library.
    The latter can easily be detected by traversing executable vm areas
    that map a file and checking for that library name.
    When such a affinity request is found, change it to use a proper
    affinity. The suspicious affinity request, the proper affinity request
    and the library name can be configured by the userspace.
    
    Change-Id: I6bb8c310ca54c03261cc721f28dfd6023ab5591a
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -139,6 +139,7 @@ static int ten_thousand = 10000;
 #ifdef CONFIG_PERF_EVENTS
 static int six_hundred_forty_kb = 640 * 1024;
 #endif
+static int two_hundred_fifty_five = 255;
 
 /* this is needed for the proc_doulongvec_minmax of vm_dirty_bytes */
 static unsigned long dirty_bytes_min = 2 * PAGE_SIZE;
@@ -578,6 +579,31 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &one,
 	},
 #endif
+	{
+		.procname	= "sched_lib_name",
+		.data		= sched_lib_name,
+		.maxlen		= LIB_PATH_LENGTH,
+		.mode		= 0644,
+		.proc_handler	= proc_dostring,
+	},
+	{
+		.procname	= "sched_lib_mask_check",
+		.data		= &sched_lib_mask_check,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_douintvec_minmax,
+		.extra1		= &zero,
+		.extra2		= &two_hundred_fifty_five,
+	},
+	{
+		.procname	= "sched_lib_mask_force",
+		.data		= &sched_lib_mask_force,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_douintvec_minmax,
+		.extra1		= &zero,
+		.extra2		= &two_hundred_fifty_five,
+	},
 #ifdef CONFIG_PROVE_LOCKING
 	{
 		.procname	= "prove_locking",
