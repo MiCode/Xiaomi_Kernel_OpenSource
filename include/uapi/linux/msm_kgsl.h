4d6804439eb8f3f838bf0c91dd912dc41b2586a4
Sunil Khatri <sunilkh@codeaurora.org>
2018-08-10 11:46:58 +0530

    msm: kgsl: Add a property to find if secure context is supported
    Add a property to determine if a target support secure context
    for use cases like CPZ.
    
    This property can be used by userspace application to create
    a secure context if its supported on the target.
    
    Change-Id: I1ccc824378fb8fbd2cfbc7b811c6c3fdcd17803e
    Signed-off-by: Sunil Khatri <sunilkh@codeaurora.org>

--- include/uapi/linux/msm_kgsl.h
+++ include/uapi/linux/msm_kgsl.h
@@ -323,6 +323,7 @@ enum kgsl_timestamp_type {
 #define KGSL_PROP_DEVICE_QTIMER	0x20
 #define KGSL_PROP_IB_TIMEOUT 0x21
 #define KGSL_PROP_SECURE_BUFFER_ALIGNMENT 0x23
+#define KGSL_PROP_SECURE_CTXT_SUPPORT 0x24
 
 struct kgsl_shadowprop {
 	unsigned long gpuaddr;
