7a9151c900762f4dee678c273607c2a7b0826dab
Alexei Avshalom Lazar <ailizaro@codeaurora.org>
2018-08-13 15:33:00 +0300

    cfg80211: Add support for 60GHz band channels 5 and 6
    The current support in the 60GHz band is for channels 1-4.
    Add support for channels 5 and 6.
    This requires enlarging ieee80211_channel.center_freq from u16 to u32.
    
    Signed-off-by: Alexei Avshalom Lazar <ailizaro@codeaurora.org>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Git-commit: 9cf0a0b4b64ae103cf0e7dfaa72b44ecda24c0eb
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/jberg/mac80211-next.git
    CRs-Fixed: 2743684
    Change-Id: I8e38c2e97d1f1e8f8f3e21e91a6a70f335a8447a
    Signed-off-by: Vinita S. Maloo <vmaloo@codeaurora.org>

--- net/wireless/util.c
+++ net/wireless/util.c
@@ -93,7 +93,7 @@ int ieee80211_channel_to_frequency(int chan, enum nl80211_band band)
 			return 5940 + chan * 5;
 		break;
 	case NL80211_BAND_60GHZ:
-		if (chan < 5)
+		if (chan < 7)
 			return 56160 + chan * 2160;
 		break;
 	default:
@@ -117,7 +117,7 @@ int ieee80211_frequency_to_channel(int freq)
 	else if (freq <= 45000) /* DMG band lower limit */
 		/* see 802.11ax D4.1 27.3.22.2 */
 		return (freq - 5940) / 5;
-	else if (freq >= 58320 && freq <= 64800)
+	else if (freq >= 58320 && freq <= 70200)
 		return (freq - 56160) / 2160;
 	else
 		return 0;
@@ -1624,7 +1624,7 @@ bool ieee80211_chandef_to_operating_class(struct cfg80211_chan_def *chandef,
 	}
 
 	/* 56.16 GHz, channel 1..4 */
-	if (freq >= 56160 + 2160 * 1 && freq <= 56160 + 2160 * 4) {
+	if (freq >= 56160 + 2160 * 1 && freq <= 56160 + 2160 * 6) {
 		if (chandef->width >= NL80211_CHAN_WIDTH_40)
 			return false;
 
