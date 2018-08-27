05fe739dcabdf5def36f7c9e57e4fb1235ae1dc4
shubham <shubgupt@codeaurora.org>
2018-08-27 19:27:05 +0530

    msm_vidc: Workaround for unblocking
    Change-Id: I0d83af4d3f0bc6600fd4a1aed78c4d24916b3fc4
    Signed-off-by: shubham <shubgupt@codeaurora.org>
    Signed-off-by: Kiran Raparthy <kraparth@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_smem.c
+++ drivers/media/platform/msm/vidc/msm_smem.c
@@ -545,8 +545,7 @@ int msm_smem_cache_operations(struct dma_buf *dbuf,
 				offset, size);
 		if (rc)
 			break;
-		rc = dma_buf_end_cpu_access_partial(dbuf, DMA_TO_DEVICE,
-				offset, size);
+		rc = dma_buf_end_cpu_access(dbuf, DMA_TO_DEVICE);
 		break;
 	case SMEM_CACHE_INVALIDATE:
 		rc = dma_buf_begin_cpu_access_partial(dbuf, DMA_TO_DEVICE,
