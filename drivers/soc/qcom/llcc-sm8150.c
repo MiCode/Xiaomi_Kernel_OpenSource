514a58c149fd6074268ae96f938d662e5ebfb926
Isaac J. Manjarres <isaacm@codeaurora.org>
2018-08-28 09:55:20 -0700

    soc: sm8150: Update SCT table as per new version
    Regenerate the SCT table based on the updated hw/sw document.
    
    Change-Id: Ice513695023fd84566c1ee966ac8a8238600c0e5
    Signed-off-by: Isaac J. Manjarres <isaacm@codeaurora.org>

--- drivers/soc/qcom/llcc-sm8150.c
+++ drivers/soc/qcom/llcc-sm8150.c
@@ -68,7 +68,7 @@ static struct llcc_slice_config sm8150_data[] =  {
 	SCT_ENTRY("compute",     10, 10, 3072, 1, 1, 0xFFF, 0x0, 0, 0, 0, 1, 0),
 	SCT_ENTRY("gpuhtw",      11, 11, 512, 1, 1, 0xFFF, 0x0, 0, 0, 0, 1, 0),
 	SCT_ENTRY("gpu",         12, 12, 2560, 1, 1, 0xFFF, 0x0, 0, 0, 0, 1, 0),
-	SCT_ENTRY("mmuhwt",      13, 13, 3072, 1, 1, 0xFFF, 0x0, 0, 0, 0, 0, 1),
+	SCT_ENTRY("mmuhwt",      13, 13, 1024, 1, 1, 0xFFF, 0x0, 0, 0, 0, 0, 1),
 	SCT_ENTRY("compute_dma", 15, 15, 3072, 1, 1, 0xFFF, 0x0, 0, 0, 0, 1, 0),
 	SCT_ENTRY("display",     16, 16, 3072, 1, 1, 0xFFF, 0x0, 0, 0, 0, 1, 0),
 	SCT_ENTRY("modemhp_fix", 20, 20, 1024, 2, 1, 0xFFF, 0x0, 0, 0, 0, 1, 0),
@@ -76,7 +76,9 @@ static struct llcc_slice_config sm8150_data[] =  {
 	SCT_ENTRY("audiohw",     22, 22, 1024, 1, 1, 0xFFF, 0x0, 0, 0, 0, 1, 0),
 	SCT_ENTRY("npu",         23, 23, 3072, 1, 1, 0xFFF, 0x0, 0, 0, 0, 1, 0),
 	SCT_ENTRY("wlan_hw",     24, 24, 3072, 1, 1, 0xFFF, 0x0, 0, 0, 0, 1, 0),
-	SCT_ENTRY("pimem",       25, 25, 1024, 1, 1, 0xFFF, 0x0, 0, 0, 0, 1, 0),
+	SCT_ENTRY("modem_vpe",   29, 29, 256, 1, 1, 0xF,  0x0, 0, 0, 0, 1, 0),
+	SCT_ENTRY("ap_tcm",      30, 30, 128, 3, 0, 0x0,  0x3, 1, 0, 0, 1, 0),
+	SCT_ENTRY("write_cache", 31, 31, 128, 1, 1, 0x3,  0x0, 0, 0, 0, 0, 0),
 };
 
 static int sm8150_qcom_llcc_probe(struct platform_device *pdev)
