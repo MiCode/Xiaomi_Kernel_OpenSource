5f7eaf83d807a75e538fbf86dd9fd71c1b814175
Liam Mark <lmark@codeaurora.org>
2018-09-06 16:15:41 -0700

    ion: Ensure non-HLOS memory cannot be mapped by CPU
    Currently it is possible for an ION client to allocate non-HLOS memory
    (ie memory which isn't assigned to the HLOS vmid), map this memory, and
    then attempt to access this memory from the CPU.
    Attempting to access non-HLOS memory from the CPU will cause a
    stage-2 fault.
    
    Fix ION so that non-HLOS memory cannot be mapped by the CPU.
    
    Change-Id: Ifb51de2eabc076cddc744c13f01ef97b4a7c6874
    Signed-off-by: Liam Mark <lmark@codeaurora.org>

--- drivers/staging/android/ion/ion_cma_heap.c
+++ drivers/staging/android/ion/ion_cma_heap.c
@@ -313,14 +313,37 @@ err:
 	return ret;
 }
 
+static void *ion_secure_cma_map_kernel(struct ion_heap *heap,
+				       struct ion_buffer *buffer)
+{
+	if (!is_buffer_hlos_assigned(buffer)) {
+		pr_info("%s: Mapping non-HLOS accessible buffer disallowed\n",
+			__func__);
+		return NULL;
+	}
+	return ion_cma_map_kernel(heap, buffer);
+}
+
+static int ion_secure_cma_map_user(struct ion_heap *mapper,
+				   struct ion_buffer *buffer,
+				   struct vm_area_struct *vma)
+{
+	if (!is_buffer_hlos_assigned(buffer)) {
+		pr_info("%s: Mapping non-HLOS accessible buffer disallowed\n",
+			__func__);
+		return -EINVAL;
+	}
+	return ion_cma_mmap(mapper, buffer, vma);
+}
+
 static struct ion_heap_ops ion_secure_cma_ops = {
 	.allocate = ion_secure_cma_allocate,
 	.free = ion_secure_cma_free,
 	.map_dma = ion_cma_heap_map_dma,
 	.unmap_dma = ion_cma_heap_unmap_dma,
 	.phys = ion_cma_phys,
-	.map_user = ion_cma_mmap,
-	.map_kernel = ion_cma_map_kernel,
+	.map_user = ion_secure_cma_map_user,
+	.map_kernel = ion_secure_cma_map_kernel,
 	.unmap_kernel = ion_cma_unmap_kernel,
 	.print_debug = ion_cma_print_debug,
 };
