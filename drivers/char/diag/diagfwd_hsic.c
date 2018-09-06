c96980a14f960b9a2d2844fa9629f476469b27de
Manoj Prabhu B <bmanoj@codeaurora.org>
2018-09-06 12:24:42 +0530

    diag: Check remote proc presence status
    Add an additional check if the remote proc is present
    by way of probe status of interface protocol when queried by
    userspace IOCTL.
    
    Change-Id: Ie95aa7d038a683778fc1cf01598ecb92984b4685
    Signed-off-by: Manoj Prabhu B <bmanoj@codeaurora.org>

--- drivers/char/diag/diagfwd_hsic.c
+++ drivers/char/diag/diagfwd_hsic.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, 2016-2017 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2014, 2016-2018 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -392,6 +392,7 @@ static struct diag_remote_dev_ops diag_hsic_fwd_ops = {
 	.queue_read = hsic_queue_read,
 	.write = hsic_write,
 	.fwd_complete = hsic_fwd_complete,
+	.remote_proc_check = NULL,
 };
 
 int diag_hsic_init(void)
