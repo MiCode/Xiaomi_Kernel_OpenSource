4ed2b08b813c2f64663c20db3daabe45767a2077
Odelu Kukatla <okukatla@codeaurora.org>
2018-09-06 19:47:36 +0530

    clk: qcom: Add support for 610MHz for GPU clock on sdm670
    GPU clock requires to support 610MHz on sdm670, so add
    support for the same.
    
    Change-Id: I810a6169b4f099a152bb312ccd313fff8d0a53e6
    Signed-off-by: Odelu Kukatla <okukatla@codeaurora.org>

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -292,6 +292,7 @@ static const struct freq_tbl ftbl_gpu_cc_gx_gfx3d_clk_src_sdm670[] = {
 	F(430000000, P_CRC_DIV,  1, 0, 0),
 	F(504000000, P_CRC_DIV,  1, 0, 0),
 	F(565000000, P_CRC_DIV,  1, 0, 0),
+	F(610000000, P_CRC_DIV,  1, 0, 0),
 	F(650000000, P_CRC_DIV,  1, 0, 0),
 	F(700000000, P_CRC_DIV,  1, 0, 0),
 	F(750000000, P_CRC_DIV,  1, 0, 0),
