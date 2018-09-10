1ed237d8647f627680fdbfa8c7b054cb9b694a11
Chandana Kishori Chiluveru <cchiluve@codeaurora.org>
2018-09-10 19:27:36 +0530

    usb: gadget: Avoid NULL pointer dereference during OS descriptors handling
    With multi-config compositions some hosts can bind the configuration
    incorrectly and sending os descriptor request on invalid interface.
    This could cause accessing NULL pointer and results in panic.
    
    Fix this by bailing out from the OS descriptor setup request handling
    if the interface is NULL.
    
    Change-Id: I65f01b876a46b907eb883e48878fc081860b0753
    Signed-off-by: Chandana Kishori Chiluveru <cchiluve@codeaurora.org>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -1624,6 +1624,9 @@ static int count_ext_prop(struct usb_configuration *c, int interface)
 	struct usb_function *f;
 	int j;
 
+	if (interface >= c->next_interface_id)
+		return -EINVAL;
+
 	f = c->interface[interface];
 	for (j = 0; j < f->os_desc_n; ++j) {
 		struct usb_os_desc *d;
@@ -1642,6 +1645,9 @@ static int len_ext_prop(struct usb_configuration *c, int interface)
 	struct usb_function *f;
 	struct usb_os_desc *d;
 	int j, res;
+
+	if (interface >= c->next_interface_id)
+		return -EINVAL;
 
 	res = 10; /* header length */
 	f = c->interface[interface];
@@ -2035,6 +2041,8 @@ unknown:
 				if (w_length == 0x0A) {
 					count = count_ext_prop(os_desc_cfg,
 						interface);
+					if (count < 0)
+						return count;
 					put_unaligned_le16(count, buf + 8);
 					count = len_ext_prop(os_desc_cfg,
 						interface);
@@ -2044,6 +2052,8 @@ unknown:
 				} else {
 					count = count_ext_prop(os_desc_cfg,
 						interface);
+					if (count < 0)
+						return count;
 					put_unaligned_le16(count, buf + 8);
 					count = len_ext_prop(os_desc_cfg,
 						interface);
