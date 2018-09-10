b27d558fb577a460257c49e248136ea652466e4c
daopingl <cdaopin@codeaurora.org>
2018-09-10 18:44:03 +0800

    msm: camera: Fix warning when v4l2 query capability
    When device capabilities are missing, there will be many
    backtrace log printed and cause potential stability issue.
    this change used to fill the device capabilities after
    sensor driver probe success.
    
    Change-Id: I500883cd287e348061d7bc10c5d9760b4a9e45d6
    Signed-off-by: Daoping Lin <cdaopin@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/camera/camera.c
+++ drivers/media/platform/msm/camera_v2/camera/camera.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -933,6 +933,7 @@ int camera_init_v4l2(struct device *dev, unsigned int *session)
 	pvdev->vdev->ioctl_ops = &camera_v4l2_ioctl_ops;
 	pvdev->vdev->minor     = -1;
 	pvdev->vdev->vfl_type  = VFL_TYPE_GRABBER;
+	pvdev->vdev->device_caps = V4L2_CAP_VIDEO_CAPTURE | V4L2_CAP_STREAMING;
 	rc = video_register_device(pvdev->vdev,
 		VFL_TYPE_GRABBER, -1);
 	if (WARN_ON(rc < 0))
