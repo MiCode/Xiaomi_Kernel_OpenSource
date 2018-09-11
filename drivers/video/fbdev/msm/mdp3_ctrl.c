cf2cfe1c8b46198bf809b521c06a79d14f32364b
Abhijith Desai <desaia@codeaurora.org>
2018-09-11 15:09:27 +0530

    msm: mdss: Perform mdp3_ctrl_reset before DMA ROI is updated
    When partial update active and blit is not performed,
    then at the time of first commit DMA ROI is invalid.
    Because dma source is not initialized and prevents ROI
    update during commit.
    
    Fix issue by performing mdp3_ctrl_reset before DMA ROI is updated.
    
    Change-Id: Ic22d52a5687821de6d6c15811f4766d0735289da
    Signed-off-by: Sachin Bhayare <sachin.bhayare@codeaurora.org>
    Signed-off-by: Abhijith Desai <desaia@codeaurora.org>

--- drivers/video/fbdev/msm/mdp3_ctrl.c
+++ drivers/video/fbdev/msm/mdp3_ctrl.c
@@ -1546,22 +1546,6 @@ static int mdp3_ctrl_display_commit_kickoff(struct msm_fb_data_type *mfd,
 		return -EPERM;
 	}
 
-	if (panel_info->partial_update_enabled &&
-		is_roi_valid(mdp3_session->dma->source_config,
-			     cmt_data->l_roi) &&
-		update_roi(mdp3_session->dma->roi, cmt_data->l_roi)) {
-		mdp3_session->dma->roi.x = cmt_data->l_roi.x;
-		mdp3_session->dma->roi.y = cmt_data->l_roi.y;
-		mdp3_session->dma->roi.w = cmt_data->l_roi.w;
-		mdp3_session->dma->roi.h = cmt_data->l_roi.h;
-		mdp3_session->dma->update_src_cfg = true;
-		pr_debug("%s: ROI: x=%d y=%d w=%d h=%d\n", __func__,
-			mdp3_session->dma->roi.x,
-			mdp3_session->dma->roi.y,
-			mdp3_session->dma->roi.w,
-			mdp3_session->dma->roi.h);
-	}
-
 	panel = mdp3_session->panel;
 	mutex_lock(&mdp3_res->fs_idle_pc_lock);
 	if (mdp3_session->in_splash_screen ||
