f9781dae275e3fb8f20c652f4b919941d7c1316f
Alok Pandey <akumarpa@codeaurora.org>
2018-09-12 15:11:14 +0530

    msm: camera: icp: Avoid accessing unclocked registers
    If icp-pc is set, it may access register after clock
    is disabled. This change avoid any such access.
    
    Change-Id: I6cb56059e3ba5c7bf809ad544c50b2e89288e271
    Signed-off-by: Alok Pandey <akumarpa@codeaurora.org>

--- drivers/media/platform/msm/camera/cam_icp/icp_hw/icp_hw_mgr/cam_icp_hw_mgr.c
+++ drivers/media/platform/msm/camera/cam_icp/icp_hw/icp_hw_mgr/cam_icp_hw_mgr.c
@@ -4574,8 +4574,8 @@ static int cam_icp_mgr_create_wq(void)
 	if (rc)
 		goto debugfs_create_failed;
 
-	icp_hw_mgr.icp_pc_flag = false;
-	icp_hw_mgr.ipe_bps_pc_flag = false;
+	icp_hw_mgr.icp_pc_flag = true;
+	icp_hw_mgr.ipe_bps_pc_flag = true;
 
 	for (i = 0; i < ICP_WORKQ_NUM_TASK; i++)
 		icp_hw_mgr.msg_work->task.pool[i].payload =
