b01faaf0744e90e8640ab607e9237d0cb3757289
Hareesh Gundu <hareeshg@codeaurora.org>
2018-09-24 16:12:31 +0530

    msm: kgsl: Initial implementation of A6XX RGMU driver
    Add support for initial implementation of A6xx RGMU
    driver. Change adds new device tree node and other
    functionalities such as probe, start, stop, etc.
    RGMU is a reduced Graphics Management Unit which
    performs GPU power collapse.
    
    Change-Id: I72186fa4cbf10cf6d612940af29d93872ee9649c
    Signed-off-by: Hareesh Gundu <hareeshg@codeaurora.org>

--- drivers/gpu/msm/adreno-gpulist.h
+++ drivers/gpu/msm/adreno-gpulist.h
@@ -449,13 +449,14 @@ static const struct adreno_gpu_core adreno_gpulist[] = {
 		.minor = 8,
 		.patchid = ANY_ID,
 		.features = ADRENO_64BIT | ADRENO_CONTENT_PROTECTION |
-			ADRENO_IOCOHERENT | ADRENO_PREEMPTION,
+			ADRENO_IOCOHERENT | ADRENO_PREEMPTION | ADRENO_GPMU,
 		.sqefw_name = "a630_sqe.fw",
 		.zap_name = "a608_zap",
 		.gpudev = &adreno_a6xx_gpudev,
 		.gmem_size = (SZ_128K + SZ_4K),
 		.num_protected_regs = 0x20,
 		.busy_mask = 0xFFFFFFFE,
+		.gpmufw_name = "a608_rgmu.bin",
 	},
 	{
 		.gpurev = ADRENO_REV_A616,
