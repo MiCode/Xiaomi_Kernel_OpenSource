8f9bba2436fdf11a08aef1da2ebb1219ee26a764
Meng Wang <mengw@codeaurora.org>
2018-09-25 12:47:54 +0800

    ARM: dts: msm: Add snapshot of audio port types for soundwire
    This is a snapshot of the audio codec port types in new header
    required by soundwire interface as of msm-4.14 commit
    'I2325b3a8d04aaa5e204ab7e51041329700156154'(Add new file of
    audio port types for soundwire)
    
    Change-Id: I2325b3a8d04aaa5e204ab7e51041329700156154
    Signed-off-by: Ramprasad Katkam <katkam@codeaurora.org>
    Signed-off-by: Meng Wang <mengw@codeaurora.org>

---
+++ include/dt-bindings/sound/audio-codec-port-types.h
@@ -0,0 +1,39 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __AUDIO_CODEC_PORT_TYPES_H
+#define __AUDIO_CODEC_PORT_TYPES_H
+
+#define SPKR_L 1
+#define SPKR_L_BOOST 2
+#define SPKR_L_COMP 3
+#define SPKR_L_VI 4
+#define SPKR_R 5
+#define SPKR_R_BOOST 6
+#define SPKR_R_COMP 7
+#define SPKR_R_VI 8
+#define HPH_L 9
+#define HPH_R 10
+#define COMP_L 11
+#define COMP_R 12
+#define CLSH 13
+#define LO 14
+#define DSD_L 15
+#define DSD_R 16
+#define MBHC 17
+#define ADC1 18
+#define ADC2 19
+#define ADC3 20
+#define ADC4 21
+#define DMIC0 22
+#define DMIC1 23
+#define DMIC2 24
+#define DMIC3 25
+#define DMIC4 26
+#define DMIC5 27
+#define DMIC6 28
+#define DMIC7 29
+#define DMIC8 30
+#define DMIC9 31
+#define DMIC10 32
+#define PCM_OUT1 33
+
+#endif /* __AUDIO_CODEC_PORT_TYPES_H */
