f3dd33108513be195dfa294de94a6e4345698827
Ankit Jain <jankit@codeaurora.org>
2018-09-26 18:03:15 +0530

    fs: fat: ratelimit the IO errors
    In case of card is removed by low-level driver on removing
    the card physically, IO errors are expected for already
    queued IO requests.
    
    Ratelimit those errors with the default ratelimit to avoid
    watchdog bark issue in some cases.
    
    Change-Id: If91f53a8a86749fc18783c58ce8eb577347810c5
    Signed-off-by: Ankit Jain <jankit@codeaurora.org>

--- fs/fat/inode.c
+++ fs/fat/inode.c
@@ -625,10 +625,8 @@ static void fat_free_eofblocks(struct inode *inode)
 		 */
 		err = __fat_write_inode(inode, inode_needs_sync(inode));
 		if (err) {
-			fat_msg(inode->i_sb, KERN_WARNING, "Failed to "
-					"update on disk inode for unused "
-					"fallocated blocks, inode could be "
-					"corrupted. Please run fsck");
+			fat_msg_ratelimit(inode->i_sb, KERN_WARNING,
+				"Failed to update on disk inode for unused fallocated blocks, inode could be corrupted. Please run fsck");
 		}
 
 	}
@@ -672,7 +670,7 @@ static void fat_set_state(struct super_block *sb,
 
 	bh = sb_bread(sb, 0);
 	if (bh == NULL) {
-		fat_msg(sb, KERN_ERR, "unable to read boot sector "
+		fat_msg_ratelimit(sb, KERN_ERR, "unable to read boot sector "
 			"to mark fs as dirty");
 		return;
 	}
@@ -851,7 +849,7 @@ retry:
 	fat_get_blknr_offset(sbi, i_pos, &blocknr, &offset);
 	bh = sb_bread(sb, blocknr);
 	if (!bh) {
-		fat_msg(sb, KERN_ERR, "unable to read inode block "
+		fat_msg_ratelimit(sb, KERN_ERR, "unable to read inode block "
 		       "for updating (i_pos %lld)", i_pos);
 		return -EIO;
 	}
