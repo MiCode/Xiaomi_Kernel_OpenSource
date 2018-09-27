e302f861255579cc6e205679654bd60582cc4d45
Paras Nagda <pnagda@codeaurora.org>
2018-09-27 19:58:38 +0530

    msm: vidc: Fix dead lock during long stability run
    During long run stability there is a possibility
    that first thread (T1) which has acquired sync lock
    during streamon, is waiting for ctrl lock to be
    released by second thread (T2) and second thread (T2)
    while performing a set ctrl, has already acquired ctrl lock
    and is waiting for sync lock to be released by T1 there
    by creating a deadlock condition.
    
    With this change, ctrl lock is avoided by keeping the
    operating rate as part of instance variable and not by
    getting it from the control directly.
    
    Change-Id: Ie5e3dbcb9007fb2961c8a01f292fcce193f33c15
    Signed-off-by: Paras Nagda <pnagda@codeaurora.org>

--- drivers/media/platform/msm/vidc_3x/msm_vidc_internal.h
+++ drivers/media/platform/msm/vidc_3x/msm_vidc_internal.h
@@ -164,6 +164,7 @@ struct session_prop {
 	u32 height[MAX_PORT_NUM];
 	u32 fps;
 	u32 bitrate;
+	u32 operating_rate;
 };
 
 struct buf_queue {
@@ -230,6 +231,7 @@ enum msm_vidc_modes {
 	VIDC_TURBO = BIT(1),
 	VIDC_THUMBNAIL = BIT(2),
 	VIDC_LOW_POWER = BIT(3),
+	VIDC_REALTIME = BIT(4),
 };
 
 struct msm_vidc_core {
