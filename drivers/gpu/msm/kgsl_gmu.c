73bf5760c49e2c61ba7aa3df57046df5b178b08a
Sushmita Susheelendra <ssusheel@codeaurora.org>
2018-09-28 15:55:54 -0600

    msm: kgsl: Send NMI to GMU upon watchdog bite
    Send NMI to GMU to dump CM3 system state which includes stack
    being used at the time of the watchdog bite, and other key registers.
    This will help record information about what instruction was executing
    when the GMU watchdog bite occurred. This CM3 state is stored in both
    TCM and memory, and hence will be accessible through either a snapshot
    or a ramdump.
    
    Change-Id: I396a7794ffa6391cac0b588c71d06cf357e07a2b
    Signed-off-by: Sushmita Susheelendra <ssusheel@codeaurora.org>
    Signed-off-by: Deepak Kumar <dkumar@codeaurora.org>

--- drivers/gpu/msm/kgsl_gmu.c
+++ drivers/gpu/msm/kgsl_gmu.c
@@ -60,6 +60,8 @@ struct gmu_vma {
 	unsigned int image_start;
 };
 
+#define GMU_CM3_CFG_NONMASKINTR_SHIFT    9
+
 struct gmu_iommu_context {
 	const char *name;
 	struct device *dev;
@@ -786,6 +788,24 @@ static int gmu_rpmh_init(struct gmu_device *gmu, struct kgsl_pwrctrl *pwr)
 	return rpmh_arc_votes_init(gmu, &cx_arc, &mx_arc, GMU_ARC_VOTE);
 }
 
+static void send_nmi_to_gmu(struct adreno_device *adreno_dev)
+{
+	/* Mask so there's no interrupt caused by NMI */
+	adreno_write_gmureg(adreno_dev,
+			ADRENO_REG_GMU_GMU2HOST_INTR_MASK, 0xFFFFFFFF);
+
+	/* Make sure the interrupt is masked before causing it */
+	wmb();
+	adreno_write_gmureg(adreno_dev,
+		ADRENO_REG_GMU_NMI_CONTROL_STATUS, 0);
+	adreno_write_gmureg(adreno_dev,
+		ADRENO_REG_GMU_CM3_CFG,
+		(1 << GMU_CM3_CFG_NONMASKINTR_SHIFT));
+
+	/* Make sure the NMI is invoked before we proceed*/
+	wmb();
+}
+
 static irqreturn_t gmu_irq_handler(int irq, void *data)
 {
 	struct gmu_device *gmu = data;
@@ -806,6 +826,13 @@ static irqreturn_t gmu_irq_handler(int irq, void *data)
 		adreno_write_gmureg(adreno_dev,
 				ADRENO_REG_GMU_AO_HOST_INTERRUPT_MASK,
 				(mask | GMU_INT_WDOG_BITE));
+
+		send_nmi_to_gmu(adreno_dev);
+		/*
+		 * There is sufficient delay for the GMU to have finished
+		 * handling the NMI before snapshot is taken, as the fault
+		 * worker is scheduled below.
+		 */
 
 		dev_err_ratelimited(&gmu->pdev->dev,
 				"GMU watchdog expired interrupt received\n");
@@ -1356,19 +1383,8 @@ void gmu_snapshot(struct kgsl_device *device)
 	struct adreno_device *adreno_dev = ADRENO_DEVICE(device);
 	struct gmu_device *gmu = &device->gmu;
 
-	/* Mask so there's no interrupt caused by NMI */
-	adreno_write_gmureg(adreno_dev,
-			ADRENO_REG_GMU_GMU2HOST_INTR_MASK, 0xFFFFFFFF);
-
-	/* Make sure the interrupt is masked before causing it */
-	wmb();
-	adreno_write_gmureg(adreno_dev,
-		ADRENO_REG_GMU_NMI_CONTROL_STATUS, 0);
-	adreno_write_gmureg(adreno_dev,
-		ADRENO_REG_GMU_CM3_CFG, (1 << 9));
-
+	send_nmi_to_gmu(adreno_dev);
 	/* Wait for the NMI to be handled */
-	wmb();
 	udelay(100);
 	kgsl_device_snapshot(device, NULL, true);
 
