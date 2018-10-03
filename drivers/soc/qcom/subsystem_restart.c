d561d4aefbbb356101da819a48ae8e04a66630c1
Isaac J. Manjarres <isaacm@codeaurora.org>
2018-10-03 12:34:38 -0700

    soc: qcom: subsys_notif: Add support for early SSR notifications
    Currently, the SSR framework sends subsystem state transtition
    notifications during SSR to inform interested clients about
    the subsystem's state. However, there may be clients of the
    notification framework that would like to know that a
    subsystem has gone down, and before that subsystem has started
    undergoing SSR, so that they can ensure that communication
    towards a downed subsystem ceases.
    
    Add support for registering callbacks to be invoked as soon as
    a subsystem crashes, and before commencing SSR.
    
    Change-Id: Ia3ce647a7b1167b44ab9c97b4b0b0637e204b116
    Signed-off-by: Isaac J. Manjarres <isaacm@codeaurora.org>

--- drivers/soc/qcom/subsystem_restart.c
+++ drivers/soc/qcom/subsystem_restart.c
@@ -187,6 +187,7 @@ struct subsys_device {
 	struct subsys_tracking track;
 
 	void *notify;
+	void *early_notify;
 	struct device dev;
 	struct module *owner;
 	int count;
@@ -1218,6 +1219,8 @@ int subsystem_restart_dev(struct subsys_device *dev)
 
 	name = dev->desc->name;
 
+	send_early_notifications(dev->early_notify);
+
 	/*
 	 * If a system reboot/shutdown is underway, ignore subsystem errors.
 	 * However, print a message so that we know that a subsystem behaved
@@ -1792,6 +1795,7 @@ struct subsys_device *subsys_register(struct subsys_desc *desc)
 			sizeof(subsys->desc->fw_name));
 
 	subsys->notify = subsys_notif_add_subsys(desc->name);
+	subsys->early_notify = subsys_get_early_notif_info(desc->name);
 
 	snprintf(subsys->wlname, sizeof(subsys->wlname), "ssr(%s)", desc->name);
 	wakeup_source_init(&subsys->ssr_wlock, subsys->wlname);
