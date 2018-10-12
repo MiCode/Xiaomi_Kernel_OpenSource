d3f9f2be26df40748e6500c4c3e3c4a72d4ac764
Fei Mao <feim1@codeaurora.org>
2018-10-12 17:47:20 +0800

    input: touchscreen: hxchipset: enable firmware auto-update
    Enable the firmware auto-update, so we can upgrade it to
    the latest one from vendor.
    
    Change-Id: Iecc6288665e423f7279427fc8d545a0d830b697e
    Signed-off-by: Fei Mao <feim1@codeaurora.org>

--- drivers/input/touchscreen/hxchipset/himax_platform.c
+++ drivers/input/touchscreen/hxchipset/himax_platform.c
@@ -822,6 +822,15 @@ int himax_chip_common_probe(struct i2c_client *client, const struct i2c_device_i
 		goto err_fb_notify_reg_failed;
 #endif
 
+#ifdef HX_AUTO_UPDATE_FW
+	ts->himax_update_wq =
+		create_singlethread_workqueue("HMX_update_request");
+	if (!ts->himax_update_wq) {
+		E(" allocate syn_update_wq failed\n");
+		goto err_fb_notify_reg_failed;
+	}
+	INIT_DELAYED_WORK(&ts->work_update, himax_update_register);
+#endif
 	return ret;
 
 err_fb_notify_reg_failed:
