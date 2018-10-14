a2dda1f6d9c3a58c3c95d40cb9c626d0e5104c8a
Harry Yang <harryy@codeaurora.org>
2018-10-13 19:13:33 -0700

    power: smb1355: support ONLINE property in parallel psy
    The ONLINE property is required for all power supplies to show their
    charging path status. It's missing in parallel psy. Add it.
    
    Change-Id: I3ee1e104240ec54ed57bb7b048b3226c0b3aa9c4
    Signed-off-by: Harry Yang <harryy@codeaurora.org>

--- drivers/power/supply/qcom/smb1355-charger.c
+++ drivers/power/supply/qcom/smb1355-charger.c
@@ -522,6 +522,7 @@ static int smb1355_parse_dt(struct smb1355 *chip)
 
 static enum power_supply_property smb1355_parallel_props[] = {
 	POWER_SUPPLY_PROP_CHARGE_TYPE,
+	POWER_SUPPLY_PROP_ONLINE,
 	POWER_SUPPLY_PROP_CHARGING_ENABLED,
 	POWER_SUPPLY_PROP_PIN_ENABLED,
 	POWER_SUPPLY_PROP_INPUT_SUSPEND,
@@ -605,6 +606,7 @@ static int smb1355_parallel_get_prop(struct power_supply *psy,
 		rc = smb1355_get_prop_batt_charge_type(chip, val);
 		break;
 	case POWER_SUPPLY_PROP_CHARGING_ENABLED:
+	case POWER_SUPPLY_PROP_ONLINE:
 		rc = smb1355_read(chip, BATTERY_STATUS_3_REG, &stat);
 		if (rc >= 0)
 			val->intval = (bool)(stat & ENABLE_CHARGING_BIT);
