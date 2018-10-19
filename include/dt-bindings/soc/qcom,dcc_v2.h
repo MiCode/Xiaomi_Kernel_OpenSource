dd79deede99235cfb8dd315a5c8090da170919d1
Mulu He <muluhe@codeaurora.org>
2018-10-19 11:16:02 +0800

    dt-bindings: soc: qcom: Add dt-bindings support for DCC
    DCC is a hardware engine to write and read registers for system debug.
    
    Change-Id: I17b40bbc9956af8d72a5123472b9a351c7b9e4d3
    Signed-off-by: Mulu He <muluhe@codeaurora.org>
    Signed-off-by: Tingwei Zhang <tingwei@codeaurora.org>

---
+++ include/dt-bindings/soc/qcom,dcc_v2.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2017-2018, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef __DT_BINDINGS_QCOM_DCC_V2_H
+#define __DT_BINDINGS_QCOM_DCC_V2_H
+
+#define DCC_READ	0
+#define DCC_WRITE	1
+#define DCC_LOOP	2
+
+#endif
