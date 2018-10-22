a05cb661cd3bd7c340461035f6309e4a92007858
Ajay Agarwal <ajaya@codeaurora.org>
2018-10-22 15:29:42 +0530

    usb: gadget: Add support for IPC driver
    IPC messages are to be exchanged between device and host using
    the IPC router platform driver on the device side. For this
    driver to use USB as transport layer, add support for function
    driver which will configure Bulk IN and OUT endpoints and expose
    proper callbacks to router driver for read and write operations.
    
    Change-Id: I350624ccd24a6c6fd4bda5ea91379fb426fae742
    Signed-off-by: Ajay Agarwal <ajaya@codeaurora.org>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -73,6 +73,7 @@
 #include "u_qc_ether.c"
 #include "f_gsi.c"
 #include "f_mass_storage.h"
+#include "f_ipc.h"
 
 USB_ETHERNET_MODULE_PARAMETERS();
 #ifdef CONFIG_MEDIA_SUPPORT
@@ -3283,6 +3284,36 @@ static struct android_usb_function dpl_gsi_function = {
 	.bind_config	= dpl_gsi_function_bind_config,
 };
 
+static int ipc_function_init(struct android_usb_function *f,
+				   struct usb_composite_dev *cdev)
+{
+	f->config = ipc_setup();
+
+	return IS_ERR(f->config);
+}
+
+static void ipc_function_cleanup(struct android_usb_function *f)
+{
+	return ipc_cleanup(f->config);
+}
+
+static int ipc_function_bind_config(struct android_usb_function *f,
+					    struct usb_configuration *c)
+{
+	struct usb_function *ipc_f = NULL;
+
+	ipc_f = ipc_bind_config((struct usb_function_instance *)f->config);
+
+	return usb_add_function(c, ipc_f);
+}
+
+static struct android_usb_function ipc_function = {
+	.name           = "ipc",
+	.init           = ipc_function_init,
+	.cleanup        = ipc_function_cleanup,
+	.bind_config    = ipc_function_bind_config,
+};
+
 static struct android_usb_function *supported_functions[] = {
 	[ANDROID_FFS] = &ffs_function,
 	[ANDROID_MBIM_BAM] = &mbim_function,
@@ -3316,6 +3347,7 @@ static struct android_usb_function *supported_functions[] = {
 	[ANDROID_RMNET_GSI] = &rmnet_gsi_function,
 	[ANDROID_MBIM_GSI] = &mbim_gsi_function,
 	[ANDROID_DPL_GSI] = &dpl_gsi_function,
+	[ANDROID_IPC] = &ipc_function,
 	NULL
 };
 
@@ -3351,6 +3383,7 @@ static struct android_usb_function *default_functions[] = {
 #ifdef CONFIG_SND_RAWMIDI
 	&midi_function,
 #endif
+	&ipc_function,
 	NULL
 };
 
