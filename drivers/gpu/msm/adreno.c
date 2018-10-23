8c3b503e760e20a8688ffa7714055afdabaf552a
Harshdeep Dhatt <hdhatt@codeaurora.org>
2018-10-23 14:36:50 -0600

    msm: kgsl: Profile scm call on a640v1
    Log an error message if scm call takes more
    than 2 seconds on a640v1. This helps to root
    cause fence timeouts seen on a640v1.
    
    Change-Id: If51c3c8c878f97a5b686be1f0c15bb5c6284dffe
    Signed-off-by: Harshdeep Dhatt <hdhatt@codeaurora.org>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1874,15 +1874,24 @@ static int _adreno_start(struct adreno_device *adreno_dev)
 	if (regulator_left_on)
 		_soft_reset(adreno_dev);
 
-	if ((adreno_is_a640v1(adreno_dev)) &&
-		scm_is_call_available(SCM_SVC_MP, CP_SMMU_APERTURE_ID)) {
-		ret = kgsl_program_smmu_aperture();
-		if (ret) {
-			pr_err("SMMU aperture programming call failed with error %d\n",
-				ret);
-			goto error_pwr_off;
+
+	if (adreno_is_a640v1(adreno_dev)) {
+		unsigned long start = jiffies;
+
+		if (scm_is_call_available(SCM_SVC_MP, CP_SMMU_APERTURE_ID)) {
+			ret = kgsl_program_smmu_aperture();
+			/* Log it if it takes more than 2 seconds */
+			if (((jiffies - start) / HZ) > 2)
+				dev_err(device->dev, "scm call took too long to finish on a640v1: %lu seconds\n",
+					((jiffies - start) / HZ));
+			if (ret) {
+				dev_err(device->dev, "SMMU aperture programming call failed with error %d\n",
+					ret);
+				goto error_pwr_off;
+			}
 		}
 	}
+
 	adreno_ringbuffer_set_global(adreno_dev, 0);
 
 	status = kgsl_mmu_start(device);
