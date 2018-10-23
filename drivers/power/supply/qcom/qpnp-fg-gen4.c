f5edfc6ce7663b23897e74c9d09b0896093ab113
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2018-10-23 12:52:52 -0700

    power: qpnp-fg-gen4: Fix battery temperature delta configuration
    Currently, battery temperature delta configuration is allowed for
    an incorrect limit. Fix it. Also, add a proper check to allow
    temperature hysteresis configuration correctly as it also adds
    a dependency to battery temperature delta interrupt. Update the
    device tree bindings documentation as well.
    
    CRs-Fixed: 2336539
    Change-Id: I916138e8d87b5497abe0a8777a9145471324e260
    Signed-off-by: Subbaraman Narayanamurthy <subbaram@codeaurora.org>

--- drivers/power/supply/qcom/qpnp-fg-gen4.c
+++ drivers/power/supply/qcom/qpnp-fg-gen4.c
@@ -3989,18 +3989,14 @@ static int fg_gen4_hw_init(struct fg_gen4_chip *chip)
 	}
 
 	if (chip->dt.batt_temp_hyst != -EINVAL) {
-		if (chip->dt.batt_temp_cold_thresh != -EINVAL &&
-			chip->dt.batt_temp_hot_thresh != -EINVAL) {
-			val = chip->dt.batt_temp_hyst & BATT_TEMP_HYST_MASK;
-			mask = BATT_TEMP_HYST_MASK;
-			rc = fg_sram_masked_write(fg, BATT_TEMP_CONFIG2_WORD,
-					BATT_TEMP_HYST_DELTA_OFFSET, mask, val,
-					FG_IMA_DEFAULT);
-			if (rc < 0) {
-				pr_err("Error in writing batt_temp_hyst, rc=%d\n",
-					rc);
-				return rc;
-			}
+		val = chip->dt.batt_temp_hyst & BATT_TEMP_HYST_MASK;
+		mask = BATT_TEMP_HYST_MASK;
+		rc = fg_sram_masked_write(fg, BATT_TEMP_CONFIG2_WORD,
+				BATT_TEMP_HYST_DELTA_OFFSET, mask, val,
+				FG_IMA_DEFAULT);
+		if (rc < 0) {
+			pr_err("Error in writing batt_temp_hyst, rc=%d\n", rc);
+			return rc;
 		}
 	}
 
@@ -4371,6 +4367,40 @@ static int fg_parse_esr_cal_params(struct fg_dev *fg)
 	return 0;
 }
 
+#define BTEMP_DELTA_LOW			0
+#define BTEMP_DELTA_HIGH		3
+
+static void fg_gen4_parse_batt_temp_dt(struct fg_gen4_chip *chip)
+{
+	struct fg_dev *fg = &chip->fg;
+	struct device_node *node = fg->dev->of_node;
+	int rc, temp;
+
+	rc = of_property_read_u32(node, "qcom,fg-batt-temp-hot", &temp);
+	if (rc < 0)
+		chip->dt.batt_temp_hot_thresh = -EINVAL;
+	else
+		chip->dt.batt_temp_hot_thresh = temp;
+
+	rc = of_property_read_u32(node, "qcom,fg-batt-temp-cold", &temp);
+	if (rc < 0)
+		chip->dt.batt_temp_cold_thresh = -EINVAL;
+	else
+		chip->dt.batt_temp_cold_thresh = temp;
+
+	rc = of_property_read_u32(node, "qcom,fg-batt-temp-hyst", &temp);
+	if (rc < 0)
+		chip->dt.batt_temp_hyst = -EINVAL;
+	else if (temp >= BTEMP_DELTA_LOW && temp <= BTEMP_DELTA_HIGH)
+		chip->dt.batt_temp_hyst = temp;
+
+	rc = of_property_read_u32(node, "qcom,fg-batt-temp-delta", &temp);
+	if (rc < 0)
+		chip->dt.batt_temp_delta = -EINVAL;
+	else if (temp >= BTEMP_DELTA_LOW && temp <= BTEMP_DELTA_HIGH)
+		chip->dt.batt_temp_delta = temp;
+}
+
 #define DEFAULT_CUTOFF_VOLT_MV		3100
 #define DEFAULT_EMPTY_VOLT_MV		2812
 #define DEFAULT_SYS_TERM_CURR_MA	-125
@@ -4383,10 +4413,9 @@ static int fg_parse_esr_cal_params(struct fg_dev *fg)
 #define DEFAULT_CL_MAX_DEC_DECIPERC	100
 #define DEFAULT_CL_MIN_LIM_DECIPERC	0
 #define DEFAULT_CL_MAX_LIM_DECIPERC	0
-#define BTEMP_DELTA_LOW			2
-#define BTEMP_DELTA_HIGH		10
 #define DEFAULT_ESR_PULSE_THRESH_MA	47
 #define DEFAULT_ESR_MEAS_CURR_MA	120
+
 static int fg_gen4_parse_dt(struct fg_gen4_chip *chip)
 {
 	struct fg_dev *fg = &chip->fg;
@@ -4585,29 +4614,7 @@ static int fg_gen4_parse_dt(struct fg_gen4_chip *chip)
 	else
 		chip->cl->dt.max_cap_limit = temp;
 
-	rc = of_property_read_u32(node, "qcom,fg-batt-temp-hot", &temp);
-	if (rc < 0)
-		chip->dt.batt_temp_hot_thresh = -EINVAL;
-	else
-		chip->dt.batt_temp_hot_thresh = temp;
-
-	rc = of_property_read_u32(node, "qcom,fg-batt-temp-cold", &temp);
-	if (rc < 0)
-		chip->dt.batt_temp_cold_thresh = -EINVAL;
-	else
-		chip->dt.batt_temp_cold_thresh = temp;
-
-	rc = of_property_read_u32(node, "qcom,fg-batt-temp-hyst", &temp);
-	if (rc < 0)
-		chip->dt.batt_temp_hyst = -EINVAL;
-	else
-		chip->dt.batt_temp_hyst = temp;
-
-	rc = of_property_read_u32(node, "qcom,fg-batt-temp-delta", &temp);
-	if (rc < 0)
-		chip->dt.batt_temp_delta = -EINVAL;
-	else if (temp > BTEMP_DELTA_LOW && temp <= BTEMP_DELTA_HIGH)
-		chip->dt.batt_temp_delta = temp;
+	fg_gen4_parse_batt_temp_dt(chip);
 
 	chip->dt.hold_soc_while_full = of_property_read_bool(node,
 					"qcom,hold-soc-while-full");
