f382a2f71ab379fca6dd26f0b964eb4565dad946
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2018-10-23 13:07:05 +0530

    dwc3-msm: Fix lpm not entering issue during mode switch
    On some platforms, one GPIO might be used to switch between host
    and peripheral modes. In this case, extcon notifications will get
    called for ID status change and VBUS status change without delay for
    mode switch like host->peripheral or peripheral to host mode. If
    VBUS notification is received before USB host mode is stopped, there
    is a chance that suspend routine aborted due to incorect check even
    though state machine is not in B_PERIPHERAL or B_SUSPEND state. This
    results in USB enumeration not working in device mode. For USB
    enumeration to work, USB has to enter low power mode between mode
    switch, else USB will not work. Fix it by checking otg state for B_SUSPEND
    or B_PERIPHERAL as well to figure out any pending remote wakeup event.
    
    Change-Id: I9c0e2873e6cff5683d7b9a3d0fc833eb8088fa7c
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -2329,7 +2329,14 @@ static int dwc3_msm_suspend(struct dwc3_msm *mdwc, bool hibernation)
 		return -EBUSY;
 	}
 
-	if (!mdwc->in_host_mode && (mdwc->vbus_active && !mdwc->suspend)) {
+	/*
+	 * Check if remote wakeup is received and pending before going
+	 * ahead with suspend routine as part of device bus suspend.
+	 */
+
+	if (!mdwc->in_host_mode && (mdwc->vbus_active &&
+		(mdwc->otg_state == OTG_STATE_B_SUSPEND ||
+		mdwc->otg_state == OTG_STATE_B_PERIPHERAL) && !mdwc->suspend)) {
 		dev_dbg(mdwc->dev,
 			"Received wakeup event before the core suspend\n");
 		mutex_unlock(&mdwc->suspend_resume_mutex);
