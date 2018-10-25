0fd7ae3f996e2b852eeb9e619e5dfb56c33aaaf2
Satya Rama Aditya Pinapala <psraditya30@codeaurora.org>
2018-10-25 16:32:21 -0700

    clk:qcom: add mdss dsi and dp pll driver support
    This snapshot adds mdss dsi and dp pll driver support for
    different nm chipsets. Change includes updated licensing
    required for 4.19. This snapshot was taken from msm-4.14
    as of commit 5cc0f9c49e65 ("drivers: net: rmnet:
    introduce rmnet traces").
    
    Change-Id: Ic5a180f43836e672f0660c6a8f4a660dfc7cb74b
    Signed-off-by: Samantha Tran <samtran@codeaurora.org>
    Signed-off-by: Satya Rama Aditya Pinapala <psraditya30@codeaurora.org>

---
+++ drivers/clk/qcom/mdss/mdss-hdmi-pll.h
@@ -0,0 +1,54 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2012-2018, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef __MDSS_HDMI_PLL_H
+#define __MDSS_HDMI_PLL_H
+
+struct hdmi_pll_cfg {
+	unsigned long vco_rate;
+	u32 reg;
+};
+
+struct hdmi_pll_vco_clk {
+	unsigned long	rate;	/* current vco rate */
+	unsigned long	min_rate;	/* min vco rate */
+	unsigned long	max_rate;	/* max vco rate */
+	bool		rate_set;
+	struct hdmi_pll_cfg *ip_seti;
+	struct hdmi_pll_cfg *cp_seti;
+	struct hdmi_pll_cfg *ip_setp;
+	struct hdmi_pll_cfg *cp_setp;
+	struct hdmi_pll_cfg *crctrl;
+	void		*priv;
+
+	struct clk	c;
+};
+
+static inline struct hdmi_pll_vco_clk *to_hdmi_vco_clk(struct clk *clk)
+{
+	return container_of(clk, struct hdmi_pll_vco_clk, c);
+}
+
+int hdmi_pll_clock_register(struct platform_device *pdev,
+				struct mdss_pll_resources *pll_res);
+
+int hdmi_20nm_pll_clock_register(struct platform_device *pdev,
+				struct mdss_pll_resources *pll_res);
+
+int hdmi_8996_v1_pll_clock_register(struct platform_device *pdev,
+				 struct mdss_pll_resources *pll_res);
+
+int hdmi_8996_v2_pll_clock_register(struct platform_device *pdev,
+				 struct mdss_pll_resources *pll_res);
+
+int hdmi_8996_v3_pll_clock_register(struct platform_device *pdev,
+				 struct mdss_pll_resources *pll_res);
+
+int hdmi_8996_v3_1p8_pll_clock_register(struct platform_device *pdev,
+				 struct mdss_pll_resources *pll_res);
+
+int hdmi_8998_pll_clock_register(struct platform_device *pdev,
+				   struct mdss_pll_resources *pll_res);
+#endif
