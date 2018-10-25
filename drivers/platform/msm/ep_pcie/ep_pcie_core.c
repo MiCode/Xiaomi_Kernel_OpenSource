413b177c95aee3d84563eeac99d93862cbc3d26c
Siva Kumar Akkireddi <sivaa@codeaurora.org>
2018-10-25 21:30:37 +0530

    msm: mhi_dev: Move PCIE-EP and MHI drivers to subsys_init
    Move PCIE EP and MHI drivers to subsys init from module init to
    allow dependent drivers to be able to use MHI driver services.
    
    Signed-off-by: Siva Kumar Akkireddi <sivaa@codeaurora.org>
    Change-Id: I4888b61d841919336c40084ebd25285dd59621f6

--- drivers/platform/msm/ep_pcie/ep_pcie_core.c
+++ drivers/platform/msm/ep_pcie/ep_pcie_core.c
@@ -2575,7 +2575,7 @@ static void __exit ep_pcie_exit(void)
 	platform_driver_unregister(&ep_pcie_driver);
 }
 
-module_init(ep_pcie_init);
+subsys_initcall(ep_pcie_init);
 module_exit(ep_pcie_exit);
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("MSM PCIe Endpoint Driver");
