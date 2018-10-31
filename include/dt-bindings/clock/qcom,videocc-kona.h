e3d4859e70a8ac866bda959f3987ca65c3d5e924
Vivek Aknurwar <viveka@codeaurora.org>
2018-10-30 17:00:24 -0700

    dt-bindings: clock: add bindings for kona video cc driver
    Added dt-bindings and mm/mx info for kona.
    
    Change-Id: I598aef9b3082d29f15e02e0157e5cc2facbd1eb5
    Signed-off-by: Vivek Aknurwar <viveka@codeaurora.org>

--- include/dt-bindings/clock/qcom,videocc-kona.h
+++ include/dt-bindings/clock/qcom,videocc-kona.h
@@ -6,17 +6,21 @@
 
 #define VIDEO_CC_AHB_CLK					0
 #define VIDEO_CC_AHB_CLK_SRC					1
-#define VIDEO_CC_MVS0_CLK					2
-#define VIDEO_CC_MVS0_CLK_SRC					3
-#define VIDEO_CC_MVS0C_CLK					4
-#define VIDEO_CC_MVS1_CLK					5
-#define VIDEO_CC_MVS1_CLK_SRC					6
-#define VIDEO_CC_MVS1_DIV2_CLK					7
-#define VIDEO_CC_MVS1C_CLK					8
-#define VIDEO_CC_XO_CLK						9
-#define VIDEO_CC_XO_CLK_SRC					10
-#define VIDEO_PLL0						11
-#define VIDEO_PLL1						12
+#define VIDEO_CC_DEBUG_CLK					2
+#define VIDEO_CC_MVS0_CLK					3
+#define VIDEO_CC_MVS0_CLK_SRC					4
+#define VIDEO_CC_MVS0C_CLK					5
+#define VIDEO_CC_MVS1_CLK					6
+#define VIDEO_CC_MVS1_CLK_SRC					7
+#define VIDEO_CC_MVS1_DIV2_CLK					8
+#define VIDEO_CC_MVS1C_CLK					9
+#define VIDEO_CC_PLL_TEST_CLK					10
+#define VIDEO_CC_SLEEP_CLK					11
+#define VIDEO_CC_SLEEP_CLK_SRC					12
+#define VIDEO_CC_XO_CLK						13
+#define VIDEO_CC_XO_CLK_SRC					14
+#define VIDEO_PLL0						15
+#define VIDEO_PLL1						16
 
 #define MVS0_GDSC						0
 #define MVS0C_GDSC						1
