caceca44a0db9c7a7d0017de9c4adbeddeb6c620
Guru Das Srinagesh <gurus@codeaurora.org>
2018-11-02 15:50:47 -0700

    power: smb5: Add snapshot of SMB5 driver and its dependencies
    Add snapshot of the QPNP-SMB5 driver, along with its dependencies, as of
    msm-4.14 'commit 7e16ff6cdde5 ("defconfig: qcs405: enable WLAN configs
    for 32-bit QCS405")'.
    
    Get rid of the use of module parameters in qpnp-smb5.c and battery.c as
    they do not scale well for multiple device instances of the same driver.
    Instead:
    - move debug_mask to debugfs (both files), and
    - associate pd_disabled and weak_chg_icl_ua with the device instead,
      using sysfs (only qpnp-smb5.c)
    
    The QPNP-SMB5 driver controls battery charging and other related
    functionalities of the PM8150B chip and chips similar to PM8150B.
    
    Change-Id: Ic356e58aaadf810aa279ade59ae11bdc743788d5
    Signed-off-by: Guru Das Srinagesh <gurus@codeaurora.org>

---
+++ drivers/power/supply/qcom/battery.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2017 The Linux Foundation. All rights reserved.
+ */
+
+#ifndef __BATTERY_H
+#define __BATTERY_H
+int qcom_batt_init(int smb_version);
+void qcom_batt_deinit(void);
+#endif /* __BATTERY_H */
---
+++ drivers/power/supply/qcom/step-chg-jeita.h
@@ -0,0 +1,30 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2017-2018 The Linux Foundation. All rights reserved.
+ */
+
+#ifndef __STEP_CHG_H__
+#define __STEP_CHG_H__
+
+#define MAX_STEP_CHG_ENTRIES	8
+
+struct step_chg_jeita_param {
+	u32			psy_prop;
+	char			*prop_name;
+	int			hysteresis;
+	bool			use_bms;
+};
+
+struct range_data {
+	u32 low_threshold;
+	u32 high_threshold;
+	u32 value;
+};
+
+int qcom_step_chg_init(struct device *dev,
+		bool step_chg_enable, bool sw_jeita_enable);
+void qcom_step_chg_deinit(void);
+int read_range_data_from_node(struct device_node *node,
+		const char *prop_str, struct range_data *ranges,
+		u32 max_threshold, u32 max_value);
+#endif /* __STEP_CHG_H__ */
