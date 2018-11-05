2b8d593b14649ce04d601fd8daa527c09bb7b6a4
Hareesh Gundu <hareeshg@codeaurora.org>
2018-11-05 16:32:46 +0530

    msm: kgsl: Add a feature ADRENO_PERFCTRL_RETAIN
    Add a ADRENO_PERFCTRL_RETAIN feature to enable it
    target specific. This feature will allows to retain
    RBBM perf control enable setting value across power
    collapse interval. This is needed to start counting
    perf counter after the IFPC.
    
    Also enable this feature for A608 GPU.
    
    Change-Id: I79ba7cfa5afb7d16745673eb740b9cddc229d091
    Signed-off-by: Hareesh Gundu <hareeshg@codeaurora.org>

--- drivers/gpu/msm/adreno-gpulist.h
+++ drivers/gpu/msm/adreno-gpulist.h
@@ -469,7 +469,7 @@ static const struct adreno_gpu_core adreno_gpulist[] = {
 		.patchid = ANY_ID,
 		.features = ADRENO_64BIT | ADRENO_CONTENT_PROTECTION |
 			ADRENO_IOCOHERENT | ADRENO_PREEMPTION | ADRENO_GPMU |
-			ADRENO_IFPC,
+			ADRENO_IFPC | ADRENO_PERFCTRL_RETAIN,
 		.sqefw_name = "a630_sqe.fw",
 		.zap_name = "a608_zap",
 		.gpudev = &adreno_a6xx_gpudev,
