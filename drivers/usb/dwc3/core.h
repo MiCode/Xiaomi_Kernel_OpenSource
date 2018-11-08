930f9fd0c8b698a7db15ac52721b98a47ee9c931
Hemant Kumar <hemantk@codeaurora.org>
2018-11-07 21:13:41 -0800

    usb: dwc3: Increase the inter packet gap for USB 2.0
    Default 0 value for IP_GAP_ADD_ON field of DWC3_GUCTL1
    register is causing failure of EL_23 test. This value
    corresponds to inter-packet gap of 182.163 ns. Increase
    the value to 1 which corresponds to 199.794 ns. This
    improves margin required to pass the test.
    
    Change-Id: Iabc71a746ac90251eaec25597d8506df416796a5
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -257,6 +257,7 @@
 /* Global User Control 1 Register */
 #define DWC3_GUCTL1_TX_IPGAP_LINECHECK_DIS	BIT(28)
 #define DWC3_GUCTL1_DEV_L1_EXIT_BY_HW	BIT(24)
+#define DWC3_GUCTL1_IP_GAP_ADD_ON(n)	(n << 21)
 
 /* Global Status Register */
 #define DWC3_GSTS_OTG_IP	BIT(10)
