2032d330ed1f6ec07e23b940fe25451576b3e273
Taniya Das <tdas@codeaurora.org>
2018-11-09 14:44:07 +0530

    clk: qcom: Add clock ids for Lito
    Clocks those would be used by client drivers of various clock controllers
    such as GCC, CAMCC, GPUCC, NPUCC, VIDEOCC and DISPCC.
    
    Change-Id: Ic1974b9ea9766a32c5b8a57ed65c751bfb71d1b7
    Signed-off-by: Taniya Das <tdas@codeaurora.org>

---
+++ include/dt-bindings/clock/qcom,dispcc-lito.h
@@ -0,0 +1,48 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Copyright (c) 2018, The Linux Foundation. All rights reserved. */
+
+#ifndef _DT_BINDINGS_CLK_QCOM_DISP_CC_LITO_H
+#define _DT_BINDINGS_CLK_QCOM_DISP_CC_LITO_H
+
+#define DISP_CC_PLL0						0
+#define DISP_CC_MDSS_AHB_CLK_SRC				1
+#define DISP_CC_MDSS_BYTE0_CLK					2
+#define DISP_CC_MDSS_BYTE0_CLK_SRC				3
+#define DISP_CC_MDSS_BYTE0_INTF_CLK				4
+#define DISP_CC_MDSS_BYTE1_CLK					5
+#define DISP_CC_MDSS_BYTE1_CLK_SRC				6
+#define DISP_CC_MDSS_BYTE1_INTF_CLK				7
+#define DISP_CC_MDSS_DP_AUX_CLK					8
+#define DISP_CC_MDSS_DP_AUX_CLK_SRC				9
+#define DISP_CC_MDSS_DP_CRYPTO_CLK				10
+#define DISP_CC_MDSS_DP_CRYPTO_CLK_SRC				11
+#define DISP_CC_MDSS_DP_LINK_CLK				12
+#define DISP_CC_MDSS_DP_LINK_CLK_SRC				13
+#define DISP_CC_MDSS_DP_LINK_INTF_CLK				14
+#define DISP_CC_MDSS_DP_PIXEL1_CLK				15
+#define DISP_CC_MDSS_DP_PIXEL1_CLK_SRC				16
+#define DISP_CC_MDSS_DP_PIXEL_CLK				17
+#define DISP_CC_MDSS_DP_PIXEL_CLK_SRC				18
+#define DISP_CC_MDSS_ESC0_CLK					19
+#define DISP_CC_MDSS_ESC0_CLK_SRC				20
+#define DISP_CC_MDSS_ESC1_CLK					21
+#define DISP_CC_MDSS_ESC1_CLK_SRC				22
+#define DISP_CC_MDSS_MDP_CLK					23
+#define DISP_CC_MDSS_MDP_CLK_SRC				24
+#define DISP_CC_MDSS_MDP_LUT_CLK				25
+#define DISP_CC_MDSS_NON_GDSC_AHB_CLK				26
+#define DISP_CC_MDSS_PCLK0_CLK					27
+#define DISP_CC_MDSS_PCLK0_CLK_SRC				28
+#define DISP_CC_MDSS_PCLK1_CLK					29
+#define DISP_CC_MDSS_PCLK1_CLK_SRC				30
+#define DISP_CC_MDSS_ROT_CLK					31
+#define DISP_CC_MDSS_ROT_CLK_SRC				32
+#define DISP_CC_MDSS_RSCC_AHB_CLK				33
+#define DISP_CC_MDSS_RSCC_VSYNC_CLK				34
+#define DISP_CC_MDSS_VSYNC_CLK					35
+#define DISP_CC_MDSS_VSYNC_CLK_SRC				36
+#define DISP_CC_MDSS_AHB_CLK					37
+#define DISP_CC_XO_CLK						38
+#define DISP_CC_XO_CLK_SRC					39
+
+#endif
---
+++ include/dt-bindings/clock/qcom,videocc-lito.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Copyright (c) 2018, The Linux Foundation. All rights reserved. */
+
+#ifndef _DT_BINDINGS_CLK_QCOM_VIDEO_CC_LITO_H
+#define _DT_BINDINGS_CLK_QCOM_VIDEO_CC_LITO_H
+
+#define VIDEO_PLL0						0
+#define VIDEO_CC_IRIS_CLK_SRC					1
+#define VIDEO_CC_MVS0_AXI_CLK					2
+#define VIDEO_CC_MVS0_CORE_CLK					3
+#define VIDEO_CC_MVS1_AXI_CLK					4
+#define VIDEO_CC_MVS1_CORE_CLK					5
+#define VIDEO_CC_MVSC_CORE_CLK					6
+#define VIDEO_CC_MVSC_CTL_AXI_CLK				7
+#define VIDEO_CC_VENUS_AHB_CLK					8
+#define VIDEO_CC_XO_CLK						9
+#define VIDEO_CC_XO_CLK_SRC					10
+#define VIDEO_CC_APB_CLK					11
+
+#define VIDEO_CC_INTERFACE_BCR					0
+#define VIDEO_CC_MVS0_BCR					1
+#define VIDEO_CC_MVS1_BCR					2
+#define VIDEO_CC_MVSC_BCR					3
+
+#endif
