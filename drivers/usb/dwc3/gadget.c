14e5222a3ecc8a4e60f6c2c0deb06e7c912b2cb5
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2018-11-09 12:50:30 +0530

    USB: dwc3: gadget.c: Use dev_err_ratelimit in ep_queue()
    If there is a chance that function disable() gets delayed, in that
    case, different function driver like f_fs.c driver endup queueing
    request on OUT endpoint during disconnect. This results in ep_queue()
    fails with -ESHUTDOWN and prints error message. Due to that, adb daemon
    keeps on closing and reopening ep files and tries to queue request again.
    This error print floods kernel log and results in watchdog timeout panic.
    Fix it by ratelimiting error message to avoid flooding of kernel log.
    
    Change-Id: I56b47960801df3b13902e93ba290120586157a97
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1493,7 +1493,7 @@ static int __dwc3_gadget_ep_queue(struct dwc3_ep *dep, struct dwc3_request *req)
 	int			ret = 0;
 
 	if (!dep->endpoint.desc || !dwc->pullups_connected) {
-		dev_err(dwc->dev, "%s: can't queue to disabled endpoint\n",
+		dev_err_ratelimited(dwc->dev, "%s: can't queue to disabled endpoint\n",
 				dep->name);
 		return -ESHUTDOWN;
 	}
