2e3fecead8f0c4ea3d6cd534b6a880a0491e43ff
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2018-11-16 17:20:56 -0800

    sched: Add sched_enq_deq_task trace point
    To debug issues related to task scheduling sched_enq_deq_task
    trace point is very useful.
    
    Change-Id: I668702701d5443a4f5b0356634622f071297eb28
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -735,6 +735,7 @@ static inline void enqueue_task(struct rq *rq, struct task_struct *p, int flags)
 		sched_info_queued(rq, p);
 
 	p->sched_class->enqueue_task(rq, p, flags);
+	trace_sched_enq_deq_task(p, 1, cpumask_bits(&p->cpus_allowed)[0]);
 }
 
 static inline void dequeue_task(struct rq *rq, struct task_struct *p, int flags)
@@ -746,6 +747,7 @@ static inline void dequeue_task(struct rq *rq, struct task_struct *p, int flags)
 		sched_info_dequeued(rq, p);
 
 	p->sched_class->dequeue_task(rq, p, flags);
+	trace_sched_enq_deq_task(p, 0, cpumask_bits(&p->cpus_allowed)[0]);
 }
 
 void activate_task(struct rq *rq, struct task_struct *p, int flags)
