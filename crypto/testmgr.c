5fad54249fc2bcadf636eff7152462ddf2c69a7c
Eric Biggers <ebiggers@google.com>
2018-11-16 17:26:20 -0800

    BACKPORT, FROMGIT: crypto: chacha20-generic - add XChaCha20 support
    Add support for the XChaCha20 stream cipher.  XChaCha20 is the
    application of the XSalsa20 construction
    (https://cr.yp.to/snuffle/xsalsa-20081128.pdf) to ChaCha20 rather than
    to Salsa20.  XChaCha20 extends ChaCha20's nonce length from 64 bits (or
    96 bits, depending on convention) to 192 bits, while provably retaining
    ChaCha20's security.  XChaCha20 uses the ChaCha20 permutation to map the
    key and first 128 nonce bits to a 256-bit subkey.  Then, it does the
    ChaCha20 stream cipher with the subkey and remaining 64 bits of nonce.
    
    We need XChaCha support in order to add support for the Adiantum
    encryption mode.  Note that to meet our performance requirements, we
    actually plan to primarily use the variant XChaCha12.  But we believe
    it's wise to first add XChaCha20 as a baseline with a higher security
    margin, in case there are any situations where it can be used.
    Supporting both variants is straightforward.
    
    Since XChaCha20's subkey differs for each request, XChaCha20 can't be a
    template that wraps ChaCha20; that would require re-keying the
    underlying ChaCha20 for every request, which wouldn't be thread-safe.
    Instead, we make XChaCha20 its own top-level algorithm which calls the
    ChaCha20 streaming implementation internally.
    
    Similar to the existing ChaCha20 implementation, we define the IV to be
    the nonce and stream position concatenated together.  This allows users
    to seek to any position in the stream.
    
    I considered splitting the code into separate chacha20-common, chacha20,
    and xchacha20 modules, so that chacha20 and xchacha20 could be
    enabled/disabled independently.  However, since nearly all the code is
    shared anyway, I ultimately decided there would have been little benefit
    to the added complexity of separate modules.
    
    Reviewed-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Acked-by: Martin Willi <martin@strongswan.org>
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    
    (cherry picked from commit de61d7ae5d3789dcba3749a418f76613fbee8414
     https://git.kernel.org/pub/scm/linux/kernel/git/herbert/cryptodev-2.6.git master)
    (adjusted test vector formatting for old testmgr)
    Bug: 112008522
    Test: As series, see Ic61c13b53facfd2173065be715a7ee5f3af8760b
    Change-Id: I5c878e1d6577abda11d7b737cbb650baf16b6886
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -3548,6 +3548,15 @@ static const struct alg_test_desc alg_test_descs[] = {
 			.hash = __VECS(aes_xcbc128_tv_template)
 		}
 	}, {
+		.alg = "xchacha20",
+		.test = alg_test_skcipher,
+		.suite = {
+			.cipher = {
+				.enc = __VECS(xchacha20_tv_template),
+				.dec = __VECS(xchacha20_tv_template)
+			}
+		},
+	}, {
 		.alg = "xts(aes)",
 		.test = alg_test_skcipher,
 		.fips_allowed = 1,
