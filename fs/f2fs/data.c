1bfbe019b4f1660e1e47c77db60d5e57bcbe580f
Jia Zhu <zhujia13@huawei.com>
2018-11-20 04:29:35 +0800

    f2fs: fix m_may_create to make OPU DIO write correctly
    Previously, we added a parameter @map.m_may_create to trigger OPU
    allocation and call f2fs_balance_fs() correctly.
    
    But in get_more_blocks(), @create has been overwritten by below code.
    So the function f2fs_map_blocks() will not allocate new block address
    but directly go out. Meanwile,there are several functions calling
    f2fs_map_blocks() directly and @map.m_may_create not initialized.
    CODE:
    create = dio->op == REQ_OP_WRITE;
    	if (dio->flags & DIO_SKIP_HOLES) {
    		if (fs_startblk <= ((i_size_read(dio->inode) - 1) >>
    						i_blkbits))
    			create = 0;
    	}
    
    This patch fixes it.
    
    Signed-off-by: Jia Zhu <zhujia13@huawei.com>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -1032,6 +1032,10 @@ int f2fs_map_blocks(struct inode *inode, struct f2fs_map_blocks *map,
 	end = pgofs + maxblocks;
 
 	if (!create && f2fs_lookup_extent_cache(inode, pgofs, &ei)) {
+		if (test_opt(sbi, LFS) && flag == F2FS_GET_BLOCK_DIO &&
+							map->m_may_create)
+			goto next_dnode;
+
 		map->m_pblk = ei.blk + pgofs - ei.fofs;
 		map->m_len = min((pgoff_t)maxblocks, ei.fofs + ei.len - pgofs);
 		map->m_flags = F2FS_MAP_MAPPED;
@@ -1243,6 +1247,7 @@ bool f2fs_overwrite_io(struct inode *inode, loff_t pos, size_t len)
 	map.m_next_pgofs = NULL;
 	map.m_next_extent = NULL;
 	map.m_seg_type = NO_CHECK_TYPE;
+	map.m_may_create = false;
 	last_lblk = F2FS_BLK_ALIGN(pos + len);
 
 	while (map.m_lblk < last_lblk) {
