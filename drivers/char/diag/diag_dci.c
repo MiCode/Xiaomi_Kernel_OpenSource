0e419250923c7d38c30da9d214d460c2caa705e1
Hardik Arya <harya@codeaurora.org>
2018-11-19 02:38:45 -0800

    Revert "diag: Set buffer to NULL after freeing from mempool"
    This reverts commit c06a3b91fc25e62828c7cf9ae1bcdcb11c3c5d33.
    
    Change-Id: Ie49bb6d0ce77a73b43b1267884f25262f4e493e0
    Signed-off-by: Hardik Arya <harya@codeaurora.org>

--- drivers/char/diag/diag_dci.c
+++ drivers/char/diag/diag_dci.c
@@ -3122,6 +3122,7 @@ int diag_dci_deinit_client(struct diag_dci_client_tbl *entry)
 		if (buf_entry->buf_type == DCI_BUF_SECONDARY) {
 			mutex_lock(&buf_entry->data_mutex);
 			diagmem_free(driver, buf_entry->data, POOL_TYPE_DCI);
+			buf_entry->data = NULL;
 			mutex_unlock(&buf_entry->data_mutex);
 			kfree(buf_entry);
 			buf_entry = NULL;
@@ -3147,6 +3148,7 @@ int diag_dci_deinit_client(struct diag_dci_client_tbl *entry)
 		if (buf_entry && buf_entry->buf_type == DCI_BUF_SECONDARY) {
 			mutex_lock(&buf_entry->data_mutex);
 			diagmem_free(driver, buf_entry->data, POOL_TYPE_DCI);
+			buf_entry->data = NULL;
 			mutex_unlock(&buf_entry->data_mutex);
 			mutex_destroy(&buf_entry->data_mutex);
 			kfree(buf_entry);
