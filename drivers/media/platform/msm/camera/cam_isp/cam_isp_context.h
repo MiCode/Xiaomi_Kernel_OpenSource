ddf00b1b689d451667ed39a06eb3254ebc29e4f8
Karthik Anantha Ram <kartanan@codeaurora.org>
2018-11-26 15:31:16 -0800

    msm: camera: isp: Avoid applying while processing bubble recovery
    If we encounter a bubble and find recovery to be enabled this change
    blocks applying any new requests for that given ctx until all the
    buf done's occur for the bubbled request.
    
    Change-Id: I59b570e0ba87628d60411ad9dbc22378744f7231
    Signed-off-by: Karthik Anantha Ram <kartanan@codeaurora.org>

--- drivers/media/platform/msm/camera/cam_isp/cam_isp_context.h
+++ drivers/media/platform/msm/camera/cam_isp/cam_isp_context.h
@@ -144,6 +144,8 @@ struct cam_isp_context_state_monitor {
  * @base:                      Common context object pointer
  * @frame_id:                  Frame id tracking for the isp context
  * @substate_actiavted:        Current substate for the activated state.
+ * @process_bubble:            Atomic variable to check if ctx is still
+ *                             processing bubble.
  * @substate_machine:          ISP substate machine for external interface
  * @substate_machine_irq:      ISP substate machine for irq handling
  * @req_base:                  Common request object storage
@@ -170,6 +172,7 @@ struct cam_isp_context {
 
 	int64_t                          frame_id;
 	uint32_t                         substate_activated;
+	atomic_t                         process_bubble;
 	struct cam_ctx_ops              *substate_machine;
 	struct cam_isp_ctx_irq_ops      *substate_machine_irq;
 
