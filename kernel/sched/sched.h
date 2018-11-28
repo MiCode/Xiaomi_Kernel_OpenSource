24f209132d2e1a1e40e64063ef8ad8194ae54048
Pavankumar Kondeti <pkondeti@codeaurora.org>
2018-11-28 11:57:29 +0530

    sched/walt: Fix stale max_capacity issue during CPU hotplug
    Scheduler keeps track of the maximum capacity among all online CPUs
    in max_capacity. This is useful in checking if a given cluster/CPU
    is a max capacity CPU or not. The capacity of a CPU gets updated
    when its max frequency is limited by cpufreq and/or thermal. The
    CPUfreq limits notifications are received via CPUfreq policy
    notifier. However CPUfreq keeps the policy intact even when all
    of the CPUs governed by the policy are hotplugged out. So the
    CPUFREQ_REMOVE_POLICY notification never arrives and scheduler's
    notion of max_capacity becomes stale. The max_capacity may get
    corrected at some point later when CPUFREQ_NOTIFY notification
    comes for other online CPUs. But when the hotplugged CPUs comes
    online the max_capacity does not reflect since CPUFREQ_ADD_POLICY
    is not sent by the cpufreq.
    
    For example consider a system with 4 BIG and 4 little CPUs. Their
    original capacities are 2048 and 1024 respectively. The max_capacity
    points to 2048 when all CPUs are online. Now,
    
    1. All 4 BIG CPUs are hotplugged out. Since there is no notification,
    the max_capacity still points to 2048, which is incorrect.
    2. User clips the little CPUs's max_freq by 50%. CPUFREQ_NOTIFY arrives
    and max_capacity is updated by iterating all the online CPUs. At this
    point max_capacity becomes 512 which is correct.
    3. User removes the above limits of little CPUs. The max_capacity
    becomes 1024 which is correct.
    4. Now, BIG CPUs are hotplugged in. Since there is no notification,
    the max_capacity still points to 1024, which is incorrect.
    
    Fix this issue by wiring the max_capacity updates in WALT to scheduler
    hotplug callbacks. Ideally we want cpufreq domain hotplug callbacks
    but such notifiers are not present. So the max_capacity update is
    forced even when it is not necessary but that should not be a concern.
    Because CPU hotplug is supposed to be a rare event.
    
    The scheduler hotplug callbacks happen even before the hotplug CPU is
    removed from cpu_online_mask, so use cpu_active() check while evaluating
    the max_capacity. Since cpu_active_mask is a subset of cpu_online_mask,
    this is sufficient.
    
    Change-Id: I97b1974e2de1a9730285715858f1ada416d92a7a
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2665,7 +2665,10 @@ static inline void __update_min_max_capacity(void)
 	int i;
 	int max_cap = 0, min_cap = INT_MAX;
 
-	for_each_online_cpu(i) {
+	for_each_possible_cpu(i) {
+		if (!cpu_active(i))
+			continue;
+
 		max_cap = max(max_cap, cpu_capacity(i));
 		min_cap = min(min_cap, cpu_capacity(i));
 	}
@@ -2950,6 +2953,7 @@ static inline enum sched_boost_policy task_boost_policy(struct task_struct *p)
 }
 
 extern void walt_map_freq_to_load(void);
+extern void walt_update_min_max_capacity(void);
 
 static inline bool is_min_capacity_cluster(struct sched_cluster *cluster)
 {
@@ -3103,6 +3107,7 @@ static inline unsigned int power_cost(int cpu, u64 demand)
 
 static inline void note_task_waking(struct task_struct *p, u64 wallclock) { }
 static inline void walt_map_freq_to_load(void) { }
+static inline void walt_update_min_max_capacity(void) { }
 #endif	/* CONFIG_SCHED_WALT */
 
 static inline bool energy_aware(void)
