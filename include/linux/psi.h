978392049e485f5748a2066433fe29ea6e392e68
Johannes Weiner <hannes@cmpxchg.org>
2018-11-30 14:09:58 -0800

    UPSTREAM: psi: make disabling/enabling easier for vendor kernels
    Mel Gorman reports a hackbench regression with psi that would prohibit
    shipping the suse kernel with it default-enabled, but he'd still like
    users to be able to opt in at little to no cost to others.
    
    With the current combination of CONFIG_PSI and the psi_disabled bool set
    from the commandline, this is a challenge.  Do the following things to
    make it easier:
    
    1. Add a config option CONFIG_PSI_DEFAULT_DISABLED that allows distros
       to enable CONFIG_PSI in their kernel but leave the feature disabled
       unless a user requests it at boot-time.
    
       To avoid double negatives, rename psi_disabled= to psi=.
    
    2. Make psi_disabled a static branch to eliminate any branch costs
       when the feature is disabled.
    
    In terms of numbers before and after this patch, Mel says:
    
    : The following is a comparision using CONFIG_PSI=n as a baseline against
    : your patch and a vanilla kernel
    :
    :                          4.20.0-rc4             4.20.0-rc4             4.20.0-rc4
    :                 kconfigdisable-v1r1                vanilla        psidisable-v1r1
    : Amean     1       1.3100 (   0.00%)      1.3923 (  -6.28%)      1.3427 (  -2.49%)
    : Amean     3       3.8860 (   0.00%)      4.1230 *  -6.10%*      3.8860 (  -0.00%)
    : Amean     5       6.8847 (   0.00%)      8.0390 * -16.77%*      6.7727 (   1.63%)
    : Amean     7       9.9310 (   0.00%)     10.8367 *  -9.12%*      9.9910 (  -0.60%)
    : Amean     12     16.6577 (   0.00%)     18.2363 *  -9.48%*     17.1083 (  -2.71%)
    : Amean     18     26.5133 (   0.00%)     27.8833 *  -5.17%*     25.7663 (   2.82%)
    : Amean     24     34.3003 (   0.00%)     34.6830 (  -1.12%)     32.0450 (   6.58%)
    : Amean     30     40.0063 (   0.00%)     40.5800 (  -1.43%)     41.5087 (  -3.76%)
    : Amean     32     40.1407 (   0.00%)     41.2273 (  -2.71%)     39.9417 (   0.50%)
    :
    : It's showing that the vanilla kernel takes a hit (as the bisection
    : indicated it would) and that disabling PSI by default is reasonably
    : close in terms of performance for this particular workload on this
    : particular machine so;
    
    Link: http://lkml.kernel.org/r/20181127165329.GA29728@cmpxchg.org
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Tested-by: Mel Gorman <mgorman@techsingularity.net>
    Reported-by: Mel Gorman <mgorman@techsingularity.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    (cherry picked from commit e0c274472d5d27f277af722e017525e0b33784cd)
    
    Bug: 127712811
    Test: lmkd in PSI mode
    Change-Id: I6cb666fa351e8901df82e4d6931bfec0c5ce230d
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- include/linux/psi.h
+++ include/linux/psi.h
@@ -1,6 +1,7 @@
 #ifndef _LINUX_PSI_H
 #define _LINUX_PSI_H
 
+#include <linux/jump_label.h>
 #include <linux/psi_types.h>
 #include <linux/sched.h>
 #include <linux/cgroup-defs.h>
@@ -10,7 +11,7 @@ struct css_set;
 
 #ifdef CONFIG_PSI
 
-extern bool psi_disabled;
+extern struct static_key_false psi_disabled;
 
 void psi_init(void);
 
