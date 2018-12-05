fb4df7d8c107a37a183363af96dfdbf828284aa9
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2018-12-05 10:50:36 -0800

    sched: fair: remove nr_running=1 restriction for sync wakeups
    Currently, the sync wakeup is honored only if waker was the single
    task running on the cpu. More often, the waker is sharing the cpu
    along with other tasks and with this check the wakee is pushed
    to a different cpu. This causes regression in some performance
    sensitive usecases.
    
    Remove this change.
    
    CRs-Fixed: 2360960
    Change-Id: Idb961bd7333be891d3643da73473c3182710876b
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5786,8 +5786,7 @@ static inline bool
 bias_to_waker_cpu(struct task_struct *p, int cpu, struct cpumask *rtg_target)
 {
 	bool base_test = cpumask_test_cpu(cpu, &p->cpus_allowed) &&
-			cpu_active(cpu) && task_fits_max(p, cpu) &&
-			cpu_rq(cpu)->nr_running == 1;
+			cpu_active(cpu) && task_fits_max(p, cpu);
 	bool rtg_test = rtg_target && cpumask_test_cpu(cpu, rtg_target);
 
 	return base_test && (!rtg_target || rtg_test);
