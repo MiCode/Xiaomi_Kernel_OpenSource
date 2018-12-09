5c1e31b6fe929b18aa0048551d25e6aea8c8afdb
Chandan Gera <cgera@codeaurora.org>
2018-12-09 07:44:05 +0530

    msm: camera: Modify active count limit.
    Modify active count limit from 2 to 4
    to avoid congestion in use cases like SHDR
    
    Change-Id: I0bb68bdb91678551809f49e7e0b1b46038534cc8
    Signed-off-by: Chandan Gera <cgera@codeaurora.org>

--- drivers/media/platform/msm/camera/cam_isp/cam_isp_context.c
+++ drivers/media/platform/msm/camera/cam_isp/cam_isp_context.c
@@ -1219,7 +1219,7 @@ static int __cam_isp_ctx_apply_req_in_activated_state(
 		ctx_isp->substate_activated);
 	req_isp = (struct cam_isp_ctx_req *) req->req_priv;
 
-	if (ctx_isp->active_req_cnt >=  2) {
+	if (ctx_isp->active_req_cnt >=  4) {
 		CAM_ERR_RATE_LIMIT(CAM_ISP,
 			"Reject apply request (id %lld) due to congestion(cnt = %d)",
 			req->request_id,
