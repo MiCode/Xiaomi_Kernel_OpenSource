3fe48dc59c046e859b8177bdbe65f1c8a579542c
Ashok Vuyyuru <avuyyuru@codeaurora.org>
2018-12-12 23:38:54 +0530

    msm: ipa3: Fix to avoid setting endpoint delay in IPA 4.2/4.5 HW
    Setting endpoint delay on IPA 4.2/4.5 results in h/w stall.
    Add fix to set endpoint delay only when the workaround support
    is enabled for the h/w.
    
    Change-Id: Iff0f95c832a5f4033abbd6d7f9d2186731150d19
    Signed-off-by: Ashok Vuyyuru <avuyyuru@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipa_i.h
+++ drivers/platform/msm/ipa/ipa_v3/ipa_i.h
@@ -1610,6 +1610,7 @@ struct ipa3_context {
 	bool modem_cfg_emb_pipe_flt;
 	bool ipa_wdi2;
 	bool ipa_wdi2_over_gsi;
+	bool ipa_endp_delay_wa;
 	bool ipa_fltrt_not_hashable;
 	bool use_64_bit_dma_mask;
 	/* featurize if memory footprint becomes a concern */
@@ -1716,6 +1717,7 @@ struct ipa3_plat_drv_res {
 	bool use_ipa_pm;
 	struct ipa_pm_init_params pm_init;
 	bool wdi_over_pcie;
+	bool ipa_endp_delay_wa;
 };
 
 /**
@@ -1997,6 +1999,11 @@ void ipa3_register_lock_unlock_callback(int (*client_cb)(bool), u32 ipa_ep_idx);
 void ipa3_deregister_lock_unlock_callback(u32 ipa_ep_idx);
 int ipa3_set_reset_client_prod_pipe_delay(bool set_reset,
 		enum ipa_client_type client);
+int ipa3_start_stop_client_prod_gsi_chnl(enum ipa_client_type client,
+		bool start_chnl);
+void ipa3_client_prod_post_shutdown_cleanup(void);
+
+
 int ipa3_set_reset_client_cons_pipe_sus_holb(bool set_reset,
 		enum ipa_client_type client);
 
