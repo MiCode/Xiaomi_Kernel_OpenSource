36cb49c4d385fbe8c707e53aa38c3e7a4e0ba37d
Subhash Jadavani <subhashj@codeaurora.org>
2018-12-13 14:52:15 -0800

    scsi: ufs: add support for low voltage VCC support
    UFS3.0 devices are capable of running at lower level (2.5v) of VCC rail.
    If both host and device supports 2.5v level then we do runtime switch
    of UFS VCC voltage level.
    
    Change-Id: Ifa7f73f6afdfc382fa018c7403a2da79d08df845
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- drivers/scsi/ufs/ufshcd-pltfrm.c
+++ drivers/scsi/ufs/ufshcd-pltfrm.c
@@ -197,6 +197,9 @@ static int ufshcd_populate_vreg(struct device *dev, const char *name,
 				vreg->min_uV = be32_to_cpup(&prop[0]);
 				vreg->max_uV = be32_to_cpup(&prop[1]);
 			}
+
+			if (of_property_read_bool(np, "vcc-low-voltage-sup"))
+				vreg->low_voltage_sup = true;
 		}
 	} else if (!strcmp(name, "vccq")) {
 		vreg->min_uV = UFS_VREG_VCCQ_MIN_UV;
