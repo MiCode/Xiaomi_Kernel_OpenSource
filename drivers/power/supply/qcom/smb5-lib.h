5704f53c19b9e327823c8b04121250c4f06f2241
Umang Agrawal <uagrawal@codeaurora.org>
2018-12-13 15:27:34 +0530

    power: smb5: add support for die/connector temp based mitigation
    Add support to configure/enable hardware controlled mitigation
    based on DIE and connector temperature. H/W controlled scheme
    manipulates input current limit(ICL) of charger whenever die or
    connector temperature crosses configured threshold.
    
    Change-Id: Ib7655a8709960c79c554657a5d144c0ceccb73d3
    Signed-off-by: Ashay Jaiswal <ashayj@codeaurora.org>
    Signed-off-by: Umang Agrawal <uagrawal@codeaurora.org>

--- drivers/power/supply/qcom/smb5-lib.h
+++ drivers/power/supply/qcom/smb5-lib.h
@@ -470,6 +470,10 @@ struct smb_charger {
 	int			jeita_soft_fv[2];
 	bool			moisture_present;
 	bool			uusb_moisture_protection_enabled;
+	bool			hw_die_temp_mitigation;
+	bool			hw_connector_mitigation;
+	bool			hw_skin_temp_mitigation;
+	int			connector_pull_up;
 
 	/* workaround flag */
 	u32			wa_flags;
