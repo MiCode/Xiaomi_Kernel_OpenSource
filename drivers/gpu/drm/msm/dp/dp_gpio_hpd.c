c32c6bee3dc6d17184cc84cebe49f01fa3d2957f
vkakani <vkakani@codeaurora.org>
2018-12-19 14:30:45 +0530

    msm: arm64: remove return value related compiler bug fixes
    Resolve compiler warnings related to no return type or
    improper return value checks across drivers.
    
    Change-Id: I11a5d1ff363520a09fb26de918ced288563473ae
    Signed-off-by: vkakani <vkakani@codeaurora.org>

--- drivers/gpu/drm/msm/dp/dp_gpio_hpd.c
+++ drivers/gpu/drm/msm/dp/dp_gpio_hpd.c
@@ -129,11 +129,12 @@ static void dp_gpio_hpd_work(struct work_struct *work)
 	struct delayed_work *dw = to_delayed_work(work);
 	struct dp_gpio_hpd_private *gpio_hpd = container_of(dw,
 		struct dp_gpio_hpd_private, work);
+	int ret;
 
 	if (gpio_hpd->hpd) {
 		devm_free_irq(gpio_hpd->dev,
 			gpio_hpd->irq, gpio_hpd);
-		devm_request_threaded_irq(gpio_hpd->dev,
+		ret = devm_request_threaded_irq(gpio_hpd->dev,
 			gpio_hpd->irq, NULL,
 			dp_gpio_isr,
 			IRQF_TRIGGER_FALLING | IRQF_ONESHOT,
@@ -142,13 +143,16 @@ static void dp_gpio_hpd_work(struct work_struct *work)
 	} else {
 		devm_free_irq(gpio_hpd->dev,
 				gpio_hpd->irq, gpio_hpd);
-		devm_request_threaded_irq(gpio_hpd->dev,
+		ret = devm_request_threaded_irq(gpio_hpd->dev,
 			gpio_hpd->irq, NULL,
 			dp_gpio_isr,
 			IRQF_TRIGGER_RISING | IRQF_ONESHOT,
 			"dp-gpio-intp", gpio_hpd);
 		dp_gpio_hpd_connect(gpio_hpd, false);
 	}
+
+	if (ret < 0)
+		pr_err("Cannot claim IRQ dp-gpio-intp\n");
 }
 
 static int dp_gpio_hpd_simulate_connect(struct dp_hpd *dp_hpd, bool hpd)
