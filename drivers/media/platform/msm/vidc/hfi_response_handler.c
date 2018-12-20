194dd382b6dbc3a614ddd96c9606ef046f596346
Dikshita Agarwal <dikshita@codeaurora.org>
2018-12-20 13:56:48 +0530

    msm: vidc: ensure codec count is in supported session range
    Ensure the count of supported encoder and decoder returned
    from firmware are within the range of supported sessions.
    
    Change-Id: If3eae7bc82dc8302444e2e4104fb6ae3cfbfed5a
    Signed-off-by: Dikshita Agarwal <dikshita@codeaurora.org>

--- drivers/media/platform/msm/vidc/hfi_response_handler.c
+++ drivers/media/platform/msm/vidc/hfi_response_handler.c
@@ -753,6 +753,11 @@ static int hfi_fill_codec_info(u8 *data_ptr,
 				vidc_get_hal_codec((1 << i) & codecs);
 			capability->domain =
 				vidc_get_hal_domain(HFI_VIDEO_DOMAIN_DECODER);
+			if (codec_count == VIDC_MAX_DECODE_SESSIONS) {
+				dprintk(VIDC_ERR,
+					"Max supported decoder sessions reached");
+				break;
+			}
 		}
 	}
 	codecs = sys_init_done->enc_codec_supported;
@@ -764,6 +769,11 @@ static int hfi_fill_codec_info(u8 *data_ptr,
 				vidc_get_hal_codec((1 << i) & codecs);
 			capability->domain =
 				vidc_get_hal_domain(HFI_VIDEO_DOMAIN_ENCODER);
+			if (codec_count == VIDC_MAX_SESSIONS) {
+				dprintk(VIDC_ERR,
+					"Max supported sessions reached");
+				break;
+			}
 		}
 	}
 	sys_init_done->codec_count = codec_count;
