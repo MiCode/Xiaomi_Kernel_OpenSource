c3e2a7dfa8dbfecd7412ffb446628cf325ef9342
Dikshita Agarwal <dikshita@codeaurora.org>
2018-12-20 13:56:48 +0530

    msm: vidc: ensure codec count is in supported session range
    Ensure the count of supported encoder and decoder returned
    from firmware are within the range of supported sessions.
    
    Change-Id: If3eae7bc82dc8302444e2e4104fb6ae3cfbfed5a
    Signed-off-by: Dikshita Agarwal <dikshita@codeaurora.org>

--- drivers/media/platform/msm/vidc/vidc_hfi_api.h
+++ drivers/media/platform/msm/vidc/vidc_hfi_api.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2017,2019 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -66,6 +66,9 @@
 
 /* 16 encoder and 16 decoder sessions */
 #define VIDC_MAX_SESSIONS               32
+#define VIDC_MAX_DECODE_SESSIONS        16
+#define VIDC_MAX_ENCODE_SESSIONS        16
+
 
 enum vidc_status {
 	VIDC_ERR_NONE = 0x0,
