d858a547cc6c4b8530d50fb9852a4c1ade3418f0
Dilip Kota <dkota@codeaurora.org>
2018-12-24 17:03:40 +0530

    slimbus: Add snapshot of slimbus driver
    This snapshot is taken as of msm-4.14 'commit <1572d8323c96>
    (Merge "soc: qcom: use interruptible wait in cdsprm for rpmsg messages")'.
    
    Change-Id: I06c84ae568d88f98435fddbbb74a9db6dbb59109
    Signed-off-by: Dilip Kota <dkota@codeaurora.org>

--- include/linux/mod_devicetable.h
+++ include/linux/mod_devicetable.h
@@ -464,11 +464,8 @@ struct spi_device_id {
 #define SLIMBUS_MODULE_PREFIX	"slim:"
 
 struct slim_device_id {
-	__u16 manf_id, prod_code;
-	__u16 dev_index, instance;
-
-	/* Data private to the driver */
-	kernel_ulong_t driver_data;
+	char name[SLIMBUS_NAME_SIZE];
+	kernel_ulong_t driver_data;     /* Data private to the driver */
 };
 
 #define APR_NAME_SIZE	32
