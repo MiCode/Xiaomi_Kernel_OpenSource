a5d4d558b6dc9eeda48a02bf6c857cc60cafa5cd
Taniya Das <tdas@codeaurora.org>
2018-12-28 16:20:33 +0530

    clk: qcom: Add support for clock dependency
    Add clock ops for taking care of a dependent clock when the actual
    clock requests are made.
    
    Change-Id: I52691ef9f183da035ddf9c05fb73811f48fef74c
    Signed-off-by: Taniya Das <tdas@codeaurora.org>

--- drivers/clk/qcom/clk-regmap.h
+++ drivers/clk/qcom/clk-regmap.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0 */
-/* Copyright (c) 2014, 2017, The Linux Foundation. All rights reserved. */
+/* Copyright (c) 2014, 2017-2018, The Linux Foundation. All rights reserved. */
 
 #ifndef __QCOM_CLK_REGMAP_H__
 #define __QCOM_CLK_REGMAP_H__
@@ -12,6 +12,7 @@ struct regmap;
 /**
  * struct clk_regmap - regmap supporting clock
  * @hw:		handle between common and hardware-specific interfaces
+ * @dependent_hw: dependent clocks clock hw
  * @regmap:	regmap to use for regmap helpers and/or by providers
  * @enable_reg: register when using regmap enable/disable ops
  * @enable_mask: mask when using regmap enable/disable ops
@@ -20,6 +21,7 @@ struct regmap;
  */
 struct clk_regmap {
 	struct clk_hw hw;
+	struct clk_hw *dependent_hw;
 	struct regmap *regmap;
 	unsigned int enable_reg;
 	unsigned int enable_mask;
