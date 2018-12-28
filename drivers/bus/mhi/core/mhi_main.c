dd595272be74ddd14959665057cab0084a442a1f
Sujeev Dias <sdias@codeaurora.org>
2018-12-27 17:04:55 -0800

    mhi: core: log device EE and device state inside intvec handler
    For debugging purpose, always print the current execution
    environment and device state whenever intvec handler is triggered.
    
    CRs-Fixed: 2373857
    Change-Id: Idf961573dbcd12c0797f94095a828dd5c675c6ef
    Signed-off-by: Sujeev Dias <sdias@codeaurora.org>

--- drivers/bus/mhi/core/mhi_main.c
+++ drivers/bus/mhi/core/mhi_main.c
@@ -1464,6 +1464,8 @@ irqreturn_t mhi_intvec_threaded_handlr(int irq_number, void *dev)
 	if (MHI_REG_ACCESS_VALID(mhi_cntrl->pm_state)) {
 		state = mhi_get_mhi_state(mhi_cntrl);
 		ee = mhi_get_exec_env(mhi_cntrl);
+		MHI_LOG("device ee:%s dev_state:%s\n", TO_MHI_EXEC_STR(ee),
+			TO_MHI_STATE_STR(state));
 	}
 
 	if (state == MHI_STATE_SYS_ERR) {
