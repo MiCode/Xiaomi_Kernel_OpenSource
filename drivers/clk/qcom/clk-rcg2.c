b256b9c894cd2d092e4a3c017aa6596bbf2270d2
Taniya Das <tdas@codeaurora.org>
2018-12-28 13:06:27 +0530

    clk: qcom: rcg: Return failure for RCG update
    In case of update config failure, return -EBUSY.
    
    Change-Id: Id753728c5045130ae68908aece9ec500ce1bc72d
    Signed-off-by: Taniya Das <tdas@codeaurora.org>

--- drivers/clk/qcom/clk-rcg2.c
+++ drivers/clk/qcom/clk-rcg2.c
@@ -147,8 +147,7 @@ static int update_config(struct clk_rcg2 *rcg, u32 cfg)
 
 	WARN_CLK(hw->core, name, count == 0,
 			"rcg didn't update its configuration.");
-
-	return 0;
+	return -EBUSY;
 }
 
 static int clk_rcg2_set_parent(struct clk_hw *hw, u8 index)
