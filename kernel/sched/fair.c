7219d9a5ff4d8552b0ec234cabf4970117adc92e
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2019-01-07 12:34:07 -0800

    sched/fair: Add rcu_read_lock() call in update_runtime_enabled()
    Commit 7ebdf76d85e1050 ("sched: Add snapshot of Window Assisted Load
    Tracking (WALT)") removed rcu_read_lock() call from
    update_runtime_enabled() which would lead to issues, fix it by
    adding rcu_read_lock() call.
    
    Change-Id: I3853fa31009da596e23fd559c55bc5c261fd0e0d
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5127,6 +5127,8 @@ static void __maybe_unused update_runtime_enabled(struct rq *rq)
 	struct task_group *tg;
 
 	lockdep_assert_held(&rq->lock);
+
+	rcu_read_lock();
 	list_for_each_entry_rcu(tg, &task_groups, list) {
 		struct cfs_bandwidth *cfs_b = &tg->cfs_bandwidth;
 		struct cfs_rq *cfs_rq = tg->cfs_rq[cpu_of(rq)];
