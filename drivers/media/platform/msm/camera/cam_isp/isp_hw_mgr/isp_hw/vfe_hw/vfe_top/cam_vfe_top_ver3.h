f60c89bd605b2936bda16344b53f40d1ffaf33e4
Vishalsingh Hajeri <vhajeri@codeaurora.org>
2019-01-09 15:46:05 -0800

    msm: camera: vfe: Update VFE top common registers struct
    This change updates the VFE top common registers struct to
    include the new registers added in VFE version 480 hw.
    
    Change-Id: I37c277340ac0791adb2dc2c594b286d896a43d92
    Signed-off-by: Vishalsingh Hajeri <vhajeri@codeaurora.org>
    Signed-off-by: Venkat Chinta <vchinta@codeaurora.org>

---
+++ drivers/media/platform/msm/camera/cam_isp/isp_hw_mgr/isp_hw/vfe_hw/vfe_top/cam_vfe_top_ver3.h
@@ -0,0 +1,56 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2019, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef _CAM_VFE_TOP_VER3_H_
+#define _CAM_VFE_TOP_VER3_H_
+
+#include "cam_vfe_camif_ver2.h"
+#include "cam_vfe_camif_lite_ver2.h"
+#include "cam_vfe_rdi.h"
+
+#define CAM_VFE_TOP_VER3_MUX_MAX     6
+
+struct cam_vfe_top_ver3_reg_offset_common {
+	uint32_t hw_version;
+	uint32_t titan_version;
+	uint32_t hw_capability;
+	uint32_t lens_feature;
+	uint32_t stats_feature;
+	uint32_t color_feature;
+	uint32_t zoom_feature;
+	uint32_t global_reset_cmd;
+	uint32_t core_cgc_ovd_0;
+	uint32_t core_cgc_ovd_1;
+	uint32_t ahb_cgc_ovd;
+	uint32_t noc_cgc_ovd;
+	uint32_t bus_cgc_ovd;
+	uint32_t core_cfg_0;
+	uint32_t core_cfg_1;
+	uint32_t reg_update_cmd;
+	uint32_t trigger_cdm_events;
+	uint32_t violation_status;
+	uint32_t sbi_frame_idx;
+	uint32_t dsp_status;
+	uint32_t diag_config;
+	uint32_t diag_sensor_status_0;
+	uint32_t diag_sensor_status_1;
+};
+
+struct cam_vfe_top_ver3_hw_info {
+	struct cam_vfe_top_ver3_reg_offset_common  *common_reg;
+	struct cam_vfe_camif_ver2_hw_info           camif_hw_info;
+	struct cam_vfe_camif_lite_ver2_hw_info      camif_lite_hw_info;
+	struct cam_vfe_rdi_ver2_hw_info             rdi_hw_info;
+	uint32_t mux_type[CAM_VFE_TOP_VER3_MUX_MAX];
+};
+
+int cam_vfe_top_ver3_init(struct cam_hw_soc_info     *soc_info,
+	struct cam_hw_intf                           *hw_intf,
+	void                                         *top_hw_info,
+	struct cam_vfe_top                          **vfe_top);
+
+int cam_vfe_top_ver3_deinit(struct cam_vfe_top      **vfe_top);
+
+#endif /* _CAM_VFE_TOP_VER3_H_ */
