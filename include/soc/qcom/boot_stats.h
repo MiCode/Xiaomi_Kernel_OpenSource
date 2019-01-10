6e0b10d5f27b326214243644e6bd5796798d4e49
Venkata Rao Kakani <vkakani@codeaurora.org>
2019-01-10 11:46:21 +0530

    msm: boot markers: Add boot markers driver
    Add boot marker driver and make support for boot stats.
    
    Change-Id: Ie37cb7c5b44a391eb180499cf86ac50fcbbb544b
    Signed-off-by: Venkata Rao Kakani <vkakani@codeaurora.org>
    Signed-off-by: Sunil Paidimarri <hisunil@codeaurora.org>

--- include/soc/qcom/boot_stats.h
+++ include/soc/qcom/boot_stats.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2014,2019 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -11,7 +11,40 @@
  */
 
 #ifdef CONFIG_MSM_BOOT_STATS
+
+#define TIMER_KHZ 32768
+extern struct boot_stats __iomem *boot_stats;
+
+struct boot_stats {
+	uint32_t bootloader_start;
+	uint32_t bootloader_end;
+	uint32_t bootloader_display;
+	uint32_t bootloader_load_kernel;
+	uint32_t load_kernel_start;
+	uint32_t load_kernel_end;
+#ifdef CONFIG_MSM_BOOT_TIME_MARKER
+	uint32_t bootloader_early_domain_start;
+	uint32_t bootloader_checksum;
+#endif
+};
+
 int boot_stats_init(void);
+int boot_stats_exit(void);
+unsigned long long int msm_timer_get_sclk_ticks(void);
+phys_addr_t msm_timer_get_pa(void);
 #else
 static inline int boot_stats_init(void) { return 0; }
+static inline unsigned long long int msm_timer_get_sclk_ticks(void)
+{
+	return 0;
+}
+static inline phys_addr_t msm_timer_get_pa(void) { return 0; }
+#endif
+
+#ifdef CONFIG_MSM_BOOT_TIME_MARKER
+static inline int boot_marker_enabled(void) { return 1; }
+void place_marker(const char *name);
+#else
+static inline void place_marker(char *name) { };
+static inline int boot_marker_enabled(void) { return 0; }
 #endif
