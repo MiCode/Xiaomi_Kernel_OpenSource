53957c19263b773e718d632070db4ae2f779b95c
Maya Erez <merez@codeaurora.org>
2019-01-11 16:16:25 +0200

    wil6210: add support for pci linkdown recovery
    Some platforms can notify on pci linkdown events.
    Add pci linkdown recovery flow, invoked upon pci linkdown
    notification.
    A new wil6210 status flag is added, wil_status_pci_linkdown, to
    prevent pci suspend / resume until the recovery is completed.
    After the pci link is recovered in the platform pci recovery
    callback the device is being reset and the the interface and AP
    mode states are restored.
    
    Change-Id: I6e0f434d52704120c683c51fb4d664eccca16f01
    Signed-off-by: Maya Erez <merez@codeaurora.org>

--- drivers/net/wireless/ath/wil6210/wil6210.h
+++ drivers/net/wireless/ath/wil6210/wil6210.h
@@ -661,6 +661,7 @@ enum { /* for wil6210_priv.status */
 	wil_status_suspending, /* suspend in progress */
 	wil_status_suspended, /* suspend completed, device is suspended */
 	wil_status_resuming, /* resume in progress */
+	wil_status_pci_linkdown, /* pci linkdown occurred */
 	wil_status_last /* keep last */
 };
 
@@ -1074,6 +1075,8 @@ struct wil6210_priv {
 	u32 max_ampdu_size;
 
 	struct wil_fw_stats_global fw_stats_global;
+
+	struct work_struct pci_linkdown_recovery_worker;
 };
 
 #define wil_to_wiphy(i) (i->wiphy)
@@ -1364,6 +1367,9 @@ void wil_probe_client_worker(struct work_struct *work);
 void wil_disconnect_worker(struct work_struct *work);
 
 void wil_init_txrx_ops(struct wil6210_priv *wil);
+
+void wil_fw_recovery(struct wil6210_priv *wil);
+void wil_pci_linkdown_recovery_worker(struct work_struct *work);
 
 /* TX API */
 int wil_ring_init_tx(struct wil6210_vif *vif, int cid);
