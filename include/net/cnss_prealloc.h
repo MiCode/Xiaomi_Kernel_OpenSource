59b57424d56ff320db7195d6824d1e77e5ef9e19
Yuanyuan Liu <yuanliu@codeaurora.org>
2019-01-11 16:07:05 -0800

    net: cnss_prealloc: Add snapshot of cnss_prealloc driver
    This is a snapshot of the CNSS2 driver and associated files as of
    msm-4.14 commit fbe4101b0189
    ("cnss_prealloc: Update prealloc memory table").
    
    Checkpatch issues are also fixed on top of this commit.
    
    CRs-Fixed: 2375545
    Change-Id: Ie57101548bb016c92405fbf34f0e7bbcd938bc4a
    Signed-off-by: Yuanyuan Liu <yuanliu@codeaurora.org>

---
+++ include/net/cnss_prealloc.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Copyright (c) 2015-2016,2019 The Linux Foundation. All rights reserved. */
+
+#ifndef _NET_CNSS_PREALLOC_H_
+#define _NET_CNSS_PREALLOC_H_
+
+#define WCNSS_PRE_ALLOC_GET_THRESHOLD (4*1024)
+
+extern void *wcnss_prealloc_get(size_t size);
+extern int wcnss_prealloc_put(void *ptr);
+extern int wcnss_pre_alloc_reset(void);
+void wcnss_prealloc_check_memory_leak(void);
+
+#endif /* _NET_CNSS__PREALLOC_H_ */
