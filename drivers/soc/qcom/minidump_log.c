faf069a555c0494dacb3209b1d1906d836571518
Rishabh Bhatnagar <rishabhb@codeaurora.org>
2019-01-22 15:34:22 -0700

    soc: qcom: Add header to fix compile error
    Add <linux/vmalloc.h> to fix a compliation error.
    
    Change-Id: If6697fc2a9936520c22fca47729400000314a247
    Signed-off-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>

--- drivers/soc/qcom/minidump_log.c
+++ drivers/soc/qcom/minidump_log.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2017-2018, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2019, The Linux Foundation. All rights reserved.
  */
 
 #include <linux/module.h>
@@ -13,6 +13,7 @@
 #include <asm/sections.h>
 #include <linux/mm.h>
 #include <linux/sched/task.h>
+#include <linux/vmalloc.h>
 
 static void __init register_log_buf(void)
 {
