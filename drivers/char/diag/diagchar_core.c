e8a6dba236a1fc72ab70c784eeca6afeb1412fb0
Manoj Prabhu B <bmanoj@codeaurora.org>
2019-01-25 14:33:15 +0530

    diag: Return proper value in error case
    Presently for error cases return value is improperly updated.
    Proper handling is added in error cases to maintain proper return
    value.
    
    Change-Id: I771124b8a1eaa29a2eca246cd8c1457309745a85
    Signed-off-by: Manoj Prabhu B <bmanoj@codeaurora.org>

--- drivers/char/diag/diagchar_core.c
+++ drivers/char/diag/diagchar_core.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2008-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -204,6 +204,7 @@ do {								\
 	if ((count < ret+length) || (copy_to_user(buf,		\
 			(void *)&data, length))) {		\
 		ret = -EFAULT;					\
+		break;							\
 	}							\
 	ret += length;						\
 } while (0)
