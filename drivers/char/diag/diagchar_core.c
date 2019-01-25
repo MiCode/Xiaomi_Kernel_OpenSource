c501758b94334ab4b5ced94b85a0564fd7f70e71
Manoj Prabhu B <bmanoj@codeaurora.org>
2019-01-25 14:33:15 +0530

    diag: Return proper value in error case
    Presently for error cases return value is improperly updated.
    Proper handling is added in error cases to maintain proper return
    value.
    
    Change-Id: I771124b8a1eaa29a2eca246cd8c1457309745a85
    Signed-off-by: Manoj Prabhu B <bmanoj@codeaurora.org>

--- drivers/char/diag/diagchar_core.c
+++ drivers/char/diag/diagchar_core.c
@@ -183,6 +183,7 @@ do {								\
 	if ((count < ret+length) || (copy_to_user(buf,		\
 			(void *)&data, length))) {		\
 		ret = -EFAULT;					\
+		break;							\
 	}							\
 	ret += length;						\
 } while (0)
