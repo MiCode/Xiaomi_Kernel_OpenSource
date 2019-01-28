ee8a2b03e8b10e330c4e32b68bf8afda1af9d496
Jordan Crouse <jcrouse@codeaurora.org>
2019-01-28 12:39:26 -0700

    esoc: Fix a uninitalized variable warning
    Fix an uninitalized variable compiler warning from last_esoc_req_show().
    
    Fixes: f2e94fd87b12 ("esoc: Add snapshot of esoc driver")
    Change-Id: Ic0dedbad09da5c9d1ab5d2897ec16b27966f0ebb
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/esoc/esoc-mdm-dbg-eng.c
+++ drivers/esoc/esoc-mdm-dbg-eng.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2015, 2017-2018, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2015, 2017-2019, The Linux Foundation. All rights reserved.
  */
 #include <linux/atomic.h>
 #include <linux/device.h>
@@ -262,7 +262,7 @@ static ssize_t last_esoc_req_show(struct device_driver *drv, char *buf)
 {
 	unsigned int i;
 	unsigned long flags;
-	size_t count;
+	size_t count = 0;
 
 	spin_lock_irqsave(&req_lock, flags);
 	for (i = 0; i < ARRAY_SIZE(req_to_str); i++) {
