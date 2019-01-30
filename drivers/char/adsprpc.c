63685722c9d33142f9bb44360f5c7e03418069ce
Tharun Kumar Merugu <mtharu@codeaurora.org>
2019-01-30 11:15:10 +0530

    msm: ADSPRPC: Error handling during pd restart
    Bail during static pd creation if we failed to unmap memory
    during audiopd restart.
    
    Change-Id: Idcaf2f57c6a1e3ab3af7382c023a62bfeed1d95e
    Acked-by: Vishnu Karthik D <vikarthi@qti.qualcomm.com>
    Signed-off-by: Tharun Kumar Merugu <mtharu@codeaurora.org>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2018, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -2133,6 +2133,8 @@ static int fastrpc_init_process(struct fastrpc_file *fl,
 		if (!strcmp(proc_name, "audiopd")) {
 			fl->spdname = AUDIO_PDR_SERVICE_LOCATION_CLIENT_NAME;
 			VERIFY(err, !fastrpc_mmap_remove_pdr(fl));
+			if (err)
+				goto bail;
 		}
 
 		if (!me->staticpd_flags && !(me->legacy_remote_heap)) {
