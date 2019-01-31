c773d5e7058964f1ae85ef255b83930cea920e9b
VijayaKumar T M <vtmuni@codeaurora.org>
2019-01-31 13:47:22 +0530

    msm: camera: QM215 delay during power up sequence
    Add a delay of 60ms for QM215 target during power up
    sequence.
    
    Change-Id: I22cc588c2e6776e408c8749db55651423e4ba1fb
    Signed-off-by: VijayaKumar T M <vtmuni@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -205,6 +205,9 @@ int msm_sensor_power_up(struct msm_sensor_ctrl_t *s_ctrl)
 					&msm_sensor_secure_func_tbl;
 			}
 		}
+#if IS_ENABLED(CONFIG_ARCH_QM215)
+		msleep(60);
+#endif
 		rc = msm_camera_power_up(power_info, s_ctrl->sensor_device_type,
 			sensor_i2c_client);
 		if (rc < 0)
