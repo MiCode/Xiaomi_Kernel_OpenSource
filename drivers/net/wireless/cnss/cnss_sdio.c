98ab007519b651fe0a5fcb2874962dd300659178
Chaoli Zhou <zchaoli@codeaurora.org>
2019-01-31 15:25:14 +0800

    cnss: Use the tsf gpio pin to indicate if tsf enable or not
    Get the tsf gpio pin to check if tsf feature enable or not,
    if not enable, will not do relevant tsf operation, which can
    avoid to print some error message that not needed.
    
    Change-Id: Iab59d4c3c4105824b3fe185311b5afc3ab28d324
    Signed-off-by: Chaoli Zhou <zchaoli@codeaurora.org>

--- drivers/net/wireless/cnss/cnss_sdio.c
+++ drivers/net/wireless/cnss/cnss_sdio.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2015-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -80,6 +80,7 @@ struct cnss_sdio_info {
 	const struct sdio_device_id *id;
 	bool skip_wlan_en_toggle;
 	bool cnss_hw_state;
+	int tsf_gpio;
 	struct cnss_cap_tsf_info cap_tsf_info;
 };
 
@@ -814,6 +815,9 @@ static void cnss_sdio_tsf_deinit(struct cnss_cap_tsf_info *tsf_info)
 static void cnss_sdio_set_platform_ops(struct device *dev)
 {
 	struct cnss_dev_platform_ops *pf_ops = &cnss_pdata->platform_ops;
+	struct cnss_sdio_info *info;
+
+	info = &cnss_pdata->cnss_sdio_info;
 
 	pf_ops->power_up = cnss_sdio_power_up;
 	pf_ops->power_down = cnss_sdio_power_down;
@@ -824,10 +828,13 @@ static void cnss_sdio_set_platform_ops(struct device *dev)
 	pf_ops->set_wlan_mac_address = cnss_sdio_set_wlan_mac_address;
 	pf_ops->schedule_recovery_work = cnss_sdio_schedule_recovery_work;
 	pf_ops->request_bus_bandwidth = cnss_sdio_request_bus_bandwidth;
-	pf_ops->register_tsf_captured_handler =
-		cnss_sdio_register_tsf_captured_handler;
-	pf_ops->unregister_tsf_captured_handler =
-		cnss_sdio_unregister_tsf_captured_handler;
+
+	if (info->tsf_gpio >= 0) {
+		pf_ops->register_tsf_captured_handler =
+			cnss_sdio_register_tsf_captured_handler;
+		pf_ops->unregister_tsf_captured_handler =
+			cnss_sdio_unregister_tsf_captured_handler;
+	}
 	dev->platform_data = pf_ops;
 }
 
@@ -1466,7 +1473,10 @@ static int cnss_sdio_probe(struct platform_device *pdev)
 							  "qcom,skip-wlan-en-toggle");
 	info->cnss_hw_state = CNSS_HW_ACTIVE;
 
-	cnss_sdio_tsf_init(dev, &info->cap_tsf_info);
+	info->tsf_gpio = of_get_named_gpio(dev->of_node,
+					   WLAN_GPIO_CAPTSF_NAME, 0);
+	if (info->tsf_gpio >= 0)
+		cnss_sdio_tsf_init(dev, &info->cap_tsf_info);
 
 	error = cnss_sdio_wlan_init();
 	if (error) {
@@ -1519,15 +1529,18 @@ err_wlan_enable_regulator:
 static int cnss_sdio_remove(struct platform_device *pdev)
 {
 	struct cnss_sdio_info *info;
-	struct cnss_cap_tsf_info *tsf_info;
 
 	if (!cnss_pdata)
 		return -ENODEV;
 
 	info = &cnss_pdata->cnss_sdio_info;
-	tsf_info = &info->cap_tsf_info;
 
-	cnss_sdio_tsf_deinit(tsf_info);
+	if (info->tsf_gpio >= 0) {
+		struct cnss_cap_tsf_info *tsf_info;
+
+		tsf_info = &info->cap_tsf_info;
+		cnss_sdio_tsf_deinit(tsf_info);
+	}
 	cnss_sdio_deinit_bus_bandwidth();
 	cnss_sdio_wlan_exit();
 	cnss_subsys_exit();
