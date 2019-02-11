90cc782279ee23111559ef152cfe2637479d62ad
Maulik Shah <mkshah@codeaurora.org>
2019-02-11 12:28:36 +0530

    soc: qcom: rpmh_master_stat: Add island stats support
    Add support to display ADSP and SLPI Island mode master stats
    as part of rpmh_master_stats.
    
    Change-Id: I0976ca23ea53b03e360a26a53fc59d1961f96a22
    Signed-off-by: Maulik Shah <mkshah@codeaurora.org>

--- drivers/soc/qcom/rpmh_master_stat.c
+++ drivers/soc/qcom/rpmh_master_stat.c
@@ -38,6 +38,7 @@ enum master_smem_id {
 	SLPI,
 	GPU,
 	DISPLAY,
+	SLPI_ISLAND = 613,
 };
 
 enum master_pid {
@@ -67,8 +68,10 @@ struct msm_rpmh_master_data {
 static const struct msm_rpmh_master_data rpmh_masters[] = {
 	{"MPSS", MPSS, PID_MPSS},
 	{"ADSP", ADSP, PID_ADSP},
+	{"ADSP_ISLAND", SLPI_ISLAND, PID_ADSP},
 	{"CDSP", CDSP, PID_CDSP},
 	{"SLPI", SLPI, PID_SLPI},
+	{"SLPI_ISLAND", SLPI_ISLAND, PID_SLPI},
 	{"GPU", GPU, PID_GPU},
 	{"DISPLAY", DISPLAY, PID_DISPLAY},
 };
