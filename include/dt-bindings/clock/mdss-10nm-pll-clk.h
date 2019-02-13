e47d034a9801e57fd11389e2277351afcd10c535
Satya Rama Aditya Pinapala <psraditya30@codeaurora.org>
2019-02-12 16:51:34 -0800

    dt-bindings: msm: Adding include files required for mdss compiling
    This change propogates files from 4.14 to 4.19 to fix compilation
    for mdss pll.
    
    Change-Id: I253277bb3b99dca94f69a3194bfb680f31297f05
    Signed-off-by: Satya Rama Aditya Pinapala <psraditya30@codeaurora.org>

---
+++ include/dt-bindings/clock/mdss-10nm-pll-clk.h
@@ -0,0 +1,38 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2017-2019, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef __MDSS_10NM_PLL_CLK_H
+#define __MDSS_10NM_PLL_CLK_H
+
+/* DSI PLL clocks */
+#define VCO_CLK_0		0
+#define PLL_OUT_DIV_0_CLK	1
+#define BITCLK_SRC_0_CLK	2
+#define BYTECLK_SRC_0_CLK	3
+#define POST_BIT_DIV_0_CLK	4
+#define POST_VCO_DIV_0_CLK	5
+#define BYTECLK_MUX_0_CLK	6
+#define PCLK_SRC_MUX_0_CLK	7
+#define PCLK_SRC_0_CLK		8
+#define PCLK_MUX_0_CLK		9
+#define VCO_CLK_1		10
+#define PLL_OUT_DIV_1_CLK	11
+#define BITCLK_SRC_1_CLK	12
+#define BYTECLK_SRC_1_CLK	13
+#define POST_BIT_DIV_1_CLK	14
+#define POST_VCO_DIV_1_CLK	15
+#define BYTECLK_MUX_1_CLK	16
+#define PCLK_SRC_MUX_1_CLK	17
+#define PCLK_SRC_1_CLK		18
+#define PCLK_MUX_1_CLK		19
+
+/* DP PLL clocks */
+#define	DP_VCO_CLK	0
+#define	DP_LINK_CLK_DIVSEL_TEN	1
+#define	DP_VCO_DIVIDED_TWO_CLK_SRC	2
+#define	DP_VCO_DIVIDED_FOUR_CLK_SRC	3
+#define	DP_VCO_DIVIDED_SIX_CLK_SRC	4
+#define	DP_VCO_DIVIDED_CLK_SRC_MUX	5
+#endif
---
+++ include/dt-bindings/clock/mdss-28nm-pll-clk.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2016-2019, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef __MDSS_28NM_PLL_CLK_H
+#define __MDSS_28NM_PLL_CLK_H
+
+/* DSI PLL clocks */
+#define VCO_CLK_0		0
+#define ANALOG_POSTDIV_0_CLK	1
+#define INDIRECT_PATH_SRC_0_CLK	2
+#define BYTECLK_SRC_MUX_0_CLK	3
+#define BYTECLK_SRC_0_CLK	4
+#define PCLK_SRC_0_CLK		5
+#define VCO_CLK_1		6
+#define ANALOG_POSTDIV_1_CLK	7
+#define INDIRECT_PATH_SRC_1_CLK	8
+#define BYTECLK_SRC_MUX_1_CLK	9
+#define BYTECLK_SRC_1_CLK	10
+#define PCLK_SRC_1_CLK		11
+
+/* HDMI PLL clocks */
+#define HDMI_VCO_CLK			0
+#define HDMI_VCO_DIVIDED_1_CLK_SRC	1
+#define HDMI_VCO_DIVIDED_TWO_CLK_SRC	2
+#define HDMI_VCO_DIVIDED_FOUR_CLK_SRC	3
+#define HDMI_VCO_DIVIDED_SIX_CLK_SRC	4
+#define HDMI_PCLK_SRC_MUX		5
+#define HDMI_PCLK_SRC			6
+#endif
