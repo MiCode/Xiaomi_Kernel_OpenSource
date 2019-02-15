ab018a200c8d5e41f515b44a148933f0e30b2b30
Bojun Pan <bojunp@codeaurora.org>
2019-02-15 13:31:08 -0800

    msm: gsi: Avoid ring event ring door bell for empty ieob
    Avoid ring event ring door bell, if it is empty ieob.
    Empty ieob means, the software read pointer is the same
    as hardware, no event ring entry process then. In this case,
    no need to ring door bell.
    
    Change-Id: I7af7aa2bf0c8a6cda8c7083a1de3c2b80e8ae9b7
    Signed-off-by: Bojun Pan <bojunp@codeaurora.org>

--- drivers/platform/msm/gsi/gsi.c
+++ drivers/platform/msm/gsi/gsi.c
@@ -629,6 +629,7 @@ static void gsi_handle_ieob(int ee)
 	unsigned long flags;
 	unsigned long cntr;
 	uint32_t msk;
+	bool empty;
 
 	ch = gsi_readl(gsi_ctx->base +
 		GSI_EE_n_CNTXT_SRC_IEOB_IRQ_OFFS(ee));
@@ -660,6 +661,7 @@ static void gsi_handle_ieob(int ee)
 			spin_lock_irqsave(&ctx->ring.slock, flags);
 check_again:
 			cntr = 0;
+			empty = true;
 			rp = gsi_readl(gsi_ctx->base +
 				GSI_EE_n_EV_CH_k_CNTXT_4_OFFS(i, ee));
 			rp |= ctx->ring.rp & 0xFFFFFFFF00000000;
@@ -673,8 +675,10 @@ check_again:
 					break;
 				}
 				gsi_process_evt_re(ctx, &notify, true);
+				empty = false;
 			}
-			gsi_ring_evt_doorbell(ctx);
+			if (!empty)
+				gsi_ring_evt_doorbell(ctx);
 			if (cntr != 0)
 				goto check_again;
 			spin_unlock_irqrestore(&ctx->ring.slock, flags);
