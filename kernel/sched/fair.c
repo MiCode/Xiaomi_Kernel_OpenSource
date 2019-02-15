06bad7edf503bebc171af8e77d1005e150a00f9b
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-02-15 15:57:40 +0530

    sched: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: I6cb5de5a2da4bac8651a35ab9e162f290f07a51b
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7273,8 +7273,9 @@ static inline bool task_fits_max(struct task_struct *p, int cpu)
 	if (capacity == max_capacity)
 		return true;
 
-	if (task_boost_policy(p) == SCHED_BOOST_ON_BIG
-			&& is_min_capacity_cpu(cpu))
+	if ((task_boost_policy(p) == SCHED_BOOST_ON_BIG ||
+			schedtune_task_boost(p) > 0) &&
+			is_min_capacity_cpu(cpu))
 		return false;
 
 	return task_fits_capacity(p, capacity, cpu);
