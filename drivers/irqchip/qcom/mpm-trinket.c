aa6f3bbf84cf36814a3c0e4992aa93189572b8ca
Raghavendra Kakarla <rkakarla@codeaurora.org>
2019-02-19 11:00:03 +0530

    irqchip: mpm: Add the correct mpm to gic interrupt mapping for usb
    Add the correct mpm to gic interrupt mapping for the usb wakeup capable
    interrupt for the trinket.
    
    Change-Id: I3dc7d58b03d8689fe2892ee1322816db6809dc2f
    Signed-off-by: Raghavendra Kakarla <rkakarla@codeaurora.org>

--- drivers/irqchip/qcom/mpm-trinket.c
+++ drivers/irqchip/qcom/mpm-trinket.c
@@ -17,8 +17,8 @@ const struct mpm_pin mpm_trinket_gic_chip_data[] = {
 	{2, 222},
 	{12, 454}, /* b3_lfps_rxterm_irq */
 	{86, 215}, /* mpm_wake,spmi_m */
-	{90, 286}, /* eud_p0_dpse_int_mx */
-	{91, 286}, /* eud_p0_dmse_int_mx */
+	{90, 292}, /* eud_p0_dpse_int_mx */
+	{91, 292}, /* eud_p0_dmse_int_mx */
 	{-1},
 };
 
