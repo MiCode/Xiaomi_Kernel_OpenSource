d8c314971bc7b0fbc77a964c3b37e0d60aa92ccc
Tatenda Chipeperekwa <tatendac@codeaurora.org>
2019-02-22 14:15:05 -0800

    drm/msm: select external display module on kona
    Select the external display module since it is needed for basic
    functionality by submodules such as the DisplayPort driver.
    
    Change-Id: Id86e2945a1236a329f2bc04199a07595fe8a289c
    Signed-off-by: Tatenda Chipeperekwa <tatendac@codeaurora.org>

--- drivers/platform/msm/msm_ext_display.c
+++ drivers/platform/msm/msm_ext_display.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2016-2018, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2016-2019, The Linux Foundation. All rights reserved.
  */
 
 #define pr_fmt(fmt)	"%s: " fmt, __func__
@@ -14,6 +14,7 @@
 #include <linux/types.h>
 #include <linux/of_platform.h>
 #include <linux/msm_ext_display.h>
+#include <linux/extcon-provider.h>
 
 struct msm_ext_disp_list {
 	struct msm_ext_disp_init_data *data;
