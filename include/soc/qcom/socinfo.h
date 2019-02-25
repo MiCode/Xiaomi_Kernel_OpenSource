4f9407dbb71e4eb28b980d89eed1cab721d6d5d8
Prateek Sood <prsood@codeaurora.org>
2019-02-25 14:53:30 +0530

    soc: qcom: Add support for sm6150p into socinfo driver
    Add sm6150p SOC information in socinfo driver.
    
    Change-Id: Id863c2d2806e837141f62ce7336894e472e27edd
    Signed-off-by: Prateek Sood <prsood@codeaurora.org>

--- include/soc/qcom/socinfo.h
+++ include/soc/qcom/socinfo.h
@@ -69,6 +69,8 @@
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,sdmshrike")
 #define early_machine_is_sm6150()	\
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,sm6150")
+#define early_machine_is_sm6150p()	\
+	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,sm6150p")
 #define early_machine_is_qcs405()	\
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,qcs405")
 #define early_machine_is_qcs403()	\
@@ -113,6 +115,7 @@
 #define early_machine_is_sa8155p()	0
 #define early_machine_is_sdmshrike()	0
 #define early_machine_is_sm6150()	0
+#define early_machine_is_sm6150p()	0
 #define early_machine_is_qcs405()	0
 #define early_machine_is_qcs403()	0
 #define early_machine_is_qcs401()	0
@@ -149,6 +152,7 @@ enum msm_cpu {
 	MSM_CPU_SA8155P,
 	MSM_CPU_SDMSHRIKE,
 	MSM_CPU_SM6150,
+	MSM_CPU_SM6150P,
 	MSM_CPU_QCS405,
 	MSM_CPU_QCS403,
 	MSM_CPU_QCS401,
