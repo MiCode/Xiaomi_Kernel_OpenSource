a202f084227a95847a1f492bfe86737e6e37f4fb
Luhua Xu <luhua.xu@mediatek.com>
2019-02-28 21:06:34 +0800

    [ALPS04030966] Sensor: new kernel porting
    add sensor kernel drv support
    1.delete typedef
    2. use __func__ instead of function name in log
    
    MTK-Commit-Id: 48ab6857cdf543ace6d73a9c9398d3e62b36bdf6
    
    Change-Id: I5400d3cb83172d99e56a700f8eb7ebfd7565b67b
    Signed-off-by: Luhua Xu <luhua.xu@mediatek.com>
    CR-Id: ALPS04030966
    Feature: Sensor Hub
    (cherry-pick from 71f537cd5e398ba7a9c382db511b0436cd3efadb)

---
+++ drivers/misc/mediatek/sensors-1.0/activity_sensor/activity.h
@@ -0,0 +1,139 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+
+#ifndef __ACTIVITY_H__
+#define __ACTIVITY_H__
+
+
+//#include <linux/pm_wakeup.h>
+#include <linux/interrupt.h>
+#include <linux/miscdevice.h>
+#include <linux/platform_device.h>
+#include <linux/input.h>
+#include <linux/workqueue.h>
+#include <linux/slab.h>
+#include <linux/module.h>
+#include <linux/poll.h>
+#include <linux/i2c.h>
+#include <linux/irq.h>
+#include <linux/uaccess.h>
+#include <linux/delay.h>
+#include <linux/kobject.h>
+#include <linux/atomic.h>
+#include <linux/ioctl.h>
+
+#include <sensors_io.h>
+#include <hwmsen_helper.h>
+#include <hwmsensor.h>
+#include "sensor_attr.h"
+#include "sensor_event.h"
+
+
+#define OP_ACT_DELAY	0X01
+#define	OP_ACT_ENABLE	0X02
+#define	OP_ACT_GET_DATA	0X04
+
+#define ACT_INVALID_VALUE -1
+
+#define EVENT_TYPE_ACT_IN_VEHICLE		ABS_X
+#define EVENT_TYPE_ACT_ON_BICYCLE		ABS_Y
+#define EVENT_TYPE_ACT_ON_FOOT			ABS_Z
+#define EVENT_TYPE_ACT_STILL			ABS_RX
+#define EVENT_TYPE_ACT_UNKNOWN			ABS_RY
+#define EVENT_TYPE_ACT_TILTING			ABS_RZ
+#define EVENT_TYPE_ACT_WALKING			ABS_HAT0X
+#define EVENT_TYPE_ACT_STANDING			ABS_HAT0Y
+#define EVENT_TYPE_ACT_LYING			ABS_HAT1X
+#define EVENT_TYPE_ACT_RUNNING			ABS_HAT1Y
+#define EVENT_TYPE_ACT_CLIMBING			ABS_HAT2X
+#define EVENT_TYPE_ACT_SITTING			ABS_HAT2Y
+#define EVENT_TYPE_ACT_STATUS		        ABS_WHEEL
+#define EVENT_TYPE_ACT_TIMESTAMP_HI		REL_HWHEEL
+#define EVENT_TYPE_ACT_TIMESTAMP_LO		REL_DIAL
+
+#define ACT_VALUE_MAX (32767)
+#define ACT_VALUE_MIN (-32768)
+#define ACT_STATUS_MIN (0)
+#define ACT_STATUS_MAX (64)
+#define ACT_DIV_MAX (32767)
+#define ACT_DIV_MIN (1)
+
+#define MAX_CHOOSE_ACT_NUM 5
+
+struct act_control_path {
+	int (*open_report_data)(int open);	/* open data rerport to HAL */
+	int (*enable_nodata)(int en);
+	int (*set_delay)(u64 delay);
+	int (*batch)(int flag, int64_t samplingPeriodNs,
+		int64_t maxBatchReportLatencyNs);
+	int (*flush)(void);/* open data rerport to HAL */
+	bool is_report_input_direct;
+	bool is_support_batch;
+};
+struct act_data_path {
+	int (*get_data)(struct hwm_sensor_data *value, int *status);
+	int vender_div;
+};
+
+struct act_init_info {
+	char *name;
+	int (*init)(void);
+	int (*uninit)(void);
+	struct platform_driver *platform_diver_addr;
+};
+
+struct act_drv_obj {
+	void *self;
+	int polling;
+	int (*act_operate)(void *self, uint32_t command, void *buff_in,
+		int size_in, void *buff_out, int size_out, int *actualout);
+};
+
+struct act_context {
+	struct input_dev *idev;
+	struct sensor_attr_t mdev;
+	struct work_struct report;
+	struct mutex act_op_mutex;
+	atomic_t delay;		/*polling period for reporting input event */
+	atomic_t wake;        /*user-space request to wake-up, used with stop */
+	struct timer_list timer;	/* polling timer */
+	atomic_t trace;
+
+	atomic_t early_suspend;
+	struct hwm_sensor_data drv_data;
+	struct act_control_path act_ctl;
+	struct act_data_path act_data;
+	bool is_active_nodata;
+	bool is_active_data;	/* Active and HAL need data . */
+	bool is_first_data_after_enable;
+	bool is_polling_run;
+	bool is_batch_enable;
+	int power;
+	int enable;
+	int64_t delay_ns;
+	int64_t latency_ns;
+};
+
+/* driver API for internal */
+/* extern int act_enable_nodata(int enable); */
+/* extern int act_attach(struct act_drv_obj *obj); */
+/* driver API for third party vendor */
+
+/* for auto detect */
+extern int act_driver_add(struct act_init_info *obj);
+extern int act_data_report(struct hwm_sensor_data *data, int status);
+extern int act_flush_report(void);
+extern int act_register_control_path(struct act_control_path *ctl);
+extern int act_register_data_path(struct act_data_path *data);
+#endif
---
+++ drivers/misc/mediatek/sensors-1.0/situation/sar/sarhub.c
@@ -0,0 +1,136 @@
+/* sarhub motion sensor driver
+ *
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#define pr_fmt(fmt) "[sarhub] " fmt
+
+#include <hwmsensor.h>
+#include "sarhub.h"
+#include <situation.h>
+#include <SCP_sensorHub.h>
+#include <linux/notifier.h>
+#include "scp_helper.h"
+
+static struct situation_init_info sarhub_init_info;
+
+static int sar_get_data(int *probability, int *status)
+{
+	int err = 0;
+	struct data_unit_t data;
+	uint64_t time_stamp = 0;
+
+	err = sensor_get_data_from_hub(ID_SAR, &data);
+	if (err < 0) {
+		pr_err_ratelimited("sensor_get_data_from_hub fail!!\n");
+		return -1;
+	}
+	time_stamp		= data.time_stamp;
+	*probability	= data.sar_event.state;
+	return 0;
+}
+static int sar_open_report_data(int open)
+{
+	int ret = 0;
+#if defined CONFIG_MTK_SCP_SENSORHUB_V1
+	if (open == 1)
+		ret = sensor_set_delay_to_hub(ID_SAR, 120);
+#elif defined CONFIG_NANOHUB
+
+#else
+
+#endif
+	ret = sensor_enable_to_hub(ID_SAR, open);
+	return ret;
+}
+static int sar_batch(int flag,
+	int64_t samplingPeriodNs, int64_t maxBatchReportLatencyNs)
+{
+	return sensor_batch_to_hub(ID_SAR,
+		flag, samplingPeriodNs, maxBatchReportLatencyNs);
+}
+
+static int sar_flush(void)
+{
+	return sensor_flush_to_hub(ID_SAR);
+}
+
+static int sar_recv_data(struct data_unit_t *event, void *reserved)
+{
+	if (event->flush_action == FLUSH_ACTION)
+		situation_flush_report(ID_SAR);
+	else if (event->flush_action == DATA_ACTION)
+		situation_data_report(ID_SAR,
+			event->sar_event.state);
+	return 0;
+}
+
+static int sarhub_local_init(void)
+{
+	struct situation_control_path ctl = {0};
+	struct situation_data_path data = {0};
+	int err = 0;
+
+	ctl.open_report_data = sar_open_report_data;
+	ctl.batch = sar_batch;
+	ctl.flush = sar_flush;
+	ctl.is_support_wake_lock = true;
+	ctl.is_support_batch = false;
+	err = situation_register_control_path(&ctl, ID_SAR);
+	if (err) {
+		pr_err("register sar control path err\n");
+		goto exit;
+	}
+
+	data.get_data = sar_get_data;
+	err = situation_register_data_path(&data, ID_SAR);
+	if (err) {
+		pr_err("register sar data path err\n");
+		goto exit;
+	}
+	err = scp_sensorHub_data_registration(ID_SAR,
+		sar_recv_data);
+	if (err) {
+		pr_err("SCP_sensorHub_data_registration fail!!\n");
+		goto exit;
+	}
+	return 0;
+exit:
+	return -1;
+}
+static int sarhub_local_uninit(void)
+{
+	return 0;
+}
+
+static struct situation_init_info sarhub_init_info = {
+	.name = "sar_hub",
+	.init = sarhub_local_init,
+	.uninit = sarhub_local_uninit,
+};
+
+static int __init sarhub_init(void)
+{
+	situation_driver_add(&sarhub_init_info, ID_SAR);
+	return 0;
+}
+
+static void __exit sarhub_exit(void)
+{
+	pr_debug("%s\n", __func__);
+}
+
+module_init(sarhub_init);
+module_exit(sarhub_exit);
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("SAR_HUB driver");
+MODULE_AUTHOR("Jashon.zhang@mediatek.com");
