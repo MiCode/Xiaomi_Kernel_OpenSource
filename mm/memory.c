e8372a3c59e6245b3f9ce3a670564e978f0ccf8d
Miles Chen <miles.chen@mediatek.com>
2019-02-28 21:22:49 +0800

    [ALPS04255650] mm: temporary mask might_lock_read()
    We observed a lockdep false positive case:
    
    There is a kernel lockdep annotation patch but the patch does not
    work for this case. Temporary mask out might_lock_read() for this
    false positive and report this issue to kernel.
    
    ======================================================
    WARNING: possible circular locking dependency detected
    4.14.87+ #1 Tainted: G S      W  O
    ------------------------------------------------------
    kswapd0/148 is trying to acquire lock:
    (&sb->s_type->i_mutex_key#9){+.+.}, at:
    [<ffffff800825ced4>] shmem_fallocate+0x64/0x3a0
    abut task is already holding lock:
    (ashmem_mutex){+.+.}, at: [<ffffff8008bc674c>] ashmem_shrink_scan+0x34/0x134
    awhich lock already depends on the new lock.x0a
    athe existing dependency chain (in reverse order) is:
    x0a-> #2 (ashmem_mutex){+.+.}:
    	__mutex_lock_common+0xac/0xbc8
    	mutex_lock_nested+0x28/0x30
    	ashmem_mmap+0x28/0x128
    	mmap_region+0x58c/0x764
    	do_mmap+0x308/0x3f8
    	vm_mmap_pgoff+0xac/0x104
    	SyS_mmap_pgoff+0x74/0xe8
    	el0_svc_naked+0x34/0x38
    x0a-> #1 (&mm->mmap_sem){++++}:
    	__might_fault+0x58/0x7c
    	iov_iter_fault_in_readable+0xa4/0x6b0
    	generic_perform_write+0x84/0x1bc
    	__generic_file_write_iter+0xc4/0x174
    	generic_file_write_iter+0xfc/0x190
    	__vfs_write+0x108/0x138
    	vfs_write+0xc8/0x184
    	SyS_write+0x54/0xb4
    	el0_svc_naked+0x34/0x38
    x0a-> #0 (&sb->s_type->i_mutex_key#9){+.+.}:
    	lock_acquire+0x224/0x25c
    	down_write+0x48/0x88
    	shmem_fallocate+0x64/0x3a0
    	ashmem_shrink_scan+0x90/0x134
    	shrink_slab+0x450/0x748
    	shrink_node+0x2cc/0x318
    	kswapd+0xa0c/0xc3c
    	kthread+0x11c/0x12c
    	ret_from_fork+0x10/0x18
    
    MTK-Commit-Id: 03a4cbff8d48917a26957f16b0263060b6ec1629
    
    Change-Id: I66c846286f5ca0738b7e3b82ced101d573e49b42
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    CR-Id: ALPS04255650
    Feature: [Module]Kernel Memory Management
    (cherry picked from commit ed32ff75d56d5cc3ee22728259116cc52de13874)
    (cherry-pick from aef18818f58ef8211b78cc2b750140ce740f56a7)

--- mm/memory.c
+++ mm/memory.c
@@ -4551,7 +4551,7 @@ void __might_fault(const char *file, int line)
 	if (pagefault_disabled())
 		return;
 	__might_sleep(file, line, 0);
-#if defined(CONFIG_DEBUG_ATOMIC_SLEEP)
+#if defined(CONFIG_DEBUG_ATOMIC_SLEEP) && 0
 	if (current->mm)
 		might_lock_read(&current->mm->mmap_sem);
 #endif
