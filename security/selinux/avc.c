bb141e627652c1a44770adbb984995cc03b19519
Sam-KY Lin <sam-ky.lin@mediatek.com>
2019-02-28 21:06:55 +0800

    [ALPS04094738] selinux: yellow screen warning
    [Detail]
     Migrate yellow screen warning feature for specified process domains.
    Its quite handy when we want to observe missing avc logs that are
    ignored by process owners.
    
    Conflicts:
    	drivers/misc/mediatek/Makefile
    
    MTK-Commit-Id: eacd14478945455185c2924585b308042b897f4f
    
    Change-Id: I7df091f7b789146d715236138f70d3452fabd41b
    Signed-off-by: Sam-KY Lin <sam-ky.lin@mediatek.com>
    CR-Id: ALPS04094738
    Feature: [Android Default] SELinux, SEAndroid, and SE-MTK
    (cherry-pick from 5e25b795610c6cf3c7362a242d6a00372b4ebfb5)

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -730,6 +730,21 @@ static void avc_audit_post_callback(struct audit_buffer *ab, void *a)
 	if (ad->selinux_audit_data->denied) {
 		audit_log_format(ab, " permissive=%u",
 				 ad->selinux_audit_data->result ? 0 : 1);
+#ifdef CONFIG_MTK_SELINUX_AEE_WARNING
+		{
+			struct nlmsghdr *nlh;
+			char *selinux_data;
+
+			if (ab) {
+				nlh = nlmsg_hdr(audit_get_skb(ab));
+				selinux_data = nlmsg_data(nlh);
+				if (nlh->nlmsg_type != AUDIT_EOE) {
+					if (nlh->nlmsg_type == 1400)
+						mtk_audit_hook(selinux_data);
+				}
+			}
+		}
+#endif
 	}
 }
 
