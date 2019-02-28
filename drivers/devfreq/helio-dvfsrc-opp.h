da4875b3df491a6a71fb722bd3be9ad23e70da07
Howard Hsu <howard.hsu@mediatek.com>
2019-02-28 21:06:12 +0800

    [ALPS04061334] VCORE DVFS: driver porting to K414
    [Detail]
    1. vcore dvfs driver porting
    2. qos driver porting
    3. mark-off not-ready api call
    4. disable by default
    
    MTK-Commit-Id: 66a3787b2dfc84bb3bc27bb24ff3b4029d876219
    
    Change-Id: I191a22c88d70ba28c7005aa7fa3cef8a286972f3
    Signed-off-by: Howard Hsu <howard.hsu@mediatek.com>
    CR-Id: ALPS04061334
    Feature: VCORE DVFS
    (cherry-pick from adce93ad25e069cb3665255f5db4e789c73a4967)

---
+++ drivers/devfreq/helio-dvfsrc-opp.h
@@ -0,0 +1,24 @@
+
+/*
+ * Copyright (C) 2017 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#ifndef __HELIO_DVFSRC_OPP_H
+#define __HELIO_DVFSRC_OPP_H
+
+#if defined(CONFIG_MACH_MT6775)
+#include <helio-dvfsrc-opp-mt6775.h>
+#elif defined(CONFIG_MACH_MT6771)
+#include <helio-dvfsrc-opp-mt6771.h>
+#endif
+
+#endif
