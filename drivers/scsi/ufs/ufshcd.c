245178151ce14808a77fbb95f2917ac2d2d39868
Can Guo <cang@codeaurora.org>
2019-02-28 10:14:21 +0800

    scsi: ufs: fix an error in getting UFS spec version
    The UFS spec version should be read out from the buffer which stores device
    descriptor before the buffer is cleared and used for reading other
    descriptors. Otherwise we will get wrong values.
    
    Change-Id: I56e665c010ceaf0916313a74926ce442874f824f
    Signed-off-by: Can Guo <cang@codeaurora.org>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -8308,6 +8308,9 @@ static int ufs_get_device_desc(struct ufs_hba *hba,
 	dev_desc->wmanufacturerid = desc_buf[DEVICE_DESC_PARAM_MANF_ID] << 8 |
 				     desc_buf[DEVICE_DESC_PARAM_MANF_ID + 1];
 
+	dev_desc->wspecversion = desc_buf[DEVICE_DESC_PARAM_SPEC_VER] << 8 |
+				  desc_buf[DEVICE_DESC_PARAM_SPEC_VER + 1];
+
 	model_index = desc_buf[DEVICE_DESC_PARAM_PRDCT_NAME];
 
 	/* Zero-pad entire buffer for string termination. */
@@ -8328,9 +8331,6 @@ static int ufs_get_device_desc(struct ufs_hba *hba,
 
 	/* Null terminate the model string */
 	dev_desc->model[MAX_MODEL_LEN] = '\0';
-
-	dev_desc->wspecversion = desc_buf[DEVICE_DESC_PARAM_SPEC_VER] << 8 |
-				  desc_buf[DEVICE_DESC_PARAM_SPEC_VER + 1];
 
 out:
 	kfree(desc_buf);
