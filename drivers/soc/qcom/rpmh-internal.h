72ec76f95cd48a89aff90f4c44c96af5ec5f90bb
Lina Iyer <ilina@codeaurora.org>
2019-03-06 12:58:44 -0700

    drivers: qcom: rpmh-rsc: add IPC logging support for RSC driver
    In addition to logging using FTRACE. let's also log in the IPC logging
    for all RPMH transactions.
    
    Change-Id: I2b19006bbc776053f63eee55b447428fdb023ba4
    Signed-off-by: Lina Iyer <ilina@codeaurora.org>

--- drivers/soc/qcom/rpmh-internal.h
+++ drivers/soc/qcom/rpmh-internal.h
@@ -94,6 +94,7 @@ struct rpmh_ctrlr {
  * @lock:       synchronize state of the controller
  * @client:     handle to the DRV's client.
  * @irq:        IRQ at gic
+ * @ipc_log_ctx IPC logger handle
  */
 struct rsc_drv {
 	const char *name;
@@ -107,6 +108,7 @@ struct rsc_drv {
 	spinlock_t lock;
 	struct rpmh_ctrlr client;
 	int irq;
+	void *ipc_log_ctx;
 };
 
 extern bool rpmh_standalone;
