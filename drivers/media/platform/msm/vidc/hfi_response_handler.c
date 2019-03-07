863bd77bde408d54f66a7238fcb4146001428d2e
Priyanka Gujjula <pgujjula@codeaurora.org>
2019-03-07 20:03:15 +0530

    msm: vidc: Resolve clang compilation error
    pkt->rg_buffer_info will always evaluate to 'true',
    hence refining the code.
    
    Change-Id: Ifb686c991b9ae06d449a5a1ec3485823ca9b926c
    Signed-off-by: Priyanka Gujjula <pgujjula@codeaurora.org>

--- drivers/media/platform/msm/vidc/hfi_response_handler.c
+++ drivers/media/platform/msm/vidc/hfi_response_handler.c
@@ -1706,13 +1706,9 @@ static int hfi_process_session_rel_buf_done(u32 device_id,
 	cmd_done.size = sizeof(struct msm_vidc_cb_cmd_done);
 	cmd_done.session_id = (void *)(uintptr_t)pkt->session_id;
 	cmd_done.status = hfi_map_err_status(pkt->error_type);
-	if (pkt->rg_buffer_info) {
-		cmd_done.data.buffer_info =
+	cmd_done.data.buffer_info =
 			*(struct hal_buffer_info *)pkt->rg_buffer_info;
-		cmd_done.size = sizeof(struct hal_buffer_info);
-	} else {
-		dprintk(VIDC_ERR, "invalid payload in rel_buff_done\n");
-	}
+	cmd_done.size = sizeof(struct hal_buffer_info);
 
 	info->response_type = HAL_SESSION_RELEASE_BUFFER_DONE;
 	info->response.cmd = cmd_done;
