a492c84fdbbcd6238cc9d3c46e3fd46cd7e9b89f
Lokesh Kumar Aakulu <lkumar@codeaurora.org>
2019-03-11 17:27:09 +0530

    msm: camera_v2: Fix page fault issue in camera ISP
    Fix page fault in ISP when there is no reg update
    for two consecutive request frame time.
    
    Change-Id: Ie246f146c1ec0785e0e6fa0671dd2ff28fbe6b38
    Signed-off-by: Lokesh Kumar Aakulu <lkumar@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp.h
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp.h
@@ -424,6 +424,12 @@ enum msm_isp_comp_irq_types {
 
 #define MSM_VFE_REQUESTQ_SIZE 8
 
+struct msm_isp_pending_buf_info {
+	uint32_t is_buf_done_pending;
+	struct msm_isp_buffer *buf;
+	uint32_t frame_id;
+};
+
 struct msm_vfe_axi_stream {
 	uint32_t frame_id;
 	enum msm_vfe_axi_state state;
@@ -480,6 +486,7 @@ struct msm_vfe_axi_stream {
 	uint32_t vfe_mask;
 	uint32_t composite_irq[MSM_ISP_COMP_IRQ_MAX];
 	int lpm_mode;
+	struct msm_isp_pending_buf_info pending_buf_info;
 };
 
 struct msm_vfe_axi_composite_info {
