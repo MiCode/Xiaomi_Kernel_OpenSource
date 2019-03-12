9301cde5defa19af8d40afee1cc359c00111642c
Vidyakumar Athota <vathota@codeaurora.org>
2019-03-12 13:13:23 -0700

    clk: dt-bindings: Define clock IDs supported by bolero codec
    Define clock IDs supported by bolero codec for kona.
    
    Change-Id: I762beb30ffaaf9235caca8de491f8b9a26840983
    Signed-off-by: Vidyakumar Athota <vathota@codeaurora.org>

---
+++ include/dt-bindings/sound/qcom,bolero-clk-rsc.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2019, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef __BOLERO_CODEC_CLK_RSC_H
+#define __BOLERO_CODEC_CLK_RSC_H
+
+/* Bolero clock types */
+#define TX_CORE_CLK	0
+#define RX_CORE_CLK	1
+#define WSA_CORE_CLK	2
+#define VA_CORE_CLK	3
+#define TX_NPL_CLK	4
+#define RX_NPL_CLK	5
+#define WSA_NPL_CLK	6
+#define VA_NPL_CLK	7
+#define MAX_CLK	8
+
+#endif /* __BOLERO_CODEC_CLK_RSC_H */
