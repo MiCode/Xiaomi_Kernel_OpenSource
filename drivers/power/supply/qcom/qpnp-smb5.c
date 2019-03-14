88ada2a9d611ef0cefdc2e7a3fa80a271551db26
Fenglin Wu <fenglinw@codeaurora.org>
2019-03-14 08:55:17 +0800

    power: supply: smb5: Configure charger termination condition
    Configure charger termination based on the number of the disqualified
    IBAT samples. If there are less than MAX_FAILED_IBATT_SAMPLE_FOR_TERM
    disqualified IBAT samples within one IBAT conversion cycle, then
    charging is terminated.
    
    Change-Id: I7a8104e84a241f6549d599edbd379b03e470c6d2
    Signed-off-by: Fenglin Wu <fenglinw@codeaurora.org>

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -2075,9 +2075,18 @@ static int smb5_configure_iterm_thresholds_adc(struct smb5 *chip)
 static int smb5_configure_iterm_thresholds(struct smb5 *chip)
 {
 	int rc = 0;
+	struct smb_charger *chg = &chip->chg;
 
 	switch (chip->dt.term_current_src) {
 	case ITERM_SRC_ADC:
+		rc = smblib_masked_write(chg, CHGR_ADC_TERM_CFG_REG,
+				TERM_BASED_ON_SYNC_CONV_OR_SAMPLE_CNT,
+				TERM_BASED_ON_SAMPLE_CNT);
+		if (rc < 0) {
+			dev_err(chg->dev, "Couldn't configure ADC_ITERM_CFG rc=%d\n",
+					rc);
+			return rc;
+		}
 		rc = smb5_configure_iterm_thresholds_adc(chip);
 		break;
 	default:
