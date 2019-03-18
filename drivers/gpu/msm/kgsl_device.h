1939da95c194e8dc2e6bb9251e4752f5ddf7ed25
Puranam V G Tejaswi <pvgtejas@codeaurora.org>
2019-03-18 20:12:15 +0530

    msm: kgsl: correct per process stat maintenance
    In functions like gpumem_alloc_entry(), per process
    stats are updated after kgsl_mem_entry_attach_process()
    returns success. But in case of failure, per process
    stats are decremented, though they are not incremented.
    Fix this by isolating stat updation from
    kgsl_mem_entry_detach_process(). Also take into account
    the memory from sparse allocations.
    
    Change-Id: I6e2bc1880bdda15aa75d546d0dc3d34c63cd7dba
    Signed-off-by: Puranam V G Tejaswi <pvgtejas@codeaurora.org>

--- drivers/gpu/msm/kgsl_device.h
+++ drivers/gpu/msm/kgsl_device.h
@@ -550,6 +550,12 @@ static inline void kgsl_process_add_stats(struct kgsl_process_private *priv,
 		priv->stats[type].max = priv->stats[type].cur;
 }
 
+static inline void kgsl_process_sub_stats(struct kgsl_process_private *priv,
+	unsigned int type, uint64_t size)
+{
+	priv->stats[type].cur -= size;
+}
+
 static inline bool kgsl_is_register_offset(struct kgsl_device *device,
 				unsigned int offsetwords)
 {
