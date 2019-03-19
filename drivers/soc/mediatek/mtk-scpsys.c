36f0e8e4f535f0238fd8dddc63492a01e81096f1
Weiyi Lu <weiyi.lu@mediatek.com>
2019-03-19 16:01:29 +0800

    FROMLIST: soc: mediatek: Switch to SPDX license identifier
    Switch to SPDX license identifier
    (am from https://patchwork.kernel.org/patch/10858921/)
    
    Signed-off-by: Weiyi Lu <weiyi.lu@mediatek.com>
    
    CR-Id: ALPS04416165
    Change-Id: I5fd7e6071ae414497df14651efec6d88c6a95e88
    Signed-off-by: YY Huang <yy.huang@mediatek.com>

--- drivers/soc/mediatek/mtk-scpsys.c
+++ drivers/soc/mediatek/mtk-scpsys.c
@@ -1,15 +1,7 @@
-/*
- * Copyright (c) 2015 Pengutronix, Sascha Hauer <kernel@pengutronix.de>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+// SPDX-License-Identifier: GPL-2.0
+//
+// Copyright (c) 2015 Pengutronix, Sascha Hauer <kernel@pengutronix.de>
+
 #include <linux/clk.h>
 #include <linux/init.h>
 #include <linux/io.h>
