55be9a0880f757cb9e68dfd7e2ef3dd576994e5d
Amir Levy <alevy@codeaurora.org>
2019-03-25 16:45:45 +0200

    msm: ipa: Support Modem distinction of LE MBB and LE MHI
    Add support for MHI protocol for QMI msg types.
    
    Change-Id: I98c5fe64ef5fae31954ffe62d40adb759851c86c
    Acked-by: Michal Amsterdam <mamsterd@qti.qualcomm.com>
    Signed-off-by: Amir Levy <alevy@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/rmnet_ipa.c
+++ drivers/platform/msm/ipa/ipa_v3/rmnet_ipa.c
@@ -2534,6 +2534,9 @@ static int ipa3_wwan_probe(struct platform_device *pdev)
 		ipa3_ctx->platform_type == IPA_PLAT_TYPE_APQ)
 		/* Android platform loads uC */
 		ipa3_qmi_service_init(QMI_IPA_PLATFORM_TYPE_MSM_ANDROID_V01);
+	else if (ipa3_ctx->ipa_config_is_mhi)
+		/* LE MHI platform */
+		ipa3_qmi_service_init(QMI_IPA_PLATFORM_TYPE_LE_MHI_V01);
 	else
 		/* LE platform not loads uC */
 		ipa3_qmi_service_init(QMI_IPA_PLATFORM_TYPE_LE_V01);
