fe7fc3947f2e09164e2b1d8edebe15345d8747e4
Jishnu Prakash <jprakash@codeaurora.org>
2019-03-26 18:44:30 +0530

    thermal: adc_tm: Update threshold cross notification to thermal client
    Increased resolution of mapping between raw adc code and temperature
    for thermistors. Adjust thresholds written to ensure interrupts get
    triggered correctly for temperatures read.
    In addition, measure temperature causing threshold violation and
    provide it directly to thermal framework instead of waiting for thermal
    framework to read it.
    
    Change-Id: I4330c659a91021ef44fa17c9c79e379105aa8fc3
    Signed-off-by: Jishnu Prakash <jprakash@codeaurora.org>

--- drivers/iio/adc/qcom-vadc-common.h
+++ drivers/iio/adc/qcom-vadc-common.h
@@ -49,6 +49,7 @@
 #define VADC5_MAX_CODE				0x7fff
 #define VADC5_FULL_SCALE_CODE			0x70e4
 #define ADC_USR_DATA_CHECK			0x8000
+#define ADC_HC_VDD_REF			1875000
 
 /**
  * struct vadc_map_pt - Map the graph representation for ADC channel
