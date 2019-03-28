19cb5cc831339e7813cf87ca43b8ab56c464a77f
Harry Yang <harryy@codeaurora.org>
2019-03-28 16:04:13 -0700

    power: smb5-lib: Report liquid presence status
    Expose moisture status via USb PSY MOISTURE_DETECTED property,
    and update on status change.
    
    Change-Id: I9abd948ad8eca7c8ee3022589aeb78cb1286418e
    Signed-off-by: Harry Yang <harryy@codeaurora.org>

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -5012,6 +5012,8 @@ enum alarmtimer_restart smblib_lpd_recheck_timer(struct alarm *alarm,
 				pval.intval, rc);
 			return ALARMTIMER_NORESTART;
 		}
+		chg->moisture_present = false;
+		power_supply_changed(chg->usb_psy);
 	} else {
 		rc = smblib_masked_write(chg, TYPE_C_INTERRUPT_EN_CFG_2_REG,
 					TYPEC_WATER_DETECTION_INT_EN_BIT,
@@ -5067,8 +5069,10 @@ static bool smblib_src_lpd(struct smb_charger *chg)
 			smblib_err(chg, "Couldn't write 0x%02x to TYPE_C_INTRPT_ENB_SOFTWARE_CTRL rc=%d\n",
 				pval.intval, rc);
 		chg->lpd_reason = LPD_MOISTURE_DETECTED;
+		chg->moisture_present =  true;
 		alarm_start_relative(&chg->lpd_recheck_timer,
 						ms_to_ktime(60000));
+		power_supply_changed(chg->usb_psy);
 	} else {
 		chg->lpd_reason = LPD_NONE;
 		chg->typec_mode = smblib_get_prop_typec_mode(chg);
@@ -6346,6 +6350,7 @@ static void smblib_lpd_ra_open_work(struct work_struct *work)
 		}
 
 		chg->lpd_reason = LPD_MOISTURE_DETECTED;
+		chg->moisture_present =  true;
 
 	} else {
 		/* Floating cable, disable water detection irq temporarily */
