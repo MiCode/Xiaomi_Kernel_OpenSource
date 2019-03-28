0b34e52eb25e895a491ab5a932f02c536bc3fa05
Shadul Shaikh <sshadu@codeaurora.org>
2019-03-28 18:22:15 +0530

    msm: camera: Avoid SMMU fault with two camera instances
    While two camera instances are running parallely, camera instance
    one sends phy protect call, unmaps CSID registers from HLOS. Meanwhile,
    CSID reset was called for camera instance two and there was a read for
    IRQ status from HLOS. Hence passing separate device ID for each camera
    instance.
    
    Change-Id: Iac7c83aa2d3a775d53093487e30c65413062313d
    Signed-off-by: Shadul Shaikh <sshadu@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/sensor/csid/msm_csid.c
+++ drivers/media/platform/msm/camera_v2/sensor/csid/msm_csid.c
@@ -288,9 +288,10 @@ static int msm_csid_reset(struct csid_device *csid_dev)
 	} else {
 		irq_bitshift =
 		csid_dev->ctrl_reg->csid_reg.csid_rst_done_irq_bitshift;
-		msm_camera_io_w(csid_dev->ctrl_reg->csid_reg.csid_rst_stb_all,
-			csid_dev->base +
-			csid_dev->ctrl_reg->csid_reg.csid_rst_cmd_addr);
+		msm_camera_vio_w(csid_dev->ctrl_reg->csid_reg.csid_rst_stb_all,
+			csid_dev->base,
+			csid_dev->ctrl_reg->csid_reg.csid_rst_cmd_addr,
+			csid_dev->pdev->id);
 		rc = wait_for_completion_timeout(&csid_dev->reset_complete,
 			CSID_TIMEOUT);
 	}
@@ -298,8 +299,9 @@ static int msm_csid_reset(struct csid_device *csid_dev)
 		pr_err("wait_for_completion in %s fail rc = %d\n",
 			__func__, rc);
 	} else if (rc == 0) {
-		irq = msm_camera_io_r(csid_dev->base +
-			csid_dev->ctrl_reg->csid_reg.csid_irq_status_addr);
+		irq = msm_camera_vio_r(csid_dev->base,
+			csid_dev->ctrl_reg->csid_reg.csid_irq_status_addr,
+			csid_dev->pdev->id);
 		pr_err_ratelimited("%s CSID%d_IRQ_STATUS_ADDR = 0x%x\n",
 			__func__, csid_dev->pdev->id, irq);
 		if (irq & (0x1 << irq_bitshift)) {
