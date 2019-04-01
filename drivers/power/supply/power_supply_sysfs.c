7eb574c53df2e50faf7d48fbc1115a6899e8a6c2
Anirudh Ghayal <aghayal@codeaurora.org>
2019-04-01 09:24:18 +0530

    power: power_supply: Add REAL_CAPACITY power-supply property
    POWER_SUPPLY_PROP_REAL_CAPACITY reports the unadjusted
    and unscaled battery capacity.
    
    Change-Id: I7a016cdf89904f4d5ecbfd9b9afbd05fbfe62441
    Signed-off-by: Anirudh Ghayal <aghayal@codeaurora.org>

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -342,6 +342,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(fcc_stepper_enable),
 	POWER_SUPPLY_ATTR(cc_soc),
 	POWER_SUPPLY_ATTR(qg_vbms_mode),
+	POWER_SUPPLY_ATTR(real_capacity),
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_ATTR(charge_counter_ext),
 	/* Properties of type `const char *' */
