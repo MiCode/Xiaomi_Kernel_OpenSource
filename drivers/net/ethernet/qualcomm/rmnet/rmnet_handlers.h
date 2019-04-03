1e767721f15ee2be561ce3f0af0fe5c7a24e36c2
Conner Huff <chuff@codeaurora.org>
2019-04-02 18:06:53 -0700

    drivers: net: rmnet: Check if SHS reports slow start
    SHS module will proactively check to see if we are
    in slow start mode. If we are then we can return true
    to whoever asks.
    
    Change-Id: Ia0e600207b2ff7fc4789006c7fcc6861908480a3
    Signed-off-by: Conner Huff <chuff@codeaurora.org>

--- drivers/net/ethernet/qualcomm/rmnet/rmnet_handlers.h
+++ drivers/net/ethernet/qualcomm/rmnet/rmnet_handlers.h
@@ -20,6 +20,7 @@ void rmnet_deliver_skb(struct sk_buff *skb, struct rmnet_port *port);
 void rmnet_deliver_skb_wq(struct sk_buff *skb, struct rmnet_port *port,
 			  enum rmnet_packet_context ctx);
 void rmnet_set_skb_proto(struct sk_buff *skb);
+bool rmnet_slow_start_on(u32 hash_key);
 rx_handler_result_t _rmnet_map_ingress_handler(struct sk_buff *skb,
 					       struct rmnet_port *port);
 rx_handler_result_t rmnet_rx_handler(struct sk_buff **pskb);
