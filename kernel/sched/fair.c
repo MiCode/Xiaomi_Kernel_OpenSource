a69d48cbbc73b4899d6bc874d3667499a961297d
Maria Yu <aiquny@codeaurora.org>
2019-04-03 18:26:21 +0800

    sched/fair: Avoid cpu idle to deep sleep when have active balance
    Avoid cpu idle to deep sleep when have active balance
    task to be coming.
    
    Change-Id: Ie1c0f7fe0ff3da38d0a8f5ebe974080a52b7e6f4
    Signed-off-by: Maria Yu <aiquny@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10550,6 +10550,7 @@ no_move:
 				busiest->active_balance = 1;
 				busiest->push_cpu = this_cpu;
 				active_balance = 1;
+				mark_reserved(this_cpu);
 			}
 			raw_spin_unlock_irqrestore(&busiest->lock, flags);
 
@@ -10782,6 +10783,7 @@ out_unlock:
 	busiest_rq->active_balance = 0;
 	push_task = busiest_rq->push_task;
 	target_cpu = busiest_rq->push_cpu;
+	clear_reserved(target_cpu);
 
 	if (push_task)
 		busiest_rq->push_task = NULL;
@@ -10792,7 +10794,6 @@ out_unlock:
 		if (push_task_detached)
 			attach_one_task(target_rq, push_task);
 		put_task_struct(push_task);
-		clear_reserved(target_cpu);
 	}
 
 	if (p)
