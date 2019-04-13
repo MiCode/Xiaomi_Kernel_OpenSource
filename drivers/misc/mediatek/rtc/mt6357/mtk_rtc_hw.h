01851aecedecebd91e6d9c455f838cd902ad873c
andrew.yang <andrew.yang@mediatek.com>
2019-04-13 17:12:29 +0800

    [ALPS04385413] RTC: Fix suspend fail issue
    Assign correct month value.
    
    MTK-Commit-Id: 657fca9397efd59d6048fc042b72f2a7acb1c869
    
    Change-Id: I2c07ca76458f37dc445c5a68a6084499ee693416
    Signed-off-by: andrew.yang <andrew.yang@mediatek.com>
    CR-Id: ALPS04385413
    Feature: Power Management

--- drivers/misc/mediatek/rtc/mt6357/mtk_rtc_hw.h
+++ drivers/misc/mediatek/rtc/mt6357/mtk_rtc_hw.h
@@ -106,7 +106,7 @@ extern unsigned int pmic_config_interface_nolock(unsigned int RegNum,
 #define RTC_TC_DOW					(PMIC_TC_DOW_ADDR)
 
 #define RTC_TC_MTH					(PMIC_TC_MONTH_ADDR)
-
+#define RTC_TC_MTH_MASK				(PMIC_TC_MONTH_MASK)
 #define RTC_TC_YEA					(PMIC_TC_YEAR_ADDR)
 
 #define RTC_AL_SEC					(PMIC_AL_SECOND_ADDR)
