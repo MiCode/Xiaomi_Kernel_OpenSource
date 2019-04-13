f6fc2e72bf3b400da10c625eea1eb23d4c487b13
Peter Wang <peter.wang@mediatek.com>
2019-04-13 17:12:35 +0800

    [ALPS04386083] Revert "sd: Fix rw_max for devices that report..."
    This reverts commit 6b7e9cde49691e04314342b7dce90c67ad567fcc.
    ("sd: Fix rw_max for devices
    that report an optimal xfer size").
    Max sectors limit for request queue(q->limits.max_sectors) is reduced
    from 1024 to 16 sectors by above commit and causing performance impact
    (reduced by upto 50%). Hence revert the change to fix the performance
    issue observed on mtk chip.
    
    MTK-Commit-Id: 978db72ed0511f1310d9d254c3a096aaeb12660d
    
    Change-Id: I7ec85552dfb5f30c57785d2b5e086f9ae698f753
    Signed-off-by: Peter Wang <peter.wang@mediatek.com>
    CR-Id: ALPS04386083
    Feature: UFS Booting

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -3207,10 +3207,18 @@ static int sd_revalidate_disk(struct gendisk *disk)
 	dev_max = min_not_zero(dev_max, sdkp->max_xfer_blocks);
 	q->limits.max_dev_sectors = logical_to_sectors(sdp, dev_max);
 
-	if (sd_validate_opt_xfer_size(sdkp, dev_max)) {
-		q->limits.io_opt = logical_to_bytes(sdp, sdkp->opt_xfer_blocks);
-		rw_max = logical_to_sectors(sdp, sdkp->opt_xfer_blocks);
-	} else
+	/*
+	 * Determine the device's preferred I/O size for reads and writes
+	 * unless the reported value is unreasonably small, large, or
+	 * garbage.
+	 */
+	if (sdkp->opt_xfer_blocks &&
+	    sdkp->opt_xfer_blocks <= dev_max &&
+	    sdkp->opt_xfer_blocks <= SD_DEF_XFER_BLOCKS &&
+	    sdkp->opt_xfer_blocks * sdp->sector_size >= PAGE_SIZE)
+		rw_max = q->limits.io_opt =
+			sdkp->opt_xfer_blocks * sdp->sector_size;
+	else
 		rw_max = min_not_zero(logical_to_sectors(sdp, dev_max),
 				      (sector_t)BLK_DEF_MAX_SECTORS);
 
