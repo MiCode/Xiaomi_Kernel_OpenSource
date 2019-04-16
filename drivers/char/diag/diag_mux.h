5a22e3e463d4099ebda31f7174e2896cd66d49b2
Sreelakshmi Gownipalli <sgownipa@codeaurora.org>
2019-04-16 16:25:27 -0700

    diag: Clear memory device entries during mhi disconnect
    Clear the entries in the memory device table during the mhi remove call
    for diag channels.
    
    Change-Id: I68e243b68a6c4a60f7af777ae9c2e4511bf899ce
    Signed-off-by: Sreelakshmi Gownipalli <sgownipa@codeaurora.org>

--- drivers/char/diag/diag_mux.h
+++ drivers/char/diag/diag_mux.h
@@ -55,6 +55,7 @@ struct diag_logger_ops {
 	int (*queue_read)(int id);
 	int (*write)(int id, unsigned char *buf, int len, int ctx);
 	int (*close_peripheral)(int id, uint8_t peripheral);
+	int (*close_device)(int id);
 };
 
 struct diag_logger_t {
@@ -71,6 +72,7 @@ int diag_mux_register(int proc, int ctx, struct diag_mux_ops *ops);
 int diag_mux_queue_read(int proc);
 int diag_mux_write(int proc, unsigned char *buf, int len, int ctx);
 int diag_mux_close_peripheral(int proc, uint8_t peripheral);
+int diag_mux_close_device(int proc);
 int diag_mux_open_all(struct diag_logger_t *logger);
 int diag_mux_close_all(void);
 int diag_mux_switch_logging(int *new_mode, int *peripheral_mask);
