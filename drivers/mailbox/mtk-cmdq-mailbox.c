8526d5b885be4f8569d27d68dc7237ffab21a523
Dennis YC Hsieh <dennis-yc.hsieh@mediatek.com>
2019-04-18 14:25:34 +0800

    mailbox: cmdq: support mt6779 gce platform definition
    Add gce v4 hardware support with different thread number and shift.
    
    Change-Id: I0e7efefdd479bfec3ee0e08be718becf8de008f1
    Signed-off-by: Dennis YC Hsieh <dennis-yc.hsieh@mediatek.com>
    CR-Id: ALPS04356772
    Feature: [Module]CMDQ Driver

--- drivers/mailbox/mtk-cmdq-mailbox.c
+++ drivers/mailbox/mtk-cmdq-mailbox.c
@@ -573,9 +573,11 @@ static const struct dev_pm_ops cmdq_pm_ops = {
 };
 
 static const struct gce_plat gce_plat_v2 = {.thread_nr = 16, .shift = 0};
+static const struct gce_plat gce_plat_v4 = {.thread_nr = 24, .shift = 3};
 
 static const struct of_device_id cmdq_of_ids[] = {
 	{.compatible = "mediatek,mt8173-gce", .data = (void *)&gce_plat_v2},
+	{.compatible = "mediatek,mt6779-gce", .data = (void *)&gce_plat_v4},
 	{}
 };
 
