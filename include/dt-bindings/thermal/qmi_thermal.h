973a700971bce4d6ec00d174ed4116581ef142a5
Ram Chandrasekar <rkumbako@codeaurora.org>
2019-04-22 14:25:59 -0600

    dt-bindings: thermal: qmi_sensor: Add MACROs for QMI sensors
    Add MACROs to reference different QMI sensor indexes. This will help in
    referencing the QMI sensor when defining a thermal zone.
    
    Change-Id: I96d576d9d5eed91cb69279248e5536acb9ba5972
    Signed-off-by: Ram Chandrasekar <rkumbako@codeaurora.org>

---
+++ include/dt-bindings/thermal/qmi_thermal.h
@@ -0,0 +1,40 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Copyright (c) 2018-2019, The Linux Foundation. All rights reserved. */
+
+#ifndef _DT_BINDINGS_QCOM_QMI_THERMAL_H
+#define _DT_BINDINGS_QCOM_QMI_THERMAL_H
+
+#define QMI_PA			0
+#define QMI_PA_1		1
+#define QMI_PA_2		2
+#define QMI_QFE_PA_0		3
+#define QMI_QFE_WTR_0		4
+#define QMI_MODEM_TSENS		5
+#define QMI_QFE_MMW_0		6
+#define QMI_QFE_MMW_1		7
+#define QMI_QFE_MMW_2		8
+#define QMI_QFE_MMW_3		9
+#define QMI_XO_THERM		10
+#define QMI_QFE_PA_MDM		11
+#define QMI_QFE_PA_WTR		12
+#define QMI_QFE_MMW_STREAMER_0	13
+#define QMI_QFE_MMW_0_MOD	14
+#define QMI_QFE_MMW_1_MOD	15
+#define QMI_QFE_MMW_2_MOD	16
+#define QMI_QFE_MMW_3_MOD	17
+#define QMI_QFE_RET_PA_0	18
+#define QMI_QFE_WTR_PA_0	19
+#define QMI_QFE_WTR_PA_1	20
+#define QMI_QFE_WTR_PA_2	21
+#define QMI_QFE_WTR_PA_3	22
+#define QMI_SYS_THERM_1		23
+#define QMI_SYS_THERM_2		24
+#define QMI_MODEM_TSENS_1	25
+
+#define QMI_MODEM_INST_ID	0x0
+#define QMI_ADSP_INST_ID	0x1
+#define QMI_CDSP_INST_ID	0x43
+#define QMI_SLPI_INST_ID	0x53
+#define QMI_MODEM_NR_INST_ID	0x64
+
+#endif
