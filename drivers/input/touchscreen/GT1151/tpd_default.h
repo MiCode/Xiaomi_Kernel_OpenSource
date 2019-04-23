20ab9e3e40fbb277c04fa618dcd2b42cc136aae8
mtk15754 <wenqiang.zhang@mediatek.com>
2019-04-23 10:04:54 +0800

    drivers: input: touch: add GT1151 driver
    through discuss with tablet,touch will remove common file.
    use vendor release touch driver directly.
    
    action:
    1.add GT1151 touch driver.
    2.remove MTK touch common file
    
    Change-Id: I9df6196ba448cc55f93afe9c9fe31160a390dc7d
    Signed-off-by: mtk15754 <wenqiang.zhang@mediatek.com>
    CR-Id: ALPS04400359
    Feature: [Android Default] CapTouch

---
+++ drivers/input/touchscreen/GT1151/tpd_default.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2019 MediaTek Inc.
+ */
+
+#ifndef TPD_FAT_TOUCH
+#define TPD_FAT_TOUCH 120
+#endif
+
+int tpd_trembling_tolerance(int t, int p);
