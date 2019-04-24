bccc6ea96bc3712b851ab0210222c0b8d533038a
Chun Fan <chun.fan@mediatek.com>
2019-04-24 19:38:34 +0800

    soc: mediatek: Add ATF used SMC fid into SMC ID table
    1. TIME_SYNC is for synchronizing kernel and ATF timestamp
    2. ATF_DEBUG is for ATF UT/IT
    
    Change-Id: I08d1cc0bb5e195f24d622e18b6d7ba38bcd9e5f0
    Signed-off-by: Chun Fan <chun.fan@mediatek.com>
    CR-Id: ALPS04385480
    Feature: [Module]ATF (ARM Trusted Firmware)

--- include/linux/soc/mediatek/mtk_sip_svc.h
+++ include/linux/soc/mediatek/mtk_sip_svc.h
@@ -22,12 +22,17 @@
 #define MTK_SIP_SMC_AARCH_BIT			0x00000000
 #endif
 
+/* 0x82000200 - 0x820003FF & 0xC2000300 - 0xC20003FF */
+/* Debug feature and ATF related */
+#define MTK_SIP_KERNEL_TIME_SYNC \
+	(0x82000202 | MTK_SIP_SMC_AARCH_BIT)
+#define MTK_SIP_KERNEL_ATF_DEBUG \
+	(0x82000204 | MTK_SIP_SMC_AARCH_BIT)
 
 /* Security related SMC call */
 /* TRNG */
 #define MTK_SIP_KERNEL_GET_RND \
 	(0x82000206 | MTK_SIP_SMC_AARCH_BIT)
-
 
 #endif				/* _MTK_SECURE_API_H_ */
 
