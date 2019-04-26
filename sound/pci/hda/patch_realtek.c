bfa5aa541c1ed6ec031551c1b7817410b2f09b41
Kailang Yang <kailang@realtek.com>
2019-04-26 16:13:54 +0800

    ALSA: hda/realtek - Fixed Dell AIO speaker noise
    commit 0700d3d117a7f110ddddbd83873e13652f69c54b upstream.
    
    Fixed Dell AIO speaker noise.
    spec->gen.auto_mute_via_amp = 1, this option was solved speaker white
    noise at boot.
    codec->power_save_node = 0, this option was solved speaker noise at
    resume back.
    
    Fixes: 9226665159f0 ("ALSA: hda/realtek - Fix Dell AIO LineOut issue")
    Signed-off-by: Kailang Yang <kailang@realtek.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- sound/pci/hda/patch_realtek.c
+++ sound/pci/hda/patch_realtek.c
@@ -5294,6 +5294,8 @@ static void alc274_fixup_bind_dacs(struct hda_codec *codec,
 		return;
 
 	spec->gen.preferred_dacs = preferred_pairs;
+	spec->gen.auto_mute_via_amp = 1;
+	codec->power_save_node = 0;
 }
 
 static void alc_fixup_disable_mic_vref(struct hda_codec *codec,
