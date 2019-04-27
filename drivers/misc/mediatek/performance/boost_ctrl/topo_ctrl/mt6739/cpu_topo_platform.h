189f56314e98afed0aabdf9baac183155bb46693
Ian-Y Chen <ian-y.chen@mediatek.com>
2019-04-27 21:20:37 +0800

    [ALPS04328846] perfmgr: add mt6739 driver
    [Detail]
    Add following driver for mt6739
    1. topo_ctrl
    2. dram_ctrl
    
    MTK-Commit-Id: b3f80e489e398fcd659048000e9dc8f128ec924c
    
    Change-Id: Icbc51181b6d5b04d17b548fe791ea0e65ec5ff8c
    Signed-off-by: Ian-Y Chen <ian-y.chen@mediatek.com>
    CR-Id: ALPS04328846
    Feature: [Module]PowerHAL

---
+++ drivers/misc/mediatek/performance/boost_ctrl/topo_ctrl/mt6739/cpu_topo_platform.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2018 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+
+#ifndef __MT_CPU_TOPO_PLATFORM_H__
+#define __MT_CPU_TOPO_PLATFORM_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*==============================================================*/
+/* Macros							*/
+/*==============================================================*/
+/* TODO: remove these workaround for k49 migration */
+#define NO_SCHEDULE_API		(1)
+
+#define TOTAL_CORE_NUM	(CORE_NUM_L)
+#define CORE_NUM_L	(4)
+
+#define get_cluster_cpu_core(id)	\
+		(id ? 0 : CORE_NUM_L)
+
+/*==============================================================*/
+/* Enum								*/
+/*==============================================================*/
+enum ppm_cluster {
+	PPM_CLUSTER_L = 0,
+
+	NR_PPM_CLUSTERS,
+};
+
+/*==============================================================*/
+/* Data Structures						*/
+/*==============================================================*/
+
+/*==============================================================*/
+/* Global Variables						*/
+/*==============================================================*/
+
+/*==============================================================*/
+/* APIs								*/
+/*==============================================================*/
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
