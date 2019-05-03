7b9010a6b1c67d3897756db6ba41c74df89b709c
Srikanth Uyyala <suyyala@codeaurora.org>
2019-05-03 10:17:03 +0530

    msm:camera: avoid remap logic for RDI path
    Frame remap logic is based on PIX SOF events only,
    this fix is to avoid this remap logic for RDI streams.
    
    Change-Id: I100d2bfd098d186f4bb3cb348e683c73e1d7c00b
    Signed-off-by: Srikanth Uyyala <suyyala@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -3550,7 +3550,8 @@ static int msm_isp_request_frame(struct vfe_device *vfe_dev,
 		vfe_ops.axi_ops.get_pingpong_status(vfe_dev);
 
 	/* As MCT is still processing it, need to drop the additional requests*/
-	if (vfe_dev->isp_page->drop_reconfig) {
+	if (vfe_dev->isp_page->drop_reconfig &&
+		frame_src == VFE_PIX_0) {
 		pr_err("%s: MCT has not yet delayed %d drop request %d\n",
 			__func__, vfe_dev->isp_page->drop_reconfig, frame_id);
 		goto error;
