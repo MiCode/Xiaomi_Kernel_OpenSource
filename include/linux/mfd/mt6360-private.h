fabb13ed004e38951f07509d8ea3b78824b8d117
Vend.Gene.Chen <vend_gene_chen001@mediatek.com>
2019-05-06 14:23:22 +0800

    leds: mt6360: add MT6360 LED driver
    add dual Flash LED drivers found on Mediatek MT6360 PMIC
    Support Torch and Strobe mode independently current source
    Include Low-VF and short protection
    
    Change-Id: Ib8a1b882f428b561e6f7c30e084b81a52a7402ef
    Signed-off-by: Vend.Gene.Chen <vend_gene_chen001@mediatek.com>
    CR-Id: ALPS04377845
    Feature: Power Management

--- include/linux/mfd/mt6360-private.h
+++ include/linux/mfd/mt6360-private.h
@@ -223,6 +223,57 @@ struct mt6360_pmu_irq_desc {
 	irq_handler_t irq_handler;
 };
 
-/* TODO: add parse and apply dts node function and define for sub-device */
+#define  MT6360_DT_VALPROP(name, type) \
+			{#name, offsetof(type, name)}
+
+struct mt6360_val_prop {
+	const char *name;
+	size_t offset;
+};
+
+static inline void mt6360_dt_parser_helper(struct device_node *np, void *data,
+					   const struct mt6360_val_prop *props,
+					   int prop_cnt)
+{
+	int i;
+
+	for (i = 0; i < prop_cnt; i++) {
+		if (unlikely(!props[i].name))
+			continue;
+		of_property_read_u32(np, props[i].name, data + props[i].offset);
+	}
+}
+
+#define MT6360_PDATA_VALPROP(name, type, reg, shift, mask, func, base) \
+			{offsetof(type, name), reg, shift, mask, func, base}
+
+struct mt6360_pdata_prop {
+	size_t offset;
+	u8 reg;
+	u8 shift;
+	u8 mask;
+	u32 (*transform)(u32 val);
+	u8 base;
+};
+
+static inline int mt6360_pdata_apply_helper(void *context, void *pdata,
+					   const struct mt6360_pdata_prop *prop,
+					   int prop_cnt)
+{
+	int i, ret;
+	u32 val;
+
+	for (i = 0; i < prop_cnt; i++) {
+		val = *(u32 *)(pdata + prop[i].offset);
+		if (prop[i].transform)
+			val = prop[i].transform(val);
+		val += prop[i].base;
+		ret = regmap_update_bits(context,
+			     prop[i].reg, prop[i].mask, val << prop[i].shift);
+		if (ret < 0)
+			return ret;
+	}
+	return 0;
+}
 
 #endif /* __MT6360_PRIVATE_H__ */
