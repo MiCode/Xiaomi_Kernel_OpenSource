10660dc8318310ecf22325c05754d394c6340bfb
Deepak Kumar <dkumar@codeaurora.org>
2019-05-07 15:28:41 +0530

    msm: kgsl: Access dma_buf_attachment pointer only if its valid
    dma_buf_attach can return a negative error code wrapped
    into a pointer on failure. Correct the error check and move
    access to dma_buf_attachment pointer after the error check
    to avoid accessing invalid pointer.
    
    Change-Id: I77426fefb17370da5ed57878632bec8bacbc77a0
    Signed-off-by: Deepak Kumar <dkumar@codeaurora.org>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -2659,6 +2659,12 @@ static int kgsl_setup_dma_buf(struct kgsl_device *device,
 		return -ENOMEM;
 
 	attach = dma_buf_attach(dmabuf, device->dev);
+
+	if (IS_ERR(attach)) {
+		ret = PTR_ERR(attach);
+		goto out;
+	}
+
 	/*
 	 * If dma buffer is marked IO coherent, skip sync at attach,
 	 * which involves flushing the buffer on CPU.
@@ -2666,11 +2672,6 @@ static int kgsl_setup_dma_buf(struct kgsl_device *device,
 	 */
 	if (entry->memdesc.flags & KGSL_MEMFLAGS_IOCOHERENT)
 		attach->dma_map_attrs |= DMA_ATTR_SKIP_CPU_SYNC;
-
-	if (IS_ERR_OR_NULL(attach)) {
-		ret = attach ? PTR_ERR(attach) : -EINVAL;
-		goto out;
-	}
 
 	meta->dmabuf = dmabuf;
 	meta->attach = attach;
