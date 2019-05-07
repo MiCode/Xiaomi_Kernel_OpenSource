f7e86ee5998aa463df398eb716a3537a56d1b406
Sahil Chandna <chandna@codeaurora.org>
2019-05-07 19:19:57 +0530

    power: smb5: Add support for PPS constant current(CC) mode
    Add support for PPS CC mode charging for dual charge pumps
    with MID-VBAT configuration.
    
    If a charging adapter is put in constant current (CC) mode during charging,
    the supply provides the requested constant current. As a result, its output
    voltage is automatically adjusted according to the load, and falls into
    charge pump operational range.
    
    For CC mode add an ability to configure the ICL and main-charger's FCC
    current using power-supply properties.
    
    Use POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT to report the total
    FCC which can be pumped into the battery at that point in time.
    
    On charger removal, reset all the votes casted during CC mode.
    
    Change-Id: I242e92b87753d962289c94eedb054b2e8b67b99d
    Signed-off-by: Sahil Chandna <chandna@codeaurora.org>

--- drivers/power/supply/qcom/smb5-lib.h
+++ drivers/power/supply/qcom/smb5-lib.h
@@ -80,6 +80,7 @@ enum print_reason {
 #define CHARGER_TYPE_VOTER		"CHARGER_TYPE_VOTER"
 #define HDC_IRQ_VOTER			"HDC_IRQ_VOTER"
 #define DETACH_DETECT_VOTER		"DETACH_DETECT_VOTER"
+#define CC_MODE_VOTER			"CC_MODE_VOTER"
 #define MAIN_FCC_VOTER			"MAIN_FCC_VOTER"
 
 #define BOOST_BACK_STORM_COUNT	3
@@ -228,6 +229,17 @@ enum chg_term_config_src {
 	ITERM_SRC_UNSPECIFIED,
 	ITERM_SRC_ADC,
 	ITERM_SRC_ANALOG
+};
+
+enum comp_clamp_levels {
+	CLAMP_LEVEL_DEFAULT = 0,
+	CLAMP_LEVEL_1,
+	MAX_CLAMP_LEVEL,
+};
+
+struct clamp_config {
+	u16 reg[3];
+	u16 val[3];
 };
 
 struct smb_irq_info {
@@ -399,6 +411,9 @@ struct smb_charger {
 	/* parallel charging */
 	struct parallel_params	pl;
 
+	/* CC Mode */
+	int	adapter_cc_mode;
+
 	/* regulators */
 	struct smb_regulator	*vbus_vreg;
 	struct smb_regulator	*vconn_vreg;
@@ -528,6 +543,7 @@ struct smb_charger {
 	int			dr_mode;
 	int			usbin_forced_max_uv;
 	int			init_thermal_ua;
+	u32			comp_clamp_level;
 
 	/* workaround flag */
 	u32			wa_flags;
