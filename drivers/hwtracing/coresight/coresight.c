e1d69fe9a945f238877e8cf836633dff2833489d
Yuanfang Zhang <zhangyuanfang@codeaurora.org>
2019-05-08 18:17:21 +0800

    Coresight-cti: add support to enable/disable multiple clks and regulators
    Some CTIs can go into power collapse when accessing CTI's register space,
    hence require a bunch of clks and regulators to be configured. This change
    add support in CIT driver to enable them when CTI is used for first time
    and disable them when CTI isn't used.
    
    Change-Id: Ia8a54cbc0f8363d19811c3a0712f5b8c23530b32
    Signed-off-by: Yuanfang Zhang <zhangyuanfang@codeaurora.org>
    Signed-off-by: Mao Jinlong <jinlmao@codeaurora.org>

--- drivers/hwtracing/coresight/coresight.c
+++ drivers/hwtracing/coresight/coresight.c
@@ -117,14 +117,14 @@ static void coresight_reset_all_sink(void)
 	bus_for_each_dev(&coresight_bustype, NULL, NULL, coresight_reset_sink);
 }
 
-void coresight_enable_reg_clk(struct coresight_device *csdev)
+int coresight_enable_reg_clk(struct coresight_device *csdev)
 {
 	struct coresight_reg_clk *reg_clk = csdev->reg_clk;
 	int ret;
 	int i, j;
 
 	if (IS_ERR_OR_NULL(reg_clk))
-		return;
+		return -EINVAL;
 
 	for (i = 0; i < reg_clk->nr_reg; i++) {
 		ret = regulator_enable(reg_clk->reg[i]);
@@ -138,7 +138,7 @@ void coresight_enable_reg_clk(struct coresight_device *csdev)
 			goto err_clks;
 	}
 
-	return;
+	return 0;
 
 err_clks:
 	for (j--; j >= 0; j--)
@@ -146,6 +146,8 @@ err_clks:
 err_regs:
 	for (i--; i >= 0; i--)
 		regulator_disable(reg_clk->reg[i]);
+
+	return ret;
 }
 EXPORT_SYMBOL(coresight_enable_reg_clk);
 
