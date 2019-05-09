07c3bd655dcce6f8d7f6deb2c82efd10e4333638
Huacai Chen <chenhc@lemote.com>
2019-05-09 16:25:36 +0800

    [ALPS04421318] UPSTREAM: kallsyms: let print_ip_sym() print raw addresses
    print_ip_sym() is mostly used for debugging, so I think it should print
    the raw addresses.
    
    MTK-Commit-Id: b7c3f5542347b0fc6401166d1b449fcd0be7b87e
    
    Change-Id: I32e8448b49ac9e816fc295bab612047184f437e4
    Link: http://lkml.kernel.org/r/1514519382-405-1-git-send-email-chenhc@lemote.com
    Signed-off-by: Huacai Chen <chenhc@lemote.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Fuxin Zhang <zhangfx@lemote.com>
    Cc: "Tobin C. Harding" <me@tobin.cc>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: chunhui.li <chunhui.li@mediatek.com>
    CR-Id: ALPS04421318
    Feature: [Module]Official Kernel Patch

--- include/linux/kallsyms.h
+++ include/linux/kallsyms.h
@@ -123,7 +123,7 @@ static inline void print_symbol(const char *fmt, unsigned long addr)
 
 static inline void print_ip_sym(unsigned long ip)
 {
-	printk("[<%p>] %pS\n", (void *) ip, (void *) ip);
+	printk("[<%px>] %pS\n", (void *) ip, (void *) ip);
 }
 
 #endif /*_LINUX_KALLSYMS_H*/
