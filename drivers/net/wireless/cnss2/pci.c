df93accff091835c3b0298ea9b1624bfe7bb7c54
Yue Ma <yuem@codeaurora.org>
2019-05-15 17:25:50 -0700

    cnss2: Set no_d3hot flag to avoid PCIe framework saving config space
    CNSS driver is already saving PCIe config space as part of suspend and
    resume. There is no need for PCIe framework to do it again. This is
    able to improve suspend/resume KPI.
    
    Change-Id: I940959ec93c435b9fd711c5e75a8bbc40ec916ad
    Signed-off-by: Yue Ma <yuem@codeaurora.org>

--- drivers/net/wireless/cnss2/pci.c
+++ drivers/net/wireless/cnss2/pci.c
@@ -2583,9 +2583,6 @@ static int cnss_pci_suspend_noirq(struct device *dev)
 	if (driver_ops && driver_ops->suspend_noirq)
 		ret = driver_ops->suspend_noirq(pci_dev);
 
-	if (pci_priv->disable_pc && !pci_dev->state_saved)
-		pci_save_state(pci_dev);
-
 out:
 	return ret;
 }
@@ -4401,6 +4398,9 @@ static int cnss_pci_probe(struct pci_dev *pci_dev,
 		goto out;
 	}
 
+#ifdef CONFIG_PCI_QTI
+	pci_dev->no_d3hot = true;
+#endif
 	pci_priv->pci_link_state = PCI_LINK_UP;
 	pci_priv->plat_priv = plat_priv;
 	pci_priv->pci_dev = pci_dev;
