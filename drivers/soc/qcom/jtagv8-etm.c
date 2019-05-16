abd7b06cb2669f4376461f3df3763500c5968d00
Divya Sharma <divyash@codeaurora.org>
2019-05-16 10:12:41 -0700

    Revert "soc: qcom: skip ETM save/restore when ETM is disabled"
    This reverts commit d17a9f41fd5b641d834d45a3303e8074ca9b0090.
    
    Change-Id: If82196ab521ff58e4369a88b60dac2e5a1845764
    Signed-off-by: Divya Sharma <divyash@codeaurora.org>

--- drivers/soc/qcom/jtagv8-etm.c
+++ drivers/soc/qcom/jtagv8-etm.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2014-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014-2018, The Linux Foundation. All rights reserved.
  *
  */
 
@@ -177,13 +177,8 @@
 #define HW_SOC_ID_M8953		(293)
 #define GET_FEAT_VERSION_CMD	3
 
-/* spread out etm register write */
 #define etm_writel(etm, val, off)	\
-do {							\
-	writel_relaxed_no_log(val, etm->base + off);	\
-	udelay(20);					\
-} while (0)
-
+		   writel_relaxed_no_log(val, etm->base + off)
 #define etm_writel_log(etm, val, off)	\
 		   __raw_writel(val, etm->base + off)
 
@@ -309,12 +304,6 @@ static inline void etm_mm_save_state(struct etm_ctx *etmdata)
 			pr_err_ratelimited("programmers model is not stable\n"
 					   );
 
-		etmdata->state[i++] = etm_readl(etmdata, TRCPRGCTLR);
-		if (!(etmdata->state[0] & BIT(0))) {
-			atomic_notifier_call_chain(&etm_save_notifier_list,
-							0, NULL);
-			break;
-		}
 		/* main control and configuration registers */
 		etmdata->state[i++] = etm_readl(etmdata, TRCPROCSELR);
 		etmdata->state[i++] = etm_readl(etmdata, TRCCONFIGR);
@@ -380,6 +369,8 @@ static inline void etm_mm_save_state(struct etm_ctx *etmdata)
 		}
 		/* claim tag registers */
 		etmdata->state[i++] = etm_readl(etmdata, TRCCLAIMCLR);
+		/* program ctrl register */
+		etmdata->state[i++] = etm_readl(etmdata, TRCPRGCTLR);
 
 		/* ensure trace unit is idle to be powered down */
 		for (count = TIMEOUT_US; (BVAL(etm_readl(etmdata, TRCSTATR), 0)
@@ -417,10 +408,6 @@ static inline void etm_mm_restore_state(struct etm_ctx *etmdata)
 			etm_os_lock(etmdata);
 		}
 
-		if (!(etmdata->state[0] & BIT(0))) {
-			etm_os_unlock(etmdata);
-			break;
-		}
 		/* main control and configuration registers */
 		etm_writel(etmdata, etmdata->state[i++], TRCPROCSELR);
 		etm_writel(etmdata, etmdata->state[i++], TRCCONFIGR);
@@ -486,7 +473,7 @@ static inline void etm_mm_restore_state(struct etm_ctx *etmdata)
 		/* claim tag registers */
 		etm_writel(etmdata, etmdata->state[i++], TRCCLAIMSET);
 		/* program ctrl register */
-		etm_writel(etmdata, etmdata->state[0], TRCPRGCTLR);
+		etm_writel(etmdata, etmdata->state[i++], TRCPRGCTLR);
 
 		etm_os_unlock(etmdata);
 		break;
