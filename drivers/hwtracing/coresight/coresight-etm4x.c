1f0a3ef77c1740ba5c6f2a4037e0946dc846c470
Ivaylo Georgiev <irgeorgiev@codeaurora.org>
2019-05-16 04:36:48 -0700

    Revert "coresight: etm4x: Add support to enable ETMv4.2"
    This reverts commit 2636ccec991e520a3f1969a89e2d8ed1d46aa72d.
    
    This is a preparation change for merging android-4.19.34 into
    msm-4.19 branch.
    
    Equivalent logic was already submitted into msm-4.19 and this
    change affects creation of coresight-etm sysfs nodes.
    
    Change-Id: Ic65c1f4707fef033e322e17db8522b6a96cf4e67
    Signed-off-by: Ivaylo Georgiev <irgeorgiev@codeaurora.org>

--- drivers/hwtracing/coresight/coresight-etm4x.c
+++ drivers/hwtracing/coresight/coresight-etm4x.c
@@ -54,8 +54,7 @@ static void etm4_os_unlock(struct etmv4_drvdata *drvdata)
 
 static bool etm4_arch_supported(u8 arch)
 {
-	/* Mask out the minor version number */
-	switch (arch & 0xf0) {
+	switch (arch) {
 	case ETM_ARCH_V4:
 		break;
 	default:
