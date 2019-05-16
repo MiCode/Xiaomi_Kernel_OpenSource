30de2f5b0b9f7b2d28cfe7a45db4d83ef8221923
Chen-Yao.Chang <chen-yao.chang@mediatek.com>
2019-05-16 19:55:34 +0800

    boot: add boot time profiling logger
    bootprof logger provide interface for recording booting
    key timestamp form kernel and user space.
    
    Change-Id: I81208e45842dec93310f3f40a11b70979dcc00ac
    CR-Id: ALPS04470599
    Feature: [Module]Boot Time Profiling
    Signed-off-by: Chen-Yao.Chang <chen-yao.chang@mediatek.com>

---
+++ include/linux/bootprof.h
@@ -0,0 +1,44 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+/*
+ *  boot logger: drivers/misc/mtprof/bootprof
+ * interface: /proc/bootprof
+ */
+#ifndef _BOOTPROF_H_
+#define _BOOTPROF_H_
+
+#ifdef CONFIG_MTPROF
+#include <linux/platform_device.h>
+#include <linux/sched/clock.h>
+
+#ifndef BOOTPROF_TIME_LOG_START
+#define BOOTPROF_TIME_LOG_START(ts) \
+	({ts = sched_clock(); })
+#endif
+
+#ifndef BOOTPROF_TIME_LOG_END
+#define BOOTPROF_TIME_LOG_END(ts) \
+	({ts = sched_clock() - ts; })
+#endif
+
+void bootprof_initcall(initcall_t fn, unsigned long long ts);
+void bootprof_probe(unsigned long long ts, struct device *dev,
+		    struct device_driver *drv, unsigned long probe);
+void bootprof_pdev_register(unsigned long long ts,
+			    struct platform_device *pdev);
+
+void bootprof_log_boot(char *str);
+
+#else /*CONFIG_MTPROF*/
+#define BOOTPROF_TIME_LOG_START(ts)
+#define BOOTPROF_TIME_LOG_END(ts)
+#define bootprof_probe(ts, dev, drv, probe)
+#define bootprof_pdev_register(ts, pdev)
+#define bootprof_initcall(fn, ts)
+#define bootprof_log_boot(str)
+#endif /*CONFIG_MTPROF*/
+
+#endif /*_BOOTPROF_H_*/
