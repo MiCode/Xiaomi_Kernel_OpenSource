174f91b27ca98d4089eeb2171cbb25c6ebfac221
Alexandre Courbot <acourbot@chromium.org>
2019-05-17 17:02:16 +0900

    media: mtk-vcodec: vdec: support stateless H.264 decoding
    Add the firmware interface allowing to decode H.264 in a stateless
    manner.
    
    Change-Id: I2bb4e8bfb07a602b27eeda4db2f6090e7daeda7b
    Signed-off-by: Alexandre Courbot <acourbot@chromium.org>
    Signed-off-by: Yunfei Dong <yunfei.dong@mediatek.com>
    CR-Id: ALPS04383538
    Feature: Video Framework

--- drivers/media/platform/mtk-vcodec/vdec_drv_if.c
+++ drivers/media/platform/mtk-vcodec/vdec_drv_if.c
@@ -25,12 +25,16 @@
 extern const struct vdec_common_if vdec_h264_if;
 extern const struct vdec_common_if vdec_vp8_if;
 extern const struct vdec_common_if vdec_vp9_if;
+extern const struct vdec_common_if vdec_h264_slice_if;
 
 int vdec_if_init(struct mtk_vcodec_ctx *ctx, unsigned int fourcc)
 {
 	int ret = 0;
 
 	switch (fourcc) {
+	case V4L2_PIX_FMT_H264_SLICE:
+		ctx->dec_if = &vdec_h264_slice_if;
+		break;
 	case V4L2_PIX_FMT_H264:
 		ctx->dec_if = &vdec_h264_if;
 		break;
