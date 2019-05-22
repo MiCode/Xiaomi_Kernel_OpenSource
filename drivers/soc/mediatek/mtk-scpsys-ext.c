c3c12419c7720b83545214feee70d7d302238bd9
mtk04593 <yy.huang@mediatek.com>
2019-05-22 15:20:00 +0800

    soc: mediatek: add way_en bus protection
    In few recent Mediatek H/W designs(e.g. MT6779), there are "way_en"
    bus protection which has different operation comparing to original bus
    protection operation.
    
    In power on cases, for example, the logic of original bus protections
    are "clear and poll". However, the way_en bus protection is "set and poll"
    reversly. And such reversed situation also happens in power off cases
    similarly.
    
    Feature:
    Change-Id: Id03166b269b4a21d432f8bed1e948c01ce42845f
    Signed-off-by: mtk04593 <yy.huang@mediatek.com>
    CR-Id: ALPS04416165

--- drivers/soc/mediatek/mtk-scpsys-ext.c
+++ drivers/soc/mediatek/mtk-scpsys-ext.c
@@ -12,7 +12,7 @@
 #define MTK_POLL_DELAY_US   10
 #define MTK_POLL_TIMEOUT    USEC_PER_SEC
 
-static int set_and_poll(struct regmap *map, struct regmap *map_ack, u32 mask,
+static int enable_way_en(struct regmap *map, struct regmap *ack, u32 mask,
 				u32 ack_mask, u32 reg_set, u32 reg_sta,
 				u32 reg_en)
 {
@@ -23,12 +23,12 @@ static int set_and_poll(struct regmap *map, struct regmap *map_ack, u32 mask,
 	else
 		regmap_update_bits(map, reg_en, mask, mask);
 
-	return regmap_read_poll_timeout(map_ack, reg_sta,
+	return regmap_read_poll_timeout(ack, reg_sta,
 			val, (val & ack_mask) == ack_mask,
 			MTK_POLL_DELAY_US, MTK_POLL_TIMEOUT);
 }
 
-static int clear_and_poll(struct regmap *map, struct regmap *map_ack, u32 mask,
+static int disable_way_en(struct regmap *map, struct regmap *ack, u32 mask,
 				u32 ack_mask, u32 reg_clr, u32 reg_sta,
 				u32 reg_en)
 {
@@ -39,37 +39,39 @@ static int clear_and_poll(struct regmap *map, struct regmap *map_ack, u32 mask,
 	else
 		regmap_update_bits(map, reg_en, mask, 0);
 
-	return regmap_read_poll_timeout(map_ack, reg_sta,
-			val, !(val & ack_mask),
+	return regmap_read_poll_timeout(ack, reg_sta,
+			val, (val & ack_mask) == ack_mask,
 			MTK_POLL_DELAY_US, MTK_POLL_TIMEOUT);
 }
 
-static int enable_way_en(struct regmap *map_set, struct regmap *map_ack,
-				const struct bus_prot *bp)
+static int set_bus_protection(struct regmap *map, u32 mask, u32 ack_mask,
+				u32 reg_set, u32 reg_sta, u32 reg_en)
 {
-	return set_and_poll(map_set, map_ack, bp->mask, bp->set_ack_mask,
-			bp->set_ofs, bp->sta_ofs, bp->en_ofs);
+	u32 val;
+
+	if (reg_set)
+		regmap_write(map, reg_set, mask);
+	else
+		regmap_update_bits(map, reg_en, mask, mask);
+
+	return regmap_read_poll_timeout(map, reg_sta,
+			val, (val & ack_mask) == ack_mask,
+			MTK_POLL_DELAY_US, MTK_POLL_TIMEOUT);
 }
 
-static int disable_way_en(struct regmap *map_clr, struct regmap *map_ack,
-				const struct bus_prot *bp)
+static int clear_bus_protection(struct regmap *map, u32 mask, u32 ack_mask,
+				u32 reg_clr, u32 reg_sta, u32 reg_en)
 {
-	return clear_and_poll(map_clr, map_ack, bp->mask, bp->clr_ack_mask,
-			bp->clr_ofs, bp->sta_ofs, bp->en_ofs);
-}
+	u32 val;
 
-static int set_bus_protection(struct regmap *map,
-				const struct bus_prot *bp)
-{
-	return set_and_poll(map, map, bp->mask, bp->set_ack_mask, bp->set_ofs,
-			bp->sta_ofs, bp->en_ofs);
-}
+	if (reg_clr)
+		regmap_write(map, reg_clr, mask);
+	else
+		regmap_update_bits(map, reg_en, mask, 0);
 
-static int clear_bus_protection(struct regmap *map,
-			const struct bus_prot *bp)
-{
-	return clear_and_poll(map, map, bp->mask, bp->clr_ack_mask, bp->clr_ofs,
-			bp->sta_ofs, bp->en_ofs);
+	return regmap_read_poll_timeout(map, reg_sta,
+			val, !(val & ack_mask),
+			MTK_POLL_DELAY_US, MTK_POLL_TIMEOUT);
 }
 
 int mtk_scpsys_ext_set_bus_protection(const struct bus_prot *bp_table,
@@ -83,16 +85,24 @@ int mtk_scpsys_ext_set_bus_protection(const struct bus_prot *bp_table,
 
 		switch (bp_table[i].type) {
 		case IFR_TYPE:
-			ret = set_bus_protection(infracfg,
-						&bp_table[i]);
+			ret = set_bus_protection(infracfg, bp_table[i].mask,
+					bp_table[i].mask, bp_table[i].set_ofs,
+					bp_table[i].sta_ofs,
+					bp_table[i].en_ofs);
 			break;
 		case SMI_TYPE:
-			ret = set_bus_protection(smi_common,
-						&bp_table[i]);
+			ret = set_bus_protection(smi_common, bp_table[i].mask,
+					bp_table[i].mask, bp_table[i].set_ofs,
+					bp_table[i].sta_ofs,
+					bp_table[i].en_ofs);
 			break;
 		case IFR_WAYEN_TYPE:
 			ret = disable_way_en(infracfg, infracfg_nao,
-						&bp_table[i]);
+					bp_table[i].mask,
+					bp_table[i].clr_ack_mask,
+					bp_table[i].clr_ofs,
+					bp_table[i].sta_ofs,
+					bp_table[i].en_ofs);
 			break;
 		default:
 			return -EINVAL;
@@ -116,16 +126,27 @@ int mtk_scpsys_ext_clear_bus_protection(const struct bus_prot *bp_table,
 
 		switch (bp_table[i].type) {
 		case IFR_TYPE:
-			ret = clear_bus_protection(infracfg,
-							&bp_table[i]);
+			ret = clear_bus_protection(infracfg, bp_table[i].mask,
+						bp_table[i].clr_ack_mask,
+						bp_table[i].clr_ofs,
+						bp_table[i].sta_ofs,
+						bp_table[i].en_ofs);
 			break;
 		case SMI_TYPE:
 			ret = clear_bus_protection(smi_common,
-							&bp_table[i]);
+						bp_table[i].mask,
+						bp_table[i].clr_ack_mask,
+						bp_table[i].clr_ofs,
+						bp_table[i].sta_ofs,
+						bp_table[i].en_ofs);
 			break;
 		case IFR_WAYEN_TYPE:
 			ret = enable_way_en(infracfg, infracfg_nao,
-							&bp_table[i]);
+						bp_table[i].mask,
+						bp_table[i].set_ack_mask,
+						bp_table[i].set_ofs,
+						bp_table[i].sta_ofs,
+						bp_table[i].en_ofs);
 			break;
 		default:
 			return -EINVAL;
