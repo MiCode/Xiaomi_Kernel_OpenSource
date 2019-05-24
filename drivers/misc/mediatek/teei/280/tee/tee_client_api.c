e04f980e2913a4b47ec78807724b529322377a14
Calvin Liao <calvin.liao@mediatek.com>
2019-05-24 15:44:27 +0800

    [ALPS04179492] TEEI: SVP
    Modify TTA/Shared-Memory/SSVP settings.
    Enable I2C API for imagesensor.
    
    [ALPS04179492] TEEI: SVP
    [ALPS04179511] TEEI: Add I2C API
    
    MTK-Commit-Id: f26545a0ae4be14f7798941b446d35423d89c48e
    
    Change-Id: I5519828e07ee6371a2a75e80f8802a1abe3930f2
    Signed-off-by: Calvin Liao <calvin.liao@mediatek.com>
    CR-Id: ALPS04179492
    Feature: Microtrust TEEI
    (cherry picked from commit 6e120d89ca9a4fe86918fe9872f2f8ddc16f540a)

--- drivers/misc/mediatek/teei/280/tee/tee_client_api.c
+++ drivers/misc/mediatek/teei/280/tee/tee_client_api.c
@@ -21,6 +21,7 @@
 #include <linux/file.h>
 #include <tee_client_api.h>
 #include <linux/mutex.h>
+#include <linux/cred.h>
 
 #include <linux/uaccess.h>
 #include <linux/mman.h>
@@ -33,6 +34,7 @@
 /* How many device sequence numbers will be tried before giving up */
 #define TEEC_MAX_DEV_SEQ	10
 #define DEFAULT_CAPABILITY "bta_loader"
+#define GPTEE_CAPABILITY "tta"
 
 static inline long ioctl(struct file *filp, unsigned int cmd, void *arg)
 {
@@ -131,6 +133,24 @@ TEEC_Result TEEC_InitializeContext(const char *name, struct TEEC_Context *ctx)
 
 	if (!ctx)
 		return TEEC_ERROR_BAD_PARAMETERS;
+
+#ifndef DEFAULT_TEE_GPTEE
+	if (!name)
+		name = (char *)DEFAULT_CAPABILITY;
+	else
+		if (strcmp(name, GPTEE_CAPABILITY) == 0)
+			name = (char *)GPTEE_CAPABILITY;
+		else
+			name = (char *)DEFAULT_CAPABILITY;
+#else
+	if (!name)
+		name = (char *)GPTEE_CAPABILITY;
+	else
+		if (strcmp(name, DEFAULT_CAPABILITY) == 0)
+			name = (char *)DEFAULT_CAPABILITY;
+		else
+			name = (char *)GPTEE_CAPABILITY;
+#endif
 
 	for (n = 0; n < TEEC_MAX_DEV_SEQ; n++) {
 		snprintf(devname, sizeof(devname), "/dev/tee%zu", n);
