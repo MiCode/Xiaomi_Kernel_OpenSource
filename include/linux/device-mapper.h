6b688c3c6be2961087a2e11d0c06017e980d9208
Blagovest Kolenichev <bkolenichev@codeaurora.org>
2019-05-27 04:06:44 -0700

    dm: Restore reverted changes
    This is a preparation change for merging android-4.14-q.133 into
    msm-4.14 branch.
    
    The following changes need to be reverted, otherwise boot
    stuck on rootfs mount:
    
    ba7dee4c76 Revert "CHROMIUM: dm: boot time specification of dm="
    cf193eee67 Revert "ANDROID: dm: do_mounts_dm: Rebase on top of 4.9"
    13c4f91f5a Revert "ANDROID: dm: do_mounts_dm: fix dm_substitute_devices()"
    321a9efa6f Revert "ANDROID: dm: do_mounts_dm: Update init/do_mounts_dm.c to the latest ChromiumOS version."
    
    The above reverted changes are required when 'system is root'
    and the mount instructions (including dm-verity) for system
    image is provided by the bootloader through the kernel cmdline
    even with AVB 2.0. Dynamic/Super partition has not been enabled
    internally which will ensure system image is mounted by early
    init. Since this will only be supported for new launches of Q,
    still these changes are required to be present for OTA devices.
    
    Change-Id: Ie6c093cdd2d1dc1e578ebce06b3923f81c06ed6f
    Signed-off-by: Blagovest Kolenichev <bkolenichev@codeaurora.org>

--- include/linux/device-mapper.h
+++ include/linux/device-mapper.h
@@ -431,6 +431,12 @@ void dm_set_mdptr(struct mapped_device *md, void *ptr);
 void *dm_get_mdptr(struct mapped_device *md);
 
 /*
+ * Export the device via the ioctl interface (uses mdptr).
+ */
+int dm_ioctl_export(struct mapped_device *md, const char *name,
+		    const char *uuid);
+
+/*
  * A device can still be used while suspended, but I/O is deferred.
  */
 int dm_suspend(struct mapped_device *md, unsigned suspend_flags);
@@ -458,6 +464,13 @@ void dm_remap_zone_report(struct dm_target *ti, struct bio *bio,
 union map_info *dm_get_rq_mapinfo(struct request *rq);
 
 struct queue_limits *dm_get_queue_limits(struct mapped_device *md);
+
+void dm_lock_md_type(struct mapped_device *md);
+void dm_unlock_md_type(struct mapped_device *md);
+void dm_set_md_type(struct mapped_device *md, unsigned type);
+unsigned dm_get_md_type(struct mapped_device *md);
+int dm_setup_md_queue(struct mapped_device *md, struct dm_table *t);
+unsigned dm_table_get_type(struct dm_table *t);
 
 /*
  * Geometry functions.
