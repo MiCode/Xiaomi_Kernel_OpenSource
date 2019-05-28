2205fcb43ffbd99981671d79c8fb50956fee0d59
Venkat Chinta <vchinta@codeaurora.org>
2019-05-28 11:47:28 -0700

    msm: camera: isp: Add debugfs parameter for state monitor dump
    This change adds a debugfs parameter to enable isp context state
    monitor dump rather than always print by default. This change
    also changes isp context substate to HALT only after stop hw
    is complete.
    
    Change-Id: I1025a46e9be63b4ee5808c2eb44672687d901c2e
    Signed-off-by: Venkat Chinta <vchinta@codeaurora.org>

--- drivers/media/platform/msm/camera/cam_isp/cam_isp_context.h
+++ drivers/media/platform/msm/camera/cam_isp/cam_isp_context.h
@@ -71,6 +71,18 @@ enum cam_isp_state_change_trigger {
 };
 
 /**
+ * struct cam_isp_ctx_debug -  Contains debug parameters
+ *
+ * @dentry:                    Debugfs entry
+ * @enable_state_monitor_dump: Enable isp state monitor dump
+ *
+ */
+struct cam_isp_ctx_debug {
+	struct dentry  *dentry;
+	uint32_t        enable_state_monitor_dump;
+};
+
+/**
  * struct cam_isp_ctx_irq_ops - Function table for handling IRQ callbacks
  *
  * @irq_ops:               Array of handle function pointers.
