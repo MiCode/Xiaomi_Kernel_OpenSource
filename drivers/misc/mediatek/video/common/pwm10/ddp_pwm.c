d27af16dd115d3751f4b9f8d92d796c5b78663ca
Joey Pan <joey.pan@mediatek.com>
2019-06-09 13:55:04 +0800

    [ALPS04329593] DISP:Sync display driver setting
    Because the default setting of display driver is not
    matched that on Android P, we sync all the display
    driver settings here.
    
    MTK-Commit-Id: c03099fd106742d2421af930bc7ca90648d7eec1
    
    Change-Id: If8f40525ab239b7a00ee3ac416bd8adbea525aec
    Signed-off-by: Joey Pan <joey.pan@mediatek.com>
    CR-Id: ALPS04329593
    Feature: [Module]Display Driver

--- drivers/misc/mediatek/video/common/pwm10/ddp_pwm.c
+++ drivers/misc/mediatek/video/common/pwm10/ddp_pwm.c
@@ -29,7 +29,9 @@
 	defined(CONFIG_MACH_MT6739) || defined(CONFIG_MACH_MT6758) || \
 	defined(CONFIG_MACH_MT6765) || defined(CONFIG_MACH_MT6761) || \
 	defined(CONFIG_MACH_MT6771) || defined(CONFIG_MACH_MT3967) || \
-	defined(CONFIG_MACH_MT8168)
+	defined(CONFIG_MACH_MT6768) || defined(CONFIG_MACH_MT8168) || \
+	defined(CONFIG_MACH_MT6885)
+
 #include <ddp_clkmgr.h>
 #endif
 #endif
@@ -38,7 +40,9 @@
 #include <disp_dts_gpio.h> /* DTS GPIO */
 #if defined(CONFIG_MACH_MT6758) || defined(CONFIG_MACH_MT6765) || \
 	defined(CONFIG_MACH_MT6761) || defined(CONFIG_MACH_MT6771) || \
-	defined(CONFIG_MACH_MT3967)
+	defined(CONFIG_MACH_MT3967) || defined(CONFIG_MACH_MT6768) || \
+	defined(CONFIG_MACH_MT6885)
+
 #include <mtk_leds_drv.h>
 #include <mtk_leds_sw.h>
 #else
@@ -641,7 +645,9 @@ static int ddp_pwm_power_on(enum DISP_MODULE_ENUM module, void *handle)
 
 #if defined(CONFIG_MACH_MT6759) || defined(CONFIG_MACH_MT6758) || \
 	defined(CONFIG_MACH_MT6739) || defined(CONFIG_MACH_MT6765) || \
-	defined(CONFIG_MACH_MT6761) || defined(CONFIG_MACH_MT8168)
+	defined(CONFIG_MACH_MT6761) || defined(CONFIG_MACH_MT6768) || \
+	defined(CONFIG_MACH_MT8168) || defined(CONFIG_MACH_MT6885)
+
 	/* pwm ccf api */
 	ddp_clk_prepare_enable(ddp_get_module_clk_id(module));
 #elif defined(CONFIG_MACH_MT6763) || defined(CONFIG_MACH_MT6771)
@@ -699,7 +705,9 @@ static int ddp_pwm_power_off(enum DISP_MODULE_ENUM module, void *handle)
 
 #if defined(CONFIG_MACH_MT6759) || defined(CONFIG_MACH_MT6758) || \
 	defined(CONFIG_MACH_MT6739) || defined(CONFIG_MACH_MT6765) || \
-	defined(CONFIG_MACH_MT6761) || defined(CONFIG_MACH_MT8168)
+	defined(CONFIG_MACH_MT6761) || defined(CONFIG_MACH_MT6768) || \
+	defined(CONFIG_MACH_MT8168) || defined(CONFIG_MACH_MT6885)
+
 	/* pwm ccf api */
 	ddp_clk_disable_unprepare(ddp_get_module_clk_id(module));
 #elif defined(CONFIG_MACH_MT6763) || defined(CONFIG_MACH_MT6771)
@@ -780,7 +788,8 @@ bool disp_pwm_is_osc(void)
 	defined(CONFIG_MACH_MT6763) || defined(CONFIG_MACH_MT6739) || \
 	defined(CONFIG_MACH_MT6758) || defined(CONFIG_MACH_MT6765) || \
 	defined(CONFIG_MACH_MT6761) || defined(CONFIG_MACH_MT6771) || \
-	defined(CONFIG_MACH_MT3967) || defined(CONFIG_MACH_MT8168)
+	defined(CONFIG_MACH_MT3967) || defined(CONFIG_MACH_MT6768) || \
+	defined(CONFIG_MACH_MT8168) || defined(CONFIG_MACH_MT6885)
 
 	is_osc = disp_pwm_mux_is_osc();
 #endif
