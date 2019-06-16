f5cbd9a4aff9f958884cf15ebe5229c61784b88e
Tzahi Sabo <stzahi@codeaurora.org>
2019-06-16 10:26:02 +0300

    wil6210: clear FW and ucode log address
    Clear the FW and ucode log address on device initialization to allow
    user space app identify when the address was set by FW/ucode and it can
    start read.
    
    Change-Id: Iefa7a9f7ae00dec2ab085972cca79bc009ad4e05
    Signed-off-by: Tzahi Sabo <stzahi@codeaurora.org>
    Signed-off-by: Maya Erez <merez@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Git-commit: f2b6b46e483b9a084088853ecc4123118791829e
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/kvalo/ath.git
    Signed-off-by: Maya Erez <merez@codeaurora.org>

--- drivers/net/wireless/ath/wil6210/wil6210.h
+++ drivers/net/wireless/ath/wil6210/wil6210.h
@@ -1490,4 +1490,5 @@ int wmi_addba_rx_resp_edma(struct wil6210_priv *wil, u8 mid, u8 cid,
 void update_supported_bands(struct wil6210_priv *wil);
 int wmi_reset_spi_slave(struct wil6210_priv *wil);
 
+void wil_clear_fw_log_addr(struct wil6210_priv *wil);
 #endif /* __WIL6210_H__ */
