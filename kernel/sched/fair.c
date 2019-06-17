b95f2ce745465ae45c5bb0b22b86755331448dad
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-06-17 15:33:34 +0530

    sched/fair: Prevent tick path active migration to the same CPU
    The misfit task running on the lower capacity CPUs are migrated
    to higher capacity CPUs from the tick path. It is not guaranteed
    that find_energy_efficient_cpu() returns a higher capacity CPU
    for the tick path active migration. It may return the previous
    CPU or another CPU that has the same capacity as the previous CPU.
    There is an explicit check in check_for_migration() to allow the
    migration from lower capacity CPU to higher capacity CPU. However
    there is a remote possibility of previous CPU capacity being changed
    at the same time and we end up queueing the active migration work
    on the previous CPU. This results in a kernel panic later when the
    migration work actually runs. Prevent this by explicitly checking
    the new_cpu against the previous CPU.
    
    Change-Id: I20b25799f388ff609bff7390cd280cf17790adb7
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -13016,13 +13016,13 @@ void check_for_migration(struct rq *rq, struct task_struct *p)
 		rcu_read_lock();
 		new_cpu = find_energy_efficient_cpu(sd, p, cpu,	prev_cpu, 0);
 		rcu_read_unlock();
-		if ((new_cpu != -1) &&
-			(capacity_orig_of(new_cpu) > capacity_orig_of(cpu))) {
+		if ((new_cpu != prev_cpu) && (capacity_orig_of(new_cpu) >
+					capacity_orig_of(prev_cpu))) {
 			active_balance = kick_active_balance(rq, p, new_cpu);
 			if (active_balance) {
 				mark_reserved(new_cpu);
 				raw_spin_unlock(&migration_lock);
-				stop_one_cpu_nowait(cpu,
+				stop_one_cpu_nowait(prev_cpu,
 					active_load_balance_cpu_stop, rq,
 					&rq->active_balance_work);
 				return;
