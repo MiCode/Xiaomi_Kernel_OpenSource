df1223083cb5d86df64c798bd4b307799ab897a9
Pankaj Gupta <gpankaj@codeaurora.org>
2019-06-17 18:20:46 +0530

    msm: kgsl: Update adreno_gpurev for a620
    The Adreno revision is marked incorrectly for A620.
    This patch fixes this problem to re enable GPU
    functionality.
    
    Change-Id: I20d4174ba2617145ae77d8f659939cda923173c8
    Signed-off-by: Pankaj Gupta <gpankaj@codeaurora.org>

--- drivers/gpu/msm/adreno-gpulist.h
+++ drivers/gpu/msm/adreno-gpulist.h
@@ -939,7 +939,7 @@ static const struct adreno_reglist a650_gbif_regs[] = {
 
 static const struct adreno_a6xx_core adreno_gpu_core_a620 = {
 	.base = {
-		DEFINE_ADRENO_REV(ADRENO_REV_A618, 6, 2, 0, 0),
+		DEFINE_ADRENO_REV(ADRENO_REV_A620, 6, 2, 0, 0),
 		.features = ADRENO_64BIT | ADRENO_RPMH | ADRENO_GPMU |
 			ADRENO_CONTENT_PROTECTION | ADRENO_IOCOHERENT |
 			ADRENO_IFPC,
