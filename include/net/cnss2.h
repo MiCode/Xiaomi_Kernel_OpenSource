2b8fde357a2df3bfa7c9529241d33b071b8fcfde
Sandeep Singh <sandsing@codeaurora.org>
2019-06-19 11:14:35 +0530

    cnss2: removed unused function and variable of IOMMU APIs
    IOMMU mapping has been deprecated from kernel. So, removing
    the unused function and variable from wlan platform driver.
    
    Change-Id: Id4ad3de3a2e672ec318549cffce4b703645c3f6c
    Signed-off-by: Sandeep Singh <sandsing@codeaurora.org>

--- include/net/cnss2.h
+++ include/net/cnss2.h
@@ -170,7 +170,6 @@ extern int cnss_get_fw_files_for_target(struct device *dev,
 					u32 target_type, u32 target_version);
 extern int cnss_get_platform_cap(struct device *dev,
 				 struct cnss_platform_cap *cap);
-extern struct dma_iommu_mapping *cnss_smmu_get_mapping(struct device *dev);
 extern struct iommu_domain *cnss_smmu_get_domain(struct device *dev);
 extern int cnss_smmu_map(struct device *dev,
 			 phys_addr_t paddr, uint32_t *iova_addr, size_t size);
