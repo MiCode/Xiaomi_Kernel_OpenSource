9eccb4c699efa05fd8e9c165f75a73cede08374a
Chih-Chieh Yeh <chih-chieh.yeh@mediatek.com>
2019-06-21 10:13:02 +0800

    [ALPS04478223] DISP: modify definition spelling
    [Detail]
    Change definition spelling from CONFIG_MT_ENG_BUILD to
    CONFIG_MTK_ENG_BUILD.
    
    MTK-Commit-Id: 640b2e9b5afb2494f3b0cdff62eeb7f6945ac813
    
    Change-Id: I52b0c5a84739cb29273dd5b72d04265457d84d30
    Signed-off-by: Chih-Chieh Yeh <chih-chieh.yeh@mediatek.com>
    CR-Id: ALPS04478223
    Feature: [Module]Display Driver

--- drivers/misc/mediatek/video/mt6771/videox/fbconfig_kdebug.c
+++ drivers/misc/mediatek/video/mt6771/videox/fbconfig_kdebug.c
@@ -1306,8 +1306,10 @@ static const struct file_operations fbconfig_fops = {
 
 void PanelMaster_Init(void)
 {
+#if defined(CONFIG_MTK_ENG_BUILD)
 	ConfigPara_dbgfs = debugfs_create_file("fbconfig", S_IFREG | 0444,
 					       NULL, (void *)0, &fbconfig_fops);
+#endif
 
 	INIT_LIST_HEAD(&head_list.list);
 }
