6099189ff02fef650f7def230b0ef92572cae314
mtk13858 <derong.liu@mediatek.com>
2019-06-21 10:13:15 +0800

    [ALPS04660215] CCCI:Porting ccci udc driver
    Porting ccci udc driver
    
    MTK-Commit-Id: 656292a36273933be2a26b19eabe488cb6726f22
    
    Change-Id: Iddd9ec72c54a9110d681b1e95e3ed2b2c3b6e7d6
    Signed-off-by: mtk13858 <derong.liu@mediatek.com>
    CR-Id: ALPS04660215
    Feature: Modem Interface Driver

--- drivers/misc/mediatek/eccci/port/port_char.c
+++ drivers/misc/mediatek/eccci/port/port_char.c
@@ -184,6 +184,7 @@ static int port_char_recv_skb(struct port_t *port, struct sk_buff *skb)
 		port->rx_ch != CCCI_PCM_RX &&
 		port->rx_ch != CCCI_FS_RX &&
 		port->rx_ch != CCCI_RPC_RX &&
+		port->rx_ch != CCCI_UDC_RX &&
 		!(port->rx_ch == CCCI_IPC_RX &&
 		port->minor ==
 		AP_IPC_LWAPROXY + CCCI_IPC_MINOR_BASE)))
