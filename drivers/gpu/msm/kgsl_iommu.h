32c02eeb2d59512a4d44b1016ce09362a0984489
Rajesh Kemisetti <rajeshk@codeaurora.org>
2019-06-27 22:11:24 +0530

    msm: kgsl: Add support to make use of iommu_unmap_fast
    iommu_unmap() generally does two things:
      - Removes the memory entry from pagetable.
      - Triggers TLB invalidation sequence by voting
        respective regulator and clocks of SMMU.
    
    SMMU HW also does invalidation of its caches across
    power collapse, so the second step in iommu_unmap()
    is redundant if SMMU is already in power collapsed state.
    
    Try to skip TLB invalidation by calling iommu_unmap_fast()
    when we know that SMMU regulator is already in OFF state.
    
    Change-Id: Idf93eab51e5db1ac8a1b8d922b3fdae64e5266ca
    Signed-off-by: Rajesh Kemisetti <rajeshk@codeaurora.org>

--- drivers/gpu/msm/kgsl_iommu.h
+++ drivers/gpu/msm/kgsl_iommu.h
@@ -129,6 +129,7 @@ struct kgsl_iommu_context {
  * @setstate: Scratch GPU memory for IOMMU operations
  * @clk_enable_count: The ref count of clock enable calls
  * @clks: Array of pointers to IOMMU clocks
+ * @vddcx_regulator: Handle to IOMMU regulator
  * @micro_mmu_ctrl: GPU register offset of this glob al register
  * @smmu_info: smmu info used in a5xx preemption
  * @protect: register protection settings for the iommu.
@@ -143,6 +144,7 @@ struct kgsl_iommu {
 	struct kgsl_memdesc setstate;
 	atomic_t clk_enable_count;
 	struct clk *clks[KGSL_IOMMU_MAX_CLKS];
+	struct regulator *vddcx_regulator;
 	unsigned int micro_mmu_ctrl;
 	struct kgsl_memdesc smmu_info;
 	unsigned int version;
