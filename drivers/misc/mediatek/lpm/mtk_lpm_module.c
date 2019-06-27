e58f9fcc02b39e3af17ef9513a175eaf39bcbf94
Yun-Chen Hsieh <yun-chen.hsieh@mediatek.com>
2019-06-27 14:00:21 +0800

    low power manager: introduce mtk lpm common kernel driver
    For lpm suspend syscore callback porting,
    to avoid entering system suspend.
    
    Feature: Power Management
    Change-Id: Id7d8e92eb9c2f76be2fefda574261b3c23af953e
    Signed-off-by: Yun-Chen Hsieh <yun-chen.hsieh@mediatek.com>
    CR-Id: ALPS04680937

---
+++ drivers/misc/mediatek/lpm/mtk_lpm_module.c
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/syscore_ops.h>
+
+static int mtk_lpm_suspend_enter(void)
+{
+	return -EACCES;
+}
+
+static void mtk_lpm_suspend_leave(void)
+{
+}
+
+static struct syscore_ops mtk_lpm_suspend = {
+	.suspend = mtk_lpm_suspend_enter,
+	.resume = mtk_lpm_suspend_leave,
+};
+
+static int __init mtk_lpm_init(void)
+{
+	register_syscore_ops(&mtk_lpm_suspend);
+
+	return 0;
+}
+device_initcall(mtk_lpm_init);
+
