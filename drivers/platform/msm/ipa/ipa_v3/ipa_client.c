59a7b0ed82ec1481408ca16bf4a81e62b52ffca7
Praveen Kurapati <pkurapat@codeaurora.org>
2019-06-28 14:45:19 +0530

    msm: ipa4: Disable uC debug stats for lito
    Currently uC debug stats not supported for lito.
    Add changes not to send uC debug stats command to uC.
    
    Change-Id: I4625a7664416a4a904a7c9f9ec6570c3f217b4ca
    Signed-off-by: Praveen Kurapati <pkurapat@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipa_client.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_client.c
@@ -266,7 +266,8 @@ static void ipa3_start_gsi_debug_monitor(u32 clnt_hdl)
 	client_type = ipa3_get_client_mapping(clnt_hdl);
 
 	/* start uC gsi dbg stats monitor */
-	if (ipa3_ctx->ipa_hw_type >= IPA_HW_v4_5) {
+	if (ipa3_ctx->ipa_hw_type >= IPA_HW_v4_5 &&
+		ipa3_ctx->ipa_hw_type != IPA_HW_v4_7) {
 		switch (client_type) {
 		case IPA_CLIENT_MHI_PRIME_TETH_PROD:
 			gsi_info = &ipa3_ctx->gsi_info[IPA_HW_PROTOCOL_MHIP];
