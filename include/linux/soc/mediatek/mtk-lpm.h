8fea310e0d68f61cd725b96e2815a02d76e87095
Chih-Wei Chiou <chih-wei.chiou@mediatek.com>
2019-06-28 17:25:33 +0800

    soc: mediatek: Add mediatek low power manager driver
    The driver provides the platform to enter deeper low power
    mode when cpu power down if there is a sufficient time
    before it wakeup.
    
    Change-Id: Iad426e2d17fc1ace47211b1838c3c6d45823f989
    Signed-off-by: Chih-Wei Chiou <chih-wei.chiou@mediatek.com>
    CR-Id: ALPS04681538
    Feature: Power Management

---
+++ include/linux/soc/mediatek/mtk-lpm.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+
+#ifndef __MTK_LPM_H__
+#define __MTK_LPM_H__
+
+#include <linux/cpu_pm.h>
+#include <linux/cpuidle.h>
+#include <asm/cpuidle.h>
+
+
+#define MTK_CPUIDLE_PM_NAME	"mtk_cpuidle_pm"
+
+struct mtk_cpuidle_op {
+	/*prompt to platform driver that cpu will enter idle state */
+	int (*cpuidle_prepare)(struct cpuidle_driver *drv, int index);
+
+	/*notify platform driver that cpu will leave idle state */
+	void (*cpuidle_resume)(struct cpuidle_driver *drv, int index);
+};
+
+int mtk_lpm_drv_cpuidle_ops_set(struct mtk_cpuidle_op *op);
+#endif
