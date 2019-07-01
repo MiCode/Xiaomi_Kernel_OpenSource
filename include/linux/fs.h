0dca705f7cdf251059de077ee334d14c4c05507b
Eric Biggers <ebiggers@google.com>
2019-07-01 13:26:29 -0700

    f2fs: use generic checking function for FS_IOC_FSSETXATTR
    Make the f2fs implementation of FS_IOC_FSSETXATTR use the new VFS helper
    function vfs_ioc_fssetxattr_check(), and remove the project quota check
    since it's now done by the helper function.
    
    This is based on a patch from Darrick Wong, but reworked to apply after
    commit 360985573b55 ("f2fs: separate f2fs i_flags from fs_flags and ext4
    i_flags").
    
    Originally-from: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -3397,4 +3397,13 @@ extern void inode_nohighmem(struct inode *inode);
 int vfs_ioc_setflags_prepare(struct inode *inode, unsigned int oldflags,
 			     unsigned int flags);
 
+int vfs_ioc_fssetxattr_check(struct inode *inode, const struct fsxattr *old_fa,
+			     struct fsxattr *fa);
+
+static inline void simple_fill_fsxattr(struct fsxattr *fa, __u32 xflags)
+{
+	memset(fa, 0, sizeof(*fa));
+	fa->fsx_xflags = xflags;
+}
+
 #endif /* _LINUX_FS_H */
