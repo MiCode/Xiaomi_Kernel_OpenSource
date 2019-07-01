d608dc9a11da6bea307f30b1a4d19dc808a51fab
Eric Biggers <ebiggers@google.com>
2019-07-01 13:26:28 -0700

    f2fs: use generic checking and prep function for FS_IOC_SETFLAGS
    Make the f2fs implementation of FS_IOC_SETFLAGS use the new VFS helper
    function vfs_ioc_setflags_prepare().
    
    This is based on a patch from Darrick Wong, but reworked to apply after
    commit 360985573b55 ("f2fs: separate f2fs i_flags from fs_flags and ext4
    i_flags").
    
    Originally-from: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -3088,4 +3088,7 @@ static inline bool dir_relax(struct inode *inode)
 
 extern bool path_noexec(const struct path *path);
 
+int vfs_ioc_setflags_prepare(struct inode *inode, unsigned int oldflags,
+			     unsigned int flags);
+
 #endif /* _LINUX_FS_H */
