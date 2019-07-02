0ced5701566a51620421eeb0a40588f561e4b06b
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-07-02 12:26:17 +0530

    sched/fair: Fix incorrect CPU access in check_for_migration()
    Commit 3a94de0be298 ("sched/fair: Prevent tick path active migration
    to the same CPU") removed the -1 check for the return value of
    find_energy_efficient_cpu() while adding the check against the
    previous CPU. If the new CPU is -1, then it passes the check of
    new_cpu != prev_cpu and end up calling capacity_orig_of() with
    -1 as an argument. Fix this issue by adding the -1 check again.
    
    Change-Id: I6768ef722f25885f97583e493a6753b5e3fe28ee
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -12503,8 +12503,8 @@ void check_for_migration(struct rq *rq, struct task_struct *p)
 		rcu_read_lock();
 		new_cpu = find_energy_efficient_cpu(p, prev_cpu, 0);
 		rcu_read_unlock();
-		if ((new_cpu != prev_cpu) && (capacity_orig_of(new_cpu) >
-					capacity_orig_of(prev_cpu))) {
+		if ((new_cpu != -1) && (new_cpu != prev_cpu) &&
+		    (capacity_orig_of(new_cpu) > capacity_orig_of(prev_cpu))) {
 			active_balance = kick_active_balance(rq, p, new_cpu);
 			if (active_balance) {
 				mark_reserved(new_cpu);
