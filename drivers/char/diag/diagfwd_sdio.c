53d7ba42ba09c2910836ec92fc0423d1e36885b8
Amandeep Singh <amansing@codeaurora.org>
2019-07-03 12:47:41 +0530

    diag: Update diag over sdio snapshot
    Update code to support diag for peripheral devices over sdio bus.
    
    Change-Id: I14678f301f94b239b61c4c94d3c0ff65204e739e
    Signed-off-by: Amandeep Singh <amansing@codeaurora.org>

--- drivers/char/diag/diagfwd_sdio.c
+++ drivers/char/diag/diagfwd_sdio.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014,2016,2018 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2019 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -23,104 +23,35 @@
 #include <asm/current.h>
 #include "diagmem.h"
 #include "diagfwd_bridge.h"
-#include "diagfwd_hsic.h"
+#include "diagfwd_sdio.h"
 
-#define DIAG_HSIC_STRING_SZ	11
+#define DIAG_SDIO_STRING_SZ	11
 
-struct diag_hsic_info diag_hsic[NUM_HSIC_DEV] = {
+struct diag_sdio_info diag_sdio[NUM_SDIO_DEV] = {
 	{
-		.id = HSIC_1,
+		.id = SDIO_1,
 		.dev_id = DIAGFWD_MDM,
 		.name = "MDM",
 		.mempool = POOL_TYPE_MDM,
 		.opened = 0,
 		.enabled = 0,
 		.suspended = 0,
-		.hsic_wq = NULL
+		.sdio_wq = NULL
 	},
-	{
-		.id = HSIC_2,
-		.dev_id = DIAGFWD_MDM_DCI,
-		.name = "MDM_DCI",
-		.mempool = POOL_TYPE_MDM_DCI,
-		.opened = 0,
-		.enabled = 0,
-		.suspended = 0,
-		.hsic_wq = NULL
-	}
 };
 
-static int hsic_buf_tbl_push(struct diag_hsic_info *ch, void *buf, int len)
-{
-	unsigned long flags;
-	struct diag_hsic_buf_tbl_t *item;
-
-	if (!ch || !buf || len < 0)
-		return -EINVAL;
-
-	item = kzalloc(sizeof(struct diag_hsic_buf_tbl_t), GFP_ATOMIC);
-	if (!item)
-		return -ENOMEM;
-	kmemleak_not_leak(item);
-
-	spin_lock_irqsave(&ch->lock, flags);
-	item->buf = buf;
-	item->len = len;
-	list_add_tail(&item->link, &ch->buf_tbl);
-	spin_unlock_irqrestore(&ch->lock, flags);
-
-	return 0;
-}
-
-static struct diag_hsic_buf_tbl_t *hsic_buf_tbl_pop(struct diag_hsic_info *ch)
-{
-	unsigned long flags;
-	struct diag_hsic_buf_tbl_t *item = NULL;
-
-	if (!ch || list_empty(&ch->buf_tbl))
-		return NULL;
-
-	spin_lock_irqsave(&ch->lock, flags);
-	item = list_first_entry(&ch->buf_tbl, struct diag_hsic_buf_tbl_t, link);
-	list_del(&item->link);
-	spin_unlock_irqrestore(&ch->lock, flags);
-
-	return item;
-}
-
-static void hsic_buf_tbl_clear(struct diag_hsic_info *ch)
-{
-	unsigned long flags;
-	struct list_head *start, *temp;
-	struct diag_hsic_buf_tbl_t *item = NULL;
-
-	if (!ch)
-		return;
-
-	/* At this point, the channel should already by closed */
-	spin_lock_irqsave(&ch->lock, flags);
-	list_for_each_safe(start, temp, &ch->buf_tbl) {
-		item = list_entry(start, struct diag_hsic_buf_tbl_t,
-				  link);
-		list_del(&item->link);
-		kfree(item);
-
-	}
-	spin_unlock_irqrestore(&ch->lock, flags);
-}
-
-static void diag_hsic_read_complete(void *ctxt, char *buf, int len,
+static void diag_sdio_read_complete(void *ctxt, char *buf, int len,
 				    int actual_size)
 {
+	int err = 0;
 	int index = (int)(uintptr_t)ctxt;
-	struct diag_hsic_info *ch = NULL;
+	struct diag_sdio_info *ch = NULL;
 
-	if (index < 0 || index >= NUM_HSIC_DEV) {
-		pr_err_ratelimited("diag: In %s, invalid HSIC index %d\n",
-				   __func__, index);
+	if (index < 0 || index >= NUM_SDIO_DEV) {
+		pr_err("diag: Invalid index %d in %s\n", index, __func__);
 		return;
 	}
-	ch = &diag_hsic[index];
+	ch = &diag_sdio[index];
 
 	/*
 	 * Don't pass on the buffer if the channel is closed when a pending read
@@ -129,108 +60,101 @@ static void diag_hsic_read_complete(void *ctxt, char *buf, int len,
 	 */
 	if (!ch->opened || actual_size <= 0)
 		goto fail;
-	hsic_buf_tbl_push(ch, buf, actual_size);
-	queue_work(ch->hsic_wq, &ch->read_complete_work);
+	err = diag_remote_dev_read_done(ch->dev_id, buf, actual_size);
+	if (err)
+		goto fail;
 	return;
 
 fail:
 	diagmem_free(driver, buf, ch->mempool);
-	queue_work(ch->hsic_wq, &ch->read_work);
+	queue_work(ch->sdio_wq, &ch->read_work);
+	return;
 }
 
-static void diag_hsic_write_complete(void *ctxt, char *buf, int len,
+static void diag_sdio_write_complete(void *ctxt, char *buf, int len,
 				     int actual_size)
 {
 	int index = (int)(uintptr_t)ctxt;
-	struct diag_hsic_info *ch = NULL;
+	struct diag_sdio_info *ch = NULL;
 
-	if (index < 0 || index >= NUM_HSIC_DEV) {
-		pr_err_ratelimited("diag: In %s, invalid HSIC index %d\n",
-				   __func__, index);
+	if (index < 0 || index >= NUM_SDIO_DEV) {
+		pr_err("diag: Invalid index %d in %s\n", index, __func__);
 		return;
 	}
 
-	ch = &diag_hsic[index];
+	ch = &diag_sdio[index];
 	diag_remote_dev_write_done(ch->dev_id, buf, actual_size, ch->id);
+	return;
 }
 
-static int diag_hsic_suspend(void *ctxt)
+static int diag_sdio_suspend(void *ctxt)
 {
 	int index = (int)(uintptr_t)ctxt;
 	unsigned long flags;
-	struct diag_hsic_info *ch = NULL;
+	struct diag_sdio_info *ch = NULL;
 
-	if (index < 0 || index >= NUM_HSIC_DEV) {
-		pr_err_ratelimited("diag: In %s, invalid HSIC index %d\n",
-				   __func__, index);
+	if (index < 0 || index >= NUM_SDIO_DEV) {
+		pr_err("diag: Invalid index %d in %s\n", index, __func__);
 		return -EINVAL;
 	}
 
-	ch = &diag_hsic[index];
+	ch = &diag_sdio[index];
 	spin_lock_irqsave(&ch->lock, flags);
 	ch->suspended = 1;
 	spin_unlock_irqrestore(&ch->lock, flags);
 	return 0;
 }
 
-static void diag_hsic_resume(void *ctxt)
+static void diag_sdio_resume(void *ctxt)
 {
 	int index = (int)(uintptr_t)ctxt;
 	unsigned long flags;
-	struct diag_hsic_info *ch = NULL;
+	struct diag_sdio_info *ch = NULL;
 
-	if (index < 0 || index >= NUM_HSIC_DEV) {
-		pr_err_ratelimited("diag: In %s, invalid HSIC index %d\n",
-				   __func__, index);
+	if (index < 0 || index >= NUM_SDIO_DEV) {
+		pr_err("diag: Invalid index %d in %s\n", index, __func__);
 		return;
 	}
-	ch = &diag_hsic[index];
+	ch = &diag_sdio[index];
 	spin_lock_irqsave(&ch->lock, flags);
 	ch->suspended = 0;
 	spin_unlock_irqrestore(&ch->lock, flags);
-	queue_work(ch->hsic_wq, &(ch->read_work));
+	queue_work(ch->sdio_wq, &(ch->read_work));
 }
 
-static struct diag_bridge_ops diag_hsic_ops[NUM_HSIC_DEV] = {
+static struct diag_bridge_ops diag_sdio_ops[NUM_SDIO_DEV] = {
 	{
-		.ctxt = (void *)HSIC_1,
-		.read_complete_cb = diag_hsic_read_complete,
-		.write_complete_cb = diag_hsic_write_complete,
-		.suspend = diag_hsic_suspend,
-		.resume = diag_hsic_resume,
+		.ctxt = (void *)SDIO_1,
+		.read_complete_cb = diag_sdio_read_complete,
+		.write_complete_cb = diag_sdio_write_complete,
+		.suspend = diag_sdio_suspend,
+		.resume = diag_sdio_resume,
 	},
-	{
-		.ctxt = (void *)HSIC_2,
-		.read_complete_cb = diag_hsic_read_complete,
-		.write_complete_cb = diag_hsic_write_complete,
-		.suspend = diag_hsic_suspend,
-		.resume = diag_hsic_resume,
-	}
 };
 
-static int hsic_open(int id)
+static int sdio_open(int id)
 {
 	int err = 0;
 	unsigned long flags;
-	struct diag_hsic_info *ch = NULL;
+	struct diag_sdio_info *ch = NULL;
 
-	if (id < 0 || id >= NUM_HSIC_DEV) {
+	if (id < 0 || id >= NUM_SDIO_DEV) {
 		pr_err("diag: Invalid index %d in %s\n", id, __func__);
 		return -EINVAL;
 	}
 
-	ch = &diag_hsic[id];
+	ch = &diag_sdio[id];
 	if (!ch->enabled)
 		return -ENODEV;
 
 	if (ch->opened) {
-		pr_debug("diag: HSIC channel %d is already opened\n", ch->id);
+		pr_debug("diag: SDIO channel %d is already opened\n", ch->id);
 		return -ENODEV;
 	}
 
-	err = diag_bridge_open(ch->id, &diag_hsic_ops[ch->id]);
+	err = qti_client_open(ch->ch_num, &diag_sdio_ops[ch->id]);
 	if (err) {
-		pr_err("diag: Unable to open HSIC channel %d, err: %d",
+		pr_err("diag: Unable to open SDIO channel %d, err: %d",
 		       ch->id, err);
 		return err;
 	}
@@ -240,61 +164,59 @@ static int hsic_open(int id)
 	diagmem_init(driver, ch->mempool);
 	/* Notify the bridge that the channel is open */
 	diag_remote_dev_open(ch->dev_id);
-	INIT_LIST_HEAD(&ch->buf_tbl);
-	queue_work(ch->hsic_wq, &(ch->read_work));
+	queue_work(ch->sdio_wq, &(ch->read_work));
 	return 0;
 }
 
-static void hsic_open_work_fn(struct work_struct *work)
+static void sdio_open_work_fn(struct work_struct *work)
 {
-	struct diag_hsic_info *ch = container_of(work, struct diag_hsic_info,
+	struct diag_sdio_info *ch = container_of(work, struct diag_sdio_info,
 						 open_work);
 	if (ch)
-		hsic_open(ch->id);
+		sdio_open(ch->id);
 }
 
-static int hsic_close(int id)
+static int sdio_close(int id)
 {
 	unsigned long flags;
-	struct diag_hsic_info *ch = NULL;
+	struct diag_sdio_info *ch = NULL;
 
-	if (id < 0 || id >= NUM_HSIC_DEV) {
+	if (id < 0 || id >= NUM_SDIO_DEV) {
 		pr_err("diag: Invalid index %d in %s\n", id, __func__);
 		return -EINVAL;
 	}
 
-	ch = &diag_hsic[id];
+	ch = &diag_sdio[id];
 	if (!ch->enabled)
 		return -ENODEV;
 
 	if (!ch->opened) {
-		pr_debug("diag: HSIC channel %d is already closed\n", ch->id);
+		pr_debug("diag: SDIO channel %d is already closed\n", ch->id);
 		return -ENODEV;
 	}
 
 	spin_lock_irqsave(&ch->lock, flags);
 	ch->opened = 0;
 	spin_unlock_irqrestore(&ch->lock, flags);
-	diag_bridge_close(ch->id);
+	qti_client_close(ch->ch_num);
 	diagmem_exit(driver, ch->mempool);
 	diag_remote_dev_close(ch->dev_id);
-	hsic_buf_tbl_clear(ch);
 	return 0;
 }
 
-static void hsic_close_work_fn(struct work_struct *work)
+static void sdio_close_work_fn(struct work_struct *work)
 {
-	struct diag_hsic_info *ch = container_of(work, struct diag_hsic_info,
+	struct diag_sdio_info *ch = container_of(work, struct diag_sdio_info,
 						 close_work);
 	if (ch)
-		hsic_close(ch->id);
+		sdio_close(ch->id);
 }
 
-static void hsic_read_work_fn(struct work_struct *work)
+static void sdio_read_work_fn(struct work_struct *work)
 {
 	int err = 0;
 	unsigned char *buf = NULL;
-	struct diag_hsic_info *ch = container_of(work, struct diag_hsic_info,
+	struct diag_sdio_info *ch = container_of(work, struct diag_sdio_info,
 						 read_work);
 	if (!ch || !ch->enabled || !ch->opened)
 		return;
@@ -306,231 +228,201 @@ static void hsic_read_work_fn(struct work_struct *work)
 			break;
 		}
 
-		err = diag_bridge_read(ch->id, buf, DIAG_MDM_BUF_SIZE);
-		if (err) {
+		err = qti_client_read(ch->ch_num, buf, DIAG_MDM_BUF_SIZE);
+		if (err < 0) {
 			diagmem_free(driver, buf, ch->mempool);
-			pr_err_ratelimited("diag: Unable to read from HSIC channel %d, err: %d\n",
-					   ch->id, err);
 			break;
 		}
 	} while (buf);
 
-	/* Read from the HSIC channel continuously if the channel is present */
+	/* Read from the SDIO channel continuously if the channel is present */
 	if (!err)
-		queue_work(ch->hsic_wq, &ch->read_work);
+		queue_work(ch->sdio_wq, &ch->read_work);
 }
 
-static void hsic_read_complete_work_fn(struct work_struct *work)
-{
-	struct diag_hsic_info *ch = container_of(work, struct diag_hsic_info,
-						 read_complete_work);
-	struct diag_hsic_buf_tbl_t *item;
-
-	do {
-		item = hsic_buf_tbl_pop(ch);
-		if (item) {
-			if (diag_remote_dev_read_done(ch->dev_id,
-						      item->buf, item->len))
-				goto fail;
-			kfree(item);
-		}
-	} while (item);
-
-	return;
-
-fail:
-	diagmem_free(driver, item->buf, ch->mempool);
-	queue_work(ch->hsic_wq, &ch->read_work);
-	kfree(item);
-}
-
-static int diag_hsic_probe(struct platform_device *pdev)
+static int diag_sdio_probe(struct platform_device *pdev)
 {
 	unsigned long flags;
-	struct diag_hsic_info *ch = NULL;
+	struct diag_sdio_info *ch = NULL;
 
 	if (!pdev)
 		return -EIO;
 
-	pr_debug("diag: hsic probe pdev: %d\n", pdev->id);
-	if (pdev->id >= NUM_HSIC_DEV) {
-		pr_err("diag: No support for HSIC device %d\n", pdev->id);
+	pr_debug("diag: sdio probe pdev: %d\n", pdev->id);
+	if (pdev->id >= NUM_SDIO_DEV) {
+		pr_err("diag: No support for SDIO device %d\n", pdev->id);
 		return -EIO;
 	}
 
-	ch = &diag_hsic[pdev->id];
+	ch = &diag_sdio[pdev->id];
+	ch->ch_num = *((int *)pdev->dev.platform_data);
+
 	if (!ch->enabled) {
 		spin_lock_irqsave(&ch->lock, flags);
 		ch->enabled = 1;
 		spin_unlock_irqrestore(&ch->lock, flags);
 	}
-	queue_work(ch->hsic_wq, &(ch->open_work));
+	queue_work(ch->sdio_wq, &(ch->open_work));
 	return 0;
 }
 
-static int diag_hsic_remove(struct platform_device *pdev)
+static int diag_sdio_remove(struct platform_device *pdev)
 {
-	struct diag_hsic_info *ch = NULL;
+	struct diag_sdio_info *ch = NULL;
 
 	if (!pdev)
 		return -EIO;
 
-	pr_debug("diag: hsic close pdev: %d\n", pdev->id);
-	if (pdev->id >= NUM_HSIC_DEV) {
-		pr_err("diag: No support for HSIC device %d\n", pdev->id);
+	pr_debug("diag: sdio close pdev: %d\n", pdev->id);
+	if (pdev->id >= NUM_SDIO_DEV) {
+		pr_err("diag: No support for SDIO device %d\n", pdev->id);
 		return -EIO;
 	}
 
-	ch = &diag_hsic[pdev->id];
-	queue_work(ch->hsic_wq, &(ch->close_work));
+	ch = &diag_sdio[pdev->id];
+	queue_work(ch->sdio_wq, &(ch->close_work));
 	return 0;
 }
 
-static int diagfwd_hsic_runtime_suspend(struct device *dev)
+static int diagfwd_sdio_runtime_suspend(struct device *dev)
 {
 	dev_dbg(dev, "pm_runtime: suspending...\n");
 	return 0;
 }
 
-static int diagfwd_hsic_runtime_resume(struct device *dev)
+static int diagfwd_sdio_runtime_resume(struct device *dev)
 {
 	dev_dbg(dev, "pm_runtime: resuming...\n");
 	return 0;
 }
 
-static const struct dev_pm_ops diagfwd_hsic_dev_pm_ops = {
-	.runtime_suspend = diagfwd_hsic_runtime_suspend,
-	.runtime_resume = diagfwd_hsic_runtime_resume,
+static const struct dev_pm_ops diagfwd_sdio_dev_pm_ops = {
+	.runtime_suspend = diagfwd_sdio_runtime_suspend,
+	.runtime_resume = diagfwd_sdio_runtime_resume,
 };
 
-static struct platform_driver msm_hsic_ch_driver = {
-	.probe = diag_hsic_probe,
-	.remove = diag_hsic_remove,
+static struct platform_driver msm_sdio_ch_driver = {
+	.probe = diag_sdio_probe,
+	.remove = diag_sdio_remove,
 	.driver = {
-		   .name = "diag_bridge",
+		   .name = "diag_bridge_sdio",
 		   .owner = THIS_MODULE,
-		   .pm   = &diagfwd_hsic_dev_pm_ops,
+		   .pm   = &diagfwd_sdio_dev_pm_ops,
 		   },
 };
 
-static int hsic_queue_read(int id)
+static int sdio_queue_read(int id)
 {
-	if (id < 0 || id >= NUM_HSIC_DEV) {
-		pr_err_ratelimited("diag: In %s, invalid index %d\n",
-				   __func__, id);
+	if (id < 0 || id >= NUM_SDIO_DEV) {
+		pr_err("diag: Invalid index %d in %s\n", id, __func__);
 		return -EINVAL;
 	}
-	queue_work(diag_hsic[id].hsic_wq, &(diag_hsic[id].read_work));
+	queue_work(diag_sdio[id].sdio_wq, &(diag_sdio[id].read_work));
 	return 0;
 }
 
-static int hsic_write(int id, unsigned char *buf, int len, int ctxt)
+static int sdio_write(int id, unsigned char *buf, int len, int ctxt)
 {
 	int err = 0;
-	struct diag_hsic_info *ch = NULL;
+	struct diag_sdio_info *ch = NULL;
 
-	if (id < 0 || id >= NUM_HSIC_DEV) {
-		pr_err_ratelimited("diag: In %s, invalid index %d\n",
-				   __func__, id);
+
+	if (id < 0 || id >= NUM_SDIO_DEV) {
+		pr_err("diag: Invalid index %d in %s\n", id, __func__);
 		return -EINVAL;
 	}
 	if (!buf || len <= 0) {
-		pr_err_ratelimited("diag: In %s, ch %d, invalid buf %pK len %d\n",
-				   __func__, id, buf, len);
 		return -EINVAL;
 	}
 
-	ch = &diag_hsic[id];
+	ch = &diag_sdio[id];
 	if (!ch->opened || !ch->enabled) {
-		pr_debug_ratelimited("diag: In %s, ch %d is disabled. opened %d enabled: %d\n",
+		pr_debug("diag: %s, ch %d is disabled. opened %d enabled %d\n",
 				     __func__, id, ch->opened, ch->enabled);
 		return -EIO;
 	}
 
-	err = diag_bridge_write(ch->id, buf, len);
-	if (err) {
-		pr_err_ratelimited("diag: cannot write to HSIC ch %d, err: %d\n",
-				   ch->id, err);
+	err = qti_client_write(ch->ch_num, buf, len);
+	if (err < 0) {
+		pr_err("diag: failed to write to ch[%d] in %s\n", ch->ch_num,
+								__func__);
+		return err;
 	}
-	return err;
+	return 0;
 }
 
-static int hsic_fwd_complete(int id, unsigned char *buf, int len, int ctxt)
+static int sdio_fwd_complete(int id, unsigned char *buf, int len, int ctxt)
 {
-	if (id < 0 || id >= NUM_HSIC_DEV) {
-		pr_err_ratelimited("diag: In %s, invalid index %d\n",
-				   __func__, id);
+	if (id < 0 || id >= NUM_SDIO_DEV) {
+		pr_err("diag: Invalid index %d in %s\n", id, __func__);
 		return -EINVAL;
 	}
 	if (!buf)
 		return -EIO;
-	diagmem_free(driver, buf, diag_hsic[id].mempool);
-	queue_work(diag_hsic[id].hsic_wq, &(diag_hsic[id].read_work));
+	diagmem_free(driver, buf, diag_sdio[id].mempool);
+	queue_work(diag_sdio[id].sdio_wq, &(diag_sdio[id].read_work));
 	return 0;
 }
 
-static struct diag_remote_dev_ops diag_hsic_fwd_ops = {
-	.open = hsic_open,
-	.close = hsic_close,
-	.queue_read = hsic_queue_read,
-	.write = hsic_write,
-	.fwd_complete = hsic_fwd_complete,
+static struct diag_remote_dev_ops diag_sdio_fwd_ops = {
+	.open = sdio_open,
+	.close = sdio_close,
+	.queue_read = sdio_queue_read,
+	.write = sdio_write,
+	.fwd_complete = sdio_fwd_complete,
 };
 
-int diag_hsic_init(void)
+int diag_sdio_init(void)
 {
 	int i;
 	int err = 0;
-	struct diag_hsic_info *ch = NULL;
-	char wq_name[DIAG_HSIC_NAME_SZ + DIAG_HSIC_STRING_SZ];
+	struct diag_sdio_info *ch = NULL;
+	char wq_name[DIAG_SDIO_NAME_SZ + DIAG_SDIO_STRING_SZ];
 
-	for (i = 0; i < NUM_HSIC_DEV; i++) {
-		ch = &diag_hsic[i];
+	for (i = 0; i < NUM_SDIO_DEV; i++) {
+		ch = &diag_sdio[i];
 		spin_lock_init(&ch->lock);
-		INIT_WORK(&(ch->read_work), hsic_read_work_fn);
-		INIT_WORK(&(ch->read_complete_work),
-			  hsic_read_complete_work_fn);
-		INIT_WORK(&(ch->open_work), hsic_open_work_fn);
-		INIT_WORK(&(ch->close_work), hsic_close_work_fn);
-		strlcpy(wq_name, "DIAG_HSIC_", DIAG_HSIC_STRING_SZ);
+		INIT_WORK(&(ch->read_work), sdio_read_work_fn);
+		INIT_WORK(&(ch->open_work), sdio_open_work_fn);
+		INIT_WORK(&(ch->close_work), sdio_close_work_fn);
+		strlcpy(wq_name, "DIAG_SDIO_", DIAG_SDIO_STRING_SZ);
 		strlcat(wq_name, ch->name, sizeof(ch->name));
-		ch->hsic_wq = create_singlethread_workqueue(wq_name);
-		if (!ch->hsic_wq)
+		ch->sdio_wq = create_singlethread_workqueue(wq_name);
+		if (!ch->sdio_wq)
 			goto fail;
 		err = diagfwd_bridge_register(ch->dev_id, ch->id,
-					      &diag_hsic_fwd_ops);
+					      &diag_sdio_fwd_ops);
 		if (err) {
-			pr_err("diag: Unable to register HSIC channel %d with bridge, err: %d\n",
+			pr_err("diag: Unable to register SDIO channel %d with bridge, err: %d\n",
 			       i, err);
 			goto fail;
 		}
 	}
 
-	err = platform_driver_register(&msm_hsic_ch_driver);
+	err = platform_driver_register(&msm_sdio_ch_driver);
 	if (err) {
-		pr_err("diag: could not register HSIC device, err: %d\n", err);
+		pr_err("diag: could not register SDIO device, err: %d\n", err);
 		goto fail;
 	}
 
 	return 0;
 fail:
-	diag_hsic_exit();
+	diag_sdio_exit();
 	return -ENOMEM;
 }
 
-void diag_hsic_exit(void)
+void diag_sdio_exit(void)
 {
 	int i;
-	struct diag_hsic_info *ch = NULL;
+	struct diag_sdio_info *ch = NULL;
 
-	for (i = 0; i < NUM_HSIC_DEV; i++) {
-		ch = &diag_hsic[i];
+	for (i = 0; i < NUM_SDIO_DEV; i++) {
+		ch = &diag_sdio[i];
 		ch->enabled = 0;
 		ch->opened = 0;
 		ch->suspended = 0;
-		if (ch->hsic_wq)
-			destroy_workqueue(ch->hsic_wq);
+		if (ch->sdio_wq)
+			destroy_workqueue(ch->sdio_wq);
 	}
-	platform_driver_unregister(&msm_hsic_ch_driver);
+	platform_driver_unregister(&msm_sdio_ch_driver);
 }
-
--- drivers/char/diag/diagfwd_sdio.h
+++ drivers/char/diag/diagfwd_sdio.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, 2018 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -10,48 +10,46 @@
  * GNU General Public License for more details.
  */
 
-#ifndef DIAGFWD_HSIC_H
-#define DIAGFWD_HSIC_H
+#ifndef DIAGFWD_SDIO_H
+#define DIAGFWD_SDIO_H
 
-#include <linux/list.h>
+#ifdef CONFIG_QCOM_SDIO_CLIENT
+
 #ifdef CONFIG_DIAG_OVER_USB
 #include <linux/usb/usbdiag.h>
 #endif
 #include <linux/usb/diag_bridge.h>
 
-#define HSIC_1			0
-#define HSIC_2			1
-#define NUM_HSIC_DEV		2
+#define SDIO_1			0
+#define NUM_SDIO_DEV		1
 
-#define DIAG_HSIC_NAME_SZ	24
+#define DIAG_SDIO_NAME_SZ	24
 
-struct diag_hsic_buf_tbl_t {
-	struct list_head link;
-	unsigned char *buf;
-	int len;
-};
-
-struct diag_hsic_info {
+struct diag_sdio_info {
 	int id;
 	int dev_id;
+	int ch_num;
 	int mempool;
 	uint8_t opened;
 	uint8_t enabled;
 	uint8_t suspended;
-	char name[DIAG_HSIC_NAME_SZ];
+	char name[DIAG_SDIO_NAME_SZ];
 	struct work_struct read_work;
-	struct work_struct read_complete_work;
 	struct work_struct open_work;
 	struct work_struct close_work;
-	struct workqueue_struct *hsic_wq;
+	struct workqueue_struct *sdio_wq;
 	spinlock_t lock;
-	struct list_head buf_tbl;
 };
 
-extern struct diag_hsic_info diag_hsic[NUM_HSIC_DEV];
+extern struct diag_sdio_info diag_sdio[NUM_SDIO_DEV];
+extern int qti_client_open(int id, struct diag_bridge_ops *ops);
+extern int qti_client_close(int id);
+extern int qti_client_read(int id, char *buf, size_t count);
+extern int qti_client_write(int id, char *buf, size_t count);
 
-int diag_hsic_init(void);
-void diag_hsic_exit(void);
+
+int diag_sdio_init(void);
+void diag_sdio_exit(void);
 
 #endif
-
+#endif /*CONFIG_QCOM_SDIO_CLIENT*/
