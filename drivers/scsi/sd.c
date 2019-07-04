7ba1aef1c69b3ecf834300853229946002b70b72
Peter Wang <peter.wang@mediatek.com>
2019-07-04 10:13:30 +0800

    Revert "sd: Fix rw_max for devices that report..."
    This reverts commit 6b7e9cde49691e04314342b7dce90c67ad567fcc.
    ("sd: Fix rw_max for devices
    that report an optimal xfer size")'.
    Max sectors limit for request queue(q->limits.max_sectors) is reduced
    from 1024 to 16 sectors by above commit and causing performance impact
    (reduced by upto 50%). Hence revert the change to fix the performance
    issue observed on mtk chip.
    
    Change-Id: Ie93e12f5880f60497719b710a408323d39bd0e5a
    Signed-off-by: Peter Wang <peter.wang@mediatek.com>
    CR-Id: ALPS04689783

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -3191,8 +3191,8 @@ static int sd_revalidate_disk(struct gendisk *disk)
 	q->limits.max_dev_sectors = logical_to_sectors(sdp, dev_max);
 
 	if (sd_validate_opt_xfer_size(sdkp, dev_max)) {
-		q->limits.io_opt = logical_to_bytes(sdp, sdkp->opt_xfer_blocks);
-		rw_max = logical_to_sectors(sdp, sdkp->opt_xfer_blocks);
+		rw_max = q->limits.io_opt =
+			logical_to_bytes(sdp, sdkp->opt_xfer_blocks);
 	} else
 		rw_max = min_not_zero(logical_to_sectors(sdp, dev_max),
 				      (sector_t)BLK_DEF_MAX_SECTORS);
