33b66409da89eb582a8e4665a1766d62eba4253c
mtk12444 <sagy.shih@mediatek.com>
2019-07-07 18:27:34 +0800

    [ALPS04427404] EMI: sync patch to q trunk
    1. Clear MD violation API
    2. Bypass violation flow
    
    MTK-Commit-Id: a6826a3084dca34546f686e0ba95671c9b69758e
    
    Change-Id: I4a7d1cfce58d33a5e29644ed8e4ed78d111b0b7c
    Signed-off-by: mtk12444 <sagy.shih@mediatek.com>
    CR-Id: ALPS04427404
    Feature: DRAM

--- drivers/misc/mediatek/emi/mt6771/mpu_platform.c
+++ drivers/misc/mediatek/emi/mt6771/mpu_platform.c
@@ -12,6 +12,9 @@
  */
 
 #include <linux/platform_device.h>
+#include <linux/module.h>
+#include <mt-plat/sync_write.h>
+#include <mt-plat/mtk_io.h>
 
 #include "mt_emi.h"
 
@@ -47,3 +50,50 @@ void set_ap_region_permission(unsigned int apc[EMI_MPU_DGROUP_NUM])
 		FORBIDDEN, FORBIDDEN, FORBIDDEN, NO_PROTECTION);
 }
 
+void bypass_init(unsigned int *init_flag)
+{
+	*init_flag = 1;
+}
+
+int bypass_violation(unsigned int mpus, unsigned int *init_flag)
+{
+	int ret;
+	void __iomem *cen_emi_base = mt_cen_emi_base_get();
+	void __iomem *infra_ao_base;
+	unsigned int i;
+
+	if (*init_flag)
+		return 0;
+
+	pr_info("[MPU] check bypass condition\n");
+
+	infra_ao_base = ioremap(0x10001000, 0x1000);
+	ret = 1;
+
+	mt_reg_sync_writel(0x02000000, cen_emi_base + 0x4E8);
+	mt_reg_sync_writel(0xFFFFFFFF, cen_emi_base + 0x4F8);
+	mt_reg_sync_writel(0x00000001, cen_emi_base + 0x400);
+
+	if ((mpus & 0x7) != 0x3)
+		ret = 0;
+
+	if (((mpus & 0x10000000) != 0x10000000)
+		&& ((mpus & 0x08000000) != 0x08000000))
+		ret = 0;
+
+	if (readl(IOMEM(cen_emi_base + 0x518)) != 0)
+		ret = 0;
+
+	for (i = 0; i < 3; i++) {
+		if (ret == 0)
+			break;
+		if ((readl(IOMEM(infra_ao_base + 0xD34)) & 0x1) != 0x1)
+			ret = 0;
+		if ((readl(IOMEM(infra_ao_base + 0xD54)) & 0xF000) != 0x7000)
+			ret = 0;
+	}
+
+	*init_flag = 1;
+
+	return ret;
+}
