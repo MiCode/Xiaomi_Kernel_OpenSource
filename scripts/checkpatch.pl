81b647f140edff4638530b4c4f840ab6c8e231bc
Hepson Sanchez <sanchez@codeaurora.org>
2019-07-10 17:54:08 -0700

    Revert "checkpatch: forbid filesystem accesses from within the kernel."
    This reverts commit f9d5afd1b560ee0c9d48c0be20071648662a535a.
    
    Change-Id: I6a9671b120c0ac855689a95dac348f34286fcfe5
    Signed-off-by: Hepson Sanchez <sanchez@codeaurora.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -5679,13 +5679,6 @@ sub process {
 			     "Avoid line continuations in quoted strings\n" . $herecurr);
 		}
 
-# sys_open/read/write/close are not allowed in the kernel
-		if ($line =~ /\b(sys_(?:open|read|write|close))\b/) {
-			ERROR("FILE_OPS",
-			      "$1 is inappropriate in kernel code.\n" .
-			      $herecurr);
-		}
-
 # read[bwl] & write[bwl] use too many barriers, use the _relaxed variants
 		if ($line =~ /\b((?:read|write)[bwl])\b/) {
 			ERROR("NON_RELAXED_IO",
@@ -5705,7 +5698,6 @@ sub process {
 			      "with appropriate memory barriers instead.\n" .
 			      $herecurr);
 		}
-
 # warn about #if 0
 		if ($line =~ /^.\s*\#\s*if\s+0\b/) {
 			WARN("IF_0",
