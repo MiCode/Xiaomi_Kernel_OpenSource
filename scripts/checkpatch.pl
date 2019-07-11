43773c4d53572bc9cfc96b56dec34b33ef51a4c6
Hepson Sanchez <sanchez@codeaurora.org>
2019-07-10 17:51:08 -0700

    Revert "checkpatch: complain about the use of dsb()."
    This reverts commit 73998fd95272ed05e0661acb235687237f6e55c4.
    
    Change-Id: Iba24396d7a4c17ac01fe2226a4a36e8c1de2e1db
    Signed-off-by: Hepson Sanchez <sanchez@codeaurora.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -5713,13 +5713,6 @@ sub process {
 			      $herecurr);
 		}
 
-# dsb is too ARMish, and should usually be mb.
-		if ($line =~ /\bdsb\b/) {
-			WARN("ARM_BARRIER",
-			     "Use of dsb is discouranged: prefer mb.\n" .
-			     $herecurr);
-		}
-
 # unbounded string functions are overflow risks
 		my %str_fns = (
 			"sprintf" => "snprintf",
