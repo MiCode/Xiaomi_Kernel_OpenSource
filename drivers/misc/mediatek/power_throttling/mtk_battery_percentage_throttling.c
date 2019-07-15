b5cbd158b08af88cd6e6f8884171bac7444fda6e
Samuel Hsieh <samuel.hsieh@mediatek.com>
2019-07-15 17:29:44 +0800

    PT: mediatek: add battery percentage power throttling
    add battery percentage power throttling, system will limit
    power when battery percentage under threshold
    
    Change-Id: I18346d2e2805cade660bafc78b9de87edf4a1746
    Signed-off-by: Samuel Hsieh <samuel.hsieh@mediatek.com>
    CR-Id: ALPS04704077
    Feature: Dynamic load power throttling

---
+++ drivers/misc/mediatek/power_throttling/mtk_battery_percentage_throttling.c
@@ -0,0 +1,158 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ * Author: Samuel Hsieh <samuel.hsieh@mediatek.com>
+ */
+
+#include <linux/module.h>
+#include <linux/mutex.h>
+#include <linux/kthread.h>
+#include <linux/device.h>
+#include <linux/power_supply.h>
+#include <mtk_battery_percentage_throttling.h>
+
+#define BAT_PERCENT_LIMIT 15
+
+static struct task_struct *bat_percent_notify_thread;
+static bool bat_percent_notify_flag;
+static DECLARE_WAIT_QUEUE_HEAD(bat_percent_notify_waiter);
+static struct wakeup_source bat_percent_notify_lock;
+static DEFINE_MUTEX(bat_percent_notify_mutex);
+static int g_battery_percent_level;
+
+struct battery_percent_callback_table {
+	void (*bpcb)(enum BATTERY_PERCENT_LEVEL_TAG);
+};
+
+#define BPCB_MAX_NUM 16
+
+static struct battery_percent_callback_table bpcb_tb[BPCB_MAX_NUM] = { {0} };
+
+static struct notifier_block bp_nb;
+
+void register_battery_percent_notify(
+	battery_percent_callback bp_cb,
+	BATTERY_PERCENT_PRIO prio_val)
+{
+	if (prio_val >= BPCB_MAX_NUM || prio_val < 0) {
+		pr_info("[%s] prio_val=%d, out of boundary\n",
+			__func__, prio_val);
+		return;
+	}
+
+	bpcb_tb[prio_val].bpcb = bp_cb;
+	pr_info("[%s] prio_val=%d\n", __func__, prio_val);
+
+	if (g_battery_percent_level == 1) {
+		pr_info("[%s] level 1 happen\n", __func__);
+		if (bp_cb != NULL)
+			bp_cb(BATTERY_PERCENT_LEVEL_1);
+	}
+}
+
+void exec_battery_percent_callback(
+	enum BATTERY_PERCENT_LEVEL_TAG battery_percent_level)
+{
+	int i;
+
+	for (i = 0; i < BPCB_MAX_NUM; i++) {
+		if (bpcb_tb[i].bpcb != NULL) {
+			bpcb_tb[i].bpcb(battery_percent_level);
+			pr_info("[%s] prio_val=%d, battery_percent_level=%d\n"
+				, __func__, i, battery_percent_level);
+		}
+	}
+}
+
+int bat_percent_notify_handler(void *unused)
+{
+	do {
+		wait_event_interruptible(bat_percent_notify_waiter,
+			(bat_percent_notify_flag == true));
+
+		__pm_stay_awake(&bat_percent_notify_lock);
+		mutex_lock(&bat_percent_notify_mutex);
+
+		exec_battery_percent_callback(g_battery_percent_level);
+		bat_percent_notify_flag = false;
+
+		mutex_unlock(&bat_percent_notify_mutex);
+		__pm_relax(&bat_percent_notify_lock);
+	} while (!kthread_should_stop());
+
+	return 0;
+}
+
+int bp_psy_event(struct notifier_block *nb, unsigned long event, void *v)
+{
+	struct power_supply *psy = v;
+	union power_supply_propval val;
+	int ret = 0;
+	int uisoc, bat_status;
+
+	if (strcmp(psy->desc->name, "battery") != 0)
+		return NOTIFY_DONE;
+
+	ret = power_supply_get_property(psy, POWER_SUPPLY_PROP_CAPACITY, &val);
+	if (ret)
+		return NOTIFY_DONE;
+
+	uisoc = val.intval;
+
+	ret = power_supply_get_property(psy, POWER_SUPPLY_PROP_STATUS, &val);
+	if (ret)
+		return NOTIFY_DONE;
+
+	bat_status = val.intval;
+
+	if ((bat_status != POWER_SUPPLY_STATUS_CHARGING) &&
+		(g_battery_percent_level == BATTERY_PERCENT_LEVEL_0) &&
+		(uisoc <= BAT_PERCENT_LIMIT && uisoc >= 0)) {
+		g_battery_percent_level = BATTERY_PERCENT_LEVEL_1;
+		bat_percent_notify_flag = true;
+		wake_up_interruptible(&bat_percent_notify_waiter);
+		pr_info("bat_percent_notify called, l=%d s=%d soc=%d\n",
+			g_battery_percent_level, bat_status, uisoc);
+	} else if ((g_battery_percent_level == BATTERY_PERCENT_LEVEL_1) &&
+		(uisoc > BAT_PERCENT_LIMIT)) {
+		g_battery_percent_level = BATTERY_PERCENT_LEVEL_0;
+		bat_percent_notify_flag = true;
+		wake_up_interruptible(&bat_percent_notify_waiter);
+		pr_info("bat_percent_notify called, l=%d s=%d soc=%d\n",
+			g_battery_percent_level, bat_status, uisoc);
+	}
+
+	return NOTIFY_DONE;
+}
+
+void bat_percent_notify_init(void)
+{
+	wakeup_source_init(&bat_percent_notify_lock,
+		"bat_percent_notify_lock wakelock");
+
+	bat_percent_notify_thread =
+		kthread_run(bat_percent_notify_handler, 0,
+			"bat_percent_notify_thread");
+	if (IS_ERR(bat_percent_notify_thread))
+		pr_notice("Failed to create bat_percent_notify_thread\n");
+
+	bp_nb.notifier_call = bp_psy_event;
+	power_supply_reg_notifier(&bp_nb);
+}
+
+static int __init battery_percentage_throttling_module_init(void)
+{
+	bat_percent_notify_init();
+	return 0;
+}
+
+static void __exit battery_percentage_throttling_module_exit(void)
+{
+}
+
+module_init(battery_percentage_throttling_module_init);
+module_exit(battery_percentage_throttling_module_exit);
+
+MODULE_AUTHOR("Samuel Hsieh");
+MODULE_DESCRIPTION("MTK battery percentage throttling driver");
+MODULE_LICENSE("GPL");
---
+++ drivers/misc/mediatek/power_throttling/mtk_battery_percentage_throttling.h
@@ -0,0 +1,43 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ * Author: Samuel Hsieh <samuel.hsieh@mediatek.com>
+ */
+
+#ifndef __MTK_BATTERY_PERCENTAGE_THROTTLING_H__
+#define __MTK_BATTERY_PERCENTAGE_THROTTLING_H__
+
+#define BATTERY_PERCENT_LEVEL enum BATTERY_PERCENT_LEVEL_TAG
+#define BATTERY_PERCENT_PRIO enum BATTERY_PERCENT_PRIO_TAG
+
+enum BATTERY_PERCENT_LEVEL_TAG {
+	BATTERY_PERCENT_LEVEL_0 = 0,
+	BATTERY_PERCENT_LEVEL_1 = 1
+};
+
+enum BATTERY_PERCENT_PRIO_TAG {
+	BATTERY_PERCENT_PRIO_CPU_B = 0,
+	BATTERY_PERCENT_PRIO_CPU_L = 1,
+	BATTERY_PERCENT_PRIO_GPU = 2,
+	BATTERY_PERCENT_PRIO_MD = 3,
+	BATTERY_PERCENT_PRIO_MD5 = 4,
+	BATTERY_PERCENT_PRIO_FLASHLIGHT = 5,
+	BATTERY_PERCENT_PRIO_VIDEO = 6,
+	BATTERY_PERCENT_PRIO_WIFI = 7,
+	BATTERY_PERCENT_PRIO_BACKLIGHT = 8
+};
+
+typedef void (*battery_percent_callback)(BATTERY_PERCENT_LEVEL tag);
+
+#ifdef CONFIG_MTK_BATTERY_PERCENTAGE_POWER_THROTTLING
+void register_battery_percent_notify(
+		battery_percent_callback bp_cb,
+		BATTERY_PERCENT_PRIO prio_val);
+#else
+static void register_battery_percent_notify(
+		battery_percent_callback bp_cb,
+		BATTERY_PERCENT_PRIO prio_val)
+{};
+#endif
+
+#endif /* __MTK_BATTERY_PERCENTAGE_THROTTLING_H__ */
