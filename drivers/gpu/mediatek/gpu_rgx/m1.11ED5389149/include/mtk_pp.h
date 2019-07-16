ba282b07953915ba5d7bc2319efaf6cee9b509a0
Vincent Sung <vincent.sung@mediatek.com>
2019-07-16 19:40:50 +0800

    arm64: gpu: mt6779: upgrade kernel driver pack
    Upgrade DDK version to fulfill Android Q Graphics requirements 
    Test: local boot up to homescreen on daily build label 2019_07_15_13_00
    
    Change-Id: I6f80b859d8bc9a6a9d4e83c64cbc9b6544330802
    CR-Id: ALPS04699154
    Signed-off-by: Vincent Sung <vincent.sung@mediatek.com>

---
+++ drivers/gpu/mediatek/gpu_rgx/m1.11ED5389149/include/mtk_pp.h
@@ -0,0 +1,80 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+#ifndef __MTK_PP_H__
+#define __MTK_PP_H__
+
+#include <linux/mutex.h>
+
+#if defined(MTK_DEBUG_PROC_PRINT)
+
+#if defined(__GNUC__)
+#define MTK_PP_FORMAT_PRINTF(x, y)	__attribute__((format(printf, x, y)))
+#else
+#define MTK_PP_FORMAT_PRINTF(x, y)
+#endif
+
+typedef enum MTKPP_ID_TAG {
+	MTKPP_ID_FW,
+	MTKPP_ID_SYNC,
+	MTKPP_ID_SHOT_FW,
+
+	MTKPP_ID_SIZE
+} MTKPP_ID;
+
+extern int g_use_id;
+
+typedef enum MTKPP_BUFFERTYPE_TAG {
+	MTKPP_QUEUEBUFFER,
+	MTKPP_RINGBUFFER
+} MTKPP_BUFFERTYPE;
+
+typedef struct MTK_PROC_PRINT_DATA_TAG {
+	MTKPP_BUFFERTYPE type;
+
+	char *data;
+	char **line;
+	int data_array_size;
+	int line_array_size;
+	int current_data;
+	int current_line;
+
+	spinlock_t lock;
+	unsigned long irqflags;
+
+	void (*pfn_print)(struct MTK_PROC_PRINT_DATA_TAG *data, const char *fmt, ...) MTK_PP_FORMAT_PRINTF(2, 3);
+
+} MTK_PROC_PRINT_DATA;
+
+void MTKPP_Init(void);
+void MTKPP_Deinit(void);
+
+MTK_PROC_PRINT_DATA *MTKPP_GetData(MTKPP_ID id);
+
+#define MTKPP_LOG(id, ...) 																\
+	do { 																				\
+		MTK_PROC_PRINT_DATA *mtkpp_data = MTKPP_GetData(id); 							\
+		if (mtkpp_data != NULL) { mtkpp_data->pfn_print(mtkpp_data, __VA_ARGS__); } 	\
+	} while(0)
+
+/* print log into both kerne log and gpulog for time sync */
+void MTKPP_LOGTIME(MTKPP_ID id, const char *);
+
+/* trigger AEE to generate a DB */
+void MTKPP_TriggerAEE(int bug_on);
+
+#else
+
+#define MTKPP_LOG(...)
+#define MTKPP_LOGTIME(...)
+#define MTKPP_TriggerAEE(...)
+
+#endif
+
+#endif	/* __MTK_PP_H__ */
+
+/******************************************************************************
+ End of file (mtk_pp.h)
+******************************************************************************/
+
