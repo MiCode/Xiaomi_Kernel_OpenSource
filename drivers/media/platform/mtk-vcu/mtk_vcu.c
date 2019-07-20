4d9d849a091ec3ee0623597669da3a35676b05a2
Chia-Mao Hung <chia-mao.hung@mediatek.com>
2019-07-20 11:20:09 +0800

    [ALPS04700841] vcu: fixed defect
    dereference before null check
    
    MTK-Commit-Id: ac48fc4d2d591b7950379ddeed385baff6007273
    
    Change-Id: I6e5b3dff1aad313eb0ba8ef64d3d6f1bdb2095d4
    Signed-off-by: Chia-Mao Hung <chia-mao.hung@mediatek.com>
    CR-Id: ALPS04700841
    Feature: [Module]Video Driver

--- drivers/media/platform/mtk-vcu/mtk_vcu.c
+++ drivers/media/platform/mtk-vcu/mtk_vcu.c
@@ -1585,10 +1585,6 @@ static int mtk_vcu_write(const char *val, const struct kernel_param *kp)
 {
 	long ret = -1;
 
-	pr_info("[log wakeup VPUD] log_info %p vcu_ptr %p val %p: %s %lu\n",
-		(char *)vcu_ptr->vdec_log_info->log_info,
-		vcu_ptr, val, val, (unsigned long)strlen(val));
-
 	if (vcu_ptr != NULL &&
 		vcu_ptr->vdec_log_info != NULL &&
 		val != NULL &&
@@ -1601,6 +1597,10 @@ static int mtk_vcu_write(const char *val, const struct kernel_param *kp)
 			val, strlen(val));
 	} else
 		return -EFAULT;
+
+	pr_info("[log wakeup VPUD] log_info %p vcu_ptr %p val %p: %s %lu\n",
+		(char *)vcu_ptr->vdec_log_info->log_info,
+		vcu_ptr, val, val, (unsigned long)strlen(val));
 
 	atomic_set(&vcu_ptr->vdec_log_got, 1);
 	wake_up(&vcu_ptr->vdec_log_get_wq);
