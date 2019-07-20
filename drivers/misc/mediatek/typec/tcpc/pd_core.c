b31e9b7959480bf94bdbe5b19ac5339b987def4d
Eddie Hung <eddie.hung@mediatek.com>
2019-07-20 11:20:03 +0800

    [ALPS04575308] tcpc: fix coverity issue
    [Detail]
    1. fix Out-of-bounds access at function pd_parse_pdata_mfrs
    2. fix Out-of-bounds access at function tcpc_reset_typec_debounce_timer
    3. fix Out-of-bounds access at function pd_parse_pdata_bat_info
    
    MTK-Commit-Id: 00e1c46c795aafa9aace1720fe56309e925c93ab
    
    Change-Id: I3e1408e5a8c9e40645f96d908b62ff5b7ffeff1d
    CR-Id: ALPS04575308
    Feature: USB TYPE-C
    Signed-off-by: Eddie Hung <eddie.hung@mediatek.com>

--- drivers/misc/mediatek/typec/tcpc/pd_core.c
+++ drivers/misc/mediatek/typec/tcpc/pd_core.c
@@ -62,11 +62,10 @@ static inline void pd_parse_pdata_bat_info(
 	ret = of_property_read_string(sub, "bat,mfrs", &mstring);
 	if (ret < 0) {
 		pr_err("%s get bat,mfrs fail\n", __func__);
-		snprintf(mfrs_info->mfrs_string,
-			PAGE_SIZE, "%s", "no_bat_mfrs_string");
-	} else
-		snprintf(mfrs_info->mfrs_string,
-			strlen(mstring)+1, "%s", mstring);
+		mstring = "no_bat_mfrs_string";
+	}
+	snprintf(mfrs_info->mfrs_string,
+		strlen(mstring)+1, "%s", mstring);
 #endif	/* CONFIG_USB_PD_REV30_MFRS_INFO_LOCAL */
 
 	ret = of_property_read_u32(sub, "bat,design_cap", &design_cap);
@@ -333,12 +332,11 @@ static inline void pd_parse_pdata_mfrs(
 
 	ret = of_property_read_string(np, "pd,mfrs", &mstring);
 	if (ret < 0) {
+		mstring = "no_mfrs_string";
 		pr_err("%s get pd mfrs fail\n", __func__);
-		snprintf(mfrs_info->mfrs_string,
-			PAGE_SIZE, "%s", "no_mfrs_string");
-	} else
-		snprintf(mfrs_info->mfrs_string,
-			strlen(mstring)+1, "%s", mstring);
+	}
+	snprintf(mfrs_info->mfrs_string,
+		strlen(mstring)+1, "%s", mstring);
 
 	pr_info("%s PD mfrs_string = %s\n",
 		__func__, mfrs_info->mfrs_string);
