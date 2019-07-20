78f5ec35d650f2756b639aa662b5d9545e8f4ad3
Chang-An Chen <chang-an.chen@mediatek.com>
2019-07-20 11:20:23 +0800

    [ALPS04307558] irtx: update mtk irtx pwm driver
    1. Make irtx pwm driver to common driver
    2. Using 26M clock source to observe high/low period instead of
    fix 1us for each bit to increase precision of irtx waveform.
    
    MTK-Commit-Id: 49fd7edced4273845d250cdc9775160d735f4f0c
    
    Change-Id: I240c64972fd00d9a6235eedb64dab9cca8677b80
    Signed-off-by: Chang-An Chen <chang-an.chen@mediatek.com>
    CR-Id: ALPS04307558
    Feature: ConsumerIR AOSP

--- drivers/misc/mediatek/irtx/mtk_irtx.h
+++ drivers/misc/mediatek/irtx/mtk_irtx.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 MediaTek Inc.
+ * Copyright (C) 2019 MediaTek Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -10,6 +10,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  */
+#include <linux/regulator/consumer.h>
 
 struct mt_irtx {
 	unsigned int pwm_ch;
@@ -19,43 +20,21 @@ struct mt_irtx {
 	struct platform_device *plat_dev;
 	unsigned int carrier_freq;
 	atomic_t usage_cnt;
-
-	/* struct clk *clk_irtx_main; */
 	struct clk *clk_irtx_main;
 	/* GPIO pin control */
 	struct pinctrl *ppinctrl_irtx;
-};
-
-struct irtx_config {
-	unsigned int start : 1;
-	unsigned int mode : 2;
-	unsigned int sw_o : 1;
-	unsigned int b_ord : 1;
-	unsigned int r_ord : 1;
-	unsigned int ir_os : 1;
-	unsigned int ir_inv : 1;
-	unsigned int bit_num : 7;
-	unsigned int data_inv : 1;
+	/* for MT6370 sub-pmic control */
+	struct regulator *buck;
 };
 
 #define IRTX_IOC_SET_CARRIER_FREQ   _IOW('R', 0, unsigned int)
 #define IRTX_IOC_GET_SOLUTTION_TYPE _IOR('R', 1, unsigned int)
+#define IRTX_IOC_SET_DUTY_CYCLE     _IOW('R', 2, unsigned int)
 #define IRTX_IOC_SET_IRTX_LED_EN    _IOW('R', 10, unsigned int)
-
-#define irtx_write32(b, a, v) mt_reg_sync_writel(v, (b)+(a))
-#define irtx_read32(b, a) ioread32((void __iomem *)((b)+(a)))
-
-#define CLOCK_SRC 26 /* MHz */
-
-#define IRTXCFG 0x0
-#define IRTXD0 0x4
-#define IRTXD1 0x8
-#define IRTXD2 0xC
-#define IRTX_L0H 0x10
-#define IRTX_L0L 0x14
-#define IRTX_L1H 0x18
-#define IRTX_L1L 0x1C
-#define IRTXSYNCH 0x20
-#define IRTXSYNCL 0x24
-#define IRTXMT 0x28
+#ifdef CONFIG_COMPAT
+#define COMPAT_IRTX_IOC_SET_CARRIER_FREQ   _IOW('R', 0, compat_uint_t)
+#define COMPAT_IRTX_IOC_GET_SOLUTTION_TYPE _IOR('R', 1, compat_uint_t)
+#define COMPAT_IRTX_IOC_SET_DUTY_CYCLE     _IOW('R', 2, compat_uint_t)
+#define COMPAT_IRTX_IOC_SET_IRTX_LED_EN    _IOW('R', 10, compat_uint_t)
+#endif
 
