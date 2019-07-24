5988c09109c13245f7ecd228a4b48b02d6eafe2f
Chandana Kishori Chiluveru <cchiluve@codeaurora.org>
2019-07-24 15:01:20 +0530

    usb: phy: snps: Turn off CXO clock from msm_hsphy_dpdm_regulator_enable
    dpdm_regulator_enable() function turn on the CXO clk to program the phy
    registers to put PHY in non-driving mode for charger detection. In charger
    connected case phy driver voting for CXO clock in dpdm_regulator_enable
    but does not disabling it which is preventing the system from
    vdd minimization. Hence fix this issue by turning off the CXO clk
    from dpdm_regulator_enable.
    
    Change-Id: Ib3a04eedcef625443077199d5920884f114db82d
    Signed-off-by: Chandana Kishori Chiluveru <cchiluve@codeaurora.org>

--- drivers/usb/phy/phy-msm-snps-hs.c
+++ drivers/usb/phy/phy-msm-snps-hs.c
@@ -615,6 +615,7 @@ static int msm_hsphy_dpdm_regulator_enable(struct regulator_dev *rdev)
 					UTMI_PHY_DATAPATH_CTRL_OVERRIDE_EN,
 					UTMI_PHY_DATAPATH_CTRL_OVERRIDE_EN);
 
+		msm_hsphy_enable_clocks(phy, false);
 		phy->dpdm_enable = true;
 	}
 	mutex_unlock(&phy->phy_lock);
@@ -633,7 +634,6 @@ static int msm_hsphy_dpdm_regulator_disable(struct regulator_dev *rdev)
 	mutex_lock(&phy->phy_lock);
 	if (phy->dpdm_enable) {
 		if (!phy->cable_connected) {
-			msm_hsphy_enable_clocks(phy, false);
 			ret = msm_hsphy_enable_power(phy, false);
 			if (ret < 0) {
 				mutex_unlock(&phy->phy_lock);
