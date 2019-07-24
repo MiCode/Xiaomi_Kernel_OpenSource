9add616db575e7a4544b1dba741013c7329a6fcc
Jeter Chen <jeter.chen@mediatek.com>
2019-07-24 16:38:32 +0800

    PT: mediatek: add PMIC lbat service
    This patch add PMIC lbat service to detect if VBAT is higher or lower
    some threshold and trigger interrupt to notify.
    
    Change-Id: Ia1b48efa8d53f811724602da5887edc9e183c566
    Signed-off-by: Jeter Chen <jeter.chen@mediatek.com>
    CR-Id: ALPS04704077
    Feature: Dynamic load power throttling

---
+++ drivers/misc/mediatek/power_throttling/pmic_lbat_service.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#ifndef __MTK_LBAT_SERVICE_H__
+#define __MTK_LBAT_SERVICE_H__
+
+struct lbat_user;
+
+/* extern function */
+struct lbat_user *lbat_user_register(const char *name, unsigned int hv_thd_volt,
+				     unsigned int lv1_thd_volt,
+				     unsigned int lv2_thd_volt,
+				     void (*callback)(unsigned int thd_volt));
+int lbat_user_set_debounce(struct lbat_user *user,
+			   unsigned int hv_deb_prd, unsigned int hv_deb_times,
+			   unsigned int lv_deb_prd, unsigned int lv_deb_times);
+unsigned int lbat_read_raw(void);
+unsigned int lbat_read_volt(void);
+
+#endif	/* __MTK_LBAT_SERVICE_H__ */
+
