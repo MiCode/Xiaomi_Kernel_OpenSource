2644111ef2d75d7069ffeca7ec7e4d738b84c982
Lina Iyer <ilina@codeaurora.org>
2019-07-24 09:45:43 -0600

    spdx: Modify spdx tag from GPL-2.0 to GPL-2.0-only
    Modify spdx tag for power drivers for Lahaina. Update year in copyright
    information.
    
    Change-Id: Icac12f1ff1a46f6498212b1ed12c183d2af28ee8
    Signed-off-by: Lina Iyer <ilina@codeaurora.org>

--- include/soc/qcom/tcs.h
+++ include/soc/qcom/tcs.h
@@ -1,7 +1,5 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-/*
- * Copyright (c) 2016-2018, The Linux Foundation. All rights reserved.
- */
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Copyright (c) 2016-2019, The Linux Foundation. All rights reserved. */
 
 #ifndef __SOC_QCOM_TCS_H__
 #define __SOC_QCOM_TCS_H__
