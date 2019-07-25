a315a7b50103ee1bd9853dda88764368f4311d6a
Tri Vo <trong@google.com>
2019-07-25 16:57:16 -0700

    ANDROID: xfrm: remove in_compat_syscall() checks
    This hack is needed to run 32-bit userspace on 64-bit kernel.
    
    Bug: 138147164
    Test: kernel_net_tests
    Signed-off-by: Tri Vo <trong@google.com>
    Change-Id: I083d32b45ca985cfadfe3ce57d253b63202befde

--- net/xfrm/xfrm_user.c
+++ net/xfrm/xfrm_user.c
@@ -2569,11 +2569,6 @@ static int xfrm_user_rcv_msg(struct sk_buff *skb, struct nlmsghdr *nlh)
 	const struct xfrm_link *link;
 	int type, err;
 
-#ifdef CONFIG_COMPAT
-	if (in_compat_syscall())
-		return -EOPNOTSUPP;
-#endif
-
 	type = nlh->nlmsg_type;
 	if (type > XFRM_MSG_MAX)
 		return -EINVAL;
