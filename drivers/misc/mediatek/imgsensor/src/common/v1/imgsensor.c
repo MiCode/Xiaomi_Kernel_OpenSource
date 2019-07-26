aeb19418abfe850cc90b6112a820c1dedfe1b471
Auto Li <auto.li@mediatek.com>
2019-07-26 11:57:37 +0800

    [ALPS04712500] CAM: Fix coverity
    Fix coverity
    
    MTK-Commit-Id: 268892d1ce65a9b9465329964d7fe848f3391c43
    
    Change-Id: Ib2de135da911aa0de6629ef85c8f4a7fe2d0daaa
    Signed-off-by: Auto Li <auto.li@mediatek.com>
    CR-Id: ALPS04712500
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/misc/mediatek/imgsensor/src/common/v1/imgsensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1/imgsensor.c
@@ -172,8 +172,7 @@ imgsensor_sensor_open(struct IMGSENSOR_SENSOR *psensor)
 #ifdef CONFIG_MTK_CCU
 	struct ccu_sensor_info ccuSensorInfo;
 	enum IMGSENSOR_SENSOR_IDX sensor_idx = psensor->inst.sensor_idx;
-	struct i2c_client *pi2c_client =
-		psensor_inst->i2c_cfg.pinst->pi2c_client;
+	struct i2c_client *pi2c_client = NULL;
 #endif
 
 	IMGSENSOR_FUNCTION_ENTRY();
@@ -223,10 +222,13 @@ imgsensor_sensor_open(struct IMGSENSOR_SENSOR *psensor)
 			ccuSensorInfo.sensor_name_string =
 			    (char *)(psensor_inst->psensor_name);
 
+			pi2c_client = psensor_inst->i2c_cfg.pinst->pi2c_client;
 			if (pi2c_client)
 				ccuSensorInfo.i2c_id =
 					(((struct mt_i2c *) i2c_get_adapdata(
 						pi2c_client->adapter))->id);
+			else
+				ccuSensorInfo.i2c_id = -1;
 
 			ccu_set_sensor_info(sensor_idx, &ccuSensorInfo);
 #endif
