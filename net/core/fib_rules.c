eba8f2a8630f4b1943105dbc427414b94039fc62
Sharath Chandra Vurukala <sharathv@codeaurora.org>
2019-07-29 16:16:50 +0530

    Revert "fib_rules: fix error in backport of e9919a24d302"
    This reverts commit b2fc35160871b97f132c8bd00b65558c93529be8.
    
    Revert "fib_rules: return 0 directly if an exactly same rule"
    "exists when NLM_F_EXCL not supplied"
    
    This reverts commit 1fff19a925e524556e85efcd728efad5274ce5b6.
    
    Change-Id: If18c802f64a41a83721e2ecf06136ac1ae8fa6cb
    Signed-off-by: Sharath Chandra Vurukala <sharathv@codeaurora.org>

--- net/core/fib_rules.c
+++ net/core/fib_rules.c
@@ -486,10 +486,9 @@ int fib_nl_newrule(struct sk_buff *skb, struct nlmsghdr *nlh)
 		rule->uid_range = fib_kuid_range_unset;
 	}
 
-	if (rule_exists(ops, frh, tb, rule)) {
-		err = 0;
-		if (nlh->nlmsg_flags & NLM_F_EXCL)
-			err = -EEXIST;
+	if ((nlh->nlmsg_flags & NLM_F_EXCL) &&
+	    rule_exists(ops, frh, tb, rule)) {
+		err = -EEXIST;
 		goto errout_free;
 	}
 
