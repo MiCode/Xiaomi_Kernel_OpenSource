86ba212e83dd18fd6f8e14dc642b583e3a0e73a8
Krishna Manikandan <mkrishn@codeaurora.org>
2019-07-30 14:13:44 +0530

    drm/msm/sde: add support to handle reduction in dma linewidth
    Add support to handle those targets where dma
    pipe width is less compared to vig pipewidth.
    
    Change-Id: I70dbd44b4883f49879686003ba1fe9694434daab
    Signed-off-by: Jayaprakash <jmadiset@codeaurora.org>
    Signed-off-by: Krishna Manikandan <mkrishn@codeaurora.org>

--- drivers/gpu/drm/msm/sde/sde_hw_catalog.h
+++ drivers/gpu/drm/msm/sde/sde_hw_catalog.h
@@ -1093,6 +1093,7 @@ struct sde_perf_cfg {
  * register offsets, capabilities of the all MDSS HW sub-blocks.
  *
  * @max_sspp_linewidth max source pipe line width support.
+ * @vig_sspp_linewidth max vig source pipe line width support.
  * @max_mixer_width    max layer mixer line width support.
  * @max_mixer_blendstages max layer mixer blend stages or
  *                       supported z order
@@ -1147,6 +1148,7 @@ struct sde_mdss_cfg {
 	u32 hwversion;
 
 	u32 max_sspp_linewidth;
+	u32 vig_sspp_linewidth;
 	u32 max_mixer_width;
 	u32 max_mixer_blendstages;
 	u32 max_wb_linewidth;
