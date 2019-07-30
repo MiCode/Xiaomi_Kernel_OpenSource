3f167d42a5a4a14806fd3db749f484942233deb2
Chih-Wei Chiou <chih-wei.chiou@mediatek.com>
2019-07-30 21:15:52 +0800

    lpm: mediatek: Introduce low power debugfs
    1. Add low power debug common
    2. Add low power suspend debug feature
    3. Add low power idle debug feature
    4. Add low power pwr debug feature
    
    Change-Id: I8974b5a37b23da800c6b71ffafaddfef2215f5e5
    Signed-off-by: Chih-Wei Chiou <chih-wei.chiou@mediatek.com>
    CR-Id: ALPS04681538
    Feature: Power Management

---
+++ drivers/misc/mediatek/lpm/modules/include/mt6779/mt6779_cond.h
@@ -0,0 +1,42 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#ifndef __MT6779_COND_H__
+#define __MT6779_COND_H__
+
+/* Definition about SPM_COND_CHECK_BLOCKED
+ * bit [00 ~ 15]: cg blocking index
+ * bit [16 ~ 29]: pll blocking index
+ * bit [30]     : pll blocking information
+ * bit [31]    : idle condition check fail
+ */
+
+#define SPM_COND_BLOCKED_CG_IDX		(0)
+#define SPM_COND_BLOCKED_PLL_IDX	(16)
+#define SPM_COND_BLOCKED_PLL		(1<<30L)
+#define SPM_COND_CHECK_FAIL		(1<<31L)
+
+enum PLAT_SPM_COND {
+	PLAT_SPM_COND_MTCMOS_0 = 0,
+	PLAT_SPM_COND_MTCMOS_1,
+	PLAT_SPM_COND_CG_INFRA_0,
+	PLAT_SPM_COND_CG_INFRA_1,
+	PLAT_SPM_COND_CG_INFRA_2,
+	PLAT_SPM_COND_CG_INFRA_3,
+	PLAT_SPM_COND_CG_MMSYS_0,
+	PLAT_SPM_COND_CG_MMSYS_1,
+	PLAT_SPM_COND_MAX,
+};
+
+enum PLAT_SPM_PLL_COND {
+	PLAT_SPM_COND_UNIVPLL = 0,
+	PLAT_SPM_COND_MFGPLL,
+	PLAT_SPM_COND_MSDCPLL,
+	PLAT_SPM_COND_TVPLL,
+	PLAT_SPM_COND_MMPLL,
+	PLAT_SPM_COND_PLL_MAX,
+};
+
+#endif
---
+++ drivers/misc/mediatek/lpm/modules/include/mtk_dbg_common_v1.h
@@ -0,0 +1,63 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#ifndef __MTK_DBG_COMMON_H__
+#define __MTK_DBG_COMMON_H__
+
+/* For sysfs get the last wake status */
+enum mt6779_spm_wake_status_enum {
+	WAKE_STA_R12,
+	WAKE_STA_R12_EXT,
+	WAKE_STA_RAW_STA,
+	WAKE_STA_RAW_EXT_STA,
+	WAKE_STA_WAKE_MISC,
+	WAKE_STA_TIMER_OUT,
+	WAKE_STA_R13,
+	WAKE_STA_IDLE_STA,
+	WAKE_STA_REQ_STA,
+	WAKE_STA_DEBUG_FLAG,
+	WAKE_STA_DEBUG_FLAG1,
+	WAKE_STA_ISR,
+
+	WAKE_STA_MAX_COUNT,
+};
+
+enum {
+	WR_NONE = 0,
+	WR_UART_BUSY = 1,
+	WR_ABORT = 2,
+	WR_PCM_TIMER = 3,
+	WR_WAKE_SRC = 4,
+	WR_DVFSRC = 5,
+	WR_PMSR = 6,
+	WR_TWAM = 7,
+	WR_SPM_ACK_CHK = 8,
+	WR_UNKNOWN = 9,
+};
+
+
+/* enum for smc resource request arg */
+enum MT_SPM_RES_TYPE {
+	MT_SPM_RES_XO_FPM,
+	MT_SPM_RES_CK_26M,
+	MT_SPM_RES_INFRA,
+	MT_SPM_RES_SYSPLL,
+	MT_SPM_RES_DRAM_S0,
+	MT_SPM_RES_DRAM_S1,
+	MT_SPM_RES_MAX,
+};
+
+enum dbg_ctrl_enum {
+	DBG_CTRL_COUNT,
+	DBG_CTRL_DURATION,
+	DBG_CTRL_MAX,
+};
+
+struct dbg_ctrl {
+	u32 count;
+	u32 duration;
+};
+
+#endif /* __MTK_DBG_COMMON_H__ */
