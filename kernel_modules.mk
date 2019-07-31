82c04a33f5f2ad188eeec517821c643dae206a50
Celeste Wang <celeste.wang@mediatek.com>
2019-07-31 20:34:17 +0800

    Android: modify kernel_modules.mk to fix build error
    Fix build error when depmod in-tree kernel modules installed to ramdisk.
    Not depmod when BOARD_RAMDISK_KERNEL_MODULES is not defined.
    
    Change-Id: I0b22e791b80c528959899930eb002460ec9a2b81
    Signed-off-by: Celeste Wang <Celeste.Wang@mediatek.com>
    CR-Id: ALPS04671194
    Feature: build process

--- kernel_modules.mk
+++ kernel_modules.mk
@@ -2,13 +2,9 @@
 # Copyright (C) 2019 MediaTek Inc.
 
 # in-tree kernel modules installed to vendor
-board_vendor_kernel_modules :=
-
-BOARD_VENDOR_KERNEL_MODULES += $(board_vendor_kernel_modules)
-$(board_vendor_kernel_modules): $(KERNEL_ZIMAGE_OUT);
+BOARD_VENDOR_KERNEL_MODULES +=
+$(BOARD_VENDOR_KERNEL_MODULES): $(KERNEL_ZIMAGE_OUT);
 
 # in-tree kernel modules installed to ramdisk
-board_ramdisk_kernel_modules :=
-
-BOARD_RAMDISK_KERNEL_MODULES += $(board_ramdisk_kernel_modules)
-$(board_ramdisk_kernel_modules): $(KERNEL_ZIMAGE_OUT);
+BOARD_RAMDISK_KERNEL_MODULES +=
+$(BOARD_RAMDISK_KERNEL_MODULES): $(KERNEL_ZIMAGE_OUT);
