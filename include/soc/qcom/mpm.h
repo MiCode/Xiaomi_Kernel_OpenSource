67371f6ddcc517f88869d8da3c9d425180d46e5e
Raghavendra Kakarla <rkakarla@codeaurora.org>
2019-08-01 11:14:24 +0530

    drivers: irqchip: mpm: Add QTI SoC interrupt controller
    Add the mpm as the wakeup capable interrupt controller.
    
    Change-Id: Ibd437ecd4efe4db134a0a026404f9b3170685db5
    Signed-off-by: Raghavendra Kakarla <rkakarla@codeaurora.org>

---
+++ include/soc/qcom/mpm.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef __QCOM_MPM_H__
+#define __QCOM_MPM_H__
+
+#include <linux/irq.h>
+#include <linux/device.h>
+
+struct mpm_pin {
+	int pin;
+	irq_hw_number_t hwirq;
+};
+
+#endif /* __QCOM_MPM_H__ */
