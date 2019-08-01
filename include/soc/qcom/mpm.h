8f6aff461d93fac10d1fe100af9b1aa24399307a
Raghavendra Kakarla <rkakarla@codeaurora.org>
2019-08-01 11:14:24 +0530

    drivers: irqchip: mpm: Add QTI SOC interrupt controller
    Add the mpm as the wakeup capable interrupt controller for
    bengal.
    
    Change-Id: Ibd437ecd4efe4db134a0a026404f9b3170685db5
    Signed-off-by: Raghavendra Kakarla <rkakarla@codeaurora.org>

---
+++ include/soc/qcom/mpm.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2018-2019, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef __QCOM_MPM_H__
+#define __QCOM_MPM_H__
+
+#include <linux/irq.h>
+#include <linux/device.h>
+
+struct mpm_pin {
+	int pin;
+	irq_hw_number_t hwirq;
+};
+
+extern const struct mpm_pin mpm_bengal_gic_chip_data[];
+#endif /* __QCOM_MPM_H__ */
