0adca3fb721e76116d0e41be2b800d3f4eb10309
Xiaowen Wu <wxiaowen@codeaurora.org>
2019-08-01 11:02:10 -0400

    drm/msm/dsi-staging: decouple label and display type for DSI
    DSI driver requires label and display type to be the same,
    which introduces the limitation that only DSI display can be the
    primary display. This change will decouple the label and display
    type to allow other display to set to primary.
    
    Change-Id: Ibff404ddc9927c239420861af6bce025ced340cc
    Signed-off-by: Xiaowen Wu <wxiaowen@codeaurora.org>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.h
@@ -147,6 +147,7 @@ struct dsi_display_ext_bridge {
  * @ext_conn:         Pointer to external connector attached to DSI connector
  * @name:             Name of the display.
  * @display_type:     Display type as defined in device tree.
+ * @dsi_type:         Display label as defined in device tree.
  * @list:             List pointer.
  * @is_active:        Is display active.
  * @is_cont_splash_enabled:  Is continuous splash enabled
@@ -198,6 +199,7 @@ struct dsi_display {
 
 	const char *name;
 	const char *display_type;
+	const char *dsi_type;
 	struct list_head list;
 	bool is_cont_splash_enabled;
 	bool sw_te_using_wd;
