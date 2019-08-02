738a5299bbe57f06b02b2383ba0ec0ed0bf61ea6
Anirudh Ghayal <aghayal@codeaurora.org>
2019-08-02 16:56:14 +0530

    power: qpnp-qg: Update the function names for QG
    Update the QG profile handling function names
    by adding a prefix "qg_", this is to differentiate
    it from the generic battery profile function
    definitions.
    
    While at it, expose the linear_interpolate function
    for it to be usable in the QG driver.
    
    Change-Id: I58214402b2d79feb25209f5a1451d3f41edf181a
    Signed-off-by: Anirudh Ghayal <aghayal@codeaurora.org>

--- drivers/power/supply/qcom/qg-battery-profile.c
+++ drivers/power/supply/qcom/qg-battery-profile.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2018-2019 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -107,7 +107,8 @@ static long qg_battery_data_ioctl(struct file *file, unsigned int cmd,
 			rc = -EINVAL;
 		} else {
 			/* OCV is passed as deci-uV  - 10^-4 V */
-			soc = interpolate_soc(&battery->profile[bp.table_index],
+			soc = qg_interpolate_soc(
+					&battery->profile[bp.table_index],
 					bp.batt_temp, UV_TO_DECIUV(bp.ocv_uv));
 			soc = CAP(QG_MIN_SOC, QG_MAX_SOC, soc);
 			rc = put_user(soc, &bp_user->soc);
@@ -127,7 +128,7 @@ static long qg_battery_data_ioctl(struct file *file, unsigned int cmd,
 					bp.table_index);
 			rc = -EINVAL;
 		} else {
-			ocv_uv = interpolate_var(
+			ocv_uv = qg_interpolate_var(
 					&battery->profile[bp.table_index],
 					bp.batt_temp, bp.soc);
 			ocv_uv = DECIUV_TO_UV(ocv_uv);
@@ -149,7 +150,7 @@ static long qg_battery_data_ioctl(struct file *file, unsigned int cmd,
 					bp.table_index);
 			rc = -EINVAL;
 		} else {
-			fcc_mah = interpolate_single_row_lut(
+			fcc_mah = qg_interpolate_single_row_lut(
 					&battery->profile[bp.table_index],
 					bp.batt_temp, DEGC_SCALE);
 			fcc_mah = CAP(QG_MIN_FCC_MAH, QG_MAX_FCC_MAH, fcc_mah);
@@ -169,7 +170,8 @@ static long qg_battery_data_ioctl(struct file *file, unsigned int cmd,
 					bp.table_index);
 			rc = -EINVAL;
 		} else {
-			var = interpolate_var(&battery->profile[bp.table_index],
+			var = qg_interpolate_var(
+					&battery->profile[bp.table_index],
 					bp.batt_temp, bp.soc);
 			var = CAP(QG_MIN_VAR, QG_MAX_VAR, var);
 			rc = put_user(var, &bp_user->var);
@@ -189,7 +191,7 @@ static long qg_battery_data_ioctl(struct file *file, unsigned int cmd,
 					bp.table_index);
 			rc = -EINVAL;
 		} else {
-			slope = interpolate_slope(
+			slope = qg_interpolate_slope(
 					&battery->profile[bp.table_index],
 					bp.batt_temp, bp.soc);
 			slope = CAP(QG_MIN_SLOPE, QG_MAX_SLOPE, slope);
@@ -401,7 +403,7 @@ int lookup_soc_ocv(u32 *soc, u32 ocv_uv, int batt_temp, bool charging)
 	if (!the_battery || !the_battery->profile_node)
 		return -ENODEV;
 
-	*soc = interpolate_soc(&the_battery->profile[table_index],
+	*soc = qg_interpolate_soc(&the_battery->profile[table_index],
 				batt_temp, UV_TO_DECIUV(ocv_uv));
 
 	*soc = CAP(0, 100, DIV_ROUND_CLOSEST(*soc, 100));
@@ -417,7 +419,7 @@ int qg_get_nominal_capacity(u32 *nom_cap_uah, int batt_temp, bool charging)
 	if (!the_battery || !the_battery->profile_node)
 		return -ENODEV;
 
-	fcc_mah = interpolate_single_row_lut(
+	fcc_mah = qg_interpolate_single_row_lut(
 				&the_battery->profile[table_index],
 					batt_temp, DEGC_SCALE);
 	fcc_mah = CAP(QG_MIN_FCC_MAH, QG_MAX_FCC_MAH, fcc_mah);
