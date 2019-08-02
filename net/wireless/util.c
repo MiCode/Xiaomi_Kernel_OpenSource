a334ed63b2ec01c6b986e376e636a0b86f332616
Arend van Spriel <arend.vanspriel@broadcom.com>
2019-08-02 13:31:00 +0200

    cfg80211: util: add 6GHz channel to freq conversion and vice versa
    Extend the functions ieee80211_channel_to_frequency() and
    ieee80211_frequency_to_channel() to support 6GHz band according
    specification in 802.11ax D4.1 27.3.22.2.
    
    Reviewed-by: Pieter-Paul Giesberts <pieter-paul.giesberts@broadcom.com>
    Reviewed-by: Leon Zegers <leon.zegers@broadcom.com>
    Signed-off-by: Arend van Spriel <arend.vanspriel@broadcom.com>
    Link: https://lore.kernel.org/r/1564745465-21234-4-git-send-email-arend.vanspriel@broadcom.com
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Git-commit: fa1f1085bc063da5a44f779c9b655b7026c52d68
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/jberg/mac80211-next.git
    Change-Id: I74d18fd05c7180ee8411334096392c5470c0d8ae
    CRs-Fixed: 2536741
    Signed-off-by: Liangwei Dong <liangwei@codeaurora.org>

--- net/wireless/util.c
+++ net/wireless/util.c
@@ -87,6 +87,11 @@ int ieee80211_channel_to_frequency(int chan, enum nl80211_band band)
 		else
 			return 5000 + chan * 5;
 		break;
+	case NL80211_BAND_6GHZ:
+		/* see 802.11ax D4.1 27.3.22.2 */
+		if (chan <= 253)
+			return 5940 + chan * 5;
+		break;
 	case NL80211_BAND_60GHZ:
 		if (chan < 5)
 			return 56160 + chan * 2160;
@@ -107,8 +112,11 @@ int ieee80211_frequency_to_channel(int freq)
 		return (freq - 2407) / 5;
 	else if (freq >= 4910 && freq <= 4980)
 		return (freq - 4000) / 5;
-	else if (freq <= 45000) /* DMG band lower limit */
+	else if (freq < 5940)
 		return (freq - 5000) / 5;
+	else if (freq <= 45000) /* DMG band lower limit */
+		/* see 802.11ax D4.1 27.3.22.2 */
+		return (freq - 5940) / 5;
 	else if (freq >= 58320 && freq <= 64800)
 		return (freq - 56160) / 2160;
 	else
