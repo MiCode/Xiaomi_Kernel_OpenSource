a6a70d50b5c979c8d230e0cda503ebea9b9fe284
Rishabh Bhatnagar <rishabhb@codeaurora.org>
2019-08-05 12:58:18 -0700

    pinctrl: Add api to enable/disable wakeup capability for a gpio
    Add api to write to mpm wakeup registers to configure a gpio
    as wakeup capable.
    
    Change-Id: Ia11a22160394e76969200691cfaf8fb1505bf5c1
    Signed-off-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>

--- include/linux/pinctrl/qcom-pinctrl.h
+++ include/linux/pinctrl/qcom-pinctrl.h
@@ -10,5 +10,7 @@
 int msm_qup_write(u32 mode, u32 val);
 int msm_qup_read(u32 mode);
 
+/* API to write to mpm_wakeup registers */
+int msm_gpio_mpm_wake_set(unsigned int gpio, bool enable);
 
 #endif /* __LINUX_PINCTRL_MSM_H__ */
