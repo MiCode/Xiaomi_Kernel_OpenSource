d48ad555456fa9257aebfeb6c9c0e04dc2b59ba9
Xinghua Yang <xinghua.yang@mediatek.com>
2019-08-05 17:18:54 +0800

    aee: add skipping align checking strncmp api
    Mrdump_mini header using noncached mapping memory
    Add skipping align checking strncmp api to eliminate potential risk of
    alignment accessing issue
    
    Change-Id: I6d6be94c484dba7585b731f74ab146628edc48f3
    Signed-off-by: Xinghua Yang <xinghua.yang@mediatek.com>
    CR-Id: ALPS04737230
    Feature: Android Exception Engine(AEE)

--- drivers/misc/mediatek/aee/mrdump/mrdump_mini.c
+++ drivers/misc/mediatek/aee/mrdump/mrdump_mini.c
@@ -270,6 +270,26 @@ static int fill_psinfo(struct elf_prpsinfo *psinfo)
 	return 0;
 }
 
+/*
+ * skip align checking purpose only
+ */
+int strncmp_sac(const char *cs, const char *ct, size_t count)
+{
+	unsigned char c1, c2;
+
+	while (count) {
+		c1 = *cs++;
+		c2 = *ct++;
+		if (c1 != c2)
+			return c1 < c2 ? -1 : 1;
+		if (!c1)
+			break;
+		count--;
+	}
+	return 0;
+}
+#define strncmp strncmp_sac
+
 #ifndef __pa_nodebug
 #ifdef __pa_symbol_nodebug
 #define __pa_nodebug __pa_symbol_nodebug
