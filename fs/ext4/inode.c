d22ac903fbc86ef62ba2308f6fda3d3866ea0948
Neeraj Soni <neersoni@codeaurora.org>
2019-08-05 20:02:19 +0530

    fs: crypto: fix issue introduced with lts-integration
    Fix the perf issue introduced with wrong conflict resolution of
    'commit 81b89766408b ("Merge android-4.9.175 (e2171b6) into msm-4.9")'.
    
    Change-Id: I3b368c5efd8e297bbe6b5c6e3ad12389a0edde7e
    Signed-off-by: Neeraj Soni <neersoni@codeaurora.org>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -3624,8 +3624,9 @@ static ssize_t ext4_direct_IO(struct kiocb *iocb, struct iov_iter *iter)
 	ssize_t ret;
 	int rw = iov_iter_rw(iter);
 
-#ifdef CONFIG_FS_ENCRYPTION
-	if (IS_ENCRYPTED(inode) && S_ISREG(inode->i_mode))
+#ifdef CONFIG_EXT4_FS_ENCRYPTION
+	if (IS_ENCRYPTED(inode) && S_ISREG(inode->i_mode)
+		&& !fscrypt_using_hardware_encryption(inode))
 		return 0;
 #endif
 
