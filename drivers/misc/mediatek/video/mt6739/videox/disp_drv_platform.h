09962826942af03de7f01d4ec157f74ca29f4991
Kamal Sharma <kamal.sharma@mediatek.com>
2019-08-06 22:53:32 +0800

    [ALPS04684722] DISP:Enable MTK_FB_MMDVFS_SUPPORT
    MTK_FB_MMDVFS_SUPPORT was disabled during bringup due to an error.
    Enable MTK_FB_MMDVFS_SUPPORT to improve display low power
    performance
    
    MTK-Commit-Id: 8d4bbc22be85c95cd9f66a90d3eb66c699abc1eb
    
    Change-Id: I42220b200b30a962bebb8e82a58e7d7074866d76
    Signed-off-by: Kamal Sharma <kamal.sharma@mediatek.com>
    CR-Id: ALPS04684722
    Feature: [Module]Display Driver

--- drivers/misc/mediatek/video/mt6739/videox/disp_drv_platform.h
+++ drivers/misc/mediatek/video/mt6739/videox/disp_drv_platform.h
@@ -42,7 +42,7 @@
 
 /* Wrap SPM/MMDVFS code for early porting */
 #define MTK_FB_SPM_SUPPORT
-/* #define MTK_FB_MMDVFS_SUPPORT*/
+#define MTK_FB_MMDVFS_SUPPORT
 /* #define DISP_SYNC_ENABLE */
 #define MTK_FB_ION_SUPPORT
 #define VIDEO_LAYER_COUNT (3)
