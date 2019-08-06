eb15a2a1cff312ed3e3149c3c7a9725a7d7808a9
Krishna Manikandan <mkrishn@codeaurora.org>
2019-08-06 15:32:34 +0530

    msm/sde/rotator: allow ot limiting for argb format
    As per hardware recommendation, allow ot limiting
    for argb format also.
    
    Change-Id: I016ec7f25878c9babfdd3ba0cbc015461755c411
    Signed-off-by: Krishna Manikandan <mkrishn@codeaurora.org>

--- drivers/media/platform/msm/sde/rotator/sde_rotator_base.c
+++ drivers/media/platform/msm/sde/rotator/sde_rotator_base.c
@@ -265,12 +265,10 @@ u32 sde_mdp_get_ot_limit(u32 width, u32 height, u32 pixfmt, u32 fps, u32 is_rd)
 
 		break;
 	default:
-		if (is_yuv) {
-			if (res <= (RES_1080p * 30))
-				ot_lim = 2;
-			else if (res <= (RES_1080p * 60))
-				ot_lim = 4;
-		}
+		if (res <= (RES_1080p * 30))
+			ot_lim = 2;
+		else if (res <= (RES_1080p * 60))
+			ot_lim = 4;
 		break;
 	}
 
