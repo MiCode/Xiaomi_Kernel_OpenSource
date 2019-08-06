fee403f7119ecace5e45cc68c0f67cb496b6b109
Oscar Liu <oscar.liu@mediatek.com>
2019-08-06 22:56:29 +0800

    [ALPS04698912] apusys: refine policy_constraints
    [Detail]
    1. add constraint for dvfs UT
    2. add thermal interface
    
    MTK-Commit-Id: 0349a4e583d59d57964154fb02e2b3a5caaf2cc5
    
    Change-Id: I92658793ad1ce9cc35eaa13a0c62782f1dceb0d9
    CR-Id: ALPS04698912
    Feature: [Module] APUSYS
    Signed-off-by: Oscar Liu <oscar.liu@mediatek.com>

--- drivers/misc/mediatek/apusys/power/2.0/test/policy_constraints_test.c
+++ drivers/misc/mediatek/apusys/power/2.0/test/policy_constraints_test.c
@@ -18,24 +18,136 @@
 #include "hal_config_power.h"
 #include "apu_log.h"
 
-#define VOLT_CONSTRAINTS_1	(50000)
+
+#define VOLT_CONSTRAINTS_1	(225000)
+#define CONSTRAINTS_2_SIZE 37
+
+enum DVFS_VOLTAGE sram_constraint[CONSTRAINTS_2_SIZE][3] = {
+	{DVFS_VOLT_00_825000_V, DVFS_VOLT_00_825000_V, DVFS_VOLT_00_825000_V},
+	{DVFS_VOLT_00_825000_V, DVFS_VOLT_00_800000_V, DVFS_VOLT_00_825000_V},
+	{DVFS_VOLT_00_825000_V, DVFS_VOLT_00_750000_V, DVFS_VOLT_00_825000_V},
+	{DVFS_VOLT_00_825000_V, DVFS_VOLT_00_700000_V, DVFS_VOLT_00_825000_V},
+	{DVFS_VOLT_00_825000_V, DVFS_VOLT_00_650000_V, DVFS_VOLT_00_825000_V},
+
+	{DVFS_VOLT_00_800000_V, DVFS_VOLT_00_825000_V, DVFS_VOLT_00_825000_V},
+	{DVFS_VOLT_00_800000_V, DVFS_VOLT_00_800000_V, DVFS_VOLT_00_825000_V},
+	{DVFS_VOLT_00_800000_V, DVFS_VOLT_00_750000_V, DVFS_VOLT_00_825000_V},
+	{DVFS_VOLT_00_800000_V, DVFS_VOLT_00_700000_V, DVFS_VOLT_00_825000_V},
+	{DVFS_VOLT_00_800000_V, DVFS_VOLT_00_650000_V, DVFS_VOLT_00_825000_V},
+
+	{DVFS_VOLT_00_750000_V, DVFS_VOLT_00_825000_V, DVFS_VOLT_00_825000_V},
+	{DVFS_VOLT_00_750000_V, DVFS_VOLT_00_800000_V, DVFS_VOLT_00_825000_V},
+	{DVFS_VOLT_00_750000_V, DVFS_VOLT_00_750000_V, DVFS_VOLT_00_825000_V},
+	{DVFS_VOLT_00_750000_V, DVFS_VOLT_00_750000_V, DVFS_VOLT_00_750000_V},
+	{DVFS_VOLT_00_750000_V, DVFS_VOLT_00_700000_V, DVFS_VOLT_00_825000_V},
+	{DVFS_VOLT_00_750000_V, DVFS_VOLT_00_700000_V, DVFS_VOLT_00_750000_V},
+	{DVFS_VOLT_00_750000_V, DVFS_VOLT_00_650000_V, DVFS_VOLT_00_825000_V},
+	{DVFS_VOLT_00_750000_V, DVFS_VOLT_00_650000_V, DVFS_VOLT_00_750000_V},
+	{DVFS_VOLT_00_750000_V, DVFS_VOLT_00_575000_V, DVFS_VOLT_00_750000_V},
+
+	{DVFS_VOLT_00_700000_V, DVFS_VOLT_00_825000_V, DVFS_VOLT_00_825000_V},
+	{DVFS_VOLT_00_700000_V, DVFS_VOLT_00_800000_V, DVFS_VOLT_00_825000_V},
+	{DVFS_VOLT_00_700000_V, DVFS_VOLT_00_750000_V, DVFS_VOLT_00_825000_V},
+	{DVFS_VOLT_00_700000_V, DVFS_VOLT_00_750000_V, DVFS_VOLT_00_750000_V},
+	{DVFS_VOLT_00_700000_V, DVFS_VOLT_00_700000_V, DVFS_VOLT_00_750000_V},
+	{DVFS_VOLT_00_700000_V, DVFS_VOLT_00_650000_V, DVFS_VOLT_00_750000_V},
+	{DVFS_VOLT_00_700000_V, DVFS_VOLT_00_575000_V, DVFS_VOLT_00_750000_V},
+
+	{DVFS_VOLT_00_650000_V, DVFS_VOLT_00_825000_V, DVFS_VOLT_00_825000_V},
+	{DVFS_VOLT_00_650000_V, DVFS_VOLT_00_800000_V, DVFS_VOLT_00_825000_V},
+	{DVFS_VOLT_00_650000_V, DVFS_VOLT_00_750000_V, DVFS_VOLT_00_825000_V},
+	{DVFS_VOLT_00_650000_V, DVFS_VOLT_00_750000_V, DVFS_VOLT_00_750000_V},
+	{DVFS_VOLT_00_650000_V, DVFS_VOLT_00_700000_V, DVFS_VOLT_00_750000_V},
+	{DVFS_VOLT_00_650000_V, DVFS_VOLT_00_650000_V, DVFS_VOLT_00_750000_V},
+	{DVFS_VOLT_00_650000_V, DVFS_VOLT_00_575000_V, DVFS_VOLT_00_750000_V},
+
+	{DVFS_VOLT_00_575000_V, DVFS_VOLT_00_750000_V, DVFS_VOLT_00_750000_V},
+	{DVFS_VOLT_00_575000_V, DVFS_VOLT_00_700000_V, DVFS_VOLT_00_750000_V},
+	{DVFS_VOLT_00_575000_V, DVFS_VOLT_00_650000_V, DVFS_VOLT_00_750000_V},
+	{DVFS_VOLT_00_575000_V, DVFS_VOLT_00_575000_V, DVFS_VOLT_00_750000_V},
+};
+
+
 
 struct hal_param_init_power init_power_data;
 
 int hal_config_power(enum HAL_POWER_CMD cmd, enum DVFS_USER user, void *param)
 {
-	LOG_INF("%s cmd = %d, user = %d\n", __func__, cmd, user);
+	int target_volt = 0;
+	enum DVFS_BUCK buck = 0;
+	int i = 0;
+	static int vpu_curr_volt = DVFS_VOLT_00_575000_V;
+	static int mdla_curr_volt = DVFS_VOLT_00_575000_V;
+	static int vsram_core_volt = DVFS_VOLT_00_750000_V;
+	static int vcore_curr_volt = DVFS_VOLT_00_575000_V;
+
+	if (cmd == PWR_CMD_SET_VOLT) {
+		buck = ((struct hal_param_volt *)param)->target_buck;
+		target_volt = ((struct hal_param_volt *)param)->target_volt;
+		if (buck == VPU_BUCK)
+			vpu_curr_volt = target_volt;
+		if (buck == MDLA_BUCK)
+			mdla_curr_volt = target_volt;
+		if (buck == VCORE_BUCK)
+			vcore_curr_volt = target_volt;
+		//int vpu_curr_volt = apusys_opps.prev_buck_volt[VPU_BUCK];
+		//int mdla_curr_volt = apusys_opps.prev_buck_volt[MDLA_BUCK];
+		vsram_core_volt = apusys_opps.vsram_volatge;
+
+		LOG_INF("%s cmd = %d, buck = %d, voltage=%d\n",
+			__func__, cmd, buck, target_volt);
+
+
+		//if (apusys_policy_checker()) {
+		//		while(1);
+		//	}
+		if ((vpu_curr_volt - mdla_curr_volt >= VOLT_CONSTRAINTS_1) ||
+		(mdla_curr_volt - vpu_curr_volt >= VOLT_CONSTRAINTS_1) ||
+		vpu_curr_volt - vcore_curr_volt >= VOLT_CONSTRAINTS_1) {
+			LOG_INF(">>>>>>>> vpu_curr_volt = %d\n",
+				vpu_curr_volt);
+			LOG_INF(">>>>>>>> mdla_curr_volt = %d\n",
+				mdla_curr_volt);
+			LOG_INF(">>>>>>>> vcore_core_volt = %d\n",
+				vcore_curr_volt);
+			LOG_WRN("%s fail : mdla, vpu volt diff > %d !\n",
+						__func__, VOLT_CONSTRAINTS_1);
+			exit(1);
+	}
+
+		for (i = 0; i < CONSTRAINTS_2_SIZE; i++) {
+			if (vpu_curr_volt == sram_constraint[i][0] &&
+			mdla_curr_volt == sram_constraint[i][1] &&
+			vsram_core_volt == sram_constraint[i][2]) {
+				break;
+			}
+		}
+
+		if (i == CONSTRAINTS_2_SIZE) {
+			LOG_INF("vpu_curr_volt = %d\n", vpu_curr_volt);
+			LOG_INF("mdla_curr_volt = %d\n", mdla_curr_volt);
+			LOG_INF("vsram_cur_volt = %d\n", vsram_core_volt);
+			LOG_WRN("%s fail : constraint violate!\n",
+				__func__);
+			exit(1);
+		}
+	} else {
+		LOG_INF("%s cmd = %d, user = %d\n", __func__, cmd, user);
+	}
 }
 
 int check_constraint_1(void)
 {
 	int vpu_curr_volt = apusys_opps.prev_buck_volt[VPU_BUCK];
 	int mdla_curr_volt = apusys_opps.prev_buck_volt[MDLA_BUCK];
+	int vcore_curr_volt = apusys_opps.prev_buck_volt[VCORE_BUCK];
 
-	if ((vpu_curr_volt - mdla_curr_volt > VOLT_CONSTRAINTS_1) ||
-		(mdla_curr_volt - vpu_curr_volt > VOLT_CONSTRAINTS_1)) {
+	if ((vpu_curr_volt - mdla_curr_volt >= VOLT_CONSTRAINTS_1) ||
+		(mdla_curr_volt - vpu_curr_volt >= VOLT_CONSTRAINTS_1) ||
+		vpu_curr_volt - vcore_curr_volt >= VOLT_CONSTRAINTS_1) {
 		LOG_INF(">>>>>>>> vpu_curr_volt = %d\n", vpu_curr_volt);
 		LOG_INF(">>>>>>>> mdla_curr_volt = %d\n", mdla_curr_volt);
+		LOG_INF(">>>>>>>> vcore_core_volt = %d\n", vcore_curr_volt);
 		LOG_WRN("%s fail : mdla, vpu volt diff larger than %d !\n",
 						__func__, VOLT_CONSTRAINTS_1);
 		return -1; // return non zero for check fail !
