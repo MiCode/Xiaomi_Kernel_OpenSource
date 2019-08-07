4cab47e991aa87b06d1f11ea238310e6172d62ba
harigovi <harigovi@codeaurora.org>
2019-08-07 15:25:47 +0530

    drm/msm/dsi-staging: add support for controller version 2.4
    This change adds support for chipsets supporting controller
    version 2.4.
    
    Change-Id: I44b95fcd1d3b719658e66ce47f8506433bd3083c
    Signed-off-by: harigovi <harigovi@codeaurora.org>

--- drivers/gpu/drm/msm/dsi-staging/dsi_ctrl_hw.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_ctrl_hw.h
@@ -36,6 +36,7 @@
  * @DSI_CTRL_VERSION_2_0:     DSI host v2.0 controller
  * @DSI_CTRL_VERSION_2_2:     DSI host v2.2 controller
  * @DSI_CTRL_VERSION_2_3:     DSI host v2.3 controller
+ * @DSI_CTRL_VERSION_2_4:     DSI host v2.4 controller
  * @DSI_CTRL_VERSION_MAX:     max version
  */
 enum dsi_ctrl_version {
@@ -44,6 +45,7 @@ enum dsi_ctrl_version {
 	DSI_CTRL_VERSION_2_0,
 	DSI_CTRL_VERSION_2_2,
 	DSI_CTRL_VERSION_2_3,
+	DSI_CTRL_VERSION_2_4,
 	DSI_CTRL_VERSION_MAX
 };
 
