1c680479a2d1123cd764ef35078a62475a1a020d
Anmolpreet Kaur <anmolpre@codeaurora.org>
2019-08-07 10:12:41 +0530

    smcinvoke: Handle NULL dereference for cb_req
    When kernel fails to allocate memory for cb_req,
    cb_req remains NULL. Going to out label is not required
    since cb_txn till this point is also NULL.
    
    Change-Id: I4f5d5bc9310a726878f306c8b65b131b4ca1e2f1
    Signed-off-by: Anmolpreet Kaur <anmolpre@codeaurora.org>

--- drivers/soc/qcom/smcinvoke.c
+++ drivers/soc/qcom/smcinvoke.c
@@ -850,8 +850,10 @@ static void process_tzcb_req(void *buf, size_t buf_len, struct file **arr_filp)
 
 	cb_req = kmemdup(buf, buf_len, GFP_KERNEL);
 	if (!cb_req) {
-		ret =  OBJECT_ERROR_KMEM;
-		goto out;
+		/* we need to return error to caller so fill up result */
+		cb_req = buf;
+		cb_req->result = OBJECT_ERROR_KMEM;
+		return;
 	}
 
 	/* check whether it is to be served by kernel or userspace */
@@ -909,9 +911,11 @@ out:
 			release_tzhandle_locked(cb_req->hdr.tzhandle);
 		}
 	}
-	hash_del(&cb_txn->hash);
-	memcpy(buf, cb_req, buf_len);
-	kref_put(&cb_txn->ref_cnt, delete_cb_txn);
+	if (cb_txn) {
+		hash_del(&cb_txn->hash);
+		memcpy(buf, cb_req, buf_len);
+		kref_put(&cb_txn->ref_cnt, delete_cb_txn);
+	}
 	mutex_unlock(&g_smcinvoke_lock);
 }
 
