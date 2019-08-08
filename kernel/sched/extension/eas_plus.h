ce3636c9618a5c553923a74808fe833359eca819
YT Chang <yt.chang@mediatek.com>
2019-08-08 16:33:18 +0800

    sched: turning point should be included in CONFIG_MTK_SCHED_EXTENSION
    turning point should be included in CONFIG_MTK_SCHED_EXTENSION
    
    Change-Id: Ia5cea73364f7d17d788c24bbf4c9f24dddec459f
    Signed-off-by: YT Chang <yt.chang@mediatek.com>
    CR-Id: ALPS04222778
    Feature: Energy Aware Scheduling Plus (EAS+)

--- kernel/sched/extension/eas_plus.h
+++ kernel/sched/extension/eas_plus.h
@@ -131,6 +131,12 @@ static inline bool is_max_capacity_cpu(int cpu)
 }
 #endif
 
+#ifdef CONFIG_MTK_SCHED_TURNING_POINT
+extern unsigned long capacity_curr_of(int cpu);
+extern int check_freq_turning_with_limit(void);
+extern int cpu_eff_tp;
+#endif
+
 /**
  *for isolation
  */
@@ -169,10 +175,4 @@ void nohz_balance_clear_nohz_mask(int cpu);
 int set_cpu_isolation(enum iso_prio_t prio, struct cpumask *cpumask_ptr);
 #endif /* CONFIG_MTK_SCHED_EXTENSION */
 
-
-#ifdef CONFIG_MTK_SCHED_TURNING_POINT
-extern unsigned long capacity_curr_of(int cpu);
-extern int check_freq_turning_with_limit(void);
-extern int cpu_eff_tp;
-#endif
 #endif
