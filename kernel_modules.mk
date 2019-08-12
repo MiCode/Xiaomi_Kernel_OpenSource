45f94de1d761fe1b4db44e158de08f59a0013228
Frankie Chang <Frankie.Chang@mediatek.com>
2019-08-12 14:44:20 +0800

    Android: move BOARD_*_KERNEL_MODULES out of kernel_modules.mk
    move BOARD_*_KERNEL_MODULES out kernel_modules.mk
    to avoid .ko not exists leading to build fail
    
    Change-Id: I5cd29090c70b05f35e49ed71fa339c0ec8109af0
    CR-Id: ALPS04671194
    Feature: Others
    Signed-off-by: Frankie Chang <Frankie.Chang@mediatek.com>

--- kernel_modules.mk
+++ kernel_modules.mk
@@ -1,10 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2019 MediaTek Inc.
 
-# in-tree kernel modules installed to vendor
-BOARD_VENDOR_KERNEL_MODULES +=
 $(BOARD_VENDOR_KERNEL_MODULES): $(KERNEL_ZIMAGE_OUT);
 
-# in-tree kernel modules installed to ramdisk
-BOARD_RAMDISK_KERNEL_MODULES +=
 $(BOARD_RAMDISK_KERNEL_MODULES): $(KERNEL_ZIMAGE_OUT);
