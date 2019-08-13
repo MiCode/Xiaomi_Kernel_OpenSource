a4d74466a2a96adff1c55eb10cb958106128a38b
Diptanshu Jamgade <djamgade@codeaurora.org>
2019-08-13 10:38:15 +0530

    include: dt-bindings: clock: Add clock IDs supported on Lagoon
    Add the GCC/DISPCC/GPUCC/CAMCC/NPUCC/VIDEOCC clock ids which
    would be used by consumers to request on various clocks from
    these subsystems.
    
    Change-Id: Iddb27c9b01b422bbc6798979308a3264de97cee6
    Signed-off-by: Diptanshu Jamgade <djamgade@codeaurora.org>

---
+++ include/dt-bindings/clock/qcom,videocc-lagoon.h
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2019, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef _DT_BINDINGS_CLK_QCOM_VIDEO_CC_LAGOON_H
+#define _DT_BINDINGS_CLK_QCOM_VIDEO_CC_LAGOON_H
+
+/* VIDEO_CC clocks */
+#define VIDEO_PLL0			0
+#define VIDEO_PLL0_OUT_EVEN             1
+#define VIDEO_CC_IRIS_AHB_CLK		2
+#define VIDEO_CC_IRIS_CLK_SRC		3
+#define VIDEO_CC_MVS0_AXI_CLK		4
+#define VIDEO_CC_MVS0_CORE_CLK		5
+#define VIDEO_CC_MVSC_CORE_CLK		6
+#define VIDEO_CC_MVSC_CTL_AXI_CLK	7
+#define VIDEO_CC_SLEEP_CLK		8
+#define VIDEO_CC_SLEEP_CLK_SRC		9
+#define VIDEO_CC_VENUS_AHB_CLK		10
+#define VIDEO_CC_XO_CLK			11
+#define VIDEO_CC_XO_CLK_SRC		12
+
+#endif
