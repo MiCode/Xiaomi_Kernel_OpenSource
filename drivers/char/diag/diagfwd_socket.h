959e069873604b2e2da2d894cc8e199a59ed743d
Hardik Arya <harya@codeaurora.org>
2019-08-14 11:33:54 +0530

    diag: Update remote address for each CNTL packet
    Possibility of stale remote address for CNTL channel is
    prevented by updating address for every CNTL packet received.
    Since APPS only writes on CNTL channel, updation is only needed
    for CNTL channel.
    Reduce the scope of notifier mutex to reduce the chances of
    race condition. Update spelling for few macros.
    
    Change-Id: I0a53ebe0a66660b763d5df1f7084b0f7fa8ebcb0
    Signed-off-by: Manoj Prabhu B <bmanoj@codeaurora.org>
    Signed-off-by: Hardik Arya <harya@codeaurora.org>

--- drivers/char/diag/diagfwd_socket.h
+++ drivers/char/diag/diagfwd_socket.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015-2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2015-2017, 2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -24,9 +24,9 @@
 #define PORT_TYPE_SERVER		0
 #define PORT_TYPE_CLIENT		1
 
-#define PEPIPHERAL_AFTER_BOOT		0
-#define PEPIPHERAL_SSR_DOWN		1
-#define PEPIPHERAL_SSR_UP		2
+#define PERIPHERAL_AFTER_BOOT		0
+#define PERIPHERAL_SSR_DOWN		1
+#define PERIPHERAL_SSR_UP		2
 
 #define CNTL_CMD_NEW_SERVER		4
 #define CNTL_CMD_REMOVE_SERVER		5
