b0db1d214d13e30e3da9c556027bb3e57c696046
Auto Li <auto.li@mediatek.com>
2019-08-16 22:41:24 +0800

    [ALPS04724049] EEPROM: Improvement perf of reading
    Improvement performance of reading.
    Read at most 32 bytes at a time instead of 8 bytes
    on mt6739, mt6765, mt6768, mt6771, mt6785, mt6885
    
    MTK-Commit-Id: a81d99f750738da8a15ec0802bba7fbbce3d7d41
    
    Change-Id: If1e578f8ea2a8ee62eef52c27a5f79cee327b38a
    Signed-off-by: Auto Li <auto.li@mediatek.com>
    CR-Id: ALPS04724049
    Feature: [Android Default] Camera Application Basic Functions

---
+++ drivers/misc/mediatek/cam_cal/src/mt6765/eeprom_platform_def.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2019 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+#ifndef __EEPROM_PLATFORM_DEF_H
+#define __EEPROM_PLATFORM_DEF_H
+
+#define EEPROM_I2C_READ_MSG_LENGTH_MAX 32
+
+#endif
