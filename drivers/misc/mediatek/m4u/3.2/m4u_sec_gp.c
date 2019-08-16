f2e5fa7a9e706bd1bccb67c5a9fa354e8e213ee1
Chao Hao <chao.hao@mediatek.com>
2019-08-16 22:38:33 +0800

    [ALPS04737067] M4U: fix m4u t_drv re_init defect
    [Detail]
    1.It should clean related flag when m4u t_drv init fail.
    2.fix return value
    
    MTK-Commit-Id: 7f87ce51fbc1a36e7e30038b290de55f67253ba0
    
    Change-Id: I003ba8074ece256ccd1de3308d6c13aa31c67124
    Signed-off-by: Chao Hao <chao.hao@mediatek.com>
    CR-Id: ALPS04737067
    Feature: [Module]ION/M4U

--- drivers/misc/mediatek/m4u/3.2/m4u_sec_gp.c
+++ drivers/misc/mediatek/m4u/3.2/m4u_sec_gp.c
@@ -22,8 +22,6 @@
 #include "tz_m4u.h"
 #include "m4u_sec_gp.h"
 
-unsigned int M4U_SEC_SESSION;
-
 static struct m4u_sec_gp_context m4u_gp_ta_ctx = {
 #if defined(CONFIG_MICROTRUST_TEE_SUPPORT) || \
 	defined(CONFIG_TRUSTONIC_TEE_SUPPORT)
@@ -113,7 +111,7 @@ static int m4u_sec_gp_init(struct m4u_sec_context *ctx)
 		M4UMSG("m4u msg is invalid\n");
 		return -1;
 	}
-	if (!M4U_SEC_SESSION) {
+	if (!gp_ctx->init) {
 		ret = TEEC_OpenSession(&gp_ctx->ctx,
 			&gp_ctx->session, &gp_ctx->uuid,
 				       TEEC_LOGIN_PUBLIC, NULL, NULL, NULL);
@@ -122,10 +120,8 @@ static int m4u_sec_gp_init(struct m4u_sec_context *ctx)
 			goto exit_release;
 
 		}
-		M4U_SEC_SESSION = 1;
+		gp_ctx->init = 1;
 	}
-
-	gp_ctx->init = 1;
 
 	M4ULOG_HIGH("%s, open TCI session success\n", __func__);
 	return ret;
