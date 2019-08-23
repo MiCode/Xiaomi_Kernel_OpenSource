add89d470c6633b9f16c7faec1307c53939d4329
Neeraj Soni <neersoni@codeaurora.org>
2019-08-23 14:10:09 +0530

    fs/crypto: do not clear encryption key before inode is updated
    Although an all zero key is not an invalid key but it is indeed a
    weak key. Clearing crypt key memory before updating the file inode with
    crypto information will lead to a weak file encryption key to be
    used in encryption operation.
    
    Change-Id: Ide6340ea58f7ff916d3161716f0e9b4adf9b9c75
    Signed-off-by: Neeraj Soni <neersoni@codeaurora.org>

--- fs/crypto/keyinfo.c
+++ fs/crypto/keyinfo.c
@@ -606,8 +606,6 @@ int fscrypt_get_encryption_info(struct inode *inode)
 	if (res)
 		goto out;
 
-	memzero_explicit(crypt_info->ci_raw_key,
-		sizeof(crypt_info->ci_raw_key));
 do_ice:
 	if (cmpxchg(&inode->i_crypt_info, NULL, crypt_info) == NULL)
 		crypt_info = NULL;
