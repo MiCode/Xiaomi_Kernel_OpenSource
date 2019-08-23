c5be65ad86a1eb56d9547ddedf4150896bf9edff
Suren Baghdasaryan <surenb@google.com>
2019-08-23 11:11:00 -0700

    ANDROID: arch: add missing pidfd_open definitions for arm32
    While backporting commit 7615d9e1780e26e0178c93c55b73309a5dc093d7 to 4.9
    arm32 definitions for pidfd_open syscall were not added correctly. Fix
    by adding missing definitions.
    
    Fixes aosp/986706 "BACKPORT: arch: wire-up pidfd_open()"
    
    Bug: 135608568
    Test: make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
    Change-Id: I68b8454971f296453d598de281ebe9225067f0b8
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- arch/arm/include/uapi/asm/unistd.h
+++ arch/arm/include/uapi/asm/unistd.h
@@ -423,6 +423,7 @@
 #define __NR_pkey_mprotect		(__NR_SYSCALL_BASE+394)
 #define __NR_pkey_alloc			(__NR_SYSCALL_BASE+395)
 #define __NR_pkey_free			(__NR_SYSCALL_BASE+396)
+#define __NR_pidfd_open		(__NR_SYSCALL_BASE+434)
 
 /*
  * The following SWIs are ARM private.
