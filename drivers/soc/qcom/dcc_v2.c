949350b2fe5bfd3afb1ffe9569477437c8545830
Mao Jinlong <jinlmao@codeaurora.org>
2019-08-27 21:32:24 +0800

    dcc_v2: Don't set the default value to sram when dcc is enabled
    If set the default value to sram when dcc is enabled, there will be
    stuck issue for dcc dump.
    
    Change-Id: I784993d6db41e1afed92ccc05bec0cf1987a3e04
    Signed-off-by: Mao Jinlong <jinlmao@codeaurora.org>

--- drivers/soc/qcom/dcc_v2.c
+++ drivers/soc/qcom/dcc_v2.c
@@ -564,18 +564,35 @@ static int dcc_valid_list(struct dcc_drvdata *drvdata, int curr_list)
 		return -EINVAL;
 
 	if (drvdata->enable[curr_list]) {
-		dev_err(drvdata->dev, "DCC is already enabled\n");
+		dev_err(drvdata->dev, "List %d is already enabled\n",
+				curr_list);
 		return -EINVAL;
 	}
 
 	lock_reg = dcc_readl(drvdata, DCC_LL_LOCK(curr_list));
 	if (lock_reg & 0x1) {
-		dev_err(drvdata->dev, "DCC is already enabled\n");
+		dev_err(drvdata->dev, "List %d is already locked\n",
+				curr_list);
 		return -EINVAL;
 	}
 
 	dev_err(drvdata->dev, "DCC list passed %d\n", curr_list);
 	return 0;
+}
+
+static bool is_dcc_enabled(struct dcc_drvdata *drvdata)
+{
+	bool dcc_enable = false;
+	int list;
+
+	for (list = 0; list < DCC_MAX_LINK_LIST; list++) {
+		if (drvdata->enable[list]) {
+			dcc_enable = true;
+			break;
+		}
+	}
+
+	return dcc_enable;
 }
 
 static int dcc_enable(struct dcc_drvdata *drvdata)
@@ -586,7 +603,9 @@ static int dcc_enable(struct dcc_drvdata *drvdata)
 
 	mutex_lock(&drvdata->mutex);
 
-	memset_io(drvdata->ram_base, 0xDE, drvdata->ram_size);
+	if (!is_dcc_enabled(drvdata)) {
+		memset_io(drvdata->ram_base, 0xDE, drvdata->ram_size);
+	}
 
 	for (list = 0; list < DCC_MAX_LINK_LIST; list++) {
 
@@ -665,21 +684,6 @@ static void dcc_disable(struct dcc_drvdata *drvdata)
 	drvdata->ram_cfg = 0;
 	drvdata->ram_start = 0;
 	mutex_unlock(&drvdata->mutex);
-}
-
-static bool is_dcc_enabled(struct dcc_drvdata *drvdata)
-{
-	bool dcc_enable = false;
-	int list;
-
-	for (list = 0; list < DCC_MAX_LINK_LIST; list++) {
-		if (drvdata->enable[list]) {
-			dcc_enable = true;
-			break;
-		}
-	}
-
-	return dcc_enable;
 }
 
 static ssize_t curr_list_show(struct device *dev,
