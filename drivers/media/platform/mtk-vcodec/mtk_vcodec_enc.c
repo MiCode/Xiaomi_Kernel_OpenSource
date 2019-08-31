ebdb64572ed620459177939849c1fc1e4b23156f
Chia-Mao Hung <chia-mao.hung@mediatek.com>
2019-08-31 09:32:43 +0800

    [ALPS04760137] v4l2: add error handling
    abort state do not send encode final
    
    MTK-Commit-Id: cf3f231c9ae1428ada9656bc9e01a3f24d360cf1
    
    Change-Id: I55440441376ea52fb066330ffd64b08da32ba6a7
    Signed-off-by: Chia-Mao Hung <chia-mao.hung@mediatek.com>
    CR-Id: ALPS04760137
    Feature: H.264 Encoder

--- drivers/media/platform/mtk-vcodec/mtk_vcodec_enc.c
+++ drivers/media/platform/mtk-vcodec/mtk_vcodec_enc.c
@@ -1613,14 +1613,15 @@ static void vb2ops_venc_stop_streaming(struct vb2_queue *q)
 
 	mtk_v4l2_debug(2, "[%d]-> type=%d", ctx->id, q->type);
 
-	ret = venc_if_encode(ctx,
-		VENC_START_OPT_ENCODE_FRAME_FINAL,
-		NULL, NULL, &enc_result);
-	return_free_buffers(ctx);
+	if (ctx->state != MTK_STATE_ABORT) {
+		ret = venc_if_encode(ctx,
+			VENC_START_OPT_ENCODE_FRAME_FINAL,
+			NULL, NULL, &enc_result);
+		return_free_buffers(ctx);
 
-	if (ret)
-		mtk_v4l2_err("venc_if_deinit failed=%d", ret);
-
+		if (ret)
+			mtk_v4l2_err("venc_if_deinit failed=%d", ret);
+	}
 	if (q->type == V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE) {
 		while ((dst_buf = v4l2_m2m_dst_buf_remove(ctx->m2m_ctx))) {
 			dst_buf->planes[0].bytesused = 0;
