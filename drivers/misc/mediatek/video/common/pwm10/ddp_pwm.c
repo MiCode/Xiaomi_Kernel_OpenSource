d9b7f79f5bf837f5e9693e2ede4a122a19e4eeb4
mtk19784 <ycw.wu@mediatek.com>
2019-08-31 09:32:58 +0800

    [ALPS04763971] PWM: Add chip define for BL control
    Add mt6785 define for pwm backlight control
    
    MTK-Commit-Id: a714fcef2427994208823045efaf1296fc65d551
    
    Change-Id: I73cb30c7dbd6896833c7b4b541c0ce5f0792411e
    CR-Id:ALPS04763971
    Feature: Ambient Light Adaptive Luma
    Signed-off-by: mtk19784 <ycw.wu@mediatek.com>

--- drivers/misc/mediatek/video/common/pwm10/ddp_pwm.c
+++ drivers/misc/mediatek/video/common/pwm10/ddp_pwm.c
@@ -30,7 +30,7 @@
 	defined(CONFIG_MACH_MT6765) || defined(CONFIG_MACH_MT6761) || \
 	defined(CONFIG_MACH_MT6771) || defined(CONFIG_MACH_MT3967) || \
 	defined(CONFIG_MACH_MT6768) || defined(CONFIG_MACH_MT8168) || \
-	defined(CONFIG_MACH_MT6885)
+	defined(CONFIG_MACH_MT6885) || defined(CONFIG_MACH_MT6785)
 
 #include <ddp_clkmgr.h>
 #endif
@@ -41,7 +41,7 @@
 #if defined(CONFIG_MACH_MT6758) || defined(CONFIG_MACH_MT6765) || \
 	defined(CONFIG_MACH_MT6761) || defined(CONFIG_MACH_MT6771) || \
 	defined(CONFIG_MACH_MT3967) || defined(CONFIG_MACH_MT6768) || \
-	defined(CONFIG_MACH_MT6885)
+	defined(CONFIG_MACH_MT6885) || defined(CONFIG_MACH_MT6785)
 
 #include <mtk_leds_drv.h>
 #include <mtk_leds_sw.h>
@@ -646,14 +646,15 @@ static int ddp_pwm_power_on(enum DISP_MODULE_ENUM module, void *handle)
 #if defined(CONFIG_MACH_MT6759) || defined(CONFIG_MACH_MT6758) || \
 	defined(CONFIG_MACH_MT6739) || defined(CONFIG_MACH_MT6765) || \
 	defined(CONFIG_MACH_MT6761) || defined(CONFIG_MACH_MT6768) || \
-	defined(CONFIG_MACH_MT8168) || defined(CONFIG_MACH_MT6885)
+	defined(CONFIG_MACH_MT8168)
 
 	/* pwm ccf api */
 	ddp_clk_prepare_enable(ddp_get_module_clk_id(module));
 #elif defined(CONFIG_MACH_MT6763) || defined(CONFIG_MACH_MT6771)
 	ddp_clk_prepare_enable(ddp_get_module_clk_id(module));
 	ddp_clk_prepare_enable(TOP_MUX_DISP_PWM);
-#elif defined(CONFIG_MACH_MT3967)
+#elif defined(CONFIG_MACH_MT3967) || defined(CONFIG_MACH_MT6785) || \
+	defined(CONFIG_MACH_MT6885)
 	ddp_clk_prepare_enable(ddp_get_module_clk_id(module));
 	ddp_clk_prepare_enable(CLK_MUX_DISP_PWM);
 #else
@@ -706,14 +707,15 @@ static int ddp_pwm_power_off(enum DISP_MODULE_ENUM module, void *handle)
 #if defined(CONFIG_MACH_MT6759) || defined(CONFIG_MACH_MT6758) || \
 	defined(CONFIG_MACH_MT6739) || defined(CONFIG_MACH_MT6765) || \
 	defined(CONFIG_MACH_MT6761) || defined(CONFIG_MACH_MT6768) || \
-	defined(CONFIG_MACH_MT8168) || defined(CONFIG_MACH_MT6885)
+	defined(CONFIG_MACH_MT8168)
 
 	/* pwm ccf api */
 	ddp_clk_disable_unprepare(ddp_get_module_clk_id(module));
 #elif defined(CONFIG_MACH_MT6763) || defined(CONFIG_MACH_MT6771)
 	ddp_clk_disable_unprepare(ddp_get_module_clk_id(module));
 	ddp_clk_disable_unprepare(TOP_MUX_DISP_PWM);
-#elif defined(CONFIG_MACH_MT3967)
+#elif defined(CONFIG_MACH_MT3967) || defined(CONFIG_MACH_MT6785) || \
+	defined(CONFIG_MACH_MT6885)
 	ddp_clk_disable_unprepare(ddp_get_module_clk_id(module));
 	ddp_clk_disable_unprepare(CLK_MUX_DISP_PWM);
 #else
@@ -789,7 +791,8 @@ bool disp_pwm_is_osc(void)
 	defined(CONFIG_MACH_MT6758) || defined(CONFIG_MACH_MT6765) || \
 	defined(CONFIG_MACH_MT6761) || defined(CONFIG_MACH_MT6771) || \
 	defined(CONFIG_MACH_MT3967) || defined(CONFIG_MACH_MT6768) || \
-	defined(CONFIG_MACH_MT8168) || defined(CONFIG_MACH_MT6885)
+	defined(CONFIG_MACH_MT8168) || defined(CONFIG_MACH_MT6885) || \
+	defined(CONFIG_MACH_MT6785)
 
 	is_osc = disp_pwm_mux_is_osc();
 #endif
