d17ab01d8cf9a61324badcae5ebbae3e24a0fca8
Dongchun Zhu <dongchun.zhu@mediatek.com>
2019-08-31 09:34:36 +0800

    [ALPS04754666] imgsensor: add support for gc2365
    1. fix gc2365 build error
    2. update imgsensor adapter layer hw_init to match device tree
    
    MTK-Commit-Id: d58b0c4901d9a692b6b508e86210a66540632a10
    
    Change-Id: Ia3aa25dd9c6941d2b9b4f8cd791d10d13e5730f2
    CR-Id: ALPS04754666
    Feature: Camera Application
    Signed-off-by: Dongchun Zhu <dongchun.zhu@mediatek.com>

--- drivers/misc/mediatek/imgsensor/src/common/v1/gc2365_mipi_raw/gc2365mipi_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1/gc2365_mipi_raw/gc2365mipi_Sensor.c
@@ -191,7 +191,7 @@ static struct imgsensor_struct imgsensor = {
 
 
 /* Sensor output window information */
-static SENSOR_WINSIZE_INFO_STRUCT imgsensor_winsize_info[5] = {
+static struct SENSOR_WINSIZE_INFO_STRUCT imgsensor_winsize_info[5] = {
 	{1600, 1200, 0, 0, 1600, 1200, 1600, 1200, 0000,
 		0000, 1600, 1200, 0, 0, 1600, 1200},	/* Preview */
 	{1600, 1200, 0, 0, 1600, 1200, 1600, 1200,
@@ -1120,7 +1120,7 @@ static kal_uint32 get_resolution(
 	return ERROR_NONE;
 }				/*    get_resolution    */
 
-static kal_uint32 get_info(MSDK_SCENARIO_ID_ENUM scenario_id,
+static kal_uint32 get_info(enum MSDK_SCENARIO_ID_ENUM scenario_id,
 			   MSDK_SENSOR_INFO_STRUCT *sensor_info,
 			   MSDK_SENSOR_CONFIG_STRUCT *sensor_config_data)
 {
@@ -1235,7 +1235,7 @@ static kal_uint32 get_info(MSDK_SCENARIO_ID_ENUM scenario_id,
 }				/*    get_info  */
 
 
-static kal_uint32 control(MSDK_SCENARIO_ID_ENUM scenario_id,
+static kal_uint32 control(enum MSDK_SCENARIO_ID_ENUM scenario_id,
 			  MSDK_SENSOR_EXPOSURE_WINDOW_STRUCT *image_window,
 			  MSDK_SENSOR_CONFIG_STRUCT *sensor_config_data)
 {
@@ -1304,7 +1304,7 @@ static kal_uint32 set_auto_flicker_mode(kal_bool enable, UINT16 framerate)
 
 
 static kal_uint32 set_max_framerate_by_scenario(
-	MSDK_SCENARIO_ID_ENUM scenario_id,
+	enum MSDK_SCENARIO_ID_ENUM scenario_id,
 	MUINT32 framerate)
 {
 	kal_uint32 frame_length;
@@ -1434,7 +1434,7 @@ static kal_uint32 set_max_framerate_by_scenario(
 
 
 static kal_uint32 get_default_framerate_by_scenario(
-	MSDK_SCENARIO_ID_ENUM scenario_id,
+	enum MSDK_SCENARIO_ID_ENUM scenario_id,
 	MUINT32 *framerate)
 {
 	LOG_INF("scenario_id = %d\n", scenario_id);
@@ -1476,7 +1476,7 @@ static kal_uint32 feature_control(MSDK_SENSOR_FEATURE_ENUM feature_id,
  * feature_para;
  */
 
-	SENSOR_WINSIZE_INFO_STRUCT *wininfo;
+	struct SENSOR_WINSIZE_INFO_STRUCT *wininfo;
 	MSDK_SENSOR_REG_INFO_STRUCT *sensor_reg_data = \
 		(MSDK_SENSOR_REG_INFO_STRUCT *) feature_para;
 
@@ -1532,13 +1532,13 @@ static kal_uint32 feature_control(MSDK_SENSOR_FEATURE_ENUM feature_id,
 		break;
 	case SENSOR_FEATURE_SET_MAX_FRAME_RATE_BY_SCENARIO:
 		set_max_framerate_by_scenario(
-			(MSDK_SCENARIO_ID_ENUM) *feature_data,
+			(enum MSDK_SCENARIO_ID_ENUM) *feature_data,
 					      *(feature_data + 1));
 		break;
 	case SENSOR_FEATURE_GET_DEFAULT_FRAME_RATE_BY_SCENARIO:
 		get_default_framerate_by_scenario(
-			(MSDK_SCENARIO_ID_ENUM) *(feature_data),
-			MUINT32 *) (uintptr_t) (*(feature_data + 1)));
+			(enum MSDK_SCENARIO_ID_ENUM) *(feature_data),
+			(MUINT32 *) (uintptr_t) (*(feature_data + 1)));
 		break;
 	case SENSOR_FEATURE_SET_TEST_PATTERN:
 		set_test_pattern_mode((BOOL) * feature_data);
@@ -1563,35 +1563,35 @@ static kal_uint32 feature_control(MSDK_SENSOR_FEATURE_ENUM feature_id,
 		LOG_INF("SENSOR_FEATURE_GET_CROP_INFO scenarioId:%d\n",
 			(UINT32) *feature_data);
 
-		wininfo = (SENSOR_WINSIZE_INFO_STRUCT *) \
+		wininfo = (struct SENSOR_WINSIZE_INFO_STRUCT *)
 			(uintptr_t)(*(feature_data + 1));
 
 		switch (*feature_data_32) {
 		case MSDK_SCENARIO_ID_CAMERA_CAPTURE_JPEG:
 			memcpy((void *)wininfo,
 				(void *)&imgsensor_winsize_info[1],
-			       sizeof(SENSOR_WINSIZE_INFO_STRUCT));
+			       sizeof(struct SENSOR_WINSIZE_INFO_STRUCT));
 			break;
 		case MSDK_SCENARIO_ID_VIDEO_PREVIEW:
 			memcpy((void *)wininfo,
 				(void *)&imgsensor_winsize_info[2],
-			       sizeof(SENSOR_WINSIZE_INFO_STRUCT));
+			       sizeof(struct SENSOR_WINSIZE_INFO_STRUCT));
 			break;
 		case MSDK_SCENARIO_ID_HIGH_SPEED_VIDEO:
 			memcpy((void *)wininfo,
 				(void *)&imgsensor_winsize_info[3],
-			       sizeof(SENSOR_WINSIZE_INFO_STRUCT));
+			       sizeof(struct SENSOR_WINSIZE_INFO_STRUCT));
 			break;
 		case MSDK_SCENARIO_ID_SLIM_VIDEO:
 			memcpy((void *)wininfo,
 				(void *)&imgsensor_winsize_info[4],
-			       sizeof(SENSOR_WINSIZE_INFO_STRUCT));
+			       sizeof(struct SENSOR_WINSIZE_INFO_STRUCT));
 			break;
 		case MSDK_SCENARIO_ID_CAMERA_PREVIEW:
 		default:
 			memcpy((void *)wininfo,
 				(void *)&imgsensor_winsize_info[0],
-			       sizeof(SENSOR_WINSIZE_INFO_STRUCT));
+			       sizeof(struct SENSOR_WINSIZE_INFO_STRUCT));
 			break;
 		}
 		break;
@@ -1611,7 +1611,7 @@ static kal_uint32 feature_control(MSDK_SENSOR_FEATURE_ENUM feature_id,
 	return ERROR_NONE;
 }				/*    feature_control()  */
 
-static SENSOR_FUNCTION_STRUCT sensor_func = {
+static struct SENSOR_FUNCTION_STRUCT sensor_func = {
 	open,
 	get_info,
 	get_resolution,
@@ -1620,7 +1620,7 @@ static SENSOR_FUNCTION_STRUCT sensor_func = {
 	close
 };
 
-UINT32 GC2365_MIPI_RAW_SensorInit(PSENSOR_FUNCTION_STRUCT *pfFunc)
+UINT32 GC2365_MIPI_RAW_SensorInit(struct SENSOR_FUNCTION_STRUCT **pfFunc)
 {
 	/* To Do : Check Sensor status here */
 	if (pfFunc != NULL)
