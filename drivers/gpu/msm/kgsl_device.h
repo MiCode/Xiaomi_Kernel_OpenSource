67d23bf802b9e3bccd49c8285f7f3a6062f77c59
Harshdeep Dhatt <hdhatt@codeaurora.org>
2019-09-09 11:05:48 -0600

    msm: kgsl: Disable deprecated ioctls
    Disabling sparse ioctls as they are deprecated.
    
    Change-Id: I5a4c78ec96fad4cda29a61d090e8bfb798123e3f
    Signed-off-by: Harshdeep Dhatt <hdhatt@codeaurora.org>
    Signed-off-by: Archana Sriram <apsrir@codeaurora.org>

--- drivers/gpu/msm/kgsl_device.h
+++ drivers/gpu/msm/kgsl_device.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2002,2007-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2002,2007-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -66,6 +66,7 @@ enum kgsl_event_results {
 };
 
 #define KGSL_FLAG_WAKE_ON_TOUCH BIT(0)
+#define KGSL_FLAG_SPARSE        BIT(1)
 
 /*
  * "list" of event types for ftrace symbolic magic
