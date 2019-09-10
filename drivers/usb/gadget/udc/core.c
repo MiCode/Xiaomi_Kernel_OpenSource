e92b17c343e440b15d8e0eae6d734bcdfa8dede2
Chandana Kishori Chiluveru <cchiluve@codeaurora.org>
2019-09-10 16:31:24 +0530

    usb: gadget: core: unmap request from DMA only if previously mapped
    In the SG case this is already handled since a non-zero
    request->num_mapped_sgs is a clear indicator that dma_map_sg()
    had been called. While it would be nice to do the same for the
    singly mapped case by simply checking for non-zero request->dma,
    it's conceivable that 0 is a valid dma_addr_t handle. Hence add
    a flag 'dma_mapped' to struct usb_request and use this to
    determine the need to call dma_unmap_single(). Otherwise, if a
    request is not DMA mapped then the result of calling
    usb_request_unmap_request() would safely be a no-op.
    
    Change-Id: Ibb00d6df31f21522da9ebbcda3ae288cb7b189fa
    Signed-off-by: Jack Pham <jackp@codeaurora.org>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>
    Git-commit: 31fe084ffaaf8abece14f8ca28e5e3b4e2bf97b6
    Git-repo: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    [cchiluve@codeaurora.org: resolve trivial merge conflicts]
    Signed-off-by: Chandana Kishori Chiluveru <cchiluve@codeaurora.org>

--- drivers/usb/gadget/udc/core.c
+++ drivers/usb/gadget/udc/core.c
@@ -857,6 +857,8 @@ int usb_gadget_map_request_by_dev(struct device *dev,
 			dev_err(dev, "failed to map buffer\n");
 			return -EFAULT;
 		}
+
+		req->dma_mapped = 1;
 	}
 
 	return 0;
@@ -881,9 +883,10 @@ void usb_gadget_unmap_request_by_dev(struct device *dev,
 				is_in ? DMA_TO_DEVICE : DMA_FROM_DEVICE);
 
 		req->num_mapped_sgs = 0;
-	} else if (req->dma != DMA_ERROR_CODE) {
+	} else if (req->dma_mapped) {
 		dma_unmap_single(dev, req->dma, req->length,
 				is_in ? DMA_TO_DEVICE : DMA_FROM_DEVICE);
+		req->dma_mapped = 0;
 	}
 }
 EXPORT_SYMBOL_GPL(usb_gadget_unmap_request_by_dev);
