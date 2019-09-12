0f4e0600d4d268d63e217225bd69ca1c9d35d4d2
ian.hou <ian.hou@mediatek.com>
2019-09-12 09:44:17 +0800

    GPU: qos_gpu v2
    qos_gpu BW prediction v2
    From frame-based prediction become job-based prediction.
    
    DDK: Gathering job, ctx, frame information
    SSPM: Get DDK information and EMI bandwidth to generate
    bandwidth trend which can use to predict next job BW.
    
    Change-Id: I289826ee11f696dd0d4848bb9aaa4edec089553d
    CR-Id: ALPS04766104
    Feature: OpenGL|ES
    Signed-off-by: ian.hou <ian.hou@mediatek.com>

---
+++ drivers/gpu/mediatek/gpu_bm/gpu_bm.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#ifndef __GPU_BM_H__
+#define __GPU_BM_H__
+
+void MTKGPUQoS_setup(uint32_t *cpuaddr, phys_addr_t phyaddr, size_t size);
+
+#endif
+
