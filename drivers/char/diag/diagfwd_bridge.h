8e1aa686ea0978ecd93f6e5220ecc878700c5d2a
Amandeep Singh <amansing@codeaurora.org>
2019-09-13 13:19:38 +0530

    msm: diag: Update LF markings to be on same line
    Change-Id: Iba238a5dfcddbf486c6877563b41a3ef74d49526
    Signed-off-by: Amandeep Singh <amansing@codeaurora.org>

--- drivers/char/diag/diagfwd_bridge.h
+++ drivers/char/diag/diagfwd_bridge.h
@@ -1,5 +1,4 @@
-/* Copyright (c) 2012-2014, 2018-2019, The Linux Foundation.
- * All rights reserved.
+/* Copyright (c) 2012-2014, 2018-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
