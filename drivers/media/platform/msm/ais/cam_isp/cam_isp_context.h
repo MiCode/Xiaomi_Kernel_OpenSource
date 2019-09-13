c62ea9700ae30ddb9541b730e46c592cff16470f
Terence Ho <terenceh@codeaurora.org>
2019-09-13 16:14:07 -0400

    msm: ais: sync to latest mobile camera
    Synching to mobile camera kernel from msm-4.14.
    
    Up to commit 91071ebcb3ad
    ("msm: camera: isp: Fix possible uninitialized variable").
    
    Change-Id: I9b38551acc9c273dd967355c5b47a2489c15e4ad
    Signed-off-by: Terence Ho <terenceh@codeaurora.org>

--- drivers/media/platform/msm/ais/cam_isp/cam_isp_context.h
+++ drivers/media/platform/msm/ais/cam_isp/cam_isp_context.h
@@ -35,6 +35,11 @@
 #define CAM_ISP_CTX_CFG_MAX                     22
 
 /*
+ * Defalut fps value set to 30
+ */
+#define CAM_ISP_CTX_DEFAULT_FPS                 30
+
+/*
  * Maximum entries in state monitoring array for error logging
  */
 #define CAM_ISP_CTX_STATE_MONITOR_MAX_ENTRIES   20
@@ -180,6 +185,9 @@ struct cam_isp_context_req_id_info {
  * @hw_acquired:               Indicate whether HW resources are acquired
  * @init_received:             Indicate whether init config packet is received
  * @split_acquire:             Indicate whether a separate acquire is expected
+ * @irq_delay_detect:          Indicate whether a irq delay has detected or not
+ * @irq_timestamps:            Timestamp from last handled IRQ
+ * @fps:                       Current FPS for the activated state.
  *
  */
 struct cam_isp_context {
@@ -209,6 +217,9 @@ struct cam_isp_context {
 	bool                             hw_acquired;
 	bool                             init_received;
 	bool                             split_acquire;
+	bool                             irq_delay_detect;
+	uint64_t                         irq_timestamps;
+	uint32_t                         fps;
 };
 
 /**
