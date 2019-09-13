0aca873c157309b9aad5eb948465cef7f3b58b1e
Johnny Yao <johnny.yao@mediatek.com>
2019-09-13 08:09:17 +0800

    [ALPS04363596] wmt: add kernel adapter for dump clock buffer info
    add kernel adapter for dump clock buffer info
    
    MTK-Commit-Id: 43c38176ed99a36a012dfa1ae48bd8036fcfd562
    
    Change-Id: Ia0190836834ff7d021fb34ce25f683931e87d8f9
    Signed-off-by: Johnny Yao <johnny.yao@mediatek.com>
    CR-Id: ALPS04363596
    Feature: [Module]WMT Driver
    (cherry picked from commit 81fcf721544813f3675326440d1dfd04dfb83fb5)

--- drivers/misc/mediatek/connectivity/common/connectivity_build_in_adapter.c
+++ drivers/misc/mediatek/connectivity/common/connectivity_build_in_adapter.c
@@ -184,7 +184,10 @@ EXPORT_SYMBOL(connectivity_export_clk_buf_ctrl);
 
 void connectivity_export_clk_buf_show_status_info(void)
 {
-#if defined(CONFIG_MACH_MT6768) || defined(CONFIG_MACH_MT6785)
+#if defined(CONFIG_MACH_MT6768) || \
+	defined(CONFIG_MACH_MT6785) || \
+	defined(CONFIG_MACH_MT6771) || \
+	defined(CONFIG_MACH_MT6739)
 	clk_buf_show_status_info();
 #endif
 }
