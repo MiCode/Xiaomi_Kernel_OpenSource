77925651537432e4a64583314515956718da0a36
Neeraj Upadhyay <neeraju@codeaurora.org>
2019-09-16 09:59:48 +0530

    soc: qcom: pil: Add property to change minidump ELF format
    Add a new property, to dump minidump ELF in ELF32 format,
    as some subsystems do not support the default format,
    which is ELF64.
    
    Change-Id: I87bc46097b4dfc0be44f0904823c5e88c476c86b
    Signed-off-by: Neeraj Upadhyay <neeraju@codeaurora.org>
    Signed-off-by: Mayank Grover <groverm@codeaurora.org>

--- drivers/soc/qcom/peripheral-loader.h
+++ drivers/soc/qcom/peripheral-loader.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2010-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -69,6 +69,7 @@ struct pil_desc {
 	int minidump_id;
 	int *aux_minidump_ids;
 	int num_aux_minidump_ids;
+	bool minidump_as_elf32;
 };
 
 /**
