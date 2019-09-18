155d8b552f0daeba8dbdf879463d6f8749d90b77
Ashok Vuyyuru <avuyyuru@codeaurora.org>
2019-09-18 20:55:33 +0530

    msm: ipa3: Fix to not remove endpoint delay if stop channel fails
    Because of removing endpoint delay when the stop channel failed case
    observing some junk packets received on a pipe. To avoid junk packets,
    add change to avoid endpoint delay if stopping GSI channel fails.
    
    Change-Id: I4d3d8d9a0de823229d780fe31c8d7f1162e20668
    Signed-off-by: Ashok Vuyyuru <avuyyuru@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipa_client.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_client.c
@@ -1063,7 +1063,8 @@ static int ipa3_stop_ul_chan_with_data_drain(u32 qmi_req_id,
 	if (!stop_in_proc)
 		goto exit;
 
-	if (remove_delay && ep->ep_delay_set == true) {
+	/* Remove delay only if stop channel success*/
+	if (remove_delay && ep->ep_delay_set == true && !stop_in_proc) {
 		memset(&ep_cfg_ctrl, 0, sizeof(struct ipa_ep_cfg_ctrl));
 		ep_cfg_ctrl.ipa_ep_delay = false;
 		result = ipa3_cfg_ep_ctrl(clnt_hdl,
@@ -1144,7 +1145,7 @@ disable_force_clear_and_exit:
 	if (should_force_clear)
 		ipa3_disable_force_clear(qmi_req_id);
 exit:
-	if (remove_delay && ep->ep_delay_set == true) {
+	if (remove_delay && ep->ep_delay_set == true && !stop_in_proc) {
 		memset(&ep_cfg_ctrl, 0, sizeof(struct ipa_ep_cfg_ctrl));
 		ep_cfg_ctrl.ipa_ep_delay = false;
 		result = ipa3_cfg_ep_ctrl(clnt_hdl,
