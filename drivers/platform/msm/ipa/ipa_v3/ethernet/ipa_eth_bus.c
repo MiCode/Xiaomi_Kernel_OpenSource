5ebda72571be008aa2c3e45b8b8b05b56c10e822
Jinesh K. Jayakumar <jineshk@codeaurora.org>
2019-09-21 00:24:34 -0400

    msm: ipa: Concurrency support in offload sub-system
    Add support for concurrently executing API calls into the offload
    sub-system from network and offload drivers while handling events
    arising from different sub-systems (network framework/drivers, PM
    framework, IPA driver, PCIe framework, MSM PCIe bus driver, panic
    notifier and debugfs) in a safe manner.
    
    Change-Id: I48257effe7b745e0a148fd85dc0cbb0c7f0ff58d
    Signed-off-by: Jinesh K. Jayakumar <jineshk@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ethernet/ipa_eth_bus.c
+++ drivers/platform/msm/ipa/ipa_v3/ethernet/ipa_eth_bus.c
@@ -16,14 +16,12 @@
 
 static bool ipa_eth_bus_is_ready;
 
-static struct dentry *ipa_eth_bus_debugfs;
-
 struct ipa_eth_bus_map {
 	struct bus_type *bus;
 	struct ipa_eth_bus *eth_bus;
-	int (*modinit)(struct dentry *);
+	int (*modinit)(void);
 	void (*modexit)(void);
-} bus_map[] = {
+} static const bus_map[] = {
 	{
 		&pci_bus_type,
 		&ipa_eth_pci_bus,
@@ -33,31 +31,9 @@ struct ipa_eth_bus_map {
 	{},
 };
 
-static int ipa_eth_bus_debugfs_init(struct dentry *dbgfs_root)
-{
-	if (!dbgfs_root)
-		return 0;
-
-	ipa_eth_bus_debugfs = debugfs_create_dir("bus", dbgfs_root);
-	if (IS_ERR_OR_NULL(ipa_eth_bus_debugfs)) {
-		int rc = ipa_eth_bus_debugfs ?
-			PTR_ERR(ipa_eth_bus_debugfs) : -EFAULT;
-
-		ipa_eth_bus_debugfs = NULL;
-		return rc;
-	}
-
-	return 0;
-}
-
-static void ipa_eth_bus_debugfs_cleanup(void)
-{
-	debugfs_remove_recursive(ipa_eth_bus_debugfs);
-}
-
 static struct ipa_eth_bus *lookup_eth_bus(struct bus_type *bus)
 {
-	struct ipa_eth_bus_map *map;
+	const struct ipa_eth_bus_map *map;
 
 	for (map = bus_map; map->bus != NULL; map++) {
 		if (map->bus == bus)
@@ -71,18 +47,13 @@ int ipa_eth_bus_register_driver(struct ipa_eth_net_driver *nd)
 {
 	struct ipa_eth_bus *eth_bus;
 
-	if (!nd->bus) {
-		ipa_eth_err("Missing bus info in net driver");
-		return -EINVAL;
-	}
-
 	eth_bus = lookup_eth_bus(nd->bus);
 	if (!eth_bus) {
 		ipa_eth_err("Unsupported bus %s", nd->bus->name);
 		return -ENOTSUPP;
 	}
 
-	return eth_bus->register_net_driver(nd);
+	return eth_bus->register_driver(nd);
 }
 
 void ipa_eth_bus_unregister_driver(struct ipa_eth_net_driver *nd)
@@ -94,7 +65,7 @@ void ipa_eth_bus_unregister_driver(struct ipa_eth_net_driver *nd)
 		return;
 	}
 
-	eth_bus->unregister_net_driver(nd);
+	eth_bus->unregister_driver(nd);
 }
 
 int ipa_eth_bus_enable_pc(struct ipa_eth_device *eth_dev)
@@ -129,20 +100,14 @@ int ipa_eth_bus_disable_pc(struct ipa_eth_device *eth_dev)
 	return -EFAULT;
 }
 
-int ipa_eth_bus_modinit(struct dentry *dbgfs_root)
+int ipa_eth_bus_modinit(void)
 {
 	int rc;
-	struct ipa_eth_bus_map *map;
-
-	rc = ipa_eth_bus_debugfs_init(dbgfs_root);
-	if (rc) {
-		ipa_eth_err("Unable to create debugfs root for bus");
-		return rc;
-	}
+	const struct ipa_eth_bus_map *map;
 
 	/* initialize all registered busses */
 	for (rc = 0, map = bus_map; map->bus != NULL; map++)
-		rc |= map->modinit(ipa_eth_bus_debugfs);
+		rc |= map->modinit();
 
 	if (rc) {
 		ipa_eth_err("Failed to initialize one or more busses");
@@ -159,14 +124,12 @@ err_init:
 	for (map = bus_map; map->bus != NULL; map++)
 		map->modexit();
 
-	ipa_eth_bus_debugfs_cleanup();
-
 	return rc;
 }
 
 void ipa_eth_bus_modexit(void)
 {
-	struct ipa_eth_bus_map *map;
+	const struct ipa_eth_bus_map *map;
 
 	ipa_eth_log("De-initing offload sub-system bus module");
 
@@ -177,6 +140,4 @@ void ipa_eth_bus_modexit(void)
 
 	for (map = bus_map; map->bus != NULL; map++)
 		map->modexit();
-
-	ipa_eth_bus_debugfs_cleanup();
 }
