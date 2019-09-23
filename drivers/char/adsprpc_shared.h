033d277a09ae58318cfe015a1718bb712fa10d34
Mohammed Nayeem Ur Rahman <mohara@codeaurora.org>
2019-09-23 19:35:19 +0530

    adsprpc: Modify Remote scalars call backward compatible
    Add proper remote scalar calls to get TZ security key, which compatible
    with all dsp process groups TZ secure channel call.
    
    Change-Id: I89663a50e27cdfb73e2c919235c44ec2643fc159
    Acked-by: Tadakamalla Krishnaiah <ktadakam@qti.qualcomm.com>
    Signed-off-by: Mohammed Nayeem Ur Rahman <mohara@codeaurora.org>

--- drivers/char/adsprpc_shared.h
+++ drivers/char/adsprpc_shared.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2018, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -124,6 +124,9 @@ do {\
 	} \
 } while (0)
 #endif
+
+/* Fall back to older APIS in case API is not supported */
+#define AEE_EUNSUPPORTED    20
 
 #define remote_arg64_t    union remote_arg64
 
