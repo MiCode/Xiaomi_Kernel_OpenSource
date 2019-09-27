641ec072067b6eaa544acfc060709f2628798244
Gray Jia <gray.jia@mediatek.com>
2019-09-27 12:01:16 +0800

    [ALPS04771116] msdc:sdcard RA hardcode to 512KB
    [Details]
    After hotplug, sdcard device RA will return to 128KB, which
    has lower performance.
    
    [Solution]
    RA size is hard coded as 512KB.
    
    MTK-Commit-Id: ded51f0933bb0ffd374680cf61c9cb6926361b77
    
    Change-Id: I6d35dbf3c0c5bb10ca7382ce644ee74fcafa69b2
    Signed-off-by: Gray Jia <gray.jia@mediatek.com>
    CR-Id: ALPS04771116
    Feature: [Android Default] SDCard

--- drivers/mmc/core/queue.c
+++ drivers/mmc/core/queue.c
@@ -561,6 +561,7 @@ int mmc_init_queue(struct mmc_queue *mq, struct mmc_card *card,
 
 	mq->queue->cmd_size = sizeof(struct mmc_queue_req);
 	mq->queue->queuedata = mq;
+	mq->queue->backing_dev_info->ra_pages = 128;
 	atomic_set(&mq->qcnt, 0);
 	ret = blk_init_allocated_queue(mq->queue);
 	if (ret) {
