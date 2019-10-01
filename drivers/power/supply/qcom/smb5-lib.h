71de40a1c3d07a472e70f13838cf181d4161a251
Fenglin Wu <fenglinw@codeaurora.org>
2019-10-01 08:29:07 +0800

    power: supply: smb5-lib: use spinlock to protect pr_lock_in_progress
    API smblib_set_prop_typec_power_role() would be called from a hrtimer
    callback which is in an interrupt context, it's not correct to use
    cancel_delayed_work_sync() function inside. Change to use the
    non-sleep version cancel_delayed_work() and use a spinlock to protect
    the pr_lock_in_progress flag.
    
    Change-Id: I0a2e6e63179250579d8b826eaa2c7192399e17e6
    Signed-off-by: Fenglin Wu <fenglinw@codeaurora.org>

--- drivers/power/supply/qcom/smb5-lib.h
+++ drivers/power/supply/qcom/smb5-lib.h
@@ -385,6 +385,7 @@ struct smb_charger {
 	struct mutex		ps_change_lock;
 	struct mutex		irq_status_lock;
 	struct mutex		dcin_aicl_lock;
+	spinlock_t		typec_pr_lock;
 
 	/* power supplies */
 	struct power_supply		*batt_psy;
