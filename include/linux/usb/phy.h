a7365063447b66fc43a9c9ea25d8c5e72a38ebdf
Sriharsha Allenki <sallenki@codeaurora.org>
2019-10-03 12:44:21 +0530

    usb: dwc3: Drive a pulse on DP on CDP detection
    Some USB host PCs switch the downstream port mode from
    CDP to SDP if the device does not pull DP up within certain
    duration (~2s).
    This limits the current drawn by the device to 500mA(or 900mA)
    as opposed to the possible 1.5A resulting in slow charging of
    the battery.
    Fix this by driving a DP pulse when there is a connect
    notification from PMIC and detected the charger type as CDP.
    
    Change-Id: I76ac4c33dbbc538f5cb680fd1b5c15f8fb3bfe50
    Signed-off-by: Sriharsha Allenki <sallenki@codeaurora.org>

--- include/linux/usb/phy.h
+++ include/linux/usb/phy.h
@@ -176,6 +176,8 @@ struct usb_phy {
 
 	/* reset the PHY clocks */
 	int     (*reset)(struct usb_phy *x);
+
+	int	(*drive_dp_pulse)(struct usb_phy *x, unsigned int pulse_width);
 };
 
 /**
@@ -255,6 +257,15 @@ usb_phy_reset(struct usb_phy *x)
 {
 	if (x && x->reset)
 		return x->reset(x);
+
+	return 0;
+}
+
+static inline int
+usb_phy_drive_dp_pulse(struct usb_phy *x, unsigned int pulse_width)
+{
+	if (x && x->drive_dp_pulse)
+		return x->drive_dp_pulse(x, pulse_width);
 
 	return 0;
 }
