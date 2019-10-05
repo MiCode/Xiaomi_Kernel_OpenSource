ba9350749c0ec8efbc542d2a1c3fff62a8a15089
Stanley Chu <stanley.chu@mediatek.com>
2019-10-05 14:52:52 +0800

    [ALPS04325004] scsi: ufs-mediatek: introduce performance mode
    Introduce performance mode for crypto clock.
    
    When performance mode is on,
    - Request VCORE to highest gear by QoS API.
    - Change parents of UFS crypto clock to switch it to higher frequency.
    
    When performance mode is off,
    - Release VCORE QoS request.
    - Roll-back parents of UFS crypto clock.
    
    The mode switching flow shall be executed while all SCSI commands
    are done and blocked.
    
    MTK-Commit-Id: 5a3dfb17918cbd7b52cc5a019e20ec7ba00b2451
    
    Change-Id: I126beebe47e02db4c1eb26120b6a6a02445c9802
    Signed-off-by: Stanley Chu <stanley.chu@mediatek.com>
    CR-Id: ALPS04325004
    Feature: UFS Booting

--- drivers/scsi/ufs/ufshcd-pltfrm.c
+++ drivers/scsi/ufs/ufshcd-pltfrm.c
@@ -107,6 +107,13 @@ static int ufshcd_parse_clock_info(struct ufs_hba *hba)
 		if (ret)
 			goto out;
 
+		/* skip vendor clk, vendor clk shall be handled by vops */
+		if (strstr(name, "vendor")) {
+			dev_info(dev, "%s: vendor clk %s is found and skipped\n",
+				 __func__, name);
+			continue;
+		}
+
 		clki = devm_kzalloc(dev, sizeof(*clki), GFP_KERNEL);
 		if (!clki) {
 			ret = -ENOMEM;
