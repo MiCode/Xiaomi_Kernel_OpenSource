641962d32f4ab4c411c71d54c61f31c2bd796b91
Coboy Chen <coboy.chen@mediatek.com>
2019-10-05 14:27:27 +0800

    [ALPS04125073] gz: driver porting
    1. Porting drivers for Geniezone.
       - trusty kernel driver.
       - geniezone kernel driver.
    2. Fix checkpatch errors.
    
    MTK-Commit-Id: 6f77fb701aff3860fba237e6e601b1e4764cf699
    
    Change-Id: I97568e6282cb10e482277f2bbe13714ecf20c04b
    Signed-off-by: Coboy Chen <coboy.chen@mediatek.com>
    CR-Id: ALPS04125073
    Feature: GenieZone
    (cherry-pick from 05b63b6f0c18feeb6ed5e7afa0950908af4c0998)

---
+++ drivers/misc/mediatek/geniezone/mtee-kree/trusty-nop.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _GZ_TRUSTY_NOP_H_
+#define _GZ_TRUSTY_NOP_H_
+extern int trusty_call_nop_std32(uint32_t type, uint64_t value);
+#endif
