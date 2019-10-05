219b97f42eacf8b26dd9a3e5371bde84b9926440
Stanley Chu <stanley.chu@mediatek.com>
2019-10-05 14:48:13 +0800

    [ALPS04362512] hie: Use info level for log printing
    Original "debug" level logs will be hidden in userdebug
    builds.
    
    Use "info" instead to show them for easier debugging.
    
    All logs are still controlled by "hie_dbg" variable
    with default value 0, and not be printed by default.
    
    MTK-Commit-Id: a5e8b35c9311f25e96e64702d83ca9b766fdff1f
    
    Change-Id: I66856cea359721356b6d88313da8c62392d33d90
    Signed-off-by: Stanley Chu <stanley.chu@mediatek.com>
    CR-Id: ALPS04362512
    Feature: [Android Default] F2FS File System
    (cherry picked from commit 28d95a57d2921e569e65bd8db7623332ad6b3c55)
    (cherry picked from commit b5a6ab2a85351815cae5d7df335ec2f2f2e9b4ef)

--- security/hie/hie.c
+++ security/hie/hie.c
@@ -220,7 +220,7 @@ static void hie_dump_bio(struct bio *bio, const char *prefix)
 
 	iv = bio_bc_iv_get(bio);
 
-	pr_debug("HIE: %s: bio: %p %s, flag: %x, size: %d, file: %s, ino: %ld, iv: %lu\n",
+	pr_info("HIE: %s: bio: %p %s, flag: %x, size: %d, file: %s, ino: %ld, iv: %lu\n",
 		prefix, bio, __rw_str(bio),
 		bio->bi_crypt_ctx.bc_flags,
 		size, ptr?ptr:"", ino, iv);
@@ -297,7 +297,7 @@ static unsigned long hie_dummy_crypt_bio(const char *prefix, struct bio *bio,
 			ptr = get_page_name(bv.bv_page, path, 255, &ino);
 
 		if (hie_debug(HIE_DBG_CRY)) {
-			pr_debug("HIE: %s: %s bio: %p, base: %p %s len: %d, file: %s, ino: %ld, sec: %lu, iv: %llx, pgidx: %u\n",
+			pr_info("HIE: %s: %s bio: %p, base: %p %s len: %d, file: %s, ino: %ld, sec: %lu, iv: %llx, pgidx: %u\n",
 			  __func__, prefix, bio, data,
 			  __rw_str(bio), bv.bv_len,
 			  ptr, ino, (unsigned long)iter.bi_sector, *iv,
@@ -351,7 +351,7 @@ static int hie_dummy_crypt_req(const char *prefix, struct request *req,
 	blksize = queue_physical_block_size(req->q);
 
 	if (hie_debug(HIE_DBG_CRY)) {
-		pr_debug("HIE: %s: %s req: %p, req_iv: %llx\n",
+		pr_info("HIE: %s: %s req: %p, req_iv: %llx\n",
 		  __func__, prefix, req, iv);
 	}
 
@@ -363,7 +363,7 @@ static int hie_dummy_crypt_req(const char *prefix, struct request *req,
 			u64 bio_iv;
 
 			bio_iv = bio_bc_iv_get(bio);
-			pr_debug("HIE: %s: %s req: %p, req_iv: %llx, bio: %p, %s, bio_iv: %llu\n",
+			pr_info("HIE: %s: %s req: %p, req_iv: %llx, bio: %p, %s, bio_iv: %llu\n",
 			  __func__, prefix, req, iv, bio,
 			  __rw_str(bio),
 			  bio_iv);
@@ -568,7 +568,7 @@ try_lock_key:
 
 #ifdef CONFIG_HIE_DEBUG
 	if (key && hie_debug(HIE_DBG_KEY)) {
-		pr_debug("HIE: %s: master key\n", __func__);
+		pr_info("HIE: %s: master key\n", __func__);
 		print_hex_dump(KERN_DEBUG, "fs-key: ", DUMP_PREFIX_ADDRESS,
 			16, 1, key, key_size, 0);
 	}
@@ -605,7 +605,7 @@ int hie_decrypt(struct hie_dev *dev, struct request *req, void *priv)
 	ret = hie_req_key_act(dev, req, dev->decrypt, priv);
 #ifdef CONFIG_HIE_DEBUG
 	if (hie_debug(HIE_DBG_HIE))
-		pr_debug("HIE: %s: req: %p, ret=%d\n", __func__, req, ret);
+		pr_info("HIE: %s: req: %p, ret=%d\n", __func__, req, ret);
 #endif
 	return ret;
 }
@@ -618,7 +618,7 @@ int hie_encrypt(struct hie_dev *dev, struct request *req, void *priv)
 	ret = hie_req_key_act(dev, req, dev->encrypt, priv);
 #ifdef CONFIG_HIE_DEBUG
 	if (hie_debug(HIE_DBG_HIE))
-		pr_debug("HIE: %s: req: %p, ret=%d\n", __func__, req, ret);
+		pr_info("HIE: %s: req: %p, ret=%d\n", __func__, req, ret);
 #endif
 	return ret;
 }
