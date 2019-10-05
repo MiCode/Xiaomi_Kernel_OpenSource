03f7b81f3c41791caf83eb8887174d110271dc60
Stanley Chu <stanley.chu@mediatek.com>
2019-10-05 14:51:03 +0800

    [ALPS04362512] scsi: ufs: Add history of fatal events
    Currently only "interrupt-based" errors have their own history,
    however there are some "non-interrupt-based" errors or events
    which need history to improve debugging or help know the health
    status of UFS devices.
    
    Example of fatal errors,
    - Link startup error
    - Suspend error
    - Resume error
    
    Example of abnormal events,
    - Task or request abort
    - Device reset (now equals to Logical Unit Reset)
    - Host reset
    
    This patch tries to track above errors and events by existed UFS error
    history mechanism.
    
    MTK-Commit-Id: 199a6ebfdda78ac4940c5dd8f3aa7f1af0696762
    
    Change-Id: Iffcd34df972ffce240865024bf5f698e75430bfa
    Signed-off-by: Stanley Chu <stanley.chu@mediatek.com>
    Reviewed-by: Avri Altman <avri.altman@wdc.com>
    CR-Id: ALPS04362512
    Feature: UFS Booting

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -485,6 +485,14 @@ struct ufs_err_reg_hist {
  * @nl_err: tracks nl-uic errors
  * @tl_err: tracks tl-uic errors
  * @dme_err: tracks dme errors
+ * @auto_hibern8_err: tracks auto-hibernate errors
+ * @fatal_err: tracks fatal errors
+ * @linkup_err: tracks link-startup errors
+ * @resume_err: tracks resume errors
+ * @suspend_err: tracks suspend errors
+ * @dev_reset: tracks device reset events
+ * @host_reset: tracks host reset events
+ * @task_abort: tracks task abort events
  */
 struct ufs_stats {
 	u32 hibern8_exit_cnt;
@@ -498,8 +506,16 @@ struct ufs_stats {
 	struct ufs_err_reg_hist dme_err;
 
 	/* fatal errors */
-	struct ufs_err_reg_hist fatal_err;
 	struct ufs_err_reg_hist auto_hibern8_err;
+	struct ufs_err_reg_hist fatal_err;
+	struct ufs_err_reg_hist link_startup_err;
+	struct ufs_err_reg_hist resume_err;
+	struct ufs_err_reg_hist suspend_err;
+
+	/* abnormal events */
+	struct ufs_err_reg_hist dev_reset;
+	struct ufs_err_reg_hist host_reset;
+	struct ufs_err_reg_hist task_abort;
 };
 
 /* MTK PATCH UFS Host Controller debug print bitmask */
