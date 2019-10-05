7cf2d4d540dc15b3ff4635faa02af9f97dd8e8f9
Joey Pan <joey.pan@mediatek.com>
2019-10-05 14:50:01 +0800

    [ALPS04258366] DISP:Fix unchecked return value error
    Because the result of sg_alloc_table might not be
    success, it would make the error propagation if we
    use the unchecked sg handle. Add the return value
    check of sg_alloc_table.
    
    MTK-Commit-Id: c45a46fbb622932f653e22363f0f7c7bb5d55c08
    
    Change-Id: I17a2674caefc92e98d1dc1132c086677e898e0d1
    Signed-off-by: Joey Pan <joey.pan@mediatek.com>
    CR-Id: ALPS04258366
    Feature: [Module]Display Driver

--- drivers/misc/mediatek/video/mt6885/videox/debug.c
+++ drivers/misc/mediatek/video/mt6885/videox/debug.c
@@ -486,7 +486,11 @@ static int alloc_buffer_from_dma(size_t size, struct test_buf_info *buf_info)
 		static struct sg_table table;
 		struct sg_table *sg_table = &table;
 
-		sg_alloc_table(sg_table, 1, GFP_KERNEL);
+		ret = sg_alloc_table(sg_table, 1, GFP_KERNEL);
+		if (ret) {
+			DISPERR("allocate sg table failed: %d\n", ret);
+			return ret;
+		}
 
 		sg_dma_address(sg_table->sgl) = buf_info->buf_pa;
 		sg_dma_len(sg_table->sgl) = size_align;
