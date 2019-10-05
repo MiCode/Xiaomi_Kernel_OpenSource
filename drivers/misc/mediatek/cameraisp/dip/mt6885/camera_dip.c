0c45b5e099c3e4f5013bb30df425505e484b0cb8
justin-yt.wu <justin-yt.wu@mediatek.com>
2019-10-05 14:51:13 +0800

    [ALPS04363798] DIP: fix iommu port name
    [Detail] fix iommu port name
    
    [Solution]
    1. fix iommu port name for translation fault callback
    
    MTK-Commit-Id: 3069b9b53267b132025458e5961d3ae95b5b82f4
    
    Change-Id: I11dd1674c74490d7a34f8b15a52e97567f2186c8
    CR-Id: ALPS04363798
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: justin-yt.wu <justin-yt.wu@mediatek.com>
    (cherry picked from commit 447b420b9104114ebab30a65c8beec498ea5e0b1)

--- drivers/misc/mediatek/cameraisp/dip/mt6885/camera_dip.c
+++ drivers/misc/mediatek/cameraisp/dip/mt6885/camera_dip.c
@@ -5488,35 +5488,73 @@ static signed int __init DIP_Init(void)
 		(DIP_BeginGCECallback, DIP_EndGCECallback);
 #endif
 	/* m4u_enable_tf(M4U_PORT_CAM_IMGI, 0);*/
-#if 0 //def CONFIG_MTK_IOMMU_V2
-	mtk_iommu_register_fault_callback(M4U_PORT_IMGI_D1,
+#ifdef CONFIG_MTK_IOMMU_V2
+	mtk_iommu_register_fault_callback(M4U_PORT_L9_IMG_IMGI_D1_MDP,
+					  ISP_M4U_TranslationFault_callback,
+					  NULL);
+	mtk_iommu_register_fault_callback(M4U_PORT_L9_IMG_IMGBI_D1_MDP,
+					  ISP_M4U_TranslationFault_callback,
+					  NULL);
+	mtk_iommu_register_fault_callback(M4U_PORT_L9_IMG_DMGI_D1_MDP,
+					  ISP_M4U_TranslationFault_callback,
+					  NULL);
+	mtk_iommu_register_fault_callback(M4U_PORT_L9_IMG_DEPI_D1_MDP,
+					  ISP_M4U_TranslationFault_callback,
+					  NULL);
+	mtk_iommu_register_fault_callback(M4U_PORT_L9_IMG_ICE_D1_MDP,
+					  ISP_M4U_TranslationFault_callback,
+					  NULL);
+	mtk_iommu_register_fault_callback(M4U_PORT_L9_IMG_SMTI_D1_MDP,
+					  ISP_M4U_TranslationFault_callback,
+					  NULL);
+	mtk_iommu_register_fault_callback(M4U_PORT_L9_IMG_SMTO_D1_MDP,
+					  ISP_M4U_TranslationFault_callback,
+					  NULL);
+	mtk_iommu_register_fault_callback(M4U_PORT_L9_IMG_SMTO_D2_MDP,
+					  ISP_M4U_TranslationFault_callback,
+					  NULL);
+	mtk_iommu_register_fault_callback(M4U_PORT_L9_IMG_CRZO_D1_MDP,
+					  ISP_M4U_TranslationFault_callback,
+					  NULL);
+	mtk_iommu_register_fault_callback(M4U_PORT_L9_IMG_IMG3O_D1_MDP,
+					  ISP_M4U_TranslationFault_callback,
+					  NULL);
+	mtk_iommu_register_fault_callback(M4U_PORT_L9_IMG_VIPI_D1_MDP,
+					  ISP_M4U_TranslationFault_callback,
+					  NULL);
+	mtk_iommu_register_fault_callback(M4U_PORT_L9_IMG_SMTI_D5_MDP,
+					  ISP_M4U_TranslationFault_callback,
+					  NULL);
+	mtk_iommu_register_fault_callback(M4U_PORT_L9_IMG_TIMGO_D1_MDP,
 					  ISP_M4U_TranslationFault_callback,
 					  NULL);
 #else
   #ifndef CONFIG_FPGA_EARLY_PORTING   //Todo: Justin EP, mt6789 porting
-	m4u_register_fault_callback(M4U_PORT_IMGI_D1,
+	m4u_register_fault_callback(M4U_PORT_L9_IMG_IMGI_D1_MDP,
 			ISP_M4U_TranslationFault_callback, NULL);
-	m4u_register_fault_callback(M4U_PORT_IMGBI_D1,
+	m4u_register_fault_callback(M4U_PORT_L9_IMG_IMGBI_D1_MDP,
 			ISP_M4U_TranslationFault_callback, NULL);
-	m4u_register_fault_callback(M4U_PORT_DMGI_D1,
+	m4u_register_fault_callback(M4U_PORT_L9_IMG_DMGI_D1_MDP,
 			ISP_M4U_TranslationFault_callback, NULL);
-	m4u_register_fault_callback(M4U_PORT_DEPI_D1,
+	m4u_register_fault_callback(M4U_PORT_L9_IMG_DEPI_D1_MDP,
 			ISP_M4U_TranslationFault_callback, NULL);
-	m4u_register_fault_callback(M4U_PORT_LCEI_D1,
+	m4u_register_fault_callback(M4U_PORT_L9_IMG_ICE_D1_MDP,
 			ISP_M4U_TranslationFault_callback, NULL);
-	m4u_register_fault_callback(M4U_PORT_SMTI_D1,
+	m4u_register_fault_callback(M4U_PORT_L9_IMG_SMTI_D1_MDP,
 			ISP_M4U_TranslationFault_callback, NULL);
-	m4u_register_fault_callback(M4U_PORT_SMTO_D1,
+	m4u_register_fault_callback(M4U_PORT_L9_IMG_SMTO_D1_MDP,
 			ISP_M4U_TranslationFault_callback, NULL);
-	m4u_register_fault_callback(M4U_PORT_SMTO_D2,
+	m4u_register_fault_callback(M4U_PORT_L9_IMG_SMTO_D2_MDP,
 			ISP_M4U_TranslationFault_callback, NULL);
-	m4u_register_fault_callback(M4U_PORT_CRZO_D1,
+	m4u_register_fault_callback(M4U_PORT_L9_IMG_CRZO_D1_MDP,
 			ISP_M4U_TranslationFault_callback, NULL);
-	m4u_register_fault_callback(M4U_PORT_IMG3O_D1,
+	m4u_register_fault_callback(M4U_PORT_L9_IMG_IMG3O_D1_MDP,
 			ISP_M4U_TranslationFault_callback, NULL);
-	m4u_register_fault_callback(M4U_PORT_VIPI_D1,
+	m4u_register_fault_callback(M4U_PORT_L9_IMG_VIPI_D1_MDP,
 			ISP_M4U_TranslationFault_callback, NULL);
-	m4u_register_fault_callback(M4U_PORT_TIMGO_D1,
+	m4u_register_fault_callback(M4U_PORT_L9_IMG_SMTI_D5_MDP,
+			ISP_M4U_TranslationFault_callback, NULL);
+	m4u_register_fault_callback(M4U_PORT_L9_IMG_TIMGO_D1_MDP,
 			ISP_M4U_TranslationFault_callback, NULL);
  #endif
 #endif
