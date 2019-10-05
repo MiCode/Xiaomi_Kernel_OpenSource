15b2b1b857e36fbab4f4e3d90386443ad72a4238
Joey Pan <joey.pan@mediatek.com>
2019-10-05 14:52:28 +0800

    [ALPS04454332] DISP: Fix display layer error
    Because HWC would push a mismatched layer
    when we turn on EXT_LAYER, we turn it off for
    temporal solution.
    
    MTK-Commit-Id: d8326da1d847084d4302131e508f480e596e23b8
    
    Change-Id: I877bbf639b4371e069fa9f969140ac7cf0ad8eaa
    Signed-off-by: Joey Pan <joey.pan@mediatek.com>
    CR-Id: ALPS04454332
    Feature: [Module]Display Driver

--- drivers/misc/mediatek/video/mt6885/videox/disp_helper.c
+++ drivers/misc/mediatek/video/mt6885/videox/disp_helper.c
@@ -410,7 +410,7 @@ void disp_helper_option_init(void)
 	disp_helper_set_option(DISP_OPT_SHADOW_MODE, 0);
 
 	/* smart layer OVL*/
-	disp_helper_set_option(DISP_OPT_OVL_EXT_LAYER, 1);
+	disp_helper_set_option(DISP_OPT_OVL_EXT_LAYER, 0);
 
 	disp_helper_set_option(DISP_OPT_REG_PARSER_RAW_DUMP, 0);
 
