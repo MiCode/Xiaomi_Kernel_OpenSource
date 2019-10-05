45f24322101bde25bb1d11cec79ef3ccde145c13
Freddy Hsin <freddy.hsin@mediatek.com>
2019-10-05 12:08:58 +0800

    [ALPS04002446] timer: flush cpu base lock after unlocking
    MTK PATCH to fix ARM v8.0 live spinlock issue.
    
    Flush lock value here if timer cancelling or deletion
    is not finished.
    
    In this case, Other CPU may need to get cpu_base spinlock
    to update running timer information. Flush lock value specifically
    after unlocking to promise that other CPU can see correct
    lock value to avoid starvation or unfair spinlock competition.
    
    MTK-Commit-Id: be8530c2a8c164d4b64a5c8693d6cdfc9eec5b1b
    
    Change-Id: I160f44997e6831837db52b2b52f7187fe0b8999e
    Signed-off-by: Freddy Hsin <freddy.hsin@mediatek.com>
    CR-Id: ALPS04002446
    Feature: [Module]Timer

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -53,6 +53,7 @@
 #include <linux/freezer.h>
 #include <linux/compat.h>
 
+#include <asm/cacheflush.h>
 #include <linux/uaccess.h>
 
 #include <trace/events/timer.h>
@@ -1014,6 +1015,34 @@ int hrtimer_try_to_cancel(struct hrtimer *timer)
 		ret = remove_hrtimer(timer, base, false);
 
 	unlock_hrtimer_base(timer, &flags);
+
+#if defined(CONFIG_SMP) && !defined(CONFIG_ARM64_LSE_ATOMICS)
+
+#ifndef dmac_flush_range
+#define dmac_flush_range __dma_flush_range
+#endif
+
+	/*
+	 * MTK PATCH to fix ARM v8.0 live spinlock issue.
+	 *
+	 * Flush lock value here if timer cancelling is not finished.
+	 *
+	 * In this case, Other CPU may need to get cpu_base spinlock
+	 * to update running timer information. Flush lock value here
+	 * to promise that other CPU can see correct lock value to avoid
+	 * starvation or unfair spinlock competition.
+	 */
+	if (ret == -1 && irqs_disabled()) {
+#ifndef __dma_flush_range
+		__dma_flush_area((void *)&base->cpu_base->lock,
+				   sizeof(raw_spinlock_t));
+#else
+		dmac_flush_range((void *)&base->cpu_base->lock,
+				   (void *)&base->cpu_base->lock +
+				   sizeof(raw_spinlock_t) - 1);
+#endif
+	}
+#endif
 
 	return ret;
 
