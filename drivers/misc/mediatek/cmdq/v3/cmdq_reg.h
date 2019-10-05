8f55f01ecf92a39103283d02b29d07a66fbf4d35
Dennis YC Hsieh <dennis-yc.hsieh@mediatek.com>
2019-10-05 12:50:54 +0800

    [ALPS04064816] cmdq: Enable cmdq driver
    Porting cmdq driver with following,
    1. CMDQ v3 driver which calls mbox cmdq.
    2. CMDQ mailbox helper and controller.
    3. DTS node document.
    4. Kernel config for v3 and mbox.
    5. CMDQ dts bindings and dts node.
    
    MTK-Commit-Id: 0a83452cf18c400c1c877d914f664695bc9a1bac
    
    Change-Id: I4ef717943be8805ebf6d4e87efb7c83ba809545c
    CR-Id: ALPS04064816
    Feature: [Module]CMDQ Driver
    Signed-off-by: Dennis YC Hsieh <dennis-yc.hsieh@mediatek.com>
    (cherry picked from commit 0960e4ba3a5bc443701aee4f94a6deb4e8e7acd2)

---
+++ drivers/misc/mediatek/cmdq/v3/cmdq_reg.h
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __CMDQ_REG_H__
+#define __CMDQ_REG_H__
+
+#include <mt-plat/sync_write.h>
+#include <linux/io.h>
+
+#include "cmdq_helper_ext.h"
+#include "cmdq_device.h"
+
+#define GCE_BASE_PA			cmdq_dev_get_module_base_PA_GCE()
+#define GCE_BASE_VA			cmdq_dev_get_module_base_VA_GCE()
+
+#define CMDQ_CORE_WARM_RESET	(GCE_BASE_VA + 0x000)
+#define CMDQ_CURR_IRQ_STATUS	(GCE_BASE_VA + 0x010)
+#define CMDQ_SECURE_IRQ_STATUS	(GCE_BASE_VA + 0x014)
+#define CMDQ_CURR_LOADED_THR	(GCE_BASE_VA + 0x018)
+#define CMDQ_THR_SLOT_CYCLES	(GCE_BASE_VA + 0x030)
+#define CMDQ_THR_EXEC_CYCLES	(GCE_BASE_VA + 0x034)
+#define CMDQ_THR_TIMEOUT_TIMER	(GCE_BASE_VA + 0x038)
+#define CMDQ_BUS_CONTROL_TYPE	(GCE_BASE_VA + 0x040)
+#define CMDQ_H_SPEED_BUSY	(GCE_BASE_VA + 0x048)
+#define CMDQ_CURR_INST_ABORT	(GCE_BASE_VA + 0x020)
+#define CMDQ_SECURITY_ABORT	(GCE_BASE_VA + 0x050)
+#define CMDQ_SECURITY_CTL	(GCE_BASE_VA + 0x054)
+
+#define CMDQ_SECURITY_STA(id)	(GCE_BASE_VA + (0x030 * id) + 0x024)
+#define CMDQ_SECURITY_SET(id)	(GCE_BASE_VA + (0x030 * id) + 0x028)
+#define CMDQ_SECURITY_CLR(id)	(GCE_BASE_VA + (0x030 * id) + 0x02C)
+
+#define CMDQ_SYNC_TOKEN_ID	(GCE_BASE_VA + 0x060)
+#define CMDQ_SYNC_TOKEN_VAL	(GCE_BASE_VA + 0x064)
+#define CMDQ_SYNC_TOKEN_UPD	(GCE_BASE_VA + 0x068)
+
+#define CMDQ_PREFETCH_GSIZE	(GCE_BASE_VA + 0x0C0)
+#define CMDQ_TPR_MASK		(GCE_BASE_VA + 0x0D0)
+#define CMDQ_TPR_GPR_TIMER	(GCE_BASE_VA + 0x0DC)
+#define CMDQ_CTL_INT0		(GCE_BASE_VA + 0x0F0)
+#define CMDQ_CTL_INT1		(GCE_BASE_VA + 0x0F4)
+#define CMDQ_CACHE_0_EN_LO	(GCE_BASE_VA + 0x10D0)
+#define CMDQ_CACHE_0_EN_HI	(GCE_BASE_VA + 0x10D4)
+#define CMDQ_CACHE_1_EN_LO	(GCE_BASE_VA + 0x10D8)
+#define CMDQ_CACHE_1_EN_HI	(GCE_BASE_VA + 0x10DC)
+#define CMDQ_TOKEN_0_EN_LO	(GCE_BASE_VA + 0x10E0)
+#define CMDQ_TOKEN_0_EN_HI	(GCE_BASE_VA + 0x10E4)
+#define CMDQ_TOKEN_1_EN_LO	(GCE_BASE_VA + 0x10E8)
+#define CMDQ_TOKEN_1_EN_HI	(GCE_BASE_VA + 0x10EC)
+
+#define CMDQ_GPR_R32(id)		(GCE_BASE_VA + (0x004 * id) + 0x80)
+#define CMDQ_GPR_R32_PA(id)		(GCE_BASE_PA + (0x004 * id) + 0x80)
+
+#define CMDQ_THR_WARM_RESET(id)		(GCE_BASE_VA + (0x080 * id) + 0x100)
+#define CMDQ_THR_ENABLE_TASK(id)	(GCE_BASE_VA + (0x080 * id) + 0x104)
+#define CMDQ_THR_SUSPEND_TASK(id)	(GCE_BASE_VA + (0x080 * id) + 0x108)
+#define CMDQ_THR_CURR_STATUS(id)	(GCE_BASE_VA + (0x080 * id) + 0x10C)
+#define CMDQ_THR_IRQ_STATUS(id)		(GCE_BASE_VA + (0x080 * id) + 0x110)
+#define CMDQ_THR_IRQ_ENABLE(id)		(GCE_BASE_VA + (0x080 * id) + 0x114)
+#define CMDQ_THR_SECURITY(id)		(GCE_BASE_VA + (0x080 * id) + 0x118)
+#define CMDQ_THR_CURR_ADDR(id)		(GCE_BASE_VA + (0x080 * id) + 0x120)
+#define CMDQ_THR_END_ADDR(id)		(GCE_BASE_VA + (0x080 * id) + 0x124)
+#define CMDQ_THR_EXEC_CNT(id)		(GCE_BASE_VA + (0x080 * id) + 0x128)
+#define CMDQ_THR_WAIT_TOKEN(id)		(GCE_BASE_VA + (0x080 * id) + 0x130)
+#define CMDQ_THR_CFG(id)		(GCE_BASE_VA + (0x080 * id) + 0x140)
+#define CMDQ_THR_PREFETCH(id)		(GCE_BASE_VA + (0x080 * id) + 0x144)
+#define CMDQ_THR_INST_CYCLES(id)	(GCE_BASE_VA + (0x080 * id) + 0x150)
+#define CMDQ_THR_INST_THRESX(id)	(GCE_BASE_VA + (0x080 * id) + 0x154)
+#define CMDQ_THR_SPR0(id)		(GCE_BASE_VA + (0x080 * id) + 0x160)
+#define CMDQ_THR_SPR1(id)		(GCE_BASE_VA + (0x080 * id) + 0x164)
+#define CMDQ_THR_SPR2(id)		(GCE_BASE_VA + (0x080 * id) + 0x168)
+#define CMDQ_THR_SPR3(id)		(GCE_BASE_VA + (0x080 * id) + 0x16c)
+
+#define CMDQ_THR_SECURITY_PA(id)	(GCE_BASE_PA + (0x080 * id) + 0x118)
+#define CMDQ_THR_CURR_ADDR_PA(id)	(GCE_BASE_PA + (0x080 * id) + 0x120)
+#define CMDQ_THR_END_ADDR_PA(id)	(GCE_BASE_PA + (0x080 * id) + 0x124)
+#define CMDQ_THR_EXEC_CNT_PA(id)	(GCE_BASE_PA + (0x080 * id) + 0x128)
+
+#define CMDQ_SECURITY_CTL_PA		(GCE_BASE_PA + 0x054)
+#define CMDQ_SYNC_TOKEN_ID_PA		(GCE_BASE_PA + 0x060)
+#define CMDQ_SYNC_TOKEN_VAL_PA		(GCE_BASE_PA + 0x064)
+#define CMDQ_PREFETCH_GSIZE_PA		(GCE_BASE_PA + 0x0C0)
+#define CMDQ_TPR_MASK_PA		(GCE_BASE_PA + 0x0D0)
+
+#define CMDQ_GCE_END_ADDR_PA		(GCE_BASE_PA + 0xC00)
+#define CMDQ_THR_FIX_END_ADDR(id)	(CMDQ_GCE_END_ADDR_PA | (id << 4))
+
+#define CMDQ_CACHE_0_EN_LO_PA		(GCE_BASE_PA + 0x10D0)
+#define CMDQ_CACHE_0_EN_HI_PA		(GCE_BASE_PA + 0x10D4)
+#define CMDQ_CACHE_1_EN_LO_PA		(GCE_BASE_PA + 0x10D8)
+#define CMDQ_CACHE_1_EN_HI_PA		(GCE_BASE_PA + 0x10DC)
+#define CMDQ_TOKEN_0_EN_LO_PA		(GCE_BASE_PA + 0x10E0)
+#define CMDQ_TOKEN_0_EN_HI_PA		(GCE_BASE_PA + 0x10E4)
+#define CMDQ_TOKEN_1_EN_LO_PA		(GCE_BASE_PA + 0x10E8)
+#define CMDQ_TOKEN_1_EN_HI_PA		(GCE_BASE_PA + 0x10EC)
+
+#define CMDQ_IS_END_ADDR(addr)		(addr == 0)
+#define CMDQ_IS_END_INSTR(p_cmd_end)	\
+	(p_cmd_end[0] == 0 && p_cmd_end[-1] == 0)
+#define CMDQ_IS_SRAM_ADDR(addr)		(((addr) & 0xFFFF) == (addr))
+
+#define CMDQ_REG_GET32(addr)		(readl((void *)addr) & 0xFFFFFFFF)
+#define CMDQ_REG_GET16(addr)		(readl((void *)addr) & 0x0000FFFF)
+
+#define CMDQ_REG_GET64_GPR_PX(id)	cmdq_core_get_gpr64(id)
+#define CMDQ_REG_SET64_GPR_PX(id, value)	cmdq_core_set_gpr64(id, value)
+
+#define CMDQ_GET_GPR_PX2RX_LOW(id)	((id & 0xf) * 2)
+#define CMDQ_GET_GPR_PX2RX_HIGH(id)	((id & 0xf) * 2 + 1)
+
+#define CMDQ_REG_SET32(addr, val)	mt_reg_sync_writel(val, (addr))
+
+
+#endif				/* __CMDQ_REG_H__ */
---
+++ drivers/misc/mediatek/cmdq/v3/cmdq_sec_gp.c
@@ -0,0 +1,159 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#include "cmdq_core.h"
+#include "cmdq_sec_gp.h"
+
+#define UUID_STR "09010000000000000000000000000000"
+
+void cmdq_sec_setup_tee_context(struct cmdq_sec_tee_context *tee)
+{
+	/* 09010000 0000 0000 0000000000000000 */
+#if defined(CONFIG_TRUSTONIC_TEE_SUPPORT)
+	tee->uuid = (struct TEEC_UUID) { 0x09010000, 0x0, 0x0,
+		{ 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 } };
+#else
+	tee->uuid = (TEEC_UUID) { 0x09010000, 0x0, 0x0,
+		{ 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 } };
+#endif
+}
+
+s32 cmdq_sec_init_context(struct cmdq_sec_tee_context *tee)
+{
+	s32 status;
+
+	CMDQ_MSG("[SEC] enter %s\n", __func__);
+#if defined(CONFIG_MICROTRUST_TEE_SUPPORT)
+	while (!is_teei_ready()) {
+		CMDQ_MSG("[SEC] Microtrust TEE is not ready, wait...\n");
+		msleep(1000);
+	}
+#elif defined(CONFIG_TRUSTONIC_TEE_SUPPORT)
+	while (!is_mobicore_ready()) {
+		CMDQ_MSG("[SEC] Trustonic TEE is not ready, wait...\n");
+		msleep(1000);
+	}
+#endif
+	CMDQ_LOG("[SEC]TEE is ready\n");
+
+	status = TEEC_InitializeContext(UUID_STR, &tee->gp_context);
+	if (status != TEEC_SUCCESS)
+		CMDQ_ERR("[SEC]init_context fail: status:0x%x\n", status);
+	else
+		CMDQ_MSG("[SEC]init_context: status:0x%x\n", status);
+	return status;
+}
+
+s32 cmdq_sec_deinit_context(struct cmdq_sec_tee_context *tee)
+{
+	TEEC_FinalizeContext(&tee->gp_context);
+	return 0;
+}
+
+s32 cmdq_sec_allocate_wsm(struct cmdq_sec_tee_context *tee,
+	void **wsm_buffer, u32 size)
+{
+	s32 status;
+
+	if (!wsm_buffer)
+		return -EINVAL;
+
+	CMDQ_MSG("enter %s, tee=%p, size=%d\n", __func__, tee, size);
+	tee->shared_mem.size = size;
+	tee->shared_mem.flags = TEEC_MEM_INPUT | TEEC_MEM_OUTPUT;
+	status = TEEC_AllocateSharedMemory(&tee->gp_context,
+		&tee->shared_mem);
+	if (status != TEEC_SUCCESS) {
+		CMDQ_ERR("[SEC]allocate_wsm: err:0x%x\n", status);
+	} else {
+		CMDQ_MSG("[SEC]allocate_wsm: status:0x%x pWsm:0x%p\n",
+			status, tee->shared_mem.buffer);
+		*wsm_buffer = (void *)tee->shared_mem.buffer;
+	}
+
+	return status;
+}
+
+s32 cmdq_sec_free_wsm(struct cmdq_sec_tee_context *tee,
+	void **wsm_buffer)
+{
+	if (!wsm_buffer)
+		return -EINVAL;
+
+	TEEC_ReleaseSharedMemory(&tee->shared_mem);
+	*wsm_buffer = NULL;
+	return 0;
+}
+
+s32 cmdq_sec_open_session(struct cmdq_sec_tee_context *tee,
+	void *wsm_buffer)
+{
+	s32 status;
+
+	if (!wsm_buffer) {
+		CMDQ_ERR("[SEC]open_session: invalid param wsm buffer:0x%p\n",
+			wsm_buffer);
+		return -EINVAL;
+	}
+
+	status = TEEC_OpenSession(&tee->gp_context,
+		&tee->session, &tee->uuid,
+		TEEC_LOGIN_PUBLIC, NULL, NULL, NULL);
+
+	if (status != TEEC_SUCCESS) {
+		/* print error message */
+		CMDQ_ERR("[SEC]open_session fail: status:0x%x\n",
+			status);
+	} else {
+		CMDQ_MSG("[SEC]open_session: status:0x%x\n", status);
+	}
+
+	return status;
+}
+
+s32 cmdq_sec_close_session(struct cmdq_sec_tee_context *tee)
+{
+	TEEC_CloseSession(&tee->session);
+	return 0;
+}
+
+s32 cmdq_sec_execute_session(struct cmdq_sec_tee_context *tee,
+	u32 cmd, s32 timeout_ms)
+{
+	s32 status;
+	struct TEEC_Operation operation;
+
+	memset(&operation, 0, sizeof(struct TEEC_Operation));
+#if defined(CONFIG_TRUSTONIC_TEE_SUPPORT)
+	operation.param_types = TEEC_PARAM_TYPES(TEEC_MEMREF_PARTIAL_INOUT,
+		TEEC_NONE, TEEC_NONE, TEEC_NONE);
+#else
+	operation.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_PARTIAL_INOUT,
+		TEEC_NONE, TEEC_NONE, TEEC_NONE);
+#endif
+	operation.params[0].memref.parent = &tee->shared_mem;
+	operation.params[0].memref.size = tee->shared_mem.size;
+	operation.params[0].memref.offset = 0;
+
+	status = TEEC_InvokeCommand(&tee->session, cmd, &operation,
+		NULL);
+	if (status != TEEC_SUCCESS)
+		CMDQ_ERR("[SEC]execute: TEEC_InvokeCommand:%u err:%d\n",
+			cmd, status);
+	else
+		CMDQ_MSG("[SEC]execute: TEEC_InvokeCommand:%u ret:%d\n",
+			cmd, status);
+
+	return status;
+}
+
