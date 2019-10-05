dd4bdbf10a511a4525a91bca29d6efd11409c727
anny hu <anny.hu@mediatek.com>
2019-10-05 14:31:03 +0800

    [ALPS04402533] netdev: wifi unregister fail under WFC
    [Detail]turn off and turn on WFC, and then turn off wifi,
    wlan0 unregister fail because dev->refcnt!=0.
    
    unregister_netdevice: waiting for wlan0 to become free. Usage count=2
    
    Because hold dev->refcnt in xfrm dst allocate and put dev->refcnt in
    xfrm dst destroy, if there one xfrm dst is not destroy, dev->refcnt
    cant be released to 0.
    
    In kernel-4.9, when free a xdst, it would be inserted into
    dst_garbage.list first. Then if its refcnt was still head somewhere,
    later it would be put into dst_busy_list and put this dev when dev is
    being unregistered.
    
    However in kernel-4.14, the garbage mechanism is removed, and the xdst
    will not be freed by garbage, and dev unregister fail.
    
    [Solution]save xdsts in uncached_list in xfrm_fill_dst(), so that the
    dev refcnt can be released in the event NETDEV_UNREGISTER process of
    fib_netdev_notifier.
    
    MTK-Commit-Id: db494efb530578421d9b81a3a2a7d88976353775
    
    Change-Id: I37753eb20db669215bfae57950346d565ad7892f
    CR-Id:ALPS04402533
    Feature:Modem Interface Driver
    Signed-off-by: anny hu <anny.hu@mediatek.com>
    (cherry picked from commit 9ea5edf8236bcefa5674cab988f604453d625dc1)

--- include/net/ip6_route.h
+++ include/net/ip6_route.h
@@ -171,6 +171,9 @@ void rt6_mtu_change(struct net_device *dev, unsigned int mtu);
 void rt6_remove_prefsrc(struct inet6_ifaddr *ifp);
 void rt6_clean_tohost(struct net *net, struct in6_addr *gateway);
 
+void rt6_uncached_list_add(struct rt6_info *rt);
+void rt6_uncached_list_del(struct rt6_info *rt);
+
 static inline const struct rt6_info *skb_rt6_info(const struct sk_buff *skb)
 {
 	const struct dst_entry *dst = skb_dst(skb);
