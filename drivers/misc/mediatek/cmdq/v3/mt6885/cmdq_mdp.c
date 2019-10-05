ef658be94e1ee3964a1e0987e240bf487994bb90
Mike Hsieh <mike.hsieh@mediatek.com>
2019-10-05 14:52:06 +0800

    [ALPS04745742] cmdq: dts naming error
    Fix dts naming error
    
    MTK-Commit-Id: 7e4eec7e3a588b6e14a69645c486fb096cca2d0c
    
    Change-Id: Ifeee490d04fcf4cb0e0b19ef25bcfb197657bc0b
    CR-Id: ALPS04745742
    Feature: [Module]CMDQ Driver
    Signed-off-by: Mike Hsieh <mike.hsieh@mediatek.com>

--- drivers/misc/mediatek/cmdq/v3/mt6885/cmdq_mdp.c
+++ drivers/misc/mediatek/cmdq/v3/mt6885/cmdq_mdp.c
@@ -746,7 +746,7 @@ void cmdq_mdp_init_module_base_VA(void)
 	gCmdqMdpModuleBaseVA.VENC =
 		cmdq_dev_alloc_reference_VA_by_name("venc");
 	gCmdqMdpModuleBaseVA.MM_MUTEX =
-		cmdq_dev_alloc_reference_VA_by_name("mdp_mutex");
+		cmdq_dev_alloc_reference_VA_by_name("mm_mutex");
 }
 
 void cmdq_mdp_deinit_module_base_VA(void)
