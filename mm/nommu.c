39aafb18cf6b2c4d025b349f732f1648ad47f811
Chinwen Chang <chinwen.chang@mediatek.com>
2019-10-05 14:51:14 +0800

    [ALPS04721294] FROMLIST: mm: introduce INIT_VMA()
    (from https://lore.kernel.org/patchwork/patch/906213/)
    
    Some VMA struct fields need to be initialized once the VMA structure is
    allocated.
    Currently this only concerns anon_vma_chain field but some other will be
    added to support the speculative page fault.
    
    Instead of spreading the initialization calls all over the code, lets
    introduce a dedicated inline function.
    
    Signed-off-by: Laurent Dufour <ldufour@linux.vnet.ibm.com>
    
    MTK-Commit-Id: 0b3aca558f8d5e55e76c734b5b83742a1734b99f
    
    Change-Id: I8cf06e24e97eb5b8fcb88ffb737e786af7ef6f68
    Signed-off-by: Chinwen Chang <chinwen.chang@mediatek.com>
    CR-Id: ALPS04721294
    Feature: Memory Optimization

--- mm/nommu.c
+++ mm/nommu.c
@@ -1244,7 +1244,7 @@ unsigned long do_mmap(struct file *file,
 	region->vm_flags = vm_flags;
 	region->vm_pgoff = pgoff;
 
-	INIT_LIST_HEAD(&vma->anon_vma_chain);
+	INIT_VMA(vma);
 	vma->vm_flags = vm_flags;
 	vma->vm_pgoff = pgoff;
 
