41f86e8be895195f0ab765dfa6b73357b51e4767
Ming-Fan Chen <ming-fan.chen@mediatek.com>
2019-10-05 14:50:21 +0800

    [ALPS04694084] smi: support multiple commons for mt6885
    1. Support multiple commons clk/pwr for mt6885.
    2. Update public functions used by fdvt for mt6885.
    3. Update configuration for mt6885.
    
    MTK-Commit-Id: 2c0c96a0198b93824e94e5e41c7f838e417e092e
    
    Change-Id: I4d99366a8d5cdd248d4bc2944695a782047bc3dc
    CR-Id: ALPS04694084
    Feature: [Module]SMI Driver
    Signed-off-by: Ming-Fan Chen <ming-fan.chen@mediatek.com>

--- drivers/misc/mediatek/smi/mt6885/smi_hw.h
+++ drivers/misc/mediatek/smi/mt6885/smi_hw.h
@@ -18,19 +18,22 @@
 #include <smi_port.h>
 
 static const u32 smi_subsys_to_larbs[NR_SYSS] = {
-	[SYS_DIS] = ((1 << 0) | (1 << (SMI_LARB_NUM))),
-	[SYS_VDE] = (1 << 1),
-	[SYS_ISP] = ((1 << 2) | (1 << 5)),
-	[SYS_CAM] = ((1 << 3) | (1 << 6)),
-	[SYS_VEN] = (1 << 4),
+	[SYS_DIS] = ((1 << 0) | (1 << 1) | (1 << (SMI_LARB_NUM))),
+	// TODO
+	[SYS_VEN] = ((1 << 7) | (1 << 8)),
+	[SYS_VDE] = ((1 << 4) | (1 << 5)),
+	[SYS_CAM] = ((1 << 13) | (1 << 14) | (1 << 15) |
+		(1 << 16) | (1 << 17) | (1 << 18)),
+	[SYS_ISP] = ((1 << 9) | (1 << 11)),
+	[SYS_IPE] = ((1 << 19) | (1 << 20)),
 };
 
 #if IS_ENABLED(CONFIG_MMPROFILE)
 #include <mmprofile.h>
 
 static const char *smi_mmp_name[NR_SYSS] = {
-	[SYS_DIS] = "DIS", [SYS_VDE] = "VDE",
-	[SYS_ISP] = "ISP", [SYS_CAM] = "CAM", [SYS_VEN] = "VEN",
+	[SYS_DIS] = "DIS", [SYS_VEN] = "VEN", [SYS_VDE] = "VDE",
+	[SYS_ISP] = "ISP", [SYS_CAM] = "CAM", [SYS_IPE] = "IPE",
 };
 static mmp_event smi_mmp_event[NR_SYSS];
 #endif
