37150e0074ed5ebea86bff2857bd5d57af2de990
mtk16547 <Zhuang.Wang@mediatek.com>
2019-10-05 14:54:30 +0800

    [ALPS04750327] cam : add imx686 driver
    [Detail] add imx686 driver and sync customer sub avdd change
    
    [Solution]
    
    MTK-Commit-Id: 3612a11133997fdbfef968259fadb1e9b781a431
    
    Change-Id: I8d84b4dcc39562ab911aadd81662af4aa6c2449b
    Signed-off-by: mtk16547 <Zhuang.Wang@mediatek.com>
    CR-Id: ALPS04750327
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/misc/mediatek/imgsensor/src/mt6885/camera_hw/imgsensor_cfg_table.c
+++ drivers/misc/mediatek/imgsensor/src/mt6885/camera_hw/imgsensor_cfg_table.c
@@ -1159,7 +1159,21 @@ struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
 			},
 		},
 #endif
-
+#if defined(IMX686_MIPI_RAW)
+	{
+		SENSOR_DRVNAME_IMX686_MIPI_RAW,
+		{
+			{RST, Vol_Low, 1},
+			{AVDD, Vol_2900, 0},
+		/*in alph.dts file, pin avdd controls two gpio pins*/
+			/*{AVDD_1, Vol_1800, 0},*/
+			{DVDD, Vol_1100, 0},
+			{DOVDD, Vol_1800, 1},
+			{SensorMCLK, Vol_High, 1},
+			{RST, Vol_High, 1}
+		},
+	},
+#endif
 	/* add new sensor before this line */
 	{NULL,},
 };
