7fb3f765012d66e48d460efe1c97682468d398e9
mtk15472 <Yu-Ren.Wang@mediatek.com>
2019-10-05 14:50:19 +0800

    [ALPS04702242] reviser: reviser driver early porting
    [Detail]
    1. 1st code commit
    
    MTK-Commit-Id: c0b1f02006e9dfd8c4758f942472a7496ba1d015
    
    Feature: MDLA Platform
    CR-Id: ALPS04702242
    Change-Id: I383ea49582da4c254f4ab6be1cd09180976afb1a
    Signed-off-by: mtk15472 <Yu-Ren.Wang@mediatek.com>

---
+++ drivers/misc/mediatek/apusys/reviser/1.0/reviser_cmn.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2019 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __APUSYS_REVISER_COMMON_H__
+#define __APUSYS_REVISER_COMMON_H__
+
+#include <linux/printk.h>
+
+#define APUSYS_LOG_PREFIX "[reviser]"
+
+#define LOG_ERR(x, args...) \
+	pr_info(APUSYS_LOG_PREFIX "[error] %s " x, __func__, ##args)
+#define LOG_WARN(x, args...) \
+	pr_info(APUSYS_LOG_PREFIX "[warn] " x, ##args)
+#define LOG_INFO(x, args...) \
+	pr_info(APUSYS_LOG_PREFIX x, ##args)
+#define LOG_DEBUG(x, args...) \
+	pr_info(APUSYS_LOG_PREFIX "[debug] %s " x, __func__, ##args)
+
+#define DEBUG_TAG LOG_DEBUG("[%d]\n", __LINE__)
+
+#endif
