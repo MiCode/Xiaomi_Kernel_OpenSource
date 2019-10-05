8380f52ac1cc28b42be5a3047c7a43f212a45bc9
Miles Chen <miles.chen@mediatek.com>
2019-10-05 14:54:34 +0800

    [ALPS04789600] mm/page_owner: fix page owner count
    We count every found as page owner as 1, which does not
    show the correct useage. Add the hit count by (1 << order).
    
    MTK-Commit-Id: 71e600fde6abfecf02244c7ec1d3c9e6048e44a4
    
    Change-Id: Ib3f0ab8df2661985c16bd082ede2a4e1362afaad
    CR-Id: ALPS04789600
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    Feature: [Module]Kernel Memory Management

--- include/linux/stackdepot.h
+++ include/linux/stackdepot.h
@@ -28,7 +28,8 @@ struct stack_trace;
 depot_stack_handle_t depot_save_stack(struct stack_trace *trace, gfp_t flags);
 
 void depot_fetch_stack(depot_stack_handle_t handle, struct stack_trace *trace);
-void depot_hit_stack(depot_stack_handle_t handle, struct stack_trace *trace);
+void depot_hit_stack(depot_stack_handle_t handle, struct stack_trace *trace,
+		int cnt);
 void show_max_hit_page(void);
 
 #endif
