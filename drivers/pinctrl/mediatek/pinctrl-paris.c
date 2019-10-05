c687d67a0a06cff1966bce5b36a417a271d5946e
Andy Teng <andy.teng@mediatek.com>
2019-10-05 14:53:27 +0800

    [ALPS04750427] pinctrl: mediatek: add adv. drive for I2C
    For I2C pins, there are existing generic driving setup and the above
    specific driving setup. I2C pins can only support 2/4/6/8/10/12/14/16mA
    driving adjustment in generic driving setup. But in specific driving
    setup, they can support 0.125/0.25/0.5/1mA adjustment.
    If we enable specific driving setup for I2C pins,
    the existing generic driving setup will be disabled.
    For some special features, we need the I2C pins specific driving setup.
    The specific driving setup is controlled by E1E0EN or EH.
    So we need add extra vendor driving preperty instead of the generic
    driving property. We can add "mediatek,drive-strength-adv = <XXX>;"
    to describe the specific driving setup property.
    "XXX" means the value of E1E0EN or EH. So the valid arguments of
    "mediatek,drive-strength-adv" are from 0 to 7.
    
    MTK-Commit-Id: 68701128c8dcf5f3048d6eb194e935a81f5e6bb5
    
    Change-Id: Ia7554519871c95d5d2f9f0b02b15510dc84abc97
    Signed-off-by: Andy Teng <andy.teng@mediatek.com>
    CR-Id: ALPS04750427
    Feature: UFS Booting

--- drivers/pinctrl/mediatek/pinctrl-paris.c
+++ drivers/pinctrl/mediatek/pinctrl-paris.c
@@ -22,12 +22,14 @@
 #define MTK_PIN_CONFIG_RDSEL	(PIN_CONFIG_END + 2)
 #define MTK_PIN_CONFIG_PU_ADV	(PIN_CONFIG_END + 3)
 #define MTK_PIN_CONFIG_PD_ADV	(PIN_CONFIG_END + 4)
+#define MTK_PIN_CONFIG_DRV_ADV	(PIN_CONFIG_END + 5)
 
 static const struct pinconf_generic_params mtk_custom_bindings[] = {
 	{"mediatek,tdsel",	MTK_PIN_CONFIG_TDSEL,		0},
 	{"mediatek,rdsel",	MTK_PIN_CONFIG_RDSEL,		0},
 	{"mediatek,pull-up-adv", MTK_PIN_CONFIG_PU_ADV,		1},
 	{"mediatek,pull-down-adv", MTK_PIN_CONFIG_PD_ADV,	1},
+	{"mediatek,drive-strength-adv", MTK_PIN_CONFIG_DRV_ADV,	2},
 };
 
 #ifdef CONFIG_DEBUG_FS
@@ -36,6 +38,7 @@ static const struct pin_config_item mtk_conf_items[] = {
 	PCONFDUMP(MTK_PIN_CONFIG_RDSEL, "rdsel", NULL, true),
 	PCONFDUMP(MTK_PIN_CONFIG_PU_ADV, "pu-adv", NULL, true),
 	PCONFDUMP(MTK_PIN_CONFIG_PD_ADV, "pd-adv", NULL, true),
+	PCONFDUMP(MTK_PIN_CONFIG_DRV_ADV, "drive-strength-adv", NULL, true),
 };
 #endif
 
@@ -168,6 +171,15 @@ static int mtk_pinconf_get(struct pinctrl_dev *pctldev,
 			err = -ENOTSUPP;
 		}
 		break;
+	case MTK_PIN_CONFIG_DRV_ADV:
+		if (hw->soc->adv_drive_get) {
+			err = hw->soc->adv_drive_get(hw, desc, &ret);
+			if (err)
+				return err;
+		} else {
+			return -ENOTSUPP;
+		}
+		break;
 	default:
 		err = -ENOTSUPP;
 	}
@@ -271,6 +283,15 @@ static int mtk_pinconf_set(struct pinctrl_dev *pctldev, unsigned int pin,
 						    arg);
 		} else {
 			err = -ENOTSUPP;
+		}
+		break;
+	case MTK_PIN_CONFIG_DRV_ADV:
+		if (hw->soc->adv_drive_set) {
+			err = hw->soc->adv_drive_set(hw, desc, arg);
+			if (err)
+				return err;
+		} else {
+			return -ENOTSUPP;
 		}
 		break;
 	default:
