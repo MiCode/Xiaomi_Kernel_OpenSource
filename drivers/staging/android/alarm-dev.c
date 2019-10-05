926cba01b81c1dd1a150e9256a88a537c080fe4d
Wilma wu <wilma.wu@mediatek.com>
2019-10-05 14:52:36 +0800

    [ALPS04762246] RTC: fix alarm dev coverity
    fix using uninitialized variable issue.
    
    MTK-Commit-Id: 7e051e91b35b7f3ef6fdfdaaab18c5a4f57610cf
    
    Change-Id: I9663685d4efa3f9c26c923b5daf916cea9a35a82
    Signed-off-by: Wilma wu <wilma.wu@mediatek.com>
    CR-Id: ALPS04762246
    Feature: RTC-Power Drop Auto Reboot

--- drivers/staging/android/alarm-dev.c
+++ drivers/staging/android/alarm-dev.c
@@ -339,9 +339,8 @@ static long alarm_do_ioctl(struct file *file, unsigned int cmd,
 
 static long alarm_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
-	struct timespec ts;
-	struct rtc_wkalrm pwron_alm;
-
+	struct timespec ts = {0};
+	struct rtc_wkalrm pwron_alm = {0};
 	int rv;
 
 	switch (ANDROID_ALARM_BASE_CMD(cmd)) {
@@ -383,8 +382,8 @@ static long alarm_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 static long alarm_compat_ioctl(struct file *file,
 			       unsigned int cmd, unsigned long arg)
 {
-	struct timespec ts;
-	struct rtc_wkalrm pwron_alm;
+	struct timespec ts = {0};
+	struct rtc_wkalrm pwron_alm = {0};
 	int rv;
 
 	switch (ANDROID_ALARM_BASE_CMD(cmd)) {
