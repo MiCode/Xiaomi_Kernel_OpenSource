d9c338a1eea4e12efd485d7494a7b64440a6b60e
mtk11285 <weiwei.zhang@mediatek.com>
2019-10-05 14:51:49 +0800

    [ALPS04729015]  AEE: fix direct unwind KE
    out-of-bounds access issue, replace strstr to strnstr
    
    MTK-Commit-Id: c99adf2b3ecb9a9fe63161ef5f0475c8bd0ade83
    
    Change-Id: Id475605b4e1d66d036dc6797213d8fba3bc169b9
    Signed-off-by: mtk11285 <weiwei.zhang@mediatek.com>
    CR-Id: ALPS04729015
    Feature: Android Exception Engine(AEE)

--- drivers/misc/mediatek/aee/aed/aed-main.c
+++ drivers/misc/mediatek/aee/aed/aed-main.c
@@ -1384,7 +1384,7 @@ static void print_vma_name(unsigned char *Userthread_maps,
 		kaddr = (const char *)kmap(page);
 		len = min(max_len, PAGE_SIZE - page_offset);
 		write_len = strnlen(kaddr + page_offset, len);
-		if (strstr((kaddr + page_offset), "signal stack")) {
+		if (strnstr((kaddr + page_offset), "signal stack", write_len)) {
 			Maps2Buffer(Userthread_maps, Userthread_mapsLength,
 				"%s[anon:%s]\n", str, (kaddr + page_offset));
 		}
