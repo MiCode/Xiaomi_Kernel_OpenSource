791b9756f7d62932d5095b1673f510229282c259
Chih-Chieh Yeh <chih-chieh.yeh@mediatek.com>
2019-10-05 14:48:22 +0800

    [ALPS04478223] DISP: fix coding style and build error
    [Detail]
    Fix coding style and build error.
    
    MTK-Commit-Id: 39e3df4d36c2ee235593f00020729abc7ed26a90
    
    Change-Id: Ic807321070eebbe9faa698303cfc517a937fb11f
    Signed-off-by: Chih-Chieh Yeh <chih-chieh.yeh@mediatek.com>
    CR-Id: ALPS04478223
    Feature: [Module]Display Driver

--- drivers/misc/mediatek/lcm/nt35695B_fhd_dsi_cmd_auo_rt5081_720p/nt35695B_fhd_dsi_cmd_auo_rt5081_720p.c
+++ drivers/misc/mediatek/lcm/nt35695B_fhd_dsi_cmd_auo_rt5081_720p/nt35695B_fhd_dsi_cmd_auo_rt5081_720p.c
@@ -41,7 +41,7 @@
 #define LCM_ID_NT35695 (0xf5)
 
 static const unsigned int BL_MIN_LEVEL = 20;
-static LCM_UTIL_FUNCS lcm_util;
+static struct LCM_UTIL_FUNCS lcm_util;
 
 
 #define SET_RESET_PIN(v)	(lcm_util.set_reset_pin((v)))
@@ -99,7 +99,7 @@ static const unsigned char LCD_MODULE_ID = 0x01;
 #define REGFLAG_RESET_LOW	0xFFFE
 #define REGFLAG_RESET_HIGH	0xFFFF
 
-static LCM_DSI_MODE_SWITCH_CMD lcm_switch_mode_cmd;
+static struct LCM_DSI_MODE_SWITCH_CMD lcm_switch_mode_cmd;
 
 #ifndef TRUE
 #define TRUE 1
@@ -1274,14 +1274,14 @@ static void push_table(void *cmdq, struct LCM_setting_table *table,
 	}
 }
 
-static void lcm_set_util_funcs(const LCM_UTIL_FUNCS *util)
+static void lcm_set_util_funcs(const struct LCM_UTIL_FUNCS *util)
 {
-	memcpy(&lcm_util, util, sizeof(LCM_UTIL_FUNCS));
+	memcpy(&lcm_util, util, sizeof(struct LCM_UTIL_FUNCS));
 }
 
-static void lcm_get_params(LCM_PARAMS *params)
+static void lcm_get_params(struct LCM_PARAMS *params)
 {
-	memset(params, 0, sizeof(LCM_PARAMS));
+	memset(params, 0, sizeof(struct LCM_PARAMS));
 
 	params->type = LCM_TYPE_DSI;
 
@@ -1648,11 +1648,11 @@ static void lcm_validate_roi(int *x, int *y, int *width, int *height)
 #endif
 
 #if (LCM_DSI_CMD_MODE)
-LCM_DRIVER nt35695B_fhd_dsi_cmd_auo_rt5081_720p_lcm_drv = {
+struct LCM_DRIVER nt35695B_fhd_dsi_cmd_auo_rt5081_720p_lcm_drv = {
 	.name = "nt35695B_fhd_dsi_cmd_auo_rt5081_720p_lcm_drv",
 #else
 
-LCM_DRIVER nt35695B_fhd_dsi_vdo_auo_rt5081_720p_lcm_drv = {
+struct LCM_DRIVER nt35695B_fhd_dsi_vdo_auo_rt5081_720p_lcm_drv = {
 	.name = "nt35695B_fhd_dsi_vdo_auo_rt5081_720p_lcm_drv",
 #endif
 	.set_util_funcs = lcm_set_util_funcs,
--- drivers/misc/mediatek/lcm/nt35695B_fhd_dsi_vdo_auo_rt5081_hdp/nt35695B_fhd_dsi_vdo_auo_rt5081_hdp.c
+++ drivers/misc/mediatek/lcm/nt35695B_fhd_dsi_vdo_auo_rt5081_hdp/nt35695B_fhd_dsi_vdo_auo_rt5081_hdp.c
@@ -28,8 +28,6 @@
 #include <string.h>
 #elif defined(BUILD_UBOOT)
 #include <asm/arch/mt_gpio.h>
-#else
-#include <linux/regulator/mediatek/mtk_regulator.h>
 #endif
 
 #ifdef BUILD_LK
@@ -43,7 +41,7 @@
 #define LCM_ID_NT35695 (0xf5)
 
 static const unsigned int BL_MIN_LEVEL = 20;
-static LCM_UTIL_FUNCS lcm_util;
+static struct LCM_UTIL_FUNCS lcm_util;
 
 
 #define SET_RESET_PIN(v)	(lcm_util.set_reset_pin((v)))
@@ -100,7 +98,7 @@ static const unsigned char LCD_MODULE_ID = 0x01;
 #define REGFLAG_RESET_LOW	0xFFFE
 #define REGFLAG_RESET_HIGH	0xFFFF
 
-static LCM_DSI_MODE_SWITCH_CMD lcm_switch_mode_cmd;
+static struct LCM_DSI_MODE_SWITCH_CMD lcm_switch_mode_cmd;
 
 #ifndef TRUE
 #define TRUE 1
@@ -1272,14 +1270,14 @@ static void push_table(void *cmdq, struct LCM_setting_table *table,
 	}
 }
 
-static void lcm_set_util_funcs(const LCM_UTIL_FUNCS *util)
+static void lcm_set_util_funcs(const struct LCM_UTIL_FUNCS *util)
 {
-	memcpy(&lcm_util, util, sizeof(LCM_UTIL_FUNCS));
+	memcpy(&lcm_util, util, sizeof(struct LCM_UTIL_FUNCS));
 }
 
-static void lcm_get_params(LCM_PARAMS *params)
+static void lcm_get_params(struct LCM_PARAMS *params)
 {
-	memset(params, 0, sizeof(LCM_PARAMS));
+	memset(params, 0, sizeof(struct LCM_PARAMS));
 
 	params->type = LCM_TYPE_DSI;
 
@@ -1664,11 +1662,11 @@ static void lcm_validate_roi(int *x, int *y, int *width, int *height)
 #endif
 
 #if (LCM_DSI_CMD_MODE)
-LCM_DRIVER nt35695B_fhd_dsi_cmd_auo_rt5081_hdp_lcm_drv = {
+struct LCM_DRIVER nt35695B_fhd_dsi_cmd_auo_rt5081_hdp_lcm_drv = {
 	.name = "nt35695B_fhd_dsi_cmd_auo_rt5081_hdp_drv",
 #else
 
-LCM_DRIVER nt35695B_fhd_dsi_vdo_auo_rt5081_hdp_lcm_drv = {
+struct LCM_DRIVER nt35695B_fhd_dsi_vdo_auo_rt5081_hdp_lcm_drv = {
 	.name = "nt35695B_fhd_dsi_vdo_auo_rt5081_hdp_drv",
 #endif
 	.set_util_funcs = lcm_set_util_funcs,
