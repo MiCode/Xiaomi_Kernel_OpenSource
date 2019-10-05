60cdefdd3e1a6bd5ff9c58241d42bcc7b29937d0
mtk03113 <nicholas.tang@mediatek.com>
2019-10-05 14:30:13 +0800

    [ALPS04307415] mrdump: Fix arm crash_setup_regs undefined error
    arm defined crash_setup_regs under CONFIG_KEXEC, workaround
    by clone it for now.
    
    mrdump_panic.c:141:3: error: implicit declaration of function
    crash_setup_regs [-Werror=implicit-function-declaration]
    
    MTK-Commit-Id: 83e20d34df11ac76493cd56a6240c1f60021c24d
    
    Change-Id: If470221b149096acab8cea0c44930ac99cbaf6c4
    CR-Id: ALPS04307415
    Feature: Memory RAM Dump (MRDUMP)
    Signed-off-by: mtk03113 <nicholas.tang@mediatek.com>

--- drivers/misc/mediatek/aee/mrdump/mrdump_private.h
+++ drivers/misc/mediatek/aee/mrdump/mrdump_private.h
@@ -88,4 +88,29 @@ static inline void show_kaslr(void)
 
 int in_fiq_handler(void);
 
+#if defined(__arm__)
+static inline void crash_setup_regs(struct pt_regs *newregs,
+				    struct pt_regs *oldregs)
+{
+	if (oldregs) {
+		memcpy(newregs, oldregs, sizeof(*newregs));
+	} else {
+		__asm__ __volatile__ (
+			"stmia	%[regs_base], {r0-r12}\n\t"
+			"mov	%[_ARM_sp], sp\n\t"
+			"str	lr, %[_ARM_lr]\n\t"
+			"adr	%[_ARM_pc], 1f\n\t"
+			"mrs	%[_ARM_cpsr], cpsr\n\t"
+		"1:"
+			: [_ARM_pc] "=r" (newregs->ARM_pc),
+			  [_ARM_cpsr] "=r" (newregs->ARM_cpsr),
+			  [_ARM_sp] "=r" (newregs->ARM_sp),
+			  [_ARM_lr] "=o" (newregs->ARM_lr)
+			: [regs_base] "r" (&newregs->ARM_r0)
+			: "memory"
+		);
+	}
+}
+#endif
+
 #endif /* __MRDUMP_PRIVATE_H__ */
