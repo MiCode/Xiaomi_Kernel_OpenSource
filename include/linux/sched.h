99b0aa582fafc5be2fe3b8500a7887741e473273
Kobe Wu <kobe-cp.wu@mediatek.com>
2019-10-05 12:52:51 +0800

    [ALPS04082523] lockdep: implement debugging and self-test function
    1. add try lock function for spinlock and monitor spin time
    2. add aee function to lockdep and spin_bug warnings
    3. enhance /proc/lockdep debugging information
    4. record call trace of held locks for debugging
    5. implement function to catch a new lock dependency
    6. implement self-test commands to verify lockdep function
    
    MTK-Commit-Id: b22752cc0c156db6adb1e633201c17ef3573da11
    
    Change-Id: I85ca99808eba3bda894c35c0ccd8c15d3e2e9f3f
    CR-Id: ALPS04082523
    Feature: [Module]Lockdep
    Signed-off-by: Kobe Wu <kobe-cp.wu@mediatek.com>
    (cherry-pick from ab0d43fe483b8679a469a8a86e15f950308ccb55)

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -971,7 +971,7 @@ struct task_struct {
 #endif
 
 #ifdef CONFIG_LOCKDEP
-# define MAX_LOCK_DEPTH			48UL
+# define MAX_LOCK_DEPTH			32UL
 	u64				curr_chain_key;
 	int				lockdep_depth;
 	unsigned int			lockdep_recursion;
---
+++ kernel/locking/lockdep_test.c
@@ -0,0 +1,221 @@
+/*
+ * Copyright (C) 2018 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
+#include <linux/spinlock.h>
+#include <linux/slab.h>
+#include <linux/delay.h>
+#include <linux/kthread.h>
+
+static spinlock_t lockA;
+static spinlock_t lockB;
+static spinlock_t lockC;
+static spinlock_t lockD;
+static struct timer_list lockdep_timer;
+
+struct lockdep_test_rcu {
+	int val;
+};
+static struct lockdep_test_rcu __rcu *lockdep_test_rcu_data;
+
+void lockdep_test_recursive_lock(void)
+{
+	/* recursive deadlock */
+	spin_lock(&lockA);
+	spin_lock(&lockA);
+	spin_unlock(&lockA);
+	spin_unlock(&lockA);
+}
+
+void lockdep_test_suspicious_rcu(void)
+{
+	struct lockdep_test_rcu *rcu_p;
+
+	rcu_p = kmalloc(sizeof(struct lockdep_test_rcu), GFP_KERNEL);
+	if (rcu_p == NULL)
+		return;
+	rcu_assign_pointer(lockdep_test_rcu_data, rcu_p);
+	synchronize_rcu();
+
+	/* rcu_read_lock() should be here */
+	rcu_p = rcu_dereference(lockdep_test_rcu_data);
+	/* rcu_read_unlock() should be here */
+}
+
+void lockdep_test_circular_deadlock(void)
+{
+	/* lockA -> lockB */
+	spin_lock(&lockA);
+	spin_lock(&lockB);
+	spin_unlock(&lockB);
+	spin_unlock(&lockA);
+
+	/* lockB -> lockA */
+	spin_lock(&lockB);
+	spin_lock(&lockA);
+	spin_unlock(&lockA);
+	spin_unlock(&lockB);
+}
+
+static void lockdep_test_timer(unsigned long arg)
+{
+	spin_lock(&lockA);
+	spin_unlock(&lockA);
+}
+
+void lockdep_test_inconsistent_lock_a(void)
+{
+	/* {SOFTIRQ-ON-W} */
+	spin_lock(&lockA);
+	spin_unlock(&lockA);
+
+	/* {IN-SOFTIRQ-W} */
+	setup_timer(&lockdep_timer, lockdep_test_timer, 0);
+	mod_timer(&lockdep_timer, jiffies + msecs_to_jiffies(20));
+}
+
+void lockdep_test_inconsistent_lock_b(void)
+{
+	/* {IN-SOFTIRQ-W} */
+	setup_timer(&lockdep_timer, lockdep_test_timer, 0);
+	mod_timer(&lockdep_timer, jiffies + msecs_to_jiffies(20));
+	mdelay(100);
+
+	/* {SOFTIRQ-ON-W} */
+	spin_lock(&lockA);
+	spin_unlock(&lockA);
+}
+
+void lockdep_test_irq_lock_inversion(void)
+{
+	unsigned long flags;
+
+	/* the order is the problem */
+	spin_lock_irqsave(&lockA, flags);
+	spin_lock(&lockB);
+	spin_unlock_irqrestore(&lockA, flags);
+	spin_unlock(&lockB);
+
+	setup_timer(&lockdep_timer, lockdep_test_timer, 0);
+	mod_timer(&lockdep_timer, jiffies + msecs_to_jiffies(20));
+}
+
+void lockdep_test_bad_magic(void)
+{
+	/* without spin_lock_init */
+	spin_lock(&lockD);
+	spin_unlock(&lockD);
+}
+
+void lockdep_test_bad_unlock_balance(void)
+{
+	spin_lock(&lockA);
+	spin_unlock(&lockA);
+	spin_unlock(&lockA);
+}
+
+void lockdep_test_wrong_owner_cpu(void)
+{
+	spin_lock(&lockA);
+	lockA.rlock.owner_cpu = -1;
+	spin_unlock(&lockA);
+}
+
+void lockdep_test_held_lock_freed(void)
+{
+	spinlock_t *lockE;
+
+	lockE = kmalloc(sizeof(spinlock_t), GFP_KERNEL);
+	spin_lock_init(lockE);
+	spin_lock(lockE);
+	kfree(lockE);
+	/* should do spin_unlock before free memory */
+}
+
+static int lockdep_test_thread(void *data)
+{
+	spin_lock(&lockA);
+	mdelay(5000);
+	spin_unlock(&lockA);
+	return 0;
+}
+
+void lockdep_test_spin_time(void)
+{
+	kthread_run(lockdep_test_thread, NULL, "lockdep_test");
+	mdelay(500);
+	spin_lock(&lockA);
+	spin_unlock(&lockA);
+}
+
+struct lockdep_test_func {
+	char name[32];
+	void (*func)(void);
+};
+
+struct lockdep_test_func lockdep_test_list[] = {
+	/* KernelAPI Dump */
+	{"circular_deadlock", lockdep_test_circular_deadlock},
+	{"suspicious_rcu", lockdep_test_suspicious_rcu},
+	{"inconsistent_lock_a", lockdep_test_inconsistent_lock_a},
+	{"inconsistent_lock_b", lockdep_test_inconsistent_lock_b},
+	{"irq_lock_inversion", lockdep_test_irq_lock_inversion},
+	{"bad_unlock_balance", lockdep_test_bad_unlock_balance},
+	{"spin_time", lockdep_test_spin_time},
+	/* KE */
+	{"bad_magic", lockdep_test_bad_magic},
+	{"wrong_owner_cpu", lockdep_test_wrong_owner_cpu},
+	{"held_lock_freed", lockdep_test_held_lock_freed},
+	/* HWT */
+	{"recursive_lock", lockdep_test_recursive_lock},
+};
+
+static ssize_t lockdep_test_write(struct file *file,
+	const char *ubuf, size_t count, loff_t *ppos)
+{
+	int i;
+	char buf[32];
+
+	if (count >= sizeof(buf) || count == 0)
+		return -EINVAL;
+
+	if (copy_from_user(&buf, ubuf, count))
+		return -EFAULT;
+
+	buf[count] = '\0';
+
+	for (i = 0; i < ARRAY_SIZE(lockdep_test_list); i++) {
+		if (strlen(lockdep_test_list[i].name) != count - 1)
+			continue;
+		if (!strncmp(buf, lockdep_test_list[i].name, count - 1))
+			lockdep_test_list[i].func();
+	}
+
+	return count;
+}
+
+static const struct file_operations proc_lockdep_test_fops = {
+	.open  = simple_open,
+	.write = lockdep_test_write,
+};
+
+void lockdep_test_init(void)
+{
+	spin_lock_init(&lockA);
+	spin_lock_init(&lockB);
+	spin_lock_init(&lockC);
+
+	proc_create("lockdep_test", 0220, NULL, &proc_lockdep_test_fops);
+}
+
