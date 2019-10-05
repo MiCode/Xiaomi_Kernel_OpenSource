1b9b04d8d2f85a1b427a719e48cf0964bc7fe9c7
James Hsu <james.hsu@mediatek.com>
2019-10-05 14:48:25 +0800

    [ALPS04532180] zmc: enlarge __GFP_BITS_SHIFT to accommodate __GFP_CMA
    Enlarge __GFP_BITS_SHIFT and update ___GFP_NOLOCKDEP to accommodate
    __GFP_CMA.
    
    MTK-Commit-Id: be774ff650be95d36622999e80cb5e452eb89c61
    
    Change-Id: I0e71a6b380ffbca45a4f9d7133546cff2a07d964
    Signed-off-by: James Hsu <james.hsu@mediatek.com>
    CR-Id: ALPS04532180
    Feature: [Module]Kernel Memory Management

--- include/linux/gfp.h
+++ include/linux/gfp.h
@@ -40,12 +40,12 @@ struct vm_area_struct;
 #define ___GFP_DIRECT_RECLAIM	0x400000u
 #define ___GFP_WRITE		0x800000u
 #define ___GFP_KSWAPD_RECLAIM	0x1000000u
+#define ___GFP_CMA		0x2000000u
 #ifdef CONFIG_LOCKDEP
-#define ___GFP_NOLOCKDEP	0x2000000u
+#define ___GFP_NOLOCKDEP	0x4000000u
 #else
 #define ___GFP_NOLOCKDEP	0
 #endif
-#define ___GFP_CMA		0x4000000u
 /* If the above are modified, __GFP_BITS_SHIFT may need updating */
 
 /*
@@ -221,7 +221,7 @@ struct vm_area_struct;
 #define __GFP_CMA ((__force gfp_t)___GFP_CMA)
 
 /* Room for N __GFP_FOO bits */
-#define __GFP_BITS_SHIFT (25 + IS_ENABLED(CONFIG_LOCKDEP))
+#define __GFP_BITS_SHIFT (26 + IS_ENABLED(CONFIG_LOCKDEP))
 #define __GFP_BITS_MASK ((__force gfp_t)((1 << __GFP_BITS_SHIFT) - 1))
 
 /*
