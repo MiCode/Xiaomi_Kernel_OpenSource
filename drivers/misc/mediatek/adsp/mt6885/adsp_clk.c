a4237ac5b5b3d1468c6ba4e96057028e9b51823c
Celine Liu <celine.liu@mediatek.com>
2019-10-05 14:51:52 +0800

    [ALPS04739523] adsp: mt6885 clock porting
    [Detail]
    1. add mtcmos ctrl in clock driver
    2. update clock definition according to platform clock table.
    
    MTK-Commit-Id: 9a97fd0266452e919a7147431d3836c61896fa50
    
    Change-Id: I41eadb7b7ac8648d5e66ce615183115c9a1a875e
    Signed-off-by: Celine Liu <celine.liu@mediatek.com>
    CR-Id: ALPS04739523
    Feature: Phone Sound

--- drivers/misc/mediatek/adsp/mt6885/adsp_clk.c
+++ drivers/misc/mediatek/adsp/mt6885/adsp_clk.c
@@ -6,67 +6,73 @@
 #include <linux/clk.h>
 #include "adsp_clk.h"
 
+
+#ifdef BRINGUP_WR
+#include <linux/io.h>
+
+#define CLK_ADSP_INFRA  CLK_TOP_ADSP_SEL
+static uint32_t adsp_clock_count;
+DEFINE_SPINLOCK(adsp_clock_spinlock);
+
+#define DRV_SetReg32(addr, val)   writel(readl(addr) | (val), addr)
+#define DRV_ClrReg32(addr, val)   writel(readl(addr) & ~(val), addr)
+#endif
+
 struct adsp_clock_attr {
 	const char *name;
-	bool clk_prepare;
-	bool clk_status;
 	struct clk *clock;
 };
 
-#ifndef ADSP_EARLY_PORTING_BYPASS
 static struct adsp_clock_attr adsp_clks[ADSP_CLK_NUM] = {
-	[CLK_ADSP_INFRA] = {"clk_adsp_infra", false, false, NULL},
-	[CLK_TOP_ADSP_SEL] = {"clk_top_adsp_sel", false, false, NULL},
-	[CLK_ADSP_CLK26M] = {"clk_adsp_clk26m", false, false, NULL},
-	[CLK_TOP_MMPLL_D4] = {"clk_top_mmpll_d4", false, false, NULL},
-	[CLK_TOP_ADSPPLL_D4] = {"clk_top_adsppll_d4", false, false, NULL},
-	[CLK_TOP_ADSPPLL_D6] = {"clk_top_adsppll_d6", false, false, NULL},
-};
-static uint32_t adsp_clock_count;
+	[CLK_SCP_SYS_ADSP] = {"scp_sys_adsp", NULL},
+#ifndef BRINGUP_WR
+	[CLK_ADSP_INFRA] = {"clk_adsp_infra", NULL},
 #endif
+	[CLK_TOP_ADSP_SEL] = {"clk_top_adsp_sel", NULL},
+	[CLK_TOP_CLK26M] = {"clk_top_clk26m", NULL},
+	[CLK_TOP_ADSPPLL] = {"clk_top_adsppll", NULL},
+};
 
-DEFINE_SPINLOCK(adsp_clock_spinlock);
+#ifdef BRINGUP_WR
+static void switch_adsp_clk_cg(bool on)
+{
+	void *cg_reg = (void *)AUDIODSP_CK_CG;
+	unsigned long spin_flags;
+
+	spin_lock_irqsave(&adsp_clock_spinlock, spin_flags);
+	if (on) {
+		if (++adsp_clock_count == 1)
+			DRV_SetReg32(cg_reg, 1);
+	} else {
+		if (--adsp_clock_count == 0)
+			DRV_ClrReg32(cg_reg, 1);
+	}
+	spin_lock_irqsave(&adsp_clock_spinlock, spin_flags);
+}
+#endif
 
 int adsp_set_top_mux(enum adsp_clk clk)
 {
 	int ret = 0;
-#ifndef ADSP_EARLY_PORTING_BYPASS
-	struct clk *parent;
 
-	pr_debug("%s(%x)\n", __func__, clk);
+	pr_info("%s(%x)\n", __func__, clk);
 
-	switch (clk) {
-	case CLK_ADSP_CLK26M:
-		parent = adsp_clks[CLK_ADSP_CLK26M].clock;
-		break;
-	case CLK_TOP_MMPLL_D4:
-		parent = adsp_clks[CLK_TOP_MMPLL_D4].clock;
-		break;
-	case CLK_TOP_ADSPPLL_D4:
-		parent = adsp_clks[CLK_TOP_ADSPPLL_D4].clock;
-		break;
-	case CLK_TOP_ADSPPLL_D6:
-		parent = adsp_clks[CLK_TOP_ADSPPLL_D6].clock;
-		break;
-	default:
-		parent = adsp_clks[CLK_ADSP_CLK26M].clock;
-		break;
-	}
-	ret = clk_set_parent(adsp_clks[CLK_TOP_ADSP_SEL].clock, parent);
+	if (clk >= ADSP_CLK_NUM)
+		return -EINVAL;
+
+	ret = clk_set_parent(adsp_clks[CLK_TOP_ADSP_SEL].clock,
+		      adsp_clks[clk].clock);
 	if (IS_ERR(&ret))
 		pr_err("[ADSP] %s clk_set_parent(clk_top_adsp_sel-%x) fail %d\n",
 		      __func__, clk, ret);
-#endif
 	return ret;
 }
 
 void adsp_set_clock_freq(enum adsp_clk clk)
 {
 	switch (clk) {
-	case CLK_ADSP_CLK26M:
-	case CLK_TOP_MMPLL_D4:
-	case CLK_TOP_ADSPPLL_D4:
-	case CLK_TOP_ADSPPLL_D6:
+	case CLK_TOP_CLK26M:
+	case CLK_TOP_ADSPPLL:
 		adsp_set_top_mux(clk);
 		break;
 	default:
@@ -78,7 +84,6 @@ void adsp_set_clock_freq(enum adsp_clk clk)
 int adsp_clk_device_probe(void *dev)
 {
 	int ret = 0;
-#ifndef ADSP_EARLY_PORTING_BYPASS
 	size_t i;
 
 	for (i = 0; i < ARRAY_SIZE(adsp_clks); i++) {
@@ -87,83 +92,63 @@ int adsp_clk_device_probe(void *dev)
 			ret = PTR_ERR(adsp_clks[i].clock);
 			pr_err("%s devm_clk_get %s fail %d\n", __func__,
 				   adsp_clks[i].name, ret);
-		} else {
-			adsp_clks[i].clk_status = true;
 		}
 	}
 
-	if (ret)
-		return ret;
-
-	for (i = 0; i < ARRAY_SIZE(adsp_clks); i++) {
-		if (adsp_clks[i].clk_status) {
-			ret = clk_prepare(adsp_clks[i].clock);
-			if (ret) {
-				pr_err("%s clk_prepare %s fail %d\n", __func__,
-					   adsp_clks[i].name, ret);
-			} else {
-				adsp_clks[i].clk_prepare = true;
-			}
-		}
-	}
-#endif
 	return ret;
 }
 
 /* clock deinit */
 void adsp_clk_device_remove(void *dev)
 {
-#ifndef ADSP_EARLY_PORTING_BYPASS
-	size_t i;
-
-	pr_debug("%s\n", __func__);
-	for (i = 0; i < ARRAY_SIZE(adsp_clks); i++) {
-		if (adsp_clks[i].clock && !IS_ERR(adsp_clks[i].clock) &&
-			adsp_clks[i].clk_prepare) {
-			clk_unprepare(adsp_clks[i].clock);
-			adsp_clks[i].clk_prepare = false;
-		}
-	}
-#endif
+	pr_info("%s\n", __func__);
 }
 
 int adsp_enable_clock(void)
 {
 	int ret = 0;
-#ifndef ADSP_EARLY_PORTING_BYPASS
-	unsigned long spin_flags;
 
-	if (adsp_clks[CLK_ADSP_INFRA].clk_prepare) {
-		spin_lock_irqsave(&adsp_clock_spinlock, spin_flags);
-		if (++adsp_clock_count == 1)
-			/* unable to access adsp sram before set way_en to 1 */
-			adsp_way_en_ctrl(1);
-		ret = clk_enable(adsp_clks[CLK_ADSP_INFRA].clock);
-		if (IS_ERR(&ret))
-			pr_err("%s(), clk_enable %s fail, ret %d\n", __func__,
-				adsp_clks[CLK_ADSP_INFRA].name, ret);
-		spin_unlock_irqrestore(&adsp_clock_spinlock, spin_flags);
-	} else {
-		pr_err("%s(), clk %s, not prepared\n",
-			__func__, adsp_clks[CLK_ADSP_INFRA].name);
-		ret = -EINVAL;
+	pr_info("%s()\n", __func__);
+
+	/* ToDo: power on protect disable,
+	 * use counter inside to ensure only do it once
+	 */
+	//adsp_way_en_ctrl(1);
+
+	ret = clk_prepare_enable(adsp_clks[CLK_SCP_SYS_ADSP].clock);
+	if (IS_ERR(&ret)) {
+		pr_err("%s(), clk_prepare_enable %s fail, ret %d\n",
+			__func__, adsp_clks[CLK_SCP_SYS_ADSP].name, ret);
+		return -EINVAL;
 	}
+
+	ret = clk_prepare_enable(adsp_clks[CLK_ADSP_INFRA].clock);
+	if (IS_ERR(&ret)) {
+		pr_err("%s(), clk_prepare_enable %s fail, ret %d\n",
+			__func__, adsp_clks[CLK_ADSP_INFRA].name, ret);
+		clk_disable_unprepare(adsp_clks[CLK_SCP_SYS_ADSP].clock);
+		return -EINVAL;
+
+	}
+#ifdef BRINGUP_WR
+	switch_adsp_clk_cg(1);
 #endif
-	return ret;
+
+	return 0;
 }
 
 void adsp_disable_clock(void)
 {
-#ifndef ADSP_EARLY_PORTING_BYPASS
-	unsigned long spin_flags;
+	pr_info("%s()\n", __func__);
 
-	if (adsp_clks[CLK_ADSP_INFRA].clk_prepare) {
-		spin_lock_irqsave(&adsp_clock_spinlock, spin_flags);
-		if (--adsp_clock_count == 0)
-			/* unable to access adsp sram before set way_en to 1 */
-			adsp_way_en_ctrl(0);
-		clk_disable(adsp_clks[CLK_ADSP_INFRA].clock);
-		spin_unlock_irqrestore(&adsp_clock_spinlock, spin_flags);
-	}
+#ifdef BRINGUP_WR
+	switch_adsp_clk_cg(0);
 #endif
+	clk_disable(adsp_clks[CLK_ADSP_INFRA].clock);
+	clk_disable_unprepare(adsp_clks[CLK_SCP_SYS_ADSP].clock);
+
+	/* ToDo: power on protect disable,
+	 * use counter inside to ensure only do it once
+	 */
+	//adsp_way_en_ctrl(0);
 }
