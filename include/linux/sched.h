cab7585d8df04fc35f24b2e65df7cb0c884dd842
Yun Hsiang <yun.hsiang@mediatek.com>
2019-10-05 14:27:56 +0800

    [ALPS04128327] sched/core: uclamp: map TASKs clamp values into CPUs  clamp groups
    [Detail]
    Backport from linux power commit 463e02d2a755("sched/core: uclamp:
    map TASKs clamp values into CPUs clamp groups")
    
    Utilization clamping requires each CPU to know which clamp values are
    assigned to tasks that are currently RUNNABLE on that CPU.
    Multiple tasks can be assigned the same clamp value and tasks with
    different clamp values can be concurrently active on the same CPU.
    Thus, a proper data structure is required to support a fast and
    efficient aggregation of the clamp values required by the currently
    RUNNABLE tasks.
    
    For this purpose we use a per-CPU array of reference counters,
    where each slot is used to account how many tasks require a certain
    clamp value are currently RUNNABLE on each CPU.
    Each clamp value corresponds to a "clamp index" which identifies the
    position within the array of reference couters.
    
                                     :
           (user-space changes)      :      (kernel space / scheduler)
                                     :
                 SLOW PATH           :             FAST PATH
                                     :
        task_struct::uclamp::value   :     sched/core::enqueue/dequeue
                                     :         cpufreq_schedutil
                                     :
      +----------------+    +--------------------+     +-------------------+
      |      TASK      |    |     CLAMP GROUP    |     |    CPU CLAMPS     |
      +----------------+    +--------------------+     +-------------------+
      |                |    |   clamp_{min,max}  |     |  clamp_{min,max}  |
      | util_{min,max} |    |      se_count      |     |    tasks count    |
      +----------------+    +--------------------+     +-------------------+
                                     :
               +------------------>  :  +------------------->
        group_id = map(clamp_value)  :  ref_count(group_id)
                                     :
                                     :
    
    Lets introduce the support to map tasks to "clamp groups".
    Specifically we introduce the required functions to translate a
    "clamp value" into a clamps "group index" (group_id).
    
    Only a limited number of (different) clamp values are supported since:
    1. there are usually only few classes of workloads for which it makes
       sense to boost/limit to different frequencies,
       e.g. background vs foreground, interactive vs low-priority
    2. it allows a simpler and more memory/time efficient tracking of
       the per-CPU clamp values in the fast path.
    
    The number of possible different clamp values is currently defined at
    compile time. Thus, setting a new clamp value for a task can result into
    a -ENOSPC error in case this will exceed the number of maximum different
    clamp values supported.
    
    MTK-Commit-Id: 4cc67ff1face7e1969d09e2eb630d8554135d2fa
    
    Change-Id: I73f64c58dfa5d9df6bdf26562b96d048d52783aa
    Signed-off-by: Yun Hsiang <yun.hsiang@mediatek.com>
    CR-Id: ALPS04128327
    Feature: Energy Aware Scheduling Plus (EAS+)
    (cherry-pick from dc60dde3c295fdb98a811dbb39c8266deaa351b8)

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -290,6 +290,9 @@ struct vtime {
 	u64			gtime;
 };
 
+/* Clamp not valid, i.e. group not assigned or invalid value */
+#define UCLAMP_NOT_VALID -1
+
 enum uclamp_id {
 	UCLAMP_MIN = 0, /* Minimum utilization */
 	UCLAMP_MAX,     /* Maximum utilization */
@@ -621,6 +624,19 @@ struct sched_dl_entity {
 	struct hrtimer inactive_timer;
 };
 
+/**
+ * Utilization's clamp group
+ *
+ * A utilization clamp group maps a "clamp value" (value), i.e.
+ * util_{min,max}, to a "clamp group index" (group_id).
+ */
+struct uclamp_se {
+	/* Utilization constraint for tasks in this group */
+	unsigned int value;
+	/* Utilization clamp group for this constraint */
+	unsigned int group_id;
+};
+
 union rcu_special {
 	struct {
 		u8			blocked;
@@ -707,7 +723,7 @@ struct task_struct {
 
 #ifdef CONFIG_UCLAMP_TASK
 	/* Utlization clamp values for this task */
-	int				uclamp[UCLAMP_CNT];
+	struct uclamp_se		uclamp[UCLAMP_CNT];
 #endif
 
 #ifdef CONFIG_PREEMPT_NOTIFIERS
