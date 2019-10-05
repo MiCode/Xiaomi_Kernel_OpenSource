75c3aa06f94672a28f1d743e53a59b2af17bd824
Ethan Chang <ethan.chang@mediatek.com>
2019-10-05 14:48:30 +0800

    [ALPS04366756] Thermal: Fix Get_Camera_Temperature weak function
    When we arent able to link to the correct Get_Camera_Temperature
    function, we will link to the weak one instead to avoid a Build Error.
    
    The original weak function didnt modify output parameters of valid and
    temp and left them with their original values.
    Unfortunately, they didnt be initialized from the caller also and ended
    up having uncertainty values.
    
    In the problem ALPS04527414, the weak function reports a 787 degrees
    Celsius, and thermal governor triggered a KE as Image sensor HW
    protection
    	(gdb) p tz->temperature
    	$15 = 787000
    
    MTK-Commit-Id: 3c1b452c984752d6d8783e64f7dc54fd2869b4a3
    
    Change-Id: I2a1be2365b36eddbdfd878c81dcb449f46984f54
    CR-Id: ALPS04366756
    Signed-off-by: Ethan Chang <ethan.chang@mediatek.com>
    Feature: Power Management
    (cherry picked from commit 430ac94e78320c3104dd029c42bc5373177a713e)

--- drivers/misc/mediatek/thermal/common/thermal_zones/mtk_ts_imgsensor.c
+++ drivers/misc/mediatek/thermal/common/thermal_zones/mtk_ts_imgsensor.c
@@ -41,8 +41,11 @@ MUINT32 __attribute__ ((weak))
 Get_Camera_Temperature(
 enum CAMERA_DUAL_CAMERA_SENSOR_ENUM senDevId, MUINT8 *valid, MUINT32 *temp)
 {
+	*valid = SENSOR_TEMPERATURE_CANNOT_SEARCH_SENSOR;
+	*temp = -127;
 	pr_notice("[Thermal/TZ/IMGS] E_WF: %s doesn't exist\n", __func__);
-	return 0;
+
+	return -1;
 }
 
 /*=============================================================
