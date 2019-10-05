1be3ae2def6f28336669beb2682cbe4f98130db7
Jeremy Chou <jeremy.chou@mediatek.com>
2019-10-05 14:54:24 +0800

    [ALPS04690782] USB: Enable USB Boost for Storage
    [Detail]
    Enable USB Boost for Storage to enhance the performance.
    
    MTK-Commit-Id: 4ab17ee630c84c41988a06a5f9121cc8626ffa34
    
    Change-Id: I197d9bd3ffa4a940d97ff7780d10cdab9c78bb48
    Signed-off-by: Jeremy Chou <jeremy.chou@mediatek.com>
    CR-Id: ALPS04690782
    Feature: USB 3.0 Device

--- drivers/usb/core/devio.c
+++ drivers/usb/core/devio.c
@@ -57,6 +57,8 @@
 
 #include "usb.h"
 
+#include "usb_boost.h"
+
 #define USB_MAXBUS			64
 #define USB_DEVICE_MAX			(USB_MAXBUS * 128)
 #define USB_SG_SIZE			16384 /* split-size for large txs */
@@ -1199,6 +1201,9 @@ static int proc_bulk(struct usb_dev_state *ps, void __user *arg)
 		goto done;
 	}
 	tmo = bulk.timeout;
+
+	usb_boost();
+
 	if (bulk.ep & 0x80) {
 		if (len1 && !access_ok(VERIFY_WRITE, bulk.data, len1)) {
 			ret = -EINVAL;
