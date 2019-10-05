6e2907b71f66173c8cfd483a7ea799d833810311
Miles Chen <miles.chen@mediatek.com>
2019-10-05 14:49:32 +0800

    [ALPS04673036] kernel: speed up kernel build with ccache
    This change introduces ccache to speed up kernel build
    
    MTK-Commit-Id: af0cdc352bb25ee9b8a85106cfefd16ac4d9afec
    
    Change-Id: I0698f848dcb7e3c30cc3e1509de55a0031b72ac2
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    CR-Id: ALPS04673036
    Feature: [Module]Kernel Maintenance

--- kenv.mk
+++ kenv.mk
@@ -49,11 +49,27 @@ ifneq ($(strip $(TARGET_NO_KERNEL)),true)
   ifeq ($(KERNEL_TARGET_ARCH),arm64)
     TARGET_KERNEL_CROSS_COMPILE ?= $(KERNEL_ROOT_DIR)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
     ifeq ($(strip $(TARGET_KERNEL_USE_CLANG)),true)
-      TARGET_KERNEL_CLANG_COMPILE := CLANG_TRIPLE=aarch64-linux-gnu- CC=$(KERNEL_ROOT_DIR)/prebuilts/clang/host/linux-x86/clang-r353983c/bin/clang
+      TARGET_KERNEL_CLANG_COMPILE := CLANG_TRIPLE=aarch64-linux-gnu-
+      CC := $(KERNEL_ROOT_DIR)/prebuilts/clang/host/linux-x86/clang-r353983c/bin/clang
     endif
   else
     TARGET_KERNEL_CROSS_COMPILE ?= $(KERNEL_ROOT_DIR)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel-
+    TARGET_KERNEL_CLANG_COMPILE :=
+    CC := $(TARGET_KERNEL_CROSS_COMPILE)gcc
   endif
+
+  ifeq ($(USE_CCACHE), true)
+    CCACHE_EXEC ?= /usr/bin/ccache
+    CCACHE_EXEC := $(abspath $(wildcard $(CCACHE_EXEC)))
+  else
+    CCACHE_EXEC :=
+  endif
+  ifneq ($(CCACHE_EXEC),)
+    TARGET_KERNEL_CLANG_COMPILE += CCACHE_CPP2=yes CC=$(CCACHE_EXEC)\ $(CC)
+  else
+    TARGET_KERNEL_CLANG_COMPILE += CC=$(CC)
+  endif
+
   KERNEL_HOST_GCC_PREFIX := $(patsubst %strip,%,$(HOST_STRIP))
   ifeq (yes,yes)
   KERNEL_HOSTCC := $(KERNEL_ROOT_DIR)/$(KERNEL_HOST_GCC_PREFIX)gcc
