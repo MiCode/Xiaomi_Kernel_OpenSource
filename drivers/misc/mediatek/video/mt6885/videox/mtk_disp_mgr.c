c4e8febb7fde2d77e4e28314beb892cfcf334941
Shiuan Huang <shiuan.huang@mediatek.com>
2019-10-05 14:48:22 +0800

    [ALPS04409652] PQ: add IOCTL for night light
    Add DISP_IOCTL_SUPPORT_COLOR_TRANSFORM for user to query
    whether the DISP_CCORR can support color transform
    of night light.
    
    MTK-Commit-Id: d4c7c5ad70c40abea6cc335217935ce07b8d0465
    
    Change-Id: I9e1614ffe1bc89700685ccb095e5aac7b512fd77
    CR-Id: ALPS04409652
    Feature: Display Quality Enhancement
    Signed-off-by: Shiuan Huang <shiuan.huang@mediatek.com>

--- drivers/misc/mediatek/video/mt6885/videox/mtk_disp_mgr.c
+++ drivers/misc/mediatek/video/mt6885/videox/mtk_disp_mgr.c
@@ -1473,6 +1473,8 @@ const char *_session_ioctl_str(unsigned int cmd)
 		return "DISP_IOCTL_CCORR_EVENTCTL";
 	case DISP_IOCTL_CCORR_GET_IRQ:
 		return "DISP_IOCTL_CCORR_GET_IRQ";
+	case DISP_IOCTL_SUPPORT_COLOR_TRANSFORM:
+		return "DISP_IOCTL_SUPPORT_COLOR_TRANSFORM";
 	case DISP_IOCTL_SET_PQPARAM:
 		return "DISP_IOCTL_SET_PQPARAM";
 	case DISP_IOCTL_GET_PQPARAM:
@@ -1558,6 +1560,7 @@ long mtk_disp_mgr_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 	case DISP_IOCTL_SET_CCORR:
 	case DISP_IOCTL_CCORR_EVENTCTL:
 	case DISP_IOCTL_CCORR_GET_IRQ:
+	case DISP_IOCTL_SUPPORT_COLOR_TRANSFORM:
 	case DISP_IOCTL_SET_PQPARAM:
 	case DISP_IOCTL_GET_PQPARAM:
 	case DISP_IOCTL_SET_PQINDEX:
@@ -1694,6 +1697,7 @@ static long mtk_disp_mgr_compat_ioctl(struct file *file, unsigned int cmd,
 	case DISP_IOCTL_SET_CCORR:
 	case DISP_IOCTL_CCORR_EVENTCTL:
 	case DISP_IOCTL_CCORR_GET_IRQ:
+	case DISP_IOCTL_SUPPORT_COLOR_TRANSFORM:
 	case DISP_IOCTL_SET_PQPARAM:
 	case DISP_IOCTL_GET_PQPARAM:
 	case DISP_IOCTL_SET_PQINDEX:
