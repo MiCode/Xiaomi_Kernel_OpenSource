f17d09aba3182d8720a30ad0c851d1982613f441
Neal Liu <neal.liu@mediatek.com>
2019-10-05 14:50:22 +0800

    [ALPS04271253] TEEs: add kernel atrace API to trace TEEs sched
    1. revise TEEs log buffer timestamp to align linux kernel
    2. add kernel atrace API to trace TEEs sched from tee_log buffer
    
    MTK-Commit-Id: 6d2acf51ee3beb3dbe3d3740312f689f590e038c
    
    Change-Id: Ia4149283f2aeedd0b6dace3c05627a608511ce9c
    Signed-off-by: Neal Liu <neal.liu@mediatek.com>
    CR-Id: ALPS04271253
    Feature: Trustonic TEE (Trusted Execution Environment)

--- drivers/misc/mediatek/timer/timesync/archcounter_timesync.h
+++ drivers/misc/mediatek/timer/timesync/archcounter_timesync.h
@@ -17,5 +17,6 @@
 void archcounter_timesync_init(uint8_t status);
 uint64_t archcounter_timesync_to_monotonic(uint64_t hwclock);
 uint64_t archcounter_timesync_to_boot(uint64_t hwclock);
+u64 mtk_get_archcounter_time(u64 cyc);
 
 #endif
