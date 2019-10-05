f21174c35179727e721a14bb4255877797c7c15c
Miles Chen <miles.chen@mediatek.com>
2019-10-05 14:49:50 +0800

    [ALPS04659758] mm/memcontrol: poison memcg before freeing it
    This debug change posions mcmcg before freeing it in order to
    trap some use-after-free cases.
    
    MTK-Commit-Id: 8285127168859bcdc450a4eb61cbeda95109198a
    
    Change-Id: Idd7297bdd832bb09f43b05529d01cefd91541141
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    CR-Id: ALPS04659758
    Feature: [Module]Kernel Memory Management

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -4241,6 +4241,8 @@ static void __mem_cgroup_free(struct mem_cgroup *memcg)
 	for_each_node(node)
 		free_mem_cgroup_per_node_info(memcg, node);
 	free_percpu(memcg->stat);
+	/* poison memcg before freeing it */
+	memset(memcg, 0x78, sizeof(struct mem_cgroup));
 	kfree(memcg);
 }
 
