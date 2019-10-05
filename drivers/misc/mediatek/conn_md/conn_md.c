59fd8fcf270e21adea75ed5f5851942a35336169
Tim Chang <tim.chang@mediatek.com>
2019-10-05 14:30:01 +0800

    [ALPS04294481] WMT: fix bug of memory copy
    When copy local parameter data, the length should
    be message info length minus length of struct local_para.
    
    MTK-Commit-Id: 478af18afe96230e8db7a0c28c50ea53c9ce792e
    
    Change-Id: Ia60a20c9f2dbb359ec61918794fbaaa2cdb0a4c3
    Signed-off-by: Tim Chang <tim.chang@mediatek.com>
    CR-Id: ALPS04294481
    Feature: [Module]WMT Driver
    (cherry picked from commit a11a37ed1f7b338991d0bb1084886f904b3b4058)

--- drivers/misc/mediatek/conn_md/conn_md.c
+++ drivers/misc/mediatek/conn_md/conn_md.c
@@ -314,7 +314,7 @@ int conn_md_send_msg(struct ipc_ilm *ilm)
 				sizeof(struct local_para));
 		/*copy data from local_para_ptr structure */
 		memcpy(p_local_para->data, ilm->local_para_ptr->data,
-				msg_info_len);
+				msg_info_len - sizeof(struct local_para));
 
 		CONN_MD_DBG_FUNC("p_local_para:0x%08x, msg_len:%d\n",
 				p_local_para, p_local_para->msg_len);
