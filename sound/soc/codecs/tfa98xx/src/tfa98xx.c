b634511838d00d6295998e97462a4fd17c4a0896
zhixiong wang <zhixiong.wang@mediatek.com>
2019-10-05 14:53:01 +0800

    [ALPS04724240] Audio: fix nxp build error
    init start_time to avoid build error
    
    MTK-Commit-Id: dbaa92a555b059c844519e301d909d6cf32b748c
    
    Change-Id: I51138548c67d7616dc842daa3481556ad42867a8
    Signed-off-by: zhixiong wang <zhixiong.wang@mediatek.com>
    CR-Id: ALPS04724240
    Feature: [Module]Audio HAL

--- sound/soc/codecs/tfa98xx/src/tfa98xx.c
+++ sound/soc/codecs/tfa98xx/src/tfa98xx.c
@@ -189,7 +189,7 @@ static enum tfa_error tfa98xx_tfa_start(struct tfa98xx *tfa98xx,
 	int next_profile, int vstep)
 {
 	enum tfa_error err;
-	ktime_t start_time, stop_time;
+	ktime_t start_time = 0, stop_time;
 	u64 delta_time;
 
 	pr_debug("next_profile=%d  vstep=%d\n", next_profile, vstep);
