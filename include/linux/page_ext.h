04856829ff836effc444f4a4bd32056b26f107a0
Sudarshan Rajagopalan <sudaraja@codeaurora.org>
2019-10-09 15:01:23 -0700

    mm, page_owner: set page owner info for tail pages
    When allocating higher order pages, currently only the head page is set
    with page owner info, leaving the tail pages with previous or stale info.
    This patch sets the info for tail pages too and sets the time of free in
    page owner info of all pages during free. This patch also adds an extra
    info to page extension flags to help check if page is in buddy or not.
    
    Change-Id: I96d13485af698d586a4dd7b1092a379a645c8fcb
    Signed-off-by: Sudarshan Rajagopalan <sudaraja@codeaurora.org>
    Signed-off-by: Vaishali Gupta <Vaishali@codeaurora.org>

--- include/linux/page_ext.h
+++ include/linux/page_ext.h
@@ -19,6 +19,7 @@ struct page_ext_operations {
 enum page_ext_flags {
 	PAGE_EXT_DEBUG_GUARD,
 	PAGE_EXT_OWNER,
+	PAGE_EXT_PG_FREE,
 #if defined(CONFIG_IDLE_PAGE_TRACKING) && !defined(CONFIG_64BIT)
 	PAGE_EXT_YOUNG,
 	PAGE_EXT_IDLE,
