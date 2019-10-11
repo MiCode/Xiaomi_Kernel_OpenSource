a1a9e031f21c74e0af6ef7219825867b043833e6
Zhenhua Huang <zhenhuah@codeaurora.org>
2019-10-11 18:52:34 +0800

    scsi: ufs: fix compiling error when FTRACE disabled
    We should not define __ufshcd_cmd_log when !CONFIG_TRACEPOINTS.
    It will not be called.
    
    Change-Id: I1c0665c7920630159990c1e81856ea45b0f5d32c
    Signed-off-by: Zhenhua Huang <zhenhuah@codeaurora.org>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -685,6 +685,7 @@ static void ufshcd_cmd_log_init(struct ufs_hba *hba)
 {
 }
 
+#ifdef CONFIG_TRACEPOINTS
 static void __ufshcd_cmd_log(struct ufs_hba *hba, char *str, char *cmd_type,
 			     unsigned int tag, u8 cmd_id, u8 idn, u8 lun,
 			     sector_t lba, int transfer_len)
@@ -700,6 +701,7 @@ static void __ufshcd_cmd_log(struct ufs_hba *hba, char *str, char *cmd_type,
 
 	ufshcd_add_command_trace(hba, &entry);
 }
+#endif
 
 static void ufshcd_dme_cmd_log(struct ufs_hba *hba, char *str, u8 cmd_id)
 {
