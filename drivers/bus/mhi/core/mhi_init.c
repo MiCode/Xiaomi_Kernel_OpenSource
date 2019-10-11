1fc5a34d1f00dfacd84e73a058a961c3842e9d7d
Vaishali Gupta <Vaishali@codeaurora.org>
2019-10-11 16:13:26 -0700

    Revert "mhi: core: Call vfree instead of kfree"
    This reverts commit cac7115671a61242ea4a1ba6dea55bdb7c087a75.
    
    Change-Id: Iee41f495e4a9b3febed0bfbe2a7d28965e5301e0
    Signed-off-by: Vaishali Gupta <Vaishali@codeaurora.org>

--- drivers/bus/mhi/core/mhi_init.c
+++ drivers/bus/mhi/core/mhi_init.c
@@ -1299,7 +1299,7 @@ static int of_parse_dt(struct mhi_controller *mhi_cntrl,
 	return 0;
 
 error_ev_cfg:
-	vfree(mhi_cntrl->mhi_chan);
+	kfree(mhi_cntrl->mhi_chan);
 
 	return ret;
 }
@@ -1442,7 +1442,7 @@ error_alloc_dev:
 	kfree(mhi_cntrl->mhi_cmd);
 
 error_alloc_cmd:
-	vfree(mhi_cntrl->mhi_chan);
+	kfree(mhi_cntrl->mhi_chan);
 	kfree(mhi_cntrl->mhi_event);
 
 	return ret;
@@ -1455,7 +1455,7 @@ void mhi_unregister_mhi_controller(struct mhi_controller *mhi_cntrl)
 
 	kfree(mhi_cntrl->mhi_cmd);
 	kfree(mhi_cntrl->mhi_event);
-	vfree(mhi_cntrl->mhi_chan);
+	kfree(mhi_cntrl->mhi_chan);
 	kfree(mhi_cntrl->mhi_tsync);
 
 	device_del(&mhi_dev->dev);
