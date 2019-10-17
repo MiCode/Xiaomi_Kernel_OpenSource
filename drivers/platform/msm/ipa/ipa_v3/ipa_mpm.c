f2c6d21e1251a9af033f24392a1b1470b57755c5
Michael Adisumarta <madisuma@codeaurora.org>
2019-10-17 13:19:55 -0700

    msm:ipa: stop the UL channels  after probe
    This change is to stop uplink MHIP IPA tethering channels if
    there is no active tethering use case.
    
    Change-Id: I93fb5fb9b6b83c4a7c8cdc2b7ab8b1c1ff9ff7fb
    Acked-by: Jyothij <jyothij@qti.qualcomm.com>
    Signed-off-by: Michael Adisumarta <madisuma@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipa_mpm.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_mpm.c
@@ -2187,8 +2187,12 @@ static int ipa_mpm_mhi_probe_cb(struct mhi_device *mhi_dev,
 	case IPA_MPM_TETH_INIT:
 		if (ul_prod != IPA_CLIENT_MAX) {
 			/* No teth started yet, disable UL channel */
-			ipa_mpm_start_stop_mhip_chan(IPA_MPM_MHIP_CHAN_UL,
-						probe_id, MPM_MHIP_STOP);
+			ret = ipa3_stop_gsi_channel(ipa_ep_idx);
+			if (ret) {
+				IPA_MPM_ERR("MHIP Stop channel err = %d\n",
+					ret);
+				goto fail_stop_channel;
+			}
 		}
 		if (is_acted)
 			ipa_mpm_vote_unvote_pcie_clk(CLK_OFF, probe_id,
@@ -2228,6 +2232,7 @@ static int ipa_mpm_mhi_probe_cb(struct mhi_device *mhi_dev,
 
 fail_gsi_setup:
 fail_start_channel:
+fail_stop_channel:
 fail_smmu:
 	if (ipa_mpm_ctx->dev_info.ipa_smmu_enabled)
 		IPA_MPM_DBG("SMMU failed\n");
