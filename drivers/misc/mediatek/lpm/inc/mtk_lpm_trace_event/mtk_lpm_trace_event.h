b5f0205374357af1b036f08da72d01789a429bf8
albert-zl.huang <albert-zl.huang@mediatek.com>
2019-10-18 11:56:28 +0800

    [ALPS04812111] spm: create trace for subsystem sleep check
    Create trace for subsystem sleep check
    
    MTK-Commit-Id: 5868c09fe0cd065dcc660643d1d026c9dc9b33f3
    
    Change-Id: If9b2944d4143140d1c931587a66dc3bbd7f387dc
    Signed-off-by: albert-zl.huang <albert-zl.huang@mediatek.com>
    CR-Id: ALPS04812111
    Feature: Power Management

--- drivers/misc/mediatek/lpm/inc/mtk_lpm_trace_event/mtk_lpm_trace_event.h
+++ drivers/misc/mediatek/lpm/inc/mtk_lpm_trace_event/mtk_lpm_trace_event.h
@@ -26,6 +26,39 @@ TRACE_EVENT(mtk_lpm_trace,
 	),
 	TP_printk("%s", (char *)__entry->_datas)
 );
+
+TRACE_EVENT(SPM__resource_req_0,
+	TP_PROTO(unsigned int md, unsigned int conn, unsigned int scp,
+		unsigned int adsp, unsigned int ufs, unsigned int msdc,
+		unsigned int disp, unsigned int apu, unsigned int spm),
+	TP_ARGS(md, conn, scp, adsp, ufs, msdc, disp, apu, spm),
+	TP_STRUCT__entry(__field(unsigned int, md)
+		__field(unsigned int, conn)
+		__field(unsigned int, scp)
+		__field(unsigned int, adsp)
+		__field(unsigned int, ufs)
+		__field(unsigned int, msdc)
+		__field(unsigned int, disp)
+		__field(unsigned int, apu)
+		__field(unsigned int, spm)
+	),
+	TP_fast_assign(__entry->md = md;
+		__entry->conn = conn;
+		__entry->scp = scp;
+		__entry->adsp = adsp;
+		__entry->ufs = ufs;
+		__entry->msdc = msdc;
+		__entry->disp = disp;
+		__entry->apu = apu;
+		__entry->spm = spm;
+	),
+	TP_printk("%d, %d, %d, %d, %d, %d, %d, %d, %d",
+		__entry->md, __entry->conn,
+		__entry->scp, __entry->adsp,
+		__entry->ufs, __entry->msdc,
+		__entry->disp, __entry->apu,
+		__entry->spm)
+);
 #endif
 
 #undef TRACE_INCLUDE_PATH
