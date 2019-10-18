ce45e7dcec5d45f2630a0e705201961c912c6d5b
wen.su <wen.su@mediatek.com>
2019-10-18 11:46:33 +0800

    [ALPS04324345] PMIC: Add MT6315 interrupt dts
    Update dts/dt-bindings for mt6315 irq support.
    
    MTK-Commit-Id: b05c296b6172ee1ec9074eaf4c18462d6c88f223
    
    Change-Id: Ie9e16ae549c2aa76311ad6c8c2ad828fe8694c76
    Signed-off-by: wen.su <wen.su@mediatek.com>
    CR-Id: ALPS04324345
    Feature: Power Management

--- include/dt-bindings/mfd/mt6315-irq.h
+++ include/dt-bindings/mfd/mt6315-irq.h
@@ -1,31 +1,26 @@
 /*
  * Copyright (C) 2019 MediaTek Inc.
- *
+
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
- *
+
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
 
-&mt6315_7_regulator {
-	compatible = "mediatek,mt6315_7-regulator";
-	interrupt-controller;
-	#interrupt-cells = <2>;
+#ifndef _DT_BINDINGS_MT6315_IRQ_H
+#define _DT_BINDINGS_MT6315_IRQ_H
 
-	mt6315_7_vbuck1: 7_vbuck1 {
-		regulator-name = "7_vbuck1";
-		regulator-min-microvolt = <300000>;
-		regulator-max-microvolt = <1193750>;
-		regulator-enable-ramp-delay = <256>;
-	};
-	mt6315_7_vbuck3: 7_vbuck3 {
-		regulator-name = "7_vbuck3";
-		regulator-min-microvolt = <300000>;
-		regulator-max-microvolt = <1193750>;
-		regulator-enable-ramp-delay = <256>;
-	};
-};
+#define INT_VBUCK1_OC			0
+#define INT_VBUCK2_OC			1
+#define INT_VBUCK3_OC			2
+#define INT_VBUCK4_OC			3
+#define INT_TEMP_L			4
+#define INT_TEMP_H			5
+#define INT_RCS0			6
+#define INT_RCS1			7
+
+#endif /* _DT_BINDINGS_MT6315_IRQ_H */
