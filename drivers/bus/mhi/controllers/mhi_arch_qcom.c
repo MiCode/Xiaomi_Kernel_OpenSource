c9b73de237b9eecbd2cf4129ee2821b628da6cb4
Bhaumik Bhatt <bbhatt@codeaurora.org>
2019-10-17 17:45:25 -0700

    mhi: cntrl: qcom: distinguish between legacy and autonomous suspend modes
    Allow for both legacy and autonomous MHI state suspend modes
    to be handled by the controller.
    
    Change-Id: Ia8920bf7fe35a0ffe5eab433d1d461a16f6db6a4
    Signed-off-by: Bhaumik Bhatt <bbhatt@codeaurora.org>

--- drivers/bus/mhi/controllers/mhi_arch_qcom.c
+++ drivers/bus/mhi/controllers/mhi_arch_qcom.c
@@ -438,10 +438,11 @@ int mhi_arch_pcie_init(struct mhi_controller *mhi_cntrl)
 				return -EINVAL;
 		}
 
-		/* register with pcie rc for WAKE# events */
+		/* register with pcie rc for WAKE# or link state events */
 		reg_event = &arch_info->pcie_reg_event;
-		reg_event->events =
-			MSM_PCIE_EVENT_WAKEUP | MSM_PCIE_EVENT_L1SS_TIMEOUT;
+		reg_event->events = mhi_dev->allow_m1 ?
+			(MSM_PCIE_EVENT_WAKEUP) :
+			(MSM_PCIE_EVENT_WAKEUP | MSM_PCIE_EVENT_L1SS_TIMEOUT);
 
 		reg_event->user = mhi_dev->pci_dev;
 		reg_event->callback = mhi_arch_pci_link_state_cb;
@@ -658,7 +659,8 @@ int mhi_arch_link_suspend(struct mhi_controller *mhi_cntrl)
 	MHI_LOG("Entered\n");
 
 	/* disable inactivity timer */
-	msm_pcie_l1ss_timeout_disable(pci_dev);
+	if (!mhi_dev->allow_m1)
+		msm_pcie_l1ss_timeout_disable(pci_dev);
 
 	switch (mhi_dev->suspend_mode) {
 	case MHI_DEFAULT_SUSPEND:
@@ -686,7 +688,7 @@ int mhi_arch_link_suspend(struct mhi_controller *mhi_cntrl)
 	}
 
 exit_suspend:
-	if (ret)
+	if (ret && !mhi_dev->allow_m1)
 		msm_pcie_l1ss_timeout_enable(pci_dev);
 
 	MHI_LOG("Exited with ret:%d\n", ret);
@@ -762,7 +764,8 @@ int mhi_arch_link_resume(struct mhi_controller *mhi_cntrl)
 		return ret;
 	}
 
-	msm_pcie_l1ss_timeout_enable(pci_dev);
+	if (!mhi_dev->allow_m1)
+		msm_pcie_l1ss_timeout_enable(pci_dev);
 
 	MHI_LOG("Exited\n");
 
--- drivers/bus/mhi/controllers/mhi_qcom.h
+++ drivers/bus/mhi/controllers/mhi_qcom.h
@@ -53,6 +53,7 @@ struct mhi_dev {
 	int resn;
 	void *arch_info;
 	bool powered_on;
+	bool allow_m1;
 	dma_addr_t iova_start;
 	dma_addr_t iova_stop;
 	enum mhi_suspend_mode suspend_mode;
