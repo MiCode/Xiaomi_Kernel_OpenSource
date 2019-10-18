3fd10f0a748a694f39fc3517dd7119758552b278
YT Chang <yt.chang@mediatek.com>
2019-10-18 11:46:14 +0800

    [ALPS04702478] sched: fix find_best_target behavior
    [Details]
    1. Need to consider turning point when check capacity. In the same time,
       for Non latency sensitive task for IDLE/ACTIVE CPUs,
       the design already check the suitable capacity.
       So, remove the check capacity before check Non latency task.
    2. Dont need to update target_capacity for Non latency sensitive tasks
       when CPU is IDLE. Use best_idle_min_cap_orig to record the CPU with
       most suitable capacity CPU
    
    MTK-Commit-Id: 61e5e316ee946252f1467223fcb4c095fcf1d985
    
    Change-Id: I1bc6f9ad753d0f55bab044234e8d35b31e1c4cd2
    Signed-off-by: YT Chang <yt.chang@mediatek.com>
    CR-Id: ALPS04702478
    Feature: Energy Aware Scheduling Plus (EAS+)

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7724,28 +7724,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			}
 
 			/*
-			 * Enforce EAS mode
-			 *
-			 * For non latency sensitive tasks, skip CPUs that
-			 * will be overutilized by moving the task there.
-			 *
-			 * The goal here is to remain in EAS mode as long as
-			 * possible at least for !prefer_idle tasks.
-			 */
-			/*
-			if ((new_util * capacity_margin) >
-			    (capacity * SCHED_CAPACITY_SCALE))
-				continue;
-			*/
-
-			/*
-			 * Favor CPUs with smaller capacity for non latency
-			 * sensitive tasks.
-			 */
-			if (capacity_orig > target_capacity)
-				continue;
-
-			/*
 			 * Case B) Non latency sensitive tasks on IDLE CPUs.
 			 *
 			 * Find an optimal backup IDLE CPU for non latency
@@ -7794,7 +7772,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 
 				/* Keep track of best idle CPU */
 				best_idle_min_cap_orig = capacity_orig;
-				target_capacity = capacity_orig;
 				best_idle_cstate = idle_idx;
 				best_idle_cpu = i;
 				continue;
