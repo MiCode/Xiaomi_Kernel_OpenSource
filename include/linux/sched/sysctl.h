2d2b0b196b7036847d25eec77d123b8180abaf49
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-10-22 09:54:21 +0530

    sched: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: If1ee58a8ed59e4a9ee25dfa6fa2a1c1654e00e6d
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -40,6 +40,7 @@ extern unsigned int sysctl_sched_cpu_high_irqload;
 extern unsigned int sysctl_sched_boost;
 extern unsigned int sysctl_sched_group_upmigrate_pct;
 extern unsigned int sysctl_sched_group_downmigrate_pct;
+extern unsigned int sysctl_sched_conservative_pl;
 extern unsigned int sysctl_sched_many_wakeup_threshold;
 extern unsigned int sysctl_sched_walt_rotate_big_tasks;
 extern unsigned int sysctl_sched_min_task_util_for_boost;
--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -371,6 +371,15 @@ static struct ctl_table kern_table[] = {
 		.extra2		= &three,
 	},
 	{
+		.procname	= "sched_conservative_pl",
+		.data		= &sysctl_sched_conservative_pl,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= &zero,
+		.extra2		= &one,
+	},
+	{
 		.procname	= "sched_many_wakeup_threshold",
 		.data		= &sysctl_sched_many_wakeup_threshold,
 		.maxlen		= sizeof(unsigned int),
