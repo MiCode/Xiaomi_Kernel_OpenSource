3fb5bac0c28c19225d2399302b81ee46fa2b8d51
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-10-23 14:59:12 +0530

    sched: remove extern declaration of msm_sched_setaffinity()
    msm_sched_setaffinity() function is not defined in the scheduler.
    Remove the extern declaration from the header file.
    
    Change-Id: I015e498e128c30f731f7f6e23cb432a87b709fc2
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1971,7 +1971,6 @@ static inline void set_task_cpu(struct task_struct *p, unsigned int cpu)
 # define vcpu_is_preempted(cpu)	false
 #endif
 
-extern long msm_sched_setaffinity(pid_t pid, struct cpumask *new_mask);
 extern long sched_setaffinity(pid_t pid, const struct cpumask *new_mask);
 extern long sched_getaffinity(pid_t pid, struct cpumask *mask);
 
