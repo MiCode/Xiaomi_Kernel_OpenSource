94544da4e33e0ff933926e7e3ebc6c549cdb4ed9
Komal Bajaj <kbajaj@codeaurora.org>
2019-10-24 15:36:07 +0530

    soc: qcom: Add soc information for Lagoon
    Add SoC information for Lagoon chipset, providing the
    SoC Id information and dt binding compatible.
    
    Change-Id: Id9b554a38b46bf5d6208002b1a8703b53bd1a7ec
    Signed-off-by: Komal Bajaj <kbajaj@codeaurora.org>

--- include/soc/qcom/socinfo.h
+++ include/soc/qcom/socinfo.h
@@ -58,6 +58,8 @@
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,lito")
 #define early_machine_is_bengal()	\
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,bengal")
+#define early_machine_is_lagoon()	\
+	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,lagoon")
 #define early_machine_is_sdmshrike()	\
 	of_flat_dt_is_compatible(of_get_flat_dt_root(), "qcom,sdmshrike")
 #define early_machine_is_sm6150()	\
@@ -91,6 +93,7 @@
 #define early_machine_is_kona()		0
 #define early_machine_is_lito()		0
 #define early_machine_is_bengal()	0
+#define early_machine_is_lagoon()	0
 #define early_machine_is_sdmshrike()	0
 #define early_machine_is_sm6150()	0
 #define early_machine_is_qcs405()	0
@@ -121,6 +124,7 @@ enum msm_cpu {
 	MSM_CPU_KONA,
 	MSM_CPU_LITO,
 	MSM_CPU_BENGAL,
+	MSM_CPU_LAGOON,
 	MSM_CPU_SDMSHRIKE,
 	MSM_CPU_SM6150,
 	MSM_CPU_QCS405,
