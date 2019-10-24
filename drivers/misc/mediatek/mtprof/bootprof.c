7d8691ec8335a4c0b5fc2e32393af580b972d37d
Oliver.Zeng <oliver.zeng@mediatek.com>
2019-10-24 14:35:13 +0800

    [ALPS04803475] printk: fix bootprof missing code
    fix bootprof missing code
    
    MTK-Commit-Id: 534fe49b738896284fbba6b9ef4e793773521f3b
    
    Change-Id: I030966f4b9c3fcaf34b3abaf74af514ea9b09014
    CR-Id: ALPS04803475
    Feature: Android Exception Engine(AEE)
    Signed-off-by: Oliver.Zeng <oliver.zeng@mediatek.com>

--- drivers/misc/mediatek/mtprof/bootprof.c
+++ drivers/misc/mediatek/mtprof/bootprof.c
@@ -21,6 +21,7 @@
 #include <linux/printk.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
+#include <log_store_kernel.h>
 
 #include "internal.h"
 #include "mtk_sched_mon.h"
@@ -173,6 +174,7 @@ static void bootup_finish(void)
 #ifdef CONFIG_MTK_SCHED_MON_DEFAULT_ENABLE
 	mt_sched_monitor_switch(1);
 #endif
+	set_logtoomuch_enable(1);
 }
 
 static void mt_bootprof_switch(int on)
@@ -192,6 +194,7 @@ static void mt_bootprof_switch(int on)
 			enabled = 0;
 			timestamp_off = ts;
 			boot_finish = true;
+			log_store_bootup();
 			bootup_finish();
 		}
 	}
