fbbe069bbb910b914f55ee3ab67103a35cc86b2d
David Sterba <dsterba@suse.com>
2019-10-24 18:28:32 +0200

    BACKPORT: crypto: testmgr - add test vectors for blake2b
    Test vectors for blake2b with various digest sizes. As the algorithm is
    the same up to the digest calculation, the key and input data length is
    distributed in a way that tests all combinanions of the two over the
    digest sizes.
    
    Based on the suggestion from Eric, the following input sizes are tested
    [0, 1, 7, 15, 64, 247, 256], where blake2b blocksize is 128, so the
    padded and the non-padded input buffers are tested.
    
              blake2b-160  blake2b-256  blake2b-384  blake2b-512
             ---------------------------------------------------
    len=0   | klen=0       klen=1       klen=32      klen=64
    len=1   | klen=32      klen=64      klen=0       klen=1
    len=7   | klen=64      klen=0       klen=1       klen=32
    len=15  | klen=1       klen=32      klen=64      klen=0
    len=64  | klen=0       klen=1       klen=32      klen=64
    len=247 | klen=32      klen=64      klen=0       klen=1
    len=256 | klen=64      klen=0       klen=1       klen=32
    
    Where key:
    
    - klen=0: empty key
    - klen=1: 1 byte value 0x42, 'B'
    - klen=32: first 32 bytes of the default key, sequence 00..1f
    - klen=64: default key, sequence 00..3f
    
    The unkeyed vectors are ordered before keyed, as this is required by
    testmgr.
    
    CC: Eric Biggers <ebiggers@kernel.org>
    Signed-off-by: David Sterba <dsterba@suse.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    
    (cherry picked from commit a1afe27492a408d45421a1812064235691303fa1)
    
    Conflicts:
    	crypto/testmgr.c
    	crypto/testmgr.h
    
    (resolved conflicts due to blake2s having been backported already)
    
    Bug: 178411248
    Change-Id: I5d6e1c16b7be981f4912264cd67dc1dad20b6bec
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -4035,6 +4035,34 @@ static const struct alg_test_desc alg_test_descs[] = {
 		.test = alg_test_null,
 		.fips_allowed = 1,
 	}, {
+		.alg = "blake2b-160",
+		.test = alg_test_hash,
+		.fips_allowed = 0,
+		.suite = {
+			.hash = __VECS(blake2b_160_tv_template)
+		}
+	}, {
+		.alg = "blake2b-256",
+		.test = alg_test_hash,
+		.fips_allowed = 0,
+		.suite = {
+			.hash = __VECS(blake2b_256_tv_template)
+		}
+	}, {
+		.alg = "blake2b-384",
+		.test = alg_test_hash,
+		.fips_allowed = 0,
+		.suite = {
+			.hash = __VECS(blake2b_384_tv_template)
+		}
+	}, {
+		.alg = "blake2b-512",
+		.test = alg_test_hash,
+		.fips_allowed = 0,
+		.suite = {
+			.hash = __VECS(blake2b_512_tv_template)
+		}
+	}, {
 		.alg = "blake2s-128",
 		.test = alg_test_hash,
 		.suite = {
