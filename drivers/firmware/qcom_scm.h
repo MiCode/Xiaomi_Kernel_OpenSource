3919d02c8c8537116a8f886d84a5a3f1d3188707
Elliot Berman <eberman@codeaurora.org>
2019-10-25 15:27:29 -0700

    firmware: qcom_scm-32: Add device argument to atomic calls
    Add this unused parameter to reduce merge friction between SMCCC and
    legacy based conventions.
    
    Change-Id: Ie31b220d751ae08430b3e89d41de0217e5213435
    Signed-off-by: Elliot Berman <eberman@codeaurora.org>

--- drivers/firmware/qcom_scm.h
+++ drivers/firmware/qcom_scm.h
@@ -9,10 +9,11 @@
 #define QCOM_SCM_BOOT_TERMINATE_PC		0x02
 #define QCOM_SCM_BOOT_SET_REMOTE_STATE		0x0a
 #define QCOM_SCM_BOOT_SET_DLOAD_MODE		0x10
-extern int __qcom_scm_set_cold_boot_addr(void *entry, const cpumask_t *cpus);
+extern int __qcom_scm_set_cold_boot_addr(struct device *dev, void *entry,
+		const cpumask_t *cpus);
 extern int __qcom_scm_set_warm_boot_addr(struct device *dev, void *entry,
 		const cpumask_t *cpus);
-extern void __qcom_scm_cpu_power_down(u32 flags);
+extern void __qcom_scm_cpu_power_down(struct device *dev, u32 flags);
 extern int __qcom_scm_set_remote_state(struct device *dev, u32 state, u32 id);
 extern int __qcom_scm_set_dload_mode(struct device *dev, bool enable);
 #define QCOM_SCM_FLUSH_FLAG_MASK	0x3
