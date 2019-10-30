e477feeb93eaa514898287742c7a5429d740a179
lijuang <lijuang@codeaurora.org>
2019-10-30 11:09:32 +0800

    AndroidKernel: For support 32-bit kernel compile
    This file only support arm64 kernel compile currently, now we need
    to supprot arm kenrel compile, so add this change.
    
    Change-Id: I171ead43c9450886d9e096fc58a2729dee3439a1
    Signed-off-by: lijuang <lijuang@codeaurora.org>

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -76,7 +76,11 @@ KERNEL_GCC_NOANDROID_CHK := $(shell (echo "int main() {return 0;}" | $(KERNEL_CR
 
 real_cc :=
 ifeq ($(KERNEL_LLVM_SUPPORT),true)
-real_cc := REAL_CC=$(KERNEL_LLVM_BIN) CLANG_TRIPLE=aarch64-linux-gnu-
+  ifeq ($(KERNEL_ARCH), arm64)
+    real_cc := REAL_CC=$(KERNEL_LLVM_BIN) CLANG_TRIPLE=aarch64-linux-gnu-
+  else
+    real_cc := REAL_CC=$(KERNEL_LLVM_BIN) CLANG_TRIPLE=arm-linux-gnueabihf
+  endif
 else
 ifeq ($(strip $(KERNEL_GCC_NOANDROID_CHK)),0)
 KERNEL_CFLAGS := KCFLAGS=-mno-android
