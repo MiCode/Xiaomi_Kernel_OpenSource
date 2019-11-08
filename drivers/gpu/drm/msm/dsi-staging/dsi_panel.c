f485d99cbff2f241b5a9d556a9057573b2485c9d
Lei Chen <chenlei@codeaurora.org>
2019-11-08 10:11:29 +0800

    disp: msm: dsi: enable multi mode support for video mode panel
    Multi-panel can't be supported by video panel, but multi panel
    operating mode can be supported for video mode panel, so enable
    multi-mode support for video mode panel for panel operating mode
    switch.
    
    Change-Id: I9a62ba0c880d13c7201235b9cb65728fa13e3232
    Signed-off-by: Lei Chen <chenlei@codeaurora.org>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -3504,9 +3504,12 @@ int dsi_panel_get_mode_count(struct dsi_panel *panel)
 		goto error;
 	}
 
-	/* No multiresolution support is available for video mode panels */
+	/* No multiresolution support is available for video mode panels.
+	 * Multi-mode is supported for video mode during POMS is enabled.
+	 */
 	if (panel->panel_mode != DSI_OP_CMD_MODE &&
-		!panel->host_config.ext_bridge_num)
+		!panel->host_config.ext_bridge_num &&
+		!panel->panel_mode_switch_enabled)
 		count = SINGLE_MODE_SUPPORT;
 
 	panel->num_timing_nodes = count;
