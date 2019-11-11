30269e04be94d00b2ee2b42663fdbcf4e882019f
Lei Chen <chenlei@codeaurora.org>
2019-11-11 13:32:17 +0800

    disp: msm: dsi: Enable ESD check after POMS done
    ESD check is disabled while switching panel operating mode.
    This change enables ESD check after panel operating
    mode switch is done.
    
    Change-Id: I421d70d9be4c14107a7b51470801157d28874ffb
    Signed-off-by: Lei Chen <chenlei@codeaurora.org>

--- drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
@@ -250,8 +250,12 @@ static void dsi_bridge_enable(struct drm_bridge *bridge)
 		pr_err("[%d] DSI display post enabled failed, rc=%d\n",
 		       c_bridge->id, rc);
 
-	if (display && display->drm_conn)
+	if (display && display->drm_conn) {
 		sde_connector_helper_bridge_enable(display->drm_conn);
+		if (c_bridge->dsi_mode.dsi_mode_flags & DSI_MODE_FLAG_POMS)
+			sde_connector_schedule_status_work(display->drm_conn,
+				true);
+	}
 }
 
 static void dsi_bridge_disable(struct drm_bridge *bridge)
