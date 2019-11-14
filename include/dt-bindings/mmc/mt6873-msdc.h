15a67233f5f99f31612bb59bc910e0bb53f38e86
mtk13858 <derong.liu@mediatek.com>
2019-11-14 11:31:26 +0800

    [ALPS04715901] msdc:porting msdc driver
    porting msdc driver for fpga bring up
    
    MTK-Commit-Id: fc1bb8bf37f132151e2b95c3fe2fa9ecd4aa4661
    
    Change-Id: Ida51508b0b40584a318b88aca131a16498a07d0e
    Signed-off-by: mtk13858 <derong.liu@mediatek.com>
    CR-Id: ALPS04715901
    Feature: eMMC Boot Up

--- include/dt-bindings/mmc/mt6873-msdc.h
+++ include/dt-bindings/mmc/mt6873-msdc.h
@@ -12,8 +12,8 @@
  * GNU General Public License for more details.
  */
 
-#ifndef _DT_BINDINGS_MMC_MT6886_H
-#define _DT_BINDINGS_MMC_MT6886_H
+#ifndef _DT_BINDINGS_MMC_MT6873_H
+#define _DT_BINDINGS_MMC_MT6873_H
 
 #define MSDC_EMMC               (0)
 #define MSDC_SD                 (1)
@@ -42,4 +42,4 @@
 #define MSDC_SMPL_FALLING       (1)
 #define MSDC_SMPL_SEPARATE      (2)
 
-#endif /* _DT_BINDINGS_MMC_MT6886_H */
+#endif /* _DT_BINDINGS_MMC_MT6873_H */
