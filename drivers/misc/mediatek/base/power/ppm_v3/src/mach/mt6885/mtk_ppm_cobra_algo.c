b24cf95832cb3265566b4e43ab8c408aee02e3ba
CCJ Yeh <ccj.yeh@mediatek.com>
2019-11-14 11:32:14 +0800

    [ALPS04863094] PPM: fix out-of-bounds read
    Prevent out-of-bounds read by checking index value and use local
    temp variable to avoid race condition.
    
    MTK-Commit-Id: 61b2b1c12c26e3e03a6ae85b215ebc6aefe2ace0
    
    Change-Id: Ib1edb35eca43477d2e4b05f8472af56c209ebd56
    Signed-off-by: CCJ Yeh <ccj.yeh@mediatek.com>
    CR-Id: ALPS04863094
    Feature: Vulnerability Scan

--- drivers/misc/mediatek/base/power/ppm_v3/src/mach/mt6885/mtk_ppm_cobra_algo.c
+++ drivers/misc/mediatek/base/power/ppm_v3/src/mach/mt6885/mtk_ppm_cobra_algo.c
@@ -645,7 +645,8 @@ void ppm_cobra_init(void)
 	cobra_tbl = ioremap_nocache(PPM_COBRA_TBL_SRAM_ADDR,
 					PPM_COBRA_TBL_SRAM_SIZE);
 #else
-	cobra_tbl = kzalloc(PPM_COBRA_TBL_SRAM_SIZE, GFP_KERNEL);
+	cobra_tbl = kzalloc(sizeof(struct ppm_cobra_data), GFP_KERNEL);
+	ppm_info("cobra_tbl: %p\n", cobra_tbl);
 #endif
 	if (!cobra_tbl) {
 		WARN_ON(1);
@@ -776,7 +777,7 @@ void ppm_cobra_dump_tbl(struct seq_file *m)
 static unsigned int get_limit_opp_and_budget(void)
 {
 	unsigned int power = 0;
-	int i, j, k, idx;
+	unsigned int i, j, k, idx, core;
 
 	for (i = 0; i <= get_cluster_min_cpufreq_idx(PPM_CLUSTER_L); i++) {
 		cobra_lookup_data.limit[PPM_CLUSTER_L].opp = i;
@@ -786,12 +787,21 @@ static unsigned int get_limit_opp_and_budget(void)
 			cobra_lookup_data.limit[PPM_CLUSTER_B].opp = j;
 
 			for_each_ppm_clusters(k) {
-				if (!cobra_lookup_data.limit[k].core)
+				core = cobra_lookup_data.limit[k].core;
+
+				if (!core)
 					continue;
 
-				idx =
-					get_idx_in_pwr_tbl(k) +
-					cobra_lookup_data.limit[k].core - 1;
+				idx = get_idx_in_pwr_tbl(k) + core - 1;
+
+				if (idx >= TOTAL_CORE_NUM ||
+					i >= DVFS_OPP_NUM ||
+					j >= DVFS_OPP_NUM) {
+					ppm_info("[%p] idx: %d i:%d j:%d core:%d\n",
+						cobra_tbl, idx, i, j, core);
+					return 0;
+				}
+
 				power += (k == PPM_CLUSTER_B)
 				? cobra_tbl->basic_pwr_tbl[idx][j].power_idx
 				: cobra_tbl->basic_pwr_tbl[idx][i].power_idx;
