0940df2586da224451114289f9f397277ed5fdc7
YT Chang <yt.chang@mediatek.com>
2019-11-14 11:33:39 +0800

    [ALPS04247789] sched: best-fit policy to select CPU
    need to check turning point before comparing
    
    MTK-Commit-Id: ad7559e0493d3da89f0be8ab5b3be9cc03393192
    
    Change-Id: Icd2d8b3c08c3f6346cb812098193c8768c424e43
    Signed-off-by: YT Chang <yt.chang@mediatek.com>
    CR-Id: ALPS04247789
    Feature: Energy Aware Scheduling Plus (EAS+)

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7674,7 +7674,8 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 					}
 
 					/* find the max spare capacity cpu */
-					if (new_util < min_cpu_util) {
+					if (!turning &&
+						(new_util < min_cpu_util)) {
 						min_target_capacity =
 							capacity_orig;
 						min_cpu_util = new_util;
@@ -7774,7 +7775,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				}
 
 				/* find the max spare capacity cpu */
-				if (new_util < min_cpu_util) {
+				if (!turning && (new_util < min_cpu_util)) {
 					best_idle_min_cap_orig =
 						capacity_orig;
 					min_cpu_util = new_util;
@@ -7830,10 +7831,12 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				continue;
 			}
 
-			target_max_spare_cap = spare_cap;
-			target_capacity = capacity_orig;
-			target_util = new_util;
-			target_cpu = i;
+			if (!turning) {
+				target_max_spare_cap = spare_cap;
+				target_capacity = capacity_orig;
+				target_util = new_util;
+				target_cpu = i;
+			}
 		}
 
 	} while (sg = sg->next, sg != sd->groups);
