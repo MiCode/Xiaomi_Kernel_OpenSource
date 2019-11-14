ce5f485300fc7295a75cf51246dc324eb2f0c201
Cui Zhang <cui.zhang@mediatek.com>
2019-11-14 11:32:33 +0800

    [ALPS04859553] iommu: fix shutdown warning
    [Detail]
    do not free irq when vpu power control limitation happened.
    
    MTK-Commit-Id: 3e8b00c42cebf8766a9ed1d476385e3ed39f2096
    
    Change-Id: I874250cf85dc943359dae256c89bc3b04e524189
    Signed-off-by: Cui Zhang <cui.zhang@mediatek.com>
    CR-Id: ALPS04859553
    Feature: [Module]ION/M4U

--- drivers/iommu/mtk_iommu_v2.c
+++ drivers/iommu/mtk_iommu_v2.c
@@ -4177,7 +4177,7 @@ static int mtk_iommu_remove(struct platform_device *pdev)
 		bus_set_iommu(&platform_bus_type, NULL);
 
 #ifdef IOMMU_POWER_CLK_SUPPORT
-	if (!data->m4u_clks->nr_powers)
+	if (data->m4u_clks->nr_powers)
 		devm_free_irq(&pdev->dev, data->irq, data);
 #endif
 	component_master_del(&pdev->dev, &mtk_iommu_com_ops);
