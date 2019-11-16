0753c830b8ad079ddc588e8ae62befe5f6115259
Bhaumik Bhatt <bbhatt@codeaurora.org>
2019-11-15 18:10:08 -0800

    msm: pcie: remove explicit control over L1ss timeout feature
    PCIe bus driver should not explicitly enable/disable L1ss
    timeout feature. Leave the full control to client.
    
    Change-Id: Id26772cdad2d469e3855173bac3d061e7ec4bdf8
    Signed-off-by: Bhaumik Bhatt <bbhatt@codeaurora.org>

--- drivers/pci/host/pci-msm.c
+++ drivers/pci/host/pci-msm.c
@@ -743,7 +743,6 @@ struct msm_pcie_dev_t {
 	void				*ipc_log_dump;
 	bool				use_19p2mhz_aux_clk;
 	bool				use_pinctrl;
-	bool enable_l1ss_timeout;
 	bool				keep_powerdown_phy;
 	struct pinctrl			*pinctrl;
 	struct pinctrl_state		*pins_default;
@@ -6644,8 +6643,6 @@ static void __msm_pcie_l1ss_timeout_enable(struct msm_pcie_dev_t *pcie_dev)
 
 	msm_pcie_write_mask(pcie_dev->parf +
 			PCIE20_PARF_DEBUG_INT_EN, 0, BIT(0));
-
-	pcie_dev->enable_l1ss_timeout = true;
 }
 
 /* Suspend the PCIe link */
@@ -6670,9 +6667,6 @@ static int msm_pcie_pm_suspend(struct pci_dev *dev,
 			pcie_dev->rc_idx);
 		return ret;
 	}
-
-	if (pcie_dev->enable_l1ss_timeout)
-		__msm_pcie_l1ss_timeout_disable(pcie_dev);
 
 	if (dev && !(options & MSM_PCIE_CONFIG_NO_CFG_RESTORE)
 		&& msm_pcie_confirm_linkup(pcie_dev, true, true,
@@ -6824,9 +6818,6 @@ static int msm_pcie_pm_resume(struct pci_dev *dev,
 			"RC%d: exit of PCIe recover config\n",
 			pcie_dev->rc_idx);
 	}
-
-	if (pcie_dev->enable_l1ss_timeout)
-		__msm_pcie_l1ss_timeout_enable(pcie_dev);
 
 	PCIE_DBG(pcie_dev, "RC%d: exit\n", pcie_dev->rc_idx);
 
