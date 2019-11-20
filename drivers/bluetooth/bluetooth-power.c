25ab34cb756df4f8db30d0f07e2ea2b44f1413e2
Balakrishna Godavarthi <bgodavar@codeaurora.org>
2019-11-20 15:53:40 +0530

    Bluetooth: Move missing dts entries logs to info message
    If dts entries are missing, might be which are not required entry
    for a SoC. Instead of logging them as error message moving them
    to log messages.
    
    Change-Id: Ieafa88159a4816d64fb33248a7dd5c82b0d46d3c
    Signed-off-by: Balakrishna Godavarthi <bgodavar@codeaurora.org>

--- drivers/bluetooth/bluetooth-power.c
+++ drivers/bluetooth/bluetooth-power.c
@@ -600,7 +600,7 @@ err:
 static int bt_dt_parse_clk_info(struct device *dev,
 		struct bt_power_clk_data **clk_data)
 {
-	int ret = -EINVAL;
+	int ret = 0;
 	struct bt_power_clk_data *clk = NULL;
 	struct device_node *np = dev->of_node;
 
@@ -637,7 +637,7 @@ static int bt_dt_parse_clk_info(struct device *dev,
 
 		*clk_data = clk;
 	} else {
-		BT_PWR_ERR("clocks is not provided in device tree");
+		BT_PWR_INFO("clocks is not provided in device tree");
 	}
 
 err:
@@ -658,7 +658,7 @@ static int bt_power_populate_dt_pinfo(struct platform_device *pdev)
 			of_get_named_gpio(pdev->dev.of_node,
 						"qca,bt-reset-gpio", 0);
 		if (bt_power_pdata->bt_gpio_sys_rst < 0)
-			BT_PWR_ERR("bt-reset-gpio not provided in device tree");
+			BT_PWR_INFO("bt-reset-gpio not provided in devicetree");
 
 		rc = bt_dt_parse_vreg_info(&pdev->dev,
 					&bt_power_pdata->bt_vdd_core,
