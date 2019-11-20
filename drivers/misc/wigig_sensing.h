551d76c84c66db9002619f6f12438a2ad300514b
Gidon Studinski <gidons@codeaurora.org>
2019-11-20 17:41:08 +0200

    wigig_sensing: relax state machine restrictions
    Impose less restrictions on state changes, as required by the new system
    design.
    
    Change-Id: I9ea6d2fcf2bde4dd013cf7c494dcb5c0c4948fd7
    Signed-off-by: Gidon Studinski <gidons@codeaurora.org>
    Signed-off-by: Alexei Avshalom Lazar <ailizaro@codeaurora.org>

--- drivers/misc/wigig_sensing.h
+++ drivers/misc/wigig_sensing.h
@@ -131,7 +131,6 @@ struct spi_fifo {
 enum wigig_sensing_stm_e {
 	WIGIG_SENSING_STATE_MIN = 0,
 	WIGIG_SENSING_STATE_INITIALIZED,
-	WIGIG_SENSING_STATE_SPI_READY,
 	WIGIG_SENSING_STATE_READY_STOPPED,
 	WIGIG_SENSING_STATE_SEARCH,
 	WIGIG_SENSING_STATE_FACIAL,
@@ -144,10 +143,8 @@ enum wigig_sensing_stm_e {
 
 struct wigig_sensing_stm {
 	bool auto_recovery;
-	bool enabled;
 	bool fw_is_ready;
 	bool spi_malfunction;
-	bool sys_assert;
 	bool waiting_for_deep_sleep_exit;
 	bool waiting_for_deep_sleep_exit_first_pass;
 	bool burst_size_ready;
