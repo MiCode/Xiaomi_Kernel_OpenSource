ce5793485ae7682f43ba79409c4c50435b452000
Ashok Vuyyuru <avuyyuru@codeaurora.org>
2019-11-24 15:38:55 +0530

    msm: ipa4: Allocate coalescing close frame command payload
    Coalescing close frame command allocated multiple times during
    every suspend instead allocate command payload once in post init.
    
    Change-Id: Iba83a7ff032707739d0a3b006ee6fa5245ac3857
    Signed-off-by: Ashok Vuyyuru <avuyyuru@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
@@ -4974,8 +4974,8 @@ start_poll:
 	}
 	cnt += weight - remain_aggr_weight * IPA_WAN_AGGR_PKT_CNT;
 	/* call repl_hdlr before napi_reschedule / napi_complete */
-	if (cnt)
-		ep->sys->repl_hdlr(ep->sys);
+	ep->sys->repl_hdlr(ep->sys);
+
 	/* When not able to replenish enough descriptors pipe wait
 	 * until minimum number descripotrs to replish.
 	 */
@@ -4988,7 +4988,7 @@ start_poll:
 		ipa_pm_deferred_deactivate(ep->sys->pm_hdl);
 	} else {
 		cnt = weight;
-		IPADBG("Client = %d not replenished free descripotrs\n",
+		IPADBG_LOW("Client = %d not replenished free descripotrs\n",
 				ep->client);
 	}
 	return cnt;
