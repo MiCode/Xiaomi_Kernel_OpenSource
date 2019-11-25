6775da60d3b63364c92dfec50bed600445e9fde3
Kavya Nunna <knunna@codeaurora.org>
2019-11-25 16:55:16 +0530

    power: qpnp-smb2: Force power-role to UFP by default
    Add a DT property "qcom,ufp-only-mode" which forces power-role
    to UFP during initialization. A role swap to DFP is not allowed
    if this property is defined.
    
    Change-Id: Id21876473ca16ea007e353b3ce84637f9f489b19
    Signed-off-by: Kavya Nunna <knunna@codeaurora.org>

--- drivers/power/supply/qcom/smb-lib.h
+++ drivers/power/supply/qcom/smb-lib.h
@@ -356,6 +356,7 @@ struct smb_charger {
 	bool			otg_present;
 	bool			disable_stat_sw_override;
 	bool			fcc_stepper_enable;
+	bool			ufp_only_mode;
 
 	/* workaround flag */
 	u32			wa_flags;
@@ -548,6 +549,7 @@ int smblib_set_prop_pr_swap_in_progress(struct smb_charger *chg,
 int smblib_stat_sw_override_cfg(struct smb_charger *chg, bool override);
 void smblib_usb_typec_change(struct smb_charger *chg);
 int smblib_toggle_stat(struct smb_charger *chg, int reset);
+int smblib_force_ufp(struct smb_charger *chg);
 
 int smblib_init(struct smb_charger *chg);
 int smblib_deinit(struct smb_charger *chg);
