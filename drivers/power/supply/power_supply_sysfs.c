51d0eb21ef26e101af5013929eb91c376f4cc831
Ashish Chavan <ashichav@codeaurora.org>
2019-11-26 15:09:50 +0530

    power: supply: Add APSD based power-supply properties
    Add following power_supply properties:
    
    POWER_SUPPLY_PROP_APSD_RERUN - Property to re-run charger power source
    detection
    
    POWER_SUPPLY_PROP_APSD_TIMEOUT - Property to report APSD timer status.
    
    Change-Id: I2f8649461e5906f17adc5b360afed20b5fa6f58c
    Signed-off-by: Ashish Chavan <ashichav@codeaurora.org>

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -408,6 +408,8 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(comp_clamp_level),
 	POWER_SUPPLY_ATTR(adapter_cc_mode),
 	POWER_SUPPLY_ATTR(skin_health),
+	POWER_SUPPLY_ATTR(apsd_rerun),
+	POWER_SUPPLY_ATTR(apsd_timeout),
 	/* Charge pump properties */
 	POWER_SUPPLY_ATTR(cp_status1),
 	POWER_SUPPLY_ATTR(cp_status2),
