5cc5f2c62405124fc33cde36223c7fa36102dd0a
Shane Chien <Shane.Chien@mediatek.com>
2019-11-28 17:50:53 +0800

    [ALPS04900369] ASoC: shrink log
    shrink log when get "Speech_A2M_Msg_ID"
    kcontrol.
    
    MTK-Commit-Id: b72bd07e4ab312e3ba6ba4a38f352347e3f37a37
    
    Change-Id: I2663fcb86341d6f86fefb4d7699ce05fb6acf8f6
    Signed-off-by: Shane Chien <Shane.Chien@mediatek.com>
    CR-Id: ALPS04900369
    Feature: [Module]Audio Kernel

--- sound/soc/mediatek/mt6885/mt6885-misc-control.c
+++ sound/soc/mediatek/mt6885/mt6885-misc-control.c
@@ -2045,8 +2045,10 @@ static int speech_property_get(struct snd_kcontrol *kcontrol,
 	}
 	ucontrol->value.integer.value[0] = *sph_property;
 
-	dev_info(afe->dev, "%s(), %s = 0x%x\n", __func__,
-		 kcontrol->id.name, *sph_property);
+	if (strcmp(kcontrol->id.name, "Speech_A2M_Msg_ID") != 0)
+		dev_info(afe->dev, "%s(), %s = 0x%x\n", __func__,
+			 kcontrol->id.name, *sph_property);
+
 	return 0;
 }
 
