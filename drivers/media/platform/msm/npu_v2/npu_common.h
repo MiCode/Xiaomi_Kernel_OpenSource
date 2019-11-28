63c7bd5b535b598582c8509dfb88edc6897e4f90
Da Hoon Pyun <dpyun@codeaurora.org>
2019-11-28 16:47:46 -0500

    msm: npu: Allocate buffer per client for register read
    In order to avoid double free or use after free the buffer
    allocated while reading registers, it is needed to allocate the
    buffer per client and free it in release function.
    
    Change-Id: I96b6b8671b517f9123b28ce07f546de06fe19eab
    Signed-off-by: Da Hoon Pyun <dpyun@codeaurora.org>

--- drivers/media/platform/msm/npu_v2/npu_common.h
+++ drivers/media/platform/msm/npu_v2/npu_common.h
@@ -116,14 +116,18 @@ struct npu_debugfs_ctx {
 	struct dentry *root;
 	uint32_t reg_off;
 	uint32_t reg_cnt;
-	char *buf;
-	size_t buf_len;
 	uint8_t *log_buf;
 	struct mutex log_lock;
 	uint32_t log_num_bytes_buffered;
 	uint32_t log_read_index;
 	uint32_t log_write_index;
 	uint32_t log_buf_size;
+};
+
+struct npu_debugfs_reg_ctx {
+	char *buf;
+	size_t buf_len;
+	struct npu_device *npu_dev;
 };
 
 struct npu_mbox {
