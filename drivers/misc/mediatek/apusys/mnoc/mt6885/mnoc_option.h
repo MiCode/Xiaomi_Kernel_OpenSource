ee9054cf7393111f93b1616f3656215a0648a020
Chien-Chih Tseng <chien-chih.tseng@mediatek.com>
2019-11-28 17:49:45 +0800

    [ALPS04889708] apusys_mnoc: disable mnoc aee warning
    [Detail]
    1. disable mnoc aee warning
    
    MTK-Commit-Id: 1f616186b539d83afce7015c81bb484e0fc1e13c
    
    Change-Id: I253d1e2d2da0424b009b63488fd0d6fe60d1139d
    Signed-off-by: Chien-Chih Tseng <chien-chih.tseng@mediatek.com>
    CR-Id: ALPS04889708
    Feature: [Module] APUSYS

--- drivers/misc/mediatek/apusys/mnoc/mt6885/mnoc_option.h
+++ drivers/misc/mediatek/apusys/mnoc/mt6885/mnoc_option.h
@@ -17,7 +17,7 @@
 #define MNOC_INT_ENABLE (1)
 #define MNOC_QOS_ENABLE (1)
 #define MNOC_DBG_ENABLE (0)
-#define MNOC_AEE_WARN_ENABLE (1)
+#define MNOC_AEE_WARN_ENABLE (0)
 #define MNOC_APU_PWR_CHK (1)
 
 #endif
