d7f22b90ade6a598f20a6e9f310940160afe0594
Xinghua Yang <xinghua.yang@mediatek.com>
2019-11-28 17:49:09 +0800

    [ALPS04721345] aee: fix build errors
    Fix build errors when "# CONFIG_MTK_RAM_CONSOLE is not set"
    Details
    kernel-4.14/drivers/misc/mediatek/aee/mrdump/mrdump_mini.c:45:10: fatal
    error: mach/memory_layout.h file not found
    kernel-4.14/drivers/misc/mediatek/aee/mrdump/mrdump_panic.c:34:
    kernel-4.14/drivers/misc/mediatek/include/mt-plat/mtk_ram_console.h:272:20:
    error: static declaration of aee_rr_rec_kaslr_offset follows
    non-static declaration
    kernel-4.14/drivers/misc/mediatek/aee/mrdump/mrdump_private.h:65:13:
    note: previous declaration is here
    
    MTK-Commit-Id: 58272f2a560d090ac5ad6d5cd0a609866055774c
    
    Change-Id: Ib075c6ef3c85366d066bb7c1460e9a2a5f536c74
    Signed-off-by: Xinghua Yang <xinghua.yang@mediatek.com>
    CR-Id: ALPS04721345
    Feature: Android Exception Engine(AEE)

--- drivers/misc/mediatek/aee/mrdump/mrdump_private.h
+++ drivers/misc/mediatek/aee/mrdump/mrdump_private.h
@@ -62,7 +62,9 @@ extern void mrdump_mini_add_entry(unsigned long addr, unsigned long size);
 int aee_dump_stack_top_binary(char *buf, int buf_len, unsigned long bottom,
 				unsigned long top);
 
+#ifdef CONFIG_MTK_RAM_CONSOLE
 extern void aee_rr_rec_kaslr_offset(uint64_t offset);
+#endif
 #if defined(CONFIG_RANDOMIZE_BASE) && defined(CONFIG_ARM64)
 static inline void show_kaslr(void)
 {
