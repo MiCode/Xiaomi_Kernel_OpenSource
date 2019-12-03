e14bbf2be8bbab467441c5f9ac2558e5cc20204c
lokesh kumar <lokekuma@codeaurora.org>
2019-12-03 10:24:28 +0530

    dma-mapping:fix build when compile without CONFIG_ARM_DMA_USE_IOMMU
    Allow compilation when CONFIG_ARM_DMA_USE_IOMMU is not
    selected by providing necessary stub functions.
    
    Change-Id: Idbd30fb84bfea491a13a3cfac8917004a40e487b
    Signed-off-by: lokesh kumar <lokekuma@codeaurora.org>

--- arch/arm/include/asm/dma-iommu.h
+++ arch/arm/include/asm/dma-iommu.h
@@ -30,6 +30,8 @@ struct dma_iommu_mapping {
 	struct dma_fast_smmu_mapping *fast;
 };
 
+#ifdef CONFIG_ARM_DMA_USE_IOMMU
+
 struct dma_iommu_mapping *
 arm_iommu_create_mapping(struct bus_type *bus, dma_addr_t base, u64 size);
 
@@ -38,6 +40,30 @@ void arm_iommu_release_mapping(struct dma_iommu_mapping *mapping);
 int arm_iommu_attach_device(struct device *dev,
 					struct dma_iommu_mapping *mapping);
 void arm_iommu_detach_device(struct device *dev);
+
+#else  /* !CONFIG_ARM_DMA_USE_IOMMU */
+
+static inline struct dma_iommu_mapping *
+arm_iommu_create_mapping(struct bus_type *bus, dma_addr_t base, u64 size)
+{
+	return NULL;
+}
+
+static inline void arm_iommu_release_mapping(struct dma_iommu_mapping *mapping)
+{
+}
+
+static inline int arm_iommu_attach_device(struct device *dev,
+					struct dma_iommu_mapping *mapping)
+{
+	return -ENODEV;
+}
+
+static inline void arm_iommu_detach_device(struct device *dev)
+{
+}
+
+#endif  /* CONFIG_ARM_DMA_USE_IOMMU */
 
 int arm_dma_supported(struct device *dev, u64 mask);
 
