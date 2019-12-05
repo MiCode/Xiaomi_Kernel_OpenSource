unknownbaka/utopia_kernel_polaris

power: qpnp-fg-gen3: Enable voltage-mode correction saturation clear
As per the hardware recommendation enable the voltage-mode correction
saturation-clear config. This resets the voltage mode correction
and allows battery-SOC to be copied from CC-SOC.

Change-Id: Iea2becd7bcca004cc861fa488161a40cab29f114
Signed-off-by: Anirudh Ghayal <aghayal@codeaurora.org>
Signed-off-by: Sahil Chandna <chandna@codeaurora.org>

drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2016-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -4325,6 +4325,7 @@ static const struct power_supply_desc fg_psy_desc = {
 
 #define DEFAULT_ESR_CHG_TIMER_RETRY	8
 #define DEFAULT_ESR_CHG_TIMER_MAX	16
+#define VOLTAGE_MODE_SAT_CLEAR_BIT	BIT(3)
 static int fg_hw_init(struct fg_chip *chip)
 {
 	int rc;
@@ -4547,6 +4548,14 @@ static int fg_hw_init(struct fg_chip *chip)
 		pr_err("Error in writing batt_temp_delta, rc=%d\n", rc);
 		return rc;
 	}
+
+	rc = fg_sram_masked_write(chip, ESR_EXTRACTION_ENABLE_WORD,
+				ESR_EXTRACTION_ENABLE_OFFSET,
+				VOLTAGE_MODE_SAT_CLEAR_BIT,
+				VOLTAGE_MODE_SAT_CLEAR_BIT,
+				FG_IMA_DEFAULT);
+	if (rc < 0)
+		return rc;
 
 	fg_encode(chip->sp, FG_SRAM_ESR_TIGHT_FILTER,
 		chip->dt.esr_tight_flt_upct, buf);

MiCode/Xiaomi_Kernel_OpenSource

drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -3908,6 +3908,7 @@ static const struct power_supply_desc fg_psy_desc = {
 
 #define DEFAULT_ESR_CHG_TIMER_RETRY	8
 #define DEFAULT_ESR_CHG_TIMER_MAX	16
+#define VOLTAGE_MODE_SAT_CLEAR_BIT	BIT(3)
 static int fg_hw_init(struct fg_dev *fg)
 {
 	struct fg_gen3_chip *chip = container_of(fg, struct fg_gen3_chip, fg);
@@ -4130,6 +4131,14 @@ static int fg_hw_init(struct fg_dev *fg)
 		pr_err("Error in writing batt_temp_delta, rc=%d\n", rc);
 		return rc;
 	}
+
+	rc = fg_sram_masked_write(fg, ESR_EXTRACTION_ENABLE_WORD,
+				ESR_EXTRACTION_ENABLE_OFFSET,
+				VOLTAGE_MODE_SAT_CLEAR_BIT,
+				VOLTAGE_MODE_SAT_CLEAR_BIT,
+				FG_IMA_DEFAULT);
+	if (rc < 0)
+		return rc;
 
 	fg_encode(fg->sp, FG_SRAM_ESR_TIGHT_FILTER,
 		chip->dt.esr_tight_flt_upct, buf);