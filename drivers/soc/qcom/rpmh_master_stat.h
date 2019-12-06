ea2e374e45f26f88aa8cff26e535ef2664084c28
Jordan Crouse <jcrouse@codeaurora.org>
2019-12-06 14:24:42 -0700

    driver: soc: qcom: Fix function prototype
    Replace a #ifdef with a IS_ENABLED() to allow rpmh master stat to be built
    as a module.
    
    Fixes: 6d5e000 ("drivers: soc: qcom: snapshot of sleep drivers for Lahaina")
    Change-Id: Ic0dedbad11812a0bd932a9c0099dfb4632842177
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/soc/qcom/rpmh_master_stat.h
+++ drivers/soc/qcom/rpmh_master_stat.h
@@ -1,12 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2018-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.
  */
 
 #ifndef __QCOM_RPM_STATS_LOG_H__
 #define __QCOM_RPM_STATS_LOG_H__
 
-#if defined(CONFIG_QTI_RPM_STATS_LOG)
+#if IS_ENABLED(CONFIG_QTI_RPM_STATS_LOG)
 
 void msm_rpmh_master_stats_update(void);
 
