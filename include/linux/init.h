fb40239300fc6af06803d8915c3dce391354c8d7
Sami Tolvanen <samitolvanen@google.com>
2019-12-10 13:53:00 -0800

    Revert "ANDROID: HACK: init: ensure initcall ordering with LTO"
    This reverts commit 727e8415818e53ba15d4e6106e7931aca5cb9032.
    
    This is change not sufficient for ThinLTO, it's replaced by
      Iddda881a52b7942781713b188d810b6100159a2b
    
    Bug: 145210207
    Change-Id: Idc03fdb8c80b5939c2a0915165cb55e4cf6f0015
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- include/linux/init.h
+++ include/linux/init.h
@@ -153,15 +153,6 @@ extern bool initcall_debug;
 
 #ifndef __ASSEMBLY__
 
-#ifdef CONFIG_LTO_CLANG
-  /* prepend the variable name with __COUNTER__ to ensure correct ordering */
-  #define ___initcall_name2(c, fn, id) 	__initcall_##c##_##fn##id
-  #define ___initcall_name1(c, fn, id)	___initcall_name2(c, fn, id)
-  #define __initcall_name(fn, id) 	___initcall_name1(__COUNTER__, fn, id)
-#else
-  #define __initcall_name(fn, id) 	__initcall_##fn##id
-#endif
-
 /*
  * initcalls are now grouped by functionality into separate
  * subsections. Ordering inside the subsections is determined
@@ -179,7 +170,7 @@ extern bool initcall_debug;
  */
 
 #define __define_initcall(fn, id) \
-	static initcall_t __initcall_name(fn, id) __used \
+	static initcall_t __initcall_##fn##id __used \
 	__attribute__((__section__(".initcall" #id ".init"))) = fn;
 
 /*
