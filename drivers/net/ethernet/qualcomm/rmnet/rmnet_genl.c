4d205565a2c16e7704f6e2139b6726195ed82e7d
Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
2019-12-09 17:00:56 -0700

    rmnet: initialize rmnet pid ht spinlock
    Initializes the rmnet ht spinlock to avoid a watchdog bite when
    spinlock debugging is enabled. Fixes the following:
    3.062142:   <6> spinlock bad magic on CPU#5
    3.062154:   <6>  lock: rmnet_pid_ht_splock+0x0/0x18, .magic: 00000000
    3.062156:   <4> Causing a watchdog bite.
    
    Change-Id: I2e87b08bf00ba1b77445b2164db58da9ab6d559b
    Signed-off-by: Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>

--- drivers/net/ethernet/qualcomm/rmnet/rmnet_genl.c
+++ drivers/net/ethernet/qualcomm/rmnet/rmnet_genl.c
@@ -48,7 +48,9 @@ struct genl_family rmnet_core_genl_family = {
 #define RMNET_PID_STATS_HT_SIZE (8)
 #define RMNET_PID_STATS_HT rmnet_pid_ht
 DEFINE_HASHTABLE(rmnet_pid_ht, RMNET_PID_STATS_HT_SIZE);
-spinlock_t rmnet_pid_ht_splock; /* Spinlock definition for pid hash table */
+
+/* Spinlock definition for pid hash table */
+static DEFINE_SPINLOCK(rmnet_pid_ht_splock);
 
 #define RMNET_GENL_SEC_TO_MSEC(x)   ((x) * 1000)
 #define RMNET_GENL_SEC_TO_NSEC(x)   ((x) * 1000000000)
