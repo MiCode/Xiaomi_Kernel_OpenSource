c99d8347ac775a65ac494d346347200405526b3f
Manu Gautam <mgautam@codeaurora.org>
2019-12-12 14:59:57 +0530

    USB: gadget: qti: Add qti_usb gadget
    Configfs based gadget relies on userspace to specify configfs
    attributes in order to enable USB composition. This causes USB
    enumeration to not happen until userspace comes up.
    However, if there is no dependency on userspace then there
    is no reason to use configfs and by having an in-kernel USB
    gadget we can get USB enumerated much early.
    
    Change-Id: I5feec52ff45c82a166fab73d0d1c586254c7025f
    Signed-off-by: Manu Gautam <mgautam@codeaurora.org>

--- include/linux/usb/composite.h
+++ include/linux/usb/composite.h
@@ -587,6 +587,9 @@ struct usb_composite_overwrite {
 void usb_composite_overwrite_options(struct usb_composite_dev *cdev,
 		struct usb_composite_overwrite *covr);
 
+int composite_dev_prepare(struct usb_composite_driver *composite,
+		struct usb_composite_dev *dev);
+
 static inline u16 get_default_bcdDevice(void)
 {
 	u16 bcdDevice;
