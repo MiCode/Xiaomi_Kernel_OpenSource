a1ac0339c1703a0e7bfd9b18a4afed96dfde31d6
Shane Chien <Shane.Chien@mediatek.com>
2019-12-12 10:38:27 +0800

    [ALPS04925456] ASoC: Enable aaudio_ion kcontrol
    enable aaudio_ion kcontrol for AAudio.
    
    MTK-Commit-Id: 8db64f2f2df7214ab93aea8663e16d100beceed6
    
    Change-Id: I314edd9f413882e60fbce5186bf24e987897a24c
    Signed-off-by: Shane Chien <Shane.Chien@mediatek.com>
    CR-Id: ALPS04925456
    Feature: Phone Sound

--- sound/soc/mediatek/mt6873/mt6873-afe-pcm.c
+++ sound/soc/mediatek/mt6873/mt6873-afe-pcm.c
@@ -1438,10 +1438,7 @@ static int mt6873_mmap_ion_get(struct snd_kcontrol *kcontrol,
 static int mt6873_mmap_ion_set(struct snd_kcontrol *kcontrol,
 			       struct snd_ctl_elem_value *ucontrol)
 {
-	/* for bring up */
-#if 0
 	mtk_get_ion_buffer();
-#endif
 	return 0;
 }
 
