6fb3184f6ef2a92b4cdd53925d19dc094b23cdf0
zhichao.liu <zhichao.liu@mediatek.com>
2019-12-13 08:17:38 +0800

    [ALPS04892265] Sensor: Regulate the Use of SPIN_LOCK
    Take the blocking function"copy_to_user()"
    out of the spin lock.
    
    MTK-Commit-Id: 4d67aeef3da0fd255db7cbeccbd72fe2ffb249ac
    
    Change-Id: Ie6e51bb31b1b8626655a656024c140feb47666e2
    Signed-off-by: zhichao.liu <zhichao.liu@mediatek.com>
    CR-Id: ALPS04892265
    Feature: Sensor Hub

--- drivers/misc/mediatek/sensors-1.0/hwmon/sensor_list/sensor_list.c
+++ drivers/misc/mediatek/sensors-1.0/hwmon/sensor_list/sensor_list.c
@@ -188,6 +188,8 @@ static ssize_t
 sensorlist_read(struct file *file, char __user *buf,
 	size_t count, loff_t *ptr)
 {
+	struct sensorlist_info_t temp[maxhandle];
+
 	if (!atomic_read(&first_ready_after_boot))
 		return -EINVAL;
 	if (count == 0)
@@ -197,12 +199,12 @@ sensorlist_read(struct file *file, char __user *buf,
 	if (count > maxhandle * sizeof(struct sensorlist_info_t))
 		count = maxhandle * sizeof(struct sensorlist_info_t);
 
+	memset(temp, 0, sizeof(temp));
 	spin_lock(&sensorlist_info_lock);
-	if (copy_to_user(buf, sensorlist_info, count)) {
-		spin_unlock(&sensorlist_info_lock);
-		return -EFAULT;
-	}
+	memcpy(temp, sensorlist_info, sizeof(temp));
 	spin_unlock(&sensorlist_info_lock);
+	if (copy_to_user(buf, temp, count))
+		return -EFAULT;
 	return count;
 }
 
