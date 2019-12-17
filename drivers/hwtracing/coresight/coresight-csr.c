b018a011f2dc792ecb18134299e575214b019c89
Tao,Zhang <taozha@codeaurora.org>
2019-12-17 16:11:59 +0800

    coresight: csr: Set EOT on periodic flush
    Set EOT on periodic flush for lagoon long periodic flush time issue
    workaround. It could make USB flush in low rate not see a big
    lantency.
    
    Change-Id: I768fb04317b184212ccae4e2912b5c538ac970de
    Signed-off-by: Tao,Zhang <taozha@codeaurora.org>
    Signed-off-by: Mao Jinlong <jinlmao@codeaurora.org>

--- drivers/hwtracing/coresight/coresight-csr.c
+++ drivers/hwtracing/coresight/coresight-csr.c
@@ -70,7 +70,9 @@ do {									\
 #define BLKSIZE_1024		2
 #define BLKSIZE_2048		3
 
+#define FLUSHPERIOD_1	    0x1
 #define FLUSHPERIOD_2048	0x800
+#define PERFLSHEOT_BIT		BIT(18)
 
 #define CSR_ATID_REG_OFFSET(atid, atid_offset) \
 		((atid / 32) * 4 + atid_offset)
@@ -94,6 +96,7 @@ struct csr_drvdata {
 	struct clk		*clk;
 	spinlock_t		spin_lock;
 	bool			usb_bam_support;
+	bool			perflsheot_set_support;
 	bool			hwctrl_set_support;
 	bool			set_byte_cntr_support;
 	bool			timestamp_support;
@@ -170,6 +173,8 @@ void msm_qdss_csr_enable_flush(struct coresight_csr *csr)
 
 	usbflshctrl = csr_readl(drvdata, CSR_USBFLSHCTRL);
 	usbflshctrl |= 0x2;
+	if (drvdata->perflsheot_set_support)
+		usbflshctrl |= PERFLSHEOT_BIT;
 	csr_writel(drvdata, usbflshctrl, CSR_USBFLSHCTRL);
 
 	CSR_LOCK(drvdata);
@@ -222,6 +227,8 @@ void msm_qdss_csr_disable_flush(struct coresight_csr *csr)
 
 	usbflshctrl = csr_readl(drvdata, CSR_USBFLSHCTRL);
 	usbflshctrl &= ~0x2;
+	if (drvdata->perflsheot_set_support)
+		usbflshctrl &= ~PERFLSHEOT_BIT;
 	csr_writel(drvdata, usbflshctrl, CSR_USBFLSHCTRL);
 
 	CSR_LOCK(drvdata);
@@ -642,7 +649,16 @@ static int csr_probe(struct platform_device *pdev)
 	else
 		dev_dbg(dev, "timestamp_support operation supported\n");
 
-	if (drvdata->usb_bam_support)
+	drvdata->perflsheot_set_support = of_property_read_bool(
+			pdev->dev.of_node, "qcom,perflsheot-set-support");
+	if (!drvdata->perflsheot_set_support)
+		dev_dbg(dev, "perflsheot_set_support handled by other subsystem\n");
+	else
+		dev_dbg(dev, "perflsheot_set_support operation supported\n");
+
+	if (drvdata->perflsheot_set_support)
+		drvdata->flushperiod = FLUSHPERIOD_1;
+	else if (drvdata->usb_bam_support)
 		drvdata->flushperiod = FLUSHPERIOD_2048;
 	drvdata->msr_support = of_property_read_bool(pdev->dev.of_node,
 						"qcom,msr-support");
