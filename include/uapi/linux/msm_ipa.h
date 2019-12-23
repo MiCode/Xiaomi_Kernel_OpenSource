db3244b7c1fb6a3aafbe8b8b5bd0415d56f4eb07
Perry Randise <prandise@codeaurora.org>
2019-12-23 10:42:03 -0500

    msm: ipa3: IPA clock vote ioctl
    The ability to vote IPA clock from user space is now required. Given
    this, voting for IPA clock can now be done via an ioctl. This header
    file supports the ioctl and can be independently propagated.
    
    Change-Id: I742a7950f9fc53f17cb6f210f2b7f06ade842011
    CRs-Fixed: 2592003
    Signed-off-by: Perry Randise <prandise@codeaurora.org>

--- include/uapi/linux/msm_ipa.h
+++ include/uapi/linux/msm_ipa.h
@@ -124,6 +124,7 @@
 #define IPA_IOCTL_FNR_COUNTER_QUERY             76
 #define IPA_IOCTL_GET_NAT_IN_SRAM_INFO          77
 #define IPA_IOCTL_GET_PHERIPHERAL_EP_INFO       78
+#define IPA_IOCTL_APP_CLOCK_VOTE                79
 
 /**
  * max size of the header to be inserted
@@ -2885,6 +2886,10 @@ struct ipa_odl_modem_config {
 				IPA_IOCTL_GET_PHERIPHERAL_EP_INFO, \
 				struct ipa_ioc_get_ep_info)
 
+#define IPA_IOC_APP_CLOCK_VOTE _IOWR(IPA_IOC_MAGIC, \
+				IPA_IOCTL_APP_CLOCK_VOTE, \
+				uint32_t)
+
 /*
  * unique magic number of the Tethering bridge ioctls
  */
@@ -2988,6 +2993,18 @@ struct ipa_nat_in_sram_info {
 	uint32_t sram_mem_available_for_nat;
 	uint32_t nat_table_offset_into_mmap;
 	uint32_t best_nat_in_sram_size_rqst;
+};
+
+/**
+ * enum ipa_app_clock_vote_type
+ *
+ * The types of votes that can be accepted by the
+ * IPA_IOC_APP_CLOCK_VOTE ioctl
+ */
+enum ipa_app_clock_vote_type {
+	IPA_APP_CLK_DEVOTE     = 0,
+	IPA_APP_CLK_VOTE       = 1,
+	IPA_APP_CLK_RESET_VOTE = 2,
 };
 
 #define TETH_BRIDGE_IOC_SET_BRIDGE_MODE _IOW(TETH_BRIDGE_IOC_MAGIC, \
