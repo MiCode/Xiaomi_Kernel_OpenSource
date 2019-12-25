79480db1b59991236aae593a998fdeea13f34b74
rogercl.yang <rogercl.yang@mediatek.com>
2019-12-25 14:39:51 +0800

    [ALPS04938473] CPUDVFS: add size range check for opp log
    add size range check for opp log.
    
    MTK-Commit-Id: 2199a1f4f2460392e6f67c1b34f2c83659719358
    
    Change-Id: I8959de30b7c74fd6bd6d4d57f42273eef43e2dda
    CR-Id: ALPS04938473
    Signed-off-by: rogercl.yang <rogercl.yang@mediatek.com>
    Feature: DVFS (Dynamic Voltage and Frequency Scaling)

--- drivers/misc/mediatek/base/power/cpufreq_v1/src/mtk_cpufreq_hybrid.c
+++ drivers/misc/mediatek/base/power/cpufreq_v1/src/mtk_cpufreq_hybrid.c
@@ -170,13 +170,17 @@ int Ripi_cpu_dvfs_thread(void *data)
 
 		/* tag_pr_info("Info: CPU DVFS thread received i=%d\n", i); */
 		spin_lock_irqsave(&cpudvfs_lock, flags);
-		memcpy(pwdata, cpufreq_buf, sizeof(pwdata));
+		if ((cpufreq_buf[0] < (OFFS_LOG_E - OFFS_LOG_S)) &&
+		    (cpufreq_buf[1] < (OFFS_LOG_E - OFFS_LOG_S)))
+			memcpy(pwdata, cpufreq_buf, sizeof(pwdata));
 		spin_unlock_irqrestore(&cpudvfs_lock, flags);
 
 		bk_log_offs = pwdata[0];
 		num_log = 0;
 
-		while ((bk_log_offs != pwdata[1]) &&
+		while ((pwdata[0] < (OFFS_LOG_E - OFFS_LOG_S)) &&
+			(pwdata[1] < (OFFS_LOG_E - OFFS_LOG_S)) &&
+			(bk_log_offs != pwdata[1]) &&
 			(num_log < MAX_LOG_FETCH)) {
 			buf[0] = csram_read(bk_log_offs);
 			bk_log_offs += 4;
