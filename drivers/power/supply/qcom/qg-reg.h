54dda8cfdc65d2be38b4b948ffffe3b655aa073c
Anirudh Ghayal <aghayal@codeaurora.org>
2020-01-06 09:27:23 +0530

    power: qpnp-qg: Report average battery-current
    Use the CURRENT_AVG power-supply property to report
    average battery current.
    
    Change-Id: I38c4dc78efde74ed790725fab7b0203c0be6a641
    Signed-off-by: Anirudh Ghayal <aghayal@codeaurora.org>

--- drivers/power/supply/qcom/qg-reg.h
+++ drivers/power/supply/qcom/qg-reg.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018-2019 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2018-2020 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -97,6 +97,7 @@
 #define QG_POST_ESR_I_DATA0_REG			0x7E
 
 #define QG_S2_NORMAL_AVG_V_DATA0_REG		0x80
+#define QG_S2_NORMAL_AVG_I_DATA0_REG		0x82
 
 #define QG_V_ACCUM_DATA0_RT_REG			0x88
 #define QG_I_ACCUM_DATA0_RT_REG			0x8B
