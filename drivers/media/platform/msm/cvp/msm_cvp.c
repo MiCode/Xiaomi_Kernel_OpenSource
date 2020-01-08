cf0fede7f7a998ac273eae6ff6352e56177e628d
Ruofei Ma <ruofeim@codeaurora.org>
2020-01-07 18:10:40 -0800

    msm: cvp: Invalidate output buffer cache for DME
    Invalidate cache for output buffer upon receiving DME
    message from CVP firmware.
    
    Change-Id: Ibbd917ae41993fa3b336deb14504ded950d53c32
    Signed-off-by: Ruofei Ma <ruofeim@codeaurora.org>

--- drivers/media/platform/msm/cvp/msm_cvp.c
+++ drivers/media/platform/msm/cvp/msm_cvp.c
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2018-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.
  */
 
 #include "msm_cvp.h"
 #include "cvp_hfi.h"
 #include <synx_api.h>
 #include "cvp_core_hfi.h"
+#include "cvp_hfi_helper.h"
 
 struct cvp_power_level {
 	unsigned long core_sum;
