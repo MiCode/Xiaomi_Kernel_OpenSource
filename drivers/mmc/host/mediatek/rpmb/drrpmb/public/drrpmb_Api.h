76987a652d067292ca810ed3687b4972b71198e3
Huan Tang <huan.tang@mediatek.com>
2020-01-09 17:14:04 +0800

    [ALPS04939295] msdc:change some ffu/rpmb file license
    change some ffu/rpmb file license:
    ffu.h->same with internet
    others->mtk license
    
    MTK-Commit-Id: 1f880c93ab7361841ec60e180e734bd74dba964f
    
    Change-Id: Ibf638b102b4bc8e79790a225269e4cb8cb4183ac
    Signed-off-by: Huan Tang <huan.tang@mediatek.com>
    CR-Id: ALPS04939295
    Feature: eMMC Boot Up

--- drivers/mmc/host/mediatek/rpmb/drrpmb/public/drrpmb_Api.h
+++ drivers/mmc/host/mediatek/rpmb/drrpmb/public/drrpmb_Api.h
@@ -1,10 +1,9 @@
 /*
- * Copyright (c) 2013-2016 TRUSTONIC LIMITED
- * All Rights Reserved.
+ * Copyright (C) 2019 MediaTek Inc.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
--- include/linux/mmc/ffu.h
+++ include/linux/mmc/ffu.h
@@ -1,14 +1,18 @@
 /*
- * Copyright (C) 2015 MediaTek Inc.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ *  ffu.h
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * Copyright (C) 2013 SanDisk Corp.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program was created by SanDisk Corp
+ * The ffu.h file is obtained under the GPL v2.0 license that is
+ * available via http://www.gnu.org/licenses/,
+ * or http://www.opensource.org/licenses/gpl-2.0.php
  */
 
 #if !defined(_FFU_H_)
