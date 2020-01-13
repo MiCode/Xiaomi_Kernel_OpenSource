2ff9c2fd795b043dd78fdb853c78f848b50b7dff
Yue Ma <yuem@codeaurora.org>
2020-01-13 15:13:43 +0800

    cnss2: Set WLAON_QFPROM_PWR_CTRL_REG during power on and off
    After device power on and link config, clear VDD4BLOW_EN bit
    of WLAON_QFPROM_PWR_CTRL_REG register to disable OTP write.
    Before device link suspend and power off, clear VDD4BLOW_EN bit
    and set SHUTDOWN_EN bit. Also add a 1ms sleep after writing
    WLAON_QFPROM_PWR_CTRL_REG register.
    
    Add a DTSI entry to control above operations.
    
    Change-Id: Icb307a85bb5d283b78bdd7615f9a4757867c8075
    CRs-Fixed: 2601850
    Signed-off-by: Yue Ma <yuem@codeaurora.org>

--- drivers/net/wireless/cnss2/main.c
+++ drivers/net/wireless/cnss2/main.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016-2019, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2016-2020, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1944,6 +1944,17 @@ static void cnss_init_control_params(struct cnss_plat_data *plat_priv)
 	plat_priv->ctrl_params.bdf_type = CNSS_BDF_TYPE_DEFAULT;
 }
 
+static void cnss_get_wlaon_pwr_ctrl_info(struct cnss_plat_data *plat_priv)
+{
+	struct device *dev = &plat_priv->plat_dev->dev;
+
+	plat_priv->set_wlaon_pwr_ctrl =
+		of_property_read_bool(dev->of_node, "qcom,set-wlaon-pwr-ctrl");
+
+	cnss_pr_dbg("set_wlaon_pwr_ctrl is %d\n",
+		    plat_priv->set_wlaon_pwr_ctrl);
+}
+
 static const struct platform_device_id cnss_platform_id_table[] = {
 	{ .name = "qca6174", .driver_data = QCA6174_DEVICE_ID, },
 	{ .name = "qca6290", .driver_data = QCA6290_DEVICE_ID, },
@@ -2052,6 +2063,7 @@ static int cnss_probe(struct platform_device *plat_dev)
 	platform_set_drvdata(plat_dev, plat_priv);
 	INIT_LIST_HEAD(&plat_priv->vreg_list);
 
+	cnss_get_wlaon_pwr_ctrl_info(plat_priv);
 	cnss_init_control_params(plat_priv);
 
 	ret = cnss_get_resources(plat_priv);
