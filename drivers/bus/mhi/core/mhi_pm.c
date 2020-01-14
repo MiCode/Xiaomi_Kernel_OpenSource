99325dd225a90d5d10396cc86c588364f0d0bfea
Bhaumik Bhatt <bbhatt@codeaurora.org>
2020-01-13 20:05:42 -0800

    mhi: core: enable doorbell method for time synchronization
    Some devices support doorbell method to collect time
    synchronization information. Add support from host for the
    updated way of handling initialization and advertising
    support.
    
    Change-Id: I5bc82b946fd64927a2a88babc8d383e5fa664cf6
    Signed-off-by: Bhaumik Bhatt <bbhatt@codeaurora.org>

--- drivers/bus/mhi/core/mhi_pm.c
+++ drivers/bus/mhi/core/mhi_pm.c
@@ -520,9 +520,8 @@ static int mhi_pm_mission_mode_transition(struct mhi_controller *mhi_cntrl)
 
 	read_unlock_bh(&mhi_cntrl->pm_lock);
 
-	/* setup support for additional features (SFR, timesync, etc.) */
+	/* setup support for additional features */
 	mhi_init_sfr(mhi_cntrl);
-	mhi_init_timesync(mhi_cntrl);
 
 	if (MHI_REG_ACCESS_VALID(mhi_cntrl->pm_state))
 		mhi_timesync_log(mhi_cntrl);
@@ -656,9 +655,6 @@ static void mhi_pm_disable_transition(struct mhi_controller *mhi_cntrl,
 				  sfr_info->dma_addr);
 		sfr_info->buf_addr = NULL;
 	}
-
-	/* remove support for time sync */
-	mhi_destroy_timesync(mhi_cntrl);
 
 	mutex_lock(&mhi_cntrl->pm_mutex);
 
