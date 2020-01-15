6832a47d82550e5c2a2e789a53863aadf404d234
Bhaumik Bhatt <bbhatt@codeaurora.org>
2020-01-15 11:42:09 -0800

    mhi: cntrl: qcom: use RC driver APIs to toggle low power modes
    PCIe root complex driver has exposed set of APIs to allow or
    prevent link level low power modes. Use those instead of
    touching device side registers to toggle the same.
    
    Change-Id: Idf4f6d60a80742524249dacd7a10fe5184acec95
    Signed-off-by: Bhaumik Bhatt <bbhatt@codeaurora.org>

--- drivers/bus/mhi/controllers/mhi_qcom.h
+++ drivers/bus/mhi/controllers/mhi_qcom.h
@@ -58,10 +58,6 @@ struct mhi_dev {
 	/* hardware info */
 	u32 serial_num;
 	u32 oem_pk_hash[MHI_BHI_OEMPKHASH_SEG];
-
-	unsigned int lpm_disable_depth;
-	/* lock to toggle low power modes */
-	spinlock_t lpm_lock;
 };
 
 void mhi_deinit_pci_dev(struct mhi_controller *mhi_cntrl);
@@ -71,6 +67,8 @@ void mhi_reg_write_work(struct work_struct *w);
 
 #ifdef CONFIG_ARCH_QCOM
 
+int mhi_arch_link_lpm_disable(struct mhi_controller *mhi_cntrl);
+int mhi_arch_link_lpm_enable(struct mhi_controller *mhi_cntrl);
 void mhi_arch_mission_mode_enter(struct mhi_controller *mhi_cntrl);
 int mhi_arch_power_up(struct mhi_controller *mhi_cntrl);
 int mhi_arch_pcie_init(struct mhi_controller *mhi_cntrl);
@@ -106,6 +104,16 @@ static inline int mhi_arch_power_up(struct mhi_controller *mhi_cntrl)
 
 static inline void mhi_arch_mission_mode_enter(struct mhi_controller *mhi_cntrl)
 {
+}
+
+static inline int mhi_arch_link_lpm_disable(struct mhi_controller *mhi_cntrl)
+{
+	return 0;
+}
+
+static inline int mhi_arch_link_lpm_enable(struct mhi_controller *mhi_cntrl)
+{
+	return 0;
 }
 
 #endif
