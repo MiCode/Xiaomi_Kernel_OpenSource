75ca302dd30cf26424664d77931bf9d490c76786
Hemant Kumar <hemantk@codeaurora.org>
2020-01-20 15:28:24 -0800

    mhi: controller: Enable L1 when mhi is not active
    offload worker checks if mhi is active before doing the reg
    write. If mhi is not active it returns without enabling L1.
    This causes prevent_l1 counter to go out of sync. Fix this
    by enabling L1 before returning from offload worker function.
    
    Change-Id: I8a356acca3d8b8a7b4ff4d95e7378a056a2b06cf
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- drivers/bus/mhi/controllers/mhi_arch_qcom.c
+++ drivers/bus/mhi/controllers/mhi_arch_qcom.c
@@ -67,12 +67,15 @@ void mhi_reg_write_work(struct work_struct *w)
 	if (!info->valid)
 		return;
 
-	if (mhi_is_active(mhi_cntrl->mhi_dev) && msm_pcie_prevent_l1(pci_dev))
+	if (!mhi_is_active(mhi_cntrl->mhi_dev))
+		return;
+
+	if (msm_pcie_prevent_l1(pci_dev))
 		return;
 
 	while (info->valid) {
 		if (!mhi_is_active(mhi_cntrl->mhi_dev))
-			return;
+			break;
 
 		writel_relaxed(info->val, info->reg_addr);
 		info->valid = false;
