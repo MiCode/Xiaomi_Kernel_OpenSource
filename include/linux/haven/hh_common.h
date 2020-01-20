77c2c03606a69ea2e336a390f46876faf5d8b80d
Elliot Berman <eberman@codeaurora.org>
2020-01-20 09:22:19 -0800

    haven: Add common Haven types and macros
    Add common Haven API types and return values.
    
    Change-Id: I89de261dbb090ee54f0b943bf34cca782a24ce1e
    Signed-off-by: Elliot Berman <eberman@codeaurora.org>
    Signed-off-by: Raghavendra Rao Ananta <rananta@codeaurora.org>

---
+++ include/linux/haven/hh_common.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ *
+ */
+
+#ifndef __HH_COMMON_H
+#define __HH_COMMON_H
+
+/* Common Haven types */
+typedef u16 hh_vmid_t;
+typedef u32 hh_rm_msgid_t;
+typedef u32 hh_virq_handle_t;
+typedef u32 hh_label_t;
+typedef u64 hh_capid_t;
+typedef u64 hh_dbl_flags_t;
+
+#endif
