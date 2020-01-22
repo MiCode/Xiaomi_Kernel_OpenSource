8651ace66afe91cb52417a3472cefac0d6603773
Blagovest Kolenichev <bkolenichev@codeaurora.org>
2020-01-22 05:14:53 -0800

    Revert "UPSTREAM: cpufreq: qcom-hw: Move driver initialization earlier"
    This reverts commit 9df826d676ecb5015a4422e27b588e3472e0aa17.
    
    This is a preparation change for merging android-5.4.6 into
    msm-5.4 branch. Equivalent logic is already committed via:
    
    ed7c3ccb4a cpufreq: qcom: Add snapshot of cpufreq-hw
    
    Change-Id: Ia3e5dc004c85885053328e845f7dd4fe0f568da6
    Signed-off-by: Blagovest Kolenichev <bkolenichev@codeaurora.org>

--- drivers/cpufreq/qcom-cpufreq-hw.c
+++ drivers/cpufreq/qcom-cpufreq-hw.c
@@ -334,7 +334,7 @@ static int __init qcom_cpufreq_hw_init(void)
 {
 	return platform_driver_register(&qcom_cpufreq_hw_driver);
 }
-postcore_initcall(qcom_cpufreq_hw_init);
+device_initcall(qcom_cpufreq_hw_init);
 
 static void __exit qcom_cpufreq_hw_exit(void)
 {
