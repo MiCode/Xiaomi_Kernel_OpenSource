e0084dfcc7e3d761b14afc1eb41b5c37eb7fe286
Michael Adisumarta <madisuma@codeaurora.org>
2020-02-04 12:00:42 -0800

    msm: ipahal: add uC activation registers and ucp structs
    Add relevant structs and functions to modify the
    address registers for NAT uC and conntrack UC
    for external, local, and shared activation tables.
    Add ucp fields to ipv4 nat and ipv6ct entries.
    
    Change-Id: Iadb3232fb45052b643bbf36bc48043df8619d2fb
    Signed-off-by: Michael Adisumarta <madisuma@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipahal/ipahal_nat_i.h
+++ drivers/platform/msm/ipa/ipa_v3/ipahal/ipahal_nat_i.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2018, 2020, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -25,7 +25,7 @@
  * |Target Port(2B) |Private Port(2B)| Public Port(2B) | Next Index(2B)  |
  * -----------------------------------------------------------------------
  * |Proto|      TimeStamp(3B)        |     Flags(2B)   |IP check sum Diff|
- * |(1B) |                           |EN|Redirect|Resv |        (2B)     |
+ * |(1B) |                           |                 |        (2B)     |
  * -----------------------------------------------------------------------
  * |TCP/UDP checksum|  PDN info(2B)  |    SW Specific Parameters(4B)     |
  * |    diff (2B)   |Info|Resv       |index table entry|  prev index     |
@@ -42,17 +42,45 @@ struct ipa_nat_hw_ipv4_entry {
 	u32 target_port : 16;
 	u32 ip_chksum : 16;
 
-	u32 rsvd1 : 14;
+	/*---------------------------------------------------
+	 *IPA NAT Flag is interpreted as follows
+	 *---------------------------------------------------
+	 *|  EN   |FIN/RST|  S   | IPv4 uC activation index |
+	 *| [15]  | [14]  | [13] |          [12:0]          |
+	 *---------------------------------------------------
+	 */
+	u32 uc_activation_index: 13;
+	u32 s : 1;
 	u32 redirect : 1;
 	u32 enable : 1;
 
 	u32 time_stamp : 24;
 	u32 protocol : 8;
 
+	/*--------------------------------------------------
+	 *32 bit sw_spec_params is interpreted as follows
+	 *------------------------------------
+	 *|     16 bits     |     16 bits    |
+	 *------------------------------------
+	 *|  index table    |  prev index    |
+	 *|     entry       |                |
+	 *------------------------------------
+	 */
 	u32 prev_index : 16;
 	u32 indx_tbl_entry : 16;
 
-	u32 rsvd2 : 12;
+	u32 rsvd2 : 11; //including next 3 reserved buts
+
+	/*-----------------------------------------
+	 *8 bit PDN info is interpreted as following
+	 *-----------------------------------------------------
+	 *|     4 bits      |     1 bit      |     3 bits     |
+	 *-----------------------------------------------------
+	 *|  PDN index      |  uC processing |     Reserved   |
+	 *|      [7:4]      |       [3]      |      [2:0]     |
+	 *-----------------------------------------------------
+	 */
+	u32 ucp : 1; /* IPA 4.0 and greater */
 	u32 pdn_index : 4; /* IPA 4.0 and greater */
 
 	u32 tcp_udp_chksum : 16;
@@ -97,7 +125,7 @@ struct ipa_nat_hw_pdn_entry {
 
 /*-------------------------  IPV6CT Table Entry  ------------------------------
  *-----------------------------------------------------------------------------
- *|   7    |      6      |  5  |  4   |        3         |  2  |   1  |   0   |
+ *|   7    |      6      |  5  |  4   |      3      |   2   |   1    |   0    |
  *-----------------------------------------------------------------------------
  *|                   Outbound Src IPv6 Address (8 LSB Bytes)                 |
  *-----------------------------------------------------------------------------
@@ -107,17 +135,23 @@ struct ipa_nat_hw_pdn_entry {
  *-----------------------------------------------------------------------------
  *|                   Outbound Dest IPv6 Address (8 MSB Bytes)                |
  *-----------------------------------------------------------------------------
- *|Protocol|      TimeStamp (3B)      |       Flags (2B)       |Reserved (2B) |
- *|  (1B)  |                          |Enable|Redirect|Resv    |              |
+ *|Protocol|      TimeStamp (3B)      |       Flags (2B)    |Rsvd   |S |uC ACT|
+ *|  (1B)  |                          |Enable|Redirect|Resv |[15:14]|13|[12:0]|
  *-----------------------------------------------------------------------------
- *|Reserved|Direction(1B)|Src Port(2B)|     Dest Port (2B)     |Next Index(2B)|
- *|  (1B)  |IN|OUT|Resv  |            |                        |              |
+ *|Reserved|Settings|    Src Port(2B) |   Dest Port (2B)    |  Next Index(2B) |
+ *|  (1B)  |  (1B)  |                 |                     |                 |
  *-----------------------------------------------------------------------------
  *|    SW Specific Parameters(4B)     |                Reserved (4B)          |
  *|    Prev Index (2B)   |Reserved(2B)|                                       |
  *-----------------------------------------------------------------------------
  *|                            Reserved (8B)                                  |
  *-----------------------------------------------------------------------------
+ *
+ * Settings
+ *-----------------------------------------------
+ *|IN Allowed|OUT Allowed|Reserved|uC processing|
+ *|[7:7]     |[6:6]      |[5:1]   |[0:0]        |
+ *-----------------------------------------------
  */
 struct ipa_nat_hw_ipv6ct_entry {
 	/* An IP address can't be bit-field, because its address is used */
@@ -126,7 +160,9 @@ struct ipa_nat_hw_ipv6ct_entry {
 	u64 dest_ipv6_lsb;
 	u64 dest_ipv6_msb;
 
-	u64 rsvd1 : 30;
+	u64 uc_activation_index : 13;
+	u64 s : 1;
+	u64 rsvd1 : 16;
 	u64 redirect : 1;
 	u64 enable : 1;
 
@@ -136,7 +172,8 @@ struct ipa_nat_hw_ipv6ct_entry {
 	u64 next_index : 16;
 	u64 dest_port : 16;
 	u64 src_port : 16;
-	u64 rsvd2 : 6;
+	u64 ucp : 1;
+	u64 rsvd2 : 5;
 	u64 out_allowed : 1;
 	u64 in_allowed : 1;
 	u64 rsvd3 : 8;
