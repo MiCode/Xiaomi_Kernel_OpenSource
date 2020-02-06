f38869ea5132125fe8dae3640373973a96fa6e87
Josh Hsiao <josh.hsiao@mediatek.com>
2020-02-06 15:07:43 +0800

    [ALPS04969196] apusys: power table
    [Detail]
    update power table
    
    MTK-Commit-Id: 21d1c419548cfd1904f0abca2cbd64ffdc67af6f
    
    Change-Id: I6c93ca2ad5df301fe0ee42dd038b9d0d78b16286
    Signed-off-by: Josh Hsiao <josh.hsiao@mediatek.com>
    CR-Id: ALPS04969196
    Feature: MDLA Platform

--- drivers/misc/mediatek/apusys/power/2.0/mt6873/apu_power_table.c
+++ drivers/misc/mediatek/apusys/power/2.0/mt6873/apu_power_table.c
@@ -14,26 +14,26 @@
 #include <linux/module.h>
 #include "apu_power_table.h"
 
-// FIXME: update vpu power table in DVT stage //[Fix me]
+// FIXME: update vpu power table in DVT stage
 /* opp, mW */
 struct apu_opp_info vpu_power_table[APU_OPP_NUM] = {
-	{APU_OPP_0, 242},
-	{APU_OPP_1, 188},
-	{APU_OPP_2, 142},
-	{APU_OPP_3, 136},
-	{APU_OPP_4, 127},
-	{APU_OPP_5, 100},
+	{APU_OPP_0, 226},
+	{APU_OPP_1, 226},
+	{APU_OPP_2, 176},
+	{APU_OPP_3, 133},
+	{APU_OPP_4, 98},
+	{APU_OPP_5, 44},
 };
 EXPORT_SYMBOL(vpu_power_table);
 
-// FIXME: update mdla power table in DVT stage //[Fix me]
+// FIXME: update mdla power table in DVT stage
 /* opp, mW */
 struct apu_opp_info mdla_power_table[APU_OPP_NUM] = {
-	{APU_OPP_0, 200},
-	{APU_OPP_1, 200},
-	{APU_OPP_2, 159},
-	{APU_OPP_3, 157},
-	{APU_OPP_4, 117},
-	{APU_OPP_5, 110},
+	{APU_OPP_0, 161},
+	{APU_OPP_1, 161},
+	{APU_OPP_2, 161},
+	{APU_OPP_3, 120},
+	{APU_OPP_4, 86},
+	{APU_OPP_5, 44},
 };
 EXPORT_SYMBOL(mdla_power_table);
