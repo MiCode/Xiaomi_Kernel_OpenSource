2b97fa103f22a5d7f34c36ad5646919906c20f89
Cloud Chou <cloud.chou@mediatek.com>
2020-02-06 15:08:34 +0800

    [ALPS04965313] v4l2: change call mmqos and clock sequence
    [Detail]
    Hold clock ref in Enable clock than call mmqos
    
    MTK-Commit-Id: 82e6de9f764b2deb61502d2f68c9dafba50b0360
    
    Change-Id: I357a790dc458e34eabae4712d99038776d80f231
    CR-Id: ALPS04965313
    Feature: [Module]Video Driver
    Signed-off-by: Cloud Chou <cloud.chou@mediatek.com>

--- drivers/media/platform/mtk-vcodec/vdec_drv_if.c
+++ drivers/media/platform/mtk-vcodec/vdec_drv_if.c
@@ -199,12 +199,13 @@ void vdec_decode_prepare(void *ctx_prepare,
 
 	mtk_vdec_pmqos_prelock(ctx, hw_id);
 	ret = mtk_vdec_lock(ctx, hw_id);
-	mtk_vdec_pmqos_begin_frame(ctx, hw_id);
 
 	mtk_vcodec_set_curr_ctx(ctx->dev, ctx, hw_id);
 	mtk_vcodec_dec_clock_on(&ctx->dev->pm, hw_id);
 	if (ret == 0)
 		enable_irq(ctx->dev->dec_irq[hw_id]);
+
+	mtk_vdec_pmqos_begin_frame(ctx, hw_id);
 }
 EXPORT_SYMBOL_GPL(vdec_decode_prepare);
 
@@ -221,12 +222,12 @@ void vdec_decode_unprepare(void *ctx_unprepare,
 			hw_id, ctx->dev->dec_sem[hw_id].count);
 		return;
 	}
+	mtk_vdec_pmqos_end_frame(ctx, hw_id);
 
 	disable_irq(ctx->dev->dec_irq[hw_id]);
 	mtk_vcodec_dec_clock_off(&ctx->dev->pm, hw_id);
 	mtk_vcodec_set_curr_ctx(ctx->dev, NULL, hw_id);
 
-	mtk_vdec_pmqos_end_frame(ctx, hw_id);
 	mtk_vdec_unlock(ctx, hw_id);
 }
 EXPORT_SYMBOL_GPL(vdec_decode_unprepare);
