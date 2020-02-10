8ce77bb7a3387fc54bc20f87260c16d64366763d
Lipsa Rout <lrout@codeaurora.org>
2020-02-10 19:50:56 +0800

    drm/msm/dsi-staging: Fix porch calculation issue for constant fps
    For constant fps feature, porch is calculated based on supported
    clk rates. Currently, data type of local variables used for porch
    calculation is u32 which leads to incorrect porch calculation for
    higher clk rates. So, update the data type to u64.
    
    Change-Id: I8eb04487d1dcce05989448c0b063e56752af412b
    Signed-off-by: Lipsa Rout <lrout@codeaurora.org>
    Signed-off-by: Yuan Zhao <yzhao@codeaurora.org>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -6149,7 +6149,7 @@ static void dsi_display_adjust_mode_timing(
 				    struct dsi_display_mode *dsi_mode,
 				    int lanes, int bpp)
 {
-	u32 new_htotal, new_vtotal, htotal, vtotal, old_htotal;
+	u64 new_htotal, new_vtotal, htotal, vtotal, old_htotal, div;
 
 	if (!dyn_clk_caps->maintain_const_fps)
 		return;
@@ -6164,8 +6164,9 @@ static void dsi_display_adjust_mode_timing(
 	case DSI_DYN_CLK_TYPE_CONST_FPS_ADJUST_HFP:
 		vtotal = DSI_V_TOTAL(&dsi_mode->timing);
 		old_htotal = DSI_H_TOTAL_DSC(&dsi_mode->timing);
-		new_htotal = (dsi_mode->timing.clk_rate_hz * lanes);
-		new_htotal /= (bpp * vtotal * dsi_mode->timing.refresh_rate);
+		new_htotal = dsi_mode->timing.clk_rate_hz * lanes;
+		div = bpp * vtotal * dsi_mode->timing.refresh_rate;
+		do_div(new_htotal, div);
 		if (old_htotal > new_htotal)
 			dsi_mode->timing.h_front_porch -=
 				(old_htotal - new_htotal);
@@ -6176,8 +6177,9 @@ static void dsi_display_adjust_mode_timing(
 
 	case DSI_DYN_CLK_TYPE_CONST_FPS_ADJUST_VFP:
 		htotal = DSI_H_TOTAL_DSC(&dsi_mode->timing);
-		new_vtotal = (dsi_mode->timing.clk_rate_hz * lanes);
-		new_vtotal /= (bpp * htotal * dsi_mode->timing.refresh_rate);
+		new_vtotal = dsi_mode->timing.clk_rate_hz * lanes;
+		div = bpp * htotal * dsi_mode->timing.refresh_rate;
+		do_div(new_vtotal, div);
 		dsi_mode->timing.v_front_porch = new_vtotal -
 			dsi_mode->timing.v_back_porch -
 			dsi_mode->timing.v_sync_width -
