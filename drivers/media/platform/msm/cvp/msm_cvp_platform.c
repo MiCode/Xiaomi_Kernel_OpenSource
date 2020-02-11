bf518628e97cd98f61474f5e4bd713b1e059103d
George Shen <sqiao@codeaurora.org>
2020-02-11 10:05:49 -0800

    msm: cvp: Enable CVP auto PIL
    The command in init.qcom.rc to load CVP firmware will take
    effect with this change.
    
    Change-Id: Ib45c77c8f34a12a9c01b462847cf2fc25ae1452d
    Signed-off-by: George Shen <sqiao@codeaurora.org>

--- drivers/media/platform/msm/cvp/msm_cvp_platform.c
+++ drivers/media/platform/msm/cvp/msm_cvp_platform.c
@@ -100,7 +100,7 @@ static struct msm_cvp_common_data sm8250_common_data[] = {
 static struct msm_cvp_common_data sm8350_common_data[] = {
 	{
 		.key = "qcom,auto-pil",
-		.value = 0,
+		.value = 1,
 	},
 	{
 		.key = "qcom,never-unload-fw",
