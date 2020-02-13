b03daf57bc0cf33579158358e7a25636d02e1178
Udipto Goswami <ugoswami@codeaurora.org>
2020-02-13 12:26:51 +0530

    usb: gadget: Prevent use after free in qdss connect & close
    Dwc3 gadget controller driver can still access usb_request
    after dequeue() returns due to async cancellation.
    Hence usb_request shouldn't be freed after calling
    usb_ep_dequeue() which can otherwise cause use after free.
    
    Fix this by allocating & de-allocating from bind and unbind
    instead of data_connection & close.
    
    Change-Id: I8155ecbebcf762fc87c17841c72c53c6dca2e4a9
    Signed-off-by: Udipto Goswami <ugoswami@codeaurora.org>
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>

--- drivers/usb/gadget/function/u_qdss.c
+++ drivers/usb/gadget/function/u_qdss.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2020, The Linux Foundation. All rights reserved.
  */
 
 #include <linux/kernel.h>
@@ -10,7 +10,7 @@
 #include <linux/usb/dwc3-msm.h>
 
 #include "f_qdss.h"
-static int alloc_sps_req(struct usb_ep *data_ep)
+int alloc_sps_req(struct usb_ep *data_ep)
 {
 	struct usb_request *req = NULL;
 	struct f_qdss *qdss = data_ep->driver_data;
@@ -95,7 +95,6 @@ int set_qdss_data_connection(struct f_qdss *qdss, int enable)
 				&bam_info.usb_bam_pipe_idx,
 				NULL, bam_info.data_fifo, NULL);
 
-		alloc_sps_req(qdss->port.data);
 		msm_data_fifo_config(qdss->port.data,
 			bam_info.data_fifo->iova,
 			bam_info.data_fifo->size,
