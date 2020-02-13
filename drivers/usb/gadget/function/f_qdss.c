135c6d8061effd08ec8bfa2c1e35bec81808030f
Udipto Goswami <ugoswami@codeaurora.org>
2020-02-13 12:26:51 +0530

    usb: gadget: Prevent use after free in qdss connect & close
    Dwc3 gadget controller driver can still access usb_request
    after dequeue() returns due to async cancellation.
    Hence usb_request shouldn't be freed after calling
    usb_ep_dequeue() which can otherwise cause use after free.
    
    Fix this by allocating & de-allocating from bind and unbind
    instead of data_connection & close.
    
    Change-Id: I8155ecbebcf762fc87c17841c72c53c6dca2e4a9
    Signed-off-by: Udipto Goswami <ugoswami@codeaurora.org>

--- drivers/usb/gadget/function/f_qdss.c
+++ drivers/usb/gadget/function/f_qdss.c
@@ -2,7 +2,7 @@
 /*
  * f_qdss.c -- QDSS function Driver
  *
- * Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2020, The Linux Foundation. All rights reserved.
  */
 
 #include <linux/init.h>
@@ -440,7 +440,7 @@ static int qdss_bind(struct usb_configuration *c, struct usb_function *f)
 		&qdss_data_ep_comp_desc);
 	if (!ep) {
 		pr_err("%s: ep_autoconfig error\n", __func__);
-		goto fail;
+		goto clear_ep;
 	}
 	qdss->port.data = ep;
 	ep->driver_data = qdss;
@@ -450,8 +450,9 @@ static int qdss_bind(struct usb_configuration *c, struct usb_function *f)
 			&qdss_ctrl_in_ep_comp_desc);
 		if (!ep) {
 			pr_err("%s: ep_autoconfig error\n", __func__);
-			goto fail;
+			goto clear_ep;
 		}
+
 		qdss->port.ctrl_in = ep;
 		ep->driver_data = qdss;
 
@@ -459,10 +460,19 @@ static int qdss_bind(struct usb_configuration *c, struct usb_function *f)
 			&qdss_ctrl_out_ep_comp_desc);
 		if (!ep) {
 			pr_err("%s: ep_autoconfig error\n", __func__);
-			goto fail;
+			goto clear_ep;
 		}
 		qdss->port.ctrl_out = ep;
 		ep->driver_data = qdss;
+	}
+
+	if (!strcmp(qdss->ch.name, USB_QDSS_CH_MSM)) {
+		ret = alloc_sps_req(qdss->port.data);
+		if (ret) {
+			pr_err("%s: alloc_sps_req error (%d)\n",
+							__func__, ret);
+			goto clear_ep;
+		}
 	}
 
 	/*update fs descriptors*/
@@ -497,9 +507,18 @@ static int qdss_bind(struct usb_configuration *c, struct usb_function *f)
 		goto fail;
 
 	return 0;
+
 fail:
+	/* check if usb_request allocated */
+	if (qdss->endless_req) {
+		usb_ep_free_request(qdss->port.data,
+				qdss->endless_req);
+		qdss->endless_req = NULL;
+	}
+
+clear_ep:
 	clear_eps(f);
-	clear_desc(gadget, f);
+
 	return -ENOTSUPP;
 }
 
@@ -512,6 +531,12 @@ static void qdss_unbind(struct usb_configuration *c, struct usb_function *f)
 	pr_debug("%s\n", __func__);
 
 	flush_workqueue(qdss->wq);
+
+	if (qdss->endless_req) {
+		usb_ep_free_request(qdss->port.data,
+				qdss->endless_req);
+		qdss->endless_req = NULL;
+	}
 
 	/* Reset string ids */
 	qdss_string_defs[QDSS_DATA_IDX].id = 0;
@@ -1025,11 +1050,6 @@ close:
 		spin_unlock_irqrestore(&qdss_lock, flags);
 		usb_ep_dequeue(qdss->port.data, qdss->endless_req);
 		spin_lock_irqsave(&qdss_lock, flags);
-		if (qdss->endless_req) {
-			usb_ep_free_request(qdss->port.data,
-					qdss->endless_req);
-			qdss->endless_req = NULL;
-		}
 	}
 	gadget = qdss->gadget;
 	ch->app_conn = 0;
