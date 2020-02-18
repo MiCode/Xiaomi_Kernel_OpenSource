c78f3997b12346ac20cff5301001ebfd0c48dff7
Lakshmi Narayana Kalavala <lkalaval@codeaurora.org>
2020-02-18 17:18:04 +0530

    drm/msm/sde: Decrese PPB Dither strength
    While playing HDR, PPB dither is stronger than we expected.
    Change configuration so that the strength is normal.
    
    Change-Id: Iae179a8c6eb0b75508284e29f1c0ef4f85e80846
    Signed-off-by: Lakshmi Narayana Kalavala <lkalaval@codeaurora.org>
    Signed-off-by: Nirmal Abraham <nabrah@codeaurora.org>

--- drivers/gpu/drm/msm/sde/sde_hw_pingpong.c
+++ drivers/gpu/drm/msm/sde/sde_hw_pingpong.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015-2019, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2015-2020, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -42,7 +42,7 @@
 
 #define DITHER_DEPTH_MAP_INDEX 9
 static u32 dither_depth_map[DITHER_DEPTH_MAP_INDEX] = {
-	0, 0, 0, 0, 0, 0, 0, 1, 2
+	0, 0, 0, 0, 0, 1, 2, 3, 3
 };
 
 #define MERGE_3D_MODE 0x004
