f4b8aa2156ed7a7e30a7903dcdf022f1c54d2634
Tatenda Chipeperekwa <tatendac@codeaurora.org>
2020-02-20 14:06:16 -0800

    dt-bindings: clock: update DisplayPort clock names for Lahaina
    Update the clock names for use in the DisplayPort PLL module.
    
    Change-Id: I8f7228d8612343f8c3c649937500e85f76814769
    Signed-off-by: Tatenda Chipeperekwa <tatendac@codeaurora.org>

--- include/dt-bindings/clock/mdss-5nm-pll-clk.h
+++ include/dt-bindings/clock/mdss-5nm-pll-clk.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2019-2020, The Linux Foundation. All rights reserved.
  */
 
 #ifndef __MDSS_5NM_PLL_CLK_H
@@ -50,5 +50,5 @@
 #define DP_VCO_DIVIDED_TWO_CLK_SRC	2
 #define DP_VCO_DIVIDED_FOUR_CLK_SRC	3
 #define DP_VCO_DIVIDED_SIX_CLK_SRC	4
-#define DP_VCO_DIVIDED_CLK_SRC_MUX	5
+#define DP_PHY_PLL_VCO_DIV_CLK		5
 #endif
