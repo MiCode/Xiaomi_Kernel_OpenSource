e0d015a1af9bf14d4da64e0ece49d33bfdf8e3ad
Saravana Kannan <saravanak@google.com>
2020-02-21 00:05:10 -0800

    UPSTREAM: driver core: Skip unnecessary work when device doesn't have sync_state()
    A bunch of busy work is done for devices that don't have sync_state()
    support. Stop doing the busy work.
    
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Link: https://lore.kernel.org/r/20200221080510.197337-4-saravanak@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 77036165d8bcf7c7b2a2df28a601ec2c52bb172d)
    Bug: 151865289
    Change-Id: Id513185be93c036150ffd7156104f898fec70c11
    Git-commit: a27d26af7d122f04734a31e16aefbc56de68ae29
    Git-repo: https://android.googlesource.com/kernel/common
    Signed-off-by: Blagovest Kolenichev <bkolenichev@codeaurora.org>

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -722,6 +722,8 @@ static void __device_links_queue_sync_state(struct device *dev,
 {
 	struct device_link *link;
 
+	if (!dev_has_sync_state(dev))
+		return;
 	if (dev->state_synced)
 		return;
 
@@ -823,7 +825,7 @@ late_initcall(sync_state_resume_initcall);
 
 static void __device_links_supplier_defer_sync(struct device *sup)
 {
-	if (list_empty(&sup->links.defer_sync))
+	if (list_empty(&sup->links.defer_sync) && dev_has_sync_state(sup))
 		list_add_tail(&sup->links.defer_sync, &deferred_sync);
 }
 
