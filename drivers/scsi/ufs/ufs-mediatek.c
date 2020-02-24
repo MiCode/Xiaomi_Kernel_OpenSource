c6059ac63293dfc9d709534fdbbb1d847a713c94
Greg Kroah-Hartman <gregkh@google.com>
2020-02-24 16:14:50 +0100

    ANDROID: fix merge issue in 5.4.22
    ufs-mediatek.c ended up duplicating the ufs_mtk_apply_dev_quirks()
    function, which caused build breaks.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Ia391fc93e7c5a54005e1c7df4280211b269000cd

--- drivers/scsi/ufs/ufs-mediatek.c
+++ drivers/scsi/ufs/ufs-mediatek.c
@@ -483,15 +483,6 @@ static int ufs_mtk_apply_dev_quirks(struct ufs_hba *hba,
 	return 0;
 }
 
-static int ufs_mtk_apply_dev_quirks(struct ufs_hba *hba,
-				    struct ufs_dev_desc *card)
-{
-	if (card->wmanufacturerid == UFS_VENDOR_SAMSUNG)
-		ufshcd_dme_set(hba, UIC_ARG_MIB(PA_TACTIVATE), 6);
-
-	return 0;
-}
-
 /**
  * struct ufs_hba_mtk_vops - UFS MTK specific variant operations
  *
