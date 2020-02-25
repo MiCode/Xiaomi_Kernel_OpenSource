ab7289b1a4b1a4c09913fcb46f1165954095aff1
Armaan Siddiqui <asiddiqu@codeaurora.org>
2020-02-25 18:26:25 +0530

    msm: ipa: Fix compilation errors when CONFIG_DEBUG_FS is disable
    Adding changes to fix compiltaion errors when
    flag CONFIG_DEBUG_FS is disable.
    
    Change-Id: I44254a896f5b392625188fafbf47d5acaffaa27b
    Signed-off-by: Armaan Siddiqui <asiddiqu@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_clients/ipa_mhi_client.c
+++ drivers/platform/msm/ipa/ipa_clients/ipa_mhi_client.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2015-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2015-2020, The Linux Foundation. All rights reserved.
  */
 
 #include <linux/debugfs.h>
@@ -147,11 +147,6 @@ struct ipa_mhi_client_ctx {
 static struct ipa_mhi_client_ctx *ipa_mhi_client_ctx;
 static DEFINE_MUTEX(mhi_client_general_mutex);
 
-#ifdef CONFIG_DEBUG_FS
-#define IPA_MHI_MAX_MSG_LEN 512
-static char dbg_buff[IPA_MHI_MAX_MSG_LEN];
-static struct dentry *dent;
-
 static char *ipa_mhi_channel_state_str[] = {
 	__stringify(IPA_HW_MHI_CHANNEL_STATE_DISABLE),
 	__stringify(IPA_HW_MHI_CHANNEL_STATE_ENABLE),
@@ -261,6 +256,11 @@ fail_dma_enable:
 	dma_free_coherent(pdev, mem.size, mem.base, mem.phys_base);
 	return res;
 }
+
+#ifdef CONFIG_DEBUG_FS
+#define IPA_MHI_MAX_MSG_LEN 512
+static char dbg_buff[IPA_MHI_MAX_MSG_LEN];
+static struct dentry *dent;
 
 static int ipa_mhi_print_channel_info(struct ipa_mhi_channel_ctx *channel,
 	char *buff, int len)
@@ -2184,10 +2184,12 @@ int ipa_mhi_destroy_all_channels(void)
 	return 0;
 }
 
+#ifdef CONFIG_DEBUG_FS
 static void ipa_mhi_debugfs_destroy(void)
 {
 	debugfs_remove_recursive(dent);
 }
+#endif
 
 static void ipa_mhi_deregister_pm(void)
 {
