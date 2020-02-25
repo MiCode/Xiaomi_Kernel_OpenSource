7e10ca7b4405eaa7705c706cb8e2e1855b46ab9f
Chris Goldsworthy <cgoldswo@codeaurora.org>
2020-02-25 09:36:04 -0800

    mm/oom-kill: Remove obsolete IS_ENABLED check
    CONFIG_HAVE_LOW_MEMORY_KILLER is no longer present on 5.4, so remove a
    check to see if it's enabled or not.
    
    Change-Id: Ifcd88a0e29b7da6dede7f98fcb4c99ffc7bf3c02
    Signed-off-by: Chris Goldsworthy <cgoldswo@codeaurora.org>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -1158,8 +1158,7 @@ void pagefault_out_of_memory(void)
 		.order = 0,
 	};
 
-	if (IS_ENABLED(CONFIG_HAVE_LOW_MEMORY_KILLER) ||
-	    IS_ENABLED(CONFIG_HAVE_USERSPACE_LOW_MEMORY_KILLER))
+	if (IS_ENABLED(CONFIG_HAVE_USERSPACE_LOW_MEMORY_KILLER))
 		return;
 
 	if (mem_cgroup_oom_synchronize(true))
