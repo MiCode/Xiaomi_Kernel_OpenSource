5c8becaa65863358ad8f6901b381743b7853bbf5
Chao Yu <yuchao0@huawei.com>
2020-02-27 19:30:05 +0800

    f2fs: fix inconsistent comments
    Lack of maintenance on comments may mislead developers, fix them.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -111,7 +111,6 @@ static int f2fs_vm_page_mkwrite(struct vm_fault *vmf)
 		}
 	}
 
-	/* fill the page */
 	f2fs_wait_on_page_writeback(page, DATA, false, true);
 
 	/* wait for GCed page writeback via META_MAPPING */
