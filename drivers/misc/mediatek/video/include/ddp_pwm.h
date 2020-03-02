dcfce1a10da028d83ad365cc936517c3010687e0
Josh-YC Liu <josh-yc.liu@mediatek.com>
2020-03-03 00:23:02 +0800

    [ALPS04090556] AAL: kernel driver migration
    PQ, AAL and disp_pwm driver kernel-4.14 migration.
    
    MTK-Commit-Id: 08b61c051881d3e9e475dad8cdd50e83f64304e1
    
    Change-Id: I817139a38b08e273737e6a6cd6d637d10fbd99e0
    Signed-off-by: Josh-YC Liu <josh-yc.liu@mediatek.com>
    CR-Id: ALPS04090556
    Feature: Ambient Light Adaptive Luma
    (cherry-pick from 75cdfefa7f5a51598770f264321ca39c0fd6be3e)

---
+++ drivers/misc/mediatek/video/include/ddp_pwm.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __DDP_PWM_H__
+#define __DDP_PWM_H__
+
+
+enum disp_pwm_id_t {
+	DISP_PWM0 = 0x1,
+	DISP_PWM1 = 0x2,
+	DISP_PWM_ALL = (DISP_PWM0 | DISP_PWM1)
+};
+
+void disp_pwm_set_main(enum disp_pwm_id_t main);
+enum disp_pwm_id_t disp_pwm_get_main(void);
+
+int disp_pwm_is_enabled(enum disp_pwm_id_t id);
+
+int disp_pwm_set_backlight(enum disp_pwm_id_t id, int level_1024);
+int disp_pwm_set_backlight_cmdq(enum disp_pwm_id_t id,
+	int level_1024, void *cmdq);
+
+int disp_pwm_set_max_backlight(enum disp_pwm_id_t id, unsigned int level_1024);
+int disp_pwm_get_max_backlight(enum disp_pwm_id_t id);
+
+/* For backward compatible */
+int disp_bls_set_max_backlight(unsigned int level_1024);
+#ifdef CONFIG_MTK_FB_DUMMY
+int disp_bls_set_backlight(int level_1024) { return 0; }
+#else
+int disp_bls_set_backlight(int level_1024);
+#endif
+bool disp_pwm_is_osc(void);
+void disp_pwm_test(const char *cmd, char *debug_output);
+
+#endif
