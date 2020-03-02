3cb504dfb6554fd932b168f6466fb96ea5af2172
Ming-Fan Chen <ming-fan.chen@mediatek.com>
2020-03-03 04:05:26 +0800

    [ALPS04313905] cmdq: update mailbox support for secure and ut
    update mailbox support for secure and unit test
    
    MTK-Commit-Id: 4fbecd8d71f76d3be07021c87fe6b818a0dca657
    
    Change-Id: I0cab137801afe3fc4de050909ec19d071a3fdb24
    CR-Id: ALPS04313905
    Feature: [Module]CMDQ Driver
    Signed-off-by: Ming-Fan Chen <ming-fan.chen@mediatek.com>

--- drivers/soc/mediatek/mtk-cmdq-helper.c
+++ drivers/soc/mediatek/mtk-cmdq-helper.c
@@ -1182,6 +1182,14 @@ s32 cmdq_pkt_finalize(struct cmdq_pkt *pkt)
 	if (cmdq_pkt_is_finalized(pkt))
 		return 0;
 
+#if IS_ENABLED(CONFIG_MTK_TEE_GP_SUPPORT)
+	if (pkt->sec_data) {
+		err = cmdq_sec_insert_backup_cookie(pkt);
+		if (err)
+			return err;
+	}
+#endif
+
 	/* insert EOC and generate IRQ for each command iteration */
 	err = cmdq_pkt_append_command(pkt, CMDQ_GET_ARG_C(CMDQ_EOC_IRQ_EN),
 		CMDQ_GET_ARG_B(CMDQ_EOC_IRQ_EN), 0, 0, 0, 0, 0, CMDQ_CODE_EOC);
