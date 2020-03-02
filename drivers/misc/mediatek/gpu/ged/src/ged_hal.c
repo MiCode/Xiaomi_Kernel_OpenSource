8119fb1a50525a902f81c1b25847c0ac48e77ac5
Figo Wang <figo.wang@mediatek.com>
2020-03-03 04:10:15 +0800

    [ALPS04762487] GED: Initialized the variables
    Initialized the variables.
    
    MTK-Commit-Id: 95e4646d0666cd7f1b463b1c0c15da05d0b99bf4
    
    Change-Id: If898325d31465ac55aae8cb70673350390bffd10
    Signed-off-by: Figo Wang <figo.wang@mediatek.com>
    CR-Id: ALPS04762487
    Feature: OpenGL|ES

--- drivers/misc/mediatek/gpu/ged/src/ged_hal.c
+++ drivers/misc/mediatek/gpu/ged/src/ged_hal.c
@@ -616,7 +616,7 @@ static ssize_t ged_gpu_info_write_entry(
 	int aint32Indx[NUM_TOKEN];
 	char *pcCMD;
 	char *pcValue;
-	int value;
+	int value = 0;
 	int i;
 
 	if (uiCount >= GED_HAL_DEBUGFS_SIZE)
@@ -624,6 +624,9 @@ static ssize_t ged_gpu_info_write_entry(
 
 	if (ged_copy_from_user(acBuffer, pszBuffer, uiCount))
 		goto normal_exit;
+
+	for (i = 0; i < NUM_TOKEN; i++)
+		aint32Indx[i] = 0;
 
 	acBuffer[uiCount] = '\0';
 	i = tokenizer(acBuffer, uiCount, aint32Indx, NUM_TOKEN);
