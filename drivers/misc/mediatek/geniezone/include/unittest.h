f716db3133eae01148dfae12c4a3fc9c77adb996
PeiLun Suei <peilun.suei@mediatek.com>
2020-03-03 04:06:34 +0800

    [ALPS04705331] gz: fix coverity check error
    [Detail]
    (1) fix coverity check error for gz_chmem_ut.c
    
    MTK-Commit-Id: ef549d3e1b9a25c224d440401978bc6f46e9e70f
    
    Change-Id: If5639b8a7838c2c4f04f9111acb12b88affacbc7
    CR-Id: ALPS04705331
    Feature: GenieZone
    Signed-off-by: PeiLun Suei <peilun.suei@mediatek.com>

--- drivers/misc/mediatek/geniezone/include/unittest.h
+++ drivers/misc/mediatek/geniezone/include/unittest.h
@@ -11,7 +11,7 @@
  * GNU General Public License for more details.
  */
 
-#define UT_LOG(fmt...) pr_debug("[GZTEST]" fmt)
+#define UT_LOG(fmt...) pr_info("[GZTEST]" fmt)
 
 #define INIT_UNITTESTS                                                         \
 	static uint _uts_total;                                                \
@@ -50,15 +50,6 @@
 		else                                                           \
 			UT_LOG("%s: FAILED\n", _test);                         \
 	}
-
-#define TEST_END_NO_PRT							\
-{									\
-	if (_success)                                                   \
-		_success = true;					\
-	else                                                            \
-		_success = false;					\
-	strcpy(_test, _test);						\
-}
 /*
  * CHECK_* macros to check test results.
  */
