2cf0e153c2280cc4a6b1ccf5f2019d07efd1368e
guanglai guo <guanglai.guo@mediatek.com>
2020-03-03 04:04:13 +0800

    [ALPS04649865] CCCI: remove the illegal RD operation of INFRA_AO WO Reg
    remove the read operation of infra ao register 0x150 & 0x154,
    because these two register is WO register.
    
    MTK-Commit-Id: 562ea7690ea7ae6484c5cb21f144df23e43b3acc
    
    Change-Id: I6d005d5d07bf6e80d989628a398c4930b65729a9
    Signed-off-by: guanglai guo <guanglai.guo@mediatek.com>
    CR-Id: ALPS04649865
    Feature: Modem Interface Driver

--- drivers/misc/mediatek/eccci/mt6785/ccci_platform.c
+++ drivers/misc/mediatek/eccci/mt6785/ccci_platform.c
@@ -144,7 +144,6 @@ void ccci_reset_ccif_hw(unsigned char md_id,
 
 	{
 		int reset_bit = -1;
-		unsigned int reg_value;
 
 		switch (ccif_id) {
 		case AP_MD1_CCIF:
@@ -160,15 +159,9 @@ void ccci_reset_ccif_hw(unsigned char md_id,
 		 *CCIF's busy/wch/irq, but not SRAM
 		 */
 		/*set reset bit*/
-		reg_value = ccci_read32(infra_ao_base, 0x150);
-		reg_value &= ~(1 << reset_bit);
-		reg_value |= (1 << reset_bit);
-		ccci_write32(infra_ao_base, 0x150, reg_value);
+		ccci_write32(infra_ao_base, 0x150, 1 << reset_bit);
 		/*clear reset bit*/
-		reg_value = ccci_read32(infra_ao_base, 0x154);
-		reg_value &= ~(1 << reset_bit);
-		reg_value |= (1 << reset_bit);
-		ccci_write32(infra_ao_base, 0x154, reg_value);
+		ccci_write32(infra_ao_base, 0x154, 1 << reset_bit);
 	}
 
 	/* clear SRAM */
