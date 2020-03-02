55e68c2deda99f9bacb47af96c3e2e0d9e49e840
Tzu-Hsien Wang <tzuhsien.wang@mediatek.com>
2020-03-03 02:00:16 +0800

    [ALPS03938808] GPU: fix build errors on 32-bits project
    [Detail]
    Fix build errors on 32-bits project
    
    MTK-Commit-Id: 6505077046fb1f63b1765d7714161ec5b8f48592
    
    Change-Id: I45639bad87cf9d74de04821114e32d141a0fef1e
    Signed-off-by: Tzu-Hsien Wang <tzuhsien.wang@mediatek.com>
    CR-Id: ALPS03938808
    Feature: OpenGL|ES
    (cherry-pick from e1b1ac432c9841f606f4a35eb93fb0e4ca395b1a)

--- drivers/misc/mediatek/base/power/mt6771/mtk_gpu_log.h
+++ drivers/misc/mediatek/base/power/mt6771/mtk_gpu_log.h
@@ -19,11 +19,16 @@
 
 extern GED_LOG_BUF_HANDLE _mtk_gpu_log_hnd;
 
-//#define GPULOG(fmt, ...) ged_log_buf_print2(_mtk_gpu_log_hnd, GED_LOG_ATTR_TIME, fmt, ##__VA_ARGS__)
-//#define GPULOG2(fmt, ...) do { \
-//	ged_log_buf_print2(_mtk_gpu_log_hnd, GED_LOG_ATTR_TIME, fmt, ##__VA_ARGS__); \
-//	gpufreq_pr_info(fmt "\n", ##__VA_ARGS__); \
-//} while (0)
+#if 0
+#define GPULOG(fmt, ...) \
+	ged_log_buf_print2(_mtk_gpu_log_hnd, \
+		GED_LOG_ATTR_TIME, fmt, ##__VA_ARGS__)
+#define GPULOG2(fmt, ...) do { \
+	ged_log_buf_print2(_mtk_gpu_log_hnd, \
+		GED_LOG_ATTR_TIME, fmt, ##__VA_ARGS__); \
+	gpufreq_pr_info(fmt "\n", ##__VA_ARGS__); \
+} while (0)
+#endif
 
 #define GPULOG(fmt, ...) do { } while (0)
 #define GPULOG2(fmt, ...) do { } while (0)
