496e8a6207c6dc41aa34a7f39683a26fcf8d907c
Gray Jia <gray.jia@mediatek.com>
2020-03-03 04:05:38 +0800

    [ALPS04674058] msdc:fix sdcard hotplug bug
     [Details]
     sync from 2434560 :
     When sd card hot-plug during data transfer,
     IO will hang in block layer.
    
    MTK-Commit-Id: dcecb949f39b833956e5820616d2363bfc3071c9
    
    Change-Id: I05cb78197f7994be0868a4d209a9bef734a5e2eb
    Signed-off-by: Gray Jia <gray.jia@mediatek.com>
    CR-Id: ALPS04674058
    Feature: [Android Default] SDCard
    (cherry picked from commit 13ac5f1f5b9304f9d7d0ecda6ccab41481184796)

--- drivers/mmc/core/queue.c
+++ drivers/mmc/core/queue.c
@@ -643,6 +643,9 @@ void mmc_cleanup_queue(struct mmc_queue *mq)
 	blk_start_queue(q);
 	spin_unlock_irqrestore(q->queue_lock, flags);
 
+	if (likely(!blk_queue_dead(q)))
+		blk_cleanup_queue(q);
+
 	mq->card = NULL;
 }
 EXPORT_SYMBOL(mmc_cleanup_queue);
