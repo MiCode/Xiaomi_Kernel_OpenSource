c2c3155e167c101f65850e8cb5bf04fc6a9c8db7
Wilma wu <wilma.wu@mediatek.com>
2020-03-03 00:12:28 +0800

    [ALPS04058673] RTC: porting mt6358 rtc driver.
    porting mt6358 rtc driver.
    
    MTK-Commit-Id: be6330f91a5dd6abcf4aa15d40d4d194fed217bb
    
    Change-Id: I99de3720007d047a94ce1345cb3f6cef70badda6
    Signed-off-by: Wilma wu <wilma.wu@mediatek.com>
    CR-Id: ALPS04058673
    Feature: RTC-Power Drop Auto Reboot

--- drivers/misc/mediatek/include/mt-plat/mtk_rtc.h
+++ drivers/misc/mediatek/include/mt-plat/mtk_rtc.h
@@ -14,9 +14,7 @@
 #ifndef MTK_RTC_H
 #define MTK_RTC_H
 
-#include <linux/ioctl.h>
 #include <linux/rtc.h>
-#include <linux/types.h>
 
 enum rtc_gpio_user_t {
 	RTC_GPIO_USER_WIFI = 8,
@@ -34,55 +32,29 @@ enum rtc_gpio_user_t {
  * by some user even if the phone is powered off
  */
 
-extern unsigned long rtc_read_hw_time(void);
 extern void rtc_gpio_enable_32k(enum rtc_gpio_user_t user);
 extern void rtc_gpio_disable_32k(enum rtc_gpio_user_t user);
-extern bool rtc_gpio_32k_status(void);
-
-/* for AUDIOPLL (deprecated) */
-extern void rtc_enable_abb_32k(void);
-extern void rtc_disable_abb_32k(void);
-
-/* NOTE: used in Sleep driver to workaround Vrtc-Vore level shifter issue */
-extern void rtc_enable_writeif(void);
-extern void rtc_disable_writeif(void);
-
 extern void rtc_mark_recovery(void);
 extern void rtc_mark_kpoc(void);
 extern void rtc_mark_fast(void);
-extern u16 rtc_rdwr_uart_bits(u16 *val);
-extern void rtc_bbpu_power_down(void);
 extern void rtc_read_pwron_alarm(struct rtc_wkalrm *alm);
 extern int get_rtc_spare_fg_value(void);
 extern int set_rtc_spare_fg_value(int val);
-extern void rtc_irq_handler(void);
+extern int get_rtc_spare0_fg_value(void);
+extern int set_rtc_spare0_fg_value(int val);
 extern bool crystal_exist_status(void);
-extern void mt_power_off(void);
-#else/*ifdef CONFIG_MTK_RTC*/
-#define rtc_read_hw_time()              ({ 0; })
+#else
 #define rtc_gpio_enable_32k(user)	({ 0; })
 #define rtc_gpio_disable_32k(user)	({ 0; })
-#define rtc_gpio_32k_status()		({ 0; })
-#define rtc_enable_abb_32k()		({ 0; })
-#define rtc_disable_abb_32k()		({ 0; })
-#define rtc_enable_writeif()		({ 0; })
-#define rtc_disable_writeif()		({ 0; })
 #define rtc_mark_recovery()             ({ 0; })
 #define rtc_mark_kpoc()                 ({ 0; })
 #define rtc_mark_fast()		        ({ 0; })
-#define rtc_rdwr_uart_bits(val)		({ 0; })
-#define rtc_bbpu_power_down()		({ 0; })
 #define rtc_read_pwron_alarm(alm)	({ 0; })
 #define get_rtc_spare_fg_value()	({ 0; })
 #define set_rtc_spare_fg_value(val)	({ 0; })
-#define rtc_irq_handler()			({ 0; })
+#define get_rtc_spare0_fg_value()		({ 0; })
+#define set_rtc_spare0_fg_value(val)	({ 0; })
 #define crystal_exist_status()		({ 0; })
-/* __weak void mt_power_off(void); */
 #endif/*ifdef CONFIG_MTK_RTC*/
-void __attribute__((weak)) rtc_clock_enable(int enable)
-{
-}
-void __attribute__((weak)) rtc_lpsd_restore_al_mask(void)
-{
-}
+
 #endif
