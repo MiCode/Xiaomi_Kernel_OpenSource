5899a2411851ed0dffd950b9effb9b66755a04bc
Miles Chen <miles.chen@mediatek.com>
2020-03-03 02:06:38 +0800

    [ALPS04297952] mrudmp: fix build break on naked functions
    Fix the following build break with clang by remove the "__nacked"
    attribute.
    
    ../drivers/misc/mediatek/aee/mrdump/mrdump_arm.c:26:16: error:
    parameter references not allowed in naked functions
    
    MTK-Commit-Id: cd38c7a3b7691341638c79ecfcc15d0532d6ad9d
    
    Change-Id: I23c5006ec8138b23e64bbe935e422362191a8f6f
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    CR-Id: ALPS04297952
    Feature: Build System

--- drivers/misc/mediatek/aee/mrdump/mrdump_arm.c
+++ drivers/misc/mediatek/aee/mrdump/mrdump_arm.c
@@ -19,7 +19,7 @@
 #include <mt-plat/mrdump.h>
 #include "mrdump_private.h"
 
-void __naked mrdump_save_current_backtrace(struct pt_regs *regs)
+void mrdump_save_current_backtrace(struct pt_regs *regs)
 {
 	asm volatile("stmia %1, {r0 - r15}\n\t"
 		     "mrs %0, cpsr\n"
