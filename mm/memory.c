5840ba397fbbfea81b574b8400bba23af44801b4
Joel Fernandes <joelaf@google.com>
2020-03-03 04:10:36 +0800

    [ALPS04737989] [PATCH] mm: emit tracepoint when rss watermark is hit
    Useful to track how rss is changing per tgid. Required for the
    memory visibility work being done for Android.
    
    Original patch by Tim Murray:
    https://partner-android-review.googlesource.com/c/kernel/private/
    msm-google/+/1081280
    
    Changes from original patch:
    - dont bloat mm_struct
    - add some noise reduction to rss tracking
    
    Change-Id: Ief904334235ff4380244e5803d7853579e70d202
    Signed-off-by: Joel Fernandes <joelaf@google.com>
    ---
    
    MTK-Commit-Id: 04500bde64fa9a1ede4b5bf955040cde3a37baf8
    
    Change-Id: I8e5deff583ddde34134bb13ca91d6071e7c73c1c
    Signed-off-by: Chinwen Chang <chinwen.chang@mediatek.com>
    CR-Id: ALPS04737989
    Feature: Google Patch

--- mm/memory.c
+++ mm/memory.c
@@ -71,6 +71,8 @@
 #include <linux/dax.h>
 #include <linux/oom.h>
 
+#include <trace/events/kmem.h>
+
 #include <asm/io.h>
 #include <asm/mmu_context.h>
 #include <asm/pgalloc.h>
@@ -144,6 +146,21 @@ static int __init init_zero_pfn(void)
 }
 core_initcall(init_zero_pfn);
 
+/*
+ * This threshold is the boundary in the value space, that the counter has to
+ * advance before we trace it. Should be a power of 2. It is to reduce unwanted
+ * trace overhead. The counter is number of pages.
+ */
+#define TRACE_MM_COUNTER_THRESHOLD 128
+
+void mm_trace_rss_stat(int member, long count, long value)
+{
+	long thresh_mask = ~(TRACE_MM_COUNTER_THRESHOLD - 1);
+
+	/* Threshold roll-over, trace it */
+	if ((count & thresh_mask) != ((count - value) & thresh_mask))
+		trace_rss_stat(member, count);
+}
 
 #if defined(SPLIT_RSS_COUNTING)
 
