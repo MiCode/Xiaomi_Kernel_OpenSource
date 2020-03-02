b0eaf30186693ff82fb6b593b01bfcd27bfc6aa3
Minchan Kim <minchan@kernel.org>
2020-03-03 04:10:45 +0800

    [ALPS04783265] FROMLIST: mm: Enhance per process reclaim to consider shared pages
    Some pages could be shared by several processes. (ex, libc)
    In case of that, its too bad to reclaim them from the beginnig.
    
    This patch causes VM to keep them on memory until last task
    try to reclaim them so shared pages will be reclaimed only if
    all of task has gone swapping out.
    
    This feature doesnt handle non-linear mapping on ramfs because
    its very time-consuming and doesnt make sure of reclaiming and
    not common.
    
    MTK-Commit-Id: b9fba9621f17e7f282236937b139ad01fc2795bb
    
    Change-Id: I7587ef53877129b7f4b136e9e0fcc4da66d0a64a
    Signed-off-by: Chinwen Chang <chinwen.chang@mediatek.com>
    CR-Id: ALPS04783265
    Feature: Memory Optimization

--- include/linux/rmap.h
+++ include/linux/rmap.h
@@ -14,7 +14,8 @@
 
 extern int isolate_lru_page(struct page *page);
 extern void putback_lru_page(struct page *page);
-extern unsigned long reclaim_pages_from_list(struct list_head *page_list);
+extern unsigned long reclaim_pages_from_list(struct list_head *page_list,
+					     struct vm_area_struct *vma);
 
 /*
  * The anon_vma heads a list of private "related" vmas, to scan if
@@ -207,7 +208,8 @@ static inline void page_dup_rmap(struct page *page, bool compound)
 int page_referenced(struct page *, int is_locked,
 			struct mem_cgroup *memcg, unsigned long *vm_flags);
 
-bool try_to_unmap(struct page *, enum ttu_flags flags);
+bool try_to_unmap(struct page *page, enum ttu_flags flags,
+				struct vm_area_struct *vma);
 
 /* Avoid racy checks */
 #define PVMW_SYNC		(1 << 0)
@@ -273,6 +275,7 @@ int page_mapped_in_vma(struct page *page, struct vm_area_struct *vma);
  */
 struct rmap_walk_control {
 	void *arg;
+	struct vm_area_struct *target_vma;
 	/*
 	 * Return false if page table scanning in rmap_walk should be stopped.
 	 * Otherwise, return true.
@@ -301,7 +304,7 @@ static inline int page_referenced(struct page *page, int is_locked,
 	return 0;
 }
 
-#define try_to_unmap(page, refs) false
+#define try_to_unmap(page, refs, vma) false
 
 static inline int page_mkclean(struct page *page)
 {
