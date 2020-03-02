a2706c368d352476ecd3add47d35a980164e3a3f
Anmin Hsu <anmin.hsu@mediatek.com>
2020-03-03 00:04:16 +0800

    [ALPS04025608] sspm: add sspm platform driver
    [Detail]
    sspm:
    1. migrate sspm of mt6771 support
    2. disable EMI protection feature
    
    MTK-Commit-Id: 0ef5ca998b3fcbacf831458dbc3fc14bce1f6497
    
    Change-Id: I871413c48a37d35509b2cd7f642439d6e5ff15a0
    Signed-off-by: Anmin Hsu <anmin.hsu@mediatek.com>
    CR-Id: ALPS04025608
    Feature: TinySys

---
+++ drivers/misc/mediatek/sspm/sspm_reservedmem.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#ifndef __SSPM_RESERVED_H__
+#define __SSPM_RESERVED_H__
+
+#include <linux/types.h>
+
+struct sspm_reserve_mblock {
+	u32 num;
+	u64 start_phys;
+	u64 start_virt;
+	u64 size;
+};
+
+phys_addr_t sspm_reserve_mem_get_phys(unsigned int id);
+phys_addr_t sspm_reserve_mem_get_virt(unsigned int id);
+phys_addr_t sspm_reserve_mem_get_size(unsigned int id);
+int sspm_reserve_memory_init(void);
+void sspm_set_emi_mpu(phys_addr_t base, phys_addr_t size);
+void sspm_lock_emi_mpu(void);
+
+#endif
