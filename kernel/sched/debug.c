7a178f83ad297e2b1e1e25a58f0753347d441f5e
LucasWei <lucas-sr.wei@mediatek.com>
2020-03-03 02:02:04 +0800

    [ALPS04150619] sched/debug: Add AEE debug information for scheduler
    Add AEE debug information for scheduler when HWT.
    (clone format from debug.c of /proc/sched_debug)
    
    MTK-Commit-Id: 4abb62ef929d6110a4509191d0258df709b35737
    
    Change-Id: I61a6158e8b57f422fe2163335c8a78fc25bdbea4
    Signed-off-by: LucasWei <lucas-sr.wei@mediatek.com>
    CR-Id: ALPS04150619
    Feature: Energy Aware Scheduling Plus (EAS+)
    (cherry-pick from 3ac32a21ffe24a300c6c90c740025a1da3392797)

--- kernel/sched/debug.c
+++ kernel/sched/debug.c
@@ -723,7 +723,8 @@ static void print_cpu(struct seq_file *m, int cpu)
 			   cpu, freq / 1000, (freq % 1000));
 	}
 #else
-	SEQ_printf(m, "cpu#%d\n", cpu);
+	SEQ_printf(m, "cpu#%d: %s\n", cpu,
+			cpu_is_offline(cpu) ? "Offline" : "Online");
 #endif
 
 #define P(x)								\
@@ -844,9 +845,15 @@ static int sched_debug_show(struct seq_file *m, void *v)
 {
 	int cpu = (unsigned long)(v - 2);
 
-	if (cpu != -1)
+	if (cpu != -1) {
+		unsigned long flags;
+
+		/* sched: add lock */
+		read_lock_irqsave(&tasklist_lock, flags);
 		print_cpu(m, cpu);
-	else
+		read_unlock_irqrestore(&tasklist_lock, flags);
+		SEQ_printf(m, "\n");
+	} else
 		sched_debug_header(m);
 
 	return 0;
@@ -855,10 +862,13 @@ static int sched_debug_show(struct seq_file *m, void *v)
 void sysrq_sched_debug_show(void)
 {
 	int cpu;
+	unsigned long flags;
 
+	read_lock_irqsave(&tasklist_lock, flags);
 	sched_debug_header(NULL);
-	for_each_online_cpu(cpu)
+	for_each_possible_cpu(cpu)
 		print_cpu(NULL, cpu);
+	read_unlock_irqrestore(&tasklist_lock, flags);
 
 }
 
@@ -1116,3 +1126,5 @@ void proc_sched_set_task(struct task_struct *p)
 	memset(&p->se.statistics, 0, sizeof(p->se.statistics));
 #endif
 }
+
+#include "debug_aee.c"
