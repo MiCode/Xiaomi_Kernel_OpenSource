a9b2b7395da77e59ca1288d78bbdac283f0d9e02
Auto Li <auto.li@mediatek.com>
2020-03-03 04:02:33 +0800

    [ALPS04305033] CAM: Move imx576 driver folder
    Move imx576 driver folder
    
    MTK-Commit-Id: 309e330ec596c5c463f7e39c79ee7500ed53f384
    
    Change-Id: I1ae42bf4a26e7588ee02b6a57f5db7fdd6051f83
    Signed-off-by: Auto Li <auto.li@mediatek.com>
    CR-Id: ALPS04305033
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/misc/mediatek/imgsensor/src/mt6771/imx576_mipi_raw/imx576mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/mt6771/imx576_mipi_raw/imx576mipiraw_Sensor.c
@@ -2143,20 +2143,20 @@ static void hdr_write_tri_shutter(kal_uint16 le, kal_uint16 me, kal_uint16 se)
 
 }
 
-static void hdr_write_tri_gain(kal_uint16 lg, kal_uint16 mg, kal_uint16 sg)
+static void hdr_write_tri_gain(kal_uint16 lgain, kal_uint16 mg, kal_uint16 sg)
 {
 	kal_uint16 reg_lg, reg_mg, reg_sg;
 
-	if (lg < BASEGAIN || lg > 16 * BASEGAIN) {
+	if (lgain < BASEGAIN || lgain > 16 * BASEGAIN) {
 		LOG_INF("Error gain setting");
 
-		if (lg < BASEGAIN)
-			lg = BASEGAIN;
-		else if (lg > 16 * BASEGAIN)
-			lg = 16 * BASEGAIN;
+		if (lgain < BASEGAIN)
+			lgain = BASEGAIN;
+		else if (lgain > 16 * BASEGAIN)
+			lgain = 16 * BASEGAIN;
 	}
 
-	reg_lg = gain2reg(lg);
+	reg_lg = gain2reg(lgain);
 	reg_mg = gain2reg(mg);
 	reg_sg = gain2reg(sg);
 	spin_lock(&imgsensor_drv_lock);
@@ -2175,8 +2175,8 @@ static void hdr_write_tri_gain(kal_uint16 lg, kal_uint16 mg, kal_uint16 sg)
 	write_cmos_sensor_8(0x0104, 0x00);
 
 	LOG_INF(
-	"lg:0x%x, reg_lg:0x%x, sg:0x%x, reg_mg:0x%x, mg:0x%x, reg_sg:0x%x\n",
-	lg, reg_lg, mg, reg_mg, sg, reg_sg);
+	"lgain:0x%x, reg_lg:0x%x, sg:0x%x, reg_mg:0x%x, mg:0x%x, reg_sg:0x%x\n",
+	lgain, reg_lg, mg, reg_mg, sg, reg_sg);
 
 }
 
