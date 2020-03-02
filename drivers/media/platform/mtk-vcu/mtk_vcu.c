f98bd844d26bef187d2f4493a648cf63fe300926
Chia-Mao Hung <chia-mao.hung@mediatek.com>
2020-03-03 04:12:05 +0800

    [ALPS04880559] vcu: fixed VPUD killed issue
    fixed VPUD abort @ wake msg ack
    need wait VPUD open count == 0 then return
    
    MTK-Commit-Id: 6fcb54522e815528f918cb19964d9839f056c1cf
    
    Change-Id: I882c701c7eb19dc9aafa184e1e2186691088a5bb
    Signed-off-by: Chia-Mao Hung <chia-mao.hung@mediatek.com>
    CR-Id: ALPS04880559
    Feature: VP9 Decoder
    (cherry picked from commit a5950f5fa849ad7cc68c4e79313ba0aa8d0eef54)

--- drivers/media/platform/mtk-vcu/mtk_vcu.c
+++ drivers/media/platform/mtk-vcu/mtk_vcu.c
@@ -426,8 +426,8 @@ int vcu_ipi_send(struct platform_device *pdev,
 	mutex_unlock(&vcu->vcu_mutex[i]);
 
 	if (vcu_ptr->abort || ret == 0) {
-		dev_err(&pdev->dev, "vcu ipi %d ack time out !", id);
-		if (!vcu_ptr->abort && vcud_task != NULL) {
+		dev_info(&pdev->dev, "vcu ipi %d ack time out !%d", id, ret);
+		if (!vcu_ptr->abort) {
 			task_lock(vcud_task);
 			send_sig(SIGTERM, vcud_task, 0);
 			task_unlock(vcud_task);
@@ -435,6 +435,12 @@ int vcu_ipi_send(struct platform_device *pdev,
 				vcu_ptr->vpud_killed.count);
 			ret = down_interruptible(&vcu_ptr->vpud_killed);
 		}
+		if (vcu_ptr->open_cnt > 0) {
+			dev_info(vcu->dev, "wait for vpud killed %d\n",
+				vcu_ptr->vpud_killed.count);
+			ret = down_interruptible(&vcu_ptr->vpud_killed);
+		}
+		dev_info(&pdev->dev, "[VCU] vpud killed\n");
 		ret = -EIO;
 		goto end;
 	} else if (-ERESTARTSYS == ret) {
