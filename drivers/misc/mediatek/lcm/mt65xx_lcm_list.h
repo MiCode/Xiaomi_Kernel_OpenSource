759dcf2691c9a083d9b12a9a310d6f965adb266b
Chen Zhong <chen.zhong@mediatek.com>
2020-03-03 02:10:10 +0800

    [ALPS04381265] lcm: jd9365_hd720_dsi: add lcm driver
    [Detail]
    add jd9365_hd720_dsi lcm driver
    
    MTK-Commit-Id: 3a60a7d6c7835f21e3b1ad532f8986d05d53ab4a
    
    Change-Id: Ic13b3a8bb18ba943ec8ba29e408a7b56c01609f3
    Feature: [Module]Display Driver
    CR-Id: ALPS04381265
    Signed-off-by: Chen Zhong <chen.zhong@mediatek.com>

--- drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
@@ -291,6 +291,7 @@ extern struct LCM_DRIVER ili9881c_hdp_dsi_vdo_ilitek_rt5081_lcm_drv;
 extern struct LCM_DRIVER nt51021_wuxga_dsi_vdo_mt8173_lcm_drv;
 extern struct LCM_DRIVER ili9881c_hdp_dsi_vdo_ilitek_rt5081_lcm_drv_ebbg;
 extern struct LCM_DRIVER hx8394f_hd720_dsi_vdo_tianma_lcm_drv;
+extern struct LCM_DRIVER jd9365_hd720_dsi_lcm_drv;
 extern struct LCM_DRIVER nt36672_fhdp_dsi_vdo_auo_lcm_drv;
 extern struct LCM_DRIVER nt36672_fhdp_dsi_vdo_auo_laneswap_lcm_drv;
 extern struct LCM_DRIVER nt35521_hd_dsi_vdo_truly_nt50358_lcm_drv;
