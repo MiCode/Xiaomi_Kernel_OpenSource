9ed46e7bf30cca21fc71e641d461379802a46595
Stanley Chu <stanley.chu@mediatek.com>
2020-03-03 04:05:46 +0800

    [ALPS04362512] scsi: ufs: Do not reset error history during host reset
    Currently UFS error history will be reset and lost during host reset
    flow by ufschd_probe_hba().
    
    We shall not reset it and then error history can be kept as completed
    as possible to improve debugging.
    
    In addition, fix a minor display error in ufshcd_print_err_hist().
    
    MTK-Commit-Id: a3875a2baecb9ae2a6ccb3cb9681768c0f3006b9
    
    Change-Id: Id0c5ddbbe8d42e2ace435eb502a33aa853143701
    Signed-off-by: Stanley Chu <stanley.chu@mediatek.com>
    CR-Id: ALPS04362512
    Feature: UFS Booting

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -7533,19 +7533,8 @@ static void ufshcd_tune_unipro_params(struct ufs_hba *hba)
 
 static void ufshcd_clear_dbg_ufs_stats(struct ufs_hba *hba)
 {
-	int err_reg_hist_size = sizeof(struct ufs_err_reg_hist);
-
 	hba->ufs_stats.hibern8_exit_cnt = 0;
 	hba->ufs_stats.last_hibern8_exit_tstamp = ktime_set(0, 0);
-
-	memset(&hba->ufs_stats.pa_err, 0, err_reg_hist_size);
-	memset(&hba->ufs_stats.dl_err, 0, err_reg_hist_size);
-	memset(&hba->ufs_stats.nl_err, 0, err_reg_hist_size);
-	memset(&hba->ufs_stats.tl_err, 0, err_reg_hist_size);
-	memset(&hba->ufs_stats.dme_err, 0, err_reg_hist_size);
-	memset(&hba->ufs_stats.fatal_err, 0, err_reg_hist_size);
-	memset(&hba->ufs_stats.auto_hibern8_err, 0, err_reg_hist_size);
-
 	hba->req_abort_count = 0;
 }
 
