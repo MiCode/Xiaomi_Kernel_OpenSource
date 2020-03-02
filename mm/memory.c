e64ab1f50c8c564ffbb77ebee343609288ce7d0b
Chinwen Chang <chinwen.chang@mediatek.com>
2020-03-03 04:07:50 +0800

    [ALPS04721022] FROMLIST: mm: make pte_unmap_same compatible with SPF
    (from https://lore.kernel.org/patchwork/patch/906212/)
    
    pte_unmap_same() is making the assumption that the page table are still
    around because the mmap_sem is held.
    This is no more the case when running a speculative page fault and
    additional check must be made to ensure that the final page table are still
    there.
    
    This is now done by calling pte_spinlock() to check for the VMAs
    consistency while locking for the page tables.
    
    This is requiring passing a vm_fault structure to pte_unmap_same() which is
    containing all the needed parameters.
    
    As pte_spinlock() may fail in the case of a speculative page fault, if the
    VMA has been touched in our back, pte_unmap_same() should now return 3
    cases :
    	1. pte are the same (0)
    	2. pte are different (VM_FAULT_PTNOTSAME)
    	3. a VMAs changes has been detected (VM_FAULT_RETRY)
    
    The case 2 is handled by the introduction of a new VM_FAULT flag named
    VM_FAULT_PTNOTSAME which is then trapped in cow_user_page().
    If VM_FAULT_RETRY is returned, it is passed up to the callers to retry the
    page fault while holding the mmap_sem.
    
    Acked-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Laurent Dufour <ldufour@linux.vnet.ibm.com>
    
    MTK-Commit-Id: de4e83f4c53bae52742a6cf66e5952870f92cb44
    
    Change-Id: I1073443b9ade429b14f7ced0886804441f8bd221
    Signed-off-by: Chinwen Chang <chinwen.chang@mediatek.com>
    CR-Id: ALPS04721022
    Feature: Memory Optimization

--- mm/memory.c
+++ mm/memory.c
@@ -2322,21 +2322,29 @@ static inline bool pte_map_lock(struct vm_fault *vmf)
  * parts, do_swap_page must check under lock before unmapping the pte and
  * proceeding (but do_wp_page is only called after already making such a check;
  * and do_anonymous_page can safely check later on).
+ *
+ * pte_unmap_same() returns:
+ *	0			if the PTE are the same
+ *	VM_FAULT_PTNOTSAME	if the PTE are different
+ *	VM_FAULT_RETRY		if the VMA has changed in our back during
+ *				a speculative page fault handling.
  */
-static inline int pte_unmap_same(struct mm_struct *mm, pmd_t *pmd,
-				pte_t *page_table, pte_t orig_pte)
+static inline int pte_unmap_same(struct vm_fault *vmf)
 {
-	int same = 1;
+	int ret = 0;
+
 #if defined(CONFIG_SMP) || defined(CONFIG_PREEMPT)
 	if (sizeof(pte_t) > sizeof(unsigned long)) {
-		spinlock_t *ptl = pte_lockptr(mm, pmd);
-		spin_lock(ptl);
-		same = pte_same(*page_table, orig_pte);
-		spin_unlock(ptl);
+		if (pte_spinlock(vmf)) {
+			if (!pte_same(*vmf->pte, vmf->orig_pte))
+				ret = VM_FAULT_PTNOTSAME;
+			spin_unlock(vmf->ptl);
+		} else
+			ret = VM_FAULT_RETRY;
 	}
 #endif
-	pte_unmap(page_table);
-	return same;
+	pte_unmap(vmf->pte);
+	return ret;
 }
 
 static inline void cow_user_page(struct page *dst, struct page *src, unsigned long va, struct vm_area_struct *vma)
@@ -2903,14 +2911,23 @@ int do_swap_page(struct vm_fault *vmf)
 	pte_t pte;
 	int locked;
 	int exclusive = 0;
-	int ret = 0;
+	int ret;
 	bool vma_readahead = swap_use_vma_readahead();
 
 	if (vma_readahead)
 		page = swap_readahead_detect(vmf, &swap_ra);
-	if (!pte_unmap_same(vma->vm_mm, vmf->pmd, vmf->pte, vmf->orig_pte)) {
+
+	ret = pte_unmap_same(vmf);
+	if (ret) {
 		if (page)
 			put_page(page);
+		/*
+		 * If pte != orig_pte, this means another thread did the
+		 * swap operation in our back.
+		 * So nothing else to do.
+		 */
+		if (ret == VM_FAULT_PTNOTSAME)
+			ret = 0;
 		goto out;
 	}
 
