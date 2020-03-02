1ea2767c9c3e4c163f23f945a5ae099833e2d34d
rogercl.yang <rogercl.yang@mediatek.com>
2020-03-03 04:04:26 +0800

    [ALPS04578330] spm: mt6771: Enable sodi/dpidle
    Enable sodi/sodi3/dpidle for MT6771.
    
    MTK-Commit-Id: 560726468fb6d479137b8c7991a152406da53b27
    
    Change-Id: I63a11fcc991078d9a3fe13df3be460441d8f6c17
    Signed-off-by: rogercl.yang <rogercl.yang@mediatek.com>
    CR-Id: ALPS04578330
    Feature: SODI (Screen On Deep Idle)

--- drivers/misc/mediatek/base/power/spm_v4/mtk_idle_mt6771.c
+++ drivers/misc/mediatek/base/power/spm_v4/mtk_idle_mt6771.c
@@ -50,9 +50,9 @@ void __iomem  *apmixed_base_in_idle;
 
 /* Idle handler on/off */
 int idle_switch[NR_TYPES] = {
-	0,	/* dpidle switch */
-	0,	/* soidle3 switch */
-	0,	/* soidle switch */
+	1,	/* dpidle switch */
+	1,	/* soidle3 switch */
+	1,	/* soidle switch */
 	1,	/* rgidle switch */
 };
 
