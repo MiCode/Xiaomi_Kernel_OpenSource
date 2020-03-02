69af69d7a14a85b09e119dc1dcc6bdb23f365767
mtk12444 <sagy.shih@mediatek.com>
2020-03-03 04:05:29 +0800

    [ALPS04427404] EMI: refine MPU bypass flow
    1. Refine workaround flow of DEVMPU violation
    2. Refine GZ bypass flow
    
    MTK-Commit-Id: 7ead676e23870aedce8c7650f9c89fb01662db53
    
    Change-Id: I322d4783c30e81877486b628c89a3d29061adf59
    Signed-off-by: mtk12444 <sagy.shih@mediatek.com>
    CR-Id: ALPS04427404
    Feature: DRAM

--- drivers/misc/mediatek/emi/mt6785/mpu_platform.c
+++ drivers/misc/mediatek/emi/mt6785/mpu_platform.c
@@ -12,8 +12,15 @@
  */
 
 #include <linux/platform_device.h>
+#include <linux/arm-smccc.h>
+#include <mt-plat/sync_write.h>
+#include <mt-plat/mtk_io.h>
+#include <mt-plat/mtk_secure_api.h>
+#include <devmpu.h>
 
 #include "mt_emi.h"
+
+static void __iomem *CEN_EMI_BASE;
 
 enum {
 	MASTER_APM0 = 0,
@@ -53,5 +60,85 @@ void set_ap_region_permission(unsigned int apc[EMI_MPU_DGROUP_NUM])
 		NO_PROTECTION, FORBIDDEN, NO_PROTECTION, NO_PROTECTION,
 		FORBIDDEN, SEC_R_NSEC_RW, FORBIDDEN, NO_PROTECTION,
 		FORBIDDEN, FORBIDDEN, FORBIDDEN, NO_PROTECTION);
+}
+
+void bypass_init(unsigned int *init_flag)
+{
+	CEN_EMI_BASE = mt_cen_emi_base_get();
+
+	*init_flag = 1;
+}
+
+static void clear_devmpu_violation(void)
+{
+	unsigned int mpus, mput, mput_2nd;
+	unsigned int i;
+	struct arm_smccc_res smc_res;
+
+	/* clear violation status */
+	for (i = 0; i < EMI_MPU_DOMAIN_NUM; i++) {
+		/* clear region abort violation */
+		mt_reg_sync_writel(0xFFFFFFFF, EMI_MPUD_ST(i));
+		/* clear out-of-range violation */
+		mt_reg_sync_writel(0x3, EMI_MPUD_ST2(i));
+	}
+
+	/* clear debug info */
+	mt_reg_sync_writel(0x80000000, EMI_MPUS);
+
+	mpus = readl(IOMEM(EMI_MPUS));
+	mput = readl(IOMEM(EMI_MPUT));
+
+	if (mpus) {
+		pr_info("[DEVMPU] fail to clear violation\n");
+		pr_info("[DEVMPU] EMI_MPUS: %x, EMI_MPUT: %x\n", mpus, mput);
+	}
+
+	/* clear hyp violation status */
+	arm_smccc_smc(MTK_SIP_KERNEL_DEVMPU_VIO_CLR,
+			0, 0, 0, 0, 0, 0, 0, &smc_res);
+	mput_2nd = readl(IOMEM(EMI_MPUT_2ND));
+	if ((smc_res.a0) || ((mput_2nd >> 21) & 0x3)) {
+		pr_info("[MPU] fail to clear hypervisor violation\n");
+		pr_info("[MPU] EMI_MPT_2ND: %x\n", mput_2nd);
+	}
+}
+
+int bypass_violation(unsigned int mpus, unsigned int *init_flag)
+{
+	unsigned int mput, mput_2nd;
+	unsigned long long vio_addr;
+	unsigned int master_id, domain_id;
+	unsigned int port_id;
+	unsigned int wr_vio, wr_oo_vio;
+	unsigned int hp_wr_vio;
+
+	mput = readl(IOMEM(EMI_MPUT));
+	mput_2nd = readl(IOMEM(EMI_MPUT_2ND));
+	vio_addr = ((((unsigned long long)(mput_2nd & 0xF)) << 32) + mput +
+		DRAM_OFFSET);
+	hp_wr_vio = (mput_2nd >> 21) & 0x3;
+
+	master_id = mpus & 0xFFFF;
+	domain_id = (mpus >> 21) & 0xF;
+	wr_vio = (mpus >> 29) & 0x3;
+	wr_oo_vio = (mpus >> 27) & 0x3;
+	port_id = master_id & 0x7;
+
+	/* if is hyperviosr MPU violation, deliver to DevMPU */
+	if (hp_wr_vio) {
+		devmpu_print_violation(vio_addr, master_id, domain_id,
+				hp_wr_vio, true);
+		clear_devmpu_violation();
+		return 1;
+	}
+
+#ifdef CONFIG_MTK_ENABLE_GENIEZONE
+	if ((wr_vio == 2) && (wr_oo_vio == 0) &&
+	    ((port_id == 0) || (port_id == 1)))
+		return 1;
+#endif
+
+	return 0;
 }
 
