8da70ea3aaf9a1bd1d1cecbde5c394d787c8e4e9
Argus Lin <argus.lin@mediatek.com>
2020-03-03 04:04:59 +0800

    [ALPS04669344] pmif: add pmif driver support for mt6789
    Add pmif driver support for mt6789, it used to access mt6315
    external buck.
    
    MTK-Commit-Id: 34fbc468e24838cab61075f802529890a9c47d45
    
    Change-Id: Iaea21fbb55a9b9fde62fb1de8e51b420827c82d2
    Signed-off-by: Argus Lin <argus.lin@mediatek.com>
    CR-Id: ALPS04669344
    Feature: Power Management
    (cherry picked from commit 77b1b702e598f258974e5723eff7dfb3fd7945b5)

---
+++ drivers/spmi/spmi_sw.h
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2019 MediaTek Inc.
+ * Author: Argus Lin <argus.lin@mediatek.com>
+ */
+#ifndef __SPMI_SW_H__
+#define __SPMI_SW_H__
+
+/* #define SPMI_DBG_DEBUG */
+#define SPMI_MONITOR_SUPPORT
+/* #define SPMI_MATCH_SUPPORT */
+/* #define SPMI_RCS_SUPPORT */
+
+#define MT6315_EVB 0
+#define MTK_PLATFORM_MT6315      1
+
+#define DEFAULT_VALUE_READ_TEST             (0x5a)
+#define DEFAULT_VALUE_WRITE_TEST            (0xa5)
+
+#endif /*__SPMI_SW_H__*/
