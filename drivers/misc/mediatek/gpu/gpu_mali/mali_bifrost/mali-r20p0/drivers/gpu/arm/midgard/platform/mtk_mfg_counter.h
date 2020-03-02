20a4497b5a410f6a3aff8f524b839c7697b2b314
Bocheng Chen <bocheng.chen@mediatek.com>
2020-03-03 04:11:23 +0800

    [ALPS04178130] Revert "[ALPS04178130] GPU:Provide perf counter for LTR"
    This reverts commit ba20500431f8532ebfc7ea9b8b5cc53d056cacca.
    
    MTK-Commit-Id: eff5b7d257f4da813240bba1be58bb95dbf60b51
    
    Change-Id: Id13228ba5e060fc1f6098bb93da69932de21a16d
    Signed-off-by: mtk15651 <bocheng.chen@mediatek.com>
    CR-Id: ALPS04178130
    Feature: OpenGL|ES

--- drivers/misc/mediatek/gpu/gpu_mali/mali_bifrost/mali-r20p0/drivers/gpu/arm/midgard/platform/mtk_mfg_counter.h
+++ drivers/misc/mediatek/gpu/gpu_mali/mali_bifrost/mali-r20p0/drivers/gpu/arm/midgard/platform/mtk_mfg_counter.h
@@ -23,48 +23,15 @@ enum {
 	PMU_RESET_VALUE = 2,
 };
 
-//MTK COUNTER
-typedef enum {
-	GPU_ACTIVE,
-	EXEC_INSTR_COUNT,
-	EXEC_CORE_ACTIVE,
-	EXEC_ACTIVE,
-	FRAG_ACTIVE,
-	TILER_ACTIVE,
-	TEX_FILT_NUM_OPERATIONS,
-	LS_MEM_READ_FULL,
-	LS_MEM_WRITE_FULL,
-	LS_MEM_READ_SHORT,
-	LS_MEM_WRITE_SHORT,
-	L2_EXT_WRITE_BEATS,
-	L2_EXT_READ_BEATS,
-	L2_EXT_RRESP_0_127,
-	L2_EXT_RRESP_128_191,
-	L2_EXT_RRESP_192_255,
-	L2_EXT_RRESP_256_319,
-	L2_EXT_RRESP_320_383,
-	L2_ANY_LOOKUP,
-	PERF_COUNTER_LAST
-} mtk_perf_counter;
-
-
 extern int (*mtk_get_gpu_pmu_init_fp)(GPU_PMU *pmus, int pmu_size, int *ret_size);
 extern int (*mtk_get_gpu_pmu_deinit_fp)(void);
 extern int (*mtk_get_gpu_pmu_swapnreset_fp)(GPU_PMU *pmus, int pmu_size);
 extern int (*mtk_get_gpu_pmu_swapnreset_stop_fp)(void);
 /* Need to get current gpu freq from GPU DVFS module */
 extern unsigned int mt_gpufreq_get_cur_freq(void);
-extern unsigned int mt_gpufreq_get_cur_volt(void);
-
-
-int get_mali_pmu_counter(int i);
-int gator_gpu_pmu_init(void);
-int mtk_mfg_update_counter(void);
-int find_name_pos(const char *name, int *pos);
 
 void mtk_mfg_counter_init(void);
 void mtk_mfg_counter_destroy(void);
-void mali_gpu_pmu_stop(void);
 
 
 #endif
