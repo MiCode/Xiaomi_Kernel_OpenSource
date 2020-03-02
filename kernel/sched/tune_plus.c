e83cde5619775bac107e841df4d75b49f7d3e1f7
Jing-Ting Wu <jing-ting.wu@mediatek.com>
2020-03-03 02:00:18 +0800

    [ALPS04025356] sched/tune: add kernel API
    [Detail]
    1. boost
    2. prefer_idle
    3. linear boost
    
    [Solution]
    
    MTK-Commit-Id: cb1545215370e5c393f9236ccd5fc0a6456a483f
    
    Change-Id: I8bf8f9adde0aca553d6786752b34c7a394e48472
    CR-Id:ALPS04025356
    Feature:Energy Aware Scheduling Plus (EAS+)
    Signed-off-by: Jing-Ting Wu <jing-ting.wu@mediatek.com>
    (cherry-pick from 3c13cf935b2f6d2cec9124aa6ecdb883da2d7979)

--- kernel/sched/tune_plus.c
+++ kernel/sched/tune_plus.c
@@ -60,3 +60,97 @@ int sched_stune_task_threshold_handler(struct ctl_table *table,
 
 	return ret;
 }
+
+int boost_write_for_perf_idx(int idx, int boost_value)
+{
+	struct schedtune *ct;
+
+	if (boost_value < 0 || boost_value > 100)
+		printk_deferred("warn: boost value should be 0~100\n");
+
+	if (boost_value >= 100)
+		boost_value = 100;
+	else if (boost_value <= 0)
+		boost_value = 0;
+
+	ct = allocated_group[idx];
+	if (ct) {
+		rcu_read_lock();
+		ct->boost = boost_value;
+
+		/* Update CPU boost */
+		schedtune_boostgroup_update(ct->idx, ct->boost);
+		rcu_read_unlock();
+
+#if MET_STUNE_DEBUG
+		/* foreground */
+		if (ct->idx == 1)
+			met_tag_oneshot(0, "sched_boost_fg", ct->boost);
+		/* top-app */
+		if (ct->idx == 3)
+			met_tag_oneshot(0, "sched_boost_ta", ct->boost);
+#endif
+	} else {
+		printk_deferred("error: stune group idx=%d is nonexistent\n",
+				idx);
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+int prefer_idle_for_perf_idx(int idx, int prefer_idle)
+{
+	struct schedtune *ct = allocated_group[idx];
+
+	if (!ct)
+		return -EINVAL;
+
+	rcu_read_lock();
+	ct->prefer_idle = prefer_idle;
+	rcu_read_unlock();
+
+#if MET_STUNE_DEBUG
+	/* top-app */
+	if (ct->idx == 3)
+		met_tag_oneshot(0, "sched_prefer_idle_ta",
+				ct->prefer_idle);
+#endif
+
+	return 0;
+}
+
+#ifdef CONFIG_MTK_SCHED_RQAVG_KS
+/* mtk: a linear boost value for tuning */
+int linear_real_boost(int linear_boost)
+{
+	int target_cpu, usage;
+	int boost;
+	int ta_org_cap;
+
+	sched_max_util_task(&target_cpu, NULL, &usage, NULL);
+
+	ta_org_cap = capacity_orig_of(target_cpu);
+
+	if (usage >= SCHED_CAPACITY_SCALE)
+		usage = SCHED_CAPACITY_SCALE;
+
+	/*
+	 * Conversion Formula of Linear Boost:
+	 *
+	 *   margin = (usage * linear_boost)/100;
+	 *   margin = (original_cap - usage) * boost/100;
+	 *   so
+	 *   boost = (usage * linear_boost) / (original_cap - usage)
+	 */
+	if (ta_org_cap <= usage) {
+		/* If target cpu is saturated, consider bigger one */
+		boost = (SCHED_CAPACITY_SCALE - usage) ?
+		   (usage * linear_boost)/(SCHED_CAPACITY_SCALE - usage) : 0;
+	} else
+		boost = (usage * linear_boost)/(ta_org_cap - usage);
+
+	return boost;
+}
+EXPORT_SYMBOL(linear_real_boost);
+#endif
