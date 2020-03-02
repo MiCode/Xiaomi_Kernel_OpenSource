4cd98953335d98207bcf1c0c4bd68f5c98a8d0bc
KaiChieh Chuang <kaichieh.chuang@mediatek.com>
2020-03-03 00:07:52 +0800

    [ALPS04003604] ASoC: mediatek: add sub dai to mtk_base_afe
    In MediaTek SoC chip we have multiple DAI,
    such as I2S, ADDA, PCM, etc.
    
    Organize each DAI in to one sub dai,
    with its dai driver, controls, widgets, routes.
    
    add mtk_afe_combine_sub_dai() to combine
    dai driver from each DAI.
    
    add mtk_afe_add_sub_dai_control() to register
    the control, widget, routes to component.
    
    Test: boot to shell
    
    MTK-Commit-Id: b61868a1fa525f06bdea7026f7761688a32fa369
    
    Change-Id: Ibdaf0d10a58724888ab0d6006485c032ea7b797b
    Signed-off-by: KaiChieh Chuang <kaichieh.chuang@mediatek.com>
    CR-Id: ALPS04003604
    Feature: Phone Sound

--- sound/soc/mediatek/common/mtk-afe-platform-driver.h
+++ sound/soc/mediatek/common/mtk-afe-platform-driver.h
@@ -17,7 +17,19 @@
 #ifndef _MTK_AFE_PLATFORM_DRIVER_H_
 #define _MTK_AFE_PLATFORM_DRIVER_H_
 
+extern const struct snd_pcm_ops mtk_afe_pcm_ops;
 extern const struct snd_soc_platform_driver mtk_afe_pcm_platform;
 
+struct mtk_base_afe;
+struct snd_pcm;
+struct snd_soc_pcm_runtime;
+struct snd_soc_platform;
+
+
+int mtk_afe_pcm_new(struct snd_soc_pcm_runtime *rtd);
+void mtk_afe_pcm_free(struct snd_pcm *pcm);
+
+int mtk_afe_combine_sub_dai(struct mtk_base_afe *afe);
+int mtk_afe_add_sub_dai_control(struct snd_soc_platform *platform);
 #endif
 
