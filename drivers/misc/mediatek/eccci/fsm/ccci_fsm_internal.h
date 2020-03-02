ab40dd4ac006d31d7e86dc092663d3bd347c010f
mtk13799 <xin.huang@mediatek.com>
2020-03-03 04:04:36 +0800

    [ALPS04590907] ccci: do not use wait flag
    When start/stop MD, ccci_mdinit cant receive
    any signal for wait_event.
    
    Do not use wait flag when start/stop md to
    avoid blocking in wait event.Polling broadcast
    message to receive md status
    
    MTK-Commit-Id: dbeccd2a9964da0b12f067bb1cbaa8036a522297
    
    Change-Id: I609c1a293470a6cf199bb5050c19c5bd810c8ced
    Signed-off-by: mtk13799 <xin.huang@mediatek.com>
    CR-Id: ALPS04590907
    Feature: Modem Interface Driver

--- drivers/misc/mediatek/eccci/fsm/ccci_fsm_internal.h
+++ drivers/misc/mediatek/eccci/fsm/ccci_fsm_internal.h
@@ -292,6 +292,7 @@ void fsm_md_no_response_handler(struct ccci_fsm_ee *ee_ctl);
 void fsm_md_exception_stage(struct ccci_fsm_ee *ee_ctl, int stage);
 void fsm_ee_message_handler(struct ccci_fsm_ee *ee_ctl, struct sk_buff *skb);
 int fsm_check_ee_done(struct ccci_fsm_ee *ee_ctl, int timeout);
+int force_md_stop(struct ccci_fsm_monitor *monitor_ctl);
 
 extern int mdee_dumper_v1_alloc(struct ccci_fsm_ee *mdee);
 extern int mdee_dumper_v2_alloc(struct ccci_fsm_ee *mdee);
