887b25e2796afe93c58711de30e20f8b7404fb44
mtk14723 <xing.zhang@mediatek.com>
2020-03-03 04:12:14 +0800

    [ALPS04762470] Vibrator: fix code scan for 10350476
    [Detail]
    Fix code scan for 10350476
    
    MTK-Commit-Id: e9fbc4d5e0b28c7c7af1167dcc3d44fbe5dd92e4
    
    Change-Id: Ia8bb80a6dc15ba835b439092d1e330cf4a6e83b0
    Signed-off-by: mtk14723 <xing.zhang@mediatek.com>
    CR-Id: ALPS04762470
    Feature: [Module]Vibrator

--- drivers/misc/mediatek/vibrator/vibrator_drv.c
+++ drivers/misc/mediatek/vibrator/vibrator_drv.c
@@ -134,7 +134,7 @@ static ssize_t vibr_activate_show(struct device *dev,
 static ssize_t vibr_activate_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t size)
 {
-	unsigned int activate, dur;
+	unsigned int activate = 0, dur = 0;
 	ssize_t ret;
 
 	ret = kstrtouint(buf, 10, &activate);
