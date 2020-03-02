1e4fc731503e21b8c882a519e6f02e7e7f6307df
mtk16229 <Heaven.Zhang@mediatek.com>
2020-03-03 02:10:16 +0800

    [ALPS04321165] touch: gslX680: debug touch feature
    [Detail]debug touch feature
    
    MTK-Commit-Id: 91e1fc411923ed15502ba791ddd541794ae9ca06
    
    Change-Id: I4f85b9dd6a16fd6eeb3d461821e1fd565f184a3c
    Signed-off-by: mtk16229 <Heaven.Zhang@mediatek.com>
    CR-Id: ALPS04321165
    Feature: [Android Default] CapTouch

--- drivers/input/touchscreen/mediatek/gslX680/mt6739/mtk_gslX680.h
+++ drivers/input/touchscreen/mediatek/gslX680/mt6739/mtk_gslX680.h
@@ -1,18 +1,18 @@
 /*drivers/input/touchscreen/mediatek/gslX680/
- *
- * 2010 - 2016 silead inc.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be a reference
- * to you, when you are integrating the Sileadinc's CTP IC into your system,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- */
+*
+* 2010 - 2016 silead inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be a reference
+* to you, when you are integrating the Sileadinc's CTP IC into your system,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*/
 #ifndef _GSLX680_H_
 #define _GSLX680_H_
 #define ATA_TP_ADDR 0x5f80
