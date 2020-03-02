807f51c0346dc9e60fe8ebb530654167ae8c749b
Figo Wang <figo.wang@mediatek.com>
2020-03-03 04:08:39 +0800

    [ALPS04744754] GPUFREQ: Enable GED Log
    Enable GED Log
    
    MTK-Commit-Id: 545e9a24ad1aa7e82b43e7b660ecc2f6266100b1
    
    Change-Id: I4578fca6a571bd117f6bf3976116a4581b55c51e
    Signed-off-by: Figo Wang <figo.wang@mediatek.com>
    CR-Id: ALPS04744754
    Feature: OpenGL|ES

--- drivers/misc/mediatek/base/power/mt6771/mtk_gpu_log.h
+++ drivers/misc/mediatek/base/power/mt6771/mtk_gpu_log.h
@@ -19,19 +19,13 @@
 
 extern GED_LOG_BUF_HANDLE _mtk_gpu_log_hnd;
 
-#if 0
-#define GPULOG(fmt, ...) \
-	ged_log_buf_print2(_mtk_gpu_log_hnd, \
-		GED_LOG_ATTR_TIME, fmt, ##__VA_ARGS__)
+#define GPULOG(fmt, ...) ged_log_buf_print2(_mtk_gpu_log_hnd, \
+	GED_LOG_ATTR_TIME, fmt, ##__VA_ARGS__)
 #define GPULOG2(fmt, ...) do { \
 	ged_log_buf_print2(_mtk_gpu_log_hnd, \
-		GED_LOG_ATTR_TIME, fmt, ##__VA_ARGS__); \
+	GED_LOG_ATTR_TIME, fmt, ##__VA_ARGS__); \
 	gpufreq_pr_info(fmt "\n", ##__VA_ARGS__); \
 } while (0)
-#endif
-
-#define GPULOG(fmt, ...) do { } while (0)
-#define GPULOG2(fmt, ...) do { } while (0)
 
 void mtk_gpu_log_init(void);
 void mtk_gpu_log_trigger_aee(const char *msg);
