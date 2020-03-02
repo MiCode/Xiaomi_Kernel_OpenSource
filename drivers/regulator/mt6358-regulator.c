a59f6c4295aa9ade091d63216f5a33a964b04c24
Jeter Chen <jeter.chen@mediatek.com>
2020-03-03 04:10:42 +0800

    [ALPS04778438] PMIC: fix Uninitialized scalar variable in MFD/regulator
    fix Uninitialized scalar variable in MFD/regulator
    
    MTK-Commit-Id: de9a8d55b58118868848dbe26c78af6798d7deef
    
    Change-Id: I4e2e12e14baf1dc565ebd56c03f8d7d47c51a3eb
    Signed-off-by: Jeter Chen <jeter.chen@mediatek.com>
    CR-Id: ALPS04778438
    Feature: Power Management

--- drivers/regulator/mt6358-regulator.c
+++ drivers/regulator/mt6358-regulator.c
@@ -483,7 +483,7 @@ static int mt6358_regulator_disable(struct regulator_dev *rdev)
 static int mt6358_regulator_get_voltage_sel(struct regulator_dev *rdev)
 {
 	struct mt6358_regulator_info *info = rdev_get_drvdata(rdev);
-	int ret, regval;
+	int ret, regval = 0;
 
 	ret = regmap_read(rdev->regmap, info->da_vsel_reg, &regval);
 	if (ret != 0) {
@@ -535,7 +535,7 @@ err_mode:
 static unsigned int mt6358_regulator_get_mode(struct regulator_dev *rdev)
 {
 	struct mt6358_regulator_info *info = rdev_get_drvdata(rdev);
-	int ret, regval;
+	int ret, regval = 0;
 
 	ret = regmap_read(rdev->regmap, info->modeset_reg, &regval);
 	if (ret != 0) {
@@ -557,7 +557,7 @@ static unsigned int mt6358_regulator_get_mode(struct regulator_dev *rdev)
 static int mt6358_get_status(struct regulator_dev *rdev)
 {
 	int ret;
-	u32 regval;
+	u32 regval = 0;
 	struct mt6358_regulator_info *info = rdev_get_drvdata(rdev);
 
 	ret = regmap_read(rdev->regmap, info->da_reg, &regval);
@@ -922,7 +922,7 @@ static int mt6358_regulator_probe(struct platform_device *pdev)
 	struct regulator_dev *rdev;
 	struct regulation_constraints *c;
 	int i;
-	u32 reg_value;
+	u32 reg_value = 0;
 
 	of_id = of_match_device(mt6358_of_match, &pdev->dev);
 	if (!of_id || !of_id->data)
