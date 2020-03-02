f8f3f23c92f2da9ac249437e5ce133ef48d5cfbd
Auto Li <auto.li@mediatek.com>
2020-03-03 04:02:23 +0800

    [ALPS04305033] EEPROM: Sync eeprom kernel-4.14 code
    Sync eeprom kernel-4.14 code
    
    MTK-Commit-Id: 6fabbbd3e3c58f2df84f7fba19812c03c00b3c36
    
    Change-Id: Ib130b3f79cc9e8eb199a5911217b5701be7a8792
    Signed-off-by: Auto Li <auto.li@mediatek.com>
    CR-Id: ALPS04305033
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/misc/mediatek/cam_cal/src/mt6768/eeprom_i2c_custom_driver.h
+++ drivers/misc/mediatek/cam_cal/src/mt6768/eeprom_i2c_custom_driver.h
@@ -10,24 +10,18 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
-#ifndef __CAM_CAL_LIST_H
-#define __CAM_CAL_LIST_H
+#ifndef __EEPROM_I2C_CUSTOM_DRIVER_H
+#define __EEPROM_I2C_CUSTOM_DRIVER_H
 #include <linux/i2c.h>
 
-#define DEFAULT_MAX_EEPROM_SIZE_8K 0x2000
-
-typedef unsigned int (*cam_cal_cmd_func) (struct i2c_client *client,
-	unsigned int addr, unsigned char *data, unsigned int size);
-
-struct stCAM_CAL_LIST_STRUCT {
-	unsigned int sensorID;
-	unsigned int slaveID;
-	cam_cal_cmd_func readCamCalData;
-	unsigned int maxEepromSize;
-};
-
-
-unsigned int cam_cal_get_sensor_list
-		(struct stCAM_CAL_LIST_STRUCT **ppCamcalList);
+/************************************************************
+ * I2C read function (Custom)
+ * Customer's driver can put on here
+ * Below is an example
+ ************************************************************/
+unsigned int Custom_read_region(struct i2c_client *client,
+				unsigned int addr,
+				unsigned char *data,
+				unsigned int size);
 
 #endif				/* __CAM_CAL_LIST_H */
