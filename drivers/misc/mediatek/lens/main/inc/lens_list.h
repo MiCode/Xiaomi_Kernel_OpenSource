91e0f822299ca50a90520a28850f958e4686448e
gtk_guanghuidu <gtk_guanghuidu@mediatek.com>
2020-03-03 04:12:03 +0800

    [ALPS04853834] camera: lens drivers porting
    [details] add main/main2 lens driver file
    
    MTK-Commit-Id: 64e5133a8029793e47b1548143d7b59c77eeff64
    
    Signed-off-by: gtk_guanghuidu <gtk_guanghuidu@mediatek.com>
    Change-Id: I75a2c468ca09933e0a85c038db421c8e4ca9bea3
    CR-Id: ALPS04853834
    Feature: Camera Application

--- drivers/misc/mediatek/lens/main/inc/lens_list.h
+++ drivers/misc/mediatek/lens/main/inc/lens_list.h
@@ -126,6 +126,15 @@ extern long DW9814AF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
 extern int DW9814AF_Release(struct inode *a_pstInode, struct file *a_pstFile);
 extern int DW9814AF_GetFileName(unsigned char *pFileName);
 
+#define DW9800WAF_SetI2Cclient DW9800WAF_SetI2Cclient_Main
+#define DW9800WAF_Ioctl DW9800WAF_Ioctl_Main
+#define DW9800WAF_Release DW9800WAF_Release_Main
+extern int DW9800WAF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient,
+				 spinlock_t *pAF_SpinLock, int *pAF_Opened);
+extern long DW9800WAF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
+			   unsigned long a_u4Param);
+extern int DW9800WAF_Release(struct inode *a_pstInode, struct file *a_pstFile);
+
 #define DW9718AF_SetI2Cclient DW9718AF_SetI2Cclient_Main
 #define DW9718AF_Ioctl DW9718AF_Ioctl_Main
 #define DW9718AF_Release DW9718AF_Release_Main
