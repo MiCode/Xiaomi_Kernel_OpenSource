8e6c9ef8d5587aac896f3777ce0c07e10f58d296
mtk11285 <weiwei.zhang@mediatek.com>
2020-03-03 00:23:56 +0800

    [ALPS04036690] AEE:add aed_common_exception api
    [Detail] add aed_common_exception api for anyone who wants to trigger ee
    
    MTK-Commit-Id: d2adcd1f614f5f44d398c434ee02fc512779c293
    
    Change-Id: Ia687ff8565787f16f781c9c04957195be8e898a4
    Signed-off-by: mtk11285 <weiwei.zhang@mediatek.com>
    CR-Id: ALPS04036690
    Feature: Android Exception Engine(AEE)
    (cherry-pick from 522e5f820806a6eda36dfdaef9c8210d4fccc1d3)

--- drivers/misc/mediatek/include/mt-plat/aee.h
+++ drivers/misc/mediatek/include/mt-plat/aee.h
@@ -174,6 +174,9 @@ struct aee_kernel_api {
 	void (*scp_exception)(const char *assert_type, const int *log,
 			int log_size, const int *phy, int phy_size,
 			const char *detail, const int db_opt);
+	void (*common_exception)(const char *assert_type, const int *log,
+			int log_size, const int *phy, int phy_size,
+			const char *detail, const int db_opt);
 };
 
 void aee_sram_printk(const char *fmt, ...);
@@ -262,6 +265,10 @@ void aee_oops_free(struct aee_oops *oops);
 #define aed_combo_exception(log, log_size, phy, phy_size, detail)	\
 	aed_combo_exception_api(log, log_size, phy, phy_size, detail,	\
 			DB_OPT_DEFAULT)
+#define aed_common_exception(assert_type, log, log_size, phy, phy_size,	\
+			detail)	\
+	aed_common_exception_api(assert_type, log, log_size, phy,	\
+			phy_size, detail, DB_OPT_DEFAULT)
 
 void aee_kernel_exception_api(const char *file, const int line,
 		const int db_opt, const char *module, const char *msg, ...);
@@ -279,6 +286,9 @@ void aed_scp_exception_api(const int *log, int log_size, const int *phy,
 			int phy_size, const char *detail, const int db_opt);
 void aed_combo_exception_api(const int *log, int log_size, const int *phy,
 			int phy_size, const char *detail, const int db_opt);
+void aed_common_exception_api(const char *assert_type, const int *log, int
+			log_size, const int *phy, int phy_size, const char
+			*detail, const int db_opt);
 
 void aee_kernel_wdt_kick_Powkey_api(const char *module, int msg);
 int aee_kernel_wdt_kick_api(int kinterval);
