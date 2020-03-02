0db5d42fb7f7fbbef41d2d847dc16bd0b49641bc
Ming-Fan Chen <ming-fan.chen@mediatek.com>
2020-03-03 04:06:26 +0800

    [ALPS04313905] cmdq: add debug info for mailbox mt6885
    Add cmdq event module dispatch for mt6885.
    
    MTK-Commit-Id: 2390c00b18690ce74be5a7e072d8c37deaad191c
    
    Change-Id: I30b6bef68c87906cef763558345a3c7df608c8a6
    CR-Id: ALPS04313905
    Feature: [Module]CMDQ Driver
    Signed-off-by: Ming-Fan Chen <ming-fan.chen@mediatek.com>

--- include/dt-bindings/gce/mt6885-gce.h
+++ include/dt-bindings/gce/mt6885-gce.h
@@ -69,30 +69,30 @@
  * Leave note for scenario usage here
  */
 /* GCE: write mask */
-#define CMDQ_GPR_R00		0x00
-#define CMDQ_GPR_R01		0x01
+#define GCE_GPR_R00		0x00
+#define GCE_GPR_R01		0x01
 /* MDP: P1: JPEG dest */
-#define CMDQ_GPR_R02		0x02
-#define CMDQ_GPR_R03		0x03
+#define GCE_GPR_R02		0x02
+#define GCE_GPR_R03		0x03
 /* MDP: PQ color */
-#define CMDQ_GPR_R04		0x04
+#define GCE_GPR_R04		0x04
 /* MDP: 2D sharpness */
-#define CMDQ_GPR_R05		0x05
+#define GCE_GPR_R05		0x05
 /* DISP: poll esd */
-#define CMDQ_GPR_R06		0x06
-#define CMDQ_GPR_R07		0x07
+#define GCE_GPR_R06		0x06
+#define GCE_GPR_R07		0x07
 /* MDP: P4: 2D sharpness dst */
-#define CMDQ_GPR_R08		0x08
-#define CMDQ_GPR_R09		0x09
+#define GCE_GPR_R08		0x08
+#define GCE_GPR_R09		0x09
 /* VCU: poll with timeout for GPR timer */
-#define CMDQ_GPR_R10		0x0A
-#define CMDQ_GPR_R11		0x0B
+#define GCE_GPR_R10		0x0A
+#define GCE_GPR_R11		0x0B
 /* CMDQ: debug */
-#define CMDQ_GPR_R12		0x0C
-#define CMDQ_GPR_R13		0x0D
+#define GCE_GPR_R12		0x0C
+#define GCE_GPR_R13		0x0D
 /* CMDQ: P7: debug */
-#define CMDQ_GPR_R14		0x0E
-#define CMDQ_GPR_R15		0x0F
+#define GCE_GPR_R14		0x0E
+#define GCE_GPR_R15		0x0F
 
 /* GCE-D hardware events */
 #define CMDQ_EVENT_DISP_OVL0_SOF			0
