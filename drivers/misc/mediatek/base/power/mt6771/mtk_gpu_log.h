38273761f291fd391ccdc97a68905d4feb81b340
Figo Wang <figo.wang@mediatek.com>
2020-03-03 04:08:32 +0800

    [ALPS04304868] GPU: Make gpufreq up-to-date
    Sync with kernel-4.4.
    
    MTK-Commit-Id: a2127e2d09684161f66d2a62502f30f9a4821baf
    
    Change-Id: I1a9cb192f62640e0fda6dee2e876ae6ceec9c360
    Signed-off-by: Figo Wang <figo.wang@mediatek.com>
    CR-Id: ALPS04304868
    Feature: OpenGL|ES

--- drivers/misc/mediatek/base/power/mt6771/mtk_gpu_log.h
+++ drivers/misc/mediatek/base/power/mt6771/mtk_gpu_log.h
@@ -19,7 +19,6 @@
 
 extern GED_LOG_BUF_HANDLE _mtk_gpu_log_hnd;
 
-#if 0
 #define GPULOG(fmt, ...) \
 	ged_log_buf_print2(_mtk_gpu_log_hnd, \
 		GED_LOG_ATTR_TIME, fmt, ##__VA_ARGS__)
@@ -28,10 +27,6 @@ extern GED_LOG_BUF_HANDLE _mtk_gpu_log_hnd;
 		GED_LOG_ATTR_TIME, fmt, ##__VA_ARGS__); \
 	gpufreq_pr_info(fmt "\n", ##__VA_ARGS__); \
 } while (0)
-#endif
-
-#define GPULOG(fmt, ...) do { } while (0)
-#define GPULOG2(fmt, ...) do { } while (0)
 
 void mtk_gpu_log_init(void);
 void mtk_gpu_log_trigger_aee(const char *msg);
