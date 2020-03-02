8f44d721db5a2835c8f090aaeaeeedd7d51a7b37
Poting Chen <poting.chen@mediatek.com>
2020-03-03 04:05:12 +0800

    [ALPS04575770] FPSGO: fix coverity
    [Detail] fix coverity
    
    MTK-Commit-Id: 2ec8a32594c7e3661ba55f3ee52d551b60099b69
    
    Change-Id: I9acfebe313193039857b549d317a04e8bd6a77ea
    CR-Id: ALPS04575770
    Feature: FPSGO
    Signed-off-by: Poting Chen <poting.chen@mediatek.com>

--- drivers/misc/mediatek/performance/fpsgo_v3/fbt/src/mini_top.c
+++ drivers/misc/mediatek/performance/fpsgo_v3/fbt/src/mini_top.c
@@ -334,7 +334,7 @@ static int __minitop_has_heavy(struct minitop_rec *mr, int *heavy)
 	ratio >>= (25 + __warmup_order);
 	mr->ratio = div_u64(ratio, mr->life);
 
-	minitop_trace(" %5d %3d%% %12llu->%12llu life-%d debnc-%d",
+	minitop_trace(" %5d %3llu%% %12llu->%12llu life-%d debnc-%d",
 		      mr->tid, mr->ratio, mr->init_runtime, runtime,
 		      mr->life, mr->debnc);
 
@@ -342,7 +342,7 @@ static int __minitop_has_heavy(struct minitop_rec *mr, int *heavy)
 		if (mr->debnc_fteh > 0)
 			keep |= 1;
 		else
-			minitop_trace(" %5d %3d%% end debnc FTEH",
+			minitop_trace(" %5d %3llu%% end debnc FTEH",
 				      mr->tid, mr->ratio);
 	}
 
@@ -388,7 +388,7 @@ static int __minitop_has_heavy(struct minitop_rec *mr, int *heavy)
 end_sched_nomi:
 	ret = keep ? 0 : -EAGAIN;
 	if (mr->ever)
-		minitop_trace(" %5d %3d%% end debnc heavy",
+		minitop_trace(" %5d %3llu%% end debnc heavy",
 			      mr->tid, (int)mr->ratio);
 
 err_cleanup:
