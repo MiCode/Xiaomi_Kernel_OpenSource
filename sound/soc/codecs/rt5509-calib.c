39179a18579fd47f0268d0870cb7b1404e76af3c
Vend.Gene.Chen <vend_gene_chen001@mediatek.com>
2020-03-03 04:12:20 +0800

    [ALPS04942087] PMIC: fix rt5509 for GPLv2 license
    [Detail]
    fix rt5509 for GPLv2 license
    
    MTK-Commit-Id: e4713124bc4b1958bce6772f5ab72344c474ebf0
    
    Change-Id: I628cf8ef04e3104e5e9cb9affa0001ebb19bf495
    Signed-off-by: Vend.Gene.Chen <vend_gene_chen001@mediatek.com>
    CR-Id: ALPS04942087
    Feature: Power Management
    (cherry picked from commit 0e4c6320b70b839661dc15ce03fafe49ec411cae)

--- sound/soc/codecs/rt5509-calib.c
+++ sound/soc/codecs/rt5509-calib.c
@@ -1,17 +1,14 @@
 /*
- *  sound/soc/codecs/rt5509-calib.c
+ * Copyright (C) 2019 MediaTek Inc.
  *
- *  Copyright (C) 2016 Richtek Technology Corp.
- *  cy_huang <cy_huang@richtek.com>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 2 as
- *  published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *  See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
 
 #include <linux/module.h>
