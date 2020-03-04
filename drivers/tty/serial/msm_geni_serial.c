65b202ede464840bc00e33fbb4bd457f32f826de
Atul Raut <araut@codeaurora.org>
2020-03-03 17:01:42 -0800

    serial: msm-geni-serial: Add support for thaw callback
    Hibernation may fail after this driver's freeze
    callback. Implement thaw callback to undo the changes
    done in freeze callback to keep system intact.
    
    Change-Id: Ic682e6d4372d4e9da8da4f4b735cef25e152d159
    Signed-off-by: Atul Raut <araut@codeaurora.org>
    Signed-off-by: Aniket Randive <quic_arandive@quicinc.com>

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -4789,6 +4789,7 @@ static const struct dev_pm_ops msm_geni_serial_pm_ops = {
 	.resume = msm_geni_serial_sys_resume,
 	.freeze = msm_geni_serial_sys_suspend,
 	.restore = msm_geni_serial_sys_hib_resume,
+	.thaw = msm_geni_serial_sys_hib_resume,
 };
 
 static struct platform_driver msm_geni_serial_platform_driver = {
