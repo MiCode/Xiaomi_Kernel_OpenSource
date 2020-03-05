f0f0c4ffde797325c2953c4f1a6b801ec3633970
Charan Teja Reddy <charante@codeaurora.org>
2020-03-05 17:57:49 +0530

    mm: allocate swapin pages from movable zone
    We allow pages to movable zone only when __GFP_CMA is set along with
    other required flags. When an anon page first allocated will
    go to the movable zone but when the same is swapped out and then swapin
    is not allowed to go. Fix it.
    
    Fixes: 0157fbd79dbe ("mm: Allow only __GFP_CMA allocations from Movable zone")
    Change-Id: I746299259a91819499e948ca34fe8880c8eaa175
    [guptap@codeaurora.org: Fixed minor merge conflicts]
    Signed-off-by: Charan Teja Reddy <charante@codeaurora.org>

--- mm/memory.c
+++ mm/memory.c
@@ -2986,8 +2986,8 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 		if (si->flags & SWP_SYNCHRONOUS_IO &&
 				__swap_count(entry) == 1) {
 			/* skip swapcache */
-			page = alloc_page_vma(GFP_HIGHUSER_MOVABLE, vma,
-							vmf->address);
+			page = alloc_page_vma(GFP_HIGHUSER_MOVABLE | __GFP_CMA,
+					      vma, vmf->address);
 			if (page) {
 				__SetPageLocked(page);
 				__SetPageSwapBacked(page);
