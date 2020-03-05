ee7e22166fc679718e9d3377caf179e99d1f825b
Eve <eve.su@mediatek.com>
2020-03-05 11:19:29 +0800

    [ALPS04989941] v4l2: Increase vdec OPP to meet fps
    Increase vdec OPP to meet fps
    
    MTK-Commit-Id: add0898b528d451d2d121104abb6532368b508bc
    
    Change-Id: I2c53de9f00d39940686e2b0bf0384eb69ff25395
    CR-Id: ALPS04989941
    Feature: MPEG4 Decoder
    Signed-off-by: Eve <eve.su@mediatek.com>

--- drivers/media/platform/mtk-vcodec/mt6873/mtk_vcodec_dec_pm.c
+++ drivers/media/platform/mtk-vcodec/mt6873/mtk_vcodec_dec_pm.c
@@ -401,6 +401,18 @@ void mtk_vdec_dvfs_begin(struct mtk_vcodec_ctx *ctx, int hw_id)
 		vdec_req_freq[hw_id] = STD_VDEC_FREQ;
 	}
 
+	if (ctx->q_data[MTK_Q_DATA_SRC].fmt->fourcc == V4L2_PIX_FMT_MPEG1 ||
+	ctx->q_data[MTK_Q_DATA_SRC].fmt->fourcc == V4L2_PIX_FMT_MPEG2 ||
+	ctx->q_data[MTK_Q_DATA_SRC].fmt->fourcc == V4L2_PIX_FMT_MPEG4 ||
+	ctx->q_data[MTK_Q_DATA_SRC].fmt->fourcc == V4L2_PIX_FMT_H263 ||
+	ctx->q_data[MTK_Q_DATA_SRC].fmt->fourcc == V4L2_PIX_FMT_XVID ||
+	ctx->q_data[MTK_Q_DATA_SRC].fmt->fourcc == V4L2_PIX_FMT_DIVX ||
+	ctx->q_data[MTK_Q_DATA_SRC].fmt->fourcc == V4L2_PIX_FMT_DIVX3 ||
+	ctx->q_data[MTK_Q_DATA_SRC].fmt->fourcc == V4L2_PIX_FMT_DIVX4 ||
+	ctx->q_data[MTK_Q_DATA_SRC].fmt->fourcc == V4L2_PIX_FMT_DIVX5 ||
+	ctx->q_data[MTK_Q_DATA_SRC].fmt->fourcc == V4L2_PIX_FMT_DIVX6)
+		vdec_req_freq[hw_id] = 312;
+
 	if (ctx->q_data[MTK_Q_DATA_SRC].fmt->fourcc == V4L2_PIX_FMT_VP8)
 		vdec_req_freq[hw_id] = 416;
 
