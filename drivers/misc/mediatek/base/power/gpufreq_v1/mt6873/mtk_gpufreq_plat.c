86219cdfaba5930aafb69bb626af21ccf532e83b
max.yu <max.yu@mediatek.com>
2020-03-05 11:18:49 +0800

    [ALPS05000575] GPU: Get voltage by regulator status
    Get voltage by regulator status
    
    MTK-Commit-Id: 492f4c89713b1489ed6dda4cb38549f4a8aa8089
    
    Change-Id: I4326355c1e1c696b1e16582af51131e741a014d5
    Signed-off-by: max.yu <max.yu@mediatek.com>
    CR-Id: ALPS05000575
    Feature: OpenGL|ES

--- drivers/misc/mediatek/base/power/gpufreq_v1/mt6873/mtk_gpufreq_plat.c
+++ drivers/misc/mediatek/base/power/gpufreq_v1/mt6873/mtk_gpufreq_plat.c
@@ -2720,8 +2720,10 @@ static unsigned int __mt_gpufreq_get_cur_vsram_gpu(void)
 {
 	unsigned int volt = 0;
 
-	/* regulator_get_voltage prints volt with uV */
-	volt = regulator_get_voltage(g_pmic->reg_vsram_gpu) / 10;
+	if (regulator_is_enabled(g_pmic->reg_vsram_gpu)) {
+		/* regulator_get_voltage prints volt with uV */
+		volt = regulator_get_voltage(g_pmic->reg_vsram_gpu) / 10;
+	}
 
 	return volt;
 }
@@ -2733,8 +2735,10 @@ static unsigned int __mt_gpufreq_get_cur_vgpu(void)
 {
 	unsigned int volt = 0;
 
-	/* regulator_get_voltage prints volt with uV */
-	volt = regulator_get_voltage(g_pmic->reg_vgpu) / 10;
+	if (regulator_is_enabled(g_pmic->reg_vgpu)) {
+		/* regulator_get_voltage prints volt with uV */
+		volt = regulator_get_voltage(g_pmic->reg_vgpu) / 10;
+	}
 
 	return volt;
 }
