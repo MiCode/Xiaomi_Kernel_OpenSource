c24ee41e1a2f33ded868627b001c71e192b5e047
wenju.xu <wenju.xu@mediatek.com>
2020-03-11 18:04:33 +0800

    [ALPS05017952] sched:fix spin_bug KE
    [2:kthreadd]BUG: spinlock uninitialized on CPU#0, kthreadd/2
    [2:kthreadd] lock: 0xffffffd1ff6b5e70, .magic: 00000000, .owner:
    	 <none>/-1, .owner_cpu: 0
    [2:kthreadd]CPU: 0 PID: 2 Comm: kthreadd Not tainted
    	4.14.150-gc9845737ede2-dirty #1
    [2:kthreadd]Hardware name: MT6768 (DT)
    [2:kthreadd]Call trace:
    [2:kthreadd][<ffffff9307c8e298>] dump_backtrace+0x0/0x580
    [2:kthreadd][<ffffff9307c8e82c>] show_stack+0x14/0x1c
    [2:kthreadd][<ffffff9308d3ef20>] dump_stack+0xe0/0x130
    [2:kthreadd][<ffffff9307d67bc0>] spin_bug+0xb8/0x378
    [2:kthreadd][<ffffff9307d66e8c>] do_raw_spin_lock+0x100/0x820
    [2:kthreadd][<ffffff9308d5ed10>] _raw_spin_lock_irqsave+0x54/0x68
    [2:kthreadd][<ffffff9307dde670>] cpu_stop_queue_work+0x68/0x14c
    [2:kthreadd][<ffffff9307dde5b4>] stop_one_cpu+0x7c/0xd0
    [2:kthreadd][<ffffff9307cfbf5c>] __set_cpus_allowed_ptr+0x1c0/0x224
    [2:kthreadd][<ffffff9307cfbd94>] set_cpus_allowed_ptr+0x10/0x18
    [2:kthreadd][<ffffff9307ce8d94>] kthreadd+0x50/0x204
    [2:kthreadd][<ffffff9307c85b1c>] ret_from_fork+0x10/0x18
    
    MTK-Commit-Id: f7033067c0465ee0f438cb46dc09d992053953c1
    
    Change-Id: I6010683cdf709d49df6fb11552c7f74bec1d233c
    Signed-off-by: wenju.xu <wenju.xu@mediatek.com>
    CR-Id: ALPS05017952
    Feature: Energy Aware Scheduling Plus (EAS+)

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2251,10 +2251,22 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 	if (cpumask_equal(&p->cpus_allowed, new_mask))
 		goto out;
 
-	dest_cpu = cpumask_any_and(cpu_valid_mask, new_mask);
+	cpumask_andnot(&allowed_mask, new_mask, cpu_isolated_mask);
+	cpumask_and(&allowed_mask, &allowed_mask, cpu_valid_mask);
+
+	dest_cpu = cpumask_any(&allowed_mask);
 	if (dest_cpu >= nr_cpu_ids) {
-		ret = -EINVAL;
-		goto out;
+		/* If p is a kthread, ignore isolated mask. */
+		if (p->flags & PF_KTHREAD)
+			cpumask_and(&allowed_mask, cpu_valid_mask, new_mask);
+		else
+			cpumask_andnot(&allowed_mask,
+					cpu_valid_mask, cpu_isolated_mask);
+		dest_cpu = cpumask_any(&allowed_mask);
+		if (dest_cpu >= nr_cpu_ids) {
+			ret = -EINVAL;
+			goto out;
+		}
 	}
 
 	do_set_cpus_allowed(p, new_mask);
