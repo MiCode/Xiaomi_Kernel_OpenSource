331cbeb2054f3e253edb76e1e138d5a6e0efdef5
Vatsal Bucha <vbucha@codeaurora.org>
2020-03-12 18:21:35 +0530

    drivers: fsa4480: Fix USB re-enumeration failure after PDR
    USB re-enumeration failure is coming post PDR due to
    switch settings being reset in fsa unreg notifier.
    Reset switch settings only for audio adapter to
    resolve issue.
    
    Change-Id: I2462a2f1d3679adbd71dd1a7abd30726f7c68e6a
    Signed-off-by: Vatsal Bucha <vbucha@codeaurora.org>

--- drivers/soc/qcom/fsa4480-i2c.c
+++ drivers/soc/qcom/fsa4480-i2c.c
@@ -254,7 +254,7 @@ int fsa4480_unreg_notifier(struct notifier_block *nb,
 		goto done;
 	}
 	/* Do not reset switch settings for usb digital hs */
-	if (mode.intval != POWER_SUPPLY_TYPEC_SINK)
+	if (mode.intval == POWER_SUPPLY_TYPEC_SINK_AUDIO_ADAPTER)
 		fsa4480_usbc_update_settings(fsa_priv, 0x18, 0x98);
 	rc = blocking_notifier_chain_unregister
 					(&fsa_priv->fsa4480_notifier, nb);
