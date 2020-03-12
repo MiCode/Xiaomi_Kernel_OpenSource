c7b91c4798f8e8a3ed0b4d1e96104ba5d3eeacb6
Gaurav Kashyap <gaurkash@codeaurora.org>
2020-03-12 13:12:55 -0700

    soc: qcom: add HWKM driver for FBE
    Add hardware key manager driver in the HLOS kernel
    to facilitate storage encryption using HWKM.
    
    Change-Id: I3531ab083443e2ef41ee5990386b9052ea69841d
    Signed-off-by: Gaurav Kashyap <gaurkash@codeaurora.org>

--- drivers/soc/qcom/crypto-qti-platform.h
+++ drivers/soc/qcom/crypto-qti-platform.h
@@ -11,13 +11,20 @@
 #include <linux/types.h>
 #include <linux/device.h>
 
-#if IS_ENABLED(CONFIG_QTI_CRYPTO_TZ)
+#if IS_ENABLED(CONFIG_QTI_CRYPTO_TZ) || IS_ENABLED(CONFIG_QTI_HW_KEY_MANAGER)
 int crypto_qti_program_key(struct crypto_vops_qti_entry *ice_entry,
 			   const struct blk_crypto_key *key,
 			   unsigned int slot,
 			   unsigned int data_unit_mask, int capid);
 int crypto_qti_invalidate_key(struct crypto_vops_qti_entry *ice_entry,
 			      unsigned int slot);
+#if IS_ENABLED(CONFIG_QTI_HW_KEY_MANAGER)
+void crypto_qti_disable_platform(struct crypto_vops_qti_entry *ice_entry);
+#else
+static inline void crypto_qti_disable_platform(
+				struct crypto_vops_qti_entry *ice_entry)
+{}
+#endif /* CONFIG_QTI_HW_KEY_MANAGER */
 #else
 static inline int crypto_qti_program_key(
 				struct crypto_vops_qti_entry *ice_entry,
@@ -32,10 +39,8 @@ static inline int crypto_qti_invalidate_key(
 {
 	return -EOPNOTSUPP;
 }
-#endif /* CONFIG_QTI_CRYPTO_TZ */
-
 static inline void crypto_qti_disable_platform(
 				struct crypto_vops_qti_entry *ice_entry)
 {}
-
+#endif /* CONFIG_QTI_CRYPTO_TZ || CONFIG_QTI_HW_KEY_MANAGER */
 #endif /* _CRYPTO_QTI_PLATFORM_H */
