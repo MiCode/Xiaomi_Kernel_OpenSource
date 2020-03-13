fb421b79a84f0c23bef7ef4c9603d627575530dc
Henry Chen <henryc.chen@mediatek.com>
2020-03-13 17:34:14 +0800

    dt-bindings: soc: Add dvfsrc driver bindings
    Document the binding for enabling dvfsrc on MediaTek SoC.
    
    (cherry-picked from commit d7dde5fe920598c1564eb346064d1bdf00e0b765)
    Signed-off-by: Henry Chen <henryc.chen@mediatek.com>
    Reviewed-by: Rob Herring <robh@kernel.org>
    Change-Id: I86268256cd4ce0524c801b930cd7e236916560a6
    CR-Id: ALPS05298037
    Feature: VCORE DVFS
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

---
+++ include/dt-bindings/soc/mtk,dvfsrc.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0
+ *
+ * Copyright (c) 2018 MediaTek Inc.
+ */
+
+#ifndef _DT_BINDINGS_POWER_MTK_DVFSRC_H
+#define _DT_BINDINGS_POWER_MTK_DVFSRC_H
+
+#define MT8183_DVFSRC_LEVEL_1	1
+#define MT8183_DVFSRC_LEVEL_2	2
+#define MT8183_DVFSRC_LEVEL_3	3
+#define MT8183_DVFSRC_LEVEL_4	4
+
+#endif /* _DT_BINDINGS_POWER_MTK_DVFSRC_H */
