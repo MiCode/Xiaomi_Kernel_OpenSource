37c1eabee683f904809c6523836f1ad68c5b3336
Yong Wu <yong.wu@mediatek.com>
2020-03-16 20:37:56 +0800

    dt-bindings: mediatek: Add mtk-smi-larb-port.h
    The macro about smi larb/port should be put togethers.
    So we need to add mtk-smi-larb-port.h definition.
    
    (cherry-picked from commit 93462e9c8f64bd0c96e8ae65b553d9020245634e)
    Signed-off-by: Yong Wu <yong.wu@mediatek.com>
    Signed-off-by: Chao Hao <chao.hao@mediatek.com>
    Change-Id: If5763074f884290f4b776162ac4fcfaf052f27e9
    CR-Id: ALPS05298037
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

---
+++ include/dt-bindings/memory/mtk-smi-larb-port.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2015-2016 MediaTek Inc.
+ * Author: Yong Wu <yong.wu@mediatek.com>
+ */
+#ifndef __DTS_MTK_IOMMU_PORT_H_
+#define __DTS_MTK_IOMMU_PORT_H_
+
+#define MTK_LARB_NR_MAX			32
+
+#define MTK_M4U_ID(larb, port)		(((larb) << 5) | (port))
+#define MTK_M4U_TO_LARB(id)		(((id) >> 5) & 0xf)
+#define MTK_M4U_TO_PORT(id)		((id) & 0x1f)
+
+#endif
