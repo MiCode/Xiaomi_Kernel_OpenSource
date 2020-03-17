d631baa93726d371733f513d7ce229c9b6621e77
Weiyi Lu <weiyi.lu@mediatek.com>
2020-03-17 10:49:30 +0800

    clk-bringup: Add clock bring-up support for MT8192
    This patch will be removed once those bring-up clocks are
    handled by their own drivers.
    
    (cherry-picked from commit 1c7bc580361792cfaee480d0e076225155d9b0b1)
    HACK: clk-bringup: Add clock bring-up support for MT8192
    Signed-off-by: Weiyi Lu <weiyi.lu@mediatek.com>
    Signed-off-by: mtk01761 <wendell.lin@mediatek.com>
    Change-Id: Icf0669e197542fdcb5e32f9a75c66852c24d7e4c
    CR-Id: ALPS05298037
    Feature: Power Management
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    Source_Git_Sha: 3a33aed4f96a205040f545a372d05edb2c175f09

---
+++ drivers/clk/mediatek/clk-bringup.c
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2020 MediaTek Inc.
+ */
+
+#include <linux/clk.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/of_platform.h>
+
+static const struct of_device_id bring_up_id_table[] = {
+	{ .compatible = "mediatek,clk-bring-up",},
+	{ },
+};
+MODULE_DEVICE_TABLE(of, bring_up_id_table);
+
+static int bring_up_probe(struct platform_device *pdev)
+{
+	struct clk *clk;
+	int clk_con, i;
+
+	clk_con = of_count_phandle_with_args(pdev->dev.of_node, "clocks",
+			"#clock-cells");
+
+	for (i = 0; i < clk_con; i++) {
+		clk = of_clk_get(pdev->dev.of_node, i);
+		if (IS_ERR(clk)) {
+			long ret = PTR_ERR(clk);
+
+			if (ret == -EPROBE_DEFER)
+				pr_notice("clk %d is not ready\n", i);
+			else
+				pr_notice("get clk %d fail, ret=%d, clk_con=%d\n",
+				       i, (int)ret, clk_con);
+		} else {
+			pr_notice("get clk %d ok\n", i);
+			clk_prepare_enable(clk);
+		}
+	}
+
+	return 0;
+}
+
+static int bring_up_remove(struct platform_device *pdev)
+{
+	return 0;
+}
+
+static struct platform_driver bring_up = {
+	.probe		= bring_up_probe,
+	.remove		= bring_up_remove,
+	.driver		= {
+		.name	= "bring_up",
+		.owner	= THIS_MODULE,
+		.of_match_table = bring_up_id_table,
+	},
+};
+
+module_platform_driver(bring_up);
