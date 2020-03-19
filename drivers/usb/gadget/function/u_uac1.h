e1f02a161bc873085612b7540b244558b0286a77
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2020-03-19 12:41:14 +0530

    f_uac1: Remove unwanted descriptors for volume/mute support
    Remove unwanted descriptors like dummy mic input descriptor and
    related feature unit descriptor as those are not required for
    volume/mute support for mic and speaker paths.
    
    Change-Id: I93b2dbe114183212570229c3ccb89a36a4008917
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>

--- drivers/usb/gadget/function/u_uac1.h
+++ drivers/usb/gadget/function/u_uac1.h
@@ -39,4 +39,20 @@ struct f_uac1_opts {
 	int				refcnt;
 };
 
+#define UAC_DT_MIXER_UNIT_SIZE		13
+/* 4.3.2.3 Mixer Unit Descriptor */
+struct uac1_mixer_unit_descriptor {
+	__u8 bLength;
+	__u8 bDescriptorType;
+	__u8 bDescriptorSubtype;
+	__u8 bUnitID;
+	__u8 bNrInPins;
+	__u8 baSourceID[2];
+	__u8  bNrChannels;
+	__le16 wChannelConfig;
+	__u8  iChannelNames;
+	__u8 bmControls;
+	__u8  iMixer;
+} __packed;
+
 #endif /* __U_UAC1_H */
