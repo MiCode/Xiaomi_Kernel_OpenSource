f6b62ee8cd3dd90064e1fbbb73c665e1fe978a93
Harvey Huang <harvey.huang@mediatek.com>
2020-03-19 20:57:42 +0800

    [ALPS05024949] speech: remove speech property debug log
    [Detail]
    remove speech property debug log to avoid log too much
    
    MTK-Commit-Id: aaffb62396272d360c38a009a19898d4fa1bebbe
    
    Change-Id: Ie32fc8e3f4a5c058504f82c6313157fd25f99701
    Signed-off-by: Harvey Huang <harvey.huang@mediatek.com>
    CR-Id: ALPS05024949
    Feature: [Module]Audio Kernel

--- sound/soc/mediatek/mt6873/mt6873-misc-control.c
+++ sound/soc/mediatek/mt6873/mt6873-misc-control.c
@@ -2043,10 +2043,6 @@ static int speech_property_get(struct snd_kcontrol *kcontrol,
 	}
 	ucontrol->value.integer.value[0] = *sph_property;
 
-	if (strcmp(kcontrol->id.name, "Speech_A2M_Msg_ID") != 0)
-		dev_info(afe->dev, "%s(), %s = 0x%x\n", __func__,
-			 kcontrol->id.name, *sph_property);
-
 	return 0;
 }
 
@@ -2066,8 +2062,6 @@ static int speech_property_set(struct snd_kcontrol *kcontrol,
 	}
 	*sph_property = ucontrol->value.integer.value[0];
 
-	dev_info(afe->dev, "%s(), %s = 0x%x\n", __func__,
-		 kcontrol->id.name, *sph_property);
 	return 0;
 }
 
