12f394471006f689f61aab12eb3df5d9216bfae2
Sriharsha Allenki <sallenki@codeaurora.org>
2020-03-19 12:18:17 +0530

    usb: dwc3: gsi: Disable GSI wrapper on clearing run_stop
    Disable GSI wrapper on clearing run_stop to prevent
    unnecessary access by GSI wrapper because of the
    pending GEVNTCOUNT on the GSI interrupters.
    
    Change-Id: I8ee7fe25d33713487b74044a3a02deff0b7057aa
    Signed-off-by: Sriharsha Allenki <sallenki@codeaurora.org>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -2158,6 +2158,9 @@ static void dwc3_msm_notify_event(struct dwc3 *dwc, unsigned int event,
 		dwc3_msm_write_reg_field(mdwc->base,
 			GSI_GENERAL_CFG_REG(mdwc->gsi_reg[GENERAL_CFG_REG]),
 			BLOCK_GSI_WR_GO_MASK, true);
+		dwc3_msm_write_reg_field(mdwc->base,
+			GSI_GENERAL_CFG_REG(mdwc->gsi_reg),
+			GSI_EN_MASK, 0);
 		break;
 	default:
 		dev_dbg(mdwc->dev, "unknown dwc3 event\n");
