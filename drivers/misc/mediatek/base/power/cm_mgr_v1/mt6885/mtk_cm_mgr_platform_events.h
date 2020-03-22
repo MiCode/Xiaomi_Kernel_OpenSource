9cfd9ed0d5d8f91ff97ecc7146bb1c61e3eb80d0
Juju Sung <juju.sung@mediatek.com>
2020-03-22 19:31:29 +0800

    [ALPS05014853] low power: mt6885 cm_mgr_v1 porting
    [Detail]
    This patch merge cm_mgr from q0 to r0
    
    MTK-Commit-Id: c493acf6daadf5aaef14274f24118533443f868b
    
    Change-Id: I5c39cf618cf8621ce1558dbf06ca289449c5d181
    Signed-off-by: Juju Sung <juju.sung@mediatek.com>
    CR-Id: ALPS05014853
    Feature: [Module]Kernel Maintenance

--- drivers/misc/mediatek/base/power/cm_mgr_v1/mt6885/mtk_cm_mgr_platform_events.h
+++ drivers/misc/mediatek/base/power/cm_mgr_v1/mt6885/mtk_cm_mgr_platform_events.h
@@ -19,30 +19,59 @@
 
 #include <linux/tracepoint.h>
 
-TRACE_EVENT(CM_MGR__stall_raio_0,
-	TP_PROTO(unsigned int ratio_ratio),
-	TP_ARGS(ratio_ratio),
+TRACE_EVENT(CM_MGR__stall_ratio,
+	TP_PROTO(int _id,
+		unsigned int _ratio),
+	TP_ARGS(_id,
+		_ratio),
 	TP_STRUCT__entry(
-		__field(unsigned int, ratio_ratio)
+		__field(int, _id)
+		__field(unsigned int, _ratio)
 	),
 	TP_fast_assign(
-		__entry->ratio_ratio = ratio_ratio;
+		__entry->_id = _id;
+		__entry->_ratio = _ratio;
 	),
-	TP_printk("mcucfg_reg__MP0_CPU_AVG_STALL_RATIO=%d",
-		__entry->ratio_ratio)
+	TP_printk("CPU0_config_reg__CPU_AVG_STALL_RATIO=%d, _id=%d",
+		__entry->_ratio, __entry->_id)
 );
 
-TRACE_EVENT(CM_MGR__stall_raio_1,
-	TP_PROTO(unsigned int ratio_ratio),
-	TP_ARGS(ratio_ratio),
+TRACE_EVENT(CM_MGR__perf_hint,
+	TP_PROTO(int _force,
+		int _enable,
+		int _opp,
+		int _base,
+		int _count,
+		int _foece_count),
+	TP_ARGS(_force,
+		_enable,
+		_opp,
+		_base,
+		_count,
+		_foece_count),
 	TP_STRUCT__entry(
-		__field(unsigned int, ratio_ratio)
+		__field(int, _force)
+		__field(int, _enable)
+		__field(int, _opp)
+		__field(int, _base)
+		__field(int, _count)
+		__field(int, _foece_count)
 	),
 	TP_fast_assign(
-		__entry->ratio_ratio = ratio_ratio;
+		__entry->_force = _force;
+		__entry->_enable = _enable;
+		__entry->_opp = _opp;
+		__entry->_base = _base;
+		__entry->_count = _count;
+		__entry->_foece_count = _foece_count;
 	),
-	TP_printk("ca57a_12ffc_config__CPU_AVG_STALL_RATIO=%d",
-		__entry->ratio_ratio)
+	TP_printk("cm_mgr perf hint %s=%d %s=%d %s=%d %s=%d %s=%d %s=%d",
+			"force", __entry->_force,
+			"enable", __entry->_enable,
+			"opp", __entry->_opp,
+			"base", __entry->_base,
+			"count", __entry->_count,
+			"force_count", __entry->_foece_count)
 );
 
 #endif /* _TRACE_MTK_CM_MGR_EVENTS_H */
