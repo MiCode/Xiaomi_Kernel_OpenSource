dbd15264efbcec707fa118c42a55247572ad33f8
Juju Sung <juju.sung@mediatek.com>
2020-03-22 19:31:38 +0800

    [ALPS05014853] isp: mt6885 sensor porting
    [Detail]
    This patch merge sensor module from q0 to r0
    
    MTK-Commit-Id: 8fdcc6c02b312e764a4a153c7b78bfb4f9fdc006
    
    Change-Id: I3ecc9f2d8a339c0a42dd8f98a0848cf1af9a2764
    Signed-off-by: Juju Sung <juju.sung@mediatek.com>
    CR-Id: ALPS05014853
    Feature: [Module]Kernel Maintenance

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/imgsensor_i2c.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/imgsensor_i2c.c
@@ -16,11 +16,19 @@
 #include <linux/ratelimit.h>
 
 struct IMGSENSOR_I2C gi2c;
+#ifdef SENSOR_PARALLEISM
+struct mutex i2c_resource_mutex;
+#endif
 
 static const struct i2c_device_id gi2c_dev_id[] = {
 	{IMGSENSOR_I2C_DRV_NAME_0, 0},
 	{IMGSENSOR_I2C_DRV_NAME_1, 0},
 	{IMGSENSOR_I2C_DRV_NAME_2, 0},
+	{IMGSENSOR_I2C_DRV_NAME_3, 0},
+	{IMGSENSOR_I2C_DRV_NAME_4, 0},
+	{IMGSENSOR_I2C_DRV_NAME_5, 0},
+	{IMGSENSOR_I2C_DRV_NAME_6, 0},
+	{IMGSENSOR_I2C_DRV_NAME_7, 0},
 	{}
 };
 
@@ -39,6 +47,32 @@ static const struct of_device_id gof_device_id_2[] = {
 	{.compatible = IMGSENSOR_I2C_OF_DRV_NAME_2,},
 	{}
 };
+
+static const struct of_device_id gof_device_id_3[] = {
+	{.compatible = IMGSENSOR_I2C_OF_DRV_NAME_3,},
+	{}
+};
+
+static const struct of_device_id gof_device_id_4[] = {
+	{.compatible = IMGSENSOR_I2C_OF_DRV_NAME_4,},
+	{}
+};
+
+static const struct of_device_id gof_device_id_5[] = {
+	{.compatible = IMGSENSOR_I2C_OF_DRV_NAME_5,},
+	{}
+};
+
+static const struct of_device_id gof_device_id_6[] = {
+	{.compatible = IMGSENSOR_I2C_OF_DRV_NAME_6,},
+	{}
+};
+
+static const struct of_device_id gof_device_id_7[] = {
+	{.compatible = IMGSENSOR_I2C_OF_DRV_NAME_7,},
+	{}
+};
+
 #endif
 
 static int imgsensor_i2c_probe_0(struct i2c_client *client,
@@ -59,6 +93,41 @@ static int imgsensor_i2c_probe_2(struct i2c_client *client,
 				const struct i2c_device_id *id)
 {
 	gi2c.inst[IMGSENSOR_I2C_DEV_2].pi2c_client = client;
+	return 0;
+}
+
+static int imgsensor_i2c_probe_3(struct i2c_client *client,
+				const struct i2c_device_id *id)
+{
+	gi2c.inst[IMGSENSOR_I2C_DEV_3].pi2c_client = client;
+	return 0;
+}
+
+static int imgsensor_i2c_probe_4(struct i2c_client *client,
+				const struct i2c_device_id *id)
+{
+	gi2c.inst[IMGSENSOR_I2C_DEV_4].pi2c_client = client;
+	return 0;
+}
+
+static int imgsensor_i2c_probe_5(struct i2c_client *client,
+				const struct i2c_device_id *id)
+{
+	gi2c.inst[IMGSENSOR_I2C_DEV_5].pi2c_client = client;
+	return 0;
+}
+
+static int imgsensor_i2c_probe_6(struct i2c_client *client,
+				const struct i2c_device_id *id)
+{
+	gi2c.inst[IMGSENSOR_I2C_DEV_6].pi2c_client = client;
+	return 0;
+}
+
+static int imgsensor_i2c_probe_7(struct i2c_client *client,
+				const struct i2c_device_id *id)
+{
+	gi2c.inst[IMGSENSOR_I2C_DEV_7].pi2c_client = client;
 	return 0;
 }
 
@@ -103,6 +172,66 @@ static struct i2c_driver gi2c_driver[IMGSENSOR_I2C_DEV_MAX_NUM] = {
 #endif
 		},
 		.id_table = gi2c_dev_id,
+	},
+	{
+		.probe = imgsensor_i2c_probe_3,
+		.remove = imgsensor_i2c_remove,
+		.driver = {
+		.name = IMGSENSOR_I2C_DRV_NAME_3,
+		.owner = THIS_MODULE,
+#ifdef CONFIG_OF
+		.of_match_table = gof_device_id_3,
+#endif
+		},
+		.id_table = gi2c_dev_id,
+	},
+	{
+		.probe = imgsensor_i2c_probe_4,
+		.remove = imgsensor_i2c_remove,
+		.driver = {
+		.name = IMGSENSOR_I2C_DRV_NAME_4,
+		.owner = THIS_MODULE,
+#ifdef CONFIG_OF
+		.of_match_table = gof_device_id_4,
+#endif
+		},
+		.id_table = gi2c_dev_id,
+	},
+	{
+		.probe = imgsensor_i2c_probe_5,
+		.remove = imgsensor_i2c_remove,
+		.driver = {
+		.name = IMGSENSOR_I2C_DRV_NAME_5,
+		.owner = THIS_MODULE,
+#ifdef CONFIG_OF
+		.of_match_table = gof_device_id_5,
+#endif
+		},
+		.id_table = gi2c_dev_id,
+	},
+	{
+		.probe = imgsensor_i2c_probe_6,
+		.remove = imgsensor_i2c_remove,
+		.driver = {
+		.name = IMGSENSOR_I2C_DRV_NAME_6,
+		.owner = THIS_MODULE,
+#ifdef CONFIG_OF
+		.of_match_table = gof_device_id_6,
+#endif
+		},
+		.id_table = gi2c_dev_id,
+	},
+	{
+		.probe = imgsensor_i2c_probe_7,
+		.remove = imgsensor_i2c_remove,
+		.driver = {
+		.name = IMGSENSOR_I2C_DRV_NAME_7,
+		.owner = THIS_MODULE,
+#ifdef CONFIG_OF
+		.of_match_table = gof_device_id_7,
+#endif
+		},
+		.id_table = gi2c_dev_id,
 	}
 };
 
@@ -139,6 +268,9 @@ enum IMGSENSOR_RETURN imgsensor_i2c_init(
 	pi2c_cfg->pi2c_driver = &gi2c_driver[device];
 
 	mutex_init(&pi2c_cfg->i2c_mutex);
+#ifdef SENSOR_PARALLEISM
+	mutex_init(&i2c_resource_mutex);
+#endif
 
 	return IMGSENSOR_RETURN_SUCCESS;
 }
@@ -182,19 +314,19 @@ enum IMGSENSOR_RETURN imgsensor_i2c_read(
 
 	mutex_lock(&pi2c_cfg->i2c_mutex);
 
-	pinst->msg[0].addr  = id >> 1;
-	pinst->msg[0].flags = 0;
-	pinst->msg[0].len   = write_length;
-	pinst->msg[0].buf   = pwrite_data;
+	pi2c_cfg->msg[0].addr  = id >> 1;
+	pi2c_cfg->msg[0].flags = 0;
+	pi2c_cfg->msg[0].len   = write_length;
+	pi2c_cfg->msg[0].buf   = pwrite_data;
 
-	pinst->msg[1].addr  = id >> 1;
-	pinst->msg[1].flags = I2C_M_RD;
-	pinst->msg[1].len   = read_length;
-	pinst->msg[1].buf   = pread_data;
+	pi2c_cfg->msg[1].addr  = id >> 1;
+	pi2c_cfg->msg[1].flags = I2C_M_RD;
+	pi2c_cfg->msg[1].len   = read_length;
+	pi2c_cfg->msg[1].buf   = pread_data;
 
 	if (mtk_i2c_transfer(
 			pinst->pi2c_client->adapter,
-			pinst->msg,
+			pi2c_cfg->msg,
 			IMGSENSOR_I2C_MSG_SIZE_READ,
 			(pi2c_cfg->pinst->status.filter_msg)
 				? I2C_A_FILTER_MSG : 0,
@@ -225,7 +357,7 @@ enum IMGSENSOR_RETURN imgsensor_i2c_write(
 {
 	struct IMGSENSOR_I2C_INST *pinst = pi2c_cfg->pinst;
 	enum   IMGSENSOR_RETURN    ret   = IMGSENSOR_RETURN_SUCCESS;
-	struct i2c_msg     *pmsg  = pinst->msg;
+	struct i2c_msg     *pmsg  = pi2c_cfg->msg;
 	u8                 *pdata = pwrite_data;
 	u8                 *pend  = pwrite_data + write_length;
 	int i   = 0;
@@ -250,7 +382,7 @@ enum IMGSENSOR_RETURN imgsensor_i2c_write(
 
 	if (mtk_i2c_transfer(
 			pinst->pi2c_client->adapter,
-			pinst->msg,
+			pi2c_cfg->msg,
 			i,
 			(pi2c_cfg->pinst->status.filter_msg)
 				? I2C_A_FILTER_MSG : 0,
@@ -277,10 +409,73 @@ void imgsensor_i2c_filter_msg(struct IMGSENSOR_I2C_CFG *pi2c_cfg, bool en)
 }
 
 #ifdef IMGSENSOR_LEGACY_COMPAT
+#ifdef SENSOR_PARALLEISM
+#include <linux/unistd.h>
+#include <linux/syscalls.h>
+
+
+
+struct IMGSENSOR_I2C_CFG *pgi2c_cfg_legacy[IMGSENSOR_SENSOR_IDX_MAX_NUM];
+pid_t tid_mapping[IMGSENSOR_SENSOR_IDX_MAX_NUM];
+#else
 struct IMGSENSOR_I2C_CFG *pgi2c_cfg_legacy;
+
+#endif
+
+
+
 void imgsensor_i2c_set_device(struct IMGSENSOR_I2C_CFG *pi2c_cfg)
 {
+#ifdef SENSOR_PARALLEISM
+	int i = 0;
+	pid_t _tid = sys_gettid();
+
+	mutex_lock(&i2c_resource_mutex);
+	if (pi2c_cfg == NULL) {
+		for (i = 0; i < IMGSENSOR_SENSOR_IDX_MAX_NUM; i++) {
+			if (tid_mapping[i] == _tid) {
+				pgi2c_cfg_legacy[i] = NULL;
+				tid_mapping[i] = 0;
+				break;
+			}
+		}
+	} else {
+		for (i = 0; i < IMGSENSOR_SENSOR_IDX_MAX_NUM; i++) {
+			if (tid_mapping[i] == 0) {
+				pgi2c_cfg_legacy[i] = pi2c_cfg;
+				tid_mapping[i] = _tid;
+				break;
+			}
+		}
+	}
+	mutex_unlock(&i2c_resource_mutex);
+	/* PK_DBG("set tid = %d i = %d pi2c_cfg %p\n", _tid, i, pi2c_cfg); */
+#else
 	pgi2c_cfg_legacy = pi2c_cfg;
+
+#endif
+
+}
+struct IMGSENSOR_I2C_CFG *imgsensor_i2c_get_device(void)
+{
+#ifdef SENSOR_PARALLEISM
+	int i = 0;
+	struct IMGSENSOR_I2C_CFG *pi2c_cfg = NULL;
+	pid_t _tid = sys_gettid();
+	/* mutex_lock(&i2c_resource_mutex); */
+
+	for (i = 0; i < IMGSENSOR_SENSOR_IDX_MAX_NUM; i++) {
+		if (tid_mapping[i] == _tid) {
+			pi2c_cfg = pgi2c_cfg_legacy[i];
+			break;
+		}
+	}
+	/* mutex_unlock(&i2c_resource_mutex); */
+	/* PK_DBG("get tid %d, i =%d, pi2c_cfg %p\n",_tid, i,pi2c_cfg); */
+	return pi2c_cfg;
+#else
+	return pgi2c_cfg_legacy;
+#endif
 }
 #endif
 
