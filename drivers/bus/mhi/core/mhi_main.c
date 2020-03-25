405811f23b852242436d0c94750dc74edb1729e5
Bhaumik Bhatt <bbhatt@codeaurora.org>
2020-03-25 12:57:13 -0700

    mhi: core: update debugfs entries to include new entries
    Include ignore override and bandwidth scaling status in the
    states debugfs file. Also, ensure time is printed consistently
    in all debugfs files.
    
    Change-Id: Ib0b3c81cf3b4069c2b617d8cc850981eb737d7e7
    Signed-off-by: Bhaumik Bhatt <bbhatt@codeaurora.org>

--- drivers/bus/mhi/core/mhi_main.c
+++ drivers/bus/mhi/core/mhi_main.c
@@ -2150,18 +2150,22 @@ error_invalid_state:
 int mhi_debugfs_mhi_states_show(struct seq_file *m, void *d)
 {
 	struct mhi_controller *mhi_cntrl = m->private;
+	struct mhi_link_info *cur_info = &mhi_cntrl->mhi_link_info;
 
 	seq_printf(m,
-		   "[%llu ns]: pm_state:%s dev_state:%s EE:%s M0:%u M2:%u M3:%u M3_Fast:%u wake:%d dev_wake:%u alloc_size:%u pending_pkts:%u\n",
+		   "[%llu ns]: pm_state:%s dev_state:%s EE:%s M0:%u M2:%u M3:%u M3_Fast:%u wake:%d ignore_override:%d dev_wake:%u alloc_size:%u pending_pkts:%u last_requested_bw:GEN%dx%d\n",
 		   sched_clock(),
 		   to_mhi_pm_state_str(mhi_cntrl->pm_state),
 		   TO_MHI_STATE_STR(mhi_cntrl->dev_state),
 		   TO_MHI_EXEC_STR(mhi_cntrl->ee),
 		   mhi_cntrl->M0, mhi_cntrl->M2, mhi_cntrl->M3,
 		   mhi_cntrl->M3_FAST, mhi_cntrl->wake_set,
+		   mhi_cntrl->ignore_override,
 		   atomic_read(&mhi_cntrl->dev_wake),
 		   atomic_read(&mhi_cntrl->alloc_size),
-		   atomic_read(&mhi_cntrl->pending_pkts));
+		   atomic_read(&mhi_cntrl->pending_pkts),
+		   cur_info->target_link_speed,
+		   cur_info->target_link_width);
 	return 0;
 }
 
@@ -2277,7 +2281,7 @@ int mhi_debugfs_mhi_vote_show(struct seq_file *m, void *d)
 
 	mhi_dev = mhi_cntrl->mhi_dev;
 
-	seq_printf(m, "At %llu ns:\n", sched_clock());
+	seq_printf(m, "[%llu ns]:\n", sched_clock());
 	seq_printf(m, "%s: device:%u, bus:%u\n", mhi_dev->chan_name,
 		   atomic_read(&mhi_dev->dev_vote),
 		   atomic_read(&mhi_dev->bus_vote));
