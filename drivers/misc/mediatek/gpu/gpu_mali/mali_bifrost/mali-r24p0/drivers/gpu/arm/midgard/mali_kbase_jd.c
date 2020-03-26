c341f77ca29adb118474c0bfa5a7c4cb853c6e62
Dong Guo <dong.guo@mediatek.com>
2020-03-26 16:01:24 +0800

    [ALPS05020907] GPU: Bifrost GPU r24p0 DDK kernel build pass version
    + add mt6768 platform code
    + add gator
    + add COMMON DVFS and other MTK modification
    + fix build error
    
    MTK-Commit-Id: 13ead85b40d5bbc20eadbc717f77129a881a0820
    
    Change-Id: I7c3faab8d781cb8089d6f1cee04d28e763018476
    CR-Id: ALPS05020907
    Feature: OpenGL|ES
    Signed-off-by: Dong Guo <dong.guo@mediatek.com>

--- drivers/misc/mediatek/gpu/gpu_mali/mali_bifrost/mali-r24p0/drivers/gpu/arm/midgard/mali_kbase_jd.c
+++ drivers/misc/mediatek/gpu/gpu_mali/mali_bifrost/mali-r24p0/drivers/gpu/arm/midgard/mali_kbase_jd.c
@@ -750,6 +750,12 @@ static bool jd_submit_atom(struct kbase_context *const kctx,
 	katom->softjob_data = NULL;
 	katom->renderpass_id = user_atom->renderpass_id;
 
+#if defined(MTK_GPU_BM_2)
+	/* set up frame number */
+	katom->frame_nr = user_atom->frame_nr;
+#endif
+
+
 	/* Implicitly sets katom->protected_state.enter as well. */
 	katom->protected_state.exit = KBASE_ATOM_EXIT_PROTECTED_CHECK;
 
