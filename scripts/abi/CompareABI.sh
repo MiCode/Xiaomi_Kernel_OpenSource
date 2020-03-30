2b897b4234099e9d3ca46a8751bf1d631a9cc8b0
Chun-Hung Wu <chun-hung.wu@mediatek.com>
2020-03-30 11:32:00 +0800

    gki: abi monitor support for kernel5.4
    [Detail]
    Support abi monitor for quark/kernel-5.4 evb6873_64_defconfig
    on branch: alps-dev-q0.mp2-common-kernel-5.4.
    
    (cherry-picked from commit 9c9bc89970425ee7b8b9e15267d0529d66c9813a)
    Signed-off-by: Chun-Hung Wu <chun-hung.wu@mediatek.com>
    Change-Id: I95ca6b34b100a15bd16280a03207e24af3ed73d3
    CR-Id: ALPS05298037
    Feature: eMMC Boot Up
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

---
+++ scripts/abi/CompareABI.sh
@@ -0,0 +1,137 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0
+
+green='\e[0;32m'
+red='\e[0;31m'
+eol='\e[0m'
+
+BASE_DIR=$PWD
+ABIGAIL_DIR=$BASE_DIR/../kernel/build/abi
+ABIGAIL_BUILD_SCRIPT=$ABIGAIL_DIR/bootstrap_src_build
+ABI_XML_DIR=$BASE_DIR/scripts/abi/abi_xml
+ABI_RESULT_DIR=$BASE_DIR/scripts/abi/abi_xml
+ORI_ABI_XML=abi_$src_defconfig.xml
+TARGET_ABI_XML=abi_target.xml
+ABI_REPORT=abi-report.out
+FINAL_ABI_REPORT=abi-report-final.out
+TARGET_KERNEL_DIR=$BASE_DIR/out
+TARGET_VMLINUX_DIR=$BASE_DIR/out_vmlinux
+
+echo "Get ABIGAIL_VERSION from $ABIGAIL_BUILD_SCRIPT"
+ABIGAIL_VERSION=`grep "ABIGAIL_VERSION=" $ABIGAIL_BUILD_SCRIPT | cut -f2- -d=`
+ABIGAIL_DIR_RELEASE=$ABIGAIL_DIR/abigail-inst/$ABIGAIL_VERSION
+echo "ABIGAIL_DIR_RELEASE=$ABIGAIL_DIR_RELEASE"
+
+function print_usage(){
+	echo -e "${green}Script for auto generate target_branch's ABI xml \
+based on src_defconfig and compare with abi_{src_defconfig}.xml${eol}"
+	echo ""
+	echo -e "${red}Command for local test:${eol}"
+	echo "[src_defconfig] mode=m ./scripts/abi/CompareABI.sh"
+	echo ""
+	echo -e "${green}Description:${eol}"
+	echo "[src_defconfig]: source project defconfig"
+	echo ""
+	echo -e "${green}Example:${eol} ${red}src_defconfig=\
+evb6873_64_defconfig mode=m \
+./scripts/abi/CompareABI.sh 2>&1 | tee buildABI.log${eol}"
+	echo ""
+	echo -e "${green}Script for auto generate target_branch's ABI xml \
+based on src_defconfig and compare with abi_{src_defconfig}.xml and save abi \
+monitor result to [abi_result_path]${eol}"
+	echo ""
+	echo -e "${red}Command for local test:${eol}"
+	echo "[src_defconfig] mode=m [abi_result_path] \
+./scripts/abi/CompareABI.sh"
+	echo ""
+	echo -e "${green}Description:${eol}"
+	echo "[src_defconfig]: source project defconfig"
+	echo ""
+	echo "[abi_result_path]: absolute path to put abi monitor result"
+	echo ""
+	echo -e "${green}Example:${eol} ${red}src_defconfig=\
+evb6873_64_defconfig mode=m abi_result_path=absolute_path \
+./scripts/abi/CompareABI.sh 2>&1 | tee buildABI.log${eol}"
+	echo ""
+	echo -e "${red}Command for delete temp files:${eol}"
+	echo "mode=d ./scripts/abi/CompareABI.sh"
+	echo ""
+	echo -e "${green}Description:${eol}"
+	echo ""
+	echo -e "${green}Example:${eol} ${red}mode=d \
+./scripts/abi/CompareABI.sh${eol}"
+}
+function del_temp_files(){
+	echo "Delete temp files $TARGET_KERNEL_DIR"
+	rm -rf $TARGET_KERNEL_DIR
+	echo "Delete temp files $TARGET_VMLINUX_DIR"
+	rm -rf $TARGET_VMLINUX_DIR
+	echo "Delete temp files $ABI_RESULT_DIR/$TARGET_ABI_XML"
+	rm -rf $ABI_RESULT_DIR/$TARGET_ABI_XML
+	echo "Delete temp files $ABI_RESULT_DIR/$ABI_REPORT"
+	rm -rf $ABI_RESULT_DIR/$ABI_REPORT
+	echo "Delete temp files $ABI_RESULT_DIR/$FINAL_ABI_REPORT"
+	rm -rf $ABI_RESULT_DIR/$FINAL_ABI_REPORT
+}
+
+if [[ "$1" == "h" ]] || [[ "$1" == "help" ]] || [ -z "mode" ]
+then
+	print_usage
+fi
+
+if [ -z "$abi_result_path" ]
+then
+	echo "ABI_XML_DIR=$ABI_XML_DIR"
+	echo "ABI_RESULT_DIR=$ABI_RESULT_DIR"
+else
+	ABI_RESULT_DIR=$abi_result_path
+	echo "ABI_XML_DIR=$ABI_XML_DIR"
+	echo "ABI_RESULT_DIR=$ABI_RESULT_DIR"
+fi
+
+if [ "$mode" == "d" ]
+then
+	del_temp_files
+fi
+
+if [ "$mode" == "m" ]
+then
+	#Build libabigail first
+	$ABIGAIL_BUILD_SCRIPT
+	#remove temp files first
+	del_temp_files
+	echo "Generate .config from src_defconfig:$src_defconfig"
+	cd ..
+	export PATH=$PWD\
+/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/:\
+$PWD/prebuilts/clang/host/linux-x86/clang-r353983c/bin/:$PATH
+	cd $BASE_DIR
+	make ARCH=arm64 CLANG_TRIPLE=aarch64-linux-gnu- \
+	CROSS_COMPILE=aarch64-linux-android- CC=clang $src_defconfig O=out
+	mosesq make ARCH=arm64 CLANG_TRIPLE=aarch64-linux-gnu- \
+	CROSS_COMPILE=aarch64-linux-android- CC=clang O=out -j24 -k
+
+
+	echo "Generate ABI xml:$TARGET_ABI_XML from kernel \
+tree:$TARGET_VMLINUX_DIR"
+	#Use abi_dump to generate $TARGET_ABI_XML
+	#export $ABIGAIL_DIR_RELEASE bin and lib
+	export PATH=${ABIGAIL_DIR_RELEASE}/bin:${PATH}
+	export LD_LIBRARY_PATH=${ABIGAIL_DIR_RELEASE}/lib:\
+${ABIGAIL_DIR_RELEASE}/lib/elfutils:${LD_LIBRARY_PATH}
+	echo "Copy vmlinux from $TARGET_KERNEL_DIR to $TARGET_VMLINUX_DIR"
+	mkdir -p $TARGET_VMLINUX_DIR
+	cp $TARGET_KERNEL_DIR/vmlinux $TARGET_VMLINUX_DIR
+	cd $ABIGAIL_DIR
+	python dump_abi --linux-tree $TARGET_VMLINUX_DIR --out-file \
+	$ABI_RESULT_DIR/$TARGET_ABI_XML
+	echo "Generate ABI report:$ABI_REPORT from \
+--baseline:$ABI_XML_DIR/$ORI_ABI_XML --new:$ABI_RESULT_DIR/$TARGET_ABI_XML"
+	python diff_abi --baseline $ABI_XML_DIR/$ORI_ABI_XML \
+	--new $ABI_RESULT_DIR/$TARGET_ABI_XML \
+	--report $ABI_RESULT_DIR/$ABI_REPORT
+
+	cd $BASE_DIR
+	echo "Generate $FINAL_ABI_REPORT"
+	abi_result_path=$abi_result_path ./scripts/abi/FinalABI.sh
+fi
---
+++ scripts/abi/FinalABI.sh
@@ -0,0 +1,112 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0
+
+green='\e[0;32m'
+red='\e[0;31m'
+eol='\e[0m'
+
+BASE_DIR=$PWD
+ABI_DIR=$BASE_DIR/scripts/abi
+ABI_XML_DIR=$BASE_DIR/scripts/abi/abi_xml
+ABI_RESULT_DIR=$BASE_DIR/scripts/abi/abi_xml
+ORI_ABI_XML=abi_ori.xml
+TARGET_ABI_XML=abi_target.xml
+ABI_REPORT=abi-report.out
+FINAL_ABI_REPORT=abi-report-final.out
+
+#include abi_white_list to bypass violations
+source $ABI_DIR/abi_white_list
+#Find Delete/Changed/Added and leaf type change
+check_arr=("\[D\]" "\[C\]" "\[A\]" "^'.*' changed:$")
+#Find Delete/Changed and leaf type change coz
+#diff_abi default report Added symbol since:
+#https://sourceware.org/git/?p=\
+#libabigail.git;a=commit;h=e84537afeaa7262e3584a772450ed6ed7d193a9b
+#And our target.xml contains both vmlinux and kernel module exported symbols.
+#Kernel module exported symbols will not in ori.xml should not take into account
+#as an abi violation symbol.
+#check_arr=("\[D\]" "\[C\]" "^'.*' changed:$")
+
+is_abi_violation_bypass=0
+declare -i abi_violation_count=0
+
+function print_usage(){
+	echo -e "${green}Script for auto generate \
+$ABI_RESULT_DIR/$FINAL_ABI_REPORT from $ABI_RESULT_DIR/$ABI_REPORT ${eol}"
+	echo ""
+	echo -e "${red}Command for local test:${eol}"
+	echo ""
+	echo -e "${green}Example:${eol} ${red}./scripts/abi/FinalABI.sh${eol}"
+	echo ""
+	echo -e "${green}Script for auto generate \
+$FINAL_ABI_REPORT by specified abi_result_path ${eol}"
+	echo ""
+	echo -e "${red}Command for local test:${eol}"
+	echo "[abi_result_path] ./scripts/abi/FinalABI.sh"
+	echo ""
+	echo -e "${green}Description:${eol}"
+	echo "[abi_result_path]: absolute path to generate fianl abi report"
+	echo ""
+	echo -e "${green}Example:${eol} ${red}abi_result_path=absolute_path \
+/scripts/abi/FinalABI.sh${eol}"
+	exit -1
+}
+
+if [ -z "$abi_result_path" ]
+then
+        echo "ABI_XML_DIR=$ABI_XML_DIR"
+        echo "ABI_RESULT_DIR=$ABI_RESULT_DIR"
+else
+        ABI_RESULT_DIR=$abi_result_path
+        echo "ABI_XML_DIR=$ABI_XML_DIR"
+        echo "ABI_RESULT_DIR=$ABI_RESULT_DIR"
+fi
+
+if [[ "$1" == "h" ]] || [[ "$1" == "help" ]]
+then
+	print_usage
+fi
+
+#remove temp files first
+rm -rf $ABI_RESULT_DIR/$FINAL_ABI_REPORT
+
+exec < $ABI_RESULT_DIR/$ABI_REPORT
+while read line
+do
+	for ((i=0; i < ${#check_arr[@]}; i++))
+	do
+		if [[ $line =~ ${check_arr[$i]} ]]
+		then
+			is_abi_violation_bypass=0
+			for ((j=0; j < ${#bypass_arr[@]}; j++))
+			do
+				if [[ $line =~ ${bypass_arr[$j]} ]]
+				then
+					is_abi_violation_bypass=1
+					break
+				fi
+			done
+
+			if [[ $is_abi_violation_bypass == 0 ]]
+			then
+				abi_violation_count+=1
+			fi
+			break
+		fi
+	done
+	#write $line to $FINAL_ABI_REPORT if $is_abi_violation = 0
+	if [[ $is_abi_violation_bypass == 0 ]]
+	then
+		echo $line >> $ABI_RESULT_DIR/$FINAL_ABI_REPORT
+	fi
+done
+
+if [[ $abi_violation_count == 0 ]]
+then
+	echo "ABI Monitor check PASS!!!"
+else
+	echo "ABI Monitor check FAILED!!!\
+Final abi_violation_count=$abi_violation_count!!!"
+echo "Please check report: $ABI_RESULT_DIR/$FINAL_ABI_REPORT for details."
+fi
+exit $abi_violation_count
