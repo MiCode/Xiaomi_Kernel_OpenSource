a8f636f2bacfb00729786b558607d5b965f9daf4
Eric Biggers <ebiggers@google.com>
2020-04-03 12:06:11 -0700

    ANDROID: block: require drivers to declare supported crypto key type(s)
    We need a way to tell which type of keys the inline crypto hardware
    supports (standard, wrapped, or both), so that fallbacks can be used
    when needed (either blk-crypto-fallback, or fscrypt fs-layer crypto).
    
    We can't simply assume that
    
        keyslot_mgmt_ll_ops::derive_raw_secret == NULL
    
    means only standard keys are supported and that
    
        keyslot_mgmt_ll_ops::derive_raw_secret != NULL
    
    means that only wrapped keys are supported, because device-mapper
    devices always implement this method.  Also, hardware might support both
    types of keys.
    
    Therefore, add a field keyslot_manager::features which contains a
    bitmask of flags which indicate the supported types of keys.  Drivers
    will need to fill this in.  This patch makes the UFS standard crypto
    code set BLK_CRYPTO_FEATURE_STANDARD_KEYS, but UFS variant drivers may
    need to set BLK_CRYPTO_FEATURE_WRAPPED_KEYS instead.
    
    Then, make keyslot_manager_crypto_mode_supported() take the key type
    into account.
    
    Bug: 137270441
    Bug: 151100202
    Test: 'atest vts_kernel_encryption_test' on Pixel 4 with the
          inline crypto patches backported, and also on Cuttlefish.
    Change-Id: Ied846c2767c1fd2f438792dcfd3649157e68b005
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Git-commit: 8f078b1b3aae280f240a75d27457e5e76dd0a92a
    Git-repo: https://android.googlesource.com/kernel/common/+/refs/heads/android-4.19
    [neersoni@codeaurora.org: key capability parameter added for ufs and emmc]
    Signed-off-by: Neeraj Soni <neersoni@codeaurora.org>

--- drivers/mmc/host/cqhci-crypto.c
+++ drivers/mmc/host/cqhci-crypto.c
@@ -335,8 +335,10 @@ int cqhci_host_init_crypto_spec(struct cqhci_host *host,
 	cqhci_crypto_clear_all_keyslots(host);
 
 	host->ksm = keyslot_manager_create(host->mmc->parent,
-					   cqhci_num_keyslots(host),
-					   ksm_ops, crypto_modes_supported,
+					   cqhci_num_keyslots(host), ksm_ops,
+					   BLK_CRYPTO_FEATURE_STANDARD_KEYS |
+                                           BLK_CRYPTO_FEATURE_WRAPPED_KEYS,
+					   crypto_modes_supported,
 					   host);
 
 	if (!host->ksm) {
