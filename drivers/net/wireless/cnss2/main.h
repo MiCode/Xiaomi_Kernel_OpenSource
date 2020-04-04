b4942ee4e6bc919cdc27abf5e39e1302ae800444
Yue Ma <yuem@codeaurora.org>
2020-04-03 18:57:58 -0700

    cnss2: Add fallback support for device specific path loading
    Add the logic to fall back to default path if it fails to load
    firmware from device specific path.
    
    Change-Id: I0595a3be97b6f803be232b73fb35b8b5b3c52b0e
    Signed-off-by: Yue Ma <yuem@codeaurora.org>

--- drivers/net/wireless/cnss2/main.h
+++ drivers/net/wireless/cnss2/main.h
@@ -369,7 +369,9 @@ struct cnss_plat_data {
 	u8 *diag_reg_read_buf;
 	u8 cal_done;
 	u8 powered_on;
+	u8 use_fw_path_with_prefix;
 	char firmware_name[MAX_FIRMWARE_NAME_LEN];
+	char fw_fallback_name[MAX_FIRMWARE_NAME_LEN];
 	struct completion rddm_complete;
 	struct completion recovery_complete;
 	struct cnss_control_params ctrl_params;
