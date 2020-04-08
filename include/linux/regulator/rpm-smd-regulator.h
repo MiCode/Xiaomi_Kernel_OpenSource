c2f05ed6b5be99d81487ce40d18aea1fe5e2b297
Jishnu Prakash <jprakash@codeaurora.org>
2020-04-08 16:54:59 +0530

    regulators: rpm-smd: Remove unused exported functions
    Remove all exported functions as clients need to use only
    functions exported from core regulator framework.
    Change rpm-smd config to tristate to ensure it can be
    loaded as a module.
    
    Change-Id: I3f72a19b23fb2b97e0a13fd93fad5eade15ff535
    Signed-off-by: Jishnu Prakash <jprakash@codeaurora.org>

--- include/linux/regulator/rpm-smd-regulator.h
+++ include/linux/regulator/rpm-smd-regulator.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2012-2013, 2015, 2017, 2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2013, 2015, 2017, 2019-2020, The Linux Foundation. All rights reserved.
  */
 
 #ifndef _LINUX_REGULATOR_RPM_SMD_H
@@ -81,47 +81,6 @@ enum rpm_regulator_mode {
 	RPM_REGULATOR_MODE_IPEAK,
 	RPM_REGULATOR_MODE_HPM,
 };
-
-#ifdef CONFIG_REGULATOR_RPM_SMD
-
-struct rpm_regulator *rpm_regulator_get(struct device *dev, const char *supply);
-
-void rpm_regulator_put(struct rpm_regulator *regulator);
-
-int rpm_regulator_enable(struct rpm_regulator *regulator);
-
-int rpm_regulator_disable(struct rpm_regulator *regulator);
-
-int rpm_regulator_set_voltage(struct rpm_regulator *regulator, int min_uV,
-			      int max_uV);
-
-int rpm_regulator_set_mode(struct rpm_regulator *regulator,
-				enum rpm_regulator_mode mode);
-
-int __init rpm_smd_regulator_driver_init(void);
-
-#else
-
-static inline struct rpm_regulator *rpm_regulator_get(struct device *dev,
-					const char *supply) { return NULL; }
-
-static inline void rpm_regulator_put(struct rpm_regulator *regulator) { }
-
-static inline int rpm_regulator_enable(struct rpm_regulator *regulator)
-			{ return 0; }
-
-static inline int rpm_regulator_disable(struct rpm_regulator *regulator)
-			{ return 0; }
-
-static inline int rpm_regulator_set_voltage(struct rpm_regulator *regulator,
-					int min_uV, int max_uV) { return 0; }
-
-static inline int rpm_regulator_set_mode(struct rpm_regulator *regulator,
-				enum rpm_regulator_mode mode) { return 0; }
-
-static inline int __init rpm_smd_regulator_driver_init(void) { return 0; }
-
-#endif /* CONFIG_REGULATOR_RPM_SMD */
 
 #ifdef CONFIG_DEBUG_FS
 
