bfbedb4d246a31efadb65700299a6013c5206fb5
Juju Sung <juju.sung@mediatek.com>
2020-04-09 10:02:11 +0800

    [ALPS05014853] timesync: add sspm support for mt6885
    [Detail]
    port mt6885 sspm/mcupm driver from q0 to r0
    
    MTK-Commit-Id: 8b22d6e9089e8f9a683ec508a40fd7237df87211
    
    Change-Id: I3659d879cb4cfc3d379d938c3a70ed37d2baa32b
    Signed-off-by: Juju Sung <juju.sung@mediatek.com>
    CR-Id: ALPS05014853
    Feature: [Module]Kernel Maintenance

--- drivers/misc/mediatek/timer/timesync/mtk_sys_timer_mbox.h
+++ drivers/misc/mediatek/timer/timesync/mtk_sys_timer_mbox.h
@@ -14,7 +14,20 @@
 #ifndef __MTK_SYS_TIMER_MBOX_H__
 #define __MTK_SYS_TIMER_MBOX_H__
 
+enum mbox_idx {
+	MBOX_TICK_H = 0,
+	MBOX_TICK_L,
+	MBOX_TS_H,
+	MBOX_TS_L,
+	MBOX_DEBUG_TS_H,
+	MBOX_DEBUG_TS_L,
+};
+
+#if defined(SSPM_V2)
+#include <sspm_mbox_pin.h>
+#else
 #include <sspm_ipi_mbox_layout.h>
+#endif
 
 #define SYS_TIMER_MBOX                      SHAREMBOX_NO_MCDI
 #define SYS_TIMER_MBOX_OFFSET_BASE          SHAREMBOX_OFFSET_TIMESTAMP
@@ -31,5 +44,11 @@
 #define SYS_TIMER_MBOX_DEBUG_TS_H           (SYS_TIMER_MBOX_OFFSET_BASE + 4)
 #define SYS_TIMER_MBOX_DEBUG_TS_L           (SYS_TIMER_MBOX_OFFSET_BASE + 5)
 
+#if defined(CONFIG_MTK_TINYSYS_MCUPM_SUPPORT)
+#include <mcupm_driver.h>
+
+#define SYS_TIMER_MCUPM_MBOX                MCUPM_MBOX_NO_SUSPEND
+#define SYS_TIMER_MCUPM_MBOX_OFFSET_BASE    MCUPM_MBOX_OFFSET_TIMESTAMP
+#endif
 #endif /* __MTK_SYS_TIMER_MBOX_H__ */
 
