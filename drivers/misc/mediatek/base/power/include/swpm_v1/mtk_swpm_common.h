70613bd929c269d3dfcc58c514d9221198348e99
Juju Sung <juju.sung@mediatek.com>
2020-04-09 10:00:56 +0800

    [ALPS05014853] low power: add mt6885 swpm driver
    [Detail]
    This patch add swpm_v1 driver from q to r
    
    MTK-Commit-Id: fe1d36a693be0b0cdecc4bb6ba6101f0e9230151
    
    Change-Id: Iec1b18ca62f1ef3144aff6fed7a428670399b362
    Signed-off-by: Juju Sung <juju.sung@mediatek.com>
    CR-Id: ALPS05014853
    Feature: [Module]Kernel Maintenance

--- drivers/misc/mediatek/base/power/include/swpm_v1/mtk_swpm_common.h
+++ drivers/misc/mediatek/base/power/include/swpm_v1/mtk_swpm_common.h
@@ -48,16 +48,35 @@
 #define swpm_clr_status(type)  (swpm_status &= ~(1 << type))
 #define for_each_pwr_mtr(i)    for (i = 0; i < NR_POWER_METER; i++)
 
+struct swpm_mem_ref_tbl {
+	bool valid;
+	phys_addr_t *virt;
+};
+
 extern bool swpm_debug;
 extern unsigned int swpm_status;
 extern struct mutex swpm_mutex;
+extern unsigned int swpm_log_mask;
+extern struct timer_list swpm_timer;
 
 extern char *swpm_power_rail_to_string(enum power_rail p);
 extern int swpm_platform_init(void);
+extern int swpm_create_procfs(void);
+extern void swpm_update_periodic_timer(void);
+extern int swpm_set_periodic_timer(void *func);
+extern void swpm_get_rec_addr(phys_addr_t *phys,
+			      phys_addr_t *virt,
+			      unsigned long long *size);
+extern int swpm_reserve_mem_init(phys_addr_t *virt,
+				 unsigned long long *size);
+extern int swpm_interface_manager_init(struct swpm_mem_ref_tbl *ref_tbl,
+				       unsigned int tbl_size);
 extern void swpm_set_enable(unsigned int type, unsigned int enable);
 extern void swpm_set_update_cnt(unsigned int type, unsigned int cnt);
 extern void swpm_send_init_ipi(unsigned int addr, unsigned int size,
 	unsigned int ch_num);
 extern void swpm_update_lkg_table(void);
+extern unsigned int swpm_get_avg_power(unsigned int type,
+				       unsigned int avg_window);
 #endif
 
