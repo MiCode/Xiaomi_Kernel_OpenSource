a1f5f6ec12294f05476b39369f89c46fc58261fe
Eddie Hung <eddie.hung@mediatek.com>
2020-04-09 10:00:42 +0800

    [ALPS04737280] xhci: add XHCI_DEV_WITH_SYNC_EP
    [Detail]
    Add XHCI_DEV_WITH_SYNC_EP to prevent build error
    
    MTK-Commit-Id: bb6f53f3683207b204b596f542aa1c55d1f09a87
    
    Change-Id: I7e6bd9b93e30cc3c4b196305a24b2463879159c8
    CR-Id: ALPS04737280
    Feature: [Android Default] USB OTG
    Signed-off-by: Eddie Hung <eddie.hung@mediatek.com>
    (cherry picked from commit e356fc3d6c4edf6c5e4c66dcf5fbfdff1e9106cc)

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1843,6 +1843,7 @@ struct xhci_hcd {
 #define XHCI_INTEL_USB_ROLE_SW	BIT_ULL(31)
 #define XHCI_RESET_PLL_ON_DISCONNECT	BIT_ULL(34)
 #define XHCI_SNPS_BROKEN_SUSPEND    BIT_ULL(35)
+#define XHCI_DEV_WITH_SYNC_EP	BIT_ULL(63)
 
 	unsigned int		num_active_eps;
 	unsigned int		limit_active_eps;
