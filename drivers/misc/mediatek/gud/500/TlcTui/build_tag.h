51fd9422ebcc68930ce30c989c2156aebe5d6f3f
Pangyen Chen <pangyen.chen@mediatek.com>
2020-04-09 10:03:22 +0800

    [ALPS04865088] Trustonic TEE: Upload GUD for Kinibi 500
    GUD is linux kernel driver for Trustonic TEE which acts as a bridge
    between normal world and secure world. To upgrade to Kinibi 500,
    GUD driver is essential.
    
    MTK-Commit-Id: 6fa0f233564f560c727f5884ac2470136bf62c2f
    
    Change-Id: I11dbf8c26210babbc590af2b2f7896cb98ac73dc
    Signed-off-by: Pangyen Chen <pangyen.chen@mediatek.com>
    CR-Id: ALPS04865088
    Feature: Trustonic TEE (Trusted Execution Environment)

---
+++ drivers/misc/mediatek/gud/500/TlcTui/build_tag.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2013-2018 TRUSTONIC LIMITED
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+#ifndef MOBICORE_COMPONENT_BUILD_TAG
+#define MOBICORE_COMPONENT_BUILD_TAG \
+	"t-base-MTK-A64-Android-500a-V002-20191002_135701_73850_100232"
+#endif
