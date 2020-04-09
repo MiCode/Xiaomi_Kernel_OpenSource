337af35feef19c52b907a967d47ef2f0d1478b8c
claude.yen <claude.yen@mediatek.com>
2020-04-09 10:00:42 +0800

    [ALPS05052466] power_gs: add enum GS_PMIC_6315
    1. add enum GS_PMIC_6315
    2. add mt6873 header file
    
    MTK-Commit-Id: ff68191a9ff07e6cfa3d289e05e4fd90d9661377
    
    Change-Id: Icb479835b0395621dd039a244333c5a5430009f1
    Signed-off-by: claude.yen <claude.yen@mediatek.com>
    CR-Id: ALPS05052466
    Feature: Power Management

---
+++ drivers/misc/mediatek/base/power/include/power_gs_v1/mt6873/mtk_power_gs_array.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef MTK_POWER_GS_ARRAY_H
+#define MTK_POWER_GS_ARRAY_H
+
+extern void mt_power_gs_sp_dump(void);
+extern unsigned int golden_read_reg(unsigned int addr);
+
+/* PMIC 6359P */
+extern const unsigned int *AP_PMIC_REG_6359P_gs_deepidle___lp_mp3_32kless;
+extern unsigned int AP_PMIC_REG_6359P_gs_deepidle___lp_mp3_32kless_len;
+
+extern const unsigned int *AP_PMIC_REG_6359P_gs_sodi3p0_32kless;
+extern unsigned int AP_PMIC_REG_6359P_gs_sodi3p0_32kless_len;
+
+extern const unsigned int *AP_PMIC_REG_6359P_gs_suspend_32kless;
+extern unsigned int AP_PMIC_REG_6359P_gs_suspend_32kless_len;
+
+/* PMIC 6315-1 */
+extern const unsigned int *AP_PMIC_REG_MT6315_1_gs_deepidle___lp_mp3_32kless;
+extern unsigned int AP_PMIC_REG_MT6315_1_gs_deepidle___lp_mp3_32kless_len;
+
+extern const unsigned int *AP_PMIC_REG_MT6315_1_gs_sodi3p0_32kless;
+extern unsigned int AP_PMIC_REG_MT6315_1_gs_sodi3p0_32kless_len;
+
+extern const unsigned int *AP_PMIC_REG_MT6315_1_gs_suspend_32kless;
+extern unsigned int AP_PMIC_REG_MT6315_1_gs_suspend_32kless_len;
+
+/* PMIC 6315-2 */
+extern const unsigned int *AP_PMIC_REG_MT6315_2_gs_deepidle___lp_mp3_32kless;
+extern unsigned int AP_PMIC_REG_MT6315_2_gs_deepidle___lp_mp3_32kless_len;
+
+extern const unsigned int *AP_PMIC_REG_MT6315_2_gs_sodi3p0_32kless;
+extern unsigned int AP_PMIC_REG_MT6315_2_gs_sodi3p0_32kless_len;
+
+extern const unsigned int *AP_PMIC_REG_MT6315_2_gs_suspend_32kless;
+extern unsigned int AP_PMIC_REG_MT6315_2_gs_suspend_32kless_len;
+
+/* PMIC 6315-3 */
+extern const unsigned int *AP_PMIC_REG_MT6315_3_gs_deepidle___lp_mp3_32kless;
+extern unsigned int AP_PMIC_REG_MT6315_3_gs_deepidle___lp_mp3_32kless_len;
+
+extern const unsigned int *AP_PMIC_REG_MT6315_3_gs_sodi3p0_32kless;
+extern unsigned int AP_PMIC_REG_MT6315_3_gs_sodi3p0_32kless_len;
+
+extern const unsigned int *AP_PMIC_REG_MT6315_3_gs_suspend_32kless;
+extern unsigned int AP_PMIC_REG_MT6315_3_gs_suspend_32kless_len;
+
+
+extern const unsigned int *AP_CG_Golden_Setting_tcl_gs_dpidle;
+extern unsigned int AP_CG_Golden_Setting_tcl_gs_dpidle_len;
+
+extern const unsigned int *AP_CG_Golden_Setting_tcl_gs_suspend;
+extern unsigned int AP_CG_Golden_Setting_tcl_gs_suspend_len;
+
+extern const unsigned int *AP_CG_Golden_Setting_tcl_gs_sodi;
+extern unsigned int AP_CG_Golden_Setting_tcl_gs_sodi_len;
+
+extern const unsigned int *AP_DCM_Golden_Setting_tcl_gs_dpidle;
+extern unsigned int AP_DCM_Golden_Setting_tcl_gs_dpidle_len;
+
+extern const unsigned int *AP_DCM_Golden_Setting_tcl_gs_suspend;
+extern unsigned int AP_DCM_Golden_Setting_tcl_gs_suspend_len;
+
+extern const unsigned int *AP_DCM_Golden_Setting_tcl_gs_sodi;
+extern unsigned int AP_DCM_Golden_Setting_tcl_gs_sodi_len;
+
+extern bool slp_chk_golden_diff_mode;
+
+#endif
