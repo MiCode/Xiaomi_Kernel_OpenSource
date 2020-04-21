7a1eafa301c7efaa9d8529569502d34ff84aead3
Maulik Shah <mkshah@codeaurora.org>
2020-04-21 19:28:32 +0530

    pinctrl: qcom: Use TLMM and PDC for dual edge wakeup interrupts
    Although PDC has the capability to route falling edge interrupt to
    GIC by inverting the input signal, falling edge of the interrupts marked
    as dual edge type is routed as is. This causes GIC to ignore to falling
    edge of these interrupts when the system is active. During APSS system
    sleep once PDC starts monitoring these interrupts, PDC always replays a
    rising edge irrespective of which edge woke the APSS from sleep.  So,
    the issue does not happen during the system sleep.
    
    To address the issue when system is active, route the falling_edge
    of these interrupts through one of the direct connect interrupts
    available for APSS. The original GIC interrupt is configured as dual
    edge at PDC, rising edge at GIC (same as earlier, this does not change).
    
    This solution requires additional programming of 2 registers:
    
    TLMM_<ZONE>_DIR_CONN_INTRn_CFG_HMSS - program GPIO_SEL with the gpio
    number and set POLAIRTY to 0 for inverting the falling edge
    
    TLMM_GPIO_INTR_CFGn - set DIR_CONN_EN bit to 1 for enabling direct
    connect for gpio.
    
    Change-Id: Iaa9dfe391ef4942cd8b12fd1e7d8f9c238947af2
    Signed-off-by: Archana Sathyakumar <asathyak@codeaurora.org>
    Signed-off-by: Maulik Shah <mkshah@codeaurora.org>

--- drivers/pinctrl/qcom/pinctrl-msm.h
+++ drivers/pinctrl/qcom/pinctrl-msm.h
@@ -38,6 +38,7 @@ struct msm_function {
  * @intr_status_reg:      Offset of the register holding the status bits for this group.
  * @intr_target_reg:      Offset of the register specifying routing of the interrupts
  *                        from this group.
+ * @dir_conn_reg:         Offset of the register hmss setup in tile.
  * @mux_bit:              Offset in @ctl_reg for the pinmux function selection.
  * @pull_bit:             Offset in @ctl_reg for the bias configuration.
  * @drv_bit:              Offset in @ctl_reg for the drive strength configuration.
@@ -56,6 +57,7 @@ struct msm_function {
  * @intr_detection_width: Number of bits used for specifying interrupt type,
  *                        Should be 2 for SoCs that can detect both edges in hardware,
  *                        otherwise 1.
+ * @dir_conn_en_bit:      Offset in @intr_cfg_reg for direct connect enable bit
  * @wake_reg:             Offset of the WAKEUP_INT_EN register from base tile
  * @wake_bit:             Bit number for the corresponding gpio
  */
@@ -72,6 +74,7 @@ struct msm_pingroup {
 	u32 intr_cfg_reg;
 	u32 intr_status_reg;
 	u32 intr_target_reg;
+	u32 dir_conn_reg;
 
 	unsigned int tile:2;
 
@@ -96,9 +99,20 @@ struct msm_pingroup {
 	unsigned intr_polarity_bit:5;
 	unsigned intr_detection_bit:5;
 	unsigned intr_detection_width:5;
+	unsigned dir_conn_en_bit:8;
 
 	u32 wake_reg;
 	unsigned int wake_bit;
+};
+
+/**
+ * struct msm_dir_conn - TLMM Direct GPIO connect configuration
+ * @gpio:	GPIO pin number
+ * @irq:	The GIC interrupt that the pin is connected to
+ */
+struct msm_dir_conn {
+	int gpio;
+	int irq;
 };
 
 /*
@@ -133,6 +147,7 @@ struct msm_gpio_wakeirq_map {
  * @pull_no_keeper: The SoC does not support keeper bias.
  * @wakeirq_map:    The map of wakeup capable GPIOs and the pin at PDC/MPM
  * @nwakeirq_map:   The number of entries in @hierarchy_map
+ * @dir_conn:       An array describing all the pins directly connected to GIC.
  */
 struct msm_pinctrl_soc_data {
 	const struct pinctrl_pin_desc *pins;
@@ -150,6 +165,7 @@ struct msm_pinctrl_soc_data {
 	unsigned int nwakeirq_map;
 	struct pinctrl_qup *qup_regs;
 	unsigned int nqup_regs;
+	struct msm_dir_conn *dir_conn;
 };
 
 extern const struct dev_pm_ops msm_pinctrl_dev_pm_ops;
