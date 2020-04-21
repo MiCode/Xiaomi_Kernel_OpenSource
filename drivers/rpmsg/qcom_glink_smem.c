054808c3d848d9f2d011b2a87b98a945a9b4bedd
Arvind Yadav <arvind.yadav.cs@gmail.com>
2020-04-21 13:40:06 +0100

    rpmsg: glink: use put_device() if device_register fail
    [ Upstream commit a9011726c4bb37e5d6a7279bf47fcc19cd9d3e1a ]
    
    if device_register() returned an error! Always use put_device()
    to give up the reference initialized. And unregister device for
    other return error.
    
    Signed-off-by: Arvind Yadav <arvind.yadav.cs@gmail.com>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/rpmsg/qcom_glink_smem.c
+++ drivers/rpmsg/qcom_glink_smem.c
@@ -212,6 +212,7 @@ struct qcom_glink *qcom_glink_smem_register(struct device *parent,
 	ret = device_register(dev);
 	if (ret) {
 		pr_err("failed to register glink edge\n");
+		put_device(dev);
 		return ERR_PTR(ret);
 	}
 
@@ -294,7 +295,7 @@ struct qcom_glink *qcom_glink_smem_register(struct device *parent,
 	return glink;
 
 err_put_dev:
-	put_device(dev);
+	device_unregister(dev);
 
 	return ERR_PTR(ret);
 }
