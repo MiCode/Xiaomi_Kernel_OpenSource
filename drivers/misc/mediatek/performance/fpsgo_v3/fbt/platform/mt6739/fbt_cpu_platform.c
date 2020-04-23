4fa4b528416d4085ffff9d3bf702bf4048736ebe
Poting Chen <poting.chen@mediatek.com>
2020-04-23 18:16:49 +0800

    [ALPS05092356] FPSGO: add fbt_cpu_platform
    [Detail] add fbt_cpu_platform
    
    MTK-Commit-Id: 0765da1e2ba9e5100bf74719a9203ce1de5c72fd
    
    Change-Id: I2a0c3c9eb18369a808f78c171d48268f25b5c22f
    Signed-off-by: Poting Chen <poting.chen@mediatek.com>
    CR-Id: ALPS05092356
    Feature: FPSGO

--- drivers/misc/mediatek/performance/fpsgo_v3/fbt/platform/mt6739/fbt_cpu_platform.c
+++ drivers/misc/mediatek/performance/fpsgo_v3/fbt/platform/mt6739/fbt_cpu_platform.c
@@ -21,6 +21,7 @@
 #include <mtk_vcorefs_governor.h>
 #include <mtk_vcorefs_manager.h>
 #include <mach/mtk_ppm_api.h>
+#include <linux/cpumask.h>
 
 int ultra_req;
 int cm_req;
@@ -29,6 +30,8 @@ int cm_req;
  */
 static int cpi_thres = 250;
 static unsigned int cpi_uclamp_thres = 82;
+static struct cpumask mask[FPSGO_PREFER_TOTAL];
+static int mask_done;
 
 void fbt_notify_CM_limit(int reach_limit)
 {
@@ -115,6 +118,61 @@ void fbt_set_per_task_min_cap(int pid, unsigned int base_blc)
 	fpsgo_systrace_c_fbt_gm(pid, base_blc, "min_cap");
 }
 
+static int generate_cpu_mask(unsigned int prefer_type, struct cpumask *cpu_mask)
+{
+	if (prefer_type == FPSGO_PREFER_BIG) {
+		cpumask_clear(cpu_mask);
+		cpumask_set_cpu(4, cpu_mask);
+		cpumask_set_cpu(5, cpu_mask);
+		cpumask_set_cpu(6, cpu_mask);
+		cpumask_set_cpu(7, cpu_mask);
+	} else if (prefer_type == FPSGO_PREFER_LITTLE) {
+		cpumask_setall(cpu_mask);
+		cpumask_clear_cpu(4, cpu_mask);
+		cpumask_clear_cpu(5, cpu_mask);
+		cpumask_clear_cpu(6, cpu_mask);
+		cpumask_clear_cpu(7, cpu_mask);
+	} else if (prefer_type == FPSGO_PREFER_NONE)
+		cpumask_setall(cpu_mask);
+	else
+		return -1;
+
+	mask_done = 1;
+
+	return 0;
+}
+
+void fbt_set_affinity(pid_t pid, unsigned int prefer_type)
+{
+	long ret;
+
+	if (!mask_done) {
+		generate_cpu_mask(FPSGO_PREFER_LITTLE,
+					&mask[FPSGO_PREFER_LITTLE]);
+		generate_cpu_mask(FPSGO_PREFER_BIG, &mask[FPSGO_PREFER_BIG]);
+		generate_cpu_mask(FPSGO_PREFER_NONE, &mask[FPSGO_PREFER_NONE]);
+	}
+
+	ret = sched_setaffinity(pid, &mask[prefer_type]);
+	if (ret != 0) {
+		fpsgo_systrace_c_fbt(pid, ret, "setaffinity fail");
+		fpsgo_systrace_c_fbt(pid, 0, "setaffinity fail");
+		return;
+	}
+	fpsgo_systrace_c_fbt(pid, prefer_type, "set_affinity");
+}
+
+void fbt_set_cpu_prefer(int pid, unsigned int prefer_type)
+{
+	long ret;
+
+	if (!pid)
+		return;
+
+	ret = sched_set_cpuprefer(pid, prefer_type);
+	fpsgo_systrace_c_fbt(pid, prefer_type, "set_cpuprefer");
+}
+
 int fbt_get_L_cluster_num(void)
 {
 	return 0;
@@ -122,12 +180,16 @@ int fbt_get_L_cluster_num(void)
 
 int fbt_get_L_min_ceiling(void)
 {
-	int freq = 0;
-
-	return freq;
+	return 0;
 }
 
 int fbt_get_default_boost_ta(void)
 {
 	return 1;
 }
+
+int fbt_get_default_adj_loading(void)
+{
+	return 1;
+}
+
