5fb2fee96a0564679dd70c0ecc44794a25e1e8c4
Dmitry Bogdanov <logost.boda@gmail.com>
2020-04-24 13:17:49 +0300

    net: macsec: count RX bytes/packets when offloaded
    Count Rx bytes/packets for frames received through MACSEC offload.
    
    Change-Id: I3f3f916de2c7f8804ac01c46bad1a4fcd0ef2ac2
    Signed-off-by: Dmitry Bogdanov <dbogdanov@marvell.com>
    Git-commit: efffd1e26af1d5eeeeca14a615e62ab45b85d6e2
    Git-repo: https://github.com/aquantia/linux-4.14-atlantic-forwarding
    Signed-off-by: Jinesh K. Jayakumar <jineshk@codeaurora.org>

--- drivers/net/macsec.c
+++ drivers/net/macsec.c
@@ -942,6 +942,7 @@ static enum rx_handler_result handle_not_macsec(struct sk_buff *skb)
 				if (!nskb)
 					break;
 
+				count_rx(ndev, nskb->len);
 				nskb->dev = ndev;
 				netif_rx(nskb);
 			} else if (ether_addr_equal_64bits(hdr->h_dest,
@@ -949,6 +950,7 @@ static enum rx_handler_result handle_not_macsec(struct sk_buff *skb)
 				/* HW offload enabled, divert skb */
 				skb->dev = ndev;
 				skb->pkt_type = PACKET_HOST;
+				count_rx(ndev, skb->len);
 				ret = RX_HANDLER_ANOTHER;
 				goto out;
 			} else if (is_multicast_ether_addr_64bits(hdr->h_dest)) {
@@ -963,6 +965,7 @@ static enum rx_handler_result handle_not_macsec(struct sk_buff *skb)
 				else
 					nskb->pkt_type = PACKET_MULTICAST;
 
+				count_rx(ndev, nskb->len);
 				netif_rx(nskb);
 			}
 			continue;
