4812ec50935dfe59ba9f48a572e278dd0b02af68
Maciej Żenczykowski <maze@google.com>
2020-04-26 09:15:25 -0700

    BACKPORT: bpf: add bpf_ktime_get_boot_ns()
    On a device like a cellphone which is constantly suspending
    and resuming CLOCK_MONOTONIC is not particularly useful for
    keeping track of or reacting to external network events.
    Instead you want to use CLOCK_BOOTTIME.
    
    Hence add bpf_ktime_get_boot_ns() as a mirror of bpf_ktime_get_ns()
    based around CLOCK_BOOTTIME instead of CLOCK_MONOTONIC.
    
    Signed-off-by: Maciej Żenczykowski <maze@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    (cherry picked from commit 71d19214776e61b33da48f7c1b46e522c7f78221)
    Change-Id: Ifd62c410dcc5112fd1a473a7e1f70231ca514bc0

--- net/core/filter.c
+++ net/core/filter.c
@@ -4844,6 +4844,8 @@ bpf_base_func_proto(enum bpf_func_id func_id)
 		return &bpf_tail_call_proto;
 	case BPF_FUNC_ktime_get_ns:
 		return &bpf_ktime_get_ns_proto;
+	case BPF_FUNC_ktime_get_boot_ns:
+		return &bpf_ktime_get_boot_ns_proto;
 	case BPF_FUNC_trace_printk:
 		if (capable(CAP_SYS_ADMIN))
 			return bpf_get_trace_printk_proto();
