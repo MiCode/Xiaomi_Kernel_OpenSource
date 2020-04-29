e7d2851f60778548e07232d1664cf0d81298e7e0
Morven-CF Yeh <morven-cf.yeh@mediatek.com>
2020-04-29 15:05:55 +0800

    cm_mgr: mediatek: make mt6873 pinctrl ready for loadable module
    Make mt6873 cm_mgr ready for building as loadable module
    
    (cherry-picked from commit 95b6fd88ee7e121636e3a2565fd52c0a602b053d)
    Signed-off-by: Morven-CF Yeh <morven-cf.yeh@mediatek.com>
    Change-Id: I93480e3508ba1bf99bfc39ca18287fd7e546a6f7
    CR-Id: ALPS05298037
    Feature: Power Management
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

---
+++ drivers/misc/mediatek/cm_mgr/mtk_cm_mgr_mt6873.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#ifndef __MTK_CM_MGR_PLATFORM_H__
+#define __MTK_CM_MGR_PLATFORM_H__
+
+#define PERF_TIME 3000
+
+#include "mtk_cm_mgr_reg_mt6873.h"
+
+#define CREATE_TRACE_POINTS
+#include "mtk_cm_mgr_events_mt6873.h"
+
+#define USE_CM_MGR_AT_SSPM
+
+enum {
+	CM_MGR_LP4X_2CH = 0,
+	CM_MGR_MAX,
+};
+
+#endif	/* __MTK_CM_MGR_PLATFORM_H__ */
