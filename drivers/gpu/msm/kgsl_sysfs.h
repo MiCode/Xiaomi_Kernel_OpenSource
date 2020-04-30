88ff0b5ea066a2354a93039c049167f4e20b70e3
Jordan Crouse <jcrouse@codeaurora.org>
2020-04-30 11:41:06 -0600

    msm: kgsl: Add kobject sysfs ops for the /sys/kernel/gpu directory
    Commit 2726e792a201 ("msm: kgsl: Fix linked sysfs files") thought it was
    clever by reusing the callbacks for the sysfs nodes that were linked to
    /sys/kernel/gpu. Unfortunately, since the /sys/kernel/gpu kobject didn't
    have a device attached the callback function ended up dereferencing
    nothing.
    
    Instead, create a full kobject infrastructure with sysfs_ops and a built
    in kobj to access the KGSL device. We can still use the callbacks albeit
    with a wrapper to make them work from both locations. While we are in
    the midst of this, make a header for the new attributes and macros so
    that we can later move some of the nodes out of kgsl_pwrctrl.c with
    less disruption.
    
    Change-Id: Ic0dedbad5b9d7323d4f43328540dce91cc58015e
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

---
+++ drivers/gpu/msm/kgsl_sysfs.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ */
+#ifndef _KGSL_SYSFS_H_
+#define _KGSL_SYSFS_H_
+
+struct kgsl_device;
+
+/**
+ * struct kgsl_gpu_sysfs_attr - Attribute definition for sysfs objects in the
+ * /sys/kernel/gpu kobject
+ */
+struct kgsl_gpu_sysfs_attr {
+	/** @attr: Attribute for the sysfs node */
+	struct attribute attr;
+	/** @show: Show function for the node */
+	ssize_t (*show)(struct kgsl_device *device, char *buf);
+	/** @store: Store function for the node */
+	ssize_t (*store)(struct kgsl_device *device, const char *buf,
+			size_t count);
+};
+
+#define GPU_SYSFS_ATTR(_name, _mode, _show, _store)		\
+struct kgsl_gpu_sysfs_attr gpu_sysfs_attr_##_name = {		\
+	.attr = { .name = __stringify(_name), .mode = _mode },	\
+	.show = _show,						\
+	.store = _store,					\
+}
+
+#endif
