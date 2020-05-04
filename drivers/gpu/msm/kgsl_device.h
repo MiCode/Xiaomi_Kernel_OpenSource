c117af8e8f2b237c615880475f8481e88324a9d3
Jordan Crouse <jcrouse@codeaurora.org>
2020-05-04 13:19:07 -0600

    msm: kgsl: Remove support for sparse memory
    We are choosing to go in a different direction for virtual buffer objects
    so remove the old sparse code to get it out of the way. In a few cases
    we'll add back similar code so there is a bit of churn but its easierto
    just delete everything and start over from scratch.
    
    This doesn't remove the uapi definitions for sparse since we're stuck
    with those forever but they'll just harmlessly return error.
    
    Change-Id: Ic0dedbad9a79ef321e9510115d64b5019743eb2b
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/kgsl_device.h
+++ drivers/gpu/msm/kgsl_device.h
@@ -51,7 +51,6 @@ enum kgsl_event_results {
 };
 
 #define KGSL_FLAG_WAKE_ON_TOUCH BIT(0)
-#define KGSL_FLAG_SPARSE        BIT(1)
 
 /*
  * "list" of event types for ftrace symbolic magic
@@ -70,8 +69,7 @@ enum kgsl_event_results {
 	{ KGSL_CONTEXT_SAVE_GMEM, "SAVE_GMEM" }, \
 	{ KGSL_CONTEXT_IFH_NOP, "IFH_NOP" }, \
 	{ KGSL_CONTEXT_SECURE, "SECURE" }, \
-	{ KGSL_CONTEXT_NO_SNAPSHOT, "NO_SNAPSHOT" }, \
-	{ KGSL_CONTEXT_SPARSE, "SPARSE" }
+	{ KGSL_CONTEXT_NO_SNAPSHOT, "NO_SNAPSHOT" }
 
 #define KGSL_CONTEXT_ID(_context) \
 	((_context != NULL) ? (_context)->id : KGSL_MEMSTORE_GLOBAL)
@@ -206,18 +204,6 @@ struct kgsl_memobj_node {
 	uint64_t size;
 	unsigned long flags;
 	unsigned long priv;
-};
-
-/**
- * struct kgsl_sparseobj_node - Sparse object descriptor
- * @node: Local list node for the sparse cmdbatch
- * @virt_id: Virtual ID to bind/unbind
- * @obj:  struct kgsl_sparse_binding_object
- */
-struct kgsl_sparseobj_node {
-	struct list_head node;
-	unsigned int virt_id;
-	struct kgsl_sparse_binding_object obj;
 };
 
 struct kgsl_device {
@@ -723,9 +709,6 @@ long kgsl_ioctl_copy_in(unsigned int kernel_cmd, unsigned int user_cmd,
 
 long kgsl_ioctl_copy_out(unsigned int kernel_cmd, unsigned int user_cmd,
 		unsigned long arg, unsigned char *ptr);
-
-void kgsl_sparse_bind(struct kgsl_process_private *private,
-		struct kgsl_drawobj_sparse *sparse);
 
 /**
  * kgsl_context_type - Return a symbolic string for the context type
