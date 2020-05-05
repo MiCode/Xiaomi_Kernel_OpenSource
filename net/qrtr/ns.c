b12a6cf8f85154ad5bea2f0c6823dbdf843577f4
Chris Lew <clew@codeaurora.org>
2020-05-05 15:45:35 -0700

    net: qrtr: Add pr_fmt to ns
    Add pr_fmt to the ns file for easier error log matching.
    
    Updated license year.
    
    Change-Id: I711735ee60c1fd9fde25850b8e625bb44c04f4c4
    Signed-off-by: Chris Lew <clew@codeaurora.org>
    Signed-off-by: Jay Jayanna <jayanna@codeaurora.org>

--- net/qrtr/ns.c
+++ net/qrtr/ns.c
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
 /*
  * Copyright (c) 2015, Sony Mobile Communications Inc.
- * Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013, 2020, The Linux Foundation. All rights reserved.
  * Copyright (c) 2020, Linaro Ltd.
  */
+
+#define pr_fmt(fmt) "qrtr: %s(): " fmt, __func__
 
 #include <linux/module.h>
 #include <linux/qrtr.h>
