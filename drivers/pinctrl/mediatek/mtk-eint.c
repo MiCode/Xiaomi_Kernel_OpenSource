59c4d60732d88d81b697305b55cfc8cb3e1b545a
mtk12444 <sagy.shih@mediatek.com>
2020-05-06 12:09:16 +0800

    [ALPS05082701] EINT: fix out-of-bounds coverity
    Fix out-of-bounds coverity
    
    MTK-Commit-Id: 2138ac9244c81ee4fd7fe630827dc46fc1b7e926
    
    Change-Id: Ia990dbff4fadc3466a3d7130b2b8a7d464209132
    Signed-off-by: mtk12444 <sagy.shih@mediatek.com>
    CR-Id: ALPS05082701
    Feature: System Performance

--- drivers/pinctrl/mediatek/mtk-eint.c
+++ drivers/pinctrl/mediatek/mtk-eint.c
@@ -465,7 +465,7 @@ int mtk_eint_set_debounce(struct mtk_eint *eint, unsigned long eint_num,
 	if (!mtk_eint_can_en_debounce(eint, eint_num))
 		return -EINVAL;
 
-	dbnc = ARRAY_SIZE(debounce_time);
+	dbnc = ARRAY_SIZE(debounce_time) - 1;
 	for (i = 0; i < ARRAY_SIZE(debounce_time); i++) {
 		if (debounce <= debounce_time[i]) {
 			dbnc = i;
