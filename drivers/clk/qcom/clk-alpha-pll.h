938fe4646923740db7455eb1a6def044c07d2544
Naveen Yadav <naveenky@codeaurora.org>
2020-05-07 16:00:59 +0530

    clk: qcom: clk-alpha-pll: Add support for fixed Lucid EVO PLL
    Lucid EVO PLL is required to be supported from HLOS clock controller,
    thus add support for fixed lucid evo pll ops.
    
    Change-Id: I09f89af4e9f3e85649a957872e6bc49e95a94679
    Signed-off-by: Naveen Yadav <naveenky@codeaurora.org>

--- drivers/clk/qcom/clk-alpha-pll.h
+++ drivers/clk/qcom/clk-alpha-pll.h
@@ -23,6 +23,7 @@ enum {
 	CLK_ALPHA_PLL_TYPE_ZONDA_5LPE,
 	CLK_ALPHA_PLL_TYPE_REGERA,
 	CLK_ALPHA_PLL_TYPE_AGERA,
+	CLK_ALPHA_PLL_TYPE_LUCID_EVO,
 	CLK_ALPHA_PLL_TYPE_MAX,
 };
 
@@ -158,6 +159,9 @@ extern const struct clk_ops clk_trion_pll_ops;
 
 extern const struct clk_ops clk_regera_pll_ops;
 extern const struct clk_ops clk_agera_pll_ops;
+
+extern const struct clk_ops clk_alpha_pll_fixed_lucid_evo_ops;
+extern const struct clk_ops clk_alpha_pll_postdiv_lucid_evo_ops;
 
 void clk_alpha_pll_configure(struct clk_alpha_pll *pll, struct regmap *regmap,
 			     const struct alpha_pll_config *config);
