dbc144f46d5b0e62e536a37981d6a6dc5c40a89a
Jason Kao <hc.kao@mediatek.com>
2020-05-09 10:07:46 +0800

    [ALPS04754032] mt6873: enable scp Logger
    1. Enable scp Logger.
    
    MTK-Commit-Id: b3117e489f89e4cc4766916d2fb16ee9f26504d4
    
    Change-Id: I7cf16f58fc2650f5aad15fde0b15b463cf0eb8ed
    CR-Id: ALPS04754032
    Feature: TinySys
    Signed-off-by: Jason Kao <hc.kao@mediatek.com>
    (cherry picked from commit 1698c1bc5e96f6bdb765a4465ba9ff92839e0c89)

--- drivers/misc/mediatek/scp/mt6873/scp_feature_define.h
+++ drivers/misc/mediatek/scp/mt6873/scp_feature_define.h
@@ -18,7 +18,7 @@
 /* scp platform configs*/
 #define SCP_BOOT_TIME_OUT_MONITOR        (1)
 #define SCP_RESERVED_MEM                 (1)
-#define SCP_LOGGER_ENABLE                (0)
+#define SCP_LOGGER_ENABLE                (1)
 #define SCP_DVFS_INIT_ENABLE             (0)
 #define SCP_VOW_LOW_POWER_MODE           (0)
 
