8778df0034ee5658077df1305284eb7a7c15185c
Juju Sung <juju.sung@mediatek.com>
2020-05-09 10:10:50 +0800

    [ALPS05111942] camera: mt6873/6853 cam_cal porting
    [Detail]
    Clone mt6853/mt6873 cam_cal from q0 to r0
    
    MTK-Commit-Id: 4906caf537287425120f8a7aefbf7bd2b01ed29f
    
    Change-Id: I1c389cec761ca4fc0417b70b998f936cd0316d98
    Signed-off-by: Juju Sung <juju.sung@mediatek.com>
    CR-Id: ALPS05111942
    Feature: [Module]Kernel Maintenance

---
+++ drivers/misc/mediatek/cam_cal/src/mt6853/cam_cal_list.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2018 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+#include <linux/kernel.h>
+#include "cam_cal_list.h"
+#include "eeprom_i2c_common_driver.h"
+#include "eeprom_i2c_custom_driver.h"
+#include "kd_imgsensor.h"
+
+#define MAX_EEPROM_SIZE_16K 0x4000
+
+struct stCAM_CAL_LIST_STRUCT g_camCalList[] = {
+	/*Below is commom sensor */
+	{IMX586_SENSOR_ID, 0xA0, Common_read_region, MAX_EEPROM_SIZE_16K},
+	{IMX576_SENSOR_ID, 0xA2, Common_read_region},
+	{IMX519_SENSOR_ID, 0xA0, Common_read_region},
+	{IMX319_SENSOR_ID, 0xA2, Common_read_region, MAX_EEPROM_SIZE_16K},
+	{S5K3M5SX_SENSOR_ID, 0xA0, Common_read_region, MAX_EEPROM_SIZE_16K},
+	{IMX686_SENSOR_ID, 0xA0, Common_read_region, MAX_EEPROM_SIZE_16K},
+	{HI846_SENSOR_ID, 0xA0, Common_read_region, MAX_EEPROM_SIZE_16K},
+	{S5KGD1SP_SENSOR_ID, 0xA8, Common_read_region, MAX_EEPROM_SIZE_16K},
+	{S5K2T7SP_SENSOR_ID, 0xA4, Common_read_region},
+	{IMX386_SENSOR_ID, 0xA0, Common_read_region},
+	{S5K2L7_SENSOR_ID, 0xA0, Common_read_region},
+	{IMX398_SENSOR_ID, 0xA0, Common_read_region},
+	{IMX350_SENSOR_ID, 0xA0, Common_read_region},
+	{IMX386_MONO_SENSOR_ID, 0xA0, Common_read_region},
+	{IMX499_SENSOR_ID, 0xA0, Common_read_region},
+	/*  ADD before this line */
+	{0, 0, 0}       /*end of list */
+};
+
+unsigned int cam_cal_get_sensor_list(
+	struct stCAM_CAL_LIST_STRUCT **ppCamcalList)
+{
+	if (ppCamcalList == NULL)
+		return 1;
+
+	*ppCamcalList = &g_camCalList[0];
+	return 0;
+}
+
+
