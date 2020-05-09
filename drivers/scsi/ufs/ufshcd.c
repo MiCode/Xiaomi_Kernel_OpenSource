30d935335b9611526bd5399673b423cd89f37bda
Stanley Chu <stanley.chu@mediatek.com>
2020-05-09 10:06:48 +0800

    [ALPS05105030] scsi: ufs: improve resume latency
    Improve UFS resume latency by below upstream patches,
    
    scsi: ufs-mediatek: customize the delay for enabling host
    scsi: ufs: make HCE polling more compact to improve initialization
    latency
    scsi: ufs: allow custom delay prior to host enabling
    scsi: ufs-mediatek: use common delay function
    scsi: ufs: introduce common and flexible delay function
    
    After applying those patches, resume latency can be reduced
    about 7~8 ms in MT6873 platforms.
    
    MTK-Commit-Id: 3696b9cb9fb5574d27bf34fddb38756a4ab90692
    
    Change-Id: Ie0e348ea15cb5c2a99c420014f215eae1ad6b8b5
    Signed-off-by: Stanley Chu <stanley.chu@mediatek.com>
    CR-Id: ALPS05105030
    Feature: UFS Booting
    (cherry picked from commit 23335d0598aa1f378460f4911a63190b07b12405)

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -822,6 +822,18 @@ static void ufshcd_print_pwr_info(struct ufs_hba *hba)
 		 hba->pwr_info.hs_rate);
 }
 
+void ufshcd_delay_us(unsigned long us, unsigned long tolerance)
+{
+	if (!us)
+		return;
+
+	if (us < 10)
+		udelay(us);
+	else
+		usleep_range(us, us + tolerance);
+}
+EXPORT_SYMBOL_GPL(ufshcd_delay_us);
+
 /*
  * ufshcd_wait_for_register - wait for register value to change
  * @hba - per-adapter interface
@@ -4807,10 +4819,10 @@ int ufshcd_hba_enable(struct ufs_hba *hba)
 	 * instruction might be read back.
 	 * This delay can be changed based on the controller.
 	 */
-	msleep(1);
+	ufshcd_delay_us(hba->hba_enable_delay_us, 100);
 
 	/* wait for the host controller to complete initialization */
-	retry = 10;
+	retry = 50;
 	while (ufshcd_is_hba_active(hba)) {
 		if (retry) {
 			retry--;
@@ -4824,7 +4836,7 @@ int ufshcd_hba_enable(struct ufs_hba *hba)
 				SW_RST_TARGET_UFSCPT | SW_RST_TARGET_UNIPRO);
 			return -EIO;
 		}
-		msleep(5);
+		usleep_range(1000, 1100);
 	}
 
 	/* enable UIC related interrupts */
@@ -9173,6 +9185,7 @@ out:
 
 	if (ret)
 		ufshcd_update_reg_hist(&hba->ufs_stats.resume_err, (u32)ret);
+
 	return ret;
 }
 
@@ -9645,6 +9658,7 @@ int ufshcd_init(struct ufs_hba *hba, void __iomem *mmio_base, unsigned int irq)
 
 	hba->mmio_base = mmio_base;
 	hba->irq = irq;
+	hba->hba_enable_delay_us = 1000;
 
 	/* Set descriptor lengths to specification defaults */
 	ufshcd_def_desc_sizes(hba);
