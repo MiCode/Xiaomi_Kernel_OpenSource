27ab3cfa2a4f7215eacbeef3e1aa75d8ac41198e
mtk03921 <redman.huang@mediatek.com>
2020-05-09 10:06:19 +0800

    [ALPS05083924] APUSYS : update reg dump mechanism
    [Detail] add Mouton reg dump code, update mem dump table, dump MNoC
    debug reg
    [Solution] Modify dump_reg.c
    
    MTK-Commit-Id: b388d4526c06e0a9b9011ec87d6cbc4d715a6cfd
    
    Signed-off-by: redman.huang <redman.huang@mediatek.com>
    Feature:[Module] APUSYS
    Change-Id: Iaaa8fc9a0a392eeed5e3833c8d75cf595649ad0b
    CR-Id:ALPS05083924
    (cherry picked from commit def02da7418fcc3029c44e59ba18877ac9886ec2)

--- drivers/misc/mediatek/apusys/midware/mt6853/apusys_plat.h
+++ drivers/misc/mediatek/apusys/midware/mt6853/apusys_plat.h
@@ -14,7 +14,7 @@
 #ifndef __APUSYS_MIDWARE_PLATFORM_H__
 #define __APUSYS_MIDWARE_PLATFORM_H__
 
-
+extern struct dentry *apusys_dbg_root;
 #define APUSYS_VLM_START 0x1D800000 // tcm tmp
 #define APUSYS_VLM_SIZE 0x100000
 
