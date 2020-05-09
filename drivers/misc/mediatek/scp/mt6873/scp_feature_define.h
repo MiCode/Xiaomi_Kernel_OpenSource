b17e389f7986dac6a2cdb28db78d29f1ef62278f
Jason Kao <hc.kao@mediatek.com>
2020-05-09 10:07:51 +0800

    [ALPS04754032] mt6873: SCP enable EMI MPU
    1. Enable SCP EMI MPU.
    
    MTK-Commit-Id: 18058bd073554de9760137f19597de20494fb4f0
    
    Change-Id: I49c7c0a6733b6178d8cbba99092054839a13f2f3
    CR-Id: ALPS04754032
    Feature: TinySys
    Signed-off-by: Jason Kao <hc.kao@mediatek.com>
    (cherry picked from commit 372846fbe98ec5b45012e830328e0d848b9b77a4)

--- drivers/misc/mediatek/scp/mt6873/scp_feature_define.h
+++ drivers/misc/mediatek/scp/mt6873/scp_feature_define.h
@@ -35,7 +35,7 @@
 #define SCP_REGISTER_SUB_SENSOR          (1)
 
 /* emi mpu define*/
-#define ENABLE_SCP_EMI_PROTECTION        (0)
+#define ENABLE_SCP_EMI_PROTECTION        (1)
 
 #define MPU_REGION_ID_SCP_SMEM           7
 #define MPU_DOMAIN_D0                    0
