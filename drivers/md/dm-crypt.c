999e55b1c1586858cc16a8157ec0a31df36785d1
Neeraj Soni <neersoni@codeaurora.org>
2020-05-17 22:37:29 +0530

    dm-crypt: Skip encryption if bio is fscrypto or blk-crypto encrypted
    Skip encryption of bio if it is already fs-crypto encrypted or is
    supposed to be encrypted by blk-crypto.
    
    Change-Id: I89053a2e0a63941265f5973f2f78786e69f0db37
    Signed-off-by: Neeraj Soni <neersoni@codeaurora.org>

--- drivers/md/dm-crypt.c
+++ drivers/md/dm-crypt.c
@@ -2880,7 +2880,8 @@ static int crypt_map(struct dm_target *ti, struct bio *bio)
 	 * - for REQ_OP_DISCARD caller must use flush if IO ordering matters
 	 */
 	if (unlikely(bio->bi_opf & REQ_PREFLUSH ||
-	    bio_op(bio) == REQ_OP_DISCARD)) {
+	    bio_op(bio) == REQ_OP_DISCARD ||
+	    bio_should_skip_dm_default_key(bio))) {
 		bio_set_dev(bio, cc->dev->bdev);
 		if (bio_sectors(bio))
 			bio->bi_iter.bi_sector = cc->start +
