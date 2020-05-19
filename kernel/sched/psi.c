028fb9dc8aae1e206478c8b6d3aed62a1f24e9f8
Suren Baghdasaryan <surenb@google.com>
2020-05-19 15:20:25 +0530

    sched/psi: Fix OOB write when writing 0 bytes to PSI files
    Issuing write() with count parameter set to 0 on any file under
    /proc/pressure/ will cause an OOB write because of the access to
    buf[buf_size-1] when NUL-termination is performed. Fix this by checking
    for buf_size to be non-zero.
    
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Link: https://lkml.kernel.org/r/20200203212216.7076-1-surenb@google.com
    Git-repo: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Git-commit: 6fcca0fa48118e6d63733eb4644c6cd880c15b8f
    Change-Id: I733341988a26bc02be9b7013c297b0ac461eda7e
    Signed-off-by: Alam Md Danish <amddan@codeaurora.org>

--- kernel/sched/psi.c
+++ kernel/sched/psi.c
@@ -1310,6 +1310,9 @@ static ssize_t psi_write(struct file *file, const char __user *user_buf,
 	if (static_branch_likely(&psi_disabled))
 		return -EOPNOTSUPP;
 
+	if (!nbytes)
+		return -EINVAL;
+
 	buf_size = min(nbytes, (sizeof(buf) - 1));
 	if (copy_from_user(buf, user_buf, buf_size))
 		return -EFAULT;
