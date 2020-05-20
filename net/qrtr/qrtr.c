783b04907968cb54688c1b4794f7b7a57dec923b
Chris Lew <clew@codeaurora.org>
2020-05-19 18:54:43 -0700

    net: qrtr: Remove rcu logic for port lookup/remove
    synchronize_rcu() is slow and can create delays on creating/removing
    sockets.
    
    Some clients have latency requirements for spinning off a
    thread that creates a client and lookup sockets for some sessions. They
    close the lookup socket during this sequence which causes delays. Each
    call to synchronize_rcu takes 20-40 ms which is too long.
    
    Remove the rcu logic and change qrtr_port_lock to a spinlock so it can
    be used in qrtr_port_lookup which may be used in atomic context.
    
    Change-Id: I3244e31d3ca0599df794d3936033735bcdd3e972
    Signed-off-by: Chris Lew <clew@codeaurora.org>
    Signed-off-by: Jay Jayanna <jayanna@codeaurora.org>

--- net/qrtr/qrtr.c
+++ net/qrtr/qrtr.c
@@ -122,6 +122,7 @@ static DECLARE_RWSEM(qrtr_epts_lock);
 /* local port allocation management */
 static DEFINE_XARRAY_ALLOC(qrtr_ports);
 u32 qrtr_ports_next = QRTR_MIN_EPH_SOCKET;
+static DEFINE_SPINLOCK(qrtr_port_lock);
 
 /**
  * struct qrtr_node - endpoint node
@@ -763,15 +764,16 @@ EXPORT_SYMBOL_GPL(qrtr_endpoint_unregister);
 static struct qrtr_sock *qrtr_port_lookup(int port)
 {
 	struct qrtr_sock *ipc;
+	unsigned long flags;
 
 	if (port == QRTR_PORT_CTRL)
 		port = 0;
 
-	rcu_read_lock();
+	spin_lock_irqsave(&qrtr_port_lock, flags);
 	ipc = xa_load(&qrtr_ports, port);
 	if (ipc)
 		sock_hold(&ipc->sk);
-	rcu_read_unlock();
+	spin_unlock_irqrestore(&qrtr_port_lock, flags);
 
 	return ipc;
 }
@@ -833,6 +835,7 @@ exit:
 static void qrtr_port_remove(struct qrtr_sock *ipc)
 {
 	int port = ipc->us.sq_port;
+	unsigned long flags;
 
 	qrtr_send_del_client(ipc);
 
@@ -841,11 +844,9 @@ static void qrtr_port_remove(struct qrtr_sock *ipc)
 
 	__sock_put(&ipc->sk);
 
+	spin_lock_irqsave(&qrtr_port_lock, flags);
 	xa_erase(&qrtr_ports, port);
-
-	/* Ensure that if qrtr_port_lookup() did enter the RCU read section we
-	 * wait for it to up increment the refcount */
-	synchronize_rcu();
+	spin_unlock_irqrestore(&qrtr_port_lock, flags);
 }
 
 /* Assign port number to socket.
@@ -912,6 +913,7 @@ static int __qrtr_bind(struct socket *sock,
 {
 	struct qrtr_sock *ipc = qrtr_sk(sock->sk);
 	struct sock *sk = sock->sk;
+	unsigned long flags;
 	int port;
 	int rc;
 
@@ -919,10 +921,17 @@ static int __qrtr_bind(struct socket *sock,
 	if (!zapped && addr->sq_port == ipc->us.sq_port)
 		return 0;
 
+	spin_lock_irqsave(&qrtr_port_lock, flags);
 	port = addr->sq_port;
 	rc = qrtr_port_assign(ipc, &port);
-	if (rc)
+	if (rc) {
+		spin_unlock_irqrestore(&qrtr_port_lock, flags);
 		return rc;
+	}
+
+	if (port == QRTR_PORT_CTRL)
+		qrtr_reset_ports();
+	spin_unlock_irqrestore(&qrtr_port_lock, flags);
 
 	/* unbind previous, if any */
 	if (!zapped)
@@ -930,10 +939,6 @@ static int __qrtr_bind(struct socket *sock,
 	ipc->us.sq_port = port;
 
 	sock_reset_flag(sk, SOCK_ZAPPED);
-
-	/* Notify all open ports about the new controller */
-	if (port == QRTR_PORT_CTRL)
-		qrtr_reset_ports();
 
 	return 0;
 }
