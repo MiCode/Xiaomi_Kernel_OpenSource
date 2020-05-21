8422271e290f92963b019412b908344315a80e38
Hareesh Gundu <hareeshg@codeaurora.org>
2020-05-21 20:57:18 +0530

    msm: kgsl: Fix incomplete sync timeline name description
    Use kasprintf() instead of fixed buffer to fix incomplete
    sync timeline name description.
    
    Change-Id: I6af6608cca6119ca6ff2f343f9b2f522a08db3a0
    Signed-off-by: Hareesh Gundu <hareeshg@codeaurora.org>

--- drivers/gpu/msm/kgsl_sync.c
+++ drivers/gpu/msm/kgsl_sync.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2020, The Linux Foundation. All rights reserved.
  */
 
 #include <linux/file.h>
@@ -309,8 +309,7 @@ int kgsl_sync_timeline_create(struct kgsl_context *context)
 	}
 
 	kref_init(&ktimeline->kref);
-	snprintf(ktimeline->name, sizeof(ktimeline->name),
-		"%s_%d-%.15s(%d)-%.15s(%d)",
+	ktimeline->name = kasprintf(GFP_KERNEL, "%s_%d-%.15s(%d)-%.15s(%d)",
 		context->device->name, context->id,
 		current->group_leader->comm, current->group_leader->pid,
 		current->comm, current->pid);
@@ -354,7 +353,10 @@ static void kgsl_sync_timeline_signal(struct kgsl_sync_timeline *ktimeline,
 
 void kgsl_sync_timeline_destroy(struct kgsl_context *context)
 {
-	kfree(context->ktimeline);
+	struct kgsl_sync_timeline *ktimeline = context->ktimeline;
+
+	kfree(ktimeline->name);
+	kfree(ktimeline);
 }
 
 static void kgsl_sync_timeline_release(struct kref *kref)
