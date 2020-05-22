d8d879b52ff2475f876cce9839ce9f18453efe80
Puranam V G Tejaswi <pvgtejas@codeaurora.org>
2020-05-22 17:36:30 +0530

    msm: kgsl: set vm_pgoff of vma to zero
    kgsl gets the entry id or the gpu address through vm_pgoff. It is used
    during mmap and never needed again. But this pgoff has different meaning
    at other parts of kernel. Not setting to zero will let way for wrong
    assumption when tried to unmap a page from the vma.
    
    Change-Id: Ia81c64a77456caf168c6bd23bdf5755c3f3ee31c
    Signed-off-by: Puranam V G Tejaswi <pvgtejas@codeaurora.org>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4879,6 +4879,15 @@ static int kgsl_mmap(struct file *file, struct vm_area_struct *vma)
 
 	entry->memdesc.useraddr = vma->vm_start;
 
+	/*
+	 * kgsl gets the entry id or the gpu address through vm_pgoff.
+	 * It is used during mmap and never needed again. But this vm_pgoff
+	 * has different meaning at other parts of kernel. Not setting to
+	 * zero will let way for wrong assumption when tried to unmap a page
+	 * from this vma.
+	 */
+	vma->vm_pgoff = 0;
+
 	trace_kgsl_mem_mmap(entry);
 	return 0;
 }
