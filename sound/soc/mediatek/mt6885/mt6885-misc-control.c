ab0d30966d8a1f1ddbbc905a991fd540e5ebd5e4
Eason Yen <eason.yen@mediatek.com>
2020-05-22 20:18:45 +0800

    [ALPS04960338] ASoC: mediatek: shrink speech kcontrol log
    Remove speech property kcontorl (set/get) log
    
    MTK-Commit-Id: cd4693a89a236a3227a1a657acc685cfbd62eb92
    
    Change-Id: Ia5f8da60a8d1f70a47221cc0ec19863294f21492
    Signed-off-by: Eason Yen <eason.yen@mediatek.com>
    CR-Id: ALPS04960338
    Feature: [Module]Audio Kernel

--- sound/soc/mediatek/mt6885/mt6885-misc-control.c
+++ sound/soc/mediatek/mt6885/mt6885-misc-control.c
@@ -2045,10 +2045,6 @@ static int speech_property_get(struct snd_kcontrol *kcontrol,
 	}
 	ucontrol->value.integer.value[0] = *sph_property;
 
-	if (strcmp(kcontrol->id.name, "Speech_A2M_Msg_ID") != 0)
-		dev_info(afe->dev, "%s(), %s = 0x%x\n", __func__,
-			 kcontrol->id.name, *sph_property);
-
 	return 0;
 }
 
@@ -2068,8 +2064,6 @@ static int speech_property_set(struct snd_kcontrol *kcontrol,
 	}
 	*sph_property = ucontrol->value.integer.value[0];
 
-	dev_info(afe->dev, "%s(), %s = 0x%x\n", __func__,
-		 kcontrol->id.name, *sph_property);
 	return 0;
 }
 
