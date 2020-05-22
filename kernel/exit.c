805706982de12a8ba4695cae8386d35fc52683b8
Minchan Kim <minchan@kernel.org>
2020-05-22 14:28:20 +1000

    pid: move pidfd_get_pid() to pid.c
    process_madvise syscall needs pidfd_get_pid function to translate pidfd to
    pid so this patch move the function to kernel/pid.c.
    
    Change-Id: Ie3bfdb29b3fb73a444481739b705ac7eb762d6c2
    Link: http://lkml.kernel.org/r/20200302193630.68771-5-minchan@kernel.org
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Reviewed-by: Suren Baghdasaryan <surenb@google.com>
    Suggested-by: Alexander Duyck <alexander.h.duyck@linux.intel.com>
    Reviewed-by: Alexander Duyck <alexander.h.duyck@linux.intel.com>
    Acked-by: Christian Brauner <christian.brauner@ubuntu.com>
    Reviewed-by: Vlastimil Babka <vbabka@suse.cz>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Jann Horn <jannh@google.com>
    Cc: Brian Geffon <bgeffon@google.com>
    Cc: Daniel Colascione <dancol@google.com>
    Cc: Joel Fernandes <joel@joelfernandes.org>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: John Dias <joaodias@google.com>
    Cc: Kirill Tkhai <ktkhai@virtuozzo.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Oleksandr Natalenko <oleksandr@redhat.com>
    Cc: Sandeep Patil <sspatil@google.com>
    Cc: SeongJae Park <sj38.park@gmail.com>
    Cc: SeongJae Park <sjpark@amazon.de>
    Cc: Shakeel Butt <shakeelb@google.com>
    Cc: Sonny Rao <sonnyrao@google.com>
    Cc: Tim Murray <timmurray@google.com>
    Cc: Christian Brauner <christian.brauner@ubuntu.com>
    Cc: <linux-man@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Git-Commit: e54e957dea26d6795d51842a785b8765efff396b
    Git-Repo: git://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git
    Signed-off-by: Charan Teja Reddy <charante@codeaurora.org>

--- kernel/exit.c
+++ kernel/exit.c
@@ -1470,23 +1470,6 @@ end:
 	return retval;
 }
 
-static struct pid *pidfd_get_pid(unsigned int fd)
-{
-	struct fd f;
-	struct pid *pid;
-
-	f = fdget(fd);
-	if (!f.file)
-		return ERR_PTR(-EBADF);
-
-	pid = pidfd_pid(f.file);
-	if (!IS_ERR(pid))
-		get_pid(pid);
-
-	fdput(f);
-	return pid;
-}
-
 static long kernel_waitid(int which, pid_t upid, struct waitid_info *infop,
 			  int options, struct rusage *ru)
 {
