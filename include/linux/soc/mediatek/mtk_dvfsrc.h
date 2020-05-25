9a010d21b3452f9d2e7cfe4b9e41c0b7b63bb7a7
Arvin Wang <arvin.wang@mediatek.com>
2020-05-25 13:31:24 +0800

    soc: mediatek: add MT6873 dvfsrc support
    1. add dvfsrc driver for MT6873
    2. change CONFIG_MTK_DVFSRC as tristate to support ko
    
    (cherry-picked from commit 9300b7f916ee50d9af50e2e27cd1835c5e113820)
    Signed-off-by: Arvin Wang <arvin.wang@mediatek.com>
    Change-Id: I5ceec78adf99872535a590deeb7f0852c9736b23
    CR-Id: ALPS05298037
    Feature: VCORE DVFS
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    Source_Git_Sha: 0914d09e6e4b8278875376f94240e4d8657f0707

--- include/linux/soc/mediatek/mtk_dvfsrc.h
+++ include/linux/soc/mediatek/mtk_dvfsrc.h
@@ -8,21 +8,27 @@
 #define MTK_DVFSRC_CMD_BW_REQUEST	0
 #define MTK_DVFSRC_CMD_OPP_REQUEST	1
 #define MTK_DVFSRC_CMD_VCORE_REQUEST	2
+#define MTK_DVFSRC_CMD_DRAM_REQUEST	3
+#define MTK_DVFSRC_CMD_HRTBW_REQUEST	4
+#define MTK_DVFSRC_CMD_VSCP_REQUEST	5
+#define MTK_DVFSRC_CMD_EMICLK_REQUEST	6
+
+#define MTK_DVFSRC_CMD_FORCEOPP_REQUEST	16
+
 
 #define MTK_DVFSRC_CMD_VCORE_QUERY	0
+#define MTK_DVFSRC_CMD_VCP_QUERY	1
+#define MTK_DVFSRC_CMD_CURR_LEVEL_QUERY	2
 
 #if IS_ENABLED(CONFIG_MTK_DVFSRC)
 void mtk_dvfsrc_send_request(const struct device *dev, u32 cmd, u64 data);
 int mtk_dvfsrc_query_info(const struct device *dev, u32 cmd, int *data);
-
 #else
-
 static inline void mtk_dvfsrc_send_request(const struct device *dev, u32 cmd,
 					   u64 data)
-{ return -ENODEV; }
-
+{ }
 static inline int mtk_dvfsrc_query_info(const struct device *dev, u32 cmd,
-					int *data);
+					int *data)
 { return -ENODEV; }
 
 #endif /* CONFIG_MTK_DVFSRC */
