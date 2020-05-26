1e4af638eea6a5591c4f1b312273f4dd0a1f8ece
hongxu.zhao <hongxu.zhao@mediatek.com>
2020-05-26 12:18:22 +0800

    [ALPS05143645] sensor: remove useless node
    remove useless node
    
    MTK-Commit-Id: 9bb2ca3559c59f04f8aebf204caeb353b4bbf65a
    
    Change-Id: I74fad0041ecd7f54ba0b494b796e699373855a93
    CR-Id: ALPS05143645
    Feature: Sensor Hub
    Signed-off-by: hongxu.zhao <hongxu.zhao@mediatek.com>

--- drivers/misc/mediatek/sensor/2.0/mtk_nanohub/nanohub/main.h
+++ drivers/misc/mediatek/sensor/2.0/mtk_nanohub/nanohub/main.h
@@ -32,12 +32,6 @@ struct nanohub_device {
 	void *drv_data;
 };
 
-struct nanohub_buf {
-	struct list_head list;
-	u8 buffer[255];
-	u8 length;
-};
-
 struct nanohub_data;
 
 struct nanohub_io {
@@ -46,13 +40,6 @@ struct nanohub_io {
 	wait_queue_head_t buf_wait;
 	struct list_head buf_list;
 };
-
-static inline struct nanohub_data *dev_get_nanohub_data(struct device *dev)
-{
-	struct nanohub_io *io = dev_get_drvdata(dev);
-
-	return io->data;
-}
 
 struct nanohub_data {
 	/* indices for io[] array */
@@ -93,59 +80,25 @@ struct nanohub_data {
 	struct mutex comms_lock;
 };
 
-enum {
-	KEY_WAKEUP_NONE,
-	KEY_WAKEUP,
-	KEY_WAKEUP_LOCK,
-};
-
-enum {
-	LOCK_MODE_NONE,
-	LOCK_MODE_NORMAL,
-	LOCK_MODE_IO,
-	LOCK_MODE_RESET,
-	LOCK_MODE_SUSPEND_RESUME,
-};
-
-int request_wakeup_ex(struct nanohub_data *data, long timeout,
-		      int key, int lock_mode);
-void release_wakeup_ex(struct nanohub_data *data, int key, int lock_mode);
 int nanohub_wait_for_interrupt(struct nanohub_data *data);
 int nanohub_wakeup_eom(struct nanohub_data *data, bool repeat);
 struct nanohub_device *
 nanohub_probe(struct device *dev, struct nanohub_device *nano_dev);
-int nanohub_reset(struct nanohub_data *data);
 int nanohub_suspend(struct nanohub_device *nano_dev);
 int nanohub_resume(struct nanohub_device *nano_dev);
 
-static inline int nanohub_irq1_fired(struct nanohub_data *data)
-{
-	const struct nanohub_platform_data *pdata = data->pdata;
-
-	return !gpio_get_value(pdata->irq1_gpio);
-}
-
-static inline int nanohub_irq2_fired(struct nanohub_data *data)
-{
-	const struct nanohub_platform_data *pdata = data->pdata;
-
-	return data->irq2 && !gpio_get_value(pdata->irq2_gpio);
-}
-
 static inline int request_wakeup_timeout(struct nanohub_data *data, int timeout)
 {
-	return request_wakeup_ex(data, timeout, KEY_WAKEUP, LOCK_MODE_NORMAL);
+	return 0;
 }
 
 static inline int request_wakeup(struct nanohub_data *data)
 {
-	return request_wakeup_ex(data, MAX_SCHEDULE_TIMEOUT, KEY_WAKEUP,
-				 LOCK_MODE_NORMAL);
+	return 0;
 }
 
 static inline void release_wakeup(struct nanohub_data *data)
 {
-	release_wakeup_ex(data, KEY_WAKEUP, LOCK_MODE_NORMAL);
 }
 
 #endif
