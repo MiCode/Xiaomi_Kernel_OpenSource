8b84707593fcbcf9acea0e53cdee9ea99e544e16
Isaac Lee <isaac.lee@mediatek.com>
2020-05-26 12:17:57 +0800

    [ALPS05123505] CPU hotplug: remove annoying debug message
    Since mt6853 is single cluster, remove unuse code to
    avoid annoying debug message
    
    MTK-Commit-Id: 4ff1c446b700104b4bd6a32cb86e1f0115b01b3d
    
    Change-Id: I67d5a8db3213e9fc8a59a0e379e85fd0bd7d4512
    Signed-off-by: Isaac Lee <isaac.lee@mediatek.com>
    CR-Id: ALPS05123505
    Feature: Power Management

--- drivers/misc/mediatek/base/power/cpuhotplug/mtk_cpuhp_core.c
+++ drivers/misc/mediatek/base/power/cpuhotplug/mtk_cpuhp_core.c
@@ -23,7 +23,8 @@
 
 static int is_multi_cluster(void)
 {
-#if defined(CONFIG_MACH_MT6885) || defined(CONFIG_MACH_MT6873)
+#if defined(CONFIG_MACH_MT6885) || defined(CONFIG_MACH_MT6873) || \
+	defined(CONFIG_MACH_MT6853)
 	return 0;
 #else
 	struct device_node *cn, *map;
