c939878ea2cbf4a0f77ec222ac2dfb597f9913de
Bhaumik Bhatt <bbhatt@codeaurora.org>
2020-05-26 18:09:17 -0700

    mhi: core: block fast suspends on controller device bus vote
    Currently only a pending packets check is used to block fast
    suspends. Since it does not exactly reflect what the core
    driver intends to do, allow a bus vote to the controller device
    to block fast suspends as well.
    
    Change-Id: I31266d296e855027f6b49b2dcfe4606bb48ac221
    Signed-off-by: Bhaumik Bhatt <bbhatt@codeaurora.org>

--- drivers/bus/mhi/core/mhi_pm.c
+++ drivers/bus/mhi/core/mhi_pm.c
@@ -1230,6 +1230,7 @@ int mhi_pm_fast_suspend(struct mhi_controller *mhi_cntrl, bool notify_client)
 	int ret;
 	enum MHI_PM_STATE new_state;
 	struct mhi_chan *itr, *tmp;
+	struct mhi_device *mhi_dev = mhi_cntrl->mhi_dev;
 
 	if (mhi_cntrl->pm_state == MHI_PM_DISABLE)
 		return -EINVAL;
@@ -1238,7 +1239,8 @@ int mhi_pm_fast_suspend(struct mhi_controller *mhi_cntrl, bool notify_client)
 		return -EIO;
 
 	/* do a quick check to see if any pending votes to keep us busy */
-	if (atomic_read(&mhi_cntrl->pending_pkts)) {
+	if (atomic_read(&mhi_cntrl->pending_pkts) ||
+	    atomic_read(&mhi_dev->bus_vote)) {
 		MHI_VERB("Busy, aborting M3\n");
 		return -EBUSY;
 	}
@@ -1257,7 +1259,8 @@ int mhi_pm_fast_suspend(struct mhi_controller *mhi_cntrl, bool notify_client)
 	 * Check the votes once more to see if we should abort
 	 * suspend.
 	 */
-	if (atomic_read(&mhi_cntrl->pending_pkts)) {
+	if (atomic_read(&mhi_cntrl->pending_pkts) ||
+	    atomic_read(&mhi_dev->bus_vote)) {
 		MHI_VERB("Busy, aborting M3\n");
 		ret = -EBUSY;
 		goto error_suspend;
