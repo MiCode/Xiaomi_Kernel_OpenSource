72a3871ceee3bd393202025b3f7072c0269c1e26
Pavankumar Kondeti <pkondeti@codeaurora.org>
2020-06-02 19:26:21 +0530

    sched: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: Icf76108b3ac676e7687848cb991ea9caf67962d0
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6496,7 +6496,7 @@ static void walt_find_best_target(struct sched_domain *sd, cpumask_t *cpus,
 					struct find_best_target_env *fbt_env)
 {
 	unsigned long min_util = uclamp_task_util(p);
-	unsigned long target_max_spare_cap = 0;
+	long target_max_spare_cap = 0;
 	unsigned long best_idle_cuml_util = ULONG_MAX;
 	/* Initialise with deepest possible cstate (INT_MAX) */
 	int shallowest_idle_cstate = INT_MAX;
