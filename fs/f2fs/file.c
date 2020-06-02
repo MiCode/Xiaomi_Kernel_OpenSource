d66ac5a713bcc3bbcce7da5307301276eded991e
Jaegeuk Kim <jaegeuk@kernel.org>
2020-06-02 10:54:55 +0800

    [ALPS05145656] f2fs: call f2fs_balance_fs outside of locked page
    Otherwise, we can hit deadlock by waiting for the locked page in
    move_data_block in GC.
    
     Thread A                     Thread B
     - do_page_mkwrite
      - f2fs_vm_page_mkwrite
       - lock_page
                                  - f2fs_balance_fs
                                      - mutex_lock(gc_mutex)
                                   - f2fs_gc
                                    - do_garbage_collect
                                     - ra_data_block
                                      - grab_cache_page
       - f2fs_balance_fs
        - mutex_lock(gc_mutex)
    
    MTK-Commit-Id: adb8004022fe762c19c80b58ac8df6fb1f1814dd
    
    Change-Id: I8d030fb8bc9af78f55dffadb773f827eb477bffc
    Fixes: 39a8695824510 ("f2fs: refactor ->page_mkwrite() flow")
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: mtk81325 <peng.zhou@mediatek.com>
    CR-Id: ALPS05145656
    Feature: [Android Default] F2FS File System

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -51,7 +51,7 @@ static int f2fs_vm_page_mkwrite(struct vm_fault *vmf)
 	struct page *page = vmf->page;
 	struct inode *inode = file_inode(vmf->vma->vm_file);
 	struct f2fs_sb_info *sbi = F2FS_I_SB(inode);
-	struct dnode_of_data dn = { .node_changed = false };
+	struct dnode_of_data dn;
 	int err;
 
 	if (unlikely(f2fs_cp_error(sbi))) {
@@ -59,11 +59,12 @@ static int f2fs_vm_page_mkwrite(struct vm_fault *vmf)
 		goto err;
 	}
 
+	/* should do out of any locked page */
+	f2fs_balance_fs(sbi, true);
+
 	sb_start_pagefault(inode->i_sb);
 
 	f2fs_bug_on(sbi, f2fs_has_inline_data(inode));
-
-	f2fs_balance_fs(sbi, dn.node_changed);
 
 	file_update_time(vmf->vma->vm_file);
 	down_read(&F2FS_I(inode)->i_mmap_sem);
