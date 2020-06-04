f1771866469cebed64521e7b9941e5cf9e255d51
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2020-06-03 20:23:34 -0700

    sched/fair: Add policy for restricting prefer_spread to newly idle balance
    Add policy for restricting prefer_spread to newly idle load balance
    by expanding the tunable range.
    
    To allow lower capacity CPUs to do aggressive newly idle load balance:
    echo 3 > /proc/sys/kernel/sched_prefer_spread
    
    To allow bother lower capacity and higher capacity CPUs to do
    aggressive newly idle load balance:
    echo 4 > /proc/sys/kernel/sched_prefer_spread
    
    Change-Id: Ia62ddb29bdf592a956a9688f277178ef71dee1b3
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
    Co-developed-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    [pkondeti@codeaurora.org: The tunable range is expanded]
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -568,7 +568,7 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler   = proc_dointvec_minmax,
 		.extra1		= &zero,
-		.extra2		= &two,
+		.extra2		= &four,
 	},
 	{
 		.procname	= "walt_rtg_cfs_boost_prio",
