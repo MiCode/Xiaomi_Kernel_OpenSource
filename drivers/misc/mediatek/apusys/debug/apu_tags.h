e050885deb4475290d7e0702e31d622addfa7cd8
Perry Hsu <perry.hsu@mediatek.com>
2020-06-05 09:47:00 +0800

    apusys: add tracepoint utility functions
    Add utility functions for APU IP drivers to register
    callback functions to the interests of their tracepoints:
    apu_tp_init() - Initialize trace point table
    apu_tp_exit() - Release trace point table
    
    Add tags utility for APU IP driver to save their traces
    in allocated ring buffer:
    apu_tags_alloc() - Allocate apu_tags, and create procfs entry
    apu_tags_free() - Free apu_tags, and removes its procfs entry
    apu_tag_add() - Add an entry to the ring tag buffer of apu_tags
    apu_tags_seq_time() - print time to seq_file
    apu_tags_seq() - print tags to seq_file
    
    (cherry-picked from commit dfbef085cae051aa59b644ef06fbc21b45611d0f)
    Signed-off-by: Perry Hsu <perry.hsu@mediatek.com>
    Change-Id: I819105534f257873f774c5e3841fe75cf2173a2c
    CR-Id: ALPS05298037
    Feature: [Module] APUSYS
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

---
+++ drivers/misc/mediatek/apusys/debug/apu_tags.h
@@ -0,0 +1,71 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2020 MediaTek Inc.
+ */
+
+#ifndef __APU_TAGS_H__
+#define __APU_TAGS_H__
+
+#include <linux/spinlock.h>
+#include <linux/proc_fs.h>
+#include <linux/types.h>
+
+#define APU_TAG_NAME_SZ 32
+
+typedef int (*apu_tags_seq_f)(struct seq_file *s, void *tag, void *priv);
+
+struct apu_tags {
+	spinlock_t lock;
+	char name[APU_TAG_NAME_SZ];
+	int dat_sz; /* size of tag data */
+	int ent_sz; /* size of tag entry (including tag info) */
+	int cnt;    /* total count of tags */
+	int idx;    /* current tag index */
+	char *tags; /* container of tags, size = ent_sz * cnt */
+	apu_tags_seq_f seq_tag;  /* seq print func. for each tag */
+	apu_tags_seq_f seq_info;  /* seq print func. for private info */
+	void *priv;  /* private data (ex: device data)*/
+	unsigned long used_mem; /* size of allocated memory */
+	struct list_head list;  /* link to apu tags list*/
+	struct proc_dir_entry *proc;  /* allocated procfs entry */
+};
+
+#if IS_ENABLED(CONFIG_MTK_APUSYS_DEBUG)
+struct apu_tags *apu_tags_alloc(const char *name, int size, int cnt,
+	apu_tags_seq_f seq_tag, apu_tags_seq_f seq_info, void *priv);
+void apu_tag_add(struct apu_tags *at, void *tag);
+void apu_tags_free(struct apu_tags *at);
+void apu_tags_seq_time(struct seq_file *s, uint64_t time);
+int apu_tags_seq(struct apu_tags *at, struct seq_file *s);
+
+#else
+static inline
+struct apu_tags *apu_tags_alloc(const char *name, int size, int cnt,
+	apu_tags_seq_f seq_tag, apu_tags_seq_f seq_info, void *priv)
+{
+	return NULL;
+}
+
+static inline
+void apu_tags_free(struct apu_tags *at)
+{
+}
+
+static inline
+void apu_tag_add(struct apu_tags *at, void *tag)
+{
+}
+
+static inline
+void apu_tags_seq_time(struct seq_file *s, uint64_t time)
+{
+}
+
+static inline
+int apu_tags_seq(struct apu_tags *at, struct seq_file *s)
+{
+	return 0;
+}
+#endif
+
+#endif
