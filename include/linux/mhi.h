6989fadad4eec966157f5893944379420841e661
Bhaumik Bhatt <bbhatt@codeaurora.org>
2020-06-05 11:41:20 -0700

    mhi: core: Use high priority workqueue for all MHI work
    Boot-up and shutdown require faster scheduling of the PM state
    worker. Use the high priority workqueue for processing any of
    the PM state transitions to ensure faster boot-up or shutdown
    handling on MHI host.
    
    Change-Id: Ie311426344d9561d490aa2bf6616aa30b4333e9a
    Signed-off-by: Bhaumik Bhatt <bbhatt@codeaurora.org>

--- include/linux/mhi.h
+++ include/linux/mhi.h
@@ -357,7 +357,7 @@ struct mhi_controller {
 	/* worker for different state transitions */
 	struct work_struct st_worker;
 	struct work_struct special_work;
-	struct workqueue_struct *special_wq;
+	struct workqueue_struct *wq;
 
 	wait_queue_head_t state_event;
 
