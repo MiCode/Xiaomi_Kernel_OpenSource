32fdd55aa511dd45263443c03609e59da0e8288d
Shane Chien <shane.chien@mediatek.com>
2020-06-05 16:02:00 +0800

    [ALPS05138726] ASoC: Fix coverity
    Fix uninitialized scalar variable.
    
    MTK-Commit-Id: f410624e1e7afecff1696e8886f0f41e0f6803a7
    
    Change-Id: Iddf42da479582c5674053f721524c5a3c7cc215b
    Signed-off-by: Shane Chien <Shane.Chien@mediatek.com>
    CR-Id: ALPS05138726
    Feature: DSP SmartPA

--- sound/soc/mediatek/mt6768/mt6768-afe-pcm.c
+++ sound/soc/mediatek/mt6768/mt6768-afe-pcm.c
@@ -2130,7 +2130,7 @@ err_irq:
 static int mt6768_afe_runtime_suspend(struct device *dev)
 {
 	struct mtk_base_afe *afe = dev_get_drvdata(dev);
-	unsigned int value;
+	unsigned int value = 0;
 	int ret;
 
 	dev_info(afe->dev, "%s()\n", __func__);
--- sound/soc/mediatek/mt6873/mt6873-afe-pcm.c
+++ sound/soc/mediatek/mt6873/mt6873-afe-pcm.c
@@ -3533,7 +3533,7 @@ err_irq:
 static int mt6873_afe_runtime_suspend(struct device *dev)
 {
 	struct mtk_base_afe *afe = dev_get_drvdata(dev);
-	unsigned int value;
+	unsigned int value = 0;
 	int ret;
 
 	dev_info(afe->dev, "%s()\n", __func__);
