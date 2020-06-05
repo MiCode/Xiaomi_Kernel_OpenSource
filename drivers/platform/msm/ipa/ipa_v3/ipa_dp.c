9fcdfafdb8c829630b49452cd838fc07cc6d0b39
Ashok Vuyyuru <avuyyuru@codeaurora.org>
2020-06-05 23:26:58 +0530

    msm: ipa3: Fix increase the NAPI budget to maximum
    Because of less NAPI budget it's need to reschedule multiple
    times in high tput scenario.To avoid multiple reschedule
    configuring maximum NAPI budget value.
    
    Change-Id: Id8745b54c3909d01df69376a823396ea4c94eebb
    Signed-off-by: Ashok Vuyyuru <avuyyuru@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
@@ -15,7 +15,7 @@
 #include "ipahal/ipahal.h"
 #include "ipahal/ipahal_fltrt.h"
 
-#define IPA_WAN_AGGR_PKT_CNT 5
+#define IPA_WAN_AGGR_PKT_CNT 1
 #define IPA_WAN_NAPI_MAX_FRAMES (NAPI_WEIGHT / IPA_WAN_AGGR_PKT_CNT)
 #define IPA_WAN_PAGE_ORDER 3
 #define IPA_LAN_AGGR_PKT_CNT 5
--- drivers/platform/msm/ipa/ipa_v3/ipa_i.h
+++ drivers/platform/msm/ipa/ipa_v3/ipa_i.h
@@ -81,7 +81,7 @@
 
 #define IPA_MAX_NUM_REQ_CACHE 10
 
-#define NAPI_WEIGHT 60
+#define NAPI_WEIGHT 64
 
 #define IPADBG(fmt, args...) \
 	do { \
