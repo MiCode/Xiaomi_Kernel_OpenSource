a334aa1a89092d4d4da7a089961a36180fb77dba
Wen Su <wen.su@mediatek.com>
2020-06-10 19:56:09 +0800

    mfd: mt6397: Add support for the MediaTek MT6359P Efuse
    This patch adds the pmic mt6359p Efuse driver
    
    (cherry-picked from commit 3041f193421d5e9720eb8278047431154a757bd2)
    Signed-off-by: Wen Su <wen.su@mediatek.com>
    Change-Id: If1f92bb711b6b2dc1a6a34adf081b0480b9db2b5
    CR-Id: ALPS05298037
    Feature: Power Management
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    Source_Git_Sha: 143ae9f26f310f6ea0134e2df45b13ebab884ec9

--- drivers/mfd/mt6397-core.c
+++ drivers/mfd/mt6397-core.c
@@ -119,6 +119,9 @@ static const struct mfd_cell mt6359p_devs[] = {
 		.num_resources = ARRAY_SIZE(mt6359p_accdet_resources),
 		.resources = mt6359p_accdet_resources,
 	}, {
+		.name = "mt6359p-efuse",
+		.of_compatible = "mediatek,mt6359p-efuse",
+	}, {
 		.name = "mt6359p-regulator",
 		.of_compatible = "mediatek,mt6359p-regulator"
 	}, {
