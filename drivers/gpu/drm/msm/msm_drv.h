ae77a32db2f69461c9ef04fa94370128cb6b65ee
Dhaval Patel <pdhaval@codeaurora.org>
2020-06-12 17:38:48 +0800

    drm/msm/sde: add connector property for frame trigger mode
    Add connector property for frame trigger mode.
    
    Change-Id: I6d193032b49c3a230b3e9a4897a9822540af61a2
    Signed-off-by: Dhaval Patel <pdhaval@codeaurora.org>
    Signed-off-by: Bruce Hoo <bingchua@codeaurora.org>
    Signed-off-by: Ray Zhang <rayz@codeaurora.org>

--- drivers/gpu/drm/msm/msm_drv.h
+++ drivers/gpu/drm/msm/msm_drv.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2016-2020, The Linux Foundation. All rights reserved.
  * Copyright (C) 2013 Red Hat
  * Author: Rob Clark <robdclark@gmail.com>
  *
@@ -199,6 +199,7 @@ enum msm_mdp_conn_property {
 	CONNECTOR_PROP_LP,
 	CONNECTOR_PROP_FB_TRANSLATION_MODE,
 	CONNECTOR_PROP_QSYNC_MODE,
+	CONNECTOR_PROP_CMD_FRAME_TRIGGER_MODE,
 
 	/* total # of properties */
 	CONNECTOR_PROP_COUNT
