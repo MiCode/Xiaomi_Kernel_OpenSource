95c72d05ab4182db77168d5bf18804fb65f93816
Jeevan Shriram <jshriram@codeaurora.org>
2020-06-17 10:50:38 -0700

    msm_rtb: Include <asm/io.h> header in rtb for QGKI
    Include io.h header files in RTB only for QGKI configuration.
    
    Change-Id: I81d7e38ed446040c63e35c66b0b0bd5352b970e6
    Signed-off-by: Jeevan Shriram <jshriram@codeaurora.org>

--- include/linux/msm_rtb.h
+++ include/linux/msm_rtb.h
@@ -1,12 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2012-2014, 2016, 2018-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2014, 2016, 2018-2020, The Linux Foundation. All rights reserved.
  */
 
 #ifndef __MSM_RTB_H__
 #define __MSM_RTB_H__
 
+#ifdef CONFIG_QCOM_RTB_QGKI
 #include <asm/io.h>
+#endif
 
 /*
  * These numbers are used from the kernel command line and sysfs
