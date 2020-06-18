b609c7cecb7173fc8a0faf4cd828de3275091a2c
Giuliano Procida <gprocida@google.com>
2020-06-18 19:32:23 +0100

    blk-mq: move _blk_mq_update_nr_hw_queues synchronize_rcu call
    This fixes the
    4.14 backport commit 574eb136ec7f315c3ef2ca68fa9b3e16c56baa24
    which was
    upstream commit f5bbbbe4d63577026f908a809f22f5fd5a90ea1f.
    
    The upstream commit added a call to synchronize_rcu to
    _blk_mq_update_nr_hw_queues, just after freezing queues.
    
    In the backport this landed just after unfreezeing queues.
    
    This commit moves the call to its intended place.
    
    Fixes: 574eb136ec7f ("blk-mq: sync the update nr_hw_queues with blk_mq_queue_tag_busy_iter")
    Signed-off-by: Giuliano Procida <gprocida@google.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -2738,6 +2738,10 @@ static void __blk_mq_update_nr_hw_queues(struct blk_mq_tag_set *set,
 
 	list_for_each_entry(q, &set->tag_list, tag_set_list)
 		blk_mq_freeze_queue(q);
+	/*
+	 * Sync with blk_mq_queue_tag_busy_iter.
+	 */
+	synchronize_rcu();
 
 	set->nr_hw_queues = nr_hw_queues;
 	blk_mq_update_queue_map(set);
@@ -2748,10 +2752,6 @@ static void __blk_mq_update_nr_hw_queues(struct blk_mq_tag_set *set,
 
 	list_for_each_entry(q, &set->tag_list, tag_set_list)
 		blk_mq_unfreeze_queue(q);
-	/*
-	 * Sync with blk_mq_queue_tag_busy_iter.
-	 */
-	synchronize_rcu();
 }
 
 void blk_mq_update_nr_hw_queues(struct blk_mq_tag_set *set, int nr_hw_queues)
