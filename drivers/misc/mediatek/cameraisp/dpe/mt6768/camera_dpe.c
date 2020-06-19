4439ff498686e612fd7df121c80660b9e1593019
Ryan Teng <ryan.teng@mediatek.com>
2020-06-19 14:22:12 +0800

    [ALPS05142691] DPE/MFB: Remove mmap functions
    [Detail] Remove mmap functions
    
    MTK-Commit-Id: 6a7766d730d0fbcccd7bb2b5d7a9505585c542f3
    
    Change-Id: If9627e4a671997495f8d8a3a702c40c45369caae
    Signed-off-by: Ryan Teng <ryan.teng@mediatek.com>
    CR-Id: ALPS05142691
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/misc/mediatek/cameraisp/dpe/mt6768/camera_dpe.c
+++ drivers/misc/mediatek/cameraisp/dpe/mt6768/camera_dpe.c
@@ -3872,49 +3872,6 @@ EXIT:
 	return 0;
 }
 
-static signed int DPE_mmap(
-	struct file *pFile, struct vm_area_struct *pVma)
-{
-	long length = 0;
-	unsigned int pfn = 0x0;
-
-	length = pVma->vm_end - pVma->vm_start;
-	/*  */
-	pVma->vm_page_prot = pgprot_noncached(pVma->vm_page_prot);
-	pfn = pVma->vm_pgoff << PAGE_SHIFT;
-
-
-	LOG_INF("%s: pVma->vm_pgoff(0x%lx)", __func__, pVma->vm_pgoff);
-	LOG_INF("%s: pfn(0x%x),phy(0x%lx)",
-		__func__, pfn, pVma->vm_pgoff << PAGE_SHIFT);
-	LOG_INF("pVmapVma->vm_start(0x%lx)", pVma->vm_start);
-	LOG_INF("pVma->vm_end(0x%lx),length(0x%lx)", pVma->vm_end, length);
-
-	switch (pfn) {
-	case DPE_BASE_HW:
-	if (length > DPE_REG_RANGE) {
-		LOG_INF("mmap range error :module:0x%x length(0x%lx)",
-		pfn,
-		length);
-		LOG_INF("mmap range error :DPE_REG_RANGE(0x%x)!",
-		DPE_REG_RANGE);
-
-		return -EAGAIN;
-	}
-	break;
-	default:
-		LOG_INF("Illegal starting HW addr for mmap!");
-		return -EAGAIN;
-	}
-	if (remap_pfn_range
-		(pVma, pVma->vm_start, pVma->vm_pgoff,
-		pVma->vm_end - pVma->vm_start, pVma->vm_page_prot)) {
-		return -EAGAIN;
-	}
-	/*  */
-	return 0;
-}
-
 static dev_t DPEDevNo;
 static struct cdev *pDPECharDrv;
 static struct class *pDPEClass;
@@ -3924,7 +3881,7 @@ static const struct file_operations DPEFileOper = {
 	.open = DPE_open,
 	.release = DPE_release,
 	/* .flush   = mt_DPE_flush, */
-	.mmap = DPE_mmap,
+	/* .mmap = DPE_mmap, */
 	.unlocked_ioctl = DPE_ioctl,
 #ifdef CONFIG_COMPAT
 	.compat_ioctl = DPE_ioctl_compat,
