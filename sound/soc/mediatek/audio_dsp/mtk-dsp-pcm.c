bb097f65d39ad99ecfe9652b7d48047d8bdd01d7
Shane Chien <shane.chien@mediatek.com>
2020-06-19 14:56:10 +0800

    [ALPS05185611] ASoC: Fix kasan user-after-free work
    [Detail]
    use dev_set_name in probe will cause
    risk (use-after-free)
    and inconsistency between .pdev->name. / .pdev->dev->kobj->name
    
    [Solution]
    sync .pdev->name. / .pdev->dev->kobj->name after dev_set_name
    
    MTK-Commit-Id: 9ff92b87b83f0b0ef54720d92f197e5b468ded28
    
    Change-Id: Ie2d43bd5dbac148a472f077f16bb61af27152bd4
    Signed-off-by: Shane Chien <Shane.Chien@mediatek.com>
    CR-Id: ALPS05185611
    Feature: Phone Sound

--- sound/soc/mediatek/audio_dsp/mtk-dsp-pcm.c
+++ sound/soc/mediatek/audio_dsp/mtk-dsp-pcm.c
@@ -157,6 +157,12 @@ static int dsp_pcm_dev_probe(struct platform_device *pdev)
 	if (pdev->dev.of_node)
 		dev_set_name(&pdev->dev, "%s", "snd_audio_dsp");
 
+	/*
+	 * fix uaf(use afer free) issue:backup pdev->name,
+	 * device_rename will free pdev->name
+	 */
+	pdev->name = pdev->dev.kobj.name;
+
 	ret = snd_soc_register_platform(&pdev->dev, &mtk_dsp_pcm_platform);
 	if (ret) {
 		dev_warn(&pdev->dev, "err_platform\n");
