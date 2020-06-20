df275d51b08f6cb8a6a062df73181d48b495ef54
Charan Teja Reddy <charante@codeaurora.org>
2020-06-21 00:25:36 +0530

    mm, memory_hotplug: update pcp lists everytime onlining a memory block
    When onlining a first memory block in a zone, pcp lists are not updated
    thus pcp struct will have the default setting of ->high = 0,->batch = 1.
    This means till the second memory block in a zone(if it have) is onlined
    the pcp lists of this zone will not contain any pages because pcp's
    ->count is always greater than ->high thus free_pcppages_bulk() is
    called to free batch size(=1) pages every time system wants to add a
    page to the pcp list through free_unref_page(). To put this in a word,
    system is not using benefits offered by the pcp lists when there is a
    single onlineable memory block in a zone. Correct this by always
    updating the pcp lists when memory block is onlined.
    
    Change-Id: I5a0c5cc128481fdb48b9dc4e1fe2860f59e958e3
    Signed-off-by: Charan Teja Reddy <charante@codeaurora.org>

--- mm/memory_hotplug.c
+++ mm/memory_hotplug.c
@@ -951,8 +951,7 @@ int __ref online_pages(unsigned long pfn, unsigned long nr_pages, int online_typ
 		node_states_set_node(nid, &arg);
 		if (need_zonelists_rebuild)
 			build_all_zonelists(NULL);
-		else
-			zone_pcp_update(zone);
+		zone_pcp_update(zone);
 	}
 
 	init_per_zone_wmark_min();
