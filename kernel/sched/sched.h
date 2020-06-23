eea68c09c66fd3d6fd57da542f0e3f09a69f3b47
Wenju Xu (许文举) <wenju.xu@mediatek.com>
2020-06-23 16:49:52 +0800

    [ALPS05163319] sched:revert debug clock_update_flags < RQCF_ACT_SKIP
    This reverts commit 42712780e69a07acde3d59f47528fb19cd30948f.
    
    MTK-Commit-Id: 4b426c001eacc3e24cef9624e4d81261db9fd5b7
    
    Change-Id: I18a4d630df1eb5c81a3d598f352310c62ee8166f
    Signed-off-by: wenju.xu <wenju.xu@mediatek.com>
    CR-Id: ALPS05163319
    Feature: Energy Aware Scheduling Plus (EAS+)

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -47,9 +47,6 @@
 # define SCHED_WARN_ON(x)	({ (void)(x), 0; })
 #endif
 
-#ifdef CONFIG_MTK_ENG_BUILD
-#include <linux/ftrace.h>
-#endif
 struct rq;
 struct cpuidle_state;
 
@@ -930,10 +927,6 @@ struct rq {
 	int idle_state_idx;
 #endif
 	unsigned long rotate_flags;
-#ifdef CONFIG_MTK_ENG_BUILD
-	unsigned long rf_bt[10];
-	unsigned long last_flags_from_rf;
-#endif
 };
 
 static inline int cpu_of(struct rq *rq)
@@ -1001,42 +994,13 @@ static inline u64 __rq_clock_broken(struct rq *rq)
 #define RQCF_ACT_SKIP	0x02
 #define RQCF_UPDATED	0x04
 
-#ifdef CONFIG_MTK_ENG_BUILD
-static inline void record_rf_bt(unsigned long *bt)
-{
-	int i = 0;
-
-	for (i = 0; i < 10; i++)
-		bt[i] = (unsigned long)ftrace_return_address(i);
-}
-
-static inline void print_rf_bt(unsigned long *bt)
-{
-	int i = 0;
-
-	for (i = 0; i < 10; i++)
-		print_ip_sym(bt[i]);
-}
-
-static inline void check_rf_info(struct rq *rq)
-{
-	if (rq->clock_update_flags >= RQCF_ACT_SKIP)
-		return;
-	print_rf_bt(rq->rf_bt);
-}
-#endif
 static inline void assert_clock_updated(struct rq *rq)
 {
 	/*
 	 * The only reason for not seeing a clock update since the
 	 * last rq_pin_lock() is if we're currently skipping updates.
 	 */
-#ifdef CONFIG_MTK_ENG_BUILD
-	check_rf_info(rq);
-	BUG_ON(rq->clock_update_flags < RQCF_ACT_SKIP);
-#else
 	SCHED_WARN_ON(rq->clock_update_flags < RQCF_ACT_SKIP);
-#endif
 }
 
 static inline u64 rq_clock(struct rq *rq)
@@ -1084,9 +1048,6 @@ static inline void rq_pin_lock(struct rq *rq, struct rq_flags *rf)
 #ifdef CONFIG_SCHED_DEBUG
 	rq->clock_update_flags &= (RQCF_REQ_SKIP|RQCF_ACT_SKIP);
 	rf->clock_update_flags = 0;
-#ifdef CONFIG_MTK_ENG_BUILD
-	record_rf_bt(rq->rf_bt);
-#endif
 #endif
 }
 
@@ -1109,9 +1070,6 @@ static inline void rq_repin_lock(struct rq *rq, struct rq_flags *rf)
 	 * Restore the value we stashed in @rf for this pin context.
 	 */
 	rq->clock_update_flags |= rf->clock_update_flags;
-#ifdef CONFIG_MTK_ENG_BUILD
-	rq->last_flags_from_rf = rf->clock_update_flags;
-#endif
 #endif
 }
 
