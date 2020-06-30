b94fd840804caddb3791ab48de2b9a0e21d7f12d
Anirudh Ghayal <aghayal@codeaurora.org>
2020-06-30 15:14:17 +0530

    power: smb5-lib: Report the CURRENT_MAX as 1A in CC-mode
    Userspace uses the CURRENT_MAX power-supply property to
    determine the strength of the charger. In CC-mode of operation
    the main-charger's ICL is configured low and hence the
    CURRENT_MAX reported is lower attributing it as a weak-charger.
    Fix this by reporting a fixed value of 1A during CC-mode.
    
    Change-Id: Id8745b54c3909d01df69376a823396ea4c94eeb1
    Signed-off-by: Anirudh Ghayal <aghayal@codeaurora.org>

--- drivers/power/supply/qcom/smb5-lib.h
+++ drivers/power/supply/qcom/smb5-lib.h
@@ -755,6 +755,8 @@ int smblib_get_prop_charger_temp(struct smb_charger *chg,
 int smblib_get_prop_die_health(struct smb_charger *chg);
 int smblib_get_prop_smb_health(struct smb_charger *chg);
 int smblib_get_prop_connector_health(struct smb_charger *chg);
+int smblib_get_prop_input_current_max(struct smb_charger *chg,
+				  union power_supply_propval *val);
 int smblib_set_prop_thermal_overheat(struct smb_charger *chg,
 			       int therm_overheat);
 int smblib_get_skin_temp_status(struct smb_charger *chg);
