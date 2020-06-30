8c5756c0e413e11d11085de9c892b238982ba8a1
Vipin Deep Kaur <vkaur@codeaurora.org>
2020-06-30 15:03:50 +0530

    spi: spi-msm-geni: Rename shared_se flag to gsi_mode flag
    Shared_se flag specifies that GSI mode is enabled. Rename
    the flag to gsi_mode.
    
    Change-Id: I2b947c1d74ddbd533466a0d48659c45c736834d9
    Signed-off-by: Vipin Deep Kaur <vkaur@codeaurora.org>

--- drivers/spi/spi-msm-geni.c
+++ drivers/spi/spi-msm-geni.c
@@ -157,7 +157,7 @@ struct spi_geni_master {
 	int num_rx_eot;
 	int num_xfers;
 	void *ipc;
-	bool shared_se; /* GSI Mode */
+	bool gsi_mode; /* GSI Mode */
 	bool shared_ee; /* Dual EE use case */
 	bool dis_autosuspend;
 	bool cmd_done;
@@ -864,7 +864,7 @@ static int spi_geni_prepare_transfer_hardware(struct spi_master *spi)
 
 	/* Adjust the IB based on the max speed of the slave in kHz.*/
 	rsc->ib = (max_speed * DEFAULT_BUS_WIDTH) / 1000;
-	if (mas->shared_se && !mas->shared_ee) {
+	if (mas->gsi_mode && !mas->shared_ee) {
 		struct se_geni_rsc *rsc;
 		int ret = 0;
 
@@ -984,7 +984,7 @@ setup_ipc:
 		if (mas->set_miso_sampling)
 			spi_geni_set_sampling_rate(mas, major, minor);
 
-		mas->shared_se =
+		mas->gsi_mode =
 			(geni_read_reg(mas->base, GENI_IF_FIFO_DISABLE_RO) &
 							FIFO_IF_DISABLE);
 		if (mas->dis_autosuspend)
@@ -1003,7 +1003,7 @@ static int spi_geni_unprepare_transfer_hardware(struct spi_master *spi)
 	if (mas->shared_ee)
 		return 0;
 
-	if (mas->shared_se) {
+	if (mas->gsi_mode) {
 		struct se_geni_rsc *rsc;
 		int ret = 0;
 
@@ -1632,7 +1632,7 @@ static int spi_geni_probe(struct platform_device *pdev)
 				"qcom,disable-autosuspend");
 	/*
 	 * This property will be set when spi is being used from
-	 * dual Execution Environments unlike shared_se flag
+	 * dual Execution Environments unlike gsi_mode flag
 	 * which is set if SE is in GSI mode.
 	 */
 	geni_mas->shared_ee =
@@ -1717,7 +1717,7 @@ static int spi_geni_runtime_suspend(struct device *dev)
 	if (geni_mas->shared_ee)
 		goto exit_rt_suspend;
 
-	if (geni_mas->shared_se) {
+	if (geni_mas->gsi_mode) {
 		ret = se_geni_clks_off(&geni_mas->spi_rsc);
 		if (ret)
 			GENI_SE_ERR(geni_mas->ipc, false, NULL,
@@ -1742,7 +1742,7 @@ static int spi_geni_runtime_resume(struct device *dev)
 	if (geni_mas->shared_ee)
 		goto exit_rt_resume;
 
-	if (geni_mas->shared_se) {
+	if (geni_mas->gsi_mode) {
 		ret = se_geni_clks_on(&geni_mas->spi_rsc);
 		if (ret)
 			GENI_SE_ERR(geni_mas->ipc, false, NULL,
