c3c273370a01fc4efdb5e7d04718104e90a0b260
John Wei <john.wei@mediatek.com>
2020-07-03 14:29:39 +0800

    [ALPS05216386] cam: fix security issue
    fix error in mmap
    
    MTK-Commit-Id: 00a6f4bd90ae254283bdd9952125b0ac70d4c98d
    
    Change-Id: I844434ffa936cfa452eef0cb9c36e7d87d916683
    Signed-off-by: John Wei <john.wei@mediatek.com>
    CR-Id: ALPS05216386
    Feature: Vulnerability Scan
    (cherry picked from commit dc069f5edbbd4cde6542ace27ab43c977a1162a9)

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/seninf.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/seninf.c
@@ -172,7 +172,7 @@ static MINT32 seninf_release(struct inode *pInode, struct file *pFile)
 static MINT32 seninf_mmap(struct file *pFile, struct vm_area_struct *pVma)
 {
 	unsigned long length = 0;
-	MUINT32 pfn = 0x0;
+	unsigned long pfn = 0x0;
 
 	/*PK_DBG("- E."); */
 	length = (pVma->vm_end - pVma->vm_start);
@@ -189,30 +189,30 @@ static MINT32 seninf_mmap(struct file *pFile, struct vm_area_struct *pVma)
 	case SENINF_MAP_BASE_REG:
 		if (length > SENINF_MAP_LENGTH_REG) {
 			PK_PR_ERR(
-			"mmap range error :module(0x%x),length(0x%lx),SENINF_BASE_RANGE(0x%x)!\n",
+			"mmap range error :module(0x%lx),length(0x%lx),SENINF_BASE_RANGE(0x%x)!\n",
 			pfn, length, SENINF_MAP_LENGTH_REG);
-			return -EAGAIN;
+			return -EINVAL;
 		}
 		break;
 	case SENINF_MAP_BASE_ANA:
 		if (length > SENINF_MAP_LENGTH_ANA) {
 			PK_PR_ERR(
-			"mmap range error :module(0x%x),length(0x%lx),MIPI_RX_RANGE(0x%x)!\n",
+			"mmap range error :module(0x%lx),length(0x%lx),MIPI_RX_RANGE(0x%x)!\n",
 			pfn, length, SENINF_MAP_LENGTH_ANA);
-			return -EAGAIN;
+			return -EINVAL;
 		}
 		break;
 	case SENINF_MAP_BASE_GPIO:
 		if (length > SENINF_MAP_LENGTH_GPIO) {
 			PK_PR_ERR(
-			"mmap range error :module(0x%x),length(0x%lx),GPIO_RX_RANGE(0x%x)!\n",
+			"mmap range error :module(0x%lx),length(0x%lx),GPIO_RX_RANGE(0x%x)!\n",
 			pfn, length, SENINF_MAP_LENGTH_GPIO);
-			return -EAGAIN;
+			return -EINVAL;
 		}
 		break;
 	default:
 		PK_PR_ERR("Illegal starting HW addr for mmap!\n");
-		return -EAGAIN;
+		return -EINVAL;
 
 	}
 
@@ -222,7 +222,7 @@ static MINT32 seninf_mmap(struct file *pFile, struct vm_area_struct *pVma)
 		pVma->vm_pgoff,
 		pVma->vm_end - pVma->vm_start,
 		pVma->vm_page_prot))
-		return -EAGAIN;
+		return -EINVAL;
 
 	return 0;
 }
