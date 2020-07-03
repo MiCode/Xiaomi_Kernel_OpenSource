ca80b7dbaeb0a6450fefc67f096e455d3c73966b
mingyuan ma <mingyuan.ma@mediatek.com>
2020-07-03 14:26:01 +0800

    [ALPS05139084] ion: add procfs support
    R requirement, disable debugfs on user load, add new debug files on
    procfs.
    
    MTK-Commit-Id: 44ec65100bb532678294bbb36ac200a0e2163754
    
    Change-Id: I228b459158fd024d2fceded1ab1fcc3d4ebd7026
    Signed-off-by: Mingyuan Ma <mingyuan.ma@mediatek.com>
    CR-Id: ALPS05139084
    Feature: [Module]ION/M4U

--- drivers/staging/android/ion/mtk/ion_mm_heap.c
+++ drivers/staging/android/ion/mtk/ion_mm_heap.c
@@ -24,7 +24,6 @@
 #include <linux/dma-buf.h>
 //#include <mmprofile.h>
 //#include <mmprofile_function.h>
-#include <linux/debugfs.h>
 #include <linux/kthread.h>
 #include <linux/fdtable.h>
 #include <linux/sched/task.h>
