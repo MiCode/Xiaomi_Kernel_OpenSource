cdd72fdb19cfe98b83be2fbd56cda42297dd0e7f
Yong Ding <yongding@codeaurora.org>
2020-07-03 15:20:53 +0800

    soc: qcom: add HAB driver
    Add snapshot for HAB from msm-4.14 commit b1ff2848541a
     (soc: qcom: hab: fix memory leak).
    
    Change-Id: I88cdfd2c3530320b80a64f294b59611d4a80cc08
    Signed-off-by: Yong Ding <yongding@codeaurora.org>

---
+++ drivers/soc/qcom/hab/khab_test.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2018, The Linux Foundation. All rights reserved.
+ */
+#ifndef __KHAB_TEST_H
+#define __KHAB_TEST_H
+
+int hab_perf_test(long testId);
+
+#endif /* __KHAB_TEST_H */
