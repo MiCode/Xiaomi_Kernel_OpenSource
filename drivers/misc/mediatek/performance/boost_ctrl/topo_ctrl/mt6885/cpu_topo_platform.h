29be8f84d05bbc89caa85ae727b9caf494a050e5
Ian-Y Chen <ian-y.chen@mediatek.com>
2020-07-03 14:28:58 +0800

    [ALPS05192918] power: support tri-gear
    [Detail]
    Support 3 gear in cpu_topo_platform.h of mt6885
    
    MTK-Commit-Id: e30f7393f65eb6f252dbd0c507fdd3fd11612afd
    
    Change-Id: I892f42a0f87a8009bc4006aac2aaf012a5c5632a
    Signed-off-by: Ian-Y Chen <ian-y.chen@mediatek.com>
    CR-Id: ALPS05192918
    Feature: [Module]PowerHAL

--- drivers/misc/mediatek/performance/boost_ctrl/topo_ctrl/mt6885/cpu_topo_platform.h
+++ drivers/misc/mediatek/performance/boost_ctrl/topo_ctrl/mt6885/cpu_topo_platform.h
@@ -26,6 +26,37 @@ extern "C" {
 /* TODO: remove these workaround for k49 migration */
 #define NO_SCHEDULE_API		(1)
 
+#if defined(CONFIG_MTK_SCHED_MULTI_GEARS)
+
+#define TOTAL_CORE_NUM	(CORE_NUM_L+CORE_NUM_B+CORE_NUM_BB)
+#define CORE_NUM_L	(4)
+#define CORE_NUM_B	(3)
+#define CORE_NUM_BB	(1)
+
+static inline unsigned int get_cluster_cpu_core(unsigned int id)
+{
+	if (id == 0)
+		return CORE_NUM_L;
+	else if (id == 1)
+		return CORE_NUM_B;
+	else if (id == 2)
+		return CORE_NUM_BB;
+	return 0;
+}
+
+/*==============================================================*/
+/* Enum								*/
+/*==============================================================*/
+enum ppm_cluster {
+	PPM_CLUSTER_L = 0,
+	PPM_CLUSTER_B,
+	PPM_CLUSTER_BB,
+
+	NR_PPM_CLUSTERS,
+};
+
+#else
+
 #define TOTAL_CORE_NUM	(CORE_NUM_L+CORE_NUM_B)
 #define CORE_NUM_L	(4)
 #define CORE_NUM_B	(4)
@@ -42,6 +73,8 @@ enum ppm_cluster {
 
 	NR_PPM_CLUSTERS,
 };
+
+#endif /* CONFIG_MTK_SCHED_MULTI_GEARS */
 
 /*==============================================================*/
 /* Data Structures						*/
