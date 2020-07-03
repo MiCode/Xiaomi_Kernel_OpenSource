eef704a52fbba614f4a07e6560e287fb9c0425cd
cc.hwang <cc.hwang@mediatek.com>
2020-07-03 12:14:54 +0800

    memory: mediatek: add EMI debug driver
    Create a new file emi-dbg.c for EMI debug functions.
    
    Currenly there is only one function EMI debug-API, which is used for
    debugging EMI hang.
    Whenever there is SMI hang, the SMI driver will call the function
    mtk_emidbg_dump() to activate the EMI debug-API function.
    Whenever there is SSPM IPI-timeout, the SSPM IPI driver will call the
    function, too.
    
    In the next year, we may add a new EMI debug function - LastEMI.
    
    (cherry-picked from commit 834c2f292be43958210bca510b0def94352a3cef)
    Signed-off-by: cc.hwang <cc.hwang@mediatek.com>
    Change-Id: If8bc4bed3315ca6c8d0d1d192f79dc48dd961785
    CR-Id: ALPS05298037
    Feature: External Memory Interface(emi)
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    Source_Git_Sha: 6e141620c3415ba781ee431686a78a5d2cd5a192

---
+++ drivers/memory/mediatek/emi-dbg.c
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: GPL-2.0
+
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#include <linux/arm-smccc.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/soc/mediatek/mtk_sip_svc.h>
+#include <soc/mediatek/emi.h>
+
+DEFINE_SPINLOCK(emidbg_lock);
+
+/*
+ * mtk_emidbg_dump - dump emi full status to atf log
+ *
+ */
+void mtk_emidbg_dump(void)
+{
+	unsigned long spinlock_save_flags;
+	struct arm_smccc_res smc_res;
+
+	spin_lock_irqsave(&emidbg_lock, spinlock_save_flags);
+
+	arm_smccc_smc(MTK_SIP_EMIMPU_CONTROL, MTK_EMIDBG_DUMP,
+		0, 0, 0, 0, 0, 0, &smc_res);
+
+	while (smc_res.a0 > 0) {
+		arm_smccc_smc(MTK_SIP_EMIMPU_CONTROL, MTK_EMIDBG_MSG,
+		0, 0, 0, 0, 0, 0, &smc_res);
+
+		pr_info("emidbg: %d, 0x%x, 0x%x, 0x%x\n",
+			(int)smc_res.a0,
+			(unsigned int)smc_res.a1,
+			(unsigned int)smc_res.a2,
+			(unsigned int)smc_res.a3);
+	}
+
+	spin_unlock_irqrestore(&emidbg_lock, spinlock_save_flags);
+}
+EXPORT_SYMBOL(mtk_emidbg_dump);
+
+static __init int emidbg_init(void)
+{
+	pr_info("emidbg was loaded\n");
+
+	return 0;
+}
+
+static __exit void emidbg_exit(void)
+{
+	pr_info("emidbg was unloaded\n");
+}
+
+module_init(emidbg_init);
+module_exit(emidbg_exit);
+
+MODULE_DESCRIPTION("MediaTek EMI Debug Driver");
+MODULE_LICENSE("GPL v2");
