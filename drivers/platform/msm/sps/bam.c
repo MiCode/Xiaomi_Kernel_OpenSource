96b68dbe9d32d10e3c96858abd9287771a311408
Rama Krishna Phani A <rphani@codeaurora.org>
2020-07-07 16:02:33 +0530

    msm: sps: Correct descriptor fifo address to dump contents
    Few clients of BAM will use SMMU for data transfers.
    Assign correct descriptor fifo address for SMMU clients
    of BAM while dumping contents.
    
    Change-Id: I21584e6778315f51cac0d202d41f4c81a56fcbf0
    Signed-off-by: Rama Krishna Phani A <rphani@codeaurora.org>

--- drivers/platform/msm/sps/bam.c
+++ drivers/platform/msm/sps/bam.c
@@ -2174,12 +2174,15 @@ void print_bam_pipe_desc_fifo(void *virt_addr, u32 pipe_index, u32 option)
 	u32 pipe = pipe_index;
 	u32 desc_fifo_addr;
 	u32 desc_fifo_size;
-	u32 *desc_fifo;
+	u32 __iomem *desc_fifo;
 	int i;
 	char desc_info[MAX_MSG_LEN];
+	struct sps_bam *dev;
 
 	if (base == NULL)
 		return;
+
+	dev = to_sps_bam_dev(virt_addr);
 
 	desc_fifo_addr = bam_read_reg(base, P_DESC_FIFO_ADDR, pipe);
 	desc_fifo_size = bam_read_reg_field(base, P_FIFO_SIZES, pipe,
@@ -2202,7 +2205,14 @@ void print_bam_pipe_desc_fifo(void *virt_addr, u32 pipe_index, u32 option)
 		"BAM_P_DESC_FIFO_SIZE: 0x%x (%d)\n\n",
 		desc_fifo_addr, desc_fifo_size, desc_fifo_size);
 
-	desc_fifo = (u32 *) phys_to_virt(desc_fifo_addr);
+	if (dev->props.options & SPS_BAM_SMMU_EN) {
+		struct sps_pipe *pipe_indx = dev->pipes[pipe_index];
+
+		SPS_DUMP("%s", "SMMU is enabled\n");
+		desc_fifo = pipe_indx->map->desc.base;
+	} else {
+		desc_fifo = (u32 __iomem *) phys_to_virt(desc_fifo_addr);
+	}
 
 	if (option == 100) {
 		SPS_DUMP("%s",
