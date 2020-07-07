e4b6ec1ac779585116a0b4410fc9045f3bd06ee1
Walter-zh Wu <walter-zh.wu@mediatek.com>
2020-07-07 20:56:45 +0800

    tmem: port trusted memory UT
    Add trusted memory UT to verify trusted memory kernel module
    
    (cherry-picked from commit 94beb2f57976251662327cd853f3331d84be8fab)
    Signed-off-by: Walter-zh Wu <walter-zh.wu@mediatek.com>
    Change-Id: Id590f7f1fc50443a111330747f0e9336a6f0929e
    CR-Id: ALPS05298037
    Feature: Trustonic TEE (Trusted Execution Environment)
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

--- drivers/misc/mediatek/trusted_mem/private/tmem_utils.h
+++ drivers/misc/mediatek/trusted_mem/private/tmem_utils.h
@@ -22,13 +22,17 @@
 #define COMPILE_ASSERT(condition) ((void)sizeof(char[1 - 2 * !!!(condition)]))
 
 #define GET_TIME_DIFF_SEC_P(start, end)                                        \
-	(int)((end->tv_usec > start->tv_usec)                                  \
+	(int)(((end->tv_nsec / NSEC_PER_USEC) >                                \
+		(start->tv_nsec / NSEC_PER_USEC))                              \
 		      ? (end->tv_sec - start->tv_sec)                          \
 		      : (end->tv_sec - start->tv_sec - 1))
 
 #define GET_TIME_DIFF_USEC_P(start, end)                                       \
-	(int)((end->tv_usec > start->tv_usec)                                  \
-		      ? (end->tv_usec - start->tv_usec)                        \
-		      : (1000000 + end->tv_usec - start->tv_usec))
+	(int)(((end->tv_nsec / NSEC_PER_USEC) >                                \
+		(start->tv_nsec / NSEC_PER_USEC))                              \
+		      ? ((end->tv_nsec / NSEC_PER_USEC) -                      \
+			(start->tv_nsec / NSEC_PER_USEC))                      \
+		      : (1000000 + (end->tv_nsec / NSEC_PER_USEC) -            \
+			(start->tv_nsec / NSEC_PER_USEC)))
 
 #endif /* end of TMEM_UTILS_H */
