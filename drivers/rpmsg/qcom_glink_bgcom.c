eaa69fb091ec2c8bf6a8f348fa8e9f8920d830e1
Prudhvi Yarlagadda <pyarlaga@codeaurora.org>
2020-07-08 11:03:26 +0530

    rpmsg: qcom_glink_bgcom: Correct the copyright licence
    Correct the copyright licence for the glink-bgcom transport
    driver.
    
    Change-Id: I40537f5e1c3ca5e7385bab03dd65fa6549c54bc6
    Signed-off-by: Prudhvi Yarlagadda <pyarlaga@codeaurora.org>

--- drivers/rpmsg/qcom_glink_bgcom.c
+++ drivers/rpmsg/qcom_glink_bgcom.c
@@ -1,4 +1,6 @@
-/* Copyright (c) 2020, The Linux Foundation. All rights reserved.
+/*
+ * Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2016-2017, Linaro Ltd
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
