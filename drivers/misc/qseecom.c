e17fa69ad9446aeeb1cba0bba5033d9b88a283d1
Monika Singh <monising@codeaurora.org>
2020-07-08 13:23:21 +0530

    qseecom: Remove virtual address print
    Printing virtual address can leak kernel addresses.So remove
    vaddr print to not leak kernel pointers to unprivileged users.
    
    Change-Id: Ib10fe6b5b308361a4df7fbe03733af8246685201
    Signed-off-by: Monika Singh <monising@codeaurora.org>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -2015,8 +2015,8 @@ static int qseecom_set_client_mem_param(struct qseecom_dev_handle *data,
 
 	if ((req.ifd_data_fd <= 0) || (req.virt_sb_base == NULL) ||
 					(req.sb_len == 0)) {
-		pr_err("Invalid input(s)ion_fd(%d), sb_len(%d), vaddr(0x%pK)\n",
-			req.ifd_data_fd, req.sb_len, req.virt_sb_base);
+		pr_err("Invalid input(s)ion_fd(%d), sb_len(%d)\n",
+			req.ifd_data_fd, req.sb_len);
 		return -EFAULT;
 	}
 	if (!access_ok(VERIFY_WRITE, (void __user *)req.virt_sb_base,
