112cc4cbe31cd0663bd724085085ad248986e720
Pradeep P V K <ppvk@codeaurora.org>
2020-07-09 19:35:17 +0530

    mtd: msm_qpic_nand: Skip erased page check upon error
    Skip erased page check upon error.
    
    Change-Id: I08ea01fdccd6896f83a42758652e19949e3d1b0e
    Signed-off-by: Pradeep P V K <ppvk@codeaurora.org>

--- drivers/mtd/devices/msm_qpic_nand.c
+++ drivers/mtd/devices/msm_qpic_nand.c
@@ -1984,7 +1984,7 @@ free_dma:
 			total_ecc_byte_cnt, DMA_FROM_DEVICE);
 	/* check for bit flips in ecc data */
 	ecc_temp = ecc;
-	for (n = rw_params->start_sector; n < cwperpage; n++) {
+	for (n = rw_params->start_sector; !err && n < cwperpage; n++) {
 		int last_pos = 0, next_pos = 0;
 		int ecc_bytes_percw_in_bits = (chip->ecc_parity_bytes * 8);
 
@@ -2651,7 +2651,7 @@ free_dma:
 			total_ecc_byte_cnt, DMA_FROM_DEVICE);
 	/* check for bit flips in ecc data */
 	ecc_temp = ecc;
-	for (n = rw_params->start_sector; n < cwperpage; n++) {
+	for (n = rw_params->start_sector; !err && n < cwperpage; n++) {
 		int last_pos = 0, next_pos = 0;
 		int ecc_bytes_percw_in_bits = (chip->ecc_parity_bytes * 8);
 
