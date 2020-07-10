f563070c3ad169c28b938260e9dd43959f920edb
Josh Hsiao <josh.hsiao@mediatek.com>
2020-07-10 21:13:20 +0800

    apusys: add power drivers
    1. apu_top module for aputop mtcmos control
    2. power2.0 build in apusys module
    
    (cherry-picked from commit 64b6d437670749c12ea27e3a399843957ddf6139)
    Signed-off-by: Josh Hsiao <josh.hsiao@mediatek.com>
    Change-Id: I6a442ea2107e72f428954ee9e50c8d9b5e6bac84
    CR-Id: ALPS05298037
    Feature: [Module] APUSYS
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

---
+++ drivers/misc/mediatek/apusys/power/2.0/mt6873/apu_power_table.c
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#include <linux/module.h>
+#include "apu_power_table.h"
+
+// FIXME: update vpu power table in DVT stage
+/* opp, mW */
+struct apu_opp_info vpu_power_table[APU_OPP_NUM] = {
+	{APU_OPP_0, 226},
+	{APU_OPP_1, 226},
+	{APU_OPP_2, 176},
+	{APU_OPP_3, 133},
+	{APU_OPP_4, 98},
+	{APU_OPP_5, 44},
+};
+EXPORT_SYMBOL(vpu_power_table);
+
+// FIXME: update mdla power table in DVT stage
+/* opp, mW */
+struct apu_opp_info mdla_power_table[APU_OPP_NUM] = {
+	{APU_OPP_0, 161},
+	{APU_OPP_1, 161},
+	{APU_OPP_2, 161},
+	{APU_OPP_3, 120},
+	{APU_OPP_4, 86},
+	{APU_OPP_5, 44},
+};
+EXPORT_SYMBOL(mdla_power_table);
