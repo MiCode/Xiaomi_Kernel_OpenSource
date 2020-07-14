882b7ca14175eb4436d0ca33798e1cb9170cc723
Greg Kroah-Hartman <gregkh@google.com>
2020-07-14 19:11:20 +0200

    ANDROID: GKI: do not export symbol_get/put()
    People keep trying to use these in Android kernels, and they should not
    be used, use the correct, real, symbol name instead.
    
    Turn off a number of things in the allmodconfig builds that were using
    these symbols to not break the build.  That was for build-testing only,
    no functional changes.
    
    Bug: 157965270
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I13bf351b3ae59fa6dfed7bd41dfff8c8d521e3a4

--- kernel/module.c
+++ kernel/module.c
@@ -1079,7 +1079,6 @@ void __symbol_put(const char *symbol)
 	module_put(owner);
 	preempt_enable();
 }
-EXPORT_SYMBOL(__symbol_put);
 
 /* Note this assumes addr is a function, which it currently always is. */
 void symbol_put_addr(void *addr)
@@ -2259,7 +2258,6 @@ void *__symbol_get(const char *symbol)
 
 	return sym ? (void *)kernel_symbol_value(sym) : NULL;
 }
-EXPORT_SYMBOL_GPL(__symbol_get);
 
 /*
  * Ensure that an exported symbol [global namespace] does not already exist
