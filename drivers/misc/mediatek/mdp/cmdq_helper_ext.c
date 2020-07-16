d08ce2d9c50edc56fac42a0e4be4531b2836155b
Damon Chu <damon.chu@mediatek.com>
2020-07-16 23:51:31 +0800

    [ALPS05107221] cmdq: suspend/resume only in normal world
    suspend/resume only in normal world
    
    MTK-Commit-Id: be381d229bcf6aee52410ef573bf47a260758f6c
    
    Change-Id: I220e05b99b09523230e0ca60a9e116caedaf99e7
    Feature:[Module]CMDQ Driver
    CR-Id:ALPS05107221
    Signed-off-by: Damon Chu <damon.chu@mediatek.com>

--- drivers/misc/mediatek/mdp/cmdq_helper_ext.c
+++ drivers/misc/mediatek/mdp/cmdq_helper_ext.c
@@ -3376,7 +3376,9 @@ static void cmdq_core_clk_enable(struct cmdqRecStruct *handle)
 
 	if (clock_count == 1) {
 		cmdq_core_reset_gce();
-		cmdq_mbox_enable(((struct cmdq_client *)handle->pkt->cl)->chan);
+		if (!handle->secData.is_secure)
+			cmdq_mbox_enable(((struct cmdq_client *)
+				handle->pkt->cl)->chan);
 	}
 
 	cmdq_core_group_clk_cb(true, handle->engineFlag, handle->engine_clk);
@@ -3393,8 +3395,9 @@ static void cmdq_core_clk_disable(struct cmdqRecStruct *handle)
 	CMDQ_MSG("[CLOCK]disable usage:%d\n", clock_count);
 
 	if (clock_count == 0) {
-		cmdq_mbox_disable(((struct cmdq_client *)
-			handle->pkt->cl)->chan);
+		if (!handle->secData.is_secure)
+			cmdq_mbox_disable(((struct cmdq_client *)
+				handle->pkt->cl)->chan);
 		/* Backup event */
 		cmdq_get_func()->eventBackup();
 		/* clock-off */
