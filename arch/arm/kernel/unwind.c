0c9c9f51de8429d0230679d640b695896357ccdd
Miles Chen <miles.chen@mediatek.com>
2020-07-16 23:56:58 +0800

    [ALPS05152194] Revert arm: temporary remove unwind fail message
    This reverts commit bef2f1ae76df3788f9cc52fef3147fee4ec5f90b.
    
    MTK-Commit-Id: a0406688ac10f6ebd007f43ab3f22dbed399b5c6
    
    Change-Id: Idfcf8b2d4a4eabd657f42abdd5fe7e4810db08fc
    Feature: [Module]Kernel Maintenance
    CR-Id: ALPS05152194
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

--- arch/arm/kernel/unwind.c
+++ arch/arm/kernel/unwind.c
@@ -403,8 +403,7 @@ int unwind_frame(struct stackframe *frame)
 
 	idx = unwind_find_idx(frame->pc);
 	if (!idx) {
-		/* tmp: remove this message */
-		/* pr_warn("unwind: Index not found %08lx\n", frame->pc); */
+		pr_warn("unwind: Index not found %08lx\n", frame->pc);
 		return -URC_FAILURE;
 	}
 
