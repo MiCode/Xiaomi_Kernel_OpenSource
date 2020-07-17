c8ede1ab813856a1b36e357811debef576e6d245
Rohith Kollalsi <rkollals@codeaurora.org>
2020-07-17 15:00:44 +0530

    usb: gadget: Revert increase write buffer size
    Commit 3fb319966d33 ("usb: gadget: increase write buffer size") brings
    additional memory requirement and leads to memory allocation failure
    when trying to allocate buffer to the request in usb_cser_alloc_req.
    Hence revert it to fix this.
    
    Change-Id: Iff5d6a2502d7ff094422fc66c20dff13d5b8d4ca
    Signed-off-by: Rohith Kollalsi <rkollals@codeaurora.org>

--- drivers/usb/gadget/function/f_cdev.c
+++ drivers/usb/gadget/function/f_cdev.c
@@ -57,11 +57,8 @@
 #define BRIDGE_RX_QUEUE_SIZE	8
 #define BRIDGE_RX_BUF_SIZE	2048
 #define BRIDGE_TX_QUEUE_SIZE	8
-#if IS_ENABLED(CONFIG_ARCH_SM8150)
-#define BRIDGE_TX_BUF_SIZE  (50 * 1024)
-#else
-#define BRIDGE_TX_BUF_SIZE 2048
-#endif
+#define BRIDGE_TX_BUF_SIZE	2048
+
 #define GS_LOG2_NOTIFY_INTERVAL		5  /* 1 << 5 == 32 msec */
 #define GS_NOTIFY_MAXPACKET		10 /* notification + 2 bytes */
 
