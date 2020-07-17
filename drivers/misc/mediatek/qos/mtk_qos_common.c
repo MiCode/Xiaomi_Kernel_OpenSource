9681a581d2402a689d6199ba3c78f7c4da98ee5f
Howard Hsu <howard.hsu@mediatek.com>
2020-07-17 19:55:07 +0800

    qos: qos driver porting for mt6873
    qos driver porting for mt6873
    
    (cherry-picked from commit 6fdc844e78bdeb5529e3c188b79aaa37680cc6f5)
    Signed-off-by: Howard Hsu <howard.hsu@mediatek.com>
    Change-Id: I6285fe90ceb4a27bb716bc1be8c84d7bffb0df32
    CR-Id: ALPS05298037
    Feature: VCORE DVFS
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

---
+++ drivers/misc/mediatek/qos/mtk_qos_common.c
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#include <linux/device.h>
+#include <linux/err.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
+#include <linux/of_device.h>
+#include <linux/kthread.h>
+#include <linux/io.h>
+
+#include "mtk_qos_ipi.h"
+#include "mtk_qos_sram.h"
+#include "mtk_qos_bound.h"
+#include "mtk_qos_sysfs.h"
+#include "mtk_qos_common.h"
+
+struct mtk_qos *m_qos;
+static void __iomem *qos_sram_base;
+static unsigned int qos_sram_bound;
+
+int qos_get_ipi_cmd(int idx)
+{
+	int cmd;
+
+	if (!m_qos)
+		return -EACCES;
+	if (m_qos->soc->ipi_pin[idx].valid == false)
+		return -EPERM;
+
+	cmd = m_qos->soc->ipi_pin[idx].id;
+
+	return cmd;
+}
+EXPORT_SYMBOL_GPL(qos_get_ipi_cmd);
+
+u32 qos_sram_read(u32 id)
+{
+	u32 offset;
+
+	if (!m_qos)
+		return 0;
+	if (m_qos->soc->sram_pin[id].valid == false)
+		return 0;
+	if (id >= QOS_SRAM_ID_MAX)
+		return 0;
+
+	offset = m_qos->soc->sram_pin[id].offset;
+	if (!qos_sram_base || offset >= qos_sram_bound)
+		return 0;
+
+	return readl(qos_sram_base + offset);
+}
+EXPORT_SYMBOL_GPL(qos_sram_read);
+
+void qos_sram_write(u32 id, u32 val)
+{
+	u32 offset;
+
+	if (!m_qos)
+		return;
+	if (m_qos->soc->sram_pin[id].valid == false)
+		return;
+	if (id >= QOS_SRAM_ID_MAX)
+		return;
+
+	offset = m_qos->soc->sram_pin[id].offset;
+	if (!qos_sram_base || offset >= qos_sram_bound)
+		return;
+
+	writel(val, qos_sram_base + offset);
+}
+EXPORT_SYMBOL_GPL(qos_sram_write);
+
+void qos_sram_init(void __iomem *regs, unsigned int bound)
+{
+	int i;
+
+	qos_sram_base = regs;
+	qos_sram_bound = bound;
+	pr_info("qos_sram addr:0x%x len:%d\n",
+		qos_sram_base, qos_sram_bound);
+
+	for (i = 0; i < bound; i += 4)
+		writel(0x0, qos_sram_base+i);
+}
+
+
+int mtk_qos_probe(struct platform_device *pdev,
+			const struct mtk_qos_soc *soc)
+{
+	struct resource *res;
+	struct mtk_qos *qos;
+
+	qos = devm_kzalloc(&pdev->dev, sizeof(*qos), GFP_KERNEL);
+	if (!qos)
+		return -ENOMEM;
+
+	qos->soc = of_device_get_match_data(&pdev->dev);
+	if (!qos->soc)
+		return -EINVAL;
+
+
+	qos->soc = soc;
+	qos->dev = &pdev->dev;
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	qos->regs = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(qos->regs))
+		return PTR_ERR(qos->regs);
+	qos->regsize = (unsigned int) resource_size(res);
+	m_qos = qos;
+	qos_sram_init(qos->regs, qos->regsize);
+	qos_add_interface(&pdev->dev);
+	qos_ipi_init(qos);
+
+	if (qos->soc->ipi_pin[QOS_IPI_QOS_BOUND].valid == true)
+		qos_bound_init();
+
+	qos_ipi_recv_init(qos);
+	platform_set_drvdata(pdev, qos);
+
+
+	pr_info("mtkqos:%s done\n", __func__);
+
+	return 0;
+}
+
+MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("MediaTek QoS driver");
---
+++ drivers/misc/mediatek/qos/mtk_qos_common.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#ifndef __MTK_QOS_COMMON_H__
+#define __MTK_QOS_COMMON_H__
+
+
+struct mtk_qos;
+
+struct mtk_qos_soc {
+	const struct qos_ipi_cmd *ipi_pin;
+	const struct qos_sram_addr *sram_pin;
+};
+
+struct mtk_qos {
+	struct device *dev;
+	const struct mtk_qos_soc *soc;
+	int dram_type;
+	void __iomem *regs;
+	unsigned int regsize;
+};
+
+extern int mtk_qos_probe(struct platform_device *pdev,
+			const struct mtk_qos_soc *soc);
+extern void qos_ipi_init(struct mtk_qos *qos);
+extern void qos_ipi_recv_init(struct mtk_qos *qos);
+extern int qos_get_ipi_cmd(int idx);
+#endif
+
