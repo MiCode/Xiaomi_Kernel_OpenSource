644613be8243812fc17ed7509b513e1153fb4bf8
Patrick Daly <pdaly@codeaurora.org>
2020-07-21 16:32:22 -0700

    qcom_iommu_util: Add iommu utility functions
    Qcom iommu drivers support a standard set of additional functionality
    which lives outside of the iommu framework.
    
    This patch only adds support for parsing the qcom,iommu-dma-addr-pool
    dt property. Subsequent patches will add more features.
    
    Change-Id: Idd8dc3d8c28cf9de0905809bfe7b205bbcb51aea
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

---
+++ include/linux/qcom-iommu-util.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef __QCOM_IOMMU_UTIL_H
+#define __QCOM_IOMMU_UTIL_H
+
+#include <linux/iommu.h>
+
+struct qcom_iommu_ops {
+	struct iommu_ops iommu_ops;
+};
+#define to_qcom_iommu_ops(x) (container_of(x, struct qcom_iommu_ops, iommu_ops))
+
+struct device_node *qcom_iommu_group_parse_phandle(struct device *dev);
+int qcom_iommu_generate_dma_regions(struct device *dev,
+				    struct list_head *head);
+void qcom_iommu_generate_resv_regions(struct device *dev,
+				      struct list_head *list);
+
+#endif /* __QCOM_IOMMU_UTIL_H */
