bec9314fb33012c8908bb209e8d0c694f46d665e
Amit Pundir <amit.pundir@linaro.org>
2020-07-22 15:50:40 +0530

    ANDROID: soc: qcom: cmd-db: Allow COMMAND_DB driver to be loaded as a module
    This patch allow the Qcom cmd-db driver to be loaded as a
    permanent module. It can be loaded as a module, but can't
    be unloaded. That should be OK because few core Qcom drivers,
    mainly RPMH, which depend on cmd-db are configured as
    permanent modules as well.
    
    This version is only for android11-5.4. For android-mainline
    and android12-5.4, the fix will come from upstream.
    
    Bug: 161128971
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Change-Id: I5bec1700b3202024c7f16bff9e823d658a371ac5

--- drivers/soc/qcom/cmd-db.c
+++ drivers/soc/qcom/cmd-db.c
@@ -2,6 +2,7 @@
 /* Copyright (c) 2016-2018, The Linux Foundation. All rights reserved. */
 
 #include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
 #include <linux/of_platform.h>
@@ -266,6 +267,7 @@ static const struct of_device_id cmd_db_match_table[] = {
 	{ .compatible = "qcom,cmd-db" },
 	{ },
 };
+MODULE_DEVICE_TABLE(of, cmd_db_match_table);
 
 static struct platform_driver cmd_db_dev_driver = {
 	.probe  = cmd_db_dev_probe,
@@ -280,3 +282,6 @@ static int __init cmd_db_device_init(void)
 	return platform_driver_register(&cmd_db_dev_driver);
 }
 arch_initcall(cmd_db_device_init);
+
+MODULE_DESCRIPTION("Qualcomm Technologies, Inc. Command DB for QCOM SoCs");
+MODULE_LICENSE("GPL v2");
