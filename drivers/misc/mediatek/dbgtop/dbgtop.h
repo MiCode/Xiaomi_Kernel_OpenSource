5b2317d94c58b57e9a147ce7a71c9bfd6a376a46
cc.hwang <cc.hwang@mediatek.com>
2020-07-24 12:18:19 +0800

    dbgtop: add dbgtop-drm for DDR_RESERVED_MODE
    DDR_RESERVED_MODE is a debugging function to retent DRAM content
    after reset. Before Dimensity 800, control of its enable is
    implemented in the RGU module. From Dimensity 800, control is
    moved to a new hardware module: DRM.
    
    Add the dbgtop-drm driver to provide an export function to
    enable DDR_RESERVED_MODE.
    
    (cherry-picked from commit 22a727e7c26733d87cac8d47b900ef9779e32e00)
    Signed-off-by: cc.hwang <cc.hwang@mediatek.com>
    Change-Id: I25cb871ef67af437e1f5e132561d00564d31b17d
    CR-Id: ALPS05298037
    Feature: Memory RAM Dump (MRDUMP)
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

---
+++ drivers/misc/mediatek/dbgtop/dbgtop.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#ifndef __DBGTOP_H__
+#define __DBGTOP_H__
+
+extern int mtk_dbgtop_dram_reserved(int enable);
+
+#endif
