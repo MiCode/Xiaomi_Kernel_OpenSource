d942cd8db6e67de4f8caa07a17a972e2de1edf48
Owen Chen <owen.chen@mediatek.com>
2020-07-29 16:06:59 +0800

    clkbuf: mediatek: add clkbuf driver
    add clkbuf driver with new constructure.
    
    (cherry-picked from commit 6cda4f871949dfaa508d2ba43c1d7edcbb77c486)
    Signed-off-by: Owen Chen <owen.chen@mediatek.com>
    Change-Id: I13d6893d9a06a36ad4d2447084118843e424181b
    CR-Id: ALPS05298037
    Feature: Power Management
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    Source_Git_Sha: a457f050ad57004104a356fd04f2aa57fdead12a

---
+++ drivers/misc/mediatek/clkbuf/src/mtk_clkbuf_ctl.h
@@ -0,0 +1,65 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+/**
+ * @file    mtk_clk_buf_ctl.h
+ * @brief   Driver for clock buffer control
+ *
+ */
+#ifndef __MTK_CLK_BUF_CTL_H__
+#define __MTK_CLK_BUF_CTL_H__
+
+enum CLK_BUF_TYPE {
+	CLK_BUF_SW_DISABLE = 0,
+	CLK_BUF_SW_ENABLE  = 1,
+};
+
+enum clk_buf_ret_type {
+	CLK_BUF_DISABLE = 0,
+	CLK_BUF_ENABLE  = 1,
+	CLK_BUF_OK = 2,
+	CLK_BUF_NOT_READY = -1,
+	CLK_BUF_NOT_SUPPORT = -2,
+	CLK_BUF_FAIL = -3,
+};
+
+/* clk_buf_id: users of clock buffer */
+enum clk_buf_id {
+	CLK_BUF_BB_MD = 0,
+	CLK_BUF_CONN,
+	CLK_BUF_NFC,
+	CLK_BUF_RF,
+	CLK_BUF_UFS = 6,	/* usually set as XO_7(idx = 6) */
+	CLK_BUF_INVALID
+};
+
+/* xo_id: clock buffer list */
+enum xo_id {
+	XO_SOC = 0,
+	XO_WCN,
+	XO_NFC,
+	XO_CEL,
+	XO_AUD,		/* Disabled */
+	XO_PD,		/* Disabled */
+	XO_EXT,		/* UFS */
+	XO_NUMBER
+};
+
+enum {
+	BBLPM_SKIP = (1 << 0),
+	BBLPM_WCN = (1 << XO_WCN),
+	BBLPM_NFC = (1 << XO_NFC),
+	BBLPM_CEL = (1 << XO_CEL),
+	BBLPM_EXT = (1 << XO_EXT),
+};
+
+extern int clk_buf_hw_ctrl(u32 id, bool onoff);
+extern int clk_buf_set_by_flightmode(bool on);
+extern int clk_buf_control_bblpm(bool on);
+extern int clk_buf_dump_log(void);
+extern int clk_buf_get_xo_en_sta(u32 id);
+extern int clk_buf_check_bblpm_enter_cond(void);
+#endif
+
