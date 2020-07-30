68d5cd1dcb65d2fc15b87975278122827ed9cdf5
Christoph Hellwig <hch@lst.de>
2020-07-30 08:10:23 +0200

    UPSTREAM: modules: unexport __module_text_address
    __module_text_address is only used by built-in code.
    
    Bug: 157965270
    Bug: 171277690
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jessica Yu <jeyu@kernel.org>
    (cherry picked from commit 3fe1e56d0e68b623dd62d8d38265d2a052e7e185)
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I1a9081c6fa3d2a6e27ede24a4fa2b6a220cd521d
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- kernel/module.c
+++ kernel/module.c
@@ -4561,7 +4561,6 @@ struct module *__module_text_address(unsigned long addr)
 	}
 	return mod;
 }
-EXPORT_SYMBOL_GPL(__module_text_address);
 
 /* Don't grab lock, we're oopsing. */
 void print_modules(void)
