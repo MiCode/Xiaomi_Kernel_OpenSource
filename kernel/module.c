ad6d414703d7ee4fa33ebc1f0578aeff0e97bd7d
Christoph Hellwig <hch@lst.de>
2020-07-30 08:10:23 +0200

    modules: unexport __module_text_address
    commit 3fe1e56d0e68b623dd62d8d38265d2a052e7e185 upstream.
    
    __module_text_address is only used by built-in code.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jessica Yu <jeyu@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/module.c
+++ kernel/module.c
@@ -4655,7 +4655,6 @@ struct module *__module_text_address(unsigned long addr)
 	}
 	return mod;
 }
-EXPORT_SYMBOL_GPL(__module_text_address);
 
 /* Don't grab lock, we're oopsing. */
 void print_modules(void)
