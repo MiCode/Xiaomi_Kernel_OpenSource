217fd1e02e20f356bd41f07d9f55eef472cae6f9
Rocco Yue <rocco.yue@mediatek.com>
2020-08-01 00:40:19 +0800

    [ALPS05238440] rtnl: enhance rtnl_lock debug info
    show task_struct stack trace;
    Print the start and end times of the rtnl_lock held by
    the PID when this lock is released and held for longer
    than 4 seconds.
    
    MTK-Commit-Id: 0cd21419b30700c4568f33a022394855565e8c0a
    
    Change-Id: If11702d6c80239ee36f86d4f9e1b915a6a8460ea
    Signed-off-by: Rocco Yue <rocco.yue@mediatek.com>
    CR-Id: ALPS05238440
    Feature: IPv6 over MD

--- net/core/rtnetlink.c
+++ net/core/rtnetlink.c
@@ -61,13 +61,15 @@
 
 /* #ifdef CONFIG_MTK_NET_LOGGING */
 #include <linux/stacktrace.h>
+#include <linux/sched/debug.h>
 #define RTNL_DEBUG_ADDRS_COUNT 10
 #define RTNL_LOCK_MAX_HOLD_TIME 3
 
 struct rtnl_debug_btrace_t {
-	char *process_name;
+	struct task_struct *task;
 	int    pid;
-	unsigned long long when;
+	unsigned long long start;
+	unsigned long long end;
 	unsigned long addrs[RTNL_DEBUG_ADDRS_COUNT];
 	unsigned int entry_nr;
 	char flag;/*1 get rtnl_lock,0 : relase rtnl_lock*/
@@ -76,9 +78,10 @@ struct rtnl_debug_btrace_t {
 };
 
 static struct rtnl_debug_btrace_t rtnl_instance = {
-	.process_name = NULL,
+	.task = NULL,
 	.pid = 0,
-	.when = 0,
+	.start = 0,
+	.end = 0,
 	.entry_nr = 0,
 	.flag = 0,
 	.rtnl_lock_owner = NULL,
@@ -87,7 +90,7 @@ static struct rtnl_debug_btrace_t rtnl_instance = {
 static void rtnl_print_btrace(unsigned long data);
 static DEFINE_TIMER(rtnl_chk_timer, rtnl_print_btrace, 0, 0);
 
-void rtnl_get_btrace(void *who)
+void rtnl_get_btrace(struct task_struct *who)
 {
 	struct stack_trace debug_trace;
 
@@ -96,8 +99,9 @@ void rtnl_get_btrace(void *who)
 	debug_trace.entries = rtnl_instance.addrs;
 	debug_trace.skip = 0;
 	save_stack_trace(&debug_trace);
-	rtnl_instance.process_name = who;
-	rtnl_instance.when = sched_clock();
+	rtnl_instance.task = who;
+	rtnl_instance.start = sched_clock();
+	rtnl_instance.end = 0;
 	rtnl_instance.flag = 1;
 	rtnl_instance.pid = current->pid;
 	rtnl_instance.rtnl_lock_owner  = current;
@@ -114,10 +118,14 @@ void rtnl_print_btrace(unsigned long data)
 		show_trace.entries = rtnl_instance.addrs;
 		show_trace.max_entries = RTNL_DEBUG_ADDRS_COUNT;
 		pr_info("-----------%s start-----------\n", __func__);
-		pr_info("[mtk_net][rtnl_lock] %s[%d] hold lock more than 2 sec,start time: %lld\n",
-			rtnl_instance.process_name,
-			rtnl_instance.pid, rtnl_instance.when);
+		pr_info("[mtk_net][rtnl_lock] %s[%d][%c] hold lock more than 2 sec,start time: %lld\n",
+			rtnl_instance.task->comm,
+			rtnl_instance.pid,
+			task_state_to_char(rtnl_instance.task),
+			rtnl_instance.start);
+
 		print_stack_trace(&show_trace, 0);
+		show_stack(rtnl_instance.task, NULL);
 		pr_info("------------%s end-----------\n", __func__);
 	} else {
 		pr_info("[mtk_net][rtnl_lock]There is no process hold rtnl lock\n");
@@ -143,7 +151,7 @@ void rtnl_lock(void)
 {
 	mutex_lock(&rtnl_mutex);
 /* #ifdef CONFIG_MTK_NET_LOGGING */
-	rtnl_get_btrace(current->comm);
+	rtnl_get_btrace(current);
 /* #endif */
 }
 EXPORT_SYMBOL(rtnl_lock);
@@ -163,6 +171,12 @@ void __rtnl_unlock(void)
 	struct sk_buff *head = defer_kfree_skb_list;
 
 	defer_kfree_skb_list = NULL;
+
+	rtnl_instance.end = sched_clock();
+	if (rtnl_instance.end - rtnl_instance.start > 4000000000ULL)//4 second
+		pr_info("[mtk_net][rtnl_unlock] rtnl_lock is held by [%d] from [%llu] to [%llu]\n",
+			rtnl_instance.pid,
+			rtnl_instance.start, rtnl_instance.end);
 
 	mutex_unlock(&rtnl_mutex);
 
