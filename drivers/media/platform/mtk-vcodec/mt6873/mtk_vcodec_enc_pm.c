5544f1c90c9ac226b0cf6b4fb441a0c4bbb163cc
Chia-Mao Hung <chia-mao.hung@mediatek.com>
2020-08-01 00:48:37 +0800

    [ALPS05219372] v4l2: fixed coverity
    1. Address that could be at negative offset of an array
    2. Deferencing job, which is known to be NULL
    
    MTK-Commit-Id: 41fc785e88f0c895c099edcd9e512b5a5192bae7
    
    Change-Id: I29ac9a39458a54d5b822bd888571fd3883f35c84
    Signed-off-by: Chia-Mao Hung <chia-mao.hung@mediatek.com>
    CR-Id: ALPS05219372
    Feature: [Module]Video Driver

--- drivers/media/platform/mtk-vcodec/mt6873/mtk_vcodec_enc_pm.c
+++ drivers/media/platform/mtk-vcodec/mt6873/mtk_vcodec_enc_pm.c
@@ -244,8 +244,11 @@ void mtk_vcodec_enc_clock_on(struct mtk_vcodec_ctx *ctx, int core_id)
 		ret = clk_prepare_enable(pm->clk_MT_CG_VENC0);
 		if (ret)
 			mtk_v4l2_err("clk_prepare_enable CG_VENC fail %d", ret);
-	} else
+	} else {
 		mtk_v4l2_err("invalid core_id %d", core_id);
+		time_check_end(MTK_FMT_ENC, core_id, 50);
+		return;
+	}
 	time_check_end(MTK_FMT_ENC, core_id, 50);
 #endif
 
@@ -634,7 +637,8 @@ void mtk_venc_pmqos_gce_flush(struct mtk_vcodec_ctx *ctx, int core_id,
 		frame_rate = ctx->enc_params.framerate_num /
 				ctx->enc_params.framerate_denom;
 	}
-	job->operation_rate = frame_rate;
+	if (job != NULL)
+		job->operation_rate = frame_rate;
 
 	if (job_cnt == 0) {
 		// Adjust dvfs immediately
