22854c1e1e641250698482635ea1a21a99ce45bf
Fp-bs.Wu@mediatek.com <Fp-bs.Wu@mediatek.com>
2020-08-01 00:46:50 +0800

    [ALPS05250924] CCU: Fix ccu open/close unpaired
    1. ccu_clock_enable/disable unpaired.
    
    MTK-Commit-Id: 71cb0b3bc4c65361b67b5452fc96d497f8a21427
    
    Change-Id: Iba7f27ad60d3bcf77618164ff4e3d3bb2c9e0f34
    CR-Id: ALPS05250924
    Feature: [Android Default] AE (Auto Exposure)
    Signed-off-by: Fp-bs.Wu@mediatek.com <Fp-bs.Wu@mediatek.com>

--- drivers/misc/mediatek/ccu/src/1.2/ccu_drv.c
+++ drivers/misc/mediatek/ccu/src/1.2/ccu_drv.c
@@ -75,7 +75,8 @@
 
 #define CCU_DEV_NAME            "ccu"
 
-struct clk *ccu_clock_ctrl;
+#define CCU_CLK_PWR_NUM 2
+struct clk *ccu_clk_pwr_ctrl[CCU_CLK_PWR_NUM];
 
 struct ccu_device_s *g_ccu_device;
 static struct ccu_power_s power;
@@ -122,6 +123,7 @@ static int ccu_suspend(struct platform_device *dev, pm_message_t mesg);
 
 static int ccu_resume(struct platform_device *dev);
 
+static int32_t _clk_count;
 /*---------------------------------------------------------------------------*/
 /* CCU Driver: pm operations                                                 */
 /*---------------------------------------------------------------------------*/
@@ -418,6 +420,9 @@ static int ccu_open(struct inode *inode, struct file *flip)
 	struct ccu_user_s *user;
 
 	LOG_INF_MUST("%s +", __func__);
+
+	_clk_count = 0;
+
 	user = NULL;
 	ccu_create_user(&user);
 	flip->private_data = user;
@@ -559,19 +564,33 @@ int ccu_clock_enable(void)
 {
 	int ret;
 
-	LOG_DBG_MUST("%s.\n", __func__);
+	LOG_DBG_MUST("%s %d.\n", __func__, _clk_count);
 
-	ret = clk_prepare_enable(ccu_clock_ctrl);
+	mutex_lock(&g_ccu_device->clk_mutex);
+	_clk_count++;
+
+	ret = clk_prepare_enable(ccu_clk_pwr_ctrl[0]);
 	if (ret)
-		LOG_ERR("clock enable fail.\n");
+		LOG_ERR("CAM_PWR enable fail.\n");
+	ret = clk_prepare_enable(ccu_clk_pwr_ctrl[1]);
+	if (ret)
+		LOG_ERR("CCU_CLK_CAM_CCU enable fail.\n");
+	mutex_unlock(&g_ccu_device->clk_mutex);
 
 	return ret;
 }
 
 void ccu_clock_disable(void)
 {
-	LOG_DBG_MUST("%s.\n", __func__);
-	clk_disable_unprepare(ccu_clock_ctrl);
+	LOG_DBG_MUST("%s %d.\n", __func__, _clk_count);
+
+	mutex_lock(&g_ccu_device->clk_mutex);
+	if (_clk_count > 0) {
+		clk_disable_unprepare(ccu_clk_pwr_ctrl[1]);
+		clk_disable_unprepare(ccu_clk_pwr_ctrl[0]);
+		_clk_count--;
+	}
+	mutex_unlock(&g_ccu_device->clk_mutex);
 }
 
 static MBOOL _is_fast_cmd(enum ccu_msg_id msg_id)
@@ -1130,11 +1149,16 @@ static int ccu_probe(struct platform_device *pdev)
 		}
 		/* get Clock control from device tree.  */
 		{
-			ccu_clock_ctrl =
+			ccu_clk_pwr_ctrl[0] =
+				devm_clk_get(g_ccu_device->dev,
+					"CAM_PWR");
+			if (ccu_clk_pwr_ctrl[0] == NULL)
+				LOG_ERR("Get CAM_PWR fail.\n");
+			ccu_clk_pwr_ctrl[1] =
 				devm_clk_get(g_ccu_device->dev,
 					"CCU_CLK_CAM_CCU");
-			if (ccu_clock_ctrl == NULL)
-				LOG_ERR("Get ccu clock ctrl fail.\n");
+			if (ccu_clk_pwr_ctrl[1] == NULL)
+				LOG_ERR("Get CCU_CLK_CAM_CCU fail.\n");
 		}
 		/**/
 		g_ccu_device->irq_num = irq_of_parse_and_map(node, 0);
@@ -1291,6 +1315,7 @@ static int __init CCU_INIT(void)
 
 	INIT_LIST_HEAD(&g_ccu_device->user_list);
 	mutex_init(&g_ccu_device->user_mutex);
+	mutex_init(&g_ccu_device->clk_mutex);
 	init_waitqueue_head(&g_ccu_device->cmd_wait);
 
 	/* Register M4U callback */
