25ff0ae41cb43f656059ce76dd704033cc797476
Lingutla Chandrasekhar <clingutla@codeaurora.org>
2020-08-06 11:58:13 +0530

    sched: rt: Add trace point for taskplacement
    Add cpu util trace point in RT EAS task placement decisions to
    help debug RT taskplacement issues.
    
    Change-Id: Ib79484e1bef863a634507989fda577ca2f79020b
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -9,6 +9,8 @@
 
 #include <linux/interrupt.h>
 
+#include <trace/events/sched.h>
+
 #include "walt.h"
 
 int sched_rr_timeslice = RR_TIMESLICE;
@@ -1795,6 +1797,9 @@ retry:
 		}
 
 		for_each_cpu_and(cpu, lowest_mask, sched_group_span(sg)) {
+
+			trace_sched_cpu_util(cpu);
+
 			if (cpu_isolated(cpu))
 				continue;
 
