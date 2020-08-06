20c012c9416fa9d265bc25fa0decaad7ca88d386
Mahesh Kumar Kalikot Veetil <mkalikot@codeaurora.org>
2020-08-05 22:30:42 -0700

    cnss2: Force assert if MHI resume fails
    Add a new API to post force assert task to work context. Call this API
    if MHI resume fails to collect dump. Also add an explicit print on
    asynchronous vs synchronous call.
    
    Change-Id: I5077b57919e475bed15e3b6f251cc3c06cf7a619
    CRs-Fixed: 2741967
    Signed-off-by: Mahesh Kumar Kalikot Veetil <mkalikot@codeaurora.org>

--- drivers/net/wireless/cnss2/pci.c
+++ drivers/net/wireless/cnss2/pci.c
@@ -1099,6 +1099,10 @@ static int cnss_pci_set_mhi_state(struct cnss_pci_data *pci_priv,
 out:
 	cnss_pr_err("Failed to set MHI state: %s(%d)\n",
 		    cnss_mhi_state_to_str(mhi_state), mhi_state);
+
+	if (mhi_state == CNSS_MHI_RESUME)
+		cnss_force_fw_assert_async(&pci_priv->pci_dev->dev);
+
 	return ret;
 }
 
