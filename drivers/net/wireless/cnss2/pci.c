79b15c7db1e56f46c67e0bbe881ca9ab6d51f92a
Mahesh Kumar Kalikot Veetil <mkalikot@codeaurora.org>
2020-08-05 17:24:11 -0700

    cnss2: Synchronize bus suspend and resume
    Bus suspend and resume functions are not re-entrant. Protect
    them with mutex as multiple contexts can call these routines
    and cause race condition.
    
    Change-Id: Id6276b7fff21f595f403ebd9e15e9ef90bfdd024
    CRs-Fixed: 2749241
    Signed-off-by: Mahesh Kumar Kalikot Veetil <mkalikot@codeaurora.org>

--- drivers/net/wireless/cnss2/pci.c
+++ drivers/net/wireless/cnss2/pci.c
@@ -2478,7 +2478,9 @@ static int cnss_pci_suspend(struct device *dev)
 		goto clear_flag;
 
 	if (!pci_priv->disable_pc) {
+		mutex_lock(&pci_priv->bus_lock);
 		ret = cnss_pci_suspend_bus(pci_priv);
+		mutex_unlock(&pci_priv->bus_lock);
 		if (ret)
 			goto resume_driver;
 	}
@@ -2517,7 +2519,9 @@ static int cnss_pci_resume(struct device *dev)
 		goto out;
 
 	if (!pci_priv->disable_pc) {
+		mutex_lock(&pci_priv->bus_lock);
 		ret = cnss_pci_resume_bus(pci_priv);
+		mutex_unlock(&pci_priv->bus_lock);
 		if (ret)
 			goto out;
 	}
