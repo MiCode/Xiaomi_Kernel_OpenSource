05033c53bf930128336f36d3dd1261a0d7dbe01d
Freddy Hsin <freddy.hsin@mediatek.com>
2020-08-07 11:31:21 +0800

    soc: mediatek: add reboot-mode header
    Mediatek platform use a RGU (Reset Generation Unit) mapped
    register store the reboot mode magic value for bootloader
    to use when system reboot.
    
    Add the shared header describing the values firmware expects
    for different boot modes.
    
    (cherry-picked from commit 290d9987c279c0e0dc62d12d015b80d291310f8b)
    Signed-off-by: Freddy Hsin <freddy.hsin@mediatek.com>
    Change-Id: I0b6a56ed10e29dc7f544bd556bfa503b812b29c5
    CR-Id: ALPS05298037
    Feature: [Module]RGU (Reset Generator Unit)
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    Source_Git_Sha: 453ee7dcf7c0b5c8942e04d540bbc3ca77025fff

---
+++ include/dt-bindings/soc/mediatek,boot-mode.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+#ifndef __MEDIATEK_BOOT_MODE_H
+#define __MEDIATEK_BOOT_MODE_H
+
+#define BOOT_CHARGER	1
+#define BOOT_RECOVERY	2
+#define BOOT_BOOTLOADER	3
+#define BOOT_DM_VERITY	4
+#define BOOT_KPOC	5
+#define BOOT_DDR_RSVD	6
+#define BOOT_META	7
+#define BOOT_RPMBPK	8
+
+#endif
