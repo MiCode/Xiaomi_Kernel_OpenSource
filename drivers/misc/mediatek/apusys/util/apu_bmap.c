166bb10c6fb54b2cd5967420e5d9e236a9adedc8
Perry Hsu <perry.hsu@mediatek.com>
2020-08-07 14:32:40 +0800

    apusys: add generic bitmap mem allocator
    Added apu_bmap to apusys for implementing memory allocators
    such as VPU, SLB, ACP, and VLM.
    
    apu_bmap_init() - Initialize bitmap
      Inputs
        ab->start: start address.
        ab->end: end address.
        ab->au: allocation unit.
        ab->align_mask: alignment mask in terms of au,
          0 = no alignment.
      Outputs
        ab->size: equals to (ab->end - ab->start).
        ab->b: allocated bitmap.
        ab->nbits: number of bits in the bitmap(ab->b).
        ab->lock: spin lock used to protect the bitmap.
      Parameters
        [in,out] ab    The apu bitmap object
        [in]     name  The name of this bitmap object
      Returns
        0: Success.
        EINVAL: start, end, or size is not aligned to allocation
          unit.
    
    apu_bmap_exit() - Release bitmap
      Parameters
        [in]  ab  The apu bitmap object to be released
    
    apu_bmap_alloc() - Allocate addresses from bitmap
      Parameters
        [in]  ab    The apu bitmap object to be allocated from.
        [in]  size  Desired allocation size in bytes.
        [in]  given_addr  Allocate on the given address,
                          0 = dyanmic allocation.
      Returns
        0: Allocation failed.
        Others: Allocated address.
      Remarks
        Searches free addresses from begin(ab->start).
    
    apu_bmap_free() - Free occupied addresses from bitmap
      Parameters
        [in]  ab    The apu bitmap object.
        [in]  addr  Allocated start address returned by
                    apu_bmap_alloc().
        [in]  size  Allocated size.
    
    (cherry-picked from commit 39fadffd35ccf75256f77fd1530854b81bd258a1)
    Signed-off-by: Perry Hsu <perry.hsu@mediatek.com>
    Change-Id: I7fab64086e1c3aad9938d8848464fdc309cf1c4e
    CR-Id: ALPS05298037
    Feature: [Module] APUSYS
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

---
+++ drivers/misc/mediatek/apusys/util/apu_bmap.c
@@ -0,0 +1,172 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2020 MediaTek Inc.
+ */
+
+#include <linux/kernel.h>
+#include <linux/bitmap.h>
+#include <linux/slab.h>
+#include <linux/spinlock.h>
+
+#include "mdw_cmn.h"
+#include "apu_bmap.h"
+
+/**
+ * Initialize bitmap<br>
+ * <b>Inputs</b><br>
+ *   ab->start: start address.<br>
+ *   ab->end: end address.<br>
+ *   ab->au: allocation unit.<br>
+ *   ab->align_mask: alignment mask in terms of au, 0 = no alignment.<br>
+ * <b>Outputs</b><br>
+ *   ab->size: equals to (ab->end - ab->start).<br>
+ *   ab->b: allocated bitmap.<br>
+ *   ab->nbits: number of bits in the bitmap(ab->b).<br>
+ *   ab->lock: spin lock used to protect the bitmap.<br>
+ * @param[in,out] ab The apu bitmap object
+ * @param[in] name The name of this bitmap object
+ * @return 0: Success.<br>
+ *   EINVAL: start, end, or size is not aligned to allocation unit.<br>
+ */
+int apu_bmap_init(struct apu_bmap *ab, const char *name)
+{
+	if (!ab || ab->start > ab->end)
+		return -EINVAL;
+
+	memset(ab->name, 0, APU_BMAP_NAME_LEN);
+	strncpy(ab->name, name, APU_BMAP_NAME_LEN - 1);
+
+	/* size must be multiple of allocation unit */
+	ab->size = ab->end - ab->start;
+	mdw_mem_debug("%s: %s: start: 0x%x, end: 0x%x, size: 0x%x, au: 0x%x\n",
+		__func__, ab->name, ab->start, ab->end, ab->size, ab->au);
+	if (!is_au_align(ab, ab->size)) {
+		mdw_drv_warn("%s: %s: size 0x%x is un-aligned to AU 0x%x\n",
+			__func__, ab->name, ab->size, ab->au);
+		return -EINVAL;
+	}
+
+	ab->nbits = ab->size / ab->au;
+	ab->b = bitmap_zalloc(ab->nbits, GFP_KERNEL);
+	spin_lock_init(&ab->lock);
+
+	return 0;
+}
+
+/**
+ * Release bitmap
+ * @param[in] ab The apu bitmap object to be released
+ */
+void apu_bmap_exit(struct apu_bmap *ab)
+{
+	if (!ab || !ab->b)
+		return;
+
+	bitmap_free(ab->b);
+	ab->b = NULL;
+}
+
+/**
+ * Allocate addresses from bitmap
+ * @param[in] ab The apu bitmap object to be allocated from.
+ * @param[in] size Desired allocation size in bytes.
+ * @param[in] given_addr Allocate on the given address,
+ *            0 = dyanmic allocation.
+ * @return 0: Allocation failed.<br>
+ *    Others: Allocated address.<br>
+ * @remark: Searches free addresses from begin(ab->start).
+ */
+uint32_t apu_bmap_alloc(struct apu_bmap *ab, unsigned int size,
+	uint32_t given_addr)
+{
+	uint32_t addr = 0;
+	unsigned int nr;
+	unsigned long offset;
+	unsigned long start = 0;
+	unsigned long flags;
+
+	if (!ab)
+		return 0;
+
+	if (given_addr) {
+		start = given_addr - ab->start;
+		if (!is_au_align(ab, start)) {
+			mdw_drv_warn("%s: %s: size: 0x%x, given addr: 0x%x, start 0x%x is un-aligned to AU 0x%x\n",
+				__func__, ab->name, size, addr, start, ab->au);
+			return 0;
+		}
+		start = start / ab->au;
+	}
+
+	spin_lock_irqsave(&ab->lock, flags);
+	nr = round_up(size, ab->au) / ab->au;
+
+	offset = bitmap_find_next_zero_area(ab->b, ab->nbits, start,
+		nr, ab->align_mask);
+
+	if (offset >= ab->nbits) {
+		mdw_drv_warn("%s: %s: size: 0x%x, given addr: 0x%x, offset %d is out of limit %d\n",
+			__func__, ab->name, size, addr, offset, ab->nbits);
+		goto out;
+	}
+
+	addr = offset * ab->au + ab->start;
+	if (given_addr && (addr != given_addr)) {
+		/* the given address had been occupied */
+		mdw_drv_warn("%s: %s: size: 0x%x, allocated addr: 0x%x, expected addr: 0x%x\n",
+			__func__, ab->name, size, addr, given_addr);
+		goto out;
+	}
+	__bitmap_set(ab->b, offset, nr);
+
+out:
+	spin_unlock_irqrestore(&ab->lock, flags);
+
+	if (addr)
+		mdw_mem_debug("%s: %s: size: 0x%x, allocated addr: 0x%x (%s)\n",
+		__func__, ab->name, size, addr,
+		given_addr ? "static" : "dynamic");
+
+	return addr;
+}
+
+/**
+ * Free occupied addresses from bitmap
+ * @param[in] ab The apu bitmap object.
+ * @param[in] addr Allocated start address returned by apu_bmap_alloc().
+ * @param[in] size Allocated size.
+ */
+void apu_bmap_free(struct apu_bmap *ab, uint32_t addr, unsigned int size)
+{
+	uint32_t ret = 0;
+	unsigned int nr;
+	unsigned long offset;
+	unsigned long flags;
+
+	if (!ab || addr < ab->start || (addr + size) > ab->end)
+		return;
+
+	nr = round_up(size, ab->au) / ab->au;
+	offset = addr - ab->start;
+
+	mdw_mem_debug("%s: %s: addr: 0x%x, size: 0x%x, nr_bits: %d\n",
+		__func__, ab->name, addr, size, nr);
+
+	if (is_au_align(ab, offset)) {
+		mdw_drv_warn("%s: %s: addr 0x%x, offset 0x%x is un-aligned to AU 0x%x\n",
+			__func__, ab->name, addr, offset, ab->au);
+		return;
+	}
+
+	offset = offset / ab->au;
+	if (offset >= ab->nbits) {
+		mdw_drv_warn("%s: %s: addr 0x%x, offset-bit %d is out of limit %d\n",
+			__func__, ab->name, addr, offset, ab->nbits);
+		return;
+	}
+
+	spin_lock_irqsave(&ab->lock, flags);
+	__bitmap_clear(ab->b, offset, nr);
+	spin_unlock_irqrestore(&ab->lock, flags);
+}
+
