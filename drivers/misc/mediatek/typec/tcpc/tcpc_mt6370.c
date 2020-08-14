c038cf9c6fa36fed88930c857a2edad8857c4210
Jiaqin Wang <jiaqin.wang@mediatek.com>
2020-08-14 22:52:09 +0800

    [ALPS05126471] usb: check snprintf return
    check snprintf return value
    
    MTK-Commit-Id: c19e16d7766fc897f89b13d4aeb96dcc14b27a01
    
    Change-Id: I7600f67dc250491e7b6ff9bdfa7c6657d56ec005
    Signed-off-by: Jiaqin Wang <jiaqin.wang@mediatek.com>
    CR-Id: ALPS05126471
    Feature: USB TYPE-C

--- drivers/misc/mediatek/typec/tcpc/tcpc_mt6370.c
+++ drivers/misc/mediatek/typec/tcpc/tcpc_mt6370.c
@@ -577,7 +577,10 @@ static int mt6370_init_alert(struct tcpc_device *tcpc)
 	if (!name)
 		return -ENOMEM;
 
-	snprintf(name, PAGE_SIZE, "%s-IRQ", chip->tcpc_desc->name);
+	ret = snprintf(name, PAGE_SIZE, "%s-IRQ", chip->tcpc_desc->name);
+	if (ret < 0 || ret >= PAGE_SIZE)
+		pr_info("%s-%d, snprintf fail, ret=%d\n",
+			__func__, __LINE__, ret);
 
 	pr_info("%s name = %s, gpio = %d\n", __func__,
 				chip->tcpc_desc->name, chip->irq_gpio);
