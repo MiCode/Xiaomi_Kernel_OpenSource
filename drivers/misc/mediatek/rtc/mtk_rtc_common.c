2ce5803357fa3d30a859cb33e1f59018b68ea14d
andrew.yang <andrew.yang@mediatek.com>
2020-08-14 22:48:46 +0800

    [ALPS05283960] RTC: Fix CERT-C Error Handling
    Detect and handle standard library errors.
    
    MTK-Commit-Id: 6420738b2c156e1fee377e65b4831812ba5d2ef0
    
    Change-Id: If7c3dcfde62e9727e3ea3fa43ab21b38d78fad6b
    Signed-off-by: andrew.yang <andrew.yang@mediatek.com>
    CR-Id: ALPS05283960
    Feature: Power-Off Alarm

--- drivers/misc/mediatek/rtc/mtk_rtc_common.c
+++ drivers/misc/mediatek/rtc/mtk_rtc_common.c
@@ -584,9 +584,20 @@ static void rtc_handler(void)
 		now_time =
 		    mktime(nowtm.tm_year, nowtm.tm_mon, nowtm.tm_mday,
 			   nowtm.tm_hour, nowtm.tm_min, nowtm.tm_sec);
+
+		if (now_time == -1) {
+			spin_unlock_irqrestore(&rtc_lock, flags);
+			return;
+		}
+
 		time =
 		    mktime(tm.tm_year, tm.tm_mon, tm.tm_mday, tm.tm_hour,
 			   tm.tm_min, tm.tm_sec);
+
+		if (time == -1) {
+			spin_unlock_irqrestore(&rtc_lock, flags);
+			return;
+		}
 
 		/* power on */
 		if (now_time >= time - 1 && now_time <= time + 4) {
