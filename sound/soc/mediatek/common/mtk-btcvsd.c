245b8a8ac309c2e821fe3e5504f720091015d47a
tina.tsai <tina.tsai@mediatek.com>
2020-08-14 22:45:06 +0800

    [ALPS05258855] ASoC: mediatek: btcvsd: tx underflow
    [Detail]
    AP Audio notify BTFW by BT_CVSD_TX_UNDERFLOW (1<<23)
    value 0: BTSRAM TX Buffer is valid
    value 1: BTSRAM TX Buffer is not valid
             AP Audio dont write valid bt bitstream to BT SRAM
             AP Audio TX task is idle
    
    MTK-Commit-Id: 088c1ec58c665df6cce177ba779943b0ac495a1d
    
    Change-Id: I7cfbf6215d9c69e3a4d7d8818006f576baf9e7f7
    Signed-off-by: tina.tsai <tina.tsai@mediatek.com>
    CR-Id: ALPS05258855
    Feature: BT AOSP

--- sound/soc/mediatek/common/mtk-btcvsd.c
+++ sound/soc/mediatek/common/mtk-btcvsd.c
@@ -136,6 +136,7 @@ struct mtk_btcvsd_snd {
 	u8 tx_packet_buf[BTCVSD_TX_BUF_SIZE];
 	u8 rx_packet_buf[BTCVSD_RX_BUF_SIZE];
 	u8 disable_write_silence;
+	u8 write_tx:1;
 
 	enum BT_SCO_BAND band;
 };
@@ -393,6 +394,7 @@ static int btcvsd_tx_clean_buffer(struct mtk_btcvsd_snd *bt)
 	}
 
 	spin_unlock_irqrestore(&bt->tx_lock, flags);
+	bt->write_tx = 1;
 
 	return 0;
 }
@@ -500,6 +502,7 @@ int mtk_btcvsd_write_to_bt(struct mtk_btcvsd_snd *bt,
 		mtk_btcvsd_snd_data_transfer(BT_SCO_DIRECT_ARM2BT,
 					     bt->tx->temp_packet_buf, dst,
 					     packet_length, packet_num);
+		bt->write_tx = 1;
 	}
 
 	/* store bt tx buffer sram info */
@@ -539,6 +542,8 @@ static irqreturn_t mtk_btcvsd_snd_irq_handler(int irq_id, void *dev)
 
 	if (__ratelimit(&_rs))
 		dev_info(bt->dev, "%s(), irq_id=%d\n", __func__, irq_id);
+
+	bt->write_tx = 0;
 
 	if (bt->bypass_bt_access)
 		goto irq_handler_exit;
@@ -673,9 +678,11 @@ static irqreturn_t mtk_btcvsd_snd_irq_handler(int irq_id, void *dev)
 	}
 
 	*bt->bt_reg_ctl &= ~BT_CVSD_CLEAR;
-	if (bt->tx->state == BT_SCO_STATE_IDLE)
+	if (bt->tx->state == BT_SCO_STATE_IDLE || bt->write_tx == 0) {
 		*bt->bt_reg_ctl |= BT_CVSD_TX_UNDERFLOW;
-
+		dev_info(bt->dev, "%s(), tx underflow, state = %d, write_tx = %d\n",
+			 __func__, bt->tx->state, bt->write_tx);
+	}
 	if (bt->rx->state == BT_SCO_STATE_RUNNING ||
 	    bt->rx->state == BT_SCO_STATE_ENDING) {
 		bt->rx->wait_flag = 1;
