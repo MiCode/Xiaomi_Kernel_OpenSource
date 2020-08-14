5abf77085e316e183aeca7b5dda0c12db3bfa3b9
Steven Hung <Steven.Hung@mediatek.com>
2020-08-14 22:43:53 +0800

    [ALPS05074711] mddp: remove unused Kconfig config
    remove unused Kconfig config
    
    MTK-Commit-Id: de59e2823c5d2ddfb43ed5f477e7ff18fbcf1c30
    
    Change-Id: I56ae74b834e28fb4c4e825e87de6572ded7f46a5
    Signed-off-by: Steven Hung <Steven.Hung@mediatek.com>
    CR-Id: ALPS05074711
    Feature: MD Direct Data Path-WiFi hotspot

--- drivers/misc/mediatek/mddp/ctrl/mddp_dummy.c
+++ drivers/misc/mediatek/mddp/ctrl/mddp_dummy.c
@@ -1,19 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
+ * mddp_dummy.c dummy file for fixing build issue
+ *
  * Copyright (c) 2020 MediaTek Inc.
  */
 
 #include <linux/module.h>
-
-static int __init mddp_init(void)
-{
-	return 0;
-}
-
-static void __exit mddp_exit(void)
-{
-}
-module_init(mddp_init);
-module_exit(mddp_exit);
 
 MODULE_LICENSE("GPL v2");
