b8c1cd835417d8b97e3d00d6f1b2dd1fbac01664
RenTing Wang <ren-ting.wang@mediatek.com>
2020-08-14 22:47:42 +0800

    [ALPS05277404] clock buffer: enable bring up
    [Detail]
    enable bring-up macro for 6893/6833.
    
    MTK-Commit-Id: cefa08417b16422684df75a4dcd9d832354c59b5
    
    Change-Id: Ia54400337669d26552d955508f7cead08a3883bc
    Signed-off-by: RenTing Wang <ren-ting.wang@mediatek.com>
    CR-Id: ALPS05277404
    Feature: [Module]Clock Buffer

--- drivers/misc/mediatek/base/power/srclken_rc_v1/mt6853/mtk_srclken_rc_hw.c
+++ drivers/misc/mediatek/base/power/srclken_rc_v1/mt6853/mtk_srclken_rc_hw.c
@@ -67,7 +67,11 @@ static void __iomem *gpio_base;
 #endif	/* CONFIG_OF */
 
 /* TODO: marked this after driver is ready */
+#if defined(CONFIG_MACH_MT6833)
+#define SRCLKEN_RC_BRINGUP			1
+#else
 #define SRCLKEN_RC_BRINGUP			0
+#endif /* defined(CONFIG_MACH_MT6833) */
 /* Pwrap Register */
 #define SRCLKEN_RCINF_STA_0			(0x1C4)
 #define SRCLKEN_RCINF_STA_1			(0x1C8)
