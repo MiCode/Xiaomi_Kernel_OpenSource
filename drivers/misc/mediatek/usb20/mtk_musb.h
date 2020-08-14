aba835da2d9a195efa91fb07283777341bdde76f
Eddie Hung <eddie.hung@mediatek.com>
2020-08-14 22:45:37 +0800

    [ALPS05275752] usb20: support different phy version address
    New version of U2 phy offset is 0x300. Add new USB2_PHY_V2 define
    for different phy version address.
    
    MTK-Commit-Id: 6499a6a63d92215a7c7bae7459ad965fda697f87
    
    Change-Id: Iccd8bfa8942c015518b08402d2cda6c7139ddc46
    Signed-off-by: Eddie Hung <eddie.hung@mediatek.com>
    CR-Id: ALPS05275752
    Feature: [Android Default] ADB (Android Debug Bridge)

--- drivers/misc/mediatek/usb20/mtk_musb.h
+++ drivers/misc/mediatek/usb20/mtk_musb.h
@@ -17,23 +17,28 @@
 #ifdef CONFIG_OF
 extern struct musb *mtk_musb;
 
+#ifdef USB2_PHY_V2
+#define USB_PHY_OFFSET 0x300
+#else
+#define USB_PHY_OFFSET 0x800
+#endif
+
 #define USBPHY_READ8(offset) \
 	readb((void __iomem *)\
 		(((unsigned long)\
-		mtk_musb->xceiv->io_priv)+0x800+offset))
+		mtk_musb->xceiv->io_priv)+USB_PHY_OFFSET+offset))
 #define USBPHY_WRITE8(offset, value)  writeb(value, (void __iomem *)\
-		(((unsigned long)mtk_musb->xceiv->io_priv)+0x800+offset))
+		(((unsigned long)mtk_musb->xceiv->io_priv)+USB_PHY_OFFSET+offset))
 #define USBPHY_SET8(offset, mask) \
 	USBPHY_WRITE8(offset, (USBPHY_READ8(offset)) | (mask))
 #define USBPHY_CLR8(offset, mask) \
 	USBPHY_WRITE8(offset, (USBPHY_READ8(offset)) & (~(mask)))
-
 #define USBPHY_READ32(offset) \
 	readl((void __iomem *)(((unsigned long)\
-		mtk_musb->xceiv->io_priv)+0x800+offset))
+		mtk_musb->xceiv->io_priv)+USB_PHY_OFFSET+offset))
 #define USBPHY_WRITE32(offset, value) \
 	writel(value, (void __iomem *)\
-		(((unsigned long)mtk_musb->xceiv->io_priv)+0x800+offset))
+		(((unsigned long)mtk_musb->xceiv->io_priv)+USB_PHY_OFFSET+offset))
 #define USBPHY_SET32(offset, mask) \
 	USBPHY_WRITE32(offset, (USBPHY_READ32(offset)) | (mask))
 #define USBPHY_CLR32(offset, mask) \
@@ -48,18 +53,18 @@ extern struct musb *mtk_musb;
 #include <mach/mt_reg_base.h>
 
 #define USBPHY_READ8(offset) \
-		readb((void __iomem *)(USB_SIF_BASE+0x800+offset))
+		readb((void __iomem *)(USB_SIF_BASE+USB_PHY_OFFSET+offset))
 #define USBPHY_WRITE8(offset, value) \
-		writeb(value, (void __iomem *)(USB_SIF_BASE+0x800+offset))
+		writeb(value, (void __iomem *)(USB_SIF_BASE+USB_PHY_OFFSET+offset))
 #define USBPHY_SET8(offset, mask) \
 	USBPHY_WRITE8(offset, (USBPHY_READ8(offset)) | (mask))
 #define USBPHY_CLR8(offset, mask) \
 	USBPHY_WRITE8(offset, (USBPHY_READ8(offset)) & (~mask))
 
 #define USBPHY_READ32(offset) \
-		readl((void __iomem *)(USB_SIF_BASE+0x800+offset))
+		readl((void __iomem *)(USB_SIF_BASE+USB_PHY_OFFSET+offset))
 #define USBPHY_WRITE32(offset, value) \
-		writel(value, (void __iomem *)(USB_SIF_BASE+0x800+offset))
+		writel(value, (void __iomem *)(USB_SIF_BASE+USB_PHY_OFFSET+offset))
 #define USBPHY_SET32(offset, mask) \
 		USBPHY_WRITE32(offset, (USBPHY_READ32(offset)) | (mask))
 #define USBPHY_CLR32(offset, mask) \
