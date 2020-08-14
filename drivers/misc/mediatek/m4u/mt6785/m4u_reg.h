87c67fda2e31f451db597703b95fc395d373deb3
Guangming Cao <Guangming.Cao@mediatek.com>
2020-08-14 22:47:29 +0800

    [ALPS05276255] m4u: kernel: mt6771: remove smi domain setting
    1. Remove domain setting to avoid devapc issue
    2. Remove related smi secure register define in kernel
    
    MTK-Commit-Id: 1925f5f7eb6521cf4d636cf49608866b1423299d
    
    Change-Id: Id1a8d32d360806de48d0a5f1e91ea083ddd1ba5a
    Signed-off-by: Guangming Cao <Guangming.Cao@mediatek.com>
    CR-Id: ALPS05276255
    Feature: [Module]ION/M4U

--- drivers/misc/mediatek/m4u/mt6785/m4u_reg.h
+++ drivers/misc/mediatek/m4u/mt6785/m4u_reg.h
@@ -426,17 +426,14 @@
 #define F_MMU0_INT_ID_TF_MSK_SEC	F_MSK(11, 2)
 
 /* ================================================================ */
-/* SMI larb */
+/* SMI larb                                                         */
+/* DON NOT read/write secure register in kernel                     */
 /* ================================================================ */
 
-#define SMI_LARB_NON_SEC_CONx(larb_port)	(0x380 + ((larb_port)<<2))
-#define F_SMI_NON_SEC_MMU_EN(en)	F_BIT_VAL(en, 0)
-#define F_SMI_MMU_EN          F_BIT_SET(0)
+#define SMI_LARB_NON_SEC_CONx(larb_port)    (0x380 + ((larb_port)<<2))
+#define F_SMI_NON_SEC_MMU_EN(en)            F_BIT_VAL(en, 0)
+#define F_SMI_MMU_EN                        F_BIT_SET(0)
 
-#define SMI_LARB_SEC_CONx(larb_port)	(0xf80 + ((larb_port)<<2))
-#define F_SMI_SEC_MMU_EN(en)	F_BIT_VAL(en, 0)
-#define F_SMI_SEC_EN(sec)	F_BIT_VAL(sec, 1)
-#define F_SMI_DOMN(domain)	F_VAL(domain, 8, 4)
 /* ========================================================================= */
 /* peripheral system */
 /* ========================================================================= */
