132fd66d9c9ac12e8e08fe5612e6a3d54f16d6c2
Yu-Huan <yu-huan.chen@mediatek.com>
2020-08-14 22:52:02 +0800

    [ALPS05271331] CAM: Extend set_shutter_frame API
    Add third parameter for set_shutter_frame control to
    let user determine whether enable auto-extend or not
    
    MTK-Commit-Id: 0eab53c577a2d775e02ffa661951814d7f52defb
    
    Change-Id: I52d6f62d4774209bbad3b8d7e0acaba3bfd0413d
    CR-Id:ALPS05271331
    Feature:[Android Default] Camera Application Basic Functions
    Signed-off-by: Yu-Huan <yu-huan.chen@mediatek.com>

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/s5k2t7sp_mipi_raw/s5k2t7spmipiraw_Sensor.h
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/s5k2t7sp_mipi_raw/s5k2t7spmipiraw_Sensor.h
@@ -133,6 +133,7 @@ struct imgsensor_info_struct {
 	kal_uint8 ihdr_support;	/* 1, support; 0,not support */
 	kal_uint8 ihdr_le_firstline;	/* 1,le first ; 0, se first */
 	kal_uint8 sensor_mode_num;	/* support sensor mode num */
+	kal_uint8 frame_time_delay_frame;
 
 	kal_uint8 cap_delay_frame;	/* enter capture delay frame num */
 	kal_uint8 pre_delay_frame;	/* enter preview delay frame num */
