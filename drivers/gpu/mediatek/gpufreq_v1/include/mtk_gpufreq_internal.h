711e749c8ddfa159d552a91637e8373bbdc3552a
Caroline Yang <caroline.yang@mediatek.com>
2020-08-20 13:48:49 +0800

    GPU: Refine related platform setting
    1. Rename mtk_gpufreq_mt6873.ko to mtk_gpufreq.ko.
    2. Since there are some hw related setting is platform dependent,
       use CONFIG_MTK_GPU_VERSION if CONFIG_MTK_PLATFORM is not set.
    
    (cherry-picked from commit a2e41a157f0e6500659ec192a5a4be7b3a824c00)
    Signed-off-by: Caroline Yang <caroline.yang@mediatek.com>
    Change-Id: I6c1182ed96b55f21f0dae10bbf57724665cb5e97
    CR-Id: ALPS05298037
    Feature: OpenGL|ES
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    Source_Git_Sha: ebd5438167381adceb54d382f07bb432e43b6b2a

--- drivers/gpu/mediatek/gpufreq_v1/include/mtk_gpufreq_internal.h
+++ drivers/gpu/mediatek/gpufreq_v1/include/mtk_gpufreq_internal.h
@@ -6,9 +6,6 @@
 #ifndef __MTK_GPUFREQ_INTERNAL_H__
 #define __MTK_GPUFREQ_INTERNAL_H__
 
-#include "mt6873/mtk_gpufreq_internal_plat.h"
-
-#if 0
 #if defined(CONFIG_MACH_MT6885)
 #include "mt6885/mtk_gpufreq_internal_plat.h"
 
@@ -17,8 +14,6 @@
 
 #elif defined(CONFIG_MACH_MT6853)
 #include "mt6853/mtk_gpufreq_internal_plat.h"
-#endif
-
 #endif
 
 #endif /* __MTK_GPUFREQ_INTERNAL_H__ */
