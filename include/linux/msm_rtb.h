a6ee1d86bbd70abd512516a42b793ca88a5d3cc4
Divya Sharma <divyash@codeaurora.org>
2020-08-24 16:11:44 -0700

    Revert "msm_rtb: Don't lose the __user tag on relaxed reads/writes"
    This reverts commit fc44c0ba389a4baa9a533568635e2c3f90190e75
    
    Change-Id: I905c520f93945cc880673b349373d8c31e756597
    Signed-off-by: Divya Sharma <divyash@codeaurora.org>

--- include/linux/msm_rtb.h
+++ include/linux/msm_rtb.h
@@ -69,10 +69,11 @@ int uncached_logk(enum logk_event_type log_type, void *data);
 /* Override the #defines in asm/io.h with the logged ones */
 #define __raw_read_logged(a, _l, _t)    ({ \
 	_t __a; \
+	void *_addr = (void *)(a); \
 	int _ret; \
-	_ret = uncached_logk(LOGK_READL, (__force void *) a); \
+	_ret = uncached_logk(LOGK_READL, _addr); \
 	ETB_WAYPOINT; \
-	__a = __raw_read##_l(a); \
+	__a = __raw_read##_l(_addr); \
 	if (_ret) \
 		LOG_BARRIER; \
 	__a; \
@@ -135,9 +136,10 @@ int uncached_logk(enum logk_event_type log_type, void *data);
 
 #define __raw_write_logged(v, a, _t) ({ \
 	int _ret; \
-	_ret = uncached_logk(LOGK_WRITEL, (__force void *) a); \
+	void *_addr = (void *)(a); \
+	_ret = uncached_logk(LOGK_WRITEL, _addr); \
 	ETB_WAYPOINT; \
-	__raw_write##_t((v), a); \
+	__raw_write##_t((v), _addr); \
 	if (_ret) \
 		LOG_BARRIER; \
 	})
