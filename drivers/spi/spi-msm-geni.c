70c8f90639b13ba00056408660100ef12e0bacc3
Ashish Kori <akori@codeaurora.org>
2020-08-25 20:07:07 +0530

    spi: spi-msm-geni: Propagate missing change from 5.4 to 5.15
    Propagate following change from 5.4 to 5.15 for SPI driver.
    'commit 2e7d855eea ("spi: spi-geni-qcom : Don't configure
    SPI WORD LEN for SPI Slave")'.
    
    Change-Id: I714f455589be75d19d8f32e9f04e8cdc518a0430
    Signed-off-by: Ashish Kori <akori@codeaurora.org>
    Signed-off-by: Aniket Randive <quic_arandive@quicinc.com>

--- drivers/spi/spi-msm-geni.c
+++ drivers/spi/spi-msm-geni.c
@@ -397,6 +397,7 @@ static int get_spi_clk_cfg(u32 speed_hz, struct spi_geni_master *mas,
 static void spi_setup_word_len(struct spi_geni_master *mas, u32 mode,
 						int bits_per_word)
 {
+	struct spi_master *spi = get_spi_master(mas->dev);
 	int pack_words = 1;
 	bool msb_first = (mode & SPI_LSB_FIRST) ? false : true;
 	u32 word_len = geni_read_reg(mas->base, SE_SPI_WORD_LEN);
@@ -407,10 +408,18 @@ static void spi_setup_word_len(struct spi_geni_master *mas, u32 mode,
 	 */
 	if (!(mas->tx_fifo_width % bits_per_word))
 		pack_words = mas->tx_fifo_width / bits_per_word;
-	word_len &= ~WORD_LEN_MSK;
-	word_len |= ((bits_per_word - MIN_WORD_LEN) & WORD_LEN_MSK);
+	/*
+	 * Don't configure SPI word length for SPI Slave it is an optional
+	 * property for SPI Slave. H/W will take care of SPI slave word
+	 * if SPI_SLAVE_EN bit is set.
+	 */
+	if (!spi->slave) {
+		word_len &= ~WORD_LEN_MSK;
+		word_len |= ((bits_per_word - MIN_WORD_LEN) & WORD_LEN_MSK);
+		geni_write_reg(word_len, mas->base, SE_SPI_WORD_LEN);
+	}
+
 	geni_se_config_packing(&mas->spi_rsc, bits_per_word, pack_words, msb_first, true, true);
-	geni_write_reg(word_len, mas->base, SE_SPI_WORD_LEN);
 	SPI_LOG_DBG(mas->ipc, false, mas->dev,
 		"%s: %lu bpw %d pack_words %d\n", __func__,
 		bits_per_word, pack_words);
