a512f1fe5e7f0998fd938f23e95f3a47c045e43d
roger-hy.wang <roger-hy.wang@mediatek.com>
2020-08-25 13:30:50 +0800

    flashlight: add pt/dlpt control
    add power throttling and dynamic loading power throttling
    
    (cherry-picked from commit b5db756615b5dfda59e17878604909dc7844ca04)
    Signed-off-by: roger-hy.wang <roger-hy.wang@mediatek.com>
    Change-Id: I98d6b9f326cd73fc9b01e86526c962f8744434c9
    CR-Id: ALPS05298037
    Feature: [Android Default] Strobe
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    Source_Git_Sha: d51a71caba14ce3e30b115b1e13336ec00d70700

--- drivers/misc/mediatek/flashlight/flashlight-core.c
+++ drivers/misc/mediatek/flashlight/flashlight-core.c
@@ -29,7 +29,7 @@
 #ifdef CONFIG_MTK_FLASHLIGHT_PT
 #include "mtk_battery_oc_throttling.h"
 #include "mtk_low_battery_throttling.h"
-#include "mtk_battery_percentage_throttling.h"
+#include "mtk_bp_thl.h"
 #endif
 
 #ifdef CONFIG_MTK_FLASHLIGHT_DLPT
@@ -536,7 +536,20 @@ static int flashlight_update_charger_status(struct flashlight_dev *fdev)
 /******************************************************************************
  * Power throttling
  *****************************************************************************/
+#ifdef CONFIG_MTK_FLASHLIGHT_DLPT
+void flashlight_kicker_pbm(bool status)
+{
+	kicker_pbm_by_flash(status);
+}
+EXPORT_SYMBOL(flashlight_kicker_pbm);
+#endif
 #ifdef CONFIG_MTK_FLASHLIGHT_PT
+int flashlight_pt_is_low(void)
+{
+	return pt_is_low(pt_low_vol, pt_low_bat, pt_over_cur);
+}
+EXPORT_SYMBOL(flashlight_pt_is_low);
+
 static int pt_arg_verify(int pt_low_vol, int pt_low_bat, int pt_over_cur)
 {
 	if (pt_low_vol < LOW_BATTERY_LEVEL_0 ||
@@ -576,33 +589,26 @@ static int pt_is_low(int pt_low_vol, int pt_low_bat, int pt_over_cur)
 static int pt_trigger(void)
 {
 	struct flashlight_dev *fdev;
-	int is_flash_enable = 0;
 
 	mutex_lock(&fl_mutex);
 	list_for_each_entry(fdev, &flashlight_list, node) {
-		if (fdev->enable)
-			is_flash_enable = 1;
-	}
-	if (is_flash_enable) {
-		list_for_each_entry(fdev, &flashlight_list, node) {
-			if (!fdev->ops)
-				continue;
+		if (!fdev->ops)
+			continue;
 
-			fdev->ops->flashlight_open();
-			fdev->ops->flashlight_set_driver(1);
-			if (pt_strict) {
-				pr_info("PT trigger(%d,%d,%d) disable flashlight\n",
-					pt_low_vol, pt_low_bat, pt_over_cur);
-				fl_enable(fdev, 0);
-			} else {
-				pr_info("PT trigger(%d,%d,%d) decrease duty: %d\n",
-					pt_low_vol, pt_low_bat,
-					pt_over_cur, fdev->low_pt_level);
-				fl_set_level(fdev, fdev->low_pt_level);
-			}
-			fdev->ops->flashlight_set_driver(0);
-			fdev->ops->flashlight_release();
+		fdev->ops->flashlight_open();
+		fdev->ops->flashlight_set_driver(1);
+		if (pt_strict) {
+			pr_info_ratelimited("PT trigger(%d,%d,%d) disable flashlight\n",
+				pt_low_vol, pt_low_bat, pt_over_cur);
+			fl_enable(fdev, 0);
+		} else {
+			pr_info_ratelimited("PT trigger(%d,%d,%d) decrease duty: %d\n",
+				pt_low_vol, pt_low_bat,
+				pt_over_cur, fdev->low_pt_level);
+			fl_set_level(fdev, fdev->low_pt_level);
 		}
+		fdev->ops->flashlight_set_driver(0);
+		fdev->ops->flashlight_release();
 	}
 	mutex_unlock(&fl_mutex);
 
@@ -1790,7 +1796,7 @@ static int __init flashlight_init(void)
 #ifdef CONFIG_MTK_FLASHLIGHT_PT
 	register_low_battery_notify(
 			&pt_low_vol_callback, LOW_BATTERY_PRIO_FLASHLIGHT);
-	register_battery_percent_notify(
+	register_bp_thl_notify(
 			&pt_low_bat_callback, BATTERY_PERCENT_PRIO_FLASHLIGHT);
 	register_battery_oc_notify(
 			&pt_oc_callback, BATTERY_OC_PRIO_FLASHLIGHT);
