0d69849f2744443e8017a318f62551a77fcd6703
Jordan Crouse <jcrouse@codeaurora.org>
2020-08-26 13:49:59 -0600

    msm: kgsl: Move the GMU kernel IOMMU device to the GMU platform device
    In order to make devlink happy we need to bind an IOMMU device on the
    primary GMU platform device.  Since we don't need GMU user yet the
    decision is easy.  Move GMU kernel to the platform device instead
    of a child.
    
    Change-Id: Ic0dedbad082c57b2aeb6a35573ead8535f3b8ab9
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/adreno_a6xx_hwsched_hfi.c
+++ drivers/gpu/msm/adreno_a6xx_hwsched_hfi.c
@@ -431,8 +431,7 @@ static int gmu_import_buffer(struct adreno_device *adreno_dev,
 
 	desc->gmu_addr = vma->next_va;
 
-	mapped = iommu_map_sg(a6xx_get_gmu_domain(gmu, desc->gmu_addr,
-			desc->size),
+	mapped = iommu_map_sg(gmu->domain,
 			desc->gmu_addr, sgt->sgl, sgt->nents, attrs);
 	if (mapped == 0)
 		dev_err(&gmu->pdev->dev, "gmu map sg err: 0x%08x, %d, %x, %zd\n",
