141226390252c374760f7dccd82005705d6e6565
Perry Hsu <perry.hsu@mediatek.com>
2020-08-27 23:58:32 +0800

    [ALPS05056551] apusys: dump: remove apu_sctrl_reviser
    Remove apu_sctrl_reviser from register dump
    to avoid DEVAPC violations
    
    MTK-Commit-Id: 9f556e6821a2cc3471c37a424325850890e0bce2
    
    Change-Id: I9c801c710ae66ef337194333871b0a5621945bcf
    Signed-off-by: Perry Hsu <perry.hsu@mediatek.com>
    CR-Id: ALPS05056551
    Feature: [Module] APUSYS

--- drivers/misc/mediatek/apusys/midware/mt6873/dump_reg.c
+++ drivers/misc/mediatek/apusys/midware/mt6873/dump_reg.c
@@ -134,7 +134,6 @@ struct reg_dump_info range_table[SEGMENT_COUNT] = {
 	{"apu_xpu_rsi_config",  0x1901C000, 0x1000},
 	{"apu_acp_ssc_config",  0x1901F000, 0x1000},
 	{"apu_conn_config",     0x19020000, 0x1000},
-	{"apu_sctrl_reviser",   0x19021000, 0x1000},
 	{"apu_sema_stimer",     0x19022000, 0x1000},
 	{"emi_config",          0x19023000, 0x1000},
 	{"apb_dapc_wrapper",    0x19064000, 0x1000},
