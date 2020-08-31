42f8ca20867663d2394f6b408682911fc98e260f
Venkata Prahlad Valluru <vvalluru@codeaurora.org>
2020-08-31 11:18:49 +0530

    video: fbdev: msm: add null check for input arguments
    Validate 'mfd' before dereferencing.
    
    Change-Id: I6efcb8c085bb17b17bc6628fa095153cb03f8e06
    Signed-off-by: Venkata Prahlad Valluru <vvalluru@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -5336,10 +5336,12 @@ void mdss_fb_calc_fps(struct msm_fb_data_type *mfd)
 
 void mdss_fb_idle_pc(struct msm_fb_data_type *mfd)
 {
-	struct mdss_overlay_private *mdp5_data = mfd_to_mdp5_data(mfd);
+	struct mdss_overlay_private *mdp5_data;
 
-	if (mdss_fb_is_power_off(mfd))
+	if (!mfd || mdss_fb_is_power_off(mfd))
 		return;
+
+	mdp5_data = mfd_to_mdp5_data(mfd);
 
 	if ((mfd->panel_info->type == MIPI_CMD_PANEL) && mdp5_data) {
 		pr_debug("Notify fb%d idle power collapsed\n", mfd->index);
