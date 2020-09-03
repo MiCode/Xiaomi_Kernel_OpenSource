d0cb56ec456e2911277cc9cb273497fcbf0f9c5c
Tony Truong <truong@codeaurora.org>
2020-09-03 14:34:56 -0700

    msm: pcie: provide client the ability to control PCIe target_link_speed
    There are usecases where clients want to change the maximum GEN
    speed that PCIe can link up with. Provide clients the ability to
    control the maximum PCIe GEN speed to for such usecases.
    
    Change-Id: I834bee2063133546c9b0ebac2d5328ffbc5ceac6
    Signed-off-by: Tony Truong <truong@codeaurora.org>

--- include/linux/msm_pcie.h
+++ include/linux/msm_pcie.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
-/* Copyright (c) 2014-2020, The Linux Foundation. All rights reserved.*/
+/* Copyright (c) 2014-2021, The Linux Foundation. All rights reserved.*/
 
 #ifndef __MSM_PCIE_H
 #define __MSM_PCIE_H
@@ -63,6 +63,24 @@ void msm_msi_config(struct irq_domain *domain);
 int msm_msi_init(struct device *dev);
 
 #if IS_ENABLED(CONFIG_PCI_MSM)
+
+/**
+ * msm_pcie_set_target_link_speed - sets the upper bound of GEN speed PCIe can
+ * link up with
+ * @rc_idx:		root complex port number that endpoint is connected to
+ * @target_link_speed:	new target link speed PCIe can link up with
+ *
+ * Provide PCIe clients the option to control upper bound of GEN speed PCIe
+ * can link up with. Clients may choose only GEN speed within root complex's
+ * controller capability or up to what is defined in devicetree,
+ * qcom,target-link-speed.
+ *
+ * Client may also pass 0 for target_link_speed to have PCIe root complex
+ * reset and use the default TLS.
+ *
+ * Return 0 on success, negative value on error
+ */
+int msm_pcie_set_target_link_speed(u32 rc_idx, u32 target_link_speed);
 
 /**
  * msm_pcie_allow_l1 - allow PCIe link to re-enter L1
@@ -217,6 +235,12 @@ int msm_pcie_debug_info(struct pci_dev *dev, u32 option, u32 base,
 #else /* !CONFIG_PCI_MSM */
 static inline int msm_pcie_pm_control(enum msm_pcie_pm_opt pm_opt, u32 busnr,
 			void *user, void *data, u32 options)
+{
+	return -ENODEV;
+}
+
+static inline int msm_pcie_set_target_link_speed(u32 rc_idx,
+						u32 target_link_speed)
 {
 	return -ENODEV;
 }
