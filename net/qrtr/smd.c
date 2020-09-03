42f4d273a41faace2ce275e345322c8ebca008db
Sarannya S <sarannya@codeaurora.org>
2020-09-03 18:21:41 +0530

    rpmsg: glink: Add error message in case of callback failure
    Sometimes due to late initialization glink callback may not be
    initialized and incoming will be dropped.
    
    Print error message if receive callback is not ready.
    
    Change-Id: I3eae1717d4e1c14b319e288078d122762fd476e7
    Signed-off-by: Sarannya S <sarannya@codeaurora.org>

--- net/qrtr/smd.c
+++ net/qrtr/smd.c
@@ -24,8 +24,10 @@ static int qcom_smd_qrtr_callback(struct rpmsg_device *rpdev,
 	struct qrtr_smd_dev *qdev = dev_get_drvdata(&rpdev->dev);
 	int rc;
 
-	if (!qdev)
+	if (!qdev) {
+		pr_err("%d:Not ready\n", __func__);
 		return -EAGAIN;
+	}
 
 	rc = qrtr_endpoint_post(&qdev->ep, data, len);
 	if (rc == -EINVAL) {
@@ -63,6 +65,7 @@ static int qcom_smd_qrtr_probe(struct rpmsg_device *rpdev)
 	u32 net_id;
 	bool rt;
 	int rc;
+	pr_err("%d:Entered\n", __func__);
 
 	qdev = devm_kzalloc(&rpdev->dev, sizeof(*qdev), GFP_KERNEL);
 	if (!qdev)
@@ -84,7 +87,8 @@ static int qcom_smd_qrtr_probe(struct rpmsg_device *rpdev)
 
 	dev_set_drvdata(&rpdev->dev, qdev);
 
-	dev_dbg(&rpdev->dev, "Qualcomm SMD QRTR driver probed\n");
+	pr_err("%d:SMD QRTR driver probed\n", __func__);
+	dev_dbg(&rpdev->dev, "SMD QRTR driver probed\n");
 
 	return 0;
 }
