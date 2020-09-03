bbbc7063779257c3d2e8eba490a7b0494bf77142
Mahesh Kumar Kalikot Veetil <mkalikot@codeaurora.org>
2020-09-03 13:31:20 -0700

    cnss_prealloc: Redesign prealloc scheme
    cnss preallocation scheme is a memory pool that always tries to
    keep a list of free memory for use in emergencies. It is
    implemented on kernel features: memorypool and kmem_cache.
    
    Change-Id: Ibc7cd0cf351ef2afa29a252623c20e4853b17d75
    Signed-off-by: Mahesh Kumar Kalikot Veetil <mkalikot@codeaurora.org>

--- drivers/net/wireless/cnss_prealloc/cnss_prealloc.c
+++ drivers/net/wireless/cnss_prealloc/cnss_prealloc.c
@@ -1,323 +1,265 @@
 // SPDX-License-Identifier: GPL-2.0-only
-/* Copyright (c) 2012,2014-2017,2019,2020 The Linux Foundation. All rights reserved. */
+/* Copyright (c) 2012,2014-2017,2019-2021 The Linux Foundation. All rights reserved. */
 
 #include <linux/module.h>
 #include <linux/slab.h>
+#include <linux/mempool.h>
+#include <linux/mm.h>
 #include <linux/err.h>
-#include <linux/stacktrace.h>
-#include <linux/spinlock.h>
-#include <linux/sysfs.h>
-#ifdef CONFIG_WCNSS_SKB_PRE_ALLOC
-#include <linux/skbuff.h>
-#endif
 #include <net/cnss_prealloc.h>
 
-static DEFINE_SPINLOCK(alloc_lock);
+MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("CNSS prealloc driver");
 
-#ifdef CONFIG_SLUB_DEBUG
-#define WCNSS_MAX_STACK_TRACE			64
-#endif
+/* cnss preallocation scheme is a memory pool that always tries to keep a
+ * list of free memory for use in emergencies. It is implemented on kernel
+ * features: memorypool and kmem cache.
+ */
 
-static struct kobject  *prealloc_kobject;
-
-struct wcnss_prealloc {
-	int occupied;
+struct cnss_pool {
 	size_t size;
-	void *ptr;
-#ifdef CONFIG_SLUB_DEBUG
-	unsigned long stack_trace[WCNSS_MAX_STACK_TRACE];
-	unsigned int stack_nr_entries;
-#endif
+	int min;
+	const char name[50];
+	mempool_t *mp;
+	struct kmem_cache *cache;
 };
 
-/* pre-alloced mem for WLAN driver */
-static struct wcnss_prealloc wcnss_allocs[] = {
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 8  * 1024, NULL},
-	{0, 16 * 1024, NULL},
-	{0, 16 * 1024, NULL},
-	{0, 16 * 1024, NULL},
-	{0, 16 * 1024, NULL},
-	{0, 16 * 1024, NULL},
-	{0, 16 * 1024, NULL},
-	{0, 16 * 1024, NULL},
-	{0, 16 * 1024, NULL},
-	{0, 16 * 1024, NULL},
-	{0, 16 * 1024, NULL},
-	{0, 16 * 1024, NULL},
-	{0, 16 * 1024, NULL},
-	{0, 16 * 1024, NULL},
-	{0, 16 * 1024, NULL},
-	{0, 16 * 1024, NULL},
-	{0, 16 * 1024, NULL},
-	{0, 32 * 1024, NULL},
-	{0, 32 * 1024, NULL},
-	{0, 32 * 1024, NULL},
-	{0, 32 * 1024, NULL},
-	{0, 32 * 1024, NULL},
-	{0, 32 * 1024, NULL},
-	{0, 64 * 1024, NULL},
-	{0, 64 * 1024, NULL},
-	{0, 64 * 1024, NULL},
-	{0, 64 * 1024, NULL},
-	{0, 64 * 1024, NULL},
-	{0, 64 * 1024, NULL},
-	{0, 64 * 1024, NULL},
-	{0, 64 * 1024, NULL},
-	{0, 128 * 1024, NULL},
+/**
+ * Memory pool
+ * -----------
+ *
+ * How to update this table:
+ *
+ *  1. Add a new row with following elements
+ *      size  : Size of one allocation unit in bytes.
+ *      min   : Minimum units to be reserved. Used only if a regular
+ *              allocation fails.
+ *      name  : Name of the cache/pool. Will be displayed in /proc/slabinfo
+ *              if not merged with another pool.
+ *      mp    : A pointer to memory pool. Updated during init.
+ *      cache : A pointer to cache. Updated during init.
+ * 2. Always keep the table in increasing order
+ * 3. Please keep the reserve pool as minimum as possible as it's always
+ *    preallocated.
+ * 4. Always profile with different use cases after updating this table.
+ * 5. A dynamic view of this pool can be viewed at /proc/slabinfo.
+ * 6. Each pool has a sys node at /sys/kernel/slab/<name>
+ *
+ */
+
+/* size, min pool reserve, name, memorypool handler, cache handler*/
+static struct cnss_pool cnss_pools[] = {
+	{8 * 1024, 22, "cnss-pool-8k", NULL, NULL},
+	{16 * 1024, 16, "cnss-pool-16k", NULL, NULL},
+	{32 * 1024, 6, "cnss-pool-32k", NULL, NULL},
+	{64 * 1024, 8, "cnss-pool-64k", NULL, NULL},
+	{128 * 1024, 2, "cnss-pool-128k", NULL, NULL},
 };
 
-int wcnss_prealloc_init(void)
+/**
+ * cnss_pool_alloc_threshold() - Allocation threshold
+ *
+ * Minimum memory size to be part of cnss pool.
+ *
+ * Return: Size
+ *
+ */
+static inline size_t cnss_pool_alloc_threshold(void)
+{
+	return cnss_pools[0].size;
+}
+
+/**
+ * cnss_pool_int() - Initialize memory pools.
+ *
+ * Create cnss pools as configured by cnss_pools[]. It is the responsibility of
+ * the caller to invoke cnss_pool_deinit() routine to clean it up. This
+ * function needs to be called at early boot to preallocate minimum buffers in
+ * the pool.
+ *
+ * Return: 0 - success, otherwise error code.
+ *
+ */
+static int cnss_pool_init(void)
 {
 	int i;
 
-	for (i = 0; i < ARRAY_SIZE(wcnss_allocs); i++) {
-		wcnss_allocs[i].occupied = 0;
-		wcnss_allocs[i].ptr = kmalloc(wcnss_allocs[i].size, GFP_KERNEL);
-		if (!wcnss_allocs[i].ptr)
-			return -ENOMEM;
+	for (i = 0; i < ARRAY_SIZE(cnss_pools); i++) {
+		/* Create the slab cache */
+		cnss_pools[i].cache =
+		    kmem_cache_create(cnss_pools[i].name, cnss_pools[i].size, 0,
+				      SLAB_ACCOUNT, NULL);
+		if (!cnss_pools[i].cache) {
+			pr_err("cnss_prealloc: cache %s failed\n",
+			       cnss_pools[i].name);
+			continue;
+		}
+
+		/* Create the pool and associate to slab cache */
+		cnss_pools[i].mp =
+		    mempool_create(cnss_pools[i].min, mempool_alloc_slab,
+				   mempool_free_slab, cnss_pools[i].cache);
+
+		if (!cnss_pools[i].mp) {
+			pr_err("cnss_prealloc: mempool %s failed\n",
+			       cnss_pools[i].name);
+			kmem_cache_destroy(cnss_pools[i].cache);
+			cnss_pools[i].cache = NULL;
+			continue;
+		}
+
+		pr_info("cnss_prealloc: created mempool %s of min size %d * %d\n",
+			cnss_pools[i].name, cnss_pools[i].min,
+			cnss_pools[i].size);
 	}
 
 	return 0;
 }
 
-void wcnss_prealloc_deinit(void)
+/**
+ * cnss_pool_deinit() - Free memory pools.
+ *
+ * Free the memory pools and return resources back to the system. It warns
+ * if there is any pending element in memory pool or cache.
+ *
+ */
+static void cnss_pool_deinit(void)
 {
-	int i = 0;
+	int i;
 
-	for (i = 0; i < ARRAY_SIZE(wcnss_allocs); i++) {
-		kfree(wcnss_allocs[i].ptr);
-		wcnss_allocs[i].ptr = NULL;
+	for (i = 0; i < ARRAY_SIZE(cnss_pools); i++) {
+		pr_info("cnss_prealloc: destroy mempool %s\n",
+			cnss_pools[i].name);
+		mempool_destroy(cnss_pools[i].mp);
+		kmem_cache_destroy(cnss_pools[i].cache);
 	}
 }
 
-#ifdef CONFIG_SLUB_DEBUG
-static void wcnss_prealloc_save_stack_trace(struct wcnss_prealloc *entry)
+/**
+ * cnss_pool_get_index() - Get the index of memory pool
+ * @mem: Allocated memory
+ *
+ * Returns the index of the memory pool which fits the reqested memory.
+ *
+ */
+static int cnss_pool_get_index(void *mem)
 {
-	entry->stack_nr_entries =
-		stack_trace_save(entry->stack_trace,
-				 ARRAY_SIZE(entry->stack_trace), 2);
-}
-#else
-static inline
-void wcnss_prealloc_save_stack_trace(struct wcnss_prealloc *entry) {}
-#endif
+	struct page *page;
+	struct kmem_cache *cache;
+	int i;
 
+	/* mem -> page -> cache */
+	page = virt_to_head_page(mem);
+	if (!page) {
+		pr_err("cnss_prealloc: no page\n");
+		return -ENOENT;
+	}
+
+	cache = page->slab_cache;
+	if (!cache) {
+		pr_err("cnss_prealloc: no cache\n");
+		return -ENOENT;
+	}
+
+	/* Check if memory belongs to a pool */
+	for (i = 0; i < ARRAY_SIZE(cnss_pools); i++) {
+		if (cnss_pools[i].cache == cache)
+			return i;
+	}
+
+	return -ENOENT;
+}
+
+/**
+ * wcnss_prealloc_get() - Get preallocated memory from a pool
+ * @size: Size to allocate
+ *
+ * Memory pool is chosen based on the size. If memory is not available in a
+ * given pool it goes to next higher sized pool until it succeeds.
+ *
+ * Return: A void pointer to allocated memory
+ */
 void *wcnss_prealloc_get(size_t size)
 {
-	int i = 0;
-	unsigned long flags;
 
-	spin_lock_irqsave(&alloc_lock, flags);
-	for (i = 0; i < ARRAY_SIZE(wcnss_allocs); i++) {
-		if (wcnss_allocs[i].occupied)
-			continue;
+	void *mem = NULL;
+	gfp_t gfp_mask = __GFP_ZERO;
+	int i;
 
-		if (wcnss_allocs[i].size >= size) {
-			/* we found the slot */
-			wcnss_allocs[i].occupied = 1;
-			spin_unlock_irqrestore(&alloc_lock, flags);
-			wcnss_prealloc_save_stack_trace(&wcnss_allocs[i]);
-			return wcnss_allocs[i].ptr;
+	if (in_interrupt() || irqs_disabled())
+		gfp_mask |= GFP_ATOMIC;
+	else
+		gfp_mask |= GFP_KERNEL;
+
+	if (size >= cnss_pool_alloc_threshold()) {
+
+		for (i = 0; i < ARRAY_SIZE(cnss_pools); i++) {
+			if (cnss_pools[i].size >= size) {
+				mem = mempool_alloc(cnss_pools[i].mp, gfp_mask);
+				if (mem)
+					break;
+			}
 		}
 	}
-	spin_unlock_irqrestore(&alloc_lock, flags);
 
-	return NULL;
+	if (!mem && size >= cnss_pool_alloc_threshold()) {
+		pr_err("cnss_prealloc: alloc failed for size %d, flag %x\n",
+		       size, gfp_mask);
+	}
+
+	return mem;
 }
 EXPORT_SYMBOL(wcnss_prealloc_get);
 
-int wcnss_prealloc_put(void *ptr)
+/**
+ * wcnss_prealloc_put() - Relase allocated memory
+ * @mem: Allocated memory
+ *
+ * Free the memory got by wcnss_prealloc_get() to slab or pool reserve if memory
+ * pool doesn't have enough elements.
+ *
+ * Return: 1 - success
+ *         0 - fail
+ */
+int wcnss_prealloc_put(void *mem)
 {
-	int i = 0;
-	unsigned long flags;
+	int i;
 
-	spin_lock_irqsave(&alloc_lock, flags);
-	for (i = 0; i < ARRAY_SIZE(wcnss_allocs); i++) {
-		if (wcnss_allocs[i].ptr == ptr) {
-			wcnss_allocs[i].occupied = 0;
-			spin_unlock_irqrestore(&alloc_lock, flags);
-			return 1;
-		}
+	if (!mem)
+		return 0;
+
+	if (ksize(mem) < cnss_pool_alloc_threshold())
+		return 0;
+
+	i = cnss_pool_get_index(mem);
+
+	if (i >= 0 && i < ARRAY_SIZE(cnss_pools)) {
+		mempool_free(mem, cnss_pools[i].mp);
+		return 1;
 	}
-	spin_unlock_irqrestore(&alloc_lock, flags);
 
 	return 0;
 }
 EXPORT_SYMBOL(wcnss_prealloc_put);
 
-#ifdef CONFIG_SLUB_DEBUG
-void wcnss_prealloc_check_memory_leak(void)
-{
-	int i, j = 0;
-
-	for (i = 0; i < ARRAY_SIZE(wcnss_allocs); i++) {
-		if (!wcnss_allocs[i].occupied)
-			continue;
-
-		if (j == 0) {
-			pr_err("wcnss_prealloc: Memory leak detected\n");
-			j++;
-		}
-
-		pr_err("Size: %zu, addr: %pK, backtrace:\n",
-		       wcnss_allocs[i].size, wcnss_allocs[i].ptr);
-		stack_trace_print(wcnss_allocs[i].stack_trace,
-				  wcnss_allocs[i].stack_nr_entries, 1);
-	}
-}
-#else
+/* Not implemented. Make use of Linux SLAB features. */
 void wcnss_prealloc_check_memory_leak(void) {}
-#endif
 EXPORT_SYMBOL(wcnss_prealloc_check_memory_leak);
 
-int wcnss_pre_alloc_reset(void)
-{
-	int i, n = 0;
-
-	for (i = 0; i < ARRAY_SIZE(wcnss_allocs); i++) {
-		if (!wcnss_allocs[i].occupied)
-			continue;
-
-		wcnss_allocs[i].occupied = 0;
-		n++;
-	}
-	return n;
-}
+/* Not implemented. Make use of Linux SLAB features. */
+int wcnss_pre_alloc_reset(void) { return -EOPNOTSUPP; }
 EXPORT_SYMBOL(wcnss_pre_alloc_reset);
 
-static ssize_t status_show(struct kobject *kobj, struct kobj_attribute *attr,
-			   char *buffer)
+static int __init cnss_prealloc_init(void)
 {
-	int i = 0;
-	int used_slots = 0, free_slots = 0;
-	unsigned int tsize = 0, tused = 0, size = 0;
-	int len = 0;
-	char *buf;
-
-	buf = buffer;
-	len += scnprintf(&buf[len], PAGE_SIZE - len,
-			"\nSlot_Size(Kb)\t\t[Used : Free]\n");
-	for (i = 0; i < ARRAY_SIZE(wcnss_allocs); i++) {
-		tsize += wcnss_allocs[i].size;
-		if (size != wcnss_allocs[i].size) {
-			if (size) {
-				len += scnprintf(&buf[len], PAGE_SIZE - len,
-						"[%d : %d]\n", used_slots,
-						free_slots);
-
-			}
-
-			size = wcnss_allocs[i].size;
-			used_slots = 0;
-			free_slots = 0;
-			len += scnprintf(&buf[len], PAGE_SIZE - len,
-					"%d Kb\t\t\t", size / 1024);
-
-		}
-
-		if (wcnss_allocs[i].occupied) {
-			tused += wcnss_allocs[i].size;
-			++used_slots;
-		} else {
-			++free_slots;
-		}
-	}
-	len += scnprintf(&buf[len], PAGE_SIZE - len,
-			"[%d : %d]\n", used_slots, free_slots);
-
-	/* Convert byte to Kb */
-	if (tsize)
-		tsize = tsize / 1024;
-	if (tused)
-		tused = tused / 1024;
-	len += scnprintf(&buf[len], PAGE_SIZE - len,
-			"\nMemory Status:\nTotal Memory: %dKb\n",
-			tsize);
-	len += scnprintf(&buf[len], PAGE_SIZE - len,
-			"Used: %dKb\nFree: %dKb\n", tused,
-			tsize - tused);
-
-	return len;
+	return cnss_pool_init();
 }
 
-static struct kobj_attribute status_attribute = __ATTR_RO(status);
-
-static int create_prealloc_status_sysfs(void)
+static void __exit cnss_prealloc_exit(void)
 {
-	int ret;
-
-	prealloc_kobject = kobject_create_and_add("cnss-prealloc",
-						  kernel_kobj);
-	if (!prealloc_kobject) {
-		pr_err("Failed to create cnss-prealloc kernel object\n");
-		return -ENOMEM;
-	}
-
-	ret = sysfs_create_file(prealloc_kobject,
-				&status_attribute.attr);
-	if (ret) {
-		pr_err("%s: Failed to create sysfs cnss-prealloc file\n",
-		       __func__);
-		kobject_put(prealloc_kobject);
-	}
-
-	return ret;
+	cnss_pool_deinit();
 }
 
-static void remove_prealloc_status_sysfs(void)
-{
-	if (prealloc_kobject) {
-		sysfs_remove_file(prealloc_kobject,
-				  &status_attribute.attr);
-		kobject_put(prealloc_kobject);
-	}
-}
+module_init(cnss_prealloc_init);
+module_exit(cnss_prealloc_exit);
 
-static int __init wcnss_pre_alloc_init(void)
-{
-	int ret;
-
-	ret = wcnss_prealloc_init();
-	if (ret) {
-		pr_err("%s: Failed to init the prealloc pool\n", __func__);
-		return ret;
-	}
-
-	create_prealloc_status_sysfs();
-
-	return ret;
-}
-
-static void __exit wcnss_pre_alloc_exit(void)
-{
-	wcnss_prealloc_deinit();
-	remove_prealloc_status_sysfs();
-}
-
-module_init(wcnss_pre_alloc_init);
-module_exit(wcnss_pre_alloc_exit);
-
-MODULE_LICENSE("GPL v2");
-MODULE_DESCRIPTION("WCNSS Prealloc Driver");
