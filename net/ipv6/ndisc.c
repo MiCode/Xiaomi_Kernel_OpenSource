3a256cea7f5d3b878ffd04970ab8ef2bd19b1642
Rocco Yue <rocco.yue@mediatek.com>
2020-09-03 14:27:31 +0800

    [ALPS05328917] IPv6: revert to kernel source
    dont clean route expires when the route_lifetime carried by
    RA msg equals to 0xFFFF
    
    MTK-Commit-Id: 00cf0cc18a4b450a77413831467db134e908d670
    
    Change-Id: I09d232d5c5a9c57c7e6d13700c22c512edeced55
    Signed-off-by: Rocco Yue <rocco.yue@mediatek.com>
    CR-Id: ALPS05328917
    Feature: IPv6 over MD

--- net/ipv6/ndisc.c
+++ net/ipv6/ndisc.c
@@ -1316,24 +1316,8 @@ static void ndisc_router_discovery(struct sk_buff *skb)
 		rt->rt6i_flags = (rt->rt6i_flags & ~RTF_PREF_MASK) | RTF_PREF(pref);
 	}
 
-	if (rt) {
-		/*MTK changes
-		 *if route lifetime carried by RA msg equals to 0xFFFF,
-		 *considering it as infinite route lifetime and cleaning
-		 *route expires. Otherwise, setting route expires according
-		 *to the lifetime value.
-		 */
-		if (lifetime == 0xffff) {
-			rt6_clean_expires(rt);
-			pr_info("[mtk_net]RA: %s, rt %p, clean route expires since lifetime %d infinite\n",
-				__func__, rt, lifetime);
-		} else {
-			rt6_set_expires(rt, jiffies + (HZ * lifetime));
-			pr_info("[mtk_net]RA: %s, rt %p, set route expires since lifetime %d finite\n",
-				__func__, rt, lifetime);
-		}
-	}
-
+	if (rt)
+		rt6_set_expires(rt, jiffies + (HZ * lifetime));
 	if (in6_dev->cnf.accept_ra_min_hop_limit < 256 &&
 	    ra_msg->icmph.icmp6_hop_limit) {
 		if (in6_dev->cnf.accept_ra_min_hop_limit <= ra_msg->icmph.icmp6_hop_limit) {
