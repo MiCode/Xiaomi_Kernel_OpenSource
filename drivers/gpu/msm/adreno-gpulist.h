6169704198fcaf015ae4e1d90c6fccb2dcbe084d
Deepak Kumar <dkumar@codeaurora.org>
2020-09-10 15:46:05 +0530

    msm: kgsl: Enable Preemption on A660 GPU variant
    Enable Preemption feature on A660 GPU variant.
    
    Change-Id: I5a5dd33ca1fc7ececc5f8e52680ccad11d028726
    Signed-off-by: Deepak Kumar <dkumar@codeaurora.org>

--- drivers/gpu/msm/adreno-gpulist.h
+++ drivers/gpu/msm/adreno-gpulist.h
@@ -1617,7 +1617,7 @@ static const struct adreno_a6xx_core adreno_gpu_core_a660_shima = {
 		.compatible = "qcom,adreno-gpu-a660-shima",
 		.features = ADRENO_RPMH | ADRENO_GPMU | ADRENO_APRIV |
 				ADRENO_IOCOHERENT | ADRENO_CONTENT_PROTECTION |
-				ADRENO_IFPC,
+				ADRENO_IFPC | ADRENO_PREEMPTION,
 		.gpudev = &adreno_a6xx_gmu_gpudev,
 		.gmem_base = 0,
 		.gmem_size = SZ_1M + SZ_512K,
