a00d72ced2641a5814b09240fdd4020e04b9a8a8
Auto Li <auto.li@mediatek.com>
2020-09-10 16:30:08 +0800

    cam: Add log of i2c retrun value
    Add log of i2c retrun value
    
    (cherry-picked from commit 6e39a694393f2e07f4bb22d528483d3a8ff2822e)
    Change-Id: Ib385431fbd7c7b6b3b63ff832e080baabca11e2e
    CR-Id: ALPS05357698
    Feature: Multimedia - Driver
    Signed-off-by: Auto Li <auto.li@mediatek.com>
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    Source_Git_Sha: 83e4e14a82e4d54a9f77e86fd32bfaaa92ec665e

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/imgsensor_i2c.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/imgsensor_i2c.c
@@ -349,6 +349,7 @@ enum IMGSENSOR_RETURN imgsensor_i2c_read(
 {
 	struct IMGSENSOR_I2C_INST *pinst = pi2c_cfg->pinst;
 	enum   IMGSENSOR_RETURN    ret   = IMGSENSOR_RETURN_SUCCESS;
+	int i2c_ret = 0;
 
 	if (pinst->pi2c_client == NULL) {
 		PK_PR_ERR("pi2c_client is NULL!\n");
@@ -367,21 +368,21 @@ enum IMGSENSOR_RETURN imgsensor_i2c_read(
 	pi2c_cfg->msg[1].len   = read_length;
 	pi2c_cfg->msg[1].buf   = pread_data;
 
-	if (mtk_i2c_transfer(
+	i2c_ret = mtk_i2c_transfer(
 			pinst->pi2c_client->adapter,
 			pi2c_cfg->msg,
 			IMGSENSOR_I2C_MSG_SIZE_READ,
 			(pi2c_cfg->pinst->status.filter_msg)
 				? I2C_A_FILTER_MSG : 0,
 			((speed > 0) && (speed <= 1000))
-				? speed * 1000 : IMGSENSOR_I2C_SPEED * 1000)
-			!= IMGSENSOR_I2C_MSG_SIZE_READ) {
+				? speed * 1000 : IMGSENSOR_I2C_SPEED * 1000);
+	if (i2c_ret != IMGSENSOR_I2C_MSG_SIZE_READ) {
 		static DEFINE_RATELIMIT_STATE(ratelimit, 1 * HZ, 30);
 
 		if (__ratelimit(&ratelimit))
 			PK_PR_ERR(
-			"I2C read failed (0x%x)! speed(0=%d) (0x%x)\n",
-			ret, speed, *pwrite_data);
+			"I2C read failed (%d)! speed(0=%d) (0x%x)\n",
+			i2c_ret, speed, *pwrite_data);
 		ret = IMGSENSOR_RETURN_ERROR;
 	}
 
@@ -404,6 +405,7 @@ enum IMGSENSOR_RETURN imgsensor_i2c_write(
 	u8                 *pdata = pwrite_data;
 	u8                 *pend  = pwrite_data + write_length;
 	int i   = 0;
+	int i2c_ret = 0;
 
 	if (pinst->pi2c_client == NULL) {
 		PK_PR_ERR("pi2c_client is NULL!\n");
@@ -423,21 +425,21 @@ enum IMGSENSOR_RETURN imgsensor_i2c_write(
 		pdata += write_per_cycle;
 	}
 
-	if (mtk_i2c_transfer(
+	i2c_ret = mtk_i2c_transfer(
 			pinst->pi2c_client->adapter,
 			pi2c_cfg->msg,
 			i,
 			(pi2c_cfg->pinst->status.filter_msg)
 				? I2C_A_FILTER_MSG : 0,
 			((speed > 0) && (speed <= 1000))
-				? speed * 1000 : IMGSENSOR_I2C_SPEED * 1000)
-			!= i) {
+				? speed * 1000 : IMGSENSOR_I2C_SPEED * 1000);
+	if (i2c_ret != i) {
 		static DEFINE_RATELIMIT_STATE(ratelimit, 1 * HZ, 30);
 
 		if (__ratelimit(&ratelimit))
 			PK_PR_ERR(
-				"I2C write failed (0x%x)! speed(0=%d) (0x%x)\n",
-				ret, speed, *pwrite_data);
+				"I2C write failed (%d)! speed(0=%d) (0x%x)\n",
+				i2c_ret, speed, *pwrite_data);
 		ret = IMGSENSOR_RETURN_ERROR;
 	}
 
