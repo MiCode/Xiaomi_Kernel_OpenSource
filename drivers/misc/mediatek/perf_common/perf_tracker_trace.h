rusyd4/kernel_xiaomi_mt6781

perf: a tracker for performance index
    A kernel module for performance index which tracks
    memory/io/cpu stall ratio/cpu freq/dram freq/emi to
    help identify performance drop cause. It is a polling
    base design(interval 4 or 8ms) and default is off.
    Cost is estimated 6 ~ 15us per 4ms in average.

    How to enable this feature(default off)
       - kernel config: CONFIG_MTK_PERF_TRACKER=y
       - echo 1 > /sys/devices/system/cpu/perf/enable
    related trace events
       - perf_index_s, perf_index_l

    [ALPS04266006] perf: a tracker for performance index
    [ALPS04272373] perf: refactor perf tracker for flexibilty
    [ALPS04272373] perf: fix undeclared vaiable build error

Change-Id: I807fa955ca3ca878acdd0a183a39bd6d200d897b
Feature: System Performance
CR-Id: ALPS04679093
Signed-off-by: JianMin Liu <jian-min.liu@mediatek.com>

drivers/misc/mediatek/perf_common/perf_tracker_trace.h
@@ -1,0 +1,146 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2019 MediaTek Inc.
+ */
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM perf_tracker
+
+#if !defined(_PERF_TRACKER_TRACE_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _PERF_TRACKER_TRACE_H
+
+#include <linux/string.h>
+#include <linux/types.h>
+#include <linux/tracepoint.h>
+#include <perf_tracker.h>
+
+TRACE_EVENT(perf_index_s,
+	TP_PROTO(
+		unsigned int sf0,
+		unsigned int sf1,
+		unsigned int sf2,
+		int dram_freq,
+		int bw_c,
+		int bw_g,
+		int bw_mm,
+		int bw_total
+	),
+
+	TP_ARGS(sf0, sf1, sf2, dram_freq, bw_c, bw_g, bw_mm, bw_total),
+
+	TP_STRUCT__entry(
+		__field(unsigned int, sf0)
+		__field(unsigned int, sf1)
+		__field(unsigned int, sf2)
+		__field(int, dram_freq)
+		__field(int, bw_c)
+		__field(int, bw_g)
+		__field(int, bw_mm)
+		__field(int, bw_total)
+	),
+
+	TP_fast_assign(
+		__entry->sf0       =  sf0;
+		__entry->sf1       =  sf1;
+		__entry->sf2       =  sf2;
+		__entry->dram_freq = dram_freq;
+		__entry->bw_c      = bw_c;
+		__entry->bw_g      = bw_g;
+		__entry->bw_mm     = bw_mm;
+		__entry->bw_total  = bw_total;
+	),
+
+	TP_printk("sched_freq=%d|%d|%d dram_freq=%d bw=%d|%d|%d|%d",
+		__entry->sf0,
+		__entry->sf1,
+		__entry->sf2,
+		__entry->dram_freq,
+		__entry->bw_c,
+		__entry->bw_g,
+		__entry->bw_mm,
+		__entry->bw_total)
+);
+
+TRACE_EVENT(perf_index_l,
+
+	TP_PROTO(
+		long free_mem,
+		long avail_mem,
+		struct mtk_btag_mictx_iostat_struct *iostatptr,
+		int *stall
+	),
+
+	TP_ARGS(free_mem,
+		avail_mem,
+		iostatptr,
+		stall
+	),
+
+	TP_STRUCT__entry(
+		__field(long, free_mem)
+		__field(long, avail_mem)
+		__field(int, io_wl)
+		__field(int, io_req_r)
+		__field(int, io_all_r)
+		__field(int, io_reqsz_r)
+		__field(int, io_reqc_r)
+		__field(int, io_req_w)
+		__field(int, io_all_w)
+		__field(int, io_reqsz_w)
+		__field(int, io_reqc_w)
+		__field(int, io_dur)
+		__field(int, io_q_dept)
+		__array(int, stall, 8)
+	),
+
+	TP_fast_assign(
+		__entry->free_mem   = free_mem;
+		__entry->avail_mem  = avail_mem;
+		__entry->io_wl      = iostatptr->wl;
+		__entry->io_req_r   = iostatptr->tp_req_r;
+		__entry->io_all_r   = iostatptr->tp_all_r;
+		__entry->io_reqsz_r = iostatptr->reqsize_r;
+		__entry->io_reqc_r  = iostatptr->reqcnt_r;
+		__entry->io_req_w   = iostatptr->tp_req_w;
+		__entry->io_all_w   = iostatptr->tp_all_w;
+		__entry->io_reqsz_w = iostatptr->reqsize_w;
+		__entry->io_reqc_w  = iostatptr->reqcnt_w;
+		__entry->io_dur     = iostatptr->duration;
+		__entry->io_q_dept  = iostatptr->q_depth;
+		memcpy(__entry->stall, stall, sizeof(int)*8);
+	),
+
+	TP_printk(
+		"free_mem=%ld avail_mem=%ld iostats=%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d stall=%d|%d|%d|%d|%d|%d|%d|%d",
+		__entry->free_mem,
+		__entry->avail_mem,
+		__entry->io_wl,
+		__entry->io_req_r,
+		__entry->io_all_r,
+		__entry->io_reqsz_r,
+		__entry->io_reqc_r,
+		__entry->io_req_w,
+		__entry->io_all_w,
+		__entry->io_reqsz_w,
+		__entry->io_reqc_w,
+		__entry->io_dur,
+		__entry->io_q_dept,
+		__entry->stall[0],
+		__entry->stall[1],
+		__entry->stall[2],
+		__entry->stall[3],
+		__entry->stall[4],
+		__entry->stall[5],
+		__entry->stall[6],
+		__entry->stall[7]
+		)
+);
+
+#endif /*_PERF_TRACKER_TRACE_H */
+
+#undef TRACE_INCLUDE_PATH
+#define TRACE_INCLUDE_PATH .
+#undef TRACE_INCLUDE_FILE
+#define TRACE_INCLUDE_FILE perf_tracker_trace
+
+/* This part must be outside protection */
+#include <trace/define_trace.h>

MiCode/Xiaomi_Kernel_OpenSource

       1) kernel config:
           - COMFIG_MTK_PERF_COMMON=m
           - CONFIG_MTK_PERF_TRACKER=y
       2) echo 1 > /sys/devices/system/cpu/perf/enable
       3) related trace events
           - perf_index_s, perf_index_l
(cherry-picked from commit 7c6dd78581db811f9eef375769cd1e7a3970290d)
Change-Id: I7e1829728fdb51ae543186bedbf0ae28c39185a4
CR-Id: ALPS05298037
Signed-off-by: Miles Chen <miles.chen@mediatek.com>

drivers/misc/mediatek/perf_common/perf_tracker_trace.h
@@ -1,0 +1,146 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2019 MediaTek Inc.
+ */
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM perf_tracker
+
+#if !defined(_PERF_TRACKER_TRACE_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _PERF_TRACKER_TRACE_H
+
+#include <linux/string.h>
+#include <linux/types.h>
+#include <linux/tracepoint.h>
+#include <perf_tracker_internal.h>
+
+TRACE_EVENT(perf_index_s,
+	TP_PROTO(
+		unsigned int sf0,
+		unsigned int sf1,
+		unsigned int sf2,
+		int dram_freq,
+		int bw_c,
+		int bw_g,
+		int bw_mm,
+		int bw_total
+	),
+
+	TP_ARGS(sf0, sf1, sf2, dram_freq, bw_c, bw_g, bw_mm, bw_total),
+
+	TP_STRUCT__entry(
+		__field(unsigned int, sf0)
+		__field(unsigned int, sf1)
+		__field(unsigned int, sf2)
+		__field(int, dram_freq)
+		__field(int, bw_c)
+		__field(int, bw_g)
+		__field(int, bw_mm)
+		__field(int, bw_total)
+	),
+
+	TP_fast_assign(
+		__entry->sf0       =  sf0;
+		__entry->sf1       =  sf1;
+		__entry->sf2       =  sf2;
+		__entry->dram_freq = dram_freq;
+		__entry->bw_c      = bw_c;
+		__entry->bw_g      = bw_g;
+		__entry->bw_mm     = bw_mm;
+		__entry->bw_total  = bw_total;
+	),
+
+	TP_printk("sched_freq=%u|%u|%u dram_freq=%d bw=%d|%d|%d|%d",
+		__entry->sf0,
+		__entry->sf1,
+		__entry->sf2,
+		__entry->dram_freq,
+		__entry->bw_c,
+		__entry->bw_g,
+		__entry->bw_mm,
+		__entry->bw_total)
+);
+
+TRACE_EVENT(perf_index_l,
+
+	TP_PROTO(
+		long free_mem,
+		long avail_mem,
+		struct mtk_btag_mictx_iostat_struct *iostatptr,
+		int *stall
+	),
+
+	TP_ARGS(free_mem,
+		avail_mem,
+		iostatptr,
+		stall
+	),
+
+	TP_STRUCT__entry(
+		__field(long, free_mem)
+		__field(long, avail_mem)
+		__field(int, io_wl)
+		__field(int, io_req_r)
+		__field(int, io_all_r)
+		__field(int, io_reqsz_r)
+		__field(int, io_reqc_r)
+		__field(int, io_req_w)
+		__field(int, io_all_w)
+		__field(int, io_reqsz_w)
+		__field(int, io_reqc_w)
+		__field(int, io_dur)
+		__field(int, io_q_dept)
+		__array(int, stall, 8)
+	),
+
+	TP_fast_assign(
+		__entry->free_mem   = free_mem;
+		__entry->avail_mem  = avail_mem;
+		__entry->io_wl      = iostatptr->wl;
+		__entry->io_req_r   = iostatptr->tp_req_r;
+		__entry->io_all_r   = iostatptr->tp_all_r;
+		__entry->io_reqsz_r = iostatptr->reqsize_r;
+		__entry->io_reqc_r  = iostatptr->reqcnt_r;
+		__entry->io_req_w   = iostatptr->tp_req_w;
+		__entry->io_all_w   = iostatptr->tp_all_w;
+		__entry->io_reqsz_w = iostatptr->reqsize_w;
+		__entry->io_reqc_w  = iostatptr->reqcnt_w;
+		__entry->io_dur     = iostatptr->duration;
+		__entry->io_q_dept  = iostatptr->q_depth;
+		memcpy(__entry->stall, stall, sizeof(int)*8);
+	),
+
+	TP_printk(
+		"free_mem=%ld avail_mem=%ld iostats=%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d stall=%d|%d|%d|%d|%d|%d|%d|%d",
+		__entry->free_mem,
+		__entry->avail_mem,
+		__entry->io_wl,
+		__entry->io_req_r,
+		__entry->io_all_r,
+		__entry->io_reqsz_r,
+		__entry->io_reqc_r,
+		__entry->io_req_w,
+		__entry->io_all_w,
+		__entry->io_reqsz_w,
+		__entry->io_reqc_w,
+		__entry->io_dur,
+		__entry->io_q_dept,
+		__entry->stall[0],
+		__entry->stall[1],
+		__entry->stall[2],
+		__entry->stall[3],
+		__entry->stall[4],
+		__entry->stall[5],
+		__entry->stall[6],
+		__entry->stall[7]
+		)
+);
+
+#endif /*_PERF_TRACKER_TRACE_H */
+
+#undef TRACE_INCLUDE_PATH
+#define TRACE_INCLUDE_PATH .
+#undef TRACE_INCLUDE_FILE
+#define TRACE_INCLUDE_FILE perf_tracker_trace
+
+/* This part must be outside protection */
+#include <trace/define_trace.h>
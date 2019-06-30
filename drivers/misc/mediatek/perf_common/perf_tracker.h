9536608487b3800da38cc8f9be9a0c533bac35fb
JianMin Liu <jian-min.liu@mediatek.com>
2019-06-30 21:13:06 +0800

    perf: a tracker for performance index
        A kernel module for performance index which tracks
        memory/io/cpu stall ratio/cpu freq/dram freq/emi to
        help identify performance drop cause. It is a polling
        base design(interval 4 or 8ms) and default is off.
        Cost is estimated 6 ~ 15us per 4ms in average.
    
        How to enable this feature(default off)
           1) kernel config:
               - COMFIG_MTK_PERF_COMMON=m
               - CONFIG_MTK_PERF_TRACKER=y
           2) echo 1 > /sys/devices/system/cpu/perf/enable
           3) related trace events
               - perf_index_s, perf_index_l
    
    perf: a tracker for performance index
    (cherry-picked from commit 7c6dd78581db811f9eef375769cd1e7a3970290d)
    Signed-off-by: JianMin Liu <jian-min.liu@mediatek.com>
    Change-Id: I7e1829728fdb51ae543186bedbf0ae28c39185a4
    CR-Id: ALPS05298037
    Feature: System Performance
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

---
+++ drivers/misc/mediatek/perf_common/perf_tracker.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2019 MediaTek Inc.
+ */
+
+#ifndef _PERF_TRACKER_H
+#define _PERF_TRACKER_H
+
+#if  IS_ENABLED(CONFIG_MTK_GPU_SWPM_SUPPORT)
+void perf_update_gpu_counter(unsigned int gpu_pmu[], unsigned int len);
+#endif
+
+#endif /* _PERF_TRACKER_H */
+
