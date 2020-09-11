bce6137346621848180aaf35e91323d5891c107b
Jack Pham <jackp@codeaurora.org>
2020-09-11 00:01:46 -0700

    usb: gadget: qdss: Use usb_ep_autoconfig() instead of ss() variant
    usb_ep_autoconfig_ss() is only needed in case a function wants
    to match on both an EP descriptor as well as a SS companion
    that specifies the number of streams. Since the QDSS function
    doesn't use streams, we can simply use the normal
    usb_ep_autoconfig() function instead to simplify the code.
    
    This also allows building usb_f_qdss as a module against a GKI
    kernel which doesn't currently have the usb_ep_autoconfig_ss()
    symbol exported from the KMI.
    
    Change-Id: Iab1caff332b7a9d708286a2d67ae2ee6bf490f34
    Signed-off-by: Jack Pham <jackp@codeaurora.org>

--- drivers/usb/gadget/function/f_qdss.c
+++ drivers/usb/gadget/function/f_qdss.c
@@ -410,8 +410,7 @@ static int qdss_bind(struct usb_configuration *c, struct usb_function *f)
 	if (!strcmp(qdss->ch.name, USB_QDSS_CH_MDM))
 		qdss_data_ep_comp_desc.bMaxBurst = 0;
 
-	ep = usb_ep_autoconfig_ss(gadget, &qdss_ss_data_desc,
-		&qdss_data_ep_comp_desc);
+	ep = usb_ep_autoconfig(gadget, &qdss_ss_data_desc);
 	if (!ep) {
 		pr_err("%s: ep_autoconfig error\n", __func__);
 		goto clear_ep;
@@ -423,8 +422,7 @@ static int qdss_bind(struct usb_configuration *c, struct usb_function *f)
 		msm_ep_set_endless(qdss->port.data, true);
 
 	if (qdss->debug_inface_enabled) {
-		ep = usb_ep_autoconfig_ss(gadget, &qdss_ss_ctrl_in_desc,
-			&qdss_ctrl_in_ep_comp_desc);
+		ep = usb_ep_autoconfig(gadget, &qdss_ss_ctrl_in_desc);
 		if (!ep) {
 			pr_err("%s: ep_autoconfig error\n", __func__);
 			goto clear_ep;
@@ -433,8 +431,7 @@ static int qdss_bind(struct usb_configuration *c, struct usb_function *f)
 		qdss->port.ctrl_in = ep;
 		ep->driver_data = qdss;
 
-		ep = usb_ep_autoconfig_ss(gadget, &qdss_ss_ctrl_out_desc,
-			&qdss_ctrl_out_ep_comp_desc);
+		ep = usb_ep_autoconfig(gadget, &qdss_ss_ctrl_out_desc);
 		if (!ep) {
 			pr_err("%s: ep_autoconfig error\n", __func__);
 			goto clear_ep;
