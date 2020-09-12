5783d009078048ee5cf5674f74ce2eb6c71dc003
JianMin Liu <jian-min.liu@mediatek.com>
2020-09-12 23:57:07 +0800

    [ALPS05086443] L3cc: introduce to L3 cache partition control
       Cache partition control limit cache use of non-critical
       task(e.g. background) against critical tasks being
       cache-evicted, as to meet higher cache hit rate for
       critical tasks. Default is  1/4 part of whole cache
       for limited access range.
       benefit: 2-4% improvement in app-switch
       overhead: 0.3-0.5 us per context switch
    
       How to enable:
        - echo 1 > /sys/module/cache_ctrl/parameters/enable
    
       How to debug:
        a. debugfs
        - cat /sys/kernel/debug/l3cc_info
    
          /* all cpu is belong to group0 */
          dump L3 partition register.
            CPU0: group[0]
            CPU1: group[0]
            CPU2: group[0]
            CPU3: group[0]
            CPU4: group[0]
            CPU5: group[0]
            CPU6: group[0]
            CPU7: group[0]
          /*
           *  There are 4 cache partition totally and the following
           *  is config to two group:
           *
           *  group 1 is 1/4 of the available cache partition,
           *  group 0 is fully available cache partition.
           */
          partcr=0x8f
    
         b. trace event:
          - apply_cache_control:
            Show the suppressing task states if applied successfully.
    
          - skip_cache_control:
            Show the task and status when appled failure.
    
    MTK-Commit-Id: 76aa1f83220a7f712ee2ac46da92b0607c04b1ee
    
    Change-Id: If40e31f5bc91b482825381e22d9b03d0d115a9d4
    Signed-off-by: mtk07831 <jian-min.liu@mediatek.com>
    CR-Id: ALPS05086443
    Feature: System Performance

--- drivers/misc/mediatek/include/mt-plat/l3cc_common.h
+++ drivers/misc/mediatek/include/mt-plat/l3cc_common.h
@@ -14,17 +14,19 @@
 #ifndef _L3CC_COMMON_H_
 #define _L3CC_COMMON_H_
 #ifdef CONFIG_MTK_CACHE_CONTROL
-extern void hook_ca_context_switch(struct rq *rq,
-		struct task_struct *prev,
-	    struct task_struct *next);
 extern int ca_force_stop_set_in_kernel(int val);
 extern void hook_ca_scheduler_tick(int cpu);
 #else
+static inline void hook_ca_scheduler_tick(int cpu) {}
+#endif
+#ifdef CONFIG_MTK_CACHE_PARTITION_CTRL
+extern void hook_ca_context_switch(struct rq *rq,
+		struct task_struct *prev,
+	    struct task_struct *next);
 
+#else
 inline void hook_ca_context_switch(struct rq *rq,
 		struct task_struct *prev,
 	    struct task_struct *next) {}
-inline void hook_ca_scheduler_tick(int cpu) {}
-int ca_force_stop_set_in_kernel(int val) {return -EINVAL; }
 #endif
 #endif
