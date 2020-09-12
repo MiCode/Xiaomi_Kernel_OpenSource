2b79e41a35433ea677d35e36e7a0de5b384c0aad
Allen Chu <allen.chu@mediatek.com>
2020-09-12 23:53:10 +0800

    [ALPS05126624] ddp_pwm: Coverity
    fix " CERT-C Error Handling" issue
    
    MTK-Commit-Id: 122f5965a7d8501af299066b3e6b2c6812e7f479
    
    Change-Id: I02f75338e34bd87774049bf96de688eb266b892c
    CR-Id: ALPS05126624
    Feature: Smart Contrast Local Tone Mapping
    Signed-off-by: Allen Chu <allen.chu@mediatek.com>

--- drivers/misc/mediatek/video/common/pwm10/ddp_pwm.c
+++ drivers/misc/mediatek/video/common/pwm10/ddp_pwm.c
@@ -236,6 +236,7 @@ static void disp_pwm_query_backlight(char *debug_output)
 	const unsigned long reg_base = pwm_get_reg_base(DISP_PWM0);
 	int index = index_of_pwm(DISP_PWM0);
 	unsigned int high_width;
+	int error = 0;
 
 	if (atomic_read(&g_pwm_is_power_on[index]) == 1) {
 		if (g_pwm_led_mode == MT65XX_LED_MODE_CUST_BLS_PWM) {
@@ -257,16 +258,19 @@ static void disp_pwm_query_backlight(char *debug_output)
 
 	if (high_width > 0) {
 		/* print backlight status */
-		snprintf(temp_buf, buf_max_len,
+		error = snprintf(temp_buf, buf_max_len,
 			"backlight is on (%d), ddp_pwm power:(%d)",
 			high_width, atomic_read(&g_pwm_is_power_on[index]));
 	} else {
-		snprintf(temp_buf, buf_max_len,
+		error = snprintf(temp_buf, buf_max_len,
 			"backlight is off, ddp_pwm power:(%d)",
 			atomic_read(&g_pwm_is_power_on[index]));
 	}
 
-	PWM_NOTICE("%s", temp_buf);
+	if (error < 0)
+		PWM_NOTICE("snprintf fail");
+	else
+		PWM_NOTICE("%s", temp_buf);
 #endif
 }
 
@@ -458,6 +462,7 @@ static void disp_pwm_log(int level_1024, int log_type)
 	struct timeval pwm_time;
 	char buffer[LOGBUFFERSIZE] = "";
 	int print_log;
+	int error = 0;
 
 	do_gettimeofday(&pwm_time);
 
@@ -472,13 +477,17 @@ static void disp_pwm_log(int level_1024, int log_type)
 	print_log = 0;
 
 	if (g_pwm_log_index >= g_pwm_log_num || level_1024 == 0) {
-		sprintf(buffer + strlen(buffer), "(latest=%2u): ",
+		error = sprintf(buffer + strlen(buffer), "(latest=%2u): ",
 			g_pwm_log_index);
+		if (error < 0)
+			PWM_MSG("sprintf fail");
 		for (i = 0; i < g_pwm_log_index; i += 1) {
-			sprintf(buffer + strlen(buffer), "%5d(%4lu,%4lu)",
+			error = sprintf(buffer + strlen(buffer), "%5d(%4lu,%4lu)",
 				g_pwm_log_buffer[i].value,
 				g_pwm_log_buffer[i].tsec,
 				g_pwm_log_buffer[i].tusec);
+			if (error < 0)
+				PWM_MSG("sprintf fail");
 		}
 
 		g_pwm_log_index = 0;
