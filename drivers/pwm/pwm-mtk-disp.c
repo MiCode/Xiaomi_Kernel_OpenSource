4f30bfde98867a6f206946ac99d4fa293bdcab28
mtk19784 <ycw.wu@mediatek.com>
2020-09-13 00:02:20 +0800

    [ALPS05340849] pwm: Porting platform node
    add platform node
    
    MTK-Commit-Id: e91533555b67556f0e5f8734da5c420605379c83
    
    Change-Id: I6f2f972e3dbc4ab4d0d9f8fc584ba57c456f4a1e
    CR-Id:ALPS05340849
    Feature: Ambient Light Adaptive Luma
    Signed-off-by: mtk19784 <ycw.wu@mediatek.com>

--- drivers/pwm/pwm-mtk-disp.c
+++ drivers/pwm/pwm-mtk-disp.c
@@ -425,6 +425,7 @@ static const struct of_device_id mtk_disp_pwm_of_match[] = {
 	{.compatible = "mediatek,mt6885-disp-pwm", .data = &mt6799_pwm_data},
 	{.compatible = "mediatek,mt6853-disp-pwm", .data = &mt6799_pwm_data},
 	{.compatible = "mediatek,mt8173-disp-pwm", .data = &mt8173_pwm_data},
+	{.compatible = "mediatek,mt6833-disp-pwm", .data = &mt6799_pwm_data},
 	{}
 };
 MODULE_DEVICE_TABLE(of, mtk_disp_pwm_of_match);
