979c9d9f7c7911e9dda04a5210626c6beb5c2321
Neeraj Soni <neersoni@codeaurora.org>
2020-09-14 17:33:14 +0530

    md: dm-default-key: Use system sector size for SDHCI devices
    When metadata content of userdata is encrypted with version 1.0.0 or
    previous the sector size on SDHCI devices is taken as default sector size
    value hence use the same sector size for upgrade scenario.
    
    Change-Id: Ia84b1e9c340678520235df428107e17f3dbeb5cb
    Signed-off-by: Neeraj Soni <neersoni@codeaurora.org>

--- drivers/md/dm-default-key.c
+++ drivers/md/dm-default-key.c
@@ -158,7 +158,9 @@ static void default_key_adjust_sector_size_and_iv(char **argv,
 
 		memcpy(raw, key_new.bytes, size);
 
-		if (ti->len & (((*dkc)->sector_size >> SECTOR_SHIFT) - 1))
+		if ((ti->len & (((*dkc)->sector_size >> SECTOR_SHIFT) - 1)) ||
+		    ((*dkc)->dev->bdev->bd_disk->disk_name[0] &&
+		     !strcmp((*dkc)->dev->bdev->bd_disk->disk_name, "mmcblk0")))
 			(*dkc)->sector_size = SECTOR_SIZE;
 
 		if (dev->bdev->bd_part)
