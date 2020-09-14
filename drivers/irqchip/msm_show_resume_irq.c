dbf1cd150d47e3a6aa1a23e4593a379c1374e999
Elliot Berman <eberman@codeaurora.org>
2020-09-14 15:38:19 -0700

    irqchip/msm-show-resume-irqs: Add vendor hook into GIC resume
    Add vendor hook into GIC resume to print pending IRQs. This helps in
    debugging the spurious wakeups due to interrupts from various
    subsystems.
    
    Change-Id: Ibf343b1e93b789216f5927b49ebe6f58e32511e3
    Signed-off-by: Elliot Berman <eberman@codeaurora.org>

--- drivers/irqchip/msm_show_resume_irq.c
+++ drivers/irqchip/msm_show_resume_irq.c
@@ -7,7 +7,63 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
+#include <linux/interrupt.h>
+#include <linux/irq.h>
+#include <linux/irqchip/arm-gic-v3.h>
+#include <trace/hooks/gic.h>
+
 int msm_show_resume_irq_mask;
 
-module_param_named(
-	debug_mask, msm_show_resume_irq_mask, int, 0664);
+module_param_named(debug_mask, msm_show_resume_irq_mask, int, 0664);
+
+static void msm_show_resume_irqs(void *data, struct irq_domain *domain,
+				 void __iomem *base)
+{
+	unsigned int i;
+	u32 enabled;
+	u32 pending[32];
+	u32 gic_line_nr;
+	u32 typer;
+
+	if (!msm_show_resume_irq_mask)
+		return;
+
+	typer = readl_relaxed(base + GICD_TYPER);
+	gic_line_nr = min(GICD_TYPER_SPIS(typer), 1023u);
+
+	for (i = 0; i * 32 < gic_line_nr; i++) {
+		enabled = readl_relaxed(base + GICD_ICENABLER + i * 4);
+		pending[i] = readl_relaxed(base + GICD_ISPENDR + i * 4);
+		pending[i] &= enabled;
+	}
+
+	for (i = find_first_bit((unsigned long *)pending, gic_line_nr);
+	     i < gic_line_nr;
+	     i = find_next_bit((unsigned long *)pending, gic_line_nr, i + 1)) {
+		unsigned int irq = irq_find_mapping(domain, i);
+		struct irq_desc *desc = irq_to_desc(irq);
+		const char *name = "null";
+
+		if (desc == NULL)
+			name = "stray irq";
+		else if (desc->action && desc->action->name)
+			name = desc->action->name;
+
+		pr_warn("%s: %d triggered %s\n", __func__, irq, name);
+	}
+}
+
+static int __init msm_show_resume_irq_init(void)
+{
+	return register_trace_android_vh_gic_resume(msm_show_resume_irqs, NULL);
+}
+
+#if IS_MODULE(CONFIG_QCOM_SHOW_RESUME_IRQ)
+module_init(msm_show_resume_irq_init);
+#else
+pure_initcall(msm_show_resume_irq_init);
+#endif
+
+MODULE_DESCRIPTION("Qualcomm Technologies, Inc. IRQ Logging driver");
+MODULE_LICENSE("GPL v2");
+
