802f3427132ac1d3ebbde92d6712f35960cc8354
yong qian <qianyong@oppo.com>
2020-09-15 21:14:57 +0800

    ANDROID: vendor_hooks: Add hooks for debugging exception context
    When some processes crashed, we want to save cpu regs(x0-x30、pc、sp)，
    and context(task_struct、thread_info) to log for debugging
    
    Bug: 166228327
    
    Signed-off-by: yong qian <qianyong@oppo.com>
    Change-Id: I4f49ffed583a0e20f1aaa7e445e85a0cbb603bbe

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -55,6 +55,7 @@
 #include <asm/pointer_auth.h>
 #include <asm/scs.h>
 #include <asm/stacktrace.h>
+#include <trace/hooks/minidump.h>
 
 #if defined(CONFIG_STACKPROTECTOR) && !defined(CONFIG_STACKPROTECTOR_PER_TASK)
 #include <linux/stackprotector.h>
@@ -254,6 +255,7 @@ void __show_regs(struct pt_regs *regs)
 		top_reg = 29;
 	}
 
+	trace_android_vh_show_regs(regs);
 	show_regs_print_info(KERN_DEFAULT);
 	print_pstate(regs);
 
