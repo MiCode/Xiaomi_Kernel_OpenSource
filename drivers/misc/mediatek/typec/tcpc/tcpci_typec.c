2c76666aba76140d4070b8789b4053b506a06ae3
Vend.Gene.Chen <vend_gene_chen001@mediatek.com>
2020-09-17 18:15:57 +0800

    usb: typec: tcpc: fix mt6360 water detect
    fix mt6360 water detect, and bypass check water protection in DPDM until
    usb pull down ready.
    
    (cherry-picked from commit 1b24ff057c8357983847cb2404aaf3a64686796f)
    Signed-off-by: Vend.Gene.Chen <vend_gene_chen001@mediatek.com>
    Change-Id: I7b9e201aee93edadf8c7aab680095135ed79d85c
    CR-Id: ALPS05398736
    Feature: [Module]PMIC
    Signed-off-by: Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
    (cherry picked from commit ad2f50226189d860050af958c967440a56738dcd)

--- drivers/misc/mediatek/typec/tcpc/tcpci_typec.c
+++ drivers/misc/mediatek/typec/tcpc/tcpci_typec.c
@@ -387,8 +387,7 @@ static inline int typec_norp_src_attached_entry(struct tcpc_device *tcpc_dev)
 #ifdef CONFIG_WATER_DETECTION
 #ifdef CONFIG_WD_POLLING_ONLY
 	if (!tcpc_dev->typec_power_ctrl) {
-		if (get_boot_mode() == KERNEL_POWER_OFF_CHARGING_BOOT ||
-		    get_boot_mode() == LOW_POWER_OFF_CHARGING_BOOT)
+		if (tcpc_dev->bootmode == 8 || tcpc_dev->bootmode == 9)
 			typec_check_water_status(tcpc_dev);
 
 		tcpci_set_usbid_polling(tcpc_dev, false);
@@ -2040,8 +2039,7 @@ int tcpc_typec_handle_cc_change(struct tcpc_device *tcpc_dev)
 		if (typec_state_old == typec_unattached_snk ||
 		    typec_state_old == typec_unattached_src) {
 #ifdef CONFIG_WD_POLLING_ONLY
-			if (get_boot_mode() == KERNEL_POWER_OFF_CHARGING_BOOT
-			    || get_boot_mode() == LOW_POWER_OFF_CHARGING_BOOT)
+			if (tcpc_dev->bootmode == 8 || tcpc_dev->bootmode == 9)
 				typec_check_water_status(tcpc_dev);
 #else
 			typec_check_water_status(tcpc_dev);
@@ -2801,9 +2799,7 @@ int tcpc_typec_handle_wd(struct tcpc_device *tcpc_dev, bool wd)
 	}
 
 #ifdef CONFIG_MTK_KERNEL_POWER_OFF_CHARGING
-	ret = get_boot_mode();
-	if (ret == KERNEL_POWER_OFF_CHARGING_BOOT ||
-	    ret == LOW_POWER_OFF_CHARGING_BOOT) {
+	if (tcpc_dev->bootmode == 8 || tcpc_dev->bootmode == 9) {
 		TYPEC_INFO("KPOC does not enter water protection\r\n");
 		goto out;
 	}
@@ -2833,6 +2829,7 @@ out:
 	}
 	return ret;
 }
+EXPORT_SYMBOL(tcpc_typec_handle_wd);
 #endif /* CONFIG_WATER_DETECTION */
 
 #ifdef CONFIG_CABLE_TYPE_DETECTION
