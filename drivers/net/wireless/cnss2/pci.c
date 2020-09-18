c6650e17df7a553bfeceb9a8783c15d27f5ac446
Lin Bai <lbai@codeaurora.org>
2020-09-18 14:36:08 +0800

    cnss2: Collect shadow registers for RDDM scenario
    Current implementation only collect shadow register when host
    force triggered RDDM.
    Extend it to any RDDM scenario.
    
    Change-Id: I8a14950b9b4cd42df016ecbb2eac6dc7d8ae115c
    Signed-off-by: Lin Bai <lbai@codeaurora.org>

--- drivers/net/wireless/cnss2/pci.c
+++ drivers/net/wireless/cnss2/pci.c
@@ -4209,6 +4209,7 @@ void cnss_pci_collect_dump_info(struct cnss_pci_data *pci_priv, bool in_panic)
 	}
 
 	cnss_pci_dump_misc_reg(pci_priv);
+	cnss_pci_dump_shadow_reg(pci_priv);
 	cnss_pci_dump_qdss_reg(pci_priv);
 
 	ret = mhi_download_rddm_img(pci_priv->mhi_ctrl, in_panic);
