c8f7d95b7134ca9d093ddca53fed0086b40151b9
Ming-Fan Chen <ming-fan.chen@mediatek.com>
2020-09-20 01:49:21 +0800

    [ALPS05347630] cmdq: free secure metadata when pkt destroy
    1. Free secure metadata when pkt destroy
    2. Remove CMDQ_MMPROFILE_SUPPORT
    
    MTK-Commit-Id: 9e4c8ba4646c16452364bc496bac8f5833c0282d
    
    Change-Id: Idf80966aa428c36754e071da21bf67aa4e5a24a1
    CR-Id: ALPS05347630
    Feature: [Module]CMDQ Driver
    Signed-off-by: Ming-Fan Chen <ming-fan.chen@mediatek.com>

--- drivers/soc/mediatek/mtk-cmdq-helper.c
+++ drivers/soc/mediatek/mtk-cmdq-helper.c
@@ -528,6 +528,12 @@ void cmdq_pkt_destroy(struct cmdq_pkt *pkt)
 		mutex_lock(&client->chan_mutex);
 	cmdq_pkt_free_buf(pkt);
 	kfree(pkt->flush_item);
+#if IS_ENABLED(CONFIG_MTK_CMDQ_MBOX_EXT)
+#if IS_ENABLED(CONFIG_MTK_SEC_VIDEO_PATH_SUPPORT) || \
+	IS_ENABLED(CONFIG_MTK_CAM_SECURITY_SUPPORT)
+	cmdq_sec_pkt_free_data(pkt);
+#endif
+#endif
 	kfree(pkt);
 	if (client)
 		mutex_unlock(&client->chan_mutex);
