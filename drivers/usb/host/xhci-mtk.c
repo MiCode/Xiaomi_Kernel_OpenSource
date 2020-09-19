03ab0526fa41f2945083be9d195cff31c841a65f
Jeremy Chou <jeremy.chou@mediatek.com>
2020-09-20 01:47:20 +0800

    [ALPS05318039] OTG: update config for USB Audio
    [Detail]
    update config to fix noises in USB Audio.
    
    MTK-Commit-Id: b37402d91dadeee1eadb04f0e82177097220f7d1
    
    Change-Id: If3d34ce45d6579cfe6dc04d40ad952395a3df893
    Signed-off-by: Jeremy Chou <jeremy.chou@mediatek.com>
    CR-Id: ALPS05318039
    Feature: [Android Default] USB Audio

--- drivers/usb/host/xhci-mtk.c
+++ drivers/usb/host/xhci-mtk.c
@@ -34,6 +34,10 @@
 #include "xhci.h"
 #include "xhci-mtk.h"
 
+#if IS_ENABLED(CONFIG_MACH_MT6853)
+#include "mtu3_hal.h"
+#endif
+
 /* ip_pw_ctrl0 register */
 #define CTRL0_IP_SW_RST	BIT(0)
 
@@ -90,6 +94,81 @@
 #define HOST_CMD_TEST_SE0_NAK       0x3
 #define HOST_CMD_TEST_PACKET        0x4
 #define PMSC_PORT_TEST_CTRL_OFFSET  28
+
+/* frmcnt */
+#define INIT_FRMCNT_LEV1_FULL_RANGE 0x944
+#define INIT_FRMCNT_FRMCNT_C(x)	(((x) & 0xfff) << 8)
+
+#if IS_ENABLED(CONFIG_MACH_MT6853)
+static void xhci_mtk_frmcnt_range(struct usb_hcd *hcd, int speed)
+{
+	struct xhci_hcd *xhci = hcd_to_xhci(hcd);
+	u32 value;
+
+	value = readl(hcd->regs + INIT_FRMCNT_LEV1_FULL_RANGE);
+	value &= ~(INIT_FRMCNT_FRMCNT_C(0xfff));
+
+	if (speed == DEV_SPEED_FULL)
+		value |= INIT_FRMCNT_FRMCNT_C(0x144);
+	else
+		value |= INIT_FRMCNT_FRMCNT_C(0x12b);
+
+	writel(value, hcd->regs + INIT_FRMCNT_LEV1_FULL_RANGE);
+}
+#endif
+
+void xhci_mtk_set_port_mode(struct usb_hcd *hcd,
+		__le32 __iomem **port_array, int port_id)
+{
+#if IS_ENABLED(CONFIG_MACH_MT6853)
+	struct xhci_hcd_mtk *mtk = hcd_to_mtk(hcd);
+	struct xhci_hcd *xhci = hcd_to_xhci(mtk->hcd);
+	unsigned long flags;
+	u32 temp;
+	int old_speed;
+	int new_speed;
+
+	temp = readl(port_array[port_id]);
+	old_speed = mtk->last_speed;
+	new_speed = DEV_PORT_SPEED(temp);
+	mtk->last_speed = new_speed;
+	xhci_info(xhci, "old_speed=%d, new_speed=%d\n",
+			  old_speed, new_speed);
+
+	if (old_speed == new_speed || (old_speed != DEV_SPEED_FULL &&
+			new_speed != DEV_SPEED_FULL))
+		return;
+
+	xhci_set_link_state(xhci, port_array, port_id, XDEV_U3);
+
+	spin_unlock_irqrestore(&xhci->lock, flags);
+	if (readl_poll_timeout_atomic(port_array[port_id], temp,
+			(temp & PORT_PLS_MASK) == XDEV_U3, 100, 100000))
+		xhci_warn(xhci, "port suspend timeout\n");
+	spin_lock_irqsave(&xhci->lock, flags);
+
+	spin_unlock_irqrestore(&xhci->lock, flags);
+	ssusb_set_phy_mode(new_speed);
+	spin_lock_irqsave(&xhci->lock, flags);
+
+	xhci_mtk_frmcnt_range(hcd, new_speed);
+
+	xhci_set_link_state(xhci, port_array, port_id, XDEV_RESUME);
+	spin_unlock_irqrestore(&xhci->lock, flags);
+	mdelay(USB_RESUME_TIMEOUT);
+	spin_lock_irqsave(&xhci->lock, flags);
+
+	xhci_set_link_state(xhci, port_array, port_id, XDEV_U0);
+	spin_unlock_irqrestore(&xhci->lock, flags);
+	mdelay(50);
+	spin_lock_irqsave(&xhci->lock, flags);
+
+	temp = readl(port_array[port_id]);
+	if ((temp & PORT_PLS_MASK) != XDEV_U0)
+		xhci_warn(xhci, "error resume 0x%08x!!!\n", temp);
+#endif
+}
+
 
 static ssize_t xhci_mtk_test_mode_write(struct file *file,
 		const char __user *ubuf, size_t count, loff_t *ppos)
@@ -899,6 +978,13 @@ static int xhci_mtk_remove(struct platform_device *dev)
 	pm_runtime_disable(&dev->dev);
 
 	xhci->xhc_state |= XHCI_STATE_REMOVING;
+
+#if IS_ENABLED(CONFIG_MACH_MT6853)
+	if (mtk->last_speed == DEV_SPEED_FULL) {
+		ssusb_set_phy_mode(DEV_SPEED_INACTIVE);
+		xhci_mtk_frmcnt_range(hcd, DEV_SPEED_INACTIVE);
+	}
+#endif
 
 	usb_remove_hcd(shared_hcd);
 	xhci->shared_hcd = NULL;
