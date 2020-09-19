b43d618178cb2a951a8895225321eae366a42732
Terry Lo <Terry.Lo@mediatek.com>
2020-09-20 01:49:30 +0800

    [ALPS05275929] ISP: Fix ISP_StopHW
    [Detail]
    1. Fix ISP_StopHW
    
    MTK-Commit-Id: 6b2f10bc48b2588c746602394bdb11adc07228db
    
    Change-Id: I93611d29b79197239376fb2293f71148fdacafb3
    CR-Id: ALPS05275929
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: Terry Lo <Terry.Lo@mediatek.com>

--- drivers/misc/mediatek/cameraisp/src/mt6768/camera_isp.c
+++ drivers/misc/mediatek/cameraisp/src/mt6768/camera_isp.c
@@ -9709,7 +9709,7 @@ static inline void ISP_StopHW(signed int module)
 	do {
 		regTGSt = (ISP_RD32(CAM_REG_TG_INTER_ST(module)) & 0x00003F00)
 			   >> 8;
-		if (regTGSt == 1)
+		if (regTGSt == 1 || regTGSt == 0)
 			break;
 
 		pr_info("%s: wait 1VD (%d)\n", moduleName, loopCnt);
@@ -9724,17 +9724,10 @@ static inline void ISP_StopHW(signed int module)
 		/* timer*/
 		m_sec = ktime_get();
 
-		while (regTGSt != 1) {
-			regTGSt = (ISP_RD32(CAM_REG_TG_INTER_ST(module))
-				   & 0x00003F00) >> 8;
-			/*timer*/
-			sec = ktime_get();
-			/* wait time>timeoutMs, break */
-			if ((sec - m_sec) > timeoutMs)
-				break;
-		}
 		if (regTGSt == 1)
 			pr_info("%s: wait idle done\n", moduleName);
+		else if (regTGSt == 0)
+			pr_info("plz check regTGSt value");
 		else
 			pr_info("%s: wait idle timeout(%lld)\n", moduleName,
 				(sec - m_sec));
