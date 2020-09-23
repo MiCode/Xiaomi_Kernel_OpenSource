640a49b0bb6c207a70cefb076470bb391289a065
Stanley Chu <stanley.chu@mediatek.com>
2020-09-23 17:44:15 +0800

    blocktag: Add Block IO Tracer core and UFS plugin
    Add Block IO Tracer kernel module which includes core
    driver and UFS plugin.
    
    (cherry-picked from commit 188401bdabff3ee29f9b3084dd23b67f3d7d7b0a)
    Signed-off-by: Stanley Chu <stanley.chu@mediatek.com>
    Change-Id: I2abb1853b5cf58e7121585d0dfceaeb4e8ae53c3
    CR-Id: ALPS05386424
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

---
+++ drivers/misc/mediatek/blocktag/blocktag-ufs.h
@@ -0,0 +1,52 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2020 MediaTek Inc.
+ */
+
+#ifndef _UFS_MEDIATEK_TRACER_H
+#define _UFS_MEDIATEK_TRACER_H
+
+#include <linux/types.h>
+#include "mtk_blocktag.h"
+
+#if IS_ENABLED(CONFIG_MTK_BLOCK_IO_TRACER)
+
+#define UFS_BIOLOG_RINGBUF_MAX    120
+#define UFS_BIOLOG_CONTEXT_TASKS  32
+#define UFS_BIOLOG_CONTEXTS       1
+
+enum {
+	tsk_send_cmd = 0,
+	tsk_req_compl,
+	tsk_max
+};
+
+struct ufs_mtk_bio_context_task {
+	__u32 qid;
+	__u16 cmd;
+	__u16 len;
+	__u32 lba;
+	uint64_t t[tsk_max];
+};
+
+/* Context of Request Queue */
+struct ufs_mtk_bio_context {
+	int id;
+	int state;
+	pid_t pid;
+	__u16 qid;
+	__u16 q_depth;
+	spinlock_t lock;
+	uint64_t busy_start_t;
+	uint64_t period_start_t;
+	uint64_t period_end_t;
+	uint64_t period_usage;
+	struct ufs_mtk_bio_context_task task[UFS_BIOLOG_CONTEXT_TASKS];
+	struct mtk_btag_workload workload;
+	struct mtk_btag_throughput throughput;
+	struct mtk_btag_pidlogger pidlog;
+};
+
+#endif
+
+#endif
