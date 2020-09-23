c0c8df53f09218831e713a7376dcd3b761c65bca
Chao Hao <chao.hao@mediatek.com>
2020-09-23 15:07:50 +0800

    iommu/mediatek: fix iommu suspend/resume issue
    [Detail]
    For mt6873, iommu runs suspend/resume by pm_runtime and doesn't
    need to suspend/resume when system suspend/resume. So when system
    suspend/resume, iommu does nothing to avoid devapc issue
    
    (cherry-picked from commit 79cb08feacdc870cdf4d8603fa34be45f223dbb6)
    Signed-off-by: Chao Hao <chao.hao@mediatek.com>
    Change-Id: I78dc173a393ee0696e04b36d6aca3738772644ed
    CR-Id: ALPS05377375
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

--- drivers/iommu/mtk_iommu.c
+++ drivers/iommu/mtk_iommu.c
@@ -926,7 +926,7 @@ static int mtk_iommu_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static int __maybe_unused mtk_iommu_suspend(struct device *dev)
+static int __maybe_unused mtk_iommu_runtime_suspend(struct device *dev)
 {
 	struct mtk_iommu_data *data = dev_get_drvdata(dev);
 	struct mtk_iommu_suspend_reg *reg = &data->reg;
@@ -944,7 +944,18 @@ static int __maybe_unused mtk_iommu_suspend(struct device *dev)
 	return 0;
 }
 
-static int __maybe_unused mtk_iommu_resume(struct device *dev)
+static int __maybe_unused mtk_iommu_suspend(struct device *dev)
+{
+	int ret = 0;
+	struct mtk_iommu_data *data = dev_get_drvdata(dev);
+
+	if (!data->plat_data->has_bclk)
+		ret = mtk_iommu_runtime_suspend(dev);
+
+	return ret;
+}
+
+static int __maybe_unused mtk_iommu_runtime_resume(struct device *dev)
 {
 	struct mtk_iommu_data *data = dev_get_drvdata(dev);
 	struct mtk_iommu_suspend_reg *reg = &data->reg;
@@ -971,8 +982,19 @@ static int __maybe_unused mtk_iommu_resume(struct device *dev)
 	return 0;
 }
 
+static int __maybe_unused mtk_iommu_resume(struct device *dev)
+{
+	int ret = 0;
+	struct mtk_iommu_data *data = dev_get_drvdata(dev);
+
+	if (!data->plat_data->has_bclk)
+		ret = mtk_iommu_resume(dev);
+
+	return ret;
+}
+
 static const struct dev_pm_ops mtk_iommu_pm_ops = {
-	SET_RUNTIME_PM_OPS(mtk_iommu_suspend, mtk_iommu_resume, NULL)
+	SET_RUNTIME_PM_OPS(mtk_iommu_runtime_suspend, mtk_iommu_runtime_resume, NULL)
 	SET_NOIRQ_SYSTEM_SLEEP_PM_OPS(mtk_iommu_suspend, mtk_iommu_resume)
 };
 
