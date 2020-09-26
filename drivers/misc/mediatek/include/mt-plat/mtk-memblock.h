c8ec11ecf94660dda12464ba433ff77bb694125c
Stanley Chu <stanley.chu@mediatek.com>
2020-09-26 23:04:05 +0800

    memblock: Add memory block module
    Add memory block module.
    
    (cherry-picked from commit 16893e2b59c89cce91b54cd2fceb18d430d1eef6)
    Signed-off-by: Stanley Chu <stanley.chu@mediatek.com>
    Change-Id: I672092a181bb80ae831ce036955d599055a135b3
    CR-Id: ALPS05386424
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    (cherry picked from commit 8cdae29fc8ae70540c5b93ac9b34d7e04cd8ada1)

---
+++ drivers/misc/mediatek/include/mt-plat/mtk-memblock.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2020 MediaTek Inc.
+ */
+
+#ifndef __MTK_MEMORY_BLOCK__
+#define __MTK_MEMORY_BLOCK__
+
+extern phys_addr_t mb_start_of_DRAM(void);
+extern phys_addr_t mb_end_of_DRAM(void);
+
+#endif
