dffe35b1804cf76f6031778e5e613395f22b4502
Greg Kroah-Hartman <gregkh@google.com>
2020-10-01 18:06:51 +0200

    Revert "exec: Add exec_update_mutex to replace cred_guard_mutex"
    This reverts commit b796d94921ce2adffcb30d25381bcb88a9d77f60 which is
    commit eea9673250db4e854e9998ef9da6d4584857f0ea.
    
    It breaks the API and we don't need it right now in the 5.4 tree.
    
    Bug: 161946584
    Change-Id: I28f98480e9f4a8599b7cfe887401d7515bf5360e
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -44,13 +44,7 @@ struct linux_binprm {
 		 * exec has happened. Used to sanitize execution environment
 		 * and to set AT_SECURE auxv for glibc.
 		 */
-		secureexec:1,
-		/*
-		 * Set by flush_old_exec, when exec_mmap has been called.
-		 * This is past the point of no return, when the
-		 * exec_update_mutex has been taken.
-		 */
-		called_exec_mmap:1;
+		secureexec:1;
 #ifdef __alpha__
 	unsigned int taso:1;
 #endif
