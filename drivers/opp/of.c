d00a32cfdc907f98be283a52852423cb3ff7dab2
Greg Kroah-Hartman <gregkh@google.com>
2020-10-01 20:54:41 +0200

    Revert "opp: Replace list_kref with a local counter"
    This reverts commit 9f7d28075ae4a3679e821051e0a69f431ca3afff which is
    commit 03758d60265c773e1d06d436b99ee338f2ac55d6 upstream.
    
    It breaks the API and we don't need it right now in the Android  5.4
    tree.
    
    Bug: 161946584
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I278601a1f26e752ddc10bcb8d9d29a0b21dc6078

--- drivers/opp/of.c
+++ drivers/opp/of.c
@@ -658,15 +658,17 @@ static int _of_add_opp_table_v2(struct device *dev, struct opp_table *opp_table)
 	struct dev_pm_opp *opp;
 
 	/* OPP table is already initialized for the device */
-	mutex_lock(&opp_table->lock);
 	if (opp_table->parsed_static_opps) {
-		opp_table->parsed_static_opps++;
-		mutex_unlock(&opp_table->lock);
+		kref_get(&opp_table->list_kref);
 		return 0;
 	}
 
-	opp_table->parsed_static_opps = 1;
-	mutex_unlock(&opp_table->lock);
+	/*
+	 * Re-initialize list_kref every time we add static OPPs to the OPP
+	 * table as the reference count may be 0 after the last tie static OPPs
+	 * were removed.
+	 */
+	kref_init(&opp_table->list_kref);
 
 	/* We have opp-table node now, iterate over it and add OPPs */
 	for_each_available_child_of_node(opp_table->np, np) {
@@ -676,7 +678,7 @@ static int _of_add_opp_table_v2(struct device *dev, struct opp_table *opp_table)
 			dev_err(dev, "%s: Failed to add OPP, %d\n", __func__,
 				ret);
 			of_node_put(np);
-			goto remove_static_opp;
+			goto put_list_kref;
 		} else if (opp) {
 			count++;
 		}
@@ -685,7 +687,7 @@ static int _of_add_opp_table_v2(struct device *dev, struct opp_table *opp_table)
 	/* There should be one of more OPP defined */
 	if (WARN_ON(!count)) {
 		ret = -ENOENT;
-		goto remove_static_opp;
+		goto put_list_kref;
 	}
 
 	list_for_each_entry(opp, &opp_table->opp_list, node)
@@ -696,16 +698,18 @@ static int _of_add_opp_table_v2(struct device *dev, struct opp_table *opp_table)
 		dev_err(dev, "Not all nodes have performance state set (%d: %d)\n",
 			count, pstate_count);
 		ret = -ENOENT;
-		goto remove_static_opp;
+		goto put_list_kref;
 	}
 
 	if (pstate_count)
 		opp_table->genpd_performance_state = true;
 
+	opp_table->parsed_static_opps = true;
+
 	return 0;
 
-remove_static_opp:
-	_opp_remove_all_static(opp_table);
+put_list_kref:
+	_put_opp_list_kref(opp_table);
 
 	return ret;
 }
@@ -742,7 +746,7 @@ static int _of_add_opp_table_v1(struct device *dev, struct opp_table *opp_table)
 		if (ret) {
 			dev_err(dev, "%s: Failed to add OPP %ld (%d)\n",
 				__func__, freq, ret);
-			_opp_remove_all_static(opp_table);
+			_put_opp_list_kref(opp_table);
 			return ret;
 		}
 		nr -= 2;
