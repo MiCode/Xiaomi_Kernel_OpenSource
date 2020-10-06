36c546d3b8ec8fa2469c09dd395613409444e6a6
Divya Sharma <divyash@codeaurora.org>
2020-10-05 18:38:55 -0700

    Revert "cpuidle: lpm-levels: Core isolation check to get PM QoS latency"
    This reverts commit e198f82fe5a96aee08377b9a0a30a6f13ea17d24.
    
    Change-Id: I9df86352cc1b00de781cc6f0a088c3dbd851c433
    Signed-off-by: Divya Sharma <divyash@codeaurora.org>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -149,8 +149,7 @@ static int lpm_cpu_qos_notify(struct notifier_block *nb,
 	int cpu = nb - dev_pm_qos_nb;
 
 	preempt_disable();
-	if (cpu != smp_processor_id() && cpu_online(cpu) &&
-	    !check_cpu_isolated(cpu))
+	if (cpu != smp_processor_id() && cpu_online(cpu))
 		wake_up_if_idle(cpu);
 	preempt_enable();
 
@@ -658,8 +657,6 @@ static inline uint32_t get_cpus_qos(const struct cpumask *mask)
 	uint32_t latency = PM_QOS_CPU_DMA_LAT_DEFAULT_VALUE;
 
 	for_each_cpu(cpu, mask) {
-		if (check_cpu_isolated(cpu))
-			continue;
 		n = cpuidle_governor_latency_req(cpu);
 		if (n < latency)
 			latency = n;
