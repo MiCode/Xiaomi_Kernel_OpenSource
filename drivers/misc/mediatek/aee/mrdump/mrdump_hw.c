c735c53ac5d1bf7fcbc25ee3a3b86a05fcff7280
mtk03113 <nicholas.tang@mediatek.com>
2020-10-06 12:33:55 +0800

    mrdump: Remove MTK_WATCHDOG kernel config reference
    MTK_WATCHDOG kernel config already removed, remove useless code
    from mrdump module.
    
    (cherry-picked from commit dbf6e58cc6eb8de910230685400fef4269941d6b)
    Change-Id: Iafa9c8c49eeb94e99c31d8913013d4443da5c32f
    CR-Id: ALPS05387457
    Feature: Android Exception Engine(AEE)
    Signed-off-by: mtk03113 <nicholas.tang@mediatek.com>
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

--- drivers/misc/mediatek/aee/mrdump/mrdump_hw.c
+++ drivers/misc/mediatek/aee/mrdump/mrdump_hw.c
@@ -17,32 +17,6 @@
 #include <dbgtop.h>
 #endif
 
-#if IS_ENABLED(CONFIG_MTK_WATCHDOG)
-#include <mtk_wd_api.h>
-
-static void mrdump_wd_dram_reserved_mode(bool drm_ready)
-{
-	int res;
-	struct wd_api *wd_api;
-
-	res = get_wd_api(&wd_api);
-	if (res < 0) {
-		pr_notice("%s: get wd api error (%d)\n", __func__, res);
-	} else {
-		if (drm_ready) {
-			res = wd_api->wd_dram_reserved_mode(drm_enabled);
-			pr_notice("%s: DDR reserved mode enabled\n",
-				  __func__);
-		} else {
-			pr_notice("%s: DDR reserved mode not ready, disabled\n",
-				  __func__);
-		}
-		pr_notice("%s: config is not enabled yet\n", __func__);
-	}
-}
-
-#endif
-
 int __init mrdump_hw_init(bool drm_ready)
 {
 #if IS_ENABLED(CONFIG_MTK_DBGTOP)
@@ -54,10 +28,6 @@ int __init mrdump_hw_init(bool drm_ready)
 			pr_info("%s: mtk_dbgtop_dram_reserved failed\n",
 				__func__);
 	}
-#else
-#if IS_ENABLED(CONFIG_MTK_WATCHDOG)
-	mrdump_wd_dram_reserved_mode(drm_enabled);
-#endif
 #endif
 
 #if IS_ENABLED(CONFIG_MTK_DFD_INTERNAL_DUMP)
