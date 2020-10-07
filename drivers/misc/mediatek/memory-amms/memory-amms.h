c4a8cef8135620d199da752e745a67e3a384e0aa
Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
2020-10-07 21:16:17 +0800

    amms: mediatek: add amms driver
    add amms driver for mediatek platform
    
    (cherry-picked from commit 8a42079b948937eef180c5152c40daa56e3e04af)
    Signed-off-by: Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
    Change-Id: I70e0bd58c533472f67327170f11a5752d0ec37f9
    CR-Id: ALPS05396263
    Feature: AMMS (AP and Modem Memory Sharing)
    Signed-off-by: Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
    (cherry picked from commit 4009a047885e97d64798b23e97b7b3aee303bd43)

---
+++ drivers/misc/mediatek/memory-amms/memory-amms.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#ifndef _MEMORY_AMMS_H_
+#define _MEMORY_AMMS_H_
+int free_reserved_memory(phys_addr_t start_phys,
+		phys_addr_t end_phys);
+#endif /* end of _MEMORY_AMMS_H_ */
