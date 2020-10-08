4a851779b129249a41484d50a1e354ea90c7a5ba
Neeraj Upadhyay <neeraju@codeaurora.org>
2020-10-08 14:39:05 +0530

    dt-bindings: ipcc: Rename WDSP client to WPSS
    Rename WDSP to WPSS, as per new naming requirement.
    
    Change-Id: If44d6d2e2233b0f2c1cd754216e121494ab005f0
    Signed-off-by: Neeraj Upadhyay <neeraju@codeaurora.org>

--- include/dt-bindings/soc/qcom,ipcc.h
+++ include/dt-bindings/soc/qcom,ipcc.h
@@ -34,7 +34,7 @@
 #define IPCC_CLIENT_PCIE1		14
 #define IPCC_CLIENT_PCIE2		15
 #define IPCC_CLIENT_SPSS		16
-#define IPCC_CLIENT_WDSP		24
+#define IPCC_CLIENT_WPSS		24
 #define IPCC_CLIENT_MAX			25 /* Used by driver only */
 
 #endif
