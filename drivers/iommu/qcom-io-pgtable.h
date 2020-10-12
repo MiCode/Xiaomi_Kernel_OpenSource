ef579906a478e9fbb21a59dcc3876ac86d865298
Patrick Daly <pdaly@codeaurora.org>
2020-10-12 10:50:48 -0700

    qcom-io-pgtable: Add back definitions for qcom io-pgtable quirks
    Prepare for the fastmap dma implementation by restoring some macro
    definitions it requires. These definitions were originally part of
    io-pgtable.h, but since they are are only supported by qcom io-pgtable
    drivers, move them to a qcom specific header.
    
    Change-Id: I02f84d2a021e4a86e403769b8dc5b5b2a39d174e
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

---
+++ drivers/iommu/qcom-io-pgtable.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ */
+#ifndef __QCOM_QCOM_IO_PGTABLE_H
+#define __QCOM_QCOM_IO_PGTABLE_H
+
+#define IO_PGTABLE_QUIRK_QCOM_USE_UPSTREAM_HINT BIT(31)
+#define IO_PGTABLE_QUIRK_QCOM_USE_LLC_NWA       BIT(30)
+
+#endif /* __QCOM_QCOM_IO_PGTABLE_H */
