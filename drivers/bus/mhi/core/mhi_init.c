7e01cae53b14e6ac4aa96cb4477e89df321731df
Hemant Kumar <hemantk@codeaurora.org>
2020-10-12 17:54:56 -0700

    mhi: core: Add missing header file to fix compilation error
    Include arch_timer.h to define arch_timer_get_cntfrq().
    
    Change-Id: Ia2d3384bfc84df68561fcbe05cd88cd7c50ccb1e
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>

--- drivers/bus/mhi/core/mhi_init.c
+++ drivers/bus/mhi/core/mhi_init.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /* Copyright (c) 2018-2020, The Linux Foundation. All rights reserved. */
 
+#include <asm/arch_timer.h>
 #include <linux/debugfs.h>
 #include <linux/device.h>
 #include <linux/dma-direction.h>
