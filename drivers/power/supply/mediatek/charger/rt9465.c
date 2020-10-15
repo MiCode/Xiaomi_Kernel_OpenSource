42d6441ded51fec5895a1aab3038676493a2e5d3
Lucas Tsai <cpi00079@mediatek.com>
2020-10-15 16:01:40 +0800

    [ALPS05129558] Charger: fix coverity issues
    [Detail]
    1. Fix array indexing and return value checking of rt9465.c
    2. Fix dead code of tcpc_mt6370.c
    
    MTK-Commit-Id: cef9d84da81978f83b5a4f24cc3a7479577a7881
    
    Change-Id: I6ec19c4f17e74bec581ffaec19bf4365c061672b
    Signed-off-by: Lucas Tsai <cpi00079@mediatek.com>
    CR-Id: ALPS05129558
    Feature: Charger
    (cherry picked from commit 10af888eb609c95ce2ec10aa2926741a31c8fafc)

--- drivers/power/supply/mediatek/charger/rt9465.c
+++ drivers/power/supply/mediatek/charger/rt9465.c
@@ -729,21 +729,9 @@ static inline const char *rt9465_get_irq_name(struct rt9465_info *info,
 	return "not found";
 }
 
-static inline void rt9465_irq_mask(struct rt9465_info *info, int irqnum)
+static inline void rt9465_irq_unmask(struct rt9465_info *info,
+	unsigned int irqnum)
 {
-	if (irqnum < 0)
-		return;
-
-	dev_dbg(info->dev, "%s: irq = %d, %s\n", __func__, irqnum,
-		rt9465_get_irq_name(info, irqnum));
-	rt9465_irqmask[irqnum / 8] |= (1 << (irqnum % 8));
-}
-
-static inline void rt9465_irq_unmask(struct rt9465_info *info, int irqnum)
-{
-	if (irqnum < 0)
-		return;
-
 	dev_dbg(info->dev, "%s: irq = %d, %s\n", __func__, irqnum,
 		rt9465_get_irq_name(info, irqnum));
 	rt9465_irqmask[irqnum / 8] &= ~(1 << (irqnum % 8));
@@ -863,9 +851,8 @@ static int rt9465_register_irq(struct rt9465_info *info)
 	len = strlen(info->desc->chg_dev_name);
 	name = devm_kzalloc(info->dev, len + 10, GFP_KERNEL);
 	ret = snprintf(name, len + 10, "%s_irq_gpio", info->desc->chg_dev_name);
-	if (ret < 0 || ret > (len + 10))
-		goto err;
-
+	if (ret >= (len + 10))
+		chr_info("%s: name truncated\n", __func__);
 	ret = devm_gpio_request_one(info->dev, info->intr_gpio, GPIOF_IN, name);
 	if (ret < 0) {
 		chr_err("%s: gpio request fail\n", __func__);
