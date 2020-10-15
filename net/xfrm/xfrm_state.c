cb5dbbc5c9eae21c01dd2a0bb25c9a3e4a0712f9
zhuoliang zhang <zhuoliang.zhang@mediatek.com>
2020-10-15 16:02:27 +0800

    [ALPS05388169] xfrm: fix a race condition bug
    [Detail] user process call function xfrm_alloc_userspi,  call flow:
    xfrm_alloc_userspi
    	-> __find_acq_core
    		->xfrm_state_alloc:alloc  new xfrm_ state: x
    		-> xfrm_hash_grow_check(trigger hash resize work)
    	->xfrm_alloc_spi ->waiting for spinlock:net->xfrmstate_lock
    	 (hold by hash resize work thread)
    hash resize work is sheculed at the same time , call flow :
     	->xfrm_hash_resize
    		->hold spin_lock:net->xfrm_state_lock
    		->xfrm_hash_transfer
    			-> add x into hlist:net->xfrm.state_byspi
    		->release spinlock:net->xfrm_state_lock
    
    user process get the spinlock and contiue to run
    	->xfrm_alloc_spi
    		-> add x into thlist: net->xfrm.state_byspi
    so the new xfrm_state (x) is added in the same hlist two times and this
    make hlist become an infinit loop list.
    
    this patch changes the x->id.spi after xfrm_alloc_spi hold the spinlock,
    Hash resize work thread can no longer add x into the hlist
    because x->id.spi is zero;
    
    MTK-Commit-Id: f86fcc42f6d79b31aa7820c92afad5356ff97ba0
    
    Change-Id: Icd1b7f0fe579b9b10061ddfd321954a1774a6615
    Signed-off-by: zhuoliang zhang <zhuoliang.zhang@mediatek.com>
    CR-Id: ALPS05388169
    Feature: [Module]IP Networking
    (cherry picked from commit 8deb113a5679df886f9ca169d419a230c41e630c)

--- net/xfrm/xfrm_state.c
+++ net/xfrm/xfrm_state.c
@@ -2016,6 +2016,7 @@ int xfrm_alloc_spi(struct xfrm_state *x, u32 low, u32 high)
 	int err = -ENOENT;
 	__be32 minspi = htonl(low);
 	__be32 maxspi = htonl(high);
+	__be32 newspi = 0;
 	u32 mark = x->mark.v & x->mark.m;
 
 	spin_lock_bh(&x->lock);
@@ -2034,34 +2035,25 @@ int xfrm_alloc_spi(struct xfrm_state *x, u32 low, u32 high)
 			xfrm_state_put(x0);
 			goto unlock;
 		}
-		x->id.spi = minspi;
+		newspi = minspi;
 	} else {
 		u32 spi = 0;
 		for (h = 0; h < high-low+1; h++) {
 			spi = low + prandom_u32()%(high-low+1);
 			x0 = xfrm_state_lookup(net, mark, &x->id.daddr, htonl(spi), x->id.proto, x->props.family);
 			if (x0 == NULL) {
-				x->id.spi = htonl(spi);
+				newspi = htonl(spi);
 				break;
 			}
 			xfrm_state_put(x0);
 		}
 	}
-	if (x->id.spi) {
+	if (newspi) {
 		spin_lock_bh(&net->xfrm.xfrm_state_lock);
+		x->id.spi = newspi;
 		h = xfrm_spi_hash(net, &x->id.daddr, x->id.spi, x->id.proto, x->props.family);
-		xfrm_state_get_back_trace(&x->xfrm_trace);
-		format_trace_info();
-		xfrm_state_check_add_byspi_hlish(net->xfrm.state_byspi + h, x, dmsg);
 		hlist_add_head_rcu(&x->byspi, net->xfrm.state_byspi + h);
-#ifdef MTK_XFM_DEBUG
-		pr_info("[mtk_net][xfrm_state]add list  %s x %px byspi %px  h %d\n",
-			__func__, x, net->xfrm.state_byspi, h);
-#endif
-		format_trace_info();
-		xfrm_state_check_add_byspi_hlish(net->xfrm.state_byspi + h, NULL, dmsg);
 		spin_unlock_bh(&net->xfrm.xfrm_state_lock);
-
 		err = 0;
 	}
 
