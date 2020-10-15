497bc97570fcf55a36ebce550251d7ee81100e6d
weichuan.lin <weichuan.lin@mediatek.com>
2020-10-15 16:02:13 +0800

    [ALPS05387186] eemgpu: sync eemgpu driver from dvt
    1. phase out ptp init01 phase
    2. sync eemgpu driver from dvt that verified
    4. modify show content in eemg_cur_volt node
    3. remove non-used define
    
    MTK-Commit-Id: 7bfb8be152c41ff48602ec7c729462727f397f49
    
    Change-Id: I053056858eb817b1d3a4e92e5407d7f3180a551a
    CR-Id: ALPS05387186
    Feature: GPU PTP-OD
    Signed-off-by: weichuan.lin <weichuan.lin@mediatek.com>

---
+++ drivers/misc/mediatek/base/power/eemgpu_v2/mt6833/inc/mtk_eemgpu_prj_config.h
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2020 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+
+#ifndef __MTK_EEMGPU_PRJ_CONFIG_H__
+#define __MTK_EEMGPU_PRJ_CONFIG_H__
+
+/*
+ * ##########################
+ * build env control
+ * ############################
+ */
+#define ENABLE_GPU (1)
+#define EN_EEMGPU (1) /* enable/disable EEM (SW) */
+#define DVT	(0)
+#define EARLY_PORTING	(0)
+
+/*
+ * ##########################
+ * debug log control
+ * ############################
+ */
+#define CONFIG_EEMG_SHOWLOG (0)
+#define EN_ISR_LOG (0)
+#define DUMP_DATA_TO_DE	(0)
+//#define CTP_EEMG_DUMP
+
+/*
+ * ##########################
+ * EFUSE feature
+ * ############################
+ */
+#define EEMG_FAKE_EFUSE		(0)
+
+/*
+ * ##########################
+ * PTP feature
+ * ############################
+ */
+#define ENABLE_LOO_G (1)
+#define ENABLE_LOO (1)
+#define SUPPORT_GPU_VB	(1)	/* gpu voltage bin */
+#define EEMG_DISABLE_DRCC (0)
+
+/*
+ * ##########################
+ * GPU DVFS feature
+ * ############################
+ */
+//#define EARLY_PORTING_GPU
+#define NR_FREQ 16
+#define NR_FREQ_GPU 16
+#define GPU_FREQ_BASE 1000000
+#define GPU_M_FREQ_BASE 700000
+
+/*
+ * ##########################
+ * PMIC feature
+ * ############################
+ */
+//#define EARLY_PORTING_PMIC
+#define SET_PMIC_VOLT		(1)
+#define SET_PMIC_VOLT_TO_DVFS	(1)
+
+/*
+ * ##########################
+ * Thermal feature
+ * ############################
+ */
+//#define EARLY_PORTING_THERMAL
+#define LVTS_THERMAL (1)
+#define GPU_THERMAL_BANK THERMAL_BANK2 /* defined in mtk_thermal.h */
+
+/*
+ * ##########################
+ * Temperature feature
+ * ############################
+ */
+#define LOW_TEMP_VAL		(25000)
+#define EXTRA_LOW_TEMP_VAL	(10000)
+#define HIGH_TEMP_VAL		(85000)
+
+/*
+ * ##########################
+ * phase out define
+ * ############################
+ */
+/* PTP feature */
+#define ENABLE_CPU (0)
+#define ENABLE_MDLA (0)
+#define ENABLE_VPU (0)
+#define ENABLE_LOO_B (0)
+
+#endif
+
