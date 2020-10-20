2d85d1a365e0129660fe2155be3167fce16f30f6
Freddy Hsin <freddy.hsin@mediatek.com>
2020-10-20 16:41:47 +0800

    driver: watchdog: add cpu info for watchdog kicker
    Add cpu info for watchdog kicker
    
    (cherry-picked from commit 025e41d7f6f89f38e275073b2c85ecfb25ebd613)
    Signed-off-by: Freddy Hsin <freddy.hsin@mediatek.com>
    Change-Id: I0d6be92da38f8b7c5b2ae512881bc73f04c3ec8e
    CR-Id: ALPS05420447
    Feature: TrustKernel TEE
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    (cherry picked from commit a7ee312a12cf14a4cce8eee0a4c4b09e70c0c54f)

--- drivers/watchdog/mtk_wdt.c
+++ drivers/watchdog/mtk_wdt.c
@@ -223,7 +223,7 @@ static int mtk_wdt_ping(struct watchdog_device *wdt_dev)
 	void __iomem *wdt_base = mtk_wdt->wdt_base;
 
 	iowrite32(WDT_RST_RELOAD, wdt_base + WDT_RST);
-	pr_info("[wdtk] kick watchdog\n");
+	pr_info("[wdtk] kick watchdog cpu%d\n", smp_processor_id());
 
 	return 0;
 }
