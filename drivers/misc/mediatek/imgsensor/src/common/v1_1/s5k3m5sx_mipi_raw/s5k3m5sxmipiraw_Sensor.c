3c0edae42686f6d2e9e040c8a3e9d81c1ed0c10b
Bella-YJ Chen <bella-yj.chen@mediatek.com>
2020-10-21 09:40:20 +0800

    [ALPS05369426] cam: fix s5k3m5 framerate setting
    [Detail] fix framerate setting
    
    MTK-Commit-Id: 9d9c42a4b310ad9bcb0cfc2a073b8b0c2c924d1f
    
    Change-Id: If93687ce94f8b7347605bc91f0b88a7b7199f582
    CR-Id: ALPS05369426
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: Bella-YJ Chen <bella-yj.chen@mediatek.com>
    (cherry picked from commit b25f2049b3ecd1df59d4c5672344dac3802601b0)

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/s5k3m5sx_mipi_raw/s5k3m5sxmipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/s5k3m5sx_mipi_raw/s5k3m5sxmipiraw_Sensor.c
@@ -3021,10 +3021,6 @@ static kal_uint32 set_max_framerate_by_scenario(
 			set_dummy();
 		break;
 	case MSDK_SCENARIO_ID_CAMERA_CAPTURE_JPEG:
-		if (imgsensor.current_fps != imgsensor_info.cap.max_framerate) {
-			LOG_INF(
-			"Warning: current_fps %d fps is not support, so use cap's setting: %d fps!\n"
-			, framerate, imgsensor_info.cap.max_framerate/10);
 		frame_length = imgsensor_info.cap.pclk / framerate * 10
 				/ imgsensor_info.cap.linelength;
 		spin_lock(&imgsensor_drv_lock);
@@ -3036,7 +3032,6 @@ static kal_uint32 set_max_framerate_by_scenario(
 				+ imgsensor.dummy_line;
 			imgsensor.min_frame_length = imgsensor.frame_length;
 			spin_unlock(&imgsensor_drv_lock);
-	}
 
 		if (imgsensor.frame_length > imgsensor.shutter)
 			set_dummy();
