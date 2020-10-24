206e048a123b681519ca2173c78947578d9dde9d
Kishore Sri venkata Ganesh Bolisetty <bsrivenk@codeaurora.org>
2020-10-24 09:06:51 -0700

    soc: qcom: msm_perf: enable msm_perf driver on default config
    carve out frequency boost functionality of msm_perf driver
    and enable it on non perf images too.
    
    Change-Id: I2a0af988edf04a88d2fc16018f50830a0ab73220
    Signed-off-by: Kishore Sri venkata Ganesh Bolisetty <bsrivenk@codeaurora.org>

--- include/soc/qcom/msm_performance.h
+++ include/soc/qcom/msm_performance.h
@@ -17,7 +17,7 @@ enum evt_update_t {
 	MSM_PERF_GFX,
 };
 
-#ifdef CONFIG_MSM_PERFORMANCE
+#if IS_ENABLED(CONFIG_MSM_PERFORMANCE) && IS_ENABLED(CONFIG_MSM_PERFORMANCE_QGKI)
 void msm_perf_events_update(enum evt_update_t update_typ,
 			enum gfx_evt_t evt_typ, pid_t pid,
 			uint32_t ctx_id, uint32_t timestamp);
