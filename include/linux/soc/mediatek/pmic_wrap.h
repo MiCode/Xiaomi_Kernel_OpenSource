afa90b0a5716749b159a46a633016e046b297054
Jeter Chen <jeter.chen@mediatek.com>
2020-11-04 14:13:38 +0800

    [ALPS05411486] pmic_wrap:disable pwrap before WDT reset
    [Detail]
    If clock is stopped during pwrap is working,
    pwrap salve side receives a incomplete command,
    pwrap slave side will be abnormal.
    
    [Solution]
    Disable pmic wrap before system WDT reset.
    
    MTK-Commit-Id: b3425424c760d60046edd50116466315a2a45720
    
    Change-Id: I0554cda008d2daeae829572c212831776d939537
    Signed-off-by: Jeter Chen <jeter.chen@mediatek.com>
    CR-Id: ALPS05411486
    Feature: [Module]PMIC

--- include/linux/soc/mediatek/pmic_wrap.h
+++ include/linux/soc/mediatek/pmic_wrap.h
@@ -15,5 +15,6 @@
 #define __SOC_MEDIATEK_PMIC_WRAP_H
 
 extern struct regmap *pwrap_node_to_regmap(struct device_node *np);
+extern void pwrap_disable(void);
 
 #endif /* __SOC_MEDIATEK_PMIC_WRAP_H */
