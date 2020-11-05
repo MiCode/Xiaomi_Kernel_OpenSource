23d5ae7b67239030cafed0ba28028f5fb83047c7
Cheng Jui Wang <cheng-jui.wang@mediatek.com>
2020-11-05 15:16:41 +0800

    mtprof: Introduce MTK IRQ monitor
    Introduce MTK IRQ Monitor, which can monitor ISR/SOFTIRQ process
    duration, irq disable/enable duration, and burst IRQs.
    
    Add CONFIG_MTK IRQ_MONITOR for IRQ Monitor.
    
    (cherry-picked from commit dc56605ef430dab3655585fb65a4ec127303b092)
    Signed-off-by: Cheng Jui Wang <cheng-jui.wang@mediatek.com>
    Change-Id: If2a9a2c8c19ec7bd27406748a5225828e0af8180
    CR-Id: ALPS05438846
    Feature: Mediatek Profiling Tools(mtprof)
    Signed-off-by: Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>

---
+++ drivers/misc/mediatek/mtprof/irq_monitor_main.c
@@ -0,0 +1,467 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2020 MediaTek Inc.
+ */
+
+#ifdef pr_fmt
+#undef pr_fmt
+#endif
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/interrupt.h>
+#include <linux/irq.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/proc_fs.h>
+#include <linux/sched/clock.h>
+#include <linux/slab.h>
+#include <linux/stacktrace.h>
+#include <linux/tracepoint.h>
+
+#include <trace/events/ipi.h>
+#include <trace/events/irq.h>
+#include <trace/events/preemptirq.h>
+
+#include "internal.h"
+
+#ifdef MODULE
+const char * const softirq_to_name[NR_SOFTIRQS] = {
+	"HI", "TIMER", "NET_TX", "NET_RX", "BLOCK", "IRQ_POLL",
+	"TASKLET", "SCHED", "HRTIMER", "RCU"
+};
+#endif
+
+struct irq_mon_tracer {
+	bool tracing;
+	unsigned int th1_ms;          /* ftrace */
+	unsigned int th2_ms;          /* kernel log */
+	unsigned int th3_ms;          /* aee */
+	unsigned int aee_limit;
+	unsigned int aee_debounce_ms;
+};
+
+static struct irq_mon_tracer irq_handler_tracer __read_mostly = {
+	.tracing = true,
+	.th1_ms = 100,
+	.th2_ms = 500
+};
+
+static struct irq_mon_tracer irq_off_tracer __read_mostly = {
+	.tracing = true,
+	.th1_ms = 9,
+	.th2_ms = 500,
+	.th3_ms = 500,
+	.aee_debounce_ms = 60000
+};
+
+static struct irq_mon_tracer preempt_off_tracer __read_mostly = {
+	.tracing = true,
+	.th1_ms = 60000,
+	.th2_ms = 180000,
+};
+
+/* structues of probe funcitons */
+
+#define MAX_STACK_TRACE_DEPTH   32
+
+struct trace_stat {
+	bool tracing;
+	unsigned long long start_timestamp;
+	unsigned long long end_timestamp;
+	/* start trace */
+	struct stack_trace start_trace;
+	unsigned long trace_entries[MAX_STACK_TRACE_DEPTH];
+};
+
+struct preemptirq_stat {
+	bool tracing;
+	bool enable_locked;
+	unsigned long long disable_timestamp;
+	unsigned long disable_ip;
+	unsigned long disable_parent_ip;
+	unsigned long long enable_timestamp;
+	unsigned long enable_ip;
+	unsigned long enable_parent_ip;
+	/* stack_trace */
+	struct stack_trace disable_trace;
+	unsigned long trace_entries[MAX_STACK_TRACE_DEPTH];
+};
+
+static DEFINE_PER_CPU(struct preemptirq_stat, irq_pi_stat);
+static DEFINE_PER_CPU(struct preemptirq_stat, preempt_pi_stat);
+
+static void irq_mon_save_stack_trace(struct preemptirq_stat *pi_stat)
+{
+	struct stack_trace *trace = &pi_stat->disable_trace;
+
+	/* init, should move to other place */
+	trace->entries = &pi_stat->trace_entries[0];
+	trace->max_entries = MAX_STACK_TRACE_DEPTH;
+	trace->skip = 2;
+
+	trace->nr_entries = 0;
+	save_stack_trace(trace);
+
+	if (trace->nr_entries != 0 &&
+	    trace->entries[trace->nr_entries - 1] == ULONG_MAX)
+		trace->nr_entries--;
+}
+
+static void irq_mon_dump_stack_trace(struct preemptirq_stat *pi_stat)
+{
+	struct stack_trace *trace = &pi_stat->disable_trace;
+	char msg2[128];
+	int i;
+
+	pr_info("disable call trace:\n");
+	for (i = 0; i < trace->nr_entries; i++) {
+		scnprintf(msg2, sizeof(msg2), "[<%p>] %pS",
+			 (void *)trace->entries[i],
+			 (void *)trace->entries[i]);
+		pr_info("%s\n", msg2);
+	}
+}
+
+/* irq: 1 = irq, 0 = preempt*/
+static void check_preemptirq_stat(struct preemptirq_stat *pi_stat, int irq)
+{
+	unsigned long long threshold = irq ?
+				irq_off_tracer.th2_ms : preempt_off_tracer.th2_ms;
+	unsigned long long duration = pi_stat->enable_timestamp - pi_stat->disable_timestamp;
+
+	/* skip <idle-0> task */
+	if (current->pid == 0)
+		return;
+
+	if (duration < threshold * 1000000ULL)
+		return;
+	pr_info("%s off, duration %llu ms, from %llu ns to %llu ns\n",
+			irq ? "irq" : "preempt",
+			msec_high(duration),
+			pi_stat->disable_timestamp,
+			pi_stat->enable_timestamp);
+	pr_info("disable_ip       : [<%p>] %pS\n",
+			(void *)pi_stat->disable_ip, (void *)pi_stat->disable_ip);
+	pr_info("disable_parent_ip: [<%p>] %pS\n",
+			(void *)pi_stat->disable_parent_ip, (void *)pi_stat->disable_parent_ip);
+	pr_info("enable_ip        : [<%p>] %pS\n",
+			(void *)pi_stat->enable_ip, (void *)pi_stat->enable_ip);
+	pr_info("enable_parent_ip : [<%p>] %pS\n",
+			(void *)pi_stat->enable_parent_ip, (void *)pi_stat->enable_parent_ip);
+	dump_stack();
+	irq_mon_dump_stack_trace(pi_stat);
+}
+
+/* probe functions*/
+static DEFINE_PER_CPU(struct trace_stat, irq_trace_stat);
+static DEFINE_PER_CPU(struct trace_stat, softirq_trace_stat);
+static DEFINE_PER_CPU(struct trace_stat, ipi_trace_stat);
+
+#define stat_dur(stat) (stat->end_timestamp - stat->start_timestamp)
+#define th_exceeded(threshold, duration, tracer) \
+	(duration > (unsigned long long)tracer.threshold * 1000000ULL)
+
+static void probe_irq_handler_entry(void *ignore,
+		int irq, struct irqaction *action)
+{
+	unsigned long long ts;
+
+	if (__this_cpu_cmpxchg(irq_trace_stat.tracing, 0, 1))
+		return;
+
+	ts = sched_clock();
+	this_cpu_write(irq_trace_stat.start_timestamp, ts);
+}
+
+static void probe_irq_handler_exit(void *ignore,
+		int irq, struct irqaction *action, int ret)
+{
+	struct trace_stat *trace_stat = raw_cpu_ptr(&irq_trace_stat);
+	unsigned long long ts, duration;
+
+	if (!__this_cpu_read(irq_trace_stat.tracing))
+		return;
+
+	ts = sched_clock();
+	trace_stat->end_timestamp = ts;
+
+	duration = stat_dur(trace_stat);
+	if (th_exceeded(th2_ms, duration, irq_handler_tracer))
+		pr_info("irq: %d %pS, duration %llu ms, from %llu ns to %llu ns\n",
+				irq, (void *)action->handler,
+				msec_high(duration),
+				trace_stat->start_timestamp,
+				trace_stat->end_timestamp);
+
+	this_cpu_write(irq_trace_stat.tracing, 0);
+}
+
+static void probe_softirq_entry(void *ignore, unsigned int vec_nr)
+{
+	unsigned long long ts;
+
+	if (__this_cpu_cmpxchg(softirq_trace_stat.tracing, 0, 1))
+		return;
+
+	ts = sched_clock();
+	this_cpu_write(softirq_trace_stat.start_timestamp, ts);
+}
+
+static void probe_softirq_exit(void *ignore, unsigned int vec_nr)
+{
+	struct trace_stat *trace_stat = raw_cpu_ptr(&softirq_trace_stat);
+	unsigned long long ts, duration;
+
+	if (!__this_cpu_read(softirq_trace_stat.tracing))
+		return;
+
+	ts = sched_clock();
+	trace_stat->end_timestamp = ts;
+
+	duration = stat_dur(trace_stat);
+	if (th_exceeded(th2_ms, duration, irq_handler_tracer))
+		pr_info("softirq: %u %s, duration %llu ms, from %llu ns to %llu ns\n",
+				vec_nr, softirq_to_name[vec_nr],
+				msec_high(duration),
+				trace_stat->start_timestamp,
+				trace_stat->end_timestamp);
+
+	this_cpu_write(softirq_trace_stat.tracing, 0);
+}
+
+static void probe_ipi_entry(void *ignore, const char *reason)
+{
+	unsigned long long ts;
+
+	if (__this_cpu_cmpxchg(ipi_trace_stat.tracing, 0, 1))
+		return;
+
+	ts = sched_clock();
+	this_cpu_write(ipi_trace_stat.start_timestamp, ts);
+}
+
+
+static void probe_ipi_exit(void *ignore, const char *reason)
+{
+	struct trace_stat *trace_stat = raw_cpu_ptr(&ipi_trace_stat);
+	unsigned long long ts, duration;
+
+	if (!__this_cpu_read(ipi_trace_stat.tracing))
+		return;
+
+	ts = sched_clock();
+	trace_stat->end_timestamp = ts;
+
+	duration = stat_dur(trace_stat);
+	if (th_exceeded(th2_ms, duration, irq_handler_tracer))
+		pr_info("ipi: %s, duration %llu ms, from %llu ns to %llu ns\n",
+				reason,
+				msec_high(duration),
+				trace_stat->start_timestamp,
+				trace_stat->end_timestamp);
+
+	this_cpu_write(ipi_trace_stat.tracing, 0);
+}
+
+static void probe_irq_disable(void *ignore,
+		unsigned long ip, unsigned long parent_ip)
+{
+	struct preemptirq_stat *pi_stat = raw_cpu_ptr(&irq_pi_stat);
+	unsigned long long ts;
+
+	if (__this_cpu_cmpxchg(irq_pi_stat.tracing, 0, 1))
+		return;
+
+	ts = sched_clock();
+	this_cpu_write(irq_pi_stat.disable_timestamp, ts);
+	this_cpu_write(irq_pi_stat.disable_ip, ip);
+	this_cpu_write(irq_pi_stat.disable_parent_ip, parent_ip);
+
+	/* high overhead */
+	irq_mon_save_stack_trace(pi_stat);
+}
+
+static void probe_irq_enable(void *ignore,
+		unsigned long ip, unsigned long parent_ip)
+{
+	struct preemptirq_stat *pi_stat = raw_cpu_ptr(&irq_pi_stat);
+	unsigned long long ts;
+
+	if (!__this_cpu_read(irq_pi_stat.tracing))
+		return;
+
+	if (!__this_cpu_cmpxchg(irq_pi_stat.enable_locked, 0, 1))
+		return;
+
+	ts = sched_clock();
+	this_cpu_write(irq_pi_stat.enable_timestamp, ts);
+	this_cpu_write(irq_pi_stat.enable_ip, ip);
+	this_cpu_write(irq_pi_stat.enable_parent_ip, parent_ip);
+
+	check_preemptirq_stat(pi_stat, 1);
+
+	this_cpu_write(irq_pi_stat.enable_locked, 0);
+	this_cpu_write(irq_pi_stat.tracing, 0);
+}
+
+static void probe_preempt_disable(void *ignore
+		, unsigned long ip, unsigned long parent_ip)
+{
+	struct preemptirq_stat *pi_stat = raw_cpu_ptr(&preempt_pi_stat);
+	unsigned long long ts;
+
+	if (__this_cpu_cmpxchg(preempt_pi_stat.tracing, 0, 1))
+		return;
+
+	ts = sched_clock();
+	this_cpu_write(preempt_pi_stat.disable_timestamp, ts);
+	this_cpu_write(preempt_pi_stat.disable_ip, ip);
+	this_cpu_write(preempt_pi_stat.disable_parent_ip, parent_ip);
+
+	/* high overhead */
+	irq_mon_save_stack_trace(pi_stat);
+}
+
+static void probe_preempt_enable(void *ignore,
+		unsigned long ip, unsigned long parent_ip)
+{
+	struct preemptirq_stat *pi_stat = raw_cpu_ptr(&preempt_pi_stat);
+	unsigned long long ts;
+
+	if (!__this_cpu_read(preempt_pi_stat.tracing))
+		return;
+
+	if (!__this_cpu_cmpxchg(preempt_pi_stat.enable_locked, 0, 1))
+		return;
+
+	ts = sched_clock();
+	this_cpu_write(preempt_pi_stat.enable_timestamp, ts);
+	this_cpu_write(preempt_pi_stat.enable_ip, ip);
+	this_cpu_write(preempt_pi_stat.enable_parent_ip, parent_ip);
+
+	check_preemptirq_stat(pi_stat, 0);
+
+	this_cpu_write(preempt_pi_stat.enable_locked, 0);
+	this_cpu_write(preempt_pi_stat.tracing, 0);
+}
+
+struct irq_mon_tracepoint {
+	struct tracepoint *tp;
+	const char *name;
+	void *func;
+	void *data;
+	bool probe;
+};
+
+struct irq_mon_tracepoint irq_mon_tracepoint_table[] = {
+	/* irq.h */
+	{.name = "irq_handler_entry", .func = probe_irq_handler_entry, .data = NULL},
+	{.name = "irq_handler_exit", .func = probe_irq_handler_exit, .data = NULL},
+	{.name = "softirq_entry", .func = probe_softirq_entry, .data = NULL},
+	{.name = "softirq_exit", .func = probe_softirq_exit, .data = NULL},
+	/* ipi.h */
+	{.name = "ipi_entry", .func = probe_ipi_entry, .data = NULL},
+	{.name = "ipi_exit", .func = probe_ipi_exit, .data = NULL},
+	/* irqoff_tracer */
+	{.name = "irq_disable", .func = probe_irq_disable, .data = NULL},
+	{.name = "irq_enable", .func = probe_irq_enable, .data = NULL},
+	/* preempt_tracer */
+	{.name = "preempt_disable", .func = probe_preempt_disable, .data = NULL},
+	{.name = "preempt_enable", .func = probe_preempt_enable, .data = NULL},
+	/* The last item must be NULL!! */
+	{.name = NULL, .func = NULL, .data = NULL}
+};
+
+/* lookup tracepoints */
+static void irq_mon_tracepoint_lookup(struct tracepoint *tp, void *priv)
+{
+	struct irq_mon_tracepoint *t;
+
+	if (!tp || !tp->name)
+		return;
+
+	t = (struct irq_mon_tracepoint *)priv;
+
+	for (; t->name != NULL; t++) {
+		if (!strcmp(t->name, tp->name)) {
+			t->tp = tp;
+			pr_info("found tp: %s,%pS\n", tp->name, tp);
+			break;
+		}
+	}
+}
+
+/* probe tracepoints for all irq_monitor tracers */
+static int irq_mon_tracepoint_init(void)
+{
+	struct irq_mon_tracepoint *t;
+
+	for_each_kernel_tracepoint(irq_mon_tracepoint_lookup
+					, (void *)irq_mon_tracepoint_table);
+
+	for (t = irq_mon_tracepoint_table; t->name != NULL; t++) {
+		int ret;
+
+		if (!t->tp) {
+			pr_info("tp: %s not found\n", t->name);
+			continue;
+		}
+		ret = tracepoint_probe_register(t->tp, t->func, t->data);
+		if (ret) {
+			pr_info("tp: %s probe failed\n", t->name);
+			continue;
+		}
+		pr_info("tp: %s,%pS probed\n", t->name, t->tp);
+		t->probe = true;
+	}
+	return 0;
+}
+
+static void irq_mon_proc_init(void)
+{
+	struct proc_dir_entry *dir;
+
+	dir = proc_mkdir("mtmon", NULL);
+	if (!dir)
+		return;
+	mt_irq_monitor_test_init(dir);
+}
+
+static int __init irq_monitor_init(void)
+{
+	// tracepoint init
+	pr_info("irq monitor init start!!\n");
+	irq_mon_tracepoint_init();
+	irq_count_tracer_init();
+	irq_mon_proc_init();
+
+	return 0;
+}
+
+/* exit */
+static int irq_mon_tracepoint_exit(void)
+{
+	struct irq_mon_tracepoint *t;
+
+	for (t = irq_mon_tracepoint_table; t->name != NULL; t++) {
+		if (!t->tp)
+			continue;
+		if (t->probe) {
+			tracepoint_probe_unregister(t->tp, t->func, t->data);
+			t->probe = false;
+		}
+	}
+	tracepoint_synchronize_unregister();
+	return 0;
+}
+
+static void __exit irq_monitor_exit(void)
+{
+	irq_mon_tracepoint_exit();
+	remove_proc_subtree("mtmon", NULL);
+}
+
+MODULE_DESCRIPTION("MEDIATEK IRQ MONITOR");
+MODULE_LICENSE("GPL v2");
+early_initcall(irq_monitor_init);
+module_exit(irq_monitor_exit);
---
+++ drivers/misc/mediatek/mtprof/irq_monitor_test.c
@@ -0,0 +1,179 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2020 MediaTek Inc.
+ */
+
+#include <linux/delay.h>
+#include <linux/interrupt.h>
+#include <linux/irq_work.h>
+#include <linux/kthread.h>
+#include <linux/proc_fs.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+#include <linux/vmalloc.h>
+
+/* TIMER_SOFTIRQ duration warning test */
+
+static struct timer_list timer;
+static void delayed_timer(struct timer_list *t)
+{
+	mdelay(600);
+}
+
+void irq_mon_test_TIMER_SOFTIRQ(void)
+{
+	timer_setup(&timer, delayed_timer, 0);
+	mod_timer(&timer, jiffies + msecs_to_jiffies(100));
+}
+
+/* TASKLET_SOFTIRQ duration warning test */
+
+static struct tasklet_struct irq_mon_tasklet;
+static void irq_mon_tasklet_func(unsigned long data)
+{
+	mdelay(600);
+}
+
+void irq_mon_test_TASKLET_SOFTIRQ(void)
+{
+	tasklet_init(&irq_mon_tasklet, irq_mon_tasklet_func, 0);
+	tasklet_schedule(&irq_mon_tasklet);
+}
+
+/* RCU_SOFTIRQ duration warning test */
+
+struct irq_mon_rcu_t {
+	int val;
+};
+
+static struct irq_mon_rcu_t __rcu *irq_mon_rcu_g;
+static struct rcu_head irq_mon_rcu_head;
+static void delayed_rcu_callback(struct rcu_head *r)
+{
+	/* RCU_SOFTIRQ handler */
+	mdelay(600);
+}
+
+void irq_mon_test_RCU_SOFTIRQ(void)
+{
+	struct irq_mon_rcu_t *irq_mon_rcu;
+
+	irq_mon_rcu = kmalloc(sizeof(*irq_mon_rcu), GFP_KERNEL);
+	if (!irq_mon_rcu)
+		return;
+	irq_mon_rcu->val = 100;
+
+	RCU_INIT_POINTER(irq_mon_rcu_g, irq_mon_rcu);
+	call_rcu(&irq_mon_rcu_head, delayed_rcu_callback);
+}
+
+/* irq_work monitor test */
+
+static struct irq_work irq_mon_irqwork;
+static void irq_mon_irq_work(struct irq_work *work)
+{
+	mdelay(600);
+}
+
+void irq_mon_test_irq_work(void)
+{
+	init_irq_work(&irq_mon_irqwork, irq_mon_irq_work);
+	irq_work_queue(&irq_mon_irqwork);
+}
+
+/* IRQ disable monitor test */
+
+void irq_mon_test_irq_disable(void)
+{
+	unsigned long flags;
+
+	local_irq_save(flags);
+	mdelay(600);
+	local_irq_restore(flags);
+
+	/* The test will trigger KernelAPI Dump */
+}
+
+/* hrtimer monitor test */
+
+static struct hrtimer irq_mon_hrtimer;
+static enum hrtimer_restart irq_mon_hrtimer_func(struct hrtimer *unused)
+{
+	mdelay(600);
+	return HRTIMER_NORESTART;
+}
+
+void irq_mon_test_HRTIMER(void)
+{
+	hrtimer_init(&irq_mon_hrtimer, CLOCK_MONOTONIC, HRTIMER_MODE_ABS);
+	irq_mon_hrtimer.function = irq_mon_hrtimer_func;
+	hrtimer_start(&irq_mon_hrtimer, ms_to_ktime(0),
+		      HRTIMER_MODE_REL_PINNED);
+}
+
+/* preempt count monitor test */
+
+static struct hrtimer irq_mon_hrtimer2;
+static enum hrtimer_restart irq_mon_hrtimer_func2(struct hrtimer *unused)
+{
+	preempt_disable();
+	return HRTIMER_NORESTART;
+}
+
+void irq_mon_test_PREEMPT_COUNT(void)
+{
+	hrtimer_init(&irq_mon_hrtimer2, CLOCK_MONOTONIC, HRTIMER_MODE_ABS);
+	irq_mon_hrtimer2.function = irq_mon_hrtimer_func2;
+	hrtimer_start(&irq_mon_hrtimer2, ms_to_ktime(0),
+		      HRTIMER_MODE_REL_PINNED);
+}
+
+struct irq_mon_test_func {
+	char name[32];
+	void (*func)(void);
+};
+
+struct irq_mon_test_func irq_mon_test_list[] = {
+	{"sirq_timer_dur", irq_mon_test_TIMER_SOFTIRQ},
+	{"sirq_tasklet_dur", irq_mon_test_TASKLET_SOFTIRQ},
+	{"sirq_rcu_dur", irq_mon_test_RCU_SOFTIRQ},
+	{"irq_work_dur", irq_mon_test_irq_work},
+	{"hirq_disable", irq_mon_test_irq_disable},
+	{"hrtimer_dur", irq_mon_test_HRTIMER},
+	{"preempt_count", irq_mon_test_PREEMPT_COUNT},
+};
+
+static ssize_t
+irq_mon_test_write(struct file *file, const char *ubuf,
+		     size_t count, loff_t *ppos)
+{
+	int i;
+	char buf[32];
+
+	if (count >= sizeof(buf) || count == 0)
+		return -EINVAL;
+
+	if (copy_from_user(&buf, ubuf, count))
+		return -EFAULT;
+
+	buf[count] = '\0';
+
+	for (i = 0; i < ARRAY_SIZE(irq_mon_test_list); i++) {
+		if (!strncmp(buf, irq_mon_test_list[i].name,
+			     strlen(irq_mon_test_list[i].name)))
+			irq_mon_test_list[i].func();
+	}
+
+	return count;
+}
+
+static const struct file_operations proc_irq_monitor_test_fops = {
+	.open  = simple_open,
+	.write = irq_mon_test_write,
+};
+
+void mt_irq_monitor_test_init(struct proc_dir_entry *dir)
+{
+	proc_create("irq_mon_test", 0220, dir,
+		    &proc_irq_monitor_test_fops);
+}
