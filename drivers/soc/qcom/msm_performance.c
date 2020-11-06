acc6749297501a6c76282c994bf7a1eda0a35634
Niranjan Reddy Dumbala <dnreddy@codeaurora.org>
2020-11-06 12:56:32 +0530

    Revert "drivers: soc: qcom: msm_performance: PLH interface support"
    This reverts commit 4d51cd653e40ab3baf8886f0bc5a4d8a50d43e7d.
    
    Change-Id: I4cf1df7070564b3bc7a29718a5d01ebe1b737929
    Signed-off-by: Niranjan Reddy Dumbala <dnreddy@codeaurora.org>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -25,7 +25,6 @@
 #include <linux/perf_event.h>
 #include <linux/errno.h>
 #include <linux/topology.h>
-#include <linux/scmi_protocol.h>
 
 /*
  * Sched will provide the data for every 20ms window,
@@ -926,168 +925,20 @@ static const struct kernel_param_ops param_ops_game_start_pid = {
 module_param_cb(evnt_gplaf_pid, &param_ops_game_start_pid, NULL, 0644);
 
 /*******************************GFX Call************************************/
+
 static int splh_notif;
-static struct scmi_handle *plh_handle;
-void rimps_plh_init(struct scmi_handle *handle)
+static void init_splh_notif(const char *buf)
 {
-	if (handle)
-		plh_handle = handle;
-}
-EXPORT_SYMBOL(rimps_plh_init);
-
-#ifdef CONFIG_QTI_PLH
-
-static int splh_init_done, plh_log_level;
-
-#define PLH_MIN_LOG_LEVEL			0
-#define PLH_MAX_LOG_LEVEL			0xF
-#define SPLH_FPS_MAX_CNT			8 //fps supported count
-#define SPLH_IPC_FREQ_VTBL_MAX_CNT	5 //ipc freq pair
-#define SPLH_INIT_IPC_FREQ_TBL_PARAMS	\
-			(2+SPLH_FPS_MAX_CNT*(1+(2*SPLH_IPC_FREQ_VTBL_MAX_CNT)))
-
-static int set_plh_log_level(const char *buf, const struct kernel_param *kp)
-{
-	int ret, log_val_backup;
-	struct scmi_plh_vendor_ops *ops;
-
-	if (!plh_handle || !plh_handle->plh_ops)
-		return -EINVAL;
-
-	ops = plh_handle->plh_ops;
-
-	log_val_backup = plh_log_level;
-
-	ret = param_set_int(buf, kp); //Updates plh_log_level
-	if (ret < 0)
-		return ret;
-
-	plh_log_level = max(plh_log_level, PLH_MIN_LOG_LEVEL);
-	plh_log_level = min(plh_log_level, PLH_MAX_LOG_LEVEL);
-	ret = ops->set_plh_log_level(plh_handle, plh_log_level);
-	if (ret < 0) {
-		plh_log_level = log_val_backup;
-		return ret;
-	}
-	return 0;
-}
-
-static const struct kernel_param_ops param_ops_plh_log_level = {
-	.set = set_plh_log_level,
-	.get = param_get_int,
-};
-module_param_cb(plh_log_level, &param_ops_plh_log_level, &plh_log_level, 0644);
-
-static int init_splh_notif(const char *buf)
-{
-	int i, j, ret, ntokens, nfps, n_ipc_freq_pair;
-	//format of cmd nfps, n_ipc_freq_pair, <fps0, <ipc0, freq0>,...>,...
-	u16 tmp[SPLH_INIT_IPC_FREQ_TBL_PARAMS];
-	u16 *ptmp = tmp, tmp_valid_len = 0;
-	const char *cp, *cp1;
-	struct scmi_plh_vendor_ops *ops;
-
 	/*buf contains the init info from user*/
-	if (buf == NULL || !plh_handle || !plh_handle->plh_ops)
-		return -EINVAL;
+	if (buf == NULL)
+		return;
 
-	cp = buf;
-	ntokens = 0;
-	while ((cp = strpbrk(cp + 1, ":")))
-		ntokens++;
-
-	cp = buf;
-	if (sscanf(cp, INIT ":%d", &nfps)) {
-		if ((nfps != ntokens-1) || (nfps > SPLH_FPS_MAX_CNT))
-			return -EINVAL;
-
-		cp = strnchr(cp, strlen(cp), ':'); //skip INIT
-		cp++;
-		cp = strnchr(cp, strlen(cp), ':'); //skip nfps
-
-		*ptmp++ = nfps; //nfps is first cmd param
-		tmp_valid_len++;
-		cp1 = cp;
-		ntokens = 0;
-		//get count of nfps * n_ipc_freq_pair * <ipc freq pair values>
-		while ((cp1 = strpbrk(cp1 + 1, ",")))
-			ntokens++;
-
-		if (ntokens % (2*nfps)) //ipc freq pair values should be multiple of nfps
-			return -EINVAL;
-
-		n_ipc_freq_pair = ntokens / (2*nfps); //ipc_freq pair values for each FPS
-		if (n_ipc_freq_pair > SPLH_IPC_FREQ_VTBL_MAX_CNT)
-			return -EINVAL;
-
-		*ptmp++ = n_ipc_freq_pair; //n_ipc_freq_pair is second cmd param
-		tmp_valid_len++;
-		cp1 = cp;
-		for (i = 0; i < nfps; i++) {
-			if (sscanf(cp1, ":%u", ptmp) != 1)
-				return -EINVAL;
-
-			ptmp++; //increment after storing FPS val
-			tmp_valid_len++;
-			cp1 = strnchr(cp1, strlen(cp1), ','); //move to ,ipc
-			for (j = 0; j < 2*n_ipc_freq_pair; j++) {
-				if (sscanf(cp1, ",%u", ptmp) != 1)
-					return -EINVAL;
-
-				ptmp++; //increment after storing ipc or freq
-				tmp_valid_len++;
-				cp1 = strnchr(cp1, strlen(cp1), ',');
-				cp1++;
-				if (j != (2*n_ipc_freq_pair-1))
-					cp1 = strnchr(cp1, strlen(cp1), ','); //move to next
-			}
-
-			if (i != (nfps-1))
-				cp1 = strnchr(cp1, strlen(cp1), ':'); //move to next FPS val
-
-		}
-	} else {
-		return -EINVAL;
-	}
-
-	ops = plh_handle->plh_ops;
-	ret = ops->init_splh_ipc_freq_tbl(plh_handle, tmp, tmp_valid_len);
-	if (ret < 0)
-		return -EINVAL;
-
-	pr_info("msm_perf: nfps=%d n_ipc_freq_pair=%d last_freq_val=%d len=%d\n",
-		nfps, n_ipc_freq_pair, *--ptmp, tmp_valid_len);
-	splh_init_done = 1;
-	return 0;
+	pr_debug("msm_perf:Init info for scroll :: %s\n", buf);
 }
 
 static void activate_splh_notif(void)
 {
-	int ret;
-	struct scmi_plh_vendor_ops *ops;
 	/*received event notification here*/
-	if (splh_notif) {
-		if (plh_handle && plh_handle->plh_ops) {
-			ops = plh_handle->plh_ops;
-			ret = ops->start_splh(plh_handle, splh_notif); //splh_notif is fps
-			if (ret < 0) {
-				pr_err("msm_perf: splh start failed, ret=%d\n", ret);
-				return;
-			}
-		} else {
-			pr_err("msm_perf: splh not supported\n");
-			return;
-		}
-	} else {
-		if (plh_handle && plh_handle->plh_ops) {
-			ops = plh_handle->plh_ops;
-			ret = ops->stop_splh(plh_handle);
-			if (ret < 0) {
-				pr_err("msm_perf: splh stop failed, ret=%d\n", ret);
-				return;
-			}
-		}
-	}
 }
 
 static int set_splh_notif(const char *buf, const struct kernel_param *kp)
@@ -1095,17 +946,8 @@ static int set_splh_notif(const char *buf, const struct kernel_param *kp)
 	int ret;
 
 	if (strnstr(buf, INIT, sizeof(INIT)) != NULL) {
-		splh_init_done = 0;
-		ret = init_splh_notif(buf);
-		if (ret < 0)
-			pr_err("msm_perf: splh ipc freq tbl init failed, ret=%d\n", ret);
-
-		return ret;
-	}
-
-	if (!splh_init_done) {
-		pr_err("msm_perf: splh ipc freq tbl not initialized\n");
-		return -EINVAL;
+		init_splh_notif(buf);
+		return 0;
 	}
 
 	ret = param_set_int(buf, kp);
@@ -1114,18 +956,8 @@ static int set_splh_notif(const char *buf, const struct kernel_param *kp)
 
 	activate_splh_notif();
 
-	return 0;
+	return ret;
 }
-
-#else //!CONFIG_QTI_PLH
-
-static int set_splh_notif(const char *buf, const struct kernel_param *kp)
-{
-	pr_err("msm_perf: splh not implemented\n");
-	return -EINVAL;
-}
-
-#endif //CONFIG_QTI_PLH
 
 static const struct kernel_param_ops param_ops_splh_notification = {
 	.set = set_splh_notif,
