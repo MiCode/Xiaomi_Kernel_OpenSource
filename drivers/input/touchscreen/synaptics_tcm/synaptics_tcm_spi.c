11fceaafc2e2ae8daecdbc6f37b83fb233f8afe6
Niranjan Reddy Dumbala <dnreddy@codeaurora.org>
2020-11-06 13:02:32 +0530

    Revert "input: touchscreen: Enable Synaptcis tcm Modules"
    This reverts commit 9fb3a209fff07496aac602154d8bbf7c5cf23110.
    
    Change-Id: Ib3b0b97226d65873b311a9e3a9f1f836810d0c10
    Signed-off-by: Niranjan Reddy Dumbala <dnreddy@codeaurora.org>

--- drivers/input/touchscreen/synaptics_tcm/synaptics_tcm_spi.c
+++ drivers/input/touchscreen/synaptics_tcm/synaptics_tcm_spi.c
@@ -649,13 +649,13 @@ static struct spi_driver syna_tcm_spi_driver = {
 	.id_table = syna_tcm_id_table,
 };
 
-int syna_tcm_bus_init_spi(void)
+int syna_tcm_bus_init(void)
 {
 	return spi_register_driver(&syna_tcm_spi_driver);
 }
-EXPORT_SYMBOL(syna_tcm_bus_init_spi);
+EXPORT_SYMBOL(syna_tcm_bus_init);
 
-void syna_tcm_bus_exit_spi(void)
+void syna_tcm_bus_exit(void)
 {
 	kfree(buf);
 
@@ -663,7 +663,7 @@ void syna_tcm_bus_exit_spi(void)
 
 	spi_unregister_driver(&syna_tcm_spi_driver);
 }
-EXPORT_SYMBOL(syna_tcm_bus_exit_spi);
+EXPORT_SYMBOL(syna_tcm_bus_exit);
 
 MODULE_AUTHOR("Synaptics, Inc.");
 MODULE_DESCRIPTION("Synaptics TCM SPI Bus Module");
