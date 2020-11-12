7bab479cc03c13ff64daa073949870a98d411573
Charan Teja Reddy <charante@codeaurora.org>
2020-11-12 19:21:25 +0530

    mm: memory_hotplug: migration failure info of a page under DEBUG_VM
    Put the migration failure info of a page under DEBUG_VM as most of
    the times this info is not required.
    
    Change-Id: I798f0bef9c558a272c6aa94424236d8f65f801dd
    Signed-off-by: Charan Teja Reddy <charante@codeaurora.org>

--- mm/memory_hotplug.c
+++ mm/memory_hotplug.c
@@ -1407,7 +1407,10 @@ do_migrate_range(unsigned long start_pfn, unsigned long end_pfn)
 				if (__ratelimit(&migrate_rs)) {
 					pr_warn("migrating pfn %lx failed ret:%d\n",
 						page_to_pfn(page), ret);
-					dump_page(page, "migration failure");
+					__dump_page(page, "migration failure");
+#if defined(CONFIG_DEBUG_VM)
+					dump_page_owner(page);
+#endif
 				}
 			}
 			putback_movable_pages(&source);
