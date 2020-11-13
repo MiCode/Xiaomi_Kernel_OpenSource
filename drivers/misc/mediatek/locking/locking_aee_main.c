eabc1498b8fb7da9002eaf4446e2a09b30082801
Cheng Jui Wang <cheng-jui.wang@mediatek.com>
2020-11-13 13:18:21 +0800

    lockdep: Add delayed aee for missing DB
    Locking aee will call aee_kernel_warning_api to generate DB when
    debug_locks is turned off. The DB will be lost if critical
    locks are held when locking aee is trying to generate DB.
    
    Add a kthread to generate the DB.
    
    (cherry-picked from commit ca66c536833f974929f51a8db3126badac8771dd)
    Change-Id: I75e155004059af92ad9ebfc60e739a335ba6b663
    CR-Id: ALPS05438846
    Feature: [Module]Lockdep
    Signed-off-by: Cheng Jui Wang <cheng-jui.wang@mediatek.com>
    Signed-off-by: Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
    (cherry picked from commit 92d7d2cccdd8299de0e5634de65b9e45efd8ddcf)

--- drivers/misc/mediatek/locking/locking_aee_main.c
+++ drivers/misc/mediatek/locking/locking_aee_main.c
@@ -5,8 +5,11 @@
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": %s: " fmt, __func__
 
+#include <linux/atomic.h>
 #include <linux/debug_locks.h>
+#include <linux/delay.h>
 #include <linux/kprobes.h>
+#include <linux/kthread.h>
 #include <linux/lockdep.h>
 #include <linux/module.h>
 #include <linux/sched.h>
@@ -14,6 +17,8 @@
 
 //lockdep_test.c
 int lockdep_test_init(void);
+
+static atomic_t warned;
 
 static const char * const critical_lock_list[] = {
 	/* runqueue */
@@ -56,13 +61,18 @@ static int debug_locks_off_kretprobe_handler(struct kretprobe_instance *ri,
 		struct pt_regs *regs)
 {
 	int retval = regs_return_value(regs);
+	u32 val_warned = 0;
 
 	if (!retval)
 		return 0;
 
-	if (!is_critical_lock_held()) {
+	if (is_critical_lock_held())
+		return 0;
+
+	if (likely(atomic_try_cmpxchg(&warned, &val_warned, 1U))) {
 		char aee_str[40];
 
+		pr_info("LockProve Warning, AEE!\n");
 		snprintf(aee_str, 40, "[%s]LockProve Warning", current->comm);
 		aee_kernel_warning_api(__FILE__, __LINE__,
 				DB_OPT_DUMMY_DUMP | DB_OPT_FTRACE,
@@ -79,6 +89,39 @@ static struct kretprobe debug_locks_off_kretprobe = {
 	.maxactive = 1,
 };
 
+static int locking_aee_thread(void *data)
+{
+	u32 val_warned = 0;
+
+	for (;;) {
+		msleep(3000);
+
+		/* is lockdep disabled? */
+		if (likely(debug_locks))
+			continue;
+
+		/* Have we called the AEE? */
+		val_warned = atomic_read(&warned);
+		if (likely(val_warned))
+			break;
+		if (likely(atomic_try_cmpxchg(&warned, &val_warned, 1U)))
+			break;
+	}
+
+	WARN_ON(debug_locks);
+
+	if (!val_warned) {
+		char aee_str[40];
+
+		pr_info("LockProve Warning, AEE!\n");
+		snprintf(aee_str, 40, "[%s]LockProve Warning", current->comm);
+		aee_kernel_warning_api(__FILE__, __LINE__,
+				DB_OPT_DUMMY_DUMP | DB_OPT_FTRACE,
+				aee_str, "LockProve Debug\n");
+	}
+	return 0;
+}
+
 static int __init locking_aee_init(void)
 {
 	int ret;
@@ -90,6 +133,7 @@ static int __init locking_aee_init(void)
 	}
 	pr_info("register debug_locks_off kretprobe succeeded.\n");
 
+	kthread_run(locking_aee_thread, NULL, "locking_aee");
 	lockdep_test_init();
 	return 0;
 }
