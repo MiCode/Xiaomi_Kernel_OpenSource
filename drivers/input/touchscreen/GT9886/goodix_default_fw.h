b973415633dcdb7ea7246117eb1d7459ec70359a
mtk15754 <wenqiang.zhang@mediatek.com>
2020-11-26 17:40:58 +0800

    TOUCH: touch driver porting
    GT9886: i2c touch
    GT9896S: SPI touch
    add touch driver for touch use
    
    (cherry-picked from commit 517f77898132b4c02cfbff8d9791ab8ac6607db8)
    Signed-off-by: mtk15754 <wenqiang.zhang@mediatek.com>
    Change-Id: I010fc51a7454f02742bc68a8d9dab100d6c8be08
    CR-Id: ALPS05476666
    Feature: [Android Default] CapTouch
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    (cherry picked from commit 662ab1b91d9d6d5db9b62fa9870f100790111847)

---
+++ drivers/input/touchscreen/GT9886/goodix_default_fw.h
@@ -0,0 +1,25 @@
+/*
+ * Goodix Touchscreen Driver
+ * Core layer of touchdriver architecture.
+ *
+ * Copyright (C) 2015 - 2016 Goodix, Inc.
+ * Authors:  Yulong Cai <caiyulong@goodix.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be a reference
+ * to you, when you are integrating the GOODiX's CTP IC into your system,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ */
+#ifndef _GOODIX_FIRMWARE_FW_H_
+#define _GOODIX_FIRMWARE_FW_H_
+
+u8 goodix_default_fw[] = { 0x00};
+
+#endif
