c24a6e5a0f9a0870b45785106539679e11b88c7c
Carl Fang <carl.fang@mediatek.com>
2020-11-26 10:01:04 +0800

    [ALPS05450000] ccu: fix undefined behaviour bool
    initial meminfo value
    
    MTK-Commit-Id: 86322775288e8642221d587fa29ed98036bcd36a
    
    Change-Id: I44d0477027612263578564a8a45dfa65e328df04
    CR-Id: ALPS05450000
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: Carl Fang <carl.fang@mediatek.com>

--- drivers/misc/mediatek/ccu/src/mt6885/ccu_drv.c
+++ drivers/misc/mediatek/ccu/src/mt6885/ccu_drv.c
@@ -1006,7 +1006,7 @@ static int ccu_release(struct inode *inode, struct file *flip)
 {
 	struct ccu_user_s *user = flip->private_data;
 	int i = 0;
-	struct CcuMemHandle handle;
+	struct CcuMemHandle handle = {0};
 
 	LOG_INF_MUST("%s +\n", __func__);
 	ccu_force_powerdown();
