2965de79b3521eb77e3be7d038f3100245223c9c
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2020-11-25 22:18:59 -0800

    haven: hcall: Add virtio API
    Define APIs that interact with Haven hypervisor for virtio device
    related emulation.
    
    Change-Id: I093bd030dd8eb70d1c8198e0a7dcccaff553a888
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>

--- include/linux/haven/hcall.h
+++ include/linux/haven/hcall.h
@@ -300,4 +300,124 @@ static inline int hh_hcall_trace_update_class_flags(
 	return ret;
 }
 
+static inline int
+hh_hcall_virtio_mmio_backend_assert_virq(hh_capid_t capid, u64 int_status)
+{
+	int ret;
+	struct hh_hcall_resp _resp = {0};
+
+	ret = _hh_hcall(0x604e,
+			(struct hh_hcall_args){ capid, int_status, 0 },
+			&_resp);
+
+	return ret;
+}
+
+static inline int
+hh_hcall_virtio_mmio_backend_set_dev_features(hh_capid_t capid,
+				u64 features_sel, u64 features)
+{
+	int ret;
+	struct hh_hcall_resp _resp = {0};
+
+	ret = _hh_hcall(0x604f,
+			(struct hh_hcall_args){ capid, features_sel,
+				features, 0 }, &_resp);
+
+	return ret;
+}
+
+static inline int
+hh_hcall_virtio_mmio_backend_set_queue_num_max(hh_capid_t capid,
+				u64 queue_sel, u64 queue_num_max)
+{
+	int ret;
+	struct hh_hcall_resp _resp = {0};
+
+	ret = _hh_hcall(0x6050,
+			(struct hh_hcall_args){ capid, queue_sel,
+					queue_num_max, 0 }, &_resp);
+
+	return ret;
+}
+
+static inline int
+hh_hcall_virtio_mmio_backend_get_drv_features(hh_capid_t capid,
+			u64 features_sel, u64 *features)
+{
+	int ret;
+	struct hh_hcall_resp _resp = {0};
+
+	ret = _hh_hcall(0x6051,
+			(struct hh_hcall_args){ capid, features_sel, 0},
+			&_resp);
+
+	if (!ret && features)
+		*features = _resp.resp1;
+
+	return ret;
+}
+
+struct hh_hcall_virtio_queue_info {
+	u64 queue_num;
+	u64 queue_ready;
+	u64 queue_desc;
+	u64 queue_driver;
+	u64 queue_device;
+};
+
+static inline int
+hh_hcall_virtio_mmio_backend_get_queue_info(hh_capid_t capid,
+		u64 queue_sel, struct hh_hcall_virtio_queue_info *queue_info)
+{
+	int ret;
+	struct hh_hcall_resp _resp = {0};
+
+	ret = _hh_hcall(0x6052,
+			(struct hh_hcall_args){ capid, queue_sel, 0},
+			&_resp);
+
+	if (!ret && queue_info) {
+		queue_info->queue_num		= _resp.resp1;
+		queue_info->queue_ready		= _resp.resp2;
+		queue_info->queue_desc		= _resp.resp3;
+		queue_info->queue_driver	= _resp.resp4;
+		queue_info->queue_device	= _resp.resp5;
+	}
+
+	return ret;
+}
+
+static inline int
+hh_hcall_virtio_mmio_backend_get_event(hh_capid_t capid,
+			u64 *event_data, u64 *event)
+{
+	int ret;
+	struct hh_hcall_resp _resp = {0};
+
+	ret = _hh_hcall(0x6053,
+			(struct hh_hcall_args){ capid, 0},
+			&_resp);
+
+	if (!ret && event_data)
+		*event_data = _resp.resp1;
+	if (!ret && event)
+		*event = _resp.resp2;
+
+	return ret;
+}
+
+static inline int
+hh_hcall_virtio_mmio_backend_ack_reset(hh_capid_t capid)
+{
+	int ret;
+	struct hh_hcall_resp _resp = {0};
+
+	ret = _hh_hcall(0x6054,
+			(struct hh_hcall_args){ capid, 0},
+			&_resp);
+
+	return ret;
+}
+
 #endif
