df4d0b0a2f1b432d2b54618522ecebccb643395f
Miles Chen <miles.chen@mediatek.com>
2020-11-26 11:11:15 +0800

    iio: adc: mediatek: fix of_find_device_by_node build breaks
    Errors:
    drivers/iio/adc/mt6577_auxadc.c:149:16: error: implicit declaration of function 'of_find_device_by_node' [-Werror,-Wimplicit-function-declaration]
    
    Change-Id: Ic574ffaf8906881626aa77c88e226ce1a35a6e52
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    CR-Id: ALPS05464465
    Feature: [Module]Kernel Maintenance

--- drivers/iio/adc/mt6577_auxadc.c
+++ drivers/iio/adc/mt6577_auxadc.c
@@ -12,6 +12,7 @@
 #include <linux/module.h>
 #include <linux/mod_devicetable.h>
 #include <linux/platform_device.h>
+#include <linux/of_platform.h>
 #include <linux/property.h>
 #include <linux/iopoll.h>
 #include <linux/io.h>
