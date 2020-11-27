0f12eb7beea5b695601eedf9514421fd2880c0ae
Sylwester Dziedziuch <sylwesterx.dziedziuch@intel.com>
2020-11-27 11:23:01 +0000

    i40e: Fix VFs not created
    [ Upstream commit dc8812626440fa6a27f1f3f654f6dc435e042e42 ]
    
    When creating VFs they were sometimes not getting resources.
    It was caused by not executing i40e_reset_all_vfs due to
    flag __I40E_VF_DISABLE being set on PF. Because of this
    IAVF was never able to finish setup sequence never
    getting reset indication from PF.
    Changed test_and_set_bit __I40E_VF_DISABLE in
    i40e_sync_filters_subtask to test_bit and removed clear_bit.
    This function should not set this bit it should only check
    if it hasn't been already set.
    
    Fixes: a7542b876075 ("i40e: check __I40E_VF_DISABLE bit in i40e_sync_filters_subtask")
    Signed-off-by: Sylwester Dziedziuch <sylwesterx.dziedziuch@intel.com>
    Tested-by: Konrad Jankowski <konrad0.jankowski@intel.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/intel/i40e/i40e_main.c
+++ drivers/net/ethernet/intel/i40e/i40e_main.c
@@ -2577,7 +2577,7 @@ static void i40e_sync_filters_subtask(struct i40e_pf *pf)
 		return;
 	if (!test_and_clear_bit(__I40E_MACVLAN_SYNC_PENDING, pf->state))
 		return;
-	if (test_and_set_bit(__I40E_VF_DISABLE, pf->state)) {
+	if (test_bit(__I40E_VF_DISABLE, pf->state)) {
 		set_bit(__I40E_MACVLAN_SYNC_PENDING, pf->state);
 		return;
 	}
@@ -2595,7 +2595,6 @@ static void i40e_sync_filters_subtask(struct i40e_pf *pf)
 			}
 		}
 	}
-	clear_bit(__I40E_VF_DISABLE, pf->state);
 }
 
 /**
