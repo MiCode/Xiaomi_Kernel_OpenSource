62aae84c26e3cc9182f397ff8ce7a09ee3d5816e
mtk81216 <lina.wang@mediatek.com>
2020-12-01 14:56:20 +0800

    rps: provide set rps API
    provide a setting RPS API for ccmni driver & rndis driver & wifi driver
    
    (cherry-picked from commit b18e3776e8e509055a4c7426b98ce4844556b1a5)
    Change-Id: I81c8e7e33e5c73ded6b5b88fed98ec75b9030fd8
    CR-Id: ALPS05487649
    Feature: Driver
    Signed-off-by: mtk81216 <lina.wang@mediatek.com>
    Signed-off-by: Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
    (cherry picked from commit 54a917d321910c272a282f2d6633702ce85b85dd)

---
+++ drivers/misc/mediatek/rps/rps_perf.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0
+ *
+ * Copyright (C) 2015 MediaTek Inc.
+ */
+
+
+#ifndef __RPS_PERF_H__
+#define __RPS_PERF_H__
+
+int set_rps_map(struct netdev_rx_queue *queue, unsigned long rps_value);
+
+#endif /* __RPS_PERF_H__ */
+
