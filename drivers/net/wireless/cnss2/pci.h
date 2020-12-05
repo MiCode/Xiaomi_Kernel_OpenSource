2dc22bc62d06c8b0e9bcf1b8448fcbc62bed9838
Yue Ma <yuem@codeaurora.org>
2020-12-04 18:34:50 -0800

    cnss2: Use CONFIG_PCI_MSM to protect MSM PCIe APIs
    This can ease the complexity to port CNSS2 driver to non-MSM
    platforms while still be able to maintain the compatibility.
    
    Change-Id: I268cdd90c59085b34aa2fe3c3b845d60c4e2a417
    Signed-off-by: Yue Ma <yuem@codeaurora.org>

--- drivers/net/wireless/cnss2/pci.h
+++ drivers/net/wireless/cnss2/pci.h
@@ -1,12 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
-/* Copyright (c) 2016-2020, The Linux Foundation. All rights reserved. */
+/* Copyright (c) 2016-2021, The Linux Foundation. All rights reserved. */
 
 #ifndef _CNSS_PCI_H
 #define _CNSS_PCI_H
 
 #include <linux/iommu.h>
 #include <linux/mhi.h>
+#if IS_ENABLED(CONFIG_PCI_MSM)
 #include <linux/msm_pcie.h>
+#endif
 #include <linux/pci.h>
 
 #include "main.h"
@@ -86,7 +88,9 @@ struct cnss_pci_data {
 	u8 pci_link_down_ind;
 	struct pci_saved_state *saved_state;
 	struct pci_saved_state *default_state;
+#if IS_ENABLED(CONFIG_PCI_MSM)
 	struct msm_pcie_register_event msm_pci_event;
+#endif
 	struct cnss_pm_stats pm_stats;
 	atomic_t auto_suspended;
 	atomic_t drv_connected;
