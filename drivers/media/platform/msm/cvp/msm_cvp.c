7fe0e8dda62845fdcc122489d5c9755b1021a3ac
George Shen <sqiao@codeaurora.org>
2020-12-08 21:38:39 -0800

    msm: cvp: Fix CVP BW overvoting issue
    Reduce min BW vote to 65MB instead of 655MB.
    
    Change-Id: I0bfb0262824d245756fcb70d41cc589b595d27ff
    Signed-off-by: George Shen <sqiao@codeaurora.org>

--- drivers/media/platform/msm/cvp/msm_cvp.c
+++ drivers/media/platform/msm/cvp/msm_cvp.c
@@ -895,12 +895,12 @@ static int adjust_bw_freqs(void)
 	cvp_max_rate = tbl[tbl_size - 1].clock_rate;
 	bus = &core->resources.bus_set.bus_tbl[1];
 	max_bw = bus->range[1];
-	min_bw = max_bw/10;
+	min_bw = max_bw/100;
 
 	aggregate_power_update(core, &nrt_pwr, &rt_pwr, cvp_max_rate);
-	dprintk(CVP_PROF, "PwrUpdate nrt %u %u rt %u %u\n",
-		nrt_pwr.core_sum, nrt_pwr.op_core_sum,
-		rt_pwr.core_sum, rt_pwr.op_core_sum);
+	dprintk(CVP_PROF, "PwrUpdate nrt %u %u %lld rt %u %u %lld\n",
+		nrt_pwr.core_sum, nrt_pwr.op_core_sum, nrt_pwr.bw_sum,
+		rt_pwr.core_sum, rt_pwr.op_core_sum, rt_pwr.bw_sum);
 
 	if (rt_pwr.core_sum > cvp_max_rate) {
 		dprintk(CVP_WARN, "%s clk vote out of range %lld\n",
