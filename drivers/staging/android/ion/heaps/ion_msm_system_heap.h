fd72ed900f62e5b20d88b872f848d796a95fbad5
Prakash Gupta <guptap@codeaurora.org>
2020-12-09 22:43:19 +0530

    ion: use correct section map size for LPAE
    Use correct section map size for LPAE. At present ion use
    CONFIG_IOMMU_IO_PGTABLE_ARMV7S to determine whether to use LPAE or short
    descriptor section size.  This is insufficient. Check for CONFIG_64bit and
    CONFIG_LPAE as well to decide correct section size.
    
    Change-Id: If730d98da110ebb338f7f1affccaae28c0c58a9e
    Signed-off-by: Prakash Gupta <guptap@codeaurora.org>

--- drivers/staging/android/ion/heaps/ion_msm_system_heap.h
+++ drivers/staging/android/ion/heaps/ion_msm_system_heap.h
@@ -9,7 +9,7 @@
 #define _ION_MSM_SYSTEM_HEAP_H
 
 #ifndef CONFIG_ALLOC_BUFFERS_IN_4K_CHUNKS
-#if defined(CONFIG_IOMMU_IO_PGTABLE_ARMV7S)
+#if defined(CONFIG_IOMMU_IO_PGTABLE_ARMV7S) && !defined(CONFIG_64BIT) && !defined(CONFIG_ARM_LPAE)
 static const unsigned int orders[] = {8, 4, 0};
 #else
 static const unsigned int orders[] = {9, 4, 0};
