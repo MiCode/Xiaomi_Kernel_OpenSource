1a38a72df3d9bbddfe009a5611e388d5d5e1861d
Michael Adisumarta <madisuma@codeaurora.org>
2020-12-10 15:02:56 -0800

    msm: ipa: create ipa_client for ethernet
    Create the ipa_client interface for ethernet offloading.
    
    Change-Id: I8120b0cca9e42a75153fb1468dc1b8bcbd43484c
    Signed-off-by: Bojun Pan <bojunp@codeaurora.org>
    Signed-off-by: Amir Levy <alevy@codeaurora.org>
    Signed-off-by: Michael Adisumarta <madisuma@codeaurora.org>

--- include/linux/ipa_fmwk.h
+++ include/linux/ipa_fmwk.h
@@ -16,6 +16,7 @@
 #include <linux/ipa_usb.h>
 #include <linux/ipa_odu_bridge.h>
 #include <linux/ipa_qmi_service_v01.h>
+#include <linux/ipa_eth.h>
 
 struct ipa_core_data {
 	int (*ipa_tx_dp)(enum ipa_client_type dst, struct sk_buff *skb,
@@ -278,6 +279,27 @@ struct ipa_wigig_data {
 	int (*ipa_wigig_save_regs)(void);
 };
 
+struct ipa_eth_data {
+	int (*ipa_eth_register_ready_cb)(struct ipa_eth_ready *ready_info);
+
+	int (*ipa_eth_unregister_ready_cb)(struct ipa_eth_ready *ready_info);
+
+	int (*ipa_eth_client_conn_pipes)(struct ipa_eth_client *client);
+
+	int (*ipa_eth_client_disconn_pipes)(struct ipa_eth_client *client);
+
+	int (*ipa_eth_client_reg_intf)(struct ipa_eth_intf_info *intf);
+
+	int (*ipa_eth_client_unreg_intf)(struct ipa_eth_intf_info *intf);
+
+	int (*ipa_eth_client_set_perf_profile)(struct ipa_eth_client *client,
+		struct ipa_eth_perf_profile *profile);
+
+	int (*ipa_eth_client_conn_evt)(struct ipa_ecm_msg *msg);
+
+	int (*ipa_eth_client_disconn_evt)(struct ipa_ecm_msg *msg);
+};
+
 #if IS_ENABLED(CONFIG_IPA3)
 
 int ipa_fmwk_register_ipa(const struct ipa_core_data *in);
@@ -295,6 +317,8 @@ int ipa_fmwk_register_ipa_mhi(const struct ipa_mhi_data *in);
 int ipa_fmwk_register_ipa_wigig(const struct ipa_wigig_data *in);
 
 int ipa_fmwk_register_ipa_qdss(const struct ipa_qdss_data *in);
+
+int ipa_fmwk_register_ipa_eth(const struct ipa_eth_data *in);
 
 #else /* IS_ENABLED(CONFIG_IPA3) */
 
@@ -334,6 +358,11 @@ int ipa_fmwk_register_ipa_mhi(const struct ipa_mhi_data *in)
 }
 
 int ipa_fmwk_register_ipa_wigig(const struct ipa_wigig_data *in)
+{
+	return -EPERM;
+}
+
+int ipa_fmwk_register_ipa_eth(const struct ipa_eth_data *in)
 {
 	return -EPERM;
 }
