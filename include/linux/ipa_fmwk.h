73aa18767196490c707f2675192949eea1f3b852
Michael Adisumarta <madisuma@codeaurora.org>
2020-12-09 20:22:03 -0800

    msm: ipa: Support hardware accelerated DIAG over qdss with ipa_fmwk
    Add support to accelarate QDSS diag traffic over IPA to the PCIe
    host with ipa fmwk support.
    
    Change-Id: I64a8c493e8b6ef3fc525a3edcfd57e1cd11ccb6c
    Signed-off-by: Michael Adisumarta <madisuma@codeaurora.org>

--- include/linux/ipa_fmwk.h
+++ include/linux/ipa_fmwk.h
@@ -12,6 +12,7 @@
 #include <linux/ipa_mhi.h>
 #include <linux/ipa_wigig.h>
 #include <linux/ipa_wdi3.h>
+#include <linux/ipa_qdss.h>
 #include <linux/ipa_usb.h>
 #include <linux/ipa_odu_bridge.h>
 #include <linux/ipa_qmi_service_v01.h>
@@ -166,6 +167,15 @@ struct ipa_wdi3_data {
 	int (*ipa_wdi_bw_monitor)(struct ipa_wdi_bw_info *info);
 
 	int (*ipa_wdi_sw_stats)(struct ipa_wdi_tx_info *info);
+
+	int (*ipa_get_wdi_version)(void);
+};
+
+struct ipa_qdss_data {
+	int (*ipa_qdss_conn_pipes)(struct ipa_qdss_conn_in_params *in,
+		struct ipa_qdss_conn_out_params *out);
+
+	int (*ipa_qdss_disconn_pipes)(void);
 };
 
 struct ipa_gsb_data {
@@ -284,6 +294,8 @@ int ipa_fmwk_register_ipa_mhi(const struct ipa_mhi_data *in);
 
 int ipa_fmwk_register_ipa_wigig(const struct ipa_wigig_data *in);
 
+int ipa_fmwk_register_ipa_qdss(const struct ipa_qdss_data *in);
+
 #else /* IS_ENABLED(CONFIG_IPA3) */
 
 int ipa_fmwk_register_ipa(const struct ipa_core_data *in)
@@ -297,6 +309,11 @@ int ipa_fmwk_register_ipa_usb(const struct ipa_usb_data *in)
 }
 
 int ipa_fmwk_register_ipa_wdi3(const struct ipa_wdi3_data *in)
+{
+	return -EPERM;
+}
+
+int ipa_fmwk_register_ipa_qdss(const struct ipa3_qdss_data *in)
 {
 	return -EPERM;
 }
