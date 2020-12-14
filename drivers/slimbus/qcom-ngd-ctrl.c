84a85bb6fcec8f5f2cee0b5bbf3eabd89b628a3f
Prudhvi Yarlagadda <pyarlaga@codeaurora.org>
2020-12-14 17:06:59 +0530

    slimbus: qcom-ngd-ctrl: Reinit the reconf completion flag
    Reinitialize the reconf completion flag when ngd registers
    are not retainied or when enumeration is lost for ngd.
    
    Change-Id: I6a006eebda544653f4a3f40bc6fb74440408453b
    Signed-off-by: Prudhvi Yarlagadda <pyarlaga@codeaurora.org>

--- drivers/slimbus/qcom-ngd-ctrl.c
+++ drivers/slimbus/qcom-ngd-ctrl.c
@@ -1257,6 +1257,9 @@ static int qcom_slim_ngd_power_up(struct qcom_slim_ngd_ctrl *ctrl)
 		return 0;
 	}
 
+	/* reinitialize it only when registers are not retained */
+	reinit_completion(&ctrl->reconf);
+
 	writel_relaxed(DEF_NGD_INT_MASK, ngd->base + NGD_INT_EN);
 	rx_msgq = readl_relaxed(ngd->base + NGD_RX_MSGQ_CFG);
 
