299c2babb8d9bc1da5829bfa19d66c31d1304b08
Deepak Kumar Singh <deesin@codeaurora.org>
2020-12-15 23:03:15 +0530

    rpmsg: glink: remove IRQF_NO_SUSPEND flag while requesting irq
    Using IRQF_NO_SUSPEND flag may not mark irq wakeup capable.
    
    Use enable_irq_wake to mark glink irq wake up capable.
    
    Change-Id: I0fb5010ff1450a3bd15894a3d4706d7bd73e8cac
    Signed-off-by: Deepak Kumar Singh <deesin@codeaurora.org>

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -2114,13 +2114,14 @@ struct qcom_glink *qcom_glink_native_probe(struct device *dev,
 	irq = of_irq_get(dev->of_node, 0);
 	ret = devm_request_irq(dev, irq,
 			       qcom_glink_native_intr,
-			       IRQF_NO_SUSPEND | IRQF_SHARED,
+			       IRQF_SHARED,
 			       glink->irqname, glink);
 	if (ret) {
 		dev_err(dev, "failed to request IRQ\n");
 		goto unregister;
 	}
 
+	enable_irq_wake(irq);
 	glink->irq = irq;
 
 	size = of_property_count_u32_elems(dev->of_node, "cpu-affinity");
