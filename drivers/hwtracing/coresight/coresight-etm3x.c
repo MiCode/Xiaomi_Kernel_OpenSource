f646b2f38e029bcee90663f8d1c8f25e32dd40ff
Yuanfang Zhang <zhangyuanfang@codeaurora.org>
2020-12-15 14:41:29 +0800

    coresight-etm3x: change ETM name to coresight-etm
    Change the name of ETM to coresight-etm.
    
    Change-Id: Ia7ce1fb455260c6f5877e3b8b81431d7129de529
    Signed-off-by: Yuanfang Zhang <zhangyuanfang@codeaurora.org>

--- drivers/hwtracing/coresight/coresight-etm3x.c
+++ drivers/hwtracing/coresight/coresight-etm3x.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (c) 2011-2012, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2011-2012, 2020, The Linux Foundation. All rights reserved.
  *
  * Description: CoreSight Program Flow Trace driver
  */
@@ -819,7 +819,9 @@ static int etm_probe(struct amba_device *adev, const struct amba_id *id)
 	if (drvdata->cpu < 0)
 		return drvdata->cpu;
 
-	desc.name  = devm_kasprintf(dev, GFP_KERNEL, "etm%d", drvdata->cpu);
+	if (of_property_read_string(dev->of_node, "coresight-name", &desc.name))
+		desc.name  = devm_kasprintf(dev, GFP_KERNEL, "coresight-etm%d", drvdata->cpu);
+
 	if (!desc.name)
 		return -ENOMEM;
 
