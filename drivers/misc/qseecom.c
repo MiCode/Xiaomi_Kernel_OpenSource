0c15225e85011c79fea9753764174e73ae4fcae9
Jiten Patel <jitepate@codeaurora.org>
2020-12-16 11:14:08 +0530

    qseecom: use legacy command for slateapp
    use the legacy command for sending any data
    to trusted application only if the app name
    is slateapp.
    
    Change-Id: Iecbe75c45b253ef87de3306b89b50c39008550fe
    Signed-off-by: Jiten Patel <jitepate@codeaurora.org>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -5307,8 +5307,10 @@ int qseecom_send_command(struct qseecom_handle *handle, void *send_buf,
 		}
 		perf_enabled = true;
 	}
-	if (!strcmp(data->client.app_name, "securemm"))
+	if (!strcmp(data->client.app_name, "securemm") ||
+	    !strcmp(data->client.app_name, "slateapp")) {
 		data->use_legacy_cmd = true;
+	}
 
 	ret = __qseecom_send_cmd(data, &req, false);
 
