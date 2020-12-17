0fcdf063b5b8fe19ead5d53c714a7accbe14d88e
Hrishikesh Vidwans <hvidwans@codeaurora.org>
2020-12-17 10:12:45 -0800

    wil6210: add ability to help debug tx latency
    This patch adds functionality to help debug issues
    related to tx latency.
    
    Change-Id: I3e94949627b48b082b2b6e7d0555fd4f79d7598f
    Signed-off-by: Hrishikesh Vidwans <hvidwans@codeaurora.org>

--- drivers/net/wireless/ath/wil6210/debugfs.c
+++ drivers/net/wireless/ath/wil6210/debugfs.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: ISC
 /*
  * Copyright (c) 2012-2017 Qualcomm Atheros, Inc.
- * Copyright (c) 2018-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.
  */
 
 #include <linux/module.h>
@@ -862,6 +862,57 @@ static ssize_t wil_write_file_rbufcap(struct file *file,
 static const struct file_operations fops_rbufcap = {
 	.write = wil_write_file_rbufcap,
 	.open  = simple_open,
+};
+
+static int wil_tx_latency_threshold_low_show(struct seq_file *s, void *data)
+{
+	struct wil6210_priv *wil = s->private;
+
+	seq_printf(s, "%d\n", wil->tx_latency_threshold_low);
+	return 0;
+}
+
+static int wil_tx_latency_threshold_low_seq_open(struct inode *inode,
+						 struct file *file)
+{
+	return single_open(file, wil_tx_latency_threshold_low_show,
+			   inode->i_private);
+}
+
+static ssize_t wil_tx_latency_threshold_low_write(struct file *file,
+						  const char __user *buf,
+						  size_t len, loff_t *ppos)
+{
+	struct seq_file *s = file->private_data;
+	struct wil6210_priv *wil = s->private;
+	int rc;
+	u32 val;
+
+	rc = kstrtouint_from_user(buf, len, 0, &val);
+	if (rc) {
+		wil_err(wil, "Invalid argument\n");
+		return rc;
+	}
+
+	if (!wil->tx_latency_threshold_base) {
+		/* tx latency debug - failure can be safely ignored */
+		rc = wmi_ut_update_txlatency_base(wil);
+		wil_dbg_misc(wil, "ut_update_txlatency base returned %d\n", rc);
+	}
+
+	wil->tx_latency_threshold_low = val;
+
+	wil_info(wil, "Setting tx_latency_threshold_low to %d\n",
+		 wil->tx_latency_threshold_low);
+	return len;
+}
+
+static const struct file_operations fops_tx_latency_threshold_low = {
+	.open		= wil_tx_latency_threshold_low_seq_open,
+	.release	= single_release,
+	.read		= seq_read,
+	.write		= wil_tx_latency_threshold_low_write,
+	.llseek		= seq_lseek,
 };
 
 /* block ack control, write:
@@ -2616,6 +2667,7 @@ static const struct {
 	{"link_stats",	0644,		&fops_link_stats},
 	{"link_stats_global",	0644,	&fops_link_stats_global},
 	{"rbufcap",	0244,		&fops_rbufcap},
+	{"tx_latency_threshold_low", 0644, &fops_tx_latency_threshold_low},
 };
 
 static void wil6210_debugfs_init_files(struct wil6210_priv *wil,
@@ -2670,6 +2722,8 @@ static const struct dbg_off dbg_wil_off[] = {
 	WIL_FIELD(force_edmg_channel, 0644,	doff_u8),
 	WIL_FIELD(ap_ps, 0644, doff_u8),
 	WIL_FIELD(tx_reserved_entries, 0644, doff_u32),
+	WIL_FIELD(tx_latency_threshold_high,	0644,	doff_u32),
+	WIL_FIELD(tx_latency_threshold_info.threshold_detected, 0644, doff_u8),
 	{},
 };
 
