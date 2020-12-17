8aa58aebee1d0994a492ff621758a061ca3300a5
Amir Vajid <avajid@codeaurora.org>
2020-12-16 22:31:05 -0800

    soc: qcom: add driver for memory latency workloads
    Create a driver that monitors performance counters to
    identify memory latency bound workloads and vote for
    higher memory frequencies through the QCOM DCVS framework.
    
    This driver is a refactor of the existing arm-memlat-mon
    and governor_memlat drivers.
    
    Change-Id: Ia20e60372714851106a74caab2d8889aa269c2cd
    Signed-off-by: Amir Vajid <avajid@codeaurora.org>

--- drivers/soc/qcom/dcvs/trace-dcvs.h
+++ drivers/soc/qcom/dcvs/trace-dcvs.h
@@ -12,6 +12,81 @@
 #include <linux/tracepoint.h>
 #include <soc/qcom/dcvs.h>
 
+TRACE_EVENT(memlat_dev_meas,
+
+	TP_PROTO(const char *name, unsigned int dev_id, unsigned long inst,
+		 unsigned long mem, unsigned long freq, unsigned int stall,
+		 unsigned int wb, unsigned int ratio),
+
+	TP_ARGS(name, dev_id, inst, mem, freq, stall, wb, ratio),
+
+	TP_STRUCT__entry(
+		__string(name, name)
+		__field(unsigned int, dev_id)
+		__field(unsigned long, inst)
+		__field(unsigned long, mem)
+		__field(unsigned long, freq)
+		__field(unsigned int, stall)
+		__field(unsigned int, wb)
+		__field(unsigned int, ratio)
+	),
+
+	TP_fast_assign(
+		__assign_str(name, name);
+		__entry->dev_id = dev_id;
+		__entry->inst = inst;
+		__entry->mem = mem;
+		__entry->freq = freq;
+		__entry->stall = stall;
+		__entry->wb = wb;
+		__entry->ratio = ratio;
+	),
+
+	TP_printk("dev: %s, id=%u, inst=%lu, mem=%lu, freq=%lu, stall=%u, wb=%u, ratio=%u",
+		__get_str(name),
+		__entry->dev_id,
+		__entry->inst,
+		__entry->mem,
+		__entry->freq,
+		__entry->stall,
+		__entry->wb,
+		__entry->ratio)
+);
+
+TRACE_EVENT(memlat_dev_update,
+
+	TP_PROTO(const char *name, unsigned int dev_id, unsigned long inst,
+		 unsigned long mem, unsigned long freq, unsigned long vote),
+
+	TP_ARGS(name, dev_id, inst, mem, freq, vote),
+
+	TP_STRUCT__entry(
+		__string(name, name)
+		__field(unsigned int, dev_id)
+		__field(unsigned long, inst)
+		__field(unsigned long, mem)
+		__field(unsigned long, freq)
+		__field(unsigned long, vote)
+	),
+
+	TP_fast_assign(
+		__assign_str(name, name);
+		__entry->dev_id = dev_id;
+		__entry->inst = inst;
+		__entry->mem = mem;
+		__entry->freq = freq;
+		__entry->vote = vote;
+	),
+
+	TP_printk("dev: %s, id=%u, inst=%lu, mem=%lu, freq=%lu, vote=%lu",
+		__get_str(name),
+		__entry->dev_id,
+		__entry->inst,
+		__entry->mem,
+		__entry->freq,
+		__entry->vote)
+);
+
 #endif /* _TRACE_DCVS_H */
 
 #undef TRACE_INCLUDE_PATH
