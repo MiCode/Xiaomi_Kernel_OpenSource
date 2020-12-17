03a042cbc2c215cf2fb6d460a44f6c09b31a23c0
Amir Vajid <avajid@codeaurora.org>
2020-12-16 20:41:53 -0800

    soc: qcom: add driver for various dcvs voting interfaces
    Create a driver to manage voting interfaces for various
    dcvs hw types and voting paths.
    
    Change-Id: I1979489a2af3f5b547e6ab418a216d711a57c0c0
    Signed-off-by: Amir Vajid <avajid@codeaurora.org>

---
+++ drivers/soc/qcom/dcvs/trace-dcvs.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ */
+
+#include <soc/qcom/dcvs.h>
+
+#define CREATE_TRACE_POINTS
+#include "trace-dcvs.h"
+
