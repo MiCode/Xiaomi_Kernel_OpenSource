174b21fd76a3d7c26d0ddb97db1b2edaf0376901
Bhaumik Bhatt <bbhatt@codeaurora.org>
2020-12-17 10:38:13 -0800

    bus: mhi: core: Move mhi_to_virtual for misc access
    mhi_to_virtual() API needs to be moved to internal.h and exposed
    to MHI core driver so misc functionality can make use of it.
    
    Change-Id: Ic3ad3e0f601e863d940cbb17d16021c4f39920e9
    Signed-off-by: Bhaumik Bhatt <bbhatt@codeaurora.org>

--- drivers/bus/mhi/core/internal.h
+++ drivers/bus/mhi/core/internal.h
@@ -668,6 +668,7 @@ void mhi_write_db(struct mhi_controller *mhi_cntrl, void __iomem *db_addr,
 void mhi_ring_cmd_db(struct mhi_controller *mhi_cntrl, struct mhi_cmd *mhi_cmd);
 void mhi_ring_chan_db(struct mhi_controller *mhi_cntrl,
 		      struct mhi_chan *mhi_chan);
+void *mhi_to_virtual(struct mhi_ring *ring, dma_addr_t addr);
 
 /* Initialization methods */
 int mhi_init_mmio(struct mhi_controller *mhi_cntrl);
