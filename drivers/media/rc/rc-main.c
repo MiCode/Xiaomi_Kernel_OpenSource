b24dc0aa7e9b258e195f9a3af00d8480d1aff655
Sean Young <sean@mess.org>
2020-12-20 13:29:54 +0100

    media: rc: ensure that uevent can be read directly after rc device register
    commit 896111dc4bcf887b835b3ef54f48b450d4692a1d upstream.
    
    There is a race condition where if the /sys/class/rc0/uevent file is read
    before rc_dev->registered is set to true, -ENODEV will be returned.
    
    Link: https://bugs.launchpad.net/ubuntu/+source/linux/+bug/1901089
    
    Cc: stable@vger.kernel.org
    Fixes: a2e2d73fa281 ("media: rc: do not access device via sysfs after rc_unregister_device()")
    Signed-off-by: Sean Young <sean@mess.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/media/rc/rc-main.c
+++ drivers/media/rc/rc-main.c
@@ -1892,6 +1892,8 @@ int rc_register_device(struct rc_dev *dev)
 			goto out_raw;
 	}
 
+	dev->registered = true;
+
 	rc = device_add(&dev->dev);
 	if (rc)
 		goto out_rx_free;
@@ -1900,8 +1902,6 @@ int rc_register_device(struct rc_dev *dev)
 	dev_info(&dev->dev, "%s as %s\n",
 		 dev->device_name ?: "Unspecified device", path ?: "N/A");
 	kfree(path);
-
-	dev->registered = true;
 
 	/*
 	 * once the the input device is registered in rc_setup_rx_device,
