71290afaa23cd2705eef5d6a70ec2a1503880a73
SW Integrator <swintegrator@mediatek.com>
2020-12-24 12:22:23 +0800

    ASoC: mt6853: fix for_each_rtdcom build breaks
    Errors:
    sound/soc/mediatek/mt6853/mt6853-mt6359p.c:353:2: error: implicit declaration of function 'for_each_rtdcom' [-Werror,-Wimplicit-function-declaration]
    sound/soc/mediatek/mt6853/mt6853-mt6359p.c:353:30: error: expected ';' after expression
    sound/soc/mediatek/mt6853/mt6853-mt6359p.c:354:21: error: incomplete definition of type 'struct snd_soc_rtdcom_list'
    sound/soc/mediatek/mt6853/mt6853-mt6359p.c:373:2: error: implicit declaration of function 'for_each_rtdcom' [-Werror,-Wimplicit-function-declaration]
    sound/soc/mediatek/mt6853/mt6853-mt6359p.c:373:30: error: expected ';' after expression
    sound/soc/mediatek/mt6853/mt6853-mt6359p.c:374:21: error: incomplete definition of type 'struct snd_soc_rtdcom_list'
    
    Change-Id: If805dd8db7e02ae56727680241920e1a0f6a21c8
    CR-Id: ALPS05502217
    Feature: [Module]Kernel Maintenance
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

--- sound/soc/mediatek/mt6853/mt6853-mt6359p.c
+++ sound/soc/mediatek/mt6853/mt6853-mt6359p.c
@@ -342,7 +342,7 @@ static int mt6853_mt6359p_vow_startup(struct snd_pcm_substream *substream)
 	struct snd_soc_component *component =
 			snd_soc_rtdcom_lookup(rtd, AFE_PCM_NAME);
 	struct mtk_base_afe *afe = snd_soc_component_get_drvdata(component);
-	struct snd_soc_rtdcom_list *rtdcom;
+	int i;
 
 	dev_info(afe->dev, "%s(), start\n", __func__);
 	snd_soc_set_runtime_hwparams(substream, &mt6853_mt6359p_vow_hardware);
@@ -350,8 +350,7 @@ static int mt6853_mt6359p_vow_startup(struct snd_pcm_substream *substream)
 	mt6853_afe_gpio_request(afe, true, MT6853_DAI_VOW, 0);
 
 	/* ASoC will call pm_runtime_get, but vow don't need */
-	for_each_rtdcom(rtd, rtdcom) {
-		component = rtdcom->component;
+	for_each_rtd_components(rtd, i, component) {
 		pm_runtime_put_autosuspend(component->dev);
 	}
 
@@ -364,14 +363,13 @@ static void mt6853_mt6359p_vow_shutdown(struct snd_pcm_substream *substream)
 	struct snd_soc_component *component =
 			snd_soc_rtdcom_lookup(rtd, AFE_PCM_NAME);
 	struct mtk_base_afe *afe = snd_soc_component_get_drvdata(component);
-	struct snd_soc_rtdcom_list *rtdcom;
+	int i;
 
 	dev_info(afe->dev, "%s(), end\n", __func__);
 	mt6853_afe_gpio_request(afe, false, MT6853_DAI_VOW, 0);
 
 	/* restore to fool ASoC */
-	for_each_rtdcom(rtd, rtdcom) {
-		component = rtdcom->component;
+	for_each_rtd_components(rtd, i, component) {
 		pm_runtime_get_sync(component->dev);
 	}
 }
