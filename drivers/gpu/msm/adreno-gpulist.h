e994cb44c29aaf75e73a86b665c898966de124cd
Pankaj Gupta <gpankaj@codeaurora.org>
2020-12-28 22:20:07 +0530

    msm: kgsl: Enable Preemption on A635 GPU
    Enable Preemption feature on A635 GPU.
    
    Change-Id: Ieea4cde2fd8678fc50d7d67b3233527a78cfae7a
    Signed-off-by: Pankaj Gupta <gpankaj@codeaurora.org>

--- drivers/gpu/msm/adreno-gpulist.h
+++ drivers/gpu/msm/adreno-gpulist.h
@@ -1685,7 +1685,8 @@ static const struct adreno_a6xx_core adreno_gpu_core_a635 = {
 	.base = {
 		DEFINE_ADRENO_REV(ADRENO_REV_A635, 6, 3, 5, ANY_ID),
 		.features = ADRENO_RPMH | ADRENO_GPMU | ADRENO_APRIV |
-				ADRENO_IOCOHERENT | ADRENO_CONTENT_PROTECTION,
+				ADRENO_IOCOHERENT | ADRENO_CONTENT_PROTECTION |
+				ADRENO_PREEMPTION,
 		.gpudev = &adreno_a6xx_gmu_gpudev,
 		.perfcounters = &adreno_a6xx_perfcounters,
 		.gmem_base = 0,
