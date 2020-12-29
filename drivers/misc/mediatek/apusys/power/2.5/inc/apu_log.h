76d49b0bd90f96eb51894ad2c907f1612fcf2b65
c-c.kuo <c-c.kuo@mediatek.com>
2020-12-29 15:19:13 +0800

    apusys power: enable AEE
    [Detail]
    enable AEE
    
    (cherry-picked from commit c67dd6729bd7768e07516bb083effdd2eefe600f)
    Signed-off-by: c-c.kuo <c-c.kuo@mediatek.com>
    Change-Id: I0f2766c8b8ca4eeeae5d8d6be63fed81c8d37fc9
    CR-Id: ALPS05511922
    Feature: [Module] APUSYS
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

--- drivers/misc/mediatek/apusys/power/2.5/inc/apu_log.h
+++ drivers/misc/mediatek/apusys/power/2.5/inc/apu_log.h
@@ -9,15 +9,21 @@
 
 #include "apu_dbg.h"
 #include "apu_common.h"
+#include "mt-plat/aee.h"
 
 #define APUCLK_PRE	"clk"
 #define aclk_err(dev, fmt, ...) \
-	pr_info("[%s:"APUCLK_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__)
-#define aclk_info(dev, fmt, ...) \
-do { \
-	if (apupw_dbg_get_loglvl() > NO_LVL) \
+	do { \
 		pr_info("[%s:"APUCLK_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__); \
-} while (0)
+		aee_kernel_warning("APU PWR", \
+			"\nCRDISPATCH_KEY:[%s:"APUCLK_PRE"]" fmt, \
+			apu_dev_name(dev), ##__VA_ARGS__); \
+	} while (0)
+#define aclk_info(dev, fmt, ...) \
+	do { \
+		if (apupw_dbg_get_loglvl() > NO_LVL) \
+			pr_info("[%s:"APUCLK_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__); \
+	} while (0)
 #define aclk_warn(dev, fmt, ...) \
 	pr_info("[%s:"APUCLK_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__)
 #define aclk_debug(dev, fmt, ...) \
@@ -25,12 +31,17 @@ do { \
 
 #define APURGU_PRE	"rgu"
 #define argul_err(dev, fmt, ...) \
-	pr_info("[%s:"APURGU_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__)
-#define argul_info(dev, fmt, ...) \
-do { \
-	if (apupw_dbg_get_loglvl() > NO_LVL) \
+	do { \
 		pr_info("[%s:"APURGU_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__); \
-} while (0)
+		aee_kernel_warning("APU PWR", \
+			"\nCRDISPATCH_KEY:[%s:"APURGU_PRE"]" fmt, \
+			apu_dev_name(dev), ##__VA_ARGS__); \
+	} while (0)
+#define argul_info(dev, fmt, ...) \
+	do { \
+		if (apupw_dbg_get_loglvl() > NO_LVL) \
+			pr_info("[%s:"APURGU_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__); \
+	} while (0)
 #define argul_warn(dev, fmt, ...) \
 	pr_info("[%s:"APURGU_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__)
 #define argul_debug(dev, fmt, ...) \
@@ -38,12 +49,17 @@ do { \
 
 #define APUPROBE_PRE	"probe"
 #define aprobe_err(dev, fmt, ...) \
-	pr_info("[%s:"APUPROBE_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__)
-#define aprobe_info(dev, fmt, ...) \
-do { \
-	if (apupw_dbg_get_loglvl() > NO_LVL) \
+	do { \
 		pr_info("[%s:"APUPROBE_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__); \
-} while (0)
+		aee_kernel_warning("APU PWR", \
+			"\nCRDISPATCH_KEY:[%s:"APUPROBE_PRE"]" fmt, \
+			apu_dev_name(dev), ##__VA_ARGS__); \
+	} while (0)
+#define aprobe_info(dev, fmt, ...) \
+	do { \
+		if (apupw_dbg_get_loglvl() > NO_LVL) \
+			pr_info("[%s:"APUPROBE_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__); \
+	} while (0)
 #define aprobe_warn(dev, fmt, ...) \
 	pr_info("[%s:"APUPROBE_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__)
 #define aprobe_debug(dev, fmt, ...) \
@@ -51,7 +67,12 @@ do { \
 
 #define APUPW_PRE	"power"
 #define apower_err(dev, fmt, ...) \
-	pr_info("[%s:"APUPW_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__)
+	do { \
+		pr_info("[%s:"APUPW_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__); \
+		aee_kernel_warning("APU PWR", \
+			"\nCRDISPATCH_KEY:[%s:"APUPW_PRE"]" fmt, \
+			apu_dev_name(dev), ##__VA_ARGS__); \
+	} while (0)
 #define apower_info(dev, fmt, ...) \
 do { \
 	if (apupw_dbg_get_loglvl() > NO_LVL) \
@@ -64,7 +85,12 @@ do { \
 
 #define APURPC_PRE	"rpc"
 #define arpc_err(dev, fmt, ...) \
-	pr_info("[%s:"APURPC_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__)
+	do { \
+		pr_info("[%s:"APURPC_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__); \
+		aee_kernel_warning("APU PWR", \
+			"\nCRDISPATCH_KEY:[%s:"APURPC_PRE"]" fmt, \
+			apu_dev_name(dev), ##__VA_ARGS__); \
+	} while (0)
 #define arpc_info(dev, fmt, ...) \
 do { \
 	if (apupw_dbg_get_loglvl() > NO_LVL) \
@@ -78,7 +104,12 @@ do { \
 
 #define APUSPM_PRE	"spm"
 #define aspm_err(dev, fmt, ...) \
-	pr_info("[%s:"APUSPM_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__)
+	do { \
+		pr_info("[%s:"APUSPM_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__); \
+		aee_kernel_warning("APU PWR", \
+			"\nCRDISPATCH_KEY:[%s:"APUSPM_PRE"]" fmt, \
+			apu_dev_name(dev), ##__VA_ARGS__); \
+	} while (0)
 #define aspm_info(dev, fmt, ...) \
 do { \
 	if (apupw_dbg_get_loglvl() > NO_LVL) \
@@ -91,7 +122,12 @@ do { \
 
 #define APUDVFS_PRE	"dvfs"
 #define advfs_err(dev, fmt, ...) \
-	pr_info("[%s:"APUDVFS_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__)
+	do { \
+		pr_info("[%s:"APUDVFS_PRE"]" fmt, apu_dev_name(dev), ##__VA_ARGS__); \
+		aee_kernel_warning("APU PWR", \
+			"\nCRDISPATCH_KEY:[%s:"APUDVFS_PRE"]" fmt, \
+			apu_dev_name(dev), ##__VA_ARGS__); \
+	} while (0)
 #define advfs_info(dev, fmt, ...) \
 do { \
 	if (apupw_dbg_get_loglvl() > NO_LVL) \
