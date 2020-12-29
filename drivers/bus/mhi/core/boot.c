f562b75c53057d8ed3d8a39affcadd4068ece16f
Bhaumik Bhatt <bbhatt@codeaurora.org>
2020-12-28 18:32:58 -0800

    bus: mhi: core: Use parent device to request firmware
    Parent device must be used to request firmware from the file-
    system otherwise the request does not go through. Make these
    necessary changes to allow boot-up for all MHI controllers.
    
    Change-Id: Ia78398c5d2d472a2f6f7e3e652f62b75927bd379
    Signed-off-by: Bhaumik Bhatt <bbhatt@codeaurora.org>

--- drivers/bus/mhi/core/boot.c
+++ drivers/bus/mhi/core/boot.c
@@ -438,7 +438,7 @@ void mhi_fw_load_handler(struct mhi_controller *mhi_cntrl)
 		goto error_fw_load;
 	}
 
-	ret = request_firmware(&firmware, fw_name, dev);
+	ret = request_firmware(&firmware, fw_name, dev->parent);
 	if (ret) {
 		if (!mhi_cntrl->fallback_fw_image) {
 			MHI_ERR("Error loading firmware: %d\n", ret);
@@ -447,7 +447,7 @@ void mhi_fw_load_handler(struct mhi_controller *mhi_cntrl)
 
 		ret = request_firmware(&firmware,
 				       mhi_cntrl->fallback_fw_image,
-				       dev);
+				       dev->parent);
 		if (ret) {
 			MHI_ERR("Error loading fallback firmware: %d\n",
 				ret);
