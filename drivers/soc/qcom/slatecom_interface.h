8f6d9d8de088e71b3600d3f82123d918c9a33ede
Ajit Kumar <kajit@codeaurora.org>
2021-01-09 15:11:40 +0530

    slatecom_interface: add slatecom_interface driver
    slatecom_interface driver is use for communication from
    slate PIL and slatecom spi to userspace.
    Changes are ported from bgcom_interface driver.
    
    Change-Id: I92379ca3d691a17f8e6507f1135f2ab340a9a8ae
    Signed-off-by: Ajit Kumar <kajit@codeaurora.org>

---
+++ drivers/soc/qcom/slatecom_interface.h
@@ -0,0 +1,29 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
+ */
+#ifndef SLATECOM_INTERFACE_H
+#define SLATECOM_INTERFACE_H
+
+/*
+ * slate_soft_reset() - soft reset Blackghost
+ * Return 0 on success or -Ve on error
+ */
+int slate_soft_reset(void);
+
+/*
+ * is_twm_exit()
+ * Return true if device is booting up on TWM exit.
+ * value is auto cleared once read.
+ */
+bool is_twm_exit(void);
+
+/*
+ * is_slate_running()
+ * Return true if slate is running.
+ * value is auto cleared once read.
+ */
+bool is_slate_running(void);
+
+#endif /* SLATECOM_INTERFACE_H */
+
