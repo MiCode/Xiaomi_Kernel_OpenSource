9ad362fbfb43f4403f2674f08d3292830d532016
Greg Kroah-Hartman <gregkh@google.com>
2021-01-11 14:19:37 +0100

    Revert "Revert "exec: Fix a deadlock in strace""
    This reverts commit 2ca0318f1b788e9741d4e9505d63de80d085aa14 which is
    commit 3e74fabd39710ee29fa25618d2c2b40cfa7d76c7 upstream.  We want it
    back for a bit to help with merges only.  It will be dropped again
    afterward...
    
    Bug: 161946584
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Ie32b2a8963d380332f7954ce40880a37f2a4473b

--- kernel/fork.c
+++ kernel/fork.c
@@ -1232,7 +1232,7 @@ struct mm_struct *mm_access(struct task_struct *task, unsigned int mode)
 	struct mm_struct *mm;
 	int err;
 
-	err =  mutex_lock_killable(&task->signal->cred_guard_mutex);
+	err =  mutex_lock_killable(&task->signal->exec_update_mutex);
 	if (err)
 		return ERR_PTR(err);
 
@@ -1242,7 +1242,7 @@ struct mm_struct *mm_access(struct task_struct *task, unsigned int mode)
 		mmput(mm);
 		mm = ERR_PTR(-EACCES);
 	}
-	mutex_unlock(&task->signal->cred_guard_mutex);
+	mutex_unlock(&task->signal->exec_update_mutex);
 
 	return mm;
 }
