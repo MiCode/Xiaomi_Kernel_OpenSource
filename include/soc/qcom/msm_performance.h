a745afb8bbe173256e5a3155fc556479805f78e3
Kishore Sri venkata Ganesh Bolisetty <bsrivenk@codeaurora.org>
2021-01-12 03:32:18 -0800

    soc: qcom: msm_perf: Port msm_performance from 5.4 to 5.10
    Port msm_performance from 5.4 to 5.10.The following commits
    were included as a part of this change.
    
    b712bdb msm: kgsl: Including drawobj flag for msm-perf api
    206e048 soc: qcom: msm_perf: enable msm_perf driver on default config
    9d8197e drivers: soc: qcom: msm_performance: plh interface support
    1df107b msm-perf: Added threshold check for perf_event
    0ef671a msm-perf: Removed game active status node
    35dd020 soc: qcom: msm_perf: Allow freq updates regardless of policy
    9cb001d msm-perf: Fixed kzalloc for perf_event
    27bee16 msm-perf: Extended support for perf notification
    ee81f18 msm-perf: Added support for Perf data collection.
    
    Change-Id: If74c9a649f2ead7ca8167e4047bcb40401e70ca8
    Signed-off-by: Kishore Sri venkata Ganesh Bolisetty <bsrivenk@codeaurora.org>

--- include/soc/qcom/msm_performance.h
+++ include/soc/qcom/msm_performance.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
  */
 
 #ifndef __MSM_PERFORMANCE_H
@@ -17,13 +17,7 @@ enum evt_update_t {
 	MSM_PERF_GFX,
 };
 
-#ifdef CONFIG_MSM_PERFORMANCE
 void msm_perf_events_update(enum evt_update_t update_typ,
 			enum gfx_evt_t evt_typ, pid_t pid,
-			uint32_t ctx_id, uint32_t timestamp);
-#else
-static inline void msm_perf_events_update(enum evt_update_t update_typ,
-			enum gfx_evt_t evt_typ, pid_t pid,
-			uint32_t ctx_id, uint32_t timestamp){}
-#endif
+			uint32_t ctx_id, uint32_t timestamp, bool end_of_frame);
 #endif
