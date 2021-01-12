d50643f773112d5316b09cebd6214bf851bf7667
Isaac J. Manjarres <isaacm@codeaurora.org>
2021-01-12 10:47:26 -0800

    qcom-io-pgtable-arm: Add support for IO_PGTABLE_QUIRK_QCOM_USE_LLC_NWA
    The IO_PGTABLE_QUIRK_QCOM_USE_LLC_NWA quirk is used to ensure that
    the IOMMU page tables are cached in the system cache with a no write
    allocation cache policy. Add support for it by setting up the TCR
    with the following memory attributes for the page table walker:
    
    TCR.SH = Outer-shareable
    TCR.IRGN = Non-cacheable normal memory
    TCR.ORGN = Write-back, no write-allocate cacheable.
    
    Change-Id: Ifa88b673de3b756e5b03bc36e89db84bc013346a
    Signed-off-by: Isaac J. Manjarres <isaacm@codeaurora.org>

--- drivers/iommu/qcom-io-pgtable-arm.c
+++ drivers/iommu/qcom-io-pgtable-arm.c
@@ -771,7 +771,8 @@ arm_64_lpae_alloc_pgtable_s1(struct io_pgtable_cfg *cfg, void *cookie)
 	if (cfg->quirks & ~(IO_PGTABLE_QUIRK_ARM_NS |
 			    IO_PGTABLE_QUIRK_NON_STRICT |
 			    IO_PGTABLE_QUIRK_ARM_TTBR1 |
-			    IO_PGTABLE_QUIRK_QCOM_USE_UPSTREAM_HINT))
+			    IO_PGTABLE_QUIRK_QCOM_USE_UPSTREAM_HINT |
+			    IO_PGTABLE_QUIRK_QCOM_USE_LLC_NWA))
 		return NULL;
 
 	data = arm_lpae_alloc_pgtable(cfg);
@@ -787,6 +788,10 @@ arm_64_lpae_alloc_pgtable_s1(struct io_pgtable_cfg *cfg, void *cookie)
 		tcr->sh = ARM_LPAE_TCR_SH_OS;
 		tcr->irgn = ARM_LPAE_TCR_RGN_NC;
 		tcr->orgn = ARM_LPAE_TCR_RGN_WBWA;
+	} else if (cfg->quirks & IO_PGTABLE_QUIRK_QCOM_USE_LLC_NWA) {
+		tcr->sh = ARM_LPAE_TCR_SH_OS;
+		tcr->irgn = ARM_LPAE_TCR_RGN_NC;
+		tcr->orgn = ARM_LPAE_TCR_RGN_WB;
 	} else {
 		tcr->sh = ARM_LPAE_TCR_SH_OS;
 		tcr->irgn = ARM_LPAE_TCR_RGN_NC;
