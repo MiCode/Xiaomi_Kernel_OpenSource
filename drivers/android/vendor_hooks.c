7285ca6e97203728ff5eddcbabedd27ca72576aa
Frankie Chang <frankie.chang@mediatek.com>
2021-01-13 10:45:58 +0800

    ANDROID: Add vendor hooks when syscall prctl finished
    Add vendor hook when syscall prctl finished for vendor-specific tuning.
    
    Bug: 181819699
    
    Signed-off-by: Frankie Chang <frankie.chang@mediatek.com>
    Change-Id: Ica42d80ab4b540045330e9c5b211e0e814eed0ff
    (cherry picked from commit 6904f0001e4ec3c637539c18969e46755eaa01f4)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -21,6 +21,7 @@
 #include <trace/hooks/wqlockup.h>
 #include <trace/hooks/sysrqcrash.h>
 #include <trace/hooks/cgroup.h>
+#include <trace/hooks/sys.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -71,3 +72,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_find_busiest_group);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_map_util_freq);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_em_pd_energy);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cgroup_set_task);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_syscall_prctl_finished);
