cdfa32e497b53631cdbc2488b96692845ac710d6
Hank Liao <Hank.Liao@mediatek.com>
2021-01-13 09:18:38 +0800

    [ALPS05508540] CLKBUF: CLKBUF early porting in kernel
    1. CLKBUF early porting in kernel
    
    2. Disable CLKBUF for bringup
    
    MTK-Commit-Id: 1a2a46ddd7902aed9a69870a975023b5000d247b
    
    Change-Id: Ieffd9cfa1b5a49b250ebd3903f814d2b548fa84d
    Signed-off-by: Hank Liao <Hank.Liao@mediatek.com>
    CR-Id: ALPS05508540
    Feature: [Module]Clock Buffer

--- drivers/misc/mediatek/base/power/clkbuf_v1/mt6853/mtk_clkbuf_hw.c
+++ drivers/misc/mediatek/base/power/clkbuf_v1/mt6853/mtk_clkbuf_hw.c
@@ -125,7 +125,9 @@ static void __iomem *spm_base;
 #define XO_BUF7_BBLPM_EN_MASK	(0x1 << 7)
 
 /* TODO: marked this after driver is ready */
-/* #define CLKBUF_BRINGUP */
+#if defined(CONFIG_MACH_MT6877)
+#define CLKBUF_BRINGUP
+#endif /* for Bring up */
 /* #define CLKBUF_CONN_SUPPORT_CTRL_FROM_I1 */
 
 #define BUF_MAN_M				0
