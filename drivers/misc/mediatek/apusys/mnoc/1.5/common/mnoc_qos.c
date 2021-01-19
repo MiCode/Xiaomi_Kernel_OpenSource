2b0c2a72ddd64e1a4ea0bec634ef2820c234ea55
c-c.kuo <c-c.kuo@mediatek.com>
2021-01-19 10:22:26 +0800

    apusys power: adopt mtk_qosk api for index and bandwidth
    [Detail]
    adopt mtk_qosk api for index and bandwidth.
    
    (cherry-picked from commit d468dfa48b330c56f736e903f4d02b8059d3c516)
    Signed-off-by: c-c.kuo <c-c.kuo@mediatek.com>
    Change-Id: I9e8604832632918322d3972045d7641e7fc10581
    CR-Id: ALPS05547109
    Feature: [Module] APUSYS
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

--- drivers/misc/mediatek/apusys/mnoc/1.5/common/mnoc_qos.c
+++ drivers/misc/mediatek/apusys/mnoc/1.5/common/mnoc_qos.c
@@ -426,7 +426,7 @@ static void qos_work_func(struct work_struct *work)
 		return;
 	}
 
-	current_idx = qos_info->idx;
+	current_idx = get_qos_bound_idx();
 
 	for (i = 0; i < NR_APU_QOS_ENGINE; i++)	{
 		peak_bw = 0;
@@ -441,11 +441,11 @@ static void qos_work_func(struct work_struct *work)
 			continue;
 		do {
 			idx = (idx + 1) % MTK_QOS_BUF_SIZE;
-			bw = qos_info->stats[idx].smibw_mon[qos_smi_idx];
+			bw = get_qos_bound_smibw_mon(idx, qos_smi_idx);
 			total_bw += bw;
 			cnt++;
 			peak_bw = peak_bw > bw ? peak_bw : bw;
-		} while (idx != qos_info->idx);
+		} while (idx != ((get_qos_bound_idx() + 1) % MTK_QOS_BUF_SIZE));
 
 		LOG_DETAIL("idx[%d](%d ~ %d)\n", i, counter->last_idx, idx);
 
