acfaa3c73b95e0f4db0249a2146752bf77ccc8df
Kishore Sri venkata Ganesh Bolisetty <bsrivenk@codeaurora.org>
2021-01-20 00:06:53 -0800

    binder: update low_latency selection for binder transactions
    The changes enables low latency for 120 priority FG binder
    transactions in the rendering path.
    
    Change-Id: I5736611c7f139362c54d0f0e73f07dfe078c8848
    Signed-off-by: Kishore Sri venkata Ganesh Bolisetty <bsrivenk@codeaurora.org>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -964,7 +964,8 @@ static void binder_wakeup_poll_threads_ilocked(struct binder_proc *proc,
 #ifdef CONFIG_SCHED_WALT
 			if (thread->task && current->signal &&
 				(current->signal->oom_score_adj == 0) &&
-				(current->prio < DEFAULT_PRIO))
+				((current->prio < DEFAULT_PRIO) ||
+					(thread->task->group_leader->prio < MAX_RT_PRIO)))
 				thread->task->wts.low_latency |=
 						WALT_LOW_LATENCY_BINDER;
 #endif
@@ -1030,7 +1031,8 @@ static void binder_wakeup_thread_ilocked(struct binder_proc *proc,
 #ifdef CONFIG_SCHED_WALT
 		if (thread->task && current->signal &&
 			(current->signal->oom_score_adj == 0) &&
-			(current->prio < DEFAULT_PRIO))
+			((current->prio < DEFAULT_PRIO) ||
+				(thread->task->group_leader->prio < MAX_RT_PRIO)))
 			thread->task->wts.low_latency |=
 					WALT_LOW_LATENCY_BINDER;
 #endif
