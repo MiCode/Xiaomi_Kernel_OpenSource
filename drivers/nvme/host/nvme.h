4bf6c84c9ad326016c179de5ba4f6e868e07acc7
Chao Leng <lengchao@huawei.com>
2021-01-21 11:32:36 +0800

    nvme-core: add cancel tagset helpers
    [ Upstream commit 2547906982e2e6a0d42f8957f55af5bb51a7e55f ]
    
    Add nvme_cancel_tagset and nvme_cancel_admin_tagset for tear down and
    reconnection error handling.
    
    Signed-off-by: Chao Leng <lengchao@huawei.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/nvme/host/nvme.h
+++ drivers/nvme/host/nvme.h
@@ -468,6 +468,8 @@ static inline void nvme_put_ctrl(struct nvme_ctrl *ctrl)
 
 void nvme_complete_rq(struct request *req);
 bool nvme_cancel_request(struct request *req, void *data, bool reserved);
+void nvme_cancel_tagset(struct nvme_ctrl *ctrl);
+void nvme_cancel_admin_tagset(struct nvme_ctrl *ctrl);
 bool nvme_change_ctrl_state(struct nvme_ctrl *ctrl,
 		enum nvme_ctrl_state new_state);
 bool nvme_wait_reset(struct nvme_ctrl *ctrl);
