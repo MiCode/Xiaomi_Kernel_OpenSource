8e01c319512a56a6612278d6216e0b8da966a517
mtk16547 <Zhuang.Wang@mediatek.com>
2021-01-29 02:23:22 +0800

    [ALPS05285337] CAM:fix 6853_alpha eeprom
    [Detail] add eeprom i2c addr
    
    MTK-Commit-Id: 7d506f838bf4d1244a7be73680a73247c1164e60
    
    Change-Id: I72606576760752fb1e42931f0297e91708dce5c3
    Signed-off-by: mtk16547 <Zhuang.Wang@mediatek.com>
    CR-Id: ALPS05285337
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/misc/mediatek/cam_cal/src/mt6853/cam_cal_list.c
+++ drivers/misc/mediatek/cam_cal/src/mt6853/cam_cal_list.c
@@ -30,6 +30,8 @@ struct stCAM_CAL_LIST_STRUCT g_camCalList[] = {
 	{IMX686_SENSOR_ID, 0xA0, Common_read_region, MAX_EEPROM_SIZE_16K},
 	{HI846_SENSOR_ID, 0xA0, Common_read_region, MAX_EEPROM_SIZE_16K},
 	{S5KGD1SP_SENSOR_ID, 0xA8, Common_read_region, MAX_EEPROM_SIZE_16K},
+	{OV16A10_SENSOR_ID, 0xA0, Common_read_region, MAX_EEPROM_SIZE_16K},
+	{S5K3P9SP_SENSOR_ID, 0xA8, Common_read_region},
 	{S5K2T7SP_SENSOR_ID, 0xA4, Common_read_region},
 	{IMX386_SENSOR_ID, 0xA0, Common_read_region},
 	{S5K2L7_SENSOR_ID, 0xA0, Common_read_region},
