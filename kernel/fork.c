3e0ae9675aa2d15a07a97b180370a5229df1d863
Walter-zh Wu <walter-zh.wu@mediatek.com>
2021-01-29 02:11:02 +0800

    [ALPS04941863] kasan: enable stack instrumentation in Tag-Based KASAN
    Need to fix it, otherwise there are many false-positives.
    
    Fixed:
    1. Enable stack checking with KASAN_OUTLINE
    2. When fork process, then reset address tag for kernel stack, so that
       it will not have false-positives after KASAN initialization.
    3. Disable stack instrumentation in init.c in order to avoid start_kernel()
       have some false-positives.
    
    MTK-Commit-Id: 3b2a21df6e99cc045ca8bba8d3a1fe13705f55ba
    
    Change-Id: I6302755d08b8b44873eea587d3c309ac6e81f623
    Signed-off-by: Walter-zh Wu <walter-zh.wu@mediatek.com>
    CR-Id:ALPS04941863
    Feature:Memory Optimization

--- kernel/fork.c
+++ kernel/fork.c
@@ -246,6 +246,7 @@ static unsigned long *alloc_thread_stack_node(struct task_struct *tsk, int node)
 
 	if (likely(page)) {
 		tsk->stack = page_address(page);
+		tsk->stack = kasan_reset_tag(tsk->stack);
 		return tsk->stack;
 	}
 	return NULL;
@@ -281,6 +282,7 @@ static unsigned long *alloc_thread_stack_node(struct task_struct *tsk,
 {
 	unsigned long *stack;
 	stack = kmem_cache_alloc_node(thread_stack_cache, THREADINFO_GFP, node);
+	stack = kasan_reset_tag(stack);
 	tsk->stack = stack;
 	return stack;
 }
