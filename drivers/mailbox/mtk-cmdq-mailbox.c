6e11ccf0418d6a0d734a86bbb48a69bdf2d70d77
Joe Hong <joe.hong@mediatek.com>
2021-01-28 22:20:35 +0800

    [ALPS05127269] cmdq: fix CMDQ mbox channel allocate issue
    [Detail]
    1. cmdq allocate mbox chan memory according to thread size.
    2. However, assigned oversize number of thread struct for it,
       which will cause kasan project boot failed.
    
    [Solution]
    Change the size in for loop to thread size of specific platform
    
    MTK-Commit-Id: 6cb2937be3f22ef4ad3c871b5a60f0c07f51a8a1
    
    Change-Id: Iff28060bc1eee682ad851d8bbeaefd3cef74e4b7
    Signed-off-by: Joe Hong <joe.hong@mediatek.com>
    CR-Id: ALPS05127269
    Feature: [Module]CMDQ Driver

--- drivers/mailbox/mtk-cmdq-mailbox.c
+++ drivers/mailbox/mtk-cmdq-mailbox.c
@@ -1205,7 +1205,7 @@ void cmdq_thread_dump_all(void *mbox_cmdq)
 	if (usage <= 0)
 		return;
 
-	for (i = 0; i < ARRAY_SIZE(cmdq->thread); i++) {
+	for (i = 0; i < cmdq->thread_nr; i++) {
 		struct cmdq_thread *thread = &cmdq->thread[i];
 
 		if (!thread->occupied || list_empty(&thread->task_busy_list))
@@ -1237,7 +1237,7 @@ void cmdq_thread_dump_all_seq(void *mbox_cmdq, struct seq_file *seq)
 	if (usage <= 0)
 		return;
 
-	for (i = 0; i < ARRAY_SIZE(cmdq->thread); i++) {
+	for (i = 0; i < cmdq->thread_nr; i++) {
 		struct cmdq_thread *thread = &cmdq->thread[i];
 
 		if (!thread->occupied || list_empty(&thread->task_busy_list))
@@ -1420,7 +1420,7 @@ static int cmdq_suspend(struct device *dev)
 
 	cmdq->suspended = true;
 
-	for (i = 0; i < ARRAY_SIZE(cmdq->thread); i++) {
+	for (i = 0; i < cmdq->thread_nr; i++) {
 		thread = &cmdq->thread[i];
 		if (!list_empty(&thread->task_busy_list)) {
 			cmdq_mbox_thread_stop(thread);
@@ -1670,7 +1670,7 @@ static int cmdq_probe(struct platform_device *pdev)
 
 	cmdq->mbox.dev = dev;
 	cmdq->mbox.chans = devm_kcalloc(dev, plat_data->thread_nr,
-					sizeof(*cmdq->mbox.chans), GFP_KERNEL);
+			sizeof(*cmdq->mbox.chans), GFP_KERNEL);
 	if (!cmdq->mbox.chans)
 		return -ENOMEM;
 
@@ -1682,7 +1682,7 @@ static int cmdq_probe(struct platform_device *pdev)
 	cmdq->mbox.txdone_irq = false;
 	cmdq->mbox.txdone_poll = false;
 
-	for (i = 0; i < ARRAY_SIZE(cmdq->thread); i++) {
+	for (i = 0; i < cmdq->thread_nr; i++) {
 		cmdq->thread[i].base = cmdq->base + CMDQ_THR_BASE +
 				CMDQ_THR_SIZE * i;
 		cmdq->thread[i].gce_pa = cmdq->base_pa;
