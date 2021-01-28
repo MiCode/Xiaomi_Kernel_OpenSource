4fad0c7ad814d8c5be05d3909ba46e4b9a1c075e
Juju Sung <juju.sung@mediatek.com>
2021-01-28 14:00:44 +0800

    [ALPS04844246] core: platform configs: add sync func support
    add mt_reg_sync_writeX related marco.
    
    MTK-Commit-Id: ed469a983350f231f1473a1afdda9b2b195cac16
    
    Change-Id: I33a2f63bdadb67bf9c7b37e65ed465a944720d79
    Signed-off-by: Juju Sung <juju.sung@mediatek.com>
    CR-Id: ALPS04844246
    Feature: [Module]Kernel Maintenance

---
+++ drivers/misc/mediatek/include/mt-plat/sync_write.h
@@ -0,0 +1,68 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#ifndef MT_SYNC_WRITE_H
+#define MT_SYNC_WRITE_H
+
+#if defined(__KERNEL__)
+
+#include <linux/io.h>
+#include <asm/cacheflush.h>
+
+/*
+ * Define macros.
+ */
+#define mt_reg_sync_writel(v, a) mt65xx_reg_sync_writel(v, a)
+
+#define mt_reg_sync_writew(v, a) mt65xx_reg_sync_writew(v, a)
+
+#define mt_reg_sync_writeb(v, a) mt65xx_reg_sync_writeb(v, a)
+
+#ifdef CONFIG_64BIT
+#define mt_reg_sync_writeq(v, a) \
+	do {    \
+		__raw_writeq((v), (void __force __iomem *)((a)));   \
+		dsb();  \
+	} while (0u == 1u)
+#endif
+
+#else				/* __KERNEL__ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+#define mt_reg_sync_writel(v, a)        mt65xx_reg_sync_writel(v, a)
+#define mt_reg_sync_writew(v, a)        mt65xx_reg_sync_writew(v, a)
+#define mt_reg_sync_writeb(v, a)        mt65xx_reg_sync_writeb(v, a)
+
+#define dsb() \
+	{    \
+		__asm__ __volatile__("dsb sy" : : : "memory"); \
+	}
+
+#define mt65xx_reg_sync_writel(v, a) \
+	do {    \
+		*(volatile unsigned int *)(a) = (v);    \
+		dsb(); \
+	} while (0)
+
+#define mt65xx_reg_sync_writew(v, a) \
+	do {    \
+		*(volatile unsigned short *)(a) = (v);    \
+		dsb(); \
+	} while (0)
+
+#define mt65xx_reg_sync_writeb(v, a) \
+	do {    \
+		*(volatile unsigned char *)(a) = (v);    \
+		dsb(); \
+	} while (0)
+
+#endif				/* __KERNEL__ */
+
+#endif				/* !MT_SYNC_WRITE_H */
