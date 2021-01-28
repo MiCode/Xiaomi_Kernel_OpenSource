df10c6e44ee974b0472f99a5a17662050f7134d1
YT Chang <yt.chang@mediatek.com>
2021-01-28 16:33:06 +0800

    [ALPS04222778] sched: phase out system-wide overutilization
    in kernel-4.19, ARMv8.2 has only 1 cluster.
    So phase out system-wide overutilization.
    
    MTK-Commit-Id: 6432b70f5210de39348458fea9acbcdeeb05e108
    
    Change-Id: If4a4616d4186716bcbb2251a79b990f5b88ee0aa
    Signed-off-by: YT Chang <yt.chang@mediatek.com>
    CR-Id: ALPS04222778
    Feature: Energy Aware Scheduling Plus (EAS+)

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5215,85 +5215,10 @@ static inline bool cpu_overutilized(int cpu)
 static inline void update_overutilized_status(struct rq *rq)
 {
 	if (!READ_ONCE(rq->rd->overutilized) && cpu_overutilized(rq->cpu)) {
-#ifdef CONFIG_MTK_SCHED_LB_ENHANCEMENT
-		if (!sched_feat(SCHED_MTK_EAS) || (sched_feat(SCHED_MTK_EAS)
-			&& capacity_orig_of(cpu_of(rq)) <
-				rq->rd->max_cpu_capacity.val)) {
-			WRITE_ONCE(rq->rd->overutilized, SG_OVERUTILIZED);
-			trace_sched_overutilized(1);
-		}
-#else
 		WRITE_ONCE(rq->rd->overutilized, SG_OVERUTILIZED);
 		trace_sched_overutilized(1);
-#endif
 	}
 }
-
-#ifdef CONFIG_MTK_SCHED_LB_ENHANCEMENT
-static
-void update_system_overutilized(struct sched_domain *sd, struct cpumask *cpus)
-{
-	unsigned long group_util;
-	bool intra_overutil = false;
-	unsigned long max_capacity;
-	struct sched_group *group = sd->groups;
-	struct root_domain *rd;
-	int this_cpu;
-	bool overutilized;
-	int i;
-
-	if (!sched_feat(SCHED_MTK_EAS))
-		return;
-
-	this_cpu = smp_processor_id();
-	rd = cpu_rq(this_cpu)->rd;
-	overutilized = READ_ONCE(rd->overutilized);
-	max_capacity = rd->max_cpu_capacity.val;
-
-	do {
-
-		group_util = 0;
-
-		for_each_cpu_and(i, sched_group_span(group), cpus) {
-
-			group_util += cpu_util(i);
-			if (cpu_overutilized(i)) {
-				if (capacity_orig_of(i) < max_capacity) {
-					intra_overutil = true;
-					break;
-				}
-			}
-		}
-
-		/*
-		 * A capacity base hint for over-utilization.
-		 * Not to trigger system overutiled if heavy tasks
-		 * in Big.cluster, so
-		 * add the free room(20%) of Big.cluster is impacted which means
-		 * system-wide over-utilization,
-		 * that considers whole cluster not single cpu
-		 */
-		if (group->group_weight > 1 && (group->sgc->capacity * 1024 <
-						group_util * 1280))  {
-			intra_overutil = true;
-			break;
-		}
-
-		group = group->next;
-
-	} while (group != sd->groups && !intra_overutil);
-
-	if (overutilized != intra_overutil) {
-		if (intra_overutil == true) {
-			WRITE_ONCE(rd->overutilized, SG_OVERUTILIZED);
-			trace_sched_overutilized(1);
-		} else {
-			WRITE_ONCE(rd->overutilized, 0);
-			trace_sched_overutilized(1);
-		}
-	}
-}
-#endif
 
 #else
 static inline void update_overutilized_status(struct rq *rq) { }
@@ -9311,29 +9236,6 @@ next_group:
 
 	env->src_grp_nr_running = sds->busiest_stat.sum_nr_running;
 
-#ifdef CONFIG_MTK_SCHED_LB_ENHANCEMENT
-	if (!env->sd->parent) {
-		struct root_domain *rd = env->dst_rq->rd;
-
-		/* update overload indicator if we are at root domain */
-		WRITE_ONCE(rd->overload, sg_status & SG_OVERLOAD);
-
-		/* Update over-utilization (tipping point, U >= 0) indicator */
-		if (!sched_feat(SCHED_MTK_EAS)) {
-			int overutil = sg_status & SG_OVERUTILIZED;
-
-			WRITE_ONCE(rd->overutilized, overutil);
-			trace_sched_overutilized(!!(overutil));
-		}
-	} else if (sg_status & SG_OVERUTILIZED) {
-		if (!sched_feat(SCHED_MTK_EAS)) {
-			struct root_domain *rd = env->dst_rq->rd;
-
-			WRITE_ONCE(rd->overutilized, SG_OVERUTILIZED);
-			trace_sched_overutilized(1);
-		}
-	}
-#else
 	if (!env->sd->parent) {
 		struct root_domain *rd = env->dst_rq->rd;
 
@@ -9347,8 +9249,6 @@ next_group:
 		WRITE_ONCE(env->dst_rq->rd->overutilized, SG_OVERUTILIZED);
 		trace_sched_overutilized(1);
 	}
-#endif
-
 }
 
 /**
@@ -9599,10 +9499,6 @@ static struct sched_group *find_busiest_group(struct lb_env *env)
 	 * this level.
 	 */
 	update_sd_lb_stats(env, &sds);
-
-#ifdef CONFIG_MTK_SCHED_LB_ENHANCEMENT
-	update_system_overutilized(env->sd, env->cpus);
-#endif
 
 	if (static_branch_unlikely(&sched_energy_present)) {
 		struct root_domain *rd = env->dst_rq->rd;
