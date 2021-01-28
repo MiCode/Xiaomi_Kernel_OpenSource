4227e631ce4a13f53035b992c9b9969cd3dc5581
Guangming Cao <Guangming.Cao@mediatek.com>
2021-01-29 02:24:38 +0800

    [ALPS05455919] m4u: fix kmemleak issue
    There is no ignore, use kmemleak_ignore to bypass check.
    
    MTK-Commit-Id: bf122cabfcbf03f30b5b09913ec669b39975185f
    
    Change-Id: Ibf73a23a362dd46f49e8c02106f66d1a0b5122cc
    Signed-off-by: Guangming Cao <Guangming.Cao@mediatek.com>
    CR-Id: ALPS05455919
    Feature: IOMMU

--- drivers/misc/mediatek/m4u/3.2/m4u_pgtable.c
+++ drivers/misc/mediatek/m4u/3.2/m4u_pgtable.c
@@ -549,8 +549,10 @@ int m4u_alloc_pte(struct m4u_domain_t *domain,
 	for (retry_cnt = 0; retry_cnt < 5; retry_cnt++) {
 		pte_new_va = kmem_cache_zalloc(gM4u_pte_kmem,
 					       GFP_KERNEL | GFP_DMA);
-		if (likely(pte_new_va))
+		if (likely(pte_new_va)) {
+			kmemleak_ignore(pte_new_va); //ignored by kmemleak tool
 			break;
+		}
 	}
 	write_lock_domain(domain);
 	if (unlikely(!pte_new_va)) {
