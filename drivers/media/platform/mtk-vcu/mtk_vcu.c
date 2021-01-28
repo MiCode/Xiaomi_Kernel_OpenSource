3d7e1b92172baedc429512e635ce308795284944
Chia-Mao Hung <chia-mao.hung@mediatek.com>
2021-01-28 21:30:30 +0800

    [ALPS04985917] v4l2: fix mt6779 use libvcodec_v3
    1. change use libvcodec_v3 user will lock/unlock HW
    2. libvcodec_v3 applied I61c3afce3e695f439dc0237130aae3c3ab269592
       reduce shared memory size to 0x80000
    
    MTK-Commit-Id: b1659eac7ccf86eab144e0f99b30f3d7514da0ce
    
    Change-Id: I8b926909c806dc0bc1506865a54066cd444f080a
    Signed-off-by: Chia-Mao Hung <chia-mao.hung@mediatek.com>
    CR-Id: ALPS04985917
    Feature: H.264 Decoder

--- drivers/media/platform/mtk-vcu/mtk_vcu.c
+++ drivers/media/platform/mtk-vcu/mtk_vcu.c
@@ -91,7 +91,7 @@
 /* vcu extended iova address*/
 #define VCU_PMEM0_IOVA(vcu_data)        (vcu_data->extmem.p_iova)
 #define VCU_DMEM0_IOVA(vcu_data)        (vcu_data->extmem.d_iova)
-#define VCU_SHMEM_SIZE 0x240000
+#define VCU_SHMEM_SIZE 0x80000
 
 #define MAP_SHMEM_ALLOC_BASE    0x80000000UL
 #define MAP_SHMEM_ALLOC_RANGE   VCU_SHMEM_SIZE
