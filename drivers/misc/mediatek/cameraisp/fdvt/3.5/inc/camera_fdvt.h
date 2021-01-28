ccaeaa7bc6cee4f4f418cbbb544c817c85fac8fe
Ryan Teng <ryan.teng@mediatek.com>
2021-01-28 16:43:01 +0800

    [ALPS04985744] FDVT: R Migration
    [Detail] FDVT R Migration
    
    MTK-Commit-Id: 4b2e54f5c0f274d7e1611b76003ddb8b25a76e81
    
    Change-Id: I5e05e7f3893a42b7249e8cb207e23b8c67d08269
    Signed-off-by: Ryan Teng <ryan.teng@mediatek.com>
    CR-Id: ALPS04985744
    Feature: [Android Default] Camera Application Basic Functions

---
+++ drivers/misc/mediatek/cameraisp/fdvt/3.5/inc/camera_fdvt.h
@@ -0,0 +1,101 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2015 MediaTek Inc.
+ */
+
+#ifndef __CAMERA_FDVT_H__
+#define __CAMERA_FDVT_H__
+
+#include <linux/ioctl.h>
+#define FDVT_IOC_MAGIC    'N'
+
+#define SIG_ERESTARTSYS 512
+
+#ifdef CONFIG_COMPAT
+/*64 bit*/
+#include <linux/fs.h>
+#include <linux/compat.h>
+#endif
+
+struct FDVTRegIO {
+	unsigned int  *pAddr;
+	unsigned int  *pData;
+	unsigned int  u4Count;
+};
+#define FDVTRegIO struct FDVTRegIO
+
+#ifdef CONFIG_COMPAT
+
+struct compat_FDVTRegIO {
+	compat_uptr_t pAddr;
+	compat_uptr_t pData;
+	unsigned int  u4Count;
+};
+#define compat_FDVTRegIO struct compat_FDVTRegIO
+
+#endif
+
+/*below is control message*/
+#define FDVT_IOC_INIT_SETPARA_CMD \
+	_IO(FDVT_IOC_MAGIC, 0x00)
+#define FDVT_IOC_STARTFD_CMD \
+	_IO(FDVT_IOC_MAGIC, 0x01)
+#define FDVT_IOC_G_WAITIRQ \
+	_IOR(FDVT_IOC_MAGIC, 0x02, unsigned int)
+#define FDVT_IOC_T_SET_FDCONF_CMD \
+	_IOW(FDVT_IOC_MAGIC, 0x03, FDVTRegIO)
+#define FDVT_IOC_G_READ_FDREG_CMD \
+	_IOWR(FDVT_IOC_MAGIC, 0x04, FDVTRegIO)
+#define FDVT_IOC_T_SET_SDCONF_CMD \
+	_IOW(FDVT_IOC_MAGIC, 0x05, FDVTRegIO)
+
+#define FDVT_IOC_T_DUMPREG \
+	_IO(FDVT_IOC_MAGIC, 0x80)
+
+#ifdef CONFIG_COMPAT
+
+#define COMPAT_FDVT_IOC_INIT_SETPARA_CMD \
+	_IO(FDVT_IOC_MAGIC, 0x00)
+#define COMPAT_FDVT_IOC_STARTFD_CMD \
+	_IO(FDVT_IOC_MAGIC, 0x01)
+#define COMPAT_FDVT_IOC_G_WAITIRQ \
+	_IOR(FDVT_IOC_MAGIC, 0x02, unsigned int)
+#define COMPAT_FDVT_IOC_T_SET_FDCONF_CMD \
+	_IOW(FDVT_IOC_MAGIC, 0x03, compat_FDVTRegIO)
+#define COMPAT_FDVT_IOC_G_READ_FDREG_CMD \
+	_IOWR(FDVT_IOC_MAGIC, 0x04, compat_FDVTRegIO)
+#define COMPAT_FDVT_IOC_T_SET_SDCONF_CMD \
+	_IOW(FDVT_IOC_MAGIC, 0x05, compat_FDVTRegIO)
+#define COMPAT_FDVT_IOC_T_DUMPREG \
+	_IO(FDVT_IOC_MAGIC, 0x80)
+
+#endif
+
+
+#endif/*__CAMERA_FDVT_H__*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
