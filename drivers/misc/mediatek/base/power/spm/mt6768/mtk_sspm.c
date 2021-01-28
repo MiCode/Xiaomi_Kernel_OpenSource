b47edfbda6eb9646ee2a5bc4eab91f9e3e90dac0
albert-zl.huang <albert-zl.huang@mediatek.com>
2021-01-29 00:39:13 +0800

    [ALPS04175988] spm: fix codex scan
    fix codex scan
    
    MTK-Commit-Id: a81ae8b47feb595e29e6bb828d1bb0338df1cb22
    
    Change-Id: Ia51fec2b865b904780c550f02ac83525e32d39af
    Signed-off-by: albert-zl.huang <albert-zl.huang@mediatek.com>
    CR-Id: ALPS04175988
    Feature: Power Management
    (cherry picked from commit 66ec593e29dfd747d23b56951467377961bc92f2)
    (cherry picked from commit 5dddf081e2d660edf88d1cb25722ff9f54343134)

--- drivers/misc/mediatek/base/power/spm/mt6768/mtk_sspm.c
+++ drivers/misc/mediatek/base/power/spm/mt6768/mtk_sspm.c
@@ -53,7 +53,7 @@ int spm_to_sspm_command_async(u32 cmd, struct spm_data *spm_d)
 
 int spm_to_sspm_command_async_wait(u32 cmd)
 {
-	int ack_data;
+	int ack_data = 0;
 	unsigned int ret = 0;
 
 	switch (cmd) {
@@ -85,7 +85,7 @@ int spm_to_sspm_command_async_wait(u32 cmd)
 
 int spm_to_sspm_command(u32 cmd, struct spm_data *spm_d)
 {
-	int ack_data;
+	int ack_data = 0;
 	unsigned int ret = 0;
 	/* struct spm_data _spm_d; */
 
