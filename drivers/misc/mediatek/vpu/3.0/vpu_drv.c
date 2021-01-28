ec2c6abe8a25a3ae81a28d540aad1d78ae673f5d
Shawn Huang <Shawn.Huang@mediatek.com>
2021-01-29 02:37:15 +0800

    [ALPS05349201] vpu 3.0: Fix vulnerability of OOB read/OOB write
    [Detail] Add sanity checks for req->buffers[i].plane_count.
    
    MTK-Commit-Id: a92c160e4043f107657100bf2eb5ea8cc2f7d409
    
    Change-Id: I1a7f2ec62394280cb3753b32ee3607748dd055fe
    Signed-off-by: Shawn Huang <Shawn.Huang@mediatek.com>
    CR-Id: ALPS05349201
    Feature: Vision Processing Unit

--- drivers/misc/mediatek/vpu/3.0/vpu_drv.c
+++ drivers/misc/mediatek/vpu/3.0/vpu_drv.c
@@ -1006,6 +1006,7 @@ static long vpu_ioctl(struct file *flip, unsigned int cmd, unsigned long arg)
 		struct vpu_request *req;
 		struct vpu_request *u_req;
 		unsigned int req_core;
+		int i, plane_count;
 
 		u_req = (struct vpu_request *) arg;
 
@@ -1110,13 +1111,30 @@ static long vpu_ioctl(struct file *flip, unsigned int cmd, unsigned long arg)
 		else if (req->buffer_count > VPU_MAX_NUM_PORTS) {
 			LOG_ERR("[ENQUE] %s, count=%d\n",
 				"wrong buffer count", req->buffer_count);
+			ret = -EINVAL;
+			goto out;
 		} else if (copy_from_user(req->buffers, u_req->buffers,
 			    req->buffer_count * sizeof(struct vpu_buffer))) {
 			LOG_ERR("[ENQUE] %s, ret=%d\n",
 				"copy 'struct buffer' failed", ret);
-		} else if (copy_from_user(req->buf_ion_infos,
+		}
+
+		/* Check if user plane_count is valid */
+		for (i = 0 ; i < req->buffer_count; i++) {
+			plane_count = req->buffers[i].plane_count;
+			if ((plane_count > VPU_MAX_NUM_PLANE) ||
+				(plane_count == 0)) {
+				ret = -EINVAL;
+		LOG_ERR("[ENQUE] Buffer#%d plane_count:%d is invalid!\n",
+					i, plane_count);
+				goto out;
+			}
+		}
+
+		if (copy_from_user(req->buf_ion_infos,
 				u_req->buf_ion_infos,
-				req->buffer_count * 3 * sizeof(uint64_t))) {
+				req->buffer_count * VPU_MAX_NUM_PLANE
+				* sizeof(uint64_t))) {
 			LOG_ERR("[ENQUE] %s, ret=%d\n",
 				"copy 'buf_share_fds' failed", ret);
 		} else if (vpu_put_request_to_pool(user, req)) {
