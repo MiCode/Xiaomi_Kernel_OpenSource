01697763a9fadcb5d388863e5b1fa425e0c35d61
chihhao.chen <chihhao.chen@mediatek.com>
2021-01-29 02:39:20 +0800

    [ALPS05435312] USB: error handling for MTP
    [Detail]
    Add device and state check for MTP driver
    
    MTK-Commit-Id: b7c9c2d0247111f00affcd7f675f124fd28a6b10
    
    Change-Id: I59e4467639d4461a93ad880f1329bc5440925f18
    Signed-off-by: chihhao.chen <chihhao.chen@mediatek.com>
    Signed-off-by: Macpaul Lin <macpaul.lin@mediatek.com>
    CR-Id: ALPS05435312
    Feature: [Module]USB 2.0
    (cherry picked from commit 6609fe4ca576ea628c22a60113d0e3858db422fd)
    (cherry-pick from 2367654a37c185f00da4939acf2d3ba2c6740bf5)

--- drivers/usb/gadget/function/f_mtp.c
+++ drivers/usb/gadget/function/f_mtp.c
@@ -807,11 +807,21 @@ static ssize_t mtp_read(struct file *fp, char __user *buf,
 		goto done;
 	}
 
+	spin_lock_irq(&dev->lock);
+
 	/* update cdev after online */
 	cdev = dev->cdev;
 
-	spin_lock_irq(&dev->lock);
+	if (dev->state == STATE_OFFLINE) {
+		spin_unlock_irq(&dev->lock);
+		return -ENODEV;
+	}
 	if (dev->ep_out->desc) {
+		if (!cdev) {
+			spin_unlock_irq(&dev->lock);
+			return -ENODEV;
+		}
+
 		len = usb_ep_align_maybe(cdev->gadget, dev->ep_out, count);
 		if (len > MTP_BULK_BUFFER_SIZE) {
 			spin_unlock_irq(&dev->lock);
@@ -2033,7 +2043,11 @@ mtp_function_unbind(struct usb_configuration *c, struct usb_function *f)
 		mtp_request_free(dev->rx_req[i], dev->ep_out);
 	while ((req = mtp_req_get(dev, &dev->intr_idle)))
 		mtp_request_free(req, dev->ep_intr);
+
+	spin_lock_irq(&dev->lock);
 	dev->state = STATE_OFFLINE;
+	dev->cdev = NULL;
+	spin_unlock_irq(&dev->lock);
 	kfree(f->os_desc_table);
 	f->os_desc_n = 0;
 }
