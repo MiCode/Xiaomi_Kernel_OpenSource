1b9f8b52a1ab84c7f1fc98b2d2d71ac167736064
Juju Sung <juju.sung@mediatek.com>
2021-01-28 18:24:32 +0800

    [ALPS05014853] Lowpower: mt6761 cpu_dvfs porting
    [Detail]
    add mt6765/61 platform dvfs driver
    
    MTK-Commit-Id: 5f6e80522c9ff1a9e5a05df4f0aae42c9a9787eb
    
    Change-Id: I93922594892b770209c4f5b7ab8903f856f3d39c
    Signed-off-by: Juju Sung <juju.sung@mediatek.com>
    CR-Id: ALPS05014853
    Feature: [Module]Kernel Maintenance

---
+++ drivers/misc/mediatek/base/power/cpufreq_v1/src/mtk_cpufreq_hybrid.h
@@ -0,0 +1,87 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2016 MediaTek Inc.
+ */
+
+#ifndef __MTK_CPUFREQ_HYBRID__
+#define __MTK_CPUFREQ_HYBRID__
+
+/* #define CPUDVFS_TIME_PROFILE 1 */
+
+struct cpu_dvfs_log {
+	unsigned int time_stamp_l_log:32;
+	unsigned int time_stamp_h_log:32;
+
+	struct {
+		unsigned int turbo_bit:2;
+		unsigned int cluster_en:2;
+		unsigned int limit:4;
+		unsigned int base:4;
+		unsigned int opp_idx_log:4;
+		unsigned int sche_idx_log:5;
+		unsigned int wfi_idx_log:4;
+		unsigned int padd:7;
+	} cluster_opp_cfg[NR_MT_CPU_DVFS];
+};
+
+struct cpu_dvfs_log_box {
+	unsigned long long time_stamp;
+	struct {
+		unsigned int freq_idx;
+		unsigned int limit_idx;
+		unsigned int base_idx;
+	} cluster_opp_cfg[NR_MT_CPU_DVFS];
+};
+
+/* Parameter Enum */
+enum cpu_dvfs_ipi_type {
+	IPI_DVFS_INIT_PTBL,
+	IPI_DVFS_INIT,
+	IPI_SET_CLUSTER_ON_OFF,
+	IPI_TURBO_MODE,
+	IPI_TIME_PROFILE,
+
+	NR_DVFS_IPI,
+};
+
+struct cdvfs_data {
+	unsigned int cmd;
+	union {
+		struct {
+			unsigned int arg[3];
+		} set_fv;
+	} u;
+};
+
+int cpuhvfs_module_init(void);
+int cpuhvfs_set_init_sta(void);
+int cpuhvfs_set_turbo_scale(unsigned int turbo_f, unsigned int turbo_v);
+int cpuhvfs_set_min_max(int cluster_id, int base, int limit);
+int cpuhvfs_set_cluster_on_off(int cluster_id, int state);
+int cpuhvfs_set_dvfs(int cluster_id, unsigned int freq);
+int cpuhvfs_set_volt(int cluster_id, unsigned int volt);
+int cpuhvfs_set_freq(int cluster_id, unsigned int freq);
+int cpuhvfs_get_cur_volt(int cluster_id);
+int cpuhvfs_get_volt(int buck_id);
+int cpuhvfs_get_freq(int pll_id);
+int cpuhvfs_set_turbo_mode(int turbo_mode, int freq_step, int volt_step);
+int cpuhvfs_get_time_profile(void);
+int cpuhvfs_set_dvfs_stress(unsigned int en);
+int cpuhvfs_get_sched_dvfs_disable(void);
+int cpuhvfs_set_sched_dvfs_disable(unsigned int disable);
+int cpuhvfs_set_turbo_disable(unsigned int disable);
+int cpuhvfs_get_cur_dvfs_freq_idx(int cluster_id);
+int cpuhvfs_set_cluster_load_freq(enum mt_cpu_dvfs_id id, unsigned int freq);
+int cpuhvfs_set_set_cci_volt(unsigned int volt);
+unsigned int cpuhvfs_get_cci_result(unsigned int idx_1, unsigned int idx_2,
+	unsigned int mode);
+void cpuhvfs_update_cci_map_tbl(unsigned int idx_1, unsigned int idx_2,
+	unsigned char result, unsigned int mode, unsigned int use_id);
+unsigned int cpuhvfs_get_cci_mode(void);
+void cpuhvfs_update_cci_mode(unsigned int mode, unsigned int use_id);
+int cpuhvfs_set_iccs_freq(enum mt_cpu_dvfs_id id, unsigned int freq);
+int cpuhvfs_update_volt(unsigned int cluster_id, unsigned int *volt_tbl,
+	char nr_volt_tbl);
+int dvfs_to_sspm_command(u32 cmd, struct cdvfs_data *cdvfs_d);
+
+#endif	/* __MTK_CPUFREQ_HYBRID__ */
---
+++ drivers/misc/mediatek/base/power/include/mtk_dvfsrc_reg.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2016 MediaTek Inc.
+ */
+
+#ifndef _COMMON_MTK_DVFSRC_REG_H
+#define _COMMON_MTK_DVFSRC_REG_H
+
+#if defined(CONFIG_MACH_MT6763)
+
+#include "spm_v4/mtk_dvfsrc_reg.h"
+
+#endif
+
+#endif /* _COMMON_MTK_DVFSRC_REG_H */
+
