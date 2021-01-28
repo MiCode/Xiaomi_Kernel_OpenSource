97c57032800e99ba74fc42201c1d7277e24467e2
Eric Chung <eric.chung@mediatek.com>
2021-01-28 22:43:25 +0800

    [ALPS04846031] GPU: Apply limited retry for thread release
    [Detail]
    GPU driver is waiting for user/kernel thread to be released,
    but it prints to much log and sometimes wait-forever.
    
    [Solution]
    Retry limited times then force to shutdown.
    
    MTK-Commit-Id: f3a71dea2b5f0c66984a99e8d3c84e589f682241
    
    Change-Id: I89811d2e359dfa43350ebb9046be883fe3b181f5
    Signed-off-by: Eric Chung <eric.chung@mediatek.com>
    CR-Id: ALPS04846031
    Feature: OpenGL|ES

--- drivers/gpu/mediatek/gpu_rgx/m1.13ED5696267/services/server/env/linux/pvr_bridge_k.c
+++ drivers/gpu/mediatek/gpu_rgx/m1.13ED5696267/services/server/env/linux/pvr_bridge_k.c
@@ -301,10 +301,12 @@ PVRSRV_ERROR OSPlatformBridgeDeInit(void)
 	return eError;
 }
 
+#define MTK_RETRY 5
 PVRSRV_ERROR LinuxBridgeBlockClientsAccess(IMG_BOOL bShutdown)
 {
 	PVRSRV_ERROR eError;
 	IMG_HANDLE hEvent;
+	int retry = MTK_RETRY;
 
 	eError = OSEventObjectOpen(g_hDriverThreadEventObject, &hEvent);
 	if (eError != PVRSRV_OK)
@@ -341,6 +343,15 @@ PVRSRV_ERROR LinuxBridgeBlockClientsAccess(IMG_BOOL bShutdown)
 		 * process). Because of that this thread shouldn't and most likely
 		 * event cannot be frozen. */
 		OSEventObjectWait(hEvent);
+
+		if (bShutdown) {
+			retry--;
+			if (retry == 0) {
+				PVR_LOG(("%s: retried %u times, force abort",
+						__func__, MTK_RETRY));
+				break;
+			}
+		}
 	}
 
 out_put:
