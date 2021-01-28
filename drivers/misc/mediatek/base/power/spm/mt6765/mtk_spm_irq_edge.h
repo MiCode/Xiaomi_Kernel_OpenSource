bfa1659742b763eef1bdd2b375744e097a1d00a6
albert-zl.huang <albert-zl.huang@mediatek.com>
2021-01-29 02:06:47 +0800

    [ALPS05051543] spm: mt6765: correct edge irq list
    Correct edge irq list
    
    MTK-Commit-Id: e6653a1691af9efa02bca4b2e34cbf91051163c5
    
    Change-Id: I8018b67c61d4d0fb118d7e93d3ce909d5b0878aa
    Signed-off-by: albert-zl.huang <albert-zl.huang@mediatek.com>
    CR-Id: ALPS05051543
    Feature: Power Management

--- drivers/misc/mediatek/base/power/spm/mt6765/mtk_spm_irq_edge.h
+++ drivers/misc/mediatek/base/power/spm/mt6765/mtk_spm_irq_edge.h
@@ -18,9 +18,9 @@ struct edge_trigger_irq_list {
 };
 
 static struct edge_trigger_irq_list list[] = {
-	{ "mediatek,infracfg_ao",   0,	0 },
+	{ "mediatek,mt6765-infracfg",   0,	0 },
 	{ "mediatek,kp",            0,  WAKE_SRC_R12_KP_IRQ_B },
-	{ "mediatek,mdcldma",       3,  WAKE_SRC_R12_MD1_WDT_B },
+	{ "mediatek,mddriver",       3,  WAKE_SRC_R12_MD1_WDT_B },
 };
 
 #endif /* __MTK_SPM_IRQ_EDGE_H__ */
