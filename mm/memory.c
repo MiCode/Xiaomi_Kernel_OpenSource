db669d4a78ceb873de3af4558eddb8292c117345
Chinwen Chang <chinwen.chang@mediatek.com>
2021-01-28 17:17:41 +0800

    [ALPS04983389] FROMLIST: mm: protect SPF handler against anon_vma changes
    (from https://lore.kernel.org/patchwork/patch/906230/)
    
    The speculative page fault handler must be protected against anon_vma
    changes. This is because page_add_new_anon_rmap() is called during the
    speculative path.
    
    In addition, dont try speculative page fault if the VMA dont have an
    anon_vma structure allocated because its allocation should be
    protected by the mmap_sem.
    
    In __vma_adjust() when importer->anon_vma is set, there is no need to
    protect against speculative page faults since speculative page fault
    is aborted if the vma->anon_vma is not set.
    
    When calling page_add_new_anon_rmap() vma->anon_vma is necessarily
    valid since we checked for it when locking the pte and the anon_vma is
    removed once the pte is unlocked. So even if the speculative page
    fault handler is running concurrently with do_unmap(), as the pte is
    locked in unmap_region() - through unmap_vmas() - and the anon_vma
    unlinked later, because we check for the vma sequence counter which is
    updated in unmap_page_range() before locking the pte, and then in
    free_pgtables() so when locking the pte the change will be detected.
    
    MTK-Commit-Id: aacec5a8daaa9f035b6365e19a5c3668d3cfb6c8
    
    FROMLIST: mm: protect SPF handler against anon_vma changes
    Signed-off-by: Laurent Dufour <ldufour@linux.vnet.ibm.com>
    Change-Id: I583a90b5a8f4991c07fb0237fc9b1ee9237eefee
    Signed-off-by: Chinwen Chang <chinwen.chang@mediatek.com>
    CR-Id: ALPS04983389
    Feature: Memory Optimization

--- mm/memory.c
+++ mm/memory.c
@@ -623,7 +623,9 @@ void free_pgtables(struct mmu_gather *tlb, struct vm_area_struct *vma,
 		 * Hide vma from rmap and truncate_pagecache before freeing
 		 * pgtables
 		 */
+		vm_write_begin(vma);
 		unlink_anon_vmas(vma);
+		vm_write_end(vma);
 		unlink_file_vma(vma);
 
 		if (is_vm_hugetlb_page(vma)) {
@@ -637,7 +639,9 @@ void free_pgtables(struct mmu_gather *tlb, struct vm_area_struct *vma,
 			       && !is_vm_hugetlb_page(next)) {
 				vma = next;
 				next = vma->vm_next;
+				vm_write_begin(vma);
 				unlink_anon_vmas(vma);
+				vm_write_end(vma);
 				unlink_file_vma(vma);
 			}
 			free_pgd_range(tlb, addr, vma->vm_end,
