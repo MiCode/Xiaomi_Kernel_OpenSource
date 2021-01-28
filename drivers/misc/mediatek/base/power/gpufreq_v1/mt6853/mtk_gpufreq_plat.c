491f5a75394cda9f3b95eaece7f23097a97770e4
Figo Wang <figo.wang@mediatek.com>
2021-01-29 02:20:22 +0800

    [ALPS05296137] GPU: correct the segment setting
    The following configures are not supported:
    CONFIG_BUILD_ARM64_DTB_OVERLAY_IMAGE_NAMES
    CONFIG_BUILD_ARM64_DTB_OVERLAY_IMAGE
    
    Change the way to recognize the segments.
    
    MTK-Commit-Id: fd937e0d6b9af38d90d41693cec3668826474279
    
    Change-Id: Ifca348e36b19a4fa53c9ca4700a8193e3a1de066
    Signed-off-by: Figo Wang <figo.wang@mediatek.com>
    CR-Id: ALPS05296137
    Feature: OpenGL|ES

--- drivers/misc/mediatek/base/power/gpufreq_v1/mt6853/mtk_gpufreq_plat.c
+++ drivers/misc/mediatek/base/power/gpufreq_v1/mt6853/mtk_gpufreq_plat.c
@@ -2997,19 +2997,17 @@ static void __mt_gpufreq_init_table(void)
 		g_segment_max_opp_idx = 18;
 
 /* Special SW setting */
-#if defined(CONFIG_ARM64) && defined(CONFIG_BUILD_ARM64_DTB_OVERLAY_IMAGE_NAMES)
-	if (strstr(CONFIG_BUILD_ARM64_DTB_OVERLAY_IMAGE_NAMES,
-						"turbo") != NULL) {
-		gpufreq_pr_info("@%s: turbo flavor name: %s\n",
-			__func__, CONFIG_BUILD_ARM64_DTB_OVERLAY_IMAGE_NAMES);
-		g_segment_max_opp_idx = 0;
-	}
-	if (strstr(CONFIG_BUILD_ARM64_DTB_OVERLAY_IMAGE_NAMES,
-						"k6853tv1") != NULL) {
-		gpufreq_pr_info("@%s: k6853tv1 flavor name: %s\n",
-			__func__, CONFIG_BUILD_ARM64_DTB_OVERLAY_IMAGE_NAMES);
-		g_segment_max_opp_idx = 0;
-	}
+#if defined(CONFIG_ARM64)
+#if defined(K6853TV1)
+	gpufreq_pr_info("@%s: k6853tv1 flavor name: %s\n",
+		__func__, CONFIG_BUILD_ARM64_DTB_OVERLAY_IMAGE_NAMES);
+	g_segment_max_opp_idx = 0;
+#endif
+#if defined(TURBO)
+	gpufreq_pr_info("@%s: turbo flavor name: %s\n",
+		__func__, CONFIG_BUILD_ARM64_DTB_OVERLAY_IMAGE_NAMES);
+	g_segment_max_opp_idx = 0;
+#endif
 #endif
 
 	g_segment_min_opp_idx = NUM_OF_OPP_IDX - 1;
