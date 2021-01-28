63af748f4e94d8dce4fbe02328a377e269c3b531
Miles Chen <miles.chen@mediatek.com>
2021-01-28 17:16:26 +0800

    [ALPS04987899] usb: gaget: fix build unused function warnings
    When CONFIG_USB_CONFIGFS_UEVENT=y, we observed the following build
    warnings:
    
    drivers/usb/gadget/configfs.c:1488:12: warning: unused function
    configfs_composite_setup [-Wunused-function]
    static int configfs_composite_setup(struct usb_gadget *gadget,
    drivers/usb/gadget/configfs.c:1513:13: warning: unused function
    configfs_composite_disconnect [-Wunused-function]
    static void configfs_composite_disconnect(struct usb_gadget *gadget)
    
    Fix it by defining these functions only when CONFIG_USB_CONFIGFS_UEVENT=n
    
    MTK-Commit-Id: 6d52179da9e4df5fbf54f852c69ba0bd1e46dd17
    
    Change-Id: I986f2fcb1fe67cea9c9ee0f6c55cd6e6425cc752
    Feature: [Module]Kernel Maintenance
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    CR-Id: ALPS04987899

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -1458,31 +1458,6 @@ static void android_work(struct work_struct *data)
 			gi->connected, gi->sw_connected, cdev->config);
 	}
 }
-
-static void configfs_composite_unbind(struct usb_gadget *gadget)
-{
-	struct usb_composite_dev	*cdev;
-	struct gadget_info		*gi;
-	unsigned long flags;
-
-	/* the gi->lock is hold by the caller */
-
-	cdev = get_gadget_data(gadget);
-	gi = container_of(cdev, struct gadget_info, cdev);
-	spin_lock_irqsave(&gi->spinlock, flags);
-	gi->unbind = 1;
-	spin_unlock_irqrestore(&gi->spinlock, flags);
-
-	kfree(otg_desc[0]);
-	otg_desc[0] = NULL;
-	purge_configs_funcs(gi);
-	composite_dev_cleanup(cdev);
-	usb_ep_autoconfig_reset(cdev->gadget);
-	spin_lock_irqsave(&gi->spinlock, flags);
-	cdev->gadget = NULL;
-	set_gadget_data(gadget, NULL);
-	spin_unlock_irqrestore(&gi->spinlock, flags);
-}
 #else
 static int configfs_composite_setup(struct usb_gadget *gadget,
 		const struct usb_ctrlrequest *ctrl)
@@ -1531,6 +1506,31 @@ static void configfs_composite_disconnect(struct usb_gadget *gadget)
 	spin_unlock_irqrestore(&gi->spinlock, flags);
 }
 #endif
+
+static void configfs_composite_unbind(struct usb_gadget *gadget)
+{
+	struct usb_composite_dev	*cdev;
+	struct gadget_info		*gi;
+	unsigned long flags;
+
+	/* the gi->lock is hold by the caller */
+
+	cdev = get_gadget_data(gadget);
+	gi = container_of(cdev, struct gadget_info, cdev);
+	spin_lock_irqsave(&gi->spinlock, flags);
+	gi->unbind = 1;
+	spin_unlock_irqrestore(&gi->spinlock, flags);
+
+	kfree(otg_desc[0]);
+	otg_desc[0] = NULL;
+	purge_configs_funcs(gi);
+	composite_dev_cleanup(cdev);
+	usb_ep_autoconfig_reset(cdev->gadget);
+	spin_lock_irqsave(&gi->spinlock, flags);
+	cdev->gadget = NULL;
+	set_gadget_data(gadget, NULL);
+	spin_unlock_irqrestore(&gi->spinlock, flags);
+}
 
 static void configfs_composite_suspend(struct usb_gadget *gadget)
 {
