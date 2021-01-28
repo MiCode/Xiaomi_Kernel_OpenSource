8ba17e7a8d9aec14690d0709df727fa8525743cb
Yun Hsiang <yun.hsiang@mediatek.com>
2021-01-29 01:02:49 +0800

    [ALPS05051187] topology: use a general methon in get_freq_capacity
    [Detail]
    The frquency limits come from users may not align with opp table.
    Find a minimum capacity that satisfy the frequency in get_freq_capacity.
    
    MTK-Commit-Id: 6f12408d9bfbabd6c2e202d4cca25e6bd697a99a
    
    Change-Id: I20359cc5d19734199d8f64845e18f138b9e09639
    Signed-off-by: Yun Hsiang <yun.hsiang@mediatek.com>
    CR-Id: ALPS05051187
    Feature: Energy Aware Scheduling Plus (EAS+)
    (cherry picked from commit eecbd6425eb7d94eedf1fd08fd4d87d2a63e9b72)

--- drivers/base/arch_topology_plus.c
+++ drivers/base/arch_topology_plus.c
@@ -10,20 +10,53 @@ static inline unsigned long get_freq_capacity(int cpu, unsigned long freq)
 {
 	int i;
 	int tmp;
-	int idx;
+	int idx = -1;
 	struct upower_tbl *tbl;
 
-	for (i = 0;; i++) {
+	tbl = upower_get_core_tbl(cpu);
+	for (i = 0; i < tbl->row_num ; i++) {
 		tmp = mt_cpufreq_get_cpu_freq(cpu, i);
-		if (unlikely(!tmp))
+		if (unlikely(!tmp)) {
 			WARN(1, "frequency not in opp table!\n");
-		if (tmp == freq) {
+			idx = tbl->row_num - 1;
+			break;
+		}
+		if (tmp >= freq) {
 			idx = i;
 			break;
 		}
 	}
 
+	if (idx < 0)
+		idx = tbl->row_num - 1;
+
+	return tbl->row[idx].cap;
+}
+
+static inline unsigned long get_freq_capacity_lt(int cpu, unsigned long freq)
+{
+	int i;
+	int tmp;
+	int idx = -1;
+	struct upower_tbl *tbl;
+
 	tbl = upower_get_core_tbl(cpu);
+	for (i = tbl->row_num - 1; i >= 0; i--) {
+		tmp = mt_cpufreq_get_cpu_freq(cpu, i);
+		if (unlikely(!tmp)) {
+			WARN(1, "frequency not in opp table!\n");
+			idx = 0;
+			break;
+		}
+		if (tmp <= freq) {
+			idx = i;
+			break;
+		}
+	}
+
+	if (idx < 0)
+		idx = 0;
+
 	return tbl->row[idx].cap;
 }
 
@@ -60,8 +93,8 @@ void arch_set_max_freq_scale(struct cpumask *cpus,
 	if (!max_freq)
 		return;
 
-	policy_max_cap = get_freq_capacity(cpu, policy_max_freq);
-	max_cap = get_freq_capacity(cpu, max_freq);
+	policy_max_cap = get_freq_capacity_lt(cpu, policy_max_freq);
+	max_cap = get_freq_capacity_lt(cpu, max_freq);
 
 	scale = (policy_max_cap << SCHED_CAPACITY_SHIFT) / max_cap;
 
