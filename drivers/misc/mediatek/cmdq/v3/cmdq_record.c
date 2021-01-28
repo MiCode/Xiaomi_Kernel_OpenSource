8a4d067c10933b2aad5366c81fe208fc8956f1ea
Bhupender tabiyad <bhupender.tabiyad@mediatek.com>
2021-01-29 02:23:30 +0800

    [ALPS05198684] cmdq: update secure check in api cmdq_op_read_reg_to_mem
    Update cmdq_op_read_reg_to_mem and restrict the usage to Normal world.
    Reading the register and writing to Dram is restricted in secure world.
    
    MTK-Commit-Id: 2cc37fb98ede4e715724a94892cf4a85460d5a57
    
    Change-Id: Ie9e4236ae35a9508964e40125f43b3b940824096
    CR-Id: ALPS05198684
    Feature: [Module]CMDQ Driver
    Signed-off-by: Bhupender Tabiyad <bhupender.tabiyad@mediatek.com>

--- drivers/misc/mediatek/cmdq/v3/cmdq_record.c
+++ drivers/misc/mediatek/cmdq/v3/cmdq_record.c
@@ -1815,6 +1815,13 @@ s32 cmdq_op_read_reg_to_mem(struct cmdqRecStruct *handle,
 	if (!handle)
 		return -EINVAL;
 
+#ifdef CMDQ_SECURE_PATH_SUPPORT
+	if (cmdq_task_is_secure(handle)) {
+		CMDQ_ERR("%s secure handle\n", __func__);
+		return -EINVAL;
+	}
+#endif
+
 	do {
 		status = cmdq_op_read_reg(handle, addr,
 			&handle->arg_value, ~0);
