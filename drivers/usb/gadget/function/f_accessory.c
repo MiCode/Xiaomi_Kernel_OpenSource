7574d57c769991ccc7189b47a4d78b3abdc51a46
Jack Pham <jackp@codeaurora.org>
2021-01-29 00:44:38 +0800

    [ALPS05083413] usb: gadget: f_accessory: Fix check for dev->disconnected
    The previous commit d378aeae512d ("usb: gadget: f_accessory: avoid
    requeuing write req if disconnected") added a check for
    dev->disconnected during write operation, but incorrectly
    inverted the value, thus it fails when connected. Fix the polarity
    of this check.
    
    MTK-Commit-Id: 5027f2044fd6ff2d54b56d4a9d93012cdb5cdaa4
    
    Change-Id: I447871d054e35a9d21eb9e55087facfbf7701dd5
    Signed-off-by: Jack Pham <jackp@codeaurora.org>
    Signed-off-by: Eddie Hung <eddie.hung@mediatek.com>
    CR-Id: ALPS05083413
    Feature: [Android Default] ADB (Android Debug Bridge)

--- drivers/usb/gadget/function/f_accessory.c
+++ drivers/usb/gadget/function/f_accessory.c
@@ -663,7 +663,7 @@ static ssize_t acc_write(struct file *fp, const char __user *buf,
 		req = 0;
 		ret = wait_event_interruptible(dev->write_wq,
 			((req = req_get(dev, &dev->tx_idle)) || !dev->online));
-		if (!dev->online || !dev->disconnected) {
+		if (!dev->online || dev->disconnected) {
 			pr_debug("acc_write dev->error\n");
 			r = -EIO;
 			break;
