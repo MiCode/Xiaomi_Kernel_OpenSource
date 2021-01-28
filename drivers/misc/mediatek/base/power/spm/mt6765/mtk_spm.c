d0b4c13972e7bf24844395c4d160ca5c531f081c
albert-zl.huang <albert-zl.huang@mediatek.com>
2021-01-29 02:06:21 +0800

    [ALPS05051543] suspend: avoid IPI cmd race condition for suspend/idle
    Avoid IPI cmd race condition for suspend/idle
    
    MTK-Commit-Id: 57161a103d1028947f141d94a5bcce1ab0facafd
    
    Change-Id: If6ef27c9cbf87f5f0672ab354971489ca737591b
    Signed-off-by: albert-zl.huang <albert-zl.huang@mediatek.com>
    CR-Id: ALPS05051543
    Feature: Power Management

--- drivers/misc/mediatek/base/power/spm/mt6765/mtk_spm.c
+++ drivers/misc/mediatek/base/power/spm/mt6765/mtk_spm.c
@@ -13,7 +13,6 @@
 #include <linux/of.h>
 #include <linux/of_irq.h>
 #include <linux/of_address.h>
-#include <linux/suspend.h>
 #include <linux/platform_device.h>
 
 #include <mtk_cpuidle.h>	/* mtk_cpuidle_init */
@@ -180,57 +179,6 @@ static struct platform_driver spm_dev_drv = {
 	},
 };
 
-#if !defined(CONFIG_FPGA_EARLY_PORTING)
-#ifdef CONFIG_PM
-static int spm_pm_event(struct notifier_block *notifier, unsigned long pm_event,
-			void *unused)
-{
-#ifdef CONFIG_MTK_TINYSYS_SSPM_SUPPORT
-	struct spm_data spm_d;
-	int ret;
-	unsigned long flags;
-#endif /* CONFIG_MTK_TINYSYS_SSPM_SUPPORT */
-
-	switch (pm_event) {
-	case PM_HIBERNATION_PREPARE:
-		return NOTIFY_DONE;
-	case PM_RESTORE_PREPARE:
-		return NOTIFY_DONE;
-	case PM_POST_HIBERNATION:
-		return NOTIFY_DONE;
-#ifdef CONFIG_MTK_TINYSYS_SSPM_SUPPORT
-	case PM_SUSPEND_PREPARE:
-		spin_lock_irqsave(&__spm_lock, flags);
-		ret = spm_to_sspm_command(SPM_SUSPEND_PREPARE, &spm_d);
-		spin_unlock_irqrestore(&__spm_lock, flags);
-		if (ret < 0) {
-			pr_err("#@# %s(%d) PM_SUSPEND_PREPARE return %d!!!\n",
-				__func__, __LINE__, ret);
-			return NOTIFY_BAD;
-		}
-		return NOTIFY_DONE;
-	case PM_POST_SUSPEND:
-		spin_lock_irqsave(&__spm_lock, flags);
-		ret = spm_to_sspm_command(SPM_POST_SUSPEND, &spm_d);
-		spin_unlock_irqrestore(&__spm_lock, flags);
-		if (ret < 0) {
-			pr_err("#@# %s(%d) PM_POST_SUSPEND return %d!!!\n",
-				__func__, __LINE__, ret);
-			return NOTIFY_BAD;
-		}
-		return NOTIFY_DONE;
-#endif /* CONFIG_MTK_TINYSYS_SSPM_SUPPORT */
-	}
-	return NOTIFY_OK;
-}
-
-static struct notifier_block spm_pm_notifier_func = {
-	.notifier_call = spm_pm_event,
-	.priority = 0,
-};
-#endif /* CONFIG_PM */
-#endif /* CONFIG_FPGA_EARLY_PORTING */
-
 static const struct mtk_idle_sysfs_op spm_last_wakeup_src_fops = {
 	.fs_read = get_spm_last_wakeup_src,
 };
@@ -299,16 +247,6 @@ static int spm_module_init(void)
 			, &spm_spmfw_version_fops, &pParent2ND, NULL);
 	}
 
-
-#if !defined(CONFIG_FPGA_EARLY_PORTING)
-#ifdef CONFIG_PM
-	ret = register_pm_notifier(&spm_pm_notifier_func);
-	if (ret) {
-		pr_debug("Failed to register PM notifier.\n");
-		return ret;
-	}
-#endif /* CONFIG_PM */
-#endif /* CONFIG_FPGA_EARLY_PORTING */
 	SMC_CALL(ARGS, SPM_ARGS_SPMFW_IDX, spm_get_spmfw_idx(), 0);
 
 	return 0;
