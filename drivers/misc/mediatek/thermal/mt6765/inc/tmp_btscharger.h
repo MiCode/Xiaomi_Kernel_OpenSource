173ef8f6d1af833585cb8076bd930a6fb86213e3
Yu-Chia Chang <ethan.chang@mediatek.com>
2021-01-28 20:47:28 +0800

    [ALPS05098578] Thermal: add mt6762 and mt6765 for R migration
    Add mt6762 and mt6765 for R migration
    
    MTK-Commit-Id: 42b4ca11bf04ec4a6b16c71d73ca9aaacc6cbe3d
    
    Change-Id: I6fb779dfdc9dfbbf4b98bd63fab9bb32025e755d
    Signed-off-by: Yu-Chia Chang <ethan.chang@mediatek.com>
    CR-Id: ALPS05098578
    Feature: Thermal Management

---
+++ drivers/misc/mediatek/thermal/mt6765/inc/tmp_btscharger.h
@@ -0,0 +1,30 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2019 MediaTek Inc.
+ */
+
+#ifndef __TMP_BTS_CHARGER_H__
+#define __TMP_BTS_CHARGER_H__
+
+/* chip dependent */
+
+#define APPLY_PRECISE_NTC_TABLE
+#define APPLY_AUXADC_CALI_DATA
+
+#define AUX_IN2_NTC (2)
+/* 390K, pull up resister */
+#define BTSCHARGER_RAP_PULL_UP_R		390000
+/* base on 100K NTC temp
+ * default value -40 deg
+ */
+#define BTSCHARGER_TAP_OVER_CRITICAL_LOW	4397119
+/* 1.8V ,pull up voltage */
+#define BTSCHARGER_RAP_PULL_UP_VOLTAGE	1800
+/* default is NCP15WF104F03RC(100K) */
+#define BTSCHARGER_RAP_NTC_TABLE		7
+
+#define BTSCHARGER_RAP_ADC_CHANNEL		AUX_IN2_NTC /* default is 2 */
+extern int IMM_GetOneChannelValue(int dwChannel, int data[4], int *rawdata);
+extern int IMM_IsAdcInitReady(void);
+
+#endif	/* __TMP_BTS_CHARGER_H__ */
