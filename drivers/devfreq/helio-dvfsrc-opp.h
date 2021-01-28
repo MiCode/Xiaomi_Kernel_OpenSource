d1c509b5d7c8d90ef7745ab91711a4685c2427a0
Arvin Wang <arvin.wang@mediatek.com>
2021-01-29 00:43:37 +0800

    [ALPS04660293] VCORE_DVFS: porting mt6785 driver
    Porting mt6785 VCORE_DVFS dvfsrc driver
    
    MTK-Commit-Id: 9827d169c1f49aea30bf1835db6e56094b51e074
    
    Change-Id: I046abee4dc2ea29cf956cc06561e28ae03b47a21
    Signed-off-by: Arvin Wang <arvin.wang@mediatek.com>
    CR-Id: ALPS04660293
    Feature: VCORE DVFS

--- drivers/devfreq/helio-dvfsrc-opp.h
+++ drivers/devfreq/helio-dvfsrc-opp.h
@@ -20,7 +20,13 @@
 #include <helio-dvfsrc-opp-mt6771.h>
 #elif defined(CONFIG_MACH_MT6768)
 #include <helio-dvfsrc-opp-mt6768.h>
+#elif defined(CONFIG_MACH_MT6785)
+#include <helio-dvfsrc-opp-mt6785.h>
+#else
+#include <helio-dvfsrc-opp-mt67xx.h>
+#endif
 
+#if defined(CONFIG_MACH_MT6768) || defined(CONFIG_MACH_MT6785)
 struct opp_profile {
 	int vcore_uv;
 	int ddr_khz;
@@ -48,9 +54,6 @@ extern void set_pwrap_cmd(int vcore_opp, int pwrap_cmd);
 extern int get_pwrap_cmd(int vcore_opp);
 extern int get_opp_ddr_freq(int ddr_opp);
 extern void set_opp_ddr_freq(int ddr_opp, int ddr_freq);
-
-#else
-#include <helio-dvfsrc-opp-mt67xx.h>
 #endif
 
 #endif /* __HELIO_DVFSRC_OPP_H */
