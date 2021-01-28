311c2bf7b69043589f32b6530ee2bc5e4fb071db
Jiaqin Wang <jiaqin.wang@mediatek.com>
2021-01-29 02:20:45 +0800

    [ALPS05125032] cert: fix 9160954
    fix cert-c 9160954
    
    MTK-Commit-Id: 49c22f44e2ae27218ab9c41760d225036baaec7d
    
    Change-Id: Ia0b62aa7fd7ad6aa806862dc1724a0c895f6b15f
    Signed-off-by: Jiaqin Wang <jiaqin.wang@mediatek.com>
    CR-Id: ALPS05125032
    Feature: [Module]USB 2.0

--- drivers/usb/gadget/function/f_mtp.c
+++ drivers/usb/gadget/function/f_mtp.c
@@ -1815,13 +1815,17 @@ static void do_monitor_work(struct work_struct *work)
 	char string_container[128];
 
 	r = sprintf(string_container, "IN <");
-	for (i = 0; i < MTP_MAX_MONITOR_TYPE; i++)
-		r += sprintf(string_container + r, "%d ", monitor_in_cnt[i]);
+	if (r >= 0 && r < ARRAY_SIZE(string_container))
+		for (i = 0; i < MTP_MAX_MONITOR_TYPE; i++)
+			r += sprintf(string_container + r, "%d ",
+				monitor_in_cnt[i]);
 	MTP_DBG("%s>\n", string_container);
 
 	r = sprintf(string_container, "OUT <");
-	for (i = 0; i < MTP_MAX_MONITOR_TYPE; i++)
-		r += sprintf(string_container + r, "%d ", monitor_out_cnt[i]);
+	if (r >= 0 && r < ARRAY_SIZE(string_container))
+		for (i = 0; i < MTP_MAX_MONITOR_TYPE; i++)
+			r += sprintf(string_container + r, "%d ",
+				monitor_out_cnt[i]);
 	MTP_DBG("%s>\n", string_container);
 
 	if (likely(!monitor_time))
@@ -1829,8 +1833,10 @@ static void do_monitor_work(struct work_struct *work)
 
 	/* TIME PROFILING */
 	r = sprintf(string_container, "TIME <");
-	for (i = 0; i < MTP_MAX_MONITOR_TYPE; i++)
-		r += sprintf(string_container + r, "%lld ", ktime_ns[i]);
+	if (r >= 0 && r < ARRAY_SIZE(string_container))
+		for (i = 0; i < MTP_MAX_MONITOR_TYPE; i++)
+			r += sprintf(string_container + r, "%lld ",
+				ktime_ns[i]);
 	MTP_DBG("%s>\n", string_container);
 
 monitor_work_exit:
