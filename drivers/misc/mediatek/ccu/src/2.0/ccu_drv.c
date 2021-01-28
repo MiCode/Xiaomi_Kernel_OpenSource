540e5eb211923e5c56a8f36c140b17a17223ee5b
Song-Tseng Ho <song-tseng.ho@mediatek.com>
2021-01-29 02:24:09 +0800

    [ALPS05372718] CCU: Fix 6785 devapc error
    1. Register CAM_PWR to ensure CCU pwr on
    before ccu_release.
    
    MTK-Commit-Id: f67b6d7ec8171b770be5a154f9e2c79746d888a5
    
    Change-Id: Id13dc9b81e81ebfb7b865bdff363457f43595b59
    CR-Id: ALPS05372718
    Feature: Camera Control Unit(ccu)
    Signed-off-by: Carl Fang <carl.fang@mediatek.com>

--- drivers/misc/mediatek/ccu/src/2.0/ccu_drv.c
+++ drivers/misc/mediatek/ccu/src/2.0/ccu_drv.c
@@ -77,7 +77,7 @@
 
 #define CCU_DEV_NAME            "ccu"
 
-#define CCU_CLK_NUM 2 /* [0]: Camsys, [1]: Mmsys */
+#define CCU_CLK_NUM 3 /* [0]: Camsys, [1]: Mmsys */
 struct clk *ccu_clk_ctrl[CCU_CLK_NUM];
 
 struct ccu_device_s *g_ccu_device;
@@ -578,21 +578,28 @@ int ccu_clock_enable(void)
 {
 	int ret;
 
-	LOG_DBG_MUST("%s.\n", __func__);
+	LOG_DBG_MUST("%s+\n", __func__);
 	ccu_qos_init();
 
-	ret = (clk_prepare_enable(ccu_clk_ctrl[0]) |
-		clk_prepare_enable(ccu_clk_ctrl[1]));
+	ret = clk_prepare_enable(ccu_clk_ctrl[0]);
 	if (ret)
-		LOG_ERR("clock enable fail.\n");
+		LOG_ERR("CCU_CLK_MMSYS_CCU enable fail.\n");
+	ret = clk_prepare_enable(ccu_clk_ctrl[1]);
+	if (ret)
+		LOG_ERR("CAM_PWR enable fail.\n");
+	ret = clk_prepare_enable(ccu_clk_ctrl[2]);
+	if (ret)
+		LOG_ERR("CCU_CLK_CAM_CCU enable fail.\n");
+
 	return ret;
 }
 
 void ccu_clock_disable(void)
 {
 	LOG_DBG_MUST("%s.\n", __func__);
-	clk_disable_unprepare(ccu_clk_ctrl[0]);
+	clk_disable_unprepare(ccu_clk_ctrl[2]);
 	clk_disable_unprepare(ccu_clk_ctrl[1]);
+	clk_disable_unprepare(ccu_clk_ctrl[0]);
 
 	ccu_qos_uninit();
 }
@@ -1163,13 +1170,19 @@ static int ccu_probe(struct platform_device *pdev)
 /* get Clock control from device tree.  */
 	{
 		ccu_clk_ctrl[0] =
-		devm_clk_get(g_ccu_device->dev, "CCU_CLK_CAM_CCU");
-		if (ccu_clk_ctrl[0] == NULL)
-			LOG_ERR("Get ccu clock ctrl camsys fail.\n");
-		ccu_clk_ctrl[1] =
 		devm_clk_get(g_ccu_device->dev, "CCU_CLK_MMSYS_CCU");
+		if (ccu_clk_ctrl[0] == NULL)
+			LOG_ERR("Get CCU_CLK_MMSYS_CCU fail.\n");
+
+		ccu_clk_ctrl[1] =
+		devm_clk_get(g_ccu_device->dev, "CAM_PWR");
 		if (ccu_clk_ctrl[1] == NULL)
-			LOG_ERR("Get ccu clock ctrl mmsys fail.\n");
+			LOG_ERR("Get CAM_PWR fail.\n");
+
+		ccu_clk_ctrl[2] =
+		devm_clk_get(g_ccu_device->dev, "CCU_CLK_CAM_CCU");
+		if (ccu_clk_ctrl[2] == NULL)
+			LOG_ERR("Get CCU_CLK_CAM_CCU fail.\n");
 	}
 	/**/
 	g_ccu_device->irq_num = irq_of_parse_and_map(node, 0);
