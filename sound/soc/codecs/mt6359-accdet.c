8feab657dc22de4a0c4445b840995c1ae3f88ac5
Argus Lin <argus.lin@mediatek.com>
2021-01-29 02:01:27 +0800

    [ALPS05087432] Accdet: fix coverity
    Fix Coverity
    
    MTK-Commit-Id: 6f4648b373e19620bf82ab01e088ea3e8b89c044
    
    Change-Id: Ie1e8b0ccaea08e8044f33a5a64f63cf27c8a6cfe
    Signed-off-by: Argus Lin <argus.lin@mediatek.com>
    CR-Id: ALPS05087432
    Feature: Headset Cable/Button Detection and Reaction

--- sound/soc/codecs/mt6359-accdet.c
+++ sound/soc/codecs/mt6359-accdet.c
@@ -380,6 +380,8 @@ static void cat_register(char *buf)
 	dump_register();
 	dump_reg = false;
 	ret = sprintf(accdet_log_buf, "ACCDET_RG\n");
+	if (ret < 0)
+		pr_notice("sprintf failed\n");
 	strncat(buf, accdet_log_buf, strlen(accdet_log_buf));
 	st_addr = ACCDET_AUXADC_SEL_ADDR;
 	end_addr = ACCDET_MON_FLAG_EN_ADDR;
@@ -391,9 +393,13 @@ static void cat_register(char *buf)
 			idx+2, accdet_read(idx+2),
 			idx+4, accdet_read(idx+4),
 			idx+6, accdet_read(idx+6));
+		if (ret < 0)
+			pr_notice("sprintf failed\n");
 		strncat(buf, accdet_log_buf, strlen(accdet_log_buf));
 	}
 	ret = sprintf(accdet_log_buf, "AUDDEC_ANA_RG\n");
+	if (ret < 0)
+		pr_notice("sprintf failed\n");
 	strncat(buf, accdet_log_buf, strlen(accdet_log_buf));
 	st_addr = RG_AUDPREAMPLON_ADDR;
 	end_addr = RG_CLKSQ_EN_ADDR;
@@ -405,17 +411,23 @@ static void cat_register(char *buf)
 			idx+2, accdet_read(idx+2),
 			idx+4, accdet_read(idx+4),
 			idx+6, accdet_read(idx+6));
+		if (ret < 0)
+			pr_notice("sprintf failed\n");
 		strncat(buf, accdet_log_buf, strlen(accdet_log_buf));
 	}
 
 	ret = sprintf(accdet_log_buf, "[0x%x]=0x%x\n",
 		RG_SCK32K_CK_PDN_ADDR,
 		accdet_read(RG_SCK32K_CK_PDN_ADDR));
+	if (ret < 0)
+		pr_notice("sprintf failed\n");
 	strncat(buf, accdet_log_buf, strlen(accdet_log_buf));
 
 	ret = sprintf(accdet_log_buf, "[0x%x]=0x%x\n",
 		RG_ACCDET_RST_ADDR,
 		accdet_read(RG_ACCDET_RST_ADDR));
+	if (ret < 0)
+		pr_notice("sprintf failed\n");
 	strncat(buf, accdet_log_buf, strlen(accdet_log_buf));
 
 	ret = sprintf(accdet_log_buf, "[0x%x]=0x%x, [0x%x]=0x%x, [0x%x]=0x%x\n",
@@ -425,6 +437,8 @@ static void cat_register(char *buf)
 		accdet_read(RG_INT_MASK_ACCDET_ADDR),
 		RG_INT_STATUS_ACCDET_ADDR,
 		accdet_read(RG_INT_STATUS_ACCDET_ADDR));
+	if (ret < 0)
+		pr_notice("sprintf failed\n");
 	strncat(buf, accdet_log_buf, strlen(accdet_log_buf));
 
 	ret = sprintf(accdet_log_buf, "[0x%x]=0x%x,[0x%x]=0x%x\n",
@@ -432,6 +446,8 @@ static void cat_register(char *buf)
 		accdet_read(RG_AUDPWDBMICBIAS1_ADDR),
 		RG_AUDACCDETMICBIAS0PULLLOW_ADDR,
 		accdet_read(RG_AUDACCDETMICBIAS0PULLLOW_ADDR));
+	if (ret < 0)
+		pr_notice("sprintf failed\n");
 	strncat(buf, accdet_log_buf, strlen(accdet_log_buf));
 
 	ret = sprintf(accdet_log_buf, "[0x%x]=0x%x, [0x%x]=0x%x\n",
@@ -439,15 +455,17 @@ static void cat_register(char *buf)
 		accdet_read(AUXADC_RQST_CH5_ADDR),
 		AUXADC_ACCDET_AUTO_SPL_ADDR,
 		accdet_read(AUXADC_ACCDET_AUTO_SPL_ADDR));
+	if (ret < 0)
+		pr_notice("sprintf failed\n");
 	strncat(buf, accdet_log_buf, strlen(accdet_log_buf));
 
 	ret = sprintf(accdet_log_buf,
 		"dtsInfo:deb0=0x%x,deb1=0x%x,deb3=0x%x,deb4=0x%x\n",
 		 cust_pwm_deb->debounce0, cust_pwm_deb->debounce1,
 		 cust_pwm_deb->debounce3, cust_pwm_deb->debounce4);
-	strncat(buf, accdet_log_buf, strlen(accdet_log_buf));
 	if (ret < 0)
 		pr_notice("sprintf failed\n");
+	strncat(buf, accdet_log_buf, strlen(accdet_log_buf));
 }
 
 static int dbug_thread(void *unused)
