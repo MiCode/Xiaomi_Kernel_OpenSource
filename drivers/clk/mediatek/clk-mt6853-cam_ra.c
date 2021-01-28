0568b76962ce87c5dc9250af053ce0d6281540d6
Juju Sung <juju.sung@mediatek.com>
2021-01-29 02:16:36 +0800

    [ALPS04999122] platform: add mt6853 clk drivers
    [Detail]
    mt_iommu_plat.h:30:10: error: clk-mt6853-pg.h file not found
    prepare mt6853 clock platform code from q0 to r0
    
    MTK-Commit-Id: 26416d029a695c93387185fbd62e174d9cb08e6d
    
    Change-Id: Ibd58877ef99e8ab5b30e2cb1e3dc8c90db872740
    Signed-off-by: Juju Sung <juju.sung@mediatek.com>
    CR-Id: ALPS04999122
    Feature: [Module]Kernel Maintenance

---
+++ drivers/clk/mediatek/clk-mt6853-cam_ra.c
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2020 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/clk-provider.h>
+#include <linux/platform_device.h>
+
+#include "clk-mtk.h"
+#include "clk-gate.h"
+
+#include <dt-bindings/clock/mt6853-clk.h>
+
+#define MT_CLKMGR_MODULE_INIT	0
+
+#define MT_CCF_BRINGUP			1
+
+#define INV_OFS			-1
+
+/* get spm power status struct to register inside clk_data */
+static struct pwr_status pwr_stat = GATE_PWR_STAT(0x16C,
+		0x170, INV_OFS, BIT(24), BIT(24));
+
+static const struct mtk_gate_regs cam_ra_cg_regs = {
+	.set_ofs = 0x4,
+	.clr_ofs = 0x8,
+	.sta_ofs = 0x0,
+};
+
+#define GATE_CAM_RA(_id, _name, _parent, _shift) {	\
+		.id = _id,				\
+		.name = _name,				\
+		.parent_name = _parent,			\
+		.regs = &cam_ra_cg_regs,			\
+		.shift = _shift,			\
+		.ops = &mtk_clk_gate_ops_setclr,	\
+		.pwr_stat = &pwr_stat,			\
+	}
+
+static const struct mtk_gate cam_ra_clks[] = {
+	GATE_CAM_RA(CLK_CAM_RA_LARBX, "cam_ra_larbx",
+			"cam_ck"/* parent */, 0),
+	GATE_CAM_RA(CLK_CAM_RA_CAM, "cam_ra_cam",
+			"cam_ck"/* parent */, 1),
+	GATE_CAM_RA(CLK_CAM_RA_CAMTG, "cam_ra_camtg",
+			"cam_ck"/* parent */, 2),
+};
+
+static int clk_mt6853_cam_ra_probe(struct platform_device *pdev)
+{
+	struct clk_onecell_data *clk_data;
+	int r;
+	struct device_node *node = pdev->dev.of_node;
+
+#if MT_CCF_BRINGUP
+	pr_notice("%s init begin\n", __func__);
+#endif
+
+	clk_data = mtk_alloc_clk_data(CLK_CAM_RA_NR_CLK);
+
+	mtk_clk_register_gates(node, cam_ra_clks, ARRAY_SIZE(cam_ra_clks),
+			clk_data);
+
+	r = of_clk_add_provider(node, of_clk_src_onecell_get, clk_data);
+
+	if (r)
+		pr_err("%s(): could not register clock provider: %d\n",
+			__func__, r);
+
+#if MT_CCF_BRINGUP
+	pr_notice("%s init end\n", __func__);
+#endif
+
+	return r;
+}
+
+static const struct of_device_id of_match_clk_mt6853_cam_ra[] = {
+	{ .compatible = "mediatek,mt6853-camsys_rawa", },
+	{}
+};
+
+#if MT_CLKMGR_MODULE_INIT
+
+static struct platform_driver clk_mt6853_cam_ra_drv = {
+	.probe = clk_mt6853_cam_ra_probe,
+	.driver = {
+		.name = "clk-mt6853-cam_ra",
+		.of_match_table = of_match_clk_mt6853_cam_ra,
+	},
+};
+
+builtin_platform_driver(clk_mt6853_cam_ra_drv);
+
+#else
+
+static struct platform_driver clk_mt6853_cam_ra_drv = {
+	.probe = clk_mt6853_cam_ra_probe,
+	.driver = {
+		.name = "clk-mt6853-cam_ra",
+		.of_match_table = of_match_clk_mt6853_cam_ra,
+	},
+};
+static int __init clk_mt6853_cam_ra_platform_init(void)
+{
+	return platform_driver_register(&clk_mt6853_cam_ra_drv);
+}
+arch_initcall(clk_mt6853_cam_ra_platform_init);
+
+#endif	/* MT_CLKMGR_MODULE_INIT */
