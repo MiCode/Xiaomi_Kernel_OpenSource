4bcac559d11c92d5eca223d609e80881badb1c78
Juju Sung <juju.sung@mediatek.com>
2021-01-29 01:02:21 +0800

    [ALPS05014853] vcodec: mt6885 pm header
    [Detail]
    This patch fix ion_drv.h file not found issue
    
    MTK-Commit-Id: 4fe2eb26ec400bdf4c3c21d4cf1e745977935fb3
    
    Change-Id: I710103c22a8ff3be50d5caa69c3577251ff96be8
    Signed-off-by: Juju Sung <juju.sung@mediatek.com>
    CR-Id: ALPS05014853
    Feature: [Module]Kernel Maintenance

--- drivers/media/platform/mtk-vcodec/mt6885/mtk_vcodec_pm.h
+++ drivers/media/platform/mtk-vcodec/mt6885/mtk_vcodec_pm.h
@@ -15,7 +15,14 @@
 #ifndef _MTK_VCODEC_PM_H_
 #define _MTK_VCODEC_PM_H_
 
+#include "ion_drv.h"
+#include "mach/mt_iommu.h"
+
+extern struct ion_device *g_ion_device;
+
 #define MTK_PLATFORM_STR        "platform:mt6885"
+#define MTK_VDEC_RACING_INFO_OFFSET  0x100
+#define MTK_VDEC_RACING_INFO_SIZE 68
 
 /**
  * struct mtk_vcodec_pm - Power management data structure
@@ -41,23 +48,29 @@ struct mtk_vcodec_pm {
 	struct device_node      *chip_node;
 	struct mtk_vcodec_dev   *mtkdev;
 
+	struct clk *clk_MT_CG_SOC;           /* VDEC SOC*/
 	struct clk *clk_MT_CG_VDEC0;         /* VDEC core 0*/
 	struct clk *clk_MT_CG_VENC0;         /* VENC core 0*/
 	struct clk *clk_MT_CG_VDEC1;         /* VDEC core 1*/
 	struct clk *clk_MT_CG_VENC1;         /* VENC core 1*/
 
+	atomic_t dec_active_cnt;
+	__u32 vdec_racing_info[MTK_VDEC_RACING_INFO_SIZE];
+	struct mutex dec_racing_info_mutex;
 };
 
 enum mtk_dec_dtsi_reg_idx {
 	VDEC_SYS,
+	VDEC_VLD,
 	VDEC_MISC,
 	VDEC_LAT_MISC,
+	VDEC_RACING_CTRL,
 	NUM_MAX_VDEC_REG_BASE,
 };
 
 enum mtk_enc_dtsi_reg_idx {
-	VENC_LT_SYS,
 	VENC_SYS,
+	VENC_C1_SYS,
 	NUM_MAX_VENC_REG_BASE
 };
 
