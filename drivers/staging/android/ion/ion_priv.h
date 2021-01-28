93f905777cab3158898c1786f34bd3e2a2c4fc2f
jun.wen <jun.wen@mediatek.com>
2021-01-29 02:19:06 +0800

    [ALPS05255994] ion: fix ION_IOC_{MAP,SHARE} use-after-free
    The ION_IOC_{MAP,SHARE} ioctls drop and reacquire client->lock several
    times while operating on one of the clients ion_handles.  This creates
    windows where userspace can call ION_IOC_FREE on the same client with
    the same handle, and effectively make the kernel drop its own reference.
    For example:
    
    - thread A: ION_IOC_ALLOC creates an ion_handle with refcount 1
    - thread A: starts ION_IOC_MAP and increments the refcount to 2
    - thread B: ION_IOC_FREE decrements the refcount to 1
    - thread B: ION_IOC_FREE decrements the refcount to 0 and frees the
                handle
    - thread A: continues ION_IOC_MAP with a dangling ion_handle * to
                freed memory
    
    Fix this by holding client->lock for the duration of
    ION_IOC_{MAP,SHARE}, preventing the concurrent ION_IOC_FREE.  Also
    remove ion_handle_get_by_id(), since theres literally no way to use it
    safely.
    
    MTK-Commit-Id: c39066f84375a04c1d7f6a0e217c8d1b3df92e5e
    
    Change-Id: Ib93464a19bf1e2f8347ba1bb14b7ac5ba43fc1c8
    Signed-off-by: Jun Wen <jun.wen@mediatek.com>
    CR-Id: ALPS05255994
    Feature: [Module]ION/M4U

--- drivers/staging/android/ion/ion_priv.h
+++ drivers/staging/android/ion/ion_priv.h
@@ -523,10 +523,6 @@ void ion_free_nolock(struct ion_client *client, struct ion_handle *handle);
 
 int ion_handle_put_nolock(struct ion_handle *handle);
 
-struct ion_handle *ion_handle_get_by_id(
-		struct ion_client *client,
-						int id);
-
 int ion_handle_put(struct ion_handle *handle);
 
 int ion_query_heaps(struct ion_client *client, struct ion_heap_query *query);
@@ -539,4 +535,7 @@ extern struct device *g_iommu_device;
 #endif
 
 extern atomic64_t page_sz_cnt;
+
+int ion_share_dma_buf_fd_nolock(struct ion_client *client,
+				struct ion_handle *handle);
 #endif /* _ION_PRIV_H */
