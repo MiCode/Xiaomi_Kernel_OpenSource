7408ee9a7b99635d6a4eac1853b310f953671290
mtk14715 <roger-th.lin@mediatek.com>
2021-01-28 21:14:09 +0800

    [ALPS05056271] EEPROM: porting 79 driver
    [Detail] porting 79 driver
    
    MTK-Commit-Id: aea7a7e6d52618b96b1c4209f42fb380e29e502a
    
    Change-Id: I851b6468567cf367a9f774fb52bda085a3798f38
    Signed-off-by: mtk14715 <roger-th.lin@mediatek.com>
    CR-Id: ALPS05056271
    Feature: [Android Default] Camera Application Basic Functions

---
+++ drivers/misc/mediatek/cam_cal/src/mt6779/cam_cal_list.c
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+#include <linux/kernel.h>
+#include "cam_cal_list.h"
+#include "eeprom_i2c_common_driver.h"
+#include "eeprom_i2c_custom_driver.h"
+#include "kd_imgsensor.h"
+
+#define IMX586_MAX_EEPROM_SIZE 0x24D0
+
+struct stCAM_CAL_LIST_STRUCT g_camCalList[] = {
+	{IMX519_SENSOR_ID, 0xA0, Common_read_region},
+	{S5K2T7SP_SENSOR_ID, 0xA4, Common_read_region},
+	{IMX386_SENSOR_ID, 0xA0, Common_read_region},
+	{IMX386_MONO_SENSOR_ID, 0xA0, Common_read_region},
+	{IMX398_SENSOR_ID, 0xA0, Common_read_region},
+	{IMX350_SENSOR_ID, 0xA0, Common_read_region},
+	{IMX586_SENSOR_ID, 0xA0, Common_read_region, IMX586_MAX_EEPROM_SIZE},
+	{IMX576_SENSOR_ID, 0xA2, Common_read_region},
+	{S5K2L7_SENSOR_ID, 0xA0, Common_read_region},
+	{IMX230_SENSOR_ID, 0xA0, Common_read_region},
+	{IMX338_SENSOR_ID, 0xA0, Common_read_region},
+	{IMX318_SENSOR_ID, 0xA0, Common_read_region},
+	{IMX258_SENSOR_ID, 0xA0, Common_read_region},
+	{S5K4E6_SENSOR_ID, 0xA8, Common_read_region},
+	/*  ADD before this line */
+	{0, 0, 0}	/*end of list */
+};
+
+unsigned int cam_cal_get_sensor_list(
+	struct stCAM_CAL_LIST_STRUCT **ppCamcalList)
+{
+	if (ppCamcalList == NULL)
+		return 1;
+
+	*ppCamcalList = &g_camCalList[0];
+	return 0;
+}
+
+
