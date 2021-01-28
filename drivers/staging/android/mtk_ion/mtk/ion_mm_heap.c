824c036cc6abdc2faac60b33381c5e233b083074
Guangming Cao <Guangming.Cao@mediatek.com>
2021-01-29 02:40:35 +0800

    [ALPS05390740] ION: fix twice mutex_unlock
    fix twice mutex_unlock
    
    MTK-Commit-Id: afa45aec2f8a6a044da1bb1dc06ee4ad1426d0bd
    
    Change-Id: Iccaa7ceb1d5ca8bd90fbbefb1ebd9bc5e148ee54
    Signed-off-by: Guangming Cao <Guangming.Cao@mediatek.com>
    CR-Id: ALPS05390740
    Feature: ION
    (cherry picked from commit 042240924a235e2f62d5880bd0f20bcaec74f2b1)

--- drivers/staging/android/mtk_ion/mtk/ion_mm_heap.c
+++ drivers/staging/android/mtk_ion/mtk/ion_mm_heap.c
@@ -2080,7 +2080,7 @@ long ion_mm_ioctl(struct ion_client *client, unsigned int cmd,
 				param.get_phys_param.len = 0;
 				IONMSG(" %s: Error. Cannot get iova.\n",
 				       __func__);
-				ret = -EFAULT;
+				return -EFAULT;
 			}
 			param.get_phys_param.phy_addr = phy_addr;
 
