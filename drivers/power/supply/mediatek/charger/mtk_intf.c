7bde7818bcf97ca79c3676402e99b77c87f8bc07
Elvin Lin <elvin.lin@mediatek.com>
2021-01-29 02:16:36 +0800

    [ALPS05204330] charger: sync charger patches
    [Detail]
    Sync the following patches from q0.mp2:
    1. charger: add return value for adapter_get_status
    2. charger: update pe4.0 cim setting
    3. charger: fix jeita cv
    4. charger: set pe4.0s max_vbus from dts
    5. charger: reset pe4.0 if hard reset is encountered
    
    MTK-Commit-Id: 18d9509e6dfe1552034311ee0163524dabea5b96
    
    Change-Id: I6039b896ab9d3f58d2ae23b9410637e9cbd4c088
    Signed-off-by: Elvin Lin <elvin.lin@mediatek.com>
    CR-Id: ALPS05204330
    Feature: Pump Express 4.0

--- drivers/power/supply/mediatek/charger/mtk_intf.c
+++ drivers/power/supply/mediatek/charger/mtk_intf.c
@@ -162,15 +162,16 @@ int adapter_get_pps_cap(struct pps_cap *cap)
 int adapter_get_status(struct ta_status *sta)
 {
 	struct adapter_status tasta = {0};
+	int ret = 0;
 
-	adapter_dev_get_status(pinfo->pd_adapter, &tasta);
+	ret = adapter_dev_get_status(pinfo->pd_adapter, &tasta);
 
 	sta->temperature = tasta.temperature;
 	sta->ocp = tasta.ocp;
 	sta->otp = tasta.otp;
 	sta->ovp = tasta.ovp;
 
-	return 0;
+	return ret;
 }
 
 int adapter_is_support_pd_pps(void)
