fb92709ed358d9892a9d23f786f43bace3bee39a
Ian-Y Chen <ian-y.chen@mediatek.com>
2021-01-29 02:06:11 +0800

    [ALPS05175403] power: fix coverity warning
    [Detail]
    Check return value of sscanf
    
    MTK-Commit-Id: 507a2da9c0e30148548e41fb57d3d7ebcac32d9c
    
    Change-Id: Ieb142e103e45e0263d0ed1200fe2b5ec974e9b19
    CR-Id: ALPS05175403
    Feature: [Module]PowerHAL
    Signed-off-by: Ian-Y Chen <ian-y.chen@mediatek.com>

--- drivers/misc/mediatek/performance/boost_ctrl/eas_ctrl/eas_ctrl.c
+++ drivers/misc/mediatek/performance/boost_ctrl/eas_ctrl/eas_ctrl.c
@@ -156,7 +156,6 @@ static ssize_t perfmgr_set_sched_isolation_proc_write(struct file *filp,
 {
 	unsigned int cpu_id = -1;
 	char buf[64];
-	int err = 0;
 
 	if (cnt >= sizeof(buf))
 		return -EINVAL;
@@ -165,7 +164,9 @@ static ssize_t perfmgr_set_sched_isolation_proc_write(struct file *filp,
 		return -EFAULT;
 	buf[cnt] = '\0';
 
-	err = sscanf(buf, "%iu", &cpu_id);
+	if (sscanf(buf, "%iu", &cpu_id) != 1)
+		return -EINVAL;
+
 	if (cpu_id >= nr_cpu_ids)
 		return cnt;
 
@@ -183,7 +184,6 @@ static ssize_t perfmgr_set_sched_deisolation_proc_write(struct file *filp,
 {
 	unsigned int cpu_id = -1;
 	char buf[64];
-	int err = 0;
 
 	if (cnt >= sizeof(buf))
 		return -EINVAL;
@@ -192,7 +192,9 @@ static ssize_t perfmgr_set_sched_deisolation_proc_write(struct file *filp,
 		return -EFAULT;
 	buf[cnt] = '\0';
 
-	err = sscanf(buf, "%iu", &cpu_id);
+	if (sscanf(buf, "%iu", &cpu_id) != 1)
+		return -EINVAL;
+
 	if (cpu_id >= nr_cpu_ids)
 		return cnt;
 
