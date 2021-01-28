cd464b6aa37df5bd631313174409c0014bdc547a
Shawn Huang <Shawn.Huang@mediatek.com>
2021-01-29 02:31:11 +0800

    [ALPS05305245] vpu 3.0: disable dynamic load algorithm support
    [Detail]
    Disable VPU_IOCTL_CREATE_ALGO & VPU_IOCTL_FREE_ALGO for security concern.
    
    MTK-Commit-Id: a24617a6855b9dc7bb7fc72d486ceea11e856312
    
    Change-Id: I6b331a87392e5e5c493eb8bf342cbd36772b5c13
    CR-Id: ALPS05305245
    Feature: VPU Platform
    Signed-off-by: Shawn Huang <shawn.huang@mediatek.com>

--- drivers/misc/mediatek/vpu/3.0/vpu_drv.c
+++ drivers/misc/mediatek/vpu/3.0/vpu_drv.c
@@ -46,6 +46,7 @@
 #endif
 
 #define VPU_DEV_NAME            "vpu"
+//#define VPU_LOAD_FW_SUPPORT
 
 static struct vpu_device *vpu_device;
 static struct list_head device_debug_list;
@@ -1258,6 +1259,7 @@ static long vpu_ioctl(struct file *flip, unsigned int cmd, unsigned long arg)
 	}
 	case VPU_IOCTL_CREATE_ALGO:
 	{
+#ifdef VPU_LOAD_FW_SUPPORT
 		struct vpu_create_algo *u_create_algo;
 		struct vpu_create_algo create_algo = {0};
 
@@ -1299,11 +1301,15 @@ static long vpu_ioctl(struct file *flip, unsigned int cmd, unsigned long arg)
 		}
 
 		vpu_add_algo_to_user(user, &create_algo);
-
+#else
+		ret = -EINVAL;
+		LOG_WRN("[CREATE_ALGO] was not support!\n");
+#endif
 		break;
 	}
 	case VPU_IOCTL_FREE_ALGO:
 	{
+#ifdef VPU_LOAD_FW_SUPPORT
 		struct vpu_create_algo *u_create_algo;
 		struct vpu_create_algo create_algo = {0};
 
@@ -1329,7 +1335,10 @@ static long vpu_ioctl(struct file *flip, unsigned int cmd, unsigned long arg)
 		create_algo.name[(sizeof(char)*32) - 1] = '\0';
 
 		vpu_free_algo_from_user(user, &create_algo);
-
+#else
+		ret = -EINVAL;
+		LOG_WRN("[FREE_ALGO] was not support!\n");
+#endif
 		break;
 	}
 
