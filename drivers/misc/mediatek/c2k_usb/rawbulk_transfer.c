7f58a5977fbcc202647de470d1a840e83d25b2a7
Jiaqin Wang <jiaqin.wang@mediatek.com>
2021-01-29 01:21:02 +0800

    [ALPS05160417] usb: usb_boost for C2K
    add usb_boost for C2K rawbulk transfer performance
    
    MTK-Commit-Id: a61b1fa0ae39ff1076df32ce512b07c950a8eed4
    
    Change-Id: Ia835c86903f9f8509d1a0131becfeba845f13a26
    Signed-off-by: Jiaqin Wang <jiaqin.wang@mediatek.com>
    CR-Id: ALPS05160417
    Feature: [Android Default] USB OTG

--- drivers/misc/mediatek/c2k_usb/rawbulk_transfer.c
+++ drivers/misc/mediatek/c2k_usb/rawbulk_transfer.c
@@ -23,6 +23,7 @@
 #endif
 #include "viatel_rawbulk.h"
 /* #include "modem_sdio.h" */
+#include "usb_boost.h"
 
 #ifdef CONFIG_MTK_ECCCI_C2K
 #define FS_CH_C2K 4
@@ -418,6 +419,8 @@ static void upstream_complete(struct usb_ep *ep, struct usb_request *req)
 
 	C2K_DBG("%s\n", __func__);
 
+	usb_boost();
+
 	t->state = UPSTREAM_STAT_FREE;
 
 	if (req->status < 0) {
@@ -757,6 +760,8 @@ static void downstream_complete(struct usb_ep *ep, struct usb_request *req)
 	struct rawbulk_transfer *transfer = t->transfer;
 
 	C2K_DBG("%s\n", __func__);
+
+	usb_boost();
 
 	t->state = DOWNSTREAM_STAT_FREE;
 
