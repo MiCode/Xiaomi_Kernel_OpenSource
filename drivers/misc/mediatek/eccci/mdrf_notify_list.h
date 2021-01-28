3c9e02aeaa273a24578687fe94765f6f1ba960ba
Guangming Cao <Guangming.Cao@mediatek.com>
2021-01-28 21:33:56 +0800

    [ALPS05053211] dispsys: add osc clk switch api in header file
    add api in eccci header file
    
    MTK-Commit-Id: c274808e4c9f483ab618cb66d6a9d805659fcd33
    
    Change-Id: Ic589dd66f1adec9d87217b52ec9c1bfc2de701a6
    Signed-off-by: Guangming Cao <Guangming.Cao@mediatek.com>
    CR-Id: ALPS05053211
    Feature: Display Driver (DSI Interface)

--- drivers/misc/mediatek/eccci/mdrf_notify_list.h
+++ drivers/misc/mediatek/eccci/mdrf_notify_list.h
@@ -9,5 +9,6 @@
  * para. 1: function name;
  * para. 2: module name;
  */
-
+MD_RF_NOTIFY(0, primary_display_ccci_mipi_callback, "MIPI_CLK")
+MD_RF_NOTIFY(1, primary_display_ccci_osc_callback, "LCM_OSC")
 
