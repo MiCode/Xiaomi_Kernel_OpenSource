36616c9065afaaf27fd13e792e760bfe1eaa0e87
Chang-An Chen <chang-an.chen@mediatek.com>
2021-01-28 20:30:39 +0800

    [ALPS04307558] RGU: fix console_unlock when lock not acquired
    Fix console_unlock when lock not acquired
    
    MTK-Commit-Id: 00ad45323766eaea33a3ddf10cd15c0dbed31208
    
    Change-Id: If162327f736120eebd599f694b8eccaa4c895aab
    Signed-off-by: Chang-An Chen <chang-an.chen@mediatek.com>
    CR-Id: ALPS04307558
    Feature: [Module]RGU (Reset Generator Unit)
    (cherry picked from commit f2c0974e1f675b0943abadf0ce60bad0bf5ca8b7)

--- drivers/watchdog/mediatek/wdk/wd_api.c
+++ drivers/watchdog/mediatek/wdk/wd_api.c
@@ -668,11 +668,12 @@ void arch_reset(char mode, const char *cmd)
 	res = get_wd_api(&wd_api);
 	pr_info("%s: cmd = %s\n", __func__, cmd ? : "NULL");
 	dump_stack();
-	if (console_trylock())
+	if (console_trylock()) {
 		pr_notice("we can get console_sem\n");
-	else
+		console_unlock();
+	} else {
 		pr_notice("we cannot get console_sem\n");
-	console_unlock();
+	}
 
 	if (cmd && !strcmp(cmd, "charger")) {
 		/* do nothing */
