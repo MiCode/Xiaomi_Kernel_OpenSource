772c3b0cfd01fe4010d62c6983d46faba45da388
andrew.yang <andrew.yang@mediatek.com>
2021-01-29 01:02:54 +0800

    [ALPS04820396] RTC: add power-off alarm support
    System will power on 1 minute earlier than expected alarm time.
    However, if system power off time is not 1 minute earlier than
    expected alarm time, system needs to power on in 15 seconds
    
    MTK-Commit-Id: 5a23bf0df38b19ee5d0be0f0556185b7574e93bf
    
    Change-Id: I8fc6688e9ca470a1c952c0ad4cbd41754b00bc11
    Signed-off-by: andrew.yang <andrew.yang@mediatek.com>
    CR-Id: ALPS04820396
    Feature: Power-Off Alarm
    (cherry picked from commit 240e9754c1def49b914081a26a3261f03f2ec75d)

--- drivers/rtc/rtc-mt6358.c
+++ drivers/rtc/rtc-mt6358.c
@@ -198,6 +198,7 @@ static int apply_lpsd_solution;
 static bool rtc_pm_notifier_registered;
 static bool kpoc_alarm;
 static unsigned long rtc_pm_status;
+static int alarm1m15s;
 
 module_param(rtc_show_time, int, 0644);
 module_param(rtc_show_alarm, int, 0644);
@@ -923,6 +924,9 @@ static int rtc_ops_set_alarm(struct device *dev, struct rtc_wkalrm *alm)
 		target = rtc_tm_to_ktime(tm);
 		target = ktime_add_ns(target, NSEC_PER_SEC);
 		tm = rtc_ktime_to_tm(target);
+	} else if (alm->enabled == 5) {
+		/* Power on system 1 minute earlier */
+		alarm1m15s = 1;
 	}
 
 	tm.tm_year -= RTC_MIN_YEAR_OFFSET;
@@ -935,10 +939,12 @@ static int rtc_ops_set_alarm(struct device *dev, struct rtc_wkalrm *alm)
 	spin_lock_irqsave(&rtc->lock, flags);
 	if (alm->enabled == 2) {	/* enable power-on alarm */
 		ret = mtk_rtc_set_pwron_alarm(true, &tm, false);
-	} else if (alm->enabled == 3) {	/* enable power-on alarm with logo */
+	} else if (alm->enabled == 3 || alm->enabled == 5) {
+		/* enable power-on alarm with logo */
 		ret = mtk_rtc_set_pwron_alarm(true, &tm, true);
 	} else if (alm->enabled == 4) {	/* disable power-on alarm */
 		ret = mtk_rtc_set_pwron_alarm(false, &tm, false);
+		alarm1m15s = 0;
 	}
 	if (ret < 0)
 		goto exit;
@@ -1091,6 +1097,69 @@ static int mtk_rtc_pdrv_remove(struct platform_device *pdev)
 	return 0;
 }
 
+static void mtk_rtc_pdrv_shutdown(struct platform_device *pdev)
+{
+	struct rtc_time rtc_time_now;
+	struct rtc_time rtc_time_alarm;
+	ktime_t ktime_now;
+	ktime_t ktime_alarm;
+	bool is_pwron_alarm;
+
+	if (alarm1m15s == 1) {
+		is_pwron_alarm = mtk_rtc_is_pwron_alarm(&rtc_time_now,
+			&rtc_time_alarm);
+		if (is_pwron_alarm) {
+			rtc_time_now.tm_year += RTC_MIN_YEAR_OFFSET;
+			rtc_time_now.tm_mon--;
+			rtc_time_alarm.tm_year += RTC_MIN_YEAR_OFFSET;
+			rtc_time_alarm.tm_mon--;
+			pr_notice("now = %04d/%02d/%02d %02d:%02d:%02d\n",
+				rtc_time_now.tm_year + 1900,
+				rtc_time_now.tm_mon + 1,
+				rtc_time_now.tm_mday,
+				rtc_time_now.tm_hour,
+				rtc_time_now.tm_min,
+				rtc_time_now.tm_sec);
+			pr_notice("alarm = %04d/%02d/%02d %02d:%02d:%02d\n",
+				rtc_time_alarm.tm_year + 1900,
+				rtc_time_alarm.tm_mon + 1,
+				rtc_time_alarm.tm_mday,
+				rtc_time_alarm.tm_hour,
+				rtc_time_alarm.tm_min,
+				rtc_time_alarm.tm_sec);
+			ktime_now = rtc_tm_to_ktime(rtc_time_now);
+			ktime_alarm = rtc_tm_to_ktime(rtc_time_alarm);
+			if (ktime_after(ktime_alarm, ktime_now)) {
+				/* alarm has not happened */
+				ktime_alarm = ktime_sub_ms(ktime_alarm,
+					MSEC_PER_SEC * 60);
+				if (ktime_after(ktime_alarm, ktime_now))
+					pr_notice("Alarm will happen after 1 minute\n");
+				else {
+					ktime_alarm = ktime_add_ms(ktime_now,
+						MSEC_PER_SEC * 15);
+					pr_notice("Alarm will happen in 15 seconds\n");
+				}
+				rtc_time_alarm = rtc_ktime_to_tm(ktime_alarm);
+				pr_notice("new alarm = %04d/%02d/%02d %02d:%02d:%02d\n",
+					rtc_time_alarm.tm_year + 1900,
+					rtc_time_alarm.tm_mon + 1,
+					rtc_time_alarm.tm_mday,
+					rtc_time_alarm.tm_hour,
+					rtc_time_alarm.tm_min,
+					rtc_time_alarm.tm_sec);
+				rtc_time_alarm.tm_year -= RTC_MIN_YEAR_OFFSET;
+				rtc_time_alarm.tm_mon++;
+				mtk_rtc_set_pwron_alarm_time(&rtc_time_alarm);
+				mtk_rtc_set_alarm(&rtc_time_alarm);
+			} else
+				pr_notice("Alarm has happened before\n");
+		} else
+			pr_notice("No power-off alarm is set\n");
+	}
+
+}
+
 static const struct of_device_id mt6358_rtc_of_match[] = {
 	{ .compatible = "mediatek,mt6358-rtc", },
 	{ .compatible = "mediatek,mt6359-rtc", },
@@ -1101,6 +1170,7 @@ MODULE_DEVICE_TABLE(of, mt6358_rtc_of_match);
 static struct platform_driver mtk_rtc_pdrv = {
 	.probe = mtk_rtc_pdrv_probe,
 	.remove = mtk_rtc_pdrv_remove,
+	.shutdown = mtk_rtc_pdrv_shutdown,
 	.driver = {
 		   .name = RTC_NAME,
 		   .owner = THIS_MODULE,
