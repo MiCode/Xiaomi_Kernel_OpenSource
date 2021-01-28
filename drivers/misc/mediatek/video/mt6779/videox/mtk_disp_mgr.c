194539d01e3c7ca43e8c4f1a2328223755c442bb
Guangming Cao <Guangming.Cao@mediatek.com>
2021-01-29 02:37:33 +0800

    [ALPS05378528] DISP: fix ioctl security bug
    [Detail]
    disp_info_user is not protected by lock
    it can be modified by other thread before kfree
    so add lock to protect it
    
    MTK-Commit-Id: d256791bfffe2cdb4a8bd722fac26eaec0fe0d17
    
    Change-Id: If05621ad98bbf1bf153af4bb9c0ce0a322651df7
    Signed-off-by: Guangming Cao <Guangming.Cao@mediatek.com>
    CR-Id: ALPS05378528
    Feature: [Module]Display Driver

--- drivers/misc/mediatek/video/mt6779/videox/mtk_disp_mgr.c
+++ drivers/misc/mediatek/video/mt6779/videox/mtk_disp_mgr.c
@@ -95,6 +95,7 @@ static int has_memory_session;
 /* @g_session: SESSION_TYPE | DEVICE_ID */
 static unsigned int g_session[MAX_SESSION_COUNT];
 static DEFINE_MUTEX(disp_session_lock);
+static DEFINE_MUTEX(disp_layer_lock);
 
 static dev_t mtk_disp_mgr_devno;
 static struct cdev *mtk_disp_mgr_cdev;
@@ -1369,7 +1370,9 @@ static long _ioctl_query_valid_layer(unsigned long arg)
 		return -EINVAL;
 	}
 
+	mutex_lock(&disp_layer_lock);
 	layering_rule_start(&disp_info_user, 0);
+	mutex_unlock(&disp_layer_lock);
 
 	if (copy_to_user(argp, &disp_info_user, sizeof(disp_info_user))) {
 		DISP_PR_ERR("[FB] copy_to_user failed! line:%d\n", __LINE__);
