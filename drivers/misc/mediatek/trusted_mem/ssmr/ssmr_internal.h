5cb81a9cbbaf47a76b69e6ecb6df1b7612372b89
James Hsu <james.hsu@mediatek.com>
2021-01-28 15:59:57 +0800

    [ALPS04916912] ssmr: support kernel module
    1. kernel module support of ssmr
    2. apply scenario base solution for CMA optimization
    
    MTK-Commit-Id: a68f90f3229e6859551136c60c185edc258c42c3
    
    Change-Id: I9faaab7067fd5f5a68dbf1aeec0460233852d17b
    Signed-off-by: James Hsu <james.hsu@mediatek.com>
    CR-Id: ALPS04916912
    Feature: [Module]SSMR (Shareable Secure Memory Region)

--- drivers/misc/mediatek/trusted_mem/ssmr/ssmr_internal.h
+++ drivers/misc/mediatek/trusted_mem/ssmr/ssmr_internal.h
@@ -43,28 +43,48 @@
 #define NAME_LEN 32
 #define CMD_LEN 64
 
-enum region_type {
-	SSMR_SECMEM,
-	SSMR_TUI,
-	SSMR_PROT_SHAREDMEM,
-	SSMR_TA_ELF,
-	SSMR_TA_STACK_HEAP,
-	SSMR_SDSP_TEE_SHAREDMEM,
-	SSMR_SDSP_FIRMWARE,
-	__MAX_NR_SSMRSUBS,
-};
+/* define scenario type */
+#define SVP_FLAGS		0x01u
+#define FACE_REGISTRATION_FLAGS 0x02u
+#define FACE_PAYMENT_FLAGS	0x04u
+#define FACE_UNLOCK_FLAGS	0x08u
+#define TUI_FLAGS		0x10u
 
 #define SSMR_INVALID_FEATURE(f) (f >= __MAX_NR_SSMR_FEATURES)
-#define SSMR_INVALID_REGION(r) (r >= __MAX_NR_SSMRSUBS)
 
+/*
+ *  req_size :         feature request size
+ *  proc_entry_fops :  file operation fun pointer
+ *  state :            region online/offline state
+ *  count :            region max alloc size by feature
+ *  alloc_pages :      current feature offline alloc size
+ *  is_unmapping :     unmapping state
+ *  use_cache_memory : when use reserved memory it will be true
+ *  page :             zmc alloc page
+ *  cache_page :       alloc page by reserved memory
+ *  usable_size :      cma usage size
+ *  scheme_flag :      show feaure support which schemes
+ *  enable :           show feature status
+ */
 struct SSMR_Feature {
+	bool is_unmapping;
+	bool use_cache_memory;
 	char dt_prop_name[NAME_LEN];
 	char feat_name[NAME_LEN];
 	char cmd_online[CMD_LEN];
 	char cmd_offline[CMD_LEN];
-	u64 req_size;
-	unsigned int region;
 	char enable[CMD_LEN];
+	const struct file_operations *proc_entry_fops;
+	dma_addr_t phy_addr;
+	size_t alloc_size;
+	struct page *page;
+	struct page *cache_page;
+	unsigned int scheme_flag;
+	unsigned int state;
+	unsigned long alloc_pages;
+	unsigned long count;
+	u64 req_size;
+	void *virt_addr;
 };
 
 enum ssmr_state {
@@ -77,202 +97,47 @@ enum ssmr_state {
 	NR_STATES,
 };
 
-/* clang-format off */
-const char *const ssmr_state_text[NR_STATES] = {
-	[SSMR_STATE_DISABLED]   = "[DISABLED]",
-	[SSMR_STATE_ONING_WAIT] = "[ONING_WAIT]",
-	[SSMR_STATE_ONING]      = "[ONING]",
-	[SSMR_STATE_ON]         = "[ON]",
-	[SSMR_STATE_OFFING]     = "[OFFING]",
-	[SSMR_STATE_OFF]        = "[OFF]",
+enum ssmr_scheme_state {
+	SSMR_SVP,
+	SSMR_FACE_REGISTRATION,
+	SSMR_FACE_PAYMENT,
+	SSMR_FACE_UNLOCK,
+	SSMR_TUI_SCHEME,
+	__MAX_NR_SCHEME,
 };
+
+/* clang-format off */
+extern const char *const ssmr_state_text[NR_STATES];
 /* clang-format on */
 
-/*
- *  name :             region name
- *  proc_entry_fops :  file operation fun pointer
- *  state :            region online/offline state
- *  count :            region max alloc size by feature
- *  alloc_pages :      current feature offline alloc size
- *  is_unmapping :     unmapping state
- *  use_cache_memory : when use reserved memory it will be true
- *  page :             zmc alloc page
- *  cache_page :       alloc page by reserved memory
- *  usable_size :      cma usage size
- *  cur_feat :         current feature in use
- */
-struct SSMR_Region {
+struct SSMR_Scheme {
 	char name[NAME_LEN];
-	const struct file_operations *proc_entry_fops;
-	unsigned int state;
-	unsigned long count;
-	unsigned long alloc_pages;
-	bool is_unmapping;
-	bool use_cache_memory;
-	struct page *page;
-	struct page *cache_page;
-	u64 usable_size;
-	unsigned int cur_feat;
+	u64  usable_size;
+	unsigned int flags;
 };
 
-/* clang-format off */
-static struct SSMR_Feature _ssmr_feats[__MAX_NR_SSMR_FEATURES] = {
-	[SSMR_FEAT_SVP] = {
-		.dt_prop_name = "svp-size",
-		.feat_name = "svp",
-		.cmd_online = "svp=on",
-		.cmd_offline = "svp=off",
-		.region = SSMR_SECMEM,
-#if defined(CONFIG_MTK_SEC_VIDEO_PATH_SUPPORT) ||\
-	defined(CONFIG_TRUSTONIC_TEE_SUPPORT) ||\
-	defined(CONFIG_MICROTRUST_TEE_SUPPORT)
-		.enable = "on"
-#else
-		.enable = "off"
-#endif
+static struct SSMR_Scheme _ssmrscheme[__MAX_NR_SCHEME] = {
+	[SSMR_SVP] = {
+		.name = "svp_scheme",
+		.flags = SVP_FLAGS
 	},
-	[SSMR_FEAT_PROT_SHAREDMEM] = {
-		.dt_prop_name = "prot-sharedmem-size",
-		.feat_name = "prot-sharedmem",
-		.cmd_online = "prot_sharedmem=on",
-		.cmd_offline = "prot_sharedmem=off",
-		.region = SSMR_PROT_SHAREDMEM,
-#ifdef CONFIG_MTK_PROT_MEM_SUPPORT
-		.enable = "on"
-#else
-		.enable = "off"
-#endif
+	[SSMR_FACE_REGISTRATION] = {
+		.name = "face_registration_scheme",
+		.flags = FACE_REGISTRATION_FLAGS
 	},
-	[SSMR_FEAT_WFD] = {
-		.dt_prop_name = "wfd-size",
-		.feat_name = "wfd",
-		.cmd_online = "wfd=on",
-		.cmd_offline = "wfd=off",
-		.region = SSMR_TUI,
-#ifdef CONFIG_MTK_SEC_VIDEO_PATH_SUPPORT
-		.enable = "on"
-#else
-		.enable = "off"
-#endif
+	[SSMR_FACE_PAYMENT] = {
+		.name = "face_payment_scheme",
+		.flags = FACE_PAYMENT_FLAGS
 	},
-	[SSMR_FEAT_TA_ELF] = {
-		.dt_prop_name = "ta-elf-size",
-		.feat_name = "ta-elf",
-		.cmd_online = "ta_elf=on",
-		.cmd_offline = "ta_elf=off",
-		.region = SSMR_TA_ELF,
-#ifdef CONFIG_MTK_HAPP_MEM_SUPPORT
-		.enable = "on"
-#else
-		.enable = "off"
-#endif
+	[SSMR_FACE_UNLOCK] = {
+		.name = "face_unlock_scheme",
+		.flags = FACE_UNLOCK_FLAGS
 	},
-	[SSMR_FEAT_TA_STACK_HEAP] = {
-		.dt_prop_name = "ta-stack-heap-size",
-		.feat_name = "ta-stack-heap",
-		.cmd_online = "ta_stack_heap=on",
-		.cmd_offline = "ta_stack_heap=off",
-		.region = SSMR_TA_STACK_HEAP,
-#ifdef CONFIG_MTK_HAPP_MEM_SUPPORT
-		.enable = "on"
-#else
-		.enable = "off"
-#endif
-	},
-	[SSMR_FEAT_SDSP_TEE_SHAREDMEM] = {
-		.dt_prop_name = "sdsp-tee-sharedmem-size",
-		.feat_name = "sdsp-tee-sharedmem",
-		.cmd_online = "sdsp_tee_sharedmem=on",
-		.cmd_offline = "sdsp_tee_sharedmem=off",
-		.region = SSMR_SDSP_TEE_SHAREDMEM,
-#ifdef CONFIG_MTK_SDSP_SHARED_MEM_SUPPORT
-		.enable = "on"
-#else
-		.enable = "off"
-#endif
-	},
-	[SSMR_FEAT_SDSP_FIRMWARE] = {
-		.dt_prop_name = "sdsp-firmware-size",
-		.feat_name = "sdsp-firmware",
-		.cmd_online = "sdsp_firmware=on",
-		.cmd_offline = "sdsp_firmware=off",
-		.region = SSMR_SDSP_FIRMWARE,
-#ifdef CONFIG_MTK_SDSP_MEM_SUPPORT
-		.enable = "on"
-#else
-		.enable = "off"
-#endif
-	},
-	[SSMR_FEAT_2D_FR] = {
-		.dt_prop_name = "2d_fr-size",
-		.feat_name = "2d_fr",
-		.cmd_online = "2d_fr=on",
-		.cmd_offline = "2d_fr=off",
-		.region = SSMR_SECMEM,
-#ifdef CONFIG_MTK_CAM_SECURITY_SUPPORT
-		.enable = "on"
-#else
-		.enable = "off"
-#endif
-	},
-	[SSMR_FEAT_TUI] = {
-		.dt_prop_name = "tui-size",
-		.feat_name = "tui",
-		.cmd_online = "tui=on",
-		.cmd_offline = "tui=off",
-		.region = SSMR_TUI,
-#if defined(CONFIG_TRUSTONIC_TRUSTED_UI) ||\
-	defined(CONFIG_BLOWFISH_TUI_SUPPORT)
-		.enable = "on"
-#else
-		.enable = "off"
-#endif
-	},
-	[SSMR_FEAT_IRIS] = {
-		.dt_prop_name = "iris-recognition-size",
-		.feat_name = "iris",
-		.cmd_online = "iris=on",
-		.cmd_offline = "iris=off",
-		.region = SSMR_SECMEM,
-#ifdef CONFIG_MTK_IRIS_SUPPORT
-		.enable = "on"
-#else
-		.enable = "off"
-#endif
+	[SSMR_TUI_SCHEME] = {
+		.name = "tui_scheme",
+		.flags = TUI_FLAGS
 	}
 };
-
-static struct SSMR_Region _ssmregs[__MAX_NR_SSMRSUBS] = {
-	[SSMR_SECMEM] = {
-		.name = "secmem_region",
-		.cur_feat = __MAX_NR_SSMR_FEATURES
-	},
-	[SSMR_TUI] = {
-		.name = "tui_region",
-		.cur_feat = __MAX_NR_SSMR_FEATURES
-	},
-	[SSMR_PROT_SHAREDMEM] = {
-		.name = "prot_sharedmem_region",
-		.cur_feat = __MAX_NR_SSMR_FEATURES
-	},
-	[SSMR_TA_ELF] = {
-		.name = "ta_elf_region",
-		.cur_feat = __MAX_NR_SSMR_FEATURES
-	},
-	[SSMR_TA_STACK_HEAP] = {
-		.name = "ta_stack_heap_region",
-		.cur_feat = __MAX_NR_SSMR_FEATURES
-	},
-	[SSMR_SDSP_TEE_SHAREDMEM] = {
-		.name = "sdsp_tee_sharedmem_region",
-		.cur_feat = __MAX_NR_SSMR_FEATURES
-	},
-	[SSMR_SDSP_FIRMWARE] = {
-		.name = "sdsp_firmware_region",
-		.cur_feat = __MAX_NR_SSMR_FEATURES
-	}
-};
-/* clang-format on */
 
 extern void show_pte(struct mm_struct *mm, unsigned long addr);
 
