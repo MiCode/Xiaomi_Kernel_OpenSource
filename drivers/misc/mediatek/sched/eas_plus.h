5d6885dd0d0646c941e097cb6d60b3ba2c1c1b68
Jing-Ting Wu <jing-ting.wu@mediatek.com>
2021-01-28 19:13:29 +0800

    [ALPS04025356] sched: introduce sched boost
    [Detail]
    Support sched boost to boost tasks into big cores.  Enable this feature
    by Kconfig (CONFIG_MTK_SCHED_BOOST=y)
    
    Sched boost type:
    0: cancel sched boost
    1: prefer boost ALL task to big cores
    2: foreground task move to big cores
    
    User space interface:
      /sys/devices/system/cpu/sched/sched_boost
      echo [sched_boost_type]  > /sys/devices/system/cpu/sched/sched_boost
    
    Kernel API:
      int set_sched_boost (unsigned int val)
      ARGUMENTS
         The sched boost type
      RETURN
         0: set successful
         -1:   fail to set boost
    
    [Solution]
    1. sched boost type=1
       tasks prefer put on big idle cores,
    2. sched boost type=2,  forground task move to big cores
       set the cpuset of foreground/top-app cgroup is big cores
    
    MTK-Commit-Id: e78be2aad8e900043055b0ecf1e34481da545e10
    
    Change-Id: I7102afe4dc974723580e6ac8f26384d1a2adc25a
    CR-Id:ALPS04025356
    Feature:Energy Aware Scheduling Plus (EAS+)
    Signed-off-by: Jing-Ting Wu <jing-ting.wu@mediatek.com>
    (cherry-pick from 756f5b0ccab9aaf562586235c71a5ecc4dbe175c)

--- drivers/misc/mediatek/sched/eas_plus.h
+++ drivers/misc/mediatek/sched/eas_plus.h
@@ -11,22 +11,6 @@
  * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
 
-
-/* turning_point:
- *	to help identify if "more-core at low freq" is suffered
- *	in share-bucked when the high OPP is triggered by heavy task.
- *  watershed:
- *	to quantify what ultra-low loading is "less-cores" prefer.
- */
-
-/* For multi-scheudling support */
-enum SCHED_LB_TYPE {
-	SCHED_HMP_LB = 0,
-	SCHED_EAS_LB,
-	SCHED_HYBRID_LB,
-	SCHED_UNKNOWN_LB
-};
-
 /* Stune group info */
 #ifdef CONFIG_SCHED_TUNE
 extern int group_boost_read(int group_idx);
