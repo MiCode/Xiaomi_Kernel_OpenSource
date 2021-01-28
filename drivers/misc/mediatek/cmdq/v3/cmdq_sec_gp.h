6640d7bec16567fdb0687d9695a74618bdcc48ca
Ming-Fan Chen <ming-fan.chen@mediatek.com>
2021-01-29 02:15:07 +0800

    [ALPS05139909] cmdq: fix build error for mt6873
    Sync structure from mailbox.
    
    MTK-Commit-Id: ffe42c1009e717c5c90e831da4034dff502c7c43
    
    Change-Id: I2bc1d587ebf6fc5b21b0bb2f7caca2dec9c6afd2
    Signed-off-by: Ming-Fan Chen <ming-fan.chen@mediatek.com>
    CR-Id: ALPS05139909
    Feature: [Module]CMDQ Driver

--- drivers/misc/mediatek/cmdq/v3/cmdq_sec_gp.h
+++ drivers/misc/mediatek/cmdq/v3/cmdq_sec_gp.h
@@ -25,6 +25,7 @@ struct cmdq_sec_tee_context {
 	struct TEEC_Session session; /* session handle */
 	struct TEEC_SharedMemory shared_mem; /* shared memory */
 	struct TEEC_SharedMemory shared_mem_ex; /* shared memory */
+	struct TEEC_SharedMemory shared_mem_ex2; /* shared memory */
 };
 
 #endif	/* __CMDQ_SEC_GP_H__ */
