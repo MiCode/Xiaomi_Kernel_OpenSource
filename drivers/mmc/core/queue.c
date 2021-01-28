481da22b6646efc68b03bd01c0f417dcac6d2985
Andy Teng <andy.teng@mediatek.com>
2021-01-29 00:51:42 +0800

    [ALPS04688289] mmc: use pre-alloc sg buffer for sw cmdq
    When using sw cmdq, use pre-alloc sg buffer to reduce
    memory pressure and allocate time.
    
    MTK-Commit-Id: c3bf8d67fd73bbfd8185b504a329190db6ac342e
    
    Change-Id: If705bf6e9ed973ad7297b2b9b58604b704eec0ac
    Signed-off-by: Andy Teng <andy.teng@mediatek.com>
    CR-Id: ALPS04688289
    Feature: eMMC Boot Up

--- drivers/mmc/core/queue.c
+++ drivers/mmc/core/queue.c
@@ -394,6 +394,12 @@ static int mmc_init_request(struct request_queue *q, struct request *req,
 	card = mq->card;
 	host = card->host;
 
+#ifdef CONFIG_MTK_EMMC_CQ_SUPPORT
+	/* cmdq use preallocate sg buffer */
+	if (mmc_blk_part_cmdq_en(mq))
+		return 0;
+#endif
+
 	mq_rq->sg = mmc_alloc_sg(host->max_segs, gfp);
 	if (!mq_rq->sg)
 		return -ENOMEM;
@@ -404,6 +410,13 @@ static int mmc_init_request(struct request_queue *q, struct request *req,
 static void mmc_exit_request(struct request_queue *q, struct request *req)
 {
 	struct mmc_queue_req *mq_rq = req_to_mmc_queue_req(req);
+
+#ifdef CONFIG_MTK_EMMC_CQ_SUPPORT
+	/* cmdq use preallocate sg buffer */
+	if (q->queuedata &&
+		mmc_blk_part_cmdq_en(q->queuedata))
+		return;
+#endif
 
 	kfree(mq_rq->sg);
 	mq_rq->sg = NULL;
