2addbf053e2d9bd5ac2a55c368ca12c7f2de85b1
Shaohua Deng <shaohua.deng@mediatek.com>
2021-01-28 15:33:37 +0800

    [ALPS04856465] DISP: disable share sram temp
    [Detail]
    disable share sram temp for boot pass
    
    MTK-Commit-Id: 70ceacdababff3d1c21e2e2ea9c78347b8648bb3
    
    Change-Id: Id1a88c3ca52b20bd2df042bb3c22e5a057442027
    Signed-off-by: Shaohua Deng <shaohua.deng@mediatek.com>
    CR-Id: ALPS04856465
    Feature: Display Driver (DSI Interface)

--- drivers/misc/mediatek/video/mt6765/videox/disp_helper.c
+++ drivers/misc/mediatek/video/mt6765/videox/disp_helper.c
@@ -348,7 +348,7 @@ void disp_helper_option_init(void)
 
 	/* 1. vdo mode + screen idle(need idlemgr) */
 	disp_helper_set_option(DISP_OPT_IDLEMGR_SWTCH_DECOUPLE,	1);
-	disp_helper_set_option(DISP_OPT_SHARE_SRAM, 1);
+	disp_helper_set_option(DISP_OPT_SHARE_SRAM, 0);
 	disp_helper_set_option(DISP_OPT_IDLEMGR_DISABLE_ROUTINE_IRQ, 1);
 
 	/* 2. cmd mode + screen idle(need idlemgr) */
@@ -371,7 +371,7 @@ void disp_helper_option_init(void)
 
 	disp_helper_set_option(DISP_OPT_BYPASS_PQ, 0);
 	disp_helper_set_option(DISP_OPT_MUTEX_EOF_EN_FOR_CMD_MODE, 1);
-	disp_helper_set_option(DISP_OPT_ESD_CHECK_RECOVERY, 1);
+	disp_helper_set_option(DISP_OPT_ESD_CHECK_RECOVERY, 0);
 	disp_helper_set_option(DISP_OPT_ESD_CHECK_SWITCH, 1);
 
 	disp_helper_set_option(DISP_OPT_BYPASS_OVL, 0);
