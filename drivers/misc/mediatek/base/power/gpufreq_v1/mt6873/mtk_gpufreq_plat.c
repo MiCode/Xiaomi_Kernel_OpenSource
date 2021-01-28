ffbdb272f74be7656cbfe48f39ccc0897de5aecf
mtk12444 <sagy.shih@mediatek.com>
2021-01-29 02:15:42 +0800

    [ALPS05072042] dbgtop: reset DFD timeout for reboot
    Reset DFD timeout for normal reboot
    
    MTK-Commit-Id: cf9738d1baa5034ea2beeb12871555ac39a48bff
    
    Change-Id: I9e6948821dfd99d10f30b8b98d358aa0594b5393
    Signed-off-by: mtk12444 <sagy.shih@mediatek.com>
    CR-Id: ALPS05072042
    Feature: System Performance
    (cherry picked from commit 85180973c1e92cbac92856810f31c6c746d4c319)

--- drivers/misc/mediatek/base/power/gpufreq_v1/mt6873/mtk_gpufreq_plat.c
+++ drivers/misc/mediatek/base/power/gpufreq_v1/mt6873/mtk_gpufreq_plat.c
@@ -912,7 +912,7 @@ static void __mt_gpufreq_config_dfd(bool enable)
 		// [8] enable
 		writel(0x0F001100, g_mfg_base + 0xA00);
 
-		mtk_dbgtop_dfd_timeout(0x3E8); // 500 ms
+		mtk_dbgtop_dfd_timeout(0x3E8, 0); // 500 ms
 
 	} else {
 		writel(0x00000000, g_mfg_base + 0xA00);
