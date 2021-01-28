03bd4039a20b5fa9d2677abe3a88a5f4739005db
Neal Liu <neal.liu@mediatek.com>
2021-01-29 02:16:09 +0800

    [ALPS05115812] mm: ioremap: catch illegal memory mapped ranges
    Catch illegal memory mapped ranges.
    
    MTK-Commit-Id: 18d615931eb6cf333d2a34277022177bbb04bc55
    
    Change-Id: Ib3d0e75541a1c07a4bf46f0d7e9f1876301ddf12
    Signed-off-by: Neal Liu <neal.liu@mediatek.com>
    CR-Id: ALPS05115812
    Feature: [Module]Device APC

--- arch/arm64/mm/ioremap.c
+++ arch/arm64/mm/ioremap.c
@@ -28,6 +28,7 @@
 #include <asm/fixmap.h>
 #include <asm/tlbflush.h>
 #include <asm/pgalloc.h>
+#include <mt-plat/devapc_public.h>
 
 static void __iomem *__ioremap_caller(phys_addr_t phys_addr, size_t size,
 				      pgprot_t prot, void *caller)
@@ -63,6 +64,10 @@ static void __iomem *__ioremap_caller(phys_addr_t phys_addr, size_t size,
 		return NULL;
 	addr = (unsigned long)area->addr;
 	area->phys_addr = phys_addr;
+
+#ifdef CONFIG_DEVAPC_MMAP_DEBUG
+	devapc_catch_illegal_range(phys_addr, size);
+#endif
 
 	err = ioremap_page_range(addr, addr + size, phys_addr, prot);
 	if (err) {
