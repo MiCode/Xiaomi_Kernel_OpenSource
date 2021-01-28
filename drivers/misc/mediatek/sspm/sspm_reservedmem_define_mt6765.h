c58ac478ae0908580266cc9cf2b39f689074caa3
Jason Kao <hc.kao@mediatek.com>
2021-01-29 00:05:20 +0800

    [ALPS05117441] sspm: add mt6765 drivers
    1. Add sspm drivers for mt6765.
    
    MTK-Commit-Id: 0819961a5d84cff598bec9ced23a563ab2b3acf3
    
    Change-Id: Icaa17bf02c9d828c6c3f0c11e1b2b4d2970a922b
    CR-Id: ALPS05117441
    Feature: TinySys
    Signed-off-by: Jason Kao <hc.kao@mediatek.com>

---
+++ drivers/misc/mediatek/sspm/sspm_reservedmem_define_mt6765.h
@@ -0,0 +1,53 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#ifndef _SSPM_RESERVEDMEM_DEFINE_H_
+#define _SSPM_RESERVEDMEM_DEFINE_H_
+#include <sspm_reservedmem.h>
+
+#define _SSPM_INTERNAL_
+
+#ifdef _SSPM_INTERNAL_
+/* The total size of sspm_reserve_mblock should less equal than
+ * reserve-memory-sspm_share of device tree
+ */
+static struct sspm_reserve_mblock mt6765_sspm_reserve_mblock[NUMS_MEM_ID] = {
+	{
+		.num = SSPM_MEM_ID,
+		.size = 0x400 + SSPM_PLT_LOGGER_BUF_LEN,
+		/* logger header + 1M log buffer */
+	},
+	{
+		.num = PWRAP_MEM_ID,
+		.size = 0x300,  /* 768 bytes */
+	},
+	{
+		.num = PMIC_MEM_ID,
+		.size = 0xC00,  /* 3K */
+	},
+	{
+		.num = UPD_MEM_ID,
+		.size = 0x1800, /* 6K */
+	},
+	{
+		.num = QOS_MEM_ID,
+		.size = 0x1000, /* 4K */
+	},
+	{
+		.num = SWPM_MEM_ID,
+		.size = 0xC00,  /* 3K */
+	},
+#if defined(CONFIG_MTK_GMO_RAM_OPTIMIZE) || defined(CONFIG_MTK_MET_MEM_ALLOC)
+#else
+	{
+		.num = MET_MEM_ID,
+		.size = 0x400000, /* 4M */
+	},
+#endif
+	/* TO align 64K, total is 1M+64K. The remaining size = 0xB900. */
+};
+#endif
+#endif
+
