b20a1fb294197b299339d36a6c394f92b0a5770d
Joey Pan <joey.pan@mediatek.com>
2021-01-29 00:49:25 +0800

    [ALPS04705611] DISP:Fix dereference after null check
    Because the trigger of ASSERT is on the false return,
    we revise the related control setting.
    
    MTK-Commit-Id: 98d8780f9efa8aad32c119a981c2cc06e468d5d2
    
    Change-Id: I7f4bcd56140f5ffeab398cbe32cdc832d763c147
    Signed-off-by: Joey Pan <joey.pan@mediatek.com>
    CR-Id: ALPS04705611
    Feature: [Module]Display Driver

--- drivers/misc/mediatek/video/mt6885/dispsys/ddp_manager.c
+++ drivers/misc/mediatek/video/mt6885/dispsys/ddp_manager.c
@@ -1123,7 +1123,7 @@ int dpmgr_path_config(disp_path_handle dp_handle,
 	int *modules;
 	struct DDP_MODULE_DRIVER *mod_drv;
 
-	ASSERT(dp_handle != NULL);
+	ASSERT(dp_handle);
 	handle = (struct ddp_path_handle *)dp_handle;
 	modules = ddp_get_scenario_list(handle->scenario);
 	module_num = ddp_get_module_num(handle->scenario);
