359b0850dc465b9e8b2c4804bd2009f1f8205614
Stanley Chu <stanley.chu@mediatek.com>
2021-01-29 00:44:22 +0800

    [ALPS04661432] blocktag: show real owner of delayed-written pages
    [Detail]
    Dirty pages may be written by writeback thread later.
    To get real i/o owner of such pages, we shall keep owner
    information before writeback takes over.
    
    This patch also enhance pidloggers logic and footprint,
    
    - Simplify owner logic:
      1. If page is dirtied before, take the owner who do it.
      2. If not, take the owner who do submit_bio.
    
    - Reduce footprint:
      In pidlogger, only one PID shall be kept for each
      pages (two PIDs before). We saves 50% footprint.
    
      For example, for 4GB DRAM projects, pidlogger needs,
      Before: 1048576 entries x 4 bytes = 4096 KB
      After : 1048576 entries x 2 bytes = 2048 KB
    
      PS. Each PID needs 2 bytes (MAX PID: 32767).
    
    MTK-Commit-Id: 199fb8e67b60a4355cb652f547799013269209ba
    
    Change-Id: I67dd60e719b09b78979f66f44853dfad33adc5b2
    Signed-off-by: Stanley Chu <stanley.chu@mediatek.com>
    CR-Id: ALPS04661432
    Feature: Android Exception Engine(AEE)

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -48,6 +48,8 @@
 #include <trace/events/ext4.h>
 #include <trace/events/android_fs.h>
 
+#include <mt-plat/mtk_blocktag.h>
+
 #define MPAGE_DA_EXTENT_TAIL 0x01
 
 static __u32 ext4_inode_csum(struct inode *inode, struct ext4_inode *raw,
@@ -1374,6 +1376,7 @@ retry_journal:
 		return ret;
 	}
 	*pagep = page;
+	mtk_btag_pidlog_set_pid(*pagep);
 	return ret;
 }
 
