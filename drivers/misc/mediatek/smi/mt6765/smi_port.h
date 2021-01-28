ba3e7c73474996d1a9757f6ffa33be2193421cf0
Sabrina Kuo <Yi.Kuo@mediatek.com>
2021-01-28 22:50:56 +0800

    [ALPS05119896] smi: fix dcr issue
    remove mt6765-larb-port.h from smi_port.h
    
    MTK-Commit-Id: b06f27b97b552b734591f5f4e5473462d71cbe4e
    
    Change-Id: I1a7f1db78011807bc089ba3bf4694d29513be71f
    Signed-off-by: Sabrina Kuo <Yi.Kuo@mediatek.com>
    CR-Id: ALPS05119896
    Feature: [Module]SMI Driver

--- drivers/misc/mediatek/smi/mt6765/smi_port.h
+++ drivers/misc/mediatek/smi/mt6765/smi_port.h
@@ -5,7 +5,6 @@
 
 #ifndef __SMI_PORT_H__
 #define __SMI_PORT_H__
-#include <dt-bindings/memory/mt6761-larb-port.h>
 #define SMI_OSTD_MAX		(0x3f)
 #define SMI_COMM_MASTER_NUM	(4)
 #define SMI_LARB_NUM		(4)
@@ -15,8 +14,7 @@
 #define SMI_LARB3_PORT_NUM	(21)	/* CAMSYS */
 #define SMI_COMM_NUM		(1)
 #define SMI_DEV_NUM		((SMI_LARB_NUM) + (SMI_COMM_NUM))
-//#define SMI_LARB_CCU0		(13)
-//#define SMI_LARB_CCU1		(14)
+
 static const bool
 SMI_COMM_BUS_SEL[SMI_COMM_MASTER_NUM] = {0, 1, 0, 1,};
 static const u32
