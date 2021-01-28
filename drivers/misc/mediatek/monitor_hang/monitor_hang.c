c123ec5d3e5ba86b3ffd90ad621d2eeb98765db8
alex.zhang <alex.zhang@mediatek.com>
2021-01-29 02:20:52 +0800

    [ALPS04671447] hang: add sub thread dump
    even though parent thread stack is null, we will dump sub thread info
    
    MTK-Commit-Id: 4270c5208cb2abfbba25c18417d52335c1ccfb78
    
    Change-Id: I8f2b19653061bda86d0d0a19e077f590fdf8e43f
    Signed-off-by: alex.zhang <alex.zhang@mediatek.com>
    CR-Id: ALPS04671447
    Feature: Android Exception Engine(AEE)
    (cherry picked from commit f95ce9c51a867603993a13dcf9c5bb42a369da82)

--- drivers/misc/mediatek/monitor_hang/monitor_hang.c
+++ drivers/misc/mediatek/monitor_hang/monitor_hang.c
@@ -53,7 +53,7 @@
 #include <mrdump.h>
 
 #ifndef TASK_STATE_TO_CHAR_STR
-#define TASK_STATE_TO_CHAR_STR "RSDTtXZxKWPNn"
+#define TASK_STATE_TO_CHAR_STR "RSDTtZXxKWPNn"
 #endif
 
 #ifdef CONFIG_MTK_HANG_DETECT_DB
@@ -1657,41 +1657,55 @@ static void show_bt_by_pid(int task_pid)
 #endif
 	int count = 0, dump_native = 0;
 	unsigned int state = 0;
-
+	char stat_nam[] = TASK_STATE_TO_CHAR_STR;
 	pid = find_get_pid(task_pid);
 	t = p = get_pid_task(pid, PIDTYPE_PID);
-
-	if (p && try_get_task_stack(p)) {
-		Log2HangInfo("%s: %d: %s.\n", __func__, task_pid, t->comm);
-		hang_log("%s: %d: %s.\n", __func__, task_pid, t->comm);
+	if (p != NULL) {
+		if (try_get_task_stack(p)) {
+			Log2HangInfo("%s: %d: %s.\n", __func__, task_pid, t->comm);
+			hang_log("%s: %d: %s.\n", __func__, task_pid, t->comm);
 #ifndef __aarch64__	 /* 32bit */
-		if (!strcmp(t->comm, "system_server"))
-			dump_native = 1;
-		else
-			dump_native = 0;
-#else
-		user_ret = task_pt_regs(t);
-
-		if (!user_mode(user_ret)) {
-			pr_info(" %s,%d:%s,fail in user_mode", __func__,
-					task_pid, t->comm);
-			dump_native = 0;
-		} else	if (!t->mm) {
-			pr_info(" %s,%d:%s, current_task->mm == NULL", __func__,
-					task_pid, t->comm);
-			dump_native = 0;
-		} else if (compat_user_mode(user_ret)) {
-			/* K64_U32 for check reg */
 			if (!strcmp(t->comm, "system_server"))
 				dump_native = 1;
 			else
 				dump_native = 0;
-		} else
-			dump_native = 1;
+#else
+			user_ret = task_pt_regs(t);
+
+			if (!user_mode(user_ret)) {
+				pr_info(" %s,%d:%s,fail in user_mode", __func__,
+						task_pid, t->comm);
+				dump_native = 0;
+			} else	if (!t->mm) {
+				pr_info(" %s,%d:%s, current_task->mm == NULL", __func__,
+						task_pid, t->comm);
+				dump_native = 0;
+			} else if (compat_user_mode(user_ret)) {
+				/* K64_U32 for check reg */
+				if (!strcmp(t->comm, "system_server"))
+					dump_native = 1;
+				else
+					dump_native = 0;
+			} else
+				dump_native = 1;
 #endif
-		if (dump_native == 1)
-			/* catch maps to Userthread_maps */
-			DumpThreadNativeMaps(task_pid, p);
+			if (dump_native == 1)
+				/* catch maps to Userthread_maps */
+				DumpThreadNativeMaps(task_pid, p);
+#ifdef MODULE
+			Pput_task_stack(t);
+#else
+			put_task_stack(p);
+#endif
+		} else {
+			state = p->state ? __ffs(p->state) + 1 : 0;
+			Log2HangInfo("%s pid %d state %c, flags %d. stack is null.\n",
+				t->comm, task_pid, state < sizeof(stat_nam) - 1 ?
+				stat_nam[state] : '?', t->flags);
+			hang_log("%s pid %d state %c, flags %d. stack is null.\n",
+				t->comm, task_pid, state < sizeof(stat_nam) - 1 ?
+				stat_nam[state] : '?', t->flags);
+		}
 		do {
 			if (t && try_get_task_stack(t)) {
 				pid_t tid = 0;
@@ -1720,18 +1734,7 @@ static void show_bt_by_pid(int task_pid)
 				msleep(20);
 			Log2HangInfo("-\n");
 		} while_each_thread(p, t);
-#ifdef MODULE
-		Pput_task_stack(t);
-#else
-		put_task_stack(p);
-#endif
 		put_task_struct(p);
-	} else if (p) {
-		put_task_struct(p);
-		Log2HangInfo("%s pid %d state %ld, flags %d. stack is null.\n",
-			t->comm, task_pid, t->state, t->flags);
-		hang_log("%s pid %d state %ld, flags %d. stack is null.\n",
-			t->comm, task_pid, t->state, t->flags);
 	}
 	put_pid(pid);
 }
