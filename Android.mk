1d151f6341c11da9d138a7a83d0a53f2bb579f3d
emily <jiehui.li@mediatek.com>
2021-01-28 22:49:58 +0800

    [ALPS05136592] DTBO: Update dtbo build template
    [Detail]
    Update dtbo build template for tablet.
    
    MTK-Commit-Id: 95b61861be549aea4b3be9c6c429b4aa76b71f4f
    
    Change-Id: I0aa3f7f760212dbff1607c28217d5fce3d6f852b
    CR-Id: ALPS05136592
    Feature: [Module]Kernel Maintenance
    Signed-off-by: emily <jiehui.li@mediatek.com>

--- Android.mk
+++ Android.mk
@@ -62,11 +62,11 @@ MTK_DTBIMAGE_DTS := $(KERNEL_DIR)/arch/$(KERNEL_TARGET_ARCH)/boot/dts/$(MTK_PLAT
 endif
 include device/mediatek/build/core/build_dtbimage.mk
 ifeq ($(KERNEL_TARGET_ARCH),arm64)
-MTK_DTBOIMAGE_DTS := $(KERNEL_DIR)/arch/$(KERNEL_TARGET_ARCH)/boot/dts/mediatek/$(MTK_BASE_PROJECT).dts
+MTK_DTBOIMAGE_DTS := $(KERNEL_DIR)/arch/$(KERNEL_TARGET_ARCH)/boot/dts/mediatek/$(MTK_TARGET_PROJECT).dts
 else
-MTK_DTBOIMAGE_DTS := $(KERNEL_DIR)/arch/$(KERNEL_TARGET_ARCH)/boot/dts/$(MTK_BASE_PROJECT).dts
+MTK_DTBOIMAGE_DTS := $(KERNEL_DIR)/arch/$(KERNEL_TARGET_ARCH)/boot/dts/$(MTK_TARGET_PROJECT).dts
 endif
-MTK_DTBOIMAGE_DWS := $(KERNEL_DIR)/drivers/misc/mediatek/dws/$(MTK_PLATFORM_DIR)/$(MTK_BASE_PROJECT).dws
+#MTK_DTBOIMAGE_DWS := $(KERNEL_DIR)/drivers/misc/mediatek/dws/$(MTK_PLATFORM_DIR)/$(MTK_BASE_PROJECT).dws
 include device/mediatek/build/core/build_dtboimage.mk
 
 endif #TARGET_NO_KERNEL
