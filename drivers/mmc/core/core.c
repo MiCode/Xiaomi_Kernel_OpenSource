688ceba964a1a0f1037272320cce127a87c41464
mtk22786 <Mason.Zhang@mediatek.com>
2021-01-29 02:33:50 +0800

    [ALPS05158648] msdc:enlarge max_discard_sectors
    enlarge max_discard_sectors for speed up discard
    
    MTK-Commit-Id: 81de94341dd916f6660cf88811ff23c41618e7a5
    
    Change-Id: If3de912be305c07ce94f86f7a7a7c59780131999
    CR-Id: ALPS05158648
    Feature: [Android Default] F2FS File System
    Signed-off-by: mtk22786 <Mason.Zhang@mediatek.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -3160,6 +3160,8 @@ unsigned int mmc_calc_max_discard(struct mmc_card *card)
 	struct mmc_host *host = card->host;
 	unsigned int max_discard, max_trim;
 
+	if (!host->max_busy_timeout)
+		return UINT_MAX;
 	/*
 	 * Without erase_group_def set, MMC erase timeout depends on clock
 	 * frequence which can change.  In that case, the best choice is
