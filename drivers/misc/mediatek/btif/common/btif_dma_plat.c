1de38ad5322d913706200d5b7bd2273585fb57f1
Robbin Chiu <robbin.chiu@mediatek.com>
2021-01-29 00:57:28 +0800

    [ALPS04778482] WMT: BTIF CRC error issue
    [Solution]
    Innvalidate memory before read EMI,
    and flush memory after write EMI
    
    MTK-Commit-Id: 57345cbc8fa148e8b6230fdb42ebb84d2d028ad4
    
    Change-Id: Id94119baad9d5bf755bc091c36f28e618e9343d9
    Signed-off-by: Robbin Chiu <robbin.chiu@mediatek.com>
    CR-Id: ALPS04778482
    Feature: [Module]WMT Driver

--- drivers/misc/mediatek/btif/common/btif_dma_plat.c
+++ drivers/misc/mediatek/btif/common/btif_dma_plat.c
@@ -874,7 +874,9 @@ int hal_dma_send_data(struct _MTK_DMA_INFO_STR_ *p_dma_info,
 		       p_data, len_to_send - tail_len);
 /*make sure all data write to memory area tx vfifo locates*/
 		mb();
-
+#ifdef CONFIG_ARM64
+		__dma_flush_area((void *)p_buf, buf_len);
+#endif
 /*calculate WPT*/
 		wpt = wpt + len_to_send - vff_size;
 		last_wpt_wrap ^= DMA_WPT_WRAP;
@@ -883,7 +885,9 @@ int hal_dma_send_data(struct _MTK_DMA_INFO_STR_ *p_dma_info,
 		       p_data, len_to_send);
 /*make sure all data write to memory area tx vfifo locates*/
 		mb();
-
+#ifdef CONFIG_ARM64
+		__dma_flush_area((void *)p_buf, buf_len);
+#endif
 /*calculate WPT*/
 		wpt += len_to_send;
 	}
