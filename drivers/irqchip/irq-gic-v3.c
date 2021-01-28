65ebba613551b6a9b1d1d31a7a252c5e16fec272
Hanks Chen <hanks.chen@mediatek.com>
2021-01-29 02:17:50 +0800

    [ALPS05228660] BACKPORT: irqchip/gic-v3: Allow interrupt to be
    [Detail]
    irqchip/gic-v3: Allow interrupt to be configured as wake-up sources
    
    Although GICv3 doesnt directly offers support for wake-up
    interrupts and relies on external HW for this,
    it shouldnt prevent the driver for such HW from doing it work.
    
    Lets set the required flags on the irq_chip structures.
    
    MTK-Commit-Id: f601bab7505348c98d48b8193fb7a441b0488f12
    
    Reported-by: Lina Iyer <ilina@codeaurora.org>
    Tested-by: Lina Iyer <ilina@codeaurora.org>
    Reviewed-by: Sudeep Holla <sudeep.holla@arm.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Hanks Chen <hanks.chen@mediatek.com>
    (cherry picked from commit bdc48fa11e46f867ea4d75fa59ee87a7f48be144)
    Change-Id: Iebcc86c840223cec4171fa341dd72ab27f8ef679
    CR-Id: ALPS05228660
    Feature: System Performance

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -833,7 +833,9 @@ static struct irq_chip gic_chip = {
 	.irq_set_affinity	= gic_set_affinity,
 	.irq_get_irqchip_state	= gic_irq_get_irqchip_state,
 	.irq_set_irqchip_state	= gic_irq_set_irqchip_state,
-	.flags			= IRQCHIP_SET_TYPE_MASKED,
+	.flags			= IRQCHIP_SET_TYPE_MASKED |
+				  IRQCHIP_SKIP_SET_WAKE |
+				  IRQCHIP_MASK_ON_SUSPEND,
 };
 
 static struct irq_chip gic_eoimode1_chip = {
@@ -846,7 +848,9 @@ static struct irq_chip gic_eoimode1_chip = {
 	.irq_get_irqchip_state	= gic_irq_get_irqchip_state,
 	.irq_set_irqchip_state	= gic_irq_set_irqchip_state,
 	.irq_set_vcpu_affinity	= gic_irq_set_vcpu_affinity,
-	.flags			= IRQCHIP_SET_TYPE_MASKED,
+	.flags			= IRQCHIP_SET_TYPE_MASKED |
+				  IRQCHIP_SKIP_SET_WAKE |
+				  IRQCHIP_MASK_ON_SUSPEND,
 };
 
 #define GIC_ID_NR		(1U << gic_data.rdists.id_bits)
