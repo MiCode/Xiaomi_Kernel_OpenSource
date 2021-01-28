27aa3830dc164b2dce4972a3eab083b9ca4eeb06
Wei-Chia Su <wei-chia.su@mediatek.com>
2021-01-29 00:06:45 +0800

    [ALPS05004581] mcdi: use printk_deferred
    [Detail] mcdi: use printk_deferred
    
    MTK-Commit-Id: c81890f45b630711a9c91b56656dd9f7f1567627
    
    Change-Id: I42ecd249ffbfdd09f239b6442a32f656289e7e77
    Signed-off-by: Wei-Chia Su <wei-chia.su@mediatek.com>
    CR-Id: ALPS05004581
    Feature: MCU Deep Idle

--- drivers/misc/mediatek/base/power/mcdi/mcdi_v1/mtk_mcdi_main.c
+++ drivers/misc/mediatek/base/power/mcdi/mcdi_v1/mtk_mcdi_main.c
@@ -625,7 +625,7 @@ void mcdi_heart_beat_log_dump(void)
 	mcdi_buf_append(buf, ", system_idle_hint = %08x",
 						system_idle_hint_result_raw());
 
-	pr_info("%s\n", get_mcdi_buf(buf));
+	printk_deferred("%s\n", get_mcdi_buf(buf));
 }
 
 int wfi_enter(int cpu)
