04129bc6bce369078d73a121054ea14fba545d15
Howie Huang <Howie.Huang@mediatek.com>
2021-01-29 02:17:03 +0800

    [ALPS05216623] ASoC:mt8168:Porting audio driver to R branch
    [Detail]
    The driver code need to be updated
    
    [Solution]
    Porting latest code of audio driver from Q branch to R branch
    
    MTK-Commit-Id: bb4aa06876058d44a87808c0de69f37fdde09a78
    
    Change-Id: I6bc169b81d8296954ee030e226ffe080d88d17d3
    Signed-off-by: Howie Huang <Howie.Huang@mediatek.com>
    CR-Id: ALPS05216623
    Feature: [Module]Audio Kernel

--- drivers/misc/mediatek/hifidsp_audio_ipi/common/framework/audio_ipi_driver.c
+++ drivers/misc/mediatek/hifidsp_audio_ipi/common/framework/audio_ipi_driver.c
@@ -60,6 +60,11 @@
 #endif
 
 
+#ifdef CONFIG_MTK_AUDIO_TUNNELING_SUPPORT
+#include "audio_ipi_client_playback.h"
+#endif
+
+
 
 /*
  * =============================================================================
