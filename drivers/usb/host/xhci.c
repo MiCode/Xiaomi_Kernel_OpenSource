be5f5d10de45a0bc5228f74bc2b40017ed0ce087
Hemant Kumar <hemantk@codeaurora.org>
2021-01-29 01:21:11 +0800

    [ALPS05158630] usb: XHCI: Implement xhci_handshake_check_state() API
    This API is called to handle command timeout which may
    happen upon XHCI stack removal. Since handshake is performed
    by polling long duration for status, this can cause watchdog
    timeout if status does not change for entire duration of
    polling. Hence add new API which also checks for xhci state
    and exit the handshake if xhci is removed. Use this api and
    bailout command abort operation upon XHCI remove.
    
    MTK-Commit-Id: 59187c18820ed9862ba2e59d8ee2c99b3d330eb4
    
    Change-Id: I9a90b2dd83d76f80e3f6fd4546b949c88850edd3
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>
    Signed-off-by: Eddie Hung <eddie.hung@mediatek.com>
    CR-Id: ALPS05158630
    Feature: [Android Default] USB OTG

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -81,6 +81,27 @@ int xhci_handshake(void __iomem *ptr, u32 mask, u32 done, int usec)
 	return ret;
 }
 
+int xhci_handshake_check_state(struct xhci_hcd *xhci,
+		void __iomem *ptr, u32 mask, u32 done, int usec)
+{
+	u32	result;
+
+	do {
+		result = readl_relaxed(ptr);
+		if (result == ~(u32)0)	/* card removed */
+			return -ENODEV;
+		/* host removed. Bail out */
+		if (xhci->xhc_state & XHCI_STATE_REMOVING)
+			return -ENODEV;
+		result &= mask;
+		if (result == done)
+			return 0;
+		udelay(1);
+		usec--;
+	} while (usec > 0);
+	return -ETIMEDOUT;
+}
+
 /*
  * Disable interrupts and begin the xHCI halting process.
  */
@@ -196,7 +217,7 @@ int xhci_reset(struct xhci_hcd *xhci)
 	if (xhci->quirks & XHCI_INTEL_HOST)
 		udelay(1000);
 
-	ret = xhci_handshake(&xhci->op_regs->command,
+	ret = xhci_handshake_check_state(xhci, &xhci->op_regs->command,
 			CMD_RESET, 0, 10 * 1000 * 1000);
 	if (ret)
 		return ret;
