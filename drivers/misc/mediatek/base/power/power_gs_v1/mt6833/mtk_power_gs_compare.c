72f5de925512ad690e31349379517ad33d3ae553
Claude Yen <claude.yen@mediatek.com>
2021-01-29 02:22:07 +0800

    [ALPS05253378] power_gs: mt6833: add dcm && cg dump
    add dcm and cg golden setting dump
    
    MTK-Commit-Id: 205cddfb78683351e05ace8a9e789866d53838b0
    
    Change-Id: I0ca2b87ef7c66f6a4fe7439f90669d28f9f0b144
    Signed-off-by: Claude Yen <claude.yen@mediatek.com>
    CR-Id: ALPS05253378
    Feature: Power Management

--- drivers/misc/mediatek/base/power/power_gs_v1/mt6833/mtk_power_gs_compare.c
+++ drivers/misc/mediatek/base/power/power_gs_v1/mt6833/mtk_power_gs_compare.c
@@ -16,7 +16,27 @@
 #include "mt-plat/mtk_rtc.h"
 extern void dump_pmic(int pmic_num, const char *scenario,
 			const unsigned int *pmic_gs, unsigned int pmic_gs_len);
-void mt_power_gs_table_init(void){}
+void mt_power_gs_table_init(void)
+{
+	mt_power_gs_base_remap_init("Suspend ", "CG  ",
+				AP_CG_Golden_Setting_tcl_gs_suspend,
+				AP_CG_Golden_Setting_tcl_gs_suspend_len);
+	mt_power_gs_base_remap_init("Suspend ", "DCM ",
+				AP_DCM_Golden_Setting_tcl_gs_suspend,
+				AP_DCM_Golden_Setting_tcl_gs_suspend_len);
+	mt_power_gs_base_remap_init("DPIdle  ", "CG  ",
+				AP_CG_Golden_Setting_tcl_gs_dpidle,
+				AP_CG_Golden_Setting_tcl_gs_dpidle_len);
+	mt_power_gs_base_remap_init("DPIdle  ", "DCM ",
+				AP_DCM_Golden_Setting_tcl_gs_dpidle,
+				AP_DCM_Golden_Setting_tcl_gs_dpidle_len);
+	mt_power_gs_base_remap_init("SODI    ", "CG  ",
+				AP_CG_Golden_Setting_tcl_gs_sodi,
+				AP_CG_Golden_Setting_tcl_gs_sodi_len);
+	mt_power_gs_base_remap_init("SODI    ", "DCM ",
+				AP_DCM_Golden_Setting_tcl_gs_sodi,
+				AP_DCM_Golden_Setting_tcl_gs_sodi_len);
+}
 
 void mt_power_gs_suspend_compare(unsigned int dump_flag)
 {
@@ -32,6 +52,16 @@ void mt_power_gs_suspend_compare(unsigned int dump_flag)
 		dump_pmic(1, "Suspend",
 			AP_PMIC_REG_MT6315_gs_suspend_32kless,
 			AP_PMIC_REG_MT6315_gs_suspend_32kless_len);
+	}
+	if (dump_flag & GS_CG) {
+		mt_power_gs_compare("Suspend ", "CG  ",
+				AP_CG_Golden_Setting_tcl_gs_suspend,
+				AP_CG_Golden_Setting_tcl_gs_suspend_len);
+	}
+	if (dump_flag & GS_DCM) {
+		mt_power_gs_compare("Suspend ", "DCM ",
+				AP_DCM_Golden_Setting_tcl_gs_suspend,
+				AP_DCM_Golden_Setting_tcl_gs_suspend_len);
 	}
 
 	mt_power_gs_sp_dump();
@@ -54,6 +84,18 @@ void mt_power_gs_dpidle_compare(unsigned int dump_flag)
 			AP_PMIC_REG_MT6315_gs_deepidle___lp_mp3_32kless_len);
 	}
 
+	if (dump_flag & GS_CG) {
+		mt_power_gs_compare("DPIdle  ", "CG  ",
+				AP_CG_Golden_Setting_tcl_gs_dpidle,
+				AP_CG_Golden_Setting_tcl_gs_dpidle_len);
+	}
+
+	if (dump_flag & GS_DCM) {
+		mt_power_gs_compare("DPIdle  ", "DCM ",
+				AP_DCM_Golden_Setting_tcl_gs_dpidle,
+				AP_DCM_Golden_Setting_tcl_gs_dpidle_len);
+	}
+
 	mt_power_gs_sp_dump();
 }
 
@@ -72,6 +114,18 @@ void mt_power_gs_sodi_compare(unsigned int dump_flag)
 		dump_pmic(1, "SODI	  ",
 			AP_PMIC_REG_MT6315_gs_sodi3p0_32kless,
 			AP_PMIC_REG_MT6315_gs_sodi3p0_32kless_len);
+	}
+
+	if (dump_flag & GS_CG) {
+		mt_power_gs_compare("SODI    ", "CG  ",
+				AP_CG_Golden_Setting_tcl_gs_sodi,
+				AP_CG_Golden_Setting_tcl_gs_sodi_len);
+	}
+
+	if (dump_flag & GS_DCM) {
+		mt_power_gs_compare("SODI    ", "DCM ",
+				AP_DCM_Golden_Setting_tcl_gs_sodi,
+				AP_DCM_Golden_Setting_tcl_gs_sodi_len);
 	}
 
 	mt_power_gs_sp_dump();
