9ad6ec78b031b4f6d7e87547dd8bbb7d78ff2df9
Ruijie Sun <ruijie.sun@mediatek.com>
2021-01-29 00:58:04 +0800

    [ALPS04778351] DISP: fix coverity 10356094
    [Detail]: fix coverity 10356094
    ext_disp_is_video_mode unchecked return value
    
    MTK-Commit-Id: 43326b5d2eeb96b1903c81234fa7e78a35f2591e
    
    Change-Id: Ie52dd5cebb8c736f595595cddddc609e2b7e0143
    Signed-off-by: Ruijie Sun <ruijie.sun@mediatek.com>
    CR-Id: ALPS04778351
    Feature: Display Driver (DSI Interface)

--- drivers/misc/mediatek/ext_disp/mt6768/external_display.c
+++ drivers/misc/mediatek/ext_disp/mt6768/external_display.c
@@ -947,8 +947,9 @@ static int ext_disp_init_hdmi(unsigned int session)
 	pgc->session = session;
 
 	EXTDINFO("ext_disp display START cmdq trigger loop finished\n");
-
-	dpmgr_path_set_video_mode(pgc->dpmgr_handle, ext_disp_is_video_mode());
+	if (ext_disp_is_video_mode() != -1)
+		dpmgr_path_set_video_mode(pgc->dpmgr_handle,
+			ext_disp_is_video_mode());
 	dpmgr_path_init(pgc->dpmgr_handle, CMDQ_DISABLE);
 	dpmgr_path_reset(pgc->dpmgr_handle, CMDQ_DISABLE);
 
