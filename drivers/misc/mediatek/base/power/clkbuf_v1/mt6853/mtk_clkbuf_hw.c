c793ec7b4ca2f460d996829a1480e7b0803d6bbb
RenTing Wang <ren-ting.wang@mediatek.com>
2021-01-29 02:19:49 +0800

    [ALPS05277404] clock buffer: enable bring up
    [Detail]
    enable bring-up macro for 6893/6833.
    
    MTK-Commit-Id: cefa08417b16422684df75a4dcd9d832354c59b5
    
    Change-Id: Ia54400337669d26552d955508f7cead08a3883bc
    Signed-off-by: RenTing Wang <ren-ting.wang@mediatek.com>
    CR-Id: ALPS05277404
    Feature: [Module]Clock Buffer

--- drivers/misc/mediatek/base/power/clkbuf_v1/mt6853/mtk_clkbuf_hw.c
+++ drivers/misc/mediatek/base/power/clkbuf_v1/mt6853/mtk_clkbuf_hw.c
@@ -131,7 +131,9 @@ static void __iomem *spm_base;
 #define XO_BUF7_BBLPM_EN_MASK	(0x1 << 7)
 
 /* TODO: marked this after driver is ready */
-/* #define CLKBUF_BRINGUP */
+#if defined(CONFIG_MACH_MT6833)
+#define CLKBUF_BRINGUP
+#endif /* defined(CONFIG_MACH_MT6833) */
 /* #define CLKBUF_CONN_SUPPORT_CTRL_FROM_I1 */
 
 #define BUF_MAN_M				0
--- drivers/misc/mediatek/base/power/clkbuf_v1/mt6885/mtk_clkbuf_hw.c
+++ drivers/misc/mediatek/base/power/clkbuf_v1/mt6885/mtk_clkbuf_hw.c
@@ -106,7 +106,9 @@ static void __iomem *pmif_spmi_base;
 #define XO_BUF7_BBLPM_EN_MASK	(0x1 << 7)
 
 /* TODO: marked this after driver is ready */
-/* #define CLKBUF_BRINGUP */
+#if defined(CONFIG_MACH_MT6893)
+#define CLKBUF_BRINGUP
+#endif /* defined(CONFIG_MACH_MT6893) */
 /* #define CLKBUF_CONN_SUPPORT_CTRL_FROM_I1 */
 
 #define BUF_MAN_M				0
