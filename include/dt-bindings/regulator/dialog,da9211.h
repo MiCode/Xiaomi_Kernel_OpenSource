306a122e2f8da6a284a31e7c5624dc6a6c10d932
heaven.zhang <Heaven.Zhang@mediatek.com>
2021-01-29 02:32:46 +0800

    [ALPS05328360] mt8173: add dt-bindings file
    [Detail] add dt-bindings file
    
    MTK-Commit-Id: c15e078537d195293f5c628202fe2596f2787cfb
    
    Change-Id: I6e9984a6076d630a2bc7501c9ccb54f37f994f62
    Signed-off-by: heaven.zhang <Heaven.Zhang@mediatek.com>
    CR-Id: ALPS05328360
    Feature: [Android Default] Little Kernel

---
+++ include/dt-bindings/regulator/dialog,da9211.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0
+ *
+ * Copyright (c) 2018 MediaTek Inc.
+ */
+
+#ifndef __DIALOG_REGULATOR_H
+#define __DIALOG_REGULATOR_H
+
+#define DA9211_BUCK_MODE_SLEEP	1
+#define DA9211_BUCK_MODE_SYNC	2
+#define DA9211_BUCK_MODE_AUTO	3
+
+#endif
