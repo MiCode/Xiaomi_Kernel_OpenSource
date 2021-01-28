366fe6bb4e3918cd291914e2df015bb38ef2742b
mark hung <mark.hung@mediatek.com>
2021-01-29 00:45:30 +0800

    [ALPS04630860] mdla: phase in apusys/mdla 1st drivers
    [Detail]
    1.phase in apusys/mdla 1st drivers,
    Support deep neural network calculation using
    hardware accelerator. Customers and third party
    could use MDLA to develop feature and algorithm
    by themselves.
    
    MTK-Commit-Id: 3048840564077f7bd70c9baf8bad1125f7fa43d6
    
    Change-Id: Ifbcd7425727a14da923fbc2702407e8e5e3c6a6b
    Signed-off-by: Mark Hung <mark.hung@mediatek.com>
    Feature: MDLA Platform
    CR-Id: ALPS04630860

---
+++ drivers/misc/mediatek/apusys/mdla/mt6885/mdla_plat_api.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2018 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _MDLA_PLAT_API_H_
+#define _MDLA_PLAT_API_H_
+#include <linux/interrupt.h>
+#include <linux/device.h>
+void mdla_reset(int core, int res);
+int mdla_process_command(int core_id, struct command_entry *ce);
+int hw_e1_timeout_detect(int core_id);
+int mdla_dts_map(struct platform_device *pdev);
+irqreturn_t mdla_interrupt(u32 mdlaid);
+
+
+#ifdef __APUSYS_PREEMPTION__
+irqreturn_t mdla_scheduler(unsigned int core_id);
+#endif
+#endif
