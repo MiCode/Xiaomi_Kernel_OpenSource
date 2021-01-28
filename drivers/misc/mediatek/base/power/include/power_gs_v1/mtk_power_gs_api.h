cb1af41b3c095afe7d7c9e89b45449056197cef0
Juju Sung <juju.sung@mediatek.com>
2021-01-28 18:24:51 +0800

    [ALPS05014853] Lowpower: mt6761 power_gs porting
    [Detail]
    add mt6765/61 platform power_gs driver
    
    MTK-Commit-Id: b6fb1d5d8758366b223c2bb50df21bde3a156c46
    
    Change-Id: Icd8358e4d3e38ee67ae6702b0da21c063eca117c
    Signed-off-by: Juju Sung <juju.sung@mediatek.com>
    CR-Id: ALPS05014853
    Feature: [Module]Kernel Maintenance

---
+++ drivers/misc/mediatek/base/power/include/power_gs_v1/mtk_power_gs_api.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2016 MediaTek Inc.
+ */
+
+#ifndef MTK_POWER_GS_API_H
+#define MTK_POWER_GS_API_H
+
+#include "mtk_power_gs_internal.h"
+#include "mtk_power_gs.h"
+
+#endif
---
+++ drivers/misc/mediatek/base/power/include/power_gs_v1/mtk_power_gs_internal.h
@@ -0,0 +1,51 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2016 MediaTek Inc.
+ */
+
+#ifndef MTK_POWER_GS_INTERNAL_H
+#define MTK_POWER_GS_INTERNAL_H
+
+extern void mt_power_gs_dump_suspend(void);
+extern void mt_power_gs_dump_dpidle(void);
+extern void mt_power_gs_dump_sodi3(void);
+extern void mt_power_gs_t_dump_suspend(int count, ...);
+extern void mt_power_gs_t_dump_dpidle(int count, ...);
+extern void mt_power_gs_t_dump_sodi3(int count, ...);
+extern void mt_power_gs_f_dump_suspend(unsigned int dump_flag);
+extern void mt_power_gs_f_dump_dpidle(unsigned int dump_flag);
+extern void mt_power_gs_f_dump_sodi3(unsigned int dump_flag);
+
+enum gs_flag {
+	GS_PMIC = (0x1 << 0),
+	GS_CG   = (0x1 << 1),
+	GS_DCM  = (0x1 << 2),
+	/* GS_ALL will need to be modified, if the gs_dump_flag is changed */
+	GS_ALL  = (GS_PMIC | GS_CG | GS_DCM),
+};
+
+#define GS_COUNT_PARMS2(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _, ...) _
+#define GS_COUNT_PARMS(...) \
+	GS_COUNT_PARMS2(__VA_ARGS__, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+#define GS_TRANS_PARMS(...) \
+	GS_COUNT_PARMS2(__VA_ARGS__, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+
+#define mt_power_gs_dump_suspend(...) \
+	mt_power_gs_t_dump_suspend( \
+		GS_TRANS_PARMS( \
+			GS_COUNT_PARMS(__VA_ARGS__), ##__VA_ARGS__ \
+		), ##__VA_ARGS__)
+
+#define mt_power_gs_dump_dpidle(...) \
+	mt_power_gs_t_dump_dpidle( \
+		GS_TRANS_PARMS( \
+			GS_COUNT_PARMS(__VA_ARGS__), ##__VA_ARGS__ \
+		), ##__VA_ARGS__)
+
+#define mt_power_gs_dump_sodi3(...) \
+	mt_power_gs_t_dump_sodi3( \
+		GS_TRANS_PARMS( \
+			GS_COUNT_PARMS(__VA_ARGS__), ##__VA_ARGS__ \
+		), ##__VA_ARGS__)
+
+#endif
