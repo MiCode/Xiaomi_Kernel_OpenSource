4a1fbd8763152bda3c3aac22ec1be7795524ffe7
Fred Cheng <fred.cheng@mediatek.com>
2021-01-29 02:05:24 +0800

    [ALPS05076069] SCP: fix Coverity issue
    fix an address that could be at negative offset of an array.
    
    MTK-Commit-Id: 1ff4deca010f7adf450b8249a7238259d05e260c
    
    Change-Id: I590ff0cbcdf5d68206f55cff4e2336cd68f6459b
    Signed-off-by: Fred Cheng <fred.cheng@mediatek.com>
    CR-Id: ALPS05076069
    Feature: TinySys

--- drivers/misc/mediatek/scp/cm4/scp_dvfs.c
+++ drivers/misc/mediatek/scp/cm4/scp_dvfs.c
@@ -626,12 +626,16 @@ static ssize_t mt_scp_dvfs_sleep_proc_write(
 {
 	char desc[64];
 	unsigned int val = 0;
-	int len = 0;
+	unsigned int len = 0;
 	int ret = 0;
+
+	if (count <= 0)
+		return 0;
 
 	len = (count < (sizeof(desc) - 1)) ? count : (sizeof(desc) - 1);
 	if (copy_from_user(desc, buffer, len))
 		return 0;
+
 	desc[len] = '\0';
 
 	if (kstrtouint(desc, 10, &val) == 0) {
@@ -698,16 +702,18 @@ static ssize_t mt_scp_dvfs_ctrl_proc_write(
 					loff_t *data)
 {
 	char desc[64], cmd[32];
-	int len = 0;
+	unsigned int len = 0;
 	int dvfs_opp;
 	int freq;
 	int n;
 
-	len = (count < (sizeof(desc) - 1)) ? count : (sizeof(desc) - 1);
-	if (len < 0)
+	if (count <= 0)
 		return 0;
+
+	len = (count < (sizeof(desc) - 1)) ? count : (sizeof(desc) - 1);
 	if (copy_from_user(desc, buffer, len))
 		return 0;
+
 	desc[len] = '\0';
 
 	n = sscanf(desc, "%31s %d", cmd, &dvfs_opp);
