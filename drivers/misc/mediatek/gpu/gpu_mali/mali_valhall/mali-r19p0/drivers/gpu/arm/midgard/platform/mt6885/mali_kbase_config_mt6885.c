ac21639843b90994ef39a11c10cb0342f33c6e99
max.yu <max.yu@mediatek.com>
2021-01-29 00:47:20 +0800

    [ALPS04668661] GPU: DDK and pltform driver handshake flow
    [Detail]
    DDK and pltform driver handshake flow
    
    MTK-Commit-Id: a970474f8a27a58932f503ed7dec838d0661e20a
    
    Change-Id: I9593f4021ffff7368599415027e1e6e65c99752f
    Signed-off-by: max.yu <max.yu@mediatek.com>
    CR-Id: ALPS04668661
    Feature: OpenGL|ES

--- drivers/misc/mediatek/gpu/gpu_mali/mali_valhall/mali-r19p0/drivers/gpu/arm/midgard/platform/mt6885/mali_kbase_config_mt6885.c
+++ drivers/misc/mediatek/gpu/gpu_mali/mali_valhall/mali-r19p0/drivers/gpu/arm/midgard/platform/mt6885/mali_kbase_config_mt6885.c
@@ -20,8 +20,6 @@
  *
  */
 
-
-
 #include <linux/ioport.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
@@ -41,66 +39,47 @@
 #include <mtk_gpu_power_sspm_ipi.h>
 #endif
 
-
 #define MALI_TAG				"[GPU/MALI]"
 #define mali_pr_info(fmt, args...)		pr_info(MALI_TAG"[INFO]"fmt, ##args)
 #define mali_pr_debug(fmt, args...)		pr_debug(MALI_TAG"[DEBUG]"fmt, ##args)
 
 DEFINE_MUTEX(g_mfg_lock);
 
-static void *g_MFG_base;
+//FIXME
 static int g_curFreqID;
 static int g_is_suspend;
 
-static void __mtk_check_MFG_bus_idle(void)
+enum gpu_dvfs_status_step {
+	GPU_DVFS_STATUS_STEP_1 = 0x1,
+	GPU_DVFS_STATUS_STEP_2 = 0x2,
+	GPU_DVFS_STATUS_STEP_3 = 0x3,
+	GPU_DVFS_STATUS_STEP_4 = 0x4,
+	GPU_DVFS_STATUS_STEP_5 = 0x5,
+	GPU_DVFS_STATUS_STEP_6 = 0x6,
+	GPU_DVFS_STATUS_STEP_7 = 0x7,
+	GPU_DVFS_STATUS_STEP_8 = 0x8,
+	GPU_DVFS_STATUS_STEP_9 = 0x9,
+	GPU_DVFS_STATUS_STEP_A = 0xA,
+	GPU_DVFS_STATUS_STEP_B = 0xB,
+	GPU_DVFS_STATUS_STEP_C = 0xC,
+	GPU_DVFS_STATUS_STEP_D = 0xD,
+	GPU_DVFS_STATUS_STEP_E = 0xE,
+	GPU_DVFS_STATUS_STEP_F = 0xF,
+};
+
+static inline void gpu_dvfs_status_footprint(enum gpu_dvfs_status_step step)
 {
-	u32 val;
-
-	/* MFG_QCHANNEL_CON (0x13fb_f0b4) bit [1:0] = 0x1 */
-	writel(0x00000001, g_MFG_base + 0xb4);
-	mali_pr_debug("@%s: 0x13fb_f0b4 val = 0x%x\n", __func__, readl(g_MFG_base + 0xb4));
-
-	/* set register MFG_DEBUG_SEL (0x13fb_f170) bit [7:0] = 0x03 */
-	writel(0x00000003, g_MFG_base + 0x170);
-	mali_pr_debug("@%s: 0x13fb_f170 val = 0x%x\n", __func__, readl(g_MFG_base + 0x170));
-
-	/* polling register MFG_DEBUG_TOP (0x13fb_f178) bit 2 = 0x1 */
-	/* => 1 for bus idle, 0 for bus non-idle */
-	do {
-		val = readl(g_MFG_base + 0x178);
-		mali_pr_debug("@%s: 0x13fb_f178 val = 0x%x\n", __func__, val);
-	} while ((val & 0x4) != 0x4);
+#ifdef CONFIG_MTK_RAM_CONSOLE
+	aee_rr_rec_gpu_dvfs_status(step |
+				(aee_rr_curr_gpu_dvfs_status() & 0xF0));
+#endif
 }
 
-static void __mtk_set_MFG_HW_CG(void)
+static inline void gpu_dvfs_status_reset_footprint(void)
 {
-	u32 val;
-
-	/* [F] & [G] adopt default setting because PD implementation issue */
-	/* it should be free run */
-
-	/* [K] MFG_GLOBAL_CON: 0x13fb_f0b0 bit [8] = 0x0 */
-	/* [D] MFG_GLOBAL_CON: 0x13fb_f0b0 bit [10] = 0x0 */
-	mali_pr_debug("@%s: 0x13fb_f0b0 = 0x%x\n", __func__, readl(g_MFG_base + 0xb0));
-	val = readl(g_MFG_base + 0xb0);
-	val &= ~(1UL << 8);
-	val &= ~(1UL << 10);
-	writel(val, g_MFG_base + 0xb0);
-	mali_pr_debug("@%s: 0x13fb_f0b0 = 0x%x\n", __func__, readl(g_MFG_base + 0xb0));
-
-	/* [H] MFG_ASYNC_CON: 0x13fb_f020 bit [23] = 0x1 */
-	/* [I] MFG_ASYNC_CON: 0x13fb_f020 bit [25] = 0x1 */
-	mali_pr_debug("@%s: 0x13fb_f020 = 0x%x\n", __func__, readl(g_MFG_base + 0x20));
-	val = readl(g_MFG_base + 0x20);
-	val |= (1UL << 23);
-	val |= (1UL << 25);
-	writel(val, g_MFG_base + 0x20);
-	mali_pr_debug("@%s: 0x13fb_f020 = 0x%x\n", __func__, readl(g_MFG_base + 0x20));
-
-	/* [J] MFG_ASYNC_CON_1: 0x13fb_f024 bit [0] = 0x1 */
-	mali_pr_debug("@%s: 0x13fb_f024 = 0x%x\n", __func__, readl(g_MFG_base + 0x24));
-	writel(readl(g_MFG_base + 0x24) | (1UL), g_MFG_base + 0x24);
-	mali_pr_debug("@%s: 0x13fb_f024 = 0x%x\n", __func__, readl(g_MFG_base + 0x24));
+#ifdef CONFIG_MTK_RAM_CONSOLE
+	aee_rr_rec_gpu_dvfs_status(0);
+#endif
 }
 
 static int pm_callback_power_on_nolock(struct kbase_device *kbdev)
@@ -110,65 +89,46 @@ static int pm_callback_power_on_nolock(struct kbase_device *kbdev)
 	return 1;
 #endif
 
+	if (!mt_gpufreq_power_ctl_en()) {
+		mtk_set_vgpu_power_on_flag(MTK_VGPU_POWER_ON);
+		ged_dvfs_gpu_clock_switch_notify(1);
+		return 1;
+	}
+
 	if (mtk_get_vgpu_power_on_flag() == MTK_VGPU_POWER_ON)
 		return 0;
 
 	mali_pr_debug("@%s: power on ...\n", __func__);
 
-#ifdef MT_GPUFREQ_SRAM_DEBUG
-	aee_rr_rec_gpu_dvfs_status(
-			0x1 | (aee_rr_curr_gpu_dvfs_status() & 0xF0));
-#endif
+	gpu_dvfs_status_footprint(GPU_DVFS_STATUS_STEP_1);
 
 	if (g_is_suspend == 1) {
 		mali_pr_info("@%s: discard powering on since GPU is suspended\n", __func__);
 		return 0;
 	}
 
-	/* power on BUCK / MTCMOS / SWCG(BG3D) */
-	mt_gpufreq_power_control(true, true, true, true);
+	/* on,off/ SWCG(BG3D)/ MTCMOS/ BUCK */
+	mt_gpufreq_power_control(POWER_ON, CG_ON, MTCMOS_ON, BUCK_ON);
 
-#ifdef MT_GPUFREQ_SRAM_DEBUG
-	aee_rr_rec_gpu_dvfs_status(
-			0x2 | (aee_rr_curr_gpu_dvfs_status() & 0xF0));
-#endif
+	gpu_dvfs_status_footprint(GPU_DVFS_STATUS_STEP_2);
 
-	/* set HWCG(DCM) */
-	__mtk_set_MFG_HW_CG();
-
-#ifdef MT_GPUFREQ_SRAM_DEBUG
-	aee_rr_rec_gpu_dvfs_status(
-			0x3 | (aee_rr_curr_gpu_dvfs_status() & 0xF0));
-#endif
-
-	/* write 1 into 0x13fb_f130 bit 0 to enable timestamp register */
-	/* timestamp will be used by clGetEventProfilingInfo*/
-	writel(0x00000001, g_MFG_base + 0x130);
+	mt_gpufreq_set_timestamp();
 
 	/* set a flag to enable GPU DVFS */
 	mtk_set_vgpu_power_on_flag(MTK_VGPU_POWER_ON);
 
-#ifdef MT_GPUFREQ_SRAM_DEBUG
-	aee_rr_rec_gpu_dvfs_status(
-			0x4 | (aee_rr_curr_gpu_dvfs_status() & 0xF0));
-#endif
+	gpu_dvfs_status_footprint(GPU_DVFS_STATUS_STEP_3);
 
 	/* resume frequency */
 	mtk_set_mt_gpufreq_target(g_curFreqID);
 
-#ifdef MT_GPUFREQ_SRAM_DEBUG
-	aee_rr_rec_gpu_dvfs_status(
-			0x5 | (aee_rr_curr_gpu_dvfs_status() & 0xF0));
-#endif
+	gpu_dvfs_status_footprint(GPU_DVFS_STATUS_STEP_4);
 
 #ifdef ENABLE_COMMON_DVFS
 	ged_dvfs_gpu_clock_switch_notify(1);
 #endif
 
-#ifdef MT_GPUFREQ_SRAM_DEBUG
-	aee_rr_rec_gpu_dvfs_status(
-			0x6 | (aee_rr_curr_gpu_dvfs_status() & 0xF0));
-#endif
+	gpu_dvfs_status_footprint(GPU_DVFS_STATUS_STEP_5);
 
 	return 1;
 }
@@ -179,56 +139,41 @@ static void pm_callback_power_off_nolock(struct kbase_device *kbdev)
 	return ;
 #endif
 
+	if (!mt_gpufreq_power_ctl_en())
+		return ;
+
 	if (mtk_get_vgpu_power_on_flag() == MTK_VGPU_POWER_OFF)
 		return;
 
 	mali_pr_debug("@%s: power off ...\n", __func__);
 
-#ifdef MT_GPUFREQ_SRAM_DEBUG
-	aee_rr_rec_gpu_dvfs_status(
-			0x7 | (aee_rr_curr_gpu_dvfs_status() & 0xF0));
-#endif
+	gpu_dvfs_status_footprint(GPU_DVFS_STATUS_STEP_6);
 
 #ifdef ENABLE_COMMON_DVFS
 	ged_dvfs_gpu_clock_switch_notify(0);
 #endif
 
-#ifdef MT_GPUFREQ_SRAM_DEBUG
-	aee_rr_rec_gpu_dvfs_status(
-			0x8 | (aee_rr_curr_gpu_dvfs_status() & 0xF0));
-#endif
+	gpu_dvfs_status_footprint(GPU_DVFS_STATUS_STEP_7);
 
 	/* set a flag to disable GPU DVFS */
 	mtk_set_vgpu_power_on_flag(MTK_VGPU_POWER_OFF);
 
-#ifdef MT_GPUFREQ_SRAM_DEBUG
-	aee_rr_rec_gpu_dvfs_status(
-			0x9 | (aee_rr_curr_gpu_dvfs_status() & 0xF0));
-#endif
+	gpu_dvfs_status_footprint(GPU_DVFS_STATUS_STEP_8);
 
 	/* suspend frequency */
 	g_curFreqID = mtk_get_ged_dvfs_last_commit_idx();
 
-#ifdef MT_GPUFREQ_SRAM_DEBUG
-	aee_rr_rec_gpu_dvfs_status(
-			0xA | (aee_rr_curr_gpu_dvfs_status() & 0xF0));
-#endif
+	gpu_dvfs_status_footprint(GPU_DVFS_STATUS_STEP_9);
 
 	/* check MFG bus if idle */
-	__mtk_check_MFG_bus_idle();
+	mt_gpufreq_check_bus_idle();
 
-#ifdef MT_GPUFREQ_SRAM_DEBUG
-	aee_rr_rec_gpu_dvfs_status(
-			0xB | (aee_rr_curr_gpu_dvfs_status() & 0xF0));
-#endif
+	gpu_dvfs_status_footprint(GPU_DVFS_STATUS_STEP_A);
 
-	/* power off SWCG(BG3D) / MTCMOS / BUCK */
-	mt_gpufreq_power_control(false, false, false, false);
+	/* on,off/ SWCG(BG3D)/ MTCMOS/ BUCK */
+	mt_gpufreq_power_control(POWER_OFF, CG_OFF, MTCMOS_OFF, BUCK_OFF);
 
-#ifdef MT_GPUFREQ_SRAM_DEBUG
-	aee_rr_rec_gpu_dvfs_status(
-			0xC | (aee_rr_curr_gpu_dvfs_status() & 0xF0));
-#endif
+	gpu_dvfs_status_footprint(GPU_DVFS_STATUS_STEP_B);
 }
 
 static int pm_callback_power_on(struct kbase_device *kbdev)
@@ -266,9 +211,7 @@ static void pm_callback_power_suspend(struct kbase_device *kbdev)
 	g_is_suspend = 1;
 	mali_pr_info("@%s: gpu_suspend\n", __func__);
 
-#ifdef MT_GPUFREQ_SRAM_DEBUG
-	aee_rr_rec_gpu_dvfs_vgpu(0x01);
-#endif
+	gpu_dvfs_status_footprint(GPU_DVFS_STATUS_STEP_E);
 
 	mutex_unlock(&g_mfg_lock);
 }
@@ -280,9 +223,7 @@ static void pm_callback_power_resume(struct kbase_device *kbdev)
 	g_is_suspend = 0;
 	mali_pr_info("@%s: gpu_resume\n", __func__);
 
-#ifdef MT_GPUFREQ_SRAM_DEBUG
-	aee_rr_rec_gpu_dvfs_vgpu(0x02);
-#endif
+	gpu_dvfs_status_footprint(GPU_DVFS_STATUS_STEP_F);
 
 	mutex_unlock(&g_mfg_lock);
 }
@@ -317,23 +258,6 @@ struct kbase_platform_config *kbase_get_platform_config(void)
 	return &versatile_platform_config;
 }
 
-/**
- * MTK internal ioremap function
- */
-static void *__mtk_of_ioremap(const char *node_name, int idx)
-{
-	struct device_node *node;
-
-	node = of_find_compatible_node(NULL, NULL, node_name);
-
-	if (node)
-		return of_iomap(node, idx);
-
-	mali_pr_info("@%s: cannot find [%s] of_node\n", __func__, node_name);
-
-	return NULL;
-}
-
 int kbase_platform_early_init(void)
 {
 	/* Nothing needed at this stage */
@@ -348,12 +272,9 @@ int mtk_platform_init(struct platform_device *pdev, struct kbase_device *kbdev)
 		return -1;
 	}
 
-	g_MFG_base = __mtk_of_ioremap("mediatek,mfgcfg", 0);
-	if (g_MFG_base == NULL) {
-		mali_pr_info("@%s: fail to remap MGFCFG register\n", __func__);
-		return -1;
-	}
+	gpu_dvfs_status_reset_footprint();
 
+	//FIXME
 	g_is_suspend = -1;
 
 	mali_pr_info("@%s: initialize successfully\n", __func__);
