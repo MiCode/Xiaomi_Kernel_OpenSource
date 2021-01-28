39ab65cdb9352940fcd50af378810b1215767143
mtk71054 <haoran.dong@mediatek.com>
2021-01-28 21:53:07 +0800

    [ALPS05116427] DVFS: bypass get level
    [Detail]
    bypass get cpu level temporarily to fix bootup fail
    
    [Solution]
    bypass get cpu level temporarily
    
    MTK-Commit-Id: ab8569780ae91f18ba082c6be122eb07d9792c37
    
    Change-Id: I42305df7a9101dde9ed8abdc68ff4ef0de061b69
    Signed-off-by: mtk71054 <haoran.dong@mediatek.com>
    CR-Id: ALPS05116427
    Feature: DVFS (Dynamic Voltage and Frequency Scaling)

--- drivers/misc/mediatek/base/power/cpufreq_v1/src/mach/mt6779/mtk_cpufreq_platform.c
+++ drivers/misc/mediatek/base/power/cpufreq_v1/src/mach/mt6779/mtk_cpufreq_platform.c
@@ -616,8 +616,11 @@ int mt_cpufreq_dts_map(void)
 unsigned int _mt_cpufreq_get_cpu_level(void)
 {
 	unsigned int lv = CPU_LEVEL_0;
-	int val = 0;
 
+#if defined(CONFIG_MACH_MT6779)
+	return lv;
+#else
+	int val = 0;
 	unsigned int efuse_seg;
 	struct platform_device *pdev;
 	struct device_node *node;
@@ -656,6 +659,7 @@ unsigned int _mt_cpufreq_get_cpu_level(void)
 	tag_pr_info("%d, %d, (%d, %d) efuse_val = 0x%x\n",
 		lv, turbo_flag, UP_SRATE, DOWN_SRATE, val);
 	return lv;
+#endif
 }
 
 unsigned int cpufreq_get_nr_clusters(void)
