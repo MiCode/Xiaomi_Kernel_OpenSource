1ae5b374e6c4817b97e20e57250a505802c44947
kuan-hsin.lee <kuan-hsin.lee@mediatek.com>
2021-01-29 02:18:47 +0800

    [ALPS05262916] FHCTL: fix patch for wrong hopping
    clk + FHCTL hopping together leads hopping twice, so we need to remove
    duplicate statement
    
    MTK-Commit-Id: 41fa53119ce9b6134333648e156d3607cf56b352
    
    Change-Id: I8af39bbd20776854b8799e15da37da483f9ec5ac
    Signed-off-by: kuan-hsin.lee <kuan-hsin.lee@mediatek.com>
    CR-Id: ALPS05262916
    Feature: Device Frequency Scaling(devfreq)
    (cherry picked from commit 4ee317ad638cb7c484dcd7de058b8df0dada288f)

--- drivers/clk/mediatek/clk-pll.c
+++ drivers/clk/mediatek/clk-pll.c
@@ -215,7 +215,6 @@ static int mtk_pll_set_rate(struct clk_hw *hw, unsigned long rate,
 	mtk_pll_calc_values(pll, &pcw, &postdiv, rate, parent_rate);
 	if (!mtk_fh_set_rate || !mtk_fh_set_rate(pll->data->id, pcw, postdiv))
 		mtk_pll_set_rate_regs(pll, pcw, postdiv);
-	mtk_pll_set_rate_regs(pll, pcw, postdiv);
 
 	return 0;
 }
