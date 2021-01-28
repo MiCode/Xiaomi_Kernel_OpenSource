c29427fdf82a49c870e4bb30ca5c1c5223d85269
Stanley Chu <stanley.chu@mediatek.com>
2021-01-29 00:59:11 +0800

    [ALPS04843386] ext4: hie: allow move extents ioctl for inline encryption
    The limitation of EXT4_IOC_MOVE_EXT ioctl for handling encrypted
    files is only applicable for SW encryption but not for inline
    encryption. Hence, enable this ioctl for inline encryption so that
    tools such as e4defrag can make use of it on such devices.
    
    MTK-Commit-Id: 1c5d23643ce90258b81651486f79b6f96cab86a8
    
    Change-Id: Ic7444fbdc9b83a367b9de5e74ed548b8441ad553
    Signed-off-by: Stanley Chu <stanley.chu@mediatek.com>
    CR-Id: ALPS04843386
    Feature: Multi-Storage
    (cherry picked from commit 87e7441337db2330440ee776c11b3c741817ef4e)

--- fs/ext4/move_extent.c
+++ fs/ext4/move_extent.c
@@ -607,11 +607,18 @@ ext4_move_extents(struct file *o_filp, struct file *d_filp, __u64 orig_blk,
 		return -EOPNOTSUPP;
 	}
 
-	if (ext4_encrypted_inode(orig_inode) ||
-	    ext4_encrypted_inode(donor_inode)) {
-		ext4_msg(orig_inode->i_sb, KERN_ERR,
-			 "Online defrag not supported for encrypted files");
-		return -EOPNOTSUPP;
+	/*
+	 * Limitaion is only applicable for SW encryption but not for
+	 * inline encryption
+	 */
+	if (!fscrypt_is_hw_encrypt(orig_inode) ||
+	    !fscrypt_is_hw_encrypt(donor_inode)) {
+		if (ext4_encrypted_inode(orig_inode) ||
+		    ext4_encrypted_inode(donor_inode)) {
+			ext4_msg(orig_inode->i_sb, KERN_ERR,
+				 "Online defrag not supported for encrypted files");
+			return -EOPNOTSUPP;
+		}
 	}
 
 	/* Protect orig and donor inodes against a truncate */
