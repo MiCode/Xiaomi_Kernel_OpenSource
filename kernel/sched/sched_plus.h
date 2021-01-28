b5d0cf459889f32708d9ca5059546703953105ed
Jing-Ting Wu <jing-ting.wu@mediatek.com>
2021-01-28 19:12:39 +0800

    [ALPS04025356] sched: Provide a sysfs interface for EAS
    [Detail]
    Provide a sysfs interface for user to get EAS related info.
    /sys/devices/system/cpu/eas/
    
    Include:
    1. EAS+ info
    2. Scheduler switch
    
    MTK-Commit-Id: 8496325b6ac1be7cee863e2ce34bb6952cd74317
    
    Change-Id: Idc8c2f8b8526db152bab36d4986626f5b91f0be8
    CR-Id:ALPS04025356
    Feature:Energy Aware Scheduling Plus (EAS+)
    Signed-off-by: Jing-Ting Wu <jing-ting.wu@mediatek.com>
    (cherry-pick from d192908eb9d3a06a83146a2601633839b6183bde)

--- kernel/sched/sched_plus.h
+++ kernel/sched/sched_plus.h
@@ -14,3 +14,13 @@ extern void unthrottle_offline_rt_rqs(struct rq *rq);
 DECLARE_PER_CPU(struct hmp_domain *, hmp_cpu_domain);
 #include "../../drivers/misc/mediatek/base/power/include/mtk_upower.h"
 extern int l_plus_cpu;
+extern unsigned long get_cpu_util(int cpu);
+#ifdef CONFIG_SMP
+#ifdef CONFIG_ARM64
+extern unsigned long arch_scale_get_max_freq(int cpu);
+extern unsigned long arch_scale_get_min_freq(int cpu);
+#else
+static inline unsigned long arch_scale_get_max_freq(int cpu) { return 0; }
+static inline unsigned long arch_scale_get_min_freq(int cpu) { return 0; }
+#endif
+#endif
