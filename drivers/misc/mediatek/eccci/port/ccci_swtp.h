19c9bc44df4b5045c5a68a8fad2fee04d61cd780
Weiqiang Hao <weiqiang.hao@mediatek.com>
2021-01-29 02:23:18 +0800

    [ALPS05347160] ccci:change swtp feature
    1.change swtp feature
    2.fix code defect
    
    MTK-Commit-Id: eae06cb77f93cfa1dce00619e4047bc11d28e20a
    
    Change-Id: I82e135c63f687b25aab1c9cde44f581a467a991f
    Signed-off-by: Weiqiang Hao <weiqiang.hao@mediatek.com>
    CR-Id: ALPS05347160
    Feature: Modem Interface Driver

--- drivers/misc/mediatek/eccci/port/ccci_swtp.h
+++ drivers/misc/mediatek/eccci/port/ccci_swtp.h
@@ -14,27 +14,33 @@
 #ifndef __SWTP_H__
 #define __SWTP_H__
 
+/* modify MAX_PIN_NUM/DTS to support more gpio,
+ * need to follow SOP for customization.
+ */
+#define MAX_PIN_NUM 2
 #define SWTP_COMPATIBLE_DEVICE_ID "mediatek, swtp-eint"
 #define SWTP1_COMPATIBLE_DEVICE_ID "mediatek, swtp1-eint"
 
-#define SWTP_EINT_PIN_PLUG_IN        (1)
-#define SWTP_EINT_PIN_PLUG_OUT       (0)
+
+#define SWTP_EINT_PIN_PLUG_IN	(1)
+#define SWTP_EINT_PIN_PLUG_OUT	(0)
+
+#define SWTP_DO_TX_POWER	(0)
+#define SWTP_NO_TX_POWER	(1)
 
 struct swtp_t {
 	unsigned int	md_id;
-	unsigned int	irq[2];
-	unsigned int	gpiopin[2];
-	unsigned int	setdebounce[2];
-	unsigned int	eint_type[2];
-	unsigned int	curr_mode[2];
-	unsigned int	final_mode;
-	unsigned int	retry_cnt;
+	unsigned int	irq[MAX_PIN_NUM];
+	unsigned int	gpiopin[MAX_PIN_NUM];
+	unsigned int	setdebounce[MAX_PIN_NUM];
+	unsigned int	eint_type[MAX_PIN_NUM];
+	unsigned int	gpio_state[MAX_PIN_NUM];
+	int	tx_power_mode;
 	spinlock_t		spinlock;
 	struct delayed_work delayed_work;
 };
 /*****************************************************************************/
 /* External API Region called by ccci_swtp object */
 /*****************************************************************************/
-extern int ccci_md_get_state_by_id(int md_id);
 extern int swtp_init(int md_id);
 #endif				/* __SWTP_H__ */
