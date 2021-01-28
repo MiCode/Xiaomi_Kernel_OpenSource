ec08d212f58e5f5f377ea18802f4bee98232d7ed
James Hsu <james.hsu@mediatek.com>
2021-01-28 19:02:40 +0800

    [ALPS04020096] memcfg: add memmap support
    [Detail]
    Add memmap support
    
    MTK-Commit-Id: 9c268c338590f307076f56151430ccd30f480754
    
    Change-Id: I791f1fcfb63045273a07fc0726fa00e088ca7fcc
    Signed-off-by: James Hsu <james.hsu@mediatek.com>
    CR-Id: ALPS04020096
    Feature: [Module]Kernel Memory Management
    (cherry-pick from 32a96560dbff016930d841d84a4bdca6bd846f17)

--- mm/memory.c
+++ mm/memory.c
@@ -88,6 +88,11 @@
 #ifndef CONFIG_NEED_MULTIPLE_NODES
 /* use the per-pgdat data instead for discontigmem - mbligh */
 unsigned long max_mapnr;
+#ifdef CONFIG_MTK_MEMCFG
+unsigned long mem_map_size;
+EXPORT_SYMBOL(mem_map_size);
+#endif
+
 EXPORT_SYMBOL(max_mapnr);
 
 struct page *mem_map;
