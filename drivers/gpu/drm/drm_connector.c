3789d38761cbc277c798b01cb4660c83151288f5
Allen Lin <dong-hung.lin@mediatek.com>
2021-01-29 02:12:36 +0800

    [ALPS05106113] disp: mt6885 can not bring up in Android R
    [Detail]
    1.Add new element to drm_connector_enum_list[] in drm_connector.c
    2.return mtk_drm_idlemgr_init function in mtk_drm_lowpower.c
    
    MTK-Commit-Id: b3ce8ea43265d2cc9217eaf48c464c9734a349b1
    
    Change-Id: I0942aa247f1978f75111869c9f5da1db994b8ddf
    Signed-off-by: Allen Lin <dong-hung.lin@mediatek.com>
    CR-Id: ALPS05106113
    Feature: Display Driver (DSI Interface)

--- drivers/gpu/drm/drm_connector.c
+++ drivers/gpu/drm/drm_connector.c
@@ -86,6 +86,7 @@ static struct drm_conn_prop_enum_list drm_connector_enum_list[] = {
 	{ DRM_MODE_CONNECTOR_VIRTUAL, "Virtual" },
 	{ DRM_MODE_CONNECTOR_DSI, "DSI" },
 	{ DRM_MODE_CONNECTOR_DPI, "DPI" },
+	{ DRM_MODE_CONNECTOR_WRITEBACK, "Writeback" },
 };
 
 void drm_connector_ida_init(void)
