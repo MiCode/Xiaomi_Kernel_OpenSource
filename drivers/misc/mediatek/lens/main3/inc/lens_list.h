e7c869aad49ae36ceb28097215f6bde38bf6b834
bradley_kung <bradley.kung@mediatek.com>
2021-01-28 18:28:26 +0800

    [ALPS05016195] AF: add lens driver for auto focus
    porting lens driver for sub/sub2/main3 camera
    
    MTK-Commit-Id: 2244bc9110d47a8877a1c913fa62e1787c312f11
    
    Change-Id: I76ff1cd5458f6578741283b50fb94414120bdce9
    CR-Id: ALPS05016195
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: bradley_kung <bradley.kung@mediatek.com>

---
+++ drivers/misc/mediatek/lens/main3/inc/lens_list.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+
+
+#ifndef _LENS_LIST_H
+
+#define _LENS_LIST_H
+
+extern void AFRegulatorCtrl(int Stage);
+#endif
---
+++ drivers/misc/mediatek/lens/sub/inc/lens_list.h
@@ -0,0 +1,89 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+
+
+#ifndef _LENS_LIST_H
+
+#define _LENS_LIST_H
+
+#define AK7371AF_SetI2Cclient AK7371AF_SetI2Cclient_Sub
+#define AK7371AF_Ioctl AK7371AF_Ioctl_Sub
+#define AK7371AF_Release AK7371AF_Release_Sub
+#define AK7371AF_GetFileName AK7371AF_GetFileName_Sub
+extern int AK7371AF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient,
+				 spinlock_t *pAF_SpinLock, int *pAF_Opened);
+extern long AK7371AF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
+			   unsigned long a_u4Param);
+extern int AK7371AF_Release(struct inode *a_pstInode, struct file *a_pstFile);
+extern int AK7371AF_GetFileName(unsigned char *pFileName);
+
+#define BU6424AF_SetI2Cclient BU6424AF_SetI2Cclient_Sub
+#define BU6424AF_Ioctl BU6424AF_Ioctl_Sub
+#define BU6424AF_Release BU6424AF_Release_Sub
+#define BU6424AF_GetFileName BU6424AF_GetFileName_Sub
+extern int BU6424AF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient,
+				 spinlock_t *pAF_SpinLock, int *pAF_Opened);
+extern long BU6424AF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
+			   unsigned long a_u4Param);
+extern int BU6424AF_Release(struct inode *a_pstInode, struct file *a_pstFile);
+extern int BU6424AF_GetFileName(unsigned char *pFileName);
+
+#define BU6429AF_SetI2Cclient BU6429AF_SetI2Cclient_Sub
+#define BU6429AF_Ioctl BU6429AF_Ioctl_Sub
+#define BU6429AF_Release BU6429AF_Release_Sub
+#define BU6429AF_GetFileName BU6429AF_GetFileName_Sub
+extern int BU6429AF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient,
+				 spinlock_t *pAF_SpinLock, int *pAF_Opened);
+extern long BU6429AF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
+			   unsigned long a_u4Param);
+extern int BU6429AF_Release(struct inode *a_pstInode, struct file *a_pstFile);
+extern int BU6429AF_GetFileName(unsigned char *pFileName);
+
+#define DW9714AF_SetI2Cclient DW9714AF_SetI2Cclient_Sub
+#define DW9714AF_Ioctl DW9714AF_Ioctl_Sub
+#define DW9714AF_Release DW9714AF_Release_Sub
+#define DW9714AF_GetFileName DW9714AF_GetFileName_Sub
+extern int DW9714AF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient,
+				 spinlock_t *pAF_SpinLock, int *pAF_Opened);
+extern long DW9714AF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
+			   unsigned long a_u4Param);
+extern int DW9714AF_Release(struct inode *a_pstInode, struct file *a_pstFile);
+extern int DW9714AF_GetFileName(unsigned char *pFileName);
+
+#define DW9814AF_SetI2Cclient DW9814AF_SetI2Cclient_Sub
+#define DW9814AF_Ioctl DW9814AF_Ioctl_Sub
+#define DW9814AF_Release DW9814AF_Release_Sub
+extern int DW9814AF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient,
+				 spinlock_t *pAF_SpinLock, int *pAF_Opened);
+extern long DW9814AF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
+			   unsigned long a_u4Param);
+extern int DW9814AF_Release(struct inode *a_pstInode, struct file *a_pstFile);
+
+#define DW9718AF_SetI2Cclient DW9718AF_SetI2Cclient_Sub
+#define DW9718AF_Ioctl DW9718AF_Ioctl_Sub
+#define DW9718AF_Release DW9718AF_Release_Sub
+#define DW9814AF_GetFileName DW9814AF_GetFileName_Sub
+extern int DW9718AF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient,
+				 spinlock_t *pAF_SpinLock, int *pAF_Opened);
+extern long DW9718AF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
+			   unsigned long a_u4Param);
+extern int DW9718AF_Release(struct inode *a_pstInode, struct file *a_pstFile);
+extern int DW9718AF_GetFileName(unsigned char *pFileName);
+
+
+#define WV511AAF_SetI2Cclient WV511AAF_SetI2Cclient_Sub
+#define WV511AAF_Ioctl WV511AAF_Ioctl_Sub
+#define WV511AAF_Release WV511AAF_Release_Sub
+#define WV511AAF_GetFileName WV511AAF_GetFileName_Sub
+extern int WV511AAF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient,
+				 spinlock_t *pAF_SpinLock, int *pAF_Opened);
+extern long WV511AAF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command,
+			   unsigned long a_u4Param);
+extern int WV511AAF_Release(struct inode *a_pstInode, struct file *a_pstFile);
+extern int WV511AAF_GetFileName(unsigned char *pFileName);
+
+extern void AFRegulatorCtrl(int Stage);
+#endif
