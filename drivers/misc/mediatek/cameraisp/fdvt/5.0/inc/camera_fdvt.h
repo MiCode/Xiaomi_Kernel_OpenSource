df2c180f37081e6378f3501aca38b25677581890
Ryan Teng <ryan.teng@mediatek.com>
2021-01-29 00:57:15 +0800

    [ALPS04386001] FDVT: Performance Improve
    [Detail] Improve Performance of Secure Flow
    
    MTK-Commit-Id: dfa3a491825aebdb83152e701a525089dc46c4b6
    
    Change-Id: I0666ba2331f1b283a1c8cbe7fd1f21777c3088ba
    Signed-off-by: Ryan Teng <ryan.teng@mediatek.com>
    CR-Id: ALPS04386001
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/misc/mediatek/cameraisp/fdvt/5.0/inc/camera_fdvt.h
+++ drivers/misc/mediatek/cameraisp/fdvt/5.0/inc/camera_fdvt.h
@@ -134,6 +134,7 @@ struct FDVT_MetaDataToGCE {
 	unsigned int rotateDegree;
 	unsigned short featureTH;
 	unsigned short SecMemType;
+	bool isReleased;
 };
 #define FDVT_MetaDataToGCE struct FDVT_MetaDataToGCE
 
