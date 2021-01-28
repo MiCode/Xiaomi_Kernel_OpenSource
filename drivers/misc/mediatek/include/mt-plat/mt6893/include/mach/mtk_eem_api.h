3247db3a8aaed4805a84599c5c0e67b71e8f0318
EastL <EastL.Lee@mediatek.com>
2021-01-29 02:15:48 +0800

    [ALPS05114312] platform: fix mt6893 build error
    [Detail]
    1. add msdc platform folder
    2. add keyboard platform folder
    3. add vibrator platform folder
    4. add mu3phy platform folder for FPGA USB
    5. add include platform folder
    6. add mach_mt6893 config
    7. add FPGA_EARLY_PORTING config for build error
    8. rm pmic in i2c project dts node for build error
    
    MTK-Commit-Id: e61eab9131fb34322fc33f57a2d0c27dc021a944
    
    Change-Id: Id4c1bfa35667b79f8c963e41816d273f3c6c5828
    Signed-off-by: EastL <EastL.Lee@mediatek.com>
    CR-Id: ALPS05114312
    Feature: System Performance

---
+++ drivers/misc/mediatek/include/mt-plat/mt6893/include/mach/mtk_eem_api.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2017 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+#ifndef __MT_EEM_API_H__
+#define __MT_EEM_API_H__
+/* DRCC */
+extern void drcc_fail_composite(void);
+#endif
