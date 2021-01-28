f945fedb180f2a3bd357f050354057c5a4646808
mtk07751 <chandler.li@mediatek.com>
2021-01-28 20:37:31 +0800

    [ALPS05092824] driver: mt6779: udi driver
    portinf udi driver for mt6779 on kernel-4.19
    
    MTK-Commit-Id: a2da980ef74b19d804193d853c00333cf7af8c1a
    
    Change-Id: Ie3b13c6cc52303c0305ad7dcc3b6c645fa9030e1
    Signed-off-by: mtk07751 <chandler.li@mediatek.com>
    CR-Id: ALPS05092824
    Feature: Power Management

---
+++ drivers/misc/mediatek/base/power/udi_v2/mtk_udi_internal.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2016 MediaTek Inc.
+ */
+/*
+ * @file    mtk_udi.h
+ * @brief   Driver for UDI interface define
+ *
+ */
+#ifndef __MTK_UDI_INTERNAL_H__
+#define __MTK_UDI_INTERNAL_H__
+
+#include <mtk_udi.h>
+
+#endif /* __MTK_UDI_INTERNAL_H__ */
