d0aaaf5e286cda5b195e0754d3410afb60febed0
YT Chang <yt.chang@mediatek.com>
2021-01-28 19:48:34 +0800

    [ALPS04222778] sched: phase in CONFIG_MTK_SCHED_EAS_POWER_SUPPORT
    [Details]
    phase in CONFIG_MTK_SCHED_EAS_POWER_SUPPORT
    
    MTK-Commit-Id: 215989066e2443a740d32f36290d77164d1c3d05
    
    Change-Id: I0fe475cdb81c18de0fb1d71403c9f6ba37068c0b
    Signed-off-by: YT Chang <yt.chang@mediatek.com>
    CR-Id: ALPS04222778
    Feature: Energy Aware Scheduling Plus (EAS+)
    (cherry-pick from b198f659a0ee307c1589a1bc1ef47efde96c6c2d)

--- kernel/sched/eas_plus.h
+++ kernel/sched/eas_plus.h
@@ -43,7 +43,7 @@ mtk_idle_power(int cpu_idx, int idle_state, int cpu, void *argu, int sd_level);
 extern
 int mtk_busy_power(int cpu_idx, int cpu, void *argu, int sd_level);
 
-extern inline
+extern
 const struct sched_group_energy * const cci_energy(void);
 #endif
 
