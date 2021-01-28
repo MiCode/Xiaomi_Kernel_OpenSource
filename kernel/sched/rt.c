36a722e7f3f008b060fb5809b9a97a749028e7a8
mtk11965 <lixiong.liu@mediatek.com>
2021-01-28 22:20:22 +0800

    [ALPS05128623] sched: fix disable CONFIG_RT_GROUP_SCHED issue
    [Detail]
    if disable CONFIG_RT_GROUP_SCHED,then there is
    no is_rt_throttle this function,but this
    function is used in some places,when I try to
    full comlile the codebase,an error occurs
    
    [solution]
    create is_rt_throttle this function when disable
    CONFIG_RT_GROUP_SCHED
    
    MTK-Commit-Id: 27dcfaecc181ebcca265b94efe74b49a980c0850
    
    Change-Id: Ic89400a5511040de70f160390fa42e6cd61f9665
    Signed-off-by: mtk11965 <lixiong.liu@mediatek.com>
    CR-Id: ALPS05128623
    Feature: Energy Aware Scheduling Plus (EAS+)

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -2528,7 +2528,6 @@ const struct sched_class rt_sched_class = {
 #endif
 };
 
-#ifdef CONFIG_RT_GROUP_SCHED
 /*
  * Ensure that the real time constraints are schedulable.
  */
@@ -2542,7 +2541,7 @@ bool is_rt_throttle(int cpu)
 	bool rt_throttle = false;
 
 	for_each_rt_rq(rt_rq, iter, rq) {
-		if (rt_rq->rt_throttled) {
+		if (rt_rq_throttled(rt_rq)) {
 			rt_throttle = true;
 			break;
 		}
@@ -2551,6 +2550,7 @@ bool is_rt_throttle(int cpu)
 }
 #endif
 
+#ifdef CONFIG_RT_GROUP_SCHED
 /* Must be called with tasklist_lock held */
 static inline int tg_has_rt_tasks(struct task_group *tg)
 {
