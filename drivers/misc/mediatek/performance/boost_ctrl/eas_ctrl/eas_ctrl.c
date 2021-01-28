c4403ae56e961c7b3f8ea9b1ff3e9f1e82d9b6ee
Ian-Y Chen <ian-y.chen@mediatek.com>
2021-01-29 02:19:05 +0800

    [ALPS05263605] power: use uclamp min
    [Detail]
    Use uclamp min instead of stune boost value in following scenarios
    1. boot boost
    2. touch boost
    
    MTK-Commit-Id: 652550becd0238a6dde9fd21f0bc7ed4919607d6
    
    Change-Id: If8fda549d01508c882e2cf86c1a953f5bb1b0737
    Signed-off-by: Ian-Y Chen <ian-y.chen@mediatek.com>
    CR-Id: ALPS05263605
    Feature: [Module]PowerHAL

--- drivers/misc/mediatek/performance/boost_ctrl/eas_ctrl/eas_ctrl.c
+++ drivers/misc/mediatek/performance/boost_ctrl/eas_ctrl/eas_ctrl.c
@@ -675,10 +675,10 @@ static ssize_t perfmgr_boot_boost_proc_write(
 	if (rv != 0)
 		return rv;
 
-	data = check_boost_value(data);
+	data = check_uclamp_value(data);
 
 	if (cgroup >= 0 && cgroup < NR_CGROUP)
-		update_eas_boost_value(EAS_KIR_BOOT, cgroup, data);
+		update_eas_uclamp_min(EAS_UCLAMP_KIR_BOOT, cgroup, data);
 
 	return cnt;
 }
@@ -688,7 +688,7 @@ static int perfmgr_boot_boost_proc_show(struct seq_file *m, void *v)
 	int i;
 
 	for (i = 0; i < NR_CGROUP; i++)
-		seq_printf(m, "%d\n", boost_value[i][EAS_KIR_BOOT]);
+		seq_printf(m, "%d\n", uclamp_min[i][EAS_UCLAMP_KIR_BOOT]);
 
 	return 0;
 }
