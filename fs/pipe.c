af197d360c125365d838f283d4b61391f600e38a
Rocco Yue <rocco.yue@mediatek.com>
2021-01-29 00:48:33 +0800

    [ALPS04676900] network: enhance kernel pipe debug log
    Netd kill dnsmasq but failed, need to figure out whether
    dnsmasq recv the kill signal but lost in pipe.
    
    In order to make sure that issue is caused by pipe,
    more enhance debug log is necessary.
    
    MTK-Commit-Id: b33016f099221a77920c0e3e0a71f9fa5e1df2b5
    
    Change-Id: Ie3c68381c53b16c28399da06813e842ce02033e1
    Signed-off-by: Rocco Yue <rocco.yue@mediatek.com>
    CR-Id: ALPS04676900
    Feature: [Android Default] USB Tethering

--- fs/pipe.c
+++ fs/pipe.c
@@ -277,6 +277,10 @@ pipe_read(struct kiocb *iocb, struct iov_iter *to)
 	do_wakeup = 0;
 	ret = 0;
 	__pipe_lock(pipe);
+
+	if (strcmp(current->comm, "dnsmasq") == 0)
+		pr_info("[mtk_net][%s]\n", __func__);
+
 	for (;;) {
 		int bufs = pipe->nrbufs;
 		if (bufs) {
@@ -341,13 +345,19 @@ pipe_read(struct kiocb *iocb, struct iov_iter *to)
 			}
 		}
 		if (signal_pending(current)) {
+			if (strcmp(current->comm, "dnsmasq") == 0)
+				pr_info("[mtk_net][%d][%s]signal_pending, ret=%zd\n",
+					__LINE__, __func__,  ret);
 			if (!ret)
 				ret = -ERESTARTSYS;
 			break;
 		}
 		if (do_wakeup) {
 			wake_up_interruptible_sync_poll(&pipe->wait, POLLOUT | POLLWRNORM);
- 			kill_fasync(&pipe->fasync_writers, SIGIO, POLL_OUT);
+			kill_fasync(&pipe->fasync_writers, SIGIO, POLL_OUT);
+			if (strcmp(current->comm, "dnsmasq") == 0)
+				pr_info("[mtk_net][%d][%s]do_wakeup\n",
+					__LINE__, __func__);
 		}
 		pipe_wait(pipe);
 	}
@@ -357,6 +367,9 @@ pipe_read(struct kiocb *iocb, struct iov_iter *to)
 	if (do_wakeup) {
 		wake_up_interruptible_sync_poll(&pipe->wait, POLLOUT | POLLWRNORM);
 		kill_fasync(&pipe->fasync_writers, SIGIO, POLL_OUT);
+		if (strcmp(current->comm, "dnsmasq") == 0)
+			pr_info("[mtk_net][%d][%s] do_wakeup\n",
+				__LINE__, __func__);
 	}
 	if (ret > 0)
 		file_accessed(filp);
@@ -389,6 +402,9 @@ pipe_write(struct kiocb *iocb, struct iov_iter *from)
 		ret = -EPIPE;
 		goto out;
 	}
+
+	if (strcmp(current->comm, "dnsmasq") == 0)
+		pr_info("[mtk_net][%d][%s]\n", __LINE__, __func__);
 
 	/* We try to merge small writes */
 	chars = total_len & (PAGE_SIZE-1); /* size of the last buffer */
@@ -477,6 +493,9 @@ pipe_write(struct kiocb *iocb, struct iov_iter *from)
 			break;
 		}
 		if (signal_pending(current)) {
+			if (strcmp(current->comm, "dnsmasq") == 0)
+				pr_info("[mtk_net][%d][%s]signal_pending, ret=%zd\n",
+					__LINE__, __func__, ret);
 			if (!ret)
 				ret = -ERESTARTSYS;
 			break;
@@ -485,6 +504,9 @@ pipe_write(struct kiocb *iocb, struct iov_iter *from)
 			wake_up_interruptible_sync_poll(&pipe->wait, POLLIN | POLLRDNORM);
 			kill_fasync(&pipe->fasync_readers, SIGIO, POLL_IN);
 			do_wakeup = 0;
+			if (strcmp(current->comm, "dnsmasq") == 0)
+				pr_info("[mtk_net][%d][%s]do_wakeup\n",
+					__LINE__, __func__);
 		}
 		pipe->waiting_writers++;
 		pipe_wait(pipe);
@@ -547,6 +569,12 @@ pipe_poll(struct file *filp, poll_table *wait)
 			mask |= POLLHUP;
 	}
 
+	if (strcmp(current->comm, "dnsmasq") == 0) {
+		if (mask)
+			pr_info("[mtk_net][%d][%s]read mask : 0x%04x\n",
+				__LINE__, __func__, mask);
+	}
+
 	if (filp->f_mode & FMODE_WRITE) {
 		mask |= (nrbufs < pipe->buffers) ? POLLOUT | POLLWRNORM : 0;
 		/*
@@ -555,8 +583,20 @@ pipe_poll(struct file *filp, poll_table *wait)
 		 */
 		if (!pipe->readers)
 			mask |= POLLERR;
+
+		if (strcmp(current->comm, "dnsmasq") == 0) {
+			if (mask)
+				pr_info("[mtk_net][%d][%s]FMODE_WRITE read mask : 0x%04x\n",
+					__LINE__, __func__, mask);
+		}
 	}
 
+	if (((filp->f_mode & FMODE_READ) == 0) &&
+				((filp->f_mode & FMODE_WRITE) == 0)) {
+		if (strcmp(current->comm, "dnsmasq") == 0)
+			pr_info("[mtk_net][%d][%s]mask NULL\n",
+				__LINE__, __func__);
+	}
 	return mask;
 }
 
@@ -590,6 +630,8 @@ pipe_release(struct inode *inode, struct file *file)
 		wake_up_interruptible_sync_poll(&pipe->wait, POLLIN | POLLOUT | POLLRDNORM | POLLWRNORM | POLLERR | POLLHUP);
 		kill_fasync(&pipe->fasync_readers, SIGIO, POLL_IN);
 		kill_fasync(&pipe->fasync_writers, SIGIO, POLL_OUT);
+		if (strcmp(current->comm, "dnsmasq") == 0)
+			pr_info("[mtk_net][%d][%s]\n", __LINE__, __func__);
 	}
 	__pipe_unlock(pipe);
 
@@ -897,6 +939,9 @@ static int wait_for_partner(struct pipe_inode_info *pipe, unsigned int *cnt)
 static void wake_up_partner(struct pipe_inode_info *pipe)
 {
 	wake_up_interruptible(&pipe->wait);
+
+	if (strcmp(current->comm, "dnsmasq") == 0)
+		pr_info("[mtk_net][%d][%s]pipe->wait\n", __LINE__, __func__);
 }
 
 static int fifo_open(struct inode *inode, struct file *filp)
@@ -1009,12 +1054,18 @@ err_rd:
 	if (!--pipe->readers)
 		wake_up_interruptible(&pipe->wait);
 	ret = -ERESTARTSYS;
+	if (strcmp(current->comm, "dnsmasq") == 0)
+		pr_info("[mtk_net][%d][%s]err_rd: pipe->wait\n",
+			__LINE__, __func__);
 	goto err;
 
 err_wr:
 	if (!--pipe->writers)
 		wake_up_interruptible(&pipe->wait);
 	ret = -ERESTARTSYS;
+	if (strcmp(current->comm, "dnsmasq") == 0)
+		pr_info("[mtk_net][%d][%s]err_wr: pipe->wait\n",
+			__LINE__, __func__);
 	goto err;
 
 err:
