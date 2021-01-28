e84b617b80c5181fd4906a2eb4e01e202546bc34
cpi00078 <cpi00078@mediatek.com>
2021-01-29 02:28:28 +0800

    [ALPS05218900] PMIC: fix adc coverity exp34_c
    [Detail]
    fix adc module coverity exp34_c for returned_null
    
    MTK-Commit-Id: 2d64667b15e1d582471a870b9de61784151666b7
    
    Change-Id: Ie53eebb8a20f2aa0e83fea8a4acb2562fb61d131
    Signed-off-by: cpi00078 <cpi00078@mediatek.com>
    CR-Id: ALPS05218900
    Feature: [Module]PMIC
    (cherry picked from commit 1d3bd724688343c3d93dac93e82feefb1fb8bc7f)

--- drivers/misc/mediatek/pmic/mt6360/pmu/mt6360_pmu_adc.c
+++ drivers/misc/mediatek/pmic/mt6360/pmu/mt6360_pmu_adc.c
@@ -315,12 +315,15 @@ static int mt6360_adc_scan_task_threadfn(void *data)
 
 static int mt6360_adc_iio_post_enable(struct iio_dev *iio_dev)
 {
+	char *p;
 	struct mt6360_pmu_adc_info *mpai = iio_priv(iio_dev);
 
 	dev_dbg(&iio_dev->dev, "%s ++\n", __func__);
-	mpai->scan_task = kthread_run(mt6360_adc_scan_task_threadfn, mpai,
-				      devm_kasprintf(mpai->dev, GFP_KERNEL,
-				      "scan_thread.%s", dev_name(mpai->dev)));
+	p = devm_kasprintf(mpai->dev, GFP_KERNEL,
+				"scan_thread.%s", dev_name(mpai->dev));
+	if (IS_ERR(p))
+		return PTR_ERR(p);
+	mpai->scan_task = kthread_run(mt6360_adc_scan_task_threadfn, mpai, p);
 	dev_dbg(&iio_dev->dev, "%s --\n", __func__);
 	return PTR_ERR_OR_ZERO(mpai->scan_task);
 }
