73a217964ec5a7245e35394f4d74ae7359e5a5e4
Rocco Yue <rocco.yue@mediatek.com>
2021-01-29 02:25:05 +0800

    [ALPS05542993] IPv6: add <iface>/ra_mtu proc
    When the MTU option is carried in the RA message, the value
    will be stored in the "proc/sys/net/ipv6/conf/<iface>/ra_mtu".
    Meanwhile, ra_mtu is a standalone proc, which is unaffected
    by updates to the interfaces mtu value.
    
    MTK-Commit-Id: 64cbdf7fc1b4de4e07085725aac8571856773797
    
    Change-Id: I532fe15e1bd55c6d8c03a36e8865e37025514962
    Signed-off-by: Rocco Yue <rocco.yue@mediatek.com>
    CR-Id: ALPS05542993
    Feature: IPv6 over MD

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -203,6 +203,7 @@ static struct ipv6_devconf ipv6_devconf __read_mostly = {
 	.forwarding		= 0,
 	.hop_limit		= IPV6_DEFAULT_HOPLIMIT,
 	.mtu6			= IPV6_MIN_MTU,
+	.ra_mtu			= 0,
 	.accept_ra		= 1,
 	.accept_redirects	= 1,
 	.autoconf		= 1,
@@ -258,6 +259,7 @@ static struct ipv6_devconf ipv6_devconf_dflt __read_mostly = {
 	.forwarding		= 0,
 	.hop_limit		= IPV6_DEFAULT_HOPLIMIT,
 	.mtu6			= IPV6_MIN_MTU,
+	.ra_mtu                 = 0,
 	.accept_ra		= 1,
 	.accept_redirects	= 1,
 	.autoconf		= 1,
@@ -5259,6 +5261,7 @@ static inline void ipv6_store_devconf(struct ipv6_devconf *cnf,
 	array[DEVCONF_FORWARDING] = cnf->forwarding;
 	array[DEVCONF_HOPLIMIT] = cnf->hop_limit;
 	array[DEVCONF_MTU6] = cnf->mtu6;
+	array[DEVCONF_RA_MTU] = cnf->ra_mtu;
 	array[DEVCONF_ACCEPT_RA] = cnf->accept_ra;
 	array[DEVCONF_ACCEPT_REDIRECTS] = cnf->accept_redirects;
 	array[DEVCONF_AUTOCONF] = cnf->autoconf;
@@ -6288,6 +6291,13 @@ static const struct ctl_table addrconf_sysctl[] = {
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
 		.proc_handler	= addrconf_sysctl_mtu,
+	},
+	{
+		.procname	= "ra_mtu",
+		.data		= &ipv6_devconf.ra_mtu,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
 	},
 	{
 		.procname	= "accept_ra",
