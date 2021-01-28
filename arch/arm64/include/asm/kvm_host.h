b2c850f31d97f21325bc2bd1f64fdc42e3594352
Andy <chia-hao.hsu@mediatek.com>
2021-01-29 00:40:35 +0800

    [ALPS04295973] Backport: arm64 SSBS feature for V4 mitigation
    [Detail]
    1. Backport V4 mitigation changes:
    Add support for PSTATE.SSBS to mitigate Spectre-v4
    arm64: Fix silly typo in comment
    arm64: cpufeature: Detect SSBS and advertise to userspace
    arm64: ssbd: Drop #ifdefs for PR_SPEC_STORE_BYPASS
    arm64: entry: Allow handling of undefined instructions from EL1
    arm64: ssbd: Add support for PSTATE.SSBS rather than trapping to EL3
    KVM: arm64: Set SCTLR_EL2.DSSBS if SSBD is forcefully disabled and !vhe
    arm64: cpu: Move errata and feature enable callbacks closer to callers
    2. Fix nested panic check flow to bypass el1_dbg
    3. Remove redundant code in nested panic check flow
    
    MTK-Commit-Id: a7ed649f86f754d74978bdff6bc3264d5205add1
    
    Change-Id: Iaaef831a6d14ee7797224313a7cff7c1025cae7f
    CR-Id: ALPS04295973
    Feature: Large DRAM
    Signed-off-by: Andy <chia-hao.hsu@mediatek.com>

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -356,6 +356,8 @@ struct kvm_vcpu *kvm_mpidr_to_vcpu(struct kvm *kvm, unsigned long mpidr);
 void __kvm_set_tpidr_el2(u64 tpidr_el2);
 DECLARE_PER_CPU(kvm_cpu_context_t, kvm_host_cpu_state);
 
+void __kvm_enable_ssbs(void);
+
 static inline void __cpu_init_hyp_mode(phys_addr_t pgd_ptr,
 				       unsigned long hyp_stack_ptr,
 				       unsigned long vector_ptr)
@@ -380,6 +382,15 @@ static inline void __cpu_init_hyp_mode(phys_addr_t pgd_ptr,
 		- (u64)kvm_ksym_ref(kvm_host_cpu_state);
 
 	kvm_call_hyp(__kvm_set_tpidr_el2, tpidr_el2);
+
+	/*
+	 * Disabling SSBD on a non-VHE system requires us to enable SSBS
+	 * at EL2.
+	 */
+	if (!has_vhe() && this_cpu_has_cap(ARM64_SSBS) &&
+	    arm64_get_ssbd_state() == ARM64_SSBD_FORCE_DISABLE) {
+		kvm_call_hyp(__kvm_enable_ssbs);
+	}
 }
 
 static inline void kvm_arch_hardware_unsetup(void) {}
