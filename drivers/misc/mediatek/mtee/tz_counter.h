71253c0661fe0c7fdd5329e27a8e2201efa4cec6
Seiya Wang <seiya.wang@mediatek.com>
2021-01-28 18:18:41 +0800

    [ALPS03934967] mtee: add mtee kernel driver
    Add mtee kernel driver
    
    MTK-Commit-Id: 8d0a49f3ea18bc5110e911969580b2c03ac01f17
    
    Change-Id: Ib78a186f64b3b99cc489876b3f1913db6d14e5d5
    CR-Id: ALPS03934967
    Feature: MTK In-House TEE (Trusted Execution Environment)
    Signed-off-by: Seiya Wang <seiya.wang@mediatek.com>
    (cherry-pick from 3490704e42704638a4709a2aafec573126eecc2f)

---
+++ drivers/misc/mediatek/mtee/tz_counter.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __KREE_INC_COUNTER
+#define __KREE_INC_COUNTER
+
+/* enable counter implementation */
+#define ENABLE_INC_ONLY_COUNTER
+
+int update_counter_thread(void *data);
+
+#endif
