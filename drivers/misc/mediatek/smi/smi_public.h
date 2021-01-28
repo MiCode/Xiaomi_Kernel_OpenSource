458d5c090fd14837c2440f40d6251e9e3c144305
Ming-Fan Chen <ming-fan.chen@mediatek.com>
2021-01-29 00:49:56 +0800

    [ALPS04694084] smi: support sysram setting api for mt6885
    Support sysram setting api for mt6885.
    
    MTK-Commit-Id: 5aa2256079e71e4a4b9d7fd3a65ff420420349c9
    
    Change-Id: I3040a8cbdedd28b91924075ce89981c54204f1b7
    CR-Id: ALPS04694084
    Feature: [Module]SMI Driver
    Signed-off-by: Ming-Fan Chen <ming-fan.chen@mediatek.com>

--- drivers/misc/mediatek/smi/smi_public.h
+++ drivers/misc/mediatek/smi/smi_public.h
@@ -15,6 +15,11 @@
 #define __SMI_PUBLIC_H__
 
 #if IS_ENABLED(CONFIG_MTK_SMI_EXT)
+
+#if IS_ENABLED(CONFIG_MACH_MT6885)
+#include "mt6885/smi_port.h"
+#endif
+
 enum {
 	SMI_LARB0, SMI_LARB1, SMI_LARB2, SMI_LARB3, SMI_LARB4,
 	SMI_LARB5, SMI_LARB6, SMI_LARB7, SMI_LARB8, SMI_LARB9,
@@ -27,11 +32,13 @@ bool smi_mm_first_get(void);
 s32 smi_bus_prepare_enable(const u32 id, const char *user);
 s32 smi_bus_disable_unprepare(const u32 id, const char *user);
 s32 smi_debug_bus_hang_detect(const bool gce, const char *user);
+s32 smi_sysram_enable(const u32 master_id, const bool enable, const char *user);
 #else
 #define smi_mm_first_get() (true)
 #define smi_bus_prepare_enable(id, user) ((void)0)
 #define smi_bus_disable_unprepare(id, user) ((void)0)
 #define smi_debug_bus_hang_detect(gce, user) ((void)0)
+#define smi_sysram_enable(master_id, enable, user) ((void)0)
 #endif
 
 #endif
