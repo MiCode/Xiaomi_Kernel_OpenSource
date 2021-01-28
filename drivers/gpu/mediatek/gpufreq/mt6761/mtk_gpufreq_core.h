6898244485d1e9b2a3b769fd4b2c1cb25bf691f7
Eric Chung <eric.chung@mediatek.com>
2021-01-28 19:55:40 +0800

    [ALPS05078283] GPU: Complement missing efuse dts node in 32-bit
    [Detail]
    CL: 3229901 only modified 64-bit and forgot the 32-bit.
    Complement missing EFUSE DTS node in 32-bit arch,
    and remove legacy function declaration.
    
    MTK-Commit-Id: eef5f97258e2c35c8f4bde87023b2d8150b90bd8
    
    Change-Id: Ia4f6cf572ea8600362b10b8bfd848787a9db2739
    Signed-off-by: Eric Chung <eric.chung@mediatek.com>
    CR-Id: ALPS05078283
    Feature: GPU DVFS

--- drivers/gpu/mediatek/gpufreq/mt6761/mtk_gpufreq_core.h
+++ drivers/gpu/mediatek/gpufreq/mt6761/mtk_gpufreq_core.h
@@ -296,7 +296,6 @@ struct g_pmic_info {
  **************************************************/
 extern bool mtk_get_gpu_loading(unsigned int *pLoading);
 extern unsigned int mt_get_ckgen_freq(unsigned int ckgen);
-extern u32 get_devinfo_with_index(u32 index);
 
 extern int (*mt_dfs_general_pll)(unsigned int pll_id, unsigned int dds);
 
