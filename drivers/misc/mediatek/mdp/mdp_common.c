76f8e8912bee8599fd29e151351dd54bd80b8975
Chu-Chiao Liao <Chu-Chiao.Liao@mediatek.com>
2021-01-29 02:23:59 +0800

    [ALPS05393487] mdp: coverity modification
    - check if nullptr before dereferencing it
    
    MTK-Commit-Id: b9a1e003740f5d0a7b89d0818e2daf56e56e0a01
    
    Change-Id: I81f5654328a6d0b8dc9be3edc504ceda7422707b
    Signed-off-by: Chu-Chiao Liao <Chu-Chiao.Liao@mediatek.com>
    CR-Id: ALPS05393487
    Feature: [Module]MDP Driver

--- drivers/misc/mediatek/mdp/mdp_common.c
+++ drivers/misc/mediatek/mdp/mdp_common.c
@@ -3303,12 +3303,18 @@ static void mdp_readback_hdr_virtual(struct cmdqRecStruct *handle,
 	cmdq_pkt_jump_addr(pkt, begin_pa);
 	condi_inst = (u32 *)cmdq_pkt_get_va_by_offset(pkt, condi_offset);
 	if (unlikely(!condi_inst)) {
-		CMDQ_ERR("%s wrong offset %u\n", condi_offset);
+		CMDQ_ERR("%s wrong offset %u\n", __func__, condi_offset);
 		return;
 	}
-	if (condi_inst[1] == 0x10000001)
+	if (condi_inst[1] == 0x10000001) {
 		condi_inst = (u32 *)cmdq_pkt_get_va_by_offset(pkt,
 			condi_offset + 8);
+		if (unlikely(!condi_inst)) {
+			CMDQ_ERR("%s wrong offset %u.\n", __func__, condi_offset);
+			return;
+		}
+	}
+
 	*condi_inst = (u32)CMDQ_REG_SHIFT_ADDR(cmdq_pkt_get_curr_buf_pa(pkt));
 
 	pa = pa + MDP_HDR_HIST_CNT * 4;
