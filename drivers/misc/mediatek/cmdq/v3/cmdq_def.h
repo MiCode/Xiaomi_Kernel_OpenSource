231f9af7b3c0d7f584f263584dfdd8737beffabd
Bhupender tabiyad <bhupender.tabiyad@mediatek.com>
2021-01-29 02:18:07 +0800

    [ALPS05107221] cmdq: synchronize iwc_common structure
    Synchronize iwc_common structure from cmdq/mailbox.
    
    MTK-Commit-Id: c0d4627c11fd1c799971637fc5bf15ae2ec01c8a
    
    Feature:[Module]CMDQ Driver
    CR-Id:ALPS05107221
    Change-Id: Idd3c8f9f1c300ff95ed496be8847ceff3a28a6ba
    Signed-off-by: Bhupender Tabiyad <Bhupender.tabiyad@mediatek.com>

--- drivers/misc/mediatek/cmdq/v3/cmdq_def.h
+++ drivers/misc/mediatek/cmdq/v3/cmdq_def.h
@@ -72,7 +72,7 @@
 #define CMDQ_MAX_INST_CYCLE             (27)
 #define CMDQ_MAX_ERROR_SIZE             (8 * 1024)
 
-#define CMDQ_MAX_TASK_IN_SECURE_THREAD	(10)
+#define CMDQ_MAX_TASK_IN_SECURE_THREAD	(3)
 
 /* max value of CMDQ_THR_EXEC_CMD_CNT (value starts from 0) */
 #ifdef CMDQ_USE_LARGE_MAX_COOKIE
