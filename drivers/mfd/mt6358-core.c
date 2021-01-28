cf936a8c4b88e29b785bf3f579019a34019e229e
Wen Su <wen.su@mediatek.com>
2021-01-29 02:23:27 +0800

    [ALPS05164566] PMIC: fix missing irq of chrdet_edge
    We need to clear edge triggered irq status first before calling
    nested irq handling of chrdet_edge to avoid missing event
    
    MTK-Commit-Id: 051183360b23ed66d909c66213956d464b04da4a
    
    Change-Id: Ib6e7657f2e455db4505b0a7a17ec2bece888107e
    Signed-off-by: Wen Su <wen.su@mediatek.com>
    CR-Id: ALPS05164566
    Feature: Power Management

--- drivers/mfd/mt6358-core.c
+++ drivers/mfd/mt6358-core.c
@@ -184,6 +184,10 @@ static void mt6358_irq_sp_handler(struct mt6358_chip *chip,
 				sta_reg, sp_int_status,
 				pmic_irqs[hwirq].name, hwirq,
 				irq_get_trigger_type(virq));
+			if (!strncmp(pmic_irqs[hwirq].name, "chrdet_edge", 11)) {
+				regmap_write(chip->regmap, sta_reg, BIT(j));
+				sp_int_status &= ~BIT(j);
+			}
 			log_threaded_irq_wakeup_reason(virq, chip->irq);
 			if (virq)
 				handle_nested_irq(virq);
