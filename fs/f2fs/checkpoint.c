886dd9c2db5818e94704e8f88b3ad90f36c5f25f
Light Hsieh <light.hsieh@mediatek.com>
2021-01-29 02:24:49 +0800

    [ALPS05474126] f2fs: fix deadlock between quota writes and checkpoint
    f2fs_write_data_pages(quota_mapping)
     __f2fs_write_data_pages       f2fs_write_checkpoint
      * blk_start_plug(&plug);
      * add bio in write_io[DATA]
                                    - block_operations
                                    - skip syncing quota by
                                              >DEFAULT_RETRY_QUOTA_FLUSH_COUNT
                                    - down_write(&sbi->node_write);
      - f2fs_write_single_data_page
       - down_read(node_write)
                                    - f2fs_wait_on_all_pages(F2FS_WB_CP_DATA);
    
    MTK-Commit-Id: 91d33cb30321b9e6c2d9b38ac667355b11faaad4
    
    Change-Id: I79829eddb41083b1a5c5078544992f5501c08a54
    Signed-off-by: Daeho Jeong <daehojeong@google.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Light Hsieh <light.hsieh@mediatek.com>
    CR-Id: ALPS05474126
    Feature: [Android Default] F2FS File System
    (cherry picked from commit 1123117f32b2037e4ff4f6fee72edc8ebae41c02)
    (cherry picked from commit 9236cf6477debc418d3c676fa53bd8d17c5c4b8d)

--- fs/f2fs/checkpoint.c
+++ fs/f2fs/checkpoint.c
@@ -107,7 +107,6 @@ struct page *f2fs_get_meta_page(struct f2fs_sb_info *sbi, pgoff_t index)
 	return __get_meta_page(sbi, index, true);
 }
 
-//struct page *f2fs_get_meta_page_nofail(struct f2fs_sb_info *sbi, pgoff_t index)
 struct page *f2fs_get_meta_page_retry(struct f2fs_sb_info *sbi, pgoff_t index)
 {
 	struct page *page;
@@ -1270,6 +1269,8 @@ void f2fs_wait_on_all_pages(struct f2fs_sb_info *sbi, int type)
 		if (type == F2FS_DIRTY_META)
 			f2fs_sync_meta_pages(sbi, META, LONG_MAX,
 							FS_CP_META_IO);
+		else if (type == F2FS_WB_CP_DATA)
+			f2fs_submit_merged_write(sbi, DATA);
 		io_schedule_timeout(DEFAULT_IO_TIMEOUT);
 	}
 	finish_wait(&sbi->cp_wait, &wait);
