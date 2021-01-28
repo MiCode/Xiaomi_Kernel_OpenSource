bb19075a49237dc0ba9b86dfd20cbd17a9e0446e
Bella Zhao <bella.zhao@mediatek.com>
2021-01-29 02:05:10 +0800

    [ALPS05202532] DLPT: fix coverity defect
    DLPT: fix coverity defect
    
    MTK-Commit-Id: 7848da2d750611668253bc0e39e621b50c134add
    
    Change-Id: I1e5345da24387c48722deaa2602c5ec1154ea1a4
    Signed-off-by: Bella Zhao <bella.zhao@mediatek.com>
    CR-Id: ALPS05202532
    Feature: Dynamic load power throttling

--- drivers/misc/mediatek/base/power/mdpm_v2/mt6779/mtk_mdpm_platform.c
+++ drivers/misc/mediatek/base/power/mdpm_v2/mt6779/mtk_mdpm_platform.c
@@ -571,6 +571,9 @@ int get_md1_scenario_power(enum md_scenario scenario,
 	mdpm_pwr_sta->scenario_id = scenario;
 	temp = snprintf(mdpm_pwr_sta->scenario_name, MAX_MDPM_NAME_LEN,
 		"%s", mdpm_scen[scenario].scenario_name);
+	if (temp < 0)
+		pr_notice("%s error scenario_name\n", __func__);
+
 	mdpm_pwr_sta->scanario_power = s_power;
 	mdpm_pwr_sta->power_type = power_type;
 
