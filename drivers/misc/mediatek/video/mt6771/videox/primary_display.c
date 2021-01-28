0274382e338184aecf897316a7097211252b6e42
Chih-Chieh Yeh <chih-chieh.yeh@mediatek.com>
2021-01-29 00:44:36 +0800

    [ALPS04478223] DISP: change ion cache sync type to by_range
    [Detail]
    Change ion cache sync type. Deprecate ION_CACHE_FLUSH_ALL and use
    ION_CACHE_FLUSH_BY_RANGE instead.
    
    MTK-Commit-Id: 7f01e892ebccce65a8580099815b74e57ff71ab8
    
    Change-Id: I901550d1f3a13887b8255a2751a7bfcf158cad3e
    Signed-off-by: Chih-Chieh Yeh <chih-chieh.yeh@mediatek.com>
    CR-Id: ALPS04478223
    Feature: [Module]Display Driver

--- drivers/misc/mediatek/video/mt6771/videox/primary_display.c
+++ drivers/misc/mediatek/video/mt6771/videox/primary_display.c
@@ -9207,7 +9207,7 @@ int primary_display_capture_framebuffer_ovl(unsigned long pbuf,
 	disp_ion_get_mva(ion_display_client, ion_display_handle, &mva,
 			 DISP_M4U_PORT_DISP_WDMA0);
 	disp_ion_cache_flush(ion_display_client, ion_display_handle,
-			     ION_CACHE_FLUSH_ALL);
+			     ION_CACHE_FLUSH_BY_RANGE);
 
 	tmp = disp_helper_get_option(DISP_OPT_SCREEN_CAP_FROM_DITHER);
 	if (tmp == 0)
@@ -9219,7 +9219,7 @@ int primary_display_capture_framebuffer_ovl(unsigned long pbuf,
 		_screen_cap_by_cpu(mva, ufmt, after_eng);
 
 	disp_ion_cache_flush(ion_display_client, ion_display_handle,
-			     ION_CACHE_INVALID_BY_RANGE);
+			     ION_CACHE_FLUSH_BY_RANGE);
 
 out:
 	if (ion_display_client)
@@ -9294,7 +9294,7 @@ int primary_display_capture_framebuffer_ovl(unsigned long pbuf,
 		_screen_cap_by_cpu(mva, ufmt, after_eng);
 
 	ret = m4u_cache_sync(m4uClient, DISP_M4U_PORT_DISP_WDMA0, pbuf,
-			     buffer_size, mva, M4U_CACHE_INVALID_BY_RANGE);
+			     buffer_size, mva, M4U_CACHE_FLUSH_BY_RANGE);
 
 out:
 	if (mva > 0)
