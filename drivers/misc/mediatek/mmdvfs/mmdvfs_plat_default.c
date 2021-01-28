99be051d026bf530cccf1143fe7c7a0c48a6c16d
Sabrina Kuo <Yi.Kuo@mediatek.com>
2021-01-28 19:48:42 +0800

    [ALPS05065095] mmdvfs: mt6761 R migration porting
    [Solution]
    mmdvfs porting
    
    MTK-Commit-Id: 6bd2541e5b1f81038026813c5ae6ec7e303ae671
    
    Change-Id: Ifb7520b473d5b5ae3e2aef24df08cda078d3e53f
    Signed-off-by: Sabrina Kuo <Yi.Kuo@mediatek.com>
    CR-Id: ALPS05065095
    Feature: [Module]MM pipeline/MM DVFS

---
+++ drivers/misc/mediatek/mmdvfs/mmdvfs_plat_default.c
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2020 MediaTek Inc.
+ */
+
+#include <linux/string.h>
+
+#include "mmdvfs_plat.h"
+
+inline void mmdvfs_update_qos_sram(struct mm_larb_request larb_req[],
+	u32 larb_update)
+{ return; }
+
+inline void mmdvfs_update_limit_config(enum mmdvfs_limit_source source,
+	u32 source_value, u32 *limit_value, u32 *limit_level)
+{ return; }
+
+bool mmdvfs_log_larb_mmp(s32 common_port_id, s32 larb_id)
+{ return false; }
+
+/* Return port number of CCU on SMI common */
+inline u32 mmdvfs_get_ccu_smi_common_port(u32 master_id)
+{ return 0; }
+
+inline s32 get_ccu_hrt_bw(struct mm_larb_request larb_req[])
+{ return 0; }
+
+inline s32 get_md_hrt_bw(void)
+{ return 0; }
+
+inline s32 dram_write_weight(s32 val)
+{ return val; }
+
---
+++ drivers/misc/mediatek/smi/smi_public.h
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2020 MediaTek Inc.
+ */
+
+#ifndef __SMI_PUBLIC_H__
+#define __SMI_PUBLIC_H__
+
+#if IS_ENABLED(CONFIG_MTK_SMI_EXT)
+
+#if IS_ENABLED(CONFIG_MACH_MT6885)
+#include "mt6885/smi_port.h"
+#endif
+
+enum {
+	SMI_LARB0, SMI_LARB1, SMI_LARB2, SMI_LARB3, SMI_LARB4,
+	SMI_LARB5, SMI_LARB6, SMI_LARB7, SMI_LARB8, SMI_LARB9,
+	SMI_LARB10, SMI_LARB11, SMI_LARB12, SMI_LARB13, SMI_LARB14,
+	SMI_LARB15, SMI_LARB16, SMI_LARB17, SMI_LARB18, SMI_LARB19,
+	SMI_LARB20,
+};
+
+bool smi_mm_first_get(void);
+s32 smi_bus_prepare_enable(const u32 id, const char *user);
+s32 smi_bus_disable_unprepare(const u32 id, const char *user);
+s32 smi_debug_bus_hang_detect(const bool gce, const char *user);
+s32 smi_sysram_enable(const u32 master_id, const bool enable, const char *user);
+#else
+#define smi_mm_first_get() (true)
+#define smi_bus_prepare_enable(id, user) ((void)0)
+#define smi_bus_disable_unprepare(id, user) ((void)0)
+#define smi_debug_bus_hang_detect(gce, user) ((void)0)
+#define smi_sysram_enable(master_id, enable, user) ((void)0)
+#endif
+
+#endif
