2027b771ebaa641ac6b35c21cdd992abfe2b3f98
Juju Sung <juju.sung@mediatek.com>
2021-01-29 02:12:57 +0800

    [ALPS05111942] LowPower: mt6873/6853 mcupm porting
    [Detail]
    Clone mt6873/mt6853 mcupm driver from q0 to r0
    
    MTK-Commit-Id: 644216588c302cfd9675bcffd15325ec6c72a0cb
    
    Change-Id: I389035670fad7cccc24a1df45a810ac7c5208905
    Signed-off-by: Juju Sung <juju.sung@mediatek.com>
    CR-Id: ALPS05111942
    Feature: [Module]Kernel Maintenance

---
+++ drivers/misc/mediatek/mcupm/mt6873/mcupm_emi_mpu.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2011-2015 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
+#ifndef __MCUPM_EMI_MPU_H__
+#define __MCUPM_EMI_MPU_H__
+
+
+#define MUCPM_MPU_REGION_ID	19
+#define MUCPM_MPU_DOMAIN_ID	14
+
+
+#endif
