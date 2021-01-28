9751a66b797dbb264199b82751f7148236d1da5e
mtk81325 <peng.zhou@mediatek.com>
2021-01-29 02:31:02 +0800

    [ALPS04896864] inline-crypto: fix coding redundancy
    fix coding redundancy
    
    MTK-Commit-Id: 6fa21a35df7230accbbe585d9b4e8c5516c46b64
    
    Change-Id: Ib780fa7b8c882b772badb95a0c63deb0e471e7b6
    Signed-off-by: mtk81325 <peng.zhou@mediatek.com>
    CR-Id: ALPS04896864
    Feature: eMMC Boot Up

--- fs/crypto/inline_crypt.c
+++ fs/crypto/inline_crypt.c
@@ -300,7 +300,7 @@ static void fscrypt_generate_iv_spec(union fscrypt_iv *iv, u64 lblk_num,
 
 		/* eMMC + F2FS security OTA only */
 		if (flags & FSCRYPT_POLICY_FLAG_IV_INO_LBLK_32)
-			lblk_num = (u32)(((u64)(ci->ci_hashed_info) & 0xFFFFFFFFULL) + lblk_num);
+			lblk_num = (u32)(((u64)(ci->ci_hashed_info) & 0xFFFFFFFF) + lblk_num);
 
 		lblk_num = (((u64)ci->ci_inode->i_ino & 0xFFFFFFFF) << 32)
 				| (lblk_num & 0xFFFFFFFF);
