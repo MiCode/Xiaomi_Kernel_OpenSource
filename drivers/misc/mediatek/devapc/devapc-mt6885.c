6281e5dc8d6bdd278b59b9d078da06c1c54a51cf
Neal Liu <neal.liu@mediatek.com>
2021-01-29 02:23:42 +0800

    [ALPS05305241] devapc: fix index to subsys mapping wrong problem
    Use device index to get subsys naming instead of vio_index.
    
    MTK-Commit-Id: 77fe4c47d239637906f9e57eb942d1f0acad660d
    
    Change-Id: I524c1c9e559f709065c301a4eae8287eaaf39662
    Signed-off-by: Neal Liu <neal.liu@mediatek.com>
    CR-Id: ALPS05305241
    Feature: [Module]Device APC

--- drivers/misc/mediatek/devapc/devapc-mt6885.c
+++ drivers/misc/mediatek/devapc/devapc-mt6885.c
@@ -1516,6 +1516,8 @@ static const char *mt6885_bus_id_to_master(uint32_t bus_id, uint32_t vio_addr,
 const char *index_to_subsys(int slave_type, uint32_t vio_index,
 		uint32_t vio_addr)
 {
+	int i;
+
 	if (slave_type == SLAVE_TYPE_INFRA &&
 			vio_index < VIO_SLAVE_NUM_INFRA) {
 
@@ -1584,7 +1586,12 @@ const char *index_to_subsys(int slave_type, uint32_t vio_index,
 		case MDP_START ... MDP_END:
 			return "MMSYS_MDP";
 		default:
-			return mt6885_devices_infra[vio_index].device;
+			break;
+		}
+
+		for (i = 0; i < VIO_SLAVE_NUM_INFRA; i++) {
+			if (vio_index == mt6885_devices_infra[i].vio_index)
+				return mt6885_devices_infra[i].device;
 		}
 
 	} else if (slave_type == SLAVE_TYPE_PERI &&
@@ -1599,7 +1606,12 @@ const char *index_to_subsys(int slave_type, uint32_t vio_index,
 		case CONN_VIO_INDEX:
 			return "CONNSYS";
 		default:
-			return mt6885_devices_peri[vio_index].device;
+			break;
+		}
+
+		for (i = 0; i < VIO_SLAVE_NUM_PERI; i++) {
+			if (vio_index == mt6885_devices_peri[i].vio_index)
+				return mt6885_devices_peri[i].device;
 		}
 
 	} else if (slave_type == SLAVE_TYPE_PERI2 &&
@@ -1611,7 +1623,10 @@ const char *index_to_subsys(int slave_type, uint32_t vio_index,
 				 vio_addr <= GCE_M2_END_ADDR))
 			return "GCE";
 
-		return mt6885_devices_peri2[vio_index].device;
+		for (i = 0; i < VIO_SLAVE_NUM_PERI2; i++) {
+			if (vio_index == mt6885_devices_peri2[i].vio_index)
+				return mt6885_devices_peri2[i].device;
+		}
 	}
 
 	return "OUT_OF_BOUND";
