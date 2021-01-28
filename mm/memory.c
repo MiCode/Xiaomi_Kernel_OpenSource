d7da9d9a1cd5635ab7cd80bc2a60bcf95094b7c6
Chinwen Chang <chinwen.chang@mediatek.com>
2021-01-29 00:50:58 +0800

    [ALPS04721022] FROMLIST: mm: introduce __lru_cache_add_active_or_unevictable
    (from https://lore.kernel.org/patchwork/patch/906228/)
    
    The speculative page fault handler which is run without holding the
    mmap_sem is calling lru_cache_add_active_or_unevictable() but the vm_flags
    is not guaranteed to remain constant.
    Introducing __lru_cache_add_active_or_unevictable() which has the vma flags
    value parameter instead of the vma pointer.
    
    Acked-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Laurent Dufour <ldufour@linux.vnet.ibm.com>
    
    MTK-Commit-Id: b8332709c7560286a018bc004b266c214327a079
    
    Change-Id: I88d7efca3ce9a55470d8bcdfc23aefe39d06cba2
    Signed-off-by: Chinwen Chang <chinwen.chang@mediatek.com>
    CR-Id: ALPS04721022
    Feature: Memory Optimization

--- mm/memory.c
+++ mm/memory.c
@@ -2573,7 +2573,7 @@ static int wp_page_copy(struct vm_fault *vmf)
 		ptep_clear_flush_notify(vma, vmf->address, vmf->pte);
 		page_add_new_anon_rmap(new_page, vma, vmf->address, false);
 		mem_cgroup_commit_charge(new_page, memcg, false, false);
-		lru_cache_add_active_or_unevictable(new_page, vma);
+		__lru_cache_add_active_or_unevictable(new_page, vmf->vma_flags);
 		/*
 		 * We call the notify macro here because, when using secondary
 		 * mmu page tables (such as kvm shadow page tables), we want the
@@ -3082,7 +3082,7 @@ int do_swap_page(struct vm_fault *vmf)
 	} else { /* ksm created a completely new copy */
 		page_add_new_anon_rmap(page, vma, vmf->address, false);
 		mem_cgroup_commit_charge(page, memcg, false, false);
-		lru_cache_add_active_or_unevictable(page, vma);
+		__lru_cache_add_active_or_unevictable(page, vmf->vma_flags);
 	}
 
 	swap_free(entry);
@@ -3228,7 +3228,7 @@ static int do_anonymous_page(struct vm_fault *vmf)
 	inc_mm_counter_fast(vma->vm_mm, MM_ANONPAGES);
 	page_add_new_anon_rmap(page, vma, vmf->address, false);
 	mem_cgroup_commit_charge(page, memcg, false, false);
-	lru_cache_add_active_or_unevictable(page, vma);
+	__lru_cache_add_active_or_unevictable(page, vmf->vma_flags);
 setpte:
 	set_pte_at(vma->vm_mm, vmf->address, vmf->pte, entry);
 
@@ -3505,7 +3505,7 @@ int alloc_set_pte(struct vm_fault *vmf, struct mem_cgroup *memcg,
 		inc_mm_counter_fast(vma->vm_mm, MM_ANONPAGES);
 		page_add_new_anon_rmap(page, vma, vmf->address, false);
 		mem_cgroup_commit_charge(page, memcg, false, false);
-		lru_cache_add_active_or_unevictable(page, vma);
+		__lru_cache_add_active_or_unevictable(page, vmf->vma_flags);
 	} else {
 		inc_mm_counter_fast(vma->vm_mm, mm_counter_file(page));
 		page_add_file_rmap(page, false);
