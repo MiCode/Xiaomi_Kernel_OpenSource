53e6e1fa1469558cb9db28ddd6e15fd4e5af3b91
Pangyen Chen <pangyen.chen@mediatek.com>
2021-01-28 19:50:22 +0800

    [ALPS05063810] Trustonic TEE: Solve build error due to clang
    GCC version should not be used as the criteria for building since
    toolchain has been switched to Clang. We removed judgement of
    GCC version in order to solve build error.
    
    MTK-Commit-Id: d71376bdf81de33ab7f46a8d348d8b12a05e2931
    
    Change-Id: I15a78c0616c3a10cc63a88d4164cd58ab7ede8cb
    Signed-off-by: Pangyen Chen <pangyen.chen@mediatek.com>
    CR-Id: ALPS05063810
    Feature: Trustonic TEE (Trusted Execution Environment)

--- drivers/tee/gud/410/MobiCoreDriver/fastcall.c
+++ drivers/tee/gud/410/MobiCoreDriver/fastcall.c
@@ -16,14 +16,8 @@
 #include "fastcall.h"
 
 /* Use the arch_extension sec pseudo op before switching to secure world */
-#if defined(__GNUC__) && \
-	defined(__GNUC_MINOR__) && \
-	defined(__GNUC_PATCHLEVEL__) && \
-	((__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)) \
-	>= 40502
 #ifndef CONFIG_ARM64
 #define MC_ARCH_EXTENSION_SEC
-#endif
 #endif
 
 /* Base for all fastcalls, do not use outside of other structs */
