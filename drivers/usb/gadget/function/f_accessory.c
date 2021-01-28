06458b4c43212e1fd9ae737f9a756b5e3693306d
Eddie Hung <eddie.hung@mediatek.com>
2021-01-29 00:44:32 +0800

    [ALPS05083413] USB: f_accessory: Fix crash if driver failed to initialize
    If accessory driver setup fails then _acc_dev points to
    freed memory and later drivers crashes on disconnect
    due to invalid memory access. Mark _acc_dev only if
    acc_setup is successful to prevent the crash.
    
    MTK-Commit-Id: 187a9f28c25c19499f202351f7862869822cd64c
    
    Change-Id: Ica933a7cf710ff082c54db77f94f802b6832bedb
    Signed-off-by: Manu Gautam <mgautam@codeaurora.org>
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>
    Signed-off-by: Eddie Hung <eddie.hung@mediatek.com>
    CR-Id: ALPS05083413
    Feature: [Android Default] ADB (Android Debug Bridge)

--- drivers/usb/gadget/function/f_accessory.c
+++ drivers/usb/gadget/function/f_accessory.c
@@ -1214,12 +1214,12 @@ static int acc_setup(void)
 	INIT_DELAYED_WORK(&dev->start_work, acc_start_work);
 	INIT_WORK(&dev->hid_work, acc_hid_work);
 
-	/* _acc_dev must be set before calling usb_gadget_register_driver */
-	_acc_dev = dev;
-
 	ret = misc_register(&acc_device);
 	if (ret)
 		goto err;
+
+	/* _acc_dev must be set before calling usb_gadget_register_driver */
+	_acc_dev = dev;
 
 	return 0;
 
