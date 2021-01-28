17956e3487f9e0d29adf830737c50387d162fb6c
Adogu Huang <adogu.huang@mediatek.com>
2021-01-29 02:20:30 +0800

    [ALPS05271225] isp p1: add and modify the ioctl
    Add and modify the new ones as below:
    ISP_CMD_ION_MAP_PA
    ISP_CMD_ION_UNMAP_PA
    ISP_CMD_ION_MAP_PA_CHK_ION_TBL
    ISP_CMD_ION_UNMAP_PA_CHK_ION_TBL
    ISP_CMD_ION_UNMAP_PA_BY_MODULE
    
    MTK-Commit-Id: 8d0f6bfc4c5e95bd55fba016ae50c0cfdb384271
    
    Change-Id: I5e26a3c28d3cce1c3faf1a0985004f78a20b690a
    CR-Id: ALPS05271225
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: Adogu Huang <adogu.huang@mediatek.com>

--- drivers/misc/mediatek/cameraisp/src/mt6885/inc/camera_isp.h
+++ drivers/misc/mediatek/cameraisp/src/mt6885/inc/camera_isp.h
@@ -406,6 +406,7 @@ struct ISP_DEV_ION_NODE_STRUCT {
 	unsigned int       devNode;
 	enum ISP_WRDMA_ENUM     dmaPort;
 	int                memID;
+	unsigned long long dma_pa;
 };
 
 struct ISP_LARB_MMU_STRUCT {
@@ -683,7 +684,14 @@ enum ISP_CMD_ENUM {
 	SV_CMD_GET_SUPPORTED_ISP_CLOCKS,
 	ISP_CMD_SET_SEC_DAPC_REG,
 	ISP_CMD_GET_CUR_HWP1DONE,
-	ISP_CMD_NOTE_CQTHR0_BASE
+	ISP_CMD_NOTE_CQTHR0_BASE,
+	ISP_CMD_ION_MAP_PA, /* AOSP ION: map physical address from fd */
+	ISP_CMD_ION_UNMAP_PA, /* AOSP ION: unmap physical address from fd */
+	/* AOSP ION: map physical address from fd and check the ion table. */
+	ISP_CMD_ION_MAP_PA_CHK_ION_TBL,
+	/* AOSP ION: unmap physical address from fd and check the ion table. */
+	ISP_CMD_ION_UNMAP_PA_CHK_ION_TBL,
+	ISP_CMD_ION_UNMAP_PA_BY_MODULE
 };
 
 enum ISP_HALT_DMA_ENUM {
@@ -815,6 +823,21 @@ enum ISP_HALT_DMA_ENUM {
 
 #define ISP_ION_FREE_BY_HWMODULE                 \
 	_IOW(ISP_MAGIC, ISP_CMD_ION_FREE_BY_HWMODULE, unsigned int)
+
+#define ISP_ION_MAP_PA                      \
+	_IOWR(ISP_MAGIC, ISP_CMD_ION_MAP_PA, struct ISP_DEV_ION_NODE_STRUCT)
+
+#define ISP_ION_UNMAP_PA                      \
+	_IOWR(ISP_MAGIC, ISP_CMD_ION_UNMAP_PA, struct ISP_DEV_ION_NODE_STRUCT)
+
+#define ISP_ION_MAP_PA_CHK_ION_TBL               \
+	_IOW(ISP_MAGIC, ISP_CMD_ION_MAP_PA_CHK_ION_TBL, struct ISP_DEV_ION_NODE_STRUCT)
+
+#define ISP_ION_UNMAP_PA_CHK_ION_TBL             \
+	_IOW(ISP_MAGIC, ISP_CMD_ION_UNMAP_PA_CHK_ION_TBL, struct ISP_DEV_ION_NODE_STRUCT)
+
+#define ISP_ION_UNMAP_PA_BY_HWMODULE             \
+	_IOW(ISP_MAGIC, ISP_CMD_ION_UNMAP_PA_BY_MODULE, struct ISP_DEV_ION_NODE_STRUCT)
 
 #define ISP_CQ_SW_PATCH                          \
 	_IOW(ISP_MAGIC, ISP_CMD_CQ_SW_PATCH, struct ISP_MULTI_RAW_CONFIG)
