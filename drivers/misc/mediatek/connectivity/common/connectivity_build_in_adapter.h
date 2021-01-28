aec71f83b09740588d613b493f81959cc70122b2
Christina Wu <christina.wu@mediatek.com>
2021-01-29 02:16:41 +0800

    [ALPS05197198] conninfra: Porting conninfra for MT6893
    Add defined(CONFIG_MACH_MT6893) for KERNEL_clk_buf_ctrl
    
    MTK-Commit-Id: b46ecca655822513e42da7f638c02857ef63d135
    
    Change-Id: I96a2385d8fc1061397de741e43c6bdf0bc89254c
    Signed-off-by: Christina Wu <christina.wu@mediatek.com>
    CR-Id: ALPS05197198
    Feature: [Module]Conninfra Driver

--- drivers/misc/mediatek/connectivity/common/connectivity_build_in_adapter.h
+++ drivers/misc/mediatek/connectivity/common/connectivity_build_in_adapter.h
@@ -61,7 +61,8 @@
 	defined(CONFIG_MACH_KIBOPLUS) || \
 	defined(CONFIG_MACH_MT6885) || \
 	defined(CONFIG_MACH_MT6873) || \
-	defined(CONFIG_MACH_ELBRUS)
+	defined(CONFIG_MACH_ELBRUS) || \
+	defined(CONFIG_MACH_MT6893)
 #define CONNADP_HAS_CLOCK_BUF_CTRL
 #define KERNEL_CLK_BUF_CHIP_NOT_SUPPORT -7788
 #define KERNEL_clk_buf_ctrl connectivity_export_clk_buf_ctrl
