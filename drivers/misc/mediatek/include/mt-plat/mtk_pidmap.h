27cd3b8b96c6e70f6fa9d7def5a40b194fc405be
Stanley Chu <stanley.chu@mediatek.com>
2021-01-28 19:46:14 +0800

    [ALPS04362512] pidmap: introduce pidmap with loadable KO builtable
    Introduce pidmap module which can be built as loadable kernel
    module.
    
    pidmap is a thread name recorder to provide pid and thread name
    mappings which helps debugging. Whenever a thread is created or
    renamed, its name is updated in the map in pidmap module.
    
    Note that more memory will be required for this feature, so please
    consider the trade-off.
    
    MTK-Commit-Id: 9bcbbb53bbb6f0f2270be5d024779663de50ada6
    
    Change-Id: I0cd740bc7e7b7c33b9123f735ca855a8b803456b
    Signed-off-by: Stanley Chu <stanley.chu@mediatek.com>
    CR-Id: ALPS04362512
    Feature: Android Exception Engine(AEE)

---
+++ drivers/misc/mediatek/include/mt-plat/mtk_pidmap.h
@@ -0,0 +1,50 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2019 MediaTek Inc.
+ */
+
+#ifndef _MTK_PIDMAP_H
+#define _MTK_PIDMAP_H
+
+#include <linux/types.h>
+#include <linux/sched.h>
+
+/* buffer size */
+#define PIDMAP_AEE_BUF_SIZE       (512 * 1024)
+#define PIDMAP_PROC_CMD_BUF_SIZE  (1)
+
+/* raw file format, unit: bytes */
+#define PIDMAP_TGID_SIZE          (sizeof(uint16_t))
+
+/* mtk_pidmap_struct */
+#define PIDMAP_ENTRY_SIZE         (16)
+#define PIDMAP_ENTRY_CNT	\
+	(PIDMAP_AEE_BUF_SIZE / PIDMAP_ENTRY_SIZE)
+#define PIDMAP_TASKNAME_SIZE \
+	(PIDMAP_ENTRY_SIZE - PIDMAP_TGID_SIZE)
+
+enum PIDMAP_PROC_MODE {
+	PIDMAP_PROC_DUMP_RAW      = 0,
+	PIDMAP_PROC_DUMP_READABLE = 1,
+};
+
+#ifndef USER_BUILD_KERNEL
+#define PIDMAP_PROC_PERM          (0660)
+#else
+#define PIDMAP_PROC_PERM          (0440)
+#endif
+
+#ifdef CONFIG_MTK_PID_MAP
+extern void get_pidmap_aee_buffer(unsigned long *vaddr, unsigned long *size);
+#else
+static inline void get_pidmap_aee_buffer(unsigned long *vaddr,
+					 unsigned long *size)
+{
+	/* return valid buffer size */
+	if (size)
+		*size = 0;
+}
+#endif
+
+#endif /* _MTK_PIDMAP_H */
+
