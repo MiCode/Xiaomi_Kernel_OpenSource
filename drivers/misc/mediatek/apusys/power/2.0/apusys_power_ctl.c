2a6d65b3dc3329861768fd1d078dc680d61577f4
Pin Fan <pin.fan@mediatek.com>
2021-01-29 02:22:58 +0800

    [ALPS05269522] apusys power: refine segment code
    [Detail]
    split two segment config to distinguish 6891 and 6893.
    SEGMENT_0 - mt6891, dvfs_table_b0
    SEGMENT_1 - mt6893, dvfs_table_b1
    
    MTK-Commit-Id: aba46e51aaffcbc03c818abdc28fd2bd35bc00c8
    
    Change-Id: Ic14ac2aebace21005513e082286f3062878b6704
    CR-Id: ALPS05269522
    Feature: [Module] APUSYS
    Signed-off-by: Pin Fan <pin.fan@mediatek.com>

--- drivers/misc/mediatek/apusys/power/2.0/apusys_power_ctl.c
+++ drivers/misc/mediatek/apusys/power/2.0/apusys_power_ctl.c
@@ -1113,8 +1113,10 @@ int apusys_power_init(enum DVFS_USER user, void *init_power_data)
 		goto out;
 
 #if defined(CONFIG_MACH_MT6893)
-	// TODO: segment judgement
-	apusys_opps.opps = dvfs_table_3;
+	if (seg_data.seg == SEGMENT_0)
+		apusys_opps.opps = dvfs_table_b0;
+	else
+		apusys_opps.opps = dvfs_table_b1;
 #else
 	if (seg_data.seg == SEGMENT_0)
 		apusys_opps.opps = dvfs_table_0;
