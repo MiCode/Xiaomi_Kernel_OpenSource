6bcf9fc8a6047353edcdd15a474572acd93944bb
Shane.Chien <shane.chien@mediatek.com>
2021-01-29 03:18:38 +0800

    [ALPS05527699] ASoC: fix log typo
    fix log typo
    
    MTK-Commit-Id: f76d67c494c55115741cdb4a77c11dca323f884e
    
    Change-Id: I053e89c4a784d16c61d284555504c8c4e70a85c2
    Signed-off-by: Shane.Chien <shane.chien@mediatek.com>
    CR-Id: ALPS05527699
    Feature: [Module]Audio Kernel

--- sound/soc/codecs/mt6359.c
+++ sound/soc/codecs/mt6359.c
@@ -7548,7 +7548,7 @@ static int mt6359_parse_dt(struct mt6359_priv *priv)
 	ret = of_property_read_u32(dev->of_node, "mediatek,dmic-mode",
 				   &priv->dmic_one_wire_mode);
 	if (ret) {
-		dev_info(dev, "%s() failed to read dmic-mode, default 1 wire\n",
+		dev_info(dev, "%s() failed to read dmic-mode, default 2 wire\n",
 			 __func__);
 		priv->dmic_one_wire_mode = 0;
 	}
