33a700e45ca96c2715954efa3de8a052465c9c06
Shane Chien <Shane.Chien@mediatek.com>
2021-01-29 00:41:49 +0800

    [ALPS04292294] ASoC: mt6885 audio driver configuration
    1. Add NXP/MTK SmartPA kconfig definition.
    2. Add audio related kconfig in correct_config.csv,
       aligned with setting in alps-mp-p0.mp9.
    3. Add mck phase invertion configuration for i2s6 to i2s9.
    
    MTK-Commit-Id: 529278fc5e77b3bbcbcb115ea5cb0d4966653c86
    
    Change-Id: I2f3e1aa602485d29f6f17f055c4fba715f5076c1
    Signed-off-by: Shane Chien <Shane.Chien@mediatek.com>
    CR-Id: ALPS04292294
    Feature: DSP SmartPA

--- sound/soc/mediatek/mt6885/mt6885-mt6359.c
+++ sound/soc/mediatek/mt6885/mt6885-mt6359.c
@@ -26,7 +26,8 @@
 
 static const char *const mt6885_spk_type_str[] = {MTK_SPK_NOT_SMARTPA_STR,
 						  MTK_SPK_RICHTEK_RT5509_STR,
-						  MTK_SPK_MEDIATEK_MT6660_STR
+						  MTK_SPK_MEDIATEK_MT6660_STR,
+						  MTK_SPK_NXP_TFA98XX_STR
 						  };
 static const char *const mt6885_spk_i2s_type_str[] = {MTK_SPK_I2S_0_STR,
 						      MTK_SPK_I2S_1_STR,
