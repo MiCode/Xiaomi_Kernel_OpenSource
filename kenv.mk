b4304c2c10c7518515d3a2e105cc3e15525d49d4
Miles Chen <miles.chen@mediatek.com>
2021-01-28 15:42:12 +0800

    [ALPS04963808] kernel: move arm CC to clang-r370808
    Move arm CC to clang-r370808.
    
    MTK-Commit-Id: 38366c4a04ae4cd4d72e9aed82d3b265981509c7
    
    CR-Id: ALPS04963808
    Change-Id: I2a5309d1ffb71999f0e063c8bebe1cb433b1d345
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    Feature: [Module]Kernel Maintenance

--- kenv.mk
+++ kenv.mk
@@ -34,6 +34,11 @@ ifneq ($(strip $(TARGET_NO_KERNEL)),true)
     TARGET_KERNEL_CROSS_COMPILE ?= $(KERNEL_ROOT_DIR)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel-
     TARGET_KERNEL_CLANG_COMPILE :=
     CC := $(TARGET_KERNEL_CROSS_COMPILE)gcc
+    ifeq ($(strip $(TARGET_KERNEL_USE_CLANG)),true)
+      CLANG_PATH := $(KERNEL_ROOT_DIR)/prebuilts/clang/host/linux-x86/clang-r370808
+      TARGET_KERNEL_CLANG_COMPILE := CLANG_TRIPLE=arm-linux-gnueabi-
+      CC := $(CLANG_PATH)/bin/clang
+    endif
   endif
 
   ifneq ($(filter-out false,$(USE_CCACHE)),)
