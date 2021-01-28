424d8b0bef5c4ca0a84fda52794b7750a5ab895b
Johnson Lin <johnson.lin@mediatek.com>
2021-01-28 20:28:06 +0800

    [ALPS04292830] mrdump_key: restore when shutdown
    [Detail]
    if mrdump_key is enabled with SYSRST
    when power off, PMIC will send SYSRST to RGU
    but RGU will recognize it as MRDUMP_KEY
    So we need to restore to RST mode before reboot
    by PMIC with SYSRST signal
    
    MTK-Commit-Id: 7d9fc97bd2e6d661a293d6b70a8445311e2d39eb
    
    Change-Id: I5b0e528e2629c670931a856a6810dae0a67b7744
    Signed-off-by: Johnson Lin <johnson.lin@mediatek.com>
    CR-Id: ALPS04292830
    Feature: Memory RAM Dump (MRDUMP)
    (cherry-pick from 129b9fb4e81917a99e59e07251f46fdb6762b09d)

--- drivers/misc/mediatek/aee/mrdump/mrdump_key_setup.c
+++ drivers/misc/mediatek/aee/mrdump/mrdump_key_setup.c
@@ -11,8 +11,18 @@
  * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
 
+#include <linux/module.h>
+#include <linux/file.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/miscdevice.h>
+#include <linux/platform_device.h>
 #include <linux/kconfig.h>
 #include <linux/module.h>
+#include <linux/io.h>           /* ioremap() */
+#include <linux/of_fdt.h>
+#include <linux/of_reserved_mem.h>
+#include <linux/of_irq.h>
 #ifdef CONFIG_OF
 #include <linux/of.h>
 #endif
@@ -33,7 +43,12 @@ enum MRDUMP_LONG_PRESS_MODE {
 	LONG_PRESS_SHUTDOWN};
 #endif
 
-static int __init mrdump_key_init(void)
+static const struct of_device_id mrdump_key_of_ids[] = {
+	{ .compatible = "mediatek, mrdump_ext_rst-eint", },
+	{}
+};
+
+static int __init mrdump_key_probe(struct platform_device *pdev)
 {
 #ifdef CONFIG_MTK_WATCHDOG_COMMON
 	int res;
@@ -50,7 +65,7 @@ static int __init mrdump_key_init(void)
 	struct device_node *node;
 	const char *source_str, *interrupts;
 	char node_name[] = "mediatek, mrdump_ext_rst-eint";
-
+	pr_notice("%s:%d\n", __func__, __LINE__);
 	node = of_find_compatible_node(NULL, NULL, node_name);
 	if (!node) {
 		pr_notice("MRDUMP_KEY:node %s is not exist\n", node_name);
@@ -134,5 +149,77 @@ out:
 	return 0;
 }
 
+static void mrdump_key_shutdown(struct platform_device *pdev)
+{
+
+#ifdef CONFIG_MTK_WATCHDOG_COMMON
+	int res;
+	struct wd_api *wd_api = NULL;
+#endif
+
+#ifdef CONFIG_MTK_PMIC_COMMON
+	pr_notice("restore pmic long_press_mode = SHUTDOWN\n");
+	pmic_enable_smart_reset(0, 0);
+#endif
+
+#ifdef CONFIG_MTK_WATCHDOG_COMMON
+	pr_notice("restore RGU to default value\n");
+	res = get_wd_api(&wd_api);
+	if (res < 0)
+		pr_notice("%s: get_wd_api failed:%d\n", __func__, res);
+	else {
+		res = wd_api->wd_debug_key_eint_config(0, WD_REQ_RST_MODE);
+		if (res == -1)
+			pr_notice("%s: disable EINT failed\n", __func__);
+		else
+			pr_notice("%s:disable EINT mode\n", __func__);
+		res = wd_api->wd_debug_key_sysrst_config(0, WD_REQ_RST_MODE);
+		if (res == -1)
+			pr_notice("%s: disable SYSRST failed\n", __func__);
+		else
+			pr_notice("%s:disable SYSRST OK\n", __func__);
+	}
+#endif
+}
+
+static void __exit mrdump_key_exit(void)
+{
+	mrdump_key_shutdown(NULL);
+}
+static int mrdump_key_remove(struct platform_device *dev)
+{
+	mrdump_key_shutdown(NULL);
+	return 0;
+}
+
+/* variable with __init* or __refdata (see linux/init.h) or */
+/* name the variable *_template, *_timer, *_sht, *_ops, *_probe, */
+/* *_probe_one, *_console */
+static struct platform_driver mrdump_key_driver_probe = {
+	.probe = mrdump_key_probe,
+	.shutdown = mrdump_key_shutdown,
+	.remove = mrdump_key_remove,
+	.driver = {
+		.name = "mrdump_key",
+		.owner = THIS_MODULE,
+#ifdef CONFIG_OF
+		.of_match_table = mrdump_key_of_ids,
+#endif
+	},
+};
+
+static int __init mrdump_key_init(void)
+{
+	int ret = 0;
+
+	ret = platform_driver_register(&mrdump_key_driver_probe);
+	if (ret)
+		pr_err("mrdump_key init FAIL, ret 0x%x!!!\n", ret);
+
+	return ret;
+}
+
+
 module_init(mrdump_key_init);
+module_exit(mrdump_key_exit);
 
