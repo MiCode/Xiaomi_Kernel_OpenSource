7c7d97f833364f85f3166fa3fb1385230e66520b
Chang-An Chen <chang-an.chen@mediatek.com>
2021-01-28 16:10:23 +0800

    [ALPS04956764] dt-bindings: reset: add binding for MT6761 reset controller
    Add the reset controller dt-bindings for reset source control.
    To be used from device tree files.
    
    MTK-Commit-Id: 00b324663665b9605699db68aded91aeb4febd9b
    
    Change-Id: I2e72956c682a92d27bddb780f5933f2d1bb6bcc4
    Signed-off-by: Chang-An Chen <chang-an.chen@mediatek.com>
    CR-Id: ALPS04956764
    Feature: [Module]RGU (Reset Generator Unit)

---
+++ include/dt-bindings/reset/mt6761-resets.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#ifndef _DT_BINDINGS_RESET_CONTROLLER_MT6761
+#define _DT_BINDINGS_RESET_CONTROLLER_MT6761
+
+/* Resource Sources */
+#define MT6761_SRC_SPM_THERMAL_RST      0
+#define MT6761_SRC_SPM_RST              1
+#define MT6761_SRC_EINT_RST             2
+#define MT6761_SRC_SYSRST_RST           3
+#define MT6761_SRC_DVFSP_RST            4
+#define MT6761_SRC_SSPM_RST             16
+#define MT6761_SRC_MDDBG_RST            17
+#define MT6761_SRC_THERMAL_RST          18
+#define MT6761_SRC_DEBUGSYS_RST         19
+
+#endif  /* _DT_BINDINGS_RESET_CONTROLLER_MT6761 */
+
