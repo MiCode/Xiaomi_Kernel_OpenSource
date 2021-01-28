3bac0404edd68a19ac4e5396e27a71787ce4429e
Juju Sung <juju.sung@mediatek.com>
2021-01-29 02:13:52 +0800

    [ALPS05111942] gpufreq: mt_gpufreq_power_table_info is invisible
    [Detail]
    mtk_thermal.h:struct mt_gpufreq_power_table_info will not be visible
    outside of this function.
    This patch expose gpu_freq header from mt6873/mt6853
    platform folder.
    
    MTK-Commit-Id: 453d4bbecb8ac514862b688d14953fc6f299f3ef
    
    Change-Id: I2fddfe5b82b7e42f2f33eb518871a56c85ba0300
    Signed-off-by: Juju Sung <juju.sung@mediatek.com>
    CR-Id: ALPS05111942
    Feature: [Module]Kernel Maintenance

--- drivers/misc/mediatek/base/power/include/gpufreq_v1/mtk_gpufreq_internal.h
+++ drivers/misc/mediatek/base/power/include/gpufreq_v1/mtk_gpufreq_internal.h
@@ -17,6 +17,12 @@
 #if defined(CONFIG_MACH_MT6885)
 #include "mt6885/mtk_gpufreq_internal_plat.h"
 
+#elif defined(CONFIG_MACH_MT6873)
+#include "mt6873/mtk_gpufreq_internal_plat.h"
+
+#elif defined(CONFIG_MACH_MT6853)
+#include "mt6853/mtk_gpufreq_internal_plat.h"
+
 #endif
 
 #endif /* __MTK_GPUFREQ_INTERNAL_H__ */
