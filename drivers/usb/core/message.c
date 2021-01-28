751b64156f034a8e61e0525e85139b014c18bc6d
Eddie Hung <eddie.hung@mediatek.com>
2021-01-29 02:21:36 +0800

    [ALPS05322520] sound: usb: add SET_ALT control transfer timeout
    In case a of a usb head phone not responding to SET_ALT driver
    context is blocked for 5 sec. This is resulting into a bad user
    experience as audio subsystem is not able to route audio path for
    5 sec. Hence use usb_set_interface_timeout() API and pass 1 sec
    as timeout value.
    
    Reference patchs form https://source.codeaurora.org
    usb: core: Add usb_set_interface_timeout API
    sound: usb: Set SET_ALT control transfer timeout as 1 sec
    
    MTK-Commit-Id: 90cb913770335eb04753bed068ecb879cc352dda
    
    Change-Id: Ic28111b1e2ec092e722664e419b85a646e6938c5
    Signed-off-by: Eddie Hung <eddie.hung@mediatek.com>
    CR-Id: ALPS05322520
    Feature: [Android Default] USB OTG

--- drivers/usb/core/message.c
+++ drivers/usb/core/message.c
@@ -1301,7 +1301,8 @@ void usb_enable_interface(struct usb_device *dev,
  * Return: Zero on success, or else the status code returned by the
  * underlying usb_control_msg() call.
  */
-int usb_set_interface(struct usb_device *dev, int interface, int alternate)
+int usb_set_interface_timeout(struct usb_device *dev, int interface,
+	int alternate, unsigned long timeout_ms)
 {
 	struct usb_interface *iface;
 	struct usb_host_interface *alt;
@@ -1365,7 +1366,7 @@ int usb_set_interface(struct usb_device *dev, int interface, int alternate)
 	else
 		ret = usb_control_msg(dev, usb_sndctrlpipe(dev, 0),
 				   USB_REQ_SET_INTERFACE, USB_RECIP_INTERFACE,
-				   alternate, interface, NULL, 0, 5000);
+				   alternate, interface, NULL, 0, timeout_ms);
 
 	/* 9.4.10 says devices don't need this and are free to STALL the
 	 * request if the interface only has one alternate setting.
@@ -1436,6 +1437,12 @@ int usb_set_interface(struct usb_device *dev, int interface, int alternate)
 		create_intf_ep_devs(iface);
 	}
 	return 0;
+}
+EXPORT_SYMBOL_GPL(usb_set_interface_timeout);
+
+int usb_set_interface(struct usb_device *dev, int interface, int alternate)
+{
+	return usb_set_interface_timeout(dev, interface, alternate, 5000);
 }
 EXPORT_SYMBOL_GPL(usb_set_interface);
 
