ec33caec5fc9a0abc5080c374760a950fa7f7a7f
Kamal Sharma <kamal.sharma@mediatek.com>
2021-01-29 02:21:03 +0800

    [ALPS04989597] DISP: fix coverity issue
    Initialize variable before use
    
    MTK-Commit-Id: d6b7e8ee2ba82140edb67a3cd8d97efcebc216ab
    
    Change-Id: I33a78dacfc9594a9cfcdce699e843b2ae6ca470f
    Signed-off-by: Kamal Sharma <kamal.sharma@mediatek.com>
    CR-Id: ALPS04989597
    Feature: [Module]Display Driver

--- drivers/misc/mediatek/video/mt6768/dispsys/ddp_ovl.c
+++ drivers/misc/mediatek/video/mt6768/dispsys/ddp_ovl.c
@@ -1032,7 +1032,7 @@ int ovl_switch_to_nonsec(enum DISP_MODULE_ENUM module, void *handle)
 static int setup_ovl_sec(enum DISP_MODULE_ENUM module,
 			 struct disp_ddp_path_config *pConfig, void *handle)
 {
-	int ret;
+	int ret = 0;
 	int layer_id;
 	int has_sec_layer = 0;
 	struct OVL_CONFIG_STRUCT *ovl_config;
