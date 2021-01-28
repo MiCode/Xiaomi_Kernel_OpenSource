915f8b163c76d24f96e1036627d186e64e74ccbd
Carl Fang <carl.fang@mediatek.com>
2021-01-29 02:34:43 +0800

    [ALPS05351549] ccu: fix ut devapc error
    fix ut devapc error
    
    MTK-Commit-Id: c9de137310b54f0621de62b67e8447914e4e7654
    
    Change-Id: Ie0bf14090682c8334dd516319e407890438cde54
    CR-Id: ALPS05351549
    Feature: Camera Control Unit(ccu)
    Signed-off-by: Carl Fang <carl.fang@mediatek.com>

--- drivers/misc/mediatek/ccu/src/mt6779/ccu_hw.c
+++ drivers/misc/mediatek/ccu/src/mt6779/ccu_hw.c
@@ -529,6 +529,7 @@ int ccu_power(struct ccu_power_s *power)
 
 	if (power->bON == 1) {
 		/*CCU power on sequence*/
+		ccu_clock_enable();
 
 		/*0. Set CCU_A_RESET. CCU_HW_RST=1*/
 		ccu_write_reg(ccu_base, RESET, 0xFF3FFCFF);	/*TSF be affected.*/
@@ -537,7 +538,6 @@ int ccu_power(struct ccu_power_s *power)
 		/*ccu_write_reg_bit(ccu_base, RESET, CCU_HW_RST, 1);*/
 
 		/*1. Enable CCU CAMSYS_CG_CON bit12 CCU_CGPDN=0*/
-		ccu_clock_enable();
 		LOG_DBG("CCU CG released\n");
 
 		/*use user space buffer*/
