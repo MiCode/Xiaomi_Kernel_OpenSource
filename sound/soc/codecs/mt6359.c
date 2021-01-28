3f7d397b346b3f83e15b863f7ca133511ede3f36
Eason Yen <eason.yen@mediatek.com>
2021-01-28 20:30:37 +0800

    [ALPS04960338] ASoC: mediatek: codec: fix hp-impedance formula
    [Detail]
    fix hp-impedance formula
    
    MTK-Commit-Id: 206502c129ac6235d166c665b4f659943881a2c0
    
    Change-Id: I6f7fc891b67e3e940606fd81ce374400180015c7
    Signed-off-by: Eason Yen <eason.yen@mediatek.com>
    CR-Id: ALPS04960338
    Feature: [Module]Audio Kernel

--- sound/soc/codecs/mt6359.c
+++ sound/soc/codecs/mt6359.c
@@ -5180,7 +5180,9 @@ static int mtk_calculate_impedance_formula(int pcm_offset, int aux_diff)
 	/* R = V /I */
 	/* V = auxDiff * (1800mv /auxResolution)  /TrimBufGain */
 	/* I =  pcmOffset * DAC_constant * Gsdm * Gibuf */
-	return DIV_ROUND_CLOSEST(3600000 / pcm_offset * aux_diff, 7832);
+	long val = 3600000 / pcm_offset * aux_diff;
+
+	return (int)DIV_ROUND_CLOSEST(val, 7832);
 }
 
 static int calculate_impedance(struct mt6359_priv *priv,
--- sound/soc/mediatek/codec/mt6357/mtk-soc-codec-6357.c
+++ sound/soc/mediatek/codec/mt6357/mtk-soc-codec-6357.c
@@ -1100,7 +1100,10 @@ static int mtk_calculate_impedance_formula(int pcm_offset, int aux_diff)
 	/* R = V /I */
 	/* V = auxDiff * (1800mv /auxResolution)  /TrimBufGain */
 	/* I =  pcmOffset * DAC_constant * Gsdm * Gibuf */
-	return DIV_ROUND_CLOSEST(3600000 / pcm_offset * aux_diff, 7832);
+
+	long val = 3600000 / pcm_offset * aux_diff;
+
+	return (int)DIV_ROUND_CLOSEST(val, 7832);
 }
 static int mtk_calculate_hp_impedance(int dc_init, int dc_input,
 				      short pcm_offset,
