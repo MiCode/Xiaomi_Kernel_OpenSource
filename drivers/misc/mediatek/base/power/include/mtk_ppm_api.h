8129dae36684c54aaff6b741db3c38a3dfaf6522
Juju Sung <juju.sung@mediatek.com>
2021-01-28 18:25:04 +0800

    [ALPS05014853] Lowpower: mt6761 ppm porting
    [Detail]
    add mt6765/61 platform ppm_v3 driver
    
    MTK-Commit-Id: a2ac81187e1457f98133c845b7bc6712c90099bd
    
    Change-Id: Ice22e2031eec35c2c1d3c6f7fe68f1306d2e781b
    Signed-off-by: Juju Sung <juju.sung@mediatek.com>
    CR-Id: ALPS05014853
    Feature: [Module]Kernel Maintenance

---
+++ drivers/misc/mediatek/base/power/include/mtk_ppm_api.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2016 MediaTek Inc.
+ */
+
+#ifndef _MTK_PPM_API_H_
+#define _MTK_PPM_API_H_
+
+#include "ppm_v3/mtk_ppm_api.h"
+
+#endif /* _MTK_PPM_API_H_ */
+
