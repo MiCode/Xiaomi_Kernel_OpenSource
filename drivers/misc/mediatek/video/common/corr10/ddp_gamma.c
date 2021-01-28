97a718e049417c3dcacb99ddf8d05b3ecd56d340
mtk08122 <shan.an@mediatek.com>
2021-01-29 00:07:44 +0800

    [ALPS05140932] PQ: add IOCTL for color transform
    Add DISP_IOCTL_SUPPORT_COLOR_TRANSFORM for user
    to query whether the DISP_CCORR can support color transform.
    
    MTK-Commit-Id: 0ff2a024314d1b81ba8a71a6708c3f1f6e6539cf
    
    Change-Id: I83235bf49dd3ed4408c463d57370abb230f73475
    CR-Id: ALPS05140932
    Signed-off-by: MTK08122 <Shan.An@mediatek.com>
    Feature: Display Driver (DSI Interface)

--- drivers/misc/mediatek/video/common/corr10/ddp_gamma.c
+++ drivers/misc/mediatek/video/common/corr10/ddp_gamma.c
@@ -25,6 +25,7 @@
 #include <ddp_reg.h>
 #include <ddp_path.h>
 #include <ddp_gamma.h>
+#include <ddp_pq.h>
 #include <disp_drv_platform.h>
 #if defined(CONFIG_MACH_MT6755) || defined(CONFIG_MACH_MT6797) || \
 	defined(CONFIG_MACH_MT6757) || defined(CONFIG_MACH_KIBOPLUS) || \
@@ -1051,6 +1052,8 @@ static int ccorr_ioctl(enum DISP_MODULE_ENUM module, void *handle,
 static int disp_ccorr_io(enum DISP_MODULE_ENUM module, unsigned int msg,
 	unsigned long arg, void *cmdq)
 {
+	struct DISP_COLOR_TRANSFORM color_transform;
+	unsigned int i;
 #ifdef CCORR_TRANSITION
 	int enabled;
 #endif
@@ -1085,6 +1088,23 @@ static int disp_ccorr_io(enum DISP_MODULE_ENUM module, unsigned int msg,
 			return -EFAULT;
 		}
 
+		break;
+#endif
+#if defined(CONFIG_MACH_MT6761)
+	case DISP_IOCTL_SUPPORT_COLOR_TRANSFORM:
+		if (copy_from_user(&color_transform, (void *)arg,
+			sizeof(struct DISP_COLOR_TRANSFORM))) {
+			CCORR_ERR("DISP_IOCTL_SUPPORT_COLOR_TRANSFORM: failed");
+			return -EFAULT;
+		}
+
+		for (i = 0 ; i < 3; i++) {
+			if (color_transform.matrix[3][i] != 0 ||
+				color_transform.matrix[i][3] != 0) {
+				CCORR_DBG("unsupported matrix");
+				return -EFAULT;
+			}
+		}
 		break;
 #endif
 	}
