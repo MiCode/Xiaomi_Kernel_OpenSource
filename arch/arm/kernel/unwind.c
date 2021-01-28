253dc2a96ca01082c7db8d59d58f3d9d15f9b3ab
Miles Chen <miles.chen@mediatek.com>
2021-01-29 01:19:37 +0800

    [ALPS05160479] arm: remove trapping code
    We now have a way to reproduce the issue and it looks like
    the unwind fail cases occurs when a kernel module pc and the
    pc does not exist the unwind table.
    
    We keep working on this, remove the BUG() here.
    
    MTK-Commit-Id: f02310972a359678deefd9ffce7c7f9aa7c9248d
    
    Change-Id: I0a2a9bc46525f28cb65a7c181a59d99ad249b56d
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    CR-Id: ALPS05160479
    Feature: [Module]Kernel Maintenance

--- arch/arm/kernel/unwind.c
+++ arch/arm/kernel/unwind.c
@@ -403,8 +403,8 @@ int unwind_frame(struct stackframe *frame)
 
 	idx = unwind_find_idx(frame->pc);
 	if (!idx) {
-		pr_warn("unwind: Index not found %08lx\n", frame->pc);
-		BUG();
+		/* tmp: remove this message */
+		/* pr_warn("unwind: Index not found %08lx\n", frame->pc); */
 		return -URC_FAILURE;
 	}
 
