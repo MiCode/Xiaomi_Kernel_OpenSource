740a797632c2c9145604fe3051e0063a54934522
Chinwen Chang <chinwen.chang@mediatek.com>
2021-01-29 00:50:57 +0800

    [ALPS04721022] FROMLIST: mm: cache some VMA fields in the vm_fault structure
    (from https://lore.kernel.org/patchwork/patch/906227/)
    
    MTK-Commit-Id: 9e82ebbede31b628bf10d7e877a451505ad54e06
    
    Change-Id: I3f2ba85b1b1cc0a67cd2a8dbd29570d1801f81f9
    Signed-off-by: Chinwen Chang <chinwen.chang@mediatek.com>
    CR-Id: ALPS04721022
    Feature: Memory Optimization

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -3726,6 +3726,8 @@ retry:
 				.vma = vma,
 				.address = address,
 				.flags = flags,
+				.vma_flags = vma->vm_flags,
+				.vma_page_prot = vma->vm_page_prot,
 				/*
 				 * Hard to debug if it ends up being
 				 * used by a callee that assumes
