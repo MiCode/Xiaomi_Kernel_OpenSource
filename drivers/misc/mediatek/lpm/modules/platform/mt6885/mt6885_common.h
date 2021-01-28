6f7d53eb4063623bd22cd8c8bc1e0b2292671558
claude.yen <claude.yen@mediatek.com>
2021-01-29 01:03:06 +0800

    [ALPS05052466] lpm: sync mt6885 lpm platform code
    sync mt6885 lpm platform code
    
    MTK-Commit-Id: 1c49807516b5df69aa61211cc790bbfaccf760ca
    
    Change-Id: I4430501db3f0f72cb392e83b6c70bf21794301d7
    Signed-off-by: claude.yen <claude.yen@mediatek.com>
    CR-Id: ALPS05052466
    Feature: Power Management

---
+++ drivers/misc/mediatek/lpm/modules/platform/mt6885/mt6885_common.h
@@ -0,0 +1,30 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#ifndef __MT6885_COMMON_H__
+#define __MT6885_COMMON_H__
+
+#include <linux/delay.h>
+
+#include <mtk_lpm_type.h>
+
+/* Platform pwr state */
+#define PLAT_XO_UFS_OFF			(1<<1L)
+#define PLAT_CLKBUF_ENTER_BBLPM		(1<<2L)
+
+#define PLAT_SUSPEND			(1<<8L)
+#define PLAT_MCUSYS_PROTECTED		(1<<9L)
+#define PLAT_CLUSTER_PROTECTED		(1<<10L)
+#define PLAT_VCORE_LP_MODE		(1<<11L)
+#define PLAT_PMIC_VCORE_SRCLKEN0	(1<<12L)
+#define PLAT_PMIC_VCORE_SRCLKEN2	(1<<13L)
+
+#define PLAT_MAINPLL_OFF		(1<<27L)
+#define PLAT_AP_WDT_RST_MODE		(1<<28L)
+#define PLAT_MCUSYSOFF_PREPARED		(1<<29L)
+#define PLAT_GIC_MASKED			(1<<30L)
+#define PLAT_PROTECTED_FAIL		(1<<31L)
+
+#endif
