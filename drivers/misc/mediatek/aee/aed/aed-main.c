3fcfe21b959e1c9368f4c700252ff5fb5f4aba0a
Huaiming Li <huaiming.li@mediatek.com>
2021-01-29 00:56:31 +0800

    [ALPS04754550] aee: fix msg_destroy issue
    [Detail]
    if one thread vfree *ppmsg but not to set it null, and the same time,
    other thread may get the *ppmsg and judge the adrress is not null,
    so the thread will also vfree the *ppmsg. Finally, the issue will occur
    
    [Solution]
    set a tmp var to get the value of *ppmsg and set the *ppmsg to null
    firstly, then vfree tmp var
    
    MTK-Commit-Id: 59bd47ae3b5ec8e1b5e4750542734777d6921a24
    
    Change-Id: I08802806434bb4c1376b9bad2e40c69dda8e21ce
    Signed-off-by: Huaiming Li <huaiming.li@mediatek.com>
    CR-Id: ALPS04754550
    Feature: Android Exception Engine(AEE)

--- drivers/misc/mediatek/aee/aed/aed-main.c
+++ drivers/misc/mediatek/aee/aed/aed-main.c
@@ -224,9 +224,10 @@ static struct aed_dev aed_dev;
 
 inline void msg_destroy(char **ppmsg)
 {
+	char *ppmsg_tmp = *ppmsg;
 	if (*ppmsg != NULL) {
-		vfree(*ppmsg);
 		*ppmsg = NULL;
+		vfree(ppmsg_tmp);
 	}
 }
 
