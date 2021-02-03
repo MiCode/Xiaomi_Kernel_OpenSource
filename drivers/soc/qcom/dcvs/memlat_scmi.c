63cf682eb7511f6ca2712f41de723af70e1f565c
Rishabh Bhatnagar <rishabhb@codeaurora.org>
2021-02-03 10:50:19 -0800

    drivers: dcvs: memlat_scmi: Add scmi modularization changes
    Pass argument of type scmi_device instead of scmi_handle to probe
    function. This is being done to incorporate SCMI modularization
    changes. Also change the dependency from QTI_HW_MEMLAT to QCOM_MEMLAT.
    
    Change-Id: I381c03930733bb3a378a79236abf95b43bbfd15d
    Signed-off-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>

--- drivers/soc/qcom/dcvs/memlat_scmi.c
+++ drivers/soc/qcom/dcvs/memlat_scmi.c
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
  */
 
 #include <linux/scmi_protocol.h>
+#include <linux/scmi_memlat.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
@@ -11,21 +12,19 @@
 #include <linux/errno.h>
 #include <linux/platform_device.h>
 
-extern void rimps_memlat_init(struct scmi_handle *handle);
+extern int cpucp_memlat_init(struct scmi_device *sdev);
 
 static int scmi_memlat_probe(struct scmi_device *sdev)
 {
-	struct scmi_handle *handle = sdev->handle;
-
-	if (!handle || !handle->memlat_ops)
+	if (!sdev)
 		return -ENODEV;
 
-	rimps_memlat_init(handle);
-	return 0;
+	return cpucp_memlat_init(sdev);
 }
 
 static const struct scmi_device_id scmi_id_table[] = {
-	{ SCMI_PROTOCOL_MEMLAT },
+	{ .protocol_id = SCMI_PROTOCOL_MEMLAT, .name = "scmi_protocol_memlat" },
+	{ },
 };
 MODULE_DEVICE_TABLE(scmi, scmi_id_table);
 
@@ -36,5 +35,6 @@ static struct scmi_driver scmi_memlat_drv = {
 };
 module_scmi_driver(scmi_memlat_drv);
 
+MODULE_SOFTDEP("pre: memlat_vendor");
 MODULE_DESCRIPTION("ARM SCMI Memlat driver");
 MODULE_LICENSE("GPL v2");
