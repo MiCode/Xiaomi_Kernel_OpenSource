b4f88d4e81a9bb20736ebbce4a1e0f6aaef272fc
Deepak Kumar Singh <deesin@codeaurora.org>
2021-02-05 10:54:19 +0530

    rpmsg: glink: Use IRQF_NO_SUSPEND and pm_system_wakeup() for glink irq
    Glink irq should be wakeup capable, to achieve this irq is marked
    with enable_irq_wake().
    
    There are some use cases where glink communication is required in system
    suspend path and glink interrupt handler needs to be run immediately.
    But wakeup cpabale interrupts even if received during suspend will not
    run immediately, this will be marked pending and run when system resumes
    from suspend. In such scenario there can be a potential deadock in suspend
    path due to any irq thread which is waiting for glink interrupt completion
    while glink interrupt cannot be handled immediately.
    
    One such use case is usb plugin interrupt received during suspend, which
    calls regulator api to communicate with rpm over glink and waits for rpm
    ack. RPM ack cannot be processed because glink interrupt received from rpm
    goes in pending state. Suspend path is blocked because regulator irq thread
    is still waiting.
    
    So there is requirement to run glink interrupt handler in suspend path at
    the same time making it wakeup capable. IRQ frameworks forbids usage of
    both IRQF_NO_SUSPEND and enable_irq_wake() together. This patch adds
    IRQF_NO_SUSPEND and pm_system_wakeup() for glink irq to implement this
    requirement.
    
    Change-Id: I1da199c9e10c640bb84d00e569c4fafc5c104d24
    Signed-off-by: Deepak Kumar Singh <deesin@codeaurora.org>

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -23,6 +23,7 @@
 #include <linux/kthread.h>
 #include <linux/mailbox_client.h>
 #include <linux/ipc_logging.h>
+#include <linux/suspend.h>
 #include <soc/qcom/subsystem_notif.h>
 
 #include "rpmsg_internal.h"
@@ -53,6 +54,8 @@ do {									     \
 
 #define RPM_GLINK_CID_MIN	1
 #define RPM_GLINK_CID_MAX	65536
+
+static int should_wake;
 
 struct glink_msg {
 	__le16 cmd;
@@ -1241,6 +1244,8 @@ static irqreturn_t qcom_glink_native_intr(int irq, void *data)
 	unsigned int cmd;
 	int ret = 0;
 
+	if (should_wake)
+		pm_system_wakeup();
 	/* To wakeup any blocking writers */
 	wake_up_all(&glink->tx_avail_notify);
 
@@ -2221,6 +2226,26 @@ void qcom_glink_native_unregister(struct qcom_glink *glink)
 	device_unregister(glink->dev);
 }
 EXPORT_SYMBOL_GPL(qcom_glink_native_unregister);
+
+static int qcom_glink_suspend_no_irq(struct device *dev)
+{
+	should_wake = true;
+
+	return 0;
+}
+
+static int qcom_glink_resume_no_irq(struct device *dev)
+{
+	should_wake = false;
+
+	return 0;
+}
+
+const struct dev_pm_ops glink_native_pm_ops = {
+	.suspend_noirq = qcom_glink_suspend_no_irq,
+	.resume_noirq = qcom_glink_resume_no_irq,
+};
+EXPORT_SYMBOL(glink_native_pm_ops);
 
 MODULE_DESCRIPTION("Qualcomm GLINK driver");
 MODULE_LICENSE("GPL v2");
