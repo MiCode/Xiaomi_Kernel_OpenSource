3d38101d3be16c1376ec0d80b5c4db526e58b338
Chris Goldsworthy <cgoldswo@codeaurora.org>
2021-02-16 13:34:28 -0800

    dma-heap: qcom: Allow others to allocate from the non-secure pools
    Expose a means for allowing people to allocate directly from the
    non-secure system heap pools.
    
    Change-Id: Ie58e9526a874b55d80588aa658d46e853269451c
    Signed-off-by: Chris Goldsworthy <cgoldswo@codeaurora.org>

--- drivers/dma-buf/heaps/qcom_system_heap.h
+++ drivers/dma-buf/heaps/qcom_system_heap.h
@@ -7,6 +7,7 @@
 #define _QCOM_SYSTEM_HEAP_H
 
 #include <linux/dma-heap.h>
+#include <linux/err.h>
 #include "qcom_dynamic_page_pool.h"
 
 struct qcom_system_heap {
@@ -17,7 +18,17 @@ struct qcom_system_heap {
 
 #ifdef CONFIG_QCOM_DMABUF_HEAPS_SYSTEM
 int qcom_system_heap_create(char *name, bool uncached, int vmid);
+struct page *qcom_sys_heap_alloc_largest_available(struct dynamic_page_pool **pools,
+						   unsigned long size,
+						   unsigned int max_order);
 #else
+struct page *qcom_sys_heap_alloc_largest_available(struct dynamic_page_pool **pools,
+						   unsigned long size,
+						   unsigned int max_order)
+{
+	return ERR_PTR(-EOPNOTSUPP);
+}
+
 static int qcom_system_heap_create(char *name, bool uncached, int vmid)
 {
 	return 1;
