528cfb77a933c400b7c11592ecb187a90579316c
Veera Vegivada <vvegivad@codeaurora.org>
2021-02-22 16:12:50 +0530

    phy: qcom-ufs: Fix LF markings
    Update with proper LF markings.
    
    Change-Id: I2f655fd16b0d98cb96f3ab0893de2099105e5830
    Signed-off-by: Veera Vegivada <vvegivad@codeaurora.org>

--- drivers/phy/qualcomm/phy-qcom-ufs-qmp-v3-660.c
+++ drivers/phy/qualcomm/phy-qcom-ufs-qmp-v3-660.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2013-2018,2021, Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2018,2021, The Linux Foundation. All rights reserved.
  */
 
 #include "phy-qcom-ufs-qmp-v3-660.h"
--- drivers/phy/qualcomm/phy-qcom-ufs-qmp-v3-660.h
+++ drivers/phy/qualcomm/phy-qcom-ufs-qmp-v3-660.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2013-2016,2019-2021 Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2016,2019-2021, The Linux Foundation. All rights reserved.
  */
 
 #ifndef UFS_QCOM_PHY_QMP_V3_660_H_
