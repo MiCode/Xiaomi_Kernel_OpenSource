b246759284d6a2bc5b6f1009caeeb3abce2ec9ff
Ondrej Mosnacek <omosnace@redhat.com>
2021-02-24 22:56:28 +0100

    perf/core: Fix unconditional security_locked_down() call
    commit 08ef1af4de5fe7de9c6d69f1e22e51b66e385d9b upstream.
    
    Currently, the lockdown state is queried unconditionally, even though
    its result is used only if the PERF_SAMPLE_REGS_INTR bit is set in
    attr.sample_type. While that doesn't matter in case of the Lockdown LSM,
    it causes trouble with the SELinux's lockdown hook implementation.
    
    SELinux implements the locked_down hook with a check whether the current
    task's type has the corresponding "lockdown" class permission
    ("integrity" or "confidentiality") allowed in the policy. This means
    that calling the hook when the access control decision would be ignored
    generates a bogus permission check and audit record.
    
    Fix this by checking sample_type first and only calling the hook when
    its result would be honored.
    
    Fixes: b0c8fdc7fdb7 ("lockdown: Lock down perf when in confidentiality mode")
    Signed-off-by: Ondrej Mosnacek <omosnace@redhat.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Paul Moore <paul@paul-moore.com>
    Link: https://lkml.kernel.org/r/20210224215628.192519-1-omosnace@redhat.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -10953,12 +10953,12 @@ SYSCALL_DEFINE5(perf_event_open,
 	    perf_paranoid_kernel() && !capable(CAP_SYS_ADMIN))
 		return -EACCES;
 
-	err = security_locked_down(LOCKDOWN_PERF);
-	if (err && (attr.sample_type & PERF_SAMPLE_REGS_INTR))
-		/* REGS_INTR can leak data, lockdown must prevent this */
-		return err;
-
-	err = 0;
+	/* REGS_INTR can leak data, lockdown must prevent this */
+	if (attr.sample_type & PERF_SAMPLE_REGS_INTR) {
+		err = security_locked_down(LOCKDOWN_PERF);
+		if (err)
+			return err;
+	}
 
 	/*
 	 * In cgroup mode, the pid argument is used to pass the fd
