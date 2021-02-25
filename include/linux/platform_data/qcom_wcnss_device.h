95e2ab545aec77d8f3686b27e4589d3d2949ba0b
Sandeep Singh <sandsing@codeaurora.org>
2021-02-25 15:23:15 +0530

    wcnss: Add wcnss snapshot to msm-4.19
    Add pronto platform driver support in kernel msm-4.19.
    This is a snapshot of the wcnss driver and associated files
    as of msm-4.14.
    
    Change-Id: I9156c4e03ba4e758face536275395ca0d2ca04fc
    Signed-off-by: Sandeep Singh <sandsing@codeaurora.org>

---
+++ include/linux/platform_data/qcom_wcnss_device.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2011, 2018, 2020-2021, The Linux Foundation. All rights reserved.
+ */
+#ifndef __QCOM_WCNSS_DEVICE__H
+#define __QCOM_WCNSS_DEVICE__H
+
+struct qcom_wcnss_opts {
+	bool has_48mhz_xo;
+};
+
+#endif /* __QCOM_WCNSS_DEVICE__H */
