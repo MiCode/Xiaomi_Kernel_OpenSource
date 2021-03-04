f79e49085d1d655f4c2df9bc076b71ebc6725185
Namkyu Kim <namkyu78.kim@samsung.com>
2021-03-04 09:31:56 +0900

    ANDROID: Add a vendor hook that allow a module to modify the wake flag
     android_vh_do_wake_up_sync:
      To modify the mode value of __wake_up_sync_key
    
     android_vh_set_wake_flags:
      To modify the wake flag from a module
    
    Bug: 181743516
    Signed-off-by: Namkyu Kim <namkyu78.kim@samsung.com>
    Change-Id: I972e2469c3f139373d21f1e8c85974763388a693
    (cherry picked from commit 97368fc2dcc29777e8d3d637d0afdef90e611763)
    (cherry picked from commit 0d0f0c5020bc425c9a51c8d17b16ca831c2598fb)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -220,3 +220,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_uninterruptible_tasks_dn);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_uclamp_eff_get);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_util_est_update);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_setscheduler_uclamp);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_wake_up_sync);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_wake_flags);
