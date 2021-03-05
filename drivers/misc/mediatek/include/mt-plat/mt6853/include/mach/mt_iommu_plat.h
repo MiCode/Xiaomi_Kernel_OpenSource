a6a790dd06f2c850765bb34eb2bf4a91c18cfda4
mingyuan ma <mingyuan.ma@mediatek.com>
2021-03-05 11:20:27 +0800

    [ALPS05589704] IOMMU: fix IOMMU power callback abnormal issue
    [Root cause]
    Since mt6833 and mt6853 are co-platform settings for IOMMU, mt6833 use
    mt6853 clock settings, but mt6853 has APU IOMMU and mt6833 has no.
    cause mt6833 mapping error power domain then power callback abnormally.
    
    [Solution]
    1. mt6833 use itself clock settings.
    2. Remove APU related members in IOMMU array if APUSYS not supported.
    
    MTK-Commit-Id: c6ed4c4d60c5c078348a5751c3d42d6666d45fcb
    
    Change-Id: I068c883955a86184152657dac06948ad61f90ab2
    Signed-off-by: Mingyuan Ma <mingyuan.ma@mediatek.com>
    CR-Id: ALPS05589704
    Feature: IOMMU
    (cherry picked from commit c0b53d272e8fe04dce9351e56b46e8ecd8605c03)

--- drivers/misc/mediatek/include/mt-plat/mt6853/include/mach/mt_iommu_plat.h
+++ drivers/misc/mediatek/include/mt-plat/mt6853/include/mach/mt_iommu_plat.h
@@ -26,10 +26,17 @@
 #ifdef IOMMU_POWER_CLK_SUPPORT
 #define MTK_IOMMU_LOW_POWER_SUPPORT
 #define MTK_WARN_PSEDUO_FIND_SG
+#if IS_ENABLED(CONFIG_MACH_MT6833)
+#include "clk-mt6833-pg.h"
+#else
 #include "clk-mt6853-pg.h"
+#endif
 
 enum subsys_id iommu_mtcmos_subsys[MTK_IOMMU_M4U_COUNT] = {
-	SYS_DIS, SYS_VPU
+	SYS_DIS,
+#ifdef CONFIG_MTK_APUSYS_SUPPORT
+	SYS_VPU
+#endif
 };
 #endif
 #endif
@@ -60,7 +67,12 @@ enum subsys_id iommu_mtcmos_subsys[MTK_IOMMU_M4U_COUNT] = {
 #ifdef CONFIG_FPGA_EARLY_PORTING
 static unsigned int g_tag_count[MTK_IOMMU_M4U_COUNT] = {64};
 #else
-static unsigned int g_tag_count[MTK_IOMMU_M4U_COUNT] = {64, 64};
+static unsigned int g_tag_count[MTK_IOMMU_M4U_COUNT] = {
+		64,
+#ifdef CONFIG_MTK_APUSYS_SUPPORT
+		64,
+#endif
+		};
 #endif
 
 char *smi_clk_name[MTK_IOMMU_LARB_NR] = {
@@ -85,12 +97,17 @@ char *iommu_secure_compatible[MTK_IOMMU_M4U_COUNT] = {
 };
 #else
 char *iommu_secure_compatible[MTK_IOMMU_M4U_COUNT] = {
-	"mediatek,sec_m4u0", "mediatek,sec_m4u1",
+	"mediatek,sec_m4u0",
+#ifdef CONFIG_MTK_APUSYS_SUPPORT
+	"mediatek,sec_m4u1",
+#endif
 };
 
 char *iommu_bank_compatible[MTK_IOMMU_M4U_COUNT][MTK_IOMMU_BANK_NODE_COUNT] = {
 	{"mediatek,bank1_m4u0", "mediatek,bank2_m4u0", "mediatek,bank3_m4u0"},
+#ifdef CONFIG_MTK_APUSYS_SUPPORT
 	{"mediatek,bank1_m4u1", "mediatek,bank2_m4u1", "mediatek,bank3_m4u1"},
+#endif
 };
 #endif
 
@@ -760,6 +777,7 @@ struct mau_config_info mt6853_mau_info[MTK_IOMMU_M4U_COUNT] = {
 		.start_bit32 = 0x4,
 		.end_bit32 = 0x7,
 	},
+#ifdef CONFIG_MTK_APUSYS_SUPPORT
 	{
 		.start = 0x40000000,
 		.end = 0xffffffff,
@@ -771,6 +789,7 @@ struct mau_config_info mt6853_mau_info[MTK_IOMMU_M4U_COUNT] = {
 		.start_bit32 = 0x4,
 		.end_bit32 = 0x7,
 	}
+#endif
 };
 
 struct mau_config_info *get_mau_info(int m4u_id)
