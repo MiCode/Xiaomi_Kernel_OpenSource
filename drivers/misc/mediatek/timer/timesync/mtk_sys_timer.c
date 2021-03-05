898efa63ed747359ae4ccd22dfb0e01826900ab7
Chang-An Chen <chang-an.chen@mediatek.com>
2021-03-05 11:13:37 +0800

    [ALPS05552819] timer: fix timesync build fail without sspm
    Fix timesync build fail when mcupm is enabled and sspm is disabled
    
    MTK-Commit-Id: 98afddd3b0259ec2a91294f345e5ba9e95733404
    
    Change-Id: I5e98983afd76d11b8885839af62320491b5b3bc2
    Signed-off-by: Chang-An Chen <chang-an.chen@mediatek.com>
    CR-Id: ALPS05552819
    Feature: [Module]Timer
    (cherry picked from commit eb7906eaf9a023d26a04789db325656398329e76)

--- drivers/misc/mediatek/timer/timesync/mtk_sys_timer.c
+++ drivers/misc/mediatek/timer/timesync/mtk_sys_timer.c
@@ -103,9 +103,11 @@ static int sys_timer_mbox_write(unsigned int id, unsigned int val)
 {
 	int res;
 #ifndef SSPM_V2
+#ifdef CONFIG_MTK_TINYSYS_SSPM_SUPPORT
 	res = sspm_mbox_write(SYS_TIMER_MBOX,
 			      SYS_TIMER_MBOX_OFFSET_BASE + id,
 			      (void *)&val, 1);
+#endif
 #else
 #ifdef CONFIG_MTK_TINYSYS_MCUPM_SUPPORT
 	res = mcupm_mbox_write(SYS_TIMER_MCUPM_MBOX,
@@ -121,7 +123,7 @@ static int sys_timer_mbox_write(unsigned int id, unsigned int val)
 	return res;
 }
 
-#ifndef SSPM_V2
+#if !defined(SSPM_V2) && defined(CONFIG_MTK_TINYSYS_SSPM_SUPPORT)
 static int sys_timer_mbox_read(unsigned int id, unsigned int *val)
 {
 	return sspm_mbox_read(SYS_TIMER_MBOX, id, val, 1);
@@ -192,6 +194,7 @@ static void sys_timer_timesync_update_sspm(int suspended,
 
 void sys_timer_timesync_verify_sspm(void)
 {
+#ifdef CONFIG_MTK_TINYSYS_SSPM_SUPPORT
 	struct plt_ipi_data_s ipi_data;
 #ifndef SSPM_V2
 	int ackdata = 0;
@@ -245,6 +248,7 @@ void sys_timer_timesync_verify_sspm(void)
 		pr_info("verify-sspm:ERROR!");
 
 	sys_timer_timesync_print_base();
+#endif
 }
 
 #else
