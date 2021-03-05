43409c8a2b8faf53b7e947f837ab9a9f297485ef
Alessio Balsini <balsini@google.com>
2021-03-05 11:57:19 +0800

    [ALPS05563935] fuse: Passthrough initialization and release
    Implement the FUSE passthrough ioctl that associates the lower
    (passthrough) file system file with the fuse_file.
    
    The file descriptor passed to the ioctl by the FUSE daemon is used to
    access the relative file pointer, that will be copied to the fuse_file
    data structure to consolidate the link between the FUSE and lower file
    system.
    
    To enable the passthrough mode, user space triggers the
    FUSE_DEV_IOC_PASSTHROUGH_OPEN ioctl and, if the call succeeds, receives
    back an identifier that will be used at open/create response time in the
    fuse_open_out field to associate the FUSE file to the lower file system
    file.
    The value returned by the ioctl to user space can be:
    - > 0: success, the identifier can be used as part of an open/create
    reply.
    - <= 0: an error occurred.
    The value 0 represents an error to preserve backward compatibility: the
    fuse_open_out field that is used to pass the passthrough_fh back to the
    kernel uses the same bits that were previously as struct padding, and is
    commonly zero-initialized (e.g., in the libfuse implementation).
    Removing 0 from the correct values fixes the ambiguity between the case
    in which 0 corresponds to a real passthrough_fh, a missing
    implementation of FUSE passthrough or a request for a normal FUSE file,
    simplifying the user space implementation.
    
    For the passthrough mode to be successfully activated, the lower file
    system file must implement both read_iter and write_iter file
    operations. This extra check avoids special pseudo files to be targeted
    for this feature.
    Passthrough comes with another limitation: no further file system
    stacking is allowed for those FUSE file systems using passthrough.
    
    MTK-Commit-Id: 2d9c6b138d8aadce28681ce2978febfa281d81ab
    
    Bug: 168023149
    Link: https://lore.kernel.org/lkml/20210125153057.3623715-5-balsini@android.com/
    Signed-off-by: Alessio Balsini <balsini@android.com>
    Change-Id: I4d8290012302fb4547bce9bb261a03cc4f66b5aa
    Signed-off-by: Alessio Balsini <balsini@google.com>
    Signed-off-by: Ed Tsai <ed.tsai@mediatek.com>
    CR-Id: ALPS05563935
    Feature: [Android Default] F2FS File System

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -984,6 +984,11 @@ static void fuse_send_init(struct fuse_conn *fc, struct fuse_req *req)
 
 static int free_fuse_passthrough(int id, void *p, void *data)
 {
+	struct fuse_passthrough *passthrough = (struct fuse_passthrough *)p;
+
+	fuse_passthrough_release(passthrough);
+	kfree(p);
+
 	return 0;
 }
 
