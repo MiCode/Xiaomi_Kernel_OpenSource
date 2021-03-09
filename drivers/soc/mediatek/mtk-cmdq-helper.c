c5a19c060e61dcaa2954d80a11dcc67f5506fe77
Ming-Fan Chen <ming-fan.chen@mediatek.com>
2021-03-09 16:44:43 +0800

    [ALPS05543104] cmdq: add dispatch module/core/thread in prefix
    add dispatch [mdoule]<core>(thread) in prefix
    
    MTK-Commit-Id: 25fc5ee7da85db98aa1502883de548b71a091dfc
    
    Change-Id: I8814e2dba2c5d1d8958aab337842a5011da2a868
    CR-Id: ALPS05543104
    Feature: [Module]CMDQ Driver
    Signed-off-by: Ming-Fan Chen <ming-fan.chen@mediatek.com>

--- drivers/soc/mediatek/mtk-cmdq-helper.c
+++ drivers/soc/mediatek/mtk-cmdq-helper.c
@@ -1505,10 +1505,10 @@ static void cmdq_pkt_err_irq_dump(struct cmdq_pkt *pkt)
 	cmdq_util_dump_lock();
 	cmdq_util_error_enable();
 
-	cmdq_util_err("begin of error irq %u", err_num++);
+	cmdq_util_user_err(client->chan, "begin of error irq %u", err_num++);
 
 	cmdq_task_get_thread_pc(client->chan, &pc);
-	cmdq_util_err("pkt:%lx thread:%d pc:%lx",
+	cmdq_util_user_err(client->chan, "pkt:%lx thread:%d pc:%lx",
 		(unsigned long)pkt, thread_id, (unsigned long)pc);
 
 	if (pc) {
@@ -1516,7 +1516,8 @@ static void cmdq_pkt_err_irq_dump(struct cmdq_pkt *pkt)
 			if (pc < buf->pa_base ||
 				pc > buf->pa_base + CMDQ_CMD_BUFFER_SIZE) {
 				size -= CMDQ_CMD_BUFFER_SIZE;
-				cmdq_util_msg("buffer %u va:0x%p pa:%pa",
+				cmdq_util_user_msg(client->chan,
+					"buffer %u va:0x%p pa:%pa",
 					cnt, buf->va_base, &buf->pa_base);
 				cnt++;
 				continue;
@@ -1527,10 +1528,11 @@ static void cmdq_pkt_err_irq_dump(struct cmdq_pkt *pkt)
 			if (size > CMDQ_CMD_BUFFER_SIZE)
 				size = CMDQ_CMD_BUFFER_SIZE;
 
-			cmdq_util_msg("error irq buffer %u va:0x%p pa:%pa",
+			cmdq_util_user_msg(client->chan,
+				"error irq buffer %u va:0x%p pa:%pa",
 				cnt, buf->va_base, &buf->pa_base);
 			cmdq_buf_cmd_parse(buf->va_base, CMDQ_NUM_CMD(size),
-				buf->pa_base, pc, NULL);
+				buf->pa_base, pc, NULL, client->chan);
 
 			break;
 		}
@@ -1597,7 +1599,7 @@ static void cmdq_print_wait_summary(void *chan, dma_addr_t pc,
 				len, ARRAY_SIZE(text_gpr));
 	}
 
-	cmdq_util_msg("curr inst: %s value:%u%s",
+	cmdq_util_user_msg(chan, "curr inst: %s value:%u%s",
 		text, cmdq_get_event(chan, inst->arg_a), text_gpr);
 }
 #endif
@@ -1625,7 +1627,7 @@ void cmdq_pkt_err_dump_cb(struct cmdq_cb_data data)
 	if (err_num == 0)
 		cmdq_util_error_enable();
 
-	cmdq_util_err("Begin of Error %u", err_num);
+	cmdq_util_user_err(client->chan, "Begin of Error %u", err_num);
 
 	cmdq_dump_core(client->chan);
 
@@ -1652,7 +1654,8 @@ void cmdq_pkt_err_dump_cb(struct cmdq_cb_data data)
 	if (inst && inst->op == CMDQ_CODE_WFE)
 		cmdq_print_wait_summary(client->chan, pc, inst);
 	else if (inst)
-		cmdq_buf_cmd_parse((u64 *)inst, 1, pc, pc, "curr inst:");
+		cmdq_buf_cmd_parse((u64 *)inst, 1, pc, pc,
+			"curr inst:", client->chan);
 	else
 		cmdq_util_msg("curr inst: Not Available");
 
@@ -1702,7 +1705,7 @@ void cmdq_pkt_err_dump_cb(struct cmdq_cb_data data)
 	}
 
 done:
-	cmdq_util_err("End of Error %u", err_num);
+	cmdq_util_user_err(client->chan, "End of Error %u", err_num);
 	if (err_num == 0) {
 		cmdq_util_error_disable();
 		cmdq_util_set_first_err_mod(client->chan, mod);
@@ -1775,7 +1778,7 @@ void cmdq_dump_summary(struct cmdq_client *client, struct cmdq_pkt *pkt)
 		cmdq_print_wait_summary(client->chan, pc, inst);
 	else if (inst)
 		cmdq_buf_cmd_parse((u64 *)inst, 1, pc, pc,
-			"curr inst:");
+			"curr inst:", client->chan);
 	else
 		cmdq_msg("curr inst: Not Available");
 	cmdq_dump_pkt(pkt, pc, false);
@@ -1821,7 +1824,8 @@ static int cmdq_pkt_wait_complete_loop(struct cmdq_pkt *pkt)
 			break;
 
 		cmdq_util_dump_lock();
-		cmdq_msg("===== SW timeout Pre-dump %u =====", cnt++);
+		cmdq_util_user_msg(client->chan,
+			"===== SW timeout Pre-dump %u =====", cnt++);
 		cmdq_dump_summary(client, pkt);
 		cmdq_util_dump_unlock();
 	}
@@ -2250,7 +2254,7 @@ static void cmdq_buf_print_misc(char *text, u32 txt_sz,
 }
 
 void cmdq_buf_cmd_parse(u64 *buf, u32 cmd_nr, dma_addr_t buf_pa,
-	dma_addr_t cur_pa, const char *info)
+	dma_addr_t cur_pa, const char *info, void *chan)
 {
 #define txt_sz 128
 	static char text[txt_sz];
@@ -2298,7 +2302,7 @@ void cmdq_buf_cmd_parse(u64 *buf, u32 cmd_nr, dma_addr_t buf_pa,
 				&cmdq_inst[i]);
 			break;
 		}
-		cmdq_util_msg("%s%s",
+		cmdq_util_user_msg(chan, "%s%s",
 			info ? info : (buf_pa == cur_pa ? ">>" : "  "),
 			text);
 		buf_pa += CMDQ_INST_SIZE;
@@ -2307,6 +2311,7 @@ void cmdq_buf_cmd_parse(u64 *buf, u32 cmd_nr, dma_addr_t buf_pa,
 
 s32 cmdq_pkt_dump_buf(struct cmdq_pkt *pkt, dma_addr_t curr_pa)
 {
+	struct cmdq_client *client = (struct cmdq_client *)pkt->cl;
 	struct cmdq_pkt_buffer *buf;
 	u32 size, cnt = 0;
 
@@ -2315,7 +2320,7 @@ s32 cmdq_pkt_dump_buf(struct cmdq_pkt *pkt, dma_addr_t curr_pa)
 			size = CMDQ_CMD_BUFFER_SIZE - pkt->avail_buf_size;
 		} else if (cnt > 2 && !(curr_pa >= buf->pa_base &&
 			curr_pa < buf->pa_base + CMDQ_BUF_ALLOC_SIZE)) {
-			cmdq_util_msg(
+			cmdq_util_user_msg(client->chan,
 				"buffer %u va:0x%p pa:%pa %#018llx (skip detail) %#018llx",
 				cnt, buf->va_base, &buf->pa_base,
 				*((u64 *)buf->va_base),
@@ -2326,10 +2331,10 @@ s32 cmdq_pkt_dump_buf(struct cmdq_pkt *pkt, dma_addr_t curr_pa)
 		} else {
 			size = CMDQ_CMD_BUFFER_SIZE;
 		}
-		cmdq_util_msg("buffer %u va:0x%p pa:%pa",
+		cmdq_util_user_msg(client->chan, "buffer %u va:0x%p pa:%pa",
 			cnt, buf->va_base, &buf->pa_base);
 		cmdq_buf_cmd_parse(buf->va_base, CMDQ_NUM_CMD(size),
-			buf->pa_base, curr_pa, NULL);
+			buf->pa_base, curr_pa, NULL, client->chan);
 		cnt++;
 	}
 
@@ -2339,16 +2344,18 @@ EXPORT_SYMBOL(cmdq_pkt_dump_buf);
 
 int cmdq_dump_pkt(struct cmdq_pkt *pkt, dma_addr_t pc, bool dump_ist)
 {
+	struct cmdq_client *client = (struct cmdq_client *)pkt->cl;
+
 	if (!pkt)
 		return -EINVAL;
 
-	cmdq_util_msg(
+	cmdq_util_user_msg(client->chan,
 		"pkt:0x%p(%#x) size:%zu/%zu avail size:%zu priority:%u%s",
 		pkt, (u32)(unsigned long)pkt, pkt->cmd_buf_size,
 		pkt->buf_size, pkt->avail_buf_size,
 		pkt->priority, pkt->loop ? " loop" : "");
 #if IS_ENABLED(CONFIG_MTK_CMDQ_MBOX_EXT)
-	cmdq_util_msg(
+	cmdq_util_user_msg(client->chan,
 		"submit:%llu trigger:%llu wait:%llu irq:%llu",
 		pkt->rec_submit, pkt->rec_trigger,
 		pkt->rec_wait, pkt->rec_irq);
