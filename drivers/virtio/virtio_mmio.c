7eee00fb51b22889fe69084618309d4d5457b1be
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2021-03-09 21:07:28 -0800

    haven: virtio: Add trace events
    Add trace events to aid debugging.
    
    Change-Id: I23d0c193ba8ba1823542c9f14edb37468a7b602a
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>

--- drivers/virtio/virtio_mmio.c
+++ drivers/virtio/virtio_mmio.c
@@ -69,6 +69,13 @@
 #include <linux/virtio_config.h>
 #include <uapi/linux/virtio_mmio.h>
 #include <linux/virtio_ring.h>
+
+#ifdef CONFIG_HH_VIRTIO_DEBUG
+#define CREATE_TRACE_POINTS
+#include <trace/events/hh_virtio_frontend.h>
+#undef CREATE_TRACE_POINTS
+#endif
+
 #ifdef CONFIG_VIRTIO_MMIO_SWIOTLB
 #include <linux/swiotlb.h>
 #include <linux/dma-direct.h>
@@ -290,6 +297,9 @@ static bool vm_notify(struct virtqueue *vq)
 {
 	struct virtio_mmio_device *vm_dev = to_virtio_mmio_device(vq->vdev);
 
+#ifdef CONFIG_HH_VIRTIO_DEBUG
+	trace_virtio_mmio_vm_notify(vq->vdev->index, vq->index);
+#endif
 	/* We write the queue's selector into the notification register to
 	 * signal the other end */
 	writel(vq->index, vm_dev->base + VIRTIO_MMIO_QUEUE_NOTIFY);
@@ -307,6 +317,10 @@ static irqreturn_t vm_interrupt(int irq, void *opaque)
 
 	/* Read and acknowledge interrupts */
 	status = readl(vm_dev->base + VIRTIO_MMIO_INTERRUPT_STATUS);
+#ifdef CONFIG_HH_VIRTIO_DEBUG
+	trace_virtio_mmio_vm_interrupt(vm_dev->vdev.index, status);
+#endif
+
 	writel(status, vm_dev->base + VIRTIO_MMIO_INTERRUPT_ACK);
 
 	if (unlikely(status & VIRTIO_MMIO_INT_CONFIG)) {
