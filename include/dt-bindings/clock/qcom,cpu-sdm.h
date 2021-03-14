00f69a7f5dd7ab42779d2d8272c438d289df3828
Chetan C R <cchinnad@codeaurora.org>
2021-03-14 14:26:21 +0530

    dt-bindings: clock: Add support for CPUCC clock ids for SDM439
    Add clock id's for CPU clock controller for SDM429 and
    SDM39 target.
    
    Change-Id: I97b7865baf70043060de51044417e98d65568854
    Signed-off-by: Chetan C R <cchinnad@codeaurora.org>

--- include/dt-bindings/clock/qcom,cpu-sdm.h
+++ include/dt-bindings/clock/qcom,cpu-sdm.h
@@ -6,8 +6,10 @@
 #ifndef _DT_BINDINGS_CLK_QCOM_CPU_SDM_H
 #define _DT_BINDINGS_CLK_QCOM_CPU_SDM_H
 
-#define APCS_CPU_PLL		0
+#define APCS_CPU_PLL1		0
 #define APCS_MUX_C1_CLK		1
 #define APCS_MUX_CCI_CLK	2
+#define APCS_CPU_PLL0		3
+#define APCS_MUX_C0_CLK		4
 
 #endif
