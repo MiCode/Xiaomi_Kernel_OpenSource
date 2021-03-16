28153bdff9d6d7aae70e83934b0099abc3b62cf2
Sahitya Tummala <stummala@codeaurora.org>
2021-03-16 14:59:18 +0530

    f2fs: allow to change discard policy based on cached discard cmds
    With the default DPOLICY_BG discard thread is ioaware, which prevents
    the discard thread from issuing the discard commands. On low RAM setups,
    it is observed that these discard commands in the cache are consuming
    high memory. This patch aims to relax the memory pressure on the system
    due to f2fs pending discard cmds by changing the policy to DPOLICY_FORCE
    based on the nm_i->ram_thresh configured.
    
    Change-Id: Icac4a6ad2885eb463d7e11bfc99486a4cb96533b
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Git-commit: d6d2b491a82e1e411a6766fbfb87c697d8701554
    Git-repo: https://android.googlesource.com/kernel/common/
    Signed-off-by: Sayali Lokhande <sayalil@codeaurora.org>

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -1733,7 +1733,8 @@ static int issue_discard_thread(void *data)
 			continue;
 		}
 
-		if (sbi->gc_mode == GC_URGENT)
+		if (sbi->gc_mode == GC_URGENT ||
+			!f2fs_available_free_memory(sbi, DISCARD_CACHE))
 			__init_discard_policy(sbi, &dpolicy, DPOLICY_FORCE, 1);
 
 		sb_start_intwrite(sbi->sb);
