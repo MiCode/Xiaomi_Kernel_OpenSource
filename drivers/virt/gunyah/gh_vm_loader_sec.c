84c286843041c7a726157238acc65137dd29b62c
Raghavendra Rao Ananta <rananta@codeaurora.org>
2021-03-16 14:25:55 -0700

    gunyah: gh_vm_loader_sec: Add support to start a VM
    Add support for the IOCTL, GH_VM_SEC_START, which is used
    to load the VM onto memory, authenticate and trigger a boot.
    
    Change-Id: I178545dc82252227ce7db50d3846a46730e537cd
    Signed-off-by: Raghavendra Rao Ananta <rananta@codeaurora.org>
    [pheragu@codeaurora.org: Use vmid from dt to alloc vmid]
    Signed-off-by: Prakruthi Deepak Heragu <pheragu@codeaurora.org>

--- drivers/virt/gunyah/gh_vm_loader_sec.c
+++ drivers/virt/gunyah/gh_vm_loader_sec.c
@@ -10,9 +10,16 @@
 #include <linux/slab.h>
 #include <linux/list.h>
 #include <linux/mutex.h>
+#include <linux/kernel.h>
+#include <linux/of_address.h>
+#include <linux/wait.h>
+#include <linux/qcom_scm.h>
+#include <linux/firmware.h>
 #include <linux/platform_device.h>
+#include <linux/soc/qcom/mdt_loader.h>
 
 #include <linux/gunyah/gh_vm.h>
+#include <linux/gunyah/gh_rm_drv.h>
 
 #include "gh_vm_loader_private.h"
 
@@ -21,6 +28,11 @@ struct gh_sec_vm_dev {
 	struct list_head list;
 	const char *vm_name;
 	struct device *dev;
+	phys_addr_t fw_phys;
+	void *fw_virt;
+	ssize_t fw_size;
+	int pas_id;
+	int vmid;
 };
 
 /* Structure per VM: Binds struct gh_vm_struct and struct gh_sec_vm_dev */
@@ -28,7 +40,12 @@ struct gh_sec_vm_struct {
 	struct gh_vm_struct *vm_struct;
 	struct gh_sec_vm_dev *vm_dev;
 	struct mutex vm_lock;
+	int vmid;
+	struct gh_vm_status vm_status;
+	wait_queue_head_t vm_status_wait;
 };
+
+#define GH_VM_LOADER_SEC_STATUS_TIMEOUT_MS 5000
 
 static DEFINE_SPINLOCK(gh_sec_vm_devs_lock);
 static LIST_HEAD(gh_sec_vm_devs);
@@ -51,15 +68,223 @@ static struct gh_sec_vm_dev *get_sec_vm_dev_by_name(const char *vm_name)
 	return NULL;
 }
 
+static int
+gh_vm_loader_wait_for_vm_status(struct gh_sec_vm_struct *sec_vm_struct,
+				int wait_status)
+{
+	struct device *dev = sec_vm_struct->vm_dev->dev;
+	long timeleft;
+
+	timeleft = wait_event_interruptible_timeout(
+			sec_vm_struct->vm_status_wait,
+			sec_vm_struct->vm_status.vm_status == wait_status,
+			msecs_to_jiffies(GH_VM_LOADER_SEC_STATUS_TIMEOUT_MS));
+	if (timeleft < 0) {
+		dev_err(dev, "Wait for VM_STATUS %d interrupt\n", wait_status);
+		return -ERESTARTSYS;
+	} else if (timeleft == 0) {
+		dev_err(dev, "Wait for VM_STATUS %d timed out\n", wait_status);
+		return -ETIMEDOUT;
+	}
+
+	return 0;
+}
+
+static int
+gh_vm_loader_wait_for_os_status(struct gh_sec_vm_struct *sec_vm_struct,
+				int wait_status)
+{
+	struct device *dev = sec_vm_struct->vm_dev->dev;
+	long timeleft;
+
+	timeleft = wait_event_interruptible_timeout(
+			sec_vm_struct->vm_status_wait,
+			sec_vm_struct->vm_status.os_status == wait_status,
+			msecs_to_jiffies(GH_VM_LOADER_SEC_STATUS_TIMEOUT_MS));
+	if (timeleft < 0) {
+		dev_err(dev, "Wait for OS_STATUS %d interrupt\n", wait_status);
+		return -ERESTARTSYS;
+	} else if (timeleft == 0) {
+		dev_err(dev, "Wait for OS_STATUS %d timed out\n", wait_status);
+		return -ETIMEDOUT;
+	}
+
+	return 0;
+}
+
+static void
+gh_vm_loader_sec_notif_vm_status(struct gh_sec_vm_struct *sec_vm_struct,
+				struct gh_rm_notif_vm_status_payload *vm_status)
+{
+	struct gh_sec_vm_dev *vm_dev = sec_vm_struct->vm_dev;
+	struct device *dev = vm_dev->dev;
+	int vmid = sec_vm_struct->vmid;
+
+	if (vmid != vm_status->vmid) {
+		dev_warn(dev, "VM_STATUS notification not for this vmid\n");
+		return;
+	}
+
+	/* Wake up the waiters only if there's a change in any of the states */
+	if (vm_status->vm_status != sec_vm_struct->vm_status.vm_status) {
+		switch (vm_status->vm_status) {
+		case GH_RM_VM_STATUS_RUNNING:
+			dev_info(dev, "VM:%d started running\n", vmid);
+			break;
+		}
+
+		sec_vm_struct->vm_status.vm_status = vm_status->vm_status;
+		wake_up_interruptible(&sec_vm_struct->vm_status_wait);
+	}
+
+	if (vm_status->os_status != sec_vm_struct->vm_status.os_status) {
+		switch (vm_status->os_status) {
+		case GH_RM_OS_STATUS_BOOT:
+			dev_info(dev, "VM:%d OS booted\n", vmid);
+			break;
+		}
+
+		sec_vm_struct->vm_status.os_status = vm_status->os_status;
+		wake_up_interruptible(&sec_vm_struct->vm_status_wait);
+	}
+}
+
 static void gh_vm_loader_sec_rm_notifier(struct gh_vm_struct *vm_struct,
 					unsigned long cmd, void *data)
 {
+	struct gh_sec_vm_struct *sec_vm_struct;
+
+	sec_vm_struct = gh_vm_loader_get_loader_data(vm_struct);
+	if (!sec_vm_struct)
+		return;
+
+	switch (cmd) {
+	case GH_RM_NOTIF_VM_STATUS:
+		gh_vm_loader_sec_notif_vm_status(sec_vm_struct, data);
+		break;
+	}
+}
+
+static int gh_vm_loader_sec_load(struct gh_sec_vm_struct *sec_vm_struct)
+{
+	struct gh_sec_vm_dev *vm_dev = sec_vm_struct->vm_dev;
+	struct device *dev = vm_dev->dev;
+	const struct firmware *fw;
+	char fw_name[32];
+	int ret;
+
+	scnprintf(fw_name, ARRAY_SIZE(fw_name), "%s.mdt", vm_dev->vm_name);
+
+	ret = request_firmware(&fw, fw_name, dev);
+	if (ret) {
+		dev_err(dev, "Error requesting fw \"%s\": %d\n", fw_name, ret);
+		return ret;
+	}
+
+	ret = qcom_mdt_load(dev, fw, fw_name, vm_dev->pas_id, vm_dev->fw_virt,
+				vm_dev->fw_phys, vm_dev->fw_size, NULL);
+	if (ret) {
+		dev_err(dev, "Failed to load fw \"%s\": %d\n", fw_name, ret);
+		goto release_fw;
+	}
+
+	ret = qcom_scm_pas_auth_and_reset(vm_dev->pas_id);
+	if (ret)
+		dev_err(dev, "Failed to auth and reset: %d\n", ret);
+
+release_fw:
+	release_firmware(fw);
+	return ret;
+}
+
+static int gh_vm_loader_sec_start(struct gh_vm_struct *vm_struct)
+{
+	struct gh_sec_vm_struct *sec_vm_struct;
+	struct gh_sec_vm_dev *vm_dev;
+	enum gh_vm_names vm_name_val;
+	struct device *dev;
+	int ret, vmid;
+
+	sec_vm_struct = gh_vm_loader_get_loader_data(vm_struct);
+	if (!sec_vm_struct)
+		return -EINVAL;
+
+	vm_dev = sec_vm_struct->vm_dev;
+	dev = vm_dev->dev;
+
+	mutex_lock(&sec_vm_struct->vm_lock);
+
+	if (sec_vm_struct->vm_status.vm_status != GH_RM_VM_STATUS_INIT) {
+		dev_err(dev, "VM already loaded\n");
+		ret = -EBUSY;
+		goto err_unlock;
+	}
+
+	vm_name_val = gh_vm_loader_get_name_val(vm_struct);
+	ret = gh_rm_vm_alloc_vmid(vm_name_val, &vm_dev->vmid);
+	if (ret < 0) {
+		dev_err(dev, "Failed to obtain the vmid: %d\n", ret);
+		goto err_unlock;
+	}
+	vmid = vm_dev->vmid;
+	sec_vm_struct->vmid = vmid;
+
+	ret = gh_vm_loader_sec_load(sec_vm_struct);
+	if (ret)
+		goto err_dealloc_vmid;
+
+	ret = gh_vm_loader_wait_for_vm_status(sec_vm_struct,
+						GH_RM_VM_STATUS_READY);
+	if (ret)
+		goto err_unload;
+
+	ret = gh_rm_populate_hyp_res(vmid, vm_dev->vm_name);
+	if (ret < 0) {
+		dev_err(dev, "Failed to populate hyp res: %d\n", ret);
+		goto err_unload;
+	}
+
+	ret = gh_rm_vm_start(vmid);
+	if (ret) {
+		dev_err(dev, "Failed to start the VM: %d\n", ret);
+		goto err_unpopulate_hyp_res;
+	}
+
+	ret = gh_vm_loader_wait_for_vm_status(sec_vm_struct,
+						GH_RM_VM_STATUS_RUNNING);
+	if (ret)
+		goto err_reset_vm;
+
+	ret = gh_vm_loader_wait_for_os_status(sec_vm_struct,
+						GH_RM_OS_STATUS_BOOT);
+	if (ret)
+		goto err_reset_vm;
+
+	mutex_unlock(&sec_vm_struct->vm_lock);
+
+	return 0;
+
+err_reset_vm:
+	gh_rm_vm_reset(vmid);
+err_unpopulate_hyp_res:
+	gh_rm_unpopulate_hyp_res(vmid, vm_dev->vm_name);
+err_unload:
+	qcom_scm_pas_shutdown(vm_dev->pas_id);
+err_dealloc_vmid:
+	gh_rm_vm_dealloc_vmid(vmid);
+err_unlock:
+	mutex_unlock(&sec_vm_struct->vm_lock);
+	return ret;
 }
 
 static long gh_vm_loader_sec_ioctl(struct file *file,
 					unsigned int cmd, unsigned long arg)
 {
+	struct gh_vm_struct *vm_struct = file->private_data;
+
 	switch (cmd) {
+	case GH_VM_SEC_START:
+		return gh_vm_loader_sec_start(vm_struct);
 	default:
 		pr_err("Invalid IOCTL for secure loader\n");
 		break;
@@ -111,39 +336,104 @@ static int gh_vm_loader_sec_vm_init(struct gh_vm_struct *vm_struct)
 		return -ENOMEM;
 
 	mutex_init(&sec_vm_struct->vm_lock);
+	init_waitqueue_head(&sec_vm_struct->vm_status_wait);
 
 	sec_vm_struct->vm_struct = vm_struct;
 	sec_vm_struct->vm_dev = vm_dev;
+	sec_vm_struct->vm_status.vm_status = GH_RM_VM_STATUS_INIT;
+	sec_vm_struct->vmid = -EINVAL;
 
 	gh_vm_loader_set_loader_data(vm_struct, sec_vm_struct);
 
 	return 0;
 }
 
+static int gh_vm_loader_mem_probe(struct gh_sec_vm_dev *sec_vm_dev)
+{
+	struct device *dev = sec_vm_dev->dev;
+	struct device_node *node;
+	struct resource res;
+	phys_addr_t phys;
+	ssize_t size;
+	void *virt;
+	int ret;
+
+	node = of_parse_phandle(dev->of_node, "memory-region", 0);
+	if (!node) {
+		dev_err(dev, "DT error getting \"memory-region\"\n");
+		return -EINVAL;
+	}
+
+	ret = of_address_to_resource(node, 0, &res);
+	if (ret) {
+		dev_err(dev, "error %d getting \"memory-region\" resource\n",
+			ret);
+		goto err_of_node_put;
+	}
+
+	phys = res.start;
+	size = (size_t)resource_size(&res);
+	virt = memremap(phys, size, MEMREMAP_WC);
+	if (!virt) {
+		dev_err(dev, "Unable to remap firmware memory\n");
+		ret = -ENOMEM;
+		goto err_of_node_put;
+	}
+
+	sec_vm_dev->fw_phys = phys;
+	sec_vm_dev->fw_virt = virt;
+	sec_vm_dev->fw_size = size;
+
+err_of_node_put:
+	of_node_put(node);
+	return ret;
+}
+
 static int gh_vm_loader_sec_probe(struct platform_device *pdev)
 {
 	struct gh_sec_vm_dev *sec_vm_dev;
+	struct device *dev = &pdev->dev;
 	int ret;
 
-	sec_vm_dev = devm_kzalloc(&pdev->dev,
-				sizeof(*sec_vm_dev), GFP_KERNEL);
+	sec_vm_dev = devm_kzalloc(dev, sizeof(*sec_vm_dev), GFP_KERNEL);
 	if (!sec_vm_dev)
 		return -ENOMEM;
+
+	sec_vm_dev->dev = dev;
+	platform_set_drvdata(pdev, sec_vm_dev);
+
+	ret = of_property_read_u32(dev->of_node,
+				"qcom,pas-id", &sec_vm_dev->pas_id);
+	if (ret) {
+		dev_err(dev, "DT error getting \"qcom,pas-id\": %d\n", ret);
+		return ret;
+	}
+
+	ret = of_property_read_u32(dev->of_node,
+				"qcom,vmid", &sec_vm_dev->vmid);
+	if (ret) {
+		dev_err(dev, "DT error getting \"qcom,vmid\": %d\n", ret);
+		return ret;
+	}
+
+	ret = gh_vm_loader_mem_probe(sec_vm_dev);
+	if (ret)
+		return ret;
 
 	ret = of_property_read_string(pdev->dev.of_node, "qcom,firmware-name",
 				      &sec_vm_dev->vm_name);
 	if (ret)
-		return ret;
-
-	sec_vm_dev->dev = &pdev->dev;
+		goto err_unmap_fw;
 
 	spin_lock(&gh_sec_vm_devs_lock);
 	list_add(&sec_vm_dev->list, &gh_sec_vm_devs);
 	spin_unlock(&gh_sec_vm_devs_lock);
 
-	platform_set_drvdata(pdev, sec_vm_dev);
-
 	return 0;
+
+err_unmap_fw:
+	memunmap(sec_vm_dev->fw_virt);
+	return ret;
 }
 
 static int gh_vm_loader_sec_remove(struct platform_device *pdev)
@@ -155,6 +445,8 @@ static int gh_vm_loader_sec_remove(struct platform_device *pdev)
 	spin_lock(&gh_sec_vm_devs_lock);
 	list_del(&sec_vm_dev->list);
 	spin_unlock(&gh_sec_vm_devs_lock);
+
+	memunmap(sec_vm_dev->fw_virt);
 
 	return 0;
 }
