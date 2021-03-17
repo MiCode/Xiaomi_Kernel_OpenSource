551b59bb9fd9d357b6c0437a4553549a33cfd3bc
Suraj Jaiswal <jsuraj@codeaurora.org>
2021-03-17 22:51:58 +0530

    net: stmmac: disable & enable clock on suspend resume
    disable all eth clocks on suspend and enable them on resume.
    
    Change-Id: I65e80188e2cce0967b5bce8f0f5f55c842f40aa0
    Acked-by: Nagarjuna Chaganti <nchagant@qti.qualcomm.com>
    Signed-off-by: Suraj Jaiswal <jsuraj@codeaurora.org>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.c
@@ -924,6 +924,18 @@ static void qcom_ethqos_phy_suspend_clks(struct qcom_ethqos *ethqos)
 
 	ethqos_update_rgmii_clk_and_bus_cfg(ethqos, 0);
 
+	if (priv->plat->stmmac_clk)
+		clk_disable_unprepare(priv->plat->stmmac_clk);
+
+	if (priv->plat->pclk)
+		clk_disable_unprepare(priv->plat->pclk);
+
+	if (priv->plat->clk_ptp_ref)
+		clk_disable_unprepare(priv->plat->clk_ptp_ref);
+
+	if (ethqos->rgmii_clk)
+		clk_disable_unprepare(ethqos->rgmii_clk);
+
 	ETHQOSINFO("Exit\n");
 }
 
@@ -952,6 +964,18 @@ static void qcom_ethqos_phy_resume_clks(struct qcom_ethqos *ethqos)
 	struct stmmac_priv *priv = qcom_ethqos_get_priv(ethqos);
 
 	ETHQOSINFO("Enter\n");
+
+	if (priv->plat->stmmac_clk)
+		clk_prepare_enable(priv->plat->stmmac_clk);
+
+	if (priv->plat->pclk)
+		clk_prepare_enable(priv->plat->pclk);
+
+	if (priv->plat->clk_ptp_ref)
+		clk_prepare_enable(priv->plat->clk_ptp_ref);
+
+	if (ethqos->rgmii_clk)
+		clk_prepare_enable(ethqos->rgmii_clk);
 
 	if (qcom_ethqos_is_phy_link_up(ethqos))
 		ethqos_update_rgmii_clk_and_bus_cfg(ethqos, ethqos->speed);
