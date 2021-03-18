6b9c704c9ba7ff5af8b1a7bb59b88826582842e6
Anvoi Liu <anvoi.liu@mediatek.com>
2021-03-18 23:37:50 +0800

    [ALPS05600590] cam: add sensor streaming info
    [Detail]
    Print out frame_cnt to get sensor streaming status.
    
    MTK-Commit-Id: 14e2382f0181a370cb97c2893af1bf4fc3e997a7
    
    Change-Id: I2a3b5959d67eda12ea3d62bcb1f33fa47ac3dc5b
    CR-Id: ALPS05600590
    Feature: Image Sensor(imgsensor)
    Signed-off-by: Anvoi Liu <anvoi.liu@mediatek.com>
    (cherry picked from commit 317063e3f6b0e240d763c98dbaae0f8c9aa131ce)

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/imx576_mipi_raw/imx576mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/imx576_mipi_raw/imx576mipiraw_Sensor.c
@@ -2479,12 +2479,31 @@ static kal_uint32 imx576_awb_gain(struct SET_SENSOR_AWB_GAIN *pSetSensorAWB)
 	return ERROR_NONE;
 }
 
+static void check_stream_is_on(void)
+{
+	int i = 0;
+	UINT32 framecnt;
+	int timeout = (10000/imgsensor.current_fps)+1;
+
+	for (i = 0; i < timeout; i++) {
+
+		framecnt = read_cmos_sensor_8(0x0005);
+		if (framecnt != 0xFF) {
+			pr_debug("IMX576 stream is on, %d \\n", framecnt);
+			break;
+		}
+		pr_debug("IMX576 stream is not on %d \\n", framecnt);
+		mdelay(1);
+	}
+}
+
 static kal_uint32 streaming_control(kal_bool enable)
 {
 	LOG_INF("streaming_enable(0=Sw Standby,1=streaming): %d\n", enable);
-	if (enable)
+	if (enable) {
 		write_cmos_sensor_8(0x0100, 0x01);
-	else
+		check_stream_is_on();
+	} else
 		write_cmos_sensor_8(0x0100, 0x00);
 	return ERROR_NONE;
 }
