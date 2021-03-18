06f8edd4915a4dd77d33bcdd161681101f7cc47c
John Wei <john.wei@mediatek.com>
2021-03-18 23:37:06 +0800

    [ALPS05594504] cam: extend sensor mode to 20
    kernel 4.14 extend sensor mode to 20 (custom15)
    
    MTK-Commit-Id: 7b2844af5e177fcde73620d8d9e84a71a28f4179
    
    Change-Id: I9174cdcf60546b49120ba130ec47abc31bd7988b
    Signed-off-by: John Wei <john.wei@mediatek.com>
    CR-Id: ALPS05594504
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/imgsensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/imgsensor.c
@@ -696,7 +696,7 @@ static inline int adopt_CAMERA_HW_GetInfo2(void *pBuf)
 	PK_DBG("[%s]Entry%d\n", __func__, pSensorGetInfo->SensorId);
 
 	for (i = MSDK_SCENARIO_ID_CAMERA_PREVIEW;
-			i < MSDK_SCENARIO_ID_CUSTOM5;
+			i < MSDK_SCENARIO_ID_CUSTOM15;
 			i++) {
 		imgsensor_sensor_get_info(psensor, i, &info, &config);
 
