be3423dab13e634ebe25869091bd9f2aaf0dd6f0
Stanley Chu <stanley.chu@mediatek.com>
2021-03-18 23:41:51 +0800

    [ALPS05614188] scsi: ufs: Fix inv. tag handler and event history
    Fix
    1. OCS condition to trigger inv. tag handler
    2. Event history for linkup fail
    
    MTK-Commit-Id: c36b7a617605edd479e43d96ec947c867d3be82a
    
    Change-Id: I6ea24aa1ef63e94468820242f53965a521b5fdb1
    Signed-off-by: Stanley Chu <stanley.chu@mediatek.com>
    CR-Id: ALPS05614188
    Feature: UFS(Universal Flash Storage)
    (cherry picked from commit ab0c9ed8a0d8959299e0116d273e02f900c73724)

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -5075,8 +5075,6 @@ link_startup:
 
 		/* check if device is detected by inter-connect layer */
 		if (!ret && !ufshcd_is_device_present(hba)) {
-			ufshcd_update_evt_hist(hba, UFS_EVT_LINK_STARTUP_FAIL,
-					       0);
 			dev_err(hba->dev, "%s: Device not present\n", __func__);
 			ret = -ENXIO;
 			goto out;
@@ -5090,17 +5088,12 @@ link_startup:
 		 * but we can't be sure if the link is up until link startup
 		 * succeeds. So reset the local Uni-Pro and try again.
 		 */
-		if (ret && ufshcd_hba_enable(hba)) {
-			ufshcd_update_evt_hist(hba, UFS_EVT_LINK_STARTUP_FAIL,
-					       (u32)ret);
+		if (ret && ufshcd_hba_enable(hba))
 			goto out;
-		}
 	} while (ret && retries--);
 
 	if (ret) {
 		/* failed to get the link up... retire */
-		ufshcd_update_evt_hist(hba, UFS_EVT_LINK_STARTUP_FAIL,
-				       (u32)ret);
 		goto out;
 	}
 
@@ -5129,6 +5122,8 @@ link_startup:
 out:
 	if (ret) {
 		dev_err(hba->dev, "link startup failed %d\n", ret);
+		ufshcd_update_evt_hist(hba, UFS_EVT_LINK_STARTUP_FAIL,
+				       (u32)ret);
 		ufshcd_print_host_state(hba, 0, NULL, NULL, NULL);
 		ufshcd_print_pwr_info(hba);
 		ufshcd_print_host_regs(hba);
@@ -5677,7 +5672,7 @@ static int __ufshcd_transfer_req_compl(struct ufs_hba *hba,
 
 				result = (DID_REQUEUE << 16);
 				requeued_reqs |= (1UL << index);
-			} else if (ocs_err_status) {
+			} else if (ocs_err_status & 0xC0000000) {
 				result = (DID_FATAL << 16);
 			} else {
 				result = ufshcd_transfer_rsp_status(hba, lrbp, index);
