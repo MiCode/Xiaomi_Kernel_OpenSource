0f2cf7c9631dce85fc613872d6c4240dcb4498a8
Tzu-Hsien Wang <tzuhsien.wang@mediatek.com>
2021-03-19 19:06:22 +0800

    [ALPS05449559] GPU: Support MTK debugging features
    [Detail]
    01. Support CONFIG_MALI_MTK_DEBUG
    02. Support CONFIG_MALI_MTK_DEBUG_DFD
    
    MTK-Commit-Id: 16857991d020e709ac81a55e31508d57985683de
    
    Change-Id: I3f813fbc768b54de5935a355f0fabaf2094c8063
    Signed-off-by: Tzu-Hsien Wang <tzuhsien.wang@mediatek.com>
    CR-Id: ALPS05449559
    Feature: GPU Extension(gpu_extension)

--- drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r30p0/drivers/gpu/arm/midgard/mmu/backend/mali_kbase_mmu_jm.c
+++ drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r30p0/drivers/gpu/arm/midgard/mmu/backend/mali_kbase_mmu_jm.c
@@ -31,6 +31,9 @@
 #include <device/mali_kbase_device.h>
 #include <mali_kbase_as_fault_debugfs.h>
 #include "../mali_kbase_mmu_internal.h"
+#if IS_ENABLED(CONFIG_MALI_MTK_DEBUG)
+#include <mtk_gpufreq.h>
+#endif
 
 void kbase_mmu_get_as_setup(struct kbase_mmu_table *mmut,
 		struct kbase_mmu_setup * const setup)
@@ -126,6 +129,10 @@ void kbase_mmu_report_fault_and_kill(struct kbase_context *kctx,
 	exception_type = fault->status & 0xFF;
 	access_type = (fault->status >> 8) & 0x3;
 	source_id = (fault->status >> 16);
+
+#if IS_ENABLED(CONFIG_MALI_MTK_DEBUG)
+	mt_gpufreq_dump_infra_status();
+#endif
 
 	/* terminal fault, print info about the fault */
 	dev_err(kbdev->dev,
