bbfdc6b62522aab87fe3cf92733037ef2a5b413b
Chandana Kishori Chiluveru <cchiluve@codeaurora.org>
2021-03-22 11:23:34 +0530

    Revert "silmbus: ngd: register controller after power up."
    This reverts commit 94fe5f2b45c4108885e4b71f6b181068632ec904.
    
    Without revert driver removes child devices upon receiving
    audio PD is down. This leads to de-register of ALSA sound card
    and leading to scheduler stuck issue. Hence reverting
    "silmbus: ngd: register controller after power up." change.
    
    Change-Id: I47836db6ce775e03817d0b9128ce529483bb588e
    Signed-off-by: Chandana Kishori Chiluveru <cchiluve@codeaurora.org>

--- drivers/slimbus/qcom-ngd-ctrl.c
+++ drivers/slimbus/qcom-ngd-ctrl.c
@@ -1478,18 +1478,10 @@ static int qcom_slim_ngd_enable(struct qcom_slim_ngd_ctrl *ctrl, bool enable)
 
 		pm_runtime_mark_last_busy(ctrl->ctrl.dev);
 		pm_runtime_put(ctrl->ctrl.dev);
-
-		ret = slim_register_controller(&ctrl->ctrl);
-		if (ret) {
-			SLIM_ERR(ctrl, "error adding slim controller\n");
-			return ret;
-		}
-
-		SLIM_INFO(ctrl, "SLIM controller Registered\n");
+		SLIM_INFO(ctrl, "SLIM NGD Enable\n");
 	} else {
 		qcom_slim_qmi_exit(ctrl);
-		slim_unregister_controller(&ctrl->ctrl);
-		SLIM_INFO(ctrl, "SLIM controller Un-Registered\n");
+		SLIM_INFO(ctrl, "SLIM NGD Disable\n");
 	}
 
 	return 0;
@@ -1724,6 +1716,11 @@ static int qcom_slim_ngd_probe(struct platform_device *pdev)
 	int ret;
 
 	ctrl->ctrl.dev = dev;
+	ret = slim_register_controller(&ctrl->ctrl);
+	if (ret) {
+		dev_err(dev, "error adding slim controller\n");
+		return ret;
+	}
 
 	platform_set_drvdata(pdev, ctrl);
 	pm_runtime_use_autosuspend(dev);
@@ -1734,7 +1731,7 @@ static int qcom_slim_ngd_probe(struct platform_device *pdev)
 	ret = qcom_slim_ngd_qmi_svc_event_init(ctrl);
 	if (ret) {
 		dev_err(&pdev->dev, "QMI service registration failed:%d", ret);
-		return ret;
+		goto err;
 	}
 
 	INIT_WORK(&ctrl->m_work, qcom_slim_ngd_master_worker);
@@ -1751,6 +1748,8 @@ wq_err:
 	qcom_slim_ngd_qmi_svc_event_deinit(&ctrl->qmi);
 	if (ctrl->mwq)
 		destroy_workqueue(ctrl->mwq);
+err:
+	slim_unregister_controller(&ctrl->ctrl);
 
 	return ret;
 }
@@ -1940,7 +1939,7 @@ static int qcom_slim_ngd_remove(struct platform_device *pdev)
 	pm_runtime_disable(&pdev->dev);
 	pdr_handle_release(ctrl->pdr);
 	qcom_unregister_ssr_notifier(ctrl->notifier, &ctrl->nb);
-	qcom_slim_ngd_enable(ctrl, false);
+	slim_unregister_controller(&ctrl->ctrl);
 	qcom_slim_ngd_exit_dma(ctrl);
 	qcom_slim_ngd_qmi_svc_event_deinit(&ctrl->qmi);
 	if (ctrl->mwq)
