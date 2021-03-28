35881bcdb6b18c536db6006ee816ba9f4ea28f80
mtk13722 <jingjing.liu@mediatek.com>
2021-03-28 12:27:58 +0800

    [ALPS05515507] disp:ioctl fuzzer fail fix
    add dsi power status check ,before read dsi regs.
    
    MTK-Commit-Id: 9250361c01d3e1ad28104d4c7709dff76e3fe099
    
    Change-Id: I5be5036474d86e1664f0865748f74c86b9b73f15
    Signed-off-by: mtk13722 <jingjing.liu@mediatek.com>
    CR-Id: ALPS05515507
    Feature: Vulnerability Scan

--- drivers/gpu/drm/mediatek/mtk_dsi.c
+++ drivers/gpu/drm/mediatek/mtk_dsi.c
@@ -6145,9 +6145,14 @@ int Panel_Master_lcm_get_dsi_timing_entry(struct drm_crtc *crtc,
 		goto done;
 	}
 	mtk_drm_idlemgr_kick(__func__, &mtk_crtc->base, 0);
+	if (mtk_drm_top_clk_isr_get("mipi_get") == false) {
+		DDPINFO("%s, top clk off\n", __func__);
+		ret = -EINVAL;
+		goto done;
+	}
 
 	ret = PanelMaster_get_dsi_timing(dsi, type);
-
+	mtk_drm_top_clk_isr_put("mipi_get");
 
 done:
 	DDP_MUTEX_UNLOCK(&mtk_crtc->lock, __func__, __LINE__);
@@ -6169,8 +6174,14 @@ int Panel_Master_mipi_set_timing_entry(struct drm_crtc *crtc,
 		goto done;
 	}
 	mtk_drm_idlemgr_kick(__func__, &mtk_crtc->base, 0);
+	if (mtk_drm_top_clk_isr_get("mipi_set") == false) {
+		DDPINFO("%s, top clk off\n", __func__);
+		ret = -EINVAL;
+		goto done;
+	}
 
 	ret = PanelMaster_DSI_set_timing(dsi, timing);
+	mtk_drm_top_clk_isr_put("mipi_set");
 
 done:
 	DDP_MUTEX_UNLOCK(&mtk_crtc->lock, __func__, __LINE__);
