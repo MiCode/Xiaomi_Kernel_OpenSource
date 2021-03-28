c8f7060f50075c4621005a5715d0945bbe408776
Zuoray Lin <zuoray.lin@mediatek.com>
2021-03-28 12:29:51 +0800

    [ALPS05583076] apusys: midware fix access free req issue
    [Detail]
    Midware access freed dev request struct at one thread get dev sync
    but receive linux event w/o delete req from list
    The freed struct will be touch at other execution thread put dev
    
    [Solution]
    1. del req from req_list after wait complete with event
    2. protect req_list by query to protect del in multiple thread at same
    time
    
    MTK-Commit-Id: ebb8e70817c7f16c231a02737e9635f696fa8425
    
    Change-Id: Ifa02142ac5d46c986786f731c1dd7f822eaa55b5
    CR-Id: ALPS05583076
    Feature: [Module] APUSYS
    Signed-off-by: Zuoray Lin <zuoray.lin@mediatek.com>
    (cherry picked from commit 01ad766696fd735516c19610d108269759b31283)

--- drivers/misc/mediatek/apusys/midware/1.1/mdw_usr.c
+++ drivers/misc/mediatek/apusys/midware/1.1/mdw_usr.c
@@ -1027,6 +1027,8 @@ void mdw_usr_destroy(struct kref *kref)
 	struct mdw_mem *mm = NULL;
 	struct mdw_apu_cmd *c = NULL;
 	struct mdw_dev_info *d = NULL;
+	uint64_t sbmp = 0;
+	int nd_type = 0;
 
 	mutex_lock(&u_mgr.mtx);
 	mutex_lock(&u->mtx);
@@ -1047,13 +1049,22 @@ void mdw_usr_destroy(struct kref *kref)
 
 	list_for_each_safe(list_ptr, tmp, &u->sdev_list) {
 		d = list_entry(list_ptr, struct mdw_dev_info, u_item);
+		if (d->type >= APUSYS_DEVICE_MAX) {
+			mdw_drv_err("known device type(%d)\n", d->type);
+			continue;
+		}
+		/* disable secure mode */
+		nd_type = d->type % APUSYS_DEVICE_RT;
+		if (!((1ULL << nd_type) & sbmp)) {
+			sbmp |= (1ULL << nd_type);
+			d->sec_off(d);
+			mdw_drv_warn("dev(%d/%d) sec off\n", nd_type, d->type);
+		}
 		list_del(&d->u_item);
 		mdw_drv_warn("residual dev(%s%d)\n", d->name, d->idx);
 		d->pwr_off(d);
 		mdw_rsc_put_dev(d);
 	}
-	if (d)
-		d->sec_off(d);
 
 	mutex_unlock(&u->mtx);
 	list_del(&u->m_item);
