414e9e0dcef0ff181f011f00896904e9cc60947a
mtk13722 <jingjing.liu@mediatek.com>
2021-03-28 12:27:58 +0800

    [ALPS05409813] display: bypass infra ddr control
    [Detail]
    display ddr is controlled by display rdma and infra,
    ddr may be disabled unexpected when display still use it.
    
    [Solution]
    config infra sys register to bypass infra ddr control
    
    MTK-Commit-Id: 591ee008b62ec52cae0b82e8a2910afc2a5e7e07
    
    Change-Id: I800e69c1c03c6084dd0e4eca4eda6b5cb6a5e6a0
    Signed-off-by: mtk13722 <jingjing.liu@mediatek.com>
    CR-Id: ALPS05409813
    Feature: [Module]Display Driver

--- drivers/gpu/drm/mediatek/mtk_drm_ddp_comp.c
+++ drivers/gpu/drm/mediatek/mtk_drm_ddp_comp.c
@@ -115,6 +115,14 @@
 #define DVFS_HALT_MASK_SEL_WDMA0	REG_FLD_MSB_LSB(22, 22)
 #define DVFS_HALT_MASK_SEL_WDMA1	REG_FLD_MSB_LSB(23, 23)
 
+#define MT6877_INFRA_MEM_IDLE_ASYNC_2  0x178
+#define MT6877_MM_PORT0_AXI_IDLE_ASYNC  REG_FLD_MSB_LSB(2, 2)
+#define MT6877_MM_PORT1_AXI_IDLE_ASYNC  REG_FLD_MSB_LSB(3, 3)
+#define MT6877_INFRA_MEM_IDLE_ASYNC_3  0x17c
+#define MT6877_MDP2INFRA0_GALS_TX_AXI_IDLE  REG_FLD_MSB_LSB(12, 12)
+#define MT6877_COMM0_GALS_TX_AXI_IDLE  REG_FLD_MSB_LSB(13, 13)
+
+
 #define MT6833_INFRA_DISP_DDR_CTL  0x2C
 #define MT6833_INFRA_FLD_DDR_MASK  REG_FLD_MSB_LSB(7, 4)
 
@@ -830,6 +838,10 @@ void mt6877_mtk_sodi_config(struct drm_device *drm, enum mtk_ddp_comp_id id,
 	struct mtk_drm_private *priv = drm->dev_private;
 	unsigned int sodi_req_val = 0, sodi_req_mask = 0;
 	unsigned int emi_req_val = 0, emi_req_mask = 0;
+	unsigned int infra_req_val1 = 0, infra_req_mask1 = 0;
+	unsigned int infra_req_val2 = 0, infra_req_mask2 = 0;
+	unsigned int infra_req_val3 = 0, infra_req_mask3 = 0;
+	unsigned int infra_req_val4 = 0, infra_req_mask4 = 0;
 	bool en = *((bool *)data);
 
 	if (id == DDP_COMPONENT_ID_MAX) { /* config when top clk on */
@@ -882,6 +894,17 @@ void mt6877_mtk_sodi_config(struct drm_device *drm, enum mtk_ddp_comp_id id,
 	} else
 		return;
 
+	if (priv->data->bypass_infra_ddr_control) {
+		SET_VAL_MASK(infra_req_val1, infra_req_mask1,
+				0x1, MT6877_MM_PORT0_AXI_IDLE_ASYNC);
+		SET_VAL_MASK(infra_req_val2, infra_req_mask2,
+				0x1, MT6877_MM_PORT1_AXI_IDLE_ASYNC);
+		SET_VAL_MASK(infra_req_val3, infra_req_mask3,
+				0x1, MT6877_MDP2INFRA0_GALS_TX_AXI_IDLE);
+		SET_VAL_MASK(infra_req_val4, infra_req_mask4,
+				0x1, MT6877_COMM0_GALS_TX_AXI_IDLE);
+	}
+
 	if (handle == NULL) {
 		unsigned int v;
 
@@ -894,11 +917,39 @@ void mt6877_mtk_sodi_config(struct drm_device *drm, enum mtk_ddp_comp_id id,
 			& (~emi_req_mask));
 		v += (emi_req_val & emi_req_mask);
 		writel_relaxed(v, priv->config_regs +  MMSYS_EMI_REQ_CTL);
+		if (priv->data->bypass_infra_ddr_control) {
+			if (!IS_ERR(priv->infra_regs)) {
+				v = (readl(priv->infra_regs + MT6877_INFRA_MEM_IDLE_ASYNC_2)
+					| infra_req_mask1 | infra_req_mask2);
+				writel_relaxed(v, priv->infra_regs + MT6877_INFRA_MEM_IDLE_ASYNC_2);
+				v = (readl(priv->infra_regs + MT6877_INFRA_MEM_IDLE_ASYNC_3)
+					| infra_req_mask3 | infra_req_mask4);
+				writel_relaxed(v, priv->infra_regs + MT6877_INFRA_MEM_IDLE_ASYNC_3);
+			} else
+				DDPINFO("%s: failed to disable infra ddr control\n", __func__);
+		}
 	} else {
 		cmdq_pkt_write(handle, NULL, priv->config_regs_pa +
 			MMSYS_SODI_REQ_MASK, sodi_req_val, sodi_req_mask);
 		cmdq_pkt_write(handle, NULL, priv->config_regs_pa +
 			MMSYS_EMI_REQ_CTL, emi_req_val, emi_req_mask);
+		if (priv->data->bypass_infra_ddr_control) {
+			if (priv->infra_regs_pa) {
+				cmdq_pkt_write(handle, NULL,  priv->infra_regs_pa +
+						MT6877_INFRA_MEM_IDLE_ASYNC_2,
+						infra_req_val1, infra_req_mask1);
+				cmdq_pkt_write(handle, NULL,  priv->infra_regs_pa +
+						MT6877_INFRA_MEM_IDLE_ASYNC_2,
+						infra_req_val2, infra_req_mask2);
+				cmdq_pkt_write(handle, NULL,  priv->infra_regs_pa +
+						MT6877_INFRA_MEM_IDLE_ASYNC_3,
+						infra_req_val3, infra_req_mask3);
+				cmdq_pkt_write(handle, NULL,  priv->infra_regs_pa +
+						MT6877_INFRA_MEM_IDLE_ASYNC_3,
+						infra_req_val4, infra_req_mask4);
+			} else
+				DDPINFO("%s: failed to disable infra ddr control\n", __func__);
+		}
 	}
 }
 
