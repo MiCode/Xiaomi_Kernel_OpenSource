f01ad8d1526921c826c0a21d6804bcef5daff0cd
Lijuan Gao <lijuang@codeaurora.org>
2021-03-30 19:25:30 +0800

    arm: traps: emulate a MRRC instruction reading CNTVCT register
    Emulate CNTVCT access, so that userspace can get CNTVCT value,
    if the direct counter read is disabled. Also, keep direct
    access disabled by default for userspace.
    
    Change-Id: I70263c129386314880cb28d1e561146ce62d52b8
    Signed-off-by: Neeraj Upadhyay <neeraju@codeaurora.org>
    Signed-off-by: Cong Zhang <congzhan@codeaurora.org>
    Signed-off-by: Shadab Naseem <snaseem@codeaurora.org>
    Signed-off-by: Lijuan Gao <lijuang@codeaurora.org>

--- drivers/clocksource/arm_arch_timer.c
+++ drivers/clocksource/arm_arch_timer.c
@@ -26,6 +26,7 @@
 #include <linux/acpi.h>
 
 #include <asm/arch_timer.h>
+#include <asm/traps.h>
 #include <asm/virt.h>
 
 #include <clocksource/arm_arch_timer.h>
@@ -1519,6 +1520,7 @@ static int __init arch_timer_mem_of_init(struct device_node *np)
 	ret = arch_timer_mem_frame_register(frame);
 	if (!ret && !arch_timer_needs_of_probing())
 		ret = arch_timer_common_init();
+	get_timer_count_hook_init();
 out:
 	kfree(timer_mem);
 	return ret;
