3402a90f7709719557edc350ba2ae4587745d512
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2021-03-31 12:24:30 +0530

    USB: diag: Add check for ctxt in usb_diag_request_size()
    Diag char driver calls usb_diag_request_size() to get max request
    size supported by underlying USB hardware. If there is race between
    disable() and this API call, it results in accessing NULL pointer
    dereference. Hence fix this by returning zero in case ctxt is NULL.
    
    Change-Id: I51274db0af4cfbc8ba975744f55ad17ec311e32e
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>

--- drivers/usb/gadget/function/f_diag.c
+++ drivers/usb/gadget/function/f_diag.c
@@ -473,8 +473,12 @@ EXPORT_SYMBOL(usb_diag_alloc_req);
 int usb_diag_request_size(struct usb_diag_ch *ch)
 {
 	struct diag_context *ctxt = ch->priv_usb;
-	struct usb_composite_dev *cdev = ctxt->cdev;
+	struct usb_composite_dev *cdev;
 
+	if (!ctxt)
+		return 0;
+
+	cdev = ctxt->cdev;
 	if (cdev->gadget->is_chipidea)
 		return CI_MAX_REQUEST_SIZE;
 
