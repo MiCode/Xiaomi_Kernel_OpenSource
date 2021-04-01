d8fb063f85de576bbf5e4919eab6aa0ea32fdf07
mtk80276 <bin.fan@mediatek.com>
2021-04-01 11:41:15 +0800

    [ALPS05617841] disp: porting ovl1_2l for wfd
    add ovl1_2l to third path
    
    MTK-Commit-Id: ab22ff420f1e0ffc1fe3e6174e51e0408708ea40
    
    Change-Id: I272d76891e3ae7a2107f870750579ccba5616550
    Signed-off-by: mtk80276 <bin.fan@mediatek.com>
    CR-Id:ALPS05617841
    Feature:[Module]Display Driver
    (cherry picked from commit d8a75b25aa0ab2ed2e2d4d78924a80f259e5c346)

--- drivers/gpu/drm/mediatek/mtk_layering_rule_base.c
+++ drivers/gpu/drm/mediatek/mtk_layering_rule_base.c
@@ -1589,7 +1589,7 @@ static int mtk_lye_get_comp_id(int disp_idx, struct drm_device *drm_dev,
 		else
 			return DDP_COMPONENT_OVL0;
 	}
-#if defined(CONFIG_MACH_MT6885) || defined(CONFIG_MACH_MT6877)
+#if defined(CONFIG_MACH_MT6885) || defined(CONFIG_MACH_MT6893) || defined(CONFIG_MACH_MT6877)
 	else if (disp_idx == 1)
 		return DDP_COMPONENT_OVL2_2L;
 	else
