a58051d6c0fb0aa176dbb0dcbb22f41003a02680
Chunlei Wang <chunlei.wang@mediatek.com>
2021-04-02 08:49:57 +0800

    [ALPS05605222] mtprintk: enable mtprintk feature
    [Detail]
    enable mtprintk feature.
    
    [Solution]
    1. uart console control.
    2. kernel log much detect.
    
    MTK-Commit-Id: d12a53857678d9d376cddd817f32f418f70f0a3c
    
    Change-Id: I848c30c310d8e7975223a4d8533da1c60d67c5eb
    Signed-off-by: Chunlei Wang <chunlei.wang@mediatek.com>
    CR-Id: ALPS05605222
    Feature: Android Exception Engine(AEE)

---
+++ drivers/misc/mediatek/mtprintk/mtk_printk_ctrl.c
@@ -0,0 +1,408 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#include <linux/proc_fs.h>
+#include <linux/sched.h>
+#include <linux/printk.h>
+#include <linux/uaccess.h>
+#include <linux/seq_file.h>
+#include <linux/delay.h>
+#include <linux/sched/clock.h>
+#include <linux/moduleparam.h>
+#include <linux/console.h>
+#include <linux/module.h>
+#include <linux/kmsg_dump.h>
+#include <uapi/linux/sched/types.h>
+#include <linux/mm.h>
+#include <linux/slab.h>
+#include <linux/kthread.h>
+
+#include "aee.h"
+
+
+/* //////////////////////////////////////////////////////// */
+/* --------------------------------------------------- */
+/* Real work */
+/* --------------------------------------------------- */
+/*                     Define Proc entry               */
+/* --------------------------------------------------- */
+
+/*
+ * printk_ctrl:
+ * 0: uart printk disable
+ * 1: uart printk enable
+ * 2: uart printk always enable
+ */
+
+static struct proc_dir_entry *entry;
+
+#ifdef CONFIG_MTK_PRINTK_UART_CONSOLE
+static int printk_ctrl = 0;
+module_param_named(disable_uart, printk_ctrl, int, 0644);
+
+bool mt_get_uartlog_status(void)
+{
+	if (printk_ctrl == 0)
+		return false;
+	else if ((printk_ctrl == 1) || (printk_ctrl == 2))
+		return true;
+	return true;
+}
+
+void mt_disable_uart(void)
+{
+	struct console *bcon = NULL;
+
+	/* uart print not always enable */
+	if (printk_ctrl != 2) {
+		printk_ctrl = 0;
+		for_each_console(bcon) {
+			pr_info("console name: %s, status 0x%x.\n", bcon->name, bcon->flags);
+			if (!strncmp(bcon->name, "ttyS", 4)) {
+				bcon->flags &= ~CON_ENABLED;
+				return;
+			}
+		}
+	}
+}
+
+void mt_enable_uart(void)
+{
+	struct console *bcon = NULL;
+
+	printk_ctrl = 1;
+	for_each_console(bcon) {
+		pr_info("console name: %s. status 0x%x.\n", bcon->name, bcon->flags);
+		if (!strncmp(bcon->name, "ttyS", 4)) {
+			bcon->flags |= CON_ENABLED;
+			return;
+		}
+	}
+}
+
+#else
+void mt_disable_uart(void)
+{
+	return;
+}
+
+void mt_enable_uart(void)
+{
+	return;
+}
+#endif
+EXPORT_SYMBOL_GPL(mt_disable_uart);
+EXPORT_SYMBOL_GPL(mt_enable_uart);
+
+
+#ifdef CONFIG_LOG_TOO_MUCH_WARNING
+static int detect_count = CONFIG_LOG_TOO_MUCH_DETECT_COUNT;
+static bool logmuch_enable;
+static int logmuch_exit;
+static void *log_much;
+static int log_much_len = 1 << (CONFIG_LOG_BUF_SHIFT + 1);
+struct proc_dir_entry *logmuch_entry;
+static u32 log_count;
+DECLARE_WAIT_QUEUE_HEAD(logmuch_thread_exit);
+void mt_print_much_log(void)
+{
+	unsigned long long t1 = 0;
+	unsigned long long t2 = 0;
+	unsigned long print_num = 0;
+
+	t1 = sched_clock();
+	pr_info("printk debug log: start time: %lld.\n", t1);
+
+	for (;;) {
+		t2 = sched_clock();
+		if ((t2 - t1) / 1000000 > 10 * 1000)
+			break;
+		pr_info("printk debug log: the %ld line, time: %lld.\n",
+			print_num++, t2);
+		__delay(5);
+	}
+	pr_info("mt log total write %ld line in 10 second.\n", print_num);
+}
+
+void set_logtoomuch_enable()
+{
+	logmuch_enable = true;
+	return;
+}
+
+void set_logtoomuch_disable()
+{
+	logmuch_enable = false;
+	return;
+}
+
+void set_detect_count(int val)
+{
+	pr_info("set log_much detect value %d.\n", val);
+	detect_count = val;
+	return;
+}
+
+int get_detect_count(void)
+{
+	pr_info("get log_much detect value %d.\n", detect_count);
+	return detect_count;
+}
+
+
+static int logmuch_dump_thread(void *arg)
+{
+	/* unsigned long flags; */
+	struct sched_param param = {
+		.sched_priority = 99
+	};
+	struct kmsg_dumper dumper = { .active = true };
+	size_t len = 0;
+	char aee_str[63] = {0};
+	int add_len;
+	u64 last_seq;
+	unsigned long long old = 0;
+	unsigned long long now = 0;
+	unsigned long long period = 0;
+
+	sched_setscheduler(current, SCHED_FIFO, &param);
+	log_much = kmalloc(log_much_len, GFP_KERNEL);
+	if (log_much == NULL) {
+		proc_remove(logmuch_entry);
+		pr_notice("printk: failed to create 0x%x log_much memory.\n", log_much_len);
+		return 1;
+	}
+
+	/* don't detect boot up phase*/
+	wait_event_interruptible_timeout(logmuch_thread_exit, logmuch_exit == 1, 60 * HZ);
+
+	while (1) {
+		if (log_much == NULL)
+			break;
+		old = sched_clock();
+		kmsg_dump_rewind(&dumper);
+		last_seq = dumper.next_seq;
+		wait_event_interruptible_timeout(logmuch_thread_exit, logmuch_exit == 1, 5 * HZ);
+		if (logmuch_enable == false)
+			continue;
+
+		now = sched_clock();
+		kmsg_dump_rewind(&dumper);
+		log_count = dumper.next_seq - last_seq;
+		period = now - old;
+		do_div(period, 1000000);
+		pr_info("log_much detect count %d, time %llu-%llu, %llu.\n", log_count, old, now, period);
+		if (period / 100 * detect_count < log_count * 10) {
+			pr_info("log_much detect.\n");
+			if (log_much == NULL) 
+				break;
+			memset((char *)log_much, 0, log_much_len);
+			kmsg_dump_rewind(&dumper);
+			kmsg_dump_get_buffer(&dumper, true, (char *)log_much,
+								 log_much_len, &len);
+			memset(aee_str, 0, 63);
+			add_len = scnprintf(aee_str, 63,
+					"Printk too much: >%d L/s, L: %llu, S: %llu.%03lu\n",
+					detect_count, log_count, period/1000, period%1000);
+			aee_kernel_warning_api(__FILE__, __LINE__,
+					DB_OPT_PRINTK_TOO_MUCH | DB_OPT_DUMMY_DUMP,
+					aee_str, "Need to shrink kernel log");
+			pr_info("log_much detect %d log.\n", len);
+			wait_event_interruptible_timeout(logmuch_thread_exit, 
+				logmuch_exit == 1, 60 * CONFIG_LOG_TOO_MUCH_DETECT_GAP * HZ);
+		}
+	}
+	pr_notice("[log_much] logmuch_Detect dump thread exit.\n");
+	logmuch_exit = 0;
+	return 0;
+}
+
+static int log_much_show(struct seq_file *m, void *v)
+{
+	if (log_much == NULL) {
+		seq_puts(m, "log_much buff is null.\n");
+		return 0;
+	}
+	seq_write(m, (char *)log_much, log_much_len);
+	return 0;
+}
+
+static int log_much_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, log_much_show, inode->i_private);
+}
+
+static const struct proc_ops log_much_ops = {
+	.proc_open = log_much_open,
+	.proc_read = seq_read,
+	.proc_lseek = seq_lseek,
+	.proc_release = single_release,
+};
+#else
+void set_detect_count(int val)
+{
+	return;
+}
+int get_detect_count(void)
+{
+	return 0;
+}
+#endif
+EXPORT_SYMBOL_GPL(set_detect_count);
+EXPORT_SYMBOL_GPL(get_detect_count);
+
+static int mt_printk_ctrl_show(struct seq_file *m, void *v)
+{
+	seq_puts(m, "=== mt printk controller ===\n");
+#ifdef CONFIG_MTK_PRINTK_UART_CONSOLE
+	seq_puts(m, "0:   printk uart disable\n");
+	seq_puts(m, "1:   printk uart enable\n");
+#endif
+#ifdef CONFIG_LOG_TOO_MUCH_WARNING
+	seq_puts(m, "2:   printk too much disable\n");
+	seq_puts(m, "3:   printk too much enable\n");
+	seq_puts(m, "4:   printk too much log in 10 seconds.\n");
+	seq_puts(m,
+"xxx: printk too much detect count(xxx represents for a integer > 50)\n");
+	seq_printf(m, "log_much detect %d Line, %d sieze.\n", log_count, log_much_len);
+#endif
+	seq_puts(m, "=== mt printk controller ===\n\n");
+#ifdef CONFIG_MTK_PRINTK_UART_CONSOLE
+	seq_printf(m, "printk uart enable: %d\n", mt_get_uartlog_status());
+#endif
+#ifdef CONFIG_LOG_TOO_MUCH_WARNING
+	seq_printf(m, "printk too much enable: %d.\n", logmuch_enable);
+	seq_printf(m, "printk too much detect count: %d\n", detect_count);
+#endif
+	return 0;
+}
+
+static ssize_t mt_printk_ctrl_write(struct file *filp,
+	const char *ubuf, size_t cnt, loff_t *data)
+{
+	char buf[64];
+	long val;
+	int ret;
+
+	if (cnt >= sizeof(buf))
+		return -EINVAL;
+
+	if (copy_from_user(&buf, ubuf, cnt))
+		return -EFAULT;
+
+	buf[cnt] = 0;
+
+	ret = kstrtoul(buf, 10, (unsigned long *)&val);
+
+	if (ret < 0)
+		return ret;
+
+	switch (val) {
+#ifdef CONFIG_MTK_PRINTK_UART_CONSOLE
+	case 0:
+		mt_disable_uart();
+		break;
+	case 1:
+		mt_enable_uart();
+		break;
+	case 5:
+		mt_enable_uart();
+		printk_ctrl = 2;
+		break;
+#endif
+#ifdef CONFIG_LOG_TOO_MUCH_WARNING
+	case 2:
+		set_logtoomuch_disable();
+		break;
+	case 3:
+		set_logtoomuch_enable();
+		break;
+	case 4:
+		mt_print_much_log();
+		break;
+	default:
+		if (val > 50)
+			set_detect_count(val);
+		break;
+#else
+	default:
+		break;
+#endif
+	}
+	return cnt;
+}
+
+/*** Seq operation of mtprof ****/
+static int mt_printk_ctrl_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, mt_printk_ctrl_show, inode->i_private);
+}
+
+static const struct proc_ops mt_printk_ctrl_fops = {
+	.proc_open = mt_printk_ctrl_open,
+	.proc_write = mt_printk_ctrl_write,
+	.proc_read = seq_read,
+	.proc_lseek = seq_lseek,
+	.proc_release = single_release,
+};
+
+static int __init mt_printk_ctrl_init(void)
+{
+#ifdef CONFIG_LOG_TOO_MUCH_WARNING
+	static struct task_struct *hd_thread;
+#endif
+
+	entry = proc_create("mtprintk", 0664, NULL, &mt_printk_ctrl_fops);
+	if (!entry)
+		return -ENOMEM;
+
+#ifdef CONFIG_LOG_TOO_MUCH_WARNING
+//	register_trace_android_vh_printk_store(mtk_printk_store, NULL);
+	logmuch_entry = proc_create("log_much", 0444, NULL, &log_much_ops);
+	if (!logmuch_entry) {
+		pr_notice("printk: failed to create proc log_much entry\n");
+		return 1;
+	}
+
+	hd_thread = kthread_create(logmuch_dump_thread, NULL, "logmuch_detect");
+	if (hd_thread)
+		wake_up_process(hd_thread);
+#endif
+	return 0;
+}
+
+
+#ifdef MODULE
+static void __exit mt_printk_ctrl_exit(void)
+{
+	if (entry)
+		proc_remove(entry);
+
+#ifdef CONFIG_LOG_TOO_MUCH_WARNING
+	if (logmuch_entry)
+		proc_remove(logmuch_entry);
+
+	if (log_much) {
+		kfree(log_much);
+		log_much = NULL;
+	}
+	logmuch_enable = false;
+	logmuch_exit = 1;
+	wake_up_interruptible(&logmuch_thread_exit);
+	while(logmuch_exit == 1)
+		ssleep(5);
+#endif
+pr_notice("log_much exit.");
+
+}
+
+module_init(mt_printk_ctrl_init);
+module_exit(mt_printk_ctrl_exit);
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("MediaTek Printk enhance");
+MODULE_AUTHOR("MediaTek Inc.");
+#else
+device_initcall(mt_printk_ctrl_init);
+#endif
