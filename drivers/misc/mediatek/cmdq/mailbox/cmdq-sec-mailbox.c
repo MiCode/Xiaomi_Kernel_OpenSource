f14e881888c52f074360e9bde8c9a4aba07a4bc5
Ming-Fan Chen <ming-fan.chen@mediatek.com>
2021-04-04 16:55:12 +0800

    [ALPS05555394] cmdq: use clk_prepare/clk_unprepare directly
    use clk_prepare/clk_unprepare directly in cmdq_sec_mbox_switch_normal()
    
    MTK-Commit-Id: af15ba723c8716cbd6eca503162c83fae3918ece
    
    Change-Id: If537baf7dbcacf7b157abba4dbbf88ffebfd6552
    CR-Id: ALPS05555394
    Feature: [Module]CMDQ Driver
    Signed-off-by: Ming-Fan Chen <ming-fan.chen@mediatek.com>
    (cherry picked from commit 161149164b6ca63b4f3ac985a996675707dd6989)

--- drivers/misc/mediatek/cmdq/mailbox/cmdq-sec-mailbox.c
+++ drivers/misc/mediatek/cmdq/mailbox/cmdq-sec-mailbox.c
@@ -1223,7 +1223,7 @@ void cmdq_sec_mbox_switch_normal(struct cmdq_client *cl)
 	struct cmdq_sec_thread *thread =
 		(struct cmdq_sec_thread *)cl->chan->con_priv;
 
-	cmdq_sec_resume(cmdq->mbox.dev);
+	WARN_ON(clk_prepare(cmdq->clock) < 0);
 	cmdq_sec_clk_enable(cmdq);
 	cmdq_log("[ IN] %s: cl:%p cmdq:%p thrd:%p idx:%u\n",
 		__func__, cl, cmdq, thread, thread->idx);
@@ -1237,7 +1237,7 @@ void cmdq_sec_mbox_switch_normal(struct cmdq_client *cl)
 	cmdq_log("[OUT] %s: cl:%p cmdq:%p thrd:%p idx:%u\n",
 		__func__, cl, cmdq, thread, thread->idx);
 	cmdq_sec_clk_disable(cmdq);
-	cmdq_sec_suspend(cmdq->mbox.dev);
+	clk_unprepare(cmdq->clock);
 #endif
 }
 EXPORT_SYMBOL(cmdq_sec_mbox_switch_normal);
