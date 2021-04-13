ef4d183ed069307d5bcdef55ffd3831c9b8568d3
Wu Gao <wugao@codeaurora.org>
2021-04-13 15:02:46 +0800

    cnss2: Add new flag to check drv support
    There is illegal logic when call cnss_pci_suspend and check DRV
    support. This change adds new flag - drv_supported to replace
    cnss_pci_get_drv_connected and fixes this issue.
    
    Change-Id: Ia836e06283ebe08483e325717fb30bac333ef5b7
    Signed-off-by: Wu Gao <wugao@codeaurora.org>

--- drivers/net/wireless/cnss2/pci.c
+++ drivers/net/wireless/cnss2/pci.c
@@ -3021,6 +3021,7 @@ static bool cnss_pci_is_drv_supported(struct cnss_pci_data *pci_priv)
 
 	if (!root_port) {
 		cnss_pr_err("PCIe DRV is not supported as root port is null\n");
+		pci_priv->drv_supported = false;
 		return drv_supported;
 	}
 
@@ -3032,6 +3033,7 @@ static bool cnss_pci_is_drv_supported(struct cnss_pci_data *pci_priv)
 
 	cnss_pr_dbg("PCIe DRV is %s\n",
 		    drv_supported ? "supported" : "not supported");
+	pci_priv->drv_supported = drv_supported;
 
 	return drv_supported;
 }
@@ -3268,7 +3270,7 @@ static int cnss_pci_suspend(struct device *dev)
 		goto out;
 
 	if (!test_bit(DISABLE_DRV, &plat_priv->ctrl_params.quirks) &&
-	    cnss_pci_get_drv_connected(pci_priv)) {
+	    pci_priv->drv_supported) {
 		pci_priv->drv_connected_last =
 			cnss_pci_get_drv_connected(pci_priv);
 		if (!pci_priv->drv_connected_last) {
@@ -3411,7 +3413,7 @@ static int cnss_pci_runtime_suspend(struct device *dev)
 	}
 
 	if (!test_bit(DISABLE_DRV, &plat_priv->ctrl_params.quirks) &&
-	    cnss_pci_get_drv_connected(pci_priv)) {
+	    pci_priv->drv_supported) {
 		pci_priv->drv_connected_last =
 			cnss_pci_get_drv_connected(pci_priv);
 		if (!pci_priv->drv_connected_last) {
