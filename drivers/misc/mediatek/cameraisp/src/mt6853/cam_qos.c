ba525e8ba7aa6839a6f78c7d84dd49b4c6304d26
Roy Yang <roy.yang@mediatek.com>
2021-04-17 15:44:18 +0800

    [ALPS05670553] ISP: enable PM_QOS
    [Detail]
    enable PM QOS for mt6833
    
    MTK-Commit-Id: 0b60af477f5b95ec22381ba5761aa833cbbe2045
    
    Change-Id: Ic929327741ad0a67453a8a3ac01ff4440ba40780
    CR-Id: ALPS05670553
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: Roy Yang <roy.yang@mediatek.com>

--- drivers/misc/mediatek/cameraisp/src/mt6853/cam_qos.c
+++ drivers/misc/mediatek/cameraisp/src/mt6853/cam_qos.c
@@ -27,8 +27,8 @@
 //#define EP_STAGE
 //#define EP_PMQOS
 #elif defined CONFIG_MACH_MT6833
-//#define CONFIG_MTK_QOS_SUPPORT_ENABLE
-#define EP_STAGE
+#define CONFIG_MTK_QOS_SUPPORT_ENABLE
+//#define EP_STAGE
 #define EP_PMQOS
 #elif defined CONFIG_MACH_MT6877
 //#define CONFIG_MTK_QOS_SUPPORT_ENABLE
