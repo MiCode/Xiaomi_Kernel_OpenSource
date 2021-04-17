e20acfb807d82b1b79837f1e0241314822f03ca8
ot900543 <ot_lining.fan@mediatek.com>
2021-04-17 15:43:42 +0800

    [ALPS05605411] usnd: fix 6833 ipi_table
    Modify platform 6833 kernel to correspond to its SCP
    
    MTK-Commit-Id: fa82532b30fe4ceb1763fb1aa2e7dee6f26a756b
    
    Change-Id: If2c37caa5e83bc90c286d5ad569e9faf56c0788a
    CR-Id: ALPS05605411
    Signed-off-by: ot900543 <ot_lining.fan@mediatek.com>
    Feature: Ultrasound Proximity Sensor

--- drivers/misc/mediatek/scp/mt6833/scp_ipi_pin.h
+++ drivers/misc/mediatek/scp/mt6833/scp_ipi_pin.h
@@ -69,6 +69,11 @@ enum {
 	/* the following will use mbox4 */
 	IPI_OUT_SCP_MPOOL_1       = 24,
 	IPI_IN_SCP_MPOOL_1        = 25,
+
+	/* ultrasound will use core 0 and mbox 1*/
+	IPI_OUT_AUDIO_ULTRA_SND_0 =  26,
+	IPI_IN_AUDIO_ULTRA_SND_ACK_0 = 27,
+	IPI_IN_AUDIO_ULTRA_SND_0  =  28,
 	SCP_IPI_COUNT
 };
 
--- drivers/misc/mediatek/scp/mt6833/scp_mbox_layout.h
+++ drivers/misc/mediatek/scp/mt6833/scp_mbox_layout.h
@@ -30,17 +30,19 @@
 #define PIN_OUT_C_SIZE_SLEEP_0          1 /* the following will use mbox 1 */
 #define PIN_OUT_R_SIZE_SLEEP_0          1 /* the following will use mbox 1 */
 #define PIN_OUT_SIZE_TEST_0		 1 /* the following will use mbox 1 */
+#define PIN_OUT_SIZE_AUDIO_ULTRA_SND_0	 9 /* the following will use mbox 1 */
 
 /* definition of slot size for received PINs */
 #define PIN_IN_SIZE_APCCCI_0		 2 /* the following will use mbox 1 */
 #define PIN_IN_SIZE_SCP_ERROR_INFO_0	10 /* the following will use mbox 1 */
 #define PIN_IN_SIZE_SCP_READY_0		 1 /* the following will use mbox 1 */
 #define PIN_IN_SIZE_SCP_RAM_DUMP_0	 2 /* the following will use mbox 1 */
+#define PIN_IN_SIZE_AUDIO_ULTRA_SND_0	 5 /* the following will use mbox 1 */
+#define PIN_IN_SIZE_AUDIO_ULTRA_SND_ACK_0 2 /* the following will use mbox 1 */
 /* ============================================================ */
 
 /* core1 */
 /* definition of slot size for send PINs */
-#define PIN_OUT_SIZE_AUDIO_ULTRA_SND_1	 2 /* the following will use mbox 3 */
 #define PIN_OUT_SIZE_DVFS_SET_FREQ_1	 1 /* the following will use mbox 3 */
 #define PIN_OUT_C_SIZE_SLEEP_1	         1 /* the following will use mbox 3 */
 #define PIN_OUT_R_SIZE_SLEEP_1	         1 /* the following will use mbox 3 */
@@ -49,7 +51,6 @@
 #define PIN_OUT_SIZE_SCPCTL_1		 2 /* the following will use mbox 3 */
 
 /* definition of slot size for received PINs */
-#define PIN_IN_SIZE_AUDIO_ULTRA_SND_1	 2 /* the following will use mbox 3 */
 #define PIN_IN_SIZE_SCP_ERROR_INFO_1	10 /* the following will use mbox 3 */
 #define PIN_IN_SIZE_LOGGER_CTRL		 6 /* the following will use mbox 3 */
 #define PIN_IN_SIZE_SCP_READY_1		 1 /* the following will use mbox 3 */
