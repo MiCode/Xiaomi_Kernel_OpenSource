95f59ade939a97a9da573c70d79032f51317792c
Ram Chandrasekar <rkumbako@codeaurora.org>
2021-04-23 10:41:41 -0700

    dt-bindings: thermal: Add new sensors support for QMI sensors
    Add support for the below list of QMI sensor macros,
    QMI_QTM_THERM
    QMI_BCL_WARN
    QMI_SDR0_PA0
    QMI_SDR0_PA1
    QMI_SDR0_PA2
    QMI_SDR0_PA3
    QMI_SDR0_PA4
    QMI_SDR0_PA5
    QMI_SDR0
    QMI_SDR1_PA0
    QMI_SDR1_PA1
    QMI_SDR1_PA2
    QMI_SDR1_PA3
    QMI_SDR1_PA4
    QMI_SDR1_PA5
    QMI_SDR1
    QMI_MMW0
    QMI_MMW1
    QMI_MMW2
    QMI_MMW3
    QMI_MMW_IFIC0
    
    This will enable devicetree configuration to select these
    sensor channels.
    
    Change-Id: I843790438e5a31fd9dc737f6a84cf847b1619e4d
    Signed-off-by: Ram Chandrasekar <rkumbako@codeaurora.org>

--- include/dt-bindings/thermal/thermal_qti.h
+++ include/dt-bindings/thermal/thermal_qti.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
-/* Copyright (c) 2020, The Linux Foundation. All rights reserved. */
+/* Copyright (c) 2020-2021, The Linux Foundation. All rights reserved. */
 
 #include <dt-bindings/thermal/thermal.h>
 
@@ -41,7 +41,7 @@
 #define QMI_MMW_PA2		27
 #define QMI_MMW_PA3		28
 #define QMI_SDR_MMW		29
-#define QMI_MSM_SKIN	30
+#define QMI_MSM_SKIN		30
 #define QMI_BEAMER_N_THERM	31
 #define QMI_BEAMER_E_THERM	32
 #define QMI_BEAMER_W_THERM	33
@@ -51,6 +51,27 @@
 #define QMI_QFE_WTR_PA2_FR1	37
 #define QMI_QFE_WTR_PA3_FR1	38
 #define QMI_QFE_WTR0_FR1	39
+#define QMI_QTM_THERM		40
+#define QMI_BCL_WARN		41
+#define QMI_SDR0_PA0		42
+#define QMI_SDR0_PA1		43
+#define QMI_SDR0_PA2		44
+#define QMI_SDR0_PA3		45
+#define QMI_SDR0_PA4		46
+#define QMI_SDR0_PA5		47
+#define QMI_SDR0		48
+#define QMI_SDR1_PA0		49
+#define QMI_SDR1_PA1		50
+#define QMI_SDR1_PA2		51
+#define QMI_SDR1_PA3		52
+#define QMI_SDR1_PA4		53
+#define QMI_SDR1_PA5		54
+#define QMI_SDR1		55
+#define QMI_MMW0		56
+#define QMI_MMW1		57
+#define QMI_MMW2		58
+#define QMI_MMW3		59
+#define QMI_MMW_IFIC0		60
 
 #define QMI_MODEM_INST_ID	0x0
 #define QMI_ADSP_INST_ID	0x1
