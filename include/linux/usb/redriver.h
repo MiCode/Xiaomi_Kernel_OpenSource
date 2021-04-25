4e87a9ffeeb7c890f94bc8361cead5572ab68ef4
Linyu Yuan <linyyuan@codeaurora.org>
2021-04-25 15:50:28 +0800

    usb: misc: nb7vpq904m: fix issue when connect USB hub
    when redriver connect to a USB hub, and do adb root operation,
    due to redriver rx termination detection issue,
    hub will not detct device logical removal.
    workaround to temp disable/enable redriver when usb pullup operation.
    
    Change-Id: Ia3b0c99a28dd7d0601e7c5bcc612c4c48447c165
    Signed-off-by: Linyu Yuan <linyyuan@codeaurora.org>

--- include/linux/usb/redriver.h
+++ include/linux/usb/redriver.h
@@ -12,6 +12,7 @@ int redriver_release_usb_lanes(struct device_node *node);
 int redriver_notify_connect(struct device_node *node);
 int redriver_notify_disconnect(struct device_node *node);
 int redriver_orientation_get(struct device_node *node);
+int redriver_gadget_pullup(struct device_node *node, int is_on);
 
 #else
 
@@ -33,6 +34,11 @@ static inline int redriver_notify_disconnect(struct device_node *node)
 static inline int redriver_orientation_get(struct device_node *node)
 {
 	return -ENODEV;
+}
+
+static inline int redriver_gadget_pullup(struct device_node *node, int is_on)
+{
+	return 0;
 }
 
 #endif
