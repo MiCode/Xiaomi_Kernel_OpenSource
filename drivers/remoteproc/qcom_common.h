704abc53645162898d9a55af3e6e5dadc0c157e0
Bjorn Andersson <bjorn.andersson@linaro.org>
2021-04-26 11:15:47 -0700

    BACKPORT: remoteproc: sysmon: Expose the shutdown result
    A graceful shutdown of the Qualcomm remote processors where
    traditionally performed by invoking a shared memory state signal and
    waiting for the associated ack.
    
    This was later superseded by the "sysmon" mechanism, where some form of
    shared memory bus is used to send a "graceful shutdown request" message
    and one of more signals comes back to indicate its success.
    
    But when this newer mechanism is in effect the firmware is shut down by
    the time the older mechanism, implemented in the remoteproc drivers,
    attempts to perform a graceful shutdown - and as such it will never
    receive an ack back.
    
    This patch therefor track the success of the latest shutdown attempt in
    sysmon and exposes a new function in the API that the remoteproc driver
    can use to query the success and the necessity of invoking the older
    mechanism.
    
    Tested-by: Steev Klimaszewski <steev@kali.org>
    Reviewed-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>
    Link: https://lore.kernel.org/r/20201122054135.802935-3-bjorn.andersson@linaro.org
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Change-Id: I9d4f6e9ec9b8023dba4713e35f2c57bba5864973
    Git-commit: 5c212aaf5457ca5bd99aba3ad29a4a17f8129939
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    [sidgup@codeaurora.org: cherry pick for downstream functionality]
    Signed-off-by: Siddharth Gupta <sidgup@codeaurora.org>

--- drivers/remoteproc/qcom_common.h
+++ drivers/remoteproc/qcom_common.h
@@ -73,6 +73,7 @@ struct qcom_sysmon *qcom_add_sysmon_subdev(struct rproc *rproc,
 					   const char *name,
 					   int ssctl_instance);
 void qcom_remove_sysmon_subdev(struct qcom_sysmon *sysmon);
+bool qcom_sysmon_shutdown_acked(struct qcom_sysmon *sysmon);
 uint32_t qcom_sysmon_get_txn_id(struct qcom_sysmon *sysmon);
 #else
 static inline struct qcom_sysmon *qcom_add_sysmon_subdev(struct rproc *rproc,
@@ -84,6 +85,11 @@ static inline struct qcom_sysmon *qcom_add_sysmon_subdev(struct rproc *rproc,
 
 static inline void qcom_remove_sysmon_subdev(struct qcom_sysmon *sysmon)
 {
+}
+
+static inline bool qcom_sysmon_shutdown_acked(struct qcom_sysmon *sysmon)
+{
+	return false;
 }
 
 static inline uint32_t qcom_sysmon_get_txn_id(struct qcom_sysmon *sysmon)
