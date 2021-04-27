68023e66a68247cf73acffa95ce771c7824ee14d
Browse Zhang <browse.zhang@mediatek.com>
2021-04-28 00:07:53 +0800

    [ALPS05564942] mt6781: emi_mpu control option change
    change mt6781 to new config: CONFIG_MEDIATEK_EMI
    
    MTK-Commit-Id: 4c8170c2dfaae62c877e0cdaccc978aeedaef01f
    
    Change-Id: Ie5ef9b110989884409a3d429967469cd23a1dca4
    Signed-off-by: Browse Zhang <browse.zhang@mediatek.com>
    CR-Id: ALPS05564942
    Feature: TinySys

--- drivers/misc/mediatek/scp/mt6785/scp_feature_define.h
+++ drivers/misc/mediatek/scp/mt6785/scp_feature_define.h
@@ -40,10 +40,9 @@
 
 /* scp sub feature register API marco*/
 #define SCP_REGISTER_SUB_SENSOR		(1)
-
-/* emi mpu define*/
-#define ENABLE_SCP_EMI_PROTECTION	(1)
 #define MPU_REGION_ID_SCP_SMEM		(7)
+#define MPU_DOMAIN_D0                0
+#define MPU_DOMAIN_D3                3
 
 /* scp feature ID list */
 enum feature_id {
--- drivers/misc/mediatek/scp/v01/scp_helper.c
+++ drivers/misc/mediatek/scp/v01/scp_helper.c
@@ -60,7 +60,10 @@
 #include "scp_reservedmem_define.h"
 #endif
 
-#if ENABLE_SCP_EMI_PROTECTION
+#ifdef CONFIG_MEDIATEK_EMI
+#include "memory/mediatek/emi.h"
+#endif
+#ifdef CONFIG_MTK_EMI
 #include <mt_emi_api.h>
 #endif
 
@@ -1150,7 +1153,29 @@ static int scp_reserve_memory_ioremap(void)
 }
 #endif
 
-#if ENABLE_SCP_EMI_PROTECTION
+#ifdef CONFIG_MEDIATEK_EMI
+void set_scp_mpu(void)
+{
+	struct emimpu_region_t md_region;
+	int ret;
+
+	ret = mtk_emimpu_init_region(&md_region, MPU_REGION_ID_SCP_SMEM);
+	if (ret) {
+		pr_notice("[SCP]mtk_emimpu_init_region failed\n");
+		return;
+	}
+	mtk_emimpu_set_addr(&md_region, scp_mem_base_phys,
+		scp_mem_base_phys + scp_mem_size - 1);
+	mtk_emimpu_set_apc(&md_region, MPU_DOMAIN_D0,
+		MTK_EMIMPU_NO_PROTECTION);
+	mtk_emimpu_set_apc(&md_region, MPU_DOMAIN_D3,
+		MTK_EMIMPU_NO_PROTECTION);
+	if (mtk_emimpu_set_protection(&md_region))
+		pr_notice("[SCP]mtk_emimpu_set_protection fail\n");
+	mtk_emimpu_free_region(&md_region);
+}
+#endif
+#ifdef CONFIG_MTK_EMI
 void set_scp_mpu(void)
 {
 	struct emi_region_info_t region_info;
@@ -2004,7 +2029,7 @@ static int __init scp_init(void)
 	}
 #endif
 
-#if ENABLE_SCP_EMI_PROTECTION
+#ifdef CONFIG_MTK_EMI
 	set_scp_mpu();
 #endif
 
@@ -2075,5 +2100,15 @@ static void __exit scp_exit(void)
 #endif
 }
 
+static int __init scp_late_init(void)
+{
+	pr_notice("[SCP] %s\n", __func__);
+#ifdef CONFIG_MEDIATEK_EMI
+	set_scp_mpu();
+#endif
+	return 0;
+}
+
 module_init(scp_init);
 module_exit(scp_exit);
+late_initcall(scp_late_init);
