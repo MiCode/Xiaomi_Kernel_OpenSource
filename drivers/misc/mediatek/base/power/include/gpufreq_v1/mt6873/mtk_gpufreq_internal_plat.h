57b67af19a81ec04b75ef66fb09390b5ea8edcd7
jingxiang.wu <jingxiang.wu@mediatek.com>
2021-04-28 00:09:15 +0800

    [ALPS05694431] GPU: Adjust dynamic power setting
    [Detail]
     Adjust dynamic power setting from correlation result.
    
    MTK-Commit-Id: 2536a0c82a6510b63631e43662102d15297ff699
    
    Change-Id: I10ca028a9fad70bfd7b6d54af235b85ff0d79154
    CR-Id: ALPS05694431
    Feature: GPU Extension(gpu_extension)
    Signed-off-by: jingxiang.wu <jingxiang.wu@mediatek.com>

--- drivers/misc/mediatek/base/power/include/gpufreq_v1/mt6873/mtk_gpufreq_internal_plat.h
+++ drivers/misc/mediatek/base/power/include/gpufreq_v1/mt6873/mtk_gpufreq_internal_plat.h
@@ -87,9 +87,9 @@
 /**************************************************
  * Reference Power Setting
  **************************************************/
-#define GPU_ACT_REF_POWER               (1285)                /* mW  */
-#define GPU_ACT_REF_FREQ                (900000)              /* KHz */
-#define GPU_ACT_REF_VOLT                (90000)               /* mV x 100 */
+#define GPU_ACT_REF_POWER               (1513)                /* mW  */
+#define GPU_ACT_REF_FREQ                (902000)              /* KHz */
+#define GPU_ACT_REF_VOLT                (77500)               /* mV x 100 */
 #define PTPOD_DISABLE_VOLT              (75000)
 
 /**************************************************
