9a241d6159dec823ed3e1ff27ba111fa94198cc8
Qilin Tan <qilin.tan@mediatek.com>
2021-04-27 23:57:50 +0800

    [ALPS05681400] pidmap: dynamic switch for pidmap
    add a dynamic switch for pidmap.
    For ago project, the feature need to disable to save memory. when a IO
    issue is encountered, the feature will help us to check the issue.
    Hence we have to enable the feature and build it again while the
    dynamic switch is not supported. after supported, we just use the
    following command to enable the feature:
    echo 2 > /proc/pidmap
    
    enable command: echo 2 > /proc/pidmap
    disable command: echo 3 > /proc/pidmap
    
    MTK-Commit-Id: ebd2224a761ba4011ef85420355ab21db006b06a
    
    Change-Id: I088e4add8a91b5c536288e19dd61ea34cbe4f13b
    Signed-off-by: Qilin Tan <qilin.tan@mediatek.com>
    CR-Id: ALPS05681400
    Feature: Storage I/O Performance Profiling Tool(blocktag)

--- drivers/misc/mediatek/pidmap/pidmap.c
+++ drivers/misc/mediatek/pidmap/pidmap.c
@@ -17,7 +17,9 @@
 #include <linux/proc_fs.h>
 #include <linux/sched.h>
 #include <linux/seq_file.h>
+#include <linux/slab.h>
 #include <linux/uaccess.h>
+#include <linux/vmalloc.h>
 #include <mt-plat/mtk_pidmap.h>
 
 /*
@@ -33,11 +35,54 @@
  * handling flow, e.g., hwt or hw reboot.
  */
 
-static char mtk_pidmap[PIDMAP_AEE_BUF_SIZE];
+#define TAG "[PIDMAP]"
+
+static char __rcu *mtk_pidmap;
+static bool mtk_pidmap_enable;
 static int  mtk_pidmap_proc_dump_mode;
 static int  mtk_pidmap_max_pid;
-static char mtk_pidmap_proc_cmd_buf[PIDMAP_PROC_CMD_BUF_SIZE];
 static struct proc_dir_entry *mtk_pidmap_proc_entry;
+
+static bool mtk_pidmap_allocate(void)
+{
+	char *pidmap = NULL;
+	char *old_pidmap = NULL;
+
+	pidmap = vmalloc(PIDMAP_AEE_BUF_SIZE);
+	if (!pidmap)
+		return false;
+
+	memset(pidmap, 0, PIDMAP_AEE_BUF_SIZE);
+	old_pidmap = rcu_dereference(mtk_pidmap);
+
+	rcu_assign_pointer(mtk_pidmap, pidmap);
+	synchronize_rcu();
+	if (old_pidmap)
+		vfree(old_pidmap);
+	return true;
+}
+
+static void mtk_pidmap_reset(void)
+{
+	char *pidmap;
+
+	rcu_read_lock();
+	pidmap = rcu_dereference(mtk_pidmap);
+	if (pidmap)
+		memset(pidmap, 0, PIDMAP_AEE_BUF_SIZE);
+	rcu_read_unlock();
+}
+
+static void mtk_pidmap_free(void)
+{
+	char *old_pidmap = NULL;
+
+	old_pidmap = rcu_dereference(mtk_pidmap);
+	rcu_assign_pointer(mtk_pidmap, NULL);
+	synchronize_rcu();
+	if (old_pidmap)
+		vfree(old_pidmap);
+}
 
 static void mtk_pidmap_init_map(void)
 {
@@ -45,8 +90,23 @@ static void mtk_pidmap_init_map(void)
 	 * now pidmap is designed for keeping
 	 * maximum 32768 pids in 512 KB buffer.
 	 */
-	mtk_pidmap_max_pid =
-		PIDMAP_AEE_BUF_SIZE / PIDMAP_ENTRY_SIZE;
+	bool rs = mtk_pidmap_allocate();
+
+	if (rs) {
+		mtk_pidmap_proc_dump_mode = PIDMAP_PROC_DUMP_RAW;
+		mtk_pidmap_max_pid = PIDMAP_AEE_BUF_SIZE / PIDMAP_ENTRY_SIZE;
+		mtk_pidmap_enable = true;
+	} else {
+		pr_info(TAG " init: fail to init pidmap\n");
+		mtk_pidmap_enable = false;
+	}
+}
+
+static void mtk_pidmap_destroy_map(void)
+{
+	mtk_pidmap_free();
+	mtk_pidmap_enable = false;
+	mtk_pidmap_max_pid = 0;
 }
 
 /*
@@ -57,17 +117,22 @@ static void mtk_pidmap_init_map(void)
  */
 void mtk_pidmap_update(struct task_struct *task)
 {
+	char *pidmap = NULL;
 	char *name;
 	int len;
 	pid_t pid;
 
-	if (unlikely(!mtk_pidmap_max_pid))
-		mtk_pidmap_init_map();
+	rcu_read_lock();
+	pidmap = rcu_dereference(mtk_pidmap);
+	if (unlikely(!pidmap)) {
+		pr_debug(TAG " update: pid map is not ready\n");
+		goto out;
+	}
 
 	pid = task->pid;
 
 	if (unlikely((pid < 1) || (pid > mtk_pidmap_max_pid)))
-		return;
+		goto out;
 
 	/*
 	 * part 1, get current task's name.
@@ -75,7 +140,7 @@ void mtk_pidmap_update(struct task_struct *task)
 	 * this could be lockless because the specific offset
 	 * will be updated by its task only.
 	 */
-	name = mtk_pidmap + ((pid - 1) * PIDMAP_ENTRY_SIZE);
+	name = pidmap + ((pid - 1) * PIDMAP_ENTRY_SIZE);
 
 	/* copy task name */
 	memcpy(name, task->comm, PIDMAP_TASKNAME_SIZE);
@@ -93,11 +158,25 @@ void mtk_pidmap_update(struct task_struct *task)
 	name += PIDMAP_TASKNAME_SIZE;
 	*(name + 0) = (char)(task->tgid & 0xFF);
 	*(name + 1) = (char)(task->tgid >> 8);
+out:
+	rcu_read_unlock();
+}
+
+static void mtk_pidmap_seq_dump_disable(struct seq_file *seq)
+{
+	seq_puts(seq, "<PID Map>\n");
+	seq_puts(seq, "PIDMAP is disabled, please use \"echo 2 > pidmap\" to enable it\n");
+	seq_puts(seq, "\n<Configuration>\n");
+	seq_puts(seq, "echo 0 > pidmap: Dump raw pidmap (default, for AEE DB)\n");
+	seq_puts(seq, "echo 1 > pidmap: Dump readable pidmap\n");
+	seq_puts(seq, "echo 2 > pidmap: Reset or Enable pidmap\n");
+	seq_puts(seq, "echo 3 > pidmap: Disable pidmap\n");
 }
 
 static void mtk_pidmap_seq_dump_readable(char **buff, unsigned long *size,
 	struct seq_file *seq)
 {
+	char *pidmap;
 	int i, active_pid;
 	pid_t tgid;
 	char *name;
@@ -111,9 +190,14 @@ static void mtk_pidmap_seq_dump_readable(char **buff, unsigned long *size,
 	 * we ignore locking here to favor performance.
 	 */
 	active_pid = 0;
-
+	rcu_read_lock();
+	pidmap = rcu_dereference(mtk_pidmap);
+	if (unlikely(!pidmap)) {
+		pr_debug(TAG " dump readable: pid map is not ready\n");
+		goto out;
+	}
 	for (i = 0; i < PIDMAP_ENTRY_CNT; i++) {
-		name = &mtk_pidmap[i * PIDMAP_ENTRY_SIZE];
+		name = pidmap + (i * PIDMAP_ENTRY_SIZE);
 		if (name[0]) {
 			/* get task name */
 			memset(name_tmp, 0, PIDMAP_TASKNAME_SIZE);
@@ -131,6 +215,9 @@ static void mtk_pidmap_seq_dump_readable(char **buff, unsigned long *size,
 		}
 	}
 
+out:
+	rcu_read_unlock();
+
 	seq_puts(seq, "\n<Information>\n");
 	seq_printf(seq, "Total PIDs: %d\n", active_pid);
 	seq_printf(seq, "Entry size: %d bytes\n",
@@ -141,20 +228,19 @@ static void mtk_pidmap_seq_dump_readable(char **buff, unsigned long *size,
 			(int)PIDMAP_TGID_SIZE);
 	seq_printf(seq, "Total Buffer Size: %d bytes\n",
 			PIDMAP_AEE_BUF_SIZE + PIDMAP_PROC_CMD_BUF_SIZE);
-	seq_printf(seq, "mtk_pidmap address: 0x%p\n",
-			&mtk_pidmap[0]);
-
+	seq_printf(seq, "mtk_pidmap address: 0x%p\n", pidmap);
 	seq_puts(seq, "\n<Configuration>\n");
 	seq_puts(seq, "echo 0 > pidmap: Dump raw pidmap (default, for AEE DB)\n");
 	seq_puts(seq, "echo 1 > pidmap: Dump readable pidmap\n");
-	seq_puts(seq, "echo 2 > pidmap: Reset pidmap\n");
+	seq_puts(seq, "echo 2 > pidmap: Reset or Enable pidmap\n");
+	seq_puts(seq, "echo 3 > pidmap: Disable pidmap\n");
 
 }
 
 static void mtk_pidmap_seq_dump_raw(struct seq_file *seq)
 {
 	int i;
-
+	char *pidmap;
 	/*
 	 * pid map shall be protected for dump, however
 	 * we ignore locking here to favor performance.
@@ -167,12 +253,24 @@ static void mtk_pidmap_seq_dump_raw(struct seq_file *seq)
 	 * 0Ah may be existed for TGID, and you'll get an incorrect
 	 * PID map outputs.
 	 */
+	rcu_read_lock();
+	pidmap = rcu_dereference(mtk_pidmap);
+	if (unlikely(!pidmap)) {
+		pr_debug(TAG " dump raw: pid map is not ready\n");
+		rcu_read_unlock();
+		return;
+	}
 	for (i = 0; i < PIDMAP_AEE_BUF_SIZE; i++)
-		seq_putc(seq, mtk_pidmap[i]);
+		seq_putc(seq, pidmap[i]);
+	rcu_read_unlock();
 }
 
 static int mtk_pidmap_seq_show(struct seq_file *seq, void *v)
 {
+	if (!mtk_pidmap_enable) {
+		mtk_pidmap_seq_dump_disable(seq);
+		return 0;
+	}
 	if (mtk_pidmap_proc_dump_mode == PIDMAP_PROC_DUMP_READABLE)
 		mtk_pidmap_seq_dump_readable(NULL, NULL, seq);
 	else if (mtk_pidmap_proc_dump_mode == PIDMAP_PROC_DUMP_RAW)
@@ -183,13 +281,26 @@ static int mtk_pidmap_seq_show(struct seq_file *seq, void *v)
 
 void get_pidmap_aee_buffer(unsigned long *vaddr, unsigned long *size)
 {
-	/* retrun start location */
-	if (vaddr)
-		*vaddr = (unsigned long)mtk_pidmap;
+	char *pidmap;
 
-	/* return valid buffer size */
-	if (size)
-		*size = PIDMAP_AEE_BUF_SIZE;
+	rcu_read_lock();
+	pidmap = rcu_dereference(mtk_pidmap);
+	if (unlikely(pidmap == NULL)) {
+		pr_info(TAG " aee buffer: pid map is not ready\n");
+		if (vaddr)
+			*vaddr = 0;
+		if (size)
+			*size = 0;
+	} else {
+		/* retrun start location */
+		/* FIXME pidmap is exposed to external module without rcu read lock. */
+		if (vaddr)
+			*vaddr = (unsigned long) pidmap;
+		/* return valid buffer size */
+		if (size)
+			*size = PIDMAP_AEE_BUF_SIZE;
+	}
+	rcu_read_unlock();
 }
 EXPORT_SYMBOL(get_pidmap_aee_buffer);
 
@@ -197,33 +308,47 @@ static ssize_t mtk_pidmap_proc_write(struct file *file, const char *buf,
 	size_t count, loff_t *data)
 {
 	int ret;
+	char cmd[PIDMAP_PROC_CMD_BUF_SIZE];
 
 	if (count == 0)
 		goto err;
 	else if (count > PIDMAP_PROC_CMD_BUF_SIZE)
 		count = PIDMAP_PROC_CMD_BUF_SIZE;
 
-	ret = copy_from_user(mtk_pidmap_proc_cmd_buf, buf, count);
+	ret = copy_from_user(cmd, buf, count);
 
 	if (ret < 0)
 		goto err;
 
-	if (mtk_pidmap_proc_cmd_buf[0] == '0') {
+	if (cmd[0] == '0') {
 		mtk_pidmap_proc_dump_mode = PIDMAP_PROC_DUMP_RAW;
-		pr_info("[pidmap] dump mode: raw\n");
-	} else if (mtk_pidmap_proc_cmd_buf[0] == '1') {
+		pr_info(TAG " dump mode: raw\n");
+	} else if (cmd[0] == '1') {
 		mtk_pidmap_proc_dump_mode = PIDMAP_PROC_DUMP_READABLE;
-		pr_info("[pidmap] dump mode: readable\n");
-	} else if (mtk_pidmap_proc_cmd_buf[0] == '2') {
-		memset(mtk_pidmap, 0, sizeof(mtk_pidmap));
-		pr_info("[pidmap] reset pidmap\n");
+		pr_info(TAG " dump mode: readable\n");
+	} else if (cmd[0] == '2') {
+		if (mtk_pidmap_enable) {
+			mtk_pidmap_reset();
+			pr_info(TAG " reset pidmap\n");
+		} else {
+			mtk_pidmap_allocate();
+			mtk_pidmap_enable = true;
+			pr_info(TAG " enable pidmap\n");
+		}
+	} else if (cmd[0] == '3') {
+		if (mtk_pidmap_enable) {
+			mtk_pidmap_free();
+			mtk_pidmap_enable = false;
+			pr_info(TAG " disable pidmap\n");
+		} else
+			pr_info(TAG " pidmap has been disabled");
 	} else
 		goto err;
 
 	goto out;
 
 err:
-	pr_info("[pidmap] invalid arg: 0x%x\n", mtk_pidmap_proc_cmd_buf[0]);
+	pr_info(TAG " invalid arg: 0x%x\n", cmd[0]);
 	return -1;
 out:
 	return count;
@@ -257,21 +382,22 @@ static int mtk_pidmap_proc_init(void)
 	if (mtk_pidmap_proc_entry)
 		proc_set_user(mtk_pidmap_proc_entry, uid, gid);
 	else
-		pr_info("[pidmap] failed to create /proc/pidmap\n");
+		pr_info(TAG " failed to create /proc/pidmap\n");
 
 	return 0;
 }
 
 static int __init mtk_pidmap_init(void)
 {
-	mtk_pidmap_init_map();
 	mtk_pidmap_proc_init();
+	mtk_pidmap_init_map();
 
 	return 0;
 }
 
 static void __exit mtk_pidmap_exit(void)
 {
+	mtk_pidmap_destroy_map();
 	proc_remove(mtk_pidmap_proc_entry);
 }
 
