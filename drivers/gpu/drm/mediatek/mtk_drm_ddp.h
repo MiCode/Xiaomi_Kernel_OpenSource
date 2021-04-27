8a082fc2d392c058980db7b8e0fa0496a7dcf0c4
tai-hua.tseng <tai-hua.tseng@mediatek.com>
2021-04-27 23:57:02 +0800

    [ALPS05584479] DISP: report platform info through report CAPS ioctl
    [Detail]
    sync display driver headers modification for kernel 5.10
    
    MTK-Commit-Id: 52e5745ae95467bf6c9e62bfab1469efca9e96b1
    
    Change-Id: Idf94f8fb158f871a8f98ca2a502930fba49b1bb6
    CR-Id: ALPS05584479
    Feature: Display Driver (DSI Interface)
    Signed-off-by: tai-hua.tseng <tai-hua.tseng@mediatek.com>

--- drivers/gpu/drm/mediatek/mtk_drm_ddp.h
+++ drivers/gpu/drm/mediatek/mtk_drm_ddp.h
@@ -30,18 +30,6 @@ struct regmap;
 struct device;
 struct mtk_disp_mutex;
 struct mtk_mmsys_reg_data;
-enum mtk_mmsys_id {
-	MMSYS_MT2701,
-	MMSYS_MT2712,
-	MMSYS_MT8173,
-	MMSYS_MT6779,
-	MMSYS_MT6885,
-	MMSYS_MT6873,
-	MMSYS_MT6853,
-	MMSYS_MT6877,
-	MMSYS_MT6833,
-	MMSYS_MAX,
-};
 
 const struct mtk_mmsys_reg_data *
 mtk_ddp_get_mmsys_reg_data(enum mtk_mmsys_id mmsys_id);
