f06ac15740e6446b4240b561e259dbbfdbb62173
SW Integrator <swintegrator@mediatek.com>
2021-04-28 16:47:34 +0800

    mmc: Mediatek: fix file_operations build breaks
    Errors:
    drivers/mmc/host/mtk-dbg.c:990:10: error: incompatible pointer types passing 'const struct file_operations *' to parameter of type 'const struct proc_ops *' [-Werror,-Wincompatible-pointer-types]
    
    Change-Id: I4f7feb9ca60e91892adac4609affad2255155eb8
    CR-Id: ALPS05708600
    Feature: [Module]Kernel Maintenance
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>

--- drivers/mmc/host/mtk-dbg.c
+++ drivers/mmc/host/mtk-dbg.c
@@ -968,12 +968,12 @@ static int mmc_debug_proc_open(struct inode *inode, struct file *file)
 	return single_open(file, mmc_debug_proc_show, inode->i_private);
 }
 
-static const struct file_operations mmc_debug_proc_fops = {
-	.open = mmc_debug_proc_open,
-	.write = mmc_debug_proc_write,
-	.read = seq_read,
-	.llseek = seq_lseek,
-	.release = single_release,
+static const struct proc_ops mmc_debug_proc_fops = {
+	.proc_open = mmc_debug_proc_open,
+	.proc_write = mmc_debug_proc_write,
+	.proc_read = seq_read,
+	.proc_lseek = seq_lseek,
+	.proc_release = single_release,
 };
 
 int mmc_debug_init_procfs(void)
