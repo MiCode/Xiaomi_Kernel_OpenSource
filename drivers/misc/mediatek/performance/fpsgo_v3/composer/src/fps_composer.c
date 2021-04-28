5b394cc357e8aa763603852b74888ed48003aa9e
xueming.jiang <xueming.jiang@mediatek.com>
2021-04-29 00:23:39 +0800

    [ALPS05653409] FPSGO: fix use-after-free
    There exists race conditions inside mutex lock and unlock.
    It is not safe to use mutex to prevent use-after-free,
    since the lock will be freed.
    Use global lock instead.
    
    MTK-Commit-Id: 5e257ed2387d70b9232eed85b7e110beefa616fb
    
    Change-Id: Ib34804764572282a5ae93b89b9d70d60fed31bb9
    Signed-off-by: xueming.jiang <xueming.jiang@mediatek.com>
    CR-Id: ALPS05653409
    Feature: FPSGO
    (cherry picked from commit 0700d226ee1ede81a8ad29dd9ab406efb5bbd53b)
    (cherry picked from commit a829ddfd5052d186851a9cf02da61df8ded99aad)

--- drivers/misc/mediatek/performance/fpsgo_v3/composer/src/fps_composer.c
+++ drivers/misc/mediatek/performance/fpsgo_v3/composer/src/fps_composer.c
@@ -237,33 +237,28 @@ void fpsgo_ctrl2comp_enqueue_start(int pid,
 	if (!f_render->api && identifier) {
 		ret = fpsgo_com_refetch_buffer(f_render, pid, identifier, 1);
 		if (!ret) {
-			fpsgo_render_tree_unlock(__func__);
-			fpsgo_thread_unlock(&f_render->thr_mlock);
+			goto exit;
 			return;
 		}
 
 		ret = fpsgo_com_update_render_api_info(f_render);
 		if (!ret) {
-			fpsgo_render_tree_unlock(__func__);
-			fpsgo_thread_unlock(&f_render->thr_mlock);
+			goto exit;
 			return;
 		}
 	} else if (identifier) {
 		ret = fpsgo_com_refetch_buffer(f_render, pid, identifier, 1);
 		if (!ret) {
-			fpsgo_render_tree_unlock(__func__);
-			fpsgo_thread_unlock(&f_render->thr_mlock);
+			goto exit;
 			return;
 		}
 	}
-
-	fpsgo_render_tree_unlock(__func__);
 
 	if (f_render->api == NATIVE_WINDOW_API_CAMERA)
 		fpsgo_comp2fstb_camera_active(pid);
 
 	if (!f_render->queue_SF) {
-		fpsgo_thread_unlock(&f_render->thr_mlock);
+		goto exit;
 		return;
 	}
 
@@ -292,7 +287,9 @@ void fpsgo_ctrl2comp_enqueue_start(int pid,
 			pid, f_render->frame_type);
 		break;
 	}
+exit:
 	fpsgo_thread_unlock(&f_render->thr_mlock);
+	fpsgo_render_tree_unlock(__func__);
 }
 
 void fpsgo_ctrl2comp_enqueue_end(int pid,
@@ -330,8 +327,7 @@ void fpsgo_ctrl2comp_enqueue_end(int pid,
 
 	ret = fpsgo_com_refetch_buffer(f_render, pid, identifier, 0);
 	if (!ret) {
-		fpsgo_render_tree_unlock(__func__);
-		fpsgo_thread_unlock(&f_render->thr_mlock);
+		goto exit;
 		return;
 	}
 
@@ -344,10 +340,8 @@ void fpsgo_ctrl2comp_enqueue_end(int pid,
 			f_render->hwui = 2;
 	}
 
-	fpsgo_render_tree_unlock(__func__);
-
 	if (!f_render->queue_SF) {
-		fpsgo_thread_unlock(&f_render->thr_mlock);
+		goto exit;
 		return;
 	}
 
@@ -392,8 +386,9 @@ void fpsgo_ctrl2comp_enqueue_end(int pid,
 			pid, f_render->frame_type);
 		break;
 	}
+exit:
 	fpsgo_thread_unlock(&f_render->thr_mlock);
-
+	fpsgo_render_tree_unlock(__func__);
 }
 
 void fpsgo_ctrl2comp_dequeue_start(int pid,
@@ -444,15 +439,12 @@ void fpsgo_ctrl2comp_dequeue_start(int pid,
 
 	ret = fpsgo_com_refetch_buffer(f_render, pid, identifier, 0);
 	if (!ret) {
-		fpsgo_render_tree_unlock(__func__);
-		fpsgo_thread_unlock(&f_render->thr_mlock);
+		goto exit;
 		return;
 	}
 
-	fpsgo_render_tree_unlock(__func__);
-
 	if (!f_render->queue_SF) {
-		fpsgo_thread_unlock(&f_render->thr_mlock);
+		goto exit;
 		return;
 	}
 
@@ -473,7 +465,9 @@ void fpsgo_ctrl2comp_dequeue_start(int pid,
 			pid, f_render->frame_type);
 		break;
 	}
+exit:
 	fpsgo_thread_unlock(&f_render->thr_mlock);
+	fpsgo_render_tree_unlock(__func__);
 
 }
 
@@ -519,15 +513,12 @@ void fpsgo_ctrl2comp_dequeue_end(int pid,
 
 	ret = fpsgo_com_refetch_buffer(f_render, pid, identifier, 0);
 	if (!ret) {
-		fpsgo_render_tree_unlock(__func__);
-		fpsgo_thread_unlock(&f_render->thr_mlock);
+		goto exit;
 		return;
 	}
 
-	fpsgo_render_tree_unlock(__func__);
-
 	if (!f_render->queue_SF) {
-		fpsgo_thread_unlock(&f_render->thr_mlock);
+		goto exit;
 		return;
 	}
 
@@ -554,8 +545,9 @@ void fpsgo_ctrl2comp_dequeue_end(int pid,
 			pid, f_render->frame_type);
 		break;
 	}
+exit:
 	fpsgo_thread_unlock(&f_render->thr_mlock);
-
+	fpsgo_render_tree_unlock(__func__);
 }
 
 void fpsgo_ctrl2comp_connect_api(int pid, int api,
