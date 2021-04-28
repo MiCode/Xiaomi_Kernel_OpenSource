e155e2eab4f8dcb6820de594aaed7f4cf83ace1c
Eddie Hung <eddie.hung@mediatek.com>
2021-04-29 00:28:53 +0800

    [ALPS05612903] usb20: enable host mode suspend for mt6781
    1. Add host mode wakeup function
    2. Add idle function to request infra power when suspend
    3. Set host_suspend true to enable it
    
    MTK-Commit-Id: a0399947fbbf5580af14b6d0f6c9b990706cf354
    
    Change-Id: I4e0737dc4ded87e88532a0cea775ae40c9071adc
    Signed-off-by: Eddie Hung <eddie.hung@mediatek.com>
    CR-Id: ALPS05612903
    Feature: [Android Default] USB OTG

--- drivers/misc/mediatek/usb20/mt6781/usb20.h
+++ drivers/misc/mediatek/usb20/mt6781/usb20.h
@@ -63,6 +63,12 @@ enum VBUS_OPS {
 	VBUS_OPS_ON
 };
 
+enum MTK_USB_SMC_CALL {
+	MTK_USB_SMC_INFRA_REQUEST = 0,
+	MTK_USB_SMC_INFRA_RELEASE,
+	MTK_USB_SMC_NUM
+};
+
 #ifdef CONFIG_MTK_UART_USB_SWITCH
 enum PORT_MODE {
 	PORT_MODE_USB = 0,
@@ -110,4 +116,9 @@ extern bool usb20_check_vbus_on(void);
 #endif
 extern bool usb_prepare_clock(bool enable);
 extern void usb_prepare_enable_clock(bool enable);
+
+/* usb host mode wakeup */
+#define USB_WAKEUP_DEC_CON1	0x404
+#define USB1_CDEN		BIT(0)
+#define USB1_CDDEBOUNCE(x)	(((x) & 0xf) << 1)
 #endif
