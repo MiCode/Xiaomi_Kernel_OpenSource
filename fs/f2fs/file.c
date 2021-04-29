6febc3942c2928d4cabccd34010622109d5b0b24
Michel Lespinasse <michel@lespinasse.org>
2021-04-29 10:28:25 -0700

    BACKPORT: FROMLIST: f2fs: implement speculative fault handling
    We just need to make sure f2fs_filemap_fault() doesn't block in the
    speculative case as it is called with an rcu read lock held.
    
    Signed-off-by: Michel Lespinasse <michel@lespinasse.org>
    Link: https://lore.kernel.org/all/20210407014502.24091-33-michel@lespinasse.org/
    
    Conflicts:
        fs/f2fs/file.c
    
    1. The change in f2fs_filemap_fault is not needed since i_mmap_sem is not
    used anymore.
    
    Bug: 161210518
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: If7a46e131ee38ca02a4c5b8a76ab4eb742acbe95

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -171,6 +171,7 @@ static const struct vm_operations_struct f2fs_file_vm_ops = {
 	.fault		= f2fs_filemap_fault,
 	.map_pages	= filemap_map_pages,
 	.page_mkwrite	= f2fs_vm_page_mkwrite,
+	.speculative	= true,
 };
 
 static int get_parent_ino(struct inode *inode, nid_t *pino)
