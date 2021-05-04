c7301b549403fb5fb86a8d2a939b151400060b9a
Nitheesh Muthuraj <nmuthuraj@codeaurora.org>
2021-05-04 05:47:02 -0700

    msm: kgsl: Deregister gpu address on memdesc_sg_virt failure
    When memdesc_sg_virt returns error in kgsl_setup_anon_useraddr function,
    the gpu address registered in SVM region will not be deregistered.
    This change deregisters the gpu address on memdesc_sg_virt failure.
    
    Change-Id: Ic99167e283a0c6331bb9f5f0b608b6cdb3c918e4
    Signed-off-by: Nitheesh Muthuraj <nmuthuraj@codeaurora.org>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -2457,6 +2457,8 @@ static int kgsl_setup_anon_useraddr(struct kgsl_pagetable *pagetable,
 {
 	/* Map an anonymous memory chunk */
 
+	int ret;
+
 	if (size == 0 || offset != 0 ||
 		!IS_ALIGNED(size, PAGE_SIZE))
 		return -EINVAL;
@@ -2466,7 +2468,6 @@ static int kgsl_setup_anon_useraddr(struct kgsl_pagetable *pagetable,
 	entry->memdesc.flags |= (uint64_t)KGSL_MEMFLAGS_USERMEM_ADDR;
 
 	if (kgsl_memdesc_use_cpu_map(&entry->memdesc)) {
-		int ret;
 
 		/* Register the address in the database */
 		ret = kgsl_mmu_set_svm_region(pagetable,
@@ -2478,7 +2479,12 @@ static int kgsl_setup_anon_useraddr(struct kgsl_pagetable *pagetable,
 		entry->memdesc.gpuaddr = (uint64_t) hostptr;
 	}
 
-	return memdesc_sg_virt(&entry->memdesc, hostptr);
+	ret =  memdesc_sg_virt(&entry->memdesc, hostptr);
+
+	if (ret && kgsl_memdesc_use_cpu_map(&entry->memdesc))
+		kgsl_mmu_put_gpuaddr(pagetable, &entry->memdesc);
+
+	return ret;
 }
 
 #ifdef CONFIG_DMA_SHARED_BUFFER
