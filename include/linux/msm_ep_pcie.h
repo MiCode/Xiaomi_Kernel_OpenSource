ccdb26e3133678058af775ecb3dbf5dccace7e2d
Prasad Malisetty <pmaliset@codeaurora.org>
2021-05-06 11:33:54 +0530

    msm: edma: Add PCIe EDMA changes snapshot for 5.10
    PCIe EDMA driver snaphot as of msm-5.4
    'Commit <b44b2b803a8f> (dmaengine: edma: Porting
    of EDMA driver from 4.14 to 5.4)'.
    
    Changes added:
      - Replaced (devm_)ioremap_nocache with(devm_)ioremap inline
        with latest implementations.
    
    Change-Id: If34162e007a1e2d1ec6fedd3edcb288b70e12305
    Signed-off-by: Prasad Malisetty <pmaliset@codeaurora.org>

--- include/linux/msm_ep_pcie.h
+++ include/linux/msm_ep_pcie.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
-/* Copyright (c) 2015, 2017, 2019-2020, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2015, 2017, 2019-2021, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
