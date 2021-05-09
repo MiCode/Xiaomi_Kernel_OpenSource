b51c567ce3e2d3fb51eccc989f5f39b069060345
Hongwei Ren <hongren@codeaurora.org>
2021-05-09 11:21:28 +0530

    dt-bindings: clock: Add support for clock ids for SCUBA
    Add clock IDs for GCC/GPUCC/DISPCC for clients to be able
    to request for clocks from these clock controllers.
    
    Change-Id: Ibd69e800ff6ebd0d59d34605deb8dc092c25b907
    Signed-off-by: Hongwei Ren <hongren@codeaurora.org>

---
+++ include/dt-bindings/clock/qcom,gpucc-scuba.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef _DT_BINDINGS_CLK_QCOM_GPU_CC_SCUBA_H
+#define _DT_BINDINGS_CLK_QCOM_GPU_CC_SCUBA_H
+
+/* GPU_CC clocks */
+#define GPU_CC_PLL0				0
+#define GPU_CC_AHB_CLK				1
+#define GPU_CC_CRC_AHB_CLK			2
+#define GPU_CC_CX_GFX3D_CLK			3
+#define GPU_CC_CX_GMU_CLK			4
+#define GPU_CC_CX_SNOC_DVM_CLK			5
+#define GPU_CC_CXO_AON_CLK			6
+#define GPU_CC_CXO_CLK				7
+#define GPU_CC_GMU_CLK_SRC			8
+#define GPU_CC_GX_CXO_CLK			9
+#define GPU_CC_GX_GFX3D_CLK			10
+#define GPU_CC_GX_GFX3D_CLK_SRC			11
+#define GPU_CC_SLEEP_CLK			12
+#define GPU_CC_HLOS1_VOTE_GPU_SMMU_CLK		13
+
+#endif
