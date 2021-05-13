fdb3723b4174d5d909bdb84654af333659b71a7c
Rocco Yue <rocco.yue@mediatek.com>
2021-05-13 11:53:19 +0800

    [ALPS05677588] ccci_util: support write proc/ccci_dump
    add write callback function for ccci_dump proc file
    
    MTK-Commit-Id: d30bd7eeea338db714e33cd5df888e4c2ba5e917
    
    Change-Id: I88279a319ef2885c6611991bd33d92bfbc075e5e
    Signed-off-by: Rocco Yue <rocco.yue@mediatek.com>
    CR-Id: ALPS05677588
    Feature: Modem Interface Driver

--- drivers/misc/mediatek/include/mt-plat/mtk_ccci_common.h
+++ drivers/misc/mediatek/include/mt-plat/mtk_ccci_common.h
@@ -664,8 +664,10 @@ void clear_meta_1st_boot_arg(int md_id);
 
 /* CCCI dump */
 #define CCCI_DUMP_TIME_FLAG		(1<<0)
-#define CCCI_DUMP_CLR_BUF_FLAG	(1<<1)
+#define CCCI_DUMP_CLR_BUF_FLAG		(1<<1)
 #define CCCI_DUMP_CURR_FLAG		(1<<2)
+#define CCCI_DUMP_ANDROID_TIME_FLAG	(1<<3)
+
 enum {
 	CCCI_DUMP_INIT = 0,
 	CCCI_DUMP_BOOTUP,
@@ -675,6 +677,7 @@ enum {
 	CCCI_DUMP_HISTORY,
 	CCCI_DUMP_REGISTER,
 	CCCI_DUMP_DPMA_DRB,
+	CCCI_DUMP_MD_INIT,
 	CCCI_DUMP_MAX,
 };
 void ccci_util_mem_dump(int md_id, int buf_type, void *start_addr, int len);
