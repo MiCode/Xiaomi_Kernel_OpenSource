3a56ecddbd60ccf40fbe6f4a5e5ddc4af6112efa
mtk13858 <derong.liu@mediatek.com>
2021-05-13 12:33:58 +0800

    [ALPS05712681] msdc:fix uninitialized scalar variable issue
    Avoid to use uninitialized value,
    wed better declare and initialize the variable.
    
    MTK-Commit-Id: 87354951561c7d8a2ba92115fb4cccd91bbaeb3f
    
    Change-Id: I8ef3af31e92728853ef3f68f128340c4c234da15
    Signed-off-by: mtk13858 <derong.liu@mediatek.com>
    CR-Id: ALPS05712681
    Feature: eMMC

--- drivers/mmc/host/mediatek/ComboA/mt6765/msdc_cust.c
+++ drivers/mmc/host/mediatek/ComboA/mt6765/msdc_cust.c
@@ -1188,7 +1188,7 @@ int msdc_of_parse(struct platform_device *pdev, struct mmc_host *mmc)
 	struct msdc_host *host = mmc_priv(mmc);
 	int ret = 0;
 	int len = 0;
-	u8 id;
+	u8 id = 0;
 	const char *dup_name;
 #if defined(CONFIG_MTK_PMIC_WRAP)
 	struct device_node *pwrap_node;
