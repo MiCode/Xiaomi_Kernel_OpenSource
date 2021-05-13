61137422c431f18435f63a8a56d44543b3fceecd
Sneh Shah <snehshah@codeaurora.org>
2021-05-13 12:54:06 +0530

    net: stmmac: fix compilation warnings for module
    Fix compilaiton warnings when stmmac is compiled as module.
    
    Change-Id: I1e87584e8a2a7d4384acb845f42088b681056f79
    Signed-off-by: Sneh Shah <snehshah@codeaurora.org>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.c
@@ -289,6 +289,7 @@ int ethqos_handle_prv_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
 	return ret;
 }
 
+#ifndef MODULE
 static int __init set_early_ethernet_ipv4(char *ipv4_addr_in)
 {
 	int ret = 1;
@@ -368,6 +369,7 @@ fail:
 }
 
 __setup("ermac=", set_early_ethernet_mac);
+#endif
 
 static int qcom_ethqos_add_ipaddr(struct ip_params *ip_info,
 				  struct net_device *dev)
