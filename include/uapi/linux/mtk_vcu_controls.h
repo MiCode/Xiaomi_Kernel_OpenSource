0aa353874efc0bace5f7cea93e4c2664998abb35
zhihao <zhihao.cheng@mediatek.com>
2021-05-13 12:33:19 +0800

    [ALPS05699831] v4l2:modify SHARE_BUF_SIZE
    The kernel SHARD_BUF_SIZE has to be 80, because the vpud SHARD_BUF_SIZE
    is modified to 80 in "[ALPS05692331] WN feature porting".
    If not, the vpud ipi messages and kernel ipi messages do not match.
    
    MTK-Commit-Id: 7fa772840e8c9637db4a43426245a5dcc9266934
    
    Change-Id: I02a2dc46127a4855a924215e4e46d5aaf7652098
    Signed-off-by: zhihao <zhihao.cheng@mediatek.com>
    CR-Id: ALPS05699831
    Feature: H.264 Encoder

--- include/uapi/linux/mtk_vcu_controls.h
+++ include/uapi/linux/mtk_vcu_controls.h
@@ -7,7 +7,7 @@
 #ifndef __UAPI_MTK_VCU_CONTROLS_H__
 #define __UAPI_MTK_VCU_CONTROLS_H__
 
-#define SHARE_BUF_SIZE 72
+#define SHARE_BUF_SIZE 80
 #define LOG_INFO_SIZE 1024
 #define VCODEC_CMDQ_CMD_MAX           (2048)
 
