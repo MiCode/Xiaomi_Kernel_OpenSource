0fe180c641412b3690d5e8e993eb64c99cdc510b
Dennis YC Hsieh <dennis-yc.hsieh@mediatek.com>
2021-05-13 11:58:45 +0800

    [ALPS05724956] dt-bindings: mml: add mml header
    Add engine id definition header to dt-binding,
    so that dts and drivers can identify same hardware.
    
    MTK-Commit-Id: ec71b77f94848cf85aee892c1c408d8ea93cfdce
    
    Change-Id: I3ce590cf6440d07267edad64155ac01060c67611
    Signed-off-by: Dennis YC Hsieh <dennis-yc.hsieh@mediatek.com>
    CR-Id: ALPS05724956
    Feature: [Module]MML driver

---
+++ include/dt-bindings/mml/mml-mt6893.h
@@ -0,0 +1,47 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2021 MediaTek Inc.
+ */
+
+#ifndef _DT_BINDINGS_MML_MT6893_H
+#define _DT_BINDINGS_MML_MT6893_H
+
+/* MML engines in mt6893 */
+#define MML_MMLSYS		0
+#define MML_MUTEX		1
+#define MML_RDMA0		2
+#define MML_RDMA1		3
+#define MML_RDMA2		4
+#define MML_RDMA3		5
+#define MML_FG0			6
+#define MML_FG1			7
+#define MML_PQ0_SOUT		8
+#define MML_PQ1_SOUT		9
+#define MML_HDR0		10
+#define MML_HDR1		11
+#define MML_COLOR0		12
+#define MML_COLOR1		13
+#define MML_AAL0		14
+#define MML_AAL1		15
+#define MML_AAL2		16
+#define MML_AAL3		17
+#define MML_RSZ0		18
+#define MML_RSZ1		19
+#define MML_RSZ2		20
+#define MML_RSZ3		21
+#define MML_TDSHP0		22
+#define MML_TDSHP1		23
+#define MML_TDSHP2		24
+#define MML_TDSHP3		25
+#define MML_TCC0		26
+#define MML_TCC1		27
+#define MML_TCC2		28
+#define MML_TCC3		29
+#define MML_WROT0		30
+#define MML_WROT1		31
+#define MML_WROT2		32
+#define MML_WROT3		33
+#define MML_ENGINE_TOTAL	34
+
+
+#endif	/* _DT_BINDINGS_MML_MT6893_H */
