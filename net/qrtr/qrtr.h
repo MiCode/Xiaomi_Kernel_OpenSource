9e2b485c1dac18329d895b6e150f7076c16e2fcb
Deepak Kumar Singh <deesin@codeaurora.org>
2021-05-18 14:24:54 +0530

    net: qrtr: log rx packet causing system wakeup
    Log qrtr rx packet in kernel logs which caused system wakeup.
    This will help to easily identify wakeup packet.
    
    Change-Id: I4a6a35dd4d5449a671464c00f90712a79d6044fe
    Signed-off-by: Deepak Kumar Singh <deesin@codeaurora.org>

--- net/qrtr/qrtr.h
+++ net/qrtr/qrtr.h
@@ -38,4 +38,6 @@ void qrtr_ns_init(void);
 void qrtr_ns_remove(void);
 
 int qrtr_peek_pkt_size(const void *data);
+
+unsigned int qrtr_get_service_id(unsigned int node_id, unsigned int port_id);
 #endif
