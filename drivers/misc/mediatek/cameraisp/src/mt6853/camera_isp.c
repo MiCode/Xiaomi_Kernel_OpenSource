4bb24650f56d50f54d4c075d74ebb96d1d84453c
Adogu Huang <adogu.huang@mediatek.com>
2021-05-18 17:14:44 +0800

    [ALPS05705657] p1: reduce debug log
    ISR log buffer size is not enough in some cases. Reduce some dbg
    msg to prevent from printing log via pr_info directly in ISR.
    
    MTK-Commit-Id: e56891b6e8c59c2a1f5432ad50c0b7283905a177
    
    Change-Id: I72e95ae0ca58ff38c2041a7aedb9a1d894259abe
    CR-Id: ALPS05705657
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: Adogu Huang <adogu.huang@mediatek.com>

--- drivers/misc/mediatek/cameraisp/src/mt6853/camera_isp.c
+++ drivers/misc/mediatek/cameraisp/src/mt6853/camera_isp.c
@@ -1718,40 +1718,49 @@ static void ISP_DumpDmaDeepDbg(enum ISP_IRQ_TYPE_ENUM module)
 	IRQ_LOG_KEEPER(module, m_CurrentPPB, _LOG_ERR, "camsys:0x%x",
 		       ISP_RD32(ISP_CAMSYS_CONFIG_BASE));
 
-	IRQ_LOG_KEEPER(
-		module, m_CurrentPPB, _LOG_ERR,
-		"%s:IMGO:0x%x,LTMSO:0x%x,RRZO:0x%x,LCSO=0x%x,LCESHO=0x%x,AAO=0x%x,",
-		cam, dmaerr[_imgo_], dmaerr[_ltmso_], dmaerr[_rrzo_],
-		dmaerr[_lcso_], dmaerr[_lcesho_], dmaerr[_aao_]);
+	if ((dmaerr[_imgo_] | dmaerr[_ltmso_] | dmaerr[_rrzo_] |
+		dmaerr[_lcso_] | dmaerr[_lcesho_] | dmaerr[_aao_]) != 0xffff0000)
+		IRQ_LOG_KEEPER(module, m_CurrentPPB, _LOG_ERR,
+			"%s:IMGO:0x%x,LTMSO:0x%x,RRZO:0x%x,LCSO=0x%x,LCESHO=0x%x,AAO=0x%x,",
+			cam, dmaerr[_imgo_], dmaerr[_ltmso_], dmaerr[_rrzo_],
+			dmaerr[_lcso_], dmaerr[_lcesho_], dmaerr[_aao_]);
 
-	IRQ_LOG_KEEPER(
-		module, m_CurrentPPB, _LOG_ERR,
-		"AAHO=0x%x,FLKO=0x%x,UFEO=0x%x,AFO=0x%x,UFGO=0x%x,RSSO=0x%x\n",
-		dmaerr[_aaho_], dmaerr[_flko_], dmaerr[_ufeo_], dmaerr[_afo_],
-		dmaerr[_ufgo_], dmaerr[_rsso_]);
+	if ((dmaerr[_aaho_] | dmaerr[_flko_] | dmaerr[_ufeo_] |
+		dmaerr[_afo_] | dmaerr[_ufgo_] | dmaerr[_rsso_]) != 0xffff0000)
+		IRQ_LOG_KEEPER(
+			module, m_CurrentPPB, _LOG_ERR,
+			"AAHO=0x%x,FLKO=0x%x,UFEO=0x%x,AFO=0x%x,UFGO=0x%x,RSSO=0x%x\n",
+			dmaerr[_aaho_], dmaerr[_flko_], dmaerr[_ufeo_], dmaerr[_afo_],
+			dmaerr[_ufgo_], dmaerr[_rsso_]);
 
-	IRQ_LOG_KEEPER(
-		module, m_CurrentPPB, _LOG_ERR,
-		"EISO=0x%x,YUVBO=0x%x,TSFSO=0x%x,PDO=0x%x,CRZO=0x%x,CRZBO=0x%x\n",
-		dmaerr[_lmvo_], dmaerr[_yuvbo_], dmaerr[_tsfso_], dmaerr[_pdo_],
-		dmaerr[_crzo_], dmaerr[_crzbo_]);
+	if ((dmaerr[_lmvo_] | dmaerr[_yuvbo_] | dmaerr[_tsfso_] |
+		dmaerr[_pdo_] | dmaerr[_crzo_] | dmaerr[_crzbo_]) != 0xffff0000)
+		IRQ_LOG_KEEPER(
+			module, m_CurrentPPB, _LOG_ERR,
+			"EISO=0x%x,YUVBO=0x%x,TSFSO=0x%x,PDO=0x%x,CRZO=0x%x,CRZBO=0x%x\n",
+			dmaerr[_lmvo_], dmaerr[_yuvbo_], dmaerr[_tsfso_], dmaerr[_pdo_],
+			dmaerr[_crzo_], dmaerr[_crzbo_]);
 
-	IRQ_LOG_KEEPER(
-		module, m_CurrentPPB, _LOG_ERR,
-		"YUVCO=0x%x,CRZO_R2=0x%x,RSSO_R2=0x%x,YUVO=0x%x\n",
-		dmaerr[_yuvco_], dmaerr[_crzo_r2_],	dmaerr[_rsso_r2_],
-		dmaerr[_yuvo_]);
+	if ((dmaerr[_yuvco_] | dmaerr[_crzo_r2_] |
+		dmaerr[_rsso_r2_] | dmaerr[_yuvo_]) != 0xffff0000)
+		IRQ_LOG_KEEPER(
+			module, m_CurrentPPB, _LOG_ERR,
+			"YUVCO=0x%x,CRZO_R2=0x%x,RSSO_R2=0x%x,YUVO=0x%x\n",
+			dmaerr[_yuvco_], dmaerr[_crzo_r2_],	dmaerr[_rsso_r2_],
+			dmaerr[_yuvo_]);
 
 	IRQ_LOG_KEEPER(
 		module, m_CurrentPPB, _LOG_ERR,
 		"DMA_DBG_SEL=0x%x,TOP_DBG_PORT=0x%x\n",
 		(unsigned int)ISP_RD32(CAM_REG_DMA_DEBUG_SEL(regModule)), 0);
 
-	IRQ_LOG_KEEPER(
-		module, m_CurrentPPB, _LOG_ERR,
-		"%s:RAWI=0x%x,BPCI:0x%x,LSCI=0x%x,BPCI_R2=0x%x,PDI=0x%x,UFDI_R2=0x%x,\n",
-		cam, dmaerr[_rawi_], dmaerr[_bpci_], dmaerr[_lsci_],
-		dmaerr[_bpci_r2_], dmaerr[_pdi_], dmaerr[_ufdi_r2_]);
+	if ((dmaerr[_rawi_] | dmaerr[_bpci_] | dmaerr[_lsci_] |
+		dmaerr[_bpci_r2_] | dmaerr[_pdi_] | dmaerr[_ufdi_r2_]) != 0xffff0000)
+		IRQ_LOG_KEEPER(
+			module, m_CurrentPPB, _LOG_ERR,
+			"%s:RAWI=0x%x,BPCI:0x%x,LSCI=0x%x,BPCI_R2=0x%x,PDI=0x%x,UFDI_R2=0x%x,\n",
+			cam, dmaerr[_rawi_], dmaerr[_bpci_], dmaerr[_lsci_],
+			dmaerr[_bpci_r2_], dmaerr[_pdi_], dmaerr[_ufdi_r2_]);
 	/* DMAO */
 	g_DmaErr_CAM[module][_imgo_] |= dmaerr[_imgo_];
 	g_DmaErr_CAM[module][_ltmso_] |= dmaerr[_ltmso_];
