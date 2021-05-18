7e3fbd1e46dd2c3e555c4d648fa294c0d7dc4828
yonggang.yu <yonggang.yu@mediatek.com>
2021-05-18 17:14:56 +0800

    [ALPS05699256] DISP: NT36672E HFR turning
    [Detail]
    NT36672E HFR turning, In order to ensure
    accurate frame rate.
    
    MTK-Commit-Id: eb9ec03e2486ea8e9dccde535cf844b516fd82a4
    
    Change-Id: I7080fdd8b561d7e4da84417df1e14ae42be49c24
    Signed-off-by: yonggang.yu <yonggang.yu@mediatek.com>
    CR-Id: ALPS05699256
    Feature: Display Driver (DSI Interface)

--- drivers/gpu/drm/panel/include/panel-tianma-nt36672e-vdo-120hz-vfp.h
+++ drivers/gpu/drm/panel/include/panel-tianma-nt36672e-vdo-120hz-vfp.h
@@ -22,21 +22,21 @@
 
 /*Parameter setting for mode 0 Start*/
 #define MODE_0_FPS                  60
-#define MODE_0_VFP                  2670
+#define MODE_0_VFP                  2630
 #define MODE_0_HFP                  161
 #define MODE_0_DATA_RATE            1200
 /*Parameter setting for mode 0 End*/
 
 /*Parameter setting for mode 1 Start*/
 #define MODE_1_FPS                  90
-#define MODE_1_VFP                  972
+#define MODE_1_VFP                  940
 #define MODE_1_HFP                  161
 #define MODE_1_DATA_RATE            1200
 /*Parameter setting for mode 1 End*/
 
 /*Parameter setting for mode 2 Start*/
 #define MODE_2_FPS                  120
-#define MODE_2_VFP                  120
+#define MODE_2_VFP                  110
 #define MODE_2_HFP                  161
 #define MODE_2_DATA_RATE            1200
 /*Parameter setting for mode 2 End*/
