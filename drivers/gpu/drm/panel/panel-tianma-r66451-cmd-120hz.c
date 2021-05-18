95bccd0dd3f7407b3d988a7d1185576929a19aad
Cui Zhang <cui.zhang@mediatek.com>
2021-05-18 17:14:57 +0800

    [ALPS05709786] display: add null pointer check
    [Detail]
    invalid mode index will get a null pointer,
    do check null pointer before use it
    
    MTK-Commit-Id: 4b22be989381b35def05d932823f46b51b0561e9
    
    Change-Id: I9f5cda777513e04f172b2de268bad6c16fce989e
    Signed-off-by: Cui Zhang <cui.zhang@mediatek.com>
    CR-Id: ALPS05709786
    Feature: Display Driver (DSI Interface)

--- drivers/gpu/drm/panel/panel-tianma-r66451-cmd-120hz.c
+++ drivers/gpu/drm/panel/panel-tianma-r66451-cmd-120hz.c
@@ -520,6 +520,7 @@ struct drm_display_mode *get_mode_by_id(struct drm_panel *panel,
 			return m;
 		i++;
 	}
+	pr_info("%s, %d, failed to get mode:%d, total:%u\n", __func__, __LINE__, mode, i);
 	return NULL;
 }
 
@@ -763,6 +764,9 @@ static int mode_switch(struct drm_panel *panel, unsigned int cur_mode,
 
 	if (cur_mode == dst_mode)
 		return ret;
+
+	if (m == NULL)
+		return -EINVAL;
 
 	if (m->vrefresh == MODE_0_FPS) { /*switch to 60 */
 		mode_switch_to_60(panel, stage);
