b89cede17ec7e73af79235a89e598a0cf9527cc9
Gaia Chang <gaia.chang@mediatek.com>
2021-05-27 13:05:24 +0800

    [ALPS05760774] scmi: enlarge mailbox respone timeout
    set .max_rx_timeout_ms from 30 to 250ms
    
    MTK-Commit-Id: e3a5ed372817f4077b0e6af297fa330eccb0af51
    
    Change-Id: I55c2373a59fe75535b6c37472923d016ba6ddc91
    Signed-off-by: Gaia Chang <gaia.chang@mediatek.com>
    CR-Id: ALPS05760774
    Feature: ARM System Control and Management Interface(scmi)

--- drivers/firmware/arm_scmi/mailbox.c
+++ drivers/firmware/arm_scmi/mailbox.c
@@ -195,7 +195,7 @@ static const struct scmi_transport_ops scmi_mailbox_ops = {
 
 const struct scmi_desc scmi_mailbox_desc = {
 	.ops = &scmi_mailbox_ops,
-	.max_rx_timeout_ms = 30, /* We may increase this if required */
+	.max_rx_timeout_ms = 250, /* We may increase this if required */
 	.max_msg = 20, /* Limited by MBOX_TX_QUEUE_LEN */
 	.max_msg_size = 128,
 };
