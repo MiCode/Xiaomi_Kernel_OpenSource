7b5b9969404dea744f2f040ce6dc7b01d94d260c
Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
2021-06-01 18:43:15 +0800

    [ALPS05750023] amms: fix coverity
    check if amms_task is null before use it.
    
    MTK-Commit-Id: 2190f3fd38d559c265f12c39dcb9d834c5b5e7b7
    
    Change-Id: Ia76d1067d967677b7fb482a072c2fd42c6b8e594
    Signed-off-by: Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
    CR-Id: ALPS05750023
    Feature: AMMS (AP and Modem Memory Sharing)
    (cherry picked from commit 1ca5559e9dc241dd27a6bd22aeb57981a5b6890c)

--- drivers/misc/mediatek/memory-amms/memory-amms.c
+++ drivers/misc/mediatek/memory-amms/memory-amms.c
@@ -832,8 +832,10 @@ static int __init amms_init(void)
 	if (ret)
 		pr_info("amms init FAIL, ret 0x%x!!!\n", ret);
 
-	/* set amms_task interruptible */
-	smp_store_mb(amms_task->state, (TASK_INTERRUPTIBLE));
+	if (amms_task) {
+		/* set amms_task interruptible */
+		smp_store_mb(amms_task->state, (TASK_INTERRUPTIBLE));
+	}
 	return ret;
 }
 
