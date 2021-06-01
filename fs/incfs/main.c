07dd6ef2db0b84d045dd4ddf7eb967de17208b5a
Light Hsieh <light.hsieh@mediatek.com>
2021-06-01 18:40:05 +0800

    [ALPS05755342] Incremental fs: Set credentials before reading/writing
    Use same selinux scheme as incfs v2
    Fix memory leak
    
    MTK-Commit-Id: b9eb9b63fe2a21d156a4176706456aba4e98cc90
    
    Change-Id: I2d7a3562ebfa0075445c2a605b6675cc307ac7bd
    Signed-off-by: Light Hsieh <light.hsieh@mediatek.com>
    CR-Id: ALPS05755342
    Feature: [Android Default] F2FS File System
    (cherry-pick from 3d98b570cc1092fcc7aefe4ba77bfeedebefe20a)

--- fs/incfs/main.c
+++ fs/incfs/main.c
@@ -30,8 +30,18 @@ static ssize_t corefs_show(struct kobject *kobj,
 
 static struct kobj_attribute corefs_attr = __ATTR_RO(corefs);
 
+static ssize_t mounter_context_for_backing_rw_show(struct kobject *kobj,
+			  struct kobj_attribute *attr, char *buff)
+{
+	return snprintf(buff, PAGE_SIZE, "supported\n");
+}
+
+static struct kobj_attribute mounter_context_for_backing_rw_attr =
+	__ATTR_RO(mounter_context_for_backing_rw);
+
 static struct attribute *attributes[] = {
 	&corefs_attr.attr,
+	&mounter_context_for_backing_rw_attr.attr,
 	NULL,
 };
 
