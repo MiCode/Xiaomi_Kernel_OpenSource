c19ddc0f308354296150dd0aa613d77020b96d46
David Collins <collinsd@codeaurora.org>
2021-06-04 19:35:36 -0700

    regulator: qcom_pm8008-regulator: add support for PMIC PM8010
    Add support to control the LDOs found on PM8010 PMICs.  PM8010 is
    a pin-compatible update of PM8008.
    
    Change-Id: Ia15481c9610bdf1b8ce305381decbf28c9f65716
    Signed-off-by: David Collins <collinsd@codeaurora.org>

--- drivers/regulator/qcom_pm8008-regulator.c
+++ drivers/regulator/qcom_pm8008-regulator.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-only
-/* Copyright (c) 2019-2020, The Linux Foundation. All rights reserved. */
+/* Copyright (c) 2019-2021, The Linux Foundation. All rights reserved. */
 
 #define pr_fmt(fmt) "PM8008: %s: " fmt, __func__
 
@@ -63,11 +63,18 @@
 
 #define LDO_STEPPER_CTL_REG(base)	(base + 0x3b)
 #define STEP_RATE_MASK			GENMASK(1, 0)
+/* Step rate in uV/us */
+#define PM8010_STEP_RATE		4800
 
 #define LDO_PD_CTL_REG(base)		(base + 0xA0)
 #define STRONG_PD_EN_BIT		BIT(7)
 
 #define PM8008_MAX_LDO			7
+
+enum pmic_subtype {
+	PM8008_SUBTYPE,
+	PM8010_SUBTYPE,
+};
 
 struct pm8008_chip {
 	struct device		*dev;
@@ -82,13 +89,20 @@ struct pm8008_chip {
 	bool			suspended;
 };
 
+struct reg_init_data {
+	u8			offset;
+	u8			data;
+};
+
 struct regulator_data {
-	char		*name;
-	char		*supply_name;
-	int		min_uv;
-	int		max_uv;
-	int		hpm_min_load_ua;
-	int		min_dropout_uv;
+	char				*name;
+	char				*supply_name;
+	int				min_uv;
+	int				max_uv;
+	int				hpm_min_load_ua;
+	int				min_dropout_uv;
+	const struct reg_init_data	*reg_init;
+	unsigned int			reg_init_size;
 };
 
 struct pm8008_regulator {
@@ -107,9 +121,10 @@ struct pm8008_regulator {
 	bool			chip_enabled;
 	int			mode;
 	int			uv;
+	enum pmic_subtype	pmic_subtype;
 };
 
-static struct regulator_data reg_data[PM8008_MAX_LDO] = {
+static const struct regulator_data pm8008_reg_data[PM8008_MAX_LDO] = {
 	/* name  parent      min_uv  max_uv  hpm_load  headroom_uv */
 	{"l1", "vdd_l1_l2",  528000, 1504000, 30000, 225000},
 	{"l2", "vdd_l1_l2",  528000, 1504000, 30000, 225000},
@@ -118,6 +133,40 @@ static struct regulator_data reg_data[PM8008_MAX_LDO] = {
 	{"l5", "vdd_l5",    1504000, 3400000, 10000, 300000},
 	{"l6", "vdd_l6",    1504000, 3400000, 10000, 300000},
 	{"l7", "vdd_l7",    1504000, 3400000, 10000, 300000},
+};
+
+static const struct reg_init_data pm8010_p300_reg_init_data[] = {
+	{0x55, 0x8A},
+	{0x77, 0x03},
+};
+
+static const struct reg_init_data pm8010_p600_reg_init_data[] = {
+	{0x76, 0x07},
+	{0x77, 0x03},
+};
+
+/*
+ * PM8010 LDOs 3, 4, and 6 can physically output a minimum of 1808 mV.  However,
+ * 1504 mV is specified here to match PM8008 and to avoid the parent supply of
+ * these regulators being stuck at an unnecessarily high voltage as a result of
+ * the framework maintaining a minimum vote of 1808 mV + headroom at all times
+ * (even when the LDOs are OFF).  This would waste power.  The LDO hardware
+ * automatically rounds up programmed voltages to supported set points.
+ */
+static const struct regulator_data pm8010_reg_data[PM8008_MAX_LDO] = {
+	/* name  parent      min_uv  max_uv  hpm_load  headroom_uv */
+	{"l1", "vdd_l1_l2",  528000, 1544000, 30000, 100000},
+	{"l2", "vdd_l1_l2",  528000, 1544000, 30000, 100000},
+	{"l3", "vdd_l3_l4", 1504000, 3312000, 10000, 300000,
+	 pm8010_p300_reg_init_data, ARRAY_SIZE(pm8010_p300_reg_init_data)},
+	{"l4", "vdd_l3_l4", 1504000, 3312000, 10000, 300000,
+	 pm8010_p300_reg_init_data, ARRAY_SIZE(pm8010_p300_reg_init_data)},
+	{"l5", "vdd_l5",    1504000, 3544000, 10000, 300000,
+	 pm8010_p600_reg_init_data, ARRAY_SIZE(pm8010_p600_reg_init_data)},
+	{"l6", "vdd_l6",    1504000, 3312000, 10000, 300000,
+	 pm8010_p300_reg_init_data, ARRAY_SIZE(pm8010_p300_reg_init_data)},
+	{"l7", "vdd_l7",    1504000, 3544000, 10000, 300000,
+	 pm8010_p600_reg_init_data, ARRAY_SIZE(pm8010_p600_reg_init_data)},
 };
 
 /* common functions */
@@ -132,7 +181,8 @@ static int pm8008_read(struct regmap *regmap,  u16 reg, u8 *val, int count)
 	return rc;
 }
 
-static int pm8008_write(struct regmap *regmap, u16 reg, u8 *val, int count)
+static int pm8008_write(struct regmap *regmap, u16 reg, const u8 *val,
+			int count)
 {
 	int rc;
 
@@ -586,6 +636,25 @@ error:
 	return NOTIFY_OK;
 }
 
+static int pm8008_regulator_register_init(struct pm8008_regulator *pm8008_reg,
+					  const struct regulator_data *reg_data)
+{
+	int i, rc;
+
+	if (!reg_data->reg_init)
+		return 0;
+
+	for (i = 0; i < reg_data->reg_init_size; i++) {
+		rc = pm8008_write(pm8008_reg->regmap,
+			pm8008_reg->base + reg_data->reg_init[i].offset,
+			&reg_data->reg_init[i].data, 1);
+		if (rc < 0)
+			return rc;
+	}
+
+	return 0;
+}
+
 static int pm8008_register_ldo(struct pm8008_regulator *pm8008_reg,
 						const char *name)
 {
@@ -593,9 +662,13 @@ static int pm8008_register_ldo(struct pm8008_regulator *pm8008_reg,
 	struct regulator_init_data *init_data;
 	struct device *dev = pm8008_reg->dev;
 	struct device_node *reg_node = pm8008_reg->of_node;
+	const struct regulator_data *reg_data;
 	int rc, i, init_voltage, is_enabled;
 	u32 base = 0;
 	u8 reg;
+
+	reg_data = pm8008_reg->pmic_subtype == PM8008_SUBTYPE ? pm8008_reg_data
+							      : pm8010_reg_data;
 
 	/* get regulator data */
 	for (i = 0; i < PM8008_MAX_LDO; i++)
@@ -613,6 +686,10 @@ static int pm8008_register_ldo(struct pm8008_regulator *pm8008_reg,
 		return rc;
 	}
 	pm8008_reg->base = base;
+
+	rc = pm8008_regulator_register_init(pm8008_reg, &reg_data[i]);
+	if (rc)
+		return rc;
 
 	pm8008_reg->hpm_min_load_ua = reg_data[i].hpm_min_load_ua;
 	of_property_read_u32(reg_node, "qcom,hpm-min-load",
@@ -644,14 +721,18 @@ static int pm8008_register_ldo(struct pm8008_regulator *pm8008_reg,
 	}
 
 	/* get slew rate */
-	rc = pm8008_read(pm8008_reg->regmap,
-			LDO_STEPPER_CTL_REG(pm8008_reg->base), &reg, 1);
-	if (rc < 0) {
-		pr_err("%s: failed to read step rate configuration rc=%d\n",
-				name, rc);
-		return rc;
+	if (pm8008_reg->pmic_subtype == PM8008_SUBTYPE) {
+		rc = pm8008_read(pm8008_reg->regmap,
+				LDO_STEPPER_CTL_REG(pm8008_reg->base), &reg, 1);
+		if (rc < 0) {
+			pr_err("%s: failed to read step rate configuration rc=%d\n",
+					name, rc);
+			return rc;
+		}
+		pm8008_reg->step_rate = 38400 >> (reg & STEP_RATE_MASK);
+	} else {
+		pm8008_reg->step_rate = PM8010_STEP_RATE;
 	}
-	pm8008_reg->step_rate = 38400 >> (reg & STEP_RATE_MASK);
 
 	init_data = of_get_regulator_init_data(dev, reg_node,
 						&pm8008_reg->rdesc);
@@ -738,6 +819,18 @@ static int pm8008_register_ldo(struct pm8008_regulator *pm8008_reg,
 	return 0;
 }
 
+static const struct of_device_id pm8008_regulator_match_table[] = {
+	{
+		.compatible	= "qcom,pm8008-regulator",
+		.data		= (void *)(uintptr_t)PM8008_SUBTYPE,
+	},
+	{
+		.compatible	= "qcom,pm8010-regulator",
+		.data		= (void *)(uintptr_t)PM8010_SUBTYPE,
+	},
+	{ },
+};
+
 /* PMIC probe and helper function */
 static int pm8008_parse_regulator(struct regmap *regmap, struct device *dev)
 {
@@ -747,14 +840,25 @@ static int pm8008_parse_regulator(struct regmap *regmap, struct device *dev)
 	struct pm8008_regulator *pm8008_reg;
 	struct regulator *en_supply;
 	struct pm8008_chip *chip;
+	const struct of_device_id *match;
+	enum pmic_subtype pmic_subtype;
 	bool ocp;
+
+	match = of_match_node(pm8008_regulator_match_table, dev->of_node);
+	if (match) {
+		pmic_subtype = (uintptr_t)match->data;
+	} else {
+		dev_err(dev, "could not find compatible string match\n");
+		return -ENODEV;
+	}
 
 	ocp = of_property_read_bool(dev->of_node, "qcom,enable-ocp-broadcast");
 
 	en_supply = devm_regulator_get(dev, "pm8008_en");
 	if (IS_ERR(en_supply)) {
 		rc = PTR_ERR(en_supply);
-		dev_err(dev, "failed to get pm8008_en supply\n");
+		if (rc != -EPROBE_DEFER)
+			dev_err(dev, "failed to get pm8008_en supply\n");
 		return rc;
 	}
 
@@ -776,6 +880,7 @@ static int pm8008_parse_regulator(struct regmap *regmap, struct device *dev)
 		pm8008_reg->enable_ocp_broadcast = ocp;
 		pm8008_reg->en_supply = en_supply;
 		pm8008_reg->chip = chip;
+		pm8008_reg->pmic_subtype = pmic_subtype;
 
 		rc = of_property_read_string(child, "regulator-name", &name);
 		if (rc)
@@ -1014,13 +1119,6 @@ static int pm8008_chip_resume(struct device *dev)
 	return 0;
 }
 #endif
-
-static const struct of_device_id pm8008_regulator_match_table[] = {
-	{
-		.compatible	= "qcom,pm8008-regulator",
-	},
-	{ },
-};
 
 static struct platform_driver pm8008_regulator_driver = {
 	.driver	= {
