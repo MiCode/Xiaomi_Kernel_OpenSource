e51400574311bf8aa4c99464bbab6fab80d8492c
Patrick Daly <pdaly@codeaurora.org>
2021-06-09 10:44:52 -0700

    mem-buf-ids: Add character device for qcom,cp_app
    Allow userspace clients to share memory with qcom,cp_app VM.
    
    Change-Id: I930501fe61bd54fa8376247f4f86f6b0ed7301bf
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

--- drivers/soc/qcom/mem_buf/mem-buf-ids.c
+++ drivers/soc/qcom/mem_buf/mem-buf-ids.c
@@ -42,6 +42,7 @@ PERIPHERAL_VM(CP_CAMERA_PREVIEW, cp_camera_preview);
 PERIPHERAL_VM(CP_SPSS_SP_SHARED, cp_spss_sp_shared);
 PERIPHERAL_VM(CP_SPSS_HLOS_SHARED, cp_spss_hlos_shared);
 PERIPHERAL_VM(CP_CDSP, cp_cdsp);
+PERIPHERAL_VM(CP_APP, cp_app);
 
 static struct mem_buf_vm vm_trusted_vm = {
 	.name = "qcom,trusted_vm",
@@ -71,6 +72,7 @@ struct mem_buf_vm *pdata_array[] = {
 	&vm_cp_spss_sp_shared,
 	&vm_cp_spss_hlos_shared,
 	&vm_cp_cdsp,
+	&vm_cp_app,
 	NULL,
 };
 
