6896b576514ca2129c5f4262bb597353859f06d9
Eric Chung <eric.chung@mediatek.com>
2021-06-10 12:10:03 +0800

    [ALPS05599694] GPU: Clone gpufreq mt6895 from mt6879
    [Detail]
    * Clone source code and build it to mtk_gpufreq_mt6895.ko
    * Add MTK_GPU_MT6895_SUPPORT Kconfig
    * Enable GPUFREQ_BRINGUP
    
    MTK-Commit-Id: faf202ae34e8eabf5c4dcef1e713bd12ad100ac8
    
    Change-Id: I9204ee2a6deeabc933750685f1c2d89c23e38242
    Signed-off-by: Eric Chung <eric.chung@mediatek.com>
    CR-Id: ALPS05599694
    Feature: GPU Extension(gpu_extension)

---
+++ drivers/gpu/mediatek/gpufreq/v2/gpudfd_mt6895.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2021 MediaTek Inc.
+ */
+
+/**
+ * @file    mtk_gpu_dfd.c
+ * @brief   GPU DFD
+ */
+
---
+++ drivers/gpu/mediatek/gpufreq/v2/include/gpudfd_mt6895.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2021 MediaTek Inc.
+ */
+
+#ifndef __GPUDFD_MT6895_H__
+#define __GPUDFD_MT6895_H__
+
+
+
+#endif /* __GPUDFD_MT6895_H__ */
