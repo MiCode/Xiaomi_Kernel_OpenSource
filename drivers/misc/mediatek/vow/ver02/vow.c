e900aa8e94f3f1ca502cba1e4b6bfe592298550c
JY Huang <jy.huang@mediatek.com>
2021-06-10 12:13:40 +0800

    [ALPS05775385] vow:FIX KE
    align VOW configuration
    
    MTK-Commit-Id: 3aee1381974a0a8ca166ae2deb1a8a70eb0b52d0
    
    Change-Id: Ib2dfd431c783932d23b2c58e67b350d34c57cf5a
    CR-Id: ALPS05775385
    Feature: VOW (MagiEar-Voice Wakeup)
    Signed-off-by: JY Huang <jy.huang@mediatek.com>

--- drivers/misc/mediatek/vow/ver02/vow.c
+++ drivers/misc/mediatek/vow/ver02/vow.c
@@ -292,10 +292,18 @@ void vow_ipi_rx_internal(unsigned int msg_id,
 			idx = DUMP_INPUT;
 			dump_work[idx].mic_data_size = ipi_ptr->mic_dump_size;
 			dump_work[idx].mic_offset = ipi_ptr->mic_offset;
+			//VOWDRV_DEBUG("%s(), [Input]dump size %d offset %d\n",
+					//__func__,
+					//dump_work[idx].mic_data_size,
+					//dump_work[idx].mic_offset);
 #if IS_ENABLED(CONFIG_MTK_VOW_DUAL_MIC_SUPPORT)
 			dump_work[idx].mic_data_size_R =
 					ipi_ptr->mic_dump_size;
 			dump_work[idx].mic_offset_R = ipi_ptr->mic_offset_R;
+			//VOWDRV_DEBUG("%s(), [Input R]dump size %d offset %d\n",
+					//__func__,
+					//dump_work[idx].mic_data_size_R,
+					//dump_work[idx].mic_offset_R);
 #endif  /* #if IS_ENABLED(CONFIG_MTK_VOW_DUAL_MIC_SUPPORT) */
 			ret = queue_work(dump_workqueue[idx],
 					 &dump_work[idx].work);
@@ -311,7 +319,10 @@ void vow_ipi_rx_internal(unsigned int msg_id,
 			idx = DUMP_BARGEIN;
 			dump_work[idx].echo_data_size = ipi_ptr->echo_dump_size;
 			dump_work[idx].echo_offset = ipi_ptr->echo_offset;
-
+			//VOWDRV_DEBUG("%s(), [BargeIn]dump size %d offset %d\n",
+					//__func__,
+					//dump_work[idx].echo_data_size,
+					//dump_work[idx].echo_offset);
 			ret = queue_work(dump_workqueue[idx],
 					 &dump_work[idx].work);
 			if (ret == 0)
@@ -1368,7 +1379,7 @@ static bool vow_stop_dump_wait(void)
 static int vow_pcm_dump_notify(bool enable)
 {
 #if IS_ENABLED(CONFIG_MTK_TINYSYS_SCP_SUPPORT)
-	unsigned int vow_ipi_buf[5] = {0};
+	unsigned int vow_ipi_buf[7] = {0};
 	bool ret;
 
 	/* if scp reset happened, need re-send PCM dump IPI to SCP again */
