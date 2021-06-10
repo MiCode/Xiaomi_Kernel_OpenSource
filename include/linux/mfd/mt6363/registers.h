06cb3bb16a6d5cf148321667a7b6a1ae498ddd0e
fengping.yu <fengping.yu@mediatek.com>
2021-06-10 12:45:39 +0800

    [ALPS05784839] drivers: input: keypad: add 6363 pmic key
    Add 6363 pmic key support
    
    MTK-Commit-Id: baf0449e4311e5ef2871a3f2b7c5f8ebbe56a0a8
    
    Change-Id: I98320b0e206ec144b925b97feebe2fce8119b344
    Signed-off-by: fengping.yu <fengping.yu@mediatek.com>
    CR-Id: ALPS05784839
    Feature: Keypad

--- include/linux/mfd/mt6363/registers.h
+++ include/linux/mfd/mt6363/registers.h
@@ -8,11 +8,14 @@
 #define __MFD_MT6363_REGISTERS_H__
 
 /* PMIC Registers */
+#define MT6363_TOPSTATUS		   (0x1e)
 #define MT6363_MISC_TOP_INT_CON0           (0x37)
 #define MT6363_MISC_TOP_INT_STATUS0        (0x43)
 #define MT6363_TOP_INT_STATUS1             (0x4e)
 #define MT6363_PSC_TOP_INT_CON0            (0x90f)
 #define MT6363_PSC_TOP_INT_STATUS0         (0x91b)
+#define MT6363_STRUP_CON11		   (0xa0e)
+#define MT6363_STRUP_CON12		   (0xa0f)
 #define MT6363_BM_TOP_INT_CON0             (0xc24)
 #define MT6363_BM_TOP_INT_STATUS0          (0xc36)
 #define MT6363_HK_TOP_INT_CON0             (0xf92)
