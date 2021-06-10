966ada84a6ca11808f79245d104d5400ca403d68
“jayce.hsu” <jayce.hsu@mediatek.com>
2021-06-10 12:08:59 +0800

    [ALPS05679031] cmdq: set sec_id by sec_handle
    [Detail]
    set sec_id by sec_handle
    
    MTK-Commit-Id: 05099020935d07ff1e4d4111ef694ac1877bfd80
    
    Change-Id: I286a537740695d28c8fc42b4ed135c4896e018d8
    Signed-off-by: “jayce.hsu” <jayce.hsu@mediatek.com>
    CR-Id:  ALPS05679031
    Feature: [Module]CMDQ Driver

--- drivers/misc/mediatek/cmdq/mailbox/cmdq-sec.h
+++ drivers/misc/mediatek/cmdq/mailbox/cmdq-sec.h
@@ -64,6 +64,8 @@ struct cmdq_sec_addr_meta {
 	uint32_t offset;	/* [IN]_b, buffser offset to secure handle */
 	uint32_t size;		/* buffer size */
 	uint32_t port;		/* hw port id (i.e. M4U port id) */
+	uint32_t sec_id;
+	uint32_t useSecIdinMeta;
 };
 
 struct cmdq_sec_data {
