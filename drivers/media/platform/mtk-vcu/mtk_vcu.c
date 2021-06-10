1e73822ac69a923b7cd751ed7cef2c8d38d3cdaf
zhimin Liu <zhimin.liu@mediatek.com>
2021-06-10 12:15:17 +0800

    [ALPS05724304] VCU: Fix 8168 build fail when enable SVP
    8168 will build fail when enable svp config
    
    MTK-Commit-Id: 5d9903be034b8b6b3c313ed859ca27fffab408a7
    
    Change-Id: I88fb36c1d51a09943e6c32825033e68d8517ab68
    Signed-off-by: zhimin Liu <zhimin.liu@mediatek.com>
    CR-Id: ALPS05724304
    Feature: [Module]Video Driver

--- drivers/media/platform/mtk-vcu/mtk_vcu.c
+++ drivers/media/platform/mtk-vcu/mtk_vcu.c
@@ -779,7 +779,7 @@ static void vcu_gce_flush_callback(struct cmdq_cb_data data)
 				buff->cmdq_buff.core_id, &vcu->flags[i]);
 #if defined(CONFIG_MTK_SEC_VIDEO_PATH_SUPPORT)
 #if !(IS_ENABLED(CONFIG_MACH_MT6768) || IS_ENABLED(CONFIG_MACH_MT6779) ||\
-IS_ENABLED(CONFIG_MACH_MT6785))
+IS_ENABLED(CONFIG_MACH_MT6785) || IS_ENABLED(CONFIG_MACH_MT8168))
 			if (buff->cmdq_buff.secure != 0)
 				cmdq_sec_mbox_switch_normal(vcu->clt_venc_sec[0],
 				(buff->cmdq_buff.secure == 3));
