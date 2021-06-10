5602292e5b8e6d8bcc80372828fb01a34b0a2452
Juju Sung <juju.sung@mediatek.com>
2021-06-10 11:49:19 +0800

    [ALPS05652867] arm64: dts: mt6893: mml kernel-5.10 compatibility
    fix dts k4.14 to k5.10 fail issue
    
    MTK-Commit-Id: 3205087f58a43d10b8fe334d622ce020d5017b7d
    
    Change-Id: Ief2e058a73c4def60e1826ac73e57d48dba556ab
    Signed-off-by: Juju Sung <juju.sung@mediatek.com>
    CR-Id: ALPS05652867
    Feature: [Module]MML driver

--- include/dt-bindings/mml/mml-mt6893.h
+++ include/dt-bindings/mml/mml-mt6893.h
@@ -63,4 +63,38 @@
 #define MML_ENGINE_TOTAL	34
 */
 
+/* MML mux types */
+#define MOUT	1
+#define SOUT	2
+#define SLIN	3
+/* MML mux registers in mt6893 */
+#define MML_ISP0_MOUT_EN	MOUT 0xf10
+#define MML_ISP1_MOUT_EN	MOUT 0xf14
+#define MML_ISP2_MOUT_EN	MOUT 0xf18
+#define MML_ISP3_MOUT_EN	MOUT 0xf1c
+#define MML_RDMA0_MOUT_EN	MOUT 0xf20
+#define MML_RDMA1_MOUT_EN	MOUT 0xf24
+#define MML_RDMA2_MOUT_EN	MOUT 0xf28
+#define MML_RDMA3_MOUT_EN	MOUT 0xf2c
+#define MML_PQ0_SEL_IN		SLIN 0xf30
+#define MML_PQ1_SEL_IN		SLIN 0xf34
+#define MML_PQ2_SEL_IN		SLIN 0xf38
+#define MML_PQ3_SEL_IN		SLIN 0xf3c
+#define MML_PQ0_SOUT_SEL	SOUT 0xf40
+#define MML_PQ1_SOUT_SEL	SOUT 0xf44
+#define MML_AAL0_MOUT_EN	MOUT 0xf48
+#define MML_AAL1_MOUT_EN	MOUT 0xf4c
+#define MML_TCC0_SOUT_SEL	SOUT 0xf50
+#define MML_TCC1_SOUT_SEL	SOUT 0xf54
+#define MML_HDR0_SEL_IN		SLIN 0xf58
+#define MML_HDR1_SEL_IN		SLIN 0xf5c
+#define MML_RSZ0_SEL_IN		SLIN 0xf60
+#define MML_RSZ1_SEL_IN		SLIN 0xf64
+#define MML_RSZ2_SEL_IN		SLIN 0xf68
+#define MML_RSZ3_SEL_IN		SLIN 0xf6c
+#define MML_WROT0_SEL_IN	SLIN 0xf70
+#define MML_WROT1_SEL_IN	SLIN 0xf74
+#define MML_WROT2_SEL_IN	SLIN 0xf78
+#define MML_WROT3_SEL_IN	SLIN 0xf7c
+
 #endif	/* _DT_BINDINGS_MML_MT6893_H */
