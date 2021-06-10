3a402a2ecf49c5aba9b76c84521cd3c6d032fb0a
Jiachiam Liu <jiachiam.liu@mediatek.com>
2021-06-10 12:20:29 +0800

    [ALPS05630321] sspm: support EMI MPU
    Fix build issue of emi mpu
    
    MTK-Commit-Id: 226121d00208f6ceee0cf32c0dec833a34c20edd
    
    Change-Id: Ibbe5fc027c0c807728d8760f9463fdef679ef140
    Signed-off-by: Jiachiam Liu <jiachiam.liu@mediatek.com>
    CR-Id: ALPS05630321
    Feature: Secure System Power Manager

--- drivers/misc/mediatek/sspm/v3/sspm_emi_mpu.c
+++ drivers/misc/mediatek/sspm/v3/sspm_emi_mpu.c
@@ -4,41 +4,32 @@
  */
 
 #include <linux/module.h>       /* needed by all modules */
-#include <memory/mediatek/emi.h>
+#include <soc/mediatek/emi.h>
 
-#define SSPM_MPU_REGION_ID     5
+#define MPU_REGION_ID_SSPM	5
+#define MPU_DOMAIN_D0_AP	0
+#define MPU_DOMAIN_D8_SSPM	8
 
-static unsigned long long sspm_start;
-static unsigned long long sspm_end;
-
-void __init sspm_set_emi_mpu(phys_addr_t base, phys_addr_t size)
-{
-	sspm_start = base;
-	sspm_end = base + size - 1;
-}
-
-static int __init post_sspm_set_emi_mpu(void)
+void sspm_set_emi_mpu(phys_addr_t base, phys_addr_t size)
 {
 	struct emimpu_region_t rg_info;
 	int ret;
 
-	ret = mtk_emimpu_init_region(&rg_info, SSPM_MPU_REGION_ID);
+	ret = mtk_emimpu_init_region(&rg_info, MPU_REGION_ID_SSPM);
 
 	if (ret) {
 		pr_info("[SSPM] set emimpu fail\n");
-		return ret;
+		return;
 	}
 
-	mtk_emimpu_set_addr(&rg_info, sspm_start, sspm_end);
+	mtk_emimpu_set_addr(&rg_info, base, base + size - 1);
 
-	mtk_emimpu_set_apc(&rg_info, 0, MTK_EMIMPU_NO_PROTECTION);
-	mtk_emimpu_set_apc(&rg_info, 8, MTK_EMIMPU_NO_PROTECTION);
+	mtk_emimpu_set_apc(&rg_info, MPU_DOMAIN_D0_AP,
+		MTK_EMIMPU_NO_PROTECTION);
+	mtk_emimpu_set_apc(&rg_info, MPU_DOMAIN_D8_SSPM,
+		MTK_EMIMPU_NO_PROTECTION);
 
 	mtk_emimpu_set_protection(&rg_info);
 
 	mtk_emimpu_free_region(&rg_info);
-
-	return 0;
 }
-
-late_initcall(post_sspm_set_emi_mpu);
