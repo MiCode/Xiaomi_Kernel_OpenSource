29ba3e01c1517a72868735ea4846579b39cd3f8a
Wei-Chia Su <wei-chia.su@mediatek.com>
2021-06-10 12:06:10 +0800

    [ALPS05787230] CPU DVFS: fix Coverity Scanned Defect
    [Detail] CPU DVFS: fix Coverity Scanned Defect
    
    MTK-Commit-Id: bc0d1f4b4062edac30422bb5d927972b32f0db55
    
    Change-Id: I856390e66a3fd276a85e242fc0b18fd47233bfaa
    Signed-off-by: Wei-Chia Su <wei-chia.su@mediatek.com>
    CR-Id: ALPS05787230
    Feature: CPU DVFS (CPU Dynamic Voltage and Frequency Scaling)

--- drivers/misc/mediatek/base/power/leakage_table_v2/mtk_static_power.c
+++ drivers/misc/mediatek/base/power/leakage_table_v2/mtk_static_power.c
@@ -632,7 +632,7 @@ int mt_spower_get_leakage(int dev, unsigned int vol, int deg)
 {
 	int ret;
 
-	if (dev < 0 || !tab_validate(&sptab[dev]))
+	if (dev >= MTK_SPOWER_MAX || dev < 0 || !tab_validate(&sptab[dev]))
 		return 0;
 
 	if (vol > mV(&sptab[dev], VSIZE - 1))
@@ -657,10 +657,7 @@ int mt_spower_get_leakage_uW(int dev, unsigned int vol, int deg)
 {
 	int ret;
 
-	if (dev < 0)
-		return 0;
-
-	if (!tab_validate(&sptab[dev]))
+	if (dev >= MTK_SPOWER_MAX || dev < 0 || !tab_validate(&sptab[dev]))
 		return 0;
 
 	if (vol > mV(&sptab[dev], VSIZE - 1))
