3d22eeb5ed64c5e9e1b1f8165c4eeda3dcc3b3af
Ping-Hsun Wu <ping-hsun.wu@mediatek.com>
2021-06-10 11:53:22 +0800

    [ALPS05652867] mml: implement mmlsys op
    Implement mmlsys operations
    
    MTK-Commit-Id: 64ea153c4c2f0166e64fa7e962d42abc023edeaa
    
    Change-Id: I94ed50cffcd2b6882aa2bb5a208d3d7e37b238f8
    Signed-off-by: Chris-YC Chen <chris-yc.chen@mediatek.com>
    Signed-off-by: Ping-Hsun Wu <ping-hsun.wu@mediatek.com>
    CR-Id: ALPS05652867
    Feature: [Module]MML driver

--- drivers/gpu/drm/mediatek/mml/mtk-mml-sys.c
+++ drivers/gpu/drm/mediatek/mml/mtk-mml-sys.c
@@ -8,35 +8,162 @@
 #include <linux/module.h>
 #include <linux/of_device.h>
 #include <linux/platform_device.h>
+#include <linux/soc/mediatek/mtk-cmdq.h>
 
 #include "mtk-mml-core.h"
 #include "mtk-mml-driver.h"
 
 #define MML_MAX_SYS_COMPONENTS	10
+#define MML_MAX_SYS_MUX_PINS	50
 
 struct mml_data {
 };
 
-struct mtk_mml_sys {
-	struct mml_data *data;
+static const struct mml_data mt6893_mml_data = {
+};
+
+enum mml_mux_type {
+	UNUSED,
+	MOUT,
+	SOUT,
+	SELIN,
+};
+
+struct mml_mux_pin {
+	u16 index;
+	u16 from;
+	u16 to;
+	u16 type;
+	u16 offset;
+} __attribute__ ((__packed__));
+
+struct mml_sys {
+	const struct mml_data *data;
 	struct mml_comp comps[MML_MAX_SYS_COMPONENTS];
 	u32 comp_cnt;
 	u32 comp_bound;
+
+	/* MML multiplexer pins.
+	 * The entry 0 leaves empty for efficiency, do not use. */
+	struct mml_mux_pin mux_pins[MML_MAX_SYS_MUX_PINS + 1];
+	u32 mux_cnt;
+	/* Table of component adjacency with mux pins.
+	 *
+	 * The element value is an index to mux pin array.
+	 * In the upper-right tri. are MOUTs or SOUTs by adjacency[from][to];
+	 * in the bottom-left tri. are SELIN pins by adjacency[to][from].
+	 * Direct-wire is not in this table.
+	 *
+	 * array data would be like:
+	 *	[0] = { 0  indices of },
+	 *	[1] = { . 0 . MOUTs & },
+	 *	[2] = { . . 0 . SOUTs },
+	 *	[3] = { . . . 0 . . . },
+	 *	[4] = { . . . . 0 . . },
+	 *	[5] = { indices . 0 . },
+	 *	[6] = { of SELINs . 0 },
+	 */
+	u8 adjacency[MML_MAX_SYS_COMPONENTS][MML_MAX_SYS_COMPONENTS];
 };
 
-static int mml_sys_init(struct platform_device *pdev, struct mtk_mml_sys *sys,
+static void config_mux(struct mml_sys *sys, struct cmdq_pkt *pkt,
+		       const phys_addr_t base_pa, u8 mux_idx,
+		       u16 *offset, u32 *mout)
+{
+	struct mml_mux_pin *mux;
+
+	if (!mux_idx)
+		return;
+	mux = &sys->mux_pins[mux_idx];
+
+	switch (mux->type) {
+	case MOUT:
+		*offset = mux->offset;
+		*mout |= 1 << mux->index;
+		break;
+	case SOUT:
+	case SELIN:
+		cmdq_pkt_write(pkt, NULL, base_pa + mux->offset,
+			       mux->index, U32_MAX);
+		break;
+	default:
+		break;
+	}
+}
+
+static s32 sys_config_tile(struct mml_comp *comp, struct mml_task *task,
+			   struct mml_comp_config *ccfg, u8 idx)
+{
+	struct mml_sys *sys = container_of(comp, struct mml_sys, comps[0]);
+	const struct mml_topology_path *path = task->config->path[ccfg->pipe];
+	struct cmdq_pkt *pkt = task->pkts[ccfg->pipe];
+	const phys_addr_t base_pa = comp->base_pa;
+	u32 i, j;
+
+	for (i = 0; i < path->node_cnt; i++) {
+		const struct mml_path_node *node = &path->nodes[i];
+		u16 offset = 0;
+		u32 mout = 0;
+		u8 from = node->id, to, mux_idx;
+
+		/* TODO: continue if node disabled */
+		for (j = 0; j < ARRAY_SIZE(node->next); j++) {
+			if (node->next[j]) {	/* && next enabled */
+				to = node->next[j]->id;
+				mux_idx = sys->adjacency[from][to];
+				config_mux(sys, pkt, base_pa, mux_idx,
+					   &offset, &mout);
+				mux_idx = sys->adjacency[to][from];
+				config_mux(sys, pkt, base_pa, mux_idx,
+					   &offset, &mout);
+			}
+		}
+		if (mout)
+			cmdq_pkt_write(pkt, NULL, base_pa + offset,
+				       mout, U32_MAX);
+	}
+	return 0;
+}
+
+static const struct mml_comp_config_ops sys_config_ops = {
+	.tile = sys_config_tile
+};
+
+static int mml_sys_init(struct platform_device *pdev, struct mml_sys *sys,
 	const struct component_ops *comp_ops)
 {
 	struct device *dev = &pdev->dev;
-	int comp_cnt, i;
+	struct device_node *node = dev->of_node;
+	int mux_cnt, comp_cnt, i;
 	int ret;
 
-	sys->data = (struct mml_data *)of_device_get_match_data(dev);
+	sys->data = (const struct mml_data *)of_device_get_match_data(dev);
 
-	comp_cnt = of_mml_count_comps(dev->of_node);
+	/* Initialize mux-pins */
+	mux_cnt = of_property_count_elems_of_size(node, "mux-pins",
+						  sizeof(struct mml_mux_pin));
+	if (mux_cnt < 0 || mux_cnt > MML_MAX_SYS_MUX_PINS) {
+		dev_err(dev, "no mux-pins or out of size in component %s: %d\n",
+			node->full_name, mux_cnt);
+		return -EINVAL;
+	}
+
+	of_property_read_u16_array(node, "mux-pins", (u16 *)&sys->mux_pins[1],
+		mux_cnt * (sizeof(struct mml_mux_pin) / sizeof(u16)));
+	for (i = 1; i <= mux_cnt; i++) {
+		struct mml_mux_pin *mux = &sys->mux_pins[i];
+
+		if (mux->type == SELIN)
+			sys->adjacency[mux->to][mux->from] = i;
+		else
+			sys->adjacency[mux->from][mux->to] = i;
+	}
+
+	/* Initialize component and subcomponents */
+	comp_cnt = of_mml_count_comps(node);
 	if (comp_cnt <= 0) {
 		dev_err(dev, "no comp-ids in component %s: %d\n",
-			dev->of_node->full_name, comp_cnt);
+			node->full_name, comp_cnt);
 		return -EINVAL;
 	}
 
@@ -48,6 +175,10 @@ static int mml_sys_init(struct platform_device *pdev, struct mtk_mml_sys *sys,
 			return ret;
 		}
 	}
+
+	/* TODO: distinguish component and subcomponents */
+	sys->comps[0].config_ops = &sys_config_ops;
+
 	ret = component_add(dev, comp_ops);
 	if (ret) {
 		dev_err(dev, "failed to add mmlsys comp-%d: %d\n", 0, ret);
@@ -70,11 +201,11 @@ err_comp_add:
 	return ret;
 }
 
-struct mtk_mml_sys *mml_sys_create(struct platform_device *pdev,
+struct mml_sys *mml_sys_create(struct platform_device *pdev,
 	const struct component_ops *comp_ops)
 {
 	struct device *dev = &pdev->dev;
-	struct mtk_mml_sys *sys;
+	struct mml_sys *sys;
 	int ret;
 
 	sys = devm_kzalloc(dev, sizeof(*sys), GFP_KERNEL);
@@ -90,7 +221,7 @@ struct mtk_mml_sys *mml_sys_create(struct platform_device *pdev,
 	return sys;
 }
 
-void mml_sys_destroy(struct platform_device *pdev, struct mtk_mml_sys *sys,
+void mml_sys_destroy(struct platform_device *pdev, struct mml_sys *sys,
 	const struct component_ops *comp_ops)
 {
 	int i;
@@ -101,7 +232,7 @@ void mml_sys_destroy(struct platform_device *pdev, struct mtk_mml_sys *sys,
 }
 
 int mml_sys_bind(struct device *dev, struct device *master,
-	struct mtk_mml_sys *sys)
+	struct mml_sys *sys)
 {
 	s32 ret;
 
@@ -117,7 +248,7 @@ int mml_sys_bind(struct device *dev, struct device *master,
 }
 
 void mml_sys_unbind(struct device *dev, struct device *master,
-	struct mtk_mml_sys *sys)
+	struct mml_sys *sys)
 {
 	if (WARN_ON(sys->comp_bound <= 0))
 		return;
@@ -141,7 +272,7 @@ static const struct component_ops mml_comp_ops = {
 
 static int probe(struct platform_device *pdev)
 {
-	struct mtk_mml_sys *priv;
+	struct mml_sys *priv;
 
 	priv = mml_sys_create(pdev, &mml_comp_ops);
 	if (IS_ERR(priv)) {
@@ -158,9 +289,6 @@ static int remove(struct platform_device *pdev)
 	mml_sys_destroy(pdev, platform_get_drvdata(pdev), &mml_comp_ops);
 	return 0;
 }
-
-static struct mml_data mt6893_mml_data = {
-};
 
 const struct of_device_id mtk_mml_of_ids[] = {
 	{
