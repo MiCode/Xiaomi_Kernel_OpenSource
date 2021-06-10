59237e0dde8c6844bdbc1636a6a71a24818362b0
Macpaul Lin <macpaul.lin@mediatek.com>
2021-06-10 12:24:28 +0800

    [ALPS05781819] eccci: enable CONFIG_MTK_ECCCI_C2K_USB module support
    eccci: enable CONFIG_MTK_ECCCI_C2K_USB support for module build.
    
    MTK-Commit-Id: 0dfdab81d8fb5ee924b95d6ec55ab2e972625fd5
    
    Change-Id: I918798c9f1eef4c5d2d8718a90a54b905abd6042
    Signed-off-by: Macpaul Lin <macpaul.lin@mediatek.com>
    CR-Id: ALPS05781819
    Feature: Modem Interface Driver

--- drivers/misc/mediatek/eccci/port/port_char.c
+++ drivers/misc/mediatek/eccci/port/port_char.c
@@ -110,7 +110,7 @@ static int port_char_init(struct port_t *port)
 	return ret;
 }
 
-#ifdef CONFIG_MTK_ECCCI_C2K
+#if IS_ENABLED(CONFIG_MTK_ECCCI_C2K_USB)
 static int c2k_req_push_to_usb(struct port_t *port, struct sk_buff *skb)
 {
 	int md_id = port->md_id;
@@ -137,13 +137,10 @@ static int c2k_req_push_to_usb(struct port_t *port, struct sk_buff *skb)
 	skb_pull(skb, sizeof(struct ccci_header));
 
 retry_push:
-	/* push to usb */
-#ifdef USB_RAWBULK_READY
+	/* push to usb: USB_RAWBULK_READY: requires C2K USB driver */
 	read_count = rawbulk_push_upstream_buffer(c2k_ch_id,
 		skb->data, read_len);
-#else
-	read_count = 0;
-#endif
+  
 	CCCI_DEBUG_LOG(md_id, CHAR,
 		"data push to usb bypass (ch%d)(%d)\n",
 		port->rx_ch, read_count);
@@ -185,7 +182,7 @@ static int port_char_recv_skb(struct port_t *port, struct sk_buff *skb)
 		AP_IPC_LWAPROXY + CCCI_IPC_MINOR_BASE)))
 		return -CCCI_ERR_DROP_PACKET;
 
-#ifdef CONFIG_MTK_ECCCI_C2K
+#if IS_ENABLED(CONFIG_MTK_ECCCI_C2K_USB)
 	if (port->interception) {
 		c2k_req_push_to_usb(port, skb);
 		return 0;
