e91b1949f19668d65615b1ce571a3d9adb0bf392
Guo Li <guo.li@mediatek.com>
2021-06-10 11:50:30 +0800

    [ALPS05710409] DISP: Use kconfig instead of macro
    [Detail]
    Macro definition write in the makefile,
    Some head file will use this macro.
    BuT if other file which didnt in the same folder,
    will include this head file, it should also add the
    macro in its makefile. Thats too bad.
    so use kconfig instead of macro will better.
    
    MTK-Commit-Id: 8240b635b82c94aa0805547f7cc6c05fb5593b9f
    
    Signed-off-by: mtk54055 <guo.li@mediatek.com>
    CR-Id: ALPS05710409
    Feature: Display Driver (DSI Interface)
    Change-Id: I6d207b82ed024e97c6a995e4ddaf139ca9777162

--- drivers/misc/mediatek/video/mt6768/dispsys/ddp_wdma_ex.c
+++ drivers/misc/mediatek/video/mt6768/dispsys/ddp_wdma_ex.c
@@ -673,7 +673,7 @@ wdma_golden_setting(enum DISP_MODULE_ENUM module,
 		res = p_golden_setting->dst_width *
 			p_golden_setting->dst_height;
 	} else {
-#ifdef FT_HDCP_FEATURE
+#ifdef CONFIG_MTK_DX_HDCP_SUPPORT
 		res = 1920 * 1080;
 		frame_rate = 60;
 #else
@@ -1208,7 +1208,11 @@ static int wdma_config_l(enum DISP_MODULE_ENUM module,
 {
 
 	struct WDMA_CONFIG_STRUCT *config = &pConfig->wdma_config;
+#ifdef CONFIG_MTK_DX_HDCP_SUPPORT
+	unsigned int is_primary_flag = 0; /*primary or external*/
+#else
 	unsigned int is_primary_flag = 1; /*primary or external*/
+#endif
 	unsigned int bwBpp;
 	unsigned long long wdma_bw;
 
