6b1809f9190a8a8ac9250d53bf8ababd8d484f56
Tim Chang <tim.chang@mediatek.com>
2021-06-10 12:00:35 +0800

    [ALPS05580683] connectivity: Connsys Power Throttling
    Connsys Power Throttling is a common driver for receiving low
    power level event from platform and notfiy radios of Connsys.
    
    MTK-Commit-Id: 2be9c2cce2fedf8e958117d39659fa7de98b796a
    
    Change-Id: Ibd21ee9117018f5a1f601dd366c1cce8727a447b
    Signed-off-by: Tim Chang <tim.chang@mediatek.com>
    CR-Id: ALPS05580683
    Feature: [Module]Conninfra Driver

---
+++ drivers/misc/mediatek/connectivity/power_throttling/core.c
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#include <linux/types.h>
+#include <linux/printk.h>
+#include <mach/upmu_sw.h>
+#include "conn_power_throttling.h"
+int conn_pwr_core_init(void)
+{
+	return 0;
+}
+
+int conn_pwr_get_drv_level(enum conn_pwr_drv_type type, enum conn_pwr_low_battery_level *level)
+{
+	return 0;
+}
+EXPORT_SYMBOL(conn_pwr_get_drv_level);
+
+int conn_pwr_arbitrate(struct conn_pwr_update_info *info)
+{
+	return 0;
+}
+
+int conn_pwr_report_level_required(enum conn_pwr_drv_type type,
+					enum conn_pwr_low_battery_level level)
+{
+	return 0;
+}
+EXPORT_SYMBOL(conn_pwr_report_level_required);
