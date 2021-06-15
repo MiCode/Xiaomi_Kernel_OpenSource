41867274cb4a449a8f0599656c2362008dba1525
Isaac J. Manjarres <isaacm@codeaurora.org>
2021-06-15 12:07:39 -0700

    mem-buf: Add a snapshot of the mem-buf driver
    Add a snapshot of the mem-buf driver as of msm-5.10
    commit <0cb60b34b392> ("Merge "mem-buf: Create IOCTL to see if buffer
    belongs solely to a VM"").
    
    Change-Id: I0cc30b4d3f3e8a17bfcf73bf9574c7422955af42
    Signed-off-by: Isaac J. Manjarres <isaacm@codeaurora.org>

---
+++ drivers/soc/qcom/mem_buf/mem-buf-dev.c
@@ -0,0 +1,421 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/memory_hotplug.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+#include <linux/gunyah/gh_rm_drv.h>
+#include <soc/qcom/secure_buffer.h>
+
+#include <linux/mem-buf.h>
+#include "mem-buf-dev.h"
+
+#define CREATE_TRACE_POINTS
+#include "trace-mem-buf.h"
+EXPORT_TRACEPOINT_SYMBOL(send_alloc_req);
+EXPORT_TRACEPOINT_SYMBOL(receive_alloc_req);
+EXPORT_TRACEPOINT_SYMBOL(send_relinquish_msg);
+EXPORT_TRACEPOINT_SYMBOL(receive_relinquish_msg);
+EXPORT_TRACEPOINT_SYMBOL(send_alloc_resp_msg);
+EXPORT_TRACEPOINT_SYMBOL(receive_alloc_resp_msg);
+EXPORT_TRACEPOINT_SYMBOL(mem_buf_alloc_info);
+
+struct device *mem_buf_dev;
+EXPORT_SYMBOL(mem_buf_dev);
+
+unsigned char mem_buf_capability;
+EXPORT_SYMBOL(mem_buf_capability);
+
+struct gh_acl_desc *mem_buf_vmid_perm_list_to_gh_acl(int *vmids, int *perms,
+		unsigned int nr_acl_entries)
+{
+	struct gh_acl_desc *gh_acl;
+	size_t size;
+	unsigned int i;
+
+	size = offsetof(struct gh_acl_desc, acl_entries[nr_acl_entries]);
+	gh_acl = kmalloc(size, GFP_KERNEL);
+	if (!gh_acl)
+		return ERR_PTR(-ENOMEM);
+
+	gh_acl->n_acl_entries = nr_acl_entries;
+	for (i = 0; i < nr_acl_entries; i++) {
+		gh_acl->acl_entries[i].vmid = vmids[i];
+		gh_acl->acl_entries[i].perms = perms[i];
+	}
+
+	return gh_acl;
+}
+EXPORT_SYMBOL(mem_buf_vmid_perm_list_to_gh_acl);
+
+struct gh_sgl_desc *mem_buf_sgt_to_gh_sgl_desc(struct sg_table *sgt)
+{
+	struct gh_sgl_desc *gh_sgl;
+	size_t size;
+	int i;
+	struct scatterlist *sg;
+
+	size = offsetof(struct gh_sgl_desc, sgl_entries[sgt->orig_nents]);
+	gh_sgl = kvmalloc(size, GFP_KERNEL);
+	if (!gh_sgl)
+		return ERR_PTR(-ENOMEM);
+
+	gh_sgl->n_sgl_entries = sgt->orig_nents;
+	for_each_sgtable_sg(sgt, sg, i) {
+		gh_sgl->sgl_entries[i].ipa_base = sg_phys(sg);
+		gh_sgl->sgl_entries[i].size = sg->length;
+	}
+
+	return gh_sgl;
+}
+EXPORT_SYMBOL(mem_buf_sgt_to_gh_sgl_desc);
+
+static int mem_buf_assign_mem_gunyah(bool is_lend, struct sg_table *sgt,
+				struct mem_buf_lend_kernel_arg *arg,
+				bool *has_lookup_sgl)
+{
+	u32 src_vmid[] = {current_vmid};
+	u32 src_perm[] = {PERM_READ | PERM_WRITE | PERM_EXEC};
+	int ret, i;
+	struct gh_sgl_desc *gh_sgl;
+	struct gh_acl_desc *gh_acl;
+	size_t size;
+	struct scatterlist *sgl;
+
+	/* Due to memory-hotplug */
+	size = 0;
+	for_each_sgtable_sg(sgt, sgl, i)
+		size += sgl->length;
+	if (!IS_ALIGNED(size, SUBSECTION_SIZE)) {
+		pr_err_ratelimited("Operation requires SUBSECTION_SIZE alignemnt, size = %zx\n",
+				   size);
+		return -EINVAL;
+	}
+
+	gh_sgl = mem_buf_sgt_to_gh_sgl_desc(sgt);
+	if (IS_ERR(gh_sgl))
+		return PTR_ERR(gh_sgl);
+
+	gh_acl = mem_buf_vmid_perm_list_to_gh_acl(arg->vmids, arg->perms,
+						  arg->nr_acl_entries);
+	if (IS_ERR(gh_acl)) {
+		ret = PTR_ERR(gh_acl);
+		goto err_gh_acl;
+	}
+
+	pr_debug("%s: Assigning memory to target VMIDs\n", __func__);
+	ret = hyp_assign_table(sgt, src_vmid, ARRAY_SIZE(src_vmid),
+			       arg->vmids, arg->perms, arg->nr_acl_entries);
+	if (ret < 0) {
+		pr_err("%s: failed to assign memory for rmt allocation rc:%d\n",
+		       __func__, ret);
+		goto err_hyp_assign;
+	} else {
+		pr_debug("%s: Memory assigned to target VMIDs\n", __func__);
+	}
+
+	/*
+	 * gh_rm_mem_qcom_lookup_sgl is no longer supported and is replaced with
+	 * gh_rm_mem_share. To ease this transition, fall back to the later on error.
+	 */
+	*has_lookup_sgl = true;
+	ret = gh_rm_mem_qcom_lookup_sgl(GH_RM_MEM_TYPE_NORMAL, arg->label,
+					gh_acl, gh_sgl, NULL,
+					&arg->memparcel_hdl);
+	trace_lookup_sgl(gh_sgl, ret, arg->memparcel_hdl);
+	if (ret) {
+		*has_lookup_sgl = false;
+		pr_debug("%s: Invoking Gunyah Lend/Share\n", __func__);
+		if (is_lend)
+			ret = gh_rm_mem_lend(GH_RM_MEM_TYPE_NORMAL, arg->flags,
+					     arg->label, gh_acl, gh_sgl,
+					     NULL /* Default memory attributes */,
+					     &arg->memparcel_hdl);
+		else
+			ret = gh_rm_mem_share(GH_RM_MEM_TYPE_NORMAL, arg->flags,
+					     arg->label, gh_acl, gh_sgl,
+					     NULL /* Default memory attributes */,
+					     &arg->memparcel_hdl);
+	}
+	if (ret < 0) {
+		pr_err("%s: Gunyah lend/share failed rc:%d\n",
+		       __func__, ret);
+		goto err_gunyah;
+	}
+
+	kfree(gh_acl);
+	kvfree(gh_sgl);
+	return 0;
+
+err_gunyah:
+	ret = hyp_assign_table(sgt, arg->vmids, arg->nr_acl_entries,
+			       src_vmid, src_perm, ARRAY_SIZE(src_vmid));
+	if (ret)
+		ret = -EADDRNOTAVAIL;
+
+err_hyp_assign:
+	kfree(gh_acl);
+err_gh_acl:
+	kvfree(gh_sgl);
+
+	return ret;
+}
+
+int mem_buf_assign_mem(bool is_lend, struct sg_table *sgt,
+			struct mem_buf_lend_kernel_arg *arg,
+			bool *has_lookup_sgl)
+{
+	u32 src_vmid = current_vmid;
+	int ret;
+	int api;
+
+	if (!sgt || !arg->nr_acl_entries || !arg->vmids || !arg->perms)
+		return -EINVAL;
+
+	api = mem_buf_vm_get_backend_api(arg->vmids, arg->nr_acl_entries);
+	if (api < 0)
+		return -EINVAL;
+
+	if (api == MEM_BUF_API_GUNYAH)
+		return mem_buf_assign_mem_gunyah(is_lend, sgt, arg, has_lookup_sgl);
+
+	pr_debug("%s: Assigning memory to target VMIDs\n", __func__);
+	ret = hyp_assign_table(sgt, &src_vmid, 1, arg->vmids, arg->perms,
+			       arg->nr_acl_entries);
+	if (ret < 0)
+		pr_err("%s: failed to assign memory for rmt allocation rc:%d\n",
+		       __func__, ret);
+	else
+		pr_debug("%s: Memory assigned to target VMIDs\n", __func__);
+
+	return ret;
+}
+EXPORT_SYMBOL(mem_buf_assign_mem);
+
+int mem_buf_unassign_mem(struct sg_table *sgt, int *src_vmids,
+			 unsigned int nr_acl_entries,
+			 gh_memparcel_handle_t memparcel_hdl,
+			 bool has_lookup_sgl)
+{
+	int dst_vmid[] = {current_vmid};
+	int dst_perm[] = {PERM_READ | PERM_WRITE | PERM_EXEC};
+	int ret, api;
+
+	if (!sgt || !src_vmids || !nr_acl_entries)
+		return -EINVAL;
+
+	api = mem_buf_vm_get_backend_api(src_vmids, nr_acl_entries);
+	if (api < 0)
+		return -EINVAL;
+
+	if (api == MEM_BUF_API_GUNYAH && !has_lookup_sgl) {
+		pr_debug("%s: Beginning gunyah reclaim\n", __func__);
+		ret = gh_rm_mem_reclaim(memparcel_hdl, 0);
+		if (ret) {
+			pr_err("%s: Gunyah reclaim failed\n", __func__);
+			return ret;
+		}
+		pr_debug("%s: Finished gunyah reclaim\n", __func__);
+	}
+
+	pr_debug("%s: Unassigning memory to HLOS\n", __func__);
+	ret = hyp_assign_table(sgt, src_vmids, nr_acl_entries,
+			       dst_vmid, dst_perm, ARRAY_SIZE(dst_vmid));
+	if (ret < 0)
+		pr_err("%s: failed to assign memory from rmt allocation rc: %d\n",
+		       __func__, ret);
+	else
+		pr_debug("%s: Unassigned memory to HLOS\n", __func__);
+
+	return ret;
+}
+EXPORT_SYMBOL(mem_buf_unassign_mem);
+
+static int mem_buf_get_mem_xfer_type(struct gh_acl_desc *acl_desc)
+{
+	u32 i, nr_acl_entries = acl_desc->n_acl_entries;
+
+	for (i = 0; i < nr_acl_entries; i++)
+		if (acl_desc->acl_entries[i].vmid == VMID_HLOS &&
+		    acl_desc->acl_entries[i].perms != 0)
+			return GH_RM_TRANS_TYPE_SHARE;
+
+	return GH_RM_TRANS_TYPE_LEND;
+}
+
+/*
+ * FIXME: gh_rm_mem_accept uses kmemdup, which isn't right for large buffers.
+ */
+struct gh_sgl_desc *mem_buf_map_mem_s2(gh_memparcel_handle_t memparcel_hdl,
+					struct gh_acl_desc *acl_desc)
+{
+	struct gh_sgl_desc *sgl_desc;
+	u8 flags = GH_RM_MEM_ACCEPT_VALIDATE_ACL_ATTRS |
+		   GH_RM_MEM_ACCEPT_MAP_IPA_CONTIGUOUS |
+		   GH_RM_MEM_ACCEPT_DONE;
+
+	if (!acl_desc)
+		return ERR_PTR(-EINVAL);
+
+	pr_debug("%s: adding CPU MMU stage 2 mappings\n", __func__);
+	sgl_desc = gh_rm_mem_accept(memparcel_hdl, GH_RM_MEM_TYPE_NORMAL,
+				    mem_buf_get_mem_xfer_type(acl_desc),
+				    flags, 0, acl_desc, NULL,
+				    NULL, 0);
+	if (IS_ERR(sgl_desc)) {
+		pr_err("%s failed to map memory in stage 2 rc: %d\n", __func__,
+		       PTR_ERR(sgl_desc));
+		return sgl_desc;
+	}
+
+	trace_map_mem_s2(memparcel_hdl, sgl_desc);
+	return sgl_desc;
+}
+EXPORT_SYMBOL(mem_buf_map_mem_s2);
+
+int mem_buf_unmap_mem_s2(gh_memparcel_handle_t memparcel_hdl)
+{
+	int ret;
+
+	pr_debug("%s: removing CPU MMU stage 2 mappings\n", __func__);
+	ret = gh_rm_mem_release(memparcel_hdl, 0);
+
+	if (ret < 0)
+		pr_err("%s: Failed to release memparcel hdl: 0x%lx rc: %d\n",
+		       __func__, memparcel_hdl, ret);
+	else
+		pr_debug("%s: CPU MMU stage 2 mappings removed\n", __func__);
+
+	return ret;
+}
+EXPORT_SYMBOL(mem_buf_unmap_mem_s2);
+
+int mem_buf_map_mem_s1(struct gh_sgl_desc *sgl_desc)
+{
+	u64 base, size;
+	int i, ret;
+
+	for (i = 0; i < sgl_desc->n_sgl_entries; i++) {
+		base = sgl_desc->sgl_entries[i].ipa_base;
+		size = sgl_desc->sgl_entries[i].size;
+
+		ret = add_memory_subsection(numa_node_id(), base, size);
+		if (ret) {
+			pr_err("%s: failed to add memory base=%llx, size=%llx, ret=%d\n",
+				__func__, base, size, ret);
+			goto out;
+		}
+	}
+
+	return 0;
+
+out:
+	for (i--; i >= 0; i--) {
+		base = sgl_desc->sgl_entries[i].ipa_base;
+		size = sgl_desc->sgl_entries[i].size;
+		remove_memory_subsection(numa_node_id(), base, size);
+	}
+
+	return ret;
+}
+EXPORT_SYMBOL(mem_buf_map_mem_s1);
+
+int mem_buf_unmap_mem_s1(struct gh_sgl_desc *sgl_desc)
+{
+	u64 base, size;
+	int i, ret = 0;
+
+	for (i = 0; i < sgl_desc->n_sgl_entries; i++) {
+		base = sgl_desc->sgl_entries[i].ipa_base;
+		size = sgl_desc->sgl_entries[i].size;
+
+		ret = remove_memory_subsection(numa_node_id(), base, size);
+		if (ret)
+			pr_err("%s: failed to remove memory base=%llx, size=%llx\n, ret=%d\n",
+				__func__, base, size, ret);
+	}
+
+	return ret;
+}
+EXPORT_SYMBOL(mem_buf_unmap_mem_s1);
+
+static int mem_buf_probe(struct platform_device *pdev)
+{
+	int ret;
+	struct device *dev = &pdev->dev;
+	u64 dma_mask = IS_ENABLED(CONFIG_ARM64) ? DMA_BIT_MASK(64) :
+		DMA_BIT_MASK(32);
+
+	if (of_property_match_string(dev->of_node, "qcom,mem-buf-capabilities",
+				     "supplier") >= 0) {
+		mem_buf_capability = MEM_BUF_CAP_SUPPLIER;
+	} else if (of_property_match_string(dev->of_node,
+					    "qcom,mem-buf-capabilities",
+					    "consumer") >= 0) {
+		mem_buf_capability = MEM_BUF_CAP_CONSUMER;
+	} else if (of_property_match_string(dev->of_node,
+					    "qcom,mem-buf-capabilities",
+					    "dual") >= 0) {
+		mem_buf_capability = MEM_BUF_CAP_DUAL;
+	} else {
+		dev_err(dev, "Transfer direction property not present or not valid\n");
+		return -EINVAL;
+	}
+
+	ret = dma_set_mask_and_coherent(dev, dma_mask);
+	if (ret) {
+		dev_err(dev, "Unable to set dma mask: %d\n", ret);
+		return ret;
+	}
+
+	ret = mem_buf_vm_init(dev);
+	if (ret) {
+		dev_err(dev, "mem_buf_vm_init failed %d\n", ret);
+		return ret;
+	}
+
+	mem_buf_dev = dev;
+	return 0;
+}
+
+static int mem_buf_remove(struct platform_device *pdev)
+{
+	mem_buf_dev = NULL;
+	return 0;
+}
+
+static const struct of_device_id mem_buf_match_tbl[] = {
+	 {.compatible = "qcom,mem-buf"},
+	 {},
+};
+
+static struct platform_driver mem_buf_driver = {
+	.probe = mem_buf_probe,
+	.remove = mem_buf_remove,
+	.driver = {
+		.name = "mem-buf",
+		.of_match_table = of_match_ptr(mem_buf_match_tbl),
+	},
+};
+
+static int __init mem_buf_dev_init(void)
+{
+	return platform_driver_register(&mem_buf_driver);
+}
+module_init(mem_buf_dev_init);
+
+static void __exit mem_buf_dev_exit(void)
+{
+	mem_buf_vm_exit();
+	platform_driver_unregister(&mem_buf_driver);
+}
+module_exit(mem_buf_dev_exit);
+
+MODULE_DESCRIPTION("Qualcomm Technologies, Inc. Memory Buffer Sharing driver");
+MODULE_LICENSE("GPL v2");
---
+++ include/linux/mem-buf.h
@@ -0,0 +1,213 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ *  Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef _MEM_BUF_H
+#define _MEM_BUF_H
+
+#include <linux/err.h>
+#include <linux/errno.h>
+#include <linux/gunyah/gh_rm_drv.h>
+#include <linux/types.h>
+#include <linux/dma-buf.h>
+#include <uapi/linux/mem-buf.h>
+
+/**
+ * Private definitions; they are just here since the tracepoint logic needs
+ * these definitions. Drivers should not use these.
+ */
+
+/**
+ * enum mem_buf_msg_type: Message types used by the membuf driver for
+ * communication.
+ * @MEM_BUF_ALLOC_REQ: The message is an allocation request from another VM to
+ * the receiving VM
+ * @MEM_BUF_ALLOC_RESP: The message is a response from a remote VM to an
+ * allocation request issued by the receiving VM
+ * @MEM_BUF_ALLOC_RELINQUISH: The message is a notification from another VM
+ * that the receiving VM can reclaim the memory.
+ */
+enum mem_buf_msg_type {
+	MEM_BUF_ALLOC_REQ,
+	MEM_BUF_ALLOC_RESP,
+	MEM_BUF_ALLOC_RELINQUISH,
+	MEM_BUF_ALLOC_REQ_MAX,
+};
+
+/**
+ * struct mem_buf_msg_hdr: The header for all membuf messages
+ * @txn_id: The transaction ID for the message. This field is only meaningful
+ * for request/response type of messages.
+ * @msg_type: The type of message.
+ */
+struct mem_buf_msg_hdr {
+	u32 txn_id;
+	u32 msg_type;
+} __packed;
+
+/**
+ * struct mem_buf_alloc_req: The message format for a memory allocation request
+ * to another VM.
+ * @hdr: Message header
+ * @size: The size of the memory allocation to be performed on the remote VM.
+ * @src_mem_type: The type of memory that the remote VM should allocate.
+ * @acl_desc: A GH ACL descriptor that describes the VMIDs that will be
+ * accessing the memory, as well as what permissions each VMID will have.
+ *
+ * NOTE: Certain memory types require additional information for the remote VM
+ * to interpret. That information should be concatenated with this structure
+ * prior to sending the allocation request to the remote VM. For example,
+ * with memory type ION, the allocation request message will consist of this
+ * structure, as well as the mem_buf_ion_alloc_data structure.
+ */
+struct mem_buf_alloc_req {
+	struct mem_buf_msg_hdr hdr;
+	u64 size;
+	u32 src_mem_type;
+	struct gh_acl_desc acl_desc;
+} __packed;
+
+/**
+ * struct mem_buf_ion_alloc_data: Represents the data needed to perform
+ * an ION allocation on a remote VM.
+ * @heap_id: The ID of the heap to allocate from
+ */
+struct mem_buf_ion_alloc_data {
+	u32 heap_id;
+} __packed;
+
+/**
+ * struct mem_buf_alloc_resp: The message format for a memory allocation
+ * request response.
+ * @hdr: Message header
+ * @ret: Return code from remote VM
+ * @hdl: The memparcel handle associated with the memory allocated to the
+ * receiving VM. This field is only meaningful if the allocation on the remote
+ * VM was carried out successfully, as denoted by @ret.
+ */
+struct mem_buf_alloc_resp {
+	struct mem_buf_msg_hdr hdr;
+	s32 ret;
+	u32 hdl;
+} __packed;
+
+/**
+ * struct mem_buf_alloc_relinquish: The message format for a notification
+ * that the current VM has relinquished access to the memory lent to it by
+ * another VM.
+ * @hdr: Message header
+ * @hdl: The memparcel handle associated with the memory.
+ */
+struct mem_buf_alloc_relinquish {
+	struct mem_buf_msg_hdr hdr;
+	u32 hdl;
+} __packed;
+
+/* Public definitions */
+
+
+/* Used to obtain the underlying vmperm struct of a DMA-BUF */
+struct mem_buf_vmperm *to_mem_buf_vmperm(struct dma_buf *dmabuf);
+
+/* Returns true if the local VM has exclusive access and is the owner */
+bool mem_buf_dma_buf_exclusive_owner(struct dma_buf *dmabuf);
+
+/*
+ * Returns a copy of the Virtual Machine vmids & permissions of the dmabuf.
+ * The caller must kfree() when finished.
+ */
+int mem_buf_dma_buf_copy_vmperm(struct dma_buf *dmabuf, int **vmids, int **perms,
+		int *nr_acl_entries);
+
+typedef int (*mem_buf_dma_buf_destructor)(void *dtor_data);
+int mem_buf_dma_buf_set_destructor(struct dma_buf *dmabuf,
+				   mem_buf_dma_buf_destructor dtor,
+				   void *dtor_data);
+
+/**
+ * struct mem_buf_allocation_data - Data structure that contains information
+ * about a memory buffer allocation request.
+ * @size: The size (in bytes) of the memory to be requested from a remote VM
+ * @nr_acl_entries: The number of ACL entries in @acl_list
+ * @acl_list: A list of VMID and permission pairs that describe what VMIDs will
+ * have access to the memory, and with what permissions
+ * @src_mem_type: The type of memory that the remote VM should allocate
+ * (e.g. ION memory)
+ * @src_data: A pointer to memory type specific data that the remote VM may need
+ * when performing an allocation (e.g. ION memory allocations require a heap ID)
+ * @dst_mem_type: The type of memory that the native VM wants (e.g. ION memory)
+ * @dst_data: A pointer to memory type specific data that the native VM may
+ * need when adding the memory from the remote VM (e.g. ION memory requires a
+ * heap ID to add the memory to).
+ */
+struct mem_buf_allocation_data {
+	size_t size;
+	unsigned int nr_acl_entries;
+	int *vmids;
+	int *perms;
+	enum mem_buf_mem_type src_mem_type;
+	void *src_data;
+	enum mem_buf_mem_type dst_mem_type;
+	void *dst_data;
+};
+
+struct mem_buf_lend_kernel_arg {
+	unsigned int nr_acl_entries;
+	int *vmids;
+	int *perms;
+	gh_memparcel_handle_t memparcel_hdl;
+	u32 flags;
+	u64 label;
+};
+
+int mem_buf_lend(struct dma_buf *dmabuf,
+		struct mem_buf_lend_kernel_arg *arg);
+
+/*
+ * mem_buf_share
+ * Grant the local VM, as well as one or more remote VMs access
+ * to the dmabuf. The permissions of the local VM default to RWX
+ * unless otherwise specified.
+ */
+int mem_buf_share(struct dma_buf *dmabuf,
+		struct mem_buf_lend_kernel_arg *arg);
+
+
+struct mem_buf_retrieve_kernel_arg {
+	u32 sender_vmid;
+	unsigned int nr_acl_entries;
+	int *vmids;
+	int *perms;
+	gh_memparcel_handle_t memparcel_hdl;
+	int fd_flags;
+};
+struct dma_buf *mem_buf_retrieve(struct mem_buf_retrieve_kernel_arg *arg);
+int mem_buf_reclaim(struct dma_buf *dmabuf);
+
+#if IS_ENABLED(CONFIG_QCOM_MEM_BUF)
+
+int mem_buf_get_fd(void *membuf_desc);
+
+void mem_buf_put(void *membuf_desc);
+
+void *mem_buf_get(int fd);
+
+#else
+
+static inline int mem_buf_get_fd(void *membuf_desc)
+{
+	return -ENODEV;
+}
+
+static inline void mem_buf_put(void *membuf_desc)
+{
+}
+
+static inline void *mem_buf_get(int fd)
+{
+	return ERR_PTR(-ENODEV);
+}
+
+#endif /* CONFIG_QCOM_MEM_BUF */
+#endif /* _MEM_BUF_H */
