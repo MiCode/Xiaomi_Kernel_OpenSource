7c39ec31270b7e62acde94d06445bf1850c6ef5d
mtk23153 <Cheng-yi.Cheng@mediatek.com>
2021-06-19 01:49:18 +0800

    [ALPS05738917] ASoC :Fix apll setting
    Turn on enable bit after the rest of the bit field is set
    
    MTK-Commit-Id: 42f10e0ef47853dee01f99a2c7fe7e7556b42cec
    
    Change-Id: I2ff9f8c3bf2467eef90c259c840a25bf0b00be3e
    Signed-off-by: Patrick Cheng <cheng-yi.cheng@mediatek.com>
    CR-Id: ALPS05738917
    Feature: [Module]Audio Kernel

--- sound/soc/mediatek/mt6781/mt6781-afe-clk.c
+++ sound/soc/mediatek/mt6781/mt6781-afe-clk.c
@@ -507,7 +507,7 @@ int mt6781_apll1_enable(struct mtk_base_afe *afe)
 	}
 
 	regmap_update_bits(afe->regmap, AFE_APLL1_TUNER_CFG,
-			   0x0000FFF7, 0x00000373);
+			   0x0000FFF7, 0x00000372);
 	regmap_update_bits(afe->regmap, AFE_APLL1_TUNER_CFG, 0x1, 0x1);
 
 	regmap_update_bits(afe->regmap, AFE_HD_ENGEN_ENABLE,
@@ -563,7 +563,7 @@ int mt6781_apll2_enable(struct mtk_base_afe *afe)
 	}
 
 	regmap_update_bits(afe->regmap, AFE_APLL2_TUNER_CFG,
-			   0x0000FFF7, 0x00000375);
+			   0x0000FFF7, 0x00000374);
 	regmap_update_bits(afe->regmap, AFE_APLL2_TUNER_CFG, 0x1, 0x1);
 
 	regmap_update_bits(afe->regmap, AFE_HD_ENGEN_ENABLE,
