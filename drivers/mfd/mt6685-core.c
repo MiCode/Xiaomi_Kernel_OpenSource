4184b469f7c712096d6f8c2f6d105f195759324a
Hank Liao <Hank.Liao@mediatek.com>
2021-06-24 16:24:18 +0800

    [ALPS05724291] MT6685: MFD Porting
    1. Add the driver source code for MT6685
    
    2. Add the register content for MT6685
    
    MTK-Commit-Id: 6da1430d81697a029781c2f3cdba29c2f89cd903
    
    Change-Id: I34aca55fc7c9e7d7bd7c8f2bae91ea2a9059c5ee
    Signed-off-by: Hank Liao <Hank.Liao@mediatek.com>
    CR-Id: ALPS05724291
    Feature: [Module]Clock Buffer

---
+++ drivers/mfd/mt6685-core.c
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2021 MediaTek Inc.
+ *
+ * Author: Hank Liao <hank.liao@mediatek.com>
+ */
+
+#include <linux/kernel.h>
+#include <linux/mfd/core.h>
+#include <linux/module.h>
+#include <linux/of_platform.h>
+#include <linux/pmif.h>
+#include <linux/regmap.h>
+#include <linux/spmi.h>
+#include <linux/mfd/mt6685/registers.h>
+
+#define MT6685_DCXO_BASE 0x780
+#define MT6685_DCXO_SIZE 0x75
+
+static const struct resource mt6685_dcxo_resources[] = {
+	DEFINE_RES_MEM(MT6685_DCXO_BASE, MT6685_DCXO_SIZE),
+};
+
+static const struct mfd_cell mt6685_devs[] = {
+	{
+		.name = "mt6685-rtc",
+		.of_compatible = "mediatek,mt6685-rtc",
+	},{
+		.name = "mt6685-clk",
+		.num_resources = ARRAY_SIZE(mt6685_dcxo_resources),
+		.resources = mt6685_dcxo_resources,
+		.of_compatible = "mediatek,mt6685-clk",
+	}
+};
+
+static const struct regmap_config spmi_regmap_config = {
+	.reg_bits	= 16,
+	.val_bits	= 8,
+	.max_register	= 0x2000,
+	.fast_io	= true,
+};
+
+static int mt6685_spmi_probe(struct spmi_device *sdev)
+{
+	int ret;
+	struct regmap *regmap;
+
+	regmap = devm_regmap_init_spmi_ext(sdev, &spmi_regmap_config);
+	if (IS_ERR(regmap))
+		return PTR_ERR(regmap);
+
+	ret = devm_mfd_add_devices(&sdev->dev, -1, mt6685_devs,
+				   ARRAY_SIZE(mt6685_devs), NULL, 0, NULL);
+	if (ret) {
+		pr_info("Failed to add child devices: %d\n", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+static const struct of_device_id mt6685_id_table[] = {
+	{ .compatible = "mediatek,mt6685", },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, mt6685_id_table);
+
+static struct spmi_driver mt6685_spmi_driver = {
+	.probe = mt6685_spmi_probe,
+	.driver = {
+		.name = "mt6685",
+		.of_match_table = mt6685_id_table,
+	},
+};
+module_spmi_driver(mt6685_spmi_driver);
+
+MODULE_DESCRIPTION("Mediatek SPMI MT6685 Clock IC driver");
+MODULE_AUTHOR("Hank Liao <hank.liao@mediatek.com>");
+MODULE_LICENSE("GPL v2");
