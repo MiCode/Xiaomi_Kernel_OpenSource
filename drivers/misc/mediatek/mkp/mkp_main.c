e26c2451a4fbd83fa53078c50096b025a7ff2371
Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
2021-06-24 16:49:33 +0800

    [ALPS05678032] mkp: Add mkp driver
    Add mkp driver
    
    MTK-Commit-Id: 72e50992b56131c55d82808989047e9ca6be19cc
    
    Change-Id: I6f4046d98dcadd2939173d8160b08e8b692c9520
    Signed-off-by: Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
    CR-Id: ALPS05678032
    Feature: Mediatek Kernel Protection (MKP)

---
+++ drivers/misc/mediatek/mkp/mkp_main.c
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2021 MediaTek Inc.
+ */
+
+#undef pr_fmt
+#define pr_fmt(fmt) "MKP: " fmt
+
+#include <linux/types.h> // for list_head
+#include <linux/module.h> // module_layout
+#include <linux/init.h> // rodata_enable support
+#include <linux/mutex.h>
+#include <linux/kernel.h> // round_up
+
+#include "mkp.h"
+
+static int __init mkp_init(void)
+{
+	int ret = 0;
+
+	pr_info("%s:%d start\n", __func__, __LINE__);
+
+	if (ret)
+		pr_info("%s: failed, ret: %d\n", __func__, ret);
+
+	pr_info("%s:%d done\n", __func__, __LINE__);
+	return ret;
+}
+module_init(mkp_init);
+
+static void  __exit mkp_exit(void)
+{
+	/*
+	 * vendor hook cannot unregister, please check vendor_hook.h
+	 */
+	pr_info("%s:%d\n", __func__, __LINE__);
+}
+module_exit(mkp_exit);
+
+MODULE_AUTHOR("<Kuan-Ying.Lee@mediatek.com>");
+MODULE_DESCRIPTION("MediaTek MKP Driver");
+MODULE_LICENSE("GPL");
