dce04a002443096eede98f1b61ed2bf999f56d99
Claude Yen <claude.yen@mediatek.com>
2021-06-24 16:39:23 +0800

    [ALPS05711078] lpm: add check for enable  kernel suspend
    add check for enable  kernel suspend
    
    MTK-Commit-Id: 743021ef77967d068ac99d34b926b3daa91b237e
    
    Change-Id: Icd0ac462243a19b53b6776ae1af7d42f329fc591
    Signed-off-by: Claude Yen <claude.yen@mediatek.com>
    CR-Id: ALPS05711078
    Feature: Suspend

--- drivers/misc/mediatek/lpm/modules/debug/v1/lpm_dbg_pm.c
+++ drivers/misc/mediatek/lpm/modules/debug/v1/lpm_dbg_pm.c
@@ -7,23 +7,34 @@
 #include <linux/fs.h>
 #include <linux/module.h>
 #include <linux/of_device.h>
+#include <linux/of.h>
 #include <linux/platform_device.h>
 #include <linux/suspend.h>
+#include <linux/delay.h>
 
+#include <lpm.h>
 #include <lpm_dbg_common_v1.h>
+
 
 static int lpm_dbg_probe(struct platform_device *pdev)
 {
 	return 0;
 }
 
-
+#define LPM_KERNEL_SUSPEND "lpm-kernel-suspend"
+static int kernel_suspend_only;
 static int lpm_dbg_suspend_noirq(struct device *dev)
 {
 	int ret = 0;
 
 	if (pm_suspend_default_s2idle())
 		ret = spm_common_dbg_dump();
+
+	if (kernel_suspend_only == 1) {
+		pr_info("[LPM] kernel suspend only ....\n");
+		mdelay(3*1000);
+		pm_system_wakeup();
+	}
 
 	return ret;
 }
@@ -48,14 +59,36 @@ static struct platform_driver lpm_dbg_driver = {
 	}
 };
 
+
 int lpm_dbg_pm_init(void)
 {
 	int ret = 0;
+
+	unsigned int val = 0;
+	struct device_node *lpm_node;
 
 	ret = platform_driver_register(&lpm_dbg_driver);
 
 	if (ret)
 		return -1;
+
+	lpm_node = of_find_compatible_node(NULL, NULL, MTK_LPM_DTS_COMPATIBLE);
+
+	if (lpm_node) {
+
+		of_property_read_u32(lpm_node, LPM_KERNEL_SUSPEND, &val);
+
+		if (val == 1) {
+			kernel_suspend_only = 1;
+		} else {
+			kernel_suspend_only = 0;
+		}
+
+		of_node_put(lpm_node);
+	}
+
+	pr_info("[name:mtk_lpm][P] - kernel suspend only = %d (%s:%d)\n",
+					kernel_suspend_only, __func__, __LINE__);
 
 	return 0;
 }
