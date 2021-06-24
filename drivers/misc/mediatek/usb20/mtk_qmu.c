783a2e7e46d7e4ddc14c8747c39e298fee88ee8b
mtk21421 <Stan.Lu@mediatek.com>
2021-06-24 16:33:29 +0800

    [ALPS05836250] MT6781: fix access NULL pointer
    Check req before do QMU done interrupt to avoid NULL pointer access.
    
    MTK-Commit-Id: 3d460a43d757fcc587f01c88fc81803ab423367f
    
    Change-Id: I9afb0ae4282ca8f8da27bc8b81a6eb6435896d1a
    Signed-off-by: mtk21421 <Stan.Lu@mediatek.com>
    CR-Id: ALPS05836250
    Feature: USB 3.0 Device
    (cherry picked from commit c1063839651d5ea9dab7372c77e117090c6aca76)

--- drivers/misc/mediatek/usb20/mtk_qmu.c
+++ drivers/misc/mediatek/usb20/mtk_qmu.c
@@ -1032,6 +1032,14 @@ void qmu_done_rx(struct musb *musb, u8 ep_num)
 		Rx_gpd_free_count[ep_num]++;
 		musb_g_giveback(musb_ep, request, 0);
 		req = next_request(musb_ep);
+
+		if (!req) {
+			QMU_ERR(
+				"[RXD]%s Cannot get next request of %d, but QMU has done.\n"
+				, __func__, ep_num);
+			return;
+		}
+
 		request = &req->request;
 	}
 
