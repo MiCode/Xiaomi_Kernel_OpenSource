cda50c19ec50313702e736a21f2d56838d88229f
Wesley Cheng <wcheng@codeaurora.org>
2021-06-28 11:51:34 -0700

    usb: dwc3: dwc3-msm-core: Set PHY_DP_MODE after enabling HS only
    There is a race condition observed where the DWC3 core RPM resume
    occurs in between the completion of dwc3_start_stop_host(), and
    setting of the DWC3 maximum speed.  Currently, we are setting
    PHY_DP_MODE before the stop/start mode sequence is run.  This avoids
    the QMP PHY init and suspend APIs from executing.  However, for
    example, during the stop host routine, the QMP PHY is powered down.
    If a RPM resume occurs on the DWC3 core device, the runtime resume
    routine will call dwc3_core_init() w/ the PHY_DP_MODE flag set.  This
    avoids initializing the QMP PHY, and the maximum speed is still set to
    SSUSB, which sets the internal clk mux to use the PIPE clk.
    Inherently that will lead to the core soft reset timeout, and mark the
    RPM runtime_error.
    
    Fix the above, by setting the PHY_DP_MODE flag only after the
    maximum_speed is set to HS only.  Discussion w/ DP team mentioned that
    the DP altmode driver will not configure the DP block until
    dwc3_msm_set_dp_mode() returns.  Thus, we can still allow the QMP PHY
    init to occur at this time.  It is observed that at the time of the
    error, the DWC3 MSM device was still active, and the runtime resume
    happened solely on the DWC3 core device.
    
    Change-Id: I2cb27492c19a86a189a793ba33d8750b637a29ae
    Signed-off-by: Wesley Cheng <wcheng@codeaurora.org>

--- drivers/usb/dwc3/dwc3-msm-core.c
+++ drivers/usb/dwc3/dwc3-msm-core.c
@@ -4515,7 +4515,6 @@ int dwc3_msm_set_dp_mode(struct device *dev, bool dp_connected, int lanes)
 	redriver_release_usb_lanes(mdwc->ss_redriver_node);
 
 	mdwc->ss_release_called = true;
-	mdwc->ss_phy->flags |= PHY_DP_MODE;
 	if (mdwc->id_state == DWC3_ID_GROUND) {
 		/* stop USB host mode */
 		ret = dwc3_start_stop_host(mdwc, false);
@@ -4524,6 +4523,7 @@ int dwc3_msm_set_dp_mode(struct device *dev, bool dp_connected, int lanes)
 
 		/* restart USB host mode into high speed */
 		dwc3_msm_set_max_speed(mdwc, USB_SPEED_HIGH);
+		mdwc->ss_phy->flags |= PHY_DP_MODE;
 		dwc3_start_stop_host(mdwc, true);
 	} else if (mdwc->vbus_active) {
 		/* stop USB device mode */
@@ -4533,10 +4533,12 @@ int dwc3_msm_set_dp_mode(struct device *dev, bool dp_connected, int lanes)
 
 		/* restart USB device mode into high speed */
 		dwc3_msm_set_max_speed(mdwc, USB_SPEED_HIGH);
+		mdwc->ss_phy->flags |= PHY_DP_MODE;
 		dwc3_start_stop_device(mdwc, true);
 	} else {
 		dbg_log_string("USB is not active.\n");
 		dwc3_msm_set_max_speed(mdwc, USB_SPEED_HIGH);
+		mdwc->ss_phy->flags |= PHY_DP_MODE;
 	}
 
 	return 0;
