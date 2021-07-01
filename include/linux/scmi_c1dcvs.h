adfe446b1c1698cd7b981ffbbafc82a4e32cba21
Rishabh Bhatnagar <rishabhb@codeaurora.org>
2021-06-30 17:55:06 -0700

    drivers: dcvs: c1dcvs: Add c1dcvs protocol and client driver
    Add c1dcvs protocol that provides enable/disable control over
    c1dcvs algorithm in cpucp. It also provides capability
    to send tunables. Add the client driver as well to expose
    sysfs entries to userspace.
    
    Change-Id: I7a6921d9f85676340bcc4aa2b6f159674091b142
    Signed-off-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>

---
+++ include/linux/scmi_c1dcvs.h
@@ -0,0 +1,39 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * scmi c1dcvs protocols header
+ *
+ * copyright (c) 2021, the linux foundation. all rights reserved.
+ */
+
+#ifndef _scmi_c1dcvs_h
+#define _scmi_c1dcvs_h
+
+#include <linux/bitfield.h>
+#include <linux/device.h>
+#include <linux/types.h>
+
+
+#define SCMI_C1DCVS_PROTOCOL    0x87
+
+struct scmi_protocol_handle;
+
+/**
+ * struct scmi_c1dcvs_vendor_ops - represents the various operations provided
+ *      by scmi c1dcvs protocol
+ *
+ * @scmi_enable_c1dcvs: enables/disables c1dcvs algorithm on cpucp.
+ */
+struct scmi_c1dcvs_vendor_ops {
+	int (*set_enable_c1dcvs)(const struct scmi_protocol_handle *ph, void *buf);
+	int (*get_enable_c1dcvs)(const struct scmi_protocol_handle *ph, void *buf);
+	int (*set_enable_trace)(const struct scmi_protocol_handle *ph, void *buf);
+	int (*get_enable_trace)(const struct scmi_protocol_handle *ph, void *buf);
+	int (*set_ipc_thresh)(const struct scmi_protocol_handle *ph, void *buf);
+	int (*get_ipc_thresh)(const struct scmi_protocol_handle *ph, void *buf);
+	int (*set_efreq_thresh)(const struct scmi_protocol_handle *ph, void *buf);
+	int (*get_efreq_thresh)(const struct scmi_protocol_handle *ph, void *buf);
+	int (*set_hysteresis)(const struct scmi_protocol_handle *ph, void *buf);
+	int (*get_hysteresis)(const struct scmi_protocol_handle *ph, void *buf);
+};
+#endif
+
