e7a467b6f105f4efe08f1220eec9488823a5375f
Anvoi Liu <anvoi.liu@mediatek.com>
2021-07-01 11:18:40 +0800

    [ALPS05820840] cam: change camera sensor type
    [Detail]
    6781 platform limitation.
    Change IMX586 sensor type to avoid third cam-AP to call remosaic mode.
    
    MTK-Commit-Id: 58591dd50ae11b7067848f32f3b740f27962c77f
    
    Change-Id: Ib2aca0210a2c78353ddfc08b94cd8ed78c5f853d
    CR-Id: ALPS05820840
    Feature:[Android Default] Camera Application Basic Functions
    Signed-off-by: Anvoi Liu <anvoi.liu@mediatek.com>

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/imx586_mipi_raw/imx586mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/imx586_mipi_raw/imx586mipiraw_Sensor.c
@@ -260,7 +260,11 @@ static struct imgsensor_info_struct imgsensor_info = {
 	.mipi_sensor_type = MIPI_CPHY, /* 0,MIPI_OPHY_NCSI2; 1,MIPI_OPHY_CSI2 */
 #endif
 	.mipi_settle_delay_mode = 0,
+#if defined(CONFIG_MACH_MT6781)
+	.sensor_output_dataformat = SENSOR_OUTPUT_FORMAT_RAW_R,
+#else
 	.sensor_output_dataformat = SENSOR_OUTPUT_FORMAT_RAW_4CELL_HW_BAYER_R,
+#endif
 	.mclk = 24, /* mclk value, suggest 24 or 26 for 24Mhz or 26Mhz */
 	/*.mipi_lane_num = SENSOR_MIPI_4_LANE,*/
 #if DPHY_2LANE
