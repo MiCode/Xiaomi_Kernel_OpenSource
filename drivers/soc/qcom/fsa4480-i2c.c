57bbdba4b201bf65068509b76d0c6bec196cfdcd
Elliot Berman <eberman@codeaurora.org>
2021-07-01 19:33:39 -0700

    fsa4480: Initialize notifier chain using correct macro
    Initialize blocking notifier chain using the corresponding init macro to
    avoid lock dependency warning:
    
    [ 16.811788] INFO: trying to register non-static key.
    [ 16.817614] The code is fine but needs lockdep annotation, or maybe
    [ 16.824775] you didn't initialize this object before use?
    [ 16.831055] turning off the locking correctness validator.
    [ 16.831059] CPU: 4 PID: 190 Comm: kworker/u16:10 Tainted: G S W O
    5.10.43-qki-consolidate-android12-7-gd8b574bd60d8 #1
    [ 16.831062] Hardware name: Qualcomm Technologies, Inc. Waipio MTP with
    PM8010 (DT)
    [ 16.831071] Workqueue: events_unbound deferred_probe_work_func
    [ 16.831077] Call trace:
    [ 16.831085] dump_backtrace.cfi_jt+0x0/0x8
    [ 16.837414] fsck.f2fs: \x09Info: No support kernel version!
    [ 16.843708] show_stack+0x1c/0x2c
    [ 16.843712] dump_stack_lvl+0xd8/0x16c
    [ 16.843717] register_lock_class+0x514/0x7a4
    [ 16.843720] __lock_acquire+0x6c/0x6c4
    [ 16.843723] lock_acquire+0xf8/0x1c8
    [ 16.843728] down_write+0x70/0x204
    [ 16.843732] blocking_notifier_chain_register+0x30/0xf0
    [ 16.856421] fsck.f2fs: Info: Segments per section = 1
    [ 16.864883] fsa4480_reg_notifier+0x38/0x8c [fsa4480_i2c]
    [ 16.865018] dp_display_probe+0x128/0x608 [msm_drm]
    [ 16.865026] platform_drv_probe+0xb4/0xf8.
    
    Use BLOCKING_INIT_NOTIFIER_HEAD to initialize notifier chain.
    
    Change-Id: Icec71cafc3090c2fe7c6e370e216decc2b95c1d9
    Signed-off-by: Elliot Berman <eberman@codeaurora.org>

--- drivers/soc/qcom/fsa4480-i2c.c
+++ drivers/soc/qcom/fsa4480-i2c.c
@@ -364,10 +364,7 @@ static int fsa4480_probe(struct i2c_client *i2c,
 	INIT_WORK(&fsa_priv->usbc_analog_work,
 		  fsa4480_usbc_analog_work_fn);
 
-	fsa_priv->fsa4480_notifier.rwsem =
-		(struct rw_semaphore)__RWSEM_INITIALIZER
-		((fsa_priv->fsa4480_notifier).rwsem);
-	fsa_priv->fsa4480_notifier.head = NULL;
+	BLOCKING_INIT_NOTIFIER_HEAD(&fsa_priv->fsa4480_notifier);
 
 	return 0;
 
