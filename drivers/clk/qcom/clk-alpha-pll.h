6db06e2a588f955167408f31884af3086b7045b3
Mike Tipton <mdtipton@codeaurora.org>
2021-07-06 13:44:18 -0700

    clk: qcom: clk-alpha-pll: Add zonda_5lpe snapshot from msm-5.10
    Add zonda_5lpe snapshot from msm-5.10 commit 57bbdba4b201 ("fsa4480:
    Initialize notifier chain using correct macro").
    
    Change-Id: Id88866ae7fd6ebbef2897e3864773bb2ece6fd33
    Signed-off-by: Mike Tipton <mdtipton@codeaurora.org>

--- drivers/clk/qcom/clk-alpha-pll.h
+++ drivers/clk/qcom/clk-alpha-pll.h
@@ -17,6 +17,7 @@ enum {
 	CLK_ALPHA_PLL_TYPE_LUCID = CLK_ALPHA_PLL_TYPE_TRION,
 	CLK_ALPHA_PLL_TYPE_AGERA,
 	CLK_ALPHA_PLL_TYPE_ZONDA,
+	CLK_ALPHA_PLL_TYPE_ZONDA_5LPE,
 	CLK_ALPHA_PLL_TYPE_MAX,
 };
 
@@ -157,6 +158,7 @@ extern const struct clk_ops clk_alpha_pll_postdiv_lucid_5lpe_ops;
 
 extern const struct clk_ops clk_alpha_pll_zonda_ops;
 extern const struct clk_ops clk_alpha_pll_postdiv_zonda_ops;
+extern const struct clk_ops clk_alpha_pll_zonda_5lpe_ops;
 
 void clk_alpha_pll_configure(struct clk_alpha_pll *pll, struct regmap *regmap,
 			     const struct alpha_pll_config *config);
@@ -169,6 +171,9 @@ void clk_agera_pll_configure(struct clk_alpha_pll *pll, struct regmap *regmap,
 #define clk_lucid_pll_configure(pll, regmap, config) \
 	clk_trion_pll_configure(pll, regmap, config)
 int clk_zonda_pll_configure(struct clk_alpha_pll *pll, struct regmap *regmap,
+				const struct alpha_pll_config *config);
+int clk_zonda_5lpe_pll_configure(struct clk_alpha_pll *pll,
+				struct regmap *regmap,
 				const struct alpha_pll_config *config);
 
 
