ea5fd8ed67a1aac5d0e895957f72556710432e55
zhiyong.wang <zhiyong.wang@mediatek.com>
2021-07-08 12:00:57 +0800

    [ALPS05887476] aee: no need to use dbgtop API
    No need to use dbgtop API to control ddr reserved mode, because all
    the work was done in lk stage.
    
    MTK-Commit-Id: 2b3f54ef6999936ce915e2e15d343e44e1b8ffec
    
    Change-Id: I49d3b76963b9b2bd8d8a90de4e895900df9ffccb
    Signed-off-by: zhiyong.wang <zhiyong.wang@mediatek.com>
    CR-Id: ALPS05887476
    Feature: Android Exception Engine(AEE)

--- drivers/misc/mediatek/aee/mrdump/mrdump_hw.c
+++ drivers/misc/mediatek/aee/mrdump/mrdump_hw.c
@@ -13,23 +13,8 @@
 #include <mtk_platform_debug.h>
 #endif
 
-#if IS_ENABLED(CONFIG_MTK_DBGTOP)
-#include <dbgtop.h>
-#endif
-
 int __init mrdump_hw_init(bool drm_ready)
 {
-#if IS_ENABLED(CONFIG_MTK_DBGTOP)
-	if (drm_ready) {
-		if (!mtk_dbgtop_dram_reserved(true))
-			pr_info("%s: DDR reserved mode enabled ok\n",
-				__func__);
-		else
-			pr_info("%s: mtk_dbgtop_dram_reserved failed\n",
-				__func__);
-	}
-#endif
-
 #if IS_ENABLED(CONFIG_MTK_DFD_INTERNAL_DUMP)
 	if (dfd_setup(DFD_BASIC_DUMP) == -1)
 		pr_notice("%s: DFD disabled\n", __func__);
