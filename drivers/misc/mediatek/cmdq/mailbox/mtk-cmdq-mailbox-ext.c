77fbf06e75cc6f0970f946c71c7984374f78d355
“jayce.hsu” <jayce.hsu@mediatek.com>
2021-07-08 11:53:09 +0800

    [ALPS05659211] cmdq: early porting for mt6879
    early porting for mt6879
    
    MTK-Commit-Id: e94244cdc9567f07b23c4148ebb168593f79a383
    
    Change-Id: I705a6b1d26a206642e9e8ed5b96c4fcc087217a1
    Signed-off-by: “jayce.hsu” <jayce.hsu@mediatek.com>
    CR-Id: ALPS05659211
    Feature: [Module]CMDQ Driver

--- drivers/misc/mediatek/cmdq/mailbox/mtk-cmdq-mailbox-ext.c
+++ drivers/misc/mediatek/cmdq/mailbox/mtk-cmdq-mailbox-ext.c
@@ -1949,6 +1949,7 @@ static const struct of_device_id cmdq_of_ids[] = {
 	{.compatible = "mediatek,mt6853-gce", .data = (void *)&gce_plat_v4},
 	{.compatible = "mediatek,mt6873-gce", .data = (void *)&gce_plat_v4},
 	{.compatible = "mediatek,mt6877-gce", .data = (void *)&gce_plat_v4},
+	{.compatible = "mediatek,mt6879-gce", .data = (void *)&gce_plat_v5},
 	{.compatible = "mediatek,mt6885-gce", .data = (void *)&gce_plat_v4},
 	{.compatible = "mediatek,mt6893-gce", .data = (void *)&gce_plat_v4},
 	{.compatible = "mediatek,mt6983-gce", .data = (void *)&gce_plat_v5},
