d3655d502b192f71d0407b51a80a62170bbfe925
mtk54497 <zhongchao.xia@mediatek.com>
2021-07-14 12:16:36 +0800

    [ALPS05869439] MDP:get sec id from ion api by fd
    [Detail]
    Get sec id from ion api by fd
    
    MTK-Commit-Id: 35bf4de5a25bc96426adf62904580b5fb4a9626e
    
    Change-Id: Ifa4c75eccd1e5bd6466d1156c96cbd711b30651a
    Signed-off-by: mtk54497 <zhongchao.xia@mediatek.com>
    CR-Id: ALPS05869439
    Feature: [Module]MDP Driver

--- drivers/misc/mediatek/mdp/mdp_def.h
+++ drivers/misc/mediatek/mdp/mdp_def.h
@@ -338,6 +338,9 @@ struct cmdqSecAddrMetadataStruct {
 	uint32_t offset;	/* [IN]_b, buffser offset to secure handle */
 	uint32_t size;		/* buffer size */
 	uint32_t port;		/* hw port id (i.e. M4U port id) */
+	uint32_t sec_id;	/* sec_id 0/1/3: secure camera/SVP/WFD */
+	uint32_t useSecIdinMeta;
+	int32_t ionFd;
 };
 
 struct cmdqMetaBuf {
