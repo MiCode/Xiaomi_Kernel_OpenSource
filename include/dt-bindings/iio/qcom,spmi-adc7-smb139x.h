eb9b1065a6053fefaf34bf8642a15f8b26e48b0a
Jeevan Shriram <jshriram@codeaurora.org>
2021-07-13 17:37:28 -0700

    dt-bindings: Add include DT binding headers
    Add include dt-bindings headers for compilation of clock, I/O,
    interconnect, UFS and IPCC drivers.
    
    Change-Id: I3e065d5122fe3e018471fbc74751e50e174793a4
    Signed-off-by: Jeevan Shriram <jshriram@codeaurora.org>

---
+++ include/dt-bindings/iio/qcom,spmi-adc7-smb139x.h
@@ -0,0 +1,41 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2020-2021 The Linux Foundation. All rights reserved.
+ */
+
+#ifndef _DT_BINDINGS_QCOM_SPMI_VADC_SMB139X_H
+#define _DT_BINDINGS_QCOM_SPMI_VADC_SMB139X_H
+
+#ifndef SMB139x_1_SID
+#define SMB139x_1_SID				0x0b
+#endif
+
+#ifndef SMB139x_2_SID
+#define SMB139x_2_SID				0x0c
+#endif
+
+#ifndef SMB1394_1_SID
+#define SMB1394_1_SID				0x09
+#endif
+
+#ifndef SMB1394_2_SID
+#define SMB1394_2_SID				0x0b
+#endif
+
+#define SMB139x_1_ADC7_SMB_TEMP			(SMB139x_1_SID << 8 | 0x06)
+#define SMB139x_1_ADC7_ICHG_SMB			(SMB139x_1_SID << 8 | 0x18)
+#define SMB139x_1_ADC7_IIN_SMB			(SMB139x_1_SID << 8 | 0x19)
+
+#define SMB139x_2_ADC7_SMB_TEMP			(SMB139x_2_SID << 8 | 0x06)
+#define SMB139x_2_ADC7_ICHG_SMB			(SMB139x_2_SID << 8 | 0x18)
+#define SMB139x_2_ADC7_IIN_SMB			(SMB139x_2_SID << 8 | 0x19)
+
+#define SMB1394_1_ADC7_SMB_TEMP			(SMB1394_1_SID << 8 | 0x06)
+#define SMB1394_1_ADC7_ICHG_SMB			(SMB1394_1_SID << 8 | 0x18)
+#define SMB1394_1_ADC7_IIN_SMB			(SMB1394_1_SID << 8 | 0x19)
+
+#define SMB1394_2_ADC7_SMB_TEMP			(SMB1394_2_SID << 8 | 0x06)
+#define SMB1394_2_ADC7_ICHG_SMB			(SMB1394_2_SID << 8 | 0x18)
+#define SMB1394_2_ADC7_IIN_SMB			(SMB1394_2_SID << 8 | 0x19)
+
+#endif
