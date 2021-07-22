e6ae657025653ef428a4b12233ca30309b6b8a68
Lumi Lee <lumi.lee@mediatek.com>
2021-07-23 02:34:55 +0800

    [ALPS05672361] Audio: add ble path
    [Detail]
    Need to sync audio task scene
    
    MTK-Commit-Id: 59daf6ca1abc6aaed47105cacf54cba36c946e04
    
    Change-Id: I0c6a40c1aeb20ccf7e51ce14f5e39063b4bd9992
    Signed-off-by: Lumi Lee <lumi.lee@mediatek.com>
    CR-Id: ALPS05672361
    Feature: [Module]Audio Kernel

--- drivers/misc/mediatek/audio_ipi/common/framework/audio_task.h
+++ drivers/misc/mediatek/audio_ipi/common/framework/audio_task.h
@@ -30,6 +30,10 @@ enum {
 	TASK_SCENE_CAPTURE_RAW          = 17,
 	TASK_SCENE_FM_ADSP              = 18,
 	TASK_SCENE_PHONE_CALL_SUB       = 19,
+	TASK_SCENE_BLEDL                = 20,
+	TASK_SCENE_BLEUL                = 21,
+	TASK_SCENE_BLEDEC               = 22,
+	TASK_SCENE_BLEENC               = 23,
 
 	/* control for driver */
 	TASK_SCENE_AUDIO_CONTROLLER_HIFI3_A,
