9bc76b83e923921a941e0c73b84355c15c181273
Yun-Chen Hsieh <yun-chen.hsieh@mediatek.com>
2021-07-23 02:59:36 +0800

    [ALPS05916224] swpm: mt6983: introduce power model platform
    -. introduce power model platform data
    -. update Kconfig and Makefile for mt6983
    
    MTK-Commit-Id: e1dee1bf346ac4b7c50a94915dc5570a102c0dac
    
    Change-Id: Ic1a01d252dc08ff221f88b7ceda0f87d33e9d82f
    Signed-off-by: Yun-Chen Hsieh <yun-chen.hsieh@mediatek.com>
    CR-Id: ALPS05916224
    Feature: SWPM(Software Power Model)

---
+++ drivers/misc/mediatek/swpm/modules/debug/v6983/swpm_me_v6983.h
@@ -0,0 +1,44 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2021 MediaTek Inc.
+ */
+
+#ifndef __SWPM_ME_V6893_H__
+#define __SWPM_ME_V6893_H__
+
+#define ME_SWPM_RESERVED_SIZE 11
+
+/* me power index structure */
+struct me_swpm_index {
+	unsigned int disp_resolution;
+	unsigned int disp_fps;
+	unsigned int disp_active;
+	unsigned int venc_freq;
+	unsigned int venc_active;
+	unsigned int venc_fps;
+	unsigned int vdec_freq;
+	unsigned int vdec_active;
+	unsigned int vdec_fps;
+	unsigned int mdp_freq;
+	unsigned int mdp_active;
+};
+
+struct me_swpm_rec_data {
+	/* 4(int) * 11 = 44 bytes */
+	unsigned int disp_resolution; /* w x h */
+	unsigned int disp_fps;
+	unsigned int disp_active;
+	unsigned int venc_freq;
+	unsigned int venc_active;
+	unsigned int venc_fps;
+	unsigned int vdec_freq;
+	unsigned int vdec_active;
+	unsigned int vdec_fps;
+	unsigned int mdp_freq;
+	unsigned int mdp_active;
+};
+
+
+
+#endif
+
