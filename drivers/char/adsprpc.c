6627f22d3a05683a17755a17b72f485617fbfa4f
Himateja Reddy <hmreddy@codeaurora.org>
2021-07-27 14:09:38 -0700

    msm: adsprpc: ignore rpmsg callback errors for duplicate responses
    When an application is running in poll mode, the backup glink
    response from the remote subsystem will usually arrive after the
    RPC call is already completed by updating of shared memory buffer.
    In such cases, do not print any error if that call's context has
    already been reassigned to the next RPC call to avoid flooding of
    the kernel logs.
    
    Change-Id: I6f32ebd309f5ae2eadc287397e98c9b7cd4bde84
    Acked-by: Thyagarajan Venkatanarayanan <venkatan@qti.qualcomm.com>
    Signed-off-by: Himateja Reddy <hmreddy@codeaurora.org>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -156,6 +156,8 @@
  */
 #define CONTEXT_PD_CHECK (3)
 
+#define GET_CTXID_FROM_RSP_CTX(rsp_ctx) (rsp_ctx & ~CONTEXT_PD_CHECK)
+
 #define RH_CID ADSP_DOMAIN_ID
 
 #define FASTRPC_STATIC_HANDLE_PROCESS_GROUP (1)
@@ -5209,7 +5211,7 @@ static int fastrpc_rpmsg_callback(struct rpmsg_device *rpdev, void *data,
 	spin_lock_irqsave(&chan->ctxlock, irq_flags);
 	ctx = chan->ctxtable[index];
 	VERIFY(err, !IS_ERR_OR_NULL(ctx) &&
-		(ctx->ctxid == (rsp->ctx & ~CONTEXT_PD_CHECK)) &&
+		(ctx->ctxid == GET_CTXID_FROM_RSP_CTX(rsp->ctx)) &&
 		ctx->magic == FASTRPC_CTX_MAGIC);
 	if (err) {
 		/*
@@ -5218,9 +5220,10 @@ static int fastrpc_rpmsg_callback(struct rpmsg_device *rpdev, void *data,
 		 * completed processing of context. Ignore the message.
 		 * Also ignore response for a call which was already
 		 * completed by update of poll memory and the context was
-		 * removed from the table.
+		 * removed from the table and possibly reused for another call.
 		 */
-		ignore_rpmsg_err = ((rsp_flags == COMPLETE_SIGNAL) || !ctx) ? 1 : 0;
+		ignore_rpmsg_err = ((rsp_flags == COMPLETE_SIGNAL) || !ctx ||
+			(ctx && (ctx->ctxid != GET_CTXID_FROM_RSP_CTX(rsp->ctx)))) ? 1 : 0;
 		goto bail_unlock;
 	}
 
