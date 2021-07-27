b4e43b6ce42af2a91add26357b53c0712ddbb009
Isaac J. Manjarres <isaacm@codeaurora.org>
2021-07-27 15:34:20 -0700

    iommu/arm-smmu-qcom: Introduce the qcom,iova-width property for TBUs
    The ECATS code currently checks to see if an IOVA is greater than 36
    bits in width. This is not entirely correct, as not all TBUs may support
    a maximum of 36 bit IOVAs.
    
    Add support for the qcom,iova-width property, which describes the
    maximum width that a TBU can handle for IOVAs, and use this value
    when checking to see if an IOVA is too big for a particular TBU.
    
    Change-Id: Id7bbeab4906d753005d4b1c5c45cf6346e323ce0
    Signed-off-by: Isaac J. Manjarres <isaacm@codeaurora.org>
    [quic_jasksing@quicinc.com: Rebased on 5.10 kernel]
    Signed-off-by: Jaskaran Singh <quic_jasksing@quicinc.com>

--- drivers/iommu/arm/arm-smmu/arm-smmu-qcom.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu-qcom.c
@@ -418,6 +418,7 @@ struct qsmmuv500_tbu_device {
 	struct arm_smmu_power_resources *pwr;
 	u32				sid_start;
 	u32				num_sids;
+	u32				iova_width;
 
 	/* Protects halt count */
 	spinlock_t			halt_lock;
@@ -1068,6 +1069,8 @@ static struct qsmmuv500_tbu_device *arm_tbu_impl_init(struct qsmmuv500_tbu_devic
 	return &arm_tbu->tbu;
 }
 
+#define QCOM_IOVA_WIDTH_DEFAULT			36
+
 static struct qsmmuv500_tbu_device *qsmmuv500_tbu_impl_init(struct qsmmuv500_tbu_device *tbu)
 {
 	return arm_tbu_impl_init(tbu);
@@ -1079,7 +1082,7 @@ static int qsmmuv500_tbu_probe(struct platform_device *pdev)
 	struct device *dev = &pdev->dev;
 	struct qsmmuv500_tbu_device *tbu;
 	const __be32 *cell;
-	int len;
+	int ret, len;
 
 	tbu = devm_kzalloc(dev, sizeof(*tbu), GFP_KERNEL);
 	if (!tbu)
@@ -1115,6 +1118,10 @@ static int qsmmuv500_tbu_probe(struct platform_device *pdev)
 
 	tbu->sid_start = of_read_number(cell, 1);
 	tbu->num_sids = of_read_number(cell + 1, 1);
+
+	ret = of_property_read_u32(dev->of_node, "qcom,iova-width", &tbu->iova_width);
+	if (ret < 0)
+		tbu->iova_width = QCOM_IOVA_WIDTH_DEFAULT;
 
 	dev_set_drvdata(dev, tbu);
 	return 0;
@@ -1606,16 +1613,14 @@ static phys_addr_t qsmmuv500_iova_to_phys(
 	int needs_redo = 0;
 	ktime_t timeout;
 
-	/* only 36 bit iova is supported */
-	if (iova >= (1ULL << 36)) {
-		dev_err_ratelimited(smmu->dev, "ECATS: address too large: %pad\n",
-					&iova);
-		return 0;
-	}
-
 	tbu = qsmmuv500_find_tbu(smmu, sid);
 	if (!tbu)
 		return 0;
+
+	if (iova >= (1ULL << tbu->iova_width)) {
+		dev_err_ratelimited(tbu->dev, "ECATS: address too large: %pad\n", &iova);
+		return 0;
+	}
 
 	ret = arm_smmu_power_on(tbu->pwr);
 	if (ret)
