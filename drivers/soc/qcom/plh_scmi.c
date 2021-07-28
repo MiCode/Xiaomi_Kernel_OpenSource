df741582c0d574606d2b956965b812332bf253c7
Gaurav Singh <sgaurav@codeaurora.org>
2021-07-28 19:56:34 +0530

    drivers: soc: qcom: Add support for plh scmi client driver
    Add support for plh scmi client driver which registers
    into scmi framework for plh vendor protocol extension.
    
    Change-Id: Ibee2cbc92493ebf875038cb27502b10fafeab5bc
    Signed-off-by: Gaurav Singh <sgaurav@codeaurora.org>
    Signed-off-by: badris <quic_badris@quicinc.com>

---
+++ drivers/soc/qcom/plh_scmi.c
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (c) 2020-2022, The Linux Foundation. All rights reserved.
+ */
+
+#include <linux/scmi_protocol.h>
+#include <linux/scmi_plh.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/err.h>
+#include <linux/errno.h>
+#include <linux/platform_device.h>
+
+extern int cpucp_plh_init(struct scmi_device *sdev);
+
+static int scmi_plh_probe(struct scmi_device *sdev)
+{
+	if (!sdev)
+		return -ENODEV;
+
+	return cpucp_plh_init(sdev);
+}
+
+static const struct scmi_device_id scmi_id_table[] = {
+	{ .protocol_id = SCMI_PROTOCOL_PLH, .name = "scmi_protocol_plh" },
+	{ },
+};
+MODULE_DEVICE_TABLE(scmi, scmi_id_table);
+
+static struct scmi_driver scmi_plh_drv = {
+	.name		= "scmi-plh-driver",
+	.probe		= scmi_plh_probe,
+	.id_table	= scmi_id_table,
+};
+module_scmi_driver(scmi_plh_drv);
+
+MODULE_SOFTDEP("pre: plh_vendor");
+MODULE_DESCRIPTION("ARM SCMI PLH driver");
+MODULE_LICENSE("GPL v2");
