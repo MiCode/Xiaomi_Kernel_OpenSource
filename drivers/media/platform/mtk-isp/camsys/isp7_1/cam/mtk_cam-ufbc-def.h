1896850607c6d344f5aa0e18fdb2b2403aac0d21
Po-Yang Huang <po-yang.huang@mediatek.com>
2021-08-04 21:28:48 +0800

    [ALPS05693565] platform: mtk-isp: sync ufbc header
    sync ufbc header with imgsys
    
    MTK-Commit-Id: e4350351c5de7b7c7da51fbb38b10f2cd5791950
    
    Change-Id: I68b536e0471f9ca1a9d4bed2a6ed53014b59b796
    Signed-off-by: Po-Yang Huang <po-yang.huang@mediatek.com>
    CR-Id: ALPS05693565
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/media/platform/mtk-isp/camsys/isp7_1/cam/mtk_cam-ufbc-def.h
+++ drivers/media/platform/mtk-isp/camsys/isp7_1/cam/mtk_cam-ufbc-def.h
@@ -7,7 +7,6 @@
 #ifndef __UFBC_DEF_H__
 #define __UFBC_DEF_H__
 
-#include "stdint.h"
 
 /**
  * Describe Mediatek UFBC (Universal Frame Buffer Compression) buffer header.
@@ -64,105 +63,108 @@
  *        real buffer layout.
  */
 
+struct UfbcBufHeader {
+	/** Describe image resolution, unit in pixel. */
+	uint32_t width;
+
+	/** Describe image resolution, unit in pixel. */
+	uint32_t height;
+
+	/** Describe UFBC data plane count, UFBC supports maximum 2 planes. */
+	uint32_t planeCount;
+
+	/** Describe the original image data bits per pixel of the given plane. */
+	uint32_t bitsPerPixel[3];
+
+	/**
+	 * Describe the offset of the given plane bit stream data in bytes,
+	 * including header size.
+	 */
+	uint32_t bitStreamOffset[3];
+
+	/** Describe the bit stream data size in bytes of the given plane. */
+	uint32_t bitStreamSize[3];
+
+	/** Describe the encoded data size in bytes of the given plane. */
+	uint32_t bitStreamDataSize[3];
+
+	/**
+	 * Describe the offset of length table of the given plane, including
+	 * header size.
+	 */
+	uint32_t tableOffset[3];
+
+	/** Describe the length table size of the given plane */
+	uint32_t tableSize[3];
+
+	/** Describe the total buffer size, including buffer header. */
+	uint32_t bufferSize;
+};
+
+
 struct UfbcBufferHeader {
 	union {
-		struct {
-			/** Describe image resolution, unit in pixel. */
-			uint32_t width;
-
-			/** Describe image resolution, unit in pixel. */
-			uint32_t height;
-
-			/** Describe UFBC data plane count, UFBC supports maximum 2 planes. */
-			uint32_t planeCount;
-
-			/** Describe the original image data bits per pixel of the given plane. */
-			uint32_t bitsPerPixel[3];
-
-			/**
-			 * Describe the offset of the given plane bit stream data in bytes,
-			 * including header size.
-			 */
-			uint32_t bitstreamOffset[3];
-
-			/** Describe the bit stream data size in bytes of the given plane. */
-			uint32_t bitStreamSize[3];
-
-			/** Describe the encoded data size in bytes of the given plane. */
-			uint32_t bitStreamDataSize[3];
-
-			/**
-			 * Describe the offset of length table of the given plane, including
-			 * header size.
-			 */
-			uint32_t tableOffset[3];
-
-			/** Describe the length table size of the given plane */
-			uint32_t tableSize[3];
-
-			/** Describe the total buffer size, including buffer header. */
-			uint32_t bufferSize;
-		};
+		struct UfbcBufHeader header;
 		uint8_t reserved[4096];
 	};
-} __attributes__((packed));
+};
 
-struct _IMG_META_INFO {
+struct IMG_META_INFO {
 	unsigned int Version;
 	unsigned int HeaderSize;
 	unsigned int BitStreamOffset[8];
 	unsigned int LengthTableOffset[8];
-} IMG_META_INFO;
+};
 
 /******************************************************************************
  * @UFBC format meta info
  *
  ******************************************************************************/
-struct _UFD_META_INFO {
+struct UFD_META_INFO {
 	unsigned int bUF;
 	unsigned int UFD_BITSTREAM_OFST_ADDR;
 	unsigned int UFD_BS_AU_START;
 	unsigned int UFD_AU2_SIZE;
 	unsigned int UFD_BOND_MODE;
-} UFD_META_INFO;
-
-struct _UFD_HW_META_INFO {
-	unsigned int Buf[32];
-} UFD_HW_META_INFO;
-
-union UFDStruct {
-	UFD_META_INFO UFD;
-	UFD_HW_META_INFO HWUFD;
 };
 
-struct _UFO_META_INFO {
-	struct UfbcBufferHeader ImgInfo;
+struct UFD_HW_META_INFO {
+	unsigned int Buf[32];
+};
+
+union UFDStruct {
+	struct UFD_META_INFO UFD;
+	struct UFD_HW_META_INFO HWUFD;
+};
+
+struct UFO_META_INFO {
+	struct UfbcBufHeader ImgInfo;
 	unsigned int AUWriteBySW;
 	union UFDStruct UFD;
-} UFO_META_INFO;
+};
 
 
-struct _YUFD_META_INFO {
+struct YUFD_META_INFO {
 	unsigned int bYUF;
 	unsigned int YUFD_BITSTREAM_OFST_ADDR;
 	unsigned int YUFD_BS_AU_START;
 	unsigned int YUFD_AU2_SIZE;
 	unsigned int YUFD_BOND_MODE;
-} YUFD_META_INFO;
-
-struct _YUFD_HW_META_INFO {
-	unsigned int Buf[32];
-} YUFD_HW_META_INFO;
-
-union YUFDStruct {
-	YUFD_META_INFO YUFD;
-	YUFD_HW_META_INFO HWYUFD;
 };
 
-struct _YUFO_META_INFO {
-	struct UfbcBufferHeader ImgInfo;
+struct YUFD_HW_META_INFO {
+	unsigned int Buf[32];
+};
+
+union YUFDStruct {
+	struct YUFD_META_INFO YUFD;
+	struct YUFD_HW_META_INFO HWYUFD;
+};
+
+struct YUFO_META_INFO {
+	struct UfbcBufHeader ImgInfo;
 	unsigned int AUWriteBySW;
 	union YUFDStruct YUFD;
-} YUFO_META_INFO;
+};
 
 #endif // __UFBC_DEF_H__
