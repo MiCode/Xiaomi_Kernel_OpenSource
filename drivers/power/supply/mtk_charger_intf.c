ead91d92fd9298c424c048b2339e828f57353345
Tony Wan <ot_tony.wan@mediatek.com>
2021-08-04 21:13:22 +0800

    [ALPS05818407] mt6761:Plugin Non-standard TA detected as PC usb port(SDP)
    [Detail]
    Plugin Non-standard TA detected as nonstd type
    
    MTK-Commit-Id: d50eba031dd98d866d181f5645c06ec96351a7ff
    
    Change-Id: Icc75bc1cea2d54684eee3d8fc0fe078ad63f028d
    Signed-off-by: Tony Wan <ot_tony.wan@mediatek.com>
    CR-Id: ALPS05818407
    Feature: Charger

--- drivers/power/supply/mtk_charger_intf.c
+++ drivers/power/supply/mtk_charger_intf.c
@@ -313,6 +313,9 @@ int get_charger_type(struct mtk_charger *info)
 		if (prop.intval == 0)
 			prop2.intval = POWER_SUPPLY_TYPE_UNKNOWN;
 		else if (prop2.intval == POWER_SUPPLY_TYPE_USB &&
+		    prop3.intval == POWER_SUPPLY_USB_TYPE_UNKNOWN)
+			prop2.intval = POWER_SUPPLY_TYPE_UNKNOWN;
+		else if (prop2.intval == POWER_SUPPLY_TYPE_USB &&
 		    prop3.intval == POWER_SUPPLY_USB_TYPE_DCP)
 			prop2.intval = POWER_SUPPLY_TYPE_USB_FLOAT;
 	}
