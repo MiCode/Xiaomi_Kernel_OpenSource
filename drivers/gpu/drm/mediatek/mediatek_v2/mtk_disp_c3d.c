2736174c00fce4d594710c9a24be7f9d558c11d6
mtk08122 <shan.an@mediatek.com>
2021-08-04 21:26:28 +0800

    [ALPS05934449] pq: remove logs
    remove logs
    
    MTK-Commit-Id: bc2e6ae2c67c04eee88e5db9175d806f51c2b9b2
    
    Change-Id: I29f525275dca297a4dcbe79fd349efd6b7b0bcee
    CR-Id: ALPS05934449
    Feature: Ambient Light Adaptive Luma
    Signed-off-by: mtk08122 <shan.an@mediatek.com>

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_c3d.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_c3d.c
@@ -404,15 +404,12 @@ static void disp_c3d_update_sram(struct mtk_ddp_comp *comp,
 
 void disp_c3d_on_start_of_frame(void)
 {
-	pr_notice("%s, line: %d", __func__, __LINE__);
-
 	atomic_set(&g_c3d_get_irq, 1);
 	wake_up_interruptible(&g_c3d_get_irq_wq);
 }
 
 void disp_c3d_on_end_of_frame_mutex(void)
 {
-	pr_notice("%s, line: %d", __func__, __LINE__);
 	atomic_set(&g_c3d_get_irq, 0);
 }
 
