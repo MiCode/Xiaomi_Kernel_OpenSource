f68577653fef25abba37d90b7de173a309729283
Ma Shi <shi.ma@mediatek.com>
2021-08-04 21:12:39 +0800

    [ALPS05729413] spi: devapc error when spi only permitted in tee
    not set set_cs when tee-only exists in spi dts node
    to avoid access spi reg
    
    MTK-Commit-Id: 4172a4961f6b33f6ccdfe4307b3ec1d6dac3bdbe
    
    Change-Id: I0d071d226b7f99eb80f1dfd2cbe3b527e034cb1a
    Signed-off-by: Ma Shi <shi.ma@mediatek.com>
    CR-Id: ALPS05729413
    Feature: [Module]SPI (Serial Peripheral Interface Bus)

--- drivers/spi/spi-mt65xx.c
+++ drivers/spi/spi-mt65xx.c
@@ -790,7 +790,6 @@ static int mtk_spi_probe(struct platform_device *pdev)
 	master->dev.of_node = pdev->dev.of_node;
 	master->mode_bits = SPI_CPOL | SPI_CPHA | SPI_LSB_FIRST;
 
-	master->set_cs = mtk_spi_set_cs;
 	master->prepare_message = mtk_spi_prepare_message;
 	master->transfer_one = mtk_spi_transfer_one;
 	master->can_dma = mtk_spi_can_dma;
@@ -823,6 +822,10 @@ static int mtk_spi_probe(struct platform_device *pdev)
 		else
 			master->rt = false;
 	}
+
+	/* avoid access spi register when accessed only in tee in case devapc error */
+	if (!of_property_read_bool(pdev->dev.of_node, "tee-only"))
+		master->set_cs = mtk_spi_set_cs;
 
 	if (mdata->dev_comp->need_pad_sel) {
 		mdata->pad_num = of_property_count_u32_elems(
