2d365033a3ca5f0fd92bcc94c4f34a47cfb4f649
Lumi Lee <lumi.lee@mediatek.com>
2021-08-13 00:19:10 +0800

    [ALPS05621599] audio: mt6983: add ble dl dai link
    [Detail]
    add ble dl dai link to support ums offload
    
    MTK-Commit-Id: 84e48f60bddf57975529a2a0060b0681b254996d
    
    Change-Id: Ib106b2e1f6ae059e55e3ef61f6a318f54ecaa8a7
    Signed-off-by: Lumi Lee <lumi.lee@mediatek.com>
    CR-Id: ALPS05621599
    Feature: [Module]Audio HAL

--- sound/soc/mediatek/mt6983/mt6983-mt6338.c
+++ sound/soc/mediatek/mt6983/mt6983-mt6338.c
@@ -727,6 +727,10 @@ SND_SOC_DAILINK_DEFS(dspa2dp,
 	DAILINK_COMP_ARRAY(COMP_CPU("audio_task_a2dp_dai")),
 	DAILINK_COMP_ARRAY(COMP_DUMMY()),
 	DAILINK_COMP_ARRAY(COMP_PLATFORM("snd_audio_dsp")));
+SND_SOC_DAILINK_DEFS(dspbledl,
+	DAILINK_COMP_ARRAY(COMP_CPU("audio_task_bledl_dai")),
+	DAILINK_COMP_ARRAY(COMP_DUMMY()),
+	DAILINK_COMP_ARRAY(COMP_PLATFORM("snd_audio_dsp")));
 #endif
 
 static struct snd_soc_dai_link mt6983_mt6338_dai_links[] = {
@@ -1355,6 +1359,11 @@ static struct snd_soc_dai_link mt6983_mt6338_dai_links[] = {
 		.name = "DSP_Playback_A2DP",
 		.stream_name = "DSP_Playback_A2DP",
 		SND_SOC_DAILINK_REG(dspa2dp),
+	},
+	{
+		.name = "DSP_Playback_BLEDL",
+		.stream_name = "DSP_Playback_BLEDL",
+		SND_SOC_DAILINK_REG(dspbledl),
 	},
 #endif
 };
