240f7251daf6843e4f9736704ec26ea31008b72b
Ting-Fang Hou <ting-fang.hou@mediatek.com>
2021-08-13 00:03:12 +0800

    [ALPS05597965] PMIC: add MT6685 audio clk driver
    add MT6685 audio clk driver porting
    
    MTK-Commit-Id: 42201b46a6ef135025d6fd4e271f3239274a3722
    
    Change-Id: I69bd838a0204f9b6eea0c2319f8eae933305bf5f
    Signed-off-by: Ting-Fang Hou <ting-fang.hou@mediatek.com>
    CR-Id: ALPS05597965
    Feature: Regulator(regulator)

--- drivers/mfd/mt6685-core.c
+++ drivers/mfd/mt6685-core.c
@@ -31,6 +31,9 @@ static const struct mfd_cell mt6685_devs[] = {
 		.num_resources = ARRAY_SIZE(mt6685_dcxo_resources),
 		.resources = mt6685_dcxo_resources,
 		.of_compatible = "mediatek,clock_buffer",
+	}, {
+		.name = "mt6685-audclk",
+		.of_compatible = "mediatek,mt6685-audclk",
 	}
 };
 
