c72be7888127c3f65afa101a20d9349f27d346a7
Mukesh Ojha <mojha@codeaurora.org>
2021-08-19 19:41:37 +0530

    pinctrl: qcom: Rename some identifiers in waipio pinctrl driver
    Rename some identifier from qcom_msm_* to waipio_* .
    
    Change-Id: I334d7bee8432deeb46d0ee2a91d700ea3650cd23
    Signed-off-by: Mukesh Ojha <mojha@codeaurora.org>

--- drivers/pinctrl/qcom/pinctrl-waipio.c
+++ drivers/pinctrl/qcom/pinctrl-waipio.c
@@ -44,7 +44,7 @@ static void qcom_trace_gpio_read(void *unused,
 	*block_gpio_read = true;
 }
 
-static int qcom_msm_pinctrl_probe(struct platform_device *pdev)
+static int waipio_pinctrl_probe(struct platform_device *pdev)
 {
 	const struct msm_pinctrl_soc_data *pinctrl_data;
 	struct device *dev = &pdev->dev;
@@ -60,34 +60,34 @@ static int qcom_msm_pinctrl_probe(struct platform_device *pdev)
 	return msm_pinctrl_probe(pdev, pinctrl_data);
 }
 
-static const struct of_device_id qcom_pinctrl_of_match[] = {
+static const struct of_device_id waipio_pinctrl_of_match[] = {
 	{ .compatible = "qcom,waipio-pinctrl", .data = &waipio_pinctrl},
 	{ .compatible = "qcom,waipio-vm-pinctrl", .data = &waipio_vm_pinctrl},
 	{ },
 };
 
-static struct platform_driver qcom_msm_pinctrl_driver = {
+static struct platform_driver waipio_pinctrl_driver = {
 	.driver = {
-		.name = "qcom_msm_pinctrl",
-		.of_match_table = qcom_pinctrl_of_match,
+		.name = "waipio-pinctrl",
+		.of_match_table = waipio_pinctrl_of_match,
 	},
-	.probe = qcom_msm_pinctrl_probe,
+	.probe = waipio_pinctrl_probe,
 	.remove = msm_pinctrl_remove,
 };
 
-static int __init qcom_msm_pinctrl_init(void)
+static int __init waipio_pinctrl_init(void)
 {
-	return platform_driver_register(&qcom_msm_pinctrl_driver);
+	return platform_driver_register(&waipio_pinctrl_driver);
 }
-arch_initcall(qcom_msm_pinctrl_init);
+arch_initcall(waipio_pinctrl_init);
 
-static void __exit qcom_msm_pinctrl_exit(void)
+static void __exit waipio_pinctrl_exit(void)
 {
-	platform_driver_unregister(&qcom_msm_pinctrl_driver);
+	platform_driver_unregister(&waipio_pinctrl_driver);
 }
-module_exit(qcom_msm_pinctrl_exit);
+module_exit(waipio_pinctrl_exit);
 
-MODULE_DESCRIPTION("QTI pinctrl driver");
+MODULE_DESCRIPTION("QTI waipio pinctrl driver");
 MODULE_LICENSE("GPL v2");
-MODULE_DEVICE_TABLE(of, qcom_pinctrl_of_match);
+MODULE_DEVICE_TABLE(of, waipio_pinctrl_of_match);
 MODULE_SOFTDEP("pre: qcom_tlmm_vm_irqchip");
