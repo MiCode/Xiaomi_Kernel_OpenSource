606c12eb965554c908b431f69ec103bff070a4f6
Mike Tipton <mdtipton@codeaurora.org>
2021-08-20 13:37:39 -0700

    cpufreq: qcom-hw: Don't use core count for end-of-table detection
    We currently check that both frequency and core count are identical to
    identify the end of the LUT table. However, the core count can change
    dynamically based on how many CPUs are active and the device
    temperature. If we're scanning the table when either of these are
    changing, then we can see the same frequency twice with different core
    counts. This results in registering a duplicate frequency with OPP,
    which prevents the cpufreq policy from being registered.
    
    To fix this, only check for an identical frequency and ignore the
    specific core count. No valid table will have the same frequency listed
    twice with different core counts.
    
    Dynamic changes to the LUT core counts are only done for mitigation
    purposes, and they're only changing the core count of boost
    configurations. The updated boost frequencies will still have a
    core_count < max_cores. So even though the table may change after we
    read it, our list of published boost frequencies is still accurate.
    
    Change-Id: Id88a0e28934df97906447d739925977e6b5779fa
    Signed-off-by: Mike Tipton <mdtipton@codeaurora.org>

--- drivers/cpufreq/qcom-cpufreq-hw.c
+++ drivers/cpufreq/qcom-cpufreq-hw.c
@@ -478,11 +478,8 @@ static int qcom_cpufreq_hw_read_lut(struct platform_device *pdev,
 		if (core_count != max_cores)
 			c->table[i].flags  = CPUFREQ_BOOST_FREQ;
 
-		/*
-		 * Two of the same frequencies with the same core counts means
-		 * end of table.
-		 */
-		if (i > 0 && prev_freq == freq && prev_cc == core_count)
+		/* Two of the same frequencies means end of table. */
+		if (i > 0 && prev_freq == freq)
 			break;
 
 		prev_cc = core_count;
