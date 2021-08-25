38e1eaf5bd875aed571e5f832e3831bebabdea9d
xin xu <xin.xu@mediatek.com>
2021-08-25 21:23:20 +0800

    [ALPS05838277] ccci: modify MD HW checksum condition
    If skb ip_summed is CHECKSUM_NONE,CHECKSUM_UNNECESSARY
    or CHECKSUM_COPLETE ,MD HW will no more do checksum
    for ipv4 and ipv6.
    
    MTK-Commit-Id: d9473e698117dead37a5efae36c50ca0c17c43f8
    
    Change-Id: Ie89b94c0aef2dfedc04fbe02bc4f046a12a24a73
    Signed-off-by: xin xu <xin.xu@mediatek.com>
    CR-Id: ALPS05838277
    Feature: Modem Interface Driver

--- drivers/misc/mediatek/eccci/hif/ccci_hif_dpmaif.c
+++ drivers/misc/mediatek/eccci/hif/ccci_hif_dpmaif.c
@@ -1808,7 +1808,9 @@ static inline int cs_type(struct sk_buff *skb)
 
 	packet_type = skb->data[0] & 0xF0;
 	if (packet_type == IPV6_VERSION) {
-		if (skb->ip_summed == CHECKSUM_NONE)
+		if (skb->ip_summed == CHECKSUM_NONE ||
+			skb->ip_summed == CHECKSUM_UNNECESSARY ||
+			skb->ip_summed == CHECKSUM_COMPLETE)
 			return 0;
 		else if (skb->ip_summed == CHECKSUM_PARTIAL)
 			return 2;
@@ -1823,7 +1825,9 @@ static inline int cs_type(struct sk_buff *skb)
 		if (iph->check == 0)
 			return 0; /* No HW check sum */
 
-		if (skb->ip_summed == CHECKSUM_NONE)
+		if (skb->ip_summed == CHECKSUM_NONE ||
+			skb->ip_summed == CHECKSUM_UNNECESSARY ||
+			skb->ip_summed == CHECKSUM_COMPLETE)
 			return 0;
 		else if (skb->ip_summed == CHECKSUM_PARTIAL)
 			return 1;
--- drivers/misc/mediatek/eccci/hif/ccci_hif_dpmaif_v3.c
+++ drivers/misc/mediatek/eccci/hif/ccci_hif_dpmaif_v3.c
@@ -2123,7 +2123,9 @@ static inline int cs_type(struct sk_buff *skb)
 
 	packet_type = skb->data[0] & 0xF0;
 	if (packet_type == IPV6_VERSION) {
-		if (skb->ip_summed == CHECKSUM_NONE)
+		if (skb->ip_summed == CHECKSUM_NONE ||
+			skb->ip_summed == CHECKSUM_UNNECESSARY ||
+			skb->ip_summed == CHECKSUM_COMPLETE)
 			return 0;
 		else if (skb->ip_summed == CHECKSUM_PARTIAL)
 			return 2;
@@ -2138,7 +2140,9 @@ static inline int cs_type(struct sk_buff *skb)
 		if (iph->check == 0)
 			return 0; /* No HW check sum */
 
-		if (skb->ip_summed == CHECKSUM_NONE)
+		if (skb->ip_summed == CHECKSUM_NONE ||
+			skb->ip_summed == CHECKSUM_UNNECESSARY ||
+			skb->ip_summed == CHECKSUM_COMPLETE)
 			return 0;
 		else if (skb->ip_summed == CHECKSUM_PARTIAL)
 			return 1;
