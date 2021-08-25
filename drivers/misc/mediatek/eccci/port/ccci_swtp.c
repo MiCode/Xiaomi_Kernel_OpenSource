41d1da352a99a3dfbf2f2d28c78dcbcfa211da20
Weiqiang Hao <weiqiang.hao@mediatek.com>
2021-08-25 21:22:25 +0800

    [ALPS05838277] ccci: Optimizing boot time
    1. delete debug code: scan gpio;
    2. SWTP will set debounce to EINT gpio,
    which is used for communication with modem,
    no need in init flow. so move it to normal flow.
    
    MTK-Commit-Id: b8329e1d8aad6c3febe8df9227535be9d57a0b4b
    
    Change-Id: I9ee0b215804278b1d8dd37faf2f9792e54bae09d
    Signed-off-by: Weiqiang Hao <weiqiang.hao@mediatek.com>
    CR-Id: ALPS05838277
    Feature: Modem Interface Driver

--- drivers/misc/mediatek/eccci/port/ccci_swtp.c
+++ drivers/misc/mediatek/eccci/port/ccci_swtp.c
@@ -20,14 +20,26 @@
 #include "ccci_swtp.h"
 #include "ccci_fsm.h"
 
+/* must keep ARRAY_SIZE(swtp_of_match) = ARRAY_SIZE(irq_name) */
 const struct of_device_id swtp_of_match[] = {
 	{ .compatible = SWTP_COMPATIBLE_DEVICE_ID, },
 	{ .compatible = SWTP1_COMPATIBLE_DEVICE_ID,},
-	{},
+	{ .compatible = SWTP2_COMPATIBLE_DEVICE_ID,},
+	{ .compatible = SWTP3_COMPATIBLE_DEVICE_ID,},
+	{ .compatible = SWTP4_COMPATIBLE_DEVICE_ID,}
 };
+
+static const char irq_name[][16] = {
+	"swtp0-eint",
+	"swtp1-eint",
+	"swtp2-eint",
+	"swtp3-eint",
+	"swtp4-eint"
+};
+
 #define SWTP_MAX_SUPPORT_MD 1
 struct swtp_t swtp_data[SWTP_MAX_SUPPORT_MD];
-#define MAX_RETRY_CNT 10
+#define MAX_RETRY_CNT 30
 
 static int swtp_send_tx_power(struct swtp_t *swtp)
 {
@@ -175,8 +187,11 @@ int swtp_md_tx_power_req_hdlr(int md_id, int data)
 	return 0;
 }
 
-int swtp_init(int md_id)
+static void swtp_init_delayed_work(struct work_struct *work)
 {
+	struct swtp_t *swtp = container_of(to_delayed_work(work),
+		struct swtp_t, init_delayed_work);
+	int md_id;
 	int i, ret = 0;
 #ifdef CONFIG_MTK_EIC
 	u32 ints[2] = { 0, 0 };
@@ -186,16 +201,27 @@ int swtp_init(int md_id)
 	u32 ints1[2] = { 0, 0 };
 	struct device_node *node = NULL;
 
-	if (md_id < 0 || md_id >= SWTP_MAX_SUPPORT_MD) {
-		CCCI_LEGACY_ERR_LOG(-1, SYS,
-			"invalid md_id = %d\n", md_id);
-		return -1;
-	}
-	swtp_data[md_id].md_id = md_id;
-	INIT_DELAYED_WORK(&swtp_data[md_id].delayed_work, swtp_tx_delayed_work);
-	swtp_data[md_id].tx_power_mode = SWTP_NO_TX_POWER;
+	CCCI_NORMAL_LOG(-1, SYS, "%s start\n", __func__);
+	CCCI_BOOTUP_LOG(-1, SYS, "%s start\n", __func__);
 
-	spin_lock_init(&swtp_data[md_id].spinlock);
+	md_id = swtp->md_id;
+	if (md_id < 0 || md_id >= SWTP_MAX_SUPPORT_MD) {
+		ret = -2;
+		CCCI_LEGACY_ERR_LOG(-1, SYS,
+			"%s: invalid md_id = %d\n", __func__, md_id);
+		goto SWTP_INIT_END;
+	}
+
+	if (ARRAY_SIZE(swtp_of_match) != ARRAY_SIZE(irq_name) ||
+		ARRAY_SIZE(swtp_of_match) > MAX_PIN_NUM ||
+		ARRAY_SIZE(irq_name) > MAX_PIN_NUM) {
+		ret = -3;
+		CCCI_LEGACY_ERR_LOG(-1, SYS,
+			"%s: invalid array count = %d(of_match), %d(irq_name)\n",
+			__func__, ARRAY_SIZE(swtp_of_match),
+			ARRAY_SIZE(irq_name));
+		goto SWTP_INIT_END;
+	}
 
 	for (i = 0; i < MAX_PIN_NUM; i++)
 		swtp_data[md_id].gpio_state[i] = SWTP_EINT_PIN_PLUG_OUT;
@@ -235,8 +261,7 @@ int swtp_init(int md_id)
 
 			ret = request_irq(swtp_data[md_id].irq[i],
 				swtp_irq_handler, IRQF_TRIGGER_NONE,
-				(i == 0 ? "swtp0-eint" : "swtp1-eint"),
-				&swtp_data[md_id]);
+				irq_name[i], &swtp_data[md_id]);
 			if (ret) {
 				CCCI_LEGACY_ERR_LOG(md_id, SYS,
 					"swtp%d-eint IRQ LINE NOT AVAILABLE\n",
@@ -247,12 +272,43 @@ int swtp_init(int md_id)
 			CCCI_LEGACY_ERR_LOG(md_id, SYS,
 				"%s:can't find swtp%d compatible node\n",
 				__func__, i);
-			ret = -1;
+			ret = -4;
 		}
 	}
 	register_ccci_sys_call_back(md_id, MD_SW_MD1_TX_POWER_REQ,
 		swtp_md_tx_power_req_hdlr);
 
-	return ret;
+SWTP_INIT_END:
+	CCCI_BOOTUP_LOG(md_id, SYS, "%s end: ret = %d\n", __func__, ret);
+	CCCI_NORMAL_LOG(md_id, SYS, "%s end: ret = %d\n", __func__, ret);
+
+	return;
 }
 
+int swtp_init(int md_id)
+{
+	/* parameter check */
+	if (md_id < 0 || md_id >= SWTP_MAX_SUPPORT_MD) {
+		CCCI_LEGACY_ERR_LOG(-1, SYS,
+			"%s: invalid md_id = %d\n", __func__, md_id);
+		return -1;
+	}
+	/* init woke setting */
+	swtp_data[md_id].md_id = md_id;
+
+	INIT_DELAYED_WORK(&swtp_data[md_id].init_delayed_work,
+		swtp_init_delayed_work);
+	/* tx work setting */
+	INIT_DELAYED_WORK(&swtp_data[md_id].delayed_work,
+		swtp_tx_delayed_work);
+	swtp_data[md_id].tx_power_mode = SWTP_NO_TX_POWER;
+
+	spin_lock_init(&swtp_data[md_id].spinlock);
+
+	/* schedule init work */
+	schedule_delayed_work(&swtp_data[md_id].init_delayed_work, HZ);
+
+	CCCI_BOOTUP_LOG(md_id, SYS, "%s end, init_delayed_work scheduled\n",
+		__func__);
+	return 0;
+}
--- drivers/misc/mediatek/eccci/port/ccci_swtp.h
+++ drivers/misc/mediatek/eccci/port/ccci_swtp.h
@@ -9,10 +9,12 @@
 /* modify MAX_PIN_NUM/DTS to support more gpio,
  * need to follow SOP for customization.
  */
-#define MAX_PIN_NUM 2
+#define MAX_PIN_NUM 5
 #define SWTP_COMPATIBLE_DEVICE_ID "mediatek, swtp-eint"
 #define SWTP1_COMPATIBLE_DEVICE_ID "mediatek, swtp1-eint"
-
+#define SWTP2_COMPATIBLE_DEVICE_ID "mediatek, swtp2-eint"
+#define SWTP3_COMPATIBLE_DEVICE_ID "mediatek, swtp3-eint"
+#define SWTP4_COMPATIBLE_DEVICE_ID "mediatek, swtp4-eint"
 
 #define SWTP_EINT_PIN_PLUG_IN	(1)
 #define SWTP_EINT_PIN_PLUG_OUT	(0)
@@ -30,6 +32,7 @@ struct swtp_t {
 	int	tx_power_mode;
 	spinlock_t		spinlock;
 	struct delayed_work delayed_work;
+	struct delayed_work init_delayed_work;
 };
 /*****************************************************************************/
 /* External API Region called by ccci_swtp object */
