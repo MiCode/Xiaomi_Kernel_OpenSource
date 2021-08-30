d6cd7dca675e9ed6f5fc2d0496eb7b06fca5ac5b
Bao D. Nguyen <nguyenb@codeaurora.org>
2021-08-30 10:17:26 -0700

    scsi: ufs-qcom: Merge qcom changes from waipio to kalama
    This is a snapshot of kernel msm-5.10 taken at commit <1b11a2cc5806>
    ("Merge "drivers: dcvs: pmu_lib: Add capability to unload pmu counters"").
    Add changes to support kalama platforms.
    
    Change-Id: I23eb6080d0e2c68e376f08b4ce2a209a9ad50c0a
    Signed-off-by: Bao D. Nguyen <nguyenb@codeaurora.org>

--- drivers/scsi/ufs/ufs-qcom.h
+++ drivers/scsi/ufs/ufs-qcom.h
@@ -7,10 +7,15 @@
 
 #include <linux/reset-controller.h>
 #include <linux/reset.h>
+#include <linux/phy/phy.h>
+#include <linux/pm_qos.h>
+#include "ufshcd.h"
+#include "unipro.h"
 
-#define MAX_UFS_QCOM_HOSTS	1
+#define MAX_UFS_QCOM_HOSTS	2
 #define MAX_U32                 (~(u32)0)
 #define MPHY_TX_FSM_STATE       0x41
+#define MPHY_RX_FSM_STATE       0xC1
 #define TX_FSM_HIBERN8          0x1
 #define HBRN8_POLL_TOUT_MS      100
 #define DEFAULT_CLK_RATE_HZ     1000000
@@ -23,31 +28,35 @@
 #define UFS_HW_VER_STEP_SHFT	(0)
 #define UFS_HW_VER_STEP_MASK	(0xFFFF << UFS_HW_VER_STEP_SHFT)
 
-/* vendor specific pre-defined parameters */
-#define UFS_HS_G4       4               /* HS Gear 4 */
+#define UFS_VENDOR_MICRON	0x12C
 
 /* vendor specific pre-defined parameters */
+#define UFS_HS_G4	4		/* HS Gear 4 */
+
 #define SLOW 1
 #define FAST 2
 
+#define UFS_QCOM_PHY_SUBMODE_NON_G4	0
+#define UFS_QCOM_PHY_SUBMODE_G4		1
+
+#define UFS_QCOM_LIMIT_NUM_LANES_RX	2
+#define UFS_QCOM_LIMIT_NUM_LANES_TX	2
+#define UFS_QCOM_LIMIT_HSGEAR_RX	UFS_HS_G4
+#define UFS_QCOM_LIMIT_HSGEAR_TX	UFS_HS_G4
+#define UFS_QCOM_LIMIT_PWMGEAR_RX	UFS_PWM_G4
+#define UFS_QCOM_LIMIT_PWMGEAR_TX	UFS_PWM_G4
+#define UFS_QCOM_LIMIT_RX_PWR_PWM	SLOW_MODE
+#define UFS_QCOM_LIMIT_TX_PWR_PWM	SLOW_MODE
+#define UFS_QCOM_LIMIT_RX_PWR_HS	FAST_MODE
+#define UFS_QCOM_LIMIT_TX_PWR_HS	FAST_MODE
 #define UFS_QCOM_LIMIT_HS_RATE		PA_HS_MODE_B
+#define UFS_QCOM_LIMIT_DESIRED_MODE	FAST
+#define UFS_QCOM_LIMIT_PHY_SUBMODE	UFS_QCOM_PHY_SUBMODE_G4
 
-#define UFS_QCOM_PHY_SUBMODE_NON_G4     0
-#define UFS_QCOM_PHY_SUBMODE_G4         1
-
-#define UFS_QCOM_LIMIT_NUM_LANES_RX     2
-#define UFS_QCOM_LIMIT_NUM_LANES_TX     2
-#define UFS_QCOM_LIMIT_HSGEAR_RX        UFS_HS_G4
-#define UFS_QCOM_LIMIT_HSGEAR_TX        UFS_HS_G4
-#define UFS_QCOM_LIMIT_PWMGEAR_RX       UFS_PWM_G4
-#define UFS_QCOM_LIMIT_PWMGEAR_TX       UFS_PWM_G4
-#define UFS_QCOM_LIMIT_RX_PWR_PWM       SLOW_MODE
-#define UFS_QCOM_LIMIT_TX_PWR_PWM       SLOW_MODE
-#define UFS_QCOM_LIMIT_RX_PWR_HS        FAST_MODE
-#define UFS_QCOM_LIMIT_TX_PWR_HS        FAST_MODE
-#define UFS_QCOM_LIMIT_HS_RATE          PA_HS_MODE_B
-#define UFS_QCOM_LIMIT_DESIRED_MODE     FAST
-#define UFS_QCOM_LIMIT_PHY_SUBMODE      UFS_QCOM_PHY_SUBMODE_G4
+/* default value of auto suspend is 3 seconds */
+#define UFS_QCOM_AUTO_SUSPEND_DELAY	3000
+#define UFS_QCOM_CLK_GATING_DELAY_MS_PWR_SAVE	10
+#define UFS_QCOM_CLK_GATING_DELAY_MS_PERF	50
 
 /* QCOM UFS host controller vendor specific registers */
 enum {
@@ -119,6 +128,17 @@ enum {
 				 DFC_HW_CGC_EN | TRLUT_HW_CGC_EN |\
 				 TMRLUT_HW_CGC_EN | OCSC_HW_CGC_EN)
 
+/* bit definitions for UFS_AH8_CFG register */
+#define CC_UFS_HCLK_REQ_EN		BIT(1)
+#define CC_UFS_SYS_CLK_REQ_EN		BIT(2)
+#define CC_UFS_ICE_CORE_CLK_REQ_EN	BIT(3)
+#define CC_UFS_UNIPRO_CORE_CLK_REQ_EN	BIT(4)
+#define CC_UFS_AUXCLK_REQ_EN		BIT(5)
+
+#define UFS_HW_CLK_CTRL_EN	(CC_UFS_SYS_CLK_REQ_EN |\
+				 CC_UFS_ICE_CORE_CLK_REQ_EN |\
+				 CC_UFS_UNIPRO_CORE_CLK_REQ_EN |\
+				 CC_UFS_AUXCLK_REQ_EN)
 /* bit offset */
 enum {
 	OFFSET_UFS_PHY_SOFT_RESET           = 1,
@@ -132,6 +152,11 @@ enum {
 	MASK_CLK_NS_REG                     = 0xFFFC00,
 };
 
+enum ufs_qcom_phy_init_type {
+	UFS_PHY_INIT_FULL,
+	UFS_PHY_INIT_CFG_RESTORE,
+};
+
 /* QCOM UFS debug print bit mask */
 #define UFS_QCOM_DBG_PRINT_REGS_EN	BIT(0)
 #define UFS_QCOM_DBG_PRINT_ICE_REGS_EN	BIT(1)
@@ -143,10 +168,45 @@ enum {
 
 /* QUniPro Vendor specific attributes */
 #define PA_VS_CONFIG_REG1	0x9000
-#define DME_VS_CORE_CLK_CTRL	0xD002
+#define BIT_TX_EOB_COND         BIT(23)
+#define PA_VS_CONFIG_REG2       0x9005
+#define H8_ENTER_COND_OFFSET 0x6
+#define H8_ENTER_COND_MASK GENMASK(7, 6)
+#define BIT_RX_EOB_COND		BIT(5)
+#define BIT_LINKCFG_WAIT_LL1_RX_CFG_RDY BIT(26)
+#define SAVECONFIGTIME_MODE_MASK        0x6000
+#define DME_VS_CORE_CLK_CTRL    0xD002
+
+
 /* bit and mask definitions for DME_VS_CORE_CLK_CTRL attribute */
 #define DME_VS_CORE_CLK_CTRL_CORE_CLK_DIV_EN_BIT		BIT(8)
 #define DME_VS_CORE_CLK_CTRL_MAX_CORE_CLK_1US_CYCLES_MASK	0xFF
+
+#define PA_VS_CLK_CFG_REG	0x9004
+#define PA_VS_CLK_CFG_REG_MASK	0x1FF
+#define DME_VS_CORE_CLK_CTRL_MAX_CORE_CLK_1US_CYCLES_MASK_V4	0xFFF
+#define DME_VS_CORE_CLK_CTRL_MAX_CORE_CLK_1US_CYCLES_OFFSET_V4	0x10
+
+#define PA_VS_CORE_CLK_40NS_CYCLES	0x9007
+#define PA_VS_CORE_CLK_40NS_CYCLES_MASK	0xF
+
+#define DL_VS_CLK_CFG		0xA00B
+#define DL_VS_CLK_CFG_MASK	0x3FF
+
+#define DME_VS_CORE_CLK_CTRL	0xD002
+/* bit and mask definitions for DME_VS_CORE_CLK_CTRL attribute */
+#define DME_VS_CORE_CLK_CTRL_MAX_CORE_CLK_1US_CYCLES_MASK_V4	0xFFF
+#define DME_VS_CORE_CLK_CTRL_MAX_CORE_CLK_1US_CYCLES_OFFSET_V4	0x10
+#define DME_VS_CORE_CLK_CTRL_MAX_CORE_CLK_1US_CYCLES_MASK	0xFF
+#define DME_VS_CORE_CLK_CTRL_CORE_CLK_DIV_EN_BIT		BIT(8)
+#define DME_VS_CORE_CLK_CTRL_DME_HW_CGC_EN			BIT(9)
+
+/* Device Quirks */
+/*
+ * Some ufs devices may need more time to be in hibern8 before exiting.
+ * Enable this quirk to give it an additional 100us.
+ */
+#define UFS_DEVICE_QUIRK_PA_HIBER8TIME          (1 << 15)
 
 static inline void
 ufs_qcom_get_controller_revision(struct ufs_hba *hba,
@@ -183,6 +243,16 @@ static inline void ufs_qcom_deassert_reset(struct ufs_hba *hba)
 	mb();
 }
 
+struct ufs_qcom_bus_vote {
+	uint32_t client_handle;
+	uint32_t curr_vote;
+	int min_bw_vote;
+	int max_bw_vote;
+	int saved_vote;
+	bool is_max_bw_needed;
+	struct device_attribute max_bus_bw;
+};
+
 /* Host controller hardware version: major.minor.step */
 struct ufs_hw_version {
 	u16 step;
@@ -196,6 +266,60 @@ struct ufs_qcom_testbus {
 };
 
 struct gpio_desc;
+
+struct qcom_bus_vectors {
+	uint32_t ab;
+	uint32_t ib;
+};
+
+struct qcom_bus_path {
+	unsigned int num_paths;
+	struct qcom_bus_vectors *vec;
+};
+
+struct qcom_bus_scale_data {
+	struct qcom_bus_path *usecase;
+	unsigned int num_usecase;
+	struct icc_path *ufs_ddr;
+	struct icc_path *cpu_ufs;
+
+	const char *name;
+};
+
+struct qos_cpu_group {
+	cpumask_t mask;
+	unsigned int *votes;
+	struct dev_pm_qos_request *qos_req;
+	bool voted;
+	struct work_struct vwork;
+	struct ufs_qcom_host *host;
+	unsigned int curr_vote;
+	bool perf_core;
+};
+
+struct ufs_qcom_qos_req {
+	struct qos_cpu_group *qcg;
+	unsigned int num_groups;
+	struct workqueue_struct *workq;
+};
+
+/* Check for QOS_POWER when added to DT */
+enum constraint {
+	QOS_PERF,
+	QOS_POWER,
+	QOS_MAX,
+};
+
+enum ufs_qcom_therm_lvl {
+	UFS_QCOM_LVL_NO_THERM, /* No thermal mitigation */
+	UFS_QCOM_LVL_AGGR_THERM, /* Aggressive thermal mitigation */
+	UFS_QCOM_LVL_MAX_THERM, /* Max thermal mitigation */
+};
+
+struct ufs_qcom_thermal {
+	struct thermal_cooling_device *tcd;
+	unsigned long curr_state;
+};
 
 struct ufs_qcom_host {
 	/*
@@ -211,10 +335,23 @@ struct ufs_qcom_host {
 	 * configuration even after UFS controller core power collapse.
 	 */
 	#define UFS_QCOM_CAP_RETAIN_SEC_CFG_AFTER_PWR_COLLAPSE	0x2
+
+	/*
+	 * Set this capability if host controller supports Qunipro internal
+	 * clock gating.
+	 */
+	#define UFS_QCOM_CAP_QUNIPRO_CLK_GATING		0x4
+
+	/*
+	 * Set this capability if host controller supports SVS2 frequencies.
+	 */
+	#define UFS_QCOM_CAP_SVS2	0x8
+
 	u32 caps;
 
 	struct phy *generic_phy;
 	struct ufs_hba *hba;
+	struct ufs_qcom_bus_vote bus_vote;
 	struct ufs_pa_layer_attr dev_req_params;
 	struct clk *rx_l0_sync_clk;
 	struct clk *tx_l0_sync_clk;
@@ -228,6 +365,9 @@ struct ufs_qcom_host {
 #ifdef CONFIG_SCSI_UFS_CRYPTO
 	void __iomem *ice_mmio;
 #endif
+#if IS_ENABLED(CONFIG_QTI_HW_KEY_MANAGER)
+	void __iomem *ice_hwkm_mmio;
+#endif
 
 	u32 dev_ref_clk_en_mask;
 
@@ -238,13 +378,45 @@ struct ufs_qcom_host {
 	/* Reset control of HCI */
 	struct reset_control *core_reset;
 	struct reset_controller_dev rcdev;
+
 	struct gpio_desc *device_reset;
 
 	int limit_tx_hs_gear;
 	int limit_rx_hs_gear;
 	int limit_tx_pwm_gear;
 	int limit_rx_pwm_gear;
+	int limit_rate;
+	int limit_phy_submode;
+
 	bool disable_lpm;
+	struct qcom_bus_scale_data *qbsd;
+	struct ufs_vreg *vddp_ref_clk;
+	struct ufs_vreg *vccq_parent;
+	bool work_pending;
+	bool bypass_g4_cfgready;
+	bool is_dt_pm_level_read;
+	bool is_phy_pwr_on;
+	/* Protect the usage of is_phy_pwr_on against racing */
+	struct mutex phy_mutex;
+	struct ufs_qcom_qos_req *ufs_qos;
+	struct ufs_qcom_thermal uqt;
+	/* FlashPVL entries */
+	bool err_occurred;
+	bool crash_on_err;
+	atomic_t scale_up;
+	atomic_t clks_on;
+	unsigned long load_delay_ms;
+#define NUM_REQS_HIGH_THRESH 64
+#define NUM_REQS_LOW_THRESH 32
+	atomic_t num_reqs_threshold;
+	bool cur_freq_vote;
+	struct delayed_work fwork;
+	bool cpufreq_dis;
+	unsigned int min_cpu_scale_freq;
+	unsigned int max_cpu_scale_freq;
+	int config_cpu;
+	void *ufs_ipc_log_ctx;
+	bool dbg_en;
 };
 
 static inline u32
@@ -261,6 +433,9 @@ ufs_qcom_get_debug_reg_offset(struct ufs_qcom_host *host, u32 reg)
 #define ufs_qcom_is_link_hibern8(hba) ufshcd_is_link_hibern8(hba)
 
 int ufs_qcom_testbus_config(struct ufs_qcom_host *host);
+void ufs_qcom_print_hw_debug_reg_all(struct ufs_hba *hba, void *priv,
+		void (*print_fn)(struct ufs_hba *hba, int offset, int num_regs,
+				const char *str, void *priv));
 
 static inline bool ufs_qcom_cap_qunipro(struct ufs_qcom_host *host)
 {
@@ -270,6 +445,94 @@ static inline bool ufs_qcom_cap_qunipro(struct ufs_qcom_host *host)
 		return false;
 }
 
+static inline bool ufs_qcom_cap_qunipro_clk_gating(struct ufs_qcom_host *host)
+{
+	return !!(host->caps & UFS_QCOM_CAP_QUNIPRO_CLK_GATING);
+}
+
+static inline bool ufs_qcom_cap_svs2(struct ufs_qcom_host *host)
+{
+	return !!(host->caps & UFS_QCOM_CAP_SVS2);
+}
+
+/**
+ * ufshcd_dme_rmw - get modify set a dme attribute
+ * @hba - per adapter instance
+ * @mask - mask to apply on read value
+ * @val - actual value to write
+ * @attr - dme attribute
+ */
+static inline int ufshcd_dme_rmw(struct ufs_hba *hba, u32 mask,
+				 u32 val, u32 attr)
+{
+	u32 cfg = 0;
+	int err = 0;
+
+	err = ufshcd_dme_get(hba, UIC_ARG_MIB(attr), &cfg);
+	if (err)
+		goto out;
+
+	cfg &= ~mask;
+	cfg |= (val & mask);
+
+	err = ufshcd_dme_set(hba, UIC_ARG_MIB(attr), cfg);
+
+out:
+	return err;
+}
+
+/*
+ *  IOCTL opcode for ufs queries has the following opcode after
+ *  SCSI_IOCTL_GET_PCI
+ */
+#define UFS_IOCTL_QUERY			0x5388
+
+/**
+ * struct ufs_ioctl_query_data - used to transfer data to and from user via
+ * ioctl
+ * @opcode: type of data to query (descriptor/attribute/flag)
+ * @idn: id of the data structure
+ * @buf_size: number of allocated bytes/data size on return
+ * @buffer: data location
+ *
+ * Received: buffer and buf_size (available space for transferred data)
+ * Submitted: opcode, idn, length, buf_size
+ */
+struct ufs_ioctl_query_data {
+	/*
+	 * User should select one of the opcode defined in "enum query_opcode".
+	 * Please check include/uapi/scsi/ufs/ufs.h for the definition of it.
+	 * Note that only UPIU_QUERY_OPCODE_READ_DESC,
+	 * UPIU_QUERY_OPCODE_READ_ATTR & UPIU_QUERY_OPCODE_READ_FLAG are
+	 * supported as of now. All other query_opcode would be considered
+	 * invalid.
+	 * As of now only read query operations are supported.
+	 */
+	__u32 opcode;
+	/*
+	 * User should select one of the idn from "enum flag_idn" or "enum
+	 * attr_idn" or "enum desc_idn" based on whether opcode above is
+	 * attribute, flag or descriptor.
+	 * Please check include/uapi/scsi/ufs/ufs.h for the definition of it.
+	 */
+	__u8 idn;
+	/*
+	 * User should specify the size of the buffer (buffer[0] below) where
+	 * it wants to read the query data (attribute/flag/descriptor).
+	 * As we might end up reading less data then what is specified in
+	 * buf_size. So we are updating buf_size to what exactly we have read.
+	 */
+	__u16 buf_size;
+	/*
+	 * placeholder for the start of the data buffer where kernel will copy
+	 * the query data (attribute/flag/descriptor) read from the UFS device
+	 * Note:
+	 * For Read/Write Attribute you will have to allocate 4 bytes
+	 * For Read/Write Flag you will have to allocate 1 byte
+	 */
+	__u8 buffer[0];
+};
+
 /* ufs-qcom-ice.c */
 
 #ifdef CONFIG_SCSI_UFS_CRYPTO
@@ -278,6 +541,7 @@ int ufs_qcom_ice_enable(struct ufs_qcom_host *host);
 int ufs_qcom_ice_resume(struct ufs_qcom_host *host);
 int ufs_qcom_ice_program_key(struct ufs_hba *hba,
 			     const union ufs_crypto_cfg_entry *cfg, int slot);
+void ufs_qcom_ice_disable(struct ufs_qcom_host *host);
 #else
 static inline int ufs_qcom_ice_init(struct ufs_qcom_host *host)
 {
@@ -288,6 +552,10 @@ static inline int ufs_qcom_ice_enable(struct ufs_qcom_host *host)
 	return 0;
 }
 static inline int ufs_qcom_ice_resume(struct ufs_qcom_host *host)
+{
+	return 0;
+}
+static inline void ufs_qcom_ice_disable(struct ufs_qcom_host *host)
 {
 	return 0;
 }
