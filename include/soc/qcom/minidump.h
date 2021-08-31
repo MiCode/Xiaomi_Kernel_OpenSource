dbf7e1902512a71171a4795797b9e4c14df3bfa5
Mukesh Ojha <quic_mojha@quicinc.com>
2021-08-31 18:35:29 +0530

    soc: qcom: Add snapshot of minidump
    Add snapshot of minidump as of commit <889f8fde8391>
    ( "soc: qcom: Register irq stacks with minidump" ).
    
    Change-Id: I3d04a2bfd260404e25f81296f6f61fc74070c95a
    Signed-off-by: Mukesh Ojha <quic_mojha@quicinc.com>
    Signed-off-by: Yogesh Lal <quic_ylal@quicinc.com>

--- include/soc/qcom/minidump.h
+++ include/soc/qcom/minidump.h
@@ -71,10 +71,20 @@ struct va_md_entry {
 };
 
 #if IS_ENABLED(CONFIG_QCOM_VA_MINIDUMP)
-extern struct atomic_notifier_head qcom_va_md_notifier_list;
+extern bool qcom_va_md_enabled(void);
+extern int qcom_va_md_register(char *name, struct notifier_block *nb);
 extern int qcom_va_md_add_region(struct va_md_entry *entry);
 #else
-static inline int qcom_va_md_add_region(struct va_md_entry *entry) { return 0; }
+static inline bool qcom_va_md_enabled(void) { return false; }
+static inline int qcom_va_md_register(char *name, struct notifier_block *nb)
+{
+	return -ENODEV;
+}
+
+static inline int qcom_va_md_add_region(struct va_md_entry *entry)
+{
+	return -ENODEV;
+}
 #endif
 
 #endif
