93f36d68531c0ce5bba7d43b8c4771baeb043f99
Chengyang.Wu <Chengyang.Wu@mediatek.com>
2021-09-01 21:26:46 +0800

    [ALPS05977257] Drivers:S BLE CG Phone Call Porting
    Sync with all kernel repo
    
    MTK-Commit-Id: be30de58b2fb37d80aaabe168628e8722b9f0060
    
    Signed-off-by: Chengyang.Wu <Chengyang.Wu@mediatek.com>
    Feature: BLE Call Gateway
    Change-Id: I9971c946773a648c06a80557769684ad6331ee9a
    CR-Id: ALPS05977257

--- drivers/misc/mediatek/audio_ipi/mt6779/audio_ipi_platform.c
+++ drivers/misc/mediatek/audio_ipi/mt6779/audio_ipi_platform.c
@@ -113,6 +113,8 @@ uint32_t audio_get_dsp_id(const uint8_t task)
 	case TASK_SCENE_KTV:
 	case TASK_SCENE_CAPTURE_RAW:
 	case TASK_SCENE_PHONE_CALL_SUB:
+	case TASK_SCENE_BLECALLDL:
+	case TASK_SCENE_BLECALLUL:
 		dsp_id = AUDIO_OPENDSP_USE_HIFI3_A;
 		break;
 	default:
