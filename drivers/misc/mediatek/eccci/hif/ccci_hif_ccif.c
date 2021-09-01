32c7e663cda193afd342557999b1b5adfc3e60db
Yanjie Jiang <yanjie.jiang@mediatek.com>
2021-09-01 21:30:44 +0800

    [ALPS05804920] ccci: notify ccci_fsd wakeup source
    If ccci_fs data is the wakeup source,
    Notify ccci_fsd.
    
    MTK-Commit-Id: edcb9414de74fc2a3b726b108be942fb4daaa6a6
    
    Change-Id: I50f4d92f23806046c33298e770cb49528a50f049
    CR-Id: ALPS05804920
    Feature: Modem Interface Driver
    Signed-off-by: Yanjie Jiang <yanjie.jiang@mediatek.com>
    (cherry picked from commit 789779a6ea5d344e7748df7f1da929007354e712)

--- drivers/misc/mediatek/eccci/hif/ccci_hif_ccif.c
+++ drivers/misc/mediatek/eccci/hif/ccci_hif_ccif.c
@@ -959,6 +959,8 @@ static int ccif_rx_collect(struct md_ccif_queue *queue, int budget,
 				queue->index, ccci_h->channel,
 				ccci_h->reserved, md_ctrl->wakeup_count,
 				ccci_port_get_dev_name(ccci_h->channel));
+			if (ccci_h->channel == CCCI_FS_RX)
+				ccci_h->data[0] |= CCCI_FS_AP_CCCI_WAKEUP;
 		}
 		if (ccci_h->channel == CCCI_C2K_LB_DL)
 			atomic_set(&lb_dl_q, queue->index);
