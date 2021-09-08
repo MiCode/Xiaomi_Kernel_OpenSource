c38add1f8d3853613f7f9be6e84a59064b6850a6
Gokul krishna Krishnakumar <quic_gokukris@quicinc.com>
2021-09-07 17:18:12 -0700

    soc: qcom: ramdump: Add support for elf64 dumps
    The qcom_elf_dump() API currently only supported elf32 dumps, but
    the kernel is compiled as CONFIG_BIT64=y. This results in the
    addresses in the elf header being truncated. This change adds
    support to specify the class of the elf file to be generated.
    
    Change-Id: I1b72a828df0b45978891ed6ba6261273200500b0
    Signed-off-by: Siddharth Gupta <sidgup@codeaurora.org>
    Signed-off-by: Gokul krishna Krishnakumar <quic_gokukris@quicinc.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -942,7 +942,7 @@ static int fastrpc_ramdump(struct device *dev, struct qcom_dump_segment *ramdump
 	INIT_LIST_HEAD(&head);
 	list_add(&ramdump_seg->node, &head);
 	if (type)
-		err = qcom_elf_dump(&head, dev);
+		err = qcom_elf_dump(&head, dev, ELF_CLASS);
 	else
 		err = qcom_dump(&head, dev);
 
@@ -4886,7 +4886,7 @@ static int fastrpc_mmap_remove_ssr(struct fastrpc_file *fl, int locked)
 				(void *)match->va;
 				ramdump_segments_rh->size = match->size;
 				list_add(&ramdump_segments_rh->node, &head);
-				ret = qcom_elf_dump(&head, NULL);
+				ret = qcom_elf_dump(&head, NULL, ELF_CLASS);
 				if (ret < 0)
 					pr_err("adsprpc: %s: unable to dump heap (err %d)\n",
 						__func__, ret);
