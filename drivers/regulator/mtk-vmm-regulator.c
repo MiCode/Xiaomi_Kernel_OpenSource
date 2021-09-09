73482f9b341c397e1dc6c38d4277c77cfb7c256c
mtk13668 <yuan-jung.kuo@mediatek.com>
2021-09-09 15:57:02 +0800

    [ALPS06100771] regulator: mt6319: Power buck enable/disable
    To avoid VMM buck power leakage, we support
    VMM power buck enable/disable. According to DEs comments
    SPM VMM buck iso controls are needed. It should follow
    the following order:
        When VMM has been turnned on:
        driver needs to set up SPM BUCK ISO
        registers as 1b0 in the following order
        SPM_VMM_EXT_BUCK_ISO_BIT (16 th)->
        SPM_AOC_VMM_SRAM_ISO_DIN_BIT (17 th)->
        SPM_AOC_VMM_SRAM_LATCH_ENB (18 th)
    
        When VMM buck is going to be turnned off:
        driver needs to set up SPM BUCK ISO registers
        as 1b1 in the following order
        SPM_AOC_VMM_SRAM_LATCH_ENB (18 th)->
        SPM_AOC_VMM_SRAM_ISO_DIN_BIT (17 th)->
        SPM_VMM_EXT_BUCK_ISO_BIT (16 th)
    
    MTK-Commit-Id: 970b9e53b8dc5685053f64cf8ccbf1e289995e28
    
    CR-Id: ALPS06100771
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: mtk13668 <yuan-jung.kuo@mediatek.com>
    Change-Id: I81b8797234f6b1b2267c4035f19776f551877a20

--- drivers/regulator/mtk-vmm-regulator.c
+++ drivers/regulator/mtk-vmm-regulator.c
@@ -21,12 +21,18 @@
 #include <linux/pm_opp.h>
 #include <linux/clk.h>
 #include <linux/debugfs.h>
+#include <linux/io.h>
 
 static struct dentry *ispdvfs_debugfs_dir;
 static struct regulator *vmm_reg;
 
 #define REGULATOR_ID_VMM 0
 #define AGING_MARGIN_MICROVOLT 12500
+
+#define SPM_VMM_ISO_REG_OFST 0xF30
+#define SPM_VMM_EXT_BUCK_ISO_BIT 16
+#define SPM_AOC_VMM_SRAM_ISO_DIN_BIT 17
+#define SPM_AOC_VMM_SRAM_LATCH_ENB 18
 
 int mtk_ispdvfs_dbg_level;
 EXPORT_SYMBOL(mtk_ispdvfs_dbg_level);
@@ -318,9 +324,46 @@ static void vmm_init_opp_table(struct dvfs_driver_data *data)
 	}
 }
 
+static void vmm_buck_isolation_off(void __iomem *base)
+{
+	void __iomem *reg_buck_iso_addr = base + SPM_VMM_ISO_REG_OFST;
+	u32 reg_buck_iso_val;
+
+	reg_buck_iso_val = readl_relaxed(reg_buck_iso_addr);
+	reg_buck_iso_val &= ~(1UL << SPM_VMM_EXT_BUCK_ISO_BIT);
+	writel_relaxed(reg_buck_iso_val, reg_buck_iso_addr);
+
+	reg_buck_iso_val = readl_relaxed(reg_buck_iso_addr);
+	reg_buck_iso_val &= ~(1UL << SPM_AOC_VMM_SRAM_ISO_DIN_BIT);
+	writel_relaxed(reg_buck_iso_val, reg_buck_iso_addr);
+
+	reg_buck_iso_val = readl_relaxed(reg_buck_iso_addr);
+	reg_buck_iso_val &= ~(1UL << SPM_AOC_VMM_SRAM_LATCH_ENB);
+	writel_relaxed(reg_buck_iso_val, reg_buck_iso_addr);
+}
+
+static void vmm_buck_isolation_on(void __iomem *base)
+{
+	void __iomem *reg_buck_iso_addr = base + SPM_VMM_ISO_REG_OFST;
+	u32 reg_buck_iso_val;
+
+	reg_buck_iso_val = readl_relaxed(reg_buck_iso_addr);
+	reg_buck_iso_val |= (1UL << SPM_AOC_VMM_SRAM_LATCH_ENB);
+	writel_relaxed(reg_buck_iso_val, reg_buck_iso_addr);
+
+	reg_buck_iso_val = readl_relaxed(reg_buck_iso_addr);
+	reg_buck_iso_val |= (1UL << SPM_AOC_VMM_SRAM_ISO_DIN_BIT);
+	writel_relaxed(reg_buck_iso_val, reg_buck_iso_addr);
+
+	reg_buck_iso_val = readl_relaxed(reg_buck_iso_addr);
+	reg_buck_iso_val |= (1UL << SPM_VMM_EXT_BUCK_ISO_BIT);
+	writel_relaxed(reg_buck_iso_val, reg_buck_iso_addr);
+}
+
 static int vmm_enable_regulator(struct regulator_dev *rdev)
 {
 	struct vmm_regulator *regulator;
+	struct dvfs_driver_data *drv_data;
 
 	ISP_LOGI("Enable vmm regulator");
 	regulator = rdev_get_drvdata(rdev);
@@ -328,6 +371,15 @@ static int vmm_enable_regulator(struct regulator_dev *rdev)
 		ISP_LOGE("rdev_get_drvdata ptr is null");
 		return PTR_ERR(regulator);
 	}
+
+	drv_data = regulator->dvfs_data;
+	if (!drv_data) {
+		ISP_LOGE("drv_data ptr is null");
+		return PTR_ERR(drv_data);
+	}
+
+	regulator_enable(vmm_reg);
+	vmm_buck_isolation_off(drv_data->spm_reg);
 
 	regulator->is_enable = 1;
 
@@ -337,6 +389,7 @@ static int vmm_enable_regulator(struct regulator_dev *rdev)
 static int vmm_disable_regulator(struct regulator_dev *rdev)
 {
 	struct vmm_regulator *regulator;
+	struct dvfs_driver_data *drv_data;
 
 	ISP_LOGI("Disable vmm regulator");
 	regulator = rdev_get_drvdata(rdev);
@@ -344,6 +397,15 @@ static int vmm_disable_regulator(struct regulator_dev *rdev)
 		ISP_LOGE("rdev_get_drvdata ptr is null");
 		return PTR_ERR(regulator);
 	}
+
+	drv_data = regulator->dvfs_data;
+	if (!drv_data) {
+		ISP_LOGE("drv_data ptr is null");
+		return PTR_ERR(drv_data);
+	}
+
+	vmm_buck_isolation_on(drv_data->spm_reg);
+	regulator_disable(vmm_reg);
 
 	regulator->is_enable = 0;
 	return 0;
@@ -409,6 +471,7 @@ static int vmm_regulator_probe(struct platform_device *pdev)
 	struct dentry *dentry;
 	struct ispdvfs_dbg_data *dbg_data;
 	u32 opp_level = 0;
+	struct resource *res;
 
 	match = of_match_node(mtk_vmm_regulator_match, dev->of_node);
 	if (!match) {
@@ -455,6 +518,19 @@ static int vmm_regulator_probe(struct platform_device *pdev)
 		num_mux++;
 	}
 	dvfs_data->num_muxes = num_mux;
+
+	/* SPM registers */
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	if (!res) {
+		ISP_LOGE("fail to get resource SPM_BASE");
+		return -EINVAL;
+	}
+
+	dvfs_data->spm_reg = devm_ioremap(dev, res->start, resource_size(res));
+	if (!(dvfs_data->spm_reg)) {
+		ISP_LOGE("fail to ioremap SPM_BASE: 0x%llx", res->start);
+		return -EINVAL;
+	}
 
 	/* Real regualtor instance which controls vmm directly */
 	vmm_reg = devm_regulator_get(dev, "buck-vmm");
