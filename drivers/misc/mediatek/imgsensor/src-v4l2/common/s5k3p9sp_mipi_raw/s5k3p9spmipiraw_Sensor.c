1ee96afdc10f1f5f651ed0fc7b52bccef3256482
ds901589 <ds_yao-kai.liu@mediatek.com>
2021-09-09 16:22:33 +0800

    [ALPS06111596] fix: s5k3p9 power on seqence
     delay for mclk
    
    MTK-Commit-Id: 83a4a083de9c2a0c524ccc57b4282b6f83791251
    
    Change-Id: I40284a00cbfffae140ede122e3587c2bd5a798f0
    Signed-off-by: ds901589 <ds_yao-kai.liu@mediatek.com>
    CR-Id: ALPS06111596
    Feature: [Android Default] Camera Application Basic Functions

--- drivers/misc/mediatek/imgsensor/src-v4l2/common/s5k3p9sp_mipi_raw/s5k3p9spmipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src-v4l2/common/s5k3p9sp_mipi_raw/s5k3p9spmipiraw_Sensor.c
@@ -1711,7 +1711,7 @@ static struct subdrv_pw_seq_entry pw_seq[] = {
 	{HW_ID_RST, 0, 1},
 	{HW_ID_DVDD, 1100000, 1},
 	{HW_ID_AVDD, 2800000, 1},
-	{HW_ID_DOVDD, 1800000, 0},
+	{HW_ID_DOVDD, 1800000, 3},
 	{HW_ID_MCLK_DRIVING_CURRENT, 2, 0},
 	{HW_ID_RST, 1, 2}
 };
