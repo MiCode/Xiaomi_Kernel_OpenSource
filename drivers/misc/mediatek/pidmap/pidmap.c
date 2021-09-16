21f73d1d3f17e8bf33de2dcf95580bbecfa5b61b
Ed Tsai <ed.tsai@mediatek.com>
2021-09-16 13:18:23 +0800

    [ALPS05817601] pidmap: use the new name for rename case
    probe_task_rename() hook at the rename event but it doesnt use the new
    one passed from the event. Use it instead of the task->comm which is the
    old name.
    
    MTK-Commit-Id: 44437ec5f2a4213c31597e0f9ee2ac6c92f5853c
    
    CR-Id: ALPS05817601
    Feature: Storage I/O Performance Profiling Tool(blocktag)
    Signed-off-by: Ed Tsai <ed.tsai@mediatek.com>
    Change-Id: Ic99fa5b80542e77213acbc6abc66a8a4645b4c3f

--- drivers/misc/mediatek/pidmap/pidmap.c
+++ drivers/misc/mediatek/pidmap/pidmap.c
@@ -47,7 +47,7 @@ struct tracepoints_table {
  *
  * task: current task
  */
-static void mtk_pidmap_update(struct task_struct *task)
+static void mtk_pidmap_update(struct task_struct *task, const char *task_comm)
 {
 	char *name;
 	int len;
@@ -67,7 +67,7 @@ static void mtk_pidmap_update(struct task_struct *task)
 	name = mtk_pidmap + ((pid - 1) * PIDMAP_ENTRY_SIZE);
 
 	/* copy task name */
-	memcpy(name, task->comm, PIDMAP_TASKNAME_SIZE);
+	memcpy(name, task_comm, PIDMAP_TASKNAME_SIZE);
 	*(name + PIDMAP_TASKNAME_SIZE) = '\0';
 
 	/* clear garbage tail chars to help parsers */
@@ -87,13 +87,13 @@ static void mtk_pidmap_update(struct task_struct *task)
 static void probe_task_rename(void *data, struct task_struct *task,
 			      const char *comm)
 {
-	mtk_pidmap_update(task);
+	mtk_pidmap_update(task, comm);
 }
 
 static void probe_task_newtask(void *data, struct task_struct *task,
 			       unsigned long clone_flags)
 {
-	mtk_pidmap_update(task);
+	mtk_pidmap_update(task, task->comm);
 }
 
 static struct tracepoints_table interests[] = {
