159443ee786c25fcc0b5bdeedea934ca52182201
Eric Lin <tesheng.lin@mediatek.com>
2021-09-16 13:17:43 +0800

    [ALPS05788102] Infra fake engine: More detail control
    Add more detail control for fake engine
    
    [Test]
    echo channel_id,en,wr_en,rd_en,latency >
    /sys/bus/platform/drivers/emi_fake_eng_drv/emi_fake_eng
    
    MTK-Commit-Id: 058136ccac38defc1f1eefd1b5b59c23924084b4
    
    Change-Id: I5d83845e5392d9f04be5faf39a074cfee26cd333
    Signed-off-by: Eric Lin <tesheng.lin@mediatek.com>
    CR-Id: ALPS05788102
    Feature: External Memory Interface(emi)

--- drivers/memory/mediatek/emi-fake-eng.c
+++ drivers/memory/mediatek/emi-fake-eng.c
@@ -44,7 +44,7 @@
 #define FAKE_ENG_START_ADDR_RD_2ND	0xc0
 #define FAKE_ENG_ADDR_RD			0xc4
 
-#define SIZE (4096*15)
+#define SIZE 256
 
 struct fake_eng_set {
 	unsigned int chn_number;
@@ -89,26 +89,22 @@ struct emi_fake_eng {
 	void __iomem **fake_eng_base;
 	void **k_addr;
 	unsigned int emi_fake_eng_cnt;
-	unsigned int start;
-	unsigned int flag;
+	unsigned int bitmap;
 	struct fake_eng_set feng_arg;
 };
 
 /* global pointer for sysfs operations*/
 static struct emi_fake_eng *fakeng;
 
-static int fake_eng_init(void)
+static int fake_eng_init(unsigned int chn_id)
 {
 	unsigned long phy_addr;
-	unsigned int i;
 	u32 val;
-	/* 4 channel chn_num correspond to fake engine */
-	unsigned int chn_num[4] = {0, 2, 1, 3};
 
-	/* Basic settign for fake engine */
+	/* Basic setting for fake engine */
 	fakeng->feng_arg.chn_number = 0;
 	fakeng->feng_arg.rd_dis = 1;
-	fakeng->feng_arg.wr_dis = 0;
+	fakeng->feng_arg.wr_dis = 1;
 	fakeng->feng_arg.loop_en = 0;
 	fakeng->feng_arg.cross_rk_en = 0;
 	fakeng->feng_arg.data_cmp_en = 0;
@@ -131,7 +127,7 @@ static int fake_eng_init(void)
 	fakeng->feng_arg.start_addr_rd_extend = 0x00000000;
 	fakeng->feng_arg.start_addr_wr_2nd_extend = 0x0;
 	fakeng->feng_arg.start_addr_rd_2nd_extend = 0x0;
-	fakeng->feng_arg.addr_offset1 = 0;
+	fakeng->feng_arg.addr_offset1 = 0x60;
 	fakeng->feng_arg.addr_offset2 = 0;
 	fakeng->feng_arg.init_pat0 = 0x0000ffff;
 	fakeng->feng_arg.init_pat1 = 0x0000ffff;
@@ -143,272 +139,282 @@ static int fake_eng_init(void)
 	fakeng->feng_arg.init_pat7 = 0xffffffff;
 	fakeng->feng_arg.freeze_en = 0;
 
-	for (i = 0; i < fakeng->emi_fake_eng_cnt; ++i) {
-		/*
-		 * 2 channel chn_num sequence corresponding
-		 * to fake engine is 0 1; 4 channel is 0 2 1 3
-		 */
-		if ((i == 1) && (fakeng->emi_fake_eng_cnt == 2))
-			fakeng->feng_arg.chn_number = chn_num[i] - 1;
-		else
-			fakeng->feng_arg.chn_number = chn_num[i];
+	fakeng->feng_arg.chn_number = chn_id;
 
-		fakeng->k_addr[i] = kmalloc(SIZE, GFP_KERNEL);
-		if (!fakeng->k_addr[i])
-			return -ENOMEM;
+	fakeng->k_addr[chn_id] = kzalloc(PAGE_SIZE * SIZE, GFP_KERNEL | GFP_DMA);
+	if (!fakeng->k_addr[chn_id])
+		return -ENOMEM;
 
-		phy_addr = virt_to_phys(fakeng->k_addr[i]);
-		fakeng->feng_arg.start_addr_wr = phy_addr;
-		fakeng->feng_arg.start_addr_rd = phy_addr;
-		fakeng->feng_arg.start_addr_wr_2nd = phy_addr;
-		fakeng->feng_arg.start_addr_rd_2nd = phy_addr;
+	phy_addr = virt_to_phys(fakeng->k_addr[chn_id]);
+	fakeng->feng_arg.start_addr_wr = phy_addr;
+	fakeng->feng_arg.start_addr_rd = phy_addr;
+	fakeng->feng_arg.start_addr_wr_2nd = phy_addr;
+	fakeng->feng_arg.start_addr_rd_2nd = phy_addr;
 
-		/* Disable fake engine*/
-		writel(0x0, fakeng->fake_eng_base[i] + FAKE_ENG_EN);
-		/* Reset fake engine*/
-		writel(0x1, fakeng->fake_eng_base[i] + FAKE_ENG_RST);
-		writel(0x0, fakeng->fake_eng_base[i] + FAKE_ENG_RST);
+	/* Disable fake engine*/
+	writel(0x0, fakeng->fake_eng_base[chn_id] + FAKE_ENG_EN);
+	/* Reset fake engine*/
+	writel(0x1, fakeng->fake_eng_base[chn_id] + FAKE_ENG_RST);
+	writel(0x0, fakeng->fake_eng_base[chn_id] + FAKE_ENG_RST);
 
-		/* FAKE_ENG_CON0 Mode enable control */
-		writel((fakeng->feng_arg.rd_wr_joint_en << 17) |
-				(fakeng->feng_arg.pat_mode << 13) |
-				(fakeng->feng_arg.data_cmp_en << 4) |
-				(fakeng->feng_arg.cross_rk_en << 3) |
-				(fakeng->feng_arg.loop_en << 2) |
-				(fakeng->feng_arg.wr_dis << 1) |
-				(fakeng->feng_arg.rd_dis << 0),
-				fakeng->fake_eng_base[i] + FAKE_ENG_CON0);
+	/* FAKE_ENG_CON0 Mode enable control */
+	writel((fakeng->feng_arg.rd_wr_joint_en << 17) |
+			(fakeng->feng_arg.pat_mode << 13) |
+			(fakeng->feng_arg.data_cmp_en << 4) |
+			(fakeng->feng_arg.cross_rk_en << 3) |
+			(fakeng->feng_arg.loop_en << 2) |
+			(fakeng->feng_arg.wr_dis << 1) |
+			(fakeng->feng_arg.rd_dis << 0),
+			fakeng->fake_eng_base[chn_id] + FAKE_ENG_CON0);
 
-		/* FAKE_ENG_CON1 Command control */
-		writel((fakeng->feng_arg.slow_down_grp << 20) |
-				(fakeng->feng_arg.slow_down << 10) |
-				(fakeng->feng_arg.wr_amount << 5) |
-				(fakeng->feng_arg.rd_amount << 0),
-				fakeng->fake_eng_base[i] + FAKE_ENG_CON1);
+	/* FAKE_ENG_CON1 Command control */
+	writel((fakeng->feng_arg.slow_down_grp << 20) |
+			(fakeng->feng_arg.slow_down << 10) |
+			(fakeng->feng_arg.wr_amount << 5) |
+			(fakeng->feng_arg.rd_amount << 0),
+			fakeng->fake_eng_base[chn_id] + FAKE_ENG_CON1);
 
-		/* FAKE_ENG_CON2 AXI protocal */
-		writel((fakeng->feng_arg.ar_slc << 19) |
-				(fakeng->feng_arg.aw_slc << 14) |
-				(fakeng->feng_arg.burst_size << 4) |
-				(fakeng->feng_arg.burst_len << 0),
-				fakeng->fake_eng_base[i] + FAKE_ENG_CON2);
+	/* FAKE_ENG_CON2 AXI protocal */
+	writel((fakeng->feng_arg.ar_slc << 19) |
+			(fakeng->feng_arg.aw_slc << 14) |
+			(fakeng->feng_arg.burst_size << 4) |
+			(fakeng->feng_arg.burst_len << 0),
+			fakeng->fake_eng_base[chn_id] + FAKE_ENG_CON2);
 
-		/* FAKE_ENG_CON3 Command control */
-		writel(fakeng->feng_arg.grp_aomunt,
-				fakeng->fake_eng_base[i] + FAKE_ENG_CON3);
+	/* FAKE_ENG_CON3 Command control */
+	writel(fakeng->feng_arg.grp_aomunt,
+			fakeng->fake_eng_base[chn_id] + FAKE_ENG_CON3);
 
-		/* FAKE_EN_START_ADDR */
-		writel(fakeng->feng_arg.start_addr_wr,
-				fakeng->fake_eng_base[i] + FAKE_ENG_START_ADDR);
+	/* FAKE_EN_START_ADDR */
+	writel(fakeng->feng_arg.start_addr_wr,
+			fakeng->fake_eng_base[chn_id] + FAKE_ENG_START_ADDR);
 
-		/* FAKE_EN_START_ADDR_RD */
-		writel(fakeng->feng_arg.start_addr_rd,
-				fakeng->fake_eng_base[i] + FAKE_ENG_START_ADDR_RD);
+	/* FAKE_EN_START_ADDR_RD */
+	writel(fakeng->feng_arg.start_addr_rd,
+			fakeng->fake_eng_base[chn_id] + FAKE_ENG_START_ADDR_RD);
 
-		/* FAKE_EN_START_ADDR_2ND */
-		writel(fakeng->feng_arg.start_addr_wr_2nd,
-				fakeng->fake_eng_base[i] + FAKE_ENG_START_ADDR_2ND);
+	/* FAKE_EN_START_ADDR_2ND */
+	writel(fakeng->feng_arg.start_addr_wr_2nd,
+			fakeng->fake_eng_base[chn_id] + FAKE_ENG_START_ADDR_2ND);
 
-		/* FAKE_EN_START_ADDR_RD_2ND */
-		writel(fakeng->feng_arg.start_addr_rd_2nd,
-				fakeng->fake_eng_base[i] + FAKE_ENG_START_ADDR_RD_2ND);
+	/* FAKE_EN_START_ADDR_RD_2ND */
+	writel(fakeng->feng_arg.start_addr_rd_2nd,
+			fakeng->fake_eng_base[chn_id] + FAKE_ENG_START_ADDR_RD_2ND);
 
-		/* FAKE_ENG_ADDR */
-		writel((fakeng->feng_arg.addr_offset2 << 18) |
-				(fakeng->feng_arg.addr_offset1 << 8) |
-				(fakeng->feng_arg.start_addr_wr_2nd_extend << 4) |
-				(fakeng->feng_arg.start_addr_wr_extend << 0),
-				fakeng->fake_eng_base[i] + FAKE_ENG_ADDR);
+	/* FAKE_ENG_ADDR */
+	writel((fakeng->feng_arg.addr_offset2 << 18) |
+			(fakeng->feng_arg.addr_offset1 << 8) |
+			(fakeng->feng_arg.start_addr_wr_2nd_extend << 4) |
+			(fakeng->feng_arg.start_addr_wr_extend << 0),
+			fakeng->fake_eng_base[chn_id] + FAKE_ENG_ADDR);
 
-		/* FAKE_ENG_ADDR_RD */
-		writel((fakeng->feng_arg.start_addr_rd_2nd_extend << 4) |
-				(fakeng->feng_arg.start_addr_rd_extend << 0),
-				fakeng->fake_eng_base[i] + FAKE_ENG_ADDR_RD);
+	/* FAKE_ENG_ADDR_RD */
+	writel((fakeng->feng_arg.start_addr_rd_2nd_extend << 4) |
+			(fakeng->feng_arg.start_addr_rd_extend << 0),
+			fakeng->fake_eng_base[chn_id] + FAKE_ENG_ADDR_RD);
 
-		/* INIT_PAT0 */
-		writel(fakeng->feng_arg.init_pat0,
-				fakeng->fake_eng_base[i] + FAKE_ENG_INIT_PAT0);
+	/* INIT_PAT0 */
+	writel(fakeng->feng_arg.init_pat0,
+			fakeng->fake_eng_base[chn_id] + FAKE_ENG_INIT_PAT0);
 
-		/* INIT_PAT1 */
-		writel(fakeng->feng_arg.init_pat1,
-				fakeng->fake_eng_base[i] + FAKE_ENG_INIT_PAT1);
+	/* INIT_PAT1 */
+	writel(fakeng->feng_arg.init_pat1,
+			fakeng->fake_eng_base[chn_id] + FAKE_ENG_INIT_PAT1);
 
-		/* INIT_PAT2 */
-		writel(fakeng->feng_arg.init_pat2,
-				fakeng->fake_eng_base[i] + FAKE_ENG_INIT_PAT2);
+	/* INIT_PAT2 */
+	writel(fakeng->feng_arg.init_pat2,
+			fakeng->fake_eng_base[chn_id] + FAKE_ENG_INIT_PAT2);
 
-		/* INIT_PAT3 */
-		writel(fakeng->feng_arg.init_pat3,
-				fakeng->fake_eng_base[i] + FAKE_ENG_INIT_PAT3);
+	/* INIT_PAT3 */
+	writel(fakeng->feng_arg.init_pat3,
+			fakeng->fake_eng_base[chn_id] + FAKE_ENG_INIT_PAT3);
 
-		/* INIT_PAT4 */
-		writel(fakeng->feng_arg.init_pat4,
-				fakeng->fake_eng_base[i] + FAKE_ENG_INIT_PAT4);
+	/* INIT_PAT4 */
+	writel(fakeng->feng_arg.init_pat4,
+			fakeng->fake_eng_base[chn_id] + FAKE_ENG_INIT_PAT4);
 
-		/* INIT_PAT5 */
-		writel(fakeng->feng_arg.init_pat5,
-				fakeng->fake_eng_base[i] + FAKE_ENG_INIT_PAT5);
+	/* INIT_PAT5 */
+	writel(fakeng->feng_arg.init_pat5,
+			fakeng->fake_eng_base[chn_id] + FAKE_ENG_INIT_PAT5);
 
-		/* INIT_PAT6 */
-		writel(fakeng->feng_arg.init_pat6,
-				fakeng->fake_eng_base[i] + FAKE_ENG_INIT_PAT6);
+	/* INIT_PAT6 */
+	writel(fakeng->feng_arg.init_pat6,
+			fakeng->fake_eng_base[chn_id] + FAKE_ENG_INIT_PAT6);
 
-		/* INIT_PAT7 */
-		writel(fakeng->feng_arg.init_pat7,
-				fakeng->fake_eng_base[i] + FAKE_ENG_INIT_PAT7);
+	/* INIT_PAT7 */
+	writel(fakeng->feng_arg.init_pat7,
+			fakeng->fake_eng_base[chn_id] + FAKE_ENG_INIT_PAT7);
 
-		/* compare result freeze */
-		writel(fakeng->feng_arg.freeze_en,
-				fakeng->fake_eng_base[i] + FAKE_ENG_FREEZE_RESULT);
+	/* compare result freeze */
+	writel(fakeng->feng_arg.freeze_en,
+			fakeng->fake_eng_base[chn_id] + FAKE_ENG_FREEZE_RESULT);
 
-		/* HASH */
-		val = (0x0 << 24) | (0x1 << 20) | (0x1 << 16) | (0x2 << 12) |
-				(0x1 << 8) | (fakeng->feng_arg.chn_number << 4) | 0x1;
-		writel(val, fakeng->fake_eng_base[i] + FAKE_ENG_HASH);
+	/* HASH */
+	val = (0x0 << 24) | (0x1 << 20) | (0x1 << 16) | (0x2 << 12) |
+			(0x1 << 8) | (fakeng->feng_arg.chn_number << 4) | 0x1;
+	writel(val, fakeng->fake_eng_base[chn_id] + FAKE_ENG_HASH);
 
-	}
 	return 0;
 }
 
-static void emi_fake_eng_stop(void)
+static void emi_fake_eng_stop(unsigned int chn_id)
 {
-	unsigned int i, val;
+	unsigned int val;
 
-	if (!fakeng->flag) {
-		pr_info("%s: fake eng no start, do nothing here\n", __func__);
+	if (!(fakeng->bitmap & (0x1 << chn_id))) {
+		pr_info("%s: fake eng:%d no start, do nothing here\n", __func__, chn_id);
 		return;
 	}
-	/* stop loop mode */
-	for (i = 0; i < fakeng->emi_fake_eng_cnt; ++i) {
-		/* set loop_en = 0 */
-		val = readl(fakeng->fake_eng_base[i] + FAKE_ENG_CON0);
-		val &= ~(0x1 << 2);
-		writel(val, fakeng->fake_eng_base[i] + FAKE_ENG_CON0);
 
-		/* wait for loop mode disable*/
-		udelay(5);
-	}
+	/* set loop_en = 0 */
+	val = readl(fakeng->fake_eng_base[chn_id] + FAKE_ENG_CON0);
+	val &= ~(0x1 << 2);
+	writel(val, fakeng->fake_eng_base[chn_id] + FAKE_ENG_CON0);
+	/* wait for loop mode disable*/
+	udelay(5);
 
     /* Wait fake engine done */
-	for (i = 0; i < fakeng->emi_fake_eng_cnt; ++i) {
-		while (1) {
-			val = readl(fakeng->fake_eng_base[i] + FAKE_ENG_DONE);
-			pr_debug_ratelimited("%s: fake eng %d, val: %d\n",
-								__func__, i, val);
+	while (1) {
+		val = readl(fakeng->fake_eng_base[chn_id] + FAKE_ENG_DONE);
+		pr_debug_ratelimited("%s: fake eng %d, val: %d\n",
+							__func__, chn_id, val);
 
-			if (val)
-				break;
-		}
-	}
-	pr_info("%s: check fake eng done\n", __func__);
-
-	for (i = 0; i < fakeng->emi_fake_eng_cnt; ++i) {
-		/* Disable fake engine*/
-		writel(0x0, fakeng->fake_eng_base[i] + FAKE_ENG_EN);
-		/* Reset fake engine*/
-		writel(0x1, fakeng->fake_eng_base[i] + FAKE_ENG_RST);
-		writel(0x0, fakeng->fake_eng_base[i] + FAKE_ENG_RST);
-
-		/* free k_addr */
-		kfree(fakeng->k_addr[i]);
+		if (val)
+			break;
 	}
 
-	fakeng->flag = 0;
+	/* Disable fake engine */
+	writel(0x0, fakeng->fake_eng_base[chn_id] + FAKE_ENG_EN);
+	/* Reset fake engine */
+	writel(0x1, fakeng->fake_eng_base[chn_id] + FAKE_ENG_RST);
+	writel(0x0, fakeng->fake_eng_base[chn_id] + FAKE_ENG_RST);
+
+	/* free k_addr */
+	kfree(fakeng->k_addr[chn_id]);
+
+	/* Clear chn_id */
+	fakeng->bitmap &= ~(0x1 << chn_id);
+
+	pr_info("%s: disable fake eng:%d done, fake eng bitmap:0x%x\n",
+				__func__, chn_id, fakeng->bitmap);
 }
 
-static void emi_fake_eng_start(u32 start)
+static void emi_fake_eng_start(unsigned int chn_id,
+								unsigned int wr_en,
+								unsigned int rd_en,
+								unsigned int latency)
 {
 	u32 loop_en = 1;
 	u32 slow_down, slow_down_grp, val;
-	unsigned int i;
 	int ret;
 
-	if (fakeng->flag) {
-		pr_info("%s: Please use echo 0 to close fake engine first\n", __func__);
+	if (fakeng->bitmap & (0x1 << chn_id)) {
+		pr_info("%s:Please disable fake eng:%d first\n", __func__, chn_id);
 		return;
 	}
 
+	/* set chn_id bitmap */
+	fakeng->bitmap |= (0x1 << chn_id);
+
 	/* Fake engine basic setting */
-	ret = fake_eng_init();
+	ret = fake_eng_init(chn_id);
 	if (ret) {
 		pr_info("%s: Cannot allocate memory, stop trigger fake engine\n", __func__);
 		return;
 	}
 
-	/* set flag true */
-	fakeng->flag = 1;
-
-	if (start == 1)
-		start = 0;
-
-	/*set slow_down */
-	slow_down = start;
+	/* set slow_down */
+	slow_down = latency;
 
 	/* Start to incease BW,  */
 	slow_down_grp = slow_down;
 
-	/* fake engine slow down setting */
-	for (i = 0; i < fakeng->emi_fake_eng_cnt; ++i) {
-		/* Disable fake engine*/
-		writel(0x0, fakeng->fake_eng_base[i] + FAKE_ENG_EN);
-		/* Reset fake engine*/
-		writel(0x1, fakeng->fake_eng_base[i] + FAKE_ENG_RST);
-		writel(0x0, fakeng->fake_eng_base[i] + FAKE_ENG_RST);
-
+	if (rd_en) {
 		/* set rd_dis = 0 to enable read command*/
-		val = readl(fakeng->fake_eng_base[i] + FAKE_ENG_CON0);
+		val = readl(fakeng->fake_eng_base[chn_id] + FAKE_ENG_CON0);
 		val &= ~(0x1 << 0);
-		writel(val, fakeng->fake_eng_base[i] + FAKE_ENG_CON0);
-
-		/* set slow_down = 0 */
-		val = readl(fakeng->fake_eng_base[i] + FAKE_ENG_CON1);
-		val &= ~(0xfffff << 10);
-		writel(val, fakeng->fake_eng_base[i] + FAKE_ENG_CON1);
-
-		/* set slow_down */
-		val = readl(fakeng->fake_eng_base[i] + FAKE_ENG_CON1);
-		val |= (slow_down_grp << 20) | (slow_down << 10);
-		writel(val, fakeng->fake_eng_base[i] + FAKE_ENG_CON1);
-
-		/* set loop_en = 1 */
-		val = readl(fakeng->fake_eng_base[i] + FAKE_ENG_CON0);
-		val |= (loop_en << 2);
-		writel(val, fakeng->fake_eng_base[i] + FAKE_ENG_CON0);
+		writel(val, fakeng->fake_eng_base[chn_id] + FAKE_ENG_CON0);
 	}
 
-	/* Enable fake engine */
-	for (i = 0 ; i < fakeng->emi_fake_eng_cnt; ++i)
-		writel(0x1, fakeng->fake_eng_base[i] + FAKE_ENG_EN);
+	if (wr_en) {
+		/* set wr_dis = 0 to enable write command*/
+		val = readl(fakeng->fake_eng_base[chn_id] + FAKE_ENG_CON0);
+		val &= ~(0x1 << 1);
+		writel(val, fakeng->fake_eng_base[chn_id] + FAKE_ENG_CON0);
+	}
 
-	pr_info("%s: enable fake eng slow_down: %d\n", __func__, slow_down);
+	/* set slow_down = 0 */
+	val = readl(fakeng->fake_eng_base[chn_id] + FAKE_ENG_CON1);
+	val &= ~(0xfffff << 10);
+	writel(val, fakeng->fake_eng_base[chn_id] + FAKE_ENG_CON1);
+
+	/* set slow_down */
+	val = readl(fakeng->fake_eng_base[chn_id] + FAKE_ENG_CON1);
+	val |= (slow_down_grp << 20) | (slow_down << 10);
+	writel(val, fakeng->fake_eng_base[chn_id] + FAKE_ENG_CON1);
+
+	/* set loop_en = 1 */
+	val = readl(fakeng->fake_eng_base[chn_id] + FAKE_ENG_CON0);
+	val |= (loop_en << 2);
+	writel(val, fakeng->fake_eng_base[chn_id] + FAKE_ENG_CON0);
+
+	/* Enable fake engine */
+	writel(0x1, fakeng->fake_eng_base[chn_id] + FAKE_ENG_EN);
+
+	pr_info("%s: chn_id:%d, wr_en:%d, rd_en:%d, latency:%d\n",
+			__func__, chn_id, wr_en, rd_en, slow_down);
+	pr_info("%s: fake eng bitmap:0x%x\n", __func__, fakeng->bitmap);
 }
 
 static ssize_t emi_fake_eng_show(struct device_driver *driver, char *buf)
 {
-	return scnprintf(buf, PAGE_SIZE, "%d\n", fakeng->start);
+	return scnprintf(buf, PAGE_SIZE, "0x%x\n", fakeng->bitmap);
 }
 
 static ssize_t emi_fake_eng_store
 	(struct device_driver *driver, const char *buf, size_t count)
 {
-	u32 start;
-	int ret;
+	unsigned int chn_id, en, wr_en, rd_en, latency;
+	int ret = 0, n;
 
-	ret = kstrtou32(buf, 10, &start);
-	if (ret)
-		return ret;
+	n = sscanf(buf, "%u,%u,%u,%u,%u\n",
+				&chn_id, &en, &wr_en, &rd_en, &latency);
 
-	fakeng->start = start;
-	pr_info("%s start: %d\n", __func__, start);
+	if (n != 5) {
+		pr_info("%s: Please enter correct input format. n=%d\n", __func__, n);
+		goto out;
+	}
 
-	if (start > 0 && start <= 100)
-		emi_fake_eng_start(start);
-	else if (start == 0)
-		emi_fake_eng_stop();
+	pr_info("%s: %u %u %u %u %u\n",
+				__func__, chn_id, en, wr_en, rd_en, latency);
+
+	if (chn_id >= fakeng->emi_fake_eng_cnt) {
+		pr_info("%s: Please enter channel number between 0 ~ %d\n",
+					__func__, fakeng->emi_fake_eng_cnt - 1);
+		goto out;
+	}
+
+	if (en > 1 || wr_en > 1 || rd_en > 1) {
+		pr_info("%s: Please enter en, wr_en, rd_en value for 0 or 1\n", __func__);
+		goto out;
+	}
+
+	if (wr_en == 0 && rd_en == 0) {
+		pr_info("%s: At least one of wr_en or rd_en should be 1\n", __func__);
+		goto out;
+	}
+
+	if (latency > 100) {
+		pr_info("%s: Please enter latency value between 1 ~ 100\n", __func__);
+		goto out;
+	}
+
+	if (en)
+		emi_fake_eng_start(chn_id, wr_en, rd_en, latency);
 	else
-		pr_info("%s: Please enter slow down value between 1 ~ 100\n", __func__);
+		emi_fake_eng_stop(chn_id);
 
-
+out:
 	return ret ? : count;
 }
 
@@ -460,12 +466,11 @@ static int emi_fake_eng_probe(struct platform_device *pdev)
 	if (!(feng->k_addr))
 		return -ENOMEM;
 
-
 	/* Set to global pointer */
 	fakeng = feng;
 
-	/* Set default flag value */
-	fakeng->flag = 0;
+	/* Initial channel bitmap */
+	fakeng->bitmap = 0;
 
 	return 0;
 }
