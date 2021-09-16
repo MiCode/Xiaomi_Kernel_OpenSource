4afa235269798a85f50f61e77cc5519198406093
mtk16965 <holiday.cheng@mediatek.com>
2021-09-16 13:17:33 +0800

    [ALPS05917394] GPU: DFD porting
    [Detail]
    1. Correct register setting
    2. Adjust the DFD flow
    
    MTK-Commit-Id: bcd18461d9f7e21766d5f1e38f19bbecaefec0b2
    
    CR-Id: ALPS05917394
    Feature: GPU Extension(gpu_extension)
    Signed-off-by: mtk16965 <holiday.cheng@mediatek.com>
    Change-Id: Ia7b480da5f5e2e3be6a3b9d2095157f424118953

--- drivers/gpu/mediatek/gpufreq/v2/include/gpudfd_mt6879.h
+++ drivers/gpu/mediatek/gpufreq/v2/include/gpudfd_mt6879.h
@@ -6,7 +6,7 @@
 #ifndef __GPUDFD_MT6879_H__
 #define __GPUDFD_MT6879_H__
 
-#define GPUDFD_ENABLE                (0)
+#define GPUDFD_ENABLE                (1)
 
 /*
  * MFGCFG_BASE        : 0x13FB_F000
