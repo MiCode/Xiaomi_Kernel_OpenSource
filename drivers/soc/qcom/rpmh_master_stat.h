e224659967e13ac2d14e9b82851d440e5db74516
Tushar Nimkar <tnimkar@codeaurora.org>
2021-09-16 15:09:49 +0530

    drivers: qcom: rpmh_master_stats: export API to get apss_master_stats info
    This change provides a API to get apss_master_stats info to other driver.
    In this way stats information can be reuse instead of maintaining again.
    
    Change-Id: Iea7d905cc71ac46b6b0d73617e94a89e15a1fec0
    Signed-off-by: Tushar Nimkar <tnimkar@codeaurora.org>

--- drivers/soc/qcom/rpmh_master_stat.h
+++ drivers/soc/qcom/rpmh_master_stat.h
@@ -1,18 +1,29 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
  */
 
 #ifndef __QCOM_RPM_STATS_LOG_H__
 #define __QCOM_RPM_STATS_LOG_H__
 
+struct msm_rpmh_master_stats {
+	uint32_t version_id;
+	uint32_t counts;
+	uint64_t last_entered;
+	uint64_t last_exited;
+	uint64_t accumulated_duration;
+};
 #if IS_ENABLED(CONFIG_QTI_RPM_STATS_LOG)
 
 void msm_rpmh_master_stats_update(void);
-
+struct msm_rpmh_master_stats *msm_rpmh_get_apss_data(void);
 #else
 
 static inline void msm_rpmh_master_stats_update(void) {}
+static inline struct msm_rpmh_master_stats *msm_rpmh_get_apss_data(void)
+{
+	return NULL;
+}
 
 #endif
 
