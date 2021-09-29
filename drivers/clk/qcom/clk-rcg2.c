08faf9323cc40ef4ede9a27b8468b6c9e17cfab2
Jagadeesh Kona <jkona@codeaurora.org>
2021-09-29 12:53:48 +0530

    clk: qcom: clk-rcg2: Update logic to calculate D value for RCG
    The current implementation does not check for D value is within
    the accepted range for a given M & N value. Update the logic to
    calculate the final D value based on the range. While at it, add
    support for 2/3 divider in frac_table_pixel.
    
    Change-Id: Ia42b9197de6d594129b1a48106f8e5a2be70a1c6
    Signed-off-by: Taniya Das <tdas@codeaurora.org>
    Signed-off-by: Jagadeesh Kona <jkona@codeaurora.org>

--- drivers/clk/qcom/clk-rcg2.c
+++ drivers/clk/qcom/clk-rcg2.c
@@ -431,7 +431,7 @@ static bool clk_rcg2_current_config(struct clk_rcg2 *rcg,
 
 static int __clk_rcg2_configure(struct clk_rcg2 *rcg, const struct freq_tbl *f)
 {
-	u32 cfg, mask;
+	u32 cfg, mask, d_val, not2d_val;
 	struct clk_hw *hw = &rcg->clkr.hw;
 	int ret, index = qcom_find_src_index(hw, rcg->parent_map, f->src);
 
@@ -450,8 +450,18 @@ static int __clk_rcg2_configure(struct clk_rcg2 *rcg, const struct freq_tbl *f)
 		if (ret)
 			return ret;
 
+		/* Calculate 2d value */
+		d_val = f->n;
+
+		if (d_val > ((f->n - f->m) * 2))
+			d_val = (f->n - f->m) * 2;
+		else if (d_val < f->m)
+			d_val = f->m;
+
+		not2d_val = ~d_val & mask;
+
 		ret = regmap_update_bits(rcg->clkr.regmap,
-				RCG_D_OFFSET(rcg), mask, ~f->n);
+				RCG_D_OFFSET(rcg), mask, not2d_val);
 		if (ret)
 			return ret;
 	}
@@ -1120,6 +1130,7 @@ static const struct frac_entry frac_table_pixel[] = {
 	{ 2, 9 },
 	{ 4, 9 },
 	{ 1, 1 },
+	{ 2, 3 },
 	{ }
 };
 
