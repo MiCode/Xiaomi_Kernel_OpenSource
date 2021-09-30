0534e5097e7ad23b85a7e57b19e32699944359dd
Josh Hsiao <josh.hsiao@mediatek.com>
2021-09-30 20:29:31 +0800

    [ALPS06122194] apusys power: all on
    [Detail]
    1. clksrc form apupll
    2. apusys power all on for bringup
    
    MTK-Commit-Id: 910616449de0b70a2d7584bdcaa2d41edb62e0cc
    
    Signed-off-by: Josh Hsiao <josh.hsiao@mediatek.com>
    CR-Id: ALPS06122194
    Feature: [Module] APUSYS
    Change-Id: I20f314a6e0bb2f77aff6f31ee5e0ce0adb245058

--- drivers/misc/mediatek/apusys/power/3.0/inc/platform/mt6895_apupwr.h
+++ drivers/misc/mediatek/apusys/power/3.0/inc/platform/mt6895_apupwr.h
@@ -9,11 +9,11 @@
 #include <linux/io.h>
 #include <linux/clk.h>
 
-#define APU_POWER_INIT		(1)	// 1: init in kernel ; 0: init in lk2
+#define APU_POWER_INIT		(0)	// 1: init in kernel ; 0: init in lk2
 #define APU_POWER_BRING_UP	(1)
-#define APU_PWR_SOC_PATH	(1)	// 1: do not run apu pll/acc init
-#define ENABLE_SW_BUCK_CTL	(1)	// 1: enable regulator in rpm resume
-#define ENABLE_SOC_CLK_MUX	(1)	// 1: enable soc clk in rpm resume
+#define APU_PWR_SOC_PATH	(0)	// 1: do not run apu pll/acc init
+#define ENABLE_SW_BUCK_CTL	(0)	// 1: enable regulator in rpm resume
+#define ENABLE_SOC_CLK_MUX	(0)	// 1: enable soc clk in rpm resume
 #define DEBUG_DUMP_REG		(1)	// dump overall apu registers for debug
 #define APMCU_REQ_RPC_SLEEP	(0)	// rpm suspend trigger sleep req to rpc
 
