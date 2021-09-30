237ebe8a479d001ba3f9dbf5b69b458f675dc8a0
mtk24296 <yc.li@mediatek.com>
2021-09-30 20:33:23 +0800

    [ALPS06177960] mmqos: mt6895 porting
    mmqos mt6895 porting
    
    MTK-Commit-Id: dabd6bd47a48ba209bb844aec7cef73e6699eeb8
    
    Signed-off-by: mtk24296 <yc.li@mediatek.com>
    Change-Id: Ia9a5c51a7c46f5828d10e52b972bb54579f2dfd3
    CR-Id: ALPS06177960
    Feature: [Module]MM pipeline/MM DVFS

--- drivers/misc/mediatek/mmqos/mmqos_wrapper.c
+++ drivers/misc/mediatek/mmqos/mmqos_wrapper.c
@@ -27,6 +27,10 @@ static struct wrapper_data wrapper_data_mt6853 = {
 	.max_ostd = 40,
 	.icc_dst_id = SLAVE_COMMON(0),
 };
+static struct wrapper_data wrapper_data_mt6895 = {
+	.max_ostd = 40,
+	.icc_dst_id = SLAVE_COMMON(0),
+};
 static struct wrapper_data *mmqos_wrapper;
 static struct device *dev;
 static BLOCKING_NOTIFIER_HEAD(hrt_bw_throttle_notifier);
@@ -201,6 +205,10 @@ static const struct of_device_id of_mmqos_wrapper_match_tbl[] = {
 	{
 		.compatible = "mediatek,mt6853-mmqos-wrapper",
 		.data = &wrapper_data_mt6853,
+	},
+	{
+		.compatible = "mediatek,mt6895-mmqos-wrapper",
+		.data = &wrapper_data_mt6895,
 	},
 	{}
 };
