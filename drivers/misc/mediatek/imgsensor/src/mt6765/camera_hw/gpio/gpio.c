2aacf9d15b49723608f62f78e16cbedec5e68826
MistyZheng <ot_misty.cheng@mediatek.com>
2021-09-30 20:27:57 +0800

    [ALPS06150508] camera: fix coverity 6762/6765
    fix Error Handling
    
    MTK-Commit-Id: 30c78fc207611b58fca4dd6ce24b0d89f7f62e1c
    
    CR-Id: ALPS06150508
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: MistyZheng <ot_misty.cheng@mediatek.com>
    Change-Id: Ic90e9ffe2424ed0372a7a5bdc18e409dd2103f27

--- drivers/misc/mediatek/imgsensor/src/mt6765/camera_hw/gpio/gpio.c
+++ drivers/misc/mediatek/imgsensor/src/mt6765/camera_hw/gpio/gpio.c
@@ -72,7 +72,7 @@ static enum IMGSENSOR_RETURN gpio_release(void *pinstance)
 }
 static enum IMGSENSOR_RETURN gpio_init(void *pinstance)
 {
-	int    i, j;
+	int    i, j, ret1 = 0;
 	struct platform_device *pplatform_dev = gpimgsensor_hw_platform_device;
 	struct GPIO            *pgpio         = (struct GPIO *)pinstance;
 	enum   IMGSENSOR_RETURN ret           = IMGSENSOR_RETURN_SUCCESS;
@@ -92,11 +92,14 @@ static enum IMGSENSOR_RETURN gpio_init(void *pinstance)
 			lookup_names =
 				gpio_pinctrl_list_cam[i].ppinctrl_lookup_names;
 			if (lookup_names) {
-				snprintf(str_pinctrl_name,
+				ret1 = snprintf(str_pinctrl_name,
 					sizeof(str_pinctrl_name),
 					"cam%d_%s",
 					j,
 					lookup_names);
+				if (ret1 < 0) {
+					pr_info("%s : snprintf error\n", __func__);
+				}
 				pgpio->ppinctrl_state_cam[j][i] =
 					pinctrl_lookup_state(
 					    pgpio->ppinctrl,
