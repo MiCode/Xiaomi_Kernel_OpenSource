e09c387028afb95fd2bb44a21be39479c38b0392
sunil.goyal <sunil.goyal@mediatek.com>
2021-09-30 20:27:29 +0800

    [ALPS06149046] ION: Dump ION usage in kernel logs
    Dump ION and GPU memory usage in kernel logs when OOM happens.
    
    MTK-Commit-Id: 5868d157ca07e481bbf118264a1a5c058cf7214b
    
    Change-Id: If973d2670789274aaa0ee7551389454c4d19dc93
    Signed-off-by: sunil.goyal <sunil.goyal@mediatek.com>
    CR-Id: ALPS06149046
    Feature: [Module]Kernel Memory Management

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -42,6 +42,14 @@
 #include <linux/init.h>
 #include <linux/mmu_notifier.h>
 
+#ifdef CONFIG_MTK_ION
+#include "mtk/ion_drv.h"
+#endif
+
+#if IS_ENABLED(CONFIG_MTK_GPU_SUPPORT)
+#include <mt-plat/mtk_gpu_utility.h>
+#endif
+
 #include <asm/tlb.h>
 #include "internal.h"
 #include "slab.h"
@@ -430,6 +438,18 @@ static void dump_tasks(struct mem_cgroup *memcg, const nodemask_t *nodemask)
 	rcu_read_unlock();
 }
 
+/* dump extra info: HW memory usage */
+static void oom_dump_extra_info(void)
+{
+#ifdef CONFIG_MTK_ION
+	ion_mm_heap_memory_detail();
+#endif
+#if IS_ENABLED(CONFIG_MTK_GPU_SUPPORT)
+	if (mtk_dump_gpu_memory_usage() == false)
+		pr_info("mtk_dump_gpu_memory_usage not support\n");
+#endif
+}
+
 static void dump_header(struct oom_control *oc, struct task_struct *p)
 {
 	pr_warn("%s invoked oom-killer: gfp_mask=%#x(%pGg), nodemask=%*pbl, order=%d, oom_score_adj=%hd\n",
@@ -450,6 +470,8 @@ static void dump_header(struct oom_control *oc, struct task_struct *p)
 	}
 	if (sysctl_oom_dump_tasks)
 		dump_tasks(oc->memcg, oc->nodemask);
+
+	oom_dump_extra_info();
 }
 
 /*
