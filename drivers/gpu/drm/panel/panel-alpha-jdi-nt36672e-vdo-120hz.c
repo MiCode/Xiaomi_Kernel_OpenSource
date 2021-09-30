975c37cbb5c4f2b72e9da7afeaed228113c1da43
jj chen <jj.chen@mediatek.com>
2021-09-30 20:33:00 +0800

    [ALPS06166822] disp: Fix nt36672e over 121hz issue
    Because dsi timcon formula has been refined,
    data rate must be fine tune from 1106 to 1098
    
    MTK-Commit-Id: 7adbf0301bc2da0a8316e2f978dd249453812d9a
    
    Signed-off-by: jj chen <jj.chen@mediatek.com>
    CR-Id: ALPS06166822
    Feature: [Module]Display Driver
    Change-Id: I960540bf182e46ae402f7321b08ef3d70a70dd14

--- drivers/gpu/drm/panel/panel-alpha-jdi-nt36672e-vdo-120hz.c
+++ drivers/gpu/drm/panel/panel-alpha-jdi-nt36672e-vdo-120hz.c
@@ -904,7 +904,7 @@ static const struct drm_display_mode performance_mode_120hz = {
 
 #if defined(CONFIG_MTK_PANEL_EXT)
 static struct mtk_panel_params ext_params = {
-	.pll_clk = 553,
+	.pll_clk = 549,
 	.vfp_low_power = 4180,
 	.cust_esd_check = 0,
 	.esd_check_enable = 1,
@@ -960,7 +960,7 @@ static struct mtk_panel_params ext_params = {
 		.rc_tgt_offset_hi = 3,
 		.rc_tgt_offset_lo = 3,
 		},
-	.data_rate = 1106,
+	.data_rate = 1098,
 	.lfr_enable = 0,
 	.lfr_minimum_fps = 60,
 	.dyn_fps = {
@@ -984,7 +984,7 @@ static struct mtk_panel_params ext_params = {
 };
 
 static struct mtk_panel_params ext_params_90hz = {
-	.pll_clk = 553,
+	.pll_clk = 549,
 	.vfp_low_power = 2528,
 	.cust_esd_check = 0,
 	.esd_check_enable = 1,
@@ -1040,7 +1040,7 @@ static struct mtk_panel_params ext_params_90hz = {
 		.rc_tgt_offset_hi = 3,
 		.rc_tgt_offset_lo = 3,
 		},
-	.data_rate = 1106,
+	.data_rate = 1098,
 	.lfr_enable = 0,
 	.lfr_minimum_fps = 60,
 	.dyn_fps = {
@@ -1064,7 +1064,7 @@ static struct mtk_panel_params ext_params_90hz = {
 };
 
 static struct mtk_panel_params ext_params_120hz = {
-	.pll_clk = 553,
+	.pll_clk = 549,
 	.vfp_low_power = 2528,
 	.cust_esd_check = 0,
 	.esd_check_enable = 1,
@@ -1120,7 +1120,7 @@ static struct mtk_panel_params ext_params_120hz = {
 		.rc_tgt_offset_hi = 3,
 		.rc_tgt_offset_lo = 3,
 		},
-	.data_rate = 1106,
+	.data_rate = 1098,
 	.lfr_enable = 0,
 	.lfr_minimum_fps = 60,
 	.dyn_fps = {
