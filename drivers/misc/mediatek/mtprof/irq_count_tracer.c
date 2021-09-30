748f1bb1c35e5eb28e801eab777761635f46da5e
ot_dick.yang <ot_dick.yang@mediatek.com>
2021-09-30 20:28:08 +0800

    [ALPS06162936] irq_mon: add whitelist
    Per owner provided information and calculation.
    Added folowing IRQs into whitelist with thresholds:
     {"DPMAIF_AP", 1837}, /* 544125 irqs per sec */ /data tput */
     {"CCIF_AP_DATA", 50000}, /* 20000 irqs per sec*/ /* MD EE */
    
    MTK-Commit-Id: 1dc60ba85fcb77343a57f734d7007f34cf124822
    
    Signed-off-by: ot_dick.yang <ot_dick.yang@mediatek.com>
    CR-Id: ALPS06162936
    Feature: [Module]Schedule Monitor
    Change-Id: I1f0fc27b4bef4801c76c4601e02bfc181aafc1b9

--- drivers/misc/mediatek/mtprof/irq_count_tracer.c
+++ drivers/misc/mediatek/mtprof/irq_count_tracer.c
@@ -48,6 +48,8 @@ struct irq_count_period_setting {
 	{"musb-hdrc", 16666}, /* 60000 irqs per sec*/
 	{"11201000.usb0", 16666}, /* 60000 irqs per sec*/
 	{"wlan0", 12500}, /* 80000 irqs per sec*/
+	{"DPMAIF_AP", 1837}, /* 544125 irqs per sec */ /* data tput */
+	{"CCIF_AP_DATA", 50000}, /* 20000 irqs per sec */ /* MD EE */
 };
 
 const char *irq_to_name(int irq)
