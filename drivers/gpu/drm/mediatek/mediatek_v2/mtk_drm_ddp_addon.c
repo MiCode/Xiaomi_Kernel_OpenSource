325743a1be6e54ca82610e6acd645975a5a073ea
tai-hua.tseng <tai-hua.tseng@mediatek.com>
2021-09-30 20:35:35 +0800

    [ALPS06158366] DISP: fix display false alarm log
    [Detail]
    Fix "crtc0 scn is wrong" false alarm log since dual pipe enable
    
    MTK-Commit-Id: 5a2ce95a27d933f79bc605e0450c6177159d68af
    
    CR-Id: ALPS06158366
    Feature: [Module]Display Driver
    Signed-off-by: tai-hua.tseng <tai-hua.tseng@mediatek.com>
    Change-Id: I87be7c251acf80c5c944d801cf70128a97c77fba

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_ddp_addon.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_ddp_addon.c
@@ -133,7 +133,7 @@ mtk_addon_get_scenario_data_dual(const char *source, struct drm_crtc *crtc,
 {
 	struct mtk_drm_crtc *mtk_crtc = to_mtk_crtc(crtc);
 
-	if ((scn < NONE) || (scn > TRIPLE_DISP)) {
+	if ((scn < NONE) || (scn > ADDON_SCN_NR)) {
 		DDPPR_ERR("[%s] crtc%d scn is wrong\n", source,
 				drm_crtc_index(crtc));
 		return NULL;
