fefea32a5839c34d9753d0943b23937a97734448
Cloud Chou <cloud.chou@mediatek.com>
2021-09-30 20:37:39 +0800

    [ALPS06105012] v4l2:add 6895 video driver
    add compatible for vcodec-enc & vcodec-dec
    
    MTK-Commit-Id: e97bea929aff096b0a1ff2085d18d259dd5351af
    
    CR-Id: ALPS06105012
    Feature: [Module]Video Driver
    Signed-off-by: Cloud Chou <cloud.chou@mediatek.com>
    Change-Id: I3375a67f1ef32ee0767a022eff874535f35cc905

--- drivers/media/platform/mtk-vcodec/mtk_vcodec_dec_drv.c
+++ drivers/media/platform/mtk-vcodec/mtk_vcodec_dec_drv.c
@@ -509,6 +509,7 @@ static const struct of_device_id mtk_vcodec_match[] = {
 	{.compatible = "mediatek,mt6853-vcodec-dec",},
 	{.compatible = "mediatek,mt6983-vcodec-dec",},
 	{.compatible = "mediatek,mt6879-vcodec-dec",},
+	{.compatible = "mediatek,mt6895-vcodec-dec",},
 	{.compatible = "mediatek,vdec_gcon",},
 	{},
 };
