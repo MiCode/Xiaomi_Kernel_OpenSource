6e112cf88fa56c81931979b82086bcb445bb8ecc
Siddharth Gupta <sidgup@codeaurora.org>
2021-10-04 18:38:41 -0700

    remoteproc: qcom: common: Do not panic if SSR fails during reboot
    SSR can fail during device reboot/shutdown due to various reason, such as
    the userspace process being killed or due to UFS shutdown. This change
    adds a check to ignore the SSR failure in these situations.
    
    Change-Id: I1576856c1d72670feff30772f19cb9aa8e58578a
    Signed-off-by: Siddharth Gupta <sidgup@codeaurora.org>
    Signed-off-by: Gokul krishna Krishnakumar <quic_gokukris@quicinc.com>

--- drivers/remoteproc/qcom_common.c
+++ drivers/remoteproc/qcom_common.c
@@ -627,8 +627,15 @@ EXPORT_SYMBOL_GPL(qcom_remove_ssr_subdev);
 
 static void qcom_check_ssr_status(void *data, struct rproc *rproc)
 {
-	if (rproc->state != RPROC_RUNNING)
-		panic("Panicking, remoteproc %s failed to recover!\n", rproc->name);
+	if (!atomic_read(&rproc->power) ||
+	    rproc->state == RPROC_RUNNING ||
+	    qcom_device_shutdown_in_progress ||
+	    system_state == SYSTEM_RESTART ||
+	    system_state == SYSTEM_POWER_OFF ||
+	    system_state == SYSTEM_HALT)
+		return;
+
+	panic("Panicking, remoteproc %s failed to recover!\n", rproc->name);
 }
 
 static int __init qcom_common_init(void)
