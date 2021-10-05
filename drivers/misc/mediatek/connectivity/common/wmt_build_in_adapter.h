689787efeba364dbe62ec8c6c2a4088ef4f5c180
Tim Chang <tim.chang@mediatek.com>
2021-10-05 10:29:03 +0800

    [ALPS06090562] connectivity: add conn_dbg_dev for common debug command
    conn_dbg_dev will invoke debug function registered by wmt or conninfra.
    
    MTK-Commit-Id: 43b55868237103456392af24cab993cb2a36e8ee
    
    Change-Id: I29972c60fd8402c7d1853f6b0526e2844bee13ef
    Signed-off-by: Tim Chang <tim.chang@mediatek.com>
    CR-Id: ALPS06090562
    Feature: [Module]Conninfra Driver

--- drivers/misc/mediatek/connectivity/common/wmt_build_in_adapter.h
+++ drivers/misc/mediatek/connectivity/common/wmt_build_in_adapter.h
@@ -7,7 +7,8 @@
 #define WMT_BUILD_IN_ADAPTER_H
 
 #include <mtk_wcn_cmb_stub.h>
-
+#include <linux/types.h>
+#include <linux/fs.h>
 
 /*******************************************************************************
  * Bridging from platform -> wmt_drv.ko
@@ -19,10 +20,14 @@ typedef void (*wmt_bridge_connsys_clock_fail_dump_cb)(void);
 typedef int (*wmt_bridge_conninfra_reg_readable)(void);
 typedef int (*wmt_bridge_conninfra_reg_is_bus_hang)(void);
 
+typedef ssize_t (*wmt_bridge_debug_cb)(struct file *, const char __user *, size_t, loff_t *);
+
+
 struct wmt_platform_bridge {
 	wmt_bridge_thermal_query_cb thermal_query_cb;
 	wmt_bridge_trigger_assert_cb trigger_assert_cb;
 	wmt_bridge_connsys_clock_fail_dump_cb clock_fail_dump_cb;
+	wmt_bridge_debug_cb debug_cb;
 
 	/* for CONNAC 2 */
 	wmt_bridge_conninfra_reg_readable conninfra_reg_readable_cb;
