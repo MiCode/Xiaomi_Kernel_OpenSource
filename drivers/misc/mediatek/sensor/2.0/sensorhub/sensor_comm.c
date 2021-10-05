1cfb846d9f3c845b3e07f380bb2edf7cb650b11c
Liyong Wu <Liyong.Wu@mediatek.com>
2021-10-05 10:10:50 +0800

    [ALPS06181798] sensor: expand timeout warning time
    expand timeout warning time.
    
    MTK-Commit-Id: c8b771e108bf833e67a273adbd8aff9368869f97
    
    CR-Id: ALPS06181798
    Feature: Sensor Hub
    Change-Id: I62af4d4a7d6f5615d2c8300048c343007bceaa86
    Signed-off-by: Liyong Wu <Liyong.Wu@mediatek.com>

--- drivers/misc/mediatek/sensor/2.0/sensorhub/sensor_comm.c
+++ drivers/misc/mediatek/sensor/2.0/sensorhub/sensor_comm.c
@@ -86,7 +86,7 @@ int sensor_comm_ctrl_send(struct sensor_comm_ctrl *ctrl, unsigned int size)
 {
 	int retry = 0, ret = 0;
 	const int max_retry = 10;
-	const int64_t timeout = 2000000000LL;
+	const int64_t timeout = 10000000000LL;
 	int64_t start_time = 0, duration = 0;
 
 	start_time = ktime_get_boottime_ns();
@@ -102,7 +102,7 @@ int sensor_comm_ctrl_send(struct sensor_comm_ctrl *ctrl, unsigned int size)
 
 	duration = ktime_get_boottime_ns() - start_time;
 	if (duration > timeout) {
-		pr_notice("running time %lld\n", duration);
+		pr_notice("running time %lld, retry:%d\n", duration, retry);
 		WARN_ON(1);
 	}
 	return ret;
