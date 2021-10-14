39a02009a97e4196bc374fbc2d39ba3baa524754
congqin wang <congqin.wang@mediatek.com>
2021-10-14 15:40:12 +0800

    [ALPS06196172] DISP:fix the round corner bug
    [Detail]
    fix the round corner bug
    
    MTK-Commit-Id: 03268e34d21ce1b1dfeb9be44ef6759500fae904
    
    Signed-off-by: congqin wang <congqin.wang@mediatek.com>
    CR-Id: ALPS06196172
    Feature: Resolution switch
    Change-Id: I0e4ccf522495cee25ca4eae80909463e47a0c940

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
@@ -2622,6 +2622,11 @@ void mtk_drm_crtc_mode_check(struct drm_crtc *crtc,
 	drm_mode_set_crtcinfo(&new_state->adjusted_mode, 0);
 }
 
+#ifdef CONFIG_MTK_ROUND_CORNER_SUPPORT
+void mtk_crtc_load_round_corner_pattern(struct drm_crtc *crtc,
+					struct cmdq_pkt *handle);
+#endif
+
 void mtk_crtc_mode_switch_config(struct mtk_drm_crtc *mtk_crtc,
 	struct drm_crtc_state *old_state)
 {
@@ -2673,6 +2678,10 @@ void mtk_crtc_mode_switch_config(struct mtk_drm_crtc *mtk_crtc,
 		mtk_crtc->gce_obj.event[EVENT_STREAM_DIRTY]);
 	cmdq_pkt_wfe(cmdq_handle,
 		mtk_crtc->gce_obj.event[EVENT_STREAM_EOF]);
+
+#ifdef CONFIG_MTK_ROUND_CORNER_SUPPORT
+	mtk_crtc_load_round_corner_pattern(&mtk_crtc->base, cmdq_handle);
+#endif
 
 	for_each_comp_in_cur_crtc_path(comp, mtk_crtc, i, j) {
 		DDPDBG("%s:%s\n", __func__, mtk_dump_comp_str(comp));
@@ -5696,7 +5705,6 @@ void mtk_crtc_load_round_corner_pattern(struct drm_crtc *crtc,
 			__load_rc_pattern(crtc, panel_ext->corner_pattern_tp_size_r,
 						panel_ext->corner_pattern_lt_addr_r);
 
-		mtk_crtc_config_round_corner(crtc, handle);
 	}
 }
 #endif
@@ -5857,6 +5865,7 @@ void mtk_crtc_first_enable_ddp_config(struct mtk_drm_crtc *mtk_crtc)
 	/*2. Load Round Corner */
 #ifdef CONFIG_MTK_ROUND_CORNER_SUPPORT
 	mtk_crtc_load_round_corner_pattern(&mtk_crtc->base, cmdq_handle);
+	mtk_crtc_config_round_corner(crtc, cmdq_handle);
 #endif
 
 	/*3. Enable M4U port and replace OVL address to mva */
