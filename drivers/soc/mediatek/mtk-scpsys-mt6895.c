3d797f78c08f7024fc63a77e0caef6caf0b88191
Owen Chen <owen.chen@mediatek.com>
2021-10-14 15:36:16 +0800

    [ALPS06171912] soc: mediatek: mt6895: workaround for isp
    workaround for isp power fail issue.
    
    MTK-Commit-Id: 403eef346b7a400e85ca7863d4749d4d8119435b
    
    Signed-off-by: Owen Chen <owen.chen@mediatek.com>
    CR-Id: ALPS06171912
    Feature: [Module]Clock Management
    Change-Id: I790da20619a53925c2e4255fe10fb1f82f5ecade

--- drivers/soc/mediatek/mtk-scpsys-mt6895.c
+++ drivers/soc/mediatek/mtk-scpsys-mt6895.c
@@ -119,7 +119,8 @@ static const struct scp_domain_data scp_domain_data_mt6895[] = {
 			BUS_PROT_IGN(IFR_TYPE, 0x0C34, 0x0C38, 0x0C30, 0x0C3C,
 				MT6895_TOP_AXI_PROT_EN_MMSYS2_ISP_MAIN),
 		},
-		.caps = MTK_SCPD_IS_PWR_CON_ON,
+		.child = {MT6895_POWER_DOMAIN_ISP_IPE, MT6895_POWER_DOMAIN_ISP_DIP1},
+		.caps = MTK_SCPD_IS_PWR_CON_ON | MTK_SCPD_CHILD_OFF,
 	},
 	[MT6895_POWER_DOMAIN_ISP_DIP1] = {
 		.name = "isp_dip1",
@@ -127,7 +128,7 @@ static const struct scp_domain_data scp_domain_data_mt6895[] = {
 		.ctl_offs = 0xE28,
 		.sram_pdn_bits = GENMASK(8, 8),
 		.sram_pdn_ack_bits = GENMASK(12, 12),
-		.caps = MTK_SCPD_IS_PWR_CON_ON,
+		.caps = MTK_SCPD_IS_PWR_CON_ON | MTK_SCPD_BYPASS_CHILD,
 	},
 	[MT6895_POWER_DOMAIN_ISP_IPE] = {
 		.name = "isp_ipe",
@@ -136,7 +137,7 @@ static const struct scp_domain_data scp_domain_data_mt6895[] = {
 		.sram_pdn_bits = GENMASK(8, 8),
 		.sram_pdn_ack_bits = GENMASK(12, 12),
 		.basic_clk_name = {"ipe"},
-		.caps = MTK_SCPD_IS_PWR_CON_ON,
+		.caps = MTK_SCPD_IS_PWR_CON_ON | MTK_SCPD_BYPASS_CHILD,
 	},
 	[MT6895_POWER_DOMAIN_ISP_VCORE] = {
 		.name = "isp_vcore",
