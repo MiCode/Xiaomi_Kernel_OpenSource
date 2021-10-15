85ff0586d5ecacaa94fa2d54f80c4ac1c1b8c78f
Patrick Daly <quic_pdaly@quicinc.com>
2021-10-15 16:57:42 -0700

    mem-buf: Move function declarations to mem-buf-ids.h
    Place non-static functions and structure definitions from
    mem-buf-ids.c in a header of the same name.
    
    Change-Id: I06eb1afaa1e38d60812a2cf3a09112d69f73875c
    Signed-off-by: Patrick Daly <quic_pdaly@quicinc.com>
    [quic_isaacm@quicinc.com: Resolve merge conflicts]
    Signed-off-by: Isaac J. Manjarres <quic_isaacm@quicinc.com>

--- drivers/soc/qcom/mem_buf/mem-buf-dev.h
+++ drivers/soc/qcom/mem_buf/mem-buf-dev.h
@@ -7,11 +7,9 @@
 #define MEM_BUF_PRIVATE_H
 
 #include <linux/device.h>
-#include <linux/cdev.h>
 #include <linux/dma-buf.h>
 #include <linux/mem-buf.h>
 #include <linux/slab.h>
-#include <linux/dma-heap.h>
 
 #define MEM_BUF_CAP_SUPPLIER	BIT(0)
 #define MEM_BUF_CAP_CONSUMER	BIT(1)
@@ -25,35 +23,6 @@ int mem_buf_assign_mem(bool is_lend, struct sg_table *sgt,
 int mem_buf_unassign_mem(struct sg_table *sgt, int *src_vmids,
 			 unsigned int nr_acl_entries,
 			 gh_memparcel_handle_t hdl);
-
-#define MEM_BUF_API_HYP_ASSIGN BIT(0)
-#define MEM_BUF_API_GUNYAH BIT(1)
-
-/* Future targets should receive a notification with the proper value */
-#define VMID_TUIVM (45)
-
-/*
- * @vmid - id assigned by hypervisor to uniquely identify a VM
- * @allowed_api - Some vms may use a different hypervisor interface.
- */
-struct mem_buf_vm {
-	const char *name;
-	u16 vmid;
-	u32 allowed_api;
-	struct cdev cdev;
-	struct device dev;
-};
-
-extern int current_vmid;
-int mem_buf_vm_init(struct device *dev);
-void mem_buf_vm_exit(void);
-/*
- * Returns a negative number for invalid arguments, otherwise a MEM_BUF_API
- * which is supported by all vmids in the array.
- */
-int mem_buf_vm_get_backend_api(int *vmids, unsigned int nr_acl_entries);
-/* @Return: A negative number on failure, or vmid on success */
-int mem_buf_fd_to_vmid(int fd);
 
 #ifdef CONFIG_QCOM_MEM_BUF_DEV_GH
 int mem_buf_map_mem_s1(struct gh_sgl_desc *sgl_desc);
@@ -144,4 +113,3 @@ static inline int mem_buf_unassign_mem_gunyah(struct sg_table *sgt, int *src_vmi
 }
 #endif
 #endif
-
