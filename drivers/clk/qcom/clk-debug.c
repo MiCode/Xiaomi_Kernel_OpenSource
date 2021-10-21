b49563b409b41c64194ac1aae7502e2587b06513
Mike Tipton <mdtipton@codeaurora.org>
2021-10-21 13:56:12 -0700

    clk: qcom: clk-debug: Fix clk_measure race with re-parenting
    Reading the clk_measure files results in calls to
    clk_hw_get_parent_by_index() outside the context of the framework locks.
    If the requested parent isn't already cached in clk_parent_map::core,
    then it'll attempt to find the parent by walking the clk_root_list and
    clk_core::children lists. If these lists are being manipulated at the
    same time by clocks being registered or re-parented, then we can walk
    into and dereference poisoned list pointers. For instance:
    
    Unable to handle kernel paging request at virtual address dead000000000048
    Call trace:
        __clk_lookup_subtree+0x18/0x94
        __clk_lookup_subtree+0x54/0x94
        clk_core_get_parent_by_index+0xec/0x148
        clk_hw_get_parent_by_index+0x18/0x30
        clk_find_and_set_parent+0xac/0xf0 [clk_qcom]
        clk_debug_measure_get+0x110/0x3f8 [clk_qcom]
        simple_attr_read+0x148/0x220
        debugfs_attr_read+0x50/0x188
    
    We can prevent this by forcing all clk_debug_mux parents to be cached
    before creating clk_measure files. We can do this by calling
    clk_hw_get_parent_by_index() in the debug_init() callback, which is
    already protected by the prepare_lock.
    
    Some clk_debug_mux parents may never be registered, in which case we
    need to prevent calling clk_hw_get_parent_by_index() in the debugfs
    path. The debugcc devices should have phandles to its parent CCs, which
    will ensure debugcc probes after its parents. Thus, if
    clk_hw_get_parent_by_index() returns NULL, then assume it'll never be
    registered and invalidate it such that we don't try to lookup the parent
    again outside the framework locks.
    
    Change-Id: Ia348222bc7c1535efb0f3979f2ae3260e6666037
    Signed-off-by: Mike Tipton <mdtipton@codeaurora.org>

--- drivers/clk/qcom/clk-debug.c
+++ drivers/clk/qcom/clk-debug.c
@@ -36,6 +36,8 @@ static DEFINE_MUTEX(clk_debug_lock);
 static LIST_HEAD(clk_hw_debug_list);
 static LIST_HEAD(clk_hw_debug_mux_list);
 
+#define INVALID_MUX_SEL		0xDEADBEEF
+
 #define TCXO_DIV_4_HZ		4800000
 #define SAMPLE_TICKS_1_MS	0x1000
 #define SAMPLE_TICKS_27_MS	0x20000
@@ -206,6 +208,8 @@ static bool clk_is_debug_mux(struct clk_hw *hw)
 
 static int clk_find_and_set_parent(struct clk_hw *mux, struct clk_hw *clk)
 {
+	struct clk_debug_mux *dmux;
+	struct clk_hw *parent;
 	int i;
 
 	if (!clk || !clk_is_debug_mux(mux))
@@ -214,8 +218,13 @@ static int clk_find_and_set_parent(struct clk_hw *mux, struct clk_hw *clk)
 	if (mux == clk || !clk_set_parent(mux->clk, clk->clk))
 		return 0;
 
+	dmux = to_clk_measure(mux);
+
 	for (i = 0; i < clk_hw_get_num_parents(mux); i++) {
-		struct clk_hw *parent = clk_hw_get_parent_by_index(mux, i);
+		if (i < dmux->num_mux_sels && dmux->mux_sels[i] == INVALID_MUX_SEL)
+			continue;
+
+		parent = clk_hw_get_parent_by_index(mux, i);
 
 		if (!clk_find_and_set_parent(parent, clk))
 			return clk_set_parent(mux->clk, parent->clk);
@@ -282,10 +291,31 @@ err:
 	return ret;
 }
 
+static void clk_debug_mux_init(struct clk_hw *hw, struct dentry *dentry)
+{
+	struct clk_debug_mux *mux;
+	struct clk_hw *parent;
+	unsigned int i;
+
+	mux = to_clk_measure(hw);
+
+	for (i = 0; i < clk_hw_get_num_parents(hw); i++) {
+		parent = clk_hw_get_parent_by_index(hw, i);
+
+		if (!parent && i < mux->num_mux_sels) {
+			mux->mux_sels[i] = INVALID_MUX_SEL;
+			pr_debug("%s: invalidating %s mux_sel %d\n", __func__,
+				 clk_hw_get_name(hw), i);
+		}
+	}
+
+	clk_debug_measure_add(hw, dentry);
+}
+
 const struct clk_ops clk_debug_mux_ops = {
 	.get_parent = clk_debug_mux_get_parent,
 	.set_parent = clk_debug_mux_set_parent,
-	.debug_init = clk_debug_measure_add,
+	.debug_init = clk_debug_mux_init,
 };
 EXPORT_SYMBOL(clk_debug_mux_ops);
 
