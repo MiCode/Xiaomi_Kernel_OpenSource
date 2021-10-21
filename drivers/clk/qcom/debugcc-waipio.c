fc979b29dacc1d15e83264ff67af3b1ec5bdf3ce
Mike Tipton <mdtipton@codeaurora.org>
2021-10-21 13:56:45 -0700

    clk: qcom: debugcc-waipio: Allow parent caching during init
    To prevent race conditions when the clk_measure files are read while the
    framework clk_root_list and clk_core::children lists are being
    manipulated, we need to cache all the debug mux parents during init
    before the clk_measure files are created. The clk-debug logic for this
    requires initializing num_mux_sels. And the mux_list and measure-only
    registration needs to be reordered to ensure all parents are registered
    before their child muxes.
    
    Change-Id: Ia63563250a57669b3a2939f9e3a6ddc8d45a3492
    Signed-off-by: Mike Tipton <mdtipton@codeaurora.org>

--- drivers/clk/qcom/debugcc-waipio.c
+++ drivers/clk/qcom/debugcc-waipio.c
@@ -57,6 +57,7 @@ static struct clk_debug_mux apss_cc_debug_mux = {
 	.post_div_shift = 0,
 	.post_div_val = 4,
 	.mux_sels = apss_cc_debug_mux_sels,
+	.num_mux_sels = ARRAY_SIZE(apss_cc_debug_mux_sels),
 	.pre_div_vals = apss_cc_debug_mux_pre_divs,
 	.hw.init = &(struct clk_init_data){
 		.name = "apss_cc_debug_mux",
@@ -227,6 +228,7 @@ static struct clk_debug_mux cam_cc_debug_mux = {
 	.post_div_shift = 0,
 	.post_div_val = 4,
 	.mux_sels = cam_cc_debug_mux_sels,
+	.num_mux_sels = ARRAY_SIZE(cam_cc_debug_mux_sels),
 	.hw.init = &(struct clk_init_data){
 		.name = "cam_cc_debug_mux",
 		.ops = &clk_debug_mux_ops,
@@ -348,6 +350,7 @@ static struct clk_debug_mux disp_cc_debug_mux = {
 	.post_div_shift = 0,
 	.post_div_val = 4,
 	.mux_sels = disp_cc_debug_mux_sels,
+	.num_mux_sels = ARRAY_SIZE(disp_cc_debug_mux_sels),
 	.hw.init = &(struct clk_init_data){
 		.name = "disp_cc_debug_mux",
 		.ops = &clk_debug_mux_ops,
@@ -627,6 +630,7 @@ static struct clk_debug_mux gcc_debug_mux = {
 	.post_div_shift = 0,
 	.post_div_val = 2,
 	.mux_sels = gcc_debug_mux_sels,
+	.num_mux_sels = ARRAY_SIZE(gcc_debug_mux_sels),
 	.hw.init = &(struct clk_init_data){
 		.name = "gcc_debug_mux",
 		.ops = &clk_debug_mux_ops,
@@ -698,6 +702,7 @@ static struct clk_debug_mux gpu_cc_debug_mux = {
 	.post_div_shift = 0,
 	.post_div_val = 2,
 	.mux_sels = gpu_cc_debug_mux_sels,
+	.num_mux_sels = ARRAY_SIZE(gpu_cc_debug_mux_sels),
 	.hw.init = &(struct clk_init_data){
 		.name = "gpu_cc_debug_mux",
 		.ops = &clk_debug_mux_ops,
@@ -737,6 +742,7 @@ static struct clk_debug_mux video_cc_debug_mux = {
 	.post_div_shift = 0,
 	.post_div_val = 3,
 	.mux_sels = video_cc_debug_mux_sels,
+	.num_mux_sels = ARRAY_SIZE(video_cc_debug_mux_sels),
 	.hw.init = &(struct clk_init_data){
 		.name = "video_cc_debug_mux",
 		.ops = &clk_debug_mux_ops,
@@ -763,10 +769,10 @@ static struct mux_regmap_names mux_list[] = {
 	{ .mux = &apss_cc_debug_mux, .regmap_name = "qcom,apsscc" },
 	{ .mux = &cam_cc_debug_mux, .regmap_name = "qcom,camcc" },
 	{ .mux = &disp_cc_debug_mux, .regmap_name = "qcom,dispcc" },
-	{ .mux = &gcc_debug_mux, .regmap_name = "qcom,gcc" },
 	{ .mux = &gpu_cc_debug_mux, .regmap_name = "qcom,gpucc" },
 	{ .mux = &mc_cc_debug_mux, .regmap_name = "qcom,mccc" },
 	{ .mux = &video_cc_debug_mux, .regmap_name = "qcom,videocc" },
+	{ .mux = &gcc_debug_mux, .regmap_name = "qcom,gcc" },
 };
 
 static struct clk_dummy measure_only_apcs_gold_post_acd_clk = {
@@ -923,16 +929,6 @@ static int clk_debug_waipio_probe(struct platform_device *pdev)
 		}
 	}
 
-	for (i = 0; i < ARRAY_SIZE(mux_list); i++) {
-		ret = devm_clk_register_debug_mux(&pdev->dev, mux_list[i].mux);
-		if (ret) {
-			dev_err(&pdev->dev, "Unable to register mux clk %s, err:(%d)\n",
-				qcom_clk_hw_get_name(&mux_list[i].mux->hw),
-				ret);
-			return ret;
-		}
-	}
-
 	for (i = 0; i < ARRAY_SIZE(debugcc_waipio_hws); i++) {
 		clk = devm_clk_register(&pdev->dev, debugcc_waipio_hws[i]);
 		if (IS_ERR(clk)) {
@@ -940,6 +936,16 @@ static int clk_debug_waipio_probe(struct platform_device *pdev)
 				clk_hw_get_name(debugcc_waipio_hws[i]),
 				PTR_ERR(clk));
 			return PTR_ERR(clk);
+		}
+	}
+
+	for (i = 0; i < ARRAY_SIZE(mux_list); i++) {
+		ret = devm_clk_register_debug_mux(&pdev->dev, mux_list[i].mux);
+		if (ret) {
+			dev_err(&pdev->dev, "Unable to register mux clk %s, err:(%d)\n",
+				qcom_clk_hw_get_name(&mux_list[i].mux->hw),
+				ret);
+			return ret;
 		}
 	}
 
