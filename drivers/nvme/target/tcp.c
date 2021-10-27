e84b38e57821e8fe383d26125319a9eebc0e415a
Amit Engel <amit.engel@dell.com>
2021-10-27 09:49:27 +0300

    nvmet-tcp: fix header digest verification
    [ Upstream commit 86aeda32b887cdaeb0f4b7bfc9971e36377181c7 ]
    
    Pass the correct length to nvmet_tcp_verify_hdgst, which is the pdu
    header length.  This fixes a wrong behaviour where header digest
    verification passes although the digest is wrong.
    
    Signed-off-by: Amit Engel <amit.engel@dell.com>
    Reviewed-by: Sagi Grimberg <sagi@grimberg.me>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/nvme/target/tcp.c
+++ drivers/nvme/target/tcp.c
@@ -1020,7 +1020,7 @@ recv:
 	}
 
 	if (queue->hdr_digest &&
-	    nvmet_tcp_verify_hdgst(queue, &queue->pdu, queue->offset)) {
+	    nvmet_tcp_verify_hdgst(queue, &queue->pdu, hdr->hlen)) {
 		nvmet_tcp_fatal_error(queue); /* fatal */
 		return -EPROTO;
 	}
