3092eea2dfc3b26a63415ad14c18ffffdbbc2c0d
Irene.Luo <irene.luo@mediatek.com>
2021-10-28 14:27:17 +0800

    [ALPS06239508] SWPM: Driver porting for mt6895
    Copied the codebase for SWPM
    
    MTK-Commit-Id: 98bc3275bc12de4d9b5da1e4c4b608f216062182
    
    Signed-off-by: Irene.Luo <irene.luo@mediatek.com>
    CR-Id: ALPS06239508
    Feature: SWPM(Software Power Model)
    Change-Id: I0e71aeb23a8fe1bf1aa0c75337322c60ee0c4c47

---
+++ drivers/misc/mediatek/swpm/modules/debug/v6895/swpm_gpu_v6895.h
@@ -0,0 +1,66 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2021 MediaTek Inc.
+ */
+
+#ifndef __SWPM_GPU_V6895_H__
+#define __SWPM_GPU_V6895_H__
+
+/* numbers of unsigned int */
+#define GPU_SWPM_RESERVED_SIZE 15
+
+enum gpu_power_counter {
+	gfreq,
+	gvolt,
+	galu_fma_urate,
+	galu_cvt_urate,
+	galu_sfu_urate,
+	gtex_urate,
+	glsc_urate,
+	gl2c_urate,
+	gvary_urate,
+	gtiler_urate,
+	grast_urate,
+	gloading,
+	gthermal,
+	glkg,
+
+	GPU_POWER_COUNTER_LAST
+};
+
+struct gpu_swpm_rec_data {
+	/* 4(int) * 15 = 60 bytes */
+	unsigned int gpu_enable;
+	unsigned int gpu_counter[GPU_POWER_COUNTER_LAST];
+};
+
+/* gpu power index data, gpu_state_ratio types */
+enum gpu_power_state {
+	GPU_ACTIVE,
+	GPU_IDLE,
+	GPU_POWER_OFF,
+
+	NR_GPU_POWER_STATE
+};
+
+/* gpu power index structure */
+struct gpu_swpm_index {
+	unsigned int gpu_state_ratio[NR_GPU_POWER_STATE];
+	unsigned int gpu_freq_mhz;
+	unsigned int gpu_volt_mv;
+	unsigned int gpu_loading;
+	unsigned int alu_fma_urate;
+	unsigned int alu_cvt_urate;
+	unsigned int alu_sfu_urate;
+	unsigned int tex_urate;
+	unsigned int lsc_urate;
+	unsigned int l2c_urate;
+	unsigned int vary_urate;
+	unsigned int tiler_urate;
+	unsigned int rast_urate;
+	unsigned int thermal;
+	unsigned int lkg;
+};
+
+#endif
+
