bd566e91602697215e08c192663d8affac1bdf56
Peter Wang <peter.wang@mediatek.com>
2021-10-28 14:18:42 +0800

    [ALPS06209328] ufs: mediatek: change auto-hibern8 to 5ms
    Change auto-hibern8 timer from 10ms to 5 ms
    
    MTK-Commit-Id: e7caf2e3f0b5f76350b2186c0527dc27977a3959
    
    Signed-off-by: Peter Wang <peter.wang@mediatek.com>
    CR-Id: ALPS06209328
    Feature: UFS(Universal Flash Storage)
    Change-Id: I06d00c071d948b78527288a7ff515e38760d5f37

--- drivers/scsi/ufs/ufs-mediatek.c
+++ drivers/scsi/ufs/ufs-mediatek.c
@@ -1781,7 +1781,7 @@ static int ufs_mtk_post_link(struct ufs_hba *hba)
 
 	/* configure auto-hibern8 timer to 10ms */
 	if (ufshcd_is_auto_hibern8_supported(hba)) {
-		hba->ahit = FIELD_PREP(UFSHCI_AHIBERN8_TIMER_MASK, 10) |
+		hba->ahit = FIELD_PREP(UFSHCI_AHIBERN8_TIMER_MASK, 5) |
 			    FIELD_PREP(UFSHCI_AHIBERN8_SCALE_MASK, 3);
 	}
 
