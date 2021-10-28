e0b187b7a273d18f8727086e9f4d9bb20599013e
charles.yang <charles.yang@mediatek.com>
2021-10-28 14:31:40 +0800

    [ALPS06262237] CM_MGR: Enable cm_mgr for 6895 kernel
    [Detail] CM_MGR: Enable cm_mgr for 6895 kernel
    
    MTK-Commit-Id: 7de970bd78a3bae1702a27881b1dbf61508b40db
    
    Signed-off-by: charles.yang <charles.yang@mediatek.com>
    CR-Id: ALPS06262237
    Feature: CGM(CPU/GPU Memory Latency Management)
    Change-Id: I7f94bc4f8a3a0600d33365ce2c67d58f9360d3ba

---
+++ drivers/misc/mediatek/cm_mgr/mtk_cm_mgr_events_mt6895.h
@@ -0,0 +1,40 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM cm_mgr_events
+
+#if !defined(_TRACE_MTK_CM_MGR_EVENTS_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_MTK_CM_MGR_EVENTS_H
+
+#include <linux/tracepoint.h>
+
+TRACE_EVENT(CM_MGR__stall_raio,
+	TP_PROTO(int _id,
+		unsigned int _ratio),
+	TP_ARGS(_id,
+		_ratio),
+	TP_STRUCT__entry(
+		__field(int, _id)
+		__field(unsigned int, _ratio)
+	),
+	TP_fast_assign(
+		__entry->_id = _id;
+		__entry->_ratio = _ratio;
+	),
+	TP_printk("CPU0_config_reg__CPU_AVG_STALL_RATIO=%d, _id=%d",
+		__entry->_ratio, __entry->_id)
+);
+
+#endif /* _TRACE_MTK_CM_MGR_EVENTS_H */
+
+/* This part must be outside protection */
+#undef TRACE_INCLUDE_PATH
+#define TRACE_INCLUDE_PATH ./
+#undef TRACE_INCLUDE_FILE
+#define TRACE_INCLUDE_FILE mtk_cm_mgr_events_mt6895
+#include <trace/define_trace.h>
+
+
