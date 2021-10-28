dfa57cee236ddcab1c438ac2d234b8a95905515c
Owen Chen <owen.chen@mediatek.com>
2021-10-28 14:40:32 +0800

    [ALPS06282222] soc: mediatek: mt6895: revert dip1/ipe combine
    revert dip1/ipe power ocntrol combine to isp_main.
    
    MTK-Commit-Id: c21a2dcab5f80538a06060c81221daad0ef304d7
    
    Signed-off-by: Owen Chen <owen.chen@mediatek.com>
    CR-Id: ALPS06282222
    Feature: [Module]Clock Management
    Change-Id: Id2a55de5ac57d0111dd698ed07198ef0093c772c

--- drivers/soc/mediatek/mtk-scpsys-mt6895.c
+++ drivers/soc/mediatek/mtk-scpsys-mt6895.c
@@ -119,8 +119,7 @@ static const struct scp_domain_data scp_domain_data_mt6895[] = {
 			BUS_PROT_IGN(IFR_TYPE, 0x0C34, 0x0C38, 0x0C30, 0x0C3C,
 				MT6895_TOP_AXI_PROT_EN_MMSYS2_ISP_MAIN),
 		},
-		.child = {MT6895_POWER_DOMAIN_ISP_IPE, MT6895_POWER_DOMAIN_ISP_DIP1},
-		.caps = MTK_SCPD_IS_PWR_CON_ON | MTK_SCPD_CHILD_OFF,
+		.caps = MTK_SCPD_IS_PWR_CON_ON,
 	},
 	[MT6895_POWER_DOMAIN_ISP_DIP1] = {
 		.name = "isp_dip1",
@@ -128,7 +127,7 @@ static const struct scp_domain_data scp_domain_data_mt6895[] = {
 		.ctl_offs = 0xE28,
 		.sram_pdn_bits = GENMASK(8, 8),
 		.sram_pdn_ack_bits = GENMASK(12, 12),
-		.caps = MTK_SCPD_IS_PWR_CON_ON | MTK_SCPD_BYPASS_CHILD,
+		.caps = MTK_SCPD_IS_PWR_CON_ON,
 	},
 	[MT6895_POWER_DOMAIN_ISP_IPE] = {
 		.name = "isp_ipe",
@@ -137,7 +136,7 @@ static const struct scp_domain_data scp_domain_data_mt6895[] = {
 		.sram_pdn_bits = GENMASK(8, 8),
 		.sram_pdn_ack_bits = GENMASK(12, 12),
 		.basic_clk_name = {"ipe"},
-		.caps = MTK_SCPD_IS_PWR_CON_ON | MTK_SCPD_BYPASS_CHILD,
+		.caps = MTK_SCPD_IS_PWR_CON_ON,
 	},
 	[MT6895_POWER_DOMAIN_ISP_VCORE] = {
 		.name = "isp_vcore",
