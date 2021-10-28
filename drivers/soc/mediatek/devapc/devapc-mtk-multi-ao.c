b772922a6fac0c04ae81b646d6af400652f3fe7a
Jackson Chang <jackson-kt.chang@mediatek.com>
2021-10-28 14:24:54 +0800

    [ALPS05970092] devapc: dump extra information
    Reverts dump extra information
    
    MTK-Commit-Id: 2ff2a8cfaaed95a93b393e58961fd04775a2b215
    
    Signed-off-by: Jackson Chang <jackson-kt.chang@mediatek.com>
    CR-Id: ALPS05970092
    Feature: [Module]Device APC
    Change-Id: Iee08b0d5a4232cb192a7378c1ad1ebce62101b98

--- drivers/soc/mediatek/devapc/devapc-mtk-multi-ao.c
+++ drivers/soc/mediatek/devapc/devapc-mtk-multi-ao.c
@@ -7,9 +7,6 @@
 #include <linux/clk.h>
 #include <linux/fs.h>
 #include <linux/interrupt.h>
-#if defined(CONFIG_MTK_SERROR_HOOK) || defined(CONFIG_MTK_SABORT_HOOK)
-#include <linux/io.h>
-#endif
 #include <linux/module.h>
 #include <linux/of_irq.h>
 #include <linux/of_address.h>
@@ -42,14 +39,6 @@ static struct mtk_devapc_context {
 
 	unsigned long mmup_enabled;
 } mtk_devapc_ctx[1];
-
-#if defined(CONFIG_MTK_SERROR_HOOK) || defined(CONFIG_MTK_SABORT_HOOK)
-/* For debug slave error, will be removed later */
-phys_addr_t dump_base1 = 0x10001000;
-phys_addr_t dump_base2 = 0x10215000;
-void __iomem *dump_virt1;
-void __iomem *dump_virt2;
-#endif
 
 static LIST_HEAD(viocb_list);
 static DEFINE_SPINLOCK(devapc_lock);
@@ -925,17 +914,6 @@ static void devapc_dump_info(void)
 	const char *vio_master;
 	uint8_t perm;
 
-	/* For debug slave error, will be removed later */
-	pr_info("dump_virt1 (0x10001F54): 0x%x\n", readl(dump_virt1 + 0xF54));
-	pr_info("dump_virt1 (0x10001F58): 0x%x\n", readl(dump_virt1 + 0xF58));
-	pr_info("dump_virt1 (0x10001C40): 0x%x\n", readl(dump_virt1 + 0xC40));
-	pr_info("dump_virt1 (0x10001C50): 0x%x\n", readl(dump_virt1 + 0xC50));
-	pr_info("dump_virt2 (0x102150E4): 0x%x\n", readl(dump_virt2 + 0x0E4));
-	pr_info("dump_virt2 (0x102150E8): 0x%x\n", readl(dump_virt2 + 0x0E8));
-	pr_info("dump_virt2 (0x102150EC): 0x%x\n", readl(dump_virt2 + 0x0EC));
-	pr_info("dump_virt2 (0x1021514C): 0x%x\n", readl(dump_virt2 + 0x14C));
-	pr_info("dump_virt2 (0x10215140): 0x%x\n", readl(dump_virt2 + 0x140));
-
 	device_info = mtk_devapc_ctx->soc->device_info;
 	vio_info = mtk_devapc_ctx->soc->vio_info;
 	vio_idx = index = -1;
@@ -1535,12 +1513,6 @@ int mtk_devapc_probe(struct platform_device *pdev,
 	int ret;
 
 	pr_info(PFX "driver registered\n");
-
-#if defined(CONFIG_MTK_SERROR_HOOK) || defined(CONFIG_MTK_SABORT_HOOK)
-	/* For debug slave error, will be removed later */
-	dump_virt1 = ioremap(dump_base1, 4096);
-	dump_virt2 = ioremap(dump_base2, 4096);
-#endif
 
 #ifdef CONFIG_MTK_SERROR_HOOK
 	ret = register_trace_android_rvh_arm64_serror_panic(
