4c2134830ae2ed487d25bc9bd0853494015737f6
Changbin Du <changbin.du@gmail.com>
2021-11-05 13:48:27 -0700

    UPSTREAM: mm/damon: remove return value from before_terminate callback
    Since the return value of 'before_terminate' callback is never used, we
    make it have no return value.
    
    Link: https://lkml.kernel.org/r/20211029005023.8895-1-changbin.du@gmail.com
    Signed-off-by: Changbin Du <changbin.du@gmail.com>
    Reviewed-by: SeongJae Park <sj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    (cherry picked from commit 658f9ae761b5965893727dd4edcdad56e5a439bb)
    
    Bug: 228223814
    Signed-off-by: zhijun wan <wanzhijun@oppo.com>
    Change-Id: Ida44623827a7a667118a2af329f6e58a57941fc9

--- mm/damon/dbgfs.c
+++ mm/damon/dbgfs.c
@@ -653,12 +653,12 @@ static void dbgfs_fill_ctx_dir(struct dentry *dir, struct damon_ctx *ctx)
 		debugfs_create_file(file_names[i], 0600, dir, ctx, fops[i]);
 }
 
-static int dbgfs_before_terminate(struct damon_ctx *ctx)
+static void dbgfs_before_terminate(struct damon_ctx *ctx)
 {
 	struct damon_target *t, *next;
 
 	if (!targetid_is_pid(ctx))
-		return 0;
+		return;
 
 	mutex_lock(&ctx->kdamond_lock);
 	damon_for_each_target_safe(t, next, ctx) {
@@ -666,7 +666,6 @@ static int dbgfs_before_terminate(struct damon_ctx *ctx)
 		damon_destroy_target(t);
 	}
 	mutex_unlock(&ctx->kdamond_lock);
-	return 0;
 }
 
 static struct damon_ctx *dbgfs_new_ctx(void)
