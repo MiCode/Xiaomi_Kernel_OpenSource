8d929114a7d6286185ee6152de4b21312598c8a4
David Brazdil <dbrazdil@google.com>
2021-11-08 10:26:35 +0000

    ANDROID: KVM: arm64: Mark select_iommu_ops static
    The function is only used in the compilation unit where it is defined.
    Silence a warning by marking it static.
    
    Test: builds
    Bug: 190463801
    Signed-off-by: David Brazdil <dbrazdil@google.com>
    Change-Id: Ic5b58bf355c1888d0b479b0f826a14492857c4aa

--- arch/arm64/kvm/hyp/nvhe/setup.c
+++ arch/arm64/kvm/hyp/nvhe/setup.c
@@ -233,7 +233,7 @@ static int finalize_host_mappings(void)
 	return 0;
 }
 
-int select_iommu_ops(enum kvm_iommu_driver driver)
+static int select_iommu_ops(enum kvm_iommu_driver driver)
 {
 	switch (driver) {
 	case KVM_IOMMU_DRIVER_NONE:
