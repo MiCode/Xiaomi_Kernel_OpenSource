8adda0d8c9ae6cda04dc06fc912fe3fbfdf109c8
Morven-CF Yeh <morven-cf.yeh@mediatek.com>
2021-11-09 11:21:08 +0800

    [ALPS06301618] slbc: enable slb permission
    1. enable slb permission
    2. resize user partition
    
    MTK-Commit-Id: 981679ffbdade1c0ea652122f8462f392348cda1
    
    Signed-off-by: Morven-CF Yeh <morven-cf.yeh@mediatek.com>
    CR-Id: ALPS06301618
    Feature: SLC(System Level Cache)
    Change-Id: Iee5a87219fb222b54820fd94a4b527d195bbfaf2

--- drivers/misc/mediatek/slbc/slbc_mt6895.c
+++ drivers/misc/mediatek/slbc/slbc_mt6895.c
@@ -97,9 +97,10 @@ static unsigned long slbc_slot_used;
 
 static struct slbc_config p_config[] = {
 	/* SLBC_ENTRY(id, sid, max, fix, p, extra, res, cache) */
-	SLBC_ENTRY(UID_MM_VENC, 0, 0, 0, 0, 0x0, 0xf, 0),
-	SLBC_ENTRY(UID_MML, 1, 0, 0, 0, 0x0, 0xf, 0),
-	SLBC_ENTRY(UID_DISP, 2, 0, 0, 0, 0x0, 0x030, 0),
+	SLBC_ENTRY(UID_MM_VENC, 0, 0, 0, 0, 0x0, 0x7, 0),
+	SLBC_ENTRY(UID_MML, 1, 0, 0, 0, 0x0, 0x7, 0),
+	SLBC_ENTRY(UID_DISP, 2, 0, 0, 0, 0x0, 0x7, 0),
+	SLBC_ENTRY(UID_HIFI3, 3, 0, 0, 0, 0x0, 0x8, 0),
 };
 
 u32 slbc_sram_read(u32 offset)
