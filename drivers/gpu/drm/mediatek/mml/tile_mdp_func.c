3ea342a63c99c680cdf947c4eefd03c9864d284d
Chris-YC Chen <chris-yc.chen@mediatek.com>
2021-11-25 13:38:01 +0800

    [ALPS06147541] mml: fix yuv afbc width constraint
    fix yuv afbc width constraint
    
    MTK-Commit-Id: 6832b0949844b0398e74cc99fa495a1f03ee2fe6
    
    Signed-off-by: Chris-YC Chen <chris-yc.chen@mediatek.com>
    CR-Id: ALPS06147541
    Feature: [Module]MML driver
    Change-Id: I693d16e519ee02088e6c8406430252b43cd205aa

--- drivers/gpu/drm/mediatek/mml/tile_mdp_func.c
+++ drivers/gpu/drm/mediatek/mml/tile_mdp_func.c
@@ -43,7 +43,7 @@ enum isp_tile_message tile_rdma_init(struct tile_func_block *ptr_func,
 		 */
 		ptr_func->in_tile_width = ((data->max_width >> 5) - 1) << 5;
 	} else if (MML_FMT_YUV_COMPRESS(data->src_fmt)) {
-		ptr_func->in_tile_width = (data->max_width >> 4) << 4;
+		ptr_func->in_tile_width = ((data->max_width >> 4) - 1) << 4;
 	} else if (MML_FMT_BLOCK(data->src_fmt)) {
 		ptr_func->in_tile_width = (data->max_width >> 6) << 6;
 	} else if ((MML_FMT_H_SUBSAMPLE(data->src_fmt) &&
