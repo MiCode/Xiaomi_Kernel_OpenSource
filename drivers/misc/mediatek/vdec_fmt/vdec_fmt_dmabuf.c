e87662c086214a5d49fb4314cd4ee4b08efbec80
Jason Hsu <yeong-cherng.hsu@mediatek.com>
2021-11-25 13:48:58 +0800

    [ALPS06350003] FMT: reduce logs to reg dump
    [Detail] 1. remove unnecessary reg dump
             2. pack multiple reg to one line to avoid printk too much
    
    MTK-Commit-Id: ec63247945242bb5cca203cb5b64e68075de9aa1
    
    Signed-off-by: Jason Hsu <yeong-cherng.hsu@mediatek.com>
    CR-Id: ALPS06350003
    Feature: Vcodec Color Converter(formatter)
    Change-Id: Iaaab708b35abfbfb9c5958af69564348f1d3514c

--- drivers/misc/mediatek/vdec_fmt/vdec_fmt_dmabuf.c
+++ drivers/misc/mediatek/vdec_fmt/vdec_fmt_dmabuf.c
@@ -81,7 +81,7 @@ u64 fmt_translate_fd(u64 fd, u32 offset, struct dmabufmap map[], struct device *
 
 	for (i = 0; i < FMT_FD_RESERVE; i++) {
 		if (fd == map[i].fd) {
-			fmt_debug(1, "quick search iova 0x%llx",
+			fmt_debug(3, "quick search iova 0x%llx",
 				map[i].iova + offset);
 			return map[i].iova + offset;
 		}
