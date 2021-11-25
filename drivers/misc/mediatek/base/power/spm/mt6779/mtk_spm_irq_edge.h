41210482a95f0922dfe217c0bdff1ee261b75acc
Chengbing Wu <chengbing.wu@mediatek.com>
2021-11-25 13:34:46 +0800

    [ALPS06348106] mt6779: spm: add display edge irq
    1. add display edge irq
    
    MTK-Commit-Id: f8502431550f33c4d38a490e9f7b3ae0a0c0a10e
    
    Signed-off-by: Chengbing Wu <chengbing.wu@mediatek.com>
    CR-Id: ALPS06348106
    Feature: SODI (Screen On Deep Idle)
    Change-Id: Ic514e33fc689f7b25636a8a0e8bc6a1c20647830

--- drivers/misc/mediatek/base/power/spm/mt6779/mtk_spm_irq_edge.h
+++ drivers/misc/mediatek/base/power/spm/mt6779/mtk_spm_irq_edge.h
@@ -22,6 +22,7 @@ static struct edge_trigger_irq_list list[] = {
 	//{ "mediatek,infracfg_ao",   0,	0 },
 	{ "mediatek,kp",		0,	R12_KP_IRQ_B },
 	{ "mediatek,mddriver",		0,	R12_MD1_WDT_B },
+	{ "mediatek,mt6779-disp_rdma0",   0,	R12_SYS_CIRQ_IRQ_B},
 };
 
 #endif /* __MTK_SPM_IRQ_EDGE_H__ */
