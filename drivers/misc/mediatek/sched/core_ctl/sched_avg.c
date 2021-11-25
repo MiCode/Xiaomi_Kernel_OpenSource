e7def73607e3a36619c9f1c1c41997e57c184deb
Jing-Ting Wu <Jing-Ting.Wu@mediatek.com>
2021-11-25 13:44:27 +0800

    [ALPS06336443] sched: module code align ack patch
    [Detail]
    ACK change _task_util_est() logic by:
    sched/fair: Fix util_est UTIL_AVG_UNCHANGED handling
    align it.
    
    MTK-Commit-Id: 535daf890f17cf8d5fffa3719ac23415127a3fcc
    
    CR-Id: ALPS06336443
    Feature: Energy Aware Scheduling Plus (EAS+)
    Signed-off-by: Jing-Ting Wu <Jing-Ting.Wu@mediatek.com>
    Change-Id: I718e21c46cf54cdf44f7ff29f2de8abcc0c446fd

--- drivers/misc/mediatek/sched/core_ctl/sched_avg.c
+++ drivers/misc/mediatek/sched/core_ctl/sched_avg.c
@@ -189,7 +189,7 @@ static inline unsigned long _task_util_est(struct task_struct *p)
 {
 	struct util_est ue = READ_ONCE(p->se.avg.util_est);
 
-	return (max(ue.ewma, ue.enqueued) | UTIL_AVG_UNCHANGED);
+	return max(ue.ewma, (ue.enqueued & ~UTIL_AVG_UNCHANGED));
 }
 
 static inline unsigned long task_util_est(struct task_struct *p)
