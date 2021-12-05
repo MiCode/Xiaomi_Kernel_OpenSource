c36180bab8c3806b4bf42fda84ce88aba414f3b1
Chia-Mao Hung <chia-mao.hung@mediatek.com>
2021-12-05 11:33:03 +0800

    [ALPS06374989] v4l2: add iova error check
    Add allocated SW working iova cross domain check & free
    
    MTK-Commit-Id: a5f32ee442c80c994802ad7f73cf954d95001e28
    
    Signed-off-by: Chia-Mao Hung <chia-mao.hung@mediatek.com>
    CR-Id: ALPS06374989
    Feature: [Module]Video Driver
    Change-Id: Ie7164729847a3b2d217fe5dfb8e09d05afb0b47f

--- drivers/media/platform/mtk-vcodec/vdec/vdec_vcp_if.c
+++ drivers/media/platform/mtk-vcodec/vdec/vdec_vcp_if.c
@@ -304,12 +304,21 @@ static void handle_vdec_mem_alloc(struct vdec_vcu_ipi_mem_op *msg)
 			return;
 
 		mtk_vcodec_debug(vcu, "va 0x%llx pa 0x%llx iova 0x%llx len %d type %d\n",
-			msg->mem.va, msg->mem.pa, msg->mem.iova, msg->mem.len,	msg->mem.type);
+			msg->mem.va, msg->mem.pa, msg->mem.iova, msg->mem.len, msg->mem.type);
 
 		inst = container_of(vcu, struct vdec_inst, vcu);
 		dev = get_dev_by_mem_type(inst, &msg->mem);
 		msg->status = mtk_vcodec_alloc_mem(&msg->mem, dev, &attach, &sgt);
 	}
+
+	/* check memory bound */
+	if (msg->mem.type == MEM_TYPE_FOR_SW || msg->mem.type == MEM_TYPE_FOR_SEC_SW) {
+		if ((msg->mem.iova >> 28) != ((msg->mem.iova + msg->mem.len) >> 28)) {
+			mtk_vcodec_free_mem(&msg->mem, dev, attach, sgt);
+			msg->status = -ENOMEM;
+		}
+	}
+
 	if (msg->status) {
 		mtk_vcodec_err(vcu, "fail %d, va 0x%llx pa 0x%llx iova 0x%llx len %d type %d",
 			msg->status, msg->mem.va, msg->mem.pa,
--- drivers/media/platform/mtk-vcodec/venc/venc_vcp_if.c
+++ drivers/media/platform/mtk-vcodec/venc/venc_vcp_if.c
@@ -206,6 +206,15 @@ static void handle_venc_mem_alloc(struct venc_vcu_ipi_mem_op *msg)
 		dev = get_dev_by_mem_type(inst, &msg->mem);
 		msg->status = mtk_vcodec_alloc_mem(&msg->mem, dev, &attach, &sgt);
 	}
+
+	/* check memory bound */
+	if (msg->mem.type == MEM_TYPE_FOR_SW || msg->mem.type == MEM_TYPE_FOR_SEC_SW) {
+		if ((msg->mem.iova >> 28) != ((msg->mem.iova + msg->mem.len) >> 28)) {
+			mtk_vcodec_free_mem(&msg->mem, dev, attach, sgt);
+			msg->status = -ENOMEM;
+		}
+	}
+
 	if (msg->status) {
 		mtk_vcodec_err(vcu, "fail %d, va 0x%llx pa 0x%llx iova 0x%llx len %d type %d",
 			msg->status, msg->mem.va, msg->mem.pa,
