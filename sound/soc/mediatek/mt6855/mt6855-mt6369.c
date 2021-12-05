1868b29668c14455203cdf7bfa6c3e1e88e2fa35
Elvin Lin <elvin.lin@mediatek.com>
2021-12-05 11:34:45 +0800

    [ALPS06355676] accdet: change for jack register
    Change the way to register jack by accdet driver
    
    MTK-Commit-Id: 62bee6a77e8ef2fbd5e342e9c05cb5555e371e27
    
    Signed-off-by: Elvin Lin <elvin.lin@mediatek.com>
    CR-Id: ALPS06355676
    Feature: Headset Cable/Button Detection and Reaction
    Change-Id: I343c1df895fba5dd49c1639ef5ddf8c3b082e3ec

--- sound/soc/mediatek/mt6855/mt6855-mt6369.c
+++ sound/soc/mediatek/mt6855/mt6855-mt6369.c
@@ -343,6 +343,9 @@ static int mt6855_mt6369_init(struct snd_soc_pcm_runtime *rtd)
 
 	/* disable ext amp connection */
 	snd_soc_dapm_disable_pin(dapm, EXT_SPK_AMP_W_NAME);
+#if IS_ENABLED(CONFIG_SND_SOC_MT6369_ACCDET)
+	mt6369_accdet_init(codec_component, rtd->card);
+#endif
 
 	return 0;
 }
@@ -1264,20 +1267,6 @@ static struct snd_soc_card mt6855_mt6369_soc_card = {
 	.num_dapm_routes = ARRAY_SIZE(mt6855_mt6369_routes),
 };
 
-#if IS_ENABLED(CONFIG_SND_SOC_MT6369_ACCDET)
-
-static int mt6855_mt6369_headset_init(struct snd_soc_component *component)
-{
-	struct snd_soc_card *card = &mt6855_mt6369_soc_card;
-
-	return mt6369_accdet_init(component, card);
-}
-
-static struct snd_soc_aux_dev mt6855_mt6369_headset_dev = {
-	.dlc = COMP_EMPTY(),
-	.init = mt6855_mt6369_headset_init,
-};
-#endif
 static int mt6855_mt6369_dev_probe(struct platform_device *pdev)
 {
 	struct snd_soc_card *card = &mt6855_mt6369_soc_card;
@@ -1336,18 +1325,6 @@ static int mt6855_mt6369_dev_probe(struct platform_device *pdev)
 	}
 
 	card->dev = &pdev->dev;
-#if IS_ENABLED(CONFIG_SND_SOC_MT6369_ACCDET)
-	mt6855_mt6369_headset_dev.dlc.of_node =
-		of_parse_phandle(pdev->dev.of_node,
-				"mediatek,headset-codec", 0);
-	if (mt6855_mt6369_headset_dev.dlc.of_node) {
-		card->aux_dev = &mt6855_mt6369_headset_dev;
-		card->num_aux_devs = 1;
-	} else
-		dev_err(&pdev->dev,
-			"Property 'mediatek,headset-codec' missing/invalid\n");
-	mt6369_accdet_set_drvdata(card);
-#endif
 	ret = devm_snd_soc_register_card(&pdev->dev, card);
 	if (ret)
 		dev_err(&pdev->dev, "%s snd_soc_register_card fail %d\n",
