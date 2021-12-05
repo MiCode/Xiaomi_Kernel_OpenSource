dd443f26e149a531953f86981784f50dcdf60c92
Ning Li <ning.li@mediatek.com>
2021-12-05 11:31:18 +0800

    [ALPS06080793] usnd: hw semaphore
    Add hw semaphore to protect ultrasounds memif and irq enable ops.
    
    MTK-Commit-Id: 0fba54606afe463a73b3a6964ccc394a79814048
    
    CR-Id: ALPS06080793
    Signed-off-by: Ning Li <ning.li@mediatek.com>
    Feature: Ultrasound Proximity Sensor
    Change-Id: I4d5ae5f6a8c06baef3576ffb47cd0dd91bcea968

--- sound/soc/mediatek/ultrasound/ultra_scp/mtk-scp-ultra-platform-driver.c
+++ sound/soc/mediatek/ultrasound/ultra_scp/mtk-scp-ultra-platform-driver.c
@@ -513,10 +513,7 @@ static int mtk_scp_ultra_pcm_start(struct snd_soc_component *component,
 	// 		fs << irq_data_dl->irq_fs_shift);
 
 	/* start dl memif */
-	regmap_update_bits(afe->regmap,
-			memif->data->enable_reg,
-			1 << memif->data->enable_shift,
-			1 << memif->data->enable_shift);
+	ultra_memif_set_enable_hw_sema(afe, ultra_mem->ultra_dl_memif_id);
 
 	/* set ul irq counter */
 	counter = param_config.period_in_size;
@@ -540,15 +537,12 @@ static int mtk_scp_ultra_pcm_start(struct snd_soc_component *component,
 	// 		  fs << irq_data_ul->irq_fs_shift);
 
 	/* Start ul memif */
-	regmap_update_bits(afe->regmap,
-			   memiful->data->enable_reg,
-			   1 << memiful->data->enable_shift,
-			   1 << memiful->data->enable_shift);
+	ultra_memif_set_enable_hw_sema(afe, ultra_mem->ultra_ul_memif_id);
 
 	/* start ul irq */
-	regmap_update_bits(afe->regmap, irq_data_ul->irq_en_reg,
-			1 << irq_data_ul->irq_en_shift,
-			1 << irq_data_ul->irq_en_shift);
+	ultra_irq_set_enable_hw_sema(afe,
+				     irq_data_ul,
+				     ultra_mem->ultra_ul_memif_id);
 	return 0;
 }
 
@@ -582,25 +576,18 @@ static int mtk_scp_ultra_pcm_stop(struct snd_soc_component *component,
 	}
 
 	/* stop dl memif */
-	regmap_update_bits(afe->regmap,
-			   memif->data->enable_reg,
-			   1 << memif->data->enable_shift,
-			   0);
+	ultra_memif_set_disable_hw_sema(afe, ultra_mem->ultra_dl_memif_id);
 	/* stop ul memif */
-	regmap_update_bits(afe->regmap,
-			   memiful->data->enable_reg,
-			   1 << memiful->data->enable_shift,
-			   0);
+	ultra_memif_set_disable_hw_sema(afe, ultra_mem->ultra_ul_memif_id);
 	/* stop dl irq */
-	regmap_update_bits(afe->regmap,
-			   irq_data_dl->irq_en_reg,
-			   1 << irq_data_dl->irq_en_shift,
-			   0);
+	ultra_irq_set_disable_hw_sema(afe,
+				      irq_data_dl,
+				      ultra_mem->ultra_dl_memif_id);
 
 	/* stop ul irq */
-	regmap_update_bits(afe->regmap, irq_data_ul->irq_en_reg,
-			   1 << irq_data_ul->irq_en_shift,
-			   0);
+	ultra_irq_set_disable_hw_sema(afe,
+				      irq_data_ul,
+				      ultra_mem->ultra_ul_memif_id);
 
 	/* clear pending dl irq */
 	regmap_write(afe->regmap, irq_data_dl->irq_clr_reg,
