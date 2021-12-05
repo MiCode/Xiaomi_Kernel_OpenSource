d936f99655c692c560436eb511948f0691986c34
Johnson-CH Chiu <johnson-ch.chiu@mediatek.com>
2021-12-05 11:30:53 +0800

    [ALPS06352810] hcp: add dma_params
    Add dma_params
    
    MTK-Commit-Id: dc4cfdf380469cfc5a0be266610164c06192cf58
    
    Signed-off-by: Johnson-CH Chiu <johnson-ch.chiu@mediatek.com>
    CR-Id: ALPS06352810
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: I0020eb21d747cf59ad88a727441dc5ccf640a163

--- drivers/media/platform/mtk-isp/mtk-hcp/mtk-hcp.c
+++ drivers/media/platform/mtk-isp/mtk-hcp/mtk-hcp.c
@@ -1765,6 +1765,16 @@ static int mtk_hcp_probe(struct platform_device *pdev)
 	if (dma_set_mask_and_coherent(&pdev->dev, DMA_BIT_MASK(34)))
 		dev_info(&pdev->dev, "%s:No DMA available\n", __func__);
 
+	if (!pdev->dev.dma_parms) {
+		pdev->dev.dma_parms =
+			devm_kzalloc(hcp_dev->dev, sizeof(*hcp_dev->dev->dma_parms), GFP_KERNEL);
+	}
+	if (hcp_dev->dev->dma_parms) {
+		ret = dma_set_max_seg_size(hcp_dev->dev, (unsigned int)DMA_BIT_MASK(34));
+		if (ret)
+			dev_info(hcp_dev->dev, "Failed to set DMA segment size\n");
+	}
+
 	atomic_set(&(hcp_dev->have_slb), 0);
 
 	hcp_dev->is_open = false;
