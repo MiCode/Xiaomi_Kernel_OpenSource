e6d573527c3f1e39b21d16db5ae9bf8002f7e47f
Yun-Chen Hsieh <yun-chen.hsieh@mediatek.com>
2021-12-15 13:37:14 +0800

    [ALPS06424218] pmsr: common: add an error handling flow
    1. check null ptr to skip init flow if dts not defined.
    2. check null ptr to skip procfs flow.
    
    MTK-Commit-Id: 3899e02a584c338815e86380838e7897e3477ced
    
    Signed-off-by: Yun-Chen Hsieh <yun-chen.hsieh@mediatek.com>
    CR-Id: ALPS06424218
    Feature: SODI (Screen On Deep Idle)
    Change-Id: I9b24b2b3bfdf4de1009a6fb43113ba1d1c7c0257

--- drivers/misc/mediatek/pmsr/pmsr.c
+++ drivers/misc/mediatek/pmsr/pmsr.c
@@ -78,6 +78,11 @@ static int pmsr_ipi_init(void)
 #if IS_ENABLED(CONFIG_MTK_TINYSYS_SCMI)
 	tinfo = get_scmi_tinysys_info();
 
+	if (!tinfo) {
+		pr_info("get scmi info fail\n");
+		return ret;
+	}
+
 	ret = of_property_read_u32(tinfo->sdev->dev.of_node, "scmi_apmcupm",
 			&scmi_apmcupm_id);
 	if (ret) {
@@ -116,6 +121,9 @@ static ssize_t remote_data_write(struct file *fp, const char __user *userbuf,
 	static unsigned int window_len_pre;
 
 	if (kstrtou32_from_user(userbuf, count, 10, v))
+		return -EFAULT;
+
+	if (!tinfo)
 		return -EFAULT;
 
 	if ((void *)v == (void *)&cfg.pause) {
