f0579720364f82a88733b97f15db7437b7411ff9
Eric Chung <eric.chung@mediatek.com>
2021-12-15 13:43:42 +0800

    [ALPS05678824] gpufreq: mt6855: Enable hwapm
    [Detail]
    Enable HWAPM to let DDK control MFG2.
    
    MTK-Commit-Id: 938a9f277c5752360fdae2f0123946fdbe70ed50
    
    Signed-off-by: Eric Chung <eric.chung@mediatek.com>
    CR-Id: ALPS05678824
    Feature: GPU Extension(gpu_extension)
    Change-Id: Icc146aefeff019cf48807ff575fb015ee55180fd

--- drivers/gpu/mediatek/gpufreq/v2/include/gpufreq_mt6855.h
+++ drivers/gpu/mediatek/gpufreq/v2/include/gpufreq_mt6855.h
@@ -63,7 +63,7 @@
 /**************************************************
  * Power Domain Setting
  **************************************************/
-#define GPUFREQ_HWAPM_ENABLE            (0)
+#define GPUFREQ_HWAPM_ENABLE            (1)
 #define GPUFREQ_CHECK_MTCMOS_PWR_STATUS (1)
 #define PWR_STATUS_OFS                  (0xF3C)
 #define PWR_STATUS_2ND_OFS              (0xF40)
