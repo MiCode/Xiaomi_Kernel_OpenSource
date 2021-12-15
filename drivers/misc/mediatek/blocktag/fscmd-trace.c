9acdf17e30bb08c0c81c28018772950680076185
ed.tsai <ed.tsai@mediatek.com>
2021-12-15 13:29:51 +0800

    [ALPS06337923] Revert "[ALPS06337923] Blocktag: Fix Race Problem in FSCMD"
    This reverts commit 10f08661012deeea917a9efe3a9fb4e7afda261b.
    
    Reason for revert: This could get a KASAN Fail
    
    MTK-Commit-Id: 67f8a9e49aee00acd30bb76fd7170b92d5619a7f
    
    Change-Id: Id9be5ef53cb25ab804ac049814976ad30b05bd47
    Signed-off-by: "ed.tsai" <ed.tsai@mediatek.com>
    CR-Id: ALPS06337923
    Feature: Storage I/O Performance Profiling Tool(blocktag)

--- drivers/misc/mediatek/blocktag/fscmd-trace.c
+++ drivers/misc/mediatek/blocktag/fscmd-trace.c
@@ -13,7 +13,6 @@
 #include "fscmd-trace.h"
 
 #define MAX_LOG_NR (10000) /* entries size of total log */
-#define MAX_RW_LOG_NR (6000) /* entries size of total log */
 #define LOG_LIST_MAX (10) /* the buffer for log list */
 #define SPREAD_PRINTF(buff, size, evt, fmt, args...) \
 do { \
@@ -48,21 +47,14 @@ struct fscmd_log_s {
 	int type;
 };
 
-enum {
-	LOG_TYPE_RW = 0,
-	LOG_TYPE_OTHER,
-	LOG_TYPE_ENTRY,
-};
-
 struct fscmd_s {
-	struct fscmd_log_s *trace[LOG_TYPE_ENTRY]; /* Cycle buffer in MAX_LOG_NR */
-	int cout[LOG_TYPE_ENTRY]; /* the top idx we store log currently */
-	int cur_idx[LOG_TYPE_ENTRY]; /* the top idx we store log currently */
+	struct fscmd_log_s trace[MAX_LOG_NR]; /* Cycle buffer in MAX_LOG_NR */
+	int cur_idx; /* the top idx we store log currently */
+	spinlock_t lock; /* access lock */
 };
 
 static struct fscmd_s gHis_Log;
-static spinlock_t gHis_lock;
-static int dump_bit; /* the top idx we store log currently */
+
 //system call we trace on
 struct nr_talbe_s {
 	char name[8];
@@ -97,28 +89,6 @@ struct nr_talbe_s {
 #endif
 	{"none", -1},
 };
-static int ghistory_struct_init(void)
-{
-	int buffer_sz = 0;
-
-	gHis_Log.cout[LOG_TYPE_RW] = MAX_RW_LOG_NR;
-	gHis_Log.cout[LOG_TYPE_OTHER] = (MAX_LOG_NR - MAX_RW_LOG_NR);
-
-	buffer_sz = (gHis_Log.cout[LOG_TYPE_RW]) * sizeof(struct fscmd_log_s);
-	gHis_Log.trace[LOG_TYPE_RW] = kmalloc(buffer_sz, GFP_NOFS);
-	if (!gHis_Log.trace[LOG_TYPE_RW])
-		return -1;
-
-	buffer_sz = (gHis_Log.cout[LOG_TYPE_OTHER]) * sizeof(struct fscmd_log_s);
-	gHis_Log.trace[LOG_TYPE_OTHER] = kmalloc(buffer_sz, GFP_NOFS);
-	if (!gHis_Log.trace[LOG_TYPE_OTHER])
-		return -1;
-
-	gHis_Log.cur_idx[LOG_TYPE_OTHER] = -1;
-	gHis_Log.cur_idx[LOG_TYPE_RW] = -1;
-
-	return 0;
-}
 
 static struct nr_talbe_s *fscmd_get_nr_table(int target_idx)
 {
@@ -137,27 +107,17 @@ static void fscmd_add_log(struct fscmd_log_s *log)
 	int next_idx = 0;
 	struct fscmd_log_s *cmdlog = NULL;
 
-	if (!dump_bit) {
-		spin_lock(&gHis_lock);
-		if (log->syscall_nr == __NR_read || log->syscall_nr == __NR_write) {
-			next_idx = (gHis_Log.cur_idx[LOG_TYPE_RW] + 1);
-			next_idx %= (gHis_Log.cout[LOG_TYPE_RW]);
-			gHis_Log.cur_idx[LOG_TYPE_RW] = next_idx;
-			cmdlog = gHis_Log.trace[LOG_TYPE_RW] + next_idx;
-		} else {
-			next_idx = (gHis_Log.cur_idx[LOG_TYPE_OTHER] + 1);
-			next_idx %= (gHis_Log.cout[LOG_TYPE_OTHER]);
-			gHis_Log.cur_idx[LOG_TYPE_OTHER] = next_idx;
-			cmdlog = gHis_Log.trace[LOG_TYPE_OTHER] + next_idx;
-		}
+	spin_lock(&gHis_Log.lock);
+	next_idx = (gHis_Log.cur_idx + 1) % MAX_LOG_NR;
+	cmdlog = gHis_Log.trace + next_idx;
+	cmdlog->time = log->time;
+	cmdlog->type = log->type;
+	cmdlog->syscall_nr = log->syscall_nr;
+	cmdlog->pid = log->pid;
+	get_task_comm(cmdlog->comm, current);
+	gHis_Log.cur_idx = next_idx;
+	spin_unlock(&gHis_Log.lock);
 
-		cmdlog->time = log->time;
-		cmdlog->type = log->type;
-		cmdlog->syscall_nr = log->syscall_nr;
-		cmdlog->pid = log->pid;
-		get_task_comm(cmdlog->comm, current);
-		spin_unlock(&gHis_lock);
-	}
 }
 
 void fscmd_trace_sys_enter(void *data,
@@ -201,48 +161,23 @@ size_t mtk_fscmd_usedmem(char **buff, unsigned long *size,
 	// start dump the farthest log in ring-buffer
 	SPREAD_PRINTF(buff, size, seq,
 			"time,entry/exit,syscall,pid,func,\n");
-	spin_lock(&gHis_lock);
-	dump_bit = 1;
-	spin_unlock(&gHis_lock);
-	for (i = 0, idx = 0; i < (gHis_Log.cout[LOG_TYPE_OTHER]); i++) {
-		pt = fscmd_get_nr_table(gHis_Log.trace[LOG_TYPE_OTHER][idx].syscall_nr);
-		idx = (gHis_Log.cur_idx[LOG_TYPE_OTHER] + i + 1) % (gHis_Log.cout[LOG_TYPE_RW]);
+	for (i = 0; i < MAX_LOG_NR; i++) {
+		pt = fscmd_get_nr_table(gHis_Log.trace[idx].syscall_nr);
+		idx = (gHis_Log.cur_idx + i + 1) % MAX_LOG_NR;
 		SPREAD_PRINTF(buff, size, seq,
 			"%llu,%s,%s,%d,%s\n",
-			gHis_Log.trace[LOG_TYPE_OTHER][idx].time,
-			(gHis_Log.trace[LOG_TYPE_OTHER][idx].type == FSCMD_SYSCALL_ENTRY)?"i":"o",
+			gHis_Log.trace[idx].time,
+			(gHis_Log.trace[idx].type == FSCMD_SYSCALL_ENTRY)?"i":"o",
 			pt->name,
-			gHis_Log.trace[LOG_TYPE_OTHER][idx].pid,
-			gHis_Log.trace[LOG_TYPE_OTHER][idx].comm);
+			gHis_Log.trace[idx].pid,
+			gHis_Log.trace[idx].comm);
 	}
-
-	for (i = 0, idx = 0; i < gHis_Log.cout[LOG_TYPE_RW]; i++) {
-		pt = fscmd_get_nr_table(gHis_Log.trace[LOG_TYPE_RW][idx].syscall_nr);
-		idx = (gHis_Log.cur_idx[LOG_TYPE_RW] + i + 1) % gHis_Log.cout[LOG_TYPE_RW];
-		SPREAD_PRINTF(buff, size, seq,
-			"%llu,%s,%s,%d,%s\n",
-			gHis_Log.trace[LOG_TYPE_RW][idx].time,
-			(gHis_Log.trace[LOG_TYPE_RW][idx].type == FSCMD_SYSCALL_ENTRY)?"i":"o",
-			pt->name,
-			gHis_Log.trace[LOG_TYPE_RW][idx].pid,
-			gHis_Log.trace[LOG_TYPE_RW][idx].comm);
-	}
-
-	spin_lock(&gHis_lock);
-	dump_bit = 0;
-	spin_unlock(&gHis_lock);
 	return size_l;
 }
 
 int mtk_fscmd_init(void)
 {
-	int ret = 0;
-
-	ret = ghistory_struct_init();
-	if (ret)
-		return ret;
-
-	dump_bit = 0;
-	spin_lock_init(&gHis_lock);
-	return ret;
+	gHis_Log.cur_idx = -1;
+	spin_lock_init(&gHis_Log.lock);
+	return 0;
 }
