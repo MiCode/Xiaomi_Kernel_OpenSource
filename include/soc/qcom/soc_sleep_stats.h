df11f0acfadc7573fbf3d7ab13b08c14fabd09c0
Minghao Zhang <quic_minghao@quicinc.com>
2021-12-20 11:19:30 +0800

    soc: qcom: Modify output of ddr residency
    This change provides below functionality:
    1. In debugfs, ddr residency can be read; calculate duration
    percentage of ddr mode and ddr freq separately.
    2. Provides 3 APIs to sync ddr freq, get ddr residency and
    get the count of supported ddr freq.
    
    Change-Id: Icda97eb4de193209a9da09786dff1227ab5fb91e
    Signed-off-by: Minghao Zhang <quic_minghao@quicinc.com>

--- include/soc/qcom/soc_sleep_stats.h
+++ include/soc/qcom/soc_sleep_stats.h
@@ -1,15 +1,26 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
  */
 
 #ifndef __SOC_SLEEP_STATS_H__
 #define __SOC_SLEEP_STATS_H__
 
+struct ddr_freq_residency {
+	uint32_t freq;
+	uint64_t residency;
+};
+
 struct ddr_stats_ss_vote_info {
 	u32 ab; /* vote_x */
 	u32 ib; /* vote_y */
 };
+
+int ddr_stats_freq_sync_send_msg(void);
+int ddr_stats_get_freq_count(void);
+int ddr_stats_get_residency(int freq_count, struct ddr_freq_residency *data);
 #if IS_ENABLED(CONFIG_QCOM_SOC_SLEEP_STATS)
 
 int ddr_stats_get_ss_count(void);
