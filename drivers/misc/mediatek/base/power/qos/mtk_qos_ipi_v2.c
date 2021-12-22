1e4f4874956e48be5c17980568a0671daf263018
yunhu she <yunhu.she@mediatek.com>
2021-12-22 13:44:19 +0800

    [ALPS06432898] QOS:qos support mt6833 mt6877
    QOS:qos support mt6833 mt6877
    
    MTK-Commit-Id: 03dc3a598df44339100c2995c37c4cddbc7b482a
    
    CR-Id: ALPS06432898
    Feature: Configuration
    Change-Id: If6946aaf7c76f9404060c18e1dbded286df1f808
    Signed-off-by: yunhu she <yunhu.she@mediatek.com>

---
+++ drivers/misc/mediatek/base/power/qos/mtk_qos_ipi_v2.c
@@ -0,0 +1,151 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2018 MediaTek Inc.
+ */
+
+#include <linux/kthread.h>
+#include <linux/mutex.h>
+
+#ifdef CONFIG_MTK_TINYSYS_SSPM_SUPPORT
+#include <sspm_ipi_id.h>
+#include <sspm_define.h>
+#endif
+
+#include "mtk_qos_bound.h"
+#ifdef QOS_PREFETCH_SUPPORT
+#include "mtk_qos_prefetch.h"
+#endif /* QOS_PREFETCH_SUPPORT */
+#include "mtk_qos_ipi.h"
+
+#if defined(CONFIG_MTK_TINYSYS_SSPM_SUPPORT)
+static DEFINE_MUTEX(qos_ipi_mutex);
+static int qos_sspm_ready;
+int qos_ipi_ackdata;
+struct qos_ipi_data qos_recv_ackdata;
+
+static void qos_sspm_enable(void)
+{
+	struct qos_ipi_data qos_ipi_d;
+
+	qos_ipi_d.cmd = QOS_IPI_QOS_ENABLE;
+	qos_ipi_to_sspm_command(&qos_ipi_d, 1);
+}
+
+static int qos_ipi_recv_thread(void *arg)
+{
+	struct qos_ipi_data *qos_ipi_d;
+
+	pr_info("%s start!\n", __func__);
+	do {
+		mtk_ipi_recv(&sspm_ipidev, IPIR_I_QOS);
+
+		qos_ipi_d = &qos_recv_ackdata;
+
+		switch (qos_ipi_d->cmd) {
+		case QOS_IPI_QOS_BOUND:
+			qos_notifier_call_chain(
+					qos_ipi_d->u.qos_bound.state,
+					get_qos_bound());
+			break;
+#ifdef QOS_PREFETCH_SUPPORT
+		case QOS_IPI_QOS_PREFETCH_CB:
+			prefetch_notifier_call_chain(
+					qos_ipi_d->u.qos_prefetch_cb.state,
+					NULL);
+			break;
+		case QOS_IPI_QOS_PREFETCH_UPDATE:
+			if (is_qos_prefetch_enabled())
+				qos_prefetch_update_all();
+			break;
+#endif /* QOS_PREFETCH_SUPPORT */
+		default:
+			pr_info("wrong QoS IPI command: %d\n", qos_ipi_d->cmd);
+		}
+	} while (!kthread_should_stop());
+
+	return 0;
+}
+#endif
+
+int qos_ipi_to_sspm_command(void *buffer, int slot)
+{
+#if defined(CONFIG_MTK_TINYSYS_SSPM_SUPPORT)
+	int ret, ackdata;
+	struct qos_ipi_data *qos_ipi_d = buffer;
+	int slot_num = sizeof(struct qos_ipi_data)/SSPM_MBOX_SLOT_SIZE;
+
+	mutex_lock(&qos_ipi_mutex);
+
+	if (qos_sspm_ready != 1) {
+		pr_info("qos ipi not ready, skip cmd=%d\n", qos_ipi_d->cmd);
+		goto error;
+	}
+
+	qos_ipi_ackdata = 0;
+
+	if (slot > slot_num) {
+		pr_info("qos ipi cmd %d req slot error(%d > %d)\n",
+			qos_ipi_d->cmd, slot, slot_num);
+		goto error;
+	}
+
+	ret = mtk_ipi_send_compl(&sspm_ipidev, IPIS_C_QOS,
+		IPI_SEND_POLLING, buffer,
+		slot_num, 2000);
+	if (ret) {
+		pr_info("qos ipi cmd %d send fail,ret=%d\n",
+		qos_ipi_d->cmd, ret);
+		goto error;
+	}
+
+	if (!qos_ipi_ackdata) {
+		pr_info("qos ipi cmd %d ack fail, ackdata=%d\n",
+		qos_ipi_d->cmd, qos_ipi_ackdata);
+		goto error;
+	}
+	ackdata = qos_ipi_ackdata;
+	mutex_unlock(&qos_ipi_mutex);
+	return ackdata;
+error:
+	mutex_unlock(&qos_ipi_mutex);
+#endif
+	return -1;
+}
+
+void qos_ipi_init(void)
+{
+#if defined(CONFIG_MTK_TINYSYS_SSPM_SUPPORT)
+	unsigned int ret;
+	/* for AP to SSPM */
+	ret = mtk_ipi_register(&sspm_ipidev, IPIS_C_QOS, NULL, NULL,
+				(void *) &qos_ipi_ackdata);
+	if (ret) {
+		pr_info("qos IPIS_C_QOS ipi_register fail, ret %d\n", ret);
+		qos_sspm_ready = -1;
+		return;
+	}
+
+	/* for SSPM to AP */
+	ret = mtk_ipi_register(&sspm_ipidev, IPIR_I_QOS, NULL, NULL,
+				(void *) &qos_recv_ackdata);
+	if (ret) {
+		pr_info("qos IPIR_I_QOS ipi_register fail, ret %d\n", ret);
+		qos_sspm_ready = -2;
+		return;
+	}
+	qos_sspm_ready = 1;
+	qos_sspm_enable();
+	pr_info("qos ipi is ready!\n");
+#endif
+}
+
+void qos_ipi_recv_init(void)
+{
+#if defined(CONFIG_MTK_TINYSYS_SSPM_SUPPORT)
+	if (qos_sspm_ready != 1) {
+		pr_info("QOS SSPM not ready, recv thread not start!\n");
+		return;
+	}
+	kthread_run(qos_ipi_recv_thread, NULL, "qos_ipi_recv");
+#endif
+}
---
+++ drivers/misc/mediatek/base/power/qos/mtk_qos_share.c
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2018 MediaTek Inc.
+ */
+
+#include <linux/io.h>
+#include <mtk_qos_ipi.h>
+#include <mtk_qos_share.h>
+
+#ifdef CONFIG_MTK_TINYSYS_SSPM_SUPPORT
+#include <sspm_reservedmem_define.h>
+#include <sspm_reservedmem.h>
+#endif
+
+struct qos_rec_data *qos_share_ref;
+
+/* share dram for subsys related table communication */
+static phys_addr_t rec_phys_addr, rec_virt_addr;
+static unsigned long long rec_size;
+
+static void qos_share_sspm_setup(void)
+{
+#if defined(CONFIG_MTK_TINYSYS_SSPM_SUPPORT)
+	struct qos_ipi_data qos_ipi_d;
+
+	qos_ipi_d.cmd = QOS_IPI_QOS_SHARE_INIT;
+	qos_ipi_d.u.qos_share_init.dram_addr = rec_phys_addr;
+	qos_ipi_d.u.qos_share_init.dram_size = rec_size;
+	qos_ipi_to_sspm_command(&qos_ipi_d, 3);
+#endif
+}
+
+
+
+static void qos_get_rec_addr(phys_addr_t *phys,
+		       phys_addr_t *virt,
+		       unsigned long long *size)
+{
+#ifdef CONFIG_MTK_TINYSYS_SSPM_SUPPORT
+	/* get sspm reserved mem */
+	*phys = sspm_reserve_mem_get_phys(QOS_MEM_ID);
+	*virt = sspm_reserve_mem_get_virt(QOS_MEM_ID);
+	*size = sspm_reserve_mem_get_size(QOS_MEM_ID);
+
+	pr_info("qos: rec phy_addr = 0x%llx, virt_addr=0x%llx, size = %llu\n",
+		(unsigned long long) *phys,
+		(unsigned long long) *virt,
+		*size);
+
+#endif
+}
+
+static int qos_reserve_mem_init(phys_addr_t *virt,
+			   unsigned long long *size)
+{
+	int i;
+	unsigned char *ptr;
+
+	if (!virt)
+		return -1;
+
+	/* clear reserve mem */
+	ptr = (unsigned char *)(uintptr_t)*virt;
+	for (i = 0; i < *size; i++)
+		ptr[i] = 0x0;
+
+	return 0;
+}
+
+
+int qos_init_rec_share(void)
+{
+	int ret = 0;
+
+	qos_get_rec_addr(&rec_phys_addr,
+			  &rec_virt_addr,
+			  &rec_size);
+
+	qos_share_ref = (struct qos_rec_data *)(uintptr_t)rec_virt_addr;
+
+	if (!qos_share_ref) {
+		pr_info("qos: get sspm dram addr failed\n");
+		ret = -1;
+		goto end;
+	}
+
+	ret = qos_reserve_mem_init(&rec_virt_addr, &rec_size);
+	qos_share_sspm_setup();
+
+end:
+	return ret;
+
+}
+
+unsigned int qos_rec_get_hist_bw(unsigned int idx, unsigned int type)
+{
+	unsigned int val = 0;
+
+	if (!qos_share_ref)
+		return val;
+
+	if (idx >= HIST_NUM || type >= BW_TYPE)
+		return val;
+
+	val = qos_share_ref->bw_hist[idx][type];
+
+	return val;
+}
+
+unsigned int qos_rec_get_hist_data_bw(unsigned int idx, unsigned int type)
+{
+	unsigned int val = 0;
+
+	if (!qos_share_ref)
+		return val;
+
+	if (idx >= HIST_NUM || type >= BW_TYPE)
+		return val;
+
+	val = qos_share_ref->data_bw_hist[idx][type];
+
+	return val;
+}
+
+unsigned int qos_rec_get_hist_idx(void)
+{
+	if (qos_share_ref)
+		return qos_share_ref->current_hist;
+	else
+		return 0xFFFF;
+}
