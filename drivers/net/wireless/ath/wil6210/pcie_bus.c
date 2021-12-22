2de103c7495e4b91e922e6f97b55fb1539ffb6e7
Visweswara Tanuku <quic_vtanuku@quicinc.com>
2021-12-22 09:02:39 +0530

    wil6210: check integrity of received AMSDU packets
    Check integrity of received AMSDU packet, 802.11ad QoS spec requires
    that AMSDU frame contains only MSDUs whose destination address (DA)
    and sender address (SA) parameter values map to the same receiver
    address (RA) and transmitter address (TA) values.
    wil6210 Talyn HW does not check this before it cuts the AMSDU frame
    into multiple received MSDU packets.
    Adding checks to wil6210 driver to enforce spec compliance behavior
    by checking all AMSDU sub frames if it complies with the following:
    1- On AP, check packet SA is its client mac address, and on Client
       check that the DA is the local mac address. If not drop the packet
    2- if AMSDU sub frame was dropped on item 1, drop all next sub frame
       of the AMSDU by checking it has same sn/tid.
    
    This patch drops all WDS frames before it checks valid AMSDU, WDS
    includes supporting MAC header with 4 addresses which is not supported
    yet, moreover, WDS implies different validity checks on AMSDU frame.
    
    Change-Id: I71a39f95c034f05023e0e7ae3ffb5d2b4f8c6b24
    Signed-off-by: Ahmad Masri <amasri@codeaurora.org>
    Signed-off-by: Visweswara Tanuku <quic_vtanuku@quicinc.com>

--- drivers/net/wireless/ath/wil6210/pcie_bus.c
+++ drivers/net/wireless/ath/wil6210/pcie_bus.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: ISC
 /*
  * Copyright (c) 2012-2017 Qualcomm Atheros, Inc.
- * Copyright (c) 2018-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
  */
 
 #include <linux/module.h>
@@ -97,7 +97,6 @@ int wil_set_capabilities(struct wil6210_priv *wil)
 		set_bit(hw_capa_no_flash, wil->hw_capa);
 		wil->use_enhanced_dma_hw = true;
 		wil->use_rx_hw_reordering = true;
-		wil->use_compressed_rx_status = true;
 		wil_fw_name = ftm_mode ? WIL_FW_NAME_FTM_TALYN :
 			      WIL_FW_NAME_TALYN;
 		if (wil_fw_verify_file_exists(wil, wil_fw_name))
