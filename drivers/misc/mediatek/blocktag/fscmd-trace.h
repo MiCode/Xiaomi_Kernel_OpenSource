424b24756785256f01cbd719cdcee64bf39f13c4
Ed Tsai <ed.tsai@mediatek.com>
2021-12-22 13:49:34 +0800

    [ALPS06457734] blocktag: FSCMD: revert "add cp and gc event log"
    This reverts commit cdbbd847cdeb43f9944bae5d7b88d47b28e2c61b.
    
    Revert this cause it would let sanity test fail.
    
    MTK-Commit-Id: 248b475b7c6eeace1d8926c1fee503f7d76bf4a5
    
    CR-Id: ALPS06457734
    Feature: Storage I/O Performance Profiling Tool(blocktag)
    Signed-off-by: Ed Tsai <ed.tsai@mediatek.com>
    Change-Id: I84c9d051f09712ab953e417909fc45bcd4dc18db

--- drivers/misc/mediatek/blocktag/fscmd-trace.h
+++ drivers/misc/mediatek/blocktag/fscmd-trace.h
@@ -16,24 +16,6 @@ void fscmd_trace_sys_enter(void *data,
 void fscmd_trace_sys_exit(void *data,
 		struct pt_regs *regs, long ret);
 
-void fscmd_trace_f2fs_write_checkpoint(void *data,
-		struct super_block *sb,
-		int reason, char *msg);
-
-void fscmd_trace_f2fs_gc_begin(void *data,
-		struct super_block *sb, bool sync, bool background,
-		long long dirty_nodes, long long dirty_dents,
-		long long dirty_imeta, unsigned int free_sec,
-		unsigned int free_seg, int reserved_seg,
-		unsigned int prefree_seg);
-
-void fscmd_trace_f2fs_gc_end(void *data,
-	struct super_block *sb, int ret, int seg_freed,
-	int sec_freed, long long dirty_nodes,
-	long long dirty_dents, long long dirty_imeta,
-	unsigned int free_sec, unsigned int free_seg,
-	int reserved_seg, unsigned int prefree_seg);
-
 void mtk_fscmd_show(char **buff, unsigned long *size,
 	struct seq_file *seq);
 
