40878bf69a22bd0e63b167ba230442800af943ba
Eddie Hung <eddie.hung@mediatek.com>
2021-12-22 13:38:50 +0800

    [ALPS06442050] mtu3: add memory barrier before set gpd HWO
    There is a seldom issue that ssusb hw access invalid address
    and trigger devapc or emimpu violation. That is due to memory
    misorder and cause gpd data is not correct.
    
    Make sure gpd data already write done before set gpd HWO.
    
    MTK-Commit-Id: e533aedc20f172ac986bacd92c7fabaeffecfe1c
    
    Signed-off-by: Eddie Hung <eddie.hung@mediatek.com>
    CR-Id: ALPS06442050
    Feature: USB 3.0 Device
    Change-Id: If6c300676518559532ba83477b3a77a886dcf506

--- drivers/usb/mtu3/mtu3_qmu.c
+++ drivers/usb/mtu3/mtu3_qmu.c
@@ -273,6 +273,8 @@ static int mtu3_prepare_tx_gpd(struct mtu3_ep *mep, struct mtu3_request *mreq)
 			gpd->dw3_info |= cpu_to_le32(GPD_EXT_FLAG_ZLP);
 	}
 
+	/* requires a memory barrier */
+	smp_mb();
 	gpd->dw0_info |= cpu_to_le32(GPD_FLAGS_IOC | GPD_FLAGS_HWO);
 
 	mreq->gpd = gpd;
@@ -306,6 +308,8 @@ static int mtu3_prepare_rx_gpd(struct mtu3_ep *mep, struct mtu3_request *mreq)
 	gpd->next_gpd = cpu_to_le32(lower_32_bits(enq_dma));
 	ext_addr |= GPD_EXT_NGP(mtu, upper_32_bits(enq_dma));
 	gpd->dw3_info = cpu_to_le32(ext_addr);
+	/* requires a memory barrier */
+	smp_mb();
 	gpd->dw0_info |= cpu_to_le32(GPD_FLAGS_IOC | GPD_FLAGS_HWO);
 
 	mreq->gpd = gpd;
@@ -445,6 +449,9 @@ static void qmu_tx_zlp_error_handler(struct mtu3 *mtu, u8 epnum)
 		return;
 	}
 	mtu3_setbits(mbase, MU3D_EP_TXCR0(mep->epnum), TX_TXPKTRDY);
+
+	/* requires a memory barrier */
+	smp_mb();
 
 	/* by pass the current GDP */
 	gpd_current->dw0_info |= cpu_to_le32(GPD_FLAGS_BPS | GPD_FLAGS_HWO);
