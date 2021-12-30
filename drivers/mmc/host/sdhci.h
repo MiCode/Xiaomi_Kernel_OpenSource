e9a8ea44931c3a5c89809d00046cb1a0d4a24986
Sarthak Garg <quic_sartgarg@quicinc.com>
2021-12-30 17:40:46 +0530

    mmc: sdhci: Dump registers in case of DATA CRC/timeout errors
    Dump registers in case of Data CRC/timeout errors and print other
    useful information.
    
    This contains below patches as well:
    
    4c196de7e0fd mmc: sdhci: rate limit sdhci_dumpregs() prints
    16dabee056d4 mmc: sdhci: Add timestamp debug info for data timeout error
    e00d878df07e mmc: sdhci: Avoid dumping registers when SD card removed.
    
    Change-Id: I5efe8ff4bacc5da3a05829be7cac4ce40f9fc584
    Signed-off-by: Sarthak Garg <quic_sartgarg@quicinc.com>

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -15,7 +15,7 @@
 #include <linux/io.h>
 #include <linux/leds.h>
 #include <linux/interrupt.h>
-
+#include <linux/ratelimit.h>
 #include <linux/mmc/host.h>
 
 /*
@@ -616,6 +616,10 @@ struct sdhci_host {
 
 	u64			data_timeout;
 
+#if defined(CONFIG_SDC_QTI)
+	ktime_t data_start_time;
+	struct ratelimit_state dbg_dump_rs;
+#endif
 	unsigned long private[0] ____cacheline_aligned;
 };
 
