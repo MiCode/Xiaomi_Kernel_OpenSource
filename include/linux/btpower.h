517f3a552dbba14f5b98cb43e800790a69659184
Balakrishna Godavarthi <quic_bgodavar@quicinc.com>
2021-12-30 14:48:51 +0530

    Add out of band sleep support to bluetooth driver
    This change adds out of band sleep support to bluetooth
    power driver. This feature will be enabled when macro
    CONFIG_MSM_BT_OOBS is define.
    
    Change-Id: I74b231956ad0884528b17720cbfa3639281af9a3
    Signed-off-by: Balakrishna Godavarthi <quic_bgodavar@quicinc.com>

--- include/linux/btpower.h
+++ include/linux/btpower.h
@@ -6,6 +6,7 @@
 #ifndef __LINUX_BLUETOOTH_POWER_H
 #define __LINUX_BLUETOOTH_POWER_H
 
+#include <linux/cdev.h>
 #include <linux/types.h>
 #include <linux/mailbox_client.h>
 #include <linux/mailbox/qmp.h>
@@ -57,6 +58,11 @@ struct btpower_platform_data {
 	int wl_gpio_sys_rst;                   /* Wlan reset gpio */
 	int bt_gpio_sw_ctrl;                   /* Bluetooth sw_ctrl gpio */
 	int bt_gpio_debug;                     /* Bluetooth debug gpio */
+#ifdef CONFIG_MSM_BT_OOBS
+	int bt_gpio_dev_wake;                  /* Bluetooth bt_wake */
+	int bt_gpio_host_wake;                 /* Bluetooth bt_host_wake */
+	int irq;                               /* Bluetooth host_wake IRQ */
+#endif
 	int xo_gpio_clk;                       /* XO clock gpio*/
 	struct device *slim_dev;
 	struct bt_power_vreg_data *vreg_info;  /* VDDIO voltage regulator */
@@ -67,6 +73,10 @@ struct btpower_platform_data {
 	struct mbox_client mbox_client_data;
 	struct mbox_chan *mbox_chan;
 	const char *vreg_ipa;
+#ifdef CONFIG_MSM_BT_OOBS
+	struct file *reffilp_obs;
+	struct task_struct *reftask_obs;
+#endif
 };
 
 int btpower_register_slimdev(struct device *dev);
@@ -80,5 +90,10 @@ int btpower_aop_mbox_init(struct btpower_platform_data *pdata);
 #define BT_CMD_CHECK_SW_CTRL	0xbfb0
 #define BT_CMD_GETVAL_POWER_SRCS	0xbfb1
 #define BT_CMD_SET_IPA_TCS_INFO  0xbfc0
+
+#ifdef CONFIG_MSM_BT_OOBS
+#define BT_CMD_OBS_SIGNAL_TASK		0xbfd0
+#define BT_CMD_OBS_VOTE_CLOCK		0xbfd1
+#endif
 
 #endif /* __LINUX_BLUETOOTH_POWER_H */
