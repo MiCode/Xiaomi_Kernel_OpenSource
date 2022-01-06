c482eb562ed5086b7141c23ca25ceae49a3ba1f2
Cheng Jui Wang <cheng-jui.wang@mediatek.com>
2022-01-07 01:58:15 +0800

    [ALPS06464135] locking_aee: remove debug_locks_off kretprobe
    The noinstr of debug_locks_off is removed by
    commit 07f95d5cb565 ("[ALPS06370394] Merge android12-5.10 into
    alps-mp-s0.mp1")
    
    Various deadlocks caused by calling aee_kernel_warning_api in kretprobe
    are detected.
    
    Remove the kretprobe and let locking_aee_thread to handle aee.
    
    MTK-Commit-Id: 8888d6567d3e46ff8dae2627553754aa0cfc4adc
    
    Signed-off-by: Cheng Jui Wang <cheng-jui.wang@mediatek.com>
    CR-Id: ALPS06464135
    Feature: [Module]Lockdep
    Change-Id: Ieeaed24e3d9bad751eeda1bb9eaccb9be2f383e2
    (cherry picked from commit fe8f6a167df94182c407af9bae9a402fa278e1b3)

--- drivers/misc/mediatek/locking/locking_aee_main.c
+++ drivers/misc/mediatek/locking/locking_aee_main.c
@@ -133,14 +133,6 @@ static void ldt_disable_aee(void)
 
 static int __init locking_aee_init(void)
 {
-	int ret;
-
-	ret = register_kretprobe(&debug_locks_off_kretprobe);
-	if (ret < 0)
-		pr_info("register debug_locks_off kretprobe failed, returned %d\n", ret);
-	else
-		pr_info("register debug_locks_off kretprobe succeeded.\n");
-
 	monitor_hang_regist_ldt(ldt_disable_aee);
 	kthread_run(locking_aee_thread, NULL, "locking_aee");
 	lockdep_test_init();
