78068218c270cd86f580298c4db389ddd449f0d5
Qilin Tan <qilin.tan@mediatek.com>
2022-01-07 01:57:53 +0800

    [ALPS06441419] scsi: ufs: fix the code coverity
    fix the code coverity
    
    MTK-Commit-Id: 084dfc1032a875e8fe0ece40b791256efc9b9f60
    
    Signed-off-by: Qilin Tan <qilin.tan@mediatek.com>
    CR-Id: ALPS06441419
    Feature: UFS(Universal Flash Storage)
    Change-Id: I5cb4f08c8145a75a2eaed1efba577868dc9fb1ed

--- drivers/scsi/ufs/ufs-mediatek.c
+++ drivers/scsi/ufs/ufs-mediatek.c
@@ -1903,14 +1903,14 @@ static bool ufs_mtk_has_vcc_always_on(struct ufs_hba *hba) {
 	struct ufs_mtk_host *host = ufshcd_get_variant(hba);
 
 	return (host && host->cfg
-		&& (host->cfg->quirks | UFS_MTK_HOST_QUIRK_UFS_VCC_ALWAYS_ON));
+		&& (host->cfg->quirks & UFS_MTK_HOST_QUIRK_UFS_VCC_ALWAYS_ON));
 }
 
 static bool ufs_mtk_has_ufshci_perf_heuristic(struct ufs_hba *hba) {
 	struct ufs_mtk_host *host = ufshcd_get_variant(hba);
 
 	return (host && host->cfg
-		&& (host->cfg->quirks | UFS_MTK_HOST_QUIRK_UFS_HCI_PERF_HEURISTIC));
+		&& (host->cfg->quirks & UFS_MTK_HOST_QUIRK_UFS_HCI_PERF_HEURISTIC));
 }
 
 /**
