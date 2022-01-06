3889e32b5ab722bc242f1260dfc88e7916270464
Poting Chen <poting.chen@mediatek.com>
2022-01-07 02:07:11 +0800

    [ALPS06468819] GBE: integrate with thermal
    [Detail]
    if device is too hot, do not boost
    
    MTK-Commit-Id: 084c2d62dbab40298855dadbaf4766f9eed29e1b
    
    CR-Id: ALPS06468819
    Feature: FPSGO
    Signed-off-by: Poting Chen <poting.chen@mediatek.com>
    Change-Id: I8f3e7d116a3e009c021a724dd6ed89239f4a9c27

--- drivers/misc/mediatek/performance/fpsgo_v3/gbe/gbe_common.c
+++ drivers/misc/mediatek/performance/fpsgo_v3/gbe/gbe_common.c
@@ -17,6 +17,7 @@
 #include <linux/slab.h>
 #include <linux/miscdevice.h>   /* for misc_register, and SYNTH_MINOR */
 #include <linux/proc_fs.h>
+#include <thermal_interface.h>
 
 
 #include <linux/workqueue.h>
@@ -26,6 +27,8 @@
 #include <linux/sched.h>
 #include <linux/slab.h>
 #include <linux/pm_qos.h>
+#include "mt-plat/fpsgo_common.h"
+#include "fpsgo_base.h"
 #include "gbe_common.h"
 #include "gbe1.h"
 #include "gbe2.h"
@@ -34,6 +37,7 @@
 static DEFINE_MUTEX(gbe_lock);
 static int boost_set[KIR_NUM];
 static unsigned long policy_mask;
+static int thrm_hdrm_thrs;
 
 enum GBE_BOOST_DEVICE {
 	GBE_BOOST_UNKNOWN = -1,
@@ -109,9 +113,12 @@ void gbe_ctrl2base_get_pwr_cmd(int *cmd, int *value1, int *value2)
 	mutex_unlock(&gbe2pwr_lock);
 }
 
+int __attribute__ ((weak))  get_thermal_headroom(enum headroom_id id)
+{ return 1000; }
 void gbe_boost(enum GBE_KICKER kicker, int boost)
 {
 	int i;
+	int thermal_headroom;
 	int boost_final = 0;
 	int cpu_boost = 0, eas_boost = -1, vcore_boost = -1,
 			io_boost = 0, he_boost = 0, gpu_boost = 0,
@@ -130,7 +137,11 @@ void gbe_boost(enum GBE_KICKER kicker, int boost)
 			break;
 		}
 
-	if (boost_final) {
+	thermal_headroom = get_thermal_headroom(PCB_AP);
+	if (thrm_hdrm_thrs > 0)
+		gbe_trace_count(-100, 0, thermal_headroom, "gbe_thrm_hdrm");
+
+	if (boost_final && thermal_headroom > thrm_hdrm_thrs) {
 		cpu_boost = 1;
 		eas_boost = 100;
 		vcore_boost = 0;
@@ -165,6 +176,52 @@ out:
 	mutex_unlock(&gbe_lock);
 
 }
+
+static ssize_t gbe_thrm_hdrm_thrs_show(struct kobject *kobj,
+		struct kobj_attribute *attr,
+		char *buf)
+{
+	return scnprintf(buf, PAGE_SIZE, "%d\n", thrm_hdrm_thrs);
+}
+
+static ssize_t gbe_thrm_hdrm_thrs_store(struct kobject *kobj,
+		struct kobj_attribute *attr,
+		const char *buf, size_t count)
+{
+	int val = 0;
+	char acBuffer[GBE_SYSFS_MAX_BUFF_SIZE];
+	int arg;
+	int cpu_boost = 0, eas_boost = -1, vcore_boost = -1,
+			io_boost = 0, he_boost = 0, gpu_boost = 0,
+			llf_boost = 0;
+
+	if ((count > 0) && (count < GBE_SYSFS_MAX_BUFF_SIZE)) {
+		if (scnprintf(acBuffer, GBE_SYSFS_MAX_BUFF_SIZE, "%s", buf)) {
+			if (kstrtoint(acBuffer, 0, &arg) == 0)
+				val = arg;
+			else
+				return count;
+		}
+	}
+
+	if (val > 1000 || val < 0)
+		return count;
+
+	mutex_lock(&gbe_lock);
+	thrm_hdrm_thrs = val;
+
+	gbe_sentcmd(GBE_BOOST_CPU, cpu_boost, -1);
+	gbe_sentcmd(GBE_BOOST_EAS, eas_boost, -1);
+	gbe_sentcmd(GBE_BOOST_VCORE, vcore_boost, -1);
+	gbe_sentcmd(GBE_BOOST_IO, io_boost, -1);
+	gbe_sentcmd(GBE_BOOST_HE, he_boost, -1);
+	gbe_sentcmd(GBE_BOOST_GPU, gpu_boost, -1);
+	gbe_sentcmd(GBE_BOOST_LLF, llf_boost, -1);
+	mutex_unlock(&gbe_lock);
+
+	return count;
+}
+static KOBJ_ATTR_RW(gbe_thrm_hdrm_thrs);
 
 static ssize_t gbe_policy_mask_show(struct kobject *kobj,
 		struct kobj_attribute *attr,
@@ -216,6 +273,7 @@ static KOBJ_ATTR_RW(gbe_policy_mask);
 void exit_gbe_common(void)
 {
 	gbe_sysfs_remove_file(&kobj_attr_gbe_policy_mask);
+	gbe_sysfs_remove_file(&kobj_attr_gbe_thrm_hdrm_thrs);
 	gbe_sysfs_exit();
 	gbe1_exit();
 	gbe2_exit();
@@ -231,6 +289,7 @@ int init_gbe_common(void)
 	gbe2_init();
 
 	gbe_sysfs_create_file(&kobj_attr_gbe_policy_mask);
+	gbe_sysfs_create_file(&kobj_attr_gbe_thrm_hdrm_thrs);
 
 
 	set_bit(GBE_BOOST_CPU, &policy_mask);
