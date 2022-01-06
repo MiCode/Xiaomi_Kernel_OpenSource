b6652ba459592c2d7c5bb857d942e14053f18b3d
Ya-Wen Hsu <Ya-Wen.Hsu@mediatek.com>
2022-01-07 02:02:02 +0800

    [ALPS06469507] RSC: remove ipe-sys dev node
    [Detail] Remove ipe-sys device node
    
    MTK-Commit-Id: 8775e8a4cee34b92e2cf0c8e4ab4783f9eb69e74
    
    Signed-off-by: Ya-Wen Hsu <Ya-Wen.Hsu@mediatek.com>
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: I5044ab60820b80959d4f97bab3e1832db820a625
    CR-Id: ALPS06469507

--- drivers/misc/mediatek/cameraisp/rsc/isp_6s/camera_rsc.c
+++ drivers/misc/mediatek/cameraisp/rsc/isp_6s/camera_rsc.c
@@ -281,7 +281,7 @@ static struct cmdq_base       *cmdq_base;
 static struct cmdq_client     *cmdq_clt;
 static s32                    cmdq_event_id;
 /* Get HW modules' base address from device nodes */
-#define RSC_DEV_NODE_IDX 1
+#define RSC_DEV_NODE_IDX 0
 #define IPESYS_DEV_MODE_IDX 0
 
 
@@ -3050,7 +3050,7 @@ static signed int RSC_probe(struct platform_device *pDev)
 #endif
 
 	/* Only register char driver in the 1st time */
-	if (nr_RSC_devs == 2) {
+	if (nr_RSC_devs == 1) {
 
 		/* Register char driver */
 		Ret = RSC_RegCharDev();
@@ -3306,7 +3306,7 @@ int RSC_pm_restore_noirq(struct device *device)
 
 
 static const struct of_device_id RSC_of_ids[] = {
-	{.compatible = "mediatek,ipesys_config",},
+/*	{.compatible = "mediatek,ipesys_config",},*/
 	{.compatible = "mediatek,rsc",},
 	{}
 };
