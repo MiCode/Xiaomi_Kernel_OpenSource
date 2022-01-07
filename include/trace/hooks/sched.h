eb994f2728240e45740702a76bac52dc80dcd4ed
Rick Yiu <rickyiu@google.com>
2022-01-07 18:31:06 +0800

    ANDROID: sched: Add vendor hook for util-update related functions
    Vendor may have the need to implement their own util tracking.
    
    Bug: 201260585
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Change-Id: I973902e6ff82a85ecd029ac5a78692d629df1ebe

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -444,6 +444,27 @@ DECLARE_RESTRICTED_HOOK(android_rvh_set_cpus_allowed_comm,
 DECLARE_HOOK(android_vh_sched_setaffinity_early,
 	TP_PROTO(struct task_struct *p, const struct cpumask *new_mask, int *retval),
 	TP_ARGS(p, new_mask, retval));
+
+DECLARE_RESTRICTED_HOOK(android_rvh_attach_entity_load_avg,
+	TP_PROTO(struct cfs_rq *cfs_rq, struct sched_entity *se),
+	TP_ARGS(cfs_rq, se), 1);
+
+DECLARE_RESTRICTED_HOOK(android_rvh_detach_entity_load_avg,
+	TP_PROTO(struct cfs_rq *cfs_rq, struct sched_entity *se),
+	TP_ARGS(cfs_rq, se), 1);
+
+DECLARE_RESTRICTED_HOOK(android_rvh_update_load_avg,
+	TP_PROTO(u64 now, struct cfs_rq *cfs_rq, struct sched_entity *se),
+	TP_ARGS(now, cfs_rq, se), 1);
+
+DECLARE_RESTRICTED_HOOK(android_rvh_remove_entity_load_avg,
+	TP_PROTO(struct cfs_rq *cfs_rq, struct sched_entity *se),
+	TP_ARGS(cfs_rq, se), 1);
+
+DECLARE_RESTRICTED_HOOK(android_rvh_update_blocked_fair,
+	TP_PROTO(struct rq *rq),
+	TP_ARGS(rq), 1);
+
 /* macro versions of hooks are no longer required */
 
 #endif /* _TRACE_HOOK_SCHED_H */
