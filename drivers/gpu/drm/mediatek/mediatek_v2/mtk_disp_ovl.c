72987d8df961c1919d96cb72a1c4a334dfb3d4ce
Cui Zhang <cui.zhang@mediatek.com>
2022-01-20 11:23:35 +0800

    [ALPS06493322] display: smi dump of underflow
    [Detail]
    smi dump every 1000 ovl underflow
    ovl dump every 1000 ovl underflow
    
    MTK-Commit-Id: dd62aa66f8439f19f9e1985630b7cf03691bac18
    
    Signed-off-by: Cui Zhang <cui.zhang@mediatek.com>
    CR-Id: ALPS06493322
    Feature: [Module]Display Driver
    Change-Id: I7e9018da87038c26ccca7a5e1ccfe7bcd3bd0988

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_ovl.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_ovl.c
@@ -754,8 +754,13 @@ static irqreturn_t mtk_disp_ovl_irq_handler(int irq, void *dev_id)
 		DDPPR_ERR("[IRQ] %s: frame underflow! cnt=%d\n",
 			  mtk_dump_comp_str(ovl), priv->underflow_cnt);
 		priv->underflow_cnt++;
-//		mtk_ovl_dump(ovl);
-//		mtk_ovl_analysis(ovl);
+		if (priv->underflow_cnt % 1000 == 0) {
+			if (ovl->id == DDP_COMPONENT_OVL0 ||
+			    ovl->id == DDP_COMPONENT_OVL1)
+				mtk_smi_dbg_hang_detect("ovl-underflow");
+			mtk_ovl_dump(ovl);
+			mtk_ovl_analysis(ovl);
+		}
 	}
 	if (val & (1 << 3))
 		DDPIRQ("[IRQ] %s: sw reset done!\n", mtk_dump_comp_str(ovl));
