5ec510d15b3ac917241eb1d9f5b0eb581c452d2b
Todd Kjos <tkjos@google.com>
2022-01-20 11:24:12 +0800

    [ALPS06511517]  binder: use euid from cred instead of using task
    Save the struct cred associated with a binder process
    at initial open to avoid potential race conditions
    when converting to an euid.
    
    Set a transactions sender_euid from the struct cred
    saved at binder_open() instead of looking up the euid
    from the binder procs struct task. This ensures
    the euid is associated with the security context that
    of the task that opened binder.
    
    MTK-Commit-Id: ae5708a2b30265c208ea7c66e0158281ffc2e610
    
    Cc: stable@vger.kernel.org # 4.4+
    Fixes: 457b9a6f09f0 ("Staging: android: add binder driver")
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Suggested-by: Stephen Smalley <stephen.smalley.work@gmail.com>
    Suggested-by: Jann Horn <jannh@google.com>
    Acked-by: Casey Schaufler <casey@schaufler-ca.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    Change-Id: I91922e7f359df5901749f1b09094c3c68d45aed4
    Bug: 200688826
    Signed-off-by: Frankie Chang <frankie.chang@mediatek.com>
    CR-Id: ALPS06511517
    Feature: Google Security Patch

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -463,6 +463,9 @@ struct binder_priority {
  * @files                 files_struct for process
  *                        (protected by @files_lock)
  * @files_lock            mutex to protect @files
+ * @cred                  struct cred associated with the `struct file`
+ *                        in binder_open()
+ *                        (invariant after initialized)
  * @deferred_work_node:   element for binder_deferred_list
  *                        (protected by binder_deferred_lock)
  * @deferred_work:        bitmap of deferred work to perform
@@ -510,6 +513,7 @@ struct binder_proc {
 	struct task_struct *tsk;
 	struct files_struct *files;
 	struct mutex files_lock;
+	const struct cred *cred;
 	struct hlist_node deferred_work_node;
 	int deferred_work;
 	bool is_dead;
@@ -3252,7 +3256,7 @@ static void binder_transaction(struct binder_proc *proc,
 		t->from = thread;
 	else
 		t->from = NULL;
-	t->sender_euid = task_euid(proc->tsk);
+	t->sender_euid = proc->cred->euid;
 	t->to_proc = target_proc;
 	t->to_thread = target_thread;
 	t->code = tr->code;
@@ -4761,6 +4765,7 @@ static void binder_free_proc(struct binder_proc *proc)
 	}
 	binder_alloc_deferred_release(&proc->alloc);
 	put_task_struct(proc->tsk);
+	put_cred(proc->cred);
 	binder_stats_deleted(BINDER_STAT_PROC);
 	kfree(proc);
 }
@@ -5290,6 +5295,7 @@ static int binder_open(struct inode *nodp, struct file *filp)
 	get_task_struct(current->group_leader);
 	proc->tsk = current->group_leader;
 	mutex_init(&proc->files_lock);
+	proc->cred = get_cred(filp->f_cred);
 	INIT_LIST_HEAD(&proc->todo);
 	if (binder_supported_policy(current->policy)) {
 		proc->default_priority.sched_policy = current->policy;
