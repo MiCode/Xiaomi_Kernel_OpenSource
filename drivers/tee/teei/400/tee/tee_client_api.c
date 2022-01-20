3c5daafe251b9f85b79e3fcf6126b0f966ee2497
huifen wang <huifen.wang@mediatek.com>
2022-01-20 11:33:37 +0800

    [ALPS06499100] TEEI:fix Vunlerability issue
    1. Fix KE when Syzkaller scan node sys/devices/platform/utos/teei_log_level
    2. Fix race condition lead to type confusion issue
    
    MTK-Commit-Id: 74f4c0285347c5490a35a69c8e0579b4577179c4
    
    Signed-off-by: huifen wang <huifen.wang@mediatek.com>
    CR-Id: ALPS06499100
    Feature: Vulnerability Scan
    Change-Id: Ia39cb4b4df7061a2737e07ec0f6539361236c482

--- drivers/tee/teei/400/tee/tee_client_api.c
+++ drivers/tee/teei/400/tee/tee_client_api.c
@@ -672,6 +672,11 @@ TEEC_Result TEEC_RegisterSharedMemory(struct TEEC_Context *ctx,
 	dma_buf = dma_buf_get(fd);
 	close(fd);
 
+	if (IS_ERR(dma_buf)) {
+		shm->id = -1;
+		return TEEC_ERROR_GENERIC;
+	}
+
 	if (!dma_buf) {
 		shm->id = -1;
 		return TEEC_ERROR_OUT_OF_MEMORY;
@@ -739,6 +744,11 @@ TEEC_Result TEEC_AllocateSharedMemory(struct TEEC_Context *ctx,
 		return TEEC_ERROR_OUT_OF_MEMORY;
 	dma_buf = dma_buf_get(fd);
 	close(fd);
+
+	if (IS_ERR(dma_buf)) {
+		shm->id = -1;
+		return TEEC_ERROR_GENERIC;
+	}
 
 	if (!dma_buf) {
 		shm->id = -1;
--- drivers/tee/teei/400/tz_driver/teei_client_main.c
+++ drivers/tee/teei/400/tz_driver/teei_client_main.c
@@ -36,7 +36,7 @@
 #if KERNEL_VERSION(4, 19, 0) <= LINUX_VERSION_CODE
 //#include "linux/bootprof.h"
 #else
-#include "bootprof.h
+#include "bootprof.h"
 #endif /* KERNEL_VERSION */
 #endif /* CONFIG_MTPROF */
 
@@ -1031,11 +1031,6 @@ static int teei_probe(struct platform_device *pdev)
 	ut_irq = platform_get_irq(pdev, 0);
 	IMSG_INFO("teei device ut_irq is %d\n", ut_irq);
 
-	if (init_sysfs(pdev) < 0) {
-		IMSG_ERROR("failed to init tz_driver sysfs\n");
-		return -1;
-	}
-
 	if (register_ut_irq_handler(ut_irq) < 0) {
 		IMSG_ERROR("teei_device can't register irq %d\n", ut_irq);
 		return -1;
@@ -1282,6 +1277,11 @@ static int teei_client_init(void)
 		goto uninit_teei_fp;
 	}
 
+	ret_code = init_sysfs(g_teei_pdev);
+	if (ret_code < 0) {
+		IMSG_ERROR("failed to init tz_driver sysfs %d!\n", ret_code);
+		goto uninit_teei_fp;
+	}
 	goto return_fn;
 
 uninit_teei_fp:
