def0d27c5baa36163e1246171b675d2eb316f8de
Tzu-Hsien Wang <tzuhsien.wang@mediatek.com>
2022-01-20 11:35:14 +0800

    [ALPS06501942] GED: Add debugging dumps when fence timeouts
    [Detail]
    Add debugging dumps when fence timeouts
    
    MTK-Commit-Id: 510db063f075a2017fc821d746c8211801a17dfe
    
    Signed-off-by: Tzu-Hsien Wang <tzuhsien.wang@mediatek.com>
    CR-Id: ALPS06501942
    Feature: ARM GPU DDK driver
    Change-Id: I0dc29630d05f67fe6c8657fbf4daa367201f8147

--- drivers/gpu/mediatek/mt-plat/mtk_gpu_utility.h
+++ drivers/gpu/mediatek/mt-plat/mtk_gpu_utility.h
@@ -104,6 +104,8 @@ bool mtk_unregister_gpu_power_change(const char *name);
 void mtk_notify_gpu_power_change(int power_on);
 
 bool mtk_notify_gpu_freq_change(u32 clk_idx, u32 gpufreq);
+
+void mtk_gpu_fence_debug_dump(int fd, int pid, int type);
 #ifdef __cplusplus
 }
 #endif
