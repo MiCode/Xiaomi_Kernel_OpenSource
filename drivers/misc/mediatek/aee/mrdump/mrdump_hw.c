a4a9fcac8c404bf7313fdd84c500f49cba9967c5
Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
2022-01-20 11:25:58 +0800

    [ALPS06087404] mrdump: use mrdump to enable ddr reserved mode
    Use mrdump to enable ddr reserved mode
    
    MTK-Commit-Id: b2baf2780708e32687dfc8bdccf38bb8d75493af
    
    Change-Id: Ie9643204c223744a5e7893246d5f969f9392b435
    Signed-off-by: Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
    CR-Id: ALPS06087404
    Feature: MRDUMP

--- drivers/misc/mediatek/aee/mrdump/mrdump_hw.c
+++ drivers/misc/mediatek/aee/mrdump/mrdump_hw.c
@@ -13,9 +13,38 @@
 #include <mtk_platform_debug.h>
 #endif
 
+#if IS_ENABLED(CONFIG_MTK_DBGTOP)
+#include <dbgtop.h>
+#else
 #if IS_ENABLED(CONFIG_MTK_WATCHDOG)
 #include <mtk_wd_api.h>
+#endif
+#endif
 
+#if IS_ENABLED(CONFIG_MTK_DBGTOP)
+static void mrdump_dbgtop_dram_reserved(bool drm_ready)
+{
+	int res;
+
+	if (drm_ready == true) {
+		pr_notice("%s: Trying to enable DDR Reserve Mode(%d)\n",
+				__func__, drm_ready);
+
+		res = mtk_dbgtop_dram_reserved(drm_ready);
+		if (res == 0) {
+			pr_notice("%s: DDR reserved mode enable ok\n",
+				 __func__);
+		} else {
+			pr_notice("%s: mtk_dbgtop_dram_reserved error(%d)\n",
+					__func__, res);
+		}
+	} else {
+		pr_notice("%s: DDR Reserve Mode disabled.(%d)\n",
+				__func__, drm_ready);
+	}
+}
+#else
+#if IS_ENABLED(CONFIG_MTK_WATCHDOG)
 static void mrdump_wd_dram_reserved_mode(bool drm_ready)
 {
 	int res;
@@ -36,14 +65,19 @@ static void mrdump_wd_dram_reserved_mode(bool drm_ready)
 		pr_notice("%s: config is not enabled yet\n", __func__);
 	}
 }
-
+#endif
 #endif
 
 int __init mrdump_hw_init(bool drm_ready)
 {
+#if IS_ENABLED(CONFIG_MTK_DBGTOP)
+	mrdump_dbgtop_dram_reserved(drm_ready);
+#else
 #if IS_ENABLED(CONFIG_MTK_WATCHDOG)
 	mrdump_wd_dram_reserved_mode(drm_ready);
 #endif
+#endif
+	pr_info("%s: init_done.\n", __func__);
 
 #if IS_ENABLED(CONFIG_MTK_DFD_INTERNAL_DUMP)
 	if (dfd_setup(DFD_BASIC_DUMP) == -1)
