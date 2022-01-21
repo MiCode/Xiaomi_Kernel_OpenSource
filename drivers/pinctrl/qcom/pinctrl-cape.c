32f67232686f7160217001a9435b5a33c0972886
Auditya Bhattaram <quic_audityab@quicinc.com>
2022-01-21 00:45:39 -0800

    pinctrl: qcom: cape: Add pinctrl VM support in Cape pinctrl driver
    Add VM support in the driver such that same driver could be
    used from secondary VM as well.
    
    Change-Id: Iee0c5b7af68aa7969260ff86411031c88a4b1b24
    Signed-off-by: Auditya Bhattaram <quic_audityab@quicinc.com>
    Signed-off-by: Naman Jain <quic_namajain@quicinc.com>

--- drivers/pinctrl/qcom/pinctrl-cape.c
+++ drivers/pinctrl/qcom/pinctrl-cape.c
@@ -27,13 +27,41 @@ static const struct msm_pinctrl_soc_data cape_pinctrl = {
 	.nwakeirq_map = ARRAY_SIZE(cape_pdc_map),
 };
 
+static const struct msm_pinctrl_soc_data cape_vm_pinctrl = {
+	.pins = cape_pins,
+	.npins = ARRAY_SIZE(cape_pins),
+	.functions = cape_functions,
+	.nfunctions = ARRAY_SIZE(cape_functions),
+	.groups = cape_groups,
+	.ngroups = ARRAY_SIZE(cape_groups),
+	.ngpios = 211,
+};
+
+static void qcom_trace_gpio_read(void *unused, struct gpio_device *gdev,
+				 bool *block_gpio_read)
+{
+	*block_gpio_read = true;
+}
+
 static int cape_pinctrl_probe(struct platform_device *pdev)
 {
-	return msm_pinctrl_probe(pdev, &cape_pinctrl);
+	const struct msm_pinctrl_soc_data *pinctrl_data;
+	struct device *dev = &pdev->dev;
+
+	pinctrl_data = of_device_get_match_data(&pdev->dev);
+	if (!pinctrl_data)
+		return -EINVAL;
+
+	if (of_device_is_compatible(dev->of_node, "qcom,cape-vm-pinctrl"))
+		register_trace_android_vh_gpio_block_read(qcom_trace_gpio_read,
+							  NULL);
+
+	return msm_pinctrl_probe(pdev, pinctrl_data);
 }
 
 static const struct of_device_id cape_pinctrl_of_match[] = {
 	{ .compatible = "qcom,cape-pinctrl", .data = &cape_pinctrl},
+	{ .compatible = "qcom,cape-vm-pinctrl", .data = &cape_vm_pinctrl},
 	{ },
 };
 
@@ -61,3 +89,4 @@ module_exit(cape_pinctrl_exit);
 MODULE_DESCRIPTION("QTI cape pinctrl driver");
 MODULE_LICENSE("GPL v2");
 MODULE_DEVICE_TABLE(of, cape_pinctrl_of_match);
+MODULE_SOFTDEP("pre: qcom_tlmm_vm_irqchip");
