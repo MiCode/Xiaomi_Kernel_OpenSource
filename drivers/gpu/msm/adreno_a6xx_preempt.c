c5098e4dac4ea238cd32f8706867ea651679c0e4
Kamal Agrawal <quic_kamaagra@quicinc.com>
2022-01-27 01:49:08 +0530

    msm: kgsl: Skip active state transition validation in atomic path
    KGSL waits for GMU to move to active state before triggering preemption.
    It is done by polling GMU_CX_GMU_RPMH_POWER_STATE register. KGSL explicitly
    keeps GPU alive while handling GPU interrupt, because of which GPU cannot
    enter IFPC. Hence, it is guaranteed that GPU will be in active state. Skip
    active state transition validation while processing GPU interrupt.
    
    Change-Id: I2f0e06f9bccb1a6b5f3047aa53bf0ff8120ab073
    Signed-off-by: Kamal Agrawal <quic_kamaagra@quicinc.com>

--- drivers/gpu/msm/adreno_a6xx_preempt.c
+++ drivers/gpu/msm/adreno_a6xx_preempt.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include "adreno.h"
@@ -333,7 +334,7 @@ void a6xx_preemption_trigger(struct adreno_device *adreno_dev, bool atomic)
 	 * preemption. This is require to make sure CP doesn't
 	 * interrupt GMU during wake-up from IFPC.
 	 */
-	if (gmu_core_dev_wait_for_active_transition(device))
+	if (!atomic && gmu_core_dev_wait_for_active_transition(device))
 		goto err;
 
 	if (a6xx_fenced_write(adreno_dev,
