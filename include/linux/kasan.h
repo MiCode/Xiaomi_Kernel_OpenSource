00ab8271574491312295b76fbc3e03d3eeb8d474
Andrey Konovalov <andreyknvl@google.com>
2022-01-27 16:25:48 +1100

    FROMGIT: kasan, page_alloc: merge kasan_alloc_pages into post_alloc_hook
    Currently, the code responsible for initializing and poisoning memory in
    post_alloc_hook() is scattered across two locations: kasan_alloc_pages()
    hook for HW_TAGS KASAN and post_alloc_hook() itself.  This is confusing.
    
    This and a few following patches combine the code from these two
    locations.  Along the way, these patches do a step-by-step restructure the
    many performed checks to make them easier to follow.
    
    Replace the only caller of kasan_alloc_pages() with its implementation.
    
    As kasan_has_integrated_init() is only true when CONFIG_KASAN_HW_TAGS is
    enabled, moving the code does no functional changes.
    
    Also move init and init_tags variables definitions out of
    kasan_has_integrated_init() clause in post_alloc_hook(), as they have the
    same values regardless of what the if condition evaluates to.
    
    This patch is not useful by itself but makes the simplifications in the
    following patches easier to follow.
    
    Link: https://lkml.kernel.org/r/5ac7e0b30f5cbb177ec363ddd7878a3141289592.1643047180.git.andreyknvl@google.com
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Acked-by: Marco Elver <elver@google.com>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Andrey Ryabinin <ryabinin.a.a@gmail.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Evgenii Stepanov <eugenis@google.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Peter Collingbourne <pcc@google.com>
    Cc: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    (cherry picked from commit 2a10554bf7e83f0613511f6781afe03cebab5a6c
     git://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git akpm)
    Bug: 217222520
    Change-Id: Iafd536232d6f9996887f9e7e47b22f6d65e6c28a
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>

--- include/linux/kasan.h
+++ include/linux/kasan.h
@@ -94,8 +94,6 @@ static inline bool kasan_hw_tags_enabled(void)
 	return kasan_enabled();
 }
 
-void kasan_alloc_pages(struct page *page, unsigned int order, gfp_t flags);
-
 #else /* CONFIG_KASAN_HW_TAGS */
 
 static inline bool kasan_enabled(void)
@@ -106,13 +104,6 @@ static inline bool kasan_enabled(void)
 static inline bool kasan_hw_tags_enabled(void)
 {
 	return false;
-}
-
-static __always_inline void kasan_alloc_pages(struct page *page,
-					      unsigned int order, gfp_t flags)
-{
-	/* Only available for integrated init. */
-	BUILD_BUG();
 }
 
 #endif /* CONFIG_KASAN_HW_TAGS */
