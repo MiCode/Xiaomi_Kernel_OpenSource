0cf02e6f6d5130914b91e1423dac0a7278360234
Pankaj Gupta <quic_gpankaj@quicinc.com>
2022-02-02 20:04:25 +0530

    msm: kgsl: Add freq limiter interrupt handler
    Add functionality to handle freq limiter interrupt.
    
    Change-Id: Idcff660c5fca9b64930fbf35911210934f661c96
    Signed-off-by: Pankaj Gupta <quic_gpankaj@quicinc.com>
    Signed-off-by: Harshitha Sai Neelati <quic_hsaineel@quicinc.com>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -1393,11 +1393,15 @@ void kgsl_pwrctrl_irq(struct kgsl_device *device, bool state)
 			&pwr->power_flags)) {
 			trace_kgsl_irq(device, state);
 			enable_irq(pwr->interrupt_num);
+			if (device->freq_limiter_intr_num > 0)
+				enable_irq(device->freq_limiter_intr_num);
 		}
 	} else {
 		if (test_and_clear_bit(KGSL_PWRFLAGS_IRQ_ON,
 			&pwr->power_flags)) {
 			trace_kgsl_irq(device, state);
+			if (device->freq_limiter_intr_num > 0)
+				disable_irq(device->freq_limiter_intr_num);
 			if (in_interrupt())
 				disable_irq_nosync(pwr->interrupt_num);
 			else
