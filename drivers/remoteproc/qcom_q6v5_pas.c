3a5de2d87e41807f5619139ec71feb35c9e7826c
Vijay Patil <quic_vijaylil@quicinc.com>
2022-02-03 12:45:39 +0530

    remoteproc: qcom: pas: Add support to boot WPSS in parrot
    Add entries to support to WPSS boot in parrot using RPROC
    for qca6750.
    
    Change-Id: I7aa3f6c6c5a556785f234968dbbaa681e6c8d97b
    Signed-off-by: Vijay Patil <quic_vijaylil@quicinc.com>

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -1236,6 +1236,18 @@ static const struct adsp_data diwali_wpss_resource = {
 	.ssctl_id = 0x19,
 };
 
+static const struct adsp_data parrot_wpss_resource = {
+	.crash_reason_smem = 626,
+	.firmware_name = "wpss.mdt",
+	.pas_id = 6,
+	.minidump_id = 4,
+	.uses_elf64 = true,
+	.ssr_name = "wpss",
+	.sysmon_name = "wpss",
+	.qmp_name = "wpss",
+	.ssctl_id = 0x19,
+};
+
 static const struct of_device_id adsp_of_match[] = {
 	{ .compatible = "qcom,msm8974-adsp-pil", .data = &adsp_resource_init},
 	{ .compatible = "qcom,msm8996-adsp-pil", .data = &adsp_resource_init},
@@ -1269,6 +1281,7 @@ static const struct of_device_id adsp_of_match[] = {
 	{ .compatible = "qcom,parrot-adsp-pas", .data = &parrot_adsp_resource},
 	{ .compatible = "qcom,parrot-cdsp-pas", .data = &parrot_cdsp_resource},
 	{ .compatible = "qcom,parrot-modem-pas", .data = &parrot_mpss_resource},
+	{ .compatible = "qcom,parrot-wpss-pas", .data = &parrot_wpss_resource},
 	{ },
 };
 MODULE_DEVICE_TABLE(of, adsp_of_match);
