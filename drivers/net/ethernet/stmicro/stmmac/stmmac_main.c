bittencourtjulival/eclipse_kernel_xiaomi_stone

net: stmmac: Add support for PHY interrupts
Driver only supported polling mode. Add PHY interrupt support.

Change-Id: Icfd4986c5e71ccbc6e10b76553a08c86cbdee05d
Acked-by: Nagarjuna Chaganti <nchagant@qti.qualcomm.com>
Signed-off-by: Suraj Jaiswal <jsuraj@codeaurora.org>

drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -1029,7 +1029,19 @@ static int stmmac_init_phy(struct net_device *dev)
 			netdev_err(priv->dev, "no phy at addr %d\n", addr);
 			return -ENODEV;
 		}
+		if (phy_intr_en) {
+			phydev->irq = PHY_IGNORE_INTERRUPT;
+			phydev->interrupts =  PHY_INTERRUPT_ENABLED;
 
+			if (phydev->drv && phydev->drv->config_intr &&
+			    !phydev->drv->config_intr(phydev)) {
+				pr_debug(" qcom-ethqos: %s config_phy_intr successful\n",
+					 __func__);
+		}
+	} else {
+		pr_err("stmmac phy polling mode\n");
+		phydev->irq = PHY_POLL;
+	}
 		ret = phylink_connect_phy(priv->phylink, phydev);
 	}
 

MiCode/Xiaomi_Kernel_OpenSource

drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -1245,7 +1245,19 @@ static int stmmac_init_phy(struct net_device *dev)
 			netdev_err(priv->dev, "no phy at addr %d\n", addr);
 			return -ENODEV;
 		}
+		if (phy_intr_en) {
+			phydev->irq = PHY_MAC_INTERRUPT;
+			phydev->interrupts =  PHY_INTERRUPT_ENABLED;
 
+			if (phydev->drv && phydev->drv->config_intr &&
+			    !phydev->drv->config_intr(phydev)) {
+				pr_debug(" qcom-ethqos: %s config_phy_intr successful\n",
+					 __func__);
+		}
+	} else {
+		pr_err("stmmac phy polling mode\n");
+		phydev->irq = PHY_POLL;
+	}
 		ret = phylink_connect_phy(priv->phylink, phydev);
 	}
 