44f4d09bcdbc35cb9f9fbd3f685ce6ac854996ae
Lixiong Zhou <lixiong.zhou@mediatek.com>
2022-02-10 17:43:47 +0800

    [ALPS06552311] sched: arm arm64: enable sched configs
    [Detail]
    Enable sched configs
    
    MTK-Commit-Id: deedfa8767a209f2a9ddf95a303518ca04276a15
    
    Signed-off-by: Lixiong Zhou <lixiong.zhou@mediatek.com>
    CR-Id: ALPS06552311
    Feature: CPU DVFS (CPU Dynamic Voltage and Frequency Scaling)
    Change-Id: I9d307b2f17f1d93457d1980aefed64254439a57f

--- drivers/misc/mediatek/include/mt-plat/mtk_cpufreq_common_api.h
+++ drivers/misc/mediatek/include/mt-plat/mtk_cpufreq_common_api.h
@@ -38,6 +38,10 @@
 #include "mt6771/include/mach/mtk_cpufreq_api.h"
 #endif
 
+#if defined(CONFIG_MACH_MT6781)
+#include "mt6781/include/mach/mtk_cpufreq_api.h"
+#endif
+
 #if defined(CONFIG_MACH_MT6785)
 #include "mt6785/include/mach/mtk_cpufreq_api.h"
 #endif
@@ -61,6 +65,10 @@
 
 #if defined(CONFIG_MACH_MT6877)
 #include "mt6877/include/mach/mtk_cpufreq_api.h"
+#endif
+
+#if defined(CONFIG_MACH_MT6893)
+#include "mt6893/include/mach/mtk_cpufreq_api.h"
 #endif
 
 #endif	/* __MTK_CPUFREQ_COMMON_API_H__ */
