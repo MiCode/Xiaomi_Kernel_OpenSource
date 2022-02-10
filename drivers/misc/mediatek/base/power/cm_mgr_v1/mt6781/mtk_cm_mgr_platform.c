bee9534974a88c6c8b2b137cf97823ebf1491796
yugang.wang <yugang.wang@mediatek.com>
2022-02-10 17:44:03 +0800

    [ALPS06517356] CM_MGR: fix coverity
    [Detail] CM_MGR: fix self assignment error
    
    MTK-Commit-Id: 97b846b258f0a962ef83b60afa336f1f280ee5a1
    
    Signed-off-by: yugang.wang <yugang.wang@mediatek.com>
    CR-Id: ALPS06517356
    Feature: CGM(CPU/GPU Memory Latency Management)
    Change-Id: Id2190cb9c89fb67454a6886af9c799df2f8f0c00

--- drivers/misc/mediatek/base/power/cm_mgr_v1/mt6781/mtk_cm_mgr_platform.c
+++ drivers/misc/mediatek/base/power/cm_mgr_v1/mt6781/mtk_cm_mgr_platform.c
@@ -1063,18 +1063,8 @@ void cm_mgr_setup_cpu_dvfs_info(void)
 
 void cm_mgr_ddr_setting_init(void)
 {
-	int i;
-	int idx = cm_mgr_get_idx();
-
-	if (idx == CM_MGR_LP4) {
-		for (i = 0; i < CM_MGR_EMI_OPP; i++) {
-			cpu_power_ratio_up[i] = cpu_power_ratio_up[i];
-			cpu_power_ratio_down[i] = cpu_power_ratio_down[i];
-			debounce_times_up_adb[i] = debounce_times_up_adb[i];
-			debounce_times_down_adb[i] = debounce_times_down_adb[i];
-		}
-	}
 #if defined(CONFIG_MTK_TINYSYS_SSPM_SUPPORT) && defined(USE_CM_MGR_AT_SSPM)
+	int i;
 	for (i = 0; i < CM_MGR_EMI_OPP; i++) {
 		cm_mgr_to_sspm_command(IPI_CM_MGR_CPU_POWER_RATIO_UP,
 			i << 16 | cpu_power_ratio_up[i]);
