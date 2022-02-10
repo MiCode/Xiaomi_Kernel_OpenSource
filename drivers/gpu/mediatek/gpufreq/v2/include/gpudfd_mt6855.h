6d6fb1233541cebbee390d26149cc9c7a588ca1b
ColinChu <Kl.Chu@mediatek.com>
2022-02-10 17:46:31 +0800

    [ALPS06477113] gpudfd: mt6855: Porting gpu dfd and refine
    [Detail]
     - Porting mt6855 gpu dfd flow
     - Adjust some register addresses to fit mt6855 HW design
    
    MTK-Commit-Id: 09609e431a589ec8a3e6d915ec2e626fff2a67f5
    
    Signed-off-by: ColinChu <Kl.Chu@mediatek.com>
    CR-Id: ALPS06477113
    Feature: MFGSYS driver
    Change-Id: I56942f511bde2d2bef87c3b1ce699e7f8e9d99d4

--- drivers/gpu/mediatek/gpufreq/v2/include/gpudfd_mt6855.h
+++ drivers/gpu/mediatek/gpufreq/v2/include/gpudfd_mt6855.h
@@ -9,9 +9,15 @@
 #define GPUDFD_ENABLE                (0)
 
 /*
+ * DRM_DEBUG_BASE     : 0x1000_D000
+ * DRM_MFG_REG        : 0x1000_D060
+ */
+#define DRM_MFG_REG                  (g_drm_debug_base + 0x60)
+
+/*
  * MFGCFG_BASE        : 0x13FB_F000
  * MFG_DEBUGMON_CON_00: 0x13FB_F8F8
- * MFG_DFD_CON_0~11   : 0x13FB_FA00 ~ 0x13FB_FA2C
+ * MFG_DFD_CON_0~9    : 0x13FB_FA00 ~ 0x13FB_FA24
  */
 #define MFG_DEBUGMON_CON_00          (g_mfg_top_base + 0x8F8)
 #define MFG_DFD_CON_0                (g_mfg_top_base + 0xA00)
@@ -24,22 +30,28 @@
 #define MFG_DFD_CON_7                (g_mfg_top_base + 0xA1C)
 #define MFG_DFD_CON_8                (g_mfg_top_base + 0xA20)
 #define MFG_DFD_CON_9                (g_mfg_top_base + 0xA24)
-#define MFG_DFD_CON_10               (g_mfg_top_base + 0xA28)
-#define MFG_DFD_CON_11               (g_mfg_top_base + 0xA2C)
+#define MFG_DFD_CON_12               (g_mfg_top_base + 0xA30)
+#define MFG_DFD_CON_13               (g_mfg_top_base + 0xA34)
+#define MFG_DFD_CON_17               (g_mfg_top_base + 0xA44)
+#define MFG_DFD_CON_18               (g_mfg_top_base + 0xA48)
+#define MFG_DFD_CON_19               (g_mfg_top_base + 0xA4C)
 
 #define MFG_DEBUGMON_CON_00_ENABLE   (0xFFFFFFFF)
-#define MFG_DFD_CON_0_ENABLE         (0x0F001100)
-#define MFG_DFD_CON_1_ENABLE         (0x0000F217)
-#define MFG_DFD_CON_2_ENABLE         (0x00015651)
-#define MFG_DFD_CON_3_ENABLE         (0x00202738)
+#define MFG_DFD_CON_0_ENABLE         (0x0F101100)
+#define MFG_DFD_CON_1_ENABLE         (0x0000D611)
+#define MFG_DFD_CON_2_ENABLE         (0x0001ffec)
+#define MFG_DFD_CON_3_ENABLE         (0x00200828)
 #define MFG_DFD_CON_4_ENABLE         (0x00000000)
 #define MFG_DFD_CON_5_ENABLE         (0x00000000)
 #define MFG_DFD_CON_6_ENABLE         (0x00000000)
 #define MFG_DFD_CON_7_ENABLE         (0x00000000)
 #define MFG_DFD_CON_8_ENABLE         (0x00000000)
 #define MFG_DFD_CON_9_ENABLE         (0x00000000)
-#define MFG_DFD_CON_10_ENABLE        (0x00000000)
-#define MFG_DFD_CON_11_ENABLE        (0x00000000)
+#define MFG_DFD_CON_12_ENABLE        (0x00000000)
+#define MFG_DFD_CON_13_ENABLE        (0x00000000)
+#define MFG_DFD_CON_17_ENABLE        (0x00000001)
+#define MFG_DFD_CON_18_ENABLE        (0x00000000)
+#define MFG_DFD_CON_19_ENABLE        (0x00000000)
 
 #define MFG_DEBUGMON_CON_00_DISABLE  (0x00000000)
 #define MFG_DFD_CON_0_DISABLE        (0x00000000)
@@ -52,8 +64,11 @@
 #define MFG_DFD_CON_7_DISABLE        (0x00000000)
 #define MFG_DFD_CON_8_DISABLE        (0x00000000)
 #define MFG_DFD_CON_9_DISABLE        (0x00000000)
-#define MFG_DFD_CON_10_DISABLE       (0x00000000)
-#define MFG_DFD_CON_11_DISABLE       (0x00000000)
+#define MFG_DFD_CON_12_DISABLE       (0x00000000)
+#define MFG_DFD_CON_13_DISABLE       (0x00000000)
+#define MFG_DFD_CON_17_DISABLE       (0x00000000)
+#define MFG_DFD_CON_18_DISABLE       (0x00000000)
+#define MFG_DFD_CON_19_DISABLE       (0x00000000)
 
 unsigned int gpudfd_init(struct platform_device *pdev);
 unsigned int __gpudfd_get_dfd_force_dump_mode(void);
