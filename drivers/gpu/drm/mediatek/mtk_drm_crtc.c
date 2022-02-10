c4d58758d7e763eaf42d65710696197836565f84
Scott Wang <scott.wang@mediatek.com>
2022-02-10 17:46:01 +0800

    [ALPS06543757] display: support 2in2out dsc path
    support 2in2out dsc path for dual mipi panel
    
    MTK-Commit-Id: fed104174dd2713013f1721125dbcd78889f538e
    
    CR-Id: ALPS06543757
    Feature: Display DSI
    Signed-off-by: Scott Wang <scott.wang@mediatek.com>
    Change-Id: I1779e65f6bc8922c8a89b926126ced3222133317

--- drivers/gpu/drm/mediatek/mtk_drm_crtc.c
+++ drivers/gpu/drm/mediatek/mtk_drm_crtc.c
@@ -283,8 +283,7 @@ void mtk_drm_crtc_dump(struct drm_crtc *crtc)
 		mtk_drm_crtc_addon_dump(crtc, addon_data);
 	}
 
-	if (panel_ext &&
-		panel_ext->output_mode == MTK_PANEL_DSC_SINGLE_PORT) {
+	if (panel_ext && panel_ext->dsc_params.enable) {
 		comp = priv->ddp_comp[DDP_COMPONENT_DSC0];
 		mtk_dump_reg(comp);
 	}
@@ -1398,8 +1397,7 @@ void mtk_crtc_ddp_prepare(struct mtk_drm_crtc *mtk_crtc)
 			}
 		}
 	}
-	if (panel_ext &&
-		panel_ext->output_mode == MTK_PANEL_DSC_SINGLE_PORT) {
+	if (panel_ext && panel_ext->dsc_params.enable) {
 		comp = priv->ddp_comp[DDP_COMPONENT_DSC0];
 		mtk_ddp_comp_clk_prepare(comp);
 	}
@@ -1462,7 +1460,7 @@ void mtk_crtc_ddp_unprepare(struct mtk_drm_crtc *mtk_crtc)
 			}
 		}
 	}
-	if (panel_ext && panel_ext->output_mode == MTK_PANEL_DSC_SINGLE_PORT) {
+	if (panel_ext && panel_ext->dsc_params.enable) {
 		comp = priv->ddp_comp[DDP_COMPONENT_DSC0];
 		mtk_ddp_comp_clk_unprepare(comp);
 	}
@@ -3796,6 +3794,7 @@ int mtk_crtc_attach_addon_path_comp(struct drm_crtc *crtc,
 int mtk_crtc_attach_ddp_comp(struct drm_crtc *crtc, int ddp_mode,
 			     bool is_attach)
 {
+	struct mtk_panel_params *panel_ext = mtk_drm_get_lcm_ext_params(crtc);
 	struct mtk_drm_crtc *mtk_crtc = to_mtk_crtc(crtc);
 	struct mtk_ddp_comp *comp;
 	int i, j;
@@ -3809,7 +3808,12 @@ int mtk_crtc_attach_ddp_comp(struct drm_crtc *crtc, int ddp_mode,
 		else
 			comp->mtk_crtc = NULL;
 	}
+	if (panel_ext && panel_ext->dsc_params.enable) {
+		struct mtk_drm_private *priv = mtk_crtc->base.dev->dev_private;
+		struct mtk_ddp_comp *dsc_comp = priv->ddp_comp[DDP_COMPONENT_DSC0];
 
+		dsc_comp->mtk_crtc = is_attach ? mtk_crtc : NULL;
+	}
 	return 0;
 }
 
@@ -3833,8 +3837,7 @@ static void mtk_crtc_addon_connector_disconnect(struct drm_crtc *crtc,
 	struct mtk_ddp_comp *dsc_comp;
 	struct mtk_drm_private *priv = mtk_crtc->base.dev->dev_private;
 
-	if (panel_ext &&
-		panel_ext->output_mode == MTK_PANEL_DSC_SINGLE_PORT) {
+	if (panel_ext && panel_ext->dsc_params.enable) {
 		dsc_comp = priv->ddp_comp[DDP_COMPONENT_DSC0];
 #if defined(CONFIG_MACH_MT6885) || defined(CONFIG_MACH_MT6893)
 		mtk_ddp_remove_dsc_prim_MT6885(mtk_crtc, handle);
@@ -3882,8 +3885,7 @@ static void mtk_crtc_addon_connector_connect(struct drm_crtc *crtc,
 	struct mtk_drm_private *priv = mtk_crtc->base.dev->dev_private;
 	struct mtk_ddp_comp *output_comp;
 
-	if (panel_ext &&
-		panel_ext->output_mode == MTK_PANEL_DSC_SINGLE_PORT) {
+	if (panel_ext && panel_ext->dsc_params.enable) {
 		struct mtk_ddp_config cfg;
 
 		dsc_comp = priv->ddp_comp[DDP_COMPONENT_DSC0];
--- drivers/gpu/drm/mediatek/mtk_drm_ddp.c
+++ drivers/gpu/drm/mediatek/mtk_drm_ddp.c
@@ -407,6 +407,7 @@
 	#define DISP_OVL3_2L_OUT0_MOUT_TO_DISP_WDMA1		0x2
 #define MT6885_DSI1_SEL_IN		0xFF0
 	#define DSI1_SEL_IN_FROM_DISP_DITHER1_MOUT	0x1
+	#define DSI1_SEL_IN_FROM_DSC_WRAP0			0x3
 #define MT6885_DISP_WDMA1_SEL_IN	0xFF4
 	#define MT6885_WDMA1_SEL_IN_FROM_DISP_DITHER1_MOUT	0x0
 	#define MT6885_WDMA1_SEL_IN_FROM_DISP_TOVL1_OUT0_MOUT	0x2
@@ -5899,6 +5900,7 @@ void mtk_ddp_remove_dsc_prim_MT6873(struct mtk_drm_crtc *mtk_crtc,
 void mtk_ddp_insert_dsc_prim_MT6885(struct mtk_drm_crtc *mtk_crtc,
 	struct cmdq_pkt *handle)
 {
+	struct mtk_panel_params *panel_ext = mtk_drm_get_lcm_ext_params(&mtk_crtc->base);
 	unsigned int addr, value;
 
 	/* remove DISP_DITHER0_MOUT -> DSI0_SEL*/
@@ -5942,6 +5944,14 @@ void mtk_ddp_insert_dsc_prim_MT6885(struct mtk_drm_crtc *mtk_crtc,
 	if (!mtk_crtc->is_dual_pipe)
 		return;
 
+	/*DSC_WARP0-> DSI1*/
+	if (panel_ext && panel_ext->output_mode == MTK_PANEL_DUAL_PORT) {
+		addr = MT6885_DSI1_SEL_IN;
+		value = DSI1_SEL_IN_FROM_DSC_WRAP0;
+		cmdq_pkt_write(handle, mtk_crtc->gce_obj.base,
+			       mtk_crtc->config_regs_pa + addr, value, ~0);
+	}
+
 	/* remove MT6885_DISP_DITHER1_MOUT_EN -> DSI1_SEL*/
 	addr = MT6885_DISP_DITHER1_MOUT_EN;
 	value = DISP_DITHER1_MOUT_EN_TO_DSI1_SEL;
@@ -5969,6 +5979,7 @@ void mtk_ddp_insert_dsc_prim_MT6885(struct mtk_drm_crtc *mtk_crtc,
 void mtk_ddp_remove_dsc_prim_MT6885(struct mtk_drm_crtc *mtk_crtc,
 	struct cmdq_pkt *handle)
 {
+	struct mtk_panel_params *panel_ext = mtk_drm_get_lcm_ext_params(&mtk_crtc->base);
 	unsigned int addr, value;
 
 	/* DISP_DITHER0_MOUT -> DISP_PQ0_SOUT */
@@ -6007,6 +6018,14 @@ void mtk_ddp_remove_dsc_prim_MT6885(struct mtk_drm_crtc *mtk_crtc,
 
 	if (!mtk_crtc->is_dual_pipe)
 		return;
+
+	/*DSC_WARP0-> DSI1*/
+	if (panel_ext && panel_ext->output_mode == MTK_PANEL_DUAL_PORT) {
+		addr = MT6885_DSI1_SEL_IN;
+		value = 0;
+		cmdq_pkt_write(handle, mtk_crtc->gce_obj.base,
+			       mtk_crtc->config_regs_pa + addr, value, ~0);
+	}
 
 	/* MT6885_DISP_DITHER1_MOUT_EN -> PQ1_SOUT */
 	addr = MT6885_DISP_DITHER1_MOUT_EN;
@@ -6404,6 +6423,7 @@ void mtk_disp_mutex_add_comp_with_cmdq(struct mtk_drm_crtc *mtk_crtc,
 				       struct cmdq_pkt *handle,
 				       unsigned int mutex_id)
 {
+	struct mtk_panel_params *panel_ext = mtk_drm_get_lcm_ext_params(&mtk_crtc->base);
 	struct mtk_disp_mutex *mutex = NULL;
 	struct mtk_ddp *ddp = NULL;
 	unsigned int reg;
@@ -6456,6 +6476,14 @@ void mtk_disp_mutex_add_comp_with_cmdq(struct mtk_drm_crtc *mtk_crtc,
 								  mutex->id),
 				       ddp->data->mutex_mod[id],
 				       ddp->data->mutex_mod[id]);
+			if (panel_ext && panel_ext->output_mode == MTK_PANEL_DUAL_PORT &&
+			    id == DDP_COMPONENT_DSC0) {
+				cmdq_pkt_write(handle, mtk_crtc->gce_obj.base, ddp->regs_pa +
+					       DISP_REG_MUTEX_MOD(ddp->data, mutex->id),
+					       MT6885_MUTEX_MOD0_DISP_DSC1,
+					       MT6885_MUTEX_MOD0_DISP_DSC1);
+				DDPINFO("mutex_add_comp /w cmdq mutex%d add DSC1\n", mutex->id);
+			}
 		} else {
 			cmdq_pkt_write(handle, mtk_crtc->gce_obj.base,
 				       ddp->regs_pa +
@@ -6520,6 +6548,7 @@ void mtk_disp_mutex_remove_comp_with_cmdq(struct mtk_drm_crtc *mtk_crtc,
 					  struct cmdq_pkt *handle,
 					  unsigned int mutex_id)
 {
+	struct mtk_panel_params *panel_ext = mtk_drm_get_lcm_ext_params(&mtk_crtc->base);
 	struct mtk_disp_mutex *mutex = NULL;
 	struct mtk_ddp *ddp = NULL;
 
@@ -6555,6 +6584,14 @@ void mtk_disp_mutex_remove_comp_with_cmdq(struct mtk_drm_crtc *mtk_crtc,
 								  mutex->id),
 				       ~(ddp->data->mutex_mod[id]),
 				       ddp->data->mutex_mod[id]);
+			if (panel_ext && panel_ext->output_mode == MTK_PANEL_DUAL_PORT &&
+			    id == DDP_COMPONENT_DSC0) {
+				cmdq_pkt_write(handle, mtk_crtc->gce_obj.base, ddp->regs_pa +
+					       DISP_REG_MUTEX_MOD(ddp->data, mutex->id),
+					       ~((unsigned int)MT6885_MUTEX_MOD0_DISP_DSC1),
+					       (unsigned int)MT6885_MUTEX_MOD0_DISP_DSC1);
+				DDPINFO("mutex_remove_comp /w cmdq mutex%d add DSC1\n", mutex->id);
+			}
 		} else {
 			cmdq_pkt_write(handle, mtk_crtc->gce_obj.base,
 				       ddp->regs_pa +
