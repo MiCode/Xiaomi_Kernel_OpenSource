25a7df226838092ab6dc2c28f0fbd2dc3cd1123a
David Brazdil <dbrazdil@google.com>
2022-02-14 17:52:34 +0000

    ANDROID: KVM: arm64: iommu: Register device hypcall
    Add '__pkvm_iommu_register' hypcall for registering a new IOMMU device.
    The handler allocates a linked-list entry for the device from a memory
    pool provided by the host. If the pool has run out, the handler returns
    -ENOMEM and expects the host to call it again with a fresh mem pool.
    
    The inputs are validated, eg. ID is unique and memory region does not
    overlap with existing IOMMUs. The driver can also implement a 'validate'
    callback for driver-specific input validation.
    
    If successful, the handler creates a private EL2 mapping for the device,
    forces the memory region is unmapped from host stage-2 and inserts the
    device into the linked list. Future attempts to map the MMIO region will
    fail because of pkvm_iommu_host_stage2_adjust_range.
    
    Bug: 190463801
    Signed-off-by: David Brazdil <dbrazdil@google.com>
    Change-Id: If54ba41cd0b219c6e63508b542d526703ab5b97e

--- arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
+++ arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
@@ -80,6 +80,7 @@ bool __pkvm_check_ioguard_page(struct kvm_vcpu *vcpu);
 bool addr_is_memory(phys_addr_t phys);
 int host_stage2_idmap_locked(phys_addr_t addr, u64 size, enum kvm_pgtable_prot prot);
 int host_stage2_set_owner_locked(phys_addr_t addr, u64 size, pkvm_id owner_id);
+int host_stage2_unmap_dev_locked(phys_addr_t start, u64 size);
 int kvm_host_prepare_stage2(void *pgt_pool_base);
 int kvm_guest_prepare_stage2(struct kvm_shadow_vm *vm, void *pgd);
 void handle_host_mem_abort(struct kvm_cpu_context *host_ctxt);
--- arch/arm64/kvm/hyp/nvhe/mem_protect.c
+++ arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -366,6 +366,13 @@ int __pkvm_prot_finalize(void)
 	return 0;
 }
 
+int host_stage2_unmap_dev_locked(phys_addr_t start, u64 size)
+{
+	hyp_assert_lock_held(&host_kvm.lock);
+
+	return kvm_pgtable_stage2_unmap(&host_kvm.pgt, start, size);
+}
+
 static int host_stage2_unmap_dev_all(void)
 {
 	struct kvm_pgtable *pgt = &host_kvm.pgt;
@@ -376,11 +383,11 @@ static int host_stage2_unmap_dev_all(void)
 	/* Unmap all non-memory regions to recycle the pages */
 	for (i = 0; i < hyp_memblock_nr; i++, addr = reg->base + reg->size) {
 		reg = &hyp_memory[i];
-		ret = kvm_pgtable_stage2_unmap(pgt, addr, reg->base - addr);
+		ret = host_stage2_unmap_dev_locked(addr, reg->base - addr);
 		if (ret)
 			return ret;
 	}
-	return kvm_pgtable_stage2_unmap(pgt, addr, BIT(pgt->ia_bits) - addr);
+	return host_stage2_unmap_dev_locked(addr, BIT(pgt->ia_bits) - addr);
 }
 
 struct kvm_mem_range {
@@ -572,6 +579,7 @@ static int host_stage2_idmap(u64 addr)
 
 	prot = is_memory ? PKVM_HOST_MEM_PROT : PKVM_HOST_MMIO_PROT;
 
+	host_lock_component();
 	/*
 	 * Adjust against IOMMU devices first. host_stage2_adjust_range() should
 	 * be called last for proper alignment.
@@ -580,10 +588,9 @@ static int host_stage2_idmap(u64 addr)
 		ret = pkvm_iommu_host_stage2_adjust_range(addr, &range.start,
 							  &range.end);
 		if (ret)
-			return ret;
+			goto unlock;
 	}
 
-	host_lock_component();
 	ret = host_stage2_adjust_range(addr, &range);
 	if (ret)
 		goto unlock;
