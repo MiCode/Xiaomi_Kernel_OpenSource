d0453bce218e44ffdf08948b59a6aa612e20777a
Mukesh Ojha <quic_mojha@quicinc.com>
2022-02-17 14:55:06 +0530

    soc: qcom: copyright fix
    Fix copyright issue seen on commit 9ded94a41554
    ("soc: qcom: socinfo: Add sku sysfs support").
    
    Change-Id: Ibc5ed8c0950b81eeeeb6688faa263ddbc8fa91ff
    Signed-off-by: Mukesh Ojha <quic_mojha@quicinc.com>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (c) 2009-2022, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2009-2021, The Linux Foundation. All rights reserved.
  * Copyright (c) 2017-2019, Linaro Ltd.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/err.h>
