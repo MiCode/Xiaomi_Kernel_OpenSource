66e0dc4d1ab5c97699db6635f8dd8905798d50c4
Harshdeep Dhatt <quic_hdhatt@quicinc.com>
2022-02-18 16:01:53 -0700

    msm: kgsl: Do not accept unknown memkinds in hwscheduling
    Currently, we are silently accepting unknown memkinds. Make sure
    we log an error when we receive a memkind that we are not
    aware of.
    
    Change-Id: I3da1dfb048e3ae03980cc969a65ee4eadabcdf43
    Signed-off-by: Harshdeep Dhatt <quic_hdhatt@quicinc.com>

--- drivers/gpu/msm/adreno_a6xx_hwsched_hfi.c
+++ drivers/gpu/msm/adreno_a6xx_hwsched_hfi.c
@@ -679,6 +679,12 @@ static struct hfi_mem_alloc_entry *get_mem_alloc_entry(
 	if (entry)
 		return entry;
 
+	if (desc->mem_kind >= HFI_MEMKIND_MAX) {
+		dev_err(&gmu->pdev->dev, "Invalid mem kind: %d\n",
+			desc->mem_kind);
+		return ERR_PTR(-EINVAL);
+	}
+
 	if (hfi->mem_alloc_entries == ARRAY_SIZE(hfi->mem_alloc_table)) {
 		dev_err(&gmu->pdev->dev,
 			"Reached max mem alloc entries\n");
