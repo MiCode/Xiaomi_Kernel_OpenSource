2e9281ebd5eb87fcbb2ec0b3cd6d73cedf4e4fc2
Claude Yen <claude.yen@mediatek.com>
2022-03-10 10:44:48 +0800

    [ALPS06701377] lpm: mt6789: update pmic gs dump
    add mt6366 golden setting dump
    
    MTK-Commit-Id: 6c800305028ba8044505d333ffc80f01d2505e56
    
    Signed-off-by: Claude Yen <claude.yen@mediatek.com>
    CR-Id: ALPS06701377
    Feature: Suspend
    Change-Id: I9e597b81aa3609d65a312e1be865290c27493ab9

---
+++ drivers/misc/mediatek/lpm/modules/debug/mt6789/dbg_power_gs.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2021 MediaTek Inc.
+ */
+
+
+#ifndef __PWR_GS_COMPARE_H__
+#define __PWR_GS_COMPARE_H__
+
+int power_gs_init(void);
+void power_gs_deinit(void);
+
+#endif
