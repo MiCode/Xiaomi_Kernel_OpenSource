a1f7bb352ff6ea326a15cd91bb89c90054594cf6
Ajay Agarwal <quic_ajaya@quicinc.com>
2022-03-15 12:05:59 +0530

    usb: dwc3-msm-core: Add support for speed limiting to Gen1 on boot
    Upon the first sm_work run when the drd_state transitions from
    DRD_STATE_UNDEFINED to host/peripheral mode, the Gen1 speed limit
    logic will not be exercised because dwc3_msm_power_collapse_por
    would bail out. To cover this case, add support for speed limit
    in dwc3_msm_core_init as well.
    
    Change-Id: I55fd81cb93db66207379078d208206393245458b
    Signed-off-by: Ajay Agarwal <quic_ajaya@quicinc.com>

--- drivers/usb/dwc3/dwc3-msm-core.c
+++ drivers/usb/dwc3/dwc3-msm-core.c
@@ -3184,6 +3184,16 @@ static void dwc3_dis_sleep_mode(struct dwc3_msm *mdwc)
 	dwc3_msm_write_reg(mdwc->base, DWC3_GUCTL1, reg);
 }
 
+
+/* Force Gen1 speed on Gen2 controller if required */
+static void dwc3_force_gen1(struct dwc3_msm *mdwc)
+{
+	struct dwc3 *dwc = platform_get_drvdata(mdwc->dwc3);
+
+	if (mdwc->force_gen1 && DWC3_IP_IS(DWC31))
+		dwc3_msm_write_reg_field(mdwc->base, DWC3_LLUCTL, DWC3_LLUCTL_FORCE_GEN1, 1);
+}
+
 static void dwc3_msm_power_collapse_por(struct dwc3_msm *mdwc)
 {
 	struct dwc3 *dwc = NULL;
@@ -3218,9 +3228,7 @@ static void dwc3_msm_power_collapse_por(struct dwc3_msm *mdwc)
 			mdwc3_dis_sending_cm_l1(mdwc);
 	}
 
-	/* Force Gen1 speed on Gen2 controller if required */
-	if (mdwc->force_gen1 && DWC3_IP_IS(DWC31))
-		dwc3_msm_write_reg_field(mdwc->base, DWC3_LLUCTL, DWC3_LLUCTL_FORCE_GEN1, 1);
+	dwc3_force_gen1(mdwc);
 }
 
 static int dwc3_msm_prepare_suspend(struct dwc3_msm *mdwc, bool ignore_p3_state)
@@ -5089,6 +5097,8 @@ static int dwc3_msm_core_init(struct dwc3_msm *mdwc)
 
 	val = dwc3_msm_read_reg(mdwc->base, DWC3_GSNPSID);
 	mdwc->ip = DWC3_GSNPS_ID(val);
+
+	dwc3_force_gen1(mdwc);
 
 	dwc3_msm_notify_event(dwc, DWC3_GSI_EVT_BUF_ALLOC, 0);
 	pm_runtime_set_autosuspend_delay(dwc->dev, 0);
