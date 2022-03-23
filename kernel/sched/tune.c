bd35f6c127f4f66eb2e0fcfca98583b8a09514c0
wenju.xu <wenju.xu@mediatek.com>
2022-03-23 20:50:54 +0800

    [ALPS06741621] sched: set max stune group count
    set BOOSTGROUPS_COUNT to 10
    
    MTK-Commit-Id: ba8f25b1fe04256c677ae5de26fbdb6223dd837c
    
    Signed-off-by: wenju.xu <wenju.xu@mediatek.com>
    CR-Id: ALPS06741621
    Feature: Energy Aware Scheduling Plus (EAS+)
    Change-Id: Id3b873bfd25a44dcc7fe5b0cc0a367915d7a808c

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -144,7 +144,7 @@ root_schedtune = {
  *    implementation especially for the computation of the per-CPU boost
  *    value
  */
-#define BOOSTGROUPS_COUNT 6
+#define BOOSTGROUPS_COUNT 10
 
 /* Array of configured boostgroups */
 static struct schedtune *allocated_group[BOOSTGROUPS_COUNT] = {
