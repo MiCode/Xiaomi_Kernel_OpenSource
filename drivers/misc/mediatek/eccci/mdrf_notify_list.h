677e1f775df22431e8561cec412bc6d54063e3ee
Zilin Wang <Zilin.Wang@mediatek.com>
2022-03-23 20:56:36 +0800

    [ALPS06529442] ccci:remove disp callback weak function
    Remove disp callback weak function, phaseout disp primary callback function
    
    MTK-Commit-Id: 611d08f5fda9986b2dc5511226f641dad51a34c5
    
    Signed-off-by: Zilin Wang <Zilin.Wang@mediatek.com>
    CR-Id: ALPS06529442
    Feature: ccmni
    Change-Id: Ic8c218701218a54c02e3853d618b00b36df04cc3

--- drivers/misc/mediatek/eccci/mdrf_notify_list.h
+++ drivers/misc/mediatek/eccci/mdrf_notify_list.h
@@ -9,10 +9,7 @@
  * para. 1: function name;
  * para. 2: module name;
  */
-#ifndef CONFIG_DRM_MEDIATEK
-MD_RF_NOTIFY(0, primary_display_ccci_mipi_callback, "MIPI_CLK")
-MD_RF_NOTIFY(1, primary_display_ccci_osc_callback, "LCM_OSC")
-#else
+#if IS_ENABLED(CONFIG_DRM_MEDIATEK)
 MD_RF_NOTIFY(0, mtk_disp_mipi_ccci_callback, "MIPI_CLK")
 MD_RF_NOTIFY(1, mtk_disp_osc_ccci_callback, "LCM_OSC")
 #endif
