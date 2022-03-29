07566786dcf5a761923753f17ec20cae4378d268
Greg Kroah-Hartman <gregkh@google.com>
2022-03-29 12:55:05 +0200

    Revert "ALSA: pcm: Fix races among concurrent prealloc proc writes"
    This reverts commit 51fce708ab8986a9879ee5da946a2cc120f1036d.
    
    It will come back after the next ABI break as it is needed to resolve
    CVE-2022-1048.  But for now, while testing, it can be reverted in order
    to preserve the ABI.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I8fa7d4fd7200f76aeb02f0741c6508e86146c2a3

--- sound/core/pcm_memory.c
+++ sound/core/pcm_memory.c
@@ -158,20 +158,19 @@ static void snd_pcm_lib_preallocate_proc_write(struct snd_info_entry *entry,
 	size_t size;
 	struct snd_dma_buffer new_dmab;
 
-	mutex_lock(&substream->pcm->open_mutex);
 	if (substream->runtime) {
 		buffer->error = -EBUSY;
-		goto unlock;
+		return;
 	}
 	if (!snd_info_get_line(buffer, line, sizeof(line))) {
 		snd_info_get_str(str, line, sizeof(str));
 		size = simple_strtoul(str, NULL, 10) * 1024;
 		if ((size != 0 && size < 8192) || size > substream->dma_max) {
 			buffer->error = -EINVAL;
-			goto unlock;
+			return;
 		}
 		if (substream->dma_buffer.bytes == size)
-			goto unlock;
+			return;
 		memset(&new_dmab, 0, sizeof(new_dmab));
 		new_dmab.dev = substream->dma_buffer.dev;
 		if (size > 0) {
@@ -184,7 +183,7 @@ static void snd_pcm_lib_preallocate_proc_write(struct snd_info_entry *entry,
 					 substream->pcm->card->number, substream->pcm->device,
 					 substream->stream ? 'c' : 'p', substream->number,
 					 substream->pcm->name, size);
-				goto unlock;
+				return;
 			}
 			substream->buffer_bytes_max = size;
 		} else {
@@ -196,8 +195,6 @@ static void snd_pcm_lib_preallocate_proc_write(struct snd_info_entry *entry,
 	} else {
 		buffer->error = -EINVAL;
 	}
- unlock:
-	mutex_unlock(&substream->pcm->open_mutex);
 }
 
 static inline void preallocate_info_init(struct snd_pcm_substream *substream)
