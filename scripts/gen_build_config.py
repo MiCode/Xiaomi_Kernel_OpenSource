1220975c858afd8665cb67ec009734777f716318
Celeste Wang <celeste.wang@mediatek.com>
2022-03-30 11:16:19 +0800

    [ALPS06740712] build: support KERNEL_BUILD_CONFIG_OVERLAYS
    Support KERNEL_BUILD_CONFIG_OVERLAYS to run more
    build.config fragments.
    
    MTK-Commit-Id: 760de59149b8949a3ba0b46128146c83149a4668
    
    Signed-off-by: Celeste Wang <celeste.wang@mediatek.com>
    CR-Id: ALPS06740712
    Feature: build process
    Change-Id: Ifcf9db3fe99ea589a4eb8cf28957661ff01263b8

--- scripts/gen_build_config.py
+++ scripts/gen_build_config.py
@@ -34,9 +34,9 @@ def get_config_in_defconfig(file_name, kernel_dir):
 
 def help():
     print 'Usage:'
-    print '  python scripts/gen_build_config.py --project <project> --kernel-defconfig <kernel project defconfig file> --kernel-defconfig-overlays <kernel project overlay defconfig files> --build-mode <mode> --out-file <gen build.config>'
+    print '  python scripts/gen_build_config.py --project <project> --kernel-defconfig <kernel project defconfig file> --kernel-defconfig-overlays <kernel project overlay defconfig files> --kernel-build-config-overlays <kernel build config overlays> --build-mode <mode> --out-file <gen build.config>'
     print 'Or:'
-    print '  python scripts/gen_build_config.py -p <project> --kernel-defconfig <kernel project defconfig file> --kernel-defconfig-overlays <kernel project overlay defconfig files> -m <mode> -o <gen build.config>'
+    print '  python scripts/gen_build_config.py -p <project> --kernel-defconfig <kernel project defconfig file> --kernel-defconfig-overlays <kernel project overlay defconfig files> --kernel-build-config-overlays <kernel build config overlays> -m <mode> -o <gen build.config>'
     print ''
     print 'Attention: Must set generated build.config, and project or kernel project defconfig file!!'
     sys.exit(2)
@@ -46,6 +46,7 @@ def main(**args):
     project = args["project"]
     kernel_defconfig = args["kernel_defconfig"]
     kernel_defconfig_overlays = args["kernel_defconfig_overlays"]
+    kernel_build_config_overlays = args["kernel_build_config_overlays"]
     build_mode = args["build_mode"]
     abi_mode = args["abi_mode"]
     out_file = args["out_file"]
@@ -200,6 +201,9 @@ def main(**args):
     file_handle.write('  BUILD_CONFIG_FRAGMENTS="${BUILD_CONFIG_FRAGMENTS} ${KERNEL_DIR}/build.config.mtk.check_gki"\n')
     file_handle.write('  ADDITIONAL_HOST_TOOLS="${ADDITIONAL_HOST_TOOLS} strings less"\n')
     file_handle.write('fi\n')
+    if kernel_build_config_overlays:
+        build_config_fragments = 'BUILD_CONFIG_FRAGMENTS="${BUILD_CONFIG_FRAGMENTS} %s"' % (kernel_build_config_overlays)
+        file_handle.write(build_config_fragments + '\n')
     file_handle.close()
 
 if __name__ == '__main__':
@@ -208,6 +212,7 @@ if __name__ == '__main__':
     parser.add_argument("-p","--project", dest="project", help="specify the project to build kernel.", default="")
     parser.add_argument("--kernel-defconfig", dest="kernel_defconfig", help="special kernel project defconfig file.",default="")
     parser.add_argument("--kernel-defconfig-overlays", dest="kernel_defconfig_overlays", help="special kernel project overlay defconfig files.",default="")
+    parser.add_argument("--kernel-build-config-overlays", dest="kernel_build_config_overlays", help="special kernel build config overlays files.",default="")
     parser.add_argument("-m","--build-mode", dest="build_mode", help="specify the build mode to build kernel.", default="user")
     parser.add_argument("--abi", dest="abi_mode", help="specify whether build.config is used to check ABI.", default="no")
     parser.add_argument("-o","--out-file", dest="out_file", help="specify the generated build.config file.", required=True)
