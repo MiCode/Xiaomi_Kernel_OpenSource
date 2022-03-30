9c0f328be3ea7e4fb2be2ce54d90afac6fa313bf
kuan-hsin.lee <kuan-hsin.lee@mediatek.com>
2022-03-30 10:53:33 +0800

    [ALPS06540609] SELinux: AEE porting
    MTK_SELINUX_AEE_WARNING debug feature porting
    
    MTK-Commit-Id: 06a8bff026db8e9a26f047d3768df52112f75aa7
    
    Change-Id: Ib0128b0896d316316c40694994d1d4943e758fde
    Signed-off-by: kuan-hsin.lee <kuan-hsin.lee@mediatek.com>
    CR-Id: ALPS06540609
    Feature: [Android Default] SELinux, SEAndroid, and SE-MTK

---
+++ drivers/misc/mediatek/selinux_warning/dummy.c
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2022 MediaTek Inc.
+ * Author: kuanhsin Lee <kuan-hsin.lee@mediatek.com>
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+static int __init selinux_init(void)
+{
+	pr_info("[SELinux] MTK SELinux init done\n");
+
+	return 0;
+}
+
+static void __exit selinux_exit(void)
+{
+	pr_info("[SELinux] MTK SELinux func exit\n");
+}
+
+module_init(selinux_init);
+module_exit(selinux_exit);
+MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("MediaTek SELINUX Driver");
+MODULE_AUTHOR("Kuan-Hsin Lee <kuan-hsin.lee@mediatek.com>");
