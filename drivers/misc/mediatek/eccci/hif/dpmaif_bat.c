6c7e7ae4fc3633f703039b86544884abdfeaa77a
ot903208 <ot_shiwei.liang@mediatek.com>
2022-03-30 11:04:04 +0800

    [ALPS06785686] ccci: dpmaif bat KE
    add wmb() after alloc skb and page.
    modify get_skb_from_tbl() function,
    using data copy instead of return struct pointer.
    
    MTK-Commit-Id: 9ac6fb8419ca66916cae14f7185150636a24b4ee
    
    Signed-off-by: ot903208 <ot_shiwei.liang@mediatek.com>
    CR-Id: ALPS06785686
    Feature: CCCI_HIF_DATA
    Change-Id: I0384909bc02932a33a171d037bb681db13233a26

--- drivers/misc/mediatek/eccci/hif/dpmaif_bat.c
+++ drivers/misc/mediatek/eccci/hif/dpmaif_bat.c
@@ -157,6 +157,10 @@ static inline void alloc_skb_to_tbl(int skb_cnt, int blocking)
 		if (skb_alloc(&skb_info->skb, &skb_info->base_addr,
 						pkt_buf_sz, blocking))
 			break;
+		/*
+		 * The wmb() flushes writes to dram before read g_skb_tbl data.
+		 */
+		wmb();
 
 		cur_tbl_idx = ringbuf_get_next_idx(MAX_INFO_COUNT,
 						cur_tbl_idx, 1);
@@ -164,20 +168,20 @@ static inline void alloc_skb_to_tbl(int skb_cnt, int blocking)
 	}
 }
 
-static inline struct temp_skb_info *get_skb_info_from_tbl(void)
+static inline int get_skb_info_from_tbl(struct temp_skb_info *skb_info)
 {
-	struct temp_skb_info *skb_info = NULL;
-
 	if (g_skb_tbl_cnt > 0) {
-		skb_info = &g_skb_tbl[g_skb_tbl_idx];
+		(*skb_info) = g_skb_tbl[g_skb_tbl_idx];
 
 		g_skb_tbl_idx = ringbuf_get_next_idx(MAX_INFO_COUNT,
 						g_skb_tbl_idx, 1);
 
 		g_skb_tbl_cnt--;
+
+		return 0;
 	}
 
-	return skb_info;
+	return -1;
 }
 
 static inline int page_alloc(
@@ -229,20 +233,20 @@ fast_retry:
 	return 0;
 }
 
-static inline struct temp_page_info *get_page_info_from_tbl(void)
+static inline int get_page_info_from_tbl(struct temp_page_info *page_info)
 {
-	struct temp_page_info *page_info = NULL;
-
 	if (g_page_tbl_cnt > 0) {
-		page_info = &g_page_tbl[g_page_tbl_idx];
+		(*page_info) = g_page_tbl[g_page_tbl_idx];
 
 		g_page_tbl_idx = ringbuf_get_next_idx(MAX_INFO_COUNT,
 					g_page_tbl_idx, 1);
 
 		g_page_tbl_cnt--;
+
+		return 0;
 	}
 
-	return page_info;
+	return -1;
 }
 
 static inline void alloc_page_to_tbl(int page_cnt, int blocking)
@@ -267,6 +271,10 @@ static inline void alloc_page_to_tbl(int page_cnt, int blocking)
 		if (page_alloc(&page_info->page, &page_info->base_addr,
 						pkt_buf_sz, blocking))
 			break;
+		/*
+		 * The wmb() flushes writes to dram before read g_skb_tbl data.
+		 */
+		wmb();
 
 		cur_tbl_idx = ringbuf_get_next_idx(MAX_INFO_COUNT,
 						cur_tbl_idx, 1);
@@ -338,12 +346,11 @@ static inline int alloc_bat_skb(
 {
 	int ret = 0;
 	unsigned long long data_base_addr;
-	struct temp_skb_info *skb_info;
+	struct temp_skb_info skb_info;
 
-	skb_info = get_skb_info_from_tbl();
-	if (skb_info) {
-		bat_skb->skb = skb_info->skb;
-		data_base_addr = skb_info->base_addr;
+	if (!get_skb_info_from_tbl(&skb_info)) {
+		bat_skb->skb = skb_info.skb;
+		data_base_addr = skb_info.base_addr;
 
 	} else {
 		ret = skb_alloc(&bat_skb->skb, &data_base_addr,
@@ -441,12 +448,11 @@ static inline int alloc_bat_page(
 {
 	unsigned long long data_base_addr;
 	int ret;
-	struct temp_page_info *page_info;
+	struct temp_page_info page_info;
 
-	page_info = get_page_info_from_tbl();
-	if (page_info) {
-		bat_page->page = page_info->page;
-		data_base_addr = page_info->base_addr;
+	if (!get_page_info_from_tbl(&page_info)) {
+		bat_page->page = page_info.page;
+		data_base_addr = page_info.base_addr;
 
 	} else {
 		ret = page_alloc(&bat_page->page, &data_base_addr,
