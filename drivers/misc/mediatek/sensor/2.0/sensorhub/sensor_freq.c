862e150ac96442be53e38385b94e16613b4aad8a
Camo Xiao <camo.xiao@mediatek.com>
2022-03-30 11:15:46 +0800

    [ALPS06465684] MSBP: add scp mcps adjust in MSBP
    Add sensor_freq.c for sensor adjust scp freq
    - Divide the scp req of sensor request into three
      tears: high,medium,low
    - Select different gears according to the demand of
      different sensors for MCPS
    
    MTK-Commit-Id: f8162afdd86521f6fbe45b294d184eb446a85dae
    
    Signed-off-by: Camo Xiao <camo.xiao@mediatek.com>
    Change-Id: I4b24cbc7780671c62e5cce297033a64493058188
    CR-Id: ALPS06465684
    Feature: MSBP

---
+++ drivers/misc/mediatek/sensor/2.0/sensorhub/sensor_freq.c
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2020 MediaTek Inc.
+ */
+
+#define pr_fmt(fmt) "sensor_freq " fmt
+
+#include <linux/err.h>
+#include <linux/device.h>
+#include <linux/printk.h>
+#include <linux/types.h>
+#include <linux/mm.h>
+#include <linux/kernel.h>
+
+#include "sensor_freq.h"
+
+static DECLARE_BITMAP(high_freq_tb, SENSOR_TYPE_SENSOR_MAX);
+static DECLARE_BITMAP(medium_freq_tb, SENSOR_TYPE_SENSOR_MAX);
+static DECLARE_BITMAP(low_freq_tb, SENSOR_TYPE_SENSOR_MAX);
+
+static struct sensor_freq_tb sensor_freq_table[] = {
+	{
+		.sensor_type = SENSOR_TYPE_OIS,
+		.freq_level = HIGH,
+		.core_id = 0,
+	},
+	{
+		.sensor_type = SENSOR_TYPE_ACCELEROMETER,
+		.freq_level = MEDIUM,
+		.core_id = 0,
+	},
+	{
+		.sensor_type = SENSOR_TYPE_GYROSCOPE,
+		.freq_level = MEDIUM,
+		.core_id = 0,
+	},
+};
+
+static int freq_tb_bitmap_set(uint8_t sensor_type)
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(sensor_freq_table); i++) {
+		if (sensor_type == sensor_freq_table[i].sensor_type) {
+			switch (sensor_freq_table[i].freq_level) {
+			case LOW:
+				set_bit(sensor_type, low_freq_tb);
+				return 0;
+			case MEDIUM:
+				set_bit(sensor_type, medium_freq_tb);
+				return 0;
+			case HIGH:
+				set_bit(sensor_type, high_freq_tb);
+				return 0;
+			default:
+				pr_err("sensor[%d] freq level err %d\n",
+					sensor_type,
+					sensor_freq_table[i].freq_level);
+				return -EINVAL;
+			}
+		}
+	}
+
+	return -EINVAL;
+}
+
+static int freq_tb_bitmap_clear(uint8_t sensor_type)
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(sensor_freq_table); i++) {
+		if (sensor_type == sensor_freq_table[i].sensor_type) {
+			switch (sensor_freq_table[i].freq_level) {
+			case LOW:
+				clear_bit(sensor_type, low_freq_tb);
+				return 0;
+			case MEDIUM:
+				clear_bit(sensor_type, medium_freq_tb);
+				return 0;
+			case HIGH:
+				clear_bit(sensor_type, high_freq_tb);
+				return 0;
+			default:
+				pr_err("sensor[%d] freq level err %d\n",
+					sensor_type,
+					sensor_freq_table[i].freq_level);
+				return -EINVAL;
+			}
+		}
+	}
+
+	return -EINVAL;
+}
+
+static int set_scp_freq(void)
+{
+	int ret = 0;
+
+	if (!bitmap_empty(high_freq_tb, SENSOR_TYPE_SENSOR_MAX)) {
+		ret = sensor_control_scp(SENS_FEATURE_ID, HIGH_FREQ_VALUE);
+		return ret;
+	}
+	if (!bitmap_empty(medium_freq_tb, SENSOR_TYPE_SENSOR_MAX)) {
+		ret = sensor_control_scp(SENS_FEATURE_ID, MEDIUM_FREQ_VALUE);
+		return ret;
+	}
+	if (!bitmap_empty(low_freq_tb, SENSOR_TYPE_SENSOR_MAX))
+		ret = sensor_control_scp(SENS_FEATURE_ID, LOW_FREQ_VALUE);
+	else
+		ret = sensor_control_scp(SENS_FEATURE_ID, 0);
+
+	return ret;
+}
+
+int sensor_register_freq(uint8_t sensor_type)
+{
+	int ret = 0;
+
+	if (!freq_tb_bitmap_set(sensor_type))
+		ret = set_scp_freq();
+
+	return ret;
+}
+
+int sensor_deregister_freq(uint8_t sensor_type)
+{
+	int ret = 0;
+
+	if (!freq_tb_bitmap_clear(sensor_type))
+		ret = set_scp_freq();
+
+	return ret;
+}
+
---
+++ drivers/misc/mediatek/sensor/2.0/sensorhub/sensor_freq.h
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2020 MediaTek Inc.
+ */
+
+#ifndef _SENSOR_FREQ_H_
+#define _SENSOR_FREQ_H_
+
+#include "scp.h"
+#include "hf_manager.h"
+
+enum freq_level {
+	LOW = 0,
+	MEDIUM,
+	HIGH,
+	LEVEL_MAX,
+};
+
+enum freq_value {
+	LOW_FREQ_VALUE = 80,
+	MEDIUM_FREQ_VALUE = 200,
+	HIGH_FREQ_VALUE = 700,
+};
+
+struct sensor_freq_tb {
+	uint8_t sensor_type;
+	uint8_t freq_level;
+	uint8_t core_id;
+};
+
+int sensor_register_freq(uint8_t sensor_type);
+int sensor_deregister_freq(uint8_t sensor_type);
+#endif
