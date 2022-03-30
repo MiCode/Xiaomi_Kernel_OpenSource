39b2a532b65cd3b4b8aac64247703da951eb6306
Frankie Chang <frankie.chang@mediatek.com>
2022-03-30 10:17:25 +0800

    [ALPS06372522] task-turbo: module code align ack patch
    [Detail]
    Aligned change of _task_util_est():
    sched/fair: Fix util_est UTIL_AVG_UNCHANGED handling.
    
    MTK-Commit-Id: 191d682a7396d44b3ec7008e3de2df02632c34f4
    
    Signed-off-by: Frankie Chang <frankie.chang@mediatek.com>
    CR-Id: ALPS06372522
    Feature: System Performance (task turbo)
    Change-Id: I073924c66656d4dbdce2d47540da41af55582638

--- drivers/misc/mediatek/task_turbo/task_turbo.c
+++ drivers/misc/mediatek/task_turbo/task_turbo.c
@@ -496,7 +496,7 @@ static inline unsigned long _task_util_est(struct task_struct *p)
 {
 	struct util_est ue = READ_ONCE(p->se.avg.util_est);
 
-	return (max(ue.ewma, ue.enqueued) | UTIL_AVG_UNCHANGED);
+	return max(ue.ewma, (ue.enqueued & ~UTIL_AVG_UNCHANGED));
 }
 
 int find_best_turbo_cpu(struct task_struct *p)
