CodeLinaro-mirror/kernel-platform-2-0-os_kernel_common

ANDROID: selftests: incfs: Add umount helper function
Bug: 211066171

Signed-off-by: Tadeusz Struk <tadeusz.struk@linaro.org>
Change-Id: I3c8eebbce3079292fc85dad8984efe6382f22cac

tools/testing/selftests/filesystems/incfs/utils.c
@@ -81,6 +81,16 @@ int mount_fs(const char *mount_dir, const char *backing_dir,
 	return result;
 }
 
+int umount_fs(const char *mount_dir)
+{
+	int result;
+
+	result = umount(mount_dir);
+	if (result != 0)
+		perror("Error mounting fs.");
+	return result;
+}
+
 int mount_fs_opt(const char *mount_dir, const char *backing_dir,
 		 const char *opt, bool remount)
 {

MiCode/Xiaomi_Kernel_OpenSource

tools/testing/selftests/filesystems/incfs/utils.c
@@ -81,6 +81,16 @@ int mount_fs(const char *mount_dir, const char *backing_dir,
 	return result;
 }
 
+int umount_fs(const char *mount_dir)
+{
+	int result;
+
+	result = umount(mount_dir);
+	if (result != 0)
+		perror("Error unmounting fs.");
+	return result;
+}
+
 int mount_fs_opt(const char *mount_dir, const char *backing_dir,
 		 const char *opt, bool remount)
 {