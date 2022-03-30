0ef21e1c1ae5cbde091fc7bbbc5ebc5962d9bb13
Saravana Kannan <saravanak@google.com>
2022-03-30 14:58:30 -0700

    ANDROID: vendor_hooks: Reduce pointless modversions CRC churn
    When vendor hooks are added to a file that previously didn't have any
    vendor hooks, we end up indirectly including linux/tracepoint.h.  This
    causes some data types that used to be opaque (forward declared) to the
    code to become visible to the code.
    
    Modversions correctly catches this change in visibility, but we don't
    really care about the data types made visible when linux/tracepoint.h is
    included. So, hide this from modversions in the central vendor_hooks.h file
    instead of having to fix this on a case by case basis.
    
    This change itself will cause a one time CRC breakage/churn because it's
    fixing the existing vendor hook headers, but should reduce unnecessary CRC
    churns in the future.
    
    To avoid future pointless CRC churn, vendor hook header files that include
    vendor_hooks.h should not include linux/tracepoint.h directly.
    
    Bug: 227513263
    Bug: 226140073
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Change-Id: Ia88e6af11dd94fe475c464eb30a6e5e1e24c938b

--- include/trace/hooks/timekeeping.h
+++ include/trace/hooks/timekeeping.h
@@ -7,7 +7,6 @@
 #if !defined(_TRACE_HOOK_TIMEKEEPING_H) || defined(TRACE_HEADER_MULTI_READ)
 #define _TRACE_HOOK_TIMEKEEPING_H
 
-#include <linux/tracepoint.h>
 #include <trace/hooks/vendor_hooks.h>
 
 struct timekeeper;
