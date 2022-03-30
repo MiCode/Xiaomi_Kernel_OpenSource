a202f8b7dc28135f7eab05c7865f92291823c73d
Cheng Jui Wang <cheng-jui.wang@mediatek.com>
2022-03-30 11:04:03 +0800

    [ALPS06395584] locking_aee: remove debug_locks_off kretprobe
    Various deadlocks caused by calling aee_kernel_warning_api in kretprobe
    are detected.
    
    Remove the kretprobe and let locking_aee_thread to handle aee.
    
    MTK-Commit-Id: 22fa9b6cb4631680f7d0849b3bdcde40628fa39d
    
    Signed-off-by: Cheng Jui Wang <cheng-jui.wang@mediatek.com>
    CR-Id: ALPS06395584
    Feature: Lockdep
    Change-Id: Ieeaed24e3d9bad751eeda1bb9eaccb9be2f383e2
    (cherry picked from commit fe8f6a167df94182c407af9bae9a402fa278e1b3)

--- drivers/misc/mediatek/locking/locking_aee_main.c
+++ drivers/misc/mediatek/locking/locking_aee_main.c
@@ -21,75 +21,6 @@ int lockdep_test_init(void);
 
 static atomic_t warned;
 
-static const char * const critical_lock_list[] = {
-	/* runqueue */
-	"&rq->lock",
-	/* workqueue */
-	"&(&pool->lock)->rlock",
-	/* kmalloc */
-	"&(&n->list_lock)->rlock",
-	"&(&zone->lock)->rlock",
-	/* stacktrace */
-	"depot_lock",
-	/* console */
-	"&(&port->lock)->rlock"
-};
-
-static int is_critical_lock_held(void)
-{
-	int i, j;
-	struct held_lock *hlock;
-
-	/* check locks held by current task */
-	if (!current->lockdep_depth)
-		return false;
-
-	hlock = current->held_locks;
-	for (i = 0; i < current->lockdep_depth; i++) {
-		struct lockdep_map *instance = (hlock + i)->instance;
-
-		if (!instance->name)
-			continue;
-
-		for (j = 0; j < ARRAY_SIZE(critical_lock_list); j++)
-			if (!strcmp(instance->name, critical_lock_list[j]))
-				return true;
-	}
-	return false;
-}
-
-static int debug_locks_off_kretprobe_handler(struct kretprobe_instance *ri,
-		struct pt_regs *regs)
-{
-	int retval = regs_return_value(regs);
-	u32 val_warned = 0;
-
-	if (!retval)
-		return 0;
-
-	if (is_critical_lock_held())
-		return 0;
-
-	if (likely(atomic_try_cmpxchg(&warned, &val_warned, 1U))) {
-		char aee_str[40];
-
-		pr_info("LockProve Warning, AEE!\n");
-		scnprintf(aee_str, 40, "[%s]LockProve Warning", current->comm);
-		aee_kernel_warning_api(__FILE__, __LINE__,
-				DB_OPT_DUMMY_DUMP | DB_OPT_FTRACE,
-				aee_str, "LockProve Debug\n");
-	}
-	return 0;
-}
-
-static struct kretprobe debug_locks_off_kretprobe = {
-	.kp = {
-		.symbol_name = "debug_locks_off",
-	},
-	.handler = debug_locks_off_kretprobe_handler,
-	.maxactive = 1,
-};
-
 static int locking_aee_thread(void *data)
 {
 	u32 val_warned = 0;
@@ -130,27 +61,12 @@ static void ldt_disable_aee(void)
 
 static int __init locking_aee_init(void)
 {
-	int ret;
-
-	ret = register_kretprobe(&debug_locks_off_kretprobe);
-	if (ret < 0)
-		pr_info("register debug_locks_off kretprobe failed, returned %d\n", ret);
-	else
-		pr_info("register debug_locks_off kretprobe succeeded.\n");
-
 	monitor_hang_regist_ldt(ldt_disable_aee);
 	kthread_run(locking_aee_thread, NULL, "locking_aee");
 	lockdep_test_init();
 	return 0;
 }
 
-static void __exit locking_aee_exit(void)
-{
-	unregister_kretprobe(&debug_locks_off_kretprobe);
-	monitor_hang_regist_ldt(NULL);
-}
-
 module_init(locking_aee_init);
-module_exit(locking_aee_exit);
 MODULE_DESCRIPTION("MEDIATEK LOCKING AEE TOOL");
 MODULE_LICENSE("GPL v2");
