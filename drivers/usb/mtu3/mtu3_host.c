af086e979300cf6bb132553c58ff097ec3c44f6b
Eddie Hung <eddie.hung@mediatek.com>
2022-03-30 11:03:44 +0800

    [ALPS06649370] mtu3: add host mode wakeup setting
    Add 1.03 version wakeup settign to support mt6983 etc.
    
    MTK-Commit-Id: 5693c5b3ab2278b367a85dccc355e7281332c1c5
    
    Signed-off-by: Eddie Hung <eddie.hung@mediatek.com>
    CR-Id: ALPS06649370
    Feature: usb_ssusb
    Change-Id: Ib3682aa3a3d32350446038b75591c62fa2d64cae

--- drivers/usb/mtu3/mtu3_host.c
+++ drivers/usb/mtu3/mtu3_host.c
@@ -17,6 +17,12 @@
 #include "mtu3.h"
 #include "mtu3_dr.h"
 
+
+/* mt6983 etc */
+#define PERI_WK_CTRL2	0x8
+#define UWK_V1_3_CTRL0_MASK	0x3
+#define UWK_V1_3_CTRL2_MASK	0x699
+
 /* mt8173 etc */
 #define PERI_WK_CTRL1	0x4
 #define WC1_IS_C(x)	(((x) & 0xf) << 26)  /* cycle debounce */
@@ -43,6 +49,7 @@ enum ssusb_uwk_vers {
 	SSUSB_UWK_V2,
 	SSUSB_UWK_V1_1 = 101,	/* specific revision 1.01 */
 	SSUSB_UWK_V1_2,		/* specific revision 1.02 */
+	SSUSB_UWK_V1_3,		/* specific revision 1.03 */
 };
 
 /*
@@ -67,6 +74,16 @@ static void ssusb_wakeup_ip_sleep_set(struct ssusb_mtk *ssusb, bool enable)
 	case SSUSB_UWK_V1_2:
 		reg = ssusb->uwk_reg_base + PERI_WK_CTRL0;
 		msk = WC0_SSUSB0_CDEN | WC0_IS_SPM_EN;
+		val = enable ? msk : 0;
+		break;
+	case SSUSB_UWK_V1_3:
+		reg = ssusb->uwk_reg_base + PERI_WK_CTRL0;
+		msk = UWK_V1_3_CTRL0_MASK;
+		val = enable ? msk : 0;
+		regmap_update_bits(ssusb->uwk, reg, msk, val);
+
+		reg = ssusb->uwk_reg_base + PERI_WK_CTRL2;
+		msk = UWK_V1_3_CTRL2_MASK;
 		val = enable ? msk : 0;
 		break;
 	case SSUSB_UWK_V2:
