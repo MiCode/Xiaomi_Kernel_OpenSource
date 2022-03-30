32d62d188679e6b4875dbfdd2703aba0bd7b3669
Celine Liu <celine.liu@mediatek.com>
2022-03-30 11:26:20 +0800

    [ALPS06818486] adsp: remove FPGA early porting option
    Remove CONFIG_FPGA_EARLY_PORTING from adsp driver.
    
    MTK-Commit-Id: 70f2007ce353d6a371111f11a72153e08c0ed94d
    
    CR-Id: ALPS06818486
    Feature: Audio OpenDSP
    Change-Id: I940d76106c7a2c7705f1c397d4eb82e409a9280c
    Signed-off-by: Celine Liu <celine.liu@mediatek.com>

--- drivers/misc/mediatek/adsp/adsp_reserved_mem.c
+++ drivers/misc/mediatek/adsp/adsp_reserved_mem.c
@@ -42,10 +42,8 @@ static struct adsp_reserve_mblock adsp_reserve_mblocks[] = {
 		= ADSP_RESERVE_MEMORY_BLOCK("adsp-rsv-core-dump-a"),
 	[ADSP_B_CORE_DUMP_MEM_ID]
 		= ADSP_RESERVE_MEMORY_BLOCK("adsp-rsv-core-dump-b"),
-#if !IS_ENABLED(CONFIG_FPGA_EARLY_PORTING)
 	[ADSP_AUDIO_COMMON_MEM_ID]
 		= ADSP_RESERVE_MEMORY_BLOCK("adsp-rsv-audio"),
-#endif
 };
 
 static struct adsp_reserve_mblock *adsp_get_reserve_mblock(
