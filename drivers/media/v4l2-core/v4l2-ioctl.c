16934ad43d4b79730d202c57cc8e51c1902d8a77
Louis Kuo <louis.kuo@mediatek.com>
2022-03-30 10:22:53 +0800

    [ALPS06348702] hook: call v4l2 hook function
    [Detail]
    call v4l2 hook function
    
    MTK-Commit-Id: 7ad042e08d5af30856266afbfdd083be78153289
    
    CR-Id: ALPS06348702
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: Louis Kuo <louis.kuo@mediatek.com>
    Change-Id: I07fa7590dee4424f31ef962a7ca8fe651d946c07

--- drivers/media/v4l2-core/v4l2-ioctl.c
+++ drivers/media/v4l2-core/v4l2-ioctl.c
@@ -29,6 +29,9 @@
 #include <media/v4l2-mem2mem.h>
 
 #include <trace/events/v4l2.h>
+#if IS_ENABLED(CONFIG_MTK_CAMSYS_VEND_HOOK)
+#include <trace/hooks/v4l2core.h>
+#endif
 
 /* Zero out the end of the struct pointed to by p.  Everything after, but
  * not including, the specified field is cleared. */
@@ -76,6 +79,19 @@ static const struct std_descr standards[] = {
 	{ V4L2_STD_SECAM_LC,	"SECAM-Lc"  },
 	{ 0,			"Unknown"   }
 };
+
+static void clear_reserved(struct v4l2_format *p)
+{
+#if IS_ENABLED(CONFIG_MTK_CAMSYS_VEND_HOOK)
+	int ret = 0;
+
+	trace_android_vh_clear_reserved_fmt_fields(p, &ret);
+	if (!ret)
+		CLEAR_AFTER_FIELD(p, fmt.pix_mp.xfer_func);
+#else
+	CLEAR_AFTER_FIELD(p, fmt.pix_mp.xfer_func);
+#endif
+}
 
 /* video4linux standard ID conversion to standard name
  */
@@ -1417,6 +1433,11 @@ static void v4l_fill_fmtdesc(struct v4l2_fmtdesc *fmt)
 		case V4L2_PIX_FMT_MT21C:	descr = "Mediatek Compressed Format"; break;
 		case V4L2_PIX_FMT_SUNXI_TILED_NV12: descr = "Sunxi Tiled NV12 Format"; break;
 		default:
+#if IS_ENABLED(CONFIG_MTK_CAMSYS_VEND_HOOK)
+			trace_android_vh_fill_ext_fmtdesc(fmt, &descr);
+			if (descr)
+				break;
+#endif
 			if (fmt->description[0])
 				return;
 			WARN(1, "Unknown pixelformat 0x%08x\n", fmt->pixelformat);
@@ -1634,7 +1655,7 @@ static int v4l_s_fmt(const struct v4l2_ioctl_ops *ops,
 	case V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE:
 		if (unlikely(!ops->vidioc_s_fmt_vid_cap_mplane))
 			break;
-		CLEAR_AFTER_FIELD(p, fmt.pix_mp.xfer_func);
+		clear_reserved(p);
 		for (i = 0; i < p->fmt.pix_mp.num_planes; i++)
 			CLEAR_AFTER_FIELD(&p->fmt.pix_mp.plane_fmt[i],
 					  bytesperline);
@@ -1665,7 +1686,7 @@ static int v4l_s_fmt(const struct v4l2_ioctl_ops *ops,
 	case V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE:
 		if (unlikely(!ops->vidioc_s_fmt_vid_out_mplane))
 			break;
-		CLEAR_AFTER_FIELD(p, fmt.pix_mp.xfer_func);
+		clear_reserved(p);
 		for (i = 0; i < p->fmt.pix_mp.num_planes; i++)
 			CLEAR_AFTER_FIELD(&p->fmt.pix_mp.plane_fmt[i],
 					  bytesperline);
@@ -1736,7 +1757,7 @@ static int v4l_try_fmt(const struct v4l2_ioctl_ops *ops,
 	case V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE:
 		if (unlikely(!ops->vidioc_try_fmt_vid_cap_mplane))
 			break;
-		CLEAR_AFTER_FIELD(p, fmt.pix_mp.xfer_func);
+		clear_reserved(p);
 		for (i = 0; i < p->fmt.pix_mp.num_planes; i++)
 			CLEAR_AFTER_FIELD(&p->fmt.pix_mp.plane_fmt[i],
 					  bytesperline);
@@ -1767,7 +1788,7 @@ static int v4l_try_fmt(const struct v4l2_ioctl_ops *ops,
 	case V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE:
 		if (unlikely(!ops->vidioc_try_fmt_vid_out_mplane))
 			break;
-		CLEAR_AFTER_FIELD(p, fmt.pix_mp.xfer_func);
+		clear_reserved(p);
 		for (i = 0; i < p->fmt.pix_mp.num_planes; i++)
 			CLEAR_AFTER_FIELD(&p->fmt.pix_mp.plane_fmt[i],
 					  bytesperline);
@@ -3118,6 +3139,9 @@ static int video_get_user(void __user *arg, void *parg,
 		if (flags & INFO_FL_CLEAR_MASK)
 			n = (flags & INFO_FL_CLEAR_MASK) >> 16;
 		*always_copy = flags & INFO_FL_ALWAYS_COPY;
+#if IS_ENABLED(CONFIG_MTK_CAMSYS_VEND_HOOK)
+		trace_android_vh_clear_mask_adjust(v4l2_ioctls[_IOC_NR(cmd)].ioctl, &n);
+#endif
 	}
 
 	if (cmd == real_cmd) {
