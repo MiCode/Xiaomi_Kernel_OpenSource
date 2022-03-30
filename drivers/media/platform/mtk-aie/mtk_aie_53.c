3baffe734fc105ba66b495e1c406dbf675cdb858
Ya-Wen Hsu <Ya-Wen.Hsu@mediatek.com>
2022-03-30 11:36:48 +0800

    [ALPS07058724] AIE: debug info for fd config
    [Detail] debug info for fd config
    
    MTK-Commit-Id: 452cb79f1d0b9bd683f862bb8de9995273618b0e
    
    Signed-off-by: Ya-Wen Hsu <Ya-Wen.Hsu@mediatek.com>
    CR-Id: ALPS07058724
    Feature: isp_fd
    Change-Id: I3bc9fc02ddc01d4e37796ba39e1d156099161692

--- drivers/media/platform/mtk-aie/mtk_aie_53.c
+++ drivers/media/platform/mtk-aie/mtk_aie_53.c
@@ -961,14 +961,13 @@ static signed int fdvt_dump_reg(struct mtk_aie_dev *fd)
 		FDVT_DumpDRAMOut(fd, (u32 *)fd->base_para->fd_rs_cfg_va, fd->fd_rs_cfg_size);
 
 		loop_num = (unsigned int)readl(fd->fd_base + FDVT_DEBUG_INFO_0) & 0xFF;
-		if (!loop_num)
-			loop_num = 1;
+
 		dev_info(fd->dev,
 			"fdmode_fdvt_fd_config:	0x%x, fdmode_fdvt_fd_config_size:	%d",
-			(unsigned int *)fd->base_para->fd_fd_cfg_va + (loop_num - 1) * 56,
-			(fd->fd_fd_cfg_size)/87);
-		FDVT_DumpDRAMOut(fd, (u32 *)fd->base_para->fd_fd_cfg_va + (loop_num - 1) * 56,
-			(fd->fd_fd_cfg_size)/87);
+			(unsigned int *)fd->base_para->fd_fd_cfg_va,
+			((fd->fd_fd_cfg_size)/87) * loop_num);
+		FDVT_DumpDRAMOut(fd, (u32 *)fd->base_para->fd_fd_cfg_va,
+			((fd->fd_fd_cfg_size)/87) * loop_num);
 
 		dev_info(fd->dev, "FDVT DMA Debug Info\n");
 
