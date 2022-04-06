19daf81ebada338ff9382c365cfa1e3ff85ac901
Wendell Lin <wendell.lin@mediatek.com>
2022-04-06 11:50:11 +0800

    [ALPS07083634]  mt6985 clk: update larb13/14 cg control
    Set larb13 & larb14 as dummy cg, and duplicae for retry
    
    MTK-Commit-Id: 371e2c575e34a48fa3588367d29807b20060b2bb
    
    Signed-off-by: Wendell Lin <wendell.lin@mediatek.com>
    CR-Id: ALPS07083634
    Feature: CLKMGR_Kernel
    Change-Id: Ifa171034d20fb3455b4dab388e30f100791ca97e

--- drivers/clk/mediatek/clk-mt6983-cam.c
+++ drivers/clk/mediatek/clk-mt6983-cam.c
@@ -53,12 +53,27 @@ static const struct mtk_gate_regs cam_main_r1a_0_cg_regs = {
 		.ops = &mtk_clk_gate_ops_setclr_dummys,			\
 	}
 
+#define GATE_CAM_MAIN_R1A_0_DUMMY(_id, _name, _parent, _shift) {		\
+		.id = _id,						\
+		.name = _name,						\
+		.parent_name = _parent,					\
+		.regs = &cam_main_r1a_0_cg_regs,			\
+		.shift = _shift,					\
+		.ops = &mtk_clk_gate_ops_setclr_dummy,			\
+	}
+
 static struct mtk_gate cam_main_r1a_clks[] = {
-	GATE_CAM_MAIN_R1A_0(CLK_CAM_MAIN_R1A_CAM_MAIN_LARB13_CG_CON /* CLK ID */,
+	GATE_CAM_MAIN_R1A_0_DUMMY(CLK_CAM_MAIN_R1A_CAM_MAIN_LARB13_CG_CON /* CLK ID */,
 		"c_larb13_con" /* name */,
 		"cam_ck" /* parent */, 0 /* bit */),
-	GATE_CAM_MAIN_R1A_0(CLK_CAM_MAIN_R1A_CAM_MAIN_LARB14_CG_CON /* CLK ID */,
+	GATE_CAM_MAIN_R1A_0_DUMMY(CLK_CAM_MAIN_R1A_CAM_MAIN_LARB14_CG_CON /* CLK ID */,
 		"c_larb14_con" /* name */,
+		"cam_ck" /* parent */, 1 /* bit */),
+	GATE_CAM_MAIN_R1A_0_DUMMY(CLK_CAM_MAIN_R1A_CAM_MAIN_LARB13R_CG_CON /* CLK ID */,
+		"c_larb13r_con" /* name */,
+		"cam_ck" /* parent */, 0 /* bit */),
+	GATE_CAM_MAIN_R1A_0_DUMMY(CLK_CAM_MAIN_R1A_CAM_MAIN_LARB14R_CG_CON /* CLK ID */,
+		"c_larb14r_con" /* name */,
 		"cam_ck" /* parent */, 1 /* bit */),
 	GATE_CAM_MAIN_R1A_0(CLK_CAM_MAIN_R1A_CAM_MAIN_CAM_CG_CON /* CLK ID */,
 		"c_cam_con" /* name */,
