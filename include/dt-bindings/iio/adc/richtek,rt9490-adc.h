1e13fb92d6564795211eb9ccc21bfca63c4759ee
Gerard Huang <Gerard.Huang@mediatek.com>
2022-04-06 11:50:25 +0800

    [ALPS07070665] dt-bindings: subpmic: Add iio and mfd dt-bindings
    Add subpmic dt-bindings for rt9490
    
    MTK-Commit-Id: 4da8e10488781ded31eb45c67b5016c2d201abcc
    
    Signed-off-by: Gerard Huang <Gerard.Huang@mediatek.com>
    CR-Id: ALPS07070665
    Feature: Charger
    Change-Id: I114ad3524412cdc64b09950ded7c0de9ac633124

---
+++ include/dt-bindings/iio/adc/richtek,rt9490-adc.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022 MediaTek Inc.
+ */
+
+#ifndef _DT_BINDINGS_IIO_ADC_RICHTEK_RT9490_ADC_H
+#define _DT_BINDINGS_IIO_ADC_RICHTEK_RT9490_ADC_H
+
+#define RT9490_CHAN_TDIE	0
+#define RT9490_CHAN_TS		1
+#define RT9490_CHAN_VSYS	2
+#define RT9490_CHAN_VBAT	3
+#define RT9490_CHAN_VBUS	4
+#define RT9490_CHAN_IBAT	5
+#define RT9490_CHAN_IBUS	6
+#define RT9490_CHAN_VAC1	7
+#define RT9490_CHAN_VAC2	8
+#define RT9490_CHAN_DM		9
+#define RT9490_CHAN_DP		10
+
+#endif
