828cd999b72253b58f5cf2a5f31b891dbfd69dbd
mtk23909 <jibin.xu@mediatek.com>
2022-04-06 21:09:04 +0800

    [ALPS06434935] DISP: mt6789 modify rdma fifo size
    [Detail]
    mt6789 modify rdma fifo size.
    
    MTK-Commit-Id: 3184991e1ace1c33761970706bf00ee901244c38
    
    CR-Id: ALPS06434935
    Feature: display_mobile
    Signed-off-by: mtk23909 <jibin.xu@mediatek.com>
    Change-Id: I792889d2c30b84106c9d361492e2b3b7639c99fa

--- drivers/gpu/drm/mediatek/mediatek_v2/platform/mtk_drm_6789.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/platform/mtk_drm_6789.c
@@ -71,7 +71,8 @@ const struct mtk_disp_wdma_data mt6789_wdma_driver_data = {
 
 // rdma
 const struct mtk_disp_rdma_data mt6789_rdma_driver_data = {
-	.fifo_size = SZ_1K * 3 + SZ_32K,
+	.fifo_size = SZ_32K + SZ_8K + SZ_4K + SZ_2K + SZ_1K +
+			SZ_512 + SZ_256 + SZ_64 + SZ_32,
 	.pre_ultra_low_us = 250,
 	.pre_ultra_high_us = 260,
 	.ultra_low_us = 230,
@@ -1003,6 +1004,8 @@ void mmsys_config_dump_analysis_mt6789(void __iomem *config_regs)
 #define MT6789_INFRA_DISP_DDR_MASK 0xC02
 #define MT6789_SODI_REQ_SEL_ALL                   REG_FLD_MSB_LSB(9, 8)
 #define MT6789_SODI_REQ_VAL_ALL                   REG_FLD_MSB_LSB(13, 12)
+#define MT6789_FLD_OVL0_RDMA_ULTRA_SEL            REG_FLD_MSB_LSB(3, 2)
+#define MT6789_FLD_OVL0_2L_RDMA_ULTRA_SEL         REG_FLD_MSB_LSB(7, 6)
 
 static void mt6789_mtk_sodi_config(struct drm_device *drm, enum mtk_ddp_comp_id id,
 			struct cmdq_pkt *handle, void *data)
@@ -1010,6 +1013,7 @@ static void mt6789_mtk_sodi_config(struct drm_device *drm, enum mtk_ddp_comp_id 
 	struct mtk_drm_private *priv = drm->dev_private;
 	unsigned int sodi_req_val = 0, sodi_req_mask = 0;
 	unsigned int emi_req_val = 0, emi_req_mask = 0;
+	unsigned int ultra_ovl_val = 0, ultra_ovl_mask = 0;
 	bool en = *((bool *)data);
 
 	if (id == DDP_COMPONENT_ID_MAX) { /* config when top clk on */
@@ -1076,6 +1080,16 @@ static void mt6789_mtk_sodi_config(struct drm_device *drm, enum mtk_ddp_comp_id 
 			} else
 				DDPINFO("%s: failed to disable infra ddr control\n", __func__);
 		}
+
+		/* enable ultra signal from rdma to ovl0 and ovl1_2l */
+		v = readl(priv->config_regs +  DISP_REG_CONFIG_MMSYS_MISC);
+		SET_VAL_MASK(ultra_ovl_val, ultra_ovl_mask, 0,
+			MT6789_FLD_OVL0_RDMA_ULTRA_SEL);
+		v = (v & ~ultra_ovl_mask) | (ultra_ovl_val & ultra_ovl_mask);
+		SET_VAL_MASK(ultra_ovl_val, ultra_ovl_mask, 0,
+			MT6789_FLD_OVL0_2L_RDMA_ULTRA_SEL);
+		v = (v & ~ultra_ovl_mask) | (ultra_ovl_val & ultra_ovl_mask);
+		writel_relaxed(v, priv->config_regs +  DISP_REG_CONFIG_MMSYS_MISC);
 	} else {
 		cmdq_pkt_write(handle, NULL, priv->config_regs_pa +
 			MMSYS_SODI_REQ_MASK, sodi_req_val, sodi_req_mask);
@@ -1090,6 +1104,16 @@ static void mt6789_mtk_sodi_config(struct drm_device *drm, enum mtk_ddp_comp_id 
 			} else
 				DDPINFO("%s: failed to disable infra ddr control\n", __func__);
 		}
+
+		/* enable ultra signal from rdma to ovl0 and ovl1_2l*/
+		SET_VAL_MASK(ultra_ovl_val, ultra_ovl_mask, 0,
+			MT6789_FLD_OVL0_RDMA_ULTRA_SEL);
+		cmdq_pkt_write(handle, NULL, priv->config_regs_pa + DISP_REG_CONFIG_MMSYS_MISC,
+			       ultra_ovl_val, ultra_ovl_mask);
+		SET_VAL_MASK(ultra_ovl_val, ultra_ovl_mask, 0,
+			MT6789_FLD_OVL0_2L_RDMA_ULTRA_SEL);
+		cmdq_pkt_write(handle, NULL, priv->config_regs_pa + DISP_REG_CONFIG_MMSYS_MISC,
+			       ultra_ovl_val, ultra_ovl_mask);
 	}
 }
 
