89f91db7ae81df925f771b4ab22996663fa3c34e
Andreas Gruenbacher <agruenba@redhat.com>
2022-04-15 06:28:48 +0800

    UPSTREAM: iomap: Fix iomap_dio_rw return value for user copies
    commit 42c498c18a94eed79896c50871889af52fa0822e upstream
    
    When a user copy fails in one of the helpers of iomap_dio_rw, fail with
    -EFAULT instead of returning 0.  This matches what iomap_dio_bio_actor
    returns when it gets an -EFAULT from bio_iov_iter_get_pages.  With these
    changes, iomap_dio_actor now consistently fails with -EFAULT when a user
    page cannot be faulted in.
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Anand Jain <anand.jain@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit a00cc46f97b9b9544c5edabc81d6cbfadd0ffdab)
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Ib80793f127fe9e0a72195802fcb5a865834c05fc

--- fs/iomap/direct-io.c
+++ fs/iomap/direct-io.c
@@ -381,6 +381,8 @@ static loff_t iomap_dio_hole_iter(const struct iomap_iter *iter,
 	loff_t length = iov_iter_zero(iomap_length(iter), dio->submit.iter);
 
 	dio->size += length;
+	if (!length)
+		return -EFAULT;
 	return length;
 }
 
@@ -412,6 +414,8 @@ static loff_t iomap_dio_inline_iter(const struct iomap_iter *iomi,
 		copied = copy_to_iter(inline_data, length, iter);
 	}
 	dio->size += copied;
+	if (!copied)
+		return -EFAULT;
 	return copied;
 }
 
