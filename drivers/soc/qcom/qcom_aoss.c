aa2547b2e6722ddcaee0d927cb45ca2dbe724690
Sivaji Boddupilli <quic_boddupil@quicinc.com>
2022-04-20 15:01:17 +0530

    soc: qcom: Add compatible string for sdxpinn
    Enable the aoss-qmp driver and compatible string
    for sdxpinn.
    
    Change-Id: Ief7100c665c22c1f706589f5aaa775726fda1df2
    Signed-off-by: Sivaji Boddupilli <quic_boddupil@quicinc.com>

--- drivers/soc/qcom/qcom_aoss.c
+++ drivers/soc/qcom/qcom_aoss.c
@@ -711,6 +711,7 @@ static const struct of_device_id qmp_dt_match[] = {
 	{ .compatible = "qcom,aoss-qmp", },
 	{ .compatible = "qcom,waipio-aoss-qmp", },
 	{ .compatible = "qcom,cinder-aoss-qmp", },
+	{ .compatible = "qcom,sdxpinn-aoss-qmp", },
 	{}
 };
 MODULE_DEVICE_TABLE(of, qmp_dt_match);
