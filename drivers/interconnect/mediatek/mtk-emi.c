ddb12d5d47802b3b80491124c74afabafde4dc40
Jerry Lin <jerry-kt.lin@mediatek.com>
2022-04-22 01:00:57 +0800

    [ALPS07068839] interconnect: mediatek: emi: add mt6886 dvfsrc
    Add support of mt6886 dvfsrc interconnect driver.
    
    MTK-Commit-Id: 0b1a29ea56fa401009e337d4b4ae1032274198d3
    
    Signed-off-by: Jerry Lin <jerry-kt.lin@mediatek.com>
    CR-Id: ALPS07068839
    Feature: VCORE DVFS
    Change-Id: I98b11f779dd0b79b7b3884b3df136c190913c0c5

--- drivers/interconnect/mediatek/mtk-emi.c
+++ drivers/interconnect/mediatek/mtk-emi.c
@@ -214,6 +214,7 @@ static const struct of_device_id emi_icc_of_match[] = {
 	{ .compatible = "mediatek,mt6879-dvfsrc", .data = &mt6873_icc },
 	{ .compatible = "mediatek,mt6855-dvfsrc", .data = &mt6873_icc },
 	{ .compatible = "mediatek,mt6985-dvfsrc", .data = &mt6873_icc },
+	{ .compatible = "mediatek,mt6886-dvfsrc", .data = &mt6873_icc },
 	{ },
 };
 MODULE_DEVICE_TABLE(of, emi_icc_of_match);
