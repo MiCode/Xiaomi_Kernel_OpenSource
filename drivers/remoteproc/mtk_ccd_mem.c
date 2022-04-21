78391453aca858c749f4716cad3a2a1cb4d789d8
roy.yang <roy.yang@mediatek.com>
2022-04-22 00:57:13 +0800

    [ALPS07071185] remoteproc: remove close fd (revert)
    close fd api is forbidden now
    
    MTK-Commit-Id: 9e59b59775cb753d156cc1832f368e4f5ca7dd88
    
    CR-Id: ALPS07071185
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: "roy.yang" <roy.yang@mediatek.com>
    Change-Id: I959fb87c1d6d956beaa304a96f9ff020f36ce1a3

--- drivers/remoteproc/mtk_ccd_mem.c
+++ drivers/remoteproc/mtk_ccd_mem.c
@@ -20,10 +20,8 @@
 #include <linux/rpmsg/mtk_ccd_rpmsg.h>
 #include <uapi/linux/mtk_ccd_controls.h>
 #include <uapi/linux/dma-heap.h>
-
 #define CCD_ALLOCATE_MAX_BUFFER_SIZE 0x20000000UL /*512MB*/
 struct mtk_ccd_buf;
-
 struct mtk_ccd_buf {
 	struct device *dev;
 	void *vaddr;
@@ -35,7 +33,6 @@ struct mtk_ccd_buf {
 	struct dma_buf_attachment *db_attach;
 	struct dma_buf_map map;
 };
-
 static struct mtk_ccd_buf *mtk_ccd_buf_alloc(
 					   struct device *dev, unsigned long size)
 {
@@ -46,26 +43,22 @@ static struct mtk_ccd_buf *mtk_ccd_buf_alloc(
 	buf = kzalloc(sizeof(*buf), GFP_KERNEL);
 	if (!buf)
 		return ERR_PTR(-ENOMEM);
-
 	dma_heap = dma_heap_find("mtk_mm-uncached");
 	if (!dma_heap) {
 		pr_info("dma_heap find fail\n");
 		return ERR_PTR(-ENOMEM);
 	}
-
 	buf->dbuf = dma_heap_buffer_alloc(dma_heap, size,
 			O_RDWR | O_CLOEXEC, DMA_HEAP_VALID_HEAP_FLAGS);
 	if (IS_ERR(buf->dbuf)) {
 		pr_info("dma_heap buffer alloc fail\n");
 		return ERR_PTR(-ENOMEM);
 	}
-
 	buf->db_attach = dma_buf_attach(buf->dbuf, dev);
 	if (IS_ERR(buf->db_attach)) {
 		pr_info("dma_heap attach fail\n");
 		return ERR_PTR(-ENOMEM);
 	}
-
 	buf->dma_sgt = dma_buf_map_attachment(buf->db_attach,
 				DMA_BIDIRECTIONAL);
 	if (IS_ERR(buf->dma_sgt)) {
@@ -73,56 +66,54 @@ static struct mtk_ccd_buf *mtk_ccd_buf_alloc(
 		dma_buf_detach(buf->dbuf, buf->db_attach);
 		return ERR_PTR(-ENOMEM);
 	}
-
+	pr_info("Before dma_buf_vmap\n");
 	dma_buf_vmap(buf->dbuf, &map);
+	pr_info("After dma_buf_vmap\n");
 	buf->vaddr = map.vaddr;
 	buf->map = map;
 	buf->dma_addr = sg_dma_address(buf->dma_sgt->sgl);
 	buf->dev = get_device(dev);
 	buf->size = size;
+	/* increase file count */
+	get_dma_buf(buf->dbuf);
 	return buf;
 }
-
 static void mtk_ccd_buf_put(struct mtk_ccd_buf *buf)
 {
 	/* free va */
-	if (buf->vaddr)
+	if (buf->vaddr) {
+		pr_info("Before dma_buf_vunmap\n");
 		dma_buf_vunmap(buf->dbuf, &buf->map);
-
+		pr_info("After dma_buf_vunmap\n");
+	}
 	/* free iova */
 	if (buf->db_attach && buf->dma_sgt)
 		dma_buf_unmap_attachment(
 			buf->db_attach, buf->dma_sgt, DMA_BIDIRECTIONAL);
-
 	if (buf->dbuf && buf->db_attach)
 		dma_buf_detach(buf->dbuf, buf->db_attach);
-
 	/* decrease file count */
 	dma_heap_buffer_free(buf->dbuf);
-
 	put_device(buf->dev);
 	kfree(buf);
 }
-
 static dma_addr_t mtk_ccd_buf_get_daddr(struct mtk_ccd_buf *buf)
 {
 	return buf->dma_addr;
 }
-
 static void *mtk_ccd_buf_get_vaddr(struct mtk_ccd_buf *buf)
 {
 	if (!buf->vaddr && buf->db_attach) {
+		pr_info("Before dma_buf_vmap\n");
 		dma_buf_vmap(buf->db_attach->dmabuf, &buf->map);
+		pr_info("After dma_buf_vmap\n");
 		buf->vaddr = buf->map.vaddr;
 	}
-
 	return buf->vaddr;
 }
-
 struct dma_buf *mtk_ccd_get_buffer_dmabuf(struct mtk_ccd *ccd, void *mem_priv)
 {
 	struct mtk_ccd_buf *buf = mem_priv;
-
 	if (ccd == NULL || buf == NULL) {
 		pr_info("mtk_ccd or mem_priv is NULL\n");
 		return NULL;
@@ -130,11 +121,9 @@ struct dma_buf *mtk_ccd_get_buffer_dmabuf(struct mtk_ccd *ccd, void *mem_priv)
 	return buf->dbuf;
 }
 EXPORT_SYMBOL_GPL(mtk_ccd_get_buffer_dmabuf);
-
 struct mtk_ccd_memory *mtk_ccd_mem_init(struct device *dev)
 {
 	struct mtk_ccd_memory *ccd_memory;
-
 	ccd_memory = kzalloc(sizeof(*ccd_memory), GFP_KERNEL);
 	if (!ccd_memory)
 		return NULL;
@@ -144,14 +133,12 @@ struct mtk_ccd_memory *mtk_ccd_mem_init(struct device *dev)
 	return ccd_memory;
 }
 EXPORT_SYMBOL_GPL(mtk_ccd_mem_init);
-
 void mtk_ccd_mem_release(struct mtk_ccd *ccd)
 {
 	struct mtk_ccd_memory *ccd_memory = ccd->ccd_memory;
 	kfree(ccd_memory);
 }
 EXPORT_SYMBOL_GPL(mtk_ccd_mem_release);
-
 void *mtk_ccd_get_buffer(struct mtk_ccd *ccd,
 			 struct mem_obj *mem_buff_data)
 {
@@ -161,7 +148,6 @@ void *mtk_ccd_get_buffer(struct mtk_ccd *ccd,
 	struct mtk_ccd_buf *buf;
 	struct mtk_ccd_mem *ccd_buffer;
 	struct mtk_ccd_memory *ccd_memory = ccd->ccd_memory;
-
 	mem_buff_data->iova = 0;
 	mem_buff_data->va = NULL;
 	mutex_lock(&ccd_memory->mmap_lock);
@@ -175,7 +161,6 @@ void *mtk_ccd_get_buffer(struct mtk_ccd *ccd,
 		mutex_unlock(&ccd_memory->mmap_lock);
 		return ERR_PTR(-EINVAL);
 	}
-
 	ccd_buffer = &ccd_memory->bufs[buffers];
 	buf = mtk_ccd_buf_alloc(ccd_memory->dev, mem_buff_data->len);
 	ccd_buffer->mem_priv = buf;
@@ -186,7 +171,6 @@ void *mtk_ccd_get_buffer(struct mtk_ccd *ccd,
 		mutex_unlock(&ccd_memory->mmap_lock);
 		return ERR_PTR(-ENOMEM);
 	}
-
 	va = mtk_ccd_buf_get_vaddr(buf);
 	da = mtk_ccd_buf_get_daddr(buf);
 	mem_buff_data->iova = da;
@@ -199,11 +183,9 @@ void *mtk_ccd_get_buffer(struct mtk_ccd *ccd,
 		 mem_buff_data->va,
 		 (unsigned int)ccd_buffer->size,
 		 ccd_buffer->mem_priv);
-
 	return ccd_buffer->mem_priv;
 }
 EXPORT_SYMBOL_GPL(mtk_ccd_get_buffer);
-
 int mtk_ccd_put_buffer(struct mtk_ccd *ccd,
 			struct mem_obj *mem_buff_data)
 {
@@ -214,7 +196,6 @@ int mtk_ccd_put_buffer(struct mtk_ccd *ccd,
 	struct mtk_ccd_mem *ccd_buffer;
 	unsigned int buffer, num_buffers, last_buffer;
 	struct mtk_ccd_memory *ccd_memory = ccd->ccd_memory;
-
 	mutex_lock(&ccd_memory->mmap_lock);
 	num_buffers = ccd_memory->num_buffers;
 	if (num_buffers != 0U) {
@@ -244,40 +225,33 @@ int mtk_ccd_put_buffer(struct mtk_ccd *ccd,
 		}
 	}
 	mutex_unlock(&ccd_memory->mmap_lock);
-
 	if (ret != 0)
 		dev_info(ccd_memory->dev,
 			"Can not free memory va %p iova %pad len %u!\n",
 			 mem_buff_data->va, &mem_buff_data->iova,
 			 mem_buff_data->len);
-
 	return ret;
 }
 EXPORT_SYMBOL_GPL(mtk_ccd_put_buffer);
-
 int mtk_ccd_get_buffer_fd(struct mtk_ccd *ccd, void *mem_priv)
 {
 	struct mtk_ccd_buf *buf = mem_priv;
-	struct dma_buf *dbuf;
 	int fd;
 
 	if (ccd == NULL || buf == NULL) {
 		pr_info("mtk_ccd or buf is NULL\n");
 		return -EINVAL;
 	}
-
 	fd = dma_buf_fd(buf->dbuf,  O_RDWR | O_CLOEXEC);
 	if (fd < 0) {
 		pr_info("mtk_ccd couldn't get fd from dma_buf\n");
 		return -EINVAL;
 	}
-
 	/* increase file count */
-	dbuf = dma_buf_get(fd);
+	get_dma_buf(buf->dbuf);
 	return fd;
 }
 EXPORT_SYMBOL_GPL(mtk_ccd_get_buffer_fd);
-
 int mtk_ccd_put_fd(struct mtk_ccd *ccd,
 			struct mem_obj *mem_buff_data,
 			unsigned int target_fd)
@@ -289,12 +263,10 @@ int mtk_ccd_put_fd(struct mtk_ccd *ccd,
 	struct mtk_ccd_mem *ccd_buffer = NULL;
 	struct mtk_ccd_memory *ccd_memory = ccd->ccd_memory;
 	num_buffers = ccd_memory->num_buffers;
-
 	if (unlikely(!num_buffers)) {
 		dev_info(ccd_memory->dev, "buffer queue is empty");
 		return -EINVAL;
 	}
-
 	for (i = 0; i < num_buffers; i++) {
 		ccd_buffer = &ccd_memory->bufs[i];
 		buf = (struct mtk_ccd_buf *)ccd_buffer->mem_priv;
@@ -304,19 +276,19 @@ int mtk_ccd_put_fd(struct mtk_ccd *ccd,
 			mem_buff_data->len == ccd_buffer->size) {
 			if (atomic_long_read(&buf->dbuf->file->f_count) > 1 &&
 			    current->files)
-				/* decrease file count */
-				dma_heap_buffer_free(buf->dbuf);
+				close_fd(target_fd);
 			else
 				dev_info(ccd_memory->dev,
 						 "%s user space signal exit to close fd already",
 						 __func__);
+			/* decrease file count */
+			dma_heap_buffer_free(buf->dbuf);
 			dev_dbg(ccd_memory->dev,
 					"put dma buf : %d, iova = %p, va = %p, fd = %d",
 					i, &mem_buff_data->iova, mem_buff_data->va, target_fd);
 			break;
 		}
 	}
-
 	if (unlikely(i == num_buffers)) {
 		dev_info(ccd_memory->dev,
 				"mismatch dma buf iova = %p, va = %p",
@@ -325,7 +297,6 @@ int mtk_ccd_put_fd(struct mtk_ccd *ccd,
 	}
 	return 0;
 }
-
 int mtk_ccd_put_buffer_fd(struct mtk_ccd *ccd,
 				struct mem_obj *mem_buff_data,
 				unsigned int target_fd)
@@ -337,6 +308,5 @@ int mtk_ccd_put_buffer_fd(struct mtk_ccd *ccd,
 	return mtk_ccd_put_fd(ccd, mem_buff_data, target_fd);
 }
 EXPORT_SYMBOL_GPL(mtk_ccd_put_buffer_fd);
-
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("MediaTek ccd memory interface");
