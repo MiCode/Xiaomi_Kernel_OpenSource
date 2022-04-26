01cf49ff18b51f64bce5d0f905da4b54c692cf85
Harshitha Sai Neelati <quic_hsaineel@quicinc.com>
2022-04-26 17:37:10 +0530

    msm: kgsl: Add compatible string for A662
    Add compatible string for A662 GPU.
    
    Change-Id: Ida4b3a9c672a99071160c082ea9633992ef4e8f8
    Signed-off-by: Harshitha Sai Neelati <quic_hsaineel@quicinc.com>

--- drivers/gpu/msm/adreno-gpulist.h
+++ drivers/gpu/msm/adreno-gpulist.h
@@ -1748,7 +1748,8 @@ static const struct adreno_a6xx_core adreno_gpu_core_a635 = {
 
 static const struct adreno_a6xx_core adreno_gpu_core_a662 = {
 	.base = {
-		DEFINE_ADRENO_REV(ADRENO_REV_A662, 6, 6, 2, ANY_ID),
+		DEFINE_ADRENO_REV(ADRENO_REV_A662, ANY_ID, ANY_ID, ANY_ID, ANY_ID),
+		.compatible = "qcom,adreno-gpu-a662",
 		.features = ADRENO_APRIV | ADRENO_IOCOHERENT |
 			ADRENO_CONTENT_PROTECTION | ADRENO_PREEMPTION |
 			ADRENO_IFPC | ADRENO_BCL | ADRENO_ACD,
