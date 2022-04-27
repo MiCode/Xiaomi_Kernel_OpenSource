0a326aff8577574d8629f8f1869f91edc0f92971
Sreekanth Gande <quic_sgande@quicinc.com>
2022-04-27 14:20:55 +0530

    soc: qcom: stop timer on active usecase in adspsleepmon
    adspsleepmon module monitors LPM/LPI violations on ADSP based on
    usecase activity information from Audio clients. Stop monitoring
    if there is an active non-LPI usecase.
    
    Change-Id: I19d2210159b5704efe96261c0cdf9f088d5cb19d
    Signed-off-by: Sreekanth Gande <quic_sgande@quicinc.com>

--- drivers/soc/qcom/adsp_sleepmon.c
+++ drivers/soc/qcom/adsp_sleepmon.c
@@ -799,12 +799,7 @@ static int adspsleepmon_device_release(struct inode *inode, struct file *fp)
 		 *   Stop -> An active session
 		 */
 		if (num_sessions != g_adspsleepmon.audio_stats.num_sessions) {
-			if (num_sessions &&
-				!g_adspsleepmon.audio_stats.num_sessions &&
-				(num_sessions != num_lpi_sessions)) {
-				del_timer(&adspsleep_timer);
-				g_adspsleepmon.timer_pending = false;
-			} else if (!num_sessions ||
+			if (!num_sessions ||
 					(num_sessions == num_lpi_sessions)) {
 
 				if (!num_sessions) {
@@ -825,6 +820,9 @@ static int adspsleepmon_device_release(struct inode *inode, struct file *fp)
 
 				mod_timer(&adspsleep_timer, jiffies + delay * HZ);
 				g_adspsleepmon.timer_pending = true;
+			} else if (g_adspsleepmon.timer_pending) {
+				del_timer(&adspsleep_timer);
+				g_adspsleepmon.timer_pending = false;
 			}
 
 			g_adspsleepmon.audio_stats.num_sessions = num_sessions;
@@ -1010,12 +1008,7 @@ static long adspsleepmon_device_ioctl(struct file *file,
 		 *				   active session)
 		 *   Stop -> An active session
 		 */
-		if (num_sessions &&
-			!g_adspsleepmon.audio_stats.num_sessions &&
-			(num_sessions != num_lpi_sessions)) {
-			del_timer(&adspsleep_timer);
-			g_adspsleepmon.timer_pending = false;
-		} else if (!num_sessions ||
+		if (!num_sessions ||
 				(num_sessions == num_lpi_sessions)) {
 
 			if (!num_sessions) {
@@ -1036,6 +1029,9 @@ static long adspsleepmon_device_ioctl(struct file *file,
 
 			mod_timer(&adspsleep_timer, jiffies + delay * HZ);
 			g_adspsleepmon.timer_pending = true;
+		} else if (g_adspsleepmon.timer_pending) {
+			del_timer(&adspsleep_timer);
+			g_adspsleepmon.timer_pending = false;
 		}
 
 		g_adspsleepmon.audio_stats.num_sessions = num_sessions;
