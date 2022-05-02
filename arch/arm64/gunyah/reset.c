c656d628b2b6fcdc74082413d433c95dbdb4d7f6
Elliot Berman <quic_eberman@quicinc.com>
2022-05-02 16:09:22 -0700

    virt: gunyah: rm_core: Validate notifications using payload size only
    Preparation for cleaning up the resource manager core.
    
    Notification size validation needs to only be performed on the payload
    size. The RM RPC header will always be present, else the RM driver would
    not have been able to determine that the message is a notification.
    Thus, considering the RPC header is not required.
    
    Change-Id: Id6a561a77d693e4f8f307d9d0d96b8d173940c60
    Signed-off-by: Elliot Berman <quic_eberman@quicinc.com>

--- arch/arm64/gunyah/reset.c
+++ arch/arm64/gunyah/reset.c
@@ -18,19 +18,15 @@
  * returned, 0 otherwise and also provides a generic reason for exit
  * which can be used by drivers.
  */
-int gh_arch_validate_vm_exited_notif(size_t buff_size, size_t hdr_size,
+int gh_arch_validate_vm_exited_notif(size_t payload_size,
 	struct gh_rm_notif_vm_exited_payload *vm_exited_payload)
 {
-	size_t min_buf_sz = hdr_size | sizeof(*vm_exited_payload);
-
 	switch (vm_exited_payload->exit_type) {
 	case GH_RM_VM_EXIT_TYPE_PSCI_SYSTEM_RESET2:
-		if ((vm_exited_payload->exit_reason_size !=
-				MAX_EXIT_REASON_SIZE) ||
-				(buff_size != min_buf_sz +
-			sizeof(struct gh_vm_exit_reason_psci_sys_reset2))) {
+		if (payload_size !=
+		    sizeof(*vm_exited_payload) + sizeof(struct gh_vm_exit_reason_psci_sys_reset2)) {
 			pr_err("%s: Invalid size for type PSCI_SYSTEM_RESET2: %u\n",
-			__func__, buff_size - hdr_size);
+			__func__, payload_size);
 			return -EINVAL;
 		}
 		vm_exited_payload->exit_type = GH_RM_VM_EXIT_TYPE_SYSTEM_RESET;
