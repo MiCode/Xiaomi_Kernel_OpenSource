9fa99dcd68fed830d8201276e345912e7f617e3f
Anthony Huang <anthony.huang@mediatek.com>
2022-05-03 18:28:24 +0800

    [ALPS07137427] dt-bindings: clk: mt6983: modify mmdvfs clk header
    1. Remove redundant definition
    2. Modify CLK naming
    
    MTK-Commit-Id: bccc0c8fe0f6b258c163c896dd044f14ad5a293a
    
    Signed-off-by: Anthony Huang <anthony.huang@mediatek.com>
    CR-Id: ALPS07137427
    Feature: mmdvfs
    Change-Id: If8d534ebea66949a6a2e96a4b71eed762886731e

--- include/dt-bindings/clock/mt6983-mmdvfs-clk.h
+++ include/dt-bindings/clock/mt6983-mmdvfs-clk.h
@@ -8,31 +8,15 @@
 #define _DT_BINDINGS_MMDVFS_CLK_MT6983_H
 
 /* For clock consumer */
-#define CLK_MMDVFS_DISP0	(0)
-#define CLK_MMDVFS_DISP1	(1)
-#define CLK_MMDVFS_MDP0		(2)
-#define CLK_MMDVFS_MDP1		(3)
-#define CLK_MMDVFS_CAM		(4)
-#define CLK_MMDVFS_SENIF	(5)
-#define CLK_MMDVFS_IMG		(6)
-#define CLK_MMDVFS_MMINFRA	(7)
+#define CLK_MMDVFS_DISP		(0)
+#define CLK_MMDVFS_MDP		(1)
+#define CLK_MMDVFS_MML		(2)
+#define CLK_MMDVFS_CAM		(3)
+#define CLK_MMDVFS_SENIF	(4)
+#define CLK_MMDVFS_IMG		(5)
+#define CLK_MMDVFS_SMI_COMMON0	(6)
+#define CLK_MMDVFS_SMI_COMMON1	(7)
 #define CLK_MMDVFS_VFMT		(8)
 #define CLK_MMDVFS_JPEGENC	(9)
-#define CLK_MMDVFS_NR		(10)
-
-/* For clock provider */
-#define PWR_VCORE	(0)
-#define PWR_OTHER	(1)
-
-#define USER_MMDVFS_DISP0	(0)
-#define USER_MMDVFS_DISP1	(1)
-#define USER_MMDVFS_MDP0	(2)
-#define USER_MMDVFS_MDP1	(3)
-#define USER_MMDVFS_CAM		(4)
-#define USER_MMDVFS_SENIF	(5)
-#define USER_MMDVFS_IMG		(6)
-#define USER_MMDVFS_MMINFRA	(7)
-#define USER_MMDVFS_VFMT	(8)
-#define USER_MMDVFS_JPEGENC	(9)
 
 #endif /* _DT_BINDINGS_MMDVFS_CLK_MT6983_H */
