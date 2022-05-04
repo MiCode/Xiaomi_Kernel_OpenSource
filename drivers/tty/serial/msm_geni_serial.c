tonzy-su/android_kernel_oppo_sm8150

serial: msm_gen_serial: Balance the usage count for UART GSI
Due to the start tx is called multiple time, the usage count
is incremented everytime as part of start tx. so this will
balance out the power vote put in during start_tx.

Change-Id: I038ea1bea17dc8dcd2926b20b41953e1cf43a04e
Signed-off-by: Aniket Randive <arandive@codeaurora.org>

drivers/tty/serial/msm_geni_serial.c
@@ -1258,9 +1258,23 @@ static void msm_geni_uart_gsi_tx_cb(void *ptr)
 	msm_port->tx_dma = (dma_addr_t)NULL;
 	msm_port->xmit_size = 0;
 	complete(&msm_port->tx_xfer);
-	queue_work(msm_port->tx_wq, &msm_port->tx_xfer_work);
+	if (!uart_circ_empty(xmit)) {
+		queue_work(msm_port->tx_wq, &msm_port->tx_xfer_work);
+		IPC_LOG_MSG(msm_port->ipc_log_misc, "%s: TX_xfer is Queued\n",
+				__func__);
+	} else {
+		/*
+		 * This will balance out the power vote put in during start_tx
+		 * allowing the device to suspend.
+		 */
+		if (!uart_console(uport)) {
+			IPC_LOG_MSG(msm_port->ipc_log_misc,
+				"%s.Tx sent out, Power off\n", __func__);
+			msm_geni_serial_power_off(uport);
+		}
+		uart_write_wakeup(uport);
+	}
 	IPC_LOG_MSG(msm_port->ipc_log_misc, "%s:End\n", __func__);
-
 }
 
 static void msm_geni_uart_rx_queue_dma_tre(int index, struct uart_port *uport)

MiCode/Xiaomi_Kernel_OpenSource

drivers/tty/serial/msm_geni_serial.c
@@ -1502,9 +1502,25 @@ static void msm_geni_uart_gsi_tx_cb(void *ptr)
 	msm_port->tx_dma = (dma_addr_t)NULL;
 	msm_port->xmit_size = 0;
 	complete(&msm_port->tx_xfer);
-	queue_work(msm_port->tx_wq, &msm_port->tx_xfer_work);
-	UART_LOG_DBG(msm_port->ipc_log_misc, msm_port->uport.dev,
-		     "%s: End\n", __func__);
+	if (!uart_circ_empty(xmit)) {
+		queue_work(msm_port->tx_wq, &msm_port->tx_xfer_work);
+		UART_LOG_DBG(msm_port->ipc_log_misc, msm_port->uport.dev,
+			     "%s: End\n", __func__);
+	} else {
+		/*
+		 * This will balance out the power vote put in during start_tx
+		 * allowing the device to suspend.
+		 */
+		if (!uart_console(uport)) {
+			UART_LOG_DBG(msm_port->ipc_log_misc,
+				     msm_port->uport.dev,
+				     "%s.Tx sent out, Power off\n", __func__);
+			msm_geni_serial_power_off(uport);
+		}
+		uart_write_wakeup(uport);
+	}
+	UART_LOG_DBG(msm_port->ipc_log_misc, msm_port->uport.dev, "%s:End\n",
+		     __func__);
 }
 
 static void msm_geni_uart_rx_queue_dma_tre(int index, struct uart_port *uport)