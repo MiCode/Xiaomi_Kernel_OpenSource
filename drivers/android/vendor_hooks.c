596dcf7986b7fa54141b5aea912db6e162f417fd
kamasali <quic_kamasali@quicinc.com>
2022-05-05 16:52:04 +0530

    ANDROID: vendor_hooks: Add hook in wakeup functionality
    Need this verndor hook to support wakeup functionality
    for sdio cards.
    
    Bug: 231668342
    Change-Id: I1d1a34d6d02f4558edf56eccdd09df57d6232c05
    Signed-off-by: Sarthak Garg <sartgarg@codeaurora.org>
    Signed-off-by: Kamasali Satyanarayan <quic_kamasali@quicinc.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -316,3 +316,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_blk_rq_ctx_init);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mmap_region);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_try_to_unmap_one);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_shrink_node_memcgs);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mmc_sdio_pm_flag_set);
