3fe8fe5832d241814d11779c53063301a61ce582
freddy.hsin <freddy.hsin@mediatek.com>
2022-05-05 16:45:08 +0800

    [ALPS07188583] wdt: Fix coverity CR
    Fix coverity CR
    
    MTK-Commit-Id: 89471b12f7c40e769a77a159aaa08dce3099e411
    
    Signed-off-by: freddy.hsin <freddy.hsin@mediatek.com>
    CR-Id: ALPS07188583
    Feature: RGU WDT Watchdog
    Change-Id: I50e41feddbc030fbf42f78004b32e49da15589e4

--- drivers/misc/mediatek/aee/hangdet/aee_hangdet.c
+++ drivers/misc/mediatek/aee/hangdet/aee_hangdet.c
@@ -268,22 +268,22 @@ void kicker_cpu_bind(int cpu)
 void wk_cpu_update_bit_flag(int cpu, int plug_status, int set_check)
 {
 	if (plug_status == 1) {	/* plug on */
-		spin_lock(&lock);
+		spin_lock_bh(&lock);
 		if (set_check)
 			cpus_kick_bit |= (1 << cpu);
 		lasthpg_cpu = cpu;
 		lasthpg_act = plug_status;
 		lasthpg_t = sched_clock();
-		spin_unlock(&lock);
+		spin_unlock_bh(&lock);
 	}
 	if (plug_status == 0) {	/* plug off */
-		spin_lock(&lock);
+		spin_lock_bh(&lock);
 		cpus_kick_bit &= (~(1 << cpu));
 		lasthpg_cpu = cpu;
 		lasthpg_act = plug_status;
 		lasthpg_t = sched_clock();
 		wk_tsk_bind[cpu] = 0;
-		spin_unlock(&lock);
+		spin_unlock_bh(&lock);
 	}
 }
 
@@ -305,7 +305,7 @@ static void kwdt_time_sync(void)
 	ktime_get_real_ts64(&tv);
 	tv_android = tv;
 	rtc_time64_to_tm(tv.tv_sec, &tm);
-	tv_android.tv_sec -= sys_tz.tz_minuteswest * 60;
+	tv_android.tv_sec -= (uint64_t)sys_tz.tz_minuteswest * 60;
 	rtc_time64_to_tm(tv_android.tv_sec, &tm_android);
 	pr_info("[thread:%d] %d-%02d-%02d %02d:%02d:%02d.%u UTC;"
 		"android time %d-%02d-%02d %02d:%02d:%02d.%03d\n",
@@ -361,6 +361,13 @@ static void aee_dump_timer_func(struct timer_list *t)
 {
 	spin_lock(&lock);
 
+	if (sched_clock() - aee_dump_timer_t < CHG_TMO_DLY_SEC * 1000000000ULL) {
+		g_change_tmo = 0;
+		aee_dump_timer_t = 0;
+		spin_unlock(&lock);
+		return;
+	}
+
 	if ((sched_clock() > all_k_timer_t) &&
 	    (sched_clock() - all_k_timer_t) < (CHG_TMO_DLY_SEC + 1) * 1000000000ULL) {
 		g_change_tmo = 0;
@@ -409,7 +416,7 @@ static void kwdt_process_kick(int local_bit, int cpu,
 			aee_dump_timer_c = 1;
 			snprintf(msg_buf, WK_MAX_MSG_SIZE, "wdtk-et %s %d cpu=%d o_k=%d\n",
 				  __func__, __LINE__, cpu, original_kicker);
-			spin_unlock(&lock);
+			spin_unlock_bh(&lock);
 			pr_info("%s", msg_buf);
 			kwdt_dump_func();
 			return;
@@ -419,7 +426,7 @@ static void kwdt_process_kick(int local_bit, int cpu,
 			"all wdtk was already stopped cpu=%d o_k=%d\n",
 			cpu, original_kicker);
 
-		spin_unlock(&lock);
+		spin_unlock_bh(&lock);
 		pr_info("%s", msg_buf);
 		return;
 	}
@@ -498,7 +505,7 @@ static void kwdt_process_kick(int local_bit, int cpu,
 		cpus_skip_bit |= (1 << cpu);
 	}
 
-	spin_unlock(&lock);
+	spin_unlock_bh(&lock);
 
 	pr_info("%s", msg_buf);
 
@@ -509,44 +516,36 @@ static void kwdt_process_kick(int local_bit, int cpu,
 			mt_irq_dump_status(systimer_irq);
 #endif
 		dump_wdk_bind_info();
-#ifdef CONFIG_MTK_SCHED_EXTENSION
-		sysrq_sched_debug_show_at_AEE();
-#endif
 
 		if (systimer_base)
-#if IS_ENABLED(CONFIG_MTK_TICK_BROADCAST_DEBUG)
-			pr_info("SYST0 CON%x VAL%x affin time %lld\n",
-				ioread32(systimer_base + SYST0_CON),
-				ioread32(systimer_base + SYST0_VAL));
-#else
 			pr_info("SYST0 CON%x VAL%x\n",
 				ioread32(systimer_base + SYST0_CON),
 				ioread32(systimer_base + SYST0_VAL));
-#endif
 #if CHG_TMO_EN
 		if (toprgu_base) {
-			spin_lock(&lock);
+			spin_lock_bh(&lock);
 			g_change_tmo = 1;
-			spin_unlock(&lock);
+			spin_unlock_bh(&lock);
 			iowrite32((WDT_LENGTH_TIMEOUT(6) << 6) | WDT_LENGTH_KEY,
 				toprgu_base + WDT_LENGTH);
 			iowrite32(WDT_RST_RELOAD, toprgu_base + WDT_RST);
 		}
 #endif
+		/* abort suspend when wdt kick */
 		if (dump_timeout == 2)
 			pm_system_wakeup();
 		else {
-			spin_lock(&lock);
+			spin_lock_bh(&lock);
 			if (g_hang_detected && !aee_dump_timer_t) {
 				pm_system_wakeup();
 				aee_dump_timer_t = sched_clock();
 				g_change_tmo = 1;
-				spin_unlock(&lock);
+				spin_unlock_bh(&lock);
 				aee_dump_timer.expires = jiffies + CHG_TMO_DLY_SEC * HZ;
 				add_timer(&aee_dump_timer);
 				return;
 			}
-			spin_unlock(&lock);
+			spin_unlock_bh(&lock);
 		}
 	}
 }
@@ -573,7 +572,7 @@ static int kwdt_thread(void *arg)
 		 * loc_wk_wdt ,loc_wk_wdt->ready);
 		 */
 		curInterval = g_kinterval*1000*1000;
-		spin_lock(&lock);
+		spin_lock_bh(&lock);
 		/* smp_processor_id does not
 		 * allowed preemptible context
 		 */
@@ -596,7 +595,7 @@ static int kwdt_thread(void *arg)
 			kwdt_process_kick(local_bit, cpu, curInterval,
 				msg_buf, *((unsigned int *)arg));
 		} else {
-			spin_unlock(&lock);
+			spin_unlock_bh(&lock);
 		}
 
 		usleep_range(curInterval, curInterval + SOFT_KICK_RANGE);
@@ -723,11 +722,11 @@ static int wdt_pm_notify(struct notifier_block *notify_block,
 		lastsuspend_t = sched_clock();
 		lastsuspend_syst = cnt;
 
-		spin_lock(&lock);
+		spin_lock_bh(&lock);
 		del_timer_sync(&aee_dump_timer);
 		aee_dump_timer_t = 0;
 		g_hang_detected = 0;
-		spin_unlock(&lock);
+		spin_unlock_bh(&lock);
 		break;
 
 	case PM_POST_SUSPEND:
