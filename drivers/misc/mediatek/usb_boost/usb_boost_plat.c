0ebed5b5d6903f44f85ed92db81762f4f183c177
chihhao.chen <chihhao.chen@mediatek.com>
2022-05-05 16:50:45 +0800

    [ALPS07177836] USB: enable USB Audio Boost
    [Detail]
    Enable USB Audio boost
    
    MTK-Commit-Id: 5bec9282d9915d7bddde35351838e6da45af3cf5
    
    Signed-off-by: chihhao.chen <chihhao.chen@mediatek.com>
    CR-Id: ALPS07177836
    Feature: USB 3.0 Device
    Change-Id: I21e7a838a503e6080dfb133bae22fdd1c3f92754

--- drivers/misc/mediatek/usb_boost/usb_boost_plat.c
+++ drivers/misc/mediatek/usb_boost/usb_boost_plat.c
@@ -262,7 +262,8 @@ int audio_freq_hold(void)
 		}
 	}
 
-	if (of_device_is_compatible(np, "mediatek,mt6855-usb_boost")) {
+	if (of_device_is_compatible(np, "mediatek,mt6855-usb_boost") ||
+		of_device_is_compatible(np, "mediatek,mt6789-usb_boost")) {
 		device_property_read_u32(gdev, "small-core", &(cpu_freq_audio[0]));
 		device_property_read_u32(gdev, "big-core", &(cpu_freq_audio[1]));
 
@@ -311,7 +312,8 @@ int audio_core_hold(void)
 	 */
 	if (of_device_is_compatible(np, "mediatek,mt6983-usb_boost") ||
 		of_device_is_compatible(np, "mediatek,mt6895-usb_boost") ||
-		of_device_is_compatible(np, "mediatek,mt6855-usb_boost")) {
+		of_device_is_compatible(np, "mediatek,mt6855-usb_boost") ||
+		of_device_is_compatible(np, "mediatek,mt6789-usb_boost")) {
 		USB_BOOST_NOTICE("\n");
 		cpu_latency_qos_update_request(&pm_qos_req, 50);
 	}
@@ -326,7 +328,8 @@ int audio_core_release(void)
 	/*Enable MCDI*/
 	if (of_device_is_compatible(np, "mediatek,mt6983-usb_boost") ||
 		of_device_is_compatible(np, "mediatek,mt6895-usb_boost") ||
-		of_device_is_compatible(np, "mediatek,mt6855-usb_boost")) {
+		of_device_is_compatible(np, "mediatek,mt6855-usb_boost") ||
+		of_device_is_compatible(np, "mediatek,mt6789-usb_boost")) {
 		USB_BOOST_NOTICE("\n");
 		cpu_latency_qos_update_request(&pm_qos_req,
 			PM_QOS_DEFAULT_VALUE);
