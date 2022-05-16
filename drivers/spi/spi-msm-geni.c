7a9d138b7fd5685c5b7c9b32c9357bc017f3141a
Mukesh Kumar Savaliya <quic_msavaliy@quicinc.com>
2022-05-16 18:22:26 +0530

    spi: spi-msm-geni: Increase timeout offset from 5 to 250 msec
    This change enhances SPI timeout offset value from 5 msec 250msec
    similer to i2c driver. This works fine as long as the system is fine
    in the sense no system level latency and all interrupts are delivered
    in time. But in some cases, due to other system load, we may face
    timeout issue if interrupt is delayed for more than the expected timeout.
    
    Hence settle the offset to little higher value, as 5 msec is very less
    in regular scenario also. This change fixes the timeout issues observed
    by reported issues on field.
    
    Change-Id: Ie636262bcd2fb81b5baab0d8be5562fce236692e
    Signed-off-by: Mukesh Kumar Savaliya <quic_msavaliy@quicinc.com>

--- drivers/spi/spi-msm-geni.c
+++ drivers/spi/spi-msm-geni.c
@@ -24,7 +24,7 @@
 #define SPI_NUM_CHIPSELECT	(4)
 #define SPI_XFER_TIMEOUT_MS	(250)
 #define SPI_AUTO_SUSPEND_DELAY	(250)
-#define SPI_XFER_TIMEOUT_OFFSET  (5)
+#define SPI_XFER_TIMEOUT_OFFSET	(250)
 /* SPI SE specific registers */
 #define SE_SPI_CPHA		(0x224)
 #define SE_SPI_LOOPBACK		(0x22C)
