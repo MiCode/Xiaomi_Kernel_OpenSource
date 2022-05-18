126fc176dd055bf4f40b66755f0ec95704eba646
bernie.yu <Bernie.Yu@mediatek.com>
2022-05-18 12:42:37 +0800

    [ALPS07202899] Revert "[ALPS07118232] enable secure normal vr concurrent"
    Revert "[ALPS07118232] enable secure normal vr concurrent"
    
    MTK-Commit-Id: 2f66a34b61b9966f2f58c831ada5fae7986ae0d4
    
    CR-Id: ALPS07202899
    Feature: Wi-Fi Display HDCP
    Signed-off-by: bernie.yu <Bernie.Yu@mediatek.com>
    Change-Id: Ie972f13bf196ee65f0e33e83f9c5e3ef57a6e15a

--- drivers/media/platform/mtk-vcodec/venc/venc_vcu_if.c
+++ drivers/media/platform/mtk-vcodec/venc/venc_vcu_if.c
@@ -118,7 +118,6 @@ int vcu_enc_ipi_handler(void *data, unsigned int len, void *priv)
 	unsigned int core_id;
 	unsigned long flags;
 	struct task_struct *task = NULL;
-	int lock = -1;
 
 	BUILD_BUG_ON(sizeof(struct venc_ap_ipi_msg_init) > SHARE_BUF_SIZE);
 	BUILD_BUG_ON(sizeof(struct venc_ap_ipi_query_cap) > SHARE_BUF_SIZE);
@@ -176,26 +175,13 @@ int vcu_enc_ipi_handler(void *data, unsigned int len, void *priv)
 		break;
 	case VCU_IPIMSG_ENC_POWER_ON:
 		/*use status to store core ID*/
-		vcu_get_gce_lock(vcu->dev, VCU_VENC);
-		while (lock != 0) {
-			lock = venc_lock(ctx, 0, true);
-			if (lock != 0) {
-				vcu_put_gce_lock(vcu->dev, VCU_VENC);
-				usleep_range(1000, 2000);
-				vcu_get_gce_lock(vcu->dev, VCU_VENC);
-			}
-		}
 		venc_encode_prepare(ctx, msg->status, &flags);
-		vcu_put_gce_lock(vcu->dev, VCU_VENC);
 		msg->status = VENC_IPI_MSG_STATUS_OK;
 		ret = 1;
 		break;
 	case VCU_IPIMSG_ENC_POWER_OFF:
-		vcu_get_gce_lock(vcu->dev, VCU_VENC);
 		/*use status to store core ID*/
 		venc_encode_unprepare(ctx, msg->status, &flags);
-		venc_unlock(ctx, 0);
-		vcu_put_gce_lock(vcu->dev, VCU_VENC);
 		msg->status = VENC_IPI_MSG_STATUS_OK;
 		ret = 1;
 		break;
