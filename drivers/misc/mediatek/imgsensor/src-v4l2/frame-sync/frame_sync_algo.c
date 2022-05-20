7515bb4a425ef2ecd36ff40aa816b9bae7f5306c
Nick Hong <nick.hong@mediatek.com>
2022-05-20 13:08:54 +0800

    [ALPS07055759] cam: FS: fix FrameSync coverity
    fix FrameSync coverity issues
    
    MTK-Commit-Id: 833b3ae8462ecb3013de84f91efe7c81b3a92b80
    
    Signed-off-by: Nick Hong <nick.hong@mediatek.com>
    CR-Id: ALPS07055759
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: I99ccac8ac2f58ebb4eb2d6fe6718dc075f3534cd

--- drivers/misc/mediatek/imgsensor/src-v4l2/frame-sync/frame_sync_algo.c
+++ drivers/misc/mediatek/imgsensor/src-v4l2/frame-sync/frame_sync_algo.c
@@ -1510,7 +1510,7 @@ static inline long long fs_alg_sa_calc_adjust_diff_master(
 	f_cell_m = get_valid_frame_cell_size(m_idx);
 
 
-	return ((long long)(p_para_m->stable_fl_us * f_cell_m) - adjust_diff_s);
+	return ((long long)(p_para_m->stable_fl_us) * f_cell_m - adjust_diff_s);
 }
 
 
@@ -1534,7 +1534,7 @@ static long long fs_alg_sa_calc_adjust_diff_slave(
 	if ((fs_inst[s_idx].fl_active_delay != fs_inst[m_idx].fl_active_delay)
 		&& (adjust_diff_s > 0)) {
 		/* if there are the pair, N+2 pred_fl will bigger than N+1 sensor */
-		adjust_diff_s -= (long long)(p_para_s->stable_fl_us * f_cell_s);
+		adjust_diff_s -= (long long)(p_para_s->stable_fl_us) * f_cell_s;
 	}
 
 
@@ -1556,7 +1556,7 @@ static long long fs_alg_sa_calc_adjust_diff_slave(
 			return 0;
 		}
 
-		adjust_diff_s += (long long)(p_para_m->stable_fl_us * f_cell_m);
+		adjust_diff_s += (long long)(p_para_m->stable_fl_us) * f_cell_m;
 	}
 
 
@@ -1668,7 +1668,7 @@ static unsigned int fs_alg_sa_adjust_slave_diff_resolver(
 
 
 	/* check if adjust diff is reasonable */
-	if (adjust_diff_s > (long long)(p_para_m->stable_fl_us * f_cell_m)) {
+	if (adjust_diff_s > (long long)(p_para_m->stable_fl_us) * f_cell_m) {
 		adjust_or_not = 0;
 
 		LOG_MUST(
