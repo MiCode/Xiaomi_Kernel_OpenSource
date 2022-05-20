8338567a5641e0824877129b9411e0f76aa7fa8f
Dong-Hung Lin <dong-hung.lin@mediatek.com>
2022-05-20 13:17:16 +0800

    [ALPS07233199] disp: fix gem create fail flow trigger KE
    fix gem create fail flow trigger KE
    if mtk_drm_gem_create return error ptr
      we dont need to do action, gem has been freed
    
    MTK-Commit-Id: ef002aa646b084ca8bbabb5cea22f7e43b4ff70e
    
    Signed-off-by: Dong-Hung Lin <dong-hung.lin@mediatek.com>
    CR-Id: ALPS07233199
    Feature: display_mobile
    Change-Id: If84027205e35b419f4a254906234e9c3f66c62ef

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_assert.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_assert.c
@@ -530,7 +530,6 @@ void mtk_drm_assert_init(struct drm_device *dev)
 	mtk_gem = mtk_drm_gem_create(dev, size, true);
 	if (IS_ERR(mtk_gem)) {
 		DDPINFO("alloc buffer fail\n");
-		drm_gem_object_release(&mtk_gem->base);
 		return;
 	}
 	//Avoid kmemleak to scan
