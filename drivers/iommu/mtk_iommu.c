151e4f5fc0e514ad66d2574e389e953515649d2e
Yunfei Wang <yf.wang@mediatek.com>
2022-05-20 13:17:26 +0800

    [ALPS07065698] iommu/mediatek: Disallow device connect with multi-larb
    The device that connects with each a larb is a independent HW.
    All the ports in each a device should be in the same larbs.
    Currently we dont have a device connect with 2 larbs at the same time.
    Disallow this case, print the error log.
    
    MTK-Commit-Id: 2f9fff517ed2044b48e34b997529ec40a1bacc91
    
    Signed-off-by: Yunfei Wang <yf.wang@mediatek.com>
    CR-Id: ALPS07065698
    Feature: iommu_kernel
    Change-Id: I80d99367f69b36e473b2180a8c3663e5851031bf

--- drivers/iommu/mtk_iommu.c
+++ drivers/iommu/mtk_iommu.c
@@ -1815,11 +1815,26 @@ static struct iommu_device *mtk_iommu_probe_device(struct device *dev)
 {
 	struct iommu_fwspec *fwspec = dev_iommu_fwspec_get(dev);
 	struct mtk_iommu_data *data;
+	unsigned int larbid, larbidx, i;
 
 	if (!fwspec || fwspec->ops != &mtk_iommu_ops)
 		return ERR_PTR(-ENODEV); /* Not a iommu client device */
 
 	data = dev_iommu_priv_get(dev);
+
+	/*
+	 * The device that connects with each a larb is a independent HW.
+	 * All the ports in each a device should be in the same larbs.
+	 */
+	larbid = MTK_M4U_TO_LARB(fwspec->ids[0]);
+	for (i = 1; i < fwspec->num_ids; i++) {
+		larbidx = MTK_M4U_TO_LARB(fwspec->ids[i]);
+		if (larbid != larbidx) {
+			dev_err(dev, "Can only use one larb. Fail@larb%d-%d.\n",
+				larbid, larbidx);
+			return ERR_PTR(-EINVAL);
+		}
+	}
 
 	return &data->iommu;
 }
