27f31bcf1f1a971c7903b0d64d5e2b4e5656470c
tai-hua.tseng <tai-hua.tseng@mediatek.com>
2022-05-26 23:08:29 +0800

    [ALPS07153401] DISP: fix mmprofile coverity issue
    [Detail]
    fix mmprofile coverity issue
    
    MTK-Commit-Id: ffd24696f2163beb60132f693d266ee5ac0caa38
    
    CR-Id: ALPS07153401
    Feature: display_mobile
    Signed-off-by: tai-hua.tseng <tai-hua.tseng@mediatek.com>
    Change-Id: I6416cfdfac8ac30baa4c5ce5c2e099dcdf46e9b6

--- drivers/misc/mediatek/mmp/src/mmprofile.c
+++ drivers/misc/mediatek/mmp/src/mmprofile.c
@@ -1478,6 +1478,10 @@ static ssize_t mmprofile_dbgfs_start_read(struct file *file, char __user *buf,
 
 	MMP_LOG(ANDROID_LOG_DEBUG, "start=%d", mmprofile_globals.start);
 	r = sprintf(str, "start = %d\n", mmprofile_globals.start);
+	if (r < 0) {
+		pr_err("%s sprintf error:%d\n", __func__, r);
+		return 0;
+	}
 	return simple_read_from_buffer(buf, size, ppos, str, r);
 }
 
@@ -1506,6 +1510,10 @@ static ssize_t mmprofile_dbgfs_enable_read(struct file *file, char __user *buf,
 
 	MMP_LOG(ANDROID_LOG_DEBUG, "enable=%d", mmprofile_globals.enable);
 	r = sprintf(str, "enable = %d\n", mmprofile_globals.enable);
+	if (r < 0) {
+		pr_err("%s sprintf error:%d\n", __func__, r);
+		return 0;
+	}
 	return simple_read_from_buffer(buf, size, ppos, str, r);
 }
 
