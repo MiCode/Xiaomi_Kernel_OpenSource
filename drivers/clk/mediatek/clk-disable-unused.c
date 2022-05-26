9302287f516a1d54dda506d384ad76c776026032
Chong-ming.Wei <chong-ming.wei@mediatek.com>
2022-05-26 23:06:45 +0800

    [ALPS07147670] clk: mediatek: common: fix coverity
    fix coverity issue(Unchecked return value).
    
    MTK-Commit-Id: 7d72f6c230870e5cd419aa51030ed9ceae43b702
    
    Signed-off-by: Chong-ming.Wei <chong-ming.wei@mediatek.com>
    CR-Id: ALPS07147670
    Feature: CLKMGR_Kernel
    Change-Id: I511b2b1ab51ba0c220ccdeb705a4cfa5e7521aff

--- drivers/clk/mediatek/clk-disable-unused.c
+++ drivers/clk/mediatek/clk-disable-unused.c
@@ -24,6 +24,7 @@ static int disable_unused_probe(struct platform_device *pdev)
 	struct clk *clk;
 	int clk_con, i = 0;
 	int pp1, en1, pp2, en2;
+	int retval = 0;
 
 	clk_con = of_count_phandle_with_args(pdev->dev.of_node, "clocks",
 			"#clock-cells");
@@ -50,15 +51,25 @@ static int disable_unused_probe(struct platform_device *pdev)
 					i, __clk_get_name(clk));
 #if DUMP_UNUSED_CLKS
 			/* enable parent clk first because of clk dependency */
-			clk_prepare_enable(clk_get_parent(clk));
-
+			retval = clk_prepare_enable(clk_get_parent(clk));
+			if (retval) {
+				pr_info("parent clk of clk %d clk_prepare_enable failed\n", i);
+				pr_info("return err code=%d\n", retval);
+				return 0;
+			}
 			/* record clk initial on/off state */
 			en1 = __clk_is_enabled(clk);
 			pp1 = clk_hw_is_prepared(__clk_get_hw(clk));
 #endif
 
 			/* enable then disable clk to turn off unused clks */
-			clk_prepare_enable(clk);
+			retval = clk_prepare_enable(clk);
+			if (retval) {
+				pr_info("clk %d clk_prepare_enable failed\n", i);
+				pr_info("return err code=%d\n", retval);
+				return 0;
+			}
+
 			clk_disable_unprepare(clk);
 
 #if DUMP_UNUSED_CLKS
