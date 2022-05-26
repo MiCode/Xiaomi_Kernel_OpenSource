e3ec95126de7c6fcccd2b8cac78b3cc1821dcaaa
Chong-ming.Wei <chong-ming.wei@mediatek.com>
2022-05-26 23:06:58 +0800

    [ALPS07147085] clk: mediatek: mt6893: fix coverity
    fix coverity issue(Logically dead code).
    
    MTK-Commit-Id: 04ac72aa5513e34b421960b91dfe825f0a6c508f
    
    Signed-off-by: Chong-ming.Wei <chong-ming.wei@mediatek.com>
    CR-Id: ALPS07147085
    Feature: CLKMGR_Kernel
    Change-Id: I169b70489fab9ace4913815dfa6bad514deeef34

--- drivers/clk/mediatek/clkchk-mt6893.c
+++ drivers/clk/mediatek/clkchk-mt6893.c
@@ -474,9 +474,6 @@ void print_subsys_reg_mt6893(enum chk_sys_id id)
 	const struct regname *rns = &rn[0];
 	int i;
 
-	if (rns == NULL)
-		return;
-
 	if (id >= chk_sys_num || id < 0) {
 		pr_info("wrong id:%d\n", id);
 		return;
