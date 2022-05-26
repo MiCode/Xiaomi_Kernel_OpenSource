60de665617111acf5c48a3e7f290d2cf6c195be2
Zhiyong.Tao <zhiyong.tao@mediatek.com>
2022-05-26 23:07:08 +0800

    [ALPS07086698] pmic: auxadc: fix Out-of-bounds read issue
    Fix [Coverity][ALPS]Out-of-bounds read: 10888663
    Test: build pass
    
    MTK-Commit-Id: a63c3d9c3954baf8b9c485ca63571912f5eb3145
    
    Signed-off-by: Zhiyong.Tao <zhiyong.tao@mediatek.com>
    CR-Id: ALPS07086698
    Feature: pmic_driver
    Change-Id: I606fb07e6021f7f607837bd602da4acd957c889a

--- drivers/iio/adc/mt635x-auxadc_v1.c
+++ drivers/iio/adc/mt635x-auxadc_v1.c
@@ -504,7 +504,8 @@ static int auxadc_get_data_from_dt(struct mt635x_auxadc_device *adc_dev,
 	struct device_node *node)
 {
 	struct auxadc_channels *auxadc_chan;
-	unsigned int value = 0, val_arr[2] = {0};
+	unsigned int value = 0;
+	unsigned char val_arr[2] = {0};
 	int ret;
 
 	ret = of_property_read_u32(node, "channel", channel);
@@ -521,7 +522,7 @@ static int auxadc_get_data_from_dt(struct mt635x_auxadc_device *adc_dev,
 	}
 	auxadc_chan = &auxadc_chans[*channel];
 
-	ret = of_property_read_u32_array(node, "resistance-ratio", val_arr, 2);
+	ret = of_property_read_u32_array(node, "resistance-ratio", (u32 *)val_arr, 2);
 	if (!ret) {
 		auxadc_chan->r_ratio[0] = val_arr[0];
 		auxadc_chan->r_ratio[1] = val_arr[1];
