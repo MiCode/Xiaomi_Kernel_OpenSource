eb48f8a8b253d1c82c7158df737d599b38ad7ca2
Peter Wang <peter.wang@mediatek.com>
2022-05-29 02:10:53 +0800

    [ALPS06799076] ufs: mediatek: split ref-clk on/off notify tfa
    Split ref-clk on/off to pre/post change notify tfa
    
    MTK-Commit-Id: 0ee132626ac4123e6199c046397d81f8ed128a1e
    
    Signed-off-by: Peter Wang <peter.wang@mediatek.com>
    CR-Id: ALPS06799076
    Feature: ufs
    Change-Id: I4ab7943e7af0e5276edafc28d93d287d79c1247a

--- drivers/scsi/ufs/ufs-mediatek-sip.h
+++ drivers/scsi/ufs/ufs-mediatek-sip.h
@@ -80,8 +80,8 @@ enum {
 #define ufs_mtk_crypto_ctrl(res, enable) \
 	ufs_mtk_smc(UFS_MTK_SIP_CRYPTO_CTRL, res, enable)
 
-#define ufs_mtk_ref_clk_notify(on, res) \
-	ufs_mtk_smc(UFS_MTK_SIP_REF_CLK_NOTIFICATION, res, on)
+#define ufs_mtk_ref_clk_notify(on, stage, res) \
+	ufs_mtk_smc(UFS_MTK_SIP_REF_CLK_NOTIFICATION, res, on, stage)
 
 #define ufs_mtk_device_reset_ctrl(high, res) \
 	ufs_mtk_smc(UFS_MTK_SIP_DEVICE_RESET, res, high)
