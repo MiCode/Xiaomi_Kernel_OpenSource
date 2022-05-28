789e54a7602f1b18b443ac20ceed88f58947a000
Auto Li <auto.li@mediatek.com>
2022-05-29 02:12:33 +0800

    [ALPS07208004] cam: Update imx766 pwr seq
    Update imx766 pwr seq
    
    MTK-Commit-Id: 2b8dd014058eb73196d1d7188b8ce5523931d293
    
    Signed-off-by: Auto Li <auto.li@mediatek.com>
    CR-Id: ALPS07208004
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: I6677a90809d2e5fd3e902f2c04f884ff632eac32

--- drivers/misc/mediatek/imgsensor/src-v4l2/common/imx766_mipi_raw/imx766mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src-v4l2/common/imx766_mipi_raw/imx766mipiraw_Sensor.c
@@ -5348,9 +5348,9 @@ static struct subdrv_pw_seq_entry pw_seq[] = {
 	{HW_ID_MCLK, 24, 0},
 	{HW_ID_PDN, 0, 0},
 	{HW_ID_RST, 0, 1},
-	{HW_ID_AVDD, 2800000, 3},
+	{HW_ID_AVDD, 1800000, 3}, // power 1.8V to enable 2.8V
 	{HW_ID_AVDD1, 1800000, 3},
-	{HW_ID_AFVDD, 2800000, 3},
+	{HW_ID_AFVDD, 1800000, 3}, // power 1.8V to enable 2.8V
 	{HW_ID_DVDD, 1100000, 4},
 	{HW_ID_DOVDD, 1800000, 1},
 	{HW_ID_MCLK_DRIVING_CURRENT, 4, 6},
--- drivers/misc/mediatek/imgsensor/src-v4l2/common/imx766dual_mipi_raw/imx766dualmipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src-v4l2/common/imx766dual_mipi_raw/imx766dualmipiraw_Sensor.c
@@ -5337,7 +5337,7 @@ static struct subdrv_pw_seq_entry pw_seq[] = {
 	{HW_ID_AVDD, 2800000, 3},
 	{HW_ID_AVDD1, 1800000, 3},
 	{HW_ID_AFVDD, 2800000, 3},
-	{HW_ID_DVDD, 1100000, 4},
+	{HW_ID_DVDD, 1090000, 4},
 	{HW_ID_DOVDD, 1800000, 1},
 	{HW_ID_MCLK_DRIVING_CURRENT, 4, 6},
 	{HW_ID_PDN, 1, 0},
