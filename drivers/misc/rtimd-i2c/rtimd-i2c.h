aaa522d77b95ad6ba39480ed1cd7735dc1fb40ca
Kevin ko <kevin.ko@raon-tech.com>
2022-06-02 14:34:45 +0530

    drivers: misc: rtimd-i2c: v.2.3.4
    v.2.3.4 from vendor.
    
    Change-Id: Id0059cce6b2f5ecb59f45b75c9583200128fdb47
    Signed-off-by: Kevin ko <kevin.ko@raon-tech.com>
    Git-commit: c36c300266b3b3e84e0cd336ef0f4f87b942c786
    Git-repo: https://github.com/RaontechSoftware/rtimd-i2c
    Signed-off-by: Kashish Jain <quic_kashjain@quicinc.com>
    Signed-off-by: Ritesh Kumar <quic_riteshk@quicinc.com>

---
+++ drivers/misc/rtimd-i2c/rtimd-i2c.h
@@ -0,0 +1,127 @@
+/*
+ * File name: rtimd-i2c.h
+ *
+ * Description : RAONTECH Micro Display I2C driver.
+ *
+ * Copyright (C) (2017, RAONTECH)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __RTIMD_I2C_H__
+#define __RTIMD_I2C_H__
+
+#include <linux/types.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/errno.h>
+#include <linux/fcntl.h>
+#include <linux/miscdevice.h>
+#include <linux/interrupt.h>
+#include <linux/ioctl.h>
+#include <linux/delay.h>
+#include <linux/vmalloc.h>
+#include <linux/uaccess.h>
+#include <linux/io.h>
+#include <linux/atomic.h>
+#include <linux/i2c.h>
+#include <linux/list.h> 
+#include <linux/jiffies.h>
+#include <linux/platform_device.h>
+#include <linux/version.h>
+#include <linux/delay.h>
+#include <asm/uaccess.h>
+#include <linux/slab.h>
+#include <linux/mutex.h>
+#include <linux/gpio.h>
+
+#define RMDERR(fmt, args...) \
+	pr_err("RTI_MD: %s(): " fmt, __func__, ## args)
+
+// pr_debug was not output msg......
+#define RMDDBG(fmt, args...) \
+	pr_err("RTI_MD: %s(): " fmt, __func__, ## args)
+
+
+/* device file name */
+#define RTI_MD_DEV_NAME		"rtimd-i2c"
+
+/* device number */
+#define RTI_MD_MAJOR_NR         240       /* MAJOR No. */
+#define RTI_MD_MINOR_NR         200       /* MINOR No. */
+
+#define MAX_RTIMD_REG_DATA_SIZE		PAGE_SIZE /* 4KB Kernel page size */
+
+/* RDC Control Block */
+typedef struct {
+	atomic_t open_flag; /* to open only once */
+
+	uint8_t	*read_buf;
+	uint8_t	*write_buf;
+
+	int bus_num;
+	struct i2c_adapter *adap;
+} RTIMD_CB_T;
+
+#define RTIMD_SINGLE_IO_MODE	1
+#define RTIMD_BURST_IO_MODE		0
+
+/*
+ * NOTE: Force align to 64-bit to compat 32-bit application.
+ */
+typedef struct {
+	uint32_t reg_addr;
+	uint8_t bus_num;
+	uint8_t slave_addr;
+	uint8_t reg_size;
+	uint8_t data;
+} RTIMD_SINGLE_WRITE_REG_T;
+
+typedef struct {
+	uint64_t wbuf_addr;
+
+	uint8_t bus_num;
+	uint8_t slave_addr;
+	uint16_t wsize;
+	uint32_t pad;
+} RTIMD_BURST_WRITE_REG_T;
+
+typedef struct {
+	uint64_t rbuf_addr;
+
+	uint32_t reg_addr;
+	uint8_t bus_num;
+	uint8_t slave_addr;
+	uint8_t reg_size;
+	uint8_t pad;
+} RTIMD_SINGLE_READ_REG_T;
+
+typedef struct {
+	uint64_t wbuf_addr;
+	uint64_t rbuf_addr;
+
+	uint16_t wsize;
+	uint16_t rsize;
+	uint8_t bus_num;
+	uint8_t slave_addr;
+	uint16_t pad;
+} RTIMD_BURST_READ_REG_T;
+
+#define RTIMD_IOC_MAGIC	'R'
+
+#define IOCTL_RTIMD_SINGLE_READ		_IOWR(RTIMD_IOC_MAGIC, 1, RTIMD_SINGLE_READ_REG_T)
+#define IOCTL_RTIMD_BURST_READ		_IOWR(RTIMD_IOC_MAGIC, 2, RTIMD_BURST_READ_REG_T)
+#define IOCTL_RTIMD_SINGLE_WRITE	_IOWR(RTIMD_IOC_MAGIC, 3, RTIMD_SINGLE_WRITE_REG_T)
+#define IOCTL_RTIMD_BURST_WRITE		_IOWR(RTIMD_IOC_MAGIC, 4, RTIMD_BURST_WRITE_REG_T)
+
+#endif /* __RTIMD_I2C_H__ */
+
