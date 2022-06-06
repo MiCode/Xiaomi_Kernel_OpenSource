26985a67c5f393da5b5b36e32f880581652d0cb0
Chetan Chinnadagudihundi Ravindranath <quic_cchinnad@quicinc.com>
2022-06-06 11:13:04 +0530

    soc: qcom: socinfo: Add the soc-id for KATMAI/KATMAIP
    Add soc-id entry for KATMAI/KATMAIP (msm-id: 575/576) to the list
    of soc-ids supported.
    
    Change-Id: Iabc5c65f3020d1d12f73ed80b220a06ac6466553
    Signed-off-by: Chetan Chinnadagudihundi Ravindranath <quic_cchinnad@quicinc.com>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -688,6 +688,8 @@ static const struct soc_id soc_id[] = {
 	{ 498, "YUPIKP-IOT" },
 	{ 499, "YUPIKP" },
 	{ 515, "YUPIK-LTE" },
+	{ 575, "KATMAI" },
+	{ 576, "KATMAIP" },
 };
 
 static struct qcom_socinfo *qsocinfo;
