909b7e47c1bed54bc6eefe6cd2f8ad0f1ea5f79d
cpi00078 <cpi00078@mediatek.com>
2022-06-16 11:05:16 +0800

    [ALPS07241809] Charger: fix coverity
    [Detail] Fix Coverity issue
    
    MTK-Commit-Id: 6f21b455e8f930a68f62ed82de9de0acec88dc52
    
    Change-Id: I3abfb56dece97418f44d1def1438756059d21d0d
    CR-Id: ALPS07241809
    Feature: rtk_driver
    Signed-off-by: cpi00078 <cpi00078@mediatek.com>

--- drivers/power/supply/rt9759.c
+++ drivers/power/supply/rt9759.c
@@ -1753,6 +1753,7 @@ static int __rt9759_init_chip(struct rt9759_chip *chip)
 static int rt9759_check_devinfo(struct i2c_client *client, u8 *chip_rev,
 				enum rt9759_type *type)
 {
+	unsigned int ms_val;
 	int ret;
 
 	dev_info(&client->dev, "%s rev\n", __func__);
@@ -1772,14 +1773,12 @@ static int rt9759_check_devinfo(struct i2c_client *client, u8 *chip_rev,
 		dev_info(&client->dev, "%s 3 ret=%d\n", __func__, ret);
 		return ret;
 	}
-	*type = (ret & RT9759_MS_MASK) >> RT9759_MS_SHFT;
-
-	if ((*type >= 0) && (*type < ARRAY_SIZE(rt9759_type_name)))
-		dev_info(&client->dev, "%s rev(0x%02X), type(%s)\n", __func__,
-			*chip_rev, rt9759_type_name[*type]);
-	else
-		dev_info(&client->dev, "%s rev(0x%02X), type(%d)\n", __func__,
-			*chip_rev, *type);
+	ms_val = (ret & RT9759_MS_MASK) >> RT9759_MS_SHFT;
+	if (ms_val >= ARRAY_SIZE(rt9759_type_name))
+		return -EINVAL;
+	*type = ms_val;
+	dev_info(&client->dev, "%s rev(0x%02X), type(%s)\n", __func__,
+		 *chip_rev, rt9759_type_name[*type]);
 
 	return 0;
 }
