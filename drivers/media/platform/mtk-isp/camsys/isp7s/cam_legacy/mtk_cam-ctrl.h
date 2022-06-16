f12a9f5d8c66119e0e74a60ae22e785e52fde76e
Frederic Chen <frederic.chen@mediatek.com>
2022-06-16 11:42:44 +0800

    [ALPS07316111] media: cam: fixed the race of raw switch flow(060618)
    Raw switching flow may trigger in dequeue flow, so we have to serialize
    some part of dequeue flow and the qneueue flow.
    
    MTK-Commit-Id: e6bd48b4ecd6aaeeb0247019174ff5ab846b5cff
    
    CR-Id: ALPS07316111
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: If0d193d6ed4ace7d3ff86073d6c608f08b4be97d
    Signed-off-by: Frederic Chen <frederic.chen@mediatek.com>

--- drivers/media/platform/mtk-isp/camsys/isp7s/cam_legacy/mtk_cam-ctrl.h
+++ drivers/media/platform/mtk-isp/camsys/isp7s/cam_legacy/mtk_cam-ctrl.h
@@ -218,7 +218,7 @@ void mtk_cam_state_del_wo_sensor(struct mtk_cam_ctx *ctx,
 void
 mtk_camsys_raw_change_pipeline(struct mtk_cam_ctx *ctx,
 			       struct mtk_camsys_sensor_ctrl *sensor_ctrl,
-			       unsigned int dequeued_frame_seq_no);
+			       struct mtk_cam_request_stream_data *req_stream_data);
 int mtk_camsv_normal_scenario_handler(struct mtk_cam_device *cam,
 	struct mtk_camsv_device *camsv_dev, struct mtk_camsys_irq_info *irq_info,
 	unsigned int tag_idx);
--- drivers/media/platform/mtk-isp/camsys/isp7s/cam_legacy/mtk_cam.h
+++ drivers/media/platform/mtk-isp/camsys/isp7s/cam_legacy/mtk_cam.h
@@ -887,6 +887,26 @@ static inline bool mtk_cam_ctx_has_raw(struct mtk_cam_ctx *ctx)
 	return (ctx && ctx->used_raw_num > 0);
 }
 
+static inline bool
+mtk_cam_is_immediate_switch_req(struct mtk_cam_request *req, int stream_id)
+{
+	if ((req->flags & MTK_CAM_REQ_FLAG_SENINF_IMMEDIATE_UPDATE) &&
+	    (req->ctx_link_update & (1 << stream_id)))
+		return true;
+	else
+		return false;
+}
+
+static inline bool
+mtk_cam_is_nonimmediate_switch_req(struct mtk_cam_request *req,
+				   int stream_id)
+{
+	if ((req->ctx_link_update & (1 << stream_id)) &&
+	    !(req->flags & MTK_CAM_REQ_FLAG_SENINF_IMMEDIATE_UPDATE))
+		return true;
+	else
+		return false;
+}
 
 //TODO: with spinlock or not? depends on how request works [TBD]
 
