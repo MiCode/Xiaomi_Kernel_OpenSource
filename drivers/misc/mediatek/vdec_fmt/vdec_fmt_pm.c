e0d28a7d3b26cf68fec896b3712709de5ee130c7
Jason Hsu <yeong-cherng.hsu@mediatek.com>
2022-06-16 19:40:03 +0800

    [ALPS07344864] FMT: workaround for cmdq_pm_lock not release issue
    [Detail] check ref count when cmdq_mbox_enable/disable and call
             cmdq_mbox_disable to reduce un-release pair.
    
    MTK-Commit-Id: 60cac3e49568f3d33cab9903527bcf9243f10535
    
    Signed-off-by: Jason Hsu <yeong-cherng.hsu@mediatek.com>
    CR-Id: ALPS07344864
    Feature: formatter
    Change-Id: I2a90225feac73270f40b0ec235e12d85b07b8c91

--- drivers/misc/mediatek/vdec_fmt/vdec_fmt_pm.c
+++ drivers/misc/mediatek/vdec_fmt/vdec_fmt_pm.c
@@ -40,8 +40,11 @@ void fmt_init_pm(struct mtk_vdec_fmt *fmt)
 int32_t fmt_clock_on(struct mtk_vdec_fmt *fmt)
 {
 	int ret = 0;
+	s32 cmdq_ret = 0;
 
-	cmdq_mbox_enable(fmt->clt_fmt[0]->chan);
+	cmdq_ret = cmdq_mbox_enable(fmt->clt_fmt[0]->chan);
+	while (cmdq_ret > 1)
+		cmdq_ret = cmdq_mbox_disable(fmt->clt_fmt[0]->chan);
 	if (fmt->fmtLarb) {
 		ret = mtk_smi_larb_get(fmt->fmtLarb);
 		if (ret) {
@@ -66,13 +69,17 @@ int32_t fmt_clock_on(struct mtk_vdec_fmt *fmt)
 
 int32_t fmt_clock_off(struct mtk_vdec_fmt *fmt)
 {
+	s32 cmdq_ret = 0;
+
 	if (fmt->clk_MINI_MDP)
 		clk_disable_unprepare(fmt->clk_MINI_MDP);
 	if (fmt->clk_VDEC)
 		clk_disable_unprepare(fmt->clk_VDEC);
 	if (fmt->fmtLarb)
 		mtk_smi_larb_put(fmt->fmtLarb);
-	cmdq_mbox_disable(fmt->clt_fmt[0]->chan);
+	cmdq_ret = cmdq_mbox_disable(fmt->clt_fmt[0]->chan);
+	while (cmdq_ret > 0)
+		cmdq_ret = cmdq_mbox_disable(fmt->clt_fmt[0]->chan);
 	atomic_set(&fmt->fmt_error, 0);
 	return 0;
 }
