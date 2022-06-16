817c3a3781ed9af6913ec628a7f1efa44f86c3a6
Sophie Wang <sophie.wang@mediatek.com>
2022-06-16 10:54:08 +0800

    [ALPS07022423] msdc: update driver
    add to support mt6886 msdc.
    
    MTK-Commit-Id: 1256b490aae9c3e4bf7e6e65e1b8521f0a34f812
    
    Signed-off-by: Sophie Wang <sophie.wang@mediatek.com>
    CR-Id: ALPS07022423
    Feature: sdcard
    Change-Id: Ide86efafb2953159eea68f8edbbfb6d71f9fe1b2

--- drivers/mmc/host/mtk-mmc.c
+++ drivers/mmc/host/mtk-mmc.c
@@ -208,6 +208,20 @@ static const struct mtk_mmc_compatible mt6985_compat = {
 	.need_gate_cg = false,
 };
 
+static const struct mtk_mmc_compatible mt6886_compat = {
+	.clk_div_bits = 12,
+	.recheck_sdio_irq = false,
+	.hs400_tune = false,
+	.pad_tune_reg = MSDC_PAD_TUNE0,
+	.async_fifo = true,
+	.data_tune = true,
+	.busy_check = true,
+	.stop_clk_fix = true,
+	.enhance_rx = true,
+	.support_64g = true,
+	.need_gate_cg = false,
+};
+
 static const struct of_device_id msdc_of_ids[] = {
 	{ .compatible = "mediatek,mt8135-mmc", .data = &mt8135_compat},
 	{ .compatible = "mediatek,mt8173-mmc", .data = &mt8173_compat},
@@ -221,6 +235,7 @@ static const struct of_device_id msdc_of_ids[] = {
 	{ .compatible = "mediatek,mt6779-mmc", .data = &mt6779_compat},
 	{ .compatible = "mediatek,common-mmc-v2", .data = &common_v2_compat},
 	{ .compatible = "mediatek,mt6985-mmc", .data = &mt6985_compat},
+	{ .compatible = "mediatek,mt6886-mmc", .data = &mt6886_compat},
 	{}
 };
 MODULE_DEVICE_TABLE(of, msdc_of_ids);
