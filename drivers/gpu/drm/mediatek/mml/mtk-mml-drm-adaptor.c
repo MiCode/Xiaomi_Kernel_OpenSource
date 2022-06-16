f7ddecbb4a24d9f612f8152c7de225290424d2be
Chris-YC Chen <chris-yc.chen@mediatek.com>
2022-06-16 11:01:23 +0800

    [ALPS07326335] mml: add buffer dst cnt check
    add buffer dst cnt check
    
    MTK-Commit-Id: 15ae50341bf83a0d8ea6d1ff2c4a340b68e53ff0
    
    Signed-off-by: Chris-YC Chen <chris-yc.chen@mediatek.com>
    CR-Id: ALPS07326335
    Feature: mml
    Change-Id: Id95dac57d6c571d6db4e231cfb6f86b3fba3529e

--- drivers/gpu/drm/mediatek/mml/mtk-mml-drm-adaptor.c
+++ drivers/gpu/drm/mediatek/mml/mtk-mml-drm-adaptor.c
@@ -757,8 +757,11 @@ s32 mml_drm_submit(struct mml_drm_ctx *ctx, struct mml_submit *submit,
 		}
 	}
 
+	/* always fixup dest_cnt > MML_MAX_OUTPUTS */
 	if (submit->info.dest_cnt > MML_MAX_OUTPUTS)
 		submit->info.dest_cnt = MML_MAX_OUTPUTS;
+	if (submit->buffer.dest_cnt > MML_MAX_OUTPUTS)
+		submit->buffer.dest_cnt = MML_MAX_OUTPUTS;
 
 	/* always fixup format/modifier for afbc case
 	 * the format in info should change to fourcc format in future design
