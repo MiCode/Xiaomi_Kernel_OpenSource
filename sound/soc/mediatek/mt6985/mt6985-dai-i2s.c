5130b081015e1301eb7fb926ae5c9f0b034df3ca
mtk23153 <Cheng-yi.Cheng@mediatek.com>
2022-06-16 10:56:25 +0800

    [ALPS06291195] Audio: update i2s8 mux
    1. update i2s8 mux
    
    MTK-Commit-Id: 49f79b1edeffc7925843ee135cdbcab19985ba6a
    
    CR-Id: ALPS06291195
    Feature: alsa_sound_soc
    Signed-off-by: mtk23153 <cheng-yi.cheng@mediatek.com>
    Change-Id: I529b2f5c729bdff267354371b7d9c428f3ef3cc3

--- sound/soc/mediatek/mt6985/mt6985-dai-i2s.c
+++ sound/soc/mediatek/mt6985/mt6985-dai-i2s.c
@@ -947,6 +947,8 @@ static const struct snd_soc_dapm_widget mtk_dai_i2s_widgets[] = {
 	SND_SOC_DAPM_INPUT("I2S_DUMMY_IN"),
 	SND_SOC_DAPM_MUX("I2S0_In_Mux",
 			 SND_SOC_NOPM, 0, 0, &i2s0_in_mux_control),
+	SND_SOC_DAPM_MUX("I2S8_In_Mux",
+			 SND_SOC_NOPM, 0, 0, &i2s8_in_mux_control),
 
 	/* i2s in lpbk */
 	SND_SOC_DAPM_MUX("I2S0_Lpbk_Mux",
