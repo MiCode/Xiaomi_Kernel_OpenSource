0d9659a0346ce14dc8a6f2302d2bd10a36b3bd38
jianjiao zeng <jianjiao.zeng@mediatek.com>
2022-06-16 19:38:25 +0800

    [ALPS07323256] iommu: Add lock for power check
    Add lock protect for power check avoid DEVAPC when SMI
    callback dump register;
    Iommu dump own register no longer depend on SMI callback
    when TLB time out.
    
    MTK-Commit-Id: 6ca509907cda0594aa46c78e4f2f9490ebba84d1
    
    Signed-off-by: jianjiao zeng <jianjiao.zeng@mediatek.com>
    CR-Id: ALPS07323256
    Feature: iommu_kernel
    Change-Id: I18f074cd75b3436dbbb0ee3f503bca5f1303ab3e

--- drivers/iommu/mtk_iommu.c
+++ drivers/iommu/mtk_iommu.c
@@ -842,6 +842,8 @@ static void mtk_iommu_isr_record(struct mtk_iommu_data *data)
 	}
 }
 
+static void mtk_dump_reg_for_hang_issue(struct mtk_iommu_data *data);
+
 static void mtk_iommu_tlb_flush_check(struct mtk_iommu_data *data, bool range)
 {
 	u32 tlb_en = readl_relaxed(data->base + REG_MMU_INVALIDATE);
@@ -852,6 +854,7 @@ static void mtk_iommu_tlb_flush_check(struct mtk_iommu_data *data, bool range)
 	if (range && (tlb_en & F_MMU_INV_RANGE)) {
 		pr_warn("%s, TLB flush Range timed out, need to extend time!!(%d, %d)\n", __func__,
 			data->plat_data->iommu_type, data->plat_data->iommu_id);
+		mtk_dump_reg_for_hang_issue(data);
 		mtk_smi_dbg_hang_detect("iommu");
 		pr_warn("%s, dump: 0x20:0x%x, 0x12c:0x%x\n",
 			__func__, readl_relaxed(data->base + REG_MMU_INVALIDATE),
@@ -860,6 +863,7 @@ static void mtk_iommu_tlb_flush_check(struct mtk_iommu_data *data, bool range)
 	} else if (!range && (tlb_en & F_ALL_INVLD)) {
 		pr_warn("%s, TLB flush All timed out, need to extend time!!(%d, %d)\n", __func__,
 			data->plat_data->iommu_type, data->plat_data->iommu_id);
+		mtk_dump_reg_for_hang_issue(data);
 		mtk_smi_dbg_hang_detect("iommu");
 		pr_warn("%s, dump: 0x20:0x%x, 0x12c:0x%x\n",
 			__func__, readl_relaxed(data->base + REG_MMU_INVALIDATE),
@@ -2101,6 +2105,9 @@ static int mtk_iommu_pd_callback(struct notifier_block *nb,
 static int mtk_iommu_dbg_hang_cb(struct notifier_block *nb,
 				 unsigned long action, void *data)
 {
+	if (!IS_ERR_OR_NULL(data) && strcmp((char *) data, "iommu") == 0)
+		return NOTIFY_DONE;
+
 	mtk_iommu_dbg_hang_detect(MM_IOMMU, DISP_IOMMU);
 	mtk_iommu_dbg_hang_detect(MM_IOMMU, MDP_IOMMU);
 	return NOTIFY_OK;
@@ -3086,7 +3093,7 @@ static int mtk_dump_rs_sta_info(const struct mtk_iommu_data *data, int mmu)
 static void mtk_dump_reg_for_hang_issue(struct mtk_iommu_data *data)
 {
 #if IS_ENABLED(CONFIG_MTK_IOMMU_MISC_SECURE)
-	int cnt, ret, i;
+	int cnt, ret, i, dump_count = 1;
 #endif
 	void __iomem *base = data->base;
 
@@ -3124,7 +3131,7 @@ static void mtk_dump_reg_for_hang_issue(struct mtk_iommu_data *data)
 		return;
 	}
 
-	for (cnt = 0; cnt < 3; cnt++) {
+	for (cnt = 0; cnt < dump_count; cnt++) {
 		pr_info("===== the %d time: REG_MMU_STA(0x008) = 0x%x =====\n",
 			cnt, readl_relaxed(base + REG_MMU_STA));
 		mtk_dump_debug_reg_info(data);
@@ -3147,6 +3154,7 @@ void mtk_iommu_dbg_hang_detect(enum mtk_iommu_type type, int id)
 {
 	struct list_head *hw_list;
 	struct mtk_iommu_data *data;
+	unsigned long flags;
 
 	if (!share_pgtable) {
 		switch (type) {
@@ -3167,15 +3175,18 @@ void mtk_iommu_dbg_hang_detect(enum mtk_iommu_type type, int id)
 
 	for_each_m4u(data, hw_list) {
 		if (data->plat_data->iommu_type == type && data->plat_data->iommu_id == id) {
+			spin_lock_irqsave(&data->tlb_lock, flags);
 			if (!mtk_iommu_power_get(data)) {
 				pr_notice("%s, iommu:(%d,%d) power off dev:%s\n",
 					  __func__, type, id, dev_name(data->dev));
+				spin_unlock_irqrestore(&data->tlb_lock, flags);
 				return;
 			}
 
 			mtk_dump_reg_for_hang_issue(data);
 
 			mtk_iommu_power_put(data);
+			spin_unlock_irqrestore(&data->tlb_lock, flags);
 			return;
 		}
 	}
