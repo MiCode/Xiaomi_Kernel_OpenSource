eeebf60da748a8974a41339e30ddec52df4b4570
Jackson Chang <jackson-kt.chang@mediatek.com>
2022-06-16 10:57:52 +0800

    [ALPS06890987] devapc: update gpu parser rule
    Update devapc gpu parser rule
    
    MTK-Commit-Id: d71ee8f5124afdd35ebec2ec2e28d886cdc1c975
    
    Signed-off-by: Jackson Chang <jackson-kt.chang@mediatek.com>
    CR-Id: ALPS06890987
    Feature: devapc
    Change-Id: I0101a32d282149a456f922e8bb11d5fea0f37096

--- drivers/soc/mediatek/devapc/devapc-mt6985.c
+++ drivers/soc/mediatek/devapc/devapc-mt6985.c
@@ -320,14 +320,18 @@ static const char *mt6985_bus_id_to_master(uint32_t bus_id, uint32_t vio_addr,
 				return infra_mi_trans(bus_id);
 		/* AO_BUS */
 		} else {
-			if (((bus_id >> 8) & 0x3) == 0x0)
-				return "GPUEB_RV33_P";
-			else if (((bus_id >> 8) & 0x3) == 0x1)
-				return "GPUEB_RV33_D";
-			else if (((bus_id >> 8) & 0x3) == 0x2)
-				return "GPUEB_DMA";
-			else
+			if ((bus_id & 0x3f) == 0x2a) {
+				if (((bus_id >> 8) & 0x3) == 0x0)
+					return "GPUEB_RV33_P";
+				else if (((bus_id >> 8) & 0x3) == 0x1)
+					return "GPUEB_RV33_D";
+				else if (((bus_id >> 8) & 0x3) == 0x2)
+					return "GPUEB_DMA";
+				else
+					return "UNKNOWN_MASTER_TO_GPU";
+			} else {
 				return infra_mi_trans(bus_id);
+			}
 		}
 	} else {
 		return infra_mi_trans(bus_id);
