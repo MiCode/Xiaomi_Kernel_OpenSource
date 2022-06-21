d2f3ed05d9c8e4e647520798c373b07aaa879819
Money.Kang <Money.Kang@mediatek.com>
2022-06-21 11:39:31 +0800

    [ALPS04752390] Tidy Up
    [Detail] Necessary platform project related files adjustment.
    
    CR-Id: ALPS04752390
    Feature: Git Support(*)
    Change-Id: I5373930e7fa3e1bf2737cf7790f20dfe3277d51a

--- drivers/misc/mediatek/ext_gic/mt6739/plat_sw_mode.c
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-/*
- * Copyright (c) 2019 MediaTek Inc.
-*/
-
-unsigned int irq_sw_mode_support(void)
-{
-	return 0;
-}
--- drivers/misc/mediatek/ext_gic/mt6765/plat_sw_mode.c
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0
-/*
- * Copyright (C) 2019 MediaTek Inc.
- */
-
-unsigned int irq_sw_mode_support(void)
-{
-	return 1;
-}
