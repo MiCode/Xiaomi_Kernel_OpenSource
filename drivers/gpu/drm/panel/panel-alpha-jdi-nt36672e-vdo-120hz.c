18515d341cc9b9eaf92c0aaa1732c9f344371a11
mtk28714 <johnny-sc.sun@mediatek.com>
2022-06-29 09:00:56 +0800

    [ALPS07232880] Disp: Fix coverity issue
    Added null checkers
    
    MTK-Commit-Id: 9fae9a4f3858eca6f43c8235ab94318e16be02e1
    
    Signed-off-by: mtk28714 <johnny-sc.sun@mediatek.com>
    CR-Id: ALPS07232880
    Feature: display_mobile
    Change-Id: I7f0d46ffaa16a12dab68ad6ab9762e1cda0dbc5b

--- drivers/gpu/drm/panel/panel-alpha-jdi-nt36672e-vdo-120hz.c
+++ drivers/gpu/drm/panel/panel-alpha-jdi-nt36672e-vdo-120hz.c
@@ -1216,6 +1216,11 @@ static int mtk_panel_ext_param_set(struct drm_panel *panel,
 	int ret = 0;
 	struct drm_display_mode *m = get_mode_by_id_hfp(connector, mode);
 
+	if (!m) {
+		pr_err("%s:%d invalid display_mode\n", __func__, __LINE__);
+		return ret;
+	}
+
 	if (drm_mode_vrefresh(m) == 60)
 		ext->params = &ext_params;
 	else if (drm_mode_vrefresh(m) == 90)
