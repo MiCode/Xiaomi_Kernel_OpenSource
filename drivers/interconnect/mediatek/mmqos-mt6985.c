e8650e6b6b7db41638ed7cf043937cd9ee614f5e
Anthony Huang <anthony.huang@mediatek.com>
2022-06-29 08:58:29 +0800

    [ALPS06641071] mmqos: set common 0 port 1 as HRT_NONE on mt6985
    for experiment
    
    MTK-Commit-Id: aa99dc102e2b90a42d98b261a51b2b866172d683
    
    Change-Id: If6dbcc079a0444ee995593becf13edf181da1072
    Signed-off-by: Anthony Huang <anthony.huang@mediatek.com>
    Signed-off-by: Dennis YC Hsieh <dennis-yc.hsieh@mediatek.com>
    CR-Id: ALPS06641071
    Feature: mml

--- drivers/interconnect/mediatek/mmqos-mt6985.c
+++ drivers/interconnect/mediatek/mmqos-mt6985.c
@@ -955,7 +955,7 @@ static const struct mtk_mmqos_desc mmqos_desc_mt6985 = {
 	.comm_port_hrt_types = {
 		{ HRT_MAX_BWL, HRT_MAX_BWL, HRT_NONE, HRT_NONE, HRT_NONE,
 			HRT_NONE, HRT_CAM, HRT_NONE, HRT_DISP },
-		{ HRT_MAX_BWL, HRT_MAX_BWL, HRT_MML, HRT_NONE, HRT_NONE,
+		{ HRT_MAX_BWL, HRT_MAX_BWL, HRT_NONE, HRT_NONE, HRT_NONE,
 			HRT_NONE, HRT_CAM, HRT_NONE, HRT_NONE },
 	},
 	//.dual_pipe_larbs = { SLAVE_LARB(1), SLAVE_LARB(20) },
