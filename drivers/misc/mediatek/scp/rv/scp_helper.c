a5e466814a8c1f5a9a111cd70f49ddf387ad8b43
Gaia Chang <gaia.chang@mediatek.com>
2022-06-30 15:34:46 +0800

    [ALPS07347387] scp: add shutdown flow handle
    Skip subsys initial flow when system shutdown.
    
    MTK-Commit-Id: da1bb3c73c3ebb7bf5bd0a63a5b092ba1ff678ee
    
    Signed-off-by: Gaia Chang <gaia.chang@mediatek.com>
    CR-Id: ALPS07347387
    Feature: SCP Driver
    Change-Id: I68c0fadbb1427b626bbe7ccee58ea403fd2e8fe1

--- drivers/misc/mediatek/scp/rv/scp_helper.c
+++ drivers/misc/mediatek/scp/rv/scp_helper.c
@@ -77,6 +77,8 @@ unsigned int scp_ready[SCP_CORE_TOTAL];
 /* scp enable status*/
 unsigned int scp_enable[SCP_CORE_TOTAL];
 
+bool system_shutdown;
+
 /* scp dvfs variable*/
 unsigned int last_scp_expected_freq;
 unsigned int scp_expected_freq;
@@ -2565,6 +2567,11 @@ static int scp_device_probe(struct platform_device *pdev)
 	return ret;
 }
 
+static void scp_device_shutdown(struct platform_device *dev)
+{
+	system_shutdown = true;
+}
+
 static int scp_device_remove(struct platform_device *dev)
 {
 	if (scp_mbox_info) {
@@ -2610,6 +2617,7 @@ static const struct of_device_id scp_of_ids[] = {
 
 static struct platform_driver mtk_scp_device = {
 	.probe = scp_device_probe,
+	.shutdown = scp_device_shutdown,
 	.remove = scp_device_remove,
 	.driver = {
 		.name = "scp",
@@ -2794,7 +2802,8 @@ static int __init scp_init(void)
 #endif
 
 	driver_init_done = true;
-	reset_scp(SCP_ALL_ENABLE);
+	if (!system_shutdown)
+		reset_scp(SCP_ALL_ENABLE);
 
 #if SCP_DVFS_INIT_ENABLE
 	if (scp_dvfs_feature_enable())
