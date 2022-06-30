fa6d4c72c936bc75726e11092f708d3c133560b4
mtk23397 <chia-chi.lee@mediatek.com>
2022-06-30 15:34:34 +0800

    [ALPS07341747] FPSGO: filter some task timer
    filter some specific task timer
    which let FSTB V2 calculate target fps incorrectly
    
    MTK-Commit-Id: 1fddd097613a9d9bdcec9d083dd34dd2bd415640
    
    Signed-off-by: mtk23397 <chia-chi.lee@mediatek.com>
    CR-Id: ALPS07341747
    Feature: FPSGO
    Change-Id: I6881b73ddb762ad528b2ace353cba7fb18057605

--- drivers/misc/mediatek/performance/fpsgo_v3/fbt/src/xgf.c
+++ drivers/misc/mediatek/performance/fpsgo_v3/fbt/src/xgf.c
@@ -515,7 +515,8 @@ int xgf_check_main_sf_pid(int pid, int process_id)
 	int ret = 0;
 	int tmp_process_id;
 	char tmp_process_name[16];
-	struct task_struct *gtsk;
+	char tmp_thread_name[16];
+	struct task_struct *gtsk, *tsk;
 
 	tmp_process_id = xgf_get_process_id(pid);
 	if (tmp_process_id < 0)
@@ -535,6 +536,23 @@ int xgf_check_main_sf_pid(int pid, int process_id)
 	if ((tmp_process_id == process_id) ||
 		strstr(tmp_process_name, "surfaceflinger"))
 		ret = 1;
+
+	if (ret) {
+		rcu_read_lock();
+		tsk = find_task_by_vpid(pid);
+		if (tsk) {
+			get_task_struct(tsk);
+			strncpy(tmp_thread_name, tsk->comm, 16);
+			tmp_thread_name[15] = '\0';
+			put_task_struct(tsk);
+		} else
+			tmp_thread_name[0] = '\0';
+		rcu_read_unlock();
+
+		if (strstr(tmp_thread_name, "RTHeartBeat") ||
+			strstr(tmp_thread_name, "mali-"))
+			ret = 0;
+	}
 
 	return ret;
 }
