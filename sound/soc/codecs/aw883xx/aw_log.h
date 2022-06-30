6ddde818203fd0a99073522f32d71244d1205f77
jianxin.xu <jianxin.xu@mediatek.com>
2022-06-30 15:34:48 +0800

    [ALPS07351004] tb8781p2_64: porting aw883xx smartpa kernel driver
    [Details] porting aw883xx smartpa kernel driver
    1. add aw883xx driver code
    2. modified Konfig and Makefile in sound/soc/codecs
    
    MTK-Commit-Id: 2412d84c3742409b9c583a626c9a272574f46716
    
    CR-Id: ALPS07351004
    Feature: alsa_codec
    Change-Id: I7191d8893ff4089e2f22154614070b850cde1365
    Signed-off-by: jianxin.xu <jianxin.xu@mediatek.com>

---
+++ sound/soc/codecs/aw883xx/aw_log.h
@@ -0,0 +1,32 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2021 MediaTek Inc.
+ */
+
+#ifndef __AWINIC_LOG_H__
+#define __AWINIC_LOG_H__
+
+/********************************************
+ * print information control
+ *******************************************/
+#define aw_dev_info(dev, format, ...) \
+	do { \
+		pr_info("[Awinic][%s]%s: " format "\n", dev_name(dev), __func__, ##__VA_ARGS__); \
+	} while (0)
+#define aw_dev_dbg(dev, format, ...) \
+	do { \
+		pr_debug("[Awinic][%s]%s: " format "\n", dev_name(dev), __func__, ##__VA_ARGS__); \
+	} while (0)
+
+#define aw_pr_info(format, ...) \
+	do { \
+		pr_info("[Awinic]%s: " format "\n", __func__, ##__VA_ARGS__); \
+	} while (0)
+#define aw_pr_dbg(format, ...) \
+	do { \
+		pr_debug("[Awinic]%s: " format "\n", __func__, ##__VA_ARGS__); \
+	} while (0)
+
+
+#endif
+
