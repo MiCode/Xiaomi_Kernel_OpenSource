b767d20c3ccc63a094e3242d9521766dc4cc9d5c
Visweswara Tanuku <quic_vtanuku@quicinc.com>
2022-07-04 02:22:43 -0700

    serial: msm_geni_serial: Handle Rx EOT & DMA_DONE after DMA Reset
    Upon DMA reset dma_status is cleared, but if Rx EOT and
    DMA_DONE bits are still set corresponding data is not
    received from rx fifo. This scenario is encountered when
    there was a cancel, abort, fsm reset failure.
    
    Handle Rx EOT & DMA_DONE if set after DMA Reset.
    
    Change-Id: Ie0e4e59510c6103cf9794f567cbac99d7a45d867
    Signed-off-by: Visweswara Tanuku <quic_vtanuku@quicinc.com>

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -2325,7 +2325,6 @@ static bool handle_rx_dma_xfer(u32 s_irq_status, struct uart_port *uport)
 			UART_LOG_DBG(msm_port->ipc_log_misc, uport->dev,
 			"%s.Reset done.  0x%x.\n", __func__, dma_rx_status);
 			ret = true;
-			goto exit;
 		}
 
 		if (dma_rx_status & UART_DMA_RX_ERRS) {
@@ -2380,7 +2379,6 @@ static bool handle_rx_dma_xfer(u32 s_irq_status, struct uart_port *uport)
 	if (s_irq_status & (S_CMD_CANCEL_EN | S_CMD_ABORT_EN))
 		ret = true;
 
-exit:
 	spin_unlock_irqrestore(&msm_port->rx_lock, lock_flags);
 	return ret;
 }
