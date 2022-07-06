f9ff996b500c1c924c5b840c266a25793d458271
quic_aryaanan <quic_aryaanan@quicinc.com>
2022-07-06 12:47:45 -0700

    Revert "gunyah: Add support to manage wdog of proxy scheduled VMs"
    This reverts commit 59ff865dd2889cbc7c5d2803ccb4adbc5dac90d9.
    
    Change-Id: I928c65c2e6de93c0bb897951cbcdb7b961d04856
    Signed-off-by: Arya Anand <quic_aryaanan@quicinc.com>

--- include/linux/gunyah/gh_rm_drv.h
+++ include/linux/gunyah/gh_rm_drv.h
@@ -314,7 +314,6 @@ struct notifier_block;
 
 typedef int (*gh_virtio_mmio_cb_t)(gh_vmid_t peer, const char *vm_name,
 	gh_label_t label, gh_capid_t cap_id, int linux_irq, u64 base, u64 size);
-typedef int (*gh_wdog_manage_cb_t)(gh_vmid_t vmid, gh_capid_t cap_id, bool populate);
 typedef int (*gh_vcpu_affinity_set_cb_t)(gh_vmid_t vmid, gh_label_t label,
 						gh_capid_t cap_id, int linux_irq);
 typedef int (*gh_vcpu_affinity_reset_cb_t)(gh_vmid_t vmid, gh_label_t label,
@@ -347,7 +346,6 @@ int gh_rm_vm_irq_reclaim(gh_virq_handle_t virq_handle);
 
 int gh_rm_set_virtio_mmio_cb(gh_virtio_mmio_cb_t fnptr);
 void gh_rm_unset_virtio_mmio_cb(void);
-int gh_rm_set_wdog_manage_cb(gh_wdog_manage_cb_t fnptr);
 int gh_rm_set_vcpu_affinity_cb(gh_vcpu_affinity_set_cb_t fnptr);
 int gh_rm_reset_vcpu_affinity_cb(gh_vcpu_affinity_reset_cb_t fnptr);
 int gh_rm_set_vpm_grp_cb(gh_vpm_grp_set_cb_t fnptr);
@@ -661,11 +659,6 @@ static inline int gh_rm_set_virtio_mmio_cb(gh_virtio_mmio_cb_t fnptr)
 static inline void gh_rm_unset_virtio_mmio_cb(void)
 {
 
-}
-
-static inline int gh_rm_set_wdog_manage_cb(gh_wdog_manage_cb_t fnptr)
-{
-	return -EINVAL;
 }
 
 static inline int gh_rm_set_vcpu_affinity_cb(gh_vcpu_affinity_set_cb_t fnptr)
