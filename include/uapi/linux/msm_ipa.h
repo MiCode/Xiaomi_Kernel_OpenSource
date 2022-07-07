9b91e63205b7d6bd9d3eb0668a78bb82909eeda2
Michael Adisumarta <quic_madisuma@quicinc.com>
2022-07-06 21:33:06 -0700

    msm: ipa3: Add support for External Router Mode FR
    Add new IOCTL to get external router mode and ipv6 prefix
    addr and mask. Define new structs and enum to hold this
    ext route info.
    
    Change-Id: I3ef9ebdb0c33c5deeda27b340c1261e08a032b7c
    Signed-off-by: Michael Adisumarta <quic_madisuma@quicinc.com>

--- include/uapi/linux/msm_ipa.h
+++ include/uapi/linux/msm_ipa.h
@@ -146,7 +146,7 @@
 #define IPA_IOCTL_SET_NAT_EXC_RT_TBL_IDX        94
 #define IPA_IOCTL_SET_CONN_TRACK_EXC_RT_TBL_IDX 95
 #define IPA_IOCTL_COAL_EVICT_POLICY             96
-
+#define IPA_IOCTL_SET_EXT_ROUTER_MODE           97
 /**
  * max size of the header to be inserted
  */
@@ -938,7 +938,13 @@ enum ipa_macsec_event {
 #define IPA_MACSEC_EVENT_MAX IPA_MACSEC_EVENT_MAX
 };
 
-#define IPA_EVENT_MAX_NUM (IPA_MACSEC_EVENT_MAX)
+enum ipa_ext_route_evt {
+	IPA_SET_EXT_ROUTER_MODE_EVENT = IPA_MACSEC_EVENT_MAX,
+	IPA_SET_EXT_ROUTER_MODE_EVENT_MAX
+#define IPA_SET_EXT_ROUTER_MODE_EVENT_MAX IPA_SET_EXT_ROUTER_MODE_EVENT_MAX
+};
+
+#define IPA_EVENT_MAX_NUM (IPA_SET_EXT_ROUTER_MODE_EVENT_MAX)
 #define IPA_EVENT_MAX ((int)IPA_EVENT_MAX_NUM)
 
 /**
@@ -3385,6 +3391,27 @@ struct ipa_ioc_macsec_info {
 	__u32 padding;
 };
 
+
+enum ipa_ext_router_mode {
+	IPA_PREFIX_DISABLED = 0,
+	IPA_PREFIX_SHARING,
+	IPA_PREFIX_DELEGATION
+};
+
+/**
+ * struct ipa_ioc_ext_router_info - provide ext_router info
+ * @ipa_ext_router_mode: prefix sharing, prefix delegation, or disabled mode
+ * @pdn_name: PDN interface name
+ * @ipv6_addr: the prefix addr used for dummy or delegated prefixes
+ * @ipv6_mask: the ipv6 mask used to mask above addr to get the correct prefix
+ */
+struct ipa_ioc_ext_router_info {
+	enum ipa_ext_router_mode mode;
+	char pdn_name[IPA_RESOURCE_NAME_MAX];
+	uint32_t ipv6_addr[4];
+	uint32_t ipv6_mask[4];
+};
+
 /**
  *   actual IOCTLs supported by IPA driver
  */
@@ -3702,6 +3729,10 @@ struct ipa_ioc_macsec_info {
 #define IPA_IOC_SET_CONN_TRACK_EXC_RT_TBL_IDX _IOW(IPA_IOC_MAGIC, \
 				IPA_IOCTL_SET_CONN_TRACK_EXC_RT_TBL_IDX, \
 				uint32_t)
+
+#define IPA_IOC_SET_EXT_ROUTER_MODE _IOWR(IPA_IOC_MAGIC, \
+				IPA_IOCTL_SET_EXT_ROUTER_MODE, \
+				struct ipa_ioc_ext_router_info)
 /*
  * unique magic number of the Tethering bridge ioctls
  */
