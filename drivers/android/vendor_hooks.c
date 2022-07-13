2ecf8fb58d94baf3f6f524ef549c6be6e7f79172
xiaofeng <xiaofeng5@xiaomi.com>
2022-07-13 10:23:51 +0800

    ANDROID: vendor_hooks:vendor hook for mmput
    add vendor hook in mmput while mm_users decreased to 0.
    
    Bug: 238821038
    Bug: 248221914
    Change-Id: I42a717cbeeb3176bac14b4b2391fdb2366c972d3
    Signed-off-by: xiaofeng <xiaofeng5@xiaomi.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -438,3 +438,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_uid);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_user);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_pages_reclaim_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_pages_failure_bypass);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mmput);
