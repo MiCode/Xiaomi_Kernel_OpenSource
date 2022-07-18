3e37ce44eb0d87fcc8c409d306ac199d331495eb
jj chen <jj.chen@mediatek.com>
2022-07-18 16:23:27 +0800

    [ALPS07437109] disp: modify panel driver to nt36672e_vdo_144hz_hfp
    modify panel driver to nt36672e_vdo_144hz_hfp
    
    MTK-Commit-Id: 6f56147614309aebcd915ab106868f7ec0494a74
    
    Signed-off-by: jj chen <jj.chen@mediatek.com>
    CR-Id: ALPS07437109
    Feature: display_mobile
    Change-Id: I2f43e31dd2f591546a54897d3dece5235ddbb89b

--- drivers/gpu/drm/panel/panel-alpha-jdi-nt36672e-vdo-144hz-hfp.c
+++ drivers/gpu/drm/panel/panel-alpha-jdi-nt36672e-vdo-144hz-hfp.c
@@ -843,7 +843,7 @@ static const struct drm_display_mode performance_mode_144hz = {
 
 #if defined(CONFIG_MTK_PANEL_EXT)
 static struct mtk_panel_params ext_params = {
-	.pll_clk = 657,
+	.pll_clk = 651,
 	.vfp_low_power = 900,
 	.cust_esd_check = 1,
 	.esd_check_enable = 1,
@@ -899,7 +899,7 @@ static struct mtk_panel_params ext_params = {
 		.rc_tgt_offset_hi = 3,
 		.rc_tgt_offset_lo = 3,
 		},
-	.data_rate = 1314,
+	.data_rate = 1302,
 	.lfr_enable = 0,
 	.lfr_minimum_fps = 60,
 	.dyn_fps = {
@@ -923,7 +923,7 @@ static struct mtk_panel_params ext_params = {
 };
 
 static struct mtk_panel_params ext_params_90hz = {
-	.pll_clk = 657,
+	.pll_clk = 651,
 	.vfp_low_power = 1320,
 	.cust_esd_check = 1,
 	.esd_check_enable = 1,
@@ -979,7 +979,7 @@ static struct mtk_panel_params ext_params_90hz = {
 		.rc_tgt_offset_hi = 3,
 		.rc_tgt_offset_lo = 3,
 		},
-	.data_rate = 1314,
+	.data_rate = 1302,
 	.lfr_enable = 0,
 	.lfr_minimum_fps = 60,
 	.dyn_fps = {
@@ -1003,7 +1003,7 @@ static struct mtk_panel_params ext_params_90hz = {
 };
 
 static struct mtk_panel_params ext_params_144hz = {
-	.pll_clk = 657,
+	.pll_clk = 651,
 	.vfp_low_power = 3560,
 	.cust_esd_check = 1,
 	.esd_check_enable = 1,
@@ -1059,7 +1059,7 @@ static struct mtk_panel_params ext_params_144hz = {
 		.rc_tgt_offset_hi = 3,
 		.rc_tgt_offset_lo = 3,
 		},
-	.data_rate = 1314,
+	.data_rate = 1302,
 	.lfr_enable = 0,
 	.lfr_minimum_fps = 60,
 	.dyn_fps = {
