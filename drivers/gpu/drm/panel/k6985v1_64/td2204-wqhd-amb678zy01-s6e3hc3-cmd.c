a93c303954eaef38cadb7fcf439facd7cac02b86
Peng Cao <Peng.Cao@mediatek.com>
2022-07-24 13:02:11 +0800

    [ALPS07311540] display: mt6985 alpha phone lcm driver porting
    mt6985 alpha phone lcm driver porting
    
    MTK-Commit-Id: b87a25d8c1d5b28a39b9b996311a8da5831f1e37
    
    Signed-off-by: Peng Cao <Peng.Cao@mediatek.com>
    CR-Id: ALPS07311540
    Feature: display_mobile
    Change-Id: I917775ad2ea997cc28804a31aecc1894ca2f7540

---
+++ drivers/gpu/drm/panel/k6985v1_64/td2204-wqhd-amb678zy01-s6e3hc3-cmd.c
@@ -0,0 +1,649 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#include <linux/backlight.h>
+#include <drm/drm_mipi_dsi.h>
+#include <drm/drm_panel.h>
+#include <drm/drm_modes.h>
+#include <linux/delay.h>
+#include <drm/drm_connector.h>
+#include <drm/drm_device.h>
+
+#include <linux/gpio/consumer.h>
+#include <linux/regulator/consumer.h>
+
+#include <video/mipi_display.h>
+#include <video/of_videomode.h>
+#include <video/videomode.h>
+
+#include <linux/module.h>
+#include <linux/of_platform.h>
+#include <linux/of_graph.h>
+#include <linux/platform_device.h>
+
+#define CONFIG_MTK_PANEL_EXT
+#if defined(CONFIG_MTK_PANEL_EXT)
+#include "../mediatek/mediatek_v2/mtk_panel_ext.h"
+#include "../mediatek/mediatek_v2/mtk_drm_graphics_base.h"
+#endif
+
+#ifdef CONFIG_MTK_ROUND_CORNER_SUPPORT
+#include "../mediatek/mediatek_v2/mtk_corner_pattern/mtk_data_hw_roundedpattern.h"
+#endif
+
+#define REGFLAG_DELAY       0xFFFC
+#define REGFLAG_UDELAY  0xFFFB
+#define REGFLAG_END_OF_TABLE    0xFFFD
+#define REGFLAG_RESET_LOW   0xFFFE
+#define REGFLAG_RESET_HIGH  0xFFFF
+#define MDSS_MAX_PANEL_LEN	256
+
+struct lcm {
+	struct device *dev;
+	struct drm_panel panel;
+	struct backlight_device *backlight;
+	struct gpio_desc *reset_gpio;
+	struct gpio_desc *vddr_gpio;
+
+	struct regulator *oled_vddi;
+	struct regulator *oled_vci;
+
+	bool prepared;
+	bool enabled;
+
+	int error;
+};
+
+#include "td2204-wqhd-amb678zy01-s6e3hc3-cmd.h"
+
+
+#define lcm_dcs_write_seq(ctx, seq...)                                         \
+	({                                                                     \
+		const u8 d[] = {seq};                                          \
+		BUILD_BUG_ON_MSG(ARRAY_SIZE(d) > 64,                           \
+				 "DCS sequence too big for stack");            \
+		lcm_dcs_write(ctx, d, ARRAY_SIZE(d));                          \
+	})
+
+#define lcm_dcs_write_seq_static(ctx, seq...)                                  \
+	({                                                                     \
+		static const u8 d[] = {seq};                                   \
+		lcm_dcs_write(ctx, d, ARRAY_SIZE(d));                          \
+	})
+
+static inline struct lcm *panel_to_lcm(struct drm_panel *panel)
+{
+	return container_of(panel, struct lcm, panel);
+}
+
+static void lcm_dcs_write(struct lcm *ctx, const void *data, size_t len)
+{
+	struct mipi_dsi_device *dsi = to_mipi_dsi_device(ctx->dev);
+	ssize_t ret;
+	char *addr;
+
+	if (ctx->error < 0)
+		return;
+
+	addr = (char *)data;
+	if ((int)*addr < 0xB0)
+		ret = mipi_dsi_dcs_write_buffer(dsi, data, len);
+	else
+		ret = mipi_dsi_generic_write(dsi, data, len);
+	if (ret < 0) {
+		pr_info("error %zd writing seq: %ph\n", ret, data);
+		ctx->error = ret;
+	}
+}
+
+inline void push_table(struct lcm *ctx, struct LCD_setting_table *table,
+	unsigned int count, unsigned char force_update)
+{
+	unsigned int i;
+	unsigned int cmd;
+
+	for (i = 0; i < count; i++) {
+		cmd = table[i].cmd;
+
+		switch (cmd) {
+
+		case REGFLAG_DELAY:
+			if (table[i].count <= 10)
+				msleep(table[i].count);
+			else
+				msleep(table[i].count);
+			break;
+
+		case REGFLAG_UDELAY:
+			msleep(table[i].count);
+			break;
+
+		case REGFLAG_END_OF_TABLE:
+			break;
+
+		default:
+			lcm_dcs_write(ctx, &table[i].para_list, table[i].count);
+		}
+	}
+}
+
+inline void push_table_cmdq(struct mtk_dsi *dsi, dcs_write_gce cb, void *handle,
+							struct LCD_setting_table *table,
+			unsigned int count, unsigned char force_update)
+{
+	unsigned int i;
+	unsigned int cmd;
+
+	for (i = 0; i < count; i++) {
+		cmd = table[i].cmd;
+
+		switch (cmd) {
+
+		case REGFLAG_DELAY:
+			if (table[i].count <= 10)
+				msleep(table[i].count);
+			else
+				msleep(table[i].count);
+			break;
+
+		case REGFLAG_UDELAY:
+			msleep(table[i].count);
+			break;
+
+		case REGFLAG_END_OF_TABLE:
+			break;
+
+		default:
+			cb(dsi, handle, &table[i].para_list, table[i].count);
+		}
+	}
+}
+
+#ifdef PANEL_SUPPORT_READBACK
+static int lcm_dcs_read(struct lcm *ctx, u8 cmd, void *data, size_t len)
+{
+	struct mipi_dsi_device *dsi = to_mipi_dsi_device(ctx->dev);
+	ssize_t ret;
+
+	if (ctx->error < 0)
+		return 0;
+
+	ret = mipi_dsi_dcs_read(dsi, cmd, data, len);
+	if (ret < 0) {
+		pr_info("error %d reading dcs seq:(%#x)\n", ret, cmd);
+		ctx->error = ret;
+	}
+
+	return ret;
+}
+
+static void lcm_panel_get_data(struct lcm *ctx)
+{
+	u8 buffer[3] = {0};
+	static int ret;
+
+	if (ret == 0) {
+		ret = lcm_dcs_read(ctx, 0x0A, buffer, 1);
+		dev_info(ctx->dev, "return %d data(0x%08x) to dsi engine\n",
+			 ret, buffer[0] | (buffer[1] << 8));
+	}
+}
+#endif
+
+static int lcm_panel_power_regulator_init(struct lcm *ctx)
+{
+	int ret = 0;
+
+	// ctx->oled_vci = regulator_get(ctx->dev, "vci");
+	// if (IS_ERR(ctx->oled_vci)) { /* handle return value */
+	// ret = PTR_ERR(ctx->oled_vci);
+	// pr_info("get vci fail, error: %d\n", ret);
+	// return ret;
+	// }
+	// ret = regulator_set_voltage(ctx->oled_vci, 3100000, 3100000);
+	// if (ret < 0)
+	// pr_info("set voltage vci fail, ret = %d\n", ret);
+
+	/* althouth preloader have enabled vci, need to enable again */
+	/* set panel power and reset gpio */
+	ctx->reset_gpio = devm_gpiod_get(ctx->dev, "reset", GPIOD_OUT_HIGH);
+	if (IS_ERR(ctx->reset_gpio)) {
+		pr_info("%s: cannot get reset-gpios %ld\n",
+			__func__, PTR_ERR(ctx->reset_gpio));
+		return PTR_ERR(ctx->reset_gpio);
+	}
+	devm_gpiod_put(ctx->dev, ctx->reset_gpio);
+
+	ctx->vddr_gpio = devm_gpiod_get_index(ctx->dev, "vddr", 0, GPIOD_OUT_HIGH);
+	if (IS_ERR(ctx->vddr_gpio)) {
+		pr_info("%s: cannot get vddr_gpio 0 %ld\n",
+			__func__, PTR_ERR(ctx->vddr_gpio));
+		return PTR_ERR(ctx->vddr_gpio);
+	}
+	devm_gpiod_put(ctx->dev, ctx->vddr_gpio);
+
+	return ret; /* must be 0 */
+}
+
+static int lcm_panel_power_enable(struct lcm *ctx)
+{
+	int ret = 0;
+
+	/* enable regulator */
+	gpiod_set_value(ctx->vddr_gpio, 1);
+	usleep_range(2000, 2100);
+//	ret = regulator_enable(ctx->oled_vci);
+//	if (ret < 0)
+//		pr_info("enable regulator oled_vci fail, ret = %d\n", ret);
+//	usleep_range(2000, 2100);
+	gpiod_set_value(ctx->reset_gpio, 1);
+	usleep_range(12000, 12100);
+	gpiod_set_value(ctx->reset_gpio, 0);
+	usleep_range(2000, 2100);
+	gpiod_set_value(ctx->reset_gpio, 1);
+	usleep_range(12000, 12100);
+
+	return ret;
+}
+
+static int lcm_panel_power_disable(struct lcm *ctx)
+{
+	int ret = 0;
+
+	gpiod_set_value(ctx->reset_gpio, 0);
+	usleep_range(2000, 2100);
+	gpiod_set_value(ctx->vddr_gpio, 0);
+	usleep_range(1000, 1100);
+	// ret = regulator_disable(ctx->oled_vci);
+	// if (ret < 0)
+	//pr_info("disable regulator oled_vci fail, ret = %d\n", ret);
+
+	return ret;
+}
+
+/* add lcm report id interface for TP use */
+unsigned int mdss_report_lcm_id(void)
+{
+	pr_info("%s; panel id = 0x23 return 0xFF\n", __func__);
+	return 0xFF;
+}
+EXPORT_SYMBOL(mdss_report_lcm_id);
+
+int mdss_dsi_panel_reset_and_powerctl(int enable)
+{
+	return 0;
+}
+EXPORT_SYMBOL(mdss_dsi_panel_reset_and_powerctl);
+
+int dsi_panel_set_tsp_sync(bool enable)
+{
+	return 0;
+}
+EXPORT_SYMBOL(dsi_panel_set_tsp_sync);
+
+static void lcm_panel_init(struct lcm *ctx)
+{
+	push_table(ctx, init_setting, sizeof(init_setting) / sizeof(struct LCD_setting_table), 1);
+}
+
+static int lcm_disable(struct drm_panel *panel)
+{
+	struct lcm *ctx = panel_to_lcm(panel);
+
+	if (!ctx->enabled)
+		return 0;
+
+	if (ctx->backlight) {
+		ctx->backlight->props.power = FB_BLANK_POWERDOWN;
+		backlight_update_status(ctx->backlight);
+	}
+
+	ctx->enabled = false;
+
+	return 0;
+}
+
+static int lcm_unprepare(struct drm_panel *panel)
+{
+	struct lcm *ctx = panel_to_lcm(panel);
+
+	if (!ctx->prepared)
+		return 0;
+
+	push_table(ctx, lcm_suspend_setting,
+		sizeof(lcm_suspend_setting) / sizeof(struct LCD_setting_table), 1);
+
+	ctx->error = 0;
+	ctx->prepared = false;
+	lcm_panel_power_disable(ctx);
+
+	return 0;
+}
+
+static int lcm_prepare(struct drm_panel *panel)
+{
+	struct lcm *ctx = panel_to_lcm(panel);
+	int ret;
+
+
+	if (ctx->prepared)
+		return 0;
+
+	lcm_panel_power_enable(ctx);
+	lcm_panel_init(ctx);
+
+	ret = ctx->error;
+	if (ret < 0)
+		lcm_unprepare(panel);
+
+	ctx->prepared = true;
+
+#if defined(CONFIG_MTK_PANEL_EXT)
+	mtk_panel_tch_rst(panel);
+#endif
+#ifdef PANEL_SUPPORT_READBACK
+	lcm_panel_get_data(ctx);
+#endif
+
+	return ret;
+}
+
+static int lcm_enable(struct drm_panel *panel)
+{
+	struct lcm *ctx = panel_to_lcm(panel);
+
+	if (ctx->enabled)
+		return 0;
+
+	if (ctx->backlight) {
+		ctx->backlight->props.power = FB_BLANK_UNBLANK;
+		backlight_update_status(ctx->backlight);
+	}
+
+	ctx->enabled = true;
+
+	return 0;
+}
+
+static const struct drm_display_mode default_mode = {
+	.clock = 163406,
+	.hdisplay = FRAME_WIDTH,
+	.hsync_start = FRAME_WIDTH + HFP,
+	.hsync_end = FRAME_WIDTH + HFP + HSA,
+	.htotal = FRAME_WIDTH + HFP + HSA + HBP,
+	.vdisplay = FRAME_HEIGHT,
+	.vsync_start = FRAME_HEIGHT + VFP,
+	.vsync_end = FRAME_HEIGHT + VFP + VSA,
+	.vtotal = FRAME_HEIGHT + VFP + VSA + VBP,
+};
+
+#if defined(CONFIG_MTK_PANEL_EXT)
+static int panel_ext_reset(struct drm_panel *panel, int on)
+{
+	struct lcm *ctx = panel_to_lcm(panel);
+
+	ctx->reset_gpio =
+		devm_gpiod_get(ctx->dev, "reset", GPIOD_OUT_HIGH);
+	if (IS_ERR(ctx->reset_gpio)) {
+		pr_info("%s: cannot get reset_gpio %ld\n",
+			__func__, PTR_ERR(ctx->reset_gpio));
+		return PTR_ERR(ctx->reset_gpio);
+	}
+	gpiod_set_value(ctx->reset_gpio, on);
+	devm_gpiod_put(ctx->dev, ctx->reset_gpio);
+
+	return 0;
+}
+
+static int panel_ata_check(struct drm_panel *panel)
+{
+	struct lcm *ctx = panel_to_lcm(panel);
+	struct mipi_dsi_device *dsi = to_mipi_dsi_device(ctx->dev);
+	unsigned char data[3] = {0x00, 0x00, 0x00};
+	unsigned char id[3] = {0x40, 0x00, 0x00};
+	ssize_t ret;
+
+	ret = mipi_dsi_dcs_read(dsi, 0x4, data, 3);
+	if (ret < 0) {
+		pr_info("%s error\n", __func__);
+		return 0;
+	}
+
+	pr_info("ATA read data %x %x %x\n", data[0], data[1], data[2]);
+
+	if (data[0] == id[0] && data[1] == id[1] && data[2] == id[2])
+		return 1;
+
+	pr_info("ATA expect read data is %x %x %x\n", id[0], id[1], id[2]);
+
+	return 0;
+}
+
+static void mipi_dcs_write_backlight_command(void *dsi, dcs_write_gce cb,
+		void *handle, unsigned int level)
+{
+	cmd_bl_level[0].para_list[1] = (unsigned char)((level>>8)&0xF);
+	cmd_bl_level[0].para_list[2] = (unsigned char)(level&0xFF);
+	pr_info("dsi set backlight level %d\n", level);
+	push_table_cmdq(dsi, cb, handle, cmd_bl_level,
+		sizeof(cmd_bl_level) / sizeof(struct LCD_setting_table), 1);
+}
+
+static int lcm_setbacklight_cmdq(void *dsi, dcs_write_gce cb, void *handle,
+				 unsigned int level)
+{
+	char bl_tb0[] = {0x51, 0xFF};
+
+	bl_tb0[1] = level;
+
+	if (!cb)
+		return -1;
+
+	mipi_dcs_write_backlight_command(dsi, cb, handle, level);
+
+	return 0;
+}
+
+static struct mtk_panel_params ext_params = {
+	.pll_clk = 385,
+	.vfp_low_power = 750,
+	.cust_esd_check = 0,
+	.esd_check_enable = 0,
+	.lcm_esd_check_table[0] = {
+		.cmd = 0x0a,
+		.count = 1,
+		.para_list[0] = 0x9c,
+	 },
+	.physical_width_um = PHYSICAL_WIDTH,
+	.physical_height_um = PHYSICAL_HEIGHT,
+	.output_mode = MTK_PANEL_DSC_SINGLE_PORT,
+	.dsc_params = {
+		.enable                =  DSC_ENABLE,
+		.ver                   =  DSC_VER,
+		.slice_mode            =  DSC_SLICE_MODE,
+		.rgb_swap              =  DSC_RGB_SWAP,
+		.dsc_cfg               =  DSC_DSC_CFG,
+		.rct_on                =  DSC_RCT_ON,
+		.bit_per_channel       =  DSC_BIT_PER_CHANNEL,
+		.dsc_line_buf_depth    =  DSC_DSC_LINE_BUF_DEPTH,
+		.bp_enable             =  DSC_BP_ENABLE,
+		.bit_per_pixel         =  DSC_BIT_PER_PIXEL,
+		.pic_height            =  FRAME_HEIGHT,
+		.pic_width             =  FRAME_WIDTH,
+		.slice_height          =  DSC_SLICE_HEIGHT,
+		.slice_width           =  DSC_SLICE_WIDTH,
+		.chunk_size            =  DSC_CHUNK_SIZE,
+		.xmit_delay            =  DSC_XMIT_DELAY,
+		.dec_delay             =  DSC_DEC_DELAY,
+		.scale_value           =  DSC_SCALE_VALUE,
+		.increment_interval    =  DSC_INCREMENT_INTERVAL,
+		.decrement_interval    =  DSC_DECREMENT_INTERVAL,
+		.line_bpg_offset       =  DSC_LINE_BPG_OFFSET,
+		.nfl_bpg_offset        =  DSC_NFL_BPG_OFFSET,
+		.slice_bpg_offset      =  DSC_SLICE_BPG_OFFSET,
+		.initial_offset        =  DSC_INITIAL_OFFSET,
+		.final_offset          =  DSC_FINAL_OFFSET,
+		.flatness_minqp        =  DSC_FLATNESS_MINQP,
+		.flatness_maxqp        =  DSC_FLATNESS_MAXQP,
+		.rc_model_size         =  DSC_RC_MODEL_SIZE,
+		.rc_edge_factor        =  DSC_RC_EDGE_FACTOR,
+		.rc_quant_incr_limit0  =  DSC_RC_QUANT_INCR_LIMIT0,
+		.rc_quant_incr_limit1  =  DSC_RC_QUANT_INCR_LIMIT1,
+		.rc_tgt_offset_hi      =  DSC_RC_TGT_OFFSET_HI,
+		.rc_tgt_offset_lo      =  DSC_RC_TGT_OFFSET_LO,
+	},
+	.data_rate = MIPI_DATA_RATE,
+};
+
+
+static struct mtk_panel_funcs ext_funcs = {
+	.reset = panel_ext_reset,
+	.set_backlight_cmdq = lcm_setbacklight_cmdq,
+	.ata_check = panel_ata_check,
+};
+#endif
+
+struct panel_desc {
+	const struct drm_display_mode *modes;
+	unsigned int num_modes;
+
+	unsigned int bpc;
+
+	struct {
+		unsigned int width;
+		unsigned int height;
+	} size;
+
+	struct {
+		unsigned int prepare;
+		unsigned int enable;
+		unsigned int disable;
+		unsigned int unprepare;
+	} delay;
+};
+
+static int lcm_get_modes(struct drm_panel *panel, struct drm_connector *connector)
+{
+	struct drm_display_mode *mode;
+
+	mode = drm_mode_duplicate(connector->dev, &default_mode);
+	if (!mode) {
+		dev_info(connector->dev->dev, "failed to add mode %ux%ux@%u\n",
+			default_mode.hdisplay, default_mode.vdisplay,
+			drm_mode_vrefresh(&default_mode));
+		return -ENOMEM;
+	}
+
+	drm_mode_set_name(mode);
+	mode->type = DRM_MODE_TYPE_DRIVER | DRM_MODE_TYPE_PREFERRED;
+	drm_mode_probed_add(connector, mode);
+
+	connector->display_info.width_mm = 64;
+	connector->display_info.height_mm = 129;
+
+	return 1;
+}
+
+static const struct drm_panel_funcs lcm_drm_funcs = {
+	.disable = lcm_disable,
+	.unprepare = lcm_unprepare,
+	.prepare = lcm_prepare,
+	.enable = lcm_enable,
+	.get_modes = lcm_get_modes,
+};
+
+static int lcm_probe(struct mipi_dsi_device *dsi)
+{
+	struct device *dev = &dsi->dev;
+	struct lcm *ctx;
+	struct device_node *backlight;
+	int ret;
+
+	ctx = devm_kzalloc(dev, sizeof(struct lcm), GFP_KERNEL);
+	if (!ctx)
+		return -ENOMEM;
+
+	mipi_dsi_set_drvdata(dsi, ctx);
+
+	ctx->dev = dev;
+	dsi->lanes = 4;
+	dsi->format = MIPI_DSI_FMT_RGB888;
+	dsi->mode_flags = MIPI_DSI_MODE_LPM | MIPI_DSI_MODE_NO_EOT_PACKET
+			 | MIPI_DSI_CLOCK_NON_CONTINUOUS;
+
+	backlight = of_parse_phandle(dev->of_node, "backlight", 0);
+	if (backlight) {
+		ctx->backlight = of_find_backlight_by_node(backlight);
+		of_node_put(backlight);
+
+		if (!ctx->backlight)
+			return -EPROBE_DEFER;
+	}
+
+	lcm_panel_power_regulator_init(ctx);
+
+#ifndef CONFIG_MTK_DISP_NO_LK
+	ctx->prepared = true;
+	ctx->enabled = true;
+#endif
+
+	drm_panel_init(&ctx->panel, dev, &lcm_drm_funcs, DRM_MODE_CONNECTOR_DSI);
+	ctx->panel.dev = dev;
+	ctx->panel.funcs = &lcm_drm_funcs;
+
+	drm_panel_add(&ctx->panel);
+
+	ret = mipi_dsi_attach(dsi);
+	if (ret < 0)
+		drm_panel_remove(&ctx->panel);
+
+#if defined(CONFIG_MTK_PANEL_EXT)
+	mtk_panel_tch_handle_reg(&ctx->panel);
+	ret = mtk_panel_ext_create(dev, &ext_params, &ext_funcs, &ctx->panel);
+	if (ret < 0)
+		return ret;
+#endif
+
+	pr_info("%s-\n", __func__);
+
+	return ret;
+}
+
+static int lcm_remove(struct mipi_dsi_device *dsi)
+{
+	struct lcm *ctx = mipi_dsi_get_drvdata(dsi);
+#if defined(CONFIG_MTK_PANEL_EXT)
+	struct mtk_panel_ctx *ext_ctx = find_panel_ctx(&ctx->panel);
+#endif
+
+#if defined(CONFIG_MTK_PANEL_EXT)
+	mtk_panel_detach(ext_ctx);
+	mtk_panel_remove(ext_ctx);
+#endif
+	return 0;
+}
+
+static const struct of_device_id lcm_of_match[] = {
+	{ .compatible = "mt6985,amb678zy01-s6e3hc3,cmd", },
+	{ }
+};
+
+MODULE_DEVICE_TABLE(of, lcm_of_match);
+
+static struct mipi_dsi_driver lcm_driver = {
+	.probe = lcm_probe,
+	.remove = lcm_remove,
+	.driver = {
+		.name = "td2204-wqhd-amb678zy01-s6e3hc3-cmd",
+		.owner = THIS_MODULE,
+		.of_match_table = lcm_of_match,
+	},
+};
+
+module_mipi_dsi_driver(lcm_driver);
+
+MODULE_AUTHOR("Peng Cao <Pengcao@mediatek.com>");
+MODULE_DESCRIPTION("mt6985 amb678zy01-s6e3hc3 dsc Panel Driver");
+MODULE_LICENSE("GPL v2");
