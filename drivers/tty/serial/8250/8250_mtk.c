f17cc7ef2076c8c5816ecd8e5f7d53546a149563
huihui.wang <huihui.wang@mediatek.com>
2022-07-24 13:02:23 +0800

    [ALPS07451921] revert 8250: mediatek: skip disable clock
    Make CG of UART with uarthub always on.
    
    MTK-Commit-Id: 40ca32ba62e6d505eebfaa211373660b14241b02
    
    Signed-off-by: huihui.wang <huihui.wang@mediatek.com>
    CR-Id: ALPS07451921
    Feature: uart_driver
    Change-Id: Id164421ad79795a51874ec9b684d3788794de923

--- drivers/tty/serial/8250/8250_mtk.c
+++ drivers/tty/serial/8250/8250_mtk.c
@@ -1022,20 +1022,17 @@ static int __maybe_unused mtk8250_runtime_suspend(struct device *dev)
 	struct mtk8250_data *data = dev_get_drvdata(dev);
 	struct uart_8250_port *up = serial8250_get_port(data->line);
 
-	if (data->support_hub) {
-		dev_dbg(dev, "%s support_hub, skip disable clock\n", __func__);
+	if (data->clk_count == 0U) {
+		dev_dbg(dev, "%s clock count is 0\n", __func__);
 	} else {
-		if (data->clk_count == 0U) {
-			dev_dbg(dev, "%s clock count is 0\n", __func__);
-		} else {
-			/* wait until UART in idle status */
-			while
-				(serial_in(up, MTK_UART_DEBUG0));
+		/* wait until UART in idle status */
+		while
+			(serial_in(up, MTK_UART_DEBUG0));
 
-			clk_disable_unprepare(data->bus_clk);
-			data->clk_count--;
-		}
+		clk_disable_unprepare(data->bus_clk);
+		data->clk_count--;
 	}
+
 	return 0;
 }
 
