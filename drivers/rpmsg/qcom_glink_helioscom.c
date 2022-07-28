69fbd9908efc5fba1e21018953bf85280a873e7d
Kishore Kumar Ravi <quic_kiskum@quicinc.com>
2022-07-28 12:25:36 +0530

    rpmsg: qcom_glink_helioscom: Create single API to send one packet
    Added support to avoid aborting packets after partial
    send to remote side. Added check to confirm if there
    is enough space in fifo to send complete packet before
    sending data and short commands.
    
    Change-Id: Idd0f3d2f30e382a5b519c25cdbac52886d40c5ac
    Signed-off-by: Kishore Kumar Ravi <quic_kiskum@quicinc.com>

--- drivers/rpmsg/qcom_glink_helioscom.c
+++ drivers/rpmsg/qcom_glink_helioscom.c
@@ -558,71 +558,6 @@ static int glink_helioscom_send_intent_req_ack(struct glink_helioscom *glink,
 	return 0;
 }
 
-static int glink_helioscom_send_data(struct glink_helioscom_channel *channel,
-			       void *data, int chunk_size, int left_size,
-			       struct glink_helioscom_rx_intent *intent, bool wait)
-{
-	struct glink_helioscom *glink = channel->glink;
-	struct {
-		struct glink_helioscom_msg msg;
-		__le32 chunk_size;
-		__le32 left_size;
-		uint64_t addr;
-	} __packed req;
-
-	CH_INFO(channel, "chunk:%d, left:%d\n", chunk_size, left_size);
-
-	memset(&req, 0, sizeof(req));
-	if (intent->offset)
-		req.msg.cmd = cpu_to_le16(HELIOSCOM_CMD_TX_DATA_CONT);
-	else
-		req.msg.cmd = cpu_to_le16(HELIOSCOM_CMD_TX_DATA);
-
-	req.msg.param1 = cpu_to_le16(channel->lcid);
-	req.msg.param2 = cpu_to_le32(intent->id);
-	req.chunk_size = cpu_to_le32(chunk_size);
-	req.left_size = cpu_to_le32(left_size);
-	req.addr = 0;
-
-	mutex_lock(&glink->tx_lock);
-	while (glink_helioscom_tx_avail(glink) < sizeof(req)/WORD_SIZE) {
-		if (!wait) {
-			mutex_unlock(&glink->tx_lock);
-			return -EAGAIN;
-		}
-
-		if (atomic_read(&glink->in_reset)) {
-			mutex_unlock(&glink->tx_lock);
-			return -EINVAL;
-		}
-
-		if (!glink->sent_read_notify) {
-			glink->sent_read_notify = true;
-			glink_helioscom_send_read_notify(glink);
-		}
-
-		/* Wait without holding the tx_lock */
-		mutex_unlock(&glink->tx_lock);
-
-		usleep_range(TX_WAIT_US, TX_WAIT_US + 50);
-
-		mutex_lock(&glink->tx_lock);
-
-		if (glink_helioscom_tx_avail(glink) >= sizeof(req)/WORD_SIZE)
-			glink->sent_read_notify = false;
-	}
-
-	helioscom_ahb_write(glink->helioscom_handle,
-	(uint32_t)(size_t)(intent->addr + intent->offset),
-	ALIGN(chunk_size, WORD_SIZE)/WORD_SIZE, data);
-
-	intent->offset += chunk_size;
-	glink_helioscom_tx_write(glink, &req, sizeof(req));
-
-	mutex_unlock(&glink->tx_lock);
-	return 0;
-}
-
 static void glink_helioscom_handle_intent_req_ack(struct glink_helioscom *glink,
 					    unsigned int cid, bool granted)
 {
@@ -719,60 +654,6 @@ static void glink_helioscom_handle_intent_req(struct glink_helioscom *glink,
 	glink_helioscom_send_intent_req_ack(glink, channel, !!intent);
 }
 
-static int glink_helioscom_send_short(struct glink_helioscom_channel *channel,
-				void *data, int len,
-				struct glink_helioscom_rx_intent *intent,
-				bool wait)
-{
-	struct glink_helioscom *glink = channel->glink;
-	struct {
-		struct glink_helioscom_msg msg;
-		u8 data[SHORT_SIZE];
-	} __packed req;
-
-	CH_INFO(channel, "intent offset:%d len:%d\n", intent->offset, len);
-
-	req.msg.cmd = cpu_to_le16(HELIOSCOM_CMD_TX_SHORT_DATA);
-	req.msg.param1 = cpu_to_le16(channel->lcid);
-	req.msg.param2 = cpu_to_le32(intent->id);
-	req.msg.param3 = cpu_to_le32(len);
-	req.msg.param4 = cpu_to_be32(0);
-	memcpy(req.data, data, len);
-
-	mutex_lock(&glink->tx_lock);
-	while (glink_helioscom_tx_avail(glink) < sizeof(req)/WORD_SIZE) {
-
-		if (!wait) {
-			mutex_unlock(&glink->tx_lock);
-			return -EAGAIN;
-		}
-
-		if (atomic_read(&glink->in_reset)) {
-			mutex_unlock(&glink->tx_lock);
-			return -EINVAL;
-		}
-
-		if (!glink->sent_read_notify) {
-			glink->sent_read_notify = true;
-			glink_helioscom_send_read_notify(glink);
-		}
-
-		/* Wait without holding the tx_lock */
-		mutex_unlock(&glink->tx_lock);
-
-		usleep_range(TX_WAIT_US, TX_WAIT_US + 50);
-
-		mutex_lock(&glink->tx_lock);
-
-		if (glink_helioscom_tx_avail(glink) >= sizeof(req)/WORD_SIZE)
-			glink->sent_read_notify = false;
-	}
-	glink_helioscom_tx_write(glink, &req, sizeof(req));
-
-	mutex_unlock(&glink->tx_lock);
-	return 0;
-}
-
 static int glink_helioscom_request_intent(struct glink_helioscom *glink,
 				    struct glink_helioscom_channel *channel,
 				    size_t size)
@@ -815,13 +696,117 @@ unlock:
 	return ret;
 }
 
+static int glink_helioscom_send_final(struct glink_helioscom_channel *channel,
+				void *data, int len,
+				struct glink_helioscom_rx_intent *intent,
+				bool wait)
+{
+	struct glink_helioscom *glink = channel->glink;
+	int size = len;
+	int chunk_size = 0;
+	int left_size = 0;
+	void *short_data;
+	u32 command_size = 0;
+	struct {
+		struct glink_helioscom_msg msg;
+		__le32 chunk_size;
+		__le32 left_size;
+		uint64_t addr;
+	} __packed req_data;
+
+	struct {
+		struct glink_helioscom_msg msg;
+		u8 data[SHORT_SIZE];
+	} __packed req_short;
+
+	memset(&req_data, 0, sizeof(req_data));
+
+	CH_INFO(channel, "size:%d, wait:%d\n", len, wait);
+	if (len <= SHORT_SIZE)
+		size = 0;
+	else if (size & (XPRT_ALIGNMENT - 1))
+		size = ALIGN(len - SHORT_SIZE, XPRT_ALIGNMENT);
+
+	if (size) {
+		chunk_size = size;
+		left_size = len - size;
+
+		if (intent->offset)
+			req_data.msg.cmd = cpu_to_le16(HELIOSCOM_CMD_TX_DATA_CONT);
+		else
+			req_data.msg.cmd = cpu_to_le16(HELIOSCOM_CMD_TX_DATA);
+
+		req_data.msg.param1 = cpu_to_le16(channel->lcid);
+		req_data.msg.param2 = cpu_to_le32(intent->id);
+		req_data.chunk_size = cpu_to_le32(chunk_size);
+		req_data.left_size = cpu_to_le32(left_size);
+		req_data.addr = 0;
+		command_size += sizeof(req_data)/WORD_SIZE;
+	}
+
+	short_data = (char *)data + size;
+	size = len - size;
+	if (size) {
+		req_short.msg.cmd = cpu_to_le16(HELIOSCOM_CMD_TX_SHORT_DATA);
+		req_short.msg.param1 = cpu_to_le16(channel->lcid);
+		req_short.msg.param2 = cpu_to_le32(intent->id);
+		req_short.msg.param3 = cpu_to_le32(size);
+		req_short.msg.param4 = cpu_to_be32(0);
+		memcpy(req_short.data, short_data, size);
+		command_size += sizeof(req_short)/WORD_SIZE;
+	}
+
+	mutex_lock(&glink->tx_lock);
+	while (glink_helioscom_tx_avail(glink) < command_size) {
+		if (!wait) {
+			mutex_unlock(&glink->tx_lock);
+			CH_INFO(channel, "failed, please retry size:%d, wait:%d\n", len, wait);
+			return -EAGAIN;
+		}
+
+		if (atomic_read(&glink->in_reset)) {
+			mutex_unlock(&glink->tx_lock);
+			return -EINVAL;
+		}
+
+		if (!glink->sent_read_notify) {
+			glink->sent_read_notify = true;
+			glink_helioscom_send_read_notify(glink);
+		}
+
+		/* Wait without holding the tx_lock */
+		mutex_unlock(&glink->tx_lock);
+
+		usleep_range(TX_WAIT_US, TX_WAIT_US + 50);
+
+		mutex_lock(&glink->tx_lock);
+
+		if (glink_helioscom_tx_avail(glink) >= command_size)
+			glink->sent_read_notify = false;
+	}
+
+	if (chunk_size) {
+		helioscom_ahb_write(glink->helioscom_handle,
+		(uint32_t)(size_t)(intent->addr + intent->offset),
+		ALIGN(chunk_size, WORD_SIZE)/WORD_SIZE, data);
+
+		intent->offset += chunk_size;
+		glink_helioscom_tx_write(glink, &req_data, sizeof(req_data));
+	}
+
+	if (size)
+		glink_helioscom_tx_write(glink, &req_short, sizeof(req_short));
+
+	mutex_unlock(&glink->tx_lock);
+	return 0;
+}
+
 static int __glink_helioscom_send(struct glink_helioscom_channel *channel,
 			    void *data, int len, bool wait)
 {
 	struct glink_helioscom *glink = channel->glink;
 	struct glink_helioscom_rx_intent *intent = NULL;
 	struct glink_helioscom_rx_intent *tmp;
-	int size = len;
 	int iid = 0;
 	int ret = 0;
 
@@ -859,22 +844,7 @@ static int __glink_helioscom_send(struct glink_helioscom_channel *channel,
 			goto tx_exit;
 	}
 
-	if (len <= SHORT_SIZE)
-		size = 0;
-	else if (size & (XPRT_ALIGNMENT - 1))
-		size = ALIGN(len - SHORT_SIZE, XPRT_ALIGNMENT);
-
-	if (size) {
-		ret = glink_helioscom_send_data(channel, data, size, len - size,
-					  intent, wait);
-		if (ret)
-			goto tx_exit;
-	}
-
-	data = (char *)data + size;
-	size = len - size;
-	if (size)
-		ret = glink_helioscom_send_short(channel, data, size, intent, wait);
+	ret = glink_helioscom_send_final(channel, data, len, intent, wait);
 
 tx_exit:
 	/* Mark intent available if we failed */
