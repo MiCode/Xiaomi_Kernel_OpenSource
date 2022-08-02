ccf0b350059e73584434903068dd7856e4c38660
Eddie Hung <eddie.hung@mediatek.com>
2022-08-02 10:23:09 +0800

    [ALPS07453206] usb: add mtk xhci folder
    Add mtk xhci folder and copy files from path drivers/usb/host/.
    (The last commit is bdcb295df0bba9b2058d81c8650eab5b4f373c3c)
    
    MTK-Commit-Id: bbb9c2e9f1a072c28369fd5b584631c1979cd343
    
    Signed-off-by: Eddie Hung <eddie.hung@mediatek.com>
    CR-Id: ALPS07453206
    Feature: usb_ssusb
    Change-Id: Ie16964cefa7a77d378452d4109cd7dd62641f497

---
+++ drivers/misc/mediatek/usb/usb_xhci/xhci-trace.c
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * xHCI host controller driver
+ *
+ * Copyright (C) 2013 Xenia Ragiadakou
+ * Copyright (C) 2022 MediaTek Inc.
+ *
+ * Author: Xenia Ragiadakou
+ * Email : burzalodowa@gmail.com
+ */
+
+#define CREATE_TRACE_POINTS
+#include "xhci-trace.h"
+
+EXPORT_TRACEPOINT_SYMBOL_GPL(xhci_dbg_quirks);
+EXPORT_TRACEPOINT_SYMBOL_GPL(xhci_urb_enqueue);
+EXPORT_TRACEPOINT_SYMBOL_GPL(xhci_handle_transfer);
+EXPORT_TRACEPOINT_SYMBOL_GPL(xhci_urb_giveback);
