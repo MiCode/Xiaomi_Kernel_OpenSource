1da44acb7f4f7285e8c009e34bdf3ff35a22c980
Harvey Liu <Harvey.Liu@mediatek.com>
2022-08-02 10:22:12 +0800

    [ALPS07415479] Camsys: fix coverity for HSF control
    [Detial]correct coverity check fail
    
    MTK-Commit-Id: 54545e12d47685e917523137a477f8291ec0b80a
    
    CR-Id: ALPS07415479
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: Harvey Liu <Harvey.Liu@mediatek.com>
    Change-Id: Icc5d5858d4a85fb4c45f743dad60a011c6da1758

--- drivers/media/platform/mtk-isp/camsys/isp7s/cam_legacy/mtk_cam-hsf.c
+++ drivers/media/platform/mtk-isp/camsys/isp7s/cam_legacy/mtk_cam-hsf.c
@@ -160,6 +160,11 @@ static void mtk_cam_power_on_ccu(struct mtk_cam_hsf_ctrl *handle_inst, unsigned 
 		get_ccu_device(handle_inst);
 
 	ccu_rproc = rproc_get_by_phandle(handle_inst->ccu_handle);
+	if (!ccu_rproc) {
+		pr_info("error: get ccu rproc failed!\n");
+		return;
+	}
+
 	if (flag > 0) {
 	/* boot up ccu */
 		ret = rproc_boot(ccu_rproc);
@@ -494,7 +499,17 @@ int mtk_cam_hsf_config(struct mtk_cam_ctx *ctx, unsigned int raw_id)
 	dma_map_chk = hsf_config->chk_buf;
 	//TEST: secure iova map to camsys device.
 	dma_map_cq->dbuf = mtk_cam_dmabuf_alloc(ctx, CQ_BUF_SIZE);
+	if (!dma_map_cq->dbuf) {
+		dev_info(cam->dev, "mtk_cam_dmabuf_alloc fail\n");
+		return -1;
+	}
+
 	dev = mtk_cam_find_raw_dev_hsf(cam, ctx->pipe->enabled_raw);
+	if (!dev) {
+		dev_info(cam->dev, "config hsf raw device not found\n");
+		return -1;
+	}
+
 	raw_dev = dev_get_drvdata(dev);
 	dev_info(cam->dev, "mtk_cam_dmabuf_alloc Done\n");
 	if (mtk_cam_dmabuf_get_iova(ctx, &(hsf_config->ccu_pdev->dev), dma_map_cq) != 0) {
