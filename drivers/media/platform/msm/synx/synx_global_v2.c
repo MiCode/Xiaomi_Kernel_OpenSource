abb9fa0b96d22212380154dae2dd6e647e17e0af
Ram Nagesh <quic_ramnages@quicinc.com>
2022-08-07 22:53:21 +0530

    msm: synx: enabling ipclite loopback for SSR
    only incase of SSR,ipclite loopback interrupt will be enabled
    to signal native fences on apps core.
    
    Change-Id: I999bb889002ac7369db13079b7a90b83f8a89ecf
    Signed-off-by: Ram Nagesh <quic_ramnages@quicinc.com>

--- drivers/media/platform/msm/synx/synx_global_v2.c
+++ drivers/media/platform/msm/synx/synx_global_v2.c
@@ -544,7 +544,18 @@ static int synx_global_update_status_core(u32 idx,
 	/* notify waiting clients on signal */
 	if (data) {
 		/* notify wait client */
-		for (i = 1; i < SYNX_CORE_MAX; i++) {
+
+	/* In case of SSR, someone might be waiting on same core
+	 * However, in other cases, synx_signal API will take care
+	 * of signaling handles on same core and thus we don't need
+	 * to send interrupt
+	 */
+		if (status == SYNX_STATE_SIGNALED_SSR)
+			i = 0;
+		else
+			i = 1;
+
+		for (; i < SYNX_CORE_MAX ; i++) {
 			if (!wait_cores[i])
 				continue;
 			dprintk(SYNX_DBG,
