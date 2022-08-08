6bad176b6a7e0fb6310b25532aab25ae42319072
Jairaj Solanki <quic_jsolanki@quicinc.com>
2022-08-08 23:20:45 +0530

    drivers: mailbox: Add compatible string for monaco
    Add "qcom,monaco-apcs-hmss-global" compatibility
    string in mailbox driver to match apcs_glb
    device node.
    
    Change-Id: I240bb180d6d69097c3e432f46837f23dec34a134
    Signed-off-by: Jairaj Solanki <quic_jsolanki@quicinc.com>

--- drivers/mailbox/qcom-apcs-ipc-mailbox.c
+++ drivers/mailbox/qcom-apcs-ipc-mailbox.c
@@ -77,6 +77,10 @@ static const struct qcom_apcs_ipc_data scuba_apcs_data = {
 	.offset = 8, .clk_name = NULL
 };
 
+static const struct qcom_apcs_ipc_data monaco_apcs_data = {
+	.offset = 8, .clk_name = NULL
+};
+
 static const struct regmap_config apcs_regmap_config = {
 	.reg_bits = 32,
 	.reg_stride = 4,
@@ -176,6 +180,7 @@ static const struct of_device_id qcom_apcs_ipc_of_match[] = {
 	{ .compatible = "qcom,msm8996-apcs-hmss-global", .data = &msm8996_apcs_data },
 	{ .compatible = "qcom,msm8998-apcs-hmss-global", .data = &msm8998_apcs_data },
 	{ .compatible = "qcom,qcs404-apcs-apps-global", .data = &msm8916_apcs_data },
+	{ .compatible = "qcom,monaco-apcs-hmss-global", .data = &monaco_apcs_data  },
 	{ .compatible = "qcom,sc7180-apss-shared", .data = &apps_shared_apcs_data },
 	{ .compatible = "qcom,sc8180x-apss-shared", .data = &apps_shared_apcs_data },
 	{ .compatible = "qcom,sdm660-apcs-hmss-global", .data = &sdm660_apcs_data },
