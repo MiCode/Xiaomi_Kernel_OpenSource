8b06e8e19f32d0de06148b4f078ae8b8cf949e37
Jeremy Chou <jeremy.chou@mediatek.com>
2022-08-09 17:39:13 +0800

    [ALPS07495334] USB_OFFLOAD: add role_mode for USB offload
    [Detail]
    1. add role_mode for USB offload check during power-on for reconnect
    
    MTK-Commit-Id: 82e186396159e45439251fc16005c5f92c2209fe
    
    Signed-off-by: Jeremy Chou <jeremy.chou@mediatek.com>
    CR-Id: ALPS07495334
    Feature: USB type C audio offload
    Change-Id: Idc0ef489a157a5676d51f9404eb6e3e2d7a8ea2e

--- drivers/usb/mtu3/mtu3_dr.c
+++ drivers/usb/mtu3/mtu3_dr.c
@@ -533,6 +533,17 @@ static ssize_t mode_show(struct device *dev,
 }
 static DEVICE_ATTR_RW(mode);
 
+static ssize_t role_mode_show(struct device *dev,
+				struct device_attribute *attr,
+				char *buf)
+{
+	struct ssusb_mtk *ssusb = dev_get_drvdata(dev);
+	struct otg_switch_mtk *otg_sx = &ssusb->otg_switch;
+
+	return sprintf(buf, "%d\n", otg_sx->current_role);
+}
+static DEVICE_ATTR_RO(role_mode);
+
 static ssize_t max_speed_store(struct device *dev,
 				 struct device_attribute *attr,
 				 const char *buf, size_t count)
@@ -605,6 +616,7 @@ static DEVICE_ATTR_RW(saving);
 
 static struct attribute *ssusb_dr_attrs[] = {
 	&dev_attr_mode.attr,
+	&dev_attr_role_mode.attr,
 	&dev_attr_max_speed.attr,
 	&dev_attr_saving.attr,
 	NULL
