bcd9c5ca914e24d78e7c2e4bf9ee228067f89f02
allan-yr <Yu-Ren.Yang@mediatek.com>
2022-08-14 23:27:20 +0800

    [ALPS07478949] ISP P1: fixed Security Vulnerability
    [Detial]
    	add dma_heap_put
    
    MTK-Commit-Id: 6c6297b10200427c159abb0ace8c03a985fe828a
    
    Signed-off-by: allan-yr <Yu-Ren.Yang@mediatek.com>
    CR-Id: ALPS07478949
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: If2972ce4b05394adf3bd53d82c950e5fe6d0b058

--- drivers/media/platform/mtk-isp/camsys/isp7s/cam_legacy/mtk_cam-hsf.c
+++ drivers/media/platform/mtk-isp/camsys/isp7s/cam_legacy/mtk_cam-hsf.c
@@ -56,6 +56,8 @@ struct dma_buf *mtk_cam_dmabuf_alloc(struct mtk_cam_ctx *ctx, unsigned int size)
 
 	dbuf = dma_heap_buffer_alloc(heap_type, size,
 		O_CLOEXEC | O_RDWR, DMA_HEAP_VALID_HEAP_FLAGS);
+
+	dma_heap_put(heap_type);
 	if (IS_ERR(dbuf)) {
 		dev_info(cam->dev, "region-based hsf buffer allocation fail\n");
 		return NULL;
