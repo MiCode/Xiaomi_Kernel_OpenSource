6759e5ca02c57d87bbc424a2da537907c0eb17ad
Elliot Berman <quic_eberman@quicinc.com>
2022-08-17 10:30:58 -0700

    ANDROID: Use rq_clock_task without CONFIG_SMP
    Fix build error caused by guarding rq_clock_task_mult with CONFIG_SMP.
    
    Issue was not seen in original commit as test case was UM Linux, which
    does not enable CONFIG_CFS_BANDWIDTH.
    
    >> kernel/sched/fair.c:4791:40: error: implicit declaration of function 'rq_clock_task_mult' [-Werror,-Wimplicit-function-declaration]
                       cfs_rq->throttled_clock_task_time += rq_clock_task_mult(rq) -
    
    Change-Id: Id3392c37ba1b2bfe78c89bc4b1ca01b920164a35
    Reported-by: kernel test robot <lkp@intel.com>
    Fixes: 567d65e53644 ("ANDROID: Guard rq_clock_task_mult with CONFIG_SMP")
    Signed-off-by: Elliot Berman <quic_eberman@quicinc.com>
    (cherry picked from commit 04c766fa7690fc18178eb78293a733630789c138)

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1203,6 +1203,11 @@ static inline u64 rq_clock_task_mult(struct rq *rq)
 
 	return per_cpu(clock_task_mult, rq->cpu);
 }
+#else
+static inline u64 rq_clock_task_mult(struct rq *rq)
+{
+	return rq_clock_task(rq);
+}
 #endif
 
 /**
