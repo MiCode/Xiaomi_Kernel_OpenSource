3269754c4ea179d98f904a508660f2cdef3d6092
Veera Vegivada <quic_vvegivad@quicinc.com>
2022-08-17 12:10:40 +0530

    icc: dt-bindings: add endpoint IDs for interconnects for SA410M
    Add master and slave ID constants for all Qualcomm Technologies, Inc.
    SA410M interconnect providers which consumers can use to set bandwidth
    constraints and find paths in the NoC (Network-On-Chip) topology.
    
    Change-Id: Ic20fc738f6189661f5b96be1d0a1ba2f8e15757e
    Signed-off-by: Veera Vegivada <quic_vvegivad@quicinc.com>

---
+++ include/dt-bindings/interconnect/qcom,sa410m.h
@@ -0,0 +1,94 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#ifndef __DT_BINDINGS_INTERCONNECT_QCOM_SA410M_H
+#define __DT_BINDINGS_INTERCONNECT_QCOM_SA410M_H
+
+#define MASTER_AMPSS_M0				0
+#define MASTER_SNOC_BIMC_NRT				1
+#define SNOC_BIMC_MAS				2
+#define MASTER_TCU_0				3
+#define MASTER_HWKM_CORE				4
+#define MASTER_PKA_CORE				5
+#define MASTER_QUP_CORE_0				6
+#define SNOC_CNOC_MAS				7
+#define MASTER_QDSS_DAP				8
+#define MASTER_SNOC_CFG				9
+#define MASTER_TIC				10
+#define MASTER_ANOC_SNOC				11
+#define MASTER_BIMC_SNOC_PCIE				12
+#define BIMC_SNOC_MAS				13
+#define MASTER_PIMEM				14
+#define MASTER_CRYPTO_CORE0				15
+#define MASTER_QDSS_BAM				16
+#define MASTER_QPIC				17
+#define MASTER_QUP_0				18
+#define CNOC_SNOC_MAS				19
+#define MASTER_IPA				20
+#define MASTER_EMAC				21
+#define MASTER_PCIE				22
+#define MASTER_QDSS_ETR				23
+#define MASTER_SDCC_1				24
+#define MASTER_SDCC_2				25
+#define MASTER_USB3				26
+#define SLAVE_EBI_CH0				512
+#define BIMC_SNOC_SLV				513
+#define SLAVE_BIMC_SNOC_PCIE				514
+#define SLAVE_HWKM_CORE				515
+#define SLAVE_PKA_CORE				516
+#define SLAVE_QUP_CORE_0				517
+#define SLAVE_AHB2PHY_USB				518
+#define SLAVE_APSS_THROTTLE_CFG				519
+#define SLAVE_BIMC_CFG				520
+#define SLAVE_APPSS				521
+#define SLAVE_CLK_CTL				522
+#define SLAVE_RBCPR_CX_CFG				523
+#define SLAVE_RBCPR_MX_CFG				524
+#define SLAVE_CRYPTO_0_CFG				525
+#define SLAVE_DCC_CFG				526
+#define SLAVE_DDR_PHY_CFG				527
+#define SLAVE_DDR_SS_CFG				528
+#define SLAVE_EMAC_CFG				529
+#define SLAVE_HWKM				530
+#define SLAVE_IMEM_CFG				531
+#define SLAVE_IPA_CFG				532
+#define SLAVE_LPASS				533
+#define SLAVE_MAPSS				534
+#define SLAVE_MDSP_MPU_CFG				535
+#define SLAVE_MESSAGE_RAM				536
+#define SLAVE_CNOC_MSS				537
+#define SLAVE_PCIE_0_CFG				538
+#define SLAVE_PDM				539
+#define SLAVE_PIMEM_CFG				540
+#define SLAVE_PKA_WRAPPER_CFG				541
+#define SLAVE_PMIC_ARB				542
+#define SLAVE_QDSS_CFG				543
+#define SLAVE_QM_CFG				544
+#define SLAVE_QM_MPU_CFG				545
+#define SLAVE_QPIC				546
+#define SLAVE_QUP_0				547
+#define SLAVE_RPM				548
+#define SLAVE_SDCC_1				549
+#define SLAVE_SDCC_2				550
+#define SLAVE_SECURITY				551
+#define SLAVE_SNOC_CFG				552
+#define SLAVE_TCSR				553
+#define SLAVE_TLMM				554
+#define SLAVE_USB3				555
+#define CNOC_SNOC_SLV				556
+#define SLAVE_SERVICE_CNOC				557
+#define SLAVE_TCU				558
+#define SLAVE_BOOT_ROM				559
+#define SNOC_CNOC_SLV				560
+#define SLAVE_OCIMEM				561
+#define SLAVE_PIMEM				562
+#define SNOC_BIMC_SLV				563
+#define SLAVE_SERVICE_SNOC				564
+#define SLAVE_PCIE_0				565
+#define SLAVE_QDSS_STM				566
+#define SLAVE_ANOC_SNOC				567
+#define SLAVE_SNOC_BIMC_NRT				568
+
+#endif
