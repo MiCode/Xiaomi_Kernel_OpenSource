e6c826aa9b62e5e5b9556abdcf97dc77a81f422a
Todd Kjos <tkjos@google.com>
2022-08-19 22:25:17 +0000

    ANDROID: reduce number of opaque structs in KMI
    Change how vendor hooks are instantiated to promote
    more complete structure definition in the ABI XML
    description without complicating hook definition for
    partners.
    
    We don't want to force partners to include all
    headers as part of the hook definition in include/trace/hooks/
    since that causes extra headers to be included in
    source files that runs the risk of changing visibility
    resulting in CRC changes to KMI symbols. Instead continue
    the practice of using forward declarations in the hook
    header files.
    
    Instead of instantiating hook tracepoints globally in
    drivers/android/vendor_hooks.c, use subsystem-specific
    vendor_hooks.c. This avoids namespace collisions between
    internal header files and limits the exposure to the internal
    headers to the instantiation, not the call sites.
    
    In this patch, all of the scheduler related hooks are
    instantiated in kernel/sched/vendor_hooks.c which can
    cleanly include scheduler-related header files to
    provide full type visibility.
    
    Since this is being done on a KMI frozen branch, there
    are some __GENKSYMS__ tricks needed to preserve the
    existing CRC as we increase the number of fully typed
    structs. These tricks won't be needed when this is
    introduced on a non-KMI-frozen branch.
    
    With this patch, the number of undefined types was
    reduced from 108 to 68.
    
    Bug: 233047575
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Change-Id: Ife5a66c2968de73e3f6d05840411310611e2e175

--- include/trace/hooks/cgroup.h
+++ include/trace/hooks/cgroup.h
@@ -46,6 +46,18 @@ struct page_counter;
 DECLARE_HOOK(android_rvh_update_watermark,
 	TP_PROTO(u64 new, struct page_counter *counter),
 	TP_ARGS(new, counter));
+
+DECLARE_RESTRICTED_HOOK(android_rvh_cpu_cgroup_attach,
+	TP_PROTO(struct cgroup_taskset *tset),
+	TP_ARGS(tset), 1);
+
+DECLARE_RESTRICTED_HOOK(android_rvh_cpu_cgroup_can_attach,
+	TP_PROTO(struct cgroup_taskset *tset, int *retval),
+	TP_ARGS(tset, retval), 1);
+
+DECLARE_RESTRICTED_HOOK(android_rvh_cpu_cgroup_online,
+	TP_PROTO(struct cgroup_subsys_state *css),
+	TP_ARGS(css), 1);
 #endif
 
 #include <trace/define_trace.h>
