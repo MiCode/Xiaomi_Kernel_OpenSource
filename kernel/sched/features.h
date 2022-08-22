31eddad3a44dc6d85a07db1e949dc2687399aa6d
Greg Kroah-Hartman <gregkh@google.com>
2022-08-22 21:39:50 +0200

    Revert "sched/fair: Introduce SIS_UTIL to search idle CPU based on sum of util_avg"
    This reverts commit 079651c6cfdc3e89577697098fdd96d70a72b405 which is
    commit 70fb5ccf2ebb09a0c8ebba775041567812d45f86 upstream.
    
    It breaks the Android GKI kernel abi, and is not needed for Android
    devices, so revert it for now.  If it is needed for this branch, it can
    come back later in an ABI-stable way.
    
    Bug: 161946584
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Ie4a8739747859bfd57e48a2745b7fcf90233fd6c

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -55,8 +55,7 @@ SCHED_FEAT(TTWU_QUEUE, true)
 /*
  * When doing wakeups, attempt to limit superfluous scans of the LLC domain.
  */
-SCHED_FEAT(SIS_PROP, false)
-SCHED_FEAT(SIS_UTIL, true)
+SCHED_FEAT(SIS_PROP, true)
 
 /*
  * Issue a WARN when we do multiple update_rq_clock() calls
