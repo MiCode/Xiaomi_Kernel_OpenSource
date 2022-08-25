49817a8c24fe1c5a592a9bf39f3f26bace9bfe0a
Visweswara Tanuku <quic_vtanuku@quicinc.com>
2022-08-25 09:28:33 -0700

    serial: msm_geni_serial: Do not enable wakeup_irq if port is closed
    After serial driver is registered via uart_add_one_port,
    pm framework invokes runtime_resume and runtime_suspend.
    As part of runtime_suspend wakeup_irq is enabled while
    hs-uart port is still not open.
    
    Since port is in closed state, due to spurious wakeup_up
    interrupts wakeup_isr is called multiple times and
    watchdog bark is seen due to excessive logging by
    WARN_ON in wakeup_isr.
    
    In runtime_suspend do not enable wakeup_irq if port
    is closed to prevent servicing spurious interrupts.
    
    Change-Id: I874613b3f837674d0c8be369ad892038a169d293
    Signed-off-by: Visweswara Tanuku <quic_vtanuku@quicinc.com>

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -3936,7 +3936,8 @@ static int msm_geni_serial_runtime_suspend(struct device *dev)
 		goto exit_runtime_suspend;
 	}
 
-	if (port->wakeup_irq > 0) {
+	/* Check if port is in open state before enabling wakeup_irq */
+	if (port->wakeup_irq > 0 && port->uport.state->port.tty) {
 		atomic_set(&port->check_wakeup_byte, 0);
 		enable_irq(port->wakeup_irq);
 		port->wakeup_enabled = true;
