ba9c2bab0b97848272841f9d9f5cb0bbf9c6833e
V S Ganga VaraPrasad (VARA) Adabala <quic_vadabala@quicinc.com>
2022-09-07 16:43:28 +0530

    Revert "ANDROID: GKI: vendor code needs __balance_callbacks access"
    This reverts commit d7670b9847c94bf4c72b3e718f12e69c40446676.
    
    Change-Id: I05e5c4b3467eb0df5b77a3a9436a876d4e874080
    Signed-off-by: V S Ganga VaraPrasad (VARA) Adabala <quic_vadabala@quicinc.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4794,11 +4794,10 @@ static inline struct callback_head *splice_balance_callbacks(struct rq *rq)
 	return __splice_balance_callbacks(rq, true);
 }
 
-void __balance_callbacks(struct rq *rq)
+static void __balance_callbacks(struct rq *rq)
 {
 	do_balance_callbacks(rq, __splice_balance_callbacks(rq, false));
 }
-EXPORT_SYMBOL_GPL(__balance_callbacks);
 
 static inline void balance_callbacks(struct rq *rq, struct callback_head *head)
 {
