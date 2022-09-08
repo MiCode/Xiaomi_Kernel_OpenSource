dd37502e09368ed6d27d9c5a41531440e770af3e
“jayce.hsu” <jayce.hsu@mediatek.com>
2022-09-08 13:16:25 +0800

    [ALPS07530950] cmdq: get cmdq thrd num by platform
    get cmdq thrd num by platform.
    
    MTK-Commit-Id: b91fa89f77000c9edb61de163d2217987e3cb3b4
    
    Signed-off-by: “jayce.hsu” <jayce.hsu@mediatek.com>
    CR-Id: ALPS07530950
    Feature: cmdq_gce
    Change-Id: I925229b42ea1037bae47809f894c218cc1ce8555

--- drivers/misc/mediatek/cmdq/mailbox/mtk-cmdq-mailbox-ext.c
+++ drivers/misc/mediatek/cmdq/mailbox/mtk-cmdq-mailbox-ext.c
@@ -45,7 +45,7 @@ cmdq_mminfra_gce_cg mminfra_gce_cg;
 
 /* ddp main/sub, mdp path 0/1/2/3, general(misc) */
 #define CMDQ_OP_CODE_MASK		(0xff << CMDQ_OP_CODE_SHIFT)
-#define CMDQ_IRQ_MASK			GENMASK(CMDQ_THR_MAX_COUNT - 1, 0)
+#define CMDQ_IRQ_MASK			GENMASK(gce_thread_nr - 1, 0)
 
 #define CMDQ_CORE_REST			0x0
 #define CMDQ_CURR_IRQ_STATUS		0x10
@@ -119,6 +119,9 @@ cmdq_mminfra_gce_cg mminfra_gce_cg;
 /* pc and end shift bit for gce, should be config in probe */
 int gce_shift_bit;
 EXPORT_SYMBOL(gce_shift_bit);
+
+u32 gce_thread_nr;
+EXPORT_SYMBOL(gce_thread_nr);
 
 int gce_mminfra;
 EXPORT_SYMBOL(gce_mminfra);
@@ -1244,7 +1247,7 @@ static irqreturn_t cmdq_irq_handler(int irq, void *dev)
 #if IS_ENABLED(CONFIG_MTK_IRQ_MONITOR_DEBUG)
 	end[end_cnt++] = sched_clock();
 #endif
-	set_bit(CMDQ_THR_MAX_COUNT, &cmdq->err_irq_idx);
+	set_bit(gce_thread_nr, &cmdq->err_irq_idx);
 	wake_up_interruptible(&cmdq->err_irq_wq);
 #if IS_ENABLED(CONFIG_MTK_IRQ_MONITOR_DEBUG)
 	end[end_cnt] = sched_clock();
@@ -1293,7 +1296,7 @@ static int cmdq_irq_handler_thread(void *data)
 		wait_event_interruptible(cmdq->err_irq_wq, cmdq->err_irq_idx);
 		irq = cmdq->err_irq_idx;
 
-		if (irq & BIT(CMDQ_THR_MAX_COUNT)) {
+		if (irq & BIT(gce_thread_nr)) {
 			struct cmdq_task *task, *tmp;
 
 			spin_lock_irqsave(&cmdq->irq_removes_lock, flags);
@@ -1309,8 +1312,8 @@ static int cmdq_irq_handler_thread(void *data)
 			}
 			spin_unlock_irqrestore(&cmdq->irq_removes_lock, flags);
 
-			clear_bit(CMDQ_THR_MAX_COUNT, &cmdq->err_irq_idx);
-			if (irq == BIT(CMDQ_THR_MAX_COUNT))
+			clear_bit(gce_thread_nr, &cmdq->err_irq_idx);
+			if (irq == BIT(gce_thread_nr))
 				continue;
 		}
 
@@ -2281,6 +2284,7 @@ static int cmdq_probe(struct platform_device *pdev)
 
 	gce_shift_bit = plat_data->shift;
 	gce_mminfra = plat_data->mminfra;
+	gce_thread_nr = plat_data->thread_nr;
 	if (of_property_read_bool(dev->of_node, "skip-poll-sleep"))
 		skip_poll_sleep = true;
 
