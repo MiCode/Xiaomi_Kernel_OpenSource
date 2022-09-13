59ad36630843daa20e90819f849167ab122db128
Subbaraman Narayanamurthy <quic_subbaram@quicinc.com>
2022-09-12 21:04:40 -0700

    remoteproc: qcom: Fix qcom_tracepoints compilation
    This reverts commit b5fafafaffe3 ("drivers: remoteproc: Disable
    qcom common tracepoints if not a module") which attempted fixing
    a linker error when rproc_qcom_common module was compiled
    statically.
    
    Since "trace/events/rproc_qcom.h" is included in several files
    and CREATE_TRACE_POINTS is defined in qcom_tracepoints.c, having
    another #define CREATE_TRACE_POINTS in qcom_q6v5_pas.c is breaking
    the compilation. Remove it.
    
    Fixes: b5fafafaffe3 ("drivers: remoteproc: Disable qcom common tracepoints if not a module")
    Change-Id: Ia84412840764eed1debfb581e7f25cafe5666075
    Signed-off-by: Subbaraman Narayanamurthy <quic_subbaram@quicinc.com>

--- drivers/remoteproc/qcom_tracepoints.c
+++ drivers/remoteproc/qcom_tracepoints.c
@@ -5,8 +5,6 @@
  * Copyright (c) 2021 The Linux Foundation. All rights reserved.
  */
 
-#if IS_MODULE(CONFIG_QCOM_RPROC_COMMON)
 #define CREATE_TRACE_POINTS
 #include <trace/events/rproc_qcom.h>
 EXPORT_TRACEPOINT_SYMBOL(rproc_qcom_event);
-#endif
