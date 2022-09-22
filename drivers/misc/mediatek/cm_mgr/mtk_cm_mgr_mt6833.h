45e770e58f3e872a26eea8ed985962f132326032
anjali.agrahri <anjali.agrahri@mediatek.com>
2022-09-22 12:04:24 +0800

    [ALPS06807359] cm_mgr mt6833 GKI 510
    cm_mgr porting for mt6833 GKI 5.10
    
    MTK-Commit-Id: f743fad506dc00f6e7caf177507852dce0edb4c2
    
    Change-Id: I43c09cc1c2f6996f70cffb450c25f91883ad6ddc
    CR-Id: ALPS06807359
    Feature: cgm_kernel
    Signed-off-by: anjali.agrahri<anjali.agrahri@mediatek.com>
    (cherry picked from commit b1aad0877896627c1900abae712e72812df5a8f9)

---
+++ drivers/misc/mediatek/cm_mgr/mtk_cm_mgr_mt6833.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+
+#ifndef __MTK_CM_MGR_PLATFORM_H__
+#define __MTK_CM_MGR_PLATFORM_H__
+
+#include "mtk_cm_mgr_reg_mt6833.h"
+
+#define CREATE_TRACE_POINTS
+#include "mtk_cm_mgr_events_mt6833.h"
+
+#if IS_ENABLED(CONFIG_MTK_DRAMC)
+#include <soc/mediatek/dramc.h>
+#endif /* CONFIG_MTK_DRAMC */
+
+#define CM_MGR_CPU_CLUSTER 2
+
+enum {
+	CM_MGR_LP4 = 0,
+	CM_MGR_MAX,
+};
+
+#endif	/* __MTK_CM_MGR_PLATFORM_H__ */
