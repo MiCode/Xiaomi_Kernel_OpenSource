1e2caea124545f221099e5713070561b7f4b378c
Siva Mullati <quic_smullati@quicinc.com>
2022-09-26 00:43:38 -0700

    msm: kgsl: Enable hwsched feature for gen7_6_0 GPU
    Enable hwsched feature for gen7_6_0 GPU.
    
    Change-Id: I0e31a64753a117073448e16f98e3fa3cc99177b9
    Signed-off-by: Siva Mullati <quic_smullati@quicinc.com>

--- drivers/gpu/msm/adreno-gpulist.h
+++ drivers/gpu/msm/adreno-gpulist.h
@@ -2179,7 +2179,7 @@ static const struct adreno_gen7_core adreno_gpu_core_gen7_6_0 = {
 		.features = ADRENO_APRIV | ADRENO_IOCOHERENT |
 				ADRENO_CONTENT_PROTECTION | ADRENO_L3_VOTE |
 				ADRENO_IFPC | ADRENO_PREEMPTION | ADRENO_BCL,
-		.gpudev = &adreno_gen7_gmu_gpudev.base,
+		.gpudev = &adreno_gen7_hwsched_gpudev.base,
 		.perfcounters = &adreno_gen7_6_0_perfcounters,
 		.uche_gmem_alignment = SZ_16M,
 		.gmem_size = 3 * SZ_1M,
