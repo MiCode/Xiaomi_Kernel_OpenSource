99e3951ea46cbc90fa412cd7f12630e4a610fb3f
Pavankumar Kondeti <quic_pkondeti@quicinc.com>
2022-09-26 18:36:10 +0530

    ANDROID: vendor_hooks: Allow shared pages reclaim via MADV_PAGEOUT
    Add a hook in madvise_cold_or_pageout_pte_range() to allow
    vendor modules to influence the shared pages reclaim.
    
    Bug: 242678506
    Change-Id: I269a385b59f7291c2e96478674bb3d05f94584cb
    Signed-off-by: Pavankumar Kondeti <quic_pkondeti@quicinc.com>

--- mm/madvise.c
+++ mm/madvise.c
@@ -31,6 +31,7 @@
 #include <linux/swapops.h>
 #include <linux/shmem_fs.h>
 #include <linux/mmu_notifier.h>
+#include <trace/hooks/mm.h>
 
 #include <asm/tlb.h>
 
@@ -324,10 +325,12 @@ static int madvise_cold_or_pageout_pte_range(pmd_t *pmd,
 	spinlock_t *ptl;
 	struct page *page = NULL;
 	LIST_HEAD(page_list);
+	bool allow_shared = false;
 
 	if (fatal_signal_pending(current))
 		return -EINTR;
 
+	trace_android_vh_madvise_cold_or_pageout(vma, &allow_shared);
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 	if (pmd_trans_huge(*pmd)) {
 		pmd_t orig_pmd;
@@ -443,7 +446,7 @@ regular_page:
 		}
 
 		/* Do not interfere with other mappings of this page */
-		if (page_mapcount(page) != 1)
+		if (!allow_shared && page_mapcount(page) != 1)
 			continue;
 
 		VM_BUG_ON_PAGE(PageTransCompound(page), page);
