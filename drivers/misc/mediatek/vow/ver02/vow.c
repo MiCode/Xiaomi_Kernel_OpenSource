b0c7d45517bda24cdfe2d2b89c31c528bc14f1ed
TT Kuang <Tt.Kuang@mediatek.com>
2022-09-29 23:24:55 +0800

    [ALPS07525053] VoW: Fix bug when vow + usnd + scp reset
    send model status before check vow hw is on
    
    MTK-Commit-Id: ad70b6c5d3076f096ae42d4dc8d536de16f9f7d3
    
    Signed-off-by: TT Kuang <Tt.Kuang@mediatek.com>
    CR-Id: ALPS07525053
    Feature: VOW Kernel
    Change-Id: Iea6a4ba0c8f6b65be9a69ae956f22ddb817234f8

--- drivers/misc/mediatek/vow/ver02/vow.c
+++ drivers/misc/mediatek/vow/ver02/vow.c
@@ -3194,6 +3194,14 @@ static int vow_scp_recover_event(struct notifier_block *this,
 		if (!ret)
 			VOWDRV_DEBUG("fail: vow_service_SetCustomModel\n");
 
+		for (I = 0; I < MAX_VOW_SPEAKER_MODEL; I++) {
+			if (vowserv.vow_speaker_model[I].enabled) {
+				vow_service_SendModelStatus(
+					I, VOW_MODEL_STATUS_START);
+				VOWDRV_DEBUG("send Model start, slot%d\n", I);
+			}
+		}
+
 		/* if vow is not enable, then return */
 		if (VowDrv_GetHWStatus() != VOW_PWR_ON) {
 			vowserv.vow_recovering = false;
@@ -3228,14 +3236,6 @@ static int vow_scp_recover_event(struct notifier_block *this,
 
 		if (!vow_service_Enable())
 			VOWDRV_DEBUG("fail: vow_service_Enable\n");
-
-		for (I = 0; I < MAX_VOW_SPEAKER_MODEL; I++) {
-			if (vowserv.vow_speaker_model[I].enabled) {
-				vow_service_SendModelStatus(
-					I, VOW_MODEL_STATUS_START);
-				VOWDRV_DEBUG("send Model start, slot%d\n", I);
-			}
-		}
 
 		vowserv.vow_recovering = false;
 		break;
