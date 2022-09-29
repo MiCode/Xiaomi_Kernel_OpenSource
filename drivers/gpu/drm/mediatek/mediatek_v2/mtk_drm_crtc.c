5990c9b32ede21006560adbf2531eea41cfe34f5
Chao Hao <chao.hao@mediatek.com>
2022-09-29 23:28:54 +0800

    [ALPS07563180] mt6886: disp: remove disp log
    Remove cmdq dump pkt log to avoid HWT issue because of in irq context
    
    MTK-Commit-Id: 2aa637077f0fcfdc9d7c87eb6331170062a48397
    
    Signed-off-by: Chao Hao <chao.hao@mediatek.com>
    CR-Id: ALPS07563180
    Feature: display_mobile
    Change-Id: I011a1d23584188bc4cf7f68b02086ffdf53e1d50

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
@@ -5551,7 +5551,6 @@ static void ddp_cmdq_cb(struct cmdq_cb_data data)
 			} else {
 				mtk_drm_crtc_analysis(crtc);
 				mtk_drm_crtc_dump(crtc);
-				cmdq_dump_pkt(cb_data->cmdq_handle, 0, true);
 			}
 		}
 		/*Msync 2.0 related function*/
