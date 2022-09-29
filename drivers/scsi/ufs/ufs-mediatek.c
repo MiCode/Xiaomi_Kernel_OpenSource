044ab52df554ef99b1a27386515878426dad05db
Peter Wang <peter.wang@mediatek.com>
2022-09-29 23:36:46 +0800

    [ALPS07589511] ufs: mediatek: tx skew fix
    Mediatek tx skew issue fix by check dts setting and vendor/model.
    Then set PA_TACTIVATE set 8.
    
    MTK-Commit-Id: 33ca4f1c6dfa92657496ae0687126f6b44ca6bb4
    
    Signed-off-by: Peter Wang <peter.wang@mediatek.com>
    CR-Id: ALPS07589511
    Feature: ufs
    Change-Id: I924542237cc6a57c9e81f30cad8af0defb286903

--- drivers/scsi/ufs/ufs-mediatek.c
+++ drivers/scsi/ufs/ufs-mediatek.c
@@ -169,6 +169,13 @@ static bool ufs_mtk_is_pmc_via_fastauto(struct ufs_hba *hba)
 	return (host->caps & UFS_MTK_CAP_PMC_VIA_FASTAUTO);
 }
 
+static bool ufs_mtk_is_tx_skew_fix(struct ufs_hba *hba)
+{
+	struct ufs_mtk_host *host = ufshcd_get_variant(hba);
+
+	return !!(host->caps & UFS_MTK_CAP_TX_SKEW_FIX);
+}
+
 static bool ufs_mtk_is_clk_scale_ready(struct ufs_hba *hba)
 {
 	struct ufs_mtk_host *host = ufshcd_get_variant(hba);
@@ -759,6 +766,9 @@ static void ufs_mtk_init_host_caps(struct ufs_hba *hba)
 
 	if (of_property_read_bool(np, "mediatek,ufs-pmc-via-fastauto"))
 		host->caps |= UFS_MTK_CAP_PMC_VIA_FASTAUTO;
+
+	if (of_property_read_bool(np, "mediatek,ufs-tx-skew-fix"))
+		host->caps |= UFS_MTK_CAP_TX_SKEW_FIX;
 
 #if IS_ENABLED(CONFIG_MTK_UFS_DEBUG)
 	if (of_property_read_bool(np, "mediatek,ufs-mphy-debug"))
@@ -2751,6 +2761,14 @@ static int ufs_mtk_apply_dev_quirks(struct ufs_hba *hba)
 	if (mid == UFS_VENDOR_SAMSUNG) {
 		ufshcd_dme_set(hba, UIC_ARG_MIB(PA_TACTIVATE), 6);
 		ufshcd_dme_set(hba, UIC_ARG_MIB(PA_HIBERN8TIME), 10);
+	} else if (mid == UFS_VENDOR_MICRON) {
+		/* Only for the host which have TX skew issue */
+		if (ufs_mtk_is_tx_skew_fix(hba) &&
+			(STR_PRFX_EQUAL("MT128GBCAV2U31", dev_info->model) ||
+			STR_PRFX_EQUAL("MT256GBCAV4U31", dev_info->model) ||
+			STR_PRFX_EQUAL("MT512GBCAV8U31", dev_info->model))) {
+			ufshcd_dme_set(hba, UIC_ARG_MIB(PA_TACTIVATE), 8);
+		}
 	}
 
 	/*
