3a23a6054188d4afdc0aeb0b7586f72f2aa34bae
tai-hua.tseng <tai-hua.tseng@mediatek.com>
2022-09-29 23:38:31 +0800

    [ALPS07630937] DISP: Fix Security Vulnerability issue
    [Detail]
    only support IOCTL MTK_FACTORY_LCM_AUTO_TEST in eng/userdebug load
    
    MTK-Commit-Id: 78b00bec5d2100ad4adc0b3258cc2d7696f95045
    
    CR-Id: ALPS07630937
    Feature: display_mobile
    Signed-off-by: tai-hua.tseng <tai-hua.tseng@mediatek.com>
    Change-Id: I0e1d8059c8f28a0d72353e61323a83c69b70c8de
    (cherry-pick from 524916c4b526e34014824fb0c87eed0789df721e)

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_drv.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_drv.c
@@ -5723,6 +5723,7 @@ static void mtk_drm_kms_deinit(struct drm_device *drm)
 	}
 }
 
+#if IS_ENABLED(CONFIG_DEBUG_FS)
 int mtk_drm_fm_lcm_auto_test(struct drm_device *dev, void *data,
 			 struct drm_file *file_priv)
 {
@@ -5773,6 +5774,7 @@ int mtk_drm_fm_lcm_auto_test(struct drm_device *dev, void *data,
 	}
 	return 0;
 }
+#endif
 
 static const struct drm_ioctl_desc mtk_ioctls[] = {
 	DRM_IOCTL_DEF_DRV(MTK_GEM_CREATE, mtk_gem_create_ioctl,
@@ -5901,8 +5903,10 @@ static const struct drm_ioctl_desc mtk_ioctls[] = {
 			  DRM_UNLOCKED),
 	DRM_IOCTL_DEF_DRV(MTK_BYPASS_DISP_GAMMA, mtk_drm_ioctl_bypass_disp_gamma,
 		DRM_UNLOCKED),
+#if IS_ENABLED(CONFIG_DEBUG_FS)
 	DRM_IOCTL_DEF_DRV(MTK_FACTORY_LCM_AUTO_TEST, mtk_drm_fm_lcm_auto_test,
 			  DRM_UNLOCKED),
+#endif
 	DRM_IOCTL_DEF_DRV(MTK_GET_PQ_CAPS, mtk_drm_ioctl_get_pq_caps,
 			  DRM_UNLOCKED),
 	DRM_IOCTL_DEF_DRV(MTK_SET_PQ_CAPS, mtk_drm_ioctl_set_pq_caps,
