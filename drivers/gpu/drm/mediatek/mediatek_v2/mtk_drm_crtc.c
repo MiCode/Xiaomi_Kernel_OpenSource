83fc9f12d1f061f8700e9bfa90cd26d53e223501
neil.yu <neil.yu@mediatek.com>
2022-09-29 23:33:12 +0800

    [ALPS07580930] disp: fix coverity
    [Detail]
    Add NULL checking
    
    MTK-Commit-Id: 5252f487b77d0a16c4ab8abd36cb4d398ee971a8
    
    Signed-off-by: neil.yu <neil.yu@mediatek.com>
    CR-Id: ALPS07580930
    Feature: display_mobile
    Change-Id: I83c4a47a2e7d966d35375b8ed3efff68ee246723

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
@@ -4239,7 +4239,10 @@ void mtk_crtc_ovl_connect_change(struct drm_crtc *crtc, unsigned int ovl_res,
 
 	for (i = 0 ; i < mtk_crtc->dual_pipe_ddp_ctx.ovl_comp_nr[DDP_FIRST_PATH] ; ++i) {
 		comp = mtk_crtc->dual_pipe_ddp_ctx.ovl_comp[DDP_FIRST_PATH][i];
-		if (comp && mtk_ddp_comp_get_type(comp->id) == MTK_DISP_VIRTUAL)
+		if (!comp) {
+			DDPPR_ERR("%s errors with NULL comp\n", __func__);
+			continue;
+		} else if (mtk_ddp_comp_get_type(comp->id) == MTK_DISP_VIRTUAL)
 			continue;
 
 		mtk_ddp_comp_stop(comp, cmdq_handle);
