a4c9e8b9c5e998c4da05fad2b4836a0ada7ddd8b
Imran Shaik <quic_imrashai@quicinc.com>
2022-09-30 09:00:47 +0530

    clk: qcom: rmph: Add support for RPMH_IPA_CLK for sdxpinn
    Add support for RPMH_IPA_CLK required to be requested from clients
    via RPMH clock driver for sdxpinn platform.
    
    Change-Id: Ia3db3390e3a2bc7ebe0aed802db2171b4b236720
    Signed-off-by: Imran Shaik <quic_imrashai@quicinc.com>

--- drivers/clk/qcom/clk-rpmh.c
+++ drivers/clk/qcom/clk-rpmh.c
@@ -693,6 +693,7 @@ static struct clk_hw *sdxpinn_rpmh_clocks[] = {
 	[RPMH_RF_CLK3]		= &sdm845_rf_clk3.hw,
 	[RPMH_RF_CLK3_A]	= &sdm845_rf_clk3_ao.hw,
 	[RPMH_QPIC_CLK]		= &sdx55_qpic_clk.hw,
+	[RPMH_IPA_CLK]		= &sdx55_ipa.hw,
 };
 
 static const struct clk_rpmh_desc clk_rpmh_sdxpinn = {
