00e67af8b10046de735aa1b2a450a54cd11fbe3a
rnamala <quic_rnamala@quicinc.com>
2022-10-13 12:54:49 +0530

    msm: adsprpc: fix to avoid double hyp assign
    Add file info for persistent maps and check file
    info before removing maps during SSR.
    
    Change-Id: I80392733ce29298e1fac25744ad9556be5d0ac43
    Acked-by: Nishant Chaubey <chaubey@qti.qualcomm.com>
    Signed-off-by: rnamala <quic_rnamala@quicinc.com>

--- drivers/char/adsprpc_shared.h
+++ drivers/char/adsprpc_shared.h
@@ -972,6 +972,7 @@ struct fastrpc_mmap {
 	struct timespec64 map_end_time;
 	/* Mapping for fastrpc shell */
 	bool is_filemap;
+	char *servloc_name;			/* Indicate which daemon mapped this */
 };
 
 enum fastrpc_perfkeys {
