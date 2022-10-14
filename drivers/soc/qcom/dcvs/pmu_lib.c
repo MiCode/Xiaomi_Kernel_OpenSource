107cf09674a0fec0480228d4f7a1a7fb56b75ad7
Shivnandan Kumar <quic_kshivnan@quicinc.com>
2022-10-14 13:55:33 +0530

    drivers: qcom: check return value of devm_protocol_get
    Validate return value of devm_protocol_get.
    Currently only NULL pointer is checked but
    we should also check if returned pointer is
    valid pointer or not. If it is not valid pointer
    then it can lead to crash.
    
    Change-Id: I2209c4e0bfd6ddb4a9bef2364c6451b578631ac2
    Signed-off-by: Shivnandan Kumar <quic_kshivnan@quicinc.com>

--- drivers/soc/qcom/dcvs/pmu_lib.c
+++ drivers/soc/qcom/dcvs/pmu_lib.c
@@ -912,8 +912,8 @@ int rimps_pmu_init(struct scmi_device *sdev)
 		return -EINVAL;
 
 	ops = sdev->handle->devm_get_protocol(sdev, SCMI_PMU_PROTOCOL, &ph);
-	if (!ops)
-		return -EINVAL;
+	if (IS_ERR(ops))
+		return PTR_ERR(ops);
 
 	/*
 	 * If communication with cpucp doesn't succeed here the device memory
