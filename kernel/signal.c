57b092f9f50b5a69e420fe688d10eb0ed07b3054
Bo Ye <bo.ye@mediatek.com>
2022-10-26 16:37:06 +0800

    [ALPS07672330] ACK: Merge android12-5.10-2022-09 R1 into alps-mp-s0.mp1
    [Target]:
        95279078149abf6684fadbc7bb6a2ab49efeea18
        UPSTREAM: usb: dwc3: gadget: Avoid duplicate requests to enable Run/Stop
    
    [Source:] kernel/common android12-5.10-2022-09
    [From:] 91bfc78bc009 ANDROID: Update symbol list for mtk
    [  To:] 95279078149a UPSTREAM: usb: dwc3: gadget: Avoid duplicate requests to enable Run/Stop
    
    MTK-Commit-Id: 14471cd3d0bcc135d0adad47bb805377fae63b9b
    
    Feature: Kernel SI Operation
    CR-Id: ALPS07672330
    Signed-off-by: Bo Ye <bo.ye@mediatek.com>
    Change-Id: Icf75929807107526b731838550f45876b35eac06

--- kernel/signal.c
+++ kernel/signal.c
@@ -1408,7 +1408,6 @@ struct sighand_struct *__lock_task_sighand(struct task_struct *tsk,
 
 	return sighand;
 }
-EXPORT_SYMBOL_GPL(__lock_task_sighand);
 
 /*
  * send signal info to all the members of a group
