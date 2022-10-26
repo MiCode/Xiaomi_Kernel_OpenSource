211826d4049225bd7b07e385901af01d97a54949
mtk14260 <Gavin.Guo@mediatek.com>
2022-10-26 16:30:51 +0800

    [ALPS07481249] disp: revise dsc conig twice
    [Detail]
    crtc1 dsc0 will be config twice without expect
    [Solution]
    revise config flow, dsc will be config at addon connect
    
    MTK-Commit-Id: da73be53caa82f094fbc74805b2ecab0dae4f5dc
    
    Signed-off-by: mtk14260 <Gavin.Guo@mediatek.com>
    CR-Id: ALPS07481249
    Feature: Dual Display for Foldable
    Change-Id: Ic54cc5aab42f88deb738a6362608ae0b82c43135

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
@@ -6253,6 +6253,10 @@ void mtk_crtc_config_default_path(struct mtk_drm_crtc *mtk_crtc)
 		mtk_crtc->gce_obj.client[CLIENT_CFG]);
 
 	for_each_comp_in_cur_crtc_path(comp, mtk_crtc, i, j) {
+		/*dsc will be config at addon connect*/
+		/*remove dsc config here to avoid config twice unexpect*/
+		if (mtk_ddp_comp_get_type(comp->id) == MTK_DISP_DSC)
+			continue;
 		mtk_ddp_comp_config(comp, &cfg, cmdq_handle);
 		mtk_ddp_comp_start(comp, cmdq_handle);
 		mtk_ddp_comp_io_cmd(comp, cmdq_handle, IRQ_LEVEL_NORMAL, NULL);
@@ -6270,6 +6274,10 @@ void mtk_crtc_config_default_path(struct mtk_drm_crtc *mtk_crtc)
 
 	if (mtk_crtc->is_dual_pipe) {
 		for_each_comp_in_dual_pipe(comp, mtk_crtc, i, j) {
+			/*dsc will be config at addon connect*/
+			/*remove dsc config here to avoid config twice unexpect*/
+			if (mtk_ddp_comp_get_type(comp->id) == MTK_DISP_DSC)
+				continue;
 			mtk_ddp_comp_config(comp, &cfg, cmdq_handle);
 			mtk_ddp_comp_start(comp, cmdq_handle);
 
