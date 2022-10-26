3157ab2ca19fd17a133aeec134774f6eec6d4a82
ot_bp.gautam <ot_bp.gautam@mediatek.com>
2022-10-26 16:33:03 +0800

    [ALPS07630443] cmdq: add new cmdq sync token macros for mdp
    Add new cmdq sync token macros for mdp
    (1) CMDQ_SYNC_TOKEN_HISTOGRAM_MDP_WAIT
    (2) CMDQ_SYNC_TOKEN_HISTOGRAM_MDP_SET
    
    MTK-Commit-Id: 559f5f00dc3ac3e5b3cbb6e2de9927e2ae0f27c3
    
    Change-Id: I74a67b24bd8c2cfdeb06ba8e01f316c6547c0d21
    Signed-off-by: ot_bp.gautam <ot_bp.gautam@mediatek.com>
    CR-Id: ALPS07630443
    Feature: mdp driver

--- include/dt-bindings/gce/mt6833-gce.h
+++ include/dt-bindings/gce/mt6833-gce.h
@@ -369,6 +369,10 @@
 #define CMDQ_SYNC_TOKEN_DISP_VA_START			692
 #define CMDQ_SYNC_TOKEN_DISP_VA_END			693
 
+
+#define CMDQ_SYNC_TOKEN_HISTOGRAM_MDP_WAIT		694
+#define CMDQ_SYNC_TOKEN_HISTOGRAM_MDP_SET		695
+
 /* GPR access tokens (for register backup)
  * There are 15 32-bit GPR, 3 GPR form a set
  * (64-bit for address, 32-bit for value)
