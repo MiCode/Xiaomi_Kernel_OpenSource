nekoshirro/platform_kernel_xiaomi_marble

msm: mhi-dev: Release and init edma channels in mhi suspend and resume
Edma block gets reset when pcie goes to D3Cold.
So, When the device is going to suspend release all the channels
and while resuming reinit the edma channels.

Change-Id: I050dbf0b523bc5b781d55e335b1ade51dd65d19e
Signed-off-by: Krishna chaitanya chundru <quic_krichai@quicinc.com>

drivers/platform/msm/mhi_dev/mhi.c
@@ -4402,21 +4402,45 @@ static void mhi_dev_pcie_handle_event(struct work_struct *work)
 	}
 }
 
-static int mhi_edma_init(struct device *dev)
+int mhi_edma_release(void)
 {
-	mhi_ctx->tx_dma_chan = dma_request_slave_channel(dev, "tx");
-	if (IS_ERR_OR_NULL(mhi_ctx->tx_dma_chan)) {
-		pr_err("%s(): request for TX chan failed\n", __func__);
-		return -EIO;
-	}
+	dma_release_channel(mhi_ctx->tx_dma_chan);
+	mhi_ctx->tx_dma_chan = NULL;
+	dma_release_channel(mhi_ctx->rx_dma_chan);
+	mhi_ctx->rx_dma_chan = NULL;
+	return 0;
+}
 
+int mhi_edma_status(void)
+{
+	int ret = 0;
+
+	if (dmaengine_tx_status(mhi_ctx->tx_dma_chan, 0, NULL) == DMA_IN_PROGRESS)
+		ret = -EBUSY;
+	if (dmaengine_tx_status(mhi_ctx->rx_dma_chan, 0, NULL) == DMA_IN_PROGRESS)
+		ret = -EBUSY;
+
+	return ret;
+}
+
+int mhi_edma_init(struct device *dev)
+{
+	if (!mhi_ctx->tx_dma_chan) {
+		mhi_ctx->tx_dma_chan = dma_request_slave_channel(dev, "tx");
+		if (IS_ERR_OR_NULL(mhi_ctx->tx_dma_chan)) {
+			pr_err("%s(): request for TX chan failed\n", __func__);
+			return -EIO;
+		}
+	}
 	mhi_log(MHI_MSG_VERBOSE, "request for TX chan returned :%pK\n",
 			mhi_ctx->tx_dma_chan);
 
-	mhi_ctx->rx_dma_chan = dma_request_slave_channel(dev, "rx");
-	if (IS_ERR_OR_NULL(mhi_ctx->rx_dma_chan)) {
-		pr_err("%s(): request for RX chan failed\n", __func__);
-		return -EIO;
+	if (!mhi_ctx->rx_dma_chan) {
+		mhi_ctx->rx_dma_chan = dma_request_slave_channel(dev, "rx");
+		if (IS_ERR_OR_NULL(mhi_ctx->rx_dma_chan)) {
+			pr_err("%s(): request for RX chan failed\n", __func__);
+			return -EIO;
+		}
 	}
 	mhi_log(MHI_MSG_VERBOSE, "request for RX chan returned :%pK\n",
 			mhi_ctx->rx_dma_chan);

drivers/platform/msm/mhi_dev/mhi_sm.c
@@ -527,6 +527,9 @@ static int mhi_sm_prepare_resume(void)
 				MHI_SM_ERR("Failed resuming ipa_mhi:%d", res);
 				goto exit;
 			}
+		} else if (mhi_sm_ctx->mhi_dev->use_edma) {
+			/* edma resets  when device goes to D3 cold*/
+			mhi_edma_init(mhi_sm_ctx->mhi_dev->dev);
 		}
 	}
 
@@ -686,27 +689,33 @@ static int mhi_sm_prepare_suspend(enum mhi_dev_state new_state)
 	if ((old_state == MHI_DEV_M0_STATE) &&
 			((new_state == MHI_DEV_M2_STATE) ||
 			 (new_state == MHI_DEV_M3_STATE))) {
-		if (mhi_sm_ctx->mhi_dev->use_ipa) {
-			MHI_SM_DBG("Disable IPA with ipa_dma_disable()\n");
-			while (wait_timeout < MHI_IPA_DISABLE_COUNTER) {
+		MHI_SM_DBG("Disable IPA with ipa_dma_disable()\n");
+		while (wait_timeout < MHI_IPA_DISABLE_COUNTER) {
+			if (mhi_sm_ctx->mhi_dev->use_ipa) {
 				/* wait for the disable to finish */
 				res = ipa_dma_disable();
-				if (!res)
-					break;
-				MHI_SM_ERR
-					("IPA disable fail cnt:%d\n",
-						wait_timeout);
-				msleep(MHI_IPA_DISABLE_DELAY_MS);
-				wait_timeout++;
+			} else if (mhi_sm_ctx->mhi_dev->use_edma) {
+				/* wait for edma to be idle*/
+				res = mhi_edma_status();
 			}
-
-			if (wait_timeout >= MHI_IPA_DISABLE_COUNTER) {
-				MHI_SM_ERR
-					("Fail to disable IPA for M3\n");
-				goto exit;
-			}
-			MHI_SM_ERR("IPA DMA successfully disabled\n");
+			if (!res)
+				break;
+			MHI_SM_ERR
+				("DMA disable fail cnt:%d\n",
+					wait_timeout);
+			msleep(MHI_IPA_DISABLE_DELAY_MS);
+			wait_timeout++;
 		}
+
+		if (wait_timeout >= MHI_IPA_DISABLE_COUNTER) {
+			MHI_SM_ERR
+				("Fail to disable DMA for M3\n");
+			goto exit;
+		}
+
+		/* edma completely resets when link goes to susupend state */
+		if (mhi_sm_ctx->mhi_dev->use_edma)
+			mhi_edma_release();
 	}
 
 	if ((old_state == MHI_DEV_M0_STATE) &&
@@ -1166,10 +1175,15 @@ int mhi_dev_sm_exit(struct mhi_dev *mhi_dev)
 	mhi_sm_debugfs_destroy();
 	flush_workqueue(mhi_sm_ctx->mhi_sm_wq);
 	destroy_workqueue(mhi_sm_ctx->mhi_sm_wq);
-	/* Initiate MHI IPA reset */
-	ipa_dma_disable();
-	ipa_mhi_destroy();
-	ipa_dma_destroy();
+
+	if (mhi_sm_ctx->mhi_dev->use_edma)
+		mhi_edma_release();
+	if (mhi_sm_ctx->mhi_dev->use_ipa) {
+		/* Initiate MHI IPA reset */
+		ipa_dma_disable();
+		ipa_mhi_destroy();
+		ipa_dma_destroy();
+	}
 	mutex_destroy(&mhi_sm_ctx->mhi_state_lock);
 	mhi_sm_ctx = NULL;
 

MiCode/Xiaomi_Kernel_OpenSource

Signed-off-by: Vivek Sharma <quic_sharmav@quicinc.com>

drivers/platform/msm/mhi_dev/mhi.c
@@ -4790,21 +4790,45 @@ static void mhi_dev_setup_virt_device(struct mhi_dev_ctx *mhictx)
 	}
 }
 
-static int mhi_edma_init(struct mhi_dev_ctx *mhi_hw_ctx, struct device *dev)
+int mhi_edma_release(void)
 {
-	mhi_hw_ctx->tx_dma_chan = dma_request_slave_channel(dev, "tx");
-	if (IS_ERR_OR_NULL(mhi_hw_ctx->tx_dma_chan)) {
-		mhi_log(MHI_MSG_ERROR, "%s(): request for TX chan failed\n", __func__);
-		return -EIO;
-	}
+	dma_release_channel(mhi_hw_ctx->tx_dma_chan);
+	mhi_hw_ctx->tx_dma_chan = NULL;
+	dma_release_channel(mhi_hw_ctx->rx_dma_chan);
+	mhi_hw_ctx->rx_dma_chan = NULL;
+	return 0;
+}
 
+int mhi_edma_status(void)
+{
+	int ret = 0;
+
+	if (dmaengine_tx_status(mhi_hw_ctx->tx_dma_chan, 0, NULL) == DMA_IN_PROGRESS)
+		ret = -EBUSY;
+	if (dmaengine_tx_status(mhi_hw_ctx->rx_dma_chan, 0, NULL) == DMA_IN_PROGRESS)
+		ret = -EBUSY;
+
+	return ret;
+}
+
+int mhi_edma_init(struct device *dev)
+{
+	if (!mhi_hw_ctx->tx_dma_chan) {
+		mhi_hw_ctx->tx_dma_chan = dma_request_slave_channel(dev, "tx");
+		if (IS_ERR_OR_NULL(mhi_hw_ctx->tx_dma_chan)) {
+			pr_err("%s(): request for TX chan failed\n", __func__);
+			return -EIO;
+		}
+	}
 	mhi_log(MHI_MSG_VERBOSE, "request for TX chan returned :%pK\n",
 			mhi_hw_ctx->tx_dma_chan);
 
-	mhi_hw_ctx->rx_dma_chan = dma_request_slave_channel(dev, "rx");
-	if (IS_ERR_OR_NULL(mhi_hw_ctx->rx_dma_chan)) {
-		mhi_log(MHI_MSG_ERROR, "%s(): request for RX chan failed\n", __func__);
-		return -EIO;
+	if (!mhi_hw_ctx->rx_dma_chan) {
+		mhi_hw_ctx->rx_dma_chan = dma_request_slave_channel(dev, "rx");
+		if (IS_ERR_OR_NULL(mhi_hw_ctx->rx_dma_chan)) {
+			pr_err("%s(): request for RX chan failed\n", __func__);
+			return -EIO;
+		}
 	}
 	mhi_log(MHI_MSG_VERBOSE, "request for RX chan returned :%pK\n",
 			mhi_hw_ctx->rx_dma_chan);
@@ -4843,7 +4867,7 @@ static int mhi_dev_probe(struct platform_device *pdev)
 	}
 
 	if (mhi_pf->use_edma) {
-		rc = mhi_edma_init(mhi_hw_ctx, &pdev->dev);
+		rc = mhi_edma_init(&pdev->dev);
 		if (rc) {
 			pr_err("MHI: mhi edma init failed, rc = %d\n", rc);
 			return rc;

drivers/platform/msm/mhi_dev/mhi_sm.c
@@ -541,6 +541,9 @@ static int mhi_sm_prepare_resume(struct mhi_sm_dev *mhi_sm_ctx)
 					goto exit;
 				}
 			}
+		} else if (mhi_sm_ctx->mhi_dev->use_edma) {
+			/* edma resets  when device goes to D3 cold*/
+			mhi_edma_init(mhi_sm_ctx->mhi_dev->mhi_hw_ctx->dev);
 		}
 	}
 
@@ -707,27 +710,33 @@ static int mhi_sm_prepare_suspend(struct mhi_sm_dev *mhi_sm_ctx, enum mhi_dev_st
 	if ((old_state == MHI_DEV_M0_STATE) &&
 			((new_state == MHI_DEV_M2_STATE) ||
 			 (new_state == MHI_DEV_M3_STATE))) {
-		if (mhi_sm_ctx->mhi_dev->use_mhi_dma) {
-			MHI_SM_DBG("Disable MHI DMA with mhi_dma_disable()\n");
-			while (wait_timeout < MHI_DMA_DISABLE_COUNTER) {
+		MHI_SM_DBG("Disable MHI-DMA with mhi_dma_memcpy_disable()\n");
+		while (wait_timeout < MHI_DMA_DISABLE_COUNTER) {
+			if (mhi_sm_ctx->mhi_dev->use_mhi_dma) {
 				/* wait for the disable to finish */
 				res = mhi_dma_fun_ops->mhi_dma_memcpy_disable(mhi_dma_fun_params);
-				if (!res)
-					break;
-				MHI_SM_ERR
-					("MHI DMA disable fail cnt:%d\n",
-						wait_timeout);
-				msleep(MHI_DMA_DISABLE_DELAY_MS);
-				wait_timeout++;
+			} else if (mhi_sm_ctx->mhi_dev->use_edma) {
+				/* wait for edma to be idle*/
+				res = mhi_edma_status();
 			}
-
-			if (wait_timeout >= MHI_DMA_DISABLE_COUNTER) {
-				MHI_SM_ERR
-					("Fail to disable MHI DMA for M3\n");
-				goto exit;
-			}
-			MHI_SM_ERR("MHI DMA successfully disabled\n");
+			if (!res)
+				break;
+			MHI_SM_ERR
+				("DMA disable fail cnt:%d\n",
+					wait_timeout);
+			msleep(MHI_DMA_DISABLE_DELAY_MS);
+			wait_timeout++;
 		}
+
+		if (wait_timeout >= MHI_DMA_DISABLE_COUNTER) {
+			MHI_SM_ERR
+				("Fail to disable DMA for M3\n");
+			goto exit;
+		}
+		MHI_SM_ERR("MHI DMA successfully disabled\n");
+		/* edma completely resets when link goes to susupend state */
+		if (mhi_sm_ctx->mhi_dev->use_edma)
+			mhi_edma_release();
 	}
 
 	if ((old_state == MHI_DEV_M0_STATE) &&
@@ -1198,9 +1207,14 @@ int mhi_dev_sm_exit(struct mhi_dev *mhi_dev)
 	flush_workqueue(mhi_sm_ctx->mhi_sm_wq);
 	destroy_workqueue(mhi_sm_ctx->mhi_sm_wq);
 	/* Initiate MHI DMA reset */
-	mhi_dma_fun_ops->mhi_dma_memcpy_disable(mhi_dma_fun_params);
-	mhi_dma_fun_ops->mhi_dma_destroy(mhi_dma_fun_params);
-	mhi_dma_fun_ops->mhi_dma_memcpy_destroy(mhi_dma_fun_params);
+	if (mhi_sm_ctx->mhi_dev->use_mhi_dma) {
+		mhi_dma_fun_ops->mhi_dma_memcpy_disable(mhi_dma_fun_params);
+		mhi_dma_fun_ops->mhi_dma_destroy(mhi_dma_fun_params);
+		mhi_dma_fun_ops->mhi_dma_memcpy_destroy(mhi_dma_fun_params);
+	}
+
+	if (mhi_sm_ctx->mhi_dev->use_edma)
+		mhi_edma_release();
 	mutex_destroy(&mhi_sm_ctx->mhi_state_lock);
 	mhi_dev_sm_ctx[vf_id] = NULL;
 