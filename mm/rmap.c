4bce99229acdc732bf6a9c0adafc78ec856e3732
Kalesh Singh <kaleshsingh@google.com>
2022-11-07 15:59:19 -0800

    Revert "FROMLIST: mm: multi-gen LRU: exploit locality in rmap"
    This reverts commit afd94c9ef99092b459213b50c61a0c65e2882cde.
    
    To be replaced with upstream version.
    
    Bug: 249601646
    Change-Id: I0c7a88b2c4fd184ee949fc84b422dbec5dc83319
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- mm/rmap.c
+++ mm/rmap.c
@@ -73,7 +73,6 @@
 #include <linux/page_idle.h>
 #include <linux/memremap.h>
 #include <linux/userfaultfd_k.h>
-#include <linux/mm_inline.h>
 
 #include <asm/tlbflush.h>
 
@@ -809,12 +808,6 @@ static bool page_referenced_one(struct page *page, struct vm_area_struct *vma,
 		}
 
 		if (pvmw.pte) {
-			if (lru_gen_enabled() && pte_young(*pvmw.pte) &&
-			    !(vma->vm_flags & (VM_SEQ_READ | VM_RAND_READ))) {
-				lru_gen_look_around(&pvmw);
-				referenced++;
-			}
-
 			if (ptep_clear_flush_young_notify(vma, address,
 						pvmw.pte)) {
 				/*
