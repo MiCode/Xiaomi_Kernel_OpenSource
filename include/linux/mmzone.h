3a3733721b59ab026f48ce413ea3ccf5d9b85e8f
Kalesh Singh <kaleshsingh@google.com>
2022-11-07 15:57:18 -0800

    Revert "FROMLIST: mm: multi-gen LRU: kill switch"
    This reverts commit 76f7f07cbfd24eb3563bdb17db6e9055c71d6377.
    
    To be replaced with upstream version.
    
    Bug: 249601646
    Change-Id: I8356c32ce1962efba62ab2fc8a256b751c974c00
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -364,13 +364,6 @@ enum {
 	LRU_GEN_FILE,
 };
 
-enum {
-	LRU_GEN_CORE,
-	LRU_GEN_MM_WALK,
-	LRU_GEN_NONLEAF_YOUNG,
-	NR_LRU_GEN_CAPS
-};
-
 #define MIN_LRU_BATCH		BITS_PER_LONG
 #define MAX_LRU_BATCH		(MIN_LRU_BATCH * 128)
 
@@ -409,8 +402,6 @@ struct lru_gen_struct {
 	/* can be modified without holding the LRU lock */
 	atomic_long_t evicted[NR_HIST_GENS][ANON_AND_FILE][MAX_NR_TIERS];
 	atomic_long_t refaulted[NR_HIST_GENS][ANON_AND_FILE][MAX_NR_TIERS];
-	/* whether the multi-gen LRU is enabled */
-	bool enabled;
 };
 
 enum {
