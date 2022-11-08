475108fadda0f25c04884071eed76cb59b33485a
Kalesh Singh <kaleshsingh@google.com>
2022-11-07 16:04:51 -0800

    Revert "FROMLIST: mm: x86: add CONFIG_ARCH_HAS_NONLEAF_PMD_YOUNG"
    This reverts commit 2f4e6bc5ac9046b80a795ff24e6e6bafe887fecc.
    
    To be replaced with upstream version.
    
    Bug: 249601646
    Change-Id: Ibcf628e3313140e79c18f33e0437356a23f0ea00
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- arch/x86/include/asm/pgtable.h
+++ arch/x86/include/asm/pgtable.h
@@ -817,8 +817,7 @@ static inline unsigned long pmd_page_vaddr(pmd_t pmd)
 
 static inline int pmd_bad(pmd_t pmd)
 {
-	return (pmd_flags(pmd) & ~(_PAGE_USER | _PAGE_ACCESSED)) !=
-	       (_KERNPG_TABLE & ~_PAGE_ACCESSED);
+	return (pmd_flags(pmd) & ~_PAGE_USER) != _KERNPG_TABLE;
 }
 
 static inline unsigned long pages_to_mb(unsigned long npg)
