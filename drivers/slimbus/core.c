0f494f09ec69e8838908fdfa988d16a39004107c
Chandana Kishori Chiluveru <quic_cchiluve@quicinc.com>
2022-11-11 02:35:38 -0800

    slimbus: qcom-ngd-ctrl: Handle pm_runtime_get_sync failures
    If qcom_slim_ngd_power_up fails during runtime_resume,
    driver return code doesn't checking return value of
    pm_runtime_get_sync(). This is further leading
    to NOC error in slimbus and audio loss failures.
    
    To fix this issue add proper return codes and cleanup in
    slimbus drivers during pm_runtime_get_sync() and runtime_resume
    failures.
    
    Change-Id: I4cc840a8f03513f9641e121f633aa8fb13f9d31f
    Signed-off-by: Chandana Kishori Chiluveru <quic_cchiluve@quicinc.com>

--- drivers/slimbus/core.c
+++ drivers/slimbus/core.c
@@ -500,6 +500,14 @@ int slim_device_report_present(struct slim_controller *ctrl,
 	int ret;
 
 	ret = pm_runtime_get_sync(ctrl->dev);
+	if (ret < 0) {
+		dev_err(ctrl->dev, "slim %s: PM get_sync failed ret :%d\n",
+			__func__, ret);
+		pm_runtime_put_noidle(ctrl->dev);
+		/* Set device in suspended since resume failed */
+		pm_runtime_set_suspended(ctrl->dev);
+		return ret;
+	}
 
 	if (ctrl->sched.clk_state != SLIM_CLK_ACTIVE) {
 		dev_err(ctrl->dev, "slim ctrl not active,state:%d, ret:%d\n",
