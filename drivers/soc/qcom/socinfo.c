5bd06d6a39ef97e55bee7137bc7af132ab10937a
Qilong Wang <quic_qilowang@quicinc.com>
2022-11-21 17:25:17 +0800

    soc: qcom: socinfo: Add support for kalama QCS variants soc-id
    Add socinfo support for kalama QCS SoC ID
    
    Change-Id: Ibf2d9a1734abe2a4055018d7f02a50632b198b93
    Signed-off-by: Qilong Wang <quic_qilowang@quicinc.com>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -1015,6 +1015,7 @@ static const struct soc_id soc_id[] = {
 	{ 590, "CINDERRU_1062"},
 	{ 600, "SG_KALAMA"},
 	{ 601, "SG_KALAMAP"},
+	{ 603, "QCS_KALAMAP"},
 };
 
 static struct qcom_socinfo *qsocinfo;
