7414b0cff4a5d91b86dbc2dad6d0f371edbacd63
Jack Hao <Jack.Hao@mediatek.com>
2022-11-22 12:51:13 +0800

    [ALPS07709632] mt6886: disp: used mode id get HRT BW
    used mode id get HRT BW in layering rule
    
    MTK-Commit-Id: b5136340f8ce3be025b37c5139832f247b32f0af
    
    Signed-off-by: Jack Hao <Jack.Hao@mediatek.com>
    CR-Id: ALPS07709632
    Feature: display_mobile
    Change-Id: Ifa83f95c4ddc77d77ad720b0ffd12770d830cafc

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
@@ -1013,7 +1013,6 @@ mtk_drm_crtc_duplicate_state(struct drm_crtc *crtc)
 		state->prop_val[CRTC_PROP_PRES_FENCE_IDX] =
 			old_state->prop_val[CRTC_PROP_PRES_FENCE_IDX];
 	}
-
 	return &state->base;
 }
 
@@ -1025,7 +1024,6 @@ static void mtk_drm_crtc_destroy_state(struct drm_crtc *crtc,
 	s = to_mtk_crtc_state(state);
 
 	__drm_atomic_helper_crtc_destroy_state(state);
-	DDPINFO("destroy/free crtc_state, 0x%llx\n", (u64)state);
 	kfree(s);
 }
 
