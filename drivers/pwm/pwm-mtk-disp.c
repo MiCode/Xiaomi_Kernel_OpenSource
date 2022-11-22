4e31dc5fe2f95b2d2f4541abc14e09e96a7dc86b
alex.sheng <alex.sheng@mediatek.com>
2022-11-22 12:49:00 +0800

    [ALPS07141738] PWM: add compatible name for mt6835
    add PWM device compatible name for mt6835
    
    MTK-Commit-Id: 1582c4c0dad2bc7a8d1dde8da9ffbad863566bb4
    
    CR-Id: ALPS07141738
    Feature: Ambient Light Adaptive Luma
    Change-Id: Iaff686d81c6f12c139eee02b7274469662ba4751
    Signed-off-by: alex.sheng <alex.sheng@mediatek.com>
    (cherry picked from commit cec489825bbaa2b0b63b62fdea7d756122766ada)
    (cherry picked from commit 4c19d6172fb96019d7eba6e4971c9006f03a47e5)

--- drivers/pwm/pwm-mtk-disp.c
+++ drivers/pwm/pwm-mtk-disp.c
@@ -325,6 +325,7 @@ static const struct of_device_id mtk_disp_pwm_of_match[] = {
 	{ .compatible = "mediatek,mt8173-disp-pwm", .data = &mt8173_pwm_data},
 	{ .compatible = "mediatek,mt8183-disp-pwm", .data = &mt8183_pwm_data},
 	{ .compatible = "mediatek,mt6886-disp-pwm", .data = &mt6799_pwm_data},
+	{ .compatible = "mediatek,mt6835-disp-pwm", .data = &mt6799_pwm_data},
 	{ }
 };
 MODULE_DEVICE_TABLE(of, mtk_disp_pwm_of_match);
