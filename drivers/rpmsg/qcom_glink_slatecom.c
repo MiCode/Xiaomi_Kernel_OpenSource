6f0881a67357c5d965209f14a628122361c084d5
Kaushal Hooda <quic_khooda@quicinc.com>
2022-11-22 17:30:55 +0530

    rpmsg: glink: Handle fifo empty event read failure
    slatecom_reg_read will return -ECANCELLED when sletcom SPI is suspended.
    Currently this error is not handled so fifo empty space information
    is not updated properly.
    
    Add retry mechanism for slatecom_reg_read to handle this error.
    revision acedffd0.
    
    Change-Id: Ia8f87a8db191e4f408e934d0706934bcf564502e
    Signed-off-by: Kaushal Hooda <quic_khooda@quicinc.com>

--- drivers/rpmsg/qcom_glink_slatecom.c
+++ drivers/rpmsg/qcom_glink_slatecom.c
@@ -391,10 +391,23 @@ free_intent:
 static void tx_wakeup_worker(struct glink_slatecom *glink)
 {
 	struct slatecom_fifo_fill fifo_fill;
+	int rc;
 
 	mutex_lock(&glink->tx_avail_lock);
-	slatecom_reg_read(glink->slatecom_handle, SLATECOM_REG_FIFO_FILL, 1,
-						&fifo_fill);
+
+	do {
+		rc = slatecom_reg_read(glink->slatecom_handle,
+						SLATECOM_REG_FIFO_FILL, 1, &fifo_fill);
+
+			if (rc < 0) {
+				GLINK_ERR(glink, "%s: Error %d receiving data\n"
+									, __func__, rc);
+				if (rc == -ECANCELED)
+					usleep_range(TX_WAIT_US, TX_WAIT_US + 1000);
+			}
+
+	} while (rc == -ECANCELED);
+
 	glink->fifo_fill.tx_avail = fifo_fill.tx_avail;
 	if (glink->fifo_fill.tx_avail > glink->fifo_size.to_slave/2)
 		glink->water_mark_reached = false;
@@ -479,13 +492,18 @@ static void glink_slatecom_send_read_notify(struct glink_slatecom *glink)
 
 	GLINK_INFO(glink, "Cmd size in words = %d\n", sizeof(msg)/WORD_SIZE);
 
-	ret = slatecom_fifo_write(glink->slatecom_handle, sizeof(msg)/WORD_SIZE,
-								&msg);
-	if (ret < 0) {
-		GLINK_ERR(glink, "%s: Error %d writing data\n",
-							__func__, ret);
-		return;
-	}
+	do {
+		ret = slatecom_fifo_write(glink->slatecom_handle, sizeof(msg)/WORD_SIZE,
+									&msg);
+		if (ret < 0) {
+			GLINK_ERR(glink, "%s: Error %d writing data\n",
+								__func__, ret);
+			if (ret == -ECANCELED)
+				usleep_range(TX_WAIT_US, TX_WAIT_US + 1000);
+			else
+				return;
+		}
+	} while (ret == -ECANCELED);
 
 	glink_slatecom_update_tx_avail(glink, sizeof(msg)/WORD_SIZE);
 }
@@ -704,6 +722,7 @@ static int glink_slatecom_send_final(struct glink_slatecom_channel *channel,
 				struct glink_slatecom_rx_intent *intent,
 				bool wait)
 {
+	int rc;
 	struct glink_slatecom *glink = channel->glink;
 	struct {
 		struct glink_slatecom_msg msg;
@@ -758,9 +777,17 @@ static int glink_slatecom_send_final(struct glink_slatecom_channel *channel,
 			glink->sent_read_notify = false;
 	}
 
-	slatecom_ahb_write_bytes(glink->slatecom_handle,
-	(uint32_t)(size_t)(intent->addr + intent->offset),
-	len, data);
+	do {
+		rc = slatecom_ahb_write_bytes(glink->slatecom_handle,
+					(uint32_t)(size_t)(intent->addr + intent->offset),
+					len, data);
+		if (rc < 0) {
+			GLINK_ERR(glink, "%s: Error %d writing data\n",
+							__func__, rc);
+			if (rc == -ECANCELED)
+				usleep_range(TX_WAIT_US, TX_WAIT_US + 1000);
+		}
+	} while (rc == -ECANCELED);
 
 	intent->offset += len;
 	glink_slatecom_tx_write(glink, &req, sizeof(req));
@@ -2019,12 +2046,23 @@ static void rx_worker(struct kthread_work *work)
 static void glink_slatecom_linkup(struct glink_slatecom *glink)
 {
 	int ret;
+	int rc;
 
 	if (glink->slatecom_status != SLATECOM_LINKUP)
 		return;
 	atomic_set(&glink->in_reset, 0);
-	slatecom_reg_read(glink->slatecom_handle, SLATECOM_REG_FIFO_SIZE, 1,
-				&glink->fifo_size);
+
+	do {
+		rc = slatecom_reg_read(glink->slatecom_handle, SLATECOM_REG_FIFO_SIZE, 1,
+			&glink->fifo_size);
+		if (rc < 0) {
+			GLINK_ERR(glink, "%s: Error %d receiving data\n",
+							__func__, rc);
+			if (rc == -ECANCELED)
+				usleep_range(TX_WAIT_US, TX_WAIT_US + 1000);
+		}
+	} while (rc == -ECANCELED);
+
 	mutex_lock(&glink->tx_avail_lock);
 	glink->fifo_fill.tx_avail = glink->fifo_size.to_master;
 	mutex_unlock(&glink->tx_avail_lock);
