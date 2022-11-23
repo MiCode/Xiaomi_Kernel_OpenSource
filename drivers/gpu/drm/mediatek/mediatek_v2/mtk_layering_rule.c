254d4e8df44f080fefc2a28d0662edb4e784a78e
Hongjiang He <Hongjiang.He@mediatek.com>
2022-11-23 22:13:22 +0800

    [ALPS07701260] disp: fix kasan ke issue
    there is a timing issue, it will occur KE when
    mtk_layering_rule_ioctl use drm_crtc_state, if
    it has already been freed by ddp_cmdq_cb.so need
    add mutex to protect mtk_drm_crtc
    
    MTK-Commit-Id: 62f74ab8c39b02e391a9543f1ac476c1874a875c
    
    Signed-off-by: Hongjiang He <Hongjiang.He@mediatek.com>
    CR-Id: ALPS07701260
    Feature: display_mobile
    Change-Id: Id4022fd2d58d55725185254327d93583e54aadda

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_layering_rule.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_layering_rule.c
@@ -25,6 +25,7 @@
 #include "mtk_rect.h"
 #include "mtk_drm_drv.h"
 #include "mtk_drm_graphics_base.h"
+#include "mtk_drm_mmp.h"
 
 #include <soc/mediatek/mmqos.h>
 
@@ -543,6 +544,7 @@ static int layering_get_valid_hrt(struct drm_crtc *crtc, int mode_idx)
 
 	dvfs_bw *= 10000;
 
+	DDP_MUTEX_LOCK(&mtk_crtc->lock, __func__, __LINE__);
 	output_comp = mtk_ddp_comp_request_output(mtk_crtc);
 	if (!mtk_crtc->res_switch) {
 		DDPDBG("%s mode_idx:%d\n", __func__, mode_idx);
@@ -558,6 +560,7 @@ static int layering_get_valid_hrt(struct drm_crtc *crtc, int mode_idx)
 			mtk_ddp_comp_io_cmd(output_comp, NULL,
 				GET_FRAME_HRT_BW_BY_MODE, &tmp);
 	}
+	DDP_MUTEX_UNLOCK(&mtk_crtc->lock, __func__, __LINE__);
 
 	if (!tmp) {
 		DDPPR_ERR("Get frame hrt bw by datarate is zero\n");
