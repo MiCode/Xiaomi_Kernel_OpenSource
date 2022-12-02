4d7510b6802b23c2bcc64e0151ec96d1dac9c326
Manoj Prabhu B <quic_bmanoj@quicinc.com>
2022-12-02 12:18:21 +0530

    memshare: Cleanup unused workqueue
    Workqueue is not used in memshare driver and hence clean up
    the same.
    
    Change-Id: Ibdcbd1952ba6ffdb6d2cda53b048739ffe4a4933
    Signed-off-by: Manoj Prabhu B <quic_bmanoj@quicinc.com>

--- drivers/soc/qcom/memshare/msm_memshare.c
+++ drivers/soc/qcom/memshare/msm_memshare.c
@@ -25,7 +25,6 @@
 static unsigned long(attrs);
 
 static struct qmi_handle *mem_share_svc_handle;
-static struct workqueue_struct *mem_share_svc_workqueue;
 static uint64_t bootup_request;
 
 static struct device *memshare_dev[MAX_CLIENTS];
@@ -595,17 +594,10 @@ static void memshare_init_worker(struct work_struct *work)
 {
 	int rc;
 
-	mem_share_svc_workqueue =
-		create_singlethread_workqueue("mem_share_svc");
-	if (!mem_share_svc_workqueue)
-		return;
-
 	mem_share_svc_handle = kzalloc(sizeof(struct qmi_handle),
 					  GFP_KERNEL);
-	if (!mem_share_svc_handle) {
-		destroy_workqueue(mem_share_svc_workqueue);
+	if (!mem_share_svc_handle)
 		return;
-	}
 
 	rc = qmi_handle_init(mem_share_svc_handle,
 		sizeof(struct qmi_elem_info),
@@ -615,7 +607,6 @@ static void memshare_init_worker(struct work_struct *work)
 			"memshare: Creating mem_share_svc qmi handle failed\n");
 		kfree(mem_share_svc_handle);
 		mem_share_svc_handle = NULL;
-		destroy_workqueue(mem_share_svc_workqueue);
 		return;
 	}
 	rc = qmi_add_server(mem_share_svc_handle, MEM_SHARE_SERVICE_SVC_ID,
@@ -628,7 +619,6 @@ static void memshare_init_worker(struct work_struct *work)
 			kfree(mem_share_svc_handle);
 			mem_share_svc_handle = NULL;
 		}
-		destroy_workqueue(mem_share_svc_workqueue);
 		return;
 	}
 	dev_dbg(memsh_drv->dev, "memshare: memshare_init successful\n");
@@ -794,13 +784,11 @@ static int memshare_remove(struct platform_device *pdev)
 	if (!memsh_drv)
 		return 0;
 
-	flush_workqueue(mem_share_svc_workqueue);
 	if (mem_share_svc_handle) {
 		qmi_handle_release(mem_share_svc_handle);
 		kfree(mem_share_svc_handle);
 		mem_share_svc_handle = NULL;
 	}
-	destroy_workqueue(mem_share_svc_workqueue);
 	return 0;
 }
 
