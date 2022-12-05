a7fc7353dcf4cfd9d7bae9616869c9cf3636249e
Satish Kumar Kodishala <quic_skodisha@quicinc.com>
2022-12-05 13:04:09 +0530

    Add support for Comanche
    Add support for Comanche while getting logical address.
    
    CRs-Fixed: 3285963
    Change-Id: I479f5c4c631f9b2a0d65792b98ad0d9c7ead9e6f
    Signed-off-by: Satish Kumar Kodishala <quic_skodisha@quicinc.com>

--- drivers/bluetooth/btfm_slim.c
+++ drivers/bluetooth/btfm_slim.c
@@ -402,8 +402,15 @@ int btfm_slim_hw_init(struct btfmslim *btfmslim)
 		chipset_ver ==  QCA_APACHE_SOC_ID_0100  ||
 		chipset_ver ==  QCA_APACHE_SOC_ID_0110  ||
 		chipset_ver ==  QCA_APACHE_SOC_ID_0120 ||
-		chipset_ver ==  QCA_APACHE_SOC_ID_0121) {
-		BTFMSLIM_INFO("chipset is Chk/Apache, overwriting EA");
+		chipset_ver ==  QCA_APACHE_SOC_ID_0121 ||
+		chipset_ver ==  QCA_COMANCHE_SOC_ID_0101 ||
+		chipset_ver ==  QCA_COMANCHE_SOC_ID_0110 ||
+		chipset_ver ==  QCA_COMANCHE_SOC_ID_0120 ||
+		chipset_ver ==  QCA_COMANCHE_SOC_ID_0130 ||
+		chipset_ver ==  QCA_COMANCHE_SOC_ID_4130 ||
+		chipset_ver ==  QCA_COMANCHE_SOC_ID_5120 ||
+		chipset_ver ==  QCA_COMANCHE_SOC_ID_5130) {
+		BTFMSLIM_INFO("chipset is Chk/Apache/CMC, overwriting EA");
 		slim->is_laddr_valid = false;
 		slim->e_addr.manf_id = SLIM_MANF_ID_QCOM;
 		slim->e_addr.prod_code = 0x220;
@@ -422,15 +429,15 @@ int btfm_slim_hw_init(struct btfmslim *btfmslim)
 		slim_ifd->e_addr.instance = 0x0;
 		slim_ifd->laddr = 0x0;
 	}
-		BTFMSLIM_INFO(
-			"PGD Enum Addr: manu id:%.02x prod code:%.02x dev idx:%.02x instance:%.02x",
-			slim->e_addr.manf_id, slim->e_addr.prod_code,
-			slim->e_addr.dev_index, slim->e_addr.instance);
+	BTFMSLIM_INFO(
+		"PGD Enum Addr: manu id:%.02x prod code:%.02x dev idx:%.02x instance:%.02x",
+		slim->e_addr.manf_id, slim->e_addr.prod_code,
+		slim->e_addr.dev_index, slim->e_addr.instance);
 
-		BTFMSLIM_INFO(
-			"IFD Enum Addr: manu id:%.02x prod code:%.02x dev idx:%.02x instance:%.02x",
-			slim_ifd->e_addr.manf_id, slim_ifd->e_addr.prod_code,
-			slim_ifd->e_addr.dev_index, slim_ifd->e_addr.instance);
+	BTFMSLIM_INFO(
+		"IFD Enum Addr: manu id:%.02x prod code:%.02x dev idx:%.02x instance:%.02x",
+		slim_ifd->e_addr.manf_id, slim_ifd->e_addr.prod_code,
+		slim_ifd->e_addr.dev_index, slim_ifd->e_addr.instance);
 
 	/* Assign Logical Address for PGD (Ported Generic Device)
 	 * enumeration address
