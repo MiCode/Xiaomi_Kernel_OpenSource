b98ed16a5762a3a7b2ad24f319b9ab095319c56f
Shreyas K K <quic_shrekk@quicinc.com>
2022-12-15 23:05:08 +0530

    dma-heaps: Add hibernation callbacks to secure heaps
    Add freeze and restore callbacks to all secure heaps. All
    hyp-assigned memory which HLOS does not have access cannot
    be saved during hibernation, so free all such memory back
    to the system from secure pools. Abort hibernation if
    clients have not freed such memory by tracking all the
    allocations.
    
    Change-Id: I8a3ba617fef5ea3fdbc60a72412af90564d75052
    Signed-off-by: Vivek Kumar <quic_vivekuma@quicinc.com>
    Signed-off-by: Shreyas K K <quic_shrekk@quicinc.com>

--- drivers/dma-buf/heaps/qcom_secure_system_heap.h
+++ drivers/dma-buf/heaps/qcom_secure_system_heap.h
@@ -8,23 +8,29 @@
 
 #include <linux/dma-heap.h>
 #include <linux/err.h>
+#include <linux/types.h>
 #include "qcom_dynamic_page_pool.h"
 
 struct qcom_secure_system_heap {
 	struct dynamic_page_pool **pool_list;
 	int vmid;
 	struct list_head list;
+	atomic_long_t total_allocated;
 };
 
 #ifdef CONFIG_QCOM_DMABUF_HEAPS_SYSTEM_SECURE
 void qcom_secure_system_heap_create(const char *name, const char *secure_system_alias,
 				    int vmid);
+int qcom_secure_system_heap_freeze(void);
+int qcom_secure_system_heap_restore(void);
 #else
 static void qcom_secure_system_heap_create(const char *name, const char *secure_system_alias,
 					   int vmid)
 {
 
 }
+static inline int qcom_secure_system_heap_freeze(void) { return 0; }
+static inline int qcom_secure_system_heap_restore(void) { return 0; }
 #endif
 
 #endif /* _QCOM_SECURE_SYSTEM_HEAP_H */
