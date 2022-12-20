756162956b05fb4a708e78e6f735e029cbbc976b
Manish Pandey <quic_mapa@quicinc.com>
2022-12-20 17:35:46 +0530

    scsi: ufs-qcom: add support to enable dynamic irq affinity
    If device tree includes 'qcom,prime-mask' node, enable dynamic
    irq affinity feature, else keep it disable by default.
    
    Change-Id: I04dad7daaafe8b8dcb45305e2bd7bf1945394905
    Signed-off-by: Manish Pandey <quic_mapa@quicinc.com>

--- drivers/scsi/ufs/ufs-qcom.c
+++ drivers/scsi/ufs/ufs-qcom.c
@@ -1364,10 +1364,12 @@ static void ufs_qcom_cpufreq_dwork(struct work_struct *work)
 
 	if (cur_thres > NUM_REQS_HIGH_THRESH && !host->cur_freq_vote) {
 		freq_val = host->max_cpu_scale_freq;
-		ufs_qcom_toggle_pri_affinity(host->hba, true);
+		if (host->irq_affinity_support)
+			ufs_qcom_toggle_pri_affinity(host->hba, true);
 	} else if (cur_thres < NUM_REQS_LOW_THRESH && host->cur_freq_vote) {
 		freq_val = host->min_cpu_scale_freq;
-		ufs_qcom_toggle_pri_affinity(host->hba, false);
+		if (host->irq_affinity_support)
+			ufs_qcom_toggle_pri_affinity(host->hba, false);
 	}
 
 	if (freq_val == -1)
@@ -3098,6 +3100,9 @@ static void ufs_qcom_parse_irq_affinity(struct ufs_hba *hba)
 			host->def_mask.bits[0] = UFS_QCOM_IRQ_SLVR_MASK;
 		}
 	}
+	/* If device includes perf mask, enable dynamic irq affinity feature */
+	if (host->perf_mask.bits[0])
+		host->irq_affinity_support = true;
 }
 
 static void ufs_qcom_parse_pm_level(struct ufs_hba *hba)
@@ -3163,7 +3168,8 @@ static int ufs_qcom_set_cur_therm_state(struct thermal_cooling_device *tcd,
 	case UFS_QCOM_LVL_NO_THERM:
 		dev_warn(tcd->devdata, "UFS host thermal mitigation stops\n");
 
-		atomic_set(&host->therm_mitigation, 0);
+		if (host->irq_affinity_support)
+			atomic_set(&host->therm_mitigation, 0);
 		/* Set the default auto-hiberate idle timer to 5 ms */
 		ufshcd_auto_hibern8_update(hba, ufs_qcom_us_to_ahit(5000));
 
@@ -3176,9 +3182,11 @@ static int ufs_qcom_set_cur_therm_state(struct thermal_cooling_device *tcd,
 	case UFS_QCOM_LVL_MAX_THERM:
 		dev_warn(tcd->devdata, "Going into UFS host thermal mitigation state, performance may be impacted before UFS host thermal mitigation stops\n");
 
-		/* Stop setting hi-pri to requests and set irq affinity to default value */
-		atomic_set(&host->therm_mitigation, 1);
-		ufs_qcom_toggle_pri_affinity(hba, false);
+		if (host->irq_affinity_support) {
+			/* Stop setting hi-pri to requests and set irq affinity to default value */
+			atomic_set(&host->therm_mitigation, 1);
+			ufs_qcom_toggle_pri_affinity(hba, false);
+		}
 		/* Set the default auto-hiberate idle timer to 1 ms */
 		ufshcd_auto_hibern8_update(hba, ufs_qcom_us_to_ahit(1000));
 
@@ -4840,7 +4848,7 @@ static void ufs_qcom_hook_send_command(void *param, struct ufs_hba *hba,
 					REG_UTP_TRANSFER_REQ_DOOR_BELL),
 				sz);
 
-		if (atomic_read(&host->hi_pri_en) && rq)
+		if ((host->irq_affinity_support) && atomic_read(&host->hi_pri_en) && rq)
 			rq->cmd_flags |= REQ_HIPRI;
 	}
 }
