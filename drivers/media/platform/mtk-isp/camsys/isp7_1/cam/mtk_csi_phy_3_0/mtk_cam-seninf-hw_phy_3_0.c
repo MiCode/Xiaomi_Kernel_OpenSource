c8f1b39df60470d0619f6543e38cb1e3f3282437
mtk23161 <yuche.huang@mediatek.com>
2022-12-22 15:03:17 +0800

    [ALPS07715686] fix seninf_top_mux race condition
    1. add mutex lock in seninf_top_mux_ctrl to avoid race condition
       while setting top mux
    
    MTK-Commit-Id: ce9ed4e2a7fe53405f96e4c7869888ceba4d48c9
    
    CR-Id: ALPS07715686
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: mtk23161 <yuche.huang@mediatek.com>
    Change-Id: I35a93c271a8fee7af6c0a1771c47b7fcc96f422b

--- drivers/media/platform/mtk-isp/camsys/isp7_1/cam/mtk_csi_phy_3_0/mtk_cam-seninf-hw_phy_3_0.c
+++ drivers/media/platform/mtk-isp/camsys/isp7_1/cam/mtk_csi_phy_3_0/mtk_cam-seninf-hw_phy_3_0.c
@@ -346,6 +346,10 @@ static int mtk_cam_seninf_set_top_mux_ctrl(struct seninf_ctx *ctx,
 				    int mux_idx, int seninf_src)
 {
 	void *pSeninf = ctx->reg_if_top;
+	struct seninf_core *core;
+
+	core = dev_get_drvdata(ctx->dev->parent);
+	mutex_lock(&core->mutex);
 
 	switch (mux_idx) {
 	case SENINF_MUX1:
@@ -441,6 +445,9 @@ static int mtk_cam_seninf_set_top_mux_ctrl(struct seninf_ctx *ctx,
 		dev_info(ctx->dev, "invalid mux_idx %d\n", mux_idx);
 		return -EINVAL;
 	}
+
+	mutex_unlock(&core->mutex);
+
 // #if LOG_MORE
 	dev_info(ctx->dev,
 		"TOP_MUX_CTRL_0(0x%x) TOP_MUX_CTRL_1(0x%x) TOP_MUX_CTRL_2(0x%x) TOP_MUX_CTRL_3(0x%x) TOP_MUX_CTRL_4(0x%x) TOP_MUX_CTRL_5(0x%x)\n",
