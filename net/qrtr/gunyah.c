32d9c3a2f2b6a4d1fc48d6871194f3faf3184e8b
Sarannya S <quic_sarannya@quicinc.com>
2022-12-23 14:29:07 +0530

    net: qrtr: gunyah: Add bounds check on tx path
    Add bounds check on values read from shared memory in the tx path. In
    cases where the VM is misbehaving, the qrtr gunyah transport should
    exit and print a warning when bogus values may cause out of bounds to
    be read.
    
    Change-Id: I73abb3994b90850ccfd8d41266e53eb4a38a62f6
    Signed-off-by: Chris Lew <quic_clew@quicinc.com>
    Signed-off-by: Sarannya S <quic_sarannya@quicinc.com>

--- net/qrtr/gunyah.c
+++ net/qrtr/gunyah.c
@@ -189,6 +189,9 @@ static size_t gunyah_tx_avail(struct gunyah_pipe *pipe)
 	else
 		avail -= FIFO_FULL_RESERVE;
 
+	if (WARN_ON_ONCE(avail > pipe->length))
+		avail = 0;
+
 	return avail;
 }
 
@@ -199,6 +202,8 @@ static void gunyah_tx_write(struct gunyah_pipe *pipe, const void *data,
 	u32 head;
 
 	head = le32_to_cpu(*pipe->head);
+	if (WARN_ON_ONCE(head > pipe->length))
+		return;
 
 	len = min_t(size_t, count, pipe->length - head);
 	if (len)
