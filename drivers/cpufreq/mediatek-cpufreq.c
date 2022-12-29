0ef8ae8d3a676ee05f92370e255c6d0dff57f377
Jia-Wei Chang <jia-wei.chang@mediatek.com>
2022-12-29 13:59:53 +0800

    ANDROID: cpufreq: add macro guard to protect vendor hook
    To fix build fail of patch [1] by adding the macro guard to protect the
    vendor hook register caller and its callback function.
    
    The report log [2] shows build fail due to the absence of macro
    definition of CONFIG_ANDROID_VENDOR_HOOKS.
    
    [1] https://android-review.googlesource.com/c/kernel/common/+/2357403
    [2] https://storage.tuxsuite.com/public/clangbuiltlinux/continuous-integration2/builds/2JGC8dCPsZEMPlkPuFiztM7k46n/build.log
    
    Bug: 260537724
    Change-Id: I33fe623e3cbe82647bda7c43d36e66ce8e22b11f
    Signed-off-by: Jia-Wei Chang <jia-wei.chang@mediatek.com>

--- drivers/cpufreq/mediatek-cpufreq.c
+++ drivers/cpufreq/mediatek-cpufreq.c
@@ -15,8 +15,10 @@
 #include <linux/regulator/consumer.h>
 #include <linux/slab.h>
 #include <linux/thermal.h>
+#if defined(CONFIG_TRACEPOINTS) && defined(CONFIG_ANDROID_VENDOR_HOOKS)
 #include <linux/device.h>
 #include <trace/hooks/cpufreq.h>
+#endif
 
 #define MIN_VOLT_SHIFT		(100000)
 #define MAX_VOLT_SHIFT		(200000)
@@ -464,10 +466,12 @@ static int mtk_cpufreq_exit(struct cpufreq_policy *policy)
 	return 0;
 }
 
+#if defined(CONFIG_TRACEPOINTS) && defined(CONFIG_ANDROID_VENDOR_HOOKS)
 static void mtk_cpufreq_suppress(void *data, struct device *dev, int val)
 {
 	dev_set_uevent_suppress(dev, val);
 }
+#endif
 
 static struct cpufreq_driver mtk_cpufreq_driver = {
 	.flags = CPUFREQ_NEED_INITIAL_FREQ_CHECK |
@@ -516,7 +520,9 @@ static int mtk_cpufreq_probe(struct platform_device *pdev)
 		goto release_dvfs_info_list;
 	}
 
+#if defined(CONFIG_TRACEPOINTS) && defined(CONFIG_ANDROID_VENDOR_HOOKS)
 	ret = register_trace_android_vh_cpufreq_offline(mtk_cpufreq_suppress, NULL);
+#endif
 
 	return 0;
 
