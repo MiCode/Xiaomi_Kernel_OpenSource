bd9d1f7d21688393d190c317f6c440345126db9d
Deepak Kumar <quic_dkumar@quicinc.com>
2023-01-03 21:38:52 +0530

    msm: kgsl: Fix null pointer dereference in gen7/a6xx_add_profile_events
    Add a null pointer check for time pointer in gen7/a6xx_add_profile_events
    function as it is expected to be null in few cases.
    
    Change-Id: Ia96fa48693f3b070e6652ddc2852c6afedefdee8
    Signed-off-by: Deepak Kumar <quic_dkumar@quicinc.com>

--- drivers/gpu/msm/adreno_gen7_hwsched_hfi.c
+++ drivers/gpu/msm/adreno_gen7_hwsched_hfi.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/dma-fence-array.h>
@@ -1661,6 +1661,9 @@ static void gen7_add_profile_events(struct adreno_device *adreno_dev,
 	struct kgsl_context *context = drawobj->context;
 	struct submission_info info = {0};
 	struct adreno_hwsched *hwsched = &adreno_dev->hwsched;
+
+	if (!time)
+		return;
 
 	/*
 	 * Here we are attempting to create a mapping between the
