d0a32664f70b497b98c46f86bfd8d82c8915ef77
Naini Singh <quic_nainsing@quicinc.com>
2023-01-16 17:20:03 +0530

    soc: qcom: socinfo: Add API for subset core/part information
    Add kernel API to provide subset core and part information.
    Also export the information via sysfs entries.
    
    Change-Id: I92b12d2c1248d78790381edf5cf270cbc72bbbf4
    Signed-off-by: Naini Singh <quic_nainsing@quicinc.com>

--- include/soc/qcom/socinfo.h
+++ include/soc/qcom/socinfo.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2019-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef __SOC_QCOM_SOCINFO_H__
@@ -8,10 +9,36 @@
 
 #include <linux/types.h>
 
+enum subset_part_type {
+	PART_UNKNOWN      = 0,
+	PART_GPU          = 1,
+	PART_VIDEO        = 2,
+	PART_CAMERA       = 3,
+	PART_DISPLAY      = 4,
+	PART_AUDIO        = 5,
+	PART_MODEM        = 6,
+	PART_WLAN         = 7,
+	PART_COMP         = 8,
+	PART_SENSORS      = 9,
+	PART_NPU          = 10,
+	PART_SPSS         = 11,
+	PART_NAV          = 12,
+	PART_COMP1        = 13,
+	PART_DISPLAY1     = 14,
+	NUM_PARTS_MAX,
+};
+
+enum subset_cluster_type {
+	CLUSTER_CPUSS      = 0,
+	NUM_CLUSTERS_MAX,
+};
+
 #if IS_ENABLED(CONFIG_QCOM_SOCINFO)
 uint32_t socinfo_get_id(void);
 uint32_t socinfo_get_serial_number(void);
 const char *socinfo_get_id_string(void);
+uint32_t socinfo_get_cluster_info(enum subset_cluster_type cluster);
+bool socinfo_get_part_info(enum subset_part_type part);
 #else
 static inline uint32_t socinfo_get_id(void)
 {
@@ -27,6 +54,16 @@ static inline const char *socinfo_get_id_string(void)
 {
 	return "N/A";
 }
+
+uint32_t socinfo_get_cluster_info(enum subset_cluster_type cluster)
+{
+	return 0;
+}
+bool socinfo_get_part_info(enum subset_part_type part)
+{
+	return false;
+}
+
 #endif /* CONFIG_QCOM_SOCINFO */
 
 #endif /* __SOC_QCOM_SOCINFO_H__ */
