d757089f258f8459c0d269e3d8e01f4e40a9a964
Pradnya Dahiwale <quic_pdahiwal@quicinc.com>
2023-01-30 19:05:08 +0530

    remoteproc: Add transaction_id to list SSR count for AON Subsystem
    Add transaction_id to list SSR count for AON Subsystem and
    update driver's remove routines with appropriate bailouts.
    
    Change-Id: I427543dbee5e5b80d544f2f06314ee326642aa04
    Signed-off-by: Pradnya Dahiwale <quic_pdahiwal@quicinc.com>

--- drivers/remoteproc/qcom_rproc_slate.c
+++ drivers/remoteproc/qcom_rproc_slate.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #define pr_fmt(fmt)    "%s: " fmt, __func__
@@ -150,6 +150,19 @@ struct qcom_slate {
 
 static irqreturn_t slate_status_change(int irq, void *dev_id);
 struct mutex cmdsync_lock;
+
+static ssize_t txn_id_show(struct device *dev, struct device_attribute *attr,
+				char *buf)
+{
+	struct platform_device *pdev = container_of(dev,
+						struct platform_device, dev);
+	struct qcom_slate *slate_data =
+			(struct qcom_slate *)platform_get_drvdata(pdev);
+
+	return sysfs_emit(buf, "%zu\n",
+			qcom_sysmon_get_txn_id(slate_data->sysmon));
+}
+static DEVICE_ATTR_RO(txn_id);
 
 /**
  * get_cmd_rsp_buffers() - Function sets cmd & rsp buffer pointers and
@@ -905,6 +918,10 @@ static int rproc_slate_driver_probe(struct platform_device *pdev)
 		goto free_rproc;
 	}
 
+	ret = device_create_file(slate->dev, &dev_attr_txn_id);
+	if (ret)
+		goto remove_subdev;
+
 	/* Register callback for handling reboot */
 	slate->reboot_nb.notifier_call = slate_app_reboot_notify;
 	register_reboot_notifier(&slate->reboot_nb);
@@ -931,6 +948,8 @@ destroy_wq:
 	destroy_workqueue(slate_reset_wq);
 unregister_notify:
 	unregister_reboot_notifier(&slate->reboot_nb);
+remove_subdev:
+	qcom_remove_sysmon_subdev(slate->sysmon);
 free_rproc:
 	rproc_free(rproc);
 	mutex_destroy(&cmdsync_lock);
@@ -944,6 +963,10 @@ static int rproc_slate_driver_remove(struct platform_device *pdev)
 
 	if (slate_reset_wq)
 		destroy_workqueue(slate_reset_wq);
+	device_remove_file(slate->dev, &dev_attr_txn_id);
+	qcom_remove_glink_subdev(slate->rproc, &slate->glink_subdev);
+	qcom_remove_sysmon_subdev(slate->sysmon);
+	qcom_remove_ssr_subdev(slate->rproc, &slate->ssr_subdev);
 	unregister_reboot_notifier(&slate->reboot_nb);
 	rproc_del(slate->rproc);
 	rproc_free(slate->rproc);
