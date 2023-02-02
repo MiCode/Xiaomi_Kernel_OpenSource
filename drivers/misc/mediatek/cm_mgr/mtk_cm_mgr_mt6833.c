8b224f2c258ede2953d578c11cc7a6328817b686
virendra.upadhyay <virendra.upadhyay@mediatek.com>
2023-02-02 09:28:03 +0800

    [ALPS07840345] cm_mgr: set perf bw path
    set bw path in mt6833 k510 .
    
    MTK-Commit-Id: f66d4eabc8807d53b64ff70231b67b438b3a4e37
    
    Change-Id: I94b8ccde52f2e1b9cfbc233e85bc1e463c136da8
    Signed-off-by: virendra.upadhyay <virendra.upadhyay@mediatek.com>
    Feature: CGM(CPU/GPU Memory Latency Management)
    CR-Id: ALPS07840345

--- drivers/misc/mediatek/cm_mgr/mtk_cm_mgr_mt6833.c
+++ drivers/misc/mediatek/cm_mgr/mtk_cm_mgr_mt6833.c
@@ -338,6 +338,8 @@ static int platform_cm_mgr_probe(struct platform_device *pdev)
 		dev_info(&pdev->dev, "get cm-perf_bw fail\n");
 		cm_mgr_set_bw_path(NULL);
 	}
+	/* set bw path */
+	cm_mgr_set_bw_path(bw_path);
 
 	if (ret > 0) {
 		cm_mgr_perfs = devm_kzalloc(&pdev->dev,
