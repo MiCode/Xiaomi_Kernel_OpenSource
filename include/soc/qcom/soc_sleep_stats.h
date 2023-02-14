fdb90d3ab977d7d369fd1171ce8fa0da1b86b338
Shiraz Hashim <quic_shashim@quicinc.com>
2023-02-14 17:51:40 +0530

    drivers: qcom: Fix build issue when !CONFIG_MSM_QMP
    Mark all empty ddr_stats_* functions in soc_sleep_stats.h
    as static inline, so as to avoid link errors due to
    multiple definitions when CONFIG_MSM_QMP is not defined.
    
      ld.lld: error: duplicate symbol:
      ddr_stats_freq_sync_send_msg
      >>> defined at soc_sleep_stats.h:34
      >>> (include/soc/qcom/soc_sleep_stats.h:34)
      >>>   soc/qcom/soc_sleep_stats.o:(ddr_stats_freq_sync_send_msg)
      >>>   in archive drivers/built-in.a
      >>> defined at soc_sleep_stats.h:34
      >>> (include/soc/qcom/soc_sleep_stats.h:34)
      >>>            soc/qcom/boot_stats.o:(.text+0x0) in
      >>>            archive drivers/built-in.a
    
    Change-Id: I10b9698855d3cabcb4ddc87bec047906614246fe
    Signed-off-by: Shiraz Hashim <quic_shashim@quicinc.com>

--- include/soc/qcom/soc_sleep_stats.h
+++ include/soc/qcom/soc_sleep_stats.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  */
 
@@ -30,19 +30,19 @@ int ddr_stats_get_ss_count(void);
 int ddr_stats_get_ss_vote_info(int ss_count,
 			       struct ddr_stats_ss_vote_info *vote_info);
 #else
-int ddr_stats_freq_sync_send_msg(void)
+static inline int ddr_stats_freq_sync_send_msg(void)
 { return -ENODEV; }
 
-int ddr_stats_get_freq_count(void)
+static inline int ddr_stats_get_freq_count(void)
 { return -ENODEV; }
 
-int ddr_stats_get_residency(int freq_count, struct ddr_freq_residency *data)
+static inline int ddr_stats_get_residency(int freq_count, struct ddr_freq_residency *data)
 { return -ENODEV; }
 
-int ddr_stats_get_ss_count(void)
+static inline int ddr_stats_get_ss_count(void)
 { return -ENODEV; }
 
-int ddr_stats_get_ss_vote_info(int ss_count,
+static inline int ddr_stats_get_ss_vote_info(int ss_count,
 			       struct ddr_stats_ss_vote_info *vote_info)
 { return -ENODEV; }
 #endif
