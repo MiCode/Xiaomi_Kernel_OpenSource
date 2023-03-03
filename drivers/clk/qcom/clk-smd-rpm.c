d95e9027a8c2a265acd819f6958d339cdeb37f2a
Taniya Das <quic_tdas@quicinc.com>
2023-03-03 19:36:26 +0530

    clk: qcom: rpm-smd: Add support for CXO_D clock for client
    CXO_D0 and CX0_D0_A clock added for clients to be able to vote on these
    clock resources.
    
    Change-Id: I478237a2b23163fc896e18e548043a66438ea485
    Signed-off-by: Taniya Das <quic_tdas@quicinc.com>

--- drivers/clk/qcom/clk-smd-rpm.c
+++ drivers/clk/qcom/clk-smd-rpm.c
@@ -2,7 +2,7 @@
 /*
  * Copyright (c) 2016, Linaro Limited
  * Copyright (c) 2014, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/clk-provider.h>
@@ -1237,6 +1237,7 @@ DEFINE_CLK_SMD_RPM(monaco, bimc_gpu_clk, bimc_gpu_a_clk,
 
 DEFINE_CLK_SMD_RPM_XO_BUFFER(monaco, ln_bb_clk2, ln_bb_clk2_a, 0x2);
 DEFINE_CLK_SMD_RPM_XO_BUFFER(monaco, rf_clk3, rf_clk3_a, 6);
+DEFINE_CLK_SMD_RPM_XO_BUFFER(monaco, cxo_d0, cxo_d0_a, 0);
 
 static struct clk_hw *monaco_clks[] = {
 	[RPM_SMD_XO_CLK_SRC] = &holi_bi_tcxo.hw,
@@ -1275,6 +1276,8 @@ static struct clk_hw *monaco_clks[] = {
 	[RPM_SMD_BIMC_GPU_A_CLK] = &monaco_bimc_gpu_a_clk.hw,
 	[RPM_SMD_CPUSS_GNOC_CLK] = &monaco_cpuss_gnoc_clk.hw,
 	[RPM_SMD_CPUSS_GNOC_A_CLK] = &monaco_cpuss_gnoc_a_clk.hw,
+	[RPM_SMD_CXO_D0] = &monaco_cxo_d0.hw,
+	[RPM_SMD_CXO_D0_A] = &monaco_cxo_d0_a.hw,
 };
 
 static const struct rpm_smd_clk_desc rpm_clk_monaco = {
