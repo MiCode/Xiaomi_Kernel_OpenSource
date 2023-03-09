8d5d653f1c71e7598202f6bad364a4290bd8acc8
Raviteja Laggyshetty <quic_rlaggysh@quicinc.com>
2023-03-09 14:53:49 +0530

    interconnect: qcom: sdxpinn: Mark QUP BCMs as keepalive
    Initial proxy bandwidth votes get removed after sync state 
    is achieved with keepalive_ealry flag. This causes QUP BCMs to 
    turn off prematurely which leads to bus error. Marking QUP BCMs 
    as keepalive instead of keepalive_early to avoid the 
    race condition between the sync_state and QUP clients requests.
    
    Change-Id: Ibe3a4b5d026b96f101308e498d826c5196dc5307
    Signed-off-by: Raviteja Laggyshetty <quic_rlaggysh@quicinc.com>

--- drivers/interconnect/qcom/sdxpinn.c
+++ drivers/interconnect/qcom/sdxpinn.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
  *
  */
 
@@ -1215,7 +1215,7 @@ static struct qcom_icc_bcm bcm_qp0 = {
 static struct qcom_icc_bcm bcm_qup0 = {
 	.name = "QUP0",
 	.voter_idx = 0,
-	.keepalive_early = true,
+	.keepalive = true,
 	.vote_scale = 1,
 	.num_nodes = 1,
 	.nodes = { &qup0_core_slave },
