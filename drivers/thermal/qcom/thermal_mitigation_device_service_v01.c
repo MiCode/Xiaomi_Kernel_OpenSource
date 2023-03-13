7a5a3a4e798d451bba197cf2b2937feb6327ae9f
Priyansh Jain <quic_priyjain@quicinc.com>
2023-03-13 20:17:00 +0530

    drivers: thermal: qmi_cooling: Add support for extended device list
    The QMI TMD get device list supports maximum 32 devices. But there
    are targets that support more than 32 QMI TMD devices. To add more
    devices support as well as to make sure backward compatibility with
    legacy device list, add new optional extended device list variable
    in QMI TMD interface. The first 32 devices will be filled in legacy
    device list variable and all other devices will be filled with new
    extended device list variable.
    
    Add support to qmi cooling device driver to get extended device
    list if any.
    
    Change-Id: I530669a9f89f8c9da7bbcc7d486dde76f0a7f3ea
    Signed-off-by: Manaf Meethalavalappu Pallikunhi <quic_manafm@quicinc.com>
    Signed-off-by: Priyansh Jain <quic_priyjain@quicinc.com>

--- drivers/thermal/qcom/thermal_mitigation_device_service_v01.c
+++ drivers/thermal/qcom/thermal_mitigation_device_service_v01.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2017-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2021-2022,2023, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/soc/qcom/qmi.h>
@@ -104,6 +105,38 @@ struct qmi_elem_info tmd_get_mitigation_device_list_resp_msg_v01_ei[] = {
 		.offset         = offsetof(
 			struct tmd_get_mitigation_device_list_resp_msg_v01,
 				mitigation_device_list),
+		.ei_array      = tmd_mitigation_dev_list_type_v01_ei,
+	},
+	{
+		.data_type      = QMI_OPT_FLAG,
+		.elem_len       = 1,
+		.elem_size      = sizeof(uint8_t),
+		.array_type     = NO_ARRAY,
+		.tlv_type       = 0x11,
+		.offset         = offsetof(
+			struct tmd_get_mitigation_device_list_resp_msg_v01,
+				mitigation_device_list_ext01_valid),
+	},
+	{
+		.data_type      = QMI_DATA_LEN,
+		.elem_len       = 1,
+		.elem_size      = sizeof(uint8_t),
+		.array_type     = NO_ARRAY,
+		.tlv_type       = 0x11,
+		.offset         = offsetof(
+			struct tmd_get_mitigation_device_list_resp_msg_v01,
+				mitigation_device_list_ext01_len),
+	},
+	{
+		.data_type      = QMI_STRUCT,
+		.elem_len       = QMI_TMD_MITIGATION_DEV_LIST_EXT01_MAX_V01,
+		.elem_size      = sizeof(
+				struct tmd_mitigation_dev_list_type_v01),
+		.array_type     = VAR_LEN_ARRAY,
+		.tlv_type       = 0x11,
+		.offset         = offsetof(
+			struct tmd_get_mitigation_device_list_resp_msg_v01,
+				mitigation_device_list_ext01),
 		.ei_array      = tmd_mitigation_dev_list_type_v01_ei,
 	},
 	{
