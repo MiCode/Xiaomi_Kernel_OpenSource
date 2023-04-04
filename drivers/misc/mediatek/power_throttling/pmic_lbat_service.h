c05eba0601be9ea5bb68b4729ec898a3d05edabf
mtk24676 <C.Cheng@mediatek.com>
2023-04-04 13:49:37 +0800

    [ALPS07848166] power throttling : code rodustness adjustment
    code rodustness adjustment.
    
    MTK-Commit-Id: ccee3a951378e3923d2cf9690185fda736a8ed50
    
    Signed-off-by: mtk24676 <C.Cheng@mediatek.com>
    CR-Id: ALPS07848166
    Feature: Dynamic load power throttling
    Change-Id: I8ce922869638efa522190737a7a1e59a3015e99b

--- drivers/misc/mediatek/power_throttling/pmic_lbat_service.h
+++ drivers/misc/mediatek/power_throttling/pmic_lbat_service.h
@@ -8,6 +8,7 @@
 
 struct lbat_user;
 
+#if IS_ENABLED(CONFIG_PMIC_LBAT_SERVICE)
 /* extern function */
 struct lbat_user *lbat_user_register_ext(const char *name, unsigned int *thd_volt_arr,
 					 unsigned int thd_volt_size,
@@ -21,5 +22,34 @@ int lbat_user_set_debounce(struct lbat_user *user,
 			   unsigned int lv_deb_prd, unsigned int lv_deb_times);
 unsigned int lbat_read_raw(void);
 unsigned int lbat_read_volt(void);
+#else
+static inline struct lbat_user *lbat_user_register_ext(const char *name, unsigned int *thd_volt_arr,
+					 unsigned int thd_volt_size,
+					 void (*callback)(unsigned int thd_volt))
+{
+	return NULL;
+}
+static inline struct lbat_user *lbat_user_register(const char *name, unsigned int hv_thd_volt,
+				     unsigned int lv1_thd_volt,
+				     unsigned int lv2_thd_volt,
+				     void (*callback)(unsigned int thd_volt))
+{
+	return NULL;
+}
+static inline int lbat_user_set_debounce(struct lbat_user *user,
+			   unsigned int hv_deb_prd, unsigned int hv_deb_times,
+			   unsigned int lv_deb_prd, unsigned int lv_deb_times)
+{
+	return 0;
+}
+static inline unsigned int lbat_read_raw(void)
+{
+	return 0;
+}
+static inline unsigned int lbat_read_volt(void)
+{
+	return 0;
+}
 
+#endif
 #endif	/* __MTK_LBAT_SERVICE_H__ */
