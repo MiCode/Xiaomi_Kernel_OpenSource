768c92d180aa92f86408df5fa8ef13c4b92e1bc2
Bai <linzong.bai@mediatek.com>
2023-04-04 13:52:12 +0800

    [ALPS07921680] add logic of CON_SCE
    For legacy chip, set different bandwidth by scenario.
    
    bandwidth_control_port.cpp will call us via
    /sys/bus/platform/drivers/emi_ctrl/concurrency_scenario.
    
    PS:
    new chip will auto fit bandwidth, no need this logic.
    
    MTK-Commit-Id: d0e12503a4d5749f33156876a9d5b2f1e1bfcf6e
    
    Signed-off-by: Bai<linzong.bai@mediatek.com>
    CR-Id: ALPS07921680
    Feature: emi
    Change-Id: I058be234fe155903fd51db4fb9d071611a3335a1
    (cherry picked from commit e50853b8010e2ecf2d0997f6541d023876d92924)

--- drivers/memory/mediatek/emi_legacy_v0/emi_ctrl.c
+++ drivers/memory/mediatek/emi_legacy_v0/emi_ctrl.c
@@ -16,6 +16,7 @@
 #include <linux/printk.h>
 #include <linux/spinlock.h>
 #include <linux/delay.h>
+#include <mt-plat/mtk_io.h>
 #include <mt-plat/sync_write.h>
 
 #include <soc/mediatek/emi_legacy_v0.h>
@@ -39,7 +40,7 @@ struct emi_info_t {
 };
 static struct emi_info_t emi_info;
 
-/*extern void mpu_init(void);*/
+// extern void mpu_init(void);
 
 unsigned int get_ch_num(void)
 {
@@ -112,10 +113,241 @@ static ssize_t elm_ctrl_store(struct device_driver *driver,
 }
 static DRIVER_ATTR_RW(elm_ctrl);
 
+
+/* BWL Begin */
+
+DEFINE_SEMAPHORE(emi_bwl_sem);
+
+#define DRIVER_ATTR(_name, _mode, _show, _store) \
+	struct driver_attribute driver_attr_##_name = \
+	__ATTR(_name, _mode, _show, _store)
+
+/* define EMI bandwiwth limiter control table */
+static struct emi_bwl_ctrl ctrl_tbl[NR_CON_SCE];
+
+/* current concurrency scenario */
+static int cur_con_sce = 0x0FFFFFFF;
+
+/* define concurrency scenario strings */
+static const char * const con_sce_str[] = {
+#define X_CON_SCE(con_sce, arba, arbb, arbc, arbd, arbe, arbf, arbg, arbh, \
+conm, mdct) (#con_sce),
+#include "con_sce_lpddr3.h"
+#undef X_CON_SCE
+};
+
+/****************** For LPDDR4-3200******************/
+
+static const unsigned int emi_arba_lpddr4_val[] = {
+#define X_CON_SCE(con_sce, arba, arbb, arbc, arbd, arbe, arbf, arbg, arbh, \
+conm, mdct) arba,
+#include "con_sce_lpddr3.h"
+#undef X_CON_SCE
+};
+static const unsigned int emi_arbb_lpddr4_val[] = {
+#define X_CON_SCE(con_sce, arba, arbb, arbc, arbd, arbe, arbf, arbg, arbh, \
+conm, mdct) arbb,
+#include "con_sce_lpddr3.h"
+#undef X_CON_SCE
+};
+static const unsigned int emi_arbc_lpddr4_val[] = {
+#define X_CON_SCE(con_sce, arba, arbb, arbc, arbd, arbe, arbf, arbg, arbh, \
+conm, mdct) arbc,
+#include "con_sce_lpddr3.h"
+#undef X_CON_SCE
+};
+static const unsigned int emi_arbd_lpddr4_val[] = {
+#define X_CON_SCE(con_sce, arba, arbb, arbc, arbd, arbe, arbf, arbg, arbh, \
+conm, mdct) arbd,
+#include "con_sce_lpddr3.h"
+#undef X_CON_SCE
+};
+static const unsigned int emi_arbe_lpddr4_val[] = {
+#define X_CON_SCE(con_sce, arba, arbb, arbc, arbd, arbe, arbf, arbg, arbh, \
+conm, mdct) arbe,
+#include "con_sce_lpddr3.h"
+#undef X_CON_SCE
+};
+static const unsigned int emi_arbf_lpddr4_val[] = {
+#define X_CON_SCE(con_sce, arba, arbb, arbc, arbd, arbe, arbf, arbg, arbh, \
+conm, mdct) arbf,
+#include "con_sce_lpddr3.h"
+#undef X_CON_SCE
+};
+static const unsigned int emi_arbg_lpddr4_val[] = {
+#define X_CON_SCE(con_sce, arba, arbb, arbc, arbd, arbe, arbf, arbg, arbh, \
+conm, mdct) arbg,
+#include "con_sce_lpddr3.h"
+#undef X_CON_SCE
+};
+static const unsigned int emi_arbh_lpddr4_val[] = {
+#define X_CON_SCE(con_sce, arba, arbb, arbc, arbd, arbe, arbf, arbg, arbh, \
+conm, mdct) arbh,
+#include "con_sce_lpddr3.h"
+#undef X_CON_SCE
+};
+static const unsigned int emi_conm_lpddr4_val[] = {
+#define X_CON_SCE(con_sce, arba, arbb, arbc, arbd, arbe, arbf, arbg, arbh, \
+conm, mdct) conm,
+#include "con_sce_lpddr3.h"
+#undef X_CON_SCE
+};
+static const unsigned int emi_mdct_lpddr4_val[] = {
+#define X_CON_SCE(con_sce, arba, arbb, arbc, arbd, arbe, arbf, arbg, arbh, \
+conm, mdct) mdct,
+#include "con_sce_lpddr3.h"
+#undef X_CON_SCE
+};
+
+/*
+ * mtk_mem_bw_ctrl: set EMI bandwidth limiter for memory bandwidth control
+ * @sce: concurrency scenario ID
+ * @op: either ENABLE_CON_SCE or DISABLE_CON_SCE
+ * Return 0 for success; return negative values for failure.
+ */
+int mtk_mem_bw_ctrl(int sce, int op)
+{
+	int i, highest;
+
+	if (sce >= NR_CON_SCE)
+		return -1;
+
+	if (op != ENABLE_CON_SCE && op != DISABLE_CON_SCE)
+		return -1;
+
+	if (in_interrupt())
+		return -1;
+
+	down(&emi_bwl_sem);
+
+	if (op == ENABLE_CON_SCE)
+		ctrl_tbl[sce].ref_cnt++;
+
+	else if (op == DISABLE_CON_SCE) {
+		if (ctrl_tbl[sce].ref_cnt != 0)
+			ctrl_tbl[sce].ref_cnt--;
+	}
+
+	/* find the scenario with the highest priority */
+	highest = -1;
+	for (i = 0; i < NR_CON_SCE; i++) {
+		if (ctrl_tbl[i].ref_cnt != 0) {
+			highest = i;
+			break;
+		}
+	}
+	if (highest == -1)
+		highest = CON_SCE_UI;
+
+	/* set new EMI bandwidth limiter value */
+	if (highest != cur_con_sce) {
+		writel(emi_arba_lpddr4_val[highest], EMI_ARBA);
+		writel(emi_arbb_lpddr4_val[highest], EMI_ARBB);
+		writel(emi_arbc_lpddr4_val[highest], EMI_ARBC);
+		writel(emi_arbd_lpddr4_val[highest], EMI_ARBD);
+		writel(emi_arbe_lpddr4_val[highest], EMI_ARBE);
+		writel(emi_arbf_lpddr4_val[highest], EMI_ARBF);
+		writel(emi_arbg_lpddr4_val[highest], EMI_ARBG);
+		writel(emi_arbh_lpddr4_val[highest], EMI_ARBH);
+		writel(emi_conm_lpddr4_val[highest], EMI_CONM);
+		mt_reg_sync_writel(emi_mdct_lpddr4_val[highest], EMI_MDCT);
+		cur_con_sce = highest;
+	}
+
+	up(&emi_bwl_sem);
+
+	return 0;
+}
+
+/*
+ * con_sce_show: sysfs con_sce file show function.
+ * @driver:
+ * @buf:
+ * Return the number of read bytes.
+ */
+static ssize_t con_sce_show(struct device_driver *driver, char *buf)
+{
+	char *ptr = buf;
+	int i = 0;
+
+	if (cur_con_sce >= NR_CON_SCE) {
+		ptr += sprintf(ptr, "none\n");
+		return strlen(buf);
+	}
+	ptr += snprintf(ptr, 64, "current scenario: %s\n", con_sce_str[cur_con_sce]);
+
+	ptr += snprintf(ptr, 32, "%s\n", con_sce_str[cur_con_sce]);
+	ptr += sprintf(ptr, "EMI_ARBA = 0x%x\n",  readl(IOMEM(EMI_ARBA)));
+	ptr += sprintf(ptr, "EMI_ARBB = 0x%x\n",  readl(IOMEM(EMI_ARBB)));
+	ptr += sprintf(ptr, "EMI_ARBC = 0x%x\n",  readl(IOMEM(EMI_ARBC)));
+	ptr += sprintf(ptr, "EMI_ARBD = 0x%x\n",  readl(IOMEM(EMI_ARBD)));
+	ptr += sprintf(ptr, "EMI_ARBE = 0x%x\n",  readl(IOMEM(EMI_ARBE)));
+	ptr += sprintf(ptr, "EMI_ARBF = 0x%x\n",  readl(IOMEM(EMI_ARBF)));
+	ptr += sprintf(ptr, "EMI_ARBG = 0x%x\n",  readl(IOMEM(EMI_ARBG)));
+	ptr += sprintf(ptr, "EMI_ARBH = 0x%x\n",  readl(IOMEM(EMI_ARBH)));
+	ptr += sprintf(ptr, "EMI_CONM = 0x%x\n",  readl(IOMEM(EMI_CONM)));
+	ptr += sprintf(ptr, "EMI_MDCT = 0x%x\n",  readl(IOMEM(EMI_MDCT)));
+	for (i = 0; i < NR_CON_SCE; i++)
+		ptr += snprintf(ptr, 64, "%s = 0x%x\n",
+			con_sce_str[i], ctrl_tbl[i].ref_cnt);
+
+	pr_debug("[EMI BWL] EMI_ARBA = 0x%x\n", readl(IOMEM(EMI_ARBA)));
+	pr_debug("[EMI BWL] EMI_ARBB = 0x%x\n", readl(IOMEM(EMI_ARBB)));
+	pr_debug("[EMI BWL] EMI_ARBC = 0x%x\n", readl(IOMEM(EMI_ARBC)));
+	pr_debug("[EMI BWL] EMI_ARBD = 0x%x\n", readl(IOMEM(EMI_ARBD)));
+	pr_debug("[EMI BWL] EMI_ARBE = 0x%x\n", readl(IOMEM(EMI_ARBE)));
+	pr_debug("[EMI BWL] EMI_ARBF = 0x%x\n", readl(IOMEM(EMI_ARBF)));
+	pr_debug("[EMI BWL] EMI_ARBG = 0x%x\n", readl(IOMEM(EMI_ARBG)));
+	pr_debug("[EMI BWL] EMI_ARBH = 0x%x\n", readl(IOMEM(EMI_ARBH)));
+	pr_debug("[EMI BWL] EMI_CONM = 0x%x\n", readl(IOMEM(EMI_CONM)));
+	pr_debug("[EMI BWL] EMI_MDCT = 0x%x\n", readl(IOMEM(EMI_MDCT)));
+
+	return strlen(buf);
+
+}
+
+/*
+ * con_sce_store: sysfs con_sce file store function.
+ * @driver:
+ * @buf:
+ * @count:
+ * Return the number of write bytes.
+ */
+static ssize_t con_sce_store(struct device_driver *driver,
+const char *buf, size_t count)
+{
+	int i;
+
+	for (i = 0; i < NR_CON_SCE; i++) {
+		if (!strncmp(buf, con_sce_str[i], strlen(con_sce_str[i]))) {
+			if (!strncmp(buf + strlen(con_sce_str[i]) + 1,
+				EN_CON_SCE_STR, strlen(EN_CON_SCE_STR))) {
+
+				mtk_mem_bw_ctrl(i, ENABLE_CON_SCE);
+				/* pr_debug("concurrency scenario %s ON\n", con_sce_str[i]); */
+				break;
+			} else if (!strncmp(buf + strlen(con_sce_str[i]) + 1,
+				DIS_CON_SCE_STR, strlen(DIS_CON_SCE_STR))) {
+
+				mtk_mem_bw_ctrl(i, DISABLE_CON_SCE);
+				/* pr_debug("concurrency scenario %s OFF\n", con_sce_str[i]); */
+				break;
+			}
+		}
+	}
+	return count;
+}
+
+DRIVER_ATTR(concurrency_scenario, 0644, con_sce_show, con_sce_store);
+
+/* BWL End */
+
+
 static int emi_probe(struct platform_device *pdev)
 {
 	struct resource *res;
 	struct device_node *node = pdev->dev.of_node;
+	int ret;
 
 	pr_debug("[EMI] module probe.\n");
 
@@ -149,6 +381,10 @@ static int emi_probe(struct platform_device *pdev)
 	pr_info("[EMI] get CEN_EMI_BASE @ %p\n", mt_cen_emi_base_get());
 	pr_info("[EMI] get CHA_EMI_BASE @ %p\n", mt_chn_emi_base_get());
 	pr_info("[EMI] get EMI_MPU_BASE @ %p\n", mt_emi_mpu_base_get());
+
+	ret = mtk_mem_bw_ctrl(CON_SCE_UI, ENABLE_CON_SCE);
+	if (ret)
+		pr_info("[EMI/BWL] fail to set EMI bandwidth limiter\n");
 
 	writel(0x00000040, CHA_EMI_BASE+0x0008);
 	mt_reg_sync_writel(0x00000913, CEN_EMI_BASE+0x5B0);
@@ -207,6 +443,10 @@ static int __init emi_ctrl_init(void)
 	ret = platform_driver_register(&emi_ctrl);
 	if (ret)
 		pr_info("[EMI/BWL] fail to register emi_ctrl driver\n");
+
+	ret = driver_create_file(&emi_ctrl.driver, &driver_attr_concurrency_scenario);
+	if (ret)
+		pr_info("[EMI/BWL] fail to create emi_bwl sysfs file\n");
 
 	ret = driver_create_file(&emi_ctrl.driver, &driver_attr_elm_ctrl);
 	if (ret)
--- drivers/memory/mediatek/emi_legacy_v0/emi_ctrl.h
+++ drivers/memory/mediatek/emi_legacy_v0/emi_ctrl.h
@@ -6,11 +6,53 @@
 #ifndef __EMI_BWL_H__
 #define __EMI_BWL_H__
 
+
+/* BWL Begin */
+
+/*
+ * Define EMI hardware registers.
+ */
+#define EMI_CONM		(CEN_EMI_BASE + 0x0060)
+#define EMI_MDCT		(CEN_EMI_BASE + 0x0078)
+#define EMI_ARBA		(CEN_EMI_BASE + 0x0100)
+#define EMI_ARBB		(CEN_EMI_BASE + 0x0108)
+#define EMI_ARBC		(CEN_EMI_BASE + 0x0110)
+#define EMI_ARBD		(CEN_EMI_BASE + 0x0118)
+#define EMI_ARBE		(CEN_EMI_BASE + 0x0120)
+#define EMI_ARBF		(CEN_EMI_BASE + 0x0128)
+#define EMI_ARBG		(CEN_EMI_BASE + 0x0130)
+#define EMI_ARBH		(CEN_EMI_BASE + 0x0138)
+
+/* define concurrency scenario ID */
+enum {
+#define X_CON_SCE(con_sce, arba, arbb, arbc, arbd, arbe, arbf, arbg, arbh, \
+conm, mdct) con_sce,
+#include "con_sce_lpddr3.h"
+#undef X_CON_SCE
+	NR_CON_SCE
+};
+
+/* define control operation */
+enum {
+	ENABLE_CON_SCE = 0,
+	DISABLE_CON_SCE = 1
+};
+
+#define EN_CON_SCE_STR "ON"
+#define DIS_CON_SCE_STR "OFF"
+
+/* define control table entry */
+struct emi_bwl_ctrl {
+	unsigned int ref_cnt;
+};
+
+/* BWL End */
+
 unsigned int get_dram_type(void);
 void __iomem *mt_cen_emi_base_get(void);
 void __iomem *mt_chn_emi_base_get(void);
 void __iomem *mt_emi_mpu_base_get(void);
 unsigned int mt_emi_mpu_irq_get(void);
-extern void mpu_init(void);
-extern unsigned int get_dram_type(void);
+extern int mpu_init(void);
+
 #endif  /* !__EMI_BWL_H__ */
