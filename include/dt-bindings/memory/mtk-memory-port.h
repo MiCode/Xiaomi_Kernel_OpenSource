3ee8da3e1f9c98a1cd9c7aedc4754d5b6358ee3a
mingyuan ma <mingyuan.ma@mediatek.com>
2023-04-04 13:49:40 +0800

    [ALPS07895386] iommu/dt-binding: Revise tab_id max definition
    Adjust MTK_M4U_TAB_NR_MAX 3 to avoid size of structure which using this
    definition is too large, and IOMMU only use 3 tab id now.
    
    MTK-Commit-Id: c49e2f4387afc5713c4f63fa6120aed40ef89381
    
    Signed-off-by: mingyuan ma <mingyuan.ma@mediatek.com>
    CR-Id: ALPS07895386
    Feature: iommu_kernel
    Change-Id: If0cf379547349a73c43fe3128dee926107104539

--- include/dt-bindings/memory/mtk-memory-port.h
+++ include/dt-bindings/memory/mtk-memory-port.h
@@ -8,12 +8,12 @@
 
 #define MTK_LARB_NR_MAX			64
 #define MTK_M4U_DOM_NR_MAX		16
-#define MTK_M4U_TAB_NR_MAX		16
+#define MTK_M4U_TAB_NR_MAX		3
 
-/* tab_id[23:20] + dom[19:16] + larb[10:5] + port[4:0] */
+/* tab_id[21:20] + dom[19:16] + larb[10:5] + port[4:0] */
 #define TAB_ID				(0)
 
-#define MTK_M4U_PORT_ID(tab, dom, larb, port)	(((tab & 0xf) << 20) | ((dom & 0xf) << 16) |\
+#define MTK_M4U_PORT_ID(tab, dom, larb, port)	(((tab & 0x3) << 20) | ((dom & 0xf) << 16) |\
 						((larb & 0x3f) << 5) | (port & 0x1f))
 
 #define MTK_M4U_DOM_ID(dom, larb, port)	MTK_M4U_PORT_ID(TAB_ID, dom, larb, port)
@@ -21,6 +21,6 @@
 #define MTK_M4U_TO_LARB(id)		(((id) >> 5) & 0x3f)
 #define MTK_M4U_TO_PORT(id)		((id) & 0x1f)
 #define MTK_M4U_TO_DOM(id)		(((id) >> 16) & 0xf)
-#define MTK_M4U_TO_TAB(id)		(((id) >> 20) & 0xf)
+#define MTK_M4U_TO_TAB(id)		(((id) >> 20) & 0x3)
 
 #endif
