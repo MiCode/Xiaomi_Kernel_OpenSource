84224118b2ae80e36596c8872bde3686c8058a33
Kamal Agrawal <quic_kamaagra@quicinc.com>
2023-04-07 15:28:03 +0530

    msm: kgsl: Restrict gpu idle check to idle timer callback
    Device suspend path and last close don't wait for the idle timer
    duration before triggering power off sequence. This causes the
    gpu idle check to fail if gpu was executing IFPC sequence.
    Therefore, restrict the gpu idle check only when the idle timer
    triggers power off sequence.
    
    Change-Id: I73b75f34b040feeef6d1e1f4a8f3774928d51153
    Signed-off-by: Kamal Agrawal <quic_kamaagra@quicinc.com>

--- drivers/gpu/msm/adreno_a6xx_hwsched.c
+++ drivers/gpu/msm/adreno_a6xx_hwsched.c
@@ -801,9 +801,6 @@ static int a6xx_hwsched_power_off(struct adreno_device *adreno_dev)
 	/* process any profiling results that are available */
 	adreno_profile_process_results(ADRENO_DEVICE(device));
 
-	if (!a6xx_hw_isidle(adreno_dev))
-		dev_err(&gmu->pdev->dev, "GPU isn't idle before SLUMBER\n");
-
 	ret = a6xx_gmu_oob_set(device, oob_gpu);
 	if (ret) {
 		a6xx_gmu_oob_clear(device, oob_gpu);
@@ -866,6 +863,10 @@ static void hwsched_idle_check(struct work_struct *work)
 
 	if (!atomic_read(&device->active_cnt) &&
 		time_is_before_eq_jiffies(device->idle_jiffies)) {
+		if (!a6xx_hw_isidle(adreno_dev)) {
+			dev_err(device->dev, "GPU isn't idle before SLUMBER\n");
+			gmu_core_fault_snapshot(device);
+		}
 		a6xx_hwsched_power_off(adreno_dev);
 	} else {
 		kgsl_pwrscale_update(device);
