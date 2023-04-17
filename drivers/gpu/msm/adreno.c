cb3763083f04fb1ffbe9be57e7b0cfe3e5cdc0a2
Harshitha Sai Neelati <quic_hsaineel@quicinc.com>
2023-04-17 16:52:47 +0530

    msm: kgsl: Use reset_control_reset() API for freq limiter irq clear
    Currently freq limiter irq is not getting cleared as expected.
    reset_control_assert() API internally calls regmap_update_bits() to assert
    the actual h/w register for irq clear. When the irq handler is called for
    the second time, regmap_update_bits() is not asserting the actual h/w
    register assuming it is redundant since the register value is already
    set (set when handler is called for the first time).
    
    reset_control_reset() API asserts and deasserts the register with a small
    delay in between. Using this API will help to clear the freq limiter irq
    every time.
    
    Change-Id: I03d0c6ab5abc338f711ccbfdcb08c5a9a110cbf5
    Signed-off-by: Harshitha Sai Neelati <quic_hsaineel@quicinc.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -424,7 +424,7 @@ static irqreturn_t adreno_freq_limiter_irq_handler(int irq, void *data)
 		pwr->pwrlevels[pwr->active_pwrlevel].gpu_freq,
 		pwr->pwrlevels[pwr->previous_pwrlevel].gpu_freq);
 
-	reset_control_assert(device->freq_limiter_irq_clear);
+	reset_control_reset(device->freq_limiter_irq_clear);
 
 	return IRQ_HANDLED;
 }
