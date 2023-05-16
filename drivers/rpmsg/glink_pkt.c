8d2a712429b532ae64b363ec54024d4248a5f4d1
Linux Image Build Automation <quic_ibautomat@quicinc.com>
2023-05-15 17:37:35 -0700

    Revert "rpmsg: glink_pkt: Cache rx done and add helper"
    This reverts commit d147ac26159ba599c5380e556f1992c532547934.
    
    Change-Id: Ie030beea251337d9a2faff74923c2cd0caebcc68
    Signed-off-by: Linux Image Build Automation <quic_ibautomat@quicinc.com>

--- drivers/rpmsg/glink_pkt.c
+++ drivers/rpmsg/glink_pkt.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-only
-/* Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved. */
+/* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved. */
 
 #include <linux/platform_device.h>
 #include <linux/ipc_logging.h>
@@ -68,7 +68,6 @@ static DEFINE_IDA(glink_pkt_minor_ida);
  * @ch_open:	wait object for opening the glink channel
  * @refcount:	count how many userspace clients have handles
  * @rpdev:	underlaying rpmsg device
- * @rx_done:	cache whether rpdev can support external rx done
  * @queue_lock:	synchronization of @queue operations
  * @queue:	incoming message queue
  * @readq:	wait object for incoming queue
@@ -91,7 +90,6 @@ struct glink_pkt_device {
 	struct completion ch_open;
 	refcount_t refcount;
 	struct rpmsg_device *rpdev;
-	bool rx_done;
 
 	spinlock_t queue_lock;
 	struct sk_buff_head queue;
@@ -151,21 +149,6 @@ static ssize_t open_timeout_show(struct device *dev,
 
 static DEVICE_ATTR_RW(open_timeout);
 
-static void glink_pkt_kfree_skb(struct glink_pkt_device *gpdev, struct sk_buff *skb)
-{
-	if (gpdev->rx_done) {
-		qcom_glink_rx_done(gpdev->rpdev->ept, skb->data);
-		/*
-		 * Data memory is freed by qcom_glink_rx_done(), reset the
-		 * skb data pointers so kfree_skb() does not try to free
-		 * a second time.
-		 */
-		skb->head = NULL;
-		skb->data = NULL;
-	}
-	kfree_skb(skb);
-}
-
 static int glink_pkt_rpdev_no_copy_cb(struct rpmsg_device *rpdev, void *buf,
 				      int len, void *priv, u32 addr)
 {
@@ -224,8 +207,8 @@ static int glink_pkt_rpdev_copy_cb(struct rpmsg_device *rpdev, void *buf,
 static int glink_pkt_rpdev_cb(struct rpmsg_device *rpdev, void *buf, int len,
 			      void *priv, u32 addr)
 {
-	struct glink_pkt_device *gpdev = dev_get_drvdata(&rpdev->dev);
-	rpmsg_rx_cb_t cb = (gpdev->rx_done) ? glink_pkt_rpdev_no_copy_cb : glink_pkt_rpdev_copy_cb;
+	rpmsg_rx_cb_t cb = (qcom_glink_rx_done_supported(rpdev->ept)) ? glink_pkt_rpdev_no_copy_cb :
+									glink_pkt_rpdev_copy_cb;
 
 	return cb(rpdev, buf, len, priv, addr);
 }
@@ -256,7 +239,6 @@ static int glink_pkt_rpdev_probe(struct rpmsg_device *rpdev)
 
 	mutex_lock(&gpdev->lock);
 	gpdev->rpdev = rpdev;
-	gpdev->rx_done = (qcom_glink_rx_done_supported(rpdev->ept) > 0) ? true : false;
 	qcom_glink_register_signals_cb(rpdev->ept, glink_pkt_rpdev_sigs);
 	mutex_unlock(&gpdev->lock);
 
@@ -357,16 +339,24 @@ static int glink_pkt_release(struct inode *inode, struct file *file)
 		spin_lock_irqsave(&gpdev->queue_lock, flags);
 
 		if (gpdev->rskb) {
-			glink_pkt_kfree_skb(gpdev, gpdev->rskb);
+			kfree_skb(gpdev->rskb);
 			gpdev->rskb = NULL;
 			gpdev->rdata = NULL;
 			gpdev->rdata_len = 0;
 		}
 
 		/* Discard all SKBs */
-		while ((skb = skb_dequeue(&gpdev->queue)))
-			glink_pkt_kfree_skb(gpdev, skb);
+		while (!skb_queue_empty(&gpdev->queue)) {
+			skb = skb_dequeue(&gpdev->queue);
 
+			if (qcom_glink_rx_done_supported(gpdev->rpdev->ept)) {
+				qcom_glink_rx_done(gpdev->rpdev->ept, skb->data);
+				skb->head = NULL;
+				skb->data = NULL;
+			}
+
+			kfree_skb(skb);
+		}
 		wake_up_interruptible(&gpdev->readq);
 		gpdev->sig_change = false;
 		spin_unlock_irqrestore(&gpdev->queue_lock, flags);
@@ -471,8 +461,19 @@ static ssize_t glink_pkt_read(struct file *file,
 	}
 	spin_unlock_irqrestore(&gpdev->queue_lock, flags);
 
-	if (skb)
-		glink_pkt_kfree_skb(gpdev, skb);
+	if (skb) {
+		if (qcom_glink_rx_done_supported(gpdev->rpdev->ept)) {
+			qcom_glink_rx_done(gpdev->rpdev->ept, skb->data);
+			/*
+			 * Data memory is freed by qcom_glink_rx_done(), reset the
+			 * skb data pointers so kfree_skb() does not try to free
+			 * a second time.
+			 */
+			skb->head = NULL;
+			skb->data = NULL;
+		}
+		kfree_skb(skb);
+	}
 
 	ret = (ret < 0) ? ret : use;
 	GLINK_PKT_INFO("end for %s by %s:%d ret[%d], remaining[%d]\n", gpdev->ch_name,
@@ -659,7 +660,7 @@ static int glink_pkt_zerocopy_done(struct glink_pkt_device *gpdev,
 	if (!PAGE_ALIGNED(address) || address != zc->address)
 		return -EINVAL;
 
-	if (!gpdev->rx_done)
+	if (!qcom_glink_rx_done_supported(gpdev->rpdev->ept))
 		return -EINVAL;
 
 	mmap_read_lock(current->mm);
@@ -688,11 +689,18 @@ static int glink_pkt_zerocopy_done(struct glink_pkt_device *gpdev,
 	if (!skb)
 		return -EINVAL;
 
+	qcom_glink_rx_done(gpdev->rpdev->ept, skb->data);
 	if (cb->trailing_page)
 		free_page(cb->trailing_page);
 	if (cb->leading_page)
 		free_page(cb->leading_page);
-	glink_pkt_kfree_skb(gpdev, skb);
+	/*
+	 * Data memory is freed by qcom_glink_rx_done(), reset the skb data
+	 * pointers so kfree_skb() does not try to free a second time.
+	 */
+	skb->head = NULL;
+	skb->data = NULL;
+	kfree_skb(skb);
 
 	return 0;
 }
@@ -724,7 +732,7 @@ static int glink_pkt_zerocopy_receive(struct glink_pkt_device *gpdev,
 	if (!PAGE_ALIGNED(address) || address != zc->address)
 		return -EINVAL;
 
-	if (!gpdev->rx_done)
+	if (!qcom_glink_rx_done_supported(gpdev->rpdev->ept))
 		return -EINVAL;
 
 	zc->offset = 0;
@@ -1121,7 +1129,6 @@ static int glink_pkt_create_device(struct device *parent,
 	/* Default open timeout for open is 120 sec */
 	gpdev->open_tout = 120;
 	gpdev->sig_change = false;
-	gpdev->rx_done = false;
 
 	spin_lock_init(&gpdev->queue_lock);
 
