MotorolaMobilityLLC/kernel-msm

interconnect: qcom: icc-rpmh: Add support for No Display and No Camera
Add support for No display and No camera by marking ICC nodes, BCMs and
voters as disabled based on the fuses read during the probe. Initialize
the providers based on the disabled nodes, so that multimedia providers
are probed successfully.

Change-Id: Ieda7177a7abc04947210b7493e570ba66ff17b22
Signed-off-by: Raviteja Laggyshetty <quic_rlaggysh@quicinc.com>

drivers/interconnect/qcom/icc-rpmh.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/clk.h>
@@ -11,6 +12,7 @@
 #include <linux/of_platform.h>
 #include <linux/of_device.h>
 #include <linux/slab.h>
+#include <soc/qcom/socinfo.h>
 
 #include "bcm-voter.h"
 #include "icc-debug.h"
@@ -200,7 +202,7 @@ int qcom_icc_bcm_init(struct qcom_icc_provider *qp, struct qcom_icc_bcm *bcm,
 	int i;
 
 	/* BCM is already initialised*/
-	if (bcm->addr)
+	if (bcm->disabled || bcm->addr)
 		return 0;
 
 	bcm->addr = cmd_db_read_addr(bcm->name);
@@ -369,6 +371,51 @@ static struct regmap *qcom_icc_rpmh_map(struct platform_device *pdev,
 	return devm_regmap_init_mmio(dev, base, desc->config);
 }
 
+static bool is_voter_disabled(char *voter)
+{
+	if ((strnstr(voter, "disp", strlen(voter)) && socinfo_get_part_info(PART_DISPLAY)) ||
+	    (strnstr(voter, "cam", strlen(voter)) && socinfo_get_part_info(PART_CAMERA)))
+		return true;
+
+	return false;
+}
+
+static int qcom_icc_init_disabled_parts(struct qcom_icc_provider *qp)
+{
+	struct qcom_icc_bcm *bcm;
+	struct qcom_icc_node **qnodes, *qn;
+	const struct qcom_icc_desc *desc;
+	int voter_idx, i, j;
+	char *voter_name;
+
+	desc = of_device_get_match_data(qp->dev);
+	if (!desc)
+		return -EINVAL;
+
+	for (i = 0; i < qp->num_bcms; i++) {
+		bcm = qp->bcms[i];
+		voter_idx = bcm->voter_idx;
+		voter_name = desc->voters[voter_idx];
+
+		/* Disable BCMs incase of NO display or No Camera */
+		if (is_voter_disabled(voter_name)) {
+			bcm->disabled = true;
+			qnodes = qp->nodes;
+
+			for (j = 0; j < qp->num_nodes; j++) {
+				qn = qnodes[j];
+				if (!qn)
+					continue;
+
+				if (strnstr(qn->name, voter_name, strlen(qn->name)))
+					qn->disabled = true;
+			}
+		}
+	}
+
+	return 0;
+}
+
 int qcom_icc_rpmh_probe(struct platform_device *pdev)
 {
 	const struct qcom_icc_desc *desc;
@@ -424,10 +471,16 @@ int qcom_icc_rpmh_probe(struct platform_device *pdev)
 		if (!qp->voters)
 			return -ENOMEM;
 
+		ret = qcom_icc_init_disabled_parts(qp);
+		if (ret)
+			return ret;
+
 		for (i = 0; i < qp->num_voters; i++) {
-			qp->voters[i] = of_bcm_voter_get(qp->dev, desc->voters[i]);
-			if (IS_ERR(qp->voters[i]))
-				return PTR_ERR(qp->voters[i]);
+			if (!is_voter_disabled(desc->voters[i])) {
+				qp->voters[i] = of_bcm_voter_get(qp->dev, desc->voters[i]);
+				if (IS_ERR(qp->voters[i]))
+					return PTR_ERR(qp->voters[i]);
+			}
 		}
 	}
 
@@ -448,7 +501,7 @@ int qcom_icc_rpmh_probe(struct platform_device *pdev)
 
 	for (i = 0; i < num_nodes; i++) {
 		qn = qnodes[i];
-		if (!qn)
+		if (!qn || qn->disabled)
 			continue;
 
 		qn->regmap = dev_get_regmap(qp->dev, NULL);

MiCode/Xiaomi_Kernel_OpenSource

drivers/interconnect/qcom/icc-rpmh.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/clk.h>
@@ -10,6 +11,7 @@
 #include <linux/of.h>
 #include <linux/of_platform.h>
 #include <linux/slab.h>
+#include <soc/qcom/socinfo.h>
 
 #include "bcm-voter.h"
 #include "icc-debug.h"
@@ -191,7 +193,7 @@ int qcom_icc_bcm_init(struct qcom_icc_bcm *bcm, struct device *dev)
 	int i;
 
 	/* BCM is already initialised*/
-	if (bcm->addr)
+	if (bcm->disabled || bcm->addr)
 		return 0;
 
 	bcm->addr = cmd_db_read_addr(bcm->name);
@@ -318,6 +320,51 @@ static struct regmap *qcom_icc_rpmh_map(struct platform_device *pdev,
 	return devm_regmap_init_mmio(dev, base, desc->config);
 }
 
+static bool is_voter_disabled(char *voter)
+{
+	if ((strnstr(voter, "disp", strlen(voter)) && socinfo_get_part_info(PART_DISPLAY)) ||
+	    (strnstr(voter, "cam", strlen(voter)) && socinfo_get_part_info(PART_CAMERA)))
+		return true;
+
+	return false;
+}
+
+static int qcom_icc_init_disabled_parts(struct qcom_icc_provider *qp)
+{
+	struct qcom_icc_bcm *bcm;
+	struct qcom_icc_node **qnodes, *qn;
+	const struct qcom_icc_desc *desc;
+	int voter_idx, i, j;
+	char *voter_name;
+
+	desc = of_device_get_match_data(qp->dev);
+	if (!desc)
+		return -EINVAL;
+
+	for (i = 0; i < qp->num_bcms; i++) {
+		bcm = qp->bcms[i];
+		voter_idx = bcm->voter_idx;
+		voter_name = desc->voters[voter_idx];
+
+		/* Disable BCMs incase of NO display or No Camera */
+		if (is_voter_disabled(voter_name)) {
+			bcm->disabled = true;
+			qnodes = desc->nodes;
+
+			for (j = 0; j < desc->num_nodes; j++) {
+				qn = qnodes[j];
+				if (!qn)
+					continue;
+
+				if (strnstr(qn->name, voter_name, strlen(qn->name)))
+					qn->disabled = true;
+			}
+		}
+	}
+
+	return 0;
+}
+
 int qcom_icc_rpmh_probe(struct platform_device *pdev)
 {
 	const struct qcom_icc_desc *desc;
@@ -365,10 +412,16 @@ int qcom_icc_rpmh_probe(struct platform_device *pdev)
 	if (!qp->voters)
 		return -ENOMEM;
 
+	ret = qcom_icc_init_disabled_parts(qp);
+	if (ret)
+		return ret;
+
 	for (i = 0; i < qp->num_voters; i++) {
-		qp->voters[i] = of_bcm_voter_get(qp->dev, desc->voters[i]);
-		if (IS_ERR(qp->voters[i]))
-			return PTR_ERR(qp->voters[i]);
+		if (desc->voters[i] && !is_voter_disabled(desc->voters[i])) {
+			qp->voters[i] = of_bcm_voter_get(qp->dev, desc->voters[i]);
+			if (IS_ERR(qp->voters[i]))
+				return PTR_ERR(qp->voters[i]);
+		}
 	}
 
 	qp->regmap = qcom_icc_rpmh_map(pdev, desc);
@@ -395,7 +448,7 @@ int qcom_icc_rpmh_probe(struct platform_device *pdev)
 	for (i = 0; i < num_nodes; i++) {
 		size_t j;
 
-		if (!qnodes[i])
+		if (!qnodes[i] || qnodes[i]->disabled)
 			continue;
 
 		qnodes[i]->regmap = dev_get_regmap(qp->dev, NULL);