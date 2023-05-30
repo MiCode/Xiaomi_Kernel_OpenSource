57ca0cd9c717b1515a74098fad063dab1f5a6973
mtk26843 <victor-wc.lin@mediatek.com>
2023-05-30 13:31:55 +0800

    [ALPS08092574] PT support CPU throttle by cluster
    PT support CPU throttle by cluster
    
    MTK-Commit-Id: d92ca81add1b717667a05a8c74b0e109e25d2970
    
    Signed-off-by: mtk26843 <victor-wc.lin@mediatek.com>
    CR-Id: ALPS08092574
    Feature: Dynamic load power throttling
    Change-Id: I07d8e3bc4e07142945efdda9697041901a173926

--- drivers/misc/mediatek/power_throttling/mtk_cpu_power_throttling.h
+++ drivers/misc/mediatek/power_throttling/mtk_cpu_power_throttling.h
@@ -3,23 +3,21 @@
  * Copyright (c) 2020 MediaTek Inc.
  * Author: Samuel Hsieh <samuel.hsieh@mediatek.com>
  */
-
 #ifndef _MTK_CPU_POWER_THROTTLING_H_
 #define _MTK_CPU_POWER_THROTTLING_H_
-
 enum cpu_pt_type {
 	LBAT_POWER_THROTTLING,
 	OC_POWER_THROTTLING,
+	SOC_POWER_THROTTLING,
 	POWER_THROTTLING_TYPE_MAX
 };
-
 struct cpu_pt_policy {
 	enum cpu_pt_type           pt_type;
+	unsigned int               pt_max_lv;
 	unsigned int               cpu;
-	s32                        cpu_limit;
+	s32                        *cpu_limit;
 	struct freq_qos_request    qos_req;
 	struct cpufreq_policy      *policy;
 	struct list_head           cpu_pt_list;
 };
-
 #endif
