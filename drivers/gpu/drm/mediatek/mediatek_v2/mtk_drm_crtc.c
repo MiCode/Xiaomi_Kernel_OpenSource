8510b0a3ebae4101f9595c30af7e370bf0816086
William Yang <william-tw.yang@mediatek.com>
2023-05-30 13:31:24 +0800

    [ALPS08033814] DISP: put mml context only when CRTC0 suspend
    put mml context only when CRTC0 suspend, not for all CRTC
    
    MTK-Commit-Id: a85b84c5595c706a7f63ddf96156e9ad7a25eb58
    
    CR-Id: ALPS08033814
    Feature: display_mobile
    Signed-off-by: William Yang <william-tw.yang@mediatek.com>
    Change-Id: I5c454d3fdcafba516e0315ace6b53cfaf6570398

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
@@ -10572,7 +10572,7 @@ void mtk_drm_crtc_disable(struct drm_crtc *crtc, bool need_wait)
 			comp->fb = NULL;
 	}
 
-	if (priv->mml_ctx && mml_drm_ctx_idle(priv->mml_ctx)) {
+	if ((crtc_id == 0) && priv && priv->mml_ctx && mml_drm_ctx_idle(priv->mml_ctx)) {
 		mml_drm_put_context(priv->mml_ctx);
 		priv->mml_ctx = NULL;
 	}
