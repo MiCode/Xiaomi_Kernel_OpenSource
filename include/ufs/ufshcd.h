6e4e15dc60110eff019ef32a1d79451d01105c40
Stanley Chu <stanley.chu@mediatek.com>
2023-06-04 11:00:33 +0800

    [ALPS08142188] ufs-mcq: Fix config for ufshcd_mcq_poll_cqe_nolock()
    Fix config for ufshcd_mcq_poll_cqe_nolock().
    
    Signed-off-by: Stanley Chu <stanley.chu@mediatek.com>
    CR-Id: ALPS08142188
    Feature: ufs
    Change-Id: Ia48dc096a6893a8eba02b6253180376cc6d573ea

--- include/ufs/ufshcd.h
+++ include/ufs/ufshcd.h
@@ -1302,7 +1302,7 @@ void ufshcd_schedule_eh_work(struct ufs_hba *hba);
 void ufshcd_mcq_config_mac(struct ufs_hba *hba, u32 max_active_cmds);
 u32 ufshcd_mcq_read_cqis(struct ufs_hba *hba, int i);
 void ufshcd_mcq_write_cqis(struct ufs_hba *hba, u32 val, int i);
-#if !IS_ENABLED(CONFIG_SCSI_UFS_MEDIATEK_DBG)
+#if !IS_ENABLED(CONFIG_MTK_UFS_DEBUG)
 unsigned long ufshcd_mcq_poll_cqe_nolock(struct ufs_hba *hba,
 					 struct ufs_hw_queue *hwq);
 #else
