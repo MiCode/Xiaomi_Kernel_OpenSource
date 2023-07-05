9d9e6c19c3ce7214283bf87753a337f5d5943a0c
Chunhui Li <chunhui.li@mediatek.com>
2023-07-05 11:52:10 +0800

    [ALPS08201866] BACKPORT: irqchip/gic-v3: Workaround for erratum 2941627
    GIC700 erratum 2941627 may cause GIC-700 missing SPIs wake
    requests when SPIs are deactivated while targeting a
    sleeping CPU - ie a CPU for which the redistributor:
    
    GICR_WAKER.ProcessorSleep == 1
    
    This runtime situation can happen if an SPI that has been
    activated on a core is retargeted to a different core, it
    becomes pending and the target core subsequently enters a
    power state quiescing the respective redistributor.
    
    When this situation is hit, the de-activation carried out
    on the core that activated the SPI (through either ICC_EOIR1_EL1
    or ICC_DIR_EL1 register writes) does not trigger a wake
    requests for the sleeping GIC redistributor even if the SPI
    is pending.
    
    Work around the erratum by de-activating the SPI using the
    redistributor GICD_ICACTIVER register if the runtime
    conditions require it (ie the IRQ was retargeted between
    activation and de-activation).
    
    Bug: 292459437
    Signed-off-by: Lorenzo Pieralisi <lpieralisi@kernel.org>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20230704155034.148262-1-lpieralisi@kernel.org
    Change-Id: Ia888cf9397ea979f5b3070161eea124b54237dca
    CR-Id: ALPS08201866
    Feature: gic
    Signed-off-by: Chunhui Li <chunhui.li@mediatek.com>

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -47,6 +47,10 @@ struct redist_region {
 	bool			single_redist;
 };
 
+#if IS_ENABLED(CONFIG_ARM64_ERRATUM_2941627)
+static DEFINE_STATIC_KEY_FALSE(gic_arm64_2941627_erratum);
+#endif
+
 static struct gic_chip_data_v3 gic_data __read_mostly;
 static DEFINE_STATIC_KEY_TRUE(supports_deactivate_key);
 
@@ -547,10 +551,39 @@ static void gic_irq_nmi_teardown(struct irq_data *d)
 	gic_irq_set_prio(d, GICD_INT_DEF_PRI);
 }
 
+#if IS_ENABLED(CONFIG_ARM64_ERRATUM_2941627)
+static bool gic_arm64_erratum_2941627_needed(struct irq_data *d)
+{
+	if (!static_branch_unlikely(&gic_arm64_2941627_erratum))
+		return false;
+
+	/*
+	 * The workaround is needed if the IRQ is an SPI and
+	 * the target cpu is different from the one we are
+	 * executing on.
+	 */
+	return !((gic_irq_in_rdist(d)) || gic_irq(d) >= 8192 ||
+		  cpumask_equal(irq_data_get_effective_affinity_mask(d),
+				cpumask_of(smp_processor_id())));
+}
+#endif
+
 static void gic_eoi_irq(struct irq_data *d)
 {
 	write_gicreg(gic_irq(d), ICC_EOIR1_EL1);
 	isb();
+
+#if IS_ENABLED(CONFIG_ARM64_ERRATUM_2941627)
+	if (gic_arm64_erratum_2941627_needed(d)) {
+		/*
+		 * Make sure the GIC stream deactivate packet
+		 * issued by ICC_EOIR1_EL1 has completed before
+		 * deactivating through GICD_IACTIVER.
+		 */
+		dsb(sy);
+		gic_poke_irq(d, GICD_ICACTIVER);
+	}
+#endif
 }
 
 static void gic_eoimode1_eoi_irq(struct irq_data *d)
@@ -561,7 +594,13 @@ static void gic_eoimode1_eoi_irq(struct irq_data *d)
 	 */
 	if (gic_irq(d) >= 8192 || irqd_is_forwarded_to_vcpu(d))
 		return;
-	gic_write_dir(gic_irq(d));
+
+#if IS_ENABLED(CONFIG_ARM64_ERRATUM_2941627)
+	if (!gic_arm64_erratum_2941627_needed(d))
+		gic_write_dir(gic_irq(d));
+	else
+		gic_poke_irq(d, GICD_ICACTIVER);
+#endif
 }
 
 static int gic_set_type(struct irq_data *d, unsigned int type)
@@ -1747,6 +1786,27 @@ static bool gic_enable_quirk_hip06_07(void *data)
 	return false;
 }
 
+#if IS_ENABLED(CONFIG_ARM64_ERRATUM_2941627)
+static bool gic_enable_quirk_arm64_2941627(void *data)
+{
+	/*
+	 * If CPUidle is not enabled the erratum runtime
+	 * conditions can't be hit, since that requires:
+	 *
+	 * - A core entering a deep power state with
+	 *   the associated GIC redistributor asleep
+	 *   and an IRQ active and pending targeted at it
+	 * - A different core handling the IRQ and
+	 *   related GIC operations at the same time
+	 */
+	if (!IS_ENABLED(CONFIG_CPU_IDLE))
+		return false;
+
+	static_branch_enable(&gic_arm64_2941627_erratum);
+	return true;
+}
+#endif
+
 static const struct gic_quirk gic_quirks[] = {
 	{
 		.desc	= "GICv3: Qualcomm MSM8996 broken firmware",
@@ -1778,6 +1838,27 @@ static const struct gic_quirk gic_quirks[] = {
 		.mask	= 0xe8f00fff,
 		.init	= gic_enable_quirk_cavium_38539,
 	},
+#if IS_ENABLED(CONFIG_ARM64_ERRATUM_2941627)
+	{
+		/*
+		 * GIC-700: 2941627 workaround - IP variant [0,1]
+		 *
+		 */
+		.desc	= "GICv3: ARM64 erratum 2941627",
+		.iidr	= 0x0400043b,
+		.mask	= 0xff0e0fff,
+		.init	= gic_enable_quirk_arm64_2941627,
+	},
+	{
+		/*
+		 * GIC-700: 2941627 workaround - IP variant [2]
+		 */
+		.desc	= "GICv3: ARM64 erratum 2941627",
+		.iidr	= 0x0402043b,
+		.mask	= 0xff0f0fff,
+		.init	= gic_enable_quirk_arm64_2941627,
+	},
+#endif
 	{
 	}
 };
