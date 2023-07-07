b5ed7b787a95ac7e6f03472748c949e305111471
Elliot Berman <quic_eberman@quicinc.com>
2023-07-07 15:38:32 -0700

    virt: gunyah: Correct max_buf_size for a connection
    Continuation messages can carry a maximum payload of
    GH_RM_MAX_MSG_SIZE_BYTES. When initializing a connection buffer for
    reply sequences, the hdr_size is 4 bytes larger than the
    GH_RM_MAX_MSG_SIZE_BYTES: see the difference between
    struct gh_rm_rpc_hdr and struct gh_rm_rpc_reply_hdr.
    
    Fix the calculation.
    
    Change-Id: If7a1124f581c9d1da8f7749d0296e064e6499cdf
    Signed-off-by: Elliot Berman <quic_eberman@quicinc.com>

--- drivers/virt/gunyah/gh_rm_core.c
+++ drivers/virt/gunyah/gh_rm_core.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  */
 
@@ -162,8 +163,8 @@ gh_rm_init_connection_buff(struct gh_rm_connection *connection,
 	if (!payload_size)
 		return 0;
 
-	max_buf_size = (GH_MSGQ_MAX_MSG_SIZE_BYTES - hdr_size) *
-			(hdr->fragments + 1);
+	max_buf_size = payload_size +
+			(hdr->fragments * GH_RM_MAX_MSG_SIZE_BYTES);
 
 	if (payload_size > max_buf_size) {
 		pr_err("%s: Payload size exceeds max buff size\n", __func__);
