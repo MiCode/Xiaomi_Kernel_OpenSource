f9f12d7c0f9977ddb03e67521bc8427946536e63
Tony Leung <tony.leung@mediatek.com>
2023-08-18 19:56:25 +0800

    [ALPS08274506] jpeg dec: fix race condition missing lock
    [Details]
    1. Add mutex lock to protect is_dec_started
    
    MTK-Commit-Id: c5ad92db594b73a81f395782569bced9d6512557
    
    Signed-off-by: Tony Leung <tony.leung@mediatek.com>
    CR-Id: ALPS08274506
    Feature: jpeg_dec
    Change-Id: I80ad94b34663b1944494ebaeb4b0d6985897dd43

--- drivers/misc/mediatek/jpeg/jpeg_drv.c
+++ drivers/misc/mediatek/jpeg/jpeg_drv.c
@@ -566,10 +566,13 @@ static int jpeg_hybrid_dec_ioctl(unsigned int cmd, unsigned long arg,
 			JPEG_LOG(0, "get hybrid dec id failed");
 			return -EFAULT;
 		}
+		mutex_lock(&jpeg_hybrid_dec_lock);
 		if (!gJpegqDev.is_dec_started[hwid]) {
 			JPEG_LOG(0, "Wait before decode get started");
+			mutex_unlock(&jpeg_hybrid_dec_lock);
 			return -EFAULT;
 		}
+		mutex_unlock(&jpeg_hybrid_dec_lock);
 	#ifdef FPGA_VERSION
 		JPEG_LOG(1, "Polling JPEG Hybrid Dec Status hwid: %d",
 				hwid);
