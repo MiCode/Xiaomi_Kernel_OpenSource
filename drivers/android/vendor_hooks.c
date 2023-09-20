09799ff8e8d2fba23d8f429be75e1ea789f3fe9d
Liangjun Kuang <ot_liangjun.kuang@mediatek.com>
2023-09-21 04:54:47 +0800

    [ALPS08332943] ACK: Merge android12-5.10-2023-07 R4 into alps-mp-s0.mp1
    [Target]:
        593c61caffd9c51a8e2052ab10d0fc1575d922b6
        ANDROID: GKI: Update symbols for zebra
    
    [Source:] kernel/common android12-5.10-2023-07
    [From:] 505b4a192915 UPSTREAM: usb: gadget: udc: renesas_usb3: Fix use after free bug in renesas_usb3_remove due to race condition
    [  To:] 593c61caffd9 ANDROID: GKI: Update symbols for zebra
    
    MTK-Commit-Id: 2a2186cc47d3d6836b253f56ba90825a4cc9b7fe
    
    Signed-off-by: Liangjun Kuang <ot_liangjun.kuang@mediatek.com>
    Feature: Kernel SI Operation
    CR-Id: ALPS08332943
    Change-Id: Icc4e27e55ba3b5b91f98ce183acc82ab65f5a368

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -76,6 +76,7 @@
 #include <trace/hooks/ipv4.h>
 #include <trace/hooks/pci.h>
 #include <trace/hooks/dmabuf.h>
+#include <trace/hooks/wakeupbypass.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -488,6 +489,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_look_around);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_look_around_migrate_page);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_test_clear_look_around_ref);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_dma_buf_stats_teardown);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_wakeup_bypass);
 /*
  * For type visibility
  */
--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -41,6 +41,8 @@
 #include <asm/current.h>
 #endif
 
+#undef CREATE_TRACE_POINTS
+#include <trace/hooks/wakeupbypass.h>
 
 /**
  * struct alarm_base - Alarm timer bases
@@ -298,6 +300,7 @@ static int alarmtimer_suspend(struct device *dev)
 #ifdef CONFIG_ALARMTIMER_DEBUG
 	struct rtc_time time;
 #endif
+	int wakeup_bypass_enabled = 0;
 
 	spin_lock_irqsave(&freezer_delta_lock, flags);
 	min = freezer_delta;
@@ -305,6 +308,10 @@ static int alarmtimer_suspend(struct device *dev)
 	type = freezer_alarmtype;
 	freezer_delta = 0;
 	spin_unlock_irqrestore(&freezer_delta_lock, flags);
+
+	trace_android_vh_wakeup_bypass(&wakeup_bypass_enabled);
+	if (wakeup_bypass_enabled)
+		return 0;
 
 	rtc = alarmtimer_get_rtcdev();
 	/* If we have no rtcdev, just return */
